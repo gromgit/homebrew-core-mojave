c4f7c188fe2c0c77754b7ecdf30ac66f47e8a750
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-18 23:30:00 +0800

    docutils: update 0.18 bottle.

--- Formula/docutils.rb
+++ Formula/docutils.rb
@@ -6,6 +6,11 @@ class Docutils < Formula
   url "https://downloads.sourceforge.net/project/docutils/docutils/0.18/docutils-0.18.tar.gz"
   sha256 "c1d5dab2b11d16397406a282e53953fe495a46d69ae329f55aa98a5c4e3c5fbb"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/docutils"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "c5480ce8cf185bb07c091402d7ccfef2de6510e863402c43b2355a2a2745615c"
+  end
 
   depends_on "python@3.10"
 
