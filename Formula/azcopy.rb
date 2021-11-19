45f7229f64a8d50ecb86c0753fe9115887a03d2a
Adrian Ho <the.gromgit@gmail.com>
2021-11-19 21:55:57 +0800

    azcopy: update 10.13.0 bottle.

--- Formula/azcopy.rb
+++ Formula/azcopy.rb
@@ -5,6 +5,11 @@ class Azcopy < Formula
   sha256 "ff3b199a044bb16abc70b1d221f4b92e693b90e204c6ca7ff27d1c857b02d444"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/azcopy"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "2e12c0fd6cecb877ff14341aa66be5182229fd88f57f54e457c6f25eaabae472"
+  end
 
   depends_on "go" => :build
 
