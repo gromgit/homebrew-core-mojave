70b51a9860cf69a2c48257cd19940778242daf42
Adrian Ho <the.gromgit@gmail.com>
2021-11-20 23:34:44 +0800

    dfmt: update 0.14.1 bottle.

--- Formula/dfmt.rb
+++ Formula/dfmt.rb
@@ -7,6 +7,11 @@ class Dfmt < Formula
   license "BSL-1.0"
   head "https://github.com/dlang-community/dfmt.git", branch: "v0.x.x"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dfmt"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "15e15d8e90ab548d9620608084d17914f0918042f4c0c1294c1b4bd71502255d"
+  end
 
   depends_on "dmd" => :build
 
