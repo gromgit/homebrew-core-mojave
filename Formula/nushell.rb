d3fd34037ddf8791eb4d2ca0d0261e9ed1a5b619
Adrian Ho <the.gromgit@gmail.com>
2021-11-21 03:39:33 +0800

    nushell: update 0.40.0 bottle.

--- Formula/nushell.rb
+++ Formula/nushell.rb
@@ -1,8 +1,8 @@
 class Nushell < Formula
   desc "Modern shell for the GitHub era"
   homepage "https://www.nushell.sh"
-  url "https://github.com/nushell/nushell/archive/0.39.0.tar.gz"
-  sha256 "86eb8079b88896dfbfb8e165adba75a7de3477b21f20ee8282efff2f0ab1356b"
+  url "https://github.com/nushell/nushell/archive/0.40.0.tar.gz"
+  sha256 "fe8a7084cf2962b49144971375b7d07f32ba96fca267b5087d03f298ee53d106"
   license "MIT"
   head "https://github.com/nushell/nushell.git", branch: "main"
 
@@ -12,6 +12,10 @@ class Nushell < Formula
     regex(%r{href=.*?/tag/v?(\d+(?:[._]\d+)+)["' >]}i)
   end
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nushell"
+    sha256 cellar: :any_skip_relocation, mojave: "9ba367c3904b19acd2b6276968368abe383cf59b8d94ef42edc6030e183af842"
+  end
 
   depends_on "rust" => :build
   depends_on "openssl@1.1"
