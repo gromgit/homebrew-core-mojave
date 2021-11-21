f8c640e3fe8d99f74473c69b4822edf534e967c4
Adrian Ho <the.gromgit@gmail.com>
2021-11-21 16:31:13 +0800

    rke: update 1.3.2 bottle.

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -5,6 +5,11 @@ class Rke < Formula
   sha256 "532fb0751eb7ca9cf855e49e433d63fb760bdf8e131af3440744e792338f706c"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rke"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "be10867f64fccd10324844a772f713808c9cd78e51542261ce9741b150d51545"
+  end
 
   depends_on "go" => :build
 
