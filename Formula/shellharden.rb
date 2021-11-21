a11aa97a5cd3736ae666d39de41e84a4ef80452a
Adrian Ho <the.gromgit@gmail.com>
2021-11-21 11:50:30 +0800

    shellharden: update 4.1.3 bottle.

--- Formula/shellharden.rb
+++ Formula/shellharden.rb
@@ -5,6 +5,11 @@ class Shellharden < Formula
   sha256 "1ea7f3af1346738689bf41f2be2a8be2285c2d66b55fe71999c0d82f50a1bdc0"
   license "MPL-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/shellharden"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "77c24d8da9328d802a9eba8dafd94fb3253e5d8326abc7b755b050a45ab898f5"
+  end
 
   depends_on "rust" => :build
 
