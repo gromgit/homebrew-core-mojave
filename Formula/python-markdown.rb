230d2b466faa8c0ca50be9310468c331446765f3
Adrian Ho <the.gromgit@gmail.com>
2021-11-21 11:21:41 +0800

    python-markdown: update 3.3.6 bottle.

--- Formula/python-markdown.rb
+++ Formula/python-markdown.rb
@@ -3,20 +3,14 @@ class PythonMarkdown < Formula
 
   desc "Python implementation of Markdown"
   homepage "https://python-markdown.github.io"
-  url "https://files.pythonhosted.org/packages/49/02/37bd82ae255bb4dfef97a4b32d95906187b7a7a74970761fca1360c4ba22/Markdown-3.3.4.tar.gz"
-  sha256 "31b5b491868dcc87d6c24b7e3d19a0d730d59d3e46f4eea6430a321bed387a49"
+  url "https://files.pythonhosted.org/packages/15/06/d60f21eda994b044cbd496892d4d4c5c708aa597fcaded7d421513cb219b/Markdown-3.3.6.tar.gz"
+  sha256 "76df8ae32294ec39dcf89340382882dfa12975f87f45c3ed1ecdb1e8cefc7006"
   license "BSD-3-Clause"
-  revision 1
   head "https://github.com/Python-Markdown/markdown.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "93492746ee664f8ac52c7b183910a1194e4301683c977c2aeb71c172e5119796"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7a0926148f224393a9d718e9ae032d503af50e4651aa799aacfcd6ae234f4909"
-    sha256 cellar: :any_skip_relocation, monterey:       "72e6cea45020e5c5cad848c72ca1a90debba6347a38cddedcf29375f2a5920f4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "17410bd96abac23079f5746f78077457b2abed0fafeb544f0b0e28ee23451587"
-    sha256 cellar: :any_skip_relocation, catalina:       "cd69e83d7367882f20a1704c583ea03f8b70af2b9e4b92d6575eaea674951c62"
-    sha256 cellar: :any_skip_relocation, mojave:         "4c511d30c1aac5d2db8bc48143cee680c5cfc0447aebf25e225270a1e54ada4f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "16cf6291aa7788af874d97da72492b7de0dc16c2eaa15c4d11a39ad2cf756a96"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/python-markdown"
+    sha256 cellar: :any_skip_relocation, mojave: "e93947320e535b92b79a599b5d3b090bd225d50938815b4a33542ab73cddcdb4"
   end
 
   depends_on "python@3.10"
