bee6c5597f5781f8e5dc56aaaa5948bcf43d2c17
Adrian Ho <the.gromgit@gmail.com>
2021-11-21 09:52:44 +0800

    tiger-vnc: update 1.12.0 bottle.

--- Formula/tiger-vnc.rb
+++ Formula/tiger-vnc.rb
@@ -1,19 +1,13 @@
 class TigerVnc < Formula
   desc "High-performance, platform-neutral implementation of VNC"
   homepage "https://tigervnc.org/"
-  url "https://github.com/TigerVNC/tigervnc/archive/v1.11.0.tar.gz"
-  sha256 "3648eca472a92a4e8fe55b27cd397b1bf16bad0b24a3a1988661f44553f5e2c3"
+  url "https://github.com/TigerVNC/tigervnc/archive/v1.12.0.tar.gz"
+  sha256 "9ff3f3948f2a4e8cc06ee598ee4b1096beb62094c13e0b1462bff78587bed789"
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 arm64_monterey: "1746ae1a34004bdf64450a397efdf0afa5ea3a5248bef4ed42d11320c5301a3c"
-    sha256 arm64_big_sur:  "d4e19469518f0167a6abf40625c3c21df88bff1b2ca19bb6feac1c4a5a077cd2"
-    sha256 monterey:       "dd45f81d38c8dd9eb9249f10df1a89b2d14a3df35e0dc394e5fb1132222db964"
-    sha256 big_sur:        "ae46d4c867f0f761368e22c80daa9e0805c15f7fc5855bf32d37575c36168367"
-    sha256 catalina:       "b9a09483c45610c81dd29fc20a41b4fa8120e1353f736bb637732d4788e4bb28"
-    sha256 mojave:         "c90bdf1ac012129c5d4caecd3e5acf2d110ca8cd68a8bcff6de07373149424db"
-    sha256 high_sierra:    "2370d829c67ca1df886e47aca162c68034e138a10a93b846c31f1c927d84c435"
-    sha256 x86_64_linux:   "a1aa65f57be2296d34ddf09f7f0e84d0357b1c9391296bcecdd2e28ff3fda483"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tiger-vnc"
+    sha256 cellar: :any, mojave: "c070345fd9448a81ad2b083581f13fe4a63be1894cfc8bb64bfb79da84d1fa84"
   end
 
   depends_on "cmake" => :build
