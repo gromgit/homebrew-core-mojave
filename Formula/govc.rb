c5e66adf9467df2b19c3d9ef74d9b62af6ae1e41
Adrian Ho <the.gromgit@gmail.com>
2021-11-21 11:27:54 +0800

    govc: update 0.27.1 bottle.

--- Formula/govc.rb
+++ Formula/govc.rb
@@ -5,6 +5,11 @@ class Govc < Formula
   sha256 "8b5f0748b6bbbd1088950769bb82c44431478a9e80f729ab791fff9862d89a09"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/govc"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "2d92feccb1915bd5a53ffd3ac81eded328d564da7be04371476ab19d15045176"
+  end
 
   depends_on "go" => :build
 
