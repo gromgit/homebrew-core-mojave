7923cb6cc875f8eb776f7c9bbf1ac820008f04f7
Adrian Ho <the.gromgit@gmail.com>
2021-11-22 02:12:57 +0800

    diskus: update 0.7.0 bottle.

--- Formula/diskus.rb
+++ Formula/diskus.rb
@@ -5,6 +5,11 @@ class Diskus < Formula
   sha256 "64b1b2e397ef4de81ea20274f98ec418b0fe19b025860e33beaba5494d3b8bd1"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/diskus"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "1c79b69278e8a8a634451cafd30e5891a2de44c3f741698fa67f9171d6ca126c"
+  end
 
   depends_on "rust" => :build
 
