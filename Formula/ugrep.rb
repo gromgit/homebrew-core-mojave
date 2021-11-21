6119f5eea0a7074839f4d6d9a02900ff86e07fdd
Adrian Ho <the.gromgit@gmail.com>
2021-11-21 11:14:28 +0800

    ugrep: update 3.3.8 bottle.

--- Formula/ugrep.rb
+++ Formula/ugrep.rb
@@ -5,6 +5,11 @@ class Ugrep < Formula
   sha256 "1bdc73ca5fc12a41c2f7949900edee0972ca3e5f39066c8d68449846df4cf1d7"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ugrep"
+    rebuild 1
+    sha256 mojave: "565274e415c9c8e9ccb5a88ee592afecc5b638fe2c2d778e168a14021a5118ad"
+  end
 
   depends_on "pcre2"
   depends_on "xz"
