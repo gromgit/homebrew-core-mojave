bda6ca9d38358267099948b141f6fc7d57c71fb8
Adrian Ho <the.gromgit@gmail.com>
2021-11-21 10:53:39 +0800

    circleci: update 0.1.16277 bottle.

--- Formula/circleci.rb
+++ Formula/circleci.rb
@@ -3,11 +3,15 @@ class Circleci < Formula
   homepage "https://circleci.com/docs/2.0/local-cli/"
   # Updates should be pushed no more frequently than once per week.
   url "https://github.com/CircleCI-Public/circleci-cli.git",
-      tag:      "v0.1.16225",
-      revision: "8d5e389c1b2a6f134b145be6dbc804cf2f96aabd"
+      tag:      "v0.1.16277",
+      revision: "0aee802464471fcd2d06b18b9a501da241afdf72"
   license "MIT"
   head "https://github.com/CircleCI-Public/circleci-cli.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/circleci"
+    sha256 cellar: :any_skip_relocation, mojave: "87a9bc41995a0230a05f5227943c41522342cda840a075a41a5ff343cfb3b761"
+  end
 
   depends_on "go" => :build
   depends_on "packr" => :build
