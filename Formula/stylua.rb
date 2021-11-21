2fa8829dfdf067cfd6ac9b1366f4b58a73ad4b0d
Adrian Ho <the.gromgit@gmail.com>
2021-11-21 10:37:42 +0800

    stylua: update 0.11.2 bottle.

--- Formula/stylua.rb
+++ Formula/stylua.rb
@@ -5,6 +5,11 @@ class Stylua < Formula
   sha256 "e870551aab62194b92fcb73607c7b7f6e4a41ad75a64e67f18e5ce0a6c608573"
   license "MPL-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/stylua"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "2140a4295d5d097beff2e20f7126969aa35ae352d86d52f5268a48704a5d349b"
+  end
 
   depends_on "rust" => :build
 
