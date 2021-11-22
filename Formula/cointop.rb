4a840fca439be0948c0fefb7c76607a443ca3db8
Adrian Ho <the.gromgit@gmail.com>
2021-11-22 09:07:35 +0800

    cointop: update 1.6.10 bottle.

--- Formula/cointop.rb
+++ Formula/cointop.rb
@@ -5,6 +5,11 @@ class Cointop < Formula
   sha256 "18da0d25288deec7156ddd1d6923960968ab4adcdc917f85726b97d555d9b1b7"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cointop"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "a5034e0945383b67ba0d96301b6ccf3dd791476f6c35312553c86d878e8b4bb6"
+  end
 
   depends_on "go" => :build
 
