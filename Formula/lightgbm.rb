df365d0716a3b921f1b68d9ab26436b169f5d7a9
Adrian Ho <the.gromgit@gmail.com>
2021-11-24 22:25:16 +0800

    lightgbm: update 3.3.1 bottle.

--- Formula/lightgbm.rb
+++ Formula/lightgbm.rb
@@ -6,6 +6,12 @@ class Lightgbm < Formula
       revision: "d4851c3381495d9a065d49e848fbf291a408477d"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lightgbm"
+    rebuild 1
+    sha256 cellar: :any, mojave: "490a079a5fcecc8eaef97ba07135b4cebca0877dfa482fc9a269f83e129de78b"
+  end
+
   depends_on "cmake" => :build
   depends_on "libomp"
 
