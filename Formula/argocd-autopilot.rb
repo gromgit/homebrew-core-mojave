fd2828d8152655ab3220dc8037ffc50321009392
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-26 07:51:45 +0800

    argocd-autopilot: update 0.2.24 bottle.

--- Formula/argocd-autopilot.rb
+++ Formula/argocd-autopilot.rb
@@ -6,6 +6,12 @@ class ArgocdAutopilot < Formula
       revision: "442e6c2dd70b093aa860b49de3626916107b9824"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/argocd-autopilot"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "034dc212966a548b0a4a8eb4fa39c7b571d71a255485d97ed7c66603b7a593b5"
+  end
+
   depends_on "go" => :build
 
   def install
