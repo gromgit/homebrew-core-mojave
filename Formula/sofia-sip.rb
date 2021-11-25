11148e73cf99df75b80f0c3b5eaf99116462c5ff
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-25 20:08:01 +0800

    sofia-sip: update 1.13.6 bottle.

--- Formula/sofia-sip.rb
+++ Formula/sofia-sip.rb
@@ -5,6 +5,12 @@ class SofiaSip < Formula
   sha256 "6f3cb48a35929abd3454087b32ad4c75fa5fe50fab8a9cc6f98559e6fd1bd64b"
   license "LGPL-2.1-or-later"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sofia-sip"
+    rebuild 1
+    sha256 cellar: :any, mojave: "40ea6af854452a1ada20bc329ff8df51baa46f5f5cba134516c191a34039ec1f"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
