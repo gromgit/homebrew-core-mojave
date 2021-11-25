307f6f390a3f852e8c317424ae29ca513d5080ed
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-26 01:45:01 +0800

    faac: update 1.30 bottle.

--- Formula/faac.rb
+++ Formula/faac.rb
@@ -5,6 +5,12 @@ class Faac < Formula
   sha256 "adc387ce588cca16d98c03b6ec1e58f0ffd9fc6eadb00e254157d6b16203b2d2"
   license "LGPL-2.1-or-later"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/faac"
+    rebuild 1
+    sha256 cellar: :any, mojave: "e5069ba06f84b1c168276b3816ab634f60fb10e62395159fe9334b5d3e229e2e"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
