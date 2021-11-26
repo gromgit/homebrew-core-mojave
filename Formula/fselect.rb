1863406ac40f9b5f959bc953a0ce7887231ffa0b
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-26 23:38:45 +0800

    fselect: update 0.7.8 bottle.

--- Formula/fselect.rb
+++ Formula/fselect.rb
@@ -5,6 +5,12 @@ class Fselect < Formula
   sha256 "9ad3b7e2a8928ac5cf1694a72594ff56ab118dcb01803b780cdf70779355c000"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fselect"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "ea560a9543f9921b2073ecad7cc781b0ccbc9d58c290709487394a2915407f36"
+  end
+
   depends_on "rust" => :build
 
   def install
