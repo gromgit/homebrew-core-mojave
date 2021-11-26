edbc3768bf6337089b1748dde42097feff2a1cd2
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-26 21:16:17 +0800

    fennel: update 1.0.0 bottle.

--- Formula/fennel.rb
+++ Formula/fennel.rb
@@ -5,6 +5,12 @@ class Fennel < Formula
   sha256 "6f619832751af9c37835737cde5cf4475ff90e073ecef4671f9e4f8be2c121a7"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fennel"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "956f9cf02519e7edbad390a8633126a984eba636926f683500c8237e667ac7f5"
+  end
+
   depends_on "lua"
 
   def install
