aa569ed98a37b7286f53b38ae107e84b38a14596
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-26 16:31:54 +0800

    docker-gen: update 0.8.0 bottle.

--- Formula/docker-gen.rb
+++ Formula/docker-gen.rb
@@ -5,6 +5,12 @@ class DockerGen < Formula
   sha256 "3d969c32d1a612d44c8193422649b375ea9cea8d78a044be463d58fe128c525a"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/docker-gen"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "244afe96498034c78cfdc3ee3cbd7e653e223819acbe3bd33fc1029546c1059c"
+  end
+
   depends_on "go" => :build
 
   def install
