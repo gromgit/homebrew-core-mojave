f3a096a5dca9dd4cc770bbb557164ae4ad176691
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-26 09:16:57 +0800

    bibutils: update 7.2 bottle.

--- Formula/bibutils.rb
+++ Formula/bibutils.rb
@@ -10,6 +10,12 @@ class Bibutils < Formula
     regex(%r{url=.*?/bibutils[._-]v?(\d+(?:\.\d+)+)[._-]src\.t}i)
   end
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bibutils"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "7c84176f930de87db9a670d9dd96edcdbd9085ab948d9f46a8d2554e9c883d7a"
+  end
+
   def install
     system "./configure", "--install-dir", bin,
                           "--install-lib", lib
