1f6c0b555b3c970717721fd59722d05481ef3fbd
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-26 14:01:52 +0800

    cpufetch: update 1.01 bottle.

--- Formula/cpufetch.rb
+++ Formula/cpufetch.rb
@@ -6,6 +6,12 @@ class Cpufetch < Formula
   license "MIT"
   head "https://github.com/Dr-Noob/cpufetch.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cpufetch"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "2c8f5f451cbfa1e80db42452cf934633dd1e52910228802798db4294922f1f2f"
+  end
+
   def install
     system "make"
     bin.install "cpufetch"
