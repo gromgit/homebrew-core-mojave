2704e45cd5b79a84dfd1747be47dd00bca1cfdd8
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-26 15:23:26 +0800

    dasel: update 1.22.1 bottle.

--- Formula/dasel.rb
+++ Formula/dasel.rb
@@ -6,6 +6,12 @@ class Dasel < Formula
   license "MIT"
   head "https://github.com/TomWright/dasel.git"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dasel"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "3e6b31b9020675d6bf4be98d266ac7e79e33f39c32190e9ce740a91ce7037f52"
+  end
+
   depends_on "go" => :build
 
   def install
