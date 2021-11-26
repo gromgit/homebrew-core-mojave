a0b565e614e1467f51e0ce2688805f2de4f8fbd1
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-26 16:12:44 +0800

    di: update 4.51 bottle.

--- Formula/di.rb
+++ Formula/di.rb
@@ -10,6 +10,12 @@ class Di < Formula
     regex(%r{url=.*?/di[._-]v?(\d+(?:\.\d+)+)\.t}i)
   end
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/di"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "dda18e67b11b5332940baf32c2df9d89c3aa428e90cf09af513116134849deb3"
+  end
+
   def install
     system "make", "install", "prefix=#{prefix}"
   end
