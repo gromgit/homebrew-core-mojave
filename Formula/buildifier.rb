e8936f7e097025602ca57d9bbd8673ca6e41df7a
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-26 09:54:33 +0800

    buildifier: update 4.2.3 bottle.

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -6,6 +6,12 @@ class Buildifier < Formula
   license "Apache-2.0"
   head "https://github.com/bazelbuild/buildtools.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/buildifier"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "46b656e9a2a94354a1ee38c5dcaef99b935ebb19040f615ef5575e3352f6bb65"
+  end
+
   depends_on "go" => :build
 
   def install
