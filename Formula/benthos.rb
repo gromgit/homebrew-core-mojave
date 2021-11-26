4d30e68eab8f90e5bc2452b8c4fbcb5817dd2636
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-26 09:12:52 +0800

    benthos: update 3.58.0 bottle.

--- Formula/benthos.rb
+++ Formula/benthos.rb
@@ -5,6 +5,12 @@ class Benthos < Formula
   sha256 "a7a71f3dff5d3b9e3acefe66a553f61b75bbd4ce98fad32a2c4a178bb6d20587"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/benthos"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "af9a1d7c4f9dddc94c379b1db5cc8987cf6acf3e1d1eddc32d323a98c7216f42"
+  end
+
   depends_on "go" => :build
 
   def install
