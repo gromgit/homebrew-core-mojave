e052fb06bcae4f50e810be5476b2c8efa3e6f478
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-28 05:55:45 +0800

    qpdf: update 10.4.0 bottle.

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -5,6 +5,12 @@ class Qpdf < Formula
   sha256 "9ac6e691cc3f35a9fe44632e3fba727e1b6ef21181c0a883287abf5cf97ae222"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/qpdf"
+    rebuild 1
+    sha256 cellar: :any, mojave: "bcfc3e80865e065a3d4ed587936ac6039a82a15dfb7b9e9f86f1fa81500c8504"
+  end
+
   depends_on "jpeg"
   depends_on "openssl@1.1"
 
