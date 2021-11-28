7d79c1b53d3c3f56cbdd4e15e1db00ad32b35fcd
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-29 01:32:31 +0800

    matterbridge: update 1.23.2 bottle.

--- Formula/matterbridge.rb
+++ Formula/matterbridge.rb
@@ -5,6 +5,12 @@ class Matterbridge < Formula
   sha256 "5f00556b89855db7ce171c91552f51de1d053e463b8c858049a872fadd5c22a6"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/matterbridge"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "68b99a2b466485691d8760d3d93c7e87eb1df33e7c9ca89e2750cd5b5ca60300"
+  end
+
   depends_on "go" => :build
 
   def install
