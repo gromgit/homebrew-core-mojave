436506a544f53241d0a462f752ff044bdf436fcd
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-28 13:31:33 +0800

    findomain: update 5.0.1 bottle.

--- Formula/findomain.rb
+++ Formula/findomain.rb
@@ -5,6 +5,12 @@ class Findomain < Formula
   sha256 "8a235bdfd5c8e63cf077929b0c3d3e0d8d704370d72103b93e701c47e524c27b"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/findomain"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "1a6623b44f77c920e1775b74b6799a2ae6343a5e59352ff03f515002aafe95b1"
+  end
+
   depends_on "rust" => :build
 
   def install
