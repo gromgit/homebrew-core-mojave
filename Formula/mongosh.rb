d011eca9ff096af5af281ddb545b599adef26c25
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-29 04:04:17 +0800

    mongosh: update 1.1.4 bottle.

--- Formula/mongosh.rb
+++ Formula/mongosh.rb
@@ -7,6 +7,12 @@ class Mongosh < Formula
   sha256 "cf9e982617f4f78216d8901f6458a3288e909d4f05ff983237b6197c7e94a5d1"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mongosh"
+    rebuild 1
+    sha256 mojave: "cdf94b31c7909dec036cb0c97c20b041c78fd332c0d23a69a18ab01c3d8367f6"
+  end
+
   depends_on "node@14"
 
   def install
