3907914ccf18417dbaf5ba70cd62fa5e12de6c85
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-28 22:51:28 +0800

    macchina: update 5.0.5 bottle.

--- Formula/macchina.rb
+++ Formula/macchina.rb
@@ -5,6 +5,12 @@ class Macchina < Formula
   sha256 "f8cd45546f3ce1e59e88b5861c1ba538039b39e7802749fff659a6367f097402"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/macchina"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "caf82cd9f33bedf80c74733df67d2ab17f1f2bbe9f3246f0f582845655bdaf05"
+  end
+
   depends_on "rust" => :build
 
   def install
