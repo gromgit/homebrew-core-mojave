65883ecb8e8f2faf5ad10cc73714eed11267e310
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-29 02:27:17 +0800

    mighttpd2: update 4.0.0 bottle.

--- Formula/mighttpd2.rb
+++ Formula/mighttpd2.rb
@@ -5,6 +5,12 @@ class Mighttpd2 < Formula
   sha256 "5afc8acb4e268401dc19964b710230e5013399b8ad3baa7ae6d5e5802ad4ac42"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mighttpd2"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "ecc9464cdf9a02163dcd85e2cc7efb9a565840b6302e882976017feae2087fc8"
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
 
