1332d500207f46be0e8d633b4580fb3878af3203
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-29 23:09:40 +0800

    sheldon: update 0.6.5 bottle.

--- Formula/sheldon.rb
+++ Formula/sheldon.rb
@@ -6,6 +6,12 @@ class Sheldon < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/rossmacarthur/sheldon.git", branch: "trunk"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sheldon"
+    rebuild 1
+    sha256 cellar: :any, mojave: "a10e7ad8cdbbd89036e9071cb6ee6463d193895849e64d30032c24f4374d66e0"
+  end
+
   depends_on "rust" => :build
   depends_on "curl"
   depends_on "openssl@1.1"
