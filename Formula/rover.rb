7839b96bdc582734e337462da31af0be88501ed5
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-29 20:46:15 +0800

    rover: update 0.4.1 bottle.

--- Formula/rover.rb
+++ Formula/rover.rb
@@ -6,6 +6,12 @@ class Rover < Formula
   license "MIT"
   head "https://github.com/apollographql/rover.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rover"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "3ff8ab85d7ea594f48a4e38faa402c704acc2728c9c44280fb8cddbff8fa8471"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
