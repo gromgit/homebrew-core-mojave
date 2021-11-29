72c4206f77592bca21edba179aca4e2c444793b3
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-29 17:23:49 +0800

    qtkeychain: update 0.13.1 bottle.

--- Formula/qtkeychain.rb
+++ Formula/qtkeychain.rb
@@ -5,6 +5,12 @@ class Qtkeychain < Formula
   sha256 "dc84aea039b81f2613c7845d2ac88bad1cf3a06646ec8af0f7276372bb010c11"
   license "BSD-2-Clause"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/qtkeychain"
+    rebuild 1
+    sha256 cellar: :any, mojave: "5da62c1aa67a4f3694fcb605ca223066e3446382efcd3c45b3c77d9f3ce36ec6"
+  end
+
   depends_on "cmake" => :build
   depends_on "qt@5"
 
