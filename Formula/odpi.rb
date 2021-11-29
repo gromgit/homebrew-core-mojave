5e6b1deedf6e7b8f71c87b5cec2fbadf6b250d17
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-29 08:00:19 +0800

    odpi: update 4.3.0 bottle.

--- Formula/odpi.rb
+++ Formula/odpi.rb
@@ -5,6 +5,12 @@ class Odpi < Formula
   sha256 "e8be95e7061cad52caaa98a4d2a25d6bff8fa29c2fa609c3c447124d46b1712b"
   license any_of: ["Apache-2.0", "UPL-1.0"]
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/odpi"
+    rebuild 1
+    sha256 cellar: :any, mojave: "598d3b0cf32316d3e5e84cc0f6de20119e598162c1c52524e26ad0a648871f39"
+  end
+
   def install
     system "make"
 
