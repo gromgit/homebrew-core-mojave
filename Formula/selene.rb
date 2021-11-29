69ce518d1077b2590b5c532af4f38a0c5706a29e
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-29 22:56:58 +0800

    selene: update 0.15.0 bottle.

--- Formula/selene.rb
+++ Formula/selene.rb
@@ -6,6 +6,12 @@ class Selene < Formula
   license "MPL-2.0"
   head "https://github.com/Kampfkarren/selene.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/selene"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "5adf3c75e2afde499ff44cee22b042f56a54d9b970586a67b76204ca441b9c34"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@1.1"
 
