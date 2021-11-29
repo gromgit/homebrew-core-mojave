eb10e82194c93edc41a7ab7dc6297a1f0eda30a0
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-29 08:28:52 +0800

    onefetch: update 2.11.0 bottle.

--- Formula/onefetch.rb
+++ Formula/onefetch.rb
@@ -5,6 +5,12 @@ class Onefetch < Formula
   sha256 "ffd3cc3bd24e299ede1fada2b2da8bf066d59219da167477e1997c860650c192"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/onefetch"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "9cddacf2a9ff4778b217244b6a263a8a23a6956604819ca82fa5cb4219bab36b"
+  end
+
   depends_on "rust" => :build
 
   uses_from_macos "zlib"
