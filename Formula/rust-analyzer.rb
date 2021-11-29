b169749dcf6bb8338ac657e59775a4db42e06c50
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-29 21:36:00 +0800

    rust-analyzer: update 2021-11-22 bottle.

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -7,6 +7,12 @@ class RustAnalyzer < Formula
   version "2021-11-22"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rust-analyzer"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "11744a59467ca5931930591e6af56e3072e5d4d243b59bfd00a0f25d23434ec0"
+  end
+
   depends_on "rust" => :build
 
   def install
