03ce186be18e07b8b97129f81335abac918b0ea7
Adrian Ho <aho+vm-mojave@03s.net>
2021-11-30 12:04:52 +0800

    zydis: update 3.2.1 bottle.

--- Formula/zydis.rb
+++ Formula/zydis.rb
@@ -7,6 +7,12 @@ class Zydis < Formula
   license "MIT"
   head "https://github.com/zyantific/zydis.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/zydis"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "a7d2cda99718ee88ac8d472b0497a2289e00b9be55c79b01c0b575cfb28d98cb"
+  end
+
   depends_on "cmake" => :build
 
   def install
