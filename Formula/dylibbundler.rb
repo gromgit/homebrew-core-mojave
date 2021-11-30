7c29821037336f119a391973dd9dea4815fb8d54
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-01 04:54:02 +0800

    dylibbundler: update 1.0.2 bottle.

--- Formula/dylibbundler.rb
+++ Formula/dylibbundler.rb
@@ -6,6 +6,12 @@ class Dylibbundler < Formula
   license "MIT"
   head "https://github.com/auriamg/macdylibbundler.git"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dylibbundler"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "a93b3cc8703cc414a73dcd6c825e0316fc0611712d12f78493fbe60e67d87b9c"
+  end
+
   def install
     system "make"
     bin.install "dylibbundler"
