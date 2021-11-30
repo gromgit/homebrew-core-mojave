c7aa39e76eead8c333741adc3a98aeb2f4ec5e5b
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-01 04:04:19 +0800

    crane: update 0.7.0 bottle.

--- Formula/crane.rb
+++ Formula/crane.rb
@@ -5,6 +5,12 @@ class Crane < Formula
   sha256 "f2e4d7f2e57811a706e669e75c5d43377d5967ab1846a83304e91b9f35ffce06"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/crane"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "81c34772d4ee29f3d863e6083c2b0264f1b4adaa80ee709083f5871ea0144454"
+  end
+
   depends_on "go" => :build
 
   def install
