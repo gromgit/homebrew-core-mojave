cbb38f10a06de851550160e4cec1a9cfa5e4de6e
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-01 03:25:19 +0800

    cilium-cli: update 0.9.3 bottle.

--- Formula/cilium-cli.rb
+++ Formula/cilium-cli.rb
@@ -5,6 +5,12 @@ class CiliumCli < Formula
   sha256 "ca11474ced97ea7958305cea3d6e80837580764d546874b1e997eb15fef86f74"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cilium-cli"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "8f03eab8badab166ee902101ff18746c998d0c38d1b306ad0b3696da5d6dc17c"
+  end
+
   depends_on "go" => :build
 
   def install
