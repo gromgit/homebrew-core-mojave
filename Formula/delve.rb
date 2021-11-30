abb487cceeec7f09c4a399fa8226ec120f2b9bd9
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-01 04:21:38 +0800

    delve: update 1.7.3 bottle.

--- Formula/delve.rb
+++ Formula/delve.rb
@@ -5,6 +5,12 @@ class Delve < Formula
   sha256 "961642eb4cd97e11093dda81273971a45e64abb2fe7db39165072c7145f4fcec"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/delve"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "211401660270552bf56231eb570489adc738131599e842680dbc5c4ccc34f549"
+  end
+
   depends_on "go" => :build
 
   def install
