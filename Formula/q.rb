66b3ec31e415a3c6464a30e7acc2174f361f2348
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-01 12:59:55 +0800

    q: update 2.0.20 bottle.

--- Formula/q.rb
+++ Formula/q.rb
@@ -8,6 +8,12 @@ class Q < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/harelba/q.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/q"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "a0145ebd0d53b99e3238dc46bbde628d37f820e12b492eb0c4145cd0384705f8"
+  end
+
   deprecate! date: "2021-11-30", because: "requires PyOxidizer, which is a disallowed dependency in homebrew/core"
 
   depends_on "ronn" => :build
