c8cfbb15695b6bb5dfa7d9e4997ed3c5338a36c4
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-01 16:15:09 +0800

    wgcf: update 2.2.10 bottle.

--- Formula/wgcf.rb
+++ Formula/wgcf.rb
@@ -6,6 +6,12 @@ class Wgcf < Formula
   license "MIT"
   head "https://github.com/ViRb3/wgcf.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/wgcf"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "ca3293e6fb4c274508376ac97c7f29dff23b5674ae458b12cbae0959588f2a88"
+  end
+
   depends_on "go" => :build
 
   def install
