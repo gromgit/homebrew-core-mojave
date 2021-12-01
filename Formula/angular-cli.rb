8fb3e2208a6ff47fb1741c3cf99a3df5d78c457e
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-02 03:12:50 +0800

    angular-cli: update 13.0.3 bottle.

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -7,6 +7,12 @@ class AngularCli < Formula
   sha256 "ae7b1ec8c1ca14313cd1884f1a3642dc33b291496de6984bac63fa2d294fd4dd"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/angular-cli"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "88fd8fa1e687db117b1eed3ac74b27b42b5d05b7fbf54a3c32867681ad5f8815"
+  end
+
   depends_on "node"
 
   def install
