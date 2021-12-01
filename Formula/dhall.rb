77e1bbcf8278b14fbac83086148b2fd4aecad4f0
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-02 06:48:14 +0800

    dhall: update 1.40.2 bottle.

--- Formula/dhall.rb
+++ Formula/dhall.rb
@@ -5,6 +5,12 @@ class Dhall < Formula
   sha256 "7e158dd30653c230692ddd5cee700cef6a42c27e61e8c47e007fdfe84e229093"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dhall"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "3d1eb8338b44d219030c8ed0a2ae02383e4190980001a8c40ecb5ad603c74611"
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
 
