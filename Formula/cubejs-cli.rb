cfe8718b65c9ebda036988ffc96a282c2c8c3f23
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-02 06:13:28 +0800

    cubejs-cli: update 0.28.61 bottle.

--- Formula/cubejs-cli.rb
+++ Formula/cubejs-cli.rb
@@ -7,6 +7,12 @@ class CubejsCli < Formula
   sha256 "de3ab78dec52802aad009af4adb87bfbff689a9c1323fc7ef72bb50a72792424"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cubejs-cli"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "351ff71edb6e89c161867e6bf075b06b9769274b442970b6a5afd653dc5b8964"
+  end
+
   depends_on "node"
 
   def install
