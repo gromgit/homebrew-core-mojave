12629d367a0b3b5ac9a7fc2b161d8ce68eb9b283
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-02 05:42:19 +0800

    bitwarden-cli: update 1.19.1 bottle.

--- Formula/bitwarden-cli.rb
+++ Formula/bitwarden-cli.rb
@@ -7,6 +7,12 @@ class BitwardenCli < Formula
   sha256 "20b34a237dd9c93f7fc50e1e216894fc552c95727cdef3688879b83c4b24ffce"
   license "GPL-3.0-only"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bitwarden-cli"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "582a3960885ade8ad8955064664903b0ad2b5e4dd76dd2ac348425b78b37881f"
+  end
+
   depends_on "node"
 
   def install
