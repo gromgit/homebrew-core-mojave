0ec6fe963a87d81ebdc3bf5b5c63bbec347f91d3
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-01 15:30:15 +0800

    ugrep: update 3.3.9 bottle.

--- Formula/ugrep.rb
+++ Formula/ugrep.rb
@@ -5,6 +5,12 @@ class Ugrep < Formula
   sha256 "2526605ab0490d2724a323e3e7822bf825b7181f1df815b6dbe8f0386f08e1d8"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ugrep"
+    rebuild 1
+    sha256 mojave: "0cf7c2a02142a5e3d07549bbdd2b0a7598b69c961478304e3b398f49e5117b43"
+  end
+
   depends_on "pcre2"
   depends_on "xz"
 
