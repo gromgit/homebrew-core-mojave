2c0b81a95aa6ac0185d4166b754f7a1e210e7293
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-02 12:29:18 +0800

    jfrog-cli: update 2.8.0 bottle.

--- Formula/jfrog-cli.rb
+++ Formula/jfrog-cli.rb
@@ -5,6 +5,12 @@ class JfrogCli < Formula
   sha256 "ec17389afe4b049ef78002b6a7826a398d43aa6198bbebf59c9a075963992f43"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jfrog-cli"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "c6be695123c351c212eb12c2c538fb069ee5eac0bc17c16058430281c18c0133"
+  end
+
   depends_on "go" => :build
 
   def install
