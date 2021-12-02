56a4a494432ed27eb3a572f045f2495c88cd7c47
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-02 09:43:14 +0800

    gtop: update 1.1.2 bottle.

--- Formula/gtop.rb
+++ Formula/gtop.rb
@@ -7,6 +7,12 @@ class Gtop < Formula
   sha256 "04fa0e7d72c07c863258d802d1fd1fbb421ea7ec9130d69dc079be8771c621fc"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gtop"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "8fd5696f898181c3244b80f10b0c32185d7ffaa9b4d02363d36bfb59ebbe2d72"
+  end
+
   depends_on "node"
 
   def install
