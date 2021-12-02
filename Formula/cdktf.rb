f26dbcd7151f849a6701b9535a9780dd60ee7193
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-03 01:31:01 +0800

    cdktf: update 0.8.0 bottle.

--- Formula/cdktf.rb
+++ Formula/cdktf.rb
@@ -7,6 +7,12 @@ class Cdktf < Formula
   sha256 "21943e63242e8e39a8159d76fe4f53eed3a441af20d299276c09a4b980809444"
   license "MPL-2.0"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cdktf"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "4156a61121b5ed95c648f3ca6d5d813b37be8ef47151702cf8dc9eec4d65d978"
+  end
+
   depends_on "node"
   depends_on "terraform"
 
