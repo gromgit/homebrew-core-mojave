b80f6806025e4511293cc60c6bd544090e6e0bc0
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-03 07:19:29 +0800

    qalculate-gtk: update 3.22.0 bottle.

--- Formula/qalculate-gtk.rb
+++ Formula/qalculate-gtk.rb
@@ -5,6 +5,12 @@ class QalculateGtk < Formula
   sha256 "ba6c0238b5f926ac94e234e15a2dfa84215938da5df6fea136db75c5db488556"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/qalculate-gtk"
+    rebuild 1
+    sha256 mojave: "e52b5271909e75061c67757fcaddf0d31a6b0fb0553408402dc81fcc510d4721"
+  end
+
   depends_on "intltool" => :build
   depends_on "pkg-config" => :build
   depends_on "adwaita-icon-theme"
