0e344cd07267d42043e6c978ae985845817351fd
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-02 23:43:51 +0800

    libqalculate: update 3.22.0 bottle.

--- Formula/libqalculate.rb
+++ Formula/libqalculate.rb
@@ -5,6 +5,12 @@ class Libqalculate < Formula
   sha256 "533ec2fc3550b44a562e4ff93f2bb21332c802c60d13ae1323bfa54ffe5d57f6"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libqalculate"
+    rebuild 1
+    sha256 mojave: "94a903d7406e61d12759ac11ebc00e603f6ad93d6dec572632732a7c834600c4"
+  end
+
   depends_on "intltool" => :build
   depends_on "pkg-config" => :build
   depends_on "gettext"
