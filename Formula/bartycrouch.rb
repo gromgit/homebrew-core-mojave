6f5b4ac08b31e10de863f79f9fb126cfb34572ce
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-07 07:01:30 +0800

    bartycrouch: update 4.0.2 bottle.

--- Formula/bartycrouch.rb
+++ Formula/bartycrouch.rb
@@ -2,16 +2,25 @@ class Bartycrouch < Formula
   desc "Incrementally update/translate your Strings files"
   homepage "https://github.com/Flinesoft/BartyCrouch"
   url "https://github.com/Flinesoft/BartyCrouch.git",
-      tag:      "4.8.0",
-      revision: "e2cd0f35fb13b596196091a9ae8de67857e3479a"
-  license "MIT"
-  head "https://github.com/Flinesoft/BartyCrouch.git", branch: "main"
+      tag:      "4.0.2",
+      revision: "7d4cfec9530c7364727a4461712b54909f8d4a90"
+  head "https://github.com/Flinesoft/BartyCrouch.git"
 
-  depends_on xcode: ["12.5", :build]
-  depends_on :macos
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bartycrouch"
+    sha256 cellar: :any_skip_relocation, mojave: "1133c4b25f7e0d730cdaf2470188b8d383db7c6cd4d82ce6d0d720f4b5acb2b9"
+  end
+
+  depends_on xcode: ["10.2", :build]
 
   def install
     system "make", "install", "prefix=#{prefix}"
+  end
+
+  def caveats
+    <<~EOS
+      IMPORTANT: This is the final version of #{name} that's compatible with Mojave.
+    EOS
   end
 
   test do
@@ -31,7 +40,7 @@ class Bartycrouch < Formula
     EOS
 
     system bin/"bartycrouch", "update"
-    assert_match '"oldKey" = "', File.read("en.lproj/Localizable.strings")
-    assert_match '"test" = "', File.read("en.lproj/Localizable.strings")
+    assert_match(/"oldKey" = "/, File.read("en.lproj/Localizable.strings"))
+    assert_match(/"test" = "/, File.read("en.lproj/Localizable.strings"))
   end
 end
