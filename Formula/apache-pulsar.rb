64d41a91aefad8dd390a9c3948413f8ebb163e7d
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-07 06:36:09 +0800

    apache-pulsar: update 2.9.0 bottle.

--- Formula/apache-pulsar.rb
+++ Formula/apache-pulsar.rb
@@ -1,17 +1,15 @@
 class ApachePulsar < Formula
   desc "Cloud-native distributed messaging and streaming platform"
   homepage "https://pulsar.apache.org/"
-  url "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=pulsar/pulsar-2.8.1/apache-pulsar-2.8.1-src.tar.gz"
-  mirror "https://archive.apache.org/dist/pulsar/pulsar-2.8.1/apache-pulsar-2.8.1-src.tar.gz"
-  sha256 "8e30d0414f840477cad8fc27a09904523f3ff039f7c8570feb6acca047661710"
+  url "https://www.apache.org/dyn/mirrors/mirrors.cgi?action=download&filename=pulsar/pulsar-2.9.0/apache-pulsar-2.9.0-src.tar.gz"
+  mirror "https://archive.apache.org/dist/pulsar/pulsar-2.9.0/apache-pulsar-2.9.0-src.tar.gz"
+  sha256 "f667d3d9e9f7a57a3912d14cd778f20e07a27f9320646bbcb705d8b8c94b18f7"
   license "Apache-2.0"
   head "https://github.com/apache/pulsar.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:      "830a4cd96a644df024e9127876c4a11a68af0ca655a20d3a15aaecc133bee023"
-    sha256 cellar: :any_skip_relocation, catalina:     "291d72373020ea60dbbc448d5595c1449cc0171dc3ad7af9ea84aa25614797aa"
-    sha256 cellar: :any_skip_relocation, mojave:       "903b07f8634e992bc7e2e05e8dfa40f8dcfe0a5121769a395092cd6250b37fe4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c57adf0e9275c1f60f00066253a961d166be2bd053f18e9f8fd236ccdbdb6dda"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/apache-pulsar"
+    sha256 cellar: :any_skip_relocation, mojave: "4fac85c9747c16c7ba54172dc05de3fadc2b436ce4b24b59216acc87c3767bdc"
   end
 
   depends_on "autoconf" => :build
