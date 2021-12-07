d211d4c97866cda7240d0a65e76f73520293b375
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-08 02:07:49 +0800

    gmsh: update 4.9.0 bottle.

--- Formula/gmsh.rb
+++ Formula/gmsh.rb
@@ -1,16 +1,19 @@
 class Gmsh < Formula
   desc "3D finite element grid generator with CAD engine"
   homepage "https://gmsh.info/"
-  url "https://gmsh.info/src/gmsh-4.8.4-source.tgz"
-  sha256 "760dbdc072eaa3c82d066c5ba3b06eacdd3304eb2a97373fe4ada9509f0b6ace"
+  url "https://gmsh.info/src/gmsh-4.9.0-source.tgz"
+  sha256 "b8ef133c9b66ffe12df1747e72d4acf19f1eb1e9cd95eb0f577cbc4081d9bea3"
   license "GPL-2.0-or-later"
-  revision 1
-  head "https://gitlab.onelab.info/gmsh/gmsh.git"
+  head "https://gitlab.onelab.info/gmsh/gmsh.git", branch: "master"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?gmsh[._-]v?(\d+(?:\.\d+)+)[._-]source\.t/i)
+  end
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gmsh"
-    rebuild 1
-    sha256 cellar: :any, mojave: "d204d8e92b3e9638a8ccb36c10d9ab569f229983421d188bcfca7c0a84ed8eb1"
+    sha256 cellar: :any, mojave: "b77375255f50b912747af4f23054e6ad0645be687e72dae25d217fb489236b47"
   end
 
   depends_on "cmake" => :build
