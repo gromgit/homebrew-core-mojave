d66f84d611d73e3c96a5725696c48c6c89975b70
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-07 15:07:35 +0800

    elixir: update 1.13.0 bottle.

--- Formula/elixir.rb
+++ Formula/elixir.rb
@@ -1,19 +1,14 @@
 class Elixir < Formula
   desc "Functional metaprogramming aware language built on Erlang VM"
   homepage "https://elixir-lang.org/"
-  url "https://github.com/elixir-lang/elixir/archive/v1.12.3.tar.gz"
-  sha256 "c5affa97defafa1fd89c81656464d61da8f76ccfec2ea80c8a528decd5cb04ad"
+  url "https://github.com/elixir-lang/elixir/archive/v1.13.0.tar.gz"
+  sha256 "0ed0fb89a9b6428cd1537b7f9aab1d6ea64e0c5972589eeb46dff6f0324468ae"
   license "Apache-2.0"
   head "https://github.com/elixir-lang/elixir.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6369dd61589032dfd414568d41f851d6c139047fca381f92d343ca9dc70a42d7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a853f176067849a6f5d5441a2ceda381d6b758065eddec2ba9f472a6cd6870e7"
-    sha256 cellar: :any_skip_relocation, monterey:       "bc6c364ecee76c602af7d3f8bf2e34a6fad4fbbd655b55c770bd95726cd7ffd1"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cefbc6925f57d73b244c45c6cfb4c527b83b9c3f24d43035275d8690b50b16dc"
-    sha256 cellar: :any_skip_relocation, catalina:       "54af07dcdf6f41708570cb45e0eb5ef7a1658d90028ba8453f3e8cc5e1df8fcc"
-    sha256 cellar: :any_skip_relocation, mojave:         "d60b62c1e2cb43410169e748dc603ac34473064ebce70dbf3a397b227a1dc2f9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b327cc676b4893bfc2e2070e6ba5739028887488e1747a3584243938a73ba7ae"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/elixir"
+    sha256 cellar: :any_skip_relocation, mojave: "c537a781f7558eae75a85e092cccfcdb426a100177fbd84f1672364528e815fb"
   end
 
   depends_on "erlang"
