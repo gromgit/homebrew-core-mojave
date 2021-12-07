b73c24da76d412f117a1b71eb505f9f5634d5847
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-08 07:46:59 +0800

    hcxtools: update 6.2.5 bottle.

--- Formula/hcxtools.rb
+++ Formula/hcxtools.rb
@@ -1,19 +1,14 @@
 class Hcxtools < Formula
   desc "Utils for conversion of cap/pcap/pcapng WiFi dump files"
   homepage "https://github.com/ZerBea/hcxtools"
-  url "https://github.com/ZerBea/hcxtools/archive/6.2.4.tar.gz"
-  sha256 "74299313dd15ed38f07b42201903ab85ebbc3ad220a01fff1bd5c967cfea817d"
+  url "https://github.com/ZerBea/hcxtools/archive/6.2.5.tar.gz"
+  sha256 "7ce5f8263cf9354f01008ba710c6e3cc2dcc861c550bd7a943ca33d29738bec3"
   license "MIT"
   head "https://github.com/ZerBea/hcxtools.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "b473d82b8435a07a793c0dab13138c3eb1cf03a7a5c4141fd580cc482582053c"
-    sha256 cellar: :any,                 arm64_big_sur:  "71ab36699020855a425e5dab7dd64e6ed2de1dbbbedc3a84ca48c99ec60ab29d"
-    sha256 cellar: :any,                 monterey:       "4f6f64cb36999b4eb9195dcaa0d285113f61687955e1ca9094e2aff5ce48d35b"
-    sha256 cellar: :any,                 big_sur:        "e823f093cc594fcd8cec3c9fe7c5743d37323bfefc1f0c423ae7cab401936830"
-    sha256 cellar: :any,                 catalina:       "97f4b6d9401be82a0d1c48ed80be2070147ff374be07a3123b763cc0f7210696"
-    sha256 cellar: :any,                 mojave:         "9a93ac0c1f2b3172d3eb2b5f822c9c043a1d269fa881f35172f195f10628ff60"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ad17f6c779701195a56f451f8e862f3b7b6e13739cc4de746b76fd3da4996456"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/hcxtools"
+    sha256 cellar: :any, mojave: "5bdbea41527cf9c2d183e50c0d1894cc926eaa140a9dfad5ea4dce02027298a3"
   end
 
   depends_on "pkg-config" => :build
