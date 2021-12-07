68522994194be0ea8c689704df07f7742d1b8357
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-07 19:28:08 +0800

    fnm: update 1.28.2 bottle.

--- Formula/fnm.rb
+++ Formula/fnm.rb
@@ -1,10 +1,10 @@
 class Fnm < Formula
   desc "Fast and simple Node.js version manager"
-  homepage "https://fnm.vercel.app"
-  url "https://github.com/Schniz/fnm/archive/v1.28.1.tar.gz"
-  sha256 "9c05bf2cf3f43baa197eb3df582efa2cde5eaf66e3b0446a78e0fe6e46bc23b5"
+  homepage "https://github.com/Schniz/fnm"
+  url "https://github.com/Schniz/fnm/archive/v1.28.2.tar.gz"
+  sha256 "02bec72f9978fc2fd7b2671b25cc2bb019a64c0f2d2fe7ab72ec0add91288599"
   license "GPL-3.0-only"
-  head "https://github.com/Schniz/fnm.git"
+  head "https://github.com/Schniz/fnm.git", branch: "master"
 
   livecheck do
     url :stable
@@ -13,8 +13,7 @@ class Fnm < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fnm"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "9c0358296506b28f2b164ac3f8c2d05c1197b37f92c01e678fe3a4bf9737064e"
+    sha256 cellar: :any_skip_relocation, mojave: "6e7a9ad47afe137da820cc5975a1a5f7877db4a2d903271401b9ab62b0f41daf"
   end
 
   depends_on "rust" => :build
@@ -24,13 +23,13 @@ class Fnm < Formula
   def install
     system "cargo", "install", *std_cargo_args
 
-    (bash_completion/"fnm").write Utils.safe_popen_read("#{bin}/fnm", "completions", "--shell=bash")
-    (fish_completion/"fnm.fish").write Utils.safe_popen_read("#{bin}/fnm", "completions", "--shell=fish")
-    (zsh_completion/"_fnm").write Utils.safe_popen_read("#{bin}/fnm", "completions", "--shell=zsh")
+    (bash_completion/"fnm").write Utils.safe_popen_read(bin/"fnm", "completions", "--shell=bash")
+    (fish_completion/"fnm.fish").write Utils.safe_popen_read(bin/"fnm", "completions", "--shell=fish")
+    (zsh_completion/"_fnm").write Utils.safe_popen_read(bin/"fnm", "completions", "--shell=zsh")
   end
 
   test do
-    system("#{bin}/fnm", "install", "12.0.0")
+    system bin/"fnm", "install", "12.0.0"
     assert_match "v12.0.0", shell_output("#{bin}/fnm exec --using=12.0.0 -- node --version")
   end
 end
