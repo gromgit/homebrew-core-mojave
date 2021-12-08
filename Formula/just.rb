504a52128f0c2acd4d6568fcb89b6716ac509c6d
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-08 13:09:42 +0800

    just: update 0.10.5 bottle.

--- Formula/just.rb
+++ Formula/just.rb
@@ -1,10 +1,10 @@
 class Just < Formula
   desc "Handy way to save and run project-specific commands"
   homepage "https://github.com/casey/just"
-  url "https://github.com/casey/just/archive/0.10.4.tar.gz"
-  sha256 "81d7e7d5496861a37f8668dbeb845381c41ea0ef5fa087c7b8bd41b70134218d"
+  url "https://github.com/casey/just/archive/0.10.5.tar.gz"
+  sha256 "02e452c0045879161ce820dc66e60d10b3e2cb9b8b152ff86cdd9221eb65d1b2"
   license "CC0-1.0"
-  head "https://github.com/casey/just.git"
+  head "https://github.com/casey/just.git", branch: "master"
 
   livecheck do
     url :stable
@@ -13,8 +13,7 @@ class Just < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/just"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "4272abc5c174e4c127030df72a73eccbff2258f541a8c35a8cd6cad1591b43fe"
+    sha256 cellar: :any_skip_relocation, mojave: "b5f196ee22ac477525cd76918d5ebaa65d4df542cb75fa3dee37b8e39f93ad12"
   end
 
   depends_on "rust" => :build
