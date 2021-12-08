e2d0ec88fd53882a6c24ee845139f068bbf33614
Adrian Ho <aho+vm-mojave@03s.net>
2021-12-09 02:12:32 +0800

    logswan: update 2.1.11 bottle.

--- Formula/logswan.rb
+++ Formula/logswan.rb
@@ -7,8 +7,8 @@ class Logswan < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/logswan"
-    rebuild 1
-    sha256 cellar: :any, mojave: "ac4ce3dd43e7e46af8d1fd9f2c1f353e79b0a6e3dc1d654445f5028a7c290311"
+    rebuild 2
+    sha256 cellar: :any, mojave: "60a3f8b90362887d7f811a6e86502046d42fb5d01a0da05c5247f5990a66df57"
   end
 
   depends_on "cmake" => :build
