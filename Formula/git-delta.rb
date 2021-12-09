dbf116a75678ebb2df637cbb263452a15c7655e0
Adrian Ho <the.gromgit@gmail.com>
2021-12-09 08:54:09 +0800

    git-delta: update 0.11.0 bottle.

--- Formula/git-delta.rb
+++ Formula/git-delta.rb
@@ -1,15 +1,14 @@
 class GitDelta < Formula
   desc "Syntax-highlighting pager for git and diff output"
   homepage "https://github.com/dandavison/delta"
-  url "https://github.com/dandavison/delta/archive/0.10.2.tar.gz"
-  sha256 "e0f71d72eca543478941401bd96fefc5fa3f70e7860a9f858f63bfecf8fd77a5"
+  url "https://github.com/dandavison/delta/archive/0.11.0.tar.gz"
+  sha256 "55e6aa28869247a7fca7aa9229ea2ebeae75e79becb22988e645301778f7388b"
   license "MIT"
   head "https://github.com/dandavison/delta.git"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-delta"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "598bf9425bd404b8c76a3b681898d5b6cd645734060609799d27b17339f2d916"
+    sha256 cellar: :any_skip_relocation, mojave: "d36715584cee5102a8965b5b41f4713a12d202d1a36b6d6c3b1afc5fa6953f4d"
   end
 
   depends_on "rust" => :build
