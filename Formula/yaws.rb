b893ade0f0a6b33dcaf1556cb8849f36a587d5a9
Adrian Ho <the.gromgit@gmail.com>
2021-12-11 13:17:30 +0800

    yaws: update 2.1.0 bottle.

--- Formula/yaws.rb
+++ Formula/yaws.rb
@@ -1,8 +1,8 @@
 class Yaws < Formula
   desc "Webserver for dynamic content (written in Erlang)"
   homepage "http://yaws.hyber.org"
-  url "https://github.com/erlyaws/yaws/archive/yaws-2.0.9.tar.gz"
-  sha256 "a2bbfe10c780ef2c3b238eaf76d902f4921c63b49d135bb9878b163ef1870a6d"
+  url "https://github.com/erlyaws/yaws/archive/yaws-2.1.0.tar.gz"
+  sha256 "84260bd95bff5fb4410df5db23b3b3e486476445d13a3c6b819fcbc31f66e0cd"
   license "BSD-3-Clause"
   head "https://github.com/erlyaws/yaws.git", branch: "master"
 
@@ -13,13 +13,8 @@ class Yaws < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "adb085c84a3b06e605da1dfac3864ccb5831327d3c5f39e7413588a32ab106f2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "55f35e91696552c9e0240835f8fc02c733b2bd657733c86123ee3d472ef5e9f6"
-    sha256 cellar: :any_skip_relocation, monterey:       "e9ad8bff29da32d63e13dfc6a97b0728f8ab2ef1e483d7ca8f21a7b682478730"
-    sha256 cellar: :any_skip_relocation, big_sur:        "79fe292028db08b81a2f66f80cbc2fd7c52e9801692c416ea275663c61dd4533"
-    sha256 cellar: :any_skip_relocation, catalina:       "99c7e7a4fb01e682a1f1cf513ac6b4202f9f030fea64836a0b71354802fde033"
-    sha256 cellar: :any_skip_relocation, mojave:         "f8b43c32a42426bc2e0b774e8abde8c7b32206ad19c230231ba22f32a1312eb5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "30dbc3c5951396f785b714bcf486d0247d399ecae66903a7e3a1872b3165d01f"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/yaws"
+    sha256 cellar: :any_skip_relocation, mojave: "cb6934eca123cf365345ef0992e991ca6ea06bd8f28ea279a7313fcf8135863a"
   end
 
   depends_on "autoconf" => :build
