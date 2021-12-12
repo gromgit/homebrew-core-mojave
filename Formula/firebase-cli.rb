01b25b27ba8c2c6ee5eb1329f6795c07e45b1275
Adrian Ho <the.gromgit@gmail.com>
2021-12-12 23:28:03 +0800

    firebase-cli: update 9.23.1 bottle.

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -3,15 +3,14 @@ require "language/node"
 class FirebaseCli < Formula
   desc "Firebase command-line tools"
   homepage "https://firebase.google.com/docs/cli/"
-  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-9.23.0.tgz"
-  sha256 "0486fcf8ab0797e3a18952315352ab760e03a15d4a980bf03c32ab74d2eb9862"
+  url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-9.23.1.tgz"
+  sha256 "ec0138165685b995a59dbf549c7f7f3b1cd86f14bd9f7ccc5eb6e17ee5c9a8ff"
   license "MIT"
   head "https://github.com/firebase/firebase-tools.git"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/firebase-cli"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "14811f1c8716352ecbbb96bdb44de7371bae8989a7abeaeee68594bde0cd6f23"
+    sha256 cellar: :any_skip_relocation, mojave: "79e3e6880063f6245c7722a0a6bd15855b209f2ddcbda7c4e3fce2b8d15931cb"
   end
 
   depends_on "node"
