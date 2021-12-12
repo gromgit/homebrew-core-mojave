86acab828a9516105587fe4bda7889b5d43cd714
Adrian Ho <the.gromgit@gmail.com>
2021-12-13 00:40:48 +0800

    gleam: update 0.18.0 bottle.

--- Formula/gleam.rb
+++ Formula/gleam.rb
@@ -1,18 +1,13 @@
 class Gleam < Formula
   desc "✨ A statically typed language for the Erlang VM"
   homepage "https://gleam.run"
-  url "https://github.com/gleam-lang/gleam/archive/v0.17.0.tar.gz"
-  sha256 "9530e616d42b4158ffcc5dd1befdc87ab2ea2af784fce6c57aec8c42a71a4ce2"
+  url "https://github.com/gleam-lang/gleam/archive/v0.18.0.tar.gz"
+  sha256 "9aead695a3baf2e62b462dbacb588be3f3ac778415afc57ab85087fc7955f0ac"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9bcd68363e64c7946796e349a9f3fc84d6746d0278850c826ac1c13e3a212d24"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5c3c84d492dbf86c9041469cabfb8c698660ded0a2da658678d9583ea21667b7"
-    sha256 cellar: :any_skip_relocation, monterey:       "4c5fcc18e6714172d522b76aa637f46797a809c6831eed67b9b9da25ee1c35fe"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1fbe03b66b76929c8cb919508f59e7fd9cfae2cd10ec177a775d60c8ef5f8b94"
-    sha256 cellar: :any_skip_relocation, catalina:       "c33692e53fbf7eca2cac472d3ca6e7a2555edbc14048a95e123160840853f314"
-    sha256 cellar: :any_skip_relocation, mojave:         "f1de3efb23c8eee281157668801ef6e1974d410a90c7708b16d3667d96f5632f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "005e249588fb532d3711b4a478f0c7ec176951ca143e1687cc8d0e465f294948"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gleam"
+    sha256 cellar: :any_skip_relocation, mojave: "4faa38400bfc49083a8b8f5a24bb3c15fc74028d85857ee2d0fbb65d14d0c921"
   end
 
   depends_on "rust" => :build
