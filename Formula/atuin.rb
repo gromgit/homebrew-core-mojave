c46ec01d49919e76734a26caf42c8d9ffb6ca4a9
Adrian Ho <the.gromgit@gmail.com>
2021-12-12 16:38:21 +0800

    atuin: update 0.7.2 bottle.

--- Formula/atuin.rb
+++ Formula/atuin.rb
@@ -1,18 +1,13 @@
 class Atuin < Formula
   desc "Improved shell history for zsh and bash"
   homepage "https://github.com/ellie/atuin"
-  url "https://github.com/ellie/atuin/archive/refs/tags/v0.7.1.tar.gz"
-  sha256 "0e1cd2104c6fa4cd2d6f30ae9771ed3f22f78a4e0e7b5243e42abb753b792c38"
+  url "https://github.com/ellie/atuin/archive/refs/tags/v0.7.2.tar.gz"
+  sha256 "e8618a56791a22f4ab93d61bd15e28a6983583769f97a3ee3dc1329729f2921f"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "74f8283b43292f8e887a65c593582ca19d6f218029ac70819a4931dbd02b0d0d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "73a3722439f584b2e0a636f5a87ff08a2a05f3f9d92ed9ec483b82af5069eecc"
-    sha256 cellar: :any_skip_relocation, monterey:       "70b718dab99844c63e72fd993c3f0b8ad321adc0a329b9c7d4ee5a1e0f1ca456"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b20e08fa6b7025c5a724db1f18a16acdd30950a46eec82f9f1a33852b71a27a9"
-    sha256 cellar: :any_skip_relocation, catalina:       "a8cf5ccab9ba3d5782a502e1e2e299e765b2b5e99a6f7adbe723648da80931ee"
-    sha256 cellar: :any_skip_relocation, mojave:         "3004a871e9de5e74d1cfb5f1e6aa84a36e5e4826f5fb7da0fe9e8c23fe7d08eb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e36422f51fd643bc0f256b5dcfba3b3a2785c58ae8f3c9628cd1a6bfa123adfb"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/atuin"
+    sha256 cellar: :any_skip_relocation, mojave: "4c414cb81c0d7bfab582d65e453de376b3639a9e60ec01b8cdbb53b3e0936348"
   end
 
   depends_on "rust" => :build
