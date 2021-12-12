f2d9ea7f611aa3c634130f65f824101c798fd4af
Adrian Ho <the.gromgit@gmail.com>
2021-12-12 18:07:04 +0800

    calicoctl: update 3.21.2 bottle.

--- Formula/calicoctl.rb
+++ Formula/calicoctl.rb
@@ -2,14 +2,19 @@ class Calicoctl < Formula
   desc "Calico CLI tool"
   homepage "https://www.projectcalico.org"
   url "https://github.com/projectcalico/calicoctl.git",
-      tag:      "v3.21.1",
-      revision: "7fc6f935fc27228593532c3a5ec61897c8051fff"
+      tag:      "v3.21.2",
+      revision: "17461419f0b20d784b2929b4aa700d4b14c2e10d"
   license "Apache-2.0"
   head "https://github.com/projectcalico/calicoctl.git", branch: "master"
 
   livecheck do
     url :stable
     regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/calicoctl"
+    sha256 cellar: :any_skip_relocation, mojave: "82f962df0bd322ea745ee8353ec52824395ee1ffc302f1c92a00750c1f97dd43"
   end
 
   depends_on "go" => :build
