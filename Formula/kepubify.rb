8827a6438692b4f336e7e108cb4c6c38d55294a5
Adrian Ho <the.gromgit@gmail.com>
2021-12-13 02:48:32 +0800

    kepubify: update 4.0.2 bottle.

--- Formula/kepubify.rb
+++ Formula/kepubify.rb
@@ -1,19 +1,14 @@
 class Kepubify < Formula
   desc "Convert ebooks from epub to kepub"
   homepage "https://pgaskin.net/kepubify/"
-  url "https://github.com/pgaskin/kepubify/archive/v4.0.1.tar.gz"
-  sha256 "30e497e3a8490bcf571d01a7bb6cc2410689fccd7f9a516c31a166460d901aa8"
+  url "https://github.com/pgaskin/kepubify/archive/v4.0.2.tar.gz"
+  sha256 "f6bf7065ec99e48766f60a126590e021f5bd4fac19754ecb2d90eaf106f4e39b"
   license "MIT"
   head "https://github.com/pgaskin/kepubify.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "48839929a4ccdd8c9064ba6ec3062b1b2ca3b14d02e96514850e06d3b7aa7de5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b6cd624dfcd004722ed211e192749443e2ca43117d499eb28929c420589978f5"
-    sha256 cellar: :any_skip_relocation, monterey:       "060e52b0317d377be3b83d5a180a0ad1ed1afc08a1a95d98d46f18acbba4497a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d7b68d8c1c34a5fe214b38c88eb5a18c11bff5698026e57d440f6f77c8f29297"
-    sha256 cellar: :any_skip_relocation, catalina:       "c8f88fe65e8d02bd957a70630d43fd4007b2c5e8f65c65789353de8b138c765f"
-    sha256 cellar: :any_skip_relocation, mojave:         "e2fdeb917185ea975bb51d87f5e50d214d4d4c8d961b16ab3634ba32dc713eb0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ad4d4a9dbc75d617f3e8b87f77251c1f82885477915e16c52cfa8f032b13b242"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kepubify"
+    sha256 cellar: :any_skip_relocation, mojave: "4a56eab3076231d28e70c600e7890d9b27633421011ee90b9046b576b131359c"
   end
 
   depends_on "go" => :build
@@ -28,7 +23,6 @@ class Kepubify < Formula
     ].each do |p|
       system "go", "build", "-o", bin/p,
                    "-ldflags", "-s -w -X main.version=#{version}",
-                   "-tags", "zip117",
                    "./cmd/#{p}"
     end
   end
