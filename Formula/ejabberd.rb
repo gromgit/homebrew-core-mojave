fad31a986e228b1497a0e1fec6f32245f9ea2a72
Adrian Ho <the.gromgit@gmail.com>
2021-12-12 22:52:05 +0800

    ejabberd: update 21.12 bottle.

--- Formula/ejabberd.rb
+++ Formula/ejabberd.rb
@@ -1,8 +1,8 @@
 class Ejabberd < Formula
   desc "XMPP application server"
   homepage "https://www.ejabberd.im"
-  url "https://static.process-one.net/ejabberd/downloads/21.07/ejabberd-21.07.tgz"
-  sha256 "0e90cfd6c03191ca8aef344b9d543a038e272770be14c2288d83cc4d34825868"
+  url "https://static.process-one.net/ejabberd/downloads/21.12/ejabberd-21.12.tgz"
+  sha256 "b6e6739947d3678525b14ee280cedb1a04280c83ea17a4741795aac99fbdad47"
   license "GPL-2.0-only"
 
   livecheck do
@@ -11,12 +11,8 @@ class Ejabberd < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "4b54e5246a1a00c93856d1786eb4067b3883f4293d3fb91a7345441fdc90ba76"
-    sha256 cellar: :any, arm64_big_sur:  "c22a2bd2a9dfcbbdc1e6df1ea63ae25924234f1ddd4cb5f4b016fc2013b97487"
-    sha256 cellar: :any, monterey:       "0e7930fc896f7e8136ed435743502e88820d1463a850537190b3d993aed6f202"
-    sha256 cellar: :any, big_sur:        "61ae3c224f34b18973bca8c311cdf990fe4e1c2be444d69ae5f9e2ecab6984b2"
-    sha256 cellar: :any, catalina:       "e864b717afec626c0a2b1e1038c1dcef082f6ef34d72085581f594d77b7a0ad1"
-    sha256 cellar: :any, mojave:         "8856dec9f546db393d17f66ec8427e0429650658e2622cad22f47347b563495b"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ejabberd"
+    sha256 cellar: :any, mojave: "c073d4fa187b6ed123e74d33abd92f18f1ed3de41011c399216c7abd06f6597c"
   end
 
   head do
