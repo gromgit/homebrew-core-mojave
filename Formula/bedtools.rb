e6e25bcd7a0af86724635324145f8b17af07ddcc
Adrian Ho <the.gromgit@gmail.com>
2021-12-12 17:44:09 +0800

    bedtools: update 2.30.0 bottle.

--- Formula/bedtools.rb
+++ Formula/bedtools.rb
@@ -6,16 +6,12 @@ class Bedtools < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "de37a80ddd3decf4a0a5ad0269de8a15a65dee4192b2e817a033010c0c5772fc"
-    sha256 cellar: :any,                 arm64_big_sur:  "d43458d5a8f50a7db0f7f728ed82c0a980d9202126624d594d17c99c2c9ea76d"
-    sha256 cellar: :any,                 monterey:       "19b9705e968d7003f9511258154ee94366a68dc4af9a7644b671d56c8595f913"
-    sha256 cellar: :any,                 big_sur:        "6554348743d3efba64e47294f8cb229229f168902234dd7e0ee0b4dfa85bb4d7"
-    sha256 cellar: :any,                 catalina:       "32d302a56df9044ce36d44db851318fc4fb45676086e48a6d913f2286ae3a756"
-    sha256 cellar: :any,                 mojave:         "69b80814d21b11edf9d45d67de8536d5db965dd6743e56079e1f3d6cf77bb6d2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b255c530815fd12473089bb12c3adfa1cb290dc40c802a8e4716b1ef434de4b5"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bedtools"
+    rebuild 1
+    sha256 cellar: :any, mojave: "ad9b4be16ff94be8374af22f6737e154410de64bfe4e931269a8c2956c984027"
   end
 
-  depends_on "python@3.9" => :build
+  depends_on "python@3.10" => :build
   depends_on "xz"
 
   uses_from_macos "bzip2"
