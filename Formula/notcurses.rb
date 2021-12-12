a4ef523556d440633e55a40f499fc74468921f7e
Adrian Ho <the.gromgit@gmail.com>
2021-12-13 05:07:25 +0800

    notcurses: update 3.0.0 bottle.

--- Formula/notcurses.rb
+++ Formula/notcurses.rb
@@ -1,14 +1,13 @@
 class Notcurses < Formula
   desc "Blingful character graphics/TUI library"
   homepage "https://nick-black.com/dankwiki/index.php/Notcurses"
-  url "https://github.com/dankamongmen/notcurses/archive/refs/tags/v2.4.9.tar.gz"
-  sha256 "a2771ad1633e0158f8273fa8b30b5bce0f12e1205e863045f4ae186b6b52f537"
+  url "https://github.com/dankamongmen/notcurses/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "b4839108ca8f9aef31d2f537485cbd878356bbcd4ad4f7a4dd19e72201d01cee"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/notcurses"
-    rebuild 1
-    sha256 mojave: "2a7234b9c2df256aa7d4cfbb66a014d494fd129a96cb73a6ae649593d7e12ecc"
+    sha256 mojave: "4a94aa67abbb6af851b5b410ca86c9c5aff083387602828544d5dde71b2c7212"
   end
 
   depends_on "cmake" => :build
@@ -16,11 +15,16 @@ class Notcurses < Formula
   depends_on "pandoc" => :build
   depends_on "pkg-config" => :build
   depends_on "ffmpeg"
+  depends_on "libdeflate"
   depends_on "libunistring"
   depends_on "ncurses"
-  uses_from_macos "zlib"
 
   fails_with gcc: "5"
+
+  patch do
+    url "https://nick-black.com/strndup-mingw-only.patch"
+    sha256 "8ea4a3be2181e1091e44868646830ea37f2efcfcde984a57e5d8dd48d6bb43e0"
+  end
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DCMAKE_INSTALL_RPATH=#{rpath}"
