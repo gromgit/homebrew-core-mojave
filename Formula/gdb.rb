41000e8ddf81c265da79594b634aaabf1d585095
Adrian Ho <the.gromgit@gmail.com>
2021-12-12 11:55:33 +0800

    gdb: update 11.1_1 bottle.

--- Formula/gdb.rb
+++ Formula/gdb.rb
@@ -5,18 +5,16 @@ class Gdb < Formula
   mirror "https://ftpmirror.gnu.org/gdb/gdb-11.1.tar.xz"
   sha256 "cccfcc407b20d343fb320d4a9a2110776dd3165118ffd41f4b1b162340333f94"
   license "GPL-3.0-or-later"
+  revision 1
   head "https://sourceware.org/git/binutils-gdb.git", branch: "master"
 
   bottle do
-    sha256 monterey:     "c4d3d881ae2ae938b37031f94c9b2780e53f78fa224fe64532b08656157f9afe"
-    sha256 big_sur:      "ce7aa7f3589b5833ff0ace1afb6e43ea01860666bf34ad7f941b9cab5c7ab5ab"
-    sha256 catalina:     "7543956f666aae922d34accd371fbc57d901adb386776b47e079163933e75755"
-    sha256 mojave:       "1d315c9b2213bfe09533c05beb2221a68ae816dc92c321d966c3fe1974e95913"
-    sha256 x86_64_linux: "e83bcc7a2b903fb7e76b2d4b2ae2935d96100385a5cbae4316641c90fc724685"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gdb"
+    sha256 mojave: "e7106a17083d37da960c7258d5b286b1c3a8e3eae1f32793178554fdc5ba629f"
   end
 
   depends_on "gmp"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
   depends_on "xz" # required for lzma support
 
   uses_from_macos "texinfo" => :build
@@ -46,7 +44,7 @@ class Gdb < Formula
       --disable-debug
       --disable-dependency-tracking
       --with-lzma
-      --with-python=#{Formula["python@3.9"].opt_bin}/python3
+      --with-python=#{Formula["python@3.10"].opt_bin}/python3
       --disable-binutils
     ]
 
