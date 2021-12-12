6dcc39502502abcc918bb41c8fce9cee17bd478b
Adrian Ho <the.gromgit@gmail.com>
2021-12-13 01:40:30 +0800

    halide: update 13.0.1_1 bottle.

--- Formula/halide.rb
+++ Formula/halide.rb
@@ -4,6 +4,7 @@ class Halide < Formula
   url "https://github.com/halide/Halide/archive/v13.0.1.tar.gz"
   sha256 "8d4b0ad622ef0ff9e28770cd950baf45a055d1229131be44f8e6333a548183f9"
   license "MIT"
+  revision 1
 
   livecheck do
     url :stable
@@ -12,15 +13,14 @@ class Halide < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/halide"
-    rebuild 2
-    sha256 cellar: :any, mojave: "b1da0d77745fab797b6f03afc68c9f9a47de0ecc1c70646b62542b1d16af51f7"
+    sha256 cellar: :any, mojave: "be44fb7552c93ccd236e233995d756e111e820855ce6b1539f76b170e64e0ca1"
   end
 
   depends_on "cmake" => :build
   depends_on "jpeg"
   depends_on "libpng"
   depends_on "llvm"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     mkdir "build" do
