5de060a229ba2b10a08ad996501a20fa9229dbe1
Adrian Ho <the.gromgit@gmail.com>
2021-12-12 19:29:47 +0800

    cobalt: update 0.17.5 bottle.

--- Formula/cobalt.rb
+++ Formula/cobalt.rb
@@ -1,18 +1,13 @@
 class Cobalt < Formula
   desc "Static site generator written in Rust"
   homepage "https://cobalt-org.github.io/"
-  url "https://github.com/cobalt-org/cobalt.rs/archive/v0.17.4.tar.gz"
-  sha256 "c50f06a6561f8e5ac5e5eb5c13cbd612ef810fc9e57d470d7793e69e243b4a52"
+  url "https://github.com/cobalt-org/cobalt.rs/archive/v0.17.5.tar.gz"
+  sha256 "17603490ba4817ec6ab3c486e39ac5863dd8625b58d8f8bfb534c83d7af334ef"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7a8bf7ad61aebf1fd9024b53436c113942bee88eb7db23bc57c0c7d968ff8baa"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "49eea0a06c946f0f3ef485cfcf79997c98acce4464a481184ec76978e3f117bb"
-    sha256 cellar: :any_skip_relocation, monterey:       "fca10f812d48c5b51ea5374642b46d3355ca47757fa7f50d6785eff179c9132e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0b508cc2831cc0c9b1f708342c16447641e4885c6477403e669dd547a355883a"
-    sha256 cellar: :any_skip_relocation, catalina:       "ee4cb9dca99bdeb3899d6b52eb09b573470df696e7ef16c5898998465827f9dc"
-    sha256 cellar: :any_skip_relocation, mojave:         "d73ffab8a234961c0028268d622bdac4512cff9f17f5f7defc2c831bc91af8da"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "95b58a7da1188741a7a462bdc33d1ad49b5172924873aed23c372537b26e48bc"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cobalt"
+    sha256 cellar: :any_skip_relocation, mojave: "c8162136a8f50cca4b6b72f04a12e929ad41b9a84dda562087682862db355b28"
   end
 
   depends_on "rust" => :build
