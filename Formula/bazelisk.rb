d5a130e1bc021739c0aae9787c13fe65c50a364b
Adrian Ho <the.gromgit@gmail.com>
2021-12-12 17:33:41 +0800

    bazelisk: update 1.11.0 bottle.

--- Formula/bazelisk.rb
+++ Formula/bazelisk.rb
@@ -2,19 +2,14 @@ class Bazelisk < Formula
   desc "User-friendly launcher for Bazel"
   homepage "https://github.com/bazelbuild/bazelisk/"
   url "https://github.com/bazelbuild/bazelisk.git",
-      tag:      "v1.10.1",
-      revision: "cf1205edacc5bc8a781786b36324922640ea6ac9"
+      tag:      "v1.11.0",
+      revision: "618382246fae452d0d92b6086b29903c3bc71361"
   license "Apache-2.0"
   head "https://github.com/bazelbuild/bazelisk.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bc592b8ca05a666976465d3620a7255ddf2cc44cbf5532a7f840477eb7402428"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3aa568fb42d31462693efea8051e7ef2489e1f1f22a07500cfd513b0c7f97cf0"
-    sha256 cellar: :any_skip_relocation, monterey:       "7dfdd39866cb60e9a47d4cbf32396b079fe6b07195017475ec832fdb88eb89df"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a0589844659d97147c7fab9b3a623192527cfbaed269bf4a502aee0dc2dcef18"
-    sha256 cellar: :any_skip_relocation, catalina:       "4b334992c7b2cd433074e100a3675bc07a7ab64de8fbb35beea250d229d12363"
-    sha256 cellar: :any_skip_relocation, mojave:         "f774b348b3e1403522e6a67126b9791f20245ea3002c23c4786094d82c9a9507"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "49d53dd4a0f10c28bc430501cf4e16fbb90d9ad316076e42c12533ea0daf2261"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bazelisk"
+    sha256 cellar: :any_skip_relocation, mojave: "591a973990800b19ede213abb9af84a278dd697c78362b15029f9b1edd19d1d0"
   end
 
   depends_on "go" => :build
