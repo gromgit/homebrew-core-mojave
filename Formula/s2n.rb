42e0592824c5e83817edf01a42cef6f156650c9a
Adrian Ho <the.gromgit@gmail.com>
2021-12-13 22:49:02 +0800

    s2n: update 1.3.1 bottle.

--- Formula/s2n.rb
+++ Formula/s2n.rb
@@ -1,8 +1,8 @@
 class S2n < Formula
   desc "Implementation of the TLS/SSL protocols"
   homepage "https://github.com/aws/s2n-tls"
-  url "https://github.com/aws/s2n-tls/archive/v1.1.2.tar.gz"
-  sha256 "91b5940ff345482cfd582a4c6d04950a9788f70ac3e383af41ac1eeee0441d1e"
+  url "https://github.com/aws/s2n-tls/archive/v1.3.1.tar.gz"
+  sha256 "5dbdafef10a868ccd7912856c88e7af2806870ea0ea2d0b99d7d2106c9ac9cdd"
   license "Apache-2.0"
   head "https://github.com/aws/s2n-tls.git", branch: "main"
 
@@ -13,8 +13,7 @@ class S2n < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/s2n"
-    rebuild 1
-    sha256 cellar: :any, mojave: "a34585ab6a0fcc254eb7190b16d31d759490de4cc477efa08a7ac464de6f6c2d"
+    sha256 cellar: :any, mojave: "7123db108407be87ccb34312d484074fc3694d681d4c5eb1ed30d49831329a97"
   end
 
   depends_on "cmake" => :build
