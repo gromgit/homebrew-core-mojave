c029996a1276676b2128ada4b465efa9c266b9dc
Adrian Ho <the.gromgit@gmail.com>
2021-12-14 03:02:04 +0800

    sngrep: update 1.4.10 bottle.

--- Formula/sngrep.rb
+++ Formula/sngrep.rb
@@ -1,18 +1,13 @@
 class Sngrep < Formula
   desc "Command-line tool for displaying SIP calls message flows"
   homepage "https://github.com/irontec/sngrep"
-  url "https://github.com/irontec/sngrep/archive/v1.4.9.tar.gz"
-  sha256 "3c6f28b5c795a5b1844a8997aa430aba72e083c8bd52939990900450c5f4c85a"
+  url "https://github.com/irontec/sngrep/archive/v1.4.10.tar.gz"
+  sha256 "cedbe521c9730deda004bff71e88c8c56ae66d3d147ddc6f5f965df2ca67a8df"
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "328666d36c468478d3c63ee9187b57dec4d8fd58a0554296c917f77062eb7a71"
-    sha256 cellar: :any,                 arm64_big_sur:  "449af17f3cb8673ec2beb158ba5a48bfc620739bac89bce15eeaea4297c65972"
-    sha256                               monterey:       "caf2d4342c7cb6bacdbe8e7fb2187f263db2454e626df689090db07fb44cbd73"
-    sha256                               big_sur:        "d226ad4dbc036097beeefdb5d181954c3ed8eaeef9d236189598783d03a6a4c3"
-    sha256                               catalina:       "95e8048031ea84674d2147c224aae73c14616164c74234f25d3001e15b779a35"
-    sha256                               mojave:         "20e51aa586d1a16ad0ed97aacc941649b8872a78e36f3ec34dbdb8ea2a674216"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0ce3e5f4cb6aded2538bef214778a5b4204b460421ec57f6314685e4a90085b9"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sngrep"
+    sha256 mojave: "06db54ca53afc39a5551e1d546d716d64c6ae5a5764b23ad5bf0e4d2a0d43f39"
   end
 
   depends_on "autoconf" => :build
