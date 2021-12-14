97cbedc9afef123ab9632ec4e8f1f0ca53e853c0
Adrian Ho <the.gromgit@gmail.com>
2021-12-14 09:44:25 +0800

    terrahub: update 0.5.6 bottle.

--- Formula/terrahub.rb
+++ Formula/terrahub.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Terrahub < Formula
   desc "Terraform automation and orchestration tool"
   homepage "https://docs.terrahub.io"
-  url "https://registry.npmjs.org/terrahub/-/terrahub-0.5.5.tgz"
-  sha256 "b24521b4f5542d50a900eb64e75d5d796d5cc2ccab76090af50cd4be15adfafc"
+  url "https://registry.npmjs.org/terrahub/-/terrahub-0.5.6.tgz"
+  sha256 "a9d6eda1ccc5acb317a62e97d40aac47ba00f8fcbcc9f40ff456d6350b86c463"
   license "MPL-2.0"
 
   livecheck do
@@ -13,13 +13,8 @@ class Terrahub < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "06c1650e2da7ea8fbde4037d522497d5f5a82e9565983610253723140e43df09"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1258000ba0fcea8aacae546c1623b98858cbcdcf3bef406a58271134bb4b3fe6"
-    sha256 cellar: :any_skip_relocation, monterey:       "795adda61afd8b014682543f6272e14bf269fa91890c33bd0b182f9bfcd5cb3d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c32fed79f29e9f9dfc5ca06ff95fee401fc12be011e424102bb7cdd1fadaeb92"
-    sha256 cellar: :any_skip_relocation, catalina:       "c32fed79f29e9f9dfc5ca06ff95fee401fc12be011e424102bb7cdd1fadaeb92"
-    sha256 cellar: :any_skip_relocation, mojave:         "c32fed79f29e9f9dfc5ca06ff95fee401fc12be011e424102bb7cdd1fadaeb92"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1258000ba0fcea8aacae546c1623b98858cbcdcf3bef406a58271134bb4b3fe6"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/terrahub"
+    sha256 cellar: :any_skip_relocation, mojave: "9f9ab0fcf2ab5702466a80617a2a94e5494d769c5f256f1c7aff5dfd3b619200"
   end
 
   depends_on "node"
