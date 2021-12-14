5a2362b53e536b97d04dce9db5a0ab779351048e
Adrian Ho <the.gromgit@gmail.com>
2021-12-14 14:53:56 +0800

    velero: update 1.7.1 bottle.

--- Formula/velero.rb
+++ Formula/velero.rb
@@ -1,18 +1,13 @@
 class Velero < Formula
   desc "Disaster recovery for Kubernetes resources and persistent volumes"
   homepage "https://github.com/vmware-tanzu/velero"
-  url "https://github.com/vmware-tanzu/velero/archive/v1.7.0.tar.gz"
-  sha256 "7e8a559911956ee25c3dbdf64fe7adc0ab783795f9a59c7b2eec8534f8e7f4cc"
+  url "https://github.com/vmware-tanzu/velero/archive/v1.7.1.tar.gz"
+  sha256 "f008ea2ed6b5f03419399289768db907ec993f4c6ef6947689737d67d4a895b8"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9417ea2c7ca850fceba82976b4448edff911a2988ef6ef61d8c26cf7030e5c2c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "951a590a2827c7147de721967365c64b0f3c41b3750f674af214ef2d17b1d117"
-    sha256 cellar: :any_skip_relocation, monterey:       "e451e937908b66ea9314b03b21cd75b7181e7d5220a4c3c073894a2413b28751"
-    sha256 cellar: :any_skip_relocation, big_sur:        "893c0ff0b5fe83162550f8c7daa45f852edd36ec42491e5db804d858bb60a94b"
-    sha256 cellar: :any_skip_relocation, catalina:       "d912d7ceaede1a0bf419915e83fb95afce89e23a2b2e308833a17a1909f33ad8"
-    sha256 cellar: :any_skip_relocation, mojave:         "cea2ae85961c4ab6e7601847cb1ac8afb441d72f2979a9953b68b98229198496"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7d4b11a0b8deb77c190b697ef714312d402ffaaf30909ca0abe943c917e68d13"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/velero"
+    sha256 cellar: :any_skip_relocation, mojave: "9d2b1ebe14c57f76e7f98c8b2d0b5b6f35d4d76f4f6d2661075e75088dfcc880"
   end
 
   depends_on "go" => :build
