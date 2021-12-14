b29eab66a38d9746c27e7ee6c336670d947d8043
Adrian Ho <the.gromgit@gmail.com>
2021-12-14 15:20:54 +0800

    viddy: update 0.3.4 bottle.

--- Formula/viddy.rb
+++ Formula/viddy.rb
@@ -1,15 +1,14 @@
 class Viddy < Formula
   desc "Modern watch command"
   homepage "https://github.com/sachaos/viddy"
-  url "https://github.com/sachaos/viddy/archive/refs/tags/v0.3.3.tar.gz"
-  sha256 "459d3ea43436b523e654f1f60356ef3150606ed17ca88895f54b7ee5999292a1"
+  url "https://github.com/sachaos/viddy/archive/refs/tags/v0.3.4.tar.gz"
+  sha256 "9a66db51e729713df102def0c2c02b786bb09c2b024204c515f9c0dd721382aa"
   license "MIT"
   head "https://github.com/sachaos/viddy.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/viddy"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "83806af54c2d18154bb44cf0d1f9e684fe05cf709ac40aeaefc9906447650849"
+    sha256 cellar: :any_skip_relocation, mojave: "85bdeff7c5d586ded33df2737681808f5bd182c6dc20aa426c96842e3440c1c4"
   end
 
   depends_on "go" => :build
