2443106e32ef3de4c6fb426c2fd6d3367a4a37d6
Adrian Ho <the.gromgit@gmail.com>
2021-12-16 17:16:01 +0800

    f3d: update 1.1.0_3 bottle.

--- Formula/f3d.rb
+++ Formula/f3d.rb
@@ -8,8 +8,8 @@ class F3d < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/f3d"
-    rebuild 1
-    sha256 cellar: :any, mojave: "3e667725c8971914aba14b3bcd8189be305adc038e8d0646d89a105c89956812"
+    rebuild 2
+    sha256 cellar: :any, mojave: "eb11f3ba6c58ae0c2c1f8961a694040a49a977850e2142cbfcd5064935d9cb90"
   end
 
   depends_on "cmake" => :build
