2aad9bd4f2500cf78d19d5064e15ea1a7e9e5214
Adrian Ho <the.gromgit@gmail.com>
2021-12-16 17:15:42 +0800

    clarinet: update 0.19.1 bottle.

--- Formula/clarinet.rb
+++ Formula/clarinet.rb
@@ -3,8 +3,8 @@ class Clarinet < Formula
   homepage "https://github.com/hirosystems/clarinet"
   # pull from git tag to get submodules
   url "https://github.com/hirosystems/clarinet.git",
-      tag:      "v0.18.0",
-      revision: "4e04586adaa38009000545420f411754219419a2"
+      tag:      "v0.19.1",
+      revision: "25e604ec2397667d171f5d2bde2cd16e9f9b7774"
   license "GPL-3.0-only"
   head "https://github.com/hirosystems/clarinet.git", branch: "main"
 
@@ -14,13 +14,8 @@ class Clarinet < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "cd61e2dd46ce40a1a3bbfcdb78a97ba3a318504f59fd25fefc35d5fdf6f0f9dd"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "236468814681e65cff4cd9e2cec11c4d6ddecc4cabcb650b58e75cfad2e2e53c"
-    sha256 cellar: :any_skip_relocation, monterey:       "d294c2faf38663de00e5fb1f507cc9c2274bbc8feef5a19be5b3184f1bb56cc4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ace0468127de4b80fb1bee09add0acd7cc0a99b4a2a7e3ead635a1e900ee58a6"
-    sha256 cellar: :any_skip_relocation, catalina:       "33311a84eca9be606e3b64f587eb91a0c19f12f895b2f0c7126da68f6cd5a569"
-    sha256 cellar: :any_skip_relocation, mojave:         "65f32d61dca55b4bfc209d868e744378d16d2396e90c37db168ab23c6c8abe02"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e1d441ee1e20513639a89fad636e0c96883d0447a7b360f7933db8d2b03c9717"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/clarinet"
+    sha256 cellar: :any_skip_relocation, mojave: "705ee31c8571d91801d17cfa4edad74b8bd3504fa97d090854906f706544cdbe"
   end
 
   depends_on "rust" => :build
