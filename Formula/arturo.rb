6750bd42f6883f1b22c152114defefe885f4db02
Adrian Ho <the.gromgit@gmail.com>
2021-12-19 11:00:54 +0800

    arturo: update 0.9.78 bottle.

--- Formula/arturo.rb
+++ Formula/arturo.rb
@@ -1,15 +1,13 @@
 class Arturo < Formula
   desc "Simple, modern and portable programming language for efficient scripting"
   homepage "https://github.com/arturo-lang/arturo"
-  url "https://github.com/arturo-lang/arturo/archive/v0.9.77.tar.gz"
-  sha256 "432239cadc4223b1bcdf79ae5fcf8d25deca442cd8865d7e4c16aa932ddee9f8"
+  url "https://github.com/arturo-lang/arturo/archive/v0.9.78.tar.gz"
+  sha256 "c3b9f06e5eadb35e4c1c4c82fed02dc278175b786318918ee80baf42b8100953"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "cb959d646145de02f2a9c75bf39a9c25c305c5215d35532515ecf29e8771c3de"
-    sha256 cellar: :any, big_sur:       "e577c8bdcc06741de97214f15541071cec57cfc5cecea62c3273b188978c3fcb"
-    sha256 cellar: :any, catalina:      "bdd63665656f3d66a972fcb7a0a9ff5e8fa5d45fdba8da51bff3ef150925b40c"
-    sha256 cellar: :any, mojave:        "9e8ba06684fc4f00daaeb6041d7fb955faabbe1b673f8fc2fd9b68733f49bc06"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/arturo"
+    sha256 cellar: :any, mojave: "681d9d4ffaee9de6bbb1d7ee68b82aa1b200be93dcdbb9d018ba0350fc663cac"
   end
 
   depends_on "nim" => :build
