2a38bee34e5374c6277141599d32f2cba2066799
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 04:46:54 +0800

    juju: update 2.9.22 bottle.

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -2,8 +2,8 @@ class Juju < Formula
   desc "DevOps management tool"
   homepage "https://juju.is/"
   url "https://github.com/juju/juju.git",
-      tag:      "juju-2.9.21",
-      revision: "8a154b7d629f6d9c0693aba7accf255789996c14"
+      tag:      "juju-2.9.22",
+      revision: "07164c4b2c6a9f5b576b1c151bd85dd3a699be16"
   license "AGPL-3.0-only"
   version_scheme 1
   head "https://github.com/juju/juju.git"
@@ -15,7 +15,7 @@ class Juju < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/juju"
-    sha256 cellar: :any_skip_relocation, mojave: "23f280fe386a05122854d4fbc41467a329855920e249159337862b3baa08177c"
+    sha256 cellar: :any_skip_relocation, mojave: "fafeec8c38c95a7e0323487bfe80fed304af0329322aa8126df0abcaba683e1e"
   end
 
   depends_on "go" => :build
@@ -26,13 +26,8 @@ class Juju < Formula
       -X version.GitCommit=#{Utils.git_head}
       -X version.GitTreeState=clean
     ]
-    system "go", "build", *std_go_args,
-                 "-ldflags", ld_flags.join(" "),
-                 "./cmd/juju"
-    system "go", "build", *std_go_args,
-                 "-ldflags", ld_flags.join(" "),
-                 "-o", bin/"juju-metadata",
-                 "./cmd/plugins/juju-metadata"
+    system "go", "build", *std_go_args(ldflags: ld_flags), "./cmd/juju"
+    system "go", "build", *std_go_args(output: bin/"juju-metadata", ldflags: ld_flags), "./cmd/plugins/juju-metadata"
     bash_completion.install "etc/bash_completion.d/juju"
   end
 
