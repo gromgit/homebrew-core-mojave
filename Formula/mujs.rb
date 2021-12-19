fe0a451c4b2ccfa54df50b63b3a7dda385e5b545
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 05:44:51 +0800

    mujs: update 1.2.0 bottle.

--- Formula/mujs.rb
+++ Formula/mujs.rb
@@ -3,19 +3,14 @@ class Mujs < Formula
   homepage "https://www.mujs.com/"
   # use tag not tarball so the version in the pkg-config file isn't blank
   url "https://github.com/ccxvii/mujs.git",
-      tag:      "1.1.3",
-      revision: "c3715ce3db4cc37ea46af8dbc891ecee1ca1b2ff"
+      tag:      "1.2.0",
+      revision: "dd0a0972b4428771e6a3887da2210c7c9dd40f9c"
   license "ISC"
   head "https://github.com/ccxvii/mujs.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "638720c7e6b645f0d3003f6113f4261a38eeb2cb0a72aca2d9be9a70700bc510"
-    sha256 cellar: :any,                 arm64_big_sur:  "78e372a317496d0c878f0b7fc8df1fbc2373e9b0ef5ae2dccf1939f4d7967fa3"
-    sha256 cellar: :any,                 monterey:       "d514ea3631d1cd61a40ca72b9e42e4c42e96466a6a8e61dfe9f7cb85f49707b8"
-    sha256 cellar: :any,                 big_sur:        "0a61a95aa8e9dc8b7fe527def9bf5fa0821177d1633af729eeb80eb1a97a73b1"
-    sha256 cellar: :any,                 catalina:       "2cd161c4cb14133645f6c2322cfd14366ccb270a3057b46441d6beae73105818"
-    sha256 cellar: :any,                 mojave:         "3918f9b4c64b7204de7b92df869dc945f11706735551750369343cb8bad16df5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d6d7d974cb43e22f2b81342824b0f487d1c3e08af0ebfd3bc37ba77aafb870b6"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mujs"
+    sha256 cellar: :any, mojave: "24526625ae6a7140567619902536df2a9d9e207dbf5484410abd549a0bdda754"
   end
 
   on_linux do
