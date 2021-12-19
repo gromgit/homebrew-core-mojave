1f538cf257d4b59b7da20ab617b4b161da061b57
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 06:43:30 +0800

    gst-plugins-bad: update 1.18.5 bottle.

--- Formula/gst-plugins-bad.rb
+++ Formula/gst-plugins-bad.rb
@@ -1,8 +1,8 @@
 class GstPluginsBad < Formula
   desc "GStreamer plugins less supported, not fully tested"
   homepage "https://gstreamer.freedesktop.org/"
-  url "https://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-1.18.4.tar.xz"
-  sha256 "74e806bc5595b18c70e9ca93571e27e79dfb808e5d2e7967afa952b52e99c85f"
+  url "https://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-1.18.5.tar.xz"
+  sha256 "a164923b94f0d08578a6fcaeaac6e0c05da788a46903a1086870e9ca45ad678e"
   license "LGPL-2.0-or-later"
   head "https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad.git"
 
@@ -12,12 +12,8 @@ class GstPluginsBad < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "ca8586eaf948a1b1d5c3e56fa258fd3098764f81d5bef1c622324aaedec53695"
-    sha256 arm64_big_sur:  "6c0ada7dac8defafc4e6d099870d22efd9cae93ef4c7afce32684e2d62dbb674"
-    sha256 monterey:       "c591057877a706e583a293a858553cf26ed3a8346dd37bfeaf2bab39b9d11daa"
-    sha256 big_sur:        "c4c7f8121511f8981dd7478d81ef8a773fab0c714b820ecbbfdfb56d71ccf737"
-    sha256 catalina:       "bb4a58be9dd95562605a8a7537eedcdb76ec4671eaebe8fdda4416b1a67b5621"
-    sha256 mojave:         "fa30f63fdccd9d97dd43612582b5fb32952f33ae1f7eac26b506fefe64a2f39b"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gst-plugins-bad"
+    sha256 mojave: "b8bb6d1831460f17659ee0db07ee99a8d96a8724d60f183f703a006eec730bf4"
   end
 
   depends_on "gobject-introspection" => :build
