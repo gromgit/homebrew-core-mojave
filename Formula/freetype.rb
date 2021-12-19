3735fe36f0f473d13d13914bd648c4d1ffd5b186
Adrian Ho <the.gromgit@gmail.com>
2021-12-19 09:12:58 +0800

    freetype: update 2.11.1 bottle.

--- Formula/freetype.rb
+++ Formula/freetype.rb
@@ -1,9 +1,9 @@
 class Freetype < Formula
   desc "Software library to render fonts"
   homepage "https://www.freetype.org/"
-  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.11.0/freetype-2.11.0.tar.xz"
-  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.11.0.tar.xz"
-  sha256 "8bee39bd3968c4804b70614a0a3ad597299ad0e824bc8aad5ce8aaf48067bde7"
+  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.11.1/freetype-2.11.1.tar.xz"
+  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.11.1.tar.xz"
+  sha256 "3333ae7cfda88429c97a7ae63b7d01ab398076c3b67182e960e5684050f2c5c8"
   license "FTL"
 
   livecheck do
@@ -12,13 +12,8 @@ class Freetype < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "dab11f55546d96775ca3b8fb9aa9225c1fb3a2baa6b6dc9c03fe41066b82c6dc"
-    sha256 cellar: :any,                 arm64_big_sur:  "e9bcfa10ec1ce289fd5fb38228918e83ec28c5aa8dd25d7b543a8db6c0da0e39"
-    sha256 cellar: :any,                 monterey:       "1293f4b7e27bdbd72bce46ab6e365c7ddf02ef84b333810b7ace18e6f087c2e9"
-    sha256 cellar: :any,                 big_sur:        "4e85cc5a025f1d8d9fedcb49775cfff4e1d2959bfda468445cecfc76c40edf17"
-    sha256 cellar: :any,                 catalina:       "619ef51d198b0c4451e55d02f750c0f8f41614dcf7e108904c57e9eecca2fff1"
-    sha256 cellar: :any,                 mojave:         "0ad6ce1ac305d4c8412f6c20ccdec2951fbf36eb2d971769d9a0910792001498"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "23cd32e04d63c7b1f2ac25da4e777d2813802c8cd9b527f50c595df722dd6d57"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/freetype"
+    sha256 cellar: :any, mojave: "b4c8a73873195a47b31d1bfd38662f6e1380ab8347f0c3c257ef7a15c0f8712c"
   end
 
   depends_on "libpng"
