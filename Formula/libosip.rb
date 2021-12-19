3adb93c70b18082f546c0a10734375332b9aca28
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 05:10:21 +0800

    libosip: update 5.3.0 bottle.

--- Formula/libosip.rb
+++ Formula/libosip.rb
@@ -1,9 +1,9 @@
 class Libosip < Formula
   desc "Implementation of the eXosip2 stack"
   homepage "https://www.gnu.org/software/osip/"
-  url "https://ftp.gnu.org/gnu/osip/libosip2-5.2.1.tar.gz"
-  mirror "https://ftpmirror.gnu.org/osip/libosip2-5.2.1.tar.gz"
-  sha256 "ee3784bc8e7774f56ecd0e2ca6e3e11d38b373435115baf1f1aa0ca0bfd02bf2"
+  url "https://ftp.gnu.org/gnu/osip/libosip2-5.3.0.tar.gz"
+  mirror "https://ftpmirror.gnu.org/osip/libosip2-5.3.0.tar.gz"
+  sha256 "f4725916c22cf514969efb15c3c207233d64739383f7d42956038b78f6cae8c8"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -12,13 +12,8 @@ class Libosip < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "d4906f9955e78e9d96dff148bfbd46bd39496dabd88b7502e436c479e2217b0b"
-    sha256 cellar: :any,                 arm64_big_sur:  "223cf167c2b2be056352ca25c6c78045f2f319fdee374310286e3e0383aad67e"
-    sha256 cellar: :any,                 monterey:       "a3ad0c1cd8614931026eea4d6ec674c216571c75baefb6f2fe74fe7dd84afd02"
-    sha256 cellar: :any,                 big_sur:        "c3855ed4bae9affb5007127469eda97fb91b395ca17f23639b4f3b08faed24cb"
-    sha256 cellar: :any,                 catalina:       "51a594ab9b9237fadbf236b6b91e7dff12b1d762a9084a9ceefa38e368236175"
-    sha256 cellar: :any,                 mojave:         "398471b49b724ac90cba0c87a44220adc0efbeb64b902429856531c263fa4404"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "56d76f7c03d4eedb0bb87a94fb46380f3c8dc2213863c66ea55ae1d67b9c9464"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libosip"
+    sha256 cellar: :any, mojave: "b86ee45cd719d8131cafb5513db0d53503ee6b9ed4d3e99a1c804144b69a9f47"
   end
 
   def install
