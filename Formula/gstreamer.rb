5eb2eb9db8b299c7c0324367ef3ae8abcc5416d7
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 03:26:41 +0800

    gstreamer: update 1.18.5 bottle.

--- Formula/gstreamer.rb
+++ Formula/gstreamer.rb
@@ -1,8 +1,8 @@
 class Gstreamer < Formula
   desc "Development framework for multimedia applications"
   homepage "https://gstreamer.freedesktop.org/"
-  url "https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-1.18.4.tar.xz"
-  sha256 "9aeec99b38e310817012aa2d1d76573b787af47f8a725a65b833880a094dfbc5"
+  url "https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-1.18.5.tar.xz"
+  sha256 "55862232a63459bbf56abebde3085ca9aec211b478e891dacea4d6df8cafe80a"
   license "LGPL-2.0-or-later"
   head "https://gitlab.freedesktop.org/gstreamer/gstreamer.git"
 
@@ -12,13 +12,8 @@ class Gstreamer < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "11505edb762ff75626deed74777d893b939929a7a4bbe152d4c1623a0e7cc38a"
-    sha256 arm64_big_sur:  "d62ea2ea98bd22267eabf676f8ec577df5eac27e7656e56545767b909d2224d5"
-    sha256 monterey:       "a3adca24f420ef935184de4acf71b6a24dde2b7dcce718e419c60b54c24b8f9c"
-    sha256 big_sur:        "a2d62c11bf92e6d4f2d02d1dff4558e945f6dc10b27f435c14824de9d2fdba48"
-    sha256 catalina:       "9fcc5eb54cebede4c30ed3bacbf82fb8272471bd0a80cbc0a63b503a79994371"
-    sha256 mojave:         "259ae892b842f10c590240abfc97e2af6d6c93f67d1362da1d858e913d3ddbd1"
-    sha256 x86_64_linux:   "ed6878d62c1ae4e7f007ddad5d307b84bef65a8ce7637aa65dcf9e2b6aebaaf9"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gstreamer"
+    sha256 mojave: "2ad357f80655bcb5b5028c675acd1c7b2f2acad70c42a7f76371639901d7c637"
   end
 
   depends_on "bison" => :build
