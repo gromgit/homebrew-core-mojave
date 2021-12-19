604fbaf0e3ab670864ae80a9bb6e48f610fe7140
Adrian Ho <the.gromgit@gmail.com>
2021-12-19 11:04:21 +0800

    asciidoc: update 10.1.0 bottle.

--- Formula/asciidoc.rb
+++ Formula/asciidoc.rb
@@ -3,8 +3,8 @@ class Asciidoc < Formula
 
   desc "Formatter/translator for text files to numerous formats"
   homepage "https://asciidoc-py.github.io/"
-  url "https://files.pythonhosted.org/packages/c2/d0/5334f7d8205aa11f2e4751f4137466c8d8a36b148dcf3874db87b40ce72e/asciidoc-10.0.2.tar.gz"
-  sha256 "1800699c579038bcf68e760e9358304b69a19ef04c9bf0b4faa76b729dcf7dbf"
+  url "https://files.pythonhosted.org/packages/c3/b6/f874a6e5ecfb7192a0199dd7652a7a8401a52090e3f6bf4a07b7b76a92ae/asciidoc-10.1.0.tar.gz"
+  sha256 "276e289ff780897b3e87f4e6420c8370d5712c442ce68eebad319bf41f2c839c"
   license "GPL-2.0-only"
   head "https://github.com/asciidoc-py/asciidoc-py.git", branch: "main"
 
@@ -15,8 +15,7 @@ class Asciidoc < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/asciidoc"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "5f1e6b70db1201226015f559dbdd3691ed894f1ab3af9f6f9325b0bcd3ef4f3f"
+    sha256 cellar: :any_skip_relocation, mojave: "95abc20e9ccbedb62af61e5f90002ce7335f48e3b7db5ad69a8fc4e6d8aef209"
   end
 
   depends_on "docbook"
