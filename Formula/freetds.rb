dfaada801aa41d17a0307c84689038bc50d0f677
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 03:05:22 +0800

    freetds: update 1.3.4 bottle.

--- Formula/freetds.rb
+++ Formula/freetds.rb
@@ -4,8 +4,8 @@ class Freetds < Formula
   license "LGPL-2.0-or-later"
 
   stable do
-    url "https://www.freetds.org/files/stable/freetds-1.3.3.tar.gz"
-    sha256 "a21a0aa351185049e11e237a795a61e3ca68d2e798259b3b5ea4a9797d5a5535"
+    url "https://www.freetds.org/files/stable/freetds-1.3.4.tar.gz", using: :homebrew_curl
+    sha256 "4c9d586265b67eecfc4c0d19858348e14375640598abe56d4bb5032f9f060949"
 
     # Fix -flat_namespace being used on Big Sur and later.
     patch do
@@ -20,13 +20,8 @@ class Freetds < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "40bff393d14033b24d4446decc292e487be71aaefffbb12bde74aa957c79c348"
-    sha256 arm64_big_sur:  "95f5ec9318ef76fc7fd19e1e4bfbb651ce0f70c0a445734ea2eb52c9431b5300"
-    sha256 monterey:       "0053a1d5b03bc3be37bdb383343cec5d6b1f209035a193e01dda2b4797987f63"
-    sha256 big_sur:        "5efb35a0a91a02ea73b82fd739f97f1db31174568e43e7021bca6a0326395dce"
-    sha256 catalina:       "24d00d8abb0e2022255cc8c57157bd348b456a2b23aa6e23698cda2707499248"
-    sha256 mojave:         "c3a7e0cb9eb4e18b6ab2fdc9b96ecbf79200897d30766c47e74bf3e676aea8e0"
-    sha256 x86_64_linux:   "ffb46b5fb70a425148a6747e284d9927f5a4e966d30fca5b4bd9dbcf0b71929e"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/freetds"
+    sha256 mojave: "c3986e700eca3934397ee83929148cd5bb6d8668cb2180cb8c45d2e5a29704b3"
   end
 
   head do
