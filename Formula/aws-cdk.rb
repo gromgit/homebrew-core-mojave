3f34584c607ef42eae20f7529a03dbd227f4047c
Adrian Ho <the.gromgit@gmail.com>
2021-12-19 11:27:21 +0800

    aws-cdk: update 2.2.0 bottle.

--- Formula/aws-cdk.rb
+++ Formula/aws-cdk.rb
@@ -3,13 +3,13 @@ require "language/node"
 class AwsCdk < Formula
   desc "AWS Cloud Development Kit - framework for defining AWS infra as code"
   homepage "https://github.com/aws/aws-cdk"
-  url "https://registry.npmjs.org/aws-cdk/-/aws-cdk-2.1.0.tgz"
-  sha256 "a869a6ffcb8c715053d22c809fd444e028d9222779d87e2771e105af7100e80f"
+  url "https://registry.npmjs.org/aws-cdk/-/aws-cdk-2.2.0.tgz"
+  sha256 "ff64798e932afc613a55563be788a57eca7fb6749bfa738b706c6ae2cd7d675a"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/aws-cdk"
-    sha256 cellar: :any_skip_relocation, mojave: "08661aace05f951105644620106de10a06a8af7097ae79535acb3dfa2afefba8"
+    sha256 cellar: :any_skip_relocation, mojave: "e32c56d67b47d0ceaf54b0ae3ca9c0c4ae8eeb2ffd2194d1c35f65ccd7451ef6"
   end
 
   depends_on "node"
