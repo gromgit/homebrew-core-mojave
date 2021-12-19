c639d7ca51c5ba8181983ee5b90bd2c15e087f8c
Adrian Ho <the.gromgit@gmail.com>
2021-12-19 17:40:48 +0800

    bosh-cli: update 6.4.9 bottle.

--- Formula/bosh-cli.rb
+++ Formula/bosh-cli.rb
@@ -1,15 +1,14 @@
 class BoshCli < Formula
   desc "Cloud Foundry BOSH CLI v2"
   homepage "https://bosh.io/docs/cli-v2/"
-  url "https://github.com/cloudfoundry/bosh-cli/archive/v6.4.8.tar.gz"
-  sha256 "0525115cdb07915321f478ff61ec6ba28971b0fbac2e8ffb0b6a5fcea12ea322"
+  url "https://github.com/cloudfoundry/bosh-cli/archive/v6.4.9.tar.gz"
+  sha256 "eb7a82f390d1a5238e0ea9d8a2d545ffc323ca91a23481185879963cc40be2bc"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/cloudfoundry/bosh-cli.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bosh-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "1c0d61a794d396280ef9b78d48f4cf6bfe3df4b9862776eaced9966e420d4855"
+    sha256 cellar: :any_skip_relocation, mojave: "8f2d276235c841a1c97f6f58796a19712bd80468a9d912bde795df0db77d32ce"
   end
 
   depends_on "go" => :build
