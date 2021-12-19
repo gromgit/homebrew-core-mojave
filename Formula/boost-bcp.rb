b949e031b3d38ba2e7c186d9eb43c94b0e5f94fb
Adrian Ho <the.gromgit@gmail.com>
2021-12-19 13:49:10 +0800

    boost-bcp: update 1.78.0 bottle.

--- Formula/boost-bcp.rb
+++ Formula/boost-bcp.rb
@@ -2,8 +2,8 @@ class BoostBcp < Formula
   desc "Utility for extracting subsets of the Boost library"
   homepage "https://www.boost.org/doc/tools/bcp/"
   # Please add to synced_versions_formulae.json once version synced with boost
-  url "https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.bz2"
-  sha256 "fc9f85fc030e233142908241af7a846e60630aa7388de9a5fafb1f3a26840854"
+  url "https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.bz2"
+  sha256 "8681f175d4bdb26c52222665793eef08490d7758529330f98d3b29dd0735bccc"
   license "BSL-1.0"
   head "https://github.com/boostorg/boost.git", branch: "master"
 
@@ -12,13 +12,8 @@ class BoostBcp < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "58fa7c3d18b6c5ac8a56475bbb315430e6f4581b39f3c6506879f4adfd8068e0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "416173b54edea43357edb6fdd76cf2c89060015b889a9e1843e33e796b6a97c4"
-    sha256 cellar: :any_skip_relocation, monterey:       "02213b90b218fb2f4d38940a59c3ae82e612405adc7e5cc79c296ea6521273a9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cc527cdb08563bbe1a03fd9d59085d3737ab197e753085e5b3d7c11704d7ca03"
-    sha256 cellar: :any_skip_relocation, catalina:       "5fbf373c4fb07fead636165d863be6ba56f52c929926e6e301796a1f00fca3af"
-    sha256 cellar: :any_skip_relocation, mojave:         "e483b259f0c9b6fb2fb1ff1256c3b3421d5f5c09a807819076200e114f40a0ac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dfa4af82624b089572d76ce463f5fc7c551bd53eeca67d4077e64bc104d28247"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/boost-bcp"
+    sha256 cellar: :any_skip_relocation, mojave: "fff9a56c2fc29b5f54edf3b789e53dcad6a9b8ee4961773d9e8b3f4b6ab3c6a0"
   end
 
   depends_on "boost-build" => :build
