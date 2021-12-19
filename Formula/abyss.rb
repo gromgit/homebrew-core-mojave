28ee5f523f0b8a5a26695224b3b1443c49b1161a
Adrian Ho <the.gromgit@gmail.com>
2021-12-19 09:30:33 +0800

    abyss: update 2.3.3 bottle.

--- Formula/abyss.rb
+++ Formula/abyss.rb
@@ -1,10 +1,9 @@
 class Abyss < Formula
   desc "Genome sequence assembler for short reads"
   homepage "https://www.bcgsc.ca/resources/software/abyss"
-  url "https://github.com/bcgsc/abyss/releases/download/2.3.1/abyss-2.3.1.tar.gz"
-  sha256 "664045e7903e9732411effc38edb9ebb1a0c1b7636c64b3a14a681f465f43677"
+  url "https://github.com/bcgsc/abyss/releases/download/2.3.3/abyss-2.3.3.tar.gz"
+  sha256 "4b14642989e84f331d8d1a854cca67bdede95e90bd6813836c173a951d71405c"
   license all_of: ["GPL-3.0-only", "LGPL-2.1-or-later", "MIT", "BSD-3-Clause"]
-  revision 1
 
   livecheck do
     url :stable
@@ -12,13 +11,8 @@ class Abyss < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "713c194554144c1d57a495ad8cfca339e3dcb144d247b03bf2d10410a1f57b89"
-    sha256                               arm64_big_sur:  "21d1cb2d31fa162cefea32db0630fc6011f8d3d0bb7ababa31f8e772820d1c7f"
-    sha256 cellar: :any,                 monterey:       "817369d04fe421b42fc2ecb8aa631a93cb2f40fc49e24d8e377356801bdc7755"
-    sha256 cellar: :any,                 big_sur:        "e56ce6b9bf533fb34bd2f87e87fb1019e3cccdcdc94d0e665e144121c95ddcad"
-    sha256 cellar: :any,                 catalina:       "5a95037e8675013c34f4119d5a2914706ec3b99f522426878a19f27031ad4d79"
-    sha256 cellar: :any,                 mojave:         "fc1208b6ff7dbcdc23d621076a271a40f1090cab41690ce35433c81425984494"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "36113b6fa458aaf594892cf86bbce9e0c6098285d42a3477dd9ffd85ecf06982"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/abyss"
+    sha256 cellar: :any, mojave: "9d9bcac51d265bd7073626a966c958232f831983501ea95b679bc4172e8bc68f"
   end
 
   head do
@@ -60,10 +54,10 @@ class Abyss < Formula
   test do
     testpath.install resource("testdata")
     if which("column")
-      system "#{bin}/abyss-pe", "k=25", "name=ts", "in=reads1.fastq reads2.fastq"
+      system "#{bin}/abyss-pe", "B=2G", "k=25", "name=ts", "in=reads1.fastq reads2.fastq"
     else
       # Fix error: abyss-tabtomd: column: not found
-      system "#{bin}/abyss-pe", "unitigs", "scaffolds", "k=25", "name=ts", "in=reads1.fastq reads2.fastq"
+      system "#{bin}/abyss-pe", "B=2G", "unitigs", "scaffolds", "k=25", "name=ts", "in=reads1.fastq reads2.fastq"
     end
     system "#{bin}/abyss-fac", "ts-unitigs.fa"
   end
