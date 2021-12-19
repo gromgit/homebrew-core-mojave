8c291b6d631ecee7a360dd771b94cb8ea8fe4606
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 06:51:21 +0800

    gst-plugins-ugly: update 1.18.5 bottle.

--- Formula/gst-plugins-ugly.rb
+++ Formula/gst-plugins-ugly.rb
@@ -1,10 +1,9 @@
 class GstPluginsUgly < Formula
   desc "Library for constructing graphs of media-handling components"
   homepage "https://gstreamer.freedesktop.org/"
-  url "https://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-1.18.4.tar.xz"
-  sha256 "218df0ce0d31e8ca9cdeb01a3b0c573172cc9c21bb3d41811c7820145623d13c"
+  url "https://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-1.18.5.tar.xz"
+  sha256 "df32803e98f8a9979373fa2ca7e05e62f977b1097576d3a80619d9f5c69f66d9"
   license "LGPL-2.0-or-later"
-  revision 1
   head "https://gitlab.freedesktop.org/gstreamer/gst-plugins-ugly.git"
 
   livecheck do
@@ -13,13 +12,8 @@ class GstPluginsUgly < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "74c50e6cf754608da7ae14b0201b99a08a1e5b34ddbc32afa854af463ba21bc7"
-    sha256 arm64_big_sur:  "93577cdbd18ee2cbdf026104ad07e9c9ae510c28a487a5fe4fbb18743ff596e8"
-    sha256 monterey:       "838ab9145393003b51a80892a3c91083d2f15a5eaef93f58bd8a7599d74dfbef"
-    sha256 big_sur:        "0a6d18fa102bcea7e8e096a8b114fa415a32c2c6cd477fdaa68255ffb119ac28"
-    sha256 catalina:       "3bf8b148e0901057104047b7392c501986096858e287cc4f11299933f1708931"
-    sha256 mojave:         "beb6b703ebad1a546c0ca4a9e9253e20bdb78c3aeba4231fdd1a645ab8fd11dc"
-    sha256 x86_64_linux:   "4793a5155acb669f37d6e29f4fc48470b9f7ac8bb9332f9cfbba6a53e372f617"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gst-plugins-ugly"
+    sha256 mojave: "88f76fa22262871fc06302bfb4e5f70e85d73446f5f0707137e0a34b592c1b85"
   end
 
   depends_on "meson" => :build
