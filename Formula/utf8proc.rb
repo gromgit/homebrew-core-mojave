7b48717ca7d6f8fa0924d80d89406084e56b80e5
Adrian Ho <the.gromgit@gmail.com>
2021-12-19 09:21:53 +0800

    utf8proc: update 2.7.0 bottle.

--- Formula/utf8proc.rb
+++ Formula/utf8proc.rb
@@ -1,18 +1,13 @@
 class Utf8proc < Formula
   desc "Clean C library for processing UTF-8 Unicode data"
   homepage "https://juliastrings.github.io/utf8proc/"
-  url "https://github.com/JuliaStrings/utf8proc/archive/v2.6.1.tar.gz"
-  sha256 "4c06a9dc4017e8a2438ef80ee371d45868bda2237a98b26554de7a95406b283b"
+  url "https://github.com/JuliaStrings/utf8proc/archive/v2.7.0.tar.gz"
+  sha256 "4bb121e297293c0fd55f08f83afab6d35d48f0af4ecc07523ad8ec99aa2b12a1"
   license all_of: ["MIT", "Unicode-DFS-2015"]
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "26bdf00e51114efbbdde4850efee15f5049ed46ad747dd546cd5dc80d354e13c"
-    sha256 cellar: :any,                 arm64_big_sur:  "e3845f14873623bba6259adc3f2b129d7a2ca41683764de6335eda07a30c3de7"
-    sha256 cellar: :any,                 monterey:       "94bcc57ea1bb7150ea7d6d3f3ffe02f9a61aa427f62a5f6bc5212180856d1f2f"
-    sha256 cellar: :any,                 big_sur:        "7d57bb4f93fdfd377fda65e74b9bf1cb7b2b4875a4481682a4b2a39cfe65d3e7"
-    sha256 cellar: :any,                 catalina:       "cf77fdee400e5692c3b78f0fc599e575b071b11a70ff1f3443e22c8dd14bd0fe"
-    sha256 cellar: :any,                 mojave:         "1bee383abb84a7f9921e1c6b3aa26de0c8c79295c4ae184959521bf537ba4552"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e574cf4da70696401aa9ebad3062929851ce5ee722216d67b454447a276e6a08"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/utf8proc"
+    sha256 cellar: :any, mojave: "826d2381f65371b25ca81c764c3a96ed38b04ef593a71b812e45c3435e244647"
   end
 
   def install
