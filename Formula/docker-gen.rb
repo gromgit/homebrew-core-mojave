8c001ebdf8e2b5cae76da8e618cc5076cf2e70fc
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 13:35:38 +0800

    docker-gen: update 0.8.1 bottle.

--- Formula/docker-gen.rb
+++ Formula/docker-gen.rb
@@ -1,14 +1,13 @@
 class DockerGen < Formula
   desc "Generate files from docker container metadata"
   homepage "https://github.com/jwilder/docker-gen"
-  url "https://github.com/jwilder/docker-gen/archive/0.8.0.tar.gz"
-  sha256 "3d969c32d1a612d44c8193422649b375ea9cea8d78a044be463d58fe128c525a"
+  url "https://github.com/jwilder/docker-gen/archive/0.8.1.tar.gz"
+  sha256 "c326e4af103ed2bf0da518f0d14f7207cfc73761955a3a6e121dd7e540ccd4d9"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/docker-gen"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "2189fef04434342a9bd1253e6535baaf394842f1a15593d4d60c051980d0a27b"
+    sha256 cellar: :any_skip_relocation, mojave: "32163225064dfd4b7803ab823be2971097c6548103caca5cf4ec6a5321488b61"
   end
 
   depends_on "go" => :build
