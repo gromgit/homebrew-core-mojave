cae723f24cceefbab08b3d026b1abb2580bb602e
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 01:41:40 +0800

    inxi: update 3.3.10-1 bottle.

--- Formula/inxi.rb
+++ Formula/inxi.rb
@@ -1,15 +1,14 @@
 class Inxi < Formula
   desc "Full featured CLI system information tool"
   homepage "https://smxi.org/docs/inxi.htm"
-  url "https://github.com/smxi/inxi/archive/3.3.08-1.tar.gz"
-  sha256 "44008d9e77dc82855fd91d634f5f817813eb4653e4df7106e56a1c9986ab8abd"
+  url "https://github.com/smxi/inxi/archive/3.3.10-1.tar.gz"
+  sha256 "0d4faa8a1b3f048d6f4ce49c64eb0e4628c63d16ffafad124cbebaca6922a302"
   license "GPL-3.0-or-later"
   head "https://github.com/smxi/inxi.git"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/inxi"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "92651fc01715c0e10b00e2c29f4218aa8adba003321f84d33cf3f4411fa88564"
+    sha256 cellar: :any_skip_relocation, mojave: "b81284918e2e159b895e0111e6f0593e61f7e4bbf2ed51fc37445227e6c5bbd8"
   end
 
   def install
