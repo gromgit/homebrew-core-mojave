547d06790052a8ac23b377ff8a5cb4c41e044ca9
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 00:20:54 +0800

    haproxy: update 2.5.0_1 bottle.

--- Formula/haproxy.rb
+++ Formula/haproxy.rb
@@ -1,9 +1,10 @@
 class Haproxy < Formula
   desc "Reliable, high performance TCP/HTTP load balancer"
   homepage "https://www.haproxy.org/"
-  url "https://www.haproxy.org/download/2.4/src/haproxy-2.4.8.tar.gz"
-  sha256 "e3e4c1ad293bc25e8d8790cc5e45133213dda008bfd0228bf3077259b32ebaa5"
+  url "https://www.haproxy.org/download/2.5/src/haproxy-2.5.0.tar.gz"
+  sha256 "16a5ed6256ca3670e41b76366a892b08485643204a3ce72b6e7a2d9a313aa225"
   license "GPL-2.0-or-later" => { with: "openvpn-openssl-exception" }
+  revision 1
 
   livecheck do
     url :homepage
@@ -12,31 +13,28 @@ class Haproxy < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/haproxy"
-    rebuild 2
-    sha256 cellar: :any, mojave: "f78595bd92679e8055fa2b45117722c97dfd04af54a92d73a2562b074fc7e543"
+    sha256 cellar: :any, mojave: "021549bd6fd313451121dcf9ac733e5ff5621b507cb49ee63a62c568e73f26ef"
   end
 
   depends_on "openssl@1.1"
-  depends_on "pcre"
+  depends_on "pcre2"
 
   uses_from_macos "zlib"
 
   def install
     args = %w[
-      USE_POLL=1
-      USE_PCRE=1
+      USE_PCRE2=1
+      USE_PCRE2_JIT=1
       USE_OPENSSL=1
-      USE_THREAD=1
       USE_ZLIB=1
-      ADDLIB=-lcrypto
     ]
-    if OS.mac?
-      args << "TARGET=generic"
-      # BSD only:
-      args << "USE_KQUEUE=1"
+
+    target = if OS.mac?
+      "osx"
     else
-      args << "TARGET=linux-glibc"
+      "linux-glibc"
     end
+    args << "TARGET=#{target}"
 
     # We build generic since the Makefile.osx doesn't appear to work
     system "make", *args
