1489e0135116682507f5ada2b464803073bbe7bd
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 23:53:28 +0800

    gst-python: update 1.18.5 bottle.

--- Formula/gst-python.rb
+++ Formula/gst-python.rb
@@ -1,8 +1,8 @@
 class GstPython < Formula
   desc "Python overrides for gobject-introspection-based pygst bindings"
   homepage "https://gstreamer.freedesktop.org/modules/gst-python.html"
-  url "https://gstreamer.freedesktop.org/src/gst-python/gst-python-1.18.4.tar.xz"
-  sha256 "cb68e08a7e825e08b83a12a22dcd6e4f1b328a7b02a7ac84f42f68f4ddc7098e"
+  url "https://gstreamer.freedesktop.org/src/gst-python/gst-python-1.18.5.tar.xz"
+  sha256 "533685871305959d6db89507f3b3aa6c765c2f2b0dacdc32c5a6543e72e5bc52"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -11,13 +11,8 @@ class GstPython < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "b4ec598c3bd3e35757da9d86c2b8cb42aa24b08226b15c1d37ab4abc77cfc73f"
-    sha256 arm64_big_sur:  "0141e26cbdf16eab9c46ca9fd95aaa31f1311297d627f18cf677d5440c9a1186"
-    sha256 monterey:       "53f99452f853e91551a9775300e1ddc667e6ae10eb3b00a15160e2305e89e0b0"
-    sha256 big_sur:        "c5461f496775e0d65ed07bb217d1f12eb68ba633ea9c688add6297fab942fdd6"
-    sha256 catalina:       "7de5518b76bea95a7769f1230db57fa03d389483f7800e060db7f2553ac0f856"
-    sha256 mojave:         "c155c0ff5e23c12f99746fd05058bfc5d01f4b3af9f2abf641035f7564efef1d"
-    sha256 x86_64_linux:   "081e9a6f6681bca1cc7f029cc483aa6821bfebe512af9046c8ac34a9c742ff5d"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gst-python"
+    sha256 mojave: "b5a7569d52396e8329152afb5ffd8b4b1079880ea13d785165c810172bfc76c1"
   end
 
   depends_on "meson" => :build
