86aa8ab661ff9e5e1a6d8139f72ab4b411a3cfd7
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 08:45:15 +0800

    cfr-decompiler: update 0.152 bottle.

--- Formula/cfr-decompiler.rb
+++ Formula/cfr-decompiler.rb
@@ -2,8 +2,8 @@ class CfrDecompiler < Formula
   desc "Yet Another Java Decompiler"
   homepage "https://www.benf.org/other/cfr/"
   url "https://github.com/leibnitz27/cfr.git",
-      tag:      "0.151",
-      revision: "fecd6421a8b9de98ade2b9f9b89caecf4a8c93d8"
+      tag:      "0.152",
+      revision: "68477be3ff7171ee17ddd1a26064b9b253f1604f"
   license "MIT"
   head "https://github.com/leibnitz27/cfr.git", branch: "master"
 
@@ -13,13 +13,8 @@ class CfrDecompiler < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d3f2087d72bd7319b4c34232db1c9e9e146c4919beb977b73af0676346612e73"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "75a69ae0a8ac09219854b6ed8b0ffff354c06cc01df7e3c343db71eb43489a02"
-    sha256 cellar: :any_skip_relocation, monterey:       "1ac3936440308ed59bd95394516b8dfaf5e9184dd766f14e64a88e80f6759d71"
-    sha256 cellar: :any_skip_relocation, big_sur:        "01b786e75cce66964d72c4da0e6545ed08275d7372e43f6e810c76bd65ee26d4"
-    sha256 cellar: :any_skip_relocation, catalina:       "59ae9cd6d368e85589ffd31f2ae0c223b9635d6cd5787e9a360de5f743f15ba2"
-    sha256 cellar: :any_skip_relocation, mojave:         "bad961cc5db80bdc29b4ac304147dc019b16efc01161a5e5289ad87cf835a5bf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ca40a7dc226badddecc91362d34dcf1a4b77c94dc779ed8138daddeb2660cde6"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cfr-decompiler"
+    sha256 cellar: :any_skip_relocation, mojave: "4407a9af52161440872e2904803137007690cc8d6f6255cc522466945b91d259"
   end
 
   depends_on "maven" => :build
