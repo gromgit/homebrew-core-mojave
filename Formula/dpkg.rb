b658cb26337b6d4229171069592aa736b75540e6
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 13:58:17 +0800

    dpkg: update 1.21.1 bottle.

--- Formula/dpkg.rb
+++ Formula/dpkg.rb
@@ -4,8 +4,8 @@ class Dpkg < Formula
   # Please use a mirror as the primary URL as the
   # dpkg site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://deb.debian.org/debian/pool/main/d/dpkg/dpkg_1.20.9.tar.xz"
-  sha256 "5ce242830f213b5620f08e6c4183adb1ef4dc9da28d31988a27c87c71fe534ce"
+  url "https://deb.debian.org/debian/pool/main/d/dpkg/dpkg_1.21.1.tar.xz"
+  sha256 "1eb9fd5228b3199284ea5134904bb45b7a5bc12fb044b8e4964d89d2e5bbb563"
   license "GPL-2.0-only"
 
   livecheck do
@@ -14,13 +14,8 @@ class Dpkg < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "e780edb6e715d268c548722db892cb84b28d4022064110e226fc59fa9500a81d"
-    sha256 arm64_big_sur:  "050cebde8ca0b1874974c4f6a03be2144055ef85485aa4da30017f8c645d440b"
-    sha256 monterey:       "acf37fed6f913397828e43e4c313908983285e848a83c2df44d45da3da6eb472"
-    sha256 big_sur:        "36fe071803813a6afff6cd69bbba249cd0321e3204302a1aee25f8f4873c934c"
-    sha256 catalina:       "15bb579c5dc9c7d36879fa5a07fe682f7064cef68ecd45f46cbe3aec0120c837"
-    sha256 mojave:         "422227f7e36fcdc361f8ba3fc5ba6c19603bd25ec3933cf6b5cef0eb5ccec523"
-    sha256 x86_64_linux:   "d1c09eb2e30e77d5a2d576c8a5ee2ab4eeff19dfce98dc5c6cc093e2c4512036"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dpkg"
+    sha256 mojave: "6ae1bc08a905bb43101fe8fa620c204488d721ea8dbd1d81ec9ae977a0c9e5ae"
   end
 
   depends_on "pkg-config" => :build
