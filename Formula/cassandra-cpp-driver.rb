c73c2b3e37bdc05155836b93b28959300754bb4c
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 08:09:39 +0800

    cassandra-cpp-driver: update 2.16.2 bottle.

--- Formula/cassandra-cpp-driver.rb
+++ Formula/cassandra-cpp-driver.rb
@@ -1,19 +1,14 @@
 class CassandraCppDriver < Formula
   desc "DataStax C/C++ Driver for Apache Cassandra"
   homepage "https://docs.datastax.com/en/developer/cpp-driver/latest"
-  url "https://github.com/datastax/cpp-driver/archive/2.16.1.tar.gz"
-  sha256 "168d6fe9f3cf61be82cf5817024b92a186d7f944f0d390ed546f521bdabfc32e"
+  url "https://github.com/datastax/cpp-driver/archive/2.16.2.tar.gz"
+  sha256 "de60751bd575b5364c2c5a17a24a40f3058264ea2ee6fef19de126ae550febc9"
   license "Apache-2.0"
   head "https://github.com/datastax/cpp-driver.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "14dbf3d7ab5519eb930e68750633b7478540f2e7b174284bfc758875665588d4"
-    sha256 cellar: :any,                 arm64_big_sur:  "c3127af73fe5c279aa1fce70d690770b00bc6754a74b9163fde9c897122517da"
-    sha256 cellar: :any,                 monterey:       "62325f0a72185c0bc298c4bdb72e165df2e2abd1c1116978bc93fa63ad9b7253"
-    sha256 cellar: :any,                 big_sur:        "a27012349a4335a6e68c87d043883b366e81b592ef0b5465281298b1c7d8aba9"
-    sha256 cellar: :any,                 catalina:       "19a4f1d92582723258c06b8e9f49783520d05ab4966de3a0ce29089d9e1bac59"
-    sha256 cellar: :any,                 mojave:         "9832a774e184a7bb13e060983cbab2ddbe63af9bbc2b36394eb74a855e6fbcde"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d368d0340f8d563c9773055722ab2410f25c4c2ca46583bf087388c8d465734a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cassandra-cpp-driver"
+    sha256 cellar: :any, mojave: "108994954fa7f350ea26cdb2263a0d1e0e69696995666fa442203c2d87620b0f"
   end
 
   depends_on "cmake" => :build
