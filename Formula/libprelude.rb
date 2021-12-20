98ecd3e5d25790dd66eefefc8ffb7e2df8bfdf99
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 06:27:13 +0800

    libprelude: update 5.2.0_2 bottle.

--- Formula/libprelude.rb
+++ Formula/libprelude.rb
@@ -4,7 +4,7 @@ class Libprelude < Formula
   url "https://www.prelude-siem.org/attachments/download/1395/libprelude-5.2.0.tar.gz"
   sha256 "187e025a5d51219810123575b32aa0b40037709a073a775bc3e5a65aa6d6a66e"
   license "GPL-2.0-or-later"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://www.prelude-siem.org/projects/prelude/files"
@@ -12,20 +12,15 @@ class Libprelude < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "67d1898db9ecddf2eec804af133dfb7151241ab61d520170bc4778564f12bd4b"
-    sha256 arm64_big_sur:  "62a0baace0727b7e5bbbf6dbb826e54604f18c28a3e2d20dda6bad782b50ffde"
-    sha256 monterey:       "588eee6b571351091d12034016622a2f787e90b26727e83a81a82f0425a89f14"
-    sha256 big_sur:        "c2e8d9d9c831ad24e13cd6d470a08c91c9dad1085ce7fe40e559df77b79a1503"
-    sha256 catalina:       "abd1ed78ae980d13cffbe2f7421179f3e416d63f0263b2df2749219bb4ade1ae"
-    sha256 mojave:         "99b39668c53d9e1514e8168db34402d897bd9159ecafdc3a377b097748945fb0"
-    sha256 x86_64_linux:   "282e13c10a69ba928d5f5597c5e88c33253caef9c5a221eae4f15dc670e125a7"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libprelude"
+    sha256 mojave: "41b9d05d8ffa3cce9268158bc4496be31bfad2a2393819d07d7b7e0718efa923"
   end
 
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "gnutls"
   depends_on "libgpg-error"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     ENV["HAVE_CXX"] = "yes"
@@ -38,7 +33,7 @@ class Libprelude < Formula
       --without-perl
       --without-swig
       --without-python2
-      --with-python3=python#{Formula["python@3.9"].version.major_minor}
+      --with-python3=python#{Language::Python.major_minor_version("python3")}
       --with-libgnutls-prefix=#{Formula["gnutls"].opt_prefix}
     ]
 
