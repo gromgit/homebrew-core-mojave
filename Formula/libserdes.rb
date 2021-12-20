21dfbee0f46b100a3622f7fa18f9b09ee7938db4
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 06:36:51 +0800

    libserdes: update 7.0.1 bottle.

--- Formula/libserdes.rb
+++ Formula/libserdes.rb
@@ -2,19 +2,14 @@ class Libserdes < Formula
   desc "Schema ser/deserializer lib for Avro + Confluent Schema Registry"
   homepage "https://github.com/confluentinc/libserdes"
   url "https://github.com/confluentinc/libserdes.git",
-      tag:      "v7.0.0",
-      revision: "a04cc80dc38aaf4ed7b4c9c60b781ebf4cbfda5e"
+      tag:      "v7.0.1",
+      revision: "8c02e4cd98f0eaf6113fc1fa0b8a0ee9350c4961"
   license "Apache-2.0"
   head "https://github.com/confluentinc/libserdes.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "7c15431602aecdd6cb2e108d7d84e4f7468a17235cdf0289dd619a3cf951251c"
-    sha256 cellar: :any,                 arm64_big_sur:  "ae93f71843a6b09dee0ec5921e12ecea336f1dab672509c3780868edd674f2b4"
-    sha256 cellar: :any,                 monterey:       "49d5c408b5f11670190146a8aa78db5499d5c84d52ced60df72b21437ce39ffa"
-    sha256 cellar: :any,                 big_sur:        "8dc8f1c91e214a86d2d2b9b3c9e093f11f8410327f0834581be528cb31ef0e2f"
-    sha256 cellar: :any,                 catalina:       "6e787eee3da70740262d668ba474c849fb060d807f5896fb5fa741f87757c379"
-    sha256 cellar: :any,                 mojave:         "bafb84c4afe99d9852283bbc86370294aa973569eb4c85f600f0c8993428de56"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3129deb96b23fc8518456f9e1673b36f1cf74e7b55e1f5f7f72fe840573c8331"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libserdes"
+    sha256 cellar: :any, mojave: "5bf57e869f84fc30cac4466f8c7cab260971ed2fd2eb1e1c39731bd507c5e97d"
   end
 
   depends_on "avro-c"
