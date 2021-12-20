418bf5f54efe49f91dfcbb074b747979e8aebc15
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 02:21:23 +0800

    ipfs: update 0.11.0 bottle.

--- Formula/ipfs.rb
+++ Formula/ipfs.rb
@@ -2,8 +2,8 @@ class Ipfs < Formula
   desc "Peer-to-peer hypermedia protocol"
   homepage "https://ipfs.io/"
   url "https://github.com/ipfs/go-ipfs.git",
-      tag:      "v0.10.0",
-      revision: "64b532fbb14145557dda7cb7986daea1e156f76d"
+      tag:      "v0.11.0",
+      revision: "67220edaaef4a938fe5fba85d793bfee59db3256"
   license all_of: [
     "MIT",
     any_of: ["MIT", "Apache-2.0"],
@@ -16,13 +16,8 @@ class Ipfs < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a0d12a2f21f6cf20269d39090e256909d83660ad3f6a34bff55acb6e86a24205"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "976419cbae8a7d319812f22f21067bc3ed592d984d91e09a2a5eb6b92ec3c702"
-    sha256 cellar: :any_skip_relocation, monterey:       "046728591c396e684b3c1b4cac281a61f76c820988e36e818932baa7d74a7e77"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8c9e173c657f3286c06d0456c8f603b3c3a2f13664a960a7b62cf8f1e1aa253d"
-    sha256 cellar: :any_skip_relocation, catalina:       "83669d4ead8a9837e27cb08abc146d9b61ebb39169744e02a1253a09bbf1e74d"
-    sha256 cellar: :any_skip_relocation, mojave:         "2921ea76d75fc514b74c0230f8f2a7ea909a63d84543a03776e1dbbc50f35e18"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ece5b0718dbe752e685c8f6c1df0ba0d92bf771305e6ce782f30fa156a8d4e6f"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ipfs"
+    sha256 cellar: :any_skip_relocation, mojave: "f230463eadd4005cac2fa3573d26d3ae6df685e2faffaf0e4811e6f9d8b00684"
   end
 
   depends_on "go" => :build
