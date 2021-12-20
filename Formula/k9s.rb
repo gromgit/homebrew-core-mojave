74ec7ec2b8783bb3523f5daca3b39f0a6bc60f14
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 04:25:49 +0800

    k9s: update 0.25.15 bottle.

--- Formula/k9s.rb
+++ Formula/k9s.rb
@@ -2,23 +2,25 @@ class K9s < Formula
   desc "Kubernetes CLI To Manage Your Clusters In Style!"
   homepage "https://k9scli.io/"
   url "https://github.com/derailed/k9s.git",
-      tag:      "v0.25.8",
-      revision: "f97bceaf643c0744353196f8d97221a2d111635a"
+      tag:      "v0.25.15",
+      revision: "df613ec88d2d77d5b6e8804ceca959a544218391"
   license "Apache-2.0"
   head "https://github.com/derailed/k9s.git"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/k9s"
-    sha256 cellar: :any_skip_relocation, mojave: "8afea8ef449a0f83c9c8419f46081be723262e54523aaac933b582a6ae25fc20"
+    sha256 cellar: :any_skip_relocation, mojave: "29c2a82252a8c9033b5902629907f05008666f919fe50158ac40ce76c4dd2d35"
   end
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-ldflags",
-             "-s -w -X github.com/derailed/k9s/cmd.version=#{version}
-             -X github.com/derailed/k9s/cmd.commit=#{Utils.git_head}",
-             *std_go_args
+    ldflags = %W[
+      -s -w
+      -X github.com/derailed/k9s/cmd.version=#{version}
+      -X github.com/derailed/k9s/cmd.commit=#{Utils.git_head}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
 
     bash_output = Utils.safe_popen_read(bin/"k9s", "completion", "bash")
     (bash_completion/"k9s").write bash_output
