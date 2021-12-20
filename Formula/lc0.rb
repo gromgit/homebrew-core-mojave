4104ca3b7a33f26025efcd3d33085131272f0750
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 05:46:27 +0800

    lc0: update 0.28.2 bottle.

--- Formula/lc0.rb
+++ Formula/lc0.rb
@@ -2,25 +2,20 @@ class Lc0 < Formula
   desc "Open source neural network based chess engine"
   homepage "https://lczero.org/"
   url "https://github.com/LeelaChessZero/lc0.git",
-      tag:      "v0.28.0",
-      revision: "3982cc0e74b576476c875da6fa0ff81164287425"
+      tag:      "v0.28.2",
+      revision: "fa5864bb5838e131d832ad63300517f4684913e7"
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "392f9168c6f244ddff5ae49eeaa336fa96c4c76e29f6cfa8932c40defca12889"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "36aa6aad76aee88ce231dc5a0aac948f097055351813351876acd08dfc450a03"
-    sha256 cellar: :any_skip_relocation, monterey:       "d109979fed723569ff979e1cc24db86f1df7c5c8bf8b8bfcf9be6eb971441e28"
-    sha256 cellar: :any_skip_relocation, big_sur:        "994f3b0045579eccda6c5eb1241a4484c202bb9c8528184ba2e4eee770a7e2c5"
-    sha256 cellar: :any_skip_relocation, catalina:       "e37cf24d564b5649d873692e5162a90e7e1840e2346326be86ef0ba25c8b7737"
-    sha256 cellar: :any_skip_relocation, mojave:         "650ff1cdf5e4aad508572322e89010722c92bf30b89ed07f95cdec6dd1fd0140"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4d89547675b9eb3c62073100047d9f9d91e51f9d3063655cf38e7f836dd28063"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lc0"
+    sha256 cellar: :any_skip_relocation, mojave: "238aa4d0d4d84a6ce1799df8f14d76ce2bc400b1fe5ec0867d52ee7efa554c8a"
   end
 
   depends_on "cmake" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@3.9" => :build # required to compile .pb files
+  depends_on "python@3.10" => :build # required to compile .pb files
   depends_on "eigen"
 
   uses_from_macos "zlib"
