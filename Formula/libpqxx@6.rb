a31156cafd2b45e63149d04da1c051074774a3cd
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 06:20:45 +0800

    libpqxx@6: update 6.4.7_1 bottle.

--- Formula/libpqxx@6.rb
+++ Formula/libpqxx@6.rb
@@ -7,14 +7,9 @@ class LibpqxxAT6 < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "31d80459d21a2c53f3a56d2b17d7ea2715aef4a32b42156fe2f83d62bacac48d"
-    sha256 cellar: :any,                 arm64_big_sur:  "b6f56911155c390dfbe7351fda8334b1dd47d7fed3d7001e767228144e45cc67"
-    sha256 cellar: :any,                 monterey:       "257da71271b6e500f91a35623b951dd1ec37fd66d9b9a9c00ef9d1baa5c33396"
-    sha256 cellar: :any,                 big_sur:        "e21e51c071cc9cb879d7ab688f3fba8cf8e32cf14f34779b04db95ec67d1289b"
-    sha256 cellar: :any,                 catalina:       "29def17a973940490a25c20f5722f6ea4d0551e41cd7986b9025abef40b1534e"
-    sha256 cellar: :any,                 mojave:         "4b544c65887866135d96226e2bf7c2b586664f8e1a049f6d3dbeca7195884a6f"
-    sha256 cellar: :any,                 high_sierra:    "39aa6c090c8341c0e9be80d055345c8322ee6a9a908a0f7863479784cbd609f5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "048e4249607f887c9787a40ca3b4b2092e5ffa8adc649f52940af42406fe5080"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libpqxx@6"
+    rebuild 1
+    sha256 cellar: :any, mojave: "4b43e01c10ab5d52e86a773c46a96db433c09fa1f7d246e1e3dad029b060125a"
   end
 
   keg_only :versioned_formula
@@ -22,12 +17,12 @@ class LibpqxxAT6 < Formula
   deprecate! date: "2020-06-23", because: :versioned_formula
 
   depends_on "pkg-config" => :build
-  depends_on "python@3.9" => :build
+  depends_on "python@3.10" => :build
   depends_on "xmlto" => :build
   depends_on "libpq"
 
   def install
-    ENV.prepend_path "PATH", Formula["python@3.9"].opt_libexec/"bin"
+    ENV.prepend_path "PATH", Formula["python@3.10"].opt_libexec/"bin"
     ENV["PG_CONFIG"] = Formula["libpq"].opt_bin/"pg_config"
 
     system "./configure", "--prefix=#{prefix}", "--enable-shared"
