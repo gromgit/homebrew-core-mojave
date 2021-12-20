3959b311fdc8386329d220296061d2bb3c5aa2ab
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 16:04:43 +0800

    elvish: update 0.17.0 bottle.

--- Formula/elvish.rb
+++ Formula/elvish.rb
@@ -1,19 +1,14 @@
 class Elvish < Formula
   desc "Friendly and expressive shell"
   homepage "https://github.com/elves/elvish"
-  url "https://github.com/elves/elvish/archive/v0.16.3.tar.gz"
-  sha256 "ea9d594070cff58ed9caedf4619ee195bfce179f79b9a8d5e7a635ce5cbba551"
+  url "https://github.com/elves/elvish/archive/v0.17.0.tar.gz"
+  sha256 "0e255849723129d8c4dc24f67656e651b4e4b7566bc16009109ba76099681fa1"
   license "BSD-2-Clause"
   head "https://github.com/elves/elvish.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0d622f7dbe863693b07eb3a03335d6633d952f924d2af276bc2cd3c915961607"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "22868343fa4e1cf95ff4c59c5a7d88216fd497e3c1ef4bce63ebd80919725cb2"
-    sha256 cellar: :any_skip_relocation, monterey:       "408a773e781c66875487fbc1a9eb97cdd9f6d42f29301ffbc199112a17e84747"
-    sha256 cellar: :any_skip_relocation, big_sur:        "baca586af00fca196d69e0e8b1b3309df1c0b8f7d72e4089fc20f8e1a2a1bbb4"
-    sha256 cellar: :any_skip_relocation, catalina:       "86453d36b3aed50cab567de1a763204f0162eb8b41ac039c7061c94cdaca630d"
-    sha256 cellar: :any_skip_relocation, mojave:         "c345a3ec54ff058d8fbd86a97948412e811785ccefe097b3defeb0061eaf0946"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7397b282b7170800c11579efe0bce701861703966dccc32113dc870345ca43e8"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/elvish"
+    sha256 cellar: :any_skip_relocation, mojave: "51139a8e0b26f459421b507dc8ca79afe96ac8f2dadbb7257efbfadb87206f77"
   end
 
   depends_on "go" => :build
