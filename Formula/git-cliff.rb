54a7a08b01bf7bb07472ec60c85b98febbda47ce
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 20:52:45 +0800

    git-cliff: update 0.5.0 bottle.

--- Formula/git-cliff.rb
+++ Formula/git-cliff.rb
@@ -1,14 +1,13 @@
 class GitCliff < Formula
   desc "Highly customizable changelog generator"
   homepage "https://github.com/orhun/git-cliff"
-  url "https://github.com/orhun/git-cliff/archive/v0.4.2.tar.gz"
-  sha256 "9f0c6e59970b39d633c94c804e979e227fed5b7d95b6c59352923aa92cdc67a7"
+  url "https://github.com/orhun/git-cliff/archive/v0.5.0.tar.gz"
+  sha256 "6172c430505ae3337e3fafad02628d9f6d1c04089580b6bc1c7ac6f1353d9b82"
   license "GPL-3.0-only"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-cliff"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "7c42b676bd4c90bf410f3cc9af5aa4bb56ca8eca36bd0bfade8d58a0afd6e470"
+    sha256 cellar: :any_skip_relocation, mojave: "95e97d570b7782b780bc326991e1ca688ba0c81cda71dd680c1557f34e18e327"
   end
 
   depends_on "rust" => :build
