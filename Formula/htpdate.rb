94d6f9ccc6067324fffb3472f14a3007447ccfb6
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 01:06:09 +0800

    htpdate: update 1.2.4 bottle.

--- Formula/htpdate.rb
+++ Formula/htpdate.rb
@@ -1,8 +1,9 @@
 class Htpdate < Formula
   desc "Synchronize time with remote web servers"
   homepage "https://www.vervest.org/htp/"
-  url "https://www.vervest.org/htp/archive/c/htpdate-1.2.3.tar.xz"
-  sha256 "6b4e3fbf93d552a3a20f30a3906bf0caac05d9626bd508220744010fe9dd53f0"
+  url "https://www.vervest.org/htp/archive/c/htpdate-1.2.4.tar.gz"
+  sha256 "8c735ccef0857b71478a838b136d7e177b8d78283a6b51633472b273cc46dd18"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url "https://www.vervest.org/htp/?download"
@@ -11,7 +12,7 @@ class Htpdate < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/htpdate"
-    sha256 cellar: :any_skip_relocation, mojave: "9e91e0ff30a4ed345d14a05a7527c0faf23bddc33f710f019838234fb3f42f2b"
+    sha256 cellar: :any_skip_relocation, mojave: "68af9b871d921f28fb7cf0b5df84282360daa8baac275658b8422abeb5c110b9"
   end
 
   depends_on macos: :high_sierra # needs <sys/timex.h>
@@ -25,6 +26,6 @@ class Htpdate < Formula
   end
 
   test do
-    system "#{bin}/htpdate", "-q", "-d", "-u", ENV["USER"], "example.org"
+    system "#{sbin}/htpdate", "-q", "-d", "-u", ENV["USER"], "example.org"
   end
 end
