0540eea2d05d4cbe233ca7e3301c2904df3104fd
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 17:36:31 +0800

    flow: update 0.168.0 bottle.

--- Formula/flow.rb
+++ Formula/flow.rb
@@ -1,14 +1,14 @@
 class Flow < Formula
   desc "Static type checker for JavaScript"
   homepage "https://flowtype.org/"
-  url "https://github.com/facebook/flow/archive/v0.166.1.tar.gz"
-  sha256 "45ed148e2862023b79259c11a75b5d7339115ae29c97b6f4e5a3411f1d2ecfaf"
+  url "https://github.com/facebook/flow/archive/v0.168.0.tar.gz"
+  sha256 "c21a013864c4c91f129d306e7d2dde946b0cbdbaa77f82b502b3d5f42122434f"
   license "MIT"
   head "https://github.com/facebook/flow.git"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/flow"
-    sha256 cellar: :any_skip_relocation, mojave: "867d3e9329663e9212c6104087b8994d8981da2dae105c9b17654ae8667e629c"
+    sha256 cellar: :any_skip_relocation, mojave: "2b94e046b8b0ada057447ffdb322334f44c4b78c66df9e1d78f7489fb555b436"
   end
 
   depends_on "ocaml" => :build
