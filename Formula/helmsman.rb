cd884282944d8310625834f037181b2321323b6c
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 00:34:07 +0800

    helmsman: update 3.8.0 bottle.

--- Formula/helmsman.rb
+++ Formula/helmsman.rb
@@ -2,15 +2,14 @@ class Helmsman < Formula
   desc "Helm Charts as Code tool"
   homepage "https://github.com/Praqma/helmsman"
   url "https://github.com/Praqma/helmsman.git",
-      tag:      "v3.7.7",
-      revision: "321afde41994fb847b36a1da3590d5d405b34f1d"
+      tag:      "v3.8.0",
+      revision: "c511a0961fff3c1880b43ed2b00adddb7ff9fc26"
   license "MIT"
   head "https://github.com/Praqma/helmsman.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/helmsman"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "e3e7ca6972f45040257270bec88cf93244a61857a547cea7361ed6217d91306f"
+    sha256 cellar: :any_skip_relocation, mojave: "ce45e795146eceb355a6d25be54166d81fce1d4dd594028ec4741bcdcdd894ba"
   end
 
   depends_on "go" => :build
