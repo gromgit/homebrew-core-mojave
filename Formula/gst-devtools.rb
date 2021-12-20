f305738f2d8ec6aab4e93c0d4bbd2fdc4b574622
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 23:26:20 +0800

    gst-devtools: update 1.18.5_1 bottle.

--- Formula/gst-devtools.rb
+++ Formula/gst-devtools.rb
@@ -6,6 +6,7 @@ class GstDevtools < Formula
   url "https://gstreamer.freedesktop.org/src/gst-devtools/gst-devtools-1.18.5.tar.xz"
   sha256 "fecffc86447daf5c2a06843c757a991d745caa2069446a0d746e99b13f7cb079"
   license "LGPL-2.1-or-later"
+  revision 1
   head "https://gitlab.freedesktop.org/gstreamer/gst-devtools.git"
 
   livecheck do
@@ -14,13 +15,8 @@ class GstDevtools < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "98d8f50b933ba73a496c847bf8468a9703766cfc33de874c1025627a85a81318"
-    sha256 arm64_big_sur:  "f80540ba393407aceb95c070461ad5ebbbb9bd9869e9148777350657d6491cfa"
-    sha256 monterey:       "140957153cd0d4b572e10820ebd22f1ea60793b7baeabf20776dda7419644cd4"
-    sha256 big_sur:        "d592155862275fb39caccda2a52faee2478755fdf3c44f63cf43043cc823a2ca"
-    sha256 catalina:       "a3867172205b79066778fc3b0337b569bbdf44c4d7c21e51720d10f0af070da9"
-    sha256 mojave:         "10ca693d40baa57b6c8c161923de066c23f1fbba9004ba7106cfa09739effe8f"
-    sha256 x86_64_linux:   "e5cd968f9cdf62927fd3054ca7205e79470a3ac11109bccc15dd89d5cab5dedd"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gst-devtools"
+    sha256 mojave: "0c168a713e964784aea5d5a87bd7bf8214565ee3ca92d5fc33526087d4e31500"
   end
 
   depends_on "gobject-introspection" => :build
