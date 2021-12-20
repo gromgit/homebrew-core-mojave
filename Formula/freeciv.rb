8054086e1cfe4fe3375411196fa71c6e43a84411
Adrian Ho <the.gromgit@gmail.com>
2021-12-20 18:22:27 +0800

    freeciv: update 2.6.6 bottle.

--- Formula/freeciv.rb
+++ Formula/freeciv.rb
@@ -1,8 +1,8 @@
 class Freeciv < Formula
   desc "Free and Open Source empire-building strategy game"
   homepage "http://freeciv.org"
-  url "https://downloads.sourceforge.net/project/freeciv/Freeciv%202.6/2.6.5/freeciv-2.6.5.tar.bz2"
-  sha256 "2e64e1c74dae12acb17bbf5daa980efc7e9fd57820afed5135319ca95291ec59"
+  url "https://downloads.sourceforge.net/project/freeciv/Freeciv%202.6/2.6.6/freeciv-2.6.6.tar.bz2"
+  sha256 "7bcfe5dd3c081122e7cde7cdb24f57c3681d6a9667496faed207a91ac3165811"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -11,12 +11,8 @@ class Freeciv < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "dc27602cdb795f299647d4262758409775599ee932606aad9fa34a78a8d102aa"
-    sha256 monterey:      "bdae2e778459cc59055d375a7664351db7abdb5e31fc90d50e89258276f20cc3"
-    sha256 big_sur:       "fa1955a0573dfabfb99742ca216f2caad95053e5909975990504097063ff239f"
-    sha256 catalina:      "e93e825aebcb5c046e1b8114a366d8010f9e30a35120543327dbb7f14303cd75"
-    sha256 mojave:        "5365cbee42fe93a79db42fe7d1f9690f523090d9b612ef5f508a70ed83063179"
-    sha256 x86_64_linux:  "4da618352125e2a2fbe8a2567ca18e8ac55271a0c2e244a000b7f72867ebb620"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/freeciv"
+    sha256 mojave: "d0ea01ac6f093b989ec16facd679430e0170b304b38ab66102bed6874bd62646"
   end
 
   head do
