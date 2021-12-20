616a00ab87abb14e303b33aa29b4fb93c51ae62b
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 05:06:58 +0800

    kubernetes-cli@1.22: update 1.22.4 bottle.

---
+++ Formula/kubernetes-cli@1.22.rb
@@ -0,0 +1,69 @@
+class KubernetesCliAT122 < Formula
+  desc "Kubernetes command-line interface"
+  homepage "https://kubernetes.io/"
+  url "https://github.com/kubernetes/kubernetes.git",
+      tag:      "v1.22.4",
+      revision: "b695d79d4f967c403a96986f1750a35eb75e75f1"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(1\.22(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kubernetes-cli@1.22"
+    sha256 cellar: :any_skip_relocation, mojave: "f86569d94a17ad2ed1062cd345a35bfda35e2c35cdf9af41b3e6c56179005886"
+  end
+
+  keg_only :versioned_formula
+
+  # https://kubernetes.io/releases/patch-releases/#1-22
+  deprecate! date: "2022-08-28", because: :deprecated_upstream
+  # disable! date: "2022-10-28", because: :deprecated_upstream
+
+  depends_on "bash" => :build
+  depends_on "coreutils" => :build
+  depends_on "go@1.16" => :build
+
+  uses_from_macos "rsync" => :build
+
+  def install
+    # Don't dirty the git tree
+    rm_rf ".brew_home"
+
+    # Make binary
+    # Deparallelize to avoid race conditions in creating symlinks, creating an error like:
+    #   ln: failed to create symbolic link: File exists
+    # See https://github.com/kubernetes/kubernetes/issues/106165
+    ENV.deparallelize
+    ENV.prepend_path "PATH", Formula["coreutils"].libexec/"gnubin" # needs GNU date
+    system "make", "WHAT=cmd/kubectl"
+    bin.install "_output/bin/kubectl"
+
+    # Install bash completion
+    output = Utils.safe_popen_read(bin/"kubectl", "completion", "bash")
+    (bash_completion/"kubectl").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read(bin/"kubectl", "completion", "zsh")
+    (zsh_completion/"_kubectl").write output
+
+    # Install man pages
+    # Leave this step for the end as this dirties the git tree
+    system "hack/update-generated-docs.sh"
+    man1.install Dir["docs/man/man1/*.1"]
+  end
+
+  test do
+    run_output = shell_output("#{bin}/kubectl 2>&1")
+    assert_match "kubectl controls the Kubernetes cluster manager.", run_output
+
+    version_output = shell_output("#{bin}/kubectl version --client 2>&1")
+    assert_match "GitTreeState:\"clean\"", version_output
+    if build.stable?
+      revision = stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision]
+      assert_match revision.to_s, version_output
+    end
+  end
+end
