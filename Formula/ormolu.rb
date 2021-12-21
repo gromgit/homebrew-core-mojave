d58e1f4a40d8ab78bfd4665cc066725d448557da
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 17:49:56 +0800

    ormolu: update 0.4.0.0 bottle.

--- Formula/ormolu.rb
+++ Formula/ormolu.rb
@@ -1,19 +1,14 @@
 class Ormolu < Formula
   desc "Formatter for Haskell source code"
   homepage "https://github.com/tweag/ormolu"
-  url "https://github.com/tweag/ormolu/archive/0.3.1.0.tar.gz"
-  sha256 "b0bbf229f1878c39aa58dbb71d8cc4fbc4713252c7acbc0fd9921804fecbd273"
+  url "https://github.com/tweag/ormolu/archive/0.4.0.0.tar.gz"
+  sha256 "c87b2e09cef54aa7568b9bc990bb6ffd7b8dae3d8b950557fbe60ec286039353"
   license "BSD-3-Clause"
   head "https://github.com/tweag/ormolu.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "acd617cb4100f5d5bacd752db1752bc4dad4449a0b06a39fac61241f000d60ad"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "70cd7434a57c0153d1408a67d8315fe70d6011f9372d9f54d7bedd970cc815f5"
-    sha256 cellar: :any_skip_relocation, monterey:       "9eeaf61e2ea11cbd8d36b11067fd2abbd4d5a884ca4877b53adf0bfe9f7706d0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "db763c64ee0667d536beed9a908ef4d098148bf653e06987304320d9bea7eebe"
-    sha256 cellar: :any_skip_relocation, catalina:       "075fc7b2afa03d4db8d517a3951ba2bd1d28859013b57e107728a2bd75ebf9d0"
-    sha256 cellar: :any_skip_relocation, mojave:         "d990e97bfb7cb25d3090f47a5c1b8e164b8a8a46cfeb2d79216c17337ef339b2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c50a4ad780a90bff58aaa6ddb13b908207b5d6188f059ab5403e64fe3dfe861e"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ormolu"
+    sha256 cellar: :any_skip_relocation, mojave: "7fb85627e98859aac4e50b0298b9efd7a053bcb460b36bec61b47390bc732810"
   end
 
   depends_on "cabal-install" => :build
