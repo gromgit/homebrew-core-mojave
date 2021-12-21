d725dea160d2ea29e0703ac46ce8df7744b1e4b9
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 19:46:50 +0800

    polyml: update 5.9 bottle.

--- Formula/polyml.rb
+++ Formula/polyml.rb
@@ -1,17 +1,14 @@
 class Polyml < Formula
   desc "Standard ML implementation"
   homepage "https://www.polyml.org/"
-  url "https://github.com/polyml/polyml/archive/v5.8.2.tar.gz"
-  sha256 "310b0ba748a50f38e99de7f65ba990bc4b4f4b0123ad76aba4c44d7cd1ed9277"
-  license "LGPL-2.1"
+  url "https://github.com/polyml/polyml/archive/v5.9.tar.gz"
+  sha256 "5aa452a49f2ac0278668772af4ea0b9bf30c93457e60ff7f264c5aec2023c83e"
+  license "LGPL-2.1-or-later"
   head "https://github.com/polyml/polyml.git", branch: "master"
 
   bottle do
-    sha256 monterey:     "66cfea4838c14363d2e8be74e3e1a6b5f9f7f690a6dabae427a133ea6b05008e"
-    sha256 big_sur:      "4b68c9e84f40360b1b65444949637bb1f5749f532ad198a44f3bb570854b9900"
-    sha256 catalina:     "1517e342bf9c4569b986d1139c063e14a999cacb29597e53e438040090e93424"
-    sha256 mojave:       "fccbd2fc3c3570178c8578475035fbee24ab9280a3366a82b797c1fb7627c588"
-    sha256 x86_64_linux: "d356087174d4a1031bbacaac76dfcb1735d613a55510ff8b0207ea6d53994038"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/polyml"
+    sha256 mojave: "60bd333e7bf57e6c03f486f6964ae2a317c93286050e6e6f97dd435d8ed5751e"
   end
 
   # Fix -flat_namespace being used on Big Sur and later.
