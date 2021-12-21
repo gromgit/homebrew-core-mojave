8e393b1f9f70728d606f07f6f85f9cd5a40ae721
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 20:25:34 +0800

    procs: update 0.11.12 bottle.

--- Formula/procs.rb
+++ Formula/procs.rb
@@ -1,18 +1,13 @@
 class Procs < Formula
   desc "Modern replacement for ps written by Rust"
   homepage "https://github.com/dalance/procs"
-  url "https://github.com/dalance/procs/archive/v0.11.10.tar.gz"
-  sha256 "e6a869722181f2122a5a223517a2d1f6505d19b8b9d46ffd59c61fb02f472403"
+  url "https://github.com/dalance/procs/archive/v0.11.12.tar.gz"
+  sha256 "ff1a293d84cc0160cc92b4530ee93cedf530796e80130df74b0490c3e884efb6"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6cda97b69cb63427f07875e3e51cac1dd861980bf165c17afca91007cb345259"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d3ced65136eb04ee75e2f76ea58662b79180bd11a798afc954aacb6e3daed175"
-    sha256 cellar: :any_skip_relocation, monterey:       "35ab2d6e018e8fe8ac93d787511341d0f32e8b559c90156ae8fde726b1595782"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5d8f5d095f2adf427156eb6cc2d99801da1c0fb9144b4d6a6a031984c12bd5fb"
-    sha256 cellar: :any_skip_relocation, catalina:       "758a0ecceb0cd3fe2cf37cb256407f8ef2863dc981d5df5e7c931069a8cf53da"
-    sha256 cellar: :any_skip_relocation, mojave:         "5a97c63219f0a142531665ee3450670cd20544ace5940584768ad688ebcc01eb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7285ee5dd8fbd6892b2da602fb205e2a65c0fe356c441f5ca625c68538557035"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/procs"
+    sha256 cellar: :any_skip_relocation, mojave: "2d8417ad7ab72392b2e8fe7c8689b3de6c0b7e07d06d812d239a38c804ef0984"
   end
 
   depends_on "rust" => :build
