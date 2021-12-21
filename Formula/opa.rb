7d0773eced3b45483786b51db3dcc7c69c6dcde3
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 17:09:38 +0800

    opa: update 0.35.0_1 bottle.

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -4,11 +4,12 @@ class Opa < Formula
   url "https://github.com/open-policy-agent/opa/archive/v0.35.0.tar.gz"
   sha256 "4107a58ee7e6db9304924ce77c1250a6449dccc3f6f7c1abb24b9fd457c3110d"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/open-policy-agent/opa.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/opa"
-    sha256 cellar: :any_skip_relocation, mojave: "f79e1d81b1b29cbfb17d45e34116954991cc3e9c9db3b1280591ae8523be86c4"
+    sha256 cellar: :any_skip_relocation, mojave: "f85fa083cd79c6033e8f011ac54a173e5ed4fdb09400b4c89190030e62e690d5"
   end
 
   depends_on "go" => :build
@@ -18,6 +19,15 @@ class Opa < Formula
               "-ldflags", "-X github.com/open-policy-agent/opa/version.Version=#{version}"
     system "./build/gen-man.sh", "man1"
     man.install "man1"
+
+    bash_output = Utils.safe_popen_read(bin/"opa", "completion", "bash")
+    (bash_completion/"opa").write bash_output
+
+    zsh_output = Utils.safe_popen_read(bin/"opa", "completion", "zsh")
+    (zsh_completion/"_opa").write zsh_output
+
+    fish_output = Utils.safe_popen_read(bin/"opa", "completion", "fish")
+    (fish_completion/"opa.fish").write fish_output
   end
 
   test do
