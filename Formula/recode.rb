cb690bc7c9db8144eefcd2a5428541df7e022d48
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 22:23:58 +0800

    recode: update 3.7.9 bottle.

--- Formula/recode.rb
+++ Formula/recode.rb
@@ -6,17 +6,13 @@ class Recode < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "34950a051066a49e08b009f7e438d318eb124c11fc4735caa40fc984abc1362c"
-    sha256 cellar: :any,                 arm64_big_sur:  "9bca3845a8c324b5f9546c2c9de7881ee2a91b47513a39188fbc65d5c119b3bd"
-    sha256 cellar: :any,                 monterey:       "b8ef0a374619a0a63c7c28da8682c6fd33d9dd85164c8dfacbbe9d3e3f5fe5c8"
-    sha256 cellar: :any,                 big_sur:        "322571c853f461cd7f85afde9da5895996479b8aafddaa729081c10f4b319c57"
-    sha256 cellar: :any,                 catalina:       "e049f7705d6f397a3a4bb87e31cc43eeb8ab7f0958b5efb3daccbab2a77aaa96"
-    sha256 cellar: :any,                 mojave:         "27ba44840e12f588f741d2a7477ac8e6c6d3df22b09b426c617c87b98518d5c8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4eaa20bd35e3d6937e2c8fa962572eda4998ef7504049b9592a62b3f133c1edd"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/recode"
+    rebuild 1
+    sha256 cellar: :any, mojave: "32bf479c118c67382882ca99e3b940f14d72df9df8df827d53f901f2071228c4"
   end
 
   depends_on "libtool" => :build
-  depends_on "python@3.9" => :build
+  depends_on "python@3.10" => :build
   depends_on "gettext"
 
   # Fix -flat_namespace being used on Big Sur and later.
