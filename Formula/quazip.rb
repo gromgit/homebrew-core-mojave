e37bfb4922985c6a4d177db6835231b216ae5a5b
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 21:29:08 +0800

    quazip: update 1.2 bottle.

--- Formula/quazip.rb
+++ Formula/quazip.rb
@@ -1,16 +1,13 @@
 class Quazip < Formula
   desc "C++ wrapper over Gilles Vollant's ZIP/UNZIP package"
   homepage "https://github.com/stachenov/quazip/"
-  url "https://github.com/stachenov/quazip/archive/v1.1.tar.gz"
-  sha256 "54edce9c11371762bd4f0003c2937b5d8806a2752dd9c0fd9085e90792612ad0"
+  url "https://github.com/stachenov/quazip/archive/v1.2.tar.gz"
+  sha256 "2dfb911d6b27545de0b98798d967c40430312377e6ade57096d6ec80c720cb61"
   license "LGPL-2.1-only"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "8dfc18198f69a4c025127fc0843c08a029ec2cd433a0c81253a9f9fff9611bdd"
-    sha256 cellar: :any, arm64_big_sur:  "1976479aab99b66660df7c4cb9cf5124885df1f4c579dea8d6efa019d3425139"
-    sha256 cellar: :any, big_sur:        "343bb099db746afecb32ef268aeacf45522e67fe063975815cfb980ed1576fda"
-    sha256 cellar: :any, catalina:       "cd85589dcc4e2f401000c786a57320a4773665c11992247a1065f6e23a4f70c0"
-    sha256 cellar: :any, mojave:         "bab3b293744908346e3438f9ed49659b8be8594ab60dd1e0bc0c88864ea359d2"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/quazip"
+    sha256 cellar: :any, mojave: "b642f526239137cc5baaff5d0bb91c7606bda00714815ed0156f671f856e6245"
   end
 
   depends_on "cmake" => :build
