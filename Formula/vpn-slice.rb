5948f3f8aa91219935e0e8d9fed37d3861f274b2
Adrian Ho <the.gromgit@gmail.com>
2021-12-22 06:19:19 +0800

    vpn-slice: update 0.16 bottle.

--- Formula/vpn-slice.rb
+++ Formula/vpn-slice.rb
@@ -3,32 +3,27 @@ class VpnSlice < Formula
 
   desc "Vpnc-script replacement for easy and secure split-tunnel VPN setup"
   homepage "https://github.com/dlenski/vpn-slice"
-  url "https://files.pythonhosted.org/packages/22/a2/55d1f41fdc1708c0a005f5fc678b85acaa3ed5ba470a3a0410898b3a61ff/vpn-slice-0.15.tar.gz"
-  sha256 "7d5133aecbed9d5696d59dcb799c3d8d30a89a08f6d36fac335f6b8357786353"
+  url "https://files.pythonhosted.org/packages/bc/79/49fc8347a51f70f4c5501f93809f4dc757729def77749cf16af6643074de/vpn-slice-0.16.tar.gz"
+  sha256 "6d5c8d972126775181397dcc65332ec60c5b35fe1647b1022ace863589c59a12"
   license "GPL-3.0-or-later"
-  revision 1
   head "https://github.com/dlenski/vpn-slice.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c7e8122a3a4c573ce3f56a4ea590d8dd624e3d9a77a6ef5c6cc7920264a8c470"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4d573308656014686896eb8f1089b34c80681dcd9f9f236830cc4047fc7b2a95"
-    sha256 cellar: :any_skip_relocation, monterey:       "fd8425b424c9a0ab4f20d520a5161ea6fd4df98dd2abae8167fce85a037f8650"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1c88c94f36629761206f33c6c15fedf999abb119e7cec586c01f1d2fe188ae70"
-    sha256 cellar: :any_skip_relocation, catalina:       "d163cd9ca3fe156de8cdc2b77a36e357cdff7317a145115157b545f0703520c4"
-    sha256 cellar: :any_skip_relocation, mojave:         "4d16aa1912e1c56a359242288d82c5bf0246c7e9cb0d507d1c267b35f176eee4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1c1e691a28af9d74ef1453b1b4b2e782cd5c8271ea1a991694daf3fab7c4dc3a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/vpn-slice"
+    sha256 cellar: :any_skip_relocation, mojave: "8a70757add36bae76c178f885634ce91180ae2b60ee7848ce794f77259e387ea"
   end
 
+  depends_on "rust" => :build # for cryptography, can remove if dnspython has new release
   depends_on "python@3.10"
 
   resource "dnspython" do
-    url "https://files.pythonhosted.org/packages/67/d0/639a9b5273103a18c5c68a7a9fc02b01cffa3403e72d553acec444f85d5b/dnspython-2.0.0.zip"
-    sha256 "044af09374469c3a39eeea1a146e8cac27daec951f1f1f157b1962fc7cb9d1b7"
+    url "https://files.pythonhosted.org/packages/13/27/5277de856f605f3429d752a39af3588e29d10181a3aa2e2ee471d817485a/dnspython-2.1.0.zip"
+    sha256 "e4a87f0b573201a0f3727fa18a516b055fd1107e0e5477cded4a2de497df1dd4"
   end
 
   resource "setproctitle" do
-    url "https://files.pythonhosted.org/packages/5a/0d/dc0d2234aacba6cf1a729964383e3452c52096dc695581248b548786f2b3/setproctitle-1.1.10.tar.gz"
-    sha256 "6283b7a58477dd8478fbb9e76defb37968ee4ba47b05ec1c053cb39638bd7398"
+    url "https://files.pythonhosted.org/packages/a1/7f/a1d4f4c7b66f0fc02f35dc5c85f45a8b4e4a7988357a29e61c14e725ef86/setproctitle-1.2.2.tar.gz"
+    sha256 "7dfb472c8852403d34007e01d6e3c68c57eb66433fb8a5c77b13b89a160d97df"
   end
 
   def install
