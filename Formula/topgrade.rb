73835d771ead6e067e3f54af15fff85d4cc00175
Adrian Ho <the.gromgit@gmail.com>
2021-12-22 03:42:15 +0800

    topgrade: update 8.1.1 bottle.

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -1,14 +1,13 @@
 class Topgrade < Formula
   desc "Upgrade all the things"
   homepage "https://github.com/r-darwish/topgrade"
-  url "https://github.com/r-darwish/topgrade/archive/v8.0.3.tar.gz"
-  sha256 "c60dd5ae7d1d3bcfe941ead9f088c4b0413b9a4561fb9154429faf86a43e0983"
+  url "https://github.com/r-darwish/topgrade/archive/v8.1.1.tar.gz"
+  sha256 "fc52b0f5a6eb2f1e797ca3aa3b7d1b6f23847c31d783ac1a13ab916894c0ff39"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/topgrade"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "3c4e0163a31cdbed69a1981204054c4f885f810e3ab5a58a9b6bbc89ae8f9de6"
+    sha256 cellar: :any_skip_relocation, mojave: "df615b34bf9494ac19cab05e7da54eceb3541aea7de4396056f90e62d16bbc33"
   end
 
   depends_on "rust" => :build
