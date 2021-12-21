a9b1ae996aea2c67a17a9267dcb48fcc883e5315
Adrian Ho <the.gromgit@gmail.com>
2021-12-22 07:26:15 +0800

    wrk: update 4.2.0 bottle.

--- Formula/wrk.rb
+++ Formula/wrk.rb
@@ -1,21 +1,18 @@
 class Wrk < Formula
   desc "HTTP benchmarking tool"
   homepage "https://github.com/wg/wrk"
-  url "https://github.com/wg/wrk/archive/4.1.0.tar.gz"
-  sha256 "6fa1020494de8c337913fd139d7aa1acb9a020de6f7eb9190753aa4b1e74271e"
+  url "https://github.com/wg/wrk/archive/4.2.0.tar.gz"
+  sha256 "e255f696bff6e329f5d19091da6b06164b8d59d62cb9e673625bdcd27fe7bdad"
   head "https://github.com/wg/wrk.git", branch: "master"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, big_sur:      "39f3b454a93381e9350a3650745039deec765bff1f72eb7b6f3d449f1cabed3a"
-    sha256 cellar: :any_skip_relocation, catalina:     "9efb9a6a4f0dcd7298e048b750b1c6b3606d15d59ce7ce859663ef65925ee69f"
-    sha256 cellar: :any_skip_relocation, mojave:       "65f16f71cfb120d12f0f993044db92b767c728274aa046917bee7ad4deacfd3e"
-    sha256 cellar: :any_skip_relocation, high_sierra:  "ccc1cc4303847b70f940b765a9229476e7018328fbf202c02a00d9a13c63919e"
-    sha256 cellar: :any_skip_relocation, sierra:       "82fd1323ac999e23107dcd1cdae9635ff047afd15532f42f8cb4e82dad0db257"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "12af637f910bcdb74321e372ebf0d2b11206cc1dde29938cc48cb3eb3b4a03b1"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/wrk"
+    sha256 cellar: :any_skip_relocation, mojave: "dbcc4657c85ec23393ab96bda2e99b3c222e8dfa812bedb62d101595bfd8b7ad"
   end
 
   depends_on "openssl@1.1"
+
+  uses_from_macos "unzip" => :build
 
   on_linux do
     depends_on "makedepend" => :build
