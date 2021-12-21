afa8d06daef6d41b21b72c0773750a1b1e93c706
Adrian Ho <the.gromgit@gmail.com>
2021-12-22 01:18:12 +0800

    statix: update 0.4.2 bottle.

---
+++ Formula/statix.rb
@@ -0,0 +1,31 @@
+class Statix < Formula
+  desc "Lints and suggestions for the nix programming language"
+  homepage "https://github.com/nerdypepper/statix"
+  url "https://github.com/nerdypepper/statix/archive/v0.4.2.tar.gz"
+  sha256 "a5285a28739a4281f9727a6aa71d3d1d300f802caa2da5a3b1089badf00832b3"
+  license "MIT"
+  head "https://github.com/nerdypepper/statix.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/statix"
+    sha256 cellar: :any_skip_relocation, mojave: "3cad2bfd04066bd509a1d4c2036e286787ae226740a9467aada71234a3adebac"
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "bin")
+  end
+
+  test do
+    (testpath/"test.nix").write <<~EOS
+      github:nerdypepper/statix
+    EOS
+    assert_match "Found unquoted URI expression", shell_output("#{bin}/statix check test.nix")
+
+    system bin/"statix", "fix", "test.nix"
+    system bin/"statix", "check", "test.nix"
+
+    assert_match version.to_s, shell_output("#{bin}/statix --version")
+  end
+end
