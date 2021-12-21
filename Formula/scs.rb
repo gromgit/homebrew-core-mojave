4d8b5531b290ec2988de149359e43a70f965daee
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 23:08:44 +0800

    scs: update 3.0.0 bottle.

--- Formula/scs.rb
+++ Formula/scs.rb
@@ -1,22 +1,22 @@
 class Scs < Formula
   desc "Conic optimization via operator splitting"
   homepage "https://web.stanford.edu/~boyd/papers/scs.html"
-  url "https://github.com/cvxgrp/scs/archive/v2.1.4.tar.gz"
-  sha256 "5583c70e97e4897bcac32ec487225c3e49161a7e2c8135d1d1a07e4be0375057"
+  url "https://github.com/cvxgrp/scs/archive/v3.0.0.tar.gz"
+  sha256 "95ab61495db72b18d6bb690cd2ae2ce88134d078c473da6cb8750857ea17f732"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "314ba6c1d1f05470bdb444d392083c2989cd6391bc525f53d719e5b5f7cd17d8"
-    sha256 cellar: :any, arm64_big_sur:  "fdbe71dc5aff701be00e9629c5ee27cdf0d5942aebea202da39a78217569097e"
-    sha256 cellar: :any, monterey:       "6555694ae2fdca5f72009c124eb7f801ed45bd9b9fdc05945fe9a92bcfaa3308"
-    sha256 cellar: :any, big_sur:        "1d41b23aceb11793eb5fed5caa703818f62636ae23e70134ae064a4d829f59d0"
-    sha256 cellar: :any, catalina:       "37e188ea7df7a55ee087a32743bfce52cdb0acd91ddcffff629659f3c6a326bc"
-    sha256 cellar: :any, mojave:         "99764bf3362d0dd01f78337f1f943e77b48d68ffc8ef18fffa811516f9763e4f"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/scs"
+    sha256 cellar: :any, mojave: "a5cfa7dc6e70dde6d8d927d1cde66c1f33de7b64462083f675d83a390ab2f32a"
+  end
+
+  on_linux do
+    depends_on "openblas"
   end
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
-    pkgshare.install "test/data/small_random_socp"
+    pkgshare.install "test/problems/random_prob"
   end
 
   test do
@@ -25,13 +25,15 @@ class Scs < Formula
       #include <scs.h>
       #include <util.h>
       int main() {
-        ScsData *data; ScsCone *cone;
-        const int status = scs_read_data("#{pkgshare}/small_random_socp",
-                                         &data, &cone);
-        ScsSolution *solution = scs_calloc(1, sizeof(ScsSolution));
+        ScsData *d; ScsCone *k; ScsSettings *stgs;
+        ScsSolution *sol = scs_calloc(1, sizeof(ScsSolution));
         ScsInfo info;
-        const int result = scs(data, cone, solution, &info);
-        scs_free_data(data, cone); scs_free_sol(solution);
+        scs_int result;
+
+        scs_read_data("#{pkgshare}/random_prob", &d, &k, &stgs);
+        result = scs(d, k, stgs, sol, &info);
+
+        scs_free_data(d, k, stgs); scs_free_sol(sol);
         return result - SCS_SOLVED;
       }
     EOS
