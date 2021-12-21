a81f98d0eb8667c3dd6eaa8cda1e651a02914c3c
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 15:36:20 +0800

    notcurses: update 3.0.1 bottle.

--- Formula/notcurses.rb
+++ Formula/notcurses.rb
@@ -1,13 +1,13 @@
 class Notcurses < Formula
   desc "Blingful character graphics/TUI library"
   homepage "https://nick-black.com/dankwiki/index.php/Notcurses"
-  url "https://github.com/dankamongmen/notcurses/archive/refs/tags/v3.0.0.tar.gz"
-  sha256 "b4839108ca8f9aef31d2f537485cbd878356bbcd4ad4f7a4dd19e72201d01cee"
+  url "https://github.com/dankamongmen/notcurses/archive/v3.0.1.tar.gz"
+  sha256 "32041c300e92fc0fe56c19e65d1d1e374e824c781dfcd4f959ab0dcdbb90cdb2"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/notcurses"
-    sha256 mojave: "4a94aa67abbb6af851b5b410ca86c9c5aff083387602828544d5dde71b2c7212"
+    sha256 mojave: "194b35f0f4bdd95e6e405f54b009e0782592c7b20b97d964f5a2071105fb35a3"
   end
 
   depends_on "cmake" => :build
@@ -20,11 +20,6 @@ class Notcurses < Formula
   depends_on "ncurses"
 
   fails_with gcc: "5"
-
-  patch do
-    url "https://nick-black.com/strndup-mingw-only.patch"
-    sha256 "8ea4a3be2181e1091e44868646830ea37f2efcfcde984a57e5d8dd48d6bb43e0"
-  end
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DCMAKE_INSTALL_RPATH=#{rpath}"
