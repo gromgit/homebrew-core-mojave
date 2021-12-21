d0cd0eecb01520474ff1eb909fe42b04575df2a0
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 15:44:28 +0800

    nsd: update 4.3.9 bottle.

--- Formula/nsd.rb
+++ Formula/nsd.rb
@@ -1,8 +1,8 @@
 class Nsd < Formula
   desc "Name server daemon"
   homepage "https://www.nlnetlabs.nl/projects/nsd/"
-  url "https://www.nlnetlabs.nl/downloads/nsd/nsd-4.3.8.tar.gz"
-  sha256 "11897e25f72f5a98f9202bd5378c936886d54376051a614d3688e451e9cb99e1"
+  url "https://www.nlnetlabs.nl/downloads/nsd/nsd-4.3.9.tar.gz"
+  sha256 "531549f09289ecbd05829e14bb34563294d85b9edde2c613a00f597af2135e8d"
   license "BSD-3-Clause"
 
   # We check the GitHub repo tags instead of
@@ -14,13 +14,8 @@ class Nsd < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "e18a0e3540af4de478b1c9178bf9c779943a3872315ed940ab06eb8e82fbe48c"
-    sha256 arm64_big_sur:  "40eccbf26b9b22bf1ae9c12aa29cb6d64025d4f8b0a33d07f80a10342e3344ad"
-    sha256 monterey:       "b7e6759930a565a9d66d0b6ffaa7adb9128249cfc494d29b7a8415c38f8fea78"
-    sha256 big_sur:        "df218d39f9ad39cc5f02262a67dd0df660d4bfd7cc3647ebe9688f33de1d5431"
-    sha256 catalina:       "5f36529e2b232c82c6f26ce53a68bc3b5f4256061ba6788cc318d6b888d49a4f"
-    sha256 mojave:         "bbf84514d5ffb755af3fb96fecedbc69982ddc61874da7acab829f7ad44e3fca"
-    sha256 x86_64_linux:   "3e856341d7f6fe3377af54a78870cdbe1dce23ee672a6d3e05d49c61e32dabcc"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nsd"
+    sha256 mojave: "4f114bc62ff545abfea8e63ca50175582c330164a83174d42ebe4113031256a4"
   end
 
   depends_on "libevent"
