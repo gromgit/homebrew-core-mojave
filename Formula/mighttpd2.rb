2d4abb6c9a335bdf5c6f78aecb5517efaaaf8a1b
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 11:07:39 +0800

    mighttpd2: update 4.0.2 bottle.

--- Formula/mighttpd2.rb
+++ Formula/mighttpd2.rb
@@ -1,14 +1,13 @@
 class Mighttpd2 < Formula
   desc "HTTP server"
   homepage "https://kazu-yamamoto.github.io/mighttpd2/"
-  url "https://hackage.haskell.org/package/mighttpd2-4.0.0/mighttpd2-4.0.0.tar.gz"
-  sha256 "5afc8acb4e268401dc19964b710230e5013399b8ad3baa7ae6d5e5802ad4ac42"
+  url "https://hackage.haskell.org/package/mighttpd2-4.0.2/mighttpd2-4.0.2.tar.gz"
+  sha256 "1d4dc43b96a3064a8c0b752f71591cb04d769b76e3b922a5ea3529057d530960"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mighttpd2"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "e31568b204ab29cac15728c5219b592079ca9b10555a63eb3eb618272e7a1b41"
+    sha256 cellar: :any_skip_relocation, mojave: "db9a6f935edf39e80af041286f929a0c77df98d392900fbea5ea42f08d91040f"
   end
 
   depends_on "cabal-install" => :build
