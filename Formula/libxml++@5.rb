621b0a9978aebe80cfd8a734d0d3b91bb36651b6
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 08:12:38 +0800

    libxml++@5: update 5.0.1 bottle.

--- Formula/libxml++@5.rb
+++ Formula/libxml++@5.rb
@@ -11,19 +11,15 @@ class LibxmlxxAT5 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "a98d69a34eb6d53430564fec383c63ec75b3e2b38fe8b2267f0d74b436d19136"
-    sha256 cellar: :any, arm64_big_sur:  "a29a9c7c8a7982e2b2e568c9cf5fa5fd5be259473d3b8cae2690482e05652650"
-    sha256 cellar: :any, monterey:       "9c875817f6ead741cc583af95ce3084a2b8fe375ae5f51eca94896d9bac5712e"
-    sha256 cellar: :any, big_sur:        "69758d9c1040b13e69b2c93b9acff4de625114289369ad31ab32343a60e724cb"
-    sha256 cellar: :any, catalina:       "c27918653b7a652ed111f61359da43fd054b68b0399cd2259a8f099c741ae149"
-    sha256 cellar: :any, mojave:         "92dbdf72a137616d5a04fc4d6f92911db4463146ebc0fd4ff88350d11e9ca4fa"
-    sha256               x86_64_linux:   "6c0512d0b89e78c7b934cebe4250e51a4b59199a5ad1a409beece4ef9084df02"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libxml++@5"
+    rebuild 1
+    sha256 cellar: :any, mojave: "7fb10f42b538ccfd44a428403ec432c98902ff1963dacdf35b390d7f56a9c6ad"
   end
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => [:build, :test]
-  depends_on "python@3.9" => :build
+  depends_on "python@3.10" => :build
 
   uses_from_macos "libxml2"
 
