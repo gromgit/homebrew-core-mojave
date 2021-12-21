bba95aa97bea1e8b2afdf7a7da41db6d73dc7188
Adrian Ho <the.gromgit@gmail.com>
2021-12-22 07:19:19 +0800

    weechat: update 3.3_1 bottle.

--- Formula/weechat.rb
+++ Formula/weechat.rb
@@ -4,16 +4,12 @@ class Weechat < Formula
   url "https://weechat.org/files/src/weechat-3.3.tar.xz"
   sha256 "cafeab8af8be4582ccfd3e74fd40e5086a1efa158231f2c26b8b05c3950fcbdf"
   license "GPL-3.0-or-later"
+  revision 1
   head "https://github.com/weechat/weechat.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "4a1e008cf76feb26be4037753389ddd5259768e40478dce3e43089cb576ff12e"
-    sha256 arm64_big_sur:  "e4f9d439f3a83efdf7c0f88fcc7d638c1b4b8a1bff55e6c6215c174ac3cddf16"
-    sha256 monterey:       "c8c53f4df53f45f666515d0e012b54c05625a8fd9e155bcf964131bd96298c12"
-    sha256 big_sur:        "f176eda50d9ef8ab5858b35a1a572c0bd50fd531f912f33210afdebb3e200677"
-    sha256 catalina:       "ccfa0fff385966e6ff08de5e280f49defac48eafc0d649c3ebe983e0475631ff"
-    sha256 mojave:         "e68941b11b7b86d8603c809420ac6fba6761ad095bcf5c6087d0f560dd542114"
-    sha256 x86_64_linux:   "d7d367abe8fe2a22ac47b929332176d22215fa230f886d52be5f5f78e1c4c940"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/weechat"
+    sha256 mojave: "67ecf562c41db44baba8f42b6e7f7539c7d41f928dac4e236bb4c5630378376c"
   end
 
   depends_on "asciidoctor" => :build
@@ -26,7 +22,7 @@ class Weechat < Formula
   depends_on "lua"
   depends_on "ncurses"
   depends_on "perl"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
   depends_on "ruby"
 
   uses_from_macos "curl"
