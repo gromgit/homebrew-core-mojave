854328eecc9b8a7df16afd9e77cf44cc6bd3e131
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 21:07:00 +0800

    px: update 1.5.6 bottle.

--- Formula/px.rb
+++ Formula/px.rb
@@ -4,21 +4,16 @@ class Px < Formula
   desc "Ps and top for human beings (px / ptop)"
   homepage "https://github.com/walles/px"
   url "https://github.com/walles/px.git",
-      tag:      "1.5.5",
-      revision: "489d1a9b53d184cd276a7f8ac4b2bb9eebbfdd59"
+      tag:      "1.5.6",
+      revision: "015793f9ef7b228812a8a887a51c0b2b0604fd4e"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "54a3db6c915b12e1fbe6ee8f9860f3c9d34907d804d28a63d213f8dee27c3a13"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bb4a05f36cd35ee70beb27a8c48affd9a31faa395a2513e8b86814ea47548265"
-    sha256 cellar: :any_skip_relocation, monterey:       "dbfb1f64c9fa03dfff9d3f3e5791943c753c76b84210854649351c8314b8e8ac"
-    sha256 cellar: :any_skip_relocation, big_sur:        "528c083770ae2896f96d85fe83950805fb09dcde74ce3f4cbe71b276630719d8"
-    sha256 cellar: :any_skip_relocation, catalina:       "a2611969d42994ea2104414b6b94369e4e5e6410264b58bfaf957f8721182cd2"
-    sha256 cellar: :any_skip_relocation, mojave:         "a88a5da3ae0d63fa13b8b231d2a7ce1094a87d5ab36615daade6b2088311867c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "47031e425bdd1bd0ad16d43427a40e3b29e1769248df9b0b1450ef422c234c31"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/px"
+    sha256 cellar: :any_skip_relocation, mojave: "358f3121db83283c84d9ebe760433e8992e7051a61bb9545a756444028db57e4"
   end
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
   depends_on "six"
 
   uses_from_macos "lsof"
