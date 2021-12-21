7112651f766b220973b5a727c04f7fe24fa11752
Adrian Ho <the.gromgit@gmail.com>
2021-12-22 06:21:09 +0800

    vroom: update 1.11.0 bottle.

--- Formula/vroom.rb
+++ Formula/vroom.rb
@@ -1,17 +1,13 @@
 class Vroom < Formula
   desc "Vehicle Routing Open-Source Optimization Machine"
   homepage "http://vroom-project.org/"
-  url "https://github.com/VROOM-Project/vroom/archive/v1.10.0.tar.gz"
-  sha256 "6426c32133b9ef7a41f88cf4b506848c34feca74fd3fc9b5bf2bcd96a2f436f1"
+  url "https://github.com/VROOM-Project/vroom/archive/v1.11.0.tar.gz"
+  sha256 "ca8c70a0ad3629640bb6c9b5fc5fc732fad36cb8572d0c58ff7e780be15aa544"
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "b752eee30dd3872d44a702f903ac8a4bc0d37a1187a4f54f038f86508409912d"
-    sha256 cellar: :any, arm64_big_sur:  "76c2218be30b352eb31c178ab9c25930b0fd80bc1d603defbf1d2ebf92e1875e"
-    sha256 cellar: :any, monterey:       "7ff7b7dfed08fae712ca628f7c2948a5006b9371c199a750ddfeda6109757fd6"
-    sha256 cellar: :any, big_sur:        "59ca347b0dd9ba423ca9006997a8051c0d9e5c98c7d3876302575aa77073dff7"
-    sha256 cellar: :any, catalina:       "637d2de102155f8cf96c3ff168c0503cb3cc0ae8abddcc33ed3fe869213613d0"
-    sha256 cellar: :any, mojave:         "c283fdbd2e5f26ed93fbf56e8aa8501381fdfa3918f4271cd87301a4e890d801"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/vroom"
+    sha256 cellar: :any, mojave: "53367bf4597127109345db4059189e0974749e6877d8b61098afda1a31cb61f6"
   end
 
   depends_on "pkg-config" => :build
