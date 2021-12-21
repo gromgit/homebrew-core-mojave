396815fd59bb6a00e980c3882166940e4df92608
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 08:10:04 +0800

    libvoikko: update 4.3.1 bottle.

--- Formula/libvoikko.rb
+++ Formula/libvoikko.rb
@@ -11,18 +11,14 @@ class Libvoikko < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "33d231fd79f10e19502f747b9d16bc40882407540b131041babbedee62f3b7aa"
-    sha256 cellar: :any,                 arm64_big_sur:  "b0a624c9b02009d2eac3b6adbf6db56c05d6cde859ea5cef7b6a06973afd8619"
-    sha256 cellar: :any,                 monterey:       "156a56c277dc6f56fb456f91258b3c75f82c852b08e54bda30a8d28791b53b31"
-    sha256 cellar: :any,                 big_sur:        "02041f6b02bbdf49d1399b6c8b0f99e00a003a9f03bf13b57fe449759f98e27e"
-    sha256 cellar: :any,                 catalina:       "ffc0a9565f9806e59b80b69523230d550a9c3cdfacf6d892a13a7c3b11ac428e"
-    sha256 cellar: :any,                 mojave:         "79b5cb80a3e95beb1d57485d549724334d687dfdbc60520d3b437f5646ae756d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee79fddd0811eb6bd7622a0a9f7ab8319e5306c91bcd3b54eee92e92734f2a98"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libvoikko"
+    rebuild 1
+    sha256 cellar: :any, mojave: "72a66585fd710df9b980793e88068e183026f72fbed8fa308c4b5a9c37330142"
   end
 
   depends_on "foma" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@3.9" => :build
+  depends_on "python@3.10" => :build
   depends_on "hfstospell"
 
   resource "voikko-fi" do
