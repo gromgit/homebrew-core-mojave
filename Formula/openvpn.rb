c707e665a20986e3493582540bd782951cdb015d
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 17:31:35 +0800

    openvpn: update 2.5.5 bottle.

--- Formula/openvpn.rb
+++ Formula/openvpn.rb
@@ -1,9 +1,9 @@
 class Openvpn < Formula
   desc "SSL/TLS VPN implementing OSI layer 2 or 3 secure network extension"
   homepage "https://openvpn.net/community/"
-  url "https://swupdate.openvpn.org/community/releases/openvpn-2.5.4.tar.xz"
-  mirror "https://build.openvpn.net/downloads/releases/openvpn-2.5.4.tar.xz"
-  sha256 "56c0dcd27ab938c4ad07469c86eb8b7408ef64c3e68f98497db8c03f11792436"
+  url "https://swupdate.openvpn.org/community/releases/openvpn-2.5.5.tar.xz"
+  mirror "https://build.openvpn.net/downloads/releases/openvpn-2.5.5.tar.xz"
+  sha256 "119bd69fa0210838f6cdaa273696dc738efa200f454dbe11eb6dfb75dfb6003b"
   license "GPL-2.0-only" => { with: "openvpn-openssl-exception" }
 
   livecheck do
@@ -12,13 +12,8 @@ class Openvpn < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "02af3b44c4340fcbcfdcc2cbb8800f5ec0701fc1dc1cbf8a4ec1df8cf539933d"
-    sha256 arm64_big_sur:  "6666fe4dc8bfa42db9bb92d52962606daacfa284be240e183de86f568ac2af43"
-    sha256 monterey:       "632edbc545de24e3a4ae0b3bbf4872d38ff5c0e8cefce4c6db71c35ac6435e56"
-    sha256 big_sur:        "682a9cd67a9ca4d1f3e98b2278bfb30cba39e30f532cdebbc25258fe7e4e69af"
-    sha256 catalina:       "904507f9c962a7294f67a92e48d0c8cfc12a71bcf91c6d5d924e0f28a4836a3a"
-    sha256 mojave:         "fec0ed2726d148cc96ac6a19a9dfa0e39703fba82c717020f5c6ccc1ee4deef9"
-    sha256 x86_64_linux:   "a8d95b95acf6cdfed376475c0e5265bc97bac433dbb51c7279f0bb76a39db6a2"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/openvpn"
+    sha256 mojave: "b566f4b62044a84ef5c95fdd0302d3432c15b352952903a2b93a680954291faf"
   end
 
   depends_on "pkg-config" => :build
