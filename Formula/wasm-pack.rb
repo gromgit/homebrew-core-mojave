0e9e4cf0d84159e08004f614267f9a93fedb8777
Adrian Ho <the.gromgit@gmail.com>
2021-12-22 06:41:36 +0800

    wasm-pack: update 0.10.2 bottle.

--- Formula/wasm-pack.rb
+++ Formula/wasm-pack.rb
@@ -1,17 +1,14 @@
 class WasmPack < Formula
   desc "Your favorite rust -> wasm workflow tool!"
   homepage "https://rustwasm.github.io/wasm-pack/"
-  url "https://github.com/rustwasm/wasm-pack/archive/v0.10.1.tar.gz"
-  sha256 "fe46f54a0fb8372446cc8a6c64049405b9c4ea7bbe5bdee7b1b219c701fa783e"
+  url "https://github.com/rustwasm/wasm-pack/archive/v0.10.2.tar.gz"
+  sha256 "533b7f63c04411e5d771d406b1c56134e3045b48fb1673985ad8fed1bc937517"
   license "Apache-2.0"
   head "https://github.com/rustwasm/wasm-pack.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:     "59a6f6ada84ec37e2329498b155704eb0a9243956de83d1e4636a013e9598f26"
-    sha256 cellar: :any_skip_relocation, big_sur:      "56ee4bb666eb8f2e4ceffeb98ec1fa03d9330cde887c8ed41795a40ef7561c2c"
-    sha256 cellar: :any_skip_relocation, catalina:     "fbf74ad3f901a30d0c8d503f033893c5f511aecd460a72ef765f0fe1b6d10d06"
-    sha256 cellar: :any_skip_relocation, mojave:       "c24a9c03bdae1f6d6d03a73c4d89ab1c7cf2f2a17df1b01a87fb96ae891e23d2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "bb4abbe7b3f01ae148caa662d6ac424a72a8a95e9debff0a9c7311bd98dd4568"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/wasm-pack"
+    sha256 cellar: :any_skip_relocation, mojave: "ece2a68c697db90d8381f922f81b862378d8eb9b33ffdbdbecadba6730795706"
   end
 
   depends_on "rust" => :build
