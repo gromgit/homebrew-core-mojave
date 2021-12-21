1cc36fa1601206500c8b9a301f0a8c532133cfc3
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 12:06:43 +0800

    mm-common: update 1.0.3_1 bottle.

--- Formula/mm-common.rb
+++ Formula/mm-common.rb
@@ -4,19 +4,16 @@ class MmCommon < Formula
   url "https://download.gnome.org/sources/mm-common/1.0/mm-common-1.0.3.tar.xz"
   sha256 "e81596625899aacf1d0bf27ccc2fcc7f373405ec48735ca1c7273c0fbcdc1ef5"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f40a6de3865dbb9c453f82b2eae422bb54ae0e422ee287ad41cea24f4b084937"
-    sha256 cellar: :any_skip_relocation, big_sur:       "f40a6de3865dbb9c453f82b2eae422bb54ae0e422ee287ad41cea24f4b084937"
-    sha256 cellar: :any_skip_relocation, catalina:      "85f048b6c104120336d3d1e727ddef7faca85feaad37bb404f502202437d1a41"
-    sha256 cellar: :any_skip_relocation, mojave:        "85f048b6c104120336d3d1e727ddef7faca85feaad37bb404f502202437d1a41"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "015f6833761b100ad4610a505700d44fc1db448147f68ae502cc8e9383048ef9"
-    sha256 cellar: :any_skip_relocation, all:           "f40a6de3865dbb9c453f82b2eae422bb54ae0e422ee287ad41cea24f4b084937"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mm-common"
+    sha256 cellar: :any_skip_relocation, mojave: "bb5913059780ade4ac219d5725d1463c56934eb6430c32848e98b935113dbca9"
   end
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     mkdir "build" do
