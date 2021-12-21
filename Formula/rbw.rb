21616ba8fbd34f04c7c4f2ac5a7ed50d58ec0140
Adrian Ho <the.gromgit@gmail.com>
2021-12-21 21:54:46 +0800

    rbw: update 1.4.1 bottle.

--- Formula/rbw.rb
+++ Formula/rbw.rb
@@ -1,19 +1,14 @@
 class Rbw < Formula
   desc "Unoffical Bitwarden CLI client"
   homepage "https://github.com/doy/rbw"
-  url "https://github.com/doy/rbw/archive/refs/tags/1.3.0.tar.gz"
-  sha256 "7255e322b8c6bd6ee9da00f4c54211e94a132abec15b9f51b8351af31a4744ac"
+  url "https://github.com/doy/rbw/archive/refs/tags/1.4.1.tar.gz"
+  sha256 "70c55c1341f4181f8974f99ec24ee1caf918487135cfa578566d9e6c44eb47b0"
   license "MIT"
   head "https://github.com/doy/rbw.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c87111e66d9c3588238918493fcff608885ffe3ab7a17d8af7e34f5d44eaca50"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0e0ba9f71889ea47c3a6b12df6723ed378b2a35be281d3badb4e7a87ac81e057"
-    sha256 cellar: :any_skip_relocation, monterey:       "59299d69cdf5cb461d0802eb7725a2d6db58dac534337bb54612a8e7833c59c2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "916a7562c8c63eb6da7745f1698a7a685bc8d56478f98793f9c7a1e05e35f4de"
-    sha256 cellar: :any_skip_relocation, catalina:       "dc4ef8419b27659d6d6b937bf21c2357b3fdea7ca0befb7060c224f8f7f36835"
-    sha256 cellar: :any_skip_relocation, mojave:         "762e2ee5b53c5c64c02d0be5bbc248d84b187e4e641c2aca4d19e7517f71ea2d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fd02ace12c281792045f496308cb037003e3727f2c79d1f984de7a77b882bc66"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rbw"
+    sha256 cellar: :any_skip_relocation, mojave: "68c1d6e9e38aa97a8197c7017a06fe63b76aed2ca02851b4596b7c25c1550f4f"
   end
 
   depends_on "rust" => :build
