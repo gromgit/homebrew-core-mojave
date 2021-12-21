7b13b26883a0b8765d7099f61efe67d20a174304
Adrian Ho <the.gromgit@gmail.com>
2021-12-22 03:23:01 +0800

    thrax: update 1.3.6_1 bottle.

--- Formula/thrax.rb
+++ Formula/thrax.rb
@@ -6,6 +6,7 @@ class Thrax < Formula
   url "https://www.openfst.org/twiki/pub/GRM/ThraxDownload/thrax-1.3.6.tar.gz"
   sha256 "5f00a2047674753cba6783b010ab273366dd3dffc160bdb356f7236059a793ba"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url "https://www.openfst.org/twiki/bin/view/GRM/ThraxDownload"
@@ -13,20 +14,15 @@ class Thrax < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "328d82ce4714605a2e1425733bb6a0e39e954df45f29ee2f060468fcde098542"
-    sha256 cellar: :any,                 arm64_big_sur:  "033c1195f0d64bf2844268850d006e79e410d5796f53b38606fdf6a24ba97d93"
-    sha256 cellar: :any,                 monterey:       "563c12082f7958c0facf2ea7605de585b597b161ace80d51ed7c099f56d8701f"
-    sha256 cellar: :any,                 big_sur:        "7120f61434e7851ae51ffba6ca11e7a20a5df0c90e59ed1f12bd831fab0d7bbc"
-    sha256 cellar: :any,                 catalina:       "fa9e11b34775d0e1e7f12e7b9d15f4c22577033e6eabff8cefdbc3f197c64504"
-    sha256 cellar: :any,                 mojave:         "3ab4c18556c41bec206362d9f564f82f6bc11388848fa7cd00242ce3c66118d2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e05985642a3ae9cc5922a2f8ced5f8dd15169a95a4de45980bb2cb19bfef1152"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/thrax"
+    sha256 cellar: :any, mojave: "5e9001cc07788c0d20ff062e6bfc5804f700ea42938d8f9e6318c5a2e464e4e1"
   end
 
   depends_on "openfst"
 
   on_linux do
     depends_on "gcc"
-    depends_on "python@3.9"
+    depends_on "python@3.10"
   end
 
   fails_with gcc: "5"
