8555ff06f1c7ebec724ac318d2a8d4a1abcceda1
Adrian Ho <the.gromgit@gmail.com>
2021-12-28 23:42:19 +0800

    etcd-cpp-apiv3: update 0.2.3_1 bottle.

---
+++ Formula/etcd-cpp-apiv3.rb
@@ -0,0 +1,108 @@
+class EtcdCppApiv3 < Formula
+  desc "C++ implementation for etcd's v3 client API, i.e., ETCDCTL_API=3"
+  homepage "https://github.com/etcd-cpp-apiv3/etcd-cpp-apiv3"
+  url "https://github.com/etcd-cpp-apiv3/etcd-cpp-apiv3/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "f74607eebffce8391b5355ed7d12d46f6627f2882687cd0f5574c90b275f77fd"
+  license "BSD-3-Clause"
+  revision 1
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/etcd-cpp-apiv3"
+    sha256 mojave: "43842e5efba60586639fb9bf3f3a983c6b12d0e6ecd48ebdb04917e3de2860ba"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "etcd" => :test
+
+  depends_on "boost"
+  depends_on "cpprestsdk"
+  depends_on "grpc"
+  depends_on "openssl@1.1"
+  depends_on "protobuf"
+
+  # grpc requrires high version of gcc and are built with high version of gcc,
+  # thus gcc 5 won't work
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DCMAKE_CXX_STANDARD=17",
+                    "-DCMAKE_CXX_STANDARD_REQUIRED=TRUE",
+                    "-DBUILD_ETCD_TESTS=OFF",
+                    "-DOPENSSL_ROOT_DIR=#{Formula["openssl@1.1"].opt_prefix}",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    port = free_port
+
+    (testpath/"test.cc").write <<~EOS
+      #include <iostream>
+      #include <etcd/Client.hpp>
+
+      int main() {
+        etcd::Client etcd("http://127.0.0.1:#{port}");
+        etcd.set("foo", "bar").wait();
+        auto response = etcd.get("foo").get();
+        std::cout << response.value().as_string() << std::endl;
+      }
+    EOS
+
+    system ENV.cxx, "test.cc", "-std=c++17",
+                    "-I#{Formula["boost"].include}",
+                    "-I#{Formula["cpprestsdk"].include}",
+                    "-I#{Formula["grpc"].include}",
+                    "-I#{Formula["openssl@1.1"].include}",
+                    "-I#{Formula["protobuf"].include}",
+                    "-I#{include}",
+                    "-L#{Formula["boost"].lib}",
+                    "-L#{Formula["cpprestsdk"].lib}",
+                    "-L#{Formula["grpc"].lib}",
+                    "-L#{Formula["openssl@1.1"].lib}",
+                    "-L#{Formula["protobuf"].lib}",
+                    "-L#{lib}",
+                    "-lboost_random-mt",
+                    "-lboost_chrono-mt",
+                    "-lboost_thread-mt",
+                    "-lboost_system-mt",
+                    "-lboost_filesystem-mt",
+                    "-lcpprest",
+                    "-letcd-cpp-api",
+                    "-lgpr", "-lgrpc", "-lgrpc++",
+                    "-lssl", "-lcrypto",
+                    "-lprotobuf",
+                    "-o", "test_etcd_cpp_apiv3"
+
+    # prepare etcd
+    etcd_pid = fork do
+      on_macos do
+        if Hardware::CPU.arm?
+          # etcd isn't officially supported on arm64
+          # https://github.com/etcd-io/etcd/issues/10318
+          # https://github.com/etcd-io/etcd/issues/10677
+          ENV["ETCD_UNSUPPORTED_ARCH"]="arm64"
+        end
+      end
+
+      exec "#{Formula["etcd"].opt_prefix}/bin/etcd",
+        "--force-new-cluster",
+        "--data-dir=#{testpath}",
+        "--listen-client-urls=http://127.0.0.1:#{port}",
+        "--advertise-client-urls=http://127.0.0.1:#{port}"
+    end
+
+    # sleep to let etcd get its wits about it
+    sleep 10
+
+    assert_equal("bar\n", shell_output("./test_etcd_cpp_apiv3"))
+  ensure
+    # clean up the etcd process before we leave
+    Process.kill("HUP", etcd_pid)
+  end
+end
