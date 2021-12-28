478b2bff31d8364382c561fd1b7605787ae31584
Adrian Ho <the.gromgit@gmail.com>
2021-12-29 02:43:07 +0800

    seaweedfs: update 2.82 bottle.

---
+++ Formula/seaweedfs.rb
@@ -0,0 +1,48 @@
+class Seaweedfs < Formula
+  desc "Fast distributed storage system"
+  homepage "https://github.com/chrislusf/seaweedfs"
+  url "https://github.com/chrislusf/seaweedfs.git",
+      tag:      "2.82",
+      revision: "c3b73ec23b9c7831e31503ebf3a64cc2f0a3c33d"
+  license "Apache-2.0"
+  head "https://github.com/chrislusf/seaweedfs.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/seaweedfs"
+    sha256 cellar: :any_skip_relocation, mojave: "1345a0fc2e1e241f330519741fd8088a37ddb2f9166919f88cc66c979d8fd404"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/chrislusf/seaweedfs/weed/util.COMMIT=#{Utils.git_head}
+    ]
+    system "go", "build", *std_go_args(output: bin/"weed", ldflags: ldflags), "./weed"
+  end
+
+  test do
+    # Start SeaweedFS master server/volume server
+    master_port = free_port
+    volume_port = free_port
+    master_grpc_port = free_port
+    volume_grpc_port = free_port
+
+    fork do
+      exec bin/"weed", "server", "-dir=#{testpath}",
+           "-master.port=#{master_port}", "-volume.port=#{volume_port}",
+           "-master.port.grpc=#{master_grpc_port}", "-volume.port.grpc=#{volume_grpc_port}"
+    end
+    sleep 30
+
+    # Upload a test file
+    fid = JSON.parse(shell_output("curl http://localhost:#{master_port}/dir/assign"))["fid"]
+    system "curl", "-F", "file=@#{test_fixtures("test.png")}", "http://localhost:#{volume_port}/#{fid}"
+
+    # Download and validate uploaded test file against the original
+    expected_sum = Digest::SHA256.hexdigest(File.read(test_fixtures("test.png")))
+    actual_sum = Digest::SHA256.hexdigest(shell_output("curl http://localhost:#{volume_port}/#{fid}"))
+    assert_equal expected_sum, actual_sum
+  end
+end
