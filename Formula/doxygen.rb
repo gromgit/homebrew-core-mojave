44b5f19ed8b10ecda3b04feb33151df674a578a6
Adrian Ho <the.gromgit@gmail.com>
2022-01-07 19:30:15 +0800

    doxygen: update 1.9.3_1 bottle.

--- Formula/doxygen.rb
+++ Formula/doxygen.rb
@@ -1,11 +1,12 @@
 class Doxygen < Formula
   desc "Generate documentation for several programming languages"
   homepage "https://www.doxygen.org/"
-  url "https://doxygen.nl/files/doxygen-1.9.2.src.tar.gz"
-  mirror "https://downloads.sourceforge.net/project/doxygen/rel-1.9.2/doxygen-1.9.2.src.tar.gz"
-  sha256 "060f254bcef48673cc7ccf542736b7455b67c110b30fdaa33512a5b09bbecee5"
+  url "https://doxygen.nl/files/doxygen-1.9.3.src.tar.gz"
+  mirror "https://downloads.sourceforge.net/project/doxygen/rel-1.9.3/doxygen-1.9.3.src.tar.gz"
+  sha256 "f352dbc3221af7012b7b00935f2dfdc9fb67a97d43287d2f6c81c50449d254e0"
   license "GPL-2.0-only"
-  head "https://github.com/doxygen/doxygen.git"
+  revision 1
+  head "https://github.com/doxygen/doxygen.git", branch: "master"
 
   livecheck do
     url "https://www.doxygen.nl/download.html"
@@ -13,13 +14,8 @@ class Doxygen < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e499b325b991b73028e5507e970d1cc32762b402c7eccf6f8c9c941ffdfe99d7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1a7ba50b992a11544f4a94ab93374eddeaef6aea5cfb2dfefb0c27a2976ef644"
-    sha256 cellar: :any_skip_relocation, monterey:       "336e7503dfa1a4448842cb02f829062b93972021179575db73f47867fea579dd"
-    sha256 cellar: :any_skip_relocation, big_sur:        "a3c10247d05fe6a007ad97b1131e522eec0729288bee680dfd3e5a4cca2ee5fb"
-    sha256 cellar: :any_skip_relocation, catalina:       "d4651ac184617629b57a0842ecb267adb25c34fc0b61b08296d80ee68928b66d"
-    sha256 cellar: :any_skip_relocation, mojave:         "cab7c99f874c1a498ce9b27ebd863a46dd9940b75a86da8782eef952d49e709a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "12a835bf9986bbbebd0f4441a9e64ffd15413fbcfb8cb199640f29721665d022"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/doxygen"
+    sha256 cellar: :any_skip_relocation, mojave: "25ed31961fab3e8a65464a7016b965890d443acaeb07fe86b1d1f1343eb96b78"
   end
 
   depends_on "bison" => :build
