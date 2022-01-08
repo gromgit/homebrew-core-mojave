07223074b03ad9e52e1c1878e166649121aac24a
Adrian Ho <the.gromgit@gmail.com>
2022-01-09 04:59:54 +0800

    alda: update 2.1.0 bottle.

--- Formula/alda.rb
+++ Formula/alda.rb
@@ -1,13 +1,13 @@
 class Alda < Formula
   desc "Music programming language for musicians"
   homepage "https://alda.io"
-  url "https://github.com/alda-lang/alda/archive/refs/tags/release-2.0.7.tar.gz"
-  sha256 "38c4ef58ab1f7173733fa50b6c5dc6e9f1260c25678cce93fd5b3908e0aab8d6"
+  url "https://github.com/alda-lang/alda/archive/refs/tags/release-2.1.0.tar.gz"
+  sha256 "fbf3a297eb79adf3301a5cf8e8aaa2cd8deaa841390ddc819491ecf67cc9062b"
   license "EPL-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/alda"
-    sha256 cellar: :any_skip_relocation, mojave: "239777a7d409537939ccd0ba428896a6ec42c25aab1352f714aa7c73227aa308"
+    sha256 cellar: :any_skip_relocation, mojave: "6b83e11f06d924ec1004f8d0285d5035bb05517cfba1da1d123c2c821e6108ae"
   end
 
   depends_on "go" => :build
