da17e65ef331a617846785242f6e5aa86730add8
Adrian Ho <the.gromgit@gmail.com>
2022-01-09 02:04:59 +0800

    libgit2-glib: update 1.0.0.1 bottle.

--- Formula/libgit2-glib.rb
+++ Formula/libgit2-glib.rb
@@ -1,11 +1,10 @@
 class Libgit2Glib < Formula
   desc "Glib wrapper library around libgit2 git access library"
   homepage "https://github.com/GNOME/libgit2-glib"
-  url "https://download.gnome.org/sources/libgit2-glib/0.99/libgit2-glib-0.99.0.1.tar.xz"
-  sha256 "e05a75c444d9c8d5991afc4a5a64cd97d731ce21aeb7c1c651ade1a3b465b9de"
-  license "LGPL-2.1"
-  revision 4
-  head "https://github.com/GNOME/libgit2-glib.git"
+  url "https://download.gnome.org/sources/libgit2-glib/1.0/libgit2-glib-1.0.0.1.tar.xz"
+  sha256 "460a5d6936950ca08d2d8518bfc90c12bb187cf6e674de715f7055fc58102b57"
+  license "LGPL-2.1-only"
+  head "https://github.com/GNOME/libgit2-glib.git", branch: "master"
 
   livecheck do
     url :stable
@@ -13,12 +12,8 @@ class Libgit2Glib < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "14f4f4a3b09727ca9993953c5fa7e0b9d0dd991cd9f782cdd42d254e6317b974"
-    sha256 cellar: :any, monterey:      "f0882a1094ecc41ffba0e08d64de5681e7a71a0ad5126789c854d38c755cf58b"
-    sha256 cellar: :any, big_sur:       "88a012ad27e2056c313cad08039c2c2ac534ff6f383baae09363b357fcbf5e7e"
-    sha256 cellar: :any, catalina:      "804db5cc23243985293b05e7ab18c54a52e36cb4f339c5d9bbaaee5ac6ebdd39"
-    sha256 cellar: :any, mojave:        "bf0aacd695af9b723be16caaba4252e87bb1001ff4710872d4b5eceff7efe8f5"
-    sha256               x86_64_linux:  "95b4558001af9d732a9d9ab29bcb0cc096af93f7b495e196db9fb0b16a9f399a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libgit2-glib"
+    sha256 mojave: "0ebdf631b34a787041b448103564a612d085d043997906f4acc45dcb757ed109"
   end
 
   depends_on "gobject-introspection" => :build
