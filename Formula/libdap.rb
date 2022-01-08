d74d1dff4c09f72175728f4882abe3eed8e82f80
Adrian Ho <the.gromgit@gmail.com>
2022-01-09 01:56:17 +0800

    libdap: update 3.20.9 bottle.

--- Formula/libdap.rb
+++ Formula/libdap.rb
@@ -1,9 +1,17 @@
 class Libdap < Formula
   desc "Framework for scientific data networking"
   homepage "https://www.opendap.org/"
-  url "https://www.opendap.org/pub/source/libdap-3.20.8.tar.gz"
-  sha256 "65eb5c8f693cf74d58eece5eaa2e7c3c65f368926b1bffab0cf5b207757b94eb"
   license "LGPL-2.1-or-later"
+
+  stable do
+    url "https://www.opendap.org/pub/source/libdap-3.20.9.tar.gz"
+    sha256 "77fc93159d2c71d6ace5709babd9dec598285186aecfba358cb623be29a8e5b9"
+
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+      sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+    end
+  end
 
   livecheck do
     url "https://www.opendap.org/pub/source/"
@@ -11,13 +19,8 @@ class Libdap < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "3cf1365602c955e21ac619446782b3d273e8d11ed8e3281a59280a18bb53ca58"
-    sha256 arm64_big_sur:  "114b06032e3190ea6af91825f75fc44017bdc474c96b4cd88e4f289ed8f03c9b"
-    sha256 monterey:       "2958548470c16f762393029b2b4bef0f6d1d74d82120ac33cb30a3d95856958a"
-    sha256 big_sur:        "d8e3d1ea27305a3e49c1dc3902f57eec4fd9ae6dfeb102f0dab709b8c8e27e9b"
-    sha256 catalina:       "7f6ab80b93c32c6cb09e30af1634f1064bcb2e3bec08f500ac7c78b86fda68dd"
-    sha256 mojave:         "433eb5d60160d3ffd96b0e7a8ea215b4b555d9a94001ff6c41c40c13f93e0f42"
-    sha256 x86_64_linux:   "1cfc0be83a99c1c2cfcdf04d22b9e8503e99a638f59b3e504b5b0bd5c87927c9"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libdap"
+    sha256 mojave: "db3d09067e53090b69d055cd1ff3c6f106befca8ab75d72450eb7120f4e7dd5b"
   end
 
   head do
