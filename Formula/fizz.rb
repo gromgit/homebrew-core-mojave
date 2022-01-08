0b5fd3634502f918361739fe547ca7a1bb4869ca
Adrian Ho <the.gromgit@gmail.com>
2022-01-09 04:52:37 +0800

    fizz: update 2022.01.03.00 bottle.

--- Formula/fizz.rb
+++ Formula/fizz.rb
@@ -1,14 +1,14 @@
 class Fizz < Formula
   desc "C++14 implementation of the TLS-1.3 standard"
   homepage "https://github.com/facebookincubator/fizz"
-  url "https://github.com/facebookincubator/fizz/releases/download/v2021.12.13.00/fizz-v2021.12.13.00.tar.gz"
-  sha256 "1f14665ea7434b7d0770985a2f64d688c5ddbeeaa85441ae3b38ccc7741d781c"
+  url "https://github.com/facebookincubator/fizz/releases/download/v2022.01.03.00/fizz-v2022.01.03.00.tar.gz"
+  sha256 "fa140140108dc3226470c59508de15e7761787b2e158171f588eec4fc415f4f6"
   license "BSD-2-Clause"
-  head "https://github.com/facebookincubator/fizz.git"
+  head "https://github.com/facebookincubator/fizz.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fizz"
-    sha256 cellar: :any, mojave: "4d5e7d8026af5f2fbcbca02ba64f618fc7d674eae4b086708615bdeb128b5ce0"
+    sha256 cellar: :any, mojave: "c6a89ef8c7289caba05df0a4ba1df9bc41f3094dc6838033b249d5bfb8cf736a"
   end
 
   depends_on "cmake" => :build
