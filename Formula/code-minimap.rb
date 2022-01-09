3315fb9d772833f57a29648e6838991903ae14c4
Adrian Ho <the.gromgit@gmail.com>
2022-01-09 22:47:54 +0800

    code-minimap: update 0.6.4 bottle.

--- Formula/code-minimap.rb
+++ Formula/code-minimap.rb
@@ -1,14 +1,13 @@
 class CodeMinimap < Formula
   desc "High performance code minimap generator"
   homepage "https://github.com/wfxr/code-minimap"
-  url "https://github.com/wfxr/code-minimap/archive/v0.6.2.tar.gz"
-  sha256 "feb3a12f3c2d81168f40c988c15b14b838fccecd97d20856319c7d05db16568a"
+  url "https://github.com/wfxr/code-minimap/archive/v0.6.4.tar.gz"
+  sha256 "4e2f15e4a0f7bd31e33f1c423e3120318e13de1b6800ba673037e38498b3a423"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/code-minimap"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "34b431704480bf24a87fcc37b9995409364df64f99a76792045761de583134c9"
+    sha256 cellar: :any_skip_relocation, mojave: "d739532022eb334c7ac250eadf9bd65a8749883c7a739f8f77671cbb2ee7b19b"
   end
 
   depends_on "rust" => :build
