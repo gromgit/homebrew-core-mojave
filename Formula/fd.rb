d05094815ec08aba66f1c11ea04054a9a1485844
Adrian Ho <the.gromgit@gmail.com>
2022-01-10 05:41:02 +0800

    fd: update 8.3.1 bottle.

--- Formula/fd.rb
+++ Formula/fd.rb
@@ -1,15 +1,14 @@
 class Fd < Formula
   desc "Simple, fast and user-friendly alternative to find"
   homepage "https://github.com/sharkdp/fd"
-  url "https://github.com/sharkdp/fd/archive/v8.3.0.tar.gz"
-  sha256 "3c5a8a03c4f6ade73b92432ed0ba51591db19b0d136073fa3ccfa99d63403d52"
+  url "https://github.com/sharkdp/fd/archive/v8.3.1.tar.gz"
+  sha256 "834a90fbb4e1deee2ca7f3aa84575c9187869d8af00f72e431ecab4776ae1f62"
   license "Apache-2.0"
-  head "https://github.com/sharkdp/fd.git"
+  head "https://github.com/sharkdp/fd.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fd"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "784babca4b12d8c610326a9af36337ea3e91094202011ec9a96fbc7bd9a52dc5"
+    sha256 cellar: :any_skip_relocation, mojave: "44996e2c385890064920bd3c258e228cfd3a7057e4e0d375d1e75afe13133260"
   end
 
   depends_on "rust" => :build
