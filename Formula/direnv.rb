5c6170cab6e5095fd7edf773f51d4913f35e1994
Adrian Ho <the.gromgit@gmail.com>
2022-01-10 02:53:05 +0800

    direnv: update 2.30.3 bottle.

--- Formula/direnv.rb
+++ Formula/direnv.rb
@@ -1,15 +1,14 @@
 class Direnv < Formula
   desc "Load/unload environment variables based on $PWD"
   homepage "https://direnv.net/"
-  url "https://github.com/direnv/direnv/archive/v2.29.0.tar.gz"
-  sha256 "a0ceb76a58a6ca81a8669a9ef2631fbad41d7c1a27cc0ec738c71c6d71f9751f"
+  url "https://github.com/direnv/direnv/archive/v2.30.3.tar.gz"
+  sha256 "7fb5431b98d57fb8c70218b4a0fab4b08f102790e7a92486f588bf3d5751ac3b"
   license "MIT"
-  head "https://github.com/direnv/direnv.git"
+  head "https://github.com/direnv/direnv.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/direnv"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "896cec453a39fcec17d34f64dbe934205df9b6664d3e81a550bee2b2ebc84665"
+    sha256 cellar: :any_skip_relocation, mojave: "7df773073a9a1953aeb756216aa9cc562f6168e7b909c68876eba28ff0af51eb"
   end
 
   depends_on "go" => :build
