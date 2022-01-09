4b92db963f165b667364cb2133cefa70b515d8bb
Adrian Ho <the.gromgit@gmail.com>
2022-01-10 00:23:30 +0800

    librist: update 0.2.6_1 bottle.

--- Formula/librist.rb
+++ Formula/librist.rb
@@ -4,6 +4,7 @@ class Librist < Formula
   url "https://code.videolan.org/rist/librist/-/archive/v0.2.6/librist-v0.2.6.tar.gz"
   sha256 "88b35b86af1ef3d306f33674f2d9511a27d3ff4ec76f20d3a3b3273b79a4521d"
   license "BSD-2-Clause"
+  revision 1
   head "https://code.videolan.org/rist/librist.git", branch: "master"
 
   livecheck do
@@ -12,13 +13,8 @@ class Librist < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "a49ba8240dce8d368b193a8d6388553b1688f09eb698891fe059c52829d941b9"
-    sha256 cellar: :any,                 arm64_big_sur:  "33edba89ab01a727ae17d5c76742a20e61030c0dc5b46c5063a07fd31ec16214"
-    sha256 cellar: :any,                 monterey:       "9b5c174610b58707c25b27c9f9ef5cac947e928cc3a1f7d80789648c5c3d0dc5"
-    sha256 cellar: :any,                 big_sur:        "32ca4949e0b34daff4eac02cef3fc018a08a29b531a16c5199c5549317292b84"
-    sha256 cellar: :any,                 catalina:       "a78e9af5a97225aa3caf5f3a700d819fd7ab011537f742fffc9ffa55ec8be035"
-    sha256 cellar: :any,                 mojave:         "4a4e0782ae28a9832313a46cfc5af148b95c0f1760dca560baefd46bf537e828"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9d5676085eb79786f4165816df783eb0662e59a715432c1d5c5700b01b7ee95b"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/librist"
+    sha256 cellar: :any, mojave: "1c20e229dfab004ceb64ed68ab41ff4acb5eb3d1a8d8014d127477f410c6d45d"
   end
 
   depends_on "meson" => :build
