edd360c47fe8e142929802ce5468078629c98e71
Adrian Ho <the.gromgit@gmail.com>
2022-01-10 02:57:37 +0800

    docker-compose: update 2.2.3 bottle.

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -1,15 +1,14 @@
 class DockerCompose < Formula
   desc "Isolated development environments using Docker"
   homepage "https://docs.docker.com/compose/"
-  url "https://github.com/docker/compose/archive/v2.1.1.tar.gz"
-  sha256 "5c9246c34cafeb51b3289c016cb2cbdd08b3eda87b0f8d4cc02fd7630cfdbd7b"
+  url "https://github.com/docker/compose/archive/v2.2.3.tar.gz"
+  sha256 "22210187e73732edd9fc02f122ea61481806c703af7b73d0a7351f2e8ed7c0b8"
   license "Apache-2.0"
   head "https://github.com/docker/compose.git", branch: "v2"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/docker-compose"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "ad023602c5cadd27795de8d1e7e3779e67b118d6c3de676e02fdd913d7127cfa"
+    sha256 cellar: :any_skip_relocation, mojave: "af736ed84076648e371bd5016c0f511e3cad3670b2eb1e17f3a33d0ecdb69fb8"
   end
 
   depends_on "go" => :build
@@ -33,6 +32,6 @@ class DockerCompose < Formula
 
   test do
     output = shell_output(bin/"docker-compose up 2>&1", 14)
-    assert_match "can't find a suitable configuration file", output
+    assert_match "no configuration file provided", output
   end
 end
