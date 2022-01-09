87bf1c22b1f7cc6695e0d6631e5e003032521e21
Adrian Ho <the.gromgit@gmail.com>
2022-01-09 10:39:34 +0800

    cflow: update 1.7 bottle.

--- Formula/cflow.rb
+++ Formula/cflow.rb
@@ -1,21 +1,14 @@
 class Cflow < Formula
   desc "Generate call graphs from C code"
   homepage "https://www.gnu.org/software/cflow/"
-  url "https://ftp.gnu.org/gnu/cflow/cflow-1.6.tar.bz2"
-  mirror "https://ftpmirror.gnu.org/cflow/cflow-1.6.tar.bz2"
-  sha256 "34487b4116e9b7ecde142b24480ce036887921ed5defb2958068bb069c1fedd7"
-  license "GPL-3.0"
+  url "https://ftp.gnu.org/gnu/cflow/cflow-1.7.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/cflow/cflow-1.7.tar.bz2"
+  sha256 "d01146caf9001e266133417c2a8258a64b5fc16fcb082a14f6528204d0c97086"
+  license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ea9bd9605326d36b0f744b380a235e9f34c2dc50fc5e771e95d3aaf1abfdbf36"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d0b017a7e277a9ba4d337df8318c0e9154c235329a846151d8be03bdef75c58e"
-    sha256 cellar: :any_skip_relocation, monterey:       "32312d230803f862d7fb53cb2a2e32892351a7ff530a94e3a3756494eb81946d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b131f52fdaf1658378dbcc134be8c2d713e2e1d66002df05ef8d1ccfcac132e8"
-    sha256 cellar: :any_skip_relocation, catalina:       "a8b7329845d3c42a17efc7af96025a1a2839ae129afd7126e977d0a245853f45"
-    sha256 cellar: :any_skip_relocation, mojave:         "a96f9cf3cb35851c27ed602f6a05489da2d64e75ab6daccecc3e23156d9fe968"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "50a816924cb91e1c4055923285ea3ceb0d815b4641477906ae5f6abdae337d52"
-    sha256 cellar: :any_skip_relocation, sierra:         "5e88f5310c34255947032f24227cf779aa8a42fe595f585605e814f001f4a151"
-    sha256                               x86_64_linux:   "06ad443c55fb5979f0986088dcf96b22b739d7c12d51d593c063bc70018de5d9"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cflow"
+    sha256 mojave: "0242015585d0f557bc53d30b89efd93957d5db1e925e1e763ede3ec61de724bb"
   end
 
   def install
