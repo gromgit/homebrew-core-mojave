a8c8ec768f9ff3ae8c82ac5d6beff44f74738aad
Adrian Ho <the.gromgit@gmail.com>
2022-01-09 23:02:30 +0800

    cppad: update 20220000.0 bottle.

--- Formula/cppad.rb
+++ Formula/cppad.rb
@@ -2,8 +2,8 @@ class Cppad < Formula
   desc "Differentiation of C++ Algorithms"
   homepage "https://www.coin-or.org/CppAD"
   # Stable versions have numbers of the form 201x0000.y
-  url "https://github.com/coin-or/CppAD/archive/20210000.8.tar.gz"
-  sha256 "465a462329fb62110c4799577178d1f28d8c0083b385b7ea08ac82bb98873844"
+  url "https://github.com/coin-or/CppAD/archive/20220000.0.tar.gz"
+  sha256 "29aad60d4d80aaf773736ed5efaaa660eaaf686d0622782f31b35d1b08d61635"
   license "EPL-2.0"
   version_scheme 1
   head "https://github.com/coin-or/CppAD.git", branch: "master"
@@ -14,13 +14,8 @@ class Cppad < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "fede696ea9899a66579f17c71657bd29ea1b0b93ec32e110da62abebbb2c4c15"
-    sha256 cellar: :any,                 arm64_big_sur:  "f1e290d82781bd8a6eb2d834141f9c02bb7006ae26113bedf3ff518eaf6c93bf"
-    sha256 cellar: :any,                 monterey:       "b52e0d8f8dc2c9c71f4f32ee74efcce38536b7802fd03c4c14c70eff61742d64"
-    sha256 cellar: :any,                 big_sur:        "52998109aba462d55b4dc2dcf3a1d3bde03ed13b0f736985d8725fff163f6b01"
-    sha256 cellar: :any,                 catalina:       "9cedc9b79c879b310618c4f423ab4ec40d5e44b668c95e83c701e0fcc63ecb47"
-    sha256 cellar: :any,                 mojave:         "531ea9476520b4ebaa94a2cdc73d5e668d47ea042d6130f745f9dc8323d67c52"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a265722ea9479807714b7cc6aa415e2a5562ae8b61ff7e149f3f9441359f9b7a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cppad"
+    sha256 cellar: :any, mojave: "0b6311110ea2c15742a45ef89bb815a23ab223b0dddbe03ea392f7738cfe473b"
   end
 
   depends_on "cmake" => :build
