1986d13b5b065e8bbc63debca5c058b40f33d801
Adrian Ho <the.gromgit@gmail.com>
2022-01-11 07:29:56 +0800

    luau: update 0.509 bottle.

--- Formula/luau.rb
+++ Formula/luau.rb
@@ -1,14 +1,14 @@
 class Luau < Formula
   desc "Fast, safe, gradually typed embeddable scripting language derived from Lua"
   homepage "https://luau-lang.org"
-  url "https://github.com/Roblox/luau/archive/0.507.tar.gz"
-  sha256 "cd9220246f2d5512d80e17f9899b6a890d239f21c863885bac673d615e63f85f"
+  url "https://github.com/Roblox/luau/archive/0.509.tar.gz"
+  sha256 "838463904d3564b756b167b1d2959664f2ff1e07224acf3ed69753724fab0754"
   license "MIT"
   head "https://github.com/Roblox/luau.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/luau"
-    sha256 cellar: :any_skip_relocation, mojave: "6c0d7576a0c26d1e2d871d473c767fecf8bb56d8ec470d9918cc6a209a80db63"
+    sha256 cellar: :any_skip_relocation, mojave: "5c8a2861fe179a88d33eeda0df22400e66e89ea3e156b9a05add0b67647a75c9"
   end
 
   depends_on "cmake" => :build
