182a39ef9cd38d090688aae529db78e20979f613
Adrian Ho <the.gromgit@gmail.com>
2022-01-10 08:13:36 +0800

    git-revise: update 0.7.0 bottle.

--- Formula/git-revise.rb
+++ Formula/git-revise.rb
@@ -3,20 +3,14 @@ class GitRevise < Formula
 
   desc "Rebase alternative for easy & efficient in-memory rebases and fixups"
   homepage "https://github.com/mystor/git-revise"
-  url "https://files.pythonhosted.org/packages/8e/80/97eae3a7d93f8c17127ac5722ffb5a0f3b3bfd18525569865d2bfb5d27a1/git-revise-0.6.0.tar.gz"
-  sha256 "21e89eba6602e8bea38b34ac6ec747acba2aee876f2e73ca0472476109e82bf4"
+  url "https://files.pythonhosted.org/packages/99/fe/03e0afc973c19af8ebf9c7a4a090a974c0c39578b1d4082d201d126b7f9a/git-revise-0.7.0.tar.gz"
+  sha256 "af92ca2e7224c5e7ac2e16ed2f302dd36839a33521655c20fe0b7d693a1dc4c4"
   license "MIT"
-  revision 2
   head "https://github.com/mystor/git-revise.git", revision: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "51ea42f3af7218aa18e655dc10892c646f394be2ac50a4a51879cd5e1aad059f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "51ea42f3af7218aa18e655dc10892c646f394be2ac50a4a51879cd5e1aad059f"
-    sha256 cellar: :any_skip_relocation, monterey:       "8a6880070fdf65aef264f9e698be293832777a17c2b9aa395338b61da1cf9c39"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8a6880070fdf65aef264f9e698be293832777a17c2b9aa395338b61da1cf9c39"
-    sha256 cellar: :any_skip_relocation, catalina:       "8a6880070fdf65aef264f9e698be293832777a17c2b9aa395338b61da1cf9c39"
-    sha256 cellar: :any_skip_relocation, mojave:         "8a6880070fdf65aef264f9e698be293832777a17c2b9aa395338b61da1cf9c39"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f2e420f2730b973463b230a961270614c84a9a10edc2c12e0f4d62d179f35747"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-revise"
+    sha256 cellar: :any_skip_relocation, mojave: "aac1ccf21a7f370ad64d30bc92cefab901913bc81b7561ce3b4232036d28da0c"
   end
 
   depends_on "python@3.10"
