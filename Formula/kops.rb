ea3f18b6ae5c6c53c147e9aa79559dc3229a88b7
Adrian Ho <the.gromgit@gmail.com>
2022-01-11 04:50:00 +0800

    kops: update 1.22.2 bottle.

--- Formula/kops.rb
+++ Formula/kops.rb
@@ -4,7 +4,7 @@ class Kops < Formula
   url "https://github.com/kubernetes/kops/archive/v1.22.2.tar.gz"
   sha256 "b6c80827d9a2562743e6b88e23f5ad21bf80d3650acc6dc6009fcc0b3d42df0a"
   license "Apache-2.0"
-  head "https://github.com/kubernetes/kops.git"
+  head "https://github.com/kubernetes/kops.git", branch: "master"
 
   livecheck do
     url :stable
@@ -13,8 +13,8 @@ class Kops < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kops"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "5cd2d6e81e335a943e2888f23faab940190d55ed689b53e1df0b825a1e762d70"
+    rebuild 3
+    sha256 cellar: :any_skip_relocation, mojave: "a5d09d66de0f9b6ecc678c16be5e88b5935b74235791851fd1902f1961cd939d"
   end
 
   depends_on "go" => :build
@@ -26,18 +26,23 @@ class Kops < Formula
     kopspath = buildpath/"src/k8s.io/kops"
     kopspath.install Dir["*"]
     system "make", "-C", kopspath
-    bin.install("bin/kops")
+    bin.install "bin/kops"
 
     # Install bash completion
-    output = Utils.safe_popen_read("#{bin}/kops", "completion", "bash")
+    output = Utils.safe_popen_read(bin/"kops", "completion", "bash")
     (bash_completion/"kops").write output
 
     # Install zsh completion
-    output = Utils.safe_popen_read("#{bin}/kops", "completion", "zsh")
+    output = Utils.safe_popen_read(bin/"kops", "completion", "zsh")
     (zsh_completion/"_kops").write output
+
+    # Install fish completion
+    output = Utils.safe_popen_read(bin/"kops", "completion", "fish")
+    (fish_completion/"kops.fish").write output
   end
 
   test do
-    system "#{bin}/kops", "version"
+    assert_match version.to_s, shell_output("#{bin}/kops version")
+    assert_match "no context set in kubecfg", shell_output("#{bin}/kops validate cluster 2>&1", 1)
   end
 end
