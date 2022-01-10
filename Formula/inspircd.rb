e8cb2e787be0df656a799ec03ae96e597c3ec7cd
Adrian Ho <the.gromgit@gmail.com>
2022-01-11 01:35:16 +0800

    inspircd: update 3.12.0 bottle.

--- Formula/inspircd.rb
+++ Formula/inspircd.rb
@@ -1,8 +1,8 @@
 class Inspircd < Formula
   desc "Modular C++ Internet Relay Chat daemon"
   homepage "https://www.inspircd.org/"
-  url "https://github.com/inspircd/inspircd/archive/v3.11.0.tar.gz"
-  sha256 "ce39a15764474a8716f6dda69e4d2b890fa17a8736bcb43c877aaacc2382479d"
+  url "https://github.com/inspircd/inspircd/archive/v3.12.0.tar.gz"
+  sha256 "3503cd7e68764819e9d0e2a7f301bb19899a1a3633d4653d877651dc35278d16"
   license "GPL-2.0-only"
 
   livecheck do
@@ -11,10 +11,8 @@ class Inspircd < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "33f3cd972d8ab19d4bac2325993d190c0af3e88b9656f4637186cacb0819a881"
-    sha256 big_sur:       "25ee9b4d13e8cfcfcc6e74ed5b3f936a220670a5a7220f1b870900a75f86dc2c"
-    sha256 catalina:      "f0694ead1168a43f793f83ddac4cfa9e173c8a236c9fd4e5b78b2d2230a27250"
-    sha256 mojave:        "b94389bfdd3396598b3f0ef09ce775540a42f0edc94618c2d56578bf9d2ec9bc"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/inspircd"
+    sha256 mojave: "0137193dda0c172691dd9a31a2a6021e64ca3bf0ab083911a7f087ecc60b0848"
   end
 
   depends_on "pkg-config" => :build
