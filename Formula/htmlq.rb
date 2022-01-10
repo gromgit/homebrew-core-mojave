f4db187262367fa487347024b0ca529ed4191f72
Adrian Ho <the.gromgit@gmail.com>
2022-01-11 00:56:31 +0800

    htmlq: update 0.4.0 bottle.

--- Formula/htmlq.rb
+++ Formula/htmlq.rb
@@ -1,19 +1,14 @@
 class Htmlq < Formula
   desc "Uses CSS selectors to extract bits content from HTML files"
   homepage "https://github.com/mgdm/htmlq"
-  url "https://github.com/mgdm/htmlq/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "df07a3afbd61824d382961620782c10b0c839d33dbd4d9a2dda261091c830f60"
+  url "https://github.com/mgdm/htmlq/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "c2954c0fcb3ada664b14834646aa0a2c4268683cc51fd60d47a71a9f7e77d4b9"
   license "MIT"
   head "https://github.com/mgdm/htmlq.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "50250a414769450f0c7a60e7c43a5c0624cec7bdc830363fdf17ac7ba129c4f9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "155a5d20ec52baa51c8d5f70f3224ff7707c860a49bc51f020b2afdf8ea1ad2c"
-    sha256 cellar: :any_skip_relocation, monterey:       "32c487944df2e353c7723141f8afccea2c1d9c5fb9cba2da07e9719afdca0997"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8ec8b00331597e951f4f449394d26549d47c1090d2a1562047268c9ed0397580"
-    sha256 cellar: :any_skip_relocation, catalina:       "9be3c8684ee2f1666837195233bfccd29975555f9c7ae36ca1d3870fe0a5ccfc"
-    sha256 cellar: :any_skip_relocation, mojave:         "568439b3497a88d0d2fc3b6d7b0e3330c2b93e6a0e0c9df8de17768321a7e9df"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee7e22902b6f9bfe4f9403430dfd16fc242d87ee99e59c99358dcbe3a34a1be2"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/htmlq"
+    sha256 cellar: :any_skip_relocation, mojave: "1b39a2eddeb0ce201b0a2fb6d463e7d2336d0897ed01f410b397c25f49f6879d"
   end
 
   depends_on "rust" => :build
