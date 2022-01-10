b80966764de75be6efad9880b7c9f074204a76a1
Adrian Ho <the.gromgit@gmail.com>
2022-01-10 09:03:50 +0800

    goplus: update 1.0.39 bottle.

--- Formula/goplus.rb
+++ Formula/goplus.rb
@@ -1,22 +1,19 @@
 class Goplus < Formula
   desc "Programming language for engineering, STEM education, and data science"
   homepage "https://goplus.org"
-  url "https://github.com/goplus/gop/archive/v1.0.36.tar.gz"
-  sha256 "d42a9bb5ca1eac3afcee149f585d2ef7737e595619e5b76f2cca79f5b9c8019f"
+  url "https://github.com/goplus/gop/archive/v1.0.39.tar.gz"
+  sha256 "abc5ed80ccd5d233c0b90e82b6fa5aaa874c4fe50cc6fe0f30372f96f7e75677"
   license "Apache-2.0"
   head "https://github.com/goplus/gop.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/goplus"
-    sha256 mojave: "e34b8f7360eb7b1e52bbd4bc19e4f7c9c1db8dd2eedaafe340b056878cdd8f61"
+    sha256 mojave: "7ac5bfc575e5b581a836638af961c3dfbdea2c685d500c0edfafba16ddd28936"
   end
 
   depends_on "go"
 
   def install
-    # Patch version to match the version of gop, currently it get version from git tag
-    inreplace "env/version.go", /^\tbuildVersion string$/, "\tbuildVersion string = \"v#{version}\"" unless build.head?
-
     ENV["GOPROOT_FINAL"] = libexec
     system "go", "run", "cmd/make.go", "--install"
 
