5f4792f12e27d75a998cf7bfda7bedc8f480b094
Adrian Ho <the.gromgit@gmail.com>
2022-01-12 03:03:13 +0800

    opencascade: update 7.6.0 bottle.

--- Formula/opencascade.rb
+++ Formula/opencascade.rb
@@ -21,17 +21,22 @@ class Opencascade < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/opencascade"
-    rebuild 2
-    sha256 mojave: "f4a244ec8f8464dc28654b4f248f23001a8ce0ce5c8848915bd745941a394c52"
+    rebuild 3
+    sha256 mojave: "4fbc416c5445938a22e9207257413eb6e530ce2edb9612a9c44831a9672f6b50"
   end
 
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
   depends_on "rapidjson" => :build
+  depends_on "fontconfig"
   depends_on "freeimage"
   depends_on "freetype"
   depends_on "tbb@2020"
   depends_on "tcl-tk"
+
+  on_linux do
+    depends_on "mesa" # For OpenGL
+  end
 
   def install
     tcltk = Formula["tcl-tk"]
@@ -64,7 +69,9 @@ class Opencascade < Formula
   end
 
   test do
-    output = shell_output("#{bin}/DRAWEXE -c \"pload ALL\"")
-    assert_equal "1", output.chomp
+    output = shell_output("#{bin}/DRAWEXE -b -c \"pload ALL\"")
+
+    # Discard the first line ("DRAW is running in batch mode"), and check that the second line is "1"
+    assert_equal "1", output.split(/\n/, 2)[1].chomp
   end
 end
