615cdecc4c4c5dbe0966ea09d9e35dd6a8242f99
Adrian Ho <the.gromgit@gmail.com>
2022-01-11 11:10:19 +0800

    mdzk: update 0.5.0 bottle.

--- Formula/mdzk.rb
+++ Formula/mdzk.rb
@@ -1,8 +1,8 @@
 class Mdzk < Formula
   desc "Plain text Zettelkasten based on mdBook"
-  homepage "https://mdzk-rs.github.io"
-  url "https://github.com/mdzk-rs/mdzk/archive/0.4.3.tar.gz"
-  sha256 "47b3333268ab92d29a2a0c017bc7ef93df82a657b42a00e5042492d51d466af1"
+  homepage "https://mdzk.app/"
+  url "https://github.com/mdzk-rs/mdzk/archive/0.5.0.tar.gz"
+  sha256 "f8b72c70cee068896a7786fdffc4c7900085aa2f1f53e973759b829183c9a8e2"
   license "MPL-2.0"
   head "https://github.com/mdzk-rs/mdzk.git", branch: "main"
 
@@ -13,8 +13,7 @@ class Mdzk < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mdzk"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "29511ebb1fc74595a65e36d359c71ccab300bcf456db73d12177d3f29958bf17"
+    sha256 cellar: :any_skip_relocation, mojave: "981cdb9839938c368c231e7a6ecc8a951314af057007736644bc4af37ab83b02"
   end
 
   depends_on "rust" => :build
