3dae7b46ce762eefc1f0f47c98ceebdb230a7f04
Adrian Ho <the.gromgit@gmail.com>
2022-01-12 05:00:09 +0800

    oq: update 1.3.2 bottle.

--- Formula/oq.rb
+++ Formula/oq.rb
@@ -1,18 +1,13 @@
 class Oq < Formula
   desc "Performant, and portable jq wrapper to support formats other than JSON"
   homepage "https://blacksmoke16.github.io/oq"
-  url "https://github.com/Blacksmoke16/oq/archive/v1.3.0.tar.gz"
-  sha256 "5bfb23b0c527f45eacd40485779c708b1a05dd36015b50b84df29c2ac3f6cdac"
+  url "https://github.com/Blacksmoke16/oq/archive/v1.3.2.tar.gz"
+  sha256 "5216b16a874e7c0e74d4e735c593c1d353061f91fac4e455f6af7975c6c22bc3"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "89e3b3a0e14c9d26556ba8fe0e518b2fd48f021ba4286ad1db4690c9a0d18953"
-    sha256 cellar: :any,                 arm64_big_sur:  "e28f2b92624cade03207c8ebec0b8b0afa96af680b53f46cf0bed65afd82d2af"
-    sha256 cellar: :any,                 monterey:       "6620d58995e30101ea8721883d91d35dbe9e475771828eb264c6cfa2a1a49ee0"
-    sha256 cellar: :any,                 big_sur:        "6dac9a710d4e0d51d68488aebd186aae114c05ffc8b62726d79338b016041ac7"
-    sha256 cellar: :any,                 catalina:       "2d0d73c1c9db23f83e66e2a1e2e1c2950246e108773adcce5b09c63c585cc781"
-    sha256 cellar: :any,                 mojave:         "9d66884fa4c1a1b605acc19e42f4f0e4aa6b768e329f1a28b2e28337570b2288"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "43537a23936df709ba4fd6a25008c80a41b7914e5ef6f447965b0059e1039a14"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/oq"
+    sha256 cellar: :any, mojave: "26190f4d7cd3578d86f28646f877f0b40911aa862679ab83b2c7e13ddf8ecf4f"
   end
 
   depends_on "crystal" => :build
