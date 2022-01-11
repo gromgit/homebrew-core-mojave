32194324d5f5da6bdc85fda4f5a90706d66f99d0
Adrian Ho <the.gromgit@gmail.com>
2022-01-11 14:47:09 +0800

    mpg321: update 0.3.2_1 bottle.

--- Formula/mpg321.rb
+++ Formula/mpg321.rb
@@ -4,19 +4,11 @@ class Mpg321 < Formula
   url "https://downloads.sourceforge.net/project/mpg321/mpg321/0.3.2/mpg321_0.3.2.orig.tar.gz"
   sha256 "056fcc03e3f5c5021ec74bb5053d32c4a3b89b4086478dcf81adae650eac284e"
   license "GPL-2.0"
+  revision 1
 
   bottle do
-    sha256 arm64_monterey: "1da7ca02424159358e08a6fad616a94fa848a0a46eb5098147b8f2a2f2d90538"
-    sha256 arm64_big_sur:  "0cde599f9f7a975ff76faddcdd85606f62445c2493e201db737916fd03ec907f"
-    sha256 monterey:       "30e6b5eb8e967e4c2597d2e2c017f1a77704d42a54ddba774f7fd603c805b502"
-    sha256 big_sur:        "f21a4649c7437144d8228ce229acf7dcd36541c8475ddc9e065bf62d7f501b35"
-    sha256 catalina:       "fdea1076a22b6af12c33b02e9eb040fceb11d39ed2eaa7307574bf5331d8f8dc"
-    sha256 mojave:         "d587b58200397ad4e8f7fa8c861e01c2c5e344d89a235e78d22fb7bd5ddf04f2"
-    sha256 high_sierra:    "6a8f8f58c8bf02f99e8206a231fce4e9f2bd7333b888581dd1838246983d139f"
-    sha256 sierra:         "a69f242f57e4211f96fa56f10573777204d5ed7d61cd7b35a04e0bbd33b9064e"
-    sha256 el_capitan:     "6c8921b0703d2952b6038ce7097957c3c2bfe9b59c2d41b5caddc268e96b245d"
-    sha256 yosemite:       "48b9ac480d966fc344c4867f3dcef7cd59be1440b11fe7d8280d51134a881f78"
-    sha256 x86_64_linux:   "f0e6431c86cba77da50d3ea89267963a78b9e7e46edf95172f52c086b5313d76"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mpg321"
+    sha256 mojave: "5e21c97e6761cde1bb4b51077833a3719c3f41f5510df2a6e30054e87be50b30"
   end
 
   depends_on "libao"
