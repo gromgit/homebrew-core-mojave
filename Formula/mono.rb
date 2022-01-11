02a4db4cb6cc37fa81662f007d1e4fa02b74da55
Adrian Ho <the.gromgit@gmail.com>
2022-01-11 14:21:06 +0800

    mono: update 6.12.0.122_1 bottle.

--- Formula/mono.rb
+++ Formula/mono.rb
@@ -4,6 +4,7 @@ class Mono < Formula
   url "https://download.mono-project.com/sources/mono/mono-6.12.0.122.tar.xz"
   sha256 "29c277660fc5e7513107aee1cbf8c5057c9370a4cdfeda2fc781be6986d89d23"
   license "MIT"
+  revision 1
 
   livecheck do
     url "https://www.mono-project.com/download/stable/"
@@ -11,15 +12,13 @@ class Mono < Formula
   end
 
   bottle do
-    sha256 monterey: "4a4560ef7bd6dc638600e6fae876c3e27b3443698719a1dae45fe49fc1987f78"
-    sha256 big_sur:  "fdd17b0e0eb154047fa8091b52763f1f0df0fc921c216f6633d4d59f7cd62af5"
-    sha256 catalina: "428998efcf415948ca793b166d7ed6e242814205238e77111419de828fd33cfe"
-    sha256 mojave:   "d25b6982b6bd7af6b001e5f7b53ff0dad68937ce11ba0dce9d1529e2b0608b85"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mono"
+    sha256 mojave: "77b29505cc6ced223db6bcd2b3d2413ae2dc7752f491f56b518ff5758430ed4c"
   end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   uses_from_macos "unzip" => :build
 
