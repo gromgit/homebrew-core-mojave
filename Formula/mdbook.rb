10bb8fa36df29d9d4a6a3f048b9e94e9e7cb8a70
Adrian Ho <the.gromgit@gmail.com>
2022-01-11 11:03:25 +0800

    mdbook: update 0.4.15 bottle.

--- Formula/mdbook.rb
+++ Formula/mdbook.rb
@@ -1,15 +1,14 @@
 class Mdbook < Formula
   desc "Create modern online books from Markdown files"
   homepage "https://rust-lang.github.io/mdBook/"
-  url "https://github.com/rust-lang/mdBook/archive/v0.4.14.tar.gz"
-  sha256 "59fd3e417e9d09deac89e20467194dd9f93854c2f1a87e845816c5cec676765c"
+  url "https://github.com/rust-lang/mdBook/archive/v0.4.15.tar.gz"
+  sha256 "fa7f217fb6be229c25be21ccb712af8d0012241b529d1bbacb8f5cac65fe0013"
   license "MPL-2.0"
-  head "https://github.com/rust-lang/mdBook.git"
+  head "https://github.com/rust-lang/mdBook.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mdbook"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "47bedf87c1266d4395d46fb67bb28b3e02eb964bcde69b70e1bbdfca73dc19f2"
+    sha256 cellar: :any_skip_relocation, mojave: "6c3473a014f545fd85c54a1327be87838e3c46a02f298190c34c18da01dce41e"
   end
 
   depends_on "rust" => :build
