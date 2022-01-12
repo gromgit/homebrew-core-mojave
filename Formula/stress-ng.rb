26987d4e794a3b77ed4a93189f0733e74f44af0b
Adrian Ho <the.gromgit@gmail.com>
2022-01-12 16:11:09 +0800

    stress-ng: update 0.13.10 bottle.

--- Formula/stress-ng.rb
+++ Formula/stress-ng.rb
@@ -1,13 +1,13 @@
 class StressNg < Formula
   desc "Stress test a computer system in various selectable ways"
   homepage "https://wiki.ubuntu.com/Kernel/Reference/stress-ng"
-  url "https://github.com/ColinIanKing/stress-ng/archive/refs/tags/V0.13.08.tar.gz"
-  sha256 "5dbb886f3926ef6c88e18cfd83c750f9fc11fb658f83b6eafcf22fd3a456cc8f"
+  url "https://github.com/ColinIanKing/stress-ng/archive/refs/tags/V0.13.10.tar.gz"
+  sha256 "972b429f9eb0afbceabf7f3babab8599d8224b5d146e244c2cfe65129befb973"
   license "GPL-2.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/stress-ng"
-    sha256 cellar: :any_skip_relocation, mojave: "f20e69bc54ecaf93cb4c82a5c926032aae36ff0a95811a6cc8ae58da26ce219a"
+    sha256 cellar: :any_skip_relocation, mojave: "f52aa5c08cf974ef3e71300f1fa4764293ed1114f9fc3a300a332b66644aeb79"
   end
 
   depends_on macos: :sierra
