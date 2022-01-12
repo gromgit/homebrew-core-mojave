b77f89783ab4e94b49e53507e3af5a19db6c162e
Adrian Ho <the.gromgit@gmail.com>
2022-01-12 15:39:19 +0800

    spiped: update 1.6.2 bottle.

--- Formula/spiped.rb
+++ Formula/spiped.rb
@@ -1,8 +1,8 @@
 class Spiped < Formula
   desc "Secure pipe daemon"
   homepage "https://www.tarsnap.com/spiped.html"
-  url "https://www.tarsnap.com/spiped/spiped-1.6.1.tgz"
-  sha256 "8d7089979db79a531a0ecc507b113ac6f2cf5f19305571eff1d3413e0ab33713"
+  url "https://www.tarsnap.com/spiped/spiped-1.6.2.tgz"
+  sha256 "05d4687d12d11d7f9888d43f3d80c541b7721c987038d085f71c91bb06204567"
 
   livecheck do
     url :homepage
@@ -10,13 +10,8 @@ class Spiped < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "27a4be44c3532d6c5fd3c35ad01b6d7b8aeba9430e97cd684f51b386843dd7e4"
-    sha256 cellar: :any, arm64_big_sur:  "74e0d5955ee9754f9a88798d8d0c5a2b425756d3359378c312874ccb3959421d"
-    sha256 cellar: :any, monterey:       "6bf6ffda856ddf718d621df5810af8b7c7cb2afc7f6d4df8f7b35d5c3a746b6c"
-    sha256 cellar: :any, big_sur:        "c245e8cf440207b8a2d229b8f8644ebe25da6999b52f2b8b16835c55b7f04b6e"
-    sha256 cellar: :any, catalina:       "efe2a93770708c9a8c1474651b7b0b221d263b7fbb7dc75e014ff21caf084510"
-    sha256 cellar: :any, mojave:         "44c1509c5faf96f0be69fd905525e2070cf25445afddfaf45584bd9c4a1d702c"
-    sha256 cellar: :any, high_sierra:    "b5615b6afbc743c7b8b2776c3537ec42a4f1519f1f2f3e12bd06ae4e96ce5f14"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/spiped"
+    sha256 cellar: :any, mojave: "96719318ceec728915ef628a28bc69f64da1f5066bb32436699bed94892476c2"
   end
 
   depends_on "bsdmake" => :build
