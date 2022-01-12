16138b65eb1dbc7d17aa094848e0c098c1426797
Adrian Ho <the.gromgit@gmail.com>
2022-01-12 14:20:04 +0800

    sdl2_mixer: update 2.0.4_3 bottle.

--- Formula/sdl2_mixer.rb
+++ Formula/sdl2_mixer.rb
@@ -1,10 +1,20 @@
 class Sdl2Mixer < Formula
   desc "Sample multi-channel audio mixer library"
   homepage "https://www.libsdl.org/projects/SDL_mixer/"
-  url "https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.tar.gz"
-  sha256 "b4cf5a382c061cd75081cf246c2aa2f9df8db04bdda8dcdc6b6cca55bede2419"
   license "Zlib"
-  revision 2
+  revision 3
+
+  stable do
+    url "https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.tar.gz"
+    sha256 "b4cf5a382c061cd75081cf246c2aa2f9df8db04bdda8dcdc6b6cca55bede2419"
+
+    # Fix fluidsynth use-after-free bug until the release after 2.0.4, upstream patch
+    # https://github.com/libsdl-org/SDL_mixer/commit/6160668079f91d57a5d7bf0b40ffdd843be70daf
+    patch :p3 do
+      url "https://github.com/libsdl-org/SDL_mixer/commit/6160668079f91d57a5d7bf0b40ffdd843be70daf.patch?full_index=1"
+      sha256 "73e5ebf9136818b7a65f1e8fcbeb99c350654d7d9e53629adc26887e9e169d8d"
+    end
+  end
 
   livecheck do
     url :homepage
@@ -12,13 +22,8 @@ class Sdl2Mixer < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "375fbfb9b64fdfc631a7446aa1e21c74f3fc1b5a2e732cbe9db711078fe9c805"
-    sha256 cellar: :any,                 arm64_big_sur:  "f3c822ba6f80353d0fee40ea106025d4519e1dbb141d097fef092393e95fb0b7"
-    sha256 cellar: :any,                 monterey:       "4784c901a5fe2da26d58b06d1c888cb861874cb16d1211705066dcd7a61027ab"
-    sha256 cellar: :any,                 big_sur:        "1529a00916c4066d8adc0987b627e2bc7cf66aca063562cb3af64f8fa5f231f7"
-    sha256 cellar: :any,                 catalina:       "9779416544a0d71a8206b45895a3060baca2bf0877441017aaa6b1d6136654a2"
-    sha256 cellar: :any,                 mojave:         "9c13dd597aca2e0d5f53f2a7b4a1ea4e5a724c08796ba0eaf71a54f9cc714fbc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "412de036ef39cab5f7e1907711c2273a3d671e995ddeda320cad9cfedaa72a28"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sdl2_mixer"
+    sha256 cellar: :any, mojave: "473c3f8bc0c3e9b0fa3f9ae36dc2ad0f91447d72f66a7e1a007d64cbcad677b8"
   end
 
   head do
