ca539139abb0ff706fcd73fd8a6d96d8da7c4127
Adrian Ho <the.gromgit@gmail.com>
2022-01-12 17:55:32 +0800

    tealdeer: update 1.5.0 bottle.

--- Formula/tealdeer.rb
+++ Formula/tealdeer.rb
@@ -1,19 +1,13 @@
 class Tealdeer < Formula
   desc "Very fast implementation of tldr in Rust"
   homepage "https://github.com/dbrgn/tealdeer"
-  url "https://github.com/dbrgn/tealdeer/archive/v1.4.1.tar.gz"
-  sha256 "eaf42fe17be751985cbf46c170ef623fcbd36028c88c2e70823492a9335a4a8e"
+  url "https://github.com/dbrgn/tealdeer/archive/v1.5.0.tar.gz"
+  sha256 "00902a50373ab75fedec4578c6c2c02523fad435486918ad9a86ed01f804358a"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a88d1a7e6cbbd536fc3fa7f4746ddc3191664943207941f2163eaabc34525a44"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4007bbd16184eea9bb652cfcec1b7456551400f85de61cf080a98046cf79c5dd"
-    sha256 cellar: :any_skip_relocation, monterey:       "4e85ccac7ae235ec3be341399f1a3eb6ff7e37b61652d87d85b111b6f0465f17"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5f323f75b7d481e86187ac45f9931ad114e1a9d4e7035f04a368d690de77e9e0"
-    sha256 cellar: :any_skip_relocation, catalina:       "db4a2fc7dceef4f4f914aaf7b655741e960664b46c61ab2c163f015a74949533"
-    sha256 cellar: :any_skip_relocation, mojave:         "9ad3e3ea878b05fd2764cc8e534888b8bf810f88cc4d986de158ed6b33633b42"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "80a29641c9b29a3cda69adf5afd2c36b27f84fb8f89a555dbf2a676dddf03b70"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1a01f8be8f2e5ba17e70706875d0322c8ddfed2c3b3e7847c642d49f71629f94"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tealdeer"
+    sha256 cellar: :any_skip_relocation, mojave: "dea0166fa14fd3e846e505758c69fadd11a47b7d1f3f5fd854fbdcdbe322dc05"
   end
 
   depends_on "rust" => :build
