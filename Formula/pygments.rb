580e6475317ee9c4c348b258fe1fbb4f7855ec5a
Adrian Ho <the.gromgit@gmail.com>
2022-01-12 08:45:49 +0800

    pygments: update 2.11.2 bottle.

--- Formula/pygments.rb
+++ Formula/pygments.rb
@@ -3,20 +3,14 @@ class Pygments < Formula
 
   desc "Generic syntax highlighter"
   homepage "https://pygments.org/"
-  url "https://files.pythonhosted.org/packages/b7/b3/5cba26637fe43500d4568d0ee7b7362de1fb29c0e158d50b4b69e9a40422/Pygments-2.10.0.tar.gz"
-  sha256 "f398865f7eb6874156579fdf36bc840a03cab64d1cde9e93d68f46a425ec52c6"
+  url "https://files.pythonhosted.org/packages/94/9c/cb656d06950268155f46d4f6ce25d7ffc51a0da47eadf1b164bbf23b718b/Pygments-2.11.2.tar.gz"
+  sha256 "4e426f72023d88d03b2fa258de560726ce890ff3b630f88c21cbb8b2503b8c6a"
   license "BSD-2-Clause"
-  revision 1
   head "https://github.com/pygments/pygments.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c625b002e4402b4c7fdecded981fa7ff87e1514d284e4057011d041d12ad093e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c625b002e4402b4c7fdecded981fa7ff87e1514d284e4057011d041d12ad093e"
-    sha256 cellar: :any_skip_relocation, monterey:       "7095fda62baec826a619282cd127e03eed82a02368eafcc4e22ea5fdb1a4ca10"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7095fda62baec826a619282cd127e03eed82a02368eafcc4e22ea5fdb1a4ca10"
-    sha256 cellar: :any_skip_relocation, catalina:       "7095fda62baec826a619282cd127e03eed82a02368eafcc4e22ea5fdb1a4ca10"
-    sha256 cellar: :any_skip_relocation, mojave:         "7095fda62baec826a619282cd127e03eed82a02368eafcc4e22ea5fdb1a4ca10"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bd675d1a3d161a05a82e0a4819eeb4464219e9eba491b6af816bff8820ce3e20"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pygments"
+    sha256 cellar: :any_skip_relocation, mojave: "1144025d257641b8df2002a0c7c55953d030050d06f8bd2f0cc739d926d993f2"
   end
 
   depends_on "python@3.10"
