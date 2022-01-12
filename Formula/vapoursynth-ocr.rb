b84998cf8eddb07fc1423196af213552201a5810
Adrian Ho <the.gromgit@gmail.com>
2022-01-12 22:06:23 +0800

    vapoursynth-ocr: update 1_1 bottle.

--- Formula/vapoursynth-ocr.rb
+++ Formula/vapoursynth-ocr.rb
@@ -4,18 +4,14 @@ class VapoursynthOcr < Formula
   url "https://github.com/vapoursynth/vs-ocr/archive/R1.tar.gz"
   sha256 "a551354c78fdbe9bcaf29f9a29ee9a7d257ed74d1b6a8403049fcd57855fa0f4"
   license "MIT"
+  revision 1
   version_scheme 1
 
   head "https://github.com/vapoursynth/vs-ocr.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "a7a6a9f8639cb97e67fb5e6bdd7d8571303adf861e9c509078844594db1b036c"
-    sha256 cellar: :any, arm64_big_sur:  "abef84d6213651b91d9e60d32ae065b3227de72a2371f97266825b81f50d7148"
-    sha256 cellar: :any, monterey:       "1a6aac2710a3c004da08525b72426c1fb0512c302fca6ba488451687bcf34622"
-    sha256 cellar: :any, big_sur:        "d31d4f0eb749e5797e856de45e1c1f3e862abe31e897696887e20123330a29ae"
-    sha256 cellar: :any, catalina:       "6c7e3eb62e6cf0a5140b158fce51c5fca682e04b7b1074fd6425c9b0bf8ef40b"
-    sha256 cellar: :any, mojave:         "916f671b1aabc46a37c7197f061bf73c0360a8d3e22a72cce74009802b341351"
-    sha256               x86_64_linux:   "f1f688bb5c6cadf726d4df511ce40d58453839d74185f29a219e7e681b2abb9c"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/vapoursynth-ocr"
+    sha256 cellar: :any, mojave: "6412fbf866ee02108593db59191198dc1f47564b2f29b638a0cf8596c35a229b"
   end
 
   depends_on "meson" => :build
