479ff60d2b352f819433896dfa01ae5f43645bec
Adrian Ho <the.gromgit@gmail.com>
2022-01-12 18:44:12 +0800

    thefuck: update 3.32 bottle.

--- Formula/thefuck.rb
+++ Formula/thefuck.rb
@@ -3,20 +3,14 @@ class Thefuck < Formula
 
   desc "Programmatically correct mistyped console commands"
   homepage "https://github.com/nvbn/thefuck"
-  url "https://files.pythonhosted.org/packages/21/d2/f5194c2d51b2aab4bda6de97c5eb1ca5a547ad9b9f778ab5bf4ce19fba88/thefuck-3.31.tar.gz"
-  sha256 "6e6083e6c94fe948fcb9c6083baee30ba4cf3f09ffd30cd9564d6473db271941"
+  url "https://files.pythonhosted.org/packages/ac/d0/0c256afd3ba1d05882154d16aa0685018f21c60a6769a496558da7d9d8f1/thefuck-3.32.tar.gz"
+  sha256 "976740b9aa536726fa23cadc9a10bf457e92e335901c61fcff9152c84485ac3d"
   license "MIT"
-  revision 1
   head "https://github.com/nvbn/thefuck.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5d6e1b84afcb1b8cb97bfe27607860b9c9ea1625d1d96adf784bd9f92c1268b7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7246194bfb83392fd64bba4abb52121c909457989178c48bdeb4a1f2131eb982"
-    sha256 cellar: :any_skip_relocation, monterey:       "ec984854e30a8b7055f2cb3d93e8298f1b4b26447bec8d98e246298daef50d23"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b6ff31e6acd33cc8693ec64f77b64ebd5214798602ea897ae0e73de6369a717a"
-    sha256 cellar: :any_skip_relocation, catalina:       "3b6da25b50f07e16fc8b178182eaedf4258cdb27ee7bde746e8d7c91bf79790a"
-    sha256 cellar: :any_skip_relocation, mojave:         "a1d299b1561cae8e6282658f378aa77e9dc4bb8b2750c07af74bab5a1510ee4b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dbe88c05c5339f3eb6d1cf31b56b2555184b2c041ab503ca946f21f1ccf09f4b"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/thefuck"
+    sha256 cellar: :any_skip_relocation, mojave: "90201973b74ee28dd53770f595ad477e91c4b971a9313348221d722874c33753"
   end
 
   depends_on "python@3.10"
@@ -27,13 +21,13 @@ class Thefuck < Formula
   end
 
   resource "decorator" do
-    url "https://files.pythonhosted.org/packages/4f/51/15a4f6b8154d292e130e5e566c730d8ec6c9802563d58760666f1818ba58/decorator-5.0.9.tar.gz"
-    sha256 "72ecfba4320a893c53f9706bebb2d55c270c1e51a28789361aa93e4a21319ed5"
+    url "https://files.pythonhosted.org/packages/92/3c/34f8448b61809968052882b830f7d8d9a8e1c07048f70deb039ae599f73c/decorator-5.1.0.tar.gz"
+    sha256 "e59913af105b9860aa2c8d3272d9de5a56a4e608db9a2f167a8480b323d529a7"
   end
 
   resource "psutil" do
-    url "https://files.pythonhosted.org/packages/e1/b0/7276de53321c12981717490516b7e612364f2cb372ee8901bd4a66a000d7/psutil-5.8.0.tar.gz"
-    sha256 "0c9ccb99ab76025f2f0bbecf341d4656e9c1351db8cc8a03ccd62e318ab4b5c6"
+    url "https://files.pythonhosted.org/packages/47/b6/ea8a7728f096a597f0032564e8013b705aa992a0990becd773dcc4d7b4a7/psutil-5.9.0.tar.gz"
+    sha256 "869842dbd66bb80c3217158e629d6fceaecc3a3166d3d1faee515b05dd26ca25"
   end
 
   resource "pyte" do
