45738b30b17ca905488e491ff73578e0b8fbfc2d
Adrian Ho <the.gromgit@gmail.com>
2022-01-12 18:41:45 +0800

    terrascan: update 1.13.0 bottle.

--- Formula/terrascan.rb
+++ Formula/terrascan.rb
@@ -1,15 +1,14 @@
 class Terrascan < Formula
   desc "Detect compliance and security violations across Infrastructure as Code"
   homepage "https://github.com/accurics/terrascan"
-  url "https://github.com/accurics/terrascan/archive/v1.12.0.tar.gz"
-  sha256 "66ec203e518d3887bf14a2535e98e9cf64dfc4b2c667655a0055dc3df8f6be78"
+  url "https://github.com/accurics/terrascan/archive/v1.13.0.tar.gz"
+  sha256 "20d7f92a88168fa803dc62323c91fbd9e7c707cc1161865053e8a9e752e170e9"
   license "Apache-2.0"
   head "https://github.com/accurics/terrascan.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/terrascan"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "e2b4f899c16ccd5a0ae79b517e6cd5ef2cc648775341464e9bff3dfcf8c7d8e0"
+    sha256 cellar: :any_skip_relocation, mojave: "a42b3c04935f318af2caf148f69a5ce8a0e8d804b86142a9763429215a9723af"
   end
 
   depends_on "go" => :build
