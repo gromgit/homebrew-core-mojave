b16476aeb82355b8be75e130f59211558d4908bc
Adrian Ho <the.gromgit@gmail.com>
2022-01-12 22:02:09 +0800

    vala-language-server: update 0.48.4 bottle.

--- Formula/vala-language-server.rb
+++ Formula/vala-language-server.rb
@@ -1,19 +1,13 @@
 class ValaLanguageServer < Formula
   desc "Code Intelligence for Vala & Genie"
   homepage "https://github.com/Prince781/vala-language-server"
-  url "https://github.com/Prince781/vala-language-server/archive/3e01b8383b3db3c39af276528663d6084c671455.tar.gz"
-  version "0.48.3"
-  sha256 "6f3b34bcb4e049c299ae3d5433153e4b685b0bace0ea8d761ffea266714ce841"
+  url "https://github.com/Prince781/vala-language-server/releases/download/0.48.4/vala-language-server-0.48.4.tar.xz"
+  sha256 "0fd65b641a4bd6e0385502f0536f803bf542fd8bae6a2adb04e2e299ccca4551"
   license "LGPL-2.1-only"
-  revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "8dd0c7a04c35f113e4790087e83862b0678a3378471710cbab086785443e9a1e"
-    sha256 cellar: :any, monterey:      "22d04a5dfd33e1fee166a4497ec10914eb56d9835af038e49183d864589112bf"
-    sha256 cellar: :any, big_sur:       "522ff0afde99dc62e32af3df6d40138c4d115591a7d2c9ddf99a7ffb5c2354d8"
-    sha256 cellar: :any, catalina:      "fc5ee5ec8ca3e5d41bc7cb5edd7a85540f5bce64d8d625548d706d884c28f83a"
-    sha256 cellar: :any, mojave:        "13333288fba772232a82c3b16c10df48a6d41d4345897cbcba5e21fc0b15a2b4"
-    sha256               x86_64_linux:  "abc6b06e996a54dd2c3c2e8567637cda104b0e4a975c9e3143519095429ab758"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/vala-language-server"
+    sha256 cellar: :any, mojave: "bd0a5f076381ff33c7daeefd63c09e74f048b5aa21a847929418cd67c702bf05"
   end
 
   depends_on "meson" => :build
