2cf48fa1ade9b22897c29b7f538731b98d572cef
Adrian Ho <the.gromgit@gmail.com>
2022-01-14 00:57:31 +0800

    xray: update 1.5.2 bottle.

--- Formula/xray.rb
+++ Formula/xray.rb
@@ -1,45 +1,38 @@
 class Xray < Formula
   desc "Platform for building proxies to bypass network restrictions"
   homepage "https://xtls.github.io/"
-  url "https://github.com/XTLS/Xray-core/archive/v1.4.5.tar.gz"
-  sha256 "54c6a687dd463b25afe8d8eb44d37e18b8177f58308207cd1d74f6cd04619854"
+  url "https://github.com/XTLS/Xray-core/archive/v1.5.2.tar.gz"
+  sha256 "b687a8fd1325bee0f6352c8dc3bfb70a7ee07cd74aacaece4e36c93cf7cda417"
   license all_of: ["MPL-2.0", "CC-BY-SA-4.0"]
   head "https://github.com/XTLS/Xray-core.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "88d3157297bd0338b65a2ddc084609112154c3657ee352b5869840ebad286ab5"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ea9626bccd0b70aac0880d33f1d34bac66285f529d16d51e78e68b504fe1f76c"
-    sha256 cellar: :any_skip_relocation, monterey:       "c870a48767aa293615e91434871faa98c15ea7706b9198c301d4dd17439e3fd8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5347284834dfeda1150dfae1831341be14f12cfd983641da9bd6b49488fd4053"
-    sha256 cellar: :any_skip_relocation, catalina:       "42ed8b7eb67b83f0b560363f3a61ee0b3e7a698cb697df08e9a06453a9db4c74"
-    sha256 cellar: :any_skip_relocation, mojave:         "47d081dfe999470fbbb7b1b64ce18c0fad7362be4b68d696db87a571c71f540d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3fc53062e26cc87199a676071958fc5f458baae4c68530c7f9315f988d7ed599"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/xray"
+    sha256 cellar: :any_skip_relocation, mojave: "e206bb52c558a175aecf0254cea2e4d90209eb70622b07df83631b5cec2245d5"
   end
 
   depends_on "go" => :build
 
   resource "geoip" do
-    url "https://github.com/v2fly/geoip/releases/download/202109060310/geoip.dat"
-    sha256 "ed94122961f358abede9f1954722039d5a0300b614c77cc27d92618c08b97bb8"
+    url "https://github.com/v2fly/geoip/releases/download/202201060033/geoip.dat"
+    sha256 "27f9cf6d647f018be425188a25ceb095076f6d29544bac843c2d51e0000d00a0"
   end
 
   resource "geosite" do
-    url "https://github.com/v2fly/domain-list-community/releases/download/20210906031055/dlc.dat"
-    sha256 "7618b876fd5a1066d0b44c1c8ce04608495ae991806890f8b1cbfafe79caf6c1"
+    url "https://github.com/v2fly/domain-list-community/releases/download/20220108151752/dlc.dat"
+    sha256 "f1961b28a8a7aa386d69c7480bf5d7bac7fa466fb8dcba499ed2d964f470d9fc"
   end
 
   resource "example_config" do
-    # borrow v2ray (v4.36.2) example config
-    url "https://raw.githubusercontent.com/v2fly/v2ray-core/v4.41.1/release/config/config.json"
+    # borrow v2ray example config
+    url "https://raw.githubusercontent.com/v2fly/v2ray-core/v4.44.0/release/config/config.json"
     sha256 "1bbadc5e1dfaa49935005e8b478b3ca49c519b66d3a3aee0b099730d05589978"
   end
 
   def install
     ldflags = "-s -w -buildid="
     execpath = libexec/name
-    system "go", "build", *std_go_args, "-o", execpath,
-                 "-ldflags", ldflags,
-                 "./main"
+    system "go", "build", *std_go_args(output: execpath, ldflags: ldflags), "./main"
     (bin/"xray").write_env_script execpath,
       XRAY_LOCATION_ASSET: "${XRAY_LOCATION_ASSET:-#{pkgshare}}"
 
