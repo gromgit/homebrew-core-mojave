92a96f4e263a318f2946fb6a37a8ff6635ad45fa
Adrian Ho <the.gromgit@gmail.com>
2022-01-14 04:12:49 +0800

    extra-cmake-modules: update 5.90.0 bottle.

--- Formula/extra-cmake-modules.rb
+++ Formula/extra-cmake-modules.rb
@@ -1,8 +1,8 @@
 class ExtraCmakeModules < Formula
   desc "Extra modules and scripts for CMake"
   homepage "https://api.kde.org/frameworks/extra-cmake-modules/html/index.html"
-  url "https://download.kde.org/stable/frameworks/5.89/extra-cmake-modules-5.89.0.tar.xz"
-  sha256 "3dd7229a225923b8570a333ee6e4a07b0f5f700ea9538fdeb22cc3cbba69f02f"
+  url "https://download.kde.org/stable/frameworks/5.90/extra-cmake-modules-5.90.0.tar.xz"
+  sha256 "f3007c3ceca56249292cf340a38518589a55a4afcf27e166ab63c5ac8ffabcb0"
   license all_of: ["BSD-2-Clause", "BSD-3-Clause", "MIT"]
   head "https://invent.kde.org/frameworks/extra-cmake-modules.git", branch: "master"
 
@@ -15,7 +15,7 @@ class ExtraCmakeModules < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/extra-cmake-modules"
-    sha256 cellar: :any_skip_relocation, mojave: "61f9cbf0e3a0870c06a4c00c82c3333b6c2a6925a2d6132e15339c3f9abb7111"
+    sha256 cellar: :any_skip_relocation, mojave: "50d27af7d3b5dab833ff0a3b45c473b281f5cd2a3ea46af650fd147bd6f30f67"
   end
 
   depends_on "cmake" => [:build, :test]
@@ -23,13 +23,15 @@ class ExtraCmakeModules < Formula
   depends_on "sphinx-doc" => :build
 
   def install
-    args = std_cmake_args
-    args << "-DBUILD_HTML_DOCS=ON"
-    args << "-DBUILD_MAN_DOCS=ON"
-    args << "-DBUILD_QTHELP_DOCS=ON"
-    args << "-DBUILD_TESTING=OFF"
+    args = std_cmake_args + %w[
+      -S .
+      -B build
+      -DBUILD_HTML_DOCS=ON
+      -DBUILD_MAN_DOCS=ON
+      -DBUILD_QTHELP_DOCS=ON
+    ]
 
-    system "cmake", "-S", ".", "-B", "build", *args
+    system "cmake", *args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
