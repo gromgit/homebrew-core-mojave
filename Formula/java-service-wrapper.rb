f7d3c2892bf143bb3c832ebab4c85608a848796d
Adrian Ho <the.gromgit@gmail.com>
2022-01-14 04:09:42 +0800

    java-service-wrapper: update 3.5.48_1 bottle.

--- Formula/java-service-wrapper.rb
+++ Formula/java-service-wrapper.rb
@@ -1,13 +1,14 @@
 class JavaServiceWrapper < Formula
   desc "Simplify the deployment, launch and monitoring of Java applications"
   homepage "https://wrapper.tanukisoftware.com/"
-  url "https://downloads.sourceforge.net/project/wrapper/wrapper_src/Wrapper_3.5.47_20211207/wrapper_3.5.47_src.tar.gz"
-  sha256 "1a171c7d81bcaf8ef1954c284668e51f5123d60559f381f19469163aa37e1651"
+  url "https://downloads.sourceforge.net/project/wrapper/wrapper_src/Wrapper_3.5.48_20211222/wrapper_3.5.48_src.tar.gz"
+  sha256 "c2800d8702ce86f4e7abe06773ccc220364424ebf7b3035f788ff79d0ed8d523"
   license "GPL-2.0-only"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/java-service-wrapper"
-    sha256 cellar: :any_skip_relocation, mojave: "17d566c0aa1c946cb31743e9472ef9a938610c1fc45e9bd936bb0175581ef4ab"
+    sha256 cellar: :any_skip_relocation, mojave: "38932e55a24644362285989149fec764aef68f4dc0128913ec0281e88f572095"
   end
 
   depends_on "ant" => :build
@@ -21,9 +22,17 @@ class JavaServiceWrapper < Formula
     # Default javac target version is 1.4, use 1.6 which is the minimum available on openjdk@11
     system "ant", "-Dbits=64", "-Djavac.target.version=1.6"
     libexec.install "lib", "bin", "src/bin" => "scripts"
+    if OS.mac?
+      if Hardware::CPU.arm?
+        ln_s "libwrapper.dylib", libexec/"lib/libwrapper.jnilib"
+      else
+        ln_s "libwrapper.jnilib", libexec/"lib/libwrapper.dylib"
+      end
+    end
   end
 
   test do
+    ENV["JAVA_HOME"] = Formula["openjdk@11"].opt_prefix
     output = shell_output("#{libexec}/bin/testwrapper status", 1)
     assert_match("Test Wrapper Sample Application", output)
   end
