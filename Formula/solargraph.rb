53b7691a02b37c750a1be4eaf0e755b3f819e165
Adrian Ho <the.gromgit@gmail.com>
2022-01-13 15:13:45 +0800

    solargraph: update 0.44.2 bottle.

---
+++ Formula/solargraph.rb
@@ -0,0 +1,46 @@
+class Solargraph < Formula
+  desc "Ruby language server"
+  homepage "https://solargraph.org"
+  # Must be git, because solargraph.gemspec uses git ls-files
+  url "https://github.com/castwide/solargraph.git",
+      tag:      "v0.44.2",
+      revision: "e50a6dc4b43e2183e245aded8d23f0003d8c6bf7"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/solargraph"
+    sha256 cellar: :any, mojave: "de88e2fd03b63a83c468774e3cfc5c0e56588788fc500f54dddb6d13f1180d7d"
+  end
+
+  depends_on "ruby"
+
+  def install
+    ENV["GEM_HOME"] = libexec
+    system "gem", "build", "#{name}.gemspec"
+    system "gem", "install", "#{name}-#{version}.gem"
+    bin.install libexec/"bin/#{name}"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+  end
+
+  test do
+    require "open3"
+
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+
+    Open3.popen3("#{bin}/solargraph", "stdio") do |stdin, stdout, _, _|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      sleep 3
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+    end
+  end
+end
