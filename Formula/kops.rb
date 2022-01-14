2dfca647b28a4de9dc9501408c208bd19bff2ab6
Adrian Ho <the.gromgit@gmail.com>
2022-01-15 02:38:10 +0800

    kops: update 1.22.3 bottle.

--- Formula/kops.rb
+++ Formula/kops.rb
@@ -1,8 +1,8 @@
 class Kops < Formula
   desc "Production Grade K8s Installation, Upgrades, and Management"
   homepage "https://kops.sigs.k8s.io/"
-  url "https://github.com/kubernetes/kops/archive/v1.22.2.tar.gz"
-  sha256 "b6c80827d9a2562743e6b88e23f5ad21bf80d3650acc6dc6009fcc0b3d42df0a"
+  url "https://github.com/kubernetes/kops/archive/v1.22.3.tar.gz"
+  sha256 "76fb2e20f1d4d54904311c3aec2298ae99dcea5ea8473677a61f6e6c7418d341"
   license "Apache-2.0"
   head "https://github.com/kubernetes/kops.git", branch: "master"
 
@@ -13,8 +13,7 @@ class Kops < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kops"
-    rebuild 3
-    sha256 cellar: :any_skip_relocation, mojave: "a5d09d66de0f9b6ecc678c16be5e88b5935b74235791851fd1902f1961cd939d"
+    sha256 cellar: :any_skip_relocation, mojave: "94e564e4e698713078697802109276efcd060978bd86fe4973989b30ff486624"
   end
 
   depends_on "go" => :build
