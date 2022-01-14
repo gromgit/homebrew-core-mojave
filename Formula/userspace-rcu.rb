79bf23c607b9361ab2a02f5a0747d1d750fc8471
Adrian Ho <the.gromgit@gmail.com>
2022-01-14 20:50:44 +0800

    userspace-rcu: update 0.13.1 bottle.

--- Formula/userspace-rcu.rb
+++ Formula/userspace-rcu.rb
@@ -1,8 +1,8 @@
 class UserspaceRcu < Formula
   desc "Library for userspace RCU (read-copy-update)"
   homepage "https://liburcu.org"
-  url "https://lttng.org/files/urcu/userspace-rcu-0.12.2.tar.bz2"
-  sha256 "4eefc11e4f6c212fc7d84d871e1cc139da0669a46ff3fda557a6fdd4d74ca67b"
+  url "https://lttng.org/files/urcu/userspace-rcu-0.13.1.tar.bz2"
+  sha256 "3213f33d2b8f710eb920eb1abb279ec04bf8ae6361f44f2513c28c20d3363083"
   license all_of: ["LGPL-2.1-or-later", "MIT"]
 
   livecheck do
@@ -11,13 +11,8 @@ class UserspaceRcu < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "5c3c2e5b8b85a43934d3514aab325202d7860df6d9d76f5200b218616f6ab533"
-    sha256 cellar: :any,                 arm64_big_sur:  "f4fc590ca3038ba37ae1f93f96799139bf99fa4c4ddcf3d1924759385d310203"
-    sha256 cellar: :any,                 monterey:       "d4c05b2b24be889411d51c145dd4f23c1b90366b15841713ad4049f9beb6a826"
-    sha256 cellar: :any,                 big_sur:        "2eaf6d663b24932de82d80effcd6bf77fe7307a301296094809495eb4f6c5597"
-    sha256 cellar: :any,                 catalina:       "4066d1afdd9ab1bd126c933bcf53bff3d74179195443f272841d9ac5da0b4b05"
-    sha256 cellar: :any,                 mojave:         "63d30f6d0d0f00b5eae317aa0cff21f28cfed4a75fd460ba7c6651cc3d3dea79"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9661f4a159015cd923c13e91f5fe0a91e96b72982effd5a67d3138fcdbe457fc"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/userspace-rcu"
+    sha256 cellar: :any, mojave: "1c9880b8c97f6c763bbb87a0850b29724d7ddea01ffab7c8dc4dea1ed4b97658"
   end
 
   # Fix -flat_namespace being used on Big Sur and later.
