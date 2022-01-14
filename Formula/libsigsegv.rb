ed2adfcc51fa0f209c6bf985d228ce647ba4ff53
Adrian Ho <the.gromgit@gmail.com>
2022-01-15 04:43:52 +0800

    libsigsegv: update 2.14 bottle.

--- Formula/libsigsegv.rb
+++ Formula/libsigsegv.rb
@@ -1,19 +1,14 @@
 class Libsigsegv < Formula
   desc "Library for handling page faults in user mode"
   homepage "https://www.gnu.org/software/libsigsegv/"
-  url "https://ftp.gnu.org/gnu/libsigsegv/libsigsegv-2.13.tar.gz"
-  mirror "https://ftpmirror.gnu.org/libsigsegv/libsigsegv-2.13.tar.gz"
-  sha256 "be78ee4176b05f7c75ff03298d84874db90f4b6c9d5503f0da1226b3a3c48119"
+  url "https://ftp.gnu.org/gnu/libsigsegv/libsigsegv-2.14.tar.gz"
+  mirror "https://ftpmirror.gnu.org/libsigsegv/libsigsegv-2.14.tar.gz"
+  sha256 "cdac3941803364cf81a908499beb79c200ead60b6b5b40cad124fd1e06caa295"
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "e131f8e70ae22c236cf1c3061de2710671b6be977f9c0640c3e3226da0cd395b"
-    sha256 cellar: :any,                 arm64_big_sur:  "709a1a801698a0e0862be0f71d9b15ed8af9b6777956ae2caf0795d418956ce4"
-    sha256 cellar: :any,                 monterey:       "e4d9698e740e043b010752b577c5a0db82db2e26645449ca45cef2906bc01611"
-    sha256 cellar: :any,                 big_sur:        "9bd929ab1b6a2c35bdde0306a2a4c30498a47659ae0877bc89a7b74f67d93425"
-    sha256 cellar: :any,                 catalina:       "0d7f731afff70661df049267de9fe2c34b74d3918a7a7695fbfd1deef664aa68"
-    sha256 cellar: :any,                 mojave:         "95525c7e620743555e44175496c21c57a8cc39b8ca2670bf0fd690cc42a2977c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "61ea7eb4168f98ae1177e9dd5def5bd9883724351d41f805bf996f5482904b80"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libsigsegv"
+    sha256 cellar: :any, mojave: "9eb16b54aac87c248f8024417adf230945335fd3b09680189f352eacba8e9fbe"
   end
 
   head do
