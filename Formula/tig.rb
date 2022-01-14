a996844375de01c3747bdee75e9515a3dcf7ef7b
Adrian Ho <the.gromgit@gmail.com>
2022-01-14 19:49:59 +0800

    tig: update 2.5.5 bottle.

--- Formula/tig.rb
+++ Formula/tig.rb
@@ -1,18 +1,13 @@
 class Tig < Formula
   desc "Text interface for Git repositories"
   homepage "https://jonas.github.io/tig/"
-  url "https://github.com/jonas/tig/releases/download/tig-2.5.4/tig-2.5.4.tar.gz"
-  sha256 "c48284d30287a6365f8a4750eb0b122e78689a1aef8ce1d2961b6843ac246aa7"
+  url "https://github.com/jonas/tig/releases/download/tig-2.5.5/tig-2.5.5.tar.gz"
+  sha256 "24ba2c8beae889e6002ea7ced0e29851dee57c27fde8480fb9c64d5eb8765313"
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "75fec92f280daf4789ccd06a2ad809185032cbd98097baffeb6b0b283b9212ef"
-    sha256 cellar: :any,                 arm64_big_sur:  "ca3273a9bb53f6b060ce4ccbffaf82308d90d833a7120505a55f877b6f197592"
-    sha256 cellar: :any,                 monterey:       "3ff950a8d1588789c3b4bb86f998464f3b69ebbf37aca1403a43b8499f93f244"
-    sha256 cellar: :any,                 big_sur:        "506b2d47105b97ba9d04f3b91a45e9a164dd409fc1fd58655bf3015bb1403e0b"
-    sha256 cellar: :any,                 catalina:       "5afa0c1a9dcb4d851f88db05b0f911e70014abb6d64b43d1b1e56f12452d233a"
-    sha256 cellar: :any,                 mojave:         "c2dff759957d7b62f5ab947e6637c428c9352c244faeb6f8e02918f8c9587771"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a71079791b2ff79c307250f00163c3320ba6c242f4115892fa490d78a71196f9"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tig"
+    sha256 cellar: :any, mojave: "c9409c48bee68b8fe87c4bddcf1928c220b57bf6a791d4594837640af121609c"
   end
 
   head do
