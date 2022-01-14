d3fc8201365461777f3f27598f64d2b8db2cb9dc
Adrian Ho <the.gromgit@gmail.com>
2022-01-14 20:22:46 +0800

    timg: update 1.4.3 bottle.

--- Formula/timg.rb
+++ Formula/timg.rb
@@ -1,16 +1,14 @@
 class Timg < Formula
   desc "Terminal image and video viewer"
   homepage "https://timg.sh/"
-  url "https://github.com/hzeller/timg/archive/refs/tags/v1.4.2.tar.gz"
-  sha256 "7607efaffbed0b65b3c824956de421b155a4f14243e7a752b19454f88bf9d563"
+  url "https://github.com/hzeller/timg/archive/refs/tags/v1.4.3.tar.gz"
+  sha256 "46eac8d5434b281afa6d64ced5f46c732c1c4d0699e16a22175b7df179221e2c"
   license "GPL-2.0-only"
-  revision 1
   head "https://github.com/hzeller/timg.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/timg"
-    rebuild 2
-    sha256 cellar: :any, mojave: "dd3e59d705bbbb75e92c4de9a943fb9edbfdb32f485262db52e3ee658d7ad58e"
+    sha256 cellar: :any, mojave: "5487c25beee27493d379861d0dd9a7a2794f284033c478b1a6205e4c7b67d60b"
   end
 
   depends_on "cmake" => :build
