3a7e6ca3601a671c7c92c656e7da899513a1959e
Adrian Ho <the.gromgit@gmail.com>
2022-01-14 19:39:01 +0800

    texmath: update 0.12.4 bottle.

--- Formula/texmath.rb
+++ Formula/texmath.rb
@@ -1,14 +1,14 @@
 class Texmath < Formula
   desc "Haskell library for converting LaTeX math to MathML"
   homepage "https://johnmacfarlane.net/texmath.html"
-  url "https://hackage.haskell.org/package/texmath-0.12.3.3/texmath-0.12.3.3.tar.gz"
-  sha256 "c80e796a22e17faf29253ce733046a12670c173a10dbfdbd8182821fccb28c7d"
+  url "https://hackage.haskell.org/package/texmath-0.12.4/texmath-0.12.4.tar.gz"
+  sha256 "4373bb9db8f977f37b9c1316c65ca97bae7600277e4f79d681dabf2fcb81f0cc"
   license "GPL-2.0-or-later"
   head "https://github.com/jgm/texmath.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/texmath"
-    sha256 cellar: :any_skip_relocation, mojave: "ca7c615ec0e7196acdd5da036bf2fbea3f5cf3daa12a2e7138a45afb8dc89898"
+    sha256 cellar: :any_skip_relocation, mojave: "2e0711b974ccd6e175227afeb2bb53102b3f18d6db8e646a8f72fd46d7283ff9"
   end
 
   depends_on "cabal-install" => :build
