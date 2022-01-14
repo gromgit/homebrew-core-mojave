82232bd8f00fcded54574fc560763cee8d56fe66
Adrian Ho <the.gromgit@gmail.com>
2022-01-14 17:11:42 +0800

    socat: update 1.7.4.3 bottle.

--- Formula/socat.rb
+++ Formula/socat.rb
@@ -1,9 +1,9 @@
 class Socat < Formula
   desc "SOcket CAT: netcat on steroids"
   homepage "http://www.dest-unreach.org/socat/"
-  url "http://www.dest-unreach.org/socat/download/socat-1.7.4.2.tar.gz"
-  sha256 "a38f507dea8aaa8f260f54ebc1de1a71e5adca416219f603cda3e3002960173c"
-  license "GPL-2.0"
+  url "http://www.dest-unreach.org/socat/download/socat-1.7.4.3.tar.gz"
+  sha256 "d697245144731423ddbbceacabbd29447089ea223e9a439b28f9ff90d0dd216e"
+  license "GPL-2.0-only"
 
   livecheck do
     url "http://www.dest-unreach.org/socat/download/"
@@ -12,15 +12,11 @@ class Socat < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/socat"
-    sha256 cellar: :any, mojave: "d0fab00f274e290891f5e5cf7cf0b51146b610e621666fe98748a3b0fa9af17f"
+    sha256 cellar: :any, mojave: "3a037b95a518646c96fa25817944311e73f4ba446c9222f7b63339296d8db1b8"
   end
 
   depends_on "openssl@1.1"
   depends_on "readline"
-
-  # Fix `error: use of undeclared identifier 'TCP_INFO'`
-  # Remove in the next release
-  patch :DATA
 
   def install
     system "./configure", *std_configure_args, "--mandir=#{man}"
@@ -32,26 +28,3 @@ class Socat < Formula
     assert_match "HTTP/1.0", output.lines.first
   end
 end
-
-__END__
-diff --git a/filan.c b/filan.c
-index 3465f7c..77c22a4 100644
---- a/filan.c
-+++ b/filan.c
-@@ -905,6 +905,7 @@ int tcpan(int fd, FILE *outfile) {
- #if WITH_TCP
-
- int tcpan2(int fd, FILE *outfile) {
-+#ifdef TCP_INFO
-    struct tcp_info tcpinfo;
-    socklen_t tcpinfolen = sizeof(tcpinfo);
-    int result;
-@@ -930,6 +931,8 @@ int tcpan2(int fd, FILE *outfile) {
-    // fprintf(outfile, "%s={%u}\t", "TCPI_", tcpinfo.tcpi_);
-
-    return 0;
-+#endif
-+   return -1;
- }
-
- #endif /* WITH_TCP */
