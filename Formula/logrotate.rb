a15d5e7930339dcdaaee7a912534f72634c371dd
Adrian Ho <the.gromgit@gmail.com>
2022-01-15 05:27:27 +0800

    logrotate: update 3.19.0 bottle.

--- Formula/logrotate.rb
+++ Formula/logrotate.rb
@@ -1,18 +1,13 @@
 class Logrotate < Formula
   desc "Rotates, compresses, and mails system logs"
   homepage "https://github.com/logrotate/logrotate"
-  url "https://github.com/logrotate/logrotate/releases/download/3.18.1/logrotate-3.18.1.tar.xz"
-  sha256 "14a924e4804b3974e85019a9f9352c2a69726702e6656155c48bcdeace68a5dc"
+  url "https://github.com/logrotate/logrotate/releases/download/3.19.0/logrotate-3.19.0.tar.xz"
+  sha256 "ddd5274d684c5c99ca724e8069329f343ebe376e07493d537d9effdc501214ba"
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "e02202d8879a0df397d38756657da92460d9574f0a5f7d222e704daba047d513"
-    sha256 cellar: :any,                 arm64_big_sur:  "7a35f391118adf13094e83d7ab66a0a0842828b6d17bb825a04a2c15ff0cb8b5"
-    sha256 cellar: :any,                 monterey:       "a177d5de09fd91c9cd28edd2ea568514a24a6e09dbcd17910233e4b44847ecbf"
-    sha256 cellar: :any,                 big_sur:        "6976c5e710c45560e47c8eaba409aaf71e607cec3e86eaa6396df5342685b720"
-    sha256 cellar: :any,                 catalina:       "82980e80bdd29e8b5d21661e5ffd283c654057912fb0d8621e30397f577dc1e4"
-    sha256 cellar: :any,                 mojave:         "2aeb1ee2d25cb426a1d7a746c54451afad0d5f00a24c6cf6f38eb5be2a0c4e5a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b450a79764d5a0bb3e4cfddf3bc337bb703c34386254b76dbcd45fd09ae72ab7"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/logrotate"
+    sha256 cellar: :any, mojave: "1db0a5f6d0dec8d565793b12e7d664b223f97fe6665bedd648242427e257a4f0"
   end
 
   depends_on "popt"
