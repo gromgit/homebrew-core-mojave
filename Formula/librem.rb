7a8f1874f2820c72669f13187989962a62007677
Adrian Ho <the.gromgit@gmail.com>
2022-01-15 04:42:29 +0800

    librem: update 1.0.0 bottle.

--- Formula/librem.rb
+++ Formula/librem.rb
@@ -1,18 +1,13 @@
 class Librem < Formula
   desc "Toolkit library for real-time audio and video processing"
-  homepage "https://github.com/creytiv/rem"
-  url "https://github.com/creytiv/rem/releases/download/v0.6.0/rem-0.6.0.tar.gz"
-  sha256 "417620da3986461598aef327c782db87ec3dd02c534701e68f4c255e54e5272c"
+  homepage "https://github.com/baresip/rem"
+  url "https://github.com/baresip/rem/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "bcc91bb521fae183357fb422b00a3981477a22e99d3afe165c4ec50a6bbed9da"
+  license "BSD-3-Clause"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "2762f50f0dce284c02dbdbbe00ae376fbc143ae74aa028dffb12f3577c720eb0"
-    sha256 cellar: :any,                 arm64_big_sur:  "f89cd157675ca8fe3154b27cfeccf20846e40911c74820ccf61d2fb83abd6bfd"
-    sha256 cellar: :any,                 monterey:       "eac3ed6596fbb6296164a74c67b2a7495cdb215a79567b90c6e3a259d50d693b"
-    sha256 cellar: :any,                 big_sur:        "b571b06f4fc31e68adc40869c1e5b415eda6aed48f9093ee844f53f17baf5f44"
-    sha256 cellar: :any,                 catalina:       "cfb5f86357e8176a51e80b2ec726e8e38fc002a7dfea8f36256328747df01d9a"
-    sha256 cellar: :any,                 mojave:         "5fc70f673166bfc2794c40b440de29cc544cb3d46cccfb538ba868362e6e813a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2d35cb23f7a65fa6b0840d03f954b0d8275bcf6d525a0b7991ba30955f0d56a9"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/librem"
+    sha256 cellar: :any, mojave: "f3ca6360f291764561d0180a10d91782601f0e112175f3d62f01b83a9eedb7c2"
   end
 
   depends_on "libre"
