fdb9aa97fb8741abb6d4a2c3fd62cdde4102335c
Adrian Ho <the.gromgit@gmail.com>
2022-01-15 02:28:39 +0800

    kompose: update 1.26.1 bottle.

--- Formula/kompose.rb
+++ Formula/kompose.rb
@@ -1,14 +1,13 @@
 class Kompose < Formula
   desc "Tool to move from `docker-compose` to Kubernetes"
   homepage "https://kompose.io/"
-  url "https://github.com/kubernetes/kompose/archive/v1.26.0.tar.gz"
-  sha256 "e24db4279d3386700e25f3eb3ae4115ed11f4e0b2eea16d28f2113c71d13fb5b"
+  url "https://github.com/kubernetes/kompose/archive/v1.26.1.tar.gz"
+  sha256 "58547107377705f48cd02e391a5faf441dc0c861aeb9bc17c7c46e9de3ae1806"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kompose"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "ab1f24b6ab134c115ec3067fbca1628bbe64c4af93e59ca61683ffe9ffe88508"
+    sha256 cellar: :any_skip_relocation, mojave: "82b1a90e32a5eb05207ce0df113e98aca69a11f00f39b097b9a488e6b06cccf1"
   end
 
   depends_on "go" => :build
