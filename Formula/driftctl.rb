f52711e63e8475c174b90b2f074d82754d6cb667
Adrian Ho <the.gromgit@gmail.com>
2022-01-15 17:07:51 +0800

    driftctl: update 0.18.5 bottle.

--- Formula/driftctl.rb
+++ Formula/driftctl.rb
@@ -1,13 +1,13 @@
 class Driftctl < Formula
   desc "Detect, track and alert on infrastructure drift"
   homepage "https://driftctl.com"
-  url "https://github.com/snyk/driftctl/archive/v0.18.1.tar.gz"
-  sha256 "6a75838f31ec24d9b53e075d2edc21c5944f45a534588b809f5121a4c8bfb21f"
+  url "https://github.com/snyk/driftctl/archive/v0.18.5.tar.gz"
+  sha256 "9306553499c649e76abe3647986a20e69832e050d1e6bb5150d144cf61b07db3"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/driftctl"
-    sha256 cellar: :any_skip_relocation, mojave: "8b073bfbf77b5eafc9ca57fb706e9d07c11cd2873e55b9971ebb1457bfff2c9a"
+    sha256 cellar: :any_skip_relocation, mojave: "be11df69bfdd4598db4aebb24cd28bddeeb0ffe6266e81f6b43ae025973ee32d"
   end
 
   depends_on "go" => :build
@@ -35,6 +35,7 @@ class Driftctl < Formula
 
   test do
     assert_match "v#{version}", shell_output("#{bin}/driftctl version")
-    assert_match "Invalid AWS Region", shell_output("#{bin}/driftctl --no-version-check scan 2>&1", 1)
+    assert_match "Downloading terraform provider: aws",
+      shell_output("#{bin}/driftctl --no-version-check scan 2>&1", 1)
   end
 end
