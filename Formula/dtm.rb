d974800ec0c0a37742969985adaba96e060da694
Adrian Ho <the.gromgit@gmail.com>
2022-01-15 17:12:59 +0800

    dtm: update 1.10.1 bottle.

--- Formula/dtm.rb
+++ Formula/dtm.rb
@@ -1,13 +1,13 @@
 class Dtm < Formula
   desc "Cross-language distributed transaction manager"
   homepage "http://d.dtm.pub"
-  url "https://github.com/dtm-labs/dtm/archive/refs/tags/v1.8.4.tar.gz"
-  sha256 "a14f7c41582262709b67fef2e857a788c68b6af1630077cb882c7126c2d41159"
+  url "https://github.com/dtm-labs/dtm/archive/refs/tags/v1.10.1.tar.gz"
+  sha256 "6a4154260a99c3717d1a11922181129cb11fc27ec2b645a7892d10fc1b58ad39"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dtm"
-    sha256 cellar: :any_skip_relocation, mojave: "826d5cc9a05339cc9f91e63b8447b07b403afacd3c2f5baea20c1a83539e6915"
+    sha256 cellar: :any_skip_relocation, mojave: "5622a51ffc1d426db4e5f25185a4ffa83aad21ddb4cf8a1d97377451a5d8bf04"
   end
 
   depends_on "go" => :build
