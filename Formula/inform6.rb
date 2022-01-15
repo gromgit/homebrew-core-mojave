5e823ec1a3dc08cdde06f2d84c3a24fc0c0d242b
Adrian Ho <the.gromgit@gmail.com>
2022-01-15 21:07:04 +0800

    inform6: update 6.35-r8 bottle.

--- Formula/inform6.rb
+++ Formula/inform6.rb
@@ -1,19 +1,18 @@
 class Inform6 < Formula
   desc "Design system for interactive fiction"
   homepage "https://inform-fiction.org/inform6.html"
-  url "https://ifarchive.org/if-archive/infocom/compilers/inform6/source/inform-6.35-r5.tar.gz"
-  version "6.35-r5"
-  sha256 "4c5aa421b1c8c944e43142e1cb6b3c1e5ad7b03589fa470ce3734298ae61eaa8"
+  url "https://ifarchive.org/if-archive/infocom/compilers/inform6/source/inform-6.35-r8.tar.gz"
+  version "6.35-r8"
+  sha256 "a1babb9ac9fc68fc397905ba713f0651e16cade77f828d692f8f91195daa0f57"
   license "Artistic-2.0"
-  head "https://gitlab.com/DavidGriffith/inform6unix.git"
+  head "https://gitlab.com/DavidGriffith/inform6unix.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/inform6"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "87211f072d4b0546a1b2e84c8ba9f75651ae5479f4107dcd06a2b2fa49d0a497"
+    sha256 cellar: :any_skip_relocation, mojave: "a60c75f6df29c41f7be53ae90b3c2d38940b02fd1eecedd4bb64aa99907d9463"
   end
 
-  resource "test_resource" do
+  resource "homebrew-test_resource" do
     url "https://inform-fiction.org/examples/Adventureland/Adventureland.inf"
     sha256 "3961388ff00b5dfd1ccc1bb0d2a5c01a44af99bdcf763868979fa43ba3393ae7"
   end
@@ -25,7 +24,7 @@ class Inform6 < Formula
   end
 
   test do
-    resource("test_resource").stage do
+    resource("homebrew-test_resource").stage do
       system "#{bin}/inform", "Adventureland.inf"
       assert_predicate Pathname.pwd/"Adventureland.z5", :exist?
     end
