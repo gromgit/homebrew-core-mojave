f8854351a052bde433e2c47e4328d1e6c8d427e1
Adrian Ho <the.gromgit@gmail.com>
2022-01-19 17:44:16 +0800

    gerbil-scheme: update 0.17 bottle.

--- Formula/gerbil-scheme.rb
+++ Formula/gerbil-scheme.rb
@@ -1,8 +1,8 @@
 class GerbilScheme < Formula
   desc "Opinionated dialect of Scheme designed for Systems Programming"
   homepage "https://cons.io"
-  url "https://github.com/vyzo/gerbil/archive/v0.16.tar.gz"
-  sha256 "1157d4ef60dab6a0f7c4986d5c938391973045093c470a03ffe02266c4d3e119"
+  url "https://github.com/vyzo/gerbil/archive/v0.17.tar.gz"
+  sha256 "1e81265aba7e9022432649eb26b2e5c85a2bb631a315e4fa840b14cf336b2483"
   license "Apache-2.0"
 
   livecheck do
@@ -11,13 +11,8 @@ class GerbilScheme < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "e77cbea05b9d1991e02abd6d1ee6a59e623e3a27dd777cde903ef61ad16d195e"
-    sha256 arm64_big_sur:  "2961aec56f4dc30a10a944ffb2f7c34d2c725c4e53b3eb5486eaf5d1a2bd92dc"
-    sha256 monterey:       "a3078cf9a1a31024c3b62b116893dee942d4c535794c53c40566f01c6c0b303f"
-    sha256 big_sur:        "c116f383adefb6954e99e3fc318244f02c30ec0f53e3a7bac135c87e02a90dbc"
-    sha256 catalina:       "2e58cb74f57dd09a84bfed13aa1d44a1f1f8f5c057c59d2a27a338950c53ce82"
-    sha256 mojave:         "119570c624e9fb56e17df10263434eb0ba94ba3cdbcf5942c10cac9b26161318"
-    sha256 high_sierra:    "adb9592baedab1841b1a6603ff8cdc30694b01ecd5444d7c637a987f98356a44"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gerbil-scheme"
+    sha256 mojave: "90f7416bc87519a68a6f5a05ae0887290b055e43f5cb23204d8e14f759e8d69a"
   end
 
   depends_on "gambit-scheme"
@@ -39,7 +34,6 @@ class GerbilScheme < Formula
       system "./build.sh"
       system "./install"
 
-      rm "#{bin}/.keep"
       mv "#{share}/emacs/site-lisp/gerbil", "#{share}/emacs/site-lisp/gerbil-scheme"
     end
   end
