4365119b3ffbabc779d089e5dbd2db874b6778fb
Adrian Ho <the.gromgit@gmail.com>
2022-01-19 22:26:13 +0800

    lychee: update 0.8.2 bottle.

--- Formula/lychee.rb
+++ Formula/lychee.rb
@@ -1,15 +1,14 @@
 class Lychee < Formula
   desc "Fast, async, resource-friendly link checker"
   homepage "https://github.com/lycheeverse/lychee"
-  url "https://github.com/lycheeverse/lychee/archive/v0.8.1.tar.gz"
-  sha256 "88416f4c674fdf76cb92cf1b744b4f246116aaf9bdbe0da05a3b75f73f64fcf5"
+  url "https://github.com/lycheeverse/lychee/archive/v0.8.2.tar.gz"
+  sha256 "55b83a5d7145afc4358b29db49d8e107494b64bf4f8938da865ffed978d29cda"
   license any_of: ["Apache-2.0", "MIT"]
-  head "https://github.com/lycheeverse/lychee.git"
+  head "https://github.com/lycheeverse/lychee.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lychee"
-    rebuild 2
-    sha256 cellar: :any, mojave: "e6bfe5408b27b81aaba0abb3e7dc0e8005b405c54f2475c7af2bbed96c4b8df8"
+    sha256 cellar: :any, mojave: "67fbb8462c266c7b813a1f5855c41608cb600e6e83fae12d250e106978f2f5df"
   end
 
   depends_on "rust" => :build
