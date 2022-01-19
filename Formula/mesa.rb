c5bde7706eddece1515dbc224bc448103ae3960b
Adrian Ho <the.gromgit@gmail.com>
2022-01-20 00:24:07 +0800

    mesa: update 21.3.4 bottle.

--- Formula/mesa.rb
+++ Formula/mesa.rb
@@ -3,14 +3,14 @@ class Mesa < Formula
 
   desc "Graphics Library"
   homepage "https://www.mesa3d.org/"
-  url "https://mesa.freedesktop.org/archive/mesa-21.3.3.tar.xz"
-  sha256 "ad7f4613ea7c5d08d9fcb5025270199e6ceb9aa99fd72ee572b70342240a8121"
+  url "https://mesa.freedesktop.org/archive/mesa-21.3.4.tar.xz"
+  sha256 "77104fd4a93bce69da3b0982f8ee88ba7c4fb98cfc491a669894339cdcd4a67d"
   license "MIT"
   head "https://gitlab.freedesktop.org/mesa/mesa.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mesa"
-    sha256 mojave: "cddf028e702aef586c30cf86fe65b030287d3e7ce403bbe10036e24fafc47e04"
+    sha256 mojave: "12512d60e250bfc818c260401abc184c44775c90e1a42de5bae489348030c783"
   end
 
   depends_on "meson" => :build
