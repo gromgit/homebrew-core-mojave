54b43baad0a9fc985e2322a238e4fb8bed93e9e5
Adrian Ho <the.gromgit@gmail.com>
2022-01-19 14:07:09 +0800

    dwm: update 6.3 bottle.

--- Formula/dwm.rb
+++ Formula/dwm.rb
@@ -1,11 +1,10 @@
 class Dwm < Formula
   desc "Dynamic window manager"
   homepage "https://dwm.suckless.org/"
-  url "https://dl.suckless.org/dwm/dwm-6.2.tar.gz"
-  sha256 "97902e2e007aaeaa3c6e3bed1f81785b817b7413947f1db1d3b62b8da4cd110e"
+  url "https://dl.suckless.org/dwm/dwm-6.3.tar.gz"
+  sha256 "badaa028529b1fba1fd7f9a84f3b64f31190466c858011b53e2f7b70c6a3078d"
   license "MIT"
-  revision 2
-  head "https://git.suckless.org/dwm", using: :git
+  head "https://git.suckless.org/dwm", using: :git, branch: "master"
 
   livecheck do
     url "https://dl.suckless.org/dwm/"
@@ -13,13 +12,8 @@ class Dwm < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "3971098bdbc2dee1f4acac3020a38052d6be49f777543c422e93e2c2f361e3bf"
-    sha256 cellar: :any, arm64_big_sur:  "7fd3a01a1f29927ca94c2c5ea32b4ee0c9f31d9ea39adc04e76ab40517663149"
-    sha256 cellar: :any, monterey:       "ac0754547b75d1e161c9401e78dcad9c9560d76f45483c19fbd1890429b88cc7"
-    sha256 cellar: :any, big_sur:        "afd787afd9c6ea4cc81c100f324d2b8aa4c65c2a06e43ca87d54135425b347cf"
-    sha256 cellar: :any, catalina:       "d872be09d1f5c11c9fb4d34002cc5f4622fbc259691800e1742354573b9effb0"
-    sha256 cellar: :any, mojave:         "e4ec85368754c0594847dad5272770a36e69876ed433fdd390d73a7d05c43263"
-    sha256 cellar: :any, high_sierra:    "b22ec01678edc39f1b82837087bb69ac311bce937eb10cb096fc8b1002f97701"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dwm"
+    sha256 cellar: :any, mojave: "42888d31314c5feba35ef7e4940b523d64eff1fb8c5f42479fe1b8b10fa689d2"
   end
 
   depends_on "dmenu"
