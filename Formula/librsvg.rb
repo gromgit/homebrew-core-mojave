c2d523ba3249b3d5eb671cf72929c1363acfccc6
Adrian Ho <the.gromgit@gmail.com>
2022-01-21 04:28:14 +0800

    librsvg: update 2.52.5 bottle.

--- Formula/librsvg.rb
+++ Formula/librsvg.rb
@@ -1,17 +1,13 @@
 class Librsvg < Formula
   desc "Library to render SVG files using Cairo"
   homepage "https://wiki.gnome.org/Projects/LibRsvg"
-  url "https://download.gnome.org/sources/librsvg/2.50/librsvg-2.50.7.tar.xz"
-  sha256 "fffb61b08cd5282aaae147a02b305166a7426fad22a8b9427708f0f2fc426ebc"
+  url "https://download.gnome.org/sources/librsvg/2.52/librsvg-2.52.5.tar.xz"
+  sha256 "407cbbab518137ea18a3f3220bea180fbee75f3e5bd6ba10a7a862c1a6f74d82"
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256                               arm64_big_sur: "373e91782be11de752c5d738a19c4b0b7631319612982fa9366e5477d4f444b2"
-    sha256                               monterey:      "3753625c61c6f0753a816bed581b6acb54be4dd8d17049dcecf52c2ed9e825bb"
-    sha256                               big_sur:       "7849cab410dfd63c3f8f635e0a0152cf34425524f728c2f01a36d7631f34688b"
-    sha256                               catalina:      "454d94e2c72567744613b69a9d879dde0a0bd9593c32929d520266b04f6b51b8"
-    sha256                               mojave:        "312d0f2fade5525cfe58f0c0e16cccc1e9c73925bb2fc8f1c320d3a0f8a3f4fc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "48c4693a1f62fc8c870f3831867d9f082d0f9f42cb3151f0be77733b4c3e7ac4"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/librsvg"
+    sha256 mojave: "52bcc73e1571e2d1a186cf1b1fe387c16beccd69ecb5ac8aaa07d6108e9b88c9"
   end
 
   depends_on "gobject-introspection" => :build
