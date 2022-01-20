348d24fbe06f97921a5ede15cb170fc582a50c86
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:03:02 +0800

    lesspipe: no rebuilding required

--- Formula/lesspipe.rb
+++ Formula/lesspipe.rb
@@ -1,12 +1,12 @@
 class Lesspipe < Formula
   desc "Input filter for the pager less"
   homepage "https://www-zeuthen.desy.de/~friebel/unix/lesspipe.html"
-  url "https://github.com/wofr06/lesspipe/archive/v2.01.tar.gz"
-  sha256 "f6642b1bda91e98c3745fe084164a556832f2aeaa9304bd7534954bff5eeceea"
+  url "https://github.com/wofr06/lesspipe/archive/v2.02.tar.gz"
+  sha256 "f66da17dce756d06087ca0d9ff98f97e29cd3d026613853b60c174aa6fa0fd47"
   license "GPL-2.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "6330ca5233a6d4dc5426377b9d6609e16f948400d58294c26d0071750cf94a7b"
+    sha256 cellar: :any_skip_relocation, all: "809f54e556c9288bf10d9acf5089dd4880aacc701eb444c09296cb410f74fab5"
   end
 
   def install
