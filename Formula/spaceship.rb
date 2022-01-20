fa21f0541f0aa6684f37958d6ad9c3c617f88fae
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:03:22 +0800

    spaceship: no rebuilding required

--- Formula/spaceship.rb
+++ Formula/spaceship.rb
@@ -1,13 +1,13 @@
 class Spaceship < Formula
   desc "Zsh prompt for Astronauts"
   homepage "https://spaceship-prompt.sh"
-  url "https://github.com/spaceship-prompt/spaceship-prompt/archive/v3.16.1.tar.gz"
-  sha256 "e61e75f9534ec3de27b7e48e1e3f93be2d5a102da063f370a50cfd18c92b419e"
+  url "https://github.com/spaceship-prompt/spaceship-prompt/archive/v3.16.2.tar.gz"
+  sha256 "ee4d814de6e0cbd16d018de198a9f41ae40f6ffc0a091231eaacdf709b18bdfa"
   license "MIT"
   head "https://github.com/spaceship-prompt/spaceship-prompt.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "bb64ffc3bdd3af35ba1cf05d938b2a04c5ef82404fba5ac62abdd91031f67bc5"
+    sha256 cellar: :any_skip_relocation, all: "586ef18148719d02465eed249deb1e05a783f7102daae08c2953b46776021654"
   end
 
   depends_on "zsh" => :test
