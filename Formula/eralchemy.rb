30e0dacfb0a12b4a4cfdb9a7cba3430a0ba2cad9
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:02:47 +0800

    eralchemy: no rebuilding required

--- Formula/eralchemy.rb
+++ Formula/eralchemy.rb
@@ -9,13 +9,14 @@ class Eralchemy < Formula
   revision 5
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "d925eb8686a32d2bbf7b9bab62f65f6b761952dcb32923ef7b6428f5325c11a1"
-    sha256 cellar: :any,                 monterey:      "dcf339a6df3abf232c794911db58daa4ed85c47c4ce36c816684f045e01f7a90"
-    sha256 cellar: :any,                 big_sur:       "2a5ce41209235c75eeb092f0b6c17c6c46c03f2faada2b2a337fcda7f28a4288"
-    sha256 cellar: :any,                 catalina:      "e844701e7824dc9497969cc6592302bf56ac060d5aa18105e1ee2887411a5f12"
-    sha256 cellar: :any,                 mojave:        "cfb423a7299d1d307e58aa756675f26ea65ad405e6e2fb707727de49aa36eb64"
-    sha256 cellar: :any,                 high_sierra:   "cb2442baa298aa27c860ad7d80116131f286ea283625dbd809d4b518ba81707a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "33457b7cd8520565ade9b63b2ea9597b0e695652ded2414d0da61425a2eb8744"
+    sha256 cellar: :any,                 arm64_monterey: "4078f3edd263be83b197c693a0d840a444e45b885cdd6e9ddd05f7f01c8c325c"
+    sha256 cellar: :any,                 arm64_big_sur:  "d925eb8686a32d2bbf7b9bab62f65f6b761952dcb32923ef7b6428f5325c11a1"
+    sha256 cellar: :any,                 monterey:       "dcf339a6df3abf232c794911db58daa4ed85c47c4ce36c816684f045e01f7a90"
+    sha256 cellar: :any,                 big_sur:        "2a5ce41209235c75eeb092f0b6c17c6c46c03f2faada2b2a337fcda7f28a4288"
+    sha256 cellar: :any,                 catalina:       "e844701e7824dc9497969cc6592302bf56ac060d5aa18105e1ee2887411a5f12"
+    sha256 cellar: :any,                 mojave:         "cfb423a7299d1d307e58aa756675f26ea65ad405e6e2fb707727de49aa36eb64"
+    sha256 cellar: :any,                 high_sierra:    "cb2442baa298aa27c860ad7d80116131f286ea283625dbd809d4b518ba81707a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "33457b7cd8520565ade9b63b2ea9597b0e695652ded2414d0da61425a2eb8744"
   end
 
   depends_on "pkg-config" => :build
