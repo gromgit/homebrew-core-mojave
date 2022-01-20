aac9998a1b799875e21c5c775c2a3296e1d98398
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:02:51 +0800

    git-flow: no rebuilding required

--- Formula/git-flow.rb
+++ Formula/git-flow.rb
@@ -2,6 +2,7 @@ class GitFlow < Formula
   desc "Extensions to follow Vincent Driessen's branching model"
   homepage "https://github.com/nvie/gitflow"
   license "BSD-2-Clause"
+  revision 1
 
   stable do
     # Use the tag instead of the tarball to get submodules
@@ -16,17 +17,7 @@ class GitFlow < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6c94609aa047ec725c65834fc330b9d3261ce742b58ee4309a9bfabfd088b86a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7da4f3bb9c8cf3678057b79f03c056b1848aea64d16d4be68621a14352ca5477"
-    sha256 cellar: :any_skip_relocation, monterey:       "04653cedc77df6d3e4b7143bdb04f0711bc5beb809b455a97d42d86c82106ae4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "46009ab147b52d74bbda2a0b4d97374f0e515a5157db4ff803f6270ded7fde48"
-    sha256 cellar: :any_skip_relocation, catalina:       "8ea12cd7e2666fb6fdfaffaeb3a0437037a40cde559a835fc9de038f36a424dc"
-    sha256 cellar: :any_skip_relocation, mojave:         "ab455769b6b8122e1d345f56d799fe43445bbbeba6265892715167388c737af9"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "ce4b64f4d13f4a5a8e05c9087627b42cd328c8accc8349c4ca256238d1d3fecc"
-    sha256 cellar: :any_skip_relocation, sierra:         "acb9a2690fa86925600fa6e67e38731ef05f7f5d6ccfceb9c5175286c1081fe0"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "56826b30d91ffb54829f4792f88c673b1c3e748aa662bef5806e4a6f5d0ee015"
-    sha256 cellar: :any_skip_relocation, yosemite:       "a5e97d4d5c082194b36c18e7b051c43b2d5b37366b2ac56c5ea9407f6315685b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6c94609aa047ec725c65834fc330b9d3261ce742b58ee4309a9bfabfd088b86a"
+    sha256 cellar: :any_skip_relocation, all: "cffa267a59238174b54b4058131b3fdf674d4fa79ff724dd7111f6bc7730c40f"
   end
 
   head do
@@ -42,11 +33,18 @@ class GitFlow < Formula
   def install
     system "make", "prefix=#{libexec}", "install"
     bin.write_exec_script libexec/"bin/git-flow"
-
     resource("completion").stage do
+      # Fix a comment referencing `/usr/local` that causes deviations between bottles.
+      inreplace "git-flow-completion.bash", "/usr/local", HOMEBREW_PREFIX
       bash_completion.install "git-flow-completion.bash"
-      zsh_completion.install "git-flow-completion.zsh"
     end
+  end
+
+  def caveats
+    <<~EOS
+      To install Zsh completions:
+        brew install zsh-completions
+    EOS
   end
 
   test do
