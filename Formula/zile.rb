3bc346e355d41587db5974fe9097b56622f2d8fe
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:03:28 +0800

    zile: no rebuilding required

--- Formula/zile.rb
+++ Formula/zile.rb
@@ -11,12 +11,13 @@ class Zile < Formula
   version_scheme 1
 
   bottle do
-    sha256 arm64_big_sur: "2975be9af2cd9d330bcf85dc81f9f74b2f30d17972e987db3fd225212b0483ff"
-    sha256 monterey:      "dfa880a340b349ada0e41f391743c4a9ea15f012e89d29695ae3f2f8c6da24ef"
-    sha256 big_sur:       "128190a766b6a418d57718f597e33502b381ee1441b01edb35396fc486196665"
-    sha256 catalina:      "a57829f30757e2cd9092e0178505a8f6b6dce9f50f5b9fada78b0c1eb2cbd692"
-    sha256 mojave:        "bfac60d46d213913b7bccc3d6cdf998f487d080fef4a46c5608d20ab09a8b988"
-    sha256 x86_64_linux:  "d300cfd773adeb8071731a7cdc19a68be46732e17e95ffd6373844b8ae6c4e0d"
+    sha256 arm64_monterey: "2fbea44ef3130aff7733469b94bc24e75978bdb5cf8858e848a9bc23c1ddd97e"
+    sha256 arm64_big_sur:  "2975be9af2cd9d330bcf85dc81f9f74b2f30d17972e987db3fd225212b0483ff"
+    sha256 monterey:       "dfa880a340b349ada0e41f391743c4a9ea15f012e89d29695ae3f2f8c6da24ef"
+    sha256 big_sur:        "128190a766b6a418d57718f597e33502b381ee1441b01edb35396fc486196665"
+    sha256 catalina:       "a57829f30757e2cd9092e0178505a8f6b6dce9f50f5b9fada78b0c1eb2cbd692"
+    sha256 mojave:         "bfac60d46d213913b7bccc3d6cdf998f487d080fef4a46c5608d20ab09a8b988"
+    sha256 x86_64_linux:   "d300cfd773adeb8071731a7cdc19a68be46732e17e95ffd6373844b8ae6c4e0d"
   end
 
   depends_on "help2man" => :build
