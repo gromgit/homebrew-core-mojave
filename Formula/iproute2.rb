d12577a50734f2a858138ec3dfde6ecc3f02135e
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:02:59 +0800

    iproute2:

--- Formula/iproute2.rb
+++ Formula/iproute2.rb
@@ -1,16 +1,26 @@
 class Iproute2 < Formula
   desc "Linux routing utilities"
   homepage "https://wiki.linuxfoundation.org/networking/iproute2"
-  url "https://mirrors.edge.kernel.org/pub/linux/utils/net/iproute2/iproute2-5.15.0.tar.xz"
-  sha256 "38e3e4a5f9a7f5575c015027a10df097c149111eeb739993128e5b2b35b291ff"
+  url "https://mirrors.edge.kernel.org/pub/linux/utils/net/iproute2/iproute2-5.16.0.tar.xz"
+  sha256 "c064b66f6b001c2a35aa5224b5b1ac8aa4bee104d7dce30d6f10a84cb8b01e2f"
   license "GPL-2.0-only"
   head "https://git.kernel.org/pub/scm/network/iproute2/iproute2.git", branch: "main"
+
+  livecheck do
+    url "https://mirrors.edge.kernel.org/pub/linux/utils/net/iproute2/"
+    regex(/href=.*?iproute2[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "41e856e4a99e7cf11f1bec446042272fdd3bc676107d07ff8d36eedcec95f7dc"
+  end
 
   depends_on "bison" => :build
   depends_on "flex" => :build
   depends_on :linux
 
   def install
+    system "make"
     system "make", "install",
            "PREFIX=#{prefix}",
            "SBINDIR=#{sbin}",
