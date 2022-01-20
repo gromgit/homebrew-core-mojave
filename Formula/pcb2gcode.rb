bc1d8fda5ce05663e2dc2bc281e21ab8ed3a0808
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:03:16 +0800

    pcb2gcode: no rebuilding required

--- Formula/pcb2gcode.rb
+++ Formula/pcb2gcode.rb
@@ -7,11 +7,12 @@ class Pcb2gcode < Formula
   head "https://github.com/pcb2gcode/pcb2gcode.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "596ef4d44d9da58ce8ce77fc51605c9a1229a8b763e47f785baf202b0ba2a208"
-    sha256 cellar: :any, monterey:      "20d4b84b6d91188a05455d39e7bf5a75314b641e11f41a860dc65ce318d78381"
-    sha256 cellar: :any, big_sur:       "d08a9c3c499b9b9d510942cb8e7f6bae057eb29cadeab474ce0bbd13ae452d43"
-    sha256 cellar: :any, catalina:      "5872fbb4710e05c4ad1795ea8cbb75fe26bc031cd00041156efc30f9fe632101"
-    sha256 cellar: :any, mojave:        "73c321af8bd386af20ec60cf9e0c98600f12681b5bdd8bac70504457ebcc8bad"
+    sha256 cellar: :any, arm64_monterey: "4facc148f0dd1ed67cb098258f40210f13431239f36dd859a3605d8aba62a663"
+    sha256 cellar: :any, arm64_big_sur:  "596ef4d44d9da58ce8ce77fc51605c9a1229a8b763e47f785baf202b0ba2a208"
+    sha256 cellar: :any, monterey:       "20d4b84b6d91188a05455d39e7bf5a75314b641e11f41a860dc65ce318d78381"
+    sha256 cellar: :any, big_sur:        "d08a9c3c499b9b9d510942cb8e7f6bae057eb29cadeab474ce0bbd13ae452d43"
+    sha256 cellar: :any, catalina:       "5872fbb4710e05c4ad1795ea8cbb75fe26bc031cd00041156efc30f9fe632101"
+    sha256 cellar: :any, mojave:         "73c321af8bd386af20ec60cf9e0c98600f12681b5bdd8bac70504457ebcc8bad"
   end
 
   # Release 2.0.0 doesn't include an autoreconfed tarball
