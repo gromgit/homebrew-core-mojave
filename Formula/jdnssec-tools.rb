9fc0fcc475a1a2e966b5f4ad838cf7020df5c923
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:02:59 +0800

    jdnssec-tools: no rebuilding required

--- Formula/jdnssec-tools.rb
+++ Formula/jdnssec-tools.rb
@@ -5,7 +5,7 @@ class JdnssecTools < Formula
   sha256 "1d4905652639b8b23084366eb2e2b33d5f534bf29fbf9b4becbf9e29f9b39fdf"
   license "LGPL-2.1"
   revision 1
-  head "https://github.com/dblacka/jdnssec-tools.git"
+  head "https://github.com/dblacka/jdnssec-tools.git", branch: "master"
 
   livecheck do
     url :stable
@@ -15,6 +15,7 @@ class JdnssecTools < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "069e93f9f47a8553de4bcbe13dbc64082905b2e4e625093a0cb6456a6743ecee"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "24738bb6c20a997b7cf23b1dd63e559d339fd1765998c5d65974b9f21775b5d7"
+    sha256 cellar: :any_skip_relocation, monterey:       "3e8de754897fd352966a4978f775205a0c8144406cd6ae5be56ef96f1afdd96d"
     sha256 cellar: :any_skip_relocation, big_sur:        "4341a864f01748c3009213510cc983ef29e354c0e486f14c3fd453ad55ac6802"
     sha256 cellar: :any_skip_relocation, catalina:       "c12eafadb12264e88ef14fe4e93cdb41f0afccbb24b8cff892e8747d8ad2d73b"
     sha256 cellar: :any_skip_relocation, mojave:         "c12eafadb12264e88ef14fe4e93cdb41f0afccbb24b8cff892e8747d8ad2d73b"
