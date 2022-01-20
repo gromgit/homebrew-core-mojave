1bfddb19a7c555f740750986778ebe5a6fc2e95f
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:02:44 +0800

    digdag: no rebuilding required

--- Formula/digdag.rb
+++ Formula/digdag.rb
@@ -1,8 +1,8 @@
 class Digdag < Formula
   desc "Workload Automation System"
   homepage "https://www.digdag.io/"
-  url "https://dl.digdag.io/digdag-0.10.3.jar"
-  sha256 "200911b6a35a3d8b40e25f028753fb9652aeb19dbd05b8f950dc35ff69547c34"
+  url "https://dl.digdag.io/digdag-0.10.4.jar"
+  sha256 "2020fa9395e74e14fdf4ada93bff0684dcea64f564788afd6a3ecc54e3bbd4bf"
   license "Apache-2.0"
 
   livecheck do
@@ -11,11 +11,9 @@ class Digdag < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "ca2ef842dc4f5b70a20896b7ba3fbeb34d14537b5dfc081ebe3e4c7150c04e8f"
+    sha256 cellar: :any_skip_relocation, all: "47b5fff24344ebadc086f1ea9421a2f96fe9816e115e568cabafdc94aabdbf4d"
   end
 
-  depends_on arch: :x86_64 # openjdk@8 is not supported on ARM
   depends_on "openjdk@8"
 
   def install
