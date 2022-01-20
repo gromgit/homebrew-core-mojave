41a9faa5abc710cfce0f2a0f1bf207a34ef41969
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:03:06 +0800

    libwbxml: no rebuilding required

--- Formula/libwbxml.rb
+++ Formula/libwbxml.rb
@@ -4,15 +4,16 @@ class Libwbxml < Formula
   url "https://github.com/libwbxml/libwbxml/archive/libwbxml-0.11.7.tar.gz"
   sha256 "35e2cf033066edebc0d96543c0bdde87273359e4f4e59291299d41e103bd6338"
   license "LGPL-2.1"
-  head "https://github.com/libwbxml/libwbxml.git"
+  head "https://github.com/libwbxml/libwbxml.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "79c3653734dc5a8fbd8707a17e085f8eb3d93367a51aa62f76da1d9ac5001ef2"
-    sha256 cellar: :any, monterey:      "7a26910220690ace39924a8cee2a44295491b4349daa3b35bd1ed11bac09a0d1"
-    sha256 cellar: :any, big_sur:       "65a96ce0682ac9e3cec8599ef84c52ce89446c001cb6d3751e3962ae62d3ed82"
-    sha256 cellar: :any, catalina:      "4adbd8447466f7d3cbad72d5aff2730a87539dacd0638180cd39a9eaee11e174"
-    sha256 cellar: :any, mojave:        "9077d1c9669a92c39590de8280678cbe3d50853e76d69fda6a476ba88d170845"
-    sha256 cellar: :any, high_sierra:   "051a666b16d73e92e4910f40559d2bb5681ae4b5028a7f86959ad5f6bdb4e55a"
+    sha256 cellar: :any, arm64_monterey: "729b58788cf91dea13972dc2321ea894aaaec6360b709c48feeff949c800c85a"
+    sha256 cellar: :any, arm64_big_sur:  "79c3653734dc5a8fbd8707a17e085f8eb3d93367a51aa62f76da1d9ac5001ef2"
+    sha256 cellar: :any, monterey:       "7a26910220690ace39924a8cee2a44295491b4349daa3b35bd1ed11bac09a0d1"
+    sha256 cellar: :any, big_sur:        "65a96ce0682ac9e3cec8599ef84c52ce89446c001cb6d3751e3962ae62d3ed82"
+    sha256 cellar: :any, catalina:       "4adbd8447466f7d3cbad72d5aff2730a87539dacd0638180cd39a9eaee11e174"
+    sha256 cellar: :any, mojave:         "9077d1c9669a92c39590de8280678cbe3d50853e76d69fda6a476ba88d170845"
+    sha256 cellar: :any, high_sierra:    "051a666b16d73e92e4910f40559d2bb5681ae4b5028a7f86959ad5f6bdb4e55a"
   end
 
   depends_on "cmake" => :build
