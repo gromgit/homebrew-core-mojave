d3bb472cbb3ebe4f4da185e9f815f9fee18bcaab
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:03:17 +0800

    pioneers: no rebuilding required

--- Formula/pioneers.rb
+++ Formula/pioneers.rb
@@ -5,12 +5,13 @@ class Pioneers < Formula
   sha256 "9a358d88548e3866e14c46c2707f66c98f8040a7857d47965e1ed9805aeb631d"
 
   bottle do
-    sha256 arm64_big_sur: "ef6e9c03dcc0c5d86ae0b08805352937c74d4ccaadbe4ac9aead56b5f7e0c7bb"
-    sha256 monterey:      "0e14d946b05d63aba6c15ad997bb4bc2998fe33c7293c14aec0763e8cfa89a99"
-    sha256 big_sur:       "b9297939922709fc05a78ee98b24091678bf4e434bea4abfc14b089fde6e9728"
-    sha256 catalina:      "c2462078e412d1e2b60108f317550690b28675da5a248de69856a558fa4e07d4"
-    sha256 mojave:        "47ca43b992b0590f90758d9eef7894361c510dce5279a602decdf8ec019086f4"
-    sha256 high_sierra:   "fdd30d2f45b5b1f701b2f358c09a79ce04cea9793383f959811141617f3a4fc0"
+    sha256 arm64_monterey: "3742eced28912e519fc675ba5ccbff4ad9e324e73994e785b9aceb78fe61f6f4"
+    sha256 arm64_big_sur:  "ef6e9c03dcc0c5d86ae0b08805352937c74d4ccaadbe4ac9aead56b5f7e0c7bb"
+    sha256 monterey:       "0e14d946b05d63aba6c15ad997bb4bc2998fe33c7293c14aec0763e8cfa89a99"
+    sha256 big_sur:        "b9297939922709fc05a78ee98b24091678bf4e434bea4abfc14b089fde6e9728"
+    sha256 catalina:       "c2462078e412d1e2b60108f317550690b28675da5a248de69856a558fa4e07d4"
+    sha256 mojave:         "47ca43b992b0590f90758d9eef7894361c510dce5279a602decdf8ec019086f4"
+    sha256 high_sierra:    "fdd30d2f45b5b1f701b2f358c09a79ce04cea9793383f959811141617f3a4fc0"
   end
 
   depends_on "intltool" => :build
