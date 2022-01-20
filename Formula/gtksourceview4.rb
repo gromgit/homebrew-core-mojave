2f0bf1851f8d91be5136b07a967fda50cc352060
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:02:56 +0800

    gtksourceview4: no rebuilding required

--- Formula/gtksourceview4.rb
+++ Formula/gtksourceview4.rb
@@ -11,12 +11,13 @@ class Gtksourceview4 < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "dbac09cb14c7566766fdeaf867d322e932142816efa5fc1f01a2a7d5792928ee"
-    sha256 monterey:      "ec1d0e3f6c5f9b2a83da4a9ec3016225bb4ffedb0619c262697b352d957bee7c"
-    sha256 big_sur:       "b714a2eaa1ad26b3aaf2b5169b85cd80ad890256886db2a3955d1d815dc9e7c3"
-    sha256 catalina:      "c48c3be2dc1bd3da0caf35b01a8238f53cc3c0d003f6ec4f845591f206041310"
-    sha256 mojave:        "73140e09740766172fba3e3184abe9a8be1358061b4ac2714995f183788e2e9b"
-    sha256 x86_64_linux:  "c28a65e0177c6ff90db9358eeb67a5f0daa24d109c905929926f56f726cf9891"
+    sha256 arm64_monterey: "9eedfbd4dedec2e06b813d4a09a3bfef0160d7e72d2fa6f96dabe06325af9447"
+    sha256 arm64_big_sur:  "dbac09cb14c7566766fdeaf867d322e932142816efa5fc1f01a2a7d5792928ee"
+    sha256 monterey:       "ec1d0e3f6c5f9b2a83da4a9ec3016225bb4ffedb0619c262697b352d957bee7c"
+    sha256 big_sur:        "b714a2eaa1ad26b3aaf2b5169b85cd80ad890256886db2a3955d1d815dc9e7c3"
+    sha256 catalina:       "c48c3be2dc1bd3da0caf35b01a8238f53cc3c0d003f6ec4f845591f206041310"
+    sha256 mojave:         "73140e09740766172fba3e3184abe9a8be1358061b4ac2714995f183788e2e9b"
+    sha256 x86_64_linux:   "c28a65e0177c6ff90db9358eeb67a5f0daa24d109c905929926f56f726cf9891"
   end
 
   depends_on "gobject-introspection" => :build
