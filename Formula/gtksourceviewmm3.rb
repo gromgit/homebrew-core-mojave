8d03053869c430e722b5a06a1c3838900b8a3465
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:02:56 +0800

    gtksourceviewmm3: no rebuilding required

--- Formula/gtksourceviewmm3.rb
+++ Formula/gtksourceviewmm3.rb
@@ -12,12 +12,13 @@ class Gtksourceviewmm3 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "787713d0f6802ea858aa728be88507a6d1b3adc6e3e648c34af059651393e83a"
-    sha256 cellar: :any,                 monterey:      "e3f82bddf2add81a84854eac842b29528e19175bcf473a5c29fdecb21702b49b"
-    sha256 cellar: :any,                 big_sur:       "e1cc731cb3f1d99a040da7719e8d91f325d7c00a46e19a10ffca8d1ec87991e8"
-    sha256 cellar: :any,                 catalina:      "2909f29ff9dce4266ba101d992bb8831487cd2084f467faad39198ce6923b729"
-    sha256 cellar: :any,                 mojave:        "548c9dfe0eb1fbbe8bf9234a3d631bc232514855457372633f37bf240e427a5d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d6de37b2284288c47e137939d4ff2eea5e235cc8967fddf00e227ed87b28855d"
+    sha256 cellar: :any,                 arm64_monterey: "0f6143ae59613281896b8e61962728fa445e0f01238be952dfeb8402e8de6796"
+    sha256 cellar: :any,                 arm64_big_sur:  "787713d0f6802ea858aa728be88507a6d1b3adc6e3e648c34af059651393e83a"
+    sha256 cellar: :any,                 monterey:       "e3f82bddf2add81a84854eac842b29528e19175bcf473a5c29fdecb21702b49b"
+    sha256 cellar: :any,                 big_sur:        "e1cc731cb3f1d99a040da7719e8d91f325d7c00a46e19a10ffca8d1ec87991e8"
+    sha256 cellar: :any,                 catalina:       "2909f29ff9dce4266ba101d992bb8831487cd2084f467faad39198ce6923b729"
+    sha256 cellar: :any,                 mojave:         "548c9dfe0eb1fbbe8bf9234a3d631bc232514855457372633f37bf240e427a5d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d6de37b2284288c47e137939d4ff2eea5e235cc8967fddf00e227ed87b28855d"
   end
 
   depends_on "pkg-config" => [:build, :test]
