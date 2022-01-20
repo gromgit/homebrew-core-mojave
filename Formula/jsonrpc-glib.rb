ef8b6561d9b46e46704fe1b030dfa9958e615759
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:03:00 +0800

    jsonrpc-glib: no rebuilding required

--- Formula/jsonrpc-glib.rb
+++ Formula/jsonrpc-glib.rb
@@ -6,12 +6,13 @@ class JsonrpcGlib < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "69e3e97a9a4193dd6f3a23b11166ea27cb99760931244f6a391a228bc4eca196"
-    sha256 cellar: :any,                 monterey:      "f25b784b62fa80160800aad053d3b1bf13267135cc0cf56908fa0142075580e2"
-    sha256 cellar: :any,                 big_sur:       "ec54de6bc6799ae0b497447de9d3dfe5b4f88ce71d3a826da43785affae68429"
-    sha256 cellar: :any,                 catalina:      "ea6e41d579c3e4d3dc33355b3de46277d6605822a78f2f395e329329155a6bf7"
-    sha256 cellar: :any,                 mojave:        "ae9d2d930e0b55dcd99e9ed1188d5a4a7adf4523851feb1fb1a01ff15d9de575"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "50fbcd7be2fa9c83aa93fd742e8c678998494c03c27223d47fafb50fbe0f1fe0"
+    sha256 cellar: :any,                 arm64_monterey: "b840c82daeec35544e1eddb0481a5a6300f867e853859b557c2cb3f0d4e19ad3"
+    sha256 cellar: :any,                 arm64_big_sur:  "69e3e97a9a4193dd6f3a23b11166ea27cb99760931244f6a391a228bc4eca196"
+    sha256 cellar: :any,                 monterey:       "f25b784b62fa80160800aad053d3b1bf13267135cc0cf56908fa0142075580e2"
+    sha256 cellar: :any,                 big_sur:        "ec54de6bc6799ae0b497447de9d3dfe5b4f88ce71d3a826da43785affae68429"
+    sha256 cellar: :any,                 catalina:       "ea6e41d579c3e4d3dc33355b3de46277d6605822a78f2f395e329329155a6bf7"
+    sha256 cellar: :any,                 mojave:         "ae9d2d930e0b55dcd99e9ed1188d5a4a7adf4523851feb1fb1a01ff15d9de575"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "50fbcd7be2fa9c83aa93fd742e8c678998494c03c27223d47fafb50fbe0f1fe0"
   end
 
   depends_on "gobject-introspection" => :build
