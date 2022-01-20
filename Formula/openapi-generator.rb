e52c50b4b5d213ec1a2e9fc21528e5096bcf2549
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:03:12 +0800

    openapi-generator: no rebuilding required

--- Formula/openapi-generator.rb
+++ Formula/openapi-generator.rb
@@ -1,16 +1,21 @@
 class OpenapiGenerator < Formula
   desc "Generate clients, server & docs from an OpenAPI spec (v2, v3)"
   homepage "https://openapi-generator.tech/"
-  url "https://search.maven.org/remotecontent?filepath=org/openapitools/openapi-generator-cli/5.3.0/openapi-generator-cli-5.3.0.jar"
-  sha256 "f29d9d715a0d67cf1457d918ae7ed33f02d7fb2730c018d246a8d4a93d5ba7e1"
+  url "https://search.maven.org/remotecontent?filepath=org/openapitools/openapi-generator-cli/5.3.1/openapi-generator-cli-5.3.1.jar"
+  sha256 "81dc1b3fb1102b43becd2f46098cfd861e9c4e84fbebb534ad67d7c6b0eeff77"
   license "Apache-2.0"
 
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=org/openapitools/openapi-generator-cli/maven-metadata.xml"
+    regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
+  end
+
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "c53d29e0fd67ec7af1423e14cbf230efdc86d4251711179579500a97d6a97eab"
+    sha256 cellar: :any_skip_relocation, all: "ead0082bf72df72f7349393f1733daaeff4f52a1d1910384fbfe2004fff4add3"
   end
 
   head do
-    url "https://github.com/OpenAPITools/openapi-generator.git"
+    url "https://github.com/OpenAPITools/openapi-generator.git", branch: "master"
 
     depends_on "maven" => :build
   end
