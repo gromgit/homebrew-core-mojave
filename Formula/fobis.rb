9176421ba014153e7d6be6f78cb50ae05ce6bbd4
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:02:48 +0800

    fobis: no rebuilding required

--- Formula/fobis.rb
+++ Formula/fobis.rb
@@ -9,13 +9,14 @@ class Fobis < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bfa1e5a5e7276be1292321294c418e9f1bac7a964f46d227a2d81fc3ad1b984e"
-    sha256 cellar: :any_skip_relocation, monterey:      "5f9140bb6e751bdd01b64abb3a3d181b9f63ff576fdea3a86efb30fa723567b2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "627133905b51d5436b9eb6a8e75080d847a6ea7c36886aa7f81e053fd89970d3"
-    sha256 cellar: :any_skip_relocation, catalina:      "0a1685a770c843092bdcd918de2439bc9ed16e75b49e61e93148386b42d326d1"
-    sha256 cellar: :any_skip_relocation, mojave:        "6b517fdd37dbbdfce2e3ca628afd4f92ef688c18939e78c2b0487dbc1ac7da5e"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "81a5206bdd09bf9b630a9ef4793015283891d88b1790dc8638e759679d892522"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "43e2e24bb2e1d5234fe148102d25b910e1a303d3ac9a71fa684ca9c993506b0d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3b5eb128ceb7ae2931ffc4b3d9559899471bd76517c008a105ffb3abdfcbfd51"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bfa1e5a5e7276be1292321294c418e9f1bac7a964f46d227a2d81fc3ad1b984e"
+    sha256 cellar: :any_skip_relocation, monterey:       "5f9140bb6e751bdd01b64abb3a3d181b9f63ff576fdea3a86efb30fa723567b2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "627133905b51d5436b9eb6a8e75080d847a6ea7c36886aa7f81e053fd89970d3"
+    sha256 cellar: :any_skip_relocation, catalina:       "0a1685a770c843092bdcd918de2439bc9ed16e75b49e61e93148386b42d326d1"
+    sha256 cellar: :any_skip_relocation, mojave:         "6b517fdd37dbbdfce2e3ca628afd4f92ef688c18939e78c2b0487dbc1ac7da5e"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "81a5206bdd09bf9b630a9ef4793015283891d88b1790dc8638e759679d892522"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "43e2e24bb2e1d5234fe148102d25b910e1a303d3ac9a71fa684ca9c993506b0d"
   end
 
   depends_on "gcc" # for gfortran
