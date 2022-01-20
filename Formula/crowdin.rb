456494846cafff9f5da78f7c1d2d7a997ff76d8a
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:02:41 +0800

    crowdin: no rebuilding required

--- Formula/crowdin.rb
+++ Formula/crowdin.rb
@@ -1,17 +1,17 @@
 class Crowdin < Formula
   desc "Command-line tool that allows to manage your resources with crowdin.com"
   homepage "https://support.crowdin.com/cli-tool/"
-  url "https://github.com/crowdin/crowdin-cli/releases/download/3.7.6/crowdin-cli.zip"
-  sha256 "5bcd848da0a2167b4197045592d64dac67b98e300cab38570db564724eaeb24b"
+  url "https://github.com/crowdin/crowdin-cli/releases/download/3.7.7/crowdin-cli.zip"
+  sha256 "f580d6b939e25de4ebf6decbcd33c19634ab80dc3755e66c90545395e928313d"
   license "MIT"
 
   livecheck do
-    url "https://github.com/crowdin/crowdin-cli.git"
+    url :stable
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "7ec05c40b57e5a0f9779e462f6b77d81d2616323c01dff7ee18c2cf5173aae55"
+    sha256 cellar: :any_skip_relocation, all: "dc08ebf34f5db3d2015d111047eaceacbccbf5648f4d251a3ee15dcfdd7a29a1"
   end
 
   depends_on "openjdk"
