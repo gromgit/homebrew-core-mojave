df1450d585e97777905e1aaf0769a96313a56a8f
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:02:53 +0800

    gpredict: no rebuilding required

--- Formula/gpredict.rb
+++ Formula/gpredict.rb
@@ -12,14 +12,15 @@ class Gpredict < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "2c367d6266bd0af3583827c588ab864c26043444ad6b6379821c1b93e5093352"
-    sha256 monterey:      "650854e63dd2ed7f88d40188575d69f5e83e311ebecfc489c13c472adfd5e947"
-    sha256 big_sur:       "eccf4afd811d590ed5c930840933905bd5b1ea9bdf42e32e52cf4926d0c1eb05"
-    sha256 catalina:      "99fff9473dcc5eaa0c58cf0b2bf04f4240e1598aada45565e4dbbf050d2ac7dc"
-    sha256 mojave:        "952941a2ecdb5f75805888dfd020acce48c4f1b29a9c2e3ec8742d35fcd9c829"
-    sha256 high_sierra:   "189249444c490bc7984506a3d041de1d057fff671ff774871f549f6b32efa042"
-    sha256 sierra:        "9a0a4b0e63b3b1f84830f508d60ee3fc5b5fd0b9a5731241873168baa88209cf"
-    sha256 x86_64_linux:  "e87f34490c8549a80627a714275d1ab1f9b73285ea76f28c91b6f45a360963bc"
+    sha256 arm64_monterey: "f873648c8df2b0e87c6b2c6e7553379ca0e239738a01f83a702788f33ba0dcc9"
+    sha256 arm64_big_sur:  "2c367d6266bd0af3583827c588ab864c26043444ad6b6379821c1b93e5093352"
+    sha256 monterey:       "650854e63dd2ed7f88d40188575d69f5e83e311ebecfc489c13c472adfd5e947"
+    sha256 big_sur:        "eccf4afd811d590ed5c930840933905bd5b1ea9bdf42e32e52cf4926d0c1eb05"
+    sha256 catalina:       "99fff9473dcc5eaa0c58cf0b2bf04f4240e1598aada45565e4dbbf050d2ac7dc"
+    sha256 mojave:         "952941a2ecdb5f75805888dfd020acce48c4f1b29a9c2e3ec8742d35fcd9c829"
+    sha256 high_sierra:    "189249444c490bc7984506a3d041de1d057fff671ff774871f549f6b32efa042"
+    sha256 sierra:         "9a0a4b0e63b3b1f84830f508d60ee3fc5b5fd0b9a5731241873168baa88209cf"
+    sha256 x86_64_linux:   "e87f34490c8549a80627a714275d1ab1f9b73285ea76f28c91b6f45a360963bc"
   end
 
   depends_on "intltool" => :build
