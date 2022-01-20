6e3edbe608206be016b6a7f66cbac7d117ec74a1
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:02:52 +0800

    gnome-latex: no rebuilding required

--- Formula/gnome-latex.rb
+++ Formula/gnome-latex.rb
@@ -7,10 +7,12 @@ class GnomeLatex < Formula
   revision 1
 
   bottle do
-    sha256 arm64_big_sur: "118ac80bf869460f820e12a63bf8808b7ad9158eeb3ee594e494e344d54cc97c"
-    sha256 big_sur:       "91916490eae6b8b5e9c8717ea9a37a2e8e383c6504a3d59c7d4f209d2f2e5db0"
-    sha256 catalina:      "34723bd50c23bc34d54750606f99247544a222f8d32ee1017422a618e7d8255c"
-    sha256 mojave:        "282a45a8580c354c10f112895d0448cc97e206da12460f75b6dbcc8906401314"
+    sha256 arm64_monterey: "6c25d1dc2043da0fd0618c57316b19e59fa7627c61ea01051a38384c1e6683ac"
+    sha256 arm64_big_sur:  "118ac80bf869460f820e12a63bf8808b7ad9158eeb3ee594e494e344d54cc97c"
+    sha256 monterey:       "ec4d6862103ee03a5198148a0ea22acb97c1da93ef91af2b793bddb339f4e9c5"
+    sha256 big_sur:        "91916490eae6b8b5e9c8717ea9a37a2e8e383c6504a3d59c7d4f209d2f2e5db0"
+    sha256 catalina:       "34723bd50c23bc34d54750606f99247544a222f8d32ee1017422a618e7d8255c"
+    sha256 mojave:         "282a45a8580c354c10f112895d0448cc97e206da12460f75b6dbcc8906401314"
   end
 
   # See: https://gitlab.gnome.org/Archive/gnome-latex
