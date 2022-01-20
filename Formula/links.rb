24505eacdc080e74f37876655b5d09e554d7aea7
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:03:06 +0800

    links: no rebuilding required

--- Formula/links.rb
+++ Formula/links.rb
@@ -11,12 +11,13 @@ class Links < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "b3e9ad1d3617acdbbc86edbc90f429083130a51eb6ea26b809774b8eaf39fa53"
-    sha256 cellar: :any,                 monterey:      "77261cf3aeedfe6b058734536d55b744b80bb52c1cf777a2d78497bd1f4c69e2"
-    sha256 cellar: :any,                 big_sur:       "9d7178901279a9ee80768056fa4f2b04676777d5ef2063c9f8a8c2b9f98a8906"
-    sha256 cellar: :any,                 catalina:      "1eaa9411f73c61b7a82ee8d16b94bd7c8c0dde86e1eec953c5ce5872394ee073"
-    sha256 cellar: :any,                 mojave:        "c12924230d58173c263dcdd827ecb4d9f44bb1e654a191ff5882a46330125cdf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f6bbd8bae1431f150cc3153f2731cf41cf4fbf06b59033a28b5ac8b72906a792"
+    sha256 cellar: :any,                 arm64_monterey: "5d69fd6ef3b865956b1975123671932ad4d0b9a9f0b662ed412519debe748809"
+    sha256 cellar: :any,                 arm64_big_sur:  "b3e9ad1d3617acdbbc86edbc90f429083130a51eb6ea26b809774b8eaf39fa53"
+    sha256 cellar: :any,                 monterey:       "77261cf3aeedfe6b058734536d55b744b80bb52c1cf777a2d78497bd1f4c69e2"
+    sha256 cellar: :any,                 big_sur:        "9d7178901279a9ee80768056fa4f2b04676777d5ef2063c9f8a8c2b9f98a8906"
+    sha256 cellar: :any,                 catalina:       "1eaa9411f73c61b7a82ee8d16b94bd7c8c0dde86e1eec953c5ce5872394ee073"
+    sha256 cellar: :any,                 mojave:         "c12924230d58173c263dcdd827ecb4d9f44bb1e654a191ff5882a46330125cdf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f6bbd8bae1431f150cc3153f2731cf41cf4fbf06b59033a28b5ac8b72906a792"
   end
 
   depends_on "pkg-config" => :build
