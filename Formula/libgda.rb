376d6e09103f0aba4138f458e0fadafbfbeb4044
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-21 00:03:03 +0800

    libgda: no rebuilding required

--- Formula/libgda.rb
+++ Formula/libgda.rb
@@ -6,11 +6,13 @@ class Libgda < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 arm64_big_sur: "993a414772b41e1f0b2cffe21f9af240dbcd7e2b6de5d62a0e51b89a8144e40a"
-    sha256 big_sur:       "1fd18afa48f013fcee08cadebf89c4bbb3e37444b591b16cd61e5848b93d6395"
-    sha256 catalina:      "83d65ccf6e92620dd833dd23d1a02880f020ab24a0a6ed2ab5cb1a5149a32c5b"
-    sha256 mojave:        "e48a5aea9d860765e58bcd756c8e81956974d4284189604755a63232fc13a806"
-    sha256 high_sierra:   "8c9a8133c1fd1c554f995c089b12cbe049d2a8a01ac31cb5e68c089857a200a1"
+    sha256 arm64_monterey: "31de7c27443d4f477cfefa7b7598311b98b1998368385ea7a0787644c30a3c6f"
+    sha256 arm64_big_sur:  "993a414772b41e1f0b2cffe21f9af240dbcd7e2b6de5d62a0e51b89a8144e40a"
+    sha256 monterey:       "13a192f90ad01f376f5cbc977308d91bb096f3132ec41bea14a4b961866bfc1d"
+    sha256 big_sur:        "1fd18afa48f013fcee08cadebf89c4bbb3e37444b591b16cd61e5848b93d6395"
+    sha256 catalina:       "83d65ccf6e92620dd833dd23d1a02880f020ab24a0a6ed2ab5cb1a5149a32c5b"
+    sha256 mojave:         "e48a5aea9d860765e58bcd756c8e81956974d4284189604755a63232fc13a806"
+    sha256 high_sierra:    "8c9a8133c1fd1c554f995c089b12cbe049d2a8a01ac31cb5e68c089857a200a1"
   end
 
   depends_on "gobject-introspection" => :build
@@ -23,6 +25,12 @@ class Libgda < Formula
   depends_on "libgee"
   depends_on "openssl@1.1"
   depends_on "readline"
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
 
   def install
     # this build uses the sqlite source code that comes with libgda,
