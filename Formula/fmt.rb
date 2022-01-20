f6c89525567dd22c040427e6321f0f357c6e0eac
Adrian Ho <the.gromgit@gmail.com>
2022-01-21 05:14:25 +0800

    fmt: update 8.1.1_1 bottle.

--- Formula/fmt.rb
+++ Formula/fmt.rb
@@ -4,13 +4,22 @@ class Fmt < Formula
   url "https://github.com/fmtlib/fmt/archive/8.1.1.tar.gz"
   sha256 "3d794d3cf67633b34b2771eb9f073bde87e846e0d395d254df7b211ef1ec7346"
   license "MIT"
+  revision 1
+  head "https://github.com/fmtlib/fmt.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fmt"
-    sha256 cellar: :any, mojave: "f492d0407af482271680c45acaa486fc9a37e3d3e3646477efad8796d9d73390"
+    sha256 cellar: :any, mojave: "724b9bedd41070b83aab669150b5d76ecef3c5f4629f67bd39a3030d47fac431"
   end
 
   depends_on "cmake" => :build
+
+  # Fix Watchman build.
+  # https://github.com/fmtlib/fmt/issues/2717
+  patch do
+    url "https://github.com/fmtlib/fmt/commit/8f8a1a02d5c5cb967d240feee3ffac00d66f22a2.patch?full_index=1"
+    sha256 "ac5d7a8f9eabd40e34f21b1e0034fbc4147008f13b7bf2314131239fb3a7bdab"
+  end
 
   def install
     system "cmake", ".", "-DBUILD_SHARED_LIBS=TRUE", *std_cmake_args
