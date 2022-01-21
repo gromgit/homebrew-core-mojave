78011871019cd13b615e2a128ba03def0ef23d80
Adrian Ho <the.gromgit@gmail.com>
2022-01-22 03:21:37 +0800

    gleam: update 0.19.0 bottle.

--- Formula/gleam.rb
+++ Formula/gleam.rb
@@ -1,18 +1,17 @@
 class Gleam < Formula
   desc "✨ A statically typed language for the Erlang VM"
   homepage "https://gleam.run"
-  url "https://github.com/gleam-lang/gleam/archive/v0.18.2.tar.gz"
-  sha256 "42e1592312660ab96ce7167e222f8eea00e8c38498cf6d30300355b69af270bb"
+  url "https://github.com/gleam-lang/gleam/archive/v0.19.0.tar.gz"
+  sha256 "4aeadce8a4ecb56f4c66190b8cc97702f7dfccf2bb4b9ffe18b2de2317f55d84"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gleam"
-    sha256 cellar: :any_skip_relocation, mojave: "b73288cdd8a907ebfd83ca8efc32267282d2940601d02e47db014eb329712627"
+    sha256 cellar: :any_skip_relocation, mojave: "c985a68b08474512e43a1f2e1be630c295559d05da5bf3ada5a233a96de65811"
   end
 
   depends_on "rust" => :build
   depends_on "erlang"
-  depends_on "rebar3"
 
   on_linux do
     depends_on "pkg-config" => :build
@@ -26,6 +25,6 @@ class Gleam < Formula
     Dir.chdir testpath
     system bin/"gleam", "new", "test_project"
     Dir.chdir "test_project"
-    system "rebar3", "eunit"
+    system "gleam", "test"
   end
 end
