c09c570c0e7b6f6a933ca9c72e32ab8c814a6434
Adrian Ho <the.gromgit@gmail.com>
2022-01-21 16:41:18 +0800

    btop: update 1.2.0 bottle.

--- Formula/btop.rb
+++ Formula/btop.rb
@@ -1,13 +1,14 @@
 class Btop < Formula
   desc "Resource monitor. C++ version and continuation of bashtop and bpytop"
   homepage "https://github.com/aristocratos/btop"
-  url "https://github.com/aristocratos/btop/archive/refs/tags/v1.1.4.tar.gz"
-  sha256 "f5e039ed235aaa24c7c7f8492043b6a251913ce8db688defc46aa83091d40ba1"
+  url "https://github.com/aristocratos/btop/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "59a87b9d0bb0f5010d53f0ac72ddee9fd7b5a4039bce51b94b262313e946df02"
   license "Apache-2.0"
+  head "https://github.com/aristocratos/btop.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/btop"
-    sha256 cellar: :any, mojave: "c6e22552ca41b3cd4ef7b17f09b0527db823c1e46f705153badc2cc9203a5966"
+    sha256 cellar: :any, mojave: "054cf00ea12bc6f7ee9513ce98bde13cf0a029f656ac768bbaf7a52c91294a76"
   end
 
   depends_on "coreutils" => :build
