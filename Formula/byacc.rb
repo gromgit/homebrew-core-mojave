ed889f8853f90eb8e6454ceeeba038c24c2f108a
Adrian Ho <the.gromgit@gmail.com>
2022-01-21 16:53:57 +0800

    byacc: update 20220114 bottle.

--- Formula/byacc.rb
+++ Formula/byacc.rb
@@ -1,8 +1,8 @@
 class Byacc < Formula
   desc "(Arguably) the best yacc variant"
   homepage "https://invisible-island.net/byacc/"
-  url "https://invisible-mirror.net/archives/byacc/byacc-20220109.tgz"
-  sha256 "98966bc5e6558f5ee50c7b33ee3e0a75efc15dd99cc96739d1ac1af9c1a43535"
+  url "https://invisible-mirror.net/archives/byacc/byacc-20220114.tgz"
+  sha256 "d131475970018ab03531ce1bac21a8deba0eea23a4ecc051b54c850dad69e479"
   license :public_domain
 
   livecheck do
@@ -12,7 +12,7 @@ class Byacc < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/byacc"
-    sha256 cellar: :any_skip_relocation, mojave: "cd4251bf40626decc9d21b2048c6841636b376bf5a0d0cf2f4813e81749dd429"
+    sha256 cellar: :any_skip_relocation, mojave: "a7d42651acdf929a33a9f50a83fda1dc5481e9f73b7060a5feeb116f1599056d"
   end
 
   def install
