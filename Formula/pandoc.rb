d3c6f5e0febb142bdf2e90f0e673f5d7ee8263d6
Adrian Ho <the.gromgit@gmail.com>
2022-01-21 08:46:49 +0800

    pandoc: update 2.17.0.1 bottle.

--- Formula/pandoc.rb
+++ Formula/pandoc.rb
@@ -1,15 +1,14 @@
 class Pandoc < Formula
   desc "Swiss-army knife of markup format conversion"
   homepage "https://pandoc.org/"
-  url "https://hackage.haskell.org/package/pandoc-2.16.2/pandoc-2.16.2.tar.gz"
-  sha256 "72f291c5b9642309ff1010eed2c990d0893923e52982f7864c710111cb26dc5e"
+  url "https://hackage.haskell.org/package/pandoc-2.17.0.1/pandoc-2.17.0.1.tar.gz"
+  sha256 "276467f08335d495340d2ec439a1734101068f690c8b53f16c3d736e39df05f6"
   license "GPL-2.0-or-later"
   head "https://github.com/jgm/pandoc.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pandoc"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "ffcb6bd1b940de641382f6adefb557898724601a0cabc8ed96edba27b054ba8a"
+    sha256 cellar: :any_skip_relocation, mojave: "92c5a3deb1cf7caaa4de464670c8685b52e02fbe9121fbda43ec9bd8d3b6558b"
   end
 
   depends_on "cabal-install" => :build
@@ -33,7 +32,8 @@ class Pandoc < Formula
     EOS
     expected_html = <<~EOS
       <h1 id="homebrew">Homebrew</h1>
-      <p>A package manager for humans. Cats should take a look at Tigerbrew.</p>
+      <p>A package manager for humans. Cats should take a look at
+      Tigerbrew.</p>
     EOS
     assert_equal expected_html, pipe_output("#{bin}/pandoc -f markdown -t html5", input_markdown)
   end
