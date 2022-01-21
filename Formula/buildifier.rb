03efdf9a89da053ce9c8bab31ec198c3ac63f908
Adrian Ho <the.gromgit@gmail.com>
2022-01-21 16:44:59 +0800

    buildifier: update 4.2.5 bottle.

--- Formula/buildifier.rb
+++ Formula/buildifier.rb
@@ -1,15 +1,14 @@
 class Buildifier < Formula
   desc "Format bazel BUILD files with a standard convention"
   homepage "https://github.com/bazelbuild/buildtools"
-  url "https://github.com/bazelbuild/buildtools/archive/4.2.3.tar.gz"
-  sha256 "614c84128ddb86aab4e1f25ba2e027d32fd5c6da302ae30685b9d7973b13da1b"
+  url "https://github.com/bazelbuild/buildtools/archive/4.2.5.tar.gz"
+  sha256 "d368c47bbfc055010f118efb2962987475418737e901f7782d2a966d1dc80296"
   license "Apache-2.0"
   head "https://github.com/bazelbuild/buildtools.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/buildifier"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "64fc1b4cc66b6613451e002b2a738840a6c4df51441c6c559e9a245db26c87af"
+    sha256 cellar: :any_skip_relocation, mojave: "78f2568aa630f210e94afeb553071d100128d97fdbc372a6a8755caa34fef62e"
   end
 
   depends_on "go" => :build
