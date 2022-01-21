a24cba32fe71b059760e978b15a6d27fc07eeca1
Adrian Ho <the.gromgit@gmail.com>
2022-01-21 20:42:16 +0800

    cweb: update 4.6.1 bottle.

--- Formula/cweb.rb
+++ Formula/cweb.rb
@@ -1,8 +1,10 @@
 class Cweb < Formula
   desc "Literate documentation system for C, C++, and Java"
   homepage "https://cs.stanford.edu/~knuth/cweb.html"
-  url "https://github.com/ascherer/cweb/archive/cweb-4.6.tar.gz"
-  sha256 "9b9d47d1c93616b07b2aa0ebbc31b928990ebd5328dfa68040c73b4c39f93729"
+  url "https://github.com/ascherer/cweb/archive/cweb-4.6.1.tar.gz"
+  sha256 "9a6558bce53e668e8474bf51990060237a521e27603629b7eb4c5f6788be8155"
+  # See disucssions in this thread, https://github.com/ascherer/cweb/issues/29
+  license :cannot_represent
 
   livecheck do
     url :stable
@@ -11,7 +13,7 @@ class Cweb < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cweb"
-    sha256 mojave: "7d125a94309e9af5f744f21b2dd5c39dc213da24d9fe3ee269b320fd25194c47"
+    sha256 mojave: "d813eee27c0d449ccd61f27b718fb1615053561d57c99acc77324c7035539eb3"
   end
 
   conflicts_with "texlive", because: "both install `cweb` binaries"
