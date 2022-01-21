d78573215b97121a2a0ab71fce684c1efa854ff6
Adrian Ho <the.gromgit@gmail.com>
2022-01-22 01:42:17 +0800

    freeipmi: update 1.6.9 bottle.

--- Formula/freeipmi.rb
+++ Formula/freeipmi.rb
@@ -1,18 +1,14 @@
 class Freeipmi < Formula
   desc "In-band and out-of-band IPMI (v1.5/2.0) software"
   homepage "https://www.gnu.org/software/freeipmi/"
-  url "https://ftp.gnu.org/gnu/freeipmi/freeipmi-1.6.8.tar.gz"
-  mirror "https://ftpmirror.gnu.org/freeipmi/freeipmi-1.6.8.tar.gz"
-  sha256 "4aa46a269ecc1bbff9412451f17b5408f64395e7dc45b713edf5eb5362700a71"
+  url "https://ftp.gnu.org/gnu/freeipmi/freeipmi-1.6.9.tar.gz"
+  mirror "https://ftpmirror.gnu.org/freeipmi/freeipmi-1.6.9.tar.gz"
+  sha256 "f25e1c35f3d0f1b5a99cc31ecc2353ca83ed46a15163842fba870127dc9c8206"
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 arm64_monterey: "92acf4bfe30602ff62420cb4b2c6824066553410bf2fd4fae5986f5e28bf178c"
-    sha256 arm64_big_sur:  "961064dca3e312d23150cb3b1fee87ef5903a3ea863404b04b90ae6fc3282d9a"
-    sha256 monterey:       "ce2d911b60b36dd247441befcc18553e322428c04222be0d4c1166368eb253a2"
-    sha256 big_sur:        "94211efde5f73e6eca23983c67514ca977be9ce821e4e6266a95c4423531348d"
-    sha256 catalina:       "5b6dd5d1c757fc344be9401e63ee1774658efc1eeb5486171161acafb60a5b89"
-    sha256 mojave:         "96bd9a3bde2379bfaf2b88b2de7e0e8c185c93a71f0b9cd864afd1d9d693bb77"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/freeipmi"
+    sha256 mojave: "8d0c1f5c5a4797c477dc6d5ed95c5f207d878ff1b768a7393d36b256d7803ca3"
   end
 
   depends_on "argp-standalone"
