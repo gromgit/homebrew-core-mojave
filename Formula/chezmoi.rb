6ac1504459cd30608c7c9e90d399c466bfc93976
Adrian Ho <the.gromgit@gmail.com>
2022-01-21 18:51:18 +0800

    chezmoi: update 2.10.0 bottle.

--- Formula/chezmoi.rb
+++ Formula/chezmoi.rb
@@ -2,14 +2,14 @@ class Chezmoi < Formula
   desc "Manage your dotfiles across multiple diverse machines, securely"
   homepage "https://chezmoi.io/"
   url "https://github.com/twpayne/chezmoi.git",
-      tag:      "v2.9.5",
-      revision: "af9290311e64e9f34b090c350be84070d1845863"
+      tag:      "v2.10.0",
+      revision: "66837906121278eab501dc91b0d9fa6b1da2a501"
   license "MIT"
   head "https://github.com/twpayne/chezmoi.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/chezmoi"
-    sha256 cellar: :any_skip_relocation, mojave: "49e1d042c7fcfc2d03f9348870730a99192926d449d758eae67f83c4010dc5d1"
+    sha256 cellar: :any_skip_relocation, mojave: "e8fceeedc819c8ebf3e1716deaae3ae3a96e68a5393f8e53f8f602535479d636"
   end
 
   depends_on "go" => :build
