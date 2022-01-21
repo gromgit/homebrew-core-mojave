8e81cf9096f3ab4f9feccd24b33274cf483ed966
Adrian Ho <the.gromgit@gmail.com>
2022-01-21 20:43:53 +0800

    darkstat: update 3.0.721 bottle.

--- Formula/darkstat.rb
+++ Formula/darkstat.rb
@@ -1,32 +1,18 @@
 class Darkstat < Formula
   desc "Network traffic analyzer"
   homepage "https://unix4lyfe.org/darkstat/"
-  url "https://unix4lyfe.org/darkstat/darkstat-3.0.719.tar.bz2"
-  sha256 "aeaf909585f7f43dc032a75328fdb62114e58405b06a92a13c0d3653236dedd7"
-
-  livecheck do
-    url :homepage
-    regex(/href=.*?darkstat[._-]v?(\d+(?:\.\d+)+)\.t/i)
-  end
+  url "https://github.com/emikulic/darkstat/archive/3.0.721.tar.gz"
+  sha256 "0b405a6c011240f577559d84db22684a6349b25067c3a800df12439783c25494"
+  license all_of: ["BSD-4-Clause-UC", "GPL-2.0-only", "GPL-3.0-or-later", "X11"]
+  head "https://github.com/emikulic/darkstat.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5d9d44eba247c8894ebdfe245d9d1c138ed8fde0044a3e76f0013f34ef6613e4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f118133e9435512691870171effd65220c2340328860a7b22ae32ad1ba3c369e"
-    sha256 cellar: :any_skip_relocation, monterey:       "0d9440ee8fc09322f360a818e5566eee64118cbedfad570f066acb6309cde07c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "384197a3882c62ebb9f346743561c6e747f9504e681e02ba57c4942439f6e459"
-    sha256 cellar: :any_skip_relocation, catalina:       "381e67870ea4f9f980421aa6f51580eedf98a916a09de042022744de1905798e"
-    sha256 cellar: :any_skip_relocation, mojave:         "edef4012fbd6b7626fbbf324ae92c34afcaba39f09a8ac7c7e80a3e25cef71c7"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "70eae96655d9872851ea02e44709155a6a9df509210fd2b49b0b72de334248f3"
-    sha256 cellar: :any_skip_relocation, sierra:         "680c9a1143b9a95990d246d7ba8357baa0ec1142545252d89734d98b8046337a"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "4e67244fc36d17dbdbe9ae33cc38bd79d2e016eeed0139c164d323e89b15c15e"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/darkstat"
+    sha256 cellar: :any_skip_relocation, mojave: "8bd8078770193769f3657a41f1ff640619f5b3b7b55b3a5e779e9650ce65cfff"
   end
 
-  head do
-    url "https://www.unix4lyfe.org/git/darkstat", using: :git
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-  end
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
 
   # Patch reported to upstream on 2017-10-08
   # Work around `redefinition of clockid_t` issue on 10.12 SDK or newer
@@ -36,7 +22,7 @@ class Darkstat < Formula
   end
 
   def install
-    system "autoreconf", "-iv" if build.head?
+    system "autoreconf", "-iv"
     system "./configure", "--disable-debug", "--prefix=#{prefix}"
     system "make", "install"
   end
