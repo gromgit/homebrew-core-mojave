aed3c71db62ee8559847062407d9d88b8f32c6eb
Adrian Ho <the.gromgit@gmail.com>
2022-01-21 19:41:58 +0800

    colima: update 0.3.2 bottle.

--- Formula/colima.rb
+++ Formula/colima.rb
@@ -2,8 +2,8 @@ class Colima < Formula
   desc "Container runtimes on MacOS with minimal setup"
   homepage "https://github.com/abiosoft/colima/blob/main/README.md"
   url "https://github.com/abiosoft/colima.git",
-      tag:      "v0.2.2",
-      revision: "b2c7697bee2d73e995f156fe8e9870eb246c07e6"
+      tag:      "v0.3.2",
+      revision: "272db4732b90390232ed9bdba955877f46a50552"
   license "MIT"
   head "https://github.com/abiosoft/colima.git", branch: "main"
 
@@ -13,12 +13,8 @@ class Colima < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9747ca65ca6be26b4263e1bce95c1a940701c536fd0f87b58832691f74881896"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "81d0c6180696757c3a29e4688128a31ef06b3f3ff6a45a7098e67ca5f2fa0e99"
-    sha256 cellar: :any_skip_relocation, monterey:       "79d53bbe6644ce662a7134c2846cc431731eb37c2cc0ac6fab7943f5fbca6d29"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f03962f3ac2eea17cb4a7c716a86c703a5f17da0ae76517ab8e3bac2002350bf"
-    sha256 cellar: :any_skip_relocation, catalina:       "6ed11d66cfc5537d24f1861bf764d88bfab103fd86b4278bc45c0f01e4ad27a0"
-    sha256 cellar: :any_skip_relocation, mojave:         "96faa5778cd2aaf10f5df4ec89c824f19573955add9cd3da68265d12688297fb"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/colima"
+    sha256 cellar: :any_skip_relocation, mojave: "f38544d83de922a665362af15d880090c9db807956b4afa06e72a097fb5832a9"
   end
 
   depends_on "go" => :build
