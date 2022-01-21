64fb515ed1820b1faa367a249896f240e9c1e1c4
Adrian Ho <the.gromgit@gmail.com>
2022-01-21 21:14:47 +0800

    dita-ot: update 3.7 bottle.

--- Formula/dita-ot.rb
+++ Formula/dita-ot.rb
@@ -1,8 +1,8 @@
 class DitaOt < Formula
   desc "DITA Open Toolkit is an implementation of the OASIS DITA specification"
   homepage "https://www.dita-ot.org/"
-  url "https://github.com/dita-ot/dita-ot/releases/download/3.6.1/dita-ot-3.6.1.zip"
-  sha256 "ebddd6152d44467a794bcfc5d28cf5effd005330677d6a8aafa5de00f250688b"
+  url "https://github.com/dita-ot/dita-ot/releases/download/3.7/dita-ot-3.7.zip"
+  sha256 "dbbc9f8dfea51300e707307b5d65e7945b9357d5fa2d484fbb9264268771abc7"
   license "Apache-2.0"
 
   livecheck do
@@ -11,13 +11,8 @@ class DitaOt < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bb3c63160256f78fbbb2bb3dccfb80a4577f5116a00c00128652e7ce96194b88"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bb3c63160256f78fbbb2bb3dccfb80a4577f5116a00c00128652e7ce96194b88"
-    sha256 cellar: :any_skip_relocation, monterey:       "d2962c76fd1ab8527a8ca065d73abe37cf0898b647368826e6d1f39e3e59cd5f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d2962c76fd1ab8527a8ca065d73abe37cf0898b647368826e6d1f39e3e59cd5f"
-    sha256 cellar: :any_skip_relocation, catalina:       "d2962c76fd1ab8527a8ca065d73abe37cf0898b647368826e6d1f39e3e59cd5f"
-    sha256 cellar: :any_skip_relocation, mojave:         "d2962c76fd1ab8527a8ca065d73abe37cf0898b647368826e6d1f39e3e59cd5f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bb3c63160256f78fbbb2bb3dccfb80a4577f5116a00c00128652e7ce96194b88"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dita-ot"
+    sha256 cellar: :any_skip_relocation, mojave: "2d09359ab97ecc2dafd95b1b429ccd272304f3871d2e257855d8be0076e4393e"
   end
 
   depends_on "openjdk"
