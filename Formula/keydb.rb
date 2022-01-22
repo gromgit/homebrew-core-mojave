2a1bc154db7afd8e1afeeeccbd1d9d06d507488e
Adrian Ho <the.gromgit@gmail.com>
2022-01-22 08:59:06 +0800

    keydb: update 6.2.2 bottle.

--- Formula/keydb.rb
+++ Formula/keydb.rb
@@ -1,26 +1,19 @@
 class Keydb < Formula
   desc "Multithreaded fork of Redis"
   homepage "https://keydb.dev"
-  url "https://github.com/EQ-Alpha/KeyDB/archive/v6.2.1.tar.gz"
-  sha256 "9376b5e14f317840cfd05fee06467e6ad7612e32da98bcb8991f5674d61d550b"
+  url "https://github.com/EQ-Alpha/KeyDB/archive/v6.2.2.tar.gz"
+  sha256 "e65eea13500c30c65f705121b67cffeb3551a1c7cc7d07b60fe6191acd4dec58"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/keydb"
-    sha256 cellar: :any_skip_relocation, mojave: "bac0d91b7fbf1a88d33262087a16a8a81d2b861e13c33ff4228d0611fbd229f1"
+    sha256 cellar: :any_skip_relocation, mojave: "b04a717ae1556fd4ae7373d9fcec99fc25c9380355fa2c0090ebfa6bd24e17b6"
   end
 
   uses_from_macos "curl"
 
   on_linux do
     depends_on "util-linux"
-  end
-
-  # Fix build on macOS (https://github.com/EQ-Alpha/KeyDB/issues/384)
-  # Patch accepted upstream, remove on next release
-  patch do
-    url "https://github.com/EQ-Alpha/KeyDB/commit/7a32ec39fdb738e9c3cd2b73ee18355ced793a65.patch?full_index=1"
-    sha256 "06d29f24ff08032f3c211f1c322ba50f64dda9d40fbbe914ce16d8553fd68870"
   end
 
   def install
