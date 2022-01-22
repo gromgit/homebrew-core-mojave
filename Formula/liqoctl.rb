5439f9a0bd6dab244fdf1c0a7fbca2d0546b4d88
Adrian Ho <the.gromgit@gmail.com>
2022-01-22 13:18:31 +0800

    liqoctl: update 0.3.2 bottle.

--- Formula/liqoctl.rb
+++ Formula/liqoctl.rb
@@ -1,14 +1,13 @@
 class Liqoctl < Formula
   desc "Is a CLI tool to install and manage Liqo-enabled clusters"
   homepage "https://liqo.io"
-  url "https://github.com/liqotech/liqo/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "4a89c95613d6a2083987aa29bf76eac0d42ae894e422e3995d039fe8c7b3b2d9"
+  url "https://github.com/liqotech/liqo/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "cfc0f20625be8fdb2c30653f7b4b9c00332c6f3247cf7da0ad73c220723f5098"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/liqoctl"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "2ac86d356e0bed1fa17077e4a36b66559ecb306e0c4127b3a5bceb451341b5fe"
+    sha256 cellar: :any_skip_relocation, mojave: "1823aa833fe178ec3cc374b0a6f5099340f1fd2efbca377c04da303924c7c79a"
   end
 
   depends_on "go" => :build
@@ -19,7 +18,7 @@ class Liqoctl < Formula
     ldflags = %W[
       -s -w
       -X github.com/liqotech/liqo/pkg/liqoctl/version.liqoctlVersion=v#{version}
-    ].join(" ")
+    ]
 
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/liqoctl"
 
@@ -36,7 +35,6 @@ class Liqoctl < Formula
     assert_match "liqoctl is a CLI tool to install and manage Liqo-enabled clusters.", run_output
     run_install_output = shell_output("#{bin}/liqoctl install kind 2>&1", 1)
     assert_match <<~EOS, run_install_output
-      Error: no configuration provided, please set the environment variable KUBECONFIG
       Error: no configuration provided, please set the environment variable KUBECONFIG
     EOS
     assert_match version.to_s, shell_output("#{bin}/liqoctl version")
