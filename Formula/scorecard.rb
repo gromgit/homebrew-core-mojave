1c759844cb1d52439393fdaa0074aa1cc29108ea
Adrian Ho <the.gromgit@gmail.com>
2022-01-24 02:59:47 +0800

    scorecard: update 4.0.1 bottle.

--- Formula/scorecard.rb
+++ Formula/scorecard.rb
@@ -1,14 +1,14 @@
 class Scorecard < Formula
   desc "Security health metrics for Open Source"
   homepage "https://github.com/ossf/scorecard"
-  url "https://github.com/ossf/scorecard/archive/v3.2.1.tar.gz"
-  sha256 "009183faea28c561f60f2c7bd84e477130d16e55d581bc354879c66e7123cdd2"
+  url "https://github.com/ossf/scorecard/archive/v4.0.1.tar.gz"
+  sha256 "8545d4aacaa2ffafbf545aeee7bccc381d2c00ffa0e4e9cb65190e68ece543d6"
   license "Apache-2.0"
   head "https://github.com/ossf/scorecard.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/scorecard"
-    sha256 cellar: :any_skip_relocation, mojave: "9e84da3033fe7efabed98fecc13931e320f79b10a9fdc1687b70b3f91eafb392"
+    sha256 cellar: :any_skip_relocation, mojave: "ed8fe04ea86b41a6df290e4d8c68a0d613dfc93083b14eb4bfd1d9744f6aeaa9"
   end
 
   depends_on "go" => :build
@@ -21,7 +21,7 @@ class Scorecard < Formula
 
   test do
     ENV["GITHUB_AUTH_TOKEN"] = "test"
-    output = shell_output("#{bin}/scorecard --repo=github.com/kubernetes/kubernetes --checks=Maintained 2>&1", 1)
+    output = shell_output("#{bin}/scorecard --repo=github.com/kubernetes/kubernetes --checks=Maintained 2>&1", 2)
     expected_output = "InitRepo: repo unreachable: GET https://api.github.com/repos/google/oss-fuzz: 401"
     assert_match expected_output, output
   end
