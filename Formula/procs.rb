d3951c3c38f28e906b113e50be07f6c9219a3b61
Adrian Ho <the.gromgit@gmail.com>
2022-01-23 22:32:46 +0800

    procs: update 0.12.0 bottle.

--- Formula/procs.rb
+++ Formula/procs.rb
@@ -1,13 +1,13 @@
 class Procs < Formula
   desc "Modern replacement for ps written by Rust"
   homepage "https://github.com/dalance/procs"
-  url "https://github.com/dalance/procs/archive/v0.11.12.tar.gz"
-  sha256 "ff1a293d84cc0160cc92b4530ee93cedf530796e80130df74b0490c3e884efb6"
+  url "https://github.com/dalance/procs/archive/v0.12.0.tar.gz"
+  sha256 "04dbb3ddc734a9d1305b26b546a913e3e04d556b5863212a2675d29268aebed3"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/procs"
-    sha256 cellar: :any_skip_relocation, mojave: "2d8417ad7ab72392b2e8fe7c8689b3de6c0b7e07d06d812d239a38c804ef0984"
+    sha256 cellar: :any_skip_relocation, mojave: "25ddc62c711b83ab49e75f43fc9832df5538db1a015a3ce6eba3454f4bfddcde"
   end
 
   depends_on "rust" => :build
