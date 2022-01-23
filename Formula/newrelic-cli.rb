f8d5a63f983116af1aeb3fad0814a6af326e8378
Adrian Ho <the.gromgit@gmail.com>
2022-01-23 14:05:15 +0800

    newrelic-cli: update 0.41.13 bottle.

--- Formula/newrelic-cli.rb
+++ Formula/newrelic-cli.rb
@@ -1,14 +1,14 @@
 class NewrelicCli < Formula
   desc "Command-line interface for New Relic"
   homepage "https://github.com/newrelic/newrelic-cli"
-  url "https://github.com/newrelic/newrelic-cli/archive/v0.41.11.tar.gz"
-  sha256 "32f207e92b848d1c92aef1190c5fe280e7618cdd75731d23f1a925c27874e8d1"
+  url "https://github.com/newrelic/newrelic-cli/archive/v0.41.13.tar.gz"
+  sha256 "83693cf8538c4c6dc4c51bbdf1b7d28c40e3841d2507d6449c4e1680ec81fcc5"
   license "Apache-2.0"
   head "https://github.com/newrelic/newrelic-cli.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/newrelic-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "79a2220f6bc6dad87e3cb6bc6b23cfc5fb867bc6887f31ccc16fc5eff6d4f36d"
+    sha256 cellar: :any_skip_relocation, mojave: "b2955326dfc2a411e6d5fbead38d5409b4279e05285b3cc915618d03a76c9649"
   end
 
   depends_on "go" => :build
