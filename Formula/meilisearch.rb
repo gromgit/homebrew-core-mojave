c1b36442d6542ac8642557e2483de84573b040ad
Adrian Ho <the.gromgit@gmail.com>
2022-01-23 10:57:50 +0800

    meilisearch: update 0.25.2 bottle.

--- Formula/meilisearch.rb
+++ Formula/meilisearch.rb
@@ -1,8 +1,8 @@
 class Meilisearch < Formula
   desc "Ultra relevant, instant and typo-tolerant full-text search API"
   homepage "https://docs.meilisearch.com/"
-  url "https://github.com/meilisearch/MeiliSearch/archive/v0.25.0.tar.gz"
-  sha256 "7c7e2caae99c7ec504eb25186c2a67832bc1cdf4572102b85807b9540709a7f8"
+  url "https://github.com/meilisearch/MeiliSearch/archive/v0.25.2.tar.gz"
+  sha256 "5097f0c6de042125561e669a1d9d09c4ee6425e9715fd0ad59dabeeef1b99ef8"
   license "MIT"
 
   livecheck do
@@ -12,7 +12,7 @@ class Meilisearch < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/meilisearch"
-    sha256 cellar: :any_skip_relocation, mojave: "388d600c39615d064508de9c39774983227f2333b47063341f395e159e9ffa4e"
+    sha256 cellar: :any_skip_relocation, mojave: "fa987455d4281c1396c8b009ec868550d56c07e2ed2689d0ae2188f54374da01"
   end
 
   depends_on "rust" => :build
