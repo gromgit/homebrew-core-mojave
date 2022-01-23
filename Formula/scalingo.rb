05f8668bcc975547fee7b077301cfebd5e9307a5
Adrian Ho <the.gromgit@gmail.com>
2022-01-24 02:55:32 +0800

    scalingo: update 1.22.1 bottle.

--- Formula/scalingo.rb
+++ Formula/scalingo.rb
@@ -1,19 +1,19 @@
 class Scalingo < Formula
   desc "CLI for working with Scalingo's PaaS"
   homepage "https://doc.scalingo.com/cli"
-  url "https://github.com/Scalingo/cli/archive/refs/tags/1.22.0.tar.gz"
-  sha256 "f08236d73195e9107f1a2d4aaf0d01bd40e852e8f032bc5d42227677f65ee4ec"
+  url "https://github.com/Scalingo/cli/archive/1.22.1.tar.gz"
+  sha256 "cef2766e41a21eadf8b6a00e467bfb2c9234373d5cc52a272100cefed99a2380"
   license "BSD-4-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/scalingo"
-    sha256 cellar: :any_skip_relocation, mojave: "922add5cd90acc94d90e1e1b79f876b366dc07bf4d15a3aa61822eac7442ec64"
+    sha256 cellar: :any_skip_relocation, mojave: "a54fa60fddc0b07cd2f8bbe9b4ccd26de70af05051516758a656e771b8fcd9d8"
   end
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args, "scalingo/main.go"
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "scalingo/main.go"
   end
 
   test do
@@ -24,7 +24,6 @@ class Scalingo < Formula
       | region            |       |
       +-------------------+-------+
     END
-    config_output = shell_output("#{bin}/scalingo config")
-    assert_equal config_output, expected
+    assert_equal expected, shell_output("#{bin}/scalingo config")
   end
 end
