55375491e5c3dd5cd6c4cb44ae872eb2b3b61f43
Adrian Ho <the.gromgit@gmail.com>
2022-01-23 13:32:58 +0800

    nats-server: update 2.7.0 bottle.

--- Formula/nats-server.rb
+++ Formula/nats-server.rb
@@ -1,20 +1,20 @@
 class NatsServer < Formula
   desc "Lightweight cloud messaging system"
   homepage "https://nats.io"
-  url "https://github.com/nats-io/nats-server/archive/refs/tags/v2.6.6.tar.gz"
-  sha256 "2f906b7dff38dc470ffcbba26632137f19282afb874cb173fc2dabf72359405f"
+  url "https://github.com/nats-io/nats-server/archive/refs/tags/v2.7.0.tar.gz"
+  sha256 "fed09a8466878bb50e441fe75745aeaee9c6d2f3fa8cddc29ff8fd15ed7fe03f"
   license "Apache-2.0"
   head "https://github.com/nats-io/nats-server.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nats-server"
-    sha256 cellar: :any_skip_relocation, mojave: "f9dfa7525577b630a1925a0e356da85a330e53f42aa3b20bb4b3ab3561e4209b"
+    sha256 cellar: :any_skip_relocation, mojave: "b8953ff2de194112229d3b798df0096407b1e98b24ec10e2400a6891d0b99092"
   end
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-ldflags", "-s -w", *std_go_args
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   service do
