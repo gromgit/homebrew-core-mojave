63ccb19fc484939c3842838b3bd3922463cb7064
Adrian Ho <the.gromgit@gmail.com>
2022-01-23 18:08:11 +0800

    ocrad: update 0.28 bottle.

--- Formula/ocrad.rb
+++ Formula/ocrad.rb
@@ -1,22 +1,17 @@
 class Ocrad < Formula
   desc "Optical character recognition (OCR) program"
   homepage "https://www.gnu.org/software/ocrad/"
-  url "https://ftp.gnu.org/gnu/ocrad/ocrad-0.27.tar.lz"
-  mirror "https://ftpmirror.gnu.org/ocrad/ocrad-0.27.tar.lz"
-  sha256 "a9bfe67e9a040907aff5640dca56392476b6a89e48e37dc94ba846c5b6733b36"
-  license "GPL-2.0"
+  url "https://ftp.gnu.org/gnu/ocrad/ocrad-0.28.tar.lz"
+  mirror "https://ftpmirror.gnu.org/ocrad/ocrad-0.28.tar.lz"
+  sha256 "34ccea576dbdadaa5979e6202344c3ff68737d829ca7b66f71c8497d36bbbf2e"
+  license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "899b2acf05936624225cb01993f3f993bf9e3cc73c7052051bed917defbd83e9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bfbe36f994bcbba6286b9bec6206e314d405a42ca222821648a6962b17a2aecb"
-    sha256 cellar: :any_skip_relocation, monterey:       "e0ec4ee19228ce64ba4da65f84a14bf7bd55e6f28b5d2584b7c49505c1abb51b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "59bfd9714c393b8910fdd9cbc337dfeaf431f78215820ffe47fe59841231e7f4"
-    sha256 cellar: :any_skip_relocation, catalina:       "6533cd452587714531d20b4aa74ea7fc1e323ff893c8a7c9729655ede1ec9df7"
-    sha256 cellar: :any_skip_relocation, mojave:         "3d1c85bb36faedf5ab12f78e8c3511dcc4164561ba8bc09924b48f6aa3fa0b37"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "ba9b30eeabc11634502e30fd9a730d5727668550f9708d46fbefc03bcb3917de"
-    sha256 cellar: :any_skip_relocation, sierra:         "903ce6530395c0973418020561ddd60da739f3a36e865500776922e18975460b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "834afd1cc5f7c095ded49166800e56137105f2777a89d877b1c9ca1bd6ad1779"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ocrad"
+    sha256 cellar: :any, mojave: "049659a50c759a4475465cc00aaa46a55ceab60aea3520d4d7d1943737626db1"
   end
+
+  depends_on "libpng"
 
   def install
     system "./configure", "--prefix=#{prefix}"
