3d435f86da070a3d387851d462890cfa5388fb6d
Adrian Ho <the.gromgit@gmail.com>
2022-01-24 07:12:25 +0800

    vite: update 2.7.13 bottle.

--- Formula/vite.rb
+++ Formula/vite.rb
@@ -3,13 +3,13 @@ require "language/node"
 class Vite < Formula
   desc "Next generation frontend tooling. It's fast!"
   homepage "https://vitejs.dev/"
-  url "https://registry.npmjs.org/vite/-/vite-2.7.10.tgz"
-  sha256 "1830eb886257d2e829ca45d79957ed4cbe4b739e9b6ae7dfa8fd83c14e0ca3f8"
+  url "https://registry.npmjs.org/vite/-/vite-2.7.13.tgz"
+  sha256 "225aed818f1e8b39e21cce15afad66b869c489182def46ebcb94e0456851c5ae"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/vite"
-    sha256 cellar: :any_skip_relocation, mojave: "d6e841001ae0a16837df2bba81fede13f8b30bbbc889127f690632434669d855"
+    sha256 cellar: :any_skip_relocation, mojave: "c6823c64f605bbb5053fea629b375f818006ef4874fde902d3284185dd265596"
   end
 
   depends_on "node"
