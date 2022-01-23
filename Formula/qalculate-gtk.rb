7e30a716a58c95edf6b82341cc0c447b25395166
Adrian Ho <the.gromgit@gmail.com>
2022-01-23 23:05:51 +0800

    qalculate-gtk: update 3.22.0 bottle.

--- Formula/qalculate-gtk.rb
+++ Formula/qalculate-gtk.rb
@@ -7,8 +7,8 @@ class QalculateGtk < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/qalculate-gtk"
-    rebuild 2
-    sha256 mojave: "12c6b2b22a91a9b41f6ce9a7970d3b7c087da01ed481d7a5baeac6695c21ddd6"
+    rebuild 3
+    sha256 mojave: "271488d5b79935853e0e729fde67ee0ec1185e4ffe38712551e762c9f46b73e9"
   end
 
   depends_on "intltool" => :build
