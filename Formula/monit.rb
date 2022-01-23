bc714e814d2ef934e7f59b14cca727b7a137f4ad
Adrian Ho <the.gromgit@gmail.com>
2022-01-23 12:34:47 +0800

    monit: update 5.30.0 bottle.

--- Formula/monit.rb
+++ Formula/monit.rb
@@ -1,8 +1,8 @@
 class Monit < Formula
   desc "Manage and monitor processes, files, directories, and devices"
   homepage "https://mmonit.com/monit/"
-  url "https://mmonit.com/monit/dist/monit-5.29.0.tar.gz"
-  sha256 "f665e6dd1f26a74b5682899a877934167de2b2582e048652ecf036318477885f"
+  url "https://mmonit.com/monit/dist/monit-5.30.0.tar.gz"
+  sha256 "e85649dfa8586f4fcdd34a0295c55ddd69b0eda6cfbdac47105a2673d10b1008"
   license "AGPL-3.0-or-later"
 
   livecheck do
@@ -11,13 +11,8 @@ class Monit < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "73c2409840f0151d6a3f167b6621e5e8fb1935cb97fd5d1390c3c635ee4646f9"
-    sha256 cellar: :any,                 arm64_big_sur:  "e8e13392bd3adc86d94271aa6e4b0b3136c3df09b9a67a178dfcc4fbaa42615e"
-    sha256 cellar: :any,                 monterey:       "6db60230fcb1c4104ea3e3eec2fb3fe347cdd6995220f54768ef0d5ce0f85a23"
-    sha256 cellar: :any,                 big_sur:        "a7fcbcba9af41d5eb405122132dbee449c403fb74192b587614903d56344941c"
-    sha256 cellar: :any,                 catalina:       "c83d409452660f7761f7514a0f72e9262ff6a8861b8e062d41b85a0f82a6b0d1"
-    sha256 cellar: :any,                 mojave:         "ed304427db3dea3a9c18d261a1d9cc24c5ca20d5fafe5e5ae397bde10f1254b8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "99573601eeb2e8d7377fad27e08e1a31a002eec231d84a492ec59532bfdeb49e"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/monit"
+    sha256 cellar: :any, mojave: "8e4bc9c0e47c7b4fb820518e5ecfc026dd0b9865e33a51a45462928178313850"
   end
 
   depends_on "openssl@1.1"
