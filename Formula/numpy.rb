2b69686f18f9bea22a7cecbd5e78b275697b5196
Adrian Ho <the.gromgit@gmail.com>
2022-01-25 01:40:08 +0800

    numpy: update 1.22.1 bottle.

--- Formula/numpy.rb
+++ Formula/numpy.rb
@@ -1,14 +1,14 @@
 class Numpy < Formula
   desc "Package for scientific computing with Python"
   homepage "https://www.numpy.org/"
-  url "https://files.pythonhosted.org/packages/50/e1/9b0c184f04b8cf5f3c941ffa56fbcbe936888bdac9aa7ba6bae405ac752b/numpy-1.22.0.zip"
-  sha256 "a955e4128ac36797aaffd49ab44ec74a71c11d6938df83b1285492d277db5397"
+  url "https://files.pythonhosted.org/packages/0a/c8/a62767a6b374a0dfb02d2a0456e5f56a372cdd1689dbc6ffb6bf1ddedbc0/numpy-1.22.1.zip"
+  sha256 "e348ccf5bc5235fc405ab19d53bec215bb373300e5523c7b476cc0da8a5e9973"
   license "BSD-3-Clause"
   head "https://github.com/numpy/numpy.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/numpy"
-    sha256 cellar: :any, mojave: "6706f91aac2f419a1b2b34110ae4cfeac5e7d0c35be3226c5ac17fbd0146f02c"
+    sha256 cellar: :any, mojave: "24593a3b7ab726d891213f4a4b12a66a91db3fdb71e0282727514b5bbe958c7a"
   end
 
   depends_on "cython" => :build
@@ -17,6 +17,13 @@ class Numpy < Formula
   depends_on "python@3.9"
 
   fails_with gcc: "5"
+
+  # numpy requires setuptools < 60.0
+  # https://github.com/numpy/numpy/issues/20824
+  resource "setuptools" do
+    url "https://files.pythonhosted.org/packages/ef/75/2bc7bef4d668f9caa9c6ed3f3187989922765403198243040d08d2a52725/setuptools-59.8.0.tar.gz"
+    sha256 "09980778aa734c3037a47997f28d6db5ab18bdf2af0e49f719bfc53967fd2e82"
+  end
 
   def install
     openblas = Formula["openblas"].opt_prefix
@@ -34,9 +41,15 @@ class Numpy < Formula
 
     xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
     ENV.prepend_create_path "PYTHONPATH", Formula["cython"].opt_libexec/"lib/python#{xy}/site-packages"
+    ENV.prepend_path "PYTHONPATH", buildpath/"temp/lib/python#{xy}/site-packages"
+    resources.each do |r|
+      r.stage do
+        system "python3", *Language::Python.setup_install_args(buildpath/"temp")
+      end
+    end
 
     system Formula["python@3.9"].opt_bin/"python3", "setup.py", "build",
-        "--fcompiler=gfortran", "--parallel=#{ENV.make_jobs}"
+        "--fcompiler=#{Formula["gcc"].opt_bin}/gfortran", "--parallel=#{ENV.make_jobs}"
     system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
   end
 
