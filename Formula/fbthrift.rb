6fe01fd8b14c63246d655fac068c7cc7b4ad1ade
Adrian Ho <the.gromgit@gmail.com>
2022-01-24 19:40:17 +0800

    fbthrift: update 2022.01.17.00 bottle.

---
+++ Formula/fbthrift.rb
@@ -0,0 +1,78 @@
+class Fbthrift < Formula
+  desc "Facebook's branch of Apache Thrift, including a new C++ server"
+  homepage "https://github.com/facebook/fbthrift"
+  url "https://github.com/facebook/fbthrift/archive/v2022.01.17.00.tar.gz"
+  sha256 "15599efa59d87d63bcb672dea7ea24cf34102da6f68254368a442493843c985b"
+  license "Apache-2.0"
+  head "https://github.com/facebook/fbthrift.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fbthrift"
+    sha256 cellar: :any, mojave: "c113f04202f022149c8b3f3ba1634f2719a2f0e83b90976e594624cfc01037b7"
+  end
+
+  depends_on "bison" => :build # Needs Bison 3.1+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "fizz"
+  depends_on "fmt"
+  depends_on "folly"
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "openssl@1.1"
+  depends_on "wangle"
+  depends_on "zstd"
+
+  uses_from_macos "flex" => :build
+  uses_from_macos "zlib"
+
+  on_macos do
+    depends_on "llvm" => :build if DevelopmentTools.clang_build_version <= 1100
+  end
+
+  on_linux do
+    depends_on "gcc@10"
+  end
+
+  fails_with :clang do
+    build 1100
+    cause <<~EOS
+      error: 'asm goto' constructs are not supported yet
+    EOS
+  end
+
+  fails_with gcc: "5" # C++ 17
+  fails_with gcc: "11" # https://github.com/facebook/folly#ubuntu-lts-centos-stream-fedora
+
+  def install
+    ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib
+    ENV.llvm_clang if OS.mac? && (DevelopmentTools.clang_build_version <= 1100)
+
+    # The static libraries are a bit annoying to build. If modifying this formula
+    # to include them, make sure `bin/thrift1` links with the dynamic libraries
+    # instead of the static ones (e.g. `libcompiler_base`, `libcompiler_lib`, etc.)
+    shared_args = ["-DBUILD_SHARED_LIBS=ON", "-DCMAKE_INSTALL_RPATH=#{rpath}"]
+    shared_args << "-DCMAKE_SHARED_LINKER_FLAGS=-Wl,-undefined,dynamic_lookup" if OS.mac?
+
+    system "cmake", "-S", ".", "-B", "build/shared", *std_cmake_args, *shared_args
+    system "cmake", "--build", "build/shared"
+    system "cmake", "--install", "build/shared"
+
+    elisp.install "thrift/contrib/thrift.el"
+    (share/"vim/vimfiles/syntax").install "thrift/contrib/thrift.vim"
+  end
+
+  test do
+    (testpath/"example.thrift").write <<~EOS
+      namespace cpp tamvm
+
+      service ExampleService {
+        i32 get_number(1:i32 number);
+      }
+    EOS
+
+    system bin/"thrift1", "--gen", "mstch_cpp2", "example.thrift"
+    assert_predicate testpath/"gen-cpp2", :exist?
+    assert_predicate testpath/"gen-cpp2", :directory?
+  end
+end
