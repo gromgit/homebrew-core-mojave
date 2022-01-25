3b926a839ba04315283b28383b1071f9bee2fc16
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-25 10:55:35 +0800

    src: no rebuilding required

--- Formula/src.rb
+++ Formula/src.rb
@@ -1,8 +1,8 @@
 class Src < Formula
   desc "Simple revision control: RCS reloaded with a modern UI"
   homepage "http://www.catb.org/~esr/src/"
-  url "http://www.catb.org/~esr/src/src-1.28.tar.gz"
-  sha256 "ee448f17e0de07eed749188bf2b977211fc609314b079ebe6c23485ac72f79ba"
+  url "http://www.catb.org/~esr/src/src-1.29.tar.gz"
+  sha256 "4dcfaa0612ed0bdbb6dc1aea567880a6b015f1885710bbdb327f97c4de414241"
   license "BSD-2-Clause"
 
   livecheck do
@@ -11,13 +11,7 @@ class Src < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "273641371d813cdd5661bf1056e6aa671f3beb53b121e5197b75cd2751461fbc"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6d72e26032c8eaf0416c285a84a3556e31d6a1ed962051aa254418f28d8b493d"
-    sha256 cellar: :any_skip_relocation, monterey:       "273641371d813cdd5661bf1056e6aa671f3beb53b121e5197b75cd2751461fbc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5485c642c815e0368ace75c43907ece44ed6b220484be136ea791ad14780ee30"
-    sha256 cellar: :any_skip_relocation, catalina:       "312d165d1840e28a6c33df33248a7236dc2c524ee792b575b2774afe5597e446"
-    sha256 cellar: :any_skip_relocation, mojave:         "312d165d1840e28a6c33df33248a7236dc2c524ee792b575b2774afe5597e446"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "312d165d1840e28a6c33df33248a7236dc2c524ee792b575b2774afe5597e446"
+    sha256 cellar: :any_skip_relocation, all: "1be6e1ab14e7503ebe94f2e896af8c3e9d605232e3ea18f09f7248a2350f90e4"
   end
 
   head do
