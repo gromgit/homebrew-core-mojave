247f3f5a1f091ff119d8661f2621f78db2370598
Adrian Ho <the.gromgit@gmail.com>
2022-01-25 11:07:12 +0800

    readline: update 8.1.2 bottle.

--- Formula/readline.rb
+++ Formula/readline.rb
@@ -3,12 +3,18 @@ class Readline < Formula
   homepage "https://tiswww.case.edu/php/chet/readline/rltop.html"
   url "https://ftp.gnu.org/gnu/readline/readline-8.1.tar.gz"
   mirror "https://ftpmirror.gnu.org/readline/readline-8.1.tar.gz"
-  version "8.1.1"
+  version "8.1.2"
   sha256 "f8ceb4ee131e3232226a17f51b164afc46cd0b9e6cef344be87c65962cb82b02"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/readline"
+    sha256 cellar: :any, mojave: "b0e125bd5c5f4dc307fb855d17b58b94977c5a7d6daa5bd5b88bf0e918bc4ff2"
+  end
+
   %w[
     001 682a465a68633650565c43d59f0b8cdf149c13a874682d3c20cb4af6709b9144
+    002 e55be055a68cb0719b0ccb5edc9a74edcc1d1f689e8a501525b3bc5ebad325dc
   ].each_slice(2) do |p, checksum|
     patch :p0 do
       url "https://ftp.gnu.org/gnu/readline/readline-8.1-patches/readline81-#{p}"
@@ -51,16 +57,6 @@ class Readline < Formula
 
       versions
     end
-  end
-
-  bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3b5aa5bdfee4d377f6b5d571fcadcdfa14f9973895bcf5197fcd546424b59e8b"
-    sha256 cellar: :any,                 arm64_big_sur:  "bcb228b99fcecebf6ecc2b2ac80ab96a396374a8d5bc13b21034ef501017254f"
-    sha256 cellar: :any,                 monterey:       "5c8cc72b8b4eb97f2e6f4fd1db5512cd1f72f71fa4a9cbdc27cac7ed6c27dae2"
-    sha256 cellar: :any,                 big_sur:        "c596199dc30f2542144a10f10ac686e441bebc5707bb63cca34159e55de66e3b"
-    sha256 cellar: :any,                 catalina:       "7a6136c28be474faf630922495ca617ecad1275baa4ef8646bbc31eece3809f4"
-    sha256 cellar: :any,                 mojave:         "0af6c77e4e554d9ee9f60f7c55ccde1cee46aa916ce8baff66ae10ed1ef13ed1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8bb1e20aafd3dfc3076b62d8f55db867edd93cbfaeb44d1d03f1968fc51209d9"
   end
 
   keg_only :shadowed_by_macos, "macOS provides BSD libedit"
