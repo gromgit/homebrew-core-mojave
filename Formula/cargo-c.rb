d23927902b73f69fa5dd9f3781f09b1c4a3ef880
Adrian Ho <the.gromgit@gmail.com>
2022-01-25 13:39:51 +0800

    cargo-c: update 0.9.7 bottle.

--- Formula/cargo-c.rb
+++ Formula/cargo-c.rb
@@ -1,8 +1,8 @@
 class CargoC < Formula
   desc "Helper program to build and install c-like libraries"
   homepage "https://github.com/lu-zero/cargo-c"
-  url "https://github.com/lu-zero/cargo-c/archive/v0.9.6.tar.gz"
-  sha256 "c121bc2069373c25a89dcf9c357aa1e91d9eb4eb46a87db4d9931114070317c5"
+  url "https://github.com/lu-zero/cargo-c/archive/v0.9.7.tar.gz"
+  sha256 "eead78d26b4e5924e9d750f66f2fef4d706680927542d87c895178fc0be016f0"
   license "MIT"
 
   livecheck do
@@ -12,7 +12,7 @@ class CargoC < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cargo-c"
-    sha256 cellar: :any, mojave: "6cae53e42a941ae5f157c0cb3577092d9c639d5d027befc52c3cf71b3ae96834"
+    sha256 cellar: :any, mojave: "5bafcefe675c8e46bd54edad69df18e7761c516abcd551efc267cd343bcefd6c"
   end
 
   depends_on "rust" => :build
