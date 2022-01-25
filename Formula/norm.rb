629b55e9819b015d7ed1974c5843919db1a71837
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-25 10:55:32 +0800

    norm: no rebuilding required

--- Formula/norm.rb
+++ Formula/norm.rb
@@ -6,11 +6,13 @@ class Norm < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "658241c3120a66b68191f64e4009617d9ee0e43c5cd264e0db59f9dd2aeca966"
-    sha256 cellar: :any, big_sur:       "0d223adbb36c557616fbb8e026e93a36603ff5478ea3912978190389e409b04a"
-    sha256 cellar: :any, catalina:      "b01566af6d67555366f350e72a9717479c1510af885a89b60827356aeba7d2af"
-    sha256 cellar: :any, mojave:        "bc9f51046dc479949b480bb9a27143679bccb5f4bab0928c5968d280f9489d86"
-    sha256 cellar: :any, high_sierra:   "c46470e7594148cbee61f851b57373374abdc6a94e91c722efabd3c90f36ec06"
+    sha256 cellar: :any, arm64_monterey: "a8a59ed661565f3cf10b7bc5eaedb45d90cc949031d5d36a9b098985e8da71cc"
+    sha256 cellar: :any, arm64_big_sur:  "658241c3120a66b68191f64e4009617d9ee0e43c5cd264e0db59f9dd2aeca966"
+    sha256 cellar: :any, monterey:       "024bbaf98d384ee89e3ff5db7e56dc706dcc6f445ea3ebd976bd32c21fe24bcf"
+    sha256 cellar: :any, big_sur:        "0d223adbb36c557616fbb8e026e93a36603ff5478ea3912978190389e409b04a"
+    sha256 cellar: :any, catalina:       "b01566af6d67555366f350e72a9717479c1510af885a89b60827356aeba7d2af"
+    sha256 cellar: :any, mojave:         "bc9f51046dc479949b480bb9a27143679bccb5f4bab0928c5968d280f9489d86"
+    sha256 cellar: :any, high_sierra:    "c46470e7594148cbee61f851b57373374abdc6a94e91c722efabd3c90f36ec06"
   end
 
   resource "protolib" do
