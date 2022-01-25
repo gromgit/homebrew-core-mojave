5858c8b5f4de89d1a4f03d85b03d6d9e77c668aa
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-25 10:55:20 +0800

    cdk8s: no rebuilding required

--- Formula/cdk8s.rb
+++ Formula/cdk8s.rb
@@ -3,12 +3,12 @@ require "language/node"
 class Cdk8s < Formula
   desc "Define k8s native apps and abstractions using object-oriented programming"
   homepage "https://cdk8s.io/"
-  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-1.0.80.tgz"
-  sha256 "284472893e32d65b0b00dced1a9a630dc0241183f9a1f7b27467923455b6e7f0"
+  url "https://registry.npmjs.org/cdk8s-cli/-/cdk8s-cli-1.0.83.tgz"
+  sha256 "92a34cbff6ade24812f2f8bbec13f7184cc5a299db6d09295e7e6d9b85cbf430"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "340cc4faf54f003c0e3277369766fce05a787938f387eddbb2f1c632000b46b7"
+    sha256 cellar: :any_skip_relocation, all: "346fa46b1dfcf5bad6a4d6d21ad738185a1f4cf8b8e18f865396ec96cefe788f"
   end
 
   depends_on "node"
