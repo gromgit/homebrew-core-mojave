5d9859580d87648642d08fcd5f7d8691bb9f8285
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-25 10:55:33 +0800

    phpunit: no rebuilding required

--- Formula/phpunit.rb
+++ Formula/phpunit.rb
@@ -1,8 +1,8 @@
 class Phpunit < Formula
   desc "Programmer-oriented testing framework for PHP"
   homepage "https://phpunit.de"
-  url "https://phar.phpunit.de/phpunit-9.5.10.phar"
-  sha256 "a34b9db21de3e75ba2e609e68a4da94633f4a99cad8413fd3731a2cd9aa08ca8"
+  url "https://phar.phpunit.de/phpunit-9.5.13.phar"
+  sha256 "16742686383afac8a7a1e3f905862d77169746b0342f66ab80b0148582e5472a"
   license "BSD-3-Clause"
 
   livecheck do
@@ -11,7 +11,7 @@ class Phpunit < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "a38b2cd99080ac4b39b5ba5887c23a94a2d254eff80ce7d8779da4d5f4d3058e"
+    sha256 cellar: :any_skip_relocation, all: "62d2b3aace68612010a8d81cd7258232a744b87c3884a69b7973ecf4496a951a"
   end
 
   depends_on "php" => :test
