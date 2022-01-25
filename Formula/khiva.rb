52c9ca5765c70c1058661b5874fb51e2d6463040
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-25 10:55:28 +0800

    khiva: no rebuilding required

--- Formula/khiva.rb
+++ Formula/khiva.rb
@@ -7,9 +7,11 @@ class Khiva < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "28cddc44c54478884807c063702fec744bc58f177fac0eb4478aa1baa8bb1824"
-    sha256 cellar: :any, catalina: "befa8229bbf8013598ff42d1318c7eb60d63e13e22902d229658954a4362b521"
-    sha256 cellar: :any, mojave:   "6d9ddb73a8000d9f968717639717d6267e2ba7fb82c13d7a8cc3aefcd40a827b"
+    sha256 cellar: :any, arm64_monterey: "514fd58094f8ac4120f8a7e8cc600a96357a0ba68c5585acf869e146c30866a6"
+    sha256 cellar: :any, monterey:       "cb986b292887168d11bf06007d42b657ee7d2d2407d23b031708a6fd5af97870"
+    sha256 cellar: :any, big_sur:        "28cddc44c54478884807c063702fec744bc58f177fac0eb4478aa1baa8bb1824"
+    sha256 cellar: :any, catalina:       "befa8229bbf8013598ff42d1318c7eb60d63e13e22902d229658954a4362b521"
+    sha256 cellar: :any, mojave:         "6d9ddb73a8000d9f968717639717d6267e2ba7fb82c13d7a8cc3aefcd40a827b"
   end
 
   depends_on "boost" => :build
