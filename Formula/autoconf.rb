afdfbe59b65d8e887b6a410ffeb5894cde8e730f
Adrian Ho <the.gromgit@gmail.com>
2022-01-26 13:47:33 +0800

    autoconf: update 2.71 bottle.

--- Formula/autoconf.rb
+++ Formula/autoconf.rb
@@ -10,13 +10,9 @@ class Autoconf < Formula
   ]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a3d366c98b0da7a0a4f352eef49af9d612ac7aea4ffe420d49ff12bd90007415"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6279cc6294da77a87b2e08783f39a97e8678bde9b3e2899685879cabee6d2945"
-    sha256 cellar: :any_skip_relocation, monterey:       "de8f4aa4123d307ad8bb11b1c685538224dc39939085fd00af928c5099c4202d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0aa64f171bac19ce6ac0c0ca697f30658db78cf175550dfde3dbda907b7f2500"
-    sha256 cellar: :any_skip_relocation, catalina:       "258a94bef23057c52818adf64d682af20bc6e09b46eac135047e2b87fc8206c7"
-    sha256 cellar: :any_skip_relocation, mojave:         "e94578bf4b4832baef1c9bbb40cb4da5fdbd9c66be5ed8d070f78be5f0cca618"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9f9a73292e64f19ec504459ee7f8b7f11f1e472d18252499705a9fc289c43f85"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/autoconf"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "eabc5c34040ae4899980f1f56e2d8f508f7ec6d236b0fa72b4c036891f2f009a"
   end
 
   depends_on "m4"
