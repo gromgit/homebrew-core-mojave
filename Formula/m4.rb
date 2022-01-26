8986774c2afa816fb566c73822796140756f0355
Adrian Ho <the.gromgit@gmail.com>
2022-01-26 13:45:20 +0800

    m4: update 1.4.19 bottle.

--- Formula/m4.rb
+++ Formula/m4.rb
@@ -7,13 +7,9 @@ class M4 < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8e9fa0d7d946f7c38e1a6f596aab3169d2440fccd34ec321b9a032d903ec951c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ea1be04e51645f9e31c8d2cab6d144bb7f47efb1f35214b9f1014e27db5a3bc1"
-    sha256 cellar: :any_skip_relocation, monterey:       "8a17c921e5135206c382fc67ae53ba8835684dac5bfe7eb2bcdfa79df4d2731d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b22472f659112cf12163bba770d891618b3ada5aaf5baa01516d80fef6214617"
-    sha256 cellar: :any_skip_relocation, catalina:       "e0fec6a49fd80cc7279c71f319d70d01ed49e894b53cd91e39f170288232fa93"
-    sha256 cellar: :any_skip_relocation, mojave:         "0cf53207764a2311db75b19628e2395ac6655ea1f7fdac97a33a0de34f315018"
-    sha256                               x86_64_linux:   "f6d1087a51e0ff2e582b3043a25a51b67971b2246cf65167ef3abf1230160f04"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/m4"
+    rebuild 1
+    sha256 mojave: "3b71da60e60afd42b34fd6e01b4f46885df969eb9dd1d501230d3ebfc559e591"
   end
 
   keg_only :provided_by_macos
