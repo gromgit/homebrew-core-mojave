5b25d096e2a82bff513cee80ff2b81c4d51c6f0f
Adrian Ho <the.gromgit@gmail.com>
2022-01-26 14:54:10 +0800

    python@3.8: update 3.8.12_1 bottle.

--- Formula/python@3.8.rb
+++ Formula/python@3.8.rb
@@ -12,13 +12,9 @@ class PythonAT38 < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "e0ddb198a05fb010c6317892e92a84cee2792e715d585cb4b06005f5be86a497"
-    sha256 arm64_big_sur:  "5a3d8a5bf18c13bce7c3d4eff430bbe368bbb90654eee05315ef9ca51a2b05f7"
-    sha256 monterey:       "9003ba90da12f509084675de2ac639356aa929f99483115250b7cfb154ade246"
-    sha256 big_sur:        "1f30194a47851b4e00abef864460d7522dd4662b03c39834e089b2b429ef541c"
-    sha256 catalina:       "d4cb99ba879e231fe4527f171f6fd41bdd36a805421ad31d0987e83af5738b98"
-    sha256 mojave:         "5e60329987598230fb729b3b7c0ea4deddbfd701dc52e6771b6d10b753913400"
-    sha256 x86_64_linux:   "f0c7c14d64457e19ee59dbee77f088c29325e1b9a7430c657d02f045952db30d"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/python@3.8"
+    rebuild 1
+    sha256 mojave: "a2c96acaabca86270abc78f4bbea90c98af7ed4de13eb2d1e2d5360aad86fbea"
   end
 
   # setuptools remembers the build flags python is built with and uses them to
