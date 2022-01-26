b061ea0ebd25b7a4da6138e11c52956ae875c896
Adrian Ho <the.gromgit@gmail.com>
2022-01-26 14:07:18 +0800

    abseil: update 20210324.2_1 bottle.

--- Formula/abseil.rb
+++ Formula/abseil.rb
@@ -8,13 +8,9 @@ class Abseil < Formula
   head "https://github.com/abseil/abseil-cpp.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "4e91703b7dacaf6700dde159935d61884633f2d75c0b18b083af9744ab92620d"
-    sha256 cellar: :any,                 arm64_big_sur:  "9d952d8755c1a1f6c7841cf34b4490dc472f76938ebee1dce2a65429273be564"
-    sha256 cellar: :any,                 monterey:       "c6c76dc1c68ba3e959e8e785ad60d2b50bde8f1303a2afa29af04a8cbb8346df"
-    sha256 cellar: :any,                 big_sur:        "d4373a873275884f01c18161826ae776d0b1d117eb3cf037763b906ccd3a60a6"
-    sha256 cellar: :any,                 catalina:       "a087569c2b0fe24bc55d24f4561542955ccb2b95d2cd27ef23f3c86e79c5893d"
-    sha256 cellar: :any,                 mojave:         "50fc095da0f1105b9d9edc73251b2ed8abeff974bbb0a604349af6f27ea2826a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "57ce077c0eebc91b6d06ad93206db43bc788783e6230b0eaee20919519da245a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/abseil"
+    rebuild 1
+    sha256 cellar: :any, mojave: "aed2ef8e6dcd7c9b51c8f4579b5992466ab1946951233e69019d87fc036e7fba"
   end
 
   depends_on "cmake" => :build
