7a948ce51473cdb35256ca5094ddd4c5c72513b8
Adrian Ho <the.gromgit@gmail.com>
2022-01-28 03:12:56 +0800

    apr-util: update 1.6.1_3 bottle.

--- Formula/apr-util.rb
+++ Formula/apr-util.rb
@@ -8,15 +8,9 @@ class AprUtil < Formula
   revision 3
 
   bottle do
-    sha256 arm64_monterey: "c47c937968e788d0a274e6bd214d20fcbfde6b5d23908b469a954d2f3e6aee59"
-    sha256 arm64_big_sur:  "29ffec15c99813b879d9410e2040044b02ee8e219492d065043b4bb65cf30741"
-    sha256 monterey:       "4459e0d3d15a973db35fb64b1da85d2185afc41e77d2989186db5574d3c75234"
-    sha256 big_sur:        "34dd598e189eefff7e09be3ee92c17816353b02cd1eb7b333ea4c74cd6204ffd"
-    sha256 catalina:       "425955a21c3fec8e78f365cd7fc4c6c4ec95d074f720a9b24e8237af90cc4dcc"
-    sha256 mojave:         "b3b8376d8f481164a34b891b926ab22acdc2903e77c4cfbc04c0ba6363ca7597"
-    sha256 high_sierra:    "20688bea4981567848393aeeb1964f2200847f63ee52eb8c68d8fff0e4dd8b45"
-    sha256 sierra:         "16e812e4be2247d8e8f4f8a68ba6765ceb5a98e22a08dda288eb99dff2e41ae0"
-    sha256 x86_64_linux:   "aa37797eb7ff78caa64082bc7a1aceeb74f8b05a73ce17a4fdbe78770bde4fea"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/apr-util"
+    rebuild 1
+    sha256 mojave: "6179fc5d2e4e9bc1f3bd06812e4950665f03f5340cbff1d48e1eb9d1df46143d"
   end
 
   keg_only :shadowed_by_macos, "Apple's CLT provides apr (but not apr-util)"
