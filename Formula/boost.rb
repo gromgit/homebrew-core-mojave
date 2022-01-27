64b6b2c02562989cd79c0aa8ea38af7c5df32758
Adrian Ho <the.gromgit@gmail.com>
2022-01-28 03:58:45 +0800

    boost: update 1.76.0 bottle.

--- Formula/boost.rb
+++ Formula/boost.rb
@@ -15,13 +15,9 @@ class Boost < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "56cd220f6bed3263cda3deb79a569fcff8e583895d1ad6cc247fafa2ef9de010"
-    sha256 cellar: :any,                 arm64_big_sur:  "3a336c8b1a917f7d9c55abba2905be99dade914bf9b829aab9d5fb6069b6ffcc"
-    sha256 cellar: :any,                 monterey:       "971054f698fc1ce13e5897f8e0b0958755aa54b6660bb197f16bef3e22308e09"
-    sha256 cellar: :any,                 big_sur:        "35c726d8bea731d85af3d6ba173e95b1726cdfac04e020e259937c8e99c3d4e7"
-    sha256 cellar: :any,                 catalina:       "758658d7f1f8cf6c6790609f2a0b0f8349993653c8afce66869ea91d57b1f26f"
-    sha256 cellar: :any,                 mojave:         "1b45c1009cef2b67b2ec21f86e0ff743f2efdbcb5af510067cba3587d44967cb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6203e3d6ae16a3e71d79ad67763a59733e84457253c1d44b01a15a72623b8a95"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/boost"
+    rebuild 1
+    sha256 cellar: :any, mojave: "9b818d1db67ca7c03bec82f76b13a6af99d73908435ba07a1b45079220f6a23f"
   end
 
   depends_on "icu4c"
