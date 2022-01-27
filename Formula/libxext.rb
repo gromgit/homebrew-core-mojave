9ec4613f00a645ef5b9096e1ac99b9a01453d77f
Adrian Ho <the.gromgit@gmail.com>
2022-01-27 12:26:48 +0800

    libxext: update 1.3.4 bottle.

--- Formula/libxext.rb
+++ Formula/libxext.rb
@@ -6,14 +6,9 @@ class Libxext < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "8e09ff59ff55eefc431207d0d6a78d6f098bb07e5a13be1dae3b0514979173b8"
-    sha256 cellar: :any,                 arm64_big_sur:  "24e44ef107138f015271fcd5aaa400403594adf7c64cf4a628b0cfe44d4e9fc6"
-    sha256 cellar: :any,                 monterey:       "04101a93002bbd6249ef45d48ff2647d315614a9c0685fcbf3e59e95e80ba5b8"
-    sha256 cellar: :any,                 big_sur:        "8a037408ba5c4c95c33af0d022edd631b744823bb9fa522a06b502ed9bf1fbc5"
-    sha256 cellar: :any,                 catalina:       "20cc49734eba43e2e9f058fa12f3782c76ac232fada3f6d297f91dca6e0582be"
-    sha256 cellar: :any,                 mojave:         "3f2da07d877e158f41231d088f0ffe5551132beaf2f3df683dae0ac2c11817cb"
-    sha256 cellar: :any,                 high_sierra:    "0070b8ea70006d011aac1c617e1a5f88caa2ae351b637858f828f859cb72d813"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dc7d5b80cf1ac00d0ffff27be679d38c0b7c8e8ee8ceb1747bb1f8ac9e5773d0"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libxext"
+    rebuild 1
+    sha256 cellar: :any, mojave: "8dd3f381ef96f4cbd4ac73f0f5a7a0f307a6be60eabc605d50c581f1c5616a9e"
   end
 
   depends_on "pkg-config" => :build
