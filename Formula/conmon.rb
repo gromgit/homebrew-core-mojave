3c72721d7bf6f91592ae3e418492ac935b62fec4
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-27 10:08:16 +0800

    conmon:

--- Formula/conmon.rb
+++ Formula/conmon.rb
@@ -1,9 +1,13 @@
 class Conmon < Formula
   desc "OCI container runtime monitor"
   homepage "https://github.com/containers/conmon"
-  url "https://github.com/containers/conmon/archive/refs/tags/v2.0.32.tar.gz"
-  sha256 "0ffd17a185322779d14d9a64c39120900f1e0cc3297b18679a933f5f6ba06a75"
+  url "https://github.com/containers/conmon/archive/refs/tags/v2.1.0.tar.gz"
+  sha256 "874909d831feac75e452562087f6ea0eb39848ef144397bdd8f0daf579c5ee85"
   license "Apache-2.0"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "544c5420a2bd61fe04f0f25c6390b657966ab895b5cd52717cae44995ea0e443"
+  end
 
   depends_on "go" => :build
   depends_on "pkg-config" => :build
