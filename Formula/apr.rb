5b5cf7bcd7cb2cabdf3e123a1e2f201befd2b047
Adrian Ho <the.gromgit@gmail.com>
2022-01-27 14:05:18 +0800

    apr: update 1.7.0_2 bottle.

--- Formula/apr.rb
+++ Formula/apr.rb
@@ -8,13 +8,9 @@ class Apr < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "ecc2c4b61e538c3a39fe228221356c2aae9c3d967f72e20a7978206321ef15b4"
-    sha256 cellar: :any,                 arm64_big_sur:  "d8adb33071a6a845ff928b6166377dea6de5b642b412042002386416354932b9"
-    sha256 cellar: :any,                 monterey:       "706df15280f05bc3ab057bd5dd856746f02c4fcc7356cccd5babd92a6362f132"
-    sha256 cellar: :any,                 big_sur:        "d9a9554a726ec60e124055a55747e6e7f4cff6310955d6340be340ac053ac097"
-    sha256 cellar: :any,                 catalina:       "3f5c1fa8f17715291ce9f66cf4eb4f518ac1aa856c485f0157036459ad63792c"
-    sha256 cellar: :any,                 mojave:         "4627416a5d9c651d2d4fbb7faa639d6f7a89c7c0558576eeac1f17a81a17f3bd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fc3b583e8e0773016a5749e947939f895043a1155968fdbab795d3fba9fe801c"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/apr"
+    rebuild 1
+    sha256 cellar: :any, mojave: "3be9db7b52ba7d20a3aeb614197f358de22c51499d673a4188394dfdb872023a"
   end
 
   keg_only :provided_by_macos, "Apple's CLT provides apr"
