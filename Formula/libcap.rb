018871a0482664abdf445e1df53ecd1b8cd4bbd2
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-27 10:08:20 +0800

    libcap:

--- Formula/libcap.rb
+++ Formula/libcap.rb
@@ -1,13 +1,17 @@
 class Libcap < Formula
   desc "User-space interfaces to POSIX 1003.1e capabilities"
   homepage "https://sites.google.com/site/fullycapable/"
-  url "https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.60.tar.xz"
-  sha256 "06a92076ce39a78bd28089e32085f1bde7f3bfa448fad37d895c2358f760b2eb"
+  url "https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.63.tar.xz"
+  sha256 "0c637b8f44fc7d8627787e9cf57f15ac06c1ddccb53e41feec5496be3466f77f"
   license all_of: ["BSD-3-Clause", "GPL-2.0-or-later"]
 
   livecheck do
     url "https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/"
     regex(/href=.*?libcap[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ede60f4406864153311d0a476071a201267ace7ddcaec18d7a7ce26731feb9d6"
   end
 
   depends_on :linux
