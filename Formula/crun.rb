4d04f83d707597ad04a09ee61e07c3da74004c1c
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-27 10:08:17 +0800

    crun:

--- Formula/crun.rb
+++ Formula/crun.rb
@@ -1,12 +1,12 @@
 class Crun < Formula
   desc "Fast and lightweight fully featured OCI runtime and C library"
   homepage "https://github.com/containers/crun"
-  url "https://github.com/containers/crun/releases/download/1.4.1/crun-1.4.1.tar.xz"
-  sha256 "8e1035c209a22a8784acd6b7b880f9c988ad0b9358f3d05e745412966e2ce546"
+  url "https://github.com/containers/crun/releases/download/1.4.2/crun-1.4.2.tar.xz"
+  sha256 "636afa526b43732909e9d9cca6597f8cd1b53a65a324179632607dd3d0008eae"
   license any_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "6c25f147a0fd53236dbe78b211b9a196e5da7afef0072687f88f32bc63c52501"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "fa2e05eb6b028e79e41b99e07e6c521d0f451f7127184c0e97b5e53a43e70afe"
   end
 
   depends_on "autoconf" => :build
