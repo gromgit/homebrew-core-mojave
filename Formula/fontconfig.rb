c6816c1dde8a7268d33cf7731e226e6196b5e29c
Adrian Ho <the.gromgit@gmail.com>
2022-01-27 11:31:00 +0800

    fontconfig: update 2.13.1 bottle.

--- Formula/fontconfig.rb
+++ Formula/fontconfig.rb
@@ -11,16 +11,9 @@ class Fontconfig < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "7b4e599e0ba2f56b5b3e22b83bebb1dc4e38c10b00d231c9c38319df31c7f65c"
-    sha256 arm64_big_sur:  "19dd1db44a74a3fedf20bc6576f81469d0ba6c4301fc865a56c3bbda2733d519"
-    sha256 monterey:       "7c40dab2076798df8b741c132bfc294f1c3fe3535c683d293b6711f2574d8675"
-    sha256 big_sur:        "ee5961891c9e943c8bea6ad280d2346caa2d3efafdbd726670e663d0bdfdb010"
-    sha256 catalina:       "64ff208b28613dfe2a65b9d74fd9b0129f3ca7e423db78329144cdaf51b36f70"
-    sha256 mojave:         "1c704a5a4249252bf42dc4f2a458f911a7858a931858ad257d9ec39978ca5095"
-    sha256 high_sierra:    "3b763143a4d6e3c74b3a8b237d2e5a383696347ea3599d07957f73a3f6521d23"
-    sha256 sierra:         "631531c4eb502bd97e4a5bef30760d1eef87dd50306ef2defb9460ac3338cfe1"
-    sha256 el_capitan:     "40d70137a970e257de5cf1251b10d56d7db835faee88a9f4c020b4a4e4f82eb1"
-    sha256 x86_64_linux:   "eccabe463fb0ecc1537d19628240caec276095a4afbbbc80233ac03c841cd4fe"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fontconfig"
+    rebuild 1
+    sha256 mojave: "eab831e1c15007800d01e879d1f4095026d18a7ce12a9d3809a5cd5fb3136224"
   end
 
   head do
