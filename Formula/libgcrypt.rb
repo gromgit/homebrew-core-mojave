524fd302a7a9693847a11a49ca86f745745fc86a
Adrian Ho <the.gromgit@gmail.com>
2022-01-27 10:46:38 +0800

    libgcrypt: update 1.9.4_1 bottle.

--- Formula/libgcrypt.rb
+++ Formula/libgcrypt.rb
@@ -12,13 +12,9 @@ class Libgcrypt < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "db8ca3ac372b23c25be44950acae40a4bb8faa4312df72023a28959a09cd8ea7"
-    sha256 cellar: :any,                 arm64_big_sur:  "ebe24d93edccd91ac094387b74b0c42aeebd44a6bb5f583816c8d1690690cf57"
-    sha256 cellar: :any,                 monterey:       "b8b834ecc967d71931b73f4102ef74be06812358def29cf37700ae1d57494c80"
-    sha256 cellar: :any,                 big_sur:        "19f11700630c036864c3acaf39d6b26b8d7f46a96b7eab4cab5d118ce5a0c28a"
-    sha256 cellar: :any,                 catalina:       "22b69fca91210d5598644b6164980ea3d53ccbb9a66124314ae3836b9100a4bf"
-    sha256 cellar: :any,                 mojave:         "d40e101e9605d7ba2b56fa6c441565192a85b3bb67302ab4feeac4d38a56d261"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c199805c55e5f11d84e19554b3583d78a2e681a0ba549508927c2528d07372cd"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libgcrypt"
+    rebuild 1
+    sha256 cellar: :any, mojave: "1db4904cff78ce69d1ce97073938a66f5b180b09fb137683b7034d9f4b239368"
   end
 
   depends_on "libgpg-error"
