cbe768c797aa47a0b26de5794731df015921c217
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-31 18:08:45 +0800

    cpp-gsl: no rebuilding required

--- Formula/cpp-gsl.rb
+++ Formula/cpp-gsl.rb
@@ -1,14 +1,13 @@
 class CppGsl < Formula
   desc "Microsoft's C++ Guidelines Support Library"
   homepage "https://github.com/Microsoft/GSL"
-  url "https://github.com/Microsoft/GSL/archive/v3.1.0.tar.gz"
-  sha256 "d3234d7f94cea4389e3ca70619b82e8fb4c2f33bb3a070799f1e18eef500a083"
+  url "https://github.com/Microsoft/GSL/archive/v4.0.0.tar.gz"
+  sha256 "95a51cd6432b491a71cac92ed279fd07668d1594d8909ea5654fb98156d57a10"
   license "MIT"
   head "https://github.com/Microsoft/GSL.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "993cd4a58ff925ff4d84fcb53426ba3125e50336856515ef005c4e7d0828c5f5"
+    sha256 cellar: :any_skip_relocation, all: "16ded5e62ddff466d23c8de41624941b2d72d663a8225e311ac12d24674afc11"
   end
 
   depends_on "cmake" => :build
