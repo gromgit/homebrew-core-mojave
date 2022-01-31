5590d0c77338d67b8470487ff89a7c0b9d886551
Adrian Ho <the.gromgit@gmail.com>
2022-02-01 01:22:21 +0800

    aerc: update 0.7.1_1 bottle.

--- Formula/aerc.rb
+++ Formula/aerc.rb
@@ -4,17 +4,19 @@ class Aerc < Formula
   url "https://git.sr.ht/~rjarry/aerc/archive/0.7.1.tar.gz"
   sha256 "e149236623c103c8526b1f872b4e630e67f15be98ac604c0ea0186054dbef0cc"
   license "MIT"
+  revision 1
+  head "https://git.sr.ht/~rjarry/aerc", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/aerc"
-    sha256 cellar: :any_skip_relocation, mojave: "3af78434c4651742b7fdeeaeafa94ed3a12026f94ce61429c6ae311a2e22cf45"
+    sha256 mojave: "24ed5208b6716645289a1bc7c3f02d037639677c655a65e0155191a876bf8328"
   end
 
   depends_on "go" => :build
   depends_on "scdoc" => :build
 
   def install
-    system "make", "PREFIX=#{prefix}"
+    system "make", "SHAREDIR=#{opt_pkgshare}", "PREFIX=#{prefix}"
     system "make", "install", "PREFIX=#{prefix}"
   end
 
