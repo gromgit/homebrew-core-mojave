468c2500a02861eccd04e891314f6df5db39de2d
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-31 18:08:58 +0800

    precomp: no rebuilding required

--- Formula/precomp.rb
+++ Formula/precomp.rb
@@ -7,13 +7,15 @@ class Precomp < Formula
   head "https://github.com/schnaader/precomp-cpp.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "527a77954282f384fd4bd01ec67750d73ee5fc0d989cee4be22ba688a461a90f"
-    sha256 cellar: :any_skip_relocation, big_sur:       "98d1f2f0987f9317b372895c3af39358585a461023e286baf2ebc67d118cf3be"
-    sha256 cellar: :any_skip_relocation, catalina:      "9ac9f156315ae463a1e378bdd9ed06d5f36437ccff4505740dfa10ee914b5adf"
-    sha256 cellar: :any_skip_relocation, mojave:        "7488435759867b2bb152cdd3ea78d2358659b34ff838e2cb97b54bd3a322147b"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "92824cc03c547d276436e1bdf55e905d402f77eeccf61f25a720d4315e5bd4cf"
-    sha256 cellar: :any_skip_relocation, sierra:        "5d852d83cf57987a521471b74e450a65473ed20e32bb14e83b5d99e969e37458"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7affc5c732e89618e28cbdc3a5b93adfd8588fe0af291dde9c940b0667ea8c06"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5567c2d4a2a3e18046d75f916742f0d112c81c2c744e63aabfaa0f75bb3ab0a8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "527a77954282f384fd4bd01ec67750d73ee5fc0d989cee4be22ba688a461a90f"
+    sha256 cellar: :any_skip_relocation, monterey:       "d7b2be3194f675f7db87d9952635a1f0bcb36b2b4cffdc63078d1adf8683bc63"
+    sha256 cellar: :any_skip_relocation, big_sur:        "98d1f2f0987f9317b372895c3af39358585a461023e286baf2ebc67d118cf3be"
+    sha256 cellar: :any_skip_relocation, catalina:       "9ac9f156315ae463a1e378bdd9ed06d5f36437ccff4505740dfa10ee914b5adf"
+    sha256 cellar: :any_skip_relocation, mojave:         "7488435759867b2bb152cdd3ea78d2358659b34ff838e2cb97b54bd3a322147b"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "92824cc03c547d276436e1bdf55e905d402f77eeccf61f25a720d4315e5bd4cf"
+    sha256 cellar: :any_skip_relocation, sierra:         "5d852d83cf57987a521471b74e450a65473ed20e32bb14e83b5d99e969e37458"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7affc5c732e89618e28cbdc3a5b93adfd8588fe0af291dde9c940b0667ea8c06"
   end
 
   depends_on "cmake" => :build
