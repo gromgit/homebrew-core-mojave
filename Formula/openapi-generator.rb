d29e398941081e739b4f7289eaf448e132057736
Adrian Ho <aho+vm-mojave@03s.net>
2022-01-31 18:08:56 +0800

    openapi-generator: no rebuilding required

--- Formula/openapi-generator.rb
+++ Formula/openapi-generator.rb
@@ -1,8 +1,8 @@
 class OpenapiGenerator < Formula
   desc "Generate clients, server & docs from an OpenAPI spec (v2, v3)"
   homepage "https://openapi-generator.tech/"
-  url "https://search.maven.org/remotecontent?filepath=org/openapitools/openapi-generator-cli/5.3.1/openapi-generator-cli-5.3.1.jar"
-  sha256 "81dc1b3fb1102b43becd2f46098cfd861e9c4e84fbebb534ad67d7c6b0eeff77"
+  url "https://search.maven.org/remotecontent?filepath=org/openapitools/openapi-generator-cli/5.4.0/openapi-generator-cli-5.4.0.jar"
+  sha256 "f3ed312310e390324b33ba2ffff290ce812935207a1493ec5c098d0a441be51c"
   license "Apache-2.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class OpenapiGenerator < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "ead0082bf72df72f7349393f1733daaeff4f52a1d1910384fbfe2004fff4add3"
+    sha256 cellar: :any_skip_relocation, all: "e61082f3c1992254dada281a7c2d794144276f7354345e919a1aa9191219413d"
   end
 
   head do
