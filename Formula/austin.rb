0278a948570a0eb30c6504713dc62060fc8f26e4
Adrian Ho <the.gromgit@gmail.com>
2022-02-01 04:06:05 +0800

    austin: update 3.3.0 bottle.

--- Formula/austin.rb
+++ Formula/austin.rb
@@ -1,14 +1,14 @@
 class Austin < Formula
   desc "Python frame stack sampler for CPython"
   homepage "https://github.com/P403n1x87/austin"
-  url "https://github.com/P403n1x87/austin/archive/v3.2.0.tar.gz"
-  sha256 "ee189af905fac77ff9173b57d8e927bdb7c4cf5e18b1bfd7f4456ac46fe04484"
+  url "https://github.com/P403n1x87/austin/archive/v3.3.0.tar.gz"
+  sha256 "a0dcfee0dffecb00b85a84f3c7befff7e61fd4b504228c1e6ce7bc5af9790506"
   license "GPL-3.0-or-later"
   head "https://github.com/P403n1x87/austin.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/austin"
-    sha256 cellar: :any_skip_relocation, mojave: "0ca432108853bf1633449acf81940f1f43dfe137b8d165c9ccbee9d8b138e32f"
+    sha256 cellar: :any_skip_relocation, mojave: "b5bb482e94a0e9a2b606ded388d1e4721d2c866711ce0fdea6ea58b12c0c104f"
   end
 
   depends_on "autoconf" => :build
