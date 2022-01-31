b80319e81c4aab6fd5f03aa728c1b305eb214011
Adrian Ho <the.gromgit@gmail.com>
2022-02-01 03:50:37 +0800

    ask-cli: update 2.26.0 bottle.

--- Formula/ask-cli.rb
+++ Formula/ask-cli.rb
@@ -3,13 +3,13 @@ require "language/node"
 class AskCli < Formula
   desc "CLI tool for Alexa Skill Kit"
   homepage "https://www.npmjs.com/package/ask-cli"
-  url "https://registry.npmjs.org/ask-cli/-/ask-cli-2.25.0.tgz"
-  sha256 "ad4c1056e0f9828c08a0ff39700a38246d7fac6ef703b545d56fbd6784bc3861"
+  url "https://registry.npmjs.org/ask-cli/-/ask-cli-2.26.0.tgz"
+  sha256 "62bdf53a6113d06cf65d476ce03ac85a9d0ee5af273087e45b8c2ee1e7c70c72"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ask-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "bf89724630d9edcce5f9e78e24a9f9b8310180ab114064cc26b56f097447ad7a"
+    sha256 cellar: :any_skip_relocation, mojave: "c2e89d8826162c02ed7f379f8474faf9c7c950cc523ba08020d6df40dbc01f0c"
   end
 
   depends_on "node"
