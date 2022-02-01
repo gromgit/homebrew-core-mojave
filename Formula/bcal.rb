4f3cc52f8a4c287a6a45ba67dcdbf4384ededda3
Adrian Ho <the.gromgit@gmail.com>
2022-02-01 15:38:58 +0800

    bcal: update 2.4 bottle.

--- Formula/bcal.rb
+++ Formula/bcal.rb
@@ -1,8 +1,8 @@
 class Bcal < Formula
   desc "Storage conversion and expression calculator"
   homepage "https://github.com/jarun/bcal"
-  url "https://github.com/jarun/bcal/archive/v2.3.tar.gz"
-  sha256 "e295b022e5187079b4cc5310447da5c787cb6bade86936dabf34a42bb85348ba"
+  url "https://github.com/jarun/bcal/archive/v2.4.tar.gz"
+  sha256 "141f39d866f62274b2262164baaac6202f60749862c84c2e6ed231f6d03ee8df"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -11,13 +11,8 @@ class Bcal < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a2503013003907b8c61799bff250beab99dc7ebcdbe6393e8fec8ca31237a1fc"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "184e482468546611a17a93f755ac988aa44b3abe1d5740b4d653e8a0684c2fc0"
-    sha256 cellar: :any_skip_relocation, monterey:       "d9607a44df122ff814a718621ebc3ac5cfcf6b33395c21e3fca1a4cd54ff33e7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "68c9fe6f9ae3bff9bc964cc07fe73b7a02f0189d179a6e7579a0b031fc9ce26e"
-    sha256 cellar: :any_skip_relocation, catalina:       "094bff2b14bc31b0009f646ab03f6a1619af5647c801912a86ebff330c46c512"
-    sha256 cellar: :any_skip_relocation, mojave:         "cf1b9ee5aaf57f8821e5208b59974953f676e26a9584ea5aa45b43b507a21369"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3e17beee69ada92813dabcf05ae0e05c1f39e7afe795cf0dc1ccf0dd1390d2ab"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bcal"
+    sha256 cellar: :any_skip_relocation, mojave: "4081e588b2c71525a044b6bfeb4bfd760ca44327dd7dad16ec9c14038f70a134"
   end
 
   on_linux do
