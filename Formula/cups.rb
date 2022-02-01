f5ed5d0d9b5bf8998518b26150dbaaa3f5f4747f
Adrian Ho <the.gromgit@gmail.com>
2022-02-02 00:50:13 +0800

    cups: update 2.4.1 bottle.

--- Formula/cups.rb
+++ Formula/cups.rb
@@ -3,8 +3,8 @@ class Cups < Formula
   homepage "https://github.com/OpenPrinting/cups"
   # This is the author's fork of CUPS. Debian have switched to this fork:
   # https://lists.debian.org/debian-printing/2020/12/msg00006.html
-  url "https://github.com/OpenPrinting/cups/releases/download/v2.4.0/cups-2.4.0-source.tar.gz"
-  sha256 "9abecec128ca6847c5bb2d3e3d30c87b782c0697b9acf284d16fa38f80a3a6de"
+  url "https://github.com/OpenPrinting/cups/releases/download/v2.4.1/cups-2.4.1-source.tar.gz"
+  sha256 "c7339f75f8d4f2dec50c673341a45fc06b6885bb6d4366d6bf59a4e6c10ae178"
   license "Apache-2.0"
   head "https://github.com/OpenPrinting/cups.git", branch: "master"
 
@@ -15,7 +15,7 @@ class Cups < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cups"
-    sha256 mojave: "1d58144ec75edbbf04c36aab4d6408350f1a3d8e6f42f0356a2ca2171821607f"
+    sha256 mojave: "379fd7e4b0151c5809d99a3471720b5a05cec539073e622b79b62b2d6e69ea56"
   end
 
   keg_only :provided_by_macos
