7667f4462a54b99e4921813913fb1b69321f3dbc
Adrian Ho <the.gromgit@gmail.com>
2022-02-01 21:11:51 +0800

    calicoctl: update 3.21.4 bottle.

--- Formula/calicoctl.rb
+++ Formula/calicoctl.rb
@@ -1,11 +1,11 @@
 class Calicoctl < Formula
   desc "Calico CLI tool"
   homepage "https://www.projectcalico.org"
-  url "https://github.com/projectcalico/calicoctl.git",
-      tag:      "v3.21.2",
-      revision: "17461419f0b20d784b2929b4aa700d4b14c2e10d"
+  url "https://github.com/projectcalico/calico.git",
+      tag:      "v3.21.4",
+      revision: "220d04c9429e8f157caccc70e4ed4c92e1d24e8d"
   license "Apache-2.0"
-  head "https://github.com/projectcalico/calicoctl.git", branch: "master"
+  head "https://github.com/projectcalico/calico.git", branch: "master"
 
   livecheck do
     url :stable
@@ -14,20 +14,23 @@ class Calicoctl < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/calicoctl"
-    sha256 cellar: :any_skip_relocation, mojave: "82f962df0bd322ea745ee8353ec52824395ee1ffc302f1c92a00750c1f97dd43"
+    sha256 cellar: :any_skip_relocation, mojave: "900ba314723280c2bad63be6003b4a78c59ab955e4e18f11a0e8af5c9619799e"
   end
 
   depends_on "go" => :build
 
   def install
-    commands = "github.com/projectcalico/calicoctl/v3/calicoctl/commands"
+    commands = "github.com/projectcalico/calico/calicoctl/calicoctl/commands"
     ldflags = "-X #{commands}.VERSION=#{version} " \
               "-X #{commands}.GIT_REVISION=#{Utils.git_short_head} " \
               "-s -w"
-    system "go", "build", *std_go_args(ldflags: ldflags), "calicoctl/calicoctl.go"
+    system "go", "build", *std_go_args(ldflags: ldflags), "calicoctl/calicoctl/calicoctl.go"
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/calicoctl version", 1)
+
+    assert_match "invalid configuration: no configuration has been provided",
+      shell_output("#{bin}/calicoctl datastore migrate lock 2>&1", 1)
   end
 end
