172efde8669b8843a725162c4af201f5cadbf348
Adrian Ho <the.gromgit@gmail.com>
2022-02-02 12:10:11 +0800

    feroxbuster: update 2.5.0 bottle.

--- Formula/feroxbuster.rb
+++ Formula/feroxbuster.rb
@@ -1,18 +1,13 @@
 class Feroxbuster < Formula
   desc "Fast, simple, recursive content discovery tool written in Rust"
   homepage "https://epi052.github.io/feroxbuster"
-  url "https://github.com/epi052/feroxbuster/archive/v2.4.0.tar.gz"
-  sha256 "077dec4b9e043b6a05582ea5598c35f4ea70beb45c8e516fee8279257ddf6c67"
+  url "https://github.com/epi052/feroxbuster/archive/v2.5.0.tar.gz"
+  sha256 "26684c4945e5a19ee44ef374f582354c520823581c9c3d9a3881cfe28b09b479"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "14a2cd0f12f5f8cf6466974beb4f7021569a6289fe9b7d4ca08174cdc3435845"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a0ca20eec229c0cd78acc1ad2819c48979bc782ccafea8ed1f7183e2a63066c3"
-    sha256 cellar: :any_skip_relocation, monterey:       "6d0833544a4a77d9be61376b5733de11596b0c6d87ffe6adb90a770929e5c347"
-    sha256 cellar: :any_skip_relocation, big_sur:        "31a6716c3fdfd369dee154888bff3c16543b038ea37a656fdce9caebe308917b"
-    sha256 cellar: :any_skip_relocation, catalina:       "e93ee95bbf9edd561c8783dd43ffecf393c4e28593ad228417fea9c1aa98412e"
-    sha256 cellar: :any_skip_relocation, mojave:         "2ce9bf75bcff1c1bf9d46e369d489698c05fc1e46de26c3cf00e8c78f10b0c84"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4df6bc88a54b6779160d4d5acf796257cf69d3ebda94e99b2ec1e221fa8b0dcf"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/feroxbuster"
+    sha256 cellar: :any_skip_relocation, mojave: "b26255d55ddcbebb64b3802e7f82a32a614f4d6558fa9d793feebf1f583461b4"
   end
 
   depends_on "rust" => :build
