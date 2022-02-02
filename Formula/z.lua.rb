af860f28a9434f3d5a756874e6b9fef95acc0776
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-03 00:50:50 +0800

    z.lua: no rebuilding required

--- Formula/z.lua.rb
+++ Formula/z.lua.rb
@@ -1,13 +1,13 @@
 class ZLua < Formula
   desc "New cd command that helps you navigate faster by learning your habits"
   homepage "https://github.com/skywind3000/z.lua"
-  url "https://github.com/skywind3000/z.lua/archive/1.8.13.tar.gz"
-  sha256 "a1215d727527e358363687a7e0b3605a132465e60a915f8e99bb5338a9c62ec6"
+  url "https://github.com/skywind3000/z.lua/archive/1.8.14.tar.gz"
+  sha256 "93784896021f84efe8430fbfcd9f0238426600d62a88780a1abda97943822bd5"
   license "MIT"
   head "https://github.com/skywind3000/z.lua.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "5e81f502f0d1aa8fcc350be77777e7dc6ee05bd04906762f4ce9a3fc285ceba5"
+    sha256 cellar: :any_skip_relocation, all: "468b67837221e43d91b09561ea571862c7c2832400e6522452444ca52ab17e0d"
   end
 
   depends_on "lua"
