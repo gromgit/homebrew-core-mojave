9411c7f9eca95a6cbeefef3c1e6518f2aaa3cdc2
Adrian Ho <the.gromgit@gmail.com>
2022-02-02 19:34:06 +0800

    git-town: update 7.7.0 bottle.

--- Formula/git-town.rb
+++ Formula/git-town.rb
@@ -1,24 +1,29 @@
 class GitTown < Formula
   desc "High-level command-line interface for Git"
   homepage "https://www.git-town.com/"
-  url "https://github.com/git-town/git-town/archive/v7.6.0.tar.gz"
-  sha256 "801d16047a5b74ccbe14f300c721289192d6c68115e97852b21a6eec4be71914"
+  url "https://github.com/git-town/git-town/archive/v7.7.0.tar.gz"
+  sha256 "edc4f87ef904ac297b9fbb30014e2ab474ee633c1687ed5011b38cd6f8b950e2"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-town"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "81cc02f6b67ce467c50b1b65ed8818487f30b7e55b382067201e6a3937f870f4"
+    sha256 cellar: :any_skip_relocation, mojave: "787c0d5f5c92e55aedc8a18e5956c53f4a129d56ca9b7a35d40a7d4552c6a21d"
   end
 
   depends_on "go" => :build
 
   def install
     ldflags = %W[
-      -X github.com/git-town/git-town/src/cmd.version=v#{version}
-      -X github.com/git-town/git-town/src/cmd.buildDate=#{time.strftime("%Y/%m/%d")}
+      -s -w
+      -X github.com/git-town/git-town/v7/src/cmd.version=v#{version}
+      -X github.com/git-town/git-town/v7/src/cmd.buildDate=#{time.strftime("%Y/%m/%d")}
     ]
     system "go", "build", *std_go_args(ldflags: ldflags)
+
+    # Install shell completions
+    (bash_completion/"git-town").write Utils.safe_popen_read(bin/"git-town", "completions", "bash")
+    (zsh_completion/"_git-town").write Utils.safe_popen_read(bin/"git-town", "completions", "zsh")
+    (fish_completion/"git-town.fish").write Utils.safe_popen_read(bin/"git-town", "completions", "fish")
   end
 
   test do
@@ -29,6 +34,6 @@ class GitTown < Formula
     system "git", "add", "testing.txt"
     system "git", "commit", "-m", "Testing!"
 
-    system "#{bin}/git-town", "config"
+    system bin/"git-town", "config"
   end
 end
