99020e7c9bfdc27db094711cf0dfbf978962e0b7
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-03 00:50:37 +0800

    falcon: no rebuilding required

--- Formula/falcon.rb
+++ Formula/falcon.rb
@@ -12,11 +12,13 @@ class Falcon < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "8727eb2b82dfbe15b089ffe42ff0e5f205399badde1c7dcfaf470a13141e4334"
-    sha256 cellar: :any, big_sur:       "fab1a5546fe1e1abff7525ef791126c341fc305ef1bee37ad3b1c2788342c451"
-    sha256 cellar: :any, catalina:      "0fdfed49f1ba12e66db6a7d9f315677280600f6db52afd52c91e6e66235c3053"
-    sha256 cellar: :any, mojave:        "d87b0664e797106f23bd5167c32988a7154d177955c2f612803999ebc4306fd9"
-    sha256 cellar: :any, high_sierra:   "670ae92a7f950558ea95001b45a848ef6d3f98d5fa414ba3549032d07badca47"
+    sha256 cellar: :any, arm64_monterey: "04d77eba622a43e3e74ce4c22bffcbb41ef71aaf7fe5a0c26db310f72597a671"
+    sha256 cellar: :any, arm64_big_sur:  "8727eb2b82dfbe15b089ffe42ff0e5f205399badde1c7dcfaf470a13141e4334"
+    sha256 cellar: :any, monterey:       "c19f0db85c93a318056d6069b4b9e49af411a7524882cf475f86ceb43630c9a1"
+    sha256 cellar: :any, big_sur:        "fab1a5546fe1e1abff7525ef791126c341fc305ef1bee37ad3b1c2788342c451"
+    sha256 cellar: :any, catalina:       "0fdfed49f1ba12e66db6a7d9f315677280600f6db52afd52c91e6e66235c3053"
+    sha256 cellar: :any, mojave:         "d87b0664e797106f23bd5167c32988a7154d177955c2f612803999ebc4306fd9"
+    sha256 cellar: :any, high_sierra:    "670ae92a7f950558ea95001b45a848ef6d3f98d5fa414ba3549032d07badca47"
   end
 
   depends_on "cmake" => :build
