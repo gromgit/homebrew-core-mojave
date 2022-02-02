948539a73bde689ae246ade8659360c9c6516625
Adrian Ho <the.gromgit@gmail.com>
2022-02-02 09:01:45 +0800

    dune: update 2.9.3 bottle.

--- Formula/dune.rb
+++ Formula/dune.rb
@@ -1,19 +1,14 @@
 class Dune < Formula
   desc "Composable build system for OCaml"
   homepage "https://dune.build/"
-  url "https://github.com/ocaml/dune/releases/download/2.9.1/dune-2.9.1.tbz"
-  sha256 "b374feb22b34099ccc6dd32128e18d088ff9a81837952b29f05110b308c09f26"
+  url "https://github.com/ocaml/dune/releases/download/2.9.3/dune-site-2.9.3.tbz"
+  sha256 "3e65ec73ab2c80d50d4ffd6c46cbfb22eacd0e5587a4be8af8ae69547d5f88d6"
   license "MIT"
-  head "https://github.com/ocaml/dune.git"
+  head "https://github.com/ocaml/dune.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3f3fc019e810f9a91b423e700f49983fd6d2f22b32b8c68d99a1cd7a2eee29eb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a272b312a8a6292184312151256f94eedd92f31919a0cae103230dc37c9051b3"
-    sha256 cellar: :any_skip_relocation, monterey:       "fa7e738c266953ec9cae9265c4b22e6846d2dbccf11dba60b0ecc8d728fa847a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5014448d4ccc8257e3b7edc05ae3d7ee4b7e3b552ea648090e5d2e1d6b11eadc"
-    sha256 cellar: :any_skip_relocation, catalina:       "e44461f150faf4ab772c7ecd2ac57bb6d42f48771a7a67b72da1a327ce38119d"
-    sha256 cellar: :any_skip_relocation, mojave:         "083e184745d8eac51f5035c3528a045e359cab971bc58ab662615128182acf4c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7c976d3c7fe424a46b16b3aa0e68f1526ba8acc098ce453b7c958720d1d36a5b"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dune"
+    sha256 cellar: :any_skip_relocation, mojave: "4305164e343c94db30ebd2151dee01964d795796f9c1ee4bca8d7de9ad228ef4"
   end
 
   depends_on "ocaml" => [:build, :test]
