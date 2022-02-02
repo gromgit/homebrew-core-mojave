75133b070fcf9e0915e85e6aac4fde8ef95ed9cd
Adrian Ho <the.gromgit@gmail.com>
2022-02-03 05:44:57 +0800

    duf: update 0.8.0 bottle.

--- Formula/duf.rb
+++ Formula/duf.rb
@@ -1,14 +1,14 @@
 class Duf < Formula
   desc "Disk Usage/Free Utility - a better 'df' alternative"
   homepage "https://github.com/muesli/duf"
-  url "https://github.com/muesli/duf/archive/v0.7.0.tar.gz"
-  sha256 "6f70fd0f0d51bfcfe20b8acc8c3a52573fc1ceed44ce97dbbb9d470bbe4467dc"
+  url "https://github.com/muesli/duf/archive/v0.8.0.tar.gz"
+  sha256 "6b483e68ec783821d07f03cb85629832b8c6f302a7d1bca25af142f891381e8b"
   license "MIT"
   head "https://github.com/muesli/duf.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/duf"
-    sha256 cellar: :any_skip_relocation, mojave: "91f5b21829679cd1ee9ae3455feed392bef5519d53afbc562271f625bad059d0"
+    sha256 cellar: :any_skip_relocation, mojave: "eab2485bf3467c0be77983348430656856faad10d6b8b2947162bd6448c15536"
   end
 
   depends_on "go" => :build
