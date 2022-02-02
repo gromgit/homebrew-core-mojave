b3e5f02df1b529bd0ec6e6eac335c1ca3c45b9af
Adrian Ho <the.gromgit@gmail.com>
2022-02-03 07:30:38 +0800

    forcecli: update 0.33.0 bottle.

--- Formula/forcecli.rb
+++ Formula/forcecli.rb
@@ -1,15 +1,14 @@
 class Forcecli < Formula
   desc "Command-line interface to Force.com"
   homepage "https://force-cli.herokuapp.com/"
-  url "https://github.com/ForceCLI/force/archive/v0.32.0.tar.gz"
-  sha256 "6ece64315c576eac8d5650fb1d8f1895b5810d268355d2c4b0e83fcea8bb9a5f"
+  url "https://github.com/ForceCLI/force/archive/v0.33.0.tar.gz"
+  sha256 "d8ab631475c9080339d1e96410ad84ea26377fa3d0662d3903f05030f929860d"
   license "MIT"
-  head "https://github.com/ForceCLI/force.git"
+  head "https://github.com/ForceCLI/force.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/forcecli"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "3cd9cf55c4bbc8e82b6e8a79373daf4315fb9885372c902652bc565e13e37776"
+    sha256 cellar: :any_skip_relocation, mojave: "d6f86f881c3a16507ad5f92b27fdfadc08dff9bb45a43b34a5e1b17da53d2558"
   end
 
   depends_on "go" => :build
