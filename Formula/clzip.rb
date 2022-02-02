921280902aa9b7d129739b6aa154243255991d0d
Adrian Ho <the.gromgit@gmail.com>
2022-02-03 04:31:35 +0800

    clzip: update 1.13 bottle.

--- Formula/clzip.rb
+++ Formula/clzip.rb
@@ -1,9 +1,9 @@
 class Clzip < Formula
   desc "C language version of lzip"
   homepage "https://www.nongnu.org/lzip/clzip.html"
-  url "https://download.savannah.gnu.org/releases/lzip/clzip/clzip-1.12.tar.gz"
-  mirror "https://download-mirror.savannah.gnu.org/releases/lzip/clzip/clzip-1.12.tar.gz"
-  sha256 "fcc92b3006d87b7c4affa03fe9dcc4869a802253052653200c26f6ba718bfee8"
+  url "https://download.savannah.gnu.org/releases/lzip/clzip/clzip-1.13.tar.gz"
+  mirror "https://download-mirror.savannah.gnu.org/releases/lzip/clzip/clzip-1.13.tar.gz"
+  sha256 "7ac9fbf5036bf50fb0b6a20e84d2293cb0d24d4044eaf33cbe9760bb9e7fea7a"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -12,13 +12,8 @@ class Clzip < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3907dfa594ee7d6d237ae118fade57365a6c1318379da8ea709845ffca68af76"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5ae403fecae4e6688b40662a5160f7fbc55e5636ce1a9447dba83c14c60e825f"
-    sha256 cellar: :any_skip_relocation, monterey:       "23691b3d00a35a10480592393ab1c3442950e663768369d64f09df09a3a63b4b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e82b38513ecfa60b53ea584aa573682fd4b7df5db3e45ccbe633840258f547ee"
-    sha256 cellar: :any_skip_relocation, catalina:       "dc96f1ee14b4b904dd1ecfef38cb3fae30cefd7b490eb31b78e57942df72c2b9"
-    sha256 cellar: :any_skip_relocation, mojave:         "d4cbf4b08330488cf80bbdf6c16d740038c72028760e1bba043122f217e00721"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0a1ea519aedf9b7ad116ad71caf65ca8fa08f2b0f444945f8127c6faf05fa6c6"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/clzip"
+    sha256 cellar: :any_skip_relocation, mojave: "09cd7e8f9d101204a982f72cffdb3cfd99a1c9d9ee5b3dd1edb1edf02ae8ccd3"
   end
 
   def install
