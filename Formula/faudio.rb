bfe10c36ece5d6c81afe1fd6e433887e947647db
Adrian Ho <the.gromgit@gmail.com>
2022-02-03 06:29:10 +0800

    faudio: update 22.02 bottle.

--- Formula/faudio.rb
+++ Formula/faudio.rb
@@ -1,14 +1,14 @@
 class Faudio < Formula
   desc "Accuracy-focused XAudio reimplementation for open platforms"
   homepage "https://fna-xna.github.io/"
-  url "https://github.com/FNA-XNA/FAudio/archive/22.01.tar.gz"
-  sha256 "e6b7c0ced90f5a3e1363f95ad990a99afee6784a35320719df48af45d1f07c7d"
+  url "https://github.com/FNA-XNA/FAudio/archive/22.02.tar.gz"
+  sha256 "fc6248fd471c59dc9dc1de706a186064511de666c78a7358861d275c084df9ed"
   license "Zlib"
   head "https://github.com/FNA-XNA/FAudio.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/faudio"
-    sha256 cellar: :any, mojave: "7059ca08f2b5f3774b4adfdec4c705551a573c8948e102c5cdab87c234c7b24d"
+    sha256 cellar: :any, mojave: "40e7ff7a593aaffb1b69b5ad6d09b45dcd2cf9ccbce5fac3eb43971b85d7b405"
   end
 
   depends_on "cmake" => :build
