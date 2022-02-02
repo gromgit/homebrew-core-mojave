54e785cce5a183107289b66e83e8cf2c9a5a4134
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-03 00:50:32 +0800

    ca-certificates: no rebuilding required

--- Formula/ca-certificates.rb
+++ Formula/ca-certificates.rb
@@ -1,8 +1,8 @@
 class CaCertificates < Formula
   desc "Mozilla CA certificate store"
   homepage "https://curl.se/docs/caextract.html"
-  url "https://curl.se/ca/cacert-2021-10-26.pem"
-  sha256 "ae31ecb3c6e9ff3154cb7a55f017090448f88482f0e94ac927c0c67a1f33b9cf"
+  url "https://curl.se/ca/cacert-2022-02-01.pem"
+  sha256 "1d9195b76d2ea25c2b5ae9bee52d05075244d78fcd9c58ee0b6fac47d395a5eb"
   license "MPL-2.0"
 
   livecheck do
@@ -11,9 +11,7 @@ class CaCertificates < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ca-certificates"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "1bbd45c16a0b9912174c553a6d7ae1b67b11abbeb3155eaf03109bb62d8e5381"
+    sha256 cellar: :any_skip_relocation, all: "40e00f88df310bc2dc42aefb3e834c0a3022f125fecdf21f26431d12a104dbc0"
   end
 
   def install
