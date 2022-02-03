613762b647a2b1ff807f098f5c0fcf096bd528f7
Adrian Ho <the.gromgit@gmail.com>
2022-02-03 20:50:15 +0800

    lean-cli: update 0.29.2 bottle.

--- Formula/lean-cli.rb
+++ Formula/lean-cli.rb
@@ -1,26 +1,21 @@
 class LeanCli < Formula
   desc "Command-line tool to develop and manage LeanCloud apps"
   homepage "https://github.com/leancloud/lean-cli"
-  url "https://github.com/leancloud/lean-cli/archive/v0.29.1.tar.gz"
-  sha256 "4729760a59d8390c615457d0a01ee38ff8f1055e9fa8645d5763d878c8239d74"
+  url "https://github.com/leancloud/lean-cli/archive/v0.29.2.tar.gz"
+  sha256 "b5d39383335ced9f7b9adb5bf9701cd3e4b4df19bae251b8a603c71b896ec32b"
   license "Apache-2.0"
-  head "https://github.com/leancloud/lean-cli.git"
+  head "https://github.com/leancloud/lean-cli.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lean-cli"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "d4262c231545faeb3fac7bbbce30273d46f1adbec01cf1ba8ba13f37448fc321"
+    sha256 cellar: :any_skip_relocation, mojave: "4790c75e8d147f97e4b22cc6c02c22180fea829039f637c230171aa513c543fa"
   end
 
   depends_on "go" => :build
 
   def install
     build_from = build.head? ? "homebrew-head" : "homebrew"
-    system "go", "build",
-            "-ldflags", "-s -w -X main.pkgType=#{build_from}",
-            *std_go_args,
-            "-o", bin/"lean",
-            "./lean"
+    system "go", "build", *std_go_args(output: bin/"lean", ldflags: "-s -w -X main.pkgType=#{build_from}"), "./lean"
 
     bash_completion.install "misc/lean-bash-completion" => "lean"
     zsh_completion.install "misc/lean-zsh-completion" => "_lean"
