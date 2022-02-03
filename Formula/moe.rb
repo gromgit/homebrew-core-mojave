6bf08eb3e83e4302107e76d99ca72a4656babdfd
Adrian Ho <the.gromgit@gmail.com>
2022-02-04 01:46:42 +0800

    moe: update 1.12 bottle.

--- Formula/moe.rb
+++ Formula/moe.rb
@@ -1,19 +1,14 @@
 class Moe < Formula
   desc "Console text editor for ISO-8859 and ASCII"
   homepage "https://www.gnu.org/software/moe/moe.html"
-  url "https://ftp.gnu.org/gnu/moe/moe-1.11.tar.lz"
-  mirror "https://ftpmirror.gnu.org/moe/moe-1.11.tar.lz"
-  sha256 "0efbcbcf5a4a8d966541c6cb099ba0ab6416780366dbce82d9ff995a85a5e2f9"
+  url "https://ftp.gnu.org/gnu/moe/moe-1.12.tar.lz"
+  mirror "https://ftpmirror.gnu.org/moe/moe-1.12.tar.lz"
+  sha256 "8a885f2be426f8e04ad39c96012bd860954085a23744f2451663168826d7a1e8"
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 arm64_monterey: "55c6298deb23524518371ef927aae3e2a4f9c6e2dc52a9318623e0b8f1c41bc7"
-    sha256 arm64_big_sur:  "ecf7d889fc677d4fbd201086dc195d5d072dfdbc78fc0c506104a8a1e5216365"
-    sha256 monterey:       "c6defb99e4c9c3743c5f1ff438d543a5ce6390d8fc3f96e5594fc22dcff50a9b"
-    sha256 big_sur:        "fd26036b9c0e0c72963f91b99f1a0787109af0a519df1d33d0f04d0d0cc12ebe"
-    sha256 catalina:       "38b7920c9d82ba731f98bd1a56932b0d0ebe675d6d9006848a48e392013aad5a"
-    sha256 mojave:         "688fc7c768e785581675079dd436c9cf3fef36094ea1aa078a8c3fc221d00fbc"
-    sha256 x86_64_linux:   "b54eb559d79b40ec9054ac5cea562fbd562c963c9965a126b99ce082e7c859d0"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/moe"
+    sha256 mojave: "ded9c1cc81514ce822e1293d4b18f686cf19b65e5bf009d03b1b8086c97a51e1"
   end
 
   uses_from_macos "ncurses"
