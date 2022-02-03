cb545acbd734d2a07585fbc5e52ed0bda59a87a3
Adrian Ho <the.gromgit@gmail.com>
2022-02-03 17:39:04 +0800

    iredis: update 1.11.0 bottle.

--- Formula/iredis.rb
+++ Formula/iredis.rb
@@ -3,19 +3,14 @@ class Iredis < Formula
 
   desc "Terminal Client for Redis with AutoCompletion and Syntax Highlighting"
   homepage "https://iredis.io"
-  url "https://github.com/laixintao/iredis/archive/refs/tags/v1.9.4.tar.gz"
-  sha256 "628479debd444c671cba866e96877b266ce1581c632d78a243549906db1ef946"
+  url "https://files.pythonhosted.org/packages/d1/83/bd1b95706cb7112bebd7396f8ad06a5c15c58b7df632d9882d85652aad83/iredis-1.11.0.tar.gz"
+  sha256 "b4a5d80c321bd267f5dc8f49d0f1d064966bffea69a6a04aa807c057860d3130"
   license "BSD-3-Clause"
-  revision 1
-  head "https://github.com/laixintao/iredis.git"
+  head "https://github.com/laixintao/iredis.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "42c16776a00ab75e483cb165926fb7abc46fc776f7ec4ebef27c4145f656fd02"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5244f3000f4ae1070ef1ff2c4a573010c855dc2bebf49dbcb51c748f12f5e930"
-    sha256 cellar: :any_skip_relocation, monterey:       "d84ce0b1db3670cb8feda63357891a6ec7dc30ea72dddc994a8c92ea1222bd99"
-    sha256 cellar: :any_skip_relocation, big_sur:        "87232e4432a038363d2f82d46432413b552e36f1d102262b44e9313f4c047477"
-    sha256 cellar: :any_skip_relocation, catalina:       "69fdb1735e81cd4c42331482397549920c14c5551459366db23abc87eb31f664"
-    sha256 cellar: :any_skip_relocation, mojave:         "e16b775087fa092b6e03091b4bda5f52e47aaea4323dc1741650d141489f9863"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/iredis"
+    sha256 cellar: :any_skip_relocation, mojave: "5a9b7c8a18bed0296ba0a36b7cd755b7358897f2ba65373e478cb1af5ff43807"
   end
 
   depends_on "python@3.10"
@@ -32,13 +27,13 @@ class Iredis < Formula
   end
 
   resource "importlib-resources" do
-    url "https://files.pythonhosted.org/packages/ac/94/30b82b3a3a7c4f7fd7be1082047cc9a135a10c1bcb0158a2aec20d8fec57/importlib_resources-5.2.0.tar.gz"
-    sha256 "22a2c42d8c6a1d30aa8a0e1f57293725bfd5c013d562585e46aff469e0ff78b3"
+    url "https://files.pythonhosted.org/packages/b5/d8/51ace1c1ea6609c01c7f46ca2978e11821aa0efaaa7516002ef6df000731/importlib_resources-5.4.0.tar.gz"
+    sha256 "d756e2f85dd4de2ba89be0b21dba2a3bbec2e871a42a3a16719258a11f87506b"
   end
 
   resource "mistune" do
-    url "https://files.pythonhosted.org/packages/2d/a4/509f6e7783ddd35482feda27bc7f72e65b5e7dc910eca4ab2164daf9c577/mistune-0.8.4.tar.gz"
-    sha256 "59a3429db53c50b5c6bcc8a07f8848cb00d7dc8bdb431a4ab41920d201d4756e"
+    url "https://files.pythonhosted.org/packages/33/36/506af4690234e7a84b8b3e0f4aee4dfe5a28b8688a0eec2047af9a078020/mistune-2.0.2.tar.gz"
+    sha256 "6fc88c3cb49dba8b16687b41725e661cf85784c12e8974a29b9d336dd596c3a1"
   end
 
   resource "pendulum" do
@@ -47,13 +42,13 @@ class Iredis < Formula
   end
 
   resource "prompt-toolkit" do
-    url "https://files.pythonhosted.org/packages/88/4b/2c0f9e2b52297bdeede91c8917c51575b125006da5d0485521fa2b1e0b75/prompt_toolkit-3.0.19.tar.gz"
-    sha256 "08360ee3a3148bdb5163621709ee322ec34fc4375099afa4bbf751e9b7b7fa4f"
+    url "https://files.pythonhosted.org/packages/15/e4/f138d6319c02a6052a590ef32e94366b74581973b43665c2960b07b9ec24/prompt_toolkit-3.0.24.tar.gz"
+    sha256 "1bb05628c7d87b645974a1bad3f17612be0c29fa39af9f7688030163f680bad6"
   end
 
   resource "Pygments" do
-    url "https://files.pythonhosted.org/packages/ba/6e/7a7c13c21d8a4a7f82ccbfe257a045890d4dbf18c023f985f565f97393e3/Pygments-2.9.0.tar.gz"
-    sha256 "a18f47b506a429f6f4b9df81bb02beab9ca21d0a5fee38ed15aef65f0545519f"
+    url "https://files.pythonhosted.org/packages/94/9c/cb656d06950268155f46d4f6ce25d7ffc51a0da47eadf1b164bbf23b718b/Pygments-2.11.2.tar.gz"
+    sha256 "4e426f72023d88d03b2fa258de560726ce890ff3b630f88c21cbb8b2503b8c6a"
   end
 
   resource "python-dateutil" do
@@ -74,11 +69,6 @@ class Iredis < Formula
   resource "wcwidth" do
     url "https://files.pythonhosted.org/packages/25/9d/0acbed6e4a4be4fc99148f275488580968f44ddb5e69b8ceb53fc9df55a0/wcwidth-0.1.9.tar.gz"
     sha256 "ee73862862a156bf77ff92b09034fc4825dd3af9cf81bc5b360668d425f3c5f1"
-  end
-
-  resource "zipp" do
-    url "https://files.pythonhosted.org/packages/3a/9f/1d4b62cbe8d222539a84089eeab603d8e45ee1f897803a0ae0860400d6e7/zipp-3.5.0.tar.gz"
-    sha256 "f5812b1e007e48cff63449a5e9f4e7ebea716b4111f9c4f9a645f91d579bf0c4"
   end
 
   def install
