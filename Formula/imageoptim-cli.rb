1e0b43d223449b6c795541a32b9d37f0b1b02df0
Adrian Ho <the.gromgit@gmail.com>
2022-02-03 15:57:25 +0800

    imageoptim-cli: update 3.0.7 bottle.

--- Formula/imageoptim-cli.rb
+++ Formula/imageoptim-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class ImageoptimCli < Formula
   desc "CLI for ImageOptim, ImageAlpha and JPEGmini"
   homepage "https://jamiemason.github.io/ImageOptim-CLI/"
-  url "https://github.com/JamieMason/ImageOptim-CLI/archive/3.0.2.tar.gz"
-  sha256 "957261d38fa85e0ec377efb2eceae695e3d87b621bae64853f9f5163efd3594b"
+  url "https://github.com/JamieMason/ImageOptim-CLI/archive/3.0.7.tar.gz"
+  sha256 "ed1a36ccb0e960152eda6b3d4c422ee355c8cf8271f1f8e71141a286c4d647e5"
   license "MIT"
 
   livecheck do
@@ -13,17 +13,17 @@ class ImageoptimCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:    "fec70bca8360bb34c5f5758da13615cc5c833dceb4559a71ee62223b0684c73b"
-    sha256 cellar: :any_skip_relocation, big_sur:     "fd8ea02974a34708556c132b2258c12d68e379abfd1f0e591d193f97489d03fb"
-    sha256 cellar: :any_skip_relocation, catalina:    "56a9b2dba8f47850a26c335311f8c436b683c0b92ef5ab0b83e91688cf64ec7a"
-    sha256 cellar: :any_skip_relocation, mojave:      "b7b1923ed31ab32540a5dffcf798675401ca48249fae54f49d67bc6c78feede9"
-    sha256 cellar: :any_skip_relocation, high_sierra: "6f1aa4b2e4de3e7a1502f1f8747283589697e5f0f0506f4d24acd53381311706"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/imageoptim-cli"
+    sha256 cellar: :any_skip_relocation, mojave: "633de8d348a6618d839ff4e16694c8634d59accafe13488cd72c88f44386cbe4"
   end
 
-  depends_on "node@10" => :build
+  depends_on "node" => :build
   depends_on "yarn" => :build
+  depends_on arch: :x86_64 # Installs pre-built x86-64 binaries
+  depends_on :macos
 
   def install
+    Language::Node.setup_npm_environment
     system "yarn"
     system "npm", "run", "build"
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
