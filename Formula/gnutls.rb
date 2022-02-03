1b09d2bff1a7fbfaeb2fd54098d1e4a3018e6c16
Adrian Ho <the.gromgit@gmail.com>
2022-02-03 08:22:21 +0800

    gnutls: update 3.7.3 bottle.

--- Formula/gnutls.rb
+++ Formula/gnutls.rb
@@ -1,25 +1,19 @@
 class Gnutls < Formula
   desc "GNU Transport Layer Security (TLS) Library"
   homepage "https://gnutls.org/"
-  url "https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/gnutls-3.6.16.tar.xz"
-  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnutls/v3.6/gnutls-3.6.16.tar.xz"
-  sha256 "1b79b381ac283d8b054368b335c408fedcb9b7144e0c07f531e3537d4328f3b3"
+  url "https://www.gnupg.org/ftp/gcrypt/gnutls/v3.7/gnutls-3.7.3.tar.xz"
+  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnutls/v3.7/gnutls-3.7.3.tar.xz"
+  sha256 "fc59c43bc31ab20a6977ff083029277a31935b8355ce387b634fa433f8f6c49a"
   license all_of: ["LGPL-2.1-or-later", "GPL-3.0-only"]
-  revision 1
 
   livecheck do
-    url "https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/"
-    regex(/href=.*?gnutls[._-]v?(\d+(?:\.\d+)+)\.t/i)
+    url "https://www.gnutls.org/news.html"
+    regex(/>\s*GnuTLS\s*v?(\d+(?:\.\d+)+)\s*</i)
   end
 
   bottle do
-    sha256 arm64_monterey: "9c493190ccb3a9e805389c5b87003407a4bb808df54f307b830e66206987b9c4"
-    sha256 arm64_big_sur:  "e1ec9389285dca7c52c0346cdd5112a2bfacfd31b1958d0267408573f1fb5ed0"
-    sha256 monterey:       "c38aebba46bca205f64c7f3990938d39f563ca27fbcaff0b9cabcd6d8683a96d"
-    sha256 big_sur:        "f165f3c8e4ecac781e269e08c39f8af457d1d634ee21f0d8edb2ca6d1808f03a"
-    sha256 catalina:       "464f68e7e6f9c7698f921e3b8e23bd2302681041bb98c5f58c0be90833b4f48f"
-    sha256 mojave:         "ea18603d9f6337b7e9a77bec91124102a7a4680ab8358f1ee8d17023223816ed"
-    sha256 x86_64_linux:   "41e3d22d3117829ab83d8d06625732bda5bcc68b362f29318a777b4d884443cb"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gnutls"
+    sha256 mojave: "c8ade8200fbeeeeca97088a62e34e88e1c62ba7673b9849066adbac604f04011"
   end
 
   depends_on "autoconf" => :build
