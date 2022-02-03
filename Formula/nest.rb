941e9ce5b4763503938d5154dd776ce940622b9f
Adrian Ho <the.gromgit@gmail.com>
2022-02-04 04:19:25 +0800

    nest: update 3.2_1 bottle.

--- Formula/nest.rb
+++ Formula/nest.rb
@@ -1,17 +1,14 @@
 class Nest < Formula
   desc "Neural Simulation Tool (NEST) with Python3 bindings (PyNEST)"
   homepage "https://www.nest-simulator.org/"
-  url "https://github.com/nest/nest-simulator/archive/v3.1.tar.gz"
-  sha256 "5c11dd6b451c4c6bf93037bf29d5231c6c75a0e1a8863344f6fb9bb225f279ca"
+  url "https://github.com/nest/nest-simulator/archive/v3.2.tar.gz"
+  sha256 "583d5725882ad5e8fd4fc7ffab425da97cbbb91fadbc327e940c184e8892b958"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
-    sha256 arm64_monterey: "3597909925e145c9bbeeec507d6b2edd069cd2dba0dd6d805438e01abb07280b"
-    sha256 arm64_big_sur:  "4360a0644a67b77a7f38c5024f590149358a596d73d75c1d5cb264d6cafb3ade"
-    sha256 monterey:       "3b7ee8c63f2fecf4639911e74baa91ed3a66237ecc56790abc15d6d012ed6bd5"
-    sha256 big_sur:        "5654dd09df57e301826de686012a935ddf37f25b2165c52daefbd20a58f6cbfb"
-    sha256 catalina:       "f90518fa24a2f931a03e27baf400c421b358cb1a64f4d2a757059d3ffa093b72"
-    sha256 mojave:         "845b603df3ce444c2b460eb07880c37e44c92afb5e38bfe04a2161161d6fecdc"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nest"
+    sha256 mojave: "f0526b22299023ff8915f5fd75f8f51be62deceeea3064a00ffc3f16a85b0e16"
   end
 
   depends_on "cmake" => :build
