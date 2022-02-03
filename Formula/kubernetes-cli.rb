72d4f9edc3bcae02ba106cad6702abe077e4ab2a
Adrian Ho <the.gromgit@gmail.com>
2022-02-03 20:12:16 +0800

    kubernetes-cli: update 1.23.3 bottle.

--- Formula/kubernetes-cli.rb
+++ Formula/kubernetes-cli.rb
@@ -2,8 +2,8 @@ class KubernetesCli < Formula
   desc "Kubernetes command-line interface"
   homepage "https://kubernetes.io/"
   url "https://github.com/kubernetes/kubernetes.git",
-      tag:      "v1.23.2",
-      revision: "9d142434e3af351a628bffee3939e64c681afa4d"
+      tag:      "v1.23.3",
+      revision: "816c97ab8cff8a1c72eccca1026f7820e93e0d25"
   license "Apache-2.0"
   head "https://github.com/kubernetes/kubernetes.git", branch: "master"
 
@@ -14,7 +14,7 @@ class KubernetesCli < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kubernetes-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "7adc21f42d36f91ee076282ce57ec37a671570b97eb4f533002876e6c96881a0"
+    sha256 cellar: :any_skip_relocation, mojave: "93cdef2748e74798744eb807de77a159bf040fc101b7b8c750783926c8b1c959"
   end
 
   depends_on "bash" => :build
@@ -61,7 +61,7 @@ class KubernetesCli < Formula
     version_output = shell_output("#{bin}/kubectl version --client 2>&1")
     assert_match "GitTreeState:\"clean\"", version_output
     if build.stable?
-      revision = stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision]
+      revision = stable.specs[:revision]
       assert_match revision.to_s, version_output
     end
   end
