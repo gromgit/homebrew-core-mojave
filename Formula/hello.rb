fecb83b8178d0e99886a0383f18838f8e1039bfc
Adrian Ho <the.gromgit@gmail.com>
2022-02-03 14:55:29 +0800

    hello: update 2.12 bottle.

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,13 +1,13 @@
 class Hello < Formula
   desc "Program providing model for GNU coding standards and practices"
   homepage "https://www.gnu.org/software/hello/"
-  url "https://ftp.gnu.org/gnu/hello/hello-2.11.tar.gz"
-  sha256 "8c9ce0572d3c44ed0670eb1cde980584e038b6f62c25fdfde8ef128de15004bd"
+  url "https://ftp.gnu.org/gnu/hello/hello-2.12.tar.gz"
+  sha256 "cf04af86dc085268c5f4470fbae49b18afbc221b78096aab842d934a76bad0ab"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/hello"
-    sha256 mojave: "f9ab50395d9cd57d31f441a6264263d584e4d1da63bbeaee74d9d8783599c395"
+    sha256 mojave: "4c4c464c456550bf172d462338c063edfc633da220217b5e5f75a909c564725e"
   end
 
   conflicts_with "perkeep", because: "both install `hello` binaries"
