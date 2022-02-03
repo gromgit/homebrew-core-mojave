f2c09448044e8587404ac502aab8472991403031
Adrian Ho <the.gromgit@gmail.com>
2022-02-03 19:12:39 +0800

    juliaup: update 1.5.34 bottle.

--- Formula/juliaup.rb
+++ Formula/juliaup.rb
@@ -1,20 +1,30 @@
 class Juliaup < Formula
   desc "Julia installer and version multiplexer"
   homepage "https://github.com/JuliaLang/juliaup"
-  url "https://github.com/JuliaLang/juliaup/archive/v1.5.29.tar.gz"
-  sha256 "4dc02d9e7ecddf7364b1862d8b478c2a6f01e0eea9dd755e40e171c42d0b3511"
+  url "https://github.com/JuliaLang/juliaup/archive/v1.5.34.tar.gz"
+  sha256 "1b7abf27e161294ceb61c854abcad208ab1aa22d803fbf311be41a65cd19c73a"
   license "MIT"
   head "https://github.com/JuliaLang/juliaup.git", branch: "master"
 
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/juliaup"
-    sha256 cellar: :any_skip_relocation, mojave: "4fffdf7db4fbb95648208be937d7acc7de0d72f70fe22a749278be49e0553526"
+    sha256 cellar: :any_skip_relocation, mojave: "932fc5e63bc0fc51bf89ca64f21d6f7878358150e5c081709e8a67be71c337f0"
   end
 
   depends_on "rust" => :build
 
+  conflicts_with "julia", because: "both install `julia` binaries"
+
   def install
-    system "cargo", "install", *std_cargo_args
+    system "cargo", "install", "--bin", "juliaup", *std_cargo_args
+    system "cargo", "install", "--bin", "julialauncher", *std_cargo_args
+
+    bin.install_symlink "julialauncher" => "julia"
   end
 
   test do
