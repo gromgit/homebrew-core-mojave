0b1ef01754daa73eab3977fc28fa5de178414c62
Adrian Ho <the.gromgit@gmail.com>
2022-02-04 06:10:23 +0800

    okteto: update 1.15.2 bottle.

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -1,14 +1,14 @@
 class Okteto < Formula
   desc "Build better apps by developing and testing code directly in Kubernetes"
   homepage "https://okteto.com"
-  url "https://github.com/okteto/okteto/archive/1.14.9.tar.gz"
-  sha256 "54d6a90d5404ff26b0286ee970455c6620af62018e6f48bcdcb21ca49bc1ed5a"
+  url "https://github.com/okteto/okteto/archive/1.15.2.tar.gz"
+  sha256 "75b9d033bf7545274cdbd01d82bf9242b61d419670407c633009e16149beac7a"
   license "Apache-2.0"
   head "https://github.com/okteto/okteto.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/okteto"
-    sha256 cellar: :any_skip_relocation, mojave: "aa561d823d0e29817ff5485a8bd01dd5af9cf54c25f77639bfac74d26ed705c9"
+    sha256 cellar: :any_skip_relocation, mojave: "955bd0f7791589fc736a58a8bd4bdd23f12e4945ac46bd34c01d4ea2746872cf"
   end
 
   depends_on "go" => :build
