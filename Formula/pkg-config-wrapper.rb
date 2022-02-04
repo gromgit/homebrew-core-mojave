40b4de9bda81ad35c7370dfab02c4a9a3174ac17
Adrian Ho <the.gromgit@gmail.com>
2022-02-04 08:48:18 +0800

    pkg-config-wrapper: update 0.2.11 bottle.

--- Formula/pkg-config-wrapper.rb
+++ Formula/pkg-config-wrapper.rb
@@ -1,8 +1,8 @@
 class PkgConfigWrapper < Formula
   desc "Easier way to include C code in your Go program"
   homepage "https://github.com/influxdata/pkg-config"
-  url "https://github.com/influxdata/pkg-config/archive/v0.2.10.tar.gz"
-  sha256 "460b389eeccf5e2e073ba3c77c04e19181e25e67e55891c75d6a46de811f60ce"
+  url "https://github.com/influxdata/pkg-config/archive/v0.2.11.tar.gz"
+  sha256 "52b22c151163dfb051fd44e7d103fc4cde6ae8ff852ffc13adeef19d21c36682"
   license "MIT"
   head "https://github.com/influxdata/pkg-config.git", branch: "master"
 
@@ -13,7 +13,7 @@ class PkgConfigWrapper < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pkg-config-wrapper"
-    sha256 cellar: :any_skip_relocation, mojave: "b0a019fa69983f6777a878a89794b60ab19fc309ac54a10178d474f9dcf4f6ce"
+    sha256 cellar: :any_skip_relocation, mojave: "2ed761847ed1fad5385215027a45a9267987ed31e2a73983722004c5bd3e2c15"
   end
 
   depends_on "go" => :build
