9cbce8a5a645717fdd3014469ba9bdc070c60702
Adrian Ho <the.gromgit@gmail.com>
2022-02-04 09:01:58 +0800

    pnpm: update 6.28.0 bottle.

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -3,8 +3,8 @@ class Pnpm < Formula
 
   desc "📦🚀 Fast, disk space efficient package manager"
   homepage "https://pnpm.io/"
-  url "https://registry.npmjs.org/pnpm/-/pnpm-6.27.1.tgz"
-  sha256 "ef7e261dd5f3304488536b70d9ea37c4405f2bfea3a85fc113d0513967fcfdf8"
+  url "https://registry.npmjs.org/pnpm/-/pnpm-6.28.0.tgz"
+  sha256 "3ac229d647ae6d0a343ed78de4583e4b6408534adc9f13e1d27a7781b2d4bb34"
   license "MIT"
 
   livecheck do
@@ -14,7 +14,7 @@ class Pnpm < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pnpm"
-    sha256 cellar: :any_skip_relocation, mojave: "0605044cff0b88c6b24f34c271fab8dc170c8698748feeafd29cc6279c786094"
+    sha256 cellar: :any_skip_relocation, mojave: "7162b5e06bb1a786ec59576d3206894c62d9ae8130c154119a226ca89a780858"
   end
 
   depends_on "node"
