1895bfd3082838c66a59860d65a019f98fbfa251
Adrian Ho <the.gromgit@gmail.com>
2022-02-05 02:23:02 +0800

    zabbix: update 5.4.10 bottle.

--- Formula/zabbix.rb
+++ Formula/zabbix.rb
@@ -1,14 +1,14 @@
 class Zabbix < Formula
   desc "Availability and monitoring solution"
   homepage "https://www.zabbix.com/"
-  url "https://cdn.zabbix.com/zabbix/sources/stable/5.4/zabbix-5.4.8.tar.gz"
-  sha256 "3f61f8c9360789dc9b163e3797af0ed8474618d475594e4bf33c4198fd830b70"
+  url "https://cdn.zabbix.com/zabbix/sources/stable/5.4/zabbix-5.4.10.tar.gz"
+  sha256 "462b1c7506f22545916a8c6ffc50c297c952e21125a9240dedcf858070e59877"
   license "GPL-2.0-or-later"
   head "https://github.com/zabbix/zabbix.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/zabbix"
-    sha256 mojave: "d607d1c5310bdb90df2712685e2159a6e0832adab4eba22b7efeb25c38fc3183"
+    sha256 mojave: "c449ea682bf4a2cb1fdadd0f289e3b21580a63aec394fc2c009c82a02f3a31b7"
   end
 
   depends_on "openssl@1.1"
