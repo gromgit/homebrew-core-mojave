84d6a5b23044e59e432d3468bbee2b47a3f10af0
Adrian Ho <the.gromgit@gmail.com>
2022-02-04 21:04:16 +0800

    ungit: update 1.5.19 bottle.

--- Formula/ungit.rb
+++ Formula/ungit.rb
@@ -3,18 +3,13 @@ require "language/node"
 class Ungit < Formula
   desc "Easiest way to use Git. On any platform. Anywhere"
   homepage "https://github.com/FredrikNoren/ungit"
-  url "https://registry.npmjs.org/ungit/-/ungit-1.5.18.tgz"
-  sha256 "7bc917ccbb9aa33773b8915400c4c7fbae375a346dba6613e5b0eaae7cad6fb8"
+  url "https://registry.npmjs.org/ungit/-/ungit-1.5.19.tgz"
+  sha256 "d9eab193b6202e8b28cc0c8bffd85335cb7d0aec8eba853526a17508ccb1b915"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "26ee350bc93b45aca15ea4ddfecad513b4fe07f2ca143ba215241387572a5afb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "54ab4e9703e79db7145005c5f4e8a2b09001bc6a6a4c0ca0e074d20cb71b252a"
-    sha256 cellar: :any_skip_relocation, monterey:       "4fa377f0d9fc242605e26308f4ddd90b317703bad6d48bd25f1175de5d1ee7ae"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6768008e7ce0700279019566b2c5fab0a906823696b225e54b883bc30d4b52b7"
-    sha256 cellar: :any_skip_relocation, catalina:       "6768008e7ce0700279019566b2c5fab0a906823696b225e54b883bc30d4b52b7"
-    sha256 cellar: :any_skip_relocation, mojave:         "6768008e7ce0700279019566b2c5fab0a906823696b225e54b883bc30d4b52b7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5fed1c88fef55106397629b390d0c369cb1b7f179922b9b938e9e5b9a02f0698"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ungit"
+    sha256 cellar: :any_skip_relocation, mojave: "dac8f3bc8f82ef053aaa7ffeb96456993515ad993aac179193e7a581ded9c31e"
   end
 
   depends_on "node"
