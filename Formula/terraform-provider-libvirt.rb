ccf477ba7f70068d2d77ccd6d16bf898911d3ab8
Adrian Ho <the.gromgit@gmail.com>
2022-02-04 19:23:11 +0800

    terraform-provider-libvirt: update 0.6.14 bottle.

--- Formula/terraform-provider-libvirt.rb
+++ Formula/terraform-provider-libvirt.rb
@@ -1,13 +1,13 @@
 class TerraformProviderLibvirt < Formula
   desc "Terraform provisioning with Linux KVM using libvirt"
   homepage "https://github.com/dmacvicar/terraform-provider-libvirt"
-  url "https://github.com/dmacvicar/terraform-provider-libvirt/archive/v0.6.12.tar.gz"
-  sha256 "67c78741f6d6c32664a346929bd81e736d06a8459249f0953b0a3019335dabed"
+  url "https://github.com/dmacvicar/terraform-provider-libvirt/archive/v0.6.14.tar.gz"
+  sha256 "0d429663ee29e4bf5138fe31245db378bafe81011adadf2983b307af0e5356a2"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/terraform-provider-libvirt"
-    sha256 cellar: :any_skip_relocation, mojave: "a5faf7448bd2975fc1d4fdc5a09c07e91155b771dfce879c2a826fe4c8358328"
+    sha256 cellar: :any_skip_relocation, mojave: "5e807118abfc66ea5d56bf95802555caaa443bfe2ab62f9fd71168632e053b23"
   end
 
   depends_on "go" => :build
