75079b0a1129860b07d4bdfff8d563fc7e440567
Adrian Ho <the.gromgit@gmail.com>
2022-02-04 16:49:16 +0800

    sofia-sip: update 1.13.7 bottle.

--- Formula/sofia-sip.rb
+++ Formula/sofia-sip.rb
@@ -1,14 +1,13 @@
 class SofiaSip < Formula
   desc "SIP User-Agent library"
   homepage "https://sofia-sip.sourceforge.io/"
-  url "https://github.com/freeswitch/sofia-sip/archive/v1.13.6.tar.gz"
-  sha256 "6f3cb48a35929abd3454087b32ad4c75fa5fe50fab8a9cc6f98559e6fd1bd64b"
+  url "https://github.com/freeswitch/sofia-sip/archive/v1.13.7.tar.gz"
+  sha256 "3bdcbe80a066c9cafa8d947d51512b86ed56bf2cdbb25dbe9b8eef6a8bab6a25"
   license "LGPL-2.1-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sofia-sip"
-    rebuild 2
-    sha256 cellar: :any, mojave: "1d5e5ac3e37a112d041bcbe367cc0b56395d139f58dcb4bb49afae144c9614b2"
+    sha256 cellar: :any, mojave: "7257ccea7de7dfb997ffb9864c0d90410655d97b104d665538576ff226957b6d"
   end
 
   depends_on "autoconf" => :build
