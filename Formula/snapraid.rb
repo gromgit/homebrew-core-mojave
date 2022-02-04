8dd858a5daeae45a5eb3adaa4ebe672abe0b1492
Adrian Ho <the.gromgit@gmail.com>
2022-02-04 16:35:50 +0800

    snapraid: update 12.1 bottle.

--- Formula/snapraid.rb
+++ Formula/snapraid.rb
@@ -1,13 +1,13 @@
 class Snapraid < Formula
   desc "Backup program for disk arrays"
-  homepage "https://snapraid.sourceforge.io/"
-  url "https://github.com/amadvance/snapraid/releases/download/v12.0/snapraid-12.0.tar.gz"
-  sha256 "f07652261e9821a5adfbfa8dad3350aae3e7c285f42a6bd7d96a854e5bc56dda"
+  homepage "https://www.snapraid.it/"
+  url "https://github.com/amadvance/snapraid/releases/download/v12.1/snapraid-12.1.tar.gz"
+  sha256 "49337d9bafa96c2beac0125463bd22622be2fc00f3b4dee7e4b0e864d2a49661"
   license "GPL-3.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/snapraid"
-    sha256 cellar: :any_skip_relocation, mojave: "cbcb5ee0038df3ef80cf685ebf27085772999e3babcd13e37cfa807db0e97f48"
+    sha256 cellar: :any_skip_relocation, mojave: "1dd15166d1d814641da356ff685511b28316e310401e08006e80d5896aadd3eb"
   end
 
   head do
