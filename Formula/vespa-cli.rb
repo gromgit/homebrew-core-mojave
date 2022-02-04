3ec8c9258ef31aadc5f10851236a1cac5576c87a
Adrian Ho <the.gromgit@gmail.com>
2022-02-04 22:57:04 +0800

    vespa-cli: update 7.534.29 bottle.

--- Formula/vespa-cli.rb
+++ Formula/vespa-cli.rb
@@ -1,8 +1,8 @@
 class VespaCli < Formula
   desc "Command-line tool for Vespa.ai"
   homepage "https://vespa.ai"
-  url "https://github.com/vespa-engine/vespa/archive/v7.528.38.tar.gz"
-  sha256 "f74a389711c4203e64a2cd6849c4a36bccd75f625021428d14a6af9364f6b729"
+  url "https://github.com/vespa-engine/vespa/archive/v7.534.29.tar.gz"
+  sha256 "40903560256fa239c1bde4e3334f6044c6a0d01bd7c0bcdf17b4ca41337278b7"
   license "Apache-2.0"
 
   livecheck do
@@ -13,7 +13,7 @@ class VespaCli < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/vespa-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "a58708e4e218f73ba9fbed8abad6408b2e8757b92e1d183b7d150c4ef7f70424"
+    sha256 cellar: :any_skip_relocation, mojave: "1ecfc6e047ddb7d88f3d65f46da8da31f66828f0b3720d0a339dc6e51e8a0c06"
   end
 
   depends_on "go" => :build
