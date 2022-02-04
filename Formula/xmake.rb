bcdb9cbad282a31bedeabc1980b691b09b7cfdca
Adrian Ho <the.gromgit@gmail.com>
2022-02-05 01:52:22 +0800

    xmake: update 2.6.3 bottle.

--- Formula/xmake.rb
+++ Formula/xmake.rb
@@ -1,14 +1,14 @@
 class Xmake < Formula
   desc "Cross-platform build utility based on Lua"
   homepage "https://xmake.io/"
-  url "https://github.com/xmake-io/xmake/releases/download/v2.6.2/xmake-v2.6.2.tar.gz"
-  sha256 "845f4bb50e300f75ed819755702e586614945eddcc74bc1dcef38c5cc107cebf"
+  url "https://github.com/xmake-io/xmake/releases/download/v2.6.3/xmake-v2.6.3.tar.gz"
+  sha256 "43c739b8faa3be986f95421c7ba1e135ff18d4723fb88b7e0bb0cdd27ef37430"
   license "Apache-2.0"
   head "https://github.com/xmake-io/xmake.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/xmake"
-    sha256 cellar: :any_skip_relocation, mojave: "b681a0914535896e4d6c7292737b558c75911b97cd0549c0af22059d683cf0d1"
+    sha256 cellar: :any_skip_relocation, mojave: "d239bf9dba07b35e5f32365060f51606b11af0f80c29d8bcc73609b849489c88"
   end
 
   on_linux do
