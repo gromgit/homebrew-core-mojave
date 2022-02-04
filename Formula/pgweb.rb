2417c59d17b3028aec7a9d0e4020939e2ec4cf3c
Adrian Ho <the.gromgit@gmail.com>
2022-02-04 08:29:36 +0800

    pgweb: update 0.11.10 bottle.

--- Formula/pgweb.rb
+++ Formula/pgweb.rb
@@ -1,18 +1,13 @@
 class Pgweb < Formula
   desc "Web-based PostgreSQL database browser"
   homepage "https://sosedoff.github.io/pgweb/"
-  url "https://github.com/sosedoff/pgweb/archive/v0.11.9.tar.gz"
-  sha256 "2b93e8ebbb381e480c70a4c25ba62b7bb31a04e60be52951ddd874f603bd3789"
+  url "https://github.com/sosedoff/pgweb/archive/v0.11.10.tar.gz"
+  sha256 "df2d16daa6f8fa9735b713199691ef74136f19fbbee14778248e41e247a1b93b"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9119c191615e7b656cd56561be91fa6491205df19a7f744c6a206946bd6916df"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8850d3804355efc5d38091e65d3f534526dfb1d054948f7204ebeafabf6cbe67"
-    sha256 cellar: :any_skip_relocation, monterey:       "52c113300ac67f9baa714f347a32cf5f6b744527d307fa3a3c975d85ca4c7e20"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ec49476c0f86d53843e63e7d0d19bce5a7f8db471bba1d050d0650bea83f8e21"
-    sha256 cellar: :any_skip_relocation, catalina:       "973ebc01c360e25e1b88e274e93e6a948390eeaadd669901916aaffb91888d5f"
-    sha256 cellar: :any_skip_relocation, mojave:         "94a5ede934e422a47e9ba408b6dd5ff7182ff4b69301420b220f4ec6fffdccfb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2fcde820a38532a5a5371ff9a390841b9013ba5a40e814002092a40061c98b78"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pgweb"
+    sha256 cellar: :any_skip_relocation, mojave: "4c0ac360423864b7ba1f8be9612b268f13709796a0490d2548f7465da0ec7553"
   end
 
   depends_on "go" => :build
