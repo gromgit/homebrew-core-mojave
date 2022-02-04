0fc0856bb9f539028904246710bfd6f392fce389
Adrian Ho <the.gromgit@gmail.com>
2022-02-04 08:24:58 +0800

    pgbadger: update 11.7 bottle.

--- Formula/pgbadger.rb
+++ Formula/pgbadger.rb
@@ -1,19 +1,14 @@
 class Pgbadger < Formula
   desc "Log analyzer for PostgreSQL"
   homepage "https://pgbadger.darold.net/"
-  url "https://github.com/darold/pgbadger/archive/v11.6.tar.gz"
-  sha256 "9c8744f76123021c2d15da8521d77b3f529e50702fb3518c4d0e0311e457a3ec"
+  url "https://github.com/darold/pgbadger/archive/v11.7.tar.gz"
+  sha256 "1bd5650440968ab942aa976f4e10aceb2b4c5064ce9549fea5142b66f8ff44d2"
   license "PostgreSQL"
   head "https://github.com/darold/pgbadger.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "407cc95231c8ecb69b60d6e2b2947deee4ab1cf8df30d7e7bbfa0e7fa918437b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fea3dce312139a44c49a6591af532b8463f798cfdaca19107b19a24e21a92d86"
-    sha256 cellar: :any_skip_relocation, monterey:       "26e871f3cc1802bff8a92bb086df07fa96795e77d22fb2a93aa980ffb23b9aed"
-    sha256 cellar: :any_skip_relocation, big_sur:        "354a71298cbd1d666fd5680e1e146f151c9a64babc067530b9c23f06ac49c844"
-    sha256 cellar: :any_skip_relocation, catalina:       "70b715507e7d1a6daa76613ad2cd6a1fb41a54c7e0afc1a0789d0550b651de3c"
-    sha256 cellar: :any_skip_relocation, mojave:         "70b715507e7d1a6daa76613ad2cd6a1fb41a54c7e0afc1a0789d0550b651de3c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bab9bbfdca9d66cfcd8eff69fc79c57e136dc66a8cb95475e888ce583fd27926"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pgbadger"
+    sha256 cellar: :any_skip_relocation, mojave: "529e3761629b32e3f6a374cd8c54770293cc24a77274567667d9be068aba3e7d"
   end
 
   def install
