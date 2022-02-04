7aeb173a6a79794e3ae150b1860e76207e0f5d57
Adrian Ho <the.gromgit@gmail.com>
2022-02-04 18:46:17 +0800

    tektoncd-cli: update 0.22.0 bottle.

--- Formula/tektoncd-cli.rb
+++ Formula/tektoncd-cli.rb
@@ -1,10 +1,9 @@
 class TektoncdCli < Formula
   desc "CLI for interacting with TektonCD"
   homepage "https://github.com/tektoncd/cli"
-  url "https://github.com/tektoncd/cli/archive/v0.21.0.tar.gz"
-  sha256 "c6fed13ddd0cbb410230cf85749458a42b9cb8d56ad6ec48b44e70e26cce9062"
+  url "https://github.com/tektoncd/cli/archive/v0.22.0.tar.gz"
+  sha256 "6aea6f79f97f17e707a13ea16c75b2693ad2e942991285452e11be240d3411f3"
   license "Apache-2.0"
-  revision 1
 
   livecheck do
     url :stable
@@ -13,8 +12,7 @@ class TektoncdCli < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tektoncd-cli"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "46929ed92bdd3508ade36fcaa7e4801bd80be6dd2e0db5b7d79144afd1867f04"
+    sha256 cellar: :any_skip_relocation, mojave: "c4860345cb1f11f4c8a3289a6d24ffc23afe6622786db0e4d4da5181981ba8e7"
   end
 
   depends_on "go" => :build
