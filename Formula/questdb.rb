45492670709ef8e7bcbe4fe2813e4859af5e464b
Adrian Ho <the.gromgit@gmail.com>
2022-02-04 11:37:23 +0800

    questdb: update 6.2 bottle.

--- Formula/questdb.rb
+++ Formula/questdb.rb
@@ -1,14 +1,13 @@
 class Questdb < Formula
   desc "Time Series Database"
   homepage "https://questdb.io"
-  url "https://github.com/questdb/questdb/releases/download/6.1.2/questdb-6.1.2-no-jre-bin.tar.gz"
-  sha256 "2a295d54125083bd0437b5360ec264819d2cf8a2ab78d95e84368e76cb9b9952"
+  url "https://github.com/questdb/questdb/releases/download/6.2/questdb-6.2-no-jre-bin.tar.gz"
+  sha256 "33eef97f93c80157b0e836795c47f4a3ee5c40a614fbc12f0c0988b5e51644ef"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/questdb"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "1521a064a9656378e459570ad57250ca2b6b3b29ce8f5ac0cdba52bae6080325"
+    sha256 cellar: :any_skip_relocation, mojave: "2a5cc758ff7ab9098d459ea1aab1ee04c1f942613162f1fdb12652ec16530678"
   end
 
   depends_on "openjdk@11"
