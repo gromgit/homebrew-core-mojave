9014aefeb2690806502f10f01a74edff20eaafbd
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-05 23:34:17 +0800

    babel: no rebuilding required

--- Formula/babel.rb
+++ Formula/babel.rb
@@ -4,12 +4,12 @@ require "json"
 class Babel < Formula
   desc "Compiler for writing next generation JavaScript"
   homepage "https://babeljs.io/"
-  url "https://registry.npmjs.org/@babel/core/-/core-7.16.12.tgz"
-  sha256 "529a1a02ee1202c86477db4878173d9d840a58cf2dc1920e9805a4d89e28317e"
+  url "https://registry.npmjs.org/@babel/core/-/core-7.17.0.tgz"
+  sha256 "71d3c48e059d2aceee50c59800047527aeb93975f444cdc3f036120ca0150d75"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "4c6a7f0a7c413e703364f1b0b6360f012654c5d2495ff0b9c0bf64a58392ddee"
+    sha256 cellar: :any_skip_relocation, all: "e85d935ce01b79da587732cd8e647d59749c3bc087f49946f50791ed0dc3428a"
   end
 
   depends_on "node"
