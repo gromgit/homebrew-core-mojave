768f5f8032e815e4f830895e1499f92580268c11
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-05 23:34:25 +0800

    lsusb: no rebuilding required

--- Formula/lsusb.rb
+++ Formula/lsusb.rb
@@ -6,17 +6,13 @@ class Lsusb < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3ad2f30f538b1ec0122a492b6e5f07ee70b2d7f2bb9e1cd267b8879b7534dc7d"
-    sha256 cellar: :any_skip_relocation, big_sur:       "a1b7ef763a83e68074528407469fae8fd265269981332d139ab0acca67ea4376"
-    sha256 cellar: :any_skip_relocation, catalina:      "7ab2cb027f186840ea1c96e47b4d48a8dfc42d91847d79bdd3faa6677ef603ca"
-    sha256 cellar: :any_skip_relocation, mojave:        "4f2f4f45cb6df2d5262bb823e02f750e7e5b4f117dca8a41fc6956435a277cb9"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "e696db36d09169064b3e97852d07464125e5bc6e400cb2a4cc186e6aa606574a"
-    sha256 cellar: :any_skip_relocation, sierra:        "e696db36d09169064b3e97852d07464125e5bc6e400cb2a4cc186e6aa606574a"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "e696db36d09169064b3e97852d07464125e5bc6e400cb2a4cc186e6aa606574a"
-    sha256 cellar: :any_skip_relocation, all:           "cb58c67458e54e712ac52a4e6ffcaab384ad2e7b61bad32c21daae3f16a42d5a"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "03895259bbcb43d072aacd4d000cb28807f5bff928bc8585663320beed5a7409"
   end
 
   depends_on :macos
+
+  conflicts_with "usbutils", because: "both provide an `lsusb` binary"
 
   def install
     bin.install "lsusb"
