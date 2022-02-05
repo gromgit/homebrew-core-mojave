85b21da61b99109d83ae14d6a6c3791ce617ce78
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-05 23:34:23 +0800

    ipv6toolkit: no rebuilding required

--- Formula/ipv6toolkit.rb
+++ Formula/ipv6toolkit.rb
@@ -1,13 +1,13 @@
 class Ipv6toolkit < Formula
   desc "Security assessment and troubleshooting tool for IPv6"
   homepage "https://www.si6networks.com/research/tools/ipv6toolkit/"
-  url "http://pages.cs.wisc.edu/~plonka/ipv6toolkit/ipv6toolkit-v2.0.tar.gz"
+  url "https://pages.cs.wisc.edu/~plonka/ipv6toolkit/ipv6toolkit-v2.0.tar.gz"
   sha256 "16f13d3e7d17940ff53f028ef0090e4aa3a193a224c97728b07ea6e26a19e987"
   license "GPL-3.0-or-later"
-  head "https://github.com/fgont/ipv6toolkit.git"
+  head "https://github.com/fgont/ipv6toolkit.git", branch: "master"
 
   livecheck do
-    url "http://pages.cs.wisc.edu/~plonka/ipv6toolkit/"
+    url "https://pages.cs.wisc.edu/~plonka/ipv6toolkit/"
     regex(/href=.*?ipv6toolkit[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
