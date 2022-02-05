f13cca153b08fca7b69ebb80331cfedde1ea5d50
Adrian Ho <the.gromgit@gmail.com>
2022-02-06 02:26:55 +0800

    atlas: update 0.3.3_1 bottle.

--- Formula/atlas.rb
+++ Formula/atlas.rb
@@ -1,14 +1,15 @@
 class Atlas < Formula
   desc "Database toolkit"
   homepage "https://atlasgo.io/"
-  url "https://github.com/ariga/atlas/archive/v0.3.2.tar.gz"
-  sha256 "c295db029aaa11e6ae2b2db477c90674e1261f4680f75e97b90525385a33bfbb"
+  url "https://github.com/ariga/atlas/archive/v0.3.3.tar.gz"
+  sha256 "62ceb2609c6a4ffc11e79e7497ec45e48bd9785b1dd986e1220bc3ad54d96c4e"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/ariga/atlas.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/atlas"
-    sha256 cellar: :any_skip_relocation, mojave: "124993401b9d339df2e3a42e57d1c99c2eaf641d7b82b56168cf8ee958632b0e"
+    sha256 cellar: :any_skip_relocation, mojave: "7a302f2f598a37ec040d45d3b5b569befc3ba484b873a5cee330d775e8c5a2ec"
   end
 
   depends_on "go" => :build
@@ -19,6 +20,15 @@ class Atlas < Formula
       -X ariga.io/atlas/cmd/action.version=v#{version}
     ]
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/atlas"
+
+    bash_output = Utils.safe_popen_read(bin/"atlas", "completion", "bash")
+    (bash_completion/"atlas").write bash_output
+
+    zsh_output = Utils.safe_popen_read(bin/"atlas", "completion", "zsh")
+    (zsh_completion/"_atlas").write zsh_output
+
+    fish_output = Utils.safe_popen_read(bin/"atlas", "completion", "fish")
+    (fish_completion/"atlas.fish").write fish_output
   end
 
   test do
