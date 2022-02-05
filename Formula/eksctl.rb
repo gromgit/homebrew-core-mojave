0d6ee8fc0286b783831b537d7983341e195e2aa6
Adrian Ho <the.gromgit@gmail.com>
2022-02-06 06:49:03 +0800

    eksctl: update 0.82.0 bottle.

--- Formula/eksctl.rb
+++ Formula/eksctl.rb
@@ -2,14 +2,14 @@ class Eksctl < Formula
   desc "Simple command-line tool for creating clusters on Amazon EKS"
   homepage "https://eksctl.io"
   url "https://github.com/weaveworks/eksctl.git",
-      tag:      "0.79.0",
-      revision: "e70880d06dff8b461ab667f58c39765c75adf37d"
+      tag:      "0.82.0",
+      revision: "254eca4e9820628f9c48215f7d861663aae2a26b"
   license "Apache-2.0"
   head "https://github.com/weaveworks/eksctl.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/eksctl"
-    sha256 cellar: :any_skip_relocation, mojave: "bfdb48c99c9c7390df3ee549acd666e364c05bac7cb92cb533d1a2864a2ce817"
+    sha256 cellar: :any_skip_relocation, mojave: "aed284f72527f82d170c0f2d06652575bf762c5d2f61af08f625022a28afe014"
   end
 
   depends_on "counterfeiter" => :build
