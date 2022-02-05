7707246df71a1472a7b654be1bc56f47b2b4c1bd
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-05 23:34:31 +0800

    vulkan-headers: no rebuilding required

--- Formula/vulkan-headers.rb
+++ Formula/vulkan-headers.rb
@@ -1,12 +1,12 @@
 class VulkanHeaders < Formula
   desc "Vulkan Header files and API registry"
   homepage "https://github.com/KhronosGroup/Vulkan-Headers"
-  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.3.204.tar.gz"
-  sha256 "2cde2c90975a8f36d00cf8eb6308cb00323aafbc0b7374f4b346f1140f760b3d"
+  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.3.205.tar.gz"
+  sha256 "f6e901ef1619e7ad526212f07e516343d36ce50f61c7e0c672272551991fa7fa"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "eab5f886659cfa995c2e2a09b2c22a48e7d09e2d876efa8733bf3eb78b15f92f"
+    sha256 cellar: :any_skip_relocation, all: "b0498c4c3d187bbfa6eb83c72f19cb56f8d0f9c8825616133f501bdfb8590af1"
   end
 
   depends_on "cmake" => :build
