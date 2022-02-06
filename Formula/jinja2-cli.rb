1bad0b904217029d59cb210757e8011baaca9faf
Adrian Ho <the.gromgit@gmail.com>
2022-02-06 13:51:52 +0800

    jinja2-cli: update 0.8.1 bottle.

--- Formula/jinja2-cli.rb
+++ Formula/jinja2-cli.rb
@@ -3,26 +3,20 @@ class Jinja2Cli < Formula
 
   desc "CLI for the Jinja2 templating language"
   homepage "https://github.com/mattrobenolt/jinja2-cli"
-  url "https://files.pythonhosted.org/packages/23/67/6f05f5f8a9fc108c58e4eac9b9b7876b400985d33149fe2faa87a9ca502b/jinja2-cli-0.7.0.tar.gz"
-  sha256 "9ccd8d530dad5d031230afd968cf54637b49842a13ececa6e17c2f67f6e9336e"
+  url "https://files.pythonhosted.org/packages/0c/df/c16c1757b0cd37c282be4f7bb2addcdf3514272d180ae2ed290a5d2472cd/jinja2-cli-0.8.1.tar.gz"
+  sha256 "fb1173811ed5b54205c65131374f342fcb924a5123af53a65fe1ffa7eb40bf19"
   license "BSD-2-Clause"
-  revision 4
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5e7e9f7d2a1b88f8f5d2d7484edeb62234a51d045b578df6963b044298e58615"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "11a23f02be9916f831a4e8639e9c581a1a22495086e36dbc9155d1e02c08ef66"
-    sha256 cellar: :any_skip_relocation, monterey:       "2391d625b1078379209b67009c9a0d2b93e8159a99eaba018c1e04422c568a0e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7fbc8bb7f975a94812101fd094963fd43605e463ca6e18ac524cb292eda60863"
-    sha256 cellar: :any_skip_relocation, catalina:       "9c776168317a1180b6a8f517bba3821f98fb5ff23fda662f1398b98cbab5df2f"
-    sha256 cellar: :any_skip_relocation, mojave:         "5cbea09cdfdc17b06ecd92cb8e209fc1a97996b7a3db2624d86c80da889f6c6d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9f0cc1ad20dc99d07c505ce4a99039f13946bfe0c804e732c0768dbd1482cd9e"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jinja2-cli"
+    sha256 cellar: :any_skip_relocation, mojave: "e2672fa02b81f822b4620c2a4d8a6b5785ad7b3efd4a3ac80a625a43c0890b7d"
   end
 
   depends_on "python@3.10"
 
   resource "Jinja2" do
-    url "https://files.pythonhosted.org/packages/f8/86/7c0eb6e8b05385d1ce682abc0f994abd1668e148fb52603fa86e15d4c110/Jinja2-3.0.2.tar.gz"
-    sha256 "827a0e32839ab1600d4eb1c4c33ec5a8edfbc5cb42dafa13b81f182f97784b45"
+    url "https://files.pythonhosted.org/packages/91/a5/429efc6246119e1e3fbf562c00187d04e83e54619249eb732bb423efa6c6/Jinja2-3.0.3.tar.gz"
+    sha256 "611bb273cd68f3b993fabdc4064fc858c5b47a973cb5aa7999ec1ba405c87cd7"
   end
 
   resource "MarkupSafe" do
