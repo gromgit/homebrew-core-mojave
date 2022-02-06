56c8d3581a5c22403786c1ae38812a1a9202679d
Adrian Ho <the.gromgit@gmail.com>
2022-02-06 13:25:18 +0800

    iperf3: update 3.11 bottle.

--- Formula/iperf3.rb
+++ Formula/iperf3.rb
@@ -1,18 +1,13 @@
 class Iperf3 < Formula
   desc "Update of iperf: measures TCP, UDP, and SCTP bandwidth"
   homepage "https://github.com/esnet/iperf"
-  url "https://github.com/esnet/iperf/archive/3.10.1.tar.gz"
-  sha256 "6a4bb4d5c124b3fa64dfbda469ab16857ad6565310bcaa3dd8cd32f96c2fc473"
+  url "https://github.com/esnet/iperf/archive/3.11.tar.gz"
+  sha256 "96e909c0d3ab6034c52328c2954fb3934aaff349395c4bc2611dcd50e6b89875"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "502bc663fcc6e2e74499818938b006fc2971e1a80378899af1bfd637fa0cad37"
-    sha256 cellar: :any,                 arm64_big_sur:  "7acd6b3fedf49e5614f825f928d24b035e93db311c25797d94b8ef880b78afe0"
-    sha256 cellar: :any,                 monterey:       "b69f87fbc6557c99e804b2aa57887a1a91d332ac336c435593fcf8ecc3883d2b"
-    sha256 cellar: :any,                 big_sur:        "3c7b4daa79c385333b89c08740c477c633d45eba240610f7d43f26a3397555b3"
-    sha256 cellar: :any,                 catalina:       "a3d90dab71e047c3c585dc6dfbb63b240b3842e02af8fdaee665bb13b3d71c82"
-    sha256 cellar: :any,                 mojave:         "dea44691e2528c58d8f30d5b0b60d2407bb3ba17db5c3d0327fc29442f911847"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6d078e2b5840746763092d35eede028ccf1b0db86274597ce064c14e5652cae7"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/iperf3"
+    sha256 cellar: :any, mojave: "88c9c25729e1aad39ae6827b29285f2902131af3cd11134efde408e1a97fb1e9"
   end
 
   head do
