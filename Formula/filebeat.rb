6545118ab64c83f0d1ed922b6fab31cee9dc6742
Adrian Ho <the.gromgit@gmail.com>
2022-02-06 08:05:18 +0800

    filebeat: update 7.17.0 bottle.

--- Formula/filebeat.rb
+++ Formula/filebeat.rb
@@ -2,8 +2,8 @@ class Filebeat < Formula
   desc "File harvester to ship log files to Elasticsearch or Logstash"
   homepage "https://www.elastic.co/products/beats/filebeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v7.16.3",
-      revision: "d420ccdaf201e32a524632b5da729522e50257ae"
+      tag:      "v7.17.0",
+      revision: "93708bd74e909e57ed5d9bea3cf2065f4cc43af3"
   # Outside of the "x-pack" folder, source code in a given file is licensed
   # under the Apache License Version 2.0
   license "Apache-2.0"
@@ -11,7 +11,7 @@ class Filebeat < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/filebeat"
-    sha256 cellar: :any_skip_relocation, mojave: "a5bc556ef02d9ed39fccb54c8d27e537ce3057484b2ff094e7b56861f0a4fb5b"
+    sha256 cellar: :any_skip_relocation, mojave: "ac3470ef13671a88dc05b7f542df82752e113de2a9bb0b979cfeadb6f50b8a48"
   end
 
   depends_on "go" => :build
