863d6c91c117f40424c6db88f9645991bc37886b
Adrian Ho <the.gromgit@gmail.com>
2022-02-06 10:08:43 +0800

    global: update 6.6.8 bottle.

--- Formula/global.rb
+++ Formula/global.rb
@@ -3,20 +3,14 @@ class Global < Formula
 
   desc "Source code tag system"
   homepage "https://www.gnu.org/software/global/"
-  url "https://ftp.gnu.org/gnu/global/global-6.6.7.tar.gz"
-  mirror "https://ftpmirror.gnu.org/global/global-6.6.7.tar.gz"
-  sha256 "69a0f77f53827c5568176c1d382166df361e74263a047f0b3058aa2f2ad58a3c"
+  url "https://ftp.gnu.org/gnu/global/global-6.6.8.tar.gz"
+  mirror "https://ftpmirror.gnu.org/global/global-6.6.8.tar.gz"
+  sha256 "6f93d9732a07175817907d26640a90dc1009918e02be761bba09d1fa068357cd"
   license "GPL-3.0-or-later"
-  revision 2
 
   bottle do
-    sha256 arm64_monterey: "020f4adf24f8ec865985ee316c21a28d723d04cbc5e40db25b7466821c4b1380"
-    sha256 arm64_big_sur:  "ac6e9593809808317fae95123ae900984864af0c1b7b75e1654943d4859755a3"
-    sha256 monterey:       "139ff10b422d0f54521e17d453d839468498986c890176951e9fb17f85d1d175"
-    sha256 big_sur:        "9db539acdbe29519f9a658b014a9b7a76f0d0df4ed9dd8140360366059c3bc79"
-    sha256 catalina:       "004ddf5a7ed57fb0793fb2b3680342643f30ca3429175c3d471a6746865a641d"
-    sha256 mojave:         "25ac8252698696fb8c6dcac75d86d6e46f2c1a7c40442463a9183defedf098f8"
-    sha256 x86_64_linux:   "e55a8b40f528a2b1047b5294f7403d3e56ab5f7efe76551f09db44b367b4acfd"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/global"
+    sha256 mojave: "fe8c5db6ef8ae93a46a87344ed8696b1b37679263251b3c15ebab44269c1f65e"
   end
 
   head do
@@ -30,36 +24,21 @@ class Global < Formula
     depends_on "libtool" => :build
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "ctags"
   depends_on "libtool"
   depends_on "ncurses"
   depends_on "python@3.10"
   depends_on "sqlite"
+  depends_on "universal-ctags"
 
   skip_clean "lib/gtags"
 
   resource "Pygments" do
-    url "https://files.pythonhosted.org/packages/ba/6e/7a7c13c21d8a4a7f82ccbfe257a045890d4dbf18c023f985f565f97393e3/Pygments-2.9.0.tar.gz"
-    sha256 "a18f47b506a429f6f4b9df81bb02beab9ca21d0a5fee38ed15aef65f0545519f"
-  end
-
-  # use homebrew sqlite instead of the older copy included in libdb/
-  # When removing the patch, check whether we can remove the
-  # autoconf/automake/libtool dependencies
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/bc4dc49c2476c2d4ffecb21bb76699e67cb57415/global/6.6.7-external-sqlite.patch"
-    sha256 "1b87c9b90a6555cd77c72de933303348e1e148b71a5976d4a0040a3038ef2627"
+    url "https://files.pythonhosted.org/packages/94/9c/cb656d06950268155f46d4f6ce25d7ffc51a0da47eadf1b164bbf23b718b/Pygments-2.11.2.tar.gz"
+    sha256 "4e426f72023d88d03b2fa258de560726ce890ff3b630f88c21cbb8b2503b8c6a"
   end
 
   def install
-    if build.head?
-      system "sh", "reconf.sh"
-    else
-      # Needed for the patch. Check that this can be removed when the patch is not necessary
-      system "autoreconf", "--force", "--install", "--symlink", "--verbose"
-    end
+    system "sh", "reconf.sh" if build.head?
 
     ENV.prepend_create_path "PYTHONPATH", libexec/Language::Python.site_packages("python3")
 
@@ -72,7 +51,7 @@ class Global < Formula
       --prefix=#{prefix}
       --sysconfdir=#{etc}
       --with-sqlite3=#{Formula["sqlite"].opt_prefix}
-      --with-exuberant-ctags=#{Formula["ctags"].opt_bin}/ctags
+      --with-universal-ctags=#{Formula["universal-ctags"].opt_bin}/ctags
     ]
 
     system "./configure", *args
