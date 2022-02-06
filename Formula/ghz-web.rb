b296bfa7ec3d79dea604a4260d08dc86c743582b
Adrian Ho <the.gromgit@gmail.com>
2022-02-06 09:13:23 +0800

    ghz-web: update 0.106.1 bottle.

--- Formula/ghz-web.rb
+++ Formula/ghz-web.rb
@@ -1,8 +1,8 @@
 class GhzWeb < Formula
   desc "Web interface for ghz"
   homepage "https://ghz.sh"
-  url "https://github.com/bojand/ghz/archive/v0.105.0.tar.gz"
-  sha256 "c2c257dcdf708e742ff80cd5a1b205991c9192cf857cafc90ed4be8ff2097ee1"
+  url "https://github.com/bojand/ghz/archive/v0.106.1.tar.gz"
+  sha256 "23220289e80920650d463413ede616b1b51b8a007301b344f76163b5b27fb0fd"
   license "Apache-2.0"
 
   livecheck do
@@ -10,13 +10,8 @@ class GhzWeb < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6e3912099f366892a5d15402f70331b0a101dddfbbf14dbc191a8e3439dd1968"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "889fb16e9087909d9657ecd3c77d7eae7e3e4cda4c4ea7d07ae75f93a7f19e10"
-    sha256 cellar: :any_skip_relocation, monterey:       "72150d0b50fec99f8ad719f5c4e1cd72a407d4f4662e30d576026071778a1436"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e47448fd61944713a7856142871d5c12349519b12e47f3f7cdc2046221f2ed3d"
-    sha256 cellar: :any_skip_relocation, catalina:       "0f6ba02a65297c33f7a94dde78e58992e9bc8cff24510a4a64469dd75416de2d"
-    sha256 cellar: :any_skip_relocation, mojave:         "8abc477e62f5f5c9473c666873123fb5743721913ecbd3b915793ff6313f8f87"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2ff5590f0cefb88c8a13ed03777b3a4e1494f7fe39d363f7133d16c315768670"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ghz-web"
+    sha256 cellar: :any_skip_relocation, mojave: "fbef825c499e3c99a499fff48626897af2e4279900c212406b96f97390460e9b"
   end
 
   depends_on "go" => :build
