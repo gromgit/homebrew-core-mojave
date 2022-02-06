6a09078af98028da4ce260add72c2bdc6adb8610
Adrian Ho <the.gromgit@gmail.com>
2022-02-06 14:29:45 +0800

    ki: update 0.4.5 bottle.

--- Formula/ki.rb
+++ Formula/ki.rb
@@ -1,14 +1,14 @@
 class Ki < Formula
   desc "Kotlin Language Interactive Shell"
   homepage "https://github.com/Kotlin/kotlin-interactive-shell"
-  url "https://github.com/Kotlin/kotlin-interactive-shell/archive/refs/tags/v0.4.1.tar.gz"
-  sha256 "b6e3ccb5ceb5a3e07d79b9f0684cec46ba0c5d4ded2346909048c72c9904d1c1"
+  url "https://github.com/Kotlin/kotlin-interactive-shell/archive/refs/tags/v0.4.5.tar.gz"
+  sha256 "b5e38918ac64216713c64170fd12f7b2c7c00124ba8d8b10ae7e53b386cb4bab"
   license "Apache-2.0"
   head "https://github.com/Kotlin/kotlin-interactive-shell.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ki"
-    sha256 cellar: :any_skip_relocation, mojave: "22076bd9e4e97f98807e3e4ca110a3d481853738f45607e3e5716c2bc1cbfb04"
+    sha256 cellar: :any_skip_relocation, mojave: "0c3265602a7dc11280221611430d73d15bce8d0c0265d3dd2f50f52b1c628a60"
   end
 
   depends_on "maven" => :build
