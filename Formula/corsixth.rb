5a9b3df475cde7ccebd95999a831f592ae78b31a
Adrian Ho <the.gromgit@gmail.com>
2022-02-08 05:05:16 +0800

    corsixth: update 0.65.1_1 bottle.

--- Formula/corsixth.rb
+++ Formula/corsixth.rb
@@ -4,25 +4,24 @@ class Corsixth < Formula
   url "https://github.com/CorsixTH/CorsixTH/archive/v0.65.1.tar.gz"
   sha256 "b8a1503371fa0c0f3d07d3b39a3de2769b8ed25923d0d931b7075bc88e3f508f"
   license "MIT"
+  revision 1
   head "https://github.com/CorsixTH/CorsixTH.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "36611a5f4a62cc90fef41ca741dc799d166002964a52ee877103cf2101d17e08"
-    sha256 arm64_big_sur:  "85159ab0c32c43fbe98e88181abd47c941acec95b5760646237d350f18ac403a"
-    sha256 monterey:       "56e6fa94b1c1a026e48ffefe5a784a3718efedde66541de92176be1073b48f11"
-    sha256 big_sur:        "7e0c673a852830521e8c5bf1edd8c0c55a73eb8e3fff9dd32e6939bbcf239620"
-    sha256 catalina:       "4e32cf5b1460d07b4e354fec82d0a5267be4c03e1d3af92fc5d96046c44f8f8c"
-    sha256 mojave:         "edb9df35dc84bddd751ecc716c37f7ef7f6f832a1b1613905214a8af6f507ea6"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/corsixth"
+    sha256 mojave: "417cbb5c620a2d23afdbf304b97bddfd0f189dfac35be21838bb255837a38598"
   end
 
   depends_on "cmake" => :build
   depends_on "luarocks" => :build
   depends_on xcode: :build
-  depends_on "ffmpeg"
+  depends_on "ffmpeg@4"
   depends_on "freetype"
   depends_on "lua"
   depends_on "sdl2"
   depends_on "sdl2_mixer"
+
+  fails_with gcc: "5" # ffmpeg is compiled with GCC
 
   resource "lpeg" do
     url "http://www.inf.puc-rio.br/~roberto/lpeg/lpeg-1.0.2.tar.gz"
