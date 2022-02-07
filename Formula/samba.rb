b5d73579dcad270909cef7a1d01a382c9db05b53
Adrian Ho <the.gromgit@gmail.com>
2022-02-07 12:02:12 +0800

    samba: update 4.15.5 bottle.

--- Formula/samba.rb
+++ Formula/samba.rb
@@ -4,8 +4,8 @@ class Samba < Formula
   # option. The shared folder appears in the guest as "\\10.0.2.4\qemu".
   desc "SMB/CIFS file, print, and login server for UNIX"
   homepage "https://www.samba.org/"
-  url "https://download.samba.org/pub/samba/stable/samba-4.15.1.tar.gz"
-  sha256 "a1811fbb4110d64969f6c108f8d161e2c3e20ddf475529a3d32bd94bb7459f00"
+  url "https://download.samba.org/pub/samba/stable/samba-4.15.5.tar.gz"
+  sha256 "69115e33831937ba5151be0247943147765aece658ba743f44741672ad68d17f"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -15,8 +15,7 @@ class Samba < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/samba"
-    rebuild 2
-    sha256 mojave: "82899e31f731ea2653030941cb968ba8cbbff1ac4ac483966d6bbad644dc5113"
+    sha256 mojave: "fed6b1707990213fe1532510577a5c1bead93f1b2a130253b36ffa6175384abe"
   end
 
   # configure requires python3 binary to be present, even when --disable-python is set.
