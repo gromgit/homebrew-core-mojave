51bd61e2c7c92dfbc4cfe2098ada7b8af9a65e04
Adrian Ho <the.gromgit@gmail.com>
2022-02-07 13:05:53 +0800

    serverless: update 3.1.1 bottle.

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -3,14 +3,14 @@ require "language/node"
 class Serverless < Formula
   desc "Build applications with serverless architectures"
   homepage "https://www.serverless.com/"
-  url "https://github.com/serverless/serverless/archive/v3.1.0.tar.gz"
-  sha256 "ef3bc2dde3413d7dab6f763c476620cb6935b622f8ca51ac5c2c59bad7948c5a"
+  url "https://github.com/serverless/serverless/archive/v3.1.1.tar.gz"
+  sha256 "88276ae35042d913718ee88bb65e9fd744490d0b22fe995ee35e69ba7ed8f79e"
   license "MIT"
   head "https://github.com/serverless/serverless.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/serverless"
-    sha256 cellar: :any_skip_relocation, mojave: "5fb1b784b564f05733c5e815c08d7a5a0ae707f8534cfe2872f3147acd70e8b0"
+    sha256 cellar: :any_skip_relocation, mojave: "068b9612e9b85c7a6a9315aaca5cff4f36a13e1cedbb24eb32d632524b2eb728"
   end
 
   depends_on "node"
