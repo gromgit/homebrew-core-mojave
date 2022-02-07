e289712f5cb3bd93e34aea2d96aeb9352b377764
Adrian Ho <the.gromgit@gmail.com>
2022-02-08 07:19:06 +0800

    dosbox-x: update 0.83.22 bottle.

--- Formula/dosbox-x.rb
+++ Formula/dosbox-x.rb
@@ -1,8 +1,8 @@
 class DosboxX < Formula
   desc "DOSBox with accurate emulation and wide testing"
   homepage "https://dosbox-x.com/"
-  url "https://github.com/joncampbell123/dosbox-x/archive/dosbox-x-v0.83.21.tar.gz"
-  sha256 "ec13bf16a9761c755df25f8b780aee589e328bcd490ac372538f8a87846456a2"
+  url "https://github.com/joncampbell123/dosbox-x/archive/dosbox-x-v0.83.22.tar.gz"
+  sha256 "75c5c1eb3b78701a80ad816f414640894cb0b5998a1a4d7f2af064b47a57b8c9"
   license "GPL-2.0-or-later"
   version_scheme 1
   head "https://github.com/joncampbell123/dosbox-x.git", branch: "master"
@@ -14,7 +14,7 @@ class DosboxX < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dosbox-x"
-    sha256 cellar: :any, mojave: "b3683b7f683fd59a908a224b4fb18a42ccb462e1dff72c957dcdad2f2f181b9e"
+    sha256 cellar: :any, mojave: "aabab343836aeaec274aa26325db5230f0e9fee3acac26d7bcbb15ef32ff2904"
   end
 
   depends_on "autoconf" => :build
