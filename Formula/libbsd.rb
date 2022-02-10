5c25aba8bb802d73937833076a11078769daeaaf
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-10 11:27:15 +0800

    libbsd:

--- Formula/libbsd.rb
+++ Formula/libbsd.rb
@@ -1,13 +1,17 @@
 class Libbsd < Formula
   desc "Utility functions from BSD systems"
   homepage "https://libbsd.freedesktop.org/"
-  url "https://libbsd.freedesktop.org/releases/libbsd-0.11.3.tar.xz"
-  sha256 "ff95cf8184151dacae4247832f8d4ea8800fa127dbd15033ecfe839f285b42a1"
+  url "https://libbsd.freedesktop.org/releases/libbsd-0.11.5.tar.xz"
+  sha256 "1a9c952525635c1bb6770cb22e969b938d8e6a9d7912362b98ee8370599b0efd"
   license "BSD-3-Clause"
 
   livecheck do
     url "https://libbsd.freedesktop.org/releases/"
     regex(/href=.*?libbsd[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2bf90214f078232dc77780d85fa561d529e0415b0daecb2a06fc558883f1f232"
   end
 
   depends_on "libmd"
@@ -22,6 +26,6 @@ class Libbsd < Formula
   end
 
   test do
-    assert_match "strtonum", shell_output("nm #{lib/"libbsd.so"}")
+    assert_match "strtonum", shell_output("nm #{lib/"libbsd.so.#{version}"}")
   end
 end
