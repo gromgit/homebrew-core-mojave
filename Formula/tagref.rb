7692383bdb252fdeb86d703407c228818efdb713
Adrian Ho <the.gromgit@gmail.com>
2022-02-10 12:53:14 +0800

    tagref: update 1.5.0 bottle.

---
+++ Formula/tagref.rb
@@ -0,0 +1,49 @@
+class Tagref < Formula
+  desc "Refer to other locations in your codebase"
+  homepage "https://github.com/stepchowfun/tagref"
+  url "https://github.com/stepchowfun/tagref/archive/v1.5.0.tar.gz"
+  sha256 "dd6321133c2bef64f9230d6aaddfba8a4327749236638c23c65d0832ca2fef48"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tagref"
+    sha256 cellar: :any_skip_relocation, mojave: "01472777ab9331e9453c9af038a629839f6134c6fcd82ef99b98808b00f13ba2"
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    (testpath/"file-1.txt").write <<~EOS
+      Here's a reference to the tag below: [ref:foo]
+      Here's a reference to a tag in another file: [ref:bar]
+      Here's a tag: [tag:foo]
+    EOS
+
+    (testpath/"file-2.txt").write <<~EOS
+      Here's a tag: [tag:bar]
+    EOS
+
+    ENV["NO_COLOR"] = "true"
+    output = shell_output("#{bin}/tagref 2>&1")
+    assert_match(
+      /2 tags and 2 references validated in \d+ files\./,
+      output,
+      "Tagref did not find all the tags.",
+    )
+
+    (testpath/"file-3.txt").write <<~EOS
+      Here's a reference to a non-existent tag: [ref:baz]
+    EOS
+
+    output = shell_output("#{bin}/tagref 2>&1", 1)
+    assert_match(
+      "No tag found for [ref:baz] @ ./file-3.txt:1.",
+      output,
+      "Tagref did not complain about a missing tag.",
+    )
+  end
+end
