4bc9693304b004a463a82015d16a4d95a87098d2
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-10 11:27:06 +0800

    artifactory: no rebuilding required

--- Formula/artifactory.rb
+++ Formula/artifactory.rb
@@ -3,8 +3,8 @@ class Artifactory < Formula
   homepage "https://www.jfrog.com/artifactory/"
   # v7 is available but does contain a number of pre-builts that need to be avoided.
   # Note that just using the source archive is not sufficient.
-  url "https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/6.23.37/jfrog-artifactory-oss-6.23.37.zip"
-  sha256 "e8bd0d48bdf1a1f6f4531325c9cf0dd59000e4796673725c18f81c0d8116f771"
+  url "https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/6.23.38/jfrog-artifactory-oss-6.23.38.zip"
+  sha256 "011adf902fed7f389b829fc715e39e467c7fec53d71795ca9b7e97fafcede45a"
   license "AGPL-3.0-or-later"
 
   livecheck do
@@ -13,7 +13,7 @@ class Artifactory < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "1805e2dd2fedc6e946ce90e25e572d8b63f0cb6af3bc76c99db0c891ca3808f7"
+    sha256 cellar: :any_skip_relocation, all: "9f581321ef75ca75e7a207dcd1b4dd6f2890982b64e9bbcc74ebcab12546db0c"
   end
 
   depends_on "openjdk"
