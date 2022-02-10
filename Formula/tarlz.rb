235d79ee0d88f68711f9d4df317343ddaa98ec80
Adrian Ho <the.gromgit@gmail.com>
2022-02-10 12:54:46 +0800

    tarlz: update 0.22 bottle.

---
+++ Formula/tarlz.rb
@@ -0,0 +1,45 @@
+class Tarlz < Formula
+  desc "Data compressor"
+  homepage "https://www.nongnu.org/lzip/tarlz.html"
+  url "https://download.savannah.gnu.org/releases/lzip/tarlz/tarlz-0.22.tar.lz"
+  mirror "https://download-mirror.savannah.gnu.org/releases/lzip/tarlz/tarlz-0.22.tar.lz"
+  sha256 "fccf7226fa24b55d326cab13f76ea349bec446c5a8df71a46d343099a05091dc"
+  license "GPL-2.0-or-later"
+
+  livecheck do
+    url "http://download.savannah.gnu.org/releases/lzip/tarlz/"
+    regex(/href=.*?tarlz[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tarlz"
+    sha256 cellar: :any_skip_relocation, mojave: "eb50037ccebd7a0cabead0d82e6c41058830d027deb36b122bc312a825764643"
+  end
+
+  depends_on "lzlib"
+
+  def install
+    system "./configure", *std_configure_args
+    system "make"
+    ENV.deparallelize
+    system "make", "install"
+  end
+
+  test do
+    spath = testpath/"source"
+    dpath = testpath/"destination"
+    stestfilepath = spath/"test.txt"
+    dtestfilepath = dpath/"source/test.txt"
+    lzipfilepath = testpath/"test.tar.lz"
+    stestfilepath.write "TEST CONTENT"
+
+    mkdir_p spath
+    mkdir_p dpath
+
+    system "#{bin}/tarlz", "-C", testpath, "-cf", lzipfilepath, "source"
+    assert_predicate lzipfilepath, :exist?
+
+    system "#{bin}/tarlz", "-C", dpath, "-xf", lzipfilepath
+    assert_equal "TEST CONTENT", dtestfilepath.read
+  end
+end
