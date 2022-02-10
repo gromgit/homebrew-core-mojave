ddeabfd2a7d028b6ed68b61cc4637774b09c9958
Adrian Ho <the.gromgit@gmail.com>
2022-02-10 12:05:39 +0800

    numdiff: update 5.9.0 bottle.

---
+++ Formula/numdiff.rb
@@ -0,0 +1,44 @@
+class Numdiff < Formula
+  desc "Putative files comparison tool"
+  homepage "https://www.nongnu.org/numdiff"
+  url "https://download.savannah.gnu.org/releases/numdiff/numdiff-5.9.0.tar.gz"
+  mirror "https://download-mirror.savannah.gnu.org/releases/numdiff/numdiff-5.9.0.tar.gz"
+  sha256 "87284a117944723eebbf077f857a0a114d818f8b5b54d289d59e73581194f5ef"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    url "https://download.savannah.gnu.org/releases/numdiff/"
+    regex(/href=.*?numdiff[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/numdiff"
+    sha256 cellar: :any, mojave: "56416d58c8def4698db56bed4ad1e0a20ab46c6d36e341b6e4e69a24eb81887e"
+  end
+
+  depends_on "gmp"
+
+  def install
+    system "./configure", "--disable-debug", "--disable-nls", "--enable-gmp",
+           "--prefix=#{prefix}", "--libdir=#{lib}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"a").write "1 2\n"
+    (testpath/"b").write "1.1 2.5\n"
+
+    expected = <<~EOS
+      ----------------
+      ##1       #:1   <== 1
+      ##1       #:1   ==> 1.1
+      @ Absolute error = 1.0000000000e-1, Relative error = 1.0000000000e-1
+      ##1       #:2   <== 2
+      ##1       #:2   ==> 2.5
+      @ Absolute error = 5.0000000000e-1, Relative error = 2.5000000000e-1
+
+      +++  File "a" differs from file "b"
+    EOS
+    assert_equal expected, shell_output("#{bin}/numdiff a b", 1)
+  end
+end
