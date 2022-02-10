99bc44f41f2077b643c34052fe795118e725bef3
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-10 11:27:25 +0800

    tika: no rebuilding required

--- Formula/tika.rb
+++ Formula/tika.rb
@@ -1,21 +1,21 @@
 class Tika < Formula
   desc "Content analysis toolkit"
   homepage "https://tika.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tika/2.2.0/tika-app-2.2.0.jar"
-  mirror "https://archive.apache.org/dist/tika/2.2.0/tika-app-2.2.0.jar"
-  sha256 "fc5697c725cc091c004e2c80034f8fca06748a7276e36e6f6b225b4bc76627c5"
+  url "https://www.apache.org/dyn/closer.lua?path=tika/2.3.0/tika-app-2.3.0.jar"
+  mirror "https://archive.apache.org/dist/tika/2.3.0/tika-app-2.3.0.jar"
+  sha256 "73e93aa3b6325227d4f1da6ce9d18695437240b45f3610337c3e7faa084274ad"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "2654871c700d815f4b3fa8a8db634ab442a6fd512db639f99460c5032643e09f"
+    sha256 cellar: :any_skip_relocation, all: "02c5b267e415fe43cee8acf467e8d1ec340c3cf57260717e9a62f58416ab3bdb"
   end
 
   depends_on "openjdk"
 
   resource "server" do
-    url "https://www.apache.org/dyn/closer.lua?path=tika/2.2.0/tika-server-standard-2.2.0.jar"
-    mirror "https://archive.apache.org/dist/tika/2.2.0/tika-server-standard-2.2.0.jar"
-    sha256 "5049043fccf5ddc3c545851e0d45d0f73be20d7a7cc2b833fbd99c2d70f316f1"
+    url "https://www.apache.org/dyn/closer.lua?path=tika/2.3.0/tika-server-standard-2.3.0.jar"
+    mirror "https://archive.apache.org/dist/tika/2.3.0/tika-server-standard-2.3.0.jar"
+    sha256 "45df8a64a5090d13109d30644a00f705282bfea0cae0948e93b2ec834c14b192"
   end
 
   def install
