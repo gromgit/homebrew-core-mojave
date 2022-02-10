abad80dd99b304ac179b40698b82b2b7b8096142
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-10 11:27:23 +0800

    singularity:

--- Formula/singularity.rb
+++ Formula/singularity.rb
@@ -1,9 +1,13 @@
 class Singularity < Formula
   desc "Application container and unprivileged sandbox platform for Linux"
   homepage "https://singularity.hpcng.org"
-  url "https://github.com/hpcng/singularity/releases/download/v3.8.4/singularity-3.8.4.tar.gz"
-  sha256 "cb95e6d68b0d20f2b87d60f23a3bf707b7d3e87cee0dd4aa4380f8f481a57ebc"
+  url "https://github.com/hpcng/singularity/releases/download/v3.8.6/singularity-3.8.6.tar.gz"
+  sha256 "bb5a3b7670ac9c7a4b3ce5b2c9f3d6b5be60e21b08d338c9dfdabb7b2a99f528"
   license "BSD-3-Clause"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ceaf9f4ba7e0fb697148e8a01ed5faae3b6e90e63f3d4fd21dc7b0dcc274d37a"
+  end
 
   # No relocation, the localstatedir to find configs etc is compiled into the program
   pour_bottle? only_if: :default_prefix
