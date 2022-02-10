0f234e8fc88eb929e76093c8bf5734b3612e5926
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-10 11:27:18 +0800

    mvnvm: no rebuilding required

--- Formula/mvnvm.rb
+++ Formula/mvnvm.rb
@@ -1,14 +1,13 @@
 class Mvnvm < Formula
   desc "Maven version manager"
   homepage "https://mvnvm.org/"
-  url "https://bitbucket.org/mjensen/mvnvm/get/mvnvm-1.0.15.tar.gz"
-  sha256 "2d2163b89d918e07146131407751eb7aff9c5fb5ae8c954b912e60860fb1c8ea"
+  url "https://bitbucket.org/mjensen/mvnvm/get/mvnvm-1.0.16.tar.gz"
+  sha256 "a21675dc5816d52b2e90d84c36c3899fd19c66ac3d63f3f635f54d0cd3761176"
   license "Apache-2.0"
-  head "https://bitbucket.org/mjensen/mvnvm.git"
+  head "https://bitbucket.org/mjensen/mvnvm.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "d302f0e086f47adc21c6c401b1ab6fab48ab5c9bbf82459c94c886a07b8a5b8c"
+    sha256 cellar: :any_skip_relocation, all: "c5a3c45b3f4a5df6323b694c4f597ea5c99d59b236e8ce90352d756739e5931e"
   end
 
   depends_on "openjdk"
