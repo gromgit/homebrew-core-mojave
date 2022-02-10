8c3ce1257d670b8ee9aabcbb4431c24f4fad5400
Adrian Ho <the.gromgit@gmail.com>
2022-02-10 11:44:42 +0800

    alpscore: update 2.2.0 bottle.

---
+++ Formula/alpscore.rb
@@ -0,0 +1,64 @@
+class Alpscore < Formula
+  desc "Applications and libraries for physics simulations"
+  homepage "https://alpscore.org"
+  url "https://github.com/ALPSCore/ALPSCore/archive/v2.2.0.tar.gz"
+  sha256 "f7bc9c8f806fb0ad4d38cb6604a10d56ab159ca63aed6530c1f84ecaf40adc61"
+  license "GPL-2.0-only"
+  head "https://github.com/ALPSCore/ALPSCore.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/alpscore"
+    sha256 cellar: :any, mojave: "c5ac6349aad84eb1481d14f9356b975acc0e37c9e877558e996f8be556471e72"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "eigen"
+  depends_on "hdf5"
+  depends_on "open-mpi"
+
+  on_macos do
+    depends_on "szip"
+  end
+
+  def install
+    args = std_cmake_args
+    args << "-DEIGEN3_INCLUDE_DIR=#{Formula["eigen"].opt_include}/eigen3"
+    args << "-DALPS_BUILD_SHARED=ON"
+    args << "-DENABLE_MPI=ON"
+    args << "-DTesting=OFF"
+
+    system "cmake", "-S", ".", "-B", "build", *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <alps/mc/api.hpp>
+      #include <alps/mc/mcbase.hpp>
+      #include <alps/accumulators.hpp>
+      #include <alps/params.hpp>
+      using namespace std;
+      int main()
+      {
+        alps::accumulators::accumulator_set set;
+        set << alps::accumulators::MeanAccumulator<double>("a");
+        set["a"] << 2.9 << 3.1;
+        alps::params p;
+        p["myparam"] = 1.0;
+        cout << set["a"] << endl << p["myparam"] << endl;
+      }
+    EOS
+
+    args = %W[
+      -std=c++11
+      -I#{include} -I#{Formula["boost"].opt_include}
+      -L#{lib} -L#{Formula["boost"].opt_lib}
+      -lalps-accumulators -lalps-hdf5 -lalps-utilities -lalps-params
+      -lboost_filesystem-mt -lboost_system-mt -lboost_program_options-mt
+    ]
+    system ENV.cxx, "test.cpp", *args, "-o", "test"
+    assert_equal "3 #2\n1 (type: double) (name='myparam')\n", shell_output("./test")
+  end
+end
