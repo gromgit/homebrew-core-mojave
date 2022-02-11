c529deb9428713b34950200a4f1d825c07ae26b8
Adrian Ho <the.gromgit@gmail.com>
2022-02-11 13:51:02 +0800

    angular-cli: update 13.2.3 bottle.

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -3,13 +3,13 @@ require "language/node"
 class AngularCli < Formula
   desc "CLI tool for Angular"
   homepage "https://cli.angular.io/"
-  url "https://registry.npmjs.org/@angular/cli/-/cli-13.2.2.tgz"
-  sha256 "58debc6f9250496419e5543112773e9826824f7684ac190176c5340553994bfa"
+  url "https://registry.npmjs.org/@angular/cli/-/cli-13.2.3.tgz"
+  sha256 "b38d7375cf8e09947df053fb963cecf86387fc79bd7afb306ac4e2b5a9e2ddfc"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/angular-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "b182e77b8ab22b6da7c2fb5214f8d464e10c9799aa209c93b0053a5f7184f252"
+    sha256 cellar: :any_skip_relocation, mojave: "ae1194eb28c94f1be78d5ed591985276393a8d71870aff91de1ae3da9f74278d"
   end
 
   depends_on "node"
