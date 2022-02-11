81c8b38db81f5a81898ee767080a707b22dcc4ba
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-11 10:22:26 +0800

    orc-tools: no rebuilding required

--- Formula/orc-tools.rb
+++ Formula/orc-tools.rb
@@ -1,8 +1,8 @@
 class OrcTools < Formula
   desc "ORC java command-line tools and utilities"
   homepage "https://orc.apache.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/apache/orc/orc-tools/1.7.1/orc-tools-1.7.1-uber.jar"
-  sha256 "53c7eae1399135227c89eecc5ec63d156bcaa753b8c1b4b69e30680e386f17a0"
+  url "https://search.maven.org/remotecontent?filepath=org/apache/orc/orc-tools/1.7.3/orc-tools-1.7.3-uber.jar"
+  sha256 "269e58e097401d07b994c0b1f5397a9b8ab92d43fad3a1bdf0f9e1b96d6fc86e"
   license "Apache-2.0"
 
   livecheck do
@@ -11,9 +11,7 @@ class OrcTools < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/orc-tools"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "251c3ec707f822901b439b079dc3da3c13bc6f2914fe809cdec8c96514d16b23"
+    sha256 cellar: :any_skip_relocation, all: "1d5e857435c24e6e80ac746679e6ad0e7abe1fb75b7db0ea6690e72cc5356594"
   end
 
   depends_on "openjdk"
