633d605c181f81b9ba2ff22a7eaed8ec72c90046
Adrian Ho <the.gromgit@gmail.com>
2022-02-11 11:01:28 +0800

    webp-pixbuf-loader: update 0.0.4 bottle.

---
+++ Formula/webp-pixbuf-loader.rb
@@ -0,0 +1,75 @@
+class WebpPixbufLoader < Formula
+  desc "WebP Image format GdkPixbuf loader"
+  homepage "https://github.com/aruiz/webp-pixbuf-loader"
+  url "https://github.com/aruiz/webp-pixbuf-loader/archive/0.0.4.tar.gz"
+  sha256 "cd6e4ec44755e8df3e298688c0aeb72b9467bbdd03009989c0d94b219b30fb51"
+  license "LGPL-2.0-or-later"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/webp-pixbuf-loader"
+    sha256 cellar: :any, mojave: "d3e92d64a0ad31852ca75dbb7d583c3541ea1a0f17495908ad3771f4b2a725a2"
+  end
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "gdk-pixbuf"
+  depends_on "webp"
+
+  # Constants for gdk-pixbuf's multiple version numbers, which are the same as
+  # the constants in the gdk-pixbuf formula.
+  def gdk_so_ver
+    Formula["gdk-pixbuf"].gdk_so_ver
+  end
+
+  def gdk_module_ver
+    Formula["gdk-pixbuf"].gdk_module_ver
+  end
+
+  # Subfolder that pixbuf loaders are installed into.
+  def module_subdir
+    "lib/gdk-pixbuf-#{gdk_so_ver}/#{gdk_module_ver}/loaders"
+  end
+
+  def install
+    mkdir "build" do
+      system "meson", *std_meson_args, "-Dgdk_pixbuf_moduledir=#{prefix}/#{module_subdir}", ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
+  end
+
+  # After the loader is linked in, update the global cache of pixbuf loaders
+  def post_install
+    ENV["GDK_PIXBUF_MODULEDIR"] = "#{HOMEBREW_PREFIX}/#{module_subdir}"
+    system "#{Formula["gdk-pixbuf"].opt_bin}/gdk-pixbuf-query-loaders", "--update-cache"
+  end
+
+  test do
+    # Generate a .webp file to test with.
+    system "#{Formula["webp"].opt_bin}/cwebp", test_fixtures("test.png"), "-o", "test.webp"
+
+    # Sample program to load a .webp file via gdk-pixbuf.
+    (testpath/"test.c").write <<~EOS
+      #include <gdk-pixbuf/gdk-pixbuf.h>
+
+      gint main (gint argc, gchar **argv)  {
+        GError *error = NULL;
+        GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file (argv[1], &error);
+        if (error) {
+          g_error("%s", error->message);
+          return 1;
+        };
+
+        g_assert(gdk_pixbuf_get_width(pixbuf) == 8);
+        g_assert(gdk_pixbuf_get_height(pixbuf) == 8);
+        g_object_unref(pixbuf);
+        return 0;
+      }
+    EOS
+
+    flags = shell_output("pkg-config --cflags --libs gdk-pixbuf-#{gdk_so_ver}").chomp.split
+    system ENV.cc, "test.c", "-o", "test_loader", *flags
+    system "./test_loader", "test.webp"
+  end
+end
