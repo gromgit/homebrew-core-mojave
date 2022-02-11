72cfa3b7d7ce0a0a0fe053477c5be1c1e58a1d65
Adrian Ho <the.gromgit@gmail.com>
2022-02-12 04:56:44 +0800

    mpg123: update 1.29.3 bottle.

--- Formula/mpg123.rb
+++ Formula/mpg123.rb
@@ -13,7 +13,8 @@ class Mpg123 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mpg123"
-    sha256 mojave: "b30eef03c4023ec565d29aa0fe35f9a11db874cf8ddd564c94851363d39a20d0"
+    rebuild 1
+    sha256 mojave: "6707f4d905b244b7087cc21badd7360ac79c0fed478f1c0d1bda687ed8440854"
   end
 
   def install
@@ -22,6 +23,7 @@ class Mpg123 < Formula
       --disable-dependency-tracking
       --prefix=#{prefix}
       --with-module-suffix=.so
+      --enable-static
     ]
 
     args << "--with-default-audio=coreaudio" if OS.mac?
