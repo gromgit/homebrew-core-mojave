d4cb7282d02b321ad712a223545f931344ff6328
Adrian Ho <the.gromgit@gmail.com>
2022-02-12 16:33:05 +0800

    fping: update 5.1 bottle.

--- Formula/fping.rb
+++ Formula/fping.rb
@@ -1,8 +1,8 @@
 class Fping < Formula
   desc "Scriptable ping program for checking if multiple hosts are up"
   homepage "https://fping.org/"
-  url "https://fping.org/dist/fping-5.0.tar.gz"
-  sha256 "ed38c0b9b64686a05d1b3bc1d66066114a492e04e44eef1821d43b1263cd57b8"
+  url "https://fping.org/dist/fping-5.1.tar.gz"
+  sha256 "1ee5268c063d76646af2b4426052e7d81a42b657e6a77d8e7d3d2e60fd7409fe"
   license "BSD-3-Clause"
 
   livecheck do
@@ -11,14 +11,8 @@ class Fping < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "73000b5b7a0e589b23f8bc15084193df6fd94da230540de4dfcc86cb804daceb"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "65a1a6e1fee0af28a38006e2ce05f71915080907cacc8bb7ef4373ae041e75f2"
-    sha256 cellar: :any_skip_relocation, monterey:       "18a6e94a256db8c3eb6156016400d7e8af64e9f84847ecf1654989470ca8ea76"
-    sha256 cellar: :any_skip_relocation, big_sur:        "012cad012acbaf32885f1d260cfa464478a0a71ad396f0711813c7f2b183112d"
-    sha256 cellar: :any_skip_relocation, catalina:       "bd1255921afca543ba440bbf84f86f7c3b0b10db4bbf1aa659a2aa686496e4d5"
-    sha256 cellar: :any_skip_relocation, mojave:         "47f38d4902f03da1e407331848e1f3a75a2b8692e4366d8a0a341e66f36962f1"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "e2d14a6c1de9032a244f7185ba8a629d61f8ed2964b96490890c87336ff4d521"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7c021898c3eb3c2496cb915101a86618f895a8234b3281cee553531353300c71"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fping"
+    sha256 cellar: :any_skip_relocation, mojave: "a1d40214d9008fe5f57a183da4d61483a8a8897b04486b737900bcdad940709c"
   end
 
   head do
