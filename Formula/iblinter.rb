1704dc663d9e960fcee379bbcf8a1bb39334e543
Adrian Ho <the.gromgit@gmail.com>
2022-02-12 18:57:23 +0800

    iblinter: update 0.5.0 bottle.

--- Formula/iblinter.rb
+++ Formula/iblinter.rb
@@ -1,18 +1,14 @@
 class Iblinter < Formula
   desc "Linter tool for Interface Builder"
   homepage "https://github.com/IBDecodable/IBLinter"
-  url "https://github.com/IBDecodable/IBLinter/archive/0.4.27.tar.gz"
-  sha256 "1403d4b104d41a8cfbc66b8533b53f1f94255f00904dfb97e91a88230f07dcfc"
+  url "https://github.com/IBDecodable/IBLinter/archive/0.5.0.tar.gz"
+  sha256 "d1aafdca18bc81205ef30a2ee59f33513061b20184f0f51436531cec4a6f7170"
   license "MIT"
-  head "https://github.com/IBDecodable/IBLinter.git"
+  head "https://github.com/IBDecodable/IBLinter.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ac6b0a40f7cad78d4afef1dd7c874356fbf870275349aecb82de7733d620f958"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e8b7e60ab04bde225bb1a598f48f816a3672ae079262438de162678e2802e48d"
-    sha256 cellar: :any_skip_relocation, monterey:       "a42624bde4bddb39236ac2261b75cd118afeca63fabfd40bd23fd6220115cb77"
-    sha256 cellar: :any_skip_relocation, big_sur:        "01a92d21d4bcafd3f50d095be30903dddbc6abfea1d05b2535a2d2b8a86d5048"
-    sha256 cellar: :any_skip_relocation, catalina:       "d1f695ee4af2122773fb1d5113cbc1ffc18520da4267a051ed275e3b01ff326d"
-    sha256 cellar: :any_skip_relocation, mojave:         "c8c64fb3319c716d4cd0d69cc383b1ae6c152112093eeaad5e7ee9aa07c50f20"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/iblinter"
+    sha256 cellar: :any_skip_relocation, mojave: "95affe89a65a6305c0d2675701ac052561ed31fbab3fc33b26376e131ed95f64"
   end
 
   depends_on xcode: ["10.2", :build]
