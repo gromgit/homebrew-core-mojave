e541262f179874a4354e4250c63521eebf50aa14
Adrian Ho <the.gromgit@gmail.com>
2022-02-13 02:00:59 +0800

    nomad: update 1.2.6 bottle.

--- Formula/nomad.rb
+++ Formula/nomad.rb
@@ -1,8 +1,8 @@
 class Nomad < Formula
   desc "Distributed, Highly Available, Datacenter-Aware Scheduler"
   homepage "https://www.nomadproject.io"
-  url "https://github.com/hashicorp/nomad/archive/v1.2.4.tar.gz"
-  sha256 "f9f2eba618283238072d18c24a2bbb8835a37aa4ed58c8732db150514cb1481e"
+  url "https://github.com/hashicorp/nomad/archive/v1.2.6.tar.gz"
+  sha256 "c69ec89ae5f76704d4b3ecb842b130f3d90e579bfd31191721b5f2da99f95a93"
   license "MPL-2.0"
   head "https://github.com/hashicorp/nomad.git", branch: "main"
 
@@ -13,7 +13,7 @@ class Nomad < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nomad"
-    sha256 cellar: :any_skip_relocation, mojave: "09924115bd21c87286992554a50a03731d0679693cb7ed9ca49ec8321f45680e"
+    sha256 cellar: :any_skip_relocation, mojave: "8f5e1dc21a619db07f2ad79713bb28d0c2bc3235c1d8a12b64a36b4cdb272e27"
   end
 
   depends_on "go" => :build
