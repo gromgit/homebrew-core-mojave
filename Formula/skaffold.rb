254890ec5f90b9465b909cd529533337675420bf
Adrian Ho <the.gromgit@gmail.com>
2022-02-13 07:03:18 +0800

    skaffold: update 1.36.0 bottle.

--- Formula/skaffold.rb
+++ Formula/skaffold.rb
@@ -2,14 +2,14 @@ class Skaffold < Formula
   desc "Easy and Repeatable Kubernetes Development"
   homepage "https://skaffold.dev/"
   url "https://github.com/GoogleContainerTools/skaffold.git",
-      tag:      "v1.35.2",
-      revision: "f8ae4e65bafdcfd39e4de67329b185432899c7ad"
+      tag:      "v1.36.0",
+      revision: "189a55291c18ac850277134d2b8f3eaa2c4f7a1d"
   license "Apache-2.0"
   head "https://github.com/GoogleContainerTools/skaffold.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/skaffold"
-    sha256 cellar: :any_skip_relocation, mojave: "2e386b5005eeddf67bb8182dfa2426b80f4b539cffcadafc5a07b89dcdf01a37"
+    sha256 cellar: :any_skip_relocation, mojave: "53bd2cbbf8424f622db3459eee6a77e0a9748973bc89638a3629618205639d2b"
   end
 
   depends_on "go" => :build
