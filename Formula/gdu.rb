efac59e4ca1283590e8061134b1acb90258bd427
Adrian Ho <the.gromgit@gmail.com>
2022-02-12 17:02:34 +0800

    gdu: update 5.13.1 bottle.

--- Formula/gdu.rb
+++ Formula/gdu.rb
@@ -1,13 +1,13 @@
 class Gdu < Formula
   desc "Disk usage analyzer with console interface written in Go"
   homepage "https://github.com/dundee/gdu"
-  url "https://github.com/dundee/gdu/archive/v5.13.0.tar.gz"
-  sha256 "efcbf98b83adc99777f1e135abb61f747dae32b04919fdb884dbc6a57d6ed5f3"
+  url "https://github.com/dundee/gdu/archive/v5.13.1.tar.gz"
+  sha256 "2fa09d79e84a1ec74db0c6bf006570f42ad3ffebbccfe98a1d14a0559892fb78"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gdu"
-    sha256 cellar: :any_skip_relocation, mojave: "f2cbb5ff4e4e94ad9238b7026e3708c424e74d5c028e2a78c49c26a24b73b603"
+    sha256 cellar: :any_skip_relocation, mojave: "f0f55188d785061d0b34a8701375573ae12ea2a5d8bce665aaf7ea4602612db3"
   end
 
   depends_on "go" => :build
