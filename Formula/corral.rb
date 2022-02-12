a842ba7a88d4338662c5c39e928e8ae37ca51285
Adrian Ho <the.gromgit@gmail.com>
2022-02-12 12:18:07 +0800

    corral: update 0.5.5 bottle.

--- Formula/corral.rb
+++ Formula/corral.rb
@@ -1,19 +1,14 @@
 class Corral < Formula
   desc "Dependency manager for the Pony language"
   homepage "https://github.com/ponylang/corral"
-  url "https://github.com/ponylang/corral/archive/0.5.4.tar.gz"
-  sha256 "ca2fff8568fd14404d25de402dec2cae16cad39edb7937f829f88a23a66da2d1"
+  url "https://github.com/ponylang/corral/archive/0.5.5.tar.gz"
+  sha256 "e124b6a217d159e7c2f09cde3e3d104c099163c54fe25ac1c546b059f575526b"
   license "BSD-2-Clause"
   head "https://github.com/ponylang/corral.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "32b223b7ffbe3f36302c029b110a5c6292270e4e27795b55a0ea5510664ba8ce"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f2e834fce3e5975278473b0b4feae20fa018e2e69c98b2830de3957117d58bea"
-    sha256 cellar: :any_skip_relocation, monterey:       "3692383008fe80f3863cfaf1411ab589d79fa44a591d8da750b67a5c4585455d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bdd3bdc4acaeab7247b76aa1dbe3b319c1a512c2d066ca41d30c03aa69dfef0b"
-    sha256 cellar: :any_skip_relocation, catalina:       "8faf06cfba7afa9efee5505a50b3c2490ea2f501085f8a5880dbd00a1f1b6997"
-    sha256 cellar: :any_skip_relocation, mojave:         "c9bec6571d663be1e46a599d5d99ae273f25816e8c19a1857f46f9652344cbb2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e2f29be05b07b7466d2a75035c19f3b7c781a5d14db250f32fc4aa8ca958c4e6"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/corral"
+    sha256 cellar: :any_skip_relocation, mojave: "6602e42ccba297b9b728acd5d101e89518c9b096553f7f68e488f1f0a21c05e7"
   end
 
   depends_on "ponyc"
