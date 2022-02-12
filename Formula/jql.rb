0962d1bef78c20016252ffcc0b4408ae94ab2488
Adrian Ho <the.gromgit@gmail.com>
2022-02-12 20:07:10 +0800

    jql: update 3.1.0 bottle.

--- Formula/jql.rb
+++ Formula/jql.rb
@@ -1,14 +1,14 @@
 class Jql < Formula
   desc "JSON query language CLI tool"
   homepage "https://github.com/yamafaktory/jql"
-  url "https://github.com/yamafaktory/jql/archive/v3.0.9.tar.gz"
-  sha256 "c5dd79942c37a483fe7b0d52f880be332b50aef2fc90bbdb1b8e3780d9dcb014"
+  url "https://github.com/yamafaktory/jql/archive/v3.1.0.tar.gz"
+  sha256 "7eeb73c76b39eb47dd8c7ec7af9b225e8c92da1d3dd704c99617d9331438f1a7"
   license "MIT"
   head "https://github.com/yamafaktory/jql.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jql"
-    sha256 cellar: :any_skip_relocation, mojave: "e21812bf729f0f4a7f1b421aa6de44e702e42ad6ef4ab722ce9e45e85cd1a3c3"
+    sha256 cellar: :any_skip_relocation, mojave: "24bb150c5aaf8969bf7e86d1c917dcc44e9e3f15780fec08778d66dea8389b84"
   end
 
   depends_on "rust" => :build
