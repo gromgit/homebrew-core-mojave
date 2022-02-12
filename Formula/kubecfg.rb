d4aa0aa0688bcbeae011fd86c366760b1b146e2a
Adrian Ho <the.gromgit@gmail.com>
2022-02-12 20:21:20 +0800

    kubecfg: update 0.25.0 bottle.

--- Formula/kubecfg.rb
+++ Formula/kubecfg.rb
@@ -1,13 +1,13 @@
 class Kubecfg < Formula
   desc "Manage complex enterprise Kubernetes environments as code"
   homepage "https://github.com/kubecfg/kubecfg"
-  url "https://github.com/kubecfg/kubecfg/archive/v0.24.1.tar.gz"
-  sha256 "4e5d01a92b04080cda5df729b21e54d44e5536904340301f183d65800eaf6818"
+  url "https://github.com/kubecfg/kubecfg/archive/v0.25.0.tar.gz"
+  sha256 "884708337842341d4f777e00321bfa72b16bf73027a583305ebb281000096714"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kubecfg"
-    sha256 cellar: :any_skip_relocation, mojave: "f6c7fe904e261d40834f51109c18ec611b7cfb92bcee8d40ce96a9bef54616cd"
+    sha256 cellar: :any_skip_relocation, mojave: "0707873a9d955d7d3b9f2ec4098deb3553da2825d3299ced562c6a0717ba81e2"
   end
 
   depends_on "go" => :build
