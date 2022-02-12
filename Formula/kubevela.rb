1dab1240f317ffb26671ca82b2331303a36da27c
Adrian Ho <the.gromgit@gmail.com>
2022-02-12 20:28:34 +0800

    kubevela: update 1.2.3 bottle.

--- Formula/kubevela.rb
+++ Formula/kubevela.rb
@@ -2,13 +2,13 @@ class Kubevela < Formula
   desc "Application Platform based on Kubernetes and Open Application Model"
   homepage "https://kubevela.io"
   url "https://github.com/oam-dev/kubevela.git",
-      tag:      "v1.2.2",
-      revision: "29ecc5c0df261369a504401fa5bbff1979667f36"
+      tag:      "v1.2.3",
+      revision: "fbef61d07663c878ffabe5ac78c2423a8543628e"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kubevela"
-    sha256 cellar: :any_skip_relocation, mojave: "a6885762c1594b6b518df8253e6c2f3089cc57dba8fc4fc0a7c73430ef6d9809"
+    sha256 cellar: :any_skip_relocation, mojave: "522466d3b025dba7e1f5c3e71c27d261b17b53fa724f28afc04c5bc9f7f64976"
   end
 
   depends_on "go" => :build
