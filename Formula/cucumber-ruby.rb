d762f67a5c3a42777ee6bf56339fd31344470583
Adrian Ho <the.gromgit@gmail.com>
2022-02-12 12:32:47 +0800

    cucumber-ruby: update 7.1.0_1 bottle.

--- Formula/cucumber-ruby.rb
+++ Formula/cucumber-ruby.rb
@@ -4,6 +4,7 @@ class CucumberRuby < Formula
   url "https://github.com/cucumber/cucumber-ruby/archive/v7.1.0.tar.gz"
   sha256 "8eafa529f1c8793de09b550b68067af1f3d1b05e8eca798f5755d05ee0aacf8c"
   license "MIT"
+  revision 1
 
   livecheck do
     url :stable
@@ -11,11 +12,8 @@ class CucumberRuby < Formula
   end
 
   bottle do
-    sha256                               monterey:     "d6ff6562f6a7d518d4ed7ad5986a2f58f7331bfa87d67f03b35603e4cdcdf15d"
-    sha256                               big_sur:      "a92759a27d110dd3884a8d421d7c507e3f0424afc5cc3994912ec57dfce1a47a"
-    sha256 cellar: :any,                 catalina:     "91f7a0452a265adc52220a926b74daf33d346a6659d71448110acbbb56781854"
-    sha256 cellar: :any,                 mojave:       "0fc96a4a30d70ddd88e725f6f3ffd4ecfc4f5f0d640546fea803d8407f392752"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "e9e9cea603b814907f60395901d4d9ca039a897930dde2a718516e8a4385aa4f"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cucumber-ruby"
+    sha256 cellar: :any, mojave: "acfeeb18a9818e03861b9a91b4f8b3a556f56c237e3fa390cf2e4823e2bdb438"
   end
 
   depends_on "pkg-config" => :build
