cc83700e08c87750de1bb0985c7e84038e506c01
Adrian Ho <the.gromgit@gmail.com>
2022-02-12 22:12:16 +0800

    lxc: update 4.23 bottle.

--- Formula/lxc.rb
+++ Formula/lxc.rb
@@ -1,8 +1,8 @@
 class Lxc < Formula
   desc "CLI client for interacting with LXD"
   homepage "https://linuxcontainers.org"
-  url "https://linuxcontainers.org/downloads/lxd/lxd-4.22.tar.gz"
-  sha256 "0977e4b8ae854278194b15f3dc6472d217b9619a024d03f60e95393352212385"
+  url "https://linuxcontainers.org/downloads/lxd/lxd-4.23.tar.gz"
+  sha256 "6cfcc7f4c462ae09776ffc24918211844bebcafcbfe4cd98f4b2cfa83e082fc5"
   license "Apache-2.0"
 
   livecheck do
@@ -12,7 +12,7 @@ class Lxc < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lxc"
-    sha256 cellar: :any_skip_relocation, mojave: "1649ea9dac426ac699b2703a1da09e263f7b9f7084aea6c74d4f6b993cbaa0d1"
+    sha256 cellar: :any_skip_relocation, mojave: "521ce72286993271e39d45f28693ed576acc913329c7e2693ac8783ae8665486"
   end
 
   depends_on "go" => :build
