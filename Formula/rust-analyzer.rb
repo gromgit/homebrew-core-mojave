55105517c9e9be5cf7b4a2765eef35b074fc60a4
Adrian Ho <the.gromgit@gmail.com>
2022-02-13 06:13:55 +0800

    rust-analyzer: update 2022-02-07 bottle.

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -2,14 +2,14 @@ class RustAnalyzer < Formula
   desc "Experimental Rust compiler front-end for IDEs"
   homepage "https://rust-analyzer.github.io/"
   url "https://github.com/rust-analyzer/rust-analyzer.git",
-       tag:      "2022-01-31",
-       revision: "fd3942eb620e37a4e4bfdd587d8a2893ccf6fea0"
-  version "2022-01-31"
+       tag:      "2022-02-07",
+       revision: "ba330548023607717295f0dfd61b72eda41aa9dd"
+  version "2022-02-07"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rust-analyzer"
-    sha256 cellar: :any_skip_relocation, mojave: "69480a89a584888009200c9694ff174d881416d741e50f43078d534c6d163429"
+    sha256 cellar: :any_skip_relocation, mojave: "306c833d49b012fa2f2fb7dbf2b92f9ab664894be77a9040d290fbe43b181749"
   end
 
   depends_on "rust" => :build
