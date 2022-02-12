6524a75562c52301128187494f745e83753ac659
Adrian Ho <the.gromgit@gmail.com>
2022-02-13 05:54:42 +0800

    rpki-client: update 7.6 bottle.

--- Formula/rpki-client.rb
+++ Formula/rpki-client.rb
@@ -1,13 +1,13 @@
 class RpkiClient < Formula
   desc "OpenBSD portable rpki-client"
   homepage "https://www.rpki-client.org/index.html"
-  url "https://ftp.openbsd.org/pub/OpenBSD/rpki-client/rpki-client-7.5.tar.gz"
-  sha256 "e956c0af4973539f725d26526669a6d01800436053b0257c1d069a42c384b2ab"
+  url "https://ftp.openbsd.org/pub/OpenBSD/rpki-client/rpki-client-7.6.tar.gz"
+  sha256 "d147bbedd1eea0fac3a431144f836074588949c67e7ef6fdd94ecec37b762b6b"
   license "ISC"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rpki-client"
-    sha256 mojave: "81301bc0b37763cc033886423a77d9e1cf320fa2a1c90faca4e00bedc4361c18"
+    sha256 mojave: "7df80b3f94eb7c3fc8f780440884143eafd51c69abcab332f36455c76f492ac2"
   end
 
   depends_on "pkg-config" => :build
@@ -31,6 +31,6 @@ class RpkiClient < Formula
   end
 
   test do
-    assert_match "fts_read ta:", shell_output("#{sbin}/rpki-client -n -d . -R . 2>&1 | tail -1")
+    assert_match "parse file ta/", shell_output("#{sbin}/rpki-client -n -d . -R . 2>&1").lines.last
   end
 end
