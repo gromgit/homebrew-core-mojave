0f645c7457e25c8ad13d8eee074cccddf7c37667
Adrian Ho <the.gromgit@gmail.com>
2022-02-12 22:32:35 +0800

    pugixml: update 1.12 bottle.

--- Formula/pugixml.rb
+++ Formula/pugixml.rb
@@ -1,10 +1,9 @@
 class Pugixml < Formula
   desc "Light-weight C++ XML processing library"
   homepage "https://pugixml.org/"
-  url "https://github.com/zeux/pugixml/releases/download/v1.11.4/pugixml-1.11.4.tar.gz"
-  sha256 "8ddf57b65fb860416979a3f0640c2ad45ddddbbafa82508ef0a0af3ce7061716"
+  url "https://github.com/zeux/pugixml/releases/download/v1.12/pugixml-1.12.tar.gz"
+  sha256 "fd6922a4448ec2f3eb9db415d10a49660e5d84ce20ce66b8a07e72ffc84270a7"
   license "MIT"
-  revision 2
 
   livecheck do
     url :stable
@@ -12,13 +11,8 @@ class Pugixml < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "a5284ea4e8d6fa76cd41505294e23a4c448b54d888de993b3cf57308ccbb5cd7"
-    sha256 cellar: :any,                 arm64_big_sur:  "72487f35e935099779ea88bbbf6dd205406710df262674e6295972edff2a9ac1"
-    sha256 cellar: :any,                 monterey:       "83d019d9a46aaa30c40a9acbebfeb19115b09f8534262a8d5ef156bdaa0c3e0b"
-    sha256 cellar: :any,                 big_sur:        "450e11f4eafe21828d3987620406eb42695a60c15086b7741898d483bb05fa8d"
-    sha256 cellar: :any,                 catalina:       "112dda2780766cf7403426252180cea172cd396f7b52aee42a690aa7539c933b"
-    sha256 cellar: :any,                 mojave:         "60d558fea876933be7a5322267ca58c0850eb23ec05d71b3d2c3876793b01367"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3c6e3c671984d4a3c53e58f79ecf45be27b3c57d1073223aa2c0c4dde91aa80e"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pugixml"
+    sha256 cellar: :any_skip_relocation, mojave: "7b8bc1184fcabbc6d6bc9a8fbb20bfce5fb7b16c5e500171315c52af4fad795a"
   end
 
   depends_on "cmake" => :build
