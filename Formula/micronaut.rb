f96addde23a576183c451911da741ac66ce4a7c5
Adrian Ho <the.gromgit@gmail.com>
2022-02-13 00:21:44 +0800

    micronaut: update 3.3.1 bottle.

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -1,8 +1,8 @@
 class Micronaut < Formula
   desc "Modern JVM-based framework for building modular microservices"
   homepage "https://micronaut.io/"
-  url "https://github.com/micronaut-projects/micronaut-starter/archive/v3.3.0.tar.gz"
-  sha256 "d3a8ef78beb90880a56d2b00a480cf0d86fb14d7957b430395eaedf75ed59c51"
+  url "https://github.com/micronaut-projects/micronaut-starter/archive/v3.3.1.tar.gz"
+  sha256 "0d02af9e8ed53763e2b0d2c01d59b88082d3e3b3b7ccb880300fb2ab0930fb77"
   license "Apache-2.0"
 
   livecheck do
@@ -12,7 +12,7 @@ class Micronaut < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/micronaut"
-    sha256 cellar: :any_skip_relocation, mojave: "7597e9734b71629e80c1bab84049d44a13a0299c2c3e8485e2a0aeec2670b473"
+    sha256 cellar: :any_skip_relocation, mojave: "8128b51bd12d6e6a5c9536ae5abaf65741a2b4186ad4f39d1c4fcb5a30995318"
   end
 
   # Uses a hardcoded list of supported JDKs. Try switching to `openjdk` on update.
