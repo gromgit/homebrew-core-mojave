5ac2c1725cfedf61e585d16ddbc336bcb6f27b5b
Adrian Ho <the.gromgit@gmail.com>
2022-02-13 23:45:22 +0800

    gst-libav: update 1.18.5_1 bottle.

--- Formula/gst-libav.rb
+++ Formula/gst-libav.rb
@@ -4,7 +4,8 @@ class GstLibav < Formula
   url "https://gstreamer.freedesktop.org/src/gst-libav/gst-libav-1.18.5.tar.xz"
   sha256 "822e008a910e9dd13aedbdd8dc63fedef4040c0ee2e927bab3112e9de693a548"
   license "LGPL-2.1-or-later"
-  head "https://gitlab.freedesktop.org/gstreamer/gst-libav.git"
+  revision 1
+  head "https://gitlab.freedesktop.org/gstreamer/gst-libav.git", branch: "master"
 
   livecheck do
     url "https://gstreamer.freedesktop.org/src/gst-libav/"
@@ -13,16 +14,18 @@ class GstLibav < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gst-libav"
-    sha256 cellar: :any, mojave: "3f09921b69f9f6d9cad1c7a63436a58a0423c0cef061a109ee5af97bcb2d578a"
+    sha256 cellar: :any, mojave: "b1020d9a787d4c1c0da7ac465b83f94f6f2deae5fea34c5130edacf74f38718c"
   end
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "yasm" => :build
-  depends_on "ffmpeg"
+  depends_on "ffmpeg@4"
   depends_on "gst-plugins-base"
   depends_on "xz" # For LZMA
+
+  fails_with gcc: "5" # ffmpeg is compiled with GCC
 
   def install
     mkdir "build" do
