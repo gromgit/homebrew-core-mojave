ad5bec88df90fc458a8a6f7a7984d7ef82ffeff7
Adrian Ho <the.gromgit@gmail.com>
2022-02-14 06:02:25 +0800

    pianobar: update 2020.11.28_2 bottle.

--- Formula/pianobar.rb
+++ Formula/pianobar.rb
@@ -4,26 +4,23 @@ class Pianobar < Formula
   url "https://6xq.net/pianobar/pianobar-2020.11.28.tar.bz2"
   sha256 "653bfb96b548259e3ac360752f66fdb77e8e220312e52a43c652f7eb96e7d4fe"
   license "MIT"
-  revision 1
+  revision 2
   head "https://github.com/PromyLOPh/pianobar.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "482957ad22086b0d1ead69b0c7d9612874c1d1f2d135824b4d9652ef7ca1b4f4"
-    sha256 cellar: :any,                 arm64_big_sur:  "67d05332f2f15473c2a26d58ecc2d944cbc395be299bd0607fb73606f16469d3"
-    sha256 cellar: :any,                 monterey:       "a0ed76ea5d38e652f1685bae005bc33c603c140e59e207167e0453e93afc732d"
-    sha256 cellar: :any,                 big_sur:        "e5db53d507cc120c3cd14d3cdf8ffe4c9084625262ebc493fd96f6202563d8c2"
-    sha256 cellar: :any,                 catalina:       "7076f3d2b4415436821a42bbfebede61dd8a14525d6b0fecce540f9ee25b2bc3"
-    sha256 cellar: :any,                 mojave:         "80ac5640ff018ca9a32c0739730e365eaf3c92b6a7f03848591e144b66c71361"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "66a0abbb1bbc97b991261015724f2c8981db7d1a8f5588971a6031a7c8e56e3f"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pianobar"
+    sha256 cellar: :any, mojave: "d8d12fee8c917dcb347c107455d09b3cfdc20847dbd6128e760f2328ab232df2"
   end
 
   depends_on "pkg-config" => :build
-  depends_on "ffmpeg"
+  depends_on "ffmpeg@4"
   depends_on "json-c"
   depends_on "libao"
   depends_on "libgcrypt"
 
   uses_from_macos "curl"
+
+  fails_with gcc: "5" # ffmpeg is compiled with GCC
 
   def install
     # Discard Homebrew's CFLAGS as Pianobar reportedly doesn't like them
