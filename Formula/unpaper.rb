1bce247e04442532a8d6b999191b7f5c9240db75
Adrian Ho <the.gromgit@gmail.com>
2022-02-14 04:20:21 +0800

    unpaper: update 6.1_8 bottle.

--- Formula/unpaper.rb
+++ Formula/unpaper.rb
@@ -4,16 +4,11 @@ class Unpaper < Formula
   url "https://www.flameeyes.com/files/unpaper-6.1.tar.xz"
   sha256 "237c84f5da544b3f7709827f9f12c37c346cdf029b1128fb4633f9bafa5cb930"
   license "GPL-2.0-or-later"
-  revision 7
+  revision 8
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "294f7cece1a54c389382ed61101719710e0ec7c5f26dd679cd9fa9ce68e919f4"
-    sha256 cellar: :any,                 arm64_big_sur:  "8aadd07d5712465893b6c3625e7da966c8bfdee572c7ba660cedaa3b0cdff034"
-    sha256 cellar: :any,                 monterey:       "dca1c952850c1e9f3496cf2facf1b570d50c90fde110d9297360db337c2d4906"
-    sha256 cellar: :any,                 big_sur:        "a9841d58884ee1a0616a2a115c21c593eab613c0e040bf2b21d02ddeec682765"
-    sha256 cellar: :any,                 catalina:       "63a30f9ac771386a0f7d7302c31abe60855b4c4028458cbf9371270f42ee49e6"
-    sha256 cellar: :any,                 mojave:         "15d95668bd014ac329b703502832f020efcdb9011558ab8ba86ee0c8a458046d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5719e8ac973a33236779dfc6f7cf56ee3d93987510d1b6edae9da1d091f162a3"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/unpaper"
+    sha256 cellar: :any, mojave: "d6cfb40c2b0d2012968a1b798ebb79fa6680cc0b1c99f20cb80696844536033f"
   end
 
   head do
@@ -23,7 +18,7 @@ class Unpaper < Formula
   end
 
   depends_on "pkg-config" => :build
-  depends_on "ffmpeg"
+  depends_on "ffmpeg@4"
 
   uses_from_macos "libxslt"
 
@@ -31,6 +26,8 @@ class Unpaper < Formula
     depends_on "autoconf" => :build
     depends_on "automake" => :build
   end
+
+  fails_with gcc: "5" # ffmpeg is compiled with GCC
 
   def install
     system "autoreconf", "-i" if build.head?
