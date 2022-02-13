c0204002fe1128e792bfe39188a55efa1d8a9a1c
Adrian Ho <the.gromgit@gmail.com>
2022-02-13 22:02:11 +0800

    gifski: update 1.6.4_1 bottle.

--- Formula/gifski.rb
+++ Formula/gifski.rb
@@ -4,15 +4,16 @@ class Gifski < Formula
   url "https://github.com/ImageOptim/gifski/archive/1.6.4.tar.gz"
   sha256 "1bcc1bdbfdb206f44de75662ee5a8a8c61d336c04cb62457e02c9fa8456f3928"
   license "AGPL-3.0-only"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gifski"
-    sha256 cellar: :any, mojave: "96f8a121fc55325fe7319aa0b08a4a0cb6227909185b6cea1cec969b4c24129c"
+    sha256 cellar: :any, mojave: "84f46af1bc839379e0a3bd8602b8e5b52855609d07327194607b6d37616a4f2c"
   end
 
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
-  depends_on "ffmpeg"
+  depends_on "ffmpeg@4"
 
   uses_from_macos "llvm" => :build
 
