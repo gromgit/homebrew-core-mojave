284cd64865efbfc74182900a0e38f33022d2ae9f
Adrian Ho <the.gromgit@gmail.com>
2022-02-13 20:02:36 +0800

    ffmpeg2theora: update 0.30_10 bottle.

--- Formula/ffmpeg2theora.rb
+++ Formula/ffmpeg2theora.rb
@@ -3,8 +3,8 @@ class Ffmpeg2theora < Formula
   homepage "https://v2v.cc/~j/ffmpeg2theora/"
   url "https://v2v.cc/~j/ffmpeg2theora/downloads/ffmpeg2theora-0.30.tar.bz2"
   sha256 "4f6464b444acab5d778e0a3359d836e0867a3dcec4ad8f1cdcf87cb711ccc6df"
-  revision 9
-  head "https://gitlab.xiph.org/xiph/ffmpeg2theora.git"
+  revision 10
+  head "https://gitlab.xiph.org/xiph/ffmpeg2theora.git", branch: "master"
 
   livecheck do
     url "http://v2v.cc/~j/ffmpeg2theora/download.html"
@@ -12,21 +12,19 @@ class Ffmpeg2theora < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "90738560daa49a15f7aabf51be18dd1ce4f4c748c35c669b279e394853200a89"
-    sha256 cellar: :any, arm64_big_sur:  "114e5f48ead0a1375f4dab1217723fe5f6850529ee5fc3f5fe4042295adf327a"
-    sha256 cellar: :any, monterey:       "21653a640f0a30053b39798bb6c4b6755c36dc30db63621f3c5666a04516d114"
-    sha256 cellar: :any, big_sur:        "1c2718b1a6c348dfceaeef1bd155b6caf385cf4756feefb568cb6f42a6f099e2"
-    sha256 cellar: :any, catalina:       "05f0fb622f434c062ea69f39a09ea1db62824efb26fcb8adf0921600785e0b3c"
-    sha256 cellar: :any, mojave:         "30967cb12c298c6441bb8f4d283a9659c314639cc0409a1a446cb1a80216a31b"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ffmpeg2theora"
+    sha256 cellar: :any, mojave: "d4a1519a693b9ef9c21b82f7fa83d95e6ab9a32095b8a5e6f147fbeed8365df4"
   end
 
   depends_on "pkg-config" => :build
   depends_on "scons" => :build
-  depends_on "ffmpeg"
+  depends_on "ffmpeg@4"
   depends_on "libkate"
   depends_on "libogg"
   depends_on "libvorbis"
   depends_on "theora"
+
+  fails_with gcc: "5" # ffmpeg is compiled with GCC
 
   # Use python3 print()
   patch do
