916c082f0f4fb1ab7c7071893e0adccdb59f3a71
Adrian Ho <the.gromgit@gmail.com>
2022-02-13 08:21:41 +0800

    statix: update 0.5.3 bottle.

--- Formula/statix.rb
+++ Formula/statix.rb
@@ -1,14 +1,14 @@
 class Statix < Formula
   desc "Lints and suggestions for the nix programming language"
   homepage "https://github.com/nerdypepper/statix"
-  url "https://github.com/nerdypepper/statix/archive/v0.4.2.tar.gz"
-  sha256 "a5285a28739a4281f9727a6aa71d3d1d300f802caa2da5a3b1089badf00832b3"
+  url "https://github.com/nerdypepper/statix/archive/v0.5.3.tar.gz"
+  sha256 "250ed47096dd58a350af3e08b6959a8ac77b0618381057fb631b40be25892012"
   license "MIT"
   head "https://github.com/nerdypepper/statix.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/statix"
-    sha256 cellar: :any_skip_relocation, mojave: "3cad2bfd04066bd509a1d4c2036e286787ae226740a9467aada71234a3adebac"
+    sha256 cellar: :any_skip_relocation, mojave: "ca969967ba81e20c828ddb3c4bc6e375a76ac374a890d9d8e0724357cb1e5d64"
   end
 
   depends_on "rust" => :build
@@ -21,7 +21,7 @@ class Statix < Formula
     (testpath/"test.nix").write <<~EOS
       github:nerdypepper/statix
     EOS
-    assert_match "Found unquoted URI expression", shell_output("#{bin}/statix check test.nix")
+    assert_match "Found unquoted URI expression", shell_output("#{bin}/statix check test.nix", 1)
 
     system bin/"statix", "fix", "test.nix"
     system bin/"statix", "check", "test.nix"
