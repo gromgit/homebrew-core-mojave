cb81c8bfc42b14905bb471b5e49f954e3cbb0c85
Adrian Ho <the.gromgit@gmail.com>
2022-02-14 01:26:56 +0800

    mgba: update 0.9.3_1 bottle.

--- Formula/mgba.rb
+++ Formula/mgba.rb
@@ -1,10 +1,11 @@
 class Mgba < Formula
   desc "Game Boy Advance emulator"
   homepage "https://mgba.io/"
-  url "https://github.com/mgba-emu/mgba/archive/0.9.2.tar.gz"
-  sha256 "29ca22ebc56b26a4e7224efbb5fa12c9c006563d41990afb0874d048db76add4"
+  url "https://github.com/mgba-emu/mgba/archive/0.9.3.tar.gz"
+  sha256 "692ff0ac50e18380df0ff3ee83071f9926715200d0dceedd9d16a028a59537a0"
   license "MPL-2.0"
-  head "https://github.com/mgba-emu/mgba.git"
+  revision 1
+  head "https://github.com/mgba-emu/mgba.git", branch: "master"
 
   livecheck do
     url :stable
@@ -12,21 +13,20 @@ class Mgba < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "abaec6f38393e67a34ae5608a96b87b6cfa79c897ed6604e9eed580a1b124f01"
-    sha256 cellar: :any, arm64_big_sur:  "748990bd29b7c677472169b38a191bb6845e0380332f674b18acae32bc872a28"
-    sha256 cellar: :any, big_sur:        "bbd6ee9c4ef3698e28e58da996235b1c51b085f79f8f9042aff9d9b25950f6dc"
-    sha256 cellar: :any, catalina:       "c3719a69184da453cbab9569ca328bde05a46b157fc1b8f1668e7c481e1b161c"
-    sha256 cellar: :any, mojave:         "ae8283ff8e328160830007d48d4b1338ffa17e01dd41e3fc9438c447f18c473d"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mgba"
+    sha256 cellar: :any, mojave: "e96e26df874d578dbb9562bc37020762cd8c9e7f779ea68d5a1a868cd4e09016"
   end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "ffmpeg"
+  depends_on "ffmpeg@4"
   depends_on "libepoxy"
   depends_on "libpng"
   depends_on "libzip"
   depends_on "qt@5"
   depends_on "sdl2"
+
+  fails_with gcc: "5" # ffmpeg is compiled with GCC
 
   def install
     # Install .app bundle into prefix, not prefix/Applications
