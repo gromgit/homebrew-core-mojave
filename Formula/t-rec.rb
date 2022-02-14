489732b174c949d92147a6119e636699191257d0
Adrian Ho <the.gromgit@gmail.com>
2022-02-14 08:05:30 +0800

    t-rec: update 0.7.0 bottle.

--- Formula/t-rec.rb
+++ Formula/t-rec.rb
@@ -1,13 +1,13 @@
 class TRec < Formula
   desc "Blazingly fast terminal recorder that generates animated gif images for the web"
   homepage "https://github.com/sassman/t-rec-rs"
-  url "https://github.com/sassman/t-rec-rs/archive/v0.6.2.tar.gz"
-  sha256 "c24a314c9426322204bf157f83443b84c7a5c22d289edd7b8f0dc1a3e7242df1"
+  url "https://github.com/sassman/t-rec-rs/archive/v0.7.0.tar.gz"
+  sha256 "f5830896bd8829061619916af5400a3a5fa974ab88d2c6631094116bd11e48a1"
   license "GPL-3.0-only"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/t-rec"
-    sha256 cellar: :any_skip_relocation, mojave: "223cb9fa9370b026fe57532f4da3436d5f7db56f74c222127fb5b0eff1c274d4"
+    sha256 cellar: :any_skip_relocation, mojave: "7280432b08a74d1bd1c9dd994c2e72e204c362795d7bfd10c16d4d5b489f2a45"
   end
 
   depends_on "rust" => :build
