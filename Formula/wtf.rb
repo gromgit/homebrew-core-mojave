7f6306869e5552bc2003ba93acc7b24905e21362
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-15 16:09:09 +0800

    wtf: no rebuilding required

--- Formula/wtf.rb
+++ Formula/wtf.rb
@@ -1,8 +1,8 @@
 class Wtf < Formula
   desc "Translate common Internet acronyms"
   homepage "https://sourceforge.net/projects/bsdwtf/"
-  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20211110.tar.gz"
-  sha256 "984c054bd0497247775623eaa709e1545e7696e7a13c6b47abd115a55c548918"
+  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20220210.tar.gz"
+  sha256 "ac1223df7eaec28c0e3c01e3f99473b7887d6e32b038415e240acbb142ded0bc"
   license :public_domain
 
   livecheck do
@@ -11,7 +11,7 @@ class Wtf < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "15ad0cd503e8215e69330b4aec0abd425cea97868124bc62738cd9aa6c23d0df"
+    sha256 cellar: :any_skip_relocation, all: "36ded16d75d48c65b9265f24ba9fcc797fe6356c5eb53de3b72d546c2316de6c"
   end
 
   def install
@@ -23,6 +23,6 @@ class Wtf < Formula
   end
 
   test do
-    system bin/"wtf", "needle"
+    assert_match "where's the food", shell_output("#{bin}/wtf wtf")
   end
 end
