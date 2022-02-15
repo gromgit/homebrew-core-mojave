2050cf1dd22a3f61d0a11bac04ead6aff5718894
Adrian Ho <the.gromgit@gmail.com>
2022-02-16 06:49:33 +0800

    cubejs-cli: update 0.29.28 bottle.

--- Formula/cubejs-cli.rb
+++ Formula/cubejs-cli.rb
@@ -3,13 +3,13 @@ require "language/node"
 class CubejsCli < Formula
   desc "Cube.js command-line interface"
   homepage "https://cube.dev/"
-  url "https://registry.npmjs.org/cubejs-cli/-/cubejs-cli-0.29.24.tgz"
-  sha256 "4c95b71517d2cbe32038a444d140f107deeec9628ab4c05a54c30330a5d901b6"
+  url "https://registry.npmjs.org/cubejs-cli/-/cubejs-cli-0.29.28.tgz"
+  sha256 "6b05a19a711730d3c689fbd5d7b45d6c88dae1e4133c3cc73b87079a428551a3"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cubejs-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "8c5d9b8d53a134c966b0f7fbc539ecd84fac1681fb47d6346cc02e334ed8763d"
+    sha256 cellar: :any_skip_relocation, mojave: "f2c330cfaccaee464783d629bf5d30a51c3273fd3d11da8c0200f1c3738a5654"
   end
 
   depends_on "node"
