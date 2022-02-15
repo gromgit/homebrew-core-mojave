24c55072b502d7178e15e33069a73833d0fb7c56
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-15 16:09:05 +0800

    pure: no rebuilding required

--- Formula/pure.rb
+++ Formula/pure.rb
@@ -1,12 +1,12 @@
 class Pure < Formula
   desc "Pretty, minimal and fast ZSH prompt"
   homepage "https://github.com/sindresorhus/pure"
-  url "https://github.com/sindresorhus/pure/archive/v1.19.0.tar.gz"
-  sha256 "fc5f21e3b2662ef26e7854ca0a88968b1f2911fae19b8efec75e9ea7877ea96b"
+  url "https://github.com/sindresorhus/pure/archive/v1.20.0.tar.gz"
+  sha256 "1fa82dc9c6894dab65d845f38a2c24c790b0095d175da22902a0eee9ea0dd38a"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "212aeb486a9f06ac07692e9cfd9a914354a443eef2b67fda44ce6594c78c15e2"
+    sha256 cellar: :any_skip_relocation, all: "86b966689984c3d9c6a7002c9f580a900f4c185923ebe0dbbc5d2bd364cc9057"
   end
 
   depends_on "zsh" => :test
