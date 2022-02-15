4c528105988e7ad4098af112a69b9c0eac2ac327
Adrian Ho <the.gromgit@gmail.com>
2022-02-16 04:02:59 +0800

    b3sum: update 1.3.1 bottle.

--- Formula/b3sum.rb
+++ Formula/b3sum.rb
@@ -1,13 +1,13 @@
 class B3sum < Formula
   desc "BLAKE3 cryptographic hash function"
   homepage "https://github.com/BLAKE3-team/BLAKE3"
-  url "https://github.com/BLAKE3-team/BLAKE3/archive/1.3.0.tar.gz"
-  sha256 "a559309c2dad8cc8314ea779664ec5093c79de2e9be14edbf76ae2ce380222c0"
+  url "https://github.com/BLAKE3-team/BLAKE3/archive/1.3.1.tar.gz"
+  sha256 "112becf0983b5c83efff07f20b458f2dbcdbd768fd46502e7ddd831b83550109"
   license "CC0-1.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/b3sum"
-    sha256 cellar: :any_skip_relocation, mojave: "91eff732aa404fdbd192d8b3f7ed3c412094709853401edb96a196ecd8c819a1"
+    sha256 cellar: :any_skip_relocation, mojave: "a4c1ffc9976449252b38594939fda726413362f8770348b01346d78c078458b5"
   end
 
   depends_on "rust" => :build
