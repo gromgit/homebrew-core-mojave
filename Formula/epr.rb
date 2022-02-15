77aff6263cc4f03a19d9efab649df6d618665b0d
Adrian Ho <the.gromgit@gmail.com>
2022-02-16 07:46:27 +0800

    epr: update 2.4.13 bottle.

--- Formula/epr.rb
+++ Formula/epr.rb
@@ -3,19 +3,14 @@ class Epr < Formula
 
   desc "Command-line EPUB reader"
   homepage "https://github.com/wustho/epr"
-  url "https://files.pythonhosted.org/packages/7f/4c/caab0fccc12990aa1a9d3ceaa5c7aeab313158293c8386c032e2e767569c/epr-reader-2.4.11.tar.gz"
-  sha256 "5e931653ff954ca8e9fc734efa1e0c0a49512fe2a8652f83a6ca63a8d1c4f2af"
+  url "https://files.pythonhosted.org/packages/c6/d7/3af4967567358fc5e6573a961ebe262179950fd5030ea1d4ee5efda1a76a/epr-reader-2.4.13.tar.gz"
+  sha256 "e9fc3a8053e307cbf6aa1298c78678786329eb405f14e971f9888f69a7950212"
   license "MIT"
-  revision 1
-  head "https://github.com/wustho/epr.git"
+  head "https://github.com/wustho/epr.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "99d91cc2d45112cc563aa030bbb735bf2fa55b0bd3fc81f664ea6bb842fda5c9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "99d91cc2d45112cc563aa030bbb735bf2fa55b0bd3fc81f664ea6bb842fda5c9"
-    sha256 cellar: :any_skip_relocation, monterey:       "0023f8d76b76e28ed7622996d26dd6efd074c6da864598fd4dffa98c77f6f7d3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0023f8d76b76e28ed7622996d26dd6efd074c6da864598fd4dffa98c77f6f7d3"
-    sha256 cellar: :any_skip_relocation, catalina:       "0023f8d76b76e28ed7622996d26dd6efd074c6da864598fd4dffa98c77f6f7d3"
-    sha256 cellar: :any_skip_relocation, mojave:         "0023f8d76b76e28ed7622996d26dd6efd074c6da864598fd4dffa98c77f6f7d3"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/epr"
+    sha256 cellar: :any_skip_relocation, mojave: "673adef6aaea5ec92d9bcab2690ed7d36fcdb7602e74614ab48df27e25ac4dde"
   end
 
   depends_on "python@3.10"
