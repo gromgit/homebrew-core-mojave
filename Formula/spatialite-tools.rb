88416420795f10029ec750bed33a4072ee811fe1
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-15 16:09:08 +0800

    spatialite-tools: no rebuilding required

--- Formula/spatialite-tools.rb
+++ Formula/spatialite-tools.rb
@@ -12,12 +12,13 @@ class SpatialiteTools < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "eb35f849a07740c11d2428ecdc269b6663a58f80387ff0b6db66f8940618f178"
-    sha256 cellar: :any, arm64_big_sur:  "1dc66a96910742c330fa90b8c547ce7d7693d7c3af118707b5a15b6d6e013ef9"
-    sha256 cellar: :any, monterey:       "873e89f72b1efb2b97ed66a8ef49f8eb59331075921eb21788c4ea9ea6f20dcb"
-    sha256 cellar: :any, big_sur:        "1e4d449a6915bfe180238dd30ed93a2909661076a86f66cea306fda8dbdeb8c4"
-    sha256 cellar: :any, catalina:       "362dd2c109f880d356d7f7e0359936717acfc1ca146277f0e2c456955c33492f"
-    sha256 cellar: :any, mojave:         "ffdc8a4b476146b0f58965f3879f1418a4b151bcbe4a213a005cd5eae5ecc50f"
+    sha256 cellar: :any,                 arm64_monterey: "eb35f849a07740c11d2428ecdc269b6663a58f80387ff0b6db66f8940618f178"
+    sha256 cellar: :any,                 arm64_big_sur:  "1dc66a96910742c330fa90b8c547ce7d7693d7c3af118707b5a15b6d6e013ef9"
+    sha256 cellar: :any,                 monterey:       "873e89f72b1efb2b97ed66a8ef49f8eb59331075921eb21788c4ea9ea6f20dcb"
+    sha256 cellar: :any,                 big_sur:        "1e4d449a6915bfe180238dd30ed93a2909661076a86f66cea306fda8dbdeb8c4"
+    sha256 cellar: :any,                 catalina:       "362dd2c109f880d356d7f7e0359936717acfc1ca146277f0e2c456955c33492f"
+    sha256 cellar: :any,                 mojave:         "ffdc8a4b476146b0f58965f3879f1418a4b151bcbe4a213a005cd5eae5ecc50f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "54f016eb120554f2bfc9bf19f02eb06817e8aab6f3ce380537025d291026e90d"
   end
 
   depends_on "pkg-config" => :build
@@ -27,7 +28,7 @@ class SpatialiteTools < Formula
 
   def install
     # See: https://github.com/Homebrew/homebrew/issues/3328
-    ENV.append "LDFLAGS", "-liconv"
+    ENV.append "LDFLAGS", "-liconv" if OS.mac?
     # Ensure Homebrew SQLite is found before system SQLite.
     #
     # spatialite-tools picks `proj` (instead of `proj@7`) if installed
