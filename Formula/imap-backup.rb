1d8e7c633a3758d8a9637ee6941eb60e4c6d4dce
Adrian Ho <the.gromgit@gmail.com>
2022-02-16 11:56:47 +0800

    imap-backup: update 5.1.0 bottle.

--- Formula/imap-backup.rb
+++ Formula/imap-backup.rb
@@ -1,13 +1,13 @@
 class ImapBackup < Formula
   desc "Backup GMail (or other IMAP) accounts to disk"
   homepage "https://github.com/joeyates/imap-backup"
-  url "https://github.com/joeyates/imap-backup/archive/refs/tags/v5.0.0.tar.gz"
-  sha256 "28792919b3d734d05ea5af750bf22fe2a5ccebadd20f4a6339b8c498e29a5ca2"
+  url "https://github.com/joeyates/imap-backup/archive/refs/tags/v5.1.0.tar.gz"
+  sha256 "beafe9510d0c810dece237f6bb7cbba896835ea3a38fbdc36e74c895fcffcbcb"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/imap-backup"
-    sha256 cellar: :any_skip_relocation, mojave: "53e2ea03880b48de42fd0c11ade5b6e40cae6cf0c84d4c10ab3dbae203671ad7"
+    sha256 cellar: :any_skip_relocation, mojave: "3ec59a5f78a55f047874112c617458bba342c59fe16ce45be36e3754429b2640"
   end
 
   uses_from_macos "ruby", since: :catalina
