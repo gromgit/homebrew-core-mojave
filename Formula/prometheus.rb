107891222f4622742c4285e5ce9be00fb930d48d
Adrian Ho <the.gromgit@gmail.com>
2022-02-16 19:20:02 +0800

    prometheus: update 2.33.3 bottle.

--- Formula/prometheus.rb
+++ Formula/prometheus.rb
@@ -1,8 +1,8 @@
 class Prometheus < Formula
   desc "Service monitoring system and time series database"
   homepage "https://prometheus.io/"
-  url "https://github.com/prometheus/prometheus/archive/v2.33.1.tar.gz"
-  sha256 "a3ec7bde701b0663e706c718ea58722544c4a3bbb902d89cf64d3b7f34523ad5"
+  url "https://github.com/prometheus/prometheus/archive/v2.33.3.tar.gz"
+  sha256 "8ad9f25fb1faf8528015154ef8fa6ebb8851a6fd19f0085e6e1e677325f972e8"
   license "Apache-2.0"
 
   livecheck do
@@ -12,7 +12,7 @@ class Prometheus < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/prometheus"
-    sha256 cellar: :any_skip_relocation, mojave: "323cfbdf12e6a8b0195634565b6803a22bede408d08d25cda66680dd214ea1dc"
+    sha256 cellar: :any_skip_relocation, mojave: "ee7de2d13c174ae3def1b3e704c5d7ea9ccf22328cec44d2d9369b293e400d8e"
   end
 
   depends_on "go" => :build
