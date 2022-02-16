1bf4c101475147f05d12f8ad0c03f6339a667d8d
Adrian Ho <the.gromgit@gmail.com>
2022-02-16 22:33:17 +0800

    tctl: update 1.15.0 bottle.

--- Formula/tctl.rb
+++ Formula/tctl.rb
@@ -1,13 +1,13 @@
 class Tctl < Formula
   desc "Temporal CLI (tctl)"
   homepage "https://temporal.io/"
-  url "https://github.com/temporalio/temporal/archive/v1.14.5.tar.gz"
-  sha256 "332520a6eb2b64199a4ceda43eb19b7a8831f9bb4d15c3a4c043b8f53d9e91f5"
+  url "https://github.com/temporalio/temporal/archive/v1.15.0.tar.gz"
+  sha256 "fc3a9c6df04a9396a65f984def2f87f24c646f1c8cbc42cc0a122bda53df3977"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tctl"
-    sha256 cellar: :any_skip_relocation, mojave: "f4b00c928922cbcc385e335fad7cf0edd8177169fee271bf846a49455edb6be3"
+    sha256 cellar: :any_skip_relocation, mojave: "7fbd217139588cd0db40b7b53e59b8d4537959b3ac7ee11ab91aaa87ce08d181"
   end
 
   depends_on "go" => :build
