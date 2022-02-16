d2882ef7ee105322d4fa0885a28baded59b395ef
Adrian Ho <the.gromgit@gmail.com>
2022-02-16 10:53:51 +0800

    goose: update 3.5.3 bottle.

--- Formula/goose.rb
+++ Formula/goose.rb
@@ -1,13 +1,13 @@
 class Goose < Formula
   desc "Go Language's command-line interface for database migrations"
   homepage "https://github.com/pressly/goose"
-  url "https://github.com/pressly/goose/archive/v3.5.2.tar.gz"
-  sha256 "295667d9f784d73be3a4728d4d1a4caf30f826892073c76983ea2ed8bd368c0f"
+  url "https://github.com/pressly/goose/archive/v3.5.3.tar.gz"
+  sha256 "9a5f9dc2b3b5f0876ad1e4609e1d1812e2b9ff03ea04e00c69ca05e7d9584601"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/goose"
-    sha256 cellar: :any_skip_relocation, mojave: "ce236b442de5161cf31965f0f80c07fa28016add132f2b3be6db6ebe4b35449c"
+    sha256 cellar: :any_skip_relocation, mojave: "6392a27255b63ff46f07f5c32794b7a3db7fa9766da31638a09a2749a49fed33"
   end
 
   depends_on "go" => :build
