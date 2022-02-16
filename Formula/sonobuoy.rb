986d0434c77dc4b189d1da02c14e3878d4447098
Adrian Ho <the.gromgit@gmail.com>
2022-02-16 22:14:29 +0800

    sonobuoy: update 0.56.1 bottle.

--- Formula/sonobuoy.rb
+++ Formula/sonobuoy.rb
@@ -1,13 +1,13 @@
 class Sonobuoy < Formula
   desc "Kubernetes component that generates reports on cluster conformance"
   homepage "https://github.com/vmware-tanzu/sonobuoy"
-  url "https://github.com/vmware-tanzu/sonobuoy/archive/v0.56.0.tar.gz"
-  sha256 "bfaa1d09df8b2f0590b57d50a9a2b556b712231519ae22a7bb71cd6cf5be8b16"
+  url "https://github.com/vmware-tanzu/sonobuoy/archive/v0.56.1.tar.gz"
+  sha256 "4aaa1cfa87ef589c8aedd1c43c78acd4a8a590e2b499627252d0347cc8c22602"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sonobuoy"
-    sha256 cellar: :any_skip_relocation, mojave: "c83bc9fb6b9bf2a375e131f76cbfc466fe95354a35d181faa1f01a666645ecb2"
+    sha256 cellar: :any_skip_relocation, mojave: "c12952b5f9d7adf40e0991d13ddcf764f40f2f33fc74187d13908b9ee29156ad"
   end
 
   depends_on "go" => :build
