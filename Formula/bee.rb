16d616521f4a30043b007d8c2100ab79a12141f6
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-18 14:16:02 +0800

    bee: no rebuilding required

--- Formula/bee.rb
+++ Formula/bee.rb
@@ -1,12 +1,12 @@
 class Bee < Formula
   desc "Tool for managing database changes"
   homepage "https://github.com/bluesoft/bee"
-  url "https://github.com/bluesoft/bee/releases/download/1.95/bee-1.95.zip"
-  sha256 "f999eca2d55a0d0ab9644be16b96dc2664a4b31ecbfad1990d8bb3c03ec4102b"
+  url "https://github.com/bluesoft/bee/releases/download/1.96/bee-1.96.zip"
+  sha256 "6f2ee8ad9b86d12b0e82211f74f551ba0ad88c9505dbf03e488a40d678eb9248"
   license "MPL-1.1"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "dafd1f5d70b1d88ef5cd43f2a53e2aab92eedbe9d4ed069d3e7573cfc8d903a0"
+    sha256 cellar: :any_skip_relocation, all: "754a11010f3da239d8282a4e79fac79e10eba3d15df8c0f5839ccdd3d23ad962"
   end
 
   depends_on "openjdk@8"
