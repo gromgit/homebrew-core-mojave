21c418744e599c0aa75ff27289b6432ca0ab6394
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-18 14:16:18 +0800

    nerdctl:

--- Formula/nerdctl.rb
+++ Formula/nerdctl.rb
@@ -1,13 +1,13 @@
 class Nerdctl < Formula
   desc "ContaiNERD CTL - Docker-compatible CLI for containerd"
   homepage "https://github.com/containerd/nerdctl"
-  url "https://github.com/containerd/nerdctl/archive/refs/tags/v0.16.1.tar.gz"
-  sha256 "be19ceed51703a71f847bfff3afcf25e0efd4c012fd8ec1ff02c545d6773989d"
+  url "https://github.com/containerd/nerdctl/archive/refs/tags/v0.17.0.tar.gz"
+  sha256 "581e0c91313dcbd0011412a7a026fa5f811ba39eaee4b45a616885fcacd7f8c7"
   license "Apache-2.0"
   head "https://github.com/containerd/nerdctl.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "a0472201306c31c7ef373a1581fb9d5178b8b30c8e23330e8699e36b9afcb872"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1b496e12e3e84d4fec82f20c04f44a3cc3956980bf0d15dbe19a7f9dcc804bb6"
   end
 
   depends_on "go" => :build
