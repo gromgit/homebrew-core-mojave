f97add88bf4e43682e40a97c3b1d5a416c89b41c
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-18 14:16:06 +0800

    ensmallen: no rebuilding required

--- Formula/ensmallen.rb
+++ Formula/ensmallen.rb
@@ -1,13 +1,13 @@
 class Ensmallen < Formula
   desc "Flexible C++ library for efficient mathematical optimization"
   homepage "https://ensmallen.org"
-  url "https://github.com/mlpack/ensmallen/archive/2.18.1.tar.gz"
-  sha256 "573964639bb52f7a5518f41d1d4c673dc69685f5d36dad3d87deaaa5f8c23e87"
+  url "https://github.com/mlpack/ensmallen/archive/2.18.2.tar.gz"
+  sha256 "62f17232d1f8236f71de967f349ef4d7d09ffd3d80c8fb9236bbdf2b5a6ea22b"
   license "BSD-3-Clause"
-  head "https://github.com/mlpack/ensmallen.git"
+  head "https://github.com/mlpack/ensmallen.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "ac49dd105f936dba0923e6a061cf15c0ff20bf26809a810077993da7c0ce9a71"
+    sha256 cellar: :any_skip_relocation, all: "6056a1b9ef0812407989420dc40a2a0d59d3aecc9033ab21bdb52c7ffaa14496"
   end
 
   depends_on "cmake" => :build
