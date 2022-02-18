ccc22b4236ebc1301d19093da317709b5adba3bb
Adrian Ho <the.gromgit@gmail.com>
2022-02-19 03:34:06 +0800

    thrift: update 0.16.0 bottle.

--- Formula/thrift.rb
+++ Formula/thrift.rb
@@ -4,9 +4,9 @@ class Thrift < Formula
   license "Apache-2.0"
 
   stable do
-    url "https://www.apache.org/dyn/closer.lua?path=thrift/0.15.0/thrift-0.15.0.tar.gz"
-    mirror "https://archive.apache.org/dist/thrift/0.15.0/thrift-0.15.0.tar.gz"
-    sha256 "d5883566d161f8f6ddd4e21f3a9e3e6b8272799d054820f1c25b11e86718f86b"
+    url "https://www.apache.org/dyn/closer.lua?path=thrift/0.16.0/thrift-0.16.0.tar.gz"
+    mirror "https://archive.apache.org/dist/thrift/0.16.0/thrift-0.16.0.tar.gz"
+    sha256 "f460b5c1ca30d8918ff95ea3eb6291b3951cf518553566088f3f2be8981f6209"
 
     # Fix -flat_namespace being used on Big Sur and later.
     patch do
@@ -16,13 +16,8 @@ class Thrift < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "77ea98789bcafe47df87460c0b236c2a4d0883d3a87f189de5fa228f14b920d1"
-    sha256 cellar: :any,                 arm64_big_sur:  "8352c16aaef3267d863dc31aa6e6562ab59ce5823b61296a0a2dfe62fb747112"
-    sha256 cellar: :any,                 monterey:       "8fafe39b9a14df03183aadb76da3d2005972de99f9ef99820a760e71bf131f8b"
-    sha256 cellar: :any,                 big_sur:        "e371159616481c100f45d01660e70ff68d9df907baef025982e8650ff485b3f4"
-    sha256 cellar: :any,                 catalina:       "77ffd9f8aea765983f90ae45c94afd32502d035bb09f58b601290f65fc2966e0"
-    sha256 cellar: :any,                 mojave:         "e1f46a93099ec7b01caf380433a05d5014686cc5ea59293286fc55a5b0efd39c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fa1ac943dc0f963402b71ce1189f19e62aa51510165c4f3607dc45b6b95f273f"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/thrift"
+    sha256 cellar: :any, mojave: "801968fa58cca29f60cc0439851aa89b053ef25a3b362061914aee6b81ae4b04"
   end
 
   head do
