2e5d5c163b3aff2feff98412e1c98779aa03c429
Adrian Ho <the.gromgit@gmail.com>
2022-02-19 16:00:06 +0800

    cbmc: update 5.51.0 bottle.

--- Formula/cbmc.rb
+++ Formula/cbmc.rb
@@ -2,13 +2,13 @@ class Cbmc < Formula
   desc "C Bounded Model Checker"
   homepage "https://www.cprover.org/cbmc/"
   url "https://github.com/diffblue/cbmc.git",
-      tag:      "cbmc-5.50.0",
-      revision: "05210b57773a6429cc183f33226a9d780c6f8757"
+      tag:      "cbmc-5.51.0",
+      revision: "5469136a4ca8307175dcbd76b796338928624f0f"
   license "BSD-4-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cbmc"
-    sha256 cellar: :any_skip_relocation, mojave: "472aa72af5efb89647c02132f5170060f6ad26089dbcdb1489b0b394b6512a86"
+    sha256 cellar: :any_skip_relocation, mojave: "ee8818b583b5d257206e7d89bb79ca171ce66efee95538e91b130c8bc32d836d"
   end
 
   depends_on "cmake" => :build
