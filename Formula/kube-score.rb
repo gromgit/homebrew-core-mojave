5d96b180d79931d70facea1bb6dc607bf98a7a0a
Adrian Ho <the.gromgit@gmail.com>
2022-02-20 01:55:52 +0800

    kube-score: update 1.14.0 bottle.

--- Formula/kube-score.rb
+++ Formula/kube-score.rb
@@ -2,15 +2,14 @@ class KubeScore < Formula
   desc "Kubernetes object analysis recommendations for improved reliability and security"
   homepage "https://kube-score.com"
   url "https://github.com/zegl/kube-score.git",
-      tag:      "v1.13.0",
-      revision: "d1ad91defc1a5814f7c7395ed64cd7039d259158"
+      tag:      "v1.14.0",
+      revision: "3d6392471f65c47a1c617d78d9f84a53457c5f5d"
   license "MIT"
   head "https://github.com/zegl/kube-score.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kube-score"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "0638ba50d600e2dca18cd3fa6628749f5fe08d410030d1ec6a29a0dd4e32b67e"
+    sha256 cellar: :any_skip_relocation, mojave: "9311cefaf3b88ef48da8eef128d749b3631d2e3660eed8ee4f3dd20f20e89089"
   end
 
   depends_on "go" => :build
@@ -21,7 +20,7 @@ class KubeScore < Formula
       -X main.version=#{version}
       -X main.commit=#{Utils.git_head}
       -X main.date=#{time.iso8601}
-    ].join(" ")
+    ]
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/kube-score"
   end
 
