a5d30589e2d36863417a5490a26f77466481ae0a
Adrian Ho <the.gromgit@gmail.com>
2022-02-20 00:41:07 +0800

    help2man: update 1.49.1 bottle.

--- Formula/help2man.rb
+++ Formula/help2man.rb
@@ -1,19 +1,14 @@
 class Help2man < Formula
   desc "Automatically generate simple man pages"
   homepage "https://www.gnu.org/software/help2man/"
-  url "https://ftp.gnu.org/gnu/help2man/help2man-1.48.5.tar.xz"
-  mirror "https://ftpmirror.gnu.org/help2man/help2man-1.48.5.tar.xz"
-  sha256 "6739e4caa42e6aed3399be4387ca79399640967334e91728863b8eaa922582be"
+  url "https://ftp.gnu.org/gnu/help2man/help2man-1.49.1.tar.xz"
+  mirror "https://ftpmirror.gnu.org/help2man/help2man-1.49.1.tar.xz"
+  sha256 "fd99a664ec4be9a86a0dd89719989f14f367a9c079d75d0e1d71e18a7bb51b03"
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5f0773ffb77e0e13dce87ce7726fe3f29184f431c345840c7dc869a3b6cf276d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5f0773ffb77e0e13dce87ce7726fe3f29184f431c345840c7dc869a3b6cf276d"
-    sha256 cellar: :any,                 monterey:       "49e2e969ec8ed1c4f7954d0236fd64719e102a128c42af685767bfc5f213ac07"
-    sha256 cellar: :any,                 big_sur:        "49eb6e847eeb7e51779289d351a337ef0c6cba97ac079806066747456036d8e6"
-    sha256 cellar: :any,                 catalina:       "4abfd6faab1e8286a08da1874e46ebf8a32812f84953740b08ddf2b17be6eb3c"
-    sha256 cellar: :any,                 mojave:         "8586eeac3d452b94b08a519bc01a4887e534832e7be01aa1c0c5bfefeda5b6a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f1316c8afe193ef525f86ba791f23557ac403d68b14677e6e29ba536df59a0be"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/help2man"
+    sha256 cellar: :any, mojave: "ad256b3da9625df825b26e1f86d2281854de16970cf9946242b646297240d90f"
   end
 
   depends_on "gettext" if Hardware::CPU.intel?
