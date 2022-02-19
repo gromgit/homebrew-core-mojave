a6f604edbd672a111d7b8b74e2debfaf215b5913
Adrian Ho <the.gromgit@gmail.com>
2022-02-20 07:00:24 +0800

    solidity: update 0.8.12 bottle.

--- Formula/solidity.rb
+++ Formula/solidity.rb
@@ -1,8 +1,8 @@
 class Solidity < Formula
   desc "Contract-oriented programming language"
   homepage "https://soliditylang.org"
-  url "https://github.com/ethereum/solidity/releases/download/v0.8.10/solidity_0.8.10.tar.gz"
-  sha256 "3f157aa2bc8bcbd8975fe5e41c476b3d777432dfe7c64e9d59247ee225a89ee3"
+  url "https://github.com/ethereum/solidity/releases/download/v0.8.12/solidity_0.8.12.tar.gz"
+  sha256 "c63c2caf64feaef9fbde7c8d26e3d15aa9b159d08e7213e1e6bd3e4fd06c8308"
   license all_of: ["GPL-3.0-or-later", "MIT", "BSD-3-Clause", "Apache-2.0", "CC0-1.0"]
 
   livecheck do
@@ -12,8 +12,7 @@ class Solidity < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/solidity"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "3e2e38be805e48d72e2be8b92541938d78e08448d1d8c9c9750564950b3b34b2"
+    sha256 cellar: :any_skip_relocation, mojave: "adea2be7ec2667fd89a95e78cc2add829220bc97b09a1218dccbd71c2777c88e"
   end
 
   depends_on "cmake" => :build
