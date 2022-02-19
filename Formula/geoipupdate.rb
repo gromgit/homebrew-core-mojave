995fad012549ad3c653b3edf7e2816dba6b782ed
Adrian Ho <the.gromgit@gmail.com>
2022-02-19 23:26:28 +0800

    geoipupdate: update 4.9.0 bottle.

--- Formula/geoipupdate.rb
+++ Formula/geoipupdate.rb
@@ -1,19 +1,14 @@
 class Geoipupdate < Formula
   desc "Automatic updates of GeoIP2 and GeoIP Legacy databases"
   homepage "https://github.com/maxmind/geoipupdate"
-  url "https://github.com/maxmind/geoipupdate/archive/v4.8.0.tar.gz"
-  sha256 "ca718c3ffcc595ef441363699888d20150f1d3a6583ac2d60bcbd34f052db09f"
+  url "https://github.com/maxmind/geoipupdate/archive/v4.9.0.tar.gz"
+  sha256 "43195d457a372dc07be593d815212d6ea21e499a37a6111058efa3296759cba9"
   license "Apache-2.0"
-  head "https://github.com/maxmind/geoipupdate.git"
+  head "https://github.com/maxmind/geoipupdate.git", branch: "main"
 
   bottle do
-    sha256 arm64_monterey: "3fd3f5ed2425347901d41d310a94a177fe8fcfccb9cd8755bcb8c7ec8a5756a3"
-    sha256 arm64_big_sur:  "7f8823d01da79c6839c4621a1ae80c20541fb83f6f1197278f4665b4ab4cb0ad"
-    sha256 monterey:       "b25b652195cb842c9e8d93f8f58ff87e422c329b926e9dc00297c7458f66b3b6"
-    sha256 big_sur:        "100e9ece8f4563fc6ed597be8cbb39c4fd4299067917234d72354df56f9a34bc"
-    sha256 catalina:       "ebffdddea99838681cdb84804386668ec9f8b2ed74895ad65bae09820a8da2df"
-    sha256 mojave:         "7842336aef38b28f567dd3b9d9379764f5725837eda91242aa26cfab8b2521c9"
-    sha256 x86_64_linux:   "87b67e239bfa889f4a35890b50003241d4e68ad639bdd9e61274c871dea4ad6c"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/geoipupdate"
+    sha256 mojave: "e380faa08ea28bbbf9aca77aabb915c6823acba20b7b7c82803b36f0671bc34d"
   end
 
   depends_on "go" => :build
