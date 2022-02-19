fde9127eb9aa64b91eefa741a3f29f263da0c6f7
Adrian Ho <the.gromgit@gmail.com>
2022-02-20 07:40:49 +0800

    taskwarrior-tui: update 0.19.4 bottle.

--- Formula/taskwarrior-tui.rb
+++ Formula/taskwarrior-tui.rb
@@ -1,8 +1,8 @@
 class TaskwarriorTui < Formula
   desc "Terminal user interface for taskwarrior"
   homepage "https://github.com/kdheepak/taskwarrior-tui"
-  url "https://github.com/kdheepak/taskwarrior-tui/archive/v0.19.1.tar.gz"
-  sha256 "a591f3e07e4c0dfb7569249f3bbc5be4a58b3272c786b630a8454090f6cca07a"
+  url "https://github.com/kdheepak/taskwarrior-tui/archive/v0.19.4.tar.gz"
+  sha256 "4c482a997da9503f77e597bf652d5b9fa49d98f76ead524af8379b93e8d1b0e6"
   license "MIT"
   head "https://github.com/kdheepak/taskwarrior-tui.git", branch: "main"
 
@@ -13,7 +13,7 @@ class TaskwarriorTui < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/taskwarrior-tui"
-    sha256 cellar: :any_skip_relocation, mojave: "091edcde4df7ca1359b6bdf7df11475b512117c929505cc6478f3bcae16cfdc6"
+    sha256 cellar: :any_skip_relocation, mojave: "25cdd6f7c8cbea8abd62effc7b9950ba5c9e38c4a65941e85da9b6da0d770250"
   end
 
   depends_on "rust" => :build
