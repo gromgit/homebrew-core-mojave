3d99a512f2fa542929c3e36d8ee616d00a6a697a
Adrian Ho <the.gromgit@gmail.com>
2022-02-20 04:37:38 +0800

    node-sass: update 1.49.8 bottle.

--- Formula/node-sass.rb
+++ Formula/node-sass.rb
@@ -3,13 +3,13 @@ class NodeSass < Formula
 
   desc "JavaScript implementation of a Sass compiler"
   homepage "https://github.com/sass/dart-sass"
-  url "https://registry.npmjs.org/sass/-/sass-1.49.7.tgz"
-  sha256 "4f41378bb578d02829a1a53b8056b2dc759e9dbd2caa9bf64583062a7af95210"
+  url "https://registry.npmjs.org/sass/-/sass-1.49.8.tgz"
+  sha256 "4a0a2bb23f5054c7e5b807f62442a86cb2dddb252f4e3aaa3b5741bdf85ecf6f"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/node-sass"
-    sha256 cellar: :any_skip_relocation, mojave: "e007d5cf5b64a19479d13f1a9016a81c6e7399ad5940523b24e9229298b94cc2"
+    sha256 cellar: :any_skip_relocation, mojave: "8a87801e8138924bfc0287cfef7ef49c8888ede2a284ea2573dadab4b54c8b0e"
   end
 
   depends_on "node"
