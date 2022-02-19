3b1a75a89236f865a570905ca177adbd95f58f6a
Adrian Ho <the.gromgit@gmail.com>
2022-02-20 01:48:40 +0800

    ko: update 0.10.0 bottle.

--- Formula/ko.rb
+++ Formula/ko.rb
@@ -1,27 +1,28 @@
 class Ko < Formula
   desc "Build and deploy Go applications on Kubernetes"
   homepage "https://github.com/google/ko"
-  url "https://github.com/google/ko/archive/v0.9.3.tar.gz"
-  sha256 "a31c9f6f3fd443599b854338f396f0e4c43a3d6ef7b1138f5df75a2c1c785c61"
+  url "https://github.com/google/ko/archive/v0.10.0.tar.gz"
+  sha256 "55431dcb5c3c82cbc3e636ead9d04d5798154abe1f601785b99923be35b3d2cf"
   license "Apache-2.0"
-  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ko"
-    sha256 cellar: :any_skip_relocation, mojave: "3dee036c7d1b25ed7645acfef02786ae288acf95c9f0fd67e05a514367010ae6"
+    sha256 cellar: :any_skip_relocation, mojave: "85fa03ad27cb07ff58a0af19f9dd1e50046d44584176a15e905fca386c376cd9"
   end
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args, "-ldflags",
-      "-s -w -X github.com/google/ko/pkg/commands.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/google/ko/pkg/commands.Version=#{version}")
 
-    bash_output = Utils.safe_popen_read(bin/"ko", "completion")
+    bash_output = Utils.safe_popen_read(bin/"ko", "completion", "bash")
     (bash_completion/"ko").write bash_output
 
-    zsh_output = Utils.safe_popen_read(bin/"ko", "completion", "--zsh")
+    zsh_output = Utils.safe_popen_read(bin/"ko", "completion", "zsh")
     (zsh_completion/"_ko").write zsh_output
+
+    fish_output = Utils.safe_popen_read(bin/"ko", "completion", "fish")
+    (fish_completion/"ko.fish").write fish_output
   end
 
   test do
