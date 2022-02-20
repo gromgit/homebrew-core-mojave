b4e12314d420084ce28a94900c0d86e364db1c87
Adrian Ho <the.gromgit@gmail.com>
2022-02-20 09:59:41 +0800

    vttest: update 20220215 bottle.

--- Formula/vttest.rb
+++ Formula/vttest.rb
@@ -1,8 +1,8 @@
 class Vttest < Formula
   desc "Test compatibility of VT100-compatible terminals"
   homepage "https://invisible-island.net/vttest/"
-  url "https://invisible-mirror.net/archives/vttest/vttest-20210210.tgz"
-  sha256 "0f98a2e305982915f1520984c3e8698e3acd508ee210711528c89f5a7ea7f046"
+  url "https://invisible-mirror.net/archives/vttest/vttest-20220215.tgz", using: :homebrew_curl
+  sha256 "4a65998c5e12cf08ced2cfce119adb44fa842ac1495d0f150f21c8a6785915a1"
   license "BSD-3-Clause"
 
   livecheck do
@@ -11,13 +11,8 @@ class Vttest < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a02544dcf6c8fc490aed7bb2b119cfe90f6b59825e31a6bbba8aca7fca742091"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5bd298d05ad47843648ba8f8b8825614d647b495e4c09c34e00e4947eff9a016"
-    sha256 cellar: :any_skip_relocation, monterey:       "75c92cbf81736c4108544e7432d1d5aec9052f6bcd5c20c6e57c3dc0ac94e370"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bcaa7bfde1caffa18b43a357d684b92e6cfc1db34e5f77a47fb4be22f9ed2051"
-    sha256 cellar: :any_skip_relocation, catalina:       "07691c12896f2adfd4cae9ddd7f04b3fcd67ccd6727fe59b7ba79200e7f4961c"
-    sha256 cellar: :any_skip_relocation, mojave:         "a73bba40a8459292c25b2999286c29278c9789cbaed70e5382fb1f2b116a0b21"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0e10da7578e73cafdf87514ae43ef35a68dcc39c7ee58e218f188443db20aae4"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/vttest"
+    sha256 cellar: :any_skip_relocation, mojave: "9b1e08a64bf323c4532f6ed294acdcb31c32f2adacdc8d18beaa562b972eb27b"
   end
 
   def install
