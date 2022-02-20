40e6900ae5ab3a196fbc1d2010a053d943e134e8
Adrian Ho <the.gromgit@gmail.com>
2022-02-20 10:15:26 +0800

    wabt: update 1.0.27 bottle.

--- Formula/wabt.rb
+++ Formula/wabt.rb
@@ -2,8 +2,8 @@ class Wabt < Formula
   desc "Web Assembly Binary Toolkit"
   homepage "https://github.com/WebAssembly/wabt"
   url "https://github.com/WebAssembly/wabt.git",
-      tag:      "1.0.25",
-      revision: "b3f1efb261b059d40a4d103c803ccbe3c32df7ae"
+      tag:      "1.0.27",
+      revision: "3e8207aabe969098d2b4941142a1973008c63033"
   license "Apache-2.0"
 
   livecheck do
@@ -13,11 +13,17 @@ class Wabt < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/wabt"
-    sha256 cellar: :any_skip_relocation, mojave: "336c03e501d74919b3d9ffddb465f19129dfa0c13d107a3eec9e5224256d2939"
+    sha256 cellar: :any_skip_relocation, mojave: "6ed64a0729c65da98ecfdbc15c3816b03cb4a1f51efba1d61b43b75d9576f978"
   end
 
   depends_on "cmake" => :build
   depends_on "python@3.10" => :build
+
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5" # C++17
 
   def install
     mkdir "build" do
