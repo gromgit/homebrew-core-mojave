dfe0e49fbb4144bb766fafd204a1989aa25312d8
Adrian Ho <the.gromgit@gmail.com>
2022-02-20 08:12:29 +0800

    tmuxinator: update 3.0.2 bottle.

--- Formula/tmuxinator.rb
+++ Formula/tmuxinator.rb
@@ -1,19 +1,14 @@
 class Tmuxinator < Formula
   desc "Manage complex tmux sessions easily"
   homepage "https://github.com/tmuxinator/tmuxinator"
-  url "https://github.com/tmuxinator/tmuxinator/archive/v3.0.1.tar.gz"
-  sha256 "1d79dd13beaaf2ed24c8a76410c1a41bedc7785498e199534fa3928bbf8aab01"
+  url "https://github.com/tmuxinator/tmuxinator/archive/v3.0.2.tar.gz"
+  sha256 "3992e4a13113ebc6d1d7571c2fae05a056655ee7bfd2eba1455242e30a6dcdd8"
   license "MIT"
   head "https://github.com/tmuxinator/tmuxinator.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b641b1fedbadf9ba1baf65e9819e718ae30f486e4b1e09e24ab72da50ff28add"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cdd418ca1a015eb94bb192d8605d019ad4acdc69b1d7d066421ea9b0538d5003"
-    sha256 cellar: :any_skip_relocation, monterey:       "8b185d024fb7ab30b4af44dc242908f1260e70c75f5b00928fa20e2ca98458ac"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0735adc0586540e832e281dcbc9e43179fb89b6f50045f253a77f6914afea273"
-    sha256 cellar: :any_skip_relocation, catalina:       "0735adc0586540e832e281dcbc9e43179fb89b6f50045f253a77f6914afea273"
-    sha256 cellar: :any_skip_relocation, mojave:         "0735adc0586540e832e281dcbc9e43179fb89b6f50045f253a77f6914afea273"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8d0233e824befa21871fa4ba3578be8e30bf952ca3fe46c6155cb2f03120d174"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tmuxinator"
+    sha256 cellar: :any_skip_relocation, mojave: "d1fb18fe9ce62b99d09dc7853956d881360595d91842584060543bc27690f54c"
   end
 
   depends_on "ruby"
@@ -27,8 +22,8 @@ class Tmuxinator < Formula
   end
 
   resource "thor" do
-    url "https://rubygems.org/downloads/thor-1.1.0.gem"
-    sha256 "cacae12a3761be4ccbe63be19261352b108f86c721c37d87664328efeaa6d0a3"
+    url "https://rubygems.org/downloads/thor-1.2.1.gem"
+    sha256 "b1752153dc9c6b8d3fcaa665e9e1a00a3e73f28da5e238b81c404502e539d446"
   end
 
   resource "xdg" do
