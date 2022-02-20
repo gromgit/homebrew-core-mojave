7998e8ce18359069106a9ac5064c38d2868a98d5
Adrian Ho <the.gromgit@gmail.com>
2022-02-20 10:52:22 +0800

    wiggle: update 1.3 bottle.

--- Formula/wiggle.rb
+++ Formula/wiggle.rb
@@ -1,8 +1,8 @@
 class Wiggle < Formula
   desc "Program for applying patches with conflicting changes"
-  homepage "https://neil.brown.name/blog/20100324064620"
-  url "https://neil.brown.name/wiggle/wiggle-1.2.tar.gz"
-  sha256 "31375badb76a4a586f2113e49d13486dbc64844962ae80976a81c6542e901622"
+  homepage "https://github.com/neilbrown/wiggle"
+  url "https://github.com/neilbrown/wiggle/archive/refs/tags/v1.3.tar.gz"
+  sha256 "ff92cf0133c1f4dce33563e263cb30e7ddb6f4abdf86d427b1ec1490bec25afa"
   license "GPL-2.0"
 
   livecheck do
@@ -11,13 +11,8 @@ class Wiggle < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "840be63d300983f3cc1b04b5d2684ba86e7942bd3a9c46cb832dfb67906f4de2"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c6cc3d15756ce45cdf194123d33103f098f2d1c1ea14a0e32731dd4bfaa3f1ec"
-    sha256 cellar: :any_skip_relocation, monterey:       "814c0133b6a6617b027c2584ba6395c10d316a0d3a65b959291477d1e0a31b6e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "db474825c0242b8557438ba22b48d18b326c0612ac1bc19ec40e08bd5529cf25"
-    sha256 cellar: :any_skip_relocation, catalina:       "ebf56026e29b37067218ad4ee2340de46df5b454b46848a0fe2ac117442cfce1"
-    sha256 cellar: :any_skip_relocation, mojave:         "6971993e24c3267d64110bf8ef472bb80db7268a0b85617d3846b66c5f5dbde1"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "5fe1b56648deb49456c668a2e99d3f7bbb2edf3045d8d55f78382ea008f640d1"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/wiggle"
+    sha256 cellar: :any_skip_relocation, mojave: "d0ade4b19e692423d9048869388f395d23cf90efada72e1c7f044925cb26f103"
   end
 
   def install
