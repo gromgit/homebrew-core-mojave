b13d8ddb650a290fe68265df347ee1bfef4e1c43
Adrian Ho <the.gromgit@gmail.com>
2022-02-20 08:39:33 +0800

    util-linux: update 2.37.4 bottle.

--- Formula/util-linux.rb
+++ Formula/util-linux.rb
@@ -1,8 +1,8 @@
 class UtilLinux < Formula
   desc "Collection of Linux utilities"
   homepage "https://github.com/karelzak/util-linux"
-  url "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.37/util-linux-2.37.3.tar.xz"
-  sha256 "590c592e58cd6bf38519cb467af05ce6a1ab18040e3e3418f24bcfb2f55f9776"
+  url "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.37/util-linux-2.37.4.tar.xz"
+  sha256 "634e6916ad913366c3536b6468e7844769549b99a7b2bf80314de78ab5655b83"
   license all_of: [
     "BSD-3-Clause",
     "BSD-4-Clause-UC",
@@ -15,7 +15,7 @@ class UtilLinux < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/util-linux"
-    sha256 mojave: "2f0636cd0afce6695dfcf745ed737ad9dff3c64d59827faacd87f7b80fd18c31"
+    sha256 mojave: "f325f6de87327543b377e0a9a3bdb7fe78a7d3430675e01e126524bff099cc1b"
   end
 
   keg_only :shadowed_by_macos, "macOS provides the uuid.h header"
