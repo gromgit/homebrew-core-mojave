55cbdc6a9d9b724c5666d06b50c65aa4915f61fd
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-22 06:22:59 +0800

    testssl: no rebuilding required

--- Formula/testssl.rb
+++ Formula/testssl.rb
@@ -1,13 +1,13 @@
 class Testssl < Formula
   desc "Tool which checks for the support of TLS/SSL ciphers and flaws"
   homepage "https://testssl.sh/"
-  url "https://github.com/drwetter/testssl.sh/archive/v3.0.6.tar.gz"
-  sha256 "05768444d6cf3dc5812f8fb88695d17a82668089deddd6aaf969041ba4c10b10"
+  url "https://github.com/drwetter/testssl.sh/archive/v3.0.7.tar.gz"
+  sha256 "c2beb3ae1fc1301ad845c7aa01c0a292c41b95747ef67f34601f21fb2da16145"
   license "GPL-2.0-or-later"
   head "https://github.com/drwetter/testssl.sh.git", branch: "3.1dev"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "2fa5f3a1c687e37727afa1e2bfa1549b8d997c2f6e51b3d3077466bdd79be3f5"
+    sha256 cellar: :any_skip_relocation, all: "6f5a1162e55cf0b03f4261d2e653c01302ede13d56c413b4172774f2e0f2d7a0"
   end
 
   depends_on "openssl@1.1"
