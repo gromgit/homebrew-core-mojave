f56e8e34931f2de501423c566cc8a055e06544a6
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-22 06:22:46 +0800

    libosmium: no rebuilding required

--- Formula/libosmium.rb
+++ Formula/libosmium.rb
@@ -1,12 +1,12 @@
 class Libosmium < Formula
   desc "Fast and flexible C++ library for working with OpenStreetMap data"
   homepage "https://osmcode.org/libosmium/"
-  url "https://github.com/osmcode/libosmium/archive/v2.17.3.tar.gz"
-  sha256 "ff85d55ca83d77fc41f6b7ae77f3d8d4337d299cbb4cc60c00e1f9d90d94e71b"
+  url "https://github.com/osmcode/libosmium/archive/v2.18.0.tar.gz"
+  sha256 "c05a3e95c9c811521ebad8637e90f43ab8fb053b310875acce741cc4c17d6f59"
   license "BSL-1.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "f6d0cc8085827052b3ac9d2b8f4fd64cf5a9ec684ec352eab696b777f335c1bb"
+    sha256 cellar: :any_skip_relocation, all: "629bd00d6aa05b58f2b937560b5edff6dbc1b47e53714ab7d005d9d54eb952fe"
   end
 
   depends_on "boost" => :build
