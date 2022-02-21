935e769ff3c83978d59b3432eb316d9e002e84f4
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-22 06:23:01 +0800

    weighttp: no rebuilding required

--- Formula/weighttp.rb
+++ Formula/weighttp.rb
@@ -7,28 +7,35 @@ class Weighttp < Formula
   head "https://git.lighttpd.net/lighttpd/weighttp.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "64057edc2b2ff52e19975c6fadcc94bb456b4a37ad0a3e7f94b93b7477cdc867"
-    sha256 cellar: :any, arm64_big_sur:  "61bd26ebdcd743d1078d4bd2138f55bcd943900c85acf567ccfda9fe4fc89379"
-    sha256 cellar: :any, monterey:       "6c94e449d1376949e49017b614bd578d297f64b59738e4a0616667d6f2f8892d"
-    sha256 cellar: :any, big_sur:        "73c147309603c830719feac16847dc9ec2f09d27dc3a3f702760efe1eaaf8405"
-    sha256 cellar: :any, catalina:       "b76ee9060b8cb86897af45c620b1f1fb3d757955a2a2f8e4c55ef6a153bfc547"
-    sha256 cellar: :any, mojave:         "2ab4f5e31f9411d55c4a4653f78bb381b70f53f49d07efaf6e99b5a86281b62a"
-    sha256 cellar: :any, high_sierra:    "4225f653fe64067e3330c33202a15ad65a6b194ce23619ae045cbe50528a9b02"
-    sha256 cellar: :any, sierra:         "242f14d7a7fb477e4722a3818a98ad25ffedd5d2c80e7c97d67c80fe2a20366c"
-    sha256 cellar: :any, el_capitan:     "e96be0135f552ddde0547ca914c2bc6635dcc59ce4bdeb803ab9412100d8d15b"
-    sha256 cellar: :any, yosemite:       "e83c9f99b524b57ba31571dc673ab6d2d2a5e38a5374ce45130f11a51c063662"
+    sha256 cellar: :any,                 arm64_monterey: "64057edc2b2ff52e19975c6fadcc94bb456b4a37ad0a3e7f94b93b7477cdc867"
+    sha256 cellar: :any,                 arm64_big_sur:  "61bd26ebdcd743d1078d4bd2138f55bcd943900c85acf567ccfda9fe4fc89379"
+    sha256 cellar: :any,                 monterey:       "6c94e449d1376949e49017b614bd578d297f64b59738e4a0616667d6f2f8892d"
+    sha256 cellar: :any,                 big_sur:        "73c147309603c830719feac16847dc9ec2f09d27dc3a3f702760efe1eaaf8405"
+    sha256 cellar: :any,                 catalina:       "b76ee9060b8cb86897af45c620b1f1fb3d757955a2a2f8e4c55ef6a153bfc547"
+    sha256 cellar: :any,                 mojave:         "2ab4f5e31f9411d55c4a4653f78bb381b70f53f49d07efaf6e99b5a86281b62a"
+    sha256 cellar: :any,                 high_sierra:    "4225f653fe64067e3330c33202a15ad65a6b194ce23619ae045cbe50528a9b02"
+    sha256 cellar: :any,                 sierra:         "242f14d7a7fb477e4722a3818a98ad25ffedd5d2c80e7c97d67c80fe2a20366c"
+    sha256 cellar: :any,                 el_capitan:     "e96be0135f552ddde0547ca914c2bc6635dcc59ce4bdeb803ab9412100d8d15b"
+    sha256 cellar: :any,                 yosemite:       "e83c9f99b524b57ba31571dc673ab6d2d2a5e38a5374ce45130f11a51c063662"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c7ef5cfd2cdadf8036c30295d3c51a56399d8acee7e2dc96aa1d75d471e2c1a0"
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
   depends_on "libev"
 
   def install
-    system "./waf", "configure"
-    system "./waf", "build"
-    bin.install "build/default/weighttp"
+    system "autoupdate"
+    system "./autogen.sh"
+    system "./configure", *std_configure_args
+    system "make"
+    system "make", "install"
   end
 
   test do
     # Stick with HTTP to avoid 'error: no ssl support yet'
-    system "#{bin}/weighttp", "-n", "1", "http://redmine.lighttpd.net/projects/weighttp/wiki"
+    system bin/"weighttp", "-n", "1", "http://redmine.lighttpd.net/projects/weighttp/wiki"
   end
 end
