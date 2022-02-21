e72353b0be7984cf1b4fb3ff32a6cc14adc51c67
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-22 06:22:47 +0800

    linux-headers@5.16:

--- Formula/linux-headers@5.16.rb
+++ Formula/linux-headers@5.16.rb
@@ -1,12 +1,12 @@
 class LinuxHeadersAT516 < Formula
   desc "Header files of the Linux kernel"
   homepage "https://kernel.org/"
-  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.16.9.tar.gz"
-  sha256 "ca84521580c9e2a5d7648a6b98150780b0e65bc8fac85c3a07d00b1b9aa68ad3"
+  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.16.10.tar.gz"
+  sha256 "1c71b2eb775bf4d2aae32907fa7ca42770c58e114c0990d7398e9db5a636bcac"
   license "GPL-2.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "71cb27ac2bb2f30bf7eba3d3da5500d6b69d389b23d755902ef22b6afc1570b2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "06c20f8c85abb46ed0869470e3f73185b4cdd47ca196e01faa4b591da12d5869"
   end
 
   keg_only :versioned_formula
