8047a6ec210e9c006e0d6e61230d62066e9687b1
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-22 06:22:53 +0800

    proguard: no rebuilding required

--- Formula/proguard.rb
+++ Formula/proguard.rb
@@ -1,9 +1,9 @@
 class Proguard < Formula
   desc "Java class file shrinker, optimizer, and obfuscator"
   homepage "https://www.guardsquare.com/en/products/proguard"
-  url "https://github.com/Guardsquare/proguard/releases/download/v7.2.0/proguard-7.2.0.tar.gz"
-  sha256 "829a3420e52f49d5d2b751b5d9ab6f543ff503a87ffd835014222b40250c0a6d"
-  license "GPL-2.0"
+  url "https://github.com/Guardsquare/proguard/releases/download/v7.2.1/proguard-7.2.1.tar.gz"
+  sha256 "c3a7062d42d2388445c4b699a131cae3f1c3c7b91834db0fbd4d964bc6376e4b"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url :stable
@@ -11,7 +11,7 @@ class Proguard < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "5f84dbd557fd81eb5e9d859a60d7eb6e699ee38b9e24217ad0d3bda926a49cb8"
+    sha256 cellar: :any_skip_relocation, all: "2eb9fced27889c301586e19b0d9282ff35935ca15467b275161f4a3f57910ca9"
   end
 
   depends_on "openjdk"
