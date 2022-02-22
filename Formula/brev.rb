cbfc052da655c37bb5f75c8d7ce5be00e25dee3a
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-22 20:30:09 +0800

    brev: update 0.6.19 bottle.

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -1,13 +1,13 @@
 class Brev < Formula
   desc "CLI tool for managing workspaces provided by brev.dev"
   homepage "https://docs.brev.dev"
-  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.17.tar.gz"
-  sha256 "cf69be0558cd87ca5838372aeca6b85fe937f1bd8eb44c03a88355ca088aa9ea"
+  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.19.tar.gz"
+  sha256 "ff6fd0afa4debad5cac190519a5cef250246df8cddfb84dc2d5bc40c2624d36d"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/brev"
-    sha256 cellar: :any_skip_relocation, mojave: "5e5f78fc089b3aa30a644b9bc1ccd31b2feff6fbe61b7d47f93b88132299b12f"
+    sha256 cellar: :any_skip_relocation, mojave: "33b68cf244648d614d239e57362fe195ee0d9457a2d3ac888c99a60a7c7e3aa3"
   end
 
   depends_on "go" => :build
