7fca5a53a87dbdd8e62bbde84c3ce1e1ef058383
Adrian Ho <the.gromgit@gmail.com>
2022-02-23 22:25:59 +0800

    lima: update 0.8.3 bottle.

--- Formula/lima.rb
+++ Formula/lima.rb
@@ -1,13 +1,13 @@
 class Lima < Formula
   desc "Linux virtual machines"
   homepage "https://github.com/lima-vm/lima"
-  url "https://github.com/lima-vm/lima/archive/v0.8.2.tar.gz"
-  sha256 "03bab7f17078e1d57c0122eb68acf3a19bf8f6f37397a2730b89d87bd9bdd4dd"
+  url "https://github.com/lima-vm/lima/archive/v0.8.3.tar.gz"
+  sha256 "8c23daa9ff3835bc47b187c47202ff95efd6a767f58789c11b2674b4ae0da403"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lima"
-    sha256 cellar: :any_skip_relocation, mojave: "1e2c65aceb200676c531885177a5424170e6b74a2e26b04109b2da7f592866d2"
+    sha256 cellar: :any_skip_relocation, mojave: "3109002e528e3698c234b4a24bb8cd164703b243be7eb3e664d308ce96efa743"
   end
 
   depends_on "go" => :build
