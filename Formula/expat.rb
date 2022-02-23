7e619b7969944c995f30bc6492a77ff45195b4b0
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 02:13:30 +0800

    expat: update 2.4.5 bottle.

--- Formula/expat.rb
+++ Formula/expat.rb
@@ -1,8 +1,8 @@
 class Expat < Formula
   desc "XML 1.0 parser"
   homepage "https://libexpat.github.io/"
-  url "https://github.com/libexpat/libexpat/releases/download/R_2_4_4/expat-2.4.4.tar.xz"
-  sha256 "b5d25d6e373351c2ed19b562b4732d01d2589ac8c8e9e7962d8df1207cc311b8"
+  url "https://github.com/libexpat/libexpat/releases/download/R_2_4_5/expat-2.4.5.tar.xz"
+  sha256 "f2af8fc7cdc63a87920da38cd6d12cb113c3c3a3f437495b1b6541e0cff32579"
   license "MIT"
 
   livecheck do
@@ -13,7 +13,7 @@ class Expat < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/expat"
-    sha256 cellar: :any, mojave: "799750e5e4c48b2ad59d245dd7d58f0b74e2dc420794f713eccdff899873bfcf"
+    sha256 cellar: :any, mojave: "e9bce9064f0b9f88b42e5138524bd3637c7488f9553d615f22a489aec2957bb9"
   end
 
   head do
