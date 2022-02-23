10ce464f239cbee45f96ab9be8d54567263ac075
Adrian Ho <the.gromgit@gmail.com>
2022-02-23 22:53:46 +0800

    cpu_features: update 0.6.0_1 bottle.

--- Formula/cpu_features.rb
+++ Formula/cpu_features.rb
@@ -4,21 +4,25 @@ class CpuFeatures < Formula
   url "https://github.com/google/cpu_features/archive/v0.6.0.tar.gz"
   sha256 "95a1cf6f24948031df114798a97eea2a71143bd38a4d07d9a758dda3924c1932"
   license "Apache-2.0"
+  revision 1
+  head "https://github.com/google/cpu_features.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:     "e4cc8363ff01721edffa3c1c48fdefc2f50973468f130feb2c8a8635da723b7c"
-    sha256 cellar: :any_skip_relocation, big_sur:      "f6bebf333094fed54f5a96c9dc96280f7a2ca6c7b075cbed9c77a9214fafd8c4"
-    sha256 cellar: :any_skip_relocation, catalina:     "ba67bb2d2166f43b17aba3fb4f8306b577e17779e8a8facea32a16451c7b369d"
-    sha256 cellar: :any_skip_relocation, mojave:       "9f7d3b134c25934208808a47a8c8ecde61d8a7c3d429246ce807d9183930bd66"
-    sha256 cellar: :any_skip_relocation, high_sierra:  "057d70560cecfd8863543a562ddb0ec64147ac3ce6292adedf0bc28c74a92349"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "eba6a3614e0c50480292250b36dfce454e572696b73eb6a6be476fb637688101"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cpu_features"
+    sha256 cellar: :any, mojave: "b43ba819b86698bd66282c6d5613178214fae27858bd2d15ab0f6154c826a73a"
   end
 
   depends_on "cmake" => :build
 
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DBUILD_SHARED_LIBS=ON"
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    # Install static lib too
+    system "cmake", "-S", ".", "-B", "build/static", *std_cmake_args
+    system "cmake", "--build", "build/static"
+    lib.install "build/static/libcpu_features.a"
   end
 
   test do
