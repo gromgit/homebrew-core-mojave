667e799b0d92ce152f02cdd2004324a5d4c2a453
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 06:53:46 +0800

    kubescape: update 2.0.148 bottle.

--- Formula/kubescape.rb
+++ Formula/kubescape.rb
@@ -1,14 +1,14 @@
 class Kubescape < Formula
   desc "Kubernetes testing according to Hardening Guidance by NSA and CISA"
   homepage "https://github.com/armosec/kubescape"
-  url "https://github.com/armosec/kubescape/archive/v2.0.147.tar.gz"
-  sha256 "d886e868cacc0554f7e80ce3885487756450d3f1d4079cd0377bda709772a3c4"
+  url "https://github.com/armosec/kubescape/archive/v2.0.148.tar.gz"
+  sha256 "33dc881c329739d0fbc45fb41ef4c5ac582dd02fc72147fd287cc3287b10c36e"
   license "Apache-2.0"
   head "https://github.com/armosec/kubescape.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kubescape"
-    sha256 cellar: :any_skip_relocation, mojave: "e4de6ac106fcd1413348bc6efb9539b2cecefc70b1587031eacefe26a8fe18bb"
+    sha256 cellar: :any_skip_relocation, mojave: "40dd3ada07c5f9cd1701becd9e3343192fd206294bc9c1ebc9bf7f7b64232e00"
   end
 
   depends_on "go" => :build
