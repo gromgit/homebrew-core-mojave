5ff96030ce3330a1aeb63c1da278259c2377464b
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 07:47:02 +0800

    libvirt: update 8.0.0 bottle.

--- Formula/libvirt.rb
+++ Formula/libvirt.rb
@@ -1,10 +1,9 @@
 class Libvirt < Formula
   desc "C virtualization API"
   homepage "https://libvirt.org/"
-  url "https://libvirt.org/sources/libvirt-7.10.0.tar.xz"
-  sha256 "cb318014af097327928c6e3d72922e3be02a3e6401247b2aa52d9ab8e0b480f9"
+  url "https://libvirt.org/sources/libvirt-8.0.0.tar.xz"
+  sha256 "51e6e8ff04bafe96d7e314b213dcd41fb1163d9b4f0f75cdab01e663728f4cf6"
   license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
-  revision 1
   head "https://gitlab.com/libvirt/libvirt.git", branch: "master"
 
   livecheck do
@@ -14,7 +13,7 @@ class Libvirt < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libvirt"
-    sha256 mojave: "b2163b6efd00e892f299ec51a26a66aaa7aafa33e4dc11328cd2aa28ab79964b"
+    sha256 mojave: "2912aed7737e1692c3285eb9193b692312ac84404e6dd702f7aaf088b4c9e563"
   end
 
   depends_on "docutils" => :build
@@ -42,6 +41,7 @@ class Libvirt < Formula
 
   on_linux do
     depends_on "libtirpc"
+    depends_on "linux-headers@5.16"
   end
 
   # Don't generate accelerator command line on macOS
@@ -61,6 +61,13 @@ class Libvirt < Formula
   patch do
     url "https://gitlab.com/abologna/libvirt/-/commit/da138afc3609a92d473fddcffa54b2020759f986.diff"
     sha256 "4eb3c9f0ca140a4d8eb5002acde0b6f1234011f82df7d8cc85252be35e8a5cff"
+  end
+
+  # Fix PermissionError: [Errno 1] Operation not permitted: '/usr/local/Cellar/yajl/2.1.0/include/libvirt'
+  # Remove with next release
+  patch do
+    url "https://gitlab.com/libvirt/libvirt/-/commit/9f2d3cb472fd4d86dc4de5d57fcf8acb14e33e00.diff"
+    sha256 "ee14a4922ddc405c6ff6c5f7e9183b83f50cfa448b2ab9e1428f3f1c47e0d34c"
   end
 
   def install
