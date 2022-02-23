0a47ed3f333b8402a9bb5d4be2036f30aa6adad2
Adrian Ho <the.gromgit@gmail.com>
2022-02-23 17:34:09 +0800

    aerc: update 0.8.1 bottle.

--- Formula/aerc.rb
+++ Formula/aerc.rb
@@ -1,22 +1,21 @@
 class Aerc < Formula
   desc "Email client that runs in your terminal"
   homepage "https://aerc-mail.org/"
-  url "https://git.sr.ht/~rjarry/aerc/archive/0.7.1.tar.gz"
-  sha256 "e149236623c103c8526b1f872b4e630e67f15be98ac604c0ea0186054dbef0cc"
+  url "https://git.sr.ht/~rjarry/aerc/archive/0.8.1.tar.gz"
+  sha256 "5bea713b211dcd85599c697a0f6563e07e2c04fd1443416545eea0a9a83a4f8d"
   license "MIT"
-  revision 1
   head "https://git.sr.ht/~rjarry/aerc", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/aerc"
-    sha256 mojave: "24ed5208b6716645289a1bc7c3f02d037639677c655a65e0155191a876bf8328"
+    sha256 cellar: :any_skip_relocation, mojave: "b734b8f43004766601c23effbc111e38770031fc7173c2bba4d86f06c8b69472"
   end
 
   depends_on "go" => :build
   depends_on "scdoc" => :build
 
   def install
-    system "make", "SHAREDIR=#{opt_pkgshare}", "PREFIX=#{prefix}"
+    system "make", "PREFIX=#{prefix}"
     system "make", "install", "PREFIX=#{prefix}"
   end
 
