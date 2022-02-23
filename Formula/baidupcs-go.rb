f4974e2dc039e4a5b1f56e7dea19b6552a2ad874
Adrian Ho <the.gromgit@gmail.com>
2022-02-23 20:10:33 +0800

    baidupcs-go: update 3.8.7 bottle.

--- Formula/baidupcs-go.rb
+++ Formula/baidupcs-go.rb
@@ -1,14 +1,14 @@
 class BaidupcsGo < Formula
   desc "Terminal utility for Baidu Network Disk"
   homepage "https://github.com/qjfoidnh/BaiduPCS-Go"
-  url "https://github.com/qjfoidnh/BaiduPCS-Go/archive/v3.8.6.tar.gz"
-  sha256 "9e263f8ba81870536e9c7453ab06ef9524696a0ddbcf5ea3f56132cd448c7e27"
+  url "https://github.com/qjfoidnh/BaiduPCS-Go/archive/v3.8.7.tar.gz"
+  sha256 "e365fabee470ea5ab51b9ba034b5168dca1d0d537ab36274a17bfc460036b965"
   license "Apache-2.0"
   head "https://github.com/qjfoidnh/BaiduPCS-Go.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/baidupcs-go"
-    sha256 cellar: :any_skip_relocation, mojave: "dab404b7ae8c6ea14696558a9a650f61dc7a21687d3c36a8dbb43c9bc8b837d0"
+    sha256 cellar: :any_skip_relocation, mojave: "3800c02b62fa9d220ee75f68d043575674e9918a21f4929e0256ed332fa1d260"
   end
 
   depends_on "go" => :build
