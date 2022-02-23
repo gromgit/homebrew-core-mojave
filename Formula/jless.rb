4aa9b9d006d9bbb830ff2cd064d4b0a83aacbad5
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 06:27:56 +0800

    jless: update 0.7.2 bottle.

--- Formula/jless.rb
+++ Formula/jless.rb
@@ -1,13 +1,13 @@
 class Jless < Formula
   desc "Command-line pager for JSON data"
   homepage "https://pauljuliusmartinez.github.io/"
-  url "https://github.com/PaulJuliusMartinez/jless/archive/refs/tags/v0.7.1.tar.gz"
-  sha256 "8b36d117ac0ef52fa0261e06b88b5ae77c2ff4beeb54f2c906a57f066dc46179"
+  url "https://github.com/PaulJuliusMartinez/jless/archive/refs/tags/v0.7.2.tar.gz"
+  sha256 "5d776cb6488743ccdaeeffb4bfc54d84862028170cee852a8bb5c156526ed263"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jless"
-    sha256 cellar: :any_skip_relocation, mojave: "7c2837eaced017cb96a1be19b4b429256186a16b2b8e83fc4305124052376829"
+    sha256 cellar: :any_skip_relocation, mojave: "b68c0c6ade5e7d66b1747317629109a559ac5f4809d34c3585ae33a6c37c6283"
   end
 
   depends_on "rust" => :build
