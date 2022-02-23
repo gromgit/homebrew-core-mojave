8929ff01f4d5b1de718f575b43260631e11b3987
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 07:03:06 +0800

    ld-find-code-refs: update 2.5.4 bottle.

--- Formula/ld-find-code-refs.rb
+++ Formula/ld-find-code-refs.rb
@@ -1,14 +1,14 @@
 class LdFindCodeRefs < Formula
   desc "Build tool for sending feature flag code references to LaunchDarkly"
   homepage "https://github.com/launchdarkly/ld-find-code-refs"
-  url "https://github.com/launchdarkly/ld-find-code-refs/archive/2.5.0.tar.gz"
-  sha256 "a58917277d7fa15367ffd3e74326b13c1371858ab1df9af4c61883bd8e5b0ec2"
+  url "https://github.com/launchdarkly/ld-find-code-refs/archive/2.5.4.tar.gz"
+  sha256 "64865e18fbe2e447ed83d920b3ca8dedbb09acb6e8afcbe4e4921f6ea2c234d2"
   license "Apache-2.0"
   head "https://github.com/launchdarkly/ld-find-code-refs.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ld-find-code-refs"
-    sha256 cellar: :any_skip_relocation, mojave: "f0c3574fff2022a92f9563db368c5ff6151d74418c0c3aee1e556d7f77771d0a"
+    sha256 cellar: :any_skip_relocation, mojave: "873a1776eed56ed24cce846cc7baa949f964018b8cbfa35addd9f8d2c2501f09"
   end
 
   depends_on "go" => :build
