99124a5484dabac2661c76fd2dff98afb413f789
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 05:16:03 +0800

    haproxy: update 2.5.3 bottle.

--- Formula/haproxy.rb
+++ Formula/haproxy.rb
@@ -1,8 +1,8 @@
 class Haproxy < Formula
   desc "Reliable, high performance TCP/HTTP load balancer"
   homepage "https://www.haproxy.org/"
-  url "https://www.haproxy.org/download/2.5/src/haproxy-2.5.2.tar.gz"
-  sha256 "2de3424fd7452be1c1c13d5e0994061285055c57046b1cb3c220d67611d0da7e"
+  url "https://www.haproxy.org/download/2.5/src/haproxy-2.5.3.tar.gz"
+  sha256 "d6fa3c66f707ff93b5bd27ce69e70a8964d7b7078548b51868d47d7df3943fe4"
   license "GPL-2.0-or-later" => { with: "openvpn-openssl-exception" }
 
   livecheck do
@@ -12,7 +12,7 @@ class Haproxy < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/haproxy"
-    sha256 cellar: :any, mojave: "ad30d57ff26df81cb5ce88a8a68d613e33f526d2fdc0889a458ea6ad1fcc8391"
+    sha256 cellar: :any, mojave: "4e1555efbd115af1d7a214ade249631ec53ae6c6718659152e4cd3ed69976611"
   end
 
   depends_on "openssl@1.1"
