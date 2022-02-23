e50f136212ab5acb53fd882fb7514b7f216c6180
Adrian Ho <the.gromgit@gmail.com>
2022-02-23 23:35:01 +0800

    duo_unix: update 1.12.0 bottle.

--- Formula/duo_unix.rb
+++ Formula/duo_unix.rb
@@ -1,23 +1,17 @@
 class DuoUnix < Formula
   desc "Two-factor authentication for SSH"
   homepage "https://www.duosecurity.com/docs/duounix"
-  url "https://github.com/duosecurity/duo_unix/archive/duo_unix-1.11.4.tar.gz"
-  sha256 "2fcba3e50fd477699d013c789ffc73a0b10c204d25c455abe7c81a2ecd886579"
-  license "GPL-2.0"
+  url "https://github.com/duosecurity/duo_unix/archive/duo_unix-1.12.0.tar.gz"
+  sha256 "a4479f893e036f38a5809d71ce47f69118f6ef61822cc1c66afccf143c5d71f8"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url "https://github.com/duosecurity/duo_unix.git"
   end
 
   bottle do
-    sha256 arm64_monterey: "095e85a126b7a377ecdb654572f8d1d1bdcdfd37e82be4d0f967c0a276255ee4"
-    sha256 arm64_big_sur:  "e323d478329b8b50be30253b7fd8b3f0387d5fd06a1047c7d18d6911860a095e"
-    sha256 monterey:       "4f047f8edb256953afa519254fffb821a5e62a6ba8e5ea57198aba25d807411d"
-    sha256 big_sur:        "dece3829732fe41b3346b5de6f04b921ab22641c39eb45b87e91a0795a384e67"
-    sha256 catalina:       "67a6ff6605578e287fc862f6fb2ee9360dea7c22f8fbaa97a1bf18dd13db5ec0"
-    sha256 mojave:         "753e3f0d60c4c0404a6e347d052bc1ed4bd1e66b43170675549a36d4fe736f25"
-    sha256 high_sierra:    "8e707bc378fa13dfd92fb39e0de055c58f2470973fe80edc41bf500e4863128a"
-    sha256 x86_64_linux:   "941aa90695f3bfbffe6aebcc7c73c8325a5de5355840b451604278172bd3fba2"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/duo_unix"
+    sha256 mojave: "42535c17f6cf8178e35ccddfa55742ffef13c3da2cc2b77f089e0c8593f9235e"
   end
 
   depends_on "autoconf" => :build
