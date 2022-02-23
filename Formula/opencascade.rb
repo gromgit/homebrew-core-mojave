702bc906a502ea072f316b9b4b17878b5d60c2a9
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 03:16:41 +0800

    opencascade: update 7.6.1 bottle.

--- Formula/opencascade.rb
+++ Formula/opencascade.rb
@@ -1,14 +1,14 @@
 class Opencascade < Formula
   desc "3D modeling and numerical simulation software for CAD/CAM/CAE"
   homepage "https://dev.opencascade.org/"
-  url "https://git.dev.opencascade.org/gitweb/?p=occt.git;a=snapshot;h=refs/tags/V7_6_0;sf=tgz"
-  version "7.6.0"
-  sha256 "e7f989d52348c3b3acb7eb4ee001bb5c2eed5250cdcceaa6ae97edc294f2cabd"
+  url "https://git.dev.opencascade.org/gitweb/?p=occt.git;a=snapshot;h=refs/tags/V7_6_1;sf=tgz"
+  version "7.6.1"
+  sha256 "c111c635fa4cae05821640f5afbbf362efaee8dc51fcbee953866eec7482cd6a"
   license "LGPL-2.1-only"
 
   # The first-party download page (https://dev.opencascade.org/release)
   # references version 7.5.0 and hasn't been updated for later maintenance
-  # releases (e.g., 7.6.0, 7.5.2), so we check the Git tags instead. Release
+  # releases (e.g., 7.6.1, 7.5.2), so we check the Git tags instead. Release
   # information is posted at https://dev.opencascade.org/forums/occt-releases
   # but the text varies enough that we can't reliably match versions from it.
   livecheck do
@@ -21,8 +21,7 @@ class Opencascade < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/opencascade"
-    rebuild 3
-    sha256 mojave: "4fbc416c5445938a22e9207257413eb6e530ce2edb9612a9c44831a9672f6b50"
+    sha256 cellar: :any, mojave: "4b3503ab405f57a5651fa2028159e8991b2ed79a00a4109386ad84c1b5e2eb86"
   end
 
   depends_on "cmake" => :build
@@ -31,11 +30,18 @@ class Opencascade < Formula
   depends_on "fontconfig"
   depends_on "freeimage"
   depends_on "freetype"
-  depends_on "tbb@2020"
+  depends_on "tbb"
   depends_on "tcl-tk"
 
   on_linux do
     depends_on "mesa" # For OpenGL
+  end
+
+  # Fix compilation errors with oneTBB 2021
+  # Issue ref: https://tracker.dev.opencascade.org/view.php?id=0032697
+  patch do
+    url "https://git.dev.opencascade.org/gitweb/?p=occt.git;a=patch;h=740833a6a88e481f474783c426b6f6311ed586d3"
+    sha256 "04932bf0674906dbc8f9c4ff0702aad3147c5db9abd0262973e18a1e4cd73976"
   end
 
   def install
@@ -49,7 +55,7 @@ class Opencascade < Formula
                     "-D3RDPARTY_FREETYPE_DIR=#{Formula["freetype"].opt_prefix}",
                     "-D3RDPARTY_RAPIDJSON_DIR=#{Formula["rapidjson"].opt_prefix}",
                     "-D3RDPARTY_RAPIDJSON_INCLUDE_DIR=#{Formula["rapidjson"].opt_include}",
-                    "-D3RDPARTY_TBB_DIR=#{Formula["tbb@2020"].opt_prefix}",
+                    "-D3RDPARTY_TBB_DIR=#{Formula["tbb"].opt_prefix}",
                     "-D3RDPARTY_TCL_DIR:PATH=#{tcltk.opt_prefix}",
                     "-D3RDPARTY_TK_DIR:PATH=#{tcltk.opt_prefix}",
                     "-D3RDPARTY_TCL_INCLUDE_DIR:PATH=#{tcltk.opt_include}",
