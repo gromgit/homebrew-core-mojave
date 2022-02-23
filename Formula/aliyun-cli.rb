b1959c94c3da07c1da11e9ba2515573eb36fa483
Adrian Ho <the.gromgit@gmail.com>
2022-02-23 17:39:59 +0800

    aliyun-cli: update 3.0.109 bottle.

--- Formula/aliyun-cli.rb
+++ Formula/aliyun-cli.rb
@@ -2,13 +2,13 @@ class AliyunCli < Formula
   desc "Universal Command-Line Interface for Alibaba Cloud"
   homepage "https://github.com/aliyun/aliyun-cli"
   url "https://github.com/aliyun/aliyun-cli.git",
-      tag:      "v3.0.108",
-      revision: "1c81252d4a7d6ab668ba5ce18a002df7f931da10"
+      tag:      "v3.0.109",
+      revision: "d1e245b1e8a6f8d563cd3f34ab276f98680fa991"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/aliyun-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "e433eccff52fb878159e426f963fcff52f4f2c9c68fbe40e811c73645c5a7e62"
+    sha256 cellar: :any_skip_relocation, mojave: "ccef26baf636d9ff2b181a6507d0349d2717be432044736a77fbad24adf90528"
   end
 
   depends_on "go" => :build
