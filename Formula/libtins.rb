b62e0e4e3f42fce29d1dcaf3cb63aa06b4f125fd
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 07:33:50 +0800

    libtins: update 4.4 bottle.

--- Formula/libtins.rb
+++ Formula/libtins.rb
@@ -1,20 +1,14 @@
 class Libtins < Formula
   desc "C++ network packet sniffing and crafting library"
   homepage "https://libtins.github.io/"
-  url "https://github.com/mfontanini/libtins/archive/v4.3.tar.gz"
-  sha256 "c70bce5a41a27258bf0e3ad535d8238fb747d909a4b87ea14620f25dd65828fd"
+  url "https://github.com/mfontanini/libtins/archive/v4.4.tar.gz"
+  sha256 "ff0121b4ec070407e29720c801b7e1a972042300d37560a62c57abadc9635634"
   license "BSD-2-Clause"
-  head "https://github.com/mfontanini/libtins.git"
+  head "https://github.com/mfontanini/libtins.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "792e4fd9072bc8300a8005210760a0c0678e4d5afbc82ef0bedffbfb37ff4de3"
-    sha256 cellar: :any,                 arm64_big_sur:  "67015c4202420ae239e94a7c1df5be67af65f5a717a965f581202557df639e04"
-    sha256 cellar: :any,                 monterey:       "6bc15a550e9824e9e118c8b316cf29f752f7d1b4313f6a179a68988a2682f5c9"
-    sha256 cellar: :any,                 big_sur:        "fbde141533f922dd195e69f432fef3d8fc3fa3234de841ae832e1513427ca528"
-    sha256 cellar: :any,                 catalina:       "698edf1fd2794c4bf81e1debcddadf1fcad906f98cde53c7240705578ec3a584"
-    sha256 cellar: :any,                 mojave:         "0cc57b006a581a0da50ef3b365f1cbd292e9ae054a552751cc7af3d93860ebce"
-    sha256 cellar: :any,                 high_sierra:    "0a15741675e5c3f65f98fd89a25f0a1167294b95ba596620b63a45ad71dedea8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5d479472d9c8f7e3c5b0492e99b0682829bd6033c34e7817c571413760f7dce8"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libtins"
+    sha256 cellar: :any, mojave: "a791822b0b5ed3021a8e330cc1bef25fecf673ae93757b27fe044e24dd6cb78e"
   end
 
   depends_on "cmake" => :build
