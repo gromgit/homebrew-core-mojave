b2c657cd6f11b021b4480337a31e7b2a590108ab
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 03:39:20 +0800

    fselect: update 0.8.0 bottle.

--- Formula/fselect.rb
+++ Formula/fselect.rb
@@ -1,13 +1,13 @@
 class Fselect < Formula
   desc "Find files with SQL-like queries"
   homepage "https://github.com/jhspetersson/fselect"
-  url "https://github.com/jhspetersson/fselect/archive/0.7.9.tar.gz"
-  sha256 "b1cb4108d1d35c8e2d2630cdb78a42e1e10ff36ea00ce2e76577e1723905d4a2"
+  url "https://github.com/jhspetersson/fselect/archive/0.8.0.tar.gz"
+  sha256 "33dfcbbf7e598bce479b1fb5c17429af1bb309beab2e4bc95642e9f4b5c2ffbd"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fselect"
-    sha256 cellar: :any_skip_relocation, mojave: "1ab70e5ae94d3dcc7f621f1bef0df7e5c7db2238f2b99148199c18d0ce2b32a9"
+    sha256 cellar: :any_skip_relocation, mojave: "ccdfbf4813cfc8dab7d1c70cf616c7ccbc5c19638f9d1f3d4b3f53390456de0b"
   end
 
   depends_on "rust" => :build
