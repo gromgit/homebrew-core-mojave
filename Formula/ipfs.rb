26ee86e7d7c724f26c292cc4212dacb0d1473dab
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 06:21:22 +0800

    ipfs: update 0.12.0 bottle.

--- Formula/ipfs.rb
+++ Formula/ipfs.rb
@@ -2,13 +2,13 @@ class Ipfs < Formula
   desc "Peer-to-peer hypermedia protocol"
   homepage "https://ipfs.io/"
   url "https://github.com/ipfs/go-ipfs.git",
-      tag:      "v0.11.0",
-      revision: "67220edaaef4a938fe5fba85d793bfee59db3256"
+      tag:      "v0.12.0",
+      revision: "06191dfef3309a34f01a273824716365149e47a2"
   license all_of: [
     "MIT",
     any_of: ["MIT", "Apache-2.0"],
   ]
-  head "https://github.com/ipfs/go-ipfs.git"
+  head "https://github.com/ipfs/go-ipfs.git", branch: "master"
 
   livecheck do
     url :stable
@@ -17,7 +17,7 @@ class Ipfs < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ipfs"
-    sha256 cellar: :any_skip_relocation, mojave: "f230463eadd4005cac2fa3573d26d3ae6df685e2faffaf0e4811e6f9d8b00684"
+    sha256 cellar: :any_skip_relocation, mojave: "9762386acdf92149b403cd684dcc40a2811fcd1d538e33f61f4e4103fd28ec06"
   end
 
   depends_on "go" => :build
