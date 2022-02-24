4ef6cee5c804cb05056080358898d0dd9dbf3c29
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 09:33:11 +0800

    netlify-cli: update 9.6.5 bottle.

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -3,14 +3,14 @@ require "language/node"
 class NetlifyCli < Formula
   desc "Netlify command-line tool"
   homepage "https://www.netlify.com/docs/cli"
-  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-9.6.0.tgz"
-  sha256 "46c57603f9f0884f304cf76827e8b656c983e35fcfc308281c996c06b4ea828c"
+  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-9.6.5.tgz"
+  sha256 "88ca279ce6c49278fde81706fd31d50708243e210e3e9acccfdba30a3a08fbc2"
   license "MIT"
   head "https://github.com/netlify/cli.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/netlify-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "60605b67e72429c097c051170e01b849c1586f7ef76f2cb28ce9630a316cca48"
+    sha256 cellar: :any_skip_relocation, mojave: "ed2a98368b43971ada2183052c9f08203bcacefe9d071c3245ee090c1c279d03"
   end
 
   depends_on "node"
