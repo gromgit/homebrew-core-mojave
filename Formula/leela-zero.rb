9965572116302a120c2581712469fe2f221f3300
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 19:13:59 +0800

    leela-zero: update 0.17_1 bottle.

--- Formula/leela-zero.rb
+++ Formula/leela-zero.rb
@@ -6,16 +6,11 @@ class LeelaZero < Formula
       tag:      "v0.17",
       revision: "3f297889563bcbec671982c655996ccff63fa253"
   license "GPL-3.0"
+  revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "52fac10fbdf2cefc65a465088565a5d55accb0abfc134755f803afe18b94673c"
-    sha256 cellar: :any, arm64_big_sur:  "634e2390b96220baa749c3dc08aefc7e668e164e5dde76a373ae59d6409ebcb7"
-    sha256 cellar: :any, monterey:       "fb80249de6bbb45e7a2f5a9b0660117cf225878c1fdfc2fb0985a8d5811f204c"
-    sha256 cellar: :any, big_sur:        "52c68240dc4e64a1e36879d2e57a704f3c1848444ab0d030c9dfefac37428876"
-    sha256 cellar: :any, catalina:       "b9764e2ebdade7c55ffb44f29f3c546be8003348ecca7b6ea0e93969cdce9032"
-    sha256 cellar: :any, mojave:         "6e95d7ef2f671bc404fcafd426b47cf3c4c9a9f2ab577772c03a1cf721a20444"
-    sha256 cellar: :any, high_sierra:    "ddb11b34f4a1e210e52ad13c4e789b2f0958278fe3cadfc94dc22afcce59bfa4"
-    sha256 cellar: :any, sierra:         "decf1639a96bb4fd9a198f74f7c20413cde1109d913769f0a32cc2a6c9527778"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/leela-zero"
+    sha256 cellar: :any, mojave: "ac8b02c62c863577b0abd2774a39b06a40fa0126f6d76334d86aafb61066da03"
   end
 
   depends_on "cmake" => :build
