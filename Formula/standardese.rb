0b1165868558deb606c5114d0fff32b9e17cce19
Adrian Ho <the.gromgit@gmail.com>
2022-02-25 05:32:44 +0800

    standardese: update 0.5.2_3 bottle.

--- Formula/standardese.rb
+++ Formula/standardese.rb
@@ -6,22 +6,20 @@ class Standardese < Formula
       tag:      "0.5.2",
       revision: "0b23537e235690e01ba7f8362a22d45125e7b675"
   license "MIT"
-  revision 2
+  revision 3
   head "https://github.com/standardese/standardese.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "663a3efb5ce8cc1a65fb8c02367c645f50af660c1ac9ae55f3927bd800df2d24"
-    sha256 arm64_big_sur:  "521548ec3aeb1793584f40be44305171edb186cd5574803f647cc3cb8c52798c"
-    sha256 monterey:       "ddf33e6b5ce26f15659b2e6000f421a003d30cf38479190e3a930d76aa599b7e"
-    sha256 big_sur:        "8a78f106d9698053ba0893682ee1015571a8b045dd0bd67221a85e447096891b"
-    sha256 catalina:       "9bdb9f9b5ad83fd207747ca32fdefc6919decabfd72ca43064b8b4c2d0d0f73b"
-    sha256 mojave:         "5dcbae4f7f53a6cf2a654c04a0cdd407c4af89a0aada89fe69791f150d04c7f5"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/standardese"
+    sha256 mojave: "598305b54ef14b666e2eeb35a7f7cb373453e6b5f6366d57a65a0806dbabf9e3"
   end
 
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "cmark-gfm"
   depends_on "llvm" # must be Homebrew LLVM, not system, because of `llvm-config`
+
+  fails_with gcc: "5" # LLVM is built with Homebrew GCC
 
   def install
     # Don't build shared libraries to avoid having to manually install and relocate
