c51e67c14b630baca81d8d128318a53d23a3ddd3
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 18:51:46 +0800

    gtkmm4: update 4.6.0 bottle.

--- Formula/gtkmm4.rb
+++ Formula/gtkmm4.rb
@@ -1,8 +1,8 @@
 class Gtkmm4 < Formula
   desc "C++ interfaces for GTK+ and GNOME"
   homepage "https://www.gtkmm.org/"
-  url "https://download.gnome.org/sources/gtkmm/4.2/gtkmm-4.2.0.tar.xz"
-  sha256 "480c4c38f2e7ffcf58f56bb4b4d612f3f0cac9fd5908fd2cd8249fe10592a98b"
+  url "https://download.gnome.org/sources/gtkmm/4.6/gtkmm-4.6.0.tar.xz"
+  sha256 "1353a09093cb571ef5ac05d93d200baf132ad604b5f4940777656e1505814c1f"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -11,12 +11,8 @@ class Gtkmm4 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "8678fd72c6111120f7b5ea4bdf0a55940a34aa52aec23e724b7d0ad2719095ee"
-    sha256 cellar: :any, arm64_big_sur:  "88cc51b5d7ff82f704b0fd4a1b53d6932ee5aeb3d5319c70226080a502ad7bf9"
-    sha256 cellar: :any, monterey:       "fb4d1543767a9513992776d3bcb15d74faa121edafc16616ec166395e60fa980"
-    sha256 cellar: :any, big_sur:        "ac0d436909b6be289508ff5aac82d6ab59c36f144145b7259b77234e2b544116"
-    sha256 cellar: :any, catalina:       "6a67aa2c9ae11a5dea8c142f221a0b6fdaece0f6ceecfd9747aeda99782cea21"
-    sha256 cellar: :any, mojave:         "5f21219575cf773cb21686b592cdf8e05617567490111a7919b87dc5d165fb63"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gtkmm4"
+    sha256 cellar: :any, mojave: "16efdcce80b95a3c16335987a751b753ce750b30721d1c92c85e83e740705a4d"
   end
 
   depends_on "meson" => :build
