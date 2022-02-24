a6552ed0a0548ebe4b577eab9b73503c7b830c62
Adrian Ho <the.gromgit@gmail.com>
2022-02-25 06:12:14 +0800

    svgbob: update 0.6.5 bottle.

--- Formula/svgbob.rb
+++ Formula/svgbob.rb
@@ -1,20 +1,20 @@
 class Svgbob < Formula
   desc "Convert your ascii diagram scribbles into happy little SVG"
   homepage "https://ivanceras.github.io/svgbob-editor/"
-  url "https://github.com/ivanceras/svgbob/archive/0.6.4.tar.gz"
-  sha256 "b609542317ddd7adac69b038cca7478849ee0012a3fa18caa8fae2748918c051"
+  url "https://github.com/ivanceras/svgbob/archive/0.6.5.tar.gz"
+  sha256 "afbf44bc536d34b974536b2d10059400a1e8e055d30e019520a2322a13a77c28"
   license "Apache-2.0"
   head "https://github.com/ivanceras/svgbob.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/svgbob"
-    sha256 cellar: :any_skip_relocation, mojave: "830fcb55a78152fce155afc108fd06cbf350427967dc3ee207abf5555bc6f990"
+    sha256 cellar: :any_skip_relocation, mojave: "99e9c85bae7d352bdb42aedf13ae680e1afba40cb364ebcf597f4984c959cb5f"
   end
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args(path: "packages/cli")
+    system "cargo", "install", *std_cargo_args(path: "packages/svgbob_cli")
     # The cli tool was renamed (0.6.2 -> 0.6.3)
     # Create a symlink to not break compatibility
     bin.install_symlink bin/"svgbob_cli" => "svgbob"
