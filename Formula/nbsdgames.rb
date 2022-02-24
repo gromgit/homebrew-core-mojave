2b7fbe312034af972bfdc5c54bb2b8c1f3e31658
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 09:13:38 +0800

    nbsdgames: update 5 bottle.

--- Formula/nbsdgames.rb
+++ Formula/nbsdgames.rb
@@ -1,19 +1,14 @@
 class Nbsdgames < Formula
   desc "Text-based modern games"
   homepage "https://github.com/abakh/nbsdgames"
-  url "https://github.com/abakh/nbsdgames/archive/refs/tags/v4.1.2.tar.gz"
-  sha256 "b4ba777791274af7db13d2827b254cf998a757468e119c6ee106ccbeafcd04c1"
+  url "https://github.com/abakh/nbsdgames/archive/refs/tags/v5.tar.gz"
+  sha256 "ca81d8b854a7bf9685bbc58aabc1a24cd617cadb7e9ddac64a513d2c8ddb2e6c"
   license :public_domain
   head "https://github.com/abakh/nbsdgames.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f27112c26f752624c594d5ab67af18de7e9138b985cfc4113ce293b7e9b91c58"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4281d8274d3f05106a5e34a31db7498615e2b4ac89e52528a864d8d4c2fa0999"
-    sha256 cellar: :any_skip_relocation, monterey:       "bbf1f0908c51d75fd78cbc5cd301c8f586e11d3357ff552dbfe61ab802cf8bdb"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d30a3395191ecada03c1c7c18b0c82e6833a35fdf47f5a4c98fb708e89eede2a"
-    sha256 cellar: :any_skip_relocation, catalina:       "6fc7bda5fa3519d9a4c241396a0ea512e252dc43e06289059b52e8017adef885"
-    sha256 cellar: :any_skip_relocation, mojave:         "88cebe1f55579632df4606eb0f9e86f4488dee8cc2b108e97a0f3bd90a6c3f90"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0323c032e3636a122be2af5faca46694b70a6072e334a42996068abccde6fb7c"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nbsdgames"
+    sha256 cellar: :any_skip_relocation, mojave: "e08248dd94e9506dc2ad43bad89d6ece698d307c713ae8e9ee1143e7901f7cdb"
   end
 
   uses_from_macos "ncurses"
@@ -29,6 +24,6 @@ class Nbsdgames < Formula
   end
 
   test do
-    assert_equal "2 <= size <= 7", shell_output("#{bin}/sudoku 1", 1).chomp
+    assert_equal "2 <= size <= 7", shell_output("#{bin}/sudoku -s 1", 1).chomp
   end
 end
