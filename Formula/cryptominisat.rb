cbf45b99a28ea8522e94d790e3a0e9ddec3ecd5c
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 14:09:00 +0800

    cryptominisat: update 5.8.0_2 bottle.

--- Formula/cryptominisat.rb
+++ Formula/cryptominisat.rb
@@ -6,7 +6,7 @@ class Cryptominisat < Formula
   # Everything that's needed to run/build/install/link the system is MIT licensed. This allows
   # easy distribution and running of the system everywhere.
   license "MIT"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://github.com/msoos/cryptominisat.git"
@@ -14,14 +14,8 @@ class Cryptominisat < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "cbfd9b642da561b05f4b73677892b9175b87af7c1bd43542f9326b164bfe330f"
-    sha256                               arm64_big_sur:  "8b9940bdf5011a8f060de82576726b7138a60975f56fcd7113b692e026444021"
-    sha256 cellar: :any,                 monterey:       "0f9ea0f5cc7850fcba9c5f72a13dcd03bad10f1b200d57c70aebde2c9aa9b67e"
-    sha256                               big_sur:        "ca952863f4a030cff0f60b3dc1b598c9a070460b5372577e63c8df577008e5eb"
-    sha256                               catalina:       "9314367f35d7d82790d4840b04d744fba37196f068fa38b899c7ac4c7e8f987b"
-    sha256                               mojave:         "2ad7c47169eae4780e42ddec65f1f6144fc59ee5585dd8d26ff5d270d25d9cc3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "add4bd75d5ec2f5a1ce559282d81e207e99cd6da2b90234a2971808e3e8a222b"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cryptominisat"
+    sha256 cellar: :any, mojave: "24c5ee1450c5f42f881a711cbae6452a4782108ebccc1ecf03b0b105db5a01e7"
   end
 
   depends_on "cmake" => :build
@@ -32,10 +26,15 @@ class Cryptominisat < Formula
     # fix audit failure with `lib/libcryptominisat5.5.7.dylib`
     inreplace "src/GitSHA1.cpp.in", "@CMAKE_CXX_COMPILER@", ENV.cxx
 
-    mkdir "build" do
-      system "cmake", "..", *std_cmake_args, "-DNOM4RI=ON", "-DCMAKE_INSTALL_RPATH=#{rpath}"
-      system "make", "install"
-    end
+    # fix building C++ with the value of PY_C_CONFIG
+    inreplace "python/setup.py.in", "cconf +", "cconf + ['-std=gnu++11'] +"
+
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DNOM4RI=ON",
+                    "-DCMAKE_INSTALL_RPATH=#{rpath}",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
