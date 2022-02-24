b97d3be3285d82caaadbeeee9b8ff0566f386235
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 18:24:10 +0800

    gearman: update 1.1.19.1_1 bottle.

--- Formula/gearman.rb
+++ Formula/gearman.rb
@@ -4,16 +4,11 @@ class Gearman < Formula
   url "https://github.com/gearman/gearmand/releases/download/1.1.19.1/gearmand-1.1.19.1.tar.gz"
   sha256 "8ea6e0d16a0c924e6a65caea8a7cd49d3840b9256d440d991de4266447166bfb"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "e1f5ee186f9a9c32001fd4736f5ff9d16aa64a15662a22cc14722d3fd86b7b7b"
-    sha256 cellar: :any,                 arm64_big_sur:  "6e3d3504ff3a5d4fb152ec03975aacea300cfca0875c8370c6dcb7bbcbc9ccda"
-    sha256 cellar: :any,                 monterey:       "93226ae14964f4e2cbe195dbfec4cbcbc85901eae7f4a29d7a023ac369dc0c50"
-    sha256 cellar: :any,                 big_sur:        "1a700a2938db5507f0f2b81c58988d563c5ad41eeb892bc31f1f4c918a882930"
-    sha256 cellar: :any,                 catalina:       "3a1a4bc57288dea7905134d9290c88a04273f7cc6361646694324e3bc9eb42d3"
-    sha256 cellar: :any,                 mojave:         "582d1de464569352536501e2aa832a9bc540220eae335b682411ecadffbfe198"
-    sha256 cellar: :any,                 high_sierra:    "8664f5b9c91ef99190cb70000758aa3d50f68afcad01d2e8cac234adf6a5424c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5c3fcefe30fbbffde1969348e359191a4b2b3aee1f0f1ea4f43e11ad57865f80"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gearman"
+    sha256 cellar: :any, mojave: "c09ab7e72a487c1b895d11f3889a44a30b988bcbc5b6289a1b29a22b8503313c"
   end
 
   depends_on "pkg-config" => :build
