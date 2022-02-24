a35816a5027bdadaf3f49ed8c2fb6cdea05e2f37
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 20:08:47 +0800

    osinfo-db-tools: update 1.10.0 bottle.

--- Formula/osinfo-db-tools.rb
+++ Formula/osinfo-db-tools.rb
@@ -1,10 +1,9 @@
 class OsinfoDbTools < Formula
   desc "Tools for managing the libosinfo database files"
   homepage "https://libosinfo.org/"
-  url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.9.0.tar.xz"
-  sha256 "255f1c878bacec70c3020ff5a9cb0f6bd861ca0009f24608df5ef6f62d5243c0"
+  url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.10.0.tar.xz"
+  sha256 "802cdd53b416706ea5844f046ddcfb658c1b4906b9f940c79ac7abc50981ca68"
   license "GPL-2.0-or-later"
-  revision 1
 
   livecheck do
     url "https://releases.pagure.org/libosinfo/?C=M&O=D"
@@ -13,8 +12,7 @@ class OsinfoDbTools < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/osinfo-db-tools"
-    rebuild 3
-    sha256 mojave: "4129abfeec36169c8c18691d6ba05c26753e03a45780d9f6fe3260b74bfe796e"
+    sha256 mojave: "52354587716726d4920370772caf07a5ab852004fb41e0e71ba67c07f3635595"
   end
 
   depends_on "meson" => :build
@@ -24,7 +22,7 @@ class OsinfoDbTools < Formula
   depends_on "glib"
   depends_on "json-glib"
   depends_on "libarchive"
-  depends_on "libsoup@2"
+  depends_on "libsoup"
   depends_on "python@3.9"
 
   uses_from_macos "pod2man" => :build
