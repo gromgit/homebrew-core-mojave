578ac4869499e6bbfa236518380e81180f27a3fd
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 23:41:14 +0800

    osrm-backend: update 5.26.0_1 bottle.

--- Formula/osrm-backend.rb
+++ Formula/osrm-backend.rb
@@ -4,6 +4,7 @@ class OsrmBackend < Formula
   url "https://github.com/Project-OSRM/osrm-backend/archive/v5.26.0.tar.gz"
   sha256 "45e986db540324bd0fc881b746e96477b054186698e8d14610ff7c095e906dcd"
   license "BSD-2-Clause"
+  revision 1
   head "https://github.com/Project-OSRM/osrm-backend.git", branch: "master"
 
   livecheck do
@@ -12,12 +13,8 @@ class OsrmBackend < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "f858c74b178e7d35752ca0ae3a61772421ea04ccff1041d27ee05433184b12d2"
-    sha256 cellar: :any, arm64_big_sur:  "a5247545b04b669711587d73ce192211669e4af288c5bce14923413936d285bf"
-    sha256 cellar: :any, monterey:       "c93ccda074ddfe27d18e17ef25aae4d5956dbab10eb45f4e1ac6ffb4ab9b3d03"
-    sha256 cellar: :any, big_sur:        "8aa632bde01a2f6566bbb006876a8d50af00bea45956e17ae0ddc87dd2278294"
-    sha256 cellar: :any, catalina:       "7caf6151936c3279afdf5c4e4b3bd5a81dbad1c33266a8fe80d3dbbeae5f7f79"
-    sha256 cellar: :any, mojave:         "e0b2e44267afd48717484ffc86fdee8096ec27020dcd2be0e2735b3cb2aef468"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/osrm-backend"
+    sha256 cellar: :any, mojave: "96e3602192c1c7957991d0b1efc10d865c7b501b96c6f062bebbd3d72455960e"
   end
 
   depends_on "cmake" => :build
@@ -27,6 +24,8 @@ class OsrmBackend < Formula
   depends_on "libzip"
   depends_on "lua"
   depends_on "tbb@2020"
+
+  conflicts_with "flatbuffers", because: "both install flatbuffers headers"
 
   def install
     lua = Formula["lua"]
