0fb7c3fe9c28a23665c5350cad2403110db3e671
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 18:39:26 +0800

    gource: update 0.51_2 bottle.

--- Formula/gource.rb
+++ Formula/gource.rb
@@ -4,17 +4,11 @@ class Gource < Formula
   url "https://github.com/acaudwell/Gource/releases/download/gource-0.51/gource-0.51.tar.gz"
   sha256 "19a3f888b1825aa7ed46f52cebce5012e3c62439e3d281102f21814c7a58e79a"
   license "GPL-3.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
-    sha256 arm64_monterey: "f0cb70d2215129ed1ea427f9c8848b63791e05a27c62d76de601468ce7646b17"
-    sha256 arm64_big_sur:  "82302ee7b3e01da62323d61a452fc81e5ec4fb62c2a04c8856e2f72373bf52cc"
-    sha256 monterey:       "e90445fc5e02e071088ffd5e7da0fce6e5f73a3b85441a9f0f35770d8118ce2a"
-    sha256 big_sur:        "bea0be45970ab69265303255911c279c2a739de3a8fd858e60ad18f1465cfc0f"
-    sha256 catalina:       "544380bd35795b5a809d536b6458b9e4a0f8fc940f36f6f8e54255091aa98250"
-    sha256 mojave:         "a603e1573f244abd8bc2a8963538ca6da27fbff90e68666e752678159c215baa"
-    sha256 high_sierra:    "9e1ed79145083e62e52f6416b08d0d80c778cae6a8ba83808fb17f841cd6c136"
-    sha256 x86_64_linux:   "65730cf451ae43ba3e105e3c7846914a65d82b4cd90699f146e15fda984de07e"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gource"
+    sha256 mojave: "f29a072a14daa1991a54f56f20dcbab0bb1c8664e56c9e465a3c89ec0493f56c"
   end
 
   head do
