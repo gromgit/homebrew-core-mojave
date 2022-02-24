e749cffd339c5e6d85e6c60071ff5ffa4ee928b0
Adrian Ho <the.gromgit@gmail.com>
2022-02-25 06:38:53 +0800

    teleport: update 8.3.1 bottle.

--- Formula/teleport.rb
+++ Formula/teleport.rb
@@ -1,8 +1,8 @@
 class Teleport < Formula
   desc "Modern SSH server for teams managing distributed infrastructure"
   homepage "https://gravitational.com/teleport"
-  url "https://github.com/gravitational/teleport/archive/v8.3.0.tar.gz"
-  sha256 "242ee0ed1ed01a328866956df12fd4567b070069cfaa605d4f59275acc390c31"
+  url "https://github.com/gravitational/teleport/archive/v8.3.1.tar.gz"
+  sha256 "b7312da301e8acd2aaf5667afc4529b1830636f2ffbbd490019d973ef510b3d6"
   license "Apache-2.0"
   head "https://github.com/gravitational/teleport.git", branch: "master"
 
@@ -17,7 +17,7 @@ class Teleport < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/teleport"
-    sha256 cellar: :any_skip_relocation, mojave: "fc752faec9405477b441a4e0d1f16bdc4558931e02f507a1a18f65089dd5e256"
+    sha256 cellar: :any_skip_relocation, mojave: "49bba95fd8a05895694854f464ffc28b21553e5e75bba847d2399f538b9dc61e"
   end
 
   depends_on "go" => :build
