152cf1262893ccc2a62434d559c808b0f339c492
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 22:51:45 +0800

    ompl: update 1.5.2_1 bottle.

--- Formula/ompl.rb
+++ Formula/ompl.rb
@@ -4,6 +4,7 @@ class Ompl < Formula
   url "https://github.com/ompl/ompl/archive/1.5.2.tar.gz"
   sha256 "db1665dd2163697437ef155668fdde6101109e064a2d1a04148e45b3747d5f98"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/ompl/ompl.git", branch: "main"
 
   # We check the first-party download page because the "latest" GitHub release
@@ -14,11 +15,8 @@ class Ompl < Formula
   end
 
   bottle do
-    sha256                               arm64_big_sur: "214c8032cd621e44527cacc9af0800312e95922f7d79e58a94c93a09977638de"
-    sha256                               big_sur:       "aab42d95974b15167f1a240f853283eed81a928877822a0aa8ee67664f1992e6"
-    sha256                               catalina:      "6b0190b615a9929cd74f03e97ff62efdca225ce9c7afc171ed3f5be31f2a8afd"
-    sha256                               mojave:        "b7d94176b089fc5959b6accb2da9390f94ab1863902f0f189bd542566815578c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d74e2b42ab0c62c1f2cf35d515cb9b3b87c24b9e3f480553064748d686fa3a04"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ompl"
+    sha256 cellar: :any, mojave: "0b5440f69f18c190282c874f66ff96c47a1d0bd98a7c02a5dab67cbb3a8343a3"
   end
 
   depends_on "cmake" => :build
