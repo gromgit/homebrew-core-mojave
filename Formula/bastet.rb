3b2dabcf8a1952f3baac78d80f300e2d26285e15
Adrian Ho <the.gromgit@gmail.com>
2022-02-24 13:07:35 +0800

    bastet: update 0.43.2_1 bottle.

--- Formula/bastet.rb
+++ Formula/bastet.rb
@@ -4,19 +4,11 @@ class Bastet < Formula
   url "https://github.com/fph/bastet/archive/0.43.2.tar.gz"
   sha256 "f219510afc1d83e4651fbffd5921b1e0b926d5311da4f8fa7df103dc7f2c403f"
   license "GPL-3.0-or-later"
+  revision 1
 
   bottle do
-    rebuild 1
-    sha256 arm64_monterey: "9847fe14d15e54e4e38438ed8282abbc922de3a3ccc7ad3648b6f1be61707a44"
-    sha256 arm64_big_sur:  "048b7752909e1c445b8367ee87002df7511d5d1520fa0f2c39103f60a7c7c3d8"
-    sha256 monterey:       "4efdae4d19ba49760a781bc930d97b7ad6559c8a8ea987cf628fc48ea6dcd625"
-    sha256 big_sur:        "fe79a047ab3449c63ca5890adbfc2c9b703bf6069ebe851cc4af1db8546b3f2b"
-    sha256 catalina:       "0dfeabb0071431e426ac18b366ff5d065067075e7d3f4572e55a281e6702e215"
-    sha256 mojave:         "d1315f05616c060c8b5e83a9ae494f2ffecd2f78d53ef554192bb0e12ef451ef"
-    sha256 high_sierra:    "188658452934d4ef5d48d6837fb0c6bf3e3875488e0c1da8dcf62ca37c1ee998"
-    sha256 sierra:         "8133c13d1b98d96eacf5d420d30378fbfcd9cbe898b0f13b188112618f4338f5"
-    sha256 el_capitan:     "e3745b716c09ce7f3834f4fc30163fa132f93feeec4c301dc9d46b0bc9ca564f"
-    sha256 x86_64_linux:   "02918b45d68fad9e1ee1e3e4f520107398e2f7df1501dce5f3a7e0052ebde359"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bastet"
+    sha256 mojave: "ace2cd919aa295203ffd264b944ba170d0f27da0fd4ec7e0c2b216e528327216"
   end
 
   depends_on "boost"
