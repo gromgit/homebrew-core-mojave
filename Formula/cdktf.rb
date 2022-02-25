d68396d56a875745a316cad99c2c4c56bd9ad2e6
Adrian Ho <the.gromgit@gmail.com>
2022-02-25 21:19:40 +0800

    cdktf: update 0.9.4 bottle.

--- Formula/cdktf.rb
+++ Formula/cdktf.rb
@@ -3,13 +3,13 @@ require "language/node"
 class Cdktf < Formula
   desc "Cloud Development Kit for Terraform"
   homepage "https://github.com/hashicorp/terraform-cdk"
-  url "https://registry.npmjs.org/cdktf-cli/-/cdktf-cli-0.9.2.tgz"
-  sha256 "e06902f9975313f707655562a17191db3f5b488f91bdeb0b9a3e6f2782e47ab8"
+  url "https://registry.npmjs.org/cdktf-cli/-/cdktf-cli-0.9.4.tgz"
+  sha256 "19d04aba527e143aba8449f06c9c7812f01feae70bf7f67a539ebe890831f4f7"
   license "MPL-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cdktf"
-    sha256 cellar: :any_skip_relocation, mojave: "f3c93b47057f452fa7ba22e9fdfc92a2379ca8c7940b41a5e8b215df015fd7cd"
+    sha256 cellar: :any_skip_relocation, mojave: "55a26014fa4df0635791b46bcb8758d6e405d716db07785ff68e93572406cfde"
   end
 
   depends_on "node"
