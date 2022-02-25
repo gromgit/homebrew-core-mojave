499e12d2c379d9b4f19776abebf7825cb49b45e2
Adrian Ho <the.gromgit@gmail.com>
2022-02-26 06:43:20 +0800

    libsecret: update 0.20.5 bottle.

--- Formula/libsecret.rb
+++ Formula/libsecret.rb
@@ -1,24 +1,20 @@
 class Libsecret < Formula
   desc "Library for storing/retrieving passwords and other secrets"
   homepage "https://wiki.gnome.org/Projects/Libsecret"
-  url "https://download.gnome.org/sources/libsecret/0.20/libsecret-0.20.4.tar.xz"
-  sha256 "325a4c54db320c406711bf2b55e5cb5b6c29823426aa82596a907595abb39d28"
+  url "https://download.gnome.org/sources/libsecret/0.20/libsecret-0.20.5.tar.xz"
+  sha256 "3fb3ce340fcd7db54d87c893e69bfc2b1f6e4d4b279065ffe66dac9f0fd12b4d"
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 arm64_monterey: "159a98d0c4dc768fe7ad719348f7c91c57500feab9fb85eee79fd76fc77fed08"
-    sha256 arm64_big_sur:  "115ff8e5af3b0bffd370a47d7165664dda193b4a2ac00e2054b455cbd77e6d6e"
-    sha256 monterey:       "bbc699fb8e22f859b07f1370e5c11d4d8dd50a6266f152b2d49bbbbd0e0ff84c"
-    sha256 big_sur:        "68da058738e04fd8a7ec9713df527afec9dc8076a219548dc194184df337fe8a"
-    sha256 catalina:       "8fc40fdf1fda5a1bd12661b96a1b0398cc0b600e9f43ef44384ffa82fa6b3133"
-    sha256 mojave:         "80fa9108466d6fac5f752ce926a9f6175e4f701764d2b077a3cdee0109be8ba6"
-    sha256 high_sierra:    "9663806ffb17b3c50eb015c43b2763ff47e12624e56d694d454f238748ea17e2"
-    sha256 x86_64_linux:   "4dbbaa5c11684e272e180de703dfac9da1e3931fb51ab73067a7fd58b572c1d4"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libsecret"
+    sha256 cellar: :any, mojave: "44f53e56fbb83c46e15f6e0d3bf3ead5cb7235f588392ff7c6560636fe0d14ee"
   end
 
   depends_on "docbook-xsl" => :build
   depends_on "gettext" => :build
   depends_on "gobject-introspection" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "vala" => :build
   depends_on "glib"
@@ -28,17 +24,13 @@ class Libsecret < Formula
   def install
     ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
 
-    args = %W[
-      --disable-debug
-      --disable-dependency-tracking
-      --disable-silent-rules
-      --prefix=#{prefix}
-      --enable-introspection
-      --enable-vala
-    ]
-
-    system "./configure", *args
-    system "make", "install"
+    mkdir "build" do
+      system "meson", "..", "-Dbashcompdir=#{bash_completion}",
+                            "-Dgtk_doc=false",
+                            *std_meson_args
+      system "ninja", "--verbose"
+      system "ninja", "install", "--verbose"
+    end
   end
 
   test do
