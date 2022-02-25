687d4b2f6f551134c7466d29a2fb105adb2e667e
Adrian Ho <the.gromgit@gmail.com>
2022-02-25 16:25:17 +0800

    alembic: update 1.8.3_1 bottle.

--- Formula/alembic.rb
+++ Formula/alembic.rb
@@ -4,16 +4,12 @@ class Alembic < Formula
   url "https://github.com/alembic/alembic/archive/1.8.3.tar.gz"
   sha256 "b0bc74833bff118a869e81e6acb810a58797e77ef63143954b2f8e817c7f65cb"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/alembic/alembic.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "4a2c94fe2b8f8c34024a4be204b4db96ada1cdd2f85457b87519a5618f130056"
-    sha256 cellar: :any,                 arm64_big_sur:  "1c7b635a35b4444ad15e39a62e41acb55b895b06e6acf4d82bd0dfb3b06306c3"
-    sha256 cellar: :any,                 monterey:       "9133bdb4d6d930c43cc373931218d930ab4fe8eed0aa6d0cdfa40d3945d48371"
-    sha256 cellar: :any,                 big_sur:        "978c247938f12a2f093e3e3caa0d9506186fc2fc922b63c328a9be08c660cc67"
-    sha256 cellar: :any,                 catalina:       "b6d5ec59340dde30d36865eb37eb667459d0c5ca8bffbd220ca580ccd9b41a29"
-    sha256 cellar: :any,                 mojave:         "c10d048c3c97a4dbd228f9c42b8b2c54e9b6eb4ffceb39e300f5676013547c25"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "16bb62b32e9e5ad09de62602a5b31c262610d4be59bc862f00b8fc669da1add3"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/alembic"
+    sha256 cellar: :any, mojave: "5e23354c808c6d76be61764ad7f091dad4520c57627f308c88eec42dd0472f44"
   end
 
   depends_on "cmake" => :build
