c354b3f5e3ef8821070828c2150985fe6e5ecb60
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-25 13:49:48 +0800

    parallel: no rebuilding required

--- Formula/parallel.rb
+++ Formula/parallel.rb
@@ -1,9 +1,9 @@
 class Parallel < Formula
   desc "Shell command parallelization utility"
   homepage "https://savannah.gnu.org/projects/parallel/"
-  url "https://ftp.gnu.org/gnu/parallel/parallel-20220122.tar.bz2"
-  mirror "https://ftpmirror.gnu.org/parallel/parallel-20220122.tar.bz2"
-  sha256 "b8221a21412bca572ad8445b7981dfd625a3c6d48772cda468dfb5b886337e00"
+  url "https://ftp.gnu.org/gnu/parallel/parallel-20220222.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/parallel/parallel-20220222.tar.bz2"
+  sha256 "f81682b863ead7fb9a114754001e9286f954550a57a3cf36c9003a8047a6a445"
   license "GPL-3.0-or-later"
   version_scheme 1
   head "https://git.savannah.gnu.org/git/parallel.git", branch: "master"
@@ -14,7 +14,7 @@ class Parallel < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "6e77bbb9c73324762e8046d7c75cb809ed8864629c86d4838cf94d61f567b214"
+    sha256 cellar: :any_skip_relocation, all: "325e8f2343c7b0cda8ca4b3d60ebf7fbb8ed4ad0652ef628a19354d9b30b4c0b"
   end
 
   conflicts_with "moreutils", because: "both install a `parallel` executable"
