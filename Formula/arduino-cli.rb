40e430b9f725c5a223d8a47ad3b6b8cae78b0757
Adrian Ho <the.gromgit@gmail.com>
2022-02-25 19:03:26 +0800

    arduino-cli: update 0.21.1 bottle.

--- Formula/arduino-cli.rb
+++ Formula/arduino-cli.rb
@@ -2,8 +2,8 @@ class ArduinoCli < Formula
   desc "Arduino command-line interface"
   homepage "https://github.com/arduino/arduino-cli"
   url "https://github.com/arduino/arduino-cli.git",
-      tag:      "0.21.0",
-      revision: "10107d2407c2d9997310fc2e0f22dfd15d48e9a8"
+      tag:      "0.21.1",
+      revision: "9fcbb392b08d2574e8182e920ac1a5779a1adf75"
   license "GPL-3.0-only"
   head "https://github.com/arduino/arduino-cli.git", branch: "master"
 
@@ -14,7 +14,7 @@ class ArduinoCli < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/arduino-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "fda471aeda0dbd3cd4e7b82b463b950f670c713a95df13a1f5ebc180508e8435"
+    sha256 cellar: :any_skip_relocation, mojave: "71b644227c02d633465735a99bd76097e028d0e7a59d4927e8667f88c54fa268"
   end
 
   depends_on "go" => :build
