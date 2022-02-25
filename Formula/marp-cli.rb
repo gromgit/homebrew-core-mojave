27c51a2057286aef28a76b27cd64c3ca4cf2be5a
Adrian Ho <the.gromgit@gmail.com>
2022-02-26 07:21:18 +0800

    marp-cli: update 1.7.0 bottle.

--- Formula/marp-cli.rb
+++ Formula/marp-cli.rb
@@ -3,13 +3,13 @@ require "language/node"
 class MarpCli < Formula
   desc "Easily convert Marp Markdown files into static HTML/CSS, PDF, PPT and images"
   homepage "https://github.com/marp-team/marp-cli"
-  url "https://registry.npmjs.org/@marp-team/marp-cli/-/marp-cli-1.6.0.tgz"
-  sha256 "c9e4300a18d1f5107ee10bbd3888116f53c8aee0a0df5f48964f963064101beb"
+  url "https://registry.npmjs.org/@marp-team/marp-cli/-/marp-cli-1.7.0.tgz"
+  sha256 "bc0e2e2080a318b482b5b569751dddd6dd9955837b1abb3501af66a929b0c7c6"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/marp-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "8649ab615435b136dca6f54bfc739d98b24dea9b40fb808a68fb918fa2e8a101"
+    sha256 cellar: :any_skip_relocation, mojave: "25de734ebd8903f508e999fd63b1eb9dcda5057dd57c7dd7f7e296f0ecee396c"
   end
 
   depends_on "node"
