a82695ba8318c5c2d027d6a109ae9b131247a953
Adrian Ho <the.gromgit@gmail.com>
2022-02-26 07:53:20 +0800

    minetest: update 5.5.0 bottle.

--- Formula/minetest.rb
+++ Formula/minetest.rb
@@ -4,12 +4,17 @@ class Minetest < Formula
   license "LGPL-2.1-or-later"
 
   stable do
-    url "https://github.com/minetest/minetest/archive/5.4.1.tar.gz"
-    sha256 "de9e4410583c845c104b4be25f9d0b8743d8573c120149b8910ae2519f9ab14e"
+    url "https://github.com/minetest/minetest/archive/5.5.0.tar.gz"
+    sha256 "8b9bef6054c8895cc3329ae6d05cb355eef9c7830600d82dc9eaa4664f87c8f9"
+
+    resource "irrlichtmt" do
+      url "https://github.com/minetest/irrlicht/archive/1.9.0mt4.tar.gz"
+      sha256 "a0e2e5239ebca804adf54400ccaacaf228ec09223cfb2e1daddc9bf2694176e6"
+    end
 
     resource "minetest_game" do
-      url "https://github.com/minetest/minetest_game/archive/5.4.1.tar.gz"
-      sha256 "b4bfa0755b88230cf4bdb6af6a0951dd1248f6cdf87fecc340e43ac12c80b0b2"
+      url "https://github.com/minetest/minetest_game/archive/5.5.0.tar.gz"
+      sha256 "1e87252e26d6b1d3efe7720e3e097d489339dea4dd25980a828d5da212b01aaa"
     end
   end
 
@@ -19,14 +24,16 @@ class Minetest < Formula
   end
 
   bottle do
-    sha256 cellar: :any, monterey: "2c83905a5ce12b80226247f7eb8888ff4ef80c5050998161f510f61667af98d2"
-    sha256 cellar: :any, big_sur:  "d391ffa3ef29483219449315ddf30ecd7d20e947bb5780081418dbb383ba938a"
-    sha256               catalina: "8c773aabb5faa6617c09f69915a9e3caa49734d2f2117d05ce455597c8803695"
-    sha256               mojave:   "e03ebb1d5ac5598e6c9273a89b98b30b95f9640bbb74d0b679e9b56621a06aca"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/minetest"
+    sha256 cellar: :any, mojave: "8541d84e3a0bc23788460d9bfcc59b65663f394173d2512681fb8c7ccdee3dd9"
   end
 
   head do
     url "https://github.com/minetest/minetest.git"
+
+    resource "irrlichtmt" do
+      url "https://github.com/minetest/irrlicht.git", branch: "master"
+    end
 
     resource "minetest_game" do
       url "https://github.com/minetest/minetest_game.git", branch: "master"
@@ -34,27 +41,58 @@ class Minetest < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "gettext" => :build
   depends_on "freetype"
-  depends_on "gettext"
-  depends_on "irrlicht"
+  depends_on "gmp"
   depends_on "jpeg"
+  depends_on "jsoncpp"
   depends_on "libogg"
+  depends_on "libpng"
   depends_on "libvorbis"
-  depends_on "luajit"
+  depends_on "luajit-openresty"
+  depends_on "zstd"
+
+  uses_from_macos "curl"
+  uses_from_macos "ncurses"
+  uses_from_macos "sqlite"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "libx11"
+    depends_on "libxxf86vm"
+    depends_on "mesa"
+    depends_on "openal-soft"
+  end
 
   def install
+    inreplace "src/CMakeLists.txt" do |s|
+      # These flags are not needed for LuaJIT 2.1 (Ref: https://luajit.org/install.html).
+      # On Apple ARM, the flags results in broken binaries and need to be removed.
+      s.gsub! " -pagezero_size 10000 -image_base 100000000\"", "\""
+      # Disable CMake fixup_bundle to prevent copying dylibs into app bundle
+      s.gsub! "fixup_bundle(", "# \\0"
+    end
+
+    # Remove bundled libraries to prevent fallback
+    %w[lua gmp jsoncpp].each { |lib| (buildpath/"lib"/lib).rmtree }
+
     (buildpath/"games/minetest_game").install resource("minetest_game")
+    (buildpath/"lib/irrlichtmt").install resource("irrlichtmt")
 
-    args = std_cmake_args
-    args << "-DBUILD_CLIENT=1" << "-DBUILD_SERVER=0"
-    args << "-DENABLE_FREETYPE=1" << "-DCMAKE_EXE_LINKER_FLAGS='-L#{Formula["freetype"].opt_lib}'"
-    args << "-DENABLE_GETTEXT=1" << "-DCUSTOM_GETTEXT_PATH=#{Formula["gettext"].opt_prefix}"
-    args << "-DIRRLICHT_LIBRARY=#{Formula["irrlicht"].opt_frameworks}/IrrFramework.framework"
+    args = %W[
+      -DBUILD_CLIENT=1
+      -DBUILD_SERVER=0
+      -DENABLE_FREETYPE=1
+      -DCMAKE_EXE_LINKER_FLAGS='-L#{Formula["freetype"].opt_lib}'
+      -DENABLE_GETTEXT=1
+      -DCUSTOM_GETTEXT_PATH=#{Formula["gettext"].opt_prefix}
+    ]
+    # Workaround for 'Could NOT find GettextLib (missing: ICONV_LIBRARY)'
+    args << "-DICONV_LIBRARY=#{MacOS.sdk_path}/usr/lib/libiconv.tbd" if MacOS.version >= :big_sur
 
-    system "cmake", ".", *args
-    system "make", "package"
-    system "unzip", "minetest-*-osx.zip"
-    prefix.install "minetest.app"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   def caveats
@@ -69,6 +107,10 @@ class Minetest < Formula
   end
 
   test do
-    system "#{prefix}/minetest.app/Contents/MacOS/minetest", "--version"
+    minetest = OS.mac? ? prefix/"minetest.app/Contents/MacOS/minetest" : bin/"minetest"
+    output = shell_output("#{minetest} --version")
+    assert_match "USE_CURL=1", output
+    assert_match "USE_GETTEXT=1", output
+    assert_match "USE_SOUND=1", output
   end
 end
