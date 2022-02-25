48fa7787cf0f4886e659fedb01293c4a73c38056
Adrian Ho <the.gromgit@gmail.com>
2022-02-26 03:25:15 +0800

    gleam: update 0.20.0 bottle.

--- Formula/gleam.rb
+++ Formula/gleam.rb
@@ -1,13 +1,13 @@
 class Gleam < Formula
   desc "✨ A statically typed language for the Erlang VM"
   homepage "https://gleam.run"
-  url "https://github.com/gleam-lang/gleam/archive/v0.19.0.tar.gz"
-  sha256 "4aeadce8a4ecb56f4c66190b8cc97702f7dfccf2bb4b9ffe18b2de2317f55d84"
+  url "https://github.com/gleam-lang/gleam/archive/v0.20.0.tar.gz"
+  sha256 "360d0ac92ceac3a23f91193709aeb23225e548ad710ad04c0875ff158099f954"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gleam"
-    sha256 cellar: :any_skip_relocation, mojave: "c985a68b08474512e43a1f2e1be630c295559d05da5bf3ada5a233a96de65811"
+    sha256 cellar: :any_skip_relocation, mojave: "3f21f58bbc73dc166af971ffb4247410228f5219e197f9f2604a8891573ca7de"
   end
 
   depends_on "rust" => :build
