9151ed95a117fc156e5b9ada2be9db63b5b500c8
Adrian Ho <the.gromgit@gmail.com>
2022-02-26 03:37:53 +0800

    glooctl: update 1.10.10 bottle.

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -4,8 +4,8 @@ class Glooctl < Formula
   # NOTE: Please wait until the newest stable release is finished building and
   # no longer marked as "Pre-release" before creating a PR for a new version.
   url "https://github.com/solo-io/gloo.git",
-      tag:      "v1.10.9",
-      revision: "ef66036d1e44b765df00254d74ead11a719f6cdf"
+      tag:      "v1.10.10",
+      revision: "d9e5879db1259c88ca64d8aafcc2598629f9533d"
   license "Apache-2.0"
   head "https://github.com/solo-io/gloo.git", branch: "master"
 
@@ -16,7 +16,7 @@ class Glooctl < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/glooctl"
-    sha256 cellar: :any_skip_relocation, mojave: "d07388f130d5a2fe246cde3c235f85e58299802e8de0e2f6ea3b47f3f3c8cfa1"
+    sha256 cellar: :any_skip_relocation, mojave: "cf6046fa5659ad1ddc5f47bbea60adaabdb191b847201cd8078bd7e10a234693"
   end
 
   depends_on "go" => :build
