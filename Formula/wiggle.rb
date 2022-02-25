1e4d01647df9fdbc34b4419ffb480dc2c76b6a42
Adrian Ho <the.gromgit@gmail.com>
2022-02-25 08:12:42 +0800

    wiggle: update 1.3 bottle.

--- Formula/wiggle.rb
+++ Formula/wiggle.rb
@@ -3,7 +3,7 @@ class Wiggle < Formula
   homepage "https://github.com/neilbrown/wiggle"
   url "https://github.com/neilbrown/wiggle/archive/refs/tags/v1.3.tar.gz"
   sha256 "ff92cf0133c1f4dce33563e263cb30e7ddb6f4abdf86d427b1ec1490bec25afa"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url "https://neil.brown.name/wiggle/"
@@ -12,8 +12,12 @@ class Wiggle < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/wiggle"
-    sha256 cellar: :any_skip_relocation, mojave: "d0ade4b19e692423d9048869388f395d23cf90efada72e1c7f044925cb26f103"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "74b4ea854de546eddcf91bf808016206be229a89cf1e76c096bd02ba5952b49b"
   end
+
+  uses_from_macos "groff" => :build
+  uses_from_macos "ncurses"
 
   def install
     system "make", "OptDbg=#{ENV.cflags}", "wiggle", "wiggle.man", "test"
