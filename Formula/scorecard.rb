1879826ed4364f738c09068ee940ebe5e915af95
Adrian Ho <the.gromgit@gmail.com>
2022-02-26 13:11:05 +0800

    scorecard: update 4.1.0 bottle.

--- Formula/scorecard.rb
+++ Formula/scorecard.rb
@@ -1,14 +1,14 @@
 class Scorecard < Formula
   desc "Security health metrics for Open Source"
   homepage "https://github.com/ossf/scorecard"
-  url "https://github.com/ossf/scorecard/archive/v4.0.1.tar.gz"
-  sha256 "8545d4aacaa2ffafbf545aeee7bccc381d2c00ffa0e4e9cb65190e68ece543d6"
+  url "https://github.com/ossf/scorecard/archive/v4.1.0.tar.gz"
+  sha256 "c460ae4cf019dbf7a582c353cf052f14524f187e9fab76132a1af1d674ef8613"
   license "Apache-2.0"
   head "https://github.com/ossf/scorecard.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/scorecard"
-    sha256 cellar: :any_skip_relocation, mojave: "ed8fe04ea86b41a6df290e4d8c68a0d613dfc93083b14eb4bfd1d9744f6aeaa9"
+    sha256 cellar: :any_skip_relocation, mojave: "2be70ff7ba9aed90f7b3028a9c7ee20b277af80753a94972c92301c8a7f7f892"
   end
 
   depends_on "go" => :build
