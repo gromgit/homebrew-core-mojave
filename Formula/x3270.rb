913397d7cf0d800233d738e0a1bc06050799d37f
Adrian Ho <the.gromgit@gmail.com>
2022-02-26 16:14:36 +0800

    x3270: update 4.1ga12 bottle.

--- Formula/x3270.rb
+++ Formula/x3270.rb
@@ -1,8 +1,8 @@
 class X3270 < Formula
   desc "IBM 3270 terminal emulator for the X Window System and Windows"
   homepage "http://x3270.bgp.nu/"
-  url "http://x3270.bgp.nu/download/04.01/suite3270-4.1ga10-src.tgz"
-  sha256 "8216572d0a14d4d18e65db97f6e2dd1aeb66eed02b4d544c79ed8d34ea54be71"
+  url "http://x3270.bgp.nu/download/04.01/suite3270-4.1ga12-src.tgz"
+  sha256 "262489641a60321a06b20ea94ac1ed204e04c4749eb1df1b9e21d0034bc17fef"
   license "BSD-3-Clause"
 
   livecheck do
@@ -12,8 +12,7 @@ class X3270 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/x3270"
-    rebuild 2
-    sha256 mojave: "9fe2e8e3a13eabfa6d14c3522ca3f85d7da5b3719abbecbc8582d96ec7d475dc"
+    sha256 mojave: "b5c68ed805fca5e05e644a749295d9faa15e609655d00b2a9886be3cc3fa37f4"
   end
 
   depends_on "readline"
@@ -21,10 +20,6 @@ class X3270 < Formula
   uses_from_macos "tcl-tk"
 
   def install
-    # use BSD date options on macOS
-    # https://sourceforge.net/p/x3270/bugs/24/
-    inreplace "Common/mkversion.sh", "date -d@", "date -r" if OS.mac?
-
     args = %W[
       --prefix=#{prefix}
       --enable-c3270
