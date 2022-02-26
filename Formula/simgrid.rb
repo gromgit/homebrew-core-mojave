544c318d34d082489c664784908d5a5ba5630f85
Adrian Ho <the.gromgit@gmail.com>
2022-02-26 13:46:06 +0800

    simgrid: update 3.29_1 bottle.

--- Formula/simgrid.rb
+++ Formula/simgrid.rb
@@ -5,6 +5,7 @@ class Simgrid < Formula
   homepage "https://simgrid.org/"
   url "https://framagit.org/simgrid/simgrid/uploads/6ca357e80bd4d401bff16367ff1d3dcc/simgrid-3.29.tar.gz"
   sha256 "83e8afd653555eeb70dc5c0737b88036c7906778ecd3c95806c6bf5535da2ccf"
+  revision 1
 
   livecheck do
     url :homepage
@@ -12,13 +13,8 @@ class Simgrid < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "d0ce6aadb39ca40b077444658bfa2d56c29754496657ff58648c6fd84cce941b"
-    sha256 arm64_big_sur:  "3bcf1b52202b0bf72913983c86bb8d6b5e842cf20c08ff9f30b5e562812d0ec7"
-    sha256 monterey:       "bb805e263f35330965f17104c685ca64ea643bba321ee9c4460ab47f48581e13"
-    sha256 big_sur:        "ed874d245adffc5455759d716afea6d4152a37df3296e8689b9b6b43a3f1344a"
-    sha256 catalina:       "3553e2e019fe3109b094fd976c9befa272046ebb21856a5a2fd15d95ee054628"
-    sha256 mojave:         "d6718c707ad66eba5d138e3289b50d37f43ff3171799a2e25e69e99309d97400"
-    sha256 x86_64_linux:   "dd8671e3e11762f09e17db9db5492d221e001854947ac3377ce05aa720c249a9"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/simgrid"
+    sha256 mojave: "6a1a42012c0289fcb00073e0515219377a6a3824bccd2ab2e7703ff1a90905dc"
   end
 
   depends_on "cmake" => :build
