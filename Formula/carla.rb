372f4f8857c9810df77ccac416f5dc034a0b4031
Adrian Ho <the.gromgit@gmail.com>
2022-02-28 00:42:15 +0800

    carla: update 2.4.2 bottle.

--- Formula/carla.rb
+++ Formula/carla.rb
@@ -1,8 +1,8 @@
 class Carla < Formula
   desc "Audio plugin host supporting LADSPA, LV2, VST2/3, SF2 and more"
   homepage "https://kxstudio.linuxaudio.org/Applications:Carla"
-  url "https://github.com/falkTX/Carla/archive/v2.4.1.tar.gz"
-  sha256 "bbb188a672ea8871b11648d36770ba013497d03407ca9c73ed68429016f7536f"
+  url "https://github.com/falkTX/Carla/archive/v2.4.2.tar.gz"
+  sha256 "376884965e685242953cab757818dde262209c651bd563a04eade0678c6b9f39"
   license "GPL-2.0-or-later"
   head "https://github.com/falkTX/Carla.git", branch: "main"
 
@@ -12,10 +12,8 @@ class Carla < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "d73c02d184d35358d663f45a067087c8b87547fce4c43a9d97059d69964b19bc"
-    sha256 cellar: :any, big_sur:       "968987ce59da62af302c1820b05a165441a6cbb8aadf8e5de9055f1f8dba615b"
-    sha256 cellar: :any, catalina:      "51e26ad503289b0241f695e9c1d3829b28ed569127168644c68c2b4dbde439ca"
-    sha256 cellar: :any, mojave:        "bd7b6ed706d425e2c94a95060e837822b666586df658518025906a66ec2115dd"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/carla"
+    sha256 cellar: :any, mojave: "b5eb0d98eaac180f7897399b0b84f77f42fcece87beef5fc82b9a049dc9dc2d8"
   end
 
   depends_on "pkg-config" => :build
