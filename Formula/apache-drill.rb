82555444fe990de61bcfc9dc8b2800f7aae0f394
Adrian Ho <aho+vm-mojave@03s.net>
2022-02-27 21:54:46 +0800

    apache-drill: no rebuilding required

--- Formula/apache-drill.rb
+++ Formula/apache-drill.rb
@@ -1,9 +1,9 @@
 class ApacheDrill < Formula
   desc "Schema-free SQL Query Engine for Hadoop, NoSQL and Cloud Storage"
   homepage "https://drill.apache.org"
-  url "https://www.apache.org/dyn/closer.lua?path=drill/drill-1.19.0/apache-drill-1.19.0.tar.gz"
-  mirror "https://archive.apache.org/dist/drill/drill-1.19.0/apache-drill-1.19.0.tar.gz"
-  sha256 "9374711a08252ff2ac7116c33b6feb627448f109c6e0834cb66d5b8587e6276b"
+  url "https://www.apache.org/dyn/closer.lua?path=drill/drill-1.20.0/apache-drill-1.20.0.tar.gz"
+  mirror "https://archive.apache.org/dist/drill/drill-1.20.0/apache-drill-1.20.0.tar.gz"
+  sha256 "a54e04d0120aa1c34da7693feff9c4497918b25d5e0a8eaf48a9a2ec2073d56e"
   license "Apache-2.0"
 
   livecheck do
@@ -12,7 +12,7 @@ class ApacheDrill < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "28861dd0b660caf226c095344b4d5bbfba86d35fb69d68ab589f968628f23c66"
+    sha256 cellar: :any_skip_relocation, all: "666767fa0ccbdb97d77f202e1a0d4c58e21b3a4a6869018bd7342f0430b82455"
   end
 
   depends_on "openjdk@11"
