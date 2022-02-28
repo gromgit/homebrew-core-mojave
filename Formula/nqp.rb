d17cd3c8e793c22195ccc59103a457d785c5593f
Adrian Ho <the.gromgit@gmail.com>
2022-03-01 01:18:27 +0800

    nqp: update 2022.02 bottle.

--- Formula/nqp.rb
+++ Formula/nqp.rb
@@ -1,14 +1,13 @@
 class Nqp < Formula
   desc "Lightweight Perl 6-like environment for virtual machines"
   homepage "https://github.com/Raku/nqp"
-  url "https://github.com/Raku/nqp/releases/download/2021.10/nqp-2021.10.tar.gz"
-  sha256 "48135bc1b3ce22c1c57ccef46531e7688eec83d6b905b502ffcefd6c23294b49"
+  url "https://github.com/Raku/nqp/releases/download/2022.02/nqp-2022.02.tar.gz"
+  sha256 "25d3c99745cd84f4049a9bd9cf26bb5dc817925abaafe71c9bdb68841cdb18b1"
   license "Artistic-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nqp"
-    rebuild 2
-    sha256 mojave: "3bb7765ab4d1333178a79775d19e4ba62df6a338e3451e5a93169a7c0bf1dacf"
+    sha256 mojave: "e701840877da41496cdb4094da3a0879ca8119b779c6bbf3ffe53536d8198329"
   end
 
   depends_on "libtommath"
