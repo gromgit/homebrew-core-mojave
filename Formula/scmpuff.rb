7c6de614250b75a4d3ec0e5780c84a15a019e1fa
Adrian Ho <the.gromgit@gmail.com>
2022-03-01 03:54:08 +0800

    scmpuff: update 0.5.0 bottle.

--- Formula/scmpuff.rb
+++ Formula/scmpuff.rb
@@ -1,13 +1,13 @@
 class Scmpuff < Formula
   desc "Adds numbered shortcuts for common git commands"
   homepage "https://mroth.github.io/scmpuff/"
-  url "https://github.com/mroth/scmpuff/archive/v0.4.0.tar.gz"
-  sha256 "2f43ab94a4027b114cde4232c5c91722bc2d7eb44b4d04ff03565fa39b642014"
+  url "https://github.com/mroth/scmpuff/archive/v0.5.0.tar.gz"
+  sha256 "e07634c7207dc51479d39895e546dd0107a50566faf5c2067f61a3b92c824fbf"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/scmpuff"
-    sha256 cellar: :any_skip_relocation, mojave: "3d3eda957b4c151e85012b2ca71c7a29b3c8c1318bc087c00cd9404a343a4a04"
+    sha256 cellar: :any_skip_relocation, mojave: "8a419e1624aa50c515e80b5f4b69bea4c3f4b27dd11d33751bab3d394af05aa0"
   end
 
   depends_on "go" => :build
