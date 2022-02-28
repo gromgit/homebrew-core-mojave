ae38bcabb3e95b63c1a4a920802d479e77758a7d
Adrian Ho <the.gromgit@gmail.com>
2022-02-28 14:44:34 +0800

    libtorrent-rasterbar: update 2.0.5_1 bottle.

--- Formula/libtorrent-rasterbar.rb
+++ Formula/libtorrent-rasterbar.rb
@@ -1,10 +1,21 @@
 class LibtorrentRasterbar < Formula
   desc "C++ bittorrent library with Python bindings"
   homepage "https://www.libtorrent.org/"
-  url "https://github.com/arvidn/libtorrent/releases/download/v2.0.5/libtorrent-rasterbar-2.0.5.tar.gz"
-  sha256 "e965c2e53170c61c0db3a2d898a61769cb7acd541bbf157cbbef97a185930ea5"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/arvidn/libtorrent.git", branch: "RC_2_0"
+
+  # Remove `stable do` block when patch is no longer needed.
+  stable do
+    url "https://github.com/arvidn/libtorrent/releases/download/v2.0.5/libtorrent-rasterbar-2.0.5.tar.gz"
+    sha256 "e965c2e53170c61c0db3a2d898a61769cb7acd541bbf157cbbef97a185930ea5"
+
+    # Fix build with Boost 1.78. Remove in next release.
+    patch do
+      url "https://github.com/arvidn/libtorrent/commit/71d608fceca7e61c9d124f9ea83f71b06eda3b17.patch?full_index=1"
+      sha256 "20b8e93b67f81af22e50bd668fbeee30147dd85d3ffdff9d624531c32f54e567"
+    end
+  end
 
   livecheck do
     url :stable
@@ -13,7 +24,7 @@ class LibtorrentRasterbar < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libtorrent-rasterbar"
-    sha256 cellar: :any, mojave: "ad07726ca9e06b8f3a5ddd11ee0045648ad9c67a8f1dddba50240cc3e77f7bdd"
+    sha256 cellar: :any, mojave: "07556f0daa1bce4d637b1d4f15dcfdf98364f2eb68f9637da7542efb26572b0e"
   end
 
   depends_on "cmake" => :build
