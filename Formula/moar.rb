9216c52fe27d31645123434d8c63bedad252d7ad
Adrian Ho <the.gromgit@gmail.com>
2022-02-28 15:55:26 +0800

    moar: update 1.8.6 bottle.

--- Formula/moar.rb
+++ Formula/moar.rb
@@ -1,13 +1,13 @@
 class Moar < Formula
   desc "Nice to use pager for humans"
   homepage "https://github.com/walles/moar"
-  url "https://github.com/walles/moar/archive/refs/tags/v1.8.5.tar.gz"
-  sha256 "71095b2c0319b29448e5554054c3fb65a35a1f62537970f482e073cd671354b6"
+  url "https://github.com/walles/moar/archive/refs/tags/v1.8.6.tar.gz"
+  sha256 "d9e44b494e06e7a7f7fc7e079be807c38bc81bfa97a4947252b5a542d499485f"
   license "BSD-2-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/moar"
-    sha256 cellar: :any_skip_relocation, mojave: "e2f62f1961fd3ff664998fe3efe921324a9254ad0b87250c029406b273a83286"
+    sha256 cellar: :any_skip_relocation, mojave: "3b8f4b7b5c2aa317509f434605a6a8a7296147a5dd11c73a6809db519f5a46ba"
   end
 
   depends_on "go" => :build
