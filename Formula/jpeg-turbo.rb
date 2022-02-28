321bbf3aa3a868432eb241bd2b3a57aad6d2dab5
Adrian Ho <the.gromgit@gmail.com>
2022-02-28 12:50:40 +0800

    jpeg-turbo: update 2.1.3 bottle.

--- Formula/jpeg-turbo.rb
+++ Formula/jpeg-turbo.rb
@@ -1,15 +1,14 @@
 class JpegTurbo < Formula
   desc "JPEG image codec that aids compression and decompression"
   homepage "https://www.libjpeg-turbo.org/"
-  url "https://downloads.sourceforge.net/project/libjpeg-turbo/2.1.2/libjpeg-turbo-2.1.2.tar.gz"
-  sha256 "09b96cb8cbff9ea556a9c2d173485fd19488844d55276ed4f42240e1e2073ce5"
+  url "https://downloads.sourceforge.net/project/libjpeg-turbo/2.1.3/libjpeg-turbo-2.1.3.tar.gz"
+  sha256 "467b310903832b033fe56cd37720d1b73a6a3bd0171dbf6ff0b620385f4f76d0"
   license "IJG"
-  head "https://github.com/libjpeg-turbo/libjpeg-turbo.git"
+  head "https://github.com/libjpeg-turbo/libjpeg-turbo.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jpeg-turbo"
-    rebuild 2
-    sha256 mojave: "341b15b76d56da56a0f0ae467bc8b55143a5993a330f0239e2e9a4afbd7a048d"
+    sha256 cellar: :any, mojave: "adfa8b38f891008fcf90fd14db90337a1f7578eef5b672d89cbd4ff9acc04d13"
   end
 
   keg_only "libjpeg-turbo is not linked to prevent conflicts with the standard libjpeg"
