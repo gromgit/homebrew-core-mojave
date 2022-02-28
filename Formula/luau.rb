9ca826e86b18439aa3795d71086423a72a74e8ca
Adrian Ho <the.gromgit@gmail.com>
2022-02-28 15:14:29 +0800

    luau: update 0.516 bottle.

--- Formula/luau.rb
+++ Formula/luau.rb
@@ -1,19 +1,14 @@
 class Luau < Formula
   desc "Fast, safe, gradually typed embeddable scripting language derived from Lua"
   homepage "https://luau-lang.org"
-  # Download from a commit temporarily since we can't untar the tagged release.
-  # This commit is the same as the tagged release but includes a fix that prevents
-  # us from using the release tag. Switch back to a release tag tarball in the next release.
-  # https://github.com/Roblox/luau/issues/373
-  url "https://github.com/Roblox/luau/archive/a9bdce6cc06577cb412c38db757e44ea783f7c05.tar.gz"
-  version "0.515"
-  sha256 "6234ea76f65f7f7eaae2e285dd834dbfe64a58a8aebc4e4681654dd635b35dfa"
+  url "https://github.com/Roblox/luau/archive/0.516.tar.gz"
+  sha256 "b7c7110873b40ac82ba3d8b2a59c090c49194b7850fe48f65850b7ccc1875ac6"
   license "MIT"
   head "https://github.com/Roblox/luau.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/luau"
-    sha256 cellar: :any_skip_relocation, mojave: "24322e12507dc7b29eaacf6081440080df041d6c4220d828bba075b10e29b3b4"
+    sha256 cellar: :any_skip_relocation, mojave: "c8d162cdeb1f9ebf9b313ecfcd99750dbdb2450b83ed350b597e901e992acf2e"
   end
 
   depends_on "cmake" => :build
