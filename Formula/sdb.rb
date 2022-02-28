88ae54a0b5b9d4133de1fd2ca43aaecda800fb28
Adrian Ho <the.gromgit@gmail.com>
2022-03-01 04:21:27 +0800

    sdb: update 1.8.6 bottle.

--- Formula/sdb.rb
+++ Formula/sdb.rb
@@ -1,14 +1,14 @@
 class Sdb < Formula
   desc "Ondisk/memory hashtable based on CDB"
   homepage "https://github.com/radare/sdb"
-  url "https://github.com/radareorg/sdb/archive/1.8.4.tar.gz"
-  sha256 "496a773fdf85c400f00b9b73bc13e4e6588f9293594e959958a17e9b43961b34"
+  url "https://github.com/radareorg/sdb/archive/1.8.6.tar.gz"
+  sha256 "0fbbbb577dec318d79cd110251a6a469460955dc49aa52b6f7796a6a7b478b00"
   license "MIT"
   head "https://github.com/radare/sdb.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sdb"
-    sha256 cellar: :any, mojave: "cbe283c1b23683580fe14bd77c339f8d5a6088f27f28bf1bbc1c07c43735c0ea"
+    sha256 cellar: :any, mojave: "4de9001e6fd001021bb4bb22999bf852b171fac0739d6ccf38035ac8c94948fa"
   end
 
   depends_on "pkg-config" => :build
