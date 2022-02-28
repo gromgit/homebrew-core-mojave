472d3f221a6bedb023fb3a1e8161252ada796639
Adrian Ho <the.gromgit@gmail.com>
2022-03-01 06:58:22 +0800

    waypoint: update 0.7.2 bottle.

--- Formula/waypoint.rb
+++ Formula/waypoint.rb
@@ -1,14 +1,14 @@
 class Waypoint < Formula
   desc "Tool to build, deploy, and release any application on any platform"
   homepage "https://www.waypointproject.io/"
-  url "https://github.com/hashicorp/waypoint/archive/v0.7.1.tar.gz"
-  sha256 "cf62a3ee7e75dd0b923072e6ce61a5920e568b6b6b550ba3184b3ceecb220e1a"
+  url "https://github.com/hashicorp/waypoint/archive/v0.7.2.tar.gz"
+  sha256 "da89019fad31ca583c8c8327c8da00fb4a76c278ff1c53583c842bb827496558"
   license "MPL-2.0"
   head "https://github.com/hashicorp/waypoint.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/waypoint"
-    sha256 cellar: :any_skip_relocation, mojave: "785a541a7f42d03d425ba3bbaba7c043ff40dfc8e46e56dd78f38647c9de4fa7"
+    sha256 cellar: :any_skip_relocation, mojave: "10b532994c88fad770c50e34387d6772d425617ab290d2ac907cb0087e45fa0b"
   end
 
   depends_on "go" => :build
