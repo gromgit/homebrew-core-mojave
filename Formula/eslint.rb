62ec070adcd78336d7d7e8b948eda7682de15382
Adrian Ho <the.gromgit@gmail.com>
2022-02-28 08:06:04 +0800

    eslint: update 8.10.0 bottle.

--- Formula/eslint.rb
+++ Formula/eslint.rb
@@ -3,13 +3,13 @@ require "language/node"
 class Eslint < Formula
   desc "AST-based pattern checker for JavaScript"
   homepage "https://eslint.org"
-  url "https://registry.npmjs.org/eslint/-/eslint-8.9.0.tgz"
-  sha256 "df6cae949ca03a0297d2eebfb82d995aac13de5e7f911e17d36c2864f51e131f"
+  url "https://registry.npmjs.org/eslint/-/eslint-8.10.0.tgz"
+  sha256 "dadd508c13ac5c54ad0f38d37f338c6f86707101915e6dec84f3e4511c96baa5"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/eslint"
-    sha256 cellar: :any_skip_relocation, mojave: "bad777fafef0f0a1a07a955ae38482a43f80e5f0a887bceda30dd81247362f9d"
+    sha256 cellar: :any_skip_relocation, mojave: "b44436db53022ff30c2925b17dd6f06b61e965b61a32fe5db334df9d74c11bb4"
   end
 
   depends_on "node"
