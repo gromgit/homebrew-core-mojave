79bf2ea3f6b1fb40e690d1747422921b8d4c6c84
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 04:28:19 +0800

    codec2: update 1.0.3 bottle.

--- Formula/codec2.rb
+++ Formula/codec2.rb
@@ -2,19 +2,16 @@ class Codec2 < Formula
   desc "Open source speech codec"
   homepage "https://www.rowetel.com/?page_id=452"
   # Linked from https://freedv.org/
-  url "https://github.com/drowe67/codec2/archive/v1.0.1.tar.gz"
-  sha256 "14227963940d79e0ec5af810f37101b30e1c7e8555abd96c56b3c0473abac8ef"
+  url "https://github.com/drowe67/codec2/archive/v1.03.tar.gz"
+  version "1.0.3"
+  sha256 "d1b156035b806fd89a29371a5ab0eefca3ccecfeff303dac0672c59d5c0c1235"
   license "LGPL-2.1-only"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "bf90a6002cc03a2e1fe2716815f091da83e72353bf980fc64cca879cc795552d"
-    sha256 cellar: :any,                 arm64_big_sur:  "dca98080fb9c5738ffcc298547ce0c92a79349b7f04fea8056d968f63c34c1ca"
-    sha256 cellar: :any,                 monterey:       "b74c862b5802d2959ba8397e09bec3601f9290363fb33af1a6d53f9a27f1e1f5"
-    sha256 cellar: :any,                 big_sur:        "5d4162b5b10568f57c326983cbebfe34c126bca31bd14923b0388d8f4ca785aa"
-    sha256 cellar: :any,                 catalina:       "2834225209e520278515857dcada021ba2cc108f92131e8c6cc786070c336bf9"
-    sha256 cellar: :any,                 mojave:         "a86e0264532c78b083ae12358ba569a43588c589c4f91569f620381e30a471b1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "55802c4923f858e36c73a6d4e7488dd5a99e06b103f3e35c014f1e19f232c83d"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/codec2"
+    sha256 cellar: :any, mojave: "1b1569a85392a804a435764240da57b9e938cb8b2f44cf5946185c7e977b3ced"
   end
+
 
   depends_on "cmake" => :build
 
