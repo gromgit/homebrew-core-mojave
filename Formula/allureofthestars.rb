eb3a41010bceca07fbb71f266d2df43d673193eb
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 00:00:22 +0800

    allureofthestars: update 0.11.0.0 bottle.

--- Formula/allureofthestars.rb
+++ Formula/allureofthestars.rb
@@ -1,16 +1,16 @@
 class Allureofthestars < Formula
   desc "Near-future Sci-Fi roguelike and tactical squad combat game"
   homepage "https://www.allureofthestars.com/"
-  url "https://hackage.haskell.org/package/Allure-0.10.3.0/Allure-0.10.3.0.tar.gz"
-  sha256 "0ae3ffbdf8bff2647ed95c2b68073874829b26d7e33231a284a2720cf3414fdc"
+  url "https://hackage.haskell.org/package/Allure-0.11.0.0/Allure-0.11.0.0.tar.gz"
+  sha256 "6125cc585e2a5f28c88855c3c328385c1f21bed093d7606478f1b2af0cb2b6d6"
   license all_of: ["AGPL-3.0-or-later", "GPL-2.0-or-later", "OFL-1.1", "MIT", :cannot_represent]
   head "https://github.com/AllureOfTheStars/Allure.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/allureofthestars"
-    rebuild 2
-    sha256 mojave: "b8fdb9780873ef82b35df96affa9e5679f6ac80399ce2703d0c8affc7f860a6a"
+    sha256 mojave: "0aab87cc28db42afc56aa1b4a0afa683a055c6f847e12f4aa57c39380410cc0c"
   end
+
 
   depends_on "cabal-install" => :build
   depends_on "pkg-config" => :build
