5205ee0254e62e8c5fa735dfac82c6ecf791d205
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 06:21:11 +0800

    dhall-bash: update 1.0.39 bottle.

--- Formula/dhall-bash.rb
+++ Formula/dhall-bash.rb
@@ -4,12 +4,14 @@ class DhallBash < Formula
   url "https://hackage.haskell.org/package/dhall-bash-1.0.39/dhall-bash-1.0.39.tar.gz"
   sha256 "68ce22ada11dcd7d92268b79363bd51c835aecd1f44e8b93ce1e448d5be1c02f"
   license "BSD-3-Clause"
-  head "https://github.com/dhall-lang/dhall-haskell.git"
+  head "https://github.com/dhall-lang/dhall-haskell.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dhall-bash"
-    sha256 cellar: :any_skip_relocation, mojave: "39ec55ee826e40a304c16f21763a2b94a05e411a85d61a6ef6ecca6e4349f44a"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "173853477f069e1dbce6820b25ef6fa92e9071155083a36e5737b0d0a68af009"
   end
+
 
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
