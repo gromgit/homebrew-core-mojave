d0e84ae0266bb0489b290e240bb2dc97f79d181c
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 05:28:01 +0800

    dcd: update 0.13.6 bottle.

--- Formula/dcd.rb
+++ Formula/dcd.rb
@@ -5,15 +5,14 @@ class Dcd < Formula
       tag:      "v0.13.6",
       revision: "02acaa534b9be65142aed7b202a6a8d5524abf2a"
   license "GPL-3.0-or-later"
-  head "https://github.com/dlang-community/dcd.git"
+  head "https://github.com/dlang-community/dcd.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:     "9e223a3fef8a39d75bbec9012bfa534d50772aa2d536857160c39221fe08f5bb"
-    sha256 cellar: :any_skip_relocation, big_sur:      "e97405796485c96ea4dd9f7458cd548ed609f61ed8d5e006fb73ec00072d0811"
-    sha256 cellar: :any_skip_relocation, catalina:     "1455dd1a3d4919d261c6cc8a73d05f62f4436f17b66d8790db249bcf4fbdcc6f"
-    sha256 cellar: :any_skip_relocation, mojave:       "484f3c51a322172c8bab3cfa850685f91ce9dfdcccc85daeedead97cc63f13e1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "58b613aa6a40fb855b592944f97bc7d66cbf87ee30e2243fd4e1551536b02d4f"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dcd"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "dbe17d624c9c3c344ffb972400f218ecb8b99d1a0febfd67fc7e5d057d3c948e"
   end
+
 
   depends_on "dmd" => :build
 
