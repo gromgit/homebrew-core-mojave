8f2464451ea71ab8062fdce1eea9bf9d02f78e23
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 03:45:52 +0800

    calicoctl: update 3.22.0 bottle.

--- Formula/calicoctl.rb
+++ Formula/calicoctl.rb
@@ -2,8 +2,8 @@ class Calicoctl < Formula
   desc "Calico CLI tool"
   homepage "https://www.projectcalico.org"
   url "https://github.com/projectcalico/calico.git",
-      tag:      "v3.21.4",
-      revision: "220d04c9429e8f157caccc70e4ed4c92e1d24e8d"
+      tag:      "v3.22.0",
+      revision: "a86e41d021c4c60e1f73d8dce396fe249d708cc7"
   license "Apache-2.0"
   head "https://github.com/projectcalico/calico.git", branch: "master"
 
@@ -14,8 +14,9 @@ class Calicoctl < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/calicoctl"
-    sha256 cellar: :any_skip_relocation, mojave: "900ba314723280c2bad63be6003b4a78c59ab955e4e18f11a0e8af5c9619799e"
+    sha256 cellar: :any_skip_relocation, mojave: "1d1e6434f7328b72a046416b81f20992fb61b3b15361c229f4486c5ac881bff3"
   end
+
 
   depends_on "go" => :build
 
@@ -28,7 +29,7 @@ class Calicoctl < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/calicoctl version", 1)
+    assert_match version.to_s, shell_output("#{bin}/calicoctl version")
 
     assert_match "invalid configuration: no configuration has been provided",
       shell_output("#{bin}/calicoctl datastore migrate lock 2>&1", 1)
