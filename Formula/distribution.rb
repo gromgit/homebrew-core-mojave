bac2ffe4935046f8480fa689bc999ee7ee15ac16
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 07:24:52 +0800

    distribution: update 1.3 bottle.

--- Formula/distribution.rb
+++ Formula/distribution.rb
@@ -4,11 +4,14 @@ class Distribution < Formula
   url "https://github.com/philovivero/distribution/archive/1.3.tar.gz"
   sha256 "d7f2c9beeee15986d24d8068eb132c0a63c0bd9ace932e724cb38c1e6e54f95d"
   license "GPL-2.0"
-  head "https://github.com/philovivero/distribution.git"
+  head "https://github.com/philovivero/distribution.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "46e6afa7ee3cdc08f4fde478e6235b1df80813391abe507505e1452926d5aff2"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/distribution"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "a1a4c268b4aa613ed47ca0bcf4b5526e534b547bfa49e5a5bdb7d90ed1f1d497"
   end
+
 
   def install
     bin.install "distribution.py" => "distribution"
