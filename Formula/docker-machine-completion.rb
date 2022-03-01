b8741e8a92456820889cd4d58e73c88505694e13
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 07:38:24 +0800

    docker-machine-completion: update 0.16.2 bottle.

--- Formula/docker-machine-completion.rb
+++ Formula/docker-machine-completion.rb
@@ -4,11 +4,14 @@ class DockerMachineCompletion < Formula
   url "https://github.com/docker/machine/archive/v0.16.2.tar.gz"
   sha256 "af8bff768cd1746c787e2f118a3a8af45ed11679404b6e45d5199e343e550059"
   license "Apache-2.0"
-  head "https://github.com/docker/machine.git"
+  head "https://github.com/docker/machine.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "a953e0a6776024c35f839a0f4a23a782e186318fd07fdaa0a8405f41fadbd01a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/docker-machine-completion"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "4a2957964ec5ed559c6ad72523c23cb7b5804f1c96fb77c404e6b38f376656a8"
   end
+
 
   deprecate! date: "2021-09-30", because: :repo_archived
 
