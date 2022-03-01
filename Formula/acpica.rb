5dda994d0d447812aabcc770d57c3e1c459d199a
Adrian Ho <the.gromgit@gmail.com>
2022-03-01 23:32:06 +0800

    acpica: update 20211217 bottle.

--- Formula/acpica.rb
+++ Formula/acpica.rb
@@ -1,8 +1,8 @@
 class Acpica < Formula
   desc "OS-independent implementation of the ACPI specification"
   homepage "https://www.acpica.org/"
-  url "https://acpica.org/sites/acpica/files/acpica-unix-20210930.tar.gz"
-  sha256 "3cd82a281a16bc70c2708665668f138c4fc606c31161d46ce77230454ec04821"
+  url "https://acpica.org/sites/acpica/files/acpica-unix-20211217.tar.gz"
+  sha256 "2511f85828820d747fa3e2c3433d3a38c22db3d9c2fd900e1a84eb4173cb5992"
   license any_of: ["Intel-ACPI", "GPL-2.0-only", "BSD-3-Clause"]
   head "https://github.com/acpica/acpica.git", branch: "master"
 
@@ -12,13 +12,10 @@ class Acpica < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3b872f35d911d1c82a2615898af5afc4770b76e059aa7a2d3bb4a6659b933bc0"
-    sha256 cellar: :any_skip_relocation, monterey:      "26e952c5c45c718c00f2c8d55b1f0024ac14eb171a21d54ee6de9e248782799e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "171ca64d2e427e06fee2f5a3e6deb8e68403c8dcb0b505be088f9c363c592eab"
-    sha256 cellar: :any_skip_relocation, catalina:      "f1e620c13d2fcb5fd8969f32467c26b588ddc70d4866bb8a9e4740e893f19a09"
-    sha256 cellar: :any_skip_relocation, mojave:        "d311e06530b37ddcbdb19f2a7131e0d12fb2c4489905d0a827aa7e8de551fdb1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "75a5c8a82145074588085877162245a1c6703a7bf89fd2577f828f4636462d4d"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/acpica"
+    sha256 cellar: :any_skip_relocation, mojave: "1220cd2bae7696e3c6532ac3c92aafdb8978dd10f536771f5d7fab08a859d9d2"
   end
+
 
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
