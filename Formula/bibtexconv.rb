291b4163ee3e8a97a2a285241e800bb4bcc0ff98
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 02:57:30 +0800

    bibtexconv: update 1.3.2 bottle.

--- Formula/bibtexconv.rb
+++ Formula/bibtexconv.rb
@@ -1,16 +1,16 @@
 class Bibtexconv < Formula
   desc "BibTeX file converter"
   homepage "https://www.uni-due.de/~be0001/bibtexconv/"
-  url "https://github.com/dreibh/bibtexconv/archive/bibtexconv-1.3.1.tar.gz"
-  sha256 "db08bd2b21b137c073ceeb97e5c7f68be4994d3e2af7a8c7dda088ccc0dea9b6"
+  url "https://github.com/dreibh/bibtexconv/archive/bibtexconv-1.3.2.tar.gz"
+  sha256 "6eb1c82a8287ae749ac129d48c241c558881385a792dcc800a30809caf2a2109"
   license "GPL-3.0-or-later"
   head "https://github.com/dreibh/bibtexconv.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bibtexconv"
-    rebuild 2
-    sha256 cellar: :any, mojave: "da8027b368334a97b71166ad7e00e0620549beff110a3f98d046fbe30f06e254"
+    sha256 cellar: :any, mojave: "0759cb8bdda51467511a47606077375b62939d45e67a4994a3bc07903a593d2e"
   end
+
 
   depends_on "bison" => :build
   depends_on "cmake" => :build
