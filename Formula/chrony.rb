f5e35da4a9b67473b0b596e52bcb7535dbd8dff9
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 03:59:41 +0800

    chrony: update 4.2 bottle.

--- Formula/chrony.rb
+++ Formula/chrony.rb
@@ -1,8 +1,8 @@
 class Chrony < Formula
   desc "Versatile implementation of the Network Time Protocol (NTP)"
   homepage "https://chrony.tuxfamily.org"
-  url "https://download.tuxfamily.org/chrony/chrony-4.1.tar.gz"
-  sha256 "ed76f2d3f9347ac6221a91ad4bd553dd0565ac188cd7490d0801d08f7171164c"
+  url "https://download.tuxfamily.org/chrony/chrony-4.2.tar.gz"
+  sha256 "273f9fd15c328ed6f3a5f6ba6baec35a421a34a73bb725605329b1712048db9a"
   license "GPL-2.0-only"
 
   livecheck do
@@ -11,14 +11,10 @@ class Chrony < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "089aa389bea9d6408b8e3f8e681af3648b37b2d5a6245fcebbe15fbf0c2dea93"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "90fcce18cd0b68629e37023124cb961ac67c2f36da2bdfaeeeb2f6b146a49b9c"
-    sha256 cellar: :any_skip_relocation, monterey:       "4c45d8bb188a7b081c14ad95e166ed201690104817b09385b3a1d3c2ecbd7c07"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ecba024c1c74b0f2d8094b06043a6ceafbc4bcd4cba944e3600792789adcfb4b"
-    sha256 cellar: :any_skip_relocation, catalina:       "dfe728a9f5ecc085ba6582a2791e1e6a2f5d2ef609ee4f3da237e4442d016dbe"
-    sha256 cellar: :any_skip_relocation, mojave:         "b908fae19168cb50d7d1fe33e2885ad19ff8bb1e190442aa3ee6ea6c47f4c72f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d7f08010b437e23b4debb9f0d278adf3eb84972a6810f2cb6daa0b81077084cc"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/chrony"
+    sha256 cellar: :any_skip_relocation, mojave: "a17ec5432bbe426d1b615ef506ad0950b7a26018b033abc77ea9b6773fa41966"
   end
+
 
   depends_on "nettle"
 
