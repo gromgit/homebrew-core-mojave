0494f46ed9c0dd3053c85f999f7d1262d815984a
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 04:30:08 +0800

    cpanminus: update 1.9019 bottle.

--- Formula/cpanminus.rb
+++ Formula/cpanminus.rb
@@ -3,8 +3,7 @@ class Cpanminus < Formula
   homepage "https://github.com/miyagawa/cpanminus"
   url "https://github.com/miyagawa/cpanminus/archive/1.9019.tar.gz"
   sha256 "d0a37547a3c4b6dbd3806e194cd6cf4632158ebed44d740ac023e0739538fb46"
-  # dual licensed same as perl (GPL-1.0 or Artistic-1.0)
-  license "GPL-1.0"
+  license any_of: ["Artistic-1.0-Perl", "GPL-1.0-or-later"]
   head "https://github.com/miyagawa/cpanminus.git", branch: "devel"
 
   livecheck do
@@ -13,14 +12,11 @@ class Cpanminus < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c279cbdfa489e4b0be72e46a8e325fc4f8070ff83c7d78026e7b7c5831e3678d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "78c0fc8d2fcb14cb94e62d972f06ae6a1762846471eb5d0669909121c24fa08d"
-    sha256 cellar: :any_skip_relocation, monterey:       "7859de46dbe67b3c9375bd8e3de6519d75f3c69a8d3698d3bb22d6163452ab39"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6a9b5bde63d8c5860788c67470c9dffcfe12036d38e331ad4c5028455ad45a79"
-    sha256 cellar: :any_skip_relocation, catalina:       "6a9b5bde63d8c5860788c67470c9dffcfe12036d38e331ad4c5028455ad45a79"
-    sha256 cellar: :any_skip_relocation, mojave:         "6a9b5bde63d8c5860788c67470c9dffcfe12036d38e331ad4c5028455ad45a79"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "78c0fc8d2fcb14cb94e62d972f06ae6a1762846471eb5d0669909121c24fa08d"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cpanminus"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "3441a262c18df83c13a619b171e16b227fee2d7b6c8d4fb5928e6724218211ac"
   end
+
 
   def install
     cd "App-cpanminus" do
