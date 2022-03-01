9afd60dcf44ed2b21e4655abee93e481d160a172
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 06:52:39 +0800

    dhall-lsp-server: update 1.0.17 bottle.

--- Formula/dhall-lsp-server.rb
+++ Formula/dhall-lsp-server.rb
@@ -4,12 +4,14 @@ class DhallLspServer < Formula
   url "https://hackage.haskell.org/package/dhall-lsp-server-1.0.17/dhall-lsp-server-1.0.17.tar.gz"
   sha256 "88433b4334d75c625d76b61859359b31e173531f11bb858ea4776eed46949c40"
   license "BSD-3-Clause"
-  head "https://github.com/dhall-lang/dhall-haskell.git"
+  head "https://github.com/dhall-lang/dhall-haskell.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dhall-lsp-server"
-    sha256 cellar: :any_skip_relocation, mojave: "7e6a7b3f772b0a0a23bcbc35de162dd68e231dc4ccc1ee7f94676b3d345bc05c"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "478a9454f3500bd7af854c09aff0649a8262fd08bbb686f9cd8e94e3518c6bde"
   end
+
 
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
