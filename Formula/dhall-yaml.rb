8c0bebf5e252823fe36fbe53eee6ad45dcdac734
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 07:08:18 +0800

    dhall-yaml: update 1.2.9 bottle.

--- Formula/dhall-yaml.rb
+++ Formula/dhall-yaml.rb
@@ -4,12 +4,14 @@ class DhallYaml < Formula
   url "https://hackage.haskell.org/package/dhall-yaml-1.2.9/dhall-yaml-1.2.9.tar.gz"
   sha256 "8637b4e78b60a9318d17ffe99a45a9931886e0a4f5e99922d2b246187196c93e"
   license "BSD-3-Clause"
-  head "https://github.com/dhall-lang/dhall-haskell.git"
+  head "https://github.com/dhall-lang/dhall-haskell.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dhall-yaml"
-    sha256 cellar: :any_skip_relocation, mojave: "1ddfd76d6647b320bfc2215e113982b376e70b10abbd20a3f403d47917eb2e5e"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "d87216f6995d2207410681fd86284530bcb23b6475fbb79ac65d529e34d89336"
   end
+
 
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
