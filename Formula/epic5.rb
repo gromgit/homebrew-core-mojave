ea537871ec7ba293d4b9c31f9de2433f6afcfdd1
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 09:42:41 +0800

    epic5: update 2.1.6 bottle.

--- Formula/epic5.rb
+++ Formula/epic5.rb
@@ -5,7 +5,7 @@ class Epic5 < Formula
   mirror "https://www.mirrorservice.org/sites/distfiles.macports.org/epic5/epic5-2.1.6.tar.xz"
   sha256 "84d59cc22cb20ffac9cbea6c97b35d1d7e4993e7b1221fa5e82bcb0f03b9066d"
   license "BSD-3-Clause"
-  head "http://git.epicsol.org/epic5.git"
+  head "http://git.epicsol.org/epic5.git", branch: "master"
 
   livecheck do
     url "http://ftp.epicsol.org/pub/epic/EPIC5-PRODUCTION/"
@@ -13,13 +13,11 @@ class Epic5 < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "a840f83399674d45b45c29bd41cbaf6a901779e33226c83cc4d71e08cbc93ec6"
-    sha256 arm64_big_sur:  "5139ccc0ee27332891de4634edccab4b74d39c0a65b9bac8034c4f76ae8dc8be"
-    sha256 monterey:       "e91fcbf5d7cc3af521719f0172836dab56634f0f7a8c52854343d38151e11f04"
-    sha256 big_sur:        "e34de29881ed577de6ffbc5fdec471f59707c4e6a91bce1f453656d93612c37f"
-    sha256 catalina:       "882c365f9c32d24c729464e83b399a35d4620944bffc5732cc21bff52751836f"
-    sha256 mojave:         "1ddec44e5b0af07dd5a434ebd913c7139a3270cb8bf6a0807eafca9795aa5dc0"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/epic5"
+    rebuild 1
+    sha256 mojave: "0964f73d26e4acfc472bc6c9c51911b67e7c4c944486b00224d414a9a42f49eb"
   end
+
 
   depends_on "openssl@1.1"
 
