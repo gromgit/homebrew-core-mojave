2867b06f9ee6953d15478f38a1a7fd896d9064fa
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 08:33:52 +0800

    doubledown: update 0.0.2 bottle.

--- Formula/doubledown.rb
+++ Formula/doubledown.rb
@@ -4,11 +4,14 @@ class Doubledown < Formula
   url "https://github.com/devstructure/doubledown/archive/v0.0.2.tar.gz"
   sha256 "47ff56b6197c5302a29ae4a373663229d3b396fd54d132adbf9f499172caeb71"
   license "BSD-2-Clause"
-  head "https://github.com/devstructure/doubledown.git"
+  head "https://github.com/devstructure/doubledown.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "78f3d65bdfb3d5184fc3a10c8b161e9e41a4d14715c284d2a179ebf5fbe52209"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/doubledown"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "1064ac05c9bfcf12e9b16120fef190e074863c78449028fa46b9ef324d5609bf"
   end
+
 
   def install
     bin.install Dir["bin/*"]
