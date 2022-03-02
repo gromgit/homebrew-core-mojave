641f0f702bf0b8a7c427639bb7410c0f4037dc4f
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-02 15:24:09 +0800

    cassandra@2.2: no rebuilding required

--- Formula/cassandra@2.2.rb
+++ Formula/cassandra@2.2.rb
@@ -14,6 +14,8 @@ class CassandraAT22 < Formula
 
   keg_only :versioned_formula
 
+  deprecate! date: "2022-03-01", because: :unsupported
+
   depends_on "cython" => :build
   depends_on arch: :x86_64 # openjdk@8 is not supported on ARM
   depends_on :macos # Due to Python 2 (does not support Python 3)
