ebb84929fe8c3a092cac227b8414737f50682629
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 09:33:16 +0800

    emacs-dracula: update 1.7.0 bottle.

--- Formula/emacs-dracula.rb
+++ Formula/emacs-dracula.rb
@@ -4,11 +4,14 @@ class EmacsDracula < Formula
   url "https://github.com/dracula/emacs/archive/v1.7.0.tar.gz"
   sha256 "dbbcc968cf8187a8ada9f040137ba03dc0e51b285e96e128d26cea05cf470330"
   license "MIT"
-  head "https://github.com/dracula/emacs.git"
+  head "https://github.com/dracula/emacs.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "369a09be422633643adec10ee34131689f9c5049615347121bd218654829241c"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/emacs-dracula"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "aa40dba91ecffa781c3ebb2f6718c6622534bc9ce03f496eec07eb669d29de97"
   end
+
 
   depends_on "emacs"
 
