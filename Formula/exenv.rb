b99c6c9c7e1ed2aa9205b8045f58ae9ac4159fcd
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 10:39:21 +0800

    exenv: update 0.1.0 bottle.

--- Formula/exenv.rb
+++ Formula/exenv.rb
@@ -4,11 +4,14 @@ class Exenv < Formula
   url "https://github.com/mururu/exenv/archive/v0.1.0.tar.gz"
   sha256 "368095760ecc386a0930954f5f0ce7cea977641fe6d27b1beff032f512598a58"
   license "MIT"
-  head "https://github.com/mururu/exenv.git"
+  head "https://github.com/mururu/exenv.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "ae3d33c35709202895d8d27bff0ea95075cd1455cc20207b139c43b73ca34322"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/exenv"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "884ff3e962b5cc0cedde06e4b836ffc9c457464f10c8d92e70b6341b0240599c"
   end
+
 
   def install
     inreplace "libexec/exenv", "/usr/local", HOMEBREW_PREFIX
