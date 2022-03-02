ae17e3951a45a371eb94bcb54d67978725b6ba24
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 09:02:10 +0800

    duti: update 1.5.4_1 bottle.

--- Formula/duti.rb
+++ Formula/duti.rb
@@ -5,7 +5,7 @@ class Duti < Formula
   sha256 "3f8f599899a0c3b85549190417e4433502f97e332ce96cd8fa95c0a9adbe56de"
   license :public_domain
   revision 1
-  head "https://github.com/moretension/duti.git"
+  head "https://github.com/moretension/duti.git", branch: "master"
 
   livecheck do
     url :stable
@@ -13,14 +13,11 @@ class Duti < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ff0b636a4b54edf70502d88e26b04281ee3d25cea64fea0b2ea8421b5fe26f47"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f7ba0f6fd9de2bd9f853883027edcb480b7cf12c7a2076aa87226efc239d5578"
-    sha256 cellar: :any_skip_relocation, monterey:       "417b9103060e5a555852dcb76a2b921359ef9153a532cc27e8dda0b74c9cc7c2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d2e0ccdd5be1c9f272787bc96ed419df33ef4199b47bb8a0762af596d3fc442b"
-    sha256 cellar: :any_skip_relocation, catalina:       "5fe04375afd229149721ce8a0cd66fe7a372fa5a5dce11084616d2a979aa47fe"
-    sha256 cellar: :any_skip_relocation, mojave:         "ffb23db168b014703e505ef2d76d7bc431efd5d4d1244833d9b2ddf5723133a6"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "e495d02894655b516f79fa10671f5d768cae04c5b73c1aa077f8b0c573584cbf"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/duti"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "443a1354fff7ff14fae0e74fd64805815167259e19b90962ee3561da1e4f1028"
   end
+
 
   depends_on "autoconf" => :build
   depends_on :macos
