bf4848b523eb4c6b7058f8648b0d270a5697cfdd
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 09:12:57 +0800

    ehco: update 1.1.1 bottle.

--- Formula/ehco.rb
+++ Formula/ehco.rb
@@ -1,8 +1,9 @@
 class Ehco < Formula
   desc "Network relay tool and a typo :)"
   homepage "https://github.com/Ehco1996/ehco"
-  url "https://github.com/Ehco1996/ehco/archive/refs/tags/v1.0.7.tar.gz"
-  sha256 "d6384cdb31befcf9a198dd1be6c0073e9fabb6823f2f92df63bb50fb14ec6ff1"
+  url "https://github.com/Ehco1996/ehco.git",
+      tag:      "v1.1.1",
+      revision: "c723fa0c3fefcc7f89c3847c6cd753cfdaf30486"
   license "GPL-3.0-only"
 
   livecheck do
@@ -11,37 +12,40 @@ class Ehco < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "58b3a1bcdc7f9aadc08965c587b3d6fa877075d62b639c90f94a0016a94ed657"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bc4c8b91493e822d5068ae6d02068f5c5e7790d8d1b776b1fb1bea9a077c4fbb"
-    sha256 cellar: :any_skip_relocation, monterey:       "1cedf54dd057b2341cd5d553427dc7109ef6b07fd028d5739c6ddde38d8f999a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bbfc3978b9a69b1f381ad64231606dfaeef27f8d3d9e9b433ecc879828ea0280"
-    sha256 cellar: :any_skip_relocation, catalina:       "039a8b48a139e971b5c8587ed75fb4f44fd4b117f824157e2cc50f85ac08ef33"
-    sha256 cellar: :any_skip_relocation, mojave:         "a790f96ce6704e4cc4d0fcc3e962ae8d6d79711d8b72bb2eb788d37db9b73b8b"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ehco"
+    sha256 cellar: :any_skip_relocation, mojave: "c8ee71c81cb9f6cbf3df53d82b258f42fd5343dbabdc095f1093cac335ec94a3"
   end
+
 
   depends_on "go" => :build
 
+  uses_from_macos "netcat" => :test
+
   def install
-    system "go", "build", *std_go_args, "cmd/ehco/main.go"
+    ldflags = %W[
+      -s -w
+      -X github.com/Ehco1996/ehco/internal/constant.GitBranch=master
+      -X github.com/Ehco1996/ehco/internal/constant.GitRevision=#{Utils.git_short_head}
+      -X github.com/Ehco1996/ehco/internal/constant.BuildTime=#{time.iso8601}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "cmd/ehco/main.go"
   end
 
   test do
-    version_info = shell_output("#{bin}/ehco -v")
-    assert_match "ehco version #{version}", version_info
+    version_info = shell_output("#{bin}/ehco -v 2>&1")
+    assert_match "Version=#{version}", version_info
 
     # run nc server
     nc_port = free_port
-    nc_pid = spawn "nc", "-l", nc_port.to_s
+    spawn "nc", "-l", nc_port.to_s
     sleep 1
 
     # run ehco server
     listen_port = free_port
-    ehco_pid = spawn bin/"ehco", "-l", "localhost:#{listen_port}", "-r", "localhost:#{nc_port}"
+    spawn bin/"ehco", "-l", "localhost:#{listen_port}", "-r", "localhost:#{nc_port}"
     sleep 1
 
     system "nc", "-z", "localhost", listen_port.to_s
-  ensure
-    Process.kill("HUP", ehco_pid)
-    Process.kill("HUP", nc_pid)
   end
 end
