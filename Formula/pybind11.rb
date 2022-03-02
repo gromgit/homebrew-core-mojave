b8b07cd5c304d8c821835df93b3c2e7c50e75a31
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-02 15:24:20 +0800

    pybind11: no rebuilding required

--- Formula/pybind11.rb
+++ Formula/pybind11.rb
@@ -1,8 +1,8 @@
 class Pybind11 < Formula
   desc "Seamless operability between C++11 and Python"
   homepage "https://github.com/pybind/pybind11"
-  url "https://github.com/pybind/pybind11/archive/v2.9.0.tar.gz"
-  sha256 "057fb68dafd972bc13afb855f3b0d8cf0fa1a78ef053e815d9af79be7ff567cb"
+  url "https://github.com/pybind/pybind11/archive/v2.9.1.tar.gz"
+  sha256 "c6160321dc98e6e1184cc791fbeadd2907bb4a0ce0e447f2ea4ff8ab56550913"
   license "BSD-3-Clause"
 
   livecheck do
@@ -11,7 +11,7 @@ class Pybind11 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "52ad809edc7bc1053f9999efc63dc7d2743efb3a4e6e93edd96049d8b54363d8"
+    sha256 cellar: :any_skip_relocation, all: "2690279a53d22445015e97cf0365a620e04bbf6fad8fda08fddf84ed9c73669f"
   end
 
   depends_on "cmake" => :build
@@ -34,10 +34,11 @@ class Pybind11 < Formula
 
     pythons.each do |python|
       # Install Python package too
-      system python.opt_bin/"python3", *Language::Python.setup_install_args(libexec)
+      site_packages = Language::Python.site_packages python.opt_bin/"python3"
+      system python.opt_bin/"python3", *Language::Python.setup_install_args(libexec),
+                                       "--install-lib=#{libexec/site_packages}"
 
       pyversion = Language::Python.major_minor_version python.opt_bin/"python3"
-      site_packages = Language::Python.site_packages python.opt_bin/"python3"
       pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
       (prefix/site_packages/"homebrew-pybind11.pth").write pth_contents
 
