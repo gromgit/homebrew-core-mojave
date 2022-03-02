af4ee3671ae193d04f79257a018bd46a19c62a28
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-02 15:24:26 +0800

    xkeyboardconfig: no rebuilding required

--- Formula/xkeyboardconfig.rb
+++ Formula/xkeyboardconfig.rb
@@ -1,45 +1,35 @@
 class Xkeyboardconfig < Formula
   desc "Keyboard configuration database for the X Window System"
   homepage "https://www.freedesktop.org/wiki/Software/XKeyboardConfig/"
-  url "https://xorg.freedesktop.org/archive/individual/data/xkeyboard-config/xkeyboard-config-2.34.tar.bz2"
-  sha256 "b321d27686ee7e6610ffe7b56e28d5bbf60625a1f595124cd320c0caa717b8ce"
+  url "https://xorg.freedesktop.org/archive/individual/data/xkeyboard-config/xkeyboard-config-2.35.1.tar.xz"
+  sha256 "18ce50ff0c74ae6093062bce1aeab3d363913ea35162fe271f8a0ce399de85cc"
   license "MIT"
-  head "https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config.git"
+  head "https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/xkeyboardconfig"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "0e38b424812f6c9a373e850a76fadb56e6fc9a0545885fa5ac5869f064c4decf"
+    sha256 cellar: :any_skip_relocation, all: "b75845c115895b723228026a66e259049cee436a805f3013fa1b22966c3fcbc1"
   end
 
   depends_on "gettext" => :build
-  depends_on "intltool" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => [:build, :test]
   depends_on "python@3.10" => :build
+
   uses_from_macos "libxslt" => :build
 
   def install
-    # Needed by intltool (xml::parser)
-    ENV.prepend_path "PERL5LIB", "#{Formula["intltool"].libexec}/lib/perl5"
-
-    args = %W[
-      --prefix=#{prefix}
-      --sysconfdir=#{etc}
-      --localstatedir=#{var}
-      --disable-dependency-tracking
-      --disable-silent-rules
-      --with-xkb-rules-symlink=xorg
-      --disable-runtime-deps
-    ]
-
-    system "./configure", *args
-    system "make"
-    system "make", "install"
+    mkdir "build" do
+      system "meson", *std_meson_args, ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
   end
 
   test do
     assert_predicate man7/"xkeyboard-config.7", :exist?
     assert_equal "#{share}/X11/xkb", shell_output("pkg-config --variable=xkb_base xkeyboard-config").chomp
-    assert_match "Language: en_GB", shell_output("strings #{share}/locale/en_GB/LC_MESSAGES/xkeyboard-config.mo")
+    assert_match "Language-Team: English",
+      shell_output("strings #{share}/locale/en_GB/LC_MESSAGES/xkeyboard-config.mo")
   end
 end
