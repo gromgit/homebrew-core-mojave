64a382197cb63472323cb7c98cb318cbb435e31b
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 11:52:43 +0800

    flatcc: update 0.6.0 bottle.

--- Formula/flatcc.rb
+++ Formula/flatcc.rb
@@ -4,18 +4,14 @@ class Flatcc < Formula
   url "https://github.com/dvidelabs/flatcc/archive/v0.6.0.tar.gz"
   sha256 "a92da3566d11e19bb807a83554b1a2c644a5bd91c9d9b088514456bb56e1c666"
   license "Apache-2.0"
-  head "https://github.com/dvidelabs/flatcc.git"
+  head "https://github.com/dvidelabs/flatcc.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e3136d54add516459ec378f5c7a4f40fb666f6cbd396d96705c0dc55e8da8366"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3a3729b2b5856179197d13c36f684f08e4f4537a2dc6f7e77e49bbd826c86323"
-    sha256 cellar: :any_skip_relocation, monterey:       "1533758578c80af5bf008e5f1c1eb41c1a51a89bf54b1c8974a28b496e1821f7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ccc4f35c551b04bbca5e4b57b1ffb71941b9105a99a1dac2092a56d1e0f2034c"
-    sha256 cellar: :any_skip_relocation, catalina:       "f1f1cd7532305f48b008fb1f3687d9369a88f184902a8992fd77789410841b4e"
-    sha256 cellar: :any_skip_relocation, mojave:         "74651142d2c732bfbe671d78cdcf3357189c9b3d3cb7078bf9e882ad9ca6b053"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "9e37511cd8069fb56ff15e72f0184d1908f9d4948ebd57d2b430c760cac60aa6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "53fd5d3dc766086d11301fcd9fed896b1ee7847e43c173999d8cd7443c402e45"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/flatcc"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "f878a5869dd8010e6af04c7015538ad02457342bf8413e3271a900272cf2cc11"
   end
+
 
   depends_on "cmake" => :build
 
