434d65282bbf963d3f9fec9a1414fad22ec203d7
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-02 15:24:24 +0800

    tomcat@8: no rebuilding required

--- Formula/tomcat@8.rb
+++ Formula/tomcat@8.rb
@@ -1,9 +1,9 @@
 class TomcatAT8 < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-8/v8.5.75/bin/apache-tomcat-8.5.75.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.75/bin/apache-tomcat-8.5.75.tar.gz"
-  sha256 "22c538f628ed03bd15b73951c73b6124a43e5148e8c6ba14cd890f16493a36d8"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-8/v8.5.76/bin/apache-tomcat-8.5.76.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.76/bin/apache-tomcat-8.5.76.tar.gz"
+  sha256 "41942b6e0f216649ebc129301f62d3ef11c20e5230b333db58517ee306536e5e"
   license "Apache-2.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class TomcatAT8 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "7381167a104491054ec376235d45a855e0b9df62515be955219d95bf4e1caf15"
+    sha256 cellar: :any_skip_relocation, all: "3b09b4201d952f00df66fde0bc1fa27c7515aad259e39710e6151f927db40d73"
   end
 
   keg_only :versioned_formula
