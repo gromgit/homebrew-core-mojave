a27efa69ffa89d999fd00d9675b840509ed3ce13
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 08:30:42 +0800

    dog: update 0.1.0 bottle.

--- Formula/dog.rb
+++ Formula/dog.rb
@@ -4,18 +4,14 @@ class Dog < Formula
   url "https://github.com/ogham/dog/archive/v0.1.0.tar.gz"
   sha256 "82387d38727bac7fcdb080970e84b36de80bfe7923ce83f993a77d9ac7847858"
   license "EUPL-1.2"
-  head "https://github.com/ogham/dog.git"
+  head "https://github.com/ogham/dog.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4f90bf064348fb099a5ecc0de69a6ef9cee7b45583d2cdaeb844214d5e773f17"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9ea5dead4f4fbe142d855e32dbb2a01a311836bdbdec746c62eb21e1c5a8b159"
-    sha256 cellar: :any_skip_relocation, monterey:       "fd49c3b016a17db8514b585a226c658aa109a7e2bf96023f4a194deb7501c70a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0666eb89b99b25ee0c5b2064f2a7a2781a325aded61691b707c6e93c512c652f"
-    sha256 cellar: :any_skip_relocation, catalina:       "21f901c6abb536f0a7bc00d97df6e561aec1acff1759a96a5f69bd51eac1da98"
-    sha256 cellar: :any_skip_relocation, mojave:         "095b6dbb23b96504a6f0e0a292a7157c78e900825e92f4c140cc29643a294e29"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "fef7ea4d925e230ba9e75cb051a888263c2bc10fcf26a96899cea2cc8891a302"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9ab73546a4ee11f186379ac21479d7e1ce052791f2840c54a43d5ed67281e500"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dog"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "381602ce73d1eec082a70017ed4dd6b325eecdeeaefa7b051be2f438ef0fb13b"
   end
+
 
   depends_on "just" => :build
   depends_on "pandoc" => :build
