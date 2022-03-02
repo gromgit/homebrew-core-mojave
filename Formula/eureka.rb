ffcb501313c90b40ab55613139dc4780fddff20a
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 10:22:40 +0800

    eureka: update 1.8.1 bottle.

--- Formula/eureka.rb
+++ Formula/eureka.rb
@@ -4,18 +4,14 @@ class Eureka < Formula
   url "https://github.com/simeg/eureka/archive/v1.8.1.tar.gz"
   sha256 "d10d412c71dea51b4973c3ded5de1503a4c5de8751be5050de989ac08eb0455e"
   license "MIT"
-  head "https://github.com/simeg/eureka.git"
+  head "https://github.com/simeg/eureka.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "d7f5807419b0fafbbe4aaed2140567ec9fbd7cf4ad96c7da03f47638d8ad4109"
-    sha256 cellar: :any,                 arm64_big_sur:  "528bd9719bd743bbe9e496ca2c856983c8acdf89fbf4364a6a1a349c8560aff1"
-    sha256 cellar: :any,                 monterey:       "fc90de8c735e615872b18000a8af2ce4b5c9c30ea135d674dddae304c1955205"
-    sha256 cellar: :any,                 big_sur:        "3447e5073ee6dd2026b39fcc2dac86465806fa57a263127b92d672a142efcbfe"
-    sha256 cellar: :any,                 catalina:       "616a745506e35e1ab0a7645d7d122f204cbf94e292d2fc56958795a056af0a9a"
-    sha256 cellar: :any,                 mojave:         "998ed401c748a0916768a566ccf44e4295c56cd6f23a90b156087588f23e7e2c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee46d57066b0a55284dcf9561ec1104133aa58bb3f1900bec1dbb2b94d14486a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/eureka"
+    rebuild 2
+    sha256 cellar: :any, mojave: "09c3589b021ae34f5de8eedf8e23b98c4a8cb64f32beaf77b74be390e2dd1d87"
   end
+
 
   depends_on "rust" => :build
   depends_on "openssl@1.1"
