45b41b1d4cf48037bd5a9009a37eac9d88a100de
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 10:51:26 +0800

    f3: update 8.0 bottle.

--- Formula/f3.rb
+++ Formula/f3.rb
@@ -4,17 +4,14 @@ class F3 < Formula
   url "https://github.com/AltraMayor/f3/archive/v8.0.tar.gz"
   sha256 "fb5e0f3b0e0b0bff2089a4ea6af53278804dfe0b87992499131445732e311ab4"
   license "GPL-3.0-only"
-  head "https://github.com/AltraMayor/f3.git"
+  head "https://github.com/AltraMayor/f3.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2b2b9129b2048339b1087ae700292d8d323cfa0f15cbc003c05fd30e7b5e051f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dd2510a756c154a088d7b1b10c532a1cd85f4699f52dabb09a340dca20a79d04"
-    sha256 cellar: :any_skip_relocation, monterey:       "c4562c2607b59802e798e69a28f3c455f056304b8f731b097b0ac6d586d02b41"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ecef018e922b79da5bed5e91df0e8798c9240d527af23b6d5a78c65fed0f4e99"
-    sha256 cellar: :any_skip_relocation, catalina:       "198f9b2d578a294fb61e2b9203cc1285c9c0a2fa6048fb1e34130f7d8a2039ff"
-    sha256 cellar: :any_skip_relocation, mojave:         "70d5966a5afb44fe91225d81f54adf80cd7b254ac9253423234d4c99d4a2435d"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "55da39f3758797df44426b7744542b4322ddc84b20fc7a5664e2da672cef0d1a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/f3"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "de5ef4775ab308e250184428fca65d8c3a3443d2a0ede7620f9b506dfc2b160d"
   end
+
 
   depends_on "argp-standalone"
 
