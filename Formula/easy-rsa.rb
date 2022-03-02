56d1ae956eddc32cccb2cc78397be44368614bc8
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 09:05:18 +0800

    easy-rsa: update 3.0.8 bottle.

--- Formula/easy-rsa.rb
+++ Formula/easy-rsa.rb
@@ -4,17 +4,14 @@ class EasyRsa < Formula
   url "https://github.com/OpenVPN/easy-rsa/archive/v3.0.8.tar.gz"
   sha256 "fd6b67d867c3b8afd53efa2ca015477f6658a02323e1799432083472ac0dd200"
   license "GPL-2.0-only"
-  head "https://github.com/OpenVPN/easy-rsa.git"
+  head "https://github.com/OpenVPN/easy-rsa.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f9267df5fe6f53503330444514b391a5e35892a3f5f3e381b1016f9402d1d7fc"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5124cc17f73f7467de387d62658658d29907e43e9e50fd25dac4f1c5ec55915b"
-    sha256 cellar: :any_skip_relocation, catalina:      "f8fb06de036f9b9d1b5483054b967c8b0ba61f7617f40c7b2d8443c87b3b54a9"
-    sha256 cellar: :any_skip_relocation, mojave:        "a9e8dd8d94adc330b85d60b74380987bd680103c2b4cac61d407eca7b272174b"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "0e814810990e326f9b20a416d684a05261c6e4b68cd9d092a09898fe50077fa0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eb9ed69d68b06a1af68732fcb9722e2016b562b3196d3017416805c81f5f977a"
-    sha256 cellar: :any_skip_relocation, all:           "eb9ed69d68b06a1af68732fcb9722e2016b562b3196d3017416805c81f5f977a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/easy-rsa"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "0070e859af9f304380faddc19cba663e93e23e74579cb8c6798f2e1f6ccc6f2e"
   end
+
 
   depends_on "openssl@1.1"
 
