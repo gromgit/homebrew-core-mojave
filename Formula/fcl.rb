77be37d06ce596051ed2afa014b9db7fd61f7209
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 15:20:58 +0800

    fcl: update 0.7.0 bottle.

--- Formula/fcl.rb
+++ Formula/fcl.rb
@@ -4,17 +4,14 @@ class Fcl < Formula
   url "https://github.com/flexible-collision-library/fcl/archive/0.7.0.tar.gz"
   sha256 "90409e940b24045987506a6b239424a4222e2daf648c86dd146cbcb692ebdcbc"
   license "BSD-3-Clause"
-  head "https://github.com/flexible-collision-library/fcl.git"
+  head "https://github.com/flexible-collision-library/fcl.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "15b9e1b7c5166ad222c94e848259726896f59187b4a31c90671f4bdd00547591"
-    sha256 cellar: :any,                 arm64_big_sur:  "d2054fa4adf473a736ee945e0a1817772a77b423c1fbd314f7ced016ad616760"
-    sha256 cellar: :any,                 monterey:       "b79ff81383e9aa9de0a692f11d287365751a9610395e99b0acaff1eae9a8f4c8"
-    sha256 cellar: :any,                 big_sur:        "211e57fdf880e25e2441be78170dd1433d746aee1bb9a98990479abc2037a546"
-    sha256 cellar: :any,                 catalina:       "2bac88b84304ad8de56bbf604f7466ee147c31e04484df02ff45ef4aa9c966cc"
-    sha256 cellar: :any,                 mojave:         "095c6887a9007ec034751d8326fa7f5052180b78605d5df92223e081a17cad3c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7b0a2f8116b46c0f78f86c850ca6a808282bbc8dc6a7768d716faef891ce64bf"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fcl"
+    rebuild 1
+    sha256 cellar: :any, mojave: "6bab659f517de6ab5fcf0604337a9ff03707dc629fb495c578ab477332715ae3"
   end
+
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
