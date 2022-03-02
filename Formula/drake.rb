e754f98a65efc12f165351f5d24d4e20032f95ef
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 08:40:20 +0800

    drake: update 1.0.3 bottle.

--- Formula/drake.rb
+++ Formula/drake.rb
@@ -4,13 +4,14 @@ class Drake < Formula
   url "https://raw.githubusercontent.com/Factual/drake/1.0.3/bin/drake-pkg"
   sha256 "adeb0bb14dbe39789273c5c766da9a019870f2a491ba1f0c8c328bd9a95711cc"
   license "EPL-1.0"
-  head "https://github.com/Factual/drake.git"
+  head "https://github.com/Factual/drake.git", branch: "develop"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "6496f23e1c0b226eefaff9b6866943f55c6ae07539b0f9f640a5dcb1ee7bfb3f"
-    sha256 cellar: :any_skip_relocation, catalina: "0bba821e663244edaa7e34dbd5fa1d5a2f4bbb7aff22b641babca781543a40a6"
-    sha256 cellar: :any_skip_relocation, mojave:   "631e9bb8583c3d54d4aba7fc0d60e59cd5cf2c815679019be8f0f48872fe7363"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/drake"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "cfe22cd2892ea6b536d21c1454b42fcbf1e36e843f18be3e39683cdfef456819"
   end
+
 
   depends_on arch: :x86_64 # openjdk@8 is not supported on ARM
   depends_on "openjdk@8"
