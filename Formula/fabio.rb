9f417f748da5817723e13177cb56712289b54655
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 10:57:05 +0800

    fabio: update 1.5.15 bottle.

--- Formula/fabio.rb
+++ Formula/fabio.rb
@@ -4,18 +4,14 @@ class Fabio < Formula
   url "https://github.com/fabiolb/fabio/archive/v1.5.15.tar.gz"
   sha256 "19dcd4d8c6e4fe16e63e4208564d08ed442a0c724661ef4d91e9dbc85a9afbe1"
   license "MIT"
-  head "https://github.com/fabiolb/fabio.git"
+  head "https://github.com/fabiolb/fabio.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4309d65486b5f6552f3142cd941563999d376a689c0a23681d199fa6bc037d07"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "24a70802040a86892080a151eafa1304f5b1a61da347f7a1cd5565ae73516477"
-    sha256 cellar: :any_skip_relocation, monterey:       "ff1b2d576c9ee56e25a8a672cefb99d53623393c87386752d87f037761282910"
-    sha256 cellar: :any_skip_relocation, big_sur:        "64a7ae7497bc62f44ad203a43b4e0c9bbcb1bad020cb876422bd0d746f6d8bcc"
-    sha256 cellar: :any_skip_relocation, catalina:       "ffbae8584c9186bb63a761cc52aafd82fb90fd3ec35bce7b9fe81ffa0baf5b0d"
-    sha256 cellar: :any_skip_relocation, mojave:         "813e3edb76153f73dbd94b5291213a7584632f43a6bc9d4a8ef4deab6e3f96d8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1acc04acdcfaba5e627a62d9257c4ddad3d3dca9169720b9b6e976a38bc3a165"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fabio"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, mojave: "e1fe0e2d02c1ac94550b1eac06ee4ee0444ce4f35d5c6029e5a99fb9e5710766"
   end
+
 
   depends_on "go" => :build
   depends_on "consul"
