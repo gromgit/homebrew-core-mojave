51022b314574775b7fdb677dc68b7c610923fe86
Adrian Ho <the.gromgit@gmail.com>
2022-03-02 08:42:11 +0800

    dromeaudio: update 0.3.0 bottle.

--- Formula/dromeaudio.rb
+++ Formula/dromeaudio.rb
@@ -4,17 +4,14 @@ class Dromeaudio < Formula
   url "https://github.com/joshb/DromeAudio/archive/v0.3.0.tar.gz"
   sha256 "d226fa3f16d8a41aeea2d0a32178ca15519aebfa109bc6eee36669fa7f7c6b83"
   license "BSD-2-Clause"
-  head "https://github.com/joshb/dromeaudio.git"
+  head "https://github.com/joshb/dromeaudio.git", branch: "master"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "56127ff9fdb552e5a521d52a9a848ddf1f4a79029740d65f053ba9cc8ab2c7f7"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ef9ce724d04545c565e1e46f06560128f54c8fd164fdc3d3abca18a4d17ad9b6"
-    sha256 cellar: :any_skip_relocation, catalina:      "5199ecfbb8454f1560685c537b1fbaf1b301b39ad8ea825a9f846cc9f3530f30"
-    sha256 cellar: :any_skip_relocation, mojave:        "062b0fa8e43363d60e5816343d1fcb7f58ce02c236512d96f4bf4ba10c96fd2c"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "1334685c021a520567e2d16bfe68ebddea8f9382a50645e241d09349cfb6b450"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9f070aab40ff55d1bc82cae306c222e05770d9b5cd22fdace7fbb7d04ea7aa6f"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dromeaudio"
+    rebuild 3
+    sha256 cellar: :any_skip_relocation, mojave: "a93e191b7f40b61fc3497904cd4a04295af9b841042fcd885699a72e7f5c9f82"
   end
+
 
   depends_on "cmake" => :build
 
