a9dbbdec2aebc6f7c96aefbe0f69a27f0c3e7663
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-03 21:37:35 +0800

    aarch64-elf-binutils: update 2.38 bottle.

---
+++ Formula/aarch64-elf-binutils.rb
@@ -0,0 +1,45 @@
+class Aarch64ElfBinutils < Formula
+  desc "GNU Binutils for aarch64-elf cross development"
+  homepage "https://www.gnu.org/software/binutils/"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.38.tar.xz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.38.tar.xz"
+  sha256 "e316477a914f567eccc34d5d29785b8b0f5a10208d36bbacedcc39048ecfe024"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    formula "binutils"
+  end
+
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/aarch64-elf-binutils"
+    sha256 mojave: "2dd219c83d2f83ac23263ac3c05e886eba21d5233f00c656e640d19ea6f0f81b"
+  end
+
+  uses_from_macos "texinfo"
+
+  def install
+    target = "aarch64-elf"
+    system "./configure", "--target=#{target}",
+           "--prefix=#{prefix}",
+           "--libdir=#{lib}/#{target}",
+           "--infodir=#{info}/#{target}",
+           "--disable-nls"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test-s.s").write <<~EOS
+      .section .text
+      .globl _start
+      _start:
+          mov x0, #0
+          mov x16, #1
+          svc #0x80
+    EOS
+    system "#{bin}/aarch64-elf-as", "-o", "test-s.o", "test-s.s"
+    assert_match "file format elf64-littleaarch64",
+                 shell_output("#{bin}/aarch64-elf-objdump -a test-s.o")
+    assert_match "f()", shell_output("#{bin}/aarch64-elf-c++filt _Z1fv")
+  end
+end
