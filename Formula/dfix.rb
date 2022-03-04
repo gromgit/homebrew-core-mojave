c2fd4968a45a73146eb74882d52009dcc09e8744
Adrian Ho <the.gromgit@gmail.com>
2022-03-05 01:21:50 +0800

    dfix: update 0.3.5 bottle.

--- Formula/dfix.rb
+++ Formula/dfix.rb
@@ -5,21 +5,17 @@ class Dfix < Formula
       tag:      "v0.3.5",
       revision: "5265a8db4b0fdc54a3d0837a7ddf520ee94579c4"
   license "BSL-1.0"
-  head "https://github.com/dlang-community/dfix.git"
+  head "https://github.com/dlang-community/dfix.git", branch: "master"
 
   livecheck do
     url "https://code.dlang.org/packages/dfix"
     regex(%r{"badge">v?(\d+(?:\.\d+)+)</strong>}i)
   end
 
-  bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, monterey:     "0102eaa83c680a6a425f8e532bfc00b5c611a86d14d1382617e3d6ae5c76a1f0"
-    sha256 cellar: :any_skip_relocation, big_sur:      "bb047ac32131d3ec25b60ca86e77959f7b9625ad469d9e0a1ada3fefe8525d9d"
-    sha256 cellar: :any_skip_relocation, catalina:     "ff3b76977bcbfb5b7a04bbebb53a794cc522b64987f724fe5f8a8236812eb1f8"
-    sha256 cellar: :any_skip_relocation, mojave:       "24d234e206efa754f8bd900102720280d8efb1af6ec93059a467589acddca3ee"
-    sha256 cellar: :any_skip_relocation, high_sierra:  "13a2621737c198bd0540f507293a9b015a0ebe36cd3373589a69ec834a863d8d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "18eaa549250f741f4459b82c5425b4378587006db96f83f76ffd3967df9eaefd"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dfix"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, mojave: "2a0efbbb33abf783c03a04e16d074134fbba00b8cf5fb30f65f65269f0954e20"
   end
 
   depends_on "dmd" => :build
