63b5d559ce0888934ee658bf2d25f199226c909d
Adrian Ho <the.gromgit@gmail.com>
2022-03-04 21:27:12 +0800

    darcs: update 2.16.5 bottle.

--- Formula/darcs.rb
+++ Formula/darcs.rb
@@ -5,9 +5,10 @@ class Darcs < Formula
   sha256 "d63c6cd236e31e812e8ad84433d27059387606269fbd953f4c349c3cb3aa242b"
   license "GPL-2.0-or-later"
 
-  bottle do
+bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/darcs"
-    sha256 cellar: :any_skip_relocation, mojave: "17989eac163a9ee6da63dc92f08f8b9738e45e38fcc37ad1f8ed6d5113a90f08"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "0b933093c9c0ce3702780c5d01f5e7fce892d15b0b41aa5efe2974f4fe6a939f"
   end
 
   depends_on "cabal-install" => :build
