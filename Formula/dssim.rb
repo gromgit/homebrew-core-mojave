6000a69fb6a51190dca0c75d9cca82b8a80d66fc
Adrian Ho <the.gromgit@gmail.com>
2022-03-05 04:16:21 +0800

    dssim: update 3.2.0 bottle.

--- Formula/dssim.rb
+++ Formula/dssim.rb
@@ -5,9 +5,10 @@ class Dssim < Formula
   sha256 "b671aec5b117adb6f9c636ef70ecafbd671caacf8b110098565c01bf5171118f"
   license "AGPL-3.0-or-later"
 
-  bottle do
+bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dssim"
-    sha256 cellar: :any_skip_relocation, mojave: "b3dda6ba6ace6267c5614457d07b8775d15934a7be42295fdfcbc6557eb4d9ae"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "dd916ab12bbb642755b1f0d3a31cfa1304dba26cc139a4a6fc0a0c6ef8c67f5b"
   end
 
   depends_on "nasm" => :build
