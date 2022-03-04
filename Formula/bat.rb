6fc710a45e33b53337711feea24b680c0d03d4c1
Adrian Ho <the.gromgit@gmail.com>
2022-03-04 11:28:40 +0800

    bat: update 0.20.0 bottle.

--- Formula/bat.rb
+++ Formula/bat.rb
@@ -1,14 +1,13 @@
 class Bat < Formula
   desc "Clone of cat(1) with syntax highlighting and Git integration"
   homepage "https://github.com/sharkdp/bat"
-  url "https://github.com/sharkdp/bat/archive/v0.19.0.tar.gz"
-  sha256 "6a920cad1e7ae069eb9393f5b6883e0a7f2c957186b1075976331daaa5e0468a"
+  url "https://github.com/sharkdp/bat/archive/v0.20.0.tar.gz"
+  sha256 "12eca3add56f21b8056a4c17cfb5bffc45e107f23f75a8e0f5de948d8e997c39"
   license any_of: ["Apache-2.0", "MIT"]
 
-  bottle do
+bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bat"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "7abf6e2cf8b04eb45d245300bf97159330d74ab44473621baace5b7973ddcba4"
+    sha256 cellar: :any_skip_relocation, mojave: "a3d1302fe0c120e90718810f16f79b43a8e5db94a75752510be5cea4cc8f0614"
   end
 
   depends_on "rust" => :build
