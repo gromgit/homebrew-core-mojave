bd15b3406ba9c1d8b31eb267abaa1ceda5f9f3d9
Adrian Ho <the.gromgit@gmail.com>
2022-03-04 11:19:01 +0800

    basis_universal: update 1.16.3 bottle.

--- Formula/basis_universal.rb
+++ Formula/basis_universal.rb
@@ -1,13 +1,13 @@
 class BasisUniversal < Formula
   desc "Basis Universal GPU texture codec command-line compression tool"
   homepage "https://github.com/BinomialLLC/basis_universal"
-  url "https://github.com/BinomialLLC/basis_universal/archive/refs/tags/1.16.2.tar.gz"
-  sha256 "ab253ef2cde8afc3f2ad9a30f6983f495e334bac6239364d2ec421d0d586a37a"
+  url "https://github.com/BinomialLLC/basis_universal/archive/refs/tags/1.16.3.tar.gz"
+  sha256 "b89563aa5879eed20f56b9cfa03b52848e759531fd5a1d51a8f63c846f96c2ac"
   license "Apache-2.0"
 
-  bottle do
+bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/basis_universal"
-    sha256 cellar: :any_skip_relocation, mojave: "eadd30c69deb97e76727067fc74927dbb7c5f89596116bacf9cfa5c6ef108596"
+    sha256 cellar: :any_skip_relocation, mojave: "50e64f2730965a038ccfab92b98841f7858484253d7c1409835b87b0ffb2f9ae"
   end
 
   depends_on "cmake" => :build
