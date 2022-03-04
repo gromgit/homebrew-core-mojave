a49ef94737977386277e8f59419c98e597ce31db
Adrian Ho <the.gromgit@gmail.com>
2022-03-04 15:48:04 +0800

    cgif: update 0.2.0 bottle.

--- Formula/cgif.rb
+++ Formula/cgif.rb
@@ -5,9 +5,10 @@ class Cgif < Formula
   sha256 "d00fd4bf2a7b47bc3b0c3b2c8f2215b1bdfd88f0569388d752909b878db27bfb"
   license "MIT"
 
-  bottle do
+bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cgif"
-    sha256 cellar: :any, mojave: "3c2d07fa44fde0ff1bf8705cf8d88f83817c80792078cbc0ac079ebbe2aca235"
+    rebuild 1
+    sha256 cellar: :any, mojave: "43b612def5d1a4d8a37590a0b3d763596abac2a825f0a750aee698e96c5b97ec"
   end
 
   depends_on "meson" => :build
