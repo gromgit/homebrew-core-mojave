eb3de11f4f4cdf87a9a11d3323bb9838366b708d
Adrian Ho <the.gromgit@gmail.com>
2022-03-04 14:06:30 +0800

    calceph: update 3.5.1 bottle.

--- Formula/calceph.rb
+++ Formula/calceph.rb
@@ -1,8 +1,8 @@
 class Calceph < Formula
   desc "C library to access the binary planetary ephemeris files"
   homepage "https://www.imcce.fr/inpop/calceph"
-  url "https://www.imcce.fr/content/medias/recherche/equipes/asd/calceph/calceph-3.5.0.tar.gz"
-  sha256 "2aa9ba47af6a73ab74ae10d1c8efbcfab9068d9d05f016a8a5fe695df7b52e00"
+  url "https://www.imcce.fr/content/medias/recherche/equipes/asd/calceph/calceph-3.5.1.tar.gz"
+  sha256 "33cc0be1b8ffb647aff9d3ac1cf025e460451e00144050d3bbc4f13bceb11c1d"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -10,14 +10,9 @@ class Calceph < Formula
     regex(/href=.*?calceph[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
-  bottle do
-    sha256 cellar: :any,                 arm64_monterey: "0b9a27bf445e9369110bca6ce6d307e4792484249cde4befb7a6405d5e890b85"
-    sha256 cellar: :any,                 arm64_big_sur:  "9c3abdc353fa28b0f073f59579866f5f4ebe1394af3f48cceff39664077f2d67"
-    sha256 cellar: :any,                 monterey:       "e68ea66a767bd309f19b691fcb210248cd3e3f0254e68c35aeb2b9690c31d68e"
-    sha256 cellar: :any,                 big_sur:        "09f5d187d213d349fe8f0035cc26a08f8cee34e43efa691f1bd882df968540b2"
-    sha256 cellar: :any,                 catalina:       "36e5388c8789221831f181d7e9ec7d2ad822e4f76afca20533912508ca38913c"
-    sha256 cellar: :any,                 mojave:         "22799e0c12c14a7a50062e5d0c482460d1fd8317a32fbf8063aca797d7e83bc4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a6684443f24392f477d629d6d3da6a067d1bf73892dd465e6bd8421037118c54"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/calceph"
+    sha256 cellar: :any, mojave: "0df4dafa76e4c30228a4b10637e8d4e83a51963bfe2da05778380eeddca8a548"
   end
 
   depends_on "gcc" # for gfortran
