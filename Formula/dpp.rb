ceb07fb826d214b946847cacc70ebc600f4ed585
Adrian Ho <the.gromgit@gmail.com>
2022-03-05 03:38:52 +0800

    dpp: update 0.4.9 bottle.

---
+++ Formula/dpp.rb
@@ -0,0 +1,60 @@
+class Dpp < Formula
+  desc "Directly include C headers in D source code"
+  homepage "https://github.com/atilaneves/dpp"
+  url "https://github.com/atilaneves/dpp.git",
+      tag:      "v0.4.9",
+      revision: "55daf91e2c180962de783752b3e4447850b7a151"
+  license "BSL-1.0"
+
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dpp"
+    sha256 cellar: :any_skip_relocation, mojave: "a7beccdce9d322a96c1b518a0ea2f4dd12b345ff466055c5838dec4cd72739f4"
+  end
+
+  depends_on "dub" => :build
+  depends_on "ldc" => [:build, :test]
+
+  uses_from_macos "llvm" # for libclang
+
+  def install
+    if OS.mac?
+      toolchain_paths = []
+      toolchain_paths << MacOS::CLT::PKG_PATH if MacOS::CLT.installed?
+      toolchain_paths << MacOS::Xcode.toolchain_path if MacOS::Xcode.installed?
+      dflags = toolchain_paths.flat_map do |path|
+        %W[
+          -L-L#{path}/usr/lib
+          -L-rpath
+          -L#{path}/usr/lib
+        ]
+      end
+      ENV["DFLAGS"] = dflags.join(" ")
+    end
+    system "dub", "add-local", buildpath
+    system "dub", "build", "dpp"
+    bin.install "bin/d++"
+  end
+
+  test do
+    (testpath/"c.h").write <<~EOS
+      #define FOO_ID(x) (x*3)
+      int twice(int i);
+    EOS
+
+    (testpath/"c.c").write <<~EOS
+      int twice(int i) { return i * 2; }
+    EOS
+
+    (testpath/"foo.dpp").write <<~EOS
+      #include "c.h"
+      void main() {
+          import std.stdio;
+          writeln(twice(FOO_ID(5)));
+      }
+    EOS
+
+    system ENV.cc, "-c", "c.c"
+    system bin/"d++", "--compiler=ldc2", "foo.dpp", "c.o"
+    assert_match "30", shell_output("./foo")
+  end
+end
