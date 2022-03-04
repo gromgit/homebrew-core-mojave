7010271ca13bff24f036e4cf419a96cbde0e3b18
Adrian Ho <the.gromgit@gmail.com>
2022-03-04 16:49:26 +0800

    chronograf: update 1.9.3 bottle.

--- Formula/chronograf.rb
+++ Formula/chronograf.rb
@@ -3,16 +3,14 @@ require "language/node"
 class Chronograf < Formula
   desc "Open source monitoring and visualization UI for the TICK stack"
   homepage "https://docs.influxdata.com/chronograf/latest/"
-  url "https://github.com/influxdata/chronograf/archive/1.9.1.tar.gz"
-  sha256 "df5600b2bd0b5ed0d3a3c548e5d72038b62fdda1501ad616eb1c1d82b054793a"
+  url "https://github.com/influxdata/chronograf/archive/1.9.3.tar.gz"
+  sha256 "10db16bb6959356c4fabe6229a500d3183436f401a3c15a5377bc7434fe489d3"
   license "AGPL-3.0-or-later"
   head "https://github.com/influxdata/chronograf.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "035e1ec95c64d5db5d2dc3305ac3f631d50b55f7d402a2ed0a74aa981f3cd9e7"
-    sha256 cellar: :any_skip_relocation, big_sur:       "36287a0454a409f11fcf156310fd5e835818266a8e25121299cc0234afc85f88"
-    sha256 cellar: :any_skip_relocation, catalina:      "3675d74285d934ae45bd86c69d91807accc50d4f31a5e3dd2963097bef49874a"
-    sha256 cellar: :any_skip_relocation, mojave:        "5be98ea883d9df39a4082192cf41044886bf8dfc7c2ea2c82c5f9392df790b15"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/chronograf"
+    sha256 cellar: :any_skip_relocation, mojave: "e58843f691f4582fe2663cdb24ec68446c2cea1cb22a471db9a17222a0116292"
   end
 
   depends_on "go" => :build
