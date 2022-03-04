97791973fa42ef5bac58ea84fe903da3a659a94f
Adrian Ho <the.gromgit@gmail.com>
2022-03-05 00:50:55 +0800

    devd: update 0.9 bottle.

--- Formula/devd.rb
+++ Formula/devd.rb
@@ -4,17 +4,12 @@ class Devd < Formula
   url "https://github.com/cortesi/devd/archive/v0.9.tar.gz"
   sha256 "5aee062c49ffba1e596713c0c32d88340360744f57619f95809d01c59bff071f"
   license "MIT"
-  head "https://github.com/cortesi/devd.git"
+  head "https://github.com/cortesi/devd.git", branch: "master"
 
-  bottle do
-    rebuild 3
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "066a8cb9c2a379b7b4c77476dcad100ef1b50ca211717460de1b23727f540e51"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1adc7ff42fb9063f2724578755a548a4b52244269c03ffef599bad06da2641d0"
-    sha256 cellar: :any_skip_relocation, monterey:       "c5a739aeae1644c64f5dde53190c6050ecf28ff7ae06867e604679391684c5df"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3a80a457e6b056c0b00d9b1dbbce26c18c36285e4296100d1196875988ee6b7c"
-    sha256 cellar: :any_skip_relocation, catalina:       "5a1dceea4de81075bab6e0617b38c39f715423c4e8e0d17f75f65d7e15cf7dee"
-    sha256 cellar: :any_skip_relocation, mojave:         "33c299776d5aa228d68b36d9d51af12b04d5e65a194b907900bd117882a45f27"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "19d3cb4f63a05ccd30e3e819940ce9f7f50732d4478a04a65cb6d54d31852a7c"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/devd"
+    rebuild 4
+    sha256 cellar: :any_skip_relocation, mojave: "06eb69dd0bb943ecf86990b24ece1379a1a9fa234b511040bafcc1d94c5fa118"
   end
 
   depends_on "dep" => :build
