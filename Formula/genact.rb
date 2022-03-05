d6229b79e8b8cd2e5dbd534d4426e1a8829cdc02
Adrian Ho <the.gromgit@gmail.com>
2022-03-05 13:08:24 +0800

    genact: update 0.12.0 bottle.

--- Formula/genact.rb
+++ Formula/genact.rb
@@ -5,9 +5,10 @@ class Genact < Formula
   sha256 "fbbbcf5e65b370fde6765a2257c6d2f12cb9568bdce785b0f548c2be28d08cd7"
   license "MIT"
 
-  bottle do
+bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/genact"
-    sha256 cellar: :any_skip_relocation, mojave: "fb34bc3269f6d459f23d20801d46c4c7468fb38e82b6b738e25212b8dcac9fc8"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "f9e280299f88207d777cb9d9082929e55df26c35da3e9a261f66bec09b2b8516"
   end
 
   depends_on "rust" => :build
