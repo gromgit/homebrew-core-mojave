fc491226d24013034e17ba0d2ff579b21b5237e2
Adrian Ho <the.gromgit@gmail.com>
2022-03-05 11:33:47 +0800

    frugal: update 3.14.15 bottle.

--- Formula/frugal.rb
+++ Formula/frugal.rb
@@ -5,9 +5,10 @@ class Frugal < Formula
   sha256 "e05879b91c80e5654394decd37b89d06cf7d67e76f544aecb9642778c6e8d06a"
   license "Apache-2.0"
 
-  bottle do
+bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/frugal"
-    sha256 cellar: :any_skip_relocation, mojave: "46d1ca2083d1073277cab4446d4ce24f43a36097892015b2a8f4fb731fa51d31"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "98b079890e59e2de08b011ae5bf7e91e21790574e1606397a8cc8e2e349fbfd7"
   end
 
   depends_on "go" => :build
