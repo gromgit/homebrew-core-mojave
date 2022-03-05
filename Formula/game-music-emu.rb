cef67f96dc1d0b76d67cfb83920a2c69c43a1c18
Adrian Ho <the.gromgit@gmail.com>
2022-03-05 12:18:59 +0800

    game-music-emu: update 0.6.3_2 bottle.

--- Formula/game-music-emu.rb
+++ Formula/game-music-emu.rb
@@ -5,16 +5,12 @@ class GameMusicEmu < Formula
   sha256 "aba34e53ef0ec6a34b58b84e28bf8cfbccee6585cebca25333604c35db3e051d"
   license one_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
   revision 2
-  head "https://bitbucket.org/mpyne/game-music-emu.git"
+  head "https://bitbucket.org/mpyne/game-music-emu.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any, arm64_monterey: "1346614b5a9561f7eaace297b5493eeb99ec4c3e561acc65669ca6dbb0cd6793"
-    sha256 cellar: :any, arm64_big_sur:  "e83fbee26086cc93f7d2eed7d3b93f00a0a0c9eb9d59abf3aba91216fe89d3d8"
-    sha256 cellar: :any, monterey:       "7b1e5a6934c8ff16fff726c1963e465abd11458f5773f26b38ce8771da3289a1"
-    sha256 cellar: :any, big_sur:        "a0abdc4c5ae05ea22ad3627a1a717ed8a1a137065188b995858c0f301dfda640"
-    sha256 cellar: :any, catalina:       "ee658e16c3d9d0061b0b930ca387a1cb2fa6b6b50d23c9f6f4ae7799ddb6f46d"
-    sha256 cellar: :any, mojave:         "754ab0c8bc0a6de76adcb56a59913c930196e8e44154958081c093fb7763edad"
-    sha256 cellar: :any, high_sierra:    "596497823bb1ebb30f20fa01c8656bb15544c12fad5d67c4de165f9ef3122e68"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/game-music-emu"
+    rebuild 1
+    sha256 cellar: :any, mojave: "0e12ce5afc217f5a6f082e94b914a6599ceb3eaf37fd94bb9eb266553b5456f7"
   end
 
   depends_on "cmake" => :build
@@ -44,8 +40,12 @@ class GameMusicEmu < Formula
       }
     EOS
 
-    ubsan_libdir = Dir["#{MacOS::CLT::PKG_PATH}/usr/lib/clang/*/lib/darwin"].first
-    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-Wl,-rpath,#{ubsan_libdir}",
+    if OS.mac?
+      ubsan_libdir = Dir["#{MacOS::CLT::PKG_PATH}/usr/lib/clang/*/lib/darwin"].first
+      rpath = "-Wl,-rpath,#{ubsan_libdir}"
+    end
+
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", *(rpath if OS.mac?),
                    "-lgme", "-o", "test", *ENV.cflags.to_s.split
     system "./test"
   end
