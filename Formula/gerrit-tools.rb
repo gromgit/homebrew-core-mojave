97928999c9c6f4c8ad0ff6b4a4d628295fb2cc49
Adrian Ho <the.gromgit@gmail.com>
2022-03-05 13:18:19 +0800

    gerrit-tools: update 1.0.0 bottle.

--- Formula/gerrit-tools.rb
+++ Formula/gerrit-tools.rb
@@ -4,15 +4,12 @@ class GerritTools < Formula
   url "https://github.com/indirect/gerrit-tools/archive/v1.0.0.tar.gz"
   sha256 "c3a84af2ddb0f17b7a384e5dbc797329fb94d2499a75b6d8f4c8ed06a4a482dd"
   license "Apache-2.0"
-  head "https://github.com/indirect/gerrit-tools.git"
+  head "https://github.com/indirect/gerrit-tools.git", branch: "main"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1d1eb15677d4ed0974a06c21e3ad17fe49b543acbea1ebc0052ef0b2900f65f0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1d1eb15677d4ed0974a06c21e3ad17fe49b543acbea1ebc0052ef0b2900f65f0"
-    sha256 cellar: :any_skip_relocation, monterey:       "e4094d8655a172f89aea151baea9298f35827c09fad7ea4d696e5c542d724c06"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e4094d8655a172f89aea151baea9298f35827c09fad7ea4d696e5c542d724c06"
-    sha256 cellar: :any_skip_relocation, catalina:       "e4094d8655a172f89aea151baea9298f35827c09fad7ea4d696e5c542d724c06"
-    sha256 cellar: :any_skip_relocation, mojave:         "e4094d8655a172f89aea151baea9298f35827c09fad7ea4d696e5c542d724c06"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gerrit-tools"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "5adb2effd6d7e14339030534504d25797bd2be214c96051bb2abb42f66bb3238"
   end
 
   def install
