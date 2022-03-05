f245648ad71647d027db436b924722b0de51f09b
Adrian Ho <the.gromgit@gmail.com>
2022-03-05 13:02:49 +0800

    gem-completion: update 2 bottle.

--- Formula/gem-completion.rb
+++ Formula/gem-completion.rb
@@ -5,14 +5,16 @@ class GemCompletion < Formula
       revision: "f3e4345042b0cc48317e45b673dfd3d23904b9a7"
   version "2"
   license "MIT"
-  head "https://github.com/mernen/completion-ruby.git"
+  head "https://github.com/mernen/completion-ruby.git", branch: "master"
 
   livecheck do
     formula "ruby-completion"
   end
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "044d02a4211a0f89e550ff2f5779903cb443528f8f703ea2bd549bc39b9be595"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gem-completion"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "0a01abb125cae8707c0684b207d5c3a08effd29a11048bdca549e245e60ea76c"
   end
 
   def install
