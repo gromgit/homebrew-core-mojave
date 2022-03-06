afa2a42a2035977ae144250c455bff00a75a6062
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-06 10:22:05 +0800

    git-gerrit: update 0.3.0 bottle.

--- Formula/git-gerrit.rb
+++ Formula/git-gerrit.rb
@@ -4,15 +4,12 @@ class GitGerrit < Formula
   url "https://github.com/fbzhong/git-gerrit/archive/v0.3.0.tar.gz"
   sha256 "433185315db3367fef82a7332c335c1c5e0b05dabf8d4fbeff9ecf6cc7e422eb"
   license "BSD-3-Clause"
-  head "https://github.com/fbzhong/git-gerrit.git"
+  head "https://github.com/fbzhong/git-gerrit.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "865ffd990de756622dd9c3bb37fd0f8e7af04eba58bd38013cda9b6a6c551f95"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "865ffd990de756622dd9c3bb37fd0f8e7af04eba58bd38013cda9b6a6c551f95"
-    sha256 cellar: :any_skip_relocation, monterey:       "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
-    sha256 cellar: :any_skip_relocation, big_sur:        "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
-    sha256 cellar: :any_skip_relocation, catalina:       "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
-    sha256 cellar: :any_skip_relocation, mojave:         "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-gerrit"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "b31bb774c3609639c5b7c599aaea4da976dc12b930c842932cd253fd6f278e4d"
   end
 
   def install
