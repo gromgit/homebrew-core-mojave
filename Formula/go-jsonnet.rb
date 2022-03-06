4eab1751c69e46d59159c5ebc430957d5736d420
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-06 10:26:23 +0800

    go-jsonnet: update 0.18.0 bottle.

--- Formula/go-jsonnet.rb
+++ Formula/go-jsonnet.rb
@@ -1,20 +1,14 @@
 class GoJsonnet < Formula
   desc "Go implementation of configuration language for defining JSON data"
   homepage "https://jsonnet.org/"
-  url "https://github.com/google/go-jsonnet/archive/v0.17.0.tar.gz"
-  sha256 "4fd04d0c9e38572ef388d28ea6b1ac151b8a9a5026ff94e3a68bdbc18c4db38a"
+  url "https://github.com/google/go-jsonnet/archive/v0.18.0.tar.gz"
+  sha256 "369af561550ba8cff5dd7dd08a771805a38d795da3285221012cf3a2933b363e"
   license "Apache-2.0"
-  head "https://github.com/google/go-jsonnet.git"
+  head "https://github.com/google/go-jsonnet.git", branch: "master"
 
-  bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5a0ab911c3a8d77eb9c2cd9ef75e8027b19c5bbb442f5b7c6c6aea076a3a211c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "93ba8dab3baf9865126d55705bcc357bf6a7d850e545ce43a5633748dcc95efb"
-    sha256 cellar: :any_skip_relocation, monterey:       "7441633a7ab1a076ddf2b3fd519b9be5d81e8147e35affe0c53a35d09f46c040"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e79f3ad29f00746532ded81842fae95a980fba36980e8d6299aa7195eb0de0da"
-    sha256 cellar: :any_skip_relocation, catalina:       "9e5ee375c84608de8566d017c9e8a0f9b3806c44156b56650918b78ffc0db9f9"
-    sha256 cellar: :any_skip_relocation, mojave:         "83baf9f7af774dcdc0fb3484eef18c74d6ee20b2ca1e2c1b37f470c76d8d5d21"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3318e83a2961f88e5859e1a6700cd0ec09b2c93744460c968bfb1582ef204c2f"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/go-jsonnet"
+    sha256 cellar: :any_skip_relocation, mojave: "db7a136f032ac873c8c77df2b0c8b6d2321dbce83114802fc5c6332ecaa758e4"
   end
 
   depends_on "go" => :build
