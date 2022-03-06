edcf13dd49c0d0c9ba7f6b405821de51a243fcf5
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-06 10:25:42 +0800

    gnirehtet: update 2.5_1 bottle.

--- Formula/gnirehtet.rb
+++ Formula/gnirehtet.rb
@@ -5,17 +5,12 @@ class Gnirehtet < Formula
   sha256 "2b55b56e1b21d1b609a0899fe85d1f311120bb12b04761ec586187338daf6ec5"
   license "Apache-2.0"
   revision 1
-  head "https://github.com/Genymobile/gnirehtet.git"
+  head "https://github.com/Genymobile/gnirehtet.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8e895ff323e648db638a97542eeed9d71936f882c36b95e410bd50a7ff272ffd"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7fd56faf93c88c65df43599e0dda5c7d3044e6b1dee05ef535d6eff42b558684"
-    sha256 cellar: :any_skip_relocation, monterey:       "473eea2bf04850ae912eb9813ec172518890d86bf311d3b88119b61d131d2885"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c81db3a1b9c0c6ebbe81ebca2ffd111f97d6eda2cff2cb92955cc8c42abcce63"
-    sha256 cellar: :any_skip_relocation, catalina:       "6f65def77cc1708e7a1ae8e85dfe2cbede4717225e8a5c2f7a9f09c8271282f3"
-    sha256 cellar: :any_skip_relocation, mojave:         "7ebc9b16c6d6856be8604388d4ca2bfc9cc2c4ec02e255f1a462be681283c6e8"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "a57d5039af819db991968751511a63874cd0c20d1d10fe106ef92e83b216eb38"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "94166648cf3a8a072b85eed7436af2bddbcf5c2dfab939cd79590d4ff2b9a9c3"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gnirehtet"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "30af374e560b0b8386d24e3bf12754eb87a6c6fe66056962cb50d90cdf353885"
   end
 
   depends_on "rust" => :build
