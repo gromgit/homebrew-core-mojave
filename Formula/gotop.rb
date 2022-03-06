4705ff71e5f35199a983ee3599e18c5b069af85f
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-06 10:28:43 +0800

    gotop: update 4.1.3 bottle.

--- Formula/gotop.rb
+++ Formula/gotop.rb
@@ -5,9 +5,10 @@ class Gotop < Formula
   sha256 "c0a02276e718b988d1220dc452063759c8634d42e1c01a04c021486c1e61612d"
   license "BSD-3-Clause"
 
-  bottle do
+bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gotop"
-    sha256 cellar: :any_skip_relocation, mojave: "4522cb02c9844d8600e4599357de8db4732e5eb0335d790862ccef06f640c54b"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "fb99aa322b3467eef65a12dd871a079ccdec164db34c5593fe4590885c9f5872"
   end
 
   depends_on "go" => :build
