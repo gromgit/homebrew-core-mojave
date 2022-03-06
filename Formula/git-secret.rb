51d71da6b64de4b6065162485372cfed47262e6c
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-07 00:27:07 +0800

    git-secret: update 0.4.0 bottle.

--- Formula/git-secret.rb
+++ Formula/git-secret.rb
@@ -2,21 +2,17 @@ class GitSecret < Formula
   desc "Bash-tool to store the private data inside a git repo"
   homepage "https://git-secret.io"
   license "MIT"
-  head "https://github.com/sobolevn/git-secret.git"
+  head "https://github.com/sobolevn/git-secret.git", branch: "master"
 
   stable do
     url "https://github.com/sobolevn/git-secret/archive/v0.4.0.tar.gz"
     sha256 "ae17bfda88eb77e8f07c5f16d833792a3a14adc9c5d2bbc840f28538c62f08ba"
   end
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8b19c1a419d844931e68bc21b0097c2d9c17275f66de939cafe6a13df91dafb3"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8b19c1a419d844931e68bc21b0097c2d9c17275f66de939cafe6a13df91dafb3"
-    sha256 cellar: :any_skip_relocation, monterey:       "67ed071e1429ea8a7e5eb4feaa3b0da1af70d38714620e81450c8b6cfcc9d7cf"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2bd6c524c359601a854696aa0b7fed558e6c5dc45791feced47f33e06103dafe"
-    sha256 cellar: :any_skip_relocation, catalina:       "2bd6c524c359601a854696aa0b7fed558e6c5dc45791feced47f33e06103dafe"
-    sha256 cellar: :any_skip_relocation, mojave:         "2bd6c524c359601a854696aa0b7fed558e6c5dc45791feced47f33e06103dafe"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "28cf4e740681f14e15661f2d954c042cb78ba80e0f36c0eae8c9096915977f7a"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-secret"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "f0f3bbadf46c9fbb213f9e92efec64471be7b02a3587ed73c89d80cc44b59362"
   end
 
   depends_on "gawk"
