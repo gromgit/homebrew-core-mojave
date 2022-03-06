ff83d625a9b61cc68a38a351973c984d075076c9
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-06 10:23:30 +0800

    git-subrepo: update 0.4.3 bottle.

--- Formula/git-subrepo.rb
+++ Formula/git-subrepo.rb
@@ -4,21 +4,17 @@ class GitSubrepo < Formula
   url "https://github.com/ingydotnet/git-subrepo/archive/0.4.3.tar.gz"
   sha256 "d2e3cc58f8ac3d90f6f351ae2f9cc999b133b8581ab7a0f7db4933dec8e62c2a"
   license "MIT"
-  head "https://github.com/ingydotnet/git-subrepo.git"
+  head "https://github.com/ingydotnet/git-subrepo.git", branch: "master"
 
   livecheck do
     url :stable
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5519cc8d068994db11fa1c3427f780a77e9c1e1f4a39cf6b6724b6c1c6097d91"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b129526636d9f095133421f59317abf144c77ad5aeb54f964fd6eab4aa2937af"
-    sha256 cellar: :any_skip_relocation, monterey:       "c5c79de823ab2736f0b0a439a71b3e03e00d9b4caaa25bdf659df8731684afb2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "dc532b9259999534520c4695392ccb28d5762ab9b9ecfd37d0457be6029b4ce1"
-    sha256 cellar: :any_skip_relocation, catalina:       "0e1c83339ad6f73952c8d4ae0c82b4cb0181d421d0a527a5e0a31f7cd3c3ac90"
-    sha256 cellar: :any_skip_relocation, mojave:         "5465f88825a9a2361f0459d8e2dfb65a4e533c2106b2f8a644f295702085b711"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5519cc8d068994db11fa1c3427f780a77e9c1e1f4a39cf6b6724b6c1c6097d91"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-subrepo"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "42e686bfb0df1a1c2fdd15fd96f444f1d6400163d08da5c92b1eb06b560acd54"
   end
 
   depends_on "bash"
