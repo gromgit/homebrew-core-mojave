bfe9c0cfe977a8f42af8b77b3308e931ea7321ab
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-06 10:29:27 +0800

    gradle-completion: update 1.4.1 bottle.

--- Formula/gradle-completion.rb
+++ Formula/gradle-completion.rb
@@ -4,10 +4,12 @@ class GradleCompletion < Formula
   url "https://github.com/gradle/gradle-completion/archive/v1.4.1.tar.gz"
   sha256 "5d77f0c739fe983cfa86078a615f43be9be0e3ce05a3a7b70cb813a1ebd1ceef"
   license "MIT"
-  head "https://github.com/gradle/gradle-completion.git"
+  head "https://github.com/gradle/gradle-completion.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "6889d645ade2d3296031a3bdebaec9c2622bf26755fae51024e1692f6872ccb3"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gradle-completion"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "3563d6c6bf5483f456fdbd43a037240bf7375f8f7e0ecabbc2787e82e7074d97"
   end
 
   def install
