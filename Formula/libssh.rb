89cf43a31b96999a55f1a8516ab13e41588b98d8
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-06 18:24:08 +0800

    libssh: update 0.9.6 bottle.

--- Formula/libssh.rb
+++ Formula/libssh.rb
@@ -4,16 +4,12 @@ class Libssh < Formula
   url "https://www.libssh.org/files/0.9/libssh-0.9.6.tar.xz"
   sha256 "86bcf885bd9b80466fe0e05453c58b877df61afa8ba947a58c356d7f0fab829b"
   license "LGPL-2.1-or-later"
-  head "https://git.libssh.org/projects/libssh.git"
+  head "https://git.libssh.org/projects/libssh.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any,                 arm64_monterey: "8487fa5c9e40e22b37862ac114c93e10384f2fd42b810bd290db627663b48af5"
-    sha256 cellar: :any,                 arm64_big_sur:  "d6c0de919fb07df2cc4c637eba40c85f2dd19808fae5d7dcabeeb8c9b8477de2"
-    sha256 cellar: :any,                 monterey:       "5c1830359324ebcaeca495d51af67b9653de39d00dae55c42a9647b98f85a24c"
-    sha256 cellar: :any,                 big_sur:        "cf2110fe6b71f0a5d59a2a4a7bc7badfa5bf53c59bfa2968ac1160519aa7285f"
-    sha256 cellar: :any,                 catalina:       "3aff6528d21c6844b4592862628ab197bb5e34e4c506008d24b63afacc116900"
-    sha256 cellar: :any,                 mojave:         "c230e01e4990691096acce9e33222f51f1db8fa21b93c8998b82efad871afcf3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "be12ab5dbd45956b1bd8c26c3c6f5e1a8d7ee20e7e60170a693969f2734309e2"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libssh"
+    rebuild 1
+    sha256 cellar: :any, mojave: "52a3b9554aa392aa0cce65d572f84b968a0a196f0160db6fdffe4ae4d5bdcaec"
   end
 
   depends_on "cmake" => :build
