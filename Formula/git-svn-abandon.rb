9acb239b4b8013249f8c979a825d0d5afae4f3e7
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-06 10:23:36 +0800

    git-svn-abandon: update 0.0.1 bottle.

--- Formula/git-svn-abandon.rb
+++ Formula/git-svn-abandon.rb
@@ -4,10 +4,12 @@ class GitSvnAbandon < Formula
   url "https://github.com/nothingmuch/git-svn-abandon/archive/0.0.1.tar.gz"
   sha256 "65c11b5e575e6af4d21ef7624941c4581a5570748d50e38714bd33fee56e4485"
   license "MIT"
-  head "https://github.com/nothingmuch/git-svn-abandon.git"
+  head "https://github.com/nothingmuch/git-svn-abandon.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "06ae7b10d6efedfb1ba1c781509f717f6b680c4a7408f15690f6800ee06594f3"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-svn-abandon"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "214342b40fa8ee0a68937b223571fd157072ee61886f1312d132f221d5550229"
   end
 
   def install
