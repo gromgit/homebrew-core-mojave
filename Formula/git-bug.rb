f320f85f23d18a14b7bdfcca4d4e5ec09942626a
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-06 10:21:32 +0800

    git-bug: update 0.7.2 bottle.

--- Formula/git-bug.rb
+++ Formula/git-bug.rb
@@ -5,16 +5,12 @@ class GitBug < Formula
       tag:      "v0.7.2",
       revision: "cc4a93c8ce931b1390c61035b888ad17110b7bd6"
   license "GPL-3.0-or-later"
-  head "https://github.com/MichaelMure/git-bug.git"
+  head "https://github.com/MichaelMure/git-bug.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "beeb321dc338263445c5a092cee6d627679ef38f65ef449cbcbf1d3dee53983b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7477b094e81aa878baf489654c8b131c6d8ebd40222b41769481e298089ba019"
-    sha256 cellar: :any_skip_relocation, monterey:       "cc519ce7c73d26de779c4eb14118e0dc01c160d463e42b54c380e629f5e9cd5c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "26ead98df2569b14356aac45755b68397dceca6a0dc2cdb6ec00f1a4926fc669"
-    sha256 cellar: :any_skip_relocation, catalina:       "60c08117214d4d8657e67f182d1380b291882944d69c54c9adaa0f8140e28993"
-    sha256 cellar: :any_skip_relocation, mojave:         "6184e21ba0f2c4c28722dd7276004f2f430e02e4f2cf56462b4914d08df6f2f0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "234a0d2749668c523e59bf359cdb0484d984fc4230cbe72c64c0ff585eaef6cc"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-bug"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "d2f42d1693e7581c9a36c5f9ca6065c9e142f456b96520a0d3cac5ec8a8e2656"
   end
 
   depends_on "go" => :build
