de6e14d56346fe781f3092271f658d7e3957c24a
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-06 10:24:04 +0800

    github-keygen: update 1.305 bottle.

--- Formula/github-keygen.rb
+++ Formula/github-keygen.rb
@@ -4,10 +4,12 @@ class GithubKeygen < Formula
   url "https://github.com/dolmen/github-keygen/archive/v1.305.tar.gz"
   sha256 "5a0a68ed9e3eb1c0e3b783c250e0790fffe17fc1a663cefa1348560ff040b940"
   license "GPL-3.0"
-  head "https://github.com/dolmen/github-keygen.git"
+  head "https://github.com/dolmen/github-keygen.git", branch: "release"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "40efaefeb31f64b474d8030ddc268250d52d6f3e17131f36d0eb8e35fa24ab8e"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/github-keygen"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "54f737ade1d010913a583f15ee1ce660afae31ffa6b1e090d24046b9e36017ee"
   end
 
   def install
