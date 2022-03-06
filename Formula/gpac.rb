cecb89cad3ff00ad6f9eb3142ee4de54882b89be
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-06 10:29:15 +0800

    gpac: update 1.0.1 bottle.

--- Formula/gpac.rb
+++ Formula/gpac.rb
@@ -12,17 +12,12 @@ class Gpac < Formula
   url "https://github.com/gpac/gpac/archive/v1.0.1.tar.gz"
   sha256 "3b0ffba73c68ea8847027c23f45cd81d705110ec47cf3c36f60e669de867e0af"
   license "LGPL-2.1-or-later"
-  head "https://github.com/gpac/gpac.git"
+  head "https://github.com/gpac/gpac.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any,                 arm64_monterey: "74f3336aa8d6f9cfc15c19ddd56a855adae55546875f113d95ac1be249688119"
-    sha256 cellar: :any,                 arm64_big_sur:  "9d969e1cab82b163e4958a99e7e73f89fcf7a10675626223c5d4be1fc3b7d427"
-    sha256 cellar: :any,                 monterey:       "283c472be56974de1c4d1a7ea733e2616621a3294ff771be200d170b3dd0b3ce"
-    sha256 cellar: :any,                 big_sur:        "f6c4c5413b6746988520e5d9b1f0ee584f7456b208ed994e87fa8675436c9c41"
-    sha256 cellar: :any,                 catalina:       "cd323eba25dac7431970a3854c1317c1e4ce71e12421a1c789bfe127f2c373d7"
-    sha256 cellar: :any,                 mojave:         "f6acea4aee0a0719ae5c8deb775529a07a7da5d8e32e9c30371a7165b010294d"
-    sha256 cellar: :any,                 high_sierra:    "b050e13507f1462dcf37d968ed24e36195cf6026dc762e7ddbfa7de9088e0a9c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "57e4c20420fd7043ccd8f9c1851453df9023065271a540c55ba837f0249d9cce"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gpac"
+    rebuild 1
+    sha256 cellar: :any, mojave: "a46c64e2c044ec7f87a3d2dbdb9e96efa2d1b7c84b687d93f95513e44727c153"
   end
 
   depends_on "pkg-config" => :build
