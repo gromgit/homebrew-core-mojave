81a6e75a3256b08fd62cefef6a683964d0789724
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-06 10:20:31 +0800

    ghi: update 1.2.0_4 bottle.

--- Formula/ghi.rb
+++ Formula/ghi.rb
@@ -5,18 +5,12 @@ class Ghi < Formula
   sha256 "ffc17cfbdc8b88bf208f5f762e62c211bf8fc837f447354ad53cce39b1400671"
   license "MIT"
   revision 4
-  head "https://github.com/stephencelis/ghi.git"
+  head "https://github.com/stephencelis/ghi.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "320e13830aac81ab0bcbef032e548f6a5f9bf71ce713d7dab24fc813087f4d4c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "78e10bdfae403bb4ca2a2d6208d7df3cd84f321711ca310ded6b00850a69e6bf"
-    sha256 cellar: :any_skip_relocation, catalina:      "36449d0c0fc0a544808178745ce7a846dfd905cf5fc2489feaa2a70d26346041"
-    sha256 cellar: :any_skip_relocation, mojave:        "b6dcd03ae7705b3a3648c6df15b9e451397cd81e41acc5c5f8444796c747c580"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "9289e061f8a249130950ec212042e3d9adfaa96e3591f0eb2d6038c28ff0e6d6"
-    sha256 cellar: :any_skip_relocation, sierra:        "d2b59c4b0326bd4d4b2de6da0310e1d5228cc63d57adb9eb37c5f5c5a9471131"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "d2b59c4b0326bd4d4b2de6da0310e1d5228cc63d57adb9eb37c5f5c5a9471131"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "31384887e9ccf6bca66b79aa8e0df82a6a67ba9e8849dbf0b0c0e0db75c944d7"
-    sha256 cellar: :any_skip_relocation, all:           "3bf94079d3d55b9bf8e901bf7791937119d4636661f876e411139e7ab09e7658"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ghi"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "2d07813a7b44a6f6ded371b75e1eb61a8962af966777e0e7322eec0df7b8f426"
   end
 
   uses_from_macos "ruby"
