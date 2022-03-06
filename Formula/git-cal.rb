4eb67df669967620d0247fa5eb31e688649c0e39
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-06 10:21:39 +0800

    git-cal: update 0.9.1_1 bottle.

--- Formula/git-cal.rb
+++ Formula/git-cal.rb
@@ -5,20 +5,17 @@ class GitCal < Formula
   sha256 "783fa73197b349a51d90670480a750b063c97e5779a5231fe046315af0a946cd"
   license "MIT"
   revision 1
-  head "https://github.com/k4rthik/git-cal.git"
+  head "https://github.com/k4rthik/git-cal.git", branch: "master"
 
   livecheck do
     url :stable
     strategy :github_latest
   end
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d2100e367528b52d5bf60d1e85687908e154fc8f831ef7bd29862b3bc899395c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1d191bdf9da21ef2dbe3eeb3909fbf738df652931b5ee9876b9868b429644899"
-    sha256 cellar: :any_skip_relocation, catalina:      "ee5e258bbc598978be1d2e3e3220c28b7ef1ff4d7e5a34bdcc852107f68b5f67"
-    sha256 cellar: :any_skip_relocation, mojave:        "80bbebc06dc4f05e6aa34324276650f303a714efe857e72f67861d7cf9194451"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "62d35c98bb021280c82914245ee51078760261e8ae8e877df3ddb7bfe8726231"
-    sha256 cellar: :any_skip_relocation, all:           "04da9240bed39d9856a9f8615073a9117b17e5aedfaaf1c988627dfae35f9d95"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-cal"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "3bf18ff2ed64b3086e0d5fccb58cd2f05f6bcb68ba15d0e27d92e4ec8bf597ce"
   end
 
   def install
