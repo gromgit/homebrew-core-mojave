e30d0c5f34d7c8cdbc8844205b8001846d874add
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-06 22:46:07 +0800

    faiss: update 1.7.2 bottle.

--- Formula/faiss.rb
+++ Formula/faiss.rb
@@ -1,8 +1,8 @@
 class Faiss < Formula
   desc "Efficient similarity search and clustering of dense vectors"
   homepage "https://github.com/facebookresearch/faiss"
-  url "https://github.com/facebookresearch/faiss/archive/v1.7.1.tar.gz"
-  sha256 "d676d3107ad41203a49e0afda2630519299dc8666f8d23322cbe1eac0c431871"
+  url "https://github.com/facebookresearch/faiss/archive/v1.7.2.tar.gz"
+  sha256 "d49b4afd6a7a5b64f260a236ee9b2efb760edb08c33d5ea5610c2f078a5995ec"
   license "MIT"
 
   livecheck do
@@ -10,13 +10,9 @@ class Faiss < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-  bottle do
-    sha256 cellar: :any, arm64_monterey: "229653a8cfb5bb2c75fadbbb26eac7ec2b1d156b6916445b11a8870dad9e5af2"
-    sha256 cellar: :any, arm64_big_sur:  "6c6e739ef44832c1d17f1d3bd330dde1a8f1c68ca691362061f48672d0081fa7"
-    sha256 cellar: :any, monterey:       "d95cf0e5c01bb7b748e79f95c5aff23c3f4a738cc70b6e7fc1e70ed832843c0e"
-    sha256 cellar: :any, big_sur:        "ca98be5aa73a7f48fb1e22bcae35687f0503a76f1ff00be7d56c88e0dcf6f111"
-    sha256 cellar: :any, catalina:       "d0ec19a749f8bf7a5a48cb828d7f6f1b7da9dff457d47c83b4a34cfedfb6d58a"
-    sha256 cellar: :any, mojave:         "a719758b2de8c7489bdcbd795b2d1c81780c82a191c3baf2477c71fb38c8ff2f"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/faiss"
+    sha256 cellar: :any, mojave: "37d8833cef9a1b7d355d7e063c4fe7c43a1709eb6f88a71b9ed6397f6f2a5f7a"
   end
 
   depends_on "cmake" => :build
