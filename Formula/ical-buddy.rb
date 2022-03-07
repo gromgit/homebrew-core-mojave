9138278328ee27d9d9ef088314a3d2a5512c898f
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-07 11:44:21 +0800

    ical-buddy: update 1.10.1 bottle.

--- Formula/ical-buddy.rb
+++ Formula/ical-buddy.rb
@@ -4,14 +4,12 @@ class IcalBuddy < Formula
   url "https://github.com/DavidKaluta/icalBuddy64/releases/download/v1.10.1/icalBuddy-v1.10.1.zip"
   sha256 "720a6a3344ce32c2cab7c3d2b686ad8de8d9744b747ac48b275247ed54cb3945"
   license "MIT"
-  head "https://github.com/DavidKaluta/icalBuddy64.git"
+  head "https://github.com/DavidKaluta/icalBuddy64.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "fde583324695c0393cad4e545697c010d2e14dca39281ceff644dee8ed9230ab"
-    sha256 cellar: :any_skip_relocation, big_sur:       "41f2928f8a9b5862e9864f5663e6f9cf179e8cfcd95305a41c7c610f7713446d"
-    sha256 cellar: :any_skip_relocation, catalina:      "4f621e8b12e2c2e5e7c9fdd97ee973b7d4b14ce58eb5a5f7a9db32243f0f99f1"
-    sha256 cellar: :any_skip_relocation, mojave:        "4f621e8b12e2c2e5e7c9fdd97ee973b7d4b14ce58eb5a5f7a9db32243f0f99f1"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "4f621e8b12e2c2e5e7c9fdd97ee973b7d4b14ce58eb5a5f7a9db32243f0f99f1"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ical-buddy"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "efc6e18c606d05fbfba0cd4604fdf9dd43aac9e616a8f08d2b6fe908ce4c2e67"
   end
 
   depends_on :macos
