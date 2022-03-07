c91592e7d1ecf2eab7abe3b6c1195b331b6e50ad
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-07 13:12:27 +0800

    json-table: update 4.3.3 bottle.

--- Formula/json-table.rb
+++ Formula/json-table.rb
@@ -4,19 +4,12 @@ class JsonTable < Formula
   url "https://github.com/micha/json-table/archive/4.3.3.tar.gz"
   sha256 "0ab7bb2a705ad3399132060b30b32903762473ff79b5a6e6f52f086e507b0911"
   license "EPL-1.0"
-  head "https://github.com/micha/json-table.git"
+  head "https://github.com/micha/json-table.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b90e09e62cd09817662e86c0c9854cd7a6e02b128dec008eeaa24d6c89482f11"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "23e482349f2641ceb4b17f3c05bba8dcbef599a2d3b7eba981dbcce14989e157"
-    sha256 cellar: :any_skip_relocation, monterey:       "91ed4ccbdeaf3571036d17d31f6a093e867fc8ec6f1c254c7f79342fcb3ab7cb"
-    sha256 cellar: :any_skip_relocation, big_sur:        "56d0c75307c1bfff97077e37070ee7a0532b09614226e5716e7d6a5ad2e2b113"
-    sha256 cellar: :any_skip_relocation, catalina:       "61d89ff5426049b2f0fbb237862ec1227cd36c8f00ce81f9a382e8b7d1c2792a"
-    sha256 cellar: :any_skip_relocation, mojave:         "49f7c1f8e757456f63d5e8b9cb7ef0f2b1cdb22303b2ae799595305cb7e65c5b"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "2afef9b965070bcde986759dbd03cfd8fe3e77aec5a12158fb4aa189626ab977"
-    sha256 cellar: :any_skip_relocation, sierra:         "e5ed8ece1e10ede4417f347703f1e62bb417c65a11f6cac5f10915d44359eb5b"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "3240813838be9e797fd443e5f51d6bb53fc56a8d958dd361cbc865de003619bd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c2a8b0576a8a0be8e8bc9bcafcb70ad0fd98197a2f465d4713163fff48ba7696"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/json-table"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "cd178eba5b2ebb984496aed88ee62817ebb239358c6593694af51aa340080bd0"
   end
 
   def install
