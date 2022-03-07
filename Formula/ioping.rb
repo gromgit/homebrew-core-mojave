419d0c25da6034c28505b7352537e3ba6c96f46b
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-07 12:34:50 +0800

    ioping: update 1.2 bottle.

--- Formula/ioping.rb
+++ Formula/ioping.rb
@@ -4,17 +4,12 @@ class Ioping < Formula
   url "https://github.com/koct9i/ioping/archive/v1.2.tar.gz"
   sha256 "d3e4497c653a1e96df67c72ce2b70da18e9f5e3b93179a5bb57a6e30ceacfa75"
   license "GPL-3.0-or-later"
-  head "https://github.com/koct9i/ioping.git"
+  head "https://github.com/koct9i/ioping.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "152051de44d80aee0e50f7d96b0a548335150e40bd0ca176bfbcda12ec785846"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e8b319c5bec1083064f584e6760a1db5a93e588e64983f0306e474ae277357a7"
-    sha256 cellar: :any_skip_relocation, monterey:       "a13430f2776847bbe161f5a68267cc39de49b819cb72076dae13a04c6923113e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "5a4e1fd55d87324605c6af3b70fecb3780ab00ddea13fc058a7911688014ef35"
-    sha256 cellar: :any_skip_relocation, catalina:       "622678afe9bf88bae08cc264dece76f0abefc854915f5b3d5355cde767aa61e1"
-    sha256 cellar: :any_skip_relocation, mojave:         "4c88038d68f17bbc405c5ed253542890e0fc1e44ece8650f1a68b6ff6df7fabf"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "9a5ee7cd526c89d70c75fe6fcf61d7b0a777d8bf3a823fe99348864a9838b6ff"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "821ef4ae7a4bb2a8a26f6b812556cbd3f079fc4bd87eeca9303d1dec1f8bc4dc"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ioping"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "33b14ba4c776058e7d8c7ea5dc81f8e080552049f8f6cf77b5f97c2b31fb2114"
   end
 
   def install
