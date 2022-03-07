ad831364e5552a4abd5dbd73d6e5d4b5f73056d2
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-07 12:50:07 +0800

    jbigkit: update 2.1 bottle.

--- Formula/jbigkit.rb
+++ Formula/jbigkit.rb
@@ -5,25 +5,17 @@ class Jbigkit < Formula
   mirror "https://deb.debian.org/debian/pool/main/j/jbigkit/jbigkit_2.1.orig.tar.gz"
   sha256 "de7106b6bfaf495d6865c7dd7ac6ca1381bd12e0d81405ea81e7f2167263d932"
   license "GPL-2.0"
-  head "https://www.cl.cam.ac.uk/~mgk25/git/jbigkit", using: :git
+  head "https://www.cl.cam.ac.uk/~mgk25/git/jbigkit", using: :git, branch: "master"
 
   livecheck do
     url :homepage
     regex(/href=.*?jbigkit[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "602ec1a2e779e96d08344017bb931518b8c4ae9b367d7d63dbbb6ffefaaf5299"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cda73dea9c469f1ad380c7fe90b75dfe22d1dcc9ba51593ba59493656cf76c94"
-    sha256 cellar: :any_skip_relocation, monterey:       "ed0440252fa7dc1d13a985498d56037c1bcb0c56fdc7220081ebf7a623524bd6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "568ea0a6734dc1da5d50b5261f43753f7cf1089fae9c786e7859a8ec22562144"
-    sha256 cellar: :any_skip_relocation, catalina:       "16936e06d59fe44d40a3829bc60fec43cb7ca23d54b5fdf9510aca78df648460"
-    sha256 cellar: :any_skip_relocation, mojave:         "887d4f100ed2264220232720a7732a969ee97df32a1c87f03897952920b6019a"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "c8a003d12559b6f506fbd912c3b68163f7ab6022fd53e069bfbd55c813f52df5"
-    sha256 cellar: :any_skip_relocation, sierra:         "831dd1ec7e8013ddc6c23641a21292eae26f397e8b61d95382a6240f18fc5602"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "bdec08cd92dd59183b698c6bbd9072881fdfce64b4ecb6182e405e0f2ad26c00"
-    sha256 cellar: :any_skip_relocation, yosemite:       "764396342e87b84253aa06f5046f90c778cacca998ce970900cb2fdf1cfdc3fa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8d787da566c3d674b9ebc93fcf4291ca28325366fad703f3a90451bd6fbfbac1"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jbigkit"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "ad06857ef40ecf219d54572bc9fcd1af4dc50a7f3c4a4a95d42b54309eb9c7ea"
   end
 
   conflicts_with "netpbm", because: "both install `pbm.5` and `pgm.5` files"
