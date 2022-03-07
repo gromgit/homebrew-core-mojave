3692f415ec58b4076d84c85a9608139e0aa955ae
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-07 13:06:33 +0800

    jpegrescan: update 1.1.0 bottle.

--- Formula/jpegrescan.rb
+++ Formula/jpegrescan.rb
@@ -3,10 +3,12 @@ class Jpegrescan < Formula
   homepage "https://github.com/kud/jpegrescan"
   url "https://github.com/kud/jpegrescan/archive/1.1.0.tar.gz"
   sha256 "a8522e971d11c904f4b61af665c3be800f26404e2b14f5f80c675b4a72a42b32"
-  head "https://github.com/kud/jpegrescan.git"
+  head "https://github.com/kud/jpegrescan.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "a09a6149fe87f3cc7a2f54b9b3c538bc6881d3b4e7edc6f947ed54d29d4ad473"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jpegrescan"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "eef4b8279e4d63a3e68a83a365d9cecb54ce8464ce94d89b4e4089c80e4b3034"
   end
 
   depends_on "jpeg"
