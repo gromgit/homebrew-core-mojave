e43e149f5e4c95d6349d6d9638c9d1951857be55
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-07 11:25:18 +0800

    hr: update 1.3 bottle.

--- Formula/hr.rb
+++ Formula/hr.rb
@@ -4,10 +4,12 @@ class Hr < Formula
   url "https://github.com/LuRsT/hr/archive/1.3.tar.gz"
   sha256 "258ff3121369d17c5c70fa18e466ac01cdb4cf890c605f7a5e706d5b1a3afebf"
   license "MIT"
-  head "https://github.com/LuRsT/hr.git"
+  head "https://github.com/LuRsT/hr.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "01e0e362b30cb5eb92751375426626c3eb8d6bfd05aa61d3cbba188a12bd1b92"
+bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/hr"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "4c98d40a979dbd2c905c79eae280575dc320dbaa9528da0547cc4d19686b8b61"
   end
 
   def install
