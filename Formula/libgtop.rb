d13a0d9ad5f5769648a9093bcca99ea7f65b6f52
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:21:39 +0800

    libgtop: no rebuilding required

--- Formula/libgtop.rb
+++ Formula/libgtop.rb
@@ -53,9 +53,7 @@ class Libgtop < Formula
       -lglib-2.0
       -lgtop-2.0
     ]
-    on_macos do
-      flags << "-lintl"
-    end
+    flags << "-lintl" if OS.mac?
     system ENV.cc, "test.c", "-o", "test", *flags
     system "./test"
   end
