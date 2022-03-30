33904c921426169a0b3cc3c1c7e6d3ee0085b342
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:33:32 +0800

    sphinx: no rebuilding required

--- Formula/sphinx.rb
+++ Formula/sphinx.rb
@@ -9,12 +9,14 @@ class Sphinx < Formula
 
   bottle do
     rebuild 2
-    sha256 arm64_big_sur: "57a2dc9f3c5c40d46785753531e4801a7db0560c11434a10d224efeba3c2c1b2"
-    sha256 big_sur:       "bbaebbfc31099a28b528c679a2c7825e218e42d83d04a4b0dc53561e70fcbdca"
-    sha256 catalina:      "f3d89ffcd2926373af5a35bb7ae6f16e59074699eeacfb4d358a0dc5742729cc"
-    sha256 mojave:        "61f1ae14e253c8c84f0e8a9f3a26833ca4a1da887d97c0df8ecebb6096222546"
-    sha256 high_sierra:   "3daf6e565c7c12803c13b6439a872e61335b3b27c06719ca6f8cec93dcd2176e"
-    sha256 x86_64_linux:  "3736780f2809b0449953cfb4ed85cc7d65b86e6c5c55aa98186062f8aff81d99"
+    sha256 arm64_monterey: "1b9d46aed9d62eff25aba72b20380e2b03fc04851b9309fbce3ffc071f2760c7"
+    sha256 arm64_big_sur:  "57a2dc9f3c5c40d46785753531e4801a7db0560c11434a10d224efeba3c2c1b2"
+    sha256 monterey:       "17c8dbbdd7f5abf50a195e72dc3eee1bea414490ecffa97473e8ed99dff90aea"
+    sha256 big_sur:        "bbaebbfc31099a28b528c679a2c7825e218e42d83d04a4b0dc53561e70fcbdca"
+    sha256 catalina:       "f3d89ffcd2926373af5a35bb7ae6f16e59074699eeacfb4d358a0dc5742729cc"
+    sha256 mojave:         "61f1ae14e253c8c84f0e8a9f3a26833ca4a1da887d97c0df8ecebb6096222546"
+    sha256 high_sierra:    "3daf6e565c7c12803c13b6439a872e61335b3b27c06719ca6f8cec93dcd2176e"
+    sha256 x86_64_linux:   "3736780f2809b0449953cfb4ed85cc7d65b86e6c5c55aa98186062f8aff81d99"
   end
 
   depends_on "mysql@5.7"
