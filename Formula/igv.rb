6ff857d654e6f4061554706e53604639651c24d2
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:18:06 +0800

    igv: no rebuilding required

--- Formula/igv.rb
+++ Formula/igv.rb
@@ -1,8 +1,8 @@
 class Igv < Formula
   desc "Interactive Genomics Viewer"
   homepage "https://www.broadinstitute.org/software/igv"
-  url "https://data.broadinstitute.org/igv/projects/downloads/2.12/IGV_2.12.2.zip"
-  sha256 "0189e89b4d44fa6788725a78c3da22b31bd00b1aa950ccfd051720c82ce06b7b"
+  url "https://data.broadinstitute.org/igv/projects/downloads/2.12/IGV_2.12.3.zip"
+  sha256 "c87a109deb35994e1b28dee80b5acfd623ec3257f031fcd9cfce008cd32a4cf2"
   license "MIT"
 
   livecheck do
@@ -11,7 +11,7 @@ class Igv < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "c54796d261bf840af369acf83a00830a72cac34cb3b1435fa86c45a4e5fcfd58"
+    sha256 cellar: :any_skip_relocation, all: "afd31a0a8579fb49ed222e8ec6959b7386044e10a646eb2c8f20dc2e71ef3e1f"
   end
 
   depends_on "openjdk"
