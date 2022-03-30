2787faa3e7ba963ded36eecff6ab0fcead741a78
Adrian Ho <the.gromgit@gmail.com>
2022-03-30 18:26:19 +0800

    aws-vault: update 6.5.0 bottle.

--- Formula/aws-vault.rb
+++ Formula/aws-vault.rb
@@ -5,9 +5,15 @@ class AwsVault < Formula
   sha256 "b2127b20a58fd75d92a2af28e77368dd9ab3c576dc73c3d56c3817e496a45515"
   license "MIT"
 
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/aws-vault"
-    sha256 cellar: :any_skip_relocation, mojave: "a71e529d1c66830f575429942b1c115764514905fcb00945b5009b08d62cc959"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "fbf2c607df9b8f189596ffee8395a9cea162a804895bfccc3cb7b080b153c700"
   end
 
   depends_on "go" => :build
