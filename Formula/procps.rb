afe96c91a4dc7562e6d96d7cf468ea1af25f3504
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:29:59 +0800

    procps:

--- Formula/procps.rb
+++ Formula/procps.rb
@@ -1,10 +1,14 @@
 class Procps < Formula
   desc "Utilities for browsing procfs"
   homepage "https://gitlab.com/procps-ng/procps"
-  url "https://gitlab.com/procps-ng/procps/-/archive/v3.3.17/procps-v3.3.17.tar.gz"
-  sha256 "efa6f6b4625a795f5c8a3d5bd630a121d270bc8573c5a0b6a6068e73611d6cd5"
+  url "https://gitlab.com/procps-ng/procps/-/archive/v4.0.0/procps-v4.0.0.tar.gz"
+  sha256 "dea39e0e7b1367e28c887d736d1a9783df617497538603cdff432811a1016945"
   license "GPL-2.0-or-later"
   head "https://gitlab.com/procps-ng/procps.git", branch: "master"
+
+  bottle do
+    sha256 x86_64_linux: "efab0532e0905c46e47f2e5ea9ac3cc413eba0b5a3a8531b04587b5592af561d"
+  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
