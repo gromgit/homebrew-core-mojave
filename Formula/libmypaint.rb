8952523f09f6e9a192c591d7cf2310203eca75a6
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:22:01 +0800

    libmypaint: no rebuilding required

--- Formula/libmypaint.rb
+++ Formula/libmypaint.rb
@@ -7,13 +7,14 @@ class Libmypaint < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "b481fb4e3ed5cb542d1ef073a5852a0a65361f0825051302ccdd6bc224901d90"
-    sha256 cellar: :any, arm64_big_sur:  "4f5f706833fb183d4ad43a0b065b2b767a7787e7963eabced95016bd04ffdd12"
-    sha256 cellar: :any, monterey:       "30623690f18dafe72d96daad871d4f7018ab3e89970ebdeda2fbf2d56c781c68"
-    sha256 cellar: :any, big_sur:        "65d3c8c494c5e3a454526e4254c4f4c1a1883ca1e99c2dcb09c2abdff141d72a"
-    sha256 cellar: :any, catalina:       "699014970a67055822e7ee2abc92c4ea2b45e51bcd58cfa01cb24c2ed08f6a2b"
-    sha256 cellar: :any, mojave:         "97ca6e5c0ae27513cc3af20c1256548d6a21e0a38bfdcea5a79f7fe1c0a6886d"
-    sha256 cellar: :any, high_sierra:    "4260697ececf5344aa3eacd16afdd5f4eff556cee6312e49a8e5544edb71aca1"
+    sha256 cellar: :any,                 arm64_monterey: "b481fb4e3ed5cb542d1ef073a5852a0a65361f0825051302ccdd6bc224901d90"
+    sha256 cellar: :any,                 arm64_big_sur:  "4f5f706833fb183d4ad43a0b065b2b767a7787e7963eabced95016bd04ffdd12"
+    sha256 cellar: :any,                 monterey:       "30623690f18dafe72d96daad871d4f7018ab3e89970ebdeda2fbf2d56c781c68"
+    sha256 cellar: :any,                 big_sur:        "65d3c8c494c5e3a454526e4254c4f4c1a1883ca1e99c2dcb09c2abdff141d72a"
+    sha256 cellar: :any,                 catalina:       "699014970a67055822e7ee2abc92c4ea2b45e51bcd58cfa01cb24c2ed08f6a2b"
+    sha256 cellar: :any,                 mojave:         "97ca6e5c0ae27513cc3af20c1256548d6a21e0a38bfdcea5a79f7fe1c0a6886d"
+    sha256 cellar: :any,                 high_sierra:    "4260697ececf5344aa3eacd16afdd5f4eff556cee6312e49a8e5544edb71aca1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6114302a8ff4e54cd64388fb0968dbb1fa4ab546bb9d2bbca786da787ec3bf62"
   end
 
   depends_on "intltool" => :build
@@ -21,7 +22,11 @@ class Libmypaint < Formula
   depends_on "gettext"
   depends_on "json-c"
 
+  uses_from_macos "perl" => :build
+
   def install
+    ENV.prepend_path "PERL5LIB", Formula["intltool"].libexec/"lib/perl5" unless OS.mac?
+
     system "./configure", "--disable-introspection",
                           "--without-glib",
                           "--prefix=#{prefix}"
