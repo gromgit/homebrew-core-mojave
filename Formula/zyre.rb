418b2213bd622449fe00cf07a67746de5589e0ad
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:39:19 +0800

    zyre: no rebuilding required

--- Formula/zyre.rb
+++ Formula/zyre.rb
@@ -6,12 +6,13 @@ class Zyre < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "97e9c9802ff4f1e0b329da1cbe426647bc55af13990e27e03e80dbd13e4a4838"
-    sha256 cellar: :any, arm64_big_sur:  "3b1d36e1f9441e338916cbc75e8701386fbeaa4c23a231061c4d6d08bc35a3f1"
-    sha256 cellar: :any, monterey:       "5a85a14ca28dc1d832545421e55cc4e5fc6e1007bbd3bcb8be36266904eacb35"
-    sha256 cellar: :any, big_sur:        "490a76ad5536efec4b40234fd693f67f7f4b0222672e0b0f39c36d2581b0f4ee"
-    sha256 cellar: :any, catalina:       "3fca3e3402fa228c40c3e2263520be64b59c414d1454b7799bb284d711a75d62"
-    sha256 cellar: :any, mojave:         "bea4248272a0c99db13a9f8c48cbbbdd1c9927b9b206689ad3b558eadef102b1"
+    sha256 cellar: :any,                 arm64_monterey: "97e9c9802ff4f1e0b329da1cbe426647bc55af13990e27e03e80dbd13e4a4838"
+    sha256 cellar: :any,                 arm64_big_sur:  "3b1d36e1f9441e338916cbc75e8701386fbeaa4c23a231061c4d6d08bc35a3f1"
+    sha256 cellar: :any,                 monterey:       "5a85a14ca28dc1d832545421e55cc4e5fc6e1007bbd3bcb8be36266904eacb35"
+    sha256 cellar: :any,                 big_sur:        "490a76ad5536efec4b40234fd693f67f7f4b0222672e0b0f39c36d2581b0f4ee"
+    sha256 cellar: :any,                 catalina:       "3fca3e3402fa228c40c3e2263520be64b59c414d1454b7799bb284d711a75d62"
+    sha256 cellar: :any,                 mojave:         "bea4248272a0c99db13a9f8c48cbbbdd1c9927b9b206689ad3b558eadef102b1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b873e7ea6e908ca4c97adcf840eaf865e1dc827716a99b2fe8d7b7ea56fc0991"
   end
 
   head do
