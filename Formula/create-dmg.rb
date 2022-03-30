902d6359921ba607e4177d3d52e057849ace9b7e
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:09:55 +0800

    create-dmg: no rebuilding required

--- Formula/create-dmg.rb
+++ Formula/create-dmg.rb
@@ -1,13 +1,15 @@
 class CreateDmg < Formula
   desc "Shell script to build fancy DMGs"
   homepage "https://github.com/create-dmg/create-dmg"
-  url "https://github.com/create-dmg/create-dmg/archive/v1.0.9.tar.gz"
-  sha256 "f0284d0a386353cd127df2f15af526c821995c664ba4063863ee500a374aada4"
+  url "https://github.com/create-dmg/create-dmg/archive/v1.0.10.tar.gz"
+  sha256 "8fd43498988f6d334d483faf4e4a330a25228784995d72c57e4565967d09e6ab"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "fbbb2e726579852cfa16abe27bc34bf78a2df0c991a9d9d6994c2ffbd5c2773d"
+    sha256 cellar: :any_skip_relocation, all: "2f7ff645731a8108b32f4aba84a8b501177c5a4de47d0a2c8db23b5119fd7c92"
   end
+
+  depends_on :macos
 
   def install
     system "make", "install", "prefix=#{prefix}"
