a58582782a311275260e88cbcead6962a4b4a611
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:36:30 +0800

    ultralist: no rebuilding required

--- Formula/ultralist.rb
+++ Formula/ultralist.rb
@@ -16,7 +16,8 @@ class Ultralist < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c1d287b06c14adcd326d8dd626209fcfa7a287919dce6f755f80a6513b2ed6e7"
   end
 
-  depends_on "go" => :build
+  # Bump to 1.18 on the next release, if possible.
+  depends_on "go@1.17" => :build
 
   def install
     system "go", "build", *std_go_args
