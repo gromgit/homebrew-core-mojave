8e9a1bbbdfa39ee3994de244aa8d26f5d8e1502b
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:24:43 +0800

    memcached:

--- Formula/memcached.rb
+++ Formula/memcached.rb
@@ -12,8 +12,12 @@ class Memcached < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/memcached"
-    sha256 cellar: :any, mojave: "2bfd9b7dee2d485414aecfe4473d1490fd1426bb2ad785f82d4f19108c690046"
+    sha256 cellar: :any,                 arm64_monterey: "a6a8fd0dfe19188ea6e42fc750c8ed3a09ba34b3bfa80694fe6c1a398e051763"
+    sha256 cellar: :any,                 arm64_big_sur:  "01603a7f7791ced3c8271e0e036d19dbea9afeb39e75d6b057af4c058521da31"
+    sha256 cellar: :any,                 monterey:       "0757348fb5c89a131dbe3a9b8dee3b76a2341bf6a264d5796d480969a20e8ddb"
+    sha256 cellar: :any,                 big_sur:        "632a9163d97df61899d84e10e7dfceb788fbe5da01a2147a445b07288b7800b9"
+    sha256 cellar: :any,                 catalina:       "d09725fb66e581866a8e7a996500323d6781b1390fd4921b876c546a9e28e390"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "27cf32dbaa1858fd5d595dff3c706c448cdfb6406b42b11dd136ac8c0245ae37"
   end
 
   depends_on "libevent"
@@ -39,9 +43,7 @@ class Memcached < Formula
       --daemon
       --pidfile=#{pidfile}
     ]
-    on_linux do
-      args << "--user=#{ENV["USER"]}" if ENV["HOMEBREW_GITHUB_ACTIONS"]
-    end
+    args << "--user=#{ENV["USER"]}" if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
     system bin/"memcached", *args
     sleep 1
     assert_predicate pidfile, :exist?, "Failed to start memcached daemon"
