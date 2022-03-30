15a4e40d1bbb778beb48fc6d80392710c2b5ff1b
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:37:40 +0800

    wartremover: no rebuilding required

--- Formula/wartremover.rb
+++ Formula/wartremover.rb
@@ -4,20 +4,20 @@ class Wartremover < Formula
   url "https://github.com/wartremover/wartremover/archive/v2.4.18.tar.gz"
   sha256 "7bfa3ee9ebfef06e880ac1890831255e3f4ba8a20dfab9940d02ce70841e47bd"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/wartremover/wartremover.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "582ff0ed5e2627e8dcfd360af6bd48958a67b9c495da68166ee28f01dae5f95b"
+    sha256 cellar: :any_skip_relocation, all: "da26653b22a38d54efa81e9e1d31befe857a932827ae5449611a7d9fc893314f"
   end
 
   depends_on "sbt" => :build
-  depends_on "openjdk@8"
+  depends_on "openjdk"
 
   def install
-    system "sbt", "-sbt-jar", Formula["sbt"].opt_libexec/"bin/sbt-launch.jar",
-                    "core/assembly"
+    system "sbt", "-sbt-jar", Formula["sbt"].opt_libexec/"bin/sbt-launch.jar", "core/assembly"
     libexec.install "wartremover-assembly.jar"
-    bin.write_jar_script libexec/"wartremover-assembly.jar", "wartremover", java_version: "1.8"
+    bin.write_jar_script libexec/"wartremover-assembly.jar", "wartremover"
   end
 
   test do
