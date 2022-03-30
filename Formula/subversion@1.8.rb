f12dabe342498bccee933744d0e287d88e82264a
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:34:23 +0800

    subversion@1.8: no rebuilding required

--- Formula/subversion@1.8.rb
+++ Formula/subversion@1.8.rb
@@ -16,6 +16,9 @@ class SubversionAT18 < Formula
 
   keg_only :versioned_formula
 
+  # depends on python@2
+  deprecate! date: "2020-12-21", because: :does_not_build
+
   depends_on "pkg-config" => :build
   depends_on "scons" => :build # For Serf
   depends_on "apr"
