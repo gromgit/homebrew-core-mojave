43d3dfc23b9fc6f3e364629e7cf4a223d3f51449
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:31:17 +0800

    qtads: no rebuilding required

--- Formula/qtads.rb
+++ Formula/qtads.rb
@@ -14,6 +14,7 @@ class Qtads < Formula
   bottle do
     sha256 cellar: :any, arm64_monterey: "ee81b4a35b37b3917513e52e3330011529dc4911a1cbfe56117d7286e3b56bdc"
     sha256 cellar: :any, arm64_big_sur:  "efb0917e30907f43e0d75d4593842bc30f89694913d0fa4d6a78b312862cc9cd"
+    sha256 cellar: :any, monterey:       "ce3ce044126f12c53557af8db60491b3196b4c5ceada3cc2f8ddd574e6b84c83"
     sha256 cellar: :any, big_sur:        "d3f92978a0454521d20577fda97c3cef034e60be673f06bcef5db44376e7ed3f"
     sha256 cellar: :any, catalina:       "16bb2be33f9757e197011ec42652b90e8220203cb6b59a261dfd2658707adc9e"
     sha256 cellar: :any, mojave:         "01215c3c22a58a47a1b4ff91e7a1558a301d0620c50b553f77f0fb30d75d8ae9"
