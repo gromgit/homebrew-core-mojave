34db19b17771f2aa29e2d513eb6298eb2b53e881
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:09:04 +0800

    clutter: no rebuilding required

--- Formula/clutter.rb
+++ Formula/clutter.rb
@@ -13,6 +13,7 @@ class Clutter < Formula
     sha256 catalina:       "ccec39ce9c941de753798e466b8cfc2a69612319d8b5a422f6e4bde49db305b1"
     sha256 mojave:         "43da6f50107059a3c9b215e77d29724f9e71a17fd89f5e72a200cd021e32f471"
     sha256 high_sierra:    "2a1f93e956dbfc9dc4f3c47dd8923b224ed155f3b8dbf32df74f365a65052bbb"
+    sha256 x86_64_linux:   "90f6ab166d1dba5dc5bfb9760dfc54ae8d20ac16948ea76783aedb16499487e5"
   end
 
   depends_on "gobject-introspection" => :build
@@ -42,9 +43,15 @@ class Clutter < Formula
       --disable-examples
       --disable-gtk-doc-html
       --enable-gdk-pixbuf=yes
-      --without-x --enable-x11-backend=no
-      --enable-quartz-backend=yes
     ]
+
+    if OS.mac?
+      args += %w[
+        --without-x
+        --enable-x11-backend=no
+        --enable-quartz-backend=yes
+      ]
+    end
 
     system "./configure", *args
     system "make", "install"
@@ -106,11 +113,11 @@ class Clutter < Formula
       -lglib-2.0
       -lgmodule-2.0
       -lgobject-2.0
-      -lintl
       -ljson-glib-1.0
       -lpango-1.0
       -lpangocairo-1.0
     ]
+    flags << "-lintl" if OS.mac?
     system ENV.cc, "test.c", "-o", "test", *flags
     system "./test"
   end
