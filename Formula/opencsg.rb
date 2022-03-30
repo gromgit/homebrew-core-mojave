70636d328990a771f1d221d73882a6c97ba7d172
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:27:44 +0800

    opencsg: no rebuilding required

--- Formula/opencsg.rb
+++ Formula/opencsg.rb
@@ -11,11 +11,13 @@ class Opencsg < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "d1bd35e73ad29214d8a5de83a4e0b80385705dbeecce9ed1132afc9eb6c71a61"
-    sha256 cellar: :any, arm64_big_sur:  "c06e0c8e9ceee5ad621e6f650a289f34f30428bce43f6a9efb95621fc7afdafb"
-    sha256 cellar: :any, big_sur:        "f7e6296d4466eea7c516fdca9e382d30fad4194b73969a1158d3d399b59c9381"
-    sha256 cellar: :any, catalina:       "730e0c7b2656e63ac4c55effbb5030fb737bfdc6ecbda700ed37534ae8b0d295"
-    sha256 cellar: :any, mojave:         "7d8c19c4b5c1d26d5f15fb01094977fd58f14ccaa92085a13be68fd27943588a"
+    sha256 cellar: :any,                 arm64_monterey: "d1bd35e73ad29214d8a5de83a4e0b80385705dbeecce9ed1132afc9eb6c71a61"
+    sha256 cellar: :any,                 arm64_big_sur:  "c06e0c8e9ceee5ad621e6f650a289f34f30428bce43f6a9efb95621fc7afdafb"
+    sha256 cellar: :any,                 monterey:       "f02b160d94f4eb0bbc6fe4381368e8c3151b6b3b1cb20f650121f186f8517b25"
+    sha256 cellar: :any,                 big_sur:        "f7e6296d4466eea7c516fdca9e382d30fad4194b73969a1158d3d399b59c9381"
+    sha256 cellar: :any,                 catalina:       "730e0c7b2656e63ac4c55effbb5030fb737bfdc6ecbda700ed37534ae8b0d295"
+    sha256 cellar: :any,                 mojave:         "7d8c19c4b5c1d26d5f15fb01094977fd58f14ccaa92085a13be68fd27943588a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "136c554f3d6e8c3af9f7b1dfd4bada577ed629573622e0ffba09379d88de867d"
   end
 
   depends_on "qt@5" => :build
@@ -47,8 +49,8 @@ class Opencsg < Formula
         Test test;
       }
     EOS
-    system ENV.cxx, "test.cpp", "-o", "test", "-L#{lib}", "-lopencsg",
-           "-framework", "OpenGL"
+    gl_lib = OS.mac? ? ["-framework", "OpenGL"] : ["-lGL"]
+    system ENV.cxx, "test.cpp", "-o", "test", "-L#{lib}", "-lopencsg", *gl_lib
     system "./test"
   end
 end
