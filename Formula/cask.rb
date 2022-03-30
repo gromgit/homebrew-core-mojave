53b3433973785434182ec160e07d45020d39da61
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:08:03 +0800

    cask: no rebuilding required

--- Formula/cask.rb
+++ Formula/cask.rb
@@ -1,13 +1,13 @@
 class Cask < Formula
   desc "Emacs dependency management"
   homepage "https://cask.readthedocs.io/"
-  url "https://github.com/cask/cask/archive/v0.8.7.tar.gz"
-  sha256 "6b664da044e8faef77717f79bb90069ec9e7868e9c47da498057236b409a501b"
+  url "https://github.com/cask/cask/archive/v0.8.8.tar.gz"
+  sha256 "94f99d4161dedda3024312dc6b929be6319aff593a6d31f1cf4f2845ae6ca5c6"
   license "GPL-3.0-or-later"
   head "https://github.com/cask/cask.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "bd85befe31659e948617b988ed604d70199af9e7dd22dcfc8be6d76ecd92ee0b"
+    sha256 cellar: :any_skip_relocation, all: "f50a59d4337bc0a5f0a82cb680cec21ca11c058eec8f22d0319d972af568b162"
   end
 
   depends_on "coreutils"
@@ -15,11 +15,10 @@ class Cask < Formula
 
   def install
     bin.install "bin/cask"
-    prefix.install "templates"
     # Lisp files must stay here: https://github.com/cask/cask/issues/305
     prefix.install Dir["*.el"]
-    elisp.install_symlink "#{prefix}/cask.el"
-    elisp.install_symlink "#{prefix}/cask-bootstrap.el"
+    elisp.install_symlink prefix/"cask.el"
+    elisp.install_symlink prefix/"cask-bootstrap.el"
 
     # Stop cask performing self-upgrades.
     touch prefix/".no-upgrade"
