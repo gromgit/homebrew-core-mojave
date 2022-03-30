b52a7f964f5bbf889e35763a2c0d2ffbe5c2cc5d
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:37:16 +0800

    virtualpg: no rebuilding required

--- Formula/virtualpg.rb
+++ Formula/virtualpg.rb
@@ -11,11 +11,13 @@ class Virtualpg < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "1d87321f13aec1d9ca1b75a9d3a3750f427910aead760d88d94ed4c9fd63e72b"
-    sha256 cellar: :any,                 big_sur:       "b0753a8f3cca894abd6c422479062ed242e6a780c497b94d7a5596009508f678"
-    sha256 cellar: :any,                 catalina:      "68282c2258b52c72bad812eddadef2f9ce0c34e4011ceb43522ec1e2b21bbc4f"
-    sha256 cellar: :any,                 mojave:        "5e14713d8a04acecf93faf9c387f0fcff32b8f5b39ee208b98355d638d60f92a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "63f218f9e93c2976082142736ae74a098b3e0e1e6aa99ad3f5da9669c4d2f68f"
+    sha256 cellar: :any,                 arm64_monterey: "3cf246326fa5bf708a4181fc9c0c1a05b086e0ac2d74a6d6b0ecc3f0605be984"
+    sha256 cellar: :any,                 arm64_big_sur:  "1d87321f13aec1d9ca1b75a9d3a3750f427910aead760d88d94ed4c9fd63e72b"
+    sha256 cellar: :any,                 monterey:       "f5cb68aeeccb43a025faa5165ef05d8218222af780197d05c28af24bb6f098f5"
+    sha256 cellar: :any,                 big_sur:        "b0753a8f3cca894abd6c422479062ed242e6a780c497b94d7a5596009508f678"
+    sha256 cellar: :any,                 catalina:       "68282c2258b52c72bad812eddadef2f9ce0c34e4011ceb43522ec1e2b21bbc4f"
+    sha256 cellar: :any,                 mojave:         "5e14713d8a04acecf93faf9c387f0fcff32b8f5b39ee208b98355d638d60f92a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "63f218f9e93c2976082142736ae74a098b3e0e1e6aa99ad3f5da9669c4d2f68f"
   end
 
   depends_on "libspatialite"
