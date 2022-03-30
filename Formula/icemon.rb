1902a05fc5286a2176152670a1840996454d8053
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:18:02 +0800

    icemon: no rebuilding required

--- Formula/icemon.rb
+++ Formula/icemon.rb
@@ -9,6 +9,7 @@ class Icemon < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "3bdc990f8c84ac25b6f68859b54423b26425d08d40b0b91d2dc034b248e8a73a"
     sha256 cellar: :any,                 arm64_big_sur:  "f310aad5e98a7cf3c6fca8a1ddcda9af425cb7b442512b5c540619de0312cb60"
+    sha256 cellar: :any,                 monterey:       "89f0d341512a55c0c50c60100b69a8b32315e5370289d6ce536c3d9ce5901d8a"
     sha256 cellar: :any,                 big_sur:        "8db3861063efc1a5e53703b41993328968aa637ae4fed60112ac8ff4e364a422"
     sha256 cellar: :any,                 catalina:       "6a80f6cbc858fbc942c5845492ca6bfefdf2dd4f0746f97e28704301585f19aa"
     sha256 cellar: :any,                 mojave:         "34f8c691f2c2d23fc65bc84429f0282c96d47f88036ada6d6f44761287a88441"
@@ -35,12 +36,11 @@ class Icemon < Formula
   end
 
   test do
-    on_macos do
+    if OS.mac?
       system "#{bin}/icemon", "--version"
-    end
-    on_linux do
+    else
       assert_match("qt.qpa.xcb: could not connect to display",
-                   shell_output("#{bin}/icemon --version 2>&1", 134))
+                         shell_output("#{bin}/icemon --version 2>&1", 134))
     end
   end
 end
