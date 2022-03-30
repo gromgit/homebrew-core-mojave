60561f161ba9de511188d3666a72716543d9a3f1
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:29:14 +0800

    phive: no rebuilding required

--- Formula/phive.rb
+++ Formula/phive.rb
@@ -1,12 +1,12 @@
 class Phive < Formula
   desc "Phar Installation and Verification Environment (PHIVE)"
   homepage "https://phar.io"
-  url "https://github.com/phar-io/phive/releases/download/0.15.0/phive-0.15.0.phar"
-  sha256 "72b32bf1de67b15b7bfb3439c4f7a987f257993127ea324556da7798e832941e"
+  url "https://github.com/phar-io/phive/releases/download/0.15.1/phive-0.15.1.phar"
+  sha256 "dfd3804452a20bca40c1c8e5f7a2cd0f470bc442c43f256c890990becc467bb0"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "acc459cd355ad9cfbece1263871399d7d319447038848cb83571e47682d60d5a"
+    sha256 cellar: :any_skip_relocation, all: "33caa5904fa5d90086354429d57e4e4ddf7e2e5e649090f9c7e187e67d75d580"
   end
 
   depends_on "php"
