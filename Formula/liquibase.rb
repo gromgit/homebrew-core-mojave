f6450456131ae879797e9ffa6780a47f366f29ac
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:23:20 +0800

    liquibase: no rebuilding required

--- Formula/liquibase.rb
+++ Formula/liquibase.rb
@@ -1,12 +1,17 @@
 class Liquibase < Formula
   desc "Library for database change tracking"
   homepage "https://www.liquibase.org/"
-  url "https://github.com/liquibase/liquibase/releases/download/v4.8.0/liquibase-4.8.0.tar.gz"
-  sha256 "7462b6e92f7077e1858865c403d52f0dce1bd66d03b1fae907815c10825feb33"
+  url "https://github.com/liquibase/liquibase/releases/download/v4.9.0/liquibase-4.9.0.tar.gz"
+  sha256 "d489d12731ea8a49ba25def3e784d6e89167dc53b42ed4ad7a158d682fab770f"
   license "Apache-2.0"
 
+  livecheck do
+    url "https://liquibase.org/download"
+    regex(/href=.*?liquibase[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "834f5d7086283d639aba0b0df361dc65f84863adcbeef0be4519e440d264ca17"
+    sha256 cellar: :any_skip_relocation, all: "e0fe489113535087e7fbcdb436784d3c0d95dceaa52d2d975f0f985d62f363be"
   end
 
   depends_on "openjdk"
@@ -16,7 +21,7 @@ class Liquibase < Formula
     chmod 0755, "liquibase"
     prefix.install_metafiles
     libexec.install Dir["*"]
-    (bin/"liquibase").write_env_script libexec/"liquibase", JAVA_HOME: Formula["openjdk"].opt_prefix
+    (bin/"liquibase").write_env_script libexec/"liquibase", Language::Java.overridable_java_home_env
     (libexec/"lib").install_symlink Dir["#{libexec}/sdk/lib-sdk/slf4j*"]
   end
 
