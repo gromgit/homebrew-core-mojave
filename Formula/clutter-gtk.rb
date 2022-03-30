20d65466490e836e93c133f3a9f7006241edfe35
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:09:04 +0800

    clutter-gtk: no rebuilding required

--- Formula/clutter-gtk.rb
+++ Formula/clutter-gtk.rb
@@ -6,13 +6,14 @@ class ClutterGtk < Formula
   revision 4
 
   bottle do
-    sha256 arm64_monterey: "28a3283252422dbc864b7beb4c69e377688c33fbb2a0e535c62a9362264162bf"
-    sha256 arm64_big_sur:  "2fe413931adfbb5801835172149dbf10aa1f1cdc669a0a8f973d834a6251fe0f"
-    sha256 monterey:       "3940367b212abbe0c266ea8982aa82cc890d8ab8f24c2c851362283a0aef5a3f"
-    sha256 big_sur:        "4d0e9365bfeff618403fb3fa6db9319eb684b4c614af80f1ad6ed0f0fec57db1"
-    sha256 catalina:       "4b5c17d3567f1a5c03f98ccc8d9275f07fa733770d4ee741800505b8894442a1"
-    sha256 mojave:         "665ed256370965e0f1e8660bf1f0372a7e5a9ea0fde176c06cb4c8e390a0739c"
-    sha256 high_sierra:    "90929f36d6105b2b046e32f2d661c91305b785da535a7654fde19eb89617008e"
+    sha256                               arm64_monterey: "28a3283252422dbc864b7beb4c69e377688c33fbb2a0e535c62a9362264162bf"
+    sha256                               arm64_big_sur:  "2fe413931adfbb5801835172149dbf10aa1f1cdc669a0a8f973d834a6251fe0f"
+    sha256                               monterey:       "3940367b212abbe0c266ea8982aa82cc890d8ab8f24c2c851362283a0aef5a3f"
+    sha256                               big_sur:        "4d0e9365bfeff618403fb3fa6db9319eb684b4c614af80f1ad6ed0f0fec57db1"
+    sha256                               catalina:       "4b5c17d3567f1a5c03f98ccc8d9275f07fa733770d4ee741800505b8894442a1"
+    sha256                               mojave:         "665ed256370965e0f1e8660bf1f0372a7e5a9ea0fde176c06cb4c8e390a0739c"
+    sha256                               high_sierra:    "90929f36d6105b2b046e32f2d661c91305b785da535a7654fde19eb89617008e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ae50137a8bf0f24cc4ea36fe43e5d16f295968ab2efa30c9af8abcb5513ba70f"
   end
 
   depends_on "gobject-introspection" => :build
@@ -114,11 +115,11 @@ class ClutterGtk < Formula
       -lgmodule-2.0
       -lgobject-2.0
       -lgtk-3
-      -lintl
       -ljson-glib-1.0
       -lpango-1.0
       -lpangocairo-1.0
     ]
+    flags << "-lintl" if OS.mac?
     system ENV.cc, "test.c", "-o", "test", *flags
     system "./test"
   end
