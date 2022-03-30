75923722b95c3afb091f0b1246fdbdb1451f4295
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:09:56 +0800

    csfml: no rebuilding required

--- Formula/csfml.rb
+++ Formula/csfml.rb
@@ -8,12 +8,13 @@ class Csfml < Formula
   head "https://github.com/SFML/CSFML.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "a8c50f1c117a704b3630dbf4047980de47bf2de5a5a865ff9d2b011028703b67"
-    sha256 cellar: :any, arm64_big_sur:  "d2021fddf559448c443461ccfab70aaeb69c6067599404bb181dc1b979c0c0ea"
-    sha256 cellar: :any, monterey:       "d172f3a39ed849b71f48c785ee74df265115579aa4204aef2b489af3c772a36b"
-    sha256 cellar: :any, big_sur:        "b8c7efa950d02d216b9c734c50b884b79e14812e738bd8a48d2113416677bd7a"
-    sha256 cellar: :any, catalina:       "4f7c022c4b4beeffd13e02b31b3feeea3cdef0e4a66a81c7630024d055b398ef"
-    sha256 cellar: :any, mojave:         "9ca41ce96eff95ff1cf5bfceebde99b720f8b8a2aaef098eb99a099393991915"
+    sha256 cellar: :any,                 arm64_monterey: "a8c50f1c117a704b3630dbf4047980de47bf2de5a5a865ff9d2b011028703b67"
+    sha256 cellar: :any,                 arm64_big_sur:  "d2021fddf559448c443461ccfab70aaeb69c6067599404bb181dc1b979c0c0ea"
+    sha256 cellar: :any,                 monterey:       "d172f3a39ed849b71f48c785ee74df265115579aa4204aef2b489af3c772a36b"
+    sha256 cellar: :any,                 big_sur:        "b8c7efa950d02d216b9c734c50b884b79e14812e738bd8a48d2113416677bd7a"
+    sha256 cellar: :any,                 catalina:       "4f7c022c4b4beeffd13e02b31b3feeea3cdef0e4a66a81c7630024d055b398ef"
+    sha256 cellar: :any,                 mojave:         "9ca41ce96eff95ff1cf5bfceebde99b720f8b8a2aaef098eb99a099393991915"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6bd7bfc04dc77180177d453bf0fc06ade3dd61b5554a77f17c5920618c54784e"
   end
 
   depends_on "cmake" => :build
@@ -35,6 +36,7 @@ class Csfml < Formula
       }
     EOS
     system ENV.cc, "test.c", "-L#{lib}", "-lcsfml-window", "-o", "test"
-    system "./test"
+    # Disable this part of the test on Linux because display is not available.
+    system "./test" if OS.mac?
   end
 end
