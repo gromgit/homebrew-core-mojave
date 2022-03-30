e056f75d3de7dca58be8b291bbaf42543cc19da1
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:33:19 +0800

    sord: no rebuilding required

--- Formula/sord.rb
+++ Formula/sord.rb
@@ -11,22 +11,24 @@ class Sord < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "65060c80a5f82092e950d54d4e0c161c276ab4011511c67b8045675c4a92e872"
-    sha256 cellar: :any, arm64_big_sur:  "ec9d443ac5fc598d0718dd33a9c19fc8f2d2f38975512ce79acabd2d5e509fd7"
-    sha256 cellar: :any, monterey:       "eb739359d67f246df683e696269444efda918395d3755f5b078b3a4727c5865b"
-    sha256 cellar: :any, big_sur:        "30fed8a7b4747de9ed632640e1b2aa326e0c5bb71030d0924b934d820fe87ef5"
-    sha256 cellar: :any, catalina:       "e0ca8e8fd6e2ba8ccec9cb75c8bb2f1d1fe09ba8ebce886f78eed4c87343ab93"
-    sha256 cellar: :any, mojave:         "2d69dca2635bf0808ce19d65e6a795d1591b5f197b2cc703fa9fa084d81d6c2f"
+    sha256 cellar: :any,                 arm64_monterey: "65060c80a5f82092e950d54d4e0c161c276ab4011511c67b8045675c4a92e872"
+    sha256 cellar: :any,                 arm64_big_sur:  "ec9d443ac5fc598d0718dd33a9c19fc8f2d2f38975512ce79acabd2d5e509fd7"
+    sha256 cellar: :any,                 monterey:       "eb739359d67f246df683e696269444efda918395d3755f5b078b3a4727c5865b"
+    sha256 cellar: :any,                 big_sur:        "30fed8a7b4747de9ed632640e1b2aa326e0c5bb71030d0924b934d820fe87ef5"
+    sha256 cellar: :any,                 catalina:       "e0ca8e8fd6e2ba8ccec9cb75c8bb2f1d1fe09ba8ebce886f78eed4c87343ab93"
+    sha256 cellar: :any,                 mojave:         "2d69dca2635bf0808ce19d65e6a795d1591b5f197b2cc703fa9fa084d81d6c2f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "16c2362dcac47cf4f10c118678422f327c9d09d90574718c93000bd30b526ef1"
   end
 
   depends_on "pkg-config" => :build
+  depends_on "python@3.10" => :build
   depends_on "pcre"
   depends_on "serd"
 
   def install
-    system "./waf", "configure", "--prefix=#{prefix}"
-    system "./waf"
-    system "./waf", "install"
+    system "python3", "./waf", "configure", "--prefix=#{prefix}"
+    system "python3", "./waf"
+    system "python3", "./waf", "install"
   end
 
   test do
