b572fafd2ea9140c60eb70916f1b0ae1316eb06a
Adrian Ho <the.gromgit@gmail.com>
2022-03-30 18:18:24 +0800

    mage: update 1.13.0 bottle.

--- Formula/mage.rb
+++ Formula/mage.rb
@@ -2,15 +2,14 @@ class Mage < Formula
   desc "Make/rake-like build tool using Go"
   homepage "https://magefile.org"
   url "https://github.com/magefile/mage.git",
-      tag:      "v1.12.1",
-      revision: "2f1ec406dfa856a4b8378ef837061abc2a0ce01b"
+      tag:      "v1.13.0",
+      revision: "3504e09d7fcfdeab6e70281edce5d5dfb205f31a"
   license "Apache-2.0"
   head "https://github.com/magefile/mage.git", branch: "master"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mage"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "ec46a0ad1ad334a2065e51e09ea0970e5681fe4fc25e4f25c75bc37b86a41597"
+    sha256 cellar: :any_skip_relocation, mojave: "04cd338741105ccaf56f66e04cdb5b8a7d42296df8100a66ce29778c242d6ed0"
   end
 
   depends_on "go"
