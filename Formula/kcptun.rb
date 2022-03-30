d7ba804d8421e5005163b63b5036d7852652d8ee
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:19:37 +0800

    kcptun: no rebuilding required

--- Formula/kcptun.rb
+++ Formula/kcptun.rb
@@ -4,7 +4,7 @@ class Kcptun < Formula
   url "https://github.com/xtaci/kcptun/archive/v20210922.tar.gz"
   sha256 "f6a08f0fe75fa85d15f9c0c28182c69a5ad909229b4c230a8cbe38f91ba2d038"
   license "MIT"
-  head "https://github.com/xtaci/kcptun.git"
+  head "https://github.com/xtaci/kcptun.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "cd7100a62dee2ca46b25ebd4fccf13d4db334720447d4c9258b8ba5df97ae8c0"
@@ -16,7 +16,8 @@ class Kcptun < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "d5d892ce0ccf516f9252e9f09eaa56896021a12ea4d14003d69dabc7559a4467"
   end
 
-  depends_on "go" => :build
+  # Bump to 1.18 on the next release, if possible.
+  depends_on "go@1.17" => :build
 
   def install
     system "go", "build", "-ldflags", "-X main.VERSION=#{version} -s -w",
