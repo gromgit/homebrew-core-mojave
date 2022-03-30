fb2e35a4394175ea0288433b0ef755d6645c63f5
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:08:15 +0800

    ceylon: no rebuilding required

--- Formula/ceylon.rb
+++ Formula/ceylon.rb
@@ -3,7 +3,7 @@ class Ceylon < Formula
   homepage "https://ceylon-lang.org/"
   url "https://ceylon-lang.org/download/dist/1_3_3"
   sha256 "4ec1f1781043ee369c3e225576787ce5518685f2206eafa7d2fd5cfe6ac9923d"
-  revision 2
+  revision 3
 
   livecheck do
     url "https://ceylon-lang.org/download/"
@@ -14,11 +14,8 @@ class Ceylon < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ceylon"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "9eea80806fd36c9eda75a1c1604fd10ea53cf7495b4c8dad1b884520c40eb8fb"
+    sha256 cellar: :any_skip_relocation, all: "c9e8be2d72811dcc4310d1633801fa34e38a7e2bbc779e945ce732ff03172dc2"
   end
-
 
   depends_on "openjdk@8"
 
@@ -28,7 +25,9 @@ class Ceylon < Formula
     bin.install "bin/ceylon"
     bin.install "bin/ceylon-sh-setup"
     libexec.install Dir["*"]
-    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
+    env = Language::Java.java_home_env("1.8")
+    env["PATH"] = "$JAVA_HOME/bin:$PATH"
+    bin.env_script_all_files libexec/"bin", env
   end
 
   test do
