8b66c1d83cc4b28e9af5a2fa23db0655a3b36373
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:10:55 +0800

    docker-machine-parallels: no rebuilding required

--- Formula/docker-machine-parallels.rb
+++ Formula/docker-machine-parallels.rb
@@ -7,13 +7,18 @@ class DockerMachineParallels < Formula
   license "MIT"
   head "https://github.com/Parallels/docker-machine-parallels.git", branch: "master"
 
-bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/docker-machine-parallels"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "19cb3d0e6dae0faa0a4c2628f64e70e6bd4a0d3f8a120b8d59585bf1466423cb"
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bf73fbfd0498623e7bae13c5edc3b7dd83faa8b62cc5fe81fcb23accd70fab76"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1bb048b170f5ca273b7e2e3be17459a9049e29982e2a3c6c866d088772b6744f"
+    sha256 cellar: :any_skip_relocation, monterey:       "dec5f7c88747688ab7395211692ced01dc6157284aed8251214ffc5147efe703"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4613d3ea83c5afdcd717d5027d254b5d01d4b715ed859f98b183e070bd51c9f6"
+    sha256 cellar: :any_skip_relocation, catalina:       "b419812f98208b1fccbbc24f198af6a1235110203b0377d642f80886c5c5fd36"
+    sha256 cellar: :any_skip_relocation, mojave:         "cce66a6fcdea79b33095c2ae7c49c93a9f730353d92738534fdbe03b3488ee43"
   end
 
-  depends_on "go" => :build
+  # Bump to 1.18 on the next release, if possible.
+  depends_on "go@1.17" => :build
   depends_on "docker-machine"
   depends_on :macos
 
