156c8b92ced2e02f65efb206aea6999cec9fca50
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:25:45 +0800

    mozilla-addon-sdk:

--- Formula/mozilla-addon-sdk.rb
+++ Formula/mozilla-addon-sdk.rb
@@ -5,6 +5,10 @@ class MozillaAddonSdk < Formula
   sha256 "16e29d92214a556c8422db156b541fb8c47addfcb3cd879e0a4ca879d6a31f65"
   license "MPL-2.0"
 
+  bottle do
+    sha256 mojave: "f27baf8ae2f171b8f7236ee399bb9df7da423c4ef81b68d7e0ece78df850d204" # fake mojave
+  end
+
   disable! date: "2021-03-24", because: :unsupported
 
   def install
