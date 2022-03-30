c1c7a284cf211810d2a99e860e5f215eb4823e2e
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:30:08 +0800

    publish:

--- Formula/publish.rb
+++ Formula/publish.rb
@@ -6,8 +6,18 @@ class Publish < Formula
   license "MIT"
   head "https://github.com/JohnSundell/Publish.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b46b98682bee859ef400576381066ac4572a256463aaa00b44bcef9141b3a502"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "43e882a02f169464d3b72b91c4221e605658f7f6a8989e069caecfd1d2c4caf6"
+    sha256 cellar: :any_skip_relocation, monterey:       "91a7e223302a9aa8617b3ec0f1d5b25470fae3b307c789b0c8d13f0e2a6370cf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "76ec25c3a77331097114f184bd69185e0b000d1b84f9f7be932021bcc62894cc"
+    sha256                               x86_64_linux:   "c1e393b661b98242b749ae27c2059e840a08926588debad995cbd45a81f872db"
+  end
+
   # https://github.com/JohnSundell/Publish#system-requirements
   depends_on xcode: ["12.5", :build]
+
+  uses_from_macos "swift"
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
