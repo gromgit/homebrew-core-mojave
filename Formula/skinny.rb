cdb7e6a5d2dd4b6a48b2041d315c2e9d9ee73725
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:32:57 +0800

    skinny: no rebuilding required

--- Formula/skinny.rb
+++ Formula/skinny.rb
@@ -1,12 +1,12 @@
 class Skinny < Formula
   desc "Full-stack web app framework in Scala"
-  homepage "http://skinny-framework.org/"
-  url "https://github.com/skinny-framework/skinny-framework/releases/download/3.1.0/skinny-3.1.0.tar.gz"
-  sha256 "4c5661f73bda7d5ccb5a8966efe801951e2a343cf152ac6e9a06d287c5c8712d"
+  homepage "https://skinny-framework.github.io"
+  url "https://github.com/skinny-framework/skinny-framework/releases/download/4.0.0/skinny-4.0.0.tar.gz"
+  sha256 "7d1370856927e2768c30be15c38dfbd5e322bc6eaf9b5ef14e69ddf2ddc91520"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "3599c215671ecb68ec4f067b69d130635d3b10fb3e28c562641683886ba02d13"
+    sha256 cellar: :any_skip_relocation, all: "136e223ff9658c5d185c911eb0e8a130bb5931814d7a788dd5bfa4f9513d37a7"
   end
 
   depends_on "openjdk"
@@ -14,12 +14,11 @@ class Skinny < Formula
   def install
     inreplace %w[skinny skinny-blank-app/skinny], "/usr/local", HOMEBREW_PREFIX
     libexec.install Dir["*"]
-    (bin/"skinny").write <<~EOS
-      #!/bin/bash
-      export PATH=#{bin}:$PATH
-      export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
-      PREFIX="#{libexec}" exec "#{libexec}/skinny" "$@"
-    EOS
+
+    skinny_env = Language::Java.overridable_java_home_env
+    skinny_env[:PATH] = "#{bin}:${PATH}"
+    skinny_env[:PREFIX] = libexec
+    (bin/"skinny").write_env_script libexec/"skinny", skinny_env
   end
 
   test do
