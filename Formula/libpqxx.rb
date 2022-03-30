41e768c4ab306f61904c58ab818ce1828cec18ca
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:22:13 +0800

    libpqxx:

--- Formula/libpqxx.rb
+++ Formula/libpqxx.rb
@@ -1,9 +1,18 @@
 class Libpqxx < Formula
   desc "C++ connector for PostgreSQL"
   homepage "http://pqxx.org/development/libpqxx/"
-  url "https://github.com/jtv/libpqxx/archive/7.6.0.tar.gz"
-  sha256 "8194ce4eff3fee5325963ccc28d3542cfaa54ba1400833d0df6948de3573c118"
+  url "https://github.com/jtv/libpqxx/archive/7.7.2.tar.gz"
+  sha256 "4b7a0b67cbd75d1c31e1e8a07c942ffbe9eec4e32c29b15d71cc225dc737e243"
   license "BSD-3-Clause"
+
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "fe1aa09e787c6a6ef9f9d18cba4a941defd84abc0d7587383bf82300c9101a18"
+    sha256 cellar: :any,                 arm64_big_sur:  "88de1e1f59c011cccf76a6a3fcd4352b20803b6b191eee9658d5ef1ce8b2bbe7"
+    sha256 cellar: :any,                 monterey:       "c143db5c27b7e7dc30bcba40f1465b58e68fcf8b2fd1de9d651a9598355ab033"
+    sha256 cellar: :any,                 big_sur:        "5f4d16846e7590e7697e0aa5ad620cf559867c14bd99f4f9371f5369ab933312"
+    sha256 cellar: :any,                 catalina:       "567272fec74fd895f9d2c37686af837ee1dcb0b610a766b19e84889d83147421"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e427ded0fcd5f22b8069b17cb1a264cdfbe88fe8de92b8f97807b305f782e98b"
+  end
 
   depends_on "pkg-config" => :build
   depends_on "python@3.9" => :build
@@ -18,6 +27,7 @@ class Libpqxx < Formula
   fails_with gcc: "5"
 
   def install
+    ENV.append "CXXFLAGS", "-std=c++17"
     ENV.prepend_path "PATH", Formula["python@3.9"].opt_libexec/"bin"
     ENV["PG_CONFIG"] = Formula["libpq"].opt_bin/"pg_config"
 
