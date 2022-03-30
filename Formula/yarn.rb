feae6e3d58da434930820affbc89f9e2696a84bc
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:38:40 +0800

    yarn: no rebuilding required

--- Formula/yarn.rb
+++ Formula/yarn.rb
@@ -1,17 +1,16 @@
 class Yarn < Formula
   desc "JavaScript package manager"
   homepage "https://yarnpkg.com/"
-  url "https://yarnpkg.com/downloads/1.22.17/yarn-v1.22.17.tar.gz"
-  sha256 "267982c61119a055ba2b23d9cf90b02d3d16c202c03cb0c3a53b9633eae37249"
+  url "https://yarnpkg.com/downloads/1.22.18/yarn-v1.22.18.tar.gz"
+  sha256 "816e5c073b3d35936a398d1fe769ebbcd517298e3510b649e8fc67cd3a62e113"
   license "BSD-2-Clause"
-  revision 2
 
   livecheck do
     skip("1.x line is frozen and features/bugfixes only happen on 2.x")
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "28016c8b31dfd51b542168bf0c0165cc7a5e50f0b4034a6cc6628e03f2c01121"
+    sha256 cellar: :any_skip_relocation, all: "e4032bfe83faec0cd66a9c52b1bc2a12ff8ecdeb1bd497a08cc077da065d5fac"
   end
 
   depends_on "node" => :test
@@ -38,9 +37,7 @@ class Yarn < Formula
   test do
     (testpath/"package.json").write('{"name": "test"}')
     system bin/"yarn", "add", "jquery"
-    on_macos do
-      # macOS specific package
-      system bin/"yarn", "add", "fsevents", "--build-from-source=true"
-    end
+    # macOS specific package
+    system bin/"yarn", "add", "fsevents", "--build-from-source=true"
   end
 end
