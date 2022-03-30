1a25e67e4ac36763ee6cd5b92c3cb8447e078e9c
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:06:45 +0800

    bas55: no rebuilding required

--- Formula/bas55.rb
+++ Formula/bas55.rb
@@ -11,10 +11,13 @@ class Bas55 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "ab48670d6620654c1e88b8076d9c9805e6f6b2fde8f1d8773ac96fd5bd234789"
-    sha256 cellar: :any_skip_relocation, big_sur:       "c9a50cf6904cafd3a75cbb762ea68d3545eadc832be0b2c0313aaadf03dfe453"
-    sha256 cellar: :any_skip_relocation, catalina:      "7eaaf506f35bbc8f2149028c7b379f3b5fc7d5bd7899d021b016f94272e9441e"
-    sha256 cellar: :any_skip_relocation, mojave:        "1c7f697e391226ba6ba292cdb80801b2b221ed355918fa6ef14deb61abbb73ac"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7789ffd65c9afc8d415385c439205a62e369c598bd6cc7b9133e306cff8a5be2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ab48670d6620654c1e88b8076d9c9805e6f6b2fde8f1d8773ac96fd5bd234789"
+    sha256 cellar: :any_skip_relocation, monterey:       "2696588487bbff56058e0370ec09b63143f5e12c1206490066d7b213d57260db"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c9a50cf6904cafd3a75cbb762ea68d3545eadc832be0b2c0313aaadf03dfe453"
+    sha256 cellar: :any_skip_relocation, catalina:       "7eaaf506f35bbc8f2149028c7b379f3b5fc7d5bd7899d021b016f94272e9441e"
+    sha256 cellar: :any_skip_relocation, mojave:         "1c7f697e391226ba6ba292cdb80801b2b221ed355918fa6ef14deb61abbb73ac"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e8fc36f305cced35e0845d73296dc55d8a6dd8ef7a645c0e9fe74f4a926564af"
   end
 
   def install
