d88d349ba3c214042e43b04246ba6d08311ab9b2
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:15:48 +0800

    gomplate: no rebuilding required

--- Formula/gomplate.rb
+++ Formula/gomplate.rb
@@ -6,13 +6,18 @@ class Gomplate < Formula
   license "MIT"
   head "https://github.com/hairyhenderson/gomplate.git", branch: "master"
 
-bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gomplate"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "3b6b73e45d78adddfc0381cd8c02beaf0a1ff4f26b7645768db4dee5e951f07c"
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "dd9583c59cdd27102266f6c324d4e7613470e48cdb5a30d1ffdcf677e497f54e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "05845f4a31da3d5b78b61e119a62d7f9c869f471194de46d0b06b3b76b3cfc14"
+    sha256 cellar: :any_skip_relocation, monterey:       "9b41465cdf57336347772059715707557335b9c1b7c026d96ad163e05e7c025b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "92ce5929e369eb6a49e478f71bf2a6fba500094b600c578f4ba3ae0c0546dcc5"
+    sha256 cellar: :any_skip_relocation, catalina:       "85ba008a9f271385b4599fe1308fa0846bde36307d96a39424c2aa53396ba12b"
+    sha256 cellar: :any_skip_relocation, mojave:         "d08b4521c7140d8391be5aef5ab3cfa9184e080e5abf46dbea079025b49c4a0f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f026fafcaeba0eb20e509c298edcd80c0158f0ab0a45a68184f754f8aa2e2754"
   end
 
-  depends_on "go" => :build
+  # Bump to 1.18 on the next release, if possible.
+  depends_on "go@1.17" => :build
 
   def install
     system "make", "build", "VERSION=#{version}"
