4d9000404b21ec658b875e13a9bbc9a673cc9178
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:19:00 +0800

    jing-trang: no rebuilding required

--- Formula/jing-trang.rb
+++ Formula/jing-trang.rb
@@ -8,12 +8,14 @@ class JingTrang < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "0a3f51890fe283b8408e95ecad3b5a6b0d9708fc73b4b6427cb333d08d8b4464"
-    sha256 cellar: :any_skip_relocation, big_sur:       "fbfaf15a1309a394ba34f78a6fd28062e56c05d0f118061031cb84a1f0cd2695"
-    sha256 cellar: :any_skip_relocation, catalina:      "1448a797ce37ead9d47b398a2c96af6da95acecf60532fd9edc302a1468308a3"
-    sha256 cellar: :any_skip_relocation, mojave:        "10424ca3b36b8219a58894bb23a29ce3abf67feb4a18e7f03a3daf2c328d459b"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "7d7c7ba57430d123a9b53e8f4f9e7427d1ab2c1fb0b37149572cd4dc5eb1ddeb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "816cb02200808cafe1cf7665d701311d681b2eefe1ee0e554972e4844f77b0b1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f0bfd7ca4e0fb2e0a60df34fdb19d0ede3b2338299f369a4c577357e3c5334df"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0a3f51890fe283b8408e95ecad3b5a6b0d9708fc73b4b6427cb333d08d8b4464"
+    sha256 cellar: :any_skip_relocation, monterey:       "2b41d463c8dc2c3e870b0818ed31b0d6bc93122cf1039875cbcfe41ca578f74a"
+    sha256 cellar: :any_skip_relocation, big_sur:        "fbfaf15a1309a394ba34f78a6fd28062e56c05d0f118061031cb84a1f0cd2695"
+    sha256 cellar: :any_skip_relocation, catalina:       "1448a797ce37ead9d47b398a2c96af6da95acecf60532fd9edc302a1468308a3"
+    sha256 cellar: :any_skip_relocation, mojave:         "10424ca3b36b8219a58894bb23a29ce3abf67feb4a18e7f03a3daf2c328d459b"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "7d7c7ba57430d123a9b53e8f4f9e7427d1ab2c1fb0b37149572cd4dc5eb1ddeb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "816cb02200808cafe1cf7665d701311d681b2eefe1ee0e554972e4844f77b0b1"
   end
 
   depends_on "ant" => :build
