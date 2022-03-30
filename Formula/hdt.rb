c9e37459d7b2243fb9d5677811a15786978d62fa
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:17:33 +0800

    hdt: no rebuilding required

--- Formula/hdt.rb
+++ Formula/hdt.rb
@@ -6,11 +6,14 @@ class Hdt < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "934a8c000b23ee6a63cda409118c47737d4549a5f0fd260a1652ecfc6b49f1d2"
-    sha256 cellar: :any, big_sur:       "614cded2abf67c909f7fd1a980b3093e8368bf0fc802adcd774716e9e301f4f9"
-    sha256 cellar: :any, catalina:      "66978658e51117e228dea28a0d4264cfe3ce9ed7e4536eb0726d8c1438d4fb59"
-    sha256 cellar: :any, mojave:        "333a1baf863f372e94a40474a799fdd7e043bd691817ab5f7467983ce31a21cb"
-    sha256 cellar: :any, high_sierra:   "709ea815a3a24e104b0bd873948d8cbaca317ed235098f1c042ab308f7c3cb6f"
+    sha256 cellar: :any,                 arm64_monterey: "13a72094b82ac91fe1bbaed2cfb12ffda92903715e105c1136e42c7a1a3d48d0"
+    sha256 cellar: :any,                 arm64_big_sur:  "934a8c000b23ee6a63cda409118c47737d4549a5f0fd260a1652ecfc6b49f1d2"
+    sha256 cellar: :any,                 monterey:       "693a2273358dcdc130f4bdc102d23e0c7d33d709a417811e737320faf96caaa4"
+    sha256 cellar: :any,                 big_sur:        "614cded2abf67c909f7fd1a980b3093e8368bf0fc802adcd774716e9e301f4f9"
+    sha256 cellar: :any,                 catalina:       "66978658e51117e228dea28a0d4264cfe3ce9ed7e4536eb0726d8c1438d4fb59"
+    sha256 cellar: :any,                 mojave:         "333a1baf863f372e94a40474a799fdd7e043bd691817ab5f7467983ce31a21cb"
+    sha256 cellar: :any,                 high_sierra:    "709ea815a3a24e104b0bd873948d8cbaca317ed235098f1c042ab308f7c3cb6f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7e252bf5067fa866d9fb2d81f5af2fc302347982ed6b8c5c7f1474da27eafff8"
   end
 
   depends_on "autoconf" => :build
