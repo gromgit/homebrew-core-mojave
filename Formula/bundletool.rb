32fd6924f482e9f618a3b4bf775eb17960a93489
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:07:36 +0800

    bundletool: no rebuilding required

--- Formula/bundletool.rb
+++ Formula/bundletool.rb
@@ -1,31 +1,28 @@
 class Bundletool < Formula
   desc "Command-line tool to manipulate Android App Bundles"
   homepage "https://github.com/google/bundletool"
-  url "https://github.com/google/bundletool/releases/download/1.8.2/bundletool-all-1.8.2.jar"
-  sha256 "682652793deccffdd3fcd05708c0fb211be817d60f8d260e2c0a6a1457778b12"
+  url "https://github.com/google/bundletool/releases/download/1.9.0/bundletool-all-1.9.0.jar"
+  sha256 "e18607df846ab9e473c84864cb7e6756f61b480b61fbc8443a3722be30a7c353"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "75c946b945ceef3db9ee8024009c9a1e81e02dab3344251baba2992728db3240"
+    sha256 cellar: :any_skip_relocation, all: "6af4f70c093c99ac9d35dc702768f7bacce0a56ee070d8db48623e4511e3b484"
   end
 
   depends_on "openjdk"
 
-  resource "bundle" do
+  resource "homebrew-test-bundle" do
     url "https://gist.githubusercontent.com/raw/ca85ede7ac072a44f48c658be55ff0d3/sample.aab"
     sha256 "aac71ad62e1f20dd19b80eba5da5cb5e589df40922f288fb6a4b37a62eba27ef"
   end
 
   def install
     libexec.install "bundletool-all-#{version}.jar" => "bundletool-all.jar"
-    (bin/"bundletool").write <<~EOS
-      #!/bin/bash
-      exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec}/bundletool-all.jar" "$@"
-    EOS
+    bin.write_jar_script libexec/"bundletool-all.jar", "bundletool"
   end
 
   test do
-    resource("bundle").stage do
+    resource("homebrew-test-bundle").stage do
       expected = <<~EOS
         App Bundle information
         ------------
