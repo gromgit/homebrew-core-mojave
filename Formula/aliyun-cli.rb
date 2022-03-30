6bf27576ca683e41703b860d39f9ff6695ae515a
Adrian Ho <the.gromgit@gmail.com>
2022-03-30 17:32:07 +0800

    aliyun-cli: update 3.0.113 bottle.

--- Formula/aliyun-cli.rb
+++ Formula/aliyun-cli.rb
@@ -2,21 +2,20 @@ class AliyunCli < Formula
   desc "Universal Command-Line Interface for Alibaba Cloud"
   homepage "https://github.com/aliyun/aliyun-cli"
   url "https://github.com/aliyun/aliyun-cli.git",
-      tag:      "v3.0.109",
-      revision: "d1e245b1e8a6f8d563cd3f34ab276f98680fa991"
+      tag:      "v3.0.113",
+      revision: "d6b8dfc440dfbec70d5d69402452a95d88e60789"
   license "Apache-2.0"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/aliyun-cli"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "ff5a03b840a9b13c3b31ab5c063bceb0e29c123754401e70310ef2338851664b"
+    sha256 cellar: :any_skip_relocation, mojave: "0ab55d27e41ae60ccd07697167fc399a96f3e5331656dcdbb9de53fada21460e"
   end
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/aliyun/aliyun-cli/cli.Version=#{version}"),
-                          "-o", bin/"aliyun", "main/main.go"
+    ldflags = "-s -w -X github.com/aliyun/aliyun-cli/cli.Version=#{version}"
+    system "go", "build", *std_go_args(output: bin/"aliyun", ldflags: ldflags), "main/main.go"
   end
 
   test do
