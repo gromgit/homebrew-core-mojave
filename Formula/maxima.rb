6b394e3cba99d5525345db15cc3668551e36b092
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:24:33 +0800

    maxima: no rebuilding required

--- Formula/maxima.rb
+++ Formula/maxima.rb
@@ -12,10 +12,13 @@ class Maxima < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f2b30807d9dc4138ad48a027c721d0f2efac636ba244abf81b76e3697e3c0482"
-    sha256 cellar: :any_skip_relocation, big_sur:       "7eb3ed75bfe3a2786dc959724c99769c123e9fcc9ac2293ad887c2c280d7e974"
-    sha256 cellar: :any_skip_relocation, catalina:      "ffb175c92077e3d87cc659bc888113d656c37a0c3cb75d479a385627a9cd2ce0"
-    sha256 cellar: :any_skip_relocation, mojave:        "0d18434459d1c811395e78b2c08ad797332fd34b0d35fe22aa4f409ae6e3500b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5c06a235975a72dd1f3441420f3f962aa5d2ff388634bf69f9898d8d30e5d035"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f2b30807d9dc4138ad48a027c721d0f2efac636ba244abf81b76e3697e3c0482"
+    sha256 cellar: :any_skip_relocation, monterey:       "431234378019407a587e8b794f40cc58e6eea474a27d2cf1d7067cfc2a7ea116"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7eb3ed75bfe3a2786dc959724c99769c123e9fcc9ac2293ad887c2c280d7e974"
+    sha256 cellar: :any_skip_relocation, catalina:       "ffb175c92077e3d87cc659bc888113d656c37a0c3cb75d479a385627a9cd2ce0"
+    sha256 cellar: :any_skip_relocation, mojave:         "0d18434459d1c811395e78b2c08ad797332fd34b0d35fe22aa4f409ae6e3500b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "618501ffc053e46a7501dab5aa12e7ab8bc3e0bd27110b98b04236e8fcc418ed"
   end
 
   depends_on "gawk" => :build
