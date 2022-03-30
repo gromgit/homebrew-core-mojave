8c91bbc5b6206d55b0c6a2373dcbfa97b4b46fd5
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:09:55 +0800

    cromwell: no rebuilding required

--- Formula/cromwell.rb
+++ Formula/cromwell.rb
@@ -1,24 +1,24 @@
 class Cromwell < Formula
   desc "Workflow Execution Engine using Workflow Description Language"
   homepage "https://github.com/broadinstitute/cromwell"
-  url "https://github.com/broadinstitute/cromwell/releases/download/77/cromwell-77.jar"
-  sha256 "0823aee9257c6a1d06eabd53ef1bfae6cdf398a27fd70fe07d7630b32b74666a"
+  url "https://github.com/broadinstitute/cromwell/releases/download/78/cromwell-78.jar"
+  sha256 "bbf8ac0941402b1e827712347bfb8b357eb54a3b2d8207055997e6fef1af7237"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "ca2a0778eff3d0ce1eea292e8fab931a54cc5af3ccaf8786f073ecf3942d3379"
+    sha256 cellar: :any_skip_relocation, all: "87ac057a5b8228163dea693d4f261978414e342dd0885556994a788c7bf7c4fb"
   end
 
   head do
-    url "https://github.com/broadinstitute/cromwell.git"
+    url "https://github.com/broadinstitute/cromwell.git", branch: "develop"
     depends_on "sbt" => :build
   end
 
   depends_on "openjdk"
 
   resource "womtool" do
-    url "https://github.com/broadinstitute/cromwell/releases/download/77/womtool-77.jar"
-    sha256 "2ce54ef6e678c73308c114e07119e8d27d39743577eb73c6c3c23fc3f43dbf46"
+    url "https://github.com/broadinstitute/cromwell/releases/download/78/womtool-78.jar"
+    sha256 "b2e446e8cbd6639e274b28e6b0603156b775d930b08921d975e3516a3620c0b0"
   end
 
   def install
@@ -33,14 +33,8 @@ class Cromwell < Formula
       end
     end
 
-    (bin/"cromwell").write <<~EOS
-      #!/bin/bash
-      exec "#{Formula["openjdk"].opt_bin}/java" $JAVA_OPTS -jar "#{libexec}/cromwell.jar" "$@"
-    EOS
-    (bin/"womtool").write <<~EOS
-      #!/bin/bash
-      exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec}/womtool.jar" "$@"
-    EOS
+    bin.write_jar_script libexec/"cromwell.jar", "cromwell", "$JAVA_OPTS"
+    bin.write_jar_script libexec/"womtool.jar", "womtool"
   end
 
   test do
