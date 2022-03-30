d66f67e3a5726cacab53d27b63b90858e694c28f
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:19:02 +0800

    jinx: no rebuilding required

--- Formula/jinx.rb
+++ Formula/jinx.rb
@@ -4,7 +4,7 @@ class Jinx < Formula
   url "https://github.com/JamesBoer/Jinx/archive/v1.2.0.tar.gz"
   sha256 "18a88ef34b06c63210ac966429785c5e6f7ec4369719260f2af4a3dee1544e1b"
   license "MIT"
-  head "https://github.com/JamesBoer/Jinx.git"
+  head "https://github.com/JamesBoer/Jinx.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "b91d30facce35e18f79655ab8ce200e413d63175e520d45e5c7276383dfcaea9"
@@ -12,9 +12,16 @@ class Jinx < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "0500e01d366b1f61c140570eeaae54003e30ad139b3f169d7dafdb08b2fd7b59"
     sha256 cellar: :any_skip_relocation, mojave:        "f12580f7319652bf5309ef767509d2f2c823bbedadeb8a85b2b6f35d0a65a747"
     sha256 cellar: :any_skip_relocation, high_sierra:   "22146d4fcd750935d4f125820ee3cb33088a977106fa630e22c7487e3a3a8132"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0f8396e3e4b0e75717697ac35f01a0eab8a05b211e3238a415fa5420ae504cef"
   end
 
   depends_on "cmake" => :build
+
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
 
   def install
     # disable building tests
