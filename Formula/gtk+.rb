7f4b95d1001b8a63e35ed8cb9e53f91460cb770f
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:16:58 +0800

    gtk+: no rebuilding required

--- Formula/gtk+.rb
+++ Formula/gtk+.rb
@@ -151,9 +151,7 @@ class Gtkx < Formula
       -lpango-1.0
       -lpangocairo-1.0
     ]
-    on_macos do
-      flags << "-lintl"
-    end
+    flags << "-lintl" if OS.mac?
     system ENV.cc, "test.c", "-o", "test", *flags
     system "./test"
   end
