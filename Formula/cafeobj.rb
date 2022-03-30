d1ae79538249071f4ffce6c47c96ea2b4328e323
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:07:46 +0800

    cafeobj: no rebuilding required

--- Formula/cafeobj.rb
+++ Formula/cafeobj.rb
@@ -6,10 +6,11 @@ class Cafeobj < Formula
   revision 2
 
   bottle do
-    sha256 arm64_big_sur: "1912508b031a0b0e098c3195a044d0a2f4988d8906bdc2c5cfae1c98e508f59a"
-    sha256 big_sur:       "724109123713a037126847a07fe06e4fa134d3e28aff72ae72de7f8f4fa77576"
-    sha256 catalina:      "7e5281633b3f18239282905a748c61b702b2d059daf559fd52187aa6d079e79c"
-    sha256 mojave:        "1a875e6c86c2d15862f0b64ee9bb90077bff62748d3c2d91f201527ea78886ac"
+    sha256 arm64_monterey: "d338e4868d48d4f244ff91c745de7d0fa6adf41825eb9bde53e371e01960381c"
+    sha256 arm64_big_sur:  "1912508b031a0b0e098c3195a044d0a2f4988d8906bdc2c5cfae1c98e508f59a"
+    sha256 big_sur:        "724109123713a037126847a07fe06e4fa134d3e28aff72ae72de7f8f4fa77576"
+    sha256 catalina:       "7e5281633b3f18239282905a748c61b702b2d059daf559fd52187aa6d079e79c"
+    sha256 mojave:         "1a875e6c86c2d15862f0b64ee9bb90077bff62748d3c2d91f201527ea78886ac"
   end
 
   depends_on "sbcl"
