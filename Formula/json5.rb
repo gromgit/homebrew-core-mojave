e7daae73abd767f2e9adaa5029a3a4a36b6cd946
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:19:09 +0800

    json5: no rebuilding required

--- Formula/json5.rb
+++ Formula/json5.rb
@@ -3,17 +3,12 @@ require "language/node"
 class Json5 < Formula
   desc "JSON enhanced with usability features"
   homepage "https://json5.org/"
-  url "https://github.com/json5/json5/archive/v2.2.0.tar.gz"
-  sha256 "3f38a1f8d1101816bfb6d2f2a7a3bf31de438491f72de2ab32bb4b4a38c87aec"
+  url "https://github.com/json5/json5/archive/v2.2.1.tar.gz"
+  sha256 "2ad1be22d6138825a712411fac53086a8710b7d987e9aa81c59ee370b5a57030"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "38b3d5fd1167020fbb15e2778ce5614fbcd1417cd8d4fb0131de2fdacdb49582"
-    sha256 cellar: :any_skip_relocation, big_sur:       "fed591c879ba5765efe81a0faf4c4cbd44e9c41b984307d0b72317933c0dadb5"
-    sha256 cellar: :any_skip_relocation, catalina:      "09e87f239bbc187dba2e2fe5f865f078a7210827983835a625524d3d800dcf18"
-    sha256 cellar: :any_skip_relocation, mojave:        "e2dc5ac648d7c114a8f6945ed986db3b88be6383afefe2f54c87a1d8deace6be"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "95bb5b8cf876c6ea17b8f2ad04a1070a8700c8ff5d047e85f106d1938459c313"
-    sha256 cellar: :any_skip_relocation, all:           "95bb5b8cf876c6ea17b8f2ad04a1070a8700c8ff5d047e85f106d1938459c313"
+    sha256 cellar: :any_skip_relocation, all: "b78db2e75223abf02b70379e741be320cc35f26190b7907035d694735b44253c"
   end
 
   depends_on "node"
