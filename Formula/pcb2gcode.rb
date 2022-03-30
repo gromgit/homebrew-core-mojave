f86dde0a2ff3067ee3c5471a2c8f94201579646f
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:28:52 +0800

    pcb2gcode: no rebuilding required

--- Formula/pcb2gcode.rb
+++ Formula/pcb2gcode.rb
@@ -7,12 +7,13 @@ class Pcb2gcode < Formula
   head "https://github.com/pcb2gcode/pcb2gcode.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "4facc148f0dd1ed67cb098258f40210f13431239f36dd859a3605d8aba62a663"
-    sha256 cellar: :any, arm64_big_sur:  "596ef4d44d9da58ce8ce77fc51605c9a1229a8b763e47f785baf202b0ba2a208"
-    sha256 cellar: :any, monterey:       "20d4b84b6d91188a05455d39e7bf5a75314b641e11f41a860dc65ce318d78381"
-    sha256 cellar: :any, big_sur:        "d08a9c3c499b9b9d510942cb8e7f6bae057eb29cadeab474ce0bbd13ae452d43"
-    sha256 cellar: :any, catalina:       "5872fbb4710e05c4ad1795ea8cbb75fe26bc031cd00041156efc30f9fe632101"
-    sha256 cellar: :any, mojave:         "73c321af8bd386af20ec60cf9e0c98600f12681b5bdd8bac70504457ebcc8bad"
+    sha256 cellar: :any,                 arm64_monterey: "4facc148f0dd1ed67cb098258f40210f13431239f36dd859a3605d8aba62a663"
+    sha256 cellar: :any,                 arm64_big_sur:  "596ef4d44d9da58ce8ce77fc51605c9a1229a8b763e47f785baf202b0ba2a208"
+    sha256 cellar: :any,                 monterey:       "20d4b84b6d91188a05455d39e7bf5a75314b641e11f41a860dc65ce318d78381"
+    sha256 cellar: :any,                 big_sur:        "d08a9c3c499b9b9d510942cb8e7f6bae057eb29cadeab474ce0bbd13ae452d43"
+    sha256 cellar: :any,                 catalina:       "5872fbb4710e05c4ad1795ea8cbb75fe26bc031cd00041156efc30f9fe632101"
+    sha256 cellar: :any,                 mojave:         "73c321af8bd386af20ec60cf9e0c98600f12681b5bdd8bac70504457ebcc8bad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "53b4615757ee9604da2668158f7e420add1e4dfe6bdf8fc7183bb906322ead46"
   end
 
   # Release 2.0.0 doesn't include an autoreconfed tarball
@@ -31,6 +32,18 @@ class Pcb2gcode < Formula
   depends_on "pkg-config" => :build
   depends_on "boost"
   depends_on "gerbv"
+
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
+
+  # Apply upstream commit to fix build with GCC 11.  Remove with next release.
+  patch do
+    url "https://github.com/pcb2gcode/pcb2gcode/commit/01cd18a6d859ab1aac6c532c99be9109f083448d.patch?full_index=1"
+    sha256 "b5b316b14e9b615ee9114261eb5d04a0b234823847a18bb5ab4d8e2af4210750"
+  end
 
   def install
     system "autoreconf", "-fvi"
