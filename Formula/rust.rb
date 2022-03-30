b04585a60be1cba8eb6e892c647689974066b1f5
Adrian Ho <the.gromgit@gmail.com>
2022-03-31 00:30:05 +0800

    rust: update 1.59.0 bottle.

--- Formula/rust.rb
+++ Formula/rust.rb
@@ -4,20 +4,20 @@ class Rust < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   stable do
-    url "https://static.rust-lang.org/dist/rustc-1.58.1-src.tar.gz"
-    sha256 "a839afdd3625d6f3f3c4c10b79813675d1775c460d14be1feaf33a6c829c07c7"
+    url "https://static.rust-lang.org/dist/rustc-1.59.0-src.tar.gz"
+    sha256 "a7c8eeaee85bfcef84c96b02b3171d1e6540d15179ff83dddd9eafba185f85f9"
 
     # From https://github.com/rust-lang/rust/tree/#{version}/src/tools
     resource "cargo" do
       url "https://github.com/rust-lang/cargo.git",
-          tag:      "0.59.0",
-          revision: "7f08ace4f1305de7f3b1b0e2f765911957226bd4"
+          tag:      "0.60.0",
+          revision: "49d8809dc2d3e6e0d5ec634fcf26d8e2aab67130"
     end
   end
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rust"
-    sha256 cellar: :any, mojave: "43a5006fd0b7ed1340f3ddce5e42219838a1d268cea812150891db7dbc9e3522"
+    sha256 cellar: :any, mojave: "59b3544950bfc3197bcc19a9ffb56e512c188621baf174e2732d17221acdeb02"
   end
 
   head do
@@ -42,25 +42,20 @@ class Rust < Formula
     on_macos do
       # From https://github.com/rust-lang/rust/blob/#{version}/src/stage0.json
       if Hardware::CPU.arm?
-        url "https://static.rust-lang.org/dist/2021-12-02/cargo-1.57.0-aarch64-apple-darwin.tar.gz"
-        sha256 "2171ecf2c18fadd90588252aa294272aeb740f065772076ef36d04b1b4bfcc4e"
+        url "https://static.rust-lang.org/dist/2022-01-13/cargo-1.58.0-aarch64-apple-darwin.tar.gz"
+        sha256 "9144ee0f614c8dcb5f34a774e47a24b676860fa442afda2a3c7f45abfe694e6a"
       else
-        url "https://static.rust-lang.org/dist/2021-12-02/cargo-1.57.0-x86_64-apple-darwin.tar.gz"
-        sha256 "26129f0e3ecd42e730f62d9c693e1a89aedfe6b7c8108cf970b2f1ed51900fb8"
+        url "https://static.rust-lang.org/dist/2022-01-13/cargo-1.58.0-x86_64-apple-darwin.tar.gz"
+        sha256 "60203fc7ec453f2a9eb93734c70a72f8ee88e349905edded04155c1646e283a6"
       end
     end
 
     on_linux do
       # From: https://github.com/rust-lang/rust/blob/#{version}/src/stage0.json
-      url "https://static.rust-lang.org/dist/2021-12-02/cargo-1.57.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "2b341034e4c30bede47a432751935549a28e40b566a1fd4b59c824ccd0a26ff6"
+      url "https://static.rust-lang.org/dist/2022-01-13/cargo-1.58.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "940aa91ad2de39c18749e8d789d88846de2debbcf6207247225b42c6c3bf731a"
     end
   end
-
-  # Pass `--enable-vendor` to `configure` when this patch is no longer needed.
-  # Make sure object files in static archives have distinct names.
-  # https://github.com/rust-lang/compiler-builtins/issues/443
-  patch :p0, :DATA
 
   def install
     ENV.prepend_path "PATH", Formula["python@3.9"].opt_libexec/"bin"
@@ -69,7 +64,7 @@ class Rust < Formula
     # https://crates.io/crates/openssl#manual-configuration
     ENV["OPENSSL_DIR"] = Formula["openssl@1.1"].opt_prefix
 
-    args = ["--prefix=#{prefix}"]
+    args = ["--prefix=#{prefix}", "--enable-vendor"]
     if build.head?
       args << "--disable-rpath"
       args << "--release-channel=nightly"
@@ -110,31 +105,15 @@ class Rust < Formula
   end
 
   test do
-    system "#{bin}/rustdoc", "-h"
+    system bin/"rustdoc", "-h"
     (testpath/"hello.rs").write <<~EOS
       fn main() {
         println!("Hello World!");
       }
     EOS
-    system "#{bin}/rustc", "hello.rs"
+    system bin/"rustc", "hello.rs"
     assert_equal "Hello World!\n", `./hello`
-    system "#{bin}/cargo", "new", "hello_world", "--bin"
+    system bin/"cargo", "new", "hello_world", "--bin"
     assert_equal "Hello, world!", cd("hello_world") { shell_output("#{bin}/cargo run").split("\n").last }
   end
 end
-
-__END__
---- Cargo.lock.orig	2022-01-05 23:36:31.734319021 +0800
-+++ Cargo.lock	2022-01-05 23:37:17.531044204 +0800
-@@ -658,9 +658,9 @@
-
- [[package]]
- name = "compiler_builtins"
--version = "0.1.53"
-+version = "0.1.55"
- source = "registry+https://github.com/rust-lang/crates.io-index"
--checksum = "2467ff455350a4df7d02f1ed1449d0279605a763de5d586dcf6aa7d732508bcb"
-+checksum = "c9ac60765140c97aaf531dae151a287646b0805ec725805da9e2a3ee31cd501c"
- dependencies = [
-  "cc",
-  "rustc-std-workspace-core",
