2e27471cc18ccd35f06b1afef29f7db5a324ec44
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:16:09 +0800

    gradle-profiler: no rebuilding required

--- Formula/gradle-profiler.rb
+++ Formula/gradle-profiler.rb
@@ -1,8 +1,8 @@
 class GradleProfiler < Formula
   desc "Profiling and benchmarking tool for Gradle builds"
   homepage "https://github.com/gradle/gradle-profiler/"
-  url "https://search.maven.org/remotecontent?filepath=org/gradle/profiler/gradle-profiler/0.17.0/gradle-profiler-0.17.0.zip"
-  sha256 "c71812c0fc79e23d8c9eafc831ba2f016bb7744935912b0aac1a98972dc30abe"
+  url "https://search.maven.org/remotecontent?filepath=org/gradle/profiler/gradle-profiler/0.18.0/gradle-profiler-0.18.0.zip"
+  sha256 "242f891590353161713c195f01a677d938cc74bb6f56832d2c76a6ab4a5a9d79"
   license "Apache-2.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class GradleProfiler < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "81650ffff52e598133f4fbbd99a954c75a2bf15381df84a5293ba7f5bfe9ffb3"
+    sha256 cellar: :any_skip_relocation, all: "c3cb58c2fb6c470c012c4ea7c89466c5327ba4dbe2660be27aaf1f0f4c6ba133"
   end
 
   # gradle currently does not support Java 17 (ARM)
