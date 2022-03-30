9e15b092e244435b2f7b109d57cf2b30c9490115
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:24:54 +0800

    mfterm: no rebuilding required

--- Formula/mfterm.rb
+++ Formula/mfterm.rb
@@ -7,13 +7,14 @@ class Mfterm < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "7c66e15be8f86726fe114bb9c0be3684386f62819af22e7892876634b22b4d8d"
-    sha256 cellar: :any, arm64_big_sur:  "e7dffabd037eaff9a07ef2adb7c5731bd6270f04e5f3590280293fc25d478ddb"
-    sha256 cellar: :any, monterey:       "375cd87233c4faea0360d7b840ff7ebd7769a455429f70c8b60305bef1b32299"
-    sha256 cellar: :any, big_sur:        "18cc9c42960d0accd760293232f236ad1d35e0fc1e7e8f44061b72db2c2acf64"
-    sha256 cellar: :any, catalina:       "1c9230a17ab7102f4b171e37a972ade6c7e2d5708102a17ea5494be0b1d1a42e"
-    sha256 cellar: :any, mojave:         "2b4c61222b70b25c523c6083efd85ff53f1187a6afd7d88115f12cb788fa6b13"
-    sha256 cellar: :any, high_sierra:    "c481733fcc5f8057aac9fa38d5445a88e6faf54a016533bcc72eba2335d9754b"
+    sha256 cellar: :any,                 arm64_monterey: "7c66e15be8f86726fe114bb9c0be3684386f62819af22e7892876634b22b4d8d"
+    sha256 cellar: :any,                 arm64_big_sur:  "e7dffabd037eaff9a07ef2adb7c5731bd6270f04e5f3590280293fc25d478ddb"
+    sha256 cellar: :any,                 monterey:       "375cd87233c4faea0360d7b840ff7ebd7769a455429f70c8b60305bef1b32299"
+    sha256 cellar: :any,                 big_sur:        "18cc9c42960d0accd760293232f236ad1d35e0fc1e7e8f44061b72db2c2acf64"
+    sha256 cellar: :any,                 catalina:       "1c9230a17ab7102f4b171e37a972ade6c7e2d5708102a17ea5494be0b1d1a42e"
+    sha256 cellar: :any,                 mojave:         "2b4c61222b70b25c523c6083efd85ff53f1187a6afd7d88115f12cb788fa6b13"
+    sha256 cellar: :any,                 high_sierra:    "c481733fcc5f8057aac9fa38d5445a88e6faf54a016533bcc72eba2335d9754b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "39fc6b17d7d53406b021389245015114195ec47f11787faafa1b60d92100e8da"
   end
 
   head do
@@ -26,6 +27,9 @@ class Mfterm < Formula
   depends_on "libnfc"
   depends_on "libusb"
   depends_on "openssl@1.1"
+
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
 
   def install
     ENV.prepend "CPPFLAGS", "-I#{Formula["openssl@1.1"].opt_include}"
