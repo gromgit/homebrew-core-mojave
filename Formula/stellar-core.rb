b7c14bad031ed82c3c0fcc379900e68839c03981
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:34:07 +0800

    stellar-core:

--- Formula/stellar-core.rb
+++ Formula/stellar-core.rb
@@ -2,17 +2,18 @@ class StellarCore < Formula
   desc "Backbone of the Stellar (XLM) network"
   homepage "https://www.stellar.org/"
   url "https://github.com/stellar/stellar-core.git",
-      tag:      "v18.3.0",
-      revision: "2f9ce11b2e7eba7d7d38b123ee6da9e0144249f8"
+      tag:      "v18.4.0",
+      revision: "13ef7c0f3ae85306ddb8633702c649c8f6ee94bb"
   license "Apache-2.0"
   head "https://github.com/stellar/stellar-core.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "048413c08407db38f016438911cc24498d0919b8cb8a09196f1ddcc31b5ddb76"
-    sha256 cellar: :any, arm64_big_sur:  "c8d41192a8f14e1990eb9110696253a0c2c24e3fa8f9532acc00596a5b356f45"
-    sha256 cellar: :any, monterey:       "2d1d408bf2741d1c8af2c997181d29848a862f1f49dbbf057417dd76ab4ae010"
-    sha256 cellar: :any, big_sur:        "b9802e2eb2a7edc26f70184cae0f882807b1ffe86cec3b5c5f49e6ce84521e5c"
-    sha256 cellar: :any, catalina:       "c1b97b9a1f273caee556d6f24dfb096f21d3955ee25cdadf81ba06e019f3165f"
+    sha256 cellar: :any,                 arm64_monterey: "0d345d8e571beb4937082694c062a553e9fb0bddbf5cc79a3c5ff8661938a6eb"
+    sha256 cellar: :any,                 arm64_big_sur:  "665e205c4d9c4ba444ed43f94d8ccc5f898becbd29e2068fbae9c0f52664e6eb"
+    sha256 cellar: :any,                 monterey:       "92bd2a6c61961089a8308ce68d23cef5288e8ca86e9a73e62c53c344f3983ab1"
+    sha256 cellar: :any,                 big_sur:        "1e01e46e0d0ddd087caafa140a1a738b38ba0ca7271c7443a999f5edc659acb2"
+    sha256 cellar: :any,                 catalina:       "f4b8f02dca775eaf9dded2504d62f72933ff5fcef8875b2a90ae81698c2323ad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d6cc582b0bb8a2636bb071bfaba8abe0ae0f7536f9459d07069364befff4f5ad"
   end
 
   depends_on "autoconf" => :build
@@ -31,12 +32,22 @@ class StellarCore < Formula
 
   on_linux do
     depends_on "gcc"
+    depends_on "libunwind"
   end
 
-  # Needs libraries at runtime:
-  # /usr/lib/x86_64-linux-gnu/libstdc++.so.6: version `GLIBCXX_3.4.22' not found
-  # Upstream has explicitly stated gcc-5 is too old: https://github.com/stellar/stellar-core/issues/1903
-  fails_with gcc: "5"
+  # https://github.com/stellar/stellar-core/blob/master/INSTALL.md#build-dependencies
+  fails_with :gcc do
+    version "7"
+    cause "Requires C++17 filesystem"
+  end
+
+  # Fix GCC error: xdrpp/marshal.cc:24:59: error: 'size' is not a constant expression.
+  # Remove when release has updated `xdrpp` submodule.
+  patch do
+    url "https://github.com/xdrpp/xdrpp/commit/b4979a55fe19b1fd6b716f6bd2400d519aced435.patch?full_index=1"
+    sha256 "5c74c40b0e412c80d994cec28e9d0c2d92d127bc5b9f8173fd525d2812513073"
+    directory "lib/xdrpp"
+  end
 
   def install
     system "./autogen.sh"
