92d074aa3b8911c730bda2f9cf7f99e566e75140
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:08:29 +0800

    charm: no rebuilding required

--- Formula/charm.rb
+++ Formula/charm.rb
@@ -15,7 +15,8 @@ class Charm < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "623e2d62e1dc1210de466ff4e4ea1d8e0c8ea059dbdc491ff5b8f3ec1cc9603e"
   end
 
-  depends_on "go" => :build
+  # Bump to 1.18 on the next release, if possible.
+  depends_on "go@1.17" => :build
 
   def install
     system "go", "build", *std_go_args, "./cmd/charm"
