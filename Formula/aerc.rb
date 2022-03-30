f1c6b6d7fd1a14bac7a007f270a17acb15874f97
Adrian Ho <the.gromgit@gmail.com>
2022-03-30 17:10:34 +0800

    aerc: update 0.9.0 bottle.

--- Formula/aerc.rb
+++ Formula/aerc.rb
@@ -1,15 +1,14 @@
 class Aerc < Formula
   desc "Email client that runs in your terminal"
   homepage "https://aerc-mail.org/"
-  url "https://git.sr.ht/~rjarry/aerc/archive/0.8.2.tar.gz"
-  sha256 "1a35db08b90c35df7c211119a8d064500999dfe2e419ed5b753007f8f7382912"
+  url "https://git.sr.ht/~rjarry/aerc/archive/0.9.0.tar.gz"
+  sha256 "b5901feb37a55edd1f713e76c1012ac3fc0757202ddacd7d388cc7ce60638023"
   license "MIT"
   head "https://git.sr.ht/~rjarry/aerc", branch: "master"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/aerc"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "88d104e1afb4c3f8049959b7fcb331ac2c711585968a5c2155c106ea3ff7c8d9"
+    sha256 mojave: "5192601cf2fc77ef4544fed3f277a7a1c2ea685a629e1e753a8b74eef19e842e"
   end
 
   depends_on "go" => :build
