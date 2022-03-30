e84313d987f256da1109cd1c79e758c19a19f8cf
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:28:42 +0800

    paps: no rebuilding required

--- Formula/paps.rb
+++ Formula/paps.rb
@@ -6,13 +6,14 @@ class Paps < Formula
   license "LGPL-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "64750f6318462484efbf5d4656bf75896fae72f2abf0636182e27ad9ba77915c"
-    sha256 cellar: :any, arm64_big_sur:  "f8ab36ee220f8e2bfd5fb7db1c16812241ec8212cfc3ecd7c070517ac0a104b0"
-    sha256 cellar: :any, monterey:       "c1b2eb436a7cc57282726120aa8f5a03f1b76c70af74760ede5525be695db75b"
-    sha256 cellar: :any, big_sur:        "1ceacf866bec6fbe8329ef4cac025f5ccb1bccda7616ffebd0fdd24fcc33e13c"
-    sha256 cellar: :any, catalina:       "4f19499edc025464f4ce74b0755ede3c404c41d131156aebd7d24ef3ca1fe64f"
-    sha256 cellar: :any, mojave:         "2852cb269611539d7d9fa227cca164318da3d1d3acec66b7a006ea958dc31d93"
-    sha256 cellar: :any, high_sierra:    "bef1ee9210f3591f0768817f4f748e49ea708742f56ce47e744bc4a1507f3f36"
+    sha256 cellar: :any,                 arm64_monterey: "64750f6318462484efbf5d4656bf75896fae72f2abf0636182e27ad9ba77915c"
+    sha256 cellar: :any,                 arm64_big_sur:  "f8ab36ee220f8e2bfd5fb7db1c16812241ec8212cfc3ecd7c070517ac0a104b0"
+    sha256 cellar: :any,                 monterey:       "c1b2eb436a7cc57282726120aa8f5a03f1b76c70af74760ede5525be695db75b"
+    sha256 cellar: :any,                 big_sur:        "1ceacf866bec6fbe8329ef4cac025f5ccb1bccda7616ffebd0fdd24fcc33e13c"
+    sha256 cellar: :any,                 catalina:       "4f19499edc025464f4ce74b0755ede3c404c41d131156aebd7d24ef3ca1fe64f"
+    sha256 cellar: :any,                 mojave:         "2852cb269611539d7d9fa227cca164318da3d1d3acec66b7a006ea958dc31d93"
+    sha256 cellar: :any,                 high_sierra:    "bef1ee9210f3591f0768817f4f748e49ea708742f56ce47e744bc4a1507f3f36"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee348bc7685c17d5453ca8f661b98020849d5c0265ec30cba5ab8321a6333bd3"
   end
 
   depends_on "autoconf" => :build
@@ -25,7 +26,11 @@ class Paps < Formula
   depends_on "glib"
   depends_on "pango"
 
+  uses_from_macos "perl" => :build
+
   def install
+    ENV.prepend_path "PERL5LIB", Formula["intltool"].libexec/"lib/perl5" unless OS.mac?
+
     system "./autogen.sh"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
