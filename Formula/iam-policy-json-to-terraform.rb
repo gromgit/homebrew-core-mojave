744c5cabf608ca769001740d1ec6a915fb0dad74
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:18:00 +0800

    iam-policy-json-to-terraform: no rebuilding required

--- Formula/iam-policy-json-to-terraform.rb
+++ Formula/iam-policy-json-to-terraform.rb
@@ -4,7 +4,7 @@ class IamPolicyJsonToTerraform < Formula
   url "https://github.com/flosell/iam-policy-json-to-terraform/archive/1.8.0.tar.gz"
   sha256 "428ee4c7c40a77c3f2c08f1ea5b5ac145db684bba038ab113848e1697ef906dc"
   license "Apache-2.0"
-  head "https://github.com/flosell/iam-policy-json-to-terraform.git"
+  head "https://github.com/flosell/iam-policy-json-to-terraform.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "783a05da4dc552503162f5011070a1f6246ef0ab7d6303220c341ea068d7b7fc"
@@ -16,7 +16,8 @@ class IamPolicyJsonToTerraform < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "04e3ec2788df5965f842759d48c939efbd699fbcc66d2d3e000a6c844607630e"
   end
 
-  depends_on "go" => :build
+  # Bump to 1.18 on the next release.
+  depends_on "go@1.17" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
