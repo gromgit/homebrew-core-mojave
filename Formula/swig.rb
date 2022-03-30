e5029b37bf153b4a9847d807c79fb4659fd58a1e
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:34:38 +0800

    swig: no rebuilding required

--- Formula/swig.rb
+++ Formula/swig.rb
@@ -6,9 +6,14 @@ class Swig < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/swig"
-    rebuild 1
-    sha256 mojave: "11346aefa111741b1f33881310f2726588285d46acc66328f2c0b82d675ee050"
+    sha256 arm64_monterey: "9e60e978ae83f0afd63aa8a27d3e418eda0cb75b89c0d22e25a0ccc5f2e00b3e"
+    sha256 arm64_big_sur:  "918c070202e0138b64b2e27f262aae3a72ab9f273f14842802d1fbe9169e66fc"
+    sha256 monterey:       "170414d38ab55ef50cb1275fbb0a9e0f0248ad1e23755af8ac2184a6825d514e"
+    sha256 big_sur:        "f198353656b61cb35b5c28ed6c9cb10689d2a0fc69529cfbcbc0fbd75a027e27"
+    sha256 catalina:       "530e80b7e7dcd28469b52fc3b668683a97b72642ebf2b6d4e6708d14f05e7286"
+    sha256 mojave:         "50afb5930cb37af2e400f0369f6da15b1d4922c1f72f45d13e7e3f8bd9d6d27b"
+    sha256 high_sierra:    "8bab440005b048ce454a3dd50ba608e1f85391edd73e9e40510269e923cad238"
+    sha256 x86_64_linux:   "2a5fe8e40d8ba5e09fd194e9e36f8fe4bd7a427e31d1456a99bb45539074c92a"
   end
 
   head do
@@ -48,12 +53,11 @@ class Swig < Formula
       puts Test.add(1, 1)
     EOS
     system "#{bin}/swig", "-ruby", "test.i"
-    on_macos do
+    if OS.mac?
       system ENV.cc, "-c", "test.c"
       system ENV.cc, "-c", "test_wrap.c", "-I#{MacOS.sdk_path}/System/Library/Frameworks/Ruby.framework/Headers/"
       system ENV.cc, "-bundle", "-undefined", "dynamic_lookup", "test.o", "test_wrap.o", "-o", "test.bundle"
-    end
-    on_linux do
+    else
       ruby = Formula["ruby"]
       args = Utils.safe_popen_read(
         ruby.opt_bin/"ruby", "-e", "'puts RbConfig::CONFIG[\"LIBRUBYARG\"]'"
