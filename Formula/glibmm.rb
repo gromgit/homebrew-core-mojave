5c78407ba9e9fb48a7fc8307e630377aada645b5
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:15:08 +0800

    glibmm: no rebuilding required

--- Formula/glibmm.rb
+++ Formula/glibmm.rb
@@ -67,9 +67,7 @@ class Glibmm < Formula
       -lgobject-2.0
       -lsigc-3.0
     ]
-    on_macos do
-      flags << "-lintl"
-    end
+    flags << "-lintl" if OS.mac?
     system ENV.cxx, "-std=c++17", "test.cpp", "-o", "test", *flags
     system "./test"
   end
