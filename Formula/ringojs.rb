72af23898346ceba553429c7a51b0e5298740950
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:31:53 +0800

    ringojs: no rebuilding required

--- Formula/ringojs.rb
+++ Formula/ringojs.rb
@@ -12,10 +12,12 @@ class Ringojs < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "86b956cc3c7d0279529323450abaf3bca2a61ae520cdc262c44a2ff5035b810c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "445095a17c58cb61c8275e174c2630cd4ac2acba61198fdf7fe978544815ad9d"
-    sha256 cellar: :any_skip_relocation, catalina:      "445095a17c58cb61c8275e174c2630cd4ac2acba61198fdf7fe978544815ad9d"
-    sha256 cellar: :any_skip_relocation, mojave:        "445095a17c58cb61c8275e174c2630cd4ac2acba61198fdf7fe978544815ad9d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d737033338e11f8af0d51d220ad0cc9ce4d64231670489ca3036b3df338f6970"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "86b956cc3c7d0279529323450abaf3bca2a61ae520cdc262c44a2ff5035b810c"
+    sha256 cellar: :any_skip_relocation, monterey:       "e704fda56cce28b10c4bdb8a3d1362e9d48178c826f676bbf33d138f615e3632"
+    sha256 cellar: :any_skip_relocation, big_sur:        "445095a17c58cb61c8275e174c2630cd4ac2acba61198fdf7fe978544815ad9d"
+    sha256 cellar: :any_skip_relocation, catalina:       "445095a17c58cb61c8275e174c2630cd4ac2acba61198fdf7fe978544815ad9d"
+    sha256 cellar: :any_skip_relocation, mojave:         "445095a17c58cb61c8275e174c2630cd4ac2acba61198fdf7fe978544815ad9d"
   end
 
   depends_on "openjdk@11"
