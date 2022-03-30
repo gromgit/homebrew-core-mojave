83f84d0cb167e9a8b4265086f732ff47f055fb48
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:11:40 +0800

    e2tools: no rebuilding required

--- Formula/e2tools.rb
+++ Formula/e2tools.rb
@@ -6,12 +6,13 @@ class E2tools < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e4c94d559938d504bc7ddc19139c8af004385b57db58bfd24946c451b4752928"
-    sha256 cellar: :any_skip_relocation, big_sur:       "38978d9bea279b06c646bc82631d59edee4a519164f4b0a4f39b33621ae1183b"
-    sha256 cellar: :any_skip_relocation, catalina:      "93eab5f2d207ac8f27a9b27db13408b4b7f8a3cfee4ecbca9d9977a851a41576"
-    sha256 cellar: :any_skip_relocation, mojave:        "1ad81d83b87fc67a54698e6af829dd0945119a41a445383268f1d0190ff7b38d"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "069988a622ce0587927a4a50b70b778b461840d2db2e49259e1123123bf6a2ff"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fc52ded6e99ba793baaf68c70a158ab156829f74e090b1182f9dc15d816357ba"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4764d51ea5016a50dc90a7665db17f327742011eb24d6bc3635b5e4cb7e3efda"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e4c94d559938d504bc7ddc19139c8af004385b57db58bfd24946c451b4752928"
+    sha256 cellar: :any_skip_relocation, big_sur:        "38978d9bea279b06c646bc82631d59edee4a519164f4b0a4f39b33621ae1183b"
+    sha256 cellar: :any_skip_relocation, catalina:       "93eab5f2d207ac8f27a9b27db13408b4b7f8a3cfee4ecbca9d9977a851a41576"
+    sha256 cellar: :any_skip_relocation, mojave:         "1ad81d83b87fc67a54698e6af829dd0945119a41a445383268f1d0190ff7b38d"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "069988a622ce0587927a4a50b70b778b461840d2db2e49259e1123123bf6a2ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fc52ded6e99ba793baaf68c70a158ab156829f74e090b1182f9dc15d816357ba"
   end
 
   depends_on "autoconf" => :build
