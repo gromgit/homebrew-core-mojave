df145263f0a6738e3b06544a34c0ea7ca12b4cc5
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:14:43 +0800

    gitbatch: no rebuilding required

--- Formula/gitbatch.rb
+++ Formula/gitbatch.rb
@@ -15,7 +15,8 @@ class Gitbatch < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "6d447c8dc2e642b6a281df3956224ece3f45cd0f074391b8dfa9f916cf5e7dcb"
   end
 
-  depends_on "go" => :build
+  # Bump to 1.18 on the next release, if possible.
+  depends_on "go@1.17" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/gitbatch"
