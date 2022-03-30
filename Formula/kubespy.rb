b3245754d363b84f7bde92ee2808a2a6d0fc4289
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:20:29 +0800

    kubespy: no rebuilding required

--- Formula/kubespy.rb
+++ Formula/kubespy.rb
@@ -16,11 +16,11 @@ class Kubespy < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "aa4dd3522e26e234773a2cdd5ba837ccadcbe4babb7f5889faf8c11dda1bb10d"
   end
 
-  depends_on "go" => :build
+  # Bump to 1.18 on the next release, if possible.
+  depends_on "go@1.17" => :build
 
   def install
-    system "go", "build", *std_go_args,
-              "-ldflags", "-X github.com/pulumi/kubespy/version.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags: "-X github.com/pulumi/kubespy/version.Version=#{version}")
   end
 
   test do
