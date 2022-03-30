b1afc3e726eec0cd4bb2008f5eaed7fff825e685
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:11:01 +0800

    dockutil: no rebuilding required

--- Formula/dockutil.rb
+++ Formula/dockutil.rb
@@ -5,6 +5,11 @@ class Dockutil < Formula
   sha256 "6dbbc1467caaab977bf4c9f2d106ceadfedd954b6a4848c54c925aff81159a65"
   license "Apache-2.0"
 
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
   bottle do
     sha256 cellar: :any_skip_relocation, all: "f5f87d9e286c2b294bb157ac9f87baa2720fff044c7a92c0b80b9cb82db8a87e"
   end
