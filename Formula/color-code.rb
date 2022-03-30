de5bf8c7c83f9b9b6a8a2ba0449c1d4dbbd20d11
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:09:18 +0800

    color-code: no rebuilding required

--- Formula/color-code.rb
+++ Formula/color-code.rb
@@ -14,6 +14,7 @@ class ColorCode < Formula
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "eba4c8dc8989ed4548ffafcfde3d8cc312a1ae523df8409d3a5a9e6d203d975c"
     sha256 cellar: :any,                 arm64_big_sur:  "8e7c81eca9f900ce20df5013b24120a39732113506ca72db063c52dec64fb028"
+    sha256 cellar: :any,                 monterey:       "2620e1ce1cfbbb23477a68a0a25fa7e49699611340798ef6674f4d49878627b0"
     sha256 cellar: :any,                 big_sur:        "9aac69e2526ff08545f2f601fb6847ea1b131fa30fee2f56f2003bafdd163cc0"
     sha256 cellar: :any,                 catalina:       "29407731a7c5e10b3812346227ef3ba75ce4b16bf48e603036442140691e0f8e"
     sha256 cellar: :any,                 mojave:         "f8f2e6a8f4aac3307568cccaa8eb202c3be01d653396e1cd8bb9ccf76d24f6db"
