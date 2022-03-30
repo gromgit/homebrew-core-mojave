7c96f440a38238eb41095360f7faa5e4b6cdf433
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-30 09:11:35 +0800

    dvorak7min: no rebuilding required

--- Formula/dvorak7min.rb
+++ Formula/dvorak7min.rb
@@ -16,7 +16,10 @@ class Dvorak7min < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "052c259da37d2e9576fdf1809ce526dd54cedd362bbe747f02fa088e6568983b"
     sha256 cellar: :any_skip_relocation, el_capitan:    "d4bf1a053028f0712193e33911c2af3fb4f0a71b37480969b5c03b798d4930ae"
     sha256 cellar: :any_skip_relocation, yosemite:      "42cad6dbf3f41053e5ba7509657dcf7e02c6211412efb246eaaa9de853a09d35"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "88198e60fed089e8dcc4c8bb9fb955428cecd480578fd1a339e177b1c1e748df"
   end
+
+  uses_from_macos "ncurses"
 
   def install
     # Remove pre-built ELF binary first
