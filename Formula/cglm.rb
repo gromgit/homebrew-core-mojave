e960895a68fac3376ec074cfc6dbf4a9f3ab4d19
Adrian Ho <the.gromgit@gmail.com>
2022-03-31 18:18:49 +0800

    cglm: update 0.8.5 bottle.

--- Formula/cglm.rb
+++ Formula/cglm.rb
@@ -1,18 +1,13 @@
 class Cglm < Formula
   desc "Optimized OpenGL/Graphics Math (glm) for C"
   homepage "https://github.com/recp/cglm"
-  url "https://github.com/recp/cglm/archive/v0.8.4.tar.gz"
-  sha256 "42f84c42c8a3e62954da77ab5c5d3264033d5802009e175db7921332a476126e"
+  url "https://github.com/recp/cglm/archive/v0.8.5.tar.gz"
+  sha256 "baefa21342d228a83c90708459a745d5aa9b0ebb381555eea42db1f37fdf7a5a"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "30c211f655e4347ed7e5582ff08643f53406b8427d804d8634b13add6dfdeb2c"
-    sha256 cellar: :any,                 arm64_big_sur:  "29ff2689e54b405a92d819ff5cf12a07f3d89bceb5094d623f3ccdccfd818862"
-    sha256 cellar: :any,                 monterey:       "99c7d4574ab2000dfc461fa623382d9432db7f9b2d92a56583ac6b63e9c62577"
-    sha256 cellar: :any,                 big_sur:        "2fe11e20a528578738d09dc81ee3454655aa847d35bb3c2476e4b398071399d0"
-    sha256 cellar: :any,                 catalina:       "cafb0e2119b9d0a56acd3cb8c2aae7438a4dd9aeb3bf4ce1014780c3ef2dcccd"
-    sha256 cellar: :any,                 mojave:         "3d9041245205d45fea1da390d139fa2457a92a5181938b6a51c1c8de3393ce74"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "35c5030d9019fae7bea72f57d9fd7ff9efbd3bc5e70bace3f34aa1e10a3d53a5"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cglm"
+    sha256 cellar: :any, mojave: "90db50e6e7b46587df1fa4cc8ed41493eea9cade0260859b7064c12f435a3704"
   end
 
   depends_on "autoconf" => :build
