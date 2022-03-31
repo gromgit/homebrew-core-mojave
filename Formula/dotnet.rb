803535512607a907142449276d5adfed1b665392
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-01 03:18:30 +0800

    dotnet: update 6.0.103 bottle.

--- Formula/dotnet.rb
+++ Formula/dotnet.rb
@@ -2,14 +2,13 @@ class Dotnet < Formula
   desc ".NET Core"
   homepage "https://dotnet.microsoft.com/"
   url "https://github.com/dotnet/installer.git",
-      tag:      "v6.0.100",
-      revision: "9e8b04bbff820c93c142f99a507a46b976f5c14c"
+      tag:      "v6.0.103-source-build",
+      revision: "2c677ffc1e93aea3b6c92d6121d04fdaeba32d32"
   license "MIT"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dotnet"
-    rebuild 1
-    sha256 cellar: :any, mojave: "4f54d284205cbb52fdf27bfacdf9757c464cd15362cd94af2e6cb0b17562778f"
+    sha256 cellar: :any, mojave: "fadc3ce26e7b20bf634c51b3e1b8464946232bf5196e0017e383a63f32bf6e12"
   end
 
   depends_on "cmake" => :build
@@ -38,22 +37,6 @@ bottle do
   # GCC builds have limited support via community.
   fails_with :gcc
 
-  # Fix build with Clang 13.
-  # PR ref: https://github.com/dotnet/runtime/pull/63314
-  # TODO: Remove this in future release with .NET runtime v6.0.2+
-  resource "runtime-clang13-patch" do
-    url "https://github.com/dotnet/runtime/commit/f86caa54678535ead8e1977da37025a96e2afe8a.patch?full_index=1"
-    sha256 "bc264ce2a1f9f7f3d27db10276bb2d1b979f66da06727aaa04be15c36086a9a3"
-  end
-
-  # Fix previously-source-built bootstrap.
-  # PR ref: https://github.com/dotnet/installer/pull/12642
-  # TODO: Remove this in the next release
-  patch do
-    url "https://github.com/dotnet/installer/commit/7f02ccd30f55e7ac3436bd32af4b207869541ebf.patch?full_index=1"
-    sha256 "ff51630f9bfc4bbb502f57c6b1348d2e530a006234150606f9327fedcbb6591c"
-  end
-
   # Fix build failure on macOS due to missing ILAsm/ILDAsm
   # Fix build failure on macOS ARM due to `osx-x64` override
   patch :DATA
@@ -65,19 +48,16 @@ bottle do
       ENV.prepend_path "PATH", Formula["gnu-sed"].opt_libexec/"gnubin"
     end
 
-    # TODO: Remove this in future release with .NET runtime v6.0.2+
-    (buildpath/"src/SourceBuild/tarball/patches/runtime").install resource("runtime-clang13-patch")
-
     Dir.mktmpdir do |sourcedir|
       system "./build.sh", "/p:ArcadeBuildTarball=true", "/p:TarballDir=#{sourcedir}"
 
       cd sourcedir
-      # Disable package validation in source-build for reliability
-      # PR ref: https://github.com/dotnet/runtime/pull/60881
-      # TODO: Remove this in the next release
-      inreplace Dir["src/runtime.*/eng/packaging.targets"].first,
-                "<EnablePackageValidation>true</EnablePackageValidation>",
-                "<EnablePackageValidation>false</EnablePackageValidation>"
+      # Workaround for error MSB4018 while building 'installer in tarball' due
+      # to trying to find aspnetcore-runtime-internal v6.0.0 rather than current.
+      # TODO: Remove when packaging is fixed
+      inreplace Dir["src/installer.*/src/redist/targets/GenerateLayout.targets"].first,
+                "$(MicrosoftAspNetCoreAppRuntimePackageVersion)",
+                "$(MicrosoftAspNetCoreAppRuntimewinx64PackageVersion)"
 
       # Rename patch fails on case-insensitive systems like macOS
       # TODO: Remove whenever patch is no longer used
@@ -178,17 +158,17 @@ index f3ed143f8..2c62d6854 100644
      <OverrideTargetRid Condition="'$(TargetOS)' == 'Windows_NT'">win-x64</OverrideTargetRid>
 
 diff --git a/src/SourceBuild/tarball/content/scripts/bootstrap/buildBootstrapPreviouslySB.csproj b/src/SourceBuild/tarball/content/scripts/bootstrap/buildBootstrapPreviouslySB.csproj
-index 0a2fcff17..9033ff11a 100644
+index 14921a48f..3a34e8749 100644
 --- a/src/SourceBuild/tarball/content/scripts/bootstrap/buildBootstrapPreviouslySB.csproj
 +++ b/src/SourceBuild/tarball/content/scripts/bootstrap/buildBootstrapPreviouslySB.csproj
-@@ -23,6 +23,14 @@
-     <PackageReference Include="runtime.linux-x64.Microsoft.NETCore.ILDAsm" Version="$(RuntimeLinuxX64MicrosoftNETCoreILDAsmVersion)" />
-     <PackageReference Include="runtime.linux-x64.Microsoft.NETCore.TestHost" Version="$(RuntimeLinuxX64MicrosoftNETCoreTestHostVersion)" />
-     <PackageReference Include="runtime.linux-x64.runtime.native.System.IO.Ports" Version="$(RuntimeLinuxX64RuntimeNativeSystemIOPortsVersion)" />
-+    <PackageReference Include="runtime.osx-arm64.Microsoft.NETCore.ILAsm" Version="$(RuntimeOsxArm64MicrosoftNETCoreILAsmVersion)" />
-+    <PackageReference Include="runtime.osx-arm64.Microsoft.NETCore.ILDAsm" Version="$(RuntimeOsxArm64MicrosoftNETCoreILDAsmVersion)" />
-+    <PackageReference Include="runtime.osx-arm64.Microsoft.NETCore.TestHost" Version="$(RuntimeOsxArm64MicrosoftNETCoreTestHostVersion)" />
-+    <PackageReference Include="runtime.osx-arm64.runtime.native.System.IO.Ports" Version="$(RuntimeOsxArm64RuntimeNativeSystemIOPortsVersion)" />
+@@ -33,6 +33,14 @@
+     <!-- There's no nuget package for runtime.linux-musl-x64.runtime.native.System.IO.Ports
+     <PackageReference Include="runtime.linux-musl-x64.runtime.native.System.IO.Ports" Version="$(RuntimeLinuxX64RuntimeNativeSystemIOPortsVersion)" />
+     -->
++    <PackageReference Include="runtime.osx-arm64.Microsoft.NETCore.ILAsm" Version="$(RuntimeLinuxX64MicrosoftNETCoreILAsmVersion)" />
++    <PackageReference Include="runtime.osx-arm64.Microsoft.NETCore.ILDAsm" Version="$(RuntimeLinuxX64MicrosoftNETCoreILDAsmVersion)" />
++    <PackageReference Include="runtime.osx-arm64.Microsoft.NETCore.TestHost" Version="$(RuntimeLinuxX64MicrosoftNETCoreTestHostVersion)" />
++    <PackageReference Include="runtime.osx-arm64.runtime.native.System.IO.Ports" Version="$(RuntimeLinuxX64RuntimeNativeSystemIOPortsVersion)" />
 +    <PackageReference Include="runtime.osx-x64.Microsoft.NETCore.ILAsm" Version="$(RuntimeLinuxX64MicrosoftNETCoreILAsmVersion)" />
 +    <PackageReference Include="runtime.osx-x64.Microsoft.NETCore.ILDAsm" Version="$(RuntimeLinuxX64MicrosoftNETCoreILDAsmVersion)" />
 +    <PackageReference Include="runtime.osx-x64.Microsoft.NETCore.TestHost" Version="$(RuntimeLinuxX64MicrosoftNETCoreTestHostVersion)" />
@@ -196,4 +176,3 @@ index 0a2fcff17..9033ff11a 100644
    </ItemGroup>
 
    <Target Name="BuildBoostrapPreviouslySourceBuilt" AfterTargets="Restore">
-
