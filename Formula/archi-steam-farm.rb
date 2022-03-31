c7be856a75359aae75b615e83f0b42062264b6b5
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-01 03:22:45 +0800

    archi-steam-farm: update 5.2.3.7 bottle.

--- Formula/archi-steam-farm.rb
+++ Formula/archi-steam-farm.rb
@@ -2,8 +2,8 @@ class ArchiSteamFarm < Formula
   desc "Application for idling Steam cards from multiple accounts simultaneously"
   homepage "https://github.com/JustArchiNET/ArchiSteamFarm"
   url "https://github.com/JustArchiNET/ArchiSteamFarm.git",
-      tag:      "5.2.2.5",
-      revision: "29fae79aa03e8d916a387099dc6decd541386e81"
+      tag:      "5.2.3.7",
+      revision: "d087aacbfbb4ce13e4ccdb852c3a58a882e9cc6d"
   license "Apache-2.0"
   head "https://github.com/JustArchiNET/ArchiSteamFarm.git", branch: "main"
 
@@ -12,10 +12,9 @@ class ArchiSteamFarm < Formula
     strategy :github_latest
   end
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/archi-steam-farm"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "77e1c525b349e370ce90c545d1ba50e7f96b2b0b697ae6a573416f56c38ec408"
+    sha256 cellar: :any_skip_relocation, mojave: "952aea5f15ca99fe554e04b1b5d662d3e933a9fdfac4135b019cd1c1406d58d6"
   end
 
   depends_on "dotnet"
