222c6bb03be1ada6ca8684196aab48ef75037e98
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-01 04:29:10 +0800

    frei0r: update 1.8.0 bottle.

--- Formula/frei0r.rb
+++ Formula/frei0r.rb
@@ -1,8 +1,8 @@
 class Frei0r < Formula
   desc "Minimalistic plugin API for video effects"
   homepage "https://frei0r.dyne.org/"
-  url "https://files.dyne.org/frei0r/releases/frei0r-plugins-1.7.0.tar.gz"
-  sha256 "1b1ff8f0f9bc23eed724e94e9a7c1d8f0244bfe33424bb4fe68e6460c088523a"
+  url "https://files.dyne.org/frei0r/releases/frei0r-plugins-1.8.0.tar.gz"
+  sha256 "45a28655caf057227b442b800ca3899e93490515c81e212d219fdf4a7613f5c4"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -11,14 +11,8 @@ class Frei0r < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bbe34892f59b38e123a2749bc068933dbe1163427bc6124cdc7fad334fa04f57"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "284401af98f9f86f7f4aa8dd2a2cd7ccd22841decc2065d730b9811cc683cd3d"
-    sha256 cellar: :any_skip_relocation, monterey:       "682822c4f6f71ac68dc89db870e06dbdda7563a991652d3cc9b5808e3b818f74"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cafe9dbba970e60d275480465cf7f87b7847063fb2113e7fd862947de0735865"
-    sha256 cellar: :any_skip_relocation, catalina:       "5076041b5f3d76b94866ab2b97ad34523ee40cfa314e6f7d2bf460ce304de872"
-    sha256 cellar: :any_skip_relocation, mojave:         "5e23b93a7ff4a2ee64c5a969b17bf6a52329e6da17c0612b46aa2ceec3fb5b39"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "a6a4648e1ff6263616f532a4648e1eb56e68d510d04e768becb2caf5ca961e3a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dd92d047a4e4cf723f8b22eb120622bd026d7c016316b7dd15c61f787ca93924"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/frei0r"
+    sha256 cellar: :any_skip_relocation, mojave: "dd7cff184ab10b3546882bb1724b7d644b255796c70cbe2f128babf11d8c89c4"
   end
 
   depends_on "cmake" => :build
