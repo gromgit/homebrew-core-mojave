6f6ab298ab04d727c52b6f81489c9b9f02d8ca11
Adrian Ho <the.gromgit@gmail.com>
2022-03-31 13:35:08 +0800

    aspectj: update 1.9.8 bottle.

--- Formula/aspectj.rb
+++ Formula/aspectj.rb
@@ -1,8 +1,8 @@
 class Aspectj < Formula
   desc "Aspect-oriented programming for Java"
   homepage "https://www.eclipse.org/aspectj/"
-  url "https://github.com/eclipse/org.aspectj/releases/download/V1_9_7/aspectj-1.9.7.jar"
-  sha256 "c6b83cf272ce71a81a02c1529e760286196fe842a1741aa42494c3ce1501bd3a"
+  url "https://github.com/eclipse/org.aspectj/releases/download/V1_9_8/aspectj-1.9.8.jar"
+  sha256 "aa0606b87987281aabbed6de681b12e721d8f4c1e99c1e861c6fcd98f9b55e5b"
   license "EPL-2.0"
 
   livecheck do
@@ -11,13 +11,8 @@ class Aspectj < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "47d9c8a95f6a6d675ace914b7b13365dcc207fed8825d96ba338b541eb2fc425"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5f94378ed79ac6d24889ebf3b40df02d159988c8673fb55c54997bc89ff46895"
-    sha256 cellar: :any_skip_relocation, monterey:       "c5cd6dae63d140d489b9aa09fc779bb100dbba85c6df71bc9e1b884c56b7c2f6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f0ae7be29aa7df4960f1ae71a30f9ce57d9c28300591deb042b68fb439157ec8"
-    sha256 cellar: :any_skip_relocation, catalina:       "445830c663aa8379176a05874f3689c13ebaf953e50afe8e193322353db9d4aa"
-    sha256 cellar: :any_skip_relocation, mojave:         "751669f59894109ea55f8d8a22ded54b459f20505f74c33fd57fc9b47de36b26"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0be92ed60e7bc7639c8ba77b7e545044f20ed9d1e44dbc797f284da6df548a1b"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/aspectj"
+    sha256 cellar: :any_skip_relocation, mojave: "a28d0e99205fe32deacaf687dc5683e35075af4049cc15efe923211704e1d83b"
   end
 
   depends_on "openjdk"
