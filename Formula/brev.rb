fca7ec9be4485c670f5fa40396633271daa5f901
Adrian Ho <the.gromgit@gmail.com>
2022-03-31 17:16:59 +0800

    brev: update 0.6.36 bottle.

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -1,14 +1,13 @@
 class Brev < Formula
   desc "CLI tool for managing workspaces provided by brev.dev"
   homepage "https://docs.brev.dev"
-  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.19.tar.gz"
-  sha256 "ff6fd0afa4debad5cac190519a5cef250246df8cddfb84dc2d5bc40c2624d36d"
+  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.36.tar.gz"
+  sha256 "f16d2bda76ae1fd5be6d26664b96302b5d4d22ca6581d46cf98d279226852834"
   license "MIT"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/brev"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "4a0842052c5a2cf42e3b943ec888aae7596e477ed237a13bb93a552b28e07e5f"
+    sha256 cellar: :any_skip_relocation, mojave: "0210351edd8e83e3f0bb9b12120e1a6f5d08dea3c08117fd560a2ab39f9de28c"
   end
 
   depends_on "go" => :build
