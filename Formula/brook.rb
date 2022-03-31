c11e75174e803e50a8fadc7693f70422996b8886
Adrian Ho <the.gromgit@gmail.com>
2022-03-31 17:18:47 +0800

    brook: update 20220406 bottle.

--- Formula/brook.rb
+++ Formula/brook.rb
@@ -1,13 +1,13 @@
 class Brook < Formula
   desc "Cross-platform strong encryption and not detectable proxy. Zero-Configuration"
   homepage "https://txthinking.github.io/brook/"
-  url "https://github.com/txthinking/brook/archive/refs/tags/v20220401.tar.gz"
-  sha256 "cd3c467d23f4677c51e6764e0a3203cf39ac6c1081203bfd9f5c79680317be22"
+  url "https://github.com/txthinking/brook/archive/refs/tags/v20220406.tar.gz"
+  sha256 "cf4433263cc755edfe56be66d206b7ee5083faaaa8b30bb4102174ad73e22764"
   license "GPL-3.0-only"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/brook"
-    sha256 cellar: :any_skip_relocation, mojave: "d7a4ed1fd9d9cc4ea46bd19ccad226ae662d89b096a936dea7b3e64bbc140d10"
+    sha256 cellar: :any_skip_relocation, mojave: "634588dba2da76b9b17f4c4e84b87070c90742653af55ba6bebba8bc598299dc"
   end
 
   depends_on "go" => :build
@@ -18,6 +18,6 @@ bottle do
 
   test do
     output = shell_output "#{bin}/brook link --server 1.2.3.4:56789 --password hello"
-    assert_match "brook://server?address=&insecure=&name=&password=hello&server=1.2.3.4%3A56789&username=", output
+    assert_match "brook://server?address=&insecure=&name=&password=hello", output
   end
 end
