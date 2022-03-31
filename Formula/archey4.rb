f5b8a771be2bc0237350856fc94a3acb8bf3fa27
Adrian Ho <the.gromgit@gmail.com>
2022-03-31 13:27:00 +0800

    archey4: update 4.13.4 bottle.

--- Formula/archey4.rb
+++ Formula/archey4.rb
@@ -3,14 +3,13 @@ class Archey4 < Formula
 
   desc "Simple system information tool written in Python"
   homepage "https://github.com/HorlogeSkynet/archey4"
-  url "https://files.pythonhosted.org/packages/db/06/45e8c09c600c0647aa549b66e08f8dcf15203f81efbdd8f0bf4a784bf8e2/archey4-4.13.3.tar.gz"
-  sha256 "8c81ad0267d6d4689357d27e274fa142e10a8e619184bfafd9e57d9a8a720848"
+  url "https://files.pythonhosted.org/packages/be/f1/48885ef271b76ac590e239c04531af48009b85b54b60c1d2afba9b0bf463/archey4-4.13.4.tar.gz"
+  sha256 "9813ed0a1d5131756375e4113e6b158723d299353dab3d51b6ac0420af402e2c"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/archey4"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "64f6418c77db68184e764372df1f5a71f7bc21df8f2e35ce3e43b41763be88af"
+    sha256 cellar: :any_skip_relocation, mojave: "d3ad369be4342de683f4424ea3f1cf76ec7cbf0eb5cc06e7f8acaac7f591bef8"
   end
 
   depends_on "python@3.10"
@@ -18,8 +17,8 @@ class Archey4 < Formula
   conflicts_with "archey", because: "both install `archey` binaries"
 
   resource "distro" do
-    url "https://files.pythonhosted.org/packages/a5/26/256fa167fe1bf8b97130b4609464be20331af8a3af190fb636a8a7efd7a2/distro-1.6.0.tar.gz"
-    sha256 "83f5e5a09f9c5f68f60173de572930effbcc0287bb84fdc4426cb4168c088424"
+    url "https://files.pythonhosted.org/packages/b5/7e/ddfbd640ac9a82e60718558a3de7d5988a7d4648385cf00318f60a8b073a/distro-1.7.0.tar.gz"
+    sha256 "151aeccf60c216402932b52e40ee477a939f8d58898927378a02abbe852c1c39"
   end
 
   resource "netifaces" do
