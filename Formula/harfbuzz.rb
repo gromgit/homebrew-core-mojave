4466086c3e52b4a9701af00169b3d27c2726ba2e
Adrian Ho <aho+vm-mojave@03s.net>
2022-03-31 22:23:18 +0800

    harfbuzz: update 4.1.0 bottle.

--- Formula/harfbuzz.rb
+++ Formula/harfbuzz.rb
@@ -1,15 +1,14 @@
 class Harfbuzz < Formula
   desc "OpenType text shaping engine"
   homepage "https://github.com/harfbuzz/harfbuzz"
-  url "https://github.com/harfbuzz/harfbuzz/archive/3.4.0.tar.gz"
-  sha256 "810bcd3d22fae3c2c18c3688455abc1cd0d7fb2fae25404890b0d77e6443bd0a"
+  url "https://github.com/harfbuzz/harfbuzz/archive/4.1.0.tar.gz"
+  sha256 "0dad9332aa017d216981382cc07a9cf115740990c83b81ce3ea71ad88026d7f1"
   license "MIT"
   head "https://github.com/harfbuzz/harfbuzz.git", branch: "main"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/harfbuzz"
-    rebuild 1
-    sha256 cellar: :any, mojave: "053bdcefc5baac83305ae700cc9dcd3bd43010428c25f26f0db9d557b50e0ccd"
+    sha256 cellar: :any, mojave: "56de076c9aade4a6044da518ddd05b96f6c15e034515f67d93b705764f90856f"
   end
 
   depends_on "meson" => :build
@@ -21,7 +20,7 @@ bottle do
   depends_on "graphite2"
   depends_on "icu4c"
 
-  resource "ttf" do
+  resource "homebrew-test-ttf" do
     url "https://github.com/harfbuzz/harfbuzz/raw/fc0daafab0336b847ac14682e581a8838f36a0bf/test/shaping/fonts/sha1sum/270b89df543a7e48e206a2d830c0e10e5265c630.ttf"
     sha256 "9535d35dab9e002963eef56757c46881f6b3d3b27db24eefcc80929781856c77"
   end
@@ -47,7 +46,7 @@ bottle do
   end
 
   test do
-    resource("ttf").stage do
+    resource("homebrew-test-ttf").stage do
       shape = `echo 'സ്റ്റ്' | #{bin}/hb-shape 270b89df543a7e48e206a2d830c0e10e5265c630.ttf`.chomp
       assert_equal "[glyph201=0+1183|U0D4D=0+0]", shape
     end
