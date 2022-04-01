e7382f88bb4e3414f98c9abf54cfa76abca59061
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-01 17:37:17 +0800

    cargo-instruments: update 0.4.5 bottle.

--- Formula/cargo-instruments.rb
+++ Formula/cargo-instruments.rb
@@ -1,14 +1,13 @@
 class CargoInstruments < Formula
   desc "Easily generate Instruments traces for your rust crate"
   homepage "https://github.com/cmyr/cargo-instruments"
-  url "https://github.com/cmyr/cargo-instruments/archive/v0.4.4.tar.gz"
-  sha256 "e71b29af433f5701483827620866fd6302999d08ddb559578261951da15601cc"
+  url "https://github.com/cmyr/cargo-instruments/archive/v0.4.5.tar.gz"
+  sha256 "aa7539badcca7ede2973face64f0daacfa8c76fac511d21397b9b6837c138cda"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cargo-instruments"
-    rebuild 2
-    sha256 cellar: :any, mojave: "cc0418e5e6e2ed0030fd9d572423c1537661c3874114b5e024a20a3633f091aa"
+    sha256 cellar: :any, mojave: "c10f12f63f2e6b3b18809a40bb513d899427de0e35bddf575dd30c2f5224a8b7"
   end
 
   depends_on "rust" => :build
