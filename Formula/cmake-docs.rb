246b5361792a1a529460a8f84e126e6d5671edda
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-01 22:58:17 +0800

    cmake-docs: update 3.23.0 bottle.

--- Formula/cmake-docs.rb
+++ Formula/cmake-docs.rb
@@ -1,10 +1,10 @@
 class CmakeDocs < Formula
   desc "Documentation for CMake"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.22.2/cmake-3.22.2.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/cmake-3.22.2.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.22.2.tar.gz"
-  sha256 "3c1c478b9650b107d452c5bd545c72e2fad4e37c09b89a1984b9a2f46df6aced"
+  url "https://github.com/Kitware/CMake/releases/download/v3.23.0/cmake-3.23.0.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/cmake-3.23.0.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.23.0.tar.gz"
+  sha256 "5ab0a12f702f44013be7e19534cd9094d65cc9fe7b2cd0f8c9e5318e0fe4ac82"
   license "BSD-3-Clause"
   head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
 
@@ -14,7 +14,7 @@ class CmakeDocs < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cmake-docs"
-    sha256 cellar: :any_skip_relocation, mojave: "df4b36a4e3d23376179630de46bde16db1d603140e34e2608e7c3b2f99384308"
+    sha256 cellar: :any_skip_relocation, mojave: "f1578f3347f7ce6c5df95b95a9fb81c3aa585668a9acea36f24b03e68265f19f"
   end
 
   depends_on "cmake" => :build
