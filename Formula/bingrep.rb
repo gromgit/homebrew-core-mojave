269fcdc88a0b173bc3d18f82ca239bd03f91e34a
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-01 13:15:14 +0800

    bingrep: update 0.10.0 bottle.

--- Formula/bingrep.rb
+++ Formula/bingrep.rb
@@ -1,13 +1,13 @@
 class Bingrep < Formula
   desc "Greps through binaries from various OSs and architectures"
   homepage "https://github.com/m4b/bingrep"
-  url "https://github.com/m4b/bingrep/archive/v0.9.1.tar.gz"
-  sha256 "25db3c1d3e8c8dc9a41c6b87199f320c79b1a2706e8a4f68d2590fce71dabf8a"
+  url "https://github.com/m4b/bingrep/archive/v0.10.0.tar.gz"
+  sha256 "3bc4ebaf179d72b82277e7130d44c15e2cc646d388124d0acdb2ca5f33e93af6"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bingrep"
-    sha256 cellar: :any_skip_relocation, mojave: "04983031a7f17493adeab9b13e2e217eee6aada77e0967bd35057cf97932eb65"
+    sha256 cellar: :any_skip_relocation, mojave: "3ca73186281c00d79bb3dfab3fa2b645e395425b4d72b872ed11ce8282829613"
   end
 
   depends_on "rust" => :build
