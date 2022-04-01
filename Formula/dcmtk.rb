923d94d7874c3645407d4de86d4a7582325dff98
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-01 13:31:17 +0800

    dcmtk: update 3.6.6 bottle.

--- Formula/dcmtk.rb
+++ Formula/dcmtk.rb
@@ -1,9 +1,20 @@
 class Dcmtk < Formula
   desc "OFFIS DICOM toolkit command-line utilities"
   homepage "https://dicom.offis.de/dcmtk.php.en"
-  url "https://dicom.offis.de/download/dcmtk/dcmtk366/dcmtk-3.6.6.tar.gz"
-  sha256 "6859c62b290ee55677093cccfd6029c04186d91cf99c7642ae43627387f3458e"
   head "https://git.dcmtk.org/dcmtk.git", branch: "master"
+
+  stable do
+    url "https://dicom.offis.de/download/dcmtk/dcmtk366/dcmtk-3.6.6.tar.gz"
+    sha256 "6859c62b290ee55677093cccfd6029c04186d91cf99c7642ae43627387f3458e"
+
+    # Fix build for Apple Silicon.
+    # Issue ref: https://support.dcmtk.org/redmine/issues/957
+    # TODO: Remove in the next release along with stable block
+    patch do
+      url "https://git.dcmtk.org/?p=dcmtk.git;a=patch;h=5fba853b6f7c13b02bed28bd9f7d3f450e4c72bb"
+      sha256 "40ca9e6f377951e2d24a509a6c95b9e572224d74d694f0d648b8b33e4d67e285"
+    end
+  end
 
   livecheck do
     url "https://dicom.offis.de/download/dcmtk/release/"
@@ -12,10 +23,9 @@ class Dcmtk < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dcmtk"
-    rebuild 1
-    sha256 mojave: "e765684abfa76fa8adaab5716a4145b3b6f71e08b4877697a05e03de67b9beed"
+    rebuild 2
+    sha256 mojave: "11441202e1a89aee7b851e3bd2d6ce51a0ee7e376db468b8ec6f0925ee4a2b46"
   end
-
 
   depends_on "cmake" => :build
   depends_on "libpng"
@@ -25,14 +35,18 @@ class Dcmtk < Formula
   uses_from_macos "libxml2"
 
   def install
-    mkdir "build" do
-      system "cmake", "-DBUILD_SHARED_LIBS=OFF", *std_cmake_args, ".."
-      system "make", "install"
-      system "cmake", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args, ".."
-      system "make", "install"
+    system "cmake", "-S", ".", "-B", "build/shared", *std_cmake_args,
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DCMAKE_INSTALL_RPATH=#{rpath}"
+    system "cmake", "--build", "build/shared"
+    system "cmake", "--install", "build/shared"
 
-      inreplace lib/"cmake/dcmtk/DCMTKConfig.cmake", Superenv.shims_path, ""
-    end
+    system "cmake", "-S", ".", "-B", "build/static", *std_cmake_args,
+                    "-DBUILD_SHARED_LIBS=OFF"
+    system "cmake", "--build", "build/static"
+    lib.install Dir["build/static/lib/*.a"]
+
+    inreplace lib/"cmake/dcmtk/DCMTKConfig.cmake", "#{Superenv.shims_path}/", ""
   end
 
   test do
