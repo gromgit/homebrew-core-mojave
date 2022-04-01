bd5bfc94a9832a9b02d5be59cda3748c088b57e9
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-01 22:19:09 +0800

    cloud-nuke: update 0.11.3 bottle.

--- Formula/cloud-nuke.rb
+++ Formula/cloud-nuke.rb
@@ -1,14 +1,14 @@
 class CloudNuke < Formula
   desc "CLI tool to nuke (delete) cloud resources"
   homepage "https://gruntwork.io/"
-  url "https://github.com/gruntwork-io/cloud-nuke/archive/v0.10.0.tar.gz"
-  sha256 "e84805a35b247959ffe84a521747dc74c4439bea34228fdb5cd48cc32b50e314"
+  url "https://github.com/gruntwork-io/cloud-nuke/archive/v0.11.3.tar.gz"
+  sha256 "071c612c63282fbc0f1b5ab6dfd4cacefc8b29cd1501fb5063a43df704e04a89"
   license "MIT"
   head "https://github.com/gruntwork-io/cloud-nuke.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cloud-nuke"
-    sha256 cellar: :any_skip_relocation, mojave: "b04b4f04232e67e79bb12d5971e7671b174fb19d0d6d5792e2535881c52c9573"
+    sha256 cellar: :any_skip_relocation, mojave: "20f834a539b5e4c1f208849a12699abc790c234b2f090d59f402a6305a804499"
   end
 
   depends_on "go" => :build
