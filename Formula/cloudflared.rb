9edac4f6637363528040ce298685de5d6959677e
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-01 22:49:03 +0800

    cloudflared: update 2022.3.4 bottle.

---
+++ Formula/cloudflared.rb
@@ -0,0 +1,33 @@
+class Cloudflared < Formula
+  desc "Cloudflare Tunnel client (formerly Argo Tunnel)"
+  homepage "https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/tunnel-guide"
+  url "https://github.com/cloudflare/cloudflared/archive/refs/tags/2022.3.4.tar.gz"
+  sha256 "e810ccc855cc25e8a2f33b42d33570edf97856b227baf4ca0def8e47a4b8d387"
+  license "Apache-2.0"
+  head "https://github.com/cloudflare/cloudflared.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cloudflared"
+    sha256 cellar: :any_skip_relocation, mojave: "0e49586315d6c1fc9fc44962b7eaef4ed508b89e0726a313ae2eaa8aadfb18af"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "install",
+      "VERSION=#{version}",
+      "DATE=#{time.iso8601}",
+      "PACKAGE_MANAGER=#{tap.user}",
+      "PREFIX=#{prefix}"
+  end
+
+  test do
+    help_output = shell_output("#{bin}/cloudflared help")
+    assert_match "cloudflared - Cloudflare's command-line tool and agent", help_output
+    assert_match version.to_s, help_output
+    assert_equal "unable to find config file\n", shell_output("#{bin}/cloudflared 2>&1", 1)
+    assert_match "Error locating origin cert", shell_output("#{bin}/cloudflared tunnel run abcd 2>&1", 1)
+    assert_match "cloudflared was installed by #{tap.user}. Please update using the same method.",
+      shell_output("#{bin}/cloudflared update 2>&1")
+  end
+end
