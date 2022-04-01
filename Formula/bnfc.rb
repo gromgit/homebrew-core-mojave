e026740679daea3e322150d0b20dd8bc238ff064
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-01 15:07:16 +0800

    bnfc: update 2.9.4 bottle.

--- Formula/bnfc.rb
+++ Formula/bnfc.rb
@@ -1,30 +1,24 @@
 class Bnfc < Formula
   desc "BNF Converter"
   homepage "https://bnfc.digitalgrammars.com/"
-  url "https://github.com/BNFC/bnfc/archive/v2.9.1.tar.gz"
-  sha256 "d79125168636fdcf0acd64a9b83ea620c311b16dcada0848d8a577aa8aeddec4"
+  url "https://github.com/BNFC/bnfc/archive/v2.9.4.tar.gz"
+  sha256 "0c2ebee6ff3f7603f650f22fbb9836919761a130994b934d6ab03a5f29583254"
   license "BSD-3-Clause"
   head "https://github.com/BNFC/bnfc.git", branch: "master"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ecadd82438160dd4d36fcc6a10a1f88b0caf9695142770a4986e9616300964a0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "582a5d263f6ac1420e1f4a749fdefad35aad9bbf960f97f57a1c16dfd1c29e4e"
-    sha256 cellar: :any_skip_relocation, monterey:       "66b6637f6e3b968e152f3b84e4ddbe50fc6d8a7bba3ce9f5e3d51ef87a16c8f2"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0f3c56a6f814bc0625000b7341ffdb6e6b9fbc60f9e9bef4819214e836b9abd8"
-    sha256 cellar: :any_skip_relocation, catalina:       "33421a59619bb8911362221d25dc6b4b857be514a63795df2ea0bd297eb5bc8a"
-    sha256 cellar: :any_skip_relocation, mojave:         "dced427e3ffcc7bdffe354025d2e19d45f31c87e8e687d243cd1505108dadc24"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3716b9a49f6a62197755414097a0af56ffe4bfda0a2fbbf898ea4d921af1b534"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bnfc"
+    sha256 cellar: :any_skip_relocation, mojave: "23f2a0f8ef8b40587baa018914b29778def81ee6381851a0fb3a5149eac13a9c"
   end
 
   depends_on "cabal-install" => [:build, :test]
   depends_on "ghc" => [:build, :test]
   depends_on "sphinx-doc" => :build
+  depends_on "agda" => :test
   depends_on "antlr" => :test
+  depends_on "bison" => :test
+  depends_on "flex" => :test
   depends_on "openjdk" => :test
-
-  uses_from_macos "bison" => :test
-  uses_from_macos "flex" => :test
 
   def install
     cd "source" do
@@ -45,6 +39,11 @@ class Bnfc < Formula
 
   test do
     ENV.prepend_create_path "PATH", testpath/"tools-bin"
+    system "cabal", "v2-update"
+    system "cabal", "v2-install",
+           "--jobs=#{ENV.make_jobs}", "--max-backjumps=100000",
+           "--install-method=copy", "--installdir=#{testpath/"tools-bin"}",
+           "alex", "happy"
 
     (testpath/"calc.cf").write <<~EOS
       EAdd. Exp  ::= Exp  "+" Exp1 ;
@@ -56,13 +55,11 @@ class Bnfc < Formula
       entrypoints Exp ;
       comment "(#" "#)" ;
     EOS
+    system bin/"bnfc", "--check", testpath/"calc.cf"
+
     (testpath/"test.calc").write "14 * (# Parsing is fun! #) (3 + 2 / 5 - 8)"
-    treespace = if build.head?
-      " "
-    else
-      ""
-    end
-    check_out = <<~EOS
+    space = " "
+    check_out_c = <<~EOS
 
       Parse Successful!
 
@@ -70,47 +67,66 @@ class Bnfc < Formula
       (EMul (EInt 14) (ESub (EAdd (EInt 3) (EDiv (EInt 2) (EInt 5))) (EInt 8)))
 
       [Linearized Tree]
-      14 * (3 + 2 / 5 - 8) #{treespace}
+      14 * (3 + 2 / 5 - 8)#{space}
 
+    EOS
+    check_out_hs = <<~EOS
+      #{testpath/"test.calc"}
+
+      Parse Successful!
+
+      [Abstract Syntax]
+
+      EMul (Just (1,1)) (EInt (Just (1,1)) 14) (ESub (Just (1,29)) (EAdd (Just (1,29)) (EInt (Just (1,29)) 3) (EDiv (Just (1,33)) (EInt (Just (1,33)) 2) (EInt (Just (1,37)) 5))) (EInt (Just (1,41)) 8))
+
+      [Linearized tree]
+
+      14 * (3 + 2 / 5 - 8)
+    EOS
+    check_out_agda = <<~EOS
+      PARSE SUCCESSFUL
+
+      14 * (3 + 2 / 5 - 8)
+    EOS
+    check_out_java = <<~EOS
+
+      Parse Succesful!
+
+      [Abstract Syntax]
+
+      (EMul (EInt 14) (ESub (EAdd (EInt 3) (EDiv (EInt 2) (EInt 5))) (EInt 8)))#{space}
+
+      [Linearized Tree]
+
+      14 * (3 + 2 / 5 - 8)
     EOS
 
     mktemp "c-test" do
       system bin/"bnfc", "-m", "-o.", "--c", testpath/"calc.cf"
-      system "make", "CC=#{ENV.cc}", "CCFLAGS=#{ENV.cflags}"
+      system "make", "CC=#{ENV.cc}", "CCFLAGS=#{ENV.cflags}",
+             "FLEX=#{Formula["flex"].bin/"flex"}",
+             "BISON=#{Formula["bison"].bin/"bison"}"
       test_out = shell_output("./Testcalc #{testpath}/test.calc")
-      assert_equal check_out, test_out
+      assert_equal check_out_c, test_out
     end
 
     mktemp "cxx-test" do
       system bin/"bnfc", "-m", "-o.", "--cpp", testpath/"calc.cf"
-      system "make", "CC=#{ENV.cxx}", "CCFLAGS=#{ENV.cxxflags}"
+      system "make", "CC=#{ENV.cxx}", "CCFLAGS=#{ENV.cxxflags}",
+             "FLEX=#{Formula["flex"].bin/"flex"}",
+             "BISON=#{Formula["bison"].bin/"bison"}"
       test_out = shell_output("./Testcalc #{testpath}/test.calc")
-      assert_equal check_out, test_out
+      assert_equal check_out_c, test_out
     end
 
-    mktemp "haskell-test" do
-      system "cabal", "v2-update"
-      system "cabal", "v2-install",
-             "--jobs=#{ENV.make_jobs}", "--max-backjumps=100000",
-             "--install-method=copy", "--installdir=#{testpath/"tools-bin"}",
-             "alex", "happy"
-      system bin/"bnfc", "-m", "-o.", "--haskell", "--ghc", "-d", testpath/"calc.cf"
+    mktemp "agda-test" do
+      system bin/"bnfc", "-m", "-o.", "--haskell", "--text-token",
+             "--generic", "--functor", "--agda", "-d", testpath/"calc.cf"
       system "make"
-      test_out = shell_output("./Calc/Test #{testpath/"test.calc"}")
-      check_out_hs = <<~EOS
-        #{testpath/"test.calc"}
-
-        Parse Successful!
-
-        [Abstract Syntax]
-
-        EMul (EInt 14) (ESub (EAdd (EInt 3) (EDiv (EInt 2) (EInt 5))) (EInt 8))
-
-        [Linearized tree]
-
-        14 * (3 + 2 / 5 - 8)
-      EOS
+      test_out = shell_output("./Calc/Test #{testpath/"test.calc"}") # Haskell
       assert_equal check_out_hs, test_out
+      test_out = shell_output("./Main #{testpath/"test.calc"}") # Agda
+      assert_equal check_out_agda, test_out
     end
 
     mktemp "java-test" do
@@ -120,22 +136,10 @@ class Bnfc < Formula
       antlr_jar = Dir[Formula["antlr"].prefix/"antlr-*-complete.jar"][0]
       ENV["CLASSPATH"] = ".:#{antlr_jar}"
       system bin/"bnfc", "-m", "-o.", "--java", "--antlr4", testpath/"calc.cf"
-      system "make", "JAVAC=#{jdk_dir/"javac"}", "JAVA=#{jdk_dir/"java"}", "LEXER=#{antlr_bin}", "PARSER=#{antlr_bin}"
+      system "make", "JAVAC=#{jdk_dir/"javac"}", "JAVA=#{jdk_dir/"java"}",
+             "LEXER=#{antlr_bin}", "PARSER=#{antlr_bin}"
       test_out = shell_output("#{jdk_dir}/java calc.Test #{testpath}/test.calc")
-      space = " "
-      check_out_j = <<~EOS
-
-        Parse Succesful!
-
-        [Abstract Syntax]
-
-        (EMul (EInt 14) (ESub (EAdd (EInt 3) (EDiv (EInt 2) (EInt 5))) (EInt 8)))#{space}
-
-        [Linearized Tree]
-
-        14 * (3 + 2 / 5 - 8)
-      EOS
-      assert_equal check_out_j, test_out
+      assert_equal check_out_java, test_out
     end
   end
 end
