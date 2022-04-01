b7854204e751c1f22f9255ceb301cb9ba5f3b9bc
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-02 01:02:29 +0800

    cosign: update 1.6.0 bottle.

--- Formula/cosign.rb
+++ Formula/cosign.rb
@@ -2,24 +2,23 @@ class Cosign < Formula
   desc "Container Signing"
   homepage "https://github.com/sigstore/cosign"
   url "https://github.com/sigstore/cosign.git",
-      tag:      "v1.5.2",
-      revision: "8ffcd1228c463e1ad26ccce68ae16deeca2960b4"
+      tag:      "v1.6.0",
+      revision: "4b2c3c0c8ee97f31b9dac3859b40e0a48b8648ee"
   license "Apache-2.0"
   head "https://github.com/sigstore/cosign.git", branch: "main"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cosign"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "135ec8ca7c64dbb627ac1a60171c5e563bde8e736476bce2c758ac7f0eb322c6"
+    sha256 cellar: :any_skip_relocation, mojave: "856ed0bc54c5029404aa487dfa6808a0ec8bdbe022973f5f7442cb58b47270d7"
   end
 
   depends_on "go" => :build
 
   def install
-    pkg = "github.com/sigstore/cosign/pkg/version"
+    pkg = "sigs.k8s.io/release-utils/version"
     ldflags = %W[
       -s -w
-      -X #{pkg}.GitVersion=#{version}
+      -X #{pkg}.gitVersion=#{version}
       -X #{pkg}.gitCommit=#{Utils.git_head}
       -X #{pkg}.gitTreeState="clean"
       -X #{pkg}.buildDate=#{time.iso8601}
@@ -33,6 +32,6 @@ bottle do
       pipe_output("#{bin}/cosign generate-key-pair 2>&1", "foo\nfoo\n")
     assert_predicate testpath/"cosign.pub", :exist?
 
-    assert_match version.to_s, shell_output(bin/"cosign version")
+    assert_match version.to_s, shell_output(bin/"cosign version 2>&1")
   end
 end
