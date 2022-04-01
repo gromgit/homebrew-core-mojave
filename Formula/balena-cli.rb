fe2c9323f290aceb935874d04dd27e53b12d0e6b
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-01 12:38:10 +0800

    balena-cli: update 13.3.0 bottle.

--- Formula/balena-cli.rb
+++ Formula/balena-cli.rb
@@ -4,8 +4,8 @@ class BalenaCli < Formula
   desc "Command-line tool for interacting with the balenaCloud and balena API"
   homepage "https://www.balena.io/docs/reference/cli/"
   # balena-cli should only be updated every 10 releases on multiples of 10
-  url "https://registry.npmjs.org/balena-cli/-/balena-cli-12.25.0.tgz"
-  sha256 "af78b891de5492e567d731dee62369b3d1247c262acf94d2bf4e2379790a947e"
+  url "https://registry.npmjs.org/balena-cli/-/balena-cli-13.3.0.tgz"
+  sha256 "4dd72d585071869354d9fd69753d2ee94f8620d75930da1b1664e7986e93ae04"
   license "Apache-2.0"
 
   livecheck do
@@ -14,16 +14,48 @@ class BalenaCli < Formula
   end
 
   bottle do
-    sha256 catalina:    "1d68785ff12ddc57c8a6e93a0b83d3613e29ccf0ae8331dc8197a06bd1f47336"
-    sha256 mojave:      "c2e8432c28bb7875f42d7815befa98001dbc72a65cffc121fb8dd6bf2d75290e"
-    sha256 high_sierra: "0e670f3cc482565ddc1a166316407fab2fd008f12128a06f7f73c3bcf07a641d"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/balena-cli"
+    sha256 mojave: "67315ae28c1f5e0333b0cd5d9a6756e58fc624c0e6915639988040a6e00909a2"
   end
 
-  depends_on "node"
+  depends_on "node@14"
+
+  on_macos do
+    depends_on "macos-term-size"
+  end
 
   def install
+    ENV.deparallelize
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    (bin/"balena").write_env_script libexec/"bin/balena", PATH: "#{Formula["node@14"].opt_bin}:$PATH"
+
+    # Remove incompatible pre-built binaries
+    os = OS.kernel_name.downcase
+    arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+    node_modules = libexec/"lib/node_modules/balena-cli/node_modules"
+    node_modules.glob("{ffi-napi,ref-napi}/prebuilds/*")
+                .each { |dir| dir.rmtree if dir.basename.to_s != "#{os}-#{arch}" }
+
+    term_size_vendor_dir = node_modules/"term-size/vendor"
+    term_size_vendor_dir.rmtree # remove pre-built binaries
+
+    if OS.mac?
+      macos_dir = term_size_vendor_dir/"macos"
+      macos_dir.mkpath
+      # Replace the vendored pre-built term-size with one we build ourselves
+      ln_sf (Formula["macos-term-size"].opt_bin/"term-size").relative_path_from(macos_dir), macos_dir
+
+      unless Hardware::CPU.intel?
+        # Replace pre-built x86_64 binaries with native binaries
+        %w[denymount macmount].each do |mod|
+          (node_modules/mod/"bin"/mod).unlink
+          system "make", "-C", node_modules/mod
+        end
+      end
+    end
+
+    # Replace universal binaries with their native slices.
+    deuniversalize_machos
   end
 
   test do
