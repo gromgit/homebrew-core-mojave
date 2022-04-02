668e0139ef47e7df9037ad154d4b63ba9648ca8f
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-02 13:19:45 +0800

    dsq: update 0.10.0 bottle.

--- Formula/dsq.rb
+++ Formula/dsq.rb
@@ -1,14 +1,14 @@
 class Dsq < Formula
   desc "CLI tool for running SQL queries against JSON, CSV, Excel, Parquet, and more"
   homepage "https://github.com/multiprocessio/dsq"
-  url "https://github.com/multiprocessio/dsq/archive/refs/tags/0.5.0.tar.gz"
-  sha256 "bc28ecb1cc78c9446d67d9ec3e4333691802adcf3bbfd430ba3c548c5415af40"
+  url "https://github.com/multiprocessio/dsq/archive/refs/tags/0.10.0.tar.gz"
+  sha256 "a5cd99c32d5ffa63736aa272455a0a5172e1820f9275f053e2de80fcae7a67e1"
   license "Apache-2.0"
   head "https://github.com/multiprocessio/dsq.git", branch: "main"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dsq"
-    sha256 cellar: :any_skip_relocation, mojave: "c779ed2ee878cb4ee7add148aadaa1bce8ebbe58cbc786fe792a92a88fdd11bb"
+    sha256 cellar: :any_skip_relocation, mojave: "199e38945c151d4675e3b3dacf685c8b306e2bfba74ee8d1fdeef098e8698c28"
   end
 
   depends_on "go" => :build
