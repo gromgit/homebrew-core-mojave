46a012932a98efa970384de240c7a26b2aa8d485
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-02 14:57:57 +0800

    elan-init: update 1.4.0 bottle.

--- Formula/elan-init.rb
+++ Formula/elan-init.rb
@@ -1,15 +1,14 @@
 class ElanInit < Formula
   desc "Lean Theorem Prover installer and version manager"
   homepage "https://github.com/leanprover/elan"
-  url "https://github.com/leanprover/elan/archive/v1.3.1.tar.gz"
-  sha256 "8e1380a1cb20cec54f07e30519ad7bc0179d9e3d68d33c02d0476248322b5015"
+  url "https://github.com/leanprover/elan/archive/v1.4.0.tar.gz"
+  sha256 "dee855cf0382e07daf8cd20f0d70ba05e803b047fd7dfa3a1b5f496cbeb1b877"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/leanprover/elan.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/elan-init"
-    rebuild 3
-    sha256 cellar: :any_skip_relocation, mojave: "da44043f70ce22e442cc48f7f8ae81ca08f9e4ce7d4963535929be4ae994da97"
+    sha256 cellar: :any_skip_relocation, mojave: "88735f4d28ce9e0a00033b82257f22232851814ea1c65d2cbf5ae7eb295cb507"
   end
 
   depends_on "rust" => :build
