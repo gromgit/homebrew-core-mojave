97db768d0c8be2e433087dba1aeaa0bf3bb232e8
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-02 20:44:20 +0800

    flarectl: update 0.35.1 bottle.

--- Formula/flarectl.rb
+++ Formula/flarectl.rb
@@ -1,14 +1,14 @@
 class Flarectl < Formula
   desc "CLI application for interacting with a Cloudflare account"
   homepage "https://github.com/cloudflare/cloudflare-go/tree/master/cmd/flarectl"
-  url "https://github.com/cloudflare/cloudflare-go/archive/v0.34.0.tar.gz"
-  sha256 "4ec0a0f1d27086a5c62fcb4ae8c4d914ae2904ef71e8269aaf0ebd35b498988a"
+  url "https://github.com/cloudflare/cloudflare-go/archive/v0.35.1.tar.gz"
+  sha256 "8d37113e9faf1ba63666fd6cb33b14a66a96eb0f0f354f9f15faac8c94942305"
   license "BSD-3-Clause"
   head "https://github.com/cloudflare/cloudflare-go.git", branch: "master"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/flarectl"
-    sha256 cellar: :any_skip_relocation, mojave: "0b0fc767fa73a225c06819fc50294f574e8ceb2e6333c036a28356221b593486"
+    sha256 cellar: :any_skip_relocation, mojave: "e1578a301f4dbc46bd7f3712fb0a0aa40e9c3cdb87bc30defc0963660e7b46e6"
   end
 
   depends_on "go" => :build
