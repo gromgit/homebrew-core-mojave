faa9b51bc00069e32c6e85ab85c6a9e5c57f20b7
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-02 21:19:45 +0800

    fluent-bit: update 1.9.1 bottle.

--- Formula/fluent-bit.rb
+++ Formula/fluent-bit.rb
@@ -1,8 +1,8 @@
 class FluentBit < Formula
   desc "Fast and Lightweight Logs and Metrics processor"
   homepage "https://github.com/fluent/fluent-bit"
-  url "https://github.com/fluent/fluent-bit/archive/v1.8.12.tar.gz"
-  sha256 "50f2a4e9eb6c2a2df58b78ef562e23c1ef9871c389c7bd0320f6b381d2b491c8"
+  url "https://github.com/fluent/fluent-bit/archive/v1.9.1.tar.gz"
+  sha256 "b0c06f8cc7e5571d9768efe56e59d9aa7efec04c797fd18a3268406973a5b72d"
   license "Apache-2.0"
   head "https://github.com/fluent/fluent-bit.git", branch: "master"
 
@@ -13,7 +13,7 @@ class FluentBit < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fluent-bit"
-    sha256 cellar: :any, mojave: "5c587aa10126f62ed36aca294f1d736817fb944755c511ad5863824dbc40248d"
+    sha256 cellar: :any, mojave: "722394001ac25483f64040a6069ccde3275ca453ae7ef812f7c78f87e6aed27f"
   end
 
   depends_on "bison" => :build
@@ -24,110 +24,26 @@ class FluentBit < Formula
     depends_on "openssl@1.1"
   end
 
-  # Apply https://github.com/fluent/fluent-bit/pull/3564 to build on M1
-  patch do
-    url "https://github.com/fluent/fluent-bit/commit/fcdf304e5abc3e3b66b1acac76dbaf23b2d22579.patch?full_index=1"
-    sha256 "80d1b0b6916ff1e0c157e6824afa769f08e28e764f65bfd28df0900d6f9bda1e"
-  end
-
-  # Fix error: use of undeclared identifier 'clock_serv_t'
-  #
-  # Also: don't install any service script for Linux
-  patch :DATA
-
   def install
+    # Prevent fluent-bit to install files into global init system
+    #
+    # For more information see https://github.com/fluent/fluent-bit/issues/3393
+    inreplace "src/CMakeLists.txt", "if(IS_DIRECTORY /lib/systemd/system)", "if(False)"
+    inreplace "src/CMakeLists.txt", "elseif(IS_DIRECTORY /usr/share/upstart)", "elif(False)"
+
     chdir "build" do
       # Per https://luajit.org/install.html: If MACOSX_DEPLOYMENT_TARGET
       # is not set then it's forced to 10.4, which breaks compile on Mojave.
       # fluent-bit builds against a vendored Luajit.
       ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version
 
-      system "cmake", "..", "-DWITH_IN_MEM=OFF", *std_cmake_args
+      system "cmake", "..", *std_cmake_args
       system "make", "install"
     end
   end
 
   test do
     output = shell_output("#{bin}/fluent-bit -V").chomp
-    assert_equal "Fluent Bit v#{version}", output
+    assert_match "Fluent Bit v#{version}", output
   end
 end
-
-__END__
-diff --git a/lib/cmetrics/src/cmt_time.c b/lib/cmetrics/src/cmt_time.c
-index 0671542a..67f1c368 100644
---- a/lib/cmetrics/src/cmt_time.c
-+++ b/lib/cmetrics/src/cmt_time.c
-@@ -20,7 +20,7 @@
- #include <cmetrics/cmt_info.h>
-
- /* MacOS */
--#ifdef FLB_HAVE_CLOCK_GET_TIME
-+#ifdef CMT_HAVE_CLOCK_GET_TIME
- #include <mach/clock.h>
- #include <mach/mach.h>
- #endif
-@@ -41,8 +41,8 @@ uint64_t cmt_time_now()
-     mach_timespec_t mts;
-     host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-     clock_get_time(cclock, &mts);
--    tm->tv_sec = mts.tv_sec;
--    tm->tv_nsec = mts.tv_nsec;
-+    tm.tv_sec = mts.tv_sec;
-+    tm.tv_nsec = mts.tv_nsec;
-     mach_port_deallocate(mach_task_self(), cclock);
- #else /* __STDC_VERSION__ */
-     clock_gettime(CLOCK_REALTIME, &tm);
-diff --git a/src/CMakeLists.txt b/src/CMakeLists.txt
-index f6654506..fe117172 100644
---- a/src/CMakeLists.txt
-+++ b/src/CMakeLists.txt
-@@ -434,27 +434,6 @@ if(FLB_BINARY)
-       DESTINATION "${FLB_INSTALL_BINDIR}")
-   endif()
-
--  # Detect init system, install upstart, systemd or init.d script
--  if(IS_DIRECTORY /lib/systemd/system)
--    set(FLB_SYSTEMD_SCRIPT "${PROJECT_SOURCE_DIR}/init/${FLB_OUT_NAME}.service")
--    configure_file(
--      "${PROJECT_SOURCE_DIR}/init/systemd.in"
--      ${FLB_SYSTEMD_SCRIPT}
--      )
--    install(FILES ${FLB_SYSTEMD_SCRIPT} COMPONENT binary DESTINATION /lib/systemd/system)
--    install(DIRECTORY DESTINATION ${FLB_INSTALL_CONFDIR} COMPONENT binary)
--  elseif(IS_DIRECTORY /usr/share/upstart)
--    set(FLB_UPSTART_SCRIPT "${PROJECT_SOURCE_DIR}/init/${FLB_OUT_NAME}.conf")
--    configure_file(
--      "${PROJECT_SOURCE_DIR}/init/upstart.in"
--      ${FLB_UPSTART_SCRIPT}
--      )
--    install(FILES ${FLB_UPSTART_SCRIPT} COMPONENT binary DESTINATION /etc/init)
--    install(DIRECTORY DESTINATION COMPONENT binary ${FLB_INSTALL_CONFDIR})
--  else()
--    # FIXME: should we support Sysv init script ?
--  endif()
--
-   install(FILES
-     "${PROJECT_SOURCE_DIR}/conf/fluent-bit.conf"
-     DESTINATION ${FLB_INSTALL_CONFDIR}
-diff --git a/src/flb_time.c b/src/flb_time.c
-index 9f3d5964..bdc89d7e 100644
---- a/src/flb_time.c
-+++ b/src/flb_time.c
-@@ -52,13 +52,13 @@ static int _flb_time_get(struct flb_time *tm)
- #elif defined FLB_HAVE_TIMESPEC_GET
-     /* C11 supported! */
-     return timespec_get(&tm->tm, TIME_UTC);
--#elif defined FLB_CLOCK_GET_TIME
-+#elif defined FLB_HAVE_CLOCK_GET_TIME
-     clock_serv_t cclock;
-     mach_timespec_t mts;
-     host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-     clock_get_time(cclock, &mts);
--    tm->tv_sec = mts.tv_sec;
--    tm->tv_nsec = mts.tv_nsec;
-+    tm->tm.tv_sec = mts.tv_sec;
-+    tm->tm.tv_nsec = mts.tv_nsec;
-     return mach_port_deallocate(mach_task_self(), cclock);
- #else /* __STDC_VERSION__ */
-     return clock_gettime(CLOCK_REALTIME, &tm->tm);
