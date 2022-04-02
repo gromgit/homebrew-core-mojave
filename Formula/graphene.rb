7dee326462663a3117d0b1dac3cfa58f4c14bb7d
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-02 14:32:44 +0800

    graphene: update 1.10.8 bottle.

--- Formula/graphene.rb
+++ Formula/graphene.rb
@@ -1,18 +1,13 @@
 class Graphene < Formula
   desc "Thin layer of graphic data types"
   homepage "https://ebassi.github.io/graphene/"
-  url "https://github.com/ebassi/graphene/releases/download/1.10.6/graphene-1.10.6.tar.xz"
-  sha256 "80ae57723e4608e6875626a88aaa6f56dd25df75024bd16e9d77e718c3560b25"
+  url "https://github.com/ebassi/graphene/archive/refs/tags/1.10.8.tar.gz"
+  sha256 "922dc109d2dc5dc56617a29bd716c79dd84db31721a8493a13a5f79109a4a4ed"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "75db915e9b0cb0c4c567912e1d8cf2e78655f136f672429c0eabe56dbd23e147"
-    sha256 cellar: :any,                 arm64_big_sur:  "64f77c528bc4e693aef3a747f76eb2c63a1a25f37e730d46fda139c002271301"
-    sha256 cellar: :any,                 monterey:       "02d923cb6c5358ae637c0f455b850b8ad7a2d97f2d8993cba979171013f942c6"
-    sha256 cellar: :any,                 big_sur:        "20b41dfc4c7bf01973d14f33129db71d7968509e8dc0761f640e36400ae8127e"
-    sha256 cellar: :any,                 catalina:       "9a39689fd7d593fc8d5b86b077d153d863c51b470703e87dfd8cfd1ee157d742"
-    sha256 cellar: :any,                 mojave:         "d6e6d695f0b7c04b6b8b0b09a18a9cd39bd25ba0e64d5843fa12c0a56100ea06"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6345068270ee59e4b48206f62bed5d258f361aa273f7a20c3847d23daa67b46c"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/graphene"
+    sha256 cellar: :any, mojave: "216ec7bbeb003bcab2fb73b31c784e1f10aabf61c30425f19601c69c1c134bc1"
   end
 
   depends_on "gobject-introspection" => :build
