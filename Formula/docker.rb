a5883ee20e49185265619a6ce9218fdb13097849
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-02 12:28:30 +0800

    docker: update 20.10.13 bottle.

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -2,8 +2,8 @@ class Docker < Formula
   desc "Pack, ship and run any application as a lightweight container"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/cli.git",
-      tag:      "v20.10.12",
-      revision: "e91ed5707e038b02af3b5120fa0835c5bedfd42e"
+      tag:      "v20.10.13",
+      revision: "a224086349269551becacce16e5842ceeb2a98d6"
   license "Apache-2.0"
   head "https://github.com/docker/cli.git", branch: "master"
 
@@ -14,10 +14,8 @@ class Docker < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/docker"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "4d2858ba998374beb74f6c534fa37df30e36ad12ab9662b70a6a0050be8a660a"
+    sha256 cellar: :any_skip_relocation, mojave: "d19cfd6139737b4e80042c45507e6b7d7e24e546e852083f058a30ebfe90f184"
   end
-
 
   depends_on "go" => :build
   depends_on "go-md2man" => :build
@@ -51,13 +49,11 @@ class Docker < Formula
   test do
     assert_match "Docker version #{version}", shell_output("#{bin}/docker --version")
 
-    on_macos do
-      assert_match "ERROR: Cannot connect to the Docker daemon", shell_output("#{bin}/docker info", 1)
+    expected = if OS.mac?
+      "ERROR: Cannot connect to the Docker daemon"
+    else
+      "ERROR: Got permission denied while trying to connect to the Docker daemon socket"
     end
-
-    on_linux do
-      assert_match "ERROR: Got permission denied while trying to connect to the Docker daemon socket",
-        shell_output("#{bin}/docker info", 1)
-    end
+    assert_match expected, shell_output("#{bin}/docker info", 1)
   end
 end
