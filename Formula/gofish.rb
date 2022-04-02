87e6ca43d0e2de697cec6f4ae5009b35462e3f5c
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-03 04:11:22 +0800

    gofish: update 0.15.1 bottle.

--- Formula/gofish.rb
+++ Formula/gofish.rb
@@ -2,13 +2,13 @@ class Gofish < Formula
   desc "Cross-platform systems package manager"
   homepage "https://gofi.sh"
   url "https://github.com/fishworks/gofish.git",
-      tag:      "v0.15.0",
-      revision: "691768d1a77f2adb3e57271cddf6fea31cc6dadf"
+      tag:      "v0.15.1",
+      revision: "5d14f73963cfc0c226e8b06c0f5c3404d2ec2e77"
   license "Apache-2.0"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gofish"
-    sha256 cellar: :any_skip_relocation, mojave: "b9a02c3fb3ce8fec94c197b9cf57ea59407424cf4900eb7048dafacb5013be9c"
+    sha256 cellar: :any_skip_relocation, mojave: "d5c99b3276240c2222608fb01e03bf861fc2f8b70df6e45197cc91e546237928"
   end
 
   depends_on "go" => :build
