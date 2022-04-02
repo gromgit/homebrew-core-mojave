f98690c64e144243ae1bfdd7d6cecf216ca04076
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-02 14:14:56 +0800

    easeprobe: update 1.1.0 bottle.

---
+++ Formula/easeprobe.rb
@@ -0,0 +1,46 @@
+class Easeprobe < Formula
+  desc "Simple, standalone, and lightWeight tool that can do health/status checking"
+  homepage "https://github.com/megaease/easeprobe"
+  url "https://github.com/megaease/easeprobe.git",
+      tag:      "v1.1.0",
+      revision: "593a5b714a9fe247c0c38e013dd72598590f0092"
+  license "Apache-2.0"
+  head "https://github.com/megaease/easeprobe.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/easeprobe"
+    sha256 cellar: :any_skip_relocation, mojave: "7690b91fd8115b3060b5a7dc5fbcd632e7b281fe402149d29dca83e3eea4c405"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/megaease/easeprobe/pkg/version.RELEASE=#{version}
+      -X github.com/megaease/easeprobe/pkg/version.COMMIT=#{Utils.git_head}
+      -X github.com/megaease/easeprobe/pkg/version.REPO=megaease/easeprobe
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/easeprobe"
+  end
+
+  test do
+    (testpath/"config.yml").write <<~EOS.chomp
+      http:
+        - name: "brew.sh"
+          url: "https://brew.sh"
+    EOS
+
+    easeprobe_stdout = (testpath/"easeprobe.log")
+
+    pid = fork do
+      $stdout.reopen(easeprobe_stdout)
+      exec bin/"easeprobe", "-f", testpath/"config.yml"
+    end
+    sleep 2
+    assert_match "Ready to monitor(http): brew.sh", easeprobe_stdout.read
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
