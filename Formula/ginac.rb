a9d36bf41fbe429b68122b1ea7a614743499a385
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-03 01:53:48 +0800

    ginac: update 1.8.3 bottle.

--- Formula/ginac.rb
+++ Formula/ginac.rb
@@ -1,8 +1,8 @@
 class Ginac < Formula
   desc "Not a Computer algebra system"
   homepage "https://www.ginac.de/"
-  url "https://www.ginac.de/ginac-1.8.2.tar.bz2"
-  sha256 "bfcd751abcaf8afddb83958c2ad22763a75ea24032553e503ee9b38e3ea3b6c3"
+  url "https://www.ginac.de/ginac-1.8.3.tar.bz2"
+  sha256 "77c71a586adf6fc0b5dab573434f30cff1579153cd77c6eba02292e178f7a490"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -12,19 +12,13 @@ class Ginac < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ginac"
-    sha256 cellar: :any, mojave: "5ddb014ea35489865cb25f4ebf8d432e8fbe4fd2902e214ed92c62f29aa471fa"
+    sha256 cellar: :any, mojave: "857faa8acce08b60156a59285f4412cde5d3cf16e4c468700c5e470e12d19026"
   end
 
   depends_on "pkg-config" => :build
   depends_on "cln"
   depends_on "python@3.10"
   depends_on "readline"
-
-  # Fix -flat_namespace being used on Big Sur and later.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-  end
 
   def install
     system "./configure", "--disable-dependency-tracking",
