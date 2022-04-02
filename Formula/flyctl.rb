4ab48ca50dc991f9bbf3e77e34091a0e5a64f473
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-02 21:48:57 +0800

    flyctl: update 0.0.310 bottle.

---
+++ Formula/flyctl.rb
@@ -0,0 +1,44 @@
+class Flyctl < Formula
+  desc "Command-line tools for fly.io services"
+  homepage "https://fly.io"
+  url "https://github.com/superfly/flyctl.git",
+      tag:      "v0.0.310",
+      revision: "51d0e489e7abba2ca5ba9579c032045b4278519c"
+  license "Apache-2.0"
+  head "https://github.com/superfly/flyctl.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/flyctl"
+    sha256 cellar: :any_skip_relocation, mojave: "2cb7d8614ad18afe2f24f6ef9b969e7aa2dbedc279417bfab1002fe9f7d9b2cc"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = %W[
+      -s -w
+      -X github.com/superfly/flyctl/internal/buildinfo.environment=production
+      -X github.com/superfly/flyctl/internal/buildinfo.buildDate=#{time.iso8601}
+      -X github.com/superfly/flyctl/internal/buildinfo.version=#{version}
+      -X github.com/superfly/flyctl/internal/buildinfo.commit=#{Utils.git_short_head}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
+
+    bin.install_symlink "flyctl" => "fly"
+
+    bash_output = Utils.safe_popen_read("#{bin}/flyctl", "completion", "bash")
+    (bash_completion/"flyctl").write bash_output
+    zsh_output = Utils.safe_popen_read("#{bin}/flyctl", "completion", "zsh")
+    (zsh_completion/"_flyctl").write zsh_output
+    fish_output = Utils.safe_popen_read("#{bin}/flyctl", "completion", "fish")
+    (fish_completion/"flyctl.fish").write fish_output
+  end
+
+  test do
+    assert_match "flyctl v#{version}", shell_output("#{bin}/flyctl version")
+
+    flyctl_status = shell_output("flyctl status 2>&1", 1)
+    assert_match "Error No access token available. Please login with 'flyctl auth login'", flyctl_status
+  end
+end
