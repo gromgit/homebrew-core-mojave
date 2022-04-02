a776cdbec8e1c9294fb47d748901d4d529e8cfbe
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-02 20:01:20 +0800

    fetchmail: update 6.4.29 bottle.

--- Formula/fetchmail.rb
+++ Formula/fetchmail.rb
@@ -1,8 +1,8 @@
 class Fetchmail < Formula
   desc "Client for fetching mail from POP, IMAP, ETRN or ODMR-capable servers"
   homepage "https://www.fetchmail.info/"
-  url "https://downloads.sourceforge.net/project/fetchmail/branch_6.4/fetchmail-6.4.27.tar.xz"
-  sha256 "9e64f9e71f798cf1fe2278b84e2f5880b806527c0c0206925c086ccd179113dc"
+  url "https://downloads.sourceforge.net/project/fetchmail/branch_6.4/fetchmail-6.4.29.tar.xz"
+  sha256 "397df984082abae51edec6831700d68636f0e117cfaffcbdd3eed66d04b97321"
   license all_of: [
     "LGPL-2.1-or-later",
     "ISC",
@@ -18,7 +18,7 @@ class Fetchmail < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fetchmail"
-    sha256 mojave: "a79279151395ddd4c5d2aa68837b51a7aec41064f17cbd5675efccdffba830e3"
+    sha256 mojave: "f9ce917ff6859ee3ef62e472331126d05edb128c32314278596458ca6c0f6146"
   end
 
   depends_on "openssl@1.1"
