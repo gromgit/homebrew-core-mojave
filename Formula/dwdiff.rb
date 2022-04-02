274a46c040d2bf98b2ff12ad44a7c7d3f36534a8
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-02 14:10:16 +0800

    dwdiff: update 2.1.4_3 bottle.

--- Formula/dwdiff.rb
+++ Formula/dwdiff.rb
@@ -4,7 +4,7 @@ class Dwdiff < Formula
   url "https://os.ghalkes.nl/dist/dwdiff-2.1.4.tar.bz2"
   sha256 "df16fec44dcb467d65a4246a43628f93741996c1773e930b90c6dde22dd58e0a"
   license "GPL-3.0-only"
-  revision 2
+  revision 3
 
   livecheck do
     url "https://os.ghalkes.nl/dist/"
@@ -12,13 +12,8 @@ class Dwdiff < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "d9d5288ce21cf9c002d8a0c57a0c88f1a8bb97af0c038f4a728d58c46ab84a46"
-    sha256 arm64_big_sur:  "04a95671f514c40ef0998a8d3304aa98b50f302ea8f6068bc93a7804db1e77e0"
-    sha256 monterey:       "6819f3f8f6b9586cda1ef6de1993196219a49179aaff34e670b036d8e21ba6fb"
-    sha256 big_sur:        "c4527f38e48df252ac20b4bc6ebc7c24baf1c3f8f157c16a5218db5e17cbf22e"
-    sha256 catalina:       "05fc8c8568063207dfb1d52149367cb2bbc960e41fecd09b63790e6b372ceb99"
-    sha256 mojave:         "ee28eff81ca945f765afe6a62110e603d40108ac112c3db3ec6b859438fdbda3"
-    sha256 x86_64_linux:   "bb0d5aa169f1d00fdabe5bf26b4769b3cbdfc288b4c1dbeaf1592d6b9d09d7a4"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dwdiff"
+    sha256 mojave: "a92cc1c3063bfb86c50d89d104c3315687c73287f21a727d92d172ded36a6d30"
   end
 
   depends_on "pkg-config" => :build
