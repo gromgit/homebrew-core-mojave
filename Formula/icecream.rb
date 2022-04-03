55e38f30ba57a02f1e50f250501da85f534da773
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-03 23:57:28 +0800

    icecream: update 1.4 bottle.

--- Formula/icecream.rb
+++ Formula/icecream.rb
@@ -1,31 +1,25 @@
 class Icecream < Formula
   desc "Distributed compiler with a central scheduler to share build load"
   homepage "https://en.opensuse.org/Icecream"
-  url "https://github.com/icecc/icecream/archive/1.3.1.tar.gz"
-  sha256 "9f45510fb2251d818baebcff19051c1cf059e48c6b830fb064a8379480159b9d"
-  license "GPL-2.0"
+  url "https://github.com/icecc/icecream/archive/1.4.tar.gz"
+  sha256 "249dcf74f0fc477ff9735ff0bdcdfaa4c257a864c4db5255d8b25c9f4fd20b6b"
+  license "GPL-2.0-or-later"
 
   bottle do
-    sha256 arm64_monterey: "6b5da13231142d20828ebd59f265652015450bc61406b0cf15777014cada7ff2"
-    sha256 arm64_big_sur:  "649b19fac1c726b6890cd7c106bef0d4b9b065847cb2d40aed20db0467db64f5"
-    sha256 monterey:       "6393eb4f02f21856e3a5197fbd5a7b4ba625ec0b8821e2a13aafb898b5493fee"
-    sha256 big_sur:        "b8a350fa7f37c1c4c6de9d5570d029ae58566b97e4d598b6a3faf7f02b6e81fb"
-    sha256 catalina:       "666f827a6a686e6d2e81dc1d0eb5aae8374f01d7d1524ef6c695e3bf207c4af5"
-    sha256 mojave:         "fb94b2d8e763469a2b0112523f89496f4a81e22ed9b7290f4280178f726853da"
-    sha256 high_sierra:    "6cc11bcddd969e9aeb7e83692e9714d5891f0530bacbc1c52b019b298bce3d24"
-    sha256 x86_64_linux:   "e0c5e8d4fd1804af9afc4425698ea01cc5a9a18303185eb7d19cdbe2b96546a7"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/icecream"
+    sha256 mojave: "651d9034ce6bfe00b411d98320e10bff049e421f6d3ded1e30a29ceb822d1acd"
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "docbook2x" => :build
   depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
   depends_on "libarchive"
   depends_on "lzo"
   depends_on "zstd"
 
   on_linux do
-    depends_on "pkg-config" => :build
     depends_on "llvm" => :test
     depends_on "libcap-ng"
   end
