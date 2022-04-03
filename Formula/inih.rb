6c80ad1774e696df4f47a1f3aacb6d4a4c7319e7
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 02:16:59 +0800

    inih: update 55 bottle.

---
+++ Formula/inih.rb
@@ -0,0 +1,83 @@
+class Inih < Formula
+  desc "Simple .INI file parser in C"
+  homepage "https://github.com/benhoyt/inih"
+  url "https://github.com/benhoyt/inih/archive/refs/tags/r55.tar.gz"
+  sha256 "ba55f8ae2a8caf0653f30f48567241e14ea916acfc13481f502d8a9c8f507f68"
+  license "BSD-3-Clause"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/inih"
+    sha256 cellar: :any, mojave: "824e30be028231faad839b8566fc7d0b98dd154744652ed44623d33757cd3ff6"
+  end
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+
+  def install
+    mkdir "build" do
+      system "meson", *std_meson_args, ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      #include <string.h>
+      #include <ini.h>
+
+      static int dumper(void* user, const char* section, const char* name, const char* value) {
+          static char prev_section[50] = "";
+          if (strcmp(section, prev_section)) {
+              printf("%s[%s]\\n", (prev_section[0] ? "\\n" : ""), section);
+              strncpy(prev_section, section, sizeof(prev_section));
+              prev_section[sizeof(prev_section) - 1] = '\\0';
+          }
+          printf("%s = %s\\n", name, value);
+          return 1;
+      }
+
+      int main(int argc, char* argv[]) {
+          if (argc <= 1) {
+              return 1;
+          }
+
+          int error = ini_parse(argv[1], dumper, NULL);
+          if (error < 0) {
+              printf("Can't read '%s'!\\n", argv[1]);
+              return 2;
+          } else if (error) {
+              printf("Bad config file (first error on line %d)!\\n", error);
+              return 3;
+          }
+          return 0;
+      }
+    EOS
+
+    (testpath/"test.ini").write <<~EOS
+      [protocol]             ; Protocol configuration
+      version=6              ; IPv6
+
+      [user]
+      name = Bob Smith       ; Spaces around '=' are stripped
+      email = bob@smith.com  ; And comments (like this) ignored
+      active = true          ; Test a boolean
+      pi = 3.14159           ; Test a floating point number
+    EOS
+
+    expected = <<~EOS
+      [protocol]
+      version = 6
+
+      [user]
+      name = Bob Smith
+      email = bob@smith.com
+      active = true
+      pi = 3.14159
+    EOS
+
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-linih", "-o", "test"
+    assert_equal expected, shell_output("./test test.ini")
+  end
+end
