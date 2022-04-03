aa23ca418f94e9d07941fd45f91baf446b6b92fb
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-03 20:35:56 +0800

    highlight: update 4.2 bottle.

--- Formula/highlight.rb
+++ Formula/highlight.rb
@@ -1,10 +1,10 @@
 class Highlight < Formula
   desc "Convert source code to formatted text with syntax highlighting"
   homepage "http://www.andre-simon.de/doku/highlight/en/highlight.php"
-  url "http://www.andre-simon.de/zip/highlight-4.1.tar.bz2"
-  sha256 "3a4b6aa55b9837ea217f78e1f52bb294dbf3aaf4ccf8a5553cf859be4fbf3907"
+  url "http://www.andre-simon.de/zip/highlight-4.2.tar.bz2"
+  sha256 "ed3efdb9b416b236e503989f9dfebdd94bf515536cfd183aefe36cefdd0d0468"
   license "GPL-3.0-or-later"
-  head "https://gitlab.com/saalen/highlight.git"
+  head "https://gitlab.com/saalen/highlight.git", branch: "master"
 
   livecheck do
     url "http://www.andre-simon.de/zip/download.php"
@@ -12,13 +12,8 @@ class Highlight < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "6727bd0348ec08e9855fd4574cf8a564d94cba5c3195f1bb9852f9733c351f08"
-    sha256 arm64_big_sur:  "90c97137575b00aaa9050fb14f049e6b1dcfe236ae4266d7e684706f09a58f0f"
-    sha256 monterey:       "4186adee8552304558aa2fd7af70db3355b92650564324b6cd02770bd22d4887"
-    sha256 big_sur:        "e364d145b581f0622acf1cc9476142b8e318efa60495ca6e9c1f44ed560a2d66"
-    sha256 catalina:       "4238b72def77283c7cedf385639bd9b28b5d0e3fb5d5c0bc6ec6fe6d06ae7bcf"
-    sha256 mojave:         "81057a5898f59793eda32340275cb8bf87da61e6330bb5d0e925690c678b606b"
-    sha256 x86_64_linux:   "f6f656122700a511ed946ca5a3d4db0e05db5c6f4f988567e866dfc50938a406"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/highlight"
+    sha256 mojave: "f0365d28eac429511c09b5289970b5fc22806977a9fe20aa3b04462a124f88cc"
   end
 
   depends_on "boost" => :build
