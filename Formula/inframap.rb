9f0454f8091adf5de1cb2c4bf30ca3f5fe4e30f8
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 02:15:07 +0800

    inframap: update 0.6.7 bottle.

--- Formula/inframap.rb
+++ Formula/inframap.rb
@@ -4,16 +4,12 @@ class Inframap < Formula
   url "https://github.com/cycloidio/inframap/archive/v0.6.7.tar.gz"
   sha256 "e9d6daa48c6fa1a8ecc5437c7121cb5072eb81c29c88ca9e6d778637c8442332"
   license "MIT"
-  head "https://github.com/cycloidio/inframap.git"
+  head "https://github.com/cycloidio/inframap.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "763af27a8d3dbb2ccb3c7bcb63d337ee96e7f5c011dcecbee7768d76214814e4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ee096fc9fe0909cc34675dbd7a7269c8092ffee0d6ba71fa333e75afc4ff28c1"
-    sha256 cellar: :any_skip_relocation, monterey:       "36b5b88b60390bc015fb56166cb7b890c8e5eb2f736a30384dc3fa92cda9f351"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0a4fe8ae6b7fcf419ab21ea31beb6b90dbeedd73ba2a771672e41c7a30ff4d0d"
-    sha256 cellar: :any_skip_relocation, catalina:       "ba85244090ace0d6a94dbb41c9693f11647cb8e949acc5511c8d9c25a3f74c1b"
-    sha256 cellar: :any_skip_relocation, mojave:         "d7bb58c695d390162b9a5376decf76acf02929d9b4421137ccbb7e91f940bdd2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d4d90d38dfbdddf34bdeea89493f6c76874392166e777d42f6d477690cb880c6"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/inframap"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "a2bec3d6d608a98565f84ca866312a2906e33e834481f4909107f4d548a4f7b1"
   end
 
   depends_on "go" => :build
