5536b0479df08faf2aceae8286a02a1b7cdffb60
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 23:12:03 +0800

    lastpass-cli: update 1.3.3_1 bottle.

--- Formula/lastpass-cli.rb
+++ Formula/lastpass-cli.rb
@@ -5,18 +5,12 @@ class LastpassCli < Formula
   sha256 "b94f591627e06c9fed3bc38007b1adc6ea77127e17c7175c85d497096768671b"
   license "GPL-2.0"
   revision 1
-  head "https://github.com/lastpass/lastpass-cli.git"
+  head "https://github.com/lastpass/lastpass-cli.git", branch: "master"
 
   bottle do
-    rebuild 4
-    sha256 cellar: :any,                 arm64_monterey: "98b073cafd20bdc9379fdd937b3f86729400bbf89e2a10f67145541d7f87089e"
-    sha256 cellar: :any,                 arm64_big_sur:  "1bccfb715b94c569d943c9ce5833f74628397abf1311f74c49d6e5d4b25b847b"
-    sha256 cellar: :any,                 monterey:       "42a83a59fe0da598dbfb09cde49d4d66845b9137f5ca102cb25f3d8cc01965b8"
-    sha256 cellar: :any,                 big_sur:        "10f9224c8bfebae0cf12df72e6144ba3a309956b1efcce574975cd21cec930c5"
-    sha256 cellar: :any,                 catalina:       "8643f81d13a40ef8b86efe83fbee1b41b22c492b7725bebab83dcb2d253fd603"
-    sha256 cellar: :any,                 mojave:         "f1b7c42dd889f597ef06f0bd72bb1b273c21dc91e5f3e313da8599254954a7ae"
-    sha256 cellar: :any,                 high_sierra:    "62629472aafb7e4927d8ab5e9d7189c913e3249a172d0445ffe7eda31b642eb7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d3ea59e2bc30ccd28e1f516be98d1f170fc0fa7ff333938212879d44dfc84477"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lastpass-cli"
+    rebuild 5
+    sha256 cellar: :any, mojave: "7a86d5b4f02b59aff84294f2c263a3bc6132a74c17cb2b09e8f00f7bb613b591"
   end
 
   depends_on "asciidoc" => :build
