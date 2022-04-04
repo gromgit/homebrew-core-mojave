abd0a6372db1a5189aab00727341c3f5420019ce
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 15:45:51 +0800

    kahip: update 3.14 bottle.

--- Formula/kahip.rb
+++ Formula/kahip.rb
@@ -4,11 +4,12 @@ class Kahip < Formula
   url "https://github.com/KaHIP/KaHIP/archive/v3.14.tar.gz"
   sha256 "9da04f3b0ea53b50eae670d6014ff54c0df2cb40f6679b2f6a96840c1217f242"
   license "MIT"
-  head "https://github.com/KaHIP/KaHIP.git"
+  head "https://github.com/KaHIP/KaHIP.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kahip"
-    sha256 cellar: :any, mojave: "b41980d03f13dd8f5cf33ce5ddaed4148733bd03a8ae5498603963608f128362"
+    rebuild 1
+    sha256 cellar: :any, mojave: "ea75ab011ff0e756692a0332d26aa95cf3746922c9c0644f0e84ad258258fe23"
   end
 
   depends_on "cmake" => :build
