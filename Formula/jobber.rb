4159ac6e912905db29ee0a859f2fbe362ff77a4a
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 11:54:05 +0800

    jobber: update 1.4.4 bottle.

--- Formula/jobber.rb
+++ Formula/jobber.rb
@@ -4,17 +4,12 @@ class Jobber < Formula
   url "https://github.com/dshearer/jobber/archive/v1.4.4.tar.gz"
   sha256 "fd88a217a413c5218316664fab5510ace941f4fdb68dcb5428385ff09c68dcc2"
   license "MIT"
-  head "https://github.com/dshearer/jobber.git"
+  head "https://github.com/dshearer/jobber.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 arm64_monterey: "14087e07df78fd1e53fa44cc873df0db56eee9b0c89154161ee1a4c617c8ae9d"
-    sha256 arm64_big_sur:  "c751dfdc4e8a2336eb4441dde62d3fc83d8ca869fe95e4804cecb99112551361"
-    sha256 monterey:       "d54b324e8914c637f54418851308b825241d2c3142c8c13e9a6316ff31ab6e99"
-    sha256 big_sur:        "669af998fd35ba85849f725ba8360cffbadfba87a8bd5f7adc43aa3a830caba5"
-    sha256 catalina:       "993170495768a40b7f86927bfc14a66397b9109c3d9520815727f0123409b1e0"
-    sha256 mojave:         "3767f3c9fa38a4ad1d8df745f8e5451bef3fea39e0f758a081e414f7d87feafa"
-    sha256 x86_64_linux:   "e8d9630a84c0fce0514498c0379961df72d461ad3eb2d82847b66ea34732188c"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jobber"
+    rebuild 2
+    sha256 mojave: "a7a01989707954ce3734f6c0ccdb5ecca52b22daebf4d66fd6f5c165e7f02bc1"
   end
 
   depends_on "go" => :build
