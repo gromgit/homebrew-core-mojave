12b850a8188fd21d9734e8dcbb8fa2fe3454a60e
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 22:52:53 +0800

    lapack: update 3.10.0 bottle.

--- Formula/lapack.rb
+++ Formula/lapack.rb
@@ -4,16 +4,12 @@ class Lapack < Formula
   url "https://github.com/Reference-LAPACK/lapack/archive/v3.10.0.tar.gz"
   sha256 "328c1bea493a32cac5257d84157dc686cc3ab0b004e2bea22044e0a59f6f8a19"
   license "BSD-3-Clause"
-  head "https://github.com/Reference-LAPACK/lapack.git"
+  head "https://github.com/Reference-LAPACK/lapack.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "95407dca1870630e2a99175bd55210f151dc1db7e264a1ff396f79965e6bdc3d"
-    sha256 cellar: :any,                 arm64_big_sur:  "57b25ebfd66edca32e16acf1f7127af22bb1ea43a1f7b758895789696fe0590e"
-    sha256 cellar: :any,                 monterey:       "ce6223d15a12cf1535460a3a6a59cd822ed5f44ca417f94a5bf29ed46931897e"
-    sha256 cellar: :any,                 big_sur:        "3b57e303806b0fa8cb17738b10b3bd2b4801ef898fc5433af05b90cab9dddf40"
-    sha256 cellar: :any,                 catalina:       "cbdfdaa3de046ff377bf0e6a974541016a0790c0ddba295eecdb4615f8ec5923"
-    sha256 cellar: :any,                 mojave:         "619839fc1623b36c0b7bf8903e424c003bbd6ef96a0f9f7ed1ff684f231c54ec"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7f1c1dcfcf68dd35f484bba3c43c7e070dcd4da9a03fad5c50a408a00d26e217"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lapack"
+    rebuild 1
+    sha256 cellar: :any, mojave: "df1503dc5babefd877177bc4af3af06c57f44e6bbe7585d81ffbf47ea69df799"
   end
 
   keg_only :shadowed_by_macos, "macOS provides LAPACK in Accelerate.framework"
