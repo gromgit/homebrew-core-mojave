55b56d8655eae1a5e6883f6c893e57379402cfae
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 23:29:07 +0800

    lbdb: update 0.50 bottle.

--- Formula/lbdb.rb
+++ Formula/lbdb.rb
@@ -1,8 +1,8 @@
 class Lbdb < Formula
   desc "Little brother's database for the mutt mail reader"
   homepage "https://www.spinnaker.de/lbdb/"
-  url "https://www.spinnaker.de/lbdb/download/lbdb_0.49.1.tar.gz"
-  sha256 "e2a57a2935d52ae5fbcc76d84ee06955d943c4696d0b43c7c1de5b8bc7f0f31e"
+  url "https://www.spinnaker.de/lbdb/download/lbdb-0.50.tar.gz"
+  sha256 "afac83d8a4e33732007af70debf71a702db256213998e2efb313bb9bb17b81b0"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -11,13 +11,8 @@ class Lbdb < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e087de084cff07eae814fc954fa74a74d73b0f06d0f91129977112d6558ecbac"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6c8daad11d118ab04bb0209f95c9c828392a5b260eaee63dbe73a7b5f30b8ba8"
-    sha256 cellar: :any_skip_relocation, monterey:       "41cdd85f7474a49f24cee7c69a7d5108c4ea24fb766674112415eb1b42659a0a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d95cc4ac68303be926a5c9ca8a4a17bbc545f3ce02ce345ecebe95a613ed5484"
-    sha256 cellar: :any_skip_relocation, catalina:       "3eafefa5553d8660c6d315f15f6bb963b7e21673b485ad681a2b8e2c3c9aeb13"
-    sha256 cellar: :any_skip_relocation, mojave:         "d8292aef28eecf34b970aba54f473dfa88cd5c64b9415002e1a62ae1e2dc393c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "482715f9ab4a754a6425a34380b0388f51eff44c73b45ed1bec356566173050a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lbdb"
+    sha256 cellar: :any_skip_relocation, mojave: "4eb4819fa53e6927691cfd97e38250d9d6c1868c962703d5964090eb8633f8d8"
   end
 
   depends_on "abook"
