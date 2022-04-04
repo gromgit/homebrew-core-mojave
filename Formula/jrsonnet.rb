7a5691bbba8717f7bc3088383b08e15de703d53a
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 13:04:11 +0800

    jrsonnet: update 0.4.2 bottle.

--- Formula/jrsonnet.rb
+++ Formula/jrsonnet.rb
@@ -4,7 +4,7 @@ class Jrsonnet < Formula
   url "https://github.com/CertainLach/jrsonnet/archive/refs/tags/v0.4.2.tar.gz"
   sha256 "2396c57a49a20db99da17b8ddd1b0b283f1a6e7c5ae1dc94823e7503cbb6ce3f"
   license "MIT"
-  head "https://github.com/CertainLach/jrsonnet.git"
+  head "https://github.com/CertainLach/jrsonnet.git", branch: "master"
 
   livecheck do
     url :stable
@@ -12,13 +12,9 @@ class Jrsonnet < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fcb5b5d99df55c43c70b475ea291ee1d614b76e705be867d688f17bb22ac9842"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "583e666fb6a076a89b1d447ad9220d30409cc62e931c1f4c9dd99cfee9291252"
-    sha256 cellar: :any_skip_relocation, monterey:       "3e79d3068b3c2ca7442c13fc57d8ddd85b0cbb8844c78b8a3c971f5873e3a3a6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "419f0a50acd6a0cce0abbfe2131d18bdf964fe0d09a095c2c92e10b8b1fb04af"
-    sha256 cellar: :any_skip_relocation, catalina:       "08a4bac487db7433903275f05ba23d11a8a8865590a23a868aa105f49416b650"
-    sha256 cellar: :any_skip_relocation, mojave:         "d927e34108112f33bfa9bb6d004c3540d983ff789d6e5969a4fc9aa8cba92a99"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ed7a1bfdfe53b056dbd4cb5433fc26a47b82dde02a913a34870fc6c365a8885a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jrsonnet"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "f058e04c0a989d1aa33197726c32da6775290722c05e4c28816cb83974fd37aa"
   end
 
   depends_on "rust" => :build
