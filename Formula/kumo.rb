e51c71b1101941e4bed1b4b5c0d78e3108ee0241
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 21:46:15 +0800

    kumo: update 1.28 bottle.

--- Formula/kumo.rb
+++ Formula/kumo.rb
@@ -5,9 +5,15 @@ class Kumo < Formula
   sha256 "43e4e2ea9da62a2230deed9151d8484f80bd6ae5fef304eaadf3301378f45fb6"
   license "MIT"
 
+  livecheck do
+    url "https://search.maven.org/remotecontent?filepath=com/kennycason/kumo-cli/maven-metadata.xml"
+    regex(%r{<version>v?(\d+(?:\.\d+)+)</version>}i)
+  end
+
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "596af3ddc1bc9b8881fbc386ae85804fe999cee118a99fc13e123db2c5acefe1"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kumo"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, mojave: "fc1014ecdf0979351407385d7bf0817b34326b0612bc27d6a062e52741e0e7e8"
   end
 
   depends_on "openjdk"
