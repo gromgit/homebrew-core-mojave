d60371e87dbf16e45ed0ab9c20327d7a3f072a21
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 16:55:55 +0800

    kepubify: update 4.0.4 bottle.

--- Formula/kepubify.rb
+++ Formula/kepubify.rb
@@ -1,14 +1,14 @@
 class Kepubify < Formula
   desc "Convert ebooks from epub to kepub"
   homepage "https://pgaskin.net/kepubify/"
-  url "https://github.com/pgaskin/kepubify/archive/v4.0.2.tar.gz"
-  sha256 "f6bf7065ec99e48766f60a126590e021f5bd4fac19754ecb2d90eaf106f4e39b"
+  url "https://github.com/pgaskin/kepubify/archive/v4.0.4.tar.gz"
+  sha256 "a3bf118a8e871b989358cb598746efd6ff4e304cba02fd2960fe35404a586ed5"
   license "MIT"
-  head "https://github.com/pgaskin/kepubify.git"
+  head "https://github.com/pgaskin/kepubify.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kepubify"
-    sha256 cellar: :any_skip_relocation, mojave: "4a56eab3076231d28e70c600e7890d9b27633421011ee90b9046b576b131359c"
+    sha256 cellar: :any_skip_relocation, mojave: "306b30a777aa9cb3f21627d0c14d98367a8c96ab53de6e7ae3209b961abd13a3"
   end
 
   depends_on "go" => :build
