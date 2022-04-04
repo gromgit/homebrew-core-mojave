cebdf92cd8dacb48744e785e4f3e6291c99f07fe
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 02:01:02 +0800

    libdivecomputer: update 0.7.0 bottle.

--- Formula/libdivecomputer.rb
+++ Formula/libdivecomputer.rb
@@ -4,14 +4,17 @@ class Libdivecomputer < Formula
   url "https://www.libdivecomputer.org/releases/libdivecomputer-0.7.0.tar.gz"
   sha256 "80d9f194ea24502039df98598482e0afc6b0e333de79db34c29b2d68934d25b9"
   license "LGPL-2.1-or-later"
-  head "https://github.com/libdivecomputer/libdivecomputer.git"
+  head "https://github.com/libdivecomputer/libdivecomputer.git", branch: "master"
+
+  livecheck do
+    url "https://www.libdivecomputer.org/releases/"
+    regex(/href=.*?libdivecomputer[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "c5f918bf0cf0716949639440453e31eb929a918d5328fb1d4dd50ad6f6a497a5"
-    sha256 cellar: :any,                 big_sur:       "80a648490411d90cee0ae9bbafbc91e48e6ee1d4b449bfad5795cd375b5337d0"
-    sha256 cellar: :any,                 catalina:      "b0e1c5af39a3a474f72a89b669acfd82628f91aabe21c719e2eee78dc0099950"
-    sha256 cellar: :any,                 mojave:        "79ea433c1787070be036fa259b1cb7bd673d5c787ac937291361711d967708d5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f3a241ae2fd6589976993f2ac0fea3114a1131514f6bafcace961a53c28658da"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libdivecomputer"
+    rebuild 1
+    sha256 cellar: :any, mojave: "1627f2f875e4b5dd11acaebc65a857b8b5a56364c0b6d8b669ca1f84fa962d40"
   end
 
   depends_on "autoconf" => :build
