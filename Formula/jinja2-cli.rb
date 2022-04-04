334b6bd72e8b94293463e6caaa6d7f395ec09a38
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 11:45:20 +0800

    jinja2-cli: update 0.8.2 bottle.

--- Formula/jinja2-cli.rb
+++ Formula/jinja2-cli.rb
@@ -3,25 +3,25 @@ class Jinja2Cli < Formula
 
   desc "CLI for the Jinja2 templating language"
   homepage "https://github.com/mattrobenolt/jinja2-cli"
-  url "https://files.pythonhosted.org/packages/0c/df/c16c1757b0cd37c282be4f7bb2addcdf3514272d180ae2ed290a5d2472cd/jinja2-cli-0.8.1.tar.gz"
-  sha256 "fb1173811ed5b54205c65131374f342fcb924a5123af53a65fe1ffa7eb40bf19"
+  url "https://files.pythonhosted.org/packages/a4/22/c922839761b311b72ccc95c2ca2239311a3e80916458878962626f96922a/jinja2-cli-0.8.2.tar.gz"
+  sha256 "a16bb1454111128e206f568c95938cdef5b5a139929378f72bb8cf6179e18e50"
   license "BSD-2-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jinja2-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "e2672fa02b81f822b4620c2a4d8a6b5785ad7b3efd4a3ac80a625a43c0890b7d"
+    sha256 cellar: :any_skip_relocation, mojave: "1218a393cc8fbd71ee37d2556082424696c27c01cd0428a949806544f17978e5"
   end
 
   depends_on "python@3.10"
 
   resource "Jinja2" do
-    url "https://files.pythonhosted.org/packages/91/a5/429efc6246119e1e3fbf562c00187d04e83e54619249eb732bb423efa6c6/Jinja2-3.0.3.tar.gz"
-    sha256 "611bb273cd68f3b993fabdc4064fc858c5b47a973cb5aa7999ec1ba405c87cd7"
+    url "https://files.pythonhosted.org/packages/1f/93/99a76d0fa1a8fc14442a6421abee323f8d15964979336eabd2a1834e9118/Jinja2-3.1.0.tar.gz"
+    sha256 "a2f09a92f358b96b5f6ca6ecb4502669c4acb55d8733bbb2b2c9c4af5564c605"
   end
 
   resource "MarkupSafe" do
-    url "https://files.pythonhosted.org/packages/bf/10/ff66fea6d1788c458663a84d88787bae15d45daa16f6b3ef33322a51fc7e/MarkupSafe-2.0.1.tar.gz"
-    sha256 "594c67807fb16238b30c44bdf74f36c02cdf22d1c8cda91ef8a0ed8dabf5620a"
+    url "https://files.pythonhosted.org/packages/1d/97/2288fe498044284f39ab8950703e88abbac2abbdf65524d576157af70556/MarkupSafe-2.1.1.tar.gz"
+    sha256 "7f91197cc9e48f989d12e4e6fbc46495c446636dfc81b9ccf50bb0ec74b91d4b"
   end
 
   def install
@@ -29,12 +29,12 @@ class Jinja2Cli < Formula
   end
 
   test do
-    on_macos do
-      assert_match version.to_s, shell_output("script -q /dev/null #{bin}/jinja2 --version")
+    output = if OS.mac?
+      shell_output("script -q /dev/null #{bin}/jinja2 --version")
+    else
+      shell_output("script -q /dev/null -e -c \"#{bin}/jinja2 --version\"")
     end
-    on_linux do
-      assert_match version.to_s, shell_output("script -q /dev/null -e -c \"#{bin}/jinja2 --version\"")
-    end
+    assert_match version.to_s, output
     expected_result = <<~EOS
       The Beatles:
       - Ringo Starr
