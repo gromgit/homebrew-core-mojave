92ff7d3a65e9f64d91a43ac94750f7f998313602
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 16:47:35 +0800

    kcov: update 40 bottle.

--- Formula/kcov.rb
+++ Formula/kcov.rb
@@ -1,13 +1,11 @@
 class Kcov < Formula
   desc "Code coverage tester for compiled programs, Python, and shell scripts"
   homepage "https://simonkagstrom.github.io/kcov/"
-  url "https://github.com/SimonKagstrom/kcov/archive/v39.tar.gz"
-  sha256 "1b538fe16acf5ffd886f1fc32e9e803a520d586666e5c90a0b8632f1459291eb"
-  license "GPL-2.0"
+  url "https://github.com/SimonKagstrom/kcov/archive/v40.tar.gz"
+  sha256 "6b1c11b066d57426d61375a31c3816f1fcd2610b447050c86d9920e22d5200b3"
+  license "GPL-3.0-or-later"
   head "https://github.com/SimonKagstrom/kcov.git", branch: "master"
 
-  # We check the Git tags because, as of writing, the "latest" release on GitHub
-  # is a prerelease version (`pre-v40`), so we can't rely on it being correct.
   livecheck do
     url :stable
     regex(/^v?(\d+(?:\.\d+)*)$/i)
@@ -15,9 +13,11 @@ class Kcov < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kcov"
-    rebuild 2
-    sha256 mojave: "97c6bb8e7988498e01cdb8fc1e131fbd3282645d516f6b19e3c058d4d0af1114"
+    sha256 mojave: "3edf6f2e32d0a83b46d93b05163df91b546e3f986ffb1697d1840b46552de8fb"
   end
+
+  # We check the Git tags because, as of writing, the "latest" release on GitHub
+  # is a prerelease version (`pre-v40`), so we can't rely on it being correct.
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
