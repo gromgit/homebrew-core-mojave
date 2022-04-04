81ebd8963ac8c918550f4be50ffc4329044f8750
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 20:13:45 +0800

    kotlin-language-server: update 1.3.0 bottle.

--- Formula/kotlin-language-server.rb
+++ Formula/kotlin-language-server.rb
@@ -1,13 +1,13 @@
 class KotlinLanguageServer < Formula
   desc "Intelligent Kotlin support for any editor/IDE using the Language Server Protocol"
   homepage "https://github.com/fwcd/kotlin-language-server"
-  url "https://github.com/fwcd/kotlin-language-server/archive/refs/tags/1.2.0.tar.gz"
-  sha256 "2121a678d9c6ae2c80a74f3036ccc18fb778fe1ef25a527b3083d24c7c1bbf75"
+  url "https://github.com/fwcd/kotlin-language-server/archive/refs/tags/1.3.0.tar.gz"
+  sha256 "679375e987aa7dea7dc8d291a1ebe729372dcd508065074c8b2d2663b6c776e6"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kotlin-language-server"
-    sha256 cellar: :any_skip_relocation, mojave: "0d4a3e7269dacda87e96c59aa6775441e335eabda769ed533349c194b4ca2475"
+    sha256 cellar: :any_skip_relocation, mojave: "ba096fa9b3589a1bcb93dffe565088cfe2e7617a7a86444f220e1fabc1b40efb"
   end
 
   depends_on "gradle" => :build
