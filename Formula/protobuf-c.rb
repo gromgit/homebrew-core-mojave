39539e28d2506318efd38e7b7886f3e886010132
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 19:25:10 +0800

    protobuf-c: update 1.4.0_1 bottle.

--- Formula/protobuf-c.rb
+++ Formula/protobuf-c.rb
@@ -4,15 +4,11 @@ class ProtobufC < Formula
   url "https://github.com/protobuf-c/protobuf-c/releases/download/v1.4.0/protobuf-c-1.4.0.tar.gz"
   sha256 "26d98ee9bf18a6eba0d3f855ddec31dbe857667d269bc0b6017335572f85bbcb"
   license "BSD-2-Clause"
+  revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "26abd73290938018b40214fbf1d76dba12f9fa5d96bcd51aa719e1793eb0c366"
-    sha256 cellar: :any,                 arm64_big_sur:  "1ca90f4286b3c7210aeb15ba9ce34c09972806c1477f40dbd2e7b3bafcdbb275"
-    sha256 cellar: :any,                 monterey:       "5948d96ab2e7476bfab2ca0fe18ae8bdb876ef06cff93664f6faae8735b250c0"
-    sha256 cellar: :any,                 big_sur:        "a79fd80a8a0fb8dd05a014cc34ac7281441c167659c0bf1ea36df7be8db3084b"
-    sha256 cellar: :any,                 catalina:       "4caae0df2e6727218460e8ef1a0cf18aa0bee6fd14841e6f3456fa325faf4326"
-    sha256 cellar: :any,                 mojave:         "96cf2cab8b8b7e5e8228fb5c6bf9bfdd34c7b54ab208eda056ace3648600777a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "adfaa9b5233ac90599f13933525f2e12fcaab3b1099aa8fc80eac7580c6b3ce5"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/protobuf-c"
+    sha256 cellar: :any, mojave: "3e078aab8a20fc7221e9ab015d271713ed7390e4354c76209a6cb135349d8c4a"
   end
 
   depends_on "pkg-config" => :build
