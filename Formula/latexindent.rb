6644fdc58be6c8b06dcfa34e62373ff5b243d2ed
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 23:17:24 +0800

    latexindent: update 3.17 bottle.

--- Formula/latexindent.rb
+++ Formula/latexindent.rb
@@ -1,27 +1,27 @@
 class Latexindent < Formula
   desc "Add indentation to LaTeX files"
   homepage "https://ctan.org/pkg/latexindent"
-  url "https://github.com/cmhughes/latexindent.pl/archive/V3.15.tar.gz"
-  sha256 "bffa75eb50b0c0cf719ea37945616b6329b9b1b1d44df37f650e0baaff709f3a"
+  url "https://github.com/cmhughes/latexindent.pl/archive/V3.17.tar.gz"
+  sha256 "e8ffdc07f2992d44e8ec9c8ac560c7f020fe9eca2e85fb00c654848c40b2431c"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/latexindent"
-    sha256 cellar: :any_skip_relocation, mojave: "603fe10b145cabc8d44cbc0967e480cb6ad7e6a9dc91009c6d471843130e1511"
+    sha256 cellar: :any_skip_relocation, mojave: "14655ec60b93975be8ac833237adc76118449789a3dacffe0d0415ae3792fcd3"
   end
 
   depends_on "perl"
 
   on_macos do
     resource "Mac::SystemDirectory" do
-      url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/Mac-SystemDirectory-0.13.tar.gz"
-      sha256 "8730740e4ff3ea4812139b0787dbd1b544e093a08218d908071629b70fde3684"
+      url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/Mac-SystemDirectory-0.14.tar.gz"
+      sha256 "b3c336fe20850042d30e1db1e8d191d3c056cc1072a472eb4e5bd7229056dee1"
     end
   end
 
   resource "B::Hooks::EndOfScope" do
-    url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/B-Hooks-EndOfScope-0.25.tar.gz"
-    sha256 "da1b6a9f7c7424776363182f9673e666b06136f13dc744241f7adce3d1ad0c1a"
+    url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/B-Hooks-EndOfScope-0.26.tar.gz"
+    sha256 "39df2f8c007a754672075f95b90797baebe97ada6d944b197a6352709cb30671"
   end
 
   resource "Class::Data::Inheritable" do
@@ -100,8 +100,8 @@ class Latexindent < Formula
   end
 
   resource "Package::Stash" do
-    url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/Package-Stash-0.39.tar.gz"
-    sha256 "9165f555112e080493ce0e9129de0886da30b2593fb353a2abd1c76b2d2621b5"
+    url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/Package-Stash-0.40.tar.gz"
+    sha256 "5a9722c6d9cb29ee133e5f7b08a5362762a0b5633ff5170642a5b0686e95e066"
   end
 
   resource "Package::Stash::XS" do
