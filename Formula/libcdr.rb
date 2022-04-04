953986102d1f55d5622642f9c230d421feb7874b
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 01:52:55 +0800

    libcdr: update 0.1.7_2 bottle.

--- Formula/libcdr.rb
+++ Formula/libcdr.rb
@@ -4,7 +4,7 @@ class Libcdr < Formula
   url "https://dev-www.libreoffice.org/src/libcdr/libcdr-0.1.7.tar.xz"
   sha256 "5666249d613466b9aa1e987ea4109c04365866e9277d80f6cd9663e86b8ecdd4"
   license "MPL-2.0"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://dev-www.libreoffice.org/src/"
@@ -12,13 +12,8 @@ class Libcdr < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "52c71577c0e2915f38ce1718e6ca9af2f466f14f834a3b32548cb4b549f8ff6c"
-    sha256 cellar: :any,                 arm64_big_sur:  "5ae923701714fad81fced4eb8c7eb2c171c3e675c685a0649b561004a587a03b"
-    sha256 cellar: :any,                 monterey:       "1982525feceb04d6555522d6198d6df1adc26e75d6cc26dca0bbee09306223de"
-    sha256 cellar: :any,                 big_sur:        "0c5c055abec9d36ae8beaaf07e0268cb5b4495ec3103b933b03ff2676d96d049"
-    sha256 cellar: :any,                 catalina:       "e79b945338269508e453ed4f8748d0a9b5e19304658621765c2eae54120d0537"
-    sha256 cellar: :any,                 mojave:         "42016c8a3b6e75ce702e84f2afdf3c328807b776b10e9c809772c0f69d569d1a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b6cf82a1af01f907eb5476947c63b6a9241c5d5d72291f3f2263c1e6c5b70cdf"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libcdr"
+    sha256 cellar: :any, mojave: "275db302a955df13f5c6faaca009b58c830f00f89012b7f01ba8c79df93dcbb4"
   end
 
   depends_on "cppunit" => :build
