c9df36af45469ee616bb9ee80717560b3dbba95f
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 17:27:42 +0800

    node@10: update 10.24.1_2 bottle.

--- Formula/node@10.rb
+++ Formula/node@10.rb
@@ -4,13 +4,11 @@ class NodeAT10 < Formula
   url "https://nodejs.org/dist/v10.24.1/node-v10.24.1.tar.xz"
   sha256 "d72fc2c244603b4668da94081dc4d6067d467fdfa026e06a274012f16600480c"
   license "MIT"
-  revision 1
+  revision 2
 
   bottle do
-    sha256 cellar: :any, monterey: "5714defcf3505e22bc5af68f26872b34b45c955db2d4a1701b080378c6b56166"
-    sha256 cellar: :any, big_sur:  "84095e53ee88c7acc39a574a64e7832bbb7326be76a4de502ed55fdd47e784c4"
-    sha256 cellar: :any, catalina: "be200f225fd88d1a4a1efba2470b305accbddff927bf7d5287eeb4777fb02b9c"
-    sha256 cellar: :any, mojave:   "cdaf548a304fb2eda428a61c8b25c406e2ada5f9eb7cf5b56174d0753f153598"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/node@10"
+    sha256 cellar: :any, mojave: "a7a60cb106ce21330d017b622c65f12bd939dd7166a5bed49d8b1043e7cc37af"
   end
 
   keg_only :versioned_formula
