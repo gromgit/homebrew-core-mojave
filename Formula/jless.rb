b2fe648e66c67c59b2a2639e5110838849680d17
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-04 11:52:01 +0800

    jless: update 0.8.0 bottle.

--- Formula/jless.rb
+++ Formula/jless.rb
@@ -1,17 +1,20 @@
 class Jless < Formula
   desc "Command-line pager for JSON data"
   homepage "https://jless.io/"
-  url "https://github.com/PaulJuliusMartinez/jless/archive/refs/tags/v0.7.2.tar.gz"
-  sha256 "5d776cb6488743ccdaeeffb4bfc54d84862028170cee852a8bb5c156526ed263"
+  url "https://github.com/PaulJuliusMartinez/jless/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "3f1168c9b2432f7f4fa9dd3c31b55e371ef9d6f822dc98a8cdce5318a112bf2d"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jless"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "c818e8d965f9c6c0335633103dc7437100fe24e19dee6de4ba7230d26fe7fa59"
+    sha256 cellar: :any_skip_relocation, mojave: "9460089037a7ba17bf1d1b5495c2c37a095f1a8e111f51a59e35455a585e4e1e"
   end
 
   depends_on "rust" => :build
+
+  on_linux do
+    depends_on "libxcb"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
