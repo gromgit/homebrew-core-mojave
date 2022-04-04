f5e65d1effb54c1f654e2894a641b10f967b3f61
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 02:32:16 +0800

    libgaiagraphics: update 0.5_8 bottle.

--- Formula/libgaiagraphics.rb
+++ Formula/libgaiagraphics.rb
@@ -11,14 +11,12 @@ class Libgaiagraphics < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "7c09f30c553254915cfa1652c7c653b6121a5c3ab58e6f326789eaa1d3a800f3"
-    sha256 cellar: :any,                 arm64_big_sur:  "6623ec87d81dc19a855c81549b00b07cd9a7f931ad40ba0cf2e8f9fcebb03b40"
-    sha256 cellar: :any,                 monterey:       "4659862840d0448395e80c6c20e44f33e385dac0731d98455c3e530bd408633b"
-    sha256 cellar: :any,                 big_sur:        "865d8050b42bc24609c47e17347c78b9cbdc0d9b585076931b58b94097076e16"
-    sha256 cellar: :any,                 catalina:       "7471281583cae58d19538fd2bb5bda8e251a6bee797c4c5191820b61537f4109"
-    sha256 cellar: :any,                 mojave:         "0c0a8eb90a920ef286d534f0ba81aea1e009c3abc6eaff86be25e906aa5795d9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9c7f5f80ad03372c1d69fe602c19a8d50080f797448a14d60ffe7db5a721e1dc"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libgaiagraphics"
+    rebuild 1
+    sha256 cellar: :any, mojave: "64b0fba78ce9138ab73b7303dde0c3cc19d6c7a48b7bed3b851c34811162cec9"
   end
+
+  deprecate! date: "2022-03-05", because: :deprecated_upstream
 
   depends_on "pkg-config" => :build
   depends_on "cairo"
