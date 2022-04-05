01ddbfdeda516505d73d96355a529a902516c763
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 07:42:58 +0800

    mockery: update 2.10.0_1 bottle.

--- Formula/mockery.rb
+++ Formula/mockery.rb
@@ -4,11 +4,12 @@ class Mockery < Formula
   url "https://github.com/vektra/mockery/archive/v2.10.0.tar.gz"
   sha256 "a3dcfa1f0599858bbc3ac1cdb17a09702ce0236fd329ddad80f975b8ee11efd1"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/vektra/mockery.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mockery"
-    sha256 cellar: :any_skip_relocation, mojave: "fab0b61aa6cda0c440a6eb3e4e6e1f188dc350009044e61bb0f28e2c2aa6bd45"
+    sha256 cellar: :any_skip_relocation, mojave: "9bee2a052c9191db9a3d2f15012e2e30225f6e9d3db113971972cb6619fbc36f"
   end
 
   depends_on "go" => :build
