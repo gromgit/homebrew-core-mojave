2e1eb57bc592a83fe298cc9af56985439792c2a6
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 01:35:21 +0800

    menhir: update 20220210 bottle.

--- Formula/menhir.rb
+++ Formula/menhir.rb
@@ -1,18 +1,13 @@
 class Menhir < Formula
   desc "LR(1) parser generator for the OCaml programming language"
   homepage "http://cristal.inria.fr/~fpottier/menhir"
-  url "https://gitlab.inria.fr/fpottier/menhir/-/archive/20210929/menhir-20210929.tar.bz2"
-  sha256 "2775a51f5f515fc4ec6088f06f5ad8149dc7e0f7019002355c09c1d5f96e7263"
+  url "https://gitlab.inria.fr/fpottier/menhir/-/archive/20220210/menhir-20220210.tar.bz2"
+  sha256 "767d7dfb1ed0d85cb19d5aef38912846b50df36751be558b0af342ccab0b1b47"
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "90ec00c926e42b81ce0571955fd748fa45fe59f76885d5b99084af73868cfe75"
-    sha256 cellar: :any,                 arm64_big_sur:  "39c8b455817f7a124d0a5cd8b576ba9de5243dfa5486d3f803b7e3c8e36fd22f"
-    sha256 cellar: :any,                 monterey:       "921cfc82b42dc804a7fde786567c8341799cfd6c47d9c41bdb2f9af5375563e2"
-    sha256 cellar: :any,                 big_sur:        "20086a5d407eaf8162f56dd1f9225491db6b987db6f00ecc9aa5a966cdc411e8"
-    sha256 cellar: :any,                 catalina:       "793848617f63013de384e88dc5ac2201b1d8cc2ac80e89f4b26b9f33661262f9"
-    sha256 cellar: :any,                 mojave:         "ab05cc4ce18b62a041365fd4d34f04dc55ae756845288dffe47786a0f2844efc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "efdd4cbdf040c3dfec55f9fd0b256450cc0c678d30e874df912b429f3dce06fc"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/menhir"
+    sha256 cellar: :any, mojave: "8574c6adf9e613256eee74dd00eaac46e18fd64379d807ad258a634cffc61667"
   end
 
   depends_on "dune" => :build
