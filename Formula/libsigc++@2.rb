afc4e20094c8f3d092dcdc0db7ebe244f2627ab1
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 12:17:32 +0800

    libsigc++@2: update 2.10.8 bottle.

--- Formula/libsigc++@2.rb
+++ Formula/libsigc++@2.rb
@@ -1,18 +1,13 @@
 class LibsigcxxAT2 < Formula
   desc "Callback framework for C++"
   homepage "https://libsigcplusplus.github.io/libsigcplusplus/"
-  url "https://download.gnome.org/sources/libsigc++/2.10/libsigc++-2.10.7.tar.xz"
-  sha256 "d082a2ce72c750f66b1a415abe3e852df2eae1e8af53010f4ac2ea261a478832"
+  url "https://download.gnome.org/sources/libsigc++/2.10/libsigc++-2.10.8.tar.xz"
+  sha256 "235a40bec7346c7b82b6a8caae0456353dc06e71f14bc414bcc858af1838719a"
   license "LGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "6b6ee5de019405fdc5b58a8b8761f60c3fe8d64a916999a0ad39a96117aab9ea"
-    sha256 cellar: :any,                 arm64_big_sur:  "3592c888aba18303eaa7e7837a27e386e311bac18684b613f705917ca6356ed6"
-    sha256 cellar: :any,                 monterey:       "5cba3fb26934f5f573872a72741b08261619e8e73eebe4384e2266b87ad2af4a"
-    sha256 cellar: :any,                 big_sur:        "c24567ac4b9a732a434afcadda0d1a02f573607c73ebb00625e8ffcc6506f331"
-    sha256 cellar: :any,                 catalina:       "6a51eeae589ad4511165b0236a64a7055315eafcf790fcc1ed3df24853fe4bdb"
-    sha256 cellar: :any,                 mojave:         "38886af39d68b304c8e9f4eac36f6a5e6bd21b79ee9a353df7266649f0490128"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7c36d5580b4f8970a6db2027820bbf1eb3cca66f56fbf8c56b32c72c3a23cc6c"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libsigc++@2"
+    sha256 cellar: :any, mojave: "bf6a1300603e06424c0348a5c67f38536406fb95557c4fbfba78e5dc4210c386"
   end
 
   depends_on "meson" => :build
