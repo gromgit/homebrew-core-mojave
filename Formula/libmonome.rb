cfb633bff725e16075d06c47615102fbba04fbab
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 11:17:15 +0800

    libmonome: update 1.4.6 bottle.

--- Formula/libmonome.rb
+++ Formula/libmonome.rb
@@ -3,25 +3,20 @@ class Libmonome < Formula
 
   desc "Library for easy interaction with monome devices"
   homepage "https://monome.org/"
-  url "https://github.com/monome/libmonome/archive/v1.4.5.tar.gz"
-  sha256 "c7109014f47f451f7b86340c40a1a05ea5c48e8c97493b1d4c0102b9ee375cd4"
+  url "https://github.com/monome/libmonome/archive/v1.4.6.tar.gz"
+  sha256 "dbb886eacb465ea893465beb7b5ed8340ae77c25b24098ab36abcb69976ef748"
   license "ISC"
   head "https://github.com/monome/libmonome.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libmonome"
-    sha256 cellar: :any, mojave: "8488ac9fc22090ebd4f1e289d3f9fc113fec685218e8dd658aaf3274ad95cfb0"
+    sha256 cellar: :any, mojave: "7b703a00801f7835eeef94e887652510e8a2c7ab8392ab658fbdeb49a2328511"
   end
 
   depends_on "python@3.10" => :build
   depends_on "liblo"
 
   def install
-    # Fix build on Mojave
-    # https://github.com/monome/libmonome/issues/62
-    inreplace "wscript", /conf.env.append_unique.*-mmacosx-version-min=10.5.*/,
-                         "pass"
-
     rewrite_shebang detected_python_shebang, *Dir.glob("**/{waf,wscript}")
 
     system "./waf", "configure", "--prefix=#{prefix}"
