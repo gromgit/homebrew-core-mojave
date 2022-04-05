9b7e6777e3fc3e7881944a53db2b14dabc4d9608
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 12:12:00 +0800

    librtlsdr: update 0.6.0 bottle.

--- Formula/librtlsdr.rb
+++ Formula/librtlsdr.rb
@@ -4,19 +4,12 @@ class Librtlsdr < Formula
   url "https://github.com/steve-m/librtlsdr/archive/0.6.0.tar.gz"
   sha256 "80a5155f3505bca8f1b808f8414d7dcd7c459b662a1cde84d3a2629a6e72ae55"
   license "GPL-2.0"
-  head "https://git.osmocom.org/rtl-sdr", using: :git
+  head "https://git.osmocom.org/rtl-sdr", using: :git, branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "f61808ab70f1d625cbc411d4f5e5e68a26b14f93eb926352353523cc54e188a6"
-    sha256 cellar: :any,                 arm64_big_sur:  "7b8ccea097dd346fcaec28c4fd3545bbffe2bf0ddcd735fa2fd5dd6920c117a0"
-    sha256 cellar: :any,                 monterey:       "39da4634626962907b3540fb365bf4272ef7082bdc8dad62763fef08658b3dae"
-    sha256 cellar: :any,                 big_sur:        "6bdf828e23854791779071bd32cd346d7cbc8d566738f63dd5c3185b91d11c73"
-    sha256 cellar: :any,                 catalina:       "8d09d3c7765995caed6f1e8fa26087e345d178c630b1ef2057fb8c34cdcddd7d"
-    sha256 cellar: :any,                 mojave:         "0e9b14804b722d9efc959940e40ebcef7bf716eb636f0bb0dc600770cb005531"
-    sha256 cellar: :any,                 high_sierra:    "71f28a8abd8e9e0245a61f841fcebcb7a179d952be786199bf21fae0edd11f6c"
-    sha256 cellar: :any,                 sierra:         "d1b83b24f32d4857205be289f7c632ee3bd77af802e3445d7565bb9ba9e4f3b1"
-    sha256 cellar: :any,                 el_capitan:     "f5196572498f20ff0ea38d4e7ceed95aea1199a558f29dd6aac5cec9db65ce33"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d055564310defc7ef61bba3a621a57b9cd563f13b54c831cf186beb160ec5c9a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/librtlsdr"
+    rebuild 1
+    sha256 cellar: :any, mojave: "479aca446c528aa1c7fb32e818881864f7ca06ad15797858b9a60d471b2d44c6"
   end
 
   depends_on "cmake" => :build
