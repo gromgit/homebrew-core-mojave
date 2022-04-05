f9a17bb65e3a59f4e2c1e10752ac8ffff702cb51
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 21:52:30 +0800

    matterbridge: update 1.24.1 bottle.

--- Formula/matterbridge.rb
+++ Formula/matterbridge.rb
@@ -1,13 +1,13 @@
 class Matterbridge < Formula
   desc "Protocol bridge for multiple chat platforms"
   homepage "https://github.com/42wim/matterbridge"
-  url "https://github.com/42wim/matterbridge/archive/v1.24.0.tar.gz"
-  sha256 "65ee39770b4799ebbe6676206cae3d067b4c7ea29773a1e7fb26c657d4b151a6"
+  url "https://github.com/42wim/matterbridge/archive/v1.24.1.tar.gz"
+  sha256 "ee2177f458a29dd8b4547cd6268fb8ac7e2ce2b551475427eca1205d67c236f4"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/matterbridge"
-    sha256 cellar: :any_skip_relocation, mojave: "da800035d043a45f892aa0998b259699fe38705579622e83956252a57f567130"
+    sha256 cellar: :any_skip_relocation, mojave: "c2474edc9f49d5a53316213ac5bd28f40da5db76c63fee505a95e676ccf52167"
   end
 
   depends_on "go" => :build
