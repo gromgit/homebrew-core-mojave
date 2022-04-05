229875c38f385a5875652be171bff6fc0da1f12b
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 21:44:42 +0800

    matlab2tikz: update 1.1.0 bottle.

--- Formula/matlab2tikz.rb
+++ Formula/matlab2tikz.rb
@@ -4,10 +4,12 @@ class Matlab2tikz < Formula
   url "https://github.com/matlab2tikz/matlab2tikz/archive/v1.1.0.tar.gz"
   sha256 "4e6fe80ebe4c8729650eb00679f97398c2696fd9399c17f9c5b60a1a6cf23a19"
   license "BSD-2-Clause"
-  head "https://github.com/matlab2tikz/matlab2tikz.git"
+  head "https://github.com/matlab2tikz/matlab2tikz.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "e798fd3ffe10d075bc3d1c25838630d2cac7cea104eac0af823b5d91c8c50854"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/matlab2tikz"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "f2189f2696a4ac8afcb36c43edd30731fdbd40a48889a724a3cc8fc4221b79a7"
   end
 
   def install
