19b9ed1863e6cd528e23cd35a9a32ce80481a7f8
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 11:02:06 +0800

    libicns: update 0.8.1_4 bottle.

--- Formula/libicns.rb
+++ Formula/libicns.rb
@@ -4,20 +4,11 @@ class Libicns < Formula
   url "https://downloads.sourceforge.net/project/icns/libicns-0.8.1.tar.gz"
   mirror "https://deb.debian.org/debian/pool/main/libi/libicns/libicns_0.8.1.orig.tar.gz"
   sha256 "335f10782fc79855cf02beac4926c4bf9f800a742445afbbf7729dab384555c2"
-  revision 3
+  revision 4
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "62da03a95a4b2b2342a762839e49c70887d9a01f5e097cc7881f9287384d1e45"
-    sha256 cellar: :any,                 arm64_big_sur:  "06b49357685dcaef31f7e32c7901ec217074a3b17d24c7a46456239e04ec3a9b"
-    sha256 cellar: :any,                 monterey:       "7f2427496f4dc8e1f04d350b4479406ffb6c9e815a93397c0255d98593274ab1"
-    sha256 cellar: :any,                 big_sur:        "6d503b029cbb646b11bae4d81c3537ea14f8c415039b196dace05a6ea691a846"
-    sha256 cellar: :any,                 catalina:       "33e4b9bf7de81d160ba9f8802c24e89c90903e6be9a3103c7e296536b1fe1511"
-    sha256 cellar: :any,                 mojave:         "fe7c57605e6f9b5626f5c2048aacd121c2c8973d24113f03275ed0659392a9fb"
-    sha256 cellar: :any,                 high_sierra:    "a2c03e94df9a8291b73c544d188d9e796161f49e0f14b8c88f94da40d3cfa04f"
-    sha256 cellar: :any,                 sierra:         "8fb5848fa2a111b3fc66aaea73c60a0795da6468e2ace92d2de57c9c8a5a3cde"
-    sha256 cellar: :any,                 el_capitan:     "12579e9f34ef57d3979b5a01206b543a67bf313cdb056d6c3e20e0b8cf0842b6"
-    sha256 cellar: :any,                 yosemite:       "aed1a7df05a8f98ea64dcdd8c4a406207239b7b187629fc03ae799308a142a4d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f2e62164cd0e78ae43714a373989b8543cb961ac177ae3b92ad7e2bc0e230818"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libicns"
+    sha256 cellar: :any, mojave: "3c1904c68e7a5614b3ace5fc22b861ba2aa797390020740560835e26019bd24a"
   end
 
   depends_on "jasper"
