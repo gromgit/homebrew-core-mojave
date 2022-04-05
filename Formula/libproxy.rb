fdb0a38efcc7bdec49cd62e2df7836b69633c531
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 11:36:41 +0800

    libproxy: update 0.4.17_1 bottle.

--- Formula/libproxy.rb
+++ Formula/libproxy.rb
@@ -5,16 +5,12 @@ class Libproxy < Formula
   sha256 "88c624711412665515e2800a7e564aabb5b3ee781b9820eca9168035b0de60a9"
   license "LGPL-2.1-or-later"
   revision 1
-  head "https://github.com/libproxy/libproxy.git"
+  head "https://github.com/libproxy/libproxy.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "ea29465d14b64ce0efdd879f98ecfad0731ba1bafff27aae5e257d78f4ddc84f"
-    sha256 arm64_big_sur:  "535133b549c369ced715e3e017d0ddeec32376a4b29469d2b18cef030b50fbe0"
-    sha256 monterey:       "22ef963f09431f28e2a50eb716cbff03a5369992640302fe4da3219674fbaab2"
-    sha256 big_sur:        "b2d8852168ed2484cadb96d11dc3e69126822dcd544ec7e5c66d0694287ad451"
-    sha256 catalina:       "c4811050c47e7178cb3ae6a5e675925d0448a39883a4a76b9d0f2d63e6ea1d53"
-    sha256 mojave:         "9e6c0ba9fb2215eb447b9613bdc62c18fc95fb0f3e681c44c794671189a20b56"
-    sha256 x86_64_linux:   "68c92b8f108d46bd729293dcf26294c030c71dcef6f9ab29c3d280017bf8e149"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libproxy"
+    rebuild 1
+    sha256 mojave: "30092f5eb19e00d357a0557c1ef58c336dc13dbeb86d3aebf986f7e4b4963f4d"
   end
 
   depends_on "cmake" => :build
