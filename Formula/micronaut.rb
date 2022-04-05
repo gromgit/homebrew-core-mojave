27e4c2fa2881d4bbe52de945253c23a434702764
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 02:40:46 +0800

    micronaut: update 3.4.0_1 bottle.

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -1,9 +1,10 @@
 class Micronaut < Formula
   desc "Modern JVM-based framework for building modular microservices"
   homepage "https://micronaut.io/"
-  url "https://github.com/micronaut-projects/micronaut-starter/archive/v3.3.3.tar.gz"
-  sha256 "100439cacbeee7100c7f8c33fed7073eb995b8840565474d5d00288c7686e86f"
+  url "https://github.com/micronaut-projects/micronaut-starter/archive/v3.4.0.tar.gz"
+  sha256 "1feed38c38e3f0cb66cb392d472c269260fdb8de8418117749c5354b9ba81d56"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url :stable
@@ -12,11 +13,11 @@ class Micronaut < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/micronaut"
-    sha256 cellar: :any_skip_relocation, mojave: "e73d1eb033f0aeaeb5291d6ebba78f9e796dd0e6a8766776a52f485682c107b1"
+    sha256 cellar: :any_skip_relocation, mojave: "eb8e869f16d893d14c7f7f0d9a8a9b47d8eaa2d26aa1873e2a56ae2ba4d1fbc5"
   end
 
   # Uses a hardcoded list of supported JDKs. Try switching to `openjdk` on update.
-  depends_on "openjdk@11"
+  depends_on "openjdk@17"
 
   def install
     system "./gradlew", "micronaut-cli:assemble", "-x", "test"
@@ -26,7 +27,7 @@ class Micronaut < Formula
     mv "starter-cli/build/exploded/lib", libexec/"lib"
 
     bash_completion.install "starter-cli/build/exploded/bin/mn_completion"
-    (bin/"mn").write_env_script libexec/"bin/mn", Language::Java.overridable_java_home_env("11")
+    (bin/"mn").write_env_script libexec/"bin/mn", Language::Java.overridable_java_home_env("17")
   end
 
   test do
