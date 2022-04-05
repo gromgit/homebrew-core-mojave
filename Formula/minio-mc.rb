474c637fd4af7d8e6245b841cf3a33ca5f9176ea
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 05:54:37 +0800

    minio-mc: update 20220317202506 bottle.

--- Formula/minio-mc.rb
+++ Formula/minio-mc.rb
@@ -2,9 +2,9 @@ class MinioMc < Formula
   desc "Replacement for ls, cp and other commands for object storage"
   homepage "https://github.com/minio/mc"
   url "https://github.com/minio/mc.git",
-      tag:      "RELEASE.2022-02-26T03-58-31Z",
-      revision: "a1d6c3d4af52833240332e08e7333e89a205f0db"
-  version "20220226035831"
+      tag:      "RELEASE.2022-03-17T20-25-06Z",
+      revision: "84a7b83eda09b25909d583e7e9fa437e2965e9b8"
+  version "20220317202506"
   license "AGPL-3.0-or-later"
   head "https://github.com/minio/mc.git", branch: "master"
 
@@ -18,7 +18,7 @@ class MinioMc < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/minio-mc"
-    sha256 cellar: :any_skip_relocation, mojave: "06086e17334741ee4e4541ec3c06999b9c0a43afddeb010fda1400a14b260c07"
+    sha256 cellar: :any_skip_relocation, mojave: "5f0dd1314bac80cc1ab7417760a4a9d0f3eef26ff6d82ec2bcf822b305d9ae83"
   end
 
   depends_on "go" => :build
