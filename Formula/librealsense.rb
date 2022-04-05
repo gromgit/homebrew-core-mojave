9555d453d4c90380880a58b334ea878f1dcdd3d1
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 12:03:53 +0800

    librealsense: update 2.50.0 bottle.

--- Formula/librealsense.rb
+++ Formula/librealsense.rb
@@ -1,10 +1,10 @@
 class Librealsense < Formula
   desc "Intel RealSense D400 series and SR300 capture"
   homepage "https://github.com/IntelRealSense/librealsense"
-  url "https://github.com/IntelRealSense/librealsense/archive/v2.49.0.tar.gz"
-  sha256 "2578ea0e75546aeebd908da732f52e0122bf37750d5a45f3adf92945a673aefd"
+  url "https://github.com/IntelRealSense/librealsense/archive/v2.50.0.tar.gz"
+  sha256 "cafeb2ed1efe5f42c4bd874296ce2860c7eebd15a9ce771f94580e0d0622098d"
   license "Apache-2.0"
-  head "https://github.com/IntelRealSense/librealsense.git"
+  head "https://github.com/IntelRealSense/librealsense.git", branch: "master"
 
   livecheck do
     url :stable
@@ -12,11 +12,8 @@ class Librealsense < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 monterey:     "bfcc6fb89a8cac2db7fe0d30b84c4d7b1585f02e3216f43435602ea484bb3d8f"
-    sha256 cellar: :any,                 big_sur:      "4c696d7911ab9321c26df52208dd433824e855f50b7a019018c980771c53b5ba"
-    sha256 cellar: :any,                 catalina:     "e32b24f59e07ceaac9482a1e1e961f4a0713ff045958e2d5f41872899f6bb4b8"
-    sha256 cellar: :any,                 mojave:       "6353572f45ee221eea411c12fb1ba1deeb24b9eea1cf8b15e860886012a000c7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "dbca3533de2b20945032d022e7776713db26f21d0c94d3ed8c8b355868cefd07"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/librealsense"
+    sha256 cellar: :any, mojave: "408bc0b5c219665d2ef5166d2726d61d1406d5a76f88cd9866d002c73b87257d"
   end
 
   depends_on "cmake" => :build
