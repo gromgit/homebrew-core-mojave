ddee80480c1ba0beb14667faa9be6c4c732e6329
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 06:33:38 +0800

    mitie: update 0.7_3 bottle.

--- Formula/mitie.rb
+++ Formula/mitie.rb
@@ -5,15 +5,12 @@ class Mitie < Formula
   sha256 "0830955e64c2a4cceab803884355f090cf8e9086e68ac5df43058f05c34697e8"
   license "BSL-1.0"
   revision 3
-  head "https://github.com/mit-nlp/MITIE.git"
+  head "https://github.com/mit-nlp/MITIE.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "85090e5f3a58d0e1b4d809bb67813684bce137134f9d903dfeae192e4540ffe8"
-    sha256 cellar: :any, arm64_big_sur:  "ad562a270ddfb8ffc19c35fd3ec680a1152f3fc25d0bfa9d07f32ba49f563086"
-    sha256 cellar: :any, monterey:       "508dd4609e72647b3534466576df9e18a0717c9387cf68c9bbf14f3d8769f5f8"
-    sha256 cellar: :any, big_sur:        "1a3f80b1b4c26c82cd9b0110d244ffa40aa30933a8b92ecfc1a6bf1e1265480a"
-    sha256 cellar: :any, catalina:       "edf72d45db9f8b0772e21dd024b9f44a2ff40b926ae2c1662e394b9468c19863"
-    sha256 cellar: :any, mojave:         "4bf2422b5d421784cc2829fd5987132c89a82637c7d05a3a34b95568084c8457"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mitie"
+    rebuild 1
+    sha256 cellar: :any, mojave: "63bbcda0d038720e43ad8978962590d5b1bf42f488951cb480c1611ad41f57a1"
   end
 
   depends_on "python@3.10"
