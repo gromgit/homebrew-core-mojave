7fc984f01242c803c58f21bf8f4ac12174788af4
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 06:50:20 +0800

    mix-completion: update 0.8.2 bottle.

--- Formula/mix-completion.rb
+++ Formula/mix-completion.rb
@@ -3,10 +3,12 @@ class MixCompletion < Formula
   homepage "https://github.com/davidhq/mix-power-completion"
   url "https://github.com/davidhq/mix-power-completion/archive/0.8.2.tar.gz"
   sha256 "0e3e94b199f847926f3668b4cebf1b132e63a44d438425dd5c45ac4a299f28f3"
-  head "https://github.com/davidhq/mix-power-completion.git"
+  head "https://github.com/davidhq/mix-power-completion.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "faa9e0edf1a9e4aac06ea640bfa2ff4ee4b6e81d086c33cd3aa999ef77b54a55"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mix-completion"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "b3752da67d9c50a5a2a9fcfe3256495d653c1a59f943be27061a03baac3973bc"
   end
 
   def install
