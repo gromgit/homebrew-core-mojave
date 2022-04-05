bfce64ad47f3abd9b1a313e25f07bab7495d027d
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 22:38:08 +0800

    md5deep: update 4.4_1 bottle.

--- Formula/md5deep.rb
+++ Formula/md5deep.rb
@@ -5,16 +5,12 @@ class Md5deep < Formula
   sha256 "dbda8ab42a9c788d4566adcae980d022d8c3d52ee732f1cbfa126c551c8fcc46"
   license "GPL-2.0"
   revision 1
-  head "https://github.com/jessek/hashdeep.git"
+  head "https://github.com/jessek/hashdeep.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1b64f262b089ff96008078a6dc0f84cce93deec0740b3476279931d982bc9636"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5d910e7454fa350663a1955628c254b7acf813dd7b3aaec162a7be2c002197f0"
-    sha256 cellar: :any_skip_relocation, monterey:       "58e0dfb42b8a8b0d89745dc0446ee660754f3350c776702384edceb1fe14b8b6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d53f71333428c98de807b2ed6be18fcfd62d473d9994e19db7c7a8db390cac95"
-    sha256 cellar: :any_skip_relocation, catalina:       "3156ba425284d497cdc5377c1d5d7659fe741811c5b1a390a2dd45f98bf0a19a"
-    sha256 cellar: :any_skip_relocation, mojave:         "c9e915e46aec5d2ec5460d6b8d73cd7f21b615b8882ab7eef3bbea6c25a8821e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a36e25199a0c133790f452fa716c07fc6bc724714f66c30be47f5989b703ed46"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/md5deep"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "ddf0b9ed452b8e2d4325e851ba2e5ece332994c46a544c368f543b1b4f569fc0"
   end
 
   depends_on "autoconf" => :build
