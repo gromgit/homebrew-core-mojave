1c91e7732e543e451efbc878e7980587c4c07473
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 18:29:26 +0800

    logswan: update 2.1.12 bottle.

--- Formula/logswan.rb
+++ Formula/logswan.rb
@@ -1,14 +1,13 @@
 class Logswan < Formula
   desc "Fast Web log analyzer using probabilistic data structures"
   homepage "https://www.logswan.org"
-  url "https://github.com/fcambus/logswan/archive/2.1.11.tar.gz"
-  sha256 "a031b04c2dcfe8195e4225b8dd5781fc76098b9d19d0e4a9cbe1da76a4928eea"
+  url "https://github.com/fcambus/logswan/archive/2.1.12.tar.gz"
+  sha256 "8e954fcf5319b83f184b1a91d0af12c58bb90ab0f322f42ea6ad36fb05d57894"
   license "BSD-2-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/logswan"
-    rebuild 2
-    sha256 cellar: :any, mojave: "60a3f8b90362887d7f811a6e86502046d42fb5d01a0da05c5247f5990a66df57"
+    sha256 cellar: :any, mojave: "b9a4a2748d4b81f211b6c0f0fb21691e6603876aee2507790b285dbabf68450a"
   end
 
   depends_on "cmake" => :build
