e3882de9b22d792274a53aac8dfbde0177a04ade
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 22:35:44 +0800

    mcfly: update 0.6.0 bottle.

--- Formula/mcfly.rb
+++ Formula/mcfly.rb
@@ -1,14 +1,14 @@
 class Mcfly < Formula
   desc "Fly through your shell history"
   homepage "https://github.com/cantino/mcfly"
-  url "https://github.com/cantino/mcfly/archive/v0.5.13.tar.gz"
-  sha256 "b4dc5fab1ef1fe05fc34b620f1dea6617b48d6d0e3aadcf29a4c9e4cb0894983"
+  url "https://github.com/cantino/mcfly/archive/v0.6.0.tar.gz"
+  sha256 "dd846f7ff7109921febc8aecdfd769a1258488a0d72b4a0cfcfa3eaac118b1bc"
   license "MIT"
   head "https://github.com/cantino/mcfly.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mcfly"
-    sha256 cellar: :any_skip_relocation, mojave: "84504564465f49a6eabd0da93b35128775196bf6548a125bbb59cc28f6c7a650"
+    sha256 cellar: :any_skip_relocation, mojave: "817286da087aaf0630af96c5db95727a44f8d948d1fb5056b7ac0127aa7340c5"
   end
 
   depends_on "rust" => :build
