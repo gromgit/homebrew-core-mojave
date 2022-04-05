1904202e7732521f2efeaa04fc9ef4ddd003d811
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 01:30:18 +0800

    memcached: update 1.6.14 bottle.

--- Formula/memcached.rb
+++ Formula/memcached.rb
@@ -12,12 +12,9 @@ class Memcached < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "a6a8fd0dfe19188ea6e42fc750c8ed3a09ba34b3bfa80694fe6c1a398e051763"
-    sha256 cellar: :any,                 arm64_big_sur:  "01603a7f7791ced3c8271e0e036d19dbea9afeb39e75d6b057af4c058521da31"
-    sha256 cellar: :any,                 monterey:       "0757348fb5c89a131dbe3a9b8dee3b76a2341bf6a264d5796d480969a20e8ddb"
-    sha256 cellar: :any,                 big_sur:        "632a9163d97df61899d84e10e7dfceb788fbe5da01a2147a445b07288b7800b9"
-    sha256 cellar: :any,                 catalina:       "d09725fb66e581866a8e7a996500323d6781b1390fd4921b876c546a9e28e390"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "27cf32dbaa1858fd5d595dff3c706c448cdfb6406b42b11dd136ac8c0245ae37"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/memcached"
+    rebuild 1
+    sha256 cellar: :any, mojave: "efd00e260b7517ebb75ccba85b6662718ef9a987b8bff21e22f28678fba0b452"
   end
 
   depends_on "libevent"
