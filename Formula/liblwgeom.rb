0a7a8d6ca068d5b4f98866b67701f0f5715a076e
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 11:12:07 +0800

    liblwgeom: update 2.5.4_1 bottle.

--- Formula/liblwgeom.rb
+++ Formula/liblwgeom.rb
@@ -5,17 +5,12 @@ class Liblwgeom < Formula
   sha256 "146d59351cf830e2a2a72fa14e700cd5eab6c18ad3e7c644f57c4cee7ed98bbe"
   license "GPL-2.0-or-later"
   revision 1
-  head "https://git.osgeo.org/gitea/postgis/postgis.git"
+  head "https://git.osgeo.org/gitea/postgis/postgis.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "c6e2f39f90827be84a17c7a1bfd7c0043bf3f0a433bfc1e38e2d9094028caad1"
-    sha256 cellar: :any,                 arm64_big_sur:  "4f8f0403e973d5e2eafb1b3d49deae54a3cb95a80dc42c50f1f28edcc73da0d8"
-    sha256 cellar: :any,                 monterey:       "fe6ef90e0a9026b59e89684f49ad9ddcb25a06fff022168b0b8f1f8d10a92bb7"
-    sha256 cellar: :any,                 big_sur:        "e28a391dfb1ccf34656e8169d5eda63bb96c7693508429f7c22b47add8a8bd47"
-    sha256 cellar: :any,                 catalina:       "cd5a31ea1b30721f36fcd64285b3150667c4cf30a148ffafa88d4e5c81456f45"
-    sha256 cellar: :any,                 mojave:         "79247efadb38c42e631ceeb750a8379fd68a2a5c720ec265f8f11502764be46b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9797785b01201a7eb6c4a2d5ba50e98c0b1c0b89e1671fd5914014a25c90b058"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/liblwgeom"
+    rebuild 2
+    sha256 cellar: :any, mojave: "4f72bde3385f6e19ecbc5ff6d165363b8747d1785edcc645f36832ad01d1fca9"
   end
 
   keg_only "conflicts with PostGIS, which also installs liblwgeom.dylib"
