84df72dc7f222abef6bded33f3c7dded008c1f7a
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 10:42:01 +0800

    libgr: update 0.64.1 bottle.

--- Formula/libgr.rb
+++ Formula/libgr.rb
@@ -1,13 +1,13 @@
 class Libgr < Formula
   desc "GR framework: a graphics library for visualisation applications"
   homepage "https://gr-framework.org/"
-  url "https://github.com/sciapp/gr/archive/v0.64.0.tar.gz"
-  sha256 "bfefe7e067480584faa2ad1ce296c1aaea1d0da2035224b46024abfaa6931076"
+  url "https://github.com/sciapp/gr/archive/v0.64.1.tar.gz"
+  sha256 "bd86856920301e444b1a144f16b239367719b63a87ee07447f0280b6ea1887f2"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libgr"
-    sha256 mojave: "50bdc6ad7b22f4271324954660ae0ef9fcc4e948a36094bcb8d96466a441b16c"
+    sha256 mojave: "cc9cef91c606702b012ae49496f306b9fdd55da22709d24dd89fae126015cbc1"
   end
 
   depends_on "cmake" => :build
