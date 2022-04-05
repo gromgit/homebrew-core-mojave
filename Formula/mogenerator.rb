7806d7d1ddf78a2ac05b89a710dc3dc318ebfa94
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 07:49:59 +0800

    mogenerator: update 1.32 bottle.

--- Formula/mogenerator.rb
+++ Formula/mogenerator.rb
@@ -4,16 +4,12 @@ class Mogenerator < Formula
   url "https://github.com/rentzsch/mogenerator/archive/1.32.tar.gz"
   sha256 "4fa660a19934d94d7ef35626d68ada9912d925416395a6bf4497bd7df35d7a8b"
   license "MIT"
-  head "https://github.com/rentzsch/mogenerator.git"
+  head "https://github.com/rentzsch/mogenerator.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "29159fa7d208a108c0a36a222f7a300151241810eccfef04059a86611dfe41d4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "51aec3a49207ae357af26a5407494bc88d98027ba06293736b2888ece7b1d71c"
-    sha256 cellar: :any_skip_relocation, monterey:       "a9907203474f336c731912e28fad5ec2e912a1e7378d5ba527a7bb3d3b160134"
-    sha256 cellar: :any_skip_relocation, big_sur:        "415e0e160574b7b16dff3d0395a7e156894675191c911d09cddf59e1d916571b"
-    sha256 cellar: :any_skip_relocation, catalina:       "d62cad0cc94a7b05286fb2a8a2f8e4a4cc3a9b46efa9a391aa9fcb00c381e85e"
-    sha256 cellar: :any_skip_relocation, mojave:         "dcb658659b696e44f13e382f553c92199a7ab0be48ff69f33a35ef98ee8a09ac"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mogenerator"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, mojave: "36fa0bc82189dcb55b047ab197eb9cbc7d2347fea2a8ddef289707524432a13f"
   end
 
   depends_on xcode: :build
