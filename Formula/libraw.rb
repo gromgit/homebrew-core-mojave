3b25140bef42b4939ee874aba0c6d49f57ded44c
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 11:50:49 +0800

    libraw: update 0.20.2_1 bottle.

--- Formula/libraw.rb
+++ Formula/libraw.rb
@@ -4,6 +4,7 @@ class Libraw < Formula
   url "https://www.libraw.org/data/LibRaw-0.20.2.tar.gz"
   sha256 "dc1b486c2003435733043e4e05273477326e51c3ea554c6864a4eafaff1004a6"
   license any_of: ["LGPL-2.1-only", "CDDL-1.0"]
+  revision 1
 
   livecheck do
     url "https://www.libraw.org/download/"
@@ -11,14 +12,8 @@ class Libraw < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "2cd59dec94bf52c1ae7c3e3a401532ded98d823a9047cac5e21626b6a7eb4a7e"
-    sha256 cellar: :any,                 arm64_big_sur:  "91215928c6f416f820d2d73c4be22175a00a7c9aa555b2dc01a3f649d888e20f"
-    sha256 cellar: :any,                 monterey:       "06fca4f57f1358542eee753e4c0ceb1a46796c5ebada6ed9a65e579e20f8932a"
-    sha256 cellar: :any,                 big_sur:        "89a0ce1dc2548f25b9813e32b9a234b44c3dab57adaf98fa204ca35f75b2f2eb"
-    sha256 cellar: :any,                 catalina:       "465ba53999bd9b1297fed5283b8cf09ccc600fc1ca00ea4be58a6193a96910d4"
-    sha256 cellar: :any,                 mojave:         "2c62cfe8160a4cf45819255cc2a1bb77427827bfa7c4cf782a42f95ee9822112"
-    sha256 cellar: :any,                 high_sierra:    "dd4ab6f94e1dc725cb23ea1f4ee0267b632e01c7187c7a2b109053f457a17284"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "88e847382d3000609414ddce26389f6a4e19392e6209e32cd5067b8d6a05616c"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libraw"
+    sha256 cellar: :any, mojave: "1818970d03fafac9fa0609fc8fac0d57b26ebb9cb4f8243629b38bbfb55b14ae"
   end
 
   depends_on "autoconf" => :build
