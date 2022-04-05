77887853b4f7d5a2a2c6aeaf8d415ba2ef32eff3
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 04:11:09 +0800

    miller: update 6.2.0 bottle.

--- Formula/miller.rb
+++ Formula/miller.rb
@@ -1,14 +1,14 @@
 class Miller < Formula
   desc "Like sed, awk, cut, join & sort for name-indexed data such as CSV"
   homepage "https://github.com/johnkerl/miller"
-  url "https://github.com/johnkerl/miller/releases/download/v6.0.0/miller-6.0.0.tar.gz"
-  sha256 "b5e04ccbcb021bebbd758db4ae844712eb4fcfc67e6aaf39e24f751f45cfd0cd"
+  url "https://github.com/johnkerl/miller/releases/download/v6.2.0/miller-6.2.0.tar.gz"
+  sha256 "da26531fe9bca1c1917ea41dd1f1d7338c61f50275ad3ea41d3bc4685da34687"
   license "BSD-2-Clause"
   head "https://github.com/johnkerl/miller.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/miller"
-    sha256 cellar: :any_skip_relocation, mojave: "1ab6d7678e7d5c8ddfab6b591fadaea9fde4a7c5c7a8d10c0cd16dec18343576"
+    sha256 cellar: :any_skip_relocation, mojave: "3d3327d14f75cd96498191f9a114c916a7edcf0245e709bead320239d484aac3"
   end
 
   depends_on "go" => :build
