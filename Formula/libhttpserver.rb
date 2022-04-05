edd4e916a220e4f966c8fc58ff783e84cf87e605
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-05 10:56:17 +0800

    libhttpserver: update 0.18.2 bottle.

--- Formula/libhttpserver.rb
+++ Formula/libhttpserver.rb
@@ -4,16 +4,12 @@ class Libhttpserver < Formula
   url "https://github.com/etr/libhttpserver/archive/0.18.2.tar.gz"
   sha256 "1dfe548ac2add77fcb6c05bd00222c55650ffd02b209f4e3f133a6e3eb29c89d"
   license "LGPL-2.1-or-later"
-  head "https://github.com/etr/libhttpserver.git"
+  head "https://github.com/etr/libhttpserver.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "a7373b51c70174b698e57eb9543662b342a9c277d87dc096fd00359e82e83c85"
-    sha256 cellar: :any,                 arm64_big_sur:  "fdec5ac92f5b5d22d3b8a335938b23b9e20605b475dc3c1b2e9ced920b0b33e0"
-    sha256 cellar: :any,                 monterey:       "ddece3c44cd79a12868efccbd72be70eef25f45a91aff5998ef3e644a215cf8d"
-    sha256 cellar: :any,                 big_sur:        "17103a950045c06e959eb1d034f0a1cee89531084940f8844f1dcd4331beb4aa"
-    sha256 cellar: :any,                 catalina:       "6684db18245d033c86c7887feca8dba18cd3e07c5dbd9a9379c4107331f68a14"
-    sha256 cellar: :any,                 mojave:         "755c274617ee811c4fda5ee110ba46dd3f171cc4bac67925ca159cfefcdb0b99"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "52afb93c01b3fd69b8f589e696c7ce8d4dd35d0c04af4c1467c441da96ca3dac"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libhttpserver"
+    rebuild 1
+    sha256 cellar: :any, mojave: "2c5d8aa2f2694916dff7dff214a49f8ea60bb171657ffaa986047a63cbeb4ea2"
   end
 
   depends_on "autoconf" => :build
