31664f101c9d05fe017093e2d4d087591f50aad9
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 10:12:05 +0800

    msgpuck: update 2.0 bottle.

--- Formula/msgpuck.rb
+++ Formula/msgpuck.rb
@@ -4,19 +4,12 @@ class Msgpuck < Formula
   url "https://github.com/rtsisyk/msgpuck/archive/2.0.tar.gz"
   sha256 "01e6aa55d4d52a5b19f7ce9a9845506d9ab3f5abcf844a75e880b8378150a63d"
   license "BSD-2-Clause"
-  head "https://github.com/rtsisyk/msgpuck.git"
+  head "https://github.com/rtsisyk/msgpuck.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "4ca7fca2ee5beb10e49b21dd3f810450442147158a291e023acc8e7982630c7f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d52c18b8fef8cbd5b59f41e014a2c85610787084714a4086c26152cd6cc59a6b"
-    sha256 cellar: :any_skip_relocation, monterey:       "350569d8ace92f17a35a90345712219f94596e8857ed222e1ca86ab086fa06ed"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c0137f8e139ada6b1c98315f7e9982a44131b133a48aa7959d64015880d7f430"
-    sha256 cellar: :any_skip_relocation, catalina:       "01dbdca0333694d379bd7b209d52d8dd8e48f5416d9df441d43cdb29c2751738"
-    sha256 cellar: :any_skip_relocation, mojave:         "0fedf815d4ba46d10e5fe7910cbcc06f1ea2906e40a4ef994ffd3aa04289c423"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "50197e08a5b55fbe804109ad01dfa815a6dde2b11b688d89a58154fed2d8d54f"
-    sha256 cellar: :any_skip_relocation, sierra:         "6f4011d177bf2e42f94f853bc93283ada6c48df8fdb7269135def453e65e598d"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "b0accfedd2582109acec3297878bb943360282520a31b0d1c16c4ec1aa70a362"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "24b63f0499e6e5bc5b138228966945ff497936a4679da98a44aa18cfeec538a1"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/msgpuck"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "e02594d0ce1b6710e89e664628740a3912eea4f5ec8117c43004cdbae78e1ce3"
   end
 
   depends_on "cmake" => :build
