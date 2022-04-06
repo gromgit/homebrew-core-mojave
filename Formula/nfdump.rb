cdb624a59272ad40f90c9ab27c831e87b02b1808
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 12:36:31 +0800

    nfdump: update 1.6.24 bottle.

--- Formula/nfdump.rb
+++ Formula/nfdump.rb
@@ -1,18 +1,14 @@
 class Nfdump < Formula
   desc "Tools to collect and process netflow data on the command-line"
   homepage "https://github.com/phaag/nfdump"
-  url "https://github.com/phaag/nfdump/archive/v1.6.23.tar.gz"
-  sha256 "8c5a7959e66bb90fcbd8ad508933a14ebde4ccf7f4ae638d8f18c9473c63af33"
+  url "https://github.com/phaag/nfdump/archive/v1.6.24.tar.gz"
+  sha256 "11ea7ecba405d57076c321f6f77491f1c64538062630131c98ac62dc4870545e"
   license "BSD-3-Clause"
   head "https://github.com/phaag/nfdump.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "39c67075d143f58892baadff95c631182c4d80183b2ab38cdeb0df475c192bd7"
-    sha256 cellar: :any, arm64_big_sur:  "0166cf87b2fac3b3d48f54a7b5d3dd2433e43fd0aa21500f485167e035b09d49"
-    sha256 cellar: :any, monterey:       "157477b2e290a049a25c4d12001fb0492e8c8342612f22ccb9169493686b58d1"
-    sha256 cellar: :any, big_sur:        "04cb78152af1e986c69f60f6e3437cce46d362f4087ded5717e6ab0390cc7041"
-    sha256 cellar: :any, catalina:       "cd1575d5edc5474ab44e8729ef1fbdb10df0508b96d408888ab147d7ba6408f5"
-    sha256 cellar: :any, mojave:         "cf8ab09a45c609ba6deb8e4b7db49077c301c228658b778ecd29f81b9fbb1482"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nfdump"
+    sha256 cellar: :any, mojave: "399f771738bacc10748d3ac50934b468aec2822b1db99e8009bc199720182964"
   end
 
   depends_on "autoconf" => :build
@@ -20,9 +16,14 @@ class Nfdump < Formula
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
 
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
+  uses_from_macos "bzip2"
+  uses_from_macos "libpcap"
+
   def install
     system "./autogen.sh"
-    system "./configure", "--prefix=#{prefix}", "--enable-readpcap"
+    system "./configure", *std_configure_args, "--enable-readpcap", "LEXLIB="
     system "make", "install"
   end
 
