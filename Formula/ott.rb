dde3ec6efaf003a3f54642ede7578d099dfbfa29
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 19:15:51 +0800

    ott: update 0.32 bottle.

--- Formula/ott.rb
+++ Formula/ott.rb
@@ -1,10 +1,9 @@
 class Ott < Formula
   desc "Tool for writing definitions of programming languages and calculi"
   homepage "https://www.cl.cam.ac.uk/~pes20/ott/"
-  url "https://github.com/ott-lang/ott/archive/0.31.tar.gz"
-  sha256 "3203f1b3eeb30e6aead9f63f6df22f5ead2407964ac9bb3cd5b0ae78df4568f8"
+  url "https://github.com/ott-lang/ott/archive/0.32.tar.gz"
+  sha256 "c4a9d9a6b67f3d581383468468ea36d54a0097804e9fac43c8090946904d3a2c"
   license "BSD-3-Clause"
-  revision 1
   head "https://github.com/ott-lang/ott.git", branch: "master"
 
   livecheck do
@@ -13,14 +12,8 @@ class Ott < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3c3c0919b9f0c3225976e7126c1b909ba4ef3a9d0aade6461ec2c73e971adc73"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "40c61230f66924d6dc12f0e7c1eefd6baa29a40a9d0b560611ae47935d828c9b"
-    sha256 cellar: :any_skip_relocation, monterey:       "159b3ac297aa40a4462d05e17534e0cadb97de1b2b6e68e1fe403209e8fe2884"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b1b1f04443bb7645a23898cb98a1308ebbf3033b2d9054e027ed59b722d6850b"
-    sha256 cellar: :any_skip_relocation, catalina:       "22f441ac37e494c9b667838b43e87f820dcf2fe090c4794db7eb0cc3cbd514a6"
-    sha256 cellar: :any_skip_relocation, mojave:         "7f4274253521ef41f38b674247906a6f9567dce515c80a5150111a1da0dc5caf"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "a59bb92116efa3e1b7c13da59a95dd7ad7a2618c6efc4b09d74a59001478bde5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cdc6d3369e71db55a8e32ce1e984b76bf91a11b0671201a2551ea2ce3e60ccd1"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ott"
+    sha256 cellar: :any_skip_relocation, mojave: "a89e9a560daa4bc4562c86805726ac0cc6e0ba2c89d1463def26fbbe87fcadb9"
   end
 
   depends_on "ocaml" => :build
