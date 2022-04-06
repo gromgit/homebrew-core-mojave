babf6fdaa45afdd7f526e490bacdef432bf75800
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-07 03:34:31 +0800

    pinboard-notes-backup: update 1.0.5.4 bottle.

--- Formula/pinboard-notes-backup.rb
+++ Formula/pinboard-notes-backup.rb
@@ -1,20 +1,18 @@
 class PinboardNotesBackup < Formula
   desc "Efficiently back up the notes you've saved to Pinboard"
   homepage "https://github.com/bdesham/pinboard-notes-backup"
-  url "https://github.com/bdesham/pinboard-notes-backup/archive/v1.0.5.3.tar.gz"
-  sha256 "75491e082812493096b68de0031f71a21b8e9e5e8b981b4bc648bb520c5432dc"
+  url "https://github.com/bdesham/pinboard-notes-backup/archive/v1.0.5.4.tar.gz"
+  sha256 "c2a239f8f5d7acba04c8a5bdf6e0f337e547f99c29d37db638d915712b97505d"
   license "GPL-3.0-or-later"
   head "https://github.com/bdesham/pinboard-notes-backup.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:      "57a89dec64c38b1742914b5ff0cb7e0a1555715644b465ecae8b2c67c7580ec2"
-    sha256 cellar: :any_skip_relocation, catalina:     "a1c7eb9a928d9461b66bcd43af6dbaa276d7cd730706e2b57833f70c503fdaaa"
-    sha256 cellar: :any_skip_relocation, mojave:       "d0e2d4abf24f35b65aa3f5454febc68444f33fcba3d0c01ce33a5f571ef4477e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "3ed421ceef39a006065676d8b8f2c3ba553e0986ab7187a7956f6504389904b2"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pinboard-notes-backup"
+    sha256 cellar: :any_skip_relocation, mojave: "3ba8670a7997c4121747eda2f1e0161f8919599f0fee3eccdf3edc2a60d8aaf2"
   end
 
   depends_on "cabal-install" => :build
-  depends_on "ghc@8.6" => :build
+  depends_on "ghc" => :build
 
   uses_from_macos "zlib"
 
