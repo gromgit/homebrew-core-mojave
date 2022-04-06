71c72fb6c83d2b0ed7a7f1e9ed419abcf480bb17
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 13:41:24 +0800

    norm: update 1.5.9 bottle.

--- Formula/norm.rb
+++ Formula/norm.rb
@@ -1,30 +1,30 @@
 class Norm < Formula
   desc "NACK-Oriented Reliable Multicast"
   homepage "https://www.nrl.navy.mil/itd/ncs/products/norm"
-  url "https://github.com/USNavalResearchLaboratory/norm/archive/v1.5.8.tar.gz"
-  sha256 "ee7493c9ae9a129e7cbcd090a412fb0d0e25ab3acaa4748e5dc696bf822a62b5"
+  url "https://github.com/USNavalResearchLaboratory/norm/releases/download/v1.5.9/src-norm-1.5.9.tgz"
+  sha256 "ef6d7bbb7b278584e057acefe3bc764d30122e83fa41d41d8211e39f25b6e3fa"
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "a8a59ed661565f3cf10b7bc5eaedb45d90cc949031d5d36a9b098985e8da71cc"
-    sha256 cellar: :any, arm64_big_sur:  "658241c3120a66b68191f64e4009617d9ee0e43c5cd264e0db59f9dd2aeca966"
-    sha256 cellar: :any, monterey:       "024bbaf98d384ee89e3ff5db7e56dc706dcc6f445ea3ebd976bd32c21fe24bcf"
-    sha256 cellar: :any, big_sur:        "0d223adbb36c557616fbb8e026e93a36603ff5478ea3912978190389e409b04a"
-    sha256 cellar: :any, catalina:       "b01566af6d67555366f350e72a9717479c1510af885a89b60827356aeba7d2af"
-    sha256 cellar: :any, mojave:         "bc9f51046dc479949b480bb9a27143679bccb5f4bab0928c5968d280f9489d86"
-    sha256 cellar: :any, high_sierra:    "c46470e7594148cbee61f851b57373374abdc6a94e91c722efabd3c90f36ec06"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/norm"
+    sha256 cellar: :any, mojave: "5d94d95876aeece0d30ef7338de954efd53af9e5b0b1793a89b747a8ddb5af31"
   end
 
-  resource "protolib" do
-    url "https://github.com/USNavalResearchLaboratory/protolib/archive/v3.0b1.tar.gz"
-    sha256 "1e15bbbef4758e0179672d456c2ad2b2087927a3796adc4a18e2338f300bc3e6"
+  depends_on "python@3.10" => :build
+
+  # Fix warning: 'visibility' attribute ignored [-Wignored-attributes]
+  # Remove in the next release
+  #
+  # Ref https://github.com/USNavalResearchLaboratory/norm/pull/27
+  patch do
+    url "https://github.com/USNavalResearchLaboratory/norm/commit/476b8bb7eba5a9ad02e094de4dce05a06584f5a0.patch?full_index=1"
+    sha256 "08f7cc7002dc1afe6834ec60d4fea5c591f88902d1e76c8c32854a732072ea56"
   end
 
   def install
-    (buildpath/"protolib").install resource("protolib")
+    system "python3", "./waf", "configure", "--prefix=#{prefix}"
+    system "python3", "./waf", "install"
 
-    system "./waf", "configure", "--prefix=#{prefix}"
-    system "./waf", "install"
     include.install "include/normApi.h"
   end
 
@@ -42,7 +42,7 @@ class Norm < Formula
         return 0;
       }
     EOS
-    system ENV.cc, "test.c", "-L#{lib}", "-lnorm", "-o", "test"
+    system ENV.cxx, "test.c", "-L#{lib}", "-lnorm", "-o", "test"
     system "./test"
   end
 end
