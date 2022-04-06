3a48e062c892377f34420b1f3a3e7e87a5234f11
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 11:13:48 +0800

    nativefier: update 46.2.0 bottle.

--- Formula/nativefier.rb
+++ Formula/nativefier.rb
@@ -3,13 +3,13 @@ require "language/node"
 class Nativefier < Formula
   desc "Wrap web apps natively"
   homepage "https://github.com/nativefier/nativefier"
-  url "https://registry.npmjs.org/nativefier/-/nativefier-46.1.1.tgz"
-  sha256 "bd9f8296595cd2a1453dadfc3dcede35e99fc21cb05bd7e7b610d6759400789e"
+  url "https://registry.npmjs.org/nativefier/-/nativefier-46.2.0.tgz"
+  sha256 "386f40c1726330af86832fc41e776b0f043bf5dfafaa8f9f01038947c124a9eb"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nativefier"
-    sha256 cellar: :any_skip_relocation, mojave: "a47917ae666ec5cec6bb7dc52b43e383a2ea66a6c87f37d8fa891b59a0ab74e5"
+    sha256 cellar: :any_skip_relocation, mojave: "01bb5154d994fbdcee9749f87d765fce671e9991ec1b4a2a88bf14e674311419"
   end
 
   depends_on "node"
