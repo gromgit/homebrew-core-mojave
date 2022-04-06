6e68fdcc381ff7dc3c8c3ff27e64b2975934f9c1
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 10:55:03 +0800

    mvtools: update 23_1 bottle.

--- Formula/mvtools.rb
+++ Formula/mvtools.rb
@@ -5,15 +5,12 @@ class Mvtools < Formula
   sha256 "3b5fdad2b52a2525764510a04af01eab3bc5e8fe6a02aba44b78955887a47d44"
   license "GPL-2.0"
   revision 1
-  head "https://github.com/dubhater/vapoursynth-mvtools.git"
+  head "https://github.com/dubhater/vapoursynth-mvtools.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 monterey:     "5fc1c4a4fda847ebc2a78fe9972fd99fa7c4f7f52b74cb68825181634f9c3d5e"
-    sha256 cellar: :any,                 big_sur:      "df691836b6052e38806e3e4a662f0b5da22120f8f586ad6ea388e2673dcf01b3"
-    sha256 cellar: :any,                 catalina:     "01785cf0cea2080cb2b875df545e027aaaf339fbbddeca53fd5dae8f39bf4726"
-    sha256 cellar: :any,                 mojave:       "0809f0353e48e30d8628bbe2124cebfa0ebd1a6add77e2d27798ce968dadb84d"
-    sha256 cellar: :any,                 high_sierra:  "0a1bab6b74375cb11959d2100e562bb2cc8124da7115b754975cd70c31e676b2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "915cd8e779a5143a86f77cecc9efae2029eda0194358b52e69c4e59811c20c6f"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mvtools"
+    rebuild 1
+    sha256 cellar: :any, mojave: "591e030d518d18f4ffcf608f17d52be922ecfc21d72e4dbc7667f49a07a7b85a"
   end
 
   depends_on "autoconf" => :build
