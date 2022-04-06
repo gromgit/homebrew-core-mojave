c47494489bc49d78d9fc0f38c472b72cb0753748
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 11:15:35 +0800

    nats-server: update 2.7.4 bottle.

--- Formula/nats-server.rb
+++ Formula/nats-server.rb
@@ -1,14 +1,14 @@
 class NatsServer < Formula
   desc "Lightweight cloud messaging system"
   homepage "https://nats.io"
-  url "https://github.com/nats-io/nats-server/archive/refs/tags/v2.7.3.tar.gz"
-  sha256 "06dc1c178f220c20122e73f84824fe29146fbe79ec189f65632c19ddad9e8fcd"
+  url "https://github.com/nats-io/nats-server/archive/refs/tags/v2.7.4.tar.gz"
+  sha256 "4a98e3ba5022f55341aed7c07b6c61bd93be82de76ef03758825ca9d53310e6f"
   license "Apache-2.0"
   head "https://github.com/nats-io/nats-server.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nats-server"
-    sha256 cellar: :any_skip_relocation, mojave: "7a3340764cd7459c3367288255b81a588935b6035289b74f4b469496cc8d6240"
+    sha256 cellar: :any_skip_relocation, mojave: "65b8038bdcabcf21c211c247d8ba8d6e215e602dadd0e100896498084557c79b"
   end
 
   depends_on "go" => :build
