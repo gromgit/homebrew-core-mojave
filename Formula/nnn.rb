6fa490b3643416a909e19d7e4aea1a5e21bb8ff9
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 13:05:27 +0800

    nnn: update 4.4_1 bottle.

--- Formula/nnn.rb
+++ Formula/nnn.rb
@@ -4,18 +4,17 @@ class Nnn < Formula
   url "https://github.com/jarun/nnn/archive/v4.4.tar.gz"
   sha256 "e04a3f0f0c2af1e18cb6f005d18267c7703644274d21bb93f03b30e4fd3d1653"
   license "BSD-2-Clause"
+  revision 1
   head "https://github.com/jarun/nnn.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nnn"
-    rebuild 2
-    sha256 cellar: :any, mojave: "faaf2933145c147643220703a1bf24aca721a1127b97296cfb799b8ac4213194"
+    sha256 cellar: :any, mojave: "a8d8c5f452bdddeeaea58c88d46ce653672e8bd100cfe08c0a09f149ae6c7768"
   end
 
   depends_on "gnu-sed"
+  depends_on "ncurses"
   depends_on "readline"
-
-  uses_from_macos "ncurses"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
@@ -23,14 +22,14 @@ class Nnn < Formula
     bash_completion.install "misc/auto-completion/bash/nnn-completion.bash"
     zsh_completion.install "misc/auto-completion/zsh/_nnn"
     fish_completion.install "misc/auto-completion/fish/nnn.fish"
+
+    pkgshare.install "misc/quitcd"
   end
 
   test do
-    on_linux do
-      # Test fails on CI: Input/output error @ io_fread - /dev/pts/0
-      # Fixing it involves pty/ruby voodoo, which is not worth spending time on
-      return if ENV["HOMEBREW_GITHUB_ACTIONS"]
-    end
+    # Test fails on CI: Input/output error @ io_fread - /dev/pts/0
+    # Fixing it involves pty/ruby voodoo, which is not worth spending time on
+    return if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
 
     # Testing this curses app requires a pty
     require "pty"
