885b63ac535bb776d419383ce13286ceb1cc65fc
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 11:17:33 +0800

    nats-streaming-server: update 0.24.3 bottle.

--- Formula/nats-streaming-server.rb
+++ Formula/nats-streaming-server.rb
@@ -1,14 +1,14 @@
 class NatsStreamingServer < Formula
   desc "Lightweight cloud messaging system"
   homepage "https://nats.io"
-  url "https://github.com/nats-io/nats-streaming-server/archive/refs/tags/v0.24.2.tar.gz"
-  sha256 "7622fb7d5a6f23b66dbbcf4296ce7ba8d98835514d3d55689a1982ae92b041af"
+  url "https://github.com/nats-io/nats-streaming-server/archive/refs/tags/v0.24.3.tar.gz"
+  sha256 "f59baaa629152ce9dcbb3a4e2ca72ce1275f43b1c24cee7d54b53c58534fa30f"
   license "Apache-2.0"
   head "https://github.com/nats-io/nats-streaming-server.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nats-streaming-server"
-    sha256 cellar: :any_skip_relocation, mojave: "f1566ca26c12af673f192cbe5e080a834413f9023129d00b46c69dfb5cd8e236"
+    sha256 cellar: :any_skip_relocation, mojave: "e0160818ff5ba47a59f21a7c57258d1abe87e2809f821e0fef11b666e8fcc969"
   end
 
   depends_on "go" => :build
