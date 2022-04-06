b5f5c53b5570ebe220875a9a4048fac013af1c80
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 10:06:05 +0800

    mpssh: update 1.3.3 bottle.

--- Formula/mpssh.rb
+++ Formula/mpssh.rb
@@ -2,7 +2,7 @@ class Mpssh < Formula
   desc "Mass parallel ssh"
   homepage "https://github.com/ndenev/mpssh"
   license "BSD-3-Clause"
-  head "https://github.com/ndenev/mpssh.git"
+  head "https://github.com/ndenev/mpssh.git", branch: "master"
 
   stable do
     url "https://github.com/ndenev/mpssh/archive/1.3.3.tar.gz"
@@ -15,17 +15,9 @@ class Mpssh < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bcef6cadd8e60b9856c5cc99d1047deaee4a18a852127c0e4f22fb59f9751371"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f9d5c61b0953345267eda6e05e0c712823ecf4d037e2960ebcd4d836c045ef4d"
-    sha256 cellar: :any_skip_relocation, monterey:       "94d6b1821f850cb852373d0e46b9da787b799d726f4917ece31a0e0dc149b25a"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d6e032b03f612d0be60c38b1af6688f8786e9c097d52c2e8bd3cd507290e4482"
-    sha256 cellar: :any_skip_relocation, catalina:       "714e7b0e97a942f68885baefa599d97e143631154480d0246d04e21a49910acf"
-    sha256 cellar: :any_skip_relocation, mojave:         "e37b5e479ba7f9ad86373e646c63485b55dd1381c2cbc130150e108886675b72"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "1057c47b866d50031a23a0bd244d3bc056b9f12a4d9bf0aeebc0ea292c484638"
-    sha256 cellar: :any_skip_relocation, sierra:         "90d758a0f7accf0b63755c3de8100a880b500e732fc8924123ab2a1c7ce688f8"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "e5ac485861dfca0be2bb1ca2eb5826b5ca5977c0d2abb12dc58de011c18046f1"
-    sha256 cellar: :any_skip_relocation, yosemite:       "2b91c9a9dbae19e99b8b8735bb3292cc056dcf8e06472c0b2d354f64896a4186"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5eabc527317cd3a5091e6efabe168b926693d6eb64644fec082a251a99725669"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mpssh"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "ae80e7f23fcf69ffd1fdbea208c7bcc872b4af90c77e31fcd1929f8849e830e8"
   end
 
   def install
