a32ae4fcffde7e831207f539ea517959a8624a29
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 14:30:18 +0800

    nushell: update 0.60.0 bottle.

--- Formula/nushell.rb
+++ Formula/nushell.rb
@@ -1,8 +1,8 @@
 class Nushell < Formula
   desc "Modern shell for the GitHub era"
   homepage "https://www.nushell.sh"
-  url "https://github.com/nushell/nushell/archive/0.44.0.tar.gz"
-  sha256 "94bb003fd05b604a174a686c40286fef460bc06616a7d273c387a54a07576ecc"
+  url "https://github.com/nushell/nushell/archive/0.60.0.tar.gz"
+  sha256 "9b9deb8e42cf18ccf328833a4d051fc14cef0be468afc3b0dbde6657deb9f079"
   license "MIT"
   head "https://github.com/nushell/nushell.git", branch: "main"
 
@@ -14,7 +14,7 @@ class Nushell < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nushell"
-    sha256 cellar: :any_skip_relocation, mojave: "9061ba1cf9aa0be2622c392eda823f2c732705ab9ceaab85484f1a0934a81ef6"
+    sha256 cellar: :any, mojave: "7eadabef0a05473070d4b6d24832ea3b29fdc6de18c7a72b85e659968de41f8a"
   end
 
   depends_on "rust" => :build
@@ -30,10 +30,16 @@ class Nushell < Formula
 
   def install
     system "cargo", "install", "--features", "extra", *std_cargo_args
+
+    buildpath.glob("crates/nu_plugin_*").each do |plugindir|
+      next unless (plugindir/"Cargo.toml").exist?
+
+      system "cargo", "install", *std_cargo_args(path: plugindir)
+    end
   end
 
   test do
     assert_match "homebrew_test",
-      pipe_output("#{bin}/nu", 'echo \'{"foo":1, "bar" : "homebrew_test"}\' | from json | get bar')
+      pipe_output("#{bin}/nu -c \'{ foo: 1, bar: homebrew_test} | get bar\'", nil)
   end
 end
