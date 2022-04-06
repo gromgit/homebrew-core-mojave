18881da010d0de112d344a997962ff910e98fa79
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-07 01:56:17 +0800

    pgpdump: update 0.35 bottle.

--- Formula/pgpdump.rb
+++ Formula/pgpdump.rb
@@ -1,14 +1,14 @@
 class Pgpdump < Formula
   desc "PGP packet visualizer"
   homepage "https://www.mew.org/~kazu/proj/pgpdump/en/"
-  url "https://github.com/kazu-yamamoto/pgpdump/archive/v0.34.tar.gz"
-  sha256 "d5bb5c5a1cbbc5a536baa64c12820a184211e8855b8d3e16ae832d1eaa314926"
+  url "https://github.com/kazu-yamamoto/pgpdump/archive/v0.35.tar.gz"
+  sha256 "50b817d0ceaee41597b51e237e318803bf561ab6cf2dc1b49f68e85635fc8b0f"
   license "BSD-3-Clause"
   head "https://github.com/kazu-yamamoto/pgpdump.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pgpdump"
-    sha256 cellar: :any_skip_relocation, mojave: "5a52834a10593e102ff2ebb8b78dd288d99e90b4c4271b84d51679044b7577c0"
+    sha256 cellar: :any_skip_relocation, mojave: "6ebc8152402418b0c261b72e66a5857bc0f19cc9e17d9c97811562376ca50dfb"
   end
 
   uses_from_macos "bzip2"
