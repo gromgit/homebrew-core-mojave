d51ef31b484a6cab338bfdb8be25d68badc412c0
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 11:18:59 +0800

    nave: update 3.3.1 bottle.

--- Formula/nave.rb
+++ Formula/nave.rb
@@ -1,14 +1,14 @@
 class Nave < Formula
   desc "Virtual environments for Node.js"
   homepage "https://github.com/isaacs/nave"
-  url "https://github.com/isaacs/nave/archive/v3.2.3.tar.gz"
-  sha256 "cd7c1f6f87b1dd83cbed52b5f101bb4fce8448030be85f5de034599179d84b78"
+  url "https://github.com/isaacs/nave/archive/v3.3.1.tar.gz"
+  sha256 "c5789615135cf1b0b55c9e7422735bc8af4a2eee717cc09c0d520cfe88cc68a0"
   license "ISC"
   head "https://github.com/isaacs/nave.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nave"
-    sha256 cellar: :any_skip_relocation, mojave: "617b3d4825d3fa11020eb73f5bdcd4ee5b2844b5bab084ef7ed821c588a32fac"
+    sha256 cellar: :any_skip_relocation, mojave: "1f245fcfdd7109a0939d1cb82a854407ac78bedb2bf140966b0d736c69e9a9a4"
   end
 
   def install
