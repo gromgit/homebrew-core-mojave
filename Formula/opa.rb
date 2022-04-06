1afb4e2259f0ee7a7563a431180b002b760107c8
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 16:13:30 +0800

    opa: update 0.38.1 bottle.

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -1,14 +1,14 @@
 class Opa < Formula
   desc "Open source, general-purpose policy engine"
   homepage "https://www.openpolicyagent.org"
-  url "https://github.com/open-policy-agent/opa/archive/v0.37.2.tar.gz"
-  sha256 "a885d219b53b3ed0a04a6b3bd09050ee1a13ba50189e0b6779c7768b74b6f137"
+  url "https://github.com/open-policy-agent/opa/archive/v0.38.1.tar.gz"
+  sha256 "829234286a44078d70e646d307740f8d44a1d72b9304406519d1c7332398f15d"
   license "Apache-2.0"
   head "https://github.com/open-policy-agent/opa.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/opa"
-    sha256 cellar: :any_skip_relocation, mojave: "4ea7eabecb982f08b1232f461a46fcd571a775b45bfbb5515285ad86e5dce3cd"
+    sha256 cellar: :any_skip_relocation, mojave: "3d0b36f7ab95c8e60ba09e69bca909e1969bb3cca902d14362e49376a7de97c5"
   end
 
   depends_on "go" => :build
