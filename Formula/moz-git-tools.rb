e6b7c786ba6652a3f75745aba32c08daf512dcbc
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 09:47:59 +0800

    moz-git-tools: update 0.1 bottle.

--- Formula/moz-git-tools.rb
+++ Formula/moz-git-tools.rb
@@ -5,18 +5,12 @@ class MozGitTools < Formula
       tag:      "v0.1",
       revision: "cfe890e6f81745c8b093b20a3dc22d28f9fc0032"
   license "GPL-2.0"
-  head "https://github.com/mozilla/moz-git-tools.git"
+  head "https://github.com/mozilla/moz-git-tools.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "99c55678ff5156827bc42fe79089d693c196375a77872c071e65dd9c295b308e"
-    sha256 cellar: :any_skip_relocation, big_sur:       "8277513f9379b1f04919d70ab3998f15a4b7003ecf5a370ce6ffdeb3ec60f813"
-    sha256 cellar: :any_skip_relocation, catalina:      "2bd3c22ef9b16601e84d060db320ca9f0a3ad8713a0a8a1274446ea35c418d0c"
-    sha256 cellar: :any_skip_relocation, mojave:        "101a581f5a39b97b8e6742bfe6d3eff742c590427ca07c768751376530bcb54f"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "0901261be02f9a82cd6ab1b287160e047c4160d81a443f4edc0a7326fdf08a6d"
-    sha256 cellar: :any_skip_relocation, sierra:        "7a771b0e71a44dafd3fc4eb2210f909d412f9ea541a7ff50a96ce272204cc501"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "c5ddb2e842a6fb26ba5feacdee6bac287d94732abd888bd11bc5c80be4f100a4"
-    sha256 cellar: :any_skip_relocation, yosemite:      "91f89ec1014d6c7b395571210c0f21b1e701f4bfb90540a94fa3daafd4472d3b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "01829ab80b5413ab1328753946c6910fe0bfe7e9153bef06efbbd6b457e3b66f"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/moz-git-tools"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "e4d8a95f59fbc7cf184cdf370c9ac96c30c53c5a1171eb273d1c260d0a6f6f66"
   end
 
   def install
