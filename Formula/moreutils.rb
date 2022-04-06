528a84f47168094b82d54e054ed8fe99e4734991
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-06 09:23:42 +0800

    moreutils: update 0.67 bottle.

--- Formula/moreutils.rb
+++ Formula/moreutils.rb
@@ -2,22 +2,17 @@ class Moreutils < Formula
   desc "Collection of tools that nobody wrote when UNIX was young"
   homepage "https://joeyh.name/code/moreutils/"
   url "https://git.joeyh.name/git/moreutils.git",
-      tag:      "0.66",
-      revision: "f0642d6331e89ca5a6ced8c0f1744428983e1780"
+      tag:      "0.67",
+      revision: "ee8e013cd5398c5fb34fb19a24db0f3b6185bac3"
   license all_of: [
     "GPL-2.0-or-later",
     { any_of: ["GPL-2.0-only", "Artistic-2.0"] },
   ]
-  head "https://git.joeyh.name/git/moreutils.git"
+  head "https://git.joeyh.name/git/moreutils.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c93055c0a353c93a20b0421a85eb055cc54340d1793cf12dd0fad275be123b71"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b7bb6a5498e898c303cbbb526f83a12e690315c43260ebab1cf348ecb2a712bb"
-    sha256 cellar: :any_skip_relocation, monterey:       "4d5c2460a0d242df46376e618207038722ffc65a9e3e6379fdd229ce821f4d52"
-    sha256 cellar: :any_skip_relocation, big_sur:        "98e36f509fe2556660dec088c1dcf380cf1ce60167fcb4289782614feb381821"
-    sha256 cellar: :any_skip_relocation, catalina:       "e7774183139434a9f4707a5931793fb2aed0678fd0257a5860571f320f507c8e"
-    sha256 cellar: :any_skip_relocation, mojave:         "099c5b0cc96ae204c211da65446db79bf83469ed0bcbc40d81e5a9e95b5a678c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0890cc88f2e311ec01cc0439cbfa6a7a43166ebaeb54a8bfa9d22fc21fba4bb4"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/moreutils"
+    sha256 cellar: :any_skip_relocation, mojave: "3a8129953d482fdd017107177a5da77dacb18ef24a2d5ab237042da6653d919e"
   end
 
   depends_on "docbook-xsl" => :build
