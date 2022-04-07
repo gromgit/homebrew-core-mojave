f592c712b9443ae94862ae5b591b7fd59032229a
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-07 20:36:06 +0800

    s-search: update 0.6.6 bottle.

--- Formula/s-search.rb
+++ Formula/s-search.rb
@@ -1,14 +1,14 @@
 class SSearch < Formula
   desc "Web search from the terminal"
   homepage "https://github.com/zquestz/s"
-  url "https://github.com/zquestz/s/archive/v0.6.4.tar.gz"
-  sha256 "5829a18da40cb5a936f32240b4244d2a0543cb85dc3d5e013ae5aa08557c148f"
+  url "https://github.com/zquestz/s/archive/v0.6.6.tar.gz"
+  sha256 "d91c8d2935f55dc6f241b7abc0325863846bdeac07a8f3bfa99b4a932d20ace3"
   license "MIT"
   head "https://github.com/zquestz/s.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/s-search"
-    sha256 cellar: :any_skip_relocation, mojave: "17137a827a46d71ccecef35d02d658cef71ae0b7bc49a654c5447e87e8ef0030"
+    sha256 cellar: :any_skip_relocation, mojave: "d21aefee749d82bb18425e17da8f7e01508f1f52b8d440b7308c6f975db6e99e"
   end
 
   depends_on "go" => :build
