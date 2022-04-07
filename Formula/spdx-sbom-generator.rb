e6d50beae1ab811bccdf4e4cdaab29d571a0e5d4
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 02:36:20 +0800

    spdx-sbom-generator: update 0.0.13_1 bottle.

--- Formula/spdx-sbom-generator.rb
+++ Formula/spdx-sbom-generator.rb
@@ -1,17 +1,18 @@
 class SpdxSbomGenerator < Formula
   desc "Support CI generation of SBOMs via golang tooling"
-  homepage "https://github.com/spdx/spdx-sbom-generator"
-  url "https://github.com/spdx/spdx-sbom-generator/archive/refs/tags/v0.0.13.tar.gz"
+  homepage "https://github.com/opensbom-generator/spdx-sbom-generator"
+  url "https://github.com/opensbom-generator/spdx-sbom-generator/archive/refs/tags/v0.0.13.tar.gz"
   sha256 "7d088f136a53d1f608b1941362c568d78cc6279df9c1bdb3516de075cb7f10c3"
   license any_of: ["Apache-2.0", "CC-BY-4.0"]
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/spdx-sbom-generator"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "90f4c59993a3ca6c24264d32bc4a56ccf4529f39a291abff2cc133af4c04f097"
+    sha256 cellar: :any_skip_relocation, mojave: "8f0dbb391e8ca1b88d280db3b8b37a195d1b0cd42b75a571dfd7d594424559e3"
   end
 
-  depends_on "go" => [:build, :test]
+  # Bump to 1.18 on the next release, if possible.
+  depends_on "go@1.17" => [:build, :test]
 
   def install
     target = if Hardware::CPU.arm?
@@ -24,11 +25,13 @@ class SpdxSbomGenerator < Formula
 
     system "make", target
 
-    bin.install "bin/spdx-sbom-generator"
+    exe = "spdx-sbom-generator"
+    (libexec/"bin").install "bin/#{exe}"
+    (bin/exe).write_env_script(libexec/"bin/#{exe}", PATH: "$PATH:#{Formula["go@1.17"].opt_bin}")
   end
 
   test do
-    system "go", "mod", "init", "example.com/tester"
+    system Formula["go@1.17"].opt_bin/"go", "mod", "init", "example.com/tester"
 
     assert_equal "panic: runtime error: index out of range [0] with length 0",
                  shell_output("#{bin}/spdx-sbom-generator 2>&1", 2).split("\n")[3]
