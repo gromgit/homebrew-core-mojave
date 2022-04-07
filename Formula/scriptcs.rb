9dd24f16f88fdd561ee5f6cf00896018cde66230
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-07 21:46:22 +0800

    scriptcs: update 0.17.1 bottle.

--- Formula/scriptcs.rb
+++ Formula/scriptcs.rb
@@ -6,17 +6,11 @@ class Scriptcs < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:    "630be0cd09a3dc16e8c7404875adfe89d009ce9a73f6d5dcae9700253a3c110c"
-    sha256 cellar: :any_skip_relocation, big_sur:     "9153f24a59a73f78f74915a8e1d8de488ec016deb1aac1825774e19d6420545d"
-    sha256 cellar: :any_skip_relocation, catalina:    "c14c80f7b24ac5f2c14119ec2f371416ae07b9fba8bd9b0eb90d57ef5104f765"
-    sha256 cellar: :any_skip_relocation, mojave:      "121137df4078b2819a16f0f3e75924b10eba51a3ca7ac0a4be3d9010d2d1f7aa"
-    sha256 cellar: :any_skip_relocation, high_sierra: "263fda7addb857a9ed3c0c15856c422d3684ad069c2efc644858bb1779a92e91"
-    sha256 cellar: :any_skip_relocation, sierra:      "9ccece2f779060ab23e699b07d6cc6ce0b2c2e0058cc995b1541e1170f69a6eb"
-    sha256 cellar: :any_skip_relocation, el_capitan:  "e3b6cb117d23ccf9a745e0ac5e61fcb531d7e8a08476699d2ece6c31e564450e"
-    sha256 cellar: :any_skip_relocation, yosemite:    "21891cea519df48979320ba74660002d270fb414181e3f7087505169af15a471"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/scriptcs"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "25b02d1fea63e74c038531cddd88fd84c0646773df37ab672b52c6f996f3b500"
   end
 
-  depends_on arch: :x86_64 # mono is not yet supported on ARM
   depends_on "mono"
 
   def install
