3cae58b178ab11274bcd10ee5cb971ea5aef1abd
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-07 14:42:10 +0800

    qrcp: update 0.9.1 bottle.

--- Formula/qrcp.rb
+++ Formula/qrcp.rb
@@ -1,21 +1,17 @@
 class Qrcp < Formula
   desc "Transfer files to and from your computer by scanning a QR code"
   homepage "https://claudiodangelis.com/qrcp"
-  url "https://github.com/claudiodangelis/qrcp/archive/0.8.4.tar.gz"
-  sha256 "b77673bad880c9ffec1fa20cef6e46ae717702edd95bca3076919225e396db57"
+  url "https://github.com/claudiodangelis/qrcp/archive/0.9.1.tar.gz"
+  sha256 "1ee0d1b04222fb2a559d412b144a49051c3315cbc99c7ea1f281bdd4f13f07bf"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a17126d0da69e86af27da5a5f519074c57fba00c3587895b802d2b862022123b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8286ab8384850578862e84cceebb7aa8efc5d2bafd1d372a0e9e1f6930a7a726"
-    sha256 cellar: :any_skip_relocation, monterey:       "3e3a7ccb0226a080b8d0ef18e85d5e05c9aac9de20ace9c2b409624d475df7cf"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b90c0f156ce21a2ec39f23405fd1213008d96a8e1d62f17cd0d5c83c17b055ea"
-    sha256 cellar: :any_skip_relocation, catalina:       "954c095b0cd034464bd929a3b28855f932d09e0cb59a741939009221f6fdac94"
-    sha256 cellar: :any_skip_relocation, mojave:         "a319771dea04ac593d2928e8e937ce1fd5a7c55c67999e9505b258cbddbe226c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "564e4415f63d446ce140cbb3baccae5902ff4be08b041ceb8bc028c2c90376e8"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/qrcp"
+    sha256 cellar: :any_skip_relocation, mojave: "51086beb5255c0ca194277c996ee62b8997f157187b12bdae6ccce356bfc80ea"
   end
 
-  depends_on "go" => :build
+  # Bump to 1.18 on the next release, if possible.
+  depends_on "go@1.17" => :build
 
   def install
     system "go", "build", *std_go_args
