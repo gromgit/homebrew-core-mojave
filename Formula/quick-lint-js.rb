b21878964122d0d51ba0e70007af413df023eca9
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-07 15:50:25 +0800

    quick-lint-js: update 2.3.1 bottle.

---
+++ Formula/quick-lint-js.rb
@@ -0,0 +1,64 @@
+class QuickLintJs < Formula
+  desc "Find bugs in your JavaScript code"
+  homepage "https://quick-lint-js.com/"
+  url "https://c.quick-lint-js.com/releases/2.3.1/source/quick-lint-js-2.3.1.tar.gz"
+  sha256 "ed13ec8a74f88bd6c981a41704dea67b98062ced45d21634e2bfb501dfa917be"
+  license "GPL-3.0-or-later"
+  head "https://github.com/quick-lint/quick-lint-js.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/quick-lint-js"
+    sha256 cellar: :any, mojave: "8446cd66d9c4f6752e4ccef41cbb15ef551316c46ccbbd18fef8f613317745e4"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "googletest" => :build
+  depends_on "pkg-config" => :build
+  depends_on "boost"
+  depends_on "simdjson"
+
+  on_linux do
+    # Use Homebrew's C++ compiler in case the host's C++
+    # compiler is too old.
+    depends_on "gcc"
+  end
+
+  # quick-lint-js requires some C++17 features, thus
+  # requires GCC 8 or newer.
+  fails_with gcc: "5"
+  fails_with gcc: "6"
+  fails_with gcc: "7"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DBUILD_TESTING=ON",
+                    "-DQUICK_LINT_JS_ENABLE_BENCHMARKS=OFF",
+                    "-DQUICK_LINT_JS_INSTALL_EMACS_DIR=#{elisp}",
+                    "-DQUICK_LINT_JS_INSTALL_VIM_NEOVIM_TAGS=ON",
+                    "-DQUICK_LINT_JS_USE_BUNDLED_BOOST=OFF",
+                    "-DQUICK_LINT_JS_USE_BUNDLED_GOOGLE_BENCHMARK=OFF",
+                    "-DQUICK_LINT_JS_USE_BUNDLED_GOOGLE_TEST=OFF",
+                    "-DQUICK_LINT_JS_USE_BUNDLED_SIMDJSON=OFF",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    chdir "build" do
+      system "ctest", "-V"
+    end
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"errors.js").write <<~EOF
+      const x = 3;
+      const x = 4;
+    EOF
+    ohai "#{bin}/quick-lint-js errors.js"
+    output = `#{bin}/quick-lint-js errors.js 2>&1`
+    puts output
+    refute_equal $CHILD_STATUS.exitstatus, 0
+    assert_match "E0034", output
+
+    (testpath/"no-errors.js").write 'console.log("hello, world!");'
+    assert_empty shell_output("#{bin}/quick-lint-js no-errors.js")
+  end
+end
