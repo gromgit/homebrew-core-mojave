624de35ed3aed79bd71c2e077e8534f38a375b6f
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-07 14:32:22 +0800

    qpdf: update 10.6.3 bottle.

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -1,13 +1,13 @@
 class Qpdf < Formula
   desc "Tools for and transforming and inspecting PDF files"
   homepage "https://github.com/qpdf/qpdf"
-  url "https://github.com/qpdf/qpdf/releases/download/release-qpdf-10.6.2/qpdf-10.6.2.tar.gz"
-  sha256 "4b8c966300fcef32352f6576b7ef40167e080e43fe8954b12ef80b49a7e5307f"
+  url "https://github.com/qpdf/qpdf/releases/download/release-qpdf-10.6.3/qpdf-10.6.3.tar.gz"
+  sha256 "e8fc23b2a584ea68c963a897515d3eb3129186741dd19d13c86d31fa33493811"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/qpdf"
-    sha256 cellar: :any, mojave: "db82b5f59743beeda6457af2899daed2f49a81504ef3669cbdd91e9360af03b4"
+    sha256 cellar: :any, mojave: "70d509d8a3f6531b1ddb81e75b8667e2963847f80d9c87602618fc7082a5395e"
   end
 
   depends_on "jpeg"
