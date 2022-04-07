e4396bef99413bf7e6a1fe56ab7131c9c7307b04
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 03:27:30 +0800

    sqlite-analyzer: update 3.38.2 bottle.

--- Formula/sqlite-analyzer.rb
+++ Formula/sqlite-analyzer.rb
@@ -1,9 +1,9 @@
 class SqliteAnalyzer < Formula
   desc "Analyze how space is allocated inside an SQLite file"
   homepage "https://www.sqlite.org/"
-  url "https://sqlite.org/2022/sqlite-src-3380000.zip"
-  version "3.38.0"
-  sha256 "77c53d4812cad7856f5f1a1e07d2a2c2a3444d26a965274279f8dd9faa6ff480"
+  url "https://sqlite.org/2022/sqlite-src-3380200.zip"
+  version "3.38.2"
+  sha256 "c7c0f070a338c92eb08805905c05f254fa46d1c4dda3548a02474f6fb567329a"
   license "blessing"
 
   livecheck do
@@ -12,7 +12,7 @@ class SqliteAnalyzer < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sqlite-analyzer"
-    sha256 cellar: :any_skip_relocation, mojave: "94f7690a9bc499b2c6250885a7cd89b76be71fd0d274a6eb72d55ac130885335"
+    sha256 cellar: :any_skip_relocation, mojave: "3a0bc72a97a97a4db94020dcb08d52a64b4e865ee266a1502fb50237d61945f4"
   end
 
   uses_from_macos "sqlite" => :test
