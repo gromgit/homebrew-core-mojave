b6315930fa01a6d494da0b2fe784c428bf8e29fd
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 00:31:40 +0800

    sniffglue: update 0.15.0 bottle.

--- Formula/sniffglue.rb
+++ Formula/sniffglue.rb
@@ -1,17 +1,17 @@
 class Sniffglue < Formula
   desc "Secure multithreaded packet sniffer"
   homepage "https://github.com/kpcyrd/sniffglue"
-  url "https://github.com/kpcyrd/sniffglue/archive/v0.14.0.tar.gz"
-  sha256 "9c3347603550349c9686639f81a933eb12bcc76efa6c46c37888cd2aaf785e39"
+  url "https://github.com/kpcyrd/sniffglue/archive/v0.15.0.tar.gz"
+  sha256 "ac30c0748a4247d2a36b82d623e88863480c300d3f6bbbdc303077240a8292c5"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sniffglue"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "1d5087e93ac29dcd7980aeecfca1650df768d4d5814edc1d298b81205a7cfaad"
+    sha256 cellar: :any_skip_relocation, mojave: "e4bb1dd4473b49cedc7b406c1ff35cf2dcaef41f9bfacb781a6f545a94a2f0a5"
   end
 
   depends_on "rust" => :build
+  depends_on "scdoc" => :build
 
   uses_from_macos "libpcap"
 
@@ -19,20 +19,21 @@ class Sniffglue < Formula
     depends_on "libseccomp"
   end
 
-  resource "testdata" do
+  resource "homebrew-testdata" do
     url "https://github.com/kpcyrd/sniffglue/raw/163ca299bab711fb0082de216d07d7089c176de6/pcaps/SkypeIRC.pcap"
     sha256 "bac79a9c3413637f871193589d848697af895b7f2700d949022224d59aa6830f"
   end
 
   def install
     system "cargo", "install", *std_cargo_args
+    system "make", "docs"
 
     etc.install "sniffglue.conf"
     man1.install "docs/sniffglue.1"
   end
 
   test do
-    testpath.install resource("testdata")
+    testpath.install resource("homebrew-testdata")
     system bin/"sniffglue", "-r", "SkypeIRC.pcap"
   end
 end
