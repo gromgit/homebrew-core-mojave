aa2c4e45f57dc844fb852f1505e7f766ab4324cd
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-07 16:25:51 +0800

    rancher-cli: update 2.6.4 bottle.

--- Formula/rancher-cli.rb
+++ Formula/rancher-cli.rb
@@ -1,8 +1,8 @@
 class RancherCli < Formula
   desc "Unified tool to manage your Rancher server"
   homepage "https://github.com/rancher/cli"
-  url "https://github.com/rancher/cli/archive/v2.6.0.tar.gz"
-  sha256 "6821730df16cc1c5b012ee0be54f480de70b373a6b95379b6bbf99243aedad5b"
+  url "https://github.com/rancher/cli/archive/v2.6.4.tar.gz"
+  sha256 "9b9eaeabcfdad7b4f47dd558f104db3cc50407743a87ffa57233091ccb759278"
   license "Apache-2.0"
   head "https://github.com/rancher/cli.git", branch: "master"
 
@@ -13,7 +13,7 @@ class RancherCli < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rancher-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "013159c8837fb9bf68b71b3550aa7634cd81a215977e5a9e1ea9e005aa48e9d3"
+    sha256 cellar: :any_skip_relocation, mojave: "b11eb469dec656bd8de0f3f07f3d46539f2e25fcaa04e562cc9fd9920b20f48d"
   end
 
   depends_on "go" => :build
