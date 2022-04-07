f17335b9d2481eca72501e74a8c4664332a8adfb
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-07 17:00:49 +0800

    regula: update 2.6.1 bottle.

--- Formula/regula.rb
+++ Formula/regula.rb
@@ -2,14 +2,14 @@ class Regula < Formula
   desc "Checks infrastructure as code templates using Open Policy Agent/Rego"
   homepage "https://regula.dev/"
   url "https://github.com/fugue/regula.git",
-      tag:      "v2.5.0",
-      revision: "4685bf74c08a4563a8550754f4ebfe861c7830cb"
+      tag:      "v2.6.1",
+      revision: "0d40e66c4bec929779b8bb5cde5f22ed1e522ede"
   license "Apache-2.0"
   head "https://github.com/fugue/regula.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/regula"
-    sha256 cellar: :any_skip_relocation, mojave: "955a959acb3185faae08cbf16f398cacc598449302a0a40ea1af9a08db146cd7"
+    sha256 cellar: :any_skip_relocation, mojave: "a7aaed4a7085718dee3db24c7d15191d0f0a31d60bb6bff0be8d08cdd7245324"
   end
 
   depends_on "go" => :build
@@ -17,8 +17,8 @@ class Regula < Formula
   def install
     ldflags = %W[
       -s -w
-      -X github.com/fugue/regula/pkg/version.Version=#{version}
-      -X github.com/fugue/regula/pkg/version.GitCommit=#{Utils.git_short_head}
+      -X github.com/fugue/regula/v2/pkg/version.Version=#{version}
+      -X github.com/fugue/regula/v2/pkg/version.GitCommit=#{Utils.git_short_head}
     ].join(" ")
 
     system "go", "build", *std_go_args(ldflags: ldflags)
@@ -45,7 +45,7 @@ class Regula < Formula
       }
     EOS
 
-    assert_match "Found 8 problems", shell_output(bin/"regula run infra", 1)
+    assert_match "Found 10 problems", shell_output(bin/"regula run infra", 1)
 
     assert_match version.to_s, shell_output(bin/"regula version")
   end
