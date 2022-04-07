de9610cc873d679c2bd524160d82b6956bcb1d84
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-07 18:15:25 +0800

    rollup: update 2.70.1 bottle.

--- Formula/rollup.rb
+++ Formula/rollup.rb
@@ -3,13 +3,13 @@ require "language/node"
 class Rollup < Formula
   desc "Next-generation ES module bundler"
   homepage "https://rollupjs.org/"
-  url "https://registry.npmjs.org/rollup/-/rollup-2.68.0.tgz"
-  sha256 "1df95469d1d48515da6c5b842a4f3c0eaebdb5a08f5c59a3282abb7dca19ee08"
+  url "https://registry.npmjs.org/rollup/-/rollup-2.70.1.tgz"
+  sha256 "60f7491cf2d28475aff1273297fd5c5dfdb55684abcfe4c43a43986321b7136c"
   license all_of: ["ISC", "MIT"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rollup"
-    sha256 cellar: :any_skip_relocation, mojave: "08cf9681c1485384780f01bf59989e2de69ac30574568e5801872f5dbd734469"
+    sha256 cellar: :any_skip_relocation, mojave: "333b5934156a082bcbf5138ef1b24d817fa52d01181175ccaf21911acd641ce2"
   end
 
   depends_on "node"
