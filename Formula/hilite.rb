80e576e95d4c85d9d20765c24e13082a74137f56
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-09 00:48:57 +0800

    hilite: no rebuilding required

--- Formula/hilite.rb
+++ Formula/hilite.rb
@@ -3,6 +3,7 @@ class Hilite < Formula
   homepage "https://sourceforge.net/projects/hilite/"
   url "https://downloads.sourceforge.net/project/hilite/hilite/1.5/hilite.c"
   sha256 "e15bdff2605e8d23832d6828a62194ca26dedab691c9d75df2877468c2f6aaeb"
+  license "GPL-2.0-or-later"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4f3e960b26ed8e81acab99dd342d1e2946b1ec8003fb99dd65b91add5af31c93"
@@ -15,10 +16,11 @@ class Hilite < Formula
     sha256 cellar: :any_skip_relocation, sierra:         "b4bab9fd50310b7401d2605b9a9cdad0a8d7069e5b477d5f53fef61847bea624"
     sha256 cellar: :any_skip_relocation, el_capitan:     "2c407d12952089ade6602be85acda46eceb3127e32ba2068c0034df8a486e989"
     sha256 cellar: :any_skip_relocation, yosemite:       "1eea4240f83568d245aa55801949cd9deb4663fc26df75569e029cc1c4c14112"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "45f0c131886b8932ee87061c612877ff71fa38650e81340eab5694de95e6cb40"
   end
 
   def install
-    system ENV.cc, ENV.cflags, "hilite.c", "-o", "hilite"
+    system ENV.cc, "hilite.c", "-o", "hilite", *ENV.cflags.to_s.split
     bin.install "hilite"
   end
 
