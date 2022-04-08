01e503678548152f0aad5af09007a0a9bd8e8cd5
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-09 00:49:56 +0800

    xrick: no rebuilding required

--- Formula/xrick.rb
+++ Formula/xrick.rb
@@ -8,13 +8,18 @@ class Xrick < Formula
 
   bottle do
     rebuild 2
-    sha256 arm64_big_sur: "06c5d5c23d3a83ae82c8efa8303c186cb8b07f2f274b911062c0a56df4207e96"
-    sha256 big_sur:       "07143daa9065782675e5288a66dc2ea8ce418754d4842175b016dbc097b982cd"
-    sha256 catalina:      "67d2a811a5ecd67de860f226fc88feec5ed479ae05fc22e917d6fbe0686be783"
-    sha256 mojave:        "1a2f4d0cc564fd627d83f2b54606c59845b50fb3db04e7a118ca281377b4805a"
+    sha256 arm64_monterey: "7030fd13f0c9ff2779deb3d3c80437d3b9903dd494928fc3aa1237b98598baa0"
+    sha256 arm64_big_sur:  "06c5d5c23d3a83ae82c8efa8303c186cb8b07f2f274b911062c0a56df4207e96"
+    sha256 monterey:       "39454803d7aad7a03e382ffa547f5b8a77f6af1e939f3edcd092e56c493d95fb"
+    sha256 big_sur:        "07143daa9065782675e5288a66dc2ea8ce418754d4842175b016dbc097b982cd"
+    sha256 catalina:       "67d2a811a5ecd67de860f226fc88feec5ed479ae05fc22e917d6fbe0686be783"
+    sha256 mojave:         "1a2f4d0cc564fd627d83f2b54606c59845b50fb3db04e7a118ca281377b4805a"
+    sha256 x86_64_linux:   "d75168c2036f46eac7dec382ad49aadce2df5253df37f94a5f47a286d3810904"
   end
 
   depends_on "sdl"
+
+  uses_from_macos "zlib"
 
   def install
     inreplace "src/xrick.c", "data.zip", "#{pkgshare}/data.zip"
