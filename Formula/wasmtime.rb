95d1d2ec75d87c3a6ac5088271f8acd05d9475fa
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 16:01:28 +0800

    wasmtime: update 0.35.1 bottle.

--- Formula/wasmtime.rb
+++ Formula/wasmtime.rb
@@ -2,8 +2,8 @@ class Wasmtime < Formula
   desc "Standalone JIT-style runtime for WebAssembly, using Cranelift"
   homepage "https://wasmtime.dev/"
   url "https://github.com/bytecodealliance/wasmtime.git",
-      tag:      "v0.34.1",
-      revision: "44c08532721a923f86b6a365308028230c7d103c"
+      tag:      "v0.35.1",
+      revision: "5b09b74f4c5e0fd817febd3263947ee3682759bd"
   license "Apache-2.0" => { with: "LLVM-exception" }
   head "https://github.com/bytecodealliance/wasmtime.git", branch: "main"
 
@@ -14,7 +14,7 @@ class Wasmtime < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/wasmtime"
-    sha256 cellar: :any_skip_relocation, mojave: "ac7c4b59868eb8c6f5e3885b4b1bff9d911ba2d5c64bce3d67d4edec1986611a"
+    sha256 cellar: :any_skip_relocation, mojave: "2c53a038021823f718be4a0d1298b29ba7cc05180cc8d354c8300e56d2b5878b"
   end
 
   depends_on "rust" => :build
