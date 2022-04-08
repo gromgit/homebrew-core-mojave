cc4acdb105fb6d224b159e1f3b02836cb0b28505
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-09 00:48:53 +0800

    greed: no rebuilding required

--- Formula/greed.rb
+++ Formula/greed.rb
@@ -11,11 +11,18 @@ class Greed < Formula
     regex(/href=.*?greed[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
-bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/greed"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "cd46806e376ebc2a1fb1ac08aa4f595e91af9984a3cdf46e535243e8fcc3bba2"
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "7e742fb2edc5d957f895a9843bb3432f0b965f582e53bf315ac6a1eec2c3cd78"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "197eaf64e266d04b451278067451a05271ee348e04c860c360212f22e0a22cd2"
+    sha256 cellar: :any_skip_relocation, monterey:       "f63c247592d7a30dfe5683919c96bc572b4841695358b94627895deb7e48dfa5"
+    sha256 cellar: :any_skip_relocation, big_sur:        "15791321c59787d6e5b633efb195e0b7edf8c92976d5c6991e12a920a9f46a00"
+    sha256 cellar: :any_skip_relocation, catalina:       "64d0028754d683a8bbe1de0bb1a7319dcf6d8020c6d3624e58df5b5be3bf4e42"
+    sha256 cellar: :any_skip_relocation, mojave:         "9cba951e4fd73d29a1e4899a4f2a7d5f0158f6f5b6d02bb75837c7296530e65c"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "9685dcc52ad08b19964cfb61f4fd0d9e28ec0d42cde2f112da4e9be1e1d15b5b"
   end
+
+  uses_from_macos "ncurses"
 
   def install
     # Handle hard-coded destination
