8853a69c9b6bedfa49ae31b09db06385367f0b11
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-09 00:48:39 +0800

    docfx: no rebuilding required

--- Formula/docfx.rb
+++ Formula/docfx.rb
@@ -1,12 +1,12 @@
 class Docfx < Formula
   desc "Tools for building and publishing API documentation for .NET projects"
   homepage "https://dotnet.github.io/docfx/"
-  url "https://github.com/dotnet/docfx/releases/download/v2.59.0/docfx.zip"
-  sha256 "e6bd6d788ddb07d9bcb6d90f2822d7e2e6c4feb0e2caeabc60ff39232d07bc52"
+  url "https://github.com/dotnet/docfx/releases/download/v2.59.1/docfx.zip"
+  sha256 "b1f5446ac3f405969505a26fc58ab7f4c8ba7f620447650f386f2ee86aeda0b5"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "9c6025656fe2c6df716b216976427cc0c768ff31cbdd1c36c0d3a7d8777357a6"
+    sha256 cellar: :any_skip_relocation, all: "280a36a298319b3eb2314f4dc2f1d5c80fde6e53b875bc61530b12d2ec76e02d"
   end
 
   depends_on "mono"
