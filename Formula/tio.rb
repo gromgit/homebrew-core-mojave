1146696bc1cf9c6a70cc8bcc326b5eac7f5aa016
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 09:33:06 +0800

    tio: update 1.36 bottle.

--- Formula/tio.rb
+++ Formula/tio.rb
@@ -1,17 +1,19 @@
 class Tio < Formula
   desc "Simple TTY terminal I/O application"
   homepage "https://tio.github.io"
-  url "https://github.com/tio/tio/releases/download/v1.35/tio-1.35.tar.xz"
-  sha256 "1309ecde7675f4e97cf363a8ab96ff668e14ab3f2176a15b6d626731251c9d09"
+  url "https://github.com/tio/tio/releases/download/v1.36/tio-1.36.tar.xz"
+  sha256 "4a73ddfceed9851944e651e21a4f45a0526f15585a26420f2afef0283b7c477c"
   license "GPL-2.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tio"
-    sha256 cellar: :any_skip_relocation, mojave: "c284030f31a6015122ffe8cc5f778dfd0e18fcca94fa891cee09847b91577647"
+    sha256 cellar: :any, mojave: "072fe37fb626642fd0996e1e2ef1b3ffba9c9ad1c60342377e1f6e66cc26c332"
   end
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "inih"
 
   def install
     mkdir "build" do
@@ -24,12 +26,12 @@ class Tio < Formula
   test do
     # Test that tio emits the correct error output when run with an argument that is not a tty.
     # Use `script` to run tio with its stdio attached to a PTY, otherwise it will complain about that instead.
-    test_str = "Error: Not a tty device"
-    on_macos do
-      assert_match test_str, shell_output("script -q /dev/null #{bin}/tio /dev/null", 1).strip
+    expected = "Error: Not a tty device"
+    output = if OS.mac?
+      shell_output("script -q /dev/null #{bin}/tio /dev/null", 1).strip
+    else
+      shell_output("script -q /dev/null -e -c \"#{bin}/tio /dev/null\"", 1).strip
     end
-    on_linux do
-      assert_match test_str, shell_output("script -q /dev/null -e -c \"#{bin}/tio /dev/null\"", 1).strip
-    end
+    assert_match expected, output
   end
 end
