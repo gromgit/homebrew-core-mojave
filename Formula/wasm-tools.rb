c24c56ad7bdf6e20d688a6c0a6f8743d044519b4
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 15:50:57 +0800

    wasm-tools: update 1.0.3 bottle.

---
+++ Formula/wasm-tools.rb
@@ -0,0 +1,41 @@
+class WasmTools < Formula
+  desc "Low level tooling for WebAssembly in Rust"
+  homepage "https://github.com/bytecodealliance/wasm-tools"
+  url "https://github.com/bytecodealliance/wasm-tools/archive/refs/tags/wasm-tools-1.0.3.tar.gz"
+  sha256 "95684121c3f8ab8de5b46b754a5093a675d56cbfd90fafb23c344f1865964c3d"
+  license "Apache-2.0" => { with: "LLVM-exception" }
+  head "https://github.com/bytecodealliance/wasm-tools.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^wasm-tools[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/wasm-tools"
+    sha256 cellar: :any_skip_relocation, mojave: "c99c47b2c82c743eae29bb059391c20397c1ff339f19c7a556d08936acb4f724"
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    wasm = ["0061736d0100000001070160027f7f017f030201000707010373756d00000a09010700200020016a0b"].pack("H*")
+    (testpath/"sum.wasm").write(wasm)
+    system bin/"wasm-tools", "validate", testpath/"sum.wasm"
+
+    expected = <<~EOS
+      (module
+        (type (;0;) (func (param i32 i32) (result i32)))
+        (func (;0;) (type 0) (param i32 i32) (result i32)
+          local.get 0
+          local.get 1
+          i32.add)
+        (export "sum" (func 0)))
+    EOS
+    assert_equal expected, shell_output("#{bin}/wasm-tools print #{testpath}/sum.wasm")
+  end
+end
