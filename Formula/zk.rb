abe2be2b47507fc1fae020430bf21c685792e992
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 19:34:44 +0800

    zk: update 0.9.0_1 bottle.

--- Formula/zk.rb
+++ Formula/zk.rb
@@ -4,12 +4,13 @@ class Zk < Formula
   url "https://github.com/mickael-menu/zk/archive/refs/tags/v0.9.0.tar.gz"
   sha256 "102754f2722e7e4ff0a83715085c8bfdac090b440f89020123481a95b566730f"
   license "GPL-3.0-only"
+  revision 1
 
   head "https://github.com/mickael-menu/zk.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/zk"
-    sha256 cellar: :any, mojave: "9a6aa05d6b59f77b0da2ff6b6182e6e281aaf2d03c18d8d212d63c8d070fae8c"
+    sha256 cellar: :any, mojave: "2342fbd864798903fb62e1aa85008f8c1e7524e33cddd641a6225c5f585c6c29"
   end
 
   depends_on "go" => :build
