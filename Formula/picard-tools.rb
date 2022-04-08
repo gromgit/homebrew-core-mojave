a0de5b4a58df795674b4f6a3e56374ab33121823
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-09 00:49:26 +0800

    picard-tools: no rebuilding required

--- Formula/picard-tools.rb
+++ Formula/picard-tools.rb
@@ -1,8 +1,8 @@
 class PicardTools < Formula
   desc "Tools for manipulating HTS data and formats"
   homepage "https://broadinstitute.github.io/picard/"
-  url "https://github.com/broadinstitute/picard/releases/download/2.26.11/picard.jar"
-  sha256 "d2cc20828d5ae60131a63dd581a58a4ff4d877badf48505ca91ac05119d33e33"
+  url "https://github.com/broadinstitute/picard/releases/download/2.27.1/picard.jar"
+  sha256 "87e94cebeeeaaa19dd2a5ab2d76b9ac51b007fbe5926294f9bb83b6811513152"
   license "MIT"
 
   livecheck do
@@ -11,7 +11,7 @@ class PicardTools < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "6e4c0abc33430e93dac142984df0b79d4e8f7cb2e8d7112e00a2b6ebc551417f"
+    sha256 cellar: :any_skip_relocation, all: "b8e5d0362e71097e889500b0fdd1a3e381b18cc286fe61168471c7fe88f56436"
   end
 
   depends_on "openjdk"
