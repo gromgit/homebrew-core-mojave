6ff6b35071c7e8bd2f0413a82c59dfa8084319af
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-09 00:48:37 +0800

    demumble: no rebuilding required

--- Formula/demumble.rb
+++ Formula/demumble.rb
@@ -7,11 +7,14 @@ class Demumble < Formula
   head "https://github.com/nico/demumble.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/demumble"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "746bc4c298ec94189b4ce8cc684c56499b4a384d888bb761babcda73bb30ec17"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ff3563d728d6ad3ba851fe1a9e95e599f0fb835ea6e046b30e329537f19aaedc"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b43c1b4396d845e300f9f5c7746fb6a393ea8a3189c822ba96c35cb1ddd7786b"
+    sha256 cellar: :any_skip_relocation, monterey:       "3677ec9a895d02cd6694710152625d98f903720e891774d292a5ea454d11bbc0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f7dc50c58c1188bd07b90c0ecb57031f24ad3b463f41befd78ea5b5cfd1577e0"
+    sha256 cellar: :any_skip_relocation, catalina:       "c79b46404de20da59fb13b225006147666199f6a03b56e43820d20ae818dafff"
+    sha256 cellar: :any_skip_relocation, mojave:         "f843e7116cf05d2a7dff4fb8351d0609e8120d49aad84e810f44cc85d6b62811"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "addf214742de13338bbbc8f19e8f41593bc66bcacb6870f9add57a9e00c0eca9"
   end
-
 
   depends_on "cmake" => :build
 
