fc6fea676cc38b7b8bd0619f48fe39bfc0ae06e0
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 13:59:14 +0800

    verilator: update 4.220 bottle.

--- Formula/verilator.rb
+++ Formula/verilator.rb
@@ -1,13 +1,13 @@
 class Verilator < Formula
   desc "Verilog simulator"
   homepage "https://www.veripool.org/wiki/verilator"
-  url "https://github.com/verilator/verilator/archive/refs/tags/v4.218.tar.gz"
-  sha256 "ef7b1e6ddb715ddb3cc998fcbefc7150cfa2efc5118cf43ddb594bf41ea41cc7"
+  url "https://github.com/verilator/verilator/archive/refs/tags/v4.220.tar.gz"
+  sha256 "e00e0c31a0c00887bebbaf7a8c771efa09420a4d1fbae54d45843baf50df4426"
   license any_of: ["LGPL-3.0-only", "Artistic-2.0"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/verilator"
-    sha256 mojave: "b4a8c04e16be80a82704d6b0c8b28c76b439c30c97703685fdb5221704adc7c5"
+    sha256 mojave: "10aa8917f952b44b5fef2caf8d48f15f059074e9d5f8e5958affd4d94854a10b"
   end
 
   head do
@@ -22,7 +22,14 @@ class Verilator < Formula
   uses_from_macos "flex"
   uses_from_macos "perl"
 
+  on_linux do
+    depends_on "gcc"
+  end
+
   skip_clean "bin" # Allows perl scripts to keep their executable flag
+
+  # error: specialization of 'template<class _Tp> struct std::hash' in different namespace
+  fails_with gcc: "5"
 
   def install
     system "autoconf"
