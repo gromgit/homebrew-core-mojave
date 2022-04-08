82d336baadc1fd3507129c8eabe96b6ccef3e6e2
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-09 00:49:59 +0800

    zsxd: no rebuilding required

--- Formula/zsxd.rb
+++ Formula/zsxd.rb
@@ -14,10 +14,14 @@ class Zsxd < Formula
     sha256 cellar: :any_skip_relocation, catalina:       "aabcc393aae8f00a45ffa24d959ff57a6023caace90a815f8107c579e113b87e"
     sha256 cellar: :any_skip_relocation, mojave:         "8b6e336bd61f16c620ab8323ccd15dfc35cf1665c71799a838c4436fefd561b0"
     sha256 cellar: :any_skip_relocation, high_sierra:    "fa0726547d624647bd7453100b6e2221ce0ec9174e0cd43275844b09aefb6c0d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6b29f50a29f5874e4197504acbc4bd1f0162df77d8e30152a10c4bf82befc446"
   end
 
   depends_on "cmake" => :build
   depends_on "solarus"
+
+  uses_from_macos "zip" => :build
+  uses_from_macos "unzip" => :test
 
   def install
     system "cmake", ".", *std_cmake_args, "-DSOLARUS_INSTALL_DATADIR=#{share}"
@@ -26,6 +30,6 @@ class Zsxd < Formula
 
   test do
     system Formula["solarus"].bin/"solarus-run", "-help"
-    system "/usr/bin/unzip", pkgshare/"data.solarus"
+    system "unzip", pkgshare/"data.solarus"
   end
 end
