743b31a78a06b2d438b7289e548df5f70bdaab3e
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-09 00:49:10 +0800

    ltl2ba: no rebuilding required

--- Formula/ltl2ba.rb
+++ Formula/ltl2ba.rb
@@ -5,11 +5,13 @@ class Ltl2ba < Formula
   sha256 "912877cb2929cddeadfd545a467135a2c61c507bbd5ae0edb695f8b5af7ce9af"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "577639caaf79515113fee790d593bc735b9f54bbb2bf93ae2788655a455aecaa"
-    sha256 cellar: :any_skip_relocation, big_sur:       "c85c152985dcdd33f028941a0b2e62d20d5f247142111b9fb30c11ea9dd424b7"
-    sha256 cellar: :any_skip_relocation, catalina:      "ede3b5e5b22b886bce4f6f2ead352dc4a676e3d8a95f9543930f2be2b3a0b4b4"
-    sha256 cellar: :any_skip_relocation, mojave:        "3e5ddce23730195799dfe85c97a57d63e892f168cda5207c72c68b459e5a92a0"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "533a278e70570b8f83550c784ccb7c921d9fb5b93ac613c3f971703090dd7921"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1e35ea05ee9b358df863d969c190abea48b61ec737c7aaa5fb01f9f7de455330"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "577639caaf79515113fee790d593bc735b9f54bbb2bf93ae2788655a455aecaa"
+    sha256 cellar: :any_skip_relocation, monterey:       "287ea81802bff62078478fccce3b85695feb945996f59bbf17d3e28712bb51ac"
+    sha256 cellar: :any_skip_relocation, big_sur:        "c85c152985dcdd33f028941a0b2e62d20d5f247142111b9fb30c11ea9dd424b7"
+    sha256 cellar: :any_skip_relocation, catalina:       "ede3b5e5b22b886bce4f6f2ead352dc4a676e3d8a95f9543930f2be2b3a0b4b4"
+    sha256 cellar: :any_skip_relocation, mojave:         "3e5ddce23730195799dfe85c97a57d63e892f168cda5207c72c68b459e5a92a0"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "533a278e70570b8f83550c784ccb7c921d9fb5b93ac613c3f971703090dd7921"
   end
 
   def install
