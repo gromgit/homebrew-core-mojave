19914ccf2cb83646955e07fee3325ce9fb3d9d27
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 08:54:36 +0800

    testkube: update 0.11.6 bottle.

---
+++ Formula/testkube.rb
@@ -0,0 +1,34 @@
+class Testkube < Formula
+  desc "Kubernetes-native framework for test definition and execution"
+  homepage "https://testkube.io"
+  url "https://github.com/kubeshop/testkube/archive/refs/tags/v0.11.6.tar.gz"
+  sha256 "d9c84dd62871cfa29b5c7036c73b8cc4961921c6c4bf779972d8fb3314508544"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/testkube"
+    sha256 cellar: :any_skip_relocation, mojave: "088be1b60db51de62668e0bb7f0775725943a8aec4932581be7a520400ab8675"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.builtBy=#{tap.user}
+    ]
+
+    system "go", "build", *std_go_args(output: bin/"kubectl-testkube", ldflags: ldflags),
+      "cmd/kubectl-testkube/main.go"
+  end
+
+  test do
+    output = shell_output("#{bin}/kubectl-testkube get tests 2>&1", 1)
+    assert_match("error: invalid configuration: no configuration has been provided", output)
+
+    output = shell_output("#{bin}/kubectl-testkube help")
+    assert_match("Testkube entrypoint for kubectl plugin", output)
+  end
+end
