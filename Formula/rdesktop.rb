485a5d134f34ca2f9bd2cb9bbd4da964cd586f74
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-09 00:49:32 +0800

    rdesktop: no rebuilding required

--- Formula/rdesktop.rb
+++ Formula/rdesktop.rb
@@ -7,15 +7,20 @@ class Rdesktop < Formula
   revision 2
 
   bottle do
-    sha256 monterey:    "087f1adc8db1045091b37fa769df85d5bd9cc886c8c12e7ff5d1216da6dd62ba"
-    sha256 big_sur:     "60d5a72e5a55cace788d0a28241b9080fbf039c25fd91eb3bf91a95a8e8e4a89"
-    sha256 catalina:    "8b22a2d1f52ff40334a16fc4614bc2f2c9e50386f0732e8e4478f68c7008f961"
-    sha256 mojave:      "91b95a137be4361dee7d8bf2e442fa75eaf159469c09e238a127aa1186534638"
-    sha256 high_sierra: "84ca9f1d74ad63108e320f2cae63a2afdfafd3995aa2d37837d551cc5dda8688"
+    sha256 arm64_monterey: "671a5f5ead3a6f2406306b8a05a0a63bee37eda0e7e771ab33acc397a8f7c645"
+    sha256 arm64_big_sur:  "1903e4da2086a2942a0cb67b7eaaab26b3fb5048114b6ccccfa0977f676f52a4"
+    sha256 monterey:       "087f1adc8db1045091b37fa769df85d5bd9cc886c8c12e7ff5d1216da6dd62ba"
+    sha256 big_sur:        "60d5a72e5a55cace788d0a28241b9080fbf039c25fd91eb3bf91a95a8e8e4a89"
+    sha256 catalina:       "8b22a2d1f52ff40334a16fc4614bc2f2c9e50386f0732e8e4478f68c7008f961"
+    sha256 mojave:         "91b95a137be4361dee7d8bf2e442fa75eaf159469c09e238a127aa1186534638"
+    sha256 high_sierra:    "84ca9f1d74ad63108e320f2cae63a2afdfafd3995aa2d37837d551cc5dda8688"
+    sha256 x86_64_linux:   "05b175c2263baf72cb59debeaf49a974d31db23caf9f1c2b5e9eb3007a02e791"
   end
 
   deprecate! date: "2020-11-12", because: :unmaintained
 
+  # Added automake as a build dependency to update config files for ARM support.
+  depends_on "automake" => :build
   depends_on "pkg-config" => :build
   depends_on "gnutls"
   depends_on "libao"
@@ -25,7 +30,14 @@ class Rdesktop < Formula
   depends_on "libxrandr"
   depends_on "nettle"
 
+  uses_from_macos "pcsc-lite"
+
   def install
+    # Workaround for ancient config files not recognizing aarch64 macos.
+    %w[config.guess config.sub].each do |fn|
+      cp Formula["automake"].share/"automake-#{Formula["automake"].version.major_minor}"/fn, fn
+    end
+
     args = %W[
       --prefix=#{prefix}
       --disable-credssp
