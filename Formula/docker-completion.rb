2754a28b857c4b53d68c3f0f5ea490fbb73d73ea
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-09 00:48:40 +0800

    docker-completion: no rebuilding required

--- Formula/docker-completion.rb
+++ Formula/docker-completion.rb
@@ -2,7 +2,7 @@ class DockerCompletion < Formula
   desc "Bash, Zsh and Fish completion for Docker"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/cli.git",
-      tag:      "v20.10.13",
+      tag:      "v20.10.14",
       revision: "a224086349269551becacce16e5842ceeb2a98d6"
   license "Apache-2.0"
   head "https://github.com/docker/cli.git", branch: "master"
@@ -12,7 +12,7 @@ class DockerCompletion < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "363a3103b06716427764a30959a4a119a179f505df53dbdd44cc349aff80937f"
+    sha256 cellar: :any_skip_relocation, all: "2798e8f6b8191da609969bbebdc9f69e66ae8bb3bcbcace8b522c1868d2510bf"
   end
 
   conflicts_with "docker",
