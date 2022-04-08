0626e302dee922de2430bed8a55bc65af04eab2c
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 11:48:18 +0800

    uni: update 2.4.0 bottle.

--- Formula/uni.rb
+++ Formula/uni.rb
@@ -1,21 +1,17 @@
 class Uni < Formula
   desc "Unicode database query tool for the command-line"
   homepage "https://github.com/arp242/uni"
-  url "https://github.com/arp242/uni/archive/v2.3.0.tar.gz"
-  sha256 "2786c0e7ebe138de3a05320525eba4d4b718d36ea7557a9b0e2009f18b59a43b"
+  url "https://github.com/arp242/uni/archive/v2.4.0.tar.gz"
+  sha256 "d9caa7753983a42588beb3e7016987928fe8cd1e1d8c3728dc1e441dc27abba7"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fb8fa8680e8fabc93289a1bd482cd694a6a6e7469bec18f5d40900f5d5783f1c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "597d988f4aeac426ecaebdc534b3c03a675a2853bc0127b3656b5842ceff8644"
-    sha256 cellar: :any_skip_relocation, monterey:       "9335b94e130fdf8f74cdb347fc4467d9e5327f217eff46d7a3bcbf8429a9a76f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "29aa78a8155cfd349cc51df530c6f1678411e42cd490349e8beed100244d001e"
-    sha256 cellar: :any_skip_relocation, catalina:       "861d1048b9716f92236be766bdd9d98ced3596f368e2be76b74bbe923618af42"
-    sha256 cellar: :any_skip_relocation, mojave:         "73f1c7ed5340c70681205550b9a3f44c2f7e2c55cc72c6a03a7e2a44425b5750"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4de67f2f23ebe181b88ac2a5af7d59b6323aafe2fae43b5b1c819f5b499930c2"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/uni"
+    sha256 cellar: :any_skip_relocation, mojave: "a40259521498bedb3c5cfdf2384c5c73165ec6043d48c31db9c78fca492c48c9"
   end
 
-  depends_on "go" => :build
+  # Bump to 1.18 on the next release, if possible.
+  depends_on "go@1.17" => :build
 
   def install
     system "go", "build", "-o", bin/"uni"
