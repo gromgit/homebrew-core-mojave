1ce871da33354e55bd54f11528ae59ea541d9e48
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-09 00:49:35 +0800

    sblim-sfcc: no rebuilding required

--- Formula/sblim-sfcc.rb
+++ Formula/sblim-sfcc.rb
@@ -11,18 +11,29 @@ class SblimSfcc < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "9b75fdfd7494479190a2651966a17408dfa29a1f2f62c860e165caa5f135a6df"
-    sha256 cellar: :any, big_sur:       "8a334b1f1c440e4103ab947b0871ea160a0d319cf095e8ce8b45521ec9440770"
-    sha256 cellar: :any, catalina:      "d20839c77aaa1c968981ca9af0e92bf1c7f600392ea5674971f532040dae518a"
-    sha256 cellar: :any, mojave:        "ff61a006626a9a36dafb474f352d798805b1a44adba341d8422bd0820eaae1ab"
-    sha256 cellar: :any, high_sierra:   "0ee558ce892d6e04acfe7ca2408a96e2837c7c858e71f6047b3a57a15b75ece0"
-    sha256 cellar: :any, sierra:        "2e1eea4bbad906293b2c48a27a09fd76d665ab0c9259ef49fcd81f4783fbb67c"
+    sha256 cellar: :any,                 arm64_monterey: "1e22137d49b22b534626dcd8de3dfd98743d0eaba4fb1768e7e8f6553bfff162"
+    sha256 cellar: :any,                 arm64_big_sur:  "9b75fdfd7494479190a2651966a17408dfa29a1f2f62c860e165caa5f135a6df"
+    sha256 cellar: :any,                 monterey:       "dde4c03d86680b2886786a8a32d644187a9eb246e6de9c4db906250baa33ba99"
+    sha256 cellar: :any,                 big_sur:        "8a334b1f1c440e4103ab947b0871ea160a0d319cf095e8ce8b45521ec9440770"
+    sha256 cellar: :any,                 catalina:       "d20839c77aaa1c968981ca9af0e92bf1c7f600392ea5674971f532040dae518a"
+    sha256 cellar: :any,                 mojave:         "ff61a006626a9a36dafb474f352d798805b1a44adba341d8422bd0820eaae1ab"
+    sha256 cellar: :any,                 high_sierra:    "0ee558ce892d6e04acfe7ca2408a96e2837c7c858e71f6047b3a57a15b75ece0"
+    sha256 cellar: :any,                 sierra:         "2e1eea4bbad906293b2c48a27a09fd76d665ab0c9259ef49fcd81f4783fbb67c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c309028903cce29bd8b4473bc4f21e02e226aaa7c497a97138f931bd9f887e93"
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "openssl@1.1"
+
+  uses_from_macos "curl"
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
+    sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
+  end
 
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
@@ -37,7 +48,7 @@ class SblimSfcc < Formula
         return 0;
       }
     EOS
-    system ENV.cxx, "test.cpp", "-L#{lib}", "-lcimcClient", "-o", "test"
+    system ENV.cxx, "test.cpp", "-L#{lib}", "-lcimcclient", "-o", "test"
     system "./test"
   end
 end
