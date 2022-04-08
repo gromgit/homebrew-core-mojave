b190f3c3341fc4ad7ec5e6812aeff4b5b0427856
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-09 00:48:39 +0800

    digdag: no rebuilding required

--- Formula/digdag.rb
+++ Formula/digdag.rb
@@ -4,6 +4,7 @@ class Digdag < Formula
   url "https://dl.digdag.io/digdag-0.10.4.jar"
   sha256 "2020fa9395e74e14fdf4ada93bff0684dcea64f564788afd6a3ecc54e3bbd4bf"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url "https://github.com/treasure-data/digdag.git"
@@ -11,14 +12,14 @@ class Digdag < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "47b5fff24344ebadc086f1ea9421a2f96fe9816e115e568cabafdc94aabdbf4d"
+    sha256 cellar: :any_skip_relocation, all: "1e8af040e14f05c1f648ef90f3e0e7da9037d7e813cfb1fc7c644d174244d0cf"
   end
 
-  depends_on "openjdk@8"
+  depends_on "openjdk@11"
 
   def install
     libexec.install "digdag-#{version}.jar"
-    bin.write_jar_script libexec/"digdag-#{version}.jar", "digdag", java_version: "1.8"
+    bin.write_jar_script libexec/"digdag-#{version}.jar", "digdag", java_version: "11"
   end
 
   test do
