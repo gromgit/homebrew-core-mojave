f2550c7e5fd49283ed299b681509acfcfbff8c19
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 09:20:36 +0800

    tfupdate: update 0.6.5 bottle.

--- Formula/tfupdate.rb
+++ Formula/tfupdate.rb
@@ -1,14 +1,14 @@
 class Tfupdate < Formula
   desc "Update version constraints in your Terraform configurations"
   homepage "https://github.com/minamijoyo/tfupdate"
-  url "https://github.com/minamijoyo/tfupdate/archive/v0.6.4.tar.gz"
-  sha256 "ee55baed73ccfe43fee8d7214e3f04e2179611c0b058a22f4be6d89797441fb9"
+  url "https://github.com/minamijoyo/tfupdate/archive/v0.6.5.tar.gz"
+  sha256 "eb63ac5a6bafda614ec128fa7be1c81e4fa54855d69c6f8cf9c632f3e40cffa3"
   license "MIT"
   head "https://github.com/minamijoyo/tfupdate.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tfupdate"
-    sha256 cellar: :any_skip_relocation, mojave: "94433d709833bc691b39c4422670591f1323df302c46ca5fad8d4d54f43fe66c"
+    sha256 cellar: :any_skip_relocation, mojave: "c69d7db94e2292c2f7f817f376459e3e00fd1fddb333c2ac6b48d38bf146e679"
   end
 
   depends_on "go" => :build
