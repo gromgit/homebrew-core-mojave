06dc1a0452cffffb96cbeb4518d3f338e4fe3713
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 08:46:46 +0800

    terragrunt: update 0.36.6 bottle.

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -1,13 +1,13 @@
 class Terragrunt < Formula
   desc "Thin wrapper for Terraform e.g. for locking state"
   homepage "https://terragrunt.gruntwork.io/"
-  url "https://github.com/gruntwork-io/terragrunt/archive/v0.36.2.tar.gz"
-  sha256 "3ee1942cc6fd1408885ac0ff3d0106207a8cc846062b0d09e981ef6a293c2c5d"
+  url "https://github.com/gruntwork-io/terragrunt/archive/v0.36.6.tar.gz"
+  sha256 "adc7b9d651188cfaadad4ab181155e13a528bbff4338144b104c233e5b249b8a"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/terragrunt"
-    sha256 cellar: :any_skip_relocation, mojave: "4251433c7dbcf0399f086ae0d5efadb2c63a007cbfc4fd4d277f87eb8ff7585f"
+    sha256 cellar: :any_skip_relocation, mojave: "8c38d731e4c28e280c7e676a76507bfffe6737ce111d953ac526f68ef62600d9"
   end
 
   depends_on "go" => :build
