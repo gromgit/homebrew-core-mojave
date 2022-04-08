d5f70a7d2ed1e864836f1186b12ce3e522df6a25
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 11:54:11 +0800

    universal-ctags: update p5.9.20220327.0 bottle.

--- Formula/universal-ctags.rb
+++ Formula/universal-ctags.rb
@@ -1,9 +1,9 @@
 class UniversalCtags < Formula
   desc "Maintained ctags implementation"
   homepage "https://github.com/universal-ctags/ctags"
-  url "https://github.com/universal-ctags/ctags/archive/refs/tags/p5.9.20220227.0.tar.gz"
-  version "p5.9.20220227.0"
-  sha256 "d2a96484a47d0151abfcfe1cc2e7cdf8809fd5657d8dea694d16954f3085facd"
+  url "https://github.com/universal-ctags/ctags/archive/refs/tags/p5.9.20220327.0.tar.gz"
+  version "p5.9.20220327.0"
+  sha256 "89f5833e31800b7b7e275cdee9f60dca3294a6525c9fb5929381776e84c14494"
   license "GPL-2.0-only"
   head "https://github.com/universal-ctags/ctags.git", branch: "master"
 
@@ -14,7 +14,7 @@ class UniversalCtags < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/universal-ctags"
-    sha256 cellar: :any, mojave: "33f32ea19270ebf57b22ce7aac8c377ba3747f31b20516c88e159ec233e83381"
+    sha256 cellar: :any, mojave: "8cb5ab397f13fe60d587e1fd10d58e2160c4a3c9b751f3239ff0a904c26ecc4d"
   end
 
   depends_on "autoconf" => :build
