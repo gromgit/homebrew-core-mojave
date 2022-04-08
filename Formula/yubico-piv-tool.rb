f09cea62e813502d03c9bb5261d6438e0c6643f0
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 19:06:19 +0800

    yubico-piv-tool: update 2.3.0 bottle.

--- Formula/yubico-piv-tool.rb
+++ Formula/yubico-piv-tool.rb
@@ -1,8 +1,8 @@
 class YubicoPivTool < Formula
   desc "Command-line tool for the YubiKey PIV application"
   homepage "https://developers.yubico.com/yubico-piv-tool/"
-  url "https://developers.yubico.com/yubico-piv-tool/Releases/yubico-piv-tool-2.2.1.tar.gz"
-  sha256 "b7ede4ddc3d6e31de67b2e2ddcd319b22b40cc2e0973b9866d052a754493b14e"
+  url "https://developers.yubico.com/yubico-piv-tool/Releases/yubico-piv-tool-2.3.0.tar.gz"
+  sha256 "a02a12d9545d1ef7a1b998606d89b7b655a5f5a1437736cf51db083f876f55a9"
   license "BSD-2-Clause"
 
   livecheck do
@@ -11,13 +11,8 @@ class YubicoPivTool < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "ff0bf8e60dcc118a6af53c268b3657b45968146e0c0ec100e39c83aaf211d93b"
-    sha256 cellar: :any,                 arm64_big_sur:  "010fd380069d1b30971f6dedc434ea2ab55a2db8b05ba0fcb74115b3e3b4a724"
-    sha256 cellar: :any,                 monterey:       "973473705c16f890b8479f6b616f0cee718195ecd645c05b1c53a8721db0acbb"
-    sha256 cellar: :any,                 big_sur:        "886605cf401ac43e73fc09d7c443977e3fef3bf49dbcc49a68638f43705ad151"
-    sha256 cellar: :any,                 catalina:       "c5b6ce9eb6b9501d4dccff86ea593d6ff1451be89712dd01369eb3671ae7fcb8"
-    sha256 cellar: :any,                 mojave:         "21e6bea1407cf476c02575caa150e233674abc27abaa5a52d41f97365eb49a41"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "04df6364c57b50081bf17a12b220fe46e8a6b73c428e83f085641f6d98d92628"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/yubico-piv-tool"
+    sha256 cellar: :any, mojave: "b9b564285e79245e7c5cf0907aeb7442644cc5b95bc9374342e92435075dbc4d"
   end
 
   depends_on "check" => :build
