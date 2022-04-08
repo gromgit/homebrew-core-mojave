fd0288c471ca74d753a5fec0d0c7fbd6d0af9abf
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 09:53:14 +0800

    torchvision: update 0.12.0 bottle.

--- Formula/torchvision.rb
+++ Formula/torchvision.rb
@@ -1,13 +1,13 @@
 class Torchvision < Formula
   desc "Datasets, transforms, and models for computer vision"
   homepage "https://github.com/pytorch/vision"
-  url "https://github.com/pytorch/vision/archive/refs/tags/v0.11.3.tar.gz"
-  sha256 "b4c51d27589783e6e6941ecaa67b55f6f41633874ec37f80b64a0c92c3196e0c"
+  url "https://github.com/pytorch/vision/archive/refs/tags/v0.12.0.tar.gz"
+  sha256 "99e6d3d304184895ff4f6152e2d2ec1cbec89b3e057d9c940ae0125546b04e91"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/torchvision"
-    sha256 cellar: :any, mojave: "24f69f60099717878736911cde02f9376c2ed92dd652f1ce079cba7702c5a7f5"
+    sha256 cellar: :any, mojave: "5976a1e18ef702da87541aba949a164fbf04e1398bebf4491f967e477b0c6518"
   end
 
   depends_on "cmake" => :build
