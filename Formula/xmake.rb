a19ebc4bf44316409d1197cd05276d3e2bf60ada
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 18:07:24 +0800

    xmake: update 2.6.4 bottle.

--- Formula/xmake.rb
+++ Formula/xmake.rb
@@ -1,14 +1,14 @@
 class Xmake < Formula
   desc "Cross-platform build utility based on Lua"
   homepage "https://xmake.io/"
-  url "https://github.com/xmake-io/xmake/releases/download/v2.6.3/xmake-v2.6.3.tar.gz"
-  sha256 "43c739b8faa3be986f95421c7ba1e135ff18d4723fb88b7e0bb0cdd27ef37430"
+  url "https://github.com/xmake-io/xmake/releases/download/v2.6.4/xmake-v2.6.4.tar.gz"
+  sha256 "ca8fb4f269718bee06078241cd72ec6883167f493fe11d623a0d37419b2a9ffd"
   license "Apache-2.0"
   head "https://github.com/xmake-io/xmake.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/xmake"
-    sha256 cellar: :any_skip_relocation, mojave: "d239bf9dba07b35e5f32365060f51606b11af0f80c29d8bcc73609b849489c88"
+    sha256 cellar: :any_skip_relocation, mojave: "29a38d8ddfcac82bcbd095fa54e1833682a12620b3560ffc6c9920cbf908f616"
   end
 
   on_linux do
@@ -23,9 +23,7 @@ class Xmake < Formula
   end
 
   test do
-    on_linux do
-      ENV["XMAKE_ROOT"] = "y" if ENV["HOMEBREW_GITHUB_ACTIONS"]
-    end
+    ENV["XMAKE_ROOT"] = "y" if OS.linux? && (ENV["HOMEBREW_GITHUB_ACTIONS"])
     system bin/"xmake", "create", "test"
     cd "test" do
       system bin/"xmake"
