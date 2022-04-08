821d898305ac3b1c461e15c7f2c23f650b5dcfc0
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 11:27:34 +0800

    ucloud: update 0.1.37 bottle.

--- Formula/ucloud.rb
+++ Formula/ucloud.rb
@@ -6,12 +6,9 @@ class Ucloud < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "99d77eefb237043ab551e21c7bc5c4d8290987e92b2b06c5ede39d6a6946358d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3104755ba0d6f6c6ece54041f351b5626a1b6eab413f02ba1d6f7910ad145ca3"
-    sha256 cellar: :any_skip_relocation, monterey:       "d3e402a2646997e17c4b585d898a05f8341a58753f16546b9c4bf7ad717bbda7"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e17891249c6853edd5e48bdb01417815944a593c2549fec53cb90797ed020878"
-    sha256 cellar: :any_skip_relocation, catalina:       "360540194c9c3f46ecd1a7fbdb659908123afcc3fa44d093cd641a6e87282ff8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e5326dd1f207ab2ac02a00c06d8231ec5895735a595d543fe3a670a2c93854e3"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ucloud"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "90c8539f028136421baa277546da2a3b68bbf415d0489b02fefbfc980f28cc9f"
   end
 
   # Bump to 1.18 on the next release, if possible.
