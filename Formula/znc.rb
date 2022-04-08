9e02ceecb6beb705744f3934ebe17e244c1b600c
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 19:39:33 +0800

    znc: update 1.8.2_5 bottle.

--- Formula/znc.rb
+++ Formula/znc.rb
@@ -4,16 +4,11 @@ class Znc < Formula
   url "https://znc.in/releases/archive/znc-1.8.2.tar.gz"
   sha256 "ff238aae3f2ae0e44e683c4aee17dc8e4fdd261ca9379d83b48a7d422488de0d"
   license "Apache-2.0"
-  revision 4
+  revision 5
 
   bottle do
-    sha256 arm64_monterey: "48bbc131756030fa8073447e12ac4d17dd02ef000510bb60d423bfbdee8618f0"
-    sha256 arm64_big_sur:  "f6976cbbb65b4261db7ca3ab26c4f442096dde743a9fd63afe282d551faa07b8"
-    sha256 monterey:       "557bd2025ab422e1f45779d35ffc7448f2f3be13ec5bdf686f5416c50fe67f46"
-    sha256 big_sur:        "5ead56d8a9fd75e5c76cf96ffc0351c4a7a6cb91d6e6ae1dc35e55c8c410e734"
-    sha256 catalina:       "6f51d8c1b693434a4faf554c20bcf05ba98cb147058fd1ba35a666839329dda6"
-    sha256 mojave:         "d8dc6d3095a4fc27d77baa19cf870922374b701a41cf503170f7f99012d44acf"
-    sha256 x86_64_linux:   "38164d05dbeec581b58630e44faec2766290d7e8c50959e8559a5fc317b5bb65"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/znc"
+    sha256 mojave: "8d95681063102ca033ec701d6aaa5234545dadd4076ad2d8c66e4f0127164da9"
   end
 
   head do
