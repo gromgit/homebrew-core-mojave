ca4254fcbc4839660f07a68565a8c791eb45f9e0
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-09 00:49:07 +0800

    libnfnetlink:

--- Formula/libnfnetlink.rb
+++ Formula/libnfnetlink.rb
@@ -1,13 +1,17 @@
 class Libnfnetlink < Formula
   desc "Low-level library for netfilter related communication"
   homepage "https://www.netfilter.org/projects/libnfnetlink"
-  url "https://www.netfilter.org/projects/libnfnetlink/files/libnfnetlink-1.0.1.tar.bz2"
-  sha256 "f270e19de9127642d2a11589ef2ec97ef90a649a74f56cf9a96306b04817b51a"
+  url "https://www.netfilter.org/projects/libnfnetlink/files/libnfnetlink-1.0.2.tar.bz2"
+  sha256 "b064c7c3d426efb4786e60a8e6859b82ee2f2c5e49ffeea640cfe4fe33cbc376"
   license "LGPL-2.1-or-later"
 
   livecheck do
     url "https://www.netfilter.org/projects/libnfnetlink/downloads.html"
     regex(/href=.*?libnfnetlink[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "302c0c8f5d1b84aa7482b918d2b70a9c88eb0e794163342317b208feff242993"
   end
 
   depends_on :linux
