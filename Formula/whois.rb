b8c0f3bc9685058698b3e2a7eb65b289bfce2605
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-08 17:00:42 +0800

    whois: update 5.5.12 bottle.

--- Formula/whois.rb
+++ Formula/whois.rb
@@ -1,14 +1,14 @@
 class Whois < Formula
   desc "Lookup tool for domain names and other internet resources"
   homepage "https://packages.debian.org/sid/whois"
-  url "https://deb.debian.org/debian/pool/main/w/whois/whois_5.5.11.tar.xz"
-  sha256 "966281eb8301c14a1acc17e46c70061f73179d80e370aef43065c22f0947fb87"
+  url "https://deb.debian.org/debian/pool/main/w/whois/whois_5.5.12.tar.xz"
+  sha256 "f1c5bab781b7f2357dab1039e8875d41ff7b5d03a78c27443fa26351952a0822"
   license "GPL-2.0-or-later"
   head "https://github.com/rfc1036/whois.git", branch: "next"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/whois"
-    sha256 cellar: :any, mojave: "891cf3f99b8a9c3dce6b36d887f36331e9d6912ee0fae31ad1c7b6a8c9a97e80"
+    sha256 cellar: :any, mojave: "89179ee60f6ea25c84f34c61d6a048d92f9539b008737dbf3550f2c1f687c3d4"
   end
 
   keg_only :provided_by_macos
