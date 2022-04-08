aae1e1c32c0fe5d9b510784c7fae41d6bf7b234f
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-09 00:49:06 +0800

    liblockfile: no rebuilding required

--- Formula/liblockfile.rb
+++ Formula/liblockfile.rb
@@ -6,24 +6,26 @@ class Liblockfile < Formula
   license "LGPL-2.0-or-later"
 
   bottle do
-    sha256 arm64_monterey: "69933a745062ff9b8a41b7b3b7fec871efa9a99896b1ace2ccbf4cbafb2437f8"
-    sha256 arm64_big_sur:  "41a9d79f95f938532b4320a29c5f5bf3d7229a6df3f06413112d903e23589078"
-    sha256 monterey:       "fec045c7ef1d9e55d1aac480298de26dce1754a73cb86f2545be737bc528d84b"
-    sha256 big_sur:        "d13b1ce9f35885e1b05c9bd436e8edd0fc1b0dc7475219773655cb69bafcfbb3"
-    sha256 catalina:       "a923faddb180ea86f1038424613c3191bf5212fc44e25548284f5a0525e1b5e9"
-    sha256 mojave:         "143542d504f3f37df987e6f2c4291c2966cdb9ac15a6fd581155a4079758575e"
+    sha256                               arm64_monterey: "69933a745062ff9b8a41b7b3b7fec871efa9a99896b1ace2ccbf4cbafb2437f8"
+    sha256                               arm64_big_sur:  "41a9d79f95f938532b4320a29c5f5bf3d7229a6df3f06413112d903e23589078"
+    sha256                               monterey:       "fec045c7ef1d9e55d1aac480298de26dce1754a73cb86f2545be737bc528d84b"
+    sha256                               big_sur:        "d13b1ce9f35885e1b05c9bd436e8edd0fc1b0dc7475219773655cb69bafcfbb3"
+    sha256                               catalina:       "a923faddb180ea86f1038424613c3191bf5212fc44e25548284f5a0525e1b5e9"
+    sha256                               mojave:         "143542d504f3f37df987e6f2c4291c2966cdb9ac15a6fd581155a4079758575e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b40e192cdbc7b9ecbc0e6ea36a893f5de7b4d26f5f8a094bec6ab31b3dc86b03"
   end
 
   def install
     # brew runs without root privileges (and the group is named "wheel" anyway)
     inreplace "Makefile.in", " -g root ", " "
 
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-debug",
-                          "--with-mailgroup=staff",
-                          "--prefix=#{prefix}",
-                          "--sysconfdir=#{etc}",
-                          "--mandir=#{man}"
+    args = %W[
+      --sysconfdir=#{etc}
+      --mandir=#{man}
+    ]
+    args << "--with-mailgroup=staff" if OS.mac?
+
+    system "./configure", *std_configure_args, *args
     bin.mkpath
     lib.mkpath
     include.mkpath
