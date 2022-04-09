39fd9579703d1c6236316288faf328e378aaf2cf
Adrian Ho <the.gromgit@gmail.com>
2022-04-09 21:57:53 +0800

    baresip: update 2.0.1 bottle.

--- Formula/baresip.rb
+++ Formula/baresip.rb
@@ -1,13 +1,13 @@
 class Baresip < Formula
   desc "Modular SIP useragent"
   homepage "https://github.com/baresip/baresip"
-  url "https://github.com/baresip/baresip/archive/v1.1.0.tar.gz"
-  sha256 "f9230b27c4a62f31223847bc485c51f3d960f8a09f36998dedb73358e1784b4e"
+  url "https://github.com/baresip/baresip/archive/v2.0.1.tar.gz"
+  sha256 "8e7a5d228d2003aec8b4e570a32de5937e9bcbceff14803198fba767c514d362"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/baresip"
-    sha256 mojave: "e9a44489ab69b8cc15945ad52ab11fa2793973e4b7ed310465dbe5ebc99012ba"
+    sha256 mojave: "9d427653db7dc7f6a2a45e76a5c00b62e2e47f954f50b2826fdc231833661a01"
   end
 
   depends_on "libre"
@@ -23,21 +23,26 @@ class Baresip < Formula
     libre = Formula["libre"]
     librem = Formula["librem"]
     # NOTE: `LIBRE_SO` is a directory but `LIBREM_SO` is a shared library.
-    system "make", "install", "PREFIX=#{prefix}",
-                              "LIBRE_MK=#{libre.opt_share}/re/re.mk",
-                              "LIBRE_INC=#{libre.opt_include}/re",
-                              "LIBRE_SO=#{libre.opt_lib}",
-                              "LIBREM_PATH=#{librem.opt_prefix}",
-                              "LIBREM_SO=#{librem.opt_lib/shared_library("librem")}",
-                              "MOD_AUTODETECT=",
-                              "USE_AVCAPTURE=1",
-                              "USE_COREAUDIO=1",
-                              "USE_G711=1",
-                              "USE_OPENGL=1",
-                              "USE_STDIO=1",
-                              "USE_UUID=1",
-                              "HAVE_GETOPT=1",
-                              "V=1"
+    args = %W[
+      PREFIX=#{prefix}
+      LIBRE_MK=#{libre.opt_share}/re/re.mk
+      LIBRE_INC=#{libre.opt_include}/re
+      LIBRE_SO=#{libre.opt_lib}
+      LIBREM_PATH=#{librem.opt_prefix}
+      LIBREM_SO=#{librem.opt_lib/shared_library("librem")}
+      MOD_AUTODETECT=
+      USE_G711=1
+      USE_OPENGL=1
+      USE_STDIO=1
+      USE_UUID=1
+      HAVE_GETOPT=1
+      V=1
+    ]
+    if OS.mac?
+      args << "USE_AVCAPTURE=1"
+      args << "USE_COREAUDIO=1"
+    end
+    system "make", "install", *args
   end
 
   test do
