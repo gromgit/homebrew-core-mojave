842dae2ff1a121a7a54dd478268750c27edcf6f2
Adrian Ho <the.gromgit@gmail.com>
2022-04-09 22:28:35 +0800

    bkt: update 0.5.4 bottle.

--- Formula/bkt.rb
+++ Formula/bkt.rb
@@ -1,13 +1,13 @@
 class Bkt < Formula
   desc "Utility for caching the results of shell commands"
   homepage "https://www.bkt.rs"
-  url "https://github.com/dimo414/bkt/archive/refs/tags/0.5.3.tar.gz"
-  sha256 "38c418e8abe1bf6835f1b16d02c03677075af6b10ad4723392540ec79797520e"
+  url "https://github.com/dimo414/bkt/archive/refs/tags/0.5.4.tar.gz"
+  sha256 "172c413709dc81ced9dfa2750aaa398864e904d1ed213bd19e51d45d1ff0a8ff"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bkt"
-    sha256 cellar: :any_skip_relocation, mojave: "d6ff84b22918c4c755f915f83d7b239bf11fe68f2c3f4f761aa281ac8209b52a"
+    sha256 cellar: :any_skip_relocation, mojave: "43b123d5c3c917b95af62fec6991cd51d84a1c72e67fbfa0f611d86732d76c12"
   end
 
   depends_on "rust" => :build
