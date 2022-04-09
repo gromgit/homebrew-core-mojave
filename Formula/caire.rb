e8138582eb2b57b64650ce713a0cc116c04d3a83
Adrian Ho <the.gromgit@gmail.com>
2022-04-09 23:23:54 +0800

    caire: update 1.4.4 bottle.

--- Formula/caire.rb
+++ Formula/caire.rb
@@ -1,14 +1,14 @@
 class Caire < Formula
   desc "Content aware image resize tool"
   homepage "https://github.com/esimov/caire"
-  url "https://github.com/esimov/caire/archive/v1.4.3.tar.gz"
-  sha256 "80841c430d3022ef768efe50f8a895239fe8f4d86f3e51a76efc0b5026f13fdc"
+  url "https://github.com/esimov/caire/archive/v1.4.4.tar.gz"
+  sha256 "2786ab0af06aeb8357ae835340b96f30ad1b134280faa346f0f250df5c7567b1"
   license "MIT"
   head "https://github.com/esimov/caire.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/caire"
-    sha256 cellar: :any_skip_relocation, mojave: "79ccacee9f1c19806eeca36bf30b816e4c4a950d8fd378185d57291c684748a0"
+    sha256 cellar: :any_skip_relocation, mojave: "7f1145b140f1aea80d971c4a3dff68093eebbbefa657fbe9bbce63d04ec0be33"
   end
 
   depends_on "go" => :build
