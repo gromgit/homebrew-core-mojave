ac2ddd23ad2669f438049b7bd742220e74408a36
Adrian Ho <the.gromgit@gmail.com>
2022-04-09 12:16:41 +0800

    acpica: update 20220331 bottle.

--- Formula/acpica.rb
+++ Formula/acpica.rb
@@ -1,8 +1,8 @@
 class Acpica < Formula
   desc "OS-independent implementation of the ACPI specification"
   homepage "https://www.acpica.org/"
-  url "https://acpica.org/sites/acpica/files/acpica-unix-20211217.tar.gz"
-  sha256 "2511f85828820d747fa3e2c3433d3a38c22db3d9c2fd900e1a84eb4173cb5992"
+  url "https://acpica.org/sites/acpica/files/acpica-unix-20220331.tar.gz"
+  sha256 "acaff68b14f1e0804ebbfc4b97268a4ccbefcfa053b02ed9924f2b14d8a98e21"
   license any_of: ["Intel-ACPI", "GPL-2.0-only", "BSD-3-Clause"]
   head "https://github.com/acpica/acpica.git", branch: "master"
 
@@ -13,9 +13,8 @@ class Acpica < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/acpica"
-    sha256 cellar: :any_skip_relocation, mojave: "1220cd2bae7696e3c6532ac3c92aafdb8978dd10f536771f5d7fab08a859d9d2"
+    sha256 cellar: :any_skip_relocation, mojave: "4c1a3ea66eea85eac714f2bd6cc0995e9a8a28ba4a8c85ead4dd97cc9d7a181f"
   end
-
 
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
