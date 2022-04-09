bd9ee8cbf641e74cc53444e30c49ece950f17489
Adrian Ho <the.gromgit@gmail.com>
2022-04-09 23:46:24 +0800

    cartridge-cli: update 2.12.0 bottle.

--- Formula/cartridge-cli.rb
+++ Formula/cartridge-cli.rb
@@ -2,13 +2,13 @@ class CartridgeCli < Formula
   desc "Tarantool Cartridge command-line utility"
   homepage "https://tarantool.org/"
   url "https://github.com/tarantool/cartridge-cli.git",
-      tag:      "2.11.0",
-      revision: "676008e78f3511422f784694407a498601f56248"
+      tag:      "2.12.0",
+      revision: "b4ed1a2a2ddf05bd6b6dd059fb68f3c1e4ff9366"
   license "BSD-2-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cartridge-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "97bcbf950e2f0c3fdfefee37f2dab1e3f0b6ce02f7284fd6a02505a6f715d403"
+    sha256 cellar: :any_skip_relocation, mojave: "f0c7be2f214139be8006a1f4683991d0bc93ecbb20699657e7fd5ba53d142d82"
   end
 
   depends_on "go" => :build
