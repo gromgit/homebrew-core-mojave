91f2d77b2990b7b2c27ee066655c98c84a895a8e
Adrian Ho <the.gromgit@gmail.com>
2022-04-10 20:45:50 +0800

    geogram: update 1.7.8 bottle.

--- Formula/geogram.rb
+++ Formula/geogram.rb
@@ -1,16 +1,13 @@
 class Geogram < Formula
   desc "Programming library of geometric algorithms"
   homepage "http://alice.loria.fr/software/geogram/doc/html/index.html"
-  # Homepage links to gforge.inria.fr for downloads, which gives a 403 response.
-  # We're using a GitHub tarball unless/until upstream finds a new home.
-  url "https://github.com/alicevision/geogram/archive/v1.7.7.tar.gz"
-  sha256 "7323d9f6a38fbaff3e07c47955e0c8f310906871d38171536ec8bc0758e816aa"
+  url "https://members.loria.fr/BLevy/PACKAGES/geogram_1.7.8.tar.gz"
+  sha256 "28e70b353705faec555700d8a7b7b9d703687702f46866bad09e033f86a96faf"
   license all_of: ["BSD-3-Clause", :public_domain, "LGPL-3.0-or-later", "MIT"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/geogram"
-    rebuild 2
-    sha256 mojave: "03ed679d5a22aa2486664701c274cac4fed2c562d72b5c3f57256a58f73cb97e"
+    sha256 cellar: :any, mojave: "cb854d83b43cc6dd37805d85c83e1fafe99b4ecd0a5f0dcde5daab9ea7f3744b"
   end
 
   depends_on "cmake" => :build
