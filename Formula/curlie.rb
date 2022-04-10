10b543bb2d34077e10f75e33a518cb8df5433141
Adrian Ho <the.gromgit@gmail.com>
2022-04-10 10:48:39 +0800

    curlie: update 1.6.9 bottle.

--- Formula/curlie.rb
+++ Formula/curlie.rb
@@ -1,14 +1,13 @@
 class Curlie < Formula
   desc "Power of curl, ease of use of httpie"
   homepage "https://curlie.io"
-  url "https://github.com/rs/curlie/archive/v1.6.7.tar.gz"
-  sha256 "25a0ea35be6ff9dd88551c992a0f7ea565ce2fae8213c674bd28a7cc512493d9"
+  url "https://github.com/rs/curlie/archive/refs/tags/v1.6.9.tar.gz"
+  sha256 "95b7061861aa8d608f9df0d63a11206f8cd532295ca13dd39ed37e0136bdcc5f"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/curlie"
-    rebuild 3
-    sha256 cellar: :any_skip_relocation, mojave: "695cc00f9256182edcc9d6b7a7260529c56a638b8b49593c89adb20548326806"
+    sha256 cellar: :any_skip_relocation, mojave: "bb9cbbb7ca13f987707332837474404f606c499d9f411db18d88e5d86d995cb6"
   end
 
   # Bump to 1.18 on the next release, if possible.
