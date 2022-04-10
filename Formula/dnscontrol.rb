576e86ade24012269204d1c30df652f236315cf2
Adrian Ho <the.gromgit@gmail.com>
2022-04-10 13:51:28 +0800

    dnscontrol: update 3.15.0 bottle.

--- Formula/dnscontrol.rb
+++ Formula/dnscontrol.rb
@@ -1,8 +1,8 @@
 class Dnscontrol < Formula
   desc "It is system for maintaining DNS zones"
   homepage "https://github.com/StackExchange/dnscontrol"
-  url "https://github.com/StackExchange/dnscontrol/archive/v3.14.0.tar.gz"
-  sha256 "d8a35ed30edba837659b3ca1177e8f14d0f00fe14cb832b61e691dddab3ac6a0"
+  url "https://github.com/StackExchange/dnscontrol/archive/v3.15.0.tar.gz"
+  sha256 "6766667e85ffd128f6f486f44ceb01e8c18442252a181d54b9c754637162d685"
   license "MIT"
   version_scheme 1
 
@@ -13,7 +13,7 @@ class Dnscontrol < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dnscontrol"
-    sha256 cellar: :any_skip_relocation, mojave: "b986abcb934ab52c6fcc493cbfb750c79238b5eaec345d75210c3f1cce9ea9e3"
+    sha256 cellar: :any_skip_relocation, mojave: "cf78e2557db3016c2899a334576a1accec356964602d28cbf38594016609faeb"
   end
 
   depends_on "go" => :build
