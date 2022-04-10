6f4da20505f4d93ac22912c1d7942cd807a71333
Adrian Ho <the.gromgit@gmail.com>
2022-04-10 15:13:30 +0800

    elixir: update 1.13.4 bottle.

--- Formula/elixir.rb
+++ Formula/elixir.rb
@@ -1,15 +1,14 @@
 class Elixir < Formula
   desc "Functional metaprogramming aware language built on Erlang VM"
   homepage "https://elixir-lang.org/"
-  url "https://github.com/elixir-lang/elixir/archive/v1.13.3.tar.gz"
-  sha256 "652779f7199f5524e2df1747de0e373d8b9f1d1206df25b2e551cd0ad33f8440"
+  url "https://github.com/elixir-lang/elixir/archive/v1.13.4.tar.gz"
+  sha256 "95daf2dd3052e6ca7d4d849457eaaba09de52d65ca38d6933c65bc1cdf6b8579"
   license "Apache-2.0"
   head "https://github.com/elixir-lang/elixir.git", branch: "main"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/elixir"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "5b85495a3ac4bb5c70b1211d4b96037625b0e0ae7aa998b369a8f57469152198"
+    sha256 cellar: :any_skip_relocation, mojave: "b2913d5f0c0ce530fa1e0cf1dcf39d730de6bb5eb887ed0915470ce2b4ada64d"
   end
 
   depends_on "erlang"
