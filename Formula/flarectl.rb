147af0a141c8326e25b7aec5ab214f53e58c7273
Adrian Ho <the.gromgit@gmail.com>
2022-04-10 19:07:49 +0800

    flarectl: update 0.36.0 bottle.

--- Formula/flarectl.rb
+++ Formula/flarectl.rb
@@ -1,14 +1,14 @@
 class Flarectl < Formula
   desc "CLI application for interacting with a Cloudflare account"
   homepage "https://github.com/cloudflare/cloudflare-go/tree/master/cmd/flarectl"
-  url "https://github.com/cloudflare/cloudflare-go/archive/v0.35.1.tar.gz"
-  sha256 "8d37113e9faf1ba63666fd6cb33b14a66a96eb0f0f354f9f15faac8c94942305"
+  url "https://github.com/cloudflare/cloudflare-go/archive/v0.36.0.tar.gz"
+  sha256 "2a453f021be34a9406e91f4452eae6138e8355f5fa30cd140560dd995177ccd5"
   license "BSD-3-Clause"
   head "https://github.com/cloudflare/cloudflare-go.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/flarectl"
-    sha256 cellar: :any_skip_relocation, mojave: "e1578a301f4dbc46bd7f3712fb0a0aa40e9c3cdb87bc30defc0963660e7b46e6"
+    sha256 cellar: :any_skip_relocation, mojave: "2445ca69a84693c4a29e7e638e55f57985d7fd232b4874184233550d213569ee"
   end
 
   depends_on "go" => :build
@@ -19,6 +19,6 @@ class Flarectl < Formula
 
   test do
     ENV["CF_API_TOKEN"] = "invalid"
-    assert_match "HTTP status 400: Invalid request headers (6003)", shell_output("#{bin}/flarectl u i", 1)
+    assert_match "Invalid request headers (6003)", shell_output("#{bin}/flarectl u i", 1)
   end
 end
