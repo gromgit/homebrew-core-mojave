0f0ea0617d117e1ebc4ec351f036e8677ea89c2c
Adrian Ho <the.gromgit@gmail.com>
2022-04-10 14:31:04 +0800

    dropbear: update 2022.82 bottle.

--- Formula/dropbear.rb
+++ Formula/dropbear.rb
@@ -1,8 +1,8 @@
 class Dropbear < Formula
   desc "Small SSH server/client for POSIX-based system"
   homepage "https://matt.ucc.asn.au/dropbear/dropbear.html"
-  url "https://matt.ucc.asn.au/dropbear/releases/dropbear-2020.81.tar.bz2"
-  sha256 "48235d10b37775dbda59341ac0c4b239b82ad6318c31568b985730c788aac53b"
+  url "https://matt.ucc.asn.au/dropbear/releases/dropbear-2022.82.tar.bz2"
+  sha256 "3a038d2bbc02bf28bbdd20c012091f741a3ec5cbe460691811d714876aad75d1"
   license "MIT"
 
   livecheck do
@@ -11,14 +11,8 @@ class Dropbear < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c358a02ddb77666f1a523422e27f6805186a07d73cc5a20faf9b4fb134f64e29"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c8681650c799bb023d972a9cc0dc9c07fd0ce37fc6f801a4b990af151f364b3b"
-    sha256 cellar: :any_skip_relocation, monterey:       "b9215fe593f1f1e709c85c05493ef7c1738cb28317e96bcaa0033ed6a7c345af"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ba4a09e7636d2629bdc11bfc5a99f9cab29eb351fb52a05c82533c76c29c87c8"
-    sha256 cellar: :any_skip_relocation, catalina:       "9659a7bdf1475748311e5a1e768ffb01d9020ddf7e19c7f8412bb62dc883d817"
-    sha256 cellar: :any_skip_relocation, mojave:         "55f1c51b6d253bcf03c6957139fae7e3b4f4cdcbdc90416ff9bd63f98d21a26d"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "59f3c740122b0f90b294d8b6e465cb9685b76617056cf9ae6554c221c681ed1c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4f09dda4db15d4d21a36d0bd9218c46bd78207c0670ff7756723a1f3e4efb7e5"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dropbear"
+    sha256 cellar: :any_skip_relocation, mojave: "a84168ccab36c0e96a513d8da6efeaaa4c18c644462b0bfcb3dc0dbd77d6157c"
   end
 
   head do
