dd9f511c021e875ddb080ee0d7e346895783792d
Adrian Ho <the.gromgit@gmail.com>
2022-04-10 09:51:20 +0800

    cosign: update 1.7.1 bottle.

--- Formula/cosign.rb
+++ Formula/cosign.rb
@@ -2,14 +2,14 @@ class Cosign < Formula
   desc "Container Signing"
   homepage "https://github.com/sigstore/cosign"
   url "https://github.com/sigstore/cosign.git",
-      tag:      "v1.6.0",
-      revision: "4b2c3c0c8ee97f31b9dac3859b40e0a48b8648ee"
+      tag:      "v1.7.1",
+      revision: "53c28e44f10548c8839d4c72e5909fa74e08b5f6"
   license "Apache-2.0"
   head "https://github.com/sigstore/cosign.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cosign"
-    sha256 cellar: :any_skip_relocation, mojave: "856ed0bc54c5029404aa487dfa6808a0ec8bdbe022973f5f7442cb58b47270d7"
+    sha256 cellar: :any_skip_relocation, mojave: "cc21e4e3761a15b1e3bf05928202e82d4baf17620fdd2f424b88c4bece413616"
   end
 
   depends_on "go" => :build
