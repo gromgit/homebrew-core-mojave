848f4ca97e7468ffaf9ed9f8ef38472b4c74f1bc
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-12 00:37:52 +0800

    takt: no rebuilding required

--- Formula/takt.rb
+++ Formula/takt.rb
@@ -16,6 +16,16 @@ class Takt < Formula
 
   depends_on "readline"
 
+  on_linux do
+    depends_on "alsa-lib"
+  end
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
+    sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--with-lispdir=#{elisp}"
     system "make", "install"
