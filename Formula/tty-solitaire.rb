93a96791e8d46319a5fcf4804285b3fab9faa8c9
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-12 00:37:53 +0800

    tty-solitaire: no rebuilding required

--- Formula/tty-solitaire.rb
+++ Formula/tty-solitaire.rb
@@ -12,6 +12,7 @@ class TtySolitaire < Formula
     sha256 cellar: :any_skip_relocation, big_sur:        "ca5bfa5041e8870b14c3ba92e73ad2766c8edc8cf139f03e9d983fc8d9a8976f"
     sha256 cellar: :any_skip_relocation, catalina:       "b315a94abfcf02186d94a6d4e78c71fec3538a65c5cc885df5dd9245efbca51c"
     sha256 cellar: :any_skip_relocation, mojave:         "ae38452ea77edde4a1c62085e6f1703cce1f91f04f869783761fa71399c03bf5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d5a265fd4dae6787d2560da92701d889f8413e1f9c13955e7ce640ff8d67714e"
   end
 
   uses_from_macos "ncurses"
