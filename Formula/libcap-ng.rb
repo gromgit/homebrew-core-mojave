924830444c003a2d12affd44f8f88c37fd20cdd0
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-12 00:37:28 +0800

    libcap-ng:

--- Formula/libcap-ng.rb
+++ Formula/libcap-ng.rb
@@ -1,14 +1,18 @@
 class LibcapNg < Formula
   desc "Library for Linux that makes using posix capabilities easy"
   homepage "https://people.redhat.com/sgrubb/libcap-ng"
-  url "https://github.com/stevegrubb/libcap-ng/archive/v0.8.2.tar.gz"
-  sha256 "65b86885b8d873e55c05bd49427fd370d559b26f0c2089ac9194828e6a2fe233"
+  url "https://github.com/stevegrubb/libcap-ng/archive/v0.8.3.tar.gz"
+  sha256 "e542e9139961f0915ab5878427890cdc7762949fbe216bd0cb4ceedb309bb854"
   license "LGPL-2.1-or-later"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "09874a93df04e5be6d46acfd1956f2cebc440d6c5aeffd578cee8d318ba7d247"
+  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on "python@3.9" => :build
+  depends_on "python@3.10" => :build
   depends_on "swig" => :build
   depends_on :linux
 
