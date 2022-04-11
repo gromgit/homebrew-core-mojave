a8a73d49357fea1120c40c5ca8f4b26c3bb8b948
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-12 00:37:22 +0800

    icdiff: no rebuilding required

--- Formula/icdiff.rb
+++ Formula/icdiff.rb
@@ -3,16 +3,13 @@ class Icdiff < Formula
 
   desc "Improved colored diff"
   homepage "https://github.com/jeffkaufman/icdiff"
-  url "https://github.com/jeffkaufman/icdiff/archive/release-2.0.4.tar.gz"
-  sha256 "ec21632b64159990a1bcedc8b25f96b476e7a6d9e18b75422420c0ae9b694eac"
+  url "https://github.com/jeffkaufman/icdiff/archive/release-2.0.5.tar.gz"
+  sha256 "e1c278222891acde5d5b47e31b89f765fdf022b8908057f583032530982fe5b4"
   license "PSF-2.0"
-  revision 1
   head "https://github.com/jeffkaufman/icdiff.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/icdiff"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "2fee1aee1e7ac56daa5f1f59dd2473773bd018b16a4609c44540fbae4235ae9d"
+    sha256 cellar: :any_skip_relocation, all: "41ff3bcb114db25d6bc5e12278e5c2e958f5178bea55959b9e0a0f22bb7d26c1"
   end
 
   depends_on "python@3.10"
@@ -24,7 +21,7 @@ class Icdiff < Formula
 
   test do
     (testpath/"file1").write "test1"
-    (testpath/"file2").write "test2"
+    (testpath/"file2").write "test1"
     system "#{bin}/icdiff", "file1", "file2"
     system "git", "init"
     system "#{bin}/git-icdiff"
