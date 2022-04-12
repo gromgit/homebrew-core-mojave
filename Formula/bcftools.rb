adb9c1860426a1781d7b68b812ae2cdd02d4a1d0
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-12 12:51:19 +0800

    bcftools: update 1.15.1 bottle.

--- Formula/bcftools.rb
+++ Formula/bcftools.rb
@@ -1,8 +1,8 @@
 class Bcftools < Formula
   desc "Tools for BCF/VCF files and variant calling from samtools"
   homepage "https://www.htslib.org/"
-  url "https://github.com/samtools/bcftools/releases/download/1.15/bcftools-1.15.tar.bz2"
-  sha256 "1885ccb450a86e97a00aa905d7381ca9e07bd8967c05705a61d0007d2e22296e"
+  url "https://github.com/samtools/bcftools/releases/download/1.15.1/bcftools-1.15.1.tar.bz2"
+  sha256 "f21f9564873eb27ccf22d13b91a64acb8fbbfe4f9e4c37933a54b9a95857f2d7"
   # The bcftools source code is MIT/Expat-licensed, but when it is configured
   # with --enable-libgsl the resulting executable is GPL-licensed.
   license "GPL-3.0-or-later"
@@ -12,10 +12,9 @@ class Bcftools < Formula
     strategy :github_latest
   end
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bcftools"
-    rebuild 1
-    sha256 mojave: "c82226b399a709ccd6adc46f82fd33a3ba4239735c79ee98aa73b51d54d62b42"
+    sha256 mojave: "33d9ed82007d3765dd233a99d4a7875a67887cefc75a7c945ce234a178dd3c43"
   end
 
   depends_on "gsl"
