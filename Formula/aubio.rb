dd4b2654523d9f9ddcd0697eaac2117ab37b1474
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-12 11:37:04 +0800

    aubio: update 0.4.9_3 bottle.

--- Formula/aubio.rb
+++ Formula/aubio.rb
@@ -3,7 +3,7 @@ class Aubio < Formula
   homepage "https://aubio.org/"
   url "https://aubio.org/pub/aubio-0.4.9.tar.bz2"
   sha256 "d48282ae4dab83b3dc94c16cf011bcb63835c1c02b515490e1883049c3d1f3da"
-  revision 2
+  revision 3
 
   livecheck do
     url "https://aubio.org/pub/"
@@ -11,19 +11,14 @@ class Aubio < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "1042328e9b10eaa62a59bc41675f72274ca60eb8cf324cd118aaf32b2f4e85af"
-    sha256 cellar: :any, arm64_big_sur:  "f6db53233e1b2855491b4b9310a579e4ac51c3dab7ac426f5c1c64b2fc0820f0"
-    sha256 cellar: :any, monterey:       "90c6f81ecc23cf88ef8a800b6fefd8487dcdb6f9606e427b5fbfebe678b8a2fb"
-    sha256 cellar: :any, big_sur:        "53bc8066ab50f4e4c24aad5f31e96f1ce0abac84930e5fcce43fa8f6083a878e"
-    sha256 cellar: :any, catalina:       "9b24159cf4c8adbb1a78c5cab192453ebebd47260612d38005683ff093250b45"
-    sha256 cellar: :any, mojave:         "653f41a951b87cf01049ed6a7019a3d2c96e635fc16d3be4d5edf3225b0a5f52"
-    sha256 cellar: :any, high_sierra:    "d6cc3ba3c3a257f4d5b4c1c3a9c0c8cca4fccf09ffc682901888d26c039886a8"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/aubio"
+    sha256 cellar: :any, mojave: "67e59fb5a3b24230bfc21236587303800075d607386c6dafd4bb4908b4df9711"
   end
 
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "numpy"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   resource "aiff" do
     url "http://www-mmsp.ece.mcgill.ca/Documents/AudioFormats/AIFF/Samples/CCRMA/wood24.aiff"
@@ -35,13 +30,14 @@ class Aubio < Formula
     ENV.refurbish_args
 
     # Ensure `python` references use our python3
-    ENV.prepend_path "PATH", Formula["python@3.9"].opt_libexec/"bin"
+    ENV.prepend_path "PATH", Formula["python@3.10"].opt_libexec/"bin"
 
     system "python3", "./waf", "configure", "--prefix=#{prefix}"
     system "python3", "./waf", "build"
     system "python3", "./waf", "install"
 
-    system "python3", *Language::Python.setup_install_args(prefix)
+    system "python3", *Language::Python.setup_install_args(prefix),
+                      "--install-lib=#{prefix/Language::Python.site_packages("python3")}"
   end
 
   test do
