798ce14417aaa04b55bc5da620482bb009aff45c
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-13 07:26:04 +0800

    gzip: update 1.12 bottle.

--- Formula/gzip.rb
+++ Formula/gzip.rb
@@ -1,19 +1,14 @@
 class Gzip < Formula
   desc "Popular GNU data compression program"
   homepage "https://www.gnu.org/software/gzip"
-  url "https://ftp.gnu.org/gnu/gzip/gzip-1.11.tar.gz"
-  mirror "https://ftpmirror.gnu.org/gzip/gzip-1.11.tar.gz"
-  sha256 "3e8a0e0c45bad3009341dce17d71536c4c655d9313039021ce7554a26cd50ed9"
+  url "https://ftp.gnu.org/gnu/gzip/gzip-1.12.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gzip/gzip-1.12.tar.gz"
+  sha256 "5b4fb14d38314e09f2fc8a1c510e7cd540a3ea0e3eb9b0420046b82c3bf41085"
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6b290a5b6a62c6a052a9952fcb0bb75a7b8076b380728c7f92a3b775983880b4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d996baa1c8f5384514e0a8ba4945cf07012664cfb481f2c9f7309d30053d5844"
-    sha256 cellar: :any_skip_relocation, monterey:       "30fef75bcd856c7dc7bc2c3e677bd10f0d8837fe26b87ea03277316328038fa4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "46d768ebba1aa240540fd620fe856d259b8e316567204b7002052dd6a6241696"
-    sha256 cellar: :any_skip_relocation, catalina:       "50c51fd0770177e688cf98f358b2383e2ebe250b0bd8bf25def80a1b8da1c318"
-    sha256 cellar: :any_skip_relocation, mojave:         "2eae57977bdfcade27141c865d01aac1ace6be66828e9e0294f12a473fd7a35d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7993153f7f73005baf13fa6ab5b1dfe011fdd92c188d4cd69aad9bee98e432de"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gzip"
+    sha256 cellar: :any_skip_relocation, mojave: "693114a5d41193fa0f873228fd61910aa6e3cd97e191bfb9046efa8ce8b460cd"
   end
 
   def install
