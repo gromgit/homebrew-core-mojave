57b4ae78b680ee622211fca31a61858063a7b4b4
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-12 20:16:03 +0800

    collectd: update 5.12.0_2 bottle.

--- Formula/collectd.rb
+++ Formula/collectd.rb
@@ -2,7 +2,7 @@ class Collectd < Formula
   desc "Statistics collection and monitoring daemon"
   homepage "https://collectd.org/"
   license "MIT"
-  revision 1
+  revision 2
 
   stable do
     url "https://collectd.org/files/collectd-5.12.0.tar.bz2"
@@ -21,13 +21,8 @@ class Collectd < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "1a82ae21e794db579661851b68e64f8b5308b970314e9904c291b33810c54ed5"
-    sha256 arm64_big_sur:  "c0a9e32a3407d094ae4fe5f8bf0fc19d0b4f5f0bb40f8ce6335fe4d2241a72b3"
-    sha256 monterey:       "f80f9f9a0d15884d587de0631f1d573d2a194e90e88bd579191825c7a58ea781"
-    sha256 big_sur:        "73233ee8e731722660a1098db2a72ae276508b8b09475f101f50a8d5ddc49251"
-    sha256 catalina:       "33f0fa042a98883dbf363865a66d64fd53e2eaebc330829257e2d5c87c7b5a4d"
-    sha256 mojave:         "a38f5912b4ed2b48e37e7285e0dd6e4f97d31799e5e7c47f438cddd7806a1252"
-    sha256 x86_64_linux:   "9b7b93198f1b6008763a8012fa7209c108b5e179a0b38da1599234b6fe06523a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/collectd"
+    sha256 mojave: "878bcf24ed09f8996e20460d0372c140b166b830738ec5d5d68567696e814d77"
   end
 
   head do
