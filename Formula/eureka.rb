64723e85321e4211c5d0b27e41a13a7b8e45c79d
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-13 03:48:57 +0800

    eureka: update 2.0.0 bottle.

--- Formula/eureka.rb
+++ Formula/eureka.rb
@@ -1,17 +1,15 @@
 class Eureka < Formula
   desc "CLI tool to input and store your ideas without leaving the terminal"
   homepage "https://github.com/simeg/eureka"
-  url "https://github.com/simeg/eureka/archive/v1.8.1.tar.gz"
-  sha256 "d10d412c71dea51b4973c3ded5de1503a4c5de8751be5050de989ac08eb0455e"
+  url "https://github.com/simeg/eureka/archive/v2.0.0.tar.gz"
+  sha256 "e874549e1447ee849543828f49c4c1657f7e6cfe787deea13d44241666d4aaa0"
   license "MIT"
   head "https://github.com/simeg/eureka.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/eureka"
-    rebuild 2
-    sha256 cellar: :any, mojave: "09c3589b021ae34f5de8eedf8e23b98c4a8cb64f32beaf77b74be390e2dd1d87"
+    sha256 cellar: :any, mojave: "c8f06f3aee6109437bff6879e876e825b1363546d725dfe8c5169d90a1660a0a"
   end
-
 
   depends_on "rust" => :build
   depends_on "openssl@1.1"
@@ -27,12 +25,12 @@ class Eureka < Formula
   end
 
   test do
-    assert_match "eureka [FLAGS]", shell_output("#{bin}/eureka --help 2>&1")
+    assert_match "eureka [OPTIONS]", shell_output("#{bin}/eureka --help 2>&1")
 
     (testpath/".eureka/repo_path").write <<~EOS
       homebrew
     EOS
 
-    assert_match "homebrew/README.md: No such file or directory", pipe_output("#{bin}/eureka --view 2>&1")
+    assert_match "ERROR eureka > No such file or directory", pipe_output("#{bin}/eureka --view 2>&1")
   end
 end
