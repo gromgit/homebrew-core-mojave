d8a180b28773f4d3487fce98a55b4543decd004d
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-13 06:49:53 +0800

    gnunet: update 0.16.3 bottle.

--- Formula/gnunet.rb
+++ Formula/gnunet.rb
@@ -1,14 +1,14 @@
 class Gnunet < Formula
   desc "Framework for distributed, secure and privacy-preserving applications"
   homepage "https://gnunet.org/"
-  url "https://ftp.gnu.org/gnu/gnunet/gnunet-0.16.2.tar.gz"
-  mirror "https://ftpmirror.gnu.org/gnunet/gnunet-0.16.2.tar.gz"
-  sha256 "b5858833836509b71d5c0d9bdc11fd1beeeaba5a75be4bbd93581a4d13e0f49a"
+  url "https://ftp.gnu.org/gnu/gnunet/gnunet-0.16.3.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gnunet/gnunet-0.16.3.tar.gz"
+  sha256 "3239052f13537a9aabaaa66ec42875dbee2f6838c5f18b3aef854e6b531ec38a"
   license "AGPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gnunet"
-    sha256 cellar: :any, mojave: "65017ef38be85fafa23e3fe16599c33e89fef5e40218778075a0544fef355628"
+    sha256 cellar: :any, mojave: "9d643315e21faf0c5d9ecad81107b521f7ea7298597a39a5e882c5cafd99be9c"
   end
 
   depends_on "pkg-config" => :build
