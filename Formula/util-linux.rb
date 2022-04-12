0f5620af80e780241c3a9218358f3065a5cb7830
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-12 10:27:32 +0800

    util-linux: update 2.38 bottle.

--- Formula/util-linux.rb
+++ Formula/util-linux.rb
@@ -1,8 +1,8 @@
 class UtilLinux < Formula
   desc "Collection of Linux utilities"
   homepage "https://github.com/karelzak/util-linux"
-  url "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.37/util-linux-2.37.4.tar.xz"
-  sha256 "634e6916ad913366c3536b6468e7844769549b99a7b2bf80314de78ab5655b83"
+  url "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.38/util-linux-2.38.tar.xz"
+  sha256 "6d111cbe4d55b336db2f1fbeffbc65b89908704c01136371d32aa9bec373eb64"
   license all_of: [
     "BSD-3-Clause",
     "BSD-4-Clause-UC",
@@ -15,8 +15,7 @@ class UtilLinux < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/util-linux"
-    rebuild 1
-    sha256 mojave: "2de6013df67e01b3b36c00b67e8f61405e3a5ecb41d6ac58e322ed85bcf3988c"
+    sha256 mojave: "775198d05ea35e869dde97a96f4625cb206182f0d4c313ccf052134eca0ae39b"
   end
 
   keg_only :shadowed_by_macos, "macOS provides the uuid.h header"
@@ -27,26 +26,38 @@ class UtilLinux < Formula
   uses_from_macos "ncurses"
   uses_from_macos "zlib"
 
+  # Everything in following macOS block is for temporary patches
+  # TODO: Remove in the next release.
+  on_macos do
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "gtk-doc" => :build
+    depends_on "libtool" => :build
+    depends_on "pkg-config" => :build
+
+    # Fix ./include/statfs_magic.h:4:10: fatal error: 'sys/statfs.h' file not found
+    patch do
+      url "https://github.com/util-linux/util-linux/commit/478b9d477ecdd8f4e3a7b488524e1d4c6a113525.patch?full_index=1"
+      sha256 "576c26c3d15642f1c44548d0120b192b855cceeebf8ad97fb5e300350e88a3f7"
+    end
+
+    # Fix lib/procfs.c:9:10: fatal error: 'sys/vfs.h' file not found
+    patch do
+      url "https://github.com/util-linux/util-linux/commit/3671d4a878fb58aa953810ecf9af41809317294f.patch?full_index=1"
+      sha256 "d38c9ae06c387da151492dd5862c58551559dd6d2b1877c74cc1e11754221fe4"
+    end
+  end
+
   on_linux do
     conflicts_with "bash-completion", because: "both install `mount`, `rfkill`, and `rtcwake` completions"
     conflicts_with "rename", because: "both install `rename` binaries"
   end
 
-  # Change mkswap.c include order to avoid "c.h" including macOS system <uuid.h> via <grp.h>.
-  # The missing definitions in uuid.h cause error: use of undeclared identifier 'UUID_STR_LEN'.
-  # Issue ref: https://github.com/karelzak/util-linux/issues/1432
-  patch :DATA
-
-  # Fix -flat_namespace being used on Big Sur and later.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-  end
-
   def install
-    args = std_configure_args + %w[
-      --disable-silent-rules
-    ]
+    # Temporary work around for patches. Remove in the next release.
+    system "autoreconf", "--force", "--install", "--verbose" if OS.mac?
+
+    args = %w[--disable-silent-rules]
 
     if OS.mac?
       args << "--disable-ipcs" # does not build on macOS
@@ -54,11 +65,6 @@ class UtilLinux < Formula
       args << "--disable-wall" # already comes with macOS
       args << "--disable-libmount" # does not build on macOS
       args << "--enable-libuuid" # conflicts with ossp-uuid
-
-      # To build `hardlink`, we need to prevent configure from detecting macOS system
-      # <sys/xattr.h>, which doesn't have all expected functions like `lgetxattr`.
-      # Issue ref: https://github.com/karelzak/util-linux/issues/1432
-      inreplace "configure", %r{^\tsys/xattr.h \\\n}, ""
     else
       args << "--disable-use-tty-group" # Fix chgrp: changing group of 'wall': Operation not permitted
       args << "--disable-kill" # Conflicts with coreutils.
@@ -74,7 +80,7 @@ class UtilLinux < Formula
       args << "--without-python"
     end
 
-    system "./configure", *args
+    system "./configure", *std_configure_args, *args
     system "make", "install"
 
     # install completions only for installed programs
@@ -127,31 +133,3 @@ class UtilLinux < Formula
     assert_equal ["d#{perms}", owner, group, "usr"], out
   end
 end
-
-__END__
-diff --git a/disk-utils/mkswap.c b/disk-utils/mkswap.c
-index c45a3a317..0040198c8 100644
---- a/disk-utils/mkswap.c
-+++ b/disk-utils/mkswap.c
-@@ -30,6 +30,10 @@
- # include <linux/fiemap.h>
- #endif
- 
-+#ifdef HAVE_LIBUUID
-+# include <uuid.h>
-+#endif
-+
- #include "linux_version.h"
- #include "swapheader.h"
- #include "strutils.h"
-@@ -42,10 +46,6 @@
- #include "closestream.h"
- #include "ismounted.h"
- 
--#ifdef HAVE_LIBUUID
--# include <uuid.h>
--#endif
--
- #ifdef HAVE_LIBBLKID
- # include <blkid.h>
- #endif
