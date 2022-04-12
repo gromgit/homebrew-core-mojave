1e5811887c0b8c1aab8ee36a528dc6227f5a32ec
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-12 11:02:08 +0800

    argocd-autopilot: update 0.3.2 bottle.

--- Formula/argocd-autopilot.rb
+++ Formula/argocd-autopilot.rb
@@ -2,13 +2,13 @@ class ArgocdAutopilot < Formula
   desc "Opinionated way of installing Argo CD and managing GitOps repositories"
   homepage "https://argoproj.io"
   url "https://github.com/argoproj-labs/argocd-autopilot.git",
-      tag:      "v0.3.1",
-      revision: "4246478fc6d9472199b33146fe52ccb4d7825a52"
+      tag:      "v0.3.2",
+      revision: "3f789c814dfd829b2f59d1a0e4941923ac899b81"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/argocd-autopilot"
-    sha256 cellar: :any_skip_relocation, mojave: "b4f9b6ff46dbdb2b02f11c6aade5b50329a1938fda64cb48df188cb2fe45292a"
+    sha256 cellar: :any_skip_relocation, mojave: "54d75021b062088b3fd9789c2f6207d3a8800ad06d1bd2f0507de0922f06e4d8"
   end
 
   depends_on "go" => :build
