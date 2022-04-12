e37b14b6b76a0d2ece6a90cf8277987f21b60c5b
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-12 23:27:27 +0800

    dosbox-x: update 0.83.24 bottle.

--- Formula/dosbox-x.rb
+++ Formula/dosbox-x.rb
@@ -1,8 +1,8 @@
 class DosboxX < Formula
   desc "DOSBox with accurate emulation and wide testing"
   homepage "https://dosbox-x.com/"
-  url "https://github.com/joncampbell123/dosbox-x/archive/dosbox-x-v0.83.23.tar.gz"
-  sha256 "026f3986aae61d5f5cc7a95c7ad8ee9646f3249b282c8136a00b239bf6fed711"
+  url "https://github.com/joncampbell123/dosbox-x/archive/dosbox-x-v0.83.24.tar.gz"
+  sha256 "f4746f1524cac58756123c7acbbc565e215d7f298a19667fd845dbba040c6021"
   license "GPL-2.0-or-later"
   version_scheme 1
   head "https://github.com/joncampbell123/dosbox-x.git", branch: "master"
@@ -12,9 +12,9 @@ class DosboxX < Formula
     regex(/^dosbox-x[._-]v?(\d+(?:\.\d+)+)$/i)
   end
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dosbox-x"
-    sha256 cellar: :any, mojave: "b2674e12a748bbc1e68316de662dd590d9345754a7567820a6cb6fe81f26f0a7"
+    sha256 cellar: :any, mojave: "7340850d1fa34d2cdf085cb74dd5704053c3b7f27cd078c5c7a40772d4bb54d0"
   end
 
   depends_on "autoconf" => :build
