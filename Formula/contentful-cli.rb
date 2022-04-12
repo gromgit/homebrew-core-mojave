ecec6873bb99500e96634d90fc876964c3d22c8b
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-12 20:30:57 +0800

    contentful-cli: update 1.12.10 bottle.

--- Formula/contentful-cli.rb
+++ Formula/contentful-cli.rb
@@ -3,14 +3,14 @@ require "language/node"
 class ContentfulCli < Formula
   desc "Contentful command-line tools"
   homepage "https://github.com/contentful/contentful-cli"
-  url "https://registry.npmjs.org/contentful-cli/-/contentful-cli-1.12.5.tgz"
-  sha256 "7f4a3cba402dc72a03317ef3e9787f04d24800b3724f08184fc6ab922a491dd2"
+  url "https://registry.npmjs.org/contentful-cli/-/contentful-cli-1.12.10.tgz"
+  sha256 "65ebc1286cb46c5a55356389321c52653ec709b52d6457b1e70d04aaa2e1538f"
   license "MIT"
   head "https://github.com/contentful/contentful-cli.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/contentful-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "648292575d72886d63145e44477bfdf8ec7c0fd802e9b490aa1c2c20507c57dc"
+    sha256 cellar: :any_skip_relocation, mojave: "59b8afd0fa202277b9c935751568b03c9c7c90ce6218d86d8918215ee7c34216"
   end
 
   depends_on "node"
