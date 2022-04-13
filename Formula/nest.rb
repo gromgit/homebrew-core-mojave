aaf5cbac4c1e6d2595b0205aad9584a0a350dfeb
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-14 07:50:14 +0800

    nest: update 3.3_1 bottle.

--- Formula/nest.rb
+++ Formula/nest.rb
@@ -4,64 +4,57 @@ class Nest < Formula
   url "https://github.com/nest/nest-simulator/archive/v3.3.tar.gz"
   sha256 "179462b966cc61f5785d2fee770bc36f86745598ace9cd97dd620622b62043ed"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nest"
-    sha256 mojave: "cbcd7c63f1f6a250237b4adcc12ad213b603ca3fa3c25009be6ffbb6dab04cf6"
+    sha256 mojave: "aedb705bed56f9fc40f01124430ff48668d77e2659465bf6c7f749284f8118e7"
   end
 
   depends_on "cmake" => :build
   depends_on "cython" => :build
   depends_on "gsl"
-  depends_on "libomp"
   depends_on "libtool"
   depends_on "numpy"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
   depends_on "readline"
 
+  uses_from_macos "ncurses"
+
+  on_macos do
+    depends_on "libomp"
+  end
+
   def install
-    args = ["-DCMAKE_INSTALL_PREFIX:PATH=#{prefix}"]
+    # Help FindReadline find macOS system ncurses library
+    sdk = MacOS.sdk_path_if_needed
+    args = sdk ? ["-DNCURSES_LIBRARY=#{sdk}/usr/lib/libncurses.tbd"] : []
 
-    libomp = Formula["libomp"]
-    args << "-Dwith-openmp=ON"
-    args << "-Dwith-libraries=#{libomp.opt_lib}/libomp.dylib"
-    args << "-DOpenMP_C_FLAGS=-Xpreprocessor\ -fopenmp\ -I#{libomp.opt_include}"
-    args << "-DOpenMP_C_LIB_NAMES=omp"
-    args << "-DOpenMP_CXX_FLAGS=-Xpreprocessor\ -fopenmp\ -I#{libomp.opt_include}"
-    args << "-DOpenMP_CXX_LIB_NAMES=omp"
-    args << "-DOpenMP_omp_LIBRARY=#{libomp.opt_lib}/libomp.dylib"
-
-    mkdir "build" do
-      system "cmake", "..", *args
-      system "make"
-      system "make", "install"
-    end
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
 
     # Replace internally accessible gcc with externally accessible version
     # in nest-config if required
-    inreplace bin/"nest-config",
-        %r{#{HOMEBREW_REPOSITORY}/Library/Homebrew/shims.*/super}o,
-        "#{HOMEBREW_PREFIX}/bin"
+    inreplace bin/"nest-config", Superenv.shims_path/ENV.cxx, ENV.cxx
   end
 
   def caveats
-    python = Formula["python@3.9"]
     <<~EOS
       The PyNEST bindings and its dependencies are installed with the python@3.9 formula.
       If you want to use PyNEST, use the Python interpreter from this path:
 
-          #{python.bin}
+          #{Formula["python@3.10"].bin}
 
       You may want to add this to your PATH.
     EOS
   end
 
   test do
-    python = Formula["python@3.9"]
     # check whether NEST was compiled & linked
     system bin/"nest", "--version"
 
     # check whether NEST is importable form python
-    system python.bin/"python3.9", "-c", "'import nest'"
+    system Formula["python@3.10"].bin/"python3.10", "-c", "'import nest'"
   end
 end
