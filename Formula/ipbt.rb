4f326d184ca9cf85c1ffc7dee37cf09ac7c1d636
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-13 13:22:47 +0800

    ipbt: update 20220403 bottle.

--- Formula/ipbt.rb
+++ Formula/ipbt.rb
@@ -1,9 +1,9 @@
 class Ipbt < Formula
   desc "Program for recording a UNIX terminal session"
   homepage "https://www.chiark.greenend.org.uk/~sgtatham/ipbt/"
-  url "https://www.chiark.greenend.org.uk/~sgtatham/ipbt/ipbt-20211203.104f822.tar.gz"
-  version "20211203"
-  sha256 "631ee26dce8d4906e52963bbd7b579c91e9902d0f28903d90415d20ea5b730ba"
+  url "https://www.chiark.greenend.org.uk/~sgtatham/ipbt/ipbt-20220403.d4e7fcd.tar.gz"
+  version "20220403"
+  sha256 "8c7f325166b86055232cca9d745c6a18dcdcb6d30a0685e07ac0eab677912b05"
   license "MIT"
 
   livecheck do
@@ -13,7 +13,7 @@ class Ipbt < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ipbt"
-    sha256 cellar: :any_skip_relocation, mojave: "5d277873f87caeef242225f9dccfe84cbe106853b0f6b26f38155d2a3deac40b"
+    sha256 cellar: :any_skip_relocation, mojave: "873d87d2e2fa807456f4fc9924815e928c1e2b6efa6d45c503ae3a060ff26898"
   end
 
   depends_on "cmake" => :build
