35558188cfb0b79e2fe0d59507e7265e359efadb
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-13 08:00:55 +0800

    hatari: update 2.3.1_1 bottle.

--- Formula/hatari.rb
+++ Formula/hatari.rb
@@ -4,6 +4,7 @@ class Hatari < Formula
   url "https://download.tuxfamily.org/hatari/2.3.1/hatari-2.3.1.tar.bz2"
   sha256 "44a2f62ca995e38d9e0874806956f0b9c3cc84ea89e0169a63849b63cd3b64bd"
   license "GPL-2.0-or-later"
+  revision 1
   head "https://git.tuxfamily.org/hatari/hatari.git", branch: "master"
 
   livecheck do
@@ -11,16 +12,15 @@ class Hatari < Formula
     regex(%r{href=["']?v?(\d+(?:\.\d+)+)/?["' >]}i)
   end
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/hatari"
-    rebuild 1
-    sha256 cellar: :any, mojave: "f7d2dbde54ba8c64d204477a5b739192e3e4cf73a034f6d52b0570c7a8f68409"
+    sha256 cellar: :any, mojave: "1e66851f39b0f9ece627da39eaae58f730568aabc8fe1735c4c43938d4eac289"
   end
 
   depends_on "cmake" => :build
   depends_on "libpng"
   depends_on "portaudio"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
   depends_on "sdl2"
 
   # Download EmuTOS ROM image
@@ -32,7 +32,7 @@ bottle do
   def install
     # Set .app bundle destination
     inreplace "src/CMakeLists.txt", "/Applications", prefix
-    system "cmake", *std_cmake_args, "-DPYTHON_EXECUTABLE=#{Formula["python@3.9"].opt_bin}/python3"
+    system "cmake", *std_cmake_args, "-DPYTHON_EXECUTABLE=#{Formula["python@3.10"].opt_bin}/python3"
     system "make"
     prefix.install "src/Hatari.app"
     bin.write_exec_script "#{prefix}/Hatari.app/Contents/MacOS/hatari"
