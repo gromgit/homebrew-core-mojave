0d8e08ca932117c19bc54247b99992e56eb15c5b
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-13 15:08:59 +0800

    jvm-mon: update 0.3_3 bottle.

--- Formula/jvm-mon.rb
+++ Formula/jvm-mon.rb
@@ -4,7 +4,7 @@ class JvmMon < Formula
   url "https://github.com/ajermakovics/jvm-mon/releases/download/0.3/jvm-mon-0.3.tar.gz"
   sha256 "9b5dd3d280cb52b6e2a9a491451da2ee41c65c770002adadb61b02aa6690c940"
   license "Apache-2.0"
-  revision 2
+  revision 3
 
   livecheck do
     url :stable
@@ -12,23 +12,21 @@ class JvmMon < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any, all: "054e482f025dfb8f3487dadd1a6c4faea207c16b8fe56ecfbdbe293745ddb8b9"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jvm-mon"
+    sha256 cellar: :any_skip_relocation, mojave: "c20d541a04a08a0282c90ed1968fbc03d5be5012f9a73e22b52d2ded67c9a880"
   end
 
-  depends_on arch: :x86_64 # openjdk@8 is not supported on ARM
   depends_on "openjdk@8"
 
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install Dir["*"]
 
-    (bin/"jvm-mon").write_env_script "#{libexec}/bin/jvm-mon",
-      Language::Java.java_home_env("1.8")
-    system "unzip", "-j", libexec/"lib/j2v8_macosx_x86_64-4.6.0.jar", "libj2v8_macosx_x86_64.dylib", "-d", libexec
+    (bin/"jvm-mon").write_env_script libexec/"bin/jvm-mon", Language::Java.java_home_env("1.8")
   end
 
   test do
+    ENV.append "_JAVA_OPTIONS", "-Duser.home=#{testpath}"
     system "echo q | #{bin}/jvm-mon"
   end
 end
