6527e76fe081281786c4c4e224dd6c208b7b8c45
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-13 13:48:03 +0800

    jd: update 1.5.2 bottle.

--- Formula/jd.rb
+++ Formula/jd.rb
@@ -1,15 +1,13 @@
 class Jd < Formula
   desc "JSON diff and patch"
   homepage "https://github.com/josephburnett/jd"
-  url "https://github.com/josephburnett/jd/archive/v1.5.1.tar.gz"
-  sha256 "dd2671e517e5dca4fe9d322dcea7cc16ab83b5d705bce13e0e9c82d43a56fb59"
+  url "https://github.com/josephburnett/jd/archive/v1.5.2.tar.gz"
+  sha256 "ff6cf8fabe65a5786652ab2a3ef762d5dc6b5595a9516f81eb0916fdfe13bab6"
   license "MIT"
-  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jd"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "2077c3c7e889d78c7ea7c64db6a80b0fd0f43e9a3a76a1cc6c39edfd182d005e"
+    sha256 cellar: :any_skip_relocation, mojave: "400b724ad320e4fb26a8576517de8b8928daa37150b280945beb636bddf04151"
   end
 
   depends_on "go" => :build
