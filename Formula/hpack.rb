70bcf98333617cb8ee7d473d5f8b24ee88d35de8
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-13 09:06:45 +0800

    hpack: update 0.34.7 bottle.

--- Formula/hpack.rb
+++ Formula/hpack.rb
@@ -1,15 +1,14 @@
 class Hpack < Formula
   desc "Modern format for Haskell packages"
   homepage "https://github.com/sol/hpack"
-  url "https://github.com/sol/hpack/archive/0.34.6.tar.gz"
-  sha256 "c2a6dc1f3db7bd97ef34d1f40b51d81e6f3775a99082c2d5186abf8f88e48e62"
+  url "https://github.com/sol/hpack/archive/0.34.7.tar.gz"
+  sha256 "640c5a235cda86ac85b8bcb2d08f679c493b8f765a7d8bf36445cb200eb8fab8"
   license "MIT"
   head "https://github.com/sol/hpack.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/hpack"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "5fcfecf75f0186f647150c2a8829ce8e232cca295751648bd85c26c4a7f26bd7"
+    sha256 cellar: :any_skip_relocation, mojave: "51b18b5450cde235a1c1506b30c7b5b16caae4a23ec7832e1c8a726d7d5ddcf7"
   end
 
   depends_on "cabal-install" => :build
