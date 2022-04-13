55014786a417328e5e38aeceef5de697a54556a3
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-13 16:59:37 +0800

    knot: update 3.1.7 bottle.

--- Formula/knot.rb
+++ Formula/knot.rb
@@ -1,8 +1,8 @@
 class Knot < Formula
   desc "High-performance authoritative-only DNS server"
   homepage "https://www.knot-dns.cz/"
-  url "https://secure.nic.cz/files/knot-dns/knot-3.1.6.tar.xz"
-  sha256 "e9ba1305d750dc08fb08687aec7ac55737ca073deaa0b867c884e0c0f2fdb753"
+  url "https://secure.nic.cz/files/knot-dns/knot-3.1.7.tar.xz"
+  sha256 "ffb6887e238ce4c7df0cc76bb55a5093465275201ac12156a3390782dc49857b"
   license all_of: ["GPL-3.0-or-later", "0BSD", "BSD-3-Clause", "LGPL-2.0-or-later", "MIT"]
 
   livecheck do
@@ -12,7 +12,7 @@ class Knot < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/knot"
-    sha256 mojave: "77d2652927c72a450acc5f9d0a03e7147cb1e0ab7e15455a090c48ed4d07132a"
+    sha256 mojave: "6595eabd47be1180bc6d7cd34e47267923812ded782968d50d870bc4e02d7232"
   end
 
   head do
