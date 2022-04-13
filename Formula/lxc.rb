e112d389eb764422cc04d4e9786615bc911c9400
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-13 23:02:11 +0800

    lxc: update 5.0.0 bottle.

--- Formula/lxc.rb
+++ Formula/lxc.rb
@@ -1,8 +1,8 @@
 class Lxc < Formula
   desc "CLI client for interacting with LXD"
   homepage "https://linuxcontainers.org"
-  url "https://linuxcontainers.org/downloads/lxd/lxd-4.24.tar.gz"
-  sha256 "97fae15a1826bc73a45cbf8e9adf775fd9702248863b8a5997de5494eaaeb252"
+  url "https://linuxcontainers.org/downloads/lxd/lxd-5.0.0.tar.gz"
+  sha256 "a99b7edfb52c8195b2de4988844d32d73be6426f6cff28408250517b238fdef9"
   license "Apache-2.0"
 
   livecheck do
@@ -12,7 +12,7 @@ class Lxc < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lxc"
-    sha256 cellar: :any_skip_relocation, mojave: "e66150b98abefab9c4da31b2fd4f378065833020b73d037514f57014a0e7945f"
+    sha256 cellar: :any_skip_relocation, mojave: "85a1f9e8f223c99ef7526faef51696c01a9e2d3141e30476f8e57ff082da91ea"
   end
 
   depends_on "go" => :build
