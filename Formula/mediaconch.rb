bc8473417af93d338466fd13952560bf1b62f5ac
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-14 01:47:32 +0800

    mediaconch: update 22.03 bottle.

--- Formula/mediaconch.rb
+++ Formula/mediaconch.rb
@@ -1,9 +1,8 @@
 class Mediaconch < Formula
   desc "Conformance checker and technical metadata reporter"
   homepage "https://mediaarea.net/MediaConch"
-  url "https://mediaarea.net/download/binary/mediaconch/18.03.2/MediaConch_CLI_18.03.2_GNU_FromSource.tar.bz2"
-  sha256 "8f8f31f1c3eb55449799ebb2031ef373934a0a9826ce6c2b2bdd32dacbf5ec4c"
-  revision 1
+  url "https://mediaarea.net/download/binary/mediaconch/22.03/MediaConch_CLI_22.03_GNU_FromSource.tar.bz2"
+  sha256 "0c35434b55c7f507b20ef13e0a33fdbc4868cca353b145abb5d2cd13c7f11f23"
 
   livecheck do
     url "https://mediaarea.net/MediaConch/Download/Source"
@@ -11,15 +10,8 @@ class Mediaconch < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "49c74edbd5813f2a2c8455bbec92bc3b6ba8e47b286275bb50f61acc550293f2"
-    sha256 cellar: :any,                 arm64_big_sur:  "2bc516280f29cda43dcda638a0a5dd586a34fc52beb724bd97f382825d347d7a"
-    sha256 cellar: :any,                 monterey:       "09fc40239da4fef379b16f29327ba1e9fa72bd08bf361b81e4fdda5a9aaac9a1"
-    sha256 cellar: :any,                 big_sur:        "1ab9e887a8787b4b3655df4f9b01214da00ef466da186db7dca1ae646bb09b3d"
-    sha256 cellar: :any,                 catalina:       "41a49bbafbffc220f140d8e466f1507757cbe552f8de4ca306217affbf1e6dd5"
-    sha256 cellar: :any,                 mojave:         "9d59b85fecc5d5caba622fe57358caab23c8ea904954a137b99e66dd4f7fedec"
-    sha256 cellar: :any,                 high_sierra:    "d59cfb9ac07ffb7eacc4c7970c38676a3909f0966481b99c745735bf87db7b8e"
-    sha256 cellar: :any,                 sierra:         "fdb3934174a68121357c21d4f0800e8bbbaa6a296f3386ab52e5298fde96a6b6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "87ccb6aac84590c501a35ea7ce511f21d1f902a7d37dbdcef722a7c6149dee0f"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mediaconch"
+    sha256 cellar: :any, mojave: "92e05985e70615186410d35b763ff64d2ce9a79375722368149f80cc2e4356b5"
   end
 
   depends_on "pkg-config" => :build
