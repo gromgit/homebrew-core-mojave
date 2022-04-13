03a5a97f2bceb2fb82b1d3d8e06abee8a4ee704f
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-13 22:52:48 +0800

    luaradio: update 0.10.0 bottle.

--- Formula/luaradio.rb
+++ Formula/luaradio.rb
@@ -4,15 +4,12 @@ class Luaradio < Formula
   url "https://github.com/vsergeev/luaradio/archive/v0.10.0.tar.gz"
   sha256 "d540aac3363255c4a1f47313888d9133b037cc5d1edca0d428499a272710b992"
   license "MIT"
-  head "https://github.com/vsergeev/luaradio.git"
+  head "https://github.com/vsergeev/luaradio.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "b04641f0b463cd38e257f954a7b2fb49a5b4fe3ee671a5faa09f9603023f7ed2"
-    sha256 cellar: :any,                 monterey:      "2789e761aa3eb0ff47516350e8e38357086cded52f1e77644ce268171fb32cec"
-    sha256 cellar: :any,                 big_sur:       "765bcff473c15da215a2c162c3247c12b3a12a6a088ff324103de2e05510e973"
-    sha256 cellar: :any,                 catalina:      "e0de1690d1a42741722374cc61a8966a51c9ff8219b46d5e361e06fdcf11e4b4"
-    sha256 cellar: :any,                 mojave:        "535aa76ad7c009e4ffa918eb910462d861081a7516f17a2210275bb6e619ad9c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3172d2fc3864696ad84bab32d36c8fb60f262a71986003b43b5a55f87fa25a7c"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/luaradio"
+    rebuild 1
+    sha256 cellar: :any, mojave: "53b1c3f2d665cf8298727dc5990fd6555e9fe414f3d5f055906318879933be10"
   end
 
   depends_on "pkg-config" => :build
