c1d5cd0fe41ed59a1eb3916a766e549e5bb4b53f
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-13 17:26:25 +0800

    kubeaudit: update 0.17.0 bottle.

--- Formula/kubeaudit.rb
+++ Formula/kubeaudit.rb
@@ -1,15 +1,14 @@
 class Kubeaudit < Formula
   desc "Helps audit your Kubernetes clusters against common security controls"
   homepage "https://github.com/Shopify/kubeaudit"
-  url "https://github.com/Shopify/kubeaudit/archive/refs/tags/0.16.0.tar.gz"
-  sha256 "1f1c21352a5788586e5903dee499668d45318fb388b0cc3860b1a0d09bb489fc"
+  url "https://github.com/Shopify/kubeaudit/archive/refs/tags/0.17.0.tar.gz"
+  sha256 "98351b9498ea053887512cc98e63b4178216dd1e4ad73345ec215ec88dea33fc"
   license "MIT"
   head "https://github.com/Shopify/kubeaudit.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kubeaudit"
-    rebuild 3
-    sha256 cellar: :any_skip_relocation, mojave: "d26d706a15448c612fc5820634ca3932b6831cbd8db1e475b13f872b26a11799"
+    sha256 cellar: :any_skip_relocation, mojave: "d7ea93cd189790635d895993cb86afc3571d65aad2d160c4e0cb22abbfd6acf2"
   end
 
   depends_on "go" => :build
