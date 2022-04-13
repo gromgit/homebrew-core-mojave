8fc7d3fd8ed9958870413f6104be1f019506f84b
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-13 21:26:02 +0800

    libspatialite: update 5.0.1_2 bottle.

--- Formula/libspatialite.rb
+++ Formula/libspatialite.rb
@@ -2,7 +2,7 @@ class Libspatialite < Formula
   desc "Adds spatial SQL capabilities to SQLite"
   homepage "https://www.gaia-gis.it/fossil/libspatialite/index"
   license any_of: ["MPL-1.1", "GPL-2.0-or-later", "LGPL-2.1-or-later"]
-  revision 1
+  revision 2
 
   stable do
     url "https://www.gaia-gis.it/gaia-sins/libspatialite-sources/libspatialite-5.0.1.tar.gz"
@@ -23,13 +23,8 @@ class Libspatialite < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "0a2b9cbcf782fb1550dcb5524a61ca69ec22a2cbbb426506b418b9fe9138e4b6"
-    sha256 cellar: :any,                 arm64_big_sur:  "dc8b7f36b4f7aba60fdb088ec17ca69bddb353fed99a1e6d0200590a0ee0adb2"
-    sha256 cellar: :any,                 monterey:       "ef04ad217e063418d0d404c66c9621de3fc3bfd1c9b8a55d4b512dca9c534ff4"
-    sha256 cellar: :any,                 big_sur:        "f740afb58268b4babe07a123e1b7857b754abea2ffced3b37b8edf7140408652"
-    sha256 cellar: :any,                 catalina:       "abf71f3882743f1bebf560c66a0f211f0298f0498ed0093b29d1feaa6c5c425f"
-    sha256 cellar: :any,                 mojave:         "64a4c76c3cc6aa7ae20c510a4688182af14bb9f1da38bed1409dcd6a1a333a5d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9e92d770bacb404f5e3573b8353ed78735fcb618f93dc070d220a9e9413700eb"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libspatialite"
+    sha256 cellar: :any, mojave: "f924844e5618968c8360fdca3325e9224eea13ea5c35eede335909fa0f1cfbae"
   end
 
   head do
@@ -45,7 +40,7 @@ class Libspatialite < Formula
   depends_on "librttopo"
   depends_on "libxml2"
   depends_on "minizip"
-  depends_on "proj@7"
+  depends_on "proj"
   depends_on "sqlite"
 
   def install
