6cbeaa21aa8d1810690dcf40fc68201dbf721943
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-13 17:15:08 +0800

    kops: update 1.23.0 bottle.

--- Formula/kops.rb
+++ Formula/kops.rb
@@ -1,8 +1,8 @@
 class Kops < Formula
   desc "Production Grade K8s Installation, Upgrades, and Management"
   homepage "https://kops.sigs.k8s.io/"
-  url "https://github.com/kubernetes/kops/archive/v1.22.4.tar.gz"
-  sha256 "8c3cd27e37b7c10c2db0acb0e42f393de7eafb34d28150428edd133d9b6d6915"
+  url "https://github.com/kubernetes/kops/archive/v1.23.0.tar.gz"
+  sha256 "752eb58feb0cb06d224b98d7fa23ac8fe9cd9ed0ffcd2706ab870bf1ad8c7d2a"
   license "Apache-2.0"
   head "https://github.com/kubernetes/kops.git", branch: "master"
 
@@ -13,7 +13,7 @@ class Kops < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kops"
-    sha256 cellar: :any_skip_relocation, mojave: "35c887d46d9eee5ea23a7933e94c6875318339bbd06580444a6c5e37b1357e8b"
+    sha256 cellar: :any_skip_relocation, mojave: "1f21ae0c2b89c11c001c505a6c6cde3a169760bad998d67afe0b3a94cf0b30cb"
   end
 
   depends_on "go" => :build
