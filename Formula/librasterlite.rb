a6d4312220cca672aca499b4cd8635c66811de9e
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-13 21:29:51 +0800

    librasterlite: update 1.1g_8 bottle.

--- Formula/librasterlite.rb
+++ Formula/librasterlite.rb
@@ -11,19 +11,18 @@ class Librasterlite < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3c1d39f40a46b6bf657e0bf55600787e07c1262e348268a3e7b2412685c90612"
-    sha256 cellar: :any,                 arm64_big_sur:  "9a685c2da2eca32a3b8e277e224465cb37c896717043407e64bcf54dbbe2fe67"
-    sha256 cellar: :any,                 monterey:       "75e64137694f0d9908da6201eb12fb428d6350026b796618338035724c69946c"
-    sha256 cellar: :any,                 big_sur:        "cf093e2b4e2d5e1e86f089d0dd47cf9fcb7197e2f7028cedc728ce174e5abeaa"
-    sha256 cellar: :any,                 catalina:       "87bda7a32b6106dd10a0a7b69ab9e2eb7795d72f09be14a264e3cf57c37706e0"
-    sha256 cellar: :any,                 mojave:         "58f04df469c79fdf8e57077e7ed2226bca0ebda941b527950baefc792df365ac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6daf56bea60e8cff42c3375976009255c9998ae09c65d939700f81f1db4a1449"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/librasterlite"
+    rebuild 1
+    sha256 cellar: :any, mojave: "4b2882718d1c67589617048c3c1d9552621eba87d9133523cbdf434467793e13"
   end
+
+  deprecate! date: "2022-03-05", because: :deprecated_upstream
 
   depends_on "pkg-config" => :build
   depends_on "libgeotiff"
   depends_on "libpng"
   depends_on "libspatialite"
+  depends_on "proj@7"
   depends_on "sqlite"
 
   def install
