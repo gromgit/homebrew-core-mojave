b7c902ebb8798891c6cd33326ea0d509c1d861d6
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-14 18:56:44 +0800

    ringojs: update 3.0.0 bottle.

--- Formula/ringojs.rb
+++ Formula/ringojs.rb
@@ -1,10 +1,9 @@
 class Ringojs < Formula
   desc "CommonJS-based JavaScript runtime"
   homepage "https://ringojs.org"
-  url "https://github.com/ringo/ringojs/releases/download/v2.0.0/ringojs-2.0.0.tar.gz"
-  sha256 "5991953012f3c493abb8c7256fa48e885bd284976bd1ec36f20fef77ff37fac9"
+  url "https://github.com/ringo/ringojs/releases/download/v3.0.0/ringojs-3.0.0.tar.gz"
+  sha256 "7f37388f5c0f05deec29c429151478a3758510566707bc0baf91f865126ca526"
   license "Apache-2.0"
-  revision 2
 
   livecheck do
     url :stable
@@ -12,12 +11,8 @@ class Ringojs < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d737033338e11f8af0d51d220ad0cc9ce4d64231670489ca3036b3df338f6970"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "86b956cc3c7d0279529323450abaf3bca2a61ae520cdc262c44a2ff5035b810c"
-    sha256 cellar: :any_skip_relocation, monterey:       "e704fda56cce28b10c4bdb8a3d1362e9d48178c826f676bbf33d138f615e3632"
-    sha256 cellar: :any_skip_relocation, big_sur:        "445095a17c58cb61c8275e174c2630cd4ac2acba61198fdf7fe978544815ad9d"
-    sha256 cellar: :any_skip_relocation, catalina:       "445095a17c58cb61c8275e174c2630cd4ac2acba61198fdf7fe978544815ad9d"
-    sha256 cellar: :any_skip_relocation, mojave:         "445095a17c58cb61c8275e174c2630cd4ac2acba61198fdf7fe978544815ad9d"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ringojs"
+    sha256 cellar: :any_skip_relocation, mojave: "6b31c53a9b6168901c52dddfccb9671150401e2a2873f7708c6fce35605bf1e0"
   end
 
   depends_on "openjdk@11"
