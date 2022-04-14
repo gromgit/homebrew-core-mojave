f68d62a20a3661eba729a74b7551ff2e585c5251
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-14 23:06:43 +0800

    stm32flash: update 0.7 bottle.

--- Formula/stm32flash.rb
+++ Formula/stm32flash.rb
@@ -3,6 +3,7 @@ class Stm32flash < Formula
   homepage "https://sourceforge.net/projects/stm32flash/"
   url "https://downloads.sourceforge.net/project/stm32flash/stm32flash-0.7.tar.gz"
   sha256 "c4c9cd8bec79da63b111d15713ef5cc2cd947deca411d35d6e3065e227dc414a"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url :stable
@@ -11,7 +12,8 @@ class Stm32flash < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/stm32flash"
-    sha256 cellar: :any_skip_relocation, mojave: "61c75507293df503522a068434b8c4ac62ca92b63f7edbf24b5c1c765da2e9d8"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "0118a701843f0d74899fdba66d41faff5fb9113bf842a98ef107400d429d7d24"
   end
 
   def install
