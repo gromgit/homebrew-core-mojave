98b1aa0da5eb36867c7c26243dda9b5630a7223d
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-14 15:28:18 +0800

    pgweb: update 0.11.11 bottle.

--- Formula/pgweb.rb
+++ Formula/pgweb.rb
@@ -1,13 +1,13 @@
 class Pgweb < Formula
   desc "Web-based PostgreSQL database browser"
   homepage "https://sosedoff.github.io/pgweb/"
-  url "https://github.com/sosedoff/pgweb/archive/v0.11.10.tar.gz"
-  sha256 "df2d16daa6f8fa9735b713199691ef74136f19fbbee14778248e41e247a1b93b"
+  url "https://github.com/sosedoff/pgweb/archive/v0.11.11.tar.gz"
+  sha256 "4d8c64db7ec463a9366d404cbaf12215db855a5bdbf09253494d79dedd92db98"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pgweb"
-    sha256 cellar: :any_skip_relocation, mojave: "4c0ac360423864b7ba1f8be9612b268f13709796a0490d2548f7465da0ec7553"
+    sha256 cellar: :any_skip_relocation, mojave: "8c01721efe7d36ca9ed8146b1591f2de69f5b2f93ea51351174746ba70d361d5"
   end
 
   depends_on "go" => :build
