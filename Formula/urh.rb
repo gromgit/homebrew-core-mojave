8d9c2d5bb00cf7878e077d984e0df52b89545a8a
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-15 21:37:14 +0800

    urh: update 2.9.3_1 bottle.

--- Formula/urh.rb
+++ Formula/urh.rb
@@ -1,22 +1,20 @@
 class Urh < Formula
   desc "Universal Radio Hacker"
   homepage "https://github.com/jopohl/urh"
-  url "https://files.pythonhosted.org/packages/06/d8/f140e9c0f592134580819b959121b47bce042694168032bf8b219d39c977/urh-2.9.2.tar.gz"
-  sha256 "e4fac51af73a69eeca25d9a12b777677b3b983de8537a2025ba698e20e6a56af"
+  url "https://files.pythonhosted.org/packages/c2/3d/9cbaac6d7101f50c408ac428d9e37668916a4a3e22292f38748b230239e0/urh-2.9.3.tar.gz"
+  sha256 "037b91bb87a113ac03d0695e0c2b5cce35d0886469b3ef46ba52d2342c8cfd8c"
   license "GPL-3.0-only"
+  revision 1
   head "https://github.com/jopohl/urh.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "f932d73089bd4efbec62d2d043c6a971518a9f92a6f54640862b4bf06a5d2d7a"
-    sha256 cellar: :any, arm64_big_sur:  "09dc76c4ab1a6fade85309ced90029fadec5e323768c4bf373f016b25c5aef73"
-    sha256 cellar: :any, big_sur:        "9e250394aeddc6784c732b991f583ad6cdac03888c3f3e7ff477c8cfda2a7856"
-    sha256 cellar: :any, catalina:       "6a0dd1e02d615789893fbf96ef3d1bc127dd927033fbfc0d6a42945c55d22070"
-    sha256 cellar: :any, mojave:         "8c61a1fb6e05c52a17c4bd2d5b22fe2b3de41e4fb8cf4199de4eceef2bd661dc"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/urh"
+    sha256 cellar: :any, mojave: "03bae8ab8ec6b385078baa5376cf92799b9900ea733c9bddfee50945a1436dcb"
   end
 
   depends_on "pkg-config" => :build
-  depends_on "cython"
   depends_on "hackrf"
+  depends_on "libcython"
   depends_on "numpy"
   depends_on "pyqt@5"
   depends_on "python@3.9"
@@ -35,7 +33,7 @@ class Urh < Formula
       end
     end
 
-    ENV.prepend_create_path "PYTHONPATH", Formula["cython"].opt_libexec/"lib/python#{xy}/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", Formula["libcython"].opt_libexec/"lib/python#{xy}/site-packages"
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
 
     system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(libexec)
