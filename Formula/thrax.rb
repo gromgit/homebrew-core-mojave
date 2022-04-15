f1a2d4b4580b723e9e41d394e76b106eb2dbd9ff
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-15 19:37:50 +0800

    thrax: update 1.3.8 bottle.

--- Formula/thrax.rb
+++ Formula/thrax.rb
@@ -3,10 +3,9 @@ class Thrax < Formula
 
   desc "Tools for compiling grammars into finite state transducers"
   homepage "https://www.openfst.org/twiki/bin/view/GRM/Thrax"
-  url "https://www.openfst.org/twiki/pub/GRM/ThraxDownload/thrax-1.3.6.tar.gz"
-  sha256 "5f00a2047674753cba6783b010ab273366dd3dffc160bdb356f7236059a793ba"
+  url "https://www.openfst.org/twiki/pub/GRM/ThraxDownload/thrax-1.3.8.tar.gz"
+  sha256 "e21c449798854f7270bb5ac723f6a8d292e149fc6bbe24fd9f345c85aabc7cd4"
   license "Apache-2.0"
-  revision 1
 
   livecheck do
     url "https://www.openfst.org/twiki/bin/view/GRM/ThraxDownload"
@@ -15,8 +14,14 @@ class Thrax < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/thrax"
-    sha256 cellar: :any, mojave: "5e9001cc07788c0d20ff062e6bfc5804f700ea42938d8f9e6318c5a2e464e4e1"
+    sha256 cellar: :any, mojave: "568229d715edfa5746c6064eed4cdd5ef460d0864da6e6e85e84c6db06b404e5"
   end
+
+  # Regenerate `configure` to avoid `-flat_namespace` bug.
+  # None of our usual patches apply.
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
 
   depends_on "openfst"
 
@@ -27,13 +32,8 @@ class Thrax < Formula
 
   fails_with gcc: "5"
 
-  # Fix -flat_namespace being used on Big Sur and later.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-  end
-
   def install
+    system "autoreconf", "--force", "--install", "--verbose"
     system "./configure", *std_configure_args
     system "make", "install"
     rewrite_shebang detected_python_shebang, bin/"thraxmakedep" if OS.linux?
