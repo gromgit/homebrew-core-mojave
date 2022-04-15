43cb0d0e3bedf9520d8f3163d4d5b8e1aa7218db
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-15 20:14:23 +0800

    triangle: update 1.2.5 bottle.

--- Formula/triangle.rb
+++ Formula/triangle.rb
@@ -1,18 +1,13 @@
 class Triangle < Formula
   desc "Convert images to computer generated art using Delaunay triangulation"
   homepage "https://github.com/esimov/triangle"
-  url "https://github.com/esimov/triangle/archive/v1.2.3.tar.gz"
-  sha256 "28296d6b469c212967192f236c8139de1e315d23113f108eec641ce5e39553b5"
+  url "https://github.com/esimov/triangle/archive/v1.2.5.tar.gz"
+  sha256 "e7b729601023620aaba3707db80aeeeee06286e131095da25d248a8325d2c549"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "563d4012a2846639399afbee7a3e075ce9fb0579beedf8dd8106406e61775838"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "704b24d681e71a55c8c6d3888524ace351bb8240cefddf91060f8d6ead3fbf08"
-    sha256 cellar: :any_skip_relocation, monterey:       "42f96ab8e1bb5879b0e0be8fd44ac830c6f8bc5f96866205ee50c6608dec6cec"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0b51538505ae028c96c122c565345e97257aef8103614afb14ecce0e5feeafe7"
-    sha256 cellar: :any_skip_relocation, catalina:       "5d3948f4093b5bc4f9b5410643eafd26a1d7c1bc87a2eb5ed7126447a46a675b"
-    sha256 cellar: :any_skip_relocation, mojave:         "a2549fdd2296b72dc857cfc4fcb89f07f4cf71f2419133ddb06eef63e3c7a7f8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1dd51ff7e1d80c1107c0c95b854db0168fda43a65a7e4bfca3201ad3d949e7ab"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/triangle"
+    sha256 cellar: :any_skip_relocation, mojave: "31e06e9c3ae3f27c7b30973b38dbbec83aa4f740261f51fa8f21f141c887fa0c"
   end
 
   # Bump to 1.18 on the next release, if possible.
