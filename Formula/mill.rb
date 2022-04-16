d2b683c1fd6810ce03729183b6ddf9449d87ca1f
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-16 09:35:44 +0800

    mill: no rebuilding required

--- Formula/mill.rb
+++ Formula/mill.rb
@@ -1,8 +1,8 @@
 class Mill < Formula
   desc "Scala build tool"
   homepage "https://com-lihaoyi.github.io/mill/mill/Intro_to_Mill.html"
-  url "https://github.com/com-lihaoyi/mill/releases/download/0.10.2/0.10.2-assembly"
-  sha256 "971e6c689746b0c4bb0cb6949e0a1abb3152d541b8418beb10b12f4e321ad684"
+  url "https://github.com/com-lihaoyi/mill/releases/download/0.10.3/0.10.3-assembly"
+  sha256 "a5342282df2c1bee00cc373715abff427dbb08d1f49481ee43740c039b8c76a2"
   license "MIT"
 
   livecheck do
@@ -11,7 +11,7 @@ class Mill < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "2eaf33525ef50ca292e1af003f469a2c18214a97e98fe768e49c17b88adb34e0"
+    sha256 cellar: :any_skip_relocation, all: "ac6ab1643981b94eb34fd3570c56d9039acbd1caa8868a4ca392409917873bd5"
   end
 
   depends_on "openjdk"
