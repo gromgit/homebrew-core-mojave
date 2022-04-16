359623d95df3c4bed7cb3f0b542da672ddc29734
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-16 09:35:27 +0800

    easy-git: no rebuilding required

--- Formula/easy-git.rb
+++ Formula/easy-git.rb
@@ -1,17 +1,15 @@
 class EasyGit < Formula
   desc "Wrapper to simplify learning and using git"
-  homepage "https://people.gnome.org/~newren/eg/"
-  url "https://people.gnome.org/~newren/eg/download/1.7.5.2/eg"
-  sha256 "59bb4f8b267261ab3d48c66b957af851d1a61126589173ebcc20ba9f43c382fb"
-
-  livecheck do
-    url "https://people.gnome.org/~newren/eg/download/"
-    regex(%r{href=.*?(\d+(?:\.\d+)+)/eg["' >]}i)
-  end
+  homepage "https://github.com/newren/easygit/"
+  url "https://github.com/newren/easygit/archive/v1.8.5.tar.gz"
+  sha256 "e2c6ac7fb390de1440a15808e5460b959bfb5000add11af91757ab61c48dead7"
+  license "GPL-2.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "d32fde25bfa6487f1ed7b4ca157b3d38cf64100b77d20c3ec35a0bf9b88894b9"
+    sha256 cellar: :any_skip_relocation, all: "4f1bd257689c7102a91799fdb7ca4a1e64638aa08ec1dad92070c875e6021a04"
   end
+
+  deprecate! date: "2022-04-12", because: :unmaintained
 
   def install
     bin.install "eg"
