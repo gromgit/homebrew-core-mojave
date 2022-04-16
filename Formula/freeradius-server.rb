9a7bee5cba6c9877447c84847e006f27afc43131
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-16 09:35:30 +0800

    freeradius-server: no rebuilding required

--- Formula/freeradius-server.rb
+++ Formula/freeradius-server.rb
@@ -1,10 +1,19 @@
 class FreeradiusServer < Formula
   desc "High-performance and highly configurable RADIUS server"
   homepage "https://freeradius.org/"
-  url "https://github.com/FreeRADIUS/freeradius-server/archive/release_3_0_25.tar.gz"
-  sha256 "493b0b9bef3d9f0e6949fcfd6aa282164f6b8e6404231f2b0d1353739199ffcd"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
-  head "https://github.com/FreeRADIUS/freeradius-server.git"
+  head "https://github.com/FreeRADIUS/freeradius-server.git", branch: "master"
+
+  stable do
+    url "https://github.com/FreeRADIUS/freeradius-server/archive/release_3_0_25.tar.gz"
+    sha256 "493b0b9bef3d9f0e6949fcfd6aa282164f6b8e6404231f2b0d1353739199ffcd"
+
+    # Fix -flat_namespace being used
+    patch do
+      url "https://github.com/FreeRADIUS/freeradius-server/commit/6c1cdb0e75ce36f6fadb8ade1a69ba5e16283689.patch?full_index=1"
+      sha256 "7e7d055d72736880ca8e1be70b81271dd02f2467156404280a117cb5dc8dccdc"
+    end
+  end
 
   livecheck do
     url :stable
@@ -12,11 +21,13 @@ class FreeradiusServer < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "f4ed69d19363b792ffba75c41b703bd6f068a2d65d76a3046ba6947832b6a6b3"
-    sha256 big_sur:       "a28e06a16084beb496edcb341dcf5aaf7f6da6ef995bd7383545d59aac76f393"
-    sha256 catalina:      "6fe8001066708fc01ba0c71a7a0246aee4af4910a68f155d82fd411a7a826c1b"
-    sha256 mojave:        "660eb25588c2d0c68aae1684bfc9f9174c6c6d4d721d196a6232c981718e9ebf"
-    sha256 x86_64_linux:  "d71cb20afdd11e9793b6299e241474f8f4d07174625809f07f228ec06cfeb950"
+    sha256 arm64_monterey: "2c4a942fb9f20633bf947ec0b8551731677fa0bdc54b7054be9c367bec8c1fcd"
+    sha256 arm64_big_sur:  "f4ed69d19363b792ffba75c41b703bd6f068a2d65d76a3046ba6947832b6a6b3"
+    sha256 monterey:       "afe19281cfa3f33f2836cea24c1efd2d98d3e185f9cab5faabd45addc34fdf20"
+    sha256 big_sur:        "a28e06a16084beb496edcb341dcf5aaf7f6da6ef995bd7383545d59aac76f393"
+    sha256 catalina:       "6fe8001066708fc01ba0c71a7a0246aee4af4910a68f155d82fd411a7a826c1b"
+    sha256 mojave:         "660eb25588c2d0c68aae1684bfc9f9174c6c6d4d721d196a6232c981718e9ebf"
+    sha256 x86_64_linux:   "d71cb20afdd11e9793b6299e241474f8f4d07174625809f07f228ec06cfeb950"
   end
 
   depends_on "openssl@1.1"
