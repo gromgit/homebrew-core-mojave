30271a079c731db4941e65887e2922a10001f42b
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-16 09:35:59 +0800

    valgrind:

--- Formula/valgrind.rb
+++ Formula/valgrind.rb
@@ -1,13 +1,17 @@
 class Valgrind < Formula
   desc "Dynamic analysis tools (memory, debug, profiling)"
   homepage "https://www.valgrind.org/"
-  url "https://sourceware.org/pub/valgrind/valgrind-3.18.1.tar.bz2"
-  sha256 "00859aa13a772eddf7822225f4b46ee0d39afbe071d32778da4d99984081f7f5"
+  url "https://sourceware.org/pub/valgrind/valgrind-3.19.0.tar.bz2"
+  sha256 "dd5e34486f1a483ff7be7300cc16b4d6b24690987877c3278d797534d6738f02"
   license "GPL-2.0-or-later"
 
   livecheck do
     url "https://sourceware.org/pub/valgrind/"
     regex(/href=.*?valgrind[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 x86_64_linux: "e8a9a03173c9fb58a583f4ea872a7ed9c3cdb0605d0ff6327fbd8b2ec9fe21de"
   end
 
   head do
