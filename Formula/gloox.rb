1491eded550a2954dffc5cbc9a5d0c0b89c8b514
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-16 09:35:33 +0800

    gloox: no rebuilding required

--- Formula/gloox.rb
+++ Formula/gloox.rb
@@ -10,12 +10,14 @@ class Gloox < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "d3a7c4aa4b061fe4dce520662e632bc9983b294cb94e479ea46221f43ea46c9f"
-    sha256 cellar: :any,                 big_sur:       "143b0a77842e9df885799b07e6cfb166c7951841b8b23027db51c9b93a5ba8a7"
-    sha256 cellar: :any,                 catalina:      "decf46b20a794855cf3bd3c06e05111d15fd11de4dec1c5fdf6a1253eb865e7a"
-    sha256 cellar: :any,                 mojave:        "607baeeadc43775af6799d5bc4715239cbe6455ec72d2e14d82523d425fa7799"
-    sha256 cellar: :any,                 high_sierra:   "011c8a88d0f8970c9ad4ed339972b55b56c26120e64ef9d1576b68c03b10f706"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "20accd40e451cec9b2bf58e94be2ff78af6db0d719ec905fa5e2123049e63267"
+    sha256 cellar: :any,                 arm64_monterey: "7ec91986b31b93a0271334d280e1838d14bab2af7d62f813f20e7c0df5f4e564"
+    sha256 cellar: :any,                 arm64_big_sur:  "d3a7c4aa4b061fe4dce520662e632bc9983b294cb94e479ea46221f43ea46c9f"
+    sha256 cellar: :any,                 monterey:       "fbd2eedb0aa00300684582f8cf54f071e84c05e917ea2360ee29e17d8c767523"
+    sha256 cellar: :any,                 big_sur:        "143b0a77842e9df885799b07e6cfb166c7951841b8b23027db51c9b93a5ba8a7"
+    sha256 cellar: :any,                 catalina:       "decf46b20a794855cf3bd3c06e05111d15fd11de4dec1c5fdf6a1253eb865e7a"
+    sha256 cellar: :any,                 mojave:         "607baeeadc43775af6799d5bc4715239cbe6455ec72d2e14d82523d425fa7799"
+    sha256 cellar: :any,                 high_sierra:    "011c8a88d0f8970c9ad4ed339972b55b56c26120e64ef9d1576b68c03b10f706"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "20accd40e451cec9b2bf58e94be2ff78af6db0d719ec905fa5e2123049e63267"
   end
 
   depends_on "pkg-config" => :build
