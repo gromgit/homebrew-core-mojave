ef57326f1ee1cddac81b4354ff0bf3a684a9a24d
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-16 09:35:39 +0800

    lft: no rebuilding required

--- Formula/lft.rb
+++ Formula/lft.rb
@@ -17,6 +17,7 @@ class Lft < Formula
     sha256 cellar: :any_skip_relocation, catalina:       "c0b69000709a507f2ec0cc2ff286910e6f2629169367828cfdc26e184654f787"
     sha256 cellar: :any_skip_relocation, mojave:         "83d6fa2b78fb9780fecb9287407825d1731f1c91da30bb75b15f26e632e0720b"
     sha256 cellar: :any_skip_relocation, high_sierra:    "e0370a6053bedd5c24f62583c2d19c3d0d2fab2fa5cf9003561e60694dad8642"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4745d9fd33ab816b785c90883a2b69e0e3e70b5176246e66adff849aada2eddf"
   end
 
   uses_from_macos "libpcap"
