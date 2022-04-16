fe37e8a13a915205385b608641ba334ef8b63fa9
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-16 09:35:57 +0800

    topgrade:

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -1,17 +1,17 @@
 class Topgrade < Formula
   desc "Upgrade all the things"
   homepage "https://github.com/r-darwish/topgrade"
-  url "https://github.com/r-darwish/topgrade/archive/v8.3.0.tar.gz"
-  sha256 "a818cbdc64aafe77a589299d5717988fd5e5403af0998a9945b9d17a5b6f499b"
+  url "https://github.com/r-darwish/topgrade/archive/v8.3.1.tar.gz"
+  sha256 "f90f25b1701e544ca1eb935b552065c0eca584eaff659920148f278aa36ee10b"
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "fc26d3fc86b470e937565feadad718581658bf6e64fd51c8cc82dd704da3d5a4"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "28c59ae0bd0d8cc997d87b55f0343aecd722be0b44c39940c95ffe1967526542"
-    sha256 cellar: :any_skip_relocation, monterey:       "a758d48253e222e78e6a2a8fddcd5076fc0ca85890e8e89759fb3d100e091abc"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0f23b782598430c3870fa542ee43e0d655e38542888047a0a8eaf967ebbecfb0"
-    sha256 cellar: :any_skip_relocation, catalina:       "88051de1a733b8e7d89da3692494c496a54f44b183e853516f54d6d49c42eb6a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bb329e1aa7af290a3261ab2ca3caf50fe2ce18b7a82a7e465ac0da6e0a0bde3f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3f6563b3c082a921179b30af0e59b2df3a86d7fedcf2fb64945166b5f0691c76"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7831e9589ef7360c8a34e1300b89b8aec7be73109d55e93f92ad328bde066cb6"
+    sha256 cellar: :any_skip_relocation, monterey:       "a7f55084bc043d824ce30011ce617ccd6f3ce96f366ffdce413642138c40adbf"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9c429848a1b5ea1d67c23078b24de00a2df7a745a583f426557a2141d26fba32"
+    sha256 cellar: :any_skip_relocation, catalina:       "eeb60ffc1bcdcc0fc581354ae6a3c046a4f55c545a8f0a5439af7536709b9f8a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c3191f48d6ec0db5bdb91a62f67a749e762daa5bd4e4d7f2be5e1fddb1c97c7c"
   end
 
   depends_on "rust" => :build
