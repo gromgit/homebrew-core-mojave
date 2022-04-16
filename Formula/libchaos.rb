6e363b073d9d417e5179cdb148fd115408f4501c
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-16 09:35:40 +0800

    libchaos: no rebuilding required

--- Formula/libchaos.rb
+++ Formula/libchaos.rb
@@ -7,11 +7,13 @@ class Libchaos < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e1b5eff28e28622055d915653c66c2448cba0cb207a8b8f243525c2deb1dd246"
-    sha256 cellar: :any_skip_relocation, big_sur:       "2b51e7e88ad2f47cdb860d3edbf65a9db6a1a0feeefbb46dae978f3b4311f20f"
-    sha256 cellar: :any_skip_relocation, catalina:      "8cd295f6ccf1c6a09ab87bef06331424da21b0b44da8f4440a11f4fccaf1370a"
-    sha256 cellar: :any_skip_relocation, mojave:        "3add0509ec493248105ad81354c4ffccef85f37c0cc445db24f115b0b8fb3576"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "8d1f167a096fae20de66286d9f33a7b93e03fcfccaecd5b15611e3fcd7c4b09c"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5cbe23d7c195b8cb1e7336596112bb1f84b3579cca069b5bc9b61e41c640e32f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e1b5eff28e28622055d915653c66c2448cba0cb207a8b8f243525c2deb1dd246"
+    sha256 cellar: :any_skip_relocation, monterey:       "018d34f680d426fab143744ab7413cfdb8db204ac5bc0a77de9767a2802bbf5c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2b51e7e88ad2f47cdb860d3edbf65a9db6a1a0feeefbb46dae978f3b4311f20f"
+    sha256 cellar: :any_skip_relocation, catalina:       "8cd295f6ccf1c6a09ab87bef06331424da21b0b44da8f4440a11f4fccaf1370a"
+    sha256 cellar: :any_skip_relocation, mojave:         "3add0509ec493248105ad81354c4ffccef85f37c0cc445db24f115b0b8fb3576"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "8d1f167a096fae20de66286d9f33a7b93e03fcfccaecd5b15611e3fcd7c4b09c"
   end
 
   depends_on "cmake" => :build
