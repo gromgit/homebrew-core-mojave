bfee366bf4f115097a8e36126c698e1a4470d40c
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-16 09:35:21 +0800

    coccinelle: no rebuilding required

--- Formula/coccinelle.rb
+++ Formula/coccinelle.rb
@@ -13,12 +13,13 @@ class Coccinelle < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "ec7ef55264ac1fca61319d7f7df11cf3d6cfd4702a53e726209f62555e9b71d3"
-    sha256 cellar: :any, arm64_big_sur:  "78e87db9e0aabffbddde513ae5100fc015792fd75c5bd1d5ca91bb53342b575c"
-    sha256 cellar: :any, monterey:       "bc4160ec45c892d9b0df334eb6830474554d64bc398fc6735d7de7b532f9a663"
-    sha256 cellar: :any, big_sur:        "f050cd80796be603afea32e24bd860c98543162d7e95e195902f5c267c2edaeb"
-    sha256 cellar: :any, catalina:       "61befc08516da9ace4eecc7f4bbd8e8b041ed709ee5f9fc8024bf2667056c3e6"
-    sha256 cellar: :any, mojave:         "4f2ca36bdd4c52eb8a074f047c231f41c941b5a1a4aa1624ec5301735e478c91"
+    sha256 cellar: :any,                 arm64_monterey: "ec7ef55264ac1fca61319d7f7df11cf3d6cfd4702a53e726209f62555e9b71d3"
+    sha256 cellar: :any,                 arm64_big_sur:  "78e87db9e0aabffbddde513ae5100fc015792fd75c5bd1d5ca91bb53342b575c"
+    sha256 cellar: :any,                 monterey:       "bc4160ec45c892d9b0df334eb6830474554d64bc398fc6735d7de7b532f9a663"
+    sha256 cellar: :any,                 big_sur:        "f050cd80796be603afea32e24bd860c98543162d7e95e195902f5c267c2edaeb"
+    sha256 cellar: :any,                 catalina:       "61befc08516da9ace4eecc7f4bbd8e8b041ed709ee5f9fc8024bf2667056c3e6"
+    sha256 cellar: :any,                 mojave:         "4f2ca36bdd4c52eb8a074f047c231f41c941b5a1a4aa1624ec5301735e478c91"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7ca3ce2e6886652238f53154bc32828ddf9902151c1f06c2650510279c407534"
   end
 
   depends_on "autoconf" => :build
@@ -29,6 +30,8 @@ class Coccinelle < Formula
   depends_on "pkg-config" => :build
   depends_on "ocaml"
   depends_on "pcre"
+
+  uses_from_macos "unzip" => :build
 
   # Bootstap resource for Ocaml 4.12 compatibility.
   # Remove when Coccinelle supports Ocaml 4.12 natively
