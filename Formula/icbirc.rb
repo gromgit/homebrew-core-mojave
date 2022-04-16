5adb73d8fa1ea14fdecda47a8d5b5e69ab7071e1
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-16 09:35:35 +0800

    icbirc: no rebuilding required

--- Formula/icbirc.rb
+++ Formula/icbirc.rb
@@ -10,14 +10,16 @@ class Icbirc < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "7b2e4accc4480ed21b70273e1bc92895e016e8100ce1e144b8dd3194d7d5217c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "60c1ccdb6ca739d0d4694d2f075507f4417cada103204c6b91ee966350ca745a"
-    sha256 cellar: :any_skip_relocation, catalina:      "c1a639673100e6c8f2f5116b11c4c7a8b38cc0af73841c4d13e611f7605a8c1e"
-    sha256 cellar: :any_skip_relocation, mojave:        "c353062cf16183b658ca999e477f2f4ac6040dd8d3a995fe2736a382d989ca8e"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "e258e2ca2bf835d76b7d509eac5417629451068c85fe729cbab7fc64e89df9c0"
-    sha256 cellar: :any_skip_relocation, sierra:        "cbec4e472c640a63081f12723fc9d144f00aa00c9229ce5bfc2edd99199aee74"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "2f943e4af7a9c1e2524d9583b0ef5539988f68f56a8f8c483b2c2d1990fff21d"
-    sha256 cellar: :any_skip_relocation, yosemite:      "c4fdfe55febee01d5fc84bdc7bb49ad6bb2366f6f5a54dfbb851ee891a67c5a3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9aa9c813f70949fb1d7784cba81133d726fb9b32aca1bc057680e92003b67640"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7b2e4accc4480ed21b70273e1bc92895e016e8100ce1e144b8dd3194d7d5217c"
+    sha256 cellar: :any_skip_relocation, monterey:       "0ac58ac9e6553f986983d97b5db7c20cc2d28d2eb6abdf2f136c16f455876342"
+    sha256 cellar: :any_skip_relocation, big_sur:        "60c1ccdb6ca739d0d4694d2f075507f4417cada103204c6b91ee966350ca745a"
+    sha256 cellar: :any_skip_relocation, catalina:       "c1a639673100e6c8f2f5116b11c4c7a8b38cc0af73841c4d13e611f7605a8c1e"
+    sha256 cellar: :any_skip_relocation, mojave:         "c353062cf16183b658ca999e477f2f4ac6040dd8d3a995fe2736a382d989ca8e"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "e258e2ca2bf835d76b7d509eac5417629451068c85fe729cbab7fc64e89df9c0"
+    sha256 cellar: :any_skip_relocation, sierra:         "cbec4e472c640a63081f12723fc9d144f00aa00c9229ce5bfc2edd99199aee74"
+    sha256 cellar: :any_skip_relocation, el_capitan:     "2f943e4af7a9c1e2524d9583b0ef5539988f68f56a8f8c483b2c2d1990fff21d"
+    sha256 cellar: :any_skip_relocation, yosemite:       "c4fdfe55febee01d5fc84bdc7bb49ad6bb2366f6f5a54dfbb851ee891a67c5a3"
   end
 
   depends_on "bsdmake" => :build
