1e3968db567ff4979615d133572291b9fb0ac708
Adrian Ho <the.gromgit@gmail.com>
2022-04-18 03:58:56 +0800

    awscli: update 2.5.5 bottle.

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -3,14 +3,14 @@ class Awscli < Formula
 
   desc "Official Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
-  url "https://github.com/aws/aws-cli/archive/2.5.4.tar.gz"
-  sha256 "e933e9d9210407eeb62f9218cac84eb38d6d90eda7a7f41b255ad60813c4d237"
+  url "https://github.com/aws/aws-cli/archive/2.5.5.tar.gz"
+  sha256 "8029a9a590d8afbf32e8a87c03106f288c86c6dadd15bd8c74928b733d258557"
   license "Apache-2.0"
   head "https://github.com/aws/aws-cli.git", branch: "v2"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/awscli"
-    sha256 cellar: :any, mojave: "d533fefdc193026f9f49b7ec09346120b1f0a125e888318396cda5c94e0a68bc"
+    sha256 cellar: :any, mojave: "0665b693cb94bc2547fc799128cb7126cc9e9bc22c668c6b966a9ed17fbc058a"
   end
 
   depends_on "cmake" => :build
