cc4e9a2bf856a00b98963e552b4f58079916dff8
Adrian Ho <the.gromgit@gmail.com>
2022-04-18 00:27:44 +0800

    antlr4-cpp-runtime: update 4.10 bottle.

--- Formula/antlr4-cpp-runtime.rb
+++ Formula/antlr4-cpp-runtime.rb
@@ -1,8 +1,8 @@
 class Antlr4CppRuntime < Formula
   desc "ANother Tool for Language Recognition C++ Runtime Library"
   homepage "https://www.antlr.org/"
-  url "https://www.antlr.org/download/antlr4-cpp-runtime-4.9.3-source.zip"
-  sha256 "5f0af6efd81f476c3e775c486eb0a71c25d6bbc14373e88a64690e2738d68e03"
+  url "https://www.antlr.org/download/antlr4-cpp-runtime-4.10-source.zip"
+  sha256 "417b5d5ff2df0c3a642fc7c56b7754344ed28b5aab861e9e8c8982663fa76762"
   license "BSD-3-Clause"
 
   livecheck do
@@ -12,16 +12,18 @@ class Antlr4CppRuntime < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/antlr4-cpp-runtime"
-    rebuild 2
-    sha256 cellar: :any, mojave: "2d5c846842821224d4292a5767c98f51a7fa737a5c49d088c4dba8efb9f2a93a"
+    sha256 cellar: :any, mojave: "0f3192bb8249b54d0ac6225951c4fd84a41b6b4f0fdcb8f779be644a712ae378"
   end
 
   depends_on "cmake" => :build
 
   on_linux do
     depends_on "pkg-config" => :build
+    depends_on "gcc"
     depends_on "util-linux"
   end
+
+  fails_with gcc: "5"
 
   def install
     system "cmake", ".", "-DANTLR4_INSTALL=ON", *std_cmake_args
@@ -40,7 +42,7 @@ class Antlr4CppRuntime < Formula
           return 0 ;
       }
     EOS
-    system ENV.cxx, "-std=c++11", "-I#{include}/antlr4-runtime", "test.cc",
+    system ENV.cxx, "-std=c++17", "-I#{include}/antlr4-runtime", "test.cc",
                     "-L#{lib}", "-lantlr4-runtime", "-o", "test"
     system "./test"
   end
