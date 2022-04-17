507e66a28c32cc9722bc1b0728a2bbd719342856
Adrian Ho <the.gromgit@gmail.com>
2022-04-17 10:52:01 +0800

    libavif: update 0.10.1 bottle.

--- Formula/libavif.rb
+++ Formula/libavif.rb
@@ -1,13 +1,13 @@
 class Libavif < Formula
   desc "Library for encoding and decoding .avif files"
   homepage "https://github.com/AOMediaCodec/libavif"
-  url "https://github.com/AOMediaCodec/libavif/archive/refs/tags/v0.10.0.tar.gz"
-  sha256 "d289e5029cb3853630ca85716b25b5fb9cdec51f8bd537b05f43b3325b480ab0"
+  url "https://github.com/AOMediaCodec/libavif/archive/refs/tags/v0.10.1.tar.gz"
+  sha256 "66e82854ceb84a3e542bc140a343bc90e56c68f3ecb4fff63e636c136ed9a05e"
   license "BSD-2-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libavif"
-    sha256 cellar: :any, mojave: "2663b558b493a8a7fa531fd26e0ad345be9bd2e88ab49ef2d65b0789e3ffee3c"
+    sha256 cellar: :any, mojave: "8dfc92aada757d48629542dfe0be1ec2ef1c6415502c8af585b3eaace44fbd2d"
   end
 
   depends_on "cmake" => :build
