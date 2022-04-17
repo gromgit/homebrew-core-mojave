d96302807435a4239ff9632aa4e5ce5be05b4881
Adrian Ho <the.gromgit@gmail.com>
2022-04-18 03:23:42 +0800

    autorestic: update 1.6.2 bottle.

--- Formula/autorestic.rb
+++ Formula/autorestic.rb
@@ -1,14 +1,14 @@
 class Autorestic < Formula
   desc "High level CLI utility for restic"
   homepage "https://autorestic.vercel.app/"
-  url "https://github.com/cupcakearmy/autorestic/archive/v1.5.8.tar.gz"
-  sha256 "ca132142d2b682002d59232c390df7f426493c01f377cf33bd8712de0c2e4bf9"
+  url "https://github.com/cupcakearmy/autorestic/archive/v1.6.2.tar.gz"
+  sha256 "3ad6e2e9bb7ad8b48c1f4405995ed757cb3d6707ec141e12969b2f14e8240459"
   license "Apache-2.0"
   head "https://github.com/cupcakearmy/autorestic.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/autorestic"
-    sha256 cellar: :any_skip_relocation, mojave: "4da03c4df37c69e4b40a056d790e3ce3114efcb78c9c3ee9245393f9ae20dbfe"
+    sha256 cellar: :any_skip_relocation, mojave: "9980f596fcca46f6c139bb62a58e3653d238c2271563667d427c85664ab04f42"
   end
 
   depends_on "go" => :build
