7c145040edba1272e50320e4518808dbbeb54a98
Adrian Ho <the.gromgit@gmail.com>
2022-04-18 11:15:51 +0800

    ephemeralpg: update 3.2 bottle.

--- Formula/ephemeralpg.rb
+++ Formula/ephemeralpg.rb
@@ -1,8 +1,8 @@
 class Ephemeralpg < Formula
   desc "Run tests on an isolated, temporary Postgres database"
   homepage "https://eradman.com/ephemeralpg/"
-  url "https://eradman.com/ephemeralpg/code/ephemeralpg-3.1.tar.gz"
-  sha256 "4693d195778c09a8e4b0fd3ec6790efcc7b4887e922d8f417bca7c8fe214e2aa"
+  url "https://eradman.com/ephemeralpg/code/ephemeralpg-3.2.tar.gz"
+  sha256 "c07df30687191dc632460d96997561d0adfc32b198f3b59b14081783f4a1b95d"
 
   livecheck do
     url "https://eradman.com/ephemeralpg/code/"
@@ -10,14 +10,8 @@ class Ephemeralpg < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f61b619659f12955247d431e3c93ff71c3c851833f1b25c00f53c919a1828b12"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fa5007de85a1480bfa6ccbbd8b82040ec9d70084cb9a95d33e0a0fcbd406a3d8"
-    sha256 cellar: :any_skip_relocation, monterey:       "48d7bf38bcc5097d0f197ae89f111ea58f5152356b6c1c8e1d55a03a2943bdd3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "30816c4f32b0ba3a38e436626a9d59f74f1f655e51c74616470908f56ea86720"
-    sha256 cellar: :any_skip_relocation, catalina:       "0ebc56c6b29ac11305a81437a0c8aa5e6b31f9ab58daad8b695e3560870f09a3"
-    sha256 cellar: :any_skip_relocation, mojave:         "56d56bf1bac23530fcdeb3d9b0f2161cac9ae606fdb19d61a08617a825cf31a6"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "ff9f13d039de049edbc0b9c085e3d49b263fe1d1a2c0e1f4c8184f121e435c9d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3cfc5ecbe7ff3fd431e659143dc39b0cf7c81e5cde19fcbd0397ce6c2ab01edb"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ephemeralpg"
+    sha256 cellar: :any_skip_relocation, mojave: "52682ed55c5b62766213a2df0c7b742870642309784ad0ef1749ec9d63f0db62"
   end
 
   depends_on "postgresql"
