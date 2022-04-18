a6b0e4e6e20f93b2ff5774f8446acd5336e15a6e
Adrian Ho <the.gromgit@gmail.com>
2022-04-18 09:12:31 +0800

    dialog: update 1.3-20220414 bottle.

--- Formula/dialog.rb
+++ Formula/dialog.rb
@@ -1,8 +1,8 @@
 class Dialog < Formula
   desc "Display user-friendly message boxes from shell scripts"
   homepage "https://invisible-island.net/dialog/"
-  url "https://invisible-mirror.net/archives/dialog/dialog-1.3-20220117.tgz"
-  sha256 "754cb6bf7dc6a9ac5c1f80c13caa4d976e30a5a6e8b46f17b3bb9b080c31041f"
+  url "https://invisible-mirror.net/archives/dialog/dialog-1.3-20220414.tgz"
+  sha256 "493781718642cdd58bdba1cdf2a26776ca855908710d813bd2e93810dc49ece2"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -12,7 +12,7 @@ class Dialog < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dialog"
-    sha256 cellar: :any_skip_relocation, mojave: "c5a8d8998daf016296079f5cbc7f3ba09f1f6337314eee0f67c4e93eb18e3361"
+    sha256 cellar: :any_skip_relocation, mojave: "bc74282cb985a20906e57b5f1d03afcfc0c3af4060fb2f91bc6f65c5e5276435"
   end
 
   uses_from_macos "ncurses"
