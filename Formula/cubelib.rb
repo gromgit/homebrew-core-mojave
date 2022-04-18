1acc125e7f2c82cc108dd031f1c76330a32f1e10
Adrian Ho <the.gromgit@gmail.com>
2022-04-18 09:00:47 +0800

    cubelib: update 4.7 bottle.

--- Formula/cubelib.rb
+++ Formula/cubelib.rb
@@ -1,8 +1,9 @@
 class Cubelib < Formula
   desc "Cube, is a performance report explorer for Scalasca and Score-P"
   homepage "https://scalasca.org/software/cube-4.x/download.html"
-  url "https://apps.fz-juelich.de/scalasca/releases/cube/4.6/dist/cubelib-4.6.tar.gz"
-  sha256 "36eaffa7688db8b9304c9e48ca5dc4edc2cb66538aaf48657b9b5ccd7979385b"
+  url "https://apps.fz-juelich.de/scalasca/releases/cube/4.7/dist/cubelib-4.7.tar.gz", using: :homebrew_curl
+  sha256 "e44352c80a25a49b0fa0748792ccc9f1be31300a96c32de982b92477a8740938"
+  license "BSD-3-Clause"
 
   livecheck do
     url :homepage
@@ -10,24 +11,38 @@ class Cubelib < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "6b647d7c7673d0b4b95c7240dc477ee13e0fdd5c9f8952bd9b8d80b0f3f55d4d"
-    sha256 big_sur:       "2680056fa693e78f9f89fcaa55b67888c4aa2d947cc4a2c9d2167e3380782517"
-    sha256 catalina:      "5b03ba168b4d88c74b68409a9d7fc5b6aca796a78b7e55d29a1cba2d29e1595a"
-    sha256 mojave:        "5eca16fc5e707d28e6595f070a49516d65a46aa495b95ab78616777027e36114"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cubelib"
+    sha256 mojave: "acf8de2d467f51c378d7842995d68c889e953e8ad72c0113e9ecfd4412bba1aa"
+  end
+
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+    directory "build-frontend"
   end
 
   def install
     ENV.deparallelize
 
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--with-nocross-compiler-suite=clang",
-                          "CXXFLAGS=-stdlib=libc++",
-                          "LDFLAGS=-stdlib=libc++",
-                          "--prefix=#{prefix}"
+    args = %w[--disable-silent-rules]
+    if ENV.compiler == :clang
+      args << "--with-nocross-compiler-suite=clang"
+      args << "CXXFLAGS=-stdlib=libc++"
+      args << "LDFLAGS=-stdlib=libc++"
+    end
+
+    system "./configure", *std_configure_args, *args
     system "make"
     system "make", "install"
+
+    inreplace pkgshare/"cubelib.summary", "#{Superenv.shims_path}/", ""
   end
 
   test do
