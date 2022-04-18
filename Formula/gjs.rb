89e77cbfd610093944422a7ec1f14b324cc2fc8f
Adrian Ho <the.gromgit@gmail.com>
2022-04-18 14:38:21 +0800

    gjs: update 1.72.0 bottle.

--- Formula/gjs.rb
+++ Formula/gjs.rb
@@ -1,84 +1,35 @@
 class Gjs < Formula
   desc "JavaScript Bindings for GNOME"
   homepage "https://gitlab.gnome.org/GNOME/gjs/wikis/Home"
-  url "https://download.gnome.org/sources/gjs/1.70/gjs-1.70.0.tar.xz"
-  sha256 "4b0629341a318a02374e113ab97f9a9f3325423269fc1e0b043a5ffb01861c5f"
+  url "https://download.gnome.org/sources/gjs/1.72/gjs-1.72.0.tar.xz"
+  sha256 "3ef0caf716e3920dd61f7748f6d56e476cc0fc18351ad0548e7df1a0ab9bdcad"
   license all_of: ["LGPL-2.0-or-later", "MIT"]
-  revision 1
+  head "https://gitlab.gnome.org/GNOME/gjs.git", branch: "master"
 
   bottle do
-    sha256 big_sur:  "ecea427c28fcc26093cf7650d82a73b4a04257d2b53ba6e472f9bb1c0b313e5e"
-    sha256 catalina: "92cfd6a80537fe3e3a661cfd6feb941e58820ca4d3649dc98a3cc5b76765f3de"
-    sha256 mojave:   "572ab35a86ea01c0c9e3ba1ede49c769bc67c9532593bc6846e7bb42c619af55"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gjs"
+    sha256 mojave: "16de87e72f73db098fc54ed7a6686ec5ec20a58e6e91a2a919b92b3607f9162b"
   end
 
-  depends_on "autoconf@2.13" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
-  depends_on "pkg-config" => :build
-  depends_on "python@3.8" => :build
-  depends_on "rust" => :build
-  depends_on "six" => :build
   depends_on "gobject-introspection"
-  depends_on "gtk+3"
-  depends_on "llvm"
-  depends_on "nspr"
   depends_on "readline"
+  depends_on "spidermonkey"
 
-  resource "mozjs78" do
-    url "https://archive.mozilla.org/pub/firefox/releases/78.10.1esr/source/firefox-78.10.1esr.source.tar.xz"
-    sha256 "c41f45072b0eb84b9c5dcb381298f91d49249db97784c7e173b5f210cd15cf3f"
+  on_linux do
+    depends_on "gcc"
   end
 
+  fails_with gcc: "5" # meson ERROR: SpiderMonkey sanity check: DID NOT COMPILE
+
   def install
-    ENV.cxx11
-
-    resource("mozjs78").stage do
-      inreplace "build/moz.configure/toolchain.configure",
-                "sdk_max_version = Version('10.15.4')",
-                "sdk_max_version = Version('11.99')"
-      inreplace "config/rules.mk",
-                "-install_name $(_LOADER_PATH)/$(SHARED_LIBRARY) ",
-                "-install_name #{lib}/$(SHARED_LIBRARY) "
-      inreplace "old-configure", "-Wl,-executable_path,${DIST}/bin", ""
-
-      mkdir("build") do
-        ENV["PYTHON"] = which("python3")
-        ENV["_MACOSX_DEPLOYMENT_TARGET"] = ENV["MACOSX_DEPLOYMENT_TARGET"]
-        ENV["CC"] = Formula["llvm"].opt_bin/"clang"
-        ENV["CXX"] = Formula["llvm"].opt_bin/"clang++"
-        ENV.prepend_path "PATH", buildpath/"autoconf/bin"
-        system "../js/src/configure", "--prefix=#{prefix}",
-                              "--with-system-nspr",
-                              "--with-system-zlib",
-                              "--with-system-icu",
-                              "--enable-readline",
-                              "--enable-shared-js",
-                              "--enable-optimize",
-                              "--enable-release",
-                              "--with-intl-api",
-                              "--disable-jemalloc"
-        system "make"
-        system "make", "install"
-        rm Dir["#{bin}/*"]
-      end
-      # headers were installed as softlinks, which is not acceptable
-      cd(include.to_s) do
-        `find . -type l`.chomp.split.each do |link|
-          header = File.readlink(link)
-          rm link
-          cp header, link
-        end
-      end
-      ENV.append_path "PKG_CONFIG_PATH", "#{lib}/pkgconfig"
-      rm "#{lib}/libjs_static.ajs"
-    end
-
     # ensure that we don't run the meson post install script
     ENV["DESTDIR"] = "/"
 
     args = std_meson_args + %w[
       -Dprofiler=disabled
+      -Dreadline=enabled
       -Dinstalled_tests=false
       -Dbsymbolic_functions=false
       -Dskip_dbus_tests=true
