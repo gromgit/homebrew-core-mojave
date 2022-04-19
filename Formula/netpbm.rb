7d62b67e8305822f656fb9ede20e303452713c0e
Adrian Ho <the.gromgit@gmail.com>
2022-04-19 19:26:02 +0800

    netpbm: update 10.86.32 bottle.

--- Formula/netpbm.rb
+++ Formula/netpbm.rb
@@ -3,8 +3,8 @@ class Netpbm < Formula
   homepage "https://netpbm.sourceforge.io/"
   # Maintainers: Look at https://sourceforge.net/p/netpbm/code/HEAD/tree/
   # for stable versions and matching revisions.
-  url "https://svn.code.sf.net/p/netpbm/code/stable", revision: "4259"
-  version "10.86.30"
+  url "https://svn.code.sf.net/p/netpbm/code/stable", revision: "4311"
+  version "10.86.32"
   license "GPL-3.0-or-later"
   version_scheme 1
   head "https://svn.code.sf.net/p/netpbm/code/trunk"
@@ -17,7 +17,7 @@ class Netpbm < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/netpbm"
-    sha256 cellar: :any, mojave: "95be170155d611d8ec8333e3aafb71a15fbd5cf428bd3a088f0bb062bae90354"
+    sha256 cellar: :any, mojave: "75e280d6d63fdbc69035de26140036d952cad8b66c03966f05f8f1d0ee51d072"
   end
 
   depends_on "jasper"
@@ -32,10 +32,6 @@ class Netpbm < Formula
   conflicts_with "jbigkit", because: "both install `pbm.5` and `pgm.5` files"
 
   def install
-    # Fix file not found errors for /usr/lib/system/libsystem_symptoms.dylib and
-    # /usr/lib/system/libsystem_darwin.dylib on 10.11 and 10.12, respectively
-    ENV["SDKROOT"] = MacOS.sdk_path if MacOS.version <= :sierra
-
     cp "config.mk.in", "config.mk"
 
     inreplace "config.mk" do |s|
