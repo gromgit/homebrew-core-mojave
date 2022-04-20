3e66eb131a9505c790214b0d555f84ea67c1272a
Adrian Ho <the.gromgit@gmail.com>
2022-04-21 00:19:16 +0800

    glibmm: update 2.72.0 bottle.

--- Formula/glibmm.rb
+++ Formula/glibmm.rb
@@ -1,18 +1,13 @@
 class Glibmm < Formula
   desc "C++ interface to glib"
   homepage "https://www.gtkmm.org/"
-  url "https://download.gnome.org/sources/glibmm/2.70/glibmm-2.70.0.tar.xz"
-  sha256 "8008fd8aeddcc867a3f97f113de625f6e96ef98cf7860379813a9c0feffdb520"
+  url "https://download.gnome.org/sources/glibmm/2.72/glibmm-2.72.0.tar.xz"
+  sha256 "782924bf136496f3878fdc2a0aa9ef40f0c515e2c3e054caffa5d2e52380c71e"
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "1ebb6889448d05a7fa6772588946d090464ba3acca808692d72ab32d2e6304cf"
-    sha256 cellar: :any, arm64_big_sur:  "85cf4f30e430474bb31b4d40331658d4abbbd6656b0fd545e83757289863545c"
-    sha256 cellar: :any, monterey:       "0a8cf3b36eef323ccfee7b4489c4783ecf4afb573ee8a74b692c36eb82801152"
-    sha256 cellar: :any, big_sur:        "edf7767c19d7af3fa0029a77b900b160856bfc1d65b789e58ad9001c40cd6552"
-    sha256 cellar: :any, catalina:       "371c5642bb40285423561696728f0bb760197d5971961819e82846f452d0659b"
-    sha256 cellar: :any, mojave:         "6b17198a3468451bf0019b02dbeffe9fa0907876964b63981cebaf53966dcbf8"
-    sha256               x86_64_linux:   "f000a536bbbfc6100c9fd63f8b7ec6e674867ab75e86e8132da1ef2b3c409988"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/glibmm"
+    sha256 cellar: :any, mojave: "3e59e35ca52ac4518f4f1589dbb0ba10b7805507af2aa03bf98112488c0f0554"
   end
 
   depends_on "meson" => :build
