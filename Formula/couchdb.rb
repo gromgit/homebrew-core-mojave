c1b95ea5c7688ee1d7632adb537071142ad36229
Adrian Ho <the.gromgit@gmail.com>
2022-04-22 21:06:05 +0800

    couchdb: update 3.2.1_3 bottle.

--- Formula/couchdb.rb
+++ Formula/couchdb.rb
@@ -5,6 +5,7 @@ class Couchdb < Formula
   mirror "https://archive.apache.org/dist/couchdb/source/3.2.1/apache-couchdb-3.2.1.tar.gz"
   sha256 "11de2d1c3a5b317017a7459ec3f76230d5c43aba427a1e71ca3437845874acf8"
   license "Apache-2.0"
+  revision 3
 
   livecheck do
     url :homepage
@@ -13,7 +14,7 @@ class Couchdb < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/couchdb"
-    sha256 cellar: :any, mojave: "cc894fe585487a52fa48c37a7b9de00468f056f64d1109d92217b16cf5fa6fde"
+    sha256 cellar: :any, mojave: "daee9816cf0d6260a039d97d63c1ae0ea754ca54d9fbcbcc68fca612d5b055c5"
   end
 
   depends_on "autoconf" => :build
@@ -24,12 +25,36 @@ class Couchdb < Formula
   depends_on "pkg-config" => :build
   depends_on "icu4c"
   depends_on "openssl@1.1"
+  # NOTE: Supported `spidermonkey` versions are hardcoded at
+  # https://github.com/apache/couchdb/blob/#{version}/src/couch/rebar.config.script
   depends_on "spidermonkey"
+
+  on_linux do
+    depends_on "gcc"
+  end
 
   conflicts_with "ejabberd", because: "both install `jiffy` lib"
 
+  fails_with :gcc do
+    version "5"
+    cause "mfbt (and Gecko) require at least gcc 6.1 to build."
+  end
+
+  # Add support for SpiderMonkey 91esr. Remove in the next release.
+  # PR ref: https://github.com/apache/couchdb/pull/3842
+  patch do
+    url "https://github.com/apache/couchdb/commit/cb6aff46b65b68fd48293971a11c29633a0e21ff.patch?full_index=1"
+    sha256 "c32bc73937dd598cfc433a44098823e069665e6c85e8ec24f6da2ba56b42b02a"
+  end
+
   def install
-    system "./configure"
+    spidermonkey = Formula["spidermonkey"]
+    inreplace "src/couch/rebar.config.script" do |s|
+      s.gsub! "-I/usr/local/include/mozjs", "-I#{spidermonkey.opt_include}/mozjs"
+      s.gsub! "-L/usr/local/lib", "-L#{spidermonkey.opt_lib} -L#{HOMEBREW_PREFIX}/lib"
+    end
+
+    system "./configure", "--spidermonkey-version", spidermonkey.version.major
     system "make", "release"
     # setting new database dir
     inreplace "rel/couchdb/etc/default.ini", "./data", "#{var}/couchdb/data"
