215c97b7934db1d8f840eff4aba7e1c9149792d0
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-23 14:53:29 +0800

    julius: no rebuilding required

--- Formula/julius.rb
+++ Formula/julius.rb
@@ -6,11 +6,14 @@ class Julius < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, monterey:    "73f943f1011686778fdae712733eb0987f97976c6f0ab26d9771c57eccf304c9"
-    sha256 cellar: :any, big_sur:     "4b8251857584f844fe5469a0283a773428383053f8d80eaeff885b745578aa1d"
-    sha256 cellar: :any, catalina:    "b06b9ca71df4cccff10e36a4a75a55f7d5bdb009f4dba9f940044da6ba0c258d"
-    sha256 cellar: :any, mojave:      "041d7a3185850375ef67148a74ab9513e9a4eb6de05deeb3595f3941c41010d6"
-    sha256 cellar: :any, high_sierra: "d699dbf645c69f795421569e21c9d676e0db534a8d72fabfb721d5864e391549"
+    sha256 cellar: :any,                 arm64_monterey: "61e0a2f95974a4fdfaee2253963fe3ab20284c4325e0f34aa22bc4a9e40a09c0"
+    sha256 cellar: :any,                 arm64_big_sur:  "dcbb2b7bfd4ba078ec6473c2193ca6fefd3f1cbe6375bd662401a5b607d99387"
+    sha256 cellar: :any,                 monterey:       "73f943f1011686778fdae712733eb0987f97976c6f0ab26d9771c57eccf304c9"
+    sha256 cellar: :any,                 big_sur:        "4b8251857584f844fe5469a0283a773428383053f8d80eaeff885b745578aa1d"
+    sha256 cellar: :any,                 catalina:       "b06b9ca71df4cccff10e36a4a75a55f7d5bdb009f4dba9f940044da6ba0c258d"
+    sha256 cellar: :any,                 mojave:         "041d7a3185850375ef67148a74ab9513e9a4eb6de05deeb3595f3941c41010d6"
+    sha256 cellar: :any,                 high_sierra:    "d699dbf645c69f795421569e21c9d676e0db534a8d72fabfb721d5864e391549"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "576576ac4069a3fd9e89e71ac2cefe9f3d1e666b9aa92915f248f8bffcf19de5"
   end
 
   depends_on "libsndfile"
