70ee68149e42f06d72478975ca83a23d088b2b83
Adrian Ho <the.gromgit@gmail.com>
2022-04-23 22:32:47 +0800

    pcre2: update 10.40 bottle.

--- Formula/pcre2.rb
+++ Formula/pcre2.rb
@@ -1,26 +1,9 @@
 class Pcre2 < Formula
   desc "Perl compatible regular expressions library with a new API"
   homepage "https://www.pcre.org/"
+  url "https://github.com/PhilipHazel/pcre2/releases/download/pcre2-10.40/pcre2-10.40.tar.bz2"
+  sha256 "14e4b83c4783933dc17e964318e6324f7cae1bc75d8f3c79bc6969f00c159d68"
   license "BSD-3-Clause"
-
-  # Remove `stable` block next release when patches are no longer needed
-  stable do
-    url "https://github.com/PhilipHazel/pcre2/releases/download/pcre2-10.39/pcre2-10.39.tar.bz2"
-    sha256 "0f03caf57f81d9ff362ac28cd389c055ec2bf0678d277349a1a4bee00ad6d440"
-
-    # enable JIT again in Apple Silicon with 11.2+ (sljit PR zherczeg/sljit#105)
-    patch :p2 do
-      url "https://github.com/zherczeg/sljit/commit/d6a0fa61e09266ad2e36d8ccd56f775e37b749e9.patch?full_index=1"
-      sha256 "8d699f6c8ae085f50cf8823dcfadb8591f7ad8f9aa0db9666bd126bb625d7543"
-      directory "src/sljit"
-    end
-
-    # https://lists.gnu.org/archive/html/libtool-patches/2020-06/msg00001.html
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-      sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-    end
-  end
 
   livecheck do
     url :stable
@@ -29,8 +12,7 @@ class Pcre2 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pcre2"
-    rebuild 2
-    sha256 cellar: :any, mojave: "d8c3481370fb4cb998a9dfd1444fce305e399bbd94e6c261dc538d631b5552b5"
+    sha256 cellar: :any, mojave: "f89cd4fbcea48259afd443c3b337d32bf627d92589aabfef4570e5a7d9ce290b"
   end
 
   head do
@@ -54,6 +36,8 @@ class Pcre2 < Formula
       --enable-pcre2grep-libbz2
       --enable-jit
     ]
+
+    args << "--enable-pcre2test-libedit" if OS.mac?
 
     system "./autogen.sh" if build.head?
 
