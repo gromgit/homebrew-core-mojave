4534fe33fdff1ea89be3af63de3bb1c6b11e0537
Adrian Ho <the.gromgit@gmail.com>
2022-04-24 02:50:29 +0800

    kumactl: update 1.6.0 bottle.

--- Formula/kumactl.rb
+++ Formula/kumactl.rb
@@ -1,8 +1,8 @@
 class Kumactl < Formula
   desc "Kuma control plane command-line utility"
   homepage "https://kuma.io/"
-  url "https://github.com/kumahq/kuma/archive/1.5.0.tar.gz"
-  sha256 "a75109f0200428d1ebba81c7f79d50cee4e91bfd3a63d92f36f0b896c52d718d"
+  url "https://github.com/kumahq/kuma/archive/1.6.0.tar.gz"
+  sha256 "5dfd6c561a0b3e190868df00f4ec4aa93a9ba159cabe982b98b20c5c0c5df6aa"
   license "Apache-2.0"
 
   livecheck do
@@ -12,8 +12,7 @@ class Kumactl < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kumactl"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "eb2ce696253bcfaf496edc6f3b0fca5e2b8c760cc9a2b083cccf605e71e3f9f8"
+    sha256 cellar: :any_skip_relocation, mojave: "2978390e531e401febd382072478956464ef3ce49f1cd5693d92b16f7be20024"
   end
 
   depends_on "go" => :build
