54f136f90811c3fa8678612d84a2b903ef1b57fe
Adrian Ho <the.gromgit@gmail.com>
2022-04-24 03:14:15 +0800

    libuv: update 1.44.1_1 bottle.

--- Formula/libuv.rb
+++ Formula/libuv.rb
@@ -4,6 +4,7 @@ class Libuv < Formula
   url "https://github.com/libuv/libuv/archive/v1.44.1.tar.gz"
   sha256 "e91614e6dc2dd0bfdd140ceace49438882206b7a6fb00b8750914e67a9ed6d6b"
   license "MIT"
+  revision 1
   head "https://github.com/libuv/libuv.git", branch: "v1.x"
 
   livecheck do
@@ -13,7 +14,7 @@ class Libuv < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libuv"
-    sha256 cellar: :any, mojave: "680f156b9b526a8edef63ab936a96ecd944846564e9ef0e10a0ade36e134ce7e"
+    sha256 cellar: :any, mojave: "91b03224bbe70b911545f405ae4e488f768d70c5df0fb94aac11a6e56f85de78"
   end
 
   depends_on "autoconf" => :build
@@ -21,6 +22,13 @@ class Libuv < Formula
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "sphinx-doc" => :build
+
+  # Fix posix_spawnp bug on macOS. https://github.com/Homebrew/homebrew-core/issues/99688
+  # Remove at next release.
+  patch do
+    url "https://github.com/libuv/libuv/commit/7c9b3938df9cc1eb9e1efec249a171c31b1a9a3a.patch?full_index=1"
+    sha256 "5fb73337d841504ad514709146ba6715f6a6f91e69a9b07b48dee4e9b6617bdc"
+  end
 
   def install
     # This isn't yet handled by the make install process sadly.
