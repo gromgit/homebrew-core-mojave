cfe4e0798dba0be521e19300299c79e5e68c4974
Adrian Ho <the.gromgit@gmail.com>
2022-04-23 18:48:05 +0800

    consul-template: update 0.29.0 bottle.

--- Formula/consul-template.rb
+++ Formula/consul-template.rb
@@ -2,14 +2,14 @@ class ConsulTemplate < Formula
   desc "Generic template rendering and notifications with Consul"
   homepage "https://github.com/hashicorp/consul-template"
   url "https://github.com/hashicorp/consul-template.git",
-      tag:      "v0.28.0",
-      revision: "ae2bbca18a8cf6c549b73a60dd26a86c814b95ed"
+      tag:      "v0.29.0",
+      revision: "34ada7ac2a590a2cd39882ec10dde38d744afd72"
   license "MPL-2.0"
   head "https://github.com/hashicorp/consul-template.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/consul-template"
-    sha256 cellar: :any_skip_relocation, mojave: "f8e12aeae4b7233f5061e8e3663da254388242c0043a9aa184bb5718f414c182"
+    sha256 cellar: :any_skip_relocation, mojave: "1369c475cce29f8d67de4af6909188201338cfb0b27079a5c52a5b7f010194fd"
   end
 
   depends_on "go" => :build
