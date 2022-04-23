cac4ebfb069099a3d8ea088752e5b5ffe66ff6bb
Adrian Ho <the.gromgit@gmail.com>
2022-04-24 00:08:58 +0800

    ical-buddy: update 1.10.1_1 bottle.

--- Formula/ical-buddy.rb
+++ Formula/ical-buddy.rb
@@ -1,22 +1,31 @@
 class IcalBuddy < Formula
   desc "Get events and tasks from the macOS calendar database"
   homepage "https://hasseg.org/icalBuddy/"
-  url "https://github.com/DavidKaluta/icalBuddy64/releases/download/v1.10.1/icalBuddy-v1.10.1.zip"
-  sha256 "720a6a3344ce32c2cab7c3d2b686ad8de8d9744b747ac48b275247ed54cb3945"
+  url "https://github.com/dkaluta/icalBuddy64/archive/refs/tags/v1.10.1.tar.gz"
+  sha256 "aff42b809044efbf9a1f7df7598e9e110c1c4de0a4c27ddccde5ea325ddc4b77"
   license "MIT"
-  head "https://github.com/DavidKaluta/icalBuddy64.git", branch: "master"
+  revision 1
+  head "https://github.com/dkaluta/icalBuddy64.git", branch: "master"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ical-buddy"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "efc6e18c606d05fbfba0cd4604fdf9dd43aac9e616a8f08d2b6fe908ce4c2e67"
+    sha256 cellar: :any_skip_relocation, mojave: "c554ff8b95ec7499ec9e78f200bc66e519ac23f81fddd59da2eb5f5c2030a253"
   end
 
   depends_on :macos
 
   def install
-    args = %W[icalBuddy icalBuddy.1 icalBuddyLocalization.1
-              icalBuddyConfig.1 COMPILER=#{ENV.cc}]
+    # Allow native builds rather than only x86_64
+    inreplace "Makefile", "-arch x86_64", ""
+
+    args = %W[
+      icalBuddy
+      icalBuddy.1
+      icalBuddyLocalization.1
+      icalBuddyConfig.1
+      COMPILER=#{ENV.cc}
+      APP_VERSION=#{version}
+    ]
     system "make", *args
     bin.install "icalBuddy"
     man1.install Dir["*.1"]
