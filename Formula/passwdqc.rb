6e287b8734c54ca40c4df6b31a6d3b49eeb7353b
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-23 14:53:40 +0800

    passwdqc: no rebuilding required

--- Formula/passwdqc.rb
+++ Formula/passwdqc.rb
@@ -11,17 +11,22 @@ class Passwdqc < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "757aec7ca4e20667bc598f0749679626bcb59a1659733fb8e9caf06c765fa7a5"
-    sha256 cellar: :any, arm64_big_sur:  "ed3dc06aa375b5fe7a83f55c5cbdb37deb2e5dea63662fbd00fb5869486821ef"
-    sha256 cellar: :any, monterey:       "37d84a6bca02a4901043ff7afb98b6d6ce033c4f4464ab52714a5da0eea81594"
-    sha256 cellar: :any, big_sur:        "2aabe92682e07ffa405d348e7d2f263c79f92e1dbb972fec4729a5e7254cc0bc"
-    sha256 cellar: :any, catalina:       "2b9fe24f7b39be287229ef32275cc7f6609e3c7fcbb8f53f2180ca1506e880b1"
-    sha256 cellar: :any, mojave:         "e5f0c3879c811e178aed95c5541ea0f3b8967865d75a425bc3ac1b102a9b1014"
+    sha256 cellar: :any,                 arm64_monterey: "757aec7ca4e20667bc598f0749679626bcb59a1659733fb8e9caf06c765fa7a5"
+    sha256 cellar: :any,                 arm64_big_sur:  "ed3dc06aa375b5fe7a83f55c5cbdb37deb2e5dea63662fbd00fb5869486821ef"
+    sha256 cellar: :any,                 monterey:       "37d84a6bca02a4901043ff7afb98b6d6ce033c4f4464ab52714a5da0eea81594"
+    sha256 cellar: :any,                 big_sur:        "2aabe92682e07ffa405d348e7d2f263c79f92e1dbb972fec4729a5e7254cc0bc"
+    sha256 cellar: :any,                 catalina:       "2b9fe24f7b39be287229ef32275cc7f6609e3c7fcbb8f53f2180ca1506e880b1"
+    sha256 cellar: :any,                 mojave:         "e5f0c3879c811e178aed95c5541ea0f3b8967865d75a425bc3ac1b102a9b1014"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2068b218232974b2b44cf9a01ac38950f781df2df5a82862b4cc1ccc2de01bbb"
+  end
+
+  on_linux do
+    depends_on "linux-pam"
   end
 
   def install
     # https://github.com/openwall/passwdqc/issues/15
-    inreplace "passwdqc_filter.h", "<endian.h>", "<machine/endian.h>"
+    inreplace "passwdqc_filter.h", "<endian.h>", "<machine/endian.h>" if OS.mac?
 
     args = %W[
       BINDIR=#{bin}
@@ -31,9 +36,14 @@ class Passwdqc < Formula
       INCLUDEDIR=#{include}
       MANDIR=#{man}
       PREFIX=#{prefix}
-      SECUREDIR_DARWIN=#{prefix}/pam
       SHARED_LIBDIR=#{lib}
     ]
+
+    args << if OS.mac?
+      "SECUREDIR_DARWIN=#{prefix}/pam"
+    else
+      "SECUREDIR=#{prefix}/pam"
+    end
 
     system "make", *args
     system "make", "install", *args
