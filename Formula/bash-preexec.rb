b368a3c14382b23f135f46f7addabba7d42ca33a
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-23 14:53:12 +0800

    bash-preexec: no rebuilding required

--- Formula/bash-preexec.rb
+++ Formula/bash-preexec.rb
@@ -1,13 +1,13 @@
 class BashPreexec < Formula
   desc "Preexec and precmd functions for Bash (like Zsh)"
   homepage "https://github.com/rcaloras/bash-preexec"
-  url "https://github.com/rcaloras/bash-preexec/archive/0.4.1.tar.gz"
-  sha256 "5e6515d247e6156c99a31de6db58e9cbef53071806292a1ca10b7af74633a8c9"
+  url "https://github.com/rcaloras/bash-preexec/archive/0.5.0.tar.gz"
+  sha256 "23c589cd1da209c0598f92fac8d81bb11632ba1b2e68ccaf4ad2c4f3204b877c"
   license "MIT"
   head "https://github.com/rcaloras/bash-preexec.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "72f047a0bb9e083b3c2a2bf491f8b3db94caa40f01710c03083ee005e2aa4454"
+    sha256 cellar: :any_skip_relocation, all: "bbc6178de7bf7f51abae6cba24f2a178efac44b00aa077cde3c5a2a4184a01b3"
   end
 
   def install
