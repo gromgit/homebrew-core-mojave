273032e528c6b0274bf3e0649db9bba72709e45a
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-23 14:53:52 +0800

    tractorgen: no rebuilding required

--- Formula/tractorgen.rb
+++ Formula/tractorgen.rb
@@ -1,7 +1,7 @@
 class Tractorgen < Formula
   desc "Generates ASCII tractor art"
-  homepage "http://www.vergenet.net/~conrad/software/tractorgen/"
-  url "http://www.vergenet.net/~conrad/software/tractorgen/dl/tractorgen-0.31.7.tar.gz"
+  homepage "https://vergenet.net/~conrad/software/tractorgen/"
+  url "https://vergenet.net/~conrad/software/tractorgen/dl/tractorgen-0.31.7.tar.gz"
   sha256 "469917e1462c8c3585a328d035ac9f00515725301a682ada1edb3d72a5995a8f"
   license "GPL-2.0-or-later"
 
