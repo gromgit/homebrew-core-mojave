904a0e7a09f29e1947cd086316f9b3693db2be2f
Adrian Ho <the.gromgit@gmail.com>
2022-04-23 23:04:16 +0800

    goreleaser: update 1.8.3 bottle.

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -2,14 +2,14 @@ class Goreleaser < Formula
   desc "Deliver Go binaries as fast and easily as possible"
   homepage "https://goreleaser.com/"
   url "https://github.com/goreleaser/goreleaser.git",
-      tag:      "v1.7.0",
-      revision: "7671dab291483b2733e871abff379d07e74dfc6c"
+      tag:      "v1.8.3",
+      revision: "63436392db6ac0557513535fc3ee4223a44810ed"
   license "MIT"
-  head "https://github.com/goreleaser/goreleaser.git", branch: "master"
+  head "https://github.com/goreleaser/goreleaser.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/goreleaser"
-    sha256 cellar: :any_skip_relocation, mojave: "92ff51ea07efc911b09bafe6c4794ccf94cc694dca1ca52706774eecd1dfd6c5"
+    sha256 cellar: :any_skip_relocation, mojave: "17c89857b83b5143a1b5ccb12f629fbf3240bc894b28207a7d7d0db7130281c4"
   end
 
   depends_on "go" => :build
