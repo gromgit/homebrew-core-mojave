6dc5b59445aacae3fc3d27ab000799beffd0a870
Adrian Ho <the.gromgit@gmail.com>
2022-04-24 07:21:16 +0800

    onscripter: update 20220123 bottle.

--- Formula/onscripter.rb
+++ Formula/onscripter.rb
@@ -1,10 +1,9 @@
 class Onscripter < Formula
   desc "NScripter-compatible visual novel engine"
   homepage "https://onscripter.osdn.jp/onscripter.html"
-  url "https://onscripter.osdn.jp/onscripter-20200722.tar.gz"
-  sha256 "12e5f4ac336ae3da46bf166ff1d439840be6336b19401a76c7d788994a9cd35e"
-  license "GPL-2.0"
-  revision 1
+  url "https://onscripter.osdn.jp/onscripter-20220123.tar.gz"
+  sha256 "5da41dc3471eeec8c93153906b39dac0a32edbb2bcefce0fa0a976c148b448ca"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url :homepage
@@ -12,12 +11,8 @@ class Onscripter < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "f161b80ef31f27da43da4d8ab6b0b36ed59fe201de38b483fd350756a8a66718"
-    sha256 cellar: :any, arm64_big_sur:  "6bf87dccb1539e34ca53a5d2912937d812d91d5540885a53d7e10e9c334a0f1a"
-    sha256 cellar: :any, monterey:       "18cf25110f0769f5aa7bbe06d5028f15a3b68d541ff203a6fb62dfe17deb0ccc"
-    sha256 cellar: :any, big_sur:        "444a199a064d388e2fdcdc2e0bde6b33b55c61423484269af44e203db2badd49"
-    sha256 cellar: :any, catalina:       "6d28e84b6fce1f2b9bbba114b325835cd03250706d510719a5291d370d6d148f"
-    sha256 cellar: :any, mojave:         "72be2f1701d8a25edd139c7beeccf563b73e4f802b1c74708e6102f6f6fd8557"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/onscripter"
+    sha256 cellar: :any, mojave: "2fa3484e9d327ee4fa4160d4d9218aefe02587179ec61ff3430a940cf48e61c6"
   end
 
   depends_on "pkg-config" => :build
