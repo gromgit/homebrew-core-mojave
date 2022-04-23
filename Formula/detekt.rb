868927632c018af121b6aa5f903a0308fe06137a
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-23 14:53:17 +0800

    detekt: no rebuilding required

--- Formula/detekt.rb
+++ Formula/detekt.rb
@@ -1,8 +1,8 @@
 class Detekt < Formula
   desc "Static code analysis for Kotlin"
   homepage "https://github.com/detekt/detekt"
-  url "https://github.com/detekt/detekt/releases/download/v1.19.0/detekt-cli-1.19.0-all.jar"
-  sha256 "4bc545c95c3711daeedd7766a9884ac50f2a49a0b472cef2b14dae332c58294d"
+  url "https://github.com/detekt/detekt/releases/download/v1.20.0/detekt-cli-1.20.0-all.jar"
+  sha256 "2e4f299cc50b010fa99f44893e0fca68ef1efb57f9e6c3758c7b021804d36c59"
   license "Apache-2.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class Detekt < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "bbd9f04be63b4f3493ea4d95676ed905a22f6196abc6ecaa4ddc8aa0e6112eb9"
+    sha256 cellar: :any_skip_relocation, all: "0d0afd41343f5e70d9fb2ecdc70c82d9380e963114167b8de6b207fa4cefac27"
   end
 
   depends_on "openjdk@11"
