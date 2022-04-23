602b636b6f9f4db73edc82932ce5cab1177e03f6
Adrian Ho <the.gromgit@gmail.com>
2022-04-23 17:21:10 +0800

    benthos: update 4.0.0 bottle.

--- Formula/benthos.rb
+++ Formula/benthos.rb
@@ -1,13 +1,13 @@
 class Benthos < Formula
   desc "Stream processor for mundane tasks written in Go"
   homepage "https://www.benthos.dev"
-  url "https://github.com/Jeffail/benthos/archive/v3.65.0.tar.gz"
-  sha256 "da218fde33286b6f0ab2c2303f3ce773c7fb4cf9fee2f44929743405e6621f19"
+  url "https://github.com/Jeffail/benthos/archive/v4.0.0.tar.gz"
+  sha256 "51e5bfb710a2e482df71c051899510191f4076776d5cd4dba08834837d957ca4"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/benthos"
-    sha256 cellar: :any_skip_relocation, mojave: "865ba942518df8c34152c76ed01973f885600e28d6705ef8c587bbfef496766b"
+    sha256 cellar: :any_skip_relocation, mojave: "837dd9cf76b123e9966a433b3df2af2981af6f5c2593144a1fe748c8dc9820d0"
   end
 
   depends_on "go" => :build
@@ -27,17 +27,14 @@ class Benthos < Formula
       logger:
         level: ERROR
       input:
-        type: file
         file:
-          path: ./sample.txt
+          paths: [ ./sample.txt ]
       pipeline:
         threads: 1
         processors:
-         - type: decode
-           decode:
-             scheme: base64
+         - bloblang: 'root = content().decode("base64")'
       output:
-        type: stdout
+        stdout: {}
     EOS
     output = shell_output("#{bin}/benthos -c test_pipeline.yaml")
     assert_match "Benthos rocks!", output.strip
