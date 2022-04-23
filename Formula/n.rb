4a6d20f1ada50ffa62ae84124ff714dc0e81265b
Adrian Ho <the.gromgit@gmail.com>
2022-04-24 06:53:35 +0800

    n: update 8.2.0 bottle.

--- Formula/n.rb
+++ Formula/n.rb
@@ -1,14 +1,14 @@
 class N < Formula
   desc "Node version management"
   homepage "https://github.com/tj/n"
-  url "https://github.com/tj/n/archive/v8.1.0.tar.gz"
-  sha256 "adf97836bfd66e79776b1330e84b9f089f73e9e89d3a6fd6e385a95d3eab2af5"
+  url "https://github.com/tj/n/archive/v8.2.0.tar.gz"
+  sha256 "75efd9e583836f3e6cc6d793df1501462fdceeb3460d5a2dbba99993997383b9"
   license "MIT"
   head "https://github.com/tj/n.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/n"
-    sha256 cellar: :any_skip_relocation, mojave: "9bfb4d89c9452cc533e1a0031eb9177397fec72e70b1c01965558e7e33fe9c45"
+    sha256 cellar: :any_skip_relocation, mojave: "1002532d9a95823156f2ea55ab16220dcb3ec57162b4fd807cead661f8234535"
   end
 
   def install
