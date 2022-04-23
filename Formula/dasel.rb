fb3c33819a5e687804ca3b7d613991d5db2ec195
Adrian Ho <the.gromgit@gmail.com>
2022-04-23 19:12:23 +0800

    dasel: update 1.24.2 bottle.

--- Formula/dasel.rb
+++ Formula/dasel.rb
@@ -1,14 +1,14 @@
 class Dasel < Formula
   desc "JSON, YAML, TOML, XML, and CSV query and modification tool"
   homepage "https://github.com/TomWright/dasel"
-  url "https://github.com/TomWright/dasel/archive/v1.24.1.tar.gz"
-  sha256 "ca1fa030f41abcd3659e76d7413ce14c551f40d41769e7a000a46f4fea6d0b64"
+  url "https://github.com/TomWright/dasel/archive/v1.24.2.tar.gz"
+  sha256 "66290c705aa6a38c95596d77ac3a70ad2014cc2918c0134e065704a0cf5f7663"
   license "MIT"
   head "https://github.com/TomWright/dasel.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dasel"
-    sha256 cellar: :any_skip_relocation, mojave: "26b711ff75e4c2580790fd87bf992471aa0cc06e85e89eb54dfc4b8379aba179"
+    sha256 cellar: :any_skip_relocation, mojave: "f5b8b9de4925b6b87c3ec6552318f722c9fe14648626c3054620ecc922a1df7c"
   end
 
   depends_on "go" => :build
