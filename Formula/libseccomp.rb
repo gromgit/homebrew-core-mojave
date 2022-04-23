10897e913f040b45493d6f3e121074ac5b3791ed
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-23 14:53:33 +0800

    libseccomp:

--- Formula/libseccomp.rb
+++ Formula/libseccomp.rb
@@ -1,8 +1,8 @@
 class Libseccomp < Formula
   desc "Interface to the Linux Kernel's syscall filtering mechanism"
   homepage "https://github.com/seccomp/libseccomp"
-  url "https://github.com/seccomp/libseccomp/releases/download/v2.5.3/libseccomp-2.5.3.tar.gz"
-  sha256 "59065c8733364725e9721ba48c3a99bbc52af921daf48df4b1e012fbc7b10a76"
+  url "https://github.com/seccomp/libseccomp/releases/download/v2.5.4/libseccomp-2.5.4.tar.gz"
+  sha256 "d82902400405cf0068574ef3dc1fe5f5926207543ba1ae6f8e7a1576351dcbdb"
   license "LGPL-2.1-only"
 
   livecheck do
@@ -10,17 +10,24 @@ class Libseccomp < Formula
     strategy :github_latest
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "49db89117b474de352eabc99840644ff081d9120d54d1eaf5cda150798852075"
+  end
+
+  head do
+    url "https://github.com/seccomp/libseccomp.git", branch: "main"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
   depends_on "gperf" => :build
-  depends_on "libtool" => :build
   depends_on :linux
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    system "./autogen.sh" if build.head?
+    system "./configure", *std_configure_args, "--disable-silent-rules"
     system "make", "install"
   end
 
