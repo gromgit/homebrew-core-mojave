62b2ad6dcfa834834cf4d35ea56520d73b423feb
Adrian Ho <the.gromgit@gmail.com>
2022-04-24 11:30:00 +0800

    scs: update 3.2.0 bottle.

--- Formula/scs.rb
+++ Formula/scs.rb
@@ -1,13 +1,13 @@
 class Scs < Formula
   desc "Conic optimization via operator splitting"
   homepage "https://web.stanford.edu/~boyd/papers/scs.html"
-  url "https://github.com/cvxgrp/scs/archive/3.1.0.tar.gz"
-  sha256 "90a7e58364ed3ea3375945a7f6f013de81c46100df80664a18f1c9b45a56be9c"
+  url "https://github.com/cvxgrp/scs/archive/3.2.0.tar.gz"
+  sha256 "df546b8b8764cacaa0e72bfeb9183586e1c64bc815174cbbecd4c9c1ef18e122"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/scs"
-    sha256 cellar: :any, mojave: "a61153451fa1e4122f35a3d8fce2e8926d429fd58cb0f8716e91a24d53d25dc5"
+    sha256 cellar: :any, mojave: "ded70126486320652f3b8d402f2e4b647b79b4f8ed513a9933191e937ba7d032"
   end
 
   on_linux do
@@ -33,7 +33,7 @@ class Scs < Formula
         _scs_read_data("#{pkgshare}/random_prob", &d, &k, &stgs);
         result = scs(d, k, stgs, sol, &info);
 
-        _scs_free_data(d, k, stgs); _scs_free_sol(sol);
+        _scs_free_data(d); _scs_free_data(k); _scs_free_sol(sol);
         return result - SCS_SOLVED;
       }
     EOS
