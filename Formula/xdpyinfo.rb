117d8ed437d7bd29ca34464bcd565a94e764f36b
Adrian Ho <the.gromgit@gmail.com>
2022-04-24 15:19:37 +0800

    xdpyinfo: update 1.3.3 bottle.

--- Formula/xdpyinfo.rb
+++ Formula/xdpyinfo.rb
@@ -1,19 +1,13 @@
 class Xdpyinfo < Formula
   desc "X.Org: Utility for displaying information about an X server"
   homepage "https://www.x.org/"
-  url "https://www.x.org/archive/individual/app/xdpyinfo-1.3.2.tar.bz2"
-  sha256 "30238ed915619e06ceb41721e5f747d67320555cc38d459e954839c189ccaf51"
+  url "https://www.x.org/archive/individual/app/xdpyinfo-1.3.3.tar.xz"
+  sha256 "356d5fd62f3e98ee36d6becf1b32d4ab6112d618339fb4b592ccffbd9e0fc206"
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "ff2ad33679d6a717493607a23d89b8a61c3a0311f58435ae74dcf50efd44ea9d"
-    sha256 cellar: :any,                 arm64_big_sur:  "233966c398633a07136501eab8fc263aaa97232c0fe27e76e8eb1fa59ed5440e"
-    sha256 cellar: :any,                 monterey:       "86fcd00824eabdc1c808663db7fc37ebc7b67e231ab038cc4f2383717a9a0835"
-    sha256 cellar: :any,                 big_sur:        "128f9ce7c4fad4a7bdc313c02c7c2324fa311845cc6ccaf9e19cc835bbef83e6"
-    sha256 cellar: :any,                 catalina:       "dfb295a0259be51cd1ec75a16bda4582608569ad098a37ad257818616f70b81e"
-    sha256 cellar: :any,                 mojave:         "2f9c704633a4a7d7df952f4874a16bae0cc8890e6ba1fdf7cd1654bb4cad01c7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9ad0f419da3316e1c46cb7f89af895d4ab2b7612a7806dba861b733b34b4b72a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/xdpyinfo"
+    sha256 cellar: :any, mojave: "e0258d14613868123c01e1dee2a377dbd82281742430f5b8e37e2e89e1cb1cb8"
   end
 
   depends_on "pkg-config" => :build
