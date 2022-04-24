78cd4f086aa3643c29a2e2c2e7c946930488864b
Adrian Ho <the.gromgit@gmail.com>
2022-04-24 12:03:14 +0800

    staticcheck: update 2022.1_1 bottle.

--- Formula/staticcheck.rb
+++ Formula/staticcheck.rb
@@ -4,20 +4,18 @@ class Staticcheck < Formula
   url "https://github.com/dominikh/go-tools/archive/2022.1.tar.gz"
   sha256 "aecfced0299fc70d17fc7d8d8dc87590429081250f03cb4c6bdd378fd50353ab"
   license "MIT"
+  revision 1
   head "https://github.com/dominikh/go-tools.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/staticcheck"
-    sha256 cellar: :any_skip_relocation, mojave: "ba842fdd7fa7f1c62c9616e0c1e8cd06c339e57afdd58ffc4b125ceef2840a91"
+    sha256 cellar: :any_skip_relocation, mojave: "8a47716de4d2e8d0d85d29723972e5cd334b2bb1d3b584a64740322e16d2cee2"
   end
 
-  # Bump to 1.18 on the next release.
-  depends_on "go@1.17"
+  depends_on "go"
 
   def install
-    output = libexec/"bin/staticcheck"
-    system "go", "build", *std_go_args(output: output), "./cmd/staticcheck"
-    (bin/"staticcheck").write_env_script(output, PATH: "$PATH:#{Formula["go@1.17"].opt_bin}")
+    system "go", "build", *std_go_args, "./cmd/staticcheck"
   end
 
   test do
