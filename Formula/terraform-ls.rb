1052ff78a317a7a2431810bef05bb9a5c13ac614
Adrian Ho <the.gromgit@gmail.com>
2022-04-24 13:05:48 +0800

    terraform-ls: update 0.27.0 bottle.

--- Formula/terraform-ls.rb
+++ Formula/terraform-ls.rb
@@ -1,8 +1,8 @@
 class TerraformLs < Formula
   desc "Terraform Language Server"
   homepage "https://github.com/hashicorp/terraform-ls"
-  url "https://github.com/hashicorp/terraform-ls/archive/v0.26.0.tar.gz"
-  sha256 "e4d67e3faee34a4555fcd8e68aa0a18a5df8669edc0e2a4d3c04acbf80d2d5c3"
+  url "https://github.com/hashicorp/terraform-ls/archive/v0.27.0.tar.gz"
+  sha256 "25ef850db974bc54d30f6d66d534313c61c16e5bddf8d137654f26fd2c4c274d"
   license "MPL-2.0"
   head "https://github.com/hashicorp/terraform-ls.git", branch: "main"
 
@@ -13,7 +13,7 @@ class TerraformLs < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/terraform-ls"
-    sha256 cellar: :any_skip_relocation, mojave: "9ffb8b6eabaa73be3ffd3cb5c9e1c7792381fb65ea23320fe7534012146bbadb"
+    sha256 cellar: :any_skip_relocation, mojave: "0aac6e59fa775e097080ea729df6db7221754ef9a8fde75d152ac5b419039bd0"
   end
 
   depends_on "go" => :build
