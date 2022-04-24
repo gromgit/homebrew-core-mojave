4a13652d8fd2f6939a9215acc0c9498bf8d6be7d
Adrian Ho <the.gromgit@gmail.com>
2022-04-24 09:08:14 +0800

    promtail: update 2.5.0 bottle.

--- Formula/promtail.rb
+++ Formula/promtail.rb
@@ -1,8 +1,8 @@
 class Promtail < Formula
   desc "Log agent for Loki"
   homepage "https://grafana.com/loki"
-  url "https://github.com/grafana/loki/archive/v2.4.2.tar.gz"
-  sha256 "725af867fa3bece6ccd46e0722eb68fe72462b15faa15c8ada609b5b2a476b07"
+  url "https://github.com/grafana/loki/archive/refs/tags/v2.5.0.tar.gz"
+  sha256 "f9ca9e52f4d9125cc31f9a593aba6a46ed6464c9cd99b2be4e35192a0ab4a76e"
   license "AGPL-3.0-only"
   head "https://github.com/grafana/loki.git", branch: "main"
 
@@ -12,8 +12,7 @@ class Promtail < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/promtail"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "463007da6554f6e0a87de32e008e0692d6aa405b1f888e92fade18709effe30f"
+    sha256 cellar: :any_skip_relocation, mojave: "6e96ee5066d5c64d2a34b2d332bf53a3f796938cfa4c52e2ce3778b565c74fec"
   end
 
   # Bump to 1.18 on the next release, if possible.
