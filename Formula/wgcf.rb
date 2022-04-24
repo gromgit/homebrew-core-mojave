06fe91c4d944198b9be64fba288b59c0f266545d
Adrian Ho <the.gromgit@gmail.com>
2022-04-24 14:58:40 +0800

    wgcf: update 2.2.13 bottle.

--- Formula/wgcf.rb
+++ Formula/wgcf.rb
@@ -1,14 +1,14 @@
 class Wgcf < Formula
   desc "Generate WireGuard profile from Cloudflare Warp account"
   homepage "https://github.com/ViRb3/wgcf"
-  url "https://github.com/ViRb3/wgcf/archive/v2.2.12.tar.gz"
-  sha256 "6945b032b9376f10167c6602f0ae3767f301b9b200c1aa6d543a874d91afbbc0"
+  url "https://github.com/ViRb3/wgcf/archive/v2.2.13.tar.gz"
+  sha256 "bb26d29dfbb673dd46a5027b5f07e0329e108507c3394a69e45ccf57ce45c9fb"
   license "MIT"
   head "https://github.com/ViRb3/wgcf.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/wgcf"
-    sha256 cellar: :any_skip_relocation, mojave: "be0fb8049687dee0af20122fc3863dc2f38cc1924e46a6af34b4629b81ffd579"
+    sha256 cellar: :any_skip_relocation, mojave: "57a0567e4d969ef28f975feab8cb52cc7088a753de104f40bd03647295f982e5"
   end
 
   depends_on "go" => :build
