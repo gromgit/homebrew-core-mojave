77fe352a9eaa2f0bbd678d61a10f1c918a182432
Adrian Ho <the.gromgit@gmail.com>
2022-04-28 00:05:03 +0800

    gnu-getopt: update 2.38 bottle.

--- Formula/gnu-getopt.rb
+++ Formula/gnu-getopt.rb
@@ -1,22 +1,31 @@
 class GnuGetopt < Formula
   desc "Command-line option parsing utility"
   homepage "https://github.com/karelzak/util-linux"
-  url "https://www.kernel.org/pub/linux/utils/util-linux/v2.37/util-linux-2.37.4.tar.xz"
-  sha256 "634e6916ad913366c3536b6468e7844769549b99a7b2bf80314de78ab5655b83"
+  url "https://www.kernel.org/pub/linux/utils/util-linux/v2.38/util-linux-2.38.tar.xz"
+  sha256 "6d111cbe4d55b336db2f1fbeffbc65b89908704c01136371d32aa9bec373eb64"
   license "GPL-2.0-or-later"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gnu-getopt"
-    rebuild 1
-    sha256 cellar: :any, mojave: "241a4eaeb075e6c139c35733e645fda0bd0142efafcc1615deccf3ef2da8f102"
+    sha256 cellar: :any, mojave: "895b077299f203ecfd3f82eca94ddb9ecf6617004b00e195c453286a725bbdb0"
   end
 
   keg_only :provided_by_macos
 
   depends_on "asciidoctor" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
 
   on_linux do
     keg_only "conflicts with util-linux"
+  end
+
+  # address `sys/vfs.h` header file missing issue on macos
+  # remove in next release
+  patch do
+    url "https://github.com/util-linux/util-linux/commit/3671d4a878fb58aa953810ecf9af41809317294f.patch?full_index=1"
+    sha256 "d38c9ae06c387da151492dd5862c58551559dd6d2b1877c74cc1e11754221fe4"
   end
 
   def install
