b6a27ed9d12046899a419ac4b2537c9507046e27
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-27 22:23:19 +0800

    remctl: no rebuilding required

--- Formula/remctl.rb
+++ Formula/remctl.rb
@@ -11,12 +11,13 @@ class Remctl < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "8b20a4f70e57a2da606bf67d22239dade23a10b4b277d3b71477bbf935a44a23"
-    sha256 cellar: :any, arm64_big_sur:  "3e7967965694f25afffdb35ee5f99857811dc64be5bd321278300725248d41e8"
-    sha256 cellar: :any, monterey:       "71d03a6a04bd587cd810655a533152bf9f30413fdccb271b403d5d66163eb003"
-    sha256 cellar: :any, big_sur:        "c021ec96746bcec68298d67fb9ac2550b059a0b6626e331b79c26e8a18feedcc"
-    sha256 cellar: :any, catalina:       "58267d5b4fc81b44c59521fce5a6c64ece78a67436d702741acaa6e656122caa"
-    sha256 cellar: :any, mojave:         "3d05fc09916078097c4cf62021d1f92bc9df6aa89e4f8c5dbd6028877a640d84"
+    sha256 cellar: :any,                 arm64_monterey: "8b20a4f70e57a2da606bf67d22239dade23a10b4b277d3b71477bbf935a44a23"
+    sha256 cellar: :any,                 arm64_big_sur:  "3e7967965694f25afffdb35ee5f99857811dc64be5bd321278300725248d41e8"
+    sha256 cellar: :any,                 monterey:       "71d03a6a04bd587cd810655a533152bf9f30413fdccb271b403d5d66163eb003"
+    sha256 cellar: :any,                 big_sur:        "c021ec96746bcec68298d67fb9ac2550b059a0b6626e331b79c26e8a18feedcc"
+    sha256 cellar: :any,                 catalina:       "58267d5b4fc81b44c59521fce5a6c64ece78a67436d702741acaa6e656122caa"
+    sha256 cellar: :any,                 mojave:         "3d05fc09916078097c4cf62021d1f92bc9df6aa89e4f8c5dbd6028877a640d84"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3017f0e726e6c6da6fbe90e6b007620465c57cb30201c7977cce85115e18f85e"
   end
 
   depends_on "libevent"
