392c5a9cf557b3f1787487d433107045f82928c7
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-27 22:23:10 +0800

    nesc: no rebuilding required

--- Formula/nesc.rb
+++ Formula/nesc.rb
@@ -14,6 +14,7 @@ class Nesc < Formula
     sha256 cellar: :any_skip_relocation, catalina:       "b2ce356c9fb1177a17e2e2b82cc7e91f9126ecc68435ba0cea0ea94f65def27c"
     sha256 cellar: :any_skip_relocation, mojave:         "9b261a0f665954574e417d0f7509d2253d09ab45f43e6db48ddaa4e81120e8ba"
     sha256 cellar: :any_skip_relocation, high_sierra:    "bb30d87ef9a3896e8dc9fa346854ecad17d2ac42ebdb3d5d800a548b839afc37"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "09def7e758ed0e91c1557f6ee6994815db45bb14c9f3f3bcba5848f6e1fa2bf3"
   end
 
   depends_on "autoconf" => :build
