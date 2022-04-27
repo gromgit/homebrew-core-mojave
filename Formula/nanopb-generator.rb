ded50c649a606c9a32856c7b163e81dd415e3752
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-27 22:23:10 +0800

    nanopb-generator: no rebuilding required

--- Formula/nanopb-generator.rb
+++ Formula/nanopb-generator.rb
@@ -1,9 +1,12 @@
 class NanopbGenerator < Formula
+  include Language::Python::Shebang
+
   desc "C library for encoding and decoding Protocol Buffer messages"
   homepage "https://jpa.kapsi.fi/nanopb/docs/index.html"
   url "https://jpa.kapsi.fi/nanopb/download/nanopb-0.4.5.tar.gz"
   sha256 "7efc553d3d861bceb1221f79d29b03e4353f0df2db690cbced0f4a81882d95fd"
   license "Zlib"
+  revision 1
 
   livecheck do
     url "https://jpa.kapsi.fi/nanopb/download/"
@@ -11,16 +14,11 @@ class NanopbGenerator < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "6e52f0be723f759324edec9d1f9fcffafbe7c84f2d3547d592d606f2f330b061"
-    sha256 cellar: :any_skip_relocation, big_sur:       "3619dec5a99713f7b2afcccb631e04b4a6cfd4cf07b140fda95d32e7405038a1"
-    sha256 cellar: :any_skip_relocation, catalina:      "fc7ebaa9f8ba1b2360c5cdfe36d08db127abfce401bbecba1797b1bf2047a236"
-    sha256 cellar: :any_skip_relocation, mojave:        "b9c33b74a6363131e3e0e9ffc1e91a71a53433e470c78703726ccbd7e2050caa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dbbb4f7447e0de14a0c0f8ddbf82d81e29001ae2493482e79c87d4cebaa555f1"
-    sha256 cellar: :any_skip_relocation, all:           "dbbb4f7447e0de14a0c0f8ddbf82d81e29001ae2493482e79c87d4cebaa555f1"
+    sha256 cellar: :any_skip_relocation, all: "6901779f65d9c073574e8889a856d9e7543b7b5a551520b796837a9529353465"
   end
 
   depends_on "protobuf"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   conflicts_with "mesos",
     because: "they depend on an incompatible version of protobuf"
@@ -28,8 +26,7 @@ class NanopbGenerator < Formula
   def install
     cd "generator" do
       system "make", "-C", "proto"
-      inreplace "nanopb_generator.py", %r{^#!/usr/bin/env python3$},
-                                       "#!/usr/bin/env #{Formula["python@3.9"].opt_bin}/python3"
+      rewrite_shebang detected_python_shebang, "nanopb_generator.py"
       libexec.install "nanopb_generator.py", "protoc-gen-nanopb", "proto"
       bin.install_symlink libexec/"protoc-gen-nanopb", libexec/"nanopb_generator.py"
     end
