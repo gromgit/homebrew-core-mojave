de8e3e49a32b1d7aa94ed886a2a3da538af80b01
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-27 22:22:51 +0800

    docbook-xsl: no rebuilding required

--- Formula/docbook-xsl.rb
+++ Formula/docbook-xsl.rb
@@ -36,9 +36,9 @@ class DocbookXsl < Formula
     sha256 "9bc38a3015717279a3a0620efb2d4bcace430077241ae2b0da609ba67d8340bc"
   end
 
-  # see http://www.linuxfromscratch.org/blfs/view/9.1/pst/docbook-xsl.html for this patch
+  # see https://www.linuxfromscratch.org/blfs/view/9.1/pst/docbook-xsl.html for this patch
   patch do
-    url "http://www.linuxfromscratch.org/patches/blfs/9.1/docbook-xsl-nons-1.79.2-stack_fix-1.patch"
+    url "https://www.linuxfromscratch.org/patches/blfs/9.1/docbook-xsl-nons-1.79.2-stack_fix-1.patch"
     mirror "https://raw.githubusercontent.com/Homebrew/formula-patches/5f2d6c1/docbook-xsl/docbook-xsl-nons-1.79.2-stack_fix-1.patch"
     sha256 "a92c39715c54949ba9369add1809527b8f155b7e2a2b2e30cb4b39ee715f2e30"
   end
