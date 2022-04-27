0635e338b3343d9a92e7181a8ac9b357f8e9632c
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-27 22:22:50 +0800

    darkice: no rebuilding required

--- Formula/darkice.rb
+++ Formula/darkice.rb
@@ -11,13 +11,14 @@ class Darkice < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "5c818db8d3d5bad23fb9125499e2840ed2d897f089407a18a5e309939986982e"
-    sha256 cellar: :any, arm64_big_sur:  "3a4f9f7d9203130529868ad9b0ff539bb795c531b4603090d6255441d5b15dbc"
-    sha256 cellar: :any, monterey:       "06d6c5a858374cec98f3b0ba736040da5424ca02d31b3417c60afb97b3bd8381"
-    sha256 cellar: :any, big_sur:        "500b7d4a2ccd852588c4ac9cd65f901817f19961ad21e8c2355b82318efd74d4"
-    sha256 cellar: :any, catalina:       "c312949cef4bec0b37951d4e9f3b9211a0a0c04d8666cb14bfde0a9f6c85ad5e"
-    sha256 cellar: :any, mojave:         "b41dd758dcda3daa8bcde6c5f161fb73d9268bef1bd68940e320fe0374b8272e"
-    sha256 cellar: :any, high_sierra:    "a8b0c02c6b00f614c9eac9d05fa17aee233021879edf7abc8cd81d1de34881e4"
+    sha256 cellar: :any,                 arm64_monterey: "5c818db8d3d5bad23fb9125499e2840ed2d897f089407a18a5e309939986982e"
+    sha256 cellar: :any,                 arm64_big_sur:  "3a4f9f7d9203130529868ad9b0ff539bb795c531b4603090d6255441d5b15dbc"
+    sha256 cellar: :any,                 monterey:       "06d6c5a858374cec98f3b0ba736040da5424ca02d31b3417c60afb97b3bd8381"
+    sha256 cellar: :any,                 big_sur:        "500b7d4a2ccd852588c4ac9cd65f901817f19961ad21e8c2355b82318efd74d4"
+    sha256 cellar: :any,                 catalina:       "c312949cef4bec0b37951d4e9f3b9211a0a0c04d8666cb14bfde0a9f6c85ad5e"
+    sha256 cellar: :any,                 mojave:         "b41dd758dcda3daa8bcde6c5f161fb73d9268bef1bd68940e320fe0374b8272e"
+    sha256 cellar: :any,                 high_sierra:    "a8b0c02c6b00f614c9eac9d05fa17aee233021879edf7abc8cd81d1de34881e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3c4618898033a3065666fae661f35c487ff2f6a52b6ac17a86d66d8bcf19eea4"
   end
 
   depends_on "pkg-config" => :build
