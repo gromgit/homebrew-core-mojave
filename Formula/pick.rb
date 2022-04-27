f071e97b72a63bc127ae9a9e9aa12406ca22e340
Adrian Ho <aho+vm-mojave@03s.net>
2022-04-27 22:23:15 +0800

    pick: no rebuilding required

--- Formula/pick.rb
+++ Formula/pick.rb
@@ -32,7 +32,11 @@ class Pick < Formula
       w.write "foo\nbar\nbaz\n\x04"
       sleep 1
       w.write "\n"
-      assert_match(/foo\r\nbar\r\nbaz\r\n\^D.*foo\r\n\z/, r.read)
+      begin
+        assert_match(/foo\r\nbar\r\nbaz\r\n\^D.*foo\r\n\z/, r.read)
+      rescue Errno::EIO
+        # GNU/Linux raises EIO when read is done on closed pty
+      end
     end
   end
 end
