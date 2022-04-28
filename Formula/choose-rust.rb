57befe3ecc637f516c90b4fa7be35a0a8222187e
Adrian Ho <the.gromgit@gmail.com>
2022-04-29 07:37:00 +0800

    choose-rust: update 1.3.4 bottle.

--- Formula/choose-rust.rb
+++ Formula/choose-rust.rb
@@ -1,18 +1,13 @@
 class ChooseRust < Formula
   desc "Human-friendly and fast alternative to cut and (sometimes) awk"
   homepage "https://github.com/theryangeary/choose"
-  url "https://github.com/theryangeary/choose/archive/v1.3.3.tar.gz"
-  sha256 "336225a05d98e536f79969c2d1727c8371df3d289e4aebe15a1a476db0f84df2"
+  url "https://github.com/theryangeary/choose/archive/v1.3.4.tar.gz"
+  sha256 "6c711901bb094a1241a2cd11951d5b7c96f337971f8d2eeff33f38dfa6ffb6ed"
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a0c6edb8ed91e01ade8f61b10d230baeaddd16b3978d1afe8b11bac9dd879917"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "440282c50599a3d526f71bdbbac3fb4a785823feca10a87fd36e4ff27adb94c8"
-    sha256 cellar: :any_skip_relocation, monterey:       "ae0ddfe3251905e1ce6c72b388b6c8af65ae720ff80527f8aed98713f085b88e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "56d66443ce6086e2d57c859a6fac3bc2d445b86c4430d607a33c6b2071d7e141"
-    sha256 cellar: :any_skip_relocation, catalina:       "406b87daee82a7cd5c1d56ec7188dbf2508ae0d7ae4471a6d1d8b2aced7f620c"
-    sha256 cellar: :any_skip_relocation, mojave:         "9a0420cd48ff40827bc3ed2db756b4a926eaa9422236e00aba05cb68c9232d33"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a785ef80c171a1a3ab4c5115669432770b21fb16717405d640bdaadc0adc19af"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/choose-rust"
+    sha256 cellar: :any_skip_relocation, mojave: "c8853dba61b76574c3c905af3a5c3229bbbe49d395b9e732db7c444030848f03"
   end
 
   depends_on "rust" => :build
