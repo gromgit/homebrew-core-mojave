f25fcb9430364bbe201f8b1b29f4b31dd8b9f559
Adrian Ho <the.gromgit@gmail.com>
2022-04-29 05:47:10 +0800

    curl: update 7.83.0 bottle.

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -1,11 +1,11 @@
 class Curl < Formula
   desc "Get a file from an HTTP, HTTPS or FTP server"
   homepage "https://curl.se"
-  url "https://curl.se/download/curl-7.82.0.tar.bz2"
-  mirror "https://github.com/curl/curl/releases/download/curl-7_82_0/curl-7.82.0.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/curl-7.82.0.tar.bz2"
-  mirror "http://fresh-center.net/linux/www/legacy/curl-7.82.0.tar.bz2"
-  sha256 "46d9a0400a33408fd992770b04a44a7434b3036f2e8089ac28b57573d59d371f"
+  url "https://curl.se/download/curl-7.83.0.tar.bz2"
+  mirror "https://github.com/curl/curl/releases/download/curl-7_82_0/curl-7.83.0.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/curl-7.83.0.tar.bz2"
+  mirror "http://fresh-center.net/linux/www/legacy/curl-7.83.0.tar.bz2"
+  sha256 "247c7ec7521c4258e65634e529270d214fe32969971cccb72845e7aa46831f96"
   license "curl"
 
   livecheck do
@@ -15,7 +15,7 @@ class Curl < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/curl"
-    sha256 cellar: :any, mojave: "2aa774787172c7dcc22e73d3cee4c89aa0131423f236084fa21d3520d3c31fde"
+    sha256 cellar: :any, mojave: "882583c14d0e3f693d828eecc5cc67c3f3b14cf858f7e0fb6eedbda23fbdedac"
   end
 
   head do
@@ -70,7 +70,7 @@ class Curl < Formula
     system "./configure", *args
     system "make", "install"
     system "make", "install", "-C", "scripts"
-    libexec.install "lib/mk-ca-bundle.pl"
+    libexec.install "scripts/mk-ca-bundle.pl"
   end
 
   test do
