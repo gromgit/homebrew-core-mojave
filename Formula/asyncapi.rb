d80dd06921e0949b530804381928556097bc802d
Adrian Ho <the.gromgit@gmail.com>
2022-04-29 02:16:06 +0800

    asyncapi: update 0.18.2 bottle.

--- Formula/asyncapi.rb
+++ Formula/asyncapi.rb
@@ -3,13 +3,13 @@ require "language/node"
 class Asyncapi < Formula
   desc "All in one CLI for all AsyncAPI tools"
   homepage "https://github.com/asyncapi/cli"
-  url "https://registry.npmjs.org/@asyncapi/cli/-/cli-0.18.0.tgz"
-  sha256 "be3ae36b14047a079521c7636773262a61b73c6b1eedbf20f3780027d187cf4b"
+  url "https://registry.npmjs.org/@asyncapi/cli/-/cli-0.18.2.tgz"
+  sha256 "37972da81bd62ab0d6f5944a00b0fc0e7208631506143719e7eaf45465488fd1"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/asyncapi"
-    sha256 cellar: :any_skip_relocation, mojave: "a91a0b061aa9f361253e8fc2f6edb4c8bc0861e938693ac7800ad2acd464ff62"
+    sha256 cellar: :any_skip_relocation, mojave: "a0cd49bb7f7dc692c0c5609f1132efce1346bf8ec78607d5a246de97a6478620"
   end
 
   depends_on "node"
