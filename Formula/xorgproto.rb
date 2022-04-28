bdfc6fa76140c38a43d09408b32b8959f005fb29
Adrian Ho <the.gromgit@gmail.com>
2022-04-29 01:02:58 +0800

    xorgproto: update 2022.1 bottle.

--- Formula/xorgproto.rb
+++ Formula/xorgproto.rb
@@ -1,8 +1,8 @@
 class Xorgproto < Formula
   desc "X.Org: Protocol Headers"
   homepage "https://www.x.org/"
-  url "https://xorg.freedesktop.org/archive/individual/proto/xorgproto-2021.5.tar.bz2"
-  sha256 "aa2f663b8dbd632960b24f7477aa07d901210057f6ab1a1db5158732569ca015"
+  url "https://xorg.freedesktop.org/archive/individual/proto/xorgproto-2022.1.tar.bz2"
+  sha256 "1d2dcc66963f234d2c1e1f8d98a0d3e8725149cdac0a263df4097593c48bc2a6"
   license "MIT"
 
   livecheck do
@@ -12,8 +12,7 @@ class Xorgproto < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/xorgproto"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "97763c1171ba9dc0772b5fa107c8dad9b19c3e73963d84887fb2d8c02a1ab59e"
+    sha256 cellar: :any_skip_relocation, mojave: "59bc0ef6734f03b980c00fed98dd2d607dda9abdfb9a0c0fb3b00ba8b5db26ac"
   end
 
   depends_on "pkg-config" => [:build, :test]
