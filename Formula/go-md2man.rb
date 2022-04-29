28deebe8b7da73f040f21cad527a48c610a3bec7
Adrian Ho <the.gromgit@gmail.com>
2022-04-29 13:34:35 +0800

    go-md2man: update 2.0.2 bottle.

--- Formula/go-md2man.rb
+++ Formula/go-md2man.rb
@@ -2,18 +2,13 @@ class GoMd2man < Formula
   desc "Converts markdown into roff (man pages)"
   homepage "https://github.com/cpuguy83/go-md2man"
   url "https://github.com/cpuguy83/go-md2man.git",
-      tag:      "v2.0.1",
-      revision: "b1ec32e02fe539480dc03e3bf381c20066e7c6cc"
+      tag:      "v2.0.2",
+      revision: "d97078115282836e16d0dca10b4b42ce60fc70e6"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a31c1e7e161708091568c9585c21f378bb754dec41a909e52758fe968698fe07"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "29c6f41dc0d4cfb59a80b55f84b2329ba00d75576cf1a95c050dd372044e30f5"
-    sha256 cellar: :any_skip_relocation, monterey:       "6e8560823efcfda202c24556d32044d6cb06d0ae06cde86dd92bb4b675b50d33"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ec0e280e3b7f68c5f0c7415aab6bf202f18080ff87750584a446be1a5636fa97"
-    sha256 cellar: :any_skip_relocation, catalina:       "ec0e280e3b7f68c5f0c7415aab6bf202f18080ff87750584a446be1a5636fa97"
-    sha256 cellar: :any_skip_relocation, mojave:         "ec0e280e3b7f68c5f0c7415aab6bf202f18080ff87750584a446be1a5636fa97"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8b39e17cacacb31eecfc3412dd251b536c717384c4634fe252691a1e0f305706"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/go-md2man"
+    sha256 cellar: :any_skip_relocation, mojave: "bf229a23b6bfe77c80872668ccaa52da73fba7b3dcce34085661dbecb5bb9f75"
   end
 
   depends_on "go" => :build
