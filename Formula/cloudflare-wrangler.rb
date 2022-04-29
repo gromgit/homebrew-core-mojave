04b55532a588d43d451d7901f5908397383138b8
Adrian Ho <the.gromgit@gmail.com>
2022-04-29 10:51:23 +0800

    cloudflare-wrangler: update 1.19.12 bottle.

--- Formula/cloudflare-wrangler.rb
+++ Formula/cloudflare-wrangler.rb
@@ -1,14 +1,14 @@
 class CloudflareWrangler < Formula
   desc "CLI tool for Cloudflare Workers"
   homepage "https://github.com/cloudflare/wrangler"
-  url "https://github.com/cloudflare/wrangler/archive/v1.19.11.tar.gz"
-  sha256 "55fd07190523d03e9a8bc5509e3aee21852979e2833f9d73c4c7f7d9f82d8724"
+  url "https://github.com/cloudflare/wrangler/archive/v1.19.12.tar.gz"
+  sha256 "42414fe79b089251a6a0f12d63dc635e5683825449e8960f70b66b810e5a60cd"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/cloudflare/wrangler.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cloudflare-wrangler"
-    sha256 cellar: :any_skip_relocation, mojave: "cbbdee8ac08904e1b3c18371738e050b5ccaf49f88e589e23b23f2e12aa166bb"
+    sha256 cellar: :any_skip_relocation, mojave: "b593b34b9050463f0a4590d87cfff0fac80c430017a3c466aae315d6a6ec019f"
   end
 
   depends_on "rust" => :build
