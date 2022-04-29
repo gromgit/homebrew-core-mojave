51e9c84c4b7b966e61a1aae491ae6f058753d7b8
Adrian Ho <the.gromgit@gmail.com>
2022-04-30 04:12:35 +0800

    weaver: update 1.0.8 bottle.

--- Formula/weaver.rb
+++ Formula/weaver.rb
@@ -1,17 +1,13 @@
 class Weaver < Formula
   desc "Command-line tool for Weaver"
   homepage "https://github.com/scribd/Weaver"
-  url "https://github.com/scribd/Weaver/archive/1.0.7.tar.gz"
-  sha256 "600a49ea7dbe58f57bf40afb7be7a42fb382ce41d807549e427b23e8504d5e02"
+  url "https://github.com/scribd/Weaver/archive/1.0.8.tar.gz"
+  sha256 "eefdd5f986be6da6256e132c83ea4a7ed711ae492a158e685385ea65670cc88d"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "63fb091fc3e723145f224db88ec2e2744ff1623347fef2b455e9a36375694d90"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bb850cf17b8647754ec204c75491cae42e2ca5dcd907155f5c37902bfc2918de"
-    sha256 cellar: :any_skip_relocation, monterey:       "1747df2e79d12eceef248f2faef237ed577e1bd07febed75ec729c6dd03a037b"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9546559d2fbfd8fbb74d6cb0f319418c2f6bec511a3ddb4e284b0195ac8a338b"
-    sha256 cellar: :any_skip_relocation, catalina:       "a0b1284a91a4647fa7b3c980fc237b0677e959c821d62ca31a5b3ea8a63abf40"
-    sha256 cellar: :any_skip_relocation, mojave:         "3db8730a06cb3ddd12a35097239afd85ea365c3e291a73422f37f23955230007"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/weaver"
+    sha256 cellar: :any_skip_relocation, mojave: "627fa140e33c63297d9864f160002eff2b4615f694df63f06487b125b35819f9"
   end
 
   depends_on xcode: ["11.2", :build]
