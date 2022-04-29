e7f034c03112c05c1987cab662cf0b0d81a83827
Adrian Ho <the.gromgit@gmail.com>
2022-04-29 16:52:29 +0800

    duckscript: update 0.8.11 bottle.

--- Formula/duckscript.rb
+++ Formula/duckscript.rb
@@ -1,17 +1,15 @@
 class Duckscript < Formula
   desc "Simple, extendable and embeddable scripting language"
   homepage "https://sagiegurari.github.io/duckscript"
-  url "https://github.com/sagiegurari/duckscript/archive/0.8.10.tar.gz"
-  sha256 "20b4cd633e0d7e72bdb3e9e4018f1342acfea6b22fd271c90bc915d2339e8e6b"
+  url "https://github.com/sagiegurari/duckscript/archive/0.8.11.tar.gz"
+  sha256 "5ee95309840140713b82ef1dadad91372734dc2fd2bd9d3a02d85f83c8d790eb"
   license "Apache-2.0"
   head "https://github.com/sagiegurari/duckscript.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/duckscript"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "af17e6f5f589c9668bc5b904b8dad97e7babbe6366d1e5252e2556fad1ae0ecb"
+    sha256 cellar: :any_skip_relocation, mojave: "46b1d03bc95586fd5f6c93d3ed975811246cacbf8bcd1b79e3ea9629cb959d0c"
   end
-
 
   depends_on "rust" => :build
 
