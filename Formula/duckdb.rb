533724405ba3c588d1a382bf0914fb9e6553aced
Adrian Ho <the.gromgit@gmail.com>
2022-04-29 16:46:22 +0800

    duckdb: update 0.3.4 bottle.

--- Formula/duckdb.rb
+++ Formula/duckdb.rb
@@ -2,13 +2,13 @@ class Duckdb < Formula
   desc "Embeddable SQL OLAP Database Management System"
   homepage "https://www.duckdb.org"
   url "https://github.com/duckdb/duckdb.git",
-      tag:      "v0.3.3",
-      revision: "fe9ba80039e4a0f20eb8d7fb7d6d9a4984156cbd"
+      tag:      "v0.3.4",
+      revision: "662041e2b43283bfcfeed984af7c3eaa85fb4996"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/duckdb"
-    sha256 cellar: :any, mojave: "aa5f878a281f0001b5d2eec96b0446547084847ddf8e14daf892d8494ee43f70"
+    sha256 cellar: :any, mojave: "3013fc6c58e3b1547b16c40d07e5f941680045625f3962e82beb9d25c986c635"
   end
 
   depends_on "cmake" => :build
