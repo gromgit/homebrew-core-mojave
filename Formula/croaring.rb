a21990c3e252c198f557a8b8ebbcd260a5f89899
Adrian Ho <the.gromgit@gmail.com>
2022-04-29 12:49:22 +0800

    croaring: update 0.5.0 bottle.

--- Formula/croaring.rb
+++ Formula/croaring.rb
@@ -1,28 +1,23 @@
 class Croaring < Formula
   desc "Roaring bitmaps in C (and C++)"
   homepage "https://roaringbitmap.org"
-  url "https://github.com/RoaringBitmap/CRoaring/archive/v0.4.0.tar.gz"
-  sha256 "0faf6ac893694d5c283a729373af74f813989ef0257781636ac4c397b48c1219"
+  url "https://github.com/RoaringBitmap/CRoaring/archive/v0.5.0.tar.gz"
+  sha256 "edab1b1a464e5a361ff622dc833170b2f33729c161aee4c2e53a324ac62ef78f"
   license "Apache-2.0"
   head "https://github.com/RoaringBitmap/CRoaring.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6148b7ee48a3a5723524e4f918d7da0205df5bfd4025bafe92ecb0a8e78f3e09"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7e7a0633d5789216b47ed5236431520ae37720cad3818cb96b0a050239e54656"
-    sha256 cellar: :any_skip_relocation, monterey:       "7276b52c9cc62874b1319bf0027205ff853c2884ed80956557877a3bbc2464e9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bc4cd03c1f0849065c0ff6e8a55a14fba01fafa1200338945f74f5273e8c05c2"
-    sha256 cellar: :any_skip_relocation, catalina:       "0f52b826c8c951829273c69a792007780363c9e0ff874f5e8693e0108786ea94"
-    sha256 cellar: :any_skip_relocation, mojave:         "9d81e20356089da75f20d83ff482999f48840a4899bb239de24142fb9e1e1f98"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b93bab19f46526216e3b1d0cf27bde0b9f10811b1de22c17b4bf56df12456ccc"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/croaring"
+    sha256 cellar: :any_skip_relocation, mojave: "87904f55972c2f9ab0af20668acd47c6ab228febb88a4aa12dc5b551246897e8"
   end
 
   depends_on "cmake" => :build
 
   def install
-    system "cmake", ".", *std_cmake_args
+    system "cmake", ".", *std_cmake_args, "-DENABLE_ROARING_TESTS=OFF"
     system "make", "install"
     system "make", "clean"
-    system "cmake", ".", "-DROARING_BUILD_STATIC=ON", *std_cmake_args
+    system "cmake", ".", *std_cmake_args, "-DROARING_BUILD_STATIC=ON"
     system "make"
     lib.install "src/libroaring.a"
   end
