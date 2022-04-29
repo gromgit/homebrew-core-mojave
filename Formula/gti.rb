dbc26eb1b2e3f15940054bcc8e1ca4381fa02c11
Adrian Ho <the.gromgit@gmail.com>
2022-04-29 21:39:35 +0800

    gti: update 1.8.0 bottle.

--- Formula/gti.rb
+++ Formula/gti.rb
@@ -3,11 +3,13 @@ class Gti < Formula
   homepage "https://r-wos.org/hacks/gti"
   url "https://github.com/rwos/gti/archive/v1.8.0.tar.gz"
   sha256 "65339ee1d52dede5e862b30582b2adf8aff2113cd6b5ece91775e1510b24ffb9"
+  license "MIT"
   head "https://github.com/rwos/gti.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gti"
-    sha256 cellar: :any_skip_relocation, mojave: "dfa11ccf76cd7abdb4d767714dcbeffa74df10e66fa60713d142867808679b76"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "286d5c3451034be587c79ca0418b521e88d2a01ee264a0a18e523dd8f6d1a25d"
   end
 
   def install
