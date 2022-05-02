42f9f0187061193861b73b04709c202cb764558c
Adrian Ho <the.gromgit@gmail.com>
2022-05-02 22:42:00 +0800

    criterion: update 2.4.1 bottle.

--- Formula/criterion.rb
+++ Formula/criterion.rb
@@ -1,14 +1,14 @@
 class Criterion < Formula
   desc "Cross-platform C and C++ unit testing framework for the 21st century"
   homepage "https://github.com/Snaipe/Criterion"
-  url "https://github.com/Snaipe/Criterion/releases/download/v2.4.0/criterion-2.4.0.tar.xz"
-  sha256 "b13bdb9e007d4d2e87a13446210630e95e3e3d92bb731951bcea4993464b9911"
+  url "https://github.com/Snaipe/Criterion/releases/download/v2.4.1/criterion-2.4.1.tar.xz"
+  sha256 "d0f86a8fc868e2c7b83894ad058313023176d406501a4ee8863e5357e31a80e7"
   license "MIT"
   head "https://github.com/Snaipe/Criterion.git", branch: "bleeding"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/criterion"
-    sha256 cellar: :any, mojave: "fba8638a3560d2e0f86da81e29358bda7a2a467a2e182968bfcee893e08875f8"
+    sha256 cellar: :any, mojave: "b1ef5b0669ebfcaffd97f42800f402df864734a8cc0173d74c204066c9e3e22f"
   end
 
   depends_on "cmake" => :build
