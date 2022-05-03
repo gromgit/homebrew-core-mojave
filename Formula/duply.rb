339284145073595eeeca0d2b24ca8379716ab763
Adrian Ho <the.gromgit@gmail.com>
2022-05-04 06:44:42 +0800

    duply: update 2.4 bottle.

--- Formula/duply.rb
+++ Formula/duply.rb
@@ -3,8 +3,8 @@ class Duply < Formula
   # Canonical domain: duply.net
   # Historical homepage: https://web.archive.org/web/20131126005707/ftplicity.sourceforge.net
   homepage "https://sourceforge.net/projects/ftplicity/"
-  url "https://downloads.sourceforge.net/project/ftplicity/duply%20%28simple%20duplicity%29/2.3.x/duply_2.3.1.tgz"
-  sha256 "f044f9d4b81891785212f856ace2064e8a08da19330587d580a5c233575a3091"
+  url "https://downloads.sourceforge.net/project/ftplicity/duply%20%28simple%20duplicity%29/2.4.x/duply_2.4.tgz"
+  sha256 "0c2ae9de8fee93391f9d568b6c82bce1b9989d7bea2acbe59069f718963788d8"
   license "GPL-2.0-only"
 
   livecheck do
@@ -13,13 +13,8 @@ class Duply < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "84068ad28382385d0d2a73ba47ec81d03da0b4e02aa715f574d01b538727f827"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "84068ad28382385d0d2a73ba47ec81d03da0b4e02aa715f574d01b538727f827"
-    sha256 cellar: :any_skip_relocation, monterey:       "9b8892466a36e8f58b5148c6c4770187266961364d8dda1971cce8f78f68da72"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9b8892466a36e8f58b5148c6c4770187266961364d8dda1971cce8f78f68da72"
-    sha256 cellar: :any_skip_relocation, catalina:       "9b8892466a36e8f58b5148c6c4770187266961364d8dda1971cce8f78f68da72"
-    sha256 cellar: :any_skip_relocation, mojave:         "9b8892466a36e8f58b5148c6c4770187266961364d8dda1971cce8f78f68da72"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "84068ad28382385d0d2a73ba47ec81d03da0b4e02aa715f574d01b538727f827"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/duply"
+    sha256 cellar: :any_skip_relocation, mojave: "1ea9eb0faab5669a11a13a37b6c3a0640f31671b4a1835bc34ec3622cdd1aea7"
   end
 
   depends_on "duplicity"
