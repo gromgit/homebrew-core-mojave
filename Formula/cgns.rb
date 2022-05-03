4bfc8beba0d95008f2dd05b712467425a2eee7d6
Adrian Ho <the.gromgit@gmail.com>
2022-05-04 03:56:45 +0800

    cgns: update 4.3.0_1 bottle.

--- Formula/cgns.rb
+++ Formula/cgns.rb
@@ -4,6 +4,7 @@ class Cgns < Formula
   url "https://github.com/CGNS/CGNS/archive/v4.3.0.tar.gz"
   sha256 "7709eb7d99731dea0dd1eff183f109eaef8d9556624e3fbc34dc5177afc0a032"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/CGNS/CGNS.git", branch: "develop"
 
   livecheck do
@@ -13,28 +14,26 @@ class Cgns < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cgns"
-    sha256 cellar: :any, mojave: "09a2221c2458904383b69b55a23bb6f97326b51842b44e3a2f97741e1113cdc2"
+    sha256 cellar: :any, mojave: "be24a555bd996b3442864264466d3df561721e73ad64400091fe3b25eb349d77"
   end
 
   depends_on "cmake" => :build
   depends_on "gcc" # for gfortran
   depends_on "hdf5"
-  depends_on "szip"
+  depends_on "libaec"
 
   uses_from_macos "zlib"
 
   def install
-    args = std_cmake_args + %w[
+    args = %w[
       -DCGNS_ENABLE_64BIT=YES
       -DCGNS_ENABLE_FORTRAN=YES
       -DCGNS_ENABLE_HDF5=YES
     ]
 
-    mkdir "build" do
-      system "cmake", "..", *args
-      system "make"
-      system "make", "install"
-    end
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
 
     # Avoid references to Homebrew shims
     inreplace include/"cgnsBuild.defs", Superenv.shims_path/ENV.cc, ENV.cc
@@ -52,7 +51,9 @@ class Cgns < Formula
         return 0;
       }
     EOS
-    system Formula["hdf5"].opt_prefix/"bin/h5cc", testpath/"test.c", "-L#{opt_lib}", "-lcgns"
+    flags = %W[-L#{lib} -lcgns]
+    flags << "-Wl,-rpath,#{lib},-rpath,#{Formula["libaec"].opt_lib}" if OS.linux?
+    system Formula["hdf5"].opt_prefix/"bin/h5cc", "test.c", *flags
     system "./a.out"
   end
 end
