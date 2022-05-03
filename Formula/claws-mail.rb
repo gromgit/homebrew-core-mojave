d125217eaa48753de2281845dec5215bfd0ca90a
Adrian Ho <the.gromgit@gmail.com>
2022-05-04 04:22:48 +0800

    claws-mail: update 3.19.0 bottle.

--- Formula/claws-mail.rb
+++ Formula/claws-mail.rb
@@ -1,8 +1,8 @@
 class ClawsMail < Formula
   desc "User-friendly, lightweight, and fast email client"
   homepage "https://www.claws-mail.org/"
-  url "https://www.claws-mail.org/releases/claws-mail-3.18.0.tar.gz"
-  sha256 "f107deec1f0debfae77f7f0022aef713f85a5b1f7d0b1a0e98f6eebe1e3556f2"
+  url "https://www.claws-mail.org/releases/claws-mail-3.19.0.tar.gz"
+  sha256 "3feef9ff72b15fb9f1ecc5102d7dfbb5b1c2c53172d331a3fb453645a6b53a6b"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -11,12 +11,8 @@ class ClawsMail < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "5f1a0bc9c1950c7bf5169f927e7f0cc6895e43e6188dafc38a2cf1cbc029f9e4"
-    sha256 arm64_big_sur:  "1e94b0baa4142502cd2f18e4bbdeafe3d73b7d0a5a6c13c16b5a74b6e49a908d"
-    sha256 monterey:       "fe2336c158d68f1b821f1f625c0a2837c5708f84f4594140ec6bec2262b9e29f"
-    sha256 big_sur:        "2c94ba339b902091781d9201c6cd71a2b9dfd401224317ac1f1368d17e44ee03"
-    sha256 catalina:       "e1691b2e1a632759a0c4aef4167265af4edde36d777eb5bfce0310c3235f2b1d"
-    sha256 mojave:         "b279ad0a5d167795deed2bdadafeea2f7fff4dca2b26c580a91c28cdaaa4b1c5"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/claws-mail"
+    sha256 mojave: "08003005c1f186841a4592540556ae80aac4a654afe61196889d0f7e3c84df16"
   end
 
   depends_on "pkg-config" => :build
