17221dc46a6419a3d6ac1bc10bd3b5187973b0fe
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-04 19:32:58 +0800

    openliberty-jakartaee8: no rebuilding required

--- Formula/openliberty-jakartaee8.rb
+++ Formula/openliberty-jakartaee8.rb
@@ -1,8 +1,8 @@
 class OpenlibertyJakartaee8 < Formula
   desc "Lightweight open framework for Java (Jakarta EE 8)"
   homepage "https://openliberty.io"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/2022-01-03_1900/openliberty-javaee8-22.0.0.1.zip"
-  sha256 "7279ea2ac23646954e8eed6ffc907ba145457c8456d884b0b6daaf2133e2dcc3"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/22.0.0.4/openliberty-javaee8-22.0.0.4.zip"
+  sha256 "e77539fd25a1e20add080e365e9ac977b24dfde187c074844a5dd15d7f09eb2a"
   license "EPL-1.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class OpenlibertyJakartaee8 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "407adebbe512ab1fc835714668fc16b122d80534aefd6ff3c0cf56e4c9f5dd05"
+    sha256 cellar: :any_skip_relocation, all: "46f1fb94c42e64507b4fa81f6d7f2b0ede91c5f5a1b581f39563d67eeac47261"
   end
 
   depends_on "openjdk"
