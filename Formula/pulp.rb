8cddd9b633e8276e329ec80a633c904fd81a6d48
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-04 19:33:04 +0800

    pulp: no rebuilding required

--- Formula/pulp.rb
+++ Formula/pulp.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Pulp < Formula
   desc "Build tool for PureScript projects"
   homepage "https://github.com/purescript-contrib/pulp"
-  url "https://registry.npmjs.org/pulp/-/pulp-16.0.0-0.tgz"
-  sha256 "77b8a5ff21291257766f54289dbe92d2558fc64e9a034513647a76400c0e7d94"
+  url "https://registry.npmjs.org/pulp/-/pulp-16.0.0-1.tgz"
+  sha256 "19a4af16751aa8c2a9ed9dbe9f192498836eb4cc1d86bed9b306246ccabd7d22"
   license "LGPL-3.0-or-later"
 
   livecheck do
@@ -13,7 +13,7 @@ class Pulp < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "c2b46cd6552c65509f86882217f616c80344e5e53a2f22866c958da790694762"
+    sha256 cellar: :any_skip_relocation, all: "df1346996d722c8a351fea7a59cabe00f3e1dd83548b19d2b789361c219405ec"
   end
 
   depends_on "bower"
