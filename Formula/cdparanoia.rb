812b75de488177295c631e55102bdc8ca3054656
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-04 19:32:29 +0800

    cdparanoia: no rebuilding required

--- Formula/cdparanoia.rb
+++ Formula/cdparanoia.rb
@@ -12,16 +12,17 @@ class Cdparanoia < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "d985bd81e9a8f5bb4f9f01a43138f1b89b385337ea2e48a22511634869496446"
-    sha256 cellar: :any, arm64_big_sur:  "79d03f652937117697ae235b7bbb8558be9cb86edc42c330316204a288d5cb59"
-    sha256 cellar: :any, monterey:       "ffe04653654cb899301fe3d0a65a207928ac26b712ef81952f9c46a06f64185d"
-    sha256 cellar: :any, big_sur:        "2b7649f89581be2a35b246e4aab15e936573d3920f794ae5187e23b796874dbf"
-    sha256 cellar: :any, catalina:       "9a2def6e4aa8db0e7f35392dd73e2bbaf86a52ddc5cb6ff80e1fcf6f34f6133e"
-    sha256 cellar: :any, mojave:         "68b478e2d9e8f7121040f99551a45cab8dd8cd91d94e8690ea17103d884daeaf"
-    sha256 cellar: :any, high_sierra:    "8b8b1eeb36773ce01ef09232e2e7270fc759aedd1814218cbd8eb9f668a4bf73"
-    sha256 cellar: :any, sierra:         "709190d769f7b8c61d19867ae2faf902a2f84dec6f0d5506bd71c56a99e4a67a"
-    sha256 cellar: :any, el_capitan:     "135250473fe692dc976ecbf7324676fa8cef3cdb48a091287bb183c31548fed9"
-    sha256 cellar: :any, yosemite:       "3cd7bbd1a4a0a7992287b255cf0d6409bdb5f4a3fed245b0fd2296e535e9f2de"
+    sha256 cellar: :any,                 arm64_monterey: "d985bd81e9a8f5bb4f9f01a43138f1b89b385337ea2e48a22511634869496446"
+    sha256 cellar: :any,                 arm64_big_sur:  "79d03f652937117697ae235b7bbb8558be9cb86edc42c330316204a288d5cb59"
+    sha256 cellar: :any,                 monterey:       "ffe04653654cb899301fe3d0a65a207928ac26b712ef81952f9c46a06f64185d"
+    sha256 cellar: :any,                 big_sur:        "2b7649f89581be2a35b246e4aab15e936573d3920f794ae5187e23b796874dbf"
+    sha256 cellar: :any,                 catalina:       "9a2def6e4aa8db0e7f35392dd73e2bbaf86a52ddc5cb6ff80e1fcf6f34f6133e"
+    sha256 cellar: :any,                 mojave:         "68b478e2d9e8f7121040f99551a45cab8dd8cd91d94e8690ea17103d884daeaf"
+    sha256 cellar: :any,                 high_sierra:    "8b8b1eeb36773ce01ef09232e2e7270fc759aedd1814218cbd8eb9f668a4bf73"
+    sha256 cellar: :any,                 sierra:         "709190d769f7b8c61d19867ae2faf902a2f84dec6f0d5506bd71c56a99e4a67a"
+    sha256 cellar: :any,                 el_capitan:     "135250473fe692dc976ecbf7324676fa8cef3cdb48a091287bb183c31548fed9"
+    sha256 cellar: :any,                 yosemite:       "3cd7bbd1a4a0a7992287b255cf0d6409bdb5f4a3fed245b0fd2296e535e9f2de"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "260243bbe3a8e726d4a9e8207c1905086fbe2375c29c10c91bd31ddd3a4f8f40"
   end
 
   depends_on "autoconf" => :build
