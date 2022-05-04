3fd569d9169fbdf07088d5c938192463562aecf7
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-04 19:33:17 +0800

    wiremock-standalone: no rebuilding required

--- Formula/wiremock-standalone.rb
+++ Formula/wiremock-standalone.rb
@@ -1,13 +1,13 @@
 class WiremockStandalone < Formula
   desc "Simulator for HTTP-based APIs"
   homepage "https://wiremock.org/docs/running-standalone/"
-  url "https://search.maven.org/remotecontent?filepath=com/github/tomakehurst/wiremock-jre8-standalone/2.33.1/wiremock-jre8-standalone-2.33.1.jar"
-  sha256 "66e873c785c2cff47c3b59411d9d87c56b8863f1da00fcca6d6243dff0b620fb"
+  url "https://search.maven.org/remotecontent?filepath=com/github/tomakehurst/wiremock-jre8-standalone/2.33.2/wiremock-jre8-standalone-2.33.2.jar"
+  sha256 "4a424d645f4d8b4ac2df525dcb4def63f38c904c0d15cecb0a7320da6fe0efed"
   license "Apache-2.0"
   head "https://github.com/tomakehurst/wiremock.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "22fba61e064ad4f8c1d5aaa63d65df5bda3ee59aec7c58dbb180cbbadb4a4cc2"
+    sha256 cellar: :any_skip_relocation, all: "671ccf27e990892c749b3a75804493f30ff93a93e9b9508ffc9e556bb1f484aa"
   end
 
   depends_on "openjdk"
