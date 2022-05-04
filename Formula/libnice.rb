098cf3953eaed161cd19193304298e9cf249769e
Adrian Ho <the.gromgit@gmail.com>
2022-05-05 04:12:09 +0800

    libnice: update 0.1.19 bottle.

--- Formula/libnice.rb
+++ Formula/libnice.rb
@@ -1,8 +1,8 @@
 class Libnice < Formula
   desc "GLib ICE implementation"
   homepage "https://wiki.freedesktop.org/nice/"
-  url "https://nice.freedesktop.org/releases/libnice-0.1.18.tar.gz"
-  sha256 "5eabd25ba2b54e817699832826269241abaa1cf78f9b240d1435f936569273f4"
+  url "https://nice.freedesktop.org/releases/libnice-0.1.19.tar.gz"
+  sha256 "6747af710998cf708a2e8ceef51cccd181373d94201dd4b8d40797a070ed47cc"
   license any_of: ["LGPL-2.1-only", "MPL-1.1"]
 
   livecheck do
@@ -11,14 +11,8 @@ class Libnice < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any, arm64_monterey: "5581f07b30fd73e609004ff104bbf029c185c19fd28d4e54f9e96dbf9d40fb0a"
-    sha256 cellar: :any, arm64_big_sur:  "0c414fc1c0583fc19cbb8e604914315a4118da705ad5cd78a6472410cd8c0b5b"
-    sha256 cellar: :any, monterey:       "a6f64b14516006f7b81dc1e74d5fe08b06e718e49b00133999b0ee054012f865"
-    sha256 cellar: :any, big_sur:        "af306d90fda80e3afe83851672ec34a679e55595431383b5ba246051fc827895"
-    sha256 cellar: :any, catalina:       "eafa60c41c7d017627859714e5a1028151376432e1c5802b95f65f81a191016d"
-    sha256 cellar: :any, mojave:         "657ffb5240531a8dc9e918c2aec1c74fca62af994524e002d674ce9fbe52e4c1"
-    sha256               x86_64_linux:   "5a9f6bdf5b31637f23cfa5bc3762a9931e46f35bef1da914f99d0c0555003b33"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libnice"
+    sha256 cellar: :any, mojave: "4e32b4aa8742685a70952c6da7a4fb21ee3760197a29dcbc77f9a3fb7f03fe12"
   end
 
   depends_on "meson" => :build
