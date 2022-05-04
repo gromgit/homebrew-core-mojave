c7a712bad1c4f357e9bfd3b4cfc548d256cbc9af
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-04 19:32:27 +0800

    bitcoin:

--- Formula/bitcoin.rb
+++ Formula/bitcoin.rb
@@ -1,10 +1,9 @@
 class Bitcoin < Formula
   desc "Decentralized, peer to peer payment network"
   homepage "https://bitcoincore.org/"
-  url "https://bitcoincore.org/bin/bitcoin-core-22.0/bitcoin-22.0.tar.gz"
-  sha256 "d0e9d089b57048b1555efa7cd5a63a7ed042482045f6f33402b1df425bf9613b"
+  url "https://bitcoincore.org/bin/bitcoin-core-23.0/bitcoin-23.0.tar.gz"
+  sha256 "26748bf49d6d6b4014d0fedccac46bf2bcca42e9d34b3acfd9e3467c415acc05"
   license "MIT"
-  revision 1
   head "https://github.com/bitcoin/bitcoin.git", branch: "master"
 
   livecheck do
@@ -12,10 +11,13 @@ class Bitcoin < Formula
     regex(/latest version.*?v?(\d+(?:\.\d+)+)/i)
   end
 
-bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bitcoin"
-    rebuild 1
-    sha256 cellar: :any, mojave: "6271a8a6be8ceff7f3e90fb3a3a4df3a21de9ec070307a49a9572ed0207ee1d5"
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "b0704a6dc3a96f5bbb71d93a468b1a4881e747e90ddfa14d6ff0d97d5a23c6eb"
+    sha256 cellar: :any,                 arm64_big_sur:  "6c8caf0509daf986cd3af45eeea78789ccd4b0a8eb5aa1dfddaa624425e5b959"
+    sha256 cellar: :any,                 monterey:       "2859a13d7a48dad1a6701476d66ddb0b6940dcfe4b809b1c54512d78de247e26"
+    sha256 cellar: :any,                 big_sur:        "e8a1f3389faae4245c5f6a6ec8f179cb46e19af31f4e4637c6d3f24d55804b45"
+    sha256 cellar: :any,                 catalina:       "3279bd808f42d6ea608d04860ac276afda7c6ac2cf5d708469d4dc66672f210a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cb60fb85781435dfb545cea0c7e08339a7b3e01d60df604ccf9cba830f148e36"
   end
 
   depends_on "autoconf" => :build
@@ -25,6 +27,7 @@ bottle do
   depends_on "berkeley-db@4"
   depends_on "boost@1.76"
   depends_on "libevent"
+  depends_on macos: :catalina
   depends_on "miniupnpc"
   depends_on "zeromq"
 
