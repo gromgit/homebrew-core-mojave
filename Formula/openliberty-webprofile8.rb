866693c23b72447e5ab18842814dbd4607ae01ab
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-04 19:32:59 +0800

    openliberty-webprofile8: no rebuilding required

--- Formula/openliberty-webprofile8.rb
+++ Formula/openliberty-webprofile8.rb
@@ -1,8 +1,8 @@
 class OpenlibertyWebprofile8 < Formula
   desc "Lightweight open framework for Java (Jakarta EE Web Profile 8)"
   homepage "https://openliberty.io"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/2022-01-03_1900/openliberty-webProfile8-22.0.0.1.zip"
-  sha256 "158d2928ad10cbf8feb0549a25b5bfcadc4945e3d199b971e39eb458de3f09da"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/22.0.0.4/openliberty-webProfile8-22.0.0.4.zip"
+  sha256 "c6a7c1d85f53ce7e03e60a3b78fcb2460e21351092e4b30d86218ad9f372202d"
   license "EPL-1.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class OpenlibertyWebprofile8 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "a0f28b761e22222683799b48f18317e5f288eb8ad0bd876aa24eb8ddec6fca7f"
+    sha256 cellar: :any_skip_relocation, all: "4d2f6e6d613d7ccc5d1031268d9ce10967e1e55ff25092b409d85db31cb1df02"
   end
 
   depends_on "openjdk"
