7aa042c856efaae67469bb881e50572ce6f705e8
Adrian Ho <the.gromgit@gmail.com>
2022-05-04 08:31:06 +0800

    exult: update 1.8 bottle.

--- Formula/exult.rb
+++ Formula/exult.rb
@@ -1,8 +1,8 @@
 class Exult < Formula
   desc "Recreation of Ultima 7"
   homepage "https://exult.sourceforge.io/"
-  url "https://github.com/exult/exult/archive/v1.6.tar.gz"
-  sha256 "6176d9feba28bdf08fbf60f9ebb28a530a589121f3664f86711ff8365c86c17a"
+  url "https://github.com/exult/exult/archive/v1.8.tar.gz"
+  sha256 "dae6b7b08925d3db1dda3aca612bdc08d934ca04de817a008f305320e667faf9"
   license "GPL-2.0-or-later"
   head "https://github.com/exult/exult.git", branch: "master"
 
@@ -13,10 +13,8 @@ class Exult < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/exult"
-    rebuild 2
-    sha256 mojave: "780e0f95c37ebe59e3dad5d47dfaac7275c6a4fc98025b7eca100917407e32d7"
+    sha256 mojave: "5c0d6a925c26bcda53f4ef8ced275cc3e3413e819fd605187b61b350d9aff524"
   end
-
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -26,19 +24,7 @@ class Exult < Formula
   depends_on "libvorbis"
   depends_on "sdl2"
 
-  # Xcode 12 compile fix for 1.6.x branch - https://github.com/exult/exult/pull/61
-  patch do
-    url "https://github.com/exult/exult/commit/b98a9eb195bf8b3f55df56499d2a7c2c5d8809d0.patch?full_index=1"
-    sha256 "c1c5b1e9e4994ecdfaba6285b86222123ee6b5590bcd8e7400871a4e65836fe0"
-  end
-
   def install
-    # Use ~/Library/... instead of /Library for the games
-    inreplace "files/utils.cc" do |s|
-      s.gsub!(/(gamehome_dir)\("\."\)/, '\1(home_dir)')
-      s.gsub!(/(gamehome_dir) =/, '\1 +=')
-    end
-
     system "./autogen.sh"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
