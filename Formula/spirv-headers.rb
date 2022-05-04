3084dcb6cd1f40c210eefbf7eeee21a7feee79c4
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-04 19:33:10 +0800

    spirv-headers: no rebuilding required

---
+++ Formula/spirv-headers.rb
@@ -0,0 +1,31 @@
+class SpirvHeaders < Formula
+  desc "Headers for SPIR-V"
+  homepage "https://github.com/KhronosGroup/SPIRV-Headers"
+  url "https://github.com/KhronosGroup/SPIRV-Headers/archive/refs/tags/sdk-1.3.211.0.tar.gz"
+  sha256 "30a78e61bd812c75e09fdc7a319af206b1044536326bc3e85fea818376a12568"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    regex(/^sdk[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "0569a8ed0a9b5af6701bad2aee726b43db9661389bdf19d58c66d31024b7fea7"
+  end
+
+  depends_on "cmake" => [:build, :test]
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    pkgshare.install "example"
+  end
+
+  test do
+    system "cmake", "-S", pkgshare/"example", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+  end
+end
