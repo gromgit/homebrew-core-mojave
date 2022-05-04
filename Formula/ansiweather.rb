e83d5997cc23c73c1da4b5c3c04f70247291c630
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-04 19:32:24 +0800

    ansiweather: no rebuilding required

--- Formula/ansiweather.rb
+++ Formula/ansiweather.rb
@@ -4,16 +4,20 @@ class Ansiweather < Formula
   url "https://github.com/fcambus/ansiweather/archive/1.18.0.tar.gz"
   sha256 "362393918b64083de466414ca3ada3e0236206b29bfb2624d4ad1284774e6a7a"
   license "BSD-2-Clause"
+  revision 1
   head "https://github.com/fcambus/ansiweather.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "242abc21c45bf8e66ff74e62dcaea4fd62b3ddeddc8d280b35e405341f37689f"
+    sha256 cellar: :any_skip_relocation, all: "80cab6cc26ed03f58ec06f593fda3dd072cbf0a8255b9f6afbcd02f516d9cd7f"
   end
 
   depends_on "jq"
 
+  uses_from_macos "bc"
+
   def install
     bin.install "ansiweather"
+    man1.install "ansiweather.1"
   end
 
   test do
