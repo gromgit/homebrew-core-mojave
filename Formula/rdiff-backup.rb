105bd751dbb6492655b23704baedf05e225fd5c4
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-04 19:33:06 +0800

    rdiff-backup: no rebuilding required

--- Formula/rdiff-backup.rb
+++ Formula/rdiff-backup.rb
@@ -12,12 +12,13 @@ class RdiffBackup < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "9841037fad91c2e5567e03916d7413fd1e74d6858dff760b2a3728c53e1baf80"
-    sha256 cellar: :any, arm64_big_sur:  "66247de6c20d7350372ecb4efb63b3f5bec4c7f2fe29c4ed80723ebdcd0018fa"
-    sha256 cellar: :any, monterey:       "c64863e034cc7deb4de5574243baac6b0c180ab556ccea2b8fde137cd1910d74"
-    sha256 cellar: :any, big_sur:        "3aaeb0620c7dd027efea476c6b3af79425a7baf2056abc29ed88e405bf2f107a"
-    sha256 cellar: :any, catalina:       "e53a41d9556104c8b72a6b876969b2634d48a1153552af42af86456b5c1add67"
-    sha256 cellar: :any, mojave:         "f3d24f92212373f45e8323a8d054cef1b1ee0b392c96034cbf461bb60b0effd6"
+    sha256 cellar: :any,                 arm64_monterey: "9841037fad91c2e5567e03916d7413fd1e74d6858dff760b2a3728c53e1baf80"
+    sha256 cellar: :any,                 arm64_big_sur:  "66247de6c20d7350372ecb4efb63b3f5bec4c7f2fe29c4ed80723ebdcd0018fa"
+    sha256 cellar: :any,                 monterey:       "c64863e034cc7deb4de5574243baac6b0c180ab556ccea2b8fde137cd1910d74"
+    sha256 cellar: :any,                 big_sur:        "3aaeb0620c7dd027efea476c6b3af79425a7baf2056abc29ed88e405bf2f107a"
+    sha256 cellar: :any,                 catalina:       "e53a41d9556104c8b72a6b876969b2634d48a1153552af42af86456b5c1add67"
+    sha256 cellar: :any,                 mojave:         "f3d24f92212373f45e8323a8d054cef1b1ee0b392c96034cbf461bb60b0effd6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dedf7b7d0f5341a6159e46485c358502f3e50682db4f33f6ac69877830d0c99e"
   end
 
   depends_on "librsync"
