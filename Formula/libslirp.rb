9a4112a3b5cae3e2230177471d7b017ca8bdf99c
Adrian Ho <the.gromgit@gmail.com>
2022-05-06 05:34:57 +0800

    libslirp: update 4.7.0 bottle.

--- Formula/libslirp.rb
+++ Formula/libslirp.rb
@@ -1,18 +1,13 @@
 class Libslirp < Formula
   desc "General purpose TCP-IP emulator"
   homepage "https://gitlab.freedesktop.org/slirp/libslirp"
-  url "https://gitlab.freedesktop.org/slirp/libslirp/-/archive/v4.6.1/libslirp-v4.6.1.tar.gz"
-  sha256 "69ad4df0123742a29cc783b35de34771ed74d085482470df6313b6abeb799b11"
+  url "https://gitlab.freedesktop.org/slirp/libslirp/-/archive/v4.7.0/libslirp-v4.7.0.tar.gz"
+  sha256 "9398f0ec5a581d4e1cd6856b88ae83927e458d643788c3391a39e61b75db3d3b"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "475c2d9ad2e1a2b44c52e9ea1cd5627d392adb216b8e9969d059a5e17d9dafc5"
-    sha256 cellar: :any, arm64_big_sur:  "93fca2e4a0c689c366680a9b56ca0c90cb424eb8fb00136a170cea67e7919d67"
-    sha256 cellar: :any, monterey:       "4738978c9d94ebd569757a5c4df3918bf7f8e2aaf75f9d04b63cafcb24b29064"
-    sha256 cellar: :any, big_sur:        "120eb1362ba0645a96ace10153fb41b5669f0a32669947cca96b2e1b3108edd3"
-    sha256 cellar: :any, catalina:       "fc267a6871f5459a38e174ffd519a3016c849b6c99c2bce2ca714c20b71ae1b6"
-    sha256 cellar: :any, mojave:         "3a19812499b688c6698c0e50b1b0a567727cf9a5af26ed71ff99a412a98f1c44"
-    sha256               x86_64_linux:   "e49c9b8b88c0f9cb434cac8b488d38450bc2b148f71ba934c7562437a5b46c58"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libslirp"
+    sha256 cellar: :any, mojave: "9129e5edbf8a10a09a190ec32eda40c53c3c38d25a202d3893f8a0fd02377201"
   end
 
   depends_on "meson" => :build
