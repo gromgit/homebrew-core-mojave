cb3102e0240562b159d3489227e6229dad3ce340
Adrian Ho <the.gromgit@gmail.com>
2022-05-05 18:44:26 +0800

    grafana: update 8.5.1 bottle.

--- Formula/grafana.rb
+++ Formula/grafana.rb
@@ -1,14 +1,14 @@
 class Grafana < Formula
   desc "Gorgeous metric visualizations and dashboards for timeseries databases"
   homepage "https://grafana.com"
-  url "https://github.com/grafana/grafana/archive/v8.5.0.tar.gz"
-  sha256 "f5e21977a0d8c473cd76f7f769f6a27434e5f3b3099c6fa3586108749819e56e"
+  url "https://github.com/grafana/grafana/archive/v8.5.1.tar.gz"
+  sha256 "3a69fea0b3b7909d21e153c2b9b75848c3345786cdfca42236e6bd3df2b8a5be"
   license "AGPL-3.0-only"
   head "https://github.com/grafana/grafana.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/grafana"
-    sha256 cellar: :any_skip_relocation, mojave: "172565c3d2c22518bf966569e14a9f4876bbfeb579d9142f5e622dbc66e99b35"
+    sha256 cellar: :any_skip_relocation, mojave: "e01a34057506a8d1e3ede819bf371142c97d42da9d34b360a68212c4d6ebad85"
   end
 
   depends_on "go" => :build
