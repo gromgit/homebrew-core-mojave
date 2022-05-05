41f049b94516157914af287ab4fc6eae76cba2f4
Adrian Ho <the.gromgit@gmail.com>
2022-05-05 21:02:13 +0800

    hlint: update 3.4 bottle.

--- Formula/hlint.rb
+++ Formula/hlint.rb
@@ -1,14 +1,14 @@
 class Hlint < Formula
   desc "Haskell source code suggestions"
   homepage "https://github.com/ndmitchell/hlint"
-  url "https://hackage.haskell.org/package/hlint-3.3.6/hlint-3.3.6.tar.gz"
-  sha256 "ae2526f4ca5c4c7090a451b1e0dedddcad98b0ab7d07c05b20de72da41b78e3c"
+  url "https://hackage.haskell.org/package/hlint-3.4/hlint-3.4.tar.gz"
+  sha256 "76fc615d6949fb9478e586c9ddd5510578ddaa32261d94f1a1f3670f20db8e95"
   license "BSD-3-Clause"
   head "https://github.com/ndmitchell/hlint.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/hlint"
-    sha256 cellar: :any_skip_relocation, mojave: "f27229ef0694aadd4aa80262b143d027bc6ad1716af0c9ad4336ad60a2bee6ff"
+    sha256 cellar: :any_skip_relocation, mojave: "a3edbcbb58e05ee90739a853c7d6806a5ea781cb0dd5d2c5033cfc12cb9edac1"
   end
 
   depends_on "cabal-install" => :build
