646f10b01dfc24620be6d6bbd1669aff9b424658
Adrian Ho <the.gromgit@gmail.com>
2022-05-05 21:35:03 +0800

    httpyac: update 5.4.0 bottle.

--- Formula/httpyac.rb
+++ Formula/httpyac.rb
@@ -3,13 +3,13 @@ require "language/node"
 class Httpyac < Formula
   desc "Quickly and easily send REST, SOAP, GraphQL and gRPC requests"
   homepage "https://httpyac.github.io/"
-  url "https://registry.npmjs.org/httpyac/-/httpyac-5.3.0.tgz"
-  sha256 "5f0a012d25832632d94434fe6c4e0d9277ebd7139e075d3af7a3598a09b9f721"
+  url "https://registry.npmjs.org/httpyac/-/httpyac-5.4.0.tgz"
+  sha256 "c4a8b54c4224f1a1ca6edfb39c20e869293db0abff3c12d92be089e2e26795ee"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/httpyac"
-    sha256 cellar: :any_skip_relocation, mojave: "e5abf71f2029248827594f57b6b27c1b714cb465c5b2f619ae65f444f4e5ccf5"
+    sha256 cellar: :any_skip_relocation, mojave: "5838c9c05473e59149d25c9fc92c2e915256381e6d4fcc2e8f7a1318f9fd57fb"
   end
 
   depends_on "node"
@@ -51,7 +51,7 @@ class Httpyac < Formula
     # for httpbin call
     assert_match "HTTP/1.1 200  - OK", output
     # for mqtt calls
-    assert_match "2 requests tested (2 succeeded, 0 failed)", output
+    assert_match "2 requests processed (2 succeeded, 0 failed)", output
 
     assert_match version.to_s, shell_output("#{bin}/httpyac --version")
   end
