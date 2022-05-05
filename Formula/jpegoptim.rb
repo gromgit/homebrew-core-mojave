dbf132d2a03d854119b0cb21223f576f63b89ed2
Adrian Ho <the.gromgit@gmail.com>
2022-05-06 02:01:07 +0800

    jpegoptim: update 1.4.7 bottle.

--- Formula/jpegoptim.rb
+++ Formula/jpegoptim.rb
@@ -1,15 +1,14 @@
 class Jpegoptim < Formula
   desc "Utility to optimize JPEG files"
   homepage "https://github.com/tjko/jpegoptim"
-  url "https://github.com/tjko/jpegoptim/archive/RELEASE.1.4.6.tar.gz"
-  sha256 "c44dcfac0a113c3bec13d0fc60faf57a0f9a31f88473ccad33ecdf210b4c0c52"
-  license "GPL-2.0"
+  url "https://github.com/tjko/jpegoptim/archive/RELEASE.1.4.7.tar.gz"
+  sha256 "9d2a13b7c531d122f360209422645206931c74ada76497c4aeb953610f0d70c1"
+  license "GPL-3.0-or-later"
   head "https://github.com/tjko/jpegoptim.git", branch: "master"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jpegoptim"
-    rebuild 1
-    sha256 cellar: :any, mojave: "fbfefe7357558e15401debf22225f4cb5e084a51f035167237e44d8b2744a6aa"
+    sha256 cellar: :any, mojave: "08a4bcb2962fec7ec87644985f1bf02ccfe6aaab516ffb8410ed2487993bc8a8"
   end
 
   depends_on "jpeg"
