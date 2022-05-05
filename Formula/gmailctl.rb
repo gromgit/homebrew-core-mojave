71d01372f595f58f6a6d2ade6265fbcc78a533e6
Adrian Ho <the.gromgit@gmail.com>
2022-05-05 17:46:22 +0800

    gmailctl: update 0.10.2 bottle.

--- Formula/gmailctl.rb
+++ Formula/gmailctl.rb
@@ -1,14 +1,14 @@
 class Gmailctl < Formula
   desc "Declarative configuration for Gmail filters"
   homepage "https://github.com/mbrt/gmailctl"
-  url "https://github.com/mbrt/gmailctl/archive/v0.10.0.tar.gz"
-  sha256 "ebc3f13b71363e49b804daadc3e1fff394cf42769429e4b081de86100fb5f685"
+  url "https://github.com/mbrt/gmailctl/archive/v0.10.2.tar.gz"
+  sha256 "e2f82a83dd8487b66142ba81783d5bf48f354e7bfbcac39ffff3b057d2121bb2"
   license "MIT"
   head "https://github.com/mbrt/gmailctl.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gmailctl"
-    sha256 cellar: :any_skip_relocation, mojave: "1b0a2a9214b3c283333d955f08da457e2c7f6cdd1d47e4bbe8a6cf36450a6d91"
+    sha256 cellar: :any_skip_relocation, mojave: "8bfdc2bb7a8ac9d726a24ee33f504330c1badba1be4ac40441ab1b3be7d9056a"
   end
 
   depends_on "go" => :build
