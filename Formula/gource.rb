7931b6273b7a84aedbeaa5cd8e253d707f1f038a
Adrian Ho <the.gromgit@gmail.com>
2022-05-05 18:07:07 +0800

    gource: update 0.53 bottle.

--- Formula/gource.rb
+++ Formula/gource.rb
@@ -1,13 +1,13 @@
 class Gource < Formula
   desc "Version Control Visualization Tool"
   homepage "https://github.com/acaudwell/Gource"
-  url "https://github.com/acaudwell/Gource/releases/download/gource-0.52/gource-0.52.tar.gz"
-  sha256 "92e713291936cc4688b6d3d52868f4cca4263c2efec9b3848086e93cd9935e08"
+  url "https://github.com/acaudwell/Gource/releases/download/gource-0.53/gource-0.53.tar.gz"
+  sha256 "3d5f64c1c6812f644c320cbc9a9858df97bc6036fc1e5f603ca46b15b8dd7237"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gource"
-    sha256 mojave: "e989e15199228126dfeaad7cc60670926658150bee97427190cb059b03d01e29"
+    sha256 mojave: "7858b331ed3ef43471ea840be6a914d9e8398f80891defd1043e0da55e4d3332"
   end
 
   head do
