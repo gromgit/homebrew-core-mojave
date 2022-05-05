599453a433e5d866cd6d22850d9d1b591239b1c1
Adrian Ho <the.gromgit@gmail.com>
2022-05-05 20:39:17 +0800

    kubernetes-cli: update 1.24.0 bottle.

--- Formula/kubernetes-cli.rb
+++ Formula/kubernetes-cli.rb
@@ -2,8 +2,8 @@ class KubernetesCli < Formula
   desc "Kubernetes command-line interface"
   homepage "https://kubernetes.io/"
   url "https://github.com/kubernetes/kubernetes.git",
-      tag:      "v1.23.6",
-      revision: "ad3338546da947756e8a88aa6822e9c11e7eac22"
+      tag:      "v1.24.0",
+      revision: "4ce5a8954017644c5420bae81d72b09b735c21f0"
   license "Apache-2.0"
   head "https://github.com/kubernetes/kubernetes.git", branch: "master"
 
@@ -14,7 +14,7 @@ class KubernetesCli < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kubernetes-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "efdccaa0ab6939bc88031e3c86974d4d3394ebf3fc41d64f824b57df551619f4"
+    sha256 cellar: :any_skip_relocation, mojave: "90a10af1e2cadeeaf795e4b1af7518f36573f8dad4b01fa2000eb0050d0a4ee5"
   end
 
   depends_on "bash" => :build
