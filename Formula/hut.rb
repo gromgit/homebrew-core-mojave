3ee915b4857a3f8293e94865a8d0deb6d966d1be
Adrian Ho <the.gromgit@gmail.com>
2022-05-05 21:45:18 +0800

    hut: update 0.1.0 bottle.

--- Formula/hut.rb
+++ Formula/hut.rb
@@ -4,13 +4,15 @@ class Hut < Formula
   url "https://git.sr.ht/~emersion/hut/archive/v0.1.0.tar.gz"
   sha256 "5af8f1111f9ec1da9a818978eb1f013dfd50ad4311c79d95b0e62ad428ac1c59"
   license "AGPL-3.0-or-later"
+  head "https://git.sr.ht/~emersion/hut", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/hut"
-    sha256 cellar: :any_skip_relocation, mojave: "cc2e3d2ced10d45a7c41b74cc31bd2c1955e4df2f10c23db6078c935abd67771"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "7e798cd20d8d2c3c6d61fd857aec37d8761dc23bdc6db9fce6b48752f9127f7a"
   end
 
-  depends_on "coreutils" => :build # GNU install
+  depends_on "coreutils" => :build # Needed for GNU install in 0.1.0, remove in next release
   depends_on "go" => :build
   depends_on "scdoc" => :build
 
