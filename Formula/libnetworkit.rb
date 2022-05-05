38db048f5573854a3b33698f6048511f96d6f6bb
Adrian Ho <the.gromgit@gmail.com>
2022-05-06 05:07:57 +0800

    libnetworkit: update 9.1.1 bottle.

--- Formula/libnetworkit.rb
+++ Formula/libnetworkit.rb
@@ -11,21 +11,35 @@ class Libnetworkit < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libnetworkit"
-    sha256 cellar: :any, mojave: "5499ef92d445e74e771241c258f7ca7f5cf3685c903d8905152ccfb1f97ebbb8"
+    rebuild 1
+    sha256 cellar: :any, mojave: "24f3087f8c960c9fc47225999e8c391cf259564fcf164eae3e34db665633627c"
   end
 
   depends_on "cmake" => :build
-  depends_on "libomp"
   depends_on "tlx"
+
+  on_macos do
+    depends_on "libomp"
+  end
+
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
 
   def install
     mkdir "build" do
-      system "cmake", ".", *std_cmake_args,
-                           "-DNETWORKIT_EXT_TLX=#{Formula["tlx"].opt_prefix}",
-                           "-DOpenMP_CXX_FLAGS='-Xpreprocessor -fopenmp -I#{Formula["libomp"].opt_prefix}/include'",
-                           "-DOpenMP_CXX_LIB_NAMES='omp'",
-                           "-DOpenMP_omp_LIBRARY=#{Formula["libomp"].opt_prefix}/lib/libomp.dylib",
-                           ".."
+      flags = ["-DNETWORKIT_EXT_TLX=#{Formula["tlx"].opt_prefix}"]
+      # GCC includes libgomp for OpenMP support and does not need any extra flags to use it.
+      if OS.mac?
+        flags += [
+          "-DOpenMP_CXX_FLAGS='-Xpreprocessor -fopenmp -I#{Formula["libomp"].opt_prefix}/include'",
+          "-DOpenMP_CXX_LIB_NAMES='omp'",
+          "-DOpenMP_omp_LIBRARY=#{Formula["libomp"].opt_prefix}/lib/libomp.dylib",
+        ]
+      end
+      system "cmake", ".", *std_cmake_args, *flags, ".."
       system "make", "install"
     end
   end
