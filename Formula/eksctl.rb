d854133ff549cdcd470b524eb37ff320a7ec89de
Adrian Ho <the.gromgit@gmail.com>
2022-05-05 13:57:10 +0800

    eksctl: update 0.95.0 bottle.

--- Formula/eksctl.rb
+++ Formula/eksctl.rb
@@ -2,24 +2,38 @@ class Eksctl < Formula
   desc "Simple command-line tool for creating clusters on Amazon EKS"
   homepage "https://eksctl.io"
   url "https://github.com/weaveworks/eksctl.git",
-      tag:      "0.90.0",
-      revision: "08de3f8af239b0882bc8e5575637bc03b7528d4a"
+      tag:      "0.95.0",
+      revision: "0c31b223180e5c87cdf109c8d1e7cc0ad9e39a9f"
   license "Apache-2.0"
   head "https://github.com/weaveworks/eksctl.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/eksctl"
-    sha256 cellar: :any_skip_relocation, mojave: "f5cf6d4983ce742786b5c3d3daaf728bc7d675c54b5ada84bfbb07e4d665b1e1"
+    sha256 cellar: :any_skip_relocation, mojave: "caf8883b35697ae68eab4222b24cc5e66d52ee387afdafbf39e575385ed15e1e"
   end
 
   depends_on "counterfeiter" => :build
   depends_on "go" => :build
   depends_on "go-bindata" => :build
-  depends_on "ifacemaker" => :build
   depends_on "mockery" => :build
   depends_on "aws-iam-authenticator"
 
+  # Eksctl requires newer version of ifacemaker
+  #
+  # Replace with `depends_on "ifacemaker" => :build` when ifacemaker > 1.2.0
+  # Until then get the resource version from go.mod
+  resource "ifacemaker" do
+    url "https://github.com/vburenin/ifacemaker/archive/b2018d8549dc4d51ce7e2254d6b0a743643613be.tar.gz"
+    sha256 "41888bf97133b4e7e190f2040378661b5bcab290d009e1098efbcb9db0f1d82f"
+  end
+
   def install
+    resource("ifacemaker").stage do
+      system "go", "build", *std_go_args(ldflags: "-s -w", output: buildpath/"ifacemaker")
+    end
+    inreplace "build/scripts/generate-aws-interfaces.sh", "${GOBIN}/ifacemaker",
+                                                          buildpath/"ifacemaker"
+
     ENV["GOBIN"] = HOMEBREW_PREFIX/"bin"
     system "make", "build"
     bin.install "eksctl"
