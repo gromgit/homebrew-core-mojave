54b63911d4544b0bcc85871b63948996c566022a
Adrian Ho <the.gromgit@gmail.com>
2022-05-06 05:41:22 +0800

    libspectre: update 0.2.10_1 bottle.

--- Formula/libspectre.rb
+++ Formula/libspectre.rb
@@ -4,6 +4,7 @@ class Libspectre < Formula
   url "https://libspectre.freedesktop.org/releases/libspectre-0.2.10.tar.gz"
   sha256 "cf60b2a80f6bfc9a6b110e18f08309040ceaa755210bf94c465a969da7524d07"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url "https://libspectre.freedesktop.org/releases/"
@@ -12,7 +13,7 @@ class Libspectre < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libspectre"
-    sha256 cellar: :any, mojave: "60192d37a4b38e6af65d11bc916b521b82cafcb681612276437daa0828cebb89"
+    sha256 cellar: :any, mojave: "8eae0749e22d5813518d257247d2c07d357b22a2dd2d35f8fe59f95d6e555c98"
   end
 
   depends_on "ghostscript"
