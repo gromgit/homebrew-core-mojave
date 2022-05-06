45b439eaecc0bed5d25529af93ec8b34c8bf378d
Adrian Ho <the.gromgit@gmail.com>
2022-05-07 04:37:06 +0800

    rust-analyzer: update 2022-05-02 bottle.

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -2,17 +2,20 @@ class RustAnalyzer < Formula
   desc "Experimental Rust compiler front-end for IDEs"
   homepage "https://rust-analyzer.github.io/"
   url "https://github.com/rust-analyzer/rust-analyzer.git",
-       tag:      "2022-04-04",
-       revision: "46d7ee68f26285db26b2640f2c07d6332380c756"
-  version "2022-04-04"
+       tag:      "2022-05-02",
+       revision: "5dce1ff0212e467271c9e895478670c74d847ee9"
+  version "2022-05-02"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rust-analyzer"
-    sha256 cellar: :any_skip_relocation, mojave: "30b1b20c10b65c1f09bad4fba8a6eb2c47f018533c6446124ed30077cde08a9a"
+    sha256 cellar: :any_skip_relocation, mojave: "08146f6381ddd866184d0a71f8d5e0e5ca43c306457ab16b24594f0e64a37e25"
   end
 
   depends_on "rust" => :build
+
+  # Remove this patch after rust 1.60 (https://github.com/rust-lang/rust-analyzer/issues/12080)
+  patch :DATA
 
   def install
     cd "crates/rust-analyzer" do
@@ -69,3 +72,22 @@ class RustAnalyzer < Formula
     assert_match output, pipe_output("#{bin}/rust-analyzer", input, 0)
   end
 end
+
+
+__END__
+diff --git a/Cargo.lock b/Cargo.lock
+index 1937b8936..e6f321394 100644
+--- a/Cargo.lock
++++ b/Cargo.lock
+@@ -1571,9 +1571,9 @@ checksum = "f2dd574626839106c320a323308629dcb1acfc96e32a8cba364ddc61ac23ee83"
+ 
+ [[package]]
+ name = "smol_str"
+-version = "0.1.22"
++version = "0.1.21"
+ source = "registry+https://github.com/rust-lang/crates.io-index"
+-checksum = "167ee181c12079444893cec9c8f21b13d6b314af789c9fdb041a0645f11ed9d2"
++checksum = "61d15c83e300cce35b7c8cd39ff567c1ef42dde6d4a1a38dbdbf9a59902261bd"
+ dependencies = [
+  "serde",
+ ]
