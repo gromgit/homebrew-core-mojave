df4552609364be4aa4295e3509d36f1d9df3d96c
Adrian Ho <the.gromgit@gmail.com>
2022-05-07 07:07:45 +0800

    sqldiff: update 3.38.3 bottle.

--- Formula/sqldiff.rb
+++ Formula/sqldiff.rb
@@ -1,9 +1,9 @@
 class Sqldiff < Formula
   desc "Displays the differences between SQLite databases"
   homepage "https://www.sqlite.org/sqldiff.html"
-  url "https://sqlite.org/2022/sqlite-src-3380200.zip"
-  version "3.38.2"
-  sha256 "c7c0f070a338c92eb08805905c05f254fa46d1c4dda3548a02474f6fb567329a"
+  url "https://sqlite.org/2022/sqlite-src-3380300.zip"
+  version "3.38.3"
+  sha256 "a104d493e084006bd74ff1ff7ac2eb2b387c7c0a3b63ba6fea2fafb4118313af"
   license "blessing"
 
   livecheck do
@@ -12,7 +12,7 @@ class Sqldiff < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sqldiff"
-    sha256 cellar: :any_skip_relocation, mojave: "52e1cd89051b37e279b4cb4204451d42c4a51b30d817952b54e1a85a34945461"
+    sha256 cellar: :any_skip_relocation, mojave: "b4ace33c1f783959fa6eab9ad27a35adce5e5a349c80cd677d01a974d3c759fc"
   end
 
   uses_from_macos "tcl-tk" => :build
