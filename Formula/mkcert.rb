cf87dec52bd477d0530f3d01eb7fd1629de9883d
Adrian Ho <the.gromgit@gmail.com>
2022-05-06 13:27:05 +0800

    mkcert: update 1.4.4 bottle.

--- Formula/mkcert.rb
+++ Formula/mkcert.rb
@@ -1,14 +1,13 @@
 class Mkcert < Formula
   desc "Simple tool to make locally trusted development certificates"
   homepage "https://github.com/FiloSottile/mkcert"
-  url "https://github.com/FiloSottile/mkcert/archive/v1.4.3.tar.gz"
-  sha256 "eaaf25bf7f6e047dc4da4533cdd5780c143a34f076f3a8096c570ac75a9225d9"
+  url "https://github.com/FiloSottile/mkcert/archive/refs/tags/v1.4.4.tar.gz"
+  sha256 "32bd5519581bf0b03f53e5b22721692b99f39ab5b161dc27532c51eafa512ca9"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mkcert"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "1da5b79d4122df7a5f1b2036e05fdf7b09c81a6a53296dc1857d344cb2f3e33d"
+    sha256 cellar: :any_skip_relocation, mojave: "b5c490a594eb0b2a7b7a2f0cb26eebc2d790dc8fdd409a5df2e46ebad88049a0"
   end
 
   depends_on "go" => :build
