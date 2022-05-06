d139a9e5a730dc839ab0ba7f27827ef7110f01e8
Adrian Ho <the.gromgit@gmail.com>
2022-05-07 01:43:59 +0800

    qbs: update 1.22.0 bottle.

--- Formula/qbs.rb
+++ Formula/qbs.rb
@@ -1,10 +1,19 @@
 class Qbs < Formula
   desc "Build tool for developing projects across multiple platforms"
   homepage "https://wiki.qt.io/Qbs"
-  url "https://download.qt.io/official_releases/qbs/1.21.0/qbs-src-1.21.0.tar.gz"
-  sha256 "bfed9d93f94989986dd72e81fac5f39e30de058431133a21aaf9971455b335e7"
   license :cannot_represent
   head "https://code.qt.io/qbs/qbs.git", branch: "master"
+
+  stable do
+    url "https://download.qt.io/official_releases/qbs/1.22.0/qbs-src-1.22.0.tar.gz"
+    sha256 "ebfd4b4f115f7ad235477ddf74cc7216dfa66b6166372dc0be454691078f9f3e"
+
+    # Fix Xcode support for 13.3. Remove in the next release.
+    patch do
+      url "https://code.qt.io/cgit/qbs/qbs.git/patch/?id=d64c7802fef2872aa6a78c06648a0aed45250955"
+      sha256 "cccc3fc3a00ddd71e12ad7f89d4e391c476b537719ba904a3afc42e658df4313"
+    end
+  end
 
   livecheck do
     url "https://download.qt.io/official_releases/qbs/"
@@ -13,8 +22,7 @@ class Qbs < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/qbs"
-    rebuild 1
-    sha256 cellar: :any, mojave: "2a4230cf789ae9cc439da2859683ad783123fc7097d3f8110303e40f77eed0d0"
+    sha256 cellar: :any, mojave: "301e04b490cf6de55fb1c45c4a2637d4a313567f50696cdcfa34ac1cdf4f47ff"
   end
 
   depends_on "cmake" => :build
