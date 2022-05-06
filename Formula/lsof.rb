a8202885b6497eae5ecbb2768c718ddab5173657
Adrian Ho <the.gromgit@gmail.com>
2022-05-06 09:02:52 +0800

    lsof: update 4.95.0 bottle.

--- Formula/lsof.rb
+++ Formula/lsof.rb
@@ -1,19 +1,13 @@
 class Lsof < Formula
   desc "Utility to list open files"
   homepage "https://github.com/lsof-org/lsof"
-  url "https://github.com/lsof-org/lsof/archive/4.94.0.tar.gz"
-  sha256 "a9865eeb581c3abaac7426962ddb112ecfd86a5ae93086eb4581ce100f8fa8f4"
+  url "https://github.com/lsof-org/lsof/archive/4.95.0.tar.gz"
+  sha256 "8ff4c77736cc7d9556da9e2c7614cc4292a12f1979f20bd520d3c6f64b66a4d7"
   license "Zlib"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b446898f51c1ba795ee34955ba5f2a9f482c5cfa54973717db1ed40dfd44af7a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3202f83509eefa73603b865ea4aca0433bbbf69d30f43d229a5ee256d1977424"
-    sha256 cellar: :any_skip_relocation, monterey:       "4a7a23611d5ed1fdb24ea4fd8dceec7a51f04d1c582dfe5dc4941323738aafd9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7dbab0c2a35d97381ed52fa32e1507c0fe83bc405fc40c4d00c79e12c79cffe4"
-    sha256 cellar: :any_skip_relocation, catalina:       "58d2ee9a7484541a7280f5a139f2d0454b494f54bca3b9f10273e036d8071bde"
-    sha256 cellar: :any_skip_relocation, mojave:         "9eb185a83e641bd8bd90fab3a8cde572b23ebb1ce269a8832fb85a66c5037318"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "268fe15ecc8d9e4dd4f2f45737c921e54a5aa999f15ab6b724b9bd34deeef8d1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bee8fc9744aa9543709ec247328c89e361dadde77cc64d10a81aed761e5a0f47"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lsof"
+    sha256 cellar: :any_skip_relocation, mojave: "aafa7fdab49eaf6f6a56a2ca919e7505bdbdede831d3c40b32558deb6f673046"
   end
 
   keg_only :provided_by_macos
