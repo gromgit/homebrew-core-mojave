9d696abe33b42a060bae098e38aa5e0ce14c942d
Adrian Ho <the.gromgit@gmail.com>
2022-05-06 12:46:24 +0800

    minimap2: update 2.24 bottle.

---
+++ Formula/minimap2.rb
@@ -0,0 +1,30 @@
+class Minimap2 < Formula
+  desc "Versatile pairwise aligner for genomic and spliced nucleotide sequences"
+  homepage "https://lh3.github.io/minimap2"
+  url "https://github.com/lh3/minimap2/archive/refs/tags/v2.24.tar.gz"
+  sha256 "2e3264300661cf1fce6adabffe6970ec59d46f3e8150dd40fa4501ff4f6c0dbc"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/minimap2"
+    sha256 cellar: :any_skip_relocation, mojave: "1a1f59a071800844c8aff224e4d5fa15c1725323658e0a7b3c1e06cce7008b3e"
+  end
+
+  uses_from_macos "zlib"
+
+  def install
+    if Hardware::CPU.arm?
+      system "make", "arm_neon=1", "aarch64=1"
+    else
+      system "make"
+    end
+    bin.install "minimap2"
+    pkgshare.install "test"
+  end
+
+  test do
+    cp_r pkgshare/"test/.", testpath
+    output = shell_output("#{bin}/minimap2 -a MT-human.fa MT-orang.fa 2>&1")
+    assert_match "mapped 1 sequences", output
+  end
+end
