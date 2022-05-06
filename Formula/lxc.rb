98297b1664a141b4b800c67ae99b9dc9287a374b
Adrian Ho <the.gromgit@gmail.com>
2022-05-06 09:15:13 +0800

    lxc: update 5.1 bottle.

--- Formula/lxc.rb
+++ Formula/lxc.rb
@@ -1,8 +1,8 @@
 class Lxc < Formula
   desc "CLI client for interacting with LXD"
   homepage "https://linuxcontainers.org"
-  url "https://linuxcontainers.org/downloads/lxd/lxd-5.0.0.tar.gz"
-  sha256 "a99b7edfb52c8195b2de4988844d32d73be6426f6cff28408250517b238fdef9"
+  url "https://linuxcontainers.org/downloads/lxd/lxd-5.1.tar.gz"
+  sha256 "319f4e93506e2144edaa280b0185fb37c4374cf7d7468a5e5c8c1b678189250a"
   license "Apache-2.0"
 
   livecheck do
@@ -12,7 +12,7 @@ class Lxc < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lxc"
-    sha256 cellar: :any_skip_relocation, mojave: "85a1f9e8f223c99ef7526faef51696c01a9e2d3141e30476f8e57ff082da91ea"
+    sha256 cellar: :any_skip_relocation, mojave: "53b122bd5576a660df8a38a9b49de04dcb7d0061b974190ff08d281a018c7b0a"
   end
 
   depends_on "go" => :build
