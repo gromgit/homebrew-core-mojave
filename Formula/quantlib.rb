ed874034ac56b5c9ef8f2e1f19ab6d547af1e7b5
Adrian Ho <the.gromgit@gmail.com>
2022-05-07 02:17:57 +0800

    quantlib: update 1.26 bottle.

--- Formula/quantlib.rb
+++ Formula/quantlib.rb
@@ -1,15 +1,13 @@
 class Quantlib < Formula
   desc "Library for quantitative finance"
   homepage "https://www.quantlib.org/"
-  url "https://github.com/lballabio/QuantLib/releases/download/QuantLib-v1.25/QuantLib-1.25.tar.gz"
-  sha256 "0fbe8f621b837b6712d74102892a97a0f09e24a55a34dfc74f1e743a45d73d1d"
+  url "https://github.com/lballabio/QuantLib/releases/download/QuantLib-v1.26/QuantLib-1.26.tar.gz"
+  sha256 "04fe6cc1a3eb7776020093f550d4da89062586cc15d73e92babdf4505e3673e9"
   license "BSD-3-Clause"
-  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/quantlib"
-    rebuild 1
-    sha256 cellar: :any, mojave: "458ad777d6ce968bb615b1ee36eebfec97b12773f40eafd72009587e3ac40e25"
+    sha256 cellar: :any, mojave: "cfeb98ec03045b81ef345be31fa63615ae86b6326f27a6ba96c659f948395281"
   end
 
   head do
