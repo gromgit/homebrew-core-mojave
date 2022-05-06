22ed30c1860f00d49ca3aef3f1c452eb5e746843
Adrian Ho <the.gromgit@gmail.com>
2022-05-07 03:45:16 +0800

    rethinkdb: update 2.4.2 bottle.

--- Formula/rethinkdb.rb
+++ Formula/rethinkdb.rb
@@ -1,8 +1,8 @@
 class Rethinkdb < Formula
   desc "Open-source database for the realtime web"
   homepage "https://rethinkdb.com/"
-  url "https://download.rethinkdb.com/repository/raw/dist/rethinkdb-2.4.1.tgz"
-  sha256 "5f1786c94797a0f8973597796e22545849dc214805cf1962ef76969e0b7d495b"
+  url "https://download.rethinkdb.com/repository/raw/dist/rethinkdb-2.4.2.tgz"
+  sha256 "35e6a76a527d473a7d962515a0414dea6e09300fff368ae713964ce2494d9c0d"
   license "Apache-2.0"
   head "https://github.com/rethinkdb/rethinkdb.git", branch: "next"
 
@@ -12,37 +12,34 @@ class Rethinkdb < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any, monterey: "38494245ae4ca4cd8b7e5d95070bea8cf0525d37254e7c415e993538192baf09"
-    sha256 cellar: :any, big_sur:  "4f4b04c2bc0243cb1be67ee785e85cccbecf507899b439a0497920637e2c14cc"
-    sha256 cellar: :any, catalina: "0937ad0dab31f165b3b33c0d8e9629b44ee4580e74009881ca097edb2a8157c2"
-    sha256 cellar: :any, mojave:   "35771b918dd1e41939aea3a8eec2b3fa8ee38ca4236c1ad0bd2695c3bb598caf"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rethinkdb"
+    sha256 cellar: :any, mojave: "0e95d01be647e53f9b241b232996ae8b39c1e92ed5b093b80129740d396af11f"
   end
 
   depends_on "boost" => :build
-  depends_on :macos # Due to Python 2 (v8 and gyp fail to build)
-  # https://github.com/Homebrew/linuxbrew-core/pull/19614
-  # https://github.com/rethinkdb/rethinkdb/pull/6401
   depends_on "openssl@1.1"
+  depends_on "protobuf"
 
+  uses_from_macos "python" => :build
   uses_from_macos "curl"
-
-  # patch submitted to upstream, https://github.com/rethinkdb/rethinkdb/pull/6934
-  # remove in next release
-  patch do
-    url "https://github.com/chenrui333/rethinkdb/commit/d7d22b4.patch?full_index=1"
-    sha256 "45d387c3360cf6265e27485c273c37224b2ef0000ebd4fdd361dcecc4d2e9791"
-  end
+  uses_from_macos "ncurses"
+  uses_from_macos "zlib"
 
   def install
-    # rethinkdb requires that protobuf be linked against libc++
-    # but brew's protobuf is sometimes linked against libstdc++
-    args = %W[--prefix=#{prefix} --fetch protobuf]
+    ENV.cxx11
+    # Can use system Python 2 for older macOS. See https://rethinkdb.com/docs/build
+    ENV["PYTHON"] = which("python3") if !OS.mac? || MacOS.version >= :catalina
+
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+    ]
     args << "--allow-fetch" if build.head?
 
     system "./configure", *args
     system "make"
-    system "make", "install-osx"
+    system "make", "install-binaries"
 
     (var/"log/rethinkdb").mkpath
 
