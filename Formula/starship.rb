62b41026bf533c0e9665f809440b78dff13093e8
Adrian Ho <the.gromgit@gmail.com>
2022-05-07 07:49:45 +0800

    starship: update 1.6.3 bottle.

--- Formula/starship.rb
+++ Formula/starship.rb
@@ -1,14 +1,14 @@
 class Starship < Formula
   desc "Cross-shell prompt for astronauts"
   homepage "https://starship.rs"
-  url "https://github.com/starship/starship/archive/v1.5.4.tar.gz"
-  sha256 "158003cd192f9375e504b9ab84d9239a06a8f9732cdd201243ab2fdcd38043f8"
+  url "https://github.com/starship/starship/archive/v1.6.3.tar.gz"
+  sha256 "a6219189eb1e9182eb092213ce4cdd5fba84ae148cb9c4188610a907231a77c7"
   license "ISC"
   head "https://github.com/starship/starship.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/starship"
-    sha256 cellar: :any_skip_relocation, mojave: "8ac2e0650094c69a276952c44041222c214fc5e1f94894f30ad9cbebfe2e28b1"
+    sha256 cellar: :any_skip_relocation, mojave: "b6004424b1d4ff426913fd02685690de65c0f6aaff1c9d264f4ee34935803fca"
   end
 
   depends_on "rust" => :build
