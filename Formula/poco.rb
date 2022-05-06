ce4c15778907d2f563b321d0bb1ee514ac41050b
Adrian Ho <the.gromgit@gmail.com>
2022-05-06 22:12:14 +0800

    poco: update 1.11.2 bottle.

--- Formula/poco.rb
+++ Formula/poco.rb
@@ -1,8 +1,8 @@
 class Poco < Formula
   desc "C++ class libraries for building network and internet-based applications"
   homepage "https://pocoproject.org/"
-  url "https://pocoproject.org/releases/poco-1.11.1/poco-1.11.1-all.tar.gz"
-  sha256 "31ccce6020047270003bfb5b0da7e2ad432884c23d3cd509c86f47cf3a5e5d2a"
+  url "https://pocoproject.org/releases/poco-1.11.2/poco-1.11.2-all.tar.gz"
+  sha256 "904b538d4096a60e0d5fe67e8812d12c633508a76e1d73fdff1ec300181eaa1d"
   license "BSL-1.0"
   head "https://github.com/pocoproject/poco.git", branch: "master"
 
@@ -13,21 +13,25 @@ class Poco < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/poco"
-    rebuild 2
-    sha256 cellar: :any, mojave: "ddac54b6f57fe02bca40df2303b0f8babde61a74fc838557adb17dce1c27dad8"
+    sha256 cellar: :any, mojave: "007c54290edd9e17509c06b31180850f2ca6c35826205d7823fa4d68f8d2104a"
   end
 
   depends_on "cmake" => :build
   depends_on "openssl@1.1"
+  depends_on "pcre"
+
+  uses_from_macos "expat"
+  uses_from_macos "sqlite"
+  uses_from_macos "zlib"
 
   def install
-    mkdir "build" do
-      system "cmake", "..", *std_cmake_args,
-                            "-DENABLE_DATA_MYSQL=OFF",
-                            "-DENABLE_DATA_ODBC=OFF",
-                            "-DCMAKE_INSTALL_RPATH=#{rpath}"
-      system "make", "install"
-    end
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args,
+                    "-DENABLE_DATA_MYSQL=OFF",
+                    "-DENABLE_DATA_ODBC=OFF",
+                    "-DCMAKE_INSTALL_RPATH=#{rpath}",
+                    "-DPOCO_UNBUNDLED=ON"
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
