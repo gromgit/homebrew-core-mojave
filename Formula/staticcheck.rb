1de2412b9ad42901cd63969c7f19609e1be159c9
Adrian Ho <the.gromgit@gmail.com>
2022-05-07 07:52:46 +0800

    staticcheck: update 2022.1.1 bottle.

--- Formula/staticcheck.rb
+++ Formula/staticcheck.rb
@@ -1,15 +1,14 @@
 class Staticcheck < Formula
   desc "State of the art linter for the Go programming language"
   homepage "https://staticcheck.io/"
-  url "https://github.com/dominikh/go-tools/archive/2022.1.tar.gz"
-  sha256 "aecfced0299fc70d17fc7d8d8dc87590429081250f03cb4c6bdd378fd50353ab"
+  url "https://github.com/dominikh/go-tools/archive/2022.1.1.tar.gz"
+  sha256 "988756d12527bf9843459a30cc6fbbf2a7d6d75e62326b0387eab6273e992543"
   license "MIT"
-  revision 1
   head "https://github.com/dominikh/go-tools.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/staticcheck"
-    sha256 cellar: :any_skip_relocation, mojave: "8a47716de4d2e8d0d85d29723972e5cd334b2bb1d3b584a64740322e16d2cee2"
+    sha256 cellar: :any_skip_relocation, mojave: "cbf0eb5c47a2bd3280b7fbbfc14f76c1a97a52c8e1cd310510999c1cf661f3bb"
   end
 
   depends_on "go"
