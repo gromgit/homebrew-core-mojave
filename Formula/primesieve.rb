511b9c8ea031ea08482fcbdb3085ef0bdb270c4d
Adrian Ho <the.gromgit@gmail.com>
2022-05-06 22:35:32 +0800

    primesieve: update 7.9 bottle.

--- Formula/primesieve.rb
+++ Formula/primesieve.rb
@@ -1,13 +1,13 @@
 class Primesieve < Formula
   desc "Fast C/C++ prime number generator"
   homepage "https://github.com/kimwalisch/primesieve"
-  url "https://github.com/kimwalisch/primesieve/archive/v7.8.tar.gz"
-  sha256 "0cd0490259e6e919c6e3dd3f3e69ac6d91e6cbe616e22a219abe4006d9293d5d"
+  url "https://github.com/kimwalisch/primesieve/archive/v7.9.tar.gz"
+  sha256 "c567f2a1a9d46a70020f920eb2c794142528a31a055995500e7fcb19642b7c91"
   license "BSD-2-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/primesieve"
-    sha256 cellar: :any, mojave: "fb3f1f4d0372c913cdeed1075d5723f188e0a64021f7ed41e30a2d301078c096"
+    sha256 cellar: :any, mojave: "040f8441afc64dd115a354f01bdd945697ea322e3c07f442a66511f93abc803f"
   end
 
   depends_on "cmake" => :build
