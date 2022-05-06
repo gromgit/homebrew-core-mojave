ccf4de943fa905d7238bd4940c5ba44191b528d3
Adrian Ho <the.gromgit@gmail.com>
2022-05-06 16:57:10 +0800

    ngs: update 0.2.14 bottle.

--- Formula/ngs.rb
+++ Formula/ngs.rb
@@ -1,14 +1,14 @@
 class Ngs < Formula
   desc "Powerful programming language and shell designed specifically for Ops"
   homepage "https://ngs-lang.org/"
-  url "https://github.com/ngs-lang/ngs/archive/v0.2.13.tar.gz"
-  sha256 "7648761edb3695292d3289b91f9644c204d42269b8af697c765707ce192e45b5"
+  url "https://github.com/ngs-lang/ngs/archive/v0.2.14.tar.gz"
+  sha256 "9432377548ef76c57918b020b2abb258137703ff0172016d58d713186fcafed3"
   license "GPL-3.0-only"
   head "https://github.com/ngs-lang/ngs.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ngs"
-    sha256 cellar: :any, mojave: "8691af55bba86bcdc4a262141fcc7578179a9d64914af0afcfd68f356d1a751e"
+    sha256 cellar: :any, mojave: "3cee1437e12525bba3b78dd8a56ee66c4d5c7d792b030703d4db15ea80420733"
   end
 
   depends_on "cmake" => :build
@@ -23,11 +23,10 @@ class Ngs < Formula
   uses_from_macos "libffi"
 
   def install
-    mkdir "build" do
-      system "cmake", "..", *std_cmake_args
-      system "make", "install"
-    end
-    share.install prefix/"man"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+    share.install prefix/"man" unless OS.mac?
   end
 
   test do
