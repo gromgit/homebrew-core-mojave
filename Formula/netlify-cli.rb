a6c1ac2cb8b236408aa542dbe599753b3adfd93f
Adrian Ho <the.gromgit@gmail.com>
2022-05-06 16:40:21 +0800

    netlify-cli: update 10.1.0 bottle.

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -3,19 +3,17 @@ require "language/node"
 class NetlifyCli < Formula
   desc "Netlify command-line tool"
   homepage "https://www.netlify.com/docs/cli"
-  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-9.16.0.tgz"
-  sha256 "48efe2134b83c1bf0970d295026cc0b25066ed3c9cac2b4dbb449b7ecf73ecb4"
+  url "https://registry.npmjs.org/netlify-cli/-/netlify-cli-10.1.0.tgz"
+  sha256 "11295ee815d6318e5cfb47e885e37733e1529346ecd272d03fd05a51cc5e5086"
   license "MIT"
   head "https://github.com/netlify/cli.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/netlify-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "e920afcb40a3daa5a51ff1b6cb478610206488a5db9b7c6b40a2ad06f5598648"
+    sha256 cellar: :any_skip_relocation, mojave: "7b90755e26dad95f46a0e016958f78a79b8fae9cf81ed851ef2778b0b3eac23a"
   end
 
   depends_on "node"
-
-  uses_from_macos "expect" => :test
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
@@ -23,10 +21,6 @@ class NetlifyCli < Formula
   end
 
   test do
-    (testpath/"test.exp").write <<~EOS
-      spawn #{bin}/netlify login
-      expect "Opening"
-    EOS
-    assert_match "Logging in", shell_output("expect -f test.exp")
+    assert_match "Not logged in. Please log in to see site status.", shell_output("#{bin}/netlify status")
   end
 end
