b9561c2debf421aa2bf1dfa9c42895442090d652
Adrian Ho <the.gromgit@gmail.com>
2022-05-06 19:50:58 +0800

    opa: update 0.40.0 bottle.

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -1,14 +1,14 @@
 class Opa < Formula
   desc "Open source, general-purpose policy engine"
   homepage "https://www.openpolicyagent.org"
-  url "https://github.com/open-policy-agent/opa/archive/v0.39.0.tar.gz"
-  sha256 "19d51c03e940e914da4f707f5095cb1e3d6e0d5becb07bbe916ce20f30355197"
+  url "https://github.com/open-policy-agent/opa/archive/v0.40.0.tar.gz"
+  sha256 "d5087651c0738b9925ecec0e545599b3b2cbf8c7e15791641b1dbc7fdeb23f6c"
   license "Apache-2.0"
   head "https://github.com/open-policy-agent/opa.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/opa"
-    sha256 cellar: :any_skip_relocation, mojave: "8cc374e179a1a9ea4f930abc4baf72c2f85fab4532f718d89795ebc9b4f0558a"
+    sha256 cellar: :any_skip_relocation, mojave: "13e1952b017df1e0a30e1dafdf6eef66080b90a41533967c7e9bc1cb1d040d51"
   end
 
   depends_on "go" => :build
