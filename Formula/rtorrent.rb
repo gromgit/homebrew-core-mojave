27f8336b9609f572c2d6b73f61bbe1c2a927b48f
Adrian Ho <the.gromgit@gmail.com>
2022-05-07 04:24:58 +0800

    rtorrent: update 0.9.8_2 bottle.

--- Formula/rtorrent.rb
+++ Formula/rtorrent.rb
@@ -4,17 +4,11 @@ class Rtorrent < Formula
   url "https://github.com/rakshasa/rtorrent/releases/download/v0.9.8/rtorrent-0.9.8.tar.gz"
   sha256 "9edf0304bf142215d3bc85a0771446b6a72d0ad8218efbe184b41e4c9c7542af"
   license "GPL-2.0"
-  revision 1
+  revision 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "3e9039176b270be8676195b8296e8c4e3cebef932b220508405f7af34d49a229"
-    sha256 cellar: :any,                 arm64_big_sur:  "8f52ee42bb5991c719be574ecc13975e0ff8ffd3adea9011aaae89944a90c893"
-    sha256 cellar: :any,                 monterey:       "749cc727d4c879a5ffad1d8f935a501b89eaac4ede2713727f705eee6347173e"
-    sha256 cellar: :any,                 big_sur:        "871a1aa520a4a0e35b354a2b350bf5db25e6ddce1f700aa8a222a3993872b124"
-    sha256 cellar: :any,                 catalina:       "fb9d292ae3f773162316eac2516bcc12a5a63718d4638aa4ff08a7d57e8a853e"
-    sha256 cellar: :any,                 mojave:         "7bdf998faac16a4411e0f52ef906e09dff47507cef2b1218ac59b82d9298ae72"
-    sha256 cellar: :any,                 high_sierra:    "ac0c6f9edd1fc875266e08a9d7576251d8be88f86162e5a431b10a5c5bb5f0b0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bb72e50c80992b004188713becd80016cdfdb6790c71f1207d969ab8ec4da55e"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rtorrent"
+    sha256 cellar: :any, mojave: "b39139497b57875a7731a11b6dea1f8394bb6e892c752c4e46523d71b778fee5"
   end
 
   depends_on "autoconf" => :build
