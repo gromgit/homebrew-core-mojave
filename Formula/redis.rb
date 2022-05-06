cf1816d567edff8f83f94d9a9839eecfd815c0a4
Adrian Ho <the.gromgit@gmail.com>
2022-05-07 03:00:37 +0800

    redis: update 7.0.0 bottle.

--- Formula/redis.rb
+++ Formula/redis.rb
@@ -1,8 +1,8 @@
 class Redis < Formula
   desc "Persistent key-value database, with built-in net interface"
   homepage "https://redis.io/"
-  url "https://download.redis.io/releases/redis-6.2.6.tar.gz"
-  sha256 "5b2b8b7a50111ef395bf1c1d5be11e6e167ac018125055daa8b5c2317ae131ab"
+  url "https://download.redis.io/releases/redis-7.0.0.tar.gz"
+  sha256 "284d8bd1fd85d6a55a05ee4e7c31c31977ad56cbf344ed83790beeb148baa720"
   license "BSD-3-Clause"
   head "https://github.com/redis/redis.git", branch: "unstable"
 
@@ -12,13 +12,8 @@ class Redis < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "a656500c3b5762c7cfe03d587a4fa08c5df4568783d167555962d850e8cab3c3"
-    sha256 cellar: :any,                 arm64_big_sur:  "846aada68ca07b36d58fd620ed5d52ae67a759526c5da27042748363bfdb6271"
-    sha256 cellar: :any,                 monterey:       "ac30519a604ff014e3903893ddca6c563c134002fec58df3613632e42c4d117c"
-    sha256 cellar: :any,                 big_sur:        "246f73498993a2a0c6c4326a298d2fcc3da6d61904ad09a631aa9c63a6800f76"
-    sha256 cellar: :any,                 catalina:       "ff93a763d622cc9130c09fa9ce2ec7236f91562667eaa5c304fcf175c1253746"
-    sha256 cellar: :any,                 mojave:         "57842762aad1434f8b511f603364b4528f0545f7d768c9387b362011351cda2b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8398fc05ef8eb1ea3d7b26844e3a314a948b3d0d4fb937a00c6c62f0abbe340a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/redis"
+    sha256 cellar: :any, mojave: "1b286304b37ef494ec7a1aa1d29a264e3a68897d1df8aa125f99bdfc4d7a8449"
   end
 
   depends_on "openssl@1.1"
