6d7561d60dd656cb9e6d19653b3d3938a0180f62
Adrian Ho <the.gromgit@gmail.com>
2022-05-07 08:56:26 +0800

    stylua: update 0.13.1 bottle.

--- Formula/stylua.rb
+++ Formula/stylua.rb
@@ -1,13 +1,13 @@
 class Stylua < Formula
   desc "Opinionated Lua code formatter"
   homepage "https://github.com/JohnnyMorganz/StyLua"
-  url "https://github.com/JohnnyMorganz/StyLua/archive/refs/tags/v0.13.0.tar.gz"
-  sha256 "ad74d1f47b0337810459dab9b49694bd5bd6c8c7381a35dcab7866752652988b"
+  url "https://github.com/JohnnyMorganz/StyLua/archive/refs/tags/v0.13.1.tar.gz"
+  sha256 "21158028569158ec7c1ad71352f3cb1906a005eb797508aa2b0b4a861162cf72"
   license "MPL-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/stylua"
-    sha256 cellar: :any_skip_relocation, mojave: "38357b7d5bac38f06933e95e8742f2d260fb9a2715bc81ce233dd51615f74972"
+    sha256 cellar: :any_skip_relocation, mojave: "d3225147cdb3ca0b80793d2fa4f98b3e64216c09554c68a1db69143c0f4e0c82"
   end
 
   depends_on "rust" => :build
