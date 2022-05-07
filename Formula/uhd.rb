dc938a22c160ad719501244dfcd5434bb3ca5dd1
Adrian Ho <the.gromgit@gmail.com>
2022-05-07 12:41:30 +0800

    uhd: update 4.2.0.0 bottle.

--- Formula/uhd.rb
+++ Formula/uhd.rb
@@ -3,10 +3,9 @@ class Uhd < Formula
   homepage "https://files.ettus.com/manual/"
   # The build system uses git to recover version information
   url "https://github.com/EttusResearch/uhd.git",
-      tag:      "v4.1.0.5",
-      revision: "6bd0be9cda5db97081e4f3ee3127c45eed21239c"
+      tag:      "v4.2.0.0",
+      revision: "46a70d853267c40205a8cfea472056bd1aa7c04e"
   license all_of: ["GPL-3.0-or-later", "LGPL-3.0-or-later", "MIT", "BSD-3-Clause", "Apache-2.0"]
-  revision 1
   head "https://github.com/EttusResearch/uhd.git", branch: "master"
 
   livecheck do
@@ -16,8 +15,7 @@ class Uhd < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/uhd"
-    rebuild 1
-    sha256 mojave: "ba0154a3874afd803c5f2e9a907fa716c8d3c543b44d250e6a66661fa74a0346"
+    sha256 mojave: "91bfeafd4b74f90715610a6d6aec696712af2a2c29a5cf7085a2cbdfa68231cb"
   end
 
   depends_on "cmake" => :build
@@ -34,17 +32,24 @@ class Uhd < Formula
   fails_with gcc: "5"
 
   resource "Mako" do
-    url "https://files.pythonhosted.org/packages/af/b6/42cd322ae555aa770d49e31b8c5c28a243ba1bbb57ad927e1a5f5b064811/Mako-1.1.6.tar.gz"
-    sha256 "4e9e345a41924a954251b95b4b28e14a301145b544901332e658907a7464b6b2"
+    url "https://files.pythonhosted.org/packages/50/ec/1d687348f0954bda388bfd1330c158ba8d7dea4044fc160e74e080babdb9/Mako-1.2.0.tar.gz"
+    sha256 "9a7c7e922b87db3686210cf49d5d767033a41d4010b284e747682c92bddd8b39"
+  end
+
+  resource "MarkupSafe" do
+    url "https://files.pythonhosted.org/packages/1d/97/2288fe498044284f39ab8950703e88abbac2abbdf65524d576157af70556/MarkupSafe-2.1.1.tar.gz"
+    sha256 "7f91197cc9e48f989d12e4e6fbc46495c446636dfc81b9ccf50bb0ec74b91d4b"
   end
 
   def install
     xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
 
-    resource("Mako").stage do
-      system Formula["python@3.9"].opt_bin/"python3",
-             *Language::Python.setup_install_args(libexec/"vendor")
+    resources.each do |r|
+      r.stage do
+        system Formula["python@3.9"].opt_bin/"python3",
+              *Language::Python.setup_install_args(libexec/"vendor")
+      end
     end
 
     mkdir "host/build" do
