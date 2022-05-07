5d7abd7c66f91d6f649237f2b8a0e330298e474c
Adrian Ho <the.gromgit@gmail.com>
2022-05-07 11:38:01 +0800

    triangle: update 2.0.0 bottle.

--- Formula/triangle.rb
+++ Formula/triangle.rb
@@ -1,17 +1,16 @@
 class Triangle < Formula
   desc "Convert images to computer generated art using Delaunay triangulation"
   homepage "https://github.com/esimov/triangle"
-  url "https://github.com/esimov/triangle/archive/v1.2.5.tar.gz"
-  sha256 "e7b729601023620aaba3707db80aeeeee06286e131095da25d248a8325d2c549"
+  url "https://github.com/esimov/triangle/archive/v2.0.0.tar.gz"
+  sha256 "071ba2a39b62e7914a233af74e7935ddb7a875bc2a5f193cd43862da65b1c516"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/triangle"
-    sha256 cellar: :any_skip_relocation, mojave: "31e06e9c3ae3f27c7b30973b38dbbec83aa4f740261f51fa8f21f141c887fa0c"
+    sha256 cellar: :any_skip_relocation, mojave: "b9566246f42449f124c59c4dbab68b204e70ddf4394da59872973d338fb03b74"
   end
 
-  # Bump to 1.18 on the next release, if possible.
-  depends_on "go@1.17" => :build
+  depends_on "go" => :build
 
   def install
     system "go", "build", "-mod=vendor", "-o", "#{bin}/triangle", "./cmd/triangle"
