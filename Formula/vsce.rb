63635b181b6de0b1cc93b31af9c3364a06962cd9
Adrian Ho <the.gromgit@gmail.com>
2022-05-07 15:36:18 +0800

    vsce: update 2.7.0 bottle.

---
+++ Formula/vsce.rb
@@ -0,0 +1,36 @@
+require "language/node"
+
+class Vsce < Formula
+  desc "Tool for packaging, publishing and managing VS Code extensions"
+  homepage "https://code.visualstudio.com/api/working-with-extensions/publishing-extension#vsce"
+  url "https://registry.npmjs.org/vsce/-/vsce-2.7.0.tgz"
+  sha256 "68016d50a3208624c525dd8d214ac81fb37772d94f1565740a2e9d061a25327c"
+  license "MIT"
+  head "https://github.com/microsoft/vscode-vsce.git", branch: "main"
+
+  livecheck do
+    url "https://registry.npmjs.org/vsce/latest"
+    regex(/["']version["']:\s*?["']([^"']+)["']/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/vsce"
+    sha256 mojave: "512cc5fcf1e538425fe184dab73f0a60df89479f1aeb58069630366a3a936d1d"
+  end
+
+  depends_on "node"
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "libsecret"
+  end
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir[libexec/"bin/*"]
+  end
+
+  test do
+    error = shell_output(bin/"vsce verify-pat 2>&1", 1)
+    assert_match "The Personal Access Token is mandatory", error
+  end
+end
