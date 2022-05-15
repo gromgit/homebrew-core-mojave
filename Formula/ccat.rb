70f77d509124f89e54ecea4496509a2b8611d193
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-15 09:10:42 +0800

    ccat: no rebuilding required

--- Formula/ccat.rb
+++ Formula/ccat.rb
@@ -1,7 +1,7 @@
 class Ccat < Formula
   desc "Like cat but displays content with syntax highlighting"
-  homepage "https://github.com/jingweno/ccat"
-  url "https://github.com/jingweno/ccat/archive/v1.1.0.tar.gz"
+  homepage "https://github.com/owenthereal/ccat"
+  url "https://github.com/owenthereal/ccat/archive/v1.1.0.tar.gz"
   sha256 "b02d2c8d573f5d73595657c7854c9019d3bd2d9e6361b66ce811937ffd2bfbe1"
   license "MIT"
 
