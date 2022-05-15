35f5f1a008ddf27628f6906e6c093310a65f925e
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-15 09:11:45 +0800

    xlearn: no rebuilding required

--- Formula/xlearn.rb
+++ Formula/xlearn.rb
@@ -7,11 +7,12 @@ class Xlearn < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, monterey:    "36bbe9dd0cc0deb15f9bca3a0f8db3da4e57cf4c62f3cfb2138b5bb88f7f4587"
-    sha256 cellar: :any, big_sur:     "a28e91b107a782fe4bfa9894ba647a36ed7669f25978bc0cec1ce25627d19b6d"
-    sha256 cellar: :any, catalina:    "4edeafacfb2f12dabd7fa08bb60d62186912c6e000a496fd5bf31523ecaa3557"
-    sha256 cellar: :any, mojave:      "e5f597c563cf3ed1ca7e4ebdc733740b976710730f4388c3e4829552713b966d"
-    sha256 cellar: :any, high_sierra: "738b94f1c782c6bce8fe042bb80b48ade32b909297a0c55bc34004f60b449463"
+    sha256 cellar: :any,                 monterey:     "36bbe9dd0cc0deb15f9bca3a0f8db3da4e57cf4c62f3cfb2138b5bb88f7f4587"
+    sha256 cellar: :any,                 big_sur:      "a28e91b107a782fe4bfa9894ba647a36ed7669f25978bc0cec1ce25627d19b6d"
+    sha256 cellar: :any,                 catalina:     "4edeafacfb2f12dabd7fa08bb60d62186912c6e000a496fd5bf31523ecaa3557"
+    sha256 cellar: :any,                 mojave:       "e5f597c563cf3ed1ca7e4ebdc733740b976710730f4388c3e4829552713b966d"
+    sha256 cellar: :any,                 high_sierra:  "738b94f1c782c6bce8fe042bb80b48ade32b909297a0c55bc34004f60b449463"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "988336d4ee9d33798bc18e46135225033cbbf083aada8ba0e7bdbfa968718229"
   end
 
   depends_on "cmake" => :build
@@ -26,7 +27,7 @@ class Xlearn < Formula
 
       bin.install "xlearn_train"
       bin.install "xlearn_predict"
-      lib.install "lib/libxlearn_api.dylib"
+      lib.install "lib/#{shared_library("libxlearn_api")}"
     end
 
     pkgshare.install "demo"
