aeac3205fb5b74517f19803cafdc387b13cfe685
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-15 09:11:45 +0800

    xsimd: no rebuilding required

--- Formula/xsimd.rb
+++ Formula/xsimd.rb
@@ -1,12 +1,12 @@
 class Xsimd < Formula
   desc "Modern, portable C++ wrappers for SIMD intrinsics"
   homepage "https://xsimd.readthedocs.io/en/latest/"
-  url "https://github.com/QuantStack/xsimd/archive/8.0.5.tar.gz"
-  sha256 "0e1b5d973b63009f06a3885931a37452580dbc8d7ca8ad40d4b8c80d2a0f84d7"
+  url "https://github.com/xtensor-stack/xsimd/archive/8.1.0.tar.gz"
+  sha256 "d52551360d37709675237d2a0418e28f70995b5b7cdad7c674626bcfbbf48328"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "b9471757cf5fe31f94cae8ead2b12616efb9ac835f268e4d57ae9d299647c42b"
+    sha256 cellar: :any_skip_relocation, all: "1bdbce654f7c9a3c36cf1dcdc132e5fb971aec1c91244efd41a2d7bd818cd238"
   end
 
   depends_on "cmake" => :build
