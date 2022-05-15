a196568d9ad7d77b5064642f749df18193cd64ee
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-15 09:11:05 +0800

    jdtls: no rebuilding required

--- Formula/jdtls.rb
+++ Formula/jdtls.rb
@@ -1,9 +1,9 @@
 class Jdtls < Formula
   desc "Java language specific implementation of the Language Server Protocol"
   homepage "https://github.com/eclipse/eclipse.jdt.ls"
-  url "https://download.eclipse.org/jdtls/milestones/1.10.0/jdt-language-server-1.10.0-202204131925.tar.gz"
-  version "1.10.0"
-  sha256 "b0faaf4ff8817cae607a6c2d54b78baad6306de3ab9104ff252b22eb33d82049"
+  url "https://download.eclipse.org/jdtls/milestones/1.11.0/jdt-language-server-1.11.0-202205051421.tar.gz"
+  version "1.11.0"
+  sha256 "a9b82b1b0c93e89302369c8c4cf749209168b67341fad37c0829c509a11445c8"
   license "EPL-2.0"
   version_scheme 1
 
@@ -13,7 +13,7 @@ class Jdtls < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "853096ecdfd8a71e00b49ae89001c9baf9dfb6c40eb99ad0b9022b58c10fac0a"
+    sha256 cellar: :any_skip_relocation, all: "9beda534a10281a2fa57c7956968d6b9dac773c48bae07883925c7bd7bc935f6"
   end
 
   depends_on "openjdk@11"
