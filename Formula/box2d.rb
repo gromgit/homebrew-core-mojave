b4131a62d26b3c472b36a96d6ba9dba149deb13e
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-15 09:10:39 +0800

    box2d: no rebuilding required

--- Formula/box2d.rb
+++ Formula/box2d.rb
@@ -7,11 +7,12 @@ class Box2d < Formula
   head "https://github.com/erincatto/Box2D.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:    "ce38e11785a57c7cd416fd4cb6a9cd2567363e1f1bc4938d010b13e0b2eefc34"
-    sha256 cellar: :any_skip_relocation, big_sur:     "bec33552a3bf184fd75f6adbb193b15595c7729dd7f457c833b7826c6253c28d"
-    sha256 cellar: :any_skip_relocation, catalina:    "5c6508a2d661409273a28ac5f0495d7d7c506b5d1bc7ceeb9ab90298db225178"
-    sha256 cellar: :any_skip_relocation, mojave:      "51709abf7cf22ce487b7fb543c2760add5f6935459b00163567448f47ab6d86c"
-    sha256 cellar: :any_skip_relocation, high_sierra: "0312b876dd91ae896fc127fa6afe21736b7dd1d55569389a6cfc20af90f83cd6"
+    sha256 cellar: :any_skip_relocation, monterey:     "ce38e11785a57c7cd416fd4cb6a9cd2567363e1f1bc4938d010b13e0b2eefc34"
+    sha256 cellar: :any_skip_relocation, big_sur:      "bec33552a3bf184fd75f6adbb193b15595c7729dd7f457c833b7826c6253c28d"
+    sha256 cellar: :any_skip_relocation, catalina:     "5c6508a2d661409273a28ac5f0495d7d7c506b5d1bc7ceeb9ab90298db225178"
+    sha256 cellar: :any_skip_relocation, mojave:       "51709abf7cf22ce487b7fb543c2760add5f6935459b00163567448f47ab6d86c"
+    sha256 cellar: :any_skip_relocation, high_sierra:  "0312b876dd91ae896fc127fa6afe21736b7dd1d55569389a6cfc20af90f83cd6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a36a72d0a0d92cc4c981ad6950fcaa106ad23c273e573dd82bdc971379e0ea70"
   end
 
   depends_on "cmake" => :build
@@ -30,8 +31,8 @@ class Box2d < Formula
   end
 
   test do
-    system ENV.cxx, "-L#{lib}", "-lbox2d", "-std=c++11",
-      pkgshare/"hello_world.cpp", "-o", testpath/"test"
+    system ENV.cxx, pkgshare/"hello_world.cpp", "-L#{lib}", "-lbox2d",
+      "-std=c++11", "-o", testpath/"test"
     assert_match "[doctest] Status: SUCCESS!", shell_output("./test")
   end
 end
