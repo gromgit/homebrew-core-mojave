1fcf20bb32ccf32b11c7c3c2705fd541b7b57c98
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-15 10:34:36 +0800

    doxygen: update 1.9.4 bottle.

--- Formula/doxygen.rb
+++ Formula/doxygen.rb
@@ -1,11 +1,10 @@
 class Doxygen < Formula
   desc "Generate documentation for several programming languages"
-  homepage "https://www.doxygen.org/"
-  url "https://doxygen.nl/files/doxygen-1.9.3.src.tar.gz"
-  mirror "https://downloads.sourceforge.net/project/doxygen/rel-1.9.3/doxygen-1.9.3.src.tar.gz"
-  sha256 "f352dbc3221af7012b7b00935f2dfdc9fb67a97d43287d2f6c81c50449d254e0"
+  homepage "https://www.doxygen.nl/"
+  url "https://doxygen.nl/files/doxygen-1.9.4.src.tar.gz"
+  mirror "https://downloads.sourceforge.net/project/doxygen/rel-1.9.4/doxygen-1.9.4.src.tar.gz"
+  sha256 "a15e9cd8c0d02b7888bc8356eac200222ecff1defd32f3fe05257d81227b1f37"
   license "GPL-2.0-only"
-  revision 1
   head "https://github.com/doxygen/doxygen.git", branch: "master"
 
   livecheck do
@@ -15,14 +14,14 @@ class Doxygen < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/doxygen"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "85833a3e2b1b88754d4e24219b8b70c26b002c8f40085e8263c63f79ee121e68"
+    sha256 cellar: :any_skip_relocation, mojave: "a180fdb1c6e91842f52161a0125ab22322e0f5c015a752ea50f4530ce699588e"
   end
 
   depends_on "bison" => :build
   depends_on "cmake" => :build
 
-  uses_from_macos "flex" => :build
+  uses_from_macos "flex" => :build, since: :big_sur
+  uses_from_macos "python" => :build
 
   on_linux do
     depends_on "gcc"
