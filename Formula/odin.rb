814ec4b44877c34d2268451178bce14dbc82a6a4
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-15 09:11:20 +0800

    odin:

--- Formula/odin.rb
+++ Formula/odin.rb
@@ -1,38 +1,38 @@
 class Odin < Formula
   desc "Programming language with focus on simplicity, performance and modern systems"
   homepage "https://odin-lang.org/"
-  url "https://github.com/odin-lang/Odin/archive/v0.13.0.tar.gz"
-  sha256 "ae88c4dcbb8fdf37f51abc701d94fb4b2a8270f65be71063e0f85a321d54cdf0"
-  license "BSD-2-Clause"
-  revision 1
+  url "https://github.com/odin-lang/Odin.git",
+      tag:      "dev-2022-05",
+      revision: "df233aee942bd85a5162a36a82bf33fe74d2f2ad"
+  version "2022-05"
+  license "BSD-3-Clause"
   head "https://github.com/odin-lang/Odin.git", branch: "master"
 
-  livecheck do
-    url :stable
-    regex(/^v?(\d+(?:\.\d+)+[a-z]?)$/i)
-  end
-
   bottle do
-    sha256 cellar: :any,                 monterey:     "f24379e907c9f66dcf9bf07327499982194ea03cc95a3c8ce4029f728bbcfbe8"
-    sha256 cellar: :any,                 big_sur:      "8756628900882ce7b6492cd63a16192c66420c792c4ae6bc625104f10cd4ad91"
-    sha256 cellar: :any,                 catalina:     "a3ae074bcf9f1b096fd0ff73ec3e4169580cbd9c992eaf96df00381fb4bc4aab"
-    sha256 cellar: :any,                 mojave:       "4f2a22f846642e17dd24a44ec9bd5e00a768facbeba1ea76f2b528f1407c6669"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "1a2aed348bd038115e6581e293a69291eca7a2ef89a59589faf36d9ed81089d1"
+    sha256 cellar: :any,                 arm64_monterey: "508b64f49f0b7f182f426e30d16abcc3dff1cdbe708780b93f7262a5f045dc4f"
+    sha256 cellar: :any,                 arm64_big_sur:  "797f9d901ac0dacf0378f945ff4cec2de0dd4b3d63af0d0ca1bacb929e314259"
+    sha256 cellar: :any,                 monterey:       "df3c2d9d81f53a6c86c9e573ff366136cb0f570f8a99064498d515d894481a6b"
+    sha256 cellar: :any,                 big_sur:        "a96a1e8760ad15d81c10c9f7d6dddaaa63db57d76d64446d577fe8491c716aff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3430531b93bb5b6126f3477ce567cd52116f839739bcf19ce337ed1387e08fc3"
   end
 
-  # Check if this can be switched to `llvm` at next release
-  depends_on "llvm@11"
+  depends_on "llvm"
+  # Build failure on macOS 10.15 due to `__ulock_wait2` usage.
+  # Issue ref: https://github.com/odin-lang/Odin/issues/1773
+  depends_on macos: :big_sur
 
-  # Fix test for 11.0. This should be removed with the next version.
-  # https://github.com/odin-lang/Odin/pull/768
-  patch :DATA
+  fails_with gcc: "5" # LLVM is built with GCC
 
   def install
+    # Keep version number consistent and reproducible for tagged releases.
+    # Issue ref: https://github.com/odin-lang/Odin/issues/1772
+    inreplace "build_odin.sh", "dev-$(date +\"%Y-%m\")", "dev-#{version}" unless build.head?
+
     system "make", "release"
     libexec.install "odin", "core", "shared"
     (bin/"odin").write <<~EOS
       #!/bin/bash
-      export PATH="#{Formula["llvm@11"].opt_bin}:$PATH"
+      export PATH="#{Formula["llvm"].opt_bin}:$PATH"
       exec -a odin "#{libexec}/odin" "$@"
     EOS
     pkgshare.install "examples"
@@ -50,76 +50,7 @@ class Odin < Formula
         fmt.println("Hellope!");
       }
     EOS
-    system "#{bin}/odin", "build", "hellope.odin"
-    assert_equal "Hellope!\n", `./hellope`
+    system "#{bin}/odin", "build", "hellope.odin", "-file"
+    assert_equal "Hellope!\n", shell_output("./hellope.bin")
   end
 end
-
-__END__
-diff --git a/src/main.cpp b/src/main.cpp
-index 13d9e53..6dbe658 100644
---- a/src/main.cpp
-+++ b/src/main.cpp
-@@ -341,12 +341,12 @@ i32 linker_stage(lbGenerator *gen) {
- 					String lib_name = lib;
- 					lib_name = remove_extension_from_path(lib_name);
- 					lib_str = gb_string_append_fmt(lib_str, " -framework %.*s ", LIT(lib_name));
--				} else if (string_ends_with(lib, str_lit(".a"))) {
-+                } else if (string_ends_with(lib, str_lit(".a")) || string_ends_with(lib, str_lit(".o")) || string_ends_with(lib, str_lit(".dylib"))) {
-+                    // For:
-+                    // object
-+                    // dynamic lib
- 					// static libs, absolute full path relative to the file in which the lib was imported from
- 					lib_str = gb_string_append_fmt(lib_str, " %.*s ", LIT(lib));
--				} else if (string_ends_with(lib, str_lit(".dylib"))) {
--					// dynamic lib
--					lib_str = gb_string_append_fmt(lib_str, " %.*s ", LIT(lib));
- 				} else {
- 					// dynamic or static system lib, just link regularly searching system library paths
- 					lib_str = gb_string_append_fmt(lib_str, " -l%.*s ", LIT(lib));
-@@ -431,8 +431,12 @@ i32 linker_stage(lbGenerator *gen) {
- 				" -e _main "
- 			#endif
- 			, linker, object_files, LIT(output_base), LIT(output_ext),
-+            #if defined(GB_SYSTEM_OSX)
-+                "-lSystem -lm -syslibroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk -L/usr/local/lib",
-+            #else
-+                "-lc -lm",
-+            #endif
- 			lib_str,
--			"-lc -lm",
- 			LIT(build_context.link_flags),
- 			LIT(build_context.extra_linker_flags),
- 			link_settings);
-@@ -2097,12 +2101,12 @@ int main(int arg_count, char const **arg_ptr) {
- 						String lib_name = lib;
- 						lib_name = remove_extension_from_path(lib_name);
- 						lib_str = gb_string_append_fmt(lib_str, " -framework %.*s ", LIT(lib_name));
--					} else if (string_ends_with(lib, str_lit(".a"))) {
--						// static libs, absolute full path relative to the file in which the lib was imported from
--						lib_str = gb_string_append_fmt(lib_str, " %.*s ", LIT(lib));
--					} else if (string_ends_with(lib, str_lit(".dylib"))) {
--						// dynamic lib
--						lib_str = gb_string_append_fmt(lib_str, " %.*s ", LIT(lib));
-+                    } else if (string_ends_with(lib, str_lit(".a")) || string_ends_with(lib, str_lit(".o")) || string_ends_with(lib, str_lit(".dylib"))) {
-+           				// For:
-+           				// object
-+           				// dynamic lib
-+                        // static libs, absolute full path relative to the file in which the lib was imported from
-+                        lib_str = gb_string_append_fmt(lib_str, " %.*s ", LIT(lib));
- 					} else {
- 						// dynamic or static system lib, just link regularly searching system library paths
- 						lib_str = gb_string_append_fmt(lib_str, " -l%.*s ", LIT(lib));
-@@ -2181,7 +2185,11 @@ int main(int arg_count, char const **arg_ptr) {
- 				#endif
- 				, linker, LIT(output_base), LIT(output_base), LIT(output_ext),
- 				lib_str,
--				"-lc -lm",
-+                #if defined(GB_SYSTEM_OSX)
-+                    "-lSystem -lm -syslibroot /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk -L/usr/local/lib",
-+                #else
-+                    "-lc -lm",
-+                #endif
- 				LIT(build_context.link_flags),
- 				LIT(build_context.extra_linker_flags),
- 				link_settings);
