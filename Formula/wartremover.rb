24b352cd505838ea1ed7f61c18fc7707f36cb629
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-15 09:11:43 +0800

    wartremover: no rebuilding required

--- Formula/wartremover.rb
+++ Formula/wartremover.rb
@@ -1,8 +1,8 @@
 class Wartremover < Formula
   desc "Flexible Scala code linting tool"
   homepage "https://github.com/wartremover/wartremover"
-  url "https://github.com/wartremover/wartremover/archive/v3.0.2.tar.gz"
-  sha256 "217df3b3e9e3123f2956ce2c1fb76ab7090f65aae4647092dd6614126d05c60f"
+  url "https://github.com/wartremover/wartremover/archive/v3.0.3.tar.gz"
+  sha256 "d625dd4e6e84e6a2a38de8ba1c86854bea818950e1f9ac4a54b4e85f50dec66c"
   license "Apache-2.0"
   head "https://github.com/wartremover/wartremover.git", branch: "master"
 
@@ -12,7 +12,7 @@ class Wartremover < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "0c266e8d212ccf7c133ace9a3a08998d142f102938857d6b9883a7fb7a126bde"
+    sha256 cellar: :any_skip_relocation, all: "f67d0af485ebab4e2bd43a28aa8df530c6e3ed20c69c9b8dc27556957b985502"
   end
 
   depends_on "sbt" => :build
