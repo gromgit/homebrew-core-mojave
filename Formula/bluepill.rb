32f9d49bdc486bd1abf9e8cbf2b2e3f6cbfcb656
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-15 09:10:38 +0800

    bluepill: no rebuilding required

--- Formula/bluepill.rb
+++ Formula/bluepill.rb
@@ -1,11 +1,11 @@
 class Bluepill < Formula
   desc "Testing tool for iOS that runs UI tests using multiple simulators"
-  homepage "https://github.com/linkedin/bluepill"
-  url "https://github.com/linkedin/bluepill.git",
+  homepage "https://github.com/MobileNativeFoundation/bluepill"
+  url "https://github.com/MobileNativeFoundation/bluepill.git",
       tag:      "v5.8.1",
       revision: "2dfc0a965ab564d015a2a0f00be89edf53c0f256"
   license "BSD-2-Clause"
-  head "https://github.com/linkedin/bluepill.git", branch: "master"
+  head "https://github.com/MobileNativeFoundation/bluepill.git", branch: "master"
 
   # Typically the preceding `v` is optional in livecheck regexes but we need it
   # to be required here to omit older versions that break version comparison
