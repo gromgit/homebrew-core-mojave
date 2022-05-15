7719b4f37780a77b0f505fa0926304309d2c252e
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-15 09:11:04 +0800

    infrakit: no rebuilding required

--- Formula/infrakit.rb
+++ Formula/infrakit.rb
@@ -1,7 +1,7 @@
 class Infrakit < Formula
   desc "Toolkit for creating and managing declarative infrastructure"
-  homepage "https://github.com/docker/deploykit"
-  url "https://github.com/docker/deploykit.git",
+  homepage "https://github.com/docker-archive/deploykit"
+  url "https://github.com/docker-archive/deploykit.git",
       tag:      "v0.5",
       revision: "3d2670e484176ce474d4b3d171994ceea7054c02"
   license "Apache-2.0"
