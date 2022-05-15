4a64d9f6aee862c53dd1fcd058c7d6f96d22ee45
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-15 11:28:41 +0800

    stanc3: update 2.29.2 bottle.

---
+++ Formula/stanc3.rb
@@ -0,0 +1,43 @@
+class Stanc3 < Formula
+  desc "Stan transpiler"
+  homepage "https://github.com/stan-dev/stanc3"
+  # git is needed for dune subst
+  url "https://github.com/stan-dev/stanc3.git",
+      tag:      "v2.29.2",
+      revision: "2c254b3b11a4dbb82395f016886b557201dad130"
+  license "BSD-3-Clause"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/stanc3"
+    sha256 cellar: :any_skip_relocation, mojave: "ea52c2e5bd63295a3ebeddb28f58e165256b87f9b88e8ef8c3a04e65037b1712"
+  end
+
+  depends_on "ocaml" => :build
+  depends_on "opam" => :build
+
+  uses_from_macos "unzip" => :build
+
+  def install
+    Dir.mktmpdir("opamroot") do |opamroot|
+      ENV["OPAMROOT"] = opamroot
+      ENV["OPAMYES"] = "1"
+      ENV["OPAMVERBOSE"] = "1"
+
+      system "opam", "init", "--no-setup", "--disable-sandboxing"
+      system "bash", "-x", "scripts/install_build_deps.sh"
+      system "opam", "exec", "dune", "subst"
+      system "opam", "exec", "dune", "build", "@install"
+
+      bin.install "_build/default/src/stanc/stanc.exe" => "stanc"
+      pkgshare.install "test"
+    end
+  end
+
+  test do
+    assert_match "stanc3 v#{version}", shell_output("#{bin}/stanc --version")
+
+    cp pkgshare/"test/integration/good/algebra_solver_good.stan", testpath
+    system bin/"stanc", "algebra_solver_good.stan"
+    assert_predicate testpath/"algebra_solver_good.hpp", :exist?
+  end
+end
