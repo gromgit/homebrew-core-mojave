67e8c343650b8a77c73dfbe786e474c18058cbdd
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-15 09:11:03 +0800

    imessage-ruby: no rebuilding required

--- Formula/imessage-ruby.rb
+++ Formula/imessage-ruby.rb
@@ -1,15 +1,13 @@
 class ImessageRuby < Formula
   desc "Command-line tool to send iMessage"
   homepage "https://github.com/linjunpop/imessage"
-  url "https://github.com/linjunpop/imessage/archive/v0.3.1.tar.gz"
-  sha256 "74ccd560dec09dcf0de28cd04fc4d512812c3348fc5618cbb73b6b36c43e14ef"
+  url "https://github.com/linjunpop/imessage/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "3d93570dd3ff000c51859aaa34c4a8e8b80b86e6bdf3018c1c7f6f8d85939b30"
   license "MIT"
   head "https://github.com/linjunpop/imessage.git", branch: "master"
 
-bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/imessage-ruby"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "cb12f32d6e4c5a6195129bca47077ea8a4eab673949e4c3f545363df649f5d48"
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "7c538ac24ec3ce437d868267582bd68aaa500eeb5a9bdbd3d0d80398b4bab19d"
   end
 
   depends_on :macos
