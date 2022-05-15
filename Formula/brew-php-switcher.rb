0f8028d24284719e5999fa679e95758777e0a6a0
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-15 09:10:39 +0800

    brew-php-switcher: no rebuilding required

--- Formula/brew-php-switcher.rb
+++ Formula/brew-php-switcher.rb
@@ -1,6 +1,6 @@
 class BrewPhpSwitcher < Formula
   desc "Switch Apache / Valet / CLI configs between PHP versions"
-  homepage "https://github.com/philcook/php-switcher"
+  homepage "https://github.com/philcook/brew-php-switcher"
   url "https://github.com/philcook/brew-php-switcher/archive/v2.4.tar.gz"
   sha256 "305627aa4165e760f95453efc75c47027e7ecee233c1dd85911a692ca48549af"
   license "MIT"
