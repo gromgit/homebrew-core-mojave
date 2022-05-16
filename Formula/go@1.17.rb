f83ac656c6587809b3d30a44ca508b5904107f2e
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-17 07:18:17 +0800

    go@1.17: update 1.17.10 bottle.

--- Formula/go@1.17.rb
+++ Formula/go@1.17.rb
@@ -1,9 +1,9 @@
 class GoAT117 < Formula
   desc "Go programming environment (1.17)"
   homepage "https://golang.org"
-  url "https://go.dev/dl/go1.17.9.src.tar.gz"
-  mirror "https://fossies.org/linux/misc/go1.17.9.src.tar.gz"
-  sha256 "763ad4bafb80a9204458c5fa2b8e7327fa971aee454252c0e362c11236156813"
+  url "https://go.dev/dl/go1.17.10.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go1.17.10.src.tar.gz"
+  sha256 "299e55af30f15691b015d8dcf8ecae72412412569e5b2ece20361753a456f2f9"
   license "BSD-3-Clause"
 
   livecheck do
@@ -13,7 +13,7 @@ class GoAT117 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/go@1.17"
-    sha256 cellar: :any_skip_relocation, mojave: "aa439f3d737130bfe90ea974d2a367f65e6b87d359d57f8868027b88a85dd03e"
+    sha256 cellar: :any_skip_relocation, mojave: "29daafbec4979b55a7029b656107b3438fe8a8b5b00b7b4af962b61cad10b3c0"
   end
 
   keg_only :versioned_formula
