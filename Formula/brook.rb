19492ba8d2a45dbf9d2eb1f9fef52314f9978a33
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-17 14:27:16 +0800

    brook: update 20220501 bottle.

--- Formula/brook.rb
+++ Formula/brook.rb
@@ -1,13 +1,13 @@
 class Brook < Formula
   desc "Cross-platform strong encryption and not detectable proxy. Zero-Configuration"
   homepage "https://txthinking.github.io/brook/"
-  url "https://github.com/txthinking/brook/archive/refs/tags/v20220406.tar.gz"
-  sha256 "cf4433263cc755edfe56be66d206b7ee5083faaaa8b30bb4102174ad73e22764"
+  url "https://github.com/txthinking/brook/archive/refs/tags/v20220501.tar.gz"
+  sha256 "31474d3a9667a153986710b989d99a4f971255f407d47eebaf15b6fc9c2f4128"
   license "GPL-3.0-only"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/brook"
-    sha256 cellar: :any_skip_relocation, mojave: "634588dba2da76b9b17f4c4e84b87070c90742653af55ba6bebba8bc598299dc"
+    sha256 cellar: :any_skip_relocation, mojave: "ec39b46ef99470d5fe75cca22325a13f4bb4e8f09c325f72630f2e862f76ecfc"
   end
 
   depends_on "go" => :build
@@ -18,6 +18,14 @@ class Brook < Formula
 
   test do
     output = shell_output "#{bin}/brook link --server 1.2.3.4:56789 --password hello"
-    assert_match "brook://server?address=&insecure=&name=&password=hello", output
+    # We expect something like "brook://server?password=hello&server=1.2.3.4%3A56789&username="
+    uri = URI(output)
+    assert_equal "brook", uri.scheme
+    assert_equal "server", uri.host
+
+    query = URI.decode_www_form(uri.query).to_h
+    assert_equal "1.2.3.4:56789", query["server"]
+    assert_equal "hello", query["password"]
+    assert_equal "", query["username"]
   end
 end
