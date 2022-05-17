122939df8a407de8f822983224158942805dd6f6
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-17 20:42:10 +0800

    chicken: update 5.3.0_1 bottle.

--- Formula/chicken.rb
+++ Formula/chicken.rb
@@ -4,6 +4,7 @@ class Chicken < Formula
   url "https://code.call-cc.org/releases/5.3.0/chicken-5.3.0.tar.gz"
   sha256 "c3ad99d8f9e17ed810912ef981ac3b0c2e2f46fb0ecc033b5c3b6dca1bdb0d76"
   license "BSD-3-Clause"
+  revision 1
   head "https://code.call-cc.org/git/chicken-core.git", branch: "master"
 
   livecheck do
@@ -13,8 +14,7 @@ class Chicken < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/chicken"
-    rebuild 2
-    sha256 mojave: "3fcea983da9e05653ea7b30554a41bfd17835ac01c7f4496f12339e9d676b1c1"
+    sha256 mojave: "4d6adcce58bcda98d4d1e0d3eba6072c4551572d366575d7f0790dd6af9dcd83"
   end
 
   def install
@@ -24,7 +24,8 @@ class Chicken < Formula
       PREFIX=#{prefix}
       C_COMPILER=#{ENV.cc}
       LIBRARIAN=ar
-      ARCH=x86-64
+      ARCH=#{Hardware::CPU.arch.to_s.tr("_", "-")}
+      LINKER_OPTIONS=-Wl,-rpath,#{rpath},-rpath,#{HOMEBREW_PREFIX}/lib
     ]
 
     if OS.mac?
@@ -40,5 +41,6 @@ class Chicken < Formula
 
   test do
     assert_equal "25", shell_output("#{bin}/csi -e '(print (* 5 5))'").strip
+    system bin/"csi", "-ne", "(import (chicken tcp))"
   end
 end
