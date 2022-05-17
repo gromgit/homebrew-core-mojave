644b1baa08b97346ed40c5c30a1ad8d06b75c819
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-17 10:09:08 +0800

    asuka: update 0.8.5 bottle.

--- Formula/asuka.rb
+++ Formula/asuka.rb
@@ -1,8 +1,8 @@
 class Asuka < Formula
   desc "Gemini Project client written in Rust with NCurses"
   homepage "https://sr.ht/~julienxx/Asuka/"
-  url "https://git.sr.ht/~julienxx/asuka/archive/0.8.3.tar.gz"
-  sha256 "1305d65e07e83fe33ca102637fa27f8dafae9a9aaa436414c29532c4ed51c6ea"
+  url "https://git.sr.ht/~julienxx/asuka/archive/0.8.5.tar.gz"
+  sha256 "f7be2925cfc7ee6dcdfa4c30b9d4f6963f729c1b3f526ac242c7e1794bb190b1"
   license "MIT"
 
   livecheck do
@@ -11,13 +11,8 @@ class Asuka < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0569256aff79179b0e44f42bd4bbcf511aa3933ed1578ed9b7c6b2f140419b43"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2b5bb131f128e4a410d01615c1ea994365d2e31ac96109f4907cae513675526c"
-    sha256 cellar: :any_skip_relocation, monterey:       "0ed9621d904ee83efd90bd1b38d117f262d7b0d7cc150d0bcec48be30ccaae4d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "26d7c7e0216db2d6909b0770e55dc92f717558aaa3ec8e7330743a58293f38b6"
-    sha256 cellar: :any_skip_relocation, catalina:       "4b44603a1aea30e38c0396d0a59f4d6ed3ba5c10919385c09a58f698da340f5e"
-    sha256 cellar: :any_skip_relocation, mojave:         "b75723fc27ad5f002a9757371d3d76a362e3690e1d434bafb033f19d4e14da98"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0cbf79e00e2a48f4d9f3356bede6bb934f2f7a17306bcce5fbb3cf285f6b1cd7"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/asuka"
+    sha256 cellar: :any_skip_relocation, mojave: "b2f3185c92ee8016a93fff14a133185f67f239dffcaf7e7aea8e37772a872cff"
   end
 
   depends_on "rust" => :build
