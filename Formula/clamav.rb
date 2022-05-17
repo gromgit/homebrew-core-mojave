235e8e9281d975e15dc03fd13563ecb7cbae79ca
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-17 22:03:40 +0800

    clamav: update 0.105.0 bottle.

--- Formula/clamav.rb
+++ Formula/clamav.rb
@@ -1,9 +1,9 @@
 class Clamav < Formula
   desc "Anti-virus software"
   homepage "https://www.clamav.net/"
-  url "https://www.clamav.net/downloads/production/clamav-0.104.2.tar.gz"
-  mirror "https://fossies.org/linux/misc/clamav-0.104.2.tar.gz"
-  sha256 "3e45e46d9aaeb3a6956ed30376237ab7c4cd9573bc0f5d6fc15c588d30978d9d"
+  url "https://www.clamav.net/downloads/production/clamav-0.105.0.tar.gz"
+  mirror "https://fossies.org/linux/misc/clamav-0.105.0.tar.gz"
+  sha256 "270203a54c458049db54fcd93683ff5b2db19151f363c48e82cecefdde2b35d4"
   license "GPL-2.0-or-later"
   head "https://github.com/Cisco-Talos/clamav-devel.git", branch: "main"
 
@@ -14,11 +14,12 @@ class Clamav < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/clamav"
-    sha256 mojave: "e99c0b2e988950d7234850bccbf758d683b80d4677fe5bf7797e9d0859f195d2"
+    sha256 mojave: "586a8d289ebd54d2ca98343606961d0f14f988d32c430a0145c68a34f00914be"
   end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
+  depends_on "rust" => :build
   depends_on "json-c"
   depends_on "libtool"
   depends_on "openssl@1.1"
