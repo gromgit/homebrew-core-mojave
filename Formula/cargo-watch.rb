f7fc7a2b357bef472f37c75cf43f19442068cac5
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-17 17:20:23 +0800

    cargo-watch: update 8.1.2 bottle.

--- Formula/cargo-watch.rb
+++ Formula/cargo-watch.rb
@@ -1,15 +1,15 @@
 class CargoWatch < Formula
   desc "Watches over your Cargo project's source"
-  homepage "https://github.com/passcod/cargo-watch"
-  url "https://github.com/passcod/cargo-watch/archive/v8.1.2.tar.gz"
+  homepage "https://watchexec.github.io/#cargo-watch"
+  url "https://github.com/watchexec/cargo-watch/archive/v8.1.2.tar.gz"
   sha256 "6fe6a45c9acddeb2e8baab84f93fc8bdb04e141639859c52715cba7e57665e97"
   license "CC0-1.0"
-  head "https://github.com/passcod/cargo-watch.git", branch: "main"
+  head "https://github.com/watchexec/cargo-watch.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cargo-watch"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "d04f14272a857929c094eda15f8e4a8f14b98f8a2fabd4a0ab38491858dbe608"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, mojave: "a500f9e6ca0a2e5ef26340ebfcc154c5ae3e5d9f51835bbebae7e7a09b72c805"
   end
 
   depends_on "rust" => [:build, :test]
