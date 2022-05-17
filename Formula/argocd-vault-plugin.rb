4658b719bc9064afa8a59f2e401d1591ba32457a
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-17 09:14:32 +0800

    argocd-vault-plugin: update 1.11.0 bottle.

--- Formula/argocd-vault-plugin.rb
+++ Formula/argocd-vault-plugin.rb
@@ -1,14 +1,15 @@
 class ArgocdVaultPlugin < Formula
   desc "Argo CD plugin to retrieve secrets from Secret Management tools"
   homepage "https://argocd-vault-plugin.readthedocs.io"
-  url "https://github.com/IBM/argocd-vault-plugin.git",
+  url "https://github.com/argoproj-labs/argocd-vault-plugin.git",
       tag:      "v1.11.0",
       revision: "4133295001e037b917a8884da84af42f12a51cae"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/argocd-vault-plugin"
-    sha256 cellar: :any_skip_relocation, mojave: "8913663ce63800efd24b419b70375ac8b2fd202162b2c5ba75e7c62aeb0192e1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "785a1c0dd502e80424501f4a92d3d8954bbcea3765561ebfac37c2a169dfc714"
   end
 
   depends_on "go" => :build
@@ -18,9 +19,9 @@ class ArgocdVaultPlugin < Formula
 
     ldflags = %W[
       -s -w
-      -X github.com/IBM/argocd-vault-plugin/version.Version=#{version}
-      -X github.com/IBM/argocd-vault-plugin/version.BuildDate=#{time.iso8601}
-      -X github.com/IBM/argocd-vault-plugin/version.CommitSHA=#{Utils.git_head}
+      -X github.com/argoproj-labs/argocd-vault-plugin/version.Version=#{version}
+      -X github.com/argoproj-labs/argocd-vault-plugin/version.BuildDate=#{time.iso8601}
+      -X github.com/argoproj-labs/argocd-vault-plugin/version.CommitSHA=#{Utils.git_head}
     ]
 
     system "go", "build", *std_go_args(ldflags: ldflags)
