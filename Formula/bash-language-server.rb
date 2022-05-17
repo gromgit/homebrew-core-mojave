c611ed39ef751ce4a7aac974a251b792c7c836a2
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-17 13:20:27 +0800

    bash-language-server: update 3.0.0 bottle.

--- Formula/bash-language-server.rb
+++ Formula/bash-language-server.rb
@@ -3,13 +3,13 @@ require "language/node"
 class BashLanguageServer < Formula
   desc "Language Server for Bash"
   homepage "https://github.com/bash-lsp/bash-language-server"
-  url "https://registry.npmjs.org/bash-language-server/-/bash-language-server-2.1.0.tgz"
-  sha256 "4efd293f40cc9238928fd9ed08e9348ee2b5d5b8e5b45c7c204de046273f59b0"
+  url "https://registry.npmjs.org/bash-language-server/-/bash-language-server-3.0.0.tgz"
+  sha256 "23a2a0f8b62a2192f001ab0811051b085c11de6203b8358417ade21c0083be7e"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bash-language-server"
-    sha256 cellar: :any_skip_relocation, mojave: "2cadb290740105a86dabb3f2b0762c0b77aff78a1c747bb4bb511ca8fb8c9c97"
+    sha256 cellar: :any_skip_relocation, mojave: "e25124c67989815a516b9da4ee78227eb0ab046eaf5c61f3db16f631c1c446a0"
   end
 
   depends_on "node"
