84c0daf4f9ade91f86eb18284823b0dcc2e5e988
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-18 07:29:36 +0800

    docker-gen: update 0.9.0 bottle.

--- Formula/docker-gen.rb
+++ Formula/docker-gen.rb
@@ -1,13 +1,15 @@
 class DockerGen < Formula
   desc "Generate files from docker container metadata"
-  homepage "https://github.com/jwilder/docker-gen"
-  url "https://github.com/jwilder/docker-gen/archive/0.9.0.tar.gz"
+  homepage "https://github.com/nginx-proxy/docker-gen"
+  url "https://github.com/nginx-proxy/docker-gen/archive/0.9.0.tar.gz"
   sha256 "9f270363d872e4d302b67b3baa3baec4d1c7b892814fd6a50e5953a2b90d745e"
   license "MIT"
+  head "https://github.com/nginx-proxy/docker-gen.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/docker-gen"
-    sha256 cellar: :any_skip_relocation, mojave: "94c4f50f7a5fce5f42e3a96cde63260e838780615808c45b3662a7a018753be3"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "6ac4e4584868ef728784076e042c9bdfa1f2f7b1334526c77982e55a7a4b0e2d"
   end
 
   depends_on "go" => :build
