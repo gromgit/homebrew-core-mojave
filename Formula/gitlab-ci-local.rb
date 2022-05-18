14569569c2ecfb8c1600cd24cff31e53d49387f0
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-18 19:44:11 +0800

    gitlab-ci-local: update 4.31.0 bottle.

--- Formula/gitlab-ci-local.rb
+++ Formula/gitlab-ci-local.rb
@@ -3,14 +3,14 @@ require "language/node"
 class GitlabCiLocal < Formula
   desc "Run gitlab pipelines locally as shell executor or docker executor"
   homepage "https://github.com/firecow/gitlab-ci-local"
-  url "https://registry.npmjs.org/gitlab-ci-local/-/gitlab-ci-local-4.30.1.tgz"
-  sha256 "43bbead096b80273183da39736bc5e98cf2b15ce7e5f548dd0faef7d53dcc43c"
+  url "https://registry.npmjs.org/gitlab-ci-local/-/gitlab-ci-local-4.31.0.tgz"
+  sha256 "2444a3123b32e4ae0b81076008e12f2e182c0afe117823fe83e4a43cb77dca67"
   license "MIT"
   head "https://github.com/firecow/gitlab-ci-local.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gitlab-ci-local"
-    sha256 cellar: :any_skip_relocation, mojave: "7a6efd6482597feae59608834a126c3ed6cf6fdee422bb631acad531348f1823"
+    sha256 cellar: :any_skip_relocation, mojave: "2e3fc7ca05372a4a1fafb47c8d6ea79540a672d2714265fab64674da354bd83c"
   end
 
   depends_on "node"
