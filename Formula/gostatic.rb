c3c73350267404146be1b6c5a1f7f10d32798f41
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-18 22:41:17 +0800

    gostatic: update 2.34 bottle.

--- Formula/gostatic.rb
+++ Formula/gostatic.rb
@@ -1,15 +1,14 @@
 class Gostatic < Formula
   desc "Fast static site generator"
   homepage "https://github.com/piranha/gostatic"
-  url "https://github.com/piranha/gostatic/archive/2.32.tar.gz"
-  sha256 "857de1667660e71f890de019a230ce6c0ab5fdb2420511c4cf74d5f73a5a224a"
+  url "https://github.com/piranha/gostatic/archive/2.34.tar.gz"
+  sha256 "fc61c77e76e81b17b9955d18341da3f62e2f8def6a8f33156681e046ab1e80c5"
   license "ISC"
   head "https://github.com/piranha/gostatic.git", branch: "master"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gostatic"
-    rebuild 3
-    sha256 cellar: :any_skip_relocation, mojave: "f2af527d2cdf5d8015d4cfc3a04aad8fd01b8b4b37d2e83b7ad5fa74aa0bb890"
+    sha256 cellar: :any_skip_relocation, mojave: "82a5e0ba41c7aed79bde7094188fa925f0c1ca84c1c4faacc580e7b64c691a1d"
   end
 
   depends_on "go" => :build
