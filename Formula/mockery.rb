b629b94de708e8650d509a3b380eb6b6cea7a45b
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-18 09:04:22 +0800

    mockery: update 2.12.2 bottle.

--- Formula/mockery.rb
+++ Formula/mockery.rb
@@ -1,14 +1,14 @@
 class Mockery < Formula
   desc "Mock code autogenerator for Golang"
   homepage "https://github.com/vektra/mockery"
-  url "https://github.com/vektra/mockery/archive/v2.12.1.tar.gz"
-  sha256 "62b2652fb245372815cdce61511dcb91d83ab6944774ef8e90bab7b30b26660c"
+  url "https://github.com/vektra/mockery/archive/v2.12.2.tar.gz"
+  sha256 "f5962e4506dd749b9bcc2785c1e5ac683bddac5ba0219382677365d558d2080c"
   license "BSD-3-Clause"
   head "https://github.com/vektra/mockery.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mockery"
-    sha256 cellar: :any_skip_relocation, mojave: "90f582b47739b66bb712be5327d289c84653242a82ac787235fa8dcccf110888"
+    sha256 cellar: :any_skip_relocation, mojave: "cfe091acb5791aeebfb65248dba650b9e8ccc628192400642c021afee0467f92"
   end
 
   depends_on "go" => :build
