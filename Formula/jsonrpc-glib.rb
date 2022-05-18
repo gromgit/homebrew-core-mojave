30cb892a1a003a0e33bb62284634f9bcce4b8d4c
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-19 07:15:52 +0800

    jsonrpc-glib: update 3.42.0 bottle.

--- Formula/jsonrpc-glib.rb
+++ Formula/jsonrpc-glib.rb
@@ -1,18 +1,13 @@
 class JsonrpcGlib < Formula
   desc "GNOME library to communicate with JSON-RPC based peers"
   homepage "https://gitlab.gnome.org/GNOME/jsonrpc-glib"
-  url "https://download.gnome.org/sources/jsonrpc-glib/3.40/jsonrpc-glib-3.40.0.tar.xz"
-  sha256 "c2e3d16257c7266cd3901884e22375575bf61a8e1f67596c88e0d87ae70d0ef4"
+  url "https://download.gnome.org/sources/jsonrpc-glib/3.42/jsonrpc-glib-3.42.0.tar.xz"
+  sha256 "221989a57ca82a12467dc427822cd7651b0cad038140c931027bf1074208276b"
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "b840c82daeec35544e1eddb0481a5a6300f867e853859b557c2cb3f0d4e19ad3"
-    sha256 cellar: :any,                 arm64_big_sur:  "69e3e97a9a4193dd6f3a23b11166ea27cb99760931244f6a391a228bc4eca196"
-    sha256 cellar: :any,                 monterey:       "f25b784b62fa80160800aad053d3b1bf13267135cc0cf56908fa0142075580e2"
-    sha256 cellar: :any,                 big_sur:        "ec54de6bc6799ae0b497447de9d3dfe5b4f88ce71d3a826da43785affae68429"
-    sha256 cellar: :any,                 catalina:       "ea6e41d579c3e4d3dc33355b3de46277d6605822a78f2f395e329329155a6bf7"
-    sha256 cellar: :any,                 mojave:         "ae9d2d930e0b55dcd99e9ed1188d5a4a7adf4523851feb1fb1a01ff15d9de575"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "50fbcd7be2fa9c83aa93fd742e8c678998494c03c27223d47fafb50fbe0f1fe0"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jsonrpc-glib"
+    sha256 cellar: :any, mojave: "1064f8882d1b375c7f3d953ab9ada40e3a6bc1c72817dc72aca9e33de00465b4"
   end
 
   depends_on "gobject-introspection" => :build
