5c9a9710a3d0a8df6b5fedeeff833392b27d5eac
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-19 00:17:23 +0800

    gsoap: update 2.8.122 bottle.

--- Formula/gsoap.rb
+++ Formula/gsoap.rb
@@ -1,8 +1,8 @@
 class Gsoap < Formula
   desc "SOAP stub and skeleton compiler for C and C++"
   homepage "https://www.genivia.com/products.html"
-  url "https://downloads.sourceforge.net/project/gsoap2/gsoap_2.8.121.zip"
-  sha256 "d5a66b9d5189143a6adba757a085f84d3c31c03b2948939cf99851003a2934a8"
+  url "https://downloads.sourceforge.net/project/gsoap2/gsoap_2.8.122.zip"
+  sha256 "3eb8486c986b44071bb5f19ef7b990791f819bb267c0bf640e95bd991070fad8"
   license any_of: ["GPL-2.0-or-later", "gSOAP-1.3b"]
 
   livecheck do
@@ -12,7 +12,7 @@ class Gsoap < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gsoap"
-    sha256 mojave: "dfabdf717dc4a7336b9dd47eac1a65222191bc8f2f6fcb2ed4d756547df93d3a"
+    sha256 mojave: "0b587c211d8c538bc5fd877dea5893bc45049750651d8652c610d8836f8bacfc"
   end
 
   depends_on "autoconf" => :build
