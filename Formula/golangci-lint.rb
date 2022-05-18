2631f9aa3191a88cf67866cb67afd3bd4db31f7b
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-18 22:28:06 +0800

    golangci-lint: update 1.46.1 bottle.

--- Formula/golangci-lint.rb
+++ Formula/golangci-lint.rb
@@ -2,14 +2,14 @@ class GolangciLint < Formula
   desc "Fast linters runner for Go"
   homepage "https://golangci-lint.run/"
   url "https://github.com/golangci/golangci-lint.git",
-      tag:      "v1.45.2",
-      revision: "8bdc4d3f8044b1a20e10a9f519b5f738e8188877"
+      tag:      "v1.46.1",
+      revision: "044f0a170269dc3d108d65cb520b34232bedde4a"
   license "GPL-3.0-only"
   head "https://github.com/golangci/golangci-lint.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/golangci-lint"
-    sha256 cellar: :any_skip_relocation, mojave: "3e4781a803bdf49b009babd69510a418e43334e521e7e33d99b29906072d1c84"
+    sha256 cellar: :any_skip_relocation, mojave: "23a1ffa801025cde66e1cebc48199ca2376117fcb8259757182058043c7ad3aa"
   end
 
   depends_on "go"
