90e242dab10e305af0ec6a3d19b97adde8109ca4
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-18 09:36:12 +0800

    epinio: update 0.8.0 bottle.

--- Formula/epinio.rb
+++ Formula/epinio.rb
@@ -1,16 +1,16 @@
 class Epinio < Formula
   desc "CLI for Epinio, the Application Development Engine for Kubernetes"
   homepage "https://epinio.io/"
-  url "https://github.com/epinio/epinio/archive/refs/tags/v0.7.1.tar.gz"
-  sha256 "3216304faef81e7668cf55f98c39ca5aef65bc3bf7a34dc8e3a9e582ba472e49"
+  url "https://github.com/epinio/epinio/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "25eeb7ac7f12fcb9f20845d8f340ab74dad24e467761f9658aaa3f864319c036"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/epinio"
-    sha256 cellar: :any_skip_relocation, mojave: "68cef370f1a8949970e88b071e4c62cd323aa66a85a555cafcc4d783cf0c393a"
+    sha256 cellar: :any_skip_relocation, mojave: "b63613af29df308b2dc8d55e9dc3bf94da6eb9cbb258cfc8da44f2669f569494"
   end
 
-  depends_on "go@1.17" => :build
+  depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/epinio/epinio/internal/version.Version=#{version}")
@@ -20,7 +20,7 @@ class Epinio < Formula
     output = shell_output("#{bin}/epinio version 2>&1")
     assert_match "Epinio Version: #{version}", output
 
-    output = shell_output("#{bin}/epinio settings update 2>&1")
+    output = shell_output("#{bin}/epinio settings update 2>&1", 255)
     assert_match "failed to get kube config", output
     assert_match "no configuration has been provided", output
   end
