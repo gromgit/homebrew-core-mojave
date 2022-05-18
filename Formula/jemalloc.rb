422f073bc118c1014aa6fd29b344dcdfbdf04fc6
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-19 06:47:57 +0800

    jemalloc: update 5.3.0 bottle.

--- Formula/jemalloc.rb
+++ Formula/jemalloc.rb
@@ -1,20 +1,13 @@
 class Jemalloc < Formula
   desc "Implementation of malloc emphasizing fragmentation avoidance"
   homepage "http://jemalloc.net/"
-  url "https://github.com/jemalloc/jemalloc/releases/download/5.2.1/jemalloc-5.2.1.tar.bz2"
-  sha256 "34330e5ce276099e2e8950d9335db5a875689a4c6a56751ef3b1d8c537f887f6"
+  url "https://github.com/jemalloc/jemalloc/releases/download/5.3.0/jemalloc-5.3.0.tar.bz2"
+  sha256 "2db82d1e7119df3e71b7640219b6dfe84789bc0537983c3b7ac4f7189aecfeaa"
   license "BSD-2-Clause"
-  revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "ba1a05f055527c490d4ba10aa4215b70d515bf70a8f593eb5bbcbc7a3c0bf48a"
-    sha256 cellar: :any,                 arm64_big_sur:  "724ab5947e53f571b9fed9e776a1ba22b1d71fe27ce5775553d70e990ef9dc63"
-    sha256 cellar: :any,                 monterey:       "4fd427b1fce4dfd4dbbd352faf5255517ca8d763cd9cd82541862dbc14cced57"
-    sha256 cellar: :any,                 big_sur:        "7797788be2da677a8343ac6199e2f180c2e6b627c0b9abc9da133fbc34e86678"
-    sha256 cellar: :any,                 catalina:       "b1b211e5bead798c236d478dd74310a97a7b59470f607b608c07222648b08bf5"
-    sha256 cellar: :any,                 mojave:         "d3f6f85e74b08c8c97448e289734df484f884af35cd10ce9d9db43cf721fbf94"
-    sha256 cellar: :any,                 high_sierra:    "8080c98844153da08346431fe0a0592f6f718cb7a17525f9ffb909c395bc0b6d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0a268dd9bb31bc2ff18b55eddf0faedd0cad6eb0e2e9bea0b47a525f2afd9697"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jemalloc"
+    sha256 cellar: :any, mojave: "c66784edd0335e74eb17e3052d74f4cdd4d505f162c7b3372842c92e0e742f67"
   end
 
   head do
@@ -22,14 +15,6 @@ class Jemalloc < Formula
 
     depends_on "autoconf" => :build
     depends_on "docbook-xsl" => :build
-  end
-
-  # Fixes an issue where jemalloc's types conflict with the system
-  # types, preventing their use. Merged upstream.
-  # https://github.com/jemalloc/jemalloc/commit/3b4a03b92b2e415415a08f0150fdb9eeb659cd52
-  patch do
-    url "https://github.com/Homebrew/formula-patches/raw/d3d5ad2b5683c1a435a185eec9c593749c7ca41a/jemalloc/fix_nothrow_type.patch"
-    sha256 "d79f5c8767695059ff541f291db3fbc57c9b67299dc129848dd365c2f51b214a"
   end
 
   def install
