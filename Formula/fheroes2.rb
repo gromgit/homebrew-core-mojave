8896e6013aa8cccc40830de46d83d028d0352a34
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-18 13:26:06 +0800

    fheroes2: update 0.9.15 bottle.

--- Formula/fheroes2.rb
+++ Formula/fheroes2.rb
@@ -1,8 +1,8 @@
 class Fheroes2 < Formula
   desc "Free Heroes of Might and Magic II is a recreation of HoMM2 game engine"
   homepage "https://ihhub.github.io/fheroes2/"
-  url "https://github.com/ihhub/fheroes2/archive/0.9.14.tar.gz"
-  sha256 "5532fa8443b8d8f54fcfc56628f1020470ad215e12285d78c8450e49ea9944be"
+  url "https://github.com/ihhub/fheroes2/archive/0.9.15.tar.gz"
+  sha256 "5325cbb6fe96ff80a27584d9fd3ea1d572eb6e21f863fbc72d0d245752b5444e"
   license "GPL-2.0-or-later"
   head "https://github.com/ihhub/fheroes2.git", branch: "master"
 
@@ -13,7 +13,7 @@ class Fheroes2 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fheroes2"
-    sha256 mojave: "f67fcffd3eec1d0bff1879779b72e3aed4fd4c8c01c68c1b81e5dac018b11f8c"
+    sha256 mojave: "84efa647d708c7ec5c4b605ba6fb6327bccb959126b7ccd5c4243f47b4f23a87"
   end
 
   depends_on "cmake" => :build
@@ -26,15 +26,16 @@ class Fheroes2 < Formula
 
   uses_from_macos "zlib"
 
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
+
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
-    system "make", "-C", "files/lang"
-
-    (pkgshare/"files/lang").install Dir["files/lang/*.mo"]
-    pkgshare.install "fheroes2.key"
-    bin.install "script/demo/download_demo_version.sh" => "fheroes2-install-demo"
   end
 
   test do
