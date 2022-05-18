84bbd644b696467475156a9d2256ca05efdc3db8
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-18 19:04:07 +0800

    ghq: update 1.3.0 bottle.

--- Formula/ghq.rb
+++ Formula/ghq.rb
@@ -2,15 +2,14 @@ class Ghq < Formula
   desc "Remote repository management made easy"
   homepage "https://github.com/x-motemen/ghq"
   url "https://github.com/x-motemen/ghq.git",
-      tag:      "v1.2.1",
-      revision: "dd139fb46cb7c1a3b19bca7a0c3762090c7c522f"
+      tag:      "v1.3.0",
+      revision: "ccbbc18efc1802a5f9bf50d9fbb6b8b020c3f8f7"
   license "MIT"
   head "https://github.com/x-motemen/ghq.git", branch: "master"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ghq"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "70a3ba0cf9f4bb8660e956b79c636af8e81a88289708aba3639d79651c2607ff"
+    sha256 cellar: :any_skip_relocation, mojave: "61fef32a6dedbe040444b313d89b44ac0cac5813e70f5e0b97324f13c638f3c4"
   end
 
   depends_on "go" => :build
@@ -20,7 +19,6 @@ bottle do
     bin.install "ghq"
     bash_completion.install "misc/bash/_ghq" => "ghq"
     zsh_completion.install "misc/zsh/_ghq"
-    prefix.install_metafiles
   end
 
   test do
