28c9526e057f1e011630246a9521edd62078af70
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-19 08:24:39 +0800

    kore: update 4.2.2_1 bottle.

--- Formula/kore.rb
+++ Formula/kore.rb
@@ -4,6 +4,7 @@ class Kore < Formula
   url "https://kore.io/releases/kore-4.2.2.tar.gz"
   sha256 "77c12d80bb76fe774b16996e6bac6d4ad950070d0816c3409dc0397dfc62725f"
   license "ISC"
+  revision 1
   head "https://github.com/jorisvink/kore.git", branch: "master"
 
   livecheck do
@@ -13,7 +14,7 @@ class Kore < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kore"
-    sha256 mojave: "1f62f3808314786b0dacbb7da58f7ec0124c3c8649e0d042e03e5354c5446071"
+    sha256 mojave: "b364e4e2a9e8a589ce0ede096c6ad84f585fa23f3e8e9f998b7867f4dcb8895b"
   end
 
   depends_on "pkg-config" => :build
