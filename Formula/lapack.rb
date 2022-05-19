3809733a519421ffe4f47b757347ae85e31154cb
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-19 09:16:21 +0800

    lapack: update 3.10.1 bottle.

--- Formula/lapack.rb
+++ Formula/lapack.rb
@@ -1,15 +1,14 @@
 class Lapack < Formula
   desc "Linear Algebra PACKage"
   homepage "https://www.netlib.org/lapack/"
-  url "https://github.com/Reference-LAPACK/lapack/archive/v3.10.0.tar.gz"
-  sha256 "328c1bea493a32cac5257d84157dc686cc3ab0b004e2bea22044e0a59f6f8a19"
+  url "https://github.com/Reference-LAPACK/lapack/archive/v3.10.1.tar.gz"
+  sha256 "cd005cd021f144d7d5f7f33c943942db9f03a28d110d6a3b80d718a295f7f714"
   license "BSD-3-Clause"
   head "https://github.com/Reference-LAPACK/lapack.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lapack"
-    rebuild 1
-    sha256 cellar: :any, mojave: "df1503dc5babefd877177bc4af3af06c57f44e6bbe7585d81ffbf47ea69df799"
+    sha256 cellar: :any, mojave: "2dfed4bebcef58cc8bee402219008a9b044ac1b8608b8824f44a394d9149d99d"
   end
 
   keg_only :shadowed_by_macos, "macOS provides LAPACK in Accelerate.framework"
