5f19565242e9f39f32dd872d0e820a3f01648a48
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 07:34:45 +0800

    postgresql: update 14.3 bottle.

--- Formula/postgresql.rb
+++ Formula/postgresql.rb
@@ -1,10 +1,9 @@
 class Postgresql < Formula
   desc "Object-relational database system"
   homepage "https://www.postgresql.org/"
-  url "https://ftp.postgresql.org/pub/source/v14.2/postgresql-14.2.tar.bz2"
-  sha256 "2cf78b2e468912f8101d695db5340cf313c2e9f68a612fb71427524e8c9a977a"
+  url "https://ftp.postgresql.org/pub/source/v14.3/postgresql-14.3.tar.bz2"
+  sha256 "279057368bf59a919c05ada8f95c5e04abb43e74b9a2a69c3d46a20e07a9af38"
   license "PostgreSQL"
-  revision 1
   head "https://github.com/postgres/postgres.git", branch: "master"
 
   livecheck do
@@ -14,7 +13,7 @@ class Postgresql < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/postgresql"
-    sha256 mojave: "3f4fe5b481869af48f2ea638c5ff8e1f5f6777630d0fd8a62af9abae15799678"
+    sha256 mojave: "d85caa1e2f0ba78d9077080ca750b7af95a72ea00845ede7e1fec5ac13ffc8c4"
   end
 
   depends_on "pkg-config" => :build
