a36ef85c4ed1e1621cdd7e9668b53c72401fbc5b
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-19 22:38:51 +0800

    neomutt: update 20220429 bottle.

--- Formula/neomutt.rb
+++ Formula/neomutt.rb
@@ -1,15 +1,14 @@
 class Neomutt < Formula
   desc "E-mail reader with support for Notmuch, NNTP and much more"
   homepage "https://neomutt.org/"
-  url "https://github.com/neomutt/neomutt/archive/20211029.tar.gz"
-  sha256 "08245cfa7aec80b895771fd1adcbb7b86e9c0434dfa64574e3c8c4d692aaa078"
+  url "https://github.com/neomutt/neomutt/archive/20220429.tar.gz"
+  sha256 "45496542897ba8de6bc7cce3f5951d9033ed1c49e5d6f1353adaeefe795d9043"
   license "GPL-2.0-or-later"
-  head "https://github.com/neomutt/neomutt.git", branch: "master"
+  head "https://github.com/neomutt/neomutt.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/neomutt"
-    rebuild 2
-    sha256 mojave: "93ed396c2c62b4345fbab12c64596f84d9b14be0161c69f4571e0427ea886172"
+    sha256 mojave: "88c3c91b834bfd978c09c162176bb585313bdb0a7e08089d75302364f762e985"
   end
 
   depends_on "docbook-xsl" => :build
@@ -18,6 +17,7 @@ class Neomutt < Formula
   depends_on "libidn2"
   depends_on "lmdb"
   depends_on "lua"
+  depends_on "ncurses"
   depends_on "notmuch"
   depends_on "openssl@1.1"
   depends_on "tokyo-cabinet"
@@ -25,7 +25,6 @@ class Neomutt < Formula
   uses_from_macos "libxslt" => :build # for xsltproc
   uses_from_macos "cyrus-sasl"
   uses_from_macos "krb5"
-  uses_from_macos "ncurses"
   uses_from_macos "zlib"
 
   on_linux do
