794e9093b966d5c7fe4f4b01c79e877ec50bd279
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 03:15:59 +0800

    opencsg: update 1.5.1 bottle.

--- Formula/opencsg.rb
+++ Formula/opencsg.rb
@@ -1,9 +1,9 @@
 class Opencsg < Formula
   desc "Constructive solid geometry rendering library"
   homepage "http://www.opencsg.org"
-  url "http://www.opencsg.org/OpenCSG-1.4.2.tar.gz"
-  sha256 "d952ec5d3a2e46a30019c210963fcddff66813efc9c29603b72f9553adff4afb"
-  revision 3
+  url "http://www.opencsg.org/OpenCSG-1.5.1.tar.gz"
+  sha256 "7adb7ec7650d803d9cb54d06572fb5ba5aca8f23e6ccb75b73c17756a9ab46e3"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url :homepage
@@ -11,13 +11,8 @@ class Opencsg < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "d1bd35e73ad29214d8a5de83a4e0b80385705dbeecce9ed1132afc9eb6c71a61"
-    sha256 cellar: :any,                 arm64_big_sur:  "c06e0c8e9ceee5ad621e6f650a289f34f30428bce43f6a9efb95621fc7afdafb"
-    sha256 cellar: :any,                 monterey:       "f02b160d94f4eb0bbc6fe4381368e8c3151b6b3b1cb20f650121f186f8517b25"
-    sha256 cellar: :any,                 big_sur:        "f7e6296d4466eea7c516fdca9e382d30fad4194b73969a1158d3d399b59c9381"
-    sha256 cellar: :any,                 catalina:       "730e0c7b2656e63ac4c55effbb5030fb737bfdc6ecbda700ed37534ae8b0d295"
-    sha256 cellar: :any,                 mojave:         "7d8c19c4b5c1d26d5f15fb01094977fd58f14ccaa92085a13be68fd27943588a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "136c554f3d6e8c3af9f7b1dfd4bada577ed629573622e0ffba09379d88de867d"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/opencsg"
+    sha256 cellar: :any, mojave: "f80fbf3ebee825ef8e59710da90104131169a0974cc5760a1799169e7b1b7229"
   end
 
   depends_on "qt@5" => :build
