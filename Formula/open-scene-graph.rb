b93d366b8c0c9e3965cbe8040f56ae37034c4f1b
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 02:52:31 +0800

    open-scene-graph: update 3.6.5_2 bottle.

--- Formula/open-scene-graph.rb
+++ Formula/open-scene-graph.rb
@@ -2,7 +2,7 @@ class OpenSceneGraph < Formula
   desc "3D graphics toolkit"
   homepage "https://github.com/openscenegraph/OpenSceneGraph"
   license "LGPL-2.1-or-later" => { with: "WxWindows-exception-3.1" }
-  revision 1
+  revision 2
   head "https://github.com/openscenegraph/OpenSceneGraph.git", branch: "master"
 
   stable do
@@ -17,24 +17,24 @@ class OpenSceneGraph < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "83350482064d3e55281b5c4a808f4629ce0c243a49fb57e68e5f63d2d5a411c4"
-    sha256 big_sur:       "77b57e3edeb952002a4c43c90af2c2ada2813bb35d45b24a07720da89fa389cf"
-    sha256 catalina:      "dfa6322ce7e63ce9194a42d3dc1d630572ff7d818ac21e3533017a0bcf5821b6"
-    sha256 mojave:        "e347cc9ef89cd9b1e8fea9a6c14a4693f30cd2b43ab4754c61724e229c62849c"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/open-scene-graph"
+    sha256 mojave: "019da331e0bce5f738c4aad2c85b0c7d91d3e09a874c7940afd9fd498518d37b"
   end
 
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
   depends_on "graphviz" => :build
   depends_on "pkg-config" => :build
+  depends_on "fontconfig"
   depends_on "freetype"
-  depends_on "gtkglext"
   depends_on "jpeg-turbo"
   depends_on "sdl2"
 
-  # patch necessary to ensure support for gtkglext-quartz
-  # filed as an issue to the developers https://github.com/openscenegraph/OpenSceneGraph/issues/34
-  patch :DATA
+  on_linux do
+    depends_on "librsvg"
+    depends_on "mesa"
+    depends_on "mesa-glu"
+  end
 
   def install
     # Fix "fatal error: 'os/availability.h' file not found" on 10.11 and
@@ -51,10 +51,15 @@ class OpenSceneGraph < Formula
       -DCMAKE_DISABLE_FIND_PACKAGE_SDL=ON
       -DCMAKE_DISABLE_FIND_PACKAGE_TIFF=ON
       -DCMAKE_CXX_FLAGS=-Wno-error=narrowing
-      -DCMAKE_OSX_ARCHITECTURES=x86_64
-      -DOSG_DEFAULT_IMAGE_PLUGIN_FOR_OSX=imageio
-      -DOSG_WINDOWING_SYSTEM=Cocoa
     ]
+
+    if OS.mac?
+      args += %w[
+        -DCMAKE_OSX_ARCHITECTURES=x86_64
+        -DOSG_DEFAULT_IMAGE_PLUGIN_FOR_OSX=imageio
+        -DOSG_WINDOWING_SYSTEM=Cocoa
+      ]
+    end
 
     mkdir "build" do
       system "cmake", "..", *args
@@ -80,18 +85,3 @@ class OpenSceneGraph < Formula
     assert_equal `./test`.chomp, version.to_s
   end
 end
-
-__END__
-diff --git a/CMakeModules/FindGtkGl.cmake b/CMakeModules/FindGtkGl.cmake
-index 321cede..6497589 100644
---- a/CMakeModules/FindGtkGl.cmake
-+++ b/CMakeModules/FindGtkGl.cmake
-@@ -10,7 +10,7 @@ IF(PKG_CONFIG_FOUND)
-     IF(WIN32)
-         PKG_CHECK_MODULES(GTKGL gtkglext-win32-1.0)
-     ELSE()
--        PKG_CHECK_MODULES(GTKGL gtkglext-x11-1.0)
-+        PKG_CHECK_MODULES(GTKGL gtkglext-quartz-1.0)
-     ENDIF()
-
- ENDIF()
