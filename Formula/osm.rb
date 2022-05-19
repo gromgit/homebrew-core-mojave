f943d7f76bf5514a2b4e6a0226642b9007d7a30c
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 05:10:07 +0800

    osm: update 1.1.1 bottle.

--- Formula/osm.rb
+++ Formula/osm.rb
@@ -2,8 +2,8 @@ class Osm < Formula
   desc "Open Service Mesh (OSM)"
   homepage "https://openservicemesh.io/"
   url "https://github.com/openservicemesh/osm.git",
-      tag:      "v1.1.0",
-      revision: "a23afae930033c22438008c1f730f13c7408d951"
+      tag:      "v1.1.1",
+      revision: "407bbedd5edb6ff9f1f51a4cabb95bedeb567312"
   license "Apache-2.0"
   head "https://github.com/openservicemesh/osm.git", branch: "main"
 
@@ -14,7 +14,7 @@ class Osm < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/osm"
-    sha256 cellar: :any_skip_relocation, mojave: "5675487322c05c54d2d1421061387b260785cbea140331edce451ec9d0698571"
+    sha256 cellar: :any_skip_relocation, mojave: "dc1fec099bb4413abc4d003f000724951b0850c8439fa97551376cdbf3f577e7"
   end
 
   depends_on "go" => :build
