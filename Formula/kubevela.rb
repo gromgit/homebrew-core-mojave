58090197be5dc8c657e674d7c1fa6860dcdadb35
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-19 08:40:40 +0800

    kubevela: update 1.3.3 bottle.

--- Formula/kubevela.rb
+++ Formula/kubevela.rb
@@ -1,14 +1,16 @@
 class Kubevela < Formula
   desc "Application Platform based on Kubernetes and Open Application Model"
   homepage "https://kubevela.io"
-  url "https://github.com/oam-dev/kubevela.git",
+  url "https://github.com/kubevela/kubevela.git",
       tag:      "v1.3.3",
       revision: "45e1de19dc736b10b3eb2d908d809210f470b24f"
   license "Apache-2.0"
+  head "https://github.com/kubevela/kubevela.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kubevela"
-    sha256 cellar: :any_skip_relocation, mojave: "bbcca5ed1c5aeaec3453b9dd39ef8cf95467b3db64671bdb58062d4489869fd8"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "2ac0e37c13468344b7b342fb48f1133ca21bba8a8f962276b2ed0ccf34c2c628"
   end
 
   depends_on "go" => :build
@@ -17,8 +19,8 @@ class Kubevela < Formula
     ENV["CGO_ENABLED"] = "0"
     ldflags = %W[
       -s -w
-      -X github.com/oam-dev/kubevela/version.VelaVersion=#{version}
-      -X github.com/oam-dev/kubevela/version.GitRevision=#{Utils.git_head}
+      -X github.com/kubevela/kubevela/version.VelaVersion=#{version}
+      -X github.com/kubevela/kubevela/version.GitRevision=#{Utils.git_head}
     ]
 
     system "go", "build", *std_go_args(output: bin/"vela", ldflags: ldflags), "./references/cmd/cli"
