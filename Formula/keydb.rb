36cbb0bec587d7705657d567b0ec2ec5b800c27a
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-19 08:10:13 +0800

    keydb: update 6.3.0 bottle.

--- Formula/keydb.rb
+++ Formula/keydb.rb
@@ -1,15 +1,16 @@
 class Keydb < Formula
   desc "Multithreaded fork of Redis"
   homepage "https://keydb.dev"
-  url "https://github.com/EQ-Alpha/KeyDB/archive/v6.2.2.tar.gz"
-  sha256 "e65eea13500c30c65f705121b67cffeb3551a1c7cc7d07b60fe6191acd4dec58"
+  url "https://github.com/Snapchat/KeyDB/archive/v6.3.0.tar.gz"
+  sha256 "58793c1ed2f0afc81582a6216844ef9e9b1b4d3ceb8a9bbda5b34ed1b1e17e0c"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/keydb"
-    sha256 cellar: :any_skip_relocation, mojave: "b04a717ae1556fd4ae7373d9fcec99fc25c9380355fa2c0090ebfa6bd24e17b6"
+    sha256 cellar: :any, mojave: "00db732186d6e29a0b0066bca790345c38c91a79c9446b38d393dfa38c3742af"
   end
 
+  depends_on "openssl@3"
   uses_from_macos "curl"
 
   on_linux do
