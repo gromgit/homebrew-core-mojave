202a755249a07a76473e57129e73301f11ba14f6
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-21 02:08:06 +0800

    widelands: update 1.0_1 bottle.

--- Formula/widelands.rb
+++ Formula/widelands.rb
@@ -3,6 +3,7 @@ class Widelands < Formula
   homepage "https://www.widelands.org/"
   url "https://github.com/widelands/widelands/archive/v1.0.tar.gz"
   sha256 "1dab0c4062873cc72c5e0558f9e9620b0ef185f1a78923a77c4ce5b9ed76031a"
+  revision 1
   version_scheme 1
 
   livecheck do
@@ -11,13 +12,8 @@ class Widelands < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "2f992a280442581a1b0e5c7b766c9ef28e26f5423774d2939798c710173c5a23"
-    sha256 arm64_big_sur:  "8f0377b940f20c79b0da12f8b5ae72d95424456b76c01d9b2f8f3032eae2b529"
-    sha256 monterey:       "516c380cd46bf5756bd737942507a118badb316e782500d36c69d2deca8449c5"
-    sha256 big_sur:        "443b39115903be7bd40d1f2353197a062a1210c42ff93f446ffc448a3ea5a183"
-    sha256 catalina:       "fa60c7429eda358d559ae1b1d5b0db456135c24bfc8ad35f8668f77c3e09cf51"
-    sha256 mojave:         "26663b82b323e4d087313ec8496f599d2aee39d5cb06c56da5576c257bca14b0"
-    sha256 x86_64_linux:   "3602f01c5e080a62ee4644abf8163b164082de937a5259b258020a67a3766248"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/widelands"
+    sha256 mojave: "ebcec1970ca4a185743f8e71ef24b5eb4ba15c9f73f8b301972fc081d2cc409f"
   end
 
   depends_on "cmake" => :build
@@ -34,6 +30,13 @@ class Widelands < Formula
   depends_on "sdl2_ttf"
 
   uses_from_macos "curl"
+
+  # Fix build with Boost 1.77+.
+  # Remove with the next release (1.1).
+  patch do
+    url "https://github.com/widelands/widelands/commit/316eaea209754368a57f445ea4dd016ecf8eded6.patch?full_index=1"
+    sha256 "358cae53bbc854e7e9248bdea0ca5af8bce51e188626a7f366bc6a87abd33dc9"
+  end
 
   def install
     ENV.cxx11
@@ -53,7 +56,7 @@ class Widelands < Formula
   end
 
   test do
-    on_linux do
+    if OS.linux?
       # Unable to start Widelands, because we were unable to add the home directory:
       # RealFSImpl::make_directory: No such file or directory: /tmp/widelands-test/.local/share/widelands
       mkdir_p ".local/share/widelands"
