13810f578fff7d63a9506f530b9eb039208c95db
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 11:53:50 +0800

    pyinvoke: update 1.7.1 bottle.

--- Formula/pyinvoke.rb
+++ Formula/pyinvoke.rb
@@ -3,14 +3,14 @@ class Pyinvoke < Formula
 
   desc "Pythonic task management & command execution"
   homepage "https://www.pyinvoke.org/"
-  url "https://files.pythonhosted.org/packages/55/a2/763dc56e746ca013061b26c95868cb7f832c2a87dc27ed749641a734957f/invoke-1.7.0.tar.gz"
-  sha256 "e332e49de40463f2016315f51df42313855772be86435686156bc18f45b5cc6c"
+  url "https://files.pythonhosted.org/packages/df/59/41b614b9d415929b4d72e3ee658bd088640e9a800e55663529a8237deae3/invoke-1.7.1.tar.gz"
+  sha256 "7b6deaf585eee0a848205d0b8c0014b9bf6f287a8eb798818a642dff1df14b19"
   license "BSD-2-Clause"
   head "https://github.com/pyinvoke/invoke.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pyinvoke"
-    sha256 cellar: :any_skip_relocation, mojave: "3574a1c728a3641db90d5f48b6419a9338a30f9fae086d274affd8612281ffd5"
+    sha256 cellar: :any_skip_relocation, mojave: "824d5bbe3056db0e176a10a5aa2ad8062f6efe77e492c1394aeca8d6c072f9ce"
   end
 
   depends_on "python@3.10"
