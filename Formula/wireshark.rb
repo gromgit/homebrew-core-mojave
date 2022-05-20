7c189d74d37fcdd2d4d8c08f8760ecb5a12695af
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-21 02:23:15 +0800

    wireshark: update 3.6.5 bottle.

--- Formula/wireshark.rb
+++ Formula/wireshark.rb
@@ -1,9 +1,9 @@
 class Wireshark < Formula
   desc "Graphical network analyzer and capture tool"
   homepage "https://www.wireshark.org"
-  url "https://www.wireshark.org/download/src/all-versions/wireshark-3.6.3.tar.xz"
-  mirror "https://1.eu.dl.wireshark.org/src/all-versions/wireshark-3.6.3.tar.xz"
-  sha256 "b60364a4c0068a10811ab3fd075ca6c1eb0e75d44600271b88a20ed93a2ef631"
+  url "https://www.wireshark.org/download/src/all-versions/wireshark-3.6.5.tar.xz"
+  mirror "https://1.eu.dl.wireshark.org/src/all-versions/wireshark-3.6.5.tar.xz"
+  sha256 "a2d741d60ff3516137d4b9f38cdc07eee54a56ec36046f4c3afee6bfb4f6eaa1"
   license "GPL-2.0-or-later"
   head "https://gitlab.com/wireshark/wireshark.git", branch: "master"
 
@@ -14,7 +14,7 @@ class Wireshark < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/wireshark"
-    sha256 mojave: "75913253b4bd12554cd4f8551cff19c7ddca34bcb584750a539ba5857529f8ed"
+    sha256 mojave: "eea8f1c64d3217a4b8c97544095ed303e399285fb489ee032c050217fae7bdf1"
   end
 
   depends_on "cmake" => :build
