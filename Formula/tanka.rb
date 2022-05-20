dad9b45426983ad7bf9868588f466c22a61eedb9
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 18:50:39 +0800

    tanka: update 0.21.0 bottle.

--- Formula/tanka.rb
+++ Formula/tanka.rb
@@ -2,8 +2,8 @@ class Tanka < Formula
   desc "Flexible, reusable and concise configuration for Kubernetes using Jsonnet"
   homepage "https://tanka.dev"
   url "https://github.com/grafana/tanka.git",
-      tag:      "v0.20.0",
-      revision: "82ab8cfcef4c16b2de068a7bd4e0f54fa6f6df82"
+      tag:      "v0.21.0",
+      revision: "68d625e128b2678996ff442c385f5361c7987491"
   license "Apache-2.0"
   head "https://github.com/grafana/tanka.git", branch: "main"
 
@@ -14,7 +14,7 @@ class Tanka < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tanka"
-    sha256 cellar: :any_skip_relocation, mojave: "e4ccec14c830786b15c42bb3f74e6e7e25a1224f8d8a2723a2bf71c26f3ac1aa"
+    sha256 cellar: :any_skip_relocation, mojave: "4ff0b70340a6c5d8b4239bfc6cd7f6b81712252cd047694a548c6450d19ab357"
   end
 
   depends_on "go" => :build
