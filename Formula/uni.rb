0a3210a1108cfce10c4e01d2b1e519c42db4c728
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 22:52:08 +0800

    uni: update 2.5.1 bottle.

--- Formula/uni.rb
+++ Formula/uni.rb
@@ -1,19 +1,19 @@
 class Uni < Formula
   desc "Unicode database query tool for the command-line"
   homepage "https://github.com/arp242/uni"
-  url "https://github.com/arp242/uni/archive/v2.5.0.tar.gz"
-  sha256 "27833125a4097e15ec6dbce33e2798a1689a4674c0d0509c0dc1039204aa1d80"
+  url "https://github.com/arp242/uni/archive/v2.5.1.tar.gz"
+  sha256 "806fbba66efaa45cd5691efcd8457ba8fe88d3b2f6fd0b027f1e6ef62253d6fb"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/uni"
-    sha256 cellar: :any_skip_relocation, mojave: "7d179cf83ea1f3b53ff36bacf760c7f43135517fe3181b98b67d3f420e6c2559"
+    sha256 cellar: :any_skip_relocation, mojave: "6910a5e7993ab53eadd2a96cd04b2931df98fa95bc41c7ac63d64705042e14ff"
   end
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-o", bin/"uni"
+    system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   test do
