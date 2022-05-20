2d995f138d4349b08d6db6a8995a985700424bcc
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 15:14:38 +0800

    rpm: update 4.17.0_1 bottle.

--- Formula/rpm.rb
+++ Formula/rpm.rb
@@ -5,6 +5,7 @@ class Rpm < Formula
   mirror "https://ftp.osuosl.org/pub/rpm/releases/rpm-4.17.x/rpm-4.17.0.tar.bz2"
   sha256 "2e0d220b24749b17810ed181ac1ed005a56bbb6bc8ac429c21f314068dc65e6a"
   license "GPL-2.0-only"
+  revision 1
   version_scheme 1
 
   livecheck do
@@ -13,20 +14,14 @@ class Rpm < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "dec1600907c7c9f4f60e133cc9f75ba72f57da692a5f3ca47c5d6429c52cfc5e"
-    sha256 arm64_big_sur:  "c4b10772346b10fce44ecb909a27701d8bd209b834755fd38abc325bcad4c75e"
-    sha256 monterey:       "0b379a488c105af62efe9e14e8508754d47bdc73334b0def3999278eee0321c9"
-    sha256 big_sur:        "6f857111ed59bf5efb8f97ad26c7ed52fb8e70a92d2978dc7d2f6173d14675cd"
-    sha256 catalina:       "29846a4e13dd2683318362442fe7a84c2b7cd71813291be24cc356bc657f8a8d"
-    sha256 mojave:         "aeab2644677216b3d631a4a1abb3d75aada85152f7f85a550ab43943b934f994"
-    sha256 x86_64_linux:   "1147c07948c53779fdf751623b349d6da6d6b4753103ce2c898da2cc68a0a041"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rpm"
+    sha256 mojave: "78da1b2cfc5b1e866315723987f8ed5ea910a794001653de97ef3e1b4a9e76bc"
   end
 
   # We need autotools for the Lua patch below. Remove when the patch is no longer needed.
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on "berkeley-db"
   depends_on "gettext"
   depends_on "libarchive"
   depends_on "libmagic"
@@ -38,6 +33,9 @@ class Rpm < Formula
   depends_on "sqlite"
   depends_on "xz"
   depends_on "zstd"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "zlib"
 
   # Fix `fstat64` detection for Apple Silicon.
   # https://github.com/rpm-software-management/rpm/pull/1775
