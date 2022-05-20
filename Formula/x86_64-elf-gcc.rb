f140dda39d95b73711a758a638f65190b5b66b85
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-21 02:48:33 +0800

    x86_64-elf-gcc: update 12.1.0 bottle.

--- Formula/x86_64-elf-gcc.rb
+++ Formula/x86_64-elf-gcc.rb
@@ -1,9 +1,9 @@
 class X8664ElfGcc < Formula
   desc "GNU compiler collection for x86_64-elf"
   homepage "https://gcc.gnu.org"
-  url "https://ftp.gnu.org/gnu/gcc/gcc-11.3.0/gcc-11.3.0.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gcc/gcc-11.3.0/gcc-11.3.0.tar.xz"
-  sha256 "b47cf2818691f5b1e21df2bb38c795fac2cfbd640ede2d0a5e1c89e338a3ac39"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-12.1.0/gcc-12.1.0.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gcc/gcc-12.1.0/gcc-12.1.0.tar.xz"
+  sha256 "62fd634889f31c02b64af2c468f064b47ad1ca78411c45abe6ac4b5f8dd19c7b"
   license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
 
   livecheck do
@@ -12,22 +12,13 @@ class X8664ElfGcc < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/x86_64-elf-gcc"
-    sha256 mojave: "4ddf044619a39d0457d3dad18a0d2e12308b0d9efe8e4bb7e936d5583171533f"
+    sha256 mojave: "f6a6924166945d05e06e67f1a001799cf96966fe71f61e3ea612f71206ac79e6"
   end
 
   depends_on "gmp"
   depends_on "libmpc"
   depends_on "mpfr"
   depends_on "x86_64-elf-binutils"
-
-  # Remove when upstream has Apple Silicon support
-  if Hardware::CPU.arm?
-    patch do
-      # patch from gcc-11.1.0-arm branch
-      url "https://github.com/fxcoudert/gcc/commit/eea3046c5fa62d4dee47e074c7a758570d9da61c.patch?full_index=1"
-      sha256 "b55ca05a0ed32f69f63bbe708568df5ad62d938da0e34b515d601bb966d32d40"
-    end
-  end
 
   def install
     target = "x86_64-elf"
