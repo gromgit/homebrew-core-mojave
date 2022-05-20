5f057bcfedbfd85122c7366a1983e83f9d5f71f7
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 21:05:32 +0800

    tfsec: update 1.21.0 bottle.

--- Formula/tfsec.rb
+++ Formula/tfsec.rb
@@ -1,8 +1,8 @@
 class Tfsec < Formula
   desc "Static analysis security scanner for your terraform code"
   homepage "https://tfsec.dev/"
-  url "https://github.com/aquasecurity/tfsec/archive/v1.19.1.tar.gz"
-  sha256 "c73f78558d27c58881063243b14c9176760f8c288d17eae82c341bd0251155fe"
+  url "https://github.com/aquasecurity/tfsec/archive/v1.21.0.tar.gz"
+  sha256 "d3ea2a68278a8a4fc0c6474b5ca5a358e900e309f46c0c88770b7302a3679ebd"
   license "MIT"
   head "https://github.com/aquasecurity/tfsec.git", branch: "master"
 
@@ -13,7 +13,7 @@ class Tfsec < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tfsec"
-    sha256 cellar: :any_skip_relocation, mojave: "495955e18569390bf723318016f2f20237b9dfa189a74bc368e77af768544682"
+    sha256 cellar: :any_skip_relocation, mojave: "5b7f3ac19d8efb1d7a731f7fc23e3a8bda4cacf12cfdf615dfc37dfd6626b86b"
   end
 
   depends_on "go" => :build
