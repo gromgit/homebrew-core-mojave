5798b9b51e2727ab65544178661b13c2270fae4c
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 18:56:26 +0800

    tcsh: update 6.24.01 bottle.

--- Formula/tcsh.rb
+++ Formula/tcsh.rb
@@ -1,9 +1,9 @@
 class Tcsh < Formula
   desc "Enhanced, fully compatible version of the Berkeley C shell"
   homepage "https://www.tcsh.org/"
-  url "https://astron.com/pub/tcsh/tcsh-6.24.00.tar.gz"
-  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/tcsh/tcsh-6.24.00.tar.gz"
-  sha256 "60be2c504bd8f1fa6e424b1956495d7e7ced52a2ac94db5fd27f4b6bfc8f74f0"
+  url "https://astron.com/pub/tcsh/tcsh-6.24.01.tar.gz"
+  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/tcsh/tcsh-6.24.01.tar.gz"
+  sha256 "5659a1b973b1f947e36238cb17a00210ac4cff3b496f8f851acd77172ab91d7e"
   license "BSD-3-Clause"
 
   livecheck do
@@ -13,7 +13,7 @@ class Tcsh < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tcsh"
-    sha256 mojave: "5dbdf4556443b38efef4ff56bebb3dcb40844ba627f4e69ca7531e1ff7420422"
+    sha256 mojave: "130d33874b9df7a974e439d04d9be9cfb173a787d38229bb367f8498555b91b1"
   end
 
   uses_from_macos "ncurses"
