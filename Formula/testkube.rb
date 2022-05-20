8eeb0e4d6f84cb4b6fe1214bac7249173c1d0d1b
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 20:54:23 +0800

    testkube: update 1.1.4 bottle.

--- Formula/testkube.rb
+++ Formula/testkube.rb
@@ -1,13 +1,13 @@
 class Testkube < Formula
   desc "Kubernetes-native framework for test definition and execution"
   homepage "https://testkube.io"
-  url "https://github.com/kubeshop/testkube/archive/refs/tags/v1.0.15.tar.gz"
-  sha256 "4ed9a129451649d618abb9cea69c77af7f4fdab883b7cde95256f82a1a31b024"
+  url "https://github.com/kubeshop/testkube/archive/refs/tags/v1.1.4.tar.gz"
+  sha256 "632c9a694198facf77bb0536a0317defa81bd3c3bd2c69a656c0d030da22d4d5"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/testkube"
-    sha256 cellar: :any_skip_relocation, mojave: "477e692eae74e2f584f166e4cdc37b3fadd21503865f30d1a79ce22897fefb3a"
+    sha256 cellar: :any_skip_relocation, mojave: "daed1b2e4f336f5ece2cb90afea41e32d1f657d25b59fdf7858c9635540dfd16"
   end
 
   depends_on "go" => :build
