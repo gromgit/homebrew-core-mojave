6626b120762800ab8a5ef525394a50f4acf9113f
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 22:46:58 +0800

    ugrep: update 3.7.11 bottle.

--- Formula/ugrep.rb
+++ Formula/ugrep.rb
@@ -1,13 +1,13 @@
 class Ugrep < Formula
   desc "Ultra fast grep with query UI, fuzzy search, archive search, and more"
   homepage "https://github.com/Genivia/ugrep"
-  url "https://github.com/Genivia/ugrep/archive/v3.7.9.tar.gz"
-  sha256 "016e771756574a2a0b026ec50f7e7f3898d39cb61771ce98bc225c34d86a03be"
+  url "https://github.com/Genivia/ugrep/archive/v3.7.11.tar.gz"
+  sha256 "47cf3eee0a6eb759a5ee53b8e0e09d5d1843c5597cfd83e9c88f1b29e6873b2b"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ugrep"
-    sha256 mojave: "c1a450c62bcbc57abea91af9d5f81410e9a66c9a7aca43e73ee6750807c81c7a"
+    sha256 mojave: "bbd05bb728ce17001795716f80e747a5a14316cdab85b15220d65f175fafe122"
   end
 
   depends_on "pcre2"
