70a412d4253f4c5d92ec82cd3003d47d11028d78
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-21 03:28:26 +0800

    yelp-tools: update 42.0 bottle.

--- Formula/yelp-tools.rb
+++ Formula/yelp-tools.rb
@@ -3,18 +3,13 @@ class YelpTools < Formula
 
   desc "Tools that help create and edit Mallard or DocBook documentation"
   homepage "https://github.com/GNOME/yelp-tools"
-  url "https://download.gnome.org/sources/yelp-tools/41/yelp-tools-41.0.tar.xz"
-  sha256 "37f1acc02bcbe68a31b86e07c129a839bd3276e656dc89eb7fc0a92746eff272"
+  url "https://download.gnome.org/sources/yelp-tools/42/yelp-tools-42.0.tar.xz"
+  sha256 "2cd43063ffa7262df15dd8d379aa3ea3999d42661f07563f4802daa1149f7df4"
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "cda13a749f9817f2e856178fda8ac72ad0104fca14a48bdb8abc26085610f051"
-    sha256 cellar: :any,                 arm64_big_sur:  "7023c23ea27f57ff8a89a378643ecdbb643f5350126ce1971bb575d9d773f739"
-    sha256 cellar: :any,                 monterey:       "0085d535d7f8a428012e1d9ed188abeb8859b8f67804d7f970b6afd25a795cb4"
-    sha256 cellar: :any,                 big_sur:        "1102fbc8573c51525f22bfc069c2cad50402012c80699d958130c6ddf153c924"
-    sha256 cellar: :any,                 catalina:       "7ed33af3a9d9c7256c06357b30f9f1ef577ae7409c376e60f8c9e10ff5d0b55d"
-    sha256 cellar: :any,                 mojave:         "533c8b568d6390cf108b222ec3334a048c5b038a59d29b53cf17e55b0191d734"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cab7be797b38e41c06681e07624a044dc4d75f837649e244062b9795e773b3a3"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/yelp-tools"
+    sha256 cellar: :any, mojave: "0750798bb11fe630b6c33cb784f687d318b8299c6709496a967a382f9406072c"
   end
 
   depends_on "gettext" => :build
@@ -30,13 +25,13 @@ class YelpTools < Formula
   uses_from_macos "libxslt"
 
   resource "lxml" do
-    url "https://files.pythonhosted.org/packages/e5/21/a2e4517e3d216f0051687eea3d3317557bde68736f038a3b105ac3809247/lxml-4.6.3.tar.gz"
-    sha256 "39b78571b3b30645ac77b95f7c69d1bffc4cf8c3b157c435a34da72e78c82468"
+    url "https://files.pythonhosted.org/packages/3b/94/e2b1b3bad91d15526c7e38918795883cee18b93f6785ea8ecf13f8ffa01e/lxml-4.8.0.tar.gz"
+    sha256 "f63f62fc60e6228a4ca9abae28228f35e1bd3ce675013d1dfb828688d50c6e23"
   end
 
   resource "yelp-xsl" do
-    url "https://download.gnome.org/sources/yelp-xsl/41/yelp-xsl-41.0.tar.xz"
-    sha256 "c8cd64c093bbd8c5d5e47fd38864e90831b5f9cf7403530870206fa96636a4a5"
+    url "https://download.gnome.org/sources/yelp-xsl/42/yelp-xsl-42.0.tar.xz"
+    sha256 "29b273cc0bd16efb6e983443803f1e9fdc03511e5c4ff6348fd30a604d4dc846"
   end
 
   def install
