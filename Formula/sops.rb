f2481b300321b2b1fd26d330efe95e6a88b60892
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 17:58:50 +0800

    sops: update 3.7.3 bottle.

--- Formula/sops.rb
+++ Formula/sops.rb
@@ -1,14 +1,14 @@
 class Sops < Formula
   desc "Editor of encrypted files"
   homepage "https://github.com/mozilla/sops"
-  url "https://github.com/mozilla/sops/archive/v3.7.2.tar.gz"
-  sha256 "905d0d85e6f3a36eb0586bae5c1bf501445841303e597136f69a33040f5123b2"
+  url "https://github.com/mozilla/sops/archive/v3.7.3.tar.gz"
+  sha256 "0e563f0c01c011ba52dd38602ac3ab0d4378f01edfa83063a00102587410ac38"
   license "MPL-2.0"
   head "https://github.com/mozilla/sops.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sops"
-    sha256 cellar: :any_skip_relocation, mojave: "b6cc777b9847cc48a21eb9667e2528e5f55fb7bfe99476c12d8836b8d21d95da"
+    sha256 cellar: :any_skip_relocation, mojave: "0e7ecd46a72a65dc3475a9265c244903221f0ee3d9df05c8720034f2461e27b5"
   end
 
   depends_on "go" => :build
