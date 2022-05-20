a33b052b84400625fcf5828f459714aa97ebac00
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 18:38:16 +0800

    syncthing: update 1.20.1 bottle.

--- Formula/syncthing.rb
+++ Formula/syncthing.rb
@@ -1,8 +1,8 @@
 class Syncthing < Formula
   desc "Open source continuous file synchronization application"
   homepage "https://syncthing.net/"
-  url "https://github.com/syncthing/syncthing/archive/v1.20.0.tar.gz"
-  sha256 "27d9b52f18ebd833938731ffaaeed4f34ced534ef3d1ed2bc126a495764f753d"
+  url "https://github.com/syncthing/syncthing/archive/v1.20.1.tar.gz"
+  sha256 "4b355eda64b9f52d53b34ab99a1ff73dc3d5ad5e43086223e7fc7f4d05a58a05"
   license "MPL-2.0"
   head "https://github.com/syncthing/syncthing.git", branch: "main"
 
@@ -13,7 +13,7 @@ class Syncthing < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/syncthing"
-    sha256 cellar: :any_skip_relocation, mojave: "e4c75a17eeff804d2158b262a2e0f0688927bca1a4999cfba3901fe92f13f195"
+    sha256 cellar: :any_skip_relocation, mojave: "d0e65860d4e8ef5c94259455a95baa71d55730bdd74199294aad69c4c1923f42"
   end
 
   depends_on "go" => :build
