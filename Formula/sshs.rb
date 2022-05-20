730ba3cfa486b7c5bd6b3de99b8ffccbab933991
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 18:18:05 +0800

    sshs: update 3.2.0 bottle.

--- Formula/sshs.rb
+++ Formula/sshs.rb
@@ -1,13 +1,13 @@
 class Sshs < Formula
   desc "Graphical command-line client for SSH"
   homepage "https://github.com/quantumsheep/sshs"
-  url "https://github.com/quantumsheep/sshs/archive/refs/tags/3.1.0.tar.gz"
-  sha256 "9a656d68b0514227a26f8a9a56537b56a5dddd079de67a892643fc0b9ac238b7"
+  url "https://github.com/quantumsheep/sshs/archive/refs/tags/3.2.0.tar.gz"
+  sha256 "8b3127178c7bff19c1ea1a5fd2c5758137863c7e7cfbc4900cdf3fa2f13007a6"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sshs"
-    sha256 cellar: :any_skip_relocation, mojave: "6a8cacb798a9dbe76d2c1f24ce249c2f258a3676437eb1d4bd8d7356b5ced681"
+    sha256 cellar: :any_skip_relocation, mojave: "6c96444975c81c5da325fdd277bd4e2ae10411ab8ac901681f9b7e172a5df62a"
   end
 
   depends_on "go" => :build
