08d02c118bff5de8d93855fba4fac54696215c1b
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 21:12:43 +0800

    thanos: update 0.26.0 bottle.

--- Formula/thanos.rb
+++ Formula/thanos.rb
@@ -1,13 +1,13 @@
 class Thanos < Formula
   desc "Highly available Prometheus setup with long term storage capabilities"
   homepage "https://thanos.io"
-  url "https://github.com/thanos-io/thanos/archive/v0.25.2.tar.gz"
-  sha256 "c77fe0ec6ce596506fd200548ec90dafe0bd268cdf0e0fb965a2ff2648b18e03"
+  url "https://github.com/thanos-io/thanos/archive/v0.26.0.tar.gz"
+  sha256 "4d34e61994d8d006f9280a5dae1d57aa25e40b79bb2d3a77440978824d036cf9"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/thanos"
-    sha256 cellar: :any_skip_relocation, mojave: "a5e7bc05c006c77768089873903890e2b27ec0bde167c00101b66bbabc719e03"
+    sha256 cellar: :any_skip_relocation, mojave: "c94445d69aa5f684461119da975c1338ddbd7cb4a75f7365c5f8b58c00954b64"
   end
 
   depends_on "go" => :build
