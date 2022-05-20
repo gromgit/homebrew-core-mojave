d5df998b35b4344562e026c36942e2a8a89a2305
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-20 20:32:52 +0800

    terraformer: update 0.8.20 bottle.

--- Formula/terraformer.rb
+++ Formula/terraformer.rb
@@ -1,14 +1,14 @@
 class Terraformer < Formula
   desc "CLI tool to generate terraform files from existing infrastructure"
   homepage "https://github.com/GoogleCloudPlatform/terraformer"
-  url "https://github.com/GoogleCloudPlatform/terraformer/archive/0.8.19.tar.gz"
-  sha256 "f25804c1e09897e42a6506fd09deb18ee7e64e5a24e9f4148a7c29f98db519eb"
+  url "https://github.com/GoogleCloudPlatform/terraformer/archive/0.8.20.tar.gz"
+  sha256 "d0349ba4e8956a045c6abd7fdc47481dfb3c514f4d5e57f7c4480bce8560c359"
   license "Apache-2.0"
   head "https://github.com/GoogleCloudPlatform/terraformer.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/terraformer"
-    sha256 cellar: :any_skip_relocation, mojave: "56d2133b66f11cdeb64603401555d5579c78ce44c0ac52c1ffe8608bc10b3918"
+    sha256 cellar: :any_skip_relocation, mojave: "a99f48204a3183a985602ca46cee2db295afeeeac4276b696f8f5de2fea4990f"
   end
 
   depends_on "go" => :build
