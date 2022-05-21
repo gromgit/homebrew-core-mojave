0478b8e1ab26d6b4b3c2c09a249bd786aaaa7411
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-22 02:33:58 +0800

    kind: update 0.13.0 bottle.

--- Formula/kind.rb
+++ Formula/kind.rb
@@ -1,14 +1,14 @@
 class Kind < Formula
   desc "Run local Kubernetes cluster in Docker"
   homepage "https://kind.sigs.k8s.io/"
-  url "https://github.com/kubernetes-sigs/kind/archive/v0.12.0.tar.gz"
-  sha256 "cd1d09921b3c8a0f58c6423e5706be0c6e556f0c3d2b9e62f42be59263b209bb"
+  url "https://github.com/kubernetes-sigs/kind/archive/v0.13.0.tar.gz"
+  sha256 "e07e3a06c8a1d3861ebee3c2fecb027e839da8abf79c6f00c394b077e1f990fd"
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/kind.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kind"
-    sha256 cellar: :any_skip_relocation, mojave: "198f7d99a3ef32390a0a8986696275492ad1d40db55f4f15a44394e54e08b89e"
+    sha256 cellar: :any_skip_relocation, mojave: "f91a46aab3d39621beeb73e0fbc037bc72316e7226473bea96ec922234f07549"
   end
 
   depends_on "go" => :build
