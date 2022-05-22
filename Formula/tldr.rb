e843bd12a990a5fa61242927def90f4403e3e91c
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 06:31:33 +0800

    tldr: update 1.4.3 bottle.

--- Formula/tldr.rb
+++ Formula/tldr.rb
@@ -1,15 +1,14 @@
 class Tldr < Formula
   desc "Simplified and community-driven man pages"
   homepage "https://tldr.sh/"
-  url "https://github.com/tldr-pages/tldr-c-client/archive/v1.4.2.tar.gz"
-  sha256 "532cc30b21ea146d23ba880142ae284bd2774c27c247ecc00221d9434bd9343f"
+  url "https://github.com/tldr-pages/tldr-c-client/archive/v1.4.3.tar.gz"
+  sha256 "273d920191c7a4f9fdd9f2798feacc65eb5b17f95690a90b6901e8c596900d9d"
   license "MIT"
   head "https://github.com/tldr-pages/tldr-c-client.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tldr"
-    rebuild 2
-    sha256 cellar: :any, mojave: "06ded954394dabea9a04987c5764ad91f7267793855e3cb0fbe93975df9e1597"
+    sha256 cellar: :any, mojave: "3b3a0cd0d44b38f9adacff6403eab5ec839d1c8b30d5f069c5a73f4f1c4dc862"
   end
 
   depends_on "pkg-config" => :build
