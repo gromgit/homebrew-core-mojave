34dcab67b9b7a6156c4164bc610db2567c02bf38
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-22 13:42:56 +0800

    nicotine-plus: update 3.2.2 bottle.

--- Formula/nicotine-plus.rb
+++ Formula/nicotine-plus.rb
@@ -3,14 +3,14 @@ class NicotinePlus < Formula
 
   desc "Graphical client for the Soulseek file sharing network"
   homepage "https://nicotine-plus.github.io/nicotine-plus/"
-  url "https://files.pythonhosted.org/packages/fa/85/cb0ecfce7b529446104cb29a7bfe78a2d0efa124444fbc591c6a5193a824/nicotine-plus-3.2.1.tar.gz"
-  sha256 "a2835bdce9054afc727e554e02585d34f153ba1d5fae9f883758212b089a8539"
+  url "https://files.pythonhosted.org/packages/8d/60/a0a7b9d9066a4edcc4b9965d71e31794fc5faf2f8624619d46a8c484d272/nicotine-plus-3.2.2.tar.gz"
+  sha256 "32bb50559bb67b0b6d1760d4a11eec0660f841d00607a1f165292c967b6001c9"
   license "GPL-3.0-or-later"
   head "https://github.com/Nicotine-Plus/nicotine-plus.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nicotine-plus"
-    sha256 cellar: :any_skip_relocation, mojave: "aaa6ec5dd1bd8bf311e0bd028e5cd257d8b80ec4eeef1a07481c9190fe7a928e"
+    sha256 cellar: :any_skip_relocation, mojave: "7e6dcfd30c38111dcb40c4888ed2a19dee87e380c6c0c76514e5930552990876"
   end
 
   depends_on "adwaita-icon-theme"
