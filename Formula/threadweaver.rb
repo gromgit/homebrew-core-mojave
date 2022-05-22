128c19d3761bfbd8269ed157013af74633a17bdb
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 06:21:11 +0800

    threadweaver: update 5.93.0 bottle.

--- Formula/threadweaver.rb
+++ Formula/threadweaver.rb
@@ -1,8 +1,8 @@
 class Threadweaver < Formula
   desc "Helper for multithreaded programming"
   homepage "https://api.kde.org/frameworks/threadweaver/html/index.html"
-  url "https://download.kde.org/stable/frameworks/5.90/threadweaver-5.90.0.tar.xz"
-  sha256 "e3fd3815b1732e5157d5d7ab2d27dd447a8d60ff50019ae9f0e5665b3ffd30bb"
+  url "https://download.kde.org/stable/frameworks/5.93/threadweaver-5.93.0.tar.xz"
+  sha256 "6c0db7d276052ebc4d512b43792ae2127cb5fafb1cfee890b5f20bd05c06f185"
   license "LGPL-2.0-or-later"
   head "https://invent.kde.org/frameworks/threadweaver.git", branch: "master"
 
@@ -15,8 +15,7 @@ class Threadweaver < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/threadweaver"
-    rebuild 1
-    sha256 cellar: :any, mojave: "93b7510b1d28f0d8ee04465fc8326279fba3b630f1a470f62588a41308a3a681"
+    sha256 cellar: :any, mojave: "41085707e8753fb2b7628dc552524d61599d26449169f1a95487bd92d2b74554"
   end
 
   depends_on "cmake" => [:build, :test]
@@ -24,6 +23,12 @@ class Threadweaver < Formula
   depends_on "extra-cmake-modules" => [:build, :test]
   depends_on "graphviz" => :build
   depends_on "qt@5"
+
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
 
   def install
     args = std_cmake_args + %w[
@@ -41,8 +46,9 @@ class Threadweaver < Formula
 
   test do
     ENV.delete "CPATH"
-    qt5_arg = "-DQt5Core_DIR=#{Formula["qt@5"].opt_lib}/cmake/Qt5Core"
-    system "cmake", (pkgshare/"examples/HelloWorld"), *std_cmake_args, qt5_arg
+    qt5_args = ["-DQt5Core_DIR=#{Formula["qt@5"].opt_lib}/cmake/Qt5Core"]
+    qt5_args << "-DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath,#{Formula["qt@5"].opt_lib}" unless OS.mac?
+    system "cmake", (pkgshare/"examples/HelloWorld"), *std_cmake_args, *qt5_args
     system "make"
 
     assert_equal "Hello World!", shell_output("./ThreadWeaver_HelloWorld 2>&1").strip
