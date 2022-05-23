1165e9ea36111f6f7015e2d2d0218ec38234789d
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:40 +0800

    sproxy: no rebuilding required

--- Formula/sproxy.rb
+++ Formula/sproxy.rb
@@ -19,6 +19,7 @@ class Sproxy < Formula
     sha256 cellar: :any_skip_relocation, sierra:        "8d57317644b76b465adc5caf984f1e3cf57f9486f642705eee66128adbcf3589"
     sha256 cellar: :any_skip_relocation, el_capitan:    "4ed786b0b05ca3c88d5904e3119d84725a9f9bedf5d952c055f22a81661a825c"
     sha256 cellar: :any_skip_relocation, yosemite:      "19da9a5b680a860e721ec60763dd48e9a5213505ee643703abcdc66707e8ce51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "713ebc755ddb821e5e0bd17a13dc0a274645c7478f5682cd9407ddadb3e93c31"
   end
 
   # Only needed due to the change to "Makefile.am"
@@ -26,7 +27,88 @@ class Sproxy < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
 
+  uses_from_macos "perl"
+
+  on_linux do
+    depends_on "openssl@1.1"
+
+    resource "File::Remove" do
+      url "https://cpan.metacpan.org/authors/id/S/SH/SHLOMIF/File-Remove-1.60.tar.gz"
+      sha256 "e86e2a40ffedc6d5697d071503fd6ba14a5f9b8220af3af022110d8e724f8ca6"
+    end
+
+    resource "YAML::Tiny" do
+      url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/YAML-Tiny-1.73.tar.gz"
+      sha256 "bc315fa12e8f1e3ee5e2f430d90b708a5dc7e47c867dba8dce3a6b8fbe257744"
+    end
+
+    resource "Module::Install" do
+      url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/Module-Install-1.19.tar.gz"
+      sha256 "1a53a78ddf3ab9e3c03fc5e354b436319a944cba4281baf0b904fa932a13011b"
+    end
+
+    resource "Net::SSLeay" do
+      url "https://cpan.metacpan.org/authors/id/C/CH/CHRISN/Net-SSLeay-1.92.tar.gz"
+      sha256 "47c2f2b300f2e7162d71d699f633dd6a35b0625a00cbda8c50ac01144a9396a9"
+    end
+
+    resource "HTML::Parser" do
+      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/HTML-Parser-3.76.tar.gz"
+      sha256 "64d9e2eb2b420f1492da01ec0e6976363245b4be9290f03f10b7d2cb63fa2f61"
+    end
+
+    resource "URI" do
+      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/URI-5.09.tar.gz"
+      sha256 "03e63ada499d2645c435a57551f041f3943970492baa3b3338246dab6f1fae0a"
+    end
+
+    resource "LWP::UserAgent" do
+      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/libwww-perl-6.64.tar.gz"
+      sha256 "48335e0992b4875bd73c6661439f3506c2c6d92b5dd601582b8dc22e767d3dae"
+    end
+
+    resource "HTTP::Request" do
+      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/HTTP-Message-6.36.tar.gz"
+      sha256 "576a53b486af87db56261a36099776370c06f0087d179fc8c7bb803b48cddd76"
+    end
+
+    resource "HTTP::Date" do
+      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/HTTP-Date-6.05.tar.gz"
+      sha256 "365d6294dfbd37ebc51def8b65b81eb79b3934ecbc95a2ec2d4d827efe6a922b"
+    end
+
+    resource "Try::Tiny" do
+      url "https://cpan.metacpan.org/authors/id/E/ET/ETHER/Try-Tiny-0.31.tar.gz"
+      sha256 "3300d31d8a4075b26d8f46ce864a1d913e0e8467ceeba6655d5d2b2e206c11be"
+    end
+
+    resource "HTTP::Daemon" do
+      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/HTTP-Daemon-6.14.tar.gz"
+      sha256 "f0767e7f3cbb80b21313c761f07ad8ed253bce9fa2d0ba806b3fb72d309b2e1d"
+    end
+
+    resource "LWP::MediaTypes" do
+      url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/LWP-MediaTypes-6.04.tar.gz"
+      sha256 "8f1bca12dab16a1c2a7c03a49c5e58cce41a6fec9519f0aadfba8dad997919d9"
+    end
+  end
+
   def install
+    unless OS.mac?
+      ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+      ENV.prepend_create_path "PERL5LIB", lib/"sproxy"
+      ENV["PERL_MM_USE_DEFAULT"] = "1"
+      ENV["OPENSSL_PREFIX"] = Formula["openssl@1.1"].opt_prefix
+
+      resources.each do |r|
+        r.stage do
+          system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+          system "make"
+          system "make", "install"
+        end
+      end
+    end
+
     # Prevents "ERROR: Can't create '/usr/local/share/man/man3'"; also fixes an
     # audit violation triggered if the man page is installed in #{prefix}/man.
     # After making the change below and running autoreconf, the default ends up
@@ -43,6 +125,13 @@ class Sproxy < Formula
                           "--prefix=#{prefix}"
     system "make"
     system "make", "install"
+
+    # sproxy must be wrapped in an ENV script on Linux so it can find
+    # the additional Perl dependencies
+    unless OS.mac?
+      bin.env_script_all_files(libexec/"bin", PERL5LIB: ENV["PERL5LIB"])
+      chmod 0755, libexec/"bin/sproxy"
+    end
   end
 
   test do
