e3c4167dfc0c195986042156668b82adbc48f329
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 19:13:02 +0800

    cppad: update 20220000.3 bottle.

--- Formula/cppad.rb
+++ Formula/cppad.rb
@@ -1,9 +1,8 @@
 class Cppad < Formula
   desc "Differentiation of C++ Algorithms"
   homepage "https://www.coin-or.org/CppAD"
-  # Stable versions have numbers of the form 201x0000.y
-  url "https://github.com/coin-or/CppAD/archive/20220000.2.tar.gz"
-  sha256 "a363996d6a36c08b37048d3c439119d1cd715ce7caf124ac6f0c2e94240985bd"
+  url "https://github.com/coin-or/CppAD/archive/20220000.3.tar.gz"
+  sha256 "7778c516a131a20e12c97dffe2b65fbf356d791741a6c63d39f16b9c4b747259"
   license "EPL-2.0"
   version_scheme 1
   head "https://github.com/coin-or/CppAD.git", branch: "master"
@@ -13,10 +12,9 @@ class Cppad < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cppad"
-    rebuild 1
-    sha256 cellar: :any, mojave: "2bc998eb29583df5b0fe97cac7e8e4e536ba98a3688270a86750528f02be7168"
+    sha256 cellar: :any, mojave: "031676941d8634722625bab5a653f89c50c11daacfd227b4f308e3aa9c94c128"
   end
 
   depends_on "cmake" => :build
