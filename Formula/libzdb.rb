8cc547f2b19f67134df3446816b3f9dd0b4503a3
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-24 05:05:42 +0800

    libzdb: update 3.2.2_2 bottle.

--- Formula/libzdb.rb
+++ Formula/libzdb.rb
@@ -4,7 +4,7 @@ class Libzdb < Formula
   url "https://tildeslash.com/libzdb/dist/libzdb-3.2.2.tar.gz"
   sha256 "d51e4e21ee1ee84ac8763de91bf485360cd76860b951ca998e891824c4f195ae"
   license "GPL-3.0-only"
-  revision 1
+  revision 2
 
   livecheck do
     url :homepage
@@ -12,11 +12,8 @@ class Libzdb < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "db54eac2ef107864c43f2888628a30ed7af5d3eae6f892b491ea7f2fe542a35b"
-    sha256 cellar: :any,                 big_sur:       "ae4c8d97236e248f1fa8fe189a4f7c049009335bc8038f541c8faf6c47c3d0e4"
-    sha256 cellar: :any,                 catalina:      "846888a4d5e47cccac9d41c95223974b16724b681c57e12e616a503409507014"
-    sha256 cellar: :any,                 mojave:        "7040dee7ee6eeb60e81aeacf6cc33f2e6e1ea5895c9a53e4a2b94ca509852974"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4f196f9126a86ccd959083301741b9d68e0e49352cb51b83cf3d82c52a0669be"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libzdb"
+    sha256 cellar: :any, mojave: "4edf5c3df788953e3f51afdf18713742a38d392d8cdbef78908a9bd48cbaf008"
   end
 
   depends_on macos: :high_sierra # C++ 17 is required
@@ -31,8 +28,14 @@ class Libzdb < Formula
 
   fails_with gcc: "5"
 
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
+    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
+  end
+
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+    system "./configure", *std_configure_args
     system "make", "install"
     (pkgshare/"test").install Dir["test/*.{c,cpp}"]
   end
