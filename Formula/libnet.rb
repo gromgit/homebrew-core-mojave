276a2fa26275b33d20ba7aefbef0916dfb9067ac
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:11 +0800

    libnet: no rebuilding required

--- Formula/libnet.rb
+++ Formula/libnet.rb
@@ -1,6 +1,6 @@
 class Libnet < Formula
   desc "C library for creating IP packets"
-  homepage "https://github.com/sam-github/libnet"
+  homepage "https://github.com/libnet/libnet"
   url "https://github.com/libnet/libnet/releases/download/v1.2/libnet-1.2.tar.gz"
   sha256 "caa4868157d9e5f32e9c7eac9461efeff30cb28357f7f6bf07e73933fb4edaa7"
   license "BSD-2-Clause"
