915df615d1bdc55fa1d2a1b0afb90339faeca69c
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-24 03:32:51 +0800

    imake: update 1.0.8_5 bottle.

--- Formula/imake.rb
+++ Formula/imake.rb
@@ -4,7 +4,7 @@ class Imake < Formula
   url "https://xorg.freedesktop.org/releases/individual/util/imake-1.0.8.tar.bz2"
   sha256 "b8d2e416b3f29cd6482bcffaaf19286d32917a164d07102a0e531ccd41a2a702"
   license "MIT"
-  revision 4
+  revision 5
 
   livecheck do
     url "https://xorg.freedesktop.org/releases/individual/util/"
@@ -12,18 +12,13 @@ class Imake < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "0e07192ab4d74a10ada2e6e1e7fbeabaa6c8d719fc9011841e421def7159e013"
-    sha256 arm64_big_sur:  "5e76cebc3734d39273587f3d2d286b15bd1e869b03af84589987f9236062a1b3"
-    sha256 monterey:       "0ed72c99749bdad72126577b8a8b09bc48a36757a12ba52834953af3636cba32"
-    sha256 big_sur:        "13f4455b0065bdd06ef93f2be612884dbe9c2196834c0a9ad7d6c67d350759de"
-    sha256 catalina:       "3baa9a70823b7b57ba43df65e54baf7dbd99e6b1615d7b15e22ac8af8746794f"
-    sha256 mojave:         "f71005738d8026816e0407da56a98f06ac96f1ab7dc5e28b155b6a4b39942f46"
-    sha256 x86_64_linux:   "d7d3a81f1cc361af9ad5e8408eb7592d346886eca0a6f81eeec80087131e6151"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/imake"
+    sha256 mojave: "5f432e0d567492deace61d2e7069c9bb233db77679605b4276df9e1629d0c552"
   end
 
   depends_on "pkg-config" => :build
   depends_on "xorgproto" => :build
-  depends_on "gcc"
+  depends_on "tradcpp"
 
   resource "xorg-cf-files" do
     url "https://xorg.freedesktop.org/releases/individual/util/xorg-cf-files-1.0.6.tar.bz2"
@@ -34,8 +29,7 @@ class Imake < Formula
     ENV.deparallelize
 
     # imake runtime is broken when used with clang's cpp
-    gcc_major_ver = Formula["gcc"].any_installed_version.major
-    cpp_program = Formula["gcc"].opt_bin/"cpp-#{gcc_major_ver}"
+    cpp_program = Formula["tradcpp"].opt_bin/"tradcpp"
     (buildpath/"imakemdep.h").append_lines [
       "#define DEFAULT_CPP \"#{cpp_program}\"",
       "#undef USE_CC_E",
@@ -61,7 +55,6 @@ class Imake < Formula
   test do
     # Use pipe_output because the return code is unimportant here.
     output = pipe_output("#{bin}/imake -v -s/dev/null -f/dev/null -T/dev/null 2>&1")
-    gcc_major_ver = Formula["gcc"].any_installed_version.major
-    assert_match "#{Formula["gcc"].opt_bin}/cpp-#{gcc_major_ver}", output
+    assert_match "#{Formula["tradcpp"].opt_bin}/tradcpp", output
   end
 end
