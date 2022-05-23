2b7966068028aca528517073f2fb63a09fef4ab7
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:13 +0800

    lit: no rebuilding required

--- Formula/lit.rb
+++ Formula/lit.rb
@@ -3,19 +3,20 @@ class Lit < Formula
 
   desc "Portable tool for LLVM- and Clang-style test suites"
   homepage "https://llvm.org"
-  url "https://files.pythonhosted.org/packages/9d/8e/a4570b82f17fe3bdc378fa41989c1652d57736918b3640b8acf5ce9dcb0b/lit-14.0.0.tar.gz"
-  sha256 "45e08ce87b0ea56ab632aa02fa857418a5dd241a711c7c756878b73a130c3efe"
+  url "https://files.pythonhosted.org/packages/65/0c/94e4da4d1dc0dd55a09257d16dd9c87ed2f8e4747278d78fd4254465d943/lit-14.0.3.tar.gz"
+  sha256 "a4126b5bfcb73b3ac0e8473cf365e844fce2867515ac56072b6950deda3d5d98"
   license "Apache-2.0" => { with: "LLVM-exception" }
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "eda14ce09883580e7908095d11aa42acfc5058f9c9ab47471afa195f0da78c44"
+    sha256 cellar: :any_skip_relocation, all: "83198fd6782b67a4b88874dc49d82f918cb5eb57dcd218bf948cc189e349aa66"
   end
 
   depends_on "llvm" => :test
   depends_on "python@3.10"
 
   def install
-    system "python3", *Language::Python.setup_install_args(prefix)
+    system "python3", *Language::Python.setup_install_args(prefix),
+                      "--install-lib=#{prefix/Language::Python.site_packages("python3")}"
 
     # Install symlinks so that `import lit` works with multiple versions of Python
     python_versions = Formula.names
