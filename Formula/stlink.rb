970ca590d694badcddcc23f92b066d0112048e8c
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:41 +0800

    stlink: no rebuilding required

--- Formula/stlink.rb
+++ Formula/stlink.rb
@@ -1,10 +1,10 @@
 class Stlink < Formula
   desc "STM32 discovery line Linux programmer"
-  homepage "https://github.com/texane/stlink"
-  url "https://github.com/texane/stlink/archive/v1.7.0.tar.gz"
+  homepage "https://github.com/stlink-org/stlink"
+  url "https://github.com/stlink-org/stlink/archive/v1.7.0.tar.gz"
   sha256 "57ec1214905aedf59bee7f70ddff02316f64fa9ba5a9b6a3a64952edc5b65855"
   license "BSD-3-Clause"
-  head "https://github.com/texane/stlink.git", branch: "develop"
+  head "https://github.com/stlink-org/stlink.git", branch: "develop"
 
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "2ea4763e208c5566d0ec1848a6d91c440fe745287aa7a5617af635fea7707af0"
