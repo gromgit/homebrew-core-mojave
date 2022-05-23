5ccc8cf9d7afa39da0a5f5926957605ad7903bdf
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 17:17:37 +0800

    aria2: update 1.36.0_1 bottle.

--- Formula/aria2.rb
+++ Formula/aria2.rb
@@ -4,19 +4,17 @@ class Aria2 < Formula
   url "https://github.com/aria2/aria2/releases/download/release-1.36.0/aria2-1.36.0.tar.xz"
   sha256 "58d1e7608c12404f0229a3d9a4953d0d00c18040504498b483305bcb3de907a5"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "74859913b0f8dc82fede202f5b6cb3384202e76887549addc050009e4d277aec"
-    sha256 cellar: :any, arm64_big_sur:  "22b11ee6f82f8d693fb2f6c0dd90a81174033640974003e63e4e8f98fbd3f145"
-    sha256 cellar: :any, monterey:       "ab509067c72eb555b1583189054622744f39e344e1ee51bfd91c3102167be95b"
-    sha256 cellar: :any, big_sur:        "8960d7b6b56fb29e020fb96da5305c3340d266f3e5e0c552248759e8c6169244"
-    sha256 cellar: :any, catalina:       "1e9db3f9fd405ed7a303b89379c2a8f3f01c5f5aa92cfbc8ae560c4e28618f8e"
-    sha256 cellar: :any, mojave:         "3ecd30ea4573748ec438182dbba6fad002c1465da7ef9f7a32f227fdeddde464"
-    sha256               x86_64_linux:   "c7dacb09aaa9eb22baf6ee6ee198896fb8a851ddf69d7c534c968f1b79edd69d"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/aria2"
+    sha256 mojave: "85819ae30402c019a7156fac9f72fdc60bd7bb74a183ac80beb361b10a24e72f"
   end
 
   depends_on "pkg-config" => :build
+  depends_on "gettext"
   depends_on "libssh2"
+  depends_on "sqlite"
 
   uses_from_macos "libxml2"
   uses_from_macos "zlib"
