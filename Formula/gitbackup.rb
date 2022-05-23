cc3510385027b639fbc1a81d8a007b654a55984a
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-24 01:38:07 +0800

    gitbackup: update 0.8.1 bottle.

--- Formula/gitbackup.rb
+++ Formula/gitbackup.rb
@@ -1,13 +1,13 @@
 class Gitbackup < Formula
   desc "Tool to backup your Bitbucket, GitHub and GitLab repositories"
   homepage "https://github.com/amitsaha/gitbackup"
-  url "https://github.com/amitsaha/gitbackup/archive/v0.8.0.tar.gz"
-  sha256 "b94bc7c55b5c086ba1504adceef74d0f0eb8c159f94cb84b23863e97f74fe217"
+  url "https://github.com/amitsaha/gitbackup/archive/v0.8.1.tar.gz"
+  sha256 "5f3313c3f226cdcb374631036b1187cfd52a857769ec254ac659098082a4e94d"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gitbackup"
-    sha256 cellar: :any_skip_relocation, mojave: "464f30bd128dfb46f01bace2f652a9175c8d5dd03ce8d997ae03acb92bc8662f"
+    sha256 cellar: :any_skip_relocation, mojave: "9f3476c0755cd6dae2943a47c61f33ec95157db8bac12790174779f24aa23450"
   end
 
   depends_on "go" => :build
