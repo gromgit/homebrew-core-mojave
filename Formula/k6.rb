66b7d71660b325e371366834cc09708610f07925
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-24 03:54:54 +0800

    k6: update 0.38.3 bottle.

--- Formula/k6.rb
+++ Formula/k6.rb
@@ -1,13 +1,13 @@
 class K6 < Formula
   desc "Modern load testing tool, using Go and JavaScript"
   homepage "https://k6.io"
-  url "https://github.com/grafana/k6/archive/v0.38.2.tar.gz"
-  sha256 "cf46cc564facf0558b92322edc53620b46aea8cb07626105b47e6d0cc6d33dd3"
+  url "https://github.com/grafana/k6/archive/v0.38.3.tar.gz"
+  sha256 "3ee4b4892a9965c336efc97b3fc9e179be0652331aecaf79e90d42dce8825c26"
   license "AGPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/k6"
-    sha256 cellar: :any_skip_relocation, mojave: "0a6dc666f8394cd3459b01275e5877108e2269a72c463d831ec5608d5ed10bf9"
+    sha256 cellar: :any_skip_relocation, mojave: "d5d8c4356e2a20cefcd24b4d796e4b47a244e3f1cd956245cc6c935698b9ae97"
   end
 
   depends_on "go" => :build
