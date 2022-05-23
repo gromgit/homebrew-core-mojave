32d807900933d25ea596f15b2a9804e3abf88f51
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:28 +0800

    pcb: no rebuilding required

--- Formula/pcb.rb
+++ Formula/pcb.rb
@@ -12,10 +12,12 @@ class Pcb < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "41a14f1d1a3439469248dd6b58535c082f084376a90ecf3ccca2513e70cd2028"
-    sha256 big_sur:       "f73590271ddcf104d25fecad90c916e4d535a5041280a1bbd661acdafc806b24"
-    sha256 catalina:      "a8937f1ce318a6472532eae067dc581ddef61518a5b56db83883cb2119c2bf32"
-    sha256 mojave:        "0607471efce526eb3fd06286f28fa664276ab8b4c3f407b14e18eb3c426cad59"
+    sha256 arm64_monterey: "71f5ca60f422ffc8d6555560f37167aae20a07c103c35fa3d100feafb9a9bc01"
+    sha256 arm64_big_sur:  "41a14f1d1a3439469248dd6b58535c082f084376a90ecf3ccca2513e70cd2028"
+    sha256 big_sur:        "f73590271ddcf104d25fecad90c916e4d535a5041280a1bbd661acdafc806b24"
+    sha256 catalina:       "a8937f1ce318a6472532eae067dc581ddef61518a5b56db83883cb2119c2bf32"
+    sha256 mojave:         "0607471efce526eb3fd06286f28fa664276ab8b4c3f407b14e18eb3c426cad59"
+    sha256 x86_64_linux:   "96a5bd2ef750cb1ae31604bb9f2e78daae83d9e20d7f432fa656601471465370"
   end
 
   head do
@@ -33,21 +35,41 @@ class Pcb < Formula
   depends_on "gtk+"
   depends_on "gtkglext"
 
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
+  uses_from_macos "perl" => :build
+  uses_from_macos "tcl-tk"
+
+  on_macos do
+    depends_on "gnu-sed"
+  end
+
   conflicts_with "gts", because: "both install a `gts.h` header"
 
   def install
+    if OS.mac?
+      ENV.prepend_path "PATH", Formula["gnu-sed"].libexec/"gnubin"
+    else
+      ENV.prepend_path "PERL5LIB", Formula["intltool"].libexec/"lib/perl5"
+    end
+
     system "./autogen.sh" if build.head?
-    system "./configure", "--prefix=#{prefix}",
-                          "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-update-desktop-database",
-                          "--disable-update-mime-database",
-                          "--disable-gl",
-                          "--without-x"
+    args = std_configure_args + %w[
+      --disable-update-desktop-database
+      --disable-update-mime-database
+      --disable-gl
+    ]
+    args << "--without-x" if OS.mac?
+
+    system "./configure", *args
     system "make", "install"
   end
 
   test do
+    # Disable test on Linux because it fails with:
+    # Gtk-WARNING **: 09:09:35.919: cannot open display
+    return if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
+
     assert_match version.to_s, shell_output("#{bin}/pcb --version")
   end
 end
