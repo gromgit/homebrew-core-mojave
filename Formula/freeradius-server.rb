53dd796b7ed49ad5ff9b263535055518c1e45fb1
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-24 00:07:01 +0800

    freeradius-server: update 3.2.0 bottle.

--- Formula/freeradius-server.rb
+++ Formula/freeradius-server.rb
@@ -5,8 +5,8 @@ class FreeradiusServer < Formula
   head "https://github.com/FreeRADIUS/freeradius-server.git", branch: "master"
 
   stable do
-    url "https://github.com/FreeRADIUS/freeradius-server/archive/release_3_0_25.tar.gz"
-    sha256 "493b0b9bef3d9f0e6949fcfd6aa282164f6b8e6404231f2b0d1353739199ffcd"
+    url "https://github.com/FreeRADIUS/freeradius-server/archive/refs/tags/release_3_2_0.tar.gz"
+    sha256 "2b8817472847e0b49395facd670be97071133730ffa825bb56386c89c18174f5"
 
     # Fix -flat_namespace being used
     patch do
@@ -21,13 +21,8 @@ class FreeradiusServer < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "2c4a942fb9f20633bf947ec0b8551731677fa0bdc54b7054be9c367bec8c1fcd"
-    sha256 arm64_big_sur:  "f4ed69d19363b792ffba75c41b703bd6f068a2d65d76a3046ba6947832b6a6b3"
-    sha256 monterey:       "afe19281cfa3f33f2836cea24c1efd2d98d3e185f9cab5faabd45addc34fdf20"
-    sha256 big_sur:        "a28e06a16084beb496edcb341dcf5aaf7f6da6ef995bd7383545d59aac76f393"
-    sha256 catalina:       "6fe8001066708fc01ba0c71a7a0246aee4af4910a68f155d82fd411a7a826c1b"
-    sha256 mojave:         "660eb25588c2d0c68aae1684bfc9f9174c6c6d4d721d196a6232c981718e9ebf"
-    sha256 x86_64_linux:   "d71cb20afdd11e9793b6299e241474f8f4d07174625809f07f228ec06cfeb950"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/freeradius-server"
+    sha256 mojave: "48a111ba651a22906ca3c36102c0e5cc9b67348b666ca5a6b199845a634b5d90"
   end
 
   depends_on "openssl@1.1"
