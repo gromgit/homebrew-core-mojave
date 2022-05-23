771f6d58c70533e3dfc695990f745a74f8b630ca
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:48 +0800

    urlview: no rebuilding required

--- Formula/urlview.rb
+++ Formula/urlview.rb
@@ -21,7 +21,10 @@ class Urlview < Formula
     sha256 cellar: :any_skip_relocation, big_sur:        "590b88c35280f2e37daacd2c510afeda9ff90c38361fa9b113a5925136dbdaa7"
     sha256 cellar: :any_skip_relocation, catalina:       "102860ddd181af6242b7aaae841e39dc05298856e43f4c7d9f8747e6d17ad8d1"
     sha256 cellar: :any_skip_relocation, mojave:         "2c93e736ee4b39f7567afe60fcb06ec2144ca054a819a3406caaa5c330ab4911"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e0a5093183e760ac371ca256f25880a33c0b1cc8d6e9da755745979b35303969"
   end
+
+  uses_from_macos "ncurses"
 
   on_linux do
     depends_on "automake"
@@ -33,20 +36,35 @@ class Urlview < Formula
   end
 
   def install
-    inreplace "urlview.man", "/etc/urlview/url_handler.sh", "open"
+    url_handler = OS.mac? ? "open" : etc/"urlview/url_handler.sh"
+    inreplace "urlview.man", "/etc/urlview/url_handler.sh", url_handler
     inreplace "urlview.c",
       '#define DEFAULT_COMMAND "/etc/urlview/url_handler.sh %s"',
-      '#define DEFAULT_COMMAND "open %s"'
+      %Q(#define DEFAULT_COMMAND "#{url_handler} %s")
 
     man1.mkpath
 
-    if OS.linux?
+    unless OS.mac?
       touch("NEWS") # autoreconf will fail if this file does not exist
       system "autoreconf", "-i"
+
+      # Disable use of librx, since it is not needed on Linux.
+      ENV["CFLAGS"] = "-DHAVE_REGEX_H"
+      (etc/"urlview").install "url_handler.sh"
     end
 
     system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}",
                           "--sysconfdir=#{etc}"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.txt").write <<~EOS
+      https://github.com/Homebrew
+    EOS
+    PTY.spawn("urlview test.txt") do |_r, w, _pid|
+      sleep 1
+      w.write("\cD")
+    end
   end
 end
