42ec1581c742e9c0e2c1210a0d1edbccacfd70ef
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-24 03:04:39 +0800

    htmldoc: update 1.9.16 bottle.

--- Formula/htmldoc.rb
+++ Formula/htmldoc.rb
@@ -1,14 +1,14 @@
 class Htmldoc < Formula
   desc "Convert HTML to PDF or PostScript"
   homepage "https://www.msweet.org/htmldoc/"
-  url "https://github.com/michaelrsweet/htmldoc/archive/v1.9.15.tar.gz"
-  sha256 "9dc88e5a2ce849105933c438bbe54f4383f0d1dadb494f52c6ec941317659431"
+  url "https://github.com/michaelrsweet/htmldoc/archive/v1.9.16.tar.gz"
+  sha256 "f0d19d8be0fd961d07556f85dbea1d95f0d38728a45dc0f2cf92c715e4140542"
   license "GPL-2.0-only"
   head "https://github.com/michaelrsweet/htmldoc.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/htmldoc"
-    sha256 mojave: "79b6fa74bb6a0a4230149ad5064473bc1e1627dd24f96bad0217a7d21dbfc209"
+    sha256 mojave: "79795c3201b03c8064b51ae8319ea573aef884afede8dff8162955ff928e0ac1"
   end
 
   depends_on "pkg-config" => :build
