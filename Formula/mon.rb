edca4e290dc1f3c36be3af0fe912fcebeb8c49e9
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:20 +0800

    mon: no rebuilding required

--- Formula/mon.rb
+++ Formula/mon.rb
@@ -1,7 +1,7 @@
 class Mon < Formula
   desc "Monitor hosts/services/whatever and alert about problems"
-  homepage "https://github.com/visionmedia/mon"
-  url "https://github.com/visionmedia/mon/archive/1.2.3.tar.gz"
+  homepage "https://github.com/tj/mon"
+  url "https://github.com/tj/mon/archive/1.2.3.tar.gz"
   sha256 "978711a1d37ede3fc5a05c778a2365ee234b196a44b6c0c69078a6c459e686ac"
   license "MIT"
 
