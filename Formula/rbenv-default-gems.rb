9f43856acc09814313b3a7bb16e795c7a956b364
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:33 +0800

    rbenv-default-gems: no rebuilding required

--- Formula/rbenv-default-gems.rb
+++ Formula/rbenv-default-gems.rb
@@ -1,11 +1,11 @@
 class RbenvDefaultGems < Formula
   desc "Auto-installs gems for Ruby installs"
-  homepage "https://github.com/sstephenson/rbenv-default-gems"
-  url "https://github.com/sstephenson/rbenv-default-gems/archive/v1.0.0.tar.gz"
+  homepage "https://github.com/rbenv/rbenv-default-gems"
+  url "https://github.com/rbenv/rbenv-default-gems/archive/v1.0.0.tar.gz"
   sha256 "8271d58168ab10f0ace285dc4c394e2de8f2d1ccc24032e6ed5924f38dc24822"
   license "MIT"
   revision 1
-  head "https://github.com/sstephenson/rbenv-default-gems.git", branch: "master"
+  head "https://github.com/rbenv/rbenv-default-gems.git", branch: "master"
 
   bottle do
     rebuild 1
@@ -14,9 +14,9 @@ class RbenvDefaultGems < Formula
 
   depends_on "rbenv"
 
-  # Upstream patch: https://github.com/sstephenson/rbenv-default-gems/pull/3
+  # Upstream patch: https://github.com/rbenv/rbenv-default-gems/pull/3
   patch do
-    url "https://github.com/sstephenson/rbenv-default-gems/commit/ead67889c91c53ad967f85f5a89d986fdb98f6fb.patch?full_index=1"
+    url "https://github.com/rbenv/rbenv-default-gems/commit/ead67889c91c53ad967f85f5a89d986fdb98f6fb.patch?full_index=1"
     sha256 "ac6a5654c11d3ef74a97029ed86b8a7b6ae75f4ca7ff4d56df3fb35e7ae0acb8"
   end
 
