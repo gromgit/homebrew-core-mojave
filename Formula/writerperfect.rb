3182a22bd0638e6f65a2d81891be1e72e5871160
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:53 +0800

    writerperfect: no rebuilding required

--- Formula/writerperfect.rb
+++ Formula/writerperfect.rb
@@ -11,13 +11,14 @@ class Writerperfect < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "00f00e38b26c51048a374d8becec3fbc1c1b1c05105710761575ae4906194920"
-    sha256 cellar: :any, big_sur:       "434788af114e54153fe89b17ff3c0ddfd879ffd0e59ac822821ede791a33d145"
-    sha256 cellar: :any, catalina:      "d9a391e73e78b29ced39f355c8d52fbba4198af66c578b9d41257422a969cd17"
-    sha256 cellar: :any, mojave:        "5e8658459f44238800ff490331d50aa6a71b48115157893c78901a4441a34dd3"
-    sha256 cellar: :any, high_sierra:   "12f30a1f15f5887da7675026656f59dd74b7fbffdf4129a2c1778578dbf4cc4a"
-    sha256 cellar: :any, sierra:        "36981e968c146d8aeca47d96327b3f3e909a3f58ca15bed17202e93fef6e17db"
-    sha256 cellar: :any, el_capitan:    "549f41525d1a5cf4cad493650ea0f8daae0208246f36984d6a56a4af533fc881"
+    sha256 cellar: :any,                 arm64_big_sur: "00f00e38b26c51048a374d8becec3fbc1c1b1c05105710761575ae4906194920"
+    sha256 cellar: :any,                 big_sur:       "434788af114e54153fe89b17ff3c0ddfd879ffd0e59ac822821ede791a33d145"
+    sha256 cellar: :any,                 catalina:      "d9a391e73e78b29ced39f355c8d52fbba4198af66c578b9d41257422a969cd17"
+    sha256 cellar: :any,                 mojave:        "5e8658459f44238800ff490331d50aa6a71b48115157893c78901a4441a34dd3"
+    sha256 cellar: :any,                 high_sierra:   "12f30a1f15f5887da7675026656f59dd74b7fbffdf4129a2c1778578dbf4cc4a"
+    sha256 cellar: :any,                 sierra:        "36981e968c146d8aeca47d96327b3f3e909a3f58ca15bed17202e93fef6e17db"
+    sha256 cellar: :any,                 el_capitan:    "549f41525d1a5cf4cad493650ea0f8daae0208246f36984d6a56a4af533fc881"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8ba99fa8423726871a3437205306e9cec03cc3715ed8630f1e29acd7ae74be14"
   end
 
   depends_on "boost" => :build
