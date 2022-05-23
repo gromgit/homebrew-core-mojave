563e680410b43ce7f0916d5f22b638db724ea4ed
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 08:11:49 +0800

    vgmstream: update r1745 bottle.

--- Formula/vgmstream.rb
+++ Formula/vgmstream.rb
@@ -1,26 +1,13 @@
 class Vgmstream < Formula
   desc "Library for playing streamed audio formats from video games"
   homepage "https://vgmstream.org"
-  version "r1702"
+  url "https://github.com/vgmstream/vgmstream.git",
+      tag:      "r1745",
+      revision: "72eb0e6b6fe2863be24650414a34ad504f56f2f8"
+  version "r1745"
   license "ISC"
   version_scheme 1
   head "https://github.com/vgmstream/vgmstream.git", branch: "master"
-
-  stable do
-    url "https://github.com/vgmstream/vgmstream.git",
-        tag:      "r1702",
-        revision: "a76ac04dcd41f1926a721e626657397bd9656a74"
-
-    # patches for fixing macOS build error, remove them at next release
-    patch do
-      url "https://github.com/vgmstream/vgmstream/commit/04b5a1f098f348c4c438ada85ee86f5a9abce2ff.patch?full_index=1"
-      sha256 "37d5d9567435eda96bf4e51b5abbac1677391e08daf4e50b1d7edc6996772919"
-    end
-    patch do
-      url "https://github.com/vgmstream/vgmstream/commit/f9230158d953dfb0fb14a2a57052a8460a5d43dd.patch?full_index=1"
-      sha256 "10ff07d7c6f48275af235d720f279ef62d444c88cde78f2fecaf065e120d134d"
-    end
-  end
 
   livecheck do
     url :stable
@@ -30,7 +17,7 @@ class Vgmstream < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/vgmstream"
-    sha256 cellar: :any, mojave: "5be733d8782985f1de85b6b22d9c75975e7a04c1e06dfdfee4c82031a67c4fe7"
+    sha256 cellar: :any, mojave: "ca4670baf39114e2ea9e631aeff6bc7dcc240608c61a4e693fed283737208f4e"
   end
 
   depends_on "autoconf" => :build
