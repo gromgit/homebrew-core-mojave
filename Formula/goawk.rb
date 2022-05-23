d4eabede1d38e0e6e4a0e93f58c3d94a853bcd84
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-24 02:10:26 +0800

    goawk: update 1.18.0 bottle.

--- Formula/goawk.rb
+++ Formula/goawk.rb
@@ -1,13 +1,13 @@
 class Goawk < Formula
   desc "POSIX-compliant AWK interpreter written in Go"
   homepage "https://benhoyt.com/writings/goawk/"
-  url "https://github.com/benhoyt/goawk/archive/refs/tags/v1.17.1.tar.gz"
-  sha256 "43054fbe53922807d97816be802a39a9485c71369eb74f9ed2c2877dcb4d3629"
+  url "https://github.com/benhoyt/goawk/archive/refs/tags/v1.18.0.tar.gz"
+  sha256 "6d90e60364026445b028c6bc3ff9ed3745f87764e4f9b6688cb498d0639a65f0"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/goawk"
-    sha256 cellar: :any_skip_relocation, mojave: "19bc3568f3bbdb2590d6982978ae71491c37da34858119bf1e5041381edf0f21"
+    sha256 cellar: :any_skip_relocation, mojave: "e6e8b966321ba76510172b2342ee367a4e68cd35b5f90a10dcffffcf344f84ab"
   end
 
   depends_on "go" => :build
