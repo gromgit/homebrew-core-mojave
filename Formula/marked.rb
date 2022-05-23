221db3d22e13d9a00ece89e23edbc00bde5a579c
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:16 +0800

    marked: no rebuilding required

--- Formula/marked.rb
+++ Formula/marked.rb
@@ -3,12 +3,12 @@ require "language/node"
 class Marked < Formula
   desc "Markdown parser and compiler built for speed"
   homepage "https://marked.js.org/"
-  url "https://registry.npmjs.org/marked/-/marked-4.0.15.tgz"
-  sha256 "02ea9a9381970aecc55322d0302721d5f2fa84478d4d0ae4dd5c407814a473f5"
+  url "https://registry.npmjs.org/marked/-/marked-4.0.16.tgz"
+  sha256 "ef928319c4a3bb74be5caf0f0d0ffcabae06a0d511f1c9d805fbb4054dc04f09"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "9b2819ac663af17cda1002a55352c2e39229e9e1c59bd092472e1e42cb9897f7"
+    sha256 cellar: :any_skip_relocation, all: "f9c20b418c87e37af6a4f970429f3bb180ad3276055729c7ea18c8be9c5f729a"
   end
 
   depends_on "node"
