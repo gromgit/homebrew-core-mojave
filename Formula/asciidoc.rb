8c6e348127ee8392f53b7e0be2350542b8e86ae1
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 17:27:13 +0800

    asciidoc: update 10.2.0 bottle.

--- Formula/asciidoc.rb
+++ Formula/asciidoc.rb
@@ -3,8 +3,8 @@ class Asciidoc < Formula
 
   desc "Formatter/translator for text files to numerous formats"
   homepage "https://asciidoc-py.github.io/"
-  url "https://files.pythonhosted.org/packages/8f/94/aa5be7907b9dd1733e4e7a1fa0bb737022d6fb13f80a9283520c66435f4d/asciidoc-10.1.4.tar.gz"
-  sha256 "e614fe7cd6ff096d2aea06733ecfb6b15411b2a0f4aef8267964a32c7f7472ac"
+  url "https://files.pythonhosted.org/packages/8a/57/50180e0430fdb552539da9b5f96f1da6f09c4bfa951b39a6e1b4fbe37d75/asciidoc-10.2.0.tar.gz"
+  sha256 "91ff1dd4c85af7b235d03e0860f0c4e79dd1ff580fb610668a39b5c77b4ccace"
   license "GPL-2.0-only"
   head "https://github.com/asciidoc-py/asciidoc-py.git", branch: "main"
 
@@ -13,9 +13,9 @@ class Asciidoc < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/asciidoc"
-    sha256 cellar: :any_skip_relocation, mojave: "9949c98c7e1f5759790dea9d97065734cf3f8ce9c434eb6cc38d812c523507bc"
+    sha256 cellar: :any_skip_relocation, mojave: "b132c3cc85f2fa12e693fc93fe640c0a3676a85747687a5917a11af7821c8667"
   end
 
   depends_on "docbook"
