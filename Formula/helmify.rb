692fae24e9e12643ec8c84ea0814d294d03a40b8
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-24 02:59:09 +0800

    helmify: update 0.3.12 bottle.

---
+++ Formula/helmify.rb
@@ -0,0 +1,50 @@
+class Helmify < Formula
+  desc "Create Helm chart from Kubernetes yaml"
+  homepage "https://github.com/arttor/helmify"
+  url "https://github.com/arttor/helmify.git",
+      tag:      "v0.3.12",
+      revision: "3634ef55f022db202f123e43e55ebb15161e1892"
+  license "MIT"
+  head "https://github.com/arttor/helmify.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/helmify"
+    sha256 cellar: :any_skip_relocation, mojave: "9ec7652bcb0ac97fdf14aea361bee8d1ec80467ea79db8eeeb78da7e7bc2bb23"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -X main.version=#{version}
+      -X main.date=#{time.iso8601}
+      -X main.commit=#{Utils.git_head}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/helmify"
+  end
+
+  test do
+    test_service = testpath/"service.yml"
+    test_service.write <<~EOS
+      apiVersion: v1
+      kind: Service
+      metadata:
+        name: brew-test
+      spec:
+        type: LoadBalancer
+    EOS
+
+    expected_values_yaml = <<~EOS
+      brewTest:
+        ports: []
+        type: LoadBalancer
+      kubernetesClusterDomain: cluster.local
+    EOS
+
+    system "cat #{test_service} | #{bin}/helmify brewtest"
+    assert_predicate testpath/"brewtest/Chart.yaml", :exist?
+    assert_equal expected_values_yaml, (testpath/"brewtest/values.yaml").read
+
+    assert_match version.to_s, shell_output("#{bin}/helmify --version")
+  end
+end
