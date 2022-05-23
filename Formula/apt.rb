59c814e7fa27029b13c6368e12ae048b098e152f
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:13:45 +0800

    apt:

--- Formula/apt.rb
+++ Formula/apt.rb
@@ -1,8 +1,8 @@
 class Apt < Formula
   desc "Advanced Package Tool"
   homepage "https://wiki.debian.org/Apt"
-  url "https://deb.debian.org/debian/pool/main/a/apt/apt_2.4.5.tar.xz"
-  sha256 "5552f175c3a3924f5cda0c079b821b30f68a2521959f2c30ab164d2ec7993ecf"
+  url "https://deb.debian.org/debian/pool/main/a/apt/apt_2.5.0.tar.xz"
+  sha256 "61cd84d04e2a7858f70dca21160977479c74150853cdeef7c843d59e72488c1f"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -11,7 +11,7 @@ class Apt < Formula
   end
 
   bottle do
-    sha256 x86_64_linux: "f1d85099b51a4ff8bde72d91073f30c93e0b0358e4c569b83eff99f01a49fc8e"
+    sha256 x86_64_linux: "f2b8bb3824e30c4c6374b6d818623d77a78342cb7be2d5322908a12d4e6089a7"
   end
 
   depends_on "cmake" => :build
