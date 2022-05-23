0e0998579bb1d921665cefdc7f52061bccc34be4
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:06 +0800

    jadx: no rebuilding required

--- Formula/jadx.rb
+++ Formula/jadx.rb
@@ -1,12 +1,12 @@
 class Jadx < Formula
   desc "Dex to Java decompiler"
   homepage "https://github.com/skylot/jadx"
-  url "https://github.com/skylot/jadx/releases/download/v1.3.5/jadx-1.3.5.zip"
-  sha256 "2313e6492a85fc2543ee60078b6b970254660ceba140317a5322987b1ee55eb8"
+  url "https://github.com/skylot/jadx/releases/download/v1.4.0/jadx-1.4.0.zip"
+  sha256 "b418619e8a2bf7ce11fd7468ce7f1170fa9a59189dedb08f31fb24d10327cbc9"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "1a13d10c6a88ea8eaf14df488092d3816cb7175ff350a4b61e90d8d4d1bf415f"
+    sha256 cellar: :any_skip_relocation, all: "7f519d9dcc3990969ebd8ac1ff8eea50c4528118ea0e3f4b643b1b4545568391"
   end
 
   head do
