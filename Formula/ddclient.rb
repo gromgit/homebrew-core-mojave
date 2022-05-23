aa100a15eaa59d58b541553188ffadc199c9a436
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:13:53 +0800

    ddclient: no rebuilding required

--- Formula/ddclient.rb
+++ Formula/ddclient.rb
@@ -12,13 +12,24 @@ class Ddclient < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ddclient"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "c2607c69934b6bf9c7ea49dd3536dd799dfac7ea58ae2349958364cdd23001d3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "69edaf36367f1c2d3bccfff16c261caf44bd134e6f06b48a92e6b9f4b550856f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a4d0f7f33c9ddd698673438ca8bb0e9d6ddd5176a57dd5b18756ecc5a204c660"
+    sha256 cellar: :any_skip_relocation, monterey:       "f0b248f41b6b2520a79ef4658d52f5c336529a18cabde9bf77051d8fa4b43736"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cb1d0b3b93fecafb93265cabf1b24c94d8df9e7399a820c4ae72a5dc7d5186f9"
+    sha256 cellar: :any_skip_relocation, catalina:       "d4e32d3e5c88ea3d8b77caccd50e1a291e44934542279289f6e8e13203496214"
+    sha256 cellar: :any_skip_relocation, mojave:         "05482e7ee83ff87306f35898b16748d0c3823d15c8879a4a4f8e6da341d299a5"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "45b2b534058896de6f98ab1da92d8a1f4ab71fc952b97129e94ae074cfa80b91"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5f3f22ad8e138b394ffa78e637637a8b9dc6c9c20a4649b4030fb45705f19171"
   end
 
-
   uses_from_macos "perl"
+
+  on_linux do
+    resource "NetAddr::IP" do
+      url "https://cpan.metacpan.org/authors/id/M/MI/MIKER/NetAddr-IP-4.079.tar.gz"
+      sha256 "ec5a82dfb7028bcd28bb3d569f95d87dd4166cc19867f2184ed3a59f6d6ca0e7"
+    end
+  end
 
   resource "Data::Validate::IP" do
     url "https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/Data-Validate-IP-0.27.tar.gz"
