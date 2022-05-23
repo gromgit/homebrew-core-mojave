20d9d75a4886eb17bb9b08979607658f711164af
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:14 +0800

    lm-sensors:

--- Formula/lm-sensors.rb
+++ Formula/lm-sensors.rb
@@ -1,10 +1,14 @@
 class LmSensors < Formula
   desc "Tools for monitoring the temperatures, voltages, and fans"
-  homepage "https://github.com/groeck/lm-sensors"
+  homepage "https://github.com/lm-sensors/lm-sensors"
   url "https://github.com/lm-sensors/lm-sensors/archive/V3-6-0.tar.gz"
   version "3.6.0"
   sha256 "0591f9fa0339f0d15e75326d0365871c2d4e2ed8aa1ff759b3a55d3734b7d197"
   license any_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
+
+  bottle do
+    sha256 x86_64_linux: "9cab2dd7da319a2eda20dffe27d52292f3e66926fabba8ac1f2204fa0885af3d"
+  end
 
   depends_on "bison" => :build
   depends_on "flex" => :build
