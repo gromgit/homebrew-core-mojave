cc1022838114e5cf0350177592591d7c006fbe2e
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 19:28:19 +0800

    croc: update 9.5.6 bottle.

--- Formula/croc.rb
+++ Formula/croc.rb
@@ -1,14 +1,14 @@
 class Croc < Formula
   desc "Securely send things from one computer to another"
   homepage "https://github.com/schollz/croc"
-  url "https://github.com/schollz/croc/archive/v9.5.5.tar.gz"
-  sha256 "221ebf034e05ef8c06b2e2290dfb83c6f8b4b3f8a9168d6d826b87ed3c62d51a"
+  url "https://github.com/schollz/croc/archive/v9.5.6.tar.gz"
+  sha256 "c03c7b9daf2ba841d373d9c43abb68dc27ab1d7e01bbadead771918d499dea9e"
   license "MIT"
   head "https://github.com/schollz/croc.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/croc"
-    sha256 cellar: :any_skip_relocation, mojave: "18ec6b6b71367c1b884e3a6d116b7212852b608a4c1033456904a302f0aa112b"
+    sha256 cellar: :any_skip_relocation, mojave: "7211a2ae40b4b304b1a6ee6ea1c09bebcbb9d682f4c99127d6167d0e3027909b"
   end
 
   depends_on "go" => :build
