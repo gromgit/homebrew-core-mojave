a4d2f02eccb2f9fd0909122aa3bc6aa8bb543aa1
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:29 +0800

    pdf2svg: no rebuilding required

--- Formula/pdf2svg.rb
+++ Formula/pdf2svg.rb
@@ -1,9 +1,9 @@
 class Pdf2svg < Formula
   desc "PDF converter to SVG"
   homepage "https://cityinthesky.co.uk/opensource/pdf2svg"
-  url "https://github.com/db9052/pdf2svg/archive/v0.2.3.tar.gz"
+  url "https://github.com/dawbarton/pdf2svg/archive/v0.2.3.tar.gz"
   sha256 "4fb186070b3e7d33a51821e3307dce57300a062570d028feccd4e628d50dea8a"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
   revision 6
 
   bottle do
