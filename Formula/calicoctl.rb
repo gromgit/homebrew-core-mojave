583bf00e1a2526db21003df5eb70dedd48e383df
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 18:24:49 +0800

    calicoctl: update 3.23.1 bottle.

--- Formula/calicoctl.rb
+++ Formula/calicoctl.rb
@@ -2,8 +2,8 @@ class Calicoctl < Formula
   desc "Calico CLI tool"
   homepage "https://www.projectcalico.org"
   url "https://github.com/projectcalico/calico.git",
-      tag:      "v3.23.0",
-      revision: "29de1d3f8dd7e86eccb2cde6e6fdff3b5c73afdc"
+      tag:      "v3.23.1",
+      revision: "967e2454314046f6568d1c571e8d97e000540f2d"
   license "Apache-2.0"
   head "https://github.com/projectcalico/calico.git", branch: "master"
 
@@ -14,7 +14,7 @@ class Calicoctl < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/calicoctl"
-    sha256 cellar: :any_skip_relocation, mojave: "340bc4f80267795f5d1ff600db9612d9441f28be3307c103fa02e4e124c9671f"
+    sha256 cellar: :any_skip_relocation, mojave: "63b7b1b82527fd2062a764d8b1b11f4e907336b93b3de88b10839f9056cf9d97"
   end
 
   depends_on "go" => :build
