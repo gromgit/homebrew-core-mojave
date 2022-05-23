e32f8a2ff1700a0fa249ef36351f67cba44b8d3a
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:17 +0800

    metabase: no rebuilding required

--- Formula/metabase.rb
+++ Formula/metabase.rb
@@ -1,8 +1,8 @@
 class Metabase < Formula
   desc "Business intelligence report server"
   homepage "https://www.metabase.com/"
-  url "https://downloads.metabase.com/v0.43.0/metabase.jar"
-  sha256 "7eaa9002c33f9a9c4aad179519b384ee12157693d0e0293a5175bcfd51ad5a00"
+  url "https://downloads.metabase.com/v0.43.1/metabase.jar"
+  sha256 "5866c8b26096b12c6013b2adaf9dfda93b7fa39709ad88b4caedd991f6f1395d"
   license "AGPL-3.0-only"
 
   livecheck do
@@ -11,7 +11,7 @@ class Metabase < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "07ca79a96dd50ec680896957bd68daad7cb8cbcd131806880f958812698617e0"
+    sha256 cellar: :any_skip_relocation, all: "2284b05a6dec2a4a3b4c820438172b0534b34afa979dfbd7a85ae69f256e57d7"
   end
 
   head do
