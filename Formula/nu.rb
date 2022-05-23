cdcd67669b378cc2dce0b2b5a693f111e1cd27bf
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:23 +0800

    nu: no rebuilding required

--- Formula/nu.rb
+++ Formula/nu.rb
@@ -1,7 +1,7 @@
 class Nu < Formula
   desc "Object-oriented, Lisp-like programming language"
   homepage "https://programming.nu/"
-  url "https://github.com/nulang/nu/archive/v2.3.0.tar.gz"
+  url "https://github.com/programming-nu/nu/archive/v2.3.0.tar.gz"
   sha256 "1a6839c1f45aff10797dd4ce5498edaf2f04c415b3c28cd06a7e0697d6133342"
   license "Apache-2.0"
 
