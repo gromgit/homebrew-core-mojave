3c5d6626e199905ee239726443b44075fe04078d
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-23 10:14:47 +0800

    typedb: no rebuilding required

--- Formula/typedb.rb
+++ Formula/typedb.rb
@@ -1,12 +1,12 @@
 class Typedb < Formula
   desc "Distributed hyper-relational database for knowledge engineering"
   homepage "https://vaticle.com/"
-  url "https://github.com/vaticle/typedb/releases/download/2.8.1/typedb-all-mac-2.8.1.zip"
-  sha256 "458a004f4a30f1faf0af737ee1fdb4d789deb10f308e83a17094099eebbedd58"
+  url "https://github.com/vaticle/typedb/releases/download/2.9.0/typedb-all-mac-2.9.0.zip"
+  sha256 "3c8a57e63cb83d0a1862cd41e1334774114ce3f67b4d21d0dd388dcbce873754"
   license "AGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "1be74c2f0c447522b3bbab166b1503bd34ce5dddfd2c2b39d34a8986bb1282e5"
+    sha256 cellar: :any_skip_relocation, all: "19b374746c6c06cd07a2a227c61c016b0e26a344f0d88596129f3172e624df94"
   end
 
   depends_on "openjdk@11"
