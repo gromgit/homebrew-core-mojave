89c360d54d6174fb10a803ca0d26a2b79122bd9a
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-24 10:20:17 +0800

    pacparser: update 1.4.0 bottle.

--- Formula/pacparser.rb
+++ Formula/pacparser.rb
@@ -1,19 +1,20 @@
 class Pacparser < Formula
   desc "Library to parse proxy auto-config (PAC) files"
-  homepage "https://github.com/pacparser/pacparser"
-  url "https://github.com/pacparser/pacparser/archive/v1.4.0.tar.gz"
+  homepage "https://github.com/manugarg/pacparser"
+  url "https://github.com/manugarg/pacparser/archive/v1.4.0.tar.gz"
   sha256 "d62d30aa6e2b4ccdf6773fc30a8b90d1d64eb6ad8edcbf56d2b803e913dcddbb"
   license "LGPL-3.0-or-later"
-  head "https://github.com/pacparser/pacparser.git", branch: "master"
+  head "https://github.com/manugarg/pacparser.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pacparser"
-    sha256 cellar: :any, mojave: "22fef30d2df1998d012a09ddcc4eca52cbf6c6e41b40f257a3a15749955ff7ef"
+    rebuild 1
+    sha256 cellar: :any, mojave: "6688910c11d07d119481d1c029e3e5428a89aee18a522ed976d7537f300a91c2"
   end
 
   def install
     # Disable parallel build due to upstream concurrency issue.
-    # https://github.com/pacparser/pacparser/issues/27
+    # https://github.com/manugarg/pacparser/issues/27
     ENV.deparallelize
     ENV["VERSION"] = version
     Dir.chdir "src"
