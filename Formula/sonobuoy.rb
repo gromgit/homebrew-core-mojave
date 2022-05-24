8187b5fe515d871d5d89c1ebd3554e1bb8bd7c30
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-24 12:21:36 +0800

    sonobuoy: update 0.56.6 bottle.

--- Formula/sonobuoy.rb
+++ Formula/sonobuoy.rb
@@ -1,13 +1,13 @@
 class Sonobuoy < Formula
   desc "Kubernetes component that generates reports on cluster conformance"
   homepage "https://github.com/vmware-tanzu/sonobuoy"
-  url "https://github.com/vmware-tanzu/sonobuoy/archive/v0.56.5.tar.gz"
-  sha256 "752e7a871f182a0ef05467e644961d74eba5110d35a7f6fd3b57445fe55e6965"
+  url "https://github.com/vmware-tanzu/sonobuoy/archive/v0.56.6.tar.gz"
+  sha256 "d2faffe1d68bcc554d201af8d763a2a820c0ed2df8a73732f5996475b94c05a7"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sonobuoy"
-    sha256 cellar: :any_skip_relocation, mojave: "b20a5075e756f86b51d3fa2ba655668bac5ad772ee047bb678053840dc3ce923"
+    sha256 cellar: :any_skip_relocation, mojave: "254bb5ec8d5b96d5b389833be0c7cf11066d271b84f4931953fd1bb5e2c37fd0"
   end
 
   # Segfaults on Go 1.18 - try test it again when updating this formula.
