1c995118099158a820cfec840886612c336aba5f
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-24 11:12:28 +0800

    quran: update 1.0.1 bottle.

--- Formula/quran.rb
+++ Formula/quran.rb
@@ -2,13 +2,13 @@ class Quran < Formula
   desc "Print Qur'an chapters and verses right in the terminal"
   homepage "https://git.hanabi.in/quran-go"
   url "https://git.hanabi.in/repos/quran-go.git",
-      tag:      "v1.0.0",
-      revision: "2558e37fc5be4904a963cea119bb6c836217c27b"
+      tag:      "v1.0.1",
+      revision: "c0e271a69a2e817bf75c8ad79a1fc93aa1b868c9"
   license "AGPL-3.0-only"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/quran"
-    sha256 cellar: :any_skip_relocation, mojave: "c7af1be0577acbb6613595023847746375a224c91f2bbc50a709cbd4c3bb7d28"
+    sha256 cellar: :any_skip_relocation, mojave: "19b17a2de22c1967f8eb593280a0536521883cd2425621a2bd29e8ec03ab953e"
   end
 
   depends_on "go" => :build
