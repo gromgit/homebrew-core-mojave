0008f039eedf1f2c383703ef37394ccd2493ec56
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-24 11:39:28 +0800

    shyaml: update 0.6.2_2 bottle.

--- Formula/shyaml.rb
+++ Formula/shyaml.rb
@@ -6,25 +6,20 @@ class Shyaml < Formula
   url "https://files.pythonhosted.org/packages/b9/59/7e6873fa73a476de053041d26d112b65d7e1e480b88a93b4baa77197bd04/shyaml-0.6.2.tar.gz"
   sha256 "696e94f1c49d496efa58e09b49c099f5ebba7e24b5abe334f15e9759740b7fd0"
   license "BSD-2-Clause"
-  revision 1
+  revision 2
   head "https://github.com/0k/shyaml.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9758d2528c7868f37da2288575d41fedd68ef58a3b94e67ef1d5f0d4887d64f0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9758d2528c7868f37da2288575d41fedd68ef58a3b94e67ef1d5f0d4887d64f0"
-    sha256 cellar: :any_skip_relocation, monterey:       "8929db2d65a2d8654dd55b1ea5baaa619a730887517d31097e99d47b2ec0bacd"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8929db2d65a2d8654dd55b1ea5baaa619a730887517d31097e99d47b2ec0bacd"
-    sha256 cellar: :any_skip_relocation, catalina:       "8929db2d65a2d8654dd55b1ea5baaa619a730887517d31097e99d47b2ec0bacd"
-    sha256 cellar: :any_skip_relocation, mojave:         "8929db2d65a2d8654dd55b1ea5baaa619a730887517d31097e99d47b2ec0bacd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f72387bb319b260214648632cd847356ca753e0193d8c7a0ce3f53ac5eed3f86"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/shyaml"
+    sha256 cellar: :any, mojave: "dcf896bf387cfd72b48578ad4d9f240126b351337d2a014ec5bc816ade7d9666"
   end
 
   depends_on "libyaml"
   depends_on "python@3.10"
 
   resource "PyYAML" do
-    url "https://files.pythonhosted.org/packages/64/c2/b80047c7ac2478f9501676c988a5411ed5572f35d1beff9cae07d321512c/PyYAML-5.3.1.tar.gz"
-    sha256 "b8eac752c5e14d3eca0e6dd9199cd627518cb5ec06add0de9d32baeee6fe645d"
+    url "https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz"
+    sha256 "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
   end
 
   def install
