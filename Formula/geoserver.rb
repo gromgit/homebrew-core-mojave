c42b45f9e89f502258930d6e3f770e2e8308db56
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-25 14:39:18 +0800

    geoserver: no rebuilding required

--- Formula/geoserver.rb
+++ Formula/geoserver.rb
@@ -1,8 +1,8 @@
 class Geoserver < Formula
   desc "Java server to share and edit geospatial data"
   homepage "https://geoserver.org/"
-  url "https://downloads.sourceforge.net/project/geoserver/GeoServer/2.20.4/geoserver-2.20.4-bin.zip"
-  sha256 "418ef109c051d997d75d86a6fe51ee5280b87fabdb73cd96d3210f2d43a79b64"
+  url "https://downloads.sourceforge.net/project/geoserver/GeoServer/2.21.0/geoserver-2.21.0-bin.zip"
+  sha256 "3046735b35da964ba60835cb49cc509cf22f93e593737cb8680442d15a692079"
 
   # GeoServer releases contain a large number of files for each version, so the
   # SourceForge RSS feed may only contain the most recent version (which may
@@ -15,7 +15,7 @@ class Geoserver < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "5309f664f62a28dd177bc1ab63c3e59f6ff47f9dc7242a277d4c9abd66d514b0"
+    sha256 cellar: :any_skip_relocation, all: "e44ff92908e2381d1bf3b15738adeefa3ec541f271c5067ccbbfaf968c5a1b1f"
   end
 
   def install
