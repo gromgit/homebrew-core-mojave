c297e50a9af225a57c182c816fb2f8ddd344317d
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-25 14:39:24 +0800

    rfcmarkup: no rebuilding required

--- Formula/rfcmarkup.rb
+++ Formula/rfcmarkup.rb
@@ -5,11 +5,6 @@ class Rfcmarkup < Formula
   sha256 "369d1b1e6ed27930150b7b0e51a5fc4e068a8980c59924abc0ece10758c6cfd7"
   license "GPL-2.0-or-later"
 
-  livecheck do
-    url :homepage
-    regex(%r{>\s*Version:\s*</i>\s*v?(\d+(?:\.\d+)+)}im)
-  end
-
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "0d97d7f0dd108bd9402c9bb0a22532f1f4f16dd8ebdd2004d865ced971d899ad"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0d97d7f0dd108bd9402c9bb0a22532f1f4f16dd8ebdd2004d865ced971d899ad"
