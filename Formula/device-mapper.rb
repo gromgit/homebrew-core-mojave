825e31a790b4fd2bca660b410ba45124695d3790
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-25 14:39:12 +0800

    device-mapper:

--- Formula/device-mapper.rb
+++ Formula/device-mapper.rb
@@ -2,8 +2,8 @@ class DeviceMapper < Formula
   desc "Userspace library and tools for logical volume management"
   homepage "https://sourceware.org/dm"
   url "https://sourceware.org/git/lvm2.git",
-      tag:      "v2_03_15",
-      revision: "6987f318ec135ffd54a05872a8aa621726fe7acb"
+      tag:      "v2_03_16",
+      revision: "6d1e894a867681af7a811ed63bc435f3aa55fec8"
   license "LGPL-2.1-only"
 
   livecheck do
@@ -13,7 +13,7 @@ class DeviceMapper < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "97dae8f1149a52afaa5d3caba930b3bc8bf2e0bca1234f83685d06e8bb4bf005"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "25c1ab99beb78513619126fafd0b11dcfaf81654520406a767e54e7b88312981"
   end
 
   depends_on "libaio"
