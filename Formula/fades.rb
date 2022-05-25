e62a0024bcfc57608d2b0eb3e7089cbc33305931
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-25 14:39:17 +0800

    fades: no rebuilding required

--- Formula/fades.rb
+++ Formula/fades.rb
@@ -1,22 +1,21 @@
 class Fades < Formula
   desc "Automatically handle virtualenvs for python scripts"
   homepage "https://fades.readthedocs.io/"
-  url "https://files.pythonhosted.org/packages/cd/b0/381b14139b36dcbd317349ce7c2bd2e2a66bfc772d13e568d71f3d98d977/fades-9.0.tar.gz"
-  sha256 "77192b76efbd08dfabce65fe6012805a2383ec1b893c12091efe35fbfd9677f6"
-  license "GPL-3.0"
-  revision 2
-  head "https://github.com/PyAr/fades.git"
+  url "https://files.pythonhosted.org/packages/8b/e8/87a44f1c33c41d1ad6ee6c0b87e957bf47150eb12e9f62cc90fdb6bf8669/fades-9.0.2.tar.gz"
+  sha256 "4a2212f48c4c377bbe4da376c4459fe2d79aea2e813f0cb60d9b9fdf43d205cc"
+  license "GPL-3.0-only"
+  head "https://github.com/PyAr/fades.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "17c25515247ef1c89e14618c1a156b0d9e4dfafd61f9dbd2b93aee1c07bc030e"
+    sha256 cellar: :any_skip_relocation, all: "51586193401133346c21af2e39a4560e7d7e2e375eaeb0aa0d036a79f824ebec"
   end
 
   depends_on "python@3.10"
 
   def install
-    pyver = Language::Python.major_minor_version "python3"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{pyver}/site-packages"
-    system "python3", *Language::Python.setup_install_args(libexec)
+    site_packages = libexec/Language::Python.site_packages("python3")
+    ENV.prepend_create_path "PYTHONPATH", site_packages
+    system "python3", *Language::Python.setup_install_args(libexec), "--install-lib=#{site_packages}"
 
     bin.install Dir[libexec/"bin/*"]
     bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
