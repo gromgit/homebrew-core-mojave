21919fc5e1d4e9908aa18163fd9f1adb17a37876
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-26 11:54:44 +0800

    plow: update 1.2.0 bottle.

--- Formula/plow.rb
+++ Formula/plow.rb
@@ -1,18 +1,13 @@
 class Plow < Formula
   desc "High-performance and real-time metrics displaying HTTP benchmarking tool"
   homepage "https://github.com/six-ddc/plow"
-  url "https://github.com/six-ddc/plow/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "aa579bfa7fee552d84723b6f49d7851759bfd2ff15c9a5d0f216c11a838472a8"
+  url "https://github.com/six-ddc/plow/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "bd57418d6842ae79a675ede027cd986d1e719edb163febfaec812d1a7cde4304"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "efe290ac3d86bec107df0e22c15f4cbdf960dc88df929b4a7b578440b3412389"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eb13e15c3cb95c4e9ae36c498f5eafbbe2401bba7846e3eef7764e56927bafb2"
-    sha256 cellar: :any_skip_relocation, monterey:       "f42f7ff80afff18956562d7c1d98a09571efb50951fe88e8363d2a16caa74dd0"
-    sha256 cellar: :any_skip_relocation, big_sur:        "35efcee910b6391bcb00a74e174bd2c7961742c75b7ce5d81cf739f5f8907e41"
-    sha256 cellar: :any_skip_relocation, catalina:       "d30cae49b41472ed81c454b2e5ef2031e1e46307f8b838b4892fedd80c41d318"
-    sha256 cellar: :any_skip_relocation, mojave:         "beb6ba83885124eb4ba8e2e4bb5bcaf8951bb574ef8d9b94c2c48307b92caff6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "528c3066e659d894685009cc237c87d3dabca4a64c5a0d2e9e2fc8d021ecdbcf"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/plow"
+    sha256 cellar: :any_skip_relocation, mojave: "af07b6bd0d20f7cce797f6b30df72591d081432ea17d3c85ed3cc8e5f8bc6f48"
   end
 
   depends_on "go" => :build
