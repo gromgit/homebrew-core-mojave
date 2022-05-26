b3d15825704e25bd74ebd8955f9bd71270cae746
Adrian Ho <the.gromgit@gmail.com>
2022-05-27 07:18:36 +0800

    cargo-edit: update 0.9.1 bottle.

--- Formula/cargo-edit.rb
+++ Formula/cargo-edit.rb
@@ -1,13 +1,13 @@
 class CargoEdit < Formula
   desc "Utility for managing cargo dependencies from the command-line"
   homepage "https://killercup.github.io/cargo-edit/"
-  url "https://github.com/killercup/cargo-edit/archive/v0.9.0.tar.gz"
-  sha256 "96c231b30339b1a05444c8faa036be84ecbb7f8eaead95c77de9a05f0c190b64"
+  url "https://github.com/killercup/cargo-edit/archive/v0.9.1.tar.gz"
+  sha256 "bae2a59dcf6110fe0c8bf8562e58d550b2b3b3a02e89b233af5a3be12d41cdf0"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cargo-edit"
-    sha256 cellar: :any, mojave: "5b784c1b5550b4a504b276cbd7dfe1c5c0ee9f9ae217232b15b2eed92a2452cb"
+    sha256 cellar: :any, mojave: "979e97c84b57157c8778d4d0d8ca7c30f14f6a6c07c5ab2ba508461a5a9d89cb"
   end
 
   depends_on "libgit2"
