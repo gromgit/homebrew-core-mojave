fee071da41ed5603c14dda7b6a1a726f6fd9a5b8
Adrian Ho <the.gromgit@gmail.com>
2022-05-26 21:49:57 +0800

    akamai: update 1.5.0 bottle.

--- Formula/akamai.rb
+++ Formula/akamai.rb
@@ -1,22 +1,23 @@
 class Akamai < Formula
   desc "CLI toolkit for working with Akamai's APIs"
   homepage "https://github.com/akamai/cli"
-  url "https://github.com/akamai/cli/archive/refs/tags/v1.4.2.tar.gz"
-  sha256 "ffd4954782a3e524b91d743a6f65f7b8514872a2c059009af638571ec7a89fe7"
+  url "https://github.com/akamai/cli/archive/refs/tags/v1.5.0.tar.gz"
+  sha256 "41687c6d4945094a9837dd2966bdd374d8265062d8547235d1c5a136dd08f79e"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/akamai"
-    sha256 cellar: :any_skip_relocation, mojave: "8efb2b8c0745c5bd8b7e6982da0c71834260c5aae22a4d239018d7405f52ef88"
+    sha256 cellar: :any_skip_relocation, mojave: "00e72c516e5916c87a0f77f680823eb7437e940f0fc88186105b7fa4d8b55b05"
   end
 
-  depends_on "go" => :build
+  depends_on "go" => [:build, :test]
 
   def install
     system "go", "build", "-tags", "noautoupgrade nofirstrun", *std_go_args, "cli/main.go"
   end
 
   test do
-    assert_match "Purge", pipe_output("#{bin}/akamai install --force purge", "n")
+    assert_match "diagnostics", shell_output("#{bin}/akamai install diagnostics")
+    system bin/"akamai", "uninstall", "diagnostics"
   end
 end
