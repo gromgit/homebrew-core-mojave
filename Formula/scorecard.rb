a84299e076e399222ea4bf70beee3d1bea3b3b05
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-26 13:26:59 +0800

    scorecard: update 4.3.0 bottle.

--- Formula/scorecard.rb
+++ Formula/scorecard.rb
@@ -2,26 +2,26 @@ class Scorecard < Formula
   desc "Security health metrics for Open Source"
   homepage "https://github.com/ossf/scorecard"
   url "https://github.com/ossf/scorecard.git",
-      tag:      "v4.1.0",
-      revision: "33f80c93dc79f860d874857c511c4d26d399609d"
+      tag:      "v4.3.0",
+      revision: "6406cfd4e34156a5ad04fb4207bef0297f83903c"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/ossf/scorecard.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/scorecard"
-    sha256 cellar: :any_skip_relocation, mojave: "8a063124e27d21ada0d2259cea7c5bef7eb2c85f7118ac496ed4628dde43cfe4"
+    sha256 cellar: :any_skip_relocation, mojave: "ac9bc7187599c3ca8aa5fc7af25e70e7c686b415c64b97b0604e536e6233258d"
   end
 
   depends_on "go" => :build
 
   def install
+    pkg = "sigs.k8s.io/release-utils/version"
     ldflags = %W[
       -s -w
-      -X github.com/ossf/scorecard/v4/pkg.gitVersion=#{version}
-      -X github.com/ossf/scorecard/v4/pkg.gitCommit=#{Utils.git_head}
-      -X github.com/ossf/scorecard/v4/pkg.gitTreeState=clean
-      -X github.com/ossf/scorecard/v4/pkg.buildDate=#{time.iso8601}
+      -X #{pkg}.gitVersion=#{version}
+      -X #{pkg}.gitCommit=#{Utils.git_head}
+      -X #{pkg}.gitTreeState=clean
+      -X #{pkg}.buildDate=#{time.iso8601}
     ]
     system "go", "build", *std_go_args(ldflags: ldflags)
     system "make", "generate-docs"
@@ -34,6 +34,6 @@ class Scorecard < Formula
     expected_output = "InitRepo: repo unreachable: GET https://api.github.com/repos/google/oss-fuzz: 401"
     assert_match expected_output, output
 
-    assert_match version.to_s, shell_output("#{bin}/scorecard version")
+    assert_match version.to_s, shell_output("#{bin}/scorecard version 2>&1")
   end
 end
