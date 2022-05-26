2e9268d739f323c3cf05383efb1340f2c2d97132
Adrian Ho <the.gromgit@gmail.com>
2022-05-27 07:07:50 +0800

    cargo-audit: update 0.17.0 bottle.

--- Formula/cargo-audit.rb
+++ Formula/cargo-audit.rb
@@ -1,8 +1,8 @@
 class CargoAudit < Formula
   desc "Audit Cargo.lock files for crates with security vulnerabilities"
   homepage "https://rustsec.org/"
-  url "https://github.com/RustSec/rustsec/archive/cargo-audit/v0.16.0.tar.gz"
-  sha256 "f0370c87c7a7976387303c7b5cc06651979968358b4f4dc867d30d65fab4d323"
+  url "https://github.com/RustSec/rustsec/archive/cargo-audit/v0.17.0.tar.gz"
+  sha256 "18fc40de40b95adc55e4c4e767f969d62f60bba23805ea9455dd12c1c19f01c0"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/RustSec/rustsec.git", branch: "main"
 
@@ -13,8 +13,7 @@ class CargoAudit < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cargo-audit"
-    rebuild 2
-    sha256 cellar: :any, mojave: "dda2b7350ca295a4097c265f634df265b41773c428d36556718e154617cb1bb8"
+    sha256 cellar: :any, mojave: "ad39f40c460aa802653cc8202fb0f0d5e4e916597f095320a7ae6e75f41edcb5"
   end
 
   depends_on "rust" => :build
@@ -37,7 +36,7 @@ class CargoAudit < Formula
   test do
     output = shell_output("#{bin}/cargo-audit audit 2>&1", 2)
     assert_predicate HOMEBREW_CACHE/"cargo_cache/advisory-db", :exist?
-    assert_match "couldn't open Cargo.lock: No such file or directory", output
+    assert_match "not found: Couldn't load Cargo.lock", output
 
     cp_r "#{pkgshare}/support/base64_vuln/.", testpath
     assert_match "error: 1 vulnerability found!", shell_output("#{bin}/cargo-audit audit 2>&1", 1)
