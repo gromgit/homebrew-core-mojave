c02e0d0213fea6ce7766164845119df02545a8c1
Adrian Ho <aho+vm-mojave@03s.net>
2022-05-26 10:45:04 +0800

    mupdf: update 1.19.1_1 bottle.

--- Formula/mupdf.rb
+++ Formula/mupdf.rb
@@ -1,10 +1,11 @@
 class Mupdf < Formula
   desc "Lightweight PDF and XPS viewer"
   homepage "https://mupdf.com/"
-  url "https://mupdf.com/downloads/archive/mupdf-1.19.0-source.tar.xz"
-  sha256 "38f39943e408d60a3e7d6c2fca0d705163540ca24d65682d4426dc6f1fee28c5"
+  url "https://mupdf.com/downloads/archive/mupdf-1.19.1-source.tar.xz"
+  sha256 "b5eac663fe74f33c430eda342f655cf41fa73d71610f0884768a856a82e3803e"
   license "AGPL-3.0-or-later"
-  head "https://git.ghostscript.com/mupdf.git"
+  revision 1
+  head "https://git.ghostscript.com/mupdf.git", branch: "master"
 
   livecheck do
     url "https://mupdf.com/downloads/archive/"
@@ -12,13 +13,8 @@ class Mupdf < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "505e822fa5a236b30d111b425c7468877b4a4d4f70627e4249c2e50831cefb19"
-    sha256 cellar: :any,                 arm64_big_sur:  "a7c433b107bec1ae16959302f426bbf9eed5144efd3db390bd50a5a993d17029"
-    sha256 cellar: :any,                 monterey:       "cbe4b42ae97e626ca05a7645c4d7ac8cd0d36c73b8328c13b132253c733cbd53"
-    sha256 cellar: :any,                 big_sur:        "d4d5eb0345a58ffb91e4119dc2c1204d0383ce9cf31a9a0e21b8b69be9e34e04"
-    sha256 cellar: :any,                 catalina:       "ce072c384dc4f09671d8b2e531f4bf2a9f6c7a6fb296be86a56d39d3a9d20582"
-    sha256 cellar: :any,                 mojave:         "9d6f5c2e18ac29be5611c6a4f7c00fe39d56ab61f96a85156c351f5bb491bace"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8514c022a9d5f79690aeaa1eac668abfe06c445e7e66382e0887a2674a634de4"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mupdf"
+    sha256 cellar: :any, mojave: "0e7fb66bfc09255fd0a59ab8198b5f981fde8dadb056b57af3b94751bbb601d1"
   end
 
   depends_on "pkg-config" => :build
@@ -33,6 +29,7 @@ class Mupdf < Formula
     glut_libs = `pkg-config --libs glut gl`.chomp
     system "make", "install",
            "build=release",
+           "shared=yes",
            "verbose=yes",
            "CC=#{ENV.cc}",
            "SYS_GLUT_CFLAGS=#{glut_cflags}",
@@ -42,6 +39,8 @@ class Mupdf < Formula
     # Symlink `mutool` as `mudraw` (a popular shortcut for `mutool draw`).
     bin.install_symlink bin/"mutool" => "mudraw"
     man1.install_symlink man1/"mutool.1" => "mudraw.1"
+
+    lib.install_symlink lib/shared_library("libmupdf") => shared_library("libmupdf-third")
   end
 
   test do
