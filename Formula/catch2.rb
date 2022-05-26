6da161ca88805a863ec939e74f1b1d9c525cf684
Adrian Ho <the.gromgit@gmail.com>
2022-05-27 07:21:34 +0800

    catch2: update 3.0.1 bottle.

--- Formula/catch2.rb
+++ Formula/catch2.rb
@@ -1,15 +1,22 @@
 class Catch2 < Formula
   desc "Modern, C++-native, header-only, test framework"
   homepage "https://github.com/catchorg/Catch2"
-  url "https://github.com/catchorg/Catch2/archive/v2.13.9.tar.gz"
-  sha256 "06dbc7620e3b96c2b69d57bf337028bf245a211b3cddb843835bfe258f427a52"
+  url "https://github.com/catchorg/Catch2/archive/v3.0.1.tar.gz"
+  sha256 "8c4173c68ae7da1b5b505194a0c2d6f1b2aef4ec1e3e7463bde451f26bbaf4e7"
   license "BSL-1.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "457e6aad19afaea53718942729f62e17ea7d1a18be2a8a6b4c94cc7fa1d20d30"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/catch2"
+    sha256 cellar: :any_skip_relocation, mojave: "468b1e450758e4b4d476b2d48e27d9464bbe8c3b56d6b33946dc1ca3ca250e44"
   end
 
   depends_on "cmake" => :build
+
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
 
   def install
     mkdir "build" do
@@ -20,8 +27,7 @@ class Catch2 < Formula
 
   test do
     (testpath/"test.cpp").write <<~EOS
-      #define CATCH_CONFIG_MAIN
-      #include <catch2/catch.hpp>
+      #include <catch2/catch_all.hpp>
       TEST_CASE("Basic", "[catch2]") {
         int x = 1;
         SECTION("Test section 1") {
@@ -33,7 +39,7 @@ class Catch2 < Formula
         }
       }
     EOS
-    system ENV.cxx, "test.cpp", "-std=c++11", "-o", "test"
+    system ENV.cxx, "test.cpp", "-std=c++14", "-L#{lib}", "-lCatch2Main", "-lCatch2", "-o", "test"
     system "./test"
   end
 end
