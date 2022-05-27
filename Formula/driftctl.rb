5003c77023b045fcd9a54fa3c456eafbee1e8eaa
Adrian Ho <the.gromgit@gmail.com>
2022-05-27 12:50:51 +0800

    driftctl: update 0.31.0 bottle.

--- Formula/driftctl.rb
+++ Formula/driftctl.rb
@@ -1,13 +1,13 @@
 class Driftctl < Formula
   desc "Detect, track and alert on infrastructure drift"
   homepage "https://driftctl.com"
-  url "https://github.com/snyk/driftctl/archive/v0.30.0.tar.gz"
-  sha256 "ee38f4d642e7b3de1aff92f310137f1abc39ffe133a5580d5826b392698b1aaf"
+  url "https://github.com/snyk/driftctl/archive/v0.31.0.tar.gz"
+  sha256 "e7a1a92d8b06e02308a8db2fdc3cf87a363deb5a52cbcd82911f348c93044ef9"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/driftctl"
-    sha256 cellar: :any_skip_relocation, mojave: "ef0e3f4b8fa6058ae982f4ff959d983c69930503bb45efb332e77924f85608c2"
+    sha256 cellar: :any_skip_relocation, mojave: "39ffe5c0f5f58fcc4430923071c2bf28b0cfff44d08b59394df6485ced2fe0fd"
   end
 
   depends_on "go" => :build
