96e3ccb4abee0617735dddd7a381ccb37897148b
Adrian Ho <the.gromgit@gmail.com>
2022-05-28 07:48:37 +0800

    manticoresearch: update 4.2.0 bottle.

--- Formula/manticoresearch.rb
+++ Formula/manticoresearch.rb
@@ -1,35 +1,54 @@
 class Manticoresearch < Formula
   desc "Open source text search engine"
   homepage "https://www.manticoresearch.com"
-  url "https://repo.manticoresearch.com/repository/manticoresearch_source/release/manticore-3.6.0-210504-96d61d8-release-source.tar.gz"
-  version "3.6.0"
-  sha256 "320a19c837caf827a75e19e11755a9586487435aeb8b8aa80e8bef552fd5e1f5"
+  url "https://github.com/manticoresoftware/manticoresearch/archive/refs/tags/4.2.0.tar.gz"
+  sha256 "6b4af70fcc56b40aa83e606240b237e47e54c0bfbfdd32c47788d59469ef7146"
   license "GPL-2.0-only"
   version_scheme 1
   head "https://github.com/manticoresoftware/manticoresearch.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/manticoresearch"
-    rebuild 2
-    sha256 mojave: "cc4cc10e62e5509a2ada6552b6106a1ab91759519d9b3fb9e0df996a645e870c"
+    sha256 mojave: "97f278397b51242fd4bfd9cc3cd5d02949e3f256660f3dc0d48233eb7040f058"
   end
 
   depends_on "boost" => :build
   depends_on "cmake" => :build
-  depends_on "icu4c" => :build
-  depends_on "libpq" => :build
-  depends_on "mysql" => :build
+  depends_on "libpq"
+  depends_on "mysql"
   depends_on "openssl@1.1"
 
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
+
+  on_linux do
+    depends_on "gcc"
+  end
+
   conflicts_with "sphinx", because: "manticoresearch is a fork of sphinx"
+
+  fails_with gcc: "5"
 
   def install
     args = %W[
       -DCMAKE_INSTALL_LOCALSTATEDIR=#{var}
-      -DDISTR_BUILD=macosbrew
       -DBoost_NO_BOOST_CMAKE=ON
+      -DWITH_ICU=OFF
       -DWITH_ODBC=OFF
     ]
+
+    if OS.mac?
+      args << "-DDISTR_BUILD=macosbrew"
+    else
+      args += %W[
+        -DCMAKE_INSTALL_BINDIR=#{bin}
+        -DCMAKE_INSTALL_DATAROOTDIR=#{share}
+        -DCMAKE_INSTALL_INCLUDEDIR=#{include}
+        -DCMAKE_INSTALL_LIBDIR=#{lib}
+        -DCMAKE_INSTALL_MANDIR=#{man}
+        -DCMAKE_INSTALL_SYSCONFDIR=#{etc}
+      ]
+    end
 
     # Disable support for Manticore Columnar Library on ARM (since the library itself doesn't support it as well)
     args << "-DWITH_COLUMNAR=OFF" if Hardware::CPU.arm?
