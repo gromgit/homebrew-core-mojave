881ed6ced3d88e0e8901a6cffc9faa3c8aac63f3
Adrian Ho <the.gromgit@gmail.com>
2022-05-28 03:21:47 +0800

    leaf-proxy: update 0.6.0 bottle.

--- Formula/leaf-proxy.rb
+++ Formula/leaf-proxy.rb
@@ -1,14 +1,14 @@
 class LeafProxy < Formula
   desc "Lightweight and fast proxy utility"
   homepage "https://github.com/eycorsican/leaf"
-  url "https://github.com/eycorsican/leaf/archive/v0.5.0.tar.gz"
-  sha256 "34e2c067a7fc3e03a4eb7f7d24888d8a7895fdfdf35490865560a655eaf90bb8"
+  url "https://github.com/eycorsican/leaf/archive/v0.6.0.tar.gz"
+  sha256 "5b22932e1dea586ead051a09a4c416e538c29c85d1782718e4652415e59884e8"
   license "Apache-2.0"
   head "https://github.com/eycorsican/leaf.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/leaf-proxy"
-    sha256 cellar: :any_skip_relocation, mojave: "589d42df238bd81f64e7c42a41543c847f09be5f67efcddb2bfef761eae1a614"
+    sha256 cellar: :any_skip_relocation, mojave: "ea1cc30cca9f0aa00811f2b81a0ffa1f049030d5f90859a1d8e2d0bcffbe2674"
   end
 
   depends_on "rust" => :build
