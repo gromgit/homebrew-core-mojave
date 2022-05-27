ca01a0003c3f132927788d50c27a9320092c5f55
Adrian Ho <the.gromgit@gmail.com>
2022-05-27 18:00:23 +0800

    fplll: update 5.4.2 bottle.

--- Formula/fplll.rb
+++ Formula/fplll.rb
@@ -1,18 +1,13 @@
 class Fplll < Formula
   desc "Lattice algorithms using floating-point arithmetic"
   homepage "https://github.com/fplll/fplll"
-  url "https://github.com/fplll/fplll/releases/download/5.4.1/fplll-5.4.1.tar.gz"
-  sha256 "7bd887957173aa592091772c1c36f6aa606b3b2ace0d14e2c26c7463dcf2deb7"
+  url "https://github.com/fplll/fplll/releases/download/5.4.2/fplll-5.4.2.tar.gz"
+  sha256 "6e7b1704fd56f29dd9fc1dd719cc1f2df1f58964fd77bbde162f594d7fff7ba5"
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 arm64_monterey: "c1b9a32a5c3bb69d36bcaabc02be954994327c5af0c730754d9fccb8d036990c"
-    sha256 arm64_big_sur:  "377b5606bb5c319ad73824afcc907f946a1c7c60ddb5a0e181d257fcafc5c5ce"
-    sha256 monterey:       "f76e9a6cce440bc8651b8e699067078c7673c371dd85a758909e8333ab1241bd"
-    sha256 big_sur:        "6290a0d579e8ffba1dab159d70f9e10f58d6600cad8564469fe066b24aa8f170"
-    sha256 catalina:       "bf39fa78c92642f571b4514a61ebe43c5aef39ff25d0e4de969cbf6bf323cc11"
-    sha256 mojave:         "2219fe4523fced68e1f9a8038848cf54d0588cc652229bcbf2bf03e696f7b971"
-    sha256 x86_64_linux:   "fca671625e1f741805cc846d31b03a23506180343dcbe3393cb73a61282703df"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fplll"
+    sha256 mojave: "0fd7c4fb0b132c09d0c3773780e33ac72e44721cbf69c155eeb07c9325fd942d"
   end
 
   depends_on "automake" => :build
@@ -39,6 +34,7 @@ class Fplll < Formula
       #include <fplll.h>
       #include <vector>
       #include <stdio.h>
+      using namespace fplll;
       int main(int c, char **v) {
         ZZ_mat<mpz_t> b;
         std::vector<Z_NR<mpz_t>> sol_coord;
