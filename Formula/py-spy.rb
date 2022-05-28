bb17f7d2f27b41239267d883b949873748f299c3
Adrian Ho <the.gromgit@gmail.com>
2022-05-28 19:09:40 +0800

    py-spy: update 0.3.12 bottle.

--- Formula/py-spy.rb
+++ Formula/py-spy.rb
@@ -1,15 +1,14 @@
 class PySpy < Formula
   desc "Sampling profiler for Python programs"
   homepage "https://github.com/benfred/py-spy"
-  url "https://github.com/benfred/py-spy/archive/refs/tags/v0.3.11.tar.gz"
-  sha256 "094cfb80e2c099763453fc39cfa9c46cfa423afa858268c6a7bc0d867763b014"
+  url "https://github.com/benfred/py-spy/archive/refs/tags/v0.3.12.tar.gz"
+  sha256 "6a4b0537e0bf9cf40fc4557931955222a25db01c110309d0b642dd28211bffeb"
   license "MIT"
   head "https://github.com/benfred/py-spy.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/py-spy"
-    rebuild 3
-    sha256 cellar: :any_skip_relocation, mojave: "180ec76b7920da80ad2f269cf23bc51804c747a23771e7c6e8965c68a7f5e412"
+    sha256 cellar: :any_skip_relocation, mojave: "b548a7ae3b327d1391f1f9ebb6aa691271fa40c41ac86fedc2d14dd725d83a5d"
   end
 
   depends_on "rust" => :build
