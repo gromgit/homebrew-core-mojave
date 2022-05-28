571aa462dff4350efea53b43de45194bd8c2fce2
Adrian Ho <the.gromgit@gmail.com>
2022-05-28 17:36:37 +0800

    pickle: update 0.7.9 bottle.

--- Formula/pickle.rb
+++ Formula/pickle.rb
@@ -1,17 +1,13 @@
 class Pickle < Formula
   desc "PHP Extension installer"
   homepage "https://github.com/FriendsOfPHP/pickle"
-  url "https://github.com/FriendsOfPHP/pickle/releases/download/v0.7.7/pickle.phar"
-  sha256 "a1c02bee3a364f1f9bf53a41cb355e450b3df0c89de0e75a20a89e9d7ac7069b"
+  url "https://github.com/FriendsOfPHP/pickle/releases/download/v0.7.9/pickle.phar"
+  sha256 "e3f787deb31862cdf3b301a08a1a3c46e311c8e1cab7a177b70983de87d2d2e9"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0cb6da56e1d2006229fe4faaedfa70c0e3cd6a0ed54d87a3965175a8831a599d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0cb6da56e1d2006229fe4faaedfa70c0e3cd6a0ed54d87a3965175a8831a599d"
-    sha256 cellar: :any_skip_relocation, monterey:       "d32f594be0e7dcbc0d24826ae517081f38fe267f96f7bc2f4e40cb59902ccc05"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d32f594be0e7dcbc0d24826ae517081f38fe267f96f7bc2f4e40cb59902ccc05"
-    sha256 cellar: :any_skip_relocation, catalina:       "d32f594be0e7dcbc0d24826ae517081f38fe267f96f7bc2f4e40cb59902ccc05"
-    sha256 cellar: :any_skip_relocation, mojave:         "d32f594be0e7dcbc0d24826ae517081f38fe267f96f7bc2f4e40cb59902ccc05"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pickle"
+    sha256 cellar: :any_skip_relocation, mojave: "51d6afa3b67522abc34f47ceeaf6b55f5eb8a98276fd1bac69649d66a00b3ecb"
   end
 
   depends_on "php"
