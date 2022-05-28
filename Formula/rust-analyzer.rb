7d6e41058372cc820b8d619e2b3aa4d423695208
Adrian Ho <the.gromgit@gmail.com>
2022-05-28 21:30:03 +0800

    rust-analyzer: update 2022-05-17 bottle.

--- Formula/rust-analyzer.rb
+++ Formula/rust-analyzer.rb
@@ -1,15 +1,15 @@
 class RustAnalyzer < Formula
   desc "Experimental Rust compiler front-end for IDEs"
   homepage "https://rust-analyzer.github.io/"
-  url "https://github.com/rust-analyzer/rust-analyzer.git",
-       tag:      "2022-05-02",
-       revision: "5dce1ff0212e467271c9e895478670c74d847ee9"
-  version "2022-05-02"
+  url "https://github.com/rust-lang/rust-analyzer.git",
+       tag:      "2022-05-17",
+       revision: "7e95c14ea730c6b06f5760c8c92e69b9a6def828"
+  version "2022-05-17"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rust-analyzer"
-    sha256 cellar: :any_skip_relocation, mojave: "08146f6381ddd866184d0a71f8d5e0e5ca43c306457ab16b24594f0e64a37e25"
+    sha256 cellar: :any_skip_relocation, mojave: "d1d6c2ca1c2e32ed5062c5b681c2e4391204db9d30ad95f3a1730b7b9370c831"
   end
 
   depends_on "rust" => :build
@@ -76,17 +76,17 @@ end
 
 __END__
 diff --git a/Cargo.lock b/Cargo.lock
-index 1937b8936..e6f321394 100644
+index 62b2ac86f..bc06c853d 100644
 --- a/Cargo.lock
 +++ b/Cargo.lock
-@@ -1571,9 +1571,9 @@ checksum = "f2dd574626839106c320a323308629dcb1acfc96e32a8cba364ddc61ac23ee83"
- 
+@@ -1546,9 +1546,9 @@ checksum = "f2dd574626839106c320a323308629dcb1acfc96e32a8cba364ddc61ac23ee83"
+
  [[package]]
  name = "smol_str"
--version = "0.1.22"
+-version = "0.1.23"
 +version = "0.1.21"
  source = "registry+https://github.com/rust-lang/crates.io-index"
--checksum = "167ee181c12079444893cec9c8f21b13d6b314af789c9fdb041a0645f11ed9d2"
+-checksum = "7475118a28b7e3a2e157ce0131ba8c5526ea96e90ee601d9f6bb2e286a35ab44"
 +checksum = "61d15c83e300cce35b7c8cd39ff567c1ef42dde6d4a1a38dbdbf9a59902261bd"
  dependencies = [
   "serde",
