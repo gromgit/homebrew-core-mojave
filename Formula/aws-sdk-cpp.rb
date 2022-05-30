77b422e56c0cc70347ff5090407178b3d5192cce
Adrian Ho <the.gromgit@gmail.com>
2022-05-30 16:18:33 +0800

    aws-sdk-cpp: update 1.9.260 bottle.

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -3,14 +3,14 @@ class AwsSdkCpp < Formula
   homepage "https://github.com/aws/aws-sdk-cpp"
   # aws-sdk-cpp should only be updated every 10 releases on multiples of 10
   url "https://github.com/aws/aws-sdk-cpp.git",
-      tag:      "1.9.240",
-      revision: "716c88324bb292042183d52888bf3bd0bbd471ea"
+      tag:      "1.9.260",
+      revision: "c1df81935a08232b853e2d7eae6fd8c70f99d459"
   license "Apache-2.0"
   head "https://github.com/aws/aws-sdk-cpp.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/aws-sdk-cpp"
-    sha256 cellar: :any, mojave: "12772f4f6f550d94b9e7ea6114e5a828a047c431da46f138439239a989972f79"
+    sha256 cellar: :any, mojave: "0d0ad3f7b3b55279f6d756493f1cfa27763603f729fcaccc272cce69a46fcbb2"
   end
 
   depends_on "cmake" => :build
