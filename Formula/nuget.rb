5b91a131aa88a00ab28e1598a4cc9ba40679ed5e
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:38:59 +0800

    nuget: no rebuilding required

--- Formula/nuget.rb
+++ Formula/nuget.rb
@@ -1,8 +1,8 @@
 class Nuget < Formula
   desc "Package manager for Microsoft development platform including .NET"
   homepage "https://www.nuget.org/"
-  url "https://dist.nuget.org/win-x86-commandline/v6.1.0/nuget.exe" # make sure libexec.install below matches case
-  sha256 "432d322b16ffab3fed5d24ab7ea30ae10c555a00339a9bf53060aa4b2aee7925"
+  url "https://dist.nuget.org/win-x86-commandline/v6.2.0/nuget.exe" # make sure libexec.install below matches case
+  sha256 "c89875d937b7faff3ac024c3ceeceaaecbd786c47b33c09dd4129275a405594b"
   license "MIT"
 
   livecheck do
@@ -11,7 +11,7 @@ class Nuget < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "8e0769492789044f1eefbda18a3709aae5b97066d6a9497b787263693a1a438a"
+    sha256 cellar: :any_skip_relocation, all: "f9b975a1367fe0efe64c50053a5767a43e9ad3d095b4cb03c27d8c86e669a0e8"
   end
 
   depends_on "mono"
