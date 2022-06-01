5faf31ebb934f70f7961e4a10548fc0e39b8372c
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:38:45 +0800

    heppdt2: no rebuilding required

--- Formula/heppdt2.rb
+++ Formula/heppdt2.rb
@@ -6,11 +6,13 @@ class Heppdt2 < Formula
   license "AFL-3.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "2b0f2ffc2837fee3dbf5aea96b1a7329c574373578548986c95cdf50b7f0171a"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ad2b96b10116b7be43a7fb93f0dd76346c904b9fc281983cbb954dd21674499e"
-    sha256 cellar: :any_skip_relocation, catalina:      "5cefdebab8dadafb0c96b121eab2dc0abf0cf6b2f7ff5acdd4d67353c05c275b"
-    sha256 cellar: :any_skip_relocation, mojave:        "2c0ccad4374004249fc7431acaa358d0b04d096526e6447ec0c10cdc3b4759af"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a9fd6f6dc87942bb05ce6105a4038ce7c824c25636a0b110b33bdb9b25cb15a"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e1edccaf839fb239d8d8b583b03537bb8789f939afa610df5b806f2ff15243d5"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2b0f2ffc2837fee3dbf5aea96b1a7329c574373578548986c95cdf50b7f0171a"
+    sha256 cellar: :any_skip_relocation, monterey:       "d41f92b1fe64fe52327e3a3f80d04acdc449a9c9d40d9a75dba40700382f51a9"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ad2b96b10116b7be43a7fb93f0dd76346c904b9fc281983cbb954dd21674499e"
+    sha256 cellar: :any_skip_relocation, catalina:       "5cefdebab8dadafb0c96b121eab2dc0abf0cf6b2f7ff5acdd4d67353c05c275b"
+    sha256 cellar: :any_skip_relocation, mojave:         "2c0ccad4374004249fc7431acaa358d0b04d096526e6447ec0c10cdc3b4759af"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0a9fd6f6dc87942bb05ce6105a4038ce7c824c25636a0b110b33bdb9b25cb15a"
   end
 
   def install
