37760c21e67be70c0ec64941060f920477ee42e8
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:38:57 +0800

    mx: no rebuilding required

--- Formula/mx.rb
+++ Formula/mx.rb
@@ -1,10 +1,9 @@
 class Mx < Formula
   desc "Command-line tool used for the development of Graal projects"
   homepage "https://github.com/graalvm/mx"
-  url "https://github.com/graalvm/mx/archive/refs/tags/5.296.0.tar.gz"
-  sha256 "d6867cdfa80575c70a3e64ada62e8cb22a226e2df6dc4c4bf6db5964bd9a8fde"
+  url "https://github.com/graalvm/mx/archive/refs/tags/6.1.0.tar.gz"
+  sha256 "75892a99840fa71e907f045c73cc7a6e46c0dd4fc3e794d55204c8227b2d1cb9"
   license "GPL-2.0-only"
-  revision 1
 
   livecheck do
     url :stable
@@ -12,16 +11,15 @@ class Mx < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "cceb7b6accacf43b11dbb6a34955cdce563c4d6fed0802ab6c542fad59289550"
+    sha256 cellar: :any_skip_relocation, all: "f982615f5287b81dfcc40ed6b665298a9ca3ce907acef6584e223f81e5a5d212"
   end
 
   depends_on "openjdk" => :test
-  depends_on arch: :x86_64
   depends_on "python@3.10"
 
-  resource("testdata") do
-    url "https://github.com/oracle/graal/archive/refs/tags/vm-21.0.0.2.tar.gz"
-    sha256 "fcb144de48bb280f7d7f6013611509676d9af4bff6607fe7aa73495f16b339b7"
+  resource "homebrew-testdata" do
+    url "https://github.com/oracle/graal/archive/refs/tags/vm-22.1.0.1.tar.gz"
+    sha256 "7653558bc4e4a5f89e5ddef7242ddc1ec5582ec75bdc94997feb76ed12ce8e94"
   end
 
   def install
@@ -30,10 +28,18 @@ class Mx < Formula
     bash_completion.install libexec/"bash_completion/mx" => "mx"
   end
 
+  def post_install
+    # Run a simple `mx` command to create required empty directories inside libexec
+    Dir.mktmpdir do |tmpdir|
+      system bin/"mx", "--user-home", tmpdir, "version"
+    end
+  end
+
   test do
     ENV["JAVA_HOME"] = Language::Java.java_home
+    ENV["MX_ALT_OUTPUT_ROOT"] = testpath
 
-    testpath.install resource("testdata")
+    testpath.install resource("homebrew-testdata")
     cd "vm" do
       output = shell_output("#{bin}/mx suites")
       assert_match "distributions:", output
