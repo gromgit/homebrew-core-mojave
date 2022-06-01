c65122ce45e5b2599f4572bd4cdb6adf84f03cfc
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:38:33 +0800

    dps8m: no rebuilding required

--- Formula/dps8m.rb
+++ Formula/dps8m.rb
@@ -11,11 +11,15 @@ class Dps8m < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dps8m"
-    rebuild 1
-    sha256 cellar: :any, mojave: "d2e2174c87275fd4390afcf386fda3c164dca926aecd4a34687f4a2935862944"
+    sha256 cellar: :any,                 arm64_monterey: "c106049f35d645b200c12f0fc5285b7a6e4fc26074817f9fd96db7f437d777a6"
+    sha256 cellar: :any,                 arm64_big_sur:  "7b6df2db14bb9e099eb1b2003c0ba410e62d4bd81046e425f6db437894938e0c"
+    sha256 cellar: :any,                 monterey:       "96edf4edaa1bce0e86156ded85877536cb36d889f555011709f516f8bcd2eacd"
+    sha256 cellar: :any,                 big_sur:        "a8f615026cb8660c2d7684387b17d1f39e9b93c547a18adf125a88610d5466d8"
+    sha256 cellar: :any,                 catalina:       "d9d967a0c7dad0b63ea6327102cb5d83345ff6b0bcdbf754398c1a5cdb0b0916"
+    sha256 cellar: :any,                 mojave:         "2c148e6bcd3a83e91b6b327d285bcfbb6490a3f7d8f08c4d904a6b907fbe61cf"
+    sha256 cellar: :any,                 high_sierra:    "600be3242396b61b2e807ed850cd65fc30a4676993c44c5171488954be496ce4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "68382b2ed447d527096d1d3e796233859940e423b3d6263821f6454dee91c52c"
   end
-
 
   depends_on "libuv"
 
