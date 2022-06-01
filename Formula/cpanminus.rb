7bc60bcde010167a579ea97d333d77d820f43314
Adrian Ho <the.gromgit@gmail.com>
2022-06-02 00:13:27 +0800

    cpanminus: update 1.7046 bottle.

--- Formula/cpanminus.rb
+++ Formula/cpanminus.rb
@@ -1,30 +1,31 @@
 class Cpanminus < Formula
   desc "Get, unpack, build, and install modules from CPAN"
   homepage "https://github.com/miyagawa/cpanminus"
-  url "https://github.com/miyagawa/cpanminus/archive/1.9019.tar.gz"
-  sha256 "d0a37547a3c4b6dbd3806e194cd6cf4632158ebed44d740ac023e0739538fb46"
+  # Don't use git tags, their naming is misleading
+  url "https://cpan.metacpan.org/authors/id/M/MI/MIYAGAWA/App-cpanminus-1.7046.tar.gz"
+  sha256 "3e8c9d9b44a7348f9acc917163dbfc15bd5ea72501492cea3a35b346440ff862"
   license any_of: ["Artistic-1.0-Perl", "GPL-1.0-or-later"]
-  head "https://github.com/miyagawa/cpanminus.git", branch: "devel"
+  version_scheme 1
 
-  livecheck do
-    url :stable
-    regex(/^v?(\d+(?:\.\d+)+)$/i)
-  end
+  head "https://github.com/miyagawa/cpanminus.git", branch: "devel"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cpanminus"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "3441a262c18df83c13a619b171e16b227fee2d7b6c8d4fb5928e6724218211ac"
+    sha256 cellar: :any_skip_relocation, mojave: "4d47cf54e738c85d981baac0f29c949dfab4551eb27cc18da52c3144344ee7c7"
   end
 
+  uses_from_macos "perl"
 
   def install
-    cd "App-cpanminus" do
-      bin.install "cpanm"
-    end
+    cd "App-cpanminus" if build.head?
+
+    system "perl", "Makefile.PL", "INSTALL_BASE=#{prefix}", "INSTALLSITEMAN1DIR=#{man1}",
+                                                            "INSTALLSITEMAN3DIR=#{man3}"
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/cpanm", "Test::More"
+    assert_match "cpan.metacpan.org", stable.url, "Don't use git tags, their naming is misleading"
+    system "#{bin}/cpanm", "--local-lib=#{testpath}/perl5", "Test::More"
   end
 end
