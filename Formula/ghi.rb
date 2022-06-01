ca2458a7e3b6100ee9270ef097b44589f37aa4e5
Adrian Ho <the.gromgit@gmail.com>
2022-06-02 05:46:08 +0800

    ghi: update 1.2.1 bottle.

--- Formula/ghi.rb
+++ Formula/ghi.rb
@@ -1,28 +1,21 @@
 class Ghi < Formula
   desc "Work on GitHub issues on the command-line"
-  homepage "https://github.com/stephencelis/ghi"
-  url "https://github.com/stephencelis/ghi/archive/1.2.0.tar.gz"
-  sha256 "ffc17cfbdc8b88bf208f5f762e62c211bf8fc837f447354ad53cce39b1400671"
+  homepage "https://github.com/drazisil/ghi"
+  url "https://github.com/drazisil/ghi/archive/refs/tags/1.2.1.tar.gz"
+  sha256 "83fbc4918ddf14df77ef06b28922f481747c6f4dc99b865e15d236b1db98c0b8"
   license "MIT"
-  revision 4
-  head "https://github.com/stephencelis/ghi.git", branch: "master"
+  head "https://github.com/drazisil/ghi.git", branch: "master"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ghi"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "2d07813a7b44a6f6ded371b75e1eb61a8962af966777e0e7322eec0df7b8f426"
+    sha256 cellar: :any_skip_relocation, mojave: "9f2de66ccc47b1b1721f3b5558312e58eacf89641eb59b23e16b8e47bbf9915a"
   end
 
   uses_from_macos "ruby"
 
-  resource "multi_json" do
-    url "https://rubygems.org/gems/multi_json-1.12.1.gem"
-    sha256 "b387722b0a31fff619a2682c7011affb5a13fed2cce240c75c5d6ca3e910ecf2"
-  end
-
   resource "pygments.rb" do
-    url "https://rubygems.org/gems/pygments.rb-1.1.2.gem"
-    sha256 "55a5deed9ecba6037ac22bf27191e0073bd460f87291b2f384922e4b0d59511c"
+    url "https://rubygems.org/gems/pygments.rb-2.3.0.gem"
+    sha256 "4c41c8baee10680d808b2fda9b236fe6b2799cd4ce5c15e29b936cf4bf97f510"
   end
 
   def install
