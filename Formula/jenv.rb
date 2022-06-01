5ce7efa8498c3a02039183f5c78c4aa3b99e891d
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:38:48 +0800

    jenv: no rebuilding required

--- Formula/jenv.rb
+++ Formula/jenv.rb
@@ -4,13 +4,16 @@ class Jenv < Formula
   url "https://github.com/jenv/jenv/archive/0.5.4.tar.gz"
   sha256 "15a78dab7310fb487d2c2cad7f69e05d5d797dc13f2d5c9e7d0bbec4ea3f2980"
   license "MIT"
+  revision 1
   head "https://github.com/jenv/jenv.git", branch: "master"
 
-bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jenv"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "b7863016e9d6998334b63544c713a152149eb60185a0993a9470c4540d6736a9"
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "7e42339ed467f521dfd3dd17d78b23fd3504994915fee2b200b8d184ec5d93ea"
   end
+
+  # Upstream ships an unnecessary export shim for fish shell, which breaks scripts
+  # https://github.com/Homebrew/homebrew-core/pull/100234#issuecomment-1111862141
+  patch :DATA
 
   def install
     libexec.install Dir["*"]
@@ -37,3 +40,14 @@ bottle do
     shell_output("eval \"$(#{bin}/jenv init -)\" && jenv versions")
   end
 end
+__END__
+diff --git a/fish/export.fish b/fish/export.fish
+deleted file mode 100644
+index 14dbbec..0000000
+--- a/fish/export.fish
++++ /dev/null
+@@ -1,4 +0,0 @@
+-function export
+-  set arr (echo $argv|tr = \n)
+-  set -gx $arr[1] $arr[2]
+-end
