55f54bd01a01e30c4790976f53a6e4bff54aea32
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:38:20 +0800

    avce00: no rebuilding required

--- Formula/avce00.rb
+++ Formula/avce00.rb
@@ -10,15 +10,17 @@ class Avce00 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9c4e10ddc6cad4b6a7001a697b8318513a09166e70fa6831ed5fdbede1e71d47"
-    sha256 cellar: :any_skip_relocation, big_sur:       "1fdd45d6a401ca88019bbd58cb3afdda23dedf706c4556e87a7cc48b1a3e952a"
-    sha256 cellar: :any_skip_relocation, catalina:      "db71ee14a03d041413530c0974ce7703100dc3259fc0d2ea5a32fadcf7180133"
-    sha256 cellar: :any_skip_relocation, mojave:        "285b4eb74ff189689097df36f62fa4c2a48b68ece7442156a5700b6c36feb743"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "40b26638adfaf290bc07ae792da49106b493ea3109a97c1fac775723a0463ac4"
-    sha256 cellar: :any_skip_relocation, sierra:        "576b5ea62376b42733d56e7bd862522588c16160ac1abb5f382c1c12055248e1"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "45f18e289431af4de0d1e96c1fadd6a056e80907a1650654f8ee0dd1dafab401"
-    sha256 cellar: :any_skip_relocation, yosemite:      "56e15b29411b2947d9a842d91ae713e16566aa59e297e06f7d4de4b301847e66"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ac93d6ed79eb10699125a47c23e8ece19290f75bd186a670dc25396ef32b1f86"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "5d68b4c2b28928e6f435666ae990ecc9f6ad4b082158374c24437c7d18d6a84f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9c4e10ddc6cad4b6a7001a697b8318513a09166e70fa6831ed5fdbede1e71d47"
+    sha256 cellar: :any_skip_relocation, monterey:       "eea33df8503aff86d07c0e10947fa54e57c579755086838aca78da96b00d4dce"
+    sha256 cellar: :any_skip_relocation, big_sur:        "1fdd45d6a401ca88019bbd58cb3afdda23dedf706c4556e87a7cc48b1a3e952a"
+    sha256 cellar: :any_skip_relocation, catalina:       "db71ee14a03d041413530c0974ce7703100dc3259fc0d2ea5a32fadcf7180133"
+    sha256 cellar: :any_skip_relocation, mojave:         "285b4eb74ff189689097df36f62fa4c2a48b68ece7442156a5700b6c36feb743"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "40b26638adfaf290bc07ae792da49106b493ea3109a97c1fac775723a0463ac4"
+    sha256 cellar: :any_skip_relocation, sierra:         "576b5ea62376b42733d56e7bd862522588c16160ac1abb5f382c1c12055248e1"
+    sha256 cellar: :any_skip_relocation, el_capitan:     "45f18e289431af4de0d1e96c1fadd6a056e80907a1650654f8ee0dd1dafab401"
+    sha256 cellar: :any_skip_relocation, yosemite:       "56e15b29411b2947d9a842d91ae713e16566aa59e297e06f7d4de4b301847e66"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ac93d6ed79eb10699125a47c23e8ece19290f75bd186a670dc25396ef32b1f86"
   end
 
   conflicts_with "gdal", because: "both install a cpl_conv.h header"
