a301dc5a8d6572285994ae5500712cf2d71cd305
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:38:49 +0800

    kerl: no rebuilding required

--- Formula/kerl.rb
+++ Formula/kerl.rb
@@ -1,13 +1,13 @@
 class Kerl < Formula
   desc "Easy building and installing of Erlang/OTP instances"
   homepage "https://github.com/kerl/kerl"
-  url "https://github.com/kerl/kerl/archive/2.4.0.tar.gz"
-  sha256 "f6fd684fb6682e901cfc54c5834cc3bf24811ccf29ae97c0a7e1f2d2be66975b"
+  url "https://github.com/kerl/kerl/archive/2.5.0.tar.gz"
+  sha256 "c2aec85632e779e12865fb147d4105204d1aa46519b6f1bfe0ee8f9041f27358"
   license "MIT"
   head "https://github.com/kerl/kerl.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "d2307a33891bcca81d40c51a2994c4c6b13a29b430bdf7091cc727386ef9ca13"
+    sha256 cellar: :any_skip_relocation, all: "c71510065b9fdf6189134235cf24ae5e77213bd0f068318828c8516f6c49c2b7"
   end
 
   def install
