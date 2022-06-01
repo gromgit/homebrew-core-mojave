c62e0184241896cc78c8441d5ba53f98cc74f13b
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:38:37 +0800

    ficy: no rebuilding required

--- Formula/ficy.rb
+++ Formula/ficy.rb
@@ -12,11 +12,15 @@ class Ficy < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ficy"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "2628b38a6f41ef4bbb9684b772d9fd9cd2c6f73111bc2b1bb44cb3b0297b0397"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c25a3ab155e4dea715aa37ef9fc544adc571300c67aab77e93fb66423337052a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "586d692ab7b64ad5805d51280e78ef997bf0ad2ebd1db2ed57ddc05b126f950b"
+    sha256 cellar: :any_skip_relocation, monterey:       "b6eeba4f52cc614eca8a65cae2f346d41e2e0376f27459fdede8e7eb82ef7a12"
+    sha256 cellar: :any_skip_relocation, big_sur:        "eb6228a79e94bd6a6d37ec647da5b0ca1863ba03992d93bed90071858d0be55e"
+    sha256 cellar: :any_skip_relocation, catalina:       "9974dd8c30bcfe482222a8e6f4040c6c5ccb21c7ef6b893dbbf3033f7e5a85ab"
+    sha256 cellar: :any_skip_relocation, mojave:         "01d1a72a131cb19375bc8a068a59759d3207a60c84a4772cd8d52641ae1f8b8e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0b055098e6bef712df9dbcf449f550b5e9b47d9b5ddbda657815f224dd2326bb"
   end
-
 
   def install
     system "make"
