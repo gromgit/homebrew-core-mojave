1c71bac5cd43f048847eca4135dfa3be3fb30208
Adrian Ho <the.gromgit@gmail.com>
2022-06-02 00:42:02 +0800

    ddcctl: update 1 bottle.

--- Formula/ddcctl.rb
+++ Formula/ddcctl.rb
@@ -1,32 +1,16 @@
 class Ddcctl < Formula
   desc "DDC monitor controls (brightness) for Mac OSX command-line"
   homepage "https://github.com/kfix/ddcctl"
-  url "https://github.com/kfix/ddcctl/archive/refs/tags/v0.tar.gz"
-  sha256 "8440f494b3c354d356213698dd113003245acdf667ed3902b0d173070a1a9d1f"
+  url "https://github.com/kfix/ddcctl/archive/refs/tags/v1.tar.gz"
+  sha256 "1b6eddd0bc20594d55d58832f2d2419ee899e74ffc79c389dcdac55617aebb90"
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b482723d37c8aa30089c7bc5f4c10664a7f25e4e0f7819cfe1611781b9b3654f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d948d479e3d967839131c366214016915831fbbbf82f8359839de8a84f787b75"
-    sha256 cellar: :any_skip_relocation, monterey:       "7e3cd50b5f89286e9b06e2006cff8bcb84ec4581b1ff3140909ec097ef671d3c"
-    sha256 cellar: :any_skip_relocation, big_sur:        "de6da4df6f856e2029ecaa89fbaae1009e8bf987f2c96b5ab71ad88f7adc7d40"
-    sha256 cellar: :any_skip_relocation, catalina:       "aa81b0a04e1ac0c6c2c4d9e37a4efd47f00081303c3bbee150da1681d0a1b809"
-    sha256 cellar: :any_skip_relocation, mojave:         "cd30f6623021ae90a9e535f0a178935e21c7ad895154de4ec97fa506a4622a5a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ddcctl"
+    sha256 cellar: :any_skip_relocation, mojave: "a735acd6dec0b3a861e30ee973f0f62cc4f8f2a5d4d5c22c3efafaaeffefcadb"
   end
 
   depends_on :macos
-
-  # Apply 2 upstream commits to fix build failures with Xcode 13.
-  # Remove with next release.
-  patch do
-    url "https://github.com/kfix/ddcctl/commit/d3ab6ecfd649fa8c335cd45d632cfc8ee2069174.patch?full_index=1"
-    sha256 "59fbd6f9e0aefdc65de967c350b9ab353001972bbc1b8fbe1bffb458e81a2700"
-  end
-
-  patch do
-    url "https://github.com/kfix/ddcctl/commit/8395b07150508305ff92ead307f1563163212383.patch?full_index=1"
-    sha256 "11646455bcb08fd3d8daad4e311df39a26936c4bc5da9180474eed33ccc52256"
-  end
 
   def install
     bin.mkpath
