949b3356c2261d34debcfe55fb1f307968e730eb
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:38:51 +0800

    launch_socket_server: no rebuilding required

--- Formula/launch_socket_server.rb
+++ Formula/launch_socket_server.rb
@@ -7,12 +7,17 @@ class LaunchSocketServer < Formula
   head "https://github.com/mistydemeo/launch_socket_server.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/launch_socket_server"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "908804735af02dcef91612ed4f606a489b915fd97f6bc3922cf66e2f4792c382"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "055953e5098f4c8e2c823d58e123eb3a905174c9619791b3fa54de1d671b193d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b95b2bbe04b54feb6b103b5ead74f984160ff920a8914619004b40c1402bceb7"
+    sha256 cellar: :any_skip_relocation, monterey:       "165fd5a279db8b88e586ca82f2c1017946a7675c8f0c158921d9cb48d152e13b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0d490a674d736272ea67f2289d5eb715b5aa48acf5e289a6a8e8327cd4cbc4a4"
+    sha256 cellar: :any_skip_relocation, catalina:       "ff86499103ad1d9d33cdc039e24f065aa08405bda980c9e242c46ed157bc33ff"
+    sha256 cellar: :any_skip_relocation, mojave:         "823d84eddeb72fdabeccdc189bc19269485bfeb23d0a57824cdbf95c92a6ccb8"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "ef58f2afc33d6454282d1e1b92e4d16269885464707ae58079c29514f4cadc60"
   end
 
   depends_on "go" => :build
+  depends_on :macos
 
   def install
     ENV["GOPATH"] = buildpath
@@ -21,46 +26,13 @@ class LaunchSocketServer < Formula
   end
 
   plist_options startup: true
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>KeepAlive</key>
-          <true/>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{opt_sbin}/launch_socket_server</string>
-            <string>-</string>
-          </array>
-          <key>Sockets</key>
-          <dict>
-            <key>Socket</key>
-            <dict>
-              <key>SockNodeName</key>
-              <string>0.0.0.0</string>
-              <key>SockServiceName</key>
-              <string>80</string>
-            </dict>
-          </dict>
-          <key>EnvironmentVariables</key>
-          <dict>
-            <key>LAUNCH_PROGRAM_TCP_ADDRESS</key>
-            <string>127.0.0.1:8080</string>
-          </dict>
-          <key>StandardErrorPath</key>
-          <string>#{var}/log/launch_socket_server.log</string>
-          <key>StandardOutPath</key>
-          <string>#{var}/log/launch_socket_server.log</string>
-        </dict>
-      </plist>
-    EOS
+  service do
+    run [opt_sbin/"launch_socket_server", "-"]
+    environment_variables LAUNCH_PROGRAM_TCP_ADDRESS: "127.0.0.1:8080"
+    keep_alive true
+    error_log_path var/"log/launch_socket_server.log"
+    log_path var/"log/launch_socket_server.log"
+    sockets "tcp://0.0.0.0:80"
   end
 
   test do
