b15e96f210fc0f785907c8044ef4ef221bb00ec2
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:38:34 +0800

    dromeaudio: no rebuilding required

--- Formula/dromeaudio.rb
+++ Formula/dromeaudio.rb
@@ -7,11 +7,16 @@ class Dromeaudio < Formula
   head "https://github.com/joshb/dromeaudio.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dromeaudio"
-    rebuild 3
-    sha256 cellar: :any_skip_relocation, mojave: "a93e191b7f40b61fc3497904cd4a04295af9b841042fcd885699a72e7f5c9f82"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9848eaeb0b335219124e08ff894bedd136c1fa95bcf72a04b69a778305fef5b1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "56127ff9fdb552e5a521d52a9a848ddf1f4a79029740d65f053ba9cc8ab2c7f7"
+    sha256 cellar: :any_skip_relocation, monterey:       "2f3eb4f1d29eb1644181305eae8444526d38f6a557c4c236407dacf7d91a9fcd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ef9ce724d04545c565e1e46f06560128f54c8fd164fdc3d3abca18a4d17ad9b6"
+    sha256 cellar: :any_skip_relocation, catalina:       "5199ecfbb8454f1560685c537b1fbaf1b301b39ad8ea825a9f846cc9f3530f30"
+    sha256 cellar: :any_skip_relocation, mojave:         "062b0fa8e43363d60e5816343d1fcb7f58ce02c236512d96f4bf4ba10c96fd2c"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "1334685c021a520567e2d16bfe68ebddea8f9382a50645e241d09349cfb6b450"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9f070aab40ff55d1bc82cae306c222e05770d9b5cd22fdace7fbb7d04ea7aa6f"
   end
-
 
   depends_on "cmake" => :build
 
