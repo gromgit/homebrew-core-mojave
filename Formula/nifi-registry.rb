e3c6f9f63496522641413ea5af3b0a9706a2e562
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:38:58 +0800

    nifi-registry: no rebuilding required

--- Formula/nifi-registry.rb
+++ Formula/nifi-registry.rb
@@ -1,13 +1,13 @@
 class NifiRegistry < Formula
   desc "Centralized storage & management of NiFi/MiNiFi shared resources"
   homepage "https://nifi.apache.org/registry"
-  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.16.1/nifi-registry-1.16.1-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/nifi/1.16.1/nifi-registry-1.16.1-bin.tar.gz"
-  sha256 "4a46072b2825cd8e897cb5978f6e11ef66aa610a572b85327a45c61f1e5360ed"
+  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.16.2/nifi-registry-1.16.2-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/nifi/1.16.2/nifi-registry-1.16.2-bin.tar.gz"
+  sha256 "dbf46cdcc6fbba96253affc953298b0eb23154af1a8c5b5cdd2c93c6886ba729"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "0373c052f3be692b91f0ecc776cd7db42f42dd6596d14c90f9852f886ee38193"
+    sha256 cellar: :any_skip_relocation, all: "5e28e566f3ec8f0200d7a785df681bd2a71d7a119d45cbad001999992265f513"
   end
 
   depends_on "openjdk"
