2bf6f2a04466fb7f03061d5357c85b659e921e80
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:38:50 +0800

    kyoto-tycoon: no rebuilding required

--- Formula/kyoto-tycoon.rb
+++ Formula/kyoto-tycoon.rb
@@ -12,11 +12,13 @@ class KyotoTycoon < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "244a150072e722f1ee861425fdfd1cb12a6a09ee27899b998b0794bd01cd1f12"
-    sha256 big_sur:       "30c5a805f4e4f672814b210a28567424b23af490a8d9555286dae17ee41506c4"
-    sha256 catalina:      "575c025f8a4479503833b3d90c8054ed3b67e8f4a14a96978ec585a76bbf7963"
-    sha256 mojave:        "aafcc936bd17bade9714e200c0e713ec4cd6ddc8f38a08d258cbf09437adec75"
-    sha256 x86_64_linux:  "bb1a1af50e64ab1cf5d39182e0ea8ef10869ecf4f995fd7b6fc31969dab97c9b"
+    sha256 arm64_monterey: "887e108eab14901d52e4e0a8b6553bc9a4bf8dc04ae7814e0aa25da08492fec4"
+    sha256 arm64_big_sur:  "244a150072e722f1ee861425fdfd1cb12a6a09ee27899b998b0794bd01cd1f12"
+    sha256 monterey:       "01ea2b5572500293e6d2be3fc51d8852a3be3e0a6a0a918f11224a39d5e0d133"
+    sha256 big_sur:        "30c5a805f4e4f672814b210a28567424b23af490a8d9555286dae17ee41506c4"
+    sha256 catalina:       "575c025f8a4479503833b3d90c8054ed3b67e8f4a14a96978ec585a76bbf7963"
+    sha256 mojave:         "aafcc936bd17bade9714e200c0e713ec4cd6ddc8f38a08d258cbf09437adec75"
+    sha256 x86_64_linux:   "bb1a1af50e64ab1cf5d39182e0ea8ef10869ecf4f995fd7b6fc31969dab97c9b"
   end
 
   depends_on "lua" => :build
