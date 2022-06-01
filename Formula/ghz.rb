bb0f5800819839eab0631ef2900f982540aff088
Adrian Ho <the.gromgit@gmail.com>
2022-06-02 05:54:02 +0800

    ghz: update 0.109.0 bottle.

--- Formula/ghz.rb
+++ Formula/ghz.rb
@@ -1,8 +1,8 @@
 class Ghz < Formula
   desc "Simple gRPC benchmarking and load testing tool"
   homepage "https://ghz.sh"
-  url "https://github.com/bojand/ghz/archive/v0.108.0.tar.gz"
-  sha256 "fd3f4f451ead288622ebf122bb52edf18828a34357489edc8446c64b0cc10770"
+  url "https://github.com/bojand/ghz/archive/v0.109.0.tar.gz"
+  sha256 "4b0b3c651861923a60ca3370de652eb9f3eb5b0c7510c877ec1af8d82508fd08"
   license "Apache-2.0"
 
   livecheck do
@@ -12,7 +12,7 @@ class Ghz < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ghz"
-    sha256 cellar: :any_skip_relocation, mojave: "9949a0e2b1e123f3bad683eebfd8790c2c4d53850e282598d51ff86a5fe438cc"
+    sha256 cellar: :any_skip_relocation, mojave: "db677dfd3bfbd93c6060c49fbdd785dde81d5f88fffd1452d6400335236ff08e"
   end
 
   depends_on "go" => :build
