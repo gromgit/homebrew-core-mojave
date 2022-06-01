dbb7399fb9da132a957f4d39e6e84edb0fa10914
Adrian Ho <the.gromgit@gmail.com>
2022-06-02 02:09:50 +0800

    easeprobe: update 1.5.0 bottle.

--- Formula/easeprobe.rb
+++ Formula/easeprobe.rb
@@ -2,14 +2,14 @@ class Easeprobe < Formula
   desc "Simple, standalone, and lightWeight tool that can do health/status checking"
   homepage "https://github.com/megaease/easeprobe"
   url "https://github.com/megaease/easeprobe.git",
-      tag:      "v1.4.0",
-      revision: "8d7c5f749a0fe3d6073f3f010a538bf6ea4edb48"
+      tag:      "v1.5.0",
+      revision: "0a8646bbd05fdcd062e31fe8979abc488e13dec2"
   license "Apache-2.0"
   head "https://github.com/megaease/easeprobe.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/easeprobe"
-    sha256 cellar: :any_skip_relocation, mojave: "9ad4ed31eda9b424d85098ce9bceced9e89a89d9d0bc7c6d43a1d2833368b8f3"
+    sha256 cellar: :any_skip_relocation, mojave: "b275406565dfc5e456938c3522933a4a90c976f70256557285d43646366d2a80"
   end
 
   depends_on "go" => :build
