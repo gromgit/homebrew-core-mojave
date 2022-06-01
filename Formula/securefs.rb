e769be2aee7795fa548b74f77d9dec83363d99d4
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:39:05 +0800

    securefs:

--- Formula/securefs.rb
+++ Formula/securefs.rb
@@ -2,15 +2,13 @@ class Securefs < Formula
   desc "Filesystem with transparent authenticated encryption"
   homepage "https://github.com/netheril96/securefs"
   url "https://github.com/netheril96/securefs.git",
-      tag:      "0.11.1",
-      revision: "dfeebf8406871d020848edde668234715356158c"
+      tag:      "0.13.0",
+      revision: "1705d14b8fef5ebb826a74549d609c6ab6cb63f7"
   license "MIT"
   head "https://github.com/netheril96/securefs.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, catalina:    "8a8c7dd74f9b3082b2b128cc058714a27206d910273e4148959a25b7d30c51b5"
-    sha256 cellar: :any, mojave:      "9bee23af87518f68df56b862ad0ef88f842ebe65f8feff02e90bf5ab8e91522e"
-    sha256 cellar: :any, high_sierra: "632496d8e9ed9fe91d18e9a2c9fef49c920dc091e10108246f8ab2056f75ea38"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "99d70dae414d78c230684f94f42cc2f36c5a8f1934c3eb066b687e199866c421"
   end
 
   depends_on "cmake" => :build
@@ -20,12 +18,13 @@ class Securefs < Formula
   end
 
   on_linux do
-    depends_on "libfuse"
+    depends_on "libfuse@2"
   end
 
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   def caveats
