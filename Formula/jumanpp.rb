d2a20bd1627c1f2bb2aaa637a0e8e63bf7de45f5
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:38:48 +0800

    jumanpp: no rebuilding required

--- Formula/jumanpp.rb
+++ Formula/jumanpp.rb
@@ -11,15 +11,17 @@ class Jumanpp < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "e9b65ec6cbc79b48a403d5a9c30a59eedfe9cb412be6a4d6ccf65f653e96e8d4"
-    sha256 big_sur:       "9c97f442fdad1ae3ab776ef16de98876db768134d50235e9ea683579fa8a85b7"
-    sha256 catalina:      "4b2c208b0954536aa3f2b838a525e2542a547a192a03951c0f8a7f69c082a60d"
-    sha256 mojave:        "248e639859582dcf3613eb4eaf491e5ada0ed527e89595d6ba110219e8f8ab28"
-    sha256 high_sierra:   "87555f0efa065cd86d3344c8304972567d4b8a84ef1c0e53acb09afabbd1fa9e"
-    sha256 sierra:        "afddd3445d86fa1969611b413d0ae460fdaa7b106cdf5edf6ce0bf9d14689a49"
-    sha256 el_capitan:    "4b4dd5ca55ba7d380a9a6bc7dda462c3825aa8650e9dc6b131e53fadbf64dc63"
-    sha256 yosemite:      "d53d25e49f4bd8cddd2657ee09eeaec56844996b10445c15561be1a12977a888"
-    sha256 x86_64_linux:  "a646201910dc5c938adcd64559fc0ce85dd8f13017ddadb1b65965f4cc2f4afe"
+    sha256 arm64_monterey: "c4db4776b58c04151f749e3e4254a437abc02fb0f284a8a1eccfa02e50b6651f"
+    sha256 arm64_big_sur:  "e9b65ec6cbc79b48a403d5a9c30a59eedfe9cb412be6a4d6ccf65f653e96e8d4"
+    sha256 monterey:       "1a49398ad2c6ace6827869ff87bb0b6018e4cbdf5259b79febdb761b6019be30"
+    sha256 big_sur:        "9c97f442fdad1ae3ab776ef16de98876db768134d50235e9ea683579fa8a85b7"
+    sha256 catalina:       "4b2c208b0954536aa3f2b838a525e2542a547a192a03951c0f8a7f69c082a60d"
+    sha256 mojave:         "248e639859582dcf3613eb4eaf491e5ada0ed527e89595d6ba110219e8f8ab28"
+    sha256 high_sierra:    "87555f0efa065cd86d3344c8304972567d4b8a84ef1c0e53acb09afabbd1fa9e"
+    sha256 sierra:         "afddd3445d86fa1969611b413d0ae460fdaa7b106cdf5edf6ce0bf9d14689a49"
+    sha256 el_capitan:     "4b4dd5ca55ba7d380a9a6bc7dda462c3825aa8650e9dc6b131e53fadbf64dc63"
+    sha256 yosemite:       "d53d25e49f4bd8cddd2657ee09eeaec56844996b10445c15561be1a12977a888"
+    sha256 x86_64_linux:   "a646201910dc5c938adcd64559fc0ce85dd8f13017ddadb1b65965f4cc2f4afe"
   end
 
   depends_on "boost-build" => :build
