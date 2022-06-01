aa7aaea892869685b8dbc89ae5e587b962c2c597
Adrian Ho <the.gromgit@gmail.com>
2022-06-02 03:43:19 +0800

    frps: update 0.43.0 bottle.

--- Formula/frps.rb
+++ Formula/frps.rb
@@ -2,13 +2,13 @@ class Frps < Formula
   desc "Server app of fast reverse proxy to expose a local server to the internet"
   homepage "https://github.com/fatedier/frp"
   url "https://github.com/fatedier/frp.git",
-      tag:      "v0.42.0",
-      revision: "eb1e19a8212061e56a43ce798a231cd64cce989d"
+      tag:      "v0.43.0",
+      revision: "fe5fb0326b2aa7741d5b5d8f8c4c3ca12bb4ed91"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/frps"
-    sha256 cellar: :any_skip_relocation, mojave: "3f061ac4e982d2676f9162c597969b1572d6b7771e985da021b6ab93077599b1"
+    sha256 cellar: :any_skip_relocation, mojave: "b5512886fa74dd32583785d26e73f3719234bf2ab15653ddd04a99b49ac4bd34"
   end
 
   depends_on "go" => :build
