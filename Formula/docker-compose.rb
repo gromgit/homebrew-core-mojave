14b92a5e666d4cd690b6f41717bc1efecc3cd2bd
Adrian Ho <the.gromgit@gmail.com>
2022-06-02 01:28:21 +0800

    docker-compose: update 2.6.0 bottle.

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -1,14 +1,14 @@
 class DockerCompose < Formula
   desc "Isolated development environments using Docker"
   homepage "https://docs.docker.com/compose/"
-  url "https://github.com/docker/compose/archive/v2.5.1.tar.gz"
-  sha256 "f3eeaac99d2467fe482f499787ae38f66738bc0641dbf34a6045f34aaab893b7"
+  url "https://github.com/docker/compose/archive/v2.6.0.tar.gz"
+  sha256 "b01b998dbc29478ec989a9df4ebaf4017b7406bba1847b061632f0a7a9841751"
   license "Apache-2.0"
   head "https://github.com/docker/compose.git", branch: "v2"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/docker-compose"
-    sha256 cellar: :any_skip_relocation, mojave: "cee5d897c34c97dfb075f7b54d12a86db8428cf4bbf438d5a5cf6a1b45fdbcf1"
+    sha256 cellar: :any_skip_relocation, mojave: "886de1e16c487da7cf62f0febf3adbea7a21f28aff542b6ec42b956f2d32eb55"
   end
 
   depends_on "go" => :build
