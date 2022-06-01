062812cbf26b6a05a6ee2983d1af275175995b33
Adrian Ho <the.gromgit@gmail.com>
2022-06-02 01:10:13 +0800

    dhall-lsp-server: update 1.1.1 bottle.

--- Formula/dhall-lsp-server.rb
+++ Formula/dhall-lsp-server.rb
@@ -1,15 +1,14 @@
 class DhallLspServer < Formula
   desc "Language Server Protocol (LSP) server for Dhall"
   homepage "https://github.com/dhall-lang/dhall-haskell/tree/master/dhall-lsp-server"
-  url "https://hackage.haskell.org/package/dhall-lsp-server-1.0.18/dhall-lsp-server-1.0.18.tar.gz"
-  sha256 "f27a3132ca04eaff5901cb2d184308b595b6bf7c58b60b4af37aae8509eb8cba"
+  url "https://hackage.haskell.org/package/dhall-lsp-server-1.1.1/dhall-lsp-server-1.1.1.tar.gz"
+  sha256 "86b22a14be0ebe016e29cfd3436e4f9776f8817dfd72a9966437fceb8e608f7c"
   license "BSD-3-Clause"
   head "https://github.com/dhall-lang/dhall-haskell.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dhall-lsp-server"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "2e028a336febb4c35fe7721134c4f75d12e0e347e46de507985fd9be58809c36"
+    sha256 cellar: :any_skip_relocation, mojave: "b85f56de22b030338b6605b9ecb02a5b2b17035dbcc39e42761522e49022fd9a"
   end
 
   depends_on "cabal-install" => :build
@@ -29,7 +28,13 @@ class DhallLspServer < Formula
       "\r\n" \
       "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"initialize\",\"params\":{\"" \
       "processId\":88075,\"rootUri\":null,\"capabilities\":{},\"trace\":\"ver" \
-      "bose\",\"workspaceFolders\":null}}\r\n"
+      "bose\",\"workspaceFolders\":null}}\r\n" \
+      "Content-Length: 46\r\n" \
+      "\r\n" \
+      "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"shutdown\"}\r\n" \
+      "Content-Length: 42\r\n" \
+      "\r\n" \
+      "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"exit\"}\r\n"
 
     output = pipe_output("#{bin}/dhall-lsp-server", input, 0)
 
