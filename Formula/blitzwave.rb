1d4c84ca73757b466044a6b8b1378ed02929f3a9
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:38:23 +0800

    blitzwave: no rebuilding required

--- Formula/blitzwave.rb
+++ Formula/blitzwave.rb
@@ -6,14 +6,16 @@ class Blitzwave < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any,                 big_sur:      "1e65e0e917a0454346801ea361b5186676150edc50f296abcb5f682456375589"
-    sha256 cellar: :any,                 catalina:     "c048a4c11493ddfd5222bbfd25934fe4e7981fb7d689eddaef38ac06fa0d5b2d"
-    sha256 cellar: :any,                 mojave:       "7bd4d442c43a1f5c2a6fbfbf77faa3d90096873a65d90317fa0dad223908b498"
-    sha256 cellar: :any,                 high_sierra:  "5ad4f6c2447b6efdad752ffc05c2d31be8ad1abbe0c6654f77f33141edaf300e"
-    sha256 cellar: :any,                 sierra:       "1722c7dfacc458ca54d05dcc06a5281bbe48935f66eaaf7374c2551ad50298a8"
-    sha256 cellar: :any,                 el_capitan:   "be9ba4deb07a468b23f430fe2f0896206b120f70e07f94d48267448c0524d3bc"
-    sha256 cellar: :any,                 yosemite:     "609c85eec329a8aa988a2b026522642f41b392039936661ce428d13887dfa84d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "353fd7cf26dc62a4441d2a7498a7d947c163dd0994a9bc2d1da6ef8677525b39"
+    sha256 cellar: :any,                 arm64_monterey: "d63c1a6c2d85a04ee6bd22f26895abac9fe84a818220437c80d56dd022826b45"
+    sha256 cellar: :any,                 monterey:       "dcbb61c393f804b7edba2e42caaf7688df8b9693ce4f4956814089f407651c09"
+    sha256 cellar: :any,                 big_sur:        "1e65e0e917a0454346801ea361b5186676150edc50f296abcb5f682456375589"
+    sha256 cellar: :any,                 catalina:       "c048a4c11493ddfd5222bbfd25934fe4e7981fb7d689eddaef38ac06fa0d5b2d"
+    sha256 cellar: :any,                 mojave:         "7bd4d442c43a1f5c2a6fbfbf77faa3d90096873a65d90317fa0dad223908b498"
+    sha256 cellar: :any,                 high_sierra:    "5ad4f6c2447b6efdad752ffc05c2d31be8ad1abbe0c6654f77f33141edaf300e"
+    sha256 cellar: :any,                 sierra:         "1722c7dfacc458ca54d05dcc06a5281bbe48935f66eaaf7374c2551ad50298a8"
+    sha256 cellar: :any,                 el_capitan:     "be9ba4deb07a468b23f430fe2f0896206b120f70e07f94d48267448c0524d3bc"
+    sha256 cellar: :any,                 yosemite:       "609c85eec329a8aa988a2b026522642f41b392039936661ce428d13887dfa84d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "353fd7cf26dc62a4441d2a7498a7d947c163dd0994a9bc2d1da6ef8677525b39"
   end
 
   depends_on "autoconf" => :build
