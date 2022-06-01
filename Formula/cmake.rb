7490f78624ea29f2e2fbcadcb5ddd60a2791e355
Adrian Ho <the.gromgit@gmail.com>
2022-06-01 20:55:50 +0800

    cmake: update 3.23.2 bottle.

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -1,12 +1,11 @@
 class Cmake < Formula
   desc "Cross-platform make"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.23.1/cmake-3.23.1.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/cmake-3.23.1.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.23.1.tar.gz"
-  sha256 "33fd10a8ec687a4d0d5b42473f10459bb92b3ae7def2b745dc10b192760869f3"
+  url "https://github.com/Kitware/CMake/releases/download/v3.23.2/cmake-3.23.2.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/cmake-3.23.2.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.23.2.tar.gz"
+  sha256 "f316b40053466f9a416adf981efda41b160ca859e97f6a484b447ea299ff26aa"
   license "BSD-3-Clause"
-  revision 1
   head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
 
   # The "latest" release on GitHub has been an unstable version before, so we
@@ -18,20 +17,13 @@ class Cmake < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cmake"
-    sha256 cellar: :any_skip_relocation, mojave: "0bd6f6b1d043d48db7da55ede7b03fd622a125c3848d14ad4c75be6f1109970e"
+    sha256 cellar: :any_skip_relocation, mojave: "9d5359e74efa18bfd8630bb1fe23eb48ee02238ce3303510fe3f75a91fd4808d"
   end
 
   uses_from_macos "ncurses"
 
   on_linux do
     depends_on "openssl@1.1"
-  end
-
-  # Tentative workaround for bug with gfortran 12 and clang
-  # https://gitlab.kitware.com/cmake/cmake/-/issues/23500
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/533fd564/cmake/gcc-12.diff"
-    sha256 "f9c7e39c10cf4c88e092da65f2859652529103e364828188e0ae4fef10a18936"
   end
 
   # The completions were removed because of problems with system bash
