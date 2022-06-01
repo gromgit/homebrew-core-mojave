751b4cbcab6ea2ffe5e25d0a058ab4074ef67aff
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-01 16:38:21 +0800

    avfs:

--- Formula/avfs.rb
+++ Formula/avfs.rb
@@ -1,13 +1,11 @@
 class Avfs < Formula
   desc "Virtual file system that facilitates looking inside archives"
   homepage "https://avf.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/avf/avfs/1.1.3/avfs-1.1.3.tar.bz2"
-  sha256 "4f4ec1e8c0d5da94949e3dab7500ee29fa3e0dda723daf8e7d60e5f3ce4450df"
+  url "https://downloads.sourceforge.net/project/avf/avfs/1.1.4/avfs-1.1.4.tar.bz2"
+  sha256 "3a7981af8557f864ae10d4b204c29969588fdb526e117456e8efd54bf8faa12b"
 
   bottle do
-    sha256 catalina:    "6f496a30b6bd1c8eba1005e4bc0da26b53353effab3f447cf8d43a669ad7a6b5"
-    sha256 mojave:      "1e75ce4753a0d9a9af12e4a718537a9e2398fd535413b72505dd126a33610fe6"
-    sha256 high_sierra: "690fbe0161f0c5ce4ec737e67624b54bfcd7825efa8b554e1773691365dcd6ed"
+    sha256 x86_64_linux: "6e490770e6562a092085e1a0855e7d6c988a6d8ce9d5cafdcbde082baea8679b"
   end
 
   depends_on "pkg-config" => :build
@@ -20,21 +18,18 @@ class Avfs < Formula
   end
 
   on_linux do
-    depends_on "libfuse"
+    depends_on "libfuse@2"
   end
 
   def install
     args = %W[
-      --prefix=#{prefix}
-      --disable-debug
-      --disable-dependency-tracking
       --disable-silent-rules
       --enable-fuse
       --enable-library
       --with-ssl=#{Formula["openssl@1.1"].opt_prefix}
     ]
 
-    system "./configure", *args
+    system "./configure", *std_configure_args, *args
     system "make", "install"
   end
 
