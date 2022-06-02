b4d6560d93073b7f21aada4bbac278397f2499c1
Adrian Ho <the.gromgit@gmail.com>
2022-06-02 13:25:51 +0800

    nspr: update 4.34 bottle.

--- Formula/nspr.rb
+++ Formula/nspr.rb
@@ -1,8 +1,8 @@
 class Nspr < Formula
   desc "Platform-neutral API for system-level and libc-like functions"
   homepage "https://hg.mozilla.org/projects/nspr"
-  url "https://archive.mozilla.org/pub/nspr/releases/v4.33/src/nspr-4.33.tar.gz"
-  sha256 "b23ee315be0e50c2fb1aa374d17f2d2d9146a835b1a79c1918ea15d075a693d7"
+  url "https://archive.mozilla.org/pub/nspr/releases/v4.34/src/nspr-4.34.tar.gz"
+  sha256 "beef011cd15d8f40794984d17014366513cec5719bf1a78f5e8a3e3a1cebf99c"
   license "MPL-2.0"
 
   livecheck do
@@ -12,7 +12,7 @@ class Nspr < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nspr"
-    sha256 cellar: :any, mojave: "6aabbe5708d1335ee0af0074efcdbf5189e14f5905e7cfbb0a2384aef519bc12"
+    sha256 cellar: :any, mojave: "517b111c98731a915714522da2915e743fb96cb8f3fac9dd1c3c3b267ac0e1a8"
   end
 
   def install
