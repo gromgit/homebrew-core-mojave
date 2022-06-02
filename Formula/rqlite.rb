7a27a72500ba62053724b33d931165f6f872dfdd
Adrian Ho <the.gromgit@gmail.com>
2022-06-02 15:27:34 +0800

    rqlite: update 7.5.0 bottle.

--- Formula/rqlite.rb
+++ Formula/rqlite.rb
@@ -1,14 +1,14 @@
 class Rqlite < Formula
   desc "Lightweight, distributed relational database built on SQLite"
   homepage "http://www.rqlite.com/"
-  url "https://github.com/rqlite/rqlite/archive/v7.4.0.tar.gz"
-  sha256 "f21abd5b989b6ca7b4b89aee3cd76fede5a3448f8127817ecc8b8507d73180eb"
+  url "https://github.com/rqlite/rqlite/archive/v7.5.0.tar.gz"
+  sha256 "d603915ba62f97415e9fd1309829b76c98f2212bbeb5e3bd51db1c7f2ca2f5bc"
   license "MIT"
   head "https://github.com/rqlite/rqlite.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rqlite"
-    sha256 cellar: :any_skip_relocation, mojave: "74bb6a55f09cc65eb3dc066c37f23b636d3123a7a1c04ba25d554a1a0bbdea79"
+    sha256 cellar: :any_skip_relocation, mojave: "61be8d131624e33a37886ba39690cddf51b9689907f42892a41cfcde2c0267fb"
   end
 
   depends_on "go" => :build
