bb3c681c4d5a477678d0012fbeb12a5fd9f48814
Adrian Ho <the.gromgit@gmail.com>
2022-06-02 10:55:22 +0800

    lv2: update 1.18.4 bottle.

--- Formula/lv2.rb
+++ Formula/lv2.rb
@@ -4,10 +4,9 @@ class Lv2 < Formula
 
   desc "Portable plugin standard for audio systems"
   homepage "https://lv2plug.in/"
-  url "https://lv2plug.in/spec/lv2-1.18.2.tar.bz2"
-  sha256 "4e891fbc744c05855beb5dfa82e822b14917dd66e98f82b8230dbd1c7ab2e05e"
+  url "https://lv2plug.in/spec/lv2-1.18.4.tar.bz2"
+  sha256 "4ddc0ed20579aabbaeb4df6fb42b69949c01dc7169ab0b945c709339509e6760"
   license "ISC"
-  revision 2
 
   livecheck do
     url "https://lv2plug.in/spec/"
@@ -16,7 +15,7 @@ class Lv2 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lv2"
-    sha256 cellar: :any_skip_relocation, mojave: "1afc3b64ac4cd78b468bf4adbca31bc130c8b18ef655c310f3d1418ffb4d2a01"
+    sha256 cellar: :any_skip_relocation, mojave: "d9cd077d6041c133219b7160f9d269c5612e573e349792580bd376f6d6dfacba"
   end
 
   depends_on "python@3.9"
