84a22ed0694e06b3f99d102db68e7ac6801ca2ab
Adrian Ho <the.gromgit@gmail.com>
2022-06-02 15:05:08 +0800

    radare2: update 5.7.0 bottle.

--- Formula/radare2.rb
+++ Formula/radare2.rb
@@ -1,8 +1,8 @@
 class Radare2 < Formula
   desc "Reverse engineering framework"
   homepage "https://radare.org"
-  url "https://github.com/radareorg/radare2/archive/5.6.8.tar.gz"
-  sha256 "320d4f4733402cbc38303c7d37db04716e4376138da44d3664bb73adbdd77e0c"
+  url "https://github.com/radareorg/radare2/archive/5.7.0.tar.gz"
+  sha256 "fe7ca861bf71dd3c4766a57f73fd97b216bcfde161720f949c05875df212976b"
   license "LGPL-3.0-only"
   head "https://github.com/radareorg/radare2.git", branch: "master"
 
@@ -13,7 +13,7 @@ class Radare2 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/radare2"
-    sha256 mojave: "7d34daff9bebb9eb675ce49538aac2e53c2ced29e9c02e0601f7984ae7eeffa0"
+    sha256 mojave: "d11d153eb93409c925978bc391235249dfd8ecbc4eb84515d50b63c8282203d7"
   end
 
   def install
