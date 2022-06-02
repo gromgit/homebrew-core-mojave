46819643d3ac07f406c27b57b6aeb5d444c9d663
Adrian Ho <the.gromgit@gmail.com>
2022-06-02 17:53:14 +0800

    yorkie: update 0.2.6 bottle.

---
+++ Formula/yorkie.rb
@@ -0,0 +1,44 @@
+class Yorkie < Formula
+  desc "Document store for collaborative applications"
+  homepage "https://yorkie.dev/"
+  url "https://github.com/yorkie-team/yorkie.git",
+    tag:      "v0.2.6",
+    revision: "284ceddfaa6400ce6eaaffad4570393c729ebf81"
+  license "Apache-2.0"
+  head "https://github.com/yorkie-team/yorkie.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/yorkie"
+    sha256 cellar: :any_skip_relocation, mojave: "212f6b9f66603657e5e7f0e0e7559c41ee8ed0100b939df4d9cbf7dda0a64352"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "build"
+    prefix.install "bin"
+  end
+
+  service do
+    run opt_bin/"yorkie"
+    run_type :immediate
+    keep_alive true
+    working_dir var
+  end
+
+  test do
+    yorkie_pid = fork do
+      exec bin/"yorkie", "server"
+    end
+    # sleep to let yorkie get ready
+    sleep 3
+
+    test_project = "test"
+    output = shell_output("#{bin}/yorkie project create #{test_project}")
+    project_info = JSON.parse(output)
+    assert_equal test_project, project_info.fetch("name")
+  ensure
+    # clean up the process before we leave
+    Process.kill("HUP", yorkie_pid)
+  end
+end
