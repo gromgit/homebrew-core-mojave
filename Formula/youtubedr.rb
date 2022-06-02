2c7e8d9e4d27faa337a2ae780519bef5a7001518
Adrian Ho <the.gromgit@gmail.com>
2022-06-02 18:01:05 +0800

    youtubedr: update 2.7.15 bottle.

--- Formula/youtubedr.rb
+++ Formula/youtubedr.rb
@@ -1,13 +1,13 @@
 class Youtubedr < Formula
   desc "Download Youtube Video in Golang"
   homepage "https://github.com/kkdai/youtube"
-  url "https://github.com/kkdai/youtube/archive/v2.7.14.tar.gz"
-  sha256 "d1047558f606a82c055c018f6cc8d1d8057a9e507e814093675ea158d733d678"
+  url "https://github.com/kkdai/youtube/archive/v2.7.15.tar.gz"
+  sha256 "093246256d8f0ef719cb1bd5a5548b56031b686c553f44f84236bd62b0519357"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/youtubedr"
-    sha256 cellar: :any_skip_relocation, mojave: "11f9686eb0a0bcc63c0c3bfbf3e0516e08ede934757c9dfc8ee554261b5e72d1"
+    sha256 cellar: :any_skip_relocation, mojave: "95b83c67af793112b1f18197971a1a4cfcfbb3056bab45a7c07c91c2947eb1b2"
   end
 
   depends_on "go" => :build
