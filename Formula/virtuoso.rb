ecabcece68d2ee547161f5bf0c4b537832304443
Adrian Ho <the.gromgit@gmail.com>
2022-06-02 17:36:42 +0800

    virtuoso: update 7.2.7 bottle.

--- Formula/virtuoso.rb
+++ Formula/virtuoso.rb
@@ -1,18 +1,13 @@
 class Virtuoso < Formula
   desc "High-performance object-relational SQL database"
   homepage "https://virtuoso.openlinksw.com/wiki/main/"
-  url "https://github.com/openlink/virtuoso-opensource/releases/download/v7.2.6.1/virtuoso-opensource-7.2.6.tar.gz"
-  sha256 "38fd3c037aef62fcc7c28de5c0d6c2577d4bb19809e71421fc42093ed4d1c753"
+  url "https://github.com/openlink/virtuoso-opensource/releases/download/v7.2.7/virtuoso-opensource-7.2.7.tar.gz"
+  sha256 "02480b930d5fb414cb328f10cfd200faa658adc10f9c68ef7034c6aa81a5a3a0"
   license "GPL-2.0-only"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "80cff43493d2981f14ec84848d5951a239a1ffe307bc030e7fed2ebc48234303"
-    sha256 cellar: :any,                 arm64_big_sur:  "07dd74a255eadcb7bf6cc41d3e07445a992069e5ab9072dc24a8f02fd98dcf74"
-    sha256 cellar: :any,                 monterey:       "dfd970140bb4e7a212d2fa8c4be8a45a75d643fa247ed185d1fc6607ff044e76"
-    sha256 cellar: :any,                 big_sur:        "d35c507e655a9b900986b609bb232f30c811cbfb4f3ec20d60d1146059ba5305"
-    sha256 cellar: :any,                 catalina:       "bb672169382bf5faa77845765420adc91f5af1e46239e1f9d45d187b2f335570"
-    sha256 cellar: :any,                 mojave:         "e19a2320912ff23b8f0a4926ee3c830d1bbbc9388fbe21634cbecf919e17f708"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d396498868d1529455ff98c3432df539f5c0acecc2f807c7d5f49e1cf10d3267"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/virtuoso"
+    sha256 cellar: :any, mojave: "b09d50a0a763037facc224c839f6d91f70812f02e9f14088c386cfb07ce4c669"
   end
 
   head do
