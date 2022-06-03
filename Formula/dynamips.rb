99cc2207c2fed56ee043db0e77e5afb79eee598f
Adrian Ho <the.gromgit@gmail.com>
2022-06-03 21:57:41 +0800

    dynamips: update 0.2.22 bottle.

--- Formula/dynamips.rb
+++ Formula/dynamips.rb
@@ -1,8 +1,8 @@
 class Dynamips < Formula
   desc "Cisco 7200/3600/3725/3745/2600/1700 Router Emulator"
   homepage "https://github.com/GNS3/dynamips"
-  url "https://github.com/GNS3/dynamips/archive/v0.2.21.tar.gz"
-  sha256 "08587589db2c3fc637e6345aebf4f9706825c12f45d9e2cf40d4189c604656d2"
+  url "https://github.com/GNS3/dynamips/archive/v0.2.22.tar.gz"
+  sha256 "5b3142eb4d2780683ea8781d5f4da6fc39c514d36546392508c74da8ba98240b"
   license "GPL-2.0"
 
   livecheck do
@@ -11,15 +11,8 @@ class Dynamips < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2733d58e4e92ebbae4458cc3a0c5af500bd007ede953efd29db7850cd6ddc5e0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b25a7870e64febcdbe828bd72f0f7ff29acc43a23267ec07e316b3ce6aea0763"
-    sha256 cellar: :any_skip_relocation, monterey:       "391e5fc83c27da6bf5203131f5c284c93b2af9ec036b999c21df4a23ec25d304"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1cf10fcefc4f6d8d2fff03b407e0d8b2730ef944c9d91762823ca1865b7b3f33"
-    sha256 cellar: :any_skip_relocation, catalina:       "ecf536589504e42389e91865495faa4eb30ba20adad56bc865c0481e80abefe4"
-    sha256 cellar: :any_skip_relocation, mojave:         "db5398464afdb11af6f26cd4780f6e688bed0f35c9fea8f8308f11991987a037"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "cb9bf6eebd6a7987976e0e2543a807e1b0f16698a1c71eb64e7da56f320fd425"
-    sha256 cellar: :any_skip_relocation, sierra:         "08b44502cd3b052592f11f5b75453fabd51fdcfe1a311405c4b7329a701dc424"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c57bab5a6ec685c6c6e5590eec88c10f22555749a84e9baea403a7a081f09207"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dynamips"
+    sha256 cellar: :any_skip_relocation, mojave: "88d2dedfe7f408ca161f81eb2ecaaf9d28f9a136e7bf44cbec1c784278871bbd"
   end
 
   depends_on "cmake" => :build
@@ -27,7 +20,7 @@ class Dynamips < Formula
   uses_from_macos "libpcap"
 
   on_macos do
-    depends_on "libelf"
+    depends_on "libelf" => :build
   end
 
   on_linux do
