9653a7832a4bd5915a00cdca5bde4a1e5eb42164
Adrian Ho <the.gromgit@gmail.com>
2022-06-04 02:03:14 +0800

    gatsby-cli: update 4.15.0 bottle.

--- Formula/gatsby-cli.rb
+++ Formula/gatsby-cli.rb
@@ -4,13 +4,13 @@ class GatsbyCli < Formula
   desc "Gatsby command-line interface"
   homepage "https://www.gatsbyjs.org/docs/gatsby-cli/"
   # gatsby-cli should only be updated every 10 releases on multiples of 10
-  url "https://registry.npmjs.org/gatsby-cli/-/gatsby-cli-4.11.0.tgz"
-  sha256 "cd4d8d680e666af8099eb8dddc99fac4159abfcd0a448a8a1991e30a1378eb52"
+  url "https://registry.npmjs.org/gatsby-cli/-/gatsby-cli-4.15.0.tgz"
+  sha256 "c55e6e124ed0a582b66c14c60c38f660e432774d1e4c150eb60f8771ccc95db0"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gatsby-cli"
-    sha256 mojave: "60c53c5be14c4b6c8e290371ffc896a356640cf98095a1f47b8ffa7572cdbdea"
+    sha256 mojave: "cb8af6475f38e6e85587b3af3dd78024532e9819f6c5b2971dd574ac2a3abfab"
   end
 
   depends_on "node"
@@ -27,18 +27,14 @@ class GatsbyCli < Formula
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir[libexec/"bin/*"]
 
-    # Avoid references to Homebrew shims
-    node_modules = libexec/"lib/node_modules/#{name}/node_modules"
-    rm_f node_modules/"websocket/builderror.log"
-
     # Remove incompatible pre-built binaries
-    os = OS.kernel_name.downcase
+    node_modules = libexec/"lib/node_modules/#{name}/node_modules"
     arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
-    node_modules.glob("{lmdb,msgpackr-extract}/prebuilds/*").each do |dir|
-      if dir.basename.to_s != "#{os}-#{arch}"
-        dir.rmtree
-      elsif OS.linux?
-        dir.glob("*.musl.node").map(&:unlink)
+    if OS.linux?
+      %w[lmdb @msgpackr-extract/msgpackr-extract].each do |mod|
+        node_modules.glob("#{mod}-linux-#{arch}/*.musl.node")
+                    .map(&:unlink)
+                    .empty? && raise("Unable to find #{mod} musl library to delete.")
       end
     end
 
