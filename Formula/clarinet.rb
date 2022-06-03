bef47bf69930912aa6023d82e03a76c689761823
Adrian Ho <the.gromgit@gmail.com>
2022-06-03 15:39:48 +0800

    clarinet: update 0.31.1 bottle.

--- Formula/clarinet.rb
+++ Formula/clarinet.rb
@@ -3,8 +3,8 @@ class Clarinet < Formula
   homepage "https://github.com/hirosystems/clarinet"
   # pull from git tag to get submodules
   url "https://github.com/hirosystems/clarinet.git",
-      tag:      "v0.31.0",
-      revision: "c9794c3170c5b978e328182b526fde7b496b5d1c"
+      tag:      "v0.31.1",
+      revision: "f29d05926195d86123c29733790a516b40ff7b5f"
   license "GPL-3.0-only"
   head "https://github.com/hirosystems/clarinet.git", branch: "main"
 
@@ -15,7 +15,7 @@ class Clarinet < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/clarinet"
-    sha256 cellar: :any_skip_relocation, mojave: "346b6523f194d5dc98641781b76576e49bbd3623a75b6341ebb96c84257271a4"
+    sha256 cellar: :any_skip_relocation, mojave: "0cb7106685c8534e68a6c5492e747f7bd585b8ddb7a131694f451b1cd3b4a823"
   end
 
   depends_on "rust" => :build
