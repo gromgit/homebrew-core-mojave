4859f0549c5524914ff95f42fea60b32395affc6
Adrian Ho <the.gromgit@gmail.com>
2022-06-03 15:12:56 +0800

    clair: update 4.4.2 bottle.

--- Formula/clair.rb
+++ Formula/clair.rb
@@ -1,8 +1,8 @@
 class Clair < Formula
   desc "Vulnerability Static Analysis for Containers"
   homepage "https://github.com/quay/clair"
-  url "https://github.com/quay/clair/archive/v4.3.6.tar.gz"
-  sha256 "6a696c53be08d3ca72b5a4226ff2c2dec6fc39eddcb17732ec1d55d32d753b42"
+  url "https://github.com/quay/clair/archive/v4.4.2.tar.gz"
+  sha256 "78843ccb2b2de390082fc6d06ae9e82036a4b615a33a5fe5cf57b294def2b771"
   license "Apache-2.0"
 
   livecheck do
@@ -12,7 +12,7 @@ class Clair < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/clair"
-    sha256 cellar: :any_skip_relocation, mojave: "f820894b6fb98cc3b9f4703d855a1cf18fe7b14d4b39dd2e55e1b57a48ef0803"
+    sha256 cellar: :any_skip_relocation, mojave: "d606b26a03ed92e083643301e93b607c6f33e90fc9d5b81c1d5a161e132678d1"
   end
 
   depends_on "go" => :build
@@ -30,8 +30,24 @@ class Clair < Formula
   end
 
   test do
-    cp etc/"clair/config.yaml.sample", testpath
-    output = shell_output("#{bin}/clair -conf #{testpath}/config.yaml.sample -mode combo 2>&1", 1)
+    http_port = free_port
+    db_port = free_port
+    (testpath/"config.yaml").write <<~EOS
+      ---
+      introspection_addr: "localhost:#{free_port}"
+      http_listen_addr: "localhost:#{http_port}"
+      indexer:
+        connstring: host=localhost port=#{db_port} user=clair dbname=clair sslmode=disable
+      matcher:
+        indexer_addr: "localhost:#{http_port}"
+        connstring: host=localhost port=#{db_port} user=clair dbname=clair sslmode=disable
+      notifier:
+        indexer_addr: "localhost:#{http_port}"
+        matcher_addr: "localhost:#{http_port}"
+        connstring: host=localhost port=#{db_port} user=clair dbname=clair sslmode=disable
+    EOS
+
+    output = shell_output("#{bin}/clair -conf #{testpath}/config.yaml -mode combo 2>&1", 1)
     # requires a Postgres database
     assert_match "service initialization failed: failed to initialize indexer: failed to create ConnPool", output
   end
