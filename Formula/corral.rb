ac156917f9b3c87b19b20b1b9ef47ecc11a295c0
Adrian Ho <the.gromgit@gmail.com>
2022-06-03 19:46:35 +0800

    corral: update 0.6.0 bottle.

--- Formula/corral.rb
+++ Formula/corral.rb
@@ -1,14 +1,14 @@
 class Corral < Formula
   desc "Dependency manager for the Pony language"
   homepage "https://github.com/ponylang/corral"
-  url "https://github.com/ponylang/corral/archive/0.5.7.tar.gz"
-  sha256 "5b7cffc54abe5b2292a2fb2c67603a52c0c02d38ab8bbd2af2af0b80ca62e414"
+  url "https://github.com/ponylang/corral/archive/0.6.0.tar.gz"
+  sha256 "d1e4cfd07c170780595b4681ad444faf69892d59adec4a51b02ede5641a4fdd2"
   license "BSD-2-Clause"
   head "https://github.com/ponylang/corral.git", branch: "main"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/corral"
-    sha256 cellar: :any_skip_relocation, mojave: "9304705afb4e5cb831882a716f44e77383c4824e13448d7e7ff2dda2c906e487"
+    sha256 cellar: :any_skip_relocation, mojave: "97f0ccc132677e44a5b5d039da1ffa0118233ef6f44dbbe2feeb643ce8ba3363"
   end
 
   depends_on "ponyc"
