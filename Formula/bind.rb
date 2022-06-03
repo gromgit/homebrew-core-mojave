8d8eac3d468d839b1e2be7a6b739580f18534ef5
Adrian Ho <the.gromgit@gmail.com>
2022-06-03 10:10:17 +0800

    bind: update 9.18.3 bottle.

--- Formula/bind.rb
+++ Formula/bind.rb
@@ -8,8 +8,8 @@ class Bind < Formula
   # "version_scheme" because someone upgraded to 9.15.0, and required a
   # downgrade.
 
-  url "https://downloads.isc.org/isc/bind9/9.18.2/bind-9.18.2.tar.xz"
-  sha256 "2e4b38779bba0a23ee634fdf7c525fd9794c41d692bfd83cda25823a2a3ed969"
+  url "https://downloads.isc.org/isc/bind9/9.18.3/bind-9.18.3.tar.xz"
+  sha256 "0ad8da773bd93cba0ef66cc81999698ebdf9c3e51faed5e5c8c1eb75cad2ae6f"
   license "MPL-2.0"
   version_scheme 1
   head "https://gitlab.isc.org/isc-projects/bind9.git", branch: "main"
@@ -23,7 +23,7 @@ class Bind < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bind"
-    sha256 mojave: "9e1bea86283e468ff5b59f1cf315ea2ff3677fdf60fe93fa6dce05a908d9c429"
+    sha256 mojave: "f785ee605b0ce740e8af893ed96133a8b35ec69d5bf3362314924846009b5477"
   end
 
   depends_on "pkg-config" => :build
