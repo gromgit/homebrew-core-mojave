2cb1fc2c0f46c91f756c428190782c5dc1be36d6
Adrian Ho <the.gromgit@gmail.com>
2022-06-04 09:13:42 +0800

    hexo: update 6.2.0 bottle.

--- Formula/hexo.rb
+++ Formula/hexo.rb
@@ -3,14 +3,14 @@ require "language/node"
 class Hexo < Formula
   desc "Fast, simple & powerful blog framework"
   homepage "https://hexo.io/"
-  url "https://registry.npmjs.org/hexo/-/hexo-6.1.0.tgz"
-  sha256 "c3cbaa3a4d72ccadf1125762895c2714e8474099c9da73c60bd766f17f92fac4"
+  url "https://registry.npmjs.org/hexo/-/hexo-6.2.0.tgz"
+  sha256 "d577729d6b0c21cccf7c6e81b395a3bfe2f058f29fca8c569643639f3b571772"
   license "MIT"
   head "https://github.com/hexojs/hexo.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/hexo"
-    sha256 cellar: :any_skip_relocation, mojave: "3b956bafe14f1523fd2dca05cdcc491bb12053d2f50cdbca1c398a0f8db34f61"
+    sha256 cellar: :any_skip_relocation, mojave: "188089e12b3d37a7f639eff66434988db179ca30bc6663c6b162611ba65b5097"
   end
 
   depends_on "node"
