179a47f749dc769a80928e39301ef95c697fd252
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 00:06:07 +0800

    json-fortran: update 8.3.0 bottle.

--- Formula/json-fortran.rb
+++ Formula/json-fortran.rb
@@ -1,15 +1,14 @@
 class JsonFortran < Formula
   desc "Fortran 2008 JSON API"
   homepage "https://github.com/jacobwilliams/json-fortran"
-  url "https://github.com/jacobwilliams/json-fortran/archive/8.2.5.tar.gz"
-  sha256 "16eec827f64340c226ba9a8463f001901d469bc400a1e88b849f258f9ef0d100"
+  url "https://github.com/jacobwilliams/json-fortran/archive/8.3.0.tar.gz"
+  sha256 "5fe9ad709a726416cec986886503e0526419742e288c4e43f63c1c22026d1e8a"
   license "BSD-3-Clause"
   head "https://github.com/jacobwilliams/json-fortran.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/json-fortran"
-    rebuild 1
-    sha256 cellar: :any, mojave: "b7a944ed0251a3aca20e9c67627e8cf7eb9fb09f73cbc38e87c4b2e6235996cd"
+    sha256 cellar: :any, mojave: "cc3f057c63f6c30a053dcb342b284e6cd59708f0070b560674c2e38bf8ea27d1"
   end
 
   depends_on "cmake" => :build
