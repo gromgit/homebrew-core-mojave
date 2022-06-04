067889671b6c92eaa50df0786ef6bd1e96106d19
Adrian Ho <the.gromgit@gmail.com>
2022-06-04 18:59:31 +0800

    eigenpy: update 2.7.6 bottle.

--- Formula/eigenpy.rb
+++ Formula/eigenpy.rb
@@ -1,14 +1,14 @@
 class Eigenpy < Formula
   desc "Python bindings of Eigen library with Numpy support"
   homepage "https://github.com/stack-of-tasks/eigenpy"
-  url "https://github.com/stack-of-tasks/eigenpy/releases/download/v2.7.4/eigenpy-2.7.4.tar.gz"
-  sha256 "54f6f61f57b6588004725476b586318e62ae4ed456b81ed9ba262775e64556c3"
+  url "https://github.com/stack-of-tasks/eigenpy/releases/download/v2.7.6/eigenpy-2.7.6.tar.gz"
+  sha256 "71cc2763e891b7ef013b1c0306cdb7e497d6aff9f648dca30997e541b1a6e05a"
   license "BSD-2-Clause"
   head "https://github.com/stack-of-tasks/eigenpy.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/eigenpy"
-    sha256 cellar: :any, mojave: "f7ffc54c0af4452aee2a1f6854be2282b045bf3629a877b3bb82e971c569896e"
+    sha256 cellar: :any, mojave: "80299050eb9b82693e2e54d051c97cb69e870dbe90eff94d95493e8cc66ccdb3"
   end
 
   depends_on "boost" => :build
