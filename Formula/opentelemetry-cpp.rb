420ec9c39f357640a05e900a61421bc7f26c7d34
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 10:59:10 +0800

    opentelemetry-cpp: update 1.4.0 bottle.

---
+++ Formula/opentelemetry-cpp.rb
@@ -0,0 +1,78 @@
+class OpentelemetryCpp < Formula
+  desc "OpenTelemetry C++ Client"
+  homepage "https://opentelemetry.io/"
+  url "https://github.com/open-telemetry/opentelemetry-cpp/archive/refs/tags/v1.4.0.tar.gz"
+  sha256 "110f4fb2e38dcc72a421647631cfbb9429afd3c77c6c98829cc1d11bd0c72563"
+  license "Apache-2.0"
+  head "https://github.com/open-telemetry/opentelemetry-cpp.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/opentelemetry-cpp"
+    sha256 cellar: :any_skip_relocation, mojave: "851ce27959c351d3ec77747d59ebafc7228b7a8c4a9250e5b3fce7086fd27a7f"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "grpc"
+  depends_on "nlohmann-json"
+  depends_on "prometheus-cpp"
+  depends_on "protobuf"
+  depends_on "thrift"
+
+  uses_from_macos "curl"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DCMAKE_INSTALL_RPATH=#{rpath}",
+                    "-DBUILD_TESTING=OFF",
+                    "-DWITH_ELASTICSEARCH=ON",
+                    "-DWITH_EXAMPLES=OFF",
+                    "-DWITH_JAEGER=ON",
+                    "-DWITH_LOGS_PREVIEW=ON",
+                    "-DWITH_METRICS_PREVIEW=ON",
+                    "-DWITH_OTLP=ON",
+                    "-DWITH_OTLP_GRPC=ON",
+                    "-DWITH_OTLP_HTTP=ON",
+                    "-DWITH_PROMETHEUS=ON",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cc").write <<~EOS
+      #include "opentelemetry/sdk/trace/simple_processor.h"
+      #include "opentelemetry/sdk/trace/tracer_provider.h"
+      #include "opentelemetry/trace/provider.h"
+      #include "opentelemetry/exporters/ostream/span_exporter.h"
+
+      namespace trace_api = opentelemetry::trace;
+      namespace trace_sdk = opentelemetry::sdk::trace;
+      namespace nostd     = opentelemetry::nostd;
+
+      int main()
+      {
+        auto exporter = std::unique_ptr<trace_sdk::SpanExporter>(
+            new opentelemetry::exporter::trace::OStreamSpanExporter);
+        auto processor = std::unique_ptr<trace_sdk::SpanProcessor>(
+            new trace_sdk::SimpleSpanProcessor(std::move(exporter)));
+        auto provider = nostd::shared_ptr<trace_api::TracerProvider>(
+            new trace_sdk::TracerProvider(std::move(processor)));
+
+        // Set the global trace provider
+        trace_api::Provider::SetTracerProvider(provider);
+
+        auto tracer = provider->GetTracer("library", OPENTELEMETRY_SDK_VERSION);
+        auto scoped_span = trace_api::Scope(tracer->StartSpan("test"));
+      }
+    EOS
+    system ENV.cxx, "test.cc", "-std=c++11", "-I#{include}", "-L#{lib}",
+           "-lopentelemetry_resources",
+           "-lopentelemetry_trace",
+           "-lopentelemetry_exporter_ostream_span",
+           "-lopentelemetry_common",
+           "-pthread",
+           "-o", "simple-example"
+    system "./simple-example"
+  end
+end
