dbf7b45c6c9da36c22ddd30f015554d2716598fd
Adrian Ho <the.gromgit@gmail.com>
2022-06-06 01:44:11 +0800

    yq: update 4.25.2 bottle.

--- Formula/yq.rb
+++ Formula/yq.rb
@@ -1,8 +1,8 @@
 class Yq < Formula
   desc "Process YAML documents from the CLI"
   homepage "https://github.com/mikefarah/yq"
-  url "https://github.com/mikefarah/yq/archive/v4.25.1.tar.gz"
-  sha256 "2f0736f0650bef121e31332e1f52c67e9bd975ca651e1507a2e5e3744c10e766"
+  url "https://github.com/mikefarah/yq/archive/v4.25.2.tar.gz"
+  sha256 "2aa2d3e4e44a74bc8a2213f60620f69366a86bbc9f5deffcc15047eaa4cf9e19"
   license "MIT"
   head "https://github.com/mikefarah/yq.git", branch: "master"
 
@@ -13,7 +13,7 @@ class Yq < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/yq"
-    sha256 cellar: :any_skip_relocation, mojave: "14552675b73fffa8e140b30292f0d50e8132cda9fe5c95f6412cba1340ec66d1"
+    sha256 cellar: :any_skip_relocation, mojave: "8e72486b7c556361fcb87d033af23bd0b439bc3aa59b42a691e89ee74a0dc86d"
   end
 
   depends_on "go" => :build
