12ce6818ea7c57990deb166a0a6e2366cb7f5f9a
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 23:28:18 +0800

    tracker: update 3.3.0 bottle.

--- Formula/tracker.rb
+++ Formula/tracker.rb
@@ -1,10 +1,9 @@
 class Tracker < Formula
   desc "Library and daemon that is an efficient search engine and triplestore"
   homepage "https://gnome.pages.gitlab.gnome.org/tracker/"
-  url "https://download.gnome.org/sources/tracker/3.1/tracker-3.1.2.tar.xz"
-  sha256 "da368962665d587bb2e4f164d75919a81dacb35c7d4cfae6f93a94c60f60ec8f"
+  url "https://download.gnome.org/sources/tracker/3.3/tracker-3.3.0.tar.xz"
+  sha256 "0706f96fe7f95df42acec812c1de7b4593a0d648321ca83506a9d71e22417bda"
   license all_of: ["LGPL-2.1-or-later", "GPL-2.0-or-later"]
-  revision 2
 
   # Tracker doesn't follow GNOME's "even-numbered minor is stable" version scheme.
   livecheck do
@@ -14,30 +13,50 @@ class Tracker < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tracker"
-    sha256 mojave: "73b906fac635c986966f3eb6afbb477a7b367f491983f79b7306d3c87cb366f7"
+    sha256 mojave: "80c3e0556ce7f1b9a276e5d8d236de8f2e186d73a28ef648af71756ee344397d"
   end
 
   depends_on "gobject-introspection" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
-  depends_on "pkg-config" => :build
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "pygobject3" => :build
   depends_on "vala" => :build
   depends_on "dbus"
+  depends_on "glib"
   depends_on "icu4c"
   depends_on "json-glib"
-  depends_on "libsoup@2"
+  depends_on "libsoup"
+  depends_on "sqlite"
+
+  uses_from_macos "libxml2"
+
+  # Fix build error: tracker_init_remote: code should not be reached
+  patch do
+    url "https://gitlab.gnome.org/GNOME/tracker/-/commit/95291bf791ead1db062dfe0f0b4626393cb58338.diff"
+    sha256 "c3485e0323fca437a95f1f0c6ce002da86781d01c27a429ed0a4f7e408a22f2f"
+  end
+
+  # Fix build error: Invalid GType function: 'tracker_endpoint_http_get_type'
+  patch do
+    url "https://gitlab.gnome.org/GNOME/tracker/-/commit/471f7fd87da2fea4aeebdddb3579e95a14208647.diff"
+    sha256 "8a90b9c197a63f55a4d126afd2e9ccad230856ba5b9bdd5c2a6a861419e57ac5"
+  end
 
   def install
     args = std_meson_args + %w[
       -Dman=false
       -Ddocs=false
       -Dsystemd_user_services=false
+      -Dtests=false
+      -Dsoup=soup3
     ]
 
     ENV["DESTDIR"] = "/"
     mkdir "build" do
       system "meson", *args, ".."
-      system "ninja", "-v"
+      # Disable parallel build due to error: 'libtracker-sparql/tracker-sparql-enum-types.h' file not found
+      system "ninja", "-v", "-j1"
       system "ninja", "install", "-v"
     end
   end
@@ -52,13 +71,13 @@ class Tracker < Formula
 
       gint main(gint argc, gchar *argv[]) {
         g_autoptr(GError) error = NULL;
-        g_autoptr(GFile) store, ontology;
+        g_autoptr(GFile) ontology;
         g_autoptr(TrackerSparqlConnection) connection;
         g_autoptr(TrackerSparqlCursor) cursor;
         int i = 0;
 
         ontology = tracker_sparql_get_ontology_nepomuk();
-        connection = tracker_sparql_connection_new(0, store, ontology, NULL, &error);
+        connection = tracker_sparql_connection_new(0, NULL, ontology, NULL, &error);
 
         if (error) {
           g_critical("Error: %s", error->message);
@@ -89,19 +108,8 @@ class Tracker < Formula
         return 0;
       }
     EOS
-    glib = Formula["glib"]
-    flags = %W[
-      -I#{glib.opt_include}/glib-2.0
-      -I#{glib.opt_lib}/glib-2.0/include
-      -I#{include}/tracker-3.0
-      -D_REENTRANT
-      -L#{glib.opt_lib}
-      -L#{lib}
-      -ltracker-sparql-3.0
-      -lgio-2.0
-      -lglib-2.0
-      -lgobject-2.0
-    ]
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["icu4c"].opt_lib/"pkgconfig" if OS.mac?
+    flags = shell_output("pkg-config --cflags --libs tracker-sparql-3.0").chomp.split
     system ENV.cc, "test.c", "-o", "test", *flags
     system "./test"
   end
