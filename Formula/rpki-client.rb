de086752e92b898fc40a72ad9aba3a4307503a25
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 19:11:02 +0800

    rpki-client: update 7.8_1 bottle.

--- Formula/rpki-client.rb
+++ Formula/rpki-client.rb
@@ -4,10 +4,11 @@ class RpkiClient < Formula
   url "https://ftp.openbsd.org/pub/OpenBSD/rpki-client/rpki-client-7.8.tar.gz"
   sha256 "7a87a6fe7b1bd36a1ce277cf50e125ece7b2ed0236e252a66e2b34ca8f88b7f5"
   license "ISC"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rpki-client"
-    sha256 mojave: "df855345346f9bbb690d0e2ece436d7c658abade2773fa0b3f6c7058f8c53299"
+    sha256 mojave: "96c2f51762bf39ca7d90ae6166281b9a542b1a3fd968d7d9921f81c46d74f325"
   end
 
   depends_on "pkg-config" => :build
