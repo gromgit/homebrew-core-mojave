2815174f8b0d9662b2c02e1348bf684810d41765
Adrian Ho <the.gromgit@gmail.com>
2022-06-05 23:06:13 +0800

    threadweaver: update 5.94.0 bottle.

--- Formula/threadweaver.rb
+++ Formula/threadweaver.rb
@@ -1,8 +1,8 @@
 class Threadweaver < Formula
   desc "Helper for multithreaded programming"
   homepage "https://api.kde.org/frameworks/threadweaver/html/index.html"
-  url "https://download.kde.org/stable/frameworks/5.93/threadweaver-5.93.0.tar.xz"
-  sha256 "6c0db7d276052ebc4d512b43792ae2127cb5fafb1cfee890b5f20bd05c06f185"
+  url "https://download.kde.org/stable/frameworks/5.94/threadweaver-5.94.0.tar.xz"
+  sha256 "87d8431a62c53b7e433b49dfad4fd6cacb58356a50cfa17ac480c6bb00a8e1c5"
   license "LGPL-2.0-or-later"
   head "https://invent.kde.org/frameworks/threadweaver.git", branch: "master"
 
@@ -15,7 +15,7 @@ class Threadweaver < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/threadweaver"
-    sha256 cellar: :any, mojave: "41085707e8753fb2b7628dc552524d61599d26449169f1a95487bd92d2b74554"
+    sha256 cellar: :any, mojave: "12e1e9c0dab36c65ba1668e973b7b45a1fdd770ef7f737dc8c0074005eeed430"
   end
 
   depends_on "cmake" => [:build, :test]
@@ -49,7 +49,7 @@ class Threadweaver < Formula
     qt5_args = ["-DQt5Core_DIR=#{Formula["qt@5"].opt_lib}/cmake/Qt5Core"]
     qt5_args << "-DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath,#{Formula["qt@5"].opt_lib}" unless OS.mac?
     system "cmake", (pkgshare/"examples/HelloWorld"), *std_cmake_args, *qt5_args
-    system "make"
+    system "cmake", "--build", "."
 
     assert_equal "Hello World!", shell_output("./ThreadWeaver_HelloWorld 2>&1").strip
   end
