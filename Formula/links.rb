d68f18a83be9bbecf4abb012cc2abb22b5fff37f
Adrian Ho <the.gromgit@gmail.com>
2022-06-12 00:51:59 +0800

    links: update 2.27 bottle.

--- Formula/links.rb
+++ Formula/links.rb
@@ -1,8 +1,8 @@
 class Links < Formula
   desc "Lynx-like WWW browser that supports tables, menus, etc."
   homepage "http://links.twibright.com/"
-  url "http://links.twibright.com/download/links-2.25.tar.gz"
-  sha256 "5c0b3b0b8fe1f3c8694f5fb7fbdb19c63278ac68ae4646da69b49640b20283b1"
+  url "http://links.twibright.com/download/links-2.27.tar.gz"
+  sha256 "b3e7f302e748f6394806aaac28ea878dbfa2af38745d96507adf68a0a541ba8b"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -11,13 +11,8 @@ class Links < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "5d69fd6ef3b865956b1975123671932ad4d0b9a9f0b662ed412519debe748809"
-    sha256 cellar: :any,                 arm64_big_sur:  "b3e9ad1d3617acdbbc86edbc90f429083130a51eb6ea26b809774b8eaf39fa53"
-    sha256 cellar: :any,                 monterey:       "77261cf3aeedfe6b058734536d55b744b80bb52c1cf777a2d78497bd1f4c69e2"
-    sha256 cellar: :any,                 big_sur:        "9d7178901279a9ee80768056fa4f2b04676777d5ef2063c9f8a8c2b9f98a8906"
-    sha256 cellar: :any,                 catalina:       "1eaa9411f73c61b7a82ee8d16b94bd7c8c0dde86e1eec953c5ce5872394ee073"
-    sha256 cellar: :any,                 mojave:         "c12924230d58173c263dcdd827ecb4d9f44bb1e654a191ff5882a46330125cdf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f6bbd8bae1431f150cc3153f2731cf41cf4fbf06b59033a28b5ac8b72906a792"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/links"
+    sha256 cellar: :any, mojave: "76b09bc9657b8d21ca29d89aa21bc31e95dd98c2e7a8e90e6caf210fb9219bc9"
   end
 
   depends_on "pkg-config" => :build
