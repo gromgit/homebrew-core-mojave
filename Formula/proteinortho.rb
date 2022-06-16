968066a6f9a4c659cc734a0c38f639663b98e1a7
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 04:20:45 +0800

    proteinortho: update 6.1.0 bottle.

--- Formula/proteinortho.rb
+++ Formula/proteinortho.rb
@@ -1,13 +1,13 @@
 class Proteinortho < Formula
   desc "Detecting orthologous genes within different species"
   homepage "https://gitlab.com/paulklemm_PHD/proteinortho"
-  url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.0.35/proteinortho-v6.0.35.tar.gz"
-  sha256 "1aa4a887d82a1074f1032d5a56858b35505d4c9937080a4f366fbdcc83e483d3"
+  url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.1.0/proteinortho-v6.1.0.tar.gz"
+  sha256 "4c087cbfd91051136df808a679694ab2ada3c266c175b4187689f302e8ccf8ac"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/proteinortho"
-    sha256 cellar: :any, mojave: "f5f1f584265e82b9d76cc164f779f2d5e0bd83dcfba86bc73c613ca8c8e5a714"
+    sha256 cellar: :any, mojave: "9cb4c6280b4719eeea514193ea0ed04fc9ba0ce215659dff2663f0e369f03c7b"
   end
 
   depends_on "diamond"
