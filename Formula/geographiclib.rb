4fd9cb40beeca247564f0d3737ad9690dfbe96e8
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 00:21:35 +0800

    geographiclib: update 2.1 bottle.

--- Formula/geographiclib.rb
+++ Formula/geographiclib.rb
@@ -1,13 +1,13 @@
 class Geographiclib < Formula
   desc "C++ geography library"
   homepage "https://geographiclib.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/geographiclib/distrib-C++/GeographicLib-2.0.tar.gz"
-  sha256 "906b862aa9e988534fd5b8d9f3bae07437e0079a4236e19942ab61fe8c83960b"
+  url "https://downloads.sourceforge.net/project/geographiclib/distrib-C++/GeographicLib-2.1.tar.gz"
+  sha256 "7a4bdbcfe76c7848960f177b597187e16abd30140da067ff5221cee900cfc029"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/geographiclib"
-    sha256 cellar: :any, mojave: "b018aefa67ca9b1b5cb8e59d819e2759d7962e7f227747a8e6d385c6d79006ca"
+    sha256 cellar: :any, mojave: "36565c6f4151cee466c0fa64293c63b3eb0181e40daadc4f4171712eb3cd1d5c"
   end
 
   depends_on "cmake" => :build
