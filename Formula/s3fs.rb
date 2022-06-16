6858b5d0e8797ec20c57cc9b0741502a355eb91c
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:12:42 +0800

    s3fs:

--- Formula/s3fs.rb
+++ Formula/s3fs.rb
@@ -1,15 +1,13 @@
 class S3fs < Formula
   desc "FUSE-based file system backed by Amazon S3"
   homepage "https://github.com/s3fs-fuse/s3fs-fuse/wiki"
-  url "https://github.com/s3fs-fuse/s3fs-fuse/archive/v1.87.tar.gz"
-  sha256 "c5e064efb8fb5134a463731a7cf8d7174c93a296957145200347d2f4d9d11985"
+  url "https://github.com/s3fs-fuse/s3fs-fuse/archive/refs/tags/v1.91.tar.gz"
+  sha256 "f130fec375dc6972145c56f53e83ea7c98c82621406d0208a328989e5d900b0f"
   license "GPL-2.0-or-later"
   head "https://github.com/s3fs-fuse/s3fs-fuse.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, catalina:    "5183ab606057fbe8e46a737b25c1ad4e82dd67389f48827d7bfd567c67cf8417"
-    sha256 cellar: :any, mojave:      "d691bdeb4abd443bc1f1f3de46c286a97829f95ba3d47b026e535a6688085d07"
-    sha256 cellar: :any, high_sierra: "f475d03b68102dd400a22de99b9ddc044653f6658e2cb84349adf507ffbddcad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "40366ae4c296e3095014f8639d77d0ede2b0f8f960d305a8ed43f0b160702fff"
   end
 
   depends_on "autoconf" => :build
@@ -19,12 +17,14 @@ class S3fs < Formula
   depends_on "libgcrypt"
   depends_on "nettle"
 
+  uses_from_macos "curl"
+
   on_macos do
     disable! date: "2021-04-08", because: "requires closed-source macFUSE"
   end
 
   on_linux do
-    depends_on "libfuse"
+    depends_on "libfuse@2"
   end
 
   def install
