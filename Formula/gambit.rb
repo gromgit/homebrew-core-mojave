bb67748aed70232f8cf5c7d95064b3e8650424af
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 00:05:15 +0800

    gambit: update 16.0.2_2 bottle.

--- Formula/gambit.rb
+++ Formula/gambit.rb
@@ -3,12 +3,12 @@ class Gambit < Formula
   homepage "http://www.gambit-project.org"
   url "https://github.com/gambitproject/gambit/archive/v16.0.2.tar.gz"
   sha256 "49837f2ccb9bb65dad2f3bba9c436c7a7df8711887e25f6bf54b074508a682d4"
-  license "Apache-2.0"
-  revision 1
+  license all_of: ["GPL-2.0-or-later", "Zlib"]
+  revision 2
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gambit"
-    sha256 cellar: :any, mojave: "d7bd9445036ef1bf6fa0a8d4140552756c8908a760c82da7d249048463837d54"
+    sha256 cellar: :any, mojave: "5a8a613dbaf62c29a8085239ab3c69e3d6160eeda3741374eeb8c0dd69d1d715"
   end
 
   depends_on "autoconf" => :build
