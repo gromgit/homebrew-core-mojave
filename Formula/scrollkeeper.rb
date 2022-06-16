6b200bf1d2d516b1edab5f24a8054ecdf3ebfe6e
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:12:43 +0800

    scrollkeeper: no rebuilding required

--- Formula/scrollkeeper.rb
+++ Formula/scrollkeeper.rb
@@ -7,12 +7,14 @@ class Scrollkeeper < Formula
   revision 2
 
   bottle do
-    sha256 arm64_big_sur: "88e96878a9f7cef658edaf418c55f7c9e6904aead82bd3102384cbdfb342a400"
-    sha256 big_sur:       "0cdfc1e87fe8d2281867eb923dfce700906894f6593a763fe79f4afc936f2ad2"
-    sha256 catalina:      "9bd348638b9e3492db3549c7ac0756975ca2c57303ec58685bb3e6694fff1dd1"
-    sha256 mojave:        "0d7cbee6e25a46848d7c387ba07c4ee110ae2256953d2e5addd26f68e21c645d"
-    sha256 high_sierra:   "efa4637b9d1b3942192dca6fb4602ef72ec6b285ba424c087d290c8feb5e2c5b"
-    sha256 x86_64_linux:  "81a0061c55b44717079288661e0ecaac1b22d6a72c72b89e98237eb51c099539"
+    sha256 arm64_monterey: "6de06691bc02972907fa268ce8501559f794454734b2d94894bd41f19cd33c4c"
+    sha256 arm64_big_sur:  "88e96878a9f7cef658edaf418c55f7c9e6904aead82bd3102384cbdfb342a400"
+    sha256 monterey:       "a9b9a811e7d62586377152f75794cb92b57782611622815816f779a823427cb4"
+    sha256 big_sur:        "0cdfc1e87fe8d2281867eb923dfce700906894f6593a763fe79f4afc936f2ad2"
+    sha256 catalina:       "9bd348638b9e3492db3549c7ac0756975ca2c57303ec58685bb3e6694fff1dd1"
+    sha256 mojave:         "0d7cbee6e25a46848d7c387ba07c4ee110ae2256953d2e5addd26f68e21c645d"
+    sha256 high_sierra:    "efa4637b9d1b3942192dca6fb4602ef72ec6b285ba424c087d290c8feb5e2c5b"
+    sha256 x86_64_linux:   "81a0061c55b44717079288661e0ecaac1b22d6a72c72b89e98237eb51c099539"
   end
 
   depends_on "docbook"
@@ -20,6 +22,13 @@ class Scrollkeeper < Formula
 
   uses_from_macos "libxslt"
   uses_from_macos "perl"
+
+  on_macos do
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "intltool" => :build
+    depends_on "libtool" => :build
+  end
 
   conflicts_with "rarian",
     because: "scrollkeeper and rarian install the same binaries"
@@ -45,6 +54,9 @@ class Scrollkeeper < Formula
       end
     end
 
+    # Run autoreconf on macOS to rebuild configure script so that it doesn't try
+    # to build with a flat namespace.
+    system "autoreconf", "--force", "--verbose", "--install" if OS.mac?
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}",
                           "--with-xml-catalog=#{etc}/xml/catalog"
