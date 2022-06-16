e3c45b1b45ad6aa694b6ed7a408acb6254bba512
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:13:09 +0800

    xlispstat: no rebuilding required

--- Formula/xlispstat.rb
+++ Formula/xlispstat.rb
@@ -7,12 +7,14 @@ class Xlispstat < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "1c7230181f7447fb264b14c84d8a6a2e3396faec78af73174ed6543f19536a8a"
-    sha256 cellar: :any,                 big_sur:       "30bde68dbe2eada5b7646e5ef4b6fc0f804be39f37ae75244955b3befe803036"
-    sha256 cellar: :any,                 catalina:      "d2e8f57e8dc13c6b1aaa38af29d291b5974b642626599cf478f3997e2981643a"
-    sha256 cellar: :any,                 mojave:        "2ad96a0eaeadb61b6eae731c7f8caf19ce6a202b4fab65d474e135c0731b8022"
-    sha256 cellar: :any,                 high_sierra:   "66e03a45aad7571b1a51c5196236099f11884ee055e7b45fcbdb19d4ae682e90"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "45f4149aaf05fa78c789492de2867074891abc4816a5b6abbf04d06433c135d6"
+    sha256 cellar: :any,                 arm64_monterey: "e11e76582f1aa365ed04b44c6caac0a786b63f12b612399e27ed909803c1bdcf"
+    sha256 cellar: :any,                 arm64_big_sur:  "1c7230181f7447fb264b14c84d8a6a2e3396faec78af73174ed6543f19536a8a"
+    sha256 cellar: :any,                 monterey:       "9d418608c03816945f00a2ccbf93def9a54e6f8b9c00c93b7835a287cf7c2305"
+    sha256 cellar: :any,                 big_sur:        "30bde68dbe2eada5b7646e5ef4b6fc0f804be39f37ae75244955b3befe803036"
+    sha256 cellar: :any,                 catalina:       "d2e8f57e8dc13c6b1aaa38af29d291b5974b642626599cf478f3997e2981643a"
+    sha256 cellar: :any,                 mojave:         "2ad96a0eaeadb61b6eae731c7f8caf19ce6a202b4fab65d474e135c0731b8022"
+    sha256 cellar: :any,                 high_sierra:    "66e03a45aad7571b1a51c5196236099f11884ee055e7b45fcbdb19d4ae682e90"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "45f4149aaf05fa78c789492de2867074891abc4816a5b6abbf04d06433c135d6"
   end
 
   depends_on "libx11"
