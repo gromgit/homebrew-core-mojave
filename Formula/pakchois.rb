718bf618d53a5635c86f75a42b73d7acf6dc0b60
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:12:27 +0800

    pakchois: no rebuilding required

--- Formula/pakchois.rb
+++ Formula/pakchois.rb
@@ -12,14 +12,19 @@ class Pakchois < Formula
 
   bottle do
     rebuild 3
-    sha256 cellar: :any,                 arm64_big_sur: "86e77a851ff2c0de31cf5e4f2587711b7b1bcc742fb878df1ce69fde836fd864"
-    sha256 cellar: :any,                 big_sur:       "fd91b09bb010ac37483a910b0431c6082903ee843a15f4cc767bde57ce0b7267"
-    sha256 cellar: :any,                 catalina:      "ca82f2950582bc54e46122eb71ff8e8acdc739772baf53ab2d545755f03303f8"
-    sha256 cellar: :any,                 mojave:        "cc98c7b706f27320ee7c673d906b4da22b402afe0d93b4c66f73a8cde86f7929"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e9e96d8cef014042091db67065c8b02439cba4cfd381ca7651bc721ec120ad4a"
+    sha256                               arm64_monterey: "fb701bf9c363b4e09f285fa9c6f60aba921a93f826c37afd5fcc54b0bc5e4ed1"
+    sha256 cellar: :any,                 arm64_big_sur:  "86e77a851ff2c0de31cf5e4f2587711b7b1bcc742fb878df1ce69fde836fd864"
+    sha256                               monterey:       "f53bdb6cb2f47b0ed2e1df5f2c80f9568c3b2c55463de2cfdfe742da945364c0"
+    sha256 cellar: :any,                 big_sur:        "fd91b09bb010ac37483a910b0431c6082903ee843a15f4cc767bde57ce0b7267"
+    sha256 cellar: :any,                 catalina:       "ca82f2950582bc54e46122eb71ff8e8acdc739772baf53ab2d545755f03303f8"
+    sha256 cellar: :any,                 mojave:         "cc98c7b706f27320ee7c673d906b4da22b402afe0d93b4c66f73a8cde86f7929"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e9e96d8cef014042091db67065c8b02439cba4cfd381ca7651bc721ec120ad4a"
   end
 
   def install
+    # Fix flat namespace usage
+    inreplace "configure", "${wl}-flat_namespace ${wl}-undefined ${wl}suppress", "${wl}-undefined ${wl}dynamic_lookup"
+
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
