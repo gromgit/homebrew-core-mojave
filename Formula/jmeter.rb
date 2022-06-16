95e8e568c78c24e5552c3165096188737286c299
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:11:59 +0800

    jmeter: no rebuilding required

--- Formula/jmeter.rb
+++ Formula/jmeter.rb
@@ -1,13 +1,13 @@
 class Jmeter < Formula
   desc "Load testing and performance measurement application"
   homepage "https://jmeter.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=jmeter/binaries/apache-jmeter-5.4.3.tgz"
-  mirror "https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.4.3.tgz"
-  sha256 "72521214c0cb87dac5b974c1c6e83a17a009c7fd377b55bf23525c72403b1dee"
+  url "https://www.apache.org/dyn/closer.lua?path=jmeter/binaries/apache-jmeter-5.5.tgz"
+  mirror "https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-5.5.tgz"
+  sha256 "60e89c7c4523731467fdb717f33d614086c10f0316369cbaa45650ae1c402e1f"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "71af8d00cd769598a7a1291565a1d15d97f9ef78ea4dc23d0025b8d4d503e76a"
+    sha256 cellar: :any_skip_relocation, all: "ad4c2d2fd4fe481f9443824e6555240d68b26bd01a7ce8444f755fe20f32e51e"
   end
 
   depends_on "openjdk"
