25208807ca59b112a65d400bd92eaf59e6c5c714
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:12:19 +0800

    nickle: no rebuilding required

--- Formula/nickle.rb
+++ Formula/nickle.rb
@@ -11,6 +11,7 @@ class Nickle < Formula
   end
 
   bottle do
+    sha256 monterey:     "b1258d434acb09bb06f6a466a0e895fabbe2469c38d45da48ca8fe8a98d64e4a"
     sha256 big_sur:      "6e377f6674d6609f634b28941d8c53fef94c9cb429f31d1c765e4a5d8607e88d"
     sha256 catalina:     "3e1d028467ee41d963e9eaa9809f288fbc3effd826e09ae69bd4e4bfd26679c5"
     sha256 mojave:       "6fa77667c30e0dfa186868159076bd2e003c34d32624915481f8c52e68b97f23"
