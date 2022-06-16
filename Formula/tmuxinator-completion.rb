d8f93941b0b3601a6faec4887b3303e40ecb09f4
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:12:55 +0800

    tmuxinator-completion: no rebuilding required

--- Formula/tmuxinator-completion.rb
+++ Formula/tmuxinator-completion.rb
@@ -1,8 +1,8 @@
 class TmuxinatorCompletion < Formula
   desc "Shell completion for Tmuxinator"
   homepage "https://github.com/tmuxinator/tmuxinator"
-  url "https://github.com/tmuxinator/tmuxinator/archive/v3.0.2.tar.gz"
-  sha256 "3992e4a13113ebc6d1d7571c2fae05a056655ee7bfd2eba1455242e30a6dcdd8"
+  url "https://github.com/tmuxinator/tmuxinator/archive/v3.0.4.tar.gz"
+  sha256 "e2bfbab36603f6fd129f093af056c7dd1b57db47fcb2dc89acfe89a095841e0b"
   license "MIT"
   head "https://github.com/tmuxinator/tmuxinator.git", branch: "master"
 
@@ -11,7 +11,7 @@ class TmuxinatorCompletion < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "89c2778c88287f00e00f005f4d0d6fa84b5913ad7c0cf53442e7378a8a647b47"
+    sha256 cellar: :any_skip_relocation, all: "c5d4757276f8cf1c482ea3f2df0fe35e15b5c94b764eeaa935108f4283347458"
   end
 
   conflicts_with "tmuxinator", because: "the tmuxinator formula includes completion"
