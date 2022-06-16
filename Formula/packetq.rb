d8b92042f70005dac295b6950419fdf8f4146c6a
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 03:31:31 +0800

    packetq: update 1.7.1 bottle.

--- Formula/packetq.rb
+++ Formula/packetq.rb
@@ -1,8 +1,8 @@
 class Packetq < Formula
   desc "SQL-like frontend to PCAP files"
   homepage "https://www.dns-oarc.net/tools/packetq"
-  url "https://www.dns-oarc.net/files/packetq/packetq-1.7.0.tar.gz"
-  sha256 "6c275d1a0139ed191973593895ac0b313866a4bfb832e969eec0650d1c03f82f"
+  url "https://www.dns-oarc.net/files/packetq/packetq-1.7.1.tar.gz"
+  sha256 "a1b087335fcb018a5ded3d067d22ee906d24b6e932f018e959302be9b527c620"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -12,7 +12,7 @@ class Packetq < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/packetq"
-    sha256 cellar: :any_skip_relocation, mojave: "781af722f758cf52158d3b14520a2d29ede07a2048ad972cc62101b319eb707e"
+    sha256 cellar: :any_skip_relocation, mojave: "0cf2638b67db04d5a4ce93d15d1aa21385a69572876feffc9eec7e4f48e1cda1"
   end
 
   uses_from_macos "zlib"
