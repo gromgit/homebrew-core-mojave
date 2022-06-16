b3221ca35466a25e29b31d710b2e0c3dbd614f4d
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:11:36 +0800

    cmu-sphinxbase: no rebuilding required

--- Formula/cmu-sphinxbase.rb
+++ Formula/cmu-sphinxbase.rb
@@ -14,11 +14,6 @@ class CmuSphinxbase < Formula
     end
   end
 
-  livecheck do
-    url :stable
-    regex(%r{url=.*?/sphinxbase[._-]v?(\d+(?:\.\d+)+)\.t}i)
-  end
-
   bottle do
     rebuild 1
     sha256 cellar: :any,                 arm64_monterey: "10c702ad300d51ffac6ed0251cf3b64952d549ad0a67792b80fe055a182014f2"
@@ -33,13 +28,15 @@ class CmuSphinxbase < Formula
   end
 
   head do
-    url "https://github.com/cmusphinx/sphinxbase.git"
+    url "https://github.com/cmusphinx/sphinxbase.git", branch: "master"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
     depends_on "swig" => :build
   end
+
+  deprecate! date: "2022-06-12", because: :repo_archived
 
   depends_on "pkg-config" => :build
   # If these are found, they will be linked against and there is no configure
