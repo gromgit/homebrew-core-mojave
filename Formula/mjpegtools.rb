60cfc91a096436017dd66afbadde1903ac969312
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 03:05:09 +0800

    mjpegtools: update 2.2.1_1 bottle.

--- Formula/mjpegtools.rb
+++ Formula/mjpegtools.rb
@@ -4,19 +4,15 @@ class Mjpegtools < Formula
   url "https://downloads.sourceforge.net/project/mjpeg/mjpegtools/2.2.1/mjpegtools-2.2.1.tar.gz"
   sha256 "b180536d7d9960b05e0023a197b00dcb100929a49aab71d19d55f4a1b210f49a"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "641462f37bf9b4525a1f1ff5310763cc518bd135fe7a71b3db707d1f175010d5"
-    sha256 cellar: :any,                 arm64_big_sur:  "57f69f4a147c3e886146105b9f2ea4f6edb15bd2f5c9e3c648af5781590f1389"
-    sha256 cellar: :any,                 monterey:       "b03c31418ea5d42e4c88a6160209ac4798dc628574ddb1d08b7cdee4e86ab9e2"
-    sha256 cellar: :any,                 big_sur:        "df7c071afa72c7fcb3e3abf340450d34f05ab1b81655ae57d48f4a3c516a7067"
-    sha256 cellar: :any,                 catalina:       "2b41e5e3f6abf5fa6767757128b8913fa5932919a02b86aba9dc3a4c4302be24"
-    sha256 cellar: :any,                 mojave:         "1c5d0fb366a3d4b0da30a2a2da4c8bc3f90cbe9238c512bfab57a4273855c98b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5cfd7380321fa05242d23477ea708de3c2b9019043f0a0b80c376d79323fdb1b"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mjpegtools"
+    sha256 cellar: :any, mojave: "0b00e6ff8fe6d9c1908b51d38da3cca2d2522e9b48e39c316c33a2fe6dc8d31b"
   end
 
   depends_on "pkg-config" => :build
-  depends_on "jpeg"
+  depends_on "jpeg-turbo"
 
   # Fix -flat_namespace being used on Big Sur and later.
   patch do
@@ -25,9 +21,7 @@ class Mjpegtools < Formula
   end
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--enable-simd-accel",
-                          "--prefix=#{prefix}"
+    system "./configure", *std_configure_args, "--enable-simd-accel"
     system "make", "install"
   end
 end
