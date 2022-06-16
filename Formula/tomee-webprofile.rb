733015c7fb4abf226132d17b2b6a0b1b9822f7a6
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:12:57 +0800

    tomee-webprofile: no rebuilding required

--- Formula/tomee-webprofile.rb
+++ Formula/tomee-webprofile.rb
@@ -1,13 +1,13 @@
 class TomeeWebprofile < Formula
   desc "All-Apache Java EE 7 Web Profile stack"
   homepage "https://tomee.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-8.0.11/apache-tomee-8.0.11-webprofile.tar.gz"
-  mirror "https://archive.apache.org/dist/tomee/tomee-8.0.11/apache-tomee-8.0.11-webprofile.tar.gz"
-  sha256 "55fdc13771e7d97524e22a6c315594f1d8cf8a6ded088257cffdb541165128b5"
+  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-8.0.12/apache-tomee-8.0.12-webprofile.tar.gz"
+  mirror "https://archive.apache.org/dist/tomee/tomee-8.0.12/apache-tomee-8.0.12-webprofile.tar.gz"
+  sha256 "56f12a31a7f75c4683b299666b7a0347b57fa1e224f1d007c875618f727307c6"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "c1f98888d8a277e1888a2d274bd1f764812530dc1ba2f67fb01af0832c60826f"
+    sha256 cellar: :any_skip_relocation, all: "cf5cf820b5514895fae689b0e87dd477b3f671b5750017b2dbfd7e4686d309f6"
   end
 
   depends_on "openjdk"
