e96479b6bf5cd403c1f6d038531733eb3c3d746c
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 00:46:29 +0800

    htmldoc: update 1.9.16_1 bottle.

--- Formula/htmldoc.rb
+++ Formula/htmldoc.rb
@@ -4,22 +4,28 @@ class Htmldoc < Formula
   url "https://github.com/michaelrsweet/htmldoc/archive/v1.9.16.tar.gz"
   sha256 "f0d19d8be0fd961d07556f85dbea1d95f0d38728a45dc0f2cf92c715e4140542"
   license "GPL-2.0-only"
+  revision 1
   head "https://github.com/michaelrsweet/htmldoc.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/htmldoc"
-    sha256 mojave: "79795c3201b03c8064b51ae8319ea573aef884afede8dff8162955ff928e0ac1"
+    sha256 mojave: "82caf181d9bc459343f473f83554bac52f399e0b1f265ecc3150f9035709995a"
   end
 
   depends_on "pkg-config" => :build
-  depends_on "jpeg"
+  depends_on "jpeg-turbo"
   depends_on "libpng"
 
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "gnutls"
+  end
+
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-ssl",
-                          "--prefix=#{prefix}",
-                          "--mandir=#{man}"
+    system "./configure", *std_configure_args,
+                          "--mandir=#{man}",
+                          "--without-gui"
     system "make"
     system "make", "install"
   end
