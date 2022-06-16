03b2b1c690bbf562cf266a8291a4af4a7d3ceaf0
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 18:12:44 +0800

    akamai: update 1.5.1 bottle.

--- Formula/akamai.rb
+++ Formula/akamai.rb
@@ -1,13 +1,13 @@
 class Akamai < Formula
   desc "CLI toolkit for working with Akamai's APIs"
   homepage "https://github.com/akamai/cli"
-  url "https://github.com/akamai/cli/archive/refs/tags/v1.5.0.tar.gz"
-  sha256 "41687c6d4945094a9837dd2966bdd374d8265062d8547235d1c5a136dd08f79e"
+  url "https://github.com/akamai/cli/archive/refs/tags/v1.5.1.tar.gz"
+  sha256 "58269ab6ab93a4b4e0a773fd62efae5f1aefc74fc6bdfcf0fdf5dbce75998146"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/akamai"
-    sha256 cellar: :any_skip_relocation, mojave: "00e72c516e5916c87a0f77f680823eb7437e940f0fc88186105b7fa4d8b55b05"
+    sha256 cellar: :any_skip_relocation, mojave: "a7b164359aefb62963b48fc2e1a7d6d1ec6c600954475986229b60e168f26e1e"
   end
 
   depends_on "go" => [:build, :test]
