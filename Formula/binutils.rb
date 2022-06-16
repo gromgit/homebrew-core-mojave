d7aa6c98877427dec2fb4e3d87209440d01ac974
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 21:27:59 +0800

    binutils: update 2.38 bottle.

--- Formula/binutils.rb
+++ Formula/binutils.rb
@@ -1,19 +1,14 @@
 class Binutils < Formula
   desc "GNU binary tools for native development"
   homepage "https://www.gnu.org/software/binutils/binutils.html"
-  url "https://ftp.gnu.org/gnu/binutils/binutils-2.37.tar.xz"
-  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.37.tar.xz"
-  sha256 "820d9724f020a3e69cb337893a0b63c2db161dadcb0e06fc11dc29eb1e84a32c"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.38.tar.xz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.38.tar.xz"
+  sha256 "e316477a914f567eccc34d5d29785b8b0f5a10208d36bbacedcc39048ecfe024"
   license all_of: ["GPL-2.0-or-later", "GPL-3.0-or-later", "LGPL-2.0-or-later", "LGPL-3.0-only"]
 
   bottle do
-    sha256 arm64_monterey: "189bea0bf26bbde02e13aa60e16cb7f4fb33c6dc601029393c4c39ee415e5a0a"
-    sha256 arm64_big_sur:  "2bf192cb717e8e107e61899218d8d25db92d8c07d164b2dea9c50a3d41b0dca9"
-    sha256 monterey:       "feca3b1dbaf91e243e47d261411977909b68b2d54d7e5b1b93c9150a7bc698ad"
-    sha256 big_sur:        "9757e5cac1e7fd0046d02671d31f608e57c2398a2d3a0042518707a5fe6fb30c"
-    sha256 catalina:       "0c90a75475fc973066ace915f8f58f83bf5009181e42c9c8140dc72453d53d0f"
-    sha256 mojave:         "58bf91ff243d080224bd9f7170307788d4319ccdd96d17e7afbf0f326a639f97"
-    sha256 x86_64_linux:   "7dcdd47b180a4dfcc838fa0a047f6fbfac0cd37bd867170ac026e6b8ae93af5d"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/binutils"
+    sha256 mojave: "213787d051d385742fa8d0cded0ab4f817dc903dd1a379f69d4dbd35f313689a"
   end
 
   keg_only :shadowed_by_macos, "Apple's CLT provides the same tools"
