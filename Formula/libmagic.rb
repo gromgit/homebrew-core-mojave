273c0963bb3c8dc09c7c6636cdcca7df1580dbf2
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 22:11:34 +0800

    libmagic: update 5.42 bottle.

--- Formula/libmagic.rb
+++ Formula/libmagic.rb
@@ -1,8 +1,8 @@
 class Libmagic < Formula
   desc "Implementation of the file(1) command"
   homepage "https://www.darwinsys.com/file/"
-  url "https://astron.com/pub/file/file-5.41.tar.gz"
-  sha256 "13e532c7b364f7d57e23dfeea3147103150cb90593a57af86c10e4f6e411603f"
+  url "https://astron.com/pub/file/file-5.42.tar.gz"
+  sha256 "c076fb4d029c74073f15c43361ef572cfb868407d347190ba834af3b1639b0e4"
   # libmagic has a BSD-2-Clause-like license
   license :cannot_represent
 
@@ -11,13 +11,8 @@ class Libmagic < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "35178fd8f00c95f6f576cb98809ba1a8778c4ff6afb3fd5dafe5122fb9172188"
-    sha256 arm64_big_sur:  "b851d23643cbb4558264dce05ae04afb67188753ed9631707ba24177f4d8b618"
-    sha256 monterey:       "e77cc066068ef97ec61fdecd1c59229baad49e82aac3be24d8b1cce37cac0045"
-    sha256 big_sur:        "90a9c204356d026e45f276ff7466b2afe11593b8992ae2f4e199951d973124a7"
-    sha256 catalina:       "bd9af8b940362c68313c49ea0481e8b705514fd558582cfa609548aea3e03b01"
-    sha256 mojave:         "ee176ca0e970104d6f4d59c752e9ce94433b1ae4e09ae12bdd5daf45e2a332f1"
-    sha256 x86_64_linux:   "045b433b1e5ce7a3adf028ce87dd03b8a28255cad4547ec318772b242523080d"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libmagic"
+    sha256 mojave: "7a9fe03a028b0f2d50f4d33743853b46af81a1a4e1d4369e1cf75d7238608521"
   end
 
   uses_from_macos "zlib"
