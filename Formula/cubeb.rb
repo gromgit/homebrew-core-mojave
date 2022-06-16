62066d09cfb617024f7e8ff164027a35873d2517
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:11:39 +0800

    cubeb: no rebuilding required

--- Formula/cubeb.rb
+++ Formula/cubeb.rb
@@ -7,14 +7,16 @@ class Cubeb < Formula
   head "https://github.com/kinetiknz/cubeb.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e56366a9d51f95c573e9bcc0a7f8985e4607cf88a9e6a87c0f2193a363c18a93"
-    sha256 cellar: :any, big_sur:       "06c2e45c008f9b2c6068c5ccb4adf3d4d7ca75e4b0b25429af1577391a6b2d8b"
-    sha256 cellar: :any, catalina:      "98061577ff4699c6d87158764616203f1bc758a858c88683fd7f7f10f90e74b5"
-    sha256 cellar: :any, mojave:        "d34baf923b56edec2ae8201857c55426584f35b47ef8e2e6577a38f684fbab75"
-    sha256 cellar: :any, high_sierra:   "618debffabe494dcde3e0d7e2231078df124ead8ee342886ab38ad7373f73e37"
-    sha256 cellar: :any, sierra:        "f89e89027370ea9da99f72f0af0529f9b63fbe31c434d3ccafdc7230664a41c2"
-    sha256 cellar: :any, el_capitan:    "f7e738b374bb07e1c420e56dfeb72caa814495b446c71d8158ef98c9b33d3a60"
-    sha256 cellar: :any, yosemite:      "b3cff6ba7008cc764f94281f7759f5d6d2a09a3bdb92f5f6e93be7d6f3ec2405"
+    sha256 cellar: :any, arm64_monterey: "506fb6090f05b4275bde1aff78c0eb1bf72959fbeac5c53018c728863ef1195f"
+    sha256 cellar: :any, arm64_big_sur:  "e56366a9d51f95c573e9bcc0a7f8985e4607cf88a9e6a87c0f2193a363c18a93"
+    sha256 cellar: :any, monterey:       "0734f84782c17da435dc805f42c1af96506669ed1337aa8a0a20f486975d771a"
+    sha256 cellar: :any, big_sur:        "06c2e45c008f9b2c6068c5ccb4adf3d4d7ca75e4b0b25429af1577391a6b2d8b"
+    sha256 cellar: :any, catalina:       "98061577ff4699c6d87158764616203f1bc758a858c88683fd7f7f10f90e74b5"
+    sha256 cellar: :any, mojave:         "d34baf923b56edec2ae8201857c55426584f35b47ef8e2e6577a38f684fbab75"
+    sha256 cellar: :any, high_sierra:    "618debffabe494dcde3e0d7e2231078df124ead8ee342886ab38ad7373f73e37"
+    sha256 cellar: :any, sierra:         "f89e89027370ea9da99f72f0af0529f9b63fbe31c434d3ccafdc7230664a41c2"
+    sha256 cellar: :any, el_capitan:     "f7e738b374bb07e1c420e56dfeb72caa814495b446c71d8158ef98c9b33d3a60"
+    sha256 cellar: :any, yosemite:       "b3cff6ba7008cc764f94281f7759f5d6d2a09a3bdb92f5f6e93be7d6f3ec2405"
   end
 
   depends_on "autoconf" => :build
