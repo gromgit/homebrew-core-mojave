c4581820cf08aa07f7589a2661f8c6aa26d337ee
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:12:32 +0800

    pillar: no rebuilding required

--- Formula/pillar.rb
+++ Formula/pillar.rb
@@ -7,6 +7,7 @@ class Pillar < Formula
   revision 1
 
   bottle do
+    sha256 cellar: :any_skip_relocation, monterey:     "75a3f4f0ac66b98d05a55687f126fadca0dc86c8a82c5b97b2cf22a1db98615e"
     sha256 cellar: :any_skip_relocation, big_sur:      "4edab61108a48ddf41f90c46872bbced08a6fb600ed84b8faa2a270be2d4eea4"
     sha256 cellar: :any_skip_relocation, catalina:     "8aac25711310b56913c1838c9d6b4ef72af78ade7b20ca0f5b8519805854e285"
     sha256 cellar: :any_skip_relocation, mojave:       "935f68b739a2d86174a045032b5606fffb8c1fa4f7ef74fd0aabc6608dfe068a"
