113fb55a081f90fe68898580b7638305a9057649
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 04:23:53 +0800

    pugixml: update 1.12.1_1 bottle.

--- Formula/pugixml.rb
+++ Formula/pugixml.rb
@@ -4,24 +4,26 @@ class Pugixml < Formula
   url "https://github.com/zeux/pugixml/releases/download/v1.12.1/pugixml-1.12.1.tar.gz"
   sha256 "dcf671a919cc4051210f08ffd3edf9e4247f79ad583c61577a13ee93af33afc7"
   license "MIT"
+  revision 1
 
   livecheck do
     url :stable
     strategy :github_latest
   end
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pugixml"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "0451e5005f9ab95bfbb9e5fe4acfab1e19091ee1e012404b6167c7f52d0bc8f5"
+    sha256 cellar: :any, mojave: "3232c33c8c2284314daed03d3b27ae62b5d0f14018d3a2df0a53347bf73391eb"
   end
 
   depends_on "cmake" => :build
 
   def install
-    system "cmake", ".", "-DBUILD_SHARED_AND_STATIC_LIBS=ON",
-                         "-DBUILD_PKGCONFIG=ON", *std_cmake_args
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args,
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DPUGIXML_BUILD_SHARED_AND_STATIC_LIBS=ON"
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
