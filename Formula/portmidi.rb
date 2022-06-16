7a749f54e5db095ccdc388afd7151f7b052b9209
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 03:57:23 +0800

    portmidi: update 2.0.3_1 bottle.

--- Formula/portmidi.rb
+++ Formula/portmidi.rb
@@ -4,12 +4,12 @@ class Portmidi < Formula
   url "https://github.com/PortMidi/portmidi/archive/refs/tags/v2.0.3.tar.gz"
   sha256 "934f80e1b09762664d995e7ab5a9932033bc70639e8ceabead817183a54c60d0"
   license "MIT"
+  revision 1
   version_scheme 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/portmidi"
-    rebuild 1
-    sha256 cellar: :any, mojave: "b661f57b3a870d8ac9ca17a78843b84b4cd804485b875697a85961e98f9a6b91"
+    sha256 cellar: :any, mojave: "f86f306df7b544be696cf81415ca0e926054ebd303f3404067a7fe2b0bc3168a"
   end
 
   depends_on "cmake" => :build
@@ -19,31 +19,17 @@ class Portmidi < Formula
   end
 
   def install
-    # need to create include/lib directories since make won't create them itself
-    include.mkpath
-    lib.mkpath
-    (lib/"pkgconfig").mkpath
-
-    if OS.mac?
+    if OS.mac? && MacOS.version <= :sierra
       # Fix "fatal error: 'os/availability.h' file not found" on 10.11 and
       # "error: expected function body after function declarator" on 10.12
-      # Requires the CLT to be the active developer directory if Xcode is installed
-      ENV["SDKROOT"] = MacOS.sdk_path if MacOS.version <= :sierra
-
-      inreplace "pm_mac/Makefile.osx", "PF=/usr/local", "PF=#{prefix}"
-
-      system "make", "-f", "pm_mac/Makefile.osx"
-      system "make", "-f", "pm_mac/Makefile.osx", "install"
-      mv lib/shared_library("libportmidi"), lib/shared_library("libportmidi", version)
-      # awaiting https://github.com/PortMidi/portmidi/issues/24
-      (lib/"pkgconfig").install "Release/packaging/portmidi.pc"
-    else
-      system "cmake", ".", *std_cmake_args, "-DCMAKE_CACHEFILE_DIR=#{buildpath}/build"
-      system "make", "install"
-      lib.install_symlink shared_library("libportmidi", version) => shared_library("libportmidi", 0)
+      # Requires the CLT to be the active developer directory if Xcode is
+      # installed
+      ENV["SDKROOT"] = MacOS.sdk_path
     end
-    lib.install_symlink shared_library("libportmidi", version) => shared_library("libportmidi")
-    lib.install_symlink shared_library("libportmidi", version) => shared_library("libportmidi", version.major.to_s)
+
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    system "make", "install"
   end
 
   test do
