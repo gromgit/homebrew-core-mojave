322278209b62946189d8524d0b4e42b848e6c945
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:12:25 +0800

    openliberty-microprofile4: no rebuilding required

--- Formula/openliberty-microprofile4.rb
+++ Formula/openliberty-microprofile4.rb
@@ -1,8 +1,8 @@
 class OpenlibertyMicroprofile4 < Formula
   desc "Lightweight open framework for Java (Micro Profile 4)"
   homepage "https://openliberty.io"
-  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/22.0.0.5/openliberty-microProfile4-22.0.0.5.zip"
-  sha256 "824be8903abcf2a968069f445c2bc594f957beb5653cf533039f46c860698645"
+  url "https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/release/22.0.0.6/openliberty-microProfile4-22.0.0.6.zip"
+  sha256 "ace1819a4f173678fe3ac651b5e6084d80aae4d59c306b351dbad21b6de47558"
   license "EPL-1.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class OpenlibertyMicroprofile4 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "54b2a28803a682d9d8c6da722f24f4f1415fc648a73c13948e32f0aa8fb7d9e1"
+    sha256 cellar: :any_skip_relocation, all: "9e5a5bdbb6011b3a6250f4f72a3fa1ab82548eacab13d76daf0a1c9ed632a5ba"
   end
 
   depends_on "openjdk"
