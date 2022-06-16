6dcd6d2afacf76a50eae3a1b4b6e2c2dd46cde5b
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:12:56 +0800

    tomee-plume: no rebuilding required

--- Formula/tomee-plume.rb
+++ Formula/tomee-plume.rb
@@ -1,13 +1,13 @@
 class TomeePlume < Formula
   desc "Apache TomEE Plume"
   homepage "https://tomee.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-8.0.11/apache-tomee-8.0.11-plume.tar.gz"
-  mirror "https://archive.apache.org/dist/tomee/tomee-8.0.11/apache-tomee-8.0.11-plume.tar.gz"
-  sha256 "1727706dd4010cec6d291bdc6aa9bdd9f274e1895c8367e896d0b51d6e0b2a6c"
+  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-8.0.12/apache-tomee-8.0.12-plume.tar.gz"
+  mirror "https://archive.apache.org/dist/tomee/tomee-8.0.12/apache-tomee-8.0.12-plume.tar.gz"
+  sha256 "3e3c9937b1e26ced4db5eb356e683131e3f4c0cc01ddc2ef3b460d30e2208e53"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "dad8ba85ad7e1ba9c55b482ffcf1f8a9b8642979f5abf2816be9dfcb97c7015e"
+    sha256 cellar: :any_skip_relocation, all: "252045e6c4e5896c8259f76665c74409afe503dfed10dd42949f5973b1db9cab"
   end
 
   depends_on "openjdk"
