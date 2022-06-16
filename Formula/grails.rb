13c2db0bafb5a2bf46cb7de41bdcd93b6ed34f88
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:11:53 +0800

    grails: no rebuilding required

--- Formula/grails.rb
+++ Formula/grails.rb
@@ -1,8 +1,8 @@
 class Grails < Formula
   desc "Web application framework for the Groovy language"
   homepage "https://grails.org"
-  url "https://github.com/grails/grails-core/releases/download/v5.1.7/grails-5.1.7.zip"
-  sha256 "4a4f95bee132cd9f6c49e23d7ceb87826812bbcf568eb8a22bfe9453f796a9a8"
+  url "https://github.com/grails/grails-core/releases/download/v5.1.8/grails-5.1.8.zip"
+  sha256 "c2c8f48dc8f6f95033a80a3df98298cd6c0e77494269eff4a45505825b41d846"
   license "Apache-2.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class Grails < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "96732300dc26d6ec7c7e94a46dd6ac7b382f8a89721d85ecf3fa6e6ed3c07414"
+    sha256 cellar: :any_skip_relocation, all: "d7bbf8d7924a1e2946b578d8aaa47e8c4089944ca345f3a4acb34b30324f864f"
   end
 
   depends_on "openjdk@11"
