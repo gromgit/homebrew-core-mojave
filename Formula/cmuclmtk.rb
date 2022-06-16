d0894482de49dc230ce7ea53df2fb67347df97ad
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:11:36 +0800

    cmuclmtk: no rebuilding required

--- Formula/cmuclmtk.rb
+++ Formula/cmuclmtk.rb
@@ -13,18 +13,26 @@ class Cmuclmtk < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "d3069c3fbd0f41bdb0b3435b7b388f9e6051639421658663185bde9a449185b8"
-    sha256 cellar: :any,                 big_sur:       "e126c9d5de2e1f4e23d4fea7e8ac51c6fc2d4328a968c907879f4ea86524fbbc"
-    sha256 cellar: :any,                 catalina:      "fb552e12a3c59e2ca6a9dd89e9ec229e5b815edef28093c3902fc4ee54b52207"
-    sha256 cellar: :any,                 mojave:        "5c71a1746a8ca516dc5d11858a7d0d85341cafeea31797b926eba3a9ed83d9ea"
-    sha256 cellar: :any,                 high_sierra:   "85a6d2a8fcad4e2b6e7d9d22ec74dd5e5f463dabc5b2f01373d3a48178b2ce6e"
-    sha256 cellar: :any,                 sierra:        "716c78af6b276392a20fb02d58ff60e705509117da932d62d3ff8c6e4dd0bf5d"
-    sha256 cellar: :any,                 el_capitan:    "c647327d709c3b4a93d5541f8b340d2726540c9efdcbc53d1124043c8c4989bd"
-    sha256 cellar: :any,                 yosemite:      "320a3590af1e9a1bee6827eb71e4d91fb283952f178b7f0393406a120046d4ee"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "708324bb6cf751c76f927c6a648416ee38012499dddfc80c4b2c50cf36431c4d"
+    sha256 cellar: :any,                 arm64_monterey:  "6f336006d80dbcfce530db381fb28d6207953c9ba792f71c31f041b983b85c53"
+    sha256 cellar: :any,                 arm64_big_sur:   "d3069c3fbd0f41bdb0b3435b7b388f9e6051639421658663185bde9a449185b8"
+    sha256 cellar: :any,                 monterey:        "0d6891a3cb5d5be6b4071bcf68a9e3449d9a79c37b4c660f1044bbc93ecafcfa"
+    sha256 cellar: :any,                 big_sur:         "e126c9d5de2e1f4e23d4fea7e8ac51c6fc2d4328a968c907879f4ea86524fbbc"
+    sha256 cellar: :any,                 catalina:        "fb552e12a3c59e2ca6a9dd89e9ec229e5b815edef28093c3902fc4ee54b52207"
+    sha256 cellar: :any,                 mojave:          "5c71a1746a8ca516dc5d11858a7d0d85341cafeea31797b926eba3a9ed83d9ea"
+    sha256 cellar: :any,                 high_sierra:     "85a6d2a8fcad4e2b6e7d9d22ec74dd5e5f463dabc5b2f01373d3a48178b2ce6e"
+    sha256 cellar: :any,                 sierra:          "716c78af6b276392a20fb02d58ff60e705509117da932d62d3ff8c6e4dd0bf5d"
+    sha256 cellar: :any,                 el_capitan:      "c647327d709c3b4a93d5541f8b340d2726540c9efdcbc53d1124043c8c4989bd"
+    sha256 cellar: :any,                 x86_64_yosemite: "320a3590af1e9a1bee6827eb71e4d91fb283952f178b7f0393406a120046d4ee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:    "708324bb6cf751c76f927c6a648416ee38012499dddfc80c4b2c50cf36431c4d"
   end
 
   depends_on "pkg-config" => :build
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
+    sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
+  end
 
   def install
     system "./configure", "--disable-dependency-tracking",
