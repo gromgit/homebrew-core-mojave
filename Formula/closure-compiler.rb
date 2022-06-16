38e91f2d70f348cf4b1fd33887b8e834d3432db6
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-16 15:11:35 +0800

    closure-compiler: no rebuilding required

--- Formula/closure-compiler.rb
+++ Formula/closure-compiler.rb
@@ -1,8 +1,8 @@
 class ClosureCompiler < Formula
   desc "JavaScript optimizing compiler"
   homepage "https://developers.google.com/closure/compiler"
-  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20220502/closure-compiler-v20220502.jar"
-  sha256 "85ad32fefa2a9d8a59ed598091f67faff230eca015fb610b39f582a458ce75de"
+  url "https://search.maven.org/remotecontent?filepath=com/google/javascript/closure-compiler/v20220601/closure-compiler-v20220601.jar"
+  sha256 "4b4eb54d072f2d13f5b70fa2d1719ca54d5ca40ea68484a70a300557f63e94b8"
   license "Apache-2.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class ClosureCompiler < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "97c8c6418c1dc4edc492d9ddfba33e351d649930832aac267296781f7791759b"
+    sha256 cellar: :any_skip_relocation, all: "2240def7f5db732558af42483c3bea90e5f2e3ea17d415685097b5e475b30c2d"
   end
 
   depends_on "openjdk"
