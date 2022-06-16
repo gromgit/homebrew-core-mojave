ab4a544fbb533235ae4bc3e7d1a0070dceacd53c
Adrian Ho <the.gromgit@gmail.com>
2022-06-16 22:05:33 +0800

    freetype: update 2.12.1 bottle.

--- Formula/freetype.rb
+++ Formula/freetype.rb
@@ -1,9 +1,9 @@
 class Freetype < Formula
   desc "Software library to render fonts"
   homepage "https://www.freetype.org/"
-  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.12.0/freetype-2.12.0.tar.xz"
-  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.12.0.tar.xz"
-  sha256 "ef5c336aacc1a079ff9262d6308d6c2a066dd4d2a905301c4adda9b354399033"
+  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.12.1/freetype-2.12.1.tar.xz"
+  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.12.1.tar.xz"
+  sha256 "4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f"
   license "FTL"
 
   livecheck do
@@ -13,7 +13,7 @@ class Freetype < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/freetype"
-    sha256 cellar: :any, mojave: "445e9395b9c74c80561b23239a540bd125d483efba5b7919b54f8f72b0d7fb54"
+    sha256 cellar: :any, mojave: "d3060920ad496376e4c1cc054b567c973cf2b2a726d6c72609ef4f2567f88fbd"
   end
 
   depends_on "libpng"
