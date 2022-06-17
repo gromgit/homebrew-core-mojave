d4591a6d166417dacef72f50efc95833d25239ad
Adrian Ho <the.gromgit@gmail.com>
2022-06-17 15:33:37 +0800

    libx11: update 1.8.1 bottle.

--- Formula/libx11.rb
+++ Formula/libx11.rb
@@ -1,13 +1,13 @@
 class Libx11 < Formula
   desc "X.Org: Core X11 protocol client library"
   homepage "https://www.x.org/"
-  url "https://www.x.org/archive/individual/lib/libX11-1.8.tar.gz"
-  sha256 "68e0a30c4248b9f41492891a4b49672c3b0c59e84c4868144f03eef01ebc5eea"
+  url "https://www.x.org/archive/individual/lib/libX11-1.8.1.tar.gz"
+  sha256 "d52f0a7c02a45449f37b0831d99ff936d92eb4ce8b4c97dc17a63cea79ce5a76"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libx11"
-    sha256 mojave: "c01f328b26f5e06cfe88bb4021c6cc8fe5e63747ad0a1da3d53903baa09f75f1"
+    sha256 mojave: "3b2b9882331683bd6cc039422f73d5407c579ed78a3bf17a976d68ecd0941e44"
   end
 
   depends_on "pkg-config" => :build
