469883db130709a94e8b647e645eb37ab009be62
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-17 12:51:15 +0800

    xcb-proto: no rebuilding required

--- Formula/xcb-proto.rb
+++ Formula/xcb-proto.rb
@@ -1,12 +1,12 @@
 class XcbProto < Formula
   desc "X.Org: XML-XCB protocol descriptions for libxcb code generation"
   homepage "https://www.x.org/"
-  url "https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-1.15.tar.xz"
-  sha256 "d34c3b264e8365d16fa9db49179cfa3e9952baaf9275badda0f413966b65955f"
+  url "https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-1.15.1.tar.xz"
+  sha256 "270eed15a98207fff89dc40a4a7ea31425fc7059d641227856bdd9191c2718ae"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "ba8f85669a932e77cb5d2d280331d768d50ed2de4b85894c7066c2ca805e326d"
+    sha256 cellar: :any_skip_relocation, all: "b099b1fb7669fac199978257bcdfe7a9b744809668843e91cea2ca82958c4dba"
   end
 
   depends_on "pkg-config" => [:build, :test]
