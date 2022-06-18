250762ad3dfa206ee432f58ba98132d8bc9c6401
Adrian Ho <the.gromgit@gmail.com>
2022-06-19 05:43:18 +0800

    cargo-bloat: update 0.11.1 bottle.

--- Formula/cargo-bloat.rb
+++ Formula/cargo-bloat.rb
@@ -1,14 +1,14 @@
 class CargoBloat < Formula
   desc "Find out what takes most of the space in your executable"
   homepage "https://github.com/RazrFalcon/cargo-bloat"
-  url "https://github.com/RazrFalcon/cargo-bloat/archive/v0.11.0.tar.gz"
-  sha256 "16be80bb0486cb0a2fd9164402b2d7449b07c18738a6259d42035c738d3b4a32"
+  url "https://github.com/RazrFalcon/cargo-bloat/archive/v0.11.1.tar.gz"
+  sha256 "4f338c1a7f7ee6bcac150f7856ed1f32cf8d9009cfd513ca6c1aac1e6685c35f"
   license "MIT"
   head "https://github.com/RazrFalcon/cargo-bloat.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cargo-bloat"
-    sha256 cellar: :any_skip_relocation, mojave: "9b6f29d8a63108147ccab70e6d891583afc5811a949f207bd2abf58968b862a5"
+    sha256 cellar: :any_skip_relocation, mojave: "a6c892259d0500b0bfed05707c537d7f7a34ead92d9dcb9acae44b7179477f75"
   end
 
   depends_on "rust"
