355832a156400280d954778d756daadf049baa17
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-18 21:40:24 +0800

    alsa-lib:

--- Formula/alsa-lib.rb
+++ Formula/alsa-lib.rb
@@ -1,8 +1,8 @@
 class AlsaLib < Formula
   desc "Provides audio and MIDI functionality to the Linux operating system"
   homepage "https://www.alsa-project.org/"
-  url "https://www.alsa-project.org/files/pub/lib/alsa-lib-1.2.7.tar.bz2"
-  sha256 "8814e61f7ec6812c76e23a85cab00e0b0d3bba40816af36b726beb1bc04c74a7"
+  url "https://www.alsa-project.org/files/pub/lib/alsa-lib-1.2.7.1.tar.bz2"
+  sha256 "046dc42dfcfad269217be05954686137e5e7397f3041372f8c6dcd7d79461e61"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -11,7 +11,7 @@ class AlsaLib < Formula
   end
 
   bottle do
-    sha256 x86_64_linux: "5513719e5e90d5a82e0fece73eecba14e3cf256625eb977f5116cdc6807646f6"
+    sha256 x86_64_linux: "4a7a74716d1cbc5df30cc78adf8250c786dbf70db54c171e00057463322b8f9b"
   end
 
   depends_on :linux
