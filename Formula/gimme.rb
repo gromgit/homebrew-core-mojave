d88d9f58b5e4b1675ba59fa5a04d5eda15f048f0
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-18 21:40:29 +0800

    gimme: no rebuilding required

--- Formula/gimme.rb
+++ Formula/gimme.rb
@@ -1,12 +1,12 @@
 class Gimme < Formula
   desc "Shell script to install any Go version"
   homepage "https://github.com/travis-ci/gimme"
-  url "https://github.com/travis-ci/gimme/archive/v1.5.4.tar.gz"
-  sha256 "71036f892b3cae08f29be6fd4c69fb20d9b003ec80a24221d73e995e12ab0fe0"
+  url "https://github.com/travis-ci/gimme/archive/v1.5.5.tar.gz"
+  sha256 "7854c3f90c2274e14041138f53c9c0bf671be86e49dfb4a61b024270a514fb40"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "955a907946bda06e3e0eeef3af936182915ddfae8040eb744a394a47a570e9d0"
+    sha256 cellar: :any_skip_relocation, all: "fd677d17e7c2316bce3c567ce9cc708b2c3e6bf57ed867f64554e202b7b78746"
   end
 
   def install
