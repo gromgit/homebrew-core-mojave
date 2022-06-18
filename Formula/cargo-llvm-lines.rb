6e7ff403377ef8fca3edd5fd883f974ecac07ebd
Adrian Ho <the.gromgit@gmail.com>
2022-06-19 05:55:49 +0800

    cargo-llvm-lines: update 0.4.16 bottle.

--- Formula/cargo-llvm-lines.rb
+++ Formula/cargo-llvm-lines.rb
@@ -1,14 +1,14 @@
 class CargoLlvmLines < Formula
   desc "Count lines of LLVM IR per generic function"
   homepage "https://github.com/dtolnay/cargo-llvm-lines"
-  url "https://github.com/dtolnay/cargo-llvm-lines/archive/0.4.15.tar.gz"
-  sha256 "32fdfdb4eea5f7d9118445a8beadfb1cea2e12c5949766453cee35584afae127"
+  url "https://github.com/dtolnay/cargo-llvm-lines/archive/0.4.16.tar.gz"
+  sha256 "284fbd1d16c52392e2eb3b0a29a05be244672b0f8eb3b70447ae7840604419a4"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/dtolnay/cargo-llvm-lines.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cargo-llvm-lines"
-    sha256 cellar: :any_skip_relocation, mojave: "64b8bd3c7c59c02a78b2abfad6a724115ecadde5720638403807bb9249d98c34"
+    sha256 cellar: :any_skip_relocation, mojave: "24358451500dd88e8ad64b62638abc6df2aca9b043430bdeb605dc2d4ed463fe"
   end
 
   depends_on "rust"
