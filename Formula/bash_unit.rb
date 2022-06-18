b82ced85717c7d6420063887581df53e2f6cde35
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-18 21:40:24 +0800

    bash_unit: no rebuilding required

--- Formula/bash_unit.rb
+++ Formula/bash_unit.rb
@@ -1,12 +1,12 @@
 class BashUnit < Formula
   desc "Bash unit testing enterprise edition framework for professionals"
   homepage "https://github.com/pgrange/bash_unit"
-  url "https://github.com/pgrange/bash_unit/archive/refs/tags/v1.9.1.tar.gz"
-  sha256 "d55a6b15e4e118157be64654616cac55bcbc24fabd486dcbb61012cc71cf2f8c"
+  url "https://github.com/pgrange/bash_unit/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "4a7c243ad0ba25c448f1a678921aa4c94b316c26cbf474e011b133c3386343b4"
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "172450ac23326f95aff9d3b7a9685743dbe45f112f3cd7b799f933d6b8f946ed"
+    sha256 cellar: :any_skip_relocation, all: "ec4841bdcc9027a5f01635b81ae15308012af99a4dc8313df95ff019fe266424"
   end
 
   uses_from_macos "bc" => :test
