97cda56807686c103942b2916d9fea98bdca715b
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-18 21:40:32 +0800

    pbc-sig: no rebuilding required

--- Formula/pbc-sig.rb
+++ Formula/pbc-sig.rb
@@ -12,19 +12,31 @@ class PbcSig < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "f99446bcb7e5930651fc63d4a6bea1b34b489e13ad7318a026d0be3ed6fe39f9"
-    sha256 cellar: :any, big_sur:       "9889f70fc5cf42a096c750b61008bf48a97bfece6179db5e7a631010749f1106"
-    sha256 cellar: :any, catalina:      "47773fefdfeb3f7381046934974bbaf7f41a641c3d3f3af5802d07a7ea340ba6"
-    sha256 cellar: :any, mojave:        "134c203178bb93b406b4c5fb5aecf171db6473d558d0bf62cf9b1682b57448e9"
-    sha256 cellar: :any, high_sierra:   "79c31a3f1bcc2429648a2258974ccb1185cfe244d4fcbbfa2840c7393e7e058a"
+    sha256 cellar: :any, arm64_monterey: "d2fde3522eb0285c965608483e1099f231df57528446ce3ebc59cee147459d58"
+    sha256 cellar: :any, arm64_big_sur:  "f99446bcb7e5930651fc63d4a6bea1b34b489e13ad7318a026d0be3ed6fe39f9"
+    sha256 cellar: :any, monterey:       "49ba0b0e8757276a5ab822f942f321e7fe5b7efbb2340946e21f3042dbe579bd"
+    sha256 cellar: :any, big_sur:        "9889f70fc5cf42a096c750b61008bf48a97bfece6179db5e7a631010749f1106"
+    sha256 cellar: :any, catalina:       "47773fefdfeb3f7381046934974bbaf7f41a641c3d3f3af5802d07a7ea340ba6"
+    sha256 cellar: :any, mojave:         "134c203178bb93b406b4c5fb5aecf171db6473d558d0bf62cf9b1682b57448e9"
+    sha256 cellar: :any, high_sierra:    "79c31a3f1bcc2429648a2258974ccb1185cfe244d4fcbbfa2840c7393e7e058a"
   end
 
   depends_on "pbc"
+
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-pre-0.4.2.418-big_sur.diff"
+    sha256 "83af02f2aa2b746bb7225872cab29a253264be49db0ecebb12f841562d9a2923"
+  end
 
   # https://groups.google.com/forum/#!topic/pbc-devel/ZmFCHZmrhcw
   patch :DATA
 
   def install
+    # Disable -fnested-functions CFLAG on ARM, which will cause it to fail with:
+    # incompatible redeclaration of library function 'pow'
+    # Reported upstream here: https://groups.google.com/g/pbc-devel/c/WXwVWKoouj0.
+    inreplace "configure", "-fnested-functions", "" if Hardware::CPU.arm?
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install"
