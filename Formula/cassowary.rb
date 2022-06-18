44c5ce3deea82394df4fd4f51c24f3d103673870
Adrian Ho <the.gromgit@gmail.com>
2022-06-19 06:01:24 +0800

    cassowary: update 0.14.1 bottle.

--- Formula/cassowary.rb
+++ Formula/cassowary.rb
@@ -1,19 +1,14 @@
 class Cassowary < Formula
   desc "Modern cross-platform HTTP load-testing tool written in Go"
   homepage "https://github.com/rogerwelin/cassowary"
-  url "https://github.com/rogerwelin/cassowary/archive/refs/tags/v0.14.0.tar.gz"
-  sha256 "385232478b8552d56429fbe2584950bfbe42e3b611919a31075366a143aae9a9"
+  url "https://github.com/rogerwelin/cassowary/archive/refs/tags/v0.14.1.tar.gz"
+  sha256 "1f10e23af218d661e8493e111d425da0ef6f4408d845a473fdbaf45dd6e2d94d"
   license "MIT"
   head "https://github.com/rogerwelin/cassowary.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d98b3b6c308986895c5fcb0966331d895035239aae504a7df75dcf7401048772"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "648d2b294a0a5523a9941c54d4399ba7af9574d00535c27560642e4df77cd883"
-    sha256 cellar: :any_skip_relocation, monterey:       "a01fed38f90e43630a2fea77a4bd55925a89b9c2e83be754db8e6265479184ea"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cf5c5498ca3f7eead7d7cd3526d968d20e546dc6c8b58d40f3a18cf8e5175406"
-    sha256 cellar: :any_skip_relocation, catalina:       "fee06e4f638390a52a6a0b3ce3f2fb833620ba383871fb03b90fe8b90366787e"
-    sha256 cellar: :any_skip_relocation, mojave:         "f83cb625a473f40918a7eff32b1f2615862faf40868cd7aec8283d6ae7abb9a9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cdd87fcbb01eb4a4aff7099a3d5b2aa88d80221657342ea3380213d2dbebb4c1"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cassowary"
+    sha256 cellar: :any_skip_relocation, mojave: "e8dcda4ce51786551dbd2158dae8b8b35b9e9ed725e23b7c39f58cf5b4eb1db3"
   end
 
   depends_on "go" => :build
