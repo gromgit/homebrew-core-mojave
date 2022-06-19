c575f0ccac7e487484a86406dbf5ccea3655de70
Adrian Ho <the.gromgit@gmail.com>
2022-06-19 10:04:12 +0800

    cmark-gfm: update 0.29.0.gfm.4 bottle.

--- Formula/cmark-gfm.rb
+++ Formula/cmark-gfm.rb
@@ -1,14 +1,14 @@
 class CmarkGfm < Formula
   desc "C implementation of GitHub Flavored Markdown"
   homepage "https://github.com/github/cmark-gfm"
-  url "https://github.com/github/cmark-gfm/archive/0.29.0.gfm.3.tar.gz"
-  version "0.29.0.gfm.3"
-  sha256 "56fba15e63676c756566743dcd43c61c6a77cc1d17ad8be88a56452276ba4d4c"
+  url "https://github.com/github/cmark-gfm/archive/0.29.0.gfm.4.tar.gz"
+  version "0.29.0.gfm.4"
+  sha256 "1be0d2c703b87cfbf51f91336db04039756e118c39398a392b9a3cca1b7d4ead"
   license "BSD-2-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cmark-gfm"
-    sha256 cellar: :any, mojave: "b26f110b2ab8210a102b432936b15adc9a29f41385e16569d865f113e8ed1a3d"
+    sha256 cellar: :any, mojave: "2756b80bf2083fba51ecdf6d23d71a2eab4b1c8fe698471951754e3598ad3c15"
   end
 
   depends_on "cmake" => :build
