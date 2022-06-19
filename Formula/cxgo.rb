a75400487d6928561f25ad6088b1088270ae8c4c
Adrian Ho <the.gromgit@gmail.com>
2022-06-19 13:26:36 +0800

    cxgo: update 0.3.4 bottle.

--- Formula/cxgo.rb
+++ Formula/cxgo.rb
@@ -2,14 +2,14 @@ class Cxgo < Formula
   desc "Transpiling C to Go"
   homepage "https://github.com/gotranspile/cxgo"
   url "https://github.com/gotranspile/cxgo.git",
-      tag:      "v0.3.3",
-      revision: "187be2cefefdd74efe861994ea51c0d0c0cd3f38"
+      tag:      "v0.3.4",
+      revision: "d57c260fb00fa8bdae1c146f79dc03c4a13ae7c8"
   license "MIT"
   head "https://github.com/gotranspile/cxgo.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cxgo"
-    sha256 cellar: :any_skip_relocation, mojave: "54fe0cf7f7b44f3963758a927bfb2ca7947896a66b69b85e231799a9ef2d1260"
+    sha256 cellar: :any_skip_relocation, mojave: "4f3cb2d1c490891f4c649b8a76b0420e08c5af9a13204725363bc2f55adb5a66"
   end
 
   depends_on "go" => :build
