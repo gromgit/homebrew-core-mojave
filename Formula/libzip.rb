d8ed47d55d4b6c36dcd2a7a3acf8f4a5d57443a7
Adrian Ho <the.gromgit@gmail.com>
2022-06-19 11:52:35 +0800

    libzip: update 1.9.0 bottle.

--- Formula/libzip.rb
+++ Formula/libzip.rb
@@ -1,10 +1,9 @@
 class Libzip < Formula
   desc "C library for reading, creating, and modifying zip archives"
   homepage "https://libzip.org/"
-  url "https://libzip.org/download/libzip-1.8.0.tar.xz", using: :homebrew_curl
-  sha256 "f0763bda24ba947e80430be787c4b068d8b6aa6027a26a19923f0acfa3dac97e"
+  url "https://libzip.org/download/libzip-1.9.0.tar.xz", using: :homebrew_curl
+  sha256 "a17240ee88f0705a9067bb0087fde1cee73948b3cf6c3978a21a58fdb73b76a2"
   license "BSD-3-Clause"
-  revision 1
 
   livecheck do
     url "https://libzip.org/download/"
@@ -12,13 +11,8 @@ class Libzip < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "e9e8c08f4a92b59d52825dd58414ef58b59561c02aa68a4081f59b8a426e78ab"
-    sha256 cellar: :any,                 arm64_big_sur:  "c5842b42b9cbec01d11657ce19789f78b3f07e0350add80e894e57682a7ea3ba"
-    sha256 cellar: :any,                 monterey:       "8ebe402be907386945c2479a5aab12d4ba08e39303837d7cf607ee226581ea91"
-    sha256 cellar: :any,                 big_sur:        "6f3266d5fd14899d3c67dcc693365fda4531f17dc414039c66a8ad4f0becc819"
-    sha256 cellar: :any,                 catalina:       "d708cee97ad3536abd1c4ab0fc4108477c04d944db47c9a427772bc23027d427"
-    sha256 cellar: :any,                 mojave:         "5e54afd324365a37e9d6f4adec4ac6ea727012eb7b23931c94e2cf59b95b1bc5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "095cf6867438b44257ea4c38e19ad1f26423ad64f9a76c4139983039ee4607ab"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libzip"
+    sha256 cellar: :any, mojave: "709f23662804f31f72e966b2a00dfe69a67a14deebf6eb2c3f8b5d7cd69b0817"
   end
 
   depends_on "cmake" => :build
