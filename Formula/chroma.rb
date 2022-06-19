23b43251589f5e6fd9781d0d14986268c9db22fb
Adrian Ho <the.gromgit@gmail.com>
2022-06-19 09:03:25 +0800

    chroma: update 2.2.0 bottle.

--- Formula/chroma.rb
+++ Formula/chroma.rb
@@ -1,13 +1,13 @@
 class Chroma < Formula
   desc "General purpose syntax highlighter in pure Go"
   homepage "https://github.com/alecthomas/chroma"
-  url "https://github.com/alecthomas/chroma/archive/refs/tags/v2.0.1.tar.gz"
-  sha256 "5d6b9986a175ed8ca789e55e42bb4d0f6089b408824b0654b57c7fcb91c06c07"
+  url "https://github.com/alecthomas/chroma/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "6870cb8c47897f2a75abaf3e6cbb160989b4e915b86fdcd9322fb17e073ccd81"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/chroma"
-    sha256 cellar: :any_skip_relocation, mojave: "852eb86da76a5008051699b0ae8f375c52ef983c51dbce58d7ae3bf651a43ac9"
+    sha256 cellar: :any_skip_relocation, mojave: "3a1470bb4d59436fa8e8205c3f7630528168b6fc7f71ae485c5f7c5daf3ca138"
   end
 
   depends_on "go" => :build
