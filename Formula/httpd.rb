706a5e2b6266a568d013c987ec02c4b0ad73436d
Adrian Ho <the.gromgit@gmail.com>
2022-06-19 11:39:14 +0800

    httpd: update 2.4.54 bottle.

--- Formula/httpd.rb
+++ Formula/httpd.rb
@@ -1,15 +1,14 @@
 class Httpd < Formula
   desc "Apache HTTP server"
   homepage "https://httpd.apache.org/"
-  url "https://dlcdn.apache.org/httpd/httpd-2.4.53.tar.bz2"
-  mirror "https://downloads.apache.org/httpd/httpd-2.4.53.tar.bz2"
-  sha256 "d0bbd1121a57b5f2a6ff92d7b96f8050c5a45d3f14db118f64979d525858db63"
+  url "https://dlcdn.apache.org/httpd/httpd-2.4.54.tar.bz2"
+  mirror "https://downloads.apache.org/httpd/httpd-2.4.54.tar.bz2"
+  sha256 "eb397feeefccaf254f8d45de3768d9d68e8e73851c49afd5b7176d1ecf80c340"
   license "Apache-2.0"
-  revision 2
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/httpd"
-    sha256 mojave: "29bed753fc086769d0fb624ea8607ca27e29a16c755b5b15987b070d44889b88"
+    sha256 mojave: "f3ae23f63a24b4142ec646e8ac3ae7a6d207b376d8043c8a16010f7fb24d3ff5"
   end
 
   depends_on "apr"
