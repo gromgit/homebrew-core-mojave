87d0295432d3e41eacad04520c1a69dba1ba9fc8
Adrian Ho <the.gromgit@gmail.com>
2022-06-20 20:08:54 +0800

    driftctl: update 0.34.0 bottle.

--- Formula/driftctl.rb
+++ Formula/driftctl.rb
@@ -1,13 +1,13 @@
 class Driftctl < Formula
   desc "Detect, track and alert on infrastructure drift"
   homepage "https://driftctl.com"
-  url "https://github.com/snyk/driftctl/archive/v0.31.1.tar.gz"
-  sha256 "7c555e182b020690aaa7a26e5a400b05c01503f417462dff28423173a887a9ac"
+  url "https://github.com/snyk/driftctl/archive/v0.34.0.tar.gz"
+  sha256 "b8b75d0756f7e703a6bc3ad2c44de75f03402854eff28f0752a9eca80691c186"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/driftctl"
-    sha256 cellar: :any_skip_relocation, mojave: "93203ceb509ea125661246661550e097b04671baf5c4d4ddaf1ee3e8384f6e26"
+    sha256 cellar: :any_skip_relocation, mojave: "393e0801fe0a9366a7aed72d585164480938355b771e9b1b309c14289a865259"
   end
 
   depends_on "go" => :build
@@ -35,7 +35,7 @@ class Driftctl < Formula
 
   test do
     assert_match "v#{version}", shell_output("#{bin}/driftctl version")
-    assert_match "Downloading terraform provider: aws",
+    assert_match "Could not find a way to authenticate on AWS!",
       shell_output("#{bin}/driftctl --no-version-check scan 2>&1", 2)
   end
 end
