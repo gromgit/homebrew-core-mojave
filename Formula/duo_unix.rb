5551daba9e72d9f0332fe7fc50e9da5734523b0e
Adrian Ho <the.gromgit@gmail.com>
2022-06-20 20:32:43 +0800

    duo_unix: update 1.12.1 bottle.

--- Formula/duo_unix.rb
+++ Formula/duo_unix.rb
@@ -1,14 +1,13 @@
 class DuoUnix < Formula
   desc "Two-factor authentication for SSH"
   homepage "https://www.duosecurity.com/docs/duounix"
-  url "https://github.com/duosecurity/duo_unix/archive/duo_unix-1.12.0.tar.gz"
-  sha256 "a4479f893e036f38a5809d71ce47f69118f6ef61822cc1c66afccf143c5d71f8"
+  url "https://github.com/duosecurity/duo_unix/archive/duo_unix-1.12.1.tar.gz"
+  sha256 "bcba8eac949c983955eadcd63199a327add3b8f00aa6e7eb87cd7f4e28b2115b"
   license "GPL-2.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/duo_unix"
-    rebuild 2
-    sha256 mojave: "f31ca7e010df81bca02aa01cea701f9109c225742aeea26a41f9f62c17022434"
+    sha256 mojave: "4f983b852a0539c0733c53fe454c841f7ed803674d0727b48a07ea86a0238fb8"
   end
 
   depends_on "autoconf" => :build
