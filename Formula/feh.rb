bbffc760aa5a818b63d04d0dfdb06b0a8d425311
Adrian Ho <the.gromgit@gmail.com>
2022-06-21 03:09:21 +0800

    feh: update 3.9 bottle.

--- Formula/feh.rb
+++ Formula/feh.rb
@@ -1,8 +1,8 @@
 class Feh < Formula
   desc "X11 image viewer"
   homepage "https://feh.finalrewind.org/"
-  url "https://feh.finalrewind.org/feh-3.8.tar.bz2"
-  sha256 "7f3c34552b39336d7ebee2d7c4bf5697aaaa2c6c102c357f6e82ea240bd62ba9"
+  url "https://feh.finalrewind.org/feh-3.9.tar.bz2"
+  sha256 "8649962c41d2c7ec4cc3f438eb327638a1820ad5a66df6a9995964601ae6bca0"
   license "MIT-feh"
 
   livecheck do
@@ -12,7 +12,7 @@ class Feh < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/feh"
-    sha256 mojave: "5e764304e1af6a6ae78cc830da01389e2f7ab9afb5e3a31ff67fae3c3bb6deb7"
+    sha256 mojave: "ac4ae899c3eceec7a01ab8352520cb0dedcddc921c6b061cbb5348e2246b2774"
   end
 
   depends_on "imlib2"
