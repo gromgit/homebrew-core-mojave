136711e515bcabcf54ec46587d948d856ff05e0a
Adrian Ho <the.gromgit@gmail.com>
2022-06-21 14:40:36 +0800

    jackett: update 0.20.1197 bottle.

---
+++ Formula/jackett.rb
@@ -0,0 +1,65 @@
+class Jackett < Formula
+  desc "API Support for your favorite torrent trackers"
+  homepage "https://github.com/Jackett/Jackett"
+  url "https://github.com/Jackett/Jackett/archive/refs/tags/v0.20.1197.tar.gz"
+  sha256 "550d07154ebf97cdb69b6f6063585b4a5510237dbc15a2f0901c8e8f43f520a4"
+  license "GPL-2.0-only"
+  head "https://github.com/Jackett/Jackett.git", branch: "master"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jackett"
+    sha256 cellar: :any, mojave: "fc8647f486c016d6c22effa11fb9adf9c88b354622b7d920f159f16dda1911d2"
+  end
+
+  depends_on "dotnet"
+
+  def install
+    cd "src" do
+      os = OS.mac? ? "osx" : OS.kernel_name.downcase
+      arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+
+      args = %W[
+        --configuration Release
+        --framework net#{Formula["dotnet"].version.major_minor}
+        --output #{libexec}
+        --runtime #{os}-#{arch}
+        --no-self-contained
+        /p:AssemblyVersion=#{version}
+        /p:FileVersion=#{version}
+        /p:InformationalVersion=#{version}
+        /p:Version=#{version}
+      ]
+
+      system "dotnet", "publish", "Jackett.Server", *args
+    end
+
+    (bin/"jackett").write_env_script libexec/"jackett", "--NoUpdates",
+      DOTNET_ROOT: "${DOTNET_ROOT:-#{Formula["dotnet"].opt_libexec}}"
+  end
+
+  service do
+    run opt_bin/"jackett"
+    keep_alive true
+    working_dir libexec
+    log_path var/"log/jackett.log"
+    error_log_path var/"log/jackett.log"
+  end
+
+  test do
+    assert_match(/^Jackett v#{Regexp.escape(version)}$/, shell_output("#{bin}/jackett --version 2>&1; true"))
+
+    port = free_port
+
+    pid = fork do
+      exec "#{bin}/jackett", "-d", testpath, "-p", port.to_s
+    end
+    sleep 10
+
+    begin
+      assert_match "<title>Jackett</title>", shell_output("curl -b cookiefile -c cookiefile -L --silent http://localhost:#{port}")
+    ensure
+      Process.kill "TERM", pid
+      Process.wait pid
+    end
+  end
+end
