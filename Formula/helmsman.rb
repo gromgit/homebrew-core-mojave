30f0a246a142738f5c5e86935344cf54f74116b4
Adrian Ho <the.gromgit@gmail.com>
2022-06-21 12:03:16 +0800

    helmsman: update 3.11.0 bottle.

--- Formula/helmsman.rb
+++ Formula/helmsman.rb
@@ -2,14 +2,14 @@ class Helmsman < Formula
   desc "Helm Charts as Code tool"
   homepage "https://github.com/Praqma/helmsman"
   url "https://github.com/Praqma/helmsman.git",
-      tag:      "v3.9.1",
-      revision: "6c4a6b08354111e21fa411f98b063fbd896c3ffa"
+      tag:      "v3.11.0",
+      revision: "9887c3853aee56b6803f589e4b426737241ac1e0"
   license "MIT"
   head "https://github.com/Praqma/helmsman.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/helmsman"
-    sha256 cellar: :any_skip_relocation, mojave: "5e1eabbe93fc25a2995e7ee3089bc1ae7a385e402ab1b995f6da7ac76ef48972"
+    sha256 cellar: :any_skip_relocation, mojave: "823e788e14254471a7be1b075f689d7eb3a308b630d034eb33cc891c5e048394"
   end
 
   depends_on "go" => :build
@@ -19,12 +19,16 @@ class Helmsman < Formula
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/helmsman"
     pkgshare.install "examples/example.yaml"
+    pkgshare.install "examples/job.yaml"
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/helmsman version")
+    ENV["ORG_PATH"] = "brewtest"
+    ENV["VALUE"] = "brewtest"
 
-    output = shell_output("#{bin}/helmsman --apply -f #{pkgshare}/example.yaml 2>&1")
+    output = shell_output("#{bin}/helmsman --apply -f #{pkgshare}/example.yaml 2>&1", 1)
     assert_match "helm diff not found", output
+
+    assert_match version.to_s, shell_output("#{bin}/helmsman version")
   end
 end
