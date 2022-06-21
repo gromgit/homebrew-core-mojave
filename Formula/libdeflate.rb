c10a856910de10eb23ee35cb021dc632478d3d80
Adrian Ho <the.gromgit@gmail.com>
2022-06-21 17:53:07 +0800

    libdeflate: update 1.12 bottle.

--- Formula/libdeflate.rb
+++ Formula/libdeflate.rb
@@ -1,13 +1,13 @@
 class Libdeflate < Formula
   desc "Heavily optimized DEFLATE/zlib/gzip compression and decompression"
   homepage "https://github.com/ebiggers/libdeflate"
-  url "https://github.com/ebiggers/libdeflate/archive/v1.10.tar.gz"
-  sha256 "5c1f75c285cd87202226f4de49985dcb75732f527eefba2b3ddd70a8865f2533"
+  url "https://github.com/ebiggers/libdeflate/archive/v1.12.tar.gz"
+  sha256 "ba89fb167a5ab6bbdfa6ee3b1a71636e8140fa8471cce8a311697584948e4d06"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libdeflate"
-    sha256 cellar: :any, mojave: "d59fb59b5a4be65d844b4e22ee246afa3d1c5b7106072378485685b193348f29"
+    sha256 cellar: :any, mojave: "d55316765d7d98ce54f1bcf589d555ac7e5c90f3085af1eed4e3812c7323c026"
   end
 
   def install
