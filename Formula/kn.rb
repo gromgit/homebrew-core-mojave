d0b4109b81cec8f634313cd0d42926922f5293d2
Adrian Ho <the.gromgit@gmail.com>
2022-06-21 16:25:30 +0800

    kn: update 1.5.0 bottle.

--- Formula/kn.rb
+++ Formula/kn.rb
@@ -2,14 +2,14 @@ class Kn < Formula
   desc "Command-line interface for managing Knative Serving and Eventing resources"
   homepage "https://github.com/knative/client"
   url "https://github.com/knative/client.git",
-      tag:      "knative-v1.4.1",
-      revision: "c53658bd0ee61dc0c87e2cda589f1bc79ec84983"
+      tag:      "knative-v1.5.0",
+      revision: "0646532018621d3d08525edf6e47a125cf58438b"
   license "Apache-2.0"
   head "https://github.com/knative/client.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kn"
-    sha256 cellar: :any_skip_relocation, mojave: "2dd02665e9d6644ebd7a19ef6548a9aa300c779d37f8c8b1a4d3797c70f7cced"
+    sha256 cellar: :any_skip_relocation, mojave: "2b7ae0c8462ab55ffa55df07b10916ae883e2c695f47e92d8b884dea8cc78104"
   end
 
   depends_on "go" => :build
