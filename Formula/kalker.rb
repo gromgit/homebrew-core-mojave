85bccb5bfdbcf60ced2d102dd9ecbfaeab0461ec
Adrian Ho <the.gromgit@gmail.com>
2022-06-21 15:50:14 +0800

    kalker: update 2.0.0 bottle.

--- Formula/kalker.rb
+++ Formula/kalker.rb
@@ -1,13 +1,13 @@
 class Kalker < Formula
   desc "Full-featured calculator with math syntax"
   homepage "https://kalker.strct.net"
-  url "https://github.com/PaddiM8/kalker/archive/v1.1.0.tar.gz"
-  sha256 "4332563b18e1c2c8c54871e46f40e55791d3f86612d4651c69b690827051a484"
+  url "https://github.com/PaddiM8/kalker/archive/v2.0.0.tar.gz"
+  sha256 "092ab13726515125ec3664c15e61fa7b8eec09ad590f0c5ef00df6e33b3b3da7"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kalker"
-    sha256 cellar: :any_skip_relocation, mojave: "1359f25c450e8ebb4f911e9c73a7993ed7caebd70661cd7aa9412ed07e40a621"
+    sha256 cellar: :any_skip_relocation, mojave: "f5c1adcf8e77df911504959075d5e8b0f4d59e4af6effff725a26de9e9fbe637"
   end
 
   depends_on "rust" => :build
@@ -21,6 +21,6 @@ class Kalker < Formula
   end
 
   test do
-    assert_equal shell_output("#{bin}/kalker 'sum(1, 3, 2n+1)'").chomp, "15"
+    assert_equal shell_output("#{bin}/kalker 'sum(n=1, 3, 2n+1)'").chomp, "15"
   end
 end
