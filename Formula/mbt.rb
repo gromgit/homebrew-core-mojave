4ba73afe7d906109dddac761ae77fe0ca7739f67
Adrian Ho <the.gromgit@gmail.com>
2022-06-22 04:43:52 +0800

    mbt: update 1.2.18 bottle.

--- Formula/mbt.rb
+++ Formula/mbt.rb
@@ -1,14 +1,14 @@
 class Mbt < Formula
   desc "Multi-Target Application (MTA) build tool for Cloud Applications"
   homepage "https://sap.github.io/cloud-mta-build-tool"
-  url "https://github.com/SAP/cloud-mta-build-tool/archive/v1.2.16.tar.gz"
-  sha256 "6fbd2a77f1da05129fce5776202543e9a033729880d8b42f59a9694d3d773d8a"
+  url "https://github.com/SAP/cloud-mta-build-tool/archive/v1.2.18.tar.gz"
+  sha256 "3590b0269b8316c498d122a03bf01ff9b18fee82fca524669061182b414d4f8c"
   license "Apache-2.0"
   head "https://github.com/SAP/cloud-mta-build-tool.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mbt"
-    sha256 cellar: :any_skip_relocation, mojave: "1c8639d4e77cc1093b438e791d609915244405039aa7768f440d901301036723"
+    sha256 cellar: :any_skip_relocation, mojave: "449d55ee5dcf75824f77e991c236d2e994afa11d4be1f4ef274e9858f5a0d784"
   end
 
   depends_on "go" => :build
