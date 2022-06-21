f7b39a440521f9568cd0f1466cb0f701697cf63a
Adrian Ho <the.gromgit@gmail.com>
2022-06-21 12:38:15 +0800

    homeworlds: update 20141022_4 bottle.

--- Formula/homeworlds.rb
+++ Formula/homeworlds.rb
@@ -5,7 +5,7 @@ class Homeworlds < Formula
       revision: "917cd7e7e6d0a5cdfcc56cd69b41e3e80b671cde"
   version "20141022"
   license "BSD-2-Clause"
-  revision 3
+  revision 4
 
   livecheck do
     skip "No version information available to check"
@@ -13,7 +13,7 @@ class Homeworlds < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/homeworlds"
-    sha256 cellar: :any, mojave: "17e513bb7662795e5d33d09110b9907bb4aad0c93031e7aed67f9628dd554661"
+    sha256 cellar: :any, mojave: "d6d41f659011dc348194da421c408359f77fb1f379cde0b1f0d69c35e14d6a42"
   end
 
   depends_on "wxwidgets"
