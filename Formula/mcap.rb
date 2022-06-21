2b9ac92f99ac0b97220b3851b1849bd834cc65aa
Adrian Ho <the.gromgit@gmail.com>
2022-06-22 04:47:27 +0800

    mcap: update 0.0.10 bottle.

---
+++ Formula/mcap.rb
@@ -0,0 +1,52 @@
+class Mcap < Formula
+  desc "Serialization-agnostic container file format for pub/sub messages"
+  homepage "https://mcap.dev"
+  url "https://github.com/foxglove/mcap/archive/refs/tags/releases/mcap-cli/v0.0.10.tar.gz"
+  sha256 "767a9cc2ced1c479604156b7b67166c95d6a52c7c4f382e40e43689f9919a143"
+  license "Apache-2.0"
+  head "https://github.com/foxglove/mcap.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mcap"
+    sha256 cellar: :any_skip_relocation, mojave: "847fd06d2e3d406f6f3fbd380e036544b2daaa63a80e86d8b1c8222d165641d0"
+  end
+
+  depends_on "go" => :build
+
+  resource "homebrew-testdata" do
+    url "https://media.githubusercontent.com/media/foxglove/mcap/releases/mcap-cli/v0.0.10/tests/conformance/data/OneMessage/OneMessage-ch-chx-mx-pad-rch-rsh-st-sum.mcap"
+    sha256 "9db644f7fad2a256b891946a011fb23127b95d67dc03551b78224aa6cad8c5db"
+  end
+
+  def install
+    cd "go/cli/mcap" do
+      system "make", "build", "VERSION=v#{version}"
+      bin.install "bin/mcap"
+    end
+  end
+
+  test do
+    assert_equal "v#{version}", pipe_output("#{bin}/mcap version").strip
+
+    resource("homebrew-testdata").stage do
+      assert_equal "2 example [Example] [1 2 3]",
+      pipe_output("#{bin}/mcap cat", File.read("OneMessage-ch-chx-mx-pad-rch-rsh-st-sum.mcap")).strip
+
+      expected_info = <<~EOF
+        library:
+        profile:
+        messages: 1
+        duration: 0s
+        start: 0.000
+        end: 0.000
+        compression:
+        	: [1/1 chunks] (0.00%)
+        channels:
+          	(1) example  1 msgs (+Inf Hz)   : Example [c]
+        attachments: 0
+      EOF
+      assert_equal expected_info.lines.map(&:strip),
+        shell_output("#{bin}/mcap info OneMessage-ch-chx-mx-pad-rch-rsh-st-sum.mcap").lines.map(&:strip)
+    end
+  end
+end
