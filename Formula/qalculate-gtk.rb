1a3de569843ac6b658e8889e704b02be8661e423
Adrian Ho <the.gromgit@gmail.com>
2022-06-22 17:27:31 +0800

    qalculate-gtk: update 4.2.0 bottle.

--- Formula/qalculate-gtk.rb
+++ Formula/qalculate-gtk.rb
@@ -1,13 +1,13 @@
 class QalculateGtk < Formula
   desc "Multi-purpose desktop calculator"
   homepage "https://qalculate.github.io/"
-  url "https://github.com/Qalculate/qalculate-gtk/releases/download/v4.1.1/qalculate-gtk-4.1.1.tar.gz"
-  sha256 "8bf7dee899ba480e4f82c70cb374ed1229da28f7d3b9b475a089630a8eeb32e5"
+  url "https://github.com/Qalculate/qalculate-gtk/releases/download/v4.2.0/qalculate-gtk-4.2.0.tar.gz"
+  sha256 "50624344d12240f6eac68555c9a03747d0c2d90dd0de1bfe1b024fd5be8149d7"
   license "GPL-2.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/qalculate-gtk"
-    sha256 mojave: "2f059bc6b7f3be381951f640c42a8f3b54267788404b24513ad4ea8ce1a657bf"
+    sha256 mojave: "cdb5408202ed629a087716b42d2e54a00d7e73d1a83008bbd09a139a078b60fc"
   end
 
   depends_on "intltool" => :build
