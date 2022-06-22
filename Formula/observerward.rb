80ab56400e7a7028d267c7acb1b9e109e3a69227
Adrian Ho <the.gromgit@gmail.com>
2022-06-22 09:54:21 +0800

    observerward: update 2022.6.14 bottle.

--- Formula/observerward.rb
+++ Formula/observerward.rb
@@ -1,13 +1,13 @@
 class Observerward < Formula
   desc "Cross platform community web fingerprint identification tool"
   homepage "https://0x727.github.io/ObserverWard/"
-  url "https://github.com/0x727/ObserverWard/archive/refs/tags/v2022.6.1.tar.gz"
-  sha256 "61f615c77799c6681d0f37ea963d833ff586c81978f4f62d723967216942f91f"
+  url "https://github.com/0x727/ObserverWard/archive/refs/tags/v2022.6.14.tar.gz"
+  sha256 "d67af3efacc181c20b3b0a4f16616e74d19565498e56ab78ca5867fa71564b59"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/observerward"
-    sha256 cellar: :any_skip_relocation, mojave: "072974cf0d65e4e123e324ad89cdbdf9fa9f41d0102b6642dc0212254677be84"
+    sha256 cellar: :any_skip_relocation, mojave: "a06ddf01890887e410bac9e4d79225485ddb28081fbeba4e04d9ef7ebcef7d3c"
   end
 
   depends_on "rust" => :build
