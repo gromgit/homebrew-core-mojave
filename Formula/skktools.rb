89840753e339a82dee6cfaa2c597a675aa25be1a
Adrian Ho <the.gromgit@gmail.com>
2022-06-23 00:12:26 +0800

    skktools: update 1.3.4_2 bottle.

--- Formula/skktools.rb
+++ Formula/skktools.rb
@@ -4,31 +4,28 @@ class Skktools < Formula
   url "https://deb.debian.org/debian/pool/main/s/skktools/skktools_1.3.4.orig.tar.gz"
   sha256 "84cc5d3344362372e0dfe93a84790a193d93730178401a96248961ef161f2168"
   license "GPL-2.0"
-  revision 1
+  revision 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "2516509661552b0b851aadc8da1e3ecf821a5527dd6ab8d0d268ad5dfdc14b33"
-    sha256 cellar: :any,                 arm64_big_sur:  "4a56dad3435d1507d1fa2a087f4294d9955eb30993a70955c75e972a5c4ddb6b"
-    sha256 cellar: :any,                 monterey:       "4054365c06a67f80e59461c48492c922ac1810580fcb366ed87b603b07b2b4d2"
-    sha256 cellar: :any,                 big_sur:        "3a36d3ceab583dd2518b5dde10b8d443fbaf30e1de0d867f095effa5982fa1f3"
-    sha256 cellar: :any,                 catalina:       "c10f7baf6bc379ed47fc82b5bf1dd2ae8cbc53c358f5c838e87d8440ae8c5ebd"
-    sha256 cellar: :any,                 mojave:         "e3e3033a8f0d05939c9ebe2ab73cff7377351a36b8276337c920026c27d0a127"
-    sha256 cellar: :any,                 high_sierra:    "65826868e8d1ca3d310ebc14e2aadc281b09751220e3310643b4e2aa4ab9ee68"
-    sha256 cellar: :any,                 sierra:         "b190a2b730f1604ac76725e99c5fd9927ed5a7cbba905291f017b77fee1e3c85"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d8f270d332f062881a6842998b32a6c7fa462f794144421cc9a7d661da6c62c1"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/skktools"
+    sha256 cellar: :any, mojave: "393d39193e8135dbbd31efa1793549bab4676dd8c340edbc306735777b6a5cee"
   end
 
   depends_on "pkg-config" => :build
   depends_on "glib"
 
   on_linux do
-    depends_on "berkeley-db"
+    depends_on "gdbm"
   end
 
   def install
-    system "./configure", "--prefix=#{prefix}",
-                          "--with-skkdic-expr2"
-
+    args = ["--with-skkdic-expr2"]
+    if OS.linux?
+      args << "--with-gdbm"
+      # Help find Homebrew's gdbm compatibility layer header
+      inreplace %w[configure skkdic-expr.c], "gdbm/ndbm.h", "gdbm-ndbm.h"
+    end
+    system "./configure", *std_configure_args, *args
     system "make", "CC=#{ENV.cc}"
     ENV.deparallelize
     system "make", "install"
