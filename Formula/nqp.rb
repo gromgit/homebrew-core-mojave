4a7ec1c8bb1132c64290c6bae461f9a02a7dd0a4
Adrian Ho <the.gromgit@gmail.com>
2022-06-22 09:19:56 +0800

    nqp: update 2022.04 bottle.

--- Formula/nqp.rb
+++ Formula/nqp.rb
@@ -1,21 +1,26 @@
 class Nqp < Formula
   desc "Lightweight Perl 6-like environment for virtual machines"
   homepage "https://github.com/Raku/nqp"
-  url "https://github.com/Raku/nqp/releases/download/2022.02/nqp-2022.02.tar.gz"
-  sha256 "25d3c99745cd84f4049a9bd9cf26bb5dc817925abaafe71c9bdb68841cdb18b1"
+  url "https://github.com/Raku/nqp/releases/download/2022.04/nqp-2022.04.tar.gz"
+  sha256 "556d458e25d3c0464af9f04ea3e92bbde10046066b329188a88663943bd4e79c"
   license "Artistic-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nqp"
-    sha256 mojave: "e701840877da41496cdb4094da3a0879ca8119b779c6bbf3ffe53536d8198329"
+    sha256 mojave: "d89f78ed03773862ef399ed48dca96eddfd890d2f5a26368624d8178ce310a70"
   end
 
   depends_on "libtommath"
   depends_on "moarvm"
 
+  uses_from_macos "perl" => :build
+
   conflicts_with "rakudo-star", because: "rakudo-star currently ships with nqp included"
 
   def install
+    # Work around Homebrew's directory structure and help find moarvm libraries
+    inreplace "tools/build/gen-version.pl", "$libdir, 'MAST'", "'#{Formula["moarvm"].opt_share}/nqp/lib/MAST'"
+
     system "perl", "Configure.pl",
                    "--backends=moar",
                    "--prefix=#{prefix}",
