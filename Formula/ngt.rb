ea9446f4f670f863846a7176437bbbbf65204d03
Adrian Ho <the.gromgit@gmail.com>
2022-06-22 09:01:06 +0800

    ngt: update 1.14.6 bottle.

--- Formula/ngt.rb
+++ Formula/ngt.rb
@@ -1,8 +1,8 @@
 class Ngt < Formula
   desc "Neighborhood graph and tree for indexing high-dimensional data"
   homepage "https://github.com/yahoojapan/NGT"
-  url "https://github.com/yahoojapan/NGT/archive/v1.14.3.tar.gz"
-  sha256 "e3a2c6085001bf9183d214f93aa1ed89ac10ce93c2327799ca8e5d8c10a8edf1"
+  url "https://github.com/yahoojapan/NGT/archive/v1.14.6.tar.gz"
+  sha256 "5d7b092855ecea76f6dcf439beeba774a36893aaf7a61732de07068449f6f86f"
   license "Apache-2.0"
 
   livecheck do
@@ -12,18 +12,19 @@ class Ngt < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ngt"
-    sha256 cellar: :any, mojave: "a27e590527b0ccc57aa2d77b0cdd374120a0538d3ecdb5c174a4f66851c410ac"
+    sha256 cellar: :any, mojave: "9ca92b5bbfc2bdedfc2b9950970db07914fb2830feb842d9c5f2ca1b50ccd65f"
   end
 
   depends_on "cmake" => :build
-  depends_on "libomp"
+
+  on_macos do
+    depends_on "libomp"
+  end
 
   def install
-    mkdir "build" do
-      system "cmake", "..", *std_cmake_args, "-DCMAKE_INSTALL_RPATH=#{rpath}"
-      system "make"
-      system "make", "install"
-    end
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DCMAKE_INSTALL_RPATH=#{rpath}"
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
     pkgshare.install "data"
   end
 
