516dafb858d96295e336b85294d7ef329ec900e5
Adrian Ho <the.gromgit@gmail.com>
2022-06-22 09:39:58 +0800

    nuclei: update 2.7.2 bottle.

--- Formula/nuclei.rb
+++ Formula/nuclei.rb
@@ -1,14 +1,14 @@
 class Nuclei < Formula
   desc "HTTP/DNS scanner configurable via YAML templates"
   homepage "https://nuclei.projectdiscovery.io/"
-  url "https://github.com/projectdiscovery/nuclei/archive/v2.7.1.tar.gz"
-  sha256 "a6283e34067a6c40ff9e239ef69c53f246f7877559f99490d65ac566f56fba84"
+  url "https://github.com/projectdiscovery/nuclei/archive/v2.7.2.tar.gz"
+  sha256 "cf4ea1639005014269e2ba95e06844c2da73bbe162c8c0a5d2fd2cc55fd25d20"
   license "MIT"
   head "https://github.com/projectdiscovery/nuclei.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nuclei"
-    sha256 cellar: :any_skip_relocation, mojave: "3368a95dafd5d0799342810e4f8fc9455bc7ceade2cf1dfd45be9a113f1cdcdd"
+    sha256 cellar: :any_skip_relocation, mojave: "077b704c5a03097ddd18fdc5ee6f775a949d9e047ee0fda95e7e5206d9de1915"
   end
 
   depends_on "go" => :build
