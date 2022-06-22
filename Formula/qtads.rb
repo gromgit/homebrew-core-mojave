41f96f969221947b8bc0f7f5cca5b647c004d3d7
Adrian Ho <the.gromgit@gmail.com>
2022-06-22 17:50:06 +0800

    qtads: update 3.3.0 bottle.

--- Formula/qtads.rb
+++ Formula/qtads.rb
@@ -1,8 +1,8 @@
 class Qtads < Formula
   desc "TADS multimedia interpreter"
   homepage "https://realnc.github.io/qtads/"
-  url "https://github.com/realnc/qtads/releases/download/v3.2.0/qtads-3.2.0-source.tar.xz"
-  sha256 "382dbeb9af6ea5048ac19e7189bb862f81bc0f2e2e7ccad42d03985db12e5cc4"
+  url "https://github.com/realnc/qtads/releases/download/v3.3.0/qtads-3.3.0-source.tar.xz"
+  sha256 "02d62f004adbcf1faaa24928b3575a771d289df0fea9a97705d3bc528d9164a1"
   license "GPL-3.0-or-later"
   head "https://github.com/realnc/qtads.git", branch: "master"
 
@@ -12,13 +12,8 @@ class Qtads < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "ee81b4a35b37b3917513e52e3330011529dc4911a1cbfe56117d7286e3b56bdc"
-    sha256 cellar: :any,                 arm64_big_sur:  "efb0917e30907f43e0d75d4593842bc30f89694913d0fa4d6a78b312862cc9cd"
-    sha256 cellar: :any,                 monterey:       "ce3ce044126f12c53557af8db60491b3196b4c5ceada3cc2f8ddd574e6b84c83"
-    sha256 cellar: :any,                 big_sur:        "d3f92978a0454521d20577fda97c3cef034e60be673f06bcef5db44376e7ed3f"
-    sha256 cellar: :any,                 catalina:       "16bb2be33f9757e197011ec42652b90e8220203cb6b59a261dfd2658707adc9e"
-    sha256 cellar: :any,                 mojave:         "01215c3c22a58a47a1b4ff91e7a1558a301d0620c50b553f77f0fb30d75d8ae9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cefcbba0619133aba0182d27468c04b1daac381550d29e29bf1dfbc80a621569"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/qtads"
+    sha256 cellar: :any, mojave: "770092fbd56131d57ba2d7abf25cd1a2f7baaf3cd10dd69d3838db5e414d0cdb"
   end
 
   depends_on "pkg-config" => :build
