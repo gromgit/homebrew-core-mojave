007a7ec89560ec87794d8c92c5ac8f730e898fe5
Adrian Ho <the.gromgit@gmail.com>
2022-06-23 04:44:08 +0800

    tbox: update 1.6.9 bottle.

--- Formula/tbox.rb
+++ Formula/tbox.rb
@@ -1,14 +1,14 @@
 class Tbox < Formula
   desc "Glib-like multi-platform C library"
   homepage "https://tboox.org/"
-  url "https://github.com/tboox/tbox/archive/v1.6.8.tar.gz"
-  sha256 "f437a31caa769a980e2e38ecc5bf37f1e572325d5d60fd242b9d6d49174b66fd"
+  url "https://github.com/tboox/tbox/archive/v1.6.9.tar.gz"
+  sha256 "31db6cc51af7db76ad5b5da88356982b1e0f1e624c466c749646dd203b68adae"
   license "Apache-2.0"
   head "https://github.com/tboox/tbox.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tbox"
-    sha256 cellar: :any_skip_relocation, mojave: "9ced717fd0a36159e151dd83a62edc908bcdfe1e658804524ff3f52ac839464a"
+    sha256 cellar: :any_skip_relocation, mojave: "3643177ca47ab1fa39d877e8995808df154316fd516ebd13995d7357eebea5ad"
   end
 
   depends_on "xmake" => :build
