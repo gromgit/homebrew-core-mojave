5f73a98ff89955c9894e331c1cfb91a6511e0f62
Adrian Ho <the.gromgit@gmail.com>
2022-06-22 13:44:44 +0800

    pgpool-ii: update 4.3.2_1 bottle.

--- Formula/pgpool-ii.rb
+++ Formula/pgpool-ii.rb
@@ -3,6 +3,7 @@ class PgpoolIi < Formula
   homepage "https://www.pgpool.net/mediawiki/index.php/Main_Page"
   url "https://www.pgpool.net/mediawiki/images/pgpool-II-4.3.2.tar.gz"
   sha256 "5715f562635f41fc6b62e241cd753109a3e2014ccfb62352063115310d034f0a"
+  revision 1
 
   livecheck do
     url "https://www.pgpool.net/mediawiki/index.php/Downloads"
@@ -11,10 +12,12 @@ class PgpoolIi < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pgpool-ii"
-    sha256 mojave: "149174372a61bfb5cd2e1442c37a87d70b73a695895fe2f4fbea715ad6491f61"
+    sha256 mojave: "58a9fb8206411894a2c1b8df623b791e2e546a900232f347766e008598dc1ee8"
   end
 
   depends_on "postgresql"
+
+  uses_from_macos "libxcrypt"
 
   # Fix -flat_namespace being used on Big Sur and later.
   patch do
