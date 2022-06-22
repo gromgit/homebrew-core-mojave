4e5286d6a701e08c01a1b78248a1ad43058b364c
Adrian Ho <the.gromgit@gmail.com>
2022-06-22 22:37:54 +0800

    scw: update 2.5.3 bottle.

--- Formula/scw.rb
+++ Formula/scw.rb
@@ -1,19 +1,19 @@
 class Scw < Formula
   desc "Command-line Interface for Scaleway"
   homepage "https://github.com/scaleway/scaleway-cli"
-  url "https://github.com/scaleway/scaleway-cli/archive/v2.5.1.tar.gz"
-  sha256 "af926168122c192b10a19d701f2a03a41f14897b2a6c654499203edd2aafcafe"
+  url "https://github.com/scaleway/scaleway-cli/archive/v2.5.3.tar.gz"
+  sha256 "cf4ce3616d61fc5c1762663d660af9e06d59a15af3a2ce164b7f4c1c5b8aed3a"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/scw"
-    sha256 cellar: :any_skip_relocation, mojave: "8f521729636759992b228a3b22b55fe349416b0ee87df2955baeabd4e0879c73"
+    sha256 cellar: :any_skip_relocation, mojave: "e93c84e34114fda9534e38ac294d5222e4bb7481342db78c815b3ddf8ac8cce1"
   end
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args, "./cmd/scw"
+    system "go", "build", *std_go_args(ldflags: "-X main.Version=#{version}"), "./cmd/scw"
 
     zsh_output = Utils.safe_popen_read({ "SHELL" => "zsh" }, bin/"scw", "autocomplete", "script")
     (zsh_completion/"_scw").write zsh_output
