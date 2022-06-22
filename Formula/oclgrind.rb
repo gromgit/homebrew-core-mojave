471a1811890134d3eb6852abce354719ddd5ef32
Adrian Ho <the.gromgit@gmail.com>
2022-06-22 10:06:55 +0800

    oclgrind: update 21.10 bottle.

--- Formula/oclgrind.rb
+++ Formula/oclgrind.rb
@@ -11,20 +11,18 @@ class Oclgrind < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "c28fb5bc8dd0fa6897dfeae3dcd1b04c7d2494d6e69f2fdad110dfe0fd53210b"
-    sha256 cellar: :any, arm64_big_sur:  "7eb82923ede6b708c46585ea8277f4f73ed521f5a9411709f27de4daa9f48bb4"
-    sha256 cellar: :any, monterey:       "456d3363136042e98b217cd9fad9165fa0038b97c68279911ff7cd93cfc4889e"
-    sha256 cellar: :any, big_sur:        "d7e20e1c27a6716a448f9e878f60706490751edfde5296a676776c2ec3d2ef2c"
-    sha256 cellar: :any, catalina:       "3b7f3865bc3ed7ea3f9d4436b9e4a003b8759262347c7a27edf68ae291e804a2"
-    sha256 cellar: :any, mojave:         "f92d8a342ad7d878f2164345e2f8982d8bd6c750ef625e1761f80cee666627fb"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/oclgrind"
+    rebuild 1
+    sha256 cellar: :any, mojave: "f28433ee2caebb620954348525be1b30cde847d047bf072d3b80aa8a7f572c91"
   end
 
   depends_on "cmake" => :build
   depends_on "llvm"
 
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DCMAKE_INSTALL_RPATH=#{rpath}"
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
