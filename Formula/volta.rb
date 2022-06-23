4b09f3de371f35252b48f4edfc1e836b105c7a6b
Adrian Ho <the.gromgit@gmail.com>
2022-06-23 10:33:57 +0800

    volta: update 1.0.8 bottle.

--- Formula/volta.rb
+++ Formula/volta.rb
@@ -2,8 +2,8 @@ class Volta < Formula
   desc "JavaScript toolchain manager for reproducible environments"
   homepage "https://volta.sh"
   url "https://github.com/volta-cli/volta.git",
-      tag:      "v1.0.7",
-      revision: "b7ed23325d2825143471962cf94b722e1305e615"
+      tag:      "v1.0.8",
+      revision: "d4fc9df13ab975d7f6424b85a4052f14ab0e9bd5"
   license "BSD-2-Clause"
 
   livecheck do
@@ -13,7 +13,7 @@ class Volta < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/volta"
-    sha256 cellar: :any_skip_relocation, mojave: "c58b2e1ce8dbd1dcfa90304814fee71363bb92673e84a6559ec45704d219d7b9"
+    sha256 cellar: :any_skip_relocation, mojave: "a75173a3c0901a8e3c607788f14475efb57cb4ba711495c7d9d4ac38049d8934"
   end
 
   depends_on "rust" => :build
