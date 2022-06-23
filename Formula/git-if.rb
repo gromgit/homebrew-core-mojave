015899ec49d7b4ff46b06c6b28d51f6d58fdab44
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-23 20:19:40 +0800

    git-if: no rebuilding required

--- Formula/git-if.rb
+++ Formula/git-if.rb
@@ -8,11 +8,13 @@ class GitIf < Formula
   head "https://github.com/DavidKinder/Git.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1cbdac43ba193647b00e4dca570d079cfdf975e520c445e98d9352d51100b91c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "56d9c7c7ba0b996340fddcfe108ff45a3e813330f9af771f6b9d398be0b7129e"
-    sha256 cellar: :any_skip_relocation, catalina:      "f866a6a21977f9fd16230087f0e5239d8c8b37f7a158fcbb8d257a225e222774"
-    sha256 cellar: :any_skip_relocation, mojave:        "f4785e352c5810e9642f490e5c27d02db1ea35c2167cb6c58d88d55002501e7c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f4f161d016e220b462129394b39b8ea7bad324349a73741489d3c6ea8d65f2db"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "e19725f9c771b3ec81d2a0e7f0d1740caf80bf7306007bd921e42e53ce56c506"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1cbdac43ba193647b00e4dca570d079cfdf975e520c445e98d9352d51100b91c"
+    sha256 cellar: :any_skip_relocation, monterey:       "52335ad39b70a74c122c3a603b8bafa90744f1b733fbd2ba2a016c2c62b63842"
+    sha256 cellar: :any_skip_relocation, big_sur:        "56d9c7c7ba0b996340fddcfe108ff45a3e813330f9af771f6b9d398be0b7129e"
+    sha256 cellar: :any_skip_relocation, catalina:       "f866a6a21977f9fd16230087f0e5239d8c8b37f7a158fcbb8d257a225e222774"
+    sha256 cellar: :any_skip_relocation, mojave:         "f4785e352c5810e9642f490e5c27d02db1ea35c2167cb6c58d88d55002501e7c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f4f161d016e220b462129394b39b8ea7bad324349a73741489d3c6ea8d65f2db"
   end
 
   depends_on "glktermw" => :build
