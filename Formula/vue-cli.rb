0272d939db33b42c6f17e42bd2dcc8b785edbc64
Adrian Ho <the.gromgit@gmail.com>
2022-06-23 11:15:52 +0800

    vue-cli: update 5.0.6 bottle.

--- Formula/vue-cli.rb
+++ Formula/vue-cli.rb
@@ -3,13 +3,13 @@ require "language/node"
 class VueCli < Formula
   desc "Standard Tooling for Vue.js Development"
   homepage "https://cli.vuejs.org/"
-  url "https://registry.npmjs.org/@vue/cli/-/cli-5.0.4.tgz"
-  sha256 "07aba062833f861b86b83e04a70633e8f2f05e56b1db0105477fdc9363a15d4b"
+  url "https://registry.npmjs.org/@vue/cli/-/cli-5.0.6.tgz"
+  sha256 "931b352f6aa7fae2158a01240312fe391a3053f7dfaa9c686b2fb370aeee4c50"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/vue-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "6c7f7530ce4d1d312717831be9207e30cdb297e3132be310de294cd076a0d1e6"
+    sha256 cellar: :any_skip_relocation, mojave: "efc0632b67a81ff9e783ea2a2df4d063bef514c2dfdfed7eebc58fa4fa0f7927"
   end
 
   depends_on "node"
