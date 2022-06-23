fbdea9bfb6b47452e27224e218414505d8077a4b
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-23 20:19:54 +0800

    pyoxidizer:

--- Formula/pyoxidizer.rb
+++ Formula/pyoxidizer.rb
@@ -1,8 +1,8 @@
 class Pyoxidizer < Formula
   desc "Modern Python application packaging and distribution tool"
   homepage "https://github.com/indygreg/PyOxidizer"
-  url "https://github.com/indygreg/PyOxidizer/archive/pyoxidizer/0.20.0.tar.gz"
-  sha256 "11baf7bac8f869d6458b5aee2cb8ecd7f2d7f170211ee97f979abb62eea22bc4"
+  url "https://github.com/indygreg/PyOxidizer/archive/pyoxidizer/0.22.0.tar.gz"
+  sha256 "16fc48067467d19049476923d82f33b27ba944551ec39d4d129415ddc0cac738"
   license "MPL-2.0"
   head "https://github.com/indygreg/PyOxidizer.git", branch: "main"
 
@@ -12,12 +12,12 @@ class Pyoxidizer < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "673b3f77fdd82c6fbde009da7461c759d8f6cf2d6090cb76359ee9001b25249f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1a636c6b8142fc459301f010519b9fb7085da25940063ed5ecd15dd06d28c499"
-    sha256 cellar: :any_skip_relocation, monterey:       "5449fa05218b1d985cfb7bd09e619f2403896732a80446f6599d73c42c9adac9"
-    sha256 cellar: :any_skip_relocation, big_sur:        "71b0ac60092c72ab35389de12abecd0ca258ede50f36a83768930b94c6718d9b"
-    sha256 cellar: :any_skip_relocation, catalina:       "e3a603c9950e3d8105359b2be8fca39a318a0215353c411bfa22f3eb1f6344ac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8b7d2be825e2393086f57e9e3e043166e0559c2de3e1170782ca9c6030dfd787"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "36363b96a5bb8b74a88fdcccc49e7d0d33de2090e42fd5dda2e75641d2c1a6a1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "35b6d9f269eae4a5611caaed0fe0779f9f5042cb25bc66a3373d652cc63cb004"
+    sha256 cellar: :any_skip_relocation, monterey:       "47b5770b9e916c3a675b0ff6d44340004987dd6d203b3735292d08ee937c930f"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f3879e10e9c10974220fdbe17b8469686182191d48c58733418dc881bdca23eb"
+    sha256 cellar: :any_skip_relocation, catalina:       "19f95c7acf6b1c2ea99c7cd59a3d684a0bff43778612d12bab76076c982bef13"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a0c4948b577a48fa5944423a1a1d148a5ee332ea4d14effe227bef7210b65184"
   end
 
   depends_on "rust" => :build
