f6478abc9f0a48201e0e797afe0616b07cf5c74f
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-23 20:19:57 +0800

    redo: no rebuilding required

--- Formula/redo.rb
+++ Formula/redo.rb
@@ -6,43 +6,50 @@ class Redo < Formula
   url "https://github.com/apenwarr/redo/archive/redo-0.42d.tar.gz"
   sha256 "47056b429ff5f85f593dcba21bae7bc6a16208a56b189424eae3de5f2e79abc1"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1a53fdaf0a75d657dd627f7ecd0fa33826e065554b31b07540c1378f383b4964"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "22dd0cd4b9d1e945d90a6bce7138c7c0b5e834d74c0be14fd39258b8225dd8af"
-    sha256 cellar: :any_skip_relocation, monterey:       "37bb6b29d00caa6c9d81fbe6418350ad833203c63095b8a79189320f32b00d6f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "db186be965150e95e72d55d47acb86cf4839fa7949149e01522870c7a640edce"
-    sha256 cellar: :any_skip_relocation, catalina:       "ed58682729a217dcbfa7ace51f5ec8a3d57cdb30248173820d1bdbd599999675"
-    sha256 cellar: :any_skip_relocation, mojave:         "653c09646f838dfed31e0f79f3364d2604cc688a8735e098736092a55a631d3e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6e6d7dcf5e439d5a882a2bf480cff8a35b2e3cfe22e967935de67313444d3a46"
+    sha256 cellar: :any_skip_relocation, all: "acf6514ab6680a75295138f1fa292008eda79e13059a48f825fb74dd5c24bc06"
   end
 
   depends_on "python@3.10"
 
-  resource "Markdown" do
-    url "https://files.pythonhosted.org/packages/49/02/37bd82ae255bb4dfef97a4b32d95906187b7a7a74970761fca1360c4ba22/Markdown-3.3.4.tar.gz"
-    sha256 "31b5b491868dcc87d6c24b7e3d19a0d730d59d3e46f4eea6430a321bed387a49"
+  resource "beautifulsoup4" do
+    url "https://files.pythonhosted.org/packages/e8/b0/cd2b968000577ec5ce6c741a54d846dfa402372369b8b6861720aa9ecea7/beautifulsoup4-4.11.1.tar.gz"
+    sha256 "ad9aa55b65ef2808eb405f46cf74df7fcb7044d5cbc26487f96eb2ef2e436693"
   end
 
-  resource "beautifulsoup4" do
-    url "https://files.pythonhosted.org/packages/a1/69/daeee6d8f22c997e522cdbeb59641c4d31ab120aba0f2c799500f7456b7e/beautifulsoup4-4.10.0.tar.gz"
-    sha256 "c23ad23c521d818955a4151a67d81580319d4bf548d3d49f4223ae041ff98891"
+  resource "Markdown" do
+    url "https://files.pythonhosted.org/packages/d6/58/79df20de6e67a83f0d0bbfe6c19bb82adf68cdf362885257eb01099f930a/Markdown-3.3.7.tar.gz"
+    sha256 "cbb516f16218e643d8e0a95b309f77eb118cb138d39a4f27851e6a63581db874"
+  end
+
+  resource "soupsieve" do
+    url "https://files.pythonhosted.org/packages/f3/03/bac179d539362319b4779a00764e95f7542f4920084163db6b0fd4742d38/soupsieve-2.3.2.post1.tar.gz"
+    sha256 "fc53893b3da2c33de295667a0e19f078c14bf86544af307354de5fcf12a3f30d"
   end
 
   def install
-    venv = virtualenv_create(libexec, Formula["python@3.10"].opt_bin/"python3")
+    # Prevent system Python 2 from being detected
+    inreplace "redo/whichpython.do", " python python3 python2 python2.7;", " python3;"
+
+    # Prepare build-only virtualenv for generating manpages.
+    venv = virtualenv_create(buildpath/"venv", "python3")
     venv.pip_install resources
-    # Set the interpreter so that ./do install can find the pip installed
-    # resources
+
+    # Set PYTHONPATH rather than prepending PATH with venv as shebangs are set to detected python.
+    ENV.prepend_path "PYTHONPATH", buildpath/"venv"/Language::Python.site_packages("python3")
+
     ENV["DESTDIR"] = ""
     ENV["PREFIX"] = prefix
     system "./do", "install"
-    man1.install Dir["docs/*.1"]
   end
 
   test do
     assert_equal version.to_s, shell_output("#{bin}/redo --version").strip
+    # Make sure man pages were generated and installed
+    assert_predicate man1/"redo.1", :exist?
+
     # Test is based on https://redo.readthedocs.io/en/latest/cookbook/hello/
     (testpath/"hello.c").write <<~EOS
       #include <stdio.h>
