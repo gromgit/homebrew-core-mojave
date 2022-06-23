b2c0022eabb7e1b112da80cf6cbb06e5534d3481
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-23 20:19:59 +0800

    snap-telemetry:

--- Formula/snap-telemetry.rb
+++ Formula/snap-telemetry.rb
@@ -7,13 +7,19 @@ class SnapTelemetry < Formula
   head "https://github.com/intelsdi-x/snap.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/snap-telemetry"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "442b15fbbf135af47a14911b23ccbaadc4b5b7fc476bbe7bdaf50ce3f0aec314"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "094be117be921cca221b7e0021e7e7d48d496e83599ed52fbd81c64b3b389d5b"
+    sha256 cellar: :any_skip_relocation, big_sur:       "4e8cca8dbc731cb1bf7b92a8f410f287678ab270450cd0f58ce6f10eb7e3e1d5"
+    sha256 cellar: :any_skip_relocation, catalina:      "6f52483af1ce2785dc7e9bf0fdc202430c61b804ef3a67e2487d669bf27edcb1"
+    sha256 cellar: :any_skip_relocation, mojave:        "1cd9b411854596b3afe7afa22ed9041d31e21a860739246a5eeb47e03a6844e8"
+    sha256 cellar: :any_skip_relocation, high_sierra:   "066cf3014caa27b6c3327f983cbe632cb85476c0731ec3fda40e85205c1a5f71"
+    sha256 cellar: :any_skip_relocation, sierra:        "1ff53b8b2f1827e2a607d81dd3db246eb1388dfd1aa7110dcf59a8e4ba606d17"
+    sha256 cellar: :any_skip_relocation, el_capitan:    "50ce1be7d6e83f309d8fd62bf2b36cb03c29b726d575abfbeef895b3f628fb46"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2ce6d7532d5ca326b0bd497988ff5aab340c49f6e9334898ecdf312df73b1dfa"
   end
 
   # https://github.com/intelsdi-x/snap/commit/e3a6c8e39994b3980df0c7b069d5ede810622952
-  deprecate! date: "2018-12-20", because: :deprecated_upstream
+  # Deprecation added 2021-02-21 / deprecated since 2018-12-20
+  disable! date: "2022-06-08", because: :deprecated_upstream
 
   depends_on "glide" => :build
   depends_on "go" => :build
