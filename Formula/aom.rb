4bcb0324db6c1ac18af3429d2a41cc6a329cc5fe
Adrian Ho <the.gromgit@gmail.com>
2022-06-24 10:59:14 +0800

    aom: update 3.4.0 bottle.

--- Formula/aom.rb
+++ Formula/aom.rb
@@ -2,13 +2,13 @@ class Aom < Formula
   desc "Codec library for encoding and decoding AV1 video streams"
   homepage "https://aomedia.googlesource.com/aom"
   url "https://aomedia.googlesource.com/aom.git",
-      tag:      "v3.3.0",
-      revision: "87460cef80fb03def7d97df1b47bad5432e5e2e4"
+      tag:      "v3.4.0",
+      revision: "fc430c57c7b0307b4c5ffb686cd90b3c010d08d2"
   license "BSD-2-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/aom"
-    sha256 cellar: :any, mojave: "fbc8a86a2200b529c900f48f7de5e9ccf8f6c8307b2437078984ccf3ce0763ee"
+    sha256 cellar: :any, mojave: "0b81b577035c1b8b91e306b36357432fafecb97740aea8d202feac8a48b9b242"
   end
 
   depends_on "cmake" => :build
@@ -21,14 +21,10 @@ class Aom < Formula
     depends_on "libvmaf"
   end
 
-  resource "bus_qcif_15fps.y4m" do
+  resource "homebrew-bus_qcif_15fps.y4m" do
     url "https://media.xiph.org/video/derf/y4m/bus_qcif_15fps.y4m"
     sha256 "868fc3446d37d0c6959a48b68906486bd64788b2e795f0e29613cbb1fa73480e"
   end
-
-  # Fix build with `-DCONFIG_TUNE_BUTTERAUGLI=1`.
-  # https://aomedia.googlesource.com/aom.git/+/b389ce89bdb6a3097e637d947123ffc4b9aea763%5E%21/
-  patch :DATA
 
   def install
     ENV.runtime_cpu_detection unless Hardware::CPU.arm?
@@ -57,7 +53,7 @@ class Aom < Formula
   end
 
   test do
-    resource("bus_qcif_15fps.y4m").stage do
+    resource("homebrew-bus_qcif_15fps.y4m").stage do
       system "#{bin}/aomenc", "--webm",
                               "--tile-columns=2",
                               "--tile-rows=2",
@@ -70,56 +66,3 @@ class Aom < Formula
     end
   end
 end
-
-__END__
-commit b389ce89bdb6a3097e637d947123ffc4b9aea763
-Author: James Zern <jzern@google.com>
-Date:   Mon Mar 7 16:35:49 2022 -0800
-
-    fix compile w/-DCONFIG_TUNE_BUTTERAUGLI=1
-    
-    This was broken independently by:
-    
-    - av1_set_quantizer() parameter update
-      b89e8f8f7 add support for qp adjustment for HDR video
-    
-    - av1_scale_if_required -> av1_realloc_and_scale_if_required
-      dba4f0f3e Allocate scaled source buffers on the fly
-    
-    Bug: b/222461449
-    Change-Id: I521e6e20a1f9dab111f2fe63eed7122f0e5d257b
-
-diff --git a/av1/encoder/tune_butteraugli.c b/av1/encoder/tune_butteraugli.c
-index c5bbee1ae..70fa23922 100644
---- a/av1/encoder/tune_butteraugli.c
-+++ b/av1/encoder/tune_butteraugli.c
-@@ -262,13 +262,15 @@ void av1_setup_butteraugli_rdmult(AV1_COMP *cpi) {
-   av1_set_frame_size(cpi, cm->superres_upscaled_width,
-                      cm->superres_upscaled_height);
- 
--  cpi->source =
--      av1_scale_if_required(cm, cpi->unscaled_source, &cpi->scaled_source,
--                            cm->features.interp_filter, 0, false, false);
-+  cpi->source = av1_realloc_and_scale_if_required(
-+      cm, cpi->unscaled_source, &cpi->scaled_source, cm->features.interp_filter,
-+      0, false, false, cpi->oxcf.border_in_pixels,
-+      cpi->oxcf.tool_cfg.enable_global_motion);
-   if (cpi->unscaled_last_source != NULL) {
--    cpi->last_source = av1_scale_if_required(
-+    cpi->last_source = av1_realloc_and_scale_if_required(
-         cm, cpi->unscaled_last_source, &cpi->scaled_last_source,
--        cm->features.interp_filter, 0, false, false);
-+        cm->features.interp_filter, 0, false, false, cpi->oxcf.border_in_pixels,
-+        cpi->oxcf.tool_cfg.enable_global_motion);
-   }
- 
-   av1_setup_butteraugli_source(cpi);
-@@ -295,7 +297,7 @@ void av1_setup_butteraugli_rdmult(AV1_COMP *cpi) {
-   // cpi->sf.part_sf.fixed_partition_size = BLOCK_32X32;
- 
-   av1_set_quantizer(cm, q_cfg->qm_minlevel, q_cfg->qm_maxlevel, q_index,
--                    q_cfg->enable_chroma_deltaq);
-+                    q_cfg->enable_chroma_deltaq, q_cfg->enable_hdr_deltaq);
-   av1_set_speed_features_qindex_dependent(cpi, oxcf->speed);
-   if (q_cfg->deltaq_mode != NO_DELTA_Q || q_cfg->enable_chroma_deltaq)
-     av1_init_quantizer(&cpi->enc_quant_dequant_params, &cm->quant_params,
