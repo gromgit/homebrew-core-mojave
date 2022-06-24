12c1ace399ad447fe837edaa2e3734347ac6f318
Adrian Ho <the.gromgit@gmail.com>
2022-06-24 18:10:58 +0800

    tflint: update 0.38.1 bottle.

--- Formula/tflint.rb
+++ Formula/tflint.rb
@@ -1,14 +1,14 @@
 class Tflint < Formula
   desc "Linter for Terraform files"
   homepage "https://github.com/terraform-linters/tflint"
-  url "https://github.com/terraform-linters/tflint/archive/v0.37.0.tar.gz"
-  sha256 "ec2024762b9e6e12629cb17f724585cc0c29ddd201590562f7f9b4be2da472fb"
+  url "https://github.com/terraform-linters/tflint/archive/v0.38.1.tar.gz"
+  sha256 "615b114cc0f740d34bdd2b2e148810d1a91ceeeda6a07bfce66f8b94661cf8fb"
   license "MPL-2.0"
   head "https://github.com/terraform-linters/tflint.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tflint"
-    sha256 cellar: :any_skip_relocation, mojave: "be88004e2cd9284bbe27a79e8d84476ee75ff3859de82952e6e57b0a93da27b6"
+    sha256 cellar: :any_skip_relocation, mojave: "5e922da3304f3823ec462fc613d555d2e7a08ac75fb3e8564d519e4f08a7bf3e"
   end
 
   depends_on "go" => :build
