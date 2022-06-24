a38b9cc700e3683e253488c3419fcbda9946fe3e
Adrian Ho <the.gromgit@gmail.com>
2022-06-24 14:00:31 +0800

    gofish: update 0.15.1 bottle.

--- Formula/gofish.rb
+++ Formula/gofish.rb
@@ -1,6 +1,6 @@
 class Gofish < Formula
   desc "Cross-platform systems package manager"
-  homepage "https://gofi.sh"
+  homepage "https://github.com/fishworks/gofish"
   url "https://github.com/fishworks/gofish.git",
       tag:      "v0.15.1",
       revision: "5d14f73963cfc0c226e8b06c0f5c3404d2ec2e77"
@@ -8,8 +8,11 @@ class Gofish < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gofish"
-    sha256 cellar: :any_skip_relocation, mojave: "d5c99b3276240c2222608fb01e03bf861fc2f8b70df6e45197cc91e546237928"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "10ec5ff54ecdc5cfea0d12d90fe1672f7182eee64aa85ecb17b09ba08ac2beef"
   end
+
+  deprecate! date: "2022-06-21", because: :repo_archived
 
   depends_on "go" => :build
 
