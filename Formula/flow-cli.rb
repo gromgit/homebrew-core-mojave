bb9d56f4fad93489ebe7a8e572804a3a9442be5d
Adrian Ho <the.gromgit@gmail.com>
2022-06-24 12:48:30 +0800

    flow-cli: update 0.37.0 bottle.

--- Formula/flow-cli.rb
+++ Formula/flow-cli.rb
@@ -1,8 +1,8 @@
 class FlowCli < Formula
   desc "Command-line interface that provides utilities for building Flow applications"
   homepage "https://onflow.org"
-  url "https://github.com/onflow/flow-cli/archive/v0.36.2.tar.gz"
-  sha256 "19eaff46eda642f2551fe524b6591c0ec5630006a817ca3810d09baae9263371"
+  url "https://github.com/onflow/flow-cli/archive/v0.37.0.tar.gz"
+  sha256 "2132f43ff7351425a091199543d7556904f3e79652a9274300646c2fe40ffd05"
   license "Apache-2.0"
   head "https://github.com/onflow/flow-cli.git", branch: "master"
 
@@ -13,7 +13,7 @@ class FlowCli < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/flow-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "9cf4279cdd2d22757318cf5aba8870991490f252c18d2920cad52fc486a1239f"
+    sha256 cellar: :any_skip_relocation, mojave: "c2a064e8325c88ed5f23e356e3cc5e954a16e1c1cea43ba9155c81e87fd4cb51"
   end
 
   depends_on "go" => :build
