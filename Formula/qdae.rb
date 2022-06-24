0a457e7ded61163ed6e20dd030c21cd22a7a7ed6
Adrian Ho <the.gromgit@gmail.com>
2022-06-24 17:09:21 +0800

    qdae: update 0.0.10_1 bottle.

--- Formula/qdae.rb
+++ Formula/qdae.rb
@@ -4,6 +4,7 @@ class Qdae < Formula
   url "https://www.seasip.info/Unix/QDAE/qdae-0.0.10.tar.gz"
   sha256 "780752c37c9ec68dd0cd08bd6fe288a1028277e10f74ef405ca200770edb5227"
   license "GPL-2.0"
+  revision 1
 
   livecheck do
     url :homepage
@@ -11,19 +12,16 @@ class Qdae < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 arm64_big_sur: "9551d2289bd90b76851cebd6d1f11e2e3b1c9ca856d8eb4f88312ec941efb097"
-    sha256 big_sur:       "1e6b4a1ae9c0f6c69623f72e6d84429f57986b6560939cfa7ea02b36a2f39830"
-    sha256 catalina:      "d951231205b4f4faf3e4f829665d25c82d236f3f52339dd5664fb8adb46e68eb"
-    sha256 mojave:        "290d931e61684c53227e0a16d808427eb7218fbec76c57eb250c03dbf15bb6b8"
-    sha256 high_sierra:   "945b28c4354053f3ebd81bb868ef6a14d8fef1c32d6cebd73455bd17f17332ae"
-    sha256 x86_64_linux:  "663f3822c76388f597539c10e9296fb58289d364902b86497f037843cce25d85"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/qdae"
+    sha256 mojave: "21000f1468f4695f743562ab37ac5e3937b8cf9d454e401c34dd7a74489bb9a8"
   end
 
-  depends_on "libxml2"
   depends_on "sdl"
 
+  uses_from_macos "libxml2"
+
   def install
+    ENV.cxx11
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
