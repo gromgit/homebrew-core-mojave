862de6df58aba4c0a570e7150654bce569f905e7
Adrian Ho <the.gromgit@gmail.com>
2022-06-24 18:34:54 +0800

    weaver: update 1.1.1 bottle.

--- Formula/weaver.rb
+++ Formula/weaver.rb
@@ -1,13 +1,13 @@
 class Weaver < Formula
   desc "Command-line tool for Weaver"
   homepage "https://github.com/scribd/Weaver"
-  url "https://github.com/scribd/Weaver/archive/1.1.0.tar.gz"
-  sha256 "b6ea521ce3bbd0f55d0f8b61181312bb23a4da6d3605fe449d080abeffe09d91"
+  url "https://github.com/scribd/Weaver/archive/1.1.1.tar.gz"
+  sha256 "2c82cafa133002addc106f4bcdac1ea32ed50eddf98b63422ce28ff5f086e508"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/weaver"
-    sha256 cellar: :any_skip_relocation, mojave: "36590ae19fcbb2774d796d33d797ea18025375b7cbd89b4ae9d430544c23c963"
+    sha256 cellar: :any_skip_relocation, mojave: "93b51bdb1c859cddf0d03cb3d69b297dc638e0d8d586c56a5620d1e1282b465d"
   end
 
   depends_on xcode: ["11.2", :build]
