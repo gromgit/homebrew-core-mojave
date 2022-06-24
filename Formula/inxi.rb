c2f3d5c663a96c003f5c2918eef3d6ec88ad4334
Adrian Ho <the.gromgit@gmail.com>
2022-06-24 14:38:13 +0800

    inxi: update 3.3.19-1 bottle.

--- Formula/inxi.rb
+++ Formula/inxi.rb
@@ -1,15 +1,14 @@
 class Inxi < Formula
   desc "Full featured CLI system information tool"
   homepage "https://smxi.org/docs/inxi.htm"
-  url "https://github.com/smxi/inxi/archive/3.3.13-1.tar.gz"
-  sha256 "44d11d88fc0fd2213401436d8e7d09b755ab8af3ca10443d7f4fb32edae92fef"
+  url "https://github.com/smxi/inxi/archive/3.3.19-1.tar.gz"
+  sha256 "bf8881c140eac6fd266e32db31bcd90d93c5e3d429c426aacfb295d0e57e1bd9"
   license "GPL-3.0-or-later"
   head "https://github.com/smxi/inxi.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/inxi"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "228b464e382e1d9dc30c3c81d6644bb50697c74728e30e96f2da64d154f3fb6c"
+    sha256 cellar: :any_skip_relocation, mojave: "6d8315a833249edc40f1bb82f50a64ecc0d7f0749cd9a1fa79f74417e1d6d635"
   end
 
   uses_from_macos "perl"
@@ -25,14 +24,6 @@ class Inxi < Formula
 
   test do
     inxi_output = shell_output("#{bin}/inxi")
-
-    # This test does not work on Linux, because on that platform
-    # inxi does not print the OS name, only the kernel version.
-    if OS.mac?
-      uname = shell_output("uname").strip
-      assert_match uname.to_str, inxi_output.to_s
-    end
-
     uname_r = shell_output("uname -r").strip
     assert_match uname_r.to_str, inxi_output.to_s
   end
