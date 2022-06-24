85d138b4443f8b5aee333461b6057e315af4b2e2
Adrian Ho <the.gromgit@gmail.com>
2022-06-24 17:24:12 +0800

    sevenzip: update 22.00 bottle.

--- Formula/sevenzip.rb
+++ Formula/sevenzip.rb
@@ -1,9 +1,9 @@
 class Sevenzip < Formula
   desc "7-Zip is a file archiver with a high compression ratio"
   homepage "https://7-zip.org"
-  url "https://7-zip.org/a/7z2107-src.tar.xz"
-  version "21.07"
-  sha256 "213d594407cb8efcba36610b152ca4921eda14163310b43903d13e68313e1e39"
+  url "https://7-zip.org/a/7z2200-src.tar.xz"
+  version "22.00"
+  sha256 "40969f601e86aff49aaa0ba0df5ce6fd397cf7e2683a84b591b0081e461ef675"
   license all_of: ["LGPL-2.1-or-later", "BSD-3-Clause"]
 
   livecheck do
@@ -13,11 +13,17 @@ class Sevenzip < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sevenzip"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "385e144a023d6a9bb7c41c581dd06be32de0a90ad489c506cb4b25b0b90a0b37"
+    sha256 cellar: :any_skip_relocation, mojave: "1417aacd5e96dc9d13636a793107a1971b044c1d3467deec85a0d157be61074a"
   end
 
   def install
+    # See https://sourceforge.net/p/sevenzip/discussion/45797/thread/9c2d9061ce/#01e7
+    if OS.mac?
+      inreplace ["Common/FileStreams.cpp", "UI/Common/UpdateCallback.cpp"].map { |d| buildpath/"CPP/7zip"/d },
+                "sysmacros.h",
+                "types.h"
+    end
+
     cd "CPP/7zip/Bundles/Alone2" do
       mac_suffix = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch
       mk_suffix, directory = if OS.mac?
