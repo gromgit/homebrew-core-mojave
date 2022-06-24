41e4cf60bd6b938623f95cb631b4cf4db79b8c0a
Adrian Ho <the.gromgit@gmail.com>
2022-06-24 12:16:15 +0800

    dbdeployer: update 1.65.0 bottle.

--- Formula/dbdeployer.rb
+++ Formula/dbdeployer.rb
@@ -1,15 +1,14 @@
 class Dbdeployer < Formula
   desc "Tool to deploy sandboxed MySQL database servers"
   homepage "https://github.com/datacharmer/dbdeployer"
-  url "https://github.com/datacharmer/dbdeployer/archive/v1.64.0.tar.gz"
-  sha256 "8065d2700f870d10e58af7b2764ffa0838254402f874294dbc39845356d6f069"
+  url "https://github.com/datacharmer/dbdeployer/archive/v1.65.0.tar.gz"
+  sha256 "2de7774c1e994b7c10ad676a3c9bed33244b198a2f880ae133a95571b4f49a30"
   license "Apache-2.0"
   head "https://github.com/datacharmer/dbdeployer.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dbdeployer"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "7ae315d94c3faec3ac2bf05e32cf636a067340a25ca50864198426919d5abd24"
+    sha256 cellar: :any_skip_relocation, mojave: "d806b344c6271c23352143490d06f2f0b78706b8b6fda4a92afea8c1b535fbd5"
   end
 
   depends_on "go" => :build
