bf1ec6a36f74acdee17541fd301235375b966d67
Adrian Ho <the.gromgit@gmail.com>
2022-06-24 12:11:27 +0800

    dar: update 2.7.6 bottle.

--- Formula/dar.rb
+++ Formula/dar.rb
@@ -1,8 +1,8 @@
 class Dar < Formula
   desc "Backup directory tree and files"
   homepage "http://dar.linux.free.fr/doc/index.html"
-  url "https://downloads.sourceforge.net/project/dar/dar/2.7.5/dar-2.7.5.tar.gz"
-  sha256 "95fa493a899a755fe84c9b0e9681f5ad81795b2de456bf84054bea67ec5a0966"
+  url "https://downloads.sourceforge.net/project/dar/dar/2.7.6/dar-2.7.6.tar.gz"
+  sha256 "3d5e444891350768109d7e9051e26039bdeb906de30294e7e0d71105b87d6daa"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -12,7 +12,7 @@ class Dar < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dar"
-    sha256 mojave: "245fe384d19e960bda4f2a478a69713c06c4f20de6acf48f9c54c7b675904cf1"
+    sha256 mojave: "2d21d75e62e15dbd27103ec511bd8a2fd3645cca6060978a16d8b65205f0de8f"
   end
 
   depends_on "upx" => :build unless Hardware::CPU.arm?
