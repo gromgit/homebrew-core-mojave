e642623abf4a7fdd90e07cc79231d12eeb46c75a
Adrian Ho <the.gromgit@gmail.com>
2022-06-26 07:33:32 +0800

    rswift: update 6.1.0 bottle.

--- Formula/rswift.rb
+++ Formula/rswift.rb
@@ -1,18 +1,14 @@
 class Rswift < Formula
   desc "Get strong typed, autocompleted resources like images, fonts and segues"
   homepage "https://github.com/mac-cain13/R.swift"
-  url "https://github.com/mac-cain13/R.swift/releases/download/v5.4.0/rswift-v5.4.0-source.tar.gz"
-  sha256 "5153e7d122412ced4f04b6fc92c10dad0a861900858543a77ce1bf11850d4184"
+  url "https://github.com/mac-cain13/R.swift/releases/download/v6.1.0/rswift-v6.1.0-source.tar.gz"
+  sha256 "f4b4c3f8748358c569c219d7f506d3b34ea5af82c882ee4a23381f23c4d277c8"
   license "MIT"
   head "https://github.com/mac-cain13/R.swift.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2eb8a16c1ea824ce6ea93423f083cd94e1fbe90ca3cb9daedc985347b101f657"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b07728bea1baa0568381b1f41a34d66bbc283e7a1c5813e7078faf30a1c6a102"
-    sha256 cellar: :any_skip_relocation, monterey:       "beb6182193ec3a444d4c4661ec9b5dc2fddbb8fe398945f2d5bbb8a63f773492"
-    sha256 cellar: :any_skip_relocation, big_sur:        "495500a61dc7f30d7f00f19a1c427a43990c1beb8cf99bee2b50c373c7d7eab7"
-    sha256 cellar: :any_skip_relocation, catalina:       "4776447ac9845ebd27c5ac2ab8bec3c50e79c8c7105e8702f67393da5a609747"
-    sha256 cellar: :any_skip_relocation, mojave:         "8c46754db5932ecefafee7b4fb665a8697fe72dadf86c6262458946e64e4265c"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rswift"
+    sha256 cellar: :any_skip_relocation, mojave: "23c4405071898027c3ef894bc5be1bd28001b2bf8cb4e3e7a95b25ee50973b46"
   end
 
   depends_on :macos # needs CoreGraphics, a macOS-only library
@@ -25,6 +21,7 @@ class Rswift < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/rswift --version")
-    assert_match "[R.swift] Failed to write out", shell_output("#{bin}/rswift generate #{testpath} 2>1&")
+    expected_output="[R.swift] Missing value for `PROJECT_FILE_PATH`"
+    assert_match expected_output, shell_output("#{bin}/rswift generate #{testpath} 2>1&")
   end
 end
