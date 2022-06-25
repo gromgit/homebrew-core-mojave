efc6898958e4692727cec847b2d19bfce0ae44dd
Adrian Ho <the.gromgit@gmail.com>
2022-06-26 01:01:20 +0800

    fswatch: update 1.17.0 bottle.

--- Formula/fswatch.rb
+++ Formula/fswatch.rb
@@ -1,8 +1,8 @@
 class Fswatch < Formula
   desc "Monitor a directory for changes and run a shell command"
   homepage "https://github.com/emcrisostomo/fswatch"
-  url "https://github.com/emcrisostomo/fswatch/releases/download/1.16.0/fswatch-1.16.0.tar.gz"
-  sha256 "95ece85eb01af71e99afef0173755fcedb737b639163f8efc7fed674f6f5372f"
+  url "https://github.com/emcrisostomo/fswatch/releases/download/1.17.0/fswatch-1.17.0.tar.gz"
+  sha256 "988d9fca774eb9b2d1b3575ef56c5ce7fdcc718dbabaf727178651eac11bdabc"
   license all_of: ["GPL-3.0-or-later", "Apache-2.0"]
 
   livecheck do
@@ -11,13 +11,8 @@ class Fswatch < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "9286194b4e2df96b68c14a6eb30175b3d6846a457806f898822b611b395c97cd"
-    sha256 cellar: :any, arm64_big_sur:  "a9857b4d1cc1320e967edcc5ba5c61bd9215fac591204bc005bc89223f107228"
-    sha256 cellar: :any, monterey:       "173627c62aeb6a956a07097a34604e00504e3224c7709858be4c82a2827c343a"
-    sha256 cellar: :any, big_sur:        "f930656cf465723b282216767a932555efdfd6b75d0404cd904c52005fad53ac"
-    sha256 cellar: :any, catalina:       "a5df0020481ef351591515771abd877adc2968061ce41f4b6429f13d6ab06d30"
-    sha256 cellar: :any, mojave:         "4423be79c01f66ffea513d6dcf8758bb9c1a005f77823620c6d93ec6f0bb3da2"
-    sha256               x86_64_linux:   "d7940f62b241d4880410bf42184fb1198e60e8cebd8fd4be51ec9801f04a08f0"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fswatch"
+    sha256 mojave: "3c5ad7cf593f47b908b6bbe1bf68368bbdd68ca958440c6b21545f31c500e821"
   end
 
   def install
