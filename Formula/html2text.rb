e2eeb0b0c05c8d8c4a9e07d75af9c064cbe8756f
Adrian Ho <the.gromgit@gmail.com>
2022-06-26 01:42:30 +0800

    html2text: update 2.1.1 bottle.

--- Formula/html2text.rb
+++ Formula/html2text.rb
@@ -1,22 +1,22 @@
 class Html2text < Formula
   desc "Advanced HTML-to-text converter"
   homepage "http://www.mbayer.de/html2text/"
-  url "https://github.com/grobian/html2text/archive/v2.0.1.tar.gz"
-  sha256 "c52f16a282b69b9dc9f7b5fac7f44b15f90b74e012f0aa2d63fbf5b0fe5e5c49"
-  license "GPL-2.0"
+  url "https://github.com/grobian/html2text/archive/v2.1.1.tar.gz"
+  sha256 "be16ec8ceb25f8e7fe438bd6e525b717d5de51bd0797eeadda0617087f1563c9"
+  license "GPL-2.0-or-later"
   head "https://github.com/grobian/html2text.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/html2text"
-    sha256 cellar: :any_skip_relocation, mojave: "0352dd9cadce1e7a3ca9a2959dbfff72c47b20a3e22c64a8a093df9f0839b6c6"
+    sha256 cellar: :any_skip_relocation, mojave: "d34920ef3df30af67e6156adbfba45ed20103dba769148394819f7049673eace"
   end
 
   def install
     ENV.cxx11
 
-    system "./configure"
+    system "./configure", *std_configure_args
     system "make", "all"
-    system "make", "install", "PREFIX=#{prefix}", "MANDIR=#{man}"
+    system "make", "install", "PREFIX=#{prefix}", "BINDIR=#{bin}", "MANDIR=#{man}", "DOCDIR=#{doc}"
   end
 
   test do
