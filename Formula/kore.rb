c1e6a283aa0698a653c4dd9a80e18d2a2a3b34a3
Adrian Ho <the.gromgit@gmail.com>
2022-06-26 02:27:25 +0800

    kore: update 4.2.2_2 bottle.

--- Formula/kore.rb
+++ Formula/kore.rb
@@ -4,7 +4,7 @@ class Kore < Formula
   url "https://kore.io/releases/kore-4.2.2.tar.gz"
   sha256 "77c12d80bb76fe774b16996e6bac6d4ad950070d0816c3409dc0397dfc62725f"
   license "ISC"
-  revision 1
+  revision 2
   head "https://github.com/jorisvink/kore.git", branch: "master"
 
   livecheck do
@@ -14,7 +14,7 @@ class Kore < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kore"
-    sha256 mojave: "b364e4e2a9e8a589ce0ede096c6ad84f585fa23f3e8e9f998b7867f4dcb8895b"
+    sha256 mojave: "d07dfb5a6a7a8fb7e0e1b983d0625934eaf52b4bc658bef22a6998b3ca920276"
   end
 
   depends_on "pkg-config" => :build
@@ -24,6 +24,10 @@ class Kore < Formula
   def install
     ENV.deparallelize { system "make", "PREFIX=#{prefix}", "TASKS=1" }
     system "make", "install", "PREFIX=#{prefix}"
+
+    # Remove openssl cellar references, which breaks kore on openssl updates
+    openssl = Formula["openssl@1.1"]
+    inreplace [pkgshare/"features", pkgshare/"linker"], openssl.prefix.realpath, openssl.opt_prefix if OS.mac?
   end
 
   test do
