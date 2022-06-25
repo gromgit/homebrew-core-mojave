84442a75f14539902338772f71831f144fdf5f7c
Adrian Ho <the.gromgit@gmail.com>
2022-06-26 01:40:17 +0800

    googletest: update 1.12.0 bottle.

--- Formula/googletest.rb
+++ Formula/googletest.rb
@@ -1,18 +1,13 @@
 class Googletest < Formula
   desc "Google Testing and Mocking Framework"
   homepage "https://github.com/google/googletest"
-  url "https://github.com/google/googletest/archive/release-1.11.0.tar.gz"
-  sha256 "b4870bf121ff7795ba20d20bcdd8627b8e088f2d1dab299a031c1034eddc93d5"
+  url "https://github.com/google/googletest/archive/release-1.12.0.tar.gz"
+  sha256 "2a4f11dce6188b256f3650061525d0fe352069e5c162452818efbbf8d0b5fe1c"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5643998289368581eb4c81fd36e4cc3311ab92782db116d1f46c659d609523f8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "53a1b277842358ceae62800c9af78271a0a33043eff7f007110353941ace54d6"
-    sha256 cellar: :any_skip_relocation, monterey:       "65d52d92904a3167ae1dc2a420dd56da5c81f3c4bd394f45ab4b8fd6b620a209"
-    sha256 cellar: :any_skip_relocation, big_sur:        "7ff36ee689017d9f973acbc0407eaf8135d983028e85d1e6375a6a144c829e7e"
-    sha256 cellar: :any_skip_relocation, catalina:       "38290ffd89c69da85af50bb3e1c1e670440b59a342865a64ce50b7abb1e424aa"
-    sha256 cellar: :any_skip_relocation, mojave:         "0581f86b3dd88e39339b4cf3b6b0174109491e2d4aa97cd49369381c6611cff0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "929f3a916ff562ab210129b6e45c06f10dbdecbce115f2b55e482cda29ae50bf"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/googletest"
+    sha256 cellar: :any_skip_relocation, mojave: "b6745e76b696831f5e9ec2828118fec8f7804b72614277432660d1ea95cd7290"
   end
 
   depends_on "cmake" => :build
