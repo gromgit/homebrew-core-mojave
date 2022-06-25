1b4f588a2e0b17db245945c714f5e060de6f83e7
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-25 14:17:03 +0800

    unnethack: no rebuilding required

--- Formula/unnethack.rb
+++ Formula/unnethack.rb
@@ -18,6 +18,7 @@ class Unnethack < Formula
     sha256 catalina:       "5a1aea5f715d4c8892be4a5e76d60157da6637559a0055c41ea8024284807e91"
     sha256 mojave:         "84267cd44f073a41058516e7a8937da6b8b0f16e3500b0fd10ab0fedad77a5ce"
     sha256 high_sierra:    "47228cb416afe4d7e9ab31a2b85914e6b27f77e88340f7ef174bb2d9dd3ea2bb"
+    sha256 x86_64_linux:   "31307b80abcdcf33c36d3716969e3a2b8202d80e6ea79574f3689d21eb3faac5"
   end
 
   uses_from_macos "bison" => :build
