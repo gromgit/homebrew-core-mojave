de431829d74730d3cac96f58d5bee0d63bf962ae
Adrian Ho <aho+vm-mojave@03s.net>
2022-06-29 00:11:16 +0800

    growly:

--- Formula/growly.rb
+++ Formula/growly.rb
@@ -3,7 +3,11 @@ class Growly < Formula
   homepage "https://github.com/ryankee/growly"
   url "https://github.com/downloads/ryankee/growly/growly-v0.2.0.tar.gz"
   sha256 "3e803207aa15e3a1ee33fc388a073bd84230dce2c579295ce26b857987e78a79"
-  head "https://github.com/ryankee/growly.git"
+  head "https://github.com/ryankee/growly.git", branch: "master"
+
+  bottle do
+    sha256 mojave: "f27baf8ae2f171b8f7236ee399bb9df7da423c4ef81b68d7e0ece78df850d204" # fake mojave
+  end
 
   disable! date: "2021-06-27", because: "depends on growlnotify which has been removed"
 
