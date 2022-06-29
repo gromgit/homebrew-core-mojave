592902f48177fefdabd88f0fe31ea4cc563fa3e3
Adrian Ho <the.gromgit@gmail.com>
2022-06-29 22:05:37 +0800

    honcho: update 1.1.0 bottle.

--- Formula/honcho.rb
+++ Formula/honcho.rb
@@ -1,24 +1,23 @@
 class Honcho < Formula
+  include Language::Python::Virtualenv
+
   desc "Python clone of Foreman, for managing Procfile-based applications"
   homepage "https://github.com/nickstenning/honcho"
   url "https://files.pythonhosted.org/packages/0e/7c/c0aa47711b5ada100273cbe190b33cc12297065ce559989699fd6c1ec0cb/honcho-1.1.0.tar.gz"
   sha256 "c5eca0bded4bef6697a23aec0422fd4f6508ea3581979a3485fc4b89357eb2a9"
   license "MIT"
-  head "https://github.com/nickstenning/honcho.git"
+  head "https://github.com/nickstenning/honcho.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "3dada63778e9ed2ba5ea8afb8a0946f886f378539b5ee07164016026452a4ecf"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/honcho"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "024ad19cd56b028052f691ddd888eb509f3ab986bca27449a0af7d135fae3e83"
   end
 
   depends_on "python@3.10"
 
   def install
-    xy = Language::Python.major_minor_version "python3"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
-    system "python3", *Language::Python.setup_install_args(libexec)
-
-    bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
+    virtualenv_install_with_resources
   end
 
   test do
