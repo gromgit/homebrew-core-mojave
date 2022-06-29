7def2b24e7933833f90e24eff46f5fa547a2cbf7
Adrian Ho <the.gromgit@gmail.com>
2022-06-29 22:48:15 +0800

    janet: update 1.23.0 bottle.

--- Formula/janet.rb
+++ Formula/janet.rb
@@ -1,18 +1,23 @@
 class Janet < Formula
   desc "Dynamic language and bytecode vm"
   homepage "https://janet-lang.org"
-  url "https://github.com/janet-lang/janet/archive/v1.22.0.tar.gz"
-  sha256 "7c6969f8e82badc7afa28aa1054555c1c91d2858f9f45c41a82557f5c5ce85bd"
+  url "https://github.com/janet-lang/janet/archive/v1.23.0.tar.gz"
+  sha256 "0b4d5d3632e0d376d9512ea8ea262f31f75c132b488dd7870f472acae709a865"
   license "MIT"
   head "https://github.com/janet-lang/janet.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/janet"
-    sha256 cellar: :any, mojave: "331b9b15bd4df265061efb310b0f815b6afd56a6d0454815d33945deac2ece6b"
+    sha256 cellar: :any, mojave: "a99209f777edebaec15d2153032b6f0fda3e246e480a49b8dbebc5b6238d17aa"
   end
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
+
+  resource "jpm" do
+    url "https://github.com/janet-lang/jpm/archive/refs/tags/v1.0.0.tar.gz"
+    sha256 "858d4ef2f6ac78222c53154dd91f8fb5994e3c3cbe253c9b0d3b9d52557eeb9b"
+  end
 
   def install
     system "meson", "setup", "build", *std_meson_args
@@ -20,9 +25,16 @@ class Janet < Formula
       system "ninja"
       system "ninja", "install"
     end
+    ENV["PREFIX"] = prefix
+    resource("jpm").stage do
+      system bin/"janet", "bootstrap.janet"
+    end
   end
 
   test do
     assert_equal "12", shell_output("#{bin}/janet -e '(print (+ 5 7))'").strip
+    assert_predicate HOMEBREW_PREFIX/"bin/jpm", :exist?, "jpm must exist"
+    assert_predicate HOMEBREW_PREFIX/"bin/jpm", :executable?, "jpm must be executable"
+    assert_match prefix.to_s, shell_output("#{bin}/jpm show-paths")
   end
 end
