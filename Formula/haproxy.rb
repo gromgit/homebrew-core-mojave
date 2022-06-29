9761155e54b5fb408ff6fe1eccfa2d9906dac756
Adrian Ho <the.gromgit@gmail.com>
2022-06-29 21:47:00 +0800

    haproxy: update 2.6.1 bottle.

--- Formula/haproxy.rb
+++ Formula/haproxy.rb
@@ -1,8 +1,8 @@
 class Haproxy < Formula
   desc "Reliable, high performance TCP/HTTP load balancer"
   homepage "https://www.haproxy.org/"
-  url "https://www.haproxy.org/download/2.5/src/haproxy-2.5.7.tar.gz"
-  sha256 "e29f6334c6bdb521f63ddf335e2621bd2164503b99cf1f495b6f56ff9f3c164e"
+  url "https://www.haproxy.org/download/2.6/src/haproxy-2.6.1.tar.gz"
+  sha256 "915b351e6450d183342c4cdcda7771eac4f0f72bf90582adcd15a01c700d29b1"
   license "GPL-2.0-or-later" => { with: "openvpn-openssl-exception" }
 
   livecheck do
@@ -12,12 +12,13 @@ class Haproxy < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/haproxy"
-    sha256 cellar: :any, mojave: "a0e03c2502a93eb094e1dea993c532c2ab1db08dcf30f43d11163b4ac257298e"
+    sha256 cellar: :any, mojave: "2059c95f63b25042b1082738819f00dbd549babe88480ab2a9baa0f56b5d0b19"
   end
 
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   depends_on "pcre2"
 
+  uses_from_macos "libxcrypt"
   uses_from_macos "zlib"
 
   def install
