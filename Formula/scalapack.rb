393dc3d9b71451517fd4257c08c95869fbda13c4
Adrian Ho <the.gromgit@gmail.com>
2022-06-30 08:41:55 +0800

    scalapack: update 2.2.0 bottle.

--- Formula/scalapack.rb
+++ Formula/scalapack.rb
@@ -1,10 +1,9 @@
 class Scalapack < Formula
   desc "High-performance linear algebra for distributed memory machines"
   homepage "https://www.netlib.org/scalapack/"
-  url "https://www.netlib.org/scalapack/scalapack-2.1.0.tgz"
-  sha256 "61d9216cf81d246944720cfce96255878a3f85dec13b9351f1fa0fd6768220a6"
+  url "https://www.netlib.org/scalapack/scalapack-2.2.0.tgz"
+  sha256 "40b9406c20735a9a3009d863318cb8d3e496fb073d201c5463df810e01ab2a57"
   license "BSD-3-Clause"
-  revision 3
 
   livecheck do
     url :homepage
@@ -12,13 +11,8 @@ class Scalapack < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "06b0a56f9c0fbe86a1345236839d83364bd296127690fdb702b9fe7aca2f3514"
-    sha256                               arm64_big_sur:  "ae872ee54a2f85ef4b8a3e5370751db4d3dd931c10d3222355fa523389592c34"
-    sha256 cellar: :any,                 monterey:       "e387af6b56b3a5a5466521c1e75f425903c35c7ad5c37c830be637997f10a36c"
-    sha256 cellar: :any,                 big_sur:        "5d33d9c7a1f92b2a30487b6218d0fc248905f7114275fe83e661411343400ada"
-    sha256 cellar: :any,                 catalina:       "0919c7e1f584fb690ce4d8e395e4b98c21d85858581eef10f1c73612216f863a"
-    sha256 cellar: :any,                 mojave:         "ff1f14376cb734a26a0d5580d0e58e7107c33def1bcda522ac7af3dfcd129f30"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bf7a2df188247f7bd79e779b2a2fa535943082c352bed86c9b454c99d3236c76"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/scalapack"
+    sha256 cellar: :any, mojave: "d99ca2aa968f724f300ab86be83f1957c2b9881b5c03266ef7ab4baaa5a9c6ab"
   end
 
   depends_on "cmake" => :build
@@ -26,12 +20,13 @@ class Scalapack < Formula
   depends_on "open-mpi"
   depends_on "openblas"
 
-  # Patch for compatibility with GCC 10
-  # https://github.com/Reference-ScaLAPACK/scalapack/pull/26
+  # Apply upstream commit to fix build with gfortran-12.  Remove in next release.
   patch do
-    url "https://github.com/Reference-ScaLAPACK/scalapack/commit/bc6cad585362aa58e05186bb85d4b619080c45a9.patch?full_index=1"
-    sha256 "f0892888e5a83d984e023e76eabae8864ad89b90ae3a41d472b960c95fdab981"
+    url "https://github.com/Reference-ScaLAPACK/scalapack/commit/a0f76fc0c1c16646875b454b7d6f8d9d17726b5a.patch?full_index=1"
+    sha256 "2b42d282a02b3e56bb9b3178e6279dc29fc8a17b9c42c0f54857109286a9461e"
   end
+
+  patch :DATA
 
   def install
     mkdir "build" do
@@ -59,3 +54,18 @@ class Scalapack < Formula
     end
   end
 end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index 85ea82a..86222e0 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -232,7 +232,7 @@ append_subdir_files(src-C "SRC")
+ 
+ if (UNIX)
+    add_library(scalapack ${blacs} ${tools} ${tools-C} ${extra_lapack} ${pblas} ${pblas-F} ${ptzblas} ${ptools} ${pbblas} ${redist} ${src} ${src-C})
+-   target_link_libraries( scalapack ${LAPACK_LIBRARIES} ${BLAS_LIBRARIES})
++   target_link_libraries( scalapack ${LAPACK_LIBRARIES} ${BLAS_LIBRARIES} ${MPI_Fortran_LIBRARIES})
+    scalapack_install_library(scalapack)
+ else (UNIX) # Need to separate Fortran and C Code
+    OPTION(BUILD_SHARED_LIBS "Build shared libraries" ON )
