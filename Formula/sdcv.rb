dcb6629fc6d89f126a1d56d6bded68f14320e44a
Adrian Ho <the.gromgit@gmail.com>
2022-06-30 08:46:26 +0800

    sdcv: update 0.5.4 bottle.

--- Formula/sdcv.rb
+++ Formula/sdcv.rb
@@ -1,21 +1,15 @@
 class Sdcv < Formula
   desc "StarDict Console Version"
   homepage "https://dushistov.github.io/sdcv/"
-  url "https://github.com/Dushistov/sdcv/archive/v0.5.3.tar.gz"
-  sha256 "75fb95b1607fdd2fb9f7795d3432d295904614150575ae539202f680499803c9"
+  url "https://github.com/Dushistov/sdcv/archive/v0.5.4.tar.gz"
+  sha256 "9fddec393f5dd6b208991d8225f90cb14d50fa9e7735f2414035d8a2ca065f28"
   license "GPL-2.0-or-later"
   version_scheme 1
   head "https://github.com/Dushistov/sdcv.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "b250877285b24e95df9b86883bc09faece5236a4708efa93493a0434c049ac57"
-    sha256 arm64_big_sur:  "201982a36d116f80314330dc00ac78c98cf4e7cfe8803addecf8f5fad2ec15d2"
-    sha256 monterey:       "a0db3441f9d2f128d419cd7b9e2be0c30a0b0e476a76f0ed388fe486db5090d2"
-    sha256 big_sur:        "15fa01158f88b31fb8f6d18e8e8e3c76492c0693fefee8245f25ac3913041bb0"
-    sha256 catalina:       "4831ddd61d8b9e9b7024cfc898d4e7e4c89207276ac2a4bc5b22911294d3e8b6"
-    sha256 mojave:         "98f5f41515dc5b1dbf8ca9bb7f47990c9c5852a8f5e56a93da303fcb024ef411"
-    sha256 high_sierra:    "9e95490034c0b964cf617e05361ac7128a5e5e886ea38dcc4f14b64468c328ac"
-    sha256 x86_64_linux:   "21ba061b7f3f91eca4a326a66730dc88c846dc0e373f581b7c0052674a9d9c4e"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sdcv"
+    sha256 mojave: "8e1b0a204aa8826664c6370de6d8f6cc0a17d4ef138e3f747b94c62d2fc93996"
   end
 
   depends_on "cmake" => :build
