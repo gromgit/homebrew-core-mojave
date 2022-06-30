5c86686b580e5a1432fe7e67fa849b7c4a762d3d
Adrian Ho <the.gromgit@gmail.com>
2022-06-30 08:20:58 +0800

    redis: update 7.0.2 bottle.

--- Formula/redis.rb
+++ Formula/redis.rb
@@ -1,8 +1,8 @@
 class Redis < Formula
   desc "Persistent key-value database, with built-in net interface"
   homepage "https://redis.io/"
-  url "https://download.redis.io/releases/redis-7.0.0.tar.gz"
-  sha256 "284d8bd1fd85d6a55a05ee4e7c31c31977ad56cbf344ed83790beeb148baa720"
+  url "https://download.redis.io/releases/redis-7.0.2.tar.gz"
+  sha256 "5e57eafe7d4ac5ecb6a7d64d6b61db775616dbf903293b3fcc660716dbda5eeb"
   license "BSD-3-Clause"
   head "https://github.com/redis/redis.git", branch: "unstable"
 
@@ -13,7 +13,7 @@ class Redis < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/redis"
-    sha256 cellar: :any, mojave: "1b286304b37ef494ec7a1aa1d29a264e3a68897d1df8aa125f99bdfc4d7a8449"
+    sha256 cellar: :any, mojave: "8d704d4e1e7e3ad78c4d8f67d340cc231355cd12e05f97f2da8d0d53c6d81585"
   end
 
   depends_on "openssl@1.1"
