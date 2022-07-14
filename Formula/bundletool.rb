ddfcf05eeafdb79b6a9e1f498694f9430294a1c6
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:43:13 +0800

    bundletool: no rebuilding required

--- Formula/bundletool.rb
+++ Formula/bundletool.rb
@@ -1,12 +1,12 @@
 class Bundletool < Formula
   desc "Command-line tool to manipulate Android App Bundles"
   homepage "https://github.com/google/bundletool"
-  url "https://github.com/google/bundletool/releases/download/1.10.0/bundletool-all-1.10.0.jar"
-  sha256 "a5eee8fd22628c3f45662492498d3da38faf2bf2c64d425911492a5db6bc081c"
+  url "https://github.com/google/bundletool/releases/download/1.11.0/bundletool-all-1.11.0.jar"
+  sha256 "c42c365ae7369dd4dc2ebc11eeebf91459f0226c53706fd24de4d006268ab88c"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "be08418364dd3284b1bbf52f867303121c5b02bc7aec69d17e846b4437c6c080"
+    sha256 cellar: :any_skip_relocation, all: "0fb79f18b633c9b225119733ca0632c9590170b25fdd460cb5ffd32e6b4e56ab"
   end
 
   depends_on "openjdk"
