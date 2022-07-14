0366d8abb3c9b410381f3680d3afc7d72685ada0
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:47:12 +0800

    t1lib: no rebuilding required

--- Formula/t1lib.rb
+++ Formula/t1lib.rb
@@ -17,7 +17,6 @@ class T1lib < Formula
     sha256                               high_sierra:    "a36bc3913f6b51cb7772609a52049f90fc6241ffca3bf18c4295455ef5f4df4c"
     sha256                               sierra:         "94789287c849a04f05a40c79940aee6efe3e03318c95db9c2be69ee4e6806d82"
     sha256                               el_capitan:     "fa356a5405f5b0cf57c15ebd5b680c215e1e498189914e9b9663eb132655a8c1"
-    sha256                               yosemite:       "6d1bf242eb7a5201180b9d4b505a7f83663802383d358180cea696714ae57fc8"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "51b691773063f4f7c0e6cbfa5be259516c77ad7b1c52bb189d045b8056216bf9"
   end
 
