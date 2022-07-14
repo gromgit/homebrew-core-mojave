7a0b2d0b96a0f17cfbd9881991d9bc9ecd91bcd7
Adrian Ho <the.gromgit@gmail.com>
2022-07-15 01:08:19 +0800

    mpg123: update 1.30.1 bottle.

--- Formula/mpg123.rb
+++ Formula/mpg123.rb
@@ -1,9 +1,9 @@
 class Mpg123 < Formula
   desc "MP3 player for Linux and UNIX"
   homepage "https://www.mpg123.de/"
-  url "https://www.mpg123.de/download/mpg123-1.30.0.tar.bz2"
-  mirror "https://downloads.sourceforge.net/project/mpg123/mpg123/1.30.0/mpg123-1.30.0.tar.bz2"
-  sha256 "397ead52f0299475f2cefd38c3835977193fd9b1db6593680346c4e9109ed61c"
+  url "https://www.mpg123.de/download/mpg123-1.30.1.tar.bz2"
+  mirror "https://downloads.sourceforge.net/project/mpg123/mpg123/1.30.1/mpg123-1.30.1.tar.bz2"
+  sha256 "1b20c9c751bea9be556749bd7f97cf580f52ed11f2540756e9af26ae036e4c59"
   license "LGPL-2.1-only"
 
   livecheck do
@@ -13,7 +13,7 @@ class Mpg123 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mpg123"
-    sha256 mojave: "bae3c86f1b9c5328215ab8d6328096c541fe1262f8d1844167880d5cf9d6db89"
+    sha256 mojave: "9fe0d99cc30cc8d994405c176daf529a2822e309ea7ec1b8a37c64fd9dcc0451"
   end
 
   def install
