a03f7df568493e06e04ff7d1603bc96c706f7688
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:47:09 +0800

    style-check: no rebuilding required

--- Formula/style-check.rb
+++ Formula/style-check.rb
@@ -22,7 +22,6 @@ class StyleCheck < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "3fdd513cff0a9f298357e5e918532bc2c236cbb6bf4c981a77270c69b49b9f08"
     sha256 cellar: :any_skip_relocation, sierra:         "1a6787ed0c5cbe6ee567e112b16d5d32193bb0dacbf23e515f512df3287a3abd"
     sha256 cellar: :any_skip_relocation, el_capitan:     "8ad833cef9ca35a1ae95569f93c8936a82c6d6039e3bd4560e2b1dc93fd029d8"
-    sha256 cellar: :any_skip_relocation, yosemite:       "08759f3a407af4dab0310f4a02ee18f15f2104011d702a300adbe9d33bcde0f5"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "abef781d8bcc9e2070e039fe4adfb5f56d5fb428af473439628ec98fe87b788d"
   end
 
