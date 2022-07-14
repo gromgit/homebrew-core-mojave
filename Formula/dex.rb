c3978240a846bf3d5bb6a09dda8d434fb7d0e286
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:43:45 +0800

    dex: no rebuilding required

--- Formula/dex.rb
+++ Formula/dex.rb
@@ -17,7 +17,6 @@ class Dex < Formula
     sha256 high_sierra:    "1d36402b9470f2e714bf9b9b94e9575d06130485559826a08181ff9087e77176"
     sha256 sierra:         "1e413a64cd9e2c594ec47c7e5e9ff36ab199126f6708265f5ad87363e66f033e"
     sha256 el_capitan:     "70c249809920acc2d10405c0487427d154ee55cf201507d910d8178693c7fd61"
-    sha256 yosemite:       "a4cffc5c0b61be9452988d4435ccff1d1c72d2b9cdec595e55ea5f37ca2541a6"
     sha256 x86_64_linux:   "53f45193c090faaeefd2c6ca8a492f51af29d6b72f7a13eacb9650b6fffd46c5"
   end
 
