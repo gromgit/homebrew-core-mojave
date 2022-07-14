1e7cefa4728b01a740c5b94588169a5de9ad278d
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:43:47 +0800

    dhcping: no rebuilding required

--- Formula/dhcping.rb
+++ Formula/dhcping.rb
@@ -20,7 +20,6 @@ class Dhcping < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "e30ef14d867a06bcc9bcde18965fa00366780c3323841ca0fb25f864077044d6"
     sha256 cellar: :any_skip_relocation, sierra:         "5c41d596cb2a9835fc5f170ccd602294c98f163ba3f2a8d5c83bae252189817e"
     sha256 cellar: :any_skip_relocation, el_capitan:     "d3b03b1004d3a2d97b80fbbe9714bd29d006d9099a8f6baec343feb2833f3996"
-    sha256 cellar: :any_skip_relocation, yosemite:       "7741adb9bc166ee2450e521f7468e2b023632e737eb4da065848c5e87b6bd35a"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "dad6d3c832c13d4199c87f8afb9bd93641b7145990edd3d0747612d3546b70cc"
   end
 
