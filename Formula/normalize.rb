13961036d6ac3b3ecd5cc97101628a28ec1411d7
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:46:02 +0800

    normalize: no rebuilding required

--- Formula/normalize.rb
+++ Formula/normalize.rb
@@ -21,7 +21,6 @@ class Normalize < Formula
     sha256 cellar: :any, high_sierra:    "e4dd195c639807e3e2e25fee6c5c6f3c4263a37a4c7f8f25ab656a96395faeaf"
     sha256 cellar: :any, sierra:         "1165de2721e8b4d7f373b9ad10f52c2cd49c44a24cd8fddab5ba51983164cefe"
     sha256 cellar: :any, el_capitan:     "052ab2e8b1f6a2aa1e634a30749612d927b5cee5cc9302e057bd02c599a1c256"
-    sha256 cellar: :any, yosemite:       "dcb42f107b9674e50d8994215f6d125e0fb9523b1d99b393fd00ee2b827be5e0"
     sha256               x86_64_linux:   "ac6f58e169897ddfc61943009dd38044958a8fc56a025e91632d2302de3063df"
   end
 
