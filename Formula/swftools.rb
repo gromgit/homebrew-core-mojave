7ff46f3987671da6a5422d1cbc8074f728aa07ef
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:47:11 +0800

    swftools: no rebuilding required

--- Formula/swftools.rb
+++ Formula/swftools.rb
@@ -19,12 +19,18 @@ class Swftools < Formula
     sha256 catalina:       "b0791e6725e6d07610847df7e4431e5839fcf72120cea34f1890b425f8e024c4"
     sha256 mojave:         "bf18bfc66b1f6d6ed247acd0a4208a09b4acf6a4668e8f7eba2e40ad33ffe9f6"
     sha256 high_sierra:    "d0e441ed7eef07c3536965d5269f648744ceb62d41fbcfe9a12248b8154c4f62"
+    sha256 x86_64_linux:   "0ed51b95634f090cb753b57fdd73df90a944cc37fc2c34de45592d74c8b74139"
   end
+
+  uses_from_macos "zlib"
 
   # Fixes a conftest for libfftwf.dylib that mistakenly calls fftw_malloc()
   # rather than fftwf_malloc().  Reported upstream to their mailing list:
   # https://lists.nongnu.org/archive/html/swftools-common/2012-04/msg00014.html
   # Patch is merged upstream.  Remove at swftools-0.9.3.
+  # Also fix build on Linux by using correct flags for rm.
+  # Linux fix sent to swftools mailing list:
+  # https://lists.nongnu.org/archive/html/swftools-common/2022-06/msg00000.html
   patch :DATA
 
   def install
@@ -50,3 +56,19 @@ __END__
      	fftwf_plan plan = fftwf_plan_dft_2d(600, 800, (fftwf_complex*)data, (fftwf_complex*)data, FFTW_FORWARD, FFTW_ESTIMATE);
  	plan = fftwf_plan_dft_r2c_2d(600, 800, (float*)data, (fftwf_complex*)data, FFTW_ESTIMATE);
  	plan = fftwf_plan_dft_c2r_2d(600, 800, (fftwf_complex*)data, (float*)data, FFTW_ESTIMATE);
+diff --git a/swfs/Makefile.in b/swfs/Makefile.in
+index d7bc400..890b9bd 100644
+--- a/swfs/Makefile.in
++++ b/swfs/Makefile.in
+@@ -41,9 +41,9 @@ install:
+ 	$(INSTALL_DATA) ./PreLoaderTemplate.swf $(pkgdatadir)/swfs/PreLoaderTemplate.swf
+ 	$(INSTALL_DATA) ./tessel_loader.swf $(pkgdatadir)/swfs/tessel_loader.swf
+ 	$(INSTALL_DATA) ./swft_loader.swf $(pkgdatadir)/swfs/swft_loader.swf
+-	rm -f $(pkgdatadir)/swfs/default_viewer.swf -o -L $(pkgdatadir)/swfs/default_viewer.swf
++	rm -f $(pkgdatadir)/swfs/default_viewer.swf
+ 	$(LN_S) $(pkgdatadir)/swfs/simple_viewer.swf $(pkgdatadir)/swfs/default_viewer.swf
+-	rm -f $(pkgdatadir)/swfs/default_loader.swf -o -L $(pkgdatadir)/swfs/default_loader.swf
++	rm -f $(pkgdatadir)/swfs/default_loader.swf
+ 	$(LN_S) $(pkgdatadir)/swfs/tessel_loader.swf $(pkgdatadir)/swfs/default_loader.swf
+ 		
+ uninstall:
