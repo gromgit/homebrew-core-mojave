0afaec822a3820a1a473c1779b6c95c24ac6f050
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:43:02 +0800

    aterm: no rebuilding required

--- Formula/aterm.rb
+++ Formula/aterm.rb
@@ -16,7 +16,6 @@ class Aterm < Formula
     sha256 cellar: :any, high_sierra:    "f56a13be464fa577fdad7fe82779f5e6bbe820995e1849b6741ca92807c10bf0"
     sha256 cellar: :any, sierra:         "dd7b81b3bd9a31746ab461b8d79e4c32838b7e86f540769e4c17825a4b89c1c2"
     sha256 cellar: :any, el_capitan:     "5140e20287eda941f8756dfdaf377663f84f6872d1ca3f6d70e04b554591d11a"
-    sha256 cellar: :any, yosemite:       "d12bebbfa2e764abb9cfac1aecd6fc04e58f83eadf0fb3db298d5be03d7f8dca"
   end
 
   deprecate! date: "2021-11-03", because: :unmaintained
