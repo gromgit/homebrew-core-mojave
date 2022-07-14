8921bfdde592926a0d13137846e8fe8079568d5f
Adrian Ho <the.gromgit@gmail.com>
2022-07-15 00:21:40 +0800

    coq: update 8.15.2 bottle.

--- Formula/coq.rb
+++ Formula/coq.rb
@@ -1,8 +1,8 @@
 class Coq < Formula
   desc "Proof assistant for higher-order logic"
   homepage "https://coq.inria.fr/"
-  url "https://github.com/coq/coq/archive/V8.15.0.tar.gz"
-  sha256 "73466e61f229b23b4daffdd964be72bd7a110963b9d84bd4a86bb05c5dc19ef3"
+  url "https://github.com/coq/coq/archive/V8.15.2.tar.gz"
+  sha256 "13a67c0a4559ae22e9765c8fdb88957b16c2b335a2d5f47e4d6d9b4b8b299926"
   license "LGPL-2.1-only"
   head "https://github.com/coq/coq.git", branch: "master"
 
@@ -13,11 +13,12 @@ class Coq < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/coq"
-    sha256 mojave: "e2918cb23bb6ee08c9f1e40c4a3bb6abc2a668573afe9cc20dc0699f258e37f4"
+    sha256 mojave: "437b0809553385f20e37d4c171333ce1d75dd8150acab7f2ea68c52b147d0717"
   end
 
   depends_on "dune" => :build
   depends_on "ocaml-findlib" => :build
+  depends_on "gmp"
   depends_on "ocaml"
   depends_on "ocaml-zarith"
 
@@ -25,9 +26,10 @@ class Coq < Formula
   uses_from_macos "unzip" => :build
 
   def install
+    ENV.prepend_path "OCAMLPATH", Formula["ocaml-zarith"].opt_lib/"ocaml"
     system "./configure", "-prefix", prefix,
                           "-mandir", man,
-                          "-docdir", "#{pkgshare}/latex",
+                          "-docdir", pkgshare/"latex",
                           "-coqide", "no",
                           "-with-doc", "no"
     system "make", "world"
@@ -60,6 +62,6 @@ class Coq < Formula
       intros; lia.
       Qed.
     EOS
-    system("#{bin}/coqc", "#{testpath}/testing.v")
+    system bin/"coqc", testpath/"testing.v"
   end
 end
