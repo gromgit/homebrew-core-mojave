e77158cd4cae20e3f7f10ae89019645e46fadc80
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:44:18 +0800

    garmintools: no rebuilding required

--- Formula/garmintools.rb
+++ Formula/garmintools.rb
@@ -15,7 +15,6 @@ class Garmintools < Formula
     sha256 cellar: :any,                 high_sierra:     "9ecdb8294089c84a229db39a395bf3f4817f185f30135a6f92711b95705ab869"
     sha256 cellar: :any,                 sierra:          "c747a668400406f6625a3832e351a4f27fd1308d8ef840120eba086d3d6adcb4"
     sha256 cellar: :any,                 el_capitan:      "dd86a8e306d3c4ebb9b94ddd4aaf60fdb79aa06fc7eb56ca95942248db33924e"
-    sha256 cellar: :any,                 x86_64_yosemite: "62d2b45ae3d7ef7de9a8deaa658e12021f16b14008f1a91e8c747f84b0e803d3"
     sha256 cellar: :any_skip_relocation, x86_64_linux:    "230b31dd83d9514c35abdb2477e4f1e4941eb1c94bf394814acd46676ced1318"
   end
 
