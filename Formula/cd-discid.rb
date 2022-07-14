36980c5b0a087fe95b437a3b173ae4038e549927
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:43:17 +0800

    cd-discid: no rebuilding required

--- Formula/cd-discid.rb
+++ Formula/cd-discid.rb
@@ -33,7 +33,6 @@ class CdDiscid < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "26b88be0312f960484625161d94adf9a44aa88ef5817ba28b61af520a6e17e03"
     sha256 cellar: :any_skip_relocation, sierra:         "6b0d9c55a1adfce8a2c6e9eabd00c37118a05b60678564e7a9695d876bca117b"
     sha256 cellar: :any_skip_relocation, el_capitan:     "f0c17cfc3c345c661104a6f29562b766cac2a80747feea0c26cda04ece3c8326"
-    sha256 cellar: :any_skip_relocation, yosemite:       "3331be095997a1e5e6acb9f82f5e5473ed51c0f35976229371dc1d0c703c2e3b"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c6e37cc61545d58bebb66ffffada804ca5e39e47e503684c7ed84cfa856dbb14"
   end
 
