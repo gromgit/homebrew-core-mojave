82e42627888cf2ec65f914e442b3cd20196c134a
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:46:59 +0800

    shivavg: no rebuilding required

--- Formula/shivavg.rb
+++ Formula/shivavg.rb
@@ -15,7 +15,6 @@ class Shivavg < Formula
     sha256 cellar: :any,                 high_sierra:     "bea07d86639a8d24f90324552ed1880fd6a162141a394338e0ad2a81a3abeb5f"
     sha256 cellar: :any,                 sierra:          "f92bdb7b86632d7bf59d25259e26eece00e502759dd52adaac7495424290da4a"
     sha256 cellar: :any,                 el_capitan:      "3e9de2887110c90051ad5b89080f62cd5990ae39f8fdef02a4c50ba11e413ca8"
-    sha256 cellar: :any,                 x86_64_yosemite: "f3de3b35fcfeff41c1836bc7722579d1d4b461e10d4152802bae1ab48b5a3bbb"
     sha256 cellar: :any_skip_relocation, x86_64_linux:    "ae5c74db54f64e3a75e394cba457cd969612c741de20f12b5941af967db03470"
   end
 
