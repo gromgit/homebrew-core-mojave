d8a3dd44e1855b4735a13a61eb2e5f612f23c6f2
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:46:41 +0800

    rdate: no rebuilding required

--- Formula/rdate.rb
+++ Formula/rdate.rb
@@ -21,7 +21,6 @@ class Rdate < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "02e41a79e9aca3bad86802e1bc32c7148e8a2ea2f410c57765f9e9d8b2686fd1"
     sha256 cellar: :any_skip_relocation, sierra:         "9f4a6300d6d3ebc9034abeb5388fd40face1f286a7b97610b6a40a1dcdf166b5"
     sha256 cellar: :any_skip_relocation, el_capitan:     "acb2ae5951a0f32cbdce39e02d86c63cdb85b41fd02aff74aac6ea4939d71d8d"
-    sha256 cellar: :any_skip_relocation, yosemite:       "553782017635be9c8d80bbf6fd033f294cddcb427a2d83fe82af8c069c60867f"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "ec0afe4b9ddec866160a04e76ce20f253c90a716c6a90c66c8185db9a32f5070"
   end
 
