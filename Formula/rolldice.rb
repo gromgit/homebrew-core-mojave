14c12050bbe62ab4f5eae144da4897a73f3ed427
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:46:47 +0800

    rolldice: no rebuilding required

--- Formula/rolldice.rb
+++ Formula/rolldice.rb
@@ -15,7 +15,6 @@ class Rolldice < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "74364058c7f8859e71b5b43b80b40c01dd99ce6b80724ef4e97f9a9ea0587775"
     sha256 cellar: :any_skip_relocation, sierra:         "a7019dfc0a37c4cb814f8d116140b9fac999d6d97e6650e0806c02cb633087fb"
     sha256 cellar: :any_skip_relocation, el_capitan:     "3ee6afe89723d119075feffe735f4b4d4552d51bab5d79df6b8e100f90d21109"
-    sha256 cellar: :any_skip_relocation, yosemite:       "9525132a3c9a1b1ac679102a2a2f39e51dcd1f2ae299a1038701bdf4f945bd4c"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c528a9c75ae75ef5bf9c28db1b40cd8e30fee54029580bbd05c7b5cbc8449936"
   end
 
