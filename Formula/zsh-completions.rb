e0c3f8e582d4ff57c9f7f9577bdb38c84dd910a8
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:47:56 +0800

    zsh-completions: no rebuilding required

--- Formula/zsh-completions.rb
+++ Formula/zsh-completions.rb
@@ -1,15 +1,16 @@
 class ZshCompletions < Formula
   desc "Additional completion definitions for zsh"
   homepage "https://github.com/zsh-users/zsh-completions"
-  url "https://github.com/zsh-users/zsh-completions/archive/0.33.0.tar.gz"
-  sha256 "39452d383d0718aa2c830edba1aa32f0ee1e40002ef6932d88699a888bd58c29"
+  url "https://github.com/zsh-users/zsh-completions/archive/0.34.0.tar.gz"
+  sha256 "21b6c194b15ae3992f4c2340ab249aa326a9874d46e3130bb3f292142c217fe2"
   license "MIT-Modern-Variant"
-  revision 1
   head "https://github.com/zsh-users/zsh-completions.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "eab228532a15b1c8eddfd38f46306cf723e6f331d38c8874d0305d6fe888ab70"
+    sha256 cellar: :any_skip_relocation, all: "513586906b397bfcd70897487fe13b8104188ecd7de04c39da9dfbacbc78ecc6"
   end
+
+  uses_from_macos "zsh" => :test
 
   def install
     inreplace "src/_ghc", "/usr/local", HOMEBREW_PREFIX
@@ -44,6 +45,6 @@ class ZshCompletions < Formula
       autoload _ack
       which _ack
     EOS
-    assert_match(/^_ack/, shell_output("/bin/zsh test.zsh"))
+    assert_match(/^_ack/, shell_output("zsh test.zsh"))
   end
 end
