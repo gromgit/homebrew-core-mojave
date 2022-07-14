6f83c1b17438700a6039270f983ec6675588060a
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:45:53 +0800

    msdl: no rebuilding required

--- Formula/msdl.rb
+++ Formula/msdl.rb
@@ -21,7 +21,6 @@ class Msdl < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "5f2922fa4f3b69f3f00cb7e29854c5a43c163e209c87d961253da9c4a7c3ec73"
     sha256 cellar: :any_skip_relocation, sierra:         "69b04b6f10ea552b6c862110434cc63dfa6bfccdc8034edd70fed5db0f79e68b"
     sha256 cellar: :any_skip_relocation, el_capitan:     "34ba320e82d1ce97fb0a106abd2c5ec848ba16857730ba51cadd0a030bee62ab"
-    sha256 cellar: :any_skip_relocation, yosemite:       "5b8ac26e3adbb19386398a5500a8d5631d426b2e0e951433134b5383b80bb568"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "5664cc49f99975d426fab1e8518356d8842512ab773aa4c2a3abe0fb957d1881"
   end
 
