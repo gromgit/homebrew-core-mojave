1dd39b8ccede7f61c7a374770461afea51617933
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:45:16 +0800

    libpcl: no rebuilding required

--- Formula/libpcl.rb
+++ Formula/libpcl.rb
@@ -1,9 +1,9 @@
 class Libpcl < Formula
   desc "C library and API for coroutines"
-  homepage "http://xmailserver.org/libpcl.html"
-  url "http://xmailserver.org/pcl-1.12.tar.gz"
+  homepage "http://www.xmailserver.org/libpcl.html"
+  url "http://www.xmailserver.org/pcl-1.12.tar.gz"
   sha256 "e7b30546765011575d54ae6b44f9d52f138f5809221270c815d2478273319e1a"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
 
   livecheck do
     url :homepage
@@ -19,7 +19,6 @@ class Libpcl < Formula
     sha256 cellar: :any,                 high_sierra:  "2d7ce1c2a11e762dacf0e28f92a1b1f6b6a45ea4564ac579b4c0683c61ac61f7"
     sha256 cellar: :any,                 sierra:       "525c0925d7d3234cf5da86a892d15aa4f6d4417f302ed821e2bfd6e7cb06ef43"
     sha256 cellar: :any,                 el_capitan:   "1975baf018352fd1f1ca88bd39fc02db384e2f6be4017976184dda3365c60608"
-    sha256 cellar: :any,                 yosemite:     "e9c6f7bc1efab583e44879426a5abb2ff5e7f3eb30261a81a7be723c3280c3a3"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "4477cd796405dfbf475dc7f65339aac5bba8fdf4d9027724c479edaf6f3f2553"
   end
 
