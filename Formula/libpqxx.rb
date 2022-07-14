530d36cc3ed960bf932ccf1c419c34020ff9de4c
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:45:17 +0800

    libpqxx:

--- Formula/libpqxx.rb
+++ Formula/libpqxx.rb
@@ -1,17 +1,17 @@
 class Libpqxx < Formula
   desc "C++ connector for PostgreSQL"
   homepage "http://pqxx.org/development/libpqxx/"
-  url "https://github.com/jtv/libpqxx/archive/7.7.3.tar.gz"
-  sha256 "11e147bbe2d3024d68d29b38eab5d75899dbb6131e421a2dbf9f88bac9bf4b0d"
+  url "https://github.com/jtv/libpqxx/archive/7.7.4.tar.gz"
+  sha256 "65b0a06fffd565a19edacedada1dcfa0c1ecd782cead0ee067b19e2464875c36"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "735b7df4d11a673461099d606727b595d491ff1f73f441bd5cfdceced9706100"
-    sha256 cellar: :any,                 arm64_big_sur:  "42e1a8488b64bdd59fe427c9dba9667b0f23bf4fad08d937fab81482064a8af9"
-    sha256 cellar: :any,                 monterey:       "860504675d2b564be0b14e3240d22d65483d39bd1213ec3e2d4ebea7cc55dd16"
-    sha256 cellar: :any,                 big_sur:        "efefcdbac601d3c83b3cd646e7a0fe2fd33ea4b91b7f101b8c07fbc6e0a16916"
-    sha256 cellar: :any,                 catalina:       "92bf4e430fd554b0a415f6143ce589ac5e20650853fbde4baf7dd21e70bbe8a8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d675a02a11124975690ccb5673bb8d0860c8d83c94518ea98bf52d61863a1e39"
+    sha256 cellar: :any,                 arm64_monterey: "41bb514c52d981ac96179d259a75cb67c0279030c8291db2ac7ec9fe1b55e706"
+    sha256 cellar: :any,                 arm64_big_sur:  "2bc989c08cab03b73e5f71092e8a1dcc8bcd8d206b3fc50ed54399b642a31607"
+    sha256 cellar: :any,                 monterey:       "0a21cb1e4cf425ba12d2a75ef844f7840a18d402537bca95610dbb800d56a808"
+    sha256 cellar: :any,                 big_sur:        "de2ba50a393064fb97f664b939933d2ae74e3ce825375578896f812d7bb944e0"
+    sha256 cellar: :any,                 catalina:       "61b12a60dacb5258d72f0219bf49023de032531ae56f25996ff88f3361cd2b3c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1091f0b1369f8ac44f91a245c86830027667b576c35dded87394229cbe30c96e"
   end
 
   depends_on "pkg-config" => :build
