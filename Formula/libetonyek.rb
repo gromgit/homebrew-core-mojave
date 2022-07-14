9fe3587b6b62d89613c3152eff446bc3cf3bd8bc
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:45:05 +0800

    libetonyek: no rebuilding required

--- Formula/libetonyek.rb
+++ Formula/libetonyek.rb
@@ -10,11 +10,13 @@ class Libetonyek < Formula
   end
 
   bottle do
-    sha256               arm64_big_sur: "8ca7825177d98f44ac9f3d6ad409eb3bc79d4621cb6c75aea43f60ca66234d0f"
-    sha256 cellar: :any, big_sur:       "c0d419840c9454c6fe46fcffe7e27d57a8a5ea6a26a9bdd75ab6756f9399b2d0"
-    sha256 cellar: :any, catalina:      "d40376bdfb4527e035ec8c0c1d65927303f81a456d02ce9fb0503a41f5e9ee60"
-    sha256 cellar: :any, mojave:        "1d30b4258651cc6edbd2e7e39d6af095a25f52994baa3ed34e2be0e2f606ecf2"
-    sha256               x86_64_linux:  "50c4ec0dd235b3cde8180781e8392183d8b2f7e932f571a4317e0437deec01cd"
+    sha256 cellar: :any, arm64_monterey: "823ff94f1331fb0f24e356c2326d27ea8b71cd12aeea2cbbea26cf953a86f95a"
+    sha256               arm64_big_sur:  "8ca7825177d98f44ac9f3d6ad409eb3bc79d4621cb6c75aea43f60ca66234d0f"
+    sha256               monterey:       "b402d2d2b2e6e885b0ee19d55d43adb5b8e7e2af5d0b913f5bbc05456f493b1c"
+    sha256 cellar: :any, big_sur:        "c0d419840c9454c6fe46fcffe7e27d57a8a5ea6a26a9bdd75ab6756f9399b2d0"
+    sha256 cellar: :any, catalina:       "d40376bdfb4527e035ec8c0c1d65927303f81a456d02ce9fb0503a41f5e9ee60"
+    sha256 cellar: :any, mojave:         "1d30b4258651cc6edbd2e7e39d6af095a25f52994baa3ed34e2be0e2f606ecf2"
+    sha256               x86_64_linux:   "50c4ec0dd235b3cde8180781e8392183d8b2f7e932f571a4317e0437deec01cd"
   end
 
   depends_on "boost" => :build
@@ -26,8 +28,8 @@ class Libetonyek < Formula
   uses_from_macos "libxml2"
 
   resource "liblangtag" do
-    url "https://bitbucket.org/tagoh/liblangtag/downloads/liblangtag-0.6.3.tar.bz2"
-    sha256 "1f12a20a02ec3a8d22e54dedb8b683a43c9c160bda1ba337bf1060607ae733bd"
+    url "https://bitbucket.org/tagoh/liblangtag/downloads/liblangtag-0.6.4.tar.bz2"
+    sha256 "5701062c17d3e73ddaa49956cbfa5d47d2f8221988dec561c0af2118c1c8a564"
   end
 
   def install
@@ -37,6 +39,11 @@ class Libetonyek < Formula
       system "make", "install"
     end
 
+    # The mdds pkg-config .pc file includes the API version in its name (ex. mdds-2.0.pc).
+    # We extract this from the filename programmatically and store it in mdds_api_version.
+    mdds_pc_file = (Formula["mdds"].share/"pkgconfig").glob("mdds-*.pc").first.to_s
+    mdds_api_version = File.basename(mdds_pc_file, File.extname(mdds_pc_file)).split("-")[1]
+
     ENV["LANGTAG_CFLAGS"] = "-I#{libexec}/include"
     ENV["LANGTAG_LIBS"] = "-L#{libexec}/lib -llangtag -lxml2"
     system "./configure", "--without-docs",
@@ -45,7 +52,7 @@ class Libetonyek < Formula
                           "--disable-werror",
                           "--disable-tests",
                           "--prefix=#{prefix}",
-                          "--with-mdds=1.5"
+                          "--with-mdds=#{mdds_api_version}"
     system "make", "install"
   end
 
