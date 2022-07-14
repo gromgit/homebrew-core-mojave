faa5b378d8fddb3e5f8a513a8798d75912cdec00
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:45:05 +0800

    libelf: no rebuilding required

--- Formula/libelf.rb
+++ Formula/libelf.rb
@@ -17,7 +17,6 @@ class Libelf < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "e11504a15c64cd7fca3248ca7ed14eead25a5d63d8bbd9a8e00f076c56602295"
     sha256 cellar: :any_skip_relocation, sierra:         "a771e35555810a4910304e3ca5967ea3e4f8cbe45576e5b2dc6b80cd9c1f0f13"
     sha256 cellar: :any_skip_relocation, el_capitan:     "a06b058c7e401942f442f573b63aa2cdd548b45d38b02b7af92393c67093f56e"
-    sha256 cellar: :any_skip_relocation, yosemite:       "3b4ea9ab20228d9e912f80a330b6d6d093f9bb65a712208c83cd49bdcc4fc9ea"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c72de6e960f70dd98ea52b419d6e254362813c899d4859c4778d385a7c80e0dd"
   end
 
