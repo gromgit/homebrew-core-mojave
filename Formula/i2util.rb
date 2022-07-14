c940a285ff4651a14cfbcedaa6a89091e9c2db1d
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:44:41 +0800

    i2util: no rebuilding required

--- Formula/i2util.rb
+++ Formula/i2util.rb
@@ -20,7 +20,6 @@ class I2util < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "47c66cf5e0bfec05a5c254dc4088fe2ec3dd45772d729bd0b38146afdfbd0f0a"
     sha256 cellar: :any_skip_relocation, sierra:         "562e2d9021ff8044ca05a63c31d6560e5071ffc62f34ff1046cf195118b3471a"
     sha256 cellar: :any_skip_relocation, el_capitan:     "44f87d48502ae3e34ebfc0882aa689a70e8c92d398247c5a53e2f4b7d7652b39"
-    sha256 cellar: :any_skip_relocation, yosemite:       "ad1821b2637c75638de2ecd2bd3127a0c8300fe4fbd72c18ae648a131b97b6f7"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "feacfcb52a15646c245053b709c6cb0105e916eab0d7e1884ac7146ff40b8513"
   end
 
