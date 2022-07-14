7e7e3bca97508b850c5d8931a93736693ef4860b
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:43:00 +0800

    arss: no rebuilding required

--- Formula/arss.rb
+++ Formula/arss.rb
@@ -14,7 +14,6 @@ class Arss < Formula
     sha256 cellar: :any,                 high_sierra:    "b848efa3abde7c5fffd18289c1ab51a842cd93e0e97d6af32329acf869909d38"
     sha256 cellar: :any,                 sierra:         "2311c31ae2e80905dfc41c8adb9639314664103352540b198f24c54e0c102550"
     sha256 cellar: :any,                 el_capitan:     "5da45934b19d0cab02c809932fb8c5da3fd76d2f781bc9e2e7a98fa1825989eb"
-    sha256 cellar: :any,                 yosemite:       "268225389842f4952424b17c7b94759b7a3d3009053b50718f1e4155b7eace86"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c5c0a1ade41f7ff063620d37b071556cd71382e9728f160c24393809f89c0f80"
   end
 
