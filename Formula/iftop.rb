21f06d3563efcb64c188a9cf9ce4b7deb4c51aee
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:44:44 +0800

    iftop: no rebuilding required

--- Formula/iftop.rb
+++ Formula/iftop.rb
@@ -25,7 +25,6 @@ class Iftop < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "c582f943657323d52493e9b85d05a9ac5b079dd1bb74075fbcd5952f2758fbd7"
     sha256 cellar: :any_skip_relocation, sierra:         "3a75c5edf29f6cbac19641910661423ed2f0b83f3e0de28c8417be76f4ce5c38"
     sha256 cellar: :any_skip_relocation, el_capitan:     "e7b9ed6bef435d7de8e986cedfc76779f2655bfac7ed780afb33a42e92b8d01d"
-    sha256 cellar: :any_skip_relocation, yosemite:       "5ac910598f86716e0dc676f0fd44af51da141ad84dfbaf5535a31f11ab91ffd7"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "eef5cd5b14c8b7b2e7964b39a3833a2cbe3253d93905157f92632c78555a8190"
   end
 
