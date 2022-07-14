1179fd26943079b0e0d7c9288906130a7b53163e
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:43:23 +0800

    chruby: no rebuilding required

--- Formula/chruby.rb
+++ Formula/chruby.rb
@@ -14,7 +14,6 @@ class Chruby < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:   "d59074fe39429eb9979acd0e81e6b9a142aa73595971cee42ab91bbe850c6105"
     sha256 cellar: :any_skip_relocation, sierra:        "17dc507695fed71749b5a58152d652bb7b92a4574f200b631a39f5f004e86cca"
     sha256 cellar: :any_skip_relocation, el_capitan:    "ff70dff83817f093d39384a40d3dfb2aaccc1cbe475d58383d4ef157085f2c64"
-    sha256 cellar: :any_skip_relocation, yosemite:      "eb14810c552b693c5ae82a577be81398e7dfeadc5489666bb0ff89581f09bfe4"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "bea6c750e5ce28f5a8ade003baef8a42bcbdf2b376e2d4ae8e12c7b3b112fef6"
     sha256 cellar: :any_skip_relocation, all:           "977cf9319a21ddbbd26d3f0a43ed75825eb2a514bdce56b4045e5214732ec13b"
   end
