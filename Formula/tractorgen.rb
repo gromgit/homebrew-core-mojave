a65b84b8fe1318ea6e4b9c68109c4bb3c3804608
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:47:23 +0800

    tractorgen: no rebuilding required

--- Formula/tractorgen.rb
+++ Formula/tractorgen.rb
@@ -20,7 +20,6 @@ class Tractorgen < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "936883746158534e9650a0b26f18e680eed527fb56f71ad51e5ec203d8f7f451"
     sha256 cellar: :any_skip_relocation, sierra:         "646d87ca0cb1a5ec93a8aa1ddaa1f28233347ca0a1f56e49c323809ec8295432"
     sha256 cellar: :any_skip_relocation, el_capitan:     "ccac503b4577fc81e69d3e778c27c31fad9a1c5fa8627e97f293d87ab1177f8d"
-    sha256 cellar: :any_skip_relocation, yosemite:       "e50de2fd2d9015873282a62fc7a21f3ef419d527d07eeab3830ace52ec25c3c9"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "d67a731185350d21bdeea6e2e6a478409b430f371d21f6b595d44e7b7ebc2363"
   end
 
