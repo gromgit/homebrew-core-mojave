c55dbabea8472dda0a543ec9f19a48c0a93d5ea4
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:43:32 +0800

    convertlit: no rebuilding required

--- Formula/convertlit.rb
+++ Formula/convertlit.rb
@@ -22,7 +22,6 @@ class Convertlit < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "f41e31b1f6f53d1441bf670e75e0315f6a0f0e938de75e9973678ed565b6b4b8"
     sha256 cellar: :any_skip_relocation, sierra:         "43e28e7711f27843223b29d351ba0ce03a4deee76bbc99c4bdac50969b8eaeb7"
     sha256 cellar: :any_skip_relocation, el_capitan:     "66b05c2c6371f16620c82b31b507413556b511b859644322c65f4ceea4a83a64"
-    sha256 cellar: :any_skip_relocation, yosemite:       "024a9fdb4b58a3e04c12ec300facbac636b3510f8726726c4be93c60cf272ab1"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "983bf456c2fed65ab702faeb6ada0265c9cf32f72fbee7fcd967e81f68b49b2f"
   end
 
