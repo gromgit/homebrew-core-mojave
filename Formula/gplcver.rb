552b102d4a305c258db89a38884c0fe87e181d49
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:44:31 +0800

    gplcver: no rebuilding required

--- Formula/gplcver.rb
+++ Formula/gplcver.rb
@@ -21,7 +21,6 @@ class Gplcver < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "2460dcc2da525280cd5b7d2452abe922874291b92f0ba3abd1316da2e5ff40f7"
     sha256 cellar: :any_skip_relocation, sierra:         "a0f14e7d01b7098ed6e770b21df05f03d7506ca0bab3d1f84845ca9ca7d1eb5b"
     sha256 cellar: :any_skip_relocation, el_capitan:     "a094d355a75148ed611e9668841a33810a1a1226bc6651b8d0c5e4868867e7fd"
-    sha256 cellar: :any_skip_relocation, yosemite:       "fc4f5fc0f1bb13139740ae6f2966bd4e3adb57c7a9803b84f946d95fcb40dd2a"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c7781b1d43d27d447386d9563e2c04d7b253b467773d077f29f2171723a66829"
   end
 
