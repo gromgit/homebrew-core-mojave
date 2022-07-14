93fe25a4d5539466d9303521fc3e774d74f2632f
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:44:01 +0800

    ent: no rebuilding required

--- Formula/ent.rb
+++ Formula/ent.rb
@@ -15,7 +15,6 @@ class Ent < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "61cac8b0bcf0c511e6c77760cc9441ec7b4d981392f98d37bd8a40fd281620df"
     sha256 cellar: :any_skip_relocation, sierra:         "9f20aba355ecd3310d5e4cd425fe69d88e20e9e1bc128a4b6a97c5d98a828135"
     sha256 cellar: :any_skip_relocation, el_capitan:     "f5244a065b7aafe3ba60077de0072e9b5d231a7fd1eb348cd7f6583a69a08ad3"
-    sha256 cellar: :any_skip_relocation, yosemite:       "072e3e71ee3b6813dafb15e53e8b474c1d15f26865b9cd05652e46c220e3926d"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "2473992bc5d574c4fff2dafdc65e23e72c6661fab3b939d6a853a98b45538e4e"
   end
 
