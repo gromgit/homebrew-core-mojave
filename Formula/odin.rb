efb69a82a9db0cd5cd6f6c0c36d19677045bf937
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:46:04 +0800

    odin:

--- Formula/odin.rb
+++ Formula/odin.rb
@@ -2,19 +2,18 @@ class Odin < Formula
   desc "Programming language with focus on simplicity, performance and modern systems"
   homepage "https://odin-lang.org/"
   url "https://github.com/odin-lang/Odin.git",
-      tag:      "dev-2022-06",
-      revision: "ba5f7c4e2af5c82c220b7e1796fde2f026ce4208"
-  version "2022-06"
+      tag:      "dev-2022-07",
+      revision: "1676c643dfd9ef45f2aaa4dfddb69cea4bcf80fc"
+  version "2022-07"
   license "BSD-3-Clause"
-  revision 1
   head "https://github.com/odin-lang/Odin.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "99d3f356276eb775996a0b694c329c22b00c2a1b54f6f9c2df634f210579b4b6"
-    sha256 cellar: :any,                 arm64_big_sur:  "3479376b9b9fecbcfba35b71e367e84d92a6c1e10a93c605ecdf3f21c64ea8b9"
-    sha256 cellar: :any,                 monterey:       "4dffaa801df406990c17d3ae92b9896a5e689ae226dcd9aa4293b0c4c105bfcc"
-    sha256 cellar: :any,                 big_sur:        "fc1a6ba86fa16ccb06cd0cf444bf074abb618c56a78dc481544117e606fca9e7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "30244b3c4d3033db671c62a6521578bd2e11a5401458e75eac99fc849d36dbaf"
+    sha256 cellar: :any,                 arm64_monterey: "5b3bdfebf981c9a493ef5ff64219ac6d7959b9cf61ea8ce122da60be5e46666b"
+    sha256 cellar: :any,                 arm64_big_sur:  "e26552b8028335a62184199c9aa138eeb315f53f0ae16d4416e82f2d2b84688a"
+    sha256 cellar: :any,                 monterey:       "bfcb5cd1468bbf2d49678e6056d17bcdc4df5aabcff055e0aac94873975c2085"
+    sha256 cellar: :any,                 big_sur:        "8270c0c05cc3e80129d931332662341e248145b36753cd3ac1aa70846d077f48"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d8e2214cd00c5e3fb1deac87a5eef246a0947c115aa2f74782ab0b9a90ba2fb0"
   end
 
   depends_on "llvm"
