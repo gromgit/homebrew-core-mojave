2fa4171fa05da03e78d8b55535bfd1c71f41e3f3
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:42:56 +0800

    apngasm:

--- Formula/apngasm.rb
+++ Formula/apngasm.rb
@@ -4,15 +4,16 @@ class Apngasm < Formula
   url "https://github.com/apngasm/apngasm/archive/3.1.10.tar.gz"
   sha256 "8171e2c1d37ab231a2061320cb1e5d15cee37642e3ce78e8ab0b8dfc45b80f6c"
   license "Zlib"
+  revision 1
   head "https://github.com/apngasm/apngasm.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "013957786451917bee554fc4a8e53a19163c15e76fa92623349e3fd75a0fdfe6"
-    sha256 cellar: :any,                 arm64_big_sur:  "4ece563fa1571fa78e0481b8aa3237035d7424366eead22c2d32be9d5415b392"
-    sha256 cellar: :any,                 monterey:       "fbe72e5a561b325803900a3ab33721b470e4fda5ddf1c1aa9330b925caf00c89"
-    sha256 cellar: :any,                 big_sur:        "256488e04ed74850915b99697fece6fdd2d0f79cef0351b56096336537b4fff7"
-    sha256 cellar: :any,                 catalina:       "08ce0311e991a31ab52c8b51dbc5cb0403f1e00ea652a6dbf1c8c535b8216a28"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "82dbded4f63ead5cc474a42793fc17edabc9455c54b5de2f748cf3a17706d529"
+    sha256 cellar: :any,                 arm64_monterey: "80b90951415a88eff1da221b4826a2f1511fa5322ba04c449f3aeec393e0999b"
+    sha256 cellar: :any,                 arm64_big_sur:  "d4bbbe2f3dce2bf221ee011f7f192ca1b5c2e1440214bc0105d09a0c0be356bd"
+    sha256 cellar: :any,                 monterey:       "2cfe286cfa16d59d200bb5b1b5f37afa66d01e270c976cc179f82dc70e6e1fe5"
+    sha256 cellar: :any,                 big_sur:        "4b7399bdb7991330dee6a26a00ee1ec58e6074895b026fda608950b0372525d7"
+    sha256 cellar: :any,                 catalina:       "bcba805553ae564c117609755b2f4c3e06f1c23579430505fa89fda8c126c06c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "428941624e3fc27d7ac937ac7eaa016724abafa63119969b01bfd4159421cbba"
   end
 
   depends_on "cmake" => :build
