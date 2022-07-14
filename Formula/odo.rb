f4e95af475acdbf3c09689564b61529fe242c0d4
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:46:05 +0800

    odo: no rebuilding required

--- Formula/odo.rb
+++ Formula/odo.rb
@@ -15,7 +15,6 @@ class Odo < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "0bfc54617186d149c98593c74dfaa59a42b2edcc7df1855fd452594ec42f1476"
     sha256 cellar: :any_skip_relocation, sierra:         "06af025b0a2df201a9b79944dcc4809708b305242622a90c92a9906a18adf2d6"
     sha256 cellar: :any_skip_relocation, el_capitan:     "979cc7131a35180614e848fa5fa12a72f734da7321358c89dfbd425fc8dff837"
-    sha256 cellar: :any_skip_relocation, yosemite:       "ebfc6a2e616694a3862b1d6d11dda1a2c1cb4c966447678b342457490e0e0abc"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "23644efe576abf9c3e3a469cf1baad05b3a3cde749998045b2992c290cf57884"
   end
 
