2283a34d626b200402f24975403ed9751570ed59
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:46:06 +0800

    ogmtools: no rebuilding required

--- Formula/ogmtools.rb
+++ Formula/ogmtools.rb
@@ -18,7 +18,6 @@ class Ogmtools < Formula
     sha256 cellar: :any,                 high_sierra:   "c84b3fe9a525a0f6719bab86a5b919af73b067b48134e9b9ff3225af9b728260"
     sha256 cellar: :any,                 sierra:        "ec07a396ce68d5c646c838e3129dbe6c8ca8ff7ea9126cd31f9844016582d0ec"
     sha256 cellar: :any,                 el_capitan:    "8e0ceae59b3a69647511dff89566a734d25a96a764893c7599ee1ece73890db5"
-    sha256 cellar: :any,                 yosemite:      "3a43fec619944cd6fa8e57bd067477ef63997919e11174ddafb160c47b28fd5d"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "0bd76fd65c8b1627890beef905c7b8eb23119ace422c6618be47a9412862479c"
   end
 
@@ -32,9 +31,11 @@ class Ogmtools < Formula
   end
 
   def install
+    ENV.append "CPPFLAGS", "-I#{Formula["libvorbis"].opt_include}"
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}", "--mandir=#{man}"
-    system "make", "install"
+
+    system "make", "install", "LIBS=-L#{Formula["libvorbis"].opt_lib} -lvorbis -lvorbisenc"
   end
 
   # Borrow warning from MacPorts
