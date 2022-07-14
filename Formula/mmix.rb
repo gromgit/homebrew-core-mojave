a9b68bfca03d144d6e80cb60e3f801656570aad6
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:45:47 +0800

    mmix: no rebuilding required

--- Formula/mmix.rb
+++ Formula/mmix.rb
@@ -19,7 +19,6 @@ class Mmix < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "7bc054e2d244fe693b4ed5ef47c56e23ac4952b15ddc5de55d19150d4dc2bf30"
     sha256 cellar: :any_skip_relocation, sierra:         "b694920e61edf2dec094618910be78fcd4fbbcad22d4d37363555aad38ee0af0"
     sha256 cellar: :any_skip_relocation, el_capitan:     "c1e8e0d2d627b3ab2c2c68a8b358981dab07466c3c70f3a2e4df8557006deb92"
-    sha256 cellar: :any_skip_relocation, yosemite:       "7675c2bc1253e4da2a126d52942449f71cabdd83c39874403d449b5a05ceb145"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "1095cb1a943d20e2613c77874a67e1f41bc17eeccc3b503cfb5ce3f6215fd01f"
   end
 
