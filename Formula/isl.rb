50928a01405c15c02a35a893c7f68e19bbf4e5b5
Adrian Ho <the.gromgit@gmail.com>
2022-07-14 15:04:33 +0800

    isl: update 0.25 bottle.

--- Formula/isl.rb
+++ Formula/isl.rb
@@ -10,8 +10,8 @@ class Isl < Formula
     # and update isl_version() function accordingly.  All other names will
     # result in isl_version() function returning "UNKNOWN" and hence break
     # package detection.
-    url "https://libisl.sourceforge.io/isl-0.24.tar.xz"
-    sha256 "043105cc544f416b48736fff8caf077fb0663a717d06b1113f16e391ac99ebad"
+    url "https://libisl.sourceforge.io/isl-0.25.tar.xz"
+    sha256 "be7b210647ccadf90a2f0b000fca11a4d40546374a850db67adb32fad4b230d9"
 
     # Fix -flat_namespace being used on Big Sur and later.
     patch do
@@ -26,13 +26,8 @@ class Isl < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "be08c3e9765655ad5bfd227f9b97acb0ef88ad2307dc214ea4064cc1f51db641"
-    sha256 cellar: :any,                 arm64_big_sur:  "cf7f8b77fc0e51bc9c9921306737176e6f9e03062255a525d9ab59cf91ed3d36"
-    sha256 cellar: :any,                 monterey:       "6eca22a381c05adc4e33195e5b409bd695e035141d577480876ac22d811ec5bf"
-    sha256 cellar: :any,                 big_sur:        "d8c7026042e122143e0729bf3a596be77753b8cfeddcae200cd3a3c18176800c"
-    sha256 cellar: :any,                 catalina:       "f33ee49a23fbde05392be23110d14add72aaed390ffd3aefc400645eeb1772d4"
-    sha256 cellar: :any,                 mojave:         "34b71567d6bfb7e4cb4aced1d089fe6d72988af02775730be26b5bcea483d065"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7b58d25de0b7ed797118f32febe116ee2d7458767e6339d6a4ce51ff4ab11191"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/isl"
+    sha256 cellar: :any, mojave: "8dd3f3c30683bb024468320eae3c79052ecd06f614624d546214644c5e81911b"
   end
 
   head do
