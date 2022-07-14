68e7c6185002d2fbe2b62c9c12e719e61b4d8b81
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:47:21 +0800

    tofrodos: no rebuilding required

--- Formula/tofrodos.rb
+++ Formula/tofrodos.rb
@@ -20,7 +20,6 @@ class Tofrodos < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "083975a39eaa51713f2eda153276ac95d8dfc1f038d25c4826be1ddcd540855b"
     sha256 cellar: :any_skip_relocation, sierra:         "3d5363cda2170ce2fbcb7e03c84f715b62ead1e5646000dd06395f5677fd2269"
     sha256 cellar: :any_skip_relocation, el_capitan:     "4a2b22ff08d0fb65c80be7359be2f04d12b70f4e6d490b96cb819ea69b3e3d88"
-    sha256 cellar: :any_skip_relocation, yosemite:       "4a5427c6870c3d4822ef4da3ddd8d79c18b91e5b7f14edb4aa449a53da70114e"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "b0f32616234ccfd1984a590ef37cd25cf15ba64fe0316f1f0a2d8fdea2cfc6f3"
   end
 
