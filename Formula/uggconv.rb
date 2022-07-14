3c97a268915c4ba73dd245953d1b2fbbcea5400c
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:47:30 +0800

    uggconv: no rebuilding required

--- Formula/uggconv.rb
+++ Formula/uggconv.rb
@@ -20,7 +20,6 @@ class Uggconv < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "ed41635e3235d763c9dcad68db2e390821af5a62ec6709fca8b9c5e9b5d0b995"
     sha256 cellar: :any_skip_relocation, sierra:         "a22594f94bf7baa1908bd1225f52f1db3dd01daa17f99038ecfbd60e22d12b5d"
     sha256 cellar: :any_skip_relocation, el_capitan:     "5ab8b271f2ccc17e5229921f01b92ff7b0c297908902c83d24612bb47592af3c"
-    sha256 cellar: :any_skip_relocation, yosemite:       "a40a8a1adee9286acedba6e8eedf20bc53e4bf291fc73478bd3ba0314792c6ce"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "70d91fd685adcb8943530056934bc1e8f0ed0c5502a9205c6b1c8fa982fdec53"
   end
 
