8ecd42ab6c2bbdcbee6bbf0c77c709c15516f1ad
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:44:12 +0800

    flvstreamer: no rebuilding required

--- Formula/flvstreamer.rb
+++ Formula/flvstreamer.rb
@@ -21,7 +21,6 @@ class Flvstreamer < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "52d09a95883b401b1d77d0e85354099cc351285a2243d00c257778033f36dbf6"
     sha256 cellar: :any_skip_relocation, sierra:         "e257779383d236611212078f2e6db28d457d51a282d6163806e0232134a046b0"
     sha256 cellar: :any_skip_relocation, el_capitan:     "5a4b649ce0f2c32bca4091f4867a37cca0e8ae2a292d4ef29aa2949530bdd651"
-    sha256 cellar: :any_skip_relocation, yosemite:       "243e6ce44b77212ff84e3a739bf2b203c687bdcdd36b17ba24daa5335bf0a151"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "99df5a533003260eea6277d1fa2bb80d0354b666a6a99195c46813e3313c701f"
   end
 
