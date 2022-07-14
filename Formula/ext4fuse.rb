b8c68bef9995c4ab3024733e23d2fe5f0c2e0fd9
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:44:06 +0800

    ext4fuse:

--- Formula/ext4fuse.rb
+++ Formula/ext4fuse.rb
@@ -4,7 +4,7 @@ class Ext4fuse < Formula
   url "https://github.com/gerard/ext4fuse/archive/v0.1.3.tar.gz"
   sha256 "550f1e152c4de7d4ea517ee1c708f57bfebb0856281c508511419db45aa3ca9f"
   license "GPL-2.0"
-  head "https://github.com/gerard/ext4fuse.git"
+  head "https://github.com/gerard/ext4fuse.git", branch: "master"
 
   bottle do
     sha256 cellar: :any,                 catalina:     "446dde5e84b058966ead0cde5e38e9411f465732527f6decfa1c0dcdbd4abbef"
@@ -12,7 +12,6 @@ class Ext4fuse < Formula
     sha256 cellar: :any,                 high_sierra:  "fc69c8993afd0ffc16a73c9c036ca8f83c77ac2a19b3237f76f9ccee8b30bbc9"
     sha256 cellar: :any,                 sierra:       "fe8bbe7cd5362f00ff06ef750926bf349d60563c20b0ecf212778631c8912ba2"
     sha256 cellar: :any,                 el_capitan:   "291047c821b7b205d85be853fb005510c6ab01bd4c2a2193c192299b6f049d35"
-    sha256 cellar: :any,                 yosemite:     "b11f564b7e7c08af0b0a3e9854973d39809bf2d8a56014f4882772b2f7307ac1"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "5dc94c281e33bde87ef6c239f7ac37ad6653e72d002359ffb01d0f50f1e8278c"
   end
 
