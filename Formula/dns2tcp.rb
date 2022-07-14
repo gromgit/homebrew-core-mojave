5bf92ddc1b370268f27f77565bb4c34cbc7f7e56
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:43:50 +0800

    dns2tcp: no rebuilding required

--- Formula/dns2tcp.rb
+++ Formula/dns2tcp.rb
@@ -20,7 +20,6 @@ class Dns2tcp < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "d6fb240175854e0a0b5069544a58c4fbcd161d3337288c2f289f48999c4dde10"
     sha256 cellar: :any_skip_relocation, sierra:         "e948ddde1e95f055a9cd3e73cd2756c22f729d9feed9ebc2929cb3df6fe09584"
     sha256 cellar: :any_skip_relocation, el_capitan:     "2cd5e77bec42f0f5e2715494c38eb8773ab30d53b140509d3f428d38890bf640"
-    sha256 cellar: :any_skip_relocation, yosemite:       "3e805ac804eea824b81bd15191b71cdc42d4ac779ebfc1d74d5de51500be18a5"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "44282da4806ffc130f6a3326925e708d70379c9c44ec735b251010927d5b920e"
   end
 
