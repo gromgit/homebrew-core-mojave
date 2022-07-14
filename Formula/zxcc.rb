7ab95cc3f8aa2d6652e371428a3ab7a11cc9e841
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:47:58 +0800

    zxcc: no rebuilding required

--- Formula/zxcc.rb
+++ Formula/zxcc.rb
@@ -20,7 +20,6 @@ class Zxcc < Formula
     sha256 high_sierra:    "0b6a6d166b5b4822b46d8a53b0a2b850619882d9d13080ecdad8b0ae492a5cc0"
     sha256 sierra:         "79aa0631d52d2d69ae554319db0027ffd59f2baa3d1c35473925f72a5c1965e3"
     sha256 el_capitan:     "11bd1697b8a6b5a3a77ce417d35ad7e1da9e6df18a36ebccfa18a47ce470d3cb"
-    sha256 yosemite:       "824c8a2511a55f9fc00b7058247e3e76d9579c14d20f2d17b5e57aaf1388671f"
     sha256 x86_64_linux:   "8a5baf11b9060b7aedcf4f25afca690fbad80df406a66a70389ddd32fe2c6075"
   end
 
