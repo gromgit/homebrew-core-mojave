c6e4dab736f8a0baeea22845375be137c824a406
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:44:49 +0800

    jcal: no rebuilding required

--- Formula/jcal.rb
+++ Formula/jcal.rb
@@ -20,7 +20,6 @@ class Jcal < Formula
     sha256 cellar: :any,                 high_sierra:    "348fdd02ce58859bf75ebe00feaf5c90e1f4f052d531e7667343f4c220d8e7bb"
     sha256 cellar: :any,                 sierra:         "d6f50844723751f0de8181f751ffc0912013b518b5ac60777a3ade7e1aaa3179"
     sha256 cellar: :any,                 el_capitan:     "4d876e18cb50c7aa31211b60b66e42637ca3c9eeed9c688c1945dc4755977597"
-    sha256 cellar: :any,                 yosemite:       "3640b058b034b519a5aa3bb1dde36b4efb2ec7bb8124bdbd106617202bf87b22"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "e7b67ec668f4880126342f70bb106b66744773ebbe8afc9e11eacf96d5a11108"
   end
 
