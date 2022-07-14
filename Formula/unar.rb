db6aa46a3f5d1888b8e576cef29363c4a56b011b
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:47:31 +0800

    unar: no rebuilding required

--- Formula/unar.rb
+++ Formula/unar.rb
@@ -7,12 +7,27 @@ class Unar < Formula
   head "https://github.com/MacPaw/XADMaster.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/unar"
-    rebuild 1
-    sha256 cellar: :any, mojave: "64d017ad469f2a903d09c543fbc6d1129c115609d076df1cd29ab3caf8b70716"
+    sha256 cellar: :any,                 arm64_monterey: "5cedc1ed00cb1f638f6e7d7f026196c19aaf8e2ce9eacb7d9220b98cae2f0649"
+    sha256 cellar: :any,                 arm64_big_sur:  "16091256fd3c0d13a774fc1900b7b21584fb9eee669a65de56906e188fbcc665"
+    sha256 cellar: :any,                 monterey:       "2da5bda2a8ad54072fffd22e81c3b3b85320f8d68b993fdc4282dc6c87cec0e6"
+    sha256 cellar: :any,                 big_sur:        "a92a0fd33d7598591efa5dc01692221053cdc612bb218f46df422af0bd5082c6"
+    sha256 cellar: :any,                 catalina:       "6207848baad1fda03e3bdda9a8cd621ef2d226a02fcf4219fec64c9f418b9a0e"
+    sha256 cellar: :any,                 mojave:         "f09e3c1eb465cec023037048305b493e3ed57696a775eb121076951b8ae63e76"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7e82767f0456781aff87035e18d2062da86ed91863ac196f059f6e799ae3db3f"
   end
 
   depends_on xcode: :build
+
+  uses_from_macos "llvm" => [:build, :test]
+  uses_from_macos "bzip2"
+
+  on_linux do
+    depends_on "gnustep-base"
+    depends_on "wavpack"
+  end
+
+  # Clang must be used on Linux because GCC Objective C support is insufficient.
+  fails_with :gcc
 
   resource "universal-detector" do
     url "https://github.com/MacPaw/universal-detector/archive/refs/tags/1.1.tar.gz"
@@ -28,21 +43,28 @@ class Unar < Formula
     # Replace usage of __DATE__ to keep builds reproducible
     inreplace %w[lsar.m unar.m], "@__DATE__", "@\"#{time.strftime("%b %d %Y")}\""
 
-    mkdir "build" do
-      # Build XADMaster.framework, unar and lsar
-      arch = Hardware::CPU.arm? ? "arm64" : "x86_64"
-      %w[XADMaster unar lsar].each do |target|
-        xcodebuild "-target", target, "-project", "../XADMaster.xcodeproj",
-                   "SYMROOT=#{buildpath/"build"}", "-configuration", "Release",
-                   "MACOSX_DEPLOYMENT_TARGET=#{MacOS.version}", "ARCHS=#{arch}", "ONLY_ACTIVE_ARCH=YES"
-      end
+    # Makefile.linux does not support an out-of-tree build.
+    if OS.mac?
+      mkdir "build" do
+        # Build XADMaster.framework, unar and lsar
+        arch = Hardware::CPU.arm? ? "arm64" : "x86_64"
+        %w[XADMaster unar lsar].each do |target|
+          xcodebuild "-target", target, "-project", "../XADMaster.xcodeproj",
+                     "SYMROOT=#{buildpath/"build"}", "-configuration", "Release",
+                     "MACOSX_DEPLOYMENT_TARGET=#{MacOS.version}", "ARCHS=#{arch}", "ONLY_ACTIVE_ARCH=YES"
+        end
 
-      bin.install "./Release/unar", "./Release/lsar"
-      %w[UniversalDetector XADMaster].each do |framework|
-        lib.install "./Release/lib#{framework}.a"
-        frameworks.install "./Release/#{framework}.framework"
-        (include/"lib#{framework}").install_symlink Dir["#{frameworks}/#{framework}.framework/Headers/*"]
+        bin.install "./Release/unar", "./Release/lsar"
+        %w[UniversalDetector XADMaster].each do |framework|
+          lib.install "./Release/lib#{framework}.a"
+          frameworks.install "./Release/#{framework}.framework"
+          (include/"lib#{framework}").install_symlink Dir["#{frameworks}/#{framework}.framework/Headers/*"]
+        end
       end
+    else
+      system "make", "-f", "Makefile.linux"
+      bin.install "unar", "lsar"
+      lib.install buildpath/"../UniversalDetector/libUniversalDetector.a", "libXADMaster.a"
     end
 
     cd "Extra" do
