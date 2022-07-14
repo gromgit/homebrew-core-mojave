ee6d3f1c79981559deb052d207624367c38a2249
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:44:53 +0800

    kaitai-struct-compiler: no rebuilding required

--- Formula/kaitai-struct-compiler.rb
+++ Formula/kaitai-struct-compiler.rb
@@ -2,8 +2,8 @@ class KaitaiStructCompiler < Formula
   desc "Compiler for generating binary data parsers"
   homepage "https://kaitai.io/"
   # Move to packages.kaitai.io when available.
-  url "https://github.com/kaitai-io/kaitai_struct_compiler/releases/download/0.9/kaitai-struct-compiler-0.9.zip"
-  sha256 "3038243334fb65bbb264f33b82986facfe1fbad2de1978766899855b40212215"
+  url "https://github.com/kaitai-io/kaitai_struct_compiler/releases/download/0.10/kaitai-struct-compiler-0.10.zip"
+  sha256 "3d11d6cc46d058afb4680fda2e7195f645ca03b2843501d652a529646e55d16b"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -12,12 +12,7 @@ class KaitaiStructCompiler < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "72c9d12038d82a2357191e78f55f840b4016afb055725244c9b26c770dc112e5"
-    sha256 cellar: :any_skip_relocation, big_sur:       "5b0f1975d18c49cb063c56d10d4788d355ca31936046d430609314740cba4058"
-    sha256 cellar: :any_skip_relocation, catalina:      "5b0f1975d18c49cb063c56d10d4788d355ca31936046d430609314740cba4058"
-    sha256 cellar: :any_skip_relocation, mojave:        "5b0f1975d18c49cb063c56d10d4788d355ca31936046d430609314740cba4058"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "546340d4c6241a006245544f6759c6cd7051a9136021ce28b9533acf1da98f85"
-    sha256 cellar: :any_skip_relocation, all:           "546340d4c6241a006245544f6759c6cd7051a9136021ce28b9533acf1da98f85"
+    sha256 cellar: :any_skip_relocation, all: "18b4819a150e1f029f8716fcc304b18f59363dcf975d4271ff9b9b0129ebec72"
   end
 
   depends_on "openjdk"
