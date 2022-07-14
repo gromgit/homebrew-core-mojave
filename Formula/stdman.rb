c880ce207583998a62bf4942dbed65024fcd8ed1
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:47:09 +0800

    stdman: no rebuilding required

--- Formula/stdman.rb
+++ Formula/stdman.rb
@@ -1,20 +1,27 @@
 class Stdman < Formula
   desc "Formatted C++ stdlib man pages from cppreference.com"
   homepage "https://github.com/jeaye/stdman"
-  url "https://github.com/jeaye/stdman/archive/2021.12.21.tar.gz"
-  sha256 "5cfea407f0cd6da0c66396814cafc57504e90df518b7c9fa3748edd5cfdd08e3"
+  url "https://github.com/jeaye/stdman/archive/2022.02.01.tar.gz"
+  sha256 "84d36791514f20a814f1530e9f4e6ff67e538e0c9b3ef25db4b007f9861c4890"
   license "MIT"
   version_scheme 1
   head "https://github.com/jeaye/stdman.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/stdman"
     rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "020351e0f4461d8ce16a8695939016aa506cc4ff3c925d6c3b77b3d2ddbf1b57"
+    sha256 cellar: :any_skip_relocation, all: "3a74e020e777587f893a2c62e7fda36ca37ba021206dbe0a57f29b9970ad6642"
   end
 
   on_linux do
     depends_on "man-db" => :test
+  end
+
+  # Preserve timestamps when doing `make install`.
+  # This helps ensure uniform bottles across builds.
+  # https://github.com/jeaye/stdman/pull/49
+  patch do
+    url "https://github.com/jeaye/stdman/commit/862e5132c18275661c468083baeab42dc4c335f2.patch?full_index=1"
+    sha256 "8b2c785fc859e31fcb8c58884eff64c371bb9afd001979ad1158517c77ea1041"
   end
 
   def install
