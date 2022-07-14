4ed4a2d5cb82e6e0602053850df3288cee1cf323
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:43:43 +0800

    ddclient: no rebuilding required

--- Formula/ddclient.rb
+++ Formula/ddclient.rb
@@ -1,10 +1,28 @@
 class Ddclient < Formula
   desc "Update dynamic DNS entries"
   homepage "https://ddclient.net/"
-  url "https://github.com/ddclient/ddclient/archive/v3.9.1.tar.gz"
-  sha256 "e4969e15cc491fc52bdcd649d4c2b0e4b1bf0c9f9dba23471c634871acc52470"
-  license "GPL-2.0"
-  head "https://github.com/wimpunk/ddclient.git", branch: "develop"
+  license "GPL-2.0-or-later"
+
+  # Remove `stable` block when resources are no longer needed.
+  stable do
+    url "https://github.com/ddclient/ddclient/archive/v3.9.1.tar.gz"
+    sha256 "e4969e15cc491fc52bdcd649d4c2b0e4b1bf0c9f9dba23471c634871acc52470"
+
+    on_linux do
+      # Dependency of Data::Validate::IP. Remove at next release.
+      resource "NetAddr::IP" do
+        url "https://cpan.metacpan.org/authors/id/M/MI/MIKER/NetAddr-IP-4.079.tar.gz"
+        sha256 "ec5a82dfb7028bcd28bb3d569f95d87dd4166cc19867f2184ed3a59f6d6ca0e7"
+      end
+    end
+
+    # TODO: Remove in next release. See:
+    # https://github.com/ddclient/ddclient/blob/v3.10.0_1/ChangeLog.md#compatibility-and-dependency-changes
+    resource "Data::Validate::IP" do
+      url "https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/Data-Validate-IP-0.27.tar.gz"
+      sha256 "e1aa92235dcb9c6fd9b6c8cda184d1af73537cc77f4f83a0f88207a8bfbfb7d6"
+    end
+  end
 
   livecheck do
     url :stable
@@ -22,22 +40,16 @@ class Ddclient < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "5f3f22ad8e138b394ffa78e637637a8b9dc6c9c20a4649b4030fb45705f19171"
   end
 
+  head do
+    url "https://github.com/ddclient/ddclient.git", branch: "develop"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   uses_from_macos "perl"
 
-  on_linux do
-    resource "NetAddr::IP" do
-      url "https://cpan.metacpan.org/authors/id/M/MI/MIKER/NetAddr-IP-4.079.tar.gz"
-      sha256 "ec5a82dfb7028bcd28bb3d569f95d87dd4166cc19867f2184ed3a59f6d6ca0e7"
-    end
-  end
-
-  resource "Data::Validate::IP" do
-    url "https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/Data-Validate-IP-0.27.tar.gz"
-    sha256 "e1aa92235dcb9c6fd9b6c8cda184d1af73537cc77f4f83a0f88207a8bfbfb7d6"
-  end
-
   def install
-    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+    ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5" if resources.present?
 
     resources.each do |r|
       r.stage do
@@ -47,14 +59,20 @@ class Ddclient < Formula
       end
     end
 
-    # Adjust default paths in script
-    inreplace "ddclient" do |s|
-      s.gsub! "/etc/ddclient", "#{etc}/ddclient"
-      s.gsub! "/var/cache/ddclient", "#{var}/run/ddclient"
-    end
+    if build.head?
+      system "./autogen"
+      system "./configure", *std_configure_args, "--sysconfdir=#{etc}", "--localstatedir=#{var}", "CURL=curl"
+      system "make", "install", "CURL=curl"
+    else
+      # Adjust default paths in script
+      inreplace "ddclient" do |s|
+        s.gsub! "/etc/ddclient", "#{etc}/ddclient"
+        s.gsub! "/var/cache/ddclient", "#{var}/run/ddclient"
+      end
 
-    sbin.install "ddclient"
-    sbin.env_script_all_files(libexec/"sbin", PERL5LIB: ENV["PERL5LIB"])
+      sbin.install "ddclient"
+      sbin.env_script_all_files(libexec/"sbin", PERL5LIB: ENV["PERL5LIB"])
+    end
 
     # Install sample files
     inreplace "sample-ddclient-wrapper.sh",
