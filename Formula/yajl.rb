062e3ac09cc3c947ce212f1027c79345a1c17ea0
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:47:53 +0800

    yajl: no rebuilding required

--- Formula/yajl.rb
+++ Formula/yajl.rb
@@ -16,7 +16,6 @@ class Yajl < Formula
     sha256 cellar: :any,                 high_sierra:    "3213f11462b3c60a33209c4f5d36c96caf1a9409103012ffb427dd51770ac120"
     sha256 cellar: :any,                 sierra:         "1f97e0bbc6680ad4735f0c7ecac20ec87531456c3ab1c93c480c5c5a93a33e1c"
     sha256 cellar: :any,                 el_capitan:     "5cfd83bfdbd7c92402f1cecc6b66788e6db0c195880a40263365d8130e47db2f"
-    sha256 cellar: :any,                 yosemite:       "600fec6352ac23a66795cce22cb0a555df43eb464c87693299cb4fc2a1307833"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "c206281370fe7277f21d17ff50fe9511233aff74501238516918027006c9cbf3"
   end
 
