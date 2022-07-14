7b29bf6fb5649ffcebdb0356eee1f95ed3b3d74c
Adrian Ho <the.gromgit@gmail.com>
2022-07-15 02:42:41 +0800

    googletest: update 1.12.1 bottle.

--- Formula/googletest.rb
+++ Formula/googletest.rb
@@ -1,13 +1,13 @@
 class Googletest < Formula
   desc "Google Testing and Mocking Framework"
   homepage "https://github.com/google/googletest"
-  url "https://github.com/google/googletest/archive/release-1.12.0.tar.gz"
-  sha256 "2a4f11dce6188b256f3650061525d0fe352069e5c162452818efbbf8d0b5fe1c"
+  url "https://github.com/google/googletest/archive/release-1.12.1.tar.gz"
+  sha256 "81964fe578e9bd7c94dfdb09c8e4d6e6759e19967e397dbea48d1c10e45d0df2"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/googletest"
-    sha256 cellar: :any_skip_relocation, mojave: "b6745e76b696831f5e9ec2828118fec8f7804b72614277432660d1ea95cd7290"
+    sha256 cellar: :any_skip_relocation, mojave: "90375e61f6c39158d9d555beefa5ddadaf9bea36110a13b224f197db178d61ac"
   end
 
   depends_on "cmake" => :build
