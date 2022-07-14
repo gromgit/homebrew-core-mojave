05cbb8a3622e034c5a24a9260fa86725d8e474b5
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:43:07 +0800

    bashish: no rebuilding required

--- Formula/bashish.rb
+++ Formula/bashish.rb
@@ -15,7 +15,6 @@ class Bashish < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "b7caabd1274134f33dd458ac444bbe14a139de76b91f8bebb56349377b840a5e"
     sha256 cellar: :any_skip_relocation, sierra:         "31134b56c7ad43b04ef186485af8581dbf8d8d8fcf615d259554d9c5adc7233f"
     sha256 cellar: :any_skip_relocation, el_capitan:     "114d2ce95e530c6850bc36a52a1053ecf05185d774ed499bd1725811b3c1b88c"
-    sha256 cellar: :any_skip_relocation, yosemite:       "cb3bfee8b595277be04660817eb269e97744d5f49dcac431ae7473982ad5d405"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "9de9a34d40236aa4fc76c38f69fa2b30b6d66bb1d6289a7552b543300abc753c"
   end
 
