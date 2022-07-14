ede21674d2ece724a74cfa53375b02929af229f8
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:44:59 +0800

    lhasa: no rebuilding required

--- Formula/lhasa.rb
+++ Formula/lhasa.rb
@@ -7,9 +7,16 @@ class Lhasa < Formula
   head "https://github.com/fragglet/lhasa.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lhasa"
-    rebuild 1
-    sha256 cellar: :any, mojave: "9d03755816b69105b1b5a5ba2c61fe3bf960ac0697545614535b5725641a7828"
+    sha256 cellar: :any,                 arm64_monterey: "f2584155441658b209d336823dd3428b4da20896e6c478d34ef6870cef4b4c74"
+    sha256 cellar: :any,                 arm64_big_sur:  "90f888591f8bbbbc49b3616dfc43287474a510517cc1222488ac8a707f9968b8"
+    sha256 cellar: :any,                 monterey:       "ebf620d9a216397aad8a1f0a67cb70e22fd4ae011ebad98fdd7c91477ee3b4cf"
+    sha256 cellar: :any,                 big_sur:        "7e1f12414e857455d57b3f943d50d921a189e817e264b14a9a0467e661e8cfc2"
+    sha256 cellar: :any,                 catalina:       "066d1b549b96700d8b7509e1f90b1564ddc66fc3b1dd18247b450c9990124f36"
+    sha256 cellar: :any,                 mojave:         "9b7b3503673097759714a75dc5ebc5a4c4e1184c88a80fa036bb39b2d896f0d8"
+    sha256 cellar: :any,                 high_sierra:    "36f6530ca2f2908bed047741ce52e41f4ec0d0d726bdd8ecb664958da821b527"
+    sha256 cellar: :any,                 sierra:         "d0abfc9315cfeff37781861e8c7ba2d3eb34003684560ee22a5dfb2acc4dfd5a"
+    sha256 cellar: :any,                 el_capitan:     "0d407f1058853c656a4aef717c1e72ff57472e0622fb344a5ef57c4c9ad8c3ee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6ea5cc36df44bf7e1b604f93003609cc3386172cad0875f3516435505c574839"
   end
 
   depends_on "autoconf" => :build
