1f8d4644bb9447f4f729762f892c6e7cce9cb8ce
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:47:39 +0800

    vncsnapshot: no rebuilding required

--- Formula/vncsnapshot.rb
+++ Formula/vncsnapshot.rb
@@ -21,7 +21,6 @@ class Vncsnapshot < Formula
     sha256 cellar: :any,                 high_sierra:    "76511216d57e76f357ab805fc700f6d777db9d173436d1b65c3de0733352472b"
     sha256 cellar: :any,                 sierra:         "534d56ed36faf618d5617a1f32b4d7e1fd7cd433a1fc4c64e8c1e9312d53b1c9"
     sha256 cellar: :any,                 el_capitan:     "1069a6f396c574c9efa7f6ac41772807f3d5e53c152a94a42733d64522d0a31d"
-    sha256 cellar: :any,                 yosemite:       "efd8f3255029bcb16a554855f3f3fdb7ea59604df888aec6380da1d7330e3094"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee069bf1de285c225dfb58852e0d760e5593ef5e695b42952b33105a8ea8918a"
   end
 
