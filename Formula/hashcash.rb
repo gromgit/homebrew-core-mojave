24bd74c963bc3cf37f715c6b35bf5b3760193f9b
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:44:36 +0800

    hashcash: no rebuilding required

--- Formula/hashcash.rb
+++ Formula/hashcash.rb
@@ -19,7 +19,6 @@ class Hashcash < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "acb58644b209a262a1f8aea8c4f40e078f4e76742d0339c4e240f92bdd2fb290"
     sha256 cellar: :any_skip_relocation, sierra:         "af78a79c6b0dbf5267781eb209cc3115f43dcdfd7a389c2740262bbab3be3c20"
     sha256 cellar: :any_skip_relocation, el_capitan:     "b9ab067b3001c71dc5cfa3085bfcd204cb4837fd6c87f5ce722bd77b8a629850"
-    sha256 cellar: :any_skip_relocation, yosemite:       "b9e7653e9f2c14aad3d4f3589bed6de036e78b766bc01be5ae9f24be0d9696c4"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "ca20a448bdeb6f1db4f657235b39a74e3a26ac00474ee196325c494c0beabe67"
   end
 
