3f41f5bce832be7dd3bbfd8c25ca2bd6a1f80136
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:44:42 +0800

    icon-naming-utils: no rebuilding required

--- Formula/icon-naming-utils.rb
+++ Formula/icon-naming-utils.rb
@@ -16,7 +16,6 @@ class IconNamingUtils < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:   "1ab22bc216fc60fe05436993a1d451542a5f57a12ecf835c85f5c850574e54f3"
     sha256 cellar: :any_skip_relocation, sierra:        "d824a2df63a9615bb242c197af07ce18f6a6a046df9c785fe31d5f39d986f4ed"
     sha256 cellar: :any_skip_relocation, el_capitan:    "f8a29d74289a555ba7969b8d8f6984de7251393d7d0270e61abf69d36f270fc0"
-    sha256 cellar: :any_skip_relocation, yosemite:      "22a80ba1d9042481251c8b85aa46dc163abfd9a54d6815ab43a3a5124592be5f"
     sha256 cellar: :any_skip_relocation, all:           "0d9b0891567661143495e9cb87f7811d66a7e980e26d403fdbd3485590f9bbf7"
   end
 
