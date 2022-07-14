4bd2b1368d1a21669cc2eb6002a835ddddf12ee2
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:45:09 +0800

    libident: no rebuilding required

--- Formula/libident.rb
+++ Formula/libident.rb
@@ -19,7 +19,6 @@ class Libident < Formula
     sha256 cellar: :any,                 high_sierra:    "f6bc989df22a80f3b0f8c6a2d458b5a00d9a4d48247cb7892877bd287e804a50"
     sha256 cellar: :any,                 sierra:         "4fb8a991f9f83d499b32a814b2d68465327b7b77c0108764e58e4296a968100f"
     sha256 cellar: :any,                 el_capitan:     "6236d3b4ee424795859cc64da30997ff67f7ac5bd42702e8eabe10f99339ca48"
-    sha256 cellar: :any,                 yosemite:       "53db8e889d8efa34b4a3b6a145bcec2bcb53595e7db0cfdd55c8d857dff3a442"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "f80a93f7750a66e987f21af7db62ba4f72c2c277036049915d3c8e6a8b044cf2"
   end
 
