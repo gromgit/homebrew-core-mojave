f522c9946823aa4c3e2ddc2fba20f5d0cdf1bd81
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:42:58 +0800

    argp-standalone: no rebuilding required

--- Formula/argp-standalone.rb
+++ Formula/argp-standalone.rb
@@ -20,7 +20,6 @@ class ArgpStandalone < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "92532fafd8c2cc86b33de0f347496746d8049bb4d1a6ce0948148e0f3c4bca5a"
     sha256 cellar: :any_skip_relocation, sierra:         "10627e72c0e0eb66cbd03a2beb767c06b8edad4bef01914de7f7c6c1be33a356"
     sha256 cellar: :any_skip_relocation, el_capitan:     "798e6ddb78957f9ad33662287b5971aaf3a43f3646e84691d56b3b85ca06d47f"
-    sha256 cellar: :any_skip_relocation, yosemite:       "c926ac0ad3b8dbb8c3e08299ade556470f81d3a88eb51dc60e7cfe107da533e8"
   end
 
   depends_on :macos # argp is provided by glibc on Linux
