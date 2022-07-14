f1b95e083a5790335492815bb5e66c78d63d1855
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:44:38 +0800

    hilite: no rebuilding required

--- Formula/hilite.rb
+++ Formula/hilite.rb
@@ -15,7 +15,6 @@ class Hilite < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "0266c20b3be69d398a14f1cb9332d34fe9ffca36c827b6e3681b636c0eab6073"
     sha256 cellar: :any_skip_relocation, sierra:         "b4bab9fd50310b7401d2605b9a9cdad0a8d7069e5b477d5f53fef61847bea624"
     sha256 cellar: :any_skip_relocation, el_capitan:     "2c407d12952089ade6602be85acda46eceb3127e32ba2068c0034df8a486e989"
-    sha256 cellar: :any_skip_relocation, yosemite:       "1eea4240f83568d245aa55801949cd9deb4663fc26df75569e029cc1c4c14112"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "45f0c131886b8932ee87061c612877ff71fa38650e81340eab5694de95e6cb40"
   end
 
