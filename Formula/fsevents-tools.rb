87544ec70ae78fb089606aa7c9c42f1bfa47d1fa
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:44:15 +0800

    fsevents-tools: no rebuilding required

--- Formula/fsevents-tools.rb
+++ Formula/fsevents-tools.rb
@@ -20,7 +20,6 @@ class FseventsTools < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "5dcd2647ccf02d20d42f3df513901714185ab717857cdb42a7f4a1e908ec7aae"
     sha256 cellar: :any_skip_relocation, sierra:         "8190bdd8d9d88f9ee6ebdc56434551a44a39502225160ad2604562d9c7ed0822"
     sha256 cellar: :any_skip_relocation, el_capitan:     "a15d1b927a74d8b76b553d696343ac5fe82396f8b5b052e15adc6c3270f2ccd6"
-    sha256 cellar: :any_skip_relocation, yosemite:       "9d4bda3175c503f8ff65f134495de43471854eacc3396a9cf1895d5c2b91cb63"
   end
 
   head do
