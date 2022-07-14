06c55a95bba412803bbaf3353b7dbab979ae0516
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:46:00 +0800

    nettoe: no rebuilding required

--- Formula/nettoe.rb
+++ Formula/nettoe.rb
@@ -15,7 +15,6 @@ class Nettoe < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "0349c1335e428d5f0b620043259908b5af60feed84d9dea911033e0d65704488"
     sha256 cellar: :any_skip_relocation, sierra:         "49ad705043bdd9f1ab860d877d3ffba584bef5ddbd4c03f6fe43adc49b9c1e5d"
     sha256 cellar: :any_skip_relocation, el_capitan:     "c8208683e4730233147e6c7153a469cdc1f477aacde0559937f0da93c8ad0345"
-    sha256 cellar: :any_skip_relocation, yosemite:       "78038d253cd382f5f3a6b3b12c7776828c44c1572f0569bec862763aa5141c2a"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "f72b5ebf9338c0c725ee8df4e6843ec8f9a13fbb9935e3bd121ce8302c3ecbbc"
   end
 
