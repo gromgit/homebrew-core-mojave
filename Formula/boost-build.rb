189cf61d316d35d6eece438d875fae77d9ad594c
Adrian Ho <the.gromgit@gmail.com>
2022-07-14 23:33:34 +0800

    boost-build: update 1.79.0 bottle.

--- Formula/boost-build.rb
+++ Formula/boost-build.rb
@@ -1,8 +1,8 @@
 class BoostBuild < Formula
   desc "C++ build system"
   homepage "https://www.boost.org/build/"
-  url "https://github.com/boostorg/build/archive/boost-1.78.0.tar.gz"
-  sha256 "78ba74587c8ba137d2f5f5ee1472e49d6291842ac944e2c24012c9e8a3d2f664"
+  url "https://github.com/boostorg/build/archive/boost-1.79.0.tar.gz"
+  sha256 "3e46a4b8d54603acdb478c15e791de772678d2c002711e41187b2d9b7fb55418"
   license "BSL-1.0"
   version_scheme 1
   head "https://github.com/boostorg/build.git", branch: "develop"
@@ -14,9 +14,8 @@ class BoostBuild < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/boost-build"
-    sha256 cellar: :any_skip_relocation, mojave: "524c42ff12c15b3327d5fd175bf942171cfcd170dbcbe905b03bc1c01f9b9c6e"
+    sha256 cellar: :any_skip_relocation, mojave: "aa105e3b7147386f25677c2641f32518ae3b6d99ff68252b75a3ba0e6b49c10f"
   end
-
 
   conflicts_with "b2-tools", because: "both install `b2` binaries"
 
