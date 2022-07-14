817043b1749dae7342bcc99f5a77c02a8431c1b1
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-14 10:46:48 +0800

    rpl: no rebuilding required

--- Formula/rpl.rb
+++ Formula/rpl.rb
@@ -15,7 +15,6 @@ class Rpl < Formula
     sha256 cellar: :any_skip_relocation, high_sierra:    "70b23d5ce18f2dfe58e8c782a00e4ab56d88c1e43b135c9e9ba0c8c387bef470"
     sha256 cellar: :any_skip_relocation, sierra:         "2c9e55b51762d835db949c20f9eba36e83213082db82c69602658e2f28003b80"
     sha256 cellar: :any_skip_relocation, el_capitan:     "175e1f127c8c707b0d90c3c7e4399cc5c1e18410bf8b7f6ec9340dbca4c16e4b"
-    sha256 cellar: :any_skip_relocation, yosemite:       "d718355e56dd13c690f1d5a0541b5f051518f65b953aade9c525853a19266a61"
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "d343c559dc0109ebc9ea3c19568ab9a6aa222feeb21c3b94e6f17c98f67f76e3"
   end
 
