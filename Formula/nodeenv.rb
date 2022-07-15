d205ec9a8a300ecc607b27f864eaa54fefebf7e0
Adrian Ho <the.gromgit@gmail.com>
2022-07-15 12:35:02 +0800

    nodeenv: update 1.7.0 bottle.

--- Formula/nodeenv.rb
+++ Formula/nodeenv.rb
@@ -1,15 +1,22 @@
 class Nodeenv < Formula
+  include Language::Python::Shebang
+
   desc "Node.js virtual environment builder"
   homepage "https://github.com/ekalinin/nodeenv"
-  url "https://github.com/ekalinin/nodeenv/archive/1.6.0.tar.gz"
-  sha256 "87b04b0832d38bd825e004e7c8ed82d75524efaa6b8a72e3a55088d93e4e17f7"
+  url "https://github.com/ekalinin/nodeenv/archive/1.7.0.tar.gz"
+  sha256 "a9e9e36e1be6439e877c53e7f27ce068f75b82cc08201f2c68471687199cfd7b"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "1e5b23648954dc4570499e605ed8ea5d3eb6124b1cc776b98ee9b4601872aec7"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nodeenv"
+    sha256 cellar: :any_skip_relocation, mojave: "b62820fdf34eb99d433736cc707c41a506f404ff2043418b62a4ab47a46fe99d"
   end
 
+  uses_from_macos "python"
+
   def install
+    rw_info = OS.mac? ? python_shebang_rewrite_info("/usr/bin/env python3") : detected_python_shebang
+    rewrite_shebang rw_info, "nodeenv.py"
     bin.install "nodeenv.py" => "nodeenv"
   end
 
