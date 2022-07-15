335bfe76a75ca9ac5890ec71ab1388894922ea1d
Adrian Ho <the.gromgit@gmail.com>
2022-07-15 13:13:09 +0800

    re-flex: update 3.2.8 bottle.

--- Formula/re-flex.rb
+++ Formula/re-flex.rb
@@ -1,13 +1,13 @@
 class ReFlex < Formula
   desc "Regex-centric, fast and flexible scanner generator for C++"
   homepage "https://www.genivia.com/doc/reflex/html"
-  url "https://github.com/Genivia/RE-flex/archive/v3.2.7.tar.gz"
-  sha256 "e1184d98ad7ae7d8b36581058218f8c42592393506f49dcc2de0c6e09946154a"
+  url "https://github.com/Genivia/RE-flex/archive/v3.2.8.tar.gz"
+  sha256 "9dc3729080377f5a67b44630010ae76f45324917a8820cb6a240702a4d6fe7c8"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/re-flex"
-    sha256 cellar: :any_skip_relocation, mojave: "68841c5195e8ea6207e9a77298c44b93f5ef797fced81990773aeceb4d97cf9e"
+    sha256 cellar: :any_skip_relocation, mojave: "71aae9e828e8b56fc0064aa2342b3f92af5f5e61d4ba7e5cd300edead05eba03"
   end
 
   depends_on "pcre2"
