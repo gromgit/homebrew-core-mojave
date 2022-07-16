7b7ccfb5def9215b760be0fbce4f75391e01928d
Adrian Ho <the.gromgit@gmail.com>
2022-07-17 05:03:24 +0800

    dumpling: update 6.1.0 bottle.

---
+++ Formula/dumpling.rb
@@ -0,0 +1,42 @@
+class Dumpling < Formula
+  desc "Creating SQL dump from a MySQL-compatible database"
+  homepage "https://github.com/pingcap/tidb"
+  url "https://github.com/pingcap/tidb.git",
+      tag:      "v6.1.0",
+      revision: "1a89decdb192cbdce6a7b0020d71128bc964d30f"
+  license "Apache-2.0"
+  head "https://github.com/pingcap/tidb.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dumpling"
+    sha256 cellar: :any_skip_relocation, mojave: "8fb442f0db9c675472f068ea68c1acd11791a3263ad9ef26ddac0805fcf9fa57"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    project = "github.com/pingcap/tidb/dumpling"
+    ldflags = %W[
+      -s -w
+      -X #{project}/cli.ReleaseVersion=#{version}
+      -X #{project}/cli.BuildTimestamp=#{time.iso8601}
+      -X #{project}/cli.GitHash=#{Utils.git_head}
+      -X #{project}/cli.GitBranch=#{version}
+      -X #{project}/cli.GoVersion=go#{Formula["go"].version}
+    ]
+
+    system "go", "build", *std_go_args(ldflags: ldflags), "./dumpling/cmd/dumpling"
+  end
+
+  test do
+    output = shell_output("#{bin}/dumpling --database db 2>&1", 1)
+    assert_match "create dumper failed", output
+
+    assert_match "Release version: #{version}", shell_output("#{bin}/dumpling --version 2>&1")
+  end
+end
