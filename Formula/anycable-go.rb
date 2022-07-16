41036302ee05b9440018416298c56ade82ee1423
Adrian Ho <the.gromgit@gmail.com>
2022-07-16 16:27:23 +0800

    anycable-go: update 1.2.1 bottle.

--- Formula/anycable-go.rb
+++ Formula/anycable-go.rb
@@ -1,8 +1,8 @@
 class AnycableGo < Formula
   desc "WebSocket server with action cable protocol"
   homepage "https://github.com/anycable/anycable-go"
-  url "https://github.com/anycable/anycable-go/archive/v1.2.0.tar.gz"
-  sha256 "d8d411e078f0e3de754623fe4d68025d60c8a9fcd31a29972d8c2f72d2a8d541"
+  url "https://github.com/anycable/anycable-go/archive/v1.2.1.tar.gz"
+  sha256 "b14a2e0a453c121c4d7bb60189e360751c6e6e2d5f3f7ccb8e17dfa1b51b35f7"
   license "MIT"
   head "https://github.com/anycable/anycable-go.git", branch: "master"
 
@@ -13,9 +13,8 @@ class AnycableGo < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/anycable-go"
-    sha256 cellar: :any_skip_relocation, mojave: "86c2712b54ef9b5c0fc4b6e0c90f1564095fd7d949c3d22f58d325174d4bd944"
+    sha256 cellar: :any_skip_relocation, mojave: "ff414e5ac9e7a6194f296316f0243f051007d54357fdc795fe8cb45f12cb0738"
   end
-
 
   depends_on "go" => :build
 
@@ -29,8 +28,7 @@ class AnycableGo < Formula
       "-X github.com/anycable/anycable-go/utils.version=#{version}"
     end
 
-    system "go", "build", "-mod=vendor", *std_go_args(ldflags: ldflags),
-                          "-v", "github.com/anycable/anycable-go/cmd/anycable-go"
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/anycable-go"
   end
 
   test do
