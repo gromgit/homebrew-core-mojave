9a56e7151ddd20e71e1670ece0d1bc6338d0c8de
Adrian Ho <the.gromgit@gmail.com>
2022-07-17 04:21:12 +0800

    doctl: update 1.78.0 bottle.

--- Formula/doctl.rb
+++ Formula/doctl.rb
@@ -1,14 +1,14 @@
 class Doctl < Formula
   desc "Command-line tool for DigitalOcean"
   homepage "https://github.com/digitalocean/doctl"
-  url "https://github.com/digitalocean/doctl/archive/v1.77.0.tar.gz"
-  sha256 "56514dd2d50d74703aef2c184127804e1b08ff2216130125bd066255ebe3220e"
+  url "https://github.com/digitalocean/doctl/archive/v1.78.0.tar.gz"
+  sha256 "8b2fb24f00b98db8f6a730cf8d7703b2e0b8fd0abe23cd1a64e325d4e8f49ffe"
   license "Apache-2.0"
   head "https://github.com/digitalocean/doctl.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/doctl"
-    sha256 cellar: :any_skip_relocation, mojave: "cb5ff54c4d660d84be7a79eafcbbc6042e21a3d8adc8720236c209b1cb68d66e"
+    sha256 cellar: :any_skip_relocation, mojave: "909ae73f77a2803d8e65071219c8c702a961fbdcf4f8fcaddd97ced9cc4232a0"
   end
 
   depends_on "go" => :build
