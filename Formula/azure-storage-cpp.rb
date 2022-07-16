489eca9dc96a9007c3762c386fb2effefb5ff5f5
Adrian Ho <the.gromgit@gmail.com>
2022-07-16 23:49:38 +0800

    azure-storage-cpp: update 7.5.0_2 bottle.

--- Formula/azure-storage-cpp.rb
+++ Formula/azure-storage-cpp.rb
@@ -4,16 +4,11 @@ class AzureStorageCpp < Formula
   url "https://github.com/Azure/azure-storage-cpp/archive/v7.5.0.tar.gz"
   sha256 "446a821d115949f6511b7eb01e6a0e4f014b17bfeba0f3dc33a51750a9d5eca5"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "bf2dc9dc42639f6a174ed7a18fb098aebebd65fc8042b90a512a3d3c455b3cb1"
-    sha256 cellar: :any,                 arm64_big_sur:  "44c92708a069bfbb747e7710ce65e20262dfd4bb67311b832639ba201a72ea7a"
-    sha256 cellar: :any,                 monterey:       "04721a7d666cb92d8ac4b4484be3ee76d745cd1a5bd08cdae2683e1c15704a84"
-    sha256 cellar: :any,                 big_sur:        "2c530902c523c7ccdb2d4514145c5c86191e530b0d88d1594f5f2716f6fc87c7"
-    sha256 cellar: :any,                 catalina:       "161ddefac718948838ab878191c45a1b6180f05598ae121401ca63e3da1b672c"
-    sha256 cellar: :any,                 mojave:         "4abae4ebc74f4758f5be5fbc7846467eb5bf429b596c31eec32f53026d65251e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aaf06755b0fc3414ff7abe7b9a7bbfbc61d62c86f0512fbfbe5cb4023500a7ac"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/azure-storage-cpp"
+    sha256 cellar: :any, mojave: "2e035e5d0e11a39643b66ac1d4df356f189b7719c77bc7e562aa4434a8c50fd2"
   end
 
   depends_on "cmake" => :build
