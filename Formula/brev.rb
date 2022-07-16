a8c5d75652fc4a184fbf7974230820ab4a832e33
Adrian Ho <the.gromgit@gmail.com>
2022-07-17 00:42:59 +0800

    brev: update 0.6.87 bottle.

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -1,8 +1,8 @@
 class Brev < Formula
   desc "CLI tool for managing workspaces provided by brev.dev"
   homepage "https://docs.brev.dev"
-  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.80.tar.gz"
-  sha256 "1e8a246ce9211ad08f4be6d1f50696980b537a445b735c2193635409c31c6ba1"
+  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.87.tar.gz"
+  sha256 "f3f5c5902d62810885be7fc0501961df5120cd0ea21bd477806e13775e1e213b"
   license "MIT"
 
   livecheck do
@@ -12,7 +12,7 @@ class Brev < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/brev"
-    sha256 cellar: :any_skip_relocation, mojave: "2b7318358f9cf4842d866ab504333bff08d99fab8126211a9ab1fdb34b1a5528"
+    sha256 cellar: :any_skip_relocation, mojave: "34952ebf8756f1d01e309296e9e9376432b76a69367191d02d473b896449be9d"
   end
 
   depends_on "go" => :build
