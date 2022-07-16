ab38fe6d798711589812b63e1528b4b9d6753957
Adrian Ho <the.gromgit@gmail.com>
2022-07-17 01:24:39 +0800

    cfengine: update 3.20.0 bottle.

--- Formula/cfengine.rb
+++ Formula/cfengine.rb
@@ -1,8 +1,8 @@
 class Cfengine < Formula
   desc "Help manage and understand IT infrastructure"
   homepage "https://cfengine.com/"
-  url "https://cfengine-package-repos.s3.amazonaws.com/tarballs/cfengine-3.19.0.tar.gz"
-  sha256 "bb43b23f76e6bd4f54e250675cfe8a01376326fa2c6c63c2a9e9bd091685d867"
+  url "https://cfengine-package-repos.s3.amazonaws.com/tarballs/cfengine-3.20.0.tar.gz"
+  sha256 "5a2ec814137bde6cc1ee35a81a78544bbf0a18282cd4307404c3a36ed1f7c4c8"
   license all_of: ["BSD-3-Clause", "GPL-2.0-or-later", "GPL-3.0-only", "LGPL-2.0-or-later"]
 
   livecheck do
@@ -12,7 +12,7 @@ class Cfengine < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cfengine"
-    sha256 mojave: "0be6ab474690415f86880764f4f047d5208fa920bd9c40ecfc3aa4d3b78aa193"
+    sha256 mojave: "a32b367dee13f86bc734573754636c43cfb426d1e6720b20f9b9d336602a4c75"
   end
 
   depends_on "lmdb"
@@ -24,8 +24,8 @@ class Cfengine < Formula
   end
 
   resource "masterfiles" do
-    url "https://cfengine-package-repos.s3.amazonaws.com/tarballs/cfengine-masterfiles-3.18.1.tar.gz"
-    sha256 "b9f5554a9122861a9a13acb2e3920c2887c309f898685713f1a35ba5be741772"
+    url "https://cfengine-package-repos.s3.amazonaws.com/tarballs/cfengine-masterfiles-3.20.0.tar.gz"
+    sha256 "a408cc2d3f4dcd64b698fa327b384208e7852305ebd69c8b9f2354df2d75fc76"
   end
 
   def install
