2500a73a2e087ae0754ece8f61eaf6d0ca4a453a
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-16 10:53:42 +0800

    tfenv: no rebuilding required

--- Formula/tfenv.rb
+++ Formula/tfenv.rb
@@ -1,8 +1,8 @@
 class Tfenv < Formula
   desc "Terraform version manager inspired by rbenv"
   homepage "https://github.com/tfutils/tfenv"
-  url "https://github.com/tfutils/tfenv/archive/v2.2.3.tar.gz"
-  sha256 "0b42330aeed675ad3e5dd6063dbd1daabecb36180e515cca1c6e105dd7a1fa49"
+  url "https://github.com/tfutils/tfenv/archive/v3.0.0.tar.gz"
+  sha256 "463132e45a211fa3faf85e62fdfaa9bb746343ff1954ccbad91cae743df3b648"
   license "MIT"
   head "https://github.com/tfutils/tfenv.git", branch: "master"
 
@@ -12,7 +12,7 @@ class Tfenv < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "d58555c2a79b2425a495e4bcdf48297067578c42c863493845030ed8f51e8d53"
+    sha256 cellar: :any_skip_relocation, all: "4905c2390b0254348be44da1c4a05b3d8bf4d8704b94d16b739d64fd4709784b"
   end
 
   uses_from_macos "unzip"
