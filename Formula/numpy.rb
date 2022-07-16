e9b31991e4524b8b6b93e7af7c8ee6d02aebf2de
Adrian Ho <the.gromgit@gmail.com>
2022-07-16 15:55:44 +0800

    numpy: update 1.23.1 bottle.

--- Formula/numpy.rb
+++ Formula/numpy.rb
@@ -1,14 +1,14 @@
 class Numpy < Formula
   desc "Package for scientific computing with Python"
   homepage "https://www.numpy.org/"
-  url "https://files.pythonhosted.org/packages/03/c6/14a17e10813b8db20d1e800ff9a3a898e65d25f2b0e9d6a94616f1e3362c/numpy-1.23.0.tar.gz"
-  sha256 "bd3fa4fe2e38533d5336e1272fc4e765cabbbde144309ccee8675509d5cd7b05"
+  url "https://files.pythonhosted.org/packages/13/b1/0c22aa7ca1deda4915cdec9562f839546bb252eecf6ad596eaec0592bd35/numpy-1.23.1.tar.gz"
+  sha256 "d748ef349bfef2e1194b59da37ed5a29c19ea8d7e6342019921ba2ba4fd8b624"
   license "BSD-3-Clause"
   head "https://github.com/numpy/numpy.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/numpy"
-    sha256 cellar: :any, mojave: "6d2247052b6bee75af9d0547555bd78e07682501a5dfdc4a4b449b1af87df4c3"
+    sha256 cellar: :any, mojave: "ea3671655a5ea7fe492e8df150a4d1b45f5d8533b9782a25a77b9e8f161af721"
   end
 
   depends_on "gcc" => :build # for gfortran
@@ -22,32 +22,33 @@ class Numpy < Formula
   def pythons
     deps.map(&:to_formula)
         .select { |f| f.name.match?(/python@\d\.\d+/) }
+        .sort_by(&:version) # so that `bin/f2py` and `bin/f2py3` use python3.10
         .map(&:opt_bin)
         .map { |bin| bin/"python3" }
   end
 
   def install
-    openblas = Formula["openblas"].opt_prefix
+    openblas = Formula["openblas"]
     ENV["ATLAS"] = "None" # avoid linking against Accelerate.framework
-    ENV["BLAS"] = ENV["LAPACK"] = "#{openblas}/lib/#{shared_library("libopenblas")}"
+    ENV["BLAS"] = ENV["LAPACK"] = openblas.opt_lib/shared_library("libopenblas")
 
     config = <<~EOS
       [openblas]
       libraries = openblas
-      library_dirs = #{openblas}/lib
-      include_dirs = #{openblas}/include
+      library_dirs = #{openblas.opt_lib}
+      include_dirs = #{openblas.opt_include}
     EOS
 
     Pathname("site.cfg").write config
 
     pythons.each do |python|
-      xy = Language::Python.major_minor_version python
-      ENV.prepend_create_path "PYTHONPATH", Formula["libcython"].opt_libexec/"lib/python#{xy}/site-packages"
+      site_packages = Language::Python.site_packages(python)
+      ENV.prepend_path "PYTHONPATH", Formula["libcython"].opt_libexec/site_packages
 
-      system python, "setup.py", "build",
-             "--fcompiler=#{Formula["gcc"].opt_bin}/gfortran", "--parallel=#{ENV.make_jobs}"
+      system python, "setup.py", "build", "--fcompiler=#{Formula["gcc"].opt_bin}/gfortran",
+                                          "--parallel=#{ENV.make_jobs}"
       system python, *Language::Python.setup_install_args(prefix),
-                     "--install-lib=#{prefix/Language::Python.site_packages(python)}"
+                     "--install-lib=#{prefix/site_packages}"
     end
   end
 
