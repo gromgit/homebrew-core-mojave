d642c9c0ef4b0c21eceaec93ba6b94eda3649e5e
Adrian Ho <the.gromgit@gmail.com>
2022-07-18 23:44:14 +0800

    kyma-cli: update 2.4.2 bottle.

--- Formula/kyma-cli.rb
+++ Formula/kyma-cli.rb
@@ -1,14 +1,14 @@
 class KymaCli < Formula
   desc "Kyma command-line interface"
   homepage "https://kyma-project.io"
-  url "https://github.com/kyma-project/cli/archive/2.3.0.tar.gz"
-  sha256 "a7bf6261fb474b79cad5f6dd2dc9d342310f2daef9038ab52054d6b196e768ab"
+  url "https://github.com/kyma-project/cli/archive/2.4.2.tar.gz"
+  sha256 "c078ee797158c1f83115e01bb4731db9f7f13e0c5451b3134e81bb809fa30460"
   license "Apache-2.0"
   head "https://github.com/kyma-project/cli.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kyma-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "95a238eaaa05a7b103a1e425936434f036e4d5e059b651ddeb80c327ca603f3e"
+    sha256 cellar: :any_skip_relocation, mojave: "b0cd13dde3f16191708fd10f5006c914268915e123f58b2a8c361072535e2c4d"
   end
 
   depends_on "go" => :build
