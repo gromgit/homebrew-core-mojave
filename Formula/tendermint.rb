19c55b92d313d0000d91c4b0568a4625410b64d2
Adrian Ho <the.gromgit@gmail.com>
2022-07-19 07:36:18 +0800

    tendermint: update 0.35.8 bottle.

--- Formula/tendermint.rb
+++ Formula/tendermint.rb
@@ -1,14 +1,14 @@
 class Tendermint < Formula
   desc "BFT state machine replication for applications in any programming languages"
   homepage "https://tendermint.com/"
-  url "https://github.com/tendermint/tendermint/archive/v0.35.7.tar.gz"
-  sha256 "d56fa85c67239c156f849d8295a5f8c660eabecf3407674f0fc49fe68b1d3748"
+  url "https://github.com/tendermint/tendermint/archive/v0.35.8.tar.gz"
+  sha256 "cadf322e5dd0c36e755cb04c431d5f3b5ecae791639f32f4f7c2637c947dcda0"
   license "Apache-2.0"
   head "https://github.com/tendermint/tendermint.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tendermint"
-    sha256 cellar: :any_skip_relocation, mojave: "c4d4784a84eb1b5499e3140660fa1933ae8536da4e596134b986e1676ddf8c45"
+    sha256 cellar: :any_skip_relocation, mojave: "36e41368e202b6dacbbe1547f4bbfcd147244ca62d9417e2568b525721c891a2"
   end
 
   depends_on "go" => :build
