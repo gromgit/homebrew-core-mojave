f3694a8f1bbc7c9cb4a2e4ca324db6d5237cbcd3
Adrian Ho <the.gromgit@gmail.com>
2022-07-18 15:00:09 +0800

    jump: update 0.50.0 bottle.

--- Formula/jump.rb
+++ Formula/jump.rb
@@ -1,14 +1,14 @@
 class Jump < Formula
   desc "Helps you navigate your file system faster by learning your habits"
   homepage "https://github.com/gsamokovarov/jump"
-  url "https://github.com/gsamokovarov/jump/archive/v0.41.0.tar.gz"
-  sha256 "833151da3e9dc09190588c0a381116e3dfdf5b728869cb7b96681eb59c452b4c"
+  url "https://github.com/gsamokovarov/jump/archive/v0.50.0.tar.gz"
+  sha256 "7832e968c81659e3750b8ecaaa49eb769fff4a96e790e28ef3d1e479f11affb4"
   license "MIT"
   head "https://github.com/gsamokovarov/jump.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jump"
-    sha256 cellar: :any_skip_relocation, mojave: "e4342881db825ebf0a5a50bc02c9099917cb77632db0734250bf634e5ce8af4f"
+    sha256 cellar: :any_skip_relocation, mojave: "bf308c5992b0518637cf791a66fc81f0938f1b29e885e5f374fb7ec7077fec95"
   end
 
   depends_on "go" => :build
