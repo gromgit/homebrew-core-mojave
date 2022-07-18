165bd3b066a3a4856ea33f85d3ab76907aa3b342
Adrian Ho <the.gromgit@gmail.com>
2022-07-18 13:46:19 +0800

    httpx: update 1.2.3 bottle.

--- Formula/httpx.rb
+++ Formula/httpx.rb
@@ -1,14 +1,14 @@
 class Httpx < Formula
   desc "Fast and multi-purpose HTTP toolkit"
   homepage "https://github.com/projectdiscovery/httpx"
-  url "https://github.com/projectdiscovery/httpx/archive/v1.2.2.tar.gz"
-  sha256 "1e358cdd4106bb70706f9f31c584d6bcdc3b240334eb7c5dafad294baee9dc73"
+  url "https://github.com/projectdiscovery/httpx/archive/v1.2.3.tar.gz"
+  sha256 "c0c688705fdf16c250d8de5a20737c37a7b786effe9e764fbac6be05504d9e5e"
   license "MIT"
   head "https://github.com/projectdiscovery/httpx.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/httpx"
-    sha256 cellar: :any_skip_relocation, mojave: "fb94b3f8b70440e72b2be82abd06b9db437cc0c685aa7ccf66439f37fa1587b0"
+    sha256 cellar: :any_skip_relocation, mojave: "19cf43cb8b8f9c5a3088f9ade7faa17f4b65d532bb972bfcec7c2efecabfa012"
   end
 
   depends_on "go" => :build
