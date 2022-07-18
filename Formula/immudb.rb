8708465822d1da0cb02b3b9f688a647b7c23cc31
Adrian Ho <the.gromgit@gmail.com>
2022-07-18 14:24:16 +0800

    immudb: update 1.3.1 bottle.

--- Formula/immudb.rb
+++ Formula/immudb.rb
@@ -1,8 +1,8 @@
 class Immudb < Formula
   desc "Lightweight, high-speed immutable database"
   homepage "https://www.codenotary.io"
-  url "https://github.com/codenotary/immudb/archive/v1.3.0.tar.gz"
-  sha256 "e874a119d13244a430e3f2353209d84f93feb6bf1e30f77d7c360d8a6d652bf1"
+  url "https://github.com/codenotary/immudb/archive/v1.3.1.tar.gz"
+  sha256 "1c61ed3bd3bec5265d86a6adb61a8020b52b5ed180809ccf23faf078d24a329f"
   license "Apache-2.0"
 
   livecheck do
@@ -12,8 +12,7 @@ class Immudb < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/immudb"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "9950c0462fef3f77264219b521e8ed45e51948b56088187a9f11e77904661537"
+    sha256 cellar: :any_skip_relocation, mojave: "5ca9b5ff2681b327467c64b015fea11e8e63e2024520b149d83a11b861b3f085"
   end
 
   depends_on "go" => :build
