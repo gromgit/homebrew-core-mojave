c180ef7880d8cac4a00a59e2387a30e75580d734
Adrian Ho <the.gromgit@gmail.com>
2022-07-19 03:22:53 +0800

    pixie: update 0.7.16 bottle.

---
+++ Formula/pixie.rb
@@ -0,0 +1,36 @@
+class Pixie < Formula
+  desc "Observability tool for Kubernetes applications"
+  homepage "https://px.dev/"
+  url "https://github.com/pixie-io/pixie.git",
+      tag:      "release/cli/v0.7.16",
+      revision: "131962699cf7581f4ed73b643c37517db8f4d777"
+  license "Apache-2.0"
+  head "https://github.com/pixie-io/pixie.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pixie"
+    sha256 cellar: :any_skip_relocation, mojave: "b5aef563739bdb3f9a6f3f82889f2488f682eed2b10412798cf47a513386eb44"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    semver = build.head? ? "0.0.0-dev" : version
+    ldflags = %W[
+      -s -w
+      -X px.dev/pixie/src/shared/goversion.buildSCMRevision=#{Utils.git_short_head}
+      -X px.dev/pixie/src/shared/goversion.buildSCMStatus=Distribution
+      -X px.dev/pixie/src/shared/goversion.buildSemver=#{semver}
+      -X px.dev/pixie/src/shared/goversion.buildTimeStamp=#{time.to_i}
+      -X px.dev/pixie/src/shared/goversion.buildNumber=#{revision + bottle&.rebuild.to_i + 1}
+      -X px.dev/pixie/src/shared/goversion.builtBy=#{tap.user}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags, output: bin/"px"), "./src/pixie_cli"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/px version")
+    assert_match tap.user.to_s, shell_output("#{bin}/px version")
+    assert_match "You must be logged in to perform this operation.", shell_output("#{bin}/px deploy 2>&1", 1)
+  end
+end
