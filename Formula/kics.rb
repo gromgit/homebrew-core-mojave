4d861e5298ff9ede796560ba40b5ead9ef828594
Adrian Ho <the.gromgit@gmail.com>
2022-07-18 15:44:29 +0800

    kics: update 1.5.11 bottle.

--- Formula/kics.rb
+++ Formula/kics.rb
@@ -1,15 +1,19 @@
 class Kics < Formula
   desc "Detect vulnerabilities, compliance issues, and misconfigurations"
   homepage "https://kics.io/"
-  url "https://github.com/Checkmarx/kics/archive/refs/tags/v1.5.10-integrations.tar.gz"
-  version "1.5.10-integrations"
-  sha256 "7913464d8b09377415777d6979ddfd9c914c58651e73a65a100d089c3c1845f0"
+  url "https://github.com/Checkmarx/kics/archive/refs/tags/v1.5.11.tar.gz"
+  sha256 "2b3eca85e53046728b730aa731954bd3cddf932efd136753a1046ef5db3e46c5"
   license "Apache-2.0"
   head "https://github.com/Checkmarx/kics.git", branch: "master"
 
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kics"
-    sha256 cellar: :any_skip_relocation, mojave: "0d11496880196789cdd7f88347ec487c83847d234326b6f06372d7a997fcf4d6"
+    sha256 cellar: :any_skip_relocation, mojave: "19cd7989366667820b80e942c76092c9356b617c38584ff0d553dab81c161e6e"
   end
 
   depends_on "go" => :build
