02a4dca5e36c65e5ab58678f500dbbc463a8ec7f
Adrian Ho <the.gromgit@gmail.com>
2022-07-19 05:47:41 +0800

    smug: update 0.3.1 bottle.

--- Formula/smug.rb
+++ Formula/smug.rb
@@ -1,13 +1,13 @@
 class Smug < Formula
   desc "Automate your tmux workflow"
   homepage "https://github.com/ivaaaan/smug"
-  url "https://github.com/ivaaaan/smug/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "2659e312fe49b3129c30db6a72191ad986c900c1f8845b7bab1d09da45be8a4e"
+  url "https://github.com/ivaaaan/smug/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "0cc7ffe94e5f0a8b2a2ea61bb44429c6959ccf5a399a0eab6ab6c6fab3b81301"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/smug"
-    sha256 cellar: :any_skip_relocation, mojave: "3fb0d1108135742a15b92d749759386b4ef14d9b7e7293c53abf31d34d9cfb01"
+    sha256 cellar: :any_skip_relocation, mojave: "c54a4fc4afa0d2e52b719fb511eed281e7450b0f2454c5da107f4d8061ff84c2"
   end
 
   depends_on "go" => :build
