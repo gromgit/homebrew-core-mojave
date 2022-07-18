48a42c36d586633c93ee5c848fe18d3dbae5d993
Adrian Ho <the.gromgit@gmail.com>
2022-07-18 14:41:17 +0800

    ioctl: update 1.8.1 bottle.

--- Formula/ioctl.rb
+++ Formula/ioctl.rb
@@ -1,13 +1,13 @@
 class Ioctl < Formula
   desc "Command-line interface for interacting with the IoTeX blockchain"
   homepage "https://docs.iotex.io/"
-  url "https://github.com/iotexproject/iotex-core/archive/v1.8.0.tar.gz"
-  sha256 "0ff566ca690801e5bdce8d24532e23a5a6e7f1969ead1e6f519651b9fabac352"
+  url "https://github.com/iotexproject/iotex-core/archive/v1.8.1.tar.gz"
+  sha256 "53b284a4829c2eba2defa8d03d12dbe81e3808cf91a31bbef591fd8e16f8c610"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ioctl"
-    sha256 cellar: :any_skip_relocation, mojave: "ff0aeb87c577ba77d991b41bd1f4c07cf85d8cd09f4b6d891e25ab3c678ca0bc"
+    sha256 cellar: :any_skip_relocation, mojave: "2cb5f215afa2701fb5fef4c31657d63b7386f9608d4eedf5556cc11293173239"
   end
 
   depends_on "go" => :build
