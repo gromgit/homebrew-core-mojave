dfc7eee2f294f1321241b05933b391088ce0007a
Adrian Ho <the.gromgit@gmail.com>
2022-07-19 20:30:54 +0800

    gnu-apl: update 1.8 bottle.

--- Formula/gnu-apl.rb
+++ Formula/gnu-apl.rb
@@ -16,15 +16,9 @@ class GnuApl < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "b7b9eef4cbb52a83000246627a0160dd39852969d1cdd98f29442e24611d97c8"
-    sha256 arm64_big_sur:  "d5c8031c3c878eadcc74fb5353e54ff8413c51a8c62bc89a51e21542a77bf3a6"
-    sha256 monterey:       "bcdc76b18f964c04e738aeffc16850029324b40ca9aaad03cbed554be431635c"
-    sha256 big_sur:        "e8da710fdd9108e7fd6d11adf6b67f8e11bf6e7b027c09a6e334b1a68b258da0"
-    sha256 catalina:       "2a7717b6b60567eade10b30f473771f563ebd6a009c91e0522eab6497516e892"
-    sha256 mojave:         "9df4d2bfcfda74e10451b132d0c274265bb1e550a9d7829402913d7798a83c46"
-    sha256 high_sierra:    "d1d035cef7cb23ecde90146a8eae564fbbeba3546228618dc250581d5611a4ab"
-    sha256 sierra:         "cbb8043b314e3141b2a9e6e3121b7c797ca68298374d9c50f6d07447e5ea7ca5"
-    sha256 x86_64_linux:   "cbf69e533bf5625cf34375113fb2e3ff339d4e8d52fb550e84010eb87b1a7919"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gnu-apl"
+    rebuild 1
+    sha256 mojave: "ad46013b546483b1249e89fb35101baeb3230c31fa4f403c7e7787ac09acc7c6"
   end
 
   head do
@@ -38,11 +32,6 @@ class GnuApl < Formula
   depends_on "readline" # GNU Readline is required, libedit won't work
 
   def install
-    # Work around "error: no member named 'signbit' in the global namespace"
-    # encountered when trying to detect boost regex in configure
-    ENV.delete("SDKROOT") if DevelopmentTools.clang_build_version >= 900
-    ENV.delete("HOMEBREW_SDKROOT") if MacOS.version == :high_sierra
-
     system "autoreconf", "-fiv" if build.head?
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
