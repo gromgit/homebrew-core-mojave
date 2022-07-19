959d1901ab081d7a30eb4eab5c51a34976d73ad0
Adrian Ho <the.gromgit@gmail.com>
2022-07-19 21:47:35 +0800

    inform6: update 6.36-r4 bottle.

--- Formula/inform6.rb
+++ Formula/inform6.rb
@@ -1,15 +1,15 @@
 class Inform6 < Formula
   desc "Design system for interactive fiction"
   homepage "https://inform-fiction.org/inform6.html"
-  url "https://ifarchive.org/if-archive/infocom/compilers/inform6/source/inform-6.36-r2.tar.gz"
-  version "6.36-r2"
-  sha256 "aaf1b2b81ef07b2cff1f0936cec3d7b6fda9a163170468e81d2ba2458faa353d"
+  url "https://ifarchive.org/if-archive/infocom/compilers/inform6/source/inform-6.36-r4.tar.gz"
+  version "6.36-r4"
+  sha256 "9becbad0cc737e993a5fc969b9ee9689781e3884658d52b17db68dad55010f2d"
   license "Artistic-2.0"
   head "https://gitlab.com/DavidGriffith/inform6unix.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/inform6"
-    sha256 cellar: :any_skip_relocation, mojave: "a23ec4ba96464d26bc2f30ae0133ced34a114d794e6d9cebe783cc283384452f"
+    sha256 cellar: :any_skip_relocation, mojave: "53ce18a8bb701d9220a1ccf85891b614fc3a9bce782d2c1ae7f132b740036926"
   end
 
   resource "homebrew-test_resource" do
