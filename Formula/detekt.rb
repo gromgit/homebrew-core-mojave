f495be0d7f7bba5030245dffb13ead5356932209
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-19 10:37:55 +0800

    detekt: no rebuilding required

--- Formula/detekt.rb
+++ Formula/detekt.rb
@@ -1,8 +1,8 @@
 class Detekt < Formula
   desc "Static code analysis for Kotlin"
   homepage "https://github.com/detekt/detekt"
-  url "https://github.com/detekt/detekt/releases/download/v1.20.0/detekt-cli-1.20.0-all.jar"
-  sha256 "2e4f299cc50b010fa99f44893e0fca68ef1efb57f9e6c3758c7b021804d36c59"
+  url "https://github.com/detekt/detekt/releases/download/v1.21.0/detekt-cli-1.21.0-all.jar"
+  sha256 "67e1bb5f2748d1bedadc3c3c06c75a04f8197d71f4d1d26724a49a42edbfa3c2"
   license "Apache-2.0"
 
   livecheck do
@@ -11,14 +11,14 @@ class Detekt < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "0d0afd41343f5e70d9fb2ecdc70c82d9380e963114167b8de6b207fa4cefac27"
+    sha256 cellar: :any_skip_relocation, all: "e3b227d1d82c944a21062fd3f2220ba157014a821d9c554ccec91681e95067d9"
   end
 
-  depends_on "openjdk@11"
+  depends_on "openjdk@17"
 
   def install
     libexec.install "detekt-cli-#{version}-all.jar"
-    bin.write_jar_script libexec/"detekt-cli-#{version}-all.jar", "detekt", java_version: "11"
+    bin.write_jar_script libexec/"detekt-cli-#{version}-all.jar", "detekt", java_version: "17"
   end
 
   test do
