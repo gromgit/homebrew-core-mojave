af813081e8f679089c3ebc7df68936bc5e3f37f7
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-19 10:38:11 +0800

    wayland-protocols:

--- Formula/wayland-protocols.rb
+++ Formula/wayland-protocols.rb
@@ -1,8 +1,8 @@
 class WaylandProtocols < Formula
   desc "Additional Wayland protocols"
   homepage "https://wayland.freedesktop.org"
-  url "https://wayland.freedesktop.org/releases/wayland-protocols-1.25.tar.xz"
-  sha256 "f1ff0f7199d0a0da337217dd8c99979967808dc37731a1e759e822b75b571460"
+  url "https://wayland.freedesktop.org/releases/wayland-protocols-1.26.tar.xz"
+  sha256 "c553384c1c68afd762fa537a2569cc9074fe7600da12d3472761e77a2ba56f13"
   license "MIT"
 
   livecheck do
@@ -11,12 +11,13 @@ class WaylandProtocols < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c7bcb5453798a2f48c986fd5787dd85dc11fb733452c766848ee382ab1cab748"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5afcaa3b93991d228ab1a1f34ca51029218fdd19a125f6f291667282fe32d1d1"
   end
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => [:build, :test]
+  depends_on "python@3.10" => :build
   depends_on "wayland" => :build
   depends_on :linux
 
