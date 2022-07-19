c3b581471e055455d5269bb39986a208c80e68eb
Adrian Ho <the.gromgit@gmail.com>
2022-07-20 00:06:07 +0800

    libp11: update 0.4.12 bottle.

--- Formula/libp11.rb
+++ Formula/libp11.rb
@@ -1,18 +1,9 @@
 class Libp11 < Formula
   desc "PKCS#11 wrapper library in C"
   homepage "https://github.com/OpenSC/libp11/wiki"
+  url "https://github.com/OpenSC/libp11/releases/download/libp11-0.4.12/libp11-0.4.12.tar.gz"
+  sha256 "1e1a2533b3fcc45fde4da64c9c00261b1047f14c3f911377ebd1b147b3321cfd"
   license "LGPL-2.1-or-later"
-
-  stable do
-    url "https://github.com/OpenSC/libp11/releases/download/libp11-0.4.11/libp11-0.4.11.tar.gz"
-    sha256 "57d47a12a76fd92664ae30032cf969284ebac1dfc25bf824999d74b016d51366"
-
-    # Fix -flat_namespace being used on Big Sur and later.
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-      sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-    end
-  end
 
   livecheck do
     url :stable
@@ -20,14 +11,8 @@ class Libp11 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "af7746ed36af308756e2da0de41e9e1f3f49bd9e2166d5d414c2b457d902cc76"
-    sha256 cellar: :any,                 arm64_big_sur:  "8286261723f0d43eb331dfceffaa13057a23cb9acd6e12b21ccca834e3bbdad5"
-    sha256 cellar: :any,                 monterey:       "dd15244317b7a9c12f6b2716d17fcb0a9204366448ff263437e5d85adbe28c98"
-    sha256 cellar: :any,                 big_sur:        "b3887a8796d78e8cfe9a298490eebe9adc6459ed71963144dd057d2a1fd56f1b"
-    sha256 cellar: :any,                 catalina:       "9da63ed34ade8ca89b600207b22d9fcc9a707aee31e3325f73c5da473e1df481"
-    sha256 cellar: :any,                 mojave:         "14a94b35751b0b820206edecd55dd713079ea20a1e72b049d290b10283a80895"
-    sha256 cellar: :any,                 high_sierra:    "95fac2824261ddc121f443a372174131eb9f31fe784a724bee7667af1302bba8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a4d5c50beb2cec5ba748f25da094871a2c6547ab44a3752c388645e9f3e00f31"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libp11"
+    sha256 cellar: :any, mojave: "556ef73d9790cfe53a7f257edc84d180d799c3f562fefd939993ba453be723c7"
   end
 
   head do
