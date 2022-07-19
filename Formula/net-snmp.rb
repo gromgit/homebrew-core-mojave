66eccef4421e9f841b7e3ceb31ae1351ca0a147a
Adrian Ho <the.gromgit@gmail.com>
2022-07-20 00:44:13 +0800

    net-snmp: update 5.9.3 bottle.

--- Formula/net-snmp.rb
+++ Formula/net-snmp.rb
@@ -1,8 +1,8 @@
 class NetSnmp < Formula
   desc "Implements SNMP v1, v2c, and v3, using IPv4 and IPv6"
   homepage "http://www.net-snmp.org/"
-  url "https://downloads.sourceforge.net/project/net-snmp/net-snmp/5.9.1/net-snmp-5.9.1.tar.gz"
-  sha256 "eb7fd4a44de6cddbffd9a92a85ad1309e5c1054fb9d5a7dd93079c8953f48c3f"
+  url "https://downloads.sourceforge.net/project/net-snmp/net-snmp/5.9.3/net-snmp-5.9.3.tar.gz"
+  sha256 "2097f29b7e1bf3f1300b4bae52fa2308d0bb8d5d3998dbe02f9462a413a2ef0a"
   license "Net-SNMP"
   head "https://github.com/net-snmp/net-snmp.git", branch: "master"
 
@@ -12,13 +12,8 @@ class NetSnmp < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "d21afc167bfdcfb751d13cc5b7971f3f4348947c2a52133e97852c909d92011c"
-    sha256 arm64_big_sur:  "78fa5061c6ba9240160cacfaa7b1c2f526d3a2dd8d3121ea4f6ba5bacced8a86"
-    sha256 monterey:       "0c2d53594b23ca23a9f4f4d0e7a9511b9e84168cfbb2595a1d93455e52a9d1e7"
-    sha256 big_sur:        "263ce5cfee921c1a75b0427e19cb15be78d6f65b2f2630d04ea4f5aac087f435"
-    sha256 catalina:       "7eaea9810b5847062284f67e1ac83a8f96739a3d9dec0428237717467aeec312"
-    sha256 mojave:         "8c57e53e0e45997e91c0071b9e7ee245d8610f935731b1ec6738b141274593eb"
-    sha256 x86_64_linux:   "177521069687eb0366887e0fedb1ebfec14a28d3dd139830cb8eca0664bfdebe"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/net-snmp"
+    sha256 mojave: "0ae8cff8a6739432d4c92ba036d9f0cd7de060b9e10410d615d4c0b79c4122de"
   end
 
   keg_only :provided_by_macos
@@ -38,9 +33,6 @@ class NetSnmp < Formula
   end
 
   def install
-    # Workaround https://github.com/net-snmp/net-snmp/issues/226 in 5.9:
-    inreplace "agent/mibgroup/mibII/icmp.h", "darwin10", "darwin"
-
     args = [
       "--disable-debugging",
       "--prefix=#{prefix}",
