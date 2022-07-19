252c93d4e35e8aaa6630eb78f307c10df9d271b6
Adrian Ho <the.gromgit@gmail.com>
2022-07-19 18:19:50 +0800

    fetchmail: update 6.4.31 bottle.

--- Formula/fetchmail.rb
+++ Formula/fetchmail.rb
@@ -1,8 +1,8 @@
 class Fetchmail < Formula
   desc "Client for fetching mail from POP, IMAP, ETRN or ODMR-capable servers"
   homepage "https://www.fetchmail.info/"
-  url "https://downloads.sourceforge.net/project/fetchmail/branch_6.4/fetchmail-6.4.30.tar.xz"
-  sha256 "35824c20aa1d8d97e3f1611fef49b9f7f68396f7cbc4d3a807fed27ae9ea445a"
+  url "https://downloads.sourceforge.net/project/fetchmail/branch_6.4/fetchmail-6.4.31.tar.xz"
+  sha256 "904bf9247054df8e91b47b1a9fe15a31dda8478c25b5291da0424653744a28f7"
   license all_of: [
     "LGPL-2.1-or-later",
     "ISC",
@@ -18,15 +18,15 @@ class Fetchmail < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fetchmail"
-    sha256 mojave: "4d597a8100d08e46f78f276032c8ab838b8d8d7c0a19e9fb46d0f414d01f1867"
+    sha256 mojave: "9bd4b357daf706befca3c50887b47ce53919193a4362e1c5e3a0fb2f5b6f17ea"
   end
 
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
-                          "--with-ssl=#{Formula["openssl@1.1"].opt_prefix}"
+                          "--with-ssl=#{Formula["openssl@3"].opt_prefix}"
     system "make", "install"
   end
 
