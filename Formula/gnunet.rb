2091c6b6ae09d4b46b00a1db315c8f803a2608f2
Adrian Ho <the.gromgit@gmail.com>
2022-07-19 20:44:15 +0800

    gnunet: update 0.17.2 bottle.

--- Formula/gnunet.rb
+++ Formula/gnunet.rb
@@ -1,14 +1,14 @@
 class Gnunet < Formula
   desc "Framework for distributed, secure and privacy-preserving applications"
   homepage "https://gnunet.org/"
-  url "https://ftp.gnu.org/gnu/gnunet/gnunet-0.17.1.tar.gz"
-  mirror "https://ftpmirror.gnu.org/gnunet/gnunet-0.17.1.tar.gz"
-  sha256 "75de0a715e3e969286483ec6ae4b25a87365664d42cdfb606df8e746a15f1265"
+  url "https://ftp.gnu.org/gnu/gnunet/gnunet-0.17.2.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gnunet/gnunet-0.17.2.tar.gz"
+  sha256 "38b13b578e2490a99222757c64727deb97939fdf797107f986287c2944ee7541"
   license "AGPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gnunet"
-    sha256 cellar: :any, mojave: "3061f88d222eec869915440e62081fe6c24ebbfaecab9d7eec196ddfddf972a6"
+    sha256 cellar: :any, mojave: "cdf39f002e1b444ea2d4c648a11ad09dbb0a8bdc83219fb3c60ce4e8e45f3e93"
   end
 
   depends_on "pkg-config" => :build
