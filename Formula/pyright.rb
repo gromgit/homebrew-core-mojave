e3adaa84c524233ab94e60076810dec845473cc9
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-19 10:38:07 +0800

    pyright: no rebuilding required

--- Formula/pyright.rb
+++ Formula/pyright.rb
@@ -3,13 +3,13 @@ require "language/node"
 class Pyright < Formula
   desc "Static type checker for Python"
   homepage "https://github.com/microsoft/pyright"
-  url "https://registry.npmjs.org/pyright/-/pyright-1.1.260.tgz"
-  sha256 "835bf85a5cc2b710d34070233b3f9a9fab5c40b7111e7d6621ae2674eea5afe6"
+  url "https://registry.npmjs.org/pyright/-/pyright-1.1.261.tgz"
+  sha256 "7caca6978176f718d696bd0b66da210bffc1614ad9dc6d3fa8c277f679306cbe"
   license "MIT"
   head "https://github.com/microsoft/pyright.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "7991698bd63ab746ffc2f7d506d67f23800c62d3b37588d501b3e8bdd7d99ae8"
+    sha256 cellar: :any_skip_relocation, all: "8197bfc264c05b543947e2943b10e8bed141f93ae22467a67c3b63aa535d84cc"
   end
 
   depends_on "node"
