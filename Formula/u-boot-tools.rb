878e7fa9389fe3b894d136cf766f7a18c8d21102
Adrian Ho <the.gromgit@gmail.com>
2022-07-20 03:18:27 +0800

    u-boot-tools: update 2022.07 bottle.

--- Formula/u-boot-tools.rb
+++ Formula/u-boot-tools.rb
@@ -1,8 +1,8 @@
 class UBootTools < Formula
   desc "Universal boot loader"
   homepage "https://www.denx.de/wiki/U-Boot/"
-  url "https://ftp.denx.de/pub/u-boot/u-boot-2022.04.tar.bz2"
-  sha256 "68e065413926778e276ec3abd28bb32fa82abaa4a6898d570c1f48fbdb08bcd0"
+  url "https://ftp.denx.de/pub/u-boot/u-boot-2022.07.tar.bz2"
+  sha256 "92b08eb49c24da14c1adbf70a71ae8f37cc53eeb4230e859ad8b6733d13dcf5e"
   license all_of: ["GPL-2.0-only", "GPL-2.0-or-later", "BSD-3-Clause"]
 
   livecheck do
@@ -12,7 +12,7 @@ class UBootTools < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/u-boot-tools"
-    sha256 cellar: :any, mojave: "12bf66fc261963e433d028b62bc0132085708e331a72f1d04ba903ae336811da"
+    sha256 cellar: :any, mojave: "c0b6127d1d45f2ed7dd34c0e3360f8600bb2fa27d87f36b0e42e7afcdb48b18f"
   end
 
   depends_on "coreutils" => :build # Makefile needs $(gdate)
