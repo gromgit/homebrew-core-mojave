768f2986b0637c755693a10c3616f54cf34fd5be
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-19 10:37:58 +0800

    grpc-swift:

--- Formula/grpc-swift.rb
+++ Formula/grpc-swift.rb
@@ -1,8 +1,8 @@
 class GrpcSwift < Formula
   desc "Swift language implementation of gRPC"
   homepage "https://github.com/grpc/grpc-swift"
-  url "https://github.com/grpc/grpc-swift/archive/1.8.1.tar.gz"
-  sha256 "5e5b7e2ba34b28eaeb7696e7924eb0ed2a32b369fe73c579d82f10ce2b783416"
+  url "https://github.com/grpc/grpc-swift/archive/1.8.2.tar.gz"
+  sha256 "84643fb635aee686dba1887824de71a04ab5dd38bac9d7fafd1991e8d778c564"
   license "Apache-2.0"
   head "https://github.com/grpc/grpc-swift.git", branch: "main"
 
@@ -12,11 +12,11 @@ class GrpcSwift < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6f44a18eff6ac919275a4385c0259f6fd666d46ead052a425df41abc3538cfe1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "df4863e4318b2dff67b4c5697426130deaf691328998a7b3fa3630c4d216b5bc"
-    sha256 cellar: :any_skip_relocation, monterey:       "3d83e564edb05783a830faa14b0e999f0d8b97e7c5681687f880fc803d35758f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f3a7b44727521804f0d89c31187fda62eec5a159e0622e972a69c8ce4ebdcd4a"
-    sha256                               x86_64_linux:   "9b24e6565eeab12912d4c9bd8858a992106b1324125f9c9bf64044327d99de84"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fc69bd52b26588b694ec63a3c176cf07b6477c45f8a4012a65ebcc8f4644e97d"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "49a539ab346c24ac3816a022eb0ad10d769544740a63ac2251c9f2f662624c16"
+    sha256 cellar: :any_skip_relocation, monterey:       "d033b5f73063f443b7407385c4e0940010acfdbdb96dc2cc276813c80d2c4bb8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "784cebca195442c0d801268501387c35b8c12ccd32debccf35c26f2083fa29a6"
+    sha256                               x86_64_linux:   "acd08bce8b97ff11d619cb851c4b6bfae985ef2980618cde1442c73b56f63fe3"
   end
 
   depends_on xcode: ["12.5", :build]
