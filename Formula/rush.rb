536b17aff751d885a18b7d8a231bbc285a8930ae
Adrian Ho <the.gromgit@gmail.com>
2022-07-20 01:56:19 +0800

    rush: update 2.3 bottle.

--- Formula/rush.rb
+++ Formula/rush.rb
@@ -1,14 +1,14 @@
 class Rush < Formula
   desc "GNU's Restricted User SHell"
   homepage "https://www.gnu.org/software/rush/"
-  url "https://ftp.gnu.org/gnu/rush/rush-2.2.tar.xz"
-  mirror "https://ftpmirror.gnu.org/rush/rush-2.2.tar.xz"
-  sha256 "b1fb69dcd2b082cc5bca804307baeec4ed6da77f747df0066c7d1ad2c353797f"
+  url "https://ftp.gnu.org/gnu/rush/rush-2.3.tar.xz"
+  mirror "https://ftpmirror.gnu.org/rush/rush-2.3.tar.xz"
+  sha256 "8cae258247cd2623e856ea5e2c62cd7f09e9e3e043e6fc63bbd1abec3d3fdd93"
   license "GPL-3.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rush"
-    sha256 mojave: "e8d4d748c37dfb79b4299b4b6428f13eefdf9507d53f918ceb2e97e6142c9b3c"
+    sha256 mojave: "6256ccafd84e65f63108d57112c28dd560c08bfb225013e8b081286a9ab9acf7"
   end
 
   def install
