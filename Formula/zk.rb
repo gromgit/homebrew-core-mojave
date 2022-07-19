6712410d4c2b5ed959e8f0e33152c52c5ed9838e
Adrian Ho <the.gromgit@gmail.com>
2022-07-19 09:53:52 +0800

    zk: update 0.11.1 bottle.

--- Formula/zk.rb
+++ Formula/zk.rb
@@ -1,14 +1,14 @@
 class Zk < Formula
   desc "Plain text note-taking assistant"
   homepage "https://github.com/mickael-menu/zk"
-  url "https://github.com/mickael-menu/zk/archive/refs/tags/v0.10.1.tar.gz"
-  sha256 "ce4851be512c446658e0945574bea52531d25e481a4fdea632f31a488445db85"
+  url "https://github.com/mickael-menu/zk/archive/refs/tags/v0.11.1.tar.gz"
+  sha256 "c38e88cbf8a6b1a00df51e1d87d6dbff4d13279c18bfef6ed275e7dc28dce0d8"
   license "GPL-3.0-only"
   head "https://github.com/mickael-menu/zk.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/zk"
-    sha256 cellar: :any, mojave: "a7ab11eb77af6fbd87ace6beceda8ab853e4e3600bee2b0f430291bddb8fbe35"
+    sha256 cellar: :any, mojave: "e2708801c43225e50991aba46d3fee1be171f781526610e92debcfe27dcb98d4"
   end
 
   depends_on "go" => :build
