9fed1a7de6c16de2f107098d9da8ee8d237cd8ce
Adrian Ho <the.gromgit@gmail.com>
2022-07-20 01:03:33 +0800

    octosql: update 0.9.1 bottle.

--- Formula/octosql.rb
+++ Formula/octosql.rb
@@ -1,14 +1,14 @@
 class Octosql < Formula
   desc "SQL query tool to analyze data from different file formats and databases"
   homepage "https://github.com/cube2222/octosql/"
-  url "https://github.com/cube2222/octosql/archive/refs/tags/v0.8.0.tar.gz"
-  sha256 "f2d4ddb5fa4425fa4543f1ec03d1325c64cf4f39c613d927e97ff58244073b6a"
+  url "https://github.com/cube2222/octosql/archive/refs/tags/v0.9.1.tar.gz"
+  sha256 "128f9505d37a682193e6db4aa5424a1874fdb8d446c16dcbf08fc1c1bc1830c8"
   license "MPL-2.0"
   head "https://github.com/cube2222/octosql.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/octosql"
-    sha256 cellar: :any_skip_relocation, mojave: "7aa0f5a945dbecd3dc5e32520c307cf8b87479c00ff20b973ea1e546f6d8a0aa"
+    sha256 cellar: :any_skip_relocation, mojave: "ec30b35cfd737558daf981230c2beb1730950c2ae6447125b498466c004a374e"
   end
 
   depends_on "go" => :build
@@ -37,13 +37,13 @@ class Octosql < Formula
     EOS
 
     expected = <<~EOS
-      +-------------+-------------+--------------------------+
-      | test.field1 | test.field2 |       test.field3        |
-      +-------------+-------------+--------------------------+
-      | 'value'     |          42 | { <null>, 'eulav',       |
-      |             |             | 'value' }                |
-      | 'value'     |          42 | { 'eulav', 24, <null> }  |
-      +-------------+-------------+--------------------------+
+      +---------+--------+--------------------------+
+      | field1  | field2 |          field3          |
+      +---------+--------+--------------------------+
+      | 'value' |     42 | { <null>, 'eulav',       |
+      |         |        | 'value' }                |
+      | 'value' |     42 | { 'eulav', 24, <null> }  |
+      +---------+--------+--------------------------+
     EOS
 
     assert_equal expected, shell_output("#{bin}/octosql \"select * from test.json\"")
