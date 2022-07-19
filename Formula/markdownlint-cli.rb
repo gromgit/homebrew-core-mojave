abf9d490ab8bddf113983b09621657a100e7144f
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-19 10:38:03 +0800

    markdownlint-cli: no rebuilding required

--- Formula/markdownlint-cli.rb
+++ Formula/markdownlint-cli.rb
@@ -3,12 +3,12 @@ require "language/node"
 class MarkdownlintCli < Formula
   desc "CLI for Node.js style checker and lint tool for Markdown files"
   homepage "https://github.com/igorshubovych/markdownlint-cli"
-  url "https://registry.npmjs.org/markdownlint-cli/-/markdownlint-cli-0.31.1.tgz"
-  sha256 "11127bcf88647405e7df9c629d8e34fe6aeedf56cda2d8c4bfa33283936e6c04"
+  url "https://registry.npmjs.org/markdownlint-cli/-/markdownlint-cli-0.32.0.tgz"
+  sha256 "cf088721f7ac57a51ccfb01419b295c93bad5f56b5ca1b814a7137f1de21c47a"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "26519a6a4330fd29cd897e61f8af2070def21758b06517c136690a5404d07d5a"
+    sha256 cellar: :any_skip_relocation, all: "75e8c4c9c02e8d6ced4edfbfaff260018047c44772fb6868e63c4fa54ae62687"
   end
 
   depends_on "node"
