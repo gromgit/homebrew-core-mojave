8032fc842cf368d2b9f29a172b66a2d14a9a16f2
Adrian Ho <the.gromgit@gmail.com>
2022-07-20 20:10:27 +0800

    fabio: update 1.6.1 bottle.

--- Formula/fabio.rb
+++ Formula/fabio.rb
@@ -1,14 +1,14 @@
 class Fabio < Formula
   desc "Zero-conf load balancing HTTP(S) router"
   homepage "https://github.com/fabiolb/fabio"
-  url "https://github.com/fabiolb/fabio/archive/v1.6.0.tar.gz"
-  sha256 "2a3a678a3ee7a5415512a1b3b799352186859f5d4dd50330d1117b21a643c398"
+  url "https://github.com/fabiolb/fabio/archive/v1.6.1.tar.gz"
+  sha256 "dafb85fb89a8d23a8edc6e96da54c4bdc0b86fce936fa6378e9f49fa70a04793"
   license "MIT"
   head "https://github.com/fabiolb/fabio.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fabio"
-    sha256 cellar: :any_skip_relocation, mojave: "46afef891dc54aa23bef6521d2e88dbca4bde478f4717be6b52fb740f76c0635"
+    sha256 cellar: :any_skip_relocation, mojave: "731c287ef838cf3b86f5fd938ba4960ed470e07b97b9fc44d49d4e438a3e02d8"
   end
 
   depends_on "go" => :build
