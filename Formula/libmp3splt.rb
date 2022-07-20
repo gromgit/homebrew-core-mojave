83cd36f2ae87ac6c929b46848bdc2dded74548b1
Adrian Ho <the.gromgit@gmail.com>
2022-07-20 21:45:58 +0800

    libmp3splt: update 0.9.2_3 bottle.

--- Formula/libmp3splt.rb
+++ Formula/libmp3splt.rb
@@ -3,7 +3,7 @@ class Libmp3splt < Formula
   homepage "https://mp3splt.sourceforge.io"
   url "https://downloads.sourceforge.net/project/mp3splt/libmp3splt/0.9.2/libmp3splt-0.9.2.tar.gz"
   sha256 "30eed64fce58cb379b7cc6a0d8e545579cb99d0f0f31eb00b9acc8aaa1b035dc"
-  revision 2
+  revision 3
 
   # We check the "libmp3splt" directory page since versions aren't present in
   # the RSS feed as of writing.
@@ -15,7 +15,7 @@ class Libmp3splt < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libmp3splt"
-    sha256 mojave: "dc95c6fa3b11c5b9961398c54676734ac10bf05beee1efb3367c0efcf4e2d969"
+    sha256 mojave: "fd5d23bb39c3ea080de66dc294d8f9dc64ca556fc1e369b7665860fc5b7725c1"
   end
 
   depends_on "pkg-config" => :build
