38d3c4b35711aed7f36fd857c4bf0654df17f60e
Adrian Ho <the.gromgit@gmail.com>
2022-07-20 19:43:39 +0800

    webp: update 1.2.3 bottle.

--- Formula/webp.rb
+++ Formula/webp.rb
@@ -1,13 +1,13 @@
 class Webp < Formula
   desc "Image format providing lossless and lossy compression for web images"
   homepage "https://developers.google.com/speed/webp/"
-  url "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.2.2.tar.gz"
-  sha256 "7656532f837af5f4cec3ff6bafe552c044dc39bf453587bd5b77450802f4aee6"
+  url "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.2.3.tar.gz"
+  sha256 "f5d7ab2390b06b8a934a4fc35784291b3885b557780d099bd32f09241f9d83f9"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/webp"
-    sha256 cellar: :any, mojave: "c43110dea34006bbcc3cd2693405f71203c88f9a37567b43087e2635a7818b00"
+    sha256 cellar: :any, mojave: "96d347577b2ca0b7ccc5cc2d7fd9f27a5ffdcc99a7fa1432f7de8fd8f2f748b9"
   end
 
   head do
