fd3462eff326b7f5b526cf6bdfd8fc1522ca7461
Adrian Ho <the.gromgit@gmail.com>
2022-07-20 20:33:28 +0800

    google-benchmark: update 1.6.2 bottle.

--- Formula/google-benchmark.rb
+++ Formula/google-benchmark.rb
@@ -1,14 +1,14 @@
 class GoogleBenchmark < Formula
   desc "C++ microbenchmark support library"
   homepage "https://github.com/google/benchmark"
-  url "https://github.com/google/benchmark/archive/v1.6.1.tar.gz"
-  sha256 "6132883bc8c9b0df5375b16ab520fac1a85dc9e4cf5be59480448ece74b278d4"
+  url "https://github.com/google/benchmark/archive/v1.6.2.tar.gz"
+  sha256 "a9f77e6188c1cd4ebedfa7538bf5176d6acc72ead6f456919e5f464ef2f06158"
   license "Apache-2.0"
   head "https://github.com/google/benchmark.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/google-benchmark"
-    sha256 cellar: :any_skip_relocation, mojave: "0a61d9bad7744bcd63dcc6c85dbde918c32e399ae2e53927e10141c692fc5f9d"
+    sha256 cellar: :any_skip_relocation, mojave: "3f4724cbf63946ee068d6776e95c9f3e715d678a794d07b273f2cc421b719f12"
   end
 
   depends_on "cmake" => :build
