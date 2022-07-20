7f3c367f429a438eb37f60da291d4380e6640f8a
Adrian Ho <the.gromgit@gmail.com>
2022-07-20 21:40:18 +0800

    lefthook: update 1.0.5 bottle.

--- Formula/lefthook.rb
+++ Formula/lefthook.rb
@@ -1,14 +1,14 @@
 class Lefthook < Formula
   desc "Fast and powerful Git hooks manager for any type of projects"
   homepage "https://github.com/evilmartians/lefthook"
-  url "https://github.com/evilmartians/lefthook/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "a824e1e3a2426fffb50c6ad49b3761262df8dabdc371e8ca414af1b2b115bba7"
+  url "https://github.com/evilmartians/lefthook/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "6a14d0354ba635c8fc188c351e59dbc057d61de3dc6a8d6a17fb4cecde260114"
   license "MIT"
   head "https://github.com/evilmartians/lefthook.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lefthook"
-    sha256 cellar: :any_skip_relocation, mojave: "3ee53c200d0cb039e5a11582c35787bfb785e35a0b870e4c2f8a47499850e3dc"
+    sha256 cellar: :any_skip_relocation, mojave: "0d9f686fba0e554e250850920f3d6842a4913879af5b66ed8eab9dadc0910824"
   end
 
   # Bump to 1.18 on the next release, if possible.
