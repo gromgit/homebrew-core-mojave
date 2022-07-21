62ca6c849d1260add7954fcff4413d9338e3c661
Adrian Ho <the.gromgit@gmail.com>
2022-07-22 00:07:15 +0800

    auditbeat: update 8.3.2 bottle.

--- Formula/auditbeat.rb
+++ Formula/auditbeat.rb
@@ -2,14 +2,14 @@ class Auditbeat < Formula
   desc "Lightweight Shipper for Audit Data"
   homepage "https://www.elastic.co/products/beats/auditbeat"
   url "https://github.com/elastic/beats.git",
-      tag:      "v8.2.3",
-      revision: "7826dc5e91c6e6d2487e05d3a8298f49041cd5c2"
+      tag:      "v8.3.2",
+      revision: "45f722f492dcf1d13698c6cf618b339b1d4907be"
   license "Apache-2.0"
   head "https://github.com/elastic/beats.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/auditbeat"
-    sha256 cellar: :any_skip_relocation, mojave: "0dc2fe46c7bd10bf50efe1df5a1823a878a0a6d260a4ce3fd93fab83cafae6d2"
+    sha256 cellar: :any_skip_relocation, mojave: "26fe2d024f4bdea355b806dcc9202fcc77c2aff902f0b1744c99ec0008ddb1a6"
   end
 
   depends_on "go" => :build
