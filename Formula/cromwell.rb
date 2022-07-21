b9422ac41cf168e444853df83444687697ca8b1e
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-21 12:09:33 +0800

    cromwell: no rebuilding required

--- Formula/cromwell.rb
+++ Formula/cromwell.rb
@@ -1,12 +1,12 @@
 class Cromwell < Formula
   desc "Workflow Execution Engine using Workflow Description Language"
   homepage "https://github.com/broadinstitute/cromwell"
-  url "https://github.com/broadinstitute/cromwell/releases/download/81/cromwell-81.jar"
-  sha256 "c298cc7883decac906863c14b980fd61b75e746a1058fd77a464e2adb370aeca"
+  url "https://github.com/broadinstitute/cromwell/releases/download/82/cromwell-82.jar"
+  sha256 "9127b49e50770184d517aa9bdbb109f16b9dd545e91a2aa194be31bb093d6876"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "a0e80e11798ef77a7358dd6fc5f0f84491b42513a9de6c8327e5826c876020f0"
+    sha256 cellar: :any_skip_relocation, all: "c9e5a2c482b5a68e206e4dc6e187d0c60a0ee1e47308615ba30542cfdff11935"
   end
 
   head do
@@ -17,8 +17,8 @@ class Cromwell < Formula
   depends_on "openjdk"
 
   resource "womtool" do
-    url "https://github.com/broadinstitute/cromwell/releases/download/81/womtool-81.jar"
-    sha256 "7958d97715cb2b8d90532e3cf5338a47bfc8199ad6661d29c287f107d1653e43"
+    url "https://github.com/broadinstitute/cromwell/releases/download/82/womtool-82.jar"
+    sha256 "a87adc2eb2c907093eebb8313051ec07164ed14dfd5d39c02ce4e84ee26e2643"
   end
 
   def install
