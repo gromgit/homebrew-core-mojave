add2e47fdbfac04183cb8d1abec7f07324f88f68
Adrian Ho <the.gromgit@gmail.com>
2022-07-21 23:14:01 +0800

    glib: update 2.72.2_1 bottle.

--- Formula/glib.rb
+++ Formula/glib.rb
@@ -6,24 +6,26 @@ class Glib < Formula
   url "https://download.gnome.org/sources/glib/2.72/glib-2.72.2.tar.xz"
   sha256 "78d599a133dba7fe2036dfa8db8fb6131ab9642783fc9578b07a20995252d2de"
   license "LGPL-2.1-or-later"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/glib"
-    sha256 mojave: "6f5a871543ab6434f1866dd75bbc2beeceff8b789584c6f8eaf0a76c225334c7"
+    sha256 mojave: "28521103495de0df1e0f40f639871e97ce48e707d98125e4ab5dbc7eb184bba9"
   end
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
+  depends_on "python@3.10" => :build
   depends_on "gettext"
   depends_on "libffi"
   depends_on "pcre"
-  depends_on "python@3.9"
 
   on_linux do
     depends_on "util-linux"
   end
 
+  # Sync this with `glib-utils.rb`
   # replace several hardcoded paths with homebrew counterparts
   patch do
     url "https://raw.githubusercontent.com/Homebrew/formula-patches/43467fd8dfc0e8954892ecc08fab131242dca025/glib/hardcoded-paths.diff"
@@ -35,6 +37,7 @@ class Glib < Formula
       "@@HOMEBREW_PREFIX@@", HOMEBREW_PREFIX
 
     # Disable dtrace; see https://trac.macports.org/ticket/30413
+    # and https://gitlab.gnome.org/GNOME/glib/-/issues/653
     args = std_meson_args + %W[
       --default-library=both
       --localstatedir=#{var}
@@ -48,7 +51,6 @@ class Glib < Formula
       system "meson", *args, ".."
       system "ninja", "-v"
       system "ninja", "install", "-v"
-      rewrite_shebang detected_python_shebang, *bin.children
     end
 
     # ensure giomoduledir contains prefix, as this pkgconfig variable will be
@@ -57,6 +59,23 @@ class Glib < Formula
     inreplace lib/"pkgconfig/gio-2.0.pc",
               "giomoduledir=#{HOMEBREW_PREFIX}/lib/gio/modules",
               "giomoduledir=${libdir}/gio/modules"
+
+    # Delete python files because they are provided by `glib-utils`
+    python_extension_regex = /\.(py(?:[diwx])?|px[di]|cpython-(?:[23]\d{1,2})[-\w]*\.(so|dylib))$/i
+    python_shebang_regex = %r{^#! ?/usr/bin/(?:env )?python(?:[23](?:\.\d{1,2})?)?( |$)}
+    shebang_max_length = 28 # the length of "#! /usr/bin/env pythonx.yyy "
+    prefix.find do |f|
+      next unless f.file?
+
+      f.unlink if python_extension_regex.match?(f.basename) || python_shebang_regex.match?(f.read(shebang_max_length))
+    end
+
+    # Delete empty directories
+    # Note: We need to traversal the directories in reverse order (i.e. deepest first).
+    #       Also, we should put checking emptiness and deletion in a single loop.
+    #       `dirs.select(&:empty?).map(&:rmdir)` will not work because it will not delete
+    #       directories that only contain empty directories.
+    prefix.find.select(&:directory?).reverse_each { |d| d.rmdir if d.empty? }
 
     if OS.mac?
       # `pkg-config --libs glib-2.0` includes -lintl, and gettext itself does not
@@ -84,6 +103,13 @@ class Glib < Formula
 
   def post_install
     (HOMEBREW_PREFIX/"lib/gio/modules").mkpath
+  end
+
+  def caveats
+    <<~EOS
+      Python executables and libraries are no longer included with this formula, but they are available separately:
+        brew install glib-utils
+    EOS
   end
 
   test do
