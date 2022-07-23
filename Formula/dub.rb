4eaa78a4665cbbd74a9db7c0c9ebe8e702574c62
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-23 15:20:43 +0800

    dub: update 1.29.1 bottle.

--- Formula/dub.rb
+++ Formula/dub.rb
@@ -1,8 +1,8 @@
 class Dub < Formula
   desc "Build tool for D projects"
   homepage "https://code.dlang.org/getting_started"
-  url "https://github.com/dlang/dub/archive/v1.29.0.tar.gz"
-  sha256 "4adf1c9101f5489548645cb4edad4b291d031149f7d970df44e3fc614b46dd2f"
+  url "https://github.com/dlang/dub/archive/v1.29.1.tar.gz"
+  sha256 "7cf67f1359ac042ed98420933408dbbbfff39d27021c086d271ee2cf6301b15c"
   license "MIT"
   version_scheme 1
   head "https://github.com/dlang/dub.git", branch: "master"
@@ -14,7 +14,7 @@ class Dub < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dub"
-    sha256 cellar: :any_skip_relocation, mojave: "247ed2c6846564c9fc221057a43c2e9d7ac1dabb4788dd181f4d37e9e51a0436"
+    sha256 cellar: :any_skip_relocation, mojave: "1ef2d283f02ef24216ef6859eb0b56c2abdecc88ecb2fd0d0199136dba80f669"
   end
 
   depends_on "ldc" => :build
