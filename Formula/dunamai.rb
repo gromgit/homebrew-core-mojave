a12ce167d41139bb1cc00b3e2c82ac209effd72f
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-23 15:32:43 +0800

    dunamai: update 1.12.0 bottle.

---
+++ Formula/dunamai.rb
@@ -0,0 +1,41 @@
+class Dunamai < Formula
+  include Language::Python::Virtualenv
+
+  desc "Dynamic version generation"
+  homepage "https://github.com/mtkennerly/dunamai"
+  url "https://files.pythonhosted.org/packages/e6/4b/57276a5edd2ddbd89f00d08ee78419f6c6432a3090125cb41d472d5155c8/dunamai-1.12.0.tar.gz"
+  sha256 "fac4f09e2b8a105bd01f8c50450fea5aa489a6c439c949950a65f0dd388b0d20"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dunamai"
+    sha256 cellar: :any_skip_relocation, mojave: "e8867b01c7fcff6a8ac619f469fb50766b44fefc4d5f29f49d5df8dd625169dd"
+  end
+
+  depends_on "python@3.10"
+
+  resource "packaging" do
+    url "https://files.pythonhosted.org/packages/df/9e/d1a7217f69310c1db8fdf8ab396229f55a699ce34a203691794c5d1cad0c/packaging-21.3.tar.gz"
+    sha256 "dd47c42927d89ab911e606518907cc2d3a1f38bbd026385970643f9c5b8ecfeb"
+  end
+
+  resource "pyparsing" do
+    url "https://files.pythonhosted.org/packages/71/22/207523d16464c40a0310d2d4d8926daffa00ac1f5b1576170a32db749636/pyparsing-3.0.9.tar.gz"
+    sha256 "2b020ecf7d21b687f219b71ecad3631f644a47f01403fa1d1036b0c6416d70fb"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system "git", "init"
+    system "git", "config", "user.name", "BrewTestBot"
+    system "git", "config", "user.email", "BrewTestBot@test.com"
+    touch "foo"
+    system "git", "add", "foo"
+    system "git", "commit", "-m", "bar"
+    system "git", "tag", "v0.1"
+    assert_equal "0.1", shell_output("#{bin}/dunamai from any").chomp
+  end
+end
