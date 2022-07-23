434e50d3e19411b59148a179495495ef0d3b0f40
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-23 14:04:44 +0800

    docutils: update 0.19 bottle.

--- Formula/docutils.rb
+++ Formula/docutils.rb
@@ -3,14 +3,13 @@ class Docutils < Formula
 
   desc "Text processing system for reStructuredText"
   homepage "https://docutils.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/docutils/docutils/0.18.1/docutils-0.18.1.tar.gz"
-  sha256 "679987caf361a7539d76e584cbeddc311e3aee937877c87346f31debc63e9d06"
+  url "https://downloads.sourceforge.net/project/docutils/docutils/0.19/docutils-0.19.tar.gz"
+  sha256 "33995a6753c30b7f577febfc2c50411fec6aac7f7ffeb7c4cfe5991072dcf9e6"
   license all_of: [:public_domain, "BSD-2-Clause", "GPL-3.0-or-later", "Python-2.0"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/docutils"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "cf6d117a6061f9be939bb6c0110d1800d839c7852715a76bf31f5481c80e0c0e"
+    sha256 cellar: :any_skip_relocation, mojave: "ae5ef91d15f574fd9adeb50a5b1023c45f56fb7eed85ac270fdd0fcc401b41e3"
   end
 
   depends_on "python@3.10"
