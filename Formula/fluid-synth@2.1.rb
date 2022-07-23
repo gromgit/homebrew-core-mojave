ef971ec069f9ff7c90eab735ac80c54b3bf7a927
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-23 17:13:42 +0800

    fluid-synth@2.1: update 2.1.9_1 bottle.

--- Formula/fluid-synth@2.1.rb
+++ Formula/fluid-synth@2.1.rb
@@ -4,15 +4,11 @@ class FluidSynthAT21 < Formula
   url "https://github.com/FluidSynth/fluidsynth/archive/v2.1.9.tar.gz"
   sha256 "365642cc64bafe0491149ad643ef7327877f99412d5abb93f1fa54e252028484"
   license "LGPL-2.1-or-later"
+  revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "23918636b21c7d2f4c617fc58fc9bfbdaffbe371be38a9b6ce1062af4dfc92b5"
-    sha256 cellar: :any,                 arm64_big_sur:  "d25c897f88f0f35bce794a51ee17db1056177a3b23a9f35c2c88542d6565bd67"
-    sha256 cellar: :any,                 monterey:       "6dc51778ae06ec9e0cfd55fc8b23587e0de968adbb26195961ec225c3af82a80"
-    sha256 cellar: :any,                 big_sur:        "3d2d82d9ff0d7ae9df77e14ee1ca9e160ba2cbc0d7cc6b96b1ec2de872472362"
-    sha256 cellar: :any,                 catalina:       "f96a93cc69dbc29048d04a00f6034b1ef3df1f088b9a893ac0744b5d11ba6189"
-    sha256 cellar: :any,                 mojave:         "553ad2667f43d1e0a45d7393fb1452de3459832603d792caab7b7d784bc95857"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2ebdb995ed2864a7ca079cc41392d7b1abc3740ba4ce70e7531978bdaaa72770"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fluid-synth@2.1"
+    sha256 cellar: :any, mojave: "7ccfda7080290e2792d7ab59e62d6a13f6ae2639829854f783dc32509fba0018"
   end
 
   keg_only :versioned_formula
@@ -22,6 +18,7 @@ class FluidSynthAT21 < Formula
   depends_on "glib"
   depends_on "libsndfile"
   depends_on "portaudio"
+  depends_on "readline"
 
   resource "example_midi" do
     url "https://upload.wikimedia.org/wikipedia/commons/6/61/Drum_sample.mid"
