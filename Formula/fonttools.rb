62f5852d46fc40fce6206d06f42ecadadfb4f86d
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-24 23:19:54 +0800

    fonttools: update 4.34.4_1 bottle.

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -3,17 +3,23 @@ class Fonttools < Formula
 
   desc "Library for manipulating fonts"
   homepage "https://github.com/fonttools/fonttools"
-  url "https://files.pythonhosted.org/packages/de/54/14376a4e5c1e7d2105a5be33ad5584b56e36753dc615506728a1489071cd/fonttools-4.33.3.zip"
-  sha256 "c0fdcfa8ceebd7c1b2021240bd46ef77aa8e7408cf10434be55df52384865f8e"
+  url "https://files.pythonhosted.org/packages/5a/a4/a97cff4c4af6764a04cc202299e5205b2e101cb1543bcaf9737be29f78ab/fonttools-4.34.4.zip"
+  sha256 "9a1c52488045cd6c6491fd07711a380f932466e317cb8e016fc4e99dc7eac2f0"
   license "MIT"
+  revision 1
   head "https://github.com/fonttools/fonttools.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fonttools"
-    sha256 cellar: :any_skip_relocation, mojave: "97e2a260bf9c29b0c35b389c2ccb5b502a98ef218f2ea7ca9cacfff3e0fcefa4"
+    sha256 cellar: :any_skip_relocation, mojave: "40040a184c32daf6dae0a738cc012a6b5f07cfbb1389917be7905601a413ef20"
   end
 
   depends_on "python@3.10"
+
+  resource "Brotli" do
+    url "https://files.pythonhosted.org/packages/2a/18/70c32fe9357f3eea18598b23aa9ed29b1711c3001835f7cf99a9818985d0/Brotli-1.0.9.zip"
+    sha256 "4d1b810aa0ed773f81dceda2cc7b403d01057458730e309856356d4ef4188438"
+  end
 
   def install
     virtualenv_install_with_resources
@@ -23,6 +29,7 @@ class Fonttools < Formula
     if OS.mac?
       cp "/System/Library/Fonts/ZapfDingbats.ttf", testpath
       system bin/"ttx", "ZapfDingbats.ttf"
+      system bin/"fonttools", "ttLib.woff2", "compress", "ZapfDingbats.ttf"
     else
       assert_match "usage", shell_output("#{bin}/ttx -h")
     end
