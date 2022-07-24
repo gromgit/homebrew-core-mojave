c31fee29a3de18d767072cc56577c9a6bea2be17
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-25 00:53:22 +0800

    git-cinnabar: update 0.5.9 bottle.

--- Formula/git-cinnabar.rb
+++ Formula/git-cinnabar.rb
@@ -1,15 +1,14 @@
 class GitCinnabar < Formula
   desc "Git remote helper to interact with mercurial repositories"
   homepage "https://github.com/glandium/git-cinnabar"
-  url "https://github.com/glandium/git-cinnabar/archive/0.5.8.tar.gz"
-  sha256 "7971c2ae17d2b919f915efab35e3aba583b951d53ca2bc6ebf69bbd0c22f1067"
+  url "https://github.com/glandium/git-cinnabar/archive/0.5.9.tar.gz"
+  sha256 "83374ff2c7e9ccbb5e866c6fb350ad3202cab4856841afbadaded1f1bef4d534"
   license "GPL-2.0-only"
-  revision 1
   head "https://github.com/glandium/git-cinnabar.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-cinnabar"
-    sha256 cellar: :any_skip_relocation, mojave: "1570bdcce06547e5120a47481d0b781c6b9fd95bee8ea15fbf3547d0088e50de"
+    sha256 cellar: :any_skip_relocation, mojave: "048ef97a25eb8dae8a8ea128eb0f458e289bb614fe22d6ed11750cc52f67a0bf"
   end
 
   depends_on "mercurial"
