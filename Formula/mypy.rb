9bb9d41ece314331bd4c7ce52346276b5fbc19d3
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-25 20:44:08 +0800

    mypy: update 0.971 bottle.

--- Formula/mypy.rb
+++ Formula/mypy.rb
@@ -3,13 +3,14 @@ class Mypy < Formula
 
   desc "Experimental optional static type checker for Python"
   homepage "http://www.mypy-lang.org/"
-  url "https://files.pythonhosted.org/packages/67/48/e73045183ce9824d98365f18255a79d0b01638f40a0a68f898dc8f3cebcc/mypy-0.961.tar.gz"
-  sha256 "f730d56cb924d371c26b8eaddeea3cc07d78ff51c521c6d04899ac6904b75492"
+  url "https://files.pythonhosted.org/packages/5e/66/00f7f751140fe6953603fb0cd56dee0314842cfe358884ca3025589ca81c/mypy-0.971.tar.gz"
+  sha256 "40b0f21484238269ae6a57200c807d80debc6459d444c0489a102d7c6a75fa56"
   license "MIT"
+  head "https://github.com/python/mypy.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mypy"
-    sha256 cellar: :any_skip_relocation, mojave: "f9efdec50a058c5d35d7fd4c5be879681f5ea1700edfd06611b468675507674a"
+    sha256 cellar: :any_skip_relocation, mojave: "a23b9c59b94397ec6cc60c7dba5f1bd0fadb038a2262a0b8e6d617f69e663d05"
   end
 
   depends_on "python@3.10"
@@ -25,8 +26,8 @@ class Mypy < Formula
   end
 
   resource "typing-extensions" do
-    url "https://files.pythonhosted.org/packages/fe/71/1df93bd59163c8084d812d166c907639646e8aac72886d563851b966bf18/typing_extensions-4.2.0.tar.gz"
-    sha256 "f1c24655a0da0d1b67f07e17a5e6b2a105894e6824b92096378bb3668ef02376"
+    url "https://files.pythonhosted.org/packages/9e/1d/d128169ff58c501059330f1ad96ed62b79114a2eb30b8238af63a2e27f70/typing_extensions-4.3.0.tar.gz"
+    sha256 "e6d2677a32f47fc7eb2795db1dd15c1f34eff616bcaf2cfb5e997f854fa1c4a6"
   end
 
   def install
