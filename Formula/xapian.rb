914912efe4f3b4dd401ab6ef498753a7ef31e5ad
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-25 20:20:33 +0800

    xapian: update 1.4.20 bottle.

--- Formula/xapian.rb
+++ Formula/xapian.rb
@@ -1,8 +1,8 @@
 class Xapian < Formula
   desc "C++ search engine library"
   homepage "https://xapian.org/"
-  url "https://oligarchy.co.uk/xapian/1.4.19/xapian-core-1.4.19.tar.xz"
-  sha256 "1fca48fca6cc3526cc4ba93dd194fe9c1326857b78edcfb37e68d086d714a9c3"
+  url "https://oligarchy.co.uk/xapian/1.4.20/xapian-core-1.4.20.tar.xz"
+  sha256 "ce2be5eff72075c8106c0340e70b1093dbcebe2ab42dc1c1be08dd3ad419442d"
   license "GPL-2.0-or-later"
   version_scheme 1
 
@@ -13,7 +13,7 @@ class Xapian < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/xapian"
-    sha256 cellar: :any, mojave: "1d50c0afc2d47c8b86d22f51eddd1c47d3ff904145b97fa0a5dadf9ab0bb79e5"
+    sha256 cellar: :any, mojave: "b2e81816cf53815a54823a9cd80f6c48a1587dd06da3ad16c2a32670003b726b"
   end
 
   depends_on "python@3.10" => [:build, :test]
@@ -28,8 +28,8 @@ class Xapian < Formula
   skip_clean :la
 
   resource "bindings" do
-    url "https://oligarchy.co.uk/xapian/1.4.19/xapian-bindings-1.4.19.tar.xz"
-    sha256 "91c385a48951aa7cdf665effd25533f7477fc22781ca712e50b5496459a2883d"
+    url "https://oligarchy.co.uk/xapian/1.4.20/xapian-bindings-1.4.20.tar.xz"
+    sha256 "786cc28d05660b227954413af0e2f66e4ead2a06d3df6dabaea484454b601ef5"
   end
 
   # Fix -flat_namespace being used on Big Sur and later.
