9a5767c6de0b22dbe1c8ad9e08297679aaca3271
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-25 20:03:58 +0800

    mongosh: update 1.5.1 bottle.

--- Formula/mongosh.rb
+++ Formula/mongosh.rb
@@ -3,13 +3,13 @@ require "language/node"
 class Mongosh < Formula
   desc "MongoDB Shell to connect, configure, query, and work with your MongoDB database"
   homepage "https://github.com/mongodb-js/mongosh#readme"
-  url "https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-1.5.0.tgz"
-  sha256 "bcabff0b1dc6a400ebe2114783b44fd91d000e31d48c81d760e2b8d0f157d2a2"
+  url "https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-1.5.1.tgz"
+  sha256 "4a9d093a4475960d3c692c5a5e7dc26da3c5ed099b81a211037ecd5de2b7881d"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mongosh"
-    sha256 mojave: "bdf52efbcdfa6428831cfa674a33a6f2fe6193f438375cf21fdc62aa884acc28"
+    sha256 mojave: "c4629c8da193fda6692ba3fe20cd2b66184f091ac27e2fb7e4a89aca5372f1bc"
   end
 
   depends_on "node@16"
