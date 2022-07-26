478fe97b8d4c7bea6a6a4aec95feec5bbefb11c7
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-26 23:09:14 +0800

    corepack: no rebuilding required

--- Formula/corepack.rb
+++ Formula/corepack.rb
@@ -3,8 +3,8 @@ class Corepack < Formula
 
   desc "Package acting as bridge between Node projects and their package managers"
   homepage "https://github.com/nodejs/corepack"
-  url "https://registry.npmjs.org/corepack/-/corepack-0.12.0.tgz"
-  sha256 "38accd581f3b7115082d74f385eefb36118adfe998ef13b3bc02ab87055430cd"
+  url "https://registry.npmjs.org/corepack/-/corepack-0.12.1.tgz"
+  sha256 "0f58d22ebb0b8893f3d9c7e62fea95dd831611c8a41d628c4bdbd9fd6f14be1c"
   license "MIT"
 
   livecheck do
@@ -13,7 +13,7 @@ class Corepack < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "e8c555f32b16b7ab18f9dfba2fdb7cdc081481ae8ae7e6f9fb409120a1ef37c2"
+    sha256 cellar: :any_skip_relocation, all: "5bc6cd0df5ff876dfb994d1c97407beea9c21f7ac22d34ab52d8ac817dbbd566"
   end
 
   depends_on "node"
