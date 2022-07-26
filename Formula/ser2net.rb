1735bce745eb8dc8f9583d8636e8c90205052962
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-26 15:13:07 +0800

    ser2net: update 4.3.7 bottle.

--- Formula/ser2net.rb
+++ Formula/ser2net.rb
@@ -1,8 +1,8 @@
 class Ser2net < Formula
   desc "Allow network connections to serial ports"
   homepage "https://ser2net.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/ser2net/ser2net/ser2net-4.3.6.tar.gz"
-  sha256 "65515c7e9a5289167ae64c4032450904449a87ce20653241022af4f5db2e9510"
+  url "https://downloads.sourceforge.net/project/ser2net/ser2net/ser2net-4.3.7.tar.gz"
+  sha256 "542915e240ae8b5c7dcec8d1589e6067818532900d45cfef226cea9f0e671d13"
   license "GPL-2.0-only"
 
   livecheck do
@@ -12,14 +12,14 @@ class Ser2net < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ser2net"
-    sha256 cellar: :any, mojave: "c79c0183b4454e868f00028327852ca9e821e89581cc0ea36cfb815a728ba8cc"
+    sha256 cellar: :any, mojave: "68baecd6ab362f957f872f08002aa62a51081057bd17561af2c5dc9167dba12a"
   end
 
   depends_on "libyaml"
 
   resource "gensio" do
-    url "https://downloads.sourceforge.net/project/ser2net/ser2net/gensio-2.3.2.tar.gz"
-    sha256 "0b6333a5546f14396041900bbe5b83575a0e97d200a581a6ddb8fcf6e95adfbd"
+    url "https://downloads.sourceforge.net/project/ser2net/ser2net/gensio-2.4.1.tar.gz"
+    sha256 "949438b558bdca142555ec482db6092eca87447d23a4fb60c1836e9e16b23ead"
 
     # Fix -flat_namespace being used on Big Sur and later.
     patch do
