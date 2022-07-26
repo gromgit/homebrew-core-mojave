92ae5a5d60e57d704dbb30474e594ce814c72b48
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-26 23:09:33 +0800

    parallel: no rebuilding required

--- Formula/parallel.rb
+++ Formula/parallel.rb
@@ -1,9 +1,9 @@
 class Parallel < Formula
   desc "Shell command parallelization utility"
   homepage "https://savannah.gnu.org/projects/parallel/"
-  url "https://ftp.gnu.org/gnu/parallel/parallel-20220622.tar.bz2"
-  mirror "https://ftpmirror.gnu.org/parallel/parallel-20220622.tar.bz2"
-  sha256 "a4bd15a8f53b20fd19b0b41b26206527c07a1dda28a7891d5253d856fe5fd5a0"
+  url "https://ftp.gnu.org/gnu/parallel/parallel-20220722.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/parallel/parallel-20220722.tar.bz2"
+  sha256 "0e4083ac0d850c434598c6dfbf98f3b6dd2cc932a3af9269eb1f9323e43af019"
   license "GPL-3.0-or-later"
   version_scheme 1
   head "https://git.savannah.gnu.org/git/parallel.git", branch: "master"
@@ -14,24 +14,26 @@ class Parallel < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "5ecefe46cb80644d4ff3f6b37d41bb39ce1bd1f81a078440be479c2917221417"
+    sha256 cellar: :any_skip_relocation, all: "f975128d36fcc257e985a374eca0677fa812283ba7f3203ccb157b549373eba7"
   end
 
   conflicts_with "moreutils", because: "both install a `parallel` executable"
 
   def install
     ENV.append_path "PATH", bin
+
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
 
     inreplace_files = [
       bin/"parallel",
-      doc/"parallel.texi",
       doc/"parallel_design.texi",
       man1/"parallel.1",
       man7/"parallel_design.7",
     ]
-    inreplace inreplace_files, "/usr/local", HOMEBREW_PREFIX
+
+    # Ignore `inreplace` failures when building from HEAD or not building a bottle.
+    inreplace inreplace_files, "/usr/local", HOMEBREW_PREFIX, build.stable? && build.bottle?
   end
 
   def caveats
