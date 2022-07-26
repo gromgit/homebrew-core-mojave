9858c66d88fb94e45317a377c90168c147c4a639
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-26 23:09:16 +0800

    dyld-headers: no rebuilding required

--- Formula/dyld-headers.rb
+++ Formula/dyld-headers.rb
@@ -1,12 +1,12 @@
 class DyldHeaders < Formula
   desc "Header files for the dynamic linker"
   homepage "https://opensource.apple.com/"
-  url "https://github.com/apple-oss-distributions/dyld/archive/refs/tags/dyld-957.tar.gz"
-  sha256 "4a632883e252f85f0aee16c9d07a47116315ee6a348ef4e7849b612112e55d19"
+  url "https://github.com/apple-oss-distributions/dyld/archive/refs/tags/dyld-960.tar.gz"
+  sha256 "4d5a878221ba7e099e1d8f0ff20e816fdad9fda3587e5c5c74e2a52fcc5c41d0"
   license "APSL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "051302c52294864242a6633a6147cbd7ab3ce0ca356f0c397ced58339057477a"
+    sha256 cellar: :any_skip_relocation, all: "d524dce0754fef77305ada7afabfdd6133bcd6f3c83b6fc222b5f96ba27b9fd6"
   end
 
   keg_only :provided_by_macos
