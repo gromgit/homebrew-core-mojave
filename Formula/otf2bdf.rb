078693c992a883b0a4e28cfa6bfc0576cba53b38
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-26 23:09:33 +0800

    otf2bdf: no rebuilding required

--- Formula/otf2bdf.rb
+++ Formula/otf2bdf.rb
@@ -23,8 +23,8 @@ class Otf2bdf < Formula
 
   depends_on "freetype"
 
-  on_linux do
-    resource "test-font" do
+  resource "test-font" do
+    on_linux do
       url "https://raw.githubusercontent.com/paddykontschak/finder/master/fonts/LucidaGrande.ttc"
       sha256 "e188b3f32f5b2d15dbf01e9b4480fed899605e287516d7c0de6809d8e7368934"
     end
