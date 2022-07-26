f5f4feb8a9cab80c26a4a6b3590625052389d4e6
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-26 23:09:20 +0800

    hspell: no rebuilding required

--- Formula/hspell.rb
+++ Formula/hspell.rb
@@ -25,10 +25,10 @@ class Hspell < Formula
 
   uses_from_macos "zlib"
 
-  on_macos do
-    # hspell was built for linux and compiles a .so shared library, to comply with macOS
-    # standards this patch creates a .dylib instead
-    patch :p0 do
+  # hspell was built for linux and compiles a .so shared library, to comply with macOS
+  # standards this patch creates a .dylib instead
+  patch :p0 do
+    on_macos do
       url "https://raw.githubusercontent.com/Homebrew/formula-patches/85fa66a9/hspell/1.3.patch"
       sha256 "63cc1bc753b1062d1144dcdd959a0a8f712b8872dce89e54ddff2d24f2ca2065"
     end
