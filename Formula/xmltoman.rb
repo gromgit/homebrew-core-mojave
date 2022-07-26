8eee37ec7f0d5185cafb5707c71a9bf23fc5ea0a
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-26 23:09:50 +0800

    xmltoman: no rebuilding required

--- Formula/xmltoman.rb
+++ Formula/xmltoman.rb
@@ -24,8 +24,8 @@ class Xmltoman < Formula
 
   uses_from_macos "perl"
 
-  on_linux do
-    resource "XML::Parser" do
+  resource "XML::Parser" do
+    on_linux do
       url "https://cpan.metacpan.org/authors/id/T/TO/TODDR/XML-Parser-2.44.tar.gz"
       sha256 "1ae9d07ee9c35326b3d9aad56eae71a6730a73a116b9fe9e8a4758b7cc033216"
     end
