bd4d6ee4530fadc16dfc41def0879a08adc452bf
Adrian Ho <aho+vm-mojave@03s.net>
2022-07-26 12:16:19 +0800

    py3cairo: update 1.21.0_1 bottle.

--- Formula/py3cairo.rb
+++ Formula/py3cairo.rb
@@ -4,21 +4,36 @@ class Py3cairo < Formula
   url "https://github.com/pygobject/pycairo/releases/download/v1.21.0/pycairo-1.21.0.tar.gz"
   sha256 "251907f18a552df938aa3386657ff4b5a4937dde70e11aa042bc297957f4b74b"
   license any_of: ["LGPL-2.1-only", "MPL-1.1"]
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/py3cairo"
-    sha256 cellar: :any, mojave: "2158ffb917b3f34bba53d121c029f6c89be3d46cb41d9d2d2b8813bf2ef58b57"
+    sha256 cellar: :any, mojave: "545fee1c5bb4da4ae151b66ae9629d35c9dea88d68a8525719f278ca64a840cb"
   end
 
   depends_on "pkg-config" => :build
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.9" => [:build, :test]
   depends_on "cairo"
-  depends_on "python@3.9"
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.match?(/python@\d\.\d+/) }
+        .map(&:opt_bin)
+        .map { |bin| bin/"python3" }
+  end
 
   def install
-    system Formula["python@3.9"].bin/"python3", *Language::Python.setup_install_args(prefix)
+    pythons.each do |python|
+      system python, *Language::Python.setup_install_args(prefix),
+                     "--install-lib=#{prefix/Language::Python.site_packages(python)}",
+                     "--install-data=#{prefix}"
+    end
   end
 
   test do
-    system Formula["python@3.9"].bin/"python3", "-c", "import cairo; print(cairo.version)"
+    pythons.each do |python|
+      system python, "-c", "import cairo; print(cairo.version)"
+    end
   end
 end
