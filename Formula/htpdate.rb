4e68f6892642e996e16ed1a427d1010887643887
Adrian Ho <the.gromgit@gmail.com>
2022-07-27 14:15:56 +0800

    htpdate: update 1.3.5 bottle.

--- Formula/htpdate.rb
+++ Formula/htpdate.rb
@@ -1,8 +1,8 @@
 class Htpdate < Formula
   desc "Synchronize time with remote web servers"
   homepage "https://www.vervest.org/htp/"
-  url "https://www.vervest.org/htp/archive/c/htpdate-1.3.4.tar.gz"
-  sha256 "744f9200cfd3b008a5516c5eb6da727af532255a329126a7b8f49a5623985642"
+  url "https://www.vervest.org/htp/archive/c/htpdate-1.3.5.tar.gz"
+  sha256 "a8734d4f1d84d0608d045508608f2d29d8b968da269f83120aaac67709b1bd03"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -12,7 +12,7 @@ class Htpdate < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/htpdate"
-    sha256 cellar: :any_skip_relocation, mojave: "f33cfb2d4b3d652d6a4f76acaf2fed3e492173b7d224e89bbd89dd493514ad01"
+    sha256 cellar: :any_skip_relocation, mojave: "471ac7f63aa16187113c2003d45e0f8c7655e1a4d9e6915a97e7ce7167b9bdf8"
   end
 
   def install
