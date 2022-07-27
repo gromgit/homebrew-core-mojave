d990deb10f486ba0a981c7062bc3403a7f2cc52a
Adrian Ho <the.gromgit@gmail.com>
2022-07-27 15:42:22 +0800

    libspiro: update 20220722 bottle.

--- Formula/libspiro.rb
+++ Formula/libspiro.rb
@@ -1,20 +1,14 @@
 class Libspiro < Formula
   desc "Library to simplify the drawing of curves"
   homepage "https://github.com/fontforge/libspiro"
-  url "https://github.com/fontforge/libspiro/releases/download/20200505/libspiro-dist-20200505.tar.gz"
-  sha256 "06c69a1e8dcbcabcf009fd96fd90b1a244d0257246e376c2c4d57c4ea4af0e49"
+  url "https://github.com/fontforge/libspiro/releases/download/20220722/libspiro-dist-20220722.tar.gz"
+  sha256 "3b8e54473f3d4d99c014f2630e62f966f5f4e25c28ca59b63d30bd8e9b7593f5"
   license "GPL-3.0"
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "16386871ef1e3136563fa5fa1d0079f39af32c6971b9072bd3775e56f66859cf"
-    sha256 cellar: :any,                 arm64_big_sur:  "4415d981f900b8187bbe5aeb94c4e6f14e2131277f0adaccef5fa2e793067918"
-    sha256 cellar: :any,                 monterey:       "1c140e2c95c5519ecbfedd2dcab839fe31f3affe4dfbd0e371d94462e5229ad0"
-    sha256 cellar: :any,                 big_sur:        "76ef3ec2cffe248bceafb680e741fc062d1c3b115d5b105632678d34eeb62f20"
-    sha256 cellar: :any,                 catalina:       "238761be2cd640f6c3f59f0461ce7f5b73dc71c9613236e180bc55f4231e167b"
-    sha256 cellar: :any,                 mojave:         "fb1b2e548eddc684a5b615cedabc2c6403e0c87409fdb419369ca40e2b70aa52"
-    sha256 cellar: :any,                 high_sierra:    "b7155fe14b9909a06a9044ca2d3275f5b2cbcd9aac23583b3819ba53898ff120"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "75b6528d51d32b13be0f7fb68b72407f877cce2c89c774ca7c1da1dfbcbea786"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libspiro"
+    sha256 cellar: :any, mojave: "f7f414e43ed4293c47c15ecfd6aa560c0602c2f507d08b887fa823832491fda2"
   end
 
   head do
