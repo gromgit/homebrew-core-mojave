06cbe1a228424137363a149be24614e82107b001
Adrian Ho <the.gromgit@gmail.com>
2022-07-27 13:48:53 +0800

    glfw: update 3.3.8 bottle.

--- Formula/glfw.rb
+++ Formula/glfw.rb
@@ -1,14 +1,14 @@
 class Glfw < Formula
   desc "Multi-platform library for OpenGL applications"
   homepage "https://www.glfw.org/"
-  url "https://github.com/glfw/glfw/archive/3.3.7.tar.gz"
-  sha256 "fd21a5f65bcc0fc3c76e0f8865776e852de09ef6fbc3620e09ce96d2b2807e04"
+  url "https://github.com/glfw/glfw/archive/3.3.8.tar.gz"
+  sha256 "f30f42e05f11e5fc62483e513b0488d5bceeab7d9c5da0ffe2252ad81816c713"
   license "Zlib"
   head "https://github.com/glfw/glfw.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/glfw"
-    sha256 cellar: :any, mojave: "93d65f1da414e7999439dcd684561880aea5c414afe4aa718f70645047a3b0f5"
+    sha256 cellar: :any, mojave: "418cd446fdce1200f2d19ffc064c6d9c0984da1f9e6dcc4e9a533bbac4c8e3de"
   end
 
   depends_on "cmake" => :build
