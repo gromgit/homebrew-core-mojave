752bd7d4211e88a5ff19e9c31b5ec2cf8dcaa0d6
Adrian Ho <the.gromgit@gmail.com>
2022-07-27 08:43:26 +0800

    dbhash: update 3.39.2 bottle.

--- Formula/dbhash.rb
+++ Formula/dbhash.rb
@@ -1,9 +1,9 @@
 class Dbhash < Formula
   desc "Computes the SHA1 hash of schema and content of a SQLite database"
   homepage "https://www.sqlite.org/dbhash.html"
-  url "https://sqlite.org/2022/sqlite-src-3390100.zip"
-  version "3.39.1"
-  sha256 "366c7abbee5dbe8882cd7578a61a6ed3f5d08c5f6de3535a0003125b4646cc57"
+  url "https://sqlite.org/2022/sqlite-src-3390200.zip"
+  version "3.39.2"
+  sha256 "e933d77000f45f3fbc8605f0050586a3013505a8de9b44032bd00ed72f1586f0"
   license "blessing"
 
   livecheck do
@@ -12,7 +12,7 @@ class Dbhash < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dbhash"
-    sha256 cellar: :any_skip_relocation, mojave: "d635be06f4323067592d29e567b052c76bcc746724873fe802ac33bcd4c53229"
+    sha256 cellar: :any_skip_relocation, mojave: "7e24ac40828dd6fcc3cb61f8811ba8d51c555a2ff04398887dcf0455e97d92c5"
   end
 
   uses_from_macos "tcl-tk" => :build
