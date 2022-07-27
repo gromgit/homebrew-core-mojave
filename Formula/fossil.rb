737a06ec1e662a5871934a5226df5e33b2993370
Adrian Ho <the.gromgit@gmail.com>
2022-07-27 09:19:13 +0800

    fossil: update 2.19 bottle.

--- Formula/fossil.rb
+++ Formula/fossil.rb
@@ -1,8 +1,8 @@
 class Fossil < Formula
   desc "Distributed software configuration management"
   homepage "https://www.fossil-scm.org/home/"
-  url "https://fossil-scm.org/home/tarball/version-2.18/fossil-src-2.18.tar.gz"
-  sha256 "300c1d5cdd6224ec6e8c88ab3f38d50f80e4071b503731b75bd61274cf310733"
+  url "https://fossil-scm.org/home/tarball/version-2.19/fossil-src-2.19.tar.gz"
+  sha256 "4f135659ec9a3958a10eec98f79d4d3fc10edeae2605b4b38e0a58826800b490"
   license "BSD-2-Clause"
   head "https://www.fossil-scm.org/", using: :fossil
 
@@ -11,13 +11,12 @@ class Fossil < Formula
     regex(/"title":\s*?"Version (\d+(?:\.\d+)+)\s*?\(/i)
   end
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fossil"
-    rebuild 1
-    sha256 cellar: :any, mojave: "decfd7122397184c8bcb8da41a67ba88b34264c0448f519f32e98fc42986adc8"
+    sha256 cellar: :any, mojave: "629d85b058d05b9d8ac6af1103357cd595bd58b55c9090087b7f95652d613c04"
   end
 
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   uses_from_macos "zlib"
 
   def install
