e2095560fe9dc0778b0811aa267dd6ad9b3d0c27
Adrian Ho <the.gromgit@gmail.com>
2022-07-29 06:00:19 +0800

    aztfy: update 0.6.0 bottle.

--- Formula/aztfy.rb
+++ Formula/aztfy.rb
@@ -2,14 +2,14 @@ class Aztfy < Formula
   desc "Bring your existing Azure resources under the management of Terraform"
   homepage "https://azure.github.io/aztfy"
   url "https://github.com/Azure/aztfy.git",
-      tag:      "v0.5.0",
-      revision: "f957fc158684aaf66551543327124865c7fe4eca"
+      tag:      "v0.6.0",
+      revision: "18b614bb384b1d413554867b8601feb753715575"
   license "MPL-2.0"
   head "https://github.com/Azure/aztfy.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/aztfy"
-    sha256 cellar: :any_skip_relocation, mojave: "9cf1a5dd53ad7af75bf0411f08d2facdd20f60b81e2c86e31983243a01b22c22"
+    sha256 cellar: :any_skip_relocation, mojave: "0c1fbbb4c588cb9e035adada127e107b56f13381a9c34bdefcf212e6c858f3bc"
   end
   depends_on "go" => :build
 
@@ -22,7 +22,7 @@ class Aztfy < Formula
     version_output = shell_output("#{bin}/aztfy -v")
     assert_match version.to_s, version_output
 
-    no_resource_group_specified_output = shell_output("#{bin}/aztfy 2>&1", 1)
+    no_resource_group_specified_output = shell_output("#{bin}/aztfy rg 2>&1", 1)
     assert_match("Error: No resource group specified", no_resource_group_specified_output)
   end
 end
