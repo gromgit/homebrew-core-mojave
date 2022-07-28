608c7a317540a1ec6b511b36b72c9a2d40b0c4a0
Adrian Ho <the.gromgit@gmail.com>
2022-07-28 09:45:36 +0800

    proftpd: update 1.3.7e bottle.

--- Formula/proftpd.rb
+++ Formula/proftpd.rb
@@ -1,11 +1,11 @@
 class Proftpd < Formula
   desc "Highly configurable GPL-licensed FTP server software"
   homepage "http://www.proftpd.org/"
-  url "https://github.com/proftpd/proftpd/archive/v1.3.7d.tar.gz"
-  mirror "https://fossies.org/linux/misc/proftpd-1.3.7d.tar.gz"
-  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/proftpd/proftpd-1.3.7d.tar.gz"
-  version "1.3.7d"
-  sha256 "b231536e2978116801d06278e805b18e5240568d2bc921693ac7147652e267e4"
+  url "https://github.com/proftpd/proftpd/archive/v1.3.7e.tar.gz"
+  mirror "https://fossies.org/linux/misc/proftpd-1.3.7e.tar.gz"
+  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/proftpd/proftpd-1.3.7e.tar.gz"
+  version "1.3.7e"
+  sha256 "6e716a3b53ee069290399fce6dccf4c229fafe6ec2cb14db3778b7aa3f9a8c92"
   license "GPL-2.0-or-later"
 
   # Proftpd uses an incrementing letter after the numeric version for
@@ -19,8 +19,10 @@ class Proftpd < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/proftpd"
-    sha256 mojave: "e95960c7dbb4cfe3edca6897094e4258deeb6c920aa07d26dcc8221ccd6daec3"
+    sha256 mojave: "66000095ec11fb459b1390dc541cce3d9dd491ac576d16ba4bd0587031e2ecdb"
   end
+
+  uses_from_macos "libxcrypt"
 
   def install
     # fixes unknown group 'nogroup'
@@ -32,7 +34,7 @@ class Proftpd < Formula
                           "--localstatedir=#{var}"
     ENV.deparallelize
     install_user = ENV["USER"]
-    install_group = `groups`.split[0]
+    install_group = Utils.safe_popen_read("groups").split.first
     system "make", "INSTALL_USER=#{install_user}", "INSTALL_GROUP=#{install_group}", "install"
   end
 
