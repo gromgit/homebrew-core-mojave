a337610dbea26a728e6da5f4bf2add959c773051
Adrian Ho <the.gromgit@gmail.com>
2022-07-28 18:19:13 +0800

    collada-dom: update 2.5.0_3 bottle.

--- Formula/collada-dom.rb
+++ Formula/collada-dom.rb
@@ -3,23 +3,30 @@ class ColladaDom < Formula
   homepage "https://www.khronos.org/collada/wiki/Portal:COLLADA_DOM"
   url "https://github.com/rdiankov/collada-dom/archive/v2.5.0.tar.gz"
   sha256 "3be672407a7aef60b64ce4b39704b32816b0b28f61ebffd4fbd02c8012901e0d"
-  revision 1
+  revision 3
   head "https://github.com/rdiankov/collada-dom.git", branch: "master"
 
-bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/collada-dom"
-    rebuild 1
-    sha256 cellar: :any, mojave: "b9f83951fb368e5a8ea809aeea419c869bd6091a49cd26a7779f0738f882ee1d"
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/collada-dom-2.5.0"
+    sha256 cellar: :any, mojave: "80396359bd77cbf3369541a538cf829e42c2f39dee1527f3670030cf9f5c0910"
   end
 
   depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
   depends_on "boost"
+  depends_on "minizip"
   depends_on "pcre"
+
   uses_from_macos "libxml2"
 
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make", "install"
+    # Remove bundled libraries to avoid fallback
+    (buildpath/"dom/external-libs").rmtree
+
+    ENV.cxx11 if OS.linux? # due to `icu4c` dependency in `libxml2`
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
