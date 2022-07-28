07b9318df9d99d2cf6fb7ba0743acfb7622d9d5a
Adrian Ho <the.gromgit@gmail.com>
2022-07-28 11:42:27 +0800

    zabbix: update 6.2.1 bottle.

--- Formula/zabbix.rb
+++ Formula/zabbix.rb
@@ -1,8 +1,8 @@
 class Zabbix < Formula
   desc "Availability and monitoring solution"
   homepage "https://www.zabbix.com/"
-  url "https://cdn.zabbix.com/zabbix/sources/stable/6.2/zabbix-6.2.0.tar.gz"
-  sha256 "2de7cb68a0d0f09bfef66328ab885920afa6a3b0b57ce6e6fc6227b10a2be02a"
+  url "https://cdn.zabbix.com/zabbix/sources/stable/6.2/zabbix-6.2.1.tar.gz"
+  sha256 "f3d6b7cf4e67d820ce7d28cd54ac67724f7453f261f668877e6410cd21ab9ea1"
   license "GPL-2.0-or-later"
   head "https://github.com/zabbix/zabbix.git", branch: "master"
 
@@ -13,7 +13,7 @@ class Zabbix < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/zabbix"
-    sha256 mojave: "31846c581118729d68ebba973e76b3df74f60e68241eb302339327efb8563a1b"
+    sha256 mojave: "eab6411bd2b001eb1b6c6d156420ece601e5a898d7e276d3443775e9e9729a98"
   end
 
   depends_on "pkg-config" => :build
