3efd3b45af24b2c4eca55f0aed1c727eba22755c
Adrian Ho <the.gromgit@gmail.com>
2022-07-28 19:39:42 +0800

    webp: update 1.2.3_1 bottle.

--- Formula/webp.rb
+++ Formula/webp.rb
@@ -4,21 +4,22 @@ class Webp < Formula
   url "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.2.3.tar.gz"
   sha256 "f5d7ab2390b06b8a934a4fc35784291b3885b557780d099bd32f09241f9d83f9"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/webp"
-    sha256 cellar: :any, mojave: "96d347577b2ca0b7ccc5cc2d7fd9f27a5ffdcc99a7fa1432f7de8fd8f2f748b9"
+    sha256 cellar: :any, mojave: "10fc002e54f0c6beeadf024dd1da9c137cfbdd964cc5c260e07ec9ddde084865"
   end
 
   head do
-    url "https://chromium.googlesource.com/webm/libwebp.git"
+    url "https://chromium.googlesource.com/webm/libwebp.git", branch: "main"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
   end
 
   depends_on "giflib"
-  depends_on "jpeg"
+  depends_on "jpeg-turbo"
   depends_on "libpng"
   depends_on "libtiff"
 
