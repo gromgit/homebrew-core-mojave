e01bcb070ed9a0eafed103f83c92b5345cfc98f5
Adrian Ho <the.gromgit@gmail.com>
2022-07-28 11:20:35 +0800

    tth: update 4.16 bottle.

--- Formula/tth.rb
+++ Formula/tth.rb
@@ -1,8 +1,8 @@
 class Tth < Formula
   desc "TeX/LaTeX to HTML converter"
   homepage "http://hutchinson.belmont.ma.us/tth/"
-  url "http://hutchinson.belmont.ma.us/tth/tth_distribution/tth_4.15.tgz"
-  sha256 "83c1f39fbf3377fb43e3d01d042302fa91f8758aa9acc10e22fe8af140f0126c"
+  url "http://hutchinson.belmont.ma.us/tth/tth_distribution/tth_4.16.tgz"
+  sha256 "ff8b88c6dbb938f01fe6a224c396fc302ae5d89b9b6d97f207f7ae0c4e7f0a32"
 
   livecheck do
     url "http://hutchinson.belmont.ma.us/tth/Version"
@@ -10,14 +10,8 @@ class Tth < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f37a1410128cc821a0d786a3e016010537191a31dd6d04bc0e38a1d924d5f2ba"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "087ea392bb9b6de0921381bbf9b635e5dfe6e6c2d75e00e2143d5b6506e6cb14"
-    sha256 cellar: :any_skip_relocation, monterey:       "fcc42fa0ad0c88c57b2659ec16959520824e7fff9573c744cd4642aa105b74c8"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9f8333051d4b9025641302175bac6db82a883dc722c8dd8286a424af16cab3a1"
-    sha256 cellar: :any_skip_relocation, catalina:       "eba438c9ec2902578cd3a043fdfbc1fa6bf22bedbde36e868c4fe02740448b06"
-    sha256 cellar: :any_skip_relocation, mojave:         "a0fcd3029a8a43a59a530537c5fef4df45c0155924f41b07ac14ec2362d73ba7"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "dc8224b651a9bbb3ab8a5f5dd2e213bfd42518962bd7132cb3d8a2db12ddd5bb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fa2cdf102a31eaee0fe9a64f9d5e0db28fc5284b02f0d005c2d91d473a574514"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tth"
+    sha256 cellar: :any_skip_relocation, mojave: "deaf6032ed49ddd5d783095332dc729df16d9bbc8b1905a0fc8fc2f26096b2c6"
   end
 
   def install
