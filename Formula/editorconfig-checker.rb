4b2e03327dd62654971bdc8ce2233f4fdca9643d
Adrian Ho <the.gromgit@gmail.com>
2022-07-29 12:22:25 +0800

    editorconfig-checker: update 2.6.0 bottle.

--- Formula/editorconfig-checker.rb
+++ Formula/editorconfig-checker.rb
@@ -1,14 +1,14 @@
 class EditorconfigChecker < Formula
   desc "Tool to verify that your files are in harmony with your .editorconfig"
   homepage "https://github.com/editorconfig-checker/editorconfig-checker"
-  url "https://github.com/editorconfig-checker/editorconfig-checker/archive/refs/tags/2.5.0.tar.gz"
-  sha256 "50274a09c760303bc88c0ead319eda4bc6f317764a1cade1ea86e9ff17d7c0b1"
+  url "https://github.com/editorconfig-checker/editorconfig-checker/archive/refs/tags/2.6.0.tar.gz"
+  sha256 "21c3ddffa915f0cd857cef580025a6ff86cdd8b78c6026a2d841d2ca482b48e7"
   license "MIT"
   head "https://github.com/editorconfig-checker/editorconfig-checker.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/editorconfig-checker"
-    sha256 cellar: :any_skip_relocation, mojave: "194fefdf129325e08b95f4158ecd48f673a3099203516429a88010c943bb79be"
+    sha256 cellar: :any_skip_relocation, mojave: "a1e76c97a829f2b4bb869c90b7cb52ba89163c97b06910e9b060c9478d5f0a65"
   end
 
   depends_on "go" => :build
