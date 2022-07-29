6df2df2f9e0532b3c0446b0316bc3e35ca4cab15
Adrian Ho <the.gromgit@gmail.com>
2022-07-29 14:42:15 +0800

    gau: update 2.1.2 bottle.

--- Formula/gau.rb
+++ Formula/gau.rb
@@ -1,14 +1,14 @@
 class Gau < Formula
   desc "Open Threat Exchange, Wayback Machine, and Common Crawl URL fetcher"
   homepage "https://github.com/lc/gau"
-  url "https://github.com/lc/gau/archive/v2.1.1.tar.gz"
-  sha256 "17bd5b8e3a6a554d82ab5de66bc1f34ce2810a3ba6b8e1456432faff9b191c8f"
+  url "https://github.com/lc/gau/archive/v2.1.2.tar.gz"
+  sha256 "2900ba86dfda01b5d8a90e1547f158feb134f6d2b757ff8fc77d96d290f72e4c"
   license "MIT"
   head "https://github.com/lc/gau.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gau"
-    sha256 cellar: :any_skip_relocation, mojave: "3742a7ef29f0a4d82351a06056d98d47007ce2a0ccc91ef96f9653a6fd84bca4"
+    sha256 cellar: :any_skip_relocation, mojave: "4116c7f6af3a2f936af5eff6c8594cd78c2733c1dc6c2091b72e0d4f821e9dc1"
   end
 
   depends_on "go" => :build
