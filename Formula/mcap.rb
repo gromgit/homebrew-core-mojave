be0be89340454a34f9a9f2e120cb45b3947061ce
Adrian Ho <the.gromgit@gmail.com>
2022-07-30 10:46:18 +0800

    mcap: update 0.0.15 bottle.

--- Formula/mcap.rb
+++ Formula/mcap.rb
@@ -1,8 +1,8 @@
 class Mcap < Formula
   desc "Serialization-agnostic container file format for pub/sub messages"
   homepage "https://mcap.dev"
-  url "https://github.com/foxglove/mcap/archive/releases/mcap-cli/v0.0.14.tar.gz"
-  sha256 "7e7c23f423a5636c17ed402c2775674ab7ace8ddf3d0e09bf0b730f4fb328144"
+  url "https://github.com/foxglove/mcap/archive/releases/mcap-cli/v0.0.15.tar.gz"
+  sha256 "b287c3999470caebcf8f97b15110d16fe9fcd8785536644a287e7435f99d883e"
   license "Apache-2.0"
   head "https://github.com/foxglove/mcap.git", branch: "main"
 
@@ -13,7 +13,7 @@ class Mcap < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mcap"
-    sha256 cellar: :any_skip_relocation, mojave: "98df566c6957ab1225e7eb8922eec4ecb3c87b648d03873967c67da7669bed16"
+    sha256 cellar: :any_skip_relocation, mojave: "f71bc83a3d2c6ba744a57bbdae4b95d98c1faaea77e39c0265fb1407d796fd23"
   end
 
   depends_on "go" => :build
