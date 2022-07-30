bd57ad38d7492c307c6820d7e782e067a79d6896
Adrian Ho <the.gromgit@gmail.com>
2022-07-30 16:50:11 +0800

    openfpgaloader: update 0.9.0 bottle.

--- Formula/openfpgaloader.rb
+++ Formula/openfpgaloader.rb
@@ -1,14 +1,14 @@
 class Openfpgaloader < Formula
   desc "Universal utility for programming FPGA"
   homepage "https://github.com/trabucayre/openFPGALoader"
-  url "https://github.com/trabucayre/openFPGALoader/archive/v0.8.0.tar.gz"
-  sha256 "1d94c2b40c4d6b22d4099ef48b7ed4cb3f3ebfc73f36b1e87c739418a7d3045d"
+  url "https://github.com/trabucayre/openFPGALoader/archive/v0.9.0.tar.gz"
+  sha256 "1555b7ba1eb5f98d4c7a0d77c1ed7ab54214e942b8c562264e74b5d3997e0dd3"
   license "Apache-2.0"
   head "https://github.com/trabucayre/openFPGALoader.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/openfpgaloader"
-    sha256 cellar: :any_skip_relocation, mojave: "edf0d22ef4ecdd148d025790415be20d80e490e86ff7805ba68693c32cb83578"
+    sha256 cellar: :any, mojave: "99633b62e0e5ce41f53b65a3607b7feeafecd76a76e8f57d93f8bc77a610fe51"
   end
 
   depends_on "cmake" => :build
