dd7f55384393d79020728fe8850a7f8218183fb1
Adrian Ho <the.gromgit@gmail.com>
2022-07-30 17:20:36 +0800

    openvi: update 7.1.18 bottle.

--- Formula/openvi.rb
+++ Formula/openvi.rb
@@ -4,10 +4,12 @@ class Openvi < Formula
   url "https://github.com/johnsonjh/OpenVi/archive/refs/tags/7.1.18.tar.gz"
   sha256 "39c4ac933f52c65021be06fcece8bfd308fc1ac08e8ff4604b2fdd1994192d08"
   license "BSD-3-Clause"
+  head "https://github.com/johnsonjh/OpenVi.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/openvi"
-    sha256 cellar: :any_skip_relocation, mojave: "fbeaa316452c1e8a4ccd8f236f8260fa85d85a9392a95c6c74a78ae9300ba889"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "b7000a433e729b48e5dffaf0547cb043908353c6f57284d141df8803fc596d36"
   end
 
   uses_from_macos "ncurses"
