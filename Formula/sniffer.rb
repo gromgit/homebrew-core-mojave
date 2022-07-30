da116d9de854882f9a83fd95de3b9a581072bc04
Adrian Ho <the.gromgit@gmail.com>
2022-07-30 20:33:41 +0800

    sniffer: update 0.6.1 bottle.

--- Formula/sniffer.rb
+++ Formula/sniffer.rb
@@ -1,13 +1,13 @@
 class Sniffer < Formula
   desc "Modern alternative network traffic sniffer"
   homepage "https://github.com/chenjiandongx/sniffer"
-  url "https://github.com/chenjiandongx/sniffer/archive/v0.6.0.tar.gz"
-  sha256 "5f6479baf3fa003aa25247d280f4a8bf2130a346a20e6feb497633650900056f"
+  url "https://github.com/chenjiandongx/sniffer/archive/v0.6.1.tar.gz"
+  sha256 "130d588c2472939fc80e3c017a1cae4d515770f1bcab263d985e3be1498d2dbc"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sniffer"
-    sha256 cellar: :any_skip_relocation, mojave: "5f1108f6891135e303df77e60d61aa5af6bdc58a1218a3774999f6b2348acb40"
+    sha256 cellar: :any_skip_relocation, mojave: "7df4161f6d9a3cb2a1ca82ddda55314511833162426dd17b3874674b816339db"
   end
 
   depends_on "go" => :build
