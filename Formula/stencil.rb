a01b393ea08e1626d8d635410f14856027aefa9c
Adrian Ho <the.gromgit@gmail.com>
2022-07-30 21:06:34 +0800

    stencil: update 1.21.1 bottle.

--- Formula/stencil.rb
+++ Formula/stencil.rb
@@ -1,14 +1,14 @@
 class Stencil < Formula
   desc "Smart templating engine for service development"
   homepage "https://engineering.outreach.io/stencil/"
-  url "https://github.com/getoutreach/stencil/archive/refs/tags/v1.21.0.tar.gz"
-  sha256 "b119463ede8c6fc75e206158111447f2ebe586f4cc371b180045fc80bba6856d"
+  url "https://github.com/getoutreach/stencil/archive/refs/tags/v1.21.1.tar.gz"
+  sha256 "e0b6c38d72d0defacc5912912e0f629b8fb6f33494995645bc438f81d76daf9e"
   license "Apache-2.0"
   head "https://github.com/getoutreach/stencil.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/stencil"
-    sha256 cellar: :any_skip_relocation, mojave: "01312a29e5fd954917b5852e06dc67b07fe4c4278da96ec2f755300586a86dd4"
+    sha256 cellar: :any_skip_relocation, mojave: "b194fc6abc43bf1664513e032d265a687e5257c76ffee99d890ea24bb48b6c7e"
   end
 
   depends_on "go" => :build
