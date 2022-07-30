ca87feb423ecccbfd3945ac3c93de47cdc1dd3f2
Adrian Ho <the.gromgit@gmail.com>
2022-07-30 18:43:24 +0800

    powerman-dockerize: update 0.16.3 bottle.

--- Formula/powerman-dockerize.rb
+++ Formula/powerman-dockerize.rb
@@ -1,14 +1,13 @@
 class PowermanDockerize < Formula
   desc "Utility to simplify running applications in docker containers"
   homepage "https://github.com/powerman/dockerize"
-  url "https://github.com/powerman/dockerize/archive/refs/tags/v0.16.0.tar.gz"
-  sha256 "b6507b24714dee193bd04be18fe8658531d2b0c0b9f6f060ac6bb387c736009f"
+  url "https://github.com/powerman/dockerize/archive/refs/tags/v0.16.3.tar.gz"
+  sha256 "ada4a61a647983cefe180561d2f20563d8109af5b4b5d8d97ee5f90f481ecd15"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/powerman-dockerize"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "8c20cd070564dbd41c5133f68120a25ed20d10aba6b9004aad125cfcb8643649"
+    sha256 cellar: :any_skip_relocation, mojave: "21940e59b767cf3b745536b4dc1eb5bd4952625a69e2b3c34726da044a52ce7c"
   end
 
   depends_on "go" => :build
