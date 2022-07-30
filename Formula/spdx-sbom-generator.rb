095c75bfd715d058f2b2fdbc549eb4c305151fbd
Adrian Ho <the.gromgit@gmail.com>
2022-07-30 20:51:49 +0800

    spdx-sbom-generator: update 0.0.15 bottle.

--- Formula/spdx-sbom-generator.rb
+++ Formula/spdx-sbom-generator.rb
@@ -1,18 +1,16 @@
 class SpdxSbomGenerator < Formula
   desc "Support CI generation of SBOMs via golang tooling"
   homepage "https://github.com/opensbom-generator/spdx-sbom-generator"
-  url "https://github.com/opensbom-generator/spdx-sbom-generator/archive/refs/tags/v0.0.13.tar.gz"
-  sha256 "7d088f136a53d1f608b1941362c568d78cc6279df9c1bdb3516de075cb7f10c3"
+  url "https://github.com/opensbom-generator/spdx-sbom-generator/archive/refs/tags/v0.0.15.tar.gz"
+  sha256 "3811d652de0f27d3bfa7c025aa6815805ef347a35b46f9e2a5093cc6b26f7b08"
   license any_of: ["Apache-2.0", "CC-BY-4.0"]
-  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/spdx-sbom-generator"
-    sha256 cellar: :any_skip_relocation, mojave: "8f0dbb391e8ca1b88d280db3b8b37a195d1b0cd42b75a571dfd7d594424559e3"
+    sha256 cellar: :any_skip_relocation, mojave: "06379e2e24cb044bd376cc937ad4a82e1b545b25fab5da447eca093ca8ae299b"
   end
 
-  # Bump to 1.18 on the next release, if possible.
-  depends_on "go@1.17" => [:build, :test]
+  depends_on "go" => [:build, :test]
 
   def install
     target = if Hardware::CPU.arm?
@@ -25,15 +23,13 @@ class SpdxSbomGenerator < Formula
 
     system "make", target
 
-    exe = "spdx-sbom-generator"
-    (libexec/"bin").install "bin/#{exe}"
-    (bin/exe).write_env_script(libexec/"bin/#{exe}", PATH: "$PATH:#{Formula["go@1.17"].opt_bin}")
+    prefix.install "bin"
   end
 
   test do
-    system Formula["go@1.17"].opt_bin/"go", "mod", "init", "example.com/tester"
+    system "go", "mod", "init", "example.com/tester"
 
     assert_equal "panic: runtime error: index out of range [0] with length 0",
-                 shell_output("#{bin}/spdx-sbom-generator 2>&1", 2).split("\n")[3]
+                 shell_output("#{bin}/spdx-sbom-generator 2>&1", 2).split("\n")[4]
   end
 end
