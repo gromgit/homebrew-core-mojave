459ce326a4e2468fb63ad499618275d0e40c1ffd
Adrian Ho <the.gromgit@gmail.com>
2022-07-30 10:49:11 +0800

    mdxmini: update 2.0.0 bottle.

--- Formula/mdxmini.rb
+++ Formula/mdxmini.rb
@@ -1,40 +1,45 @@
 class Mdxmini < Formula
   desc "Plays music in X68000 MDX chiptune format"
   homepage "https://github.com/mistydemeo/mdxmini/"
-  url "https://github.com/mistydemeo/mdxmini/archive/v1.0.0.tar.gz"
-  sha256 "5a407203f35d873c3cd5977213b0c33a1ce283d6b14483e9d434de79b05ca4e2"
+  url "https://github.com/mistydemeo/mdxmini/archive/v2.0.0.tar.gz"
+  sha256 "9b623b365e893a769084f7a2effedc9ece453c6e3861c571ba503f045471a0e0"
   license "GPL-2.0-or-later"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "dabc5df917cc5cbbbd5ee9196edbbc864baeaa8ce7e1911721951bf5eedab58d"
-    sha256 cellar: :any, big_sur:       "7b372fab53a00c33796d91b6f2a7261a10ab51d1893f82176dd75ce89577d524"
-    sha256 cellar: :any, catalina:      "04b985db291b09f8f1c4a57e522700f9c67eadcd1074faae78ab0e2ff268d9da"
-    sha256 cellar: :any, mojave:        "e93281dc0c64642e33763f0dc2a4cfa0a6da0dd4739222b0411e54913435ee27"
-    sha256 cellar: :any, high_sierra:   "5bf36e82084146ab4604b4746bcf6634cfe4268f2044712e4d13519b21ab5165"
-    sha256 cellar: :any, sierra:        "8e0daf3d508dad59074c567b8c8e60bd88c8026b7dfe1305e4e9c50ec5d8fbbd"
-    sha256 cellar: :any, el_capitan:    "d20b94107c25833096401be6336544f283e6956758d4238e207e6a4e34fa5fdf"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mdxmini"
+    sha256 cellar: :any, mojave: "2924bd60bd2a9cd8d29ac26f1eea538f29aa49ebd21d88dcd93e4a103bdad7ba"
   end
 
-  depends_on "sdl"
+  depends_on "sdl2"
 
   resource "test_song" do
     url "https://ftp.modland.com/pub/modules/MDX/-%20unknown/Popful%20Mail/pop-00.mdx"
     sha256 "86f21fbbaf93eb60e79fa07c759b906a782afe4e1db5c7e77a1640e6bf63fd14"
   end
 
+  # Fix build on Linux
+  patch :DATA
+
   def install
-    # Specify Homebrew's cc
-    inreplace "mak/general.mak", "gcc", ENV.cc
-    system "make"
+    system "make", "CC=#{ENV.cc}", "LD=#{ENV.cc}"
 
     # Makefile doesn't build a dylib
-    system ENV.cc, "-dynamiclib", "-install_name", "#{lib}/libmdxmini.dylib",
-                   "-o", "libmdxmini.dylib", "-undefined", "dynamic_lookup",
-                   *Dir["obj/*"]
+    libmdxmini = shared_library("libmdxmini")
+
+    flags = if OS.mac?
+      %W[
+        -dynamiclib
+        -install_name #{lib/libmdxmini}
+        -undefined dynamic_lookup
+      ]
+    else
+      ["-shared"]
+    end
+
+    system ENV.cc, *flags, "-o", libmdxmini, *Dir["obj/*.o"]
 
     bin.install "mdxplay"
-    lib.install "libmdxmini.dylib"
+    lib.install libmdxmini
     (include/"libmdxmini").install Dir["src/*.h"]
   end
 
@@ -53,7 +58,7 @@ class Mdxmini < Formula
           printf("%s\\n", title);
       }
     EOS
-    system ENV.cc, "mdxtest.c", "-L#{lib}", "-lmdxmini", "-o", "mdxtest"
+    system ENV.cc, "mdxtest.c", "-L#{lib}", "-L#{Formula["sdl2"].opt_lib}", "-lmdxmini", "-lSDL2", "-o", "mdxtest"
 
     result = shell_output("#{testpath}/mdxtest #{testpath}/pop-00.mdx #{testpath}").chomp
     result.force_encoding("ascii-8bit") if result.respond_to? :force_encoding
@@ -71,3 +76,41 @@ class Mdxmini < Formula
     assert_equal expected.delete!("\n"), result
   end
 end
+
+__END__
+diff --git a/Makefile b/Makefile
+index 9b63041..ff725c3 100644
+--- a/Makefile
++++ b/Makefile
+@@ -43,6 +43,7 @@ FILES_ORG = COPYING AUTHORS
+ LIB = $(OBJDIR)/lib$(TITLE).a
+
+ LIBS += $(LIB)
++LIBS += -lm
+
+ ZIPSRC = $(TITLE)`date +"%y%m%d"`.zip
+ TOUCH = touch -t `date +"%m%d0000"`
+diff --git a/mak/general.mak b/mak/general.mak
+index 6f88e4c..c552eb3 100644
+--- a/mak/general.mak
++++ b/mak/general.mak
+@@ -17,10 +17,16 @@ CFLAGS = -g -O3
+ OBJDIR = obj
+ endif
+
+-# iconv
++# iconv and -fPIC flags
+ ifneq ($(OS),Windows_NT)
+-CFLAGS += -DUSE_ICONV
+-LIBS += -liconv
++  UNAME_S := $(shell uname -s)
++  ifeq ($(UNAME_S),Darwin)
++    CFLAGS += -DUSE_ICONV
++    LIBS += -liconv
++  endif
++  ifeq ($(UNAME_S),Linux)
++    CFLAGS += -fPIC
++  endif
+ endif
+
+ #
