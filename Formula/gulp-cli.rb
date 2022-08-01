2755e611362609d4aaddf627eb720bfc30074002
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-01 21:24:01 +0800

    gulp-cli: no rebuilding required

--- Formula/gulp-cli.rb
+++ Formula/gulp-cli.rb
@@ -7,6 +7,7 @@ class GulpCli < Formula
   sha256 "0a5a76e5be9856edf019fb5be0ed8501a8d815da1beeb9c6effca07a93873ba4"
   license "MIT"
 
+  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8ea471114a8812cf87daaea09ea00dc61297ac35c4227372a9c68aaa04ed1901"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fc2ba1fab80c6b58a6f916e317141e155369834a16526ce271758f46384813a5"
@@ -15,7 +16,6 @@ class GulpCli < Formula
     sha256 cellar: :any_skip_relocation, catalina:       "231b635ddf8a704a3be4a6ba34611248ece69ed1de04fb82adfa6a20ac83fddb"
     sha256 cellar: :any_skip_relocation, mojave:         "29ec2f9cf132be84c577ff6d6ea02845ee96d995e1affdea8961903f9fec616a"
     sha256 cellar: :any_skip_relocation, high_sierra:    "e4d363c9d5035fc814ca6a6820b9c8c35a320cd507067cad5eb0c0e6c337c36e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bdba5b8c37fcd4ade1460d72bc881e345b2eafbfaa0f84a57aeb19479abcaeba"
   end
 
   depends_on "node"
