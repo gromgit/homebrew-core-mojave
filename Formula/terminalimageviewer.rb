b9369215eb68251b599e846a8e5a23866aff2d1a
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-01 21:24:26 +0800

    terminalimageviewer:

--- Formula/terminalimageviewer.rb
+++ Formula/terminalimageviewer.rb
@@ -6,21 +6,17 @@ class Terminalimageviewer < Formula
   license "Apache-2.0"
   head "https://github.com/stefanhaustein/TerminalImageViewer.git", branch: "master"
 
+  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "fbf2316ed0c4cc099c29b52dcf1970d78a90c3325ae89e3dff2e66c4faf1722d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d9d5d315e5ac576411e6bc71adc39566f1610d355b4d2f243568ee44b79228e7"
     sha256 cellar: :any_skip_relocation, monterey:       "f9939913e227575dcadd570ffce7325f338de0b32381a13a57548dc81874639f"
     sha256 cellar: :any_skip_relocation, big_sur:        "d37173ffc79b0f4e7bef0d9f24105209ddf1ea9c7283e6607b0b7549ecadcd6e"
     sha256 cellar: :any_skip_relocation, catalina:       "b0a77c46278852529506b8a6e4b0a377445005fa4ab32b003c12f0b87bddd01d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a612b37b5f01ee66ea80005c2a3d9dadbb5717512dcb890927ad1cba14e55db5"
   end
 
   depends_on "imagemagick"
   depends_on macos: :catalina
-
-  on_linux do
-    depends_on "gcc"
-  end
 
   fails_with gcc: "5"
 
