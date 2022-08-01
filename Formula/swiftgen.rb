f4a8e4fdc3eec3b6d289c83d7d7a90067e62890c
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-01 21:24:24 +0800

    swiftgen:

--- Formula/swiftgen.rb
+++ Formula/swiftgen.rb
@@ -1,26 +1,24 @@
 class Swiftgen < Formula
   desc "Swift code generator for assets, storyboards, Localizable.strings, …"
   homepage "https://github.com/SwiftGen/SwiftGen"
-  url "https://github.com/SwiftGen/SwiftGen/archive/6.5.1.tar.gz"
-  sha256 "bcd52ddc581eea94070eaaab7b94218971f7424b7800696f6be0a1accfc7a0a9"
+  url "https://github.com/SwiftGen/SwiftGen/archive/6.6.1.tar.gz"
+  sha256 "b5ad147775c29d8d13d0d10124d78d6d7185e574b01ae6f2bcd11667c9b6f6f8"
   license "MIT"
   head "https://github.com/SwiftGen/SwiftGen.git", branch: "stable"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "bf3abab4eac9a3886d79d6eb625b1d010c4490cd04391d02a78d44ea67ffbc5f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ea599c0ad955644eb3f946360603090f0b05b859c892ccb94d85795eb68612d6"
-    sha256 cellar: :any_skip_relocation, monterey:       "ea47c833c24c992ecba7fff825d94efd15ed4be586c6c39188fdad1c11605f2f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "d23540aaccbe68f26fc50260a29904afa1e84f97c678e76d4c842927215e91e9"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "661272d66717f8fa88060fd2802b98843c7c27c518a9b57f4abc214d02ba7b86"
+    sha256 cellar: :any_skip_relocation, monterey:       "38c56d7a8b88c07d6761d2fbff99d009c04a8fd580a7eafb650236d38f90004c"
   end
 
-  depends_on xcode: ["13.0", :build]
+  depends_on xcode: ["13.3", :build]
   depends_on :macos
 
   uses_from_macos "ruby" => :build, since: :high_sierra
 
   resource("testdata") do
-    url "https://github.com/SwiftGen/SwiftGen/archive/6.5.1.tar.gz"
-    sha256 "bcd52ddc581eea94070eaaab7b94218971f7424b7800696f6be0a1accfc7a0a9"
+    url "https://github.com/SwiftGen/SwiftGen/archive/6.6.1.tar.gz"
+    sha256 "b5ad147775c29d8d13d0d10124d78d6d7185e574b01ae6f2bcd11667c9b6f6f8"
   end
 
   def install
