9c8c5a4eaad7326a18c9f6c58f4cc97c6dadad68
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-01 21:23:43 +0800

    apngasm:

--- Formula/apngasm.rb
+++ Formula/apngasm.rb
@@ -7,13 +7,13 @@ class Apngasm < Formula
   revision 1
   head "https://github.com/apngasm/apngasm.git", branch: "master"
 
+  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any,                 arm64_monterey: "80b90951415a88eff1da221b4826a2f1511fa5322ba04c449f3aeec393e0999b"
     sha256 cellar: :any,                 arm64_big_sur:  "d4bbbe2f3dce2bf221ee011f7f192ca1b5c2e1440214bc0105d09a0c0be356bd"
     sha256 cellar: :any,                 monterey:       "2cfe286cfa16d59d200bb5b1b5f37afa66d01e270c976cc179f82dc70e6e1fe5"
     sha256 cellar: :any,                 big_sur:        "4b7399bdb7991330dee6a26a00ee1ec58e6074895b026fda608950b0372525d7"
     sha256 cellar: :any,                 catalina:       "bcba805553ae564c117609755b2f4c3e06f1c23579430505fa89fda8c126c06c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "428941624e3fc27d7ac937ac7eaa016724abafa63119969b01bfd4159421cbba"
   end
 
   depends_on "cmake" => :build
@@ -22,10 +22,6 @@ class Apngasm < Formula
   depends_on "libpng"
   depends_on "lzlib"
   depends_on macos: :catalina
-
-  on_linux do
-    depends_on "gcc"
-  end
 
   fails_with :gcc do
     version "7"
