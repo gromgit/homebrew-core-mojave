9464168794e423da2322de7e68801f64de37b012
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-01 21:23:46 +0800

    bee: no rebuilding required

--- Formula/bee.rb
+++ Formula/bee.rb
@@ -1,20 +1,20 @@
 class Bee < Formula
   desc "Tool for managing database changes"
   homepage "https://github.com/bluesoft/bee"
-  url "https://github.com/bluesoft/bee/releases/download/1.97/bee-1.97.zip"
-  sha256 "d97d7ef0b3dff27cb4c9eb5c9da3831fdf91105208fbae26cef46f86e1b8fbf3"
+  url "https://github.com/bluesoft/bee/releases/download/1.98/bee-1.98.zip"
+  sha256 "1ee5f4f902103d73d6711c4378e1ed14774e795778132973fadf2173ad6cc0ec"
   license "MPL-1.1"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "95a35b0bb062508bb8b7fcc3f79e485ddc4f06583f41b272d397f347ec93662e"
+    sha256 cellar: :any_skip_relocation, all: "a6dc0a2c6a83e541ee2c9a4daf767b4a4dc88bde97c6454248f3311ca17970ca"
   end
 
-  depends_on "openjdk@8"
+  depends_on "openjdk"
 
   def install
     rm_rf Dir["bin/*.bat"]
     libexec.install Dir["*"]
-    (bin/"bee").write_env_script libexec/"bin/bee", Language::Java.java_home_env("1.8")
+    (bin/"bee").write_env_script libexec/"bin/bee", Language::Java.java_home_env
   end
 
   test do
