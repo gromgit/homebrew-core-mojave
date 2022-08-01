2d4aebe58bbb29970e9dda3848444f819932fcfe
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-01 21:24:18 +0800

    python-tabulate: no rebuilding required

--- Formula/python-tabulate.rb
+++ Formula/python-tabulate.rb
@@ -4,13 +4,14 @@ class PythonTabulate < Formula
   url "https://files.pythonhosted.org/packages/7a/53/afac341569b3fd558bf2b5428e925e2eb8753ad9627c1f9188104c6e0c4a/tabulate-0.8.10.tar.gz"
   sha256 "6c57f3f3dd7ac2782770155f3adb2db0b1a269637e42f27599925e64b114f519"
   license "MIT"
+  revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "96aade7fd67304139f7c2ba7e3585393348fff2b7c9cc2067269cecccfdd9aec"
+    sha256 cellar: :any_skip_relocation, all: "e521ca072a0ffb9f7e0cd463b7b9988a8285584172dabb197663c1b60ab3c1c8"
   end
 
   depends_on "libpython-tabulate"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     # Install the binary only, the lib part is provided by libpython-tabulate
