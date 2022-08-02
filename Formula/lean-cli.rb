fbfaf77b8851ca68b0d82d3e64e695fee8ca0975
Adrian Ho <the.gromgit@gmail.com>
2022-08-03 00:32:07 +0800

    lean-cli: update 1.0.2 bottle.

--- Formula/lean-cli.rb
+++ Formula/lean-cli.rb
@@ -1,14 +1,14 @@
 class LeanCli < Formula
   desc "Command-line tool to develop and manage LeanCloud apps"
   homepage "https://github.com/leancloud/lean-cli"
-  url "https://github.com/leancloud/lean-cli/archive/v1.0.1.tar.gz"
-  sha256 "d1cd31c4942a1d321ce6c608973f32f513df892b1441930169bc825149f77e98"
+  url "https://github.com/leancloud/lean-cli/archive/v1.0.2.tar.gz"
+  sha256 "4d844ee3a216da8c2aa720adb936d8364280439f9839f8cd6eca59d91321f742"
   license "Apache-2.0"
   head "https://github.com/leancloud/lean-cli.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lean-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "8f9702584eeb6c8b4eee5570834c5ad518c49736d44edd4b2f5ac3c713a3866b"
+    sha256 cellar: :any_skip_relocation, mojave: "dd39ebb13c3a217ed415051aacea08073d3db46dfc44c12bf7d78c55a57e85c9"
   end
 
   # Bump to 1.18 on the next release, if possible.
