cc8af71b2b28c1df57fcc662877746781ab98405
Adrian Ho <the.gromgit@gmail.com>
2022-08-02 20:38:10 +0800

    git-xargs: update 0.1.1 bottle.

--- Formula/git-xargs.rb
+++ Formula/git-xargs.rb
@@ -1,13 +1,13 @@
 class GitXargs < Formula
   desc "CLI for making updates across multiple Github repositories with a single command"
   homepage "https://github.com/gruntwork-io/git-xargs"
-  url "https://github.com/gruntwork-io/git-xargs/archive/v0.1.0.tar.gz"
-  sha256 "449ddfe8f76a3dfd79b415a11f8c531dcc1913be7173d57f269dcaf8f36b65b5"
+  url "https://github.com/gruntwork-io/git-xargs/archive/v0.1.1.tar.gz"
+  sha256 "7a80f457ee15851c7a8828ede6d40ad052654e36f86a3a582cebc7a28b9ee12f"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-xargs"
-    sha256 cellar: :any_skip_relocation, mojave: "997ba05afe58bb87b72954ebcfcbf3a917c7b06afd09bae5fa90cfd47c2de162"
+    sha256 cellar: :any_skip_relocation, mojave: "5e22fc8b246f2ed6570f7ce594fb15009352364b7713c34a48786904b3f88bf2"
   end
 
   depends_on "go" => :build
