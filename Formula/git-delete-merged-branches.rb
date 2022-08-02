adcaaaeb67b7f3f5c74ae46b255c3abf36a4d4a3
Adrian Ho <the.gromgit@gmail.com>
2022-08-02 20:32:06 +0800

    git-delete-merged-branches: update 7.0.0 bottle.

---
+++ Formula/git-delete-merged-branches.rb
@@ -0,0 +1,59 @@
+class GitDeleteMergedBranches < Formula
+  include Language::Python::Virtualenv
+
+  desc "Command-line tool to delete merged Git branches"
+  homepage "https://github.com/hartwork/git-delete-merged-branches"
+  url "https://files.pythonhosted.org/packages/61/2e/9c15363d21e85b91d1c6dc4253b2ae0d6d5a77a140474a3969a239786d49/git-delete-merged-branches-7.0.0.tar.gz"
+  sha256 "0ac1a543e4c5b585339c77d3abb14236516637a143e5e1ecc24f9b2ee2123433"
+  license "GPL-3.0-only"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-delete-merged-branches"
+    sha256 cellar: :any_skip_relocation, mojave: "14e13cdb4e08270d66e270a92cacfab452d1efe5693578eecd41d7f90c52c7c4"
+  end
+
+  depends_on "python@3.10"
+
+  resource "colorama" do
+    url "https://files.pythonhosted.org/packages/2b/65/24d033a9325ce42ccbfa3ca2d0866c7e89cc68e5b9d92ecaba9feef631df/colorama-0.4.5.tar.gz"
+    sha256 "e6c6b4334fc50988a639d9b98aa429a0b57da6e17b9a44f0451f930b6967b7a4"
+  end
+
+  resource "prompt-toolkit" do
+    url "https://files.pythonhosted.org/packages/c5/7e/71693dc21d20464e4cd7c600f2d8fad1159601a42ed55566500272fe69b5/prompt_toolkit-3.0.30.tar.gz"
+    sha256 "859b283c50bde45f5f97829f77a4674d1c1fcd88539364f1b28a37805cfd89c0"
+  end
+
+  resource "wcwidth" do
+    url "https://files.pythonhosted.org/packages/89/38/459b727c381504f361832b9e5ace19966de1a235d73cdbdea91c771a1155/wcwidth-0.2.5.tar.gz"
+    sha256 "c4d647b99872929fdb7bdcaa4fbe7f01413ed3d98077df798530e5b04f116c83"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    origin = testpath/"origin"
+    origin.mkdir
+    clone = testpath/"clone"
+
+    cd origin do
+      system "git", "init"
+      system "git", "commit", "--allow-empty", "-m", "Initial commit"
+    end
+
+    system "git", "clone", origin, clone
+
+    cd clone do
+      system "git", "config", "remote.origin.dmb-enabled", "true"
+      system "git", "config", "branch.master.dmb-required", "true"
+      system "git", "config", "delete-merged-branches.configured", "5.0.0+"
+      system "git", "checkout", "-b", "new-branch"
+      system "git", "checkout", "-"
+      system "git", "delete-merged-branches", "--yes"
+      branches = shell_output("git branch").split("\n")
+      assert_equal 1, branches.length
+    end
+  end
+end
