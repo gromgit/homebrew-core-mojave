c5f62346ab34f01cab3bacdeae1dd3481a28de8c
Adrian Ho <the.gromgit@gmail.com>
2022-08-03 09:50:26 +0800

    pulumi: update 3.37.2 bottle.

--- Formula/pulumi.rb
+++ Formula/pulumi.rb
@@ -2,14 +2,14 @@ class Pulumi < Formula
   desc "Cloud native development platform"
   homepage "https://pulumi.io/"
   url "https://github.com/pulumi/pulumi.git",
-      tag:      "v3.37.1",
-      revision: "8fc9bc906cebfb87604f42aa1c8dfc2ccc2ab961"
+      tag:      "v3.37.2",
+      revision: "0ba7aaf8c789d787267e75ce8c938d0de067c311"
   license "Apache-2.0"
   head "https://github.com/pulumi/pulumi.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pulumi"
-    sha256 cellar: :any_skip_relocation, mojave: "789ceadda4cfe9378de34e8e06e7e441664de5068b8981d306aebf301fe1c51f"
+    sha256 cellar: :any_skip_relocation, mojave: "99b20f979683aef68cec7d480ff5b437c234637cb912c50f3b4b8374fabe990a"
   end
 
   depends_on "go" => :build
