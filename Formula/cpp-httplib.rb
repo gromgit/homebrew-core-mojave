59161d4c2b5bbc361e8a7314f49dd754b1c35c1e
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-04 09:58:03 +0800

    cpp-httplib: no rebuilding required

---
+++ Formula/cpp-httplib.rb
@@ -0,0 +1,63 @@
+class CppHttplib < Formula
+  desc "C++ header-only HTTP/HTTPS server and client library"
+  homepage "https://github.com/yhirose/cpp-httplib"
+  url "https://github.com/yhirose/cpp-httplib/archive/refs/tags/v0.11.0.tar.gz"
+  sha256 "9f533b0aa67066bd8f049e080eef75ad710a2f3ba3d80edbb13957389a5eeca7"
+  license "MIT"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "d3bbe07f1f79a8753debd5788d01946558f6cd3c770759099737f1664dbb54f9"
+  end
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+
+  def install
+    system "meson", "build", *std_meson_args
+    system "meson", "compile", "-C", "build"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    (testpath/"server.cpp").write <<~CPP
+      #include <httplib.h>
+      using namespace httplib;
+
+      int main(void) {
+        Server svr;
+
+        svr.Get("/hi", [](const Request &, Response &res) {
+          res.set_content("Hello World!", "text/plain");
+        });
+
+        svr.listen("0.0.0.0", 8080);
+      }
+    CPP
+    (testpath/"client.cpp").write <<~CPP
+      #include <httplib.h>
+      #include <iostream>
+      using namespace httplib;
+      using namespace std;
+
+      int main(void) {
+        Client cli("localhost", 8080);
+        if (auto res = cli.Get("/hi")) {
+          cout << res->status << endl;
+          cout << res->get_header_value("Content-Type") << endl;
+          cout << res->body << endl;
+          return 0;
+        } else {
+          return 1;
+        }
+      }
+    CPP
+    system ENV.cxx, "server.cpp", "-I#{include}", "-lpthread", "-std=c++11", "-o", "server"
+    system ENV.cxx, "client.cpp", "-I#{include}", "-lpthread", "-std=c++11", "-o", "client"
+
+    fork do
+      exec "./server"
+    end
+    sleep 3
+    assert_match "Hello World!", shell_output("./client")
+  end
+end
