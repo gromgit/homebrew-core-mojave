2ab92c98c811a97e420445d8f897752675f4117f
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-04 09:58:47 +0800

    timewarrior: no rebuilding required

--- Formula/timewarrior.rb
+++ Formula/timewarrior.rb
@@ -7,7 +7,6 @@ class Timewarrior < Formula
   revision 1
   head "https://github.com/GothenburgBitFactory/timewarrior.git", branch: "dev"
 
-  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "712cd5e3d2766ce2d32e87799c619547cb65084c9e0d23ffb2b2aa5136cb63b6"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c237684f26cc8b20162ab44dc3fd1822317bcc7988004ebfaaae1b262706a9c0"
@@ -15,6 +14,7 @@ class Timewarrior < Formula
     sha256 cellar: :any_skip_relocation, big_sur:        "408828a1e987d46c028e984d205a05e08e9f1f1c22f2e06f63d83f1c4103abed"
     sha256 cellar: :any_skip_relocation, catalina:       "bfd843a753ea65204f5c2d125a6bfcb97034e5b0ca997c5c224338755e4a1b36"
     sha256 cellar: :any_skip_relocation, mojave:         "adcac7b2fe1e61a589a674b581ad77b2d0f6e6646454d12da192ceb8ff4d8dd1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a2639fbb7dc4ec4c629539f10ac0d0766bad0b01100c9a25a00f5d2bf9185267"
   end
 
   depends_on "asciidoctor" => :build
@@ -22,6 +22,7 @@ class Timewarrior < Formula
 
   on_linux do
     depends_on "man-db" => :test
+    depends_on "gcc"
   end
 
   fails_with gcc: "5"
