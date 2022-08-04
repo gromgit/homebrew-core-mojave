a8fca26bf15f46de60fd4ef3a7d9d36de28a3368
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-04 09:58:31 +0800

    pdfsandwich: no rebuilding required

--- Formula/pdfsandwich.rb
+++ Formula/pdfsandwich.rb
@@ -6,7 +6,6 @@ class Pdfsandwich < Formula
   revision 4
   head "https://svn.code.sf.net/p/pdfsandwich/code/trunk/src"
 
-  # Linux bottle removed for GCC 12 migration
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_monterey: "a6af2fc71eb56e9f121e035b6348a1fa984989096a7158b963a84d5f7b92cc44"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9d03e5564d606b37f3b2aaa2dc68837ca023e87c455c9543836a854ec7728c4f"
@@ -14,6 +13,7 @@ class Pdfsandwich < Formula
     sha256 cellar: :any_skip_relocation, big_sur:        "eed36d608adf9c4c6a7bcfa2f8d51fc7d7db6b9625d8dd87420b0a49432ed099"
     sha256 cellar: :any_skip_relocation, catalina:       "e45ad2480a96ef2ff2ee1a0a561004510d3d3f2b61117fce51d2995b5a004b34"
     sha256 cellar: :any_skip_relocation, mojave:         "dd4a617ef7bb8bb83cb9da94556537624bcf188789846e978ccb25926fcd7027"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6f77d1aa373059a0b32d879bc45f075527a28c6a0a2068b8d38d634dfd2d7d60"
   end
 
   depends_on "gawk" => :build
