bd086927862defc98b4ade1f29f4bf4d1c77ba73
Adrian Ho <the.gromgit@gmail.com>
2022-08-05 15:57:00 +0800

    cargo-nextest: update 0.9.33 bottle.

---
+++ Formula/cargo-nextest.rb
@@ -0,0 +1,51 @@
+class CargoNextest < Formula
+  desc "Next-generation test runner for Rust"
+  homepage "https://nexte.st"
+  url "https://github.com/nextest-rs/nextest/archive/refs/tags/cargo-nextest-0.9.33.tar.gz"
+  sha256 "f75c53b4df3c72ae5a89e75c15d571645ee6719d86bb1171d64d0281eb71361f"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^cargo-nextest[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cargo-nextest"
+    sha256 cellar: :any_skip_relocation, mojave: "355fdafffd757f67cae7a2ef70eae88b56b72e4cafd483487329686ee3f994f1"
+  end
+
+  depends_on "rust" # uses `cargo` at runtime
+
+  def install
+    # Fix a performance regression. This can be removed once Rust 1.64 is stable.
+    # See https://github.com/nextest-rs/nextest/releases/tag/cargo-nextest-0.9.30
+    ENV["RUSTC_BOOTSTRAP"] = "1"
+    ENV["RUSTFLAGS"] = "--cfg process_group --cfg process_group_bootstrap_hack"
+    system "cargo", "install", "--no-default-features", "--features", "default-no-update",
+                    *std_cargo_args(path: "cargo-nextest")
+  end
+
+  test do
+    crate = testpath/"demo-crate"
+    mkdir crate do
+      (crate/"src/main.rs").write <<~EOS
+        #[cfg(test)]
+        mod tests {
+          #[test]
+          fn test_it() {
+            assert_eq!(1 + 1, 2);
+          }
+        }
+      EOS
+      (crate/"Cargo.toml").write <<~EOS
+        [package]
+        name = "demo-crate"
+        version = "0.1.0"
+      EOS
+
+      output = shell_output("cargo nextest run 2>&1")
+      assert_match "Starting 1 tests across 1 binaries", output
+    end
+  end
+end
