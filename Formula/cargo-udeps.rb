0a9f90497d72a3ce7989955675b4dfad71389db3
Adrian Ho <the.gromgit@gmail.com>
2022-08-05 16:08:32 +0800

    cargo-udeps: update 0.1.30 bottle.

---
+++ Formula/cargo-udeps.rb
@@ -0,0 +1,42 @@
+class CargoUdeps < Formula
+  desc "Find unused dependencies in Cargo.toml"
+  homepage "https://github.com/est31/cargo-udeps"
+  url "https://github.com/est31/cargo-udeps/archive/refs/tags/v0.1.30.tar.gz"
+  sha256 "a1dd8b533fa915783919b78705d4a377fb021b67f8386023866017a139caf935"
+  license any_of: ["Apache-2.0", "MIT"]
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cargo-udeps"
+    sha256 cellar: :any, mojave: "090eab123d478732237bb10b9ab512dbcc8d19aa39092b1526c59888addad290"
+  end
+
+  depends_on "rust" => [:build, :test]
+
+  on_linux do
+    depends_on "pkg-config" => :build
+    depends_on "openssl@1.1"
+  end
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    crate = testpath/"demo-crate"
+    mkdir crate do
+      (crate/"src/main.rs").write "// Dummy file"
+      (crate/"Cargo.toml").write <<~EOS
+        [package]
+        name = "demo-crate"
+        version = "0.1.0"
+
+        [dependencies]
+        clap = "3"
+      EOS
+
+      output = shell_output("cargo udeps 2>&1", 101)
+      # `cargo udeps` can be installed on Rust stable, but only runs with cargo with `cargo +nightly udeps`
+      assert_match "error: the option `Z` is only accepted on the nightly compiler", output
+    end
+  end
+end
