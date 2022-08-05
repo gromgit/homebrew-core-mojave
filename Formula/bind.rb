e00d8fd6888bd2edd15a8c5fa9b92a7bc919332f
Adrian Ho <the.gromgit@gmail.com>
2022-08-05 13:20:22 +0800

    bind: update 9.18.5 bottle.

--- Formula/bind.rb
+++ Formula/bind.rb
@@ -8,8 +8,8 @@ class Bind < Formula
   # "version_scheme" because someone upgraded to 9.15.0, and required a
   # downgrade.
 
-  url "https://downloads.isc.org/isc/bind9/9.18.4/bind-9.18.4.tar.xz"
-  sha256 "f277ae50159a00c300eb926a9c5d51953038a936bd8242d6913dfb6eac42761d"
+  url "https://downloads.isc.org/isc/bind9/9.18.5/bind-9.18.5.tar.xz"
+  sha256 "0cee078d74f0bdc4ec374435026b25de7892f26540a18b22a02ef728a11dcae7"
   license "MPL-2.0"
   version_scheme 1
   head "https://gitlab.isc.org/isc-projects/bind9.git", branch: "main"
@@ -23,7 +23,7 @@ class Bind < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bind"
-    sha256 mojave: "130994ac39d3425484f72c058d459a50b3522d676a06cb8baa93641e2c7eecef"
+    sha256 mojave: "18d96eca17360b0b297a018656669dc0c566a7a853374d124f4f309289f36f5a"
   end
 
   depends_on "pkg-config" => :build
@@ -31,7 +31,7 @@ class Bind < Formula
   depends_on "libidn2"
   depends_on "libnghttp2"
   depends_on "libuv"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   def install
     args = [
@@ -40,7 +40,7 @@ class Bind < Formula
       "--localstatedir=#{var}",
       "--with-json-c",
       "--with-libidn2=#{Formula["libidn2"].opt_prefix}",
-      "--with-openssl=#{Formula["openssl@1.1"].opt_prefix}",
+      "--with-openssl=#{Formula["openssl@3"].opt_prefix}",
       "--without-lmdb",
     ]
     args << "--disable-linux-caps" if OS.linux?
