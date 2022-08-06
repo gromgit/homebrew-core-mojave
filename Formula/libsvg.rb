c70f223c67142684a96b894596f8c22576aa6ec0
Adrian Ho <the.gromgit@gmail.com>
2022-08-06 17:27:39 +0800

    libsvg: update 0.1.4_2 bottle.

--- Formula/libsvg.rb
+++ Formula/libsvg.rb
@@ -4,7 +4,7 @@ class Libsvg < Formula
   url "https://cairographics.org/snapshots/libsvg-0.1.4.tar.gz"
   sha256 "4c3bf9292e676a72b12338691be64d0f38cd7f2ea5e8b67fbbf45f1ed404bc8f"
   license "LGPL-2.1-or-later"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://cairographics.org/snapshots/"
@@ -12,24 +12,15 @@ class Libsvg < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "b969b2900a6b58d0d9dbab2e75a4acf80710b94e4efd3834a3a83f154546e7e4"
-    sha256 cellar: :any,                 arm64_big_sur:  "4b7fe8ed8d541c65566d01ce26d3d31a6f051c13d24e947000140de55fe01164"
-    sha256 cellar: :any,                 monterey:       "7907ff895333961593131035febcafd23397f791b80cb0b58d33919b3291bba5"
-    sha256 cellar: :any,                 big_sur:        "96c398556141fc2ad73955de0c52a0217eeeb627102099592d1ccc85250809c9"
-    sha256 cellar: :any,                 catalina:       "e0f21af595963a7c99ffa098f593f5d46cf5f78facf1df84ffe97858f29fecbe"
-    sha256 cellar: :any,                 mojave:         "3984d65fa6524a142ad9094aa095f106ca9c8b6857cdd3f62b913e7e3c8f5b65"
-    sha256 cellar: :any,                 high_sierra:    "7cfe0b5417654beb7092afec3389a14a4c67eeaa760eb77c9b28082e40f0b11a"
-    sha256 cellar: :any,                 sierra:         "c9435455e3fb30ce81d467edf1cf4c15c39fb1d061c21738007d6af2565455a7"
-    sha256 cellar: :any,                 el_capitan:     "4e7903c15847c2d07a2bdf16d6ddad5a0191ef452cf7733624703fd1b5fd7859"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0c894d2f375a55bfe838e43f76176168aafc12474d4c25f6be612de3d240ea3b"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libsvg"
+    sha256 cellar: :any, mojave: "c3f597498e0d30d8f094715fb04062f0ffc4ab940a6cc1423bde5ec9fc428bd1"
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "jpeg"
+  depends_on "jpeg-turbo"
   depends_on "libpng"
 
   uses_from_macos "libxml2"
@@ -45,8 +36,8 @@ class Libsvg < Formula
   patch :DATA
 
   def install
-    system "autoreconf", "-fiv"
-    system "./configure", "--prefix=#{prefix}"
+    system "autoreconf", "--force", "--install", "--verbose"
+    system "./configure", *std_configure_args
     system "make", "install"
   end
 
@@ -129,7 +120,9 @@ class Libsvg < Formula
     system ENV.cc, "test.c", "-o", "test",
                    "-I#{include}", "-L#{lib}", "-lsvg",
                    "-L#{Formula["libpng"].opt_lib}", "-lpng",
-                   "-L#{Formula["jpeg"].opt_lib}", "-ljpeg"
+                   "-L#{Formula["jpeg-turbo"].opt_lib}", "-ljpeg",
+                   "-Wl,-rpath,#{Formula["jpeg-turbo"].opt_lib}",
+                   "-Wl,-rpath,#{HOMEBREW_PREFIX}/lib"
     assert_equal "1\n2\n3\n4\n5\n6\nSUCCESS\n", shell_output("./test")
   end
 end
