b17742c4971f93cf449dc01e7d0a929d1acf25a9
Adrian Ho <the.gromgit@gmail.com>
2022-08-06 18:40:27 +0800

    links: update 2.27_1 bottle.

--- Formula/links.rb
+++ Formula/links.rb
@@ -4,6 +4,7 @@ class Links < Formula
   url "http://links.twibright.com/download/links-2.27.tar.bz2"
   sha256 "d8ddcbfcede7cdde80abeb0a236358f57fa6beb2bcf92e109624e9b896f9ebb4"
   license "GPL-2.0-or-later"
+  revision 1
 
   livecheck do
     url "http://links.twibright.com/download.php"
@@ -12,27 +13,20 @@ class Links < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/links"
-    rebuild 1
-    sha256 cellar: :any, mojave: "27f9a4b7f2ef11d4cc7adeba35f5b0e33144abbea3b3568e15439b70e6e8aeb4"
+    sha256 cellar: :any, mojave: "e4b85e1966820631ee4bb247cd046d02ade656c6a6f4ad8ff10702982a5c5e8e"
   end
 
   depends_on "pkg-config" => :build
-  depends_on "jpeg"
-  depends_on "librsvg"
-  depends_on "libtiff"
   depends_on "openssl@1.1"
 
-  def install
-    args = %W[
-      --disable-debug
-      --disable-dependency-tracking
-      --prefix=#{prefix}
-      --mandir=#{man}
-      --with-ssl=#{Formula["openssl@1.1"].opt_prefix}
-      --without-lzma
-    ]
+  uses_from_macos "bzip2"
+  uses_from_macos "zlib"
 
-    system "./configure", *args
+  def install
+    system "./configure", *std_configure_args,
+                          "--mandir=#{man}",
+                          "--with-ssl=#{Formula["openssl@1.1"].opt_prefix}",
+                          "--without-lzma"
     system "make", "install"
     doc.install Dir["doc/*"]
   end
