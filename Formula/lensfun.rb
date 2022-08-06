edd7c8f4440d3bebbbc01b2fe106d4640044dbff
Adrian Ho <the.gromgit@gmail.com>
2022-08-06 15:49:19 +0800

    lensfun: update 0.3.3_1 bottle.

--- Formula/lensfun.rb
+++ Formula/lensfun.rb
@@ -11,6 +11,7 @@ class Lensfun < Formula
     "CC-BY-3.0",
     :public_domain,
   ]
+  revision 1
   version_scheme 1
   head "https://github.com/lensfun/lensfun.git", branch: "master"
 
@@ -21,7 +22,7 @@ class Lensfun < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lensfun"
-    sha256 mojave: "d5912b4a9c5f56025141c9421553f77c886bda509028dc6b5434003f401c0907"
+    sha256 mojave: "87745747e4040f0bc14cb9b3b311eaa458e6474983d5249c822c86677007dc26"
   end
 
   depends_on "cmake" => :build
@@ -29,13 +30,20 @@ class Lensfun < Formula
   depends_on "gettext"
   depends_on "glib"
   depends_on "libpng"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     # setuptools>=60 prefers its own bundled distutils, which breaks the installation
     ENV["SETUPTOOLS_USE_DISTUTILS"] = "stdlib"
-    system "cmake", ".", *std_cmake_args
-    system "make", "install"
+
+    # Work around Homebrew's "prefix scheme" patch which causes non-pip installs
+    # to incorrectly try to write into HOMEBREW_PREFIX/lib since Python 3.10.
+    site_packages = prefix/Language::Python.site_packages("python3")
+    inreplace "apps/CMakeLists.txt", "${SETUP_PY} install ", "\\0 --install-lib=#{site_packages} "
+
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
 
     rewrite_shebang detected_python_shebang,
       bin/"lensfun-add-adapter", bin/"lensfun-convert-lcp", bin/"lensfun-update-data"
