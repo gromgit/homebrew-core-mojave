56c8bcc700b0a5d783e8849f5ce4eb0d5d412e95
Adrian Ho <the.gromgit@gmail.com>
2022-08-06 08:55:35 +0800

    jpegoptim: update 1.4.7_1 bottle.

--- Formula/jpegoptim.rb
+++ Formula/jpegoptim.rb
@@ -4,19 +4,18 @@ class Jpegoptim < Formula
   url "https://github.com/tjko/jpegoptim/archive/RELEASE.1.4.7.tar.gz"
   sha256 "9d2a13b7c531d122f360209422645206931c74ada76497c4aeb953610f0d70c1"
   license "GPL-3.0-or-later"
+  revision 1
   head "https://github.com/tjko/jpegoptim.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jpegoptim"
-    sha256 cellar: :any, mojave: "08a4bcb2962fec7ec87644985f1bf02ccfe6aaab516ffb8410ed2487993bc8a8"
+    sha256 cellar: :any, mojave: "c999aca7bae06e1674564d4569ac731724ec5672ebee8c53d65cfabb8370a3f8"
   end
 
-  depends_on "jpeg"
+  depends_on "jpeg-turbo"
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", *std_configure_args
     ENV.deparallelize # Install is not parallel-safe
     system "make", "install"
   end
