c7f5ec79df970fd0e9fd77e3c4e8d7d139f50b06
Adrian Ho <the.gromgit@gmail.com>
2022-08-07 17:46:49 +0800

    oha: update 0.5.3 bottle.

--- Formula/oha.rb
+++ Formula/oha.rb
@@ -1,14 +1,14 @@
 class Oha < Formula
   desc "HTTP load generator, inspired by rakyll/hey with tui animation"
   homepage "https://github.com/hatoo/oha/"
-  url "https://github.com/hatoo/oha/archive/refs/tags/v0.5.1.tar.gz"
-  sha256 "1879b2e155679241560ce805965bbd7ac5fb1b602115d97b2b31afe8a0964051"
+  url "https://github.com/hatoo/oha/archive/refs/tags/v0.5.3.tar.gz"
+  sha256 "f49c3584746d387b83038bfa58039d9edf5148d0fb625403184e8d997b673105"
   license "MIT"
   head "https://github.com/hatoo/oha.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/oha"
-    sha256 cellar: :any_skip_relocation, mojave: "6d0e263260fab4d5d6ba90ed9291ab2604fbdc9493629e0cab6f8806bd95094b"
+    sha256 cellar: :any_skip_relocation, mojave: "eb76b0d311d9b5ccd36586edc4b09bd342df08429b48640d136f8c3cd2a3f2bf"
   end
 
   depends_on "rust" => :build
