21d44598ab98f270883b9f597fb013f2611c129f
Adrian Ho <the.gromgit@gmail.com>
2022-08-07 23:51:40 +0800

    rover: update 0.8.1 bottle.

--- Formula/rover.rb
+++ Formula/rover.rb
@@ -1,14 +1,14 @@
 class Rover < Formula
   desc "CLI for managing and maintaining data graphs with Apollo Studio"
   homepage "https://www.apollographql.com/docs/rover/"
-  url "https://github.com/apollographql/rover/archive/v0.5.4.tar.gz"
-  sha256 "1728ee7eb5d87d9838972c1c441a7323315ded01c09d1edc7cf65c0e201a9fc9"
+  url "https://github.com/apollographql/rover/archive/v0.8.1.tar.gz"
+  sha256 "85ae6a9ca5c81f9b30cfbf56130dddad9b57e2fc895a0eccf27a88dd619ae905"
   license "MIT"
   head "https://github.com/apollographql/rover.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rover"
-    sha256 cellar: :any_skip_relocation, mojave: "ca145e148760137bf06eb719e1bdac0caec1178e9be149b16ff93de7712e0da4"
+    sha256 cellar: :any_skip_relocation, mojave: "a173e2c2510f8ed00466007935e9c1b0835fdb03e1f973976da4ecdcb3d4d397"
   end
 
   depends_on "rust" => :build
