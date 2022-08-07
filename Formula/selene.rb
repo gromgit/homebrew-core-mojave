7b13a880a192c33a38cb7924a8ff20db8b18ff6f
Adrian Ho <the.gromgit@gmail.com>
2022-08-08 01:28:50 +0800

    selene: update 0.20.0 bottle.

--- Formula/selene.rb
+++ Formula/selene.rb
@@ -1,14 +1,14 @@
 class Selene < Formula
   desc "Blazing-fast modern Lua linter"
   homepage "https://kampfkarren.github.io/selene"
-  url "https://github.com/Kampfkarren/selene/archive/0.18.2.tar.gz"
-  sha256 "16401ee92f0df8d6384160110c5f30e5935b4cd687a6d24de4e744278223ce30"
+  url "https://github.com/Kampfkarren/selene/archive/0.20.0.tar.gz"
+  sha256 "ff37af1ef978ff66cd77d03396185b8abcaacb9860f1f1195d01325b4874cfd9"
   license "MPL-2.0"
   head "https://github.com/Kampfkarren/selene.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/selene"
-    sha256 cellar: :any_skip_relocation, mojave: "83ff60843ff1e5fef7220c5f5234df00cfc2fbdca7812451a545ce683c43a22a"
+    sha256 cellar: :any_skip_relocation, mojave: "5fe673c840a696bfba3b243d900f9075e1b2168cedf1beb4b229ad72f8863683"
   end
 
   depends_on "rust" => :build
