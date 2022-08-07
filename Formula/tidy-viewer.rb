1907b14cee28b15731a6333ad079c77d8ec09d10
Adrian Ho <the.gromgit@gmail.com>
2022-08-08 07:56:13 +0800

    tidy-viewer: update 1.4.6 bottle.

--- Formula/tidy-viewer.rb
+++ Formula/tidy-viewer.rb
@@ -1,13 +1,19 @@
 class TidyViewer < Formula
   desc "CLI csv pretty printer"
   homepage "https://github.com/alexhallam/tv"
-  url "https://github.com/alexhallam/tv/archive/refs/tags/1.4.5.tar.gz"
-  sha256 "06906324bc510698651e57b1dfe9a28301ccbdd509f079bf09ce7f6d2f2fad2b"
+  url "https://github.com/alexhallam/tv/archive/refs/tags/1.4.6.tar.gz"
+  sha256 "e9a2fc904f2e115c715df80421c39e0f226b6750a56db96d994acfe9336ec219"
   license "Unlicense"
+
+  livecheck do
+    url "https://github.com/alexhallam/tv/releases?q=prerelease%3Afalse"
+    regex(%r{href=["']?[^"' >]*?/tag/v?(\d+(?:\.\d+)+)(?:[._-]release)?["' >]}i)
+    strategy :page_match
+  end
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tidy-viewer"
-    sha256 cellar: :any_skip_relocation, mojave: "8cfd967ccd0fe9da24ad8621cefedc1ae3d70f83a2d77426efd2eec30969b9b2"
+    sha256 cellar: :any_skip_relocation, mojave: "3a8293510455645435b6c8247b61217c00249ed607d71c80089779cddb756a1b"
   end
 
   depends_on "rust" => :build
