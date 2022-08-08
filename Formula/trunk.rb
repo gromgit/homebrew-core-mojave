6d199bb2bbcdb82861a38034c8c4aeaadf7acea6
Adrian Ho <the.gromgit@gmail.com>
2022-08-08 09:35:58 +0800

    trunk: update 0.16.0 bottle.

--- Formula/trunk.rb
+++ Formula/trunk.rb
@@ -1,14 +1,14 @@
 class Trunk < Formula
   desc "Build, bundle & ship your Rust WASM application to the web"
   homepage "https://github.com/thedodd/trunk"
-  url "https://github.com/thedodd/trunk/archive/v0.15.0.tar.gz"
-  sha256 "cb11302aa1d41ad6cd2ad7734a6717a4d4a947d59605e831adbf84df1940618e"
+  url "https://github.com/thedodd/trunk/archive/v0.16.0.tar.gz"
+  sha256 "035f3508ad3954aa1117f662f9f59541e7a0059483b15d573a8146d997b54827"
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/thedodd/trunk.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/trunk"
-    sha256 cellar: :any_skip_relocation, mojave: "d509c131b64827a8215a27bd11957c3064b7c46337c4ce6b33a85d34d88c0607"
+    sha256 cellar: :any_skip_relocation, mojave: "2ecbc82d0dba8cba795df9625c9b25a4b9f03ecfe16dbd7c3f53fac8b20c89bc"
   end
 
   depends_on "rust" => :build
