397dd962e5414683f1d7349212cc05666d3664ec
Adrian Ho <the.gromgit@gmail.com>
2022-08-08 11:53:16 +0800

    yosys: update 0.20 bottle.

--- Formula/yosys.rb
+++ Formula/yosys.rb
@@ -1,14 +1,14 @@
 class Yosys < Formula
   desc "Framework for Verilog RTL synthesis"
   homepage "https://yosyshq.net/yosys/"
-  url "https://github.com/YosysHQ/yosys/archive/yosys-0.19.tar.gz"
-  sha256 "d527fd88a9f7101c6f8e37889b14add0b6d2c74c2c611295f3813db4f397518f"
+  url "https://github.com/YosysHQ/yosys/archive/yosys-0.20.tar.gz"
+  sha256 "ee261487badf1b554616d555da8496a7c84ef21ae66a979ddd946b6949a780a4"
   license "ISC"
   head "https://github.com/YosysHQ/yosys.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/yosys"
-    sha256 mojave: "252127325103d97fe6dc3ef2b3fc2f84a2e595810423bceffca9144883b191ed"
+    sha256 mojave: "1615b9487f365f3154b44efa1143801c5f55f0f0c6676531e4ca8f5721bdd5f0"
   end
 
   depends_on "bison" => :build
