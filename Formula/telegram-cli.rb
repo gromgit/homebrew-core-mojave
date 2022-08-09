cbf5ca541fa476762d69fdce6076c237dfadc071
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-09 12:36:27 +0800

    telegram-cli:

--- Formula/telegram-cli.rb
+++ Formula/telegram-cli.rb
@@ -22,7 +22,7 @@ class TelegramCli < Formula
 
   # "This project is deprecated and is no longer being maintained.
   # Last commit was in 2016
-  deprecate! date: "2021-06-30", because: :unmaintained
+  disable! date: "2022-07-31", because: :unmaintained
 
   depends_on "pkg-config" => :build
   depends_on "jansson"
