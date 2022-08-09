304edecd4983a2d4aeee3e35905b81ba9a42784b
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-09 12:36:30 +0800

    tkdiff: no rebuilding required

--- Formula/tkdiff.rb
+++ Formula/tkdiff.rb
@@ -1,9 +1,9 @@
 class Tkdiff < Formula
   desc "Graphical side by side diff utility"
   homepage "https://tkdiff.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/tkdiff/tkdiff/5.4/tkdiff-5-4.zip"
-  version "5.4"
-  sha256 "773b0b49fb7519260abd46f1cf6eef7c30042a8c025f3e8eee6422659485326e"
+  url "https://downloads.sourceforge.net/project/tkdiff/tkdiff/5.5/tkdiff-5-5.zip"
+  version "5.5"
+  sha256 "f0ad72a53287000ab791b500c3d47aa78a4bcbdbdd6bf0d665ea187148594ab7"
   license "GPL-2.0-only"
 
   livecheck do
@@ -12,7 +12,7 @@ class Tkdiff < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "60c8912e850e5f029702a02e1c2551ab6e67cd626e8dd2e1a12573c2824e821c"
+    sha256 cellar: :any_skip_relocation, all: "f1a95e44025e58280b6089f291d4594be37875180745cad52451766170012973"
   end
 
   uses_from_macos "tcl-tk"
