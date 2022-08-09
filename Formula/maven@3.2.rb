b941d380eeda10803835bebaa4101cb99001cd08
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-09 12:36:00 +0800

    maven@3.2:

--- Formula/maven@3.2.rb
+++ Formula/maven@3.2.rb
@@ -13,7 +13,7 @@ class MavenAT32 < Formula
 
   keg_only :versioned_formula
 
-  deprecate! date: "2015-11-14", because: :unmaintained
+  disable! date: "2022-07-31", because: :unmaintained
 
   depends_on "openjdk"
 
