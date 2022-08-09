d898af9caed5de60c890b83193ebe12a9ac39496
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-09 12:36:05 +0800

    mysql@5.6:

--- Formula/mysql@5.6.rb
+++ Formula/mysql@5.6.rb
@@ -16,7 +16,7 @@ class MysqlAT56 < Formula
 
   keg_only :versioned_formula
 
-  deprecate! date: "2021-02-01", because: :unsupported
+  disable! date: "2022-07-31", because: :unsupported
 
   depends_on "cmake" => :build
   depends_on "openssl@1.1"
