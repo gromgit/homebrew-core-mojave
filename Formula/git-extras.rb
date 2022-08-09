597bc21ef8e9769deeb433c3d93667154899a0ec
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-09 12:35:43 +0800

    git-extras: no rebuilding required

--- Formula/git-extras.rb
+++ Formula/git-extras.rb
@@ -7,7 +7,8 @@ class GitExtras < Formula
   head "https://github.com/tj/git-extras.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "75cbcd4fe8ad39801a46a6abadc8831aaacdb0bbf6f65eccc190c65c8b220985"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "139de676324d68edf3e958adb4806ef20d9a3d4e7f80588b8d21e5b1ff1feb56"
   end
 
   on_linux do
@@ -16,6 +17,9 @@ class GitExtras < Formula
 
   conflicts_with "git-utils",
     because: "both install a `git-pull-request` script"
+
+  conflicts_with "git-sync",
+    because: "both install a `git-sync` binary"
 
   def install
     system "make", "PREFIX=#{prefix}", "INSTALL_VIA=brew", "install"
