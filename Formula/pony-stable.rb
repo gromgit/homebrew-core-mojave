867e310f325a483348188ba5916e5333228a3003
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-09 12:36:13 +0800

    pony-stable:

--- Formula/pony-stable.rb
+++ Formula/pony-stable.rb
@@ -15,7 +15,7 @@ class PonyStable < Formula
 
   # "Stable is no longer being developed. It's been replaced by Corral
   # (https://github.com/ponylang/corral)."
-  deprecate! date: "2020-05-12", because: :repo_archived
+  disable! date: "2022-07-31", because: :repo_archived
 
   depends_on "ponyc"
 
