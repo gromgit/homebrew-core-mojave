4f7766779a078c61d1747f994c68918eacebdf46
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-09 12:35:21 +0800

    aws-okta:

--- Formula/aws-okta.rb
+++ Formula/aws-okta.rb
@@ -16,7 +16,7 @@ class AwsOkta < Formula
   end
 
   # See https://github.com/segmentio/aws-okta/issues/278
-  deprecate! date: "2020-01-20", because: :deprecated_upstream
+  disable! date: "2022-07-31", because: :deprecated_upstream
 
   depends_on "go" => :build
 
