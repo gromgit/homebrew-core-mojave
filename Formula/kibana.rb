68473f38702bbfd2be4f43a3ddf19c098275d3fd
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-09 12:35:53 +0800

    kibana:

--- Formula/kibana.rb
+++ Formula/kibana.rb
@@ -9,14 +9,13 @@ class Kibana < Formula
   head "https://github.com/elastic/kibana.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kibana"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "bfe36a5db9fc21cfff52961421ff6a3e622441f498f5c1a30aa243a0986e5298"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "308f92ff1e8455e9521c07b3d18d6d8f15b0cf107754620971e6346cfbb39b0e"
   end
 
   # elasticsearch will be relicensed before v7.11.
   # https://www.elastic.co/blog/licensing-change
-  deprecate! date: "2021-01-14", because: "is switching to an incompatible license"
+  disable! date: "2022-07-31", because: "is switching to an incompatible license"
 
   depends_on "python@3.10" => :build
   depends_on "yarn" => :build
