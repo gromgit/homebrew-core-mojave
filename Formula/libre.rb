8b6c9944a0a4903c0ea891037bbaf57da88c6a35
Adrian Ho <the.gromgit@gmail.com>
2022-08-11 01:10:57 +0800

    libre: update 2.6.1 bottle.

--- Formula/libre.rb
+++ Formula/libre.rb
@@ -1,13 +1,13 @@
 class Libre < Formula
   desc "Toolkit library for asynchronous network I/O with protocol stacks"
   homepage "https://github.com/baresip/re"
-  url "https://github.com/baresip/re/archive/refs/tags/v2.5.0.tar.gz"
-  sha256 "937e4d9d94684159d264d8982bad599d594bef00d62170174dba3026fe2bcdcc"
+  url "https://github.com/baresip/re/archive/refs/tags/v2.6.1.tar.gz"
+  sha256 "665c9de5181d4c193d667e99aa23d9c9303e0ee458e97e93806a5a5eb49a2d81"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libre"
-    sha256 cellar: :any, mojave: "5f82ba36b009e7dbc089b76f52868e4e4cec628745542b3828c808cf6d183e87"
+    sha256 cellar: :any, mojave: "b703b1b901337d71e207041a193ce72897a541d6f9a3516921a2bbdc81c416ee"
   end
 
   depends_on "openssl@1.1"
