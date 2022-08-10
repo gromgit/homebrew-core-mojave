d5cdeabc7a95a6cc597850842d9f4b4d3e3cd46e
Adrian Ho <the.gromgit@gmail.com>
2022-08-10 18:51:12 +0800

    gtk-doc: update 1.33.2_1 bottle.

--- Formula/gtk-doc.rb
+++ Formula/gtk-doc.rb
@@ -6,6 +6,7 @@ class GtkDoc < Formula
   url "https://download.gnome.org/sources/gtk-doc/1.33/gtk-doc-1.33.2.tar.xz"
   sha256 "cc1b709a20eb030a278a1f9842a362e00402b7f834ae1df4c1998a723152bf43"
   license "GPL-2.0-or-later"
+  revision 1
 
   # We use a common regex because gtk-doc doesn't use GNOME's
   # "even-numbered minor is stable" version scheme.
@@ -15,13 +16,8 @@ class GtkDoc < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "31f6f5bc5918792ddece0aca021f8726d1fc88b76ef4e6b6ef40dd865624916f"
-    sha256 cellar: :any,                 arm64_big_sur:  "6be94177308c7cb81573af6612f3bfcbf050a1539d2b2de8d8ebbde017fa00f4"
-    sha256 cellar: :any,                 monterey:       "9304600335bd228e2825e875fb8d62e72e2b7d7d9bc2bcd33fe3d41d904f30d9"
-    sha256 cellar: :any,                 big_sur:        "b3d2370a6a3fff63341d86c5313e238af04af84dac34a6a19d6f7a1a77acb885"
-    sha256 cellar: :any,                 catalina:       "cfaf657a0e2b8ac879deaefbd2361a2fe7a8e9b3234f73d0ad32eaa5be4a6beb"
-    sha256 cellar: :any,                 mojave:         "abd68d022b9e19677a00c397fc316370b2106133f3a37c9f9c8b13e2d6b099a0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "415526676543d0f3899bd76944217912d9e37387b0039ee830c887fec03b6ad4"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gtk-doc"
+    sha256 cellar: :any, mojave: "b5b013a8c30182b1c1c84a3ce89a47f2776469540210c39aacd52d12d6fc6701"
   end
 
   depends_on "meson" => :build
@@ -30,7 +26,7 @@ class GtkDoc < Formula
   depends_on "docbook"
   depends_on "docbook-xsl"
   depends_on "libxml2"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   uses_from_macos "libxslt"
 
@@ -57,16 +53,9 @@ class GtkDoc < Formula
     venv.pip_install resources
     ENV.prepend_path "PATH", libexec/"bin"
 
-    args = std_meson_args + %w[
-      -Dtests=false
-      -Dyelp_manual=false
-    ]
-
-    mkdir "build" do
-      system "meson", *args, ".."
-      system "ninja", "-v"
-      system "ninja", "install", "-v"
-    end
+    system "meson", *std_meson_args, "build", "-Dtests=false", "-Dyelp_manual=false"
+    system "meson", "compile", "-C", "build", "-v"
+    system "meson", "install", "-C", "build"
   end
 
   test do
