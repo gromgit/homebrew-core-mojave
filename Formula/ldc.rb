4b80315c7e2d729566404b316f08c4db2bbdcd34
Adrian Ho <the.gromgit@gmail.com>
2022-08-11 22:46:40 +0800

    ldc: update 1.30.0 bottle.

--- Formula/ldc.rb
+++ Formula/ldc.rb
@@ -1,8 +1,8 @@
 class Ldc < Formula
   desc "Portable D programming language compiler"
   homepage "https://wiki.dlang.org/LDC"
-  url "https://github.com/ldc-developers/ldc/releases/download/v1.29.0/ldc-1.29.0-src.tar.gz"
-  sha256 "d0c066eb965467625d9c5e75c00c583451b9ffa363601f9e37275ca8a8aea140"
+  url "https://github.com/ldc-developers/ldc/releases/download/v1.30.0/ldc-1.30.0-src.tar.gz"
+  sha256 "fdbb376f08242d917922a6a22a773980217fafa310046fc5d6459490af23dacd"
   license "BSD-3-Clause"
   head "https://github.com/ldc-developers/ldc.git", branch: "master"
 
@@ -13,26 +13,26 @@ class Ldc < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ldc"
-    sha256 mojave: "16a828709a5fb9e9b835a7738cabe2be3149f6dca65fca1e014598c84b85455e"
+    sha256 mojave: "3a6a666ba16cd11a0b4799399cccdda7524bfde9c344e3ae5b72fd29d52d45f2"
   end
 
   depends_on "cmake" => :build
   depends_on "libconfig" => :build
   depends_on "pkg-config" => :build
-  depends_on "llvm@12"
+  depends_on "llvm"
 
   uses_from_macos "libxml2" => :build
-  # CompilerSelectionError: ldc cannot be built with any available compilers.
-  uses_from_macos "llvm" => [:build, :test]
 
   fails_with :gcc
 
   resource "ldc-bootstrap" do
     on_macos do
-      if Hardware::CPU.intel?
+      on_intel do
         url "https://github.com/ldc-developers/ldc/releases/download/v1.28.1/ldc2-1.28.1-osx-x86_64.tar.xz"
         sha256 "9aa43e84d94378f3865f69b08041331c688e031dd2c5f340eb1f3e30bdea626c"
-      else
+      end
+
+      on_arm do
         url "https://github.com/ldc-developers/ldc/releases/download/v1.28.1/ldc2-1.28.1-osx-arm64.tar.xz"
         sha256 "9bddeb1b2c277019cf116b2572b5ee1819d9f99fe63602c869ebe42ffb813aed"
       end
@@ -56,15 +56,34 @@ class Ldc < Formula
     ENV.cxx11
     (buildpath/"ldc-bootstrap").install resource("ldc-bootstrap")
 
-    # Fix ldc-bootstrap/bin/ldmd2: error while loading shared libraries: libxml2.so.2
-    ENV.prepend_path "LD_LIBRARY_PATH", Formula["libxml2"].lib if OS.linux?
-
     args = %W[
       -DLLVM_ROOT_DIR=#{llvm.opt_prefix}
       -DINCLUDE_INSTALL_DIR=#{include}/dlang/ldc
       -DD_COMPILER=#{buildpath}/ldc-bootstrap/bin/ldmd2
     ]
-    args << "-DCMAKE_INSTALL_RPATH=#{rpath};@loader_path/#{llvm.opt_lib.relative_path_from(lib)}" if OS.mac?
+
+    if OS.mac?
+      args << "-DCMAKE_INSTALL_RPATH=#{rpath};@loader_path/#{llvm.opt_lib.relative_path_from(lib)}"
+    else
+      # Fix ldc-bootstrap/bin/ldmd2: error while loading shared libraries: libxml2.so.2
+      ENV.prepend_path "LD_LIBRARY_PATH", Formula["libxml2"].lib if OS.linux?
+
+      gcc = Formula["gcc"]
+      # Link to libstdc++ for brewed GCC rather than the host GCC which is too old.
+      libstdcxx_lib = gcc.opt_lib/"gcc"/gcc.version.major
+      linux_linker_flags = "-L#{libstdcxx_lib} -Wl,-rpath,#{libstdcxx_lib}"
+
+      # Use libstdc++ headers for brewed GCC rather than host GCC which is too old.
+      libstdcxx_include = gcc.opt_include/"c++"/gcc.version.major
+      linux_cxx_flags = "-nostdinc++ -isystem#{libstdcxx_include} -isystem#{libstdcxx_include}/x86_64-pc-linux-gnu"
+
+      args += %W[
+        -DCMAKE_EXE_LINKER_FLAGS=#{linux_linker_flags}
+        -DCMAKE_MODULE_LINKER_FLAGS=#{linux_linker_flags}
+        -DCMAKE_SHARED_LINKER_FLAGS=#{linux_linker_flags}
+        -DCMAKE_CXX_FLAGS=#{linux_cxx_flags}
+      ]
+    end
 
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
     system "cmake", "--build", "build"
