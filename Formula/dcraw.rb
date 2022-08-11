1ba58a0d074f6028e5dc352dc9a654719af2f4e7
Adrian Ho <the.gromgit@gmail.com>
2022-08-11 19:52:31 +0800

    dcraw: update 9.28.0_2 bottle.

--- Formula/dcraw.rb
+++ Formula/dcraw.rb
@@ -4,7 +4,7 @@ class Dcraw < Formula
   url "https://www.dechifro.org/dcraw/archive/dcraw-9.28.0.tar.gz"
   mirror "https://mirrorservice.org/sites/distfiles.macports.org/dcraw/dcraw-9.28.0.tar.gz"
   sha256 "2890c3da2642cd44c5f3bfed2c9b2c1db83da5cec09cc17e0fa72e17541fb4b9"
-  revision 1
+  revision 2
 
   livecheck do
     url "https://distfiles.macports.org/dcraw/"
@@ -13,16 +13,16 @@ class Dcraw < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dcraw"
-    sha256 cellar: :any, mojave: "e9c32b8c306728da820f905dffee787dfacc5d180dfb89625631e1acf16ee27c"
+    sha256 cellar: :any, mojave: "e35b518c31ace36009c7b8df7530368d5e0590f4f9de0541884d2f77887fed5d"
   end
 
   depends_on "jasper"
-  depends_on "jpeg"
+  depends_on "jpeg-turbo"
   depends_on "little-cms2"
 
   def install
-    ENV.append_to_cflags "-I#{HOMEBREW_PREFIX}/include -L#{HOMEBREW_PREFIX}/lib"
-    system ENV.cc, "-o", "dcraw", ENV.cflags, "dcraw.c", "-lm", "-ljpeg", "-llcms2", "-ljasper"
+    ENV.append "LDLIBS", "-lm -ljpeg -llcms2 -ljasper"
+    system "make", "dcraw"
     bin.install "dcraw"
     man1.install "dcraw.1"
   end
