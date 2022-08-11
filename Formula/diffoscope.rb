1c66348464c4c984d705a857152fea4e5f364deb
Adrian Ho <the.gromgit@gmail.com>
2022-08-11 21:55:48 +0800

    diffoscope: update 220 bottle.

--- Formula/diffoscope.rb
+++ Formula/diffoscope.rb
@@ -3,13 +3,13 @@ class Diffoscope < Formula
 
   desc "In-depth comparison of files, archives, and directories"
   homepage "https://diffoscope.org"
-  url "https://files.pythonhosted.org/packages/cc/0a/97261ced64fd4e7db04e35ac99521e3c2f87b9da344c6e860fcc46e5b7b2/diffoscope-219.tar.gz"
-  sha256 "564af9bf31142b6ba000d2735f65fb45773c0a61e00b9cb8e274694f1b9471e4"
+  url "https://files.pythonhosted.org/packages/8a/89/1d0589a1e7182526f479c30462a9951f032ee3bde06f4868203bc9b366ef/diffoscope-220.tar.gz"
+  sha256 "7873e13ac8b11b634ee3490b70b056c6a6bae9cfb794d6ba7cb43e7797b2a829"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/diffoscope"
-    sha256 cellar: :any_skip_relocation, mojave: "4918da74212013c86a3a700031195c0705ed78465fe2753300049f0b218c997e"
+    sha256 cellar: :any_skip_relocation, mojave: "1bd9fa427c347903145f0f99617ec43defb91f841dad143b10f60ec898c9c559"
   end
 
   depends_on "libarchive"
