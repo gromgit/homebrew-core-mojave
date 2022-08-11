0ae9e304b853b1a7c59188fba8f1653d9529f9aa
Adrian Ho <the.gromgit@gmail.com>
2022-08-11 23:25:13 +0800

    mupdf: update 1.20.0_1 bottle.

--- Formula/mupdf.rb
+++ Formula/mupdf.rb
@@ -4,6 +4,7 @@ class Mupdf < Formula
   url "https://mupdf.com/downloads/archive/mupdf-1.20.0-source.tar.lz"
   sha256 "68dbb1cf5e31603380ce3f1c7f6c431ad442fa735d048700f50ab4de4c3b0f82"
   license "AGPL-3.0-or-later"
+  revision 1
   head "https://git.ghostscript.com/mupdf.git", branch: "master"
 
   livecheck do
@@ -13,27 +14,59 @@ class Mupdf < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mupdf"
-    sha256 cellar: :any, mojave: "f276a8a879f17088b2006083628e7fc6eca295c728ea2a0351733934285b72c0"
+    sha256 cellar: :any, mojave: "08bc89989b53f9c23811634b8a7257e6242a487ec949410649b8d202f4838562"
   end
 
   depends_on "pkg-config" => :build
-  depends_on "freeglut"
-  depends_on "mesa"
+  depends_on "freetype"
+  depends_on "gumbo-parser"
+  depends_on "harfbuzz"
+  depends_on "jbig2dec"
+  depends_on "jpeg-turbo"
+  depends_on "mujs"
+  depends_on "openjpeg"
+  depends_on "openssl@1.1"
+
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "freeglut"
+    depends_on "libx11"
+    depends_on "libxext"
+    depends_on "mesa"
+  end
 
   conflicts_with "mupdf-tools",
     because: "mupdf and mupdf-tools install the same binaries"
 
   def install
-    glut_cflags = `pkg-config --cflags glut gl`.chomp
-    glut_libs = `pkg-config --libs glut gl`.chomp
-    system "make", "install",
-           "build=release",
-           "shared=yes",
-           "verbose=yes",
-           "CC=#{ENV.cc}",
-           "SYS_GLUT_CFLAGS=#{glut_cflags}",
-           "SYS_GLUT_LIBS=#{glut_libs}",
-           "prefix=#{prefix}"
+    # Remove bundled libraries excluding `extract` and "strongly preferred" `lcms2mt` (lcms2 fork)
+    keep = %w[extract lcms2]
+    (buildpath/"thirdparty").each_child { |path| path.rmtree if keep.exclude? path.basename.to_s }
+
+    args = %W[
+      build=release
+      shared=yes
+      verbose=yes
+      prefix=#{prefix}
+      CC=#{ENV.cc}
+      USE_SYSTEM_LIBS=yes
+      USE_SYSTEM_MUJS=yes
+    ]
+    # Build only runs pkg-config for libcrypto on macOS, so help find other libs
+    if OS.mac?
+      [
+        ["FREETYPE", "freetype2"],
+        ["GUMBO", "gumbo"],
+        ["HARFBUZZ", "harfbuzz"],
+        ["LIBJPEG", "libjpeg"],
+        ["OPENJPEG", "libopenjp2"],
+      ].each do |argname, libname|
+        args << "SYS_#{argname}_CFLAGS=#{Utils.safe_popen_read("pkg-config", "--cflags", libname).strip}"
+        args << "SYS_#{argname}_LIBS=#{Utils.safe_popen_read("pkg-config", "--libs", libname).strip}"
+      end
+    end
+    system "make", "install", *args
 
     # Symlink `mutool` as `mudraw` (a popular shortcut for `mutool draw`).
     bin.install_symlink bin/"mutool" => "mudraw"
