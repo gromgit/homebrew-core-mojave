55bad942efad52ab44734589236be9be6cee98d3
Adrian Ho <the.gromgit@gmail.com>
2022-08-12 00:07:13 +0800

    esbuild: update 0.14.53 bottle.

--- Formula/esbuild.rb
+++ Formula/esbuild.rb
@@ -3,13 +3,13 @@ require "language/node"
 class Esbuild < Formula
   desc "Extremely fast JavaScript bundler and minifier"
   homepage "https://esbuild.github.io/"
-  url "https://registry.npmjs.org/esbuild/-/esbuild-0.14.45.tgz"
-  sha256 "9a88eb91cd7f053d4229710e635ec0b4e33308fb4195f7c5993a52001301f0df"
+  url "https://registry.npmjs.org/esbuild/-/esbuild-0.14.53.tgz"
+  sha256 "24907e776463af5288a6e4fd1cbb6dd0a7145d03817ddf3bb2adff8095aa4ec7"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/esbuild"
-    sha256 cellar: :any_skip_relocation, mojave: "ba136c45da1c7506a497f184b85ca251df023622ce97827b7d46d2d6df48e5b7"
+    sha256 cellar: :any_skip_relocation, mojave: "d55b4d17ec28bfe5f9c9e968b4970085056bea37b94dd3a1a628c99fbc109df2"
   end
 
   depends_on "node"
