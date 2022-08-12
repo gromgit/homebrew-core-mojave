9314a641df936bec42e5d775705de5b7e47663be
Adrian Ho <the.gromgit@gmail.com>
2022-08-13 01:56:31 +0800

    hadoop: update 3.3.4 bottle.

--- Formula/hadoop.rb
+++ Formula/hadoop.rb
@@ -1,37 +1,72 @@
 class Hadoop < Formula
   desc "Framework for distributed processing of large data sets"
   homepage "https://hadoop.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=hadoop/common/hadoop-3.3.2/hadoop-3.3.2.tar.gz"
-  mirror "https://archive.apache.org/dist/hadoop/common/hadoop-3.3.2/hadoop-3.3.2.tar.gz"
-  sha256 "b341587495b12eec0b244b517f21df88eb46ef634dc7dc3e5969455b80ce2ce5"
+  url "https://www.apache.org/dyn/closer.lua?path=hadoop/common/hadoop-3.3.4/hadoop-3.3.4.tar.gz"
+  mirror "https://archive.apache.org/dist/hadoop/common/hadoop-3.3.4/hadoop-3.3.4.tar.gz"
+  sha256 "6a483d1a0b123490ebd8df3f71b64eb39f333f78b95f090aeb58e433cbc2416d"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/hadoop"
-    sha256 cellar: :any_skip_relocation, mojave: "ff9fc2728e7cf70c56c4dab0423c5d666e3d08ba32d336a7f8ea911f8c57f24d"
+    sha256 cellar: :any_skip_relocation, mojave: "a7285cf8aeaae3e175340d73f5458aad739dcf377e5e3c622867f3889a5921da"
   end
 
-  depends_on "openjdk"
+  # WARNING: Check https://cwiki.apache.org/confluence/display/HADOOP/Hadoop+Java+Versions before updating JDK version
+  depends_on "openjdk@11"
 
   conflicts_with "yarn", because: "both install `yarn` binaries"
 
   def install
     rm_f Dir["bin/*.cmd", "sbin/*.cmd", "libexec/*.cmd", "etc/hadoop/*.cmd"]
     libexec.install %w[bin sbin libexec share etc]
-    Dir["#{libexec}/bin/*"].each do |path|
-      (bin/File.basename(path)).write_env_script path, JAVA_HOME: Formula["openjdk"].opt_prefix
+
+    hadoop_env = Language::Java.java_home_env("11")
+    hadoop_env[:HADOOP_LOG_DIR] = var/"hadoop"
+
+    (libexec/"bin").each_child do |path|
+      (bin/File.basename(path)).write_env_script path, hadoop_env
     end
-    Dir["#{libexec}/sbin/*"].each do |path|
-      (sbin/File.basename(path)).write_env_script path, JAVA_HOME: Formula["openjdk"].opt_prefix
+    (libexec/"sbin").each_child do |path|
+      (sbin/File.basename(path)).write_env_script path, hadoop_env
     end
-    Dir["#{libexec}/libexec/*.sh"].each do |path|
-      (libexec/File.basename(path)).write_env_script path, JAVA_HOME: Formula["openjdk"].opt_prefix
+    libexec.glob("libexec/*.sh").each do |path|
+      (libexec/File.basename(path)).write_env_script path, hadoop_env
     end
+
     # Temporary fix until https://github.com/Homebrew/brew/pull/4512 is fixed
-    chmod 0755, Dir["#{libexec}/*.sh"]
+    chmod 0755, libexec.glob("*.sh")
   end
 
   test do
     system bin/"hadoop", "fs", "-ls"
+
+    # Test if resource manager can start successfully
+    port = free_port
+    classpaths = %w[
+      etc/hadoop
+      share/hadoop/common/lib/*
+      share/hadoop/common/*
+      share/hadoop/hdfs
+      share/hadoop/hdfs/lib/*
+      share/hadoop/hdfs/*
+      share/hadoop/mapreduce/*
+      share/hadoop/yarn
+      share/hadoop/yarn/lib/*
+      share/hadoop/yarn/*
+      share/hadoop/yarn/timelineservice/*
+      share/hadoop/yarn/timelineservice/lib/*
+    ].map { |path| libexec/path }
+
+    pid = Process.spawn({
+      "JAVA_HOME" => Language::Java.java_home("11"),
+      "CLASSPATH" => classpaths.join(":"),
+    }, Formula["openjdk@11"].opt_bin/"java", "org.apache.hadoop.yarn.server.resourcemanager.ResourceManager",
+                                             "-Dyarn.resourcemanager.webapp.address=127.0.0.1:#{port}")
+    sleep 8
+
+    Process.getpgid pid
+    system "curl", "http://127.0.0.1:#{port}"
+  ensure
+    Process.kill "TERM", pid
   end
 end
