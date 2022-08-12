8df70def1b39942e773ee1acd5dcd33a5e5772c8
Adrian Ho <the.gromgit@gmail.com>
2022-08-13 03:35:59 +0800

    hsd: update 4.0.2 bottle.

--- Formula/hsd.rb
+++ Formula/hsd.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Hsd < Formula
   desc "Handshake Daemon & Full Node"
   homepage "https://handshake.org"
-  url "https://github.com/handshake-org/hsd/archive/v3.0.1.tar.gz"
-  sha256 "2952dd9fe5c1d5db448e0881cc1656c5efaa61cb6030c4bc629e04f53feec3b4"
+  url "https://github.com/handshake-org/hsd/archive/v4.0.2.tar.gz"
+  sha256 "ee58d681491bee90299ea2fa18656ac26fc6aef2bf3e363cb996be5015b06feb"
   license "MIT"
 
   livecheck do
@@ -14,19 +14,16 @@ class Hsd < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/hsd"
-    rebuild 5
-    sha256 mojave: "db833e654588ff820649c589ba38b84e205b7aa32125c878027ae24f74681be8"
+    sha256 mojave: "87d59979022ef1260ba3b63ad9f265f0e0d09fb13ff192da0c1c67b4f9a1fe53"
   end
 
   depends_on "python@3.10" => :build
-  depends_on "node@14"
+  depends_on "node"
   depends_on "unbound"
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    (bin/"hsd").write_env_script libexec/"bin/hsd", PATH: "#{Formula["node@14"].opt_bin}:$PATH"
-    bin.install_symlink libexec/"bin/hsd-cli"
-    bin.install_symlink libexec/"bin/hsw-cli"
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
@@ -43,7 +40,7 @@ class Hsd < Formula
         await node.ensure();
       })();
     EOS
-    system "#{Formula["node@14"].opt_bin}/node", testpath/"script.js"
+    system "#{Formula["node"].opt_bin}/node", testpath/"script.js"
     assert File.directory?("#{testpath}/.hsd")
   end
 end
