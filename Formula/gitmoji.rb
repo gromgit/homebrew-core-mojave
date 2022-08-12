743c26bcfe42b70b6426d0a35ae0d654632b3f0d
Adrian Ho <the.gromgit@gmail.com>
2022-08-13 01:07:15 +0800

    gitmoji: update 5.0.3 bottle.

--- Formula/gitmoji.rb
+++ Formula/gitmoji.rb
@@ -3,13 +3,13 @@ require "language/node"
 class Gitmoji < Formula
   desc "Interactive command-line tool for using emoji in commit messages"
   homepage "https://gitmoji.dev"
-  url "https://registry.npmjs.org/gitmoji-cli/-/gitmoji-cli-5.0.1.tgz"
-  sha256 "c0031d926c07ecfb3d03b27b0f26ed9c2711749015f5766109e87bfac1e6791b"
+  url "https://registry.npmjs.org/gitmoji-cli/-/gitmoji-cli-5.0.3.tgz"
+  sha256 "8575c951a89aa5bf5727c8146fd1bdf04418685564de780888abd53deb0f16f4"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gitmoji"
-    sha256 cellar: :any_skip_relocation, mojave: "f4bb6635930bfcbaab8b2a963450f27f3a747f0037f7b09f6889f0c665f52603"
+    sha256 cellar: :any_skip_relocation, mojave: "e38996355db0b8412fa558b9288c8b2a34925c76d3665baab2872c17746572cc"
   end
 
   depends_on "node"
