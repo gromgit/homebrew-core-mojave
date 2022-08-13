2514d4a5266aab9e6e2be4c8392f55d7a4b93e17
Adrian Ho <the.gromgit@gmail.com>
2022-08-13 12:11:41 +0800

    libgsf: update 1.14.50 bottle.

--- Formula/libgsf.rb
+++ Formula/libgsf.rb
@@ -1,13 +1,13 @@
 class Libgsf < Formula
   desc "I/O abstraction library for dealing with structured file formats"
   homepage "https://gitlab.gnome.org/GNOME/libgsf"
-  url "https://download.gnome.org/sources/libgsf/1.14/libgsf-1.14.49.tar.xz"
-  sha256 "e9ebe36688f010c9e6e40c8903f3732948deb8aca032578d07d0751bd82cf857"
-  license "LGPL-2.1-only"
+  url "https://download.gnome.org/sources/libgsf/1.14/libgsf-1.14.50.tar.xz"
+  sha256 "6e6c20d0778339069d583c0d63759d297e817ea10d0d897ebbe965f16e2e8e52"
+  license all_of: ["GPL-2.0-or-later", "LGPL-2.1-only"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libgsf"
-    sha256 mojave: "cef959e4400499e23ac94c68ab0727abfe834237ba14229b2f8efdca786cb0ba"
+    sha256 mojave: "e775d525250354355a48d4532774a129017e04b8e82b0223fa34b9983fa19b15"
   end
 
   head do
