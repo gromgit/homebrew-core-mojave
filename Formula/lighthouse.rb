14edc12313de5749c475cdc0a28637c66c7164c7
Adrian Ho <the.gromgit@gmail.com>
2022-08-13 14:51:54 +0800

    lighthouse: update 2.4.0 bottle.

--- Formula/lighthouse.rb
+++ Formula/lighthouse.rb
@@ -1,25 +1,24 @@
 class Lighthouse < Formula
   desc "Rust Ethereum 2.0 Client"
   homepage "https://github.com/sigp/lighthouse"
-  url "https://github.com/sigp/lighthouse/archive/refs/tags/v2.3.0.tar.gz"
-  sha256 "191dd902f8d4f1d67d3edf4b3af808fddefaa5d85a83da2d8f6268306331bcfa"
+  url "https://github.com/sigp/lighthouse/archive/refs/tags/v2.4.0.tar.gz"
+  sha256 "6cef50170072e7c38a3cbcfa1af1a4c10b8fe3f31230c4b6d11f19d958c68e15"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lighthouse"
-    sha256 cellar: :any_skip_relocation, mojave: "99593c048cab17433d5f36abcb5f6a80ceea0ba53e289f1b5aace3d16ccf7e39"
+    sha256 cellar: :any_skip_relocation, mojave: "11993080cdd723de3d55672907f65b1e6054c9b22a8564770720625533a2ea2a"
   end
 
   depends_on "cmake" => :build
+  depends_on "protobuf" => :build
   depends_on "rust" => :build
 
+  uses_from_macos "llvm" => :build
   uses_from_macos "zlib"
 
-  on_linux do
-    depends_on "llvm" => :build
-  end
-
   def install
+    ENV["PROTOC_NO_VENDOR"] = "1"
     system "cargo", "install", *std_cargo_args(path: "./lighthouse")
   end
 
