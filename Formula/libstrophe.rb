4d41277448dc1164079b165917b77b4a8ca04207
Adrian Ho <the.gromgit@gmail.com>
2022-08-13 12:57:53 +0800

    libstrophe: update 0.12.2 bottle.

--- Formula/libstrophe.rb
+++ Formula/libstrophe.rb
@@ -1,14 +1,14 @@
 class Libstrophe < Formula
   desc "XMPP library for C"
   homepage "https://strophe.im/libstrophe/"
-  url "https://github.com/strophe/libstrophe/archive/0.12.1.tar.gz"
-  sha256 "91fc40a89528a32b0bda975d84f8f567b57e687898e22e328b3733c2ae0393d8"
+  url "https://github.com/strophe/libstrophe/archive/0.12.2.tar.gz"
+  sha256 "049232e3968ad65c65e08601cb3de171d3a25d154b6f14332c35a7bf961e2c9d"
   license all_of: ["GPL-3.0-only", "MIT"]
   head "https://github.com/strophe/libstrophe.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libstrophe"
-    sha256 cellar: :any, mojave: "a11bb66a1ba7f2e8e9b6f515a12286a2f93546e6c4dd722571a06030e8fc11ed"
+    sha256 cellar: :any, mojave: "cf6401a38ffe5f3be5b58e8f5701b6cab4d0f898c02c5d44cb4d8b412d350847"
   end
 
   depends_on "autoconf" => :build
