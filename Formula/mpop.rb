5a649eea57d919e5643ed0d860ebb08ed4d8442f
Adrian Ho <the.gromgit@gmail.com>
2022-08-13 19:49:18 +0800

    mpop: update 1.4.17 bottle.

--- Formula/mpop.rb
+++ Formula/mpop.rb
@@ -1,8 +1,8 @@
 class Mpop < Formula
   desc "POP3 client"
   homepage "https://marlam.de/mpop/"
-  url "https://marlam.de/mpop/releases/mpop-1.4.16.tar.xz"
-  sha256 "870eb571eae6d23fb92ad0c84d79de9c38c5f624e3614937d574bfe49ba687f9"
+  url "https://marlam.de/mpop/releases/mpop-1.4.17.tar.xz"
+  sha256 "42ae494bad2941d9f647c48c3ed98c38ba9aa5cf3fe48fb0fe06e5b6dadf8bd5"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -12,8 +12,7 @@ class Mpop < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mpop"
-    rebuild 2
-    sha256 mojave: "15669c8f77117177f30c05bc7d50d0a8a0878c87d5b4a7751bdecf3ae492bc4b"
+    sha256 mojave: "da10656d643bd92b06399d691703aad761b0c6b8de8a5a7e8bcec2be210c59d9"
   end
 
   depends_on "pkg-config" => :build
