2673417769e27a5e09027ebbc0abb099ec0c920a
Adrian Ho <the.gromgit@gmail.com>
2022-08-14 10:30:45 +0800

    openslide: update 3.4.1_7 bottle.

--- Formula/openslide.rb
+++ Formula/openslide.rb
@@ -4,18 +4,18 @@ class Openslide < Formula
   url "https://github.com/openslide/openslide/releases/download/v3.4.1/openslide-3.4.1.tar.xz"
   sha256 "9938034dba7f48fadc90a2cdf8cfe94c5613b04098d1348a5ff19da95b990564"
   license "LGPL-2.1-only"
-  revision 6
+  revision 7
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/openslide"
-    sha256 cellar: :any, mojave: "21f92ab35839660eb7106e8321f37cfe3853c48e630644b506cb505a1a841349"
+    sha256 cellar: :any, mojave: "d2d66e3954b4eb3251e6bd5fd29b9c63ea4af58d9deeb1e38ea81454856402c8"
   end
 
   depends_on "pkg-config" => :build
   depends_on "cairo"
   depends_on "gdk-pixbuf"
   depends_on "glib"
-  depends_on "jpeg"
+  depends_on "jpeg-turbo"
   depends_on "libpng"
   depends_on "libtiff"
   depends_on "libxml2"
@@ -29,8 +29,7 @@ class Openslide < Formula
   end
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", *std_configure_args
     system "make", "install"
   end
 
