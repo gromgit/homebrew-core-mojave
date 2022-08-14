a8b0debd730200a720435d62d135aa8fac456246
Adrian Ho <the.gromgit@gmail.com>
2022-08-14 19:07:06 +0800

    qsv: update 0.61.4 bottle.

--- Formula/qsv.rb
+++ Formula/qsv.rb
@@ -1,14 +1,14 @@
 class Qsv < Formula
   desc "Ultra-fast CSV data-wrangling toolkit"
   homepage "https://github.com/jqnatividad/qsv"
-  url "https://github.com/jqnatividad/qsv/archive/0.61.1.tar.gz"
-  sha256 "ca7d93a83ea83b29762edfcf3863f08ac8f0ac83701cb8d251ff1ee5daa0287a"
+  url "https://github.com/jqnatividad/qsv/archive/0.61.4.tar.gz"
+  sha256 "d82093b4b3fbae9469000b1970c34ad31b9e59918f617ffab6d9ba5eb785e04a"
   license any_of: ["MIT", "Unlicense"]
   head "https://github.com/jqnatividad/qsv.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/qsv"
-    sha256 cellar: :any_skip_relocation, mojave: "90d7577a561a173b21d6f5d5a35f286c41b045a12f3adb5c3007c171cc1650fd"
+    sha256 cellar: :any_skip_relocation, mojave: "e0996af1fc6b6f709f6ecdea1c23907306194fb737e58ca89c961762bb6c245c"
   end
 
   depends_on "rust" => :build
