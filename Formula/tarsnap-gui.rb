7c89dd8bd937f6faacced6634fd804ed565fea27
Adrian Ho <the.gromgit@gmail.com>
2022-08-15 02:54:39 +0800

    tarsnap-gui: update 1.0.2_1 bottle.

--- Formula/tarsnap-gui.rb
+++ Formula/tarsnap-gui.rb
@@ -4,26 +4,57 @@ class TarsnapGui < Formula
   url "https://github.com/Tarsnap/tarsnap-gui/archive/v1.0.2.tar.gz"
   sha256 "3b271f474abc0bbeb3d5d62ee76b82785c7d64145e6e8b51fa7907b724c83eae"
   license "BSD-2-Clause"
+  revision 1
   head "https://github.com/Tarsnap/tarsnap-gui.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any, catalina:    "34d641a2f477699fdbace87e2366ad998c012915f7e64315c90be60db07b5ae0"
-    sha256 cellar: :any, mojave:      "5423a7d16f7c629fb65a3f4a9240ce388cffbeee214f40b12599abd56fe39df9"
-    sha256 cellar: :any, high_sierra: "d3e5e2aeee094f0e5944d68765965f2b9261cce026bafe45482a2b1e9abb1273"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tarsnap-gui"
+    sha256 cellar: :any, mojave: "500ada372b65be012a3cf2e0db2eb8d7935d6cc54ca7b56bdbca872f2b841c78"
   end
 
-  depends_on "qt"
+  depends_on "qt@5"
   depends_on "tarsnap"
+
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5" # qt@5 is built with GCC
+
+  # Work around build error: Set: Entry, ":CFBundleGetInfoString", Does Not Exist
+  # Issue ref: https://github.com/Tarsnap/tarsnap-gui/issues/557
+  patch :DATA
 
   def install
     system "qmake"
     system "make"
-    system "macdeployqt", "Tarsnap.app"
-    prefix.install "Tarsnap.app"
+    if OS.mac?
+      prefix.install "Tarsnap.app"
+      bin.install_symlink prefix/"Tarsnap.app/Contents/MacOS/Tarsnap" => "tarsnap-gui"
+    else
+      bin.install "tarsnap-gui"
+    end
   end
 
   test do
-    system "#{opt_prefix}/Tarsnap.app/Contents/MacOS/Tarsnap", "--version"
+    # Set QT_QPA_PLATFORM to minimal to avoid error "could not connect to display"
+    ENV["QT_QPA_PLATFORM"] = "minimal" if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
+    system bin/"tarsnap-gui", "--version"
   end
 end
+
+__END__
+diff --git a/Tarsnap.pro b/Tarsnap.pro
+index 9954fc5c..560621b1 100644
+--- a/Tarsnap.pro
++++ b/Tarsnap.pro
+@@ -131,5 +131,8 @@ osx {
+ 
+     # Add VERSION to the app bundle.  (Why doesn't qmake do this?)
+     INFO_PLIST_PATH = $$shell_quote($${OUT_PWD}/$${TARGET}.app/Contents/Info.plist)
+-    QMAKE_POST_LINK += /usr/libexec/PlistBuddy -c \"Set :CFBundleGetInfoString $${VERSION}\" $${INFO_PLIST_PATH} ;
++    QMAKE_POST_LINK += /usr/libexec/PlistBuddy \
++                            -c \"Add :CFBundleVersionString string $${VERSION}\" \
++                            -c \"Add :CFBundleShortVersionString string $${VERSION}\" \
++                            $${INFO_PLIST_PATH} ;
+ }
