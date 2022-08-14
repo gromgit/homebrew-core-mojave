831f783346ff2244a295750be0b1d072e5964692
Adrian Ho <the.gromgit@gmail.com>
2022-08-15 03:20:09 +0800

    texlab: update 4.2.1 bottle.

--- Formula/texlab.rb
+++ Formula/texlab.rb
@@ -1,14 +1,14 @@
 class Texlab < Formula
   desc "Implementation of the Language Server Protocol for LaTeX"
   homepage "https://texlab.netlify.com/"
-  url "https://github.com/latex-lsp/texlab/archive/v4.2.0.tar.gz"
-  sha256 "fd7cc35c63e041b7947d31eb321669736edc900a71c31c53f5d5b7841273d494"
+  url "https://github.com/latex-lsp/texlab/archive/v4.2.1.tar.gz"
+  sha256 "5333487e31c6b21fc9a4b351de844fc11626452b98d6413d9f5bc620aa5a0966"
   license "GPL-3.0-only"
   head "https://github.com/latex-lsp/texlab.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/texlab"
-    sha256 cellar: :any_skip_relocation, mojave: "38eadb8f1b2b4564a4bf97aa316f0679cd1afe4c228808109fbb3d8d1018d82e"
+    sha256 cellar: :any_skip_relocation, mojave: "7e35ac906395acbae8678a454be2dc7fc79904bbfc572207d1911f3994f5544b"
   end
 
   depends_on "rust" => :build
