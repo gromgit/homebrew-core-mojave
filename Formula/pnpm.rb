628be81dea1cbee08cbb567ac080967dbc3c738c
Adrian Ho <the.gromgit@gmail.com>
2022-08-14 12:45:26 +0800

    pnpm: update 7.9.0 bottle.

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -3,8 +3,8 @@ class Pnpm < Formula
 
   desc "📦🚀 Fast, disk space efficient package manager"
   homepage "https://pnpm.io/"
-  url "https://registry.npmjs.org/pnpm/-/pnpm-7.2.1.tgz"
-  sha256 "c5fb3e8e09c78887dddd91bc5b3465fe298fb50ac796f9a45e560b2d69a717ea"
+  url "https://registry.npmjs.org/pnpm/-/pnpm-7.9.0.tgz"
+  sha256 "f474fe002ffbc3651504e9815257e73b52c553ebb04e4fd79257de6042260a3a"
   license "MIT"
 
   livecheck do
@@ -14,16 +14,24 @@ class Pnpm < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pnpm"
-    sha256 cellar: :any_skip_relocation, mojave: "25cf228c8caa6b8647dcd58376710c6c9774b41bc0dc0479b15509195deda0a1"
+    sha256 cellar: :any_skip_relocation, mojave: "c39fe39f09ad6da258cedc60257d194c69f29c84f0f610226cff326dcfa3925a"
   end
 
-  depends_on "node"
+  depends_on "node" => :test
 
   conflicts_with "corepack", because: "both installs `pnpm` and `pnpx` binaries"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    libexec.install buildpath.glob("*")
+    bin.install_symlink "#{libexec}/bin/pnpm.cjs" => "pnpm"
+    bin.install_symlink "#{libexec}/bin/pnpx.cjs" => "pnpx"
+  end
+
+  def caveats
+    <<~EOS
+      pnpm requires a Node installation to function. You can install one with:
+        brew install node
+    EOS
   end
 
   test do
