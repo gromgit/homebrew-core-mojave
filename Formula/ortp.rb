8156db761e394022c54f3c4922ae77bf25695fb5
Adrian Ho <the.gromgit@gmail.com>
2022-08-14 11:32:19 +0800

    ortp: update 5.1.55 bottle.

--- Formula/ortp.rb
+++ Formula/ortp.rb
@@ -1,14 +1,14 @@
 class Ortp < Formula
   desc "Real-time transport protocol (RTP, RFC3550) library"
   homepage "https://www.linphone.org/technical-corner/ortp"
-  url "https://gitlab.linphone.org/BC/public/ortp/-/archive/5.1.51/ortp-5.1.51.tar.bz2"
-  sha256 "a5fa6923db766be43298118136fd6fa0c7d7b1050cc296bfdbc774d3022a13e9"
+  url "https://gitlab.linphone.org/BC/public/ortp/-/archive/5.1.55/ortp-5.1.55.tar.bz2"
+  sha256 "a8dce7185401eb693389716269c4426036c41a25fb99b8075129c45f491d02b2"
   license "GPL-3.0-or-later"
   head "https://gitlab.linphone.org/BC/public/ortp.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ortp"
-    sha256 cellar: :any, mojave: "6aad9328c9bc12ab9a48e90e1ba5b6371b6a7a2ba4ea480f8db7439edcdfb374"
+    sha256 cellar: :any, mojave: "3c15bee0b347ac3cafc8aae469ee9211fa3442f9d31416d484472c12923462a0"
   end
 
   depends_on "cmake" => :build
@@ -19,29 +19,32 @@ class Ortp < Formula
   # https://github.com/BelledonneCommunications/bctoolbox
   resource "bctoolbox" do
     # Don't forget to change both instances of the version in the URL.
-    url "https://gitlab.linphone.org/BC/public/bctoolbox/-/archive/5.1.51/bctoolbox-5.1.51.tar.bz2"
-    sha256 "bc1a190de273483cec81d1be761f5254451fc875f82a26a2b53a0ec0fde3625c"
+    url "https://gitlab.linphone.org/BC/public/bctoolbox/-/archive/5.1.55/bctoolbox-5.1.55.tar.bz2"
+    sha256 "19613e4a8f5b107af0acb849f45974a1af438c4e888e096c6216047fce6397a2"
   end
 
   def install
     resource("bctoolbox").stage do
-      system "cmake", ".", *std_cmake_args(install_prefix: libexec), "-DENABLE_TESTS_COMPONENT=OFF"
-      system "make", "install"
+      system "cmake", "-S", ".", "-B", "build",
+                      "-DENABLE_TESTS_COMPONENT=OFF",
+                      *std_cmake_args(install_prefix: libexec)
+      system "cmake", "--build", "build"
+      system "cmake", "--install", "build"
     end
 
     ENV.prepend_path "PKG_CONFIG_PATH", libexec/"lib/pkgconfig"
     ENV.append "LDFLAGS", "-Wl,-rpath,#{libexec}/lib" if OS.linux?
 
-    args = std_cmake_args + %W[
+    args = %W[
       -DCMAKE_PREFIX_PATH=#{libexec}
       -DCMAKE_C_FLAGS=-I#{libexec}/include
       -DCMAKE_CXX_FLAGS=-I#{libexec}/include
       -DENABLE_DOC=NO
     ]
-    mkdir "build" do
-      system "cmake", "..", *args
-      system "make", "install"
-    end
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
