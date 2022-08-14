88afd450bd3971935d1cd55f18b15ae31c8b137e
Adrian Ho <the.gromgit@gmail.com>
2022-08-14 13:50:01 +0800

    prql-compiler: update 0.2.6 bottle.

--- Formula/prql-compiler.rb
+++ Formula/prql-compiler.rb
@@ -1,13 +1,13 @@
 class PrqlCompiler < Formula
   desc "Simple, powerful, pipelined SQL replacement"
   homepage "https://prql-lang.org"
-  url "https://github.com/prql/prql/archive/refs/tags/0.2.5.tar.gz"
-  sha256 "0446ff0698ebb46b02ebb5481f4ab954562d56361b01040e9d6c2b7e251ece64"
+  url "https://github.com/prql/prql/archive/refs/tags/0.2.6.tar.gz"
+  sha256 "4079d5f505250e6fe071215499b21e8ac7ecea23b55a16a4054bc5bf5707faf6"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/prql-compiler"
-    sha256 cellar: :any_skip_relocation, mojave: "c3d619585de7e8beb6b4cb6e9dab7d52317ff5d0876a6ed525e15c946d455b96"
+    sha256 cellar: :any_skip_relocation, mojave: "a03e2e03a30e2ecf8f433cb47817cca4df45eb90924bfe6f5e615a15044f3d9f"
   end
 
   depends_on "rust" => :build
