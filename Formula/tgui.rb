a7e0bcb67381031a11c92f437e69919481fa92e3
Adrian Ho <the.gromgit@gmail.com>
2022-08-15 03:47:55 +0800

    tgui: update 0.9.5 bottle.

--- Formula/tgui.rb
+++ Formula/tgui.rb
@@ -1,25 +1,29 @@
 class Tgui < Formula
   desc "GUI library for use with sfml"
   homepage "https://tgui.eu"
-  url "https://github.com/texus/TGUI/archive/v0.9.4.tar.gz"
-  sha256 "08ce4893a5ab25a151be317c87395ac3567654547c9854b16c3142e750389cf6"
+  url "https://github.com/texus/TGUI/archive/v0.9.5.tar.gz"
+  sha256 "819865bf13661050161bce1e1ad68530a1f234becd3358c96d8701ea4e76bcc1"
   license "Zlib"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tgui"
-    sha256 cellar: :any, mojave: "05693dfa03fef6f49d5e61f621f55abf72c0f957823ee77bf5ff64ccbc006440"
+    sha256 cellar: :any, mojave: "be00d38f2a1bddbe814dc4233e186879265c7c581064b40193c3cf6763e8c8fd"
   end
 
   depends_on "cmake" => :build
   depends_on "sfml"
 
   def install
-    system "cmake", ".", *std_cmake_args,
-                    "-DTGUI_MISC_INSTALL_PREFIX=#{pkgshare}",
-                    "-DTGUI_BUILD_FRAMEWORK=FALSE",
-                    "-DTGUI_BUILD_EXAMPLES=TRUE",
-                    "-DTGUI_BUILD_GUI_BUILDER=TRUE",
-                    "-DTGUI_BUILD_TESTS=FALSE"
+    args = std_cmake_args + %W[
+      -DTGUI_MISC_INSTALL_PREFIX=#{pkgshare}
+      -DTGUI_BUILD_FRAMEWORK=FALSE
+      -DTGUI_BUILD_EXAMPLES=TRUE
+      -DTGUI_BUILD_GUI_BUILDER=TRUE
+      -DTGUI_BUILD_TESTS=FALSE
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+    ]
+
+    system "cmake", ".", *args
     system "make", "install"
   end
 
