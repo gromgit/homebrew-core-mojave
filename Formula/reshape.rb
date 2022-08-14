e37c6533105f0cc7519c9b8e3577d2e992a7f2c6
Adrian Ho <the.gromgit@gmail.com>
2022-08-14 19:24:58 +0800

    reshape: update 0.6.1 bottle.

--- Formula/reshape.rb
+++ Formula/reshape.rb
@@ -1,14 +1,14 @@
 class Reshape < Formula
   desc "Easy-to-use, zero-downtime schema migration tool for Postgres"
   homepage "https://github.com/fabianlindfors/reshape"
-  url "https://github.com/fabianlindfors/reshape/archive/v0.6.0.tar.gz"
-  sha256 "4a69ec4476e0983224d446b63ebf78e3f764b57ac4e73aab49ac34b8f506851c"
+  url "https://github.com/fabianlindfors/reshape/archive/v0.6.1.tar.gz"
+  sha256 "5d22b7b2f015c79734f35d0fcd43b21bac89d9032dd66307450b3b8c4cc0a33b"
   license "MIT"
   head "https://github.com/fabianlindfors/reshape.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/reshape"
-    sha256 cellar: :any_skip_relocation, mojave: "6d514f91ac2fa9c61abafcea031683858413da7e6028ac5110f321d4a2e152b5"
+    sha256 cellar: :any_skip_relocation, mojave: "5c35ee354f98b78ce704cf2a8596206b2fcf3116fb0578faae99bc703d3e9c41"
   end
 
   depends_on "rust" => :build
