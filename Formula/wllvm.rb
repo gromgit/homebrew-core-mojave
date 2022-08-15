b3b630cb62ce9e1ee5b3d543344b86db1bb91daf
Adrian Ho <the.gromgit@gmail.com>
2022-08-15 08:54:55 +0800

    wllvm: update 1.3.1_1 bottle.

--- Formula/wllvm.rb
+++ Formula/wllvm.rb
@@ -6,20 +6,15 @@ class Wllvm < Formula
   url "https://files.pythonhosted.org/packages/4b/df/31d7519052bc21d0e9771e9a6540d6310bfb13bae7dacde060d8f647b8d3/wllvm-1.3.1.tar.gz"
   sha256 "3e057a575f05c9ecc8669a8c4046f2bfdf0c69533b87b4fbfcabe0df230cc331"
   license "MIT"
+  revision 1
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "658e5b083456506153ed16be765b47296542dc80889fb1d4ab99b1ed46979e19"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c20248474947e050b8e5c8950301896c7343beca3baa4063a910c68e44f59cd4"
-    sha256 cellar: :any_skip_relocation, monterey:       "1f5146c3f4eb527d865e739ff990436d0c23dd35551a111633f70286c51974ea"
-    sha256 cellar: :any_skip_relocation, big_sur:        "946b4f71c813838a6bcf71992379dbc092fe1586374c8b372f70a5a9f007a4fd"
-    sha256 cellar: :any_skip_relocation, catalina:       "946b4f71c813838a6bcf71992379dbc092fe1586374c8b372f70a5a9f007a4fd"
-    sha256 cellar: :any_skip_relocation, mojave:         "d371b3aeaae37cdb1f250e536aaa6f52dd0da4e132bdddb02a238a702d130735"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "05f8b627879dd34512b6423047ec04d5b668ac28dbde974c2e7df85ddb703a6b"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/wllvm"
+    sha256 cellar: :any_skip_relocation, mojave: "68f0c14c32a408b35a74ffd3993ddc00a76ead4fbb486591df2ff2c69d8b4485"
   end
 
   depends_on "llvm" => :test
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     virtualenv_install_with_resources
