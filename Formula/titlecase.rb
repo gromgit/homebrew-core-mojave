8bff3126c85ce82d947dfd2cf2fae732f44416d4
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-17 10:27:00 +0800

    titlecase: no rebuilding required

--- Formula/titlecase.rb
+++ Formula/titlecase.rb
@@ -1,13 +1,13 @@
 class Titlecase < Formula
   desc "Script to convert text to title case"
   homepage "http://plasmasturm.org/code/titlecase/"
-  url "https://github.com/ap/titlecase/archive/v1.004.tar.gz"
-  sha256 "fbaafdb66ab4ba26f1f03fa292771d146c050fff0a3a640e11314ace322c2d92"
+  url "https://github.com/ap/titlecase/archive/v1.005.tar.gz"
+  sha256 "6483798bac1e359be4b3c48b8f710fd35cc4671dfe201322cbb3461a200b4f76"
   license "MIT"
   head "https://github.com/ap/titlecase.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "27a6bbb0ade7749f3ac3f72871f7f9f611a61e8af02046511d33f723881fba0d"
+    sha256 cellar: :any_skip_relocation, all: "016d29b351920229fead9d3b8ac962522ff95f52dbfa711ba0867cc10d0d762c"
   end
 
   def install
