2c0f2ab415e13004f41492d73884ce623f8d6bf8
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-17 10:26:45 +0800

    nifi: no rebuilding required

--- Formula/nifi.rb
+++ Formula/nifi.rb
@@ -1,13 +1,13 @@
 class Nifi < Formula
   desc "Easy to use, powerful, and reliable system to process and distribute data"
   homepage "https://nifi.apache.org"
-  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.16.3/nifi-1.16.3-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/nifi/1.16.3/nifi-1.16.3-bin.tar.gz"
-  sha256 "e7b66d80ad59f06fe75f6aed7fba2c9b29af13846e9068bb088bc2bd140d2ae1"
+  url "https://www.apache.org/dyn/closer.lua?path=/nifi/1.17.0/nifi-1.17.0-bin.zip"
+  mirror " https://archive.apache.org/dist/nifi/1.17.0/nifi-1.17.0-bin.zip"
+  sha256 "176eb0925493ce61eb3d8c3c0dcfa029a546b0014aeea731749823cbc069ecff"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "7b38b4789fef02da2919c6d91dace608cf1f02baa9ea4d031ba5c390bfa537a9"
+    sha256 cellar: :any_skip_relocation, all: "0a93be16bc08a85b354da5c2bafde64066571259839ac9d5f233b1e8264324c2"
   end
 
   depends_on "openjdk@11"
