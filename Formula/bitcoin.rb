6d1bcd0aa9c1b33cd9123ab7ca7f1d3506a583ff
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-17 10:26:19 +0800

    bitcoin:

--- Formula/bitcoin.rb
+++ Formula/bitcoin.rb
@@ -4,7 +4,7 @@ class Bitcoin < Formula
   url "https://bitcoincore.org/bin/bitcoin-core-23.0/bitcoin-23.0.tar.gz"
   sha256 "26748bf49d6d6b4014d0fedccac46bf2bcca42e9d34b3acfd9e3467c415acc05"
   license "MIT"
-  revision 1
+  revision 3
   head "https://github.com/bitcoin/bitcoin.git", branch: "master"
 
   livecheck do
@@ -13,24 +13,26 @@ class Bitcoin < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "c084959d808330eb564cf27b7ea51c62497a8008c2d584640466c6019bdc55b9"
-    sha256 cellar: :any,                 arm64_big_sur:  "0bbf8b60af0394b210c88202c7319b785eff65635f8cbb55682793a9c674b73a"
-    sha256 cellar: :any,                 monterey:       "de3f8d260583f8b12f2159358deaec0c71408cf745c250e72cc43a208c96e5a3"
-    sha256 cellar: :any,                 big_sur:        "ee59dc2285d421fac229272894a9297dd97f7538995cc651839494fa184f4cdd"
-    sha256 cellar: :any,                 catalina:       "225a9045c66625bb715fc92d4971f4f847ae459e22cb9e93910048f2576fe23e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d6565228ccfa1b5c59aa14fde06a7ac901261c89d0d1099bcb78791265d783ae"
+    sha256 cellar: :any,                 arm64_monterey: "85032174d1a8fef79d83bafa0d83a8658b991de922e5d6f22e4eab8685f7bb6e"
+    sha256 cellar: :any,                 arm64_big_sur:  "3a6071e17033c44f15b93df3d40c0a8ce3839fcc9459734b89e816af0dec3b43"
+    sha256 cellar: :any,                 monterey:       "bdaf808516f2d8e20d51781df622b0c8f7352b6b1d0ad8c2154c90435c04012e"
+    sha256 cellar: :any,                 big_sur:        "905774fb45316d1ca5e8188ee38b9c8ca1d5c5fd8d6dda41c3282788e3f9289b"
+    sha256 cellar: :any,                 catalina:       "2f428c157c1b86ab696b045afdd98fc3d4c414d4d7ea90b053bbc77bfa8760d1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fe839cb818d429fc721a45777db1568bef4b3b32cd66429d7d8574dc149a1596"
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "berkeley-db@4"
+  depends_on "berkeley-db@5"
   depends_on "boost"
   depends_on "libevent"
   depends_on macos: :catalina
   depends_on "miniupnpc"
   depends_on "zeromq"
+
+  uses_from_macos "sqlite"
 
   on_linux do
     depends_on "util-linux" => :build # for `hexdump`
