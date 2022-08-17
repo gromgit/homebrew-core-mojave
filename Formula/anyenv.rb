31b359a3e2a204022246bbf7e6d4eb9ec0c25533
Adrian Ho <the.gromgit@gmail.com>
2022-08-17 22:34:42 +0800

    anyenv: update 1.1.5 bottle.

--- Formula/anyenv.rb
+++ Formula/anyenv.rb
@@ -1,18 +1,13 @@
 class Anyenv < Formula
   desc "All in one for **env"
   homepage "https://anyenv.github.io/"
-  url "https://github.com/anyenv/anyenv/archive/v1.1.4.tar.gz"
-  sha256 "efb5663ea44ba53c692bb1db439f4b9cf69099614030d0950fdd083f07a2e3c7"
+  url "https://github.com/anyenv/anyenv/archive/v1.1.5.tar.gz"
+  sha256 "ed086fb8f5ee6bd8136364c94a9a76a24c65e0a950bb015e1b83389879a56ba8"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "64a65deada2b73eea41aaf1072d015c193e46cc38d524e3770d8e319b56b54fe"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "64a65deada2b73eea41aaf1072d015c193e46cc38d524e3770d8e319b56b54fe"
-    sha256 cellar: :any_skip_relocation, monterey:       "3cbde52463957a85f70e02c7c4e4ae793a3d5cbd6a44db6ec930f6f370db8b66"
-    sha256 cellar: :any_skip_relocation, big_sur:        "3cbde52463957a85f70e02c7c4e4ae793a3d5cbd6a44db6ec930f6f370db8b66"
-    sha256 cellar: :any_skip_relocation, catalina:       "3cbde52463957a85f70e02c7c4e4ae793a3d5cbd6a44db6ec930f6f370db8b66"
-    sha256 cellar: :any_skip_relocation, mojave:         "3cbde52463957a85f70e02c7c4e4ae793a3d5cbd6a44db6ec930f6f370db8b66"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "64a65deada2b73eea41aaf1072d015c193e46cc38d524e3770d8e319b56b54fe"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/anyenv"
+    sha256 cellar: :any_skip_relocation, mojave: "e7a9bd58702840c6aee47e42ff2691681721a494a47cceac00cf10e1b1748879"
   end
 
   def install
