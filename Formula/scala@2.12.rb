9b18dd2d5546cdca7c39d54aec16985aa30b4e4a
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-17 10:26:54 +0800

    scala@2.12: no rebuilding required

--- Formula/scala@2.12.rb
+++ Formula/scala@2.12.rb
@@ -1,14 +1,19 @@
 class ScalaAT212 < Formula
   desc "JVM-based programming language"
   homepage "https://www.scala-lang.org/"
-  url "https://downloads.lightbend.com/scala/2.12.14/scala-2.12.14.tgz"
-  mirror "https://www.scala-lang.org/files/archive/scala-2.12.14.tgz"
-  mirror "https://downloads.typesafe.com/scala/2.12.14/scala-2.12.14.tgz"
-  sha256 "fd7e3e4032288013a29c0a1447c597faf7b0e499762c0d981db21099e9780426"
+  url "https://downloads.lightbend.com/scala/2.12.16/scala-2.12.16.tgz"
+  mirror "https://www.scala-lang.org/files/archive/scala-2.12.16.tgz"
+  mirror "https://downloads.typesafe.com/scala/2.12.16/scala-2.12.16.tgz"
+  sha256 "2be383c1140e5e8dc84707fae81734f42b666766f91e55bbb04e444af8f3467f"
   license "Apache-2.0"
 
+  livecheck do
+    url "https://www.scala-lang.org/files/archive/"
+    regex(/href=.*?scala[._-]v?(2\.12(?:\.\d+)+)(?:[._-]final)?\.t/i)
+  end
+
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "cdf530f84277789ecac9c149fe5b3c13e68dcabfef366194b02b463a5ac1ae1d"
+    sha256 cellar: :any_skip_relocation, all: "b5b1fb55d493da7768620fc9cd88837ee878da81be629abe6444f2254423559c"
   end
 
   keg_only :versioned_formula
