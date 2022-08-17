7752b295c5ac2018cb9cd265b36a73a5191769a8
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-17 10:26:51 +0800

    psql2csv: no rebuilding required

--- Formula/psql2csv.rb
+++ Formula/psql2csv.rb
@@ -4,12 +4,13 @@ class Psql2csv < Formula
   url "https://github.com/fphilipe/psql2csv/archive/v0.12.tar.gz"
   sha256 "bd99442ee5b6892589986cb93f928ec9e823d17d06f10c8e74e7522bf021ca28"
   license "MIT"
+  revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "6c33e20a01b09f6161318136ae5e4529cea292fd1783ce8b81bdf13a58c5453b"
+    sha256 cellar: :any_skip_relocation, all: "7b159e3b53c7d0b6bd568e504e918a1c96cff36007bcd009550c21e18c3cd269"
   end
 
-  depends_on "postgresql"
+  depends_on "libpq"
 
   def install
     bin.install "psql2csv"
