d876bf06c4b50026b931ab46ef614fdca0ba152f
Adrian Ho <the.gromgit@gmail.com>
2022-08-18 20:27:25 +0800

    istioctl: update 1.14.3 bottle.

--- Formula/istioctl.rb
+++ Formula/istioctl.rb
@@ -2,18 +2,20 @@ class Istioctl < Formula
   desc "Istio configuration command-line utility"
   homepage "https://istio.io/"
   url "https://github.com/istio/istio.git",
-      tag:      "1.14.1",
-      revision: "f59ce19ec6b63bbb70a65c43ac423845f1129464"
+      tag:      "1.14.3",
+      revision: "a95e01fe300e14a11e7e9503d4b2c196ab755fcf"
   license "Apache-2.0"
   head "https://github.com/istio/istio.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/istioctl"
-    sha256 cellar: :any_skip_relocation, mojave: "ac46c14577dbcce6c0b378f78e8a8813ba0ac7ffcc01e1bb1c90e7c5fe0442ed"
+    sha256 cellar: :any_skip_relocation, mojave: "d014b4eb5244b221bf13f4beed68263b21e28022d7d63d2e026e2e040c1c6550"
   end
 
-  depends_on "go" => :build
   depends_on "go-bindata" => :build
+  # Required lucas-clemente/quic-go >= 0.28
+  # Try to switch to the latest go on the next release
+  depends_on "go@1.18" => :build
 
   uses_from_macos "curl" => :build
 
@@ -26,6 +28,8 @@ class Istioctl < Formula
 
     os = OS.kernel_name.downcase
     arch = Hardware::CPU.intel? ? "amd64" : Hardware::CPU.arch.to_s
+
+    ENV.prepend_path "PATH", Formula["curl"].opt_bin if OS.linux?
 
     system "make", "istioctl"
     bin.install "out/#{os}_#{arch}/istioctl"
