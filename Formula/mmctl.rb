196c067c652b922bc24ac1e917ca5aa94789b17c
Adrian Ho <the.gromgit@gmail.com>
2022-08-19 01:22:51 +0800

    mmctl: update 7.2.0 bottle.

--- Formula/mmctl.rb
+++ Formula/mmctl.rb
@@ -2,8 +2,8 @@ class Mmctl < Formula
   desc "Remote CLI tool for Mattermost server"
   homepage "https://github.com/mattermost/mmctl"
   url "https://github.com/mattermost/mmctl.git",
-      tag:      "v7.1.2",
-      revision: "a60dace71816ac4e38567c1ef766e51cac3b3a20"
+      tag:      "v7.2.0",
+      revision: "9e6121b94efac5f6a0dfa74ec707a01c3e1f6ceb"
   license "Apache-2.0"
   head "https://github.com/mattermost/mmctl.git", branch: "master"
 
@@ -14,7 +14,7 @@ class Mmctl < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mmctl"
-    sha256 cellar: :any_skip_relocation, mojave: "c41a61a75f29426927dbe9435f942ea0716459703a72b8e13feeddbf3016af3a"
+    sha256 cellar: :any_skip_relocation, mojave: "b54d553979e50fd1fe36376c0d0d7400ec14b488a7b9e3f9013c1e09845112c3"
   end
 
   depends_on "go" => :build
