51c1d2bcab0c690c0503972c62ff8405406a3acd
Adrian Ho <the.gromgit@gmail.com>
2022-08-18 18:09:15 +0800

    gopass-jsonapi: update 1.14.3 bottle.

--- Formula/gopass-jsonapi.rb
+++ Formula/gopass-jsonapi.rb
@@ -1,17 +1,13 @@
 class GopassJsonapi < Formula
   desc "Gopass Browser Bindings"
   homepage "https://github.com/gopasspw/gopass-jsonapi"
-  url "https://github.com/gopasspw/gopass-jsonapi/archive/v1.11.1.tar.gz"
-  sha256 "3c1666cdbf78a73736b089c3188c06724de53a96e43cb1d82fedd7e9543c120f"
+  url "https://github.com/gopasspw/gopass-jsonapi/archive/v1.14.3.tar.gz"
+  sha256 "64aea30cc1473bce81e80e407bf2911b0b67d08dbe17321c729685f847904259"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5b029bea2be7e8ac0019e06a5483a3593e6b741c0b85b69e99f3625bbaa1f10e"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "866c68f3fd44e4245febcee728c110d2fd2651514890eb7b4a31e6c5e3d429dc"
-    sha256 cellar: :any_skip_relocation, monterey:       "5ac1be40eec6bd6436c1f0ffc38fca10fdc1d4e57619d79dfbd6c395e8f2e44e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "57b58bc0327828830342b5268360f7f8b9c4980e3a07f1adac51d380f8241677"
-    sha256 cellar: :any_skip_relocation, catalina:       "d8d10b1fb7fa79a18f6fa2949ce1bccc2582bd9628e46e43da34e56a5eedc4a1"
-    sha256 cellar: :any_skip_relocation, mojave:         "9d9a8953da2f0777cef157a69c2784f46f3c5873fbb14f61dedffabce1074153"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gopass-jsonapi"
+    sha256 cellar: :any_skip_relocation, mojave: "2cd56f4b54b864579622f697232bb65070d3768e4efdddb002dfdfb2babc26e2"
   end
 
   depends_on "go" => :build
