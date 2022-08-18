aaa8f21029a027a9bd6cf024035017572178bfa9
Adrian Ho <the.gromgit@gmail.com>
2022-08-19 03:03:04 +0800

    tor: update 0.4.7.10 bottle.

--- Formula/tor.rb
+++ Formula/tor.rb
@@ -1,9 +1,9 @@
 class Tor < Formula
   desc "Anonymizing overlay network for TCP"
   homepage "https://www.torproject.org/"
-  url "https://www.torproject.org/dist/tor-0.4.7.8.tar.gz"
-  mirror "https://www.torservers.net/mirrors/torproject.org/dist/tor-0.4.7.8.tar.gz"
-  sha256 "9e9a5c67ad2acdd5f0f8be14ed591fed076b1708abf8344066990a0fa66fe195"
+  url "https://www.torproject.org/dist/tor-0.4.7.10.tar.gz"
+  mirror "https://www.torservers.net/mirrors/torproject.org/dist/tor-0.4.7.10.tar.gz"
+  sha256 "647e56dfa59ea36dab052027fcfc7663905c826c03509363c456900ecd435a5b"
   # Complete list of licenses:
   # https://gitweb.torproject.org/tor.git/plain/LICENSE
   license all_of: [
@@ -20,7 +20,7 @@ class Tor < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tor"
-    sha256 mojave: "d910a751136c70cc72db4ccf91bf5ac86b7ef24010f969bdb4f2d8019a2f3338"
+    sha256 mojave: "19d6fae745be87427f34b7f7daee058588da5822961fbb3575866a3ac5829963"
   end
 
   depends_on "pkg-config" => :build
