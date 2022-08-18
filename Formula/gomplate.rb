7c36041c51d23e718a85c871c1039e90982fc5f6
Adrian Ho <the.gromgit@gmail.com>
2022-08-18 17:50:38 +0800

    gomplate: update 3.11.2 bottle.

--- Formula/gomplate.rb
+++ Formula/gomplate.rb
@@ -1,14 +1,14 @@
 class Gomplate < Formula
   desc "Command-line Golang template processor"
   homepage "https://gomplate.hairyhenderson.ca/"
-  url "https://github.com/hairyhenderson/gomplate/archive/v3.11.1.tar.gz"
-  sha256 "888f5ab1eda0ee26a3d7494f5fe4b10a64b3a519a559ba949019559dff17149e"
+  url "https://github.com/hairyhenderson/gomplate/archive/v3.11.2.tar.gz"
+  sha256 "310f2ae19f409ad45f0f19a53045a3e8345e4723fa5ca9c5eeb8cbaf7cc0e195"
   license "MIT"
   head "https://github.com/hairyhenderson/gomplate.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gomplate"
-    sha256 cellar: :any_skip_relocation, mojave: "52f82223d9614252eeee2d0b1d039d6e8c20055a459036c037f9f52a793ede1d"
+    sha256 cellar: :any_skip_relocation, mojave: "ec9c3529344540584ef54b5305eff03024f7ce1ad2a4d105fe3ec646d7fbe44b"
   end
 
   depends_on "go" => :build
