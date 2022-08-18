c9df9058c4b26cd5467233c4b9576b944bf782d0
Adrian Ho <the.gromgit@gmail.com>
2022-08-18 14:24:42 +0800

    empty: update 0.6.22b bottle.

--- Formula/empty.rb
+++ Formula/empty.rb
@@ -1,8 +1,8 @@
 class Empty < Formula
   desc "Lightweight Expect-like PTY tool for shell scripts"
   homepage "https://empty.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/empty/empty/empty-0.6.21b/empty-0.6.21b.tgz"
-  sha256 "2fccd0faa1b3deaec1add679cbde3f34250e45872ad5df463badd4bb4edeb797"
+  url "https://downloads.sourceforge.net/project/empty/empty/empty-0.6.22b/empty-0.6.22b.tgz"
+  sha256 "f581d560e1fa857f5304586e3c2107b5838bcf11dedcccec4a7191fa9e261b69"
   license "BSD-3-Clause"
 
   livecheck do
@@ -11,14 +11,8 @@ class Empty < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "5bf2f0dabd46de8bef94154d66dfab1b9650097e92d92126b572fb7acd894764"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b330bcc238599680bc706db6b9f5a2947ad46c60196afce716811ccd2deb0ce0"
-    sha256 cellar: :any_skip_relocation, monterey:       "d5a02c5a6c5cb8973346e2f70326eb90d078900fa9da930164276cf9d9052c02"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e33747b4b2d1fc4e3fcfd35471c283e64c60c6cf6d558c6783d7a076cb84e233"
-    sha256 cellar: :any_skip_relocation, catalina:       "8eac558272ccf2338a374ea2e7158a4b0cf9008cc0111fefa8c85a80cfab2ee1"
-    sha256 cellar: :any_skip_relocation, mojave:         "8fb4ab0e88893f107afe0e69a48ed6f257a11b370bd56b2237ecadec771e1a17"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "3c5daa156ad925469841f360ca2687011a96086f7d6c5b8af0fedea97ee059ca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fbe3a154fe8d74f1f47499dd6abd381523b947385ede6152e82581e37f010fef"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/empty"
+    sha256 cellar: :any_skip_relocation, mojave: "db6fa84eefa0698ed5f5d564fe6d748bf844eb80862d206ece9c998d9ae94139"
   end
 
   def install
