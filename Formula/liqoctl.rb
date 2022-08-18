81474fcbfd285af940a39b06357b368865dbc6ad
Adrian Ho <the.gromgit@gmail.com>
2022-08-18 23:16:18 +0800

    liqoctl: update 0.5.4 bottle.

--- Formula/liqoctl.rb
+++ Formula/liqoctl.rb
@@ -1,13 +1,13 @@
 class Liqoctl < Formula
   desc "Is a CLI tool to install and manage Liqo-enabled clusters"
   homepage "https://liqo.io"
-  url "https://github.com/liqotech/liqo/archive/refs/tags/v0.5.3.tar.gz"
-  sha256 "d53935cc0f82d7562adabdb60614ffdb76bc944cab5d2df087e1046379e1f63f"
+  url "https://github.com/liqotech/liqo/archive/refs/tags/v0.5.4.tar.gz"
+  sha256 "f4ed993012d2315080a758e683ebe5f36eff6f389b2e68b2857f380bf1049228"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/liqoctl"
-    sha256 cellar: :any_skip_relocation, mojave: "261e57d2b6d4c0b5829d4a8c2e49090efa2a5930d4aa6a28aa6d3b9aa389fa44"
+    sha256 cellar: :any_skip_relocation, mojave: "ab45220dcfba835f03ef45dd3f8ab6ffea0983dc924a09e5fd788dc62d99a3c2"
   end
 
   depends_on "go" => :build
