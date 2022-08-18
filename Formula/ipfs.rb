cd997a4029a3934b29d277fa1417e11363552ba2
Adrian Ho <the.gromgit@gmail.com>
2022-08-18 20:14:06 +0800

    ipfs: update 0.14.0 bottle.

--- Formula/ipfs.rb
+++ Formula/ipfs.rb
@@ -1,14 +1,14 @@
 class Ipfs < Formula
   desc "Peer-to-peer hypermedia protocol"
-  homepage "https://ipfs.io/"
-  url "https://github.com/ipfs/go-ipfs.git",
-      tag:      "v0.13.1",
-      revision: "8ffc7a8a6c0d9ecdffd3624688fbf0cf348752d2"
+  homepage "https://ipfs.tech/"
+  url "https://github.com/ipfs/kubo.git",
+      tag:      "v0.14.0",
+      revision: "e0fabd6dbf69624a259dd735065465e09ebb0a61"
   license all_of: [
     "MIT",
     any_of: ["MIT", "Apache-2.0"],
   ]
-  head "https://github.com/ipfs/go-ipfs.git", branch: "master"
+  head "https://github.com/ipfs/kubo.git", branch: "master"
 
   livecheck do
     url :stable
@@ -17,10 +17,12 @@ class Ipfs < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ipfs"
-    sha256 cellar: :any_skip_relocation, mojave: "5b1ee49cd7bcad76098850f47e745130fc81c744132615262a8952f6e270e8a6"
+    sha256 cellar: :any_skip_relocation, mojave: "04bfb7d13093ef04b1a147bb3e495d9acc2f0901bb59e27d0dd0265e5185c24b"
   end
 
-  depends_on "go" => :build
+  # Required lucas-clemente/quic-go >= 0.28
+  # Try to switch to the latest go on the next release
+  depends_on "go@1.18" => :build
 
   def install
     system "make", "build"
