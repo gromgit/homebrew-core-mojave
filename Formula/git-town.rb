389238a218a5d1e0c3e3f653d889110033e40a3d
Adrian Ho <the.gromgit@gmail.com>
2022-08-18 16:46:43 +0800

    git-town: update 7.8.0 bottle.

--- Formula/git-town.rb
+++ Formula/git-town.rb
@@ -1,13 +1,13 @@
 class GitTown < Formula
   desc "High-level command-line interface for Git"
   homepage "https://www.git-town.com/"
-  url "https://github.com/git-town/git-town/archive/v7.7.0.tar.gz"
-  sha256 "edc4f87ef904ac297b9fbb30014e2ab474ee633c1687ed5011b38cd6f8b950e2"
+  url "https://github.com/git-town/git-town/archive/v7.8.0.tar.gz"
+  sha256 "a5c04923307ffe8e6cf6ec3ea720170e1565078af5eebba743556db855da8d03"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-town"
-    sha256 cellar: :any_skip_relocation, mojave: "787c0d5f5c92e55aedc8a18e5956c53f4a129d56ca9b7a35d40a7d4552c6a21d"
+    sha256 cellar: :any_skip_relocation, mojave: "2be7ba6cbc0bf0a52b7666e9c62f6b232450aae4b273d7832b72588053f001f2"
   end
 
   depends_on "go" => :build
