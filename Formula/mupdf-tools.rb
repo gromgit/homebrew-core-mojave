6465c1e5f9ac02ff58e461465bea3377103b0afc
Adrian Ho <the.gromgit@gmail.com>
2022-08-19 01:52:06 +0800

    mupdf-tools: update 1.20.3 bottle.

--- Formula/mupdf-tools.rb
+++ Formula/mupdf-tools.rb
@@ -1,8 +1,8 @@
 class MupdfTools < Formula
   desc "Lightweight PDF and XPS viewer"
   homepage "https://mupdf.com/"
-  url "https://mupdf.com/downloads/archive/mupdf-1.20.0-source.tar.lz"
-  sha256 "68dbb1cf5e31603380ce3f1c7f6c431ad442fa735d048700f50ab4de4c3b0f82"
+  url "https://mupdf.com/downloads/archive/mupdf-1.20.3-source.tar.lz"
+  sha256 "6f73f63ef8aa81991dfd023d4426a548827d1d74e0bfcf2a013acad63b651868"
   license "AGPL-3.0-or-later"
   head "https://git.ghostscript.com/mupdf.git", branch: "master"
 
@@ -12,7 +12,7 @@ class MupdfTools < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mupdf-tools"
-    sha256 cellar: :any_skip_relocation, mojave: "72277dcb0d75f1770f9d4c911dc0edf2114f722d1f2767cf96fb10c1e45285e0"
+    sha256 cellar: :any_skip_relocation, mojave: "bf51b2c3ee613d9a2f0817a25a5006b81ce636a9b7d61a0452cd2faa13fd7f5b"
   end
 
   conflicts_with "mupdf",
