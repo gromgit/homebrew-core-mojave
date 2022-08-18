b37d20a3d6860c65caf8fbb0b4add7c626dd2b9f
Adrian Ho <the.gromgit@gmail.com>
2022-08-18 11:51:58 +0800

    datree: update 1.6.6 bottle.

--- Formula/datree.rb
+++ Formula/datree.rb
@@ -1,8 +1,8 @@
 class Datree < Formula
   desc "CLI tool to run policies against Kubernetes manifests YAML files or Helm charts"
   homepage "https://www.datree.io/"
-  url "https://github.com/datreeio/datree/archive/1.6.0.tar.gz"
-  sha256 "98b9cd8c7cd3a75b4199b6872b4dfccc025af87cfc9568c2b23eaf523dfed72a"
+  url "https://github.com/datreeio/datree/archive/1.6.6.tar.gz"
+  sha256 "65380b8fcef40243ba7bc27f541eb90e4ee8e8a5515b7d7d434eeafa4ff33a2f"
   license "Apache-2.0"
   head "https://github.com/datreeio/datree.git", branch: "main"
 
@@ -13,7 +13,7 @@ class Datree < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/datree"
-    sha256 cellar: :any_skip_relocation, mojave: "a4475aacbf647f4049f5ff9f9a163400625ff58c9150f4ef6fdcd97bd097323a"
+    sha256 cellar: :any_skip_relocation, mojave: "2b891dca5dfbb30ba850fcee9d40f823ce5288067772a84a7bb9ad43aaff2681"
   end
 
   depends_on "go" => :build
