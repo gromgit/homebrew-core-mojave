7fe7c02c81a194f7eca3f41c65400e85f2ec2d7c
Adrian Ho <the.gromgit@gmail.com>
2022-08-18 18:47:42 +0800

    grpcurl: update 1.8.7 bottle.

--- Formula/grpcurl.rb
+++ Formula/grpcurl.rb
@@ -1,14 +1,13 @@
 class Grpcurl < Formula
   desc "Like cURL, but for gRPC"
   homepage "https://github.com/fullstorydev/grpcurl"
-  url "https://github.com/fullstorydev/grpcurl/archive/v1.8.6.tar.gz"
-  sha256 "18b457f644baabeef0de350596dd8d23563586ee94a3ed3cb290063e097ab934"
+  url "https://github.com/fullstorydev/grpcurl/archive/v1.8.7.tar.gz"
+  sha256 "7f7a59f8a5ef8833d30a94e1c36ddb0d76bab1ae64cd5c8bcb87d42e877c3bca"
   license "MIT"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/grpcurl"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "01b0c164335d05e5d77ac86f2f5129754e719d0966829ff63f23e34b61582d41"
+    sha256 cellar: :any_skip_relocation, mojave: "c3e6488a3d7179be82c5601a815a49ab36c791febf32d35b34ca02dfda79e6e9"
   end
 
   depends_on "go" => :build
