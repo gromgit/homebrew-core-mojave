269dbd04588b7e00af2da3f6bdf11cf1060e5aa0
Adrian Ho <the.gromgit@gmail.com>
2022-08-18 11:34:05 +0800

    csvq: update 1.17.10 bottle.

--- Formula/csvq.rb
+++ Formula/csvq.rb
@@ -1,8 +1,8 @@
 class Csvq < Formula
   desc "SQL-like query language for csv"
   homepage "https://mithrandie.github.io/csvq"
-  url "https://github.com/mithrandie/csvq/archive/v1.17.8.tar.gz"
-  sha256 "2e5c189d62f2dfd4f7a593be640893925626e49ea44ad020c1b141d82849e128"
+  url "https://github.com/mithrandie/csvq/archive/v1.17.10.tar.gz"
+  sha256 "0285f2d1e11eb3d571809d5bb4c2cf568b22c5dec68193c828b2f1c89f59da4e"
   license "MIT"
 
   livecheck do
@@ -12,7 +12,7 @@ class Csvq < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/csvq"
-    sha256 cellar: :any_skip_relocation, mojave: "445a0dd14897549bf5dc7682cb0c9fd69c130ca4161ccfe66a4ade56a4ece190"
+    sha256 cellar: :any_skip_relocation, mojave: "ce78b2a676a0167a156058b60b594aa2ed976108bc28ba17c04bbcad951664ae"
   end
 
   depends_on "go" => :build
