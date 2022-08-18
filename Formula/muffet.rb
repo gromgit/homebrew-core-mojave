c457a53bafa4409db9703af260249d8812c3ff47
Adrian Ho <the.gromgit@gmail.com>
2022-08-19 01:42:05 +0800

    muffet: update 2.6.0 bottle.

--- Formula/muffet.rb
+++ Formula/muffet.rb
@@ -1,14 +1,14 @@
 class Muffet < Formula
   desc "Fast website link checker in Go"
   homepage "https://github.com/raviqqe/muffet"
-  url "https://github.com/raviqqe/muffet/archive/v2.5.0.tar.gz"
-  sha256 "53a0f673181be525ad0c937c0c93d57343dc303886a9b60332730ea17258b52f"
+  url "https://github.com/raviqqe/muffet/archive/v2.6.0.tar.gz"
+  sha256 "77559c591f475d89c7608d2bbb815765fe85bfcd0e5bbbf819f8087730a11df6"
   license "MIT"
   head "https://github.com/raviqqe/muffet.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/muffet"
-    sha256 cellar: :any_skip_relocation, mojave: "1d0660803da33b792a5c705a63576b5d79d1994f7b9061a3afebc1184b7e307a"
+    sha256 cellar: :any_skip_relocation, mojave: "95bd482621ee9bc0e61bdeb2a737d774623e79269958e099ddf3c7c36e60eadd"
   end
 
   depends_on "go" => :build
