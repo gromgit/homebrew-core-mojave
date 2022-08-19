46b4d2034f674ff70824c469308e557cf9f66820
Adrian Ho <the.gromgit@gmail.com>
2022-08-19 10:30:38 +0800

    pint: update 0.28.6 bottle.

--- Formula/pint.rb
+++ Formula/pint.rb
@@ -1,13 +1,13 @@
 class Pint < Formula
   desc "Prometheus rule linter/validator"
   homepage "https://cloudflare.github.io/pint/"
-  url "https://github.com/cloudflare/pint/archive/refs/tags/v0.28.3.tar.gz"
-  sha256 "5c12d379a08de6483e8c7b7503c3b317cc639c8ecbd8015b8d415ec98018328a"
+  url "https://github.com/cloudflare/pint/archive/refs/tags/v0.28.6.tar.gz"
+  sha256 "48b14fa9c1a961a7a1c763c97bcc085c6ac312f129a966d4c59c7fa21a6424c6"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pint"
-    sha256 cellar: :any_skip_relocation, mojave: "b8f79ed7368b33300419433f5c91f6efc52c32567dd8310d610aec1cc085ea23"
+    sha256 cellar: :any_skip_relocation, mojave: "3cb84cc2cea4aee1479f978cf83694728d08dee89e162c68f28d4e262edfed53"
   end
 
   depends_on "go" => :build
@@ -19,6 +19,8 @@ class Pint < Formula
       -X main.commit=#{tap.user}
     ]
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/pint"
+
+    pkgshare.install "docs/examples"
   end
 
   test do
@@ -35,7 +37,12 @@ class Pint < Formula
             summary: High request latency
     EOS
 
-    assert_equal("level=info msg=\"File parsed\" path=#{testpath}/test.yaml rules=1\n",
-                 shell_output("#{bin}/pint -n lint #{testpath}/test.yaml 2>&1"))
+    cp pkgshare/"examples/simple.hcl", testpath/".pint.hcl"
+
+    output = shell_output("#{bin}/pint -n lint #{testpath}/test.yaml 2>&1")
+    assert_match "level=info msg=\"Loading configuration file\" path=.pint.hcl", output
+    assert_match "level=info msg=\"Problems found\" Warning=3", output
+
+    assert_match version.to_s, shell_output("#{bin}/pint version")
   end
 end
