1dff1ff9e8e3018d34059b3a36448e4b480e4f71
Adrian Ho <the.gromgit@gmail.com>
2022-08-20 01:51:58 +0800

    terracognita: update 0.8.1 bottle.

--- Formula/terracognita.rb
+++ Formula/terracognita.rb
@@ -1,14 +1,14 @@
 class Terracognita < Formula
   desc "Reads from existing Cloud Providers and generates Terraform code"
   homepage "https://github.com/cycloidio/terracognita"
-  url "https://github.com/cycloidio/terracognita/archive/v0.7.6.tar.gz"
-  sha256 "bc2361718cce62fb799f8470a268b9d2af7ab95a8337bbaa05d79f4b636482d1"
+  url "https://github.com/cycloidio/terracognita/archive/v0.8.1.tar.gz"
+  sha256 "dff35a6913d64dbd4e0595bd15d2f2029a8605a8bf0ba32a1de95f3236d85f1c"
   license "MIT"
   head "https://github.com/cycloidio/terracognita.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/terracognita"
-    sha256 cellar: :any_skip_relocation, mojave: "cf2ad6895e932711b9fe4576dedfaacc37252fa2caf1c2e09166b37c390e1785"
+    sha256 cellar: :any_skip_relocation, mojave: "18c7b0837599ebb0705c2ba7bf6acb4c0bb5255c2b7081c180c7f19d8403e20b"
   end
 
   depends_on "go" => :build
