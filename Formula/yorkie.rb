0b4b4795f494e20a841282059c68d5b08e669706
Adrian Ho <the.gromgit@gmail.com>
2022-08-20 04:20:26 +0800

    yorkie: update 0.2.16 bottle.

--- Formula/yorkie.rb
+++ Formula/yorkie.rb
@@ -2,17 +2,19 @@ class Yorkie < Formula
   desc "Document store for collaborative applications"
   homepage "https://yorkie.dev/"
   url "https://github.com/yorkie-team/yorkie.git",
-    tag:      "v0.2.13",
-    revision: "0cca5e25b8b83ad5b389f84735324a016f9691f9"
+    tag:      "v0.2.16",
+    revision: "b5321a49607ec6c52f7d46594cb7f16a0ebd41c9"
   license "Apache-2.0"
   head "https://github.com/yorkie-team/yorkie.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/yorkie"
-    sha256 cellar: :any_skip_relocation, mojave: "233f3084db6ef338700fbb1cfed03ee5b95f3c5fb92ff33ad1e75c6f6473163f"
+    sha256 cellar: :any_skip_relocation, mojave: "6522001e95bab948f041808683eff6fdf300258592f47f92584e02382a010391"
   end
 
-  depends_on "go" => :build
+  # Doesn't build with latest go
+  # See https://github.com/yorkie-team/yorkie/issues/378
+  depends_on "go@1.18" => :build
 
   def install
     system "make", "build"
@@ -32,6 +34,7 @@ class Yorkie < Formula
     end
     # sleep to let yorkie get ready
     sleep 3
+    system bin/"yorkie", "login", "-u", "admin", "-p", "admin"
 
     test_project = "test"
     output = shell_output("#{bin}/yorkie project create #{test_project}")
