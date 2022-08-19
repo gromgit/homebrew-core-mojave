f1100dee562f61162c8418be4ab64cefb7898053
Adrian Ho <the.gromgit@gmail.com>
2022-08-20 03:15:03 +0800

    txr: update 280 bottle.

--- Formula/txr.rb
+++ Formula/txr.rb
@@ -1,8 +1,8 @@
 class Txr < Formula
-  desc "Original, new programming language for convenient data munging"
+  desc "Lisp-like programming language for convenient data munging"
   homepage "https://www.nongnu.org/txr/"
-  url "http://www.kylheku.com/cgit/txr/snapshot/txr-278.tar.bz2"
-  sha256 "4703dd0d01852fce7579e8107cc6fcc47c42f003f7f617922002d78e9ff9fdcc"
+  url "http://www.kylheku.com/cgit/txr/snapshot/txr-280.tar.bz2"
+  sha256 "d62a967ab51e84b14f33add98c9618eb1a3da07a0d2bb9defdae8cdfee0be2ca"
   license "BSD-2-Clause"
 
   livecheck do
@@ -12,7 +12,7 @@ class Txr < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/txr"
-    sha256 cellar: :any, mojave: "a3386c4b41f17ae6d0e4b12f075905ba34d0d0ac474fca4634a917b486c00bc2"
+    sha256 cellar: :any, mojave: "e43584faed3893dceba65e40ddf3fdf5500ffc166187c4a072da7f00064f615b"
   end
 
   depends_on "libffi"
