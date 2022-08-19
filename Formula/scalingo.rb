674079538abb8fdc79adc4e5ed2a044b9b921cb7
Adrian Ho <the.gromgit@gmail.com>
2022-08-19 22:59:07 +0800

    scalingo: update 1.24.0 bottle.

--- Formula/scalingo.rb
+++ Formula/scalingo.rb
@@ -1,13 +1,13 @@
 class Scalingo < Formula
   desc "CLI for working with Scalingo's PaaS"
   homepage "https://doc.scalingo.com/cli"
-  url "https://github.com/Scalingo/cli/archive/1.23.0.tar.gz"
-  sha256 "382e0272296595f0327146cb37c5b991c5e3387e26d0f0a4a01b9ab6df76befe"
+  url "https://github.com/Scalingo/cli/archive/1.24.0.tar.gz"
+  sha256 "0bc043ecc6f8b536800d7cab2974addec2737d51a80c4cef020bc47f82a5f7f2"
   license "BSD-4-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/scalingo"
-    sha256 cellar: :any_skip_relocation, mojave: "ae6b08703ec4596d43dc849ae53d8ec4c3fa648dc6c1b7461cdfa03efa74a588"
+    sha256 cellar: :any_skip_relocation, mojave: "adbeb5d530e85bb4f4918074cfee361a6cd97ac8da08df00bb936636b50ef97b"
   end
 
   depends_on "go" => :build
