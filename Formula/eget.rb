9daa4e678c8af27f710d4dab53052ad81a1d692a
Adrian Ho <the.gromgit@gmail.com>
2022-08-21 04:31:05 +0800

    eget: update 1.2.0 bottle.

--- Formula/eget.rb
+++ Formula/eget.rb
@@ -1,14 +1,14 @@
 class Eget < Formula
   desc "Easily install prebuilt binaries from GitHub"
   homepage "https://github.com/zyedidia/eget"
-  url "https://github.com/zyedidia/eget/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "0075a032e31e0525a8171b326ed8fb923e3d1e5e30201557d6820cf2ffd047ee"
+  url "https://github.com/zyedidia/eget/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "e2a81c68cbd70de6cd89c87e4497a700974a058366bb5bb1f4244a5115995b21"
   license "MIT"
   head "https://github.com/zyedidia/eget.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/eget"
-    sha256 cellar: :any_skip_relocation, mojave: "cf2b67be2d8bab377dddc5d4d81a2d728710a4033f061012ecebe17ca73bb161"
+    sha256 cellar: :any_skip_relocation, mojave: "26ad264ea7f1b70efb389fbb007e9e6266807035db53e3d3a8aaf6ee6ae80291"
   end
 
   depends_on "go" => :build
