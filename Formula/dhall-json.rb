0200e3dac8df10a6de3e113f9a9bcdf03bcbc4f8
Adrian Ho <the.gromgit@gmail.com>
2022-08-21 01:58:33 +0800

    dhall-json: update 1.7.11 bottle.

--- Formula/dhall-json.rb
+++ Formula/dhall-json.rb
@@ -1,14 +1,14 @@
 class DhallJson < Formula
   desc "Dhall to JSON compiler and a Dhall to YAML compiler"
   homepage "https://github.com/dhall-lang/dhall-haskell/tree/master/dhall-json"
-  url "https://hackage.haskell.org/package/dhall-json-1.7.10/dhall-json-1.7.10.tar.gz"
-  sha256 "e15cd486b287ed281cd56da8d0603bb70dbfa33f2b3e107682d82935dad3f785"
+  url "https://hackage.haskell.org/package/dhall-json-1.7.11/dhall-json-1.7.11.tar.gz"
+  sha256 "26c2acf33bfc597f4d2ac333a431180c9929d7a9b4ef7cf70c4bd45eba65ef28"
   license "BSD-3-Clause"
   head "https://github.com/dhall-lang/dhall-haskell.git", branch: "master"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dhall-json"
-    sha256 cellar: :any_skip_relocation, mojave: "9dcc740147c2745c4bde929daf4ca1ad2482cb126ee3ce73809f640ccc480abe"
+    sha256 cellar: :any_skip_relocation, mojave: "54da7e71d98031422192de37b4ef54d648b5c3fdb85fd5825641d2c3cdf1de52"
   end
 
   depends_on "cabal-install" => :build
