b2ec2b97fce12a4852da17e87af76efc700422f0
Adrian Ho <the.gromgit@gmail.com>
2022-08-21 02:39:01 +0800

    dhall-yaml: update 1.2.11 bottle.

--- Formula/dhall-yaml.rb
+++ Formula/dhall-yaml.rb
@@ -1,15 +1,14 @@
 class DhallYaml < Formula
   desc "Convert between Dhall and YAML"
   homepage "https://github.com/dhall-lang/dhall-haskell/tree/master/dhall-yaml"
-  url "https://hackage.haskell.org/package/dhall-yaml-1.2.10/dhall-yaml-1.2.10.tar.gz"
-  sha256 "d6228d330ca593c98a1882f4d0f201917dff395b09b7bfd23e78d1940e416fa8"
+  url "https://hackage.haskell.org/package/dhall-yaml-1.2.11/dhall-yaml-1.2.11.tar.gz"
+  sha256 "90ba0e61caeb1f1b671dd5d21ced247d5c761fb9b1c3d7b467c907fbe24b16c7"
   license "BSD-3-Clause"
   head "https://github.com/dhall-lang/dhall-haskell.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dhall-yaml"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "b2ac94036d8c43fb41eff766520db0886dd0dafb75f3b72fdae95c95cb47bdc4"
+    sha256 cellar: :any_skip_relocation, mojave: "5a1798fcf0322bef3d15886f96b76dc5f1712f186926e5c2a6e7a7f551b71826"
   end
 
   depends_on "cabal-install" => :build
