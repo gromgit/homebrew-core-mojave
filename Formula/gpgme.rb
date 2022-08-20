f82eae3f99d0b06ccee7a27ab7ab840c6ac1fe3d
Adrian Ho <the.gromgit@gmail.com>
2022-08-21 07:01:35 +0800

    gpgme: update 1.18.0 bottle.

--- Formula/gpgme.rb
+++ Formula/gpgme.rb
@@ -1,10 +1,9 @@
 class Gpgme < Formula
   desc "Library access to GnuPG"
   homepage "https://www.gnupg.org/related_software/gpgme/"
-  url "https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.17.1.tar.bz2"
-  sha256 "711eabf5dd661b9b04be9edc9ace2a7bc031f6bd9d37a768d02d0efdef108f5f"
+  url "https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.18.0.tar.bz2"
+  sha256 "361d4eae47ce925dba0ea569af40e7b52c645c4ae2e65e5621bf1b6cdd8b0e9e"
   license "LGPL-2.1-or-later"
-  revision 1
 
   livecheck do
     url "https://gnupg.org/ftp/gcrypt/gpgme/"
@@ -13,7 +12,7 @@ class Gpgme < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gpgme"
-    sha256 cellar: :any, mojave: "946d22fb31a4f2a67b738a9ef4977c2ee37bf2efa5bd91dccfa4f52f8964b915"
+    sha256 cellar: :any, mojave: "923f4bee2963c6fd168d354d5a85008896afd1cae07ade000499bd5a0ff6744b"
   end
 
   depends_on "python@3.10" => [:build, :test]
@@ -32,16 +31,21 @@ class Gpgme < Formula
   end
 
   def install
-    ENV["PYTHON"] = which("python3")
+    python = "python3.10"
+    ENV["PYTHON"] = python
     # setuptools>=60 prefers its own bundled distutils, which breaks the installation
     # Remove when distutils is no longer used. Related PR: https://dev.gnupg.org/D545
     ENV["SETUPTOOLS_USE_DISTUTILS"] = "stdlib"
+
+    # Uses generic lambdas.
+    # error: 'auto' not allowed in lambda parameter
+    ENV.append "CXXFLAGS", "-std=c++14"
 
     # Work around Homebrew's "prefix scheme" patch which causes non-pip installs
     # to incorrectly try to write into HOMEBREW_PREFIX/lib since Python 3.10.
     inreplace "lang/python/Makefile.in",
               /^\s*install\s*\\\n\s*--prefix "\$\(DESTDIR\)\$\(prefix\)"/,
-              "\\0 --install-lib=#{prefix/Language::Python.site_packages("python3")}"
+              "\\0 --install-lib=#{prefix/Language::Python.site_packages(python)}"
 
     system "./configure", *std_configure_args,
                           "--disable-silent-rules",
@@ -55,6 +59,6 @@ class Gpgme < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/gpgme-tool --lib-version")
-    system Formula["python@3.10"].opt_bin/"python3", "-c", "import gpg; print(gpg.version.versionstr)"
+    system Formula["python@3.10"].opt_bin/"python3.10", "-c", "import gpg; print(gpg.version.versionstr)"
   end
 end
