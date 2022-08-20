c35d8e8976b4d2e83a70b74544d28520dc7f68ae
Adrian Ho <the.gromgit@gmail.com>
2022-08-21 07:05:28 +0800

    grpcui: update 1.3.1 bottle.

--- Formula/grpcui.rb
+++ Formula/grpcui.rb
@@ -1,14 +1,13 @@
 class Grpcui < Formula
   desc "Interactive web UI for gRPC, along the lines of postman"
   homepage "https://github.com/fullstorydev/grpcui"
-  url "https://github.com/fullstorydev/grpcui/archive/v1.3.0.tar.gz"
-  sha256 "56519818d08a47339dece319cb4c8387a65bf24623f49242ef6a1201a1eb8b15"
+  url "https://github.com/fullstorydev/grpcui/archive/v1.3.1.tar.gz"
+  sha256 "01cfa0bbaf9cfdaa61ae0341c83cde3372854133d62cb9b91c3a111eaa145815"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/grpcui"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "ab30f63d27f29a46d0da26c0e64dca2fc6ad38c5a4d6c1162c91e63c09b75bed"
+    sha256 cellar: :any_skip_relocation, mojave: "e531c4ae7f3f18a8d74c819961ce5b11c3bb1eeefd7ee3e92dbc8a6dab655728"
   end
 
   # Bump to 1.18 on the next release, if possible.
