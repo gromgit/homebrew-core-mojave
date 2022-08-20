c1373ccdd66af35a0889213e8c9241d0019682be
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-20 14:38:06 +0800

    php-cs-fixer: no rebuilding required

--- Formula/php-cs-fixer.rb
+++ Formula/php-cs-fixer.rb
@@ -1,12 +1,12 @@
 class PhpCsFixer < Formula
   desc "Tool to automatically fix PHP coding standards issues"
   homepage "https://cs.symfony.com/"
-  url "https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v3.9.5/php-cs-fixer.phar"
-  sha256 "b83f0dfdf2406fc96fb05bcfff3bb0f63c15f2783fff1584fa836e399e79dd45"
+  url "https://github.com/FriendsOfPHP/PHP-CS-Fixer/releases/download/v3.10.0/php-cs-fixer.phar"
+  sha256 "7615e4b70f7072d230bc894c138738c81c3ba817df7ad88446dd42e90582ad0a"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "8893087037fc8d4f1d3dd340bf69902872223649b048f069d01beafe383bf221"
+    sha256 cellar: :any_skip_relocation, all: "9548a064dad8ce2cdb0d46e841c725e57dded8a2be70f0d53c09a9da9298ea0a"
   end
 
   depends_on "php"
