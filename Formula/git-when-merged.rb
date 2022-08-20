348a1cbf1f0c39210b8f98d2c67a2ad392f78e2e
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-20 14:37:59 +0800

    git-when-merged: no rebuilding required

--- Formula/git-when-merged.rb
+++ Formula/git-when-merged.rb
@@ -6,16 +6,23 @@ class GitWhenMerged < Formula
   url "https://github.com/mhagger/git-when-merged/archive/v1.2.1.tar.gz"
   sha256 "46ba5076981862ac2ad0fa0a94b9a5401ef6b5c5b0506c6e306b76e5798e1f58"
   license "GPL-2.0-only"
+  head "https://github.com/mhagger/git-when-merged.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "cef2e377082bfa5b6457f6b6d19066fd7a74312860533b85fb62d6e16c0986b7"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "351c05a7dfe0568ffde09a4633ab23d253ab10e0c3e0b2421e3f9b4740bae4b7"
   end
 
-  depends_on "python@3.10"
+  # TODO: Update this to whichever python has `bin/python3`.
+  depends_on "python@3.10" => :test
+  uses_from_macos "python"
 
   def install
-    rewrite_shebang detected_python_shebang, "bin/git-when-merged"
-    bin.install "bin/git-when-merged"
+    bin.install "src/git_when_merged.py" => "git-when-merged"
+
+    if !OS.mac? || MacOS.version >= :catalina
+      rewrite_shebang detected_python_shebang(use_python_from_path: true), bin/"git-when-merged"
+    end
   end
 
   test do
@@ -34,6 +41,11 @@ class GitWhenMerged < Formula
     touch "baz"
     system "git", "add", "baz"
     system "git", "commit", "-m", "baz"
-    system "#{bin}/git-when-merged", "bar"
+    system bin/"git-when-merged", "bar"
+
+    # Test with both Homebrew Python3 and system Python3 to validate our shebang.
+    which_all("python3").each do |python|
+      system python, bin/"git-when-merged", "bar"
+    end
   end
 end
