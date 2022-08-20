71b7b7a1d256802f694fb38731a623c1b35d11ca
Adrian Ho <the.gromgit@gmail.com>
2022-08-21 01:36:53 +0800

    cython: update 0.29.32_1 bottle.

--- Formula/cython.rb
+++ Formula/cython.rb
@@ -4,10 +4,11 @@ class Cython < Formula
   url "https://files.pythonhosted.org/packages/4c/76/1e41fbb365ad20b6efab2e61b0f4751518444c953b390f9b2d36cf97eea0/Cython-0.29.32.tar.gz"
   sha256 "8733cf4758b79304f2a4e39ebfac5e92341bce47bcceb26c1254398b2f8c1af7"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cython"
-    sha256 cellar: :any_skip_relocation, mojave: "d399231a5bb393277dfb0c7c9462aa0994c9ffd1920ecf13bbcc5ec163bf9b38"
+    sha256 cellar: :any_skip_relocation, mojave: "069e877a0e4231b63a77eeb790497ca1142f94575f8f23cc4ce31a7f566bb227"
   end
 
   keg_only <<~EOS
@@ -15,20 +16,20 @@ class Cython < Formula
     Users are advised to use `pip` to install cython
   EOS
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
-    xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
-    system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(libexec)
+    python = "python3.10"
+    ENV.prepend_create_path "PYTHONPATH", libexec/Language::Python.site_packages(python)
+    system python, *Language::Python.setup_install_args(libexec, python)
 
-    bin.install Dir[libexec/"bin/*"]
+    bin.install (libexec/"bin").children
     bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
   end
 
   test do
-    xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
-    ENV.prepend_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+    python = Formula["python@3.10"].opt_bin/"python3.10"
+    ENV.prepend_path "PYTHONPATH", libexec/Language::Python.site_packages(python)
 
     phrase = "You are using Homebrew"
     (testpath/"package_manager.pyx").write "print '#{phrase}'"
@@ -40,7 +41,7 @@ class Cython < Formula
         ext_modules = cythonize("package_manager.pyx")
       )
     EOS
-    system Formula["python@3.9"].opt_bin/"python3", "setup.py", "build_ext", "--inplace"
-    assert_match phrase, shell_output("#{Formula["python@3.9"].opt_bin}/python3 -c 'import package_manager'")
+    system python, "setup.py", "build_ext", "--inplace"
+    assert_match phrase, shell_output("#{python} -c 'import package_manager'")
   end
 end
