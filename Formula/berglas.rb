a188f95f65b0d7e35b542beebb9bd123f53fcb7c
Adrian Ho <the.gromgit@gmail.com>
2022-08-21 00:26:52 +0800

    berglas: update 1.0.1 bottle.

--- Formula/berglas.rb
+++ Formula/berglas.rb
@@ -1,13 +1,13 @@
 class Berglas < Formula
   desc "Tool for managing secrets on Google Cloud"
   homepage "https://github.com/GoogleCloudPlatform/berglas"
-  url "https://github.com/GoogleCloudPlatform/berglas/archive/v1.0.0.tar.gz"
-  sha256 "634238a2793867a5b8c209617a025fe19002a88b53cb54eef45fc2b9c0fcc55a"
+  url "https://github.com/GoogleCloudPlatform/berglas/archive/v1.0.1.tar.gz"
+  sha256 "be619fe870249e74f52076d16020808b6020fedf2b98685f7c14145a291a2fe7"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/berglas"
-    sha256 cellar: :any_skip_relocation, mojave: "ff63ca51e700ebeb990494659a7c5ce258c3d84ad34dc2b42ac010babc21c4c0"
+    sha256 cellar: :any_skip_relocation, mojave: "15baee033fd6e55f60e49db28ded626c32264e2a7b248db13fd595e534a52f25"
   end
 
   depends_on "go" => :build
