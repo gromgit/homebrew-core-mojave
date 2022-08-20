dcf9d0299bfef62cced14eb52c14ce9e4faf7aea
Adrian Ho <the.gromgit@gmail.com>
2022-08-21 06:49:59 +0800

    gost: update 2.11.3 bottle.

--- Formula/gost.rb
+++ Formula/gost.rb
@@ -1,14 +1,14 @@
 class Gost < Formula
   desc "GO Simple Tunnel - a simple tunnel written in golang"
   homepage "https://github.com/ginuerzh/gost"
-  url "https://github.com/ginuerzh/gost/archive/v2.11.2.tar.gz"
-  sha256 "143174a9ba5b0b6251d1d9a52267220f97bec1319676618746c1a5d7a7a86d96"
+  url "https://github.com/ginuerzh/gost/archive/v2.11.3.tar.gz"
+  sha256 "2e3a696c4147b188da2b2a91db91ce0608d6b41478fd7e77e1b009916f3bff09"
   license "MIT"
   head "https://github.com/ginuerzh/gost.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gost"
-    sha256 cellar: :any_skip_relocation, mojave: "b97dd37c4b981ef2f836a1e98fe693b77b9d966cc69ab2de24c9d9ef4f2a7afa"
+    sha256 cellar: :any_skip_relocation, mojave: "ec0932ee8c9757743a4d074cc268e35fc257071f6a21b6949d6a9050601e41f2"
   end
 
   # Bump to 1.18 on the next release, if possible.
