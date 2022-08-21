62f664f0892efba7358bcb433c327d47d329c888
Adrian Ho <the.gromgit@gmail.com>
2022-08-21 09:42:35 +0800

    kubergrunt: update 0.9.2 bottle.

--- Formula/kubergrunt.rb
+++ Formula/kubergrunt.rb
@@ -1,13 +1,13 @@
 class Kubergrunt < Formula
   desc "Collection of commands to fill in the gaps between Terraform, Helm, and Kubectl"
   homepage "https://github.com/gruntwork-io/kubergrunt"
-  url "https://github.com/gruntwork-io/kubergrunt/archive/v0.9.1.tar.gz"
-  sha256 "8bc05d05ffb61af96c00823ecc1eacc0fdd33de985bfc2501baf0ea829762367"
+  url "https://github.com/gruntwork-io/kubergrunt/archive/v0.9.2.tar.gz"
+  sha256 "7e7ec360c78ebfc672593d66766e387f95d5a7807e79d13298efb9e0128feb61"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kubergrunt"
-    sha256 cellar: :any_skip_relocation, mojave: "634cc7fb4b1181e80b66b23558d82b286d8831d6bae73575ee9cf9e56428b05f"
+    sha256 cellar: :any_skip_relocation, mojave: "4d7a3d220da63fef65d87d264174f1e236523d78f3dc85371ef3b9dc7b7cd492"
   end
 
   # Bump to 1.18 on the next release, if possible.
