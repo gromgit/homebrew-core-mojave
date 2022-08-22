300834c8e5e1427120944b036518cffb73585b37
Adrian Ho <the.gromgit@gmail.com>
2022-08-22 19:02:38 +0800

    trivy: update 0.31.2 bottle.

--- Formula/trivy.rb
+++ Formula/trivy.rb
@@ -1,14 +1,14 @@
 class Trivy < Formula
   desc "Vulnerability scanner for container images, file systems, and Git repos"
   homepage "https://aquasecurity.github.io/trivy/"
-  url "https://github.com/aquasecurity/trivy/archive/v0.31.0.tar.gz"
-  sha256 "29aa7e34d368e09bbf0a91a37ce9a817805580cb322dc128ee5ebd53c6f6720c"
+  url "https://github.com/aquasecurity/trivy/archive/v0.31.2.tar.gz"
+  sha256 "3058e1b6fed23c1cad10b6b9d950174a6579083821b1ef6788efaf3955bf3253"
   license "Apache-2.0"
   head "https://github.com/aquasecurity/trivy.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/trivy"
-    sha256 cellar: :any_skip_relocation, mojave: "3c011b16861559444d79f78ca99bf71b14bb780cd2c49954a15ad72f93ffc062"
+    sha256 cellar: :any_skip_relocation, mojave: "36d6bd59f5f24a1e5f1db6640bac1f36a72dae8ad75ecc3dd402b4b0a1e17882"
   end
 
   depends_on "go" => :build
