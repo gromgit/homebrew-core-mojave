5dc9caf884ae9af2617af9803641a8f8268217a2
Adrian Ho <the.gromgit@gmail.com>
2022-08-22 17:19:39 +0800

    sonarqube: update 9.6.0.59041 bottle.

--- Formula/sonarqube.rb
+++ Formula/sonarqube.rb
@@ -1,8 +1,8 @@
 class Sonarqube < Formula
   desc "Manage code quality"
   homepage "https://www.sonarqube.org/"
-  url "https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.4.0.54424.zip"
-  sha256 "df8d97ae93002593fcaceacfe4dfdab68982a8dc52002c441ee6218f4d71961c"
+  url "https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.6.0.59041.zip"
+  sha256 "e3c21ba1f6a53f766e4ab56a484ddedbba9ebd31e82a1999e618ee5ffb73e5f2"
   license "LGPL-3.0-or-later"
 
   livecheck do
@@ -12,43 +12,44 @@ class Sonarqube < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sonarqube"
-    sha256 cellar: :any_skip_relocation, mojave: "9f40efc9c1cfca5bb0cfd138dddec5c65e5dd7339ecfab5b81e595f1fd108430"
+    sha256 cellar: :any_skip_relocation, mojave: "071b655784b46732eb512294c659b690910abb168d890fa0e991cac4dc008230"
   end
 
-  depends_on "java-service-wrapper"
   depends_on "openjdk@11"
 
   conflicts_with "sonarqube-lts", because: "both install the same binaries"
 
   def install
-    # Use Java Service Wrapper 3.5.46 which is Apple Silicon compatible
-    # Java Service Wrapper doesn't support the  wrapper binary to be symlinked, so it's copied
-    jsw_libexec = Formula["java-service-wrapper"].opt_libexec
-    ln_s jsw_libexec/"lib/wrapper.jar", "#{buildpath}/lib/jsw/wrapper-3.5.46.jar"
-    ln_s jsw_libexec/"lib/libwrapper.dylib", "#{buildpath}/bin/macosx-universal-64/lib/"
-    cp jsw_libexec/"bin/wrapper", "#{buildpath}/bin/macosx-universal-64/"
-    cp jsw_libexec/"scripts/App.sh.in", "#{buildpath}/bin/macosx-universal-64/sonar.sh"
-    sonar_sh_file = "bin/macosx-universal-64/sonar.sh"
-    inreplace sonar_sh_file, "@app.name@", "SonarQube"
-    inreplace sonar_sh_file, "@app.long.name@", "SonarQube"
-    inreplace sonar_sh_file, "../conf/wrapper.conf", "../../conf/wrapper.conf"
-    inreplace "conf/wrapper.conf", "wrapper-3.2.3.jar", "wrapper-3.5.46.jar"
-    rm "lib/jsw/wrapper-3.2.3.jar"
-    rm "bin/macosx-universal-64/lib/libwrapper.jnilib"
+    platform = OS.mac? ? "macosx-universal-64" : "linux-x86-64"
 
-    # Delete native bin directories for other systems
-    remove, keep = if OS.mac?
-      ["linux", "macosx-universal"]
-    else
-      ["macosx", "linux-x86"]
+    inreplace buildpath/"bin"/platform/"sonar.sh",
+      %r{^PIDFILE="\./\$APP_NAME\.pid"$},
+      "PIDFILE=#{var}/run/\$APP_NAME.pid"
+
+    inreplace "conf/sonar.properties" do |s|
+      # Write log/data/temp files outside of installation directory
+      s.sub!(/^#sonar\.path\.data=.*/, "sonar.path.data=#{var}/sonarqube/data")
+      s.sub!(/^#sonar\.path\.logs=.*/, "sonar.path.logs=#{var}/sonarqube/logs")
+      s.sub!(/^#sonar\.path\.temp=.*/, "sonar.path.temp=#{var}/sonarqube/temp")
     end
 
-    rm_rf Dir["bin/{#{remove},windows}-*"]
-
     libexec.install Dir["*"]
+    env = Language::Java.overridable_java_home_env("11")
+    env["PATH"] = "$JAVA_HOME/bin:$PATH"
+    (bin/"sonar").write_env_script libexec/"bin"/platform/"sonar.sh", env
+  end
 
-    (bin/"sonar").write_env_script libexec/"bin/#{keep}-64/sonar.sh",
-      Language::Java.overridable_java_home_env("11")
+  def post_install
+    (var/"run").mkpath
+    (var/"sonarqube/logs").mkpath
+  end
+
+  def caveats
+    <<~EOS
+      Data: #{var}/sonarqube/data
+      Logs: #{var}/sonarqube/logs
+      Temp: #{var}/sonarqube/temp
+    EOS
   end
 
   service do
@@ -57,6 +58,24 @@ class Sonarqube < Formula
   end
 
   test do
-    assert_match "SonarQube", shell_output("#{bin}/sonar status", 1)
+    port = free_port
+    ENV["SONAR_WEB_PORT"] = port.to_s
+    ENV["SONAR_EMBEDDEDDATABASE_PORT"] = free_port.to_s
+    ENV["SONAR_SEARCH_PORT"] = free_port.to_s
+    ENV["SONAR_PATH_DATA"] = testpath/"data"
+    ENV["SONAR_PATH_LOGS"] = testpath/"logs"
+    ENV["SONAR_PATH_TEMP"] = testpath/"temp"
+    ENV["SONAR_TELEMETRY_ENABLE"] = "false"
+
+    assert_match(/SonarQube.* is not running/, shell_output("#{bin}/sonar status", 1))
+    pid = fork { exec bin/"sonar", "console" }
+    begin
+      sleep 15
+      output = shell_output("#{bin}/sonar status")
+      assert_match(/SonarQube is running \([0-9]*?\)/, output)
+    ensure
+      Process.kill 9, pid
+      Process.wait pid
+    end
   end
 end
