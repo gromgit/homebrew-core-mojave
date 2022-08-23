218dc453c21cb3e84e07c7653d250b9a542e3e52
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-23 12:48:03 +0800

    arcade-learning-environment:

--- Formula/arcade-learning-environment.rb
+++ Formula/arcade-learning-environment.rb
@@ -7,15 +7,16 @@ class ArcadeLearningEnvironment < Formula
       tag:      "v0.7.5",
       revision: "db3728264f382402120913d76c4fa0dc320ef59f"
   license "GPL-2.0-only"
+  revision 1
   head "https://github.com/mgbellemare/Arcade-Learning-Environment.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "69b3c918b471d86a15ddc33ca7d8788b7df97cdb37c24b0f9bf278acea044a92"
-    sha256 cellar: :any,                 arm64_big_sur:  "b91365dd433a5aaa6c63e1930fa485a0c7d8f30aee4709f77d47a1c227aad39c"
-    sha256 cellar: :any,                 monterey:       "928deddbfd6b71a6683d9baabb34e95e42b2426e306d050cf785e5aa0ea45c07"
-    sha256 cellar: :any,                 big_sur:        "385f3c78c9926ea3cf5fb5ea44d0ef363d3c097058e039e822a127d3f7f031f1"
-    sha256 cellar: :any,                 catalina:       "6b4982ba66a1dae92d8894c64d10413eefe553bcb3e368770d49a4ea66b6af3b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "11739052d50db05aac650a0d23c46aeb04535b9ff97b982c3687bb5baac0fbbc"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f44aa61c351794dacdd3156f3e9747d2d0c6af6ef6e24daad37d3088f11b778c"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3fd32b01bfd47208e8c915d7a7f1d72a7508834a67a34a470319c9667f23ba75"
+    sha256 cellar: :any_skip_relocation, monterey:       "4da8b46caf13dd98f1ed7154dee2db9cbe9747dfde30e6e597fbe5455ccb93aa"
+    sha256 cellar: :any_skip_relocation, big_sur:        "d533c3f619efa49e2b9f4c45f8b7f7d16186dababb36ce2aad167741156c9b6a"
+    sha256 cellar: :any_skip_relocation, catalina:       "e2d34a7ce0ed9c85cc3e77df1aac73df83b1e68df5ad2b3ad3cce8eccce2d7d5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "31838cbb634a1cc7803fccf6b454bd9437b72a9e92e72f9541fe81615174093a"
   end
 
   depends_on "cmake" => :build
@@ -32,42 +33,46 @@ class ArcadeLearningEnvironment < Formula
 
   fails_with gcc: "5"
 
-  # Issue building with older setuptools currently included with Python 3.10.4.
-  # TODO: remove after next python update
-  resource "setuptools" do
-    url "https://files.pythonhosted.org/packages/4a/25/ec29a23ef38b9456f9965c57a9e1221e6c246d87abbf2a31158799bca201/setuptools-62.3.2.tar.gz"
-    sha256 "a43bdedf853c670e5fed28e5623403bad2f73cf02f9a2774e91def6bda8265a7"
-  end
-
   resource "importlib-resources" do
     url "https://files.pythonhosted.org/packages/07/3c/4e27ef7d4cea5203ed4b52b7fe96ddd08559d9f147a2a4307e7d6d98c035/importlib_resources-5.7.1.tar.gz"
     sha256 "b6062987dfc51f0fcb809187cffbd60f35df7acb4589091f154214af6d0d49d3"
   end
 
+  def python3
+    "python3.10"
+  end
+
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args,
-                    "-DCMAKE_INSTALL_NAME_DIR=#{opt_lib}",
-                    "-DCMAKE_BUILD_WITH_INSTALL_NAME_DIR=ON",
+    system "cmake", "-S", ".", "-B", "build",
                     "-DSDL_SUPPORT=ON",
-                    "-DSDL_DYNLOAD=ON"
+                    "-DSDL_DYNLOAD=ON",
+                    *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
     pkgshare.install "tests/resources/tetris.bin"
 
-    venv = virtualenv_create(libexec, "python3")
+    venv = virtualenv_create(libexec, python3)
     venv.pip_install resources
 
     # error: no member named 'signbit' in the global namespace
     inreplace "setup.py", "cmake_args = [", "\\0\"-DCMAKE_OSX_SYSROOT=#{MacOS.sdk_path}\"," if OS.mac?
 
     # `venv.pip_install_and_link buildpath` fails to install scripts, so manually run setup.py instead
-    bin_before = Dir[libexec/"bin/*"].to_set
-    system libexec/"bin/python3", *Language::Python.setup_install_args(libexec)
-    bin.install_symlink (Dir[libexec/"bin/*"].to_set - bin_before).to_a
+    bin_before = (libexec/"bin").children.to_set
+    system libexec/"bin/python", *Language::Python.setup_install_args(libexec)
+    bin.install_symlink ((libexec/"bin").children.to_set - bin_before).to_a
 
-    site_packages = Language::Python.site_packages("python3")
+    site_packages = Language::Python.site_packages(python3)
     pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
     (prefix/site_packages/"homebrew-ale-py.pth").write pth_contents
+
+    # Replace vendored `libSDL2` with a symlink to our own.
+    libsdl2 = Formula["sdl2"].opt_lib/shared_library("libSDL2")
+    vendored_libsdl2_dir = libexec/site_packages/"ale_py"
+    (vendored_libsdl2_dir/shared_library("libSDL2")).unlink
+
+    # Use `ln_s` to avoid referencing a Cellar path.
+    ln_s libsdl2.relative_path_from(vendored_libsdl2_dir), vendored_libsdl2_dir
   end
 
   test do
@@ -90,7 +95,7 @@ class ArcadeLearningEnvironment < Formula
       assert len(ale.getLegalActionSet()) == 18
     EOS
 
-    output = shell_output("#{Formula["python@3.10"].opt_bin}/python3 test.py 2>&1")
+    output = shell_output("#{python3} test.py 2>&1")
     assert_match <<~EOS, output
       Game console created:
         ROM file:  tetris.bin
