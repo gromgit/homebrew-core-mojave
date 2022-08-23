b5bd472af2451c59958ee53aab065c8d6a03e760
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-23 12:48:12 +0800

    linux-headers@5.15:

---
+++ Formula/linux-headers@5.15.rb
@@ -0,0 +1,25 @@
+class LinuxHeadersAT515 < Formula
+  desc "Header files of the Linux kernel"
+  homepage "https://kernel.org/"
+  url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.57.tar.gz"
+  sha256 "d33d0e62f05d90d579eb0cc51343f48c32eb11e4fef0306c374b908212f51578"
+  license "GPL-2.0-only"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "adb558844cfbf7fb8af6bd8134bc61120218246bd40b4746250fba1ae0a7933a"
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "rsync" => :build
+  depends_on :linux
+
+  def install
+    system "make", "headers_install", "INSTALL_HDR_PATH=#{prefix}"
+    rm prefix.glob("**/{.install,..install.cmd}")
+  end
+
+  test do
+    assert_match "KERNEL_VERSION", (include/"linux/version.h").read
+  end
+end
