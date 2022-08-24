bd22ceeed5013f531a2fa1c229c09ac101f5dc2d
Adrian Ho <the.gromgit@gmail.com>
2022-08-25 06:16:33 +0800

    libvatek: update 3.05 bottle.

---
+++ Formula/libvatek.rb
@@ -0,0 +1,48 @@
+class Libvatek < Formula
+  desc "User library to control VATek chips"
+  homepage "https://github.com/VisionAdvanceTechnologyInc/vatek_sdk_2"
+  url "https://github.com/VisionAdvanceTechnologyInc/vatek_sdk_2/archive/v3.05.tar.gz"
+  sha256 "35cabadc9a626460f030959ed86ad8619ce070ddb5fa528abf15dfa34c10619d"
+  license "BSD-2-Clause"
+  head "https://github.com/VisionAdvanceTechnologyInc/vatek_sdk_2.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libvatek"
+    sha256 cellar: :any, mojave: "61c6378976fae8497065278926f864f086a0977809689932f7be9b7b20129f6c"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "libusb"
+
+  def install
+    system "cmake", "-S", ".", "-B", "builddir",
+                    "-DSDK2_EN_QT=OFF", "-DSDK2_EN_APP=OFF", "-DSDK2_EN_SAMPLE=OFF",
+                    *std_cmake_args
+    system "cmake", "--build", "builddir"
+    system "cmake", "--install", "builddir"
+  end
+
+  test do
+    (testpath/"vatek_test.c").write <<~EOS
+      #include <vatek_sdk_device.h>
+      #include <stdio.h>
+      #include <stdlib.h>
+
+      int main()
+      {
+          hvatek_devices hdevices = NULL;
+          vatek_result devcount = vatek_device_list_enum(DEVICE_BUS_USB, service_transform, &hdevices);
+          if (is_vatek_success(devcount)) {
+              printf("passed\\n");
+              return EXIT_SUCCESS;
+          }
+          else {
+              printf("failed\\n");
+              return EXIT_FAILURE;
+          }
+      }
+    EOS
+    system ENV.cc, "vatek_test.c", "-I#{include}/vatek", "-L#{lib}", "-lvatek_core", "-o", "vatek_test"
+    assert_equal "passed", shell_output("./vatek_test").strip
+  end
+end
