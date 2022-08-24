ae5c910597ef91641a7b8ff115078bb6b5184a56
Adrian Ho <the.gromgit@gmail.com>
2022-08-25 07:55:25 +0800

    miller: update 6.4.0 bottle.

--- Formula/miller.rb
+++ Formula/miller.rb
@@ -1,14 +1,14 @@
 class Miller < Formula
   desc "Like sed, awk, cut, join & sort for name-indexed data such as CSV"
   homepage "https://github.com/johnkerl/miller"
-  url "https://github.com/johnkerl/miller/releases/download/v6.3.0/miller-6.3.0.tar.gz"
-  sha256 "6af8d2b8387c416090a9bf02129667920b86903e67561ccc4a5c9a3b33ea76cb"
+  url "https://github.com/johnkerl/miller/releases/download/v6.4.0/miller-6.4.0.tar.gz"
+  sha256 "20a8687a0c5b5fedf4fc3a794ef1cee7e9872e87476e1f24bde8de25799f8c51"
   license "BSD-2-Clause"
   head "https://github.com/johnkerl/miller.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/miller"
-    sha256 cellar: :any_skip_relocation, mojave: "d82427942ed2050deefff0c3ea763ab4f36c18c5b27525279561885364bc860a"
+    sha256 cellar: :any_skip_relocation, mojave: "4e5b1a7e5d58ae7daeb48ccfd64e17931a95c1b082cf10ce4dbf6607f8af744d"
   end
 
   depends_on "go" => :build
