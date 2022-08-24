673ed87d3029ac2d985f8f3a122561f19c582f3f
Adrian Ho <the.gromgit@gmail.com>
2022-08-25 06:37:46 +0800

    sord: update 0.16.12 bottle.

--- Formula/sord.rb
+++ Formula/sord.rb
@@ -1,8 +1,8 @@
 class Sord < Formula
   desc "C library for storing RDF data in memory"
   homepage "https://drobilla.net/software/sord.html"
-  url "https://download.drobilla.net/sord-0.16.10.tar.bz2"
-  sha256 "9c70b3fbbb0c5c7bf761ef66c3d5b939ab45ad063e055990f17f40f1f6f96572"
+  url "https://download.drobilla.net/sord-0.16.12.tar.xz"
+  sha256 "fde269893cb24b2ab7b75708d7a349c6e760c47a0d967aeca5b1c651294ff9f2"
   license "ISC"
 
   livecheck do
@@ -12,18 +12,21 @@ class Sord < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sord"
-    sha256 cellar: :any, mojave: "0c5496b0922b73414f5d049fb17c8139e350340d8a3766fad4da38da71c33e38"
+    sha256 cellar: :any, mojave: "212b9fdd7f19fb7102cfbb9d34eab79d53cae0fd312d48b40df09dc553451175"
   end
 
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@3.10" => :build
   depends_on "pcre"
   depends_on "serd"
 
   def install
-    system "python3", "./waf", "configure", "--prefix=#{prefix}"
-    system "python3", "./waf"
-    system "python3", "./waf", "install"
+    mkdir "build" do
+      system "meson", *std_meson_args, "-Dtests=disabled", ".."
+      system "ninja"
+      system "ninja", "install"
+    end
   end
 
   test do
