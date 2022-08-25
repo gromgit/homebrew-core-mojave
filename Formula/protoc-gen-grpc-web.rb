ed58684ac723c55ad42143b9c240046aab01c7c0
Adrian Ho <the.gromgit@gmail.com>
2022-08-26 05:26:30 +0800

    protoc-gen-grpc-web: update 1.3.1_1 bottle.

--- Formula/protoc-gen-grpc-web.rb
+++ Formula/protoc-gen-grpc-web.rb
@@ -6,23 +6,31 @@ class ProtocGenGrpcWeb < Formula
   url "https://github.com/grpc/grpc-web/archive/1.3.1.tar.gz"
   sha256 "d292df306b269ebf83fb53a349bbec61c07de4d628bd6a02d75ad3bd2f295574"
   license "Apache-2.0"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/protoc-gen-grpc-web"
-    sha256 cellar: :any, mojave: "b6d9cc6c5d3062ee40158e94bce61e231f18e633bf4e55a671620baca7d02c0d"
+    sha256 cellar: :any, mojave: "2959227abd58022add1f4f6850ad10d42cc50b8a6721f6559dc889c53ac07cfa"
   end
 
   depends_on "cmake" => :build
   depends_on "node" => :test
   depends_on "typescript" => :test
-  depends_on "protobuf"
+  depends_on "protobuf@3"
 
   def install
     bin.mkpath
     system "make", "install-plugin", "PREFIX=#{prefix}"
+
+    # Remove these two lines when this formula depends on unversioned `protobuf`.
+    libexec.install bin/"protoc-gen-grpc-web"
+    (bin/"protoc-gen-grpc-web").write_env_script libexec/"protoc-gen-grpc-web",
+                                                 PATH: "#{Formula["protobuf@3"].opt_bin}:${PATH}"
   end
 
   test do
+    ENV.prepend_path "PATH", Formula["protobuf@3"].opt_bin
+
     # First use the plugin to generate the files.
     testdata = <<~EOS
       syntax = "proto3";
@@ -42,8 +50,8 @@ class ProtocGenGrpcWeb < Formula
     EOS
     (testpath/"test.proto").write testdata
     system "protoc", "test.proto", "--plugin=#{bin}/protoc-gen-grpc-web",
-      "--js_out=import_style=commonjs:.",
-      "--grpc-web_out=import_style=typescript,mode=grpcwebtext:."
+                     "--js_out=import_style=commonjs:.",
+                     "--grpc-web_out=import_style=typescript,mode=grpcwebtext:."
 
     # Now see if we can import them.
     testts = <<~EOS
