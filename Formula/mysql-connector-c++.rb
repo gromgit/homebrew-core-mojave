77b709dc170185f02d95b93a2e5e35c6cd454bda
Adrian Ho <the.gromgit@gmail.com>
2022-08-25 10:10:14 +0800

    mysql-connector-c++: update 8.0.30 bottle.

--- Formula/mysql-connector-c++.rb
+++ Formula/mysql-connector-c++.rb
@@ -1,8 +1,8 @@
 class MysqlConnectorCxx < Formula
   desc "MySQL database connector for C++ applications"
   homepage "https://dev.mysql.com/downloads/connector/cpp/"
-  url "https://dev.mysql.com/get/Downloads/Connector-C++/mysql-connector-c++-8.0.29-src.tar.gz"
-  sha256 "9a6236a28bca33ae951d8ccaabb8ff51a188863e8599f9096f4ae0a1da19f87f"
+  url "https://dev.mysql.com/get/Downloads/Connector-C++/mysql-connector-c++-8.0.30-src.tar.gz"
+  sha256 "5b2ceebe3986fe6d6b0c6f29b6912cb3a1cabf998d2c4c4127452768de75ab0b"
   license "GPL-2.0-only" => { with: "Universal-FOSS-exception-1.0" }
 
   livecheck do
@@ -12,7 +12,7 @@ class MysqlConnectorCxx < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mysql-connector-c++"
-    sha256 cellar: :any, mojave: "2da8e6f16880523351db6a3f171d03f5c4a1ca0e1c5b72326fc1f4e7dce6ebd8"
+    sha256 cellar: :any, mojave: "6fc99073633ecad7fdca9ef9a4b6de02acb76040c5caed4f08e7ba92d01f1021"
   end
 
   depends_on "boost" => :build
