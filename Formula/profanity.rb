f64eae3a0443c2c1cae3c997900d76387c78fcee
Adrian Ho <the.gromgit@gmail.com>
2022-08-26 05:05:31 +0800

    profanity: update 0.12.1_2 bottle.

--- Formula/profanity.rb
+++ Formula/profanity.rb
@@ -4,15 +4,15 @@ class Profanity < Formula
   url "https://profanity-im.github.io/tarballs/profanity-0.12.1.tar.gz"
   sha256 "e344481e7bf3b16baf58a169d321b809c4700becffb70db6f1c39adc3fad306e"
   license "GPL-3.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/profanity"
-    sha256 mojave: "1b44b004eb704c533b51046ea0c7d1562699b0ace0e33cc8583623c47ffac295"
+    sha256 mojave: "07e66b7b89a65a2de563369dc24ddc05a00eab7aa74a898a87b6af97a3e30b24"
   end
 
   head do
-    url "https://github.com/profanity-im/profanity.git"
+    url "https://github.com/profanity-im/profanity.git", branch: "master"
 
     depends_on "autoconf" => :build
     depends_on "autoconf-archive" => :build
@@ -29,7 +29,7 @@ class Profanity < Formula
   depends_on "libsignal-protocol-c"
   depends_on "libstrophe"
   depends_on "openssl@1.1"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
   depends_on "readline"
 
   on_macos do
@@ -37,16 +37,15 @@ class Profanity < Formula
   end
 
   def install
-    ENV.prepend_path "PATH", Formula["python@3.9"].opt_libexec/"bin"
+    ENV.prepend_path "PATH", Formula["python@3.10"].opt_libexec/"bin"
 
     system "./bootstrap.sh" if build.head?
 
     # We need to pass `BREW` to `configure` to make sure it can be found inside the sandbox in non-default
     # prefixes. `configure` knows to check `/opt/homebrew` and `/usr/local`, but the sanitised build
     # environment will prevent any other `brew` installations from being found.
-    system "./configure", "--disable-dependency-tracking",
+    system "./configure", *std_configure_args,
                           "--disable-silent-rules",
-                          "--prefix=#{prefix}",
                           "BREW=#{HOMEBREW_BREW_FILE}"
     system "make", "install"
   end
