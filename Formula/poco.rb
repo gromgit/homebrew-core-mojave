26c4d93ad914b853f554d4e2dfc0731e5a05b289
Adrian Ho <the.gromgit@gmail.com>
2022-08-26 04:00:54 +0800

    poco: update 1.12.2 bottle.

--- Formula/poco.rb
+++ Formula/poco.rb
@@ -1,8 +1,8 @@
 class Poco < Formula
   desc "C++ class libraries for building network and internet-based applications"
   homepage "https://pocoproject.org/"
-  url "https://pocoproject.org/releases/poco-1.12.1/poco-1.12.1-all.tar.gz"
-  sha256 "1a32814b7ac2d7ea68f8519d3bad173e8be0c8306a755130388dc62d70330844"
+  url "https://pocoproject.org/releases/poco-1.12.2/poco-1.12.2-all.tar.gz"
+  sha256 "642faec888acb619954d870f89c12a834052813306ff8d8a071becb1eee708aa"
   license "BSL-1.0"
   head "https://github.com/pocoproject/poco.git", branch: "master"
 
@@ -13,11 +13,11 @@ class Poco < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/poco"
-    sha256 cellar: :any, mojave: "563423bfa70b2790b09e88933d99b46135583f0104202a643372fa7a88a3b603"
+    sha256 cellar: :any, mojave: "b704b0bf65854ac474e8ea494f1f27e7c3b94253a29a45dcf531bf586c217fb6"
   end
 
   depends_on "cmake" => :build
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   depends_on "pcre2"
 
   uses_from_macos "expat"
