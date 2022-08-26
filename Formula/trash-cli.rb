37f028478a454330f9f4db67b9d9c321d2659a52
Adrian Ho <the.gromgit@gmail.com>
2022-08-26 22:58:26 +0800

    trash-cli: update 0.22.8.21.16 bottle.

--- Formula/trash-cli.rb
+++ Formula/trash-cli.rb
@@ -3,14 +3,14 @@ class TrashCli < Formula
 
   desc "Command-line interface to the freedesktop.org trashcan"
   homepage "https://github.com/andreafrancia/trash-cli"
-  url "https://files.pythonhosted.org/packages/36/d2/6415cda6cdd81ee0eb07b357e96a03c96a78af324d7eb2c52ff51d080210/trash-cli-0.22.4.16.tar.gz"
-  sha256 "ec60ff1a038402f4b1e7360b32707bc36dc275fa32e512bb81274c0375d20003"
+  url "https://files.pythonhosted.org/packages/c4/17/3d153e9ed159c8ab0e01fb3d991e7ac144c6a995d788ad96085c3ee7a093/trash-cli-0.22.8.21.16.tar.gz"
+  sha256 "ef77198e79ac02608872c027de91c0e18353f8d2bf1b7d11a1d8bde66fa5669e"
   license "GPL-2.0-or-later"
   head "https://github.com/andreafrancia/trash-cli.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/trash-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "07085931e9c7286928e0446327d38deb06511511834051aa4dbe87ea60d1f17e"
+    sha256 cellar: :any_skip_relocation, mojave: "da6b4976c80fc6eaf62aa80085dc3597a8cf293186defe6905d5787982f6acd2"
   end
 
   depends_on "python@3.10"
@@ -19,8 +19,8 @@ class TrashCli < Formula
   conflicts_with "trash", because: "both install a `trash` binary"
 
   resource "psutil" do
-    url "https://files.pythonhosted.org/packages/47/b6/ea8a7728f096a597f0032564e8013b705aa992a0990becd773dcc4d7b4a7/psutil-5.9.0.tar.gz"
-    sha256 "869842dbd66bb80c3217158e629d6fceaecc3a3166d3d1faee515b05dd26ca25"
+    url "https://files.pythonhosted.org/packages/d6/de/0999ea2562b96d7165812606b18f7169307b60cd378bc29cf3673322c7e9/psutil-5.9.1.tar.gz"
+    sha256 "57f1819b5d9e95cdfb0c881a8a5b7d542ed0b7c522d575706a80bedc848c8954"
   end
 
   def install
