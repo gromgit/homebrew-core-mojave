b6e62e5e635806f288b66ceacd89b0f19de79f96
Adrian Ho <the.gromgit@gmail.com>
2022-08-27 00:38:36 +0800

    vermin: update 1.4.2 bottle.

--- Formula/vermin.rb
+++ Formula/vermin.rb
@@ -3,14 +3,14 @@ class Vermin < Formula
 
   desc "Concurrently detect the minimum Python versions needed to run code"
   homepage "https://github.com/netromdk/vermin"
-  url "https://github.com/netromdk/vermin/archive/v1.4.1.tar.gz"
-  sha256 "ee69d5e84f0d446e0d6574ec60c428798de6e6c8d055589f65ac02f074a7da25"
+  url "https://github.com/netromdk/vermin/archive/v1.4.2.tar.gz"
+  sha256 "c9a69420b610bfb25d5a2abd7da6edf0ae4329481a857ef6c5d71f602ed5c63d"
   license "MIT"
   head "https://github.com/netromdk/vermin.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/vermin"
-    sha256 cellar: :any_skip_relocation, mojave: "e6342067906c2748f93a63baf1ecfbdd7f65a4140875f333ba456dcb03d19fad"
+    sha256 cellar: :any_skip_relocation, mojave: "b3010a816194fab5d82a519b82855a5a99e161355483c98361fc27b9bddd9f21"
   end
 
   depends_on "python@3.10"
@@ -20,7 +20,7 @@ class Vermin < Formula
   end
 
   test do
-    path = libexec/"lib/python3.10/site-packages/vermin"
+    path = libexec/Language::Python.site_packages("python3.10")/"vermin"
     assert_match "Minimum required versions: 2.7, 3.0", shell_output("#{bin}/vermin #{path}")
   end
 end
