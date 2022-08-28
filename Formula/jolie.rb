86d2b5a96e49f3a47f8d5efc64415bced56ea2b7
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-28 22:41:54 +0800

    jolie: no rebuilding required

--- Formula/jolie.rb
+++ Formula/jolie.rb
@@ -1,12 +1,12 @@
 class Jolie < Formula
   desc "Service-oriented programming language"
   homepage "https://www.jolie-lang.org/"
-  url "https://github.com/jolie/jolie/releases/download/v1.10.7/jolie-1.10.7.jar"
-  sha256 "22e30ba88ddea7197843e868a2115edbfce6a871d7dc58ec35a34c709579fa0a"
+  url "https://github.com/jolie/jolie/releases/download/v1.10.13/jolie-1.10.13.jar"
+  sha256 "475c32552eaacb0de1f50e109f52e713610a99538b71abfc9167755e41c022a1"
   license "LGPL-2.1-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "56f8f9b008a733c98119fd49c7e966510cc3c502496000d91d0a6d1aa9f10585"
+    sha256 cellar: :any_skip_relocation, all: "3f2b2870d5769acb0d8a1ab6a3b7f24e4c3a654d55567627a708e641256e8206"
   end
 
   depends_on "openjdk"
