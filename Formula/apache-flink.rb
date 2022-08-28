18e63a8cad5025b2201763fd6723e7ab10a03d02
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-28 22:41:39 +0800

    apache-flink: no rebuilding required

--- Formula/apache-flink.rb
+++ Formula/apache-flink.rb
@@ -1,15 +1,15 @@
 class ApacheFlink < Formula
   desc "Scalable batch and stream data processing"
   homepage "https://flink.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.15.1/flink-1.15.1-bin-scala_2.12.tgz"
-  mirror "https://archive.apache.org/dist/flink/flink-1.15.1/flink-1.15.1-bin-scala_2.12.tgz"
-  version "1.15.1"
-  sha256 "9213f9e4bb087cbc6d3c9ab4ac4215fab4199bbfabf0a09f3c138f216d854c5b"
+  url "https://www.apache.org/dyn/closer.lua?path=flink/flink-1.15.2/flink-1.15.2-bin-scala_2.12.tgz"
+  mirror "https://archive.apache.org/dist/flink/flink-1.15.2/flink-1.15.2-bin-scala_2.12.tgz"
+  version "1.15.2"
+  sha256 "32bb0a119b64827e05f63f742a2e68e1cfd8477cf0e93b7aea1bb80f8b1a2e15"
   license "Apache-2.0"
   head "https://github.com/apache/flink.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "9a062793325af350899b8c96ed6302bb911019100a080c96e63074e73970769c"
+    sha256 cellar: :any_skip_relocation, all: "05d7ed169e730ead5c004616b2c7e1f8299db86bc5c25072a97c9bda395dbf5d"
   end
 
   depends_on "openjdk@11"
