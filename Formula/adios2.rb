0c4914227c3fbfa65f467dd12a7876ba95124826
Adrian Ho <the.gromgit@gmail.com>
2022-08-29 01:58:12 +0800

    adios2: update 2.8.3 bottle.

--- Formula/adios2.rb
+++ Formula/adios2.rb
@@ -1,10 +1,9 @@
 class Adios2 < Formula
   desc "Next generation of ADIOS developed in the Exascale Computing Program"
   homepage "https://adios2.readthedocs.io"
-  url "https://github.com/ornladios/ADIOS2/archive/v2.8.2.tar.gz"
-  sha256 "9909f6409dc44b2c28c1fda0042dab4b711f25ec3277ef0cb6ffc40f5483910d"
+  url "https://github.com/ornladios/ADIOS2/archive/v2.8.3.tar.gz"
+  sha256 "4906ab1899721c41dd918dddb039ba2848a1fb0cf84f3a563a1179b9d6ee0d9f"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/ornladios/ADIOS2.git", branch: "master"
 
   livecheck do
@@ -14,30 +13,43 @@ class Adios2 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/adios2"
-    sha256 mojave: "6aff827c52f7d8874a87ac83f4e0e903a09cb0bd7eb28b3c9336930f79906ea3"
+    sha256 mojave: "65b209a930c165e5ddb709fece31bd346b1758b69a5e76fcb9914e944a2810e5"
   end
 
   depends_on "cmake" => :build
   depends_on "gcc" => :build
+  depends_on "nlohmann-json" => :build
   depends_on "c-blosc"
   depends_on "libfabric"
   depends_on "libpng"
   depends_on "mpi4py"
   depends_on "numpy"
   depends_on "open-mpi"
+  depends_on "pugixml"
+  depends_on "pybind11"
   depends_on "python@3.10"
+  depends_on "yaml-cpp"
   depends_on "zeromq"
 
   uses_from_macos "bzip2"
 
+  def python3
+    "python3.10"
+  end
+
   def install
+    # Fix for newer CMake
+    # https://github.com/ornladios/ADIOS2/issues/3309
+    inreplace "CMakeLists.txt", "cmake_minimum_required(VERSION 3.12)",
+                                "cmake_minimum_required(VERSION 3.12...3.23)"
+
     # fix `include/adios2/common/ADIOSConfig.h` file audit failure
     inreplace "source/adios2/common/ADIOSConfig.h.in" do |s|
       s.gsub! ": @CMAKE_C_COMPILER@", ": #{ENV.cc}"
       s.gsub! ": @CMAKE_CXX_COMPILER@", ": #{ENV.cxx}"
     end
 
-    args = std_cmake_args + %W[
+    args = %W[
       -DADIOS2_USE_Blosc=ON
       -DADIOS2_USE_BZip2=ON
       -DADIOS2_USE_DataSpaces=OFF
@@ -56,12 +68,14 @@ class Adios2 < Formula
       -DCMAKE_DISABLE_FIND_PACKAGE_FLEX=TRUE
       -DCMAKE_DISABLE_FIND_PACKAGE_LibFFI=TRUE
       -DCMAKE_DISABLE_FIND_PACKAGE_NVSTREAM=TRUE
-      -DPython_EXECUTABLE=#{which("python3")}
-      -DCMAKE_INSTALL_PYTHONDIR=#{prefix/Language::Python.site_packages("python3")}
+      -DPython_EXECUTABLE=#{which(python3)}
+      -DCMAKE_INSTALL_PYTHONDIR=#{prefix/Language::Python.site_packages(python3)}
       -DADIOS2_BUILD_TESTING=OFF
       -DADIOS2_BUILD_EXAMPLES=OFF
+      -DADIOS2_USE_EXTERNAL_DEPENDENCIES=ON
     ]
-    system "cmake", "-S", ".", "-B", "build", *args
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
 
@@ -70,16 +84,13 @@ class Adios2 < Formula
   end
 
   test do
-    adios2_config_flags = `adios2-config --cxx`.chomp.split
-    system "mpic++",
-           (pkgshare/"test/helloBPWriter.cpp"),
-           *adios2_config_flags
+    adios2_config_flags = Utils.safe_popen_read(bin/"adios2-config", "--cxx").chomp.split
+    system "mpic++", pkgshare/"test/helloBPWriter.cpp", *adios2_config_flags
     system "./a.out"
     assert_predicate testpath/"myVector_cpp.bp", :exist?
 
-    python = Formula["python@3.10"].opt_bin/"python3"
-    system python, "-c", "import adios2"
-    system python, (pkgshare/"test/helloBPWriter.py")
+    system python3, "-c", "import adios2"
+    system python3, pkgshare/"test/helloBPWriter.py"
     assert_predicate testpath/"npArray.bp", :exist?
   end
 end
