3a975b6d563e53e85044341fc4005b10c36c0a3e
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-28 22:41:53 +0800

    jena: no rebuilding required

--- Formula/jena.rb
+++ Formula/jena.rb
@@ -1,13 +1,13 @@
 class Jena < Formula
   desc "Framework for building semantic web and linked data apps"
   homepage "https://jena.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-4.5.0.tar.gz"
-  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-4.5.0.tar.gz"
-  sha256 "fca1e384c367a9a7361c6ff2bc725ad0cc1d45e5fa5ee7b1a2368c81115d696a"
+  url "https://www.apache.org/dyn/closer.lua?path=jena/binaries/apache-jena-4.6.0.tar.gz"
+  mirror "https://archive.apache.org/dist/jena/binaries/apache-jena-4.6.0.tar.gz"
+  sha256 "d5f365e8ef4554bcc1c7853ee04ef6198eb450631244574a37122b3dd71bfda0"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "cd0788e1b3c372b40129f809f83b9030253b6ba369d49faeca017a47ff939ca4"
+    sha256 cellar: :any_skip_relocation, all: "097eefc19c380a9217a6b158ad12ef52eea3cb0e9931660f90228887c81cb949"
   end
 
   depends_on "openjdk"
