653567740e21efcbe8bb6f5206d03b9947d78ec9
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-28 22:41:57 +0800

    libnsl:

--- Formula/libnsl.rb
+++ Formula/libnsl.rb
@@ -1,46 +1,60 @@
 class Libnsl < Formula
   desc "Public client interface for NIS(YP) and NIS+"
   homepage "https://github.com/thkukuk/libnsl"
-  url "https://github.com/thkukuk/libnsl/archive/v2.0.0.tar.gz"
-  sha256 "eb37be57c1cf650b3a8a4fc7cd66c8b3dfc06215b41956a16325a9388171bc40"
+  url "https://github.com/thkukuk/libnsl/releases/download/v2.0.0/libnsl-2.0.0.tar.xz"
+  sha256 "2da075ef1893ebdfc5f074f83ac811873dc06fd5c62bc9a4729fd2e27a40341a"
   license "LGPL-2.1-or-later"
+  revision 1
 
-  keg_only "it conflicts with glibc"
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ed70b285939e2ab21ba53d122ce2d4beab4cd0f9c86925c3d3a2cfb1b0eeecb3"
+  end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
   depends_on "gettext" => :build
-  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "libtirpc"
   depends_on :linux
 
-  uses_from_macos "m4" => :build
+  link_overwrite "include/rpcsvc"
+  link_overwrite "lib/libnsl.a"
+  link_overwrite "lib/libnsl.so"
 
   def install
-    inreplace "po/Makefile.in.in" do |s|
-      s.gsub!(/GETTEXT_MACRO_VERSION =.*/,
-        "GETTEXT_MACRO_VERSION = #{Formula["gettext"].version.to_s[/(\d\.\d+)/, 1]}")
-    end
-    system "./autogen.sh"
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    system "./configure", *std_configure_args,
+                          "--disable-silent-rules"
     system "make"
     system "make", "install"
   end
 
   test do
-    (testpath/"test.c").write <<~EOS
-      #include <rpcsvc/nis.h>
+    (testpath/"test.c").write <<~'EOS'
+      #include <rpcsvc/ypclnt.h>
 
       int main(int argc, char *argv[]) {
-          if(NIS_PK_NONE != 0)
-              return 1;
+         char *domain;
+         switch (yp_get_default_domain(&domain)) {
+         case YPERR_SUCCESS:
+           printf("Domain: %s\n", domain);
+           return 0;
+         case YPERR_NODOM:
+           printf("No domain\n");
+           return 0;
+         default:
+           return 1;
+         }
       }
     EOS
 
     system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lnsl", "-o", "test"
-    system "./test"
+
+    domain = Utils.popen_read("ypdomainname").chomp
+    domain_exists = $CHILD_STATUS.success?
+
+    output = shell_output("./test").chomp
+    if domain_exists
+      assert_equal "Domain: #{domain}", output
+    else
+      assert_equal "No domain", output
+    end
   end
 end
