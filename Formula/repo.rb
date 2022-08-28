3a618bdc65ff7359c2ad41c2e53de5519e711696
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-28 22:42:07 +0800

    repo: no rebuilding required

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -4,22 +4,25 @@ class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
   url "https://gerrit.googlesource.com/git-repo.git",
-      tag:      "v2.28",
-      revision: "a8cf575d68e7e211292d967f4a12cf609a028b20"
+      tag:      "v2.29.1",
+      revision: "4112c07688d0e0e568478e9f42be349bdd511d45"
   license "Apache-2.0"
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "1883ffa6c441a80dbf597a53d1dc62e7436279d1bdaf171d7101ec43eb1b57fa"
+    sha256 cellar: :any_skip_relocation, all: "5928aa907b8bd66c9cf626c1dd2cb02ac36a999e20a465883e1285bcdc003163"
   end
 
-  depends_on "python@3.10"
+  uses_from_macos "python"
 
   def install
     bin.install "repo"
-    rewrite_shebang detected_python_shebang, bin/"repo"
-
     doc.install (buildpath/"docs").children
+
+    # Need Catalina+ for `python3`.
+    return if OS.mac? && MacOS.version < :catalina
+
+    rewrite_shebang detected_python_shebang(use_python_from_path: true), bin/"repo"
   end
 
   test do
