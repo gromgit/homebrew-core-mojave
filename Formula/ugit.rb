fe80b8e05abff3bb94aeb4e97567fcbacb79f03b
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-28 22:42:12 +0800

    ugit: no rebuilding required

--- Formula/ugit.rb
+++ Formula/ugit.rb
@@ -1,12 +1,12 @@
 class Ugit < Formula
   desc "Undo git commands. Your damage control git buddy"
   homepage "https://bhupesh.me/undo-your-last-git-mistake-with-ugit/"
-  url "https://github.com/Bhupesh-V/ugit/archive/refs/tags/v5.2.tar.gz"
-  sha256 "0eb27258cec03ee569bf564016a277548774cf9460eb2c1c63f675b80c7a1fb9"
+  url "https://github.com/Bhupesh-V/ugit/archive/refs/tags/v5.3.tar.gz"
+  sha256 "c83183ac91becc2ee281da5d8e67d2a25b7bd750f437e5d3f0214051c7162aa9"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "f6f5e1b00f3a4c3228dfb5615b5a23c1f2be8b95d84a2aad6b1b42fe903d9ab2"
+    sha256 cellar: :any_skip_relocation, all: "bbca08614a3794ba4002544c8ca2c7fe5d28e66282b580a7f69b957071e24615"
   end
 
   depends_on "bash"
