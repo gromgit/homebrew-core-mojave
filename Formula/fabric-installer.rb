1307ea91215ff0bccffe16c3e7fc842c39c319f6
Adrian Ho <aho+vm-mojave@03s.net>
2022-08-28 22:41:45 +0800

    fabric-installer: no rebuilding required

--- Formula/fabric-installer.rb
+++ Formula/fabric-installer.rb
@@ -1,8 +1,8 @@
 class FabricInstaller < Formula
   desc "Installer for Fabric for the vanilla launcher"
   homepage "https://fabricmc.net/"
-  url "https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.11.0/fabric-installer-0.11.0.jar"
-  sha256 "68ba42ea4f82ba6d107dd21ad6c5174b80412854ee74919b7234b52d214fd10a"
+  url "https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.11.1/fabric-installer-0.11.1.jar"
+  sha256 "7917f9fa14be6da6ef3cdb7fe6bf3b63a593a0eb873bd627e981a7e3988cd73a"
   license "Apache-2.0"
 
   # The first-party download page (https://fabricmc.net/use/) uses JavaScript
@@ -13,7 +13,7 @@ class FabricInstaller < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "49c790c1f96aa072ccbcbd70b4b366434069b7102b147e2b10835f709261bee2"
+    sha256 cellar: :any_skip_relocation, all: "9428f27d95d8ae7e4f552f3ba6a426add1c3184fa127f219b8bd19385881b61a"
   end
 
   depends_on "openjdk"
