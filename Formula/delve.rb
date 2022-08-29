fa42681a4aa902ede533446d693ca840dd081b48
Adrian Ho <the.gromgit@gmail.com>
2022-08-30 02:27:50 +0800

    delve: update 1.9.1 bottle.

--- Formula/delve.rb
+++ Formula/delve.rb
@@ -1,13 +1,13 @@
 class Delve < Formula
   desc "Debugger for the Go programming language"
   homepage "https://github.com/go-delve/delve"
-  url "https://github.com/go-delve/delve/archive/v1.9.0.tar.gz"
-  sha256 "daec5909ce6e44b7b2c49eea90b4aa964d2ff96c1e875b09891a26546f9ae871"
+  url "https://github.com/go-delve/delve/archive/v1.9.1.tar.gz"
+  sha256 "d8d119e74ae47799baa60c08faf2c2872fefce9264b36475ddac8e3a7efc6727"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/delve"
-    sha256 cellar: :any_skip_relocation, mojave: "46854559f4c60dadf6bc985d11128e32bd852b3e252bb1b3d4c627d76209c393"
+    sha256 cellar: :any_skip_relocation, mojave: "5ccd949bc43f53bc0969cc47df43ed50cccf363ee3dd150b63c8143210d678b8"
   end
 
   depends_on "go" => :build
