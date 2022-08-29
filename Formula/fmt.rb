fda344a5031296b082e731585465340781de0b78
Adrian Ho <the.gromgit@gmail.com>
2022-08-29 19:00:54 +0800

    fmt: update 9.1.0 bottle.

--- Formula/fmt.rb
+++ Formula/fmt.rb
@@ -1,25 +1,17 @@
 class Fmt < Formula
   desc "Open-source formatting library for C++"
   homepage "https://fmt.dev/"
-  url "https://github.com/fmtlib/fmt/archive/8.1.1.tar.gz"
-  sha256 "3d794d3cf67633b34b2771eb9f073bde87e846e0d395d254df7b211ef1ec7346"
+  url "https://github.com/fmtlib/fmt/archive/9.1.0.tar.gz"
+  sha256 "5dea48d1fcddc3ec571ce2058e13910a0d4a6bab4cc09a809d8b1dd1c88ae6f2"
   license "MIT"
-  revision 1
   head "https://github.com/fmtlib/fmt.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fmt"
-    sha256 cellar: :any, mojave: "724b9bedd41070b83aab669150b5d76ecef3c5f4629f67bd39a3030d47fac431"
+    sha256 cellar: :any, mojave: "20dc7e7dcbff98b1652ac08daf96fd788c0c242517bc16065bc0c3d00f8a5b62"
   end
 
   depends_on "cmake" => :build
-
-  # Fix Watchman build.
-  # https://github.com/fmtlib/fmt/issues/2717
-  patch do
-    url "https://github.com/fmtlib/fmt/commit/8f8a1a02d5c5cb967d240feee3ffac00d66f22a2.patch?full_index=1"
-    sha256 "ac5d7a8f9eabd40e34f21b1e0034fbc4147008f13b7bf2314131239fb3a7bdab"
-  end
 
   def install
     system "cmake", ".", "-DBUILD_SHARED_LIBS=TRUE", *std_cmake_args
