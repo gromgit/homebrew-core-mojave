15d2a37efa0758d50afc2dadca7ce17d334d68bc
Adrian Ho <the.gromgit@gmail.com>
2022-08-30 07:32:21 +0800

    glslang: update 11.11.0 bottle.

--- Formula/glslang.rb
+++ Formula/glslang.rb
@@ -1,8 +1,8 @@
 class Glslang < Formula
   desc "OpenGL and OpenGL ES reference compiler for shading languages"
   homepage "https://www.khronos.org/opengles/sdk/tools/Reference-Compiler/"
-  url "https://github.com/KhronosGroup/glslang/archive/11.10.0.tar.gz"
-  sha256 "8ffc19c435232d09299dd2c91e247292b3508c1b826a3497c60682e4bbf2d602"
+  url "https://github.com/KhronosGroup/glslang/archive/11.11.0.tar.gz"
+  sha256 "26c216c3062512c018cbdd752224b8dad703b7e5bb90bf338ba2dbb5d4f11438"
   license all_of: ["BSD-3-Clause", "GPL-3.0-or-later", "MIT", "Apache-2.0"]
   head "https://github.com/KhronosGroup/glslang.git"
 
@@ -13,7 +13,7 @@ class Glslang < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/glslang"
-    sha256 cellar: :any_skip_relocation, mojave: "b26f6102e5c4773cf2cebe1ed5414140e2c639a5a34c82e32c9d4d059f5cb28e"
+    sha256 cellar: :any_skip_relocation, mojave: "0c4d638d4d93a26da4f74a3af3ec3d930f71cacfcb061e77f5275dbfb9c2bbba"
   end
 
   depends_on "cmake" => :build
