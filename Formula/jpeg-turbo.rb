cad57c9cc83caf1adce8e8ad3025ef90d6bc78c7
Adrian Ho <the.gromgit@gmail.com>
2022-08-29 12:12:38 +0800

    jpeg-turbo: update 2.1.4 bottle.

--- Formula/jpeg-turbo.rb
+++ Formula/jpeg-turbo.rb
@@ -1,34 +1,43 @@
 class JpegTurbo < Formula
   desc "JPEG image codec that aids compression and decompression"
   homepage "https://www.libjpeg-turbo.org/"
-  url "https://downloads.sourceforge.net/project/libjpeg-turbo/2.1.3/libjpeg-turbo-2.1.3.tar.gz"
-  sha256 "467b310903832b033fe56cd37720d1b73a6a3bd0171dbf6ff0b620385f4f76d0"
+  url "https://downloads.sourceforge.net/project/libjpeg-turbo/2.1.4/libjpeg-turbo-2.1.4.tar.gz"
+  sha256 "d3ed26a1131a13686dfca4935e520eb7c90ae76fbc45d98bb50a8dc86230342b"
   license "IJG"
   head "https://github.com/libjpeg-turbo/libjpeg-turbo.git", branch: "main"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jpeg-turbo"
-    rebuild 1
-    sha256 cellar: :any, mojave: "16cd9eea3a283412316beeb8fd98b6d4241623f649bed173ce9a7db419bb1f63"
+    sha256 cellar: :any, mojave: "f470843e572f23b568f24651f1a560e36a4b08067ec9f19fcd84cda9aabd6564"
   end
 
-  keg_only "libjpeg-turbo is not linked to prevent conflicts with the standard libjpeg"
-
   depends_on "cmake" => :build
-  depends_on "nasm" => :build
+
+  on_intel do
+    # Required only for x86 SIMD extensions.
+    depends_on "nasm" => :build
+  end
+
+  # These conflict with `jpeg`, which is now keg-only.
+  link_overwrite "bin/cjpeg", "bin/djpeg", "bin/jpegtran", "bin/rdjpgcom", "bin/wrjpgcom"
+  link_overwrite "include/jconfig.h", "include/jerror.h", "include/jmorecfg.h", "include/jpeglib.h"
+  link_overwrite "lib/libjpeg.dylib", "lib/libjpeg.so", "lib/libjpeg.a", "lib/pkgconfig/libjpeg.pc"
+  link_overwrite "share/man/man1/cjpeg.1", "share/man/man1/djpeg.1", "share/man/man1/jpegtran.1",
+                 "share/man/man1/rdjpgcom.1", "share/man/man1/wrjpgcom.1"
 
   def install
-    args = std_cmake_args - %w[-DCMAKE_INSTALL_LIBDIR=lib]
-    system "cmake", ".", "-DWITH_JPEG8=1",
-                         "-DCMAKE_INSTALL_LIBDIR=#{lib}",
-                         *args
-    system "make"
-    system "make", "test"
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", "-DWITH_JPEG8=1", *std_cmake_args(install_libdir: lib)
+    system "cmake", "--build", "build"
+    system "cmake", "--build", "build", "--target", "test"
+    system "cmake", "--install", "build"
   end
 
   test do
-    system "#{bin}/jpegtran", "-crop", "1x1", "-transpose", "-perfect",
-                              "-outfile", "out.jpg", test_fixtures("test.jpg")
+    system bin/"jpegtran", "-crop", "1x1",
+                           "-transpose",
+                           "-perfect",
+                           "-outfile", "out.jpg",
+                           test_fixtures("test.jpg")
+    assert_predicate testpath/"out.jpg", :exist?
   end
 end
