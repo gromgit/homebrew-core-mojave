a42467b5a0c41ab22499016a086a9b63b7c17b65
Adrian Ho <the.gromgit@gmail.com>
2022-08-30 04:39:37 +0800

    fennel: update 1.2.0 bottle.

--- Formula/fennel.rb
+++ Formula/fennel.rb
@@ -1,13 +1,13 @@
 class Fennel < Formula
   desc "Lua Lisp Language"
   homepage "https://fennel-lang.org"
-  url "https://github.com/bakpakin/Fennel/archive/1.1.0.tar.gz"
-  sha256 "14873fb319ace8707a075bc4696d3691f5045686e5738822bdf4cc014d14b4b8"
+  url "https://github.com/bakpakin/Fennel/archive/1.2.0.tar.gz"
+  sha256 "a0c9b1d23d9d9714afe93542c5314fad8e1771bf8b616d0decfeabe88318313e"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fennel"
-    sha256 cellar: :any_skip_relocation, mojave: "4d5e9076d4a8764a12fb67775f802ca01857f76e6a8ff343286be228d3dfd179"
+    sha256 cellar: :any_skip_relocation, mojave: "9f26ea712b05fce128ba65049e878a657f66ac2091dc62a01bcc82a82f557187"
   end
 
   depends_on "lua"
