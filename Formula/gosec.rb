253c1be1f8bf7b0742f0230b37fb8c7411e3519d
Adrian Ho <the.gromgit@gmail.com>
2022-08-30 07:51:50 +0800

    gosec: update 2.13.1 bottle.

--- Formula/gosec.rb
+++ Formula/gosec.rb
@@ -1,14 +1,14 @@
 class Gosec < Formula
   desc "Golang security checker"
   homepage "https://securego.io/"
-  url "https://github.com/securego/gosec/archive/v2.13.0.tar.gz"
-  sha256 "f78c9694f16ab1f64485c803c17cf0b9faefbe956f8d2a49108a95419696a3b9"
+  url "https://github.com/securego/gosec/archive/v2.13.1.tar.gz"
+  sha256 "72e107b5c1f71df8e8dedacbd3650a9c5d931bc8e8be1c7ead51ce379a6ab5ae"
   license "Apache-2.0"
   head "https://github.com/securego/gosec.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gosec"
-    sha256 cellar: :any_skip_relocation, mojave: "1c635cb83200b80bce30c4f289d975b27a333a59c19a857c3324ea3aad88eac7"
+    sha256 cellar: :any_skip_relocation, mojave: "7f853317f28e29be8a444547c4fa723525393f0844535b2d0f7fee9ba97751c6"
   end
 
   depends_on "go"
