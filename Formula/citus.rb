6a1836afcd8510e6f5bc5d71b3ec28cc78dc8f7a
Adrian Ho <the.gromgit@gmail.com>
2022-08-29 23:34:37 +0800

    citus: update 11.0.6_1 bottle.

--- Formula/citus.rb
+++ Formula/citus.rb
@@ -1,39 +1,64 @@
 class Citus < Formula
   desc "PostgreSQL-based distributed RDBMS"
   homepage "https://www.citusdata.com"
-  url "https://github.com/citusdata/citus/archive/v11.0.5.tar.gz"
-  sha256 "6f1e57de9beac4c08329e74b58d8c77ebde67f418a90e5b15f4472f1ad3f0688"
+  url "https://github.com/citusdata/citus/archive/v11.0.6.tar.gz"
+  sha256 "f3f2deb3e7f31844f4cc3bb0a311b52a4179cabe08c72b409819fa6f6e72f5f4"
   license "AGPL-3.0-only"
+  revision 1
   head "https://github.com/citusdata/citus.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/citus"
-    sha256 cellar: :any, mojave: "7fcc4a91eabc706d6fa4e83fee7582f9d9b9841dd5dfd8633096cbb481c10723"
+    sha256 cellar: :any, mojave: "c62efd79f93db94426689410f3e36cd1593f3d63836fe38aad4bc69d26b490f3"
   end
 
   depends_on "lz4"
-  depends_on "postgresql"
+  depends_on "postgresql@14"
   depends_on "readline"
   depends_on "zstd"
 
   uses_from_macos "curl"
 
+  def postgresql
+    Formula["postgresql@14"]
+  end
+
   def install
-    ENV["PG_CONFIG"] = Formula["postgresql"].opt_bin/"pg_config"
+    ENV["PG_CONFIG"] = postgresql.opt_bin/"pg_config"
 
     system "./configure"
-
     # workaround for https://github.com/Homebrew/legacy-homebrew/issues/49948
-    system "make", "libpq=-L#{Formula["postgresql"].opt_lib} -lpq"
+    system "make", "libpq=-L#{postgresql.opt_lib} -lpq"
 
     # Use stage directory to prevent installing to pg_config-defined dirs,
     # which would not be within this package's Cellar.
     mkdir "stage"
     system "make", "install", "DESTDIR=#{buildpath}/stage"
 
-    path = File.join("stage", HOMEBREW_PREFIX)
-    lib.install (buildpath/path/"lib").children
-    include.install (buildpath/path/"include").children
-    (share/"postgresql/extension").install (buildpath/path/"share/postgresql/extension").children
+    stage_path = File.join("stage", HOMEBREW_PREFIX)
+    lib.install (buildpath/stage_path/"lib").children
+    include.install (buildpath/stage_path/"include").children
+    share.install (buildpath/stage_path/"share").children
+
+    bin.install (buildpath/File.join("stage", postgresql.bin.realpath)).children
+  end
+
+  test do
+    pg_ctl = postgresql.opt_bin/"pg_ctl"
+    psql = postgresql.opt_bin/"psql"
+    port = free_port
+
+    system pg_ctl, "initdb", "-D", testpath/"test"
+    (testpath/"test/postgresql.conf").write <<~EOS, mode: "a+"
+
+      shared_preload_libraries = 'citus'
+      port = #{port}
+    EOS
+    system pg_ctl, "start", "-D", testpath/"test", "-l", testpath/"log"
+    begin
+      system psql, "-p", port.to_s, "-c", "CREATE EXTENSION \"citus\";", "postgres"
+    ensure
+      system pg_ctl, "stop", "-D", testpath/"test"
+    end
   end
 end
