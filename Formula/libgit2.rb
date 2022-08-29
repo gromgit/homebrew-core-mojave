c56f66e505fb67e9e9e64caff932e74dd060164c
Adrian Ho <the.gromgit@gmail.com>
2022-08-29 19:57:44 +0800

    libgit2: update 1.5.0 bottle.

--- Formula/libgit2.rb
+++ Formula/libgit2.rb
@@ -1,8 +1,8 @@
 class Libgit2 < Formula
   desc "C library of Git core methods that is re-entrant and linkable"
   homepage "https://libgit2.github.com/"
-  url "https://github.com/libgit2/libgit2/archive/v1.4.4.tar.gz"
-  sha256 "e9923e9916a32f54c661d55d79c28fa304cb23617639e68bff9f94d3e18f2d4b"
+  url "https://github.com/libgit2/libgit2/archive/v1.5.0.tar.gz"
+  sha256 "8de872a0f201b33d9522b817c92e14edb4efad18dae95cf156cf240b2efff93e"
   license "GPL-2.0-only"
   head "https://github.com/libgit2/libgit2.git", branch: "main"
 
@@ -13,7 +13,7 @@ class Libgit2 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libgit2"
-    sha256 cellar: :any, mojave: "96eb4afebb6702ac8b43dde7d3c0a63c7d4178d00453ec76aa5619c533df7e14"
+    sha256 cellar: :any, mojave: "d27408c8a174815dea6832afcbc4d02df5206ec9647e430b6c13f03b20f8f72b"
   end
 
   depends_on "cmake" => :build
