b277304eeb5734f26d7bf39c3cdec152c0c7a7f6
Adrian Ho <the.gromgit@gmail.com>
2022-08-30 09:44:20 +0800

    i686-elf-binutils: update 2.39 bottle.

--- Formula/i686-elf-binutils.rb
+++ Formula/i686-elf-binutils.rb
@@ -1,19 +1,18 @@
 class I686ElfBinutils < Formula
   desc "GNU Binutils for i686-elf cross development"
   homepage "https://www.gnu.org/software/binutils/"
-  url "https://ftp.gnu.org/gnu/binutils/binutils-2.38.tar.xz"
-  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.38.tar.xz"
-  sha256 "e316477a914f567eccc34d5d29785b8b0f5a10208d36bbacedcc39048ecfe024"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.xz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.39.tar.xz"
+  sha256 "645c25f563b8adc0a81dbd6a41cffbf4d37083a382e02d5d3df4f65c09516d00"
   license "GPL-3.0-or-later"
 
   livecheck do
     formula "binutils"
   end
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/i686-elf-binutils"
-    rebuild 1
-    sha256 mojave: "c01f5d216c5df48a3a2dc967da6267cf47e1b493c2e40f961b0fe63abc23ed24"
+    sha256 mojave: "fe081439f5de236fc31774c8c332912d193a6acb0b009eef8c97e9418d2a0f53"
   end
 
   uses_from_macos "texinfo"
