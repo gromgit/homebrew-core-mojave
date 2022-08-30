fe8c4ae7f7224ffdf27617a84d44533a04417003
Adrian Ho <the.gromgit@gmail.com>
2022-08-30 11:56:46 +0800

    janet: update 1.24.1 bottle.

--- Formula/janet.rb
+++ Formula/janet.rb
@@ -1,22 +1,22 @@
 class Janet < Formula
   desc "Dynamic language and bytecode vm"
   homepage "https://janet-lang.org"
-  url "https://github.com/janet-lang/janet/archive/v1.23.0.tar.gz"
-  sha256 "0b4d5d3632e0d376d9512ea8ea262f31f75c132b488dd7870f472acae709a865"
+  url "https://github.com/janet-lang/janet/archive/v1.24.1.tar.gz"
+  sha256 "67964e63957299fd46d9d2640443ae1b7ae3528a396c993c457c3296b1ae586e"
   license "MIT"
   head "https://github.com/janet-lang/janet.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/janet"
-    sha256 cellar: :any, mojave: "a99209f777edebaec15d2153032b6f0fda3e246e480a49b8dbebc5b6238d17aa"
+    sha256 cellar: :any, mojave: "481db552bb0d4bfb95db1e4c095a4471da8c48059461a1954393752d55c48018"
   end
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
 
   resource "jpm" do
-    url "https://github.com/janet-lang/jpm/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "858d4ef2f6ac78222c53154dd91f8fb5994e3c3cbe253c9b0d3b9d52557eeb9b"
+    url "https://github.com/janet-lang/jpm/archive/refs/tags/v1.1.0.tar.gz"
+    sha256 "337c40d9b8c087b920202287b375c2962447218e8e127ce3a5a12e6e47ac6f16"
   end
 
   def install
