7bc89f1697cc1da679a8c5a59c8774db8e17cf19
Adrian Ho <the.gromgit@gmail.com>
2022-08-30 11:42:34 +0800

    ioping: update 1.3 bottle.

--- Formula/ioping.rb
+++ Formula/ioping.rb
@@ -1,15 +1,14 @@
 class Ioping < Formula
   desc "Tool to monitor I/O latency in real time"
   homepage "https://github.com/koct9i/ioping"
-  url "https://github.com/koct9i/ioping/archive/v1.2.tar.gz"
-  sha256 "d3e4497c653a1e96df67c72ce2b70da18e9f5e3b93179a5bb57a6e30ceacfa75"
+  url "https://github.com/koct9i/ioping/archive/v1.3.tar.gz"
+  sha256 "7aa48e70aaa766bc112dea57ebbe56700626871052380709df3a26f46766e8c8"
   license "GPL-3.0-or-later"
   head "https://github.com/koct9i/ioping.git", branch: "master"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ioping"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "33b14ba4c776058e7d8c7ea5dc81f8e080552049f8f6cf77b5f97c2b31fb2114"
+    sha256 cellar: :any_skip_relocation, mojave: "314f99ca6aee88aed56b004e400f6bde458d43d63d99459952377d2329ac52ea"
   end
 
   def install
