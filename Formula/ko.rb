42d399f17dc8886719c19325a1d57aa1a4e7b057
Adrian Ho <the.gromgit@gmail.com>
2022-08-30 12:52:27 +0800

    ko: update 0.12.0 bottle.

--- Formula/ko.rb
+++ Formula/ko.rb
@@ -1,13 +1,13 @@
 class Ko < Formula
   desc "Build and deploy Go applications on Kubernetes"
   homepage "https://github.com/google/ko"
-  url "https://github.com/google/ko/archive/v0.11.2.tar.gz"
-  sha256 "000045afa2eaee9a6af475a41fb60541b39e2e7bfb7b985ff7b1040b6bc2c58c"
+  url "https://github.com/google/ko/archive/v0.12.0.tar.gz"
+  sha256 "cc42e9cde0b4d3380b680cf100c9be9acac67948f3dcfe65d71b87e2da797600"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ko"
-    sha256 cellar: :any_skip_relocation, mojave: "ee4ee6a6511ba6dfeaa7918412cfeb71457cbd7f13a8bf9d733ba3cd7ef86ee3"
+    sha256 cellar: :any_skip_relocation, mojave: "30d11d88a7cc421715ad8bcb594b714dd410dbde4326e3e6d2d620db158549af"
   end
 
   depends_on "go" => :build
