056b37ff24269b7f05ce37d8101a83846df05a6d
Adrian Ho <the.gromgit@gmail.com>
2022-08-31 11:37:19 +0800

    railway: update 2.0.10 bottle.

--- Formula/railway.rb
+++ Formula/railway.rb
@@ -1,14 +1,14 @@
 class Railway < Formula
   desc "Develop and deploy code with zero configuration"
   homepage "https://railway.app/"
-  url "https://github.com/railwayapp/cli/archive/refs/tags/v2.0.8.tar.gz"
-  sha256 "4dfc6869ee125127741ae5182163698d7072450eb1c37f173895a22273bfcdaa"
+  url "https://github.com/railwayapp/cli/archive/refs/tags/v2.0.10.tar.gz"
+  sha256 "b0077fd4408227c4b5509971a220fd4ae5d8bec1b05d340503905a6dae306fdd"
   license "MIT"
   head "https://github.com/railwayapp/cli.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/railway"
-    sha256 cellar: :any_skip_relocation, mojave: "f37495f511e06d203b35e60f99f35f1344feb42345e5a418cd73c95e180289c7"
+    sha256 cellar: :any_skip_relocation, mojave: "d8d8d02a50d7a8caaedc26ede6a3e8b27d6b54b9db21fba0ab9a2e567470349e"
   end
 
   depends_on "go" => :build
