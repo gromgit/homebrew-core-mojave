31c4d7b8d9744e70deac7be06af2da788aab45ad
Adrian Ho <the.gromgit@gmail.com>
2022-08-31 11:58:43 +0800

    regina-rexx: update 3.9.5 bottle.

--- Formula/regina-rexx.rb
+++ Formula/regina-rexx.rb
@@ -1,19 +1,20 @@
 class ReginaRexx < Formula
   desc "Interpreter for Rexx"
   homepage "https://regina-rexx.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/regina-rexx/regina-rexx/3.9.4/regina-rexx-3.9.4.tar.gz"
-  sha256 "a4002237d0c625ded6a270c407643f49738de4eb755b68abdbf69c3f306d18be"
+  url "https://downloads.sourceforge.net/project/regina-rexx/regina-rexx/3.9.5/regina-rexx-3.9.5.tar.gz"
+  sha256 "08e9a9061bee0038cfb45446de20766ffdae50eea37f6642446ec4e73a2abc51"
   license "GPL-2.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/regina-rexx"
-    sha256 mojave: "987a9486936fabcda565455dac9e7ec3003580952ac610ce1ff146685ec2663f"
+    sha256 mojave: "f366422554f29a15eb5af72f81bc42467373d32290851dd2aa3dbbcc7fac42f4"
   end
+
+  uses_from_macos "libxcrypt"
 
   def install
     ENV.deparallelize # No core usage for you, otherwise race condition = missing files.
-    system "./configure", "--disable-debug",
-                          "--prefix=#{prefix}"
+    system "./configure", *std_configure_args
     system "make", "install"
   end
 
