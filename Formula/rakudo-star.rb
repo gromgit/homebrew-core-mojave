462e64cbb16b1b76c8a1a824cbc7224913b7cf3a
Adrian Ho <the.gromgit@gmail.com>
2022-08-31 11:53:06 +0800

    rakudo-star: update 2022.07 bottle.

--- Formula/rakudo-star.rb
+++ Formula/rakudo-star.rb
@@ -1,31 +1,33 @@
 class RakudoStar < Formula
   desc "Rakudo compiler and commonly used packages"
   homepage "https://rakudo.org/"
-  url "https://github.com/rakudo/star/releases/download/2022.06/rakudo-star-2022.06.tar.gz"
-  sha256 "1248ae17aa0f361d8b551a8957bc5b7f8dcda4c02b3839d0f80877f86397bc09"
+  url "https://github.com/rakudo/star/releases/download/2022.07/rakudo-star-2022.07.tar.gz"
+  sha256 "f2c9819cb37ce82e278c4da5a2512cd9a333cac55c21874cf2f388bd9d888537"
   license "Artistic-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rakudo-star"
-    sha256 mojave: "a4893b8b2804f21b2e9d9961e57083eef9cafaaee844ecfce9900026eab4035f"
+    sha256 mojave: "f421f2f0534c10a6e7440b4751a2955f5f54fcfe57861d5937ab3b528a65204d"
   end
 
   depends_on "bash" => :build
   depends_on "gmp"
   depends_on "icu4c"
-  depends_on "libffi"
   depends_on "openssl@3"
   depends_on "pcre"
   depends_on "readline"
+  uses_from_macos "libffi", since: :catalina
 
   conflicts_with "moarvm", "nqp", because: "rakudo-star currently ships with moarvm and nqp included"
   conflicts_with "parrot"
   conflicts_with "rakudo"
 
   def install
-    libffi = Formula["libffi"]
-    ENV.remove "CPPFLAGS", "-I#{libffi.include}"
-    ENV.prepend "CPPFLAGS", "-I#{libffi.lib}/libffi-#{libffi.version}/include"
+    if MacOS.version < :catalina
+      libffi = Formula["libffi"]
+      ENV.remove "CPPFLAGS", "-I#{libffi.include}"
+      ENV.prepend "CPPFLAGS", "-I#{libffi.lib}/libffi-#{libffi.version}/include"
+    end
 
     ENV.deparallelize # An intermittent race condition causes random build failures.
 
@@ -41,18 +43,17 @@ class RakudoStar < Formula
     system "bin/rstar", "install", "-p", prefix.to_s
 
     #  Installed scripts are now in share/perl/{site|vendor}/bin, so we need to symlink it too.
-    bin.install_symlink Dir[share/"perl6/vendor/bin/*"]
-    bin.install_symlink Dir[share/"perl6/site/bin/*"]
+    bin.install_symlink (share/"perl6/vendor/bin").children
+    bin.install_symlink (share/"perl6/site/bin").children
 
     # Move the man pages out of the top level into share.
     # Not all backends seem to generate man pages at this point (moar does not, parrot does),
     # so we need to check if the directory exists first.
-    mv "#{prefix}/man", share if File.directory?("#{prefix}/man")
+    share.install prefix/"man" if (prefix/"man").directory?
   end
 
   test do
-    out = `#{bin}/raku -e 'loop (my $i = 0; $i < 10; $i++) { print $i }'`
+    out = shell_output("#{bin}/raku -e 'loop (my $i = 0; $i < 10; $i++) { print $i }'")
     assert_equal "0123456789", out
-    assert_equal 0, $CHILD_STATUS.exitstatus
   end
 end
