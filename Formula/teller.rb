0e4e691f30932b389236d2f0d4e3b0776fb649a2
Adrian Ho <the.gromgit@gmail.com>
2022-08-31 13:35:49 +0800

    teller: update 1.5.4 bottle.

--- Formula/teller.rb
+++ Formula/teller.rb
@@ -2,14 +2,14 @@ class Teller < Formula
   desc "Secrets management tool for developers built in Go"
   homepage "https://tlr.dev/"
   url "https://github.com/SpectralOps/teller.git",
-      tag:      "v1.5.3",
-      revision: "f7f686544190265db41a2af7bd3313f94d67c880"
+      tag:      "v1.5.4",
+      revision: "604abece2a4a4ca1e32c6d812f5f0aa0e256548c"
   license "Apache-2.0"
   head "https://github.com/SpectralOps/teller.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/teller"
-    sha256 cellar: :any_skip_relocation, mojave: "5e7e4de69aebee04ae50282ba1c6a7a18ae22182e92625798e07f49a223334ed"
+    sha256 cellar: :any_skip_relocation, mojave: "08c42cdc78f7c6169fa9d7fa6382387d5c4a2f2d71e072d3ec18c7744bd43ba7"
   end
 
   depends_on "go" => :build
