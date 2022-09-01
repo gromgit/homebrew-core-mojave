e2832487191c26a507c0ad310135e91cff2a4db7
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-01 16:19:03 +0800

    linux-headers@4.4:

--- Formula/linux-headers@4.4.rb
+++ Formula/linux-headers@4.4.rb
@@ -5,6 +5,13 @@ class LinuxHeadersAT44 < Formula
   sha256 "291d844619b5e7c43bd5aa0b2c286274fc5ffe31494ba475f167a21157e88186"
   license "GPL-2.0-only"
 
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e924cdf506f91e9010ea3dd8a32396262a28bc54ac0290982fd85a3215e7db2a"
+  end
+
+  keg_only :versioned_formula
+
   depends_on :linux
 
   def install
