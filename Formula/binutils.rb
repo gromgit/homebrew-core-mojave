88cb2822c1ae7a6baf3887694406117b4adc3a47
Adrian Ho <the.gromgit@gmail.com>
2022-09-02 00:26:51 +0800

    binutils: update 2.39_1 bottle.

--- Formula/binutils.rb
+++ Formula/binutils.rb
@@ -5,22 +5,27 @@ class Binutils < Formula
   mirror "https://ftpmirror.gnu.org/binutils/binutils-2.39.tar.xz"
   sha256 "645c25f563b8adc0a81dbd6a41cffbf4d37083a382e02d5d3df4f65c09516d00"
   license all_of: ["GPL-2.0-or-later", "GPL-3.0-or-later", "LGPL-2.0-or-later", "LGPL-3.0-only"]
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/binutils"
-    sha256 mojave: "e65db565f13bcc9bbe034bef0689ec6c83a46fe8f3273371e4bfc7e31300524d"
+    sha256 mojave: "9af2e03ddf4cd2403b7166fcda022b808bb36c05df1828525887a69905e17b55"
   end
 
   keg_only :shadowed_by_macos, "Apple's CLT provides the same tools"
 
+  uses_from_macos "bison" => :build
   uses_from_macos "zlib"
 
-  on_linux do
-    depends_on "glibc@2.13" => :build
-    depends_on "linux-headers@4.4" => :build
-  end
+  link_overwrite "bin/gold"
+  link_overwrite "bin/ld.gold"
+  link_overwrite "bin/dwp"
 
   def install
+    # Workaround https://sourceware.org/bugzilla/show_bug.cgi?id=28909
+    touch "gas/doc/.dirstamp", mtime: Time.utc(2022, 1, 1)
+    make_args = OS.mac? ? [] : ["MAKEINFO=true"] # for gprofng
+
     args = [
       "--disable-debug",
       "--disable-dependency-tracking",
@@ -32,17 +37,13 @@ class Binutils < Formula
       "--enable-interwork",
       "--enable-multilib",
       "--enable-64-bit-bfd",
+      "--enable-gold",
       "--enable-plugins",
       "--enable-targets=all",
       "--with-system-zlib",
       "--disable-nls",
-      "--disable-gold",
-      "--disable-gprofng", # Fails to build on Linux
     ]
     system "./configure", *args
-    # Pass MAKEINFO=true to disable generation of HTML documentation.
-    # This avoids a build time dependency on texinfo.
-    make_args = OS.mac? ? [] : ["MAKEINFO=true"]
     system "make", *make_args
     system "make", "install", *make_args
 
@@ -51,8 +52,10 @@ class Binutils < Formula
         bin.install_symlink f => "g" + File.basename(f)
       end
     else
+      bin.install_symlink "ld.gold" => "gold"
       # Reduce the size of the bottle.
-      system "strip", *bin.children, *lib.glob("*.a")
+      bin_files = bin.children.select(&:elf?)
+      system "strip", *bin_files, *lib.glob("*.a")
     end
   end
 
