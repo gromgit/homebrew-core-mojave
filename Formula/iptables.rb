b255ec8d07f5d2031620e65354f949ea98cccd83
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-01 16:19:01 +0800

    iptables:

--- Formula/iptables.rb
+++ Formula/iptables.rb
@@ -4,12 +4,13 @@ class Iptables < Formula
   url "https://www.netfilter.org/pub/iptables/iptables-1.8.8.tar.bz2"
   sha256 "71c75889dc710676631553eb1511da0177bbaaf1b551265b912d236c3f51859f"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
-    sha256 x86_64_linux: "3924660d88ad974260e7b6b5d2bac9a724a94daa2b1a301f457db368fe1cd2fa"
+    sha256 x86_64_linux: "55ab232eefa2576a6a92bef15a4231515afc2b1f942acc96dcf11d4637caeeee"
   end
 
-  depends_on "linux-headers@4.15" => :build
+  depends_on "linux-headers@5.15" => :build
   depends_on "pkg-config" => :build
   depends_on "libmnl"
   depends_on "libnetfilter_conntrack"
@@ -21,7 +22,7 @@ class Iptables < Formula
   uses_from_macos "libpcap"
 
   def install
-    ENV.append "CFLAGS", "-I#{Formula["linux-headers@4.15"].opt_include}"
+    ENV.append "CFLAGS", "-I#{Formula["linux-headers@5.15"].opt_include}"
     system "./configure", *std_configure_args, "--disable-silent-rules",
       "--enable-bpf-compiler",
       "--enable-devel",
