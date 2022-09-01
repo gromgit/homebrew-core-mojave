183e7d583e9dc9e2758e4ebb545c596240058a67
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-01 16:18:58 +0800

    flyway: no rebuilding required

--- Formula/flyway.rb
+++ Formula/flyway.rb
@@ -1,8 +1,8 @@
 class Flyway < Formula
   desc "Database version control to control migrations"
   homepage "https://flywaydb.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/9.2.0/flyway-commandline-9.2.0.tar.gz"
-  sha256 "bcd4cbd7220662095d8c2ebd6218fc7c64a1bdf683bf9cccf36731c19329cd0c"
+  url "https://search.maven.org/remotecontent?filepath=org/flywaydb/flyway-commandline/9.2.1/flyway-commandline-9.2.1.tar.gz"
+  sha256 "aa7585f7f3e6db751494abbb2b878269f2a385e97e37d1fbbfd55f98c279e710"
   license "Apache-2.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class Flyway < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "31993201891cea8eb1f5162e6f6f410bc3180259e59f72301373a2c28f3e1f6a"
+    sha256 cellar: :any_skip_relocation, all: "9dbb9014a6b65eb2e6321ad1912f19a34ca020198bfd5c18cc8e8ca2904cde33"
   end
 
   depends_on "openjdk"
