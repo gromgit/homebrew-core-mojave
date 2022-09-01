efd9a1568fb919c9d525ee56a841313e7470c181
Adrian Ho <the.gromgit@gmail.com>
2022-09-02 00:16:01 +0800

    benthos: update 4.6.0 bottle.

--- Formula/benthos.rb
+++ Formula/benthos.rb
@@ -1,14 +1,14 @@
 class Benthos < Formula
   desc "Stream processor for mundane tasks written in Go"
   homepage "https://www.benthos.dev"
-  url "https://github.com/benthosdev/benthos/archive/v4.5.1.tar.gz"
-  sha256 "76ca4a920089367f725218c73c024de07a3700584569bbad3c8b870c12d231f1"
+  url "https://github.com/benthosdev/benthos/archive/v4.6.0.tar.gz"
+  sha256 "ab552ea0344d97735dd9b1882cb5bc9575f9a246b3fb67f0822357ab55243ebe"
   license "MIT"
   head "https://github.com/benthosdev/benthos.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/benthos"
-    sha256 cellar: :any_skip_relocation, mojave: "de4f3fbddb84a4a161dfe5a0e481131c1abda34d8d75f51683dc8ee1b3b5d22e"
+    sha256 cellar: :any_skip_relocation, mojave: "1fd66e77bcf105066f23b8695d979e5fab2d5b295e99210f48d8435354f2552b"
   end
 
   depends_on "go" => :build
