d0530e3d38ac2eacf4d7f23764d2bc63316402df
Adrian Ho <the.gromgit@gmail.com>
2022-09-03 00:09:28 +0800

    highs: update 1.2.2_1 bottle.

--- Formula/highs.rb
+++ Formula/highs.rb
@@ -4,10 +4,11 @@ class Highs < Formula
   url "https://github.com/ERGO-Code/HiGHS/archive/refs/tags/v1.2.2.tar.gz"
   sha256 "e849276134eb0e7d876be655ff5fe3aa6ecf1030d605edee760620469f9e97cf"
   license "MIT"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/highs"
-    sha256 cellar: :any, mojave: "8ec6d6c28cb6752debd340b962ffb245f38856e4981a7891952807775a70846d"
+    sha256 cellar: :any, mojave: "6b4d8c71188f0ba05672985b1091713e84a734ec01b6a3ea5ca8bf2595f0330c"
   end
 
   depends_on "cmake" => :build
