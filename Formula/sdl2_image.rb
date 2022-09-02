df9a84d491e1f947351d0f8757dbd80fc41b1109
Adrian Ho <the.gromgit@gmail.com>
2022-09-02 13:23:55 +0800

    sdl2_image: update 2.6.2 bottle.

--- Formula/sdl2_image.rb
+++ Formula/sdl2_image.rb
@@ -1,21 +1,21 @@
 class Sdl2Image < Formula
   desc "Library for loading images as SDL surfaces and textures"
   homepage "https://github.com/libsdl-org/SDL_image"
-  url "https://github.com/libsdl-org/SDL_image/releases/download/release-2.6.0/SDL2_image-2.6.0.tar.gz"
-  sha256 "611c862f40de3b883393aabaa8d6df350aa3ae4814d65030972e402edae85aaa"
+  url "https://github.com/libsdl-org/SDL_image/releases/download/release-2.6.2/SDL2_image-2.6.2.tar.gz"
+  sha256 "48355fb4d8d00bac639cd1c4f4a7661c4afef2c212af60b340e06b7059814777"
   license "Zlib"
-  revision 1
 
   # This formula uses a file from a GitHub release, so we check the latest
   # release version instead of Git tags.
   livecheck do
     url :stable
     strategy :github_latest
+    regex(%r{href=.*?/tag/release[._-]v?(\d+(?:\.\d+)+)["' >]}i)
   end
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sdl2_image"
-    sha256 cellar: :any, mojave: "8a5bb54046773c45442c871aeb8c267d0d1ba276a680db15c6b74cb62360fd1a"
+    sha256 cellar: :any, mojave: "afb9b889309853fe1f4a3f808f6b520733a4c0c6f8e887c8a471931f109c43bd"
   end
 
   head do
