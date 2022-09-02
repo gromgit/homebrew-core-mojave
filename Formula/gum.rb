20fee3ab90bab6ccc74d970f60b08d1b7114c3fa
Adrian Ho <the.gromgit@gmail.com>
2022-09-02 23:35:43 +0800

    gum: update 0.5.0 bottle.

--- Formula/gum.rb
+++ Formula/gum.rb
@@ -1,14 +1,14 @@
 class Gum < Formula
   desc "Tool for glamorous shell scripts"
   homepage "https://github.com/charmbracelet/gum"
-  url "https://github.com/charmbracelet/gum/archive/v0.4.0.tar.gz"
-  sha256 "504a92791dacaa06e025a7fea32f96f9d4f67b26a38b1a07eb2703e5519cea1b"
+  url "https://github.com/charmbracelet/gum/archive/v0.5.0.tar.gz"
+  sha256 "0f8e071cc3610c641cc0539f7feb9652b0e8a1fa8f7409c81625cbe71027f28e"
   license "MIT"
   head "https://github.com/charmbracelet/gum.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gum"
-    sha256 cellar: :any_skip_relocation, mojave: "bb8da038ccb67fe0c5b689272d200485e181d98a5763f4b66999244eeb186965"
+    sha256 cellar: :any_skip_relocation, mojave: "fae968ce83f246d353e1f019115bebbb64c161fa1f2e04a109ca681a36fde515"
   end
 
   depends_on "go" => :build
