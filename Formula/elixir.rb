4dd742ed59992d03343e4288844d279991a8a0e7
Adrian Ho <the.gromgit@gmail.com>
2022-09-02 11:13:50 +0800

    elixir: update 1.13.4_1 bottle.

--- Formula/elixir.rb
+++ Formula/elixir.rb
@@ -4,11 +4,12 @@ class Elixir < Formula
   url "https://github.com/elixir-lang/elixir/archive/v1.13.4.tar.gz"
   sha256 "95daf2dd3052e6ca7d4d849457eaaba09de52d65ca38d6933c65bc1cdf6b8579"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/elixir-lang/elixir.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/elixir"
-    sha256 cellar: :any_skip_relocation, mojave: "b2913d5f0c0ce530fa1e0cf1dcf39d730de6bb5eb887ed0915470ce2b4ada64d"
+    sha256 cellar: :any_skip_relocation, mojave: "86f390fc17e4d65b186f09c9aa383b258cdd5a25d112f4505e8739fbbcf68929"
   end
 
   depends_on "erlang"
@@ -26,6 +27,6 @@ class Elixir < Formula
   end
 
   test do
-    system "#{bin}/elixir", "-v"
+    assert_match(%r{(compiled with Erlang/OTP 25)}, shell_output("#{bin}/elixir -v"))
   end
 end
