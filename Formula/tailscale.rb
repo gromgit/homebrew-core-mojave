07daf0148cb27d68cb053990abed184f31b877f8
Adrian Ho <the.gromgit@gmail.com>
2022-09-04 00:02:58 +0800

    tailscale: update 1.30.0 bottle.

--- Formula/tailscale.rb
+++ Formula/tailscale.rb
@@ -2,13 +2,13 @@ class Tailscale < Formula
   desc "Easiest, most secure way to use WireGuard and 2FA"
   homepage "https://tailscale.com"
   url "https://github.com/tailscale/tailscale.git",
-      tag:      "v1.28.0",
-      revision: "aabca3a4c431d24199c1deb25d4684516ead88ca"
+      tag:      "v1.30.0",
+      revision: "0b00b7a135b635fadd8a9a38567562798873ab82"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tailscale"
-    sha256 cellar: :any_skip_relocation, mojave: "dd01aa7624e0f5afaff72b5a390f2fd5442af6fa30ad780ded4fcf6b701e7971"
+    sha256 cellar: :any_skip_relocation, mojave: "a1217ed1df4269c1a9955c362735c643be0d432f6dc31764ad9715c1d11703f2"
   end
 
   depends_on "go" => :build
