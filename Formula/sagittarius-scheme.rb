a1807d584c02ffa59b69473ed5fef9c6b85a3f6d
Adrian Ho <the.gromgit@gmail.com>
2022-09-03 23:11:21 +0800

    sagittarius-scheme: update 0.9.8_1 bottle.

--- Formula/sagittarius-scheme.rb
+++ Formula/sagittarius-scheme.rb
@@ -7,24 +7,24 @@ class SagittariusScheme < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 monterey:     "0e3bc5c34bd1acc50c8ba8d98b474c329068d3ff1a911bf1d8d7e3c4f02c115e"
-    sha256 cellar: :any,                 big_sur:      "a44076e41f030ddccfed6768ada1ff0201ff6ffb86cce425c3fbabda799883a0"
-    sha256 cellar: :any,                 catalina:     "aa2fbe6b306de8d985d0e93f12d9f896b4f7ae5403778508d077bdf975868bcb"
-    sha256 cellar: :any,                 mojave:       "dccfa0d38b7096e3c27676fd09d8984009128c1017948d711087dc9d66f44f6f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "1b527a2abacbc00092ddabd94995149367ac2f388ec739b90a462160d29cbe78"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sagittarius-scheme"
+    rebuild 1
+    sha256 cellar: :any, mojave: "2156ca50b86cf0a9a521686ac284cafccf48239dbbf10f5f25aba9845ef10db3"
   end
 
   depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
   depends_on "bdw-gc"
-  depends_on "libffi"
   depends_on "openssl@1.1"
   depends_on "unixodbc"
 
+  uses_from_macos "libffi", since: :catalina
   uses_from_macos "zlib"
 
   def install
-    system "cmake", ".", *std_cmake_args, "-DODBC_LIBRARIES=odbc"
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", "-DODBC_LIBRARIES=odbc", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
