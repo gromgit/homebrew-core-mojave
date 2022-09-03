67c72ce27077d435f05ac2552a0938cfed94f70e
Adrian Ho <the.gromgit@gmail.com>
2022-09-03 23:50:36 +0800

    swiftformat: update 0.49.18 bottle.

--- Formula/swiftformat.rb
+++ Formula/swiftformat.rb
@@ -1,14 +1,14 @@
 class Swiftformat < Formula
   desc "Formatting tool for reformatting Swift code"
   homepage "https://github.com/nicklockwood/SwiftFormat"
-  url "https://github.com/nicklockwood/SwiftFormat/archive/0.49.17.tar.gz"
-  sha256 "a08d61042905390b7ad2546f45db76f377758c402710481f19c5a0b4c1ece3b4"
+  url "https://github.com/nicklockwood/SwiftFormat/archive/0.49.18.tar.gz"
+  sha256 "a6abc684e1197c96d46a5e6138cee36c6101bd0b19467a476f9abb0d1032a298"
   license "MIT"
   head "https://github.com/nicklockwood/SwiftFormat.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/swiftformat"
-    sha256 cellar: :any_skip_relocation, mojave: "c5e5910a603c8ab7f1e32af5b3b86af6a7786898540f855c3295ea7e7da22438"
+    sha256 cellar: :any_skip_relocation, mojave: "4cb01480b1faebaddd03c766bacc66f49cb256ae673c3014c8e516fd2894828a"
   end
 
   depends_on xcode: ["10.1", :build]
