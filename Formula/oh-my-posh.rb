71fe7a9f56d890fec03d375d4110f03a43602071
Adrian Ho <the.gromgit@gmail.com>
2022-09-03 08:01:28 +0800

    oh-my-posh: update 8.36.6 bottle.

--- Formula/oh-my-posh.rb
+++ Formula/oh-my-posh.rb
@@ -1,14 +1,14 @@
 class OhMyPosh < Formula
   desc "Prompt theme engine for any shell"
   homepage "https://ohmyposh.dev"
-  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v8.35.1.tar.gz"
-  sha256 "cc96df134568864617d2afc4ad3210ab9a856345b35c75c6c390a10d11fe52c2"
+  url "https://github.com/JanDeDobbeleer/oh-my-posh/archive/v8.36.6.tar.gz"
+  sha256 "7c45a3f608d63ab903a7f18610e655f6d557d7711402abf894d009a26b70ec13"
   license "MIT"
   head "https://github.com/JanDeDobbeleer/oh-my-posh.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/oh-my-posh"
-    sha256 cellar: :any_skip_relocation, mojave: "1ececc29c5f1ee503a9c6e4ddb1f666f5006e99af8fd64480cbad63b98740c19"
+    sha256 cellar: :any_skip_relocation, mojave: "eb1f082e2deb557bc7c4e54ba8b3a6595188396ded1fe21bee43a9a804fef591"
   end
 
   depends_on "go" => :build
