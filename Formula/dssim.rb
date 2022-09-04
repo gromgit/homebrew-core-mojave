6ad0c9b449b97232889f8515f86544177751124a
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-05 02:33:24 +0800

    dssim: no rebuilding required

--- Formula/dssim.rb
+++ Formula/dssim.rb
@@ -5,7 +5,7 @@ class Dssim < Formula
   sha256 "b671aec5b117adb6f9c636ef70ecafbd671caacf8b110098565c01bf5171118f"
   license "AGPL-3.0-or-later"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dssim"
     rebuild 1
     sha256 cellar: :any_skip_relocation, mojave: "dd916ab12bbb642755b1f0d3a31cfa1304dba26cc139a4a6fc0a0c6ef8c67f5b"
