f5edacee4b688307701eb1323da7f41ef732d7c2
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-05 02:21:20 +0800

    jhipster:

--- Formula/jhipster.rb
+++ Formula/jhipster.rb
@@ -3,13 +3,12 @@ require "language/node"
 class Jhipster < Formula
   desc "Generate, develop and deploy Spring Boot + Angular/React applications"
   homepage "https://www.jhipster.tech/"
-  url "https://registry.npmjs.org/generator-jhipster/-/generator-jhipster-7.8.1.tgz"
-  sha256 "c39921657dc50a106e0533229492915304b30a85d647e9a65788e19546ed4ed0"
+  url "https://registry.npmjs.org/generator-jhipster/-/generator-jhipster-7.9.2.tgz"
+  sha256 "5f089544d6c2f06bbd109d06460ff6f2387b10d4c8fe9a54d4be070a00779b56"
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jhipster"
-    sha256 cellar: :any_skip_relocation, mojave: "26f019071456b743f559654e97d8b55e61ddf69c0fff67b500ba181d53181ec3"
+    sha256 mojave: "f27baf8ae2f171b8f7236ee399bb9df7da423c4ef81b68d7e0ece78df850d204" # fake mojave
   end
 
   depends_on "node"
