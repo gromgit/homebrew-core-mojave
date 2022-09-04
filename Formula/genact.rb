87a3caacd1c7c44c19b29c494b1b3cd57aad045d
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-05 02:15:57 +0800

    genact: no rebuilding required

--- Formula/genact.rb
+++ Formula/genact.rb
@@ -5,7 +5,7 @@ class Genact < Formula
   sha256 "fbbbcf5e65b370fde6765a2257c6d2f12cb9568bdce785b0f548c2be28d08cd7"
   license "MIT"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/genact"
     rebuild 1
     sha256 cellar: :any_skip_relocation, mojave: "f9e280299f88207d777cb9d9082929e55df26c35da3e9a261f66bec09b2b8516"
