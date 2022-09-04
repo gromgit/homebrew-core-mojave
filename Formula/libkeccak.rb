2187f7ef9ae62abc2b6123271744fcd516f076d4
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-05 02:23:36 +0800

    libkeccak: no rebuilding required

--- Formula/libkeccak.rb
+++ Formula/libkeccak.rb
@@ -5,7 +5,7 @@ class Libkeccak < Formula
   sha256 "c17df59e038f9f1b0f09aa79944ba572f5c4efcbfe8bc6bc7aae1b40f035abe9"
   license "ISC"
 
-bottle do
+  bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libkeccak"
     rebuild 1
     sha256 cellar: :any, mojave: "4128cc4c40bbd628d3a69749c0808e620de8df5b50ef46c78e6fda00e2dbbfa8"
