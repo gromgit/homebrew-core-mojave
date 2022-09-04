4c1983cf8a8296f01f9ff5924a4668f7716a3169
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-05 02:35:23 +0800

    wallpaper:

--- Formula/wallpaper.rb
+++ Formula/wallpaper.rb
@@ -1,16 +1,20 @@
 class Wallpaper < Formula
   desc "Manage the desktop wallpaper"
   homepage "https://github.com/sindresorhus/macos-wallpaper"
-  url "https://github.com/sindresorhus/macos-wallpaper/archive/v2.0.0.tar.gz"
-  sha256 "49ab6121dcc78d17aae3219ceeeb1846792855179f11021192e5c42e500b166c"
-  head "https://github.com/sindresorhus/macos-wallpaper.git"
+  url "https://github.com/sindresorhus/macos-wallpaper/archive/v2.3.1.tar.gz"
+  sha256 "d6aebaca1083ee3e5d6494f5574931691bad239a98e8fe99655790a40f2cb80a"
+  license "MIT"
+  head "https://github.com/sindresorhus/macos-wallpaper.git", branch: "main"
 
-  disable! date: "2021-12-24", because: "fails testing under Mojave"
+  bottle do
+    sha256 mojave: "f27baf8ae2f171b8f7236ee399bb9df7da423c4ef81b68d7e0ece78df850d204" # fake mojave
+  end
 
-  depends_on xcode: ["10.0", :build]
+  depends_on xcode: ["13.0", :build]
+  depends_on :macos
 
   def install
-    system "swift", "build", "-c", "release", "-Xswiftc", "-static-stdlib", "--disable-sandbox"
+    system "swift", "build", "--disable-sandbox", "-c", "release"
     bin.install ".build/release/wallpaper"
   end
 
