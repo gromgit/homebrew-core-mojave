da2b397f8d38869853fb168e22479f9fa3869d92
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-05 02:16:21 +0800

    osm2pgrouting:

--- Formula/osm2pgrouting.rb
+++ Formula/osm2pgrouting.rb
@@ -4,16 +4,26 @@ class Osm2pgrouting < Formula
   url "https://github.com/pgRouting/osm2pgrouting/archive/v2.3.8.tar.gz"
   sha256 "e3a58bcacf0c8811e0dcf3cf3791a4a7cc5ea2a901276133eacf227b30fd8355"
   license "GPL-2.0-or-later"
-  revision 1
+  revision 4
   head "https://github.com/pgRouting/osm2pgrouting.git", branch: "main"
+
+  bottle do
+    sha256 mojave: "f27baf8ae2f171b8f7236ee399bb9df7da423c4ef81b68d7e0ece78df850d204" # fake mojave
+  end
 
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "expat"
+  depends_on "libpq"
   depends_on "libpqxx"
   depends_on "pgrouting"
   depends_on "postgis"
-  depends_on "postgresql"
+
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
 
   def install
     mkdir "build" do
