5f6c79abe09f4ff2563a7e234fcece9372805d82
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-05 02:35:20 +0800

    volk:

--- Formula/volk.rb
+++ Formula/volk.rb
@@ -3,42 +3,42 @@ class Volk < Formula
 
   desc "Vector Optimized Library of Kernels"
   homepage "https://www.libvolk.org/"
-  url "https://github.com/gnuradio/volk.git",
-      tag:      "v2.5.0",
-      revision: "237a6fc9242ea8c48d2bbd417a6ea14feaf7314a"
+  url "https://github.com/gnuradio/volk/releases/download/v2.5.1/volk-2.5.1.tar.gz"
+  sha256 "8f7f2f8918c6ba63ebe8375fe87add347046b8b3acbba2fb582577bebd8852df"
   license "GPL-3.0-or-later"
+  revision 2
 
   bottle do
-    sha256 monterey:     "5461f0cc22da460567168e94b836d4c946ad57e3dad48d6f616b88e1eecae77e"
-    sha256 big_sur:      "13397948c2762d27f80e47fa13ef95930aea8a1b9221ca266f34de6f052890e8"
-    sha256 catalina:     "e1fce59ef2e0308beb2688319d62cce2086159f792838e97d82315fab965c24c"
-    sha256 mojave:       "7e2e2549521877c836c6b2c806f52c6256a9bc43c1d82b1f0edbbce0338bd512"
-    sha256 x86_64_linux: "676c8cf00523f1c0771276bbcb9c99e2d465b2bf8d9d887b4fe03b8a3a524618"
+    sha256 mojave: "f27baf8ae2f171b8f7236ee399bb9df7da423c4ef81b68d7e0ece78df850d204" # fake mojave
   end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "boost"
   depends_on "orc"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   on_linux do
     depends_on "gcc"
   end
 
+  on_intel do
+    depends_on "cpu_features"
+  end
+
   fails_with gcc: "5" # https://github.com/gnuradio/volk/issues/375
 
   resource "Mako" do
-    url "https://files.pythonhosted.org/packages/5c/db/2d2d88b924aa4674a080aae83b59ea19d593250bfe5ed789947c21736785/Mako-1.1.4.tar.gz"
-    sha256 "17831f0b7087c313c0ffae2bcbbd3c1d5ba9eeac9c38f2eb7b50e8c99fe9d5ab"
+    url "https://files.pythonhosted.org/packages/af/b6/42cd322ae555aa770d49e31b8c5c28a243ba1bbb57ad927e1a5f5b064811/Mako-1.1.6.tar.gz"
+    sha256 "4e9e345a41924a954251b95b4b28e14a301145b544901332e658907a7464b6b2"
   end
 
   def install
+    python = "python3.10"
+
     # Set up Mako
     venv_root = libexec/"venv"
-    xy = Language::Python.major_minor_version "python3"
-    ENV.prepend_create_path "PYTHONPATH", "#{venv_root}/lib/python#{xy}/site-packages"
-    venv = virtualenv_create(venv_root, "python3")
+    ENV.prepend_create_path "PYTHONPATH", venv_root/Language::Python.site_packages(python)
+    venv = virtualenv_create(venv_root, python)
     venv.pip_install resource("Mako")
 
     # Avoid references to the Homebrew shims directory
@@ -47,14 +47,20 @@ class Volk < Formula
       s.gsub! "${CMAKE_CXX_COMPILER}", ENV.cxx
     end
 
-    args = %W[-DPYTHON_EXECUTABLE=#{venv_root}/bin/python -DENABLE_TESTING=OFF]
-    system "cmake", ".", *std_cmake_args, *args
-    system "make", "install"
+    # cpu_features fails to build on ARM macOS.
+    args = %W[
+      -DPYTHON_EXECUTABLE=#{venv_root}/bin/python
+      -DENABLE_TESTING=OFF
+      -DVOLK_CPU_FEATURES=#{Hardware::CPU.intel?}
+    ]
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
 
     # Set up volk_modtool paths
-    site_packages = lib/"python#{xy}/site-packages"
+    site_packages = prefix/Language::Python.site_packages(python)
     pth_contents = "import site; site.addsitedir('#{site_packages}')\n"
-    (venv_root/"lib/python#{xy}/site-packages/homebrew-volk.pth").write pth_contents
+    (venv_root/Language::Python.site_packages(python)/"homebrew-volk.pth").write pth_contents
   end
 
   test do
