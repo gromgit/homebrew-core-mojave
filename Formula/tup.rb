0e1aba15a174b47e5716c3f9f59979277c96b0a2
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-05 02:05:03 +0800

    tup:

--- Formula/tup.rb
+++ Formula/tup.rb
@@ -6,6 +6,10 @@ class Tup < Formula
   license "GPL-2.0-only"
   head "https://github.com/gittup/tup.git", branch: "master"
 
+  bottle do
+    sha256 mojave: "f27baf8ae2f171b8f7236ee399bb9df7da423c4ef81b68d7e0ece78df850d204" # fake mojave
+  end
+
   depends_on "pkg-config" => :build
 
   on_macos do
