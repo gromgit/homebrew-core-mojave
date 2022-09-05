e7cc28c16a5242708234c24283d058bd228c521f
Adrian Ho <the.gromgit@gmail.com>
2022-09-05 18:49:52 +0800

    cargo-outdated: update 0.11.1_1 bottle.

--- Formula/cargo-outdated.rb
+++ Formula/cargo-outdated.rb
@@ -4,11 +4,12 @@ class CargoOutdated < Formula
   url "https://github.com/kbknapp/cargo-outdated/archive/v0.11.1.tar.gz"
   sha256 "2d80f0243d70a3563c48644dd3567519c32a733fb5d20f1161fd5d9f8e6e9146"
   license "MIT"
+  revision 1
   head "https://github.com/kbknapp/cargo-outdated.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cargo-outdated"
-    sha256 cellar: :any, mojave: "130f9206a3b362ca4ac49b5d48a8c5083e49870d0061d4bf01f11ce046552c69"
+    sha256 cellar: :any, mojave: "4e78037bb0e1b3cd081fe3d8a854da1646d0d0ef3f7c80ab856508a9846ef9b1"
   end
 
   depends_on "libgit2"
