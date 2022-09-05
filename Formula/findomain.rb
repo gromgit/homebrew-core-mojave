0e58e63d0ad6561209a8130eda67b7b316ad357a
Adrian Ho <the.gromgit@gmail.com>
2022-09-06 06:07:55 +0800

    findomain: update 8.2.1 bottle.

--- Formula/findomain.rb
+++ Formula/findomain.rb
@@ -1,13 +1,13 @@
 class Findomain < Formula
   desc "Cross-platform subdomain enumerator"
   homepage "https://github.com/Findomain/Findomain"
-  url "https://github.com/Findomain/Findomain/archive/8.1.1.tar.gz"
-  sha256 "817497a17bf131afb1ea4fa53eaeeca566949360be168b393260a18ea64c0dbb"
+  url "https://github.com/Findomain/Findomain/archive/8.2.1.tar.gz"
+  sha256 "93c580c9773e991e1073b9b597bb7ccf77f54cebfb9761e4b1180ff97fb15bf6"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/findomain"
-    sha256 cellar: :any_skip_relocation, mojave: "e661dc0d3e5a11a39a108e09f1d9aa68aaca292b984cc00fea7bf9b390f8dfd7"
+    sha256 cellar: :any_skip_relocation, mojave: "fb1be5fa4693a51c41e954d5d452f7819f75bb8556cf7a170350cdf8a50b2a4b"
   end
 
   depends_on "rust" => :build
