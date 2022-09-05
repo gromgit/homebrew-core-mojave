12cf1566426a8812b3151ba4824d053681fba131
Adrian Ho <the.gromgit@gmail.com>
2022-09-05 19:20:52 +0800

    ccrypt: update 1.11 bottle.

--- Formula/ccrypt.rb
+++ Formula/ccrypt.rb
@@ -6,16 +6,9 @@ class Ccrypt < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 arm64_monterey: "6df2f69dee386a1f37820245fdcf2f2f6e52389e1617b8bcd72dfae25d829207"
-    sha256 arm64_big_sur:  "df71b344abdb49c98de85ee062d3e505afdcdb203cde01d165e326b52e7bb891"
-    sha256 monterey:       "77326e57d8ebf598daed98540cde9e40b67dc5f759c5ea01a48a8defec9c2347"
-    sha256 big_sur:        "f416ae1ffac238640025b992cfedb05ab6894d0ef6c60742b3ab95757bd137f0"
-    sha256 catalina:       "e09c7818b7de98e36d433080334e169ac970e1a020114ddab1fdbbd54135ddbc"
-    sha256 mojave:         "49054d9d502ab13e65ab873cc9d355ab75438372a7770c38c4c7c35c84c31e3a"
-    sha256 high_sierra:    "a4d270d5b5f467870f0b265f6f2d1861762d853df46756a34ac7e6a6d83e2121"
-    sha256 sierra:         "048295cb4f95c9f0f3c5f1a619141e08c0326b6d8252c62c97608fb028cb48f7"
-    sha256 el_capitan:     "a98ea0f3dbee5e9086bea342ac8291303970b1d8a85344be2b4d91330a919ae9"
-    sha256 x86_64_linux:   "3e2c5e49110742fb547d82b661695d2044a2404869e7224c1de1be036dd253de"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ccrypt"
+    rebuild 1
+    sha256 mojave: "07156b0368ca778d13862be910219956e740879b24ca1fe2bf28df38177c7c89"
   end
 
   conflicts_with "ccat", because: "both install `ccat` binaries"
