ae9f254ecc537df60769c1457e55effd1632c503
Adrian Ho <the.gromgit@gmail.com>
2022-09-05 17:00:00 +0800

    brag: update 1.4.3 bottle.

--- Formula/brag.rb
+++ Formula/brag.rb
@@ -5,7 +5,9 @@ class Brag < Formula
   sha256 "f2c8110c38805c31ad181f4737c26e766dc8ecfa2bce158197b985be892cece6"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "33072e85eec020579548bd6559d88e8ca2e192ac09c921620d3b1f1cb9349cea"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/brag"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "33072e85eec020579548bd6559d88e8ca2e192ac09c921620d3b1f1cb9349cea"
   end
 
   depends_on "uudeview"
