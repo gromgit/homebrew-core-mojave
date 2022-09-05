9df0c191164e2e40cbe72661060ccde3f1c7b5f4
Adrian Ho <the.gromgit@gmail.com>
2022-09-05 21:05:01 +0800

    cobalt: update 0.18.3 bottle.

--- Formula/cobalt.rb
+++ Formula/cobalt.rb
@@ -1,13 +1,13 @@
 class Cobalt < Formula
   desc "Static site generator written in Rust"
   homepage "https://cobalt-org.github.io/"
-  url "https://github.com/cobalt-org/cobalt.rs/archive/v0.18.0.tar.gz"
-  sha256 "1ecab64da429ab362fa2c1eced7b1bb91bae0c3d144808db82d7410059099aff"
+  url "https://github.com/cobalt-org/cobalt.rs/archive/v0.18.3.tar.gz"
+  sha256 "32350ef91a0c1dd81b75e8eb94f5a591ca91bd35d1a6d97622996b1086d5ced2"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cobalt"
-    sha256 cellar: :any_skip_relocation, mojave: "df5bcf36302fe3a3097e9adaaa41bb6073bcd05a9ec473338b0d2ee7f7f51e52"
+    sha256 cellar: :any_skip_relocation, mojave: "32de5675f9c875c71c0e6d08fac0cdb3f8a852172bed1baf7e85a755409da3e9"
   end
 
   depends_on "rust" => :build
