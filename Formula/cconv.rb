80862367d06a88bf26048f75d101317a79f76139
Adrian Ho <the.gromgit@gmail.com>
2022-09-05 19:17:22 +0800

    cconv: update 0.6.3 bottle.

--- Formula/cconv.rb
+++ Formula/cconv.rb
@@ -6,16 +6,9 @@ class Cconv < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "26691ab7b01031ee988442fb08c173d48da19e26d9cc494c829284b0248ee6d2"
-    sha256 cellar: :any,                 arm64_big_sur:  "58c753e4b4b6887b81202d33c63ac6ef153d7166c6542661ab3dcbb322f173bf"
-    sha256 cellar: :any,                 monterey:       "0e2e409ca1a2358465d8a780c94df0041cf7f162139eebdb86e9b6443376fac8"
-    sha256 cellar: :any,                 big_sur:        "f39720a1d032edbcdbf6ccfd6a5f8c9dc46faaf95e479904cfde25ec5c7622d6"
-    sha256 cellar: :any,                 catalina:       "06b6bafaadcaa16329ba0cdeee7d11a13e94f126a4011b54253e31a1ea82108e"
-    sha256 cellar: :any,                 mojave:         "ffaf8b5cab0618e52cfedff14a5084cfe54e0b1b6480433e2ffb4beee8e47ec9"
-    sha256 cellar: :any,                 high_sierra:    "c4d197f979340a89d5a87e05eae6a39db38863f89b6ddda42f924472d87a5b0d"
-    sha256 cellar: :any,                 sierra:         "2e885b9571a8814f2b23b088f3f0d45f47b1fe762f040c3e66b1a81f84673646"
-    sha256 cellar: :any,                 el_capitan:     "bda78602260276dd3e5187a5a9d6bbcfb95ff40aa513840569e490d5dc96aab2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d7176512194c3bed0f8db76bf768136559dd09bfaaa3562908acaa748ccbd288"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cconv"
+    rebuild 1
+    sha256 cellar: :any, mojave: "992401f39698bcac9d74c7f4dad55a9f4469001c7826eb0897d7988f806cce71"
   end
 
   depends_on "autoconf" => :build
