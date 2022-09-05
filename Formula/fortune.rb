66441947977b0deb4eb2764e391e6ad5c480f92c
Adrian Ho <the.gromgit@gmail.com>
2022-09-06 07:01:33 +0800

    fortune: update 9708 bottle.

--- Formula/fortune.rb
+++ Formula/fortune.rb
@@ -11,15 +11,9 @@ class Fortune < Formula
   end
 
   bottle do
-    rebuild 4
-    sha256 arm64_monterey: "9412148af6d5be4f3256e07427834522e5c241b5ba7d6b71fefa1112774bf337"
-    sha256 arm64_big_sur:  "78689923b7ba4d7d5a4541a93a543effcbc7dcd722d803b47954656c92dfdeca"
-    sha256 monterey:       "edf16c29c279acfdbeecb0202f93ef36852f96060fadf7645b94aa19b0cb054d"
-    sha256 big_sur:        "de301856c24aee684544214cb83474fe8237104c9cf214df6777267418b17d9f"
-    sha256 catalina:       "9d1ed340349cd7995d1308fc09fc69c3520c96b329ab881dc0d96fce914e029c"
-    sha256 mojave:         "9a7a866859df246c3fe9331cb1b131562359690dbc5bfed6ee4e8f5a4585025e"
-    sha256 high_sierra:    "3421fe011b2f27d30ae6e56d880eba8a68cb1249d6c4cd063a04fd61022507be"
-    sha256 x86_64_linux:   "791d7f7963c86af2f2ef311f739f8faddcbd0448feb0b8213d2b3c2263fc317e"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fortune"
+    rebuild 5
+    sha256 mojave: "c89b174941a61d9e9db4cbd17357848033c53d6373f1a43208f938d9486290a9"
   end
 
   def install
