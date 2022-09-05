3d96b1de1412e1a6a5ec8344dc229fc71ed18965
Adrian Ho <the.gromgit@gmail.com>
2022-09-05 19:12:59 +0800

    ccd2iso: update 0.3 bottle.

--- Formula/ccd2iso.rb
+++ Formula/ccd2iso.rb
@@ -6,16 +6,9 @@ class Ccd2iso < Formula
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "3a6f9de23756982dfeb30537a00671d0a3d51edb2e322a9d4285c3a89da3be63"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a5744e3d028a63b27e0a6b9c8f0c839effbd4385f41af50bb44725d5310b61ee"
-    sha256 cellar: :any_skip_relocation, monterey:       "16d7cbbc96fab04752d418f7e4f196e2d0d36e494a27002bf76f907d160cba50"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1bd1d2fedb8cd0bfe682f80bffeced5c7e273a2d24c2dd01a7b777e4d3ee0115"
-    sha256 cellar: :any_skip_relocation, catalina:       "741bb587861701e9900ede511e2db1e73815428eb2f0f2c697313dad70609853"
-    sha256 cellar: :any_skip_relocation, mojave:         "710ddc04aac005477e9aaa73e882bc1d8cbe96412ac949ff4a7501c6a53ca018"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "9d33b636be5f43c1e40955323c2f5d4a02d603c990aab2c89e98b5cb16a5cf93"
-    sha256 cellar: :any_skip_relocation, sierra:         "c855496f0265a8f806228cddc1c15d5a1d6e7186f4bb43c0a317a6256d8e8e85"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "e74b2779ef3d832bc899422285c2d03ea33aa6ab979ca835914343999b444671"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "eca2835beb5b622b6f0b2b781524e638e67a65b8d4f683cb0984c74c34c1603b"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ccd2iso"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "cde25d8099c32b8322ad5b75c9c645c45cd1ff0f68ff779fae0231867dc00981"
   end
 
   def install
