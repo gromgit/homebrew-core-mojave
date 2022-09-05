7555a805a4f1099b46eeafb7ef9c3c1b1f21e30a
Adrian Ho <the.gromgit@gmail.com>
2022-09-06 07:23:46 +0800

    fsql: update 0.4.0 bottle.

--- Formula/fsql.rb
+++ Formula/fsql.rb
@@ -6,13 +6,9 @@ class Fsql < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d08ff0913536b1cd4b7116c27146e976442b255977de14003d1049dfc8e84aa1"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "77ec663d81151aebf469db5626893d130ff21114f603227046999a27ea843f8f"
-    sha256 cellar: :any_skip_relocation, monterey:       "eda303b91bc756145a51a374ffadc4862d7967c2358be1c3f0d544951390d13d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "53d773ca5cba807627e4e3b7cdb36990c18c3b1944c847756c1acf95d99d6af6"
-    sha256 cellar: :any_skip_relocation, catalina:       "c97d021f2047654b1141141bfdaceba40953a39cc150233cb9ec3bb85ee9b675"
-    sha256 cellar: :any_skip_relocation, mojave:         "5d984747c498019e9950c57084e2549417dce8206ab489650fcba917ad7a30af"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9f54460f0a2744214e28ad7a8d9ef8f583ca32a91bf60588cb2dc2cc467aff56"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fsql"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "58c68c15d5d0d8003e083cac83a5b7966498280eaaf2751d9064aee30bc58374"
   end
 
   # Bump to 1.18 on the next release, if possible.
