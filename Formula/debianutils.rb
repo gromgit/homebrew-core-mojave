146cfb46969c7214d6d64f4466e634dbf5153d8c
Adrian Ho <the.gromgit@gmail.com>
2022-09-06 00:34:27 +0800

    debianutils: update 4.11.2 bottle.

--- Formula/debianutils.rb
+++ Formula/debianutils.rb
@@ -11,14 +11,9 @@ class Debianutils < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "391f28aad571638e900584cb160e65029ff58f29af2e1cc078cc64fe742e042c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d85a9b994fc1a8feee952fcdc7f745801fb1e39fdc733ea76f69c22dd7636fef"
-    sha256 cellar: :any_skip_relocation, monterey:       "936c65c8eb8cd3f673792bedbfea37dab226367718f4d33b4213cf62da1f1c6d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6c099d05496a2f897a037aa8b20944faa62254b5486838a29f9735c696e7cb73"
-    sha256 cellar: :any_skip_relocation, catalina:       "b6a3110aa8113eb30d7b3dd71ac194d476969322e2a184172c8da9923c497c19"
-    sha256 cellar: :any_skip_relocation, mojave:         "5d50261564a4696a8f9d0eed99ffa0ed8eebc8344a0365d5c9b4083a54d3b6de"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "be68111406f254d184ffecf06a181df3000525e05b18f9b072c4cdd0ef30b3c1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6fd96fb4733f4fc575514e778d83ee69006440d867df416ed58b45bfb954ae30"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/debianutils"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "b6cd06aa3a6f67b6250d46995180e8668028c77c2fd0ed666ee7d6322e5172aa"
   end
 
   def install
