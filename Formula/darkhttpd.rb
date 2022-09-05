806b882691a27fe1c0ec61b2e9aaea753b4219f2
Adrian Ho <the.gromgit@gmail.com>
2022-09-05 23:53:50 +0800

    darkhttpd: update 1.13 bottle.

--- Formula/darkhttpd.rb
+++ Formula/darkhttpd.rb
@@ -6,13 +6,9 @@ class Darkhttpd < Formula
   license "ISC"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0cf5e582aaab036d6a467b399416ec554c595f6b85187e76d43f57f2eb7dbbe3"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "36ae262543c3c5a76c282267e400ed1566cf31d1955f9a2d2298e2cb14a42cc8"
-    sha256 cellar: :any_skip_relocation, monterey:       "34522ba6119c42022afeb73ef5b95a360d3293322c43af7d5e3edfc8034a5c8e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "1643b2894325e4ed51b6007c1f1c6a935cc0780f48f3d953620d7b0ab50d6dbc"
-    sha256 cellar: :any_skip_relocation, catalina:       "161992a2da584f5704fc6923d26fe6675a2ac23b3a66d9c1bba154b2a5888833"
-    sha256 cellar: :any_skip_relocation, mojave:         "fdc947505f7ee3885b23431afe3603cb6e75f7edeea9784dab45975b30956086"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dddd097bfb71ec14118ed242beb0e7feb31012cb3888a9c6f5ed0c6ecdeda830"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/darkhttpd"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "f417a0bf3183bc06b0e98a66b069937b9b70ac6cc9a99ebced2f1c8ebdaad761"
   end
 
   def install
