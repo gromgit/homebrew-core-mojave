20d9ab67f5befcc8168648a116ced09e3ce87d83
Adrian Ho <the.gromgit@gmail.com>
2022-09-06 04:20:29 +0800

    eg-examples: update 1.2.1_1 bottle.

--- Formula/eg-examples.rb
+++ Formula/eg-examples.rb
@@ -9,13 +9,9 @@ class EgExamples < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "6d089071964aeefc2ca28062eff414a0c0dea3d940495212c71a2609e4749b36"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6d089071964aeefc2ca28062eff414a0c0dea3d940495212c71a2609e4749b36"
-    sha256 cellar: :any_skip_relocation, monterey:       "9e766b57ef1ede0a5cd68da245d372573860545702af673ba18693159cc95657"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9e766b57ef1ede0a5cd68da245d372573860545702af673ba18693159cc95657"
-    sha256 cellar: :any_skip_relocation, catalina:       "9e766b57ef1ede0a5cd68da245d372573860545702af673ba18693159cc95657"
-    sha256 cellar: :any_skip_relocation, mojave:         "9e766b57ef1ede0a5cd68da245d372573860545702af673ba18693159cc95657"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6d057caedc12f52baeaec1c47fa9b6f3346d9d7c4e8e519ca5781f1915e6f9bc"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/eg-examples"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "8ad5951295b60ccd37774e71973cca05ad9a99adf1148b1d05ef2549e1cfefb6"
   end
 
   depends_on "python@3.10"
