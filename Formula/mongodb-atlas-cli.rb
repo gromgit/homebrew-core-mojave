4fed1305b68092484dd47add2886bf0d778a1bd9
Adrian Ho <the.gromgit@gmail.com>
2022-09-07 00:08:47 +0800

    mongodb-atlas-cli: update 1.1.7 bottle.

--- Formula/mongodb-atlas-cli.rb
+++ Formula/mongodb-atlas-cli.rb
@@ -1,8 +1,8 @@
 class MongodbAtlasCli < Formula
   desc "Atlas CLI enables you to manage your MongoDB Atlas"
   homepage "https://www.mongodb.com/docs/atlas/cli/stable/"
-  url "https://github.com/mongodb/mongodb-atlas-cli/archive/refs/tags/atlascli/v1.1.4.tar.gz"
-  sha256 "78e030dabaf114d9359eeac4fac1bce1eb9ee68ffca0b6e6e32bbf3f3189d0a7"
+  url "https://github.com/mongodb/mongodb-atlas-cli/archive/refs/tags/atlascli/v1.1.7.tar.gz"
+  sha256 "3f027fc126a86370fa50f8c21200a2a68d3d1d3ecec969e834891cf625360fea"
   license "Apache-2.0"
   head "https://github.com/mongodb/mongodb-atlas-cli.git", branch: "master"
 
@@ -13,7 +13,7 @@ class MongodbAtlasCli < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mongodb-atlas-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "5226d4e8f46330e6edf11eeea903553ee89a31ca211710599458b2b9e9c4eb5d"
+    sha256 cellar: :any_skip_relocation, mojave: "aa1855bfade55c373d9a278db2685d5ee40183d35a5827661718db41c8ebb1f2"
   end
 
   depends_on "go" => :build
