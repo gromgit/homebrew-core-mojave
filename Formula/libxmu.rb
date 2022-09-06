4383670e4133a7c5caab24c9397ce5d32850713b
Adrian Ho <the.gromgit@gmail.com>
2022-09-06 12:06:00 +0800

    libxmu: update 1.1.3 bottle.

--- Formula/libxmu.rb
+++ Formula/libxmu.rb
@@ -6,14 +6,9 @@ class Libxmu < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "5ad15776b11e79adc0381fcfc2e45ada0c01e73abbb5527602f4b0b4694de5a3"
-    sha256 cellar: :any,                 arm64_big_sur:  "c0efd6d85d0f2738138ce77b3a5eb18cfcc984f8cded38351670a1066f00d732"
-    sha256 cellar: :any,                 monterey:       "2b0a4000ba1637d33b565fbc6af7f125db337485e438a689a2b5eba1364bd362"
-    sha256 cellar: :any,                 big_sur:        "2b2e329a7ebf57e09d29296db2763f2ea105ce73082f2c11d4c29f9da32c0070"
-    sha256 cellar: :any,                 catalina:       "ff33cd2f865f77d04ad861dd5e10784842511050c754e9bd772ab81b2e1c6918"
-    sha256 cellar: :any,                 mojave:         "a437eedd57edefe94c7a4d7cb30ac03d5ea6852f7a6ba6be33e12c839cde6ac6"
-    sha256 cellar: :any,                 high_sierra:    "24ca27eaf60f6dfe9d35d61ff4edc5ba7b9e04e3d9efc9938fe5eb905057c8e6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "53788e56315f8ab486c9607172e7718229a0606f7ab1e80a632ccf8fee3da644"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libxmu"
+    rebuild 1
+    sha256 cellar: :any, mojave: "62253fc39bf1a3fd3265927c4407a6ad44334b4976cf56974e9b6f8c8490ae2d"
   end
 
   depends_on "pkg-config" => :build
