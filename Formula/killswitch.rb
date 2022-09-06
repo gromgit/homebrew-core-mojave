d0f4910e2aaa6f5e55bce924c0659627953e50cd
Adrian Ho <the.gromgit@gmail.com>
2022-09-06 19:07:45 +0800

    killswitch: update 0.7.2 bottle.

--- Formula/killswitch.rb
+++ Formula/killswitch.rb
@@ -6,13 +6,9 @@ class Killswitch < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "73d4c8aa15e1aa363089be53bc11fb347994c1144ad35a65d01584727f74082d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cf8c995419a923e0512fd5e2d54a88d46719e157f1f9592ec82289e97f3da876"
-    sha256 cellar: :any_skip_relocation, monterey:       "aaddf26597e4de2554dfc11bb909615a612e0389774cea37f4356432a666bb41"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8d3e4381fb6137e38fd394941e40f1c0fab743c7b4bfef3c2ecb43679b6ab03b"
-    sha256 cellar: :any_skip_relocation, catalina:       "3e00a8591a897509a48c65d76e529c6f4ef6fc910ebb762c8e5e7f54e2e03a43"
-    sha256 cellar: :any_skip_relocation, mojave:         "4cdbf573342205befe4e908ae318125be61850d2346c5ca649cdd867067eab63"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "82a98dbef512e928dfcee02d0c7c50889856ce88740645ec1af0fcac7edfab12"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/killswitch"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "94a3d670c97b8d1c76f8af76baf808c75e7b5db47133f6a30025ce6e82c941fb"
   end
 
   # Bump to 1.18 on the next release, if possible.
