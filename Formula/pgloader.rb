450b0b2596faac8ce30ccb313cc4f49317c0b3a2
Adrian Ho <the.gromgit@gmail.com>
2022-09-07 13:17:54 +0800

    pgloader: update 3.6.7 bottle.

--- Formula/pgloader.rb
+++ Formula/pgloader.rb
@@ -1,8 +1,8 @@
 class Pgloader < Formula
   desc "Data loading tool for PostgreSQL"
   homepage "https://github.com/dimitri/pgloader"
-  url "https://github.com/dimitri/pgloader/releases/download/v3.6.6/pgloader-bundle-3.6.6.tgz"
-  sha256 "1837565d8fcedb132c68885a40893ec3c590b7da9ebcef1c0e580b19f353544d"
+  url "https://github.com/dimitri/pgloader/releases/download/v3.6.7/pgloader-bundle-3.6.7.tgz"
+  sha256 "25f1767a5d2f2630c0c81da5dc7e1d2e010882799796b094558283a63da33356"
   license "PostgreSQL"
   head "https://github.com/dimitri/pgloader.git", branch: "master"
 
@@ -13,13 +13,13 @@ class Pgloader < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pgloader"
-    sha256 cellar: :any, mojave: "37c2039406b2f939693d9808baad9d4926bd529643c63226258fc64fe17d6592"
+    sha256 cellar: :any, mojave: "f6ecca43f89e61714b2e68a1365677a0e1837f8b1affc86d7b11feaf82bc054c"
   end
 
   depends_on "buildapp" => :build
   depends_on "freetds"
+  depends_on "libpq"
   depends_on "openssl@1.1"
-  depends_on "postgresql"
   depends_on "sbcl"
 
   def install
