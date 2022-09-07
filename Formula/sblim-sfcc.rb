4c30fb6a849e2ee9109f8a2048eb16a0add833e5
Adrian Ho <the.gromgit@gmail.com>
2022-09-07 22:01:40 +0800

    sblim-sfcc: update 2.2.8_1 bottle.

--- Formula/sblim-sfcc.rb
+++ Formula/sblim-sfcc.rb
@@ -11,15 +11,9 @@ class SblimSfcc < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "1e22137d49b22b534626dcd8de3dfd98743d0eaba4fb1768e7e8f6553bfff162"
-    sha256 cellar: :any,                 arm64_big_sur:  "9b75fdfd7494479190a2651966a17408dfa29a1f2f62c860e165caa5f135a6df"
-    sha256 cellar: :any,                 monterey:       "dde4c03d86680b2886786a8a32d644187a9eb246e6de9c4db906250baa33ba99"
-    sha256 cellar: :any,                 big_sur:        "8a334b1f1c440e4103ab947b0871ea160a0d319cf095e8ce8b45521ec9440770"
-    sha256 cellar: :any,                 catalina:       "d20839c77aaa1c968981ca9af0e92bf1c7f600392ea5674971f532040dae518a"
-    sha256 cellar: :any,                 mojave:         "ff61a006626a9a36dafb474f352d798805b1a44adba341d8422bd0820eaae1ab"
-    sha256 cellar: :any,                 high_sierra:    "0ee558ce892d6e04acfe7ca2408a96e2837c7c858e71f6047b3a57a15b75ece0"
-    sha256 cellar: :any,                 sierra:         "2e1eea4bbad906293b2c48a27a09fd76d665ab0c9259ef49fcd81f4783fbb67c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c309028903cce29bd8b4473bc4f21e02e226aaa7c497a97138f931bd9f887e93"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sblim-sfcc"
+    rebuild 1
+    sha256 cellar: :any, mojave: "befc8032c58e2851c7c933be1308956cc38164e2d488916f3b462171e4655842"
   end
 
   depends_on "autoconf" => :build
