3d6a01afba295da6d7bcefc226ed9569e38ac644
Adrian Ho <the.gromgit@gmail.com>
2022-09-08 00:49:21 +0800

    ssed: update 3.62 bottle.

--- Formula/ssed.rb
+++ Formula/ssed.rb
@@ -11,14 +11,9 @@ class Ssed < Formula
   end
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0177eab6f33edfd99c8471ccc15826b000dbea9d582194971fbd268f83f21e7d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2b6c860af3e99b067b867a53b1b7135918cf1ff10e27b744a4157c2134b866f5"
-    sha256 cellar: :any_skip_relocation, monterey:       "0cee50ad78c7007b7240e372a8df0858c7944e2e8da88f33565141e79fbe7354"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8ead33fc5954cd35bdff6291cec76e4ee2d6011d0f4d7025e832c9fa2514c31b"
-    sha256 cellar: :any_skip_relocation, catalina:       "69a3bbba8a87299f96f1b51c612d3335d1114ffb0bc6aa186c6f3e87335767e6"
-    sha256 cellar: :any_skip_relocation, mojave:         "b3b9297ad7ac202de1403508682aaf1dbb187fe3ab1f4c6cc9bee7b7a69ab1d9"
-    sha256                               x86_64_linux:   "093ca16b33b896aebd964aa694592dea5a87e47f59d46e52b11ffb46bca3d488"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ssed"
+    rebuild 3
+    sha256 cellar: :any, mojave: "f8b65deea1150b305fd89ae322c00e599851290867c1440011329c698966e352"
   end
 
   conflicts_with "gnu-sed", because: "both install share/info/sed.info"
