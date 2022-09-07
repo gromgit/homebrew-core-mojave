2a3d46715176df2d951cdc75d490fc3ca888a817
Adrian Ho <the.gromgit@gmail.com>
2022-09-07 13:06:44 +0800

    pg_cron: update 1.4.2_1 bottle.

--- Formula/pg_cron.rb
+++ Formula/pg_cron.rb
@@ -4,41 +4,44 @@ class PgCron < Formula
   url "https://github.com/citusdata/pg_cron/archive/refs/tags/v1.4.2.tar.gz"
   sha256 "3652722ea98d94d8e27bf5e708dd7359f55a818a43550d046c5064c98876f1a8"
   license "PostgreSQL"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pg_cron"
-    sha256 cellar: :any, mojave: "58ffbc913d0ce0565886183b2f7f5487411846c8a42ec97dadce5dd92750782e"
+    sha256 cellar: :any, mojave: "f9fa7cdc2eb463424fd7b22508287df70968d7336678d50aa92242359ea63391"
   end
 
-  depends_on "postgresql"
+  depends_on "postgresql@14"
+
+  def postgresql
+    Formula["postgresql@14"]
+  end
 
   def install
+    ENV["PG_CONFIG"] = postgresql.opt_bin/"pg_config"
+
     system "make"
-    (lib/"postgresql").install "pg_cron.so"
-    (share/"postgresql/extension").install Dir["pg_cron--*.sql"]
-    (share/"postgresql/extension").install "pg_cron.control"
+    (lib/postgresql.name).install "pg_cron.so"
+    (share/postgresql.name/"extension").install Dir["pg_cron--*.sql"]
+    (share/postgresql.name/"extension").install "pg_cron.control"
   end
 
   test do
-    # Testing steps:
-    # - create new temporary postgres database
-    system "pg_ctl", "initdb", "-D", testpath/"test"
-
+    pg_ctl = postgresql.opt_bin/"pg_ctl"
+    psql = postgresql.opt_bin/"psql"
     port = free_port
-    # - enable pg_cron in temporary database
+
+    system pg_ctl, "initdb", "-D", testpath/"test"
     (testpath/"test/postgresql.conf").write <<~EOS, mode: "a+"
 
       shared_preload_libraries = 'pg_cron'
       port = #{port}
     EOS
-
-    # - restart temporary postgres
-    system "pg_ctl", "start", "-D", testpath/"test", "-l", testpath/"log"
-
-    # - run "CREATE EXTENSION pg_cron;" in temp database
-    system "psql", "-p", port.to_s, "-c", "CREATE EXTENSION pg_cron;", "postgres"
-
-    # - shutdown temp postgres
-    system "pg_ctl", "stop", "-D", testpath/"test"
+    system pg_ctl, "start", "-D", testpath/"test", "-l", testpath/"log"
+    begin
+      system psql, "-p", port.to_s, "-c", "CREATE EXTENSION \"pg_cron\";", "postgres"
+    ensure
+      system pg_ctl, "stop", "-D", testpath/"test"
+    end
   end
 end
