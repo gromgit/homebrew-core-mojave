36322e0fb90735b567e294ec0b97acfa3a87dc67
Adrian Ho <the.gromgit@gmail.com>
2022-09-07 09:05:53 +0800

    openmama: update 6.3.1 bottle.

--- Formula/openmama.rb
+++ Formula/openmama.rb
@@ -6,14 +6,9 @@ class Openmama < Formula
   license "LGPL-2.1-only"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "2e7a0417aeac01af231fb8302c4fbe3c212d81f2d657fbef9a54894aa0e3d52f"
-    sha256 cellar: :any,                 arm64_big_sur:  "e68c9fee04206d5f5b21d84889bf645c7f4b2b2922a886e9bbcc226a05faa183"
-    sha256 cellar: :any,                 monterey:       "af3c17bf4293979ab3fb40f6fbfaf6ffedb4dc5ec74eb1345770da6da7ff5597"
-    sha256 cellar: :any,                 big_sur:        "b3d28de466d5f2d17ddb57b2f5004e3defc7f8d48922814f61f733aa7015639c"
-    sha256 cellar: :any,                 catalina:       "50fe6f8436bd5d7729f9f20c21de8d398e50546754514708e24715a097bd21f1"
-    sha256 cellar: :any,                 mojave:         "b0a5d95139fce5f6f72b5a2906c5e2e6b604aef25e2b76e7c448ab1bfcefe6d6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "cec891fa1150c54fb96efaf979e1efbbaf50b0327978c55a59242c83cb96a73e"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/openmama"
+    rebuild 2
+    sha256 cellar: :any, mojave: "d8d9d61f31bd81737971917bf8d0f3924a64fe9987d5e55a8177ebb22773d204"
   end
 
   depends_on "cmake" => :build
