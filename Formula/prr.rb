dc003e1964d76a8efe6643c762ea86f91677e3c9
Adrian Ho <the.gromgit@gmail.com>
2022-09-07 17:29:59 +0800

    prr: update 0.5.1 bottle.

--- Formula/prr.rb
+++ Formula/prr.rb
@@ -1,21 +1,21 @@
 class Prr < Formula
   desc "Mailing list style code reviews for github"
   homepage "https://github.com/danobi/prr"
-  url "https://github.com/danobi/prr/archive/refs/tags/v0.4.3.tar.gz"
-  sha256 "becebd6746b62380ff354e0d4e92b515c0aac719e9d504f33b6d9321b2d80853"
+  url "https://github.com/danobi/prr/archive/refs/tags/v0.5.1.tar.gz"
+  sha256 "0506ade022a7e4be858cdc4048d82a80587f9ca7ebd78c5a652baa1e967cf41e"
   license "GPL-2.0-only"
   head "https://github.com/danobi/prr.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/prr"
-    sha256 cellar: :any_skip_relocation, mojave: "936bd63502574a6cc5e29aab3d684ee4feb6d40fbb9db429078ec22c89bffd3a"
+    sha256 cellar: :any_skip_relocation, mojave: "b45abaa94d54b7af194775a90034b2d33389a9f7c72e59c5ed8d6105727c39c0"
   end
 
   depends_on "rust" => :build
 
   on_linux do
     depends_on "pkg-config" => :build
-    depends_on "openssl@1.1"
+    depends_on "openssl@3"
   end
 
   def install
