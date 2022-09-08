7459ff6892367f148cc7b5f83a65676f0f6ea090
Adrian Ho <the.gromgit@gmail.com>
2022-09-08 19:36:50 +0800

    gitless: update 0.8.8_14 bottle.

--- Formula/gitless.rb
+++ Formula/gitless.rb
@@ -6,15 +6,16 @@ class Gitless < Formula
   url "https://files.pythonhosted.org/packages/9c/2e/457ae38c636c5947d603c84fea1cf51b7fcd0c8a5e4a9f2899b5b71534a0/gitless-0.8.8.tar.gz"
   sha256 "590d9636d2ca743fdd972d9bf1f55027c1d7bc2ab1d5e877868807c3359b78ef"
   license "MIT"
-  revision 12
+  revision 14
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gitless"
-    sha256 cellar: :any, mojave: "73123e11516bd4c4cf4f9507a1d4471006207d7f24b2e63ae5453eda3fcd5b21"
+    sha256 cellar: :any, mojave: "0f63a22dcb0fb448ae2e5e1596788767b9c14c8b0df5bdee438ae59bf25afa07"
   end
 
   depends_on "libgit2"
   depends_on "python@3.10"
+  depends_on "six"
 
   uses_from_macos "libffi"
 
@@ -27,14 +28,9 @@ class Gitless < Formula
     sha256 "a785b8d837625e9b61c39108532d95b85274acd679693b71ebb5156848fcf814"
   end
 
-  resource "cached-property" do
-    url "https://files.pythonhosted.org/packages/57/8e/0698e10350a57d46b3bcfe8eff1d4181642fd1724073336079cb13c5cf7f/cached-property-1.5.1.tar.gz"
-    sha256 "9217a59f14a5682da7c4b8829deadbfc194ac22e9908ccf7c8820234e80a1504"
-  end
-
   resource "cffi" do
-    url "https://files.pythonhosted.org/packages/66/6a/98e023b3d11537a5521902ac6b50db470c826c682be6a8c661549cb7717a/cffi-1.14.4.tar.gz"
-    sha256 "1a465cbe98a7fd391d47dce4b8f7e5b921e6cd805ef421d04f5f66ba8f06086c"
+    url "https://files.pythonhosted.org/packages/2b/a8/050ab4f0c3d4c1b8aaa805f70e26e84d0e27004907c5b8ecc1d31815f92a/cffi-1.15.1.tar.gz"
+    sha256 "d400bfb9a37b1351253cb402671cea7e89bdecc294e8016a707f6d1d8ac934f9"
   end
 
   resource "clint" do
@@ -43,23 +39,18 @@ class Gitless < Formula
   end
 
   resource "pycparser" do
-    url "https://files.pythonhosted.org/packages/8c/2d/aad7f16146f4197a11f8e91fb81df177adcc2073d36a17b1491fd09df6ed/pycparser-2.18.tar.gz"
-    sha256 "99a8ca03e29851d96616ad0404b4aad7d9ee16f25c9f9708a11faf2810f7b226"
+    url "https://files.pythonhosted.org/packages/5e/0b/95d387f5f4433cb0f53ff7ad859bd2c6051051cebbb564f139a999ab46de/pycparser-2.21.tar.gz"
+    sha256 "e644fdec12f7872f86c58ff790da456218b10f863970249516d60a5eaca77206"
   end
 
   resource "pygit2" do
-    url "https://files.pythonhosted.org/packages/7e/8c/c162e50ad20c36b457aa97a9d96536fde316d90052fb03fc4ae22a7fe9ea/pygit2-1.9.0.tar.gz"
-    sha256 "c5e8588acad5e32fa0595582571059e6b90ec7c487c58b4e53c2800dcbde44c8"
+    url "https://files.pythonhosted.org/packages/e7/8a/e52a1c8b9878e9d9743089393f8289bb9c8a81eaab722df22df46a38b9e9/pygit2-1.10.0.tar.gz"
+    sha256 "7c751eee88c731b922e4e487ee287e2e40906b2bd32d0bfd2105947f63e867de"
   end
 
   resource "sh" do
     url "https://files.pythonhosted.org/packages/7c/71/199d27d3e7e78bf448bcecae0105a1d5b29173ffd2bbadaa95a74c156770/sh-1.12.14.tar.gz"
     sha256 "b52bf5833ed01c7b5c5fb73a7f71b3d98d48e9b9b8764236237bdc7ecae850fc"
-  end
-
-  resource "six" do
-    url "https://files.pythonhosted.org/packages/16/d8/bc6316cf98419719bd59c91742194c111b6f2e85abac88e496adefaf7afe/six-1.11.0.tar.gz"
-    sha256 "70e8a77beed4562e7f14fe23a786b54f6296e34344c23bc42f07b15018ff98e9"
   end
 
   # Allow to be dependent on pygit2 1.9.0
@@ -83,19 +74,19 @@ end
 
 __END__
 diff --git a/requirements.txt b/requirements.txt
-index 05f190a..5eb025f 100644
+index 05f190a..6777cce 100644
 --- a/requirements.txt
 +++ b/requirements.txt
 @@ -1,6 +1,6 @@
  # make sure to update setup.py
 
 -pygit2==0.28.2  # requires libgit2 0.28
-+pygit2==1.9.0  # requires libgit2 1.4
++pygit2==1.10.0  # requires libgit2 0.28
  clint==0.5.1
  sh==1.12.14;sys_platform!='win32'
  pbs==0.110;sys_platform=='win32'
 diff --git a/setup.py b/setup.py
-index 68a3a87..d1704a8 100755
+index 68a3a87..388ca66 100755
 --- a/setup.py
 +++ b/setup.py
 @@ -68,7 +68,7 @@ setup(
@@ -103,7 +94,7 @@ index 68a3a87..d1704a8 100755
      install_requires=[
        # make sure it matches requirements.txt
 -      'pygit2==0.28.2', # requires libgit2 0.28
-+      'pygit2==1.9.0', # requires libgit2 1.4
++      'pygit2==1.10.0', # requires libgit2 0.28
        'clint>=0.3.6',
        'sh>=1.11' if sys.platform != 'win32' else 'pbs>=0.11'
      ],
