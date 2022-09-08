19fe77160d3a656bcd8e39aa1e2b37274bf41d5a
Adrian Ho <the.gromgit@gmail.com>
2022-09-08 13:01:23 +0800

    zellij: update 0.31.3 bottle.

--- Formula/zellij.rb
+++ Formula/zellij.rb
@@ -1,13 +1,13 @@
 class Zellij < Formula
   desc "Pluggable terminal workspace, with terminal multiplexer as the base feature"
   homepage "https://zellij.dev"
-  url "https://github.com/zellij-org/zellij/archive/v0.31.1.tar.gz"
-  sha256 "8fbe83ec4f4dcb5c157c5755978d00de6dfa2c9790c3788262f23c8d1b75350b"
+  url "https://github.com/zellij-org/zellij/archive/v0.31.3.tar.gz"
+  sha256 "61949cc0c44b11082e6a4347d50910c576b1f131daa054a17ed153a6fd0e8b20"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/zellij"
-    sha256 cellar: :any_skip_relocation, mojave: "63bc6548220ea440606de35b4213264d9556cdb78ed6987c4edd25e576aae8e1"
+    sha256 cellar: :any_skip_relocation, mojave: "7e59e65d633a18c80d6abe4a57fa0b04809b12d1e0a1e0ca469126e4c3decae6"
   end
 
   depends_on "rust" => :build
