55c06372ed26c705d83cc1d98a32c02d97f4518e
Adrian Ho <the.gromgit@gmail.com>
2022-09-08 11:09:51 +0800

    wapm: update 0.5.5 bottle.

--- Formula/wapm.rb
+++ Formula/wapm.rb
@@ -1,8 +1,8 @@
 class Wapm < Formula
   desc "WebAssembly Package Manager (CLI)"
   homepage "https://wapm.io/"
-  url "https://github.com/wasmerio/wapm-cli/archive/v0.5.4.tar.gz"
-  sha256 "783e1187dbcc78021180c1f7d35ea7d165415b736488c72c9ff6ea390d3c4de0"
+  url "https://github.com/wasmerio/wapm-cli/archive/v0.5.5.tar.gz"
+  sha256 "145b91406b55671a88bb238bf8545d9e354da1a7e82fba7739b81fffaabd7d88"
   license "MIT"
   head "https://github.com/wasmerio/wapm-cli.git", branch: "master"
 
@@ -13,7 +13,7 @@ class Wapm < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/wapm"
-    sha256 cellar: :any_skip_relocation, mojave: "4dafb7d02f8bb2308ac40742046df4a6c13244549d66d936402e5b4be4e43af9"
+    sha256 cellar: :any_skip_relocation, mojave: "8af7162a108d6951f94ca0158c32eca3af9e5a4151221b417a9d55d7ec1777fd"
   end
 
   depends_on "rust" => :build
