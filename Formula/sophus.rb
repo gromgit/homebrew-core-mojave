e2c7c97c45b1965344cff1925f55395078f6a055
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-13 19:54:34 +0800

    sophus: no rebuilding required

--- Formula/sophus.rb
+++ Formula/sophus.rb
@@ -4,26 +4,23 @@ class Sophus < Formula
   url "https://github.com/strasdat/Sophus/archive/refs/tags/v22.04.1.tar.gz"
   sha256 "635dc536e7768c91e89d537608226b344eef901b51fbc51c9f220c95feaa0b54"
   license "MIT"
+  revision 1
   head "https://github.com/strasdat/Sophus.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "dfe43682c321e12b9fa343d74f4e9c4649f6fe362365ed1552a3c4f79c2c1a9c"
+    sha256 cellar: :any_skip_relocation, all: "01dc489f067505c1f4cec647efaf37da33a1a7e73ae6043b49e91ca986265e65"
   end
 
   depends_on "cmake" => [:build, :test]
   depends_on "ceres-solver"
   depends_on "eigen"
-  depends_on "fmt"
-
-  on_linux do
-    depends_on "gcc"
-  end
 
   fails_with gcc: "5" # C++17 (ceres-solver dependency)
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args,
-                    "-DBUILD_SOPHUS_EXAMPLES=OFF"
+                    "-DBUILD_SOPHUS_EXAMPLES=OFF",
+                    "-DSOPHUS_USE_BASIC_LOGGING=ON"
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
     (pkgshare/"examples").install "examples/HelloSO3.cpp"
