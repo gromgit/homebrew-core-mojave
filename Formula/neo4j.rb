1250e2673810cf3f992101e2e01d16b994a04677
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-13 19:54:02 +0800

    neo4j: no rebuilding required

--- Formula/neo4j.rb
+++ Formula/neo4j.rb
@@ -1,8 +1,8 @@
 class Neo4j < Formula
   desc "Robust (fully ACID) transactional property graph database"
   homepage "https://neo4j.com/"
-  url "https://neo4j.com/artifact.php?name=neo4j-community-4.4.10-unix.tar.gz"
-  sha256 "869ecb89ce36fe457b4d4c4fcaa48fdf2f2d739c45323bd2c8ccdf09aa11abbf"
+  url "https://neo4j.com/artifact.php?name=neo4j-community-4.4.11-unix.tar.gz"
+  sha256 "28810daac5de4a5d5b77ce2da7d7c3da4c5ccf13288bad885b833c35b96100c8"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -12,7 +12,7 @@ class Neo4j < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "7608f0866129ba0295c59e12e84bea97dfa9613771d29e23d336a81dd06fb309"
+    sha256 cellar: :any_skip_relocation, all: "73bc7b410291367f0e77f403bd3fb49c118a2ff828afbabf9030a9a98dbce383"
   end
 
   depends_on "openjdk@11"
