b427dd192edb771bb198fb86be8e855016dc775d
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-13 19:54:23 +0800

    questdb: no rebuilding required

--- Formula/questdb.rb
+++ Formula/questdb.rb
@@ -1,12 +1,12 @@
 class Questdb < Formula
   desc "Time Series Database"
   homepage "https://questdb.io"
-  url "https://github.com/questdb/questdb/releases/download/6.5/questdb-6.5-no-jre-bin.tar.gz"
-  sha256 "a3cdc050ce178d0373d853d00952d8ec2d21ef952ea4d97281d96223b90c1b9a"
+  url "https://github.com/questdb/questdb/releases/download/6.5.2/questdb-6.5.2-no-jre-bin.tar.gz"
+  sha256 "dd5596331e7f83bdc7587319579c37faa417102d0833a4f4d4448428e8c40d67"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "0044cec5ac1652a665fe16543aee1d9fafb1cc792e5ba23403bea0173ea7bd24"
+    sha256 cellar: :any_skip_relocation, all: "767d2442a1abb8dec166aa0d66411c547b8268bd668a01e8417990af5ed5fdcf"
   end
 
   depends_on "openjdk@11"
