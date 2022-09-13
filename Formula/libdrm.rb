18a6212986477c2e830791db673db7bb9fc13eda
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-13 19:53:46 +0800

    libdrm:

--- Formula/libdrm.rb
+++ Formula/libdrm.rb
@@ -1,8 +1,8 @@
 class Libdrm < Formula
   desc "Library for accessing the direct rendering manager"
   homepage "https://dri.freedesktop.org"
-  url "https://dri.freedesktop.org/libdrm/libdrm-2.4.112.tar.xz"
-  sha256 "00b07710bd09b35cd8d80eaf4f4497fe27f4becf467a9830f1f5e8324f8420ff"
+  url "https://dri.freedesktop.org/libdrm/libdrm-2.4.113.tar.xz"
+  sha256 "7fd7eb2967f63beb4606f22d50e277d993480d05ef75dd88a9bd8e677323e5e1"
   license "MIT"
 
   livecheck do
@@ -11,7 +11,7 @@ class Libdrm < Formula
   end
 
   bottle do
-    sha256 x86_64_linux: "1797ab0bedd39323a22eff6b0f970736fabeb4ba47a610321cfda401c08ce6d6"
+    sha256 x86_64_linux: "d4f9c979e3a78430d1679c592cf2641ce96851596fb119c36b3fe268d9812bb8"
   end
 
   depends_on "docutils" => :build
@@ -22,11 +22,9 @@ class Libdrm < Formula
   depends_on :linux
 
   def install
-    mkdir "build" do
-      system "meson", *std_meson_args, "-Dcairo-tests=false", ".."
-      system "ninja"
-      system "ninja", "install"
-    end
+    system "meson", "setup", "build", "-Dcairo-tests=disabled", "-Dvalgrind=disabled", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
   end
 
   test do
