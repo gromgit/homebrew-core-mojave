1b65e9a7b212ce7faf673e0ee7d9651285631b70
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-13 19:53:26 +0800

    git-bug: no rebuilding required

--- Formula/git-bug.rb
+++ Formula/git-bug.rb
@@ -8,9 +8,13 @@ class GitBug < Formula
   head "https://github.com/MichaelMure/git-bug.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-bug"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "d2f42d1693e7581c9a36c5f9ca6065c9e142f456b96520a0d3cac5ec8a8e2656"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "beeb321dc338263445c5a092cee6d627679ef38f65ef449cbcbf1d3dee53983b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7477b094e81aa878baf489654c8b131c6d8ebd40222b41769481e298089ba019"
+    sha256 cellar: :any_skip_relocation, monterey:       "cc519ce7c73d26de779c4eb14118e0dc01c160d463e42b54c380e629f5e9cd5c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "26ead98df2569b14356aac45755b68397dceca6a0dc2cdb6ec00f1a4926fc669"
+    sha256 cellar: :any_skip_relocation, catalina:       "60c08117214d4d8657e67f182d1380b291882944d69c54c9adaa0f8140e28993"
+    sha256 cellar: :any_skip_relocation, mojave:         "6184e21ba0f2c4c28722dd7276004f2f430e02e4f2cf56462b4914d08df6f2f0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "234a0d2749668c523e59bf359cdb0484d984fc4230cbe72c64c0ff585eaef6cc"
   end
 
   depends_on "go" => :build
@@ -21,8 +25,15 @@ class GitBug < Formula
 
     man1.install Dir["doc/man/*.1"]
     doc.install Dir["doc/md/*.md"]
-    bash_completion.install "misc/bash_completion/git-bug"
-    zsh_completion.install "misc/zsh_completion/git-bug" => "_git-bug"
+
+    if build.stable?
+      bash_completion.install "misc/bash_completion/git-bug"
+      zsh_completion.install "misc/zsh_completion/git-bug" => "_git-bug"
+    else
+      bash_completion.install "misc/completion/bash/git-bug"
+      zsh_completion.install "misc/completion/zsh/git-bug" => "_git-bug"
+      fish_completion.install "misc/completion/fish/git-bug" => "git-bug.fish"
+    end
   end
 
   test do
