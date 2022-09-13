be5d763ae7ac3bd57cccb0772ebec20b27c0719c
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-13 19:54:01 +0800

    needle:

--- Formula/needle.rb
+++ Formula/needle.rb
@@ -1,24 +1,35 @@
 class Needle < Formula
   desc "Compile-time safe Swift dependency injection framework with real code"
   homepage "https://github.com/uber/needle"
+  # TODO: Check if a GitHub tarball is sufficient here.
   url "https://github.com/uber/needle.git",
-      tag:      "v0.17.2",
-      revision: "6a2d5e25cd3c77ddfa57835e991469db791c4744"
+      tag:      "v0.19.0",
+      revision: "9d15211866bd307c7bfef789fe77ce1e97aeb978"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "1929735212f6d3ee84ac3019cd82d4b76e352f780e68f22e381a44e687ae59a0"
-    sha256 cellar: :any, big_sur:       "efd84a4bd0890a28c3b9e7d2d9ac9b84a8f4d6bb6a0586380f4d653a6092e52a"
-    sha256 cellar: :any, catalina:      "f45fa77b9e00be408206fc2cf945f41ca3f4661bbb06c8d2aadd015f4d75dfdd"
+    sha256 cellar: :any, arm64_monterey: "0cdbc53949504cdb3d0d723af00bf452731c3891c6eca10a89edfb9871c7bd08"
+    sha256 cellar: :any, arm64_big_sur:  "97cb4f17a1642708c59a07eca04d36b377d2a1f0ca0a71aa595b66935875e1a5"
+    sha256 cellar: :any, monterey:       "f99d8fc72e9588b310cc7e44895722214edccc536b2a1646bcbd35d63425675d"
+    sha256 cellar: :any, big_sur:        "ab1412e10cbe5098cba3bd3729dd9c2189c0f9a24edb865b76e2c9d0174ba85e"
   end
 
-  depends_on xcode: ["12.2", :build]
+  depends_on xcode: ["13.0", :build] # Swift 5.5+
   depends_on :macos
 
   def install
-    system "make", "install", "BINARY_FOLDER_PREFIX=#{prefix}"
+    # Avoid building a universal binary.
+    swift_build_flags = (buildpath/"Makefile").read[/^SWIFT_BUILD_FLAGS=(.*)$/, 1].split
+    %w[--arch arm64 x86_64].each do |flag|
+      swift_build_flags.delete(flag)
+    end
+
+    system "make", "install", "BINARY_FOLDER_PREFIX=#{prefix}", "SWIFT_BUILD_FLAGS=#{swift_build_flags.join(" ")}"
     bin.install "./Generator/bin/needle"
     libexec.install "./Generator/bin/lib_InternalSwiftSyntaxParser.dylib"
+
+    # lib_InternalSwiftSyntaxParser is taken from Xcode, so it's a universal binary.
+    deuniversalize_machos(libexec/"lib_InternalSwiftSyntaxParser.dylib")
   end
 
   test do
