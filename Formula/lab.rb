c6f86a8cfa500b398ef70b31b9c3da5385ae430a
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-13 19:53:45 +0800

    lab: no rebuilding required

--- Formula/lab.rb
+++ Formula/lab.rb
@@ -7,9 +7,13 @@ class Lab < Formula
   head "https://github.com/zaquestion/lab.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lab"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "eb163e34d1ee4ceb50e0aac5641b6e83ac5eef2f7510920bf9f408e424fc52ad"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4e6c5f7468bcdda2dd60824e289016e574356a3d12687200f483a3511813a96a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8fc0866043b2825d9c6cd55768c6dbf6a5252ee81cf08d1ec972f2f0c63e75f9"
+    sha256 cellar: :any_skip_relocation, monterey:       "bdf4b6b4eaa8cd5a867bbaa1c569896052687f777df2fdddc260ca6328e236bd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cf2122351ee8c417e167b9266f396d71c1bf076376920b00bfabea8b66d36be5"
+    sha256 cellar: :any_skip_relocation, catalina:       "831ebd5e87cfe24b4867a3a08b4c3714a050cb100ef4138d338c3d4e947ec026"
+    sha256 cellar: :any_skip_relocation, mojave:         "50e3df561e2df7c25b663adb7428cff384adde8f58129d1848b674200c132522"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "103f4ef8df39bd5fef22d6867c010fe6369da47c467bf67924aaf07f33464841"
   end
 
   depends_on "go" => :build
@@ -17,12 +21,8 @@ class Lab < Formula
   def install
     ldflags = "-X main.version=#{version} -s -w"
     system "go", "build", *std_go_args(ldflags: ldflags)
-    output = Utils.safe_popen_read("#{bin}/lab", "completion", "bash")
-    (bash_completion/"lab").write output
-    output = Utils.safe_popen_read("#{bin}/lab", "completion", "zsh")
-    (zsh_completion/"_lab").write output
-    output = Utils.safe_popen_read("#{bin}/lab", "completion", "fish")
-    (fish_completion/"lab.fish").write output
+
+    generate_completions_from_executable(bin/"lab", "completion")
   end
 
   test do
