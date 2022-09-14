bbfae069a3cf2f0f6a527f4a1f617ad62c1dbdf7
Adrian Ho <the.gromgit@gmail.com>
2022-09-14 23:44:44 +0800

    open-mpi: update 4.1.4_2 bottle.

--- Formula/open-mpi.rb
+++ Formula/open-mpi.rb
@@ -4,7 +4,7 @@ class OpenMpi < Formula
   url "https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.4.tar.bz2"
   sha256 "92912e175fd1234368c8730c03f4996fe5942e7479bb1d10059405e7f2b3930d"
   license "BSD-3-Clause"
-  revision 1
+  revision 2
 
   livecheck do
     url :homepage
@@ -13,8 +13,7 @@ class OpenMpi < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/open-mpi"
-    rebuild 1
-    sha256 mojave: "c475be6de3a741fca41d47b14e020982ae69fd52cdfb914beb5055e220f4b9e0"
+    sha256 mojave: "a3116fc51ad82efe30d1d461bb909530488805740111ff4d6016cfd7441247b9"
   end
 
   head do
@@ -40,12 +39,14 @@ class OpenMpi < Formula
       oshmem/tools/oshmem_info/param.c
     ]
 
-    inreplace inreplace_files, "OMPI_CXX_ABSOLUTE", "\"#{ENV.cxx}\""
+    cxx = OS.linux? ? "g++" : ENV.cxx
+    inreplace inreplace_files, "OMPI_CXX_ABSOLUTE", "\"#{cxx}\""
 
     inreplace_files << "orte/tools/orte-info/param.c" unless build.head?
     inreplace_files << "opal/mca/pmix/pmix3x/pmix/src/tools/pmix_info/support.c" unless build.head?
 
-    inreplace inreplace_files, /(OPAL|PMIX)_CC_ABSOLUTE/, "\"#{ENV.cc}\""
+    cc = OS.linux? ? "gcc" : ENV.cc
+    inreplace inreplace_files, /(OPAL|PMIX)_CC_ABSOLUTE/, "\"#{cc}\""
 
     ENV.cxx11
     ENV.runtime_cpu_detection
