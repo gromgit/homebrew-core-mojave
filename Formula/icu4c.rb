b988003a723fe09e5055ce41af3fd09d33eef516
Adrian Ho <the.gromgit@gmail.com>
2022-09-14 22:23:19 +0800

    icu4c: update 71.1 bottle.

--- Formula/icu4c.rb
+++ Formula/icu4c.rb
@@ -1,9 +1,9 @@
 class Icu4c < Formula
   desc "C/C++ and Java libraries for Unicode and globalization"
   homepage "https://icu.unicode.org/home"
-  url "https://github.com/unicode-org/icu/releases/download/release-70-1/icu4c-70_1-src.tgz"
-  version "70.1"
-  sha256 "8d205428c17bf13bb535300669ed28b338a157b1c01ae66d31d0d3e2d47c3fd5"
+  url "https://github.com/unicode-org/icu/releases/download/release-71-1/icu4c-71_1-src.tgz"
+  version "71.1"
+  sha256 "67a7e6e51f61faf1306b6935333e13b2c48abd8da6d2f46ce6adca24b1e21ebf"
   license "ICU"
 
   livecheck do
@@ -16,8 +16,7 @@ class Icu4c < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/icu4c"
-    rebuild 1
-    sha256 cellar: :any, mojave: "1587998b89382f5c24b3479e3b4e27aa5778e465e2895c2aa21c389a1a5c267c"
+    sha256 cellar: :any, mojave: "0982d896542506e267de6aec96a9e4b9c886ab630c0dac12388a8fd31997c506"
   end
 
   keg_only :provided_by_macos, "macOS provides libicucore.dylib (but nothing else)"
