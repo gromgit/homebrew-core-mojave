ddace881e59d94a02b8a017a8dab8d5fa2da0eb5
Adrian Ho <the.gromgit@gmail.com>
2022-09-16 06:35:32 +0800

    fdupes: update 2.2.1 bottle.

--- Formula/fdupes.rb
+++ Formula/fdupes.rb
@@ -1,14 +1,14 @@
 class Fdupes < Formula
   desc "Identify or delete duplicate files"
   homepage "https://github.com/adrianlopezroche/fdupes"
-  url "https://github.com/adrianlopezroche/fdupes/releases/download/v2.2.0/fdupes-2.2.0.tar.gz"
-  sha256 "174e488c21c3d0475228fe95c268c7f518d92f2732bdf36d1964dc285538e1ac"
+  url "https://github.com/adrianlopezroche/fdupes/releases/download/v2.2.1/fdupes-2.2.1.tar.gz"
+  sha256 "846bb79ca3f0157856aa93ed50b49217feb68e1b35226193b6bc578be0c5698d"
   license "MIT"
   version_scheme 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fdupes"
-    sha256 cellar: :any, mojave: "703e85847b01cda69fd5ac65003c6b3a88126efbe457d922d1bd72793daceab3"
+    sha256 cellar: :any, mojave: "b8df60268535843d8434d014498625ca96f246c5749b8e5c38a26aada4c11959"
   end
 
   depends_on "pcre2"
