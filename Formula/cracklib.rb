ebfe614bdabd6db6bc4ea37d7a84969dad32187c
Adrian Ho <the.gromgit@gmail.com>
2022-09-16 02:38:44 +0800

    cracklib: update 2.9.8 bottle.

--- Formula/cracklib.rb
+++ Formula/cracklib.rb
@@ -1,10 +1,9 @@
 class Cracklib < Formula
   desc "LibCrack password checking library"
   homepage "https://github.com/cracklib/cracklib"
-  url "https://github.com/cracklib/cracklib/releases/download/v2.9.7/cracklib-2.9.7.tar.bz2"
-  sha256 "fe82098509e4d60377b998662facf058dc405864a8947956718857dbb4bc35e6"
+  url "https://github.com/cracklib/cracklib/releases/download/v2.9.8/cracklib-2.9.8.tar.bz2"
+  sha256 "1f9d34385ea3aa7cd7c07fa388dc25810aea9d3c33e260c713a3a5873d70e386"
   license "LGPL-2.1"
-  revision 1
 
   livecheck do
     url :stable
@@ -12,21 +11,14 @@ class Cracklib < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "6c9f2d8a461d48c833fb9e3f1df5f394efb5512bb851f113aee02be62347efb2"
-    sha256 cellar: :any,                 arm64_big_sur:  "ffc09f71e17accfb3b76513b8fe6220aa683bfce4132e182eaa8e47993f9d3df"
-    sha256 cellar: :any,                 monterey:       "9ecd3347d1d4020526434c540c6afda0b71863d0b7edf1ba3d8c61fcebaae38c"
-    sha256 cellar: :any,                 big_sur:        "308feca305163e5333e84e3fbbfa497c0b483b13f99ed62971e1d503dd137150"
-    sha256 cellar: :any,                 catalina:       "6b22a44df4e1602edc9d248bd1ef58a638c1d04cfdfcbc745f331d05ea91d8ac"
-    sha256 cellar: :any,                 mojave:         "cdf8e3240e77e574df95271024c7b260ef5eafea27dfa6f6188c1a686dd1b9be"
-    sha256 cellar: :any,                 high_sierra:    "210b950eee847fdccdb388c14d87eb425182282e581187302daa91dfa166fb78"
-    sha256 cellar: :any,                 sierra:         "3e74c66c810e5faa99833fc89d375945d0059ddc4b13b5f57128de70cff9dfef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "453522bad648a4fe745e92c977ba9b5810c1246aeb9ba12efa81ac8680fc137a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cracklib"
+    sha256 cellar: :any, mojave: "7f08c6e608ef75298262482fa39917b9dbce2defd861b54ef82c3c64046aa84c"
   end
 
   depends_on "gettext"
 
   resource "cracklib-words" do
-    url "https://github.com/cracklib/cracklib/releases/download/v2.9.7/cracklib-words-2.9.7.bz2"
+    url "https://github.com/cracklib/cracklib/releases/download/v2.9.8/cracklib-words-2.9.8.bz2"
     sha256 "ec25ac4a474588c58d901715512d8902b276542b27b8dd197e9c2ad373739ec4"
   end
 
