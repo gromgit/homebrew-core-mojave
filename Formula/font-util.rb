cd88dff6e9bc483625b532102229aeaf61b8f92f
Adrian Ho <the.gromgit@gmail.com>
2022-09-16 07:05:35 +0800

    font-util: update 1.3.3_1 bottle.

--- Formula/font-util.rb
+++ Formula/font-util.rb
@@ -4,27 +4,26 @@ class FontUtil < Formula
   url "https://www.x.org/archive/individual/font/font-util-1.3.3.tar.xz"
   sha256 "e791c890779c40056ab63aaed5e031bb6e2890a98418ca09c534e6261a2eebd2"
   license "MIT"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/font-util"
-    sha256 cellar: :any_skip_relocation, mojave: "44118f071703b1c8068848c995bf10bfde0486c9d7208a310cff30fbb3cd7e9f"
+    sha256 cellar: :any_skip_relocation, mojave: "2aaf71fcbffb8a9e64a96d613cbb30438929972228188fe51cb0079b013f57d9"
   end
 
   depends_on "pkg-config" => [:build, :test]
   depends_on "util-macros" => :build
 
   def install
-    args = %W[
-      --prefix=#{prefix}
+    args = std_configure_args + %W[
       --sysconfdir=#{etc}
       --localstatedir=#{var}
-      --disable-dependency-tracking
-      --disable-silent-rules
+      --with-fontrootdir=#{HOMEBREW_PREFIX}/share/fonts/X11
     ]
 
     system "./configure", *args
     system "make"
-    system "make", "install"
+    system "make", "fontrootdir=#{share}/fonts/X11", "install"
   end
 
   def post_install
