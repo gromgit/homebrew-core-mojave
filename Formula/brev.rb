4ec07243fec674889fb824da831cfb76fec146fa
Adrian Ho <the.gromgit@gmail.com>
2022-09-15 22:12:03 +0800

    brev: update 0.6.104 bottle.

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -1,8 +1,8 @@
 class Brev < Formula
   desc "CLI tool for managing workspaces provided by brev.dev"
   homepage "https://docs.brev.dev"
-  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.98.tar.gz"
-  sha256 "3917aa3c2ad622294e901a09bcea5b2005ba69100b2729e046d5ea3b0b56df78"
+  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.104.tar.gz"
+  sha256 "34decf70f69fd3ca6c140d734a7ddf8a29ce3859019a0dcd6209c31099ee37f2"
   license "MIT"
 
   livecheck do
@@ -12,7 +12,7 @@ class Brev < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/brev"
-    sha256 cellar: :any_skip_relocation, mojave: "89c8ca8994f2c67b29d02569a447369a1ef34f59673ca9d5f76ecb224ef71c95"
+    sha256 cellar: :any_skip_relocation, mojave: "10360f57c7f08a92be8b8c6aa71a272dff9ddc2bd1d55dc43aa1770875962887"
   end
 
   # Required latest gvisor.dev/gvisor/pkg/gohacks instead of inet.af/netstack/gohacks
