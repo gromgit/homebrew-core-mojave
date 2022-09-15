575179f9fb1401bb2cc93ca03fb85c722bc5a2ca
Adrian Ho <the.gromgit@gmail.com>
2022-09-16 02:01:18 +0800

    clisp: update 2.49.92_1 bottle.

--- Formula/clisp.rb
+++ Formula/clisp.rb
@@ -2,6 +2,7 @@ class Clisp < Formula
   desc "GNU CLISP, a Common Lisp implementation"
   homepage "https://clisp.sourceforge.io/"
   license "GPL-2.0-or-later"
+  revision 1
   head "https://gitlab.com/gnu-clisp/clisp.git", branch: "master"
 
   stable do
@@ -22,17 +23,13 @@ class Clisp < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "fd0612e62737575c94b319dfeaf34a7ef598422199313cc88bf980a3e9dca8c7"
-    sha256 cellar: :any, arm64_big_sur:  "47b8617bc92bfcef6ce59ddb16ce923c49807d342c6086c41130def364f68bcd"
-    sha256 cellar: :any, monterey:       "a681906d5fc0756209090b467e642c6d0ff899c333f9e5576e539a5347775a5e"
-    sha256 cellar: :any, big_sur:        "ddb6cb44d70ff18bd12bd186679112e8f4fa1f33d4849697fa2564c51195b5fe"
-    sha256 cellar: :any, catalina:       "2011ba42953cd6363aab0b23154947d364197fb28305efdb08ebe4640447d311"
-    sha256 cellar: :any, mojave:         "2e74f9680f7b060a0d6a71b2fdae0d00fa6e6009af104f9fb7839368b109ae1f"
-    sha256               x86_64_linux:   "80d7aa1e12fb3af6a08d6a5f53c5696cfdd0d277e54fdfb18108b13c4cee55f2"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/clisp"
+    sha256 cellar: :any, mojave: "46600f30167ba9f495d9900a76495fc71072a8593d46fe20ec3641852e9d5d0f"
   end
 
   depends_on "libsigsegv"
   depends_on "readline"
+  uses_from_macos "libxcrypt"
 
   def install
     system "./configure", *std_configure_args,
