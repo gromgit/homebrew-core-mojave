68901d10d45785851aad8b179061d94f29e374d7
Adrian Ho <the.gromgit@gmail.com>
2022-09-16 05:38:37 +0800

    earthly: update 0.6.23 bottle.

--- Formula/earthly.rb
+++ Formula/earthly.rb
@@ -2,14 +2,14 @@ class Earthly < Formula
   desc "Build automation tool for the container era"
   homepage "https://earthly.dev/"
   url "https://github.com/earthly/earthly.git",
-      tag:      "v0.6.22",
-      revision: "1c8bcfbab64b9b52302ef63c897e6b989b88a794"
+      tag:      "v0.6.23",
+      revision: "d6f689104aaaf38178a3f58e1a969eacded7341d"
   license "MPL-2.0"
   head "https://github.com/earthly/earthly.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/earthly"
-    sha256 cellar: :any_skip_relocation, mojave: "679c63585021b25ea97d07f1ef00add9184c2442b75b22efda08e8c53a50d8c4"
+    sha256 cellar: :any_skip_relocation, mojave: "ab31097f38932c26e5f3c4682fc2683dda796cbe5f3dc6011c3f1649ddb77d44"
   end
 
   depends_on "go" => :build
@@ -25,10 +25,7 @@ class Earthly < Formula
     tags = "dfrunmount dfrunsecurity dfsecrets dfssh dfrunnetwork"
     system "go", "build", "-tags", tags, *std_go_args(ldflags: ldflags), "./cmd/earthly"
 
-    bash_output = Utils.safe_popen_read("#{bin}/earthly", "bootstrap", "--source", "bash")
-    (bash_completion/"earthly").write bash_output
-    zsh_output = Utils.safe_popen_read("#{bin}/earthly", "bootstrap", "--source", "zsh")
-    (zsh_completion/"_earthly").write zsh_output
+    generate_completions_from_executable(bin/"earthly", "bootstrap", "--source", shells: [:bash, :zsh])
   end
 
   test do
