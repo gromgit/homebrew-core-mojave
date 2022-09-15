37c46f341d13a56a999a070192993b0f3f3df436
Adrian Ho <the.gromgit@gmail.com>
2022-09-16 01:18:56 +0800

    circumflex: update 2.4 bottle.

--- Formula/circumflex.rb
+++ Formula/circumflex.rb
@@ -1,13 +1,13 @@
 class Circumflex < Formula
   desc "Hacker News in your terminal"
   homepage "https://github.com/bensadeh/circumflex"
-  url "https://github.com/bensadeh/circumflex/archive/refs/tags/2.3.tar.gz"
-  sha256 "ba898331d76e266ae3af4e5eab5c6af30ef004c133104fec3e0bbb9497f5c70a"
+  url "https://github.com/bensadeh/circumflex/archive/refs/tags/2.4.tar.gz"
+  sha256 "1231c78eb8bd6784be3d90059b132a14932b265ec74ae6d719aa7c9679f15bdc"
   license "AGPL-3.0-only"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/circumflex"
-    sha256 cellar: :any_skip_relocation, mojave: "174d92e1ef1349fba1799857402ec7ac471ab7003002ffa7cf2d865c7d669a80"
+    sha256 cellar: :any_skip_relocation, mojave: "fb32a484cdd1e46434971279f160e47705bb193683177939352984324f2ad653"
   end
 
   depends_on "go" => :build
