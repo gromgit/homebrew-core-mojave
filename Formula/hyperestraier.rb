25672e748c7e215a0d5b3306c88dafb9b451566f
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-21 17:45:09 +0800

    hyperestraier: no rebuilding required

--- Formula/hyperestraier.rb
+++ Formula/hyperestraier.rb
@@ -11,21 +11,23 @@ class Hyperestraier < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 monterey:     "e97a30177dd2112ae7ef36b2165213874faa7a4ef1e40dd6433ccfdd3eae7ac2"
-    sha256 cellar: :any,                 big_sur:      "98338e8f67c7cba1df436607f09415415e39a38f695805ddd94720326eae9212"
-    sha256 cellar: :any,                 catalina:     "0304cb2db3ed4e35c12ccaac0251ea19f7fd4c0f2a5b9f3ffad0f201f7f4357c"
-    sha256 cellar: :any,                 mojave:       "4275d3ad552f225c5b686532d6cc2703481284fa73eaf3c5b35bc5551dc95761"
-    sha256 cellar: :any,                 high_sierra:  "f0eeb8e60dc0639fdbf5c15fc22c954a627b5136525021706876972b5bfdd816"
-    sha256 cellar: :any,                 sierra:       "c6018d888e9a4f03546f1727d9ec7b6d7eb6a87fc4f6755667bdafa71929aca7"
-    sha256 cellar: :any,                 el_capitan:   "c90ef2d3ccac1af3247726697be33748ec53df85a98af4611b6dbfc9a8dca0c7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "67da1265df5336838e42f563b8b90041d83d848739bf7972950de444cef78650"
+    sha256 cellar: :any,                 arm64_monterey: "4c7b331c57a8dc648150b89336b93cd18c172bce495a2fe5f7e351037d640305"
+    sha256 cellar: :any,                 arm64_big_sur:  "77cc656687e473f7ae65dac6d4efb0ed75538f22890a3f48251791c1947487d0"
+    sha256 cellar: :any,                 monterey:       "e97a30177dd2112ae7ef36b2165213874faa7a4ef1e40dd6433ccfdd3eae7ac2"
+    sha256 cellar: :any,                 big_sur:        "98338e8f67c7cba1df436607f09415415e39a38f695805ddd94720326eae9212"
+    sha256 cellar: :any,                 catalina:       "0304cb2db3ed4e35c12ccaac0251ea19f7fd4c0f2a5b9f3ffad0f201f7f4357c"
+    sha256 cellar: :any,                 mojave:         "4275d3ad552f225c5b686532d6cc2703481284fa73eaf3c5b35bc5551dc95761"
+    sha256 cellar: :any,                 high_sierra:    "f0eeb8e60dc0639fdbf5c15fc22c954a627b5136525021706876972b5bfdd816"
+    sha256 cellar: :any,                 sierra:         "c6018d888e9a4f03546f1727d9ec7b6d7eb6a87fc4f6755667bdafa71929aca7"
+    sha256 cellar: :any,                 el_capitan:     "c90ef2d3ccac1af3247726697be33748ec53df85a98af4611b6dbfc9a8dca0c7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "67da1265df5336838e42f563b8b90041d83d848739bf7972950de444cef78650"
   end
 
+  depends_on "pkg-config" => :build
   depends_on "qdbm"
 
   def install
-    system "./configure", "--disable-debug", "--prefix=#{prefix}",
-                          "--disable-dependency-tracking"
+    system "./configure", *std_configure_args
     if OS.mac?
       system "make", "mac"
       system "make", "check-mac"
