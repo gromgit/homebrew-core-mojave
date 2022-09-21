4545df9d5f248ff1febdef05a1aead926fa4d80c
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-21 17:46:24 +0800

    mplayershell: no rebuilding required

--- Formula/mplayershell.rb
+++ Formula/mplayershell.rb
@@ -7,13 +7,15 @@ class Mplayershell < Formula
   head "https://github.com/donmelton/MPlayerShell.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, monterey:    "394a7fd5b3beef51cc57058e2210cccfd9fda7ae045fba2551c1e62149bae6df"
-    sha256 cellar: :any_skip_relocation, big_sur:     "4d08f027c84780edc46b13b1e45a8255de0ec6a35798a1ea5230ef8cb4396e13"
-    sha256 cellar: :any_skip_relocation, catalina:    "09cfdf5d08af35a3be96623c6535fece3acfbc60cf81247b118778cb2b68acc3"
-    sha256 cellar: :any_skip_relocation, mojave:      "1be2bb2a8eccce7fa190b85af6e67fb7fe36393c32a8295852af0e6e390b6ee9"
-    sha256 cellar: :any_skip_relocation, high_sierra: "c0b558e0508d80fe05a1d9617b7aa1986066c54bc0a3da585631eb406da5eb93"
-    sha256 cellar: :any_skip_relocation, sierra:      "e9377eaebb65903037105bf3ed6ee301a182452791e9daeaadd08ccb732d9d1b"
-    sha256 cellar: :any_skip_relocation, el_capitan:  "ae4c1c9d069053afa7e71867256b577e23bd0dec87a90ccab2ebeab089a3634b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3003edef26c3863115869954941cff51ef7976b31ddef7130ea8931073011fbb"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ab5dcc40124f4b2e1e3971050548e96bf3e652fbd4c682a701c0d3549ced4c21"
+    sha256 cellar: :any_skip_relocation, monterey:       "394a7fd5b3beef51cc57058e2210cccfd9fda7ae045fba2551c1e62149bae6df"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4d08f027c84780edc46b13b1e45a8255de0ec6a35798a1ea5230ef8cb4396e13"
+    sha256 cellar: :any_skip_relocation, catalina:       "09cfdf5d08af35a3be96623c6535fece3acfbc60cf81247b118778cb2b68acc3"
+    sha256 cellar: :any_skip_relocation, mojave:         "1be2bb2a8eccce7fa190b85af6e67fb7fe36393c32a8295852af0e6e390b6ee9"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "c0b558e0508d80fe05a1d9617b7aa1986066c54bc0a3da585631eb406da5eb93"
+    sha256 cellar: :any_skip_relocation, sierra:         "e9377eaebb65903037105bf3ed6ee301a182452791e9daeaadd08ccb732d9d1b"
+    sha256 cellar: :any_skip_relocation, el_capitan:     "ae4c1c9d069053afa7e71867256b577e23bd0dec87a90ccab2ebeab089a3634b"
   end
 
   depends_on xcode: :build
