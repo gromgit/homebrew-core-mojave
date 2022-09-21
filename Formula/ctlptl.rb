626aeee09987179e6b8c572d110a784e0499cd28
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 07:53:25 +0800

    ctlptl: update 0.8.8 bottle.

--- Formula/ctlptl.rb
+++ Formula/ctlptl.rb
@@ -1,13 +1,13 @@
 class Ctlptl < Formula
   desc "Making local Kubernetes clusters fun and easy to set up"
   homepage "https://github.com/tilt-dev/ctlptl"
-  url "https://github.com/tilt-dev/ctlptl/archive/v0.8.6.tar.gz"
-  sha256 "19a3af55614da8594a56e954f0085700dd44f1c1c09f860f7dbee95d1f256306"
+  url "https://github.com/tilt-dev/ctlptl/archive/v0.8.8.tar.gz"
+  sha256 "09191d6902115c2b4d463695224de87eb490e1865eb27583ec27e64d9a9ca12e"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ctlptl"
-    sha256 cellar: :any_skip_relocation, mojave: "5d0d7d4305d5486d3d0857cc35f1da80a63b10c68310d980a22326e72fa56b2f"
+    sha256 cellar: :any_skip_relocation, mojave: "c97aa7a6c80d182c9cfef36f509e9220ac3e08b7baca8926e71d6de6dce40d18"
   end
 
   depends_on "go" => :build
@@ -20,17 +20,7 @@ class Ctlptl < Formula
     ]
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/ctlptl"
 
-    # Install bash completion
-    output = Utils.safe_popen_read(bin/"ctlptl", "completion", "bash")
-    (bash_completion/"ctlptl").write output
-
-    # Install zsh completion
-    output = Utils.safe_popen_read(bin/"ctlptl", "completion", "zsh")
-    (zsh_completion/"_ctlptl").write output
-
-    # Install fish completion
-    output = Utils.safe_popen_read(bin/"ctlptl", "completion", "fish")
-    (fish_completion/"ctlptl.fish").write output
+    generate_completions_from_executable(bin/"ctlptl", "completion")
   end
 
   test do
