0e74515757843560fd39c2da4fe2b5e5b1ad1ad7
Adrian Ho <aho+vm-mojave@03s.net>
2022-09-21 17:42:14 +0800

    afl-fuzz: no rebuilding required

--- Formula/afl-fuzz.rb
+++ Formula/afl-fuzz.rb
@@ -21,6 +21,8 @@ class AflFuzz < Formula
     sha256 x86_64_linux: "f5d1ccfa91754283abf27bc096aeb33ac130dfab750b9e3547cb59a584c5b6c7"
   end
 
+  deprecate! date: "2022-09-17", because: :repo_archived
+
   def install
     system "make", "PREFIX=#{prefix}", "AFL_NO_X86=1"
     system "make", "install", "PREFIX=#{prefix}", "AFL_NO_X86=1"
