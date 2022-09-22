7647e3dbdcc195bd9dfbcb6cee7861d097f0addb
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 10:39:09 +0800

    gmailctl: update 0.10.5 bottle.

--- Formula/gmailctl.rb
+++ Formula/gmailctl.rb
@@ -1,20 +1,22 @@
 class Gmailctl < Formula
   desc "Declarative configuration for Gmail filters"
   homepage "https://github.com/mbrt/gmailctl"
-  url "https://github.com/mbrt/gmailctl/archive/v0.10.4.tar.gz"
-  sha256 "779752e853d5f59d73e37a74681d52ba6f98ecb54c31fe266d4b85bedafc7774"
+  url "https://github.com/mbrt/gmailctl/archive/v0.10.5.tar.gz"
+  sha256 "53410051ad246e452878d7b9de241a7173f588ec2b68f02adb40e7ec01b16912"
   license "MIT"
   head "https://github.com/mbrt/gmailctl.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gmailctl"
-    sha256 cellar: :any_skip_relocation, mojave: "5ce0bc7ece68745c7ce563dbbeb7634a59dbdb9bed1fddf2565f6d6248db2f4d"
+    sha256 cellar: :any_skip_relocation, mojave: "2fdd1fe03d894657277097f8806403379ea03790f13393b0ee70a017f2642942"
   end
 
   depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "cmd/gmailctl/main.go"
+
+    generate_completions_from_executable(bin/"gmailctl", "completion")
   end
 
   test do
