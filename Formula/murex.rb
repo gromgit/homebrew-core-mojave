f64027dbaa35eb921e42cf3ccf8ebe475c570843
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 20:51:36 +0800

    murex: update 2.11.2030 bottle.

--- Formula/murex.rb
+++ Formula/murex.rb
@@ -1,14 +1,14 @@
 class Murex < Formula
   desc "Bash-like shell designed for greater command-line productivity and safer scripts"
   homepage "https://murex.rocks"
-  url "https://github.com/lmorg/murex/archive/v2.10.2400.tar.gz"
-  sha256 "4503e20d144cdadf60e01b97d420bc3ab7d35b3c079712fd3e37477bdeac67b3"
+  url "https://github.com/lmorg/murex/archive/v2.11.2030.tar.gz"
+  sha256 "5d7dadc2d48bde20fe2239d8087612eccaa310b0f88863bdae2288b30388dff7"
   license "GPL-2.0-only"
   head "https://github.com/lmorg/murex.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/murex"
-    sha256 cellar: :any_skip_relocation, mojave: "006e388fc36e5ee0770f8d4681e5a90cedb073101034e966d63cac5f7f71471e"
+    sha256 cellar: :any_skip_relocation, mojave: "dd49e040344ee62d0f7565ea1c07465229a8e63478b531241eb55604268765dd"
   end
 
   depends_on "go" => :build
