8292f368819f124cfb1a669c540c5821017951c5
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 18:06:18 +0800

    ld-find-code-refs: update 2.7.0 bottle.

--- Formula/ld-find-code-refs.rb
+++ Formula/ld-find-code-refs.rb
@@ -1,14 +1,14 @@
 class LdFindCodeRefs < Formula
   desc "Build tool for sending feature flag code references to LaunchDarkly"
   homepage "https://github.com/launchdarkly/ld-find-code-refs"
-  url "https://github.com/launchdarkly/ld-find-code-refs/archive/v2.5.8.tar.gz"
-  sha256 "1abfce59d1676d858672bf8f3924e2f9ace826411bad95d8171aa5e67b7f9f35"
+  url "https://github.com/launchdarkly/ld-find-code-refs/archive/v2.7.0.tar.gz"
+  sha256 "4793197ded1d5747be765aab04d7c7f9fb47f94cad591cb778a9b1ef221b1ae0"
   license "Apache-2.0"
   head "https://github.com/launchdarkly/ld-find-code-refs.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ld-find-code-refs"
-    sha256 cellar: :any_skip_relocation, mojave: "d05d450da91c99f788d5028703b0fa6b5ecd70f115bf63f5d8f96dcfd60464d9"
+    sha256 cellar: :any_skip_relocation, mojave: "95e9d4cea409bcc2514c679e6bf16d27a08de585a6de30160eab9572488d5882"
   end
 
   depends_on "go" => :build
@@ -24,7 +24,8 @@ class LdFindCodeRefs < Formula
     system "git", "add", "README", ".gitignore"
     system "git", "commit", "-m", "Initial commit"
 
-    assert_match "git branch: master",
-      shell_output(bin/"ld-find-code-refs --dryRun --ignoreServiceErrors -t=xx -p=test -r=test -d=.")
+    assert_match "could not retrieve flag key",
+      shell_output(bin/"ld-find-code-refs --dryRun " \
+                       "--ignoreServiceErrors -t=xx -p=test -r=test -d=. 2>&1", 1)
   end
 end
