698103a6c2b83aa1d1f0009548df1c80892eb30d
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 15:42:56 +0800

    kubecfg: update 0.27.0 bottle.

--- Formula/kubecfg.rb
+++ Formula/kubecfg.rb
@@ -1,13 +1,13 @@
 class Kubecfg < Formula
   desc "Manage complex enterprise Kubernetes environments as code"
   homepage "https://github.com/kubecfg/kubecfg"
-  url "https://github.com/kubecfg/kubecfg/archive/v0.26.0.tar.gz"
-  sha256 "322ed2b6d4214bafac63ee3d666aa240b077a0949d68bc97e5b6dfc484345b7e"
+  url "https://github.com/kubecfg/kubecfg/archive/v0.27.0.tar.gz"
+  sha256 "4fed9a09ee12fa2452becb9b64278444f99093d3044bc417aaa92ab3b5851d81"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kubecfg"
-    sha256 cellar: :any_skip_relocation, mojave: "3134ddc8c4dcef579df8900985a34a3636d1fce82f31534f323bb3de521d9bbd"
+    sha256 cellar: :any_skip_relocation, mojave: "26f4c88caaeee1af0cc62ea7dbf72885ed38754be8d0556ada7b53e7c664e5ef"
   end
 
   depends_on "go" => :build
@@ -18,17 +18,15 @@ class Kubecfg < Formula
     cd "src/github.com/kubecfg/kubecfg" do
       system "make", "VERSION=v#{version}"
       bin.install "kubecfg"
-      pkgshare.install Dir["examples/*"], "testdata/kubecfg_test.jsonnet"
+      pkgshare.install Pathname("examples").children
+      pkgshare.install Pathname("testdata").children
       prefix.install_metafiles
     end
 
-    output = Utils.safe_popen_read("#{bin}/kubecfg", "completion", "--shell", "bash")
-    (bash_completion/"kubecfg").write output
-    output = Utils.safe_popen_read("#{bin}/kubecfg", "completion", "--shell", "zsh")
-    (zsh_completion/"_kubecfg").write output
+    generate_completions_from_executable(bin/"kubecfg", "completion", "--shell", shells: [:bash, :zsh])
   end
 
   test do
-    system bin/"kubecfg", "show", pkgshare/"kubecfg_test.jsonnet"
+    system bin/"kubecfg", "show", "--alpha", pkgshare/"kubecfg_test.jsonnet"
   end
 end
