4c56dae19cb60f8f173663aa504420c68a926fc4
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 21:43:25 +0800

    obfs4proxy: update 0.0.14 bottle.

--- Formula/obfs4proxy.rb
+++ Formula/obfs4proxy.rb
@@ -1,8 +1,8 @@
 class Obfs4proxy < Formula
   desc "Pluggable transport proxy for Tor, implementing obfs4"
   homepage "https://gitlab.com/yawning/obfs4"
-  url "https://gitlab.com/yawning/obfs4/-/archive/obfs4proxy-0.0.13/obfs4-obfs4proxy-0.0.13.tar.gz"
-  sha256 "ddd9291cb7c41326f076f622118816f09148e1aac79ec440d46436e802918e84"
+  url "https://gitlab.com/yawning/obfs4/-/archive/obfs4proxy-0.0.14/obfs4-obfs4proxy-0.0.14.tar.gz"
+  sha256 "a4b7520e732b0f168832f6f2fdf1be57f3e2cce0612e743d3f6b51341a740903"
   license "BSD-2-Clause"
 
   livecheck do
@@ -12,7 +12,7 @@ class Obfs4proxy < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/obfs4proxy"
-    sha256 cellar: :any_skip_relocation, mojave: "d6db34d64c73cb74361465f2e63b1c26d3a51662622bb75c4e3274b773742a64"
+    sha256 cellar: :any_skip_relocation, mojave: "ca55fc36c6af4495e1dcfafd13b313177e695e6e08c742b3b943657e37e8189a"
   end
 
   depends_on "go" => :build
