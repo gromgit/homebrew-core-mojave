34726c778f68fb426e09901f22928c268b321fb0
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 22:20:43 +0800

    openfortivpn: update 1.18.0 bottle.

--- Formula/openfortivpn.rb
+++ Formula/openfortivpn.rb
@@ -1,13 +1,13 @@
 class Openfortivpn < Formula
   desc "Open Fortinet client for PPP+SSL VPN tunnel services"
   homepage "https://github.com/adrienverge/openfortivpn"
-  url "https://github.com/adrienverge/openfortivpn/archive/v1.17.3.tar.gz"
-  sha256 "60f319166fcbe8514dc7160346698ad83d8b09e2d4f5f011e16057bcfecf801f"
+  url "https://github.com/adrienverge/openfortivpn/archive/v1.18.0.tar.gz"
+  sha256 "01f4c5d97113d55d469b8078d1b2a8966ee1dfd231574322b36ec48c897c5ae9"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/openfortivpn"
-    sha256 mojave: "f61618cf432b13943ebcb34ae4e16da85b8e1a769c6f49e8cb3fd3e8fbdee46a"
+    sha256 mojave: "68231e30686cc0a9eced3965c38555260c7c1cf8816149388543c7ed757b2641"
   end
 
   depends_on "autoconf" => :build
