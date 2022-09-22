6fc45d03e29026601b59aeaa754d0c0c4b59c1fd
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 15:28:14 +0800

    kube-linter: update 0.5.0 bottle.

--- Formula/kube-linter.rb
+++ Formula/kube-linter.rb
@@ -1,14 +1,14 @@
 class KubeLinter < Formula
   desc "Static analysis tool for Kubernetes YAML files and Helm charts"
   homepage "https://github.com/stackrox/kube-linter"
-  url "https://github.com/stackrox/kube-linter/archive/0.4.0.tar.gz"
-  sha256 "5397d913e757fdf80f2ebd99c1b7264a41d85d72d7d8d079a2a8dd6040c3d5c9"
+  url "https://github.com/stackrox/kube-linter/archive/0.5.0.tar.gz"
+  sha256 "448122a7d93239ad55ddc88157ef8c77158e77da792f57faf50b080734295d1b"
   license "Apache-2.0"
   head "https://github.com/stackrox/kube-linter.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kube-linter"
-    sha256 cellar: :any_skip_relocation, mojave: "4c61d5deeeb60be6c6a918594209d577afd6a664865bd9e9aac6c7b6d6425486"
+    sha256 cellar: :any_skip_relocation, mojave: "6afe851068045910e45c3385d6dcc4a81a0b947d0e1171140d80987faa65f8fc"
   end
 
   depends_on "go" => :build
@@ -18,14 +18,7 @@ class KubeLinter < Formula
     ldflags = "-s -w -X golang.stackrox.io/kube-linter/internal/version.version=#{version}"
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/kube-linter"
 
-    bash_output = Utils.safe_popen_read(bin/"kube-linter", "completion", "bash")
-    (bash_completion/"kube-linter").write bash_output
-
-    zsh_output = Utils.safe_popen_read(bin/"kube-linter", "completion", "zsh")
-    (zsh_completion/"_kube-linter").write zsh_output
-
-    fish_output = Utils.safe_popen_read(bin/"kube-linter", "completion", "fish")
-    (fish_completion/"kube-linter.fish").write fish_output
+    generate_completions_from_executable(bin/"kube-linter", "completion")
   end
 
   test do
