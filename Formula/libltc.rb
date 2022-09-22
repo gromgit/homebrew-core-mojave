ba76c11664d2ef1cbcced2a770276e19981f0eee
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 18:29:49 +0800

    libltc: update 1.3.2 bottle.

--- Formula/libltc.rb
+++ Formula/libltc.rb
@@ -1,20 +1,13 @@
 class Libltc < Formula
   desc "POSIX-C Library for handling Linear/Logitudinal Time Code (LTC)"
   homepage "https://x42.github.io/libltc/"
-  url "https://github.com/x42/libltc/releases/download/v1.3.1/libltc-1.3.1.tar.gz"
-  sha256 "50e63eb3b767151bc0159a3cc5d426d03a42fd69029bc9b3b7c346555f4b709c"
+  url "https://github.com/x42/libltc/releases/download/v1.3.2/libltc-1.3.2.tar.gz"
+  sha256 "0a6d42cd6c21e925a27fa560dc45ac80057d275f23342102825909c02d3b1249"
   license "LGPL-3.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "749ea141468995c93c9ce63433317d7453c9d4f7214251e7efc19b254308ce16"
-    sha256 cellar: :any,                 arm64_big_sur:  "ee2457189224ff2af56fb6696e81f2b1ecf4cb285b9e7d20f2f70c4fc762b446"
-    sha256 cellar: :any,                 monterey:       "f6bf5f1b6366328cca58f935645a40719b629d4fb897ac20581692fdad212d1e"
-    sha256 cellar: :any,                 big_sur:        "2c69d93b07e1af11b703f6598bfedd51995f84c691ae5e3cdd88e31b614d67f7"
-    sha256 cellar: :any,                 catalina:       "c73e9da760961fc899dcd41c52f4e74cf2e29fb8ea619920c06c9ace25f717ab"
-    sha256 cellar: :any,                 mojave:         "aecfd413335e0981c5ac452bc0b81bee57d3a4e5974237f5b6a64aae734369cf"
-    sha256 cellar: :any,                 high_sierra:    "30f7ddada1d191c63214d44d9acec4862e71bca2ee67368c8546ec7240f65a9a"
-    sha256 cellar: :any,                 sierra:         "1c51db3447916e208601775d43fb248bdc76f3b29b6f309ccc74eddc0a11c9c6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ee1e76f2fee2534cfee95a78186c162f5d21a8e5efcdaa36c6aa483cab747e4d"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libltc"
+    sha256 cellar: :any, mojave: "72e1365ced9701b332fbbad044f029ceab3c06ac1fddbf7d069a0dd3227df79f"
   end
 
   def install
