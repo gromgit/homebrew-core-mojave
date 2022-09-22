9717d626fd6d59ea34af45715adca2390e6d1e2e
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 19:01:59 +0800

    livekit: update 1.2.1 bottle.

--- Formula/livekit.rb
+++ Formula/livekit.rb
@@ -1,14 +1,14 @@
 class Livekit < Formula
   desc "Scalable, high-performance WebRTC server"
   homepage "https://livekit.io"
-  url "https://github.com/livekit/livekit/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "4f25983914a2611ad7f534f0d76204c0f39c81b670272903ca10f2119f4ea257"
+  url "https://github.com/livekit/livekit/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "e3cc9d98c8936ce1c3d55aed93ed7932f29b077370ae63ed4e81d4b052a72512"
   license "Apache-2.0"
   head "https://github.com/livekit/livekit.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/livekit"
-    sha256 cellar: :any_skip_relocation, mojave: "fb096e124ef9e575ea3bb52ad4d3862595faccbc2bd7c4adaa1da4886925ad52"
+    sha256 cellar: :any_skip_relocation, mojave: "5db338de037fe469d0219299097a20d4db431470d8a58d25b01e8dd2b60781f7"
   end
 
   depends_on "go" => :build
