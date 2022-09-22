ed23af9d49af2d41b68706044cee46c39f2154b8
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 19:25:21 +0800

    mapcidr: update 1.0.2 bottle.

--- Formula/mapcidr.rb
+++ Formula/mapcidr.rb
@@ -1,14 +1,14 @@
 class Mapcidr < Formula
   desc "Subnet/CIDR operation utility"
   homepage "https://projectdiscovery.io"
-  url "https://github.com/projectdiscovery/mapcidr/archive/v1.0.1.tar.gz"
-  sha256 "8ea191b954a217b6a4bdc2111b9ac3c9b67bc05fb0e613d2bae8e45769851a35"
+  url "https://github.com/projectdiscovery/mapcidr/archive/v1.0.2.tar.gz"
+  sha256 "c77ba39d93566512cacea672c4c595881fe4a36b29fab909fd61aba80b0646db"
   license "MIT"
   head "https://github.com/projectdiscovery/mapcidr.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mapcidr"
-    sha256 cellar: :any_skip_relocation, mojave: "0933a4d38bba79b513a8f81761c52140696f2aae1bb6bbb9481822ec436c409b"
+    sha256 cellar: :any_skip_relocation, mojave: "02f9db4a67a50f79a3fd62ab2719b59e6df8c29909c5b925398f4b261cd9581e"
   end
 
   depends_on "go" => :build
