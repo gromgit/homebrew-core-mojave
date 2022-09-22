476ce4124cb13375760230a7b733fe4f0f6e1fca
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 08:34:11 +0800

    dolt: update 0.41.4 bottle.

--- Formula/dolt.rb
+++ Formula/dolt.rb
@@ -1,8 +1,8 @@
 class Dolt < Formula
   desc "Git for Data"
   homepage "https://github.com/dolthub/dolt"
-  url "https://github.com/dolthub/dolt/archive/v0.41.2.tar.gz"
-  sha256 "c28ff0adb430025a3d479ea8020bdc10412b498ad14875298e7108c65984c0b1"
+  url "https://github.com/dolthub/dolt/archive/v0.41.4.tar.gz"
+  sha256 "08d4e343dd465ba6cfcf9f5da2db80de01c923e4ff3a2ace98f4027a15f493b6"
   license "Apache-2.0"
 
   livecheck do
@@ -12,7 +12,7 @@ class Dolt < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dolt"
-    sha256 cellar: :any_skip_relocation, mojave: "cf041cc624d71af7cb4011c180f519e4e55f5449d9e14053b6269c1bb570b5c4"
+    sha256 cellar: :any_skip_relocation, mojave: "95fa135c02f09e16f442774398c29fdf193cfd04b2c35f43fb29b07ffca0e004"
   end
 
   depends_on "go" => :build
