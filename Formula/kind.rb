6cd7246dd33430e9c7c9a5032dda21b730d35c00
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 15:00:31 +0800

    kind: update 0.15.0 bottle.

--- Formula/kind.rb
+++ Formula/kind.rb
@@ -1,14 +1,14 @@
 class Kind < Formula
   desc "Run local Kubernetes cluster in Docker"
   homepage "https://kind.sigs.k8s.io/"
-  url "https://github.com/kubernetes-sigs/kind/archive/v0.14.0.tar.gz"
-  sha256 "7850a3bb4c644622a1c643e63306ddcd76a5b729375df9bc97f87a82375b9439"
+  url "https://github.com/kubernetes-sigs/kind/archive/v0.15.0.tar.gz"
+  sha256 "a3a0abbce70c5da267fabcb0409e0e373e8bc657679cc4cc38844743dd8a97d0"
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/kind.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kind"
-    sha256 cellar: :any_skip_relocation, mojave: "ff35f792be3afaa9235a9045ef36e8be5c9c0de5fb3990c788c193a743fd918a"
+    sha256 cellar: :any_skip_relocation, mojave: "ac900a31c3a3bcc592ec3229b99f521ee4e46ed9b6123ad046a4df424653cebb"
   end
 
   depends_on "go" => :build
@@ -17,17 +17,7 @@ class Kind < Formula
   def install
     system "go", "build", *std_go_args
 
-    # Install bash completion
-    output = Utils.safe_popen_read("#{bin}/kind", "completion", "bash")
-    (bash_completion/"kind").write output
-
-    # Install zsh completion
-    output = Utils.safe_popen_read("#{bin}/kind", "completion", "zsh")
-    (zsh_completion/"_kind").write output
-
-    # Install fish completion
-    output = Utils.safe_popen_read("#{bin}/kind", "completion", "fish")
-    (fish_completion/"kind.fish").write output
+    generate_completions_from_executable(bin/"kind", "completion")
   end
 
   test do
