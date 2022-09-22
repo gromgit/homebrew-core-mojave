cd236b9d7b0cf075b92d79e1e860b4464de603fd
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 11:16:26 +0800

    got: update 0.75.1 bottle.

---
+++ Formula/got.rb
@@ -0,0 +1,46 @@
+class Got < Formula
+  desc "Version control system"
+  homepage "https://gameoftrees.org/"
+  url "https://gameoftrees.org/releases/portable/got-portable-0.75.1.tar.gz"
+  sha256 "1a511707cf5f64f459030bd45077322821f7263e403af3121c5a1db1529ea524"
+  license "ISC"
+
+  livecheck do
+    url "https://gameoftrees.org/releases/portable/"
+    regex(/href=.*?got-portable[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/got"
+    sha256 mojave: "79ca402988a0ff244d1347950d2c04beb5455bbb8e312f40c29ec4d7ebade664"
+  end
+
+  depends_on "bison" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libevent"
+  depends_on :macos # FIXME: build fails on Linux.
+  depends_on "ncurses"
+  depends_on "openssl@1.1"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "libmd"
+    depends_on "util-linux" # for libuuid
+  end
+
+  def install
+    # The `configure` script hardcodes our `openssl@3`, but we can't use it due to `libevent`.
+    inreplace "configure", %r{\$\{HOMEBREW_PREFIX?\}/opt/openssl@3}, Formula["openssl@1.1"].opt_prefix
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "make", "install"
+  end
+
+  test do
+    ENV["GOT_AUTHOR"] = "Flan Hacker <flan_hacker@openbsd.org>"
+    system bin/"gotadmin", "init", "repo.git"
+    mkdir "import-dir"
+    %w[haunted house].each { |f| touch testpath/"import-dir"/f }
+    system bin/"got", "import", "-m", "Initial Commit", "-r", "repo.git", "import-dir"
+    system bin/"got", "checkout", "repo.git", "src"
+  end
+end
