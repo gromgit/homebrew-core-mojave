4b38f99448f0822421918a577e5192698f730fd9
Adrian Ho <the.gromgit@gmail.com>
2022-09-22 15:32:08 +0800

    kubeaudit: update 0.20.0 bottle.

--- Formula/kubeaudit.rb
+++ Formula/kubeaudit.rb
@@ -1,14 +1,14 @@
 class Kubeaudit < Formula
   desc "Helps audit your Kubernetes clusters against common security controls"
   homepage "https://github.com/Shopify/kubeaudit"
-  url "https://github.com/Shopify/kubeaudit/archive/refs/tags/v0.19.0.tar.gz"
-  sha256 "846ad5000e43e37c6089332f964d421a72fdded710066b58de8dcae655627749"
+  url "https://github.com/Shopify/kubeaudit/archive/refs/tags/v0.20.0.tar.gz"
+  sha256 "352a1c9d3d3a2492da8ab4ab20b19185f2277112821eb72c3aa06cd1c10c6c4e"
   license "MIT"
   head "https://github.com/Shopify/kubeaudit.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kubeaudit"
-    sha256 cellar: :any_skip_relocation, mojave: "5689daf0b0d306d8795c4eb7d20a0a64f519b8f95c4dfbd63af2a333297ba8cb"
+    sha256 cellar: :any_skip_relocation, mojave: "ef6498f6004c1907c56ca3b610f2a09e39a66de07853ced84e05cd82db0f3a0d"
   end
 
   depends_on "go" => :build
@@ -21,6 +21,8 @@ class Kubeaudit < Formula
     ]
 
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd"
+
+    generate_completions_from_executable(bin/"kubeaudit", "completion")
   end
 
   test do
