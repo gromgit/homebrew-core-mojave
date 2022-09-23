ea6629fc3008d5c4d45945e91af2ffba7f979bfe
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 14:47:40 +0800

    telegraf: update 1.24.1 bottle.

--- Formula/telegraf.rb
+++ Formula/telegraf.rb
@@ -1,8 +1,8 @@
 class Telegraf < Formula
   desc "Server-level metric gathering agent for InfluxDB"
   homepage "https://www.influxdata.com/"
-  url "https://github.com/influxdata/telegraf/archive/v1.23.4.tar.gz"
-  sha256 "05188b5f0c0dfa204dc6bd8429ebc5366b73e42c7bdd4f1a50fffa2a1e75616f"
+  url "https://github.com/influxdata/telegraf/archive/v1.24.1.tar.gz"
+  sha256 "2aa1f7e358b9fe990984c25da3317cbed6cf4f2c6ee6707549a12d5b634ea770"
   license "MIT"
   head "https://github.com/influxdata/telegraf.git", branch: "master"
 
@@ -13,7 +13,7 @@ class Telegraf < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/telegraf"
-    sha256 cellar: :any_skip_relocation, mojave: "cf8d116757a3f21a72c909bcd6fc095c00ce831c2da2b74a0325aac78a98e5a9"
+    sha256 cellar: :any_skip_relocation, mojave: "bcbbd4fbe309ea74261daeb4b5245f9133e8baa518495109ffecc64f785aa49f"
   end
 
   depends_on "go" => :build
