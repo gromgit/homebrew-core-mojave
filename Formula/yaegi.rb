33ec77d85f57a64c30011b259a453ca006e477a4
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 18:09:50 +0800

    yaegi: update 0.14.2 bottle.

--- Formula/yaegi.rb
+++ Formula/yaegi.rb
@@ -1,14 +1,14 @@
 class Yaegi < Formula
   desc "Yet another elegant Go interpreter"
   homepage "https://github.com/traefik/yaegi"
-  url "https://github.com/traefik/yaegi/archive/v0.14.1.tar.gz"
-  sha256 "f480cc37c2d443ccb6fa34477deaf82bc7d3773fc4ea6871b261d31bd430a132"
+  url "https://github.com/traefik/yaegi/archive/v0.14.2.tar.gz"
+  sha256 "78f69e7a2de98b3ea23b05385d3da9cf98b967a398b2af9545b9bba48533ea46"
   license "Apache-2.0"
   head "https://github.com/traefik/yaegi.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/yaegi"
-    sha256 cellar: :any_skip_relocation, mojave: "25570b367123fe16ff39ea8225a00f1dceb9e23a1319602bf431d87bba1cc9d1"
+    sha256 cellar: :any_skip_relocation, mojave: "38b4ec54bcf5ef9bb919e660b9e728884c82a1010e8ecd832f3f11708daa31e2"
   end
 
   depends_on "go" => :build
