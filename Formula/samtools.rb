6d6e26f1ac5d9bef408f878f7b2213020f36ee75
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 12:04:53 +0800

    samtools: update 1.16.1 bottle.

--- Formula/samtools.rb
+++ Formula/samtools.rb
@@ -1,14 +1,13 @@
 class Samtools < Formula
   desc "Tools for manipulating next-generation sequencing data"
   homepage "https://www.htslib.org/"
-  url "https://github.com/samtools/samtools/releases/download/1.16/samtools-1.16.tar.bz2"
-  sha256 "6106b27fea5a6962cf5f257d68219e20235cce46039572afeff2c6cf11554764"
+  url "https://github.com/samtools/samtools/releases/download/1.16.1/samtools-1.16.1.tar.bz2"
+  sha256 "2fa0a25f78594cf23d07c9d32d5060a14f1c5ee14d7b0af7a8a71abc9fdf1d07"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/samtools"
-    rebuild 1
-    sha256 cellar: :any, mojave: "8dc7b70ae4b44bf553d5270992e8a5f3c17b81c3ab4558f50a0b89c2e589ae7b"
+    sha256 cellar: :any, mojave: "b2cb953a6e2e8427d964b8b65ae4b8f63226b358bb0c93dbbdcc4476d066af9b"
   end
 
   depends_on "htslib"
