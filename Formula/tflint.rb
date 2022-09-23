e6b260e7cf90a2764ebd374cabb66d09562d01b5
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 15:47:10 +0800

    tflint: update 0.40.1 bottle.

--- Formula/tflint.rb
+++ Formula/tflint.rb
@@ -1,14 +1,14 @@
 class Tflint < Formula
   desc "Linter for Terraform files"
   homepage "https://github.com/terraform-linters/tflint"
-  url "https://github.com/terraform-linters/tflint/archive/v0.39.3.tar.gz"
-  sha256 "d928f7f70741a6c33c2bc9b6db820e53f6f314dfa87d1591709ec395481cbb3d"
+  url "https://github.com/terraform-linters/tflint/archive/v0.40.1.tar.gz"
+  sha256 "cc7612386cb63a72a6b49e70e71996c5d6504f66538ad9e9c9a8d374723aa0a4"
   license "MPL-2.0"
   head "https://github.com/terraform-linters/tflint.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tflint"
-    sha256 cellar: :any_skip_relocation, mojave: "ff1f2ea90b45bc2094f082605ba404a7ae5ac5cac5b46fc6fbd373975d3dfe7f"
+    sha256 cellar: :any_skip_relocation, mojave: "c2644b1cb74d4ed8f8c2ee86d2fea9ae73bbc82522b780d30921354ee0285381"
   end
 
   depends_on "go" => :build
@@ -19,6 +19,15 @@ class Tflint < Formula
 
   test do
     (testpath/"test.tf").write <<~EOS
+      terraform {
+        required_providers {
+          aws = {
+            source = "hashicorp/aws"
+            version = "~> 4"
+          }
+        }
+      }
+
       provider "aws" {
         region = var.aws_region
       }
