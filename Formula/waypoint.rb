ef89b50f764e45884f0aae9fdac37228362c35b5
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 17:25:40 +0800

    waypoint: update 0.10.0 bottle.

--- Formula/waypoint.rb
+++ Formula/waypoint.rb
@@ -1,14 +1,14 @@
 class Waypoint < Formula
   desc "Tool to build, deploy, and release any application on any platform"
   homepage "https://www.waypointproject.io/"
-  url "https://github.com/hashicorp/waypoint/archive/v0.9.1.tar.gz"
-  sha256 "486f50d2e51a4e7f3e35617d17ba50aed02b76cc59bcf9e790cb3dc982afe138"
+  url "https://github.com/hashicorp/waypoint/archive/v0.10.0.tar.gz"
+  sha256 "2304f3e48dab78751b5307c6be0fcf9efffa434042d9debfb28434ae05cec097"
   license "MPL-2.0"
   head "https://github.com/hashicorp/waypoint.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/waypoint"
-    sha256 cellar: :any_skip_relocation, mojave: "dd47662540ec89a3b84f4895821e9966fe6409015f8064ab35551c5916439c76"
+    sha256 cellar: :any_skip_relocation, mojave: "c7a3592795a65b9877dddb509546123f4a878d1c90f1c0bd1469b41af1070e9f"
   end
 
   depends_on "go" => :build
@@ -20,8 +20,8 @@ class Waypoint < Formula
   end
 
   test do
-    assert_match "Initial Waypoint configuration created!", shell_output("#{bin}/waypoint init")
-    assert_match "# An application to deploy.", File.read("waypoint.hcl")
+    output = shell_output("#{bin}/waypoint context list")
+    assert_match "No contexts. Create one with `waypoint context create`.", output
 
     assert_match "! failed to create client: no server connection configuration found",
       shell_output("#{bin}/waypoint server bootstrap 2>&1", 1)
