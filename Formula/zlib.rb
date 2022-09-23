1e1d954b07b605965b30fd7fdadda14ac2752546
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 18:24:13 +0800

    zlib: update 1.2.12_1 bottle.

--- Formula/zlib.rb
+++ Formula/zlib.rb
@@ -7,6 +7,7 @@ class Zlib < Formula
   mirror "http://fresh-center.net/linux/misc/legacy/zlib-1.2.12.tar.gz"
   sha256 "91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9"
   license "Zlib"
+  revision 1
   head "https://github.com/madler/zlib.git", branch: "develop"
 
   livecheck do
@@ -16,8 +17,7 @@ class Zlib < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/zlib"
-    rebuild 3
-    sha256 cellar: :any, mojave: "ed273cc425d44a59f3f7db177eeb338b9ad791fc96360af5a87d53f2b36e518b"
+    sha256 cellar: :any, mojave: "14cb3b46984112e5d3320fba00839696ea51f392bc452fcf6d05250514b81a5d"
   end
 
   keg_only :provided_by_macos
@@ -41,6 +41,20 @@ class Zlib < Formula
   patch do
     url "https://github.com/madler/zlib/commit/ec3df00224d4b396e2ac6586ab5d25f673caa4c2.patch?full_index=1"
     sha256 "c7d1cbb58b144c48b7fa8b52c57531e9fd80ab7d87c5d58ba76a9d33c12cb047"
+  end
+
+  # Patch for CVE-2022-37434
+  # Remove with the next release
+  patch do
+    url "https://github.com/madler/zlib/commit/eff308af425b67093bab25f80f1ae950166bece1.patch?full_index=1"
+    sha256 "b6d631860d5d02e3261c0e5c06ba598fb82fa64995ba527861c6c18542eca05c"
+  end
+
+  # Amendment of the above patch to fix a segfault
+  # Remove with the next release
+  patch do
+    url "https://github.com/madler/zlib/commit/1eb7682f845ac9e9bf9ae35bbfb3bad5dacbd91d.patch?full_index=1"
+    sha256 "5483f1e4cad801bdaff948e1092f1e8de892e5ee817bf371c69a5d5f20d27b27"
   end
 
   def install
