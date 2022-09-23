4ba48614a5dbb44c3c943142cc78954ed2b4c4f9
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 17:41:41 +0800

    wimlib: update 1.13.6 bottle.

--- Formula/wimlib.rb
+++ Formula/wimlib.rb
@@ -1,8 +1,8 @@
 class Wimlib < Formula
   desc "Library to create, extract, and modify Windows Imaging files"
   homepage "https://wimlib.net/"
-  url "https://wimlib.net/downloads/wimlib-1.13.5.tar.gz"
-  sha256 "32fcc9e9b144b7cb1db4c86e104ca78283cdc225e13fe82b273660586aefe323"
+  url "https://wimlib.net/downloads/wimlib-1.13.6.tar.gz"
+  sha256 "0a0f9c1c0d3a2a76645535aeb0f62e03fc55914ca65f3a4d5599bb8b0260dbd9"
   license "GPL-3.0-or-later"
 
   livecheck do
@@ -12,19 +12,13 @@ class Wimlib < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/wimlib"
-    sha256 cellar: :any, mojave: "1330a442f5b2ca29886c275795e6f506ee749d1e6fd1d29c8aa99085b62e2e7a"
+    sha256 cellar: :any, mojave: "f66eaff197902ed35587b2b91f533b46cc76246466c39d573f4ae0041b35a803"
   end
 
   depends_on "pkg-config" => :build
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   uses_from_macos "libxml2"
-
-  # Fix -flat_namespace being used on Big Sur and later.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-  end
 
   def install
     # fuse requires librt, unavailable on OSX
