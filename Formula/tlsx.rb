e375c9336f450279338149e3720377e3d7022157
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 15:56:03 +0800

    tlsx: update 0.0.7 bottle.

--- Formula/tlsx.rb
+++ Formula/tlsx.rb
@@ -1,14 +1,14 @@
 class Tlsx < Formula
   desc "Fast and configurable TLS grabber focused on TLS based data collection"
   homepage "https://github.com/projectdiscovery/tlsx"
-  url "https://github.com/projectdiscovery/tlsx/archive/v0.0.6.tar.gz"
-  sha256 "79c663951053e866d0ad28a61bc218054e80611c29b4de6680d4eb7448c67593"
+  url "https://github.com/projectdiscovery/tlsx/archive/v0.0.7.tar.gz"
+  sha256 "82b54e89ff5c73f5a8536443466c897a6b0bb5ee50ea101390c10211bdf4f00e"
   license "MIT"
   head "https://github.com/projectdiscovery/tlsx.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tlsx"
-    sha256 cellar: :any_skip_relocation, mojave: "b92d4c3ad858acbfa9df414dbe535d4f43609dd72564ba77122d388a2255c62c"
+    sha256 cellar: :any_skip_relocation, mojave: "95d76f930b1c8168f994e19110e55c52002d74294199871cb4e6f1dc6c0031ef"
   end
 
   depends_on "go" => :build
