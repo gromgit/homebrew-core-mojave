baaf77e5384d0b12c6f81858a1a5ee10cdf9a7cc
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 16:54:54 +0800

    vespa-cli: update 8.54.61 bottle.

--- Formula/vespa-cli.rb
+++ Formula/vespa-cli.rb
@@ -1,8 +1,8 @@
 class VespaCli < Formula
   desc "Command-line tool for Vespa.ai"
   homepage "https://vespa.ai"
-  url "https://github.com/vespa-engine/vespa/archive/v8.40.27.tar.gz"
-  sha256 "cc28ab0e3c8a24ea105d18daf0c3b41908332f16208f6fe5a1bab11659eb8048"
+  url "https://github.com/vespa-engine/vespa/archive/v8.54.61.tar.gz"
+  sha256 "5169d06ad5065c12f3968909570baacdfef6e84b28243d1173b7d508284847a2"
   license "Apache-2.0"
 
   livecheck do
@@ -13,7 +13,7 @@ class VespaCli < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/vespa-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "2861ea06790a2ba3fe815a2f1ce6951261e137601dd6cf7fd3e4718c12ac1324"
+    sha256 cellar: :any_skip_relocation, mojave: "345b63d6f06eb86c65fff882afdab1f68d5a466733edbd9a307324e4e5534010"
   end
 
   depends_on "go" => :build
@@ -25,15 +25,13 @@ class VespaCli < Formula
       end
       bin.install "bin/vespa"
       man1.install Dir["share/man/man1/vespa*.1"]
-      (bash_completion/"vespa").write Utils.safe_popen_read(bin/"vespa", "completion", "bash")
-      (fish_completion/"vespa.fish").write Utils.safe_popen_read(bin/"vespa", "completion", "fish")
-      (zsh_completion/"_vespa").write Utils.safe_popen_read(bin/"vespa", "completion", "zsh")
+      generate_completions_from_executable(bin/"vespa", "completion", base_name: "vespa")
     end
   end
 
   test do
     ENV["VESPA_CLI_HOME"] = testpath
-    assert_match "vespa version #{version}", shell_output("#{bin}/vespa version")
+    assert_match "Vespa CLI version #{version}", shell_output("#{bin}/vespa version")
     doc_id = "id:mynamespace:music::a-head-full-of-dreams"
     assert_match "Error: Request failed", shell_output("#{bin}/vespa document get #{doc_id} 2>&1", 1)
     system "#{bin}/vespa", "config", "set", "target", "cloud"
