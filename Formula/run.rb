ffb3b79dc9cffa2c20c5bb64c57b41454a992764
Adrian Ho <the.gromgit@gmail.com>
2022-09-23 11:57:35 +0800

    run: update 0.9.0 bottle.

--- Formula/run.rb
+++ Formula/run.rb
@@ -1,13 +1,13 @@
 class Run < Formula
   desc "Easily manage and invoke small scripts and wrappers"
   homepage "https://github.com/TekWizely/run"
-  url "https://github.com/TekWizely/run/archive/v0.8.0.tar.gz"
-  sha256 "60b4f20152be20f0d7b76adb98ac49eff5f283d9e748165508fd54b62603bb1e"
+  url "https://github.com/TekWizely/run/archive/v0.9.0.tar.gz"
+  sha256 "f7bd2dbcf41b27f67797c2b51291245f64dcf4aef243df205aed22f6f321c4d7"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/run"
-    sha256 cellar: :any_skip_relocation, mojave: "b2e7f801799f4a39582451e7b3972c8f133ad4977a52c8ac2b876ea79649f7d6"
+    sha256 cellar: :any_skip_relocation, mojave: "597042a8a1810ff01cb201084be5ae277f19e80fc32d02e269518c6f9884c62c"
   end
 
   depends_on "go" => :build
