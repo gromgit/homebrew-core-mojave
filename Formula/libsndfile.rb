c16b4670b2d8375cf7f8115c3284f580b6a5f4e9
Adrian Ho <the.gromgit@gmail.com>
2022-09-26 11:00:13 +0800

    libsndfile: update 1.1.0_1 bottle.

--- Formula/libsndfile.rb
+++ Formula/libsndfile.rb
@@ -4,6 +4,7 @@ class Libsndfile < Formula
   url "https://github.com/libsndfile/libsndfile/releases/download/1.1.0/libsndfile-1.1.0.tar.xz"
   sha256 "0f98e101c0f7c850a71225fb5feaf33b106227b3d331333ddc9bacee190bcf41"
   license "LGPL-2.1-or-later"
+  revision 1
 
   livecheck do
     url :stable
@@ -12,14 +13,15 @@ class Libsndfile < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libsndfile"
-    rebuild 1
-    sha256 cellar: :any, mojave: "7fe7b6484827edef76e7bb77ce6658a89dd206ec975375c26af254ab6002775d"
+    sha256 cellar: :any, mojave: "2929886a1bc7c11073a8a877c57c5ef3a70ffe8c4151a8a1dd33cd9ffd7ac098"
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
+  # TODO: check if this can be `uses_from_macos "python" => :build`.
+  depends_on "python@3.10" => :build
   depends_on "flac"
   depends_on "libogg"
   depends_on "libvorbis"
