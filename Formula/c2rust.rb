ebb514bdc838577768b71b91b4f6eddd3f8e19f3
Adrian Ho <the.gromgit@gmail.com>
2022-10-03 00:50:32 +0800

    c2rust: update 0.16.0_1 bottle.

--- Formula/c2rust.rb
+++ Formula/c2rust.rb
@@ -4,19 +4,16 @@ class C2rust < Formula
   url "https://github.com/immunant/c2rust/archive/refs/tags/v0.16.0.tar.gz"
   sha256 "9ed1720672afb503db91b30cec1dedcf878841f57eaea4c7046839890990d8cd"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/c2rust"
-    sha256 cellar: :any, mojave: "aa255a968f9aae85cf65f3548a85112c46940927a7ba054af6bfc66e4b469f91"
+    sha256 cellar: :any, mojave: "eace950e85f604e327c2e8b9107da5bf6c3253e7f5c94bae65380304b75abfb1"
   end
 
   depends_on "cmake" => [:build, :test]
   depends_on "rust" => :build
-  depends_on "llvm"
-
-  on_linux do
-    depends_on "gcc"
-  end
+  depends_on "llvm@14"
 
   fails_with gcc: "5"
 
