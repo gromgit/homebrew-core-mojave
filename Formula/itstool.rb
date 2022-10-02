212fa956a5a2685e78d675cea741e5f9c4b54c3c
Adrian Ho <the.gromgit@gmail.com>
2022-10-02 19:54:05 +0800

    itstool: update 2.0.7_1 bottle.

--- Formula/itstool.rb
+++ Formula/itstool.rb
@@ -3,13 +3,14 @@ class Itstool < Formula
   homepage "http://itstool.org/"
   url "https://github.com/itstool/itstool/archive/2.0.7.tar.gz"
   sha256 "fba78a37dc3535e4686c7f57407b97d03c676e3a57beac5fb2315162b0cc3176"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
   revision 1
   head "https://github.com/itstool/itstool.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/itstool"
-    sha256 cellar: :any_skip_relocation, mojave: "211f56048a0498fd935fac91466be64590276ae236d5a60be45ff2061713087f"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "fbfc77345dc63340e7bfed0340671d34e12ce8d388070bdc3e833816c6bf6df0"
   end
 
   depends_on "autoconf" => :build
@@ -18,9 +19,10 @@ class Itstool < Formula
   depends_on "python@3.10"
 
   def install
-    ENV.append_path "PYTHONPATH", Formula["libxml2"].opt_prefix/Language::Python.site_packages("python3")
+    python3 = "python3.10"
+    ENV.append_path "PYTHONPATH", Formula["libxml2"].opt_prefix/Language::Python.site_packages(python3)
 
-    system "./autogen.sh", "--prefix=#{libexec}", "PYTHON=#{which("python3")}"
+    system "./autogen.sh", "--prefix=#{libexec}", "PYTHON=#{which(python3)}"
     system "make", "install"
 
     bin.install Dir["#{libexec}/bin/*"]
