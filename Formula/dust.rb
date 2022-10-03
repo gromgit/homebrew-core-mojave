5ac2f544460524093bc8a24004c7f3d1299530b6
Adrian Ho <the.gromgit@gmail.com>
2022-10-03 21:58:25 +0800

    dust: update 0.8.3 bottle.

--- Formula/dust.rb
+++ Formula/dust.rb
@@ -1,10 +1,9 @@
 class Dust < Formula
   desc "More intuitive version of du in rust"
   homepage "https://github.com/bootandy/dust"
-  url "https://github.com/bootandy/dust/archive/v0.8.2.tar.gz"
-  sha256 "890972fbf1a7f0a336c0f20e1e9ecc756c62d3debd75d22b596af993a3d8af01"
+  url "https://github.com/bootandy/dust/archive/v0.8.3.tar.gz"
+  sha256 "1e07203546274276503a4510adcf5dc6eacd5d1e20604fcd55a353b3b63c1213"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/bootandy/dust.git", branch: "master"
 
   livecheck do
@@ -14,8 +13,7 @@ class Dust < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dust"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "0316a870f02832794cedc1406996d24cfb9acc9a81b428800a88cf1203890530"
+    sha256 cellar: :any_skip_relocation, mojave: "48d7548e3ef3c16505d01b4529586b9ebc6923bd6c6f9ae86480f4948c35752d"
   end
 
   depends_on "rust" => :build
