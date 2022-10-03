262af90dfbe962cf23b16bc0e152d1fc76a9a777
Adrian Ho <the.gromgit@gmail.com>
2022-10-04 01:13:10 +0800

    file-formula: update 5.43 bottle.

--- Formula/file-formula.rb
+++ Formula/file-formula.rb
@@ -2,8 +2,8 @@
 class FileFormula < Formula
   desc "Utility to determine file types"
   homepage "https://darwinsys.com/file/"
-  url "https://astron.com/pub/file/file-5.42.tar.gz"
-  sha256 "c076fb4d029c74073f15c43361ef572cfb868407d347190ba834af3b1639b0e4"
+  url "https://astron.com/pub/file/file-5.43.tar.gz"
+  sha256 "8c8015e91ae0e8d0321d94c78239892ef9dbc70c4ade0008c0e95894abfb1991"
   # file-formula has a BSD-2-Clause-like license
   license :cannot_represent
   head "https://github.com/file/file.git", branch: "master"
@@ -15,7 +15,7 @@ class FileFormula < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/file-formula"
-    sha256 cellar: :any, mojave: "5c963bf6e2f04abe2c94095bef6f11ae7cdcb43544dfb707023df768b9825d0e"
+    sha256 cellar: :any, mojave: "267944347bb4ff8281e69ea94fba2d1e52ccb6b4ca734347b805f0d74b5afc10"
   end
 
   keg_only :provided_by_macos
