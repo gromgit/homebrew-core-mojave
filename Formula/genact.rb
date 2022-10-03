4bbd9b6d176a5e7fd24399dc4fc47bba7fe7c8a9
Adrian Ho <the.gromgit@gmail.com>
2022-10-04 03:33:04 +0800

    genact: update 1.1.1 bottle.

--- Formula/genact.rb
+++ Formula/genact.rb
@@ -1,14 +1,13 @@
 class Genact < Formula
   desc "Nonsense activity generator"
   homepage "https://github.com/svenstaro/genact"
-  url "https://github.com/svenstaro/genact/archive/v0.12.0.tar.gz"
-  sha256 "fbbbcf5e65b370fde6765a2257c6d2f12cb9568bdce785b0f548c2be28d08cd7"
+  url "https://github.com/svenstaro/genact/archive/v1.1.1.tar.gz"
+  sha256 "231ea3735c59a659264d11b7ef1a6a2572c73f7bd7c9cb3efb940709673b58bf"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/genact"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "f9e280299f88207d777cb9d9082929e55df26c35da3e9a261f66bec09b2b8516"
+    sha256 cellar: :any_skip_relocation, mojave: "497818c75d9504e1f169d5b43163a810a55b0a83c5a107971d39164770504e91"
   end
 
   depends_on "rust" => :build
