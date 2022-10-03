20951b6be769bede70a6fa24b6cc16b0ffe9d678
Adrian Ho <the.gromgit@gmail.com>
2022-10-03 21:23:09 +0800

    dssim: update 3.2.3 bottle.

--- Formula/dssim.rb
+++ Formula/dssim.rb
@@ -1,14 +1,13 @@
 class Dssim < Formula
   desc "RGBA Structural Similarity Rust implementation"
   homepage "https://github.com/kornelski/dssim"
-  url "https://github.com/kornelski/dssim/archive/3.2.0.tar.gz"
-  sha256 "b671aec5b117adb6f9c636ef70ecafbd671caacf8b110098565c01bf5171118f"
+  url "https://github.com/kornelski/dssim/archive/3.2.3.tar.gz"
+  sha256 "f536e2b798d1731f2bc3575c4c2283a25a02e092d22ea6a25c3c3a6a84564bac"
   license "AGPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dssim"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "dd916ab12bbb642755b1f0d3a31cfa1304dba26cc139a4a6fc0a0c6ef8c67f5b"
+    sha256 cellar: :any_skip_relocation, mojave: "b258778432a65b48b29c7516d381200234bae041656214dabed77a879ab04867"
   end
 
   depends_on "nasm" => :build
