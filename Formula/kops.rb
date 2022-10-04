fe1106d7a6f645ca720d4e475889ec77daec5e7f
Adrian Ho <the.gromgit@gmail.com>
2022-10-04 13:41:47 +0800

    kops: update 1.25.0 bottle.

--- Formula/kops.rb
+++ Formula/kops.rb
@@ -1,8 +1,8 @@
 class Kops < Formula
   desc "Production Grade K8s Installation, Upgrades, and Management"
   homepage "https://kops.sigs.k8s.io/"
-  url "https://github.com/kubernetes/kops/archive/v1.24.1.tar.gz"
-  sha256 "011c01528e5906e6d4ffa4371f9f855b8fe8c635f67a056eaeda0b02f8050e92"
+  url "https://github.com/kubernetes/kops/archive/v1.25.0.tar.gz"
+  sha256 "a75d7971ee52eed5167c8eee085a02740dc73526d01fd56d3e8bb849c53b1861"
   license "Apache-2.0"
   head "https://github.com/kubernetes/kops.git", branch: "master"
 
@@ -13,7 +13,7 @@ class Kops < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kops"
-    sha256 cellar: :any_skip_relocation, mojave: "714739c4e8d41a0f9b306ecdd747adba60b6352bb4de33471412335a9b51209f"
+    sha256 cellar: :any_skip_relocation, mojave: "cd6a9382f49aa7af30b9c65f6f7494a6a9158aec3df9c6b21a3597547751bcad"
   end
 
   depends_on "go" => :build
@@ -23,12 +23,7 @@ class Kops < Formula
     ldflags = "-s -w -X k8s.io/kops.Version=#{version}"
     system "go", "build", *std_go_args(ldflags: ldflags), "k8s.io/kops/cmd/kops"
 
-    bash_output = Utils.safe_popen_read(bin/"kops", "completion", "bash")
-    (bash_completion/"kops").write bash_output
-    zsh_output = Utils.safe_popen_read(bin/"kops", "completion", "zsh")
-    (zsh_completion/"_kops").write zsh_output
-    fish_output = Utils.safe_popen_read(bin/"kops", "completion", "fish")
-    (fish_completion/"kops.fish").write fish_output
+    generate_completions_from_executable(bin/"kops", "completion")
   end
 
   test do
