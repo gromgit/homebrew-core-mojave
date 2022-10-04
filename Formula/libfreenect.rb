f832e1caea26ab74e90c4e79b29a9e974c62c288
Adrian Ho <the.gromgit@gmail.com>
2022-10-04 14:51:16 +0800

    libfreenect: update 0.6.4 bottle.

--- Formula/libfreenect.rb
+++ Formula/libfreenect.rb
@@ -1,15 +1,14 @@
 class Libfreenect < Formula
   desc "Drivers and libraries for the Xbox Kinect device"
   homepage "https://openkinect.org/"
-  url "https://github.com/OpenKinect/libfreenect/archive/v0.6.2.tar.gz"
-  sha256 "e135f5e60ae290bf1aa403556211f0a62856a9e34f12f12400ec593620a36bfa"
+  url "https://github.com/OpenKinect/libfreenect/archive/v0.6.4.tar.gz"
+  sha256 "6169600f999729a7f99dd71a9825ed6e7aec8b8aac4f532ecf2911f76133c125"
   license any_of: ["Apache-2.0", "GPL-2.0-only"]
   head "https://github.com/OpenKinect/libfreenect.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libfreenect"
-    rebuild 1
-    sha256 cellar: :any, mojave: "9c9734c508a68c26e20696009705a2b894d2edc223377f9098631708a72ed43b"
+    sha256 cellar: :any, mojave: "b57c2d177a74d14b492239e0e696dbb8526343bee091dea1f9e5058ed4fcbeef"
   end
 
   depends_on "cmake" => :build
