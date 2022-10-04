58c2c96f14fca0dae8e702e90db7224f811e93c3
Adrian Ho <the.gromgit@gmail.com>
2022-10-04 11:04:43 +0800

    irssi: update 1.2.3_2 bottle.

--- Formula/irssi.rb
+++ Formula/irssi.rb
@@ -4,7 +4,7 @@ class Irssi < Formula
   url "https://github.com/irssi/irssi/releases/download/1.2.3/irssi-1.2.3.tar.xz"
   sha256 "a647bfefed14d2221fa77b6edac594934dc672c4a560417b1abcbbc6b88d769f"
   license "GPL-2.0-or-later"
-  revision 1
+  revision 2
 
   # This formula uses a file from a GitHub release, so we check the latest
   # release version instead of Git tags.
@@ -14,13 +14,8 @@ class Irssi < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "6f90ced76f4dff3f6a4a65f47cdb996dc8e0473c677a4ae939019c54e69c88a8"
-    sha256 arm64_big_sur:  "745a8f336278ed4d2ccb4f7a396b8dd3b8cb6ac4b8cc20ae9e39822815aeb01b"
-    sha256 monterey:       "e8842954b54c584b9669ba93f2a9717edb536dca7c06f0bc9ef5703a701e1c25"
-    sha256 big_sur:        "837696228d18006f66c7669f3bc64daf8425d07231ffd33650ca0cf3754e63bd"
-    sha256 catalina:       "4906dd3fa6634f850b5c5bbcef90288f7e005401de43a5de144bdf824a93d1ba"
-    sha256 mojave:         "e24824148ee68afeb363aaae3db05a4fb30cb632416d04bd18c4763be8ae95b7"
-    sha256 x86_64_linux:   "20589844282f32a4ddeb87db55f9a8fcbfd890fa0d0d3ff2d42f639cdbed2383"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/irssi"
+    sha256 mojave: "37925442531bd544ca8b370f40c457854e0c068b1fd303fd63a2eceeccc71c94"
   end
 
   head do
