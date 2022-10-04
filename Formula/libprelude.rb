09fb45b3f3b1457c847cb1ee09fab76e022d4310
Adrian Ho <the.gromgit@gmail.com>
2022-10-04 15:47:52 +0800

    libprelude: update 5.2.0_2 bottle.

--- Formula/libprelude.rb
+++ Formula/libprelude.rb
@@ -13,7 +13,8 @@ class Libprelude < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libprelude"
-    sha256 mojave: "41b9d05d8ffa3cce9268158bc4496be31bfad2a2393819d07d7b7e0718efa923"
+    rebuild 1
+    sha256 mojave: "45ea4e621776239c1c88892beb60a94d1ada9da12aa426cd14b12ae9c0c406af"
   end
 
   depends_on "libtool" => :build
@@ -23,6 +24,13 @@ class Libprelude < Formula
   depends_on "python@3.10"
 
   def install
+    python3 = "python3.10"
+    # Work around Homebrew's "prefix scheme" patch which causes non-pip installs
+    # to incorrectly try to write into HOMEBREW_PREFIX/lib since Python 3.10.
+    inreplace "bindings/python/Makefile.in",
+              "--prefix @prefix@",
+              "\\0 --install-lib=#{prefix/Language::Python.site_packages(python3)}"
+
     ENV["HAVE_CXX"] = "yes"
     args = %W[
       --disable-dependency-tracking
@@ -33,7 +41,7 @@ class Libprelude < Formula
       --without-perl
       --without-swig
       --without-python2
-      --with-python3=python#{Language::Python.major_minor_version("python3")}
+      --with-python3=#{python3}
       --with-libgnutls-prefix=#{Formula["gnutls"].opt_prefix}
     ]
 
