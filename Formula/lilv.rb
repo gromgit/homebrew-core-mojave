0409e90648d38b2952fba3c3b42fa4e33ca94133
Adrian Ho <the.gromgit@gmail.com>
2022-10-04 22:38:27 +0800

    lilv: update 0.24.20 bottle.

--- Formula/lilv.rb
+++ Formula/lilv.rb
@@ -1,8 +1,8 @@
 class Lilv < Formula
   desc "C library to use LV2 plugins"
   homepage "https://drobilla.net/software/lilv.html"
-  url "https://download.drobilla.net/lilv-0.24.18.tar.xz"
-  sha256 "f65814ae60be54d65f1671dff7538aeddcda3610cb6e46ec96de47f84ab0f3b8"
+  url "https://download.drobilla.net/lilv-0.24.20.tar.xz"
+  sha256 "4fb082b9b8b286ea92bbb71bde6b75624cecab6df0cc639ee75a2a096212eebc"
   license "ISC"
 
   livecheck do
@@ -12,7 +12,7 @@ class Lilv < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lilv"
-    sha256 cellar: :any, mojave: "7ec9ec2b0713ccb94e6c21cb51c4a48c03d628af946f0db59c49b348516048ce"
+    sha256 cellar: :any, mojave: "0ff9bc24295b8911fdaf792b380612f8b8fc1c7d2b6d595de22c37fc23744f5b"
   end
 
   depends_on "meson" => :build
