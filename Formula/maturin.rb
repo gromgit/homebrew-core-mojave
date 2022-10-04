1e9fa57ad74593cf6448bcf0145ef2e17202d58d
Adrian Ho <the.gromgit@gmail.com>
2022-10-05 01:10:51 +0800

    maturin: update 0.13.3 bottle.

--- Formula/maturin.rb
+++ Formula/maturin.rb
@@ -1,14 +1,14 @@
 class Maturin < Formula
   desc "Build and publish Rust crates as Python packages"
   homepage "https://github.com/PyO3/maturin"
-  url "https://github.com/PyO3/maturin/archive/refs/tags/v0.13.2.tar.gz"
-  sha256 "22afa6d4367eed3225a8650604483f13c127df612cb4ed66e074244c2344c668"
+  url "https://github.com/PyO3/maturin/archive/refs/tags/v0.13.3.tar.gz"
+  sha256 "0fa5f2bebaefe706c12db0fbb15a56a74d0b55a44742520114b84afc1507a794"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/PyO3/maturin.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/maturin"
-    sha256 cellar: :any_skip_relocation, mojave: "e00e1494e952d38e88d6e6213613ba829ced87c2ba5d8dd20d590e117a8a3b97"
+    sha256 cellar: :any_skip_relocation, mojave: "d5daa80c050020bc8fc2b78cb569f274795bc235b55e6f5eaaab05e85f510971"
   end
 
   depends_on "python@3.10" => :test
@@ -17,19 +17,14 @@ class Maturin < Formula
   def install
     system "cargo", "install", *std_cargo_args
 
-    bash_output = Utils.safe_popen_read(bin/"maturin", "completions", "bash")
-    (bash_completion/"maturin").write bash_output
-    zsh_output = Utils.safe_popen_read(bin/"maturin", "completions", "zsh")
-    (zsh_completion/"_maturin").write zsh_output
-    fish_output = Utils.safe_popen_read(bin/"maturin", "completions", "fish")
-    (fish_completion/"maturin.fish").write fish_output
+    generate_completions_from_executable(bin/"maturin", "completions")
   end
 
   test do
-    ENV.prepend_path "PATH", Formula["python@3.10"].opt_bin
+    python = Formula["python@3.10"].opt_bin/"python3.10"
     system "cargo", "new", "hello_world", "--bin"
     system bin/"maturin", "build", "-m", "hello_world/Cargo.toml", "-b", "bin", "-o", "dist", "--compatibility", "off"
-    system "python3", "-m", "pip", "install", "hello_world", "--no-index", "--find-links", testpath/"dist"
-    system "python3", "-m", "pip", "uninstall", "-y", "hello_world"
+    system python, "-m", "pip", "install", "hello_world", "--no-index", "--find-links", testpath/"dist"
+    system python, "-m", "pip", "uninstall", "-y", "hello_world"
   end
 end
