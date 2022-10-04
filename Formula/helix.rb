3e0835f3a1a215ae4355a279a4816bca20aa7bdd
Adrian Ho <the.gromgit@gmail.com>
2022-10-04 08:22:31 +0800

    helix: update 22.08.1 bottle.

--- Formula/helix.rb
+++ Formula/helix.rb
@@ -1,23 +1,19 @@
 class Helix < Formula
   desc "Post-modern modal text editor"
   homepage "https://helix-editor.com"
-  url "https://github.com/helix-editor/helix/releases/download/22.08/helix-22.08-source.tar.xz"
-  sha256 "2c96c00d2c7df98d4426884ee3b217aaadb985f3301f5399e2436db6440373a3"
+  url "https://github.com/helix-editor/helix/releases/download/22.08.1/helix-22.08.1-source.tar.xz"
+  sha256 "962cfb913b40b6b5e3896fce5d52590d83fa2e9c35dfba45fdfa26bada54f343"
   license "MPL-2.0"
   head "https://github.com/helix-editor/helix.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/helix"
-    sha256 cellar: :any, mojave: "49867fe8a521afbd9d8bd7448617a18f987c8b133455670a6f7f61da8745762b"
+    sha256 cellar: :any, mojave: "491ea923cb6a63f8f2cb6033cc84ae1cf592a818069b37e8431e5d686ad020d7"
   end
 
   depends_on "rust" => :build
 
-  on_linux do
-    depends_on "gcc" # For C++17
-  end
-
-  fails_with gcc: "5"
+  fails_with gcc: "5" # For C++17
 
   def install
     system "cargo", "install", "-vv", *std_cargo_args(root: libexec, path: "helix-term")
@@ -33,6 +29,6 @@ class Helix < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/hx -V")
-    assert_match "post-modern text editor", shell_output("#{bin}/hx -h")
+    assert_match "✓", shell_output("#{bin}/hx --health")
   end
 end
