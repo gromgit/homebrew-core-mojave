4744c79b3f61c74a64ed4fb392f15d6bc4f38c7d
Adrian Ho <the.gromgit@gmail.com>
2022-10-04 08:46:37 +0800

    hfstospell: update 0.5.3_1 bottle.

--- Formula/hfstospell.rb
+++ Formula/hfstospell.rb
@@ -4,6 +4,7 @@ class Hfstospell < Formula
   url "https://github.com/hfst/hfst-ospell/releases/download/v0.5.3/hfst-ospell-0.5.3.tar.bz2"
   sha256 "01bc5af763e4232d8aace8e4e8e03e1904de179d9e860b7d2d13f83c66f17111"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url :stable
@@ -12,7 +13,7 @@ class Hfstospell < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/hfstospell"
-    sha256 cellar: :any, mojave: "a8e5bd7824b6924d1b2eb7f5fe2161e18600c8d032acbaa233351a8e06674596"
+    sha256 cellar: :any, mojave: "e4991c48b86da523355babe2f0f7d5fcd947c5ed83967b05a4cfd307722a0e24"
   end
 
   depends_on "autoconf" => :build
