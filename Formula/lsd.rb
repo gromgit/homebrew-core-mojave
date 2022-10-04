c2b9209ed15efeaaf4c1a3c950c92f9f4a021dd6
Adrian Ho <the.gromgit@gmail.com>
2022-10-04 23:57:39 +0800

    lsd: update 0.23.1 bottle.

--- Formula/lsd.rb
+++ Formula/lsd.rb
@@ -1,13 +1,13 @@
 class Lsd < Formula
   desc "Clone of ls with colorful output, file type icons, and more"
   homepage "https://github.com/Peltoche/lsd"
-  url "https://github.com/Peltoche/lsd/archive/0.22.0.tar.gz"
-  sha256 "30ad1b1d014c7b6a2fca44c6de3b17198c84168d34fc946245d67094ebc0f7ed"
+  url "https://github.com/Peltoche/lsd/archive/0.23.1.tar.gz"
+  sha256 "9698919689178cc095f39dcb6a8a41ce32d5a1283e6fe62755e9a861232c307d"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lsd"
-    sha256 cellar: :any_skip_relocation, mojave: "524a82223362079f15a8144aa1ae0821ebcac4b6c0cb224961361246537ae3ff"
+    sha256 cellar: :any_skip_relocation, mojave: "87232334fc8e150df30ba672643d34a358411e03c385d66bce08722bf76cbe60"
   end
 
   depends_on "rust" => :build
