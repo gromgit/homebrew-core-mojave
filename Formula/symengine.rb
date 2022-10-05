9293d2643bcf975d963109528a47b6b10077e235
Adrian Ho <the.gromgit@gmail.com>
2022-10-06 05:24:08 +0800

    symengine: update 0.9.0_2 bottle.

--- Formula/symengine.rb
+++ Formula/symengine.rb
@@ -4,14 +4,15 @@ class Symengine < Formula
   url "https://github.com/symengine/symengine/releases/download/v0.9.0/symengine-0.9.0.tar.gz"
   sha256 "dcf174ac708ed2acea46691f6e78b9eb946d8a2ba62f75e87cf3bf4f0d651724"
   license "MIT"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/symengine"
-    sha256 cellar: :any, mojave: "e3e79e8fcd17a48459afc74eb378bc6d8dd29158ec733f4de857e19efc92b91e"
+    sha256 cellar: :any, mojave: "df8394210cb4a1cb0e286c3d63225b94ff5d1eef4d9a9b087152170b13d58ca7"
   end
 
   depends_on "cmake" => :build
+  depends_on "cereal"
   depends_on "flint"
   depends_on "gmp"
   depends_on "libmpc"
@@ -19,6 +20,15 @@ class Symengine < Formula
   depends_on "mpfr"
 
   fails_with gcc: "5"
+
+  # Avoid static linkage with LLVM. The static libraries contain
+  # LTOed objects which causes errors with Apple's `ld`.
+  # An alternative workaround is to use `lld` with `-fuse-ld=lld`.
+  # TODO(carlocab): Upstream a version of this patch.
+  patch do
+    url "https://gitweb.gentoo.org/repo/gentoo.git/plain/sci-libs/symengine/files/symengine-0.8.1-fix_llvm.patch?id=83ab9587be9f89e667506b861208d613a2f016e5"
+    sha256 "c654ea7c4ee44c689433e87f71c7ae78e6c04968e7dfe89be5e4ba4c8c53713b"
+  end
 
   def install
     system "cmake", "-S", ".", "-B", "build",
@@ -31,6 +41,7 @@ class Symengine < Formula
                     "-DWITH_COTIRE=OFF",
                     "-DLLVM_DIR=#{Formula["llvm"].opt_lib}/cmake/llvm",
                     "-DWITH_SYMENGINE_THREAD_SAFE=ON",
+                    "-DWITH_SYSTEM_CEREAL=ON",
                     *std_cmake_args
 
     system "cmake", "--build", "build"
