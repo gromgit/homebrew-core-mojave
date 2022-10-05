ebab4f3fe4e1850213a6ff6de7be88a6f810daf9
Adrian Ho <the.gromgit@gmail.com>
2022-10-06 00:41:46 +0800

    scour: update 0.38.2_1 bottle.

--- Formula/scour.rb
+++ Formula/scour.rb
@@ -11,21 +11,13 @@ class Scour < Formula
   head "https://github.com/scour-project/scour.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0a19de3b6e7507c4472d6807f651ba1dc9364091db38bf495964f1bc8aea5eca"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0a19de3b6e7507c4472d6807f651ba1dc9364091db38bf495964f1bc8aea5eca"
-    sha256 cellar: :any_skip_relocation, monterey:       "f4540d61288bd5dc596393a3d87870d1aad8fe61771379b9fa98a564fc239a71"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f4540d61288bd5dc596393a3d87870d1aad8fe61771379b9fa98a564fc239a71"
-    sha256 cellar: :any_skip_relocation, catalina:       "f4540d61288bd5dc596393a3d87870d1aad8fe61771379b9fa98a564fc239a71"
-    sha256 cellar: :any_skip_relocation, mojave:         "f4540d61288bd5dc596393a3d87870d1aad8fe61771379b9fa98a564fc239a71"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "f1d4f9a36356337522bfcc13866638b977ac217d2602aee271c152b60aa3cd62"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/scour"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "293fee6eb7fbd1d4ba156ef42f3300fa41aacc92e06dc5b68b97997584ac178c"
   end
 
   depends_on "python@3.10"
-
-  resource "six" do
-    url "https://files.pythonhosted.org/packages/6b/34/415834bfdafca3c5f451532e8a8d9ba89a21c9743a0c59fbd0205c7f9426/six-1.15.0.tar.gz"
-    sha256 "30639c035cdb23534cd4aa2dd52c3bf48f06e5f4a941509c8bafd8ce11080259"
-  end
+  depends_on "six"
 
   def install
     virtualenv_install_with_resources
