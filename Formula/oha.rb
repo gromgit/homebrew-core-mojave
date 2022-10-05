2ef3f43ad89b842878c434e9f4f4425fe2d58e02
Adrian Ho <the.gromgit@gmail.com>
2022-10-05 11:05:02 +0800

    oha: update 0.5.5 bottle.

--- Formula/oha.rb
+++ Formula/oha.rb
@@ -1,21 +1,21 @@
 class Oha < Formula
   desc "HTTP load generator, inspired by rakyll/hey with tui animation"
   homepage "https://github.com/hatoo/oha/"
-  url "https://github.com/hatoo/oha/archive/refs/tags/v0.5.4.tar.gz"
-  sha256 "96b4101bbfd65453d2bd44dea10d99b2553508fff9f5552673bf76c08d8c15f8"
+  url "https://github.com/hatoo/oha/archive/refs/tags/v0.5.5.tar.gz"
+  sha256 "8af14d4e14373e2c0c0b6473b53e51be1ec186259f5098d11a0241817cf139a8"
   license "MIT"
   head "https://github.com/hatoo/oha.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/oha"
-    sha256 cellar: :any_skip_relocation, mojave: "838d270941fb11cd8464dfaf95176d0853ce127361c392123a26b8b9cf2fca38"
+    sha256 cellar: :any_skip_relocation, mojave: "8a72d7fa404e34acdd2d8e7556cc805d2db12964104433e2803fa32c032446d1"
   end
 
   depends_on "rust" => :build
 
   on_linux do
     depends_on "pkg-config" => :build
-    depends_on "openssl@1.1" # Uses Secure Transport on macOS
+    depends_on "openssl@3" # Uses Secure Transport on macOS
   end
 
   def install
