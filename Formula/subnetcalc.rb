1a77f243ea64af923c3e27cc5da4b8e1e5a3f832
Adrian Ho <the.gromgit@gmail.com>
2022-10-06 05:08:15 +0800

    subnetcalc: update 2.4.20 bottle.

--- Formula/subnetcalc.rb
+++ Formula/subnetcalc.rb
@@ -1,15 +1,14 @@
 class Subnetcalc < Formula
   desc "IPv4/IPv6 subnet calculator"
   homepage "https://www.uni-due.de/~be0001/subnetcalc/"
-  url "https://www.uni-due.de/~be0001/subnetcalc/download/subnetcalc-2.4.19.tar.xz"
-  sha256 "13f35abc0782c7453da22602128eb93fa645039d92cd5ab3c528ae9e6032cd67"
+  url "https://www.uni-due.de/~be0001/subnetcalc/download/subnetcalc-2.4.20.tar.xz"
+  sha256 "5bc3a2ca7542d9ba0903632c9fc9d032e1929595dde7248e9d49b58a4d6556ba"
   license "GPL-3.0-or-later"
   head "https://github.com/dreibh/subnetcalc.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/subnetcalc"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "c37264918cb97ce1d47856f5e3f031bbbbc1400d182350bba969053a103bfae1"
+    sha256 cellar: :any_skip_relocation, mojave: "88ef58924ee6de293ca555f00a042b2a1c1210d5343a97907fcaebe3a2813cd8"
   end
 
   depends_on "cmake" => :build
