82893b649113df9abdba036a0482ce83c9894b5c
Adrian Ho <the.gromgit@gmail.com>
2022-10-05 18:54:25 +0800

    py-spy: update 0.3.14 bottle.

--- Formula/py-spy.rb
+++ Formula/py-spy.rb
@@ -1,14 +1,14 @@
 class PySpy < Formula
   desc "Sampling profiler for Python programs"
   homepage "https://github.com/benfred/py-spy"
-  url "https://github.com/benfred/py-spy/archive/refs/tags/v0.3.12.tar.gz"
-  sha256 "6a4b0537e0bf9cf40fc4557931955222a25db01c110309d0b642dd28211bffeb"
+  url "https://github.com/benfred/py-spy/archive/refs/tags/v0.3.14.tar.gz"
+  sha256 "c01da8b74be0daba79781cfc125ffcd3df3a0d090157fe0081c71da2f6057905"
   license "MIT"
   head "https://github.com/benfred/py-spy.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/py-spy"
-    sha256 cellar: :any_skip_relocation, mojave: "b548a7ae3b327d1391f1f9ebb6aa691271fa40c41ac86fedc2d14dd725d83a5d"
+    sha256 cellar: :any_skip_relocation, mojave: "16561baa9ee27e71e5ce66cbfa364a4dda35e5ad1836c34488ce5984b16c35f4"
   end
 
   depends_on "rust" => :build
@@ -21,16 +21,11 @@ class PySpy < Formula
   def install
     system "cargo", "install", *std_cargo_args
 
-    bash_output = Utils.safe_popen_read(bin/"py-spy", "completions", "bash")
-    (bash_completion/"py-spy").write bash_output
-    zsh_output = Utils.safe_popen_read(bin/"py-spy", "completions", "zsh")
-    (zsh_completion/"_py-spy").write zsh_output
-    fish_output = Utils.safe_popen_read(bin/"py-spy", "completions", "fish")
-    (fish_completion/"py-spy.fish").write fish_output
+    generate_completions_from_executable(bin/"py-spy", "completions")
   end
 
   test do
-    python = Formula["python@3.10"].opt_bin/"python3"
+    python = Formula["python@3.10"].opt_bin/"python3.10"
     output = shell_output("#{bin}/py-spy record #{python} 2>&1", 1)
     assert_match "Try running again with elevated permissions by going", output
   end
