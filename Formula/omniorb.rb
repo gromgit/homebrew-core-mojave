12afa1aa500e596bb3b059697e605c26c8c69ddc
Adrian Ho <the.gromgit@gmail.com>
2022-10-05 11:14:36 +0800

    omniorb: update 4.2.4_2 bottle.

--- Formula/omniorb.rb
+++ Formula/omniorb.rb
@@ -3,7 +3,7 @@ class Omniorb < Formula
   homepage "https://omniorb.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/omniorb/omniORB/omniORB-4.2.4/omniORB-4.2.4.tar.bz2"
   sha256 "28c01cd0df76c1e81524ca369dc9e6e75f57dc70f30688c99c67926e4bdc7a6f"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
   revision 2
 
   livecheck do
@@ -13,8 +13,8 @@ class Omniorb < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/omniorb"
-    rebuild 1
-    sha256 cellar: :any, mojave: "d6143d3cb3ec76d52fe8eb1d9761786c2c263b3dc935f10a9fc54b62102c0325"
+    rebuild 2
+    sha256 cellar: :any, mojave: "81faca9bd59a540461b48da61fcfc3aa74cd57798d5b8fd32c979f4262310f16"
   end
 
   depends_on "pkg-config" => :build
@@ -26,12 +26,19 @@ class Omniorb < Formula
   end
 
   def install
-    ENV["PYTHON"] = which("python3")
+    ENV["PYTHON"] = python3 = which("python3.10")
+    xy = Language::Python.major_minor_version python3
+    inreplace "configure",
+              /am_cv_python_version=`.*`/,
+              "am_cv_python_version='#{xy}'"
     system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
 
     resource("bindings").stage do
+      inreplace "configure",
+                /am_cv_python_version=`.*`/,
+                "am_cv_python_version='#{xy}'"
       system "./configure", "--prefix=#{prefix}"
       system "make", "install"
     end
