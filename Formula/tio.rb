b93b1ec130c54e4e73482de568305708651a8f6e
Adrian Ho <the.gromgit@gmail.com>
2022-10-06 06:57:13 +0800

    tio: update 2.0 bottle.

--- Formula/tio.rb
+++ Formula/tio.rb
@@ -1,14 +1,14 @@
 class Tio < Formula
   desc "Simple TTY terminal I/O application"
   homepage "https://tio.github.io"
-  url "https://github.com/tio/tio/releases/download/v1.47/tio-1.47.tar.xz"
-  sha256 "6f39ac582de747feb9a64c14e6b378c61cb0c3bfa6639e62050022c1b7f5c544"
+  url "https://github.com/tio/tio/releases/download/v2.0/tio-2.0.tar.xz"
+  sha256 "e5826109773a89b267c3484df71234bc6c45715f735e3fdbee5984768faf5836"
   license "GPL-2.0-or-later"
   head "https://github.com/tio/tio.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tio"
-    sha256 cellar: :any, mojave: "165264e2bcde5d3eb4124e49be9b74605c2edb77e286df67c216cd9459fbee27"
+    sha256 cellar: :any, mojave: "08b5dc09f8bf7466aa3f12c8fe739b6b65a0a194ad25d40e65cb274561fd3e7e"
   end
 
   depends_on "meson" => :build
