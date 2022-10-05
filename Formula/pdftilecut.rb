b104301b7f3d60cdec4e8015bc1759512714520f
Adrian Ho <the.gromgit@gmail.com>
2022-10-05 14:06:20 +0800

    pdftilecut: update 0.6_1 bottle.

--- Formula/pdftilecut.rb
+++ Formula/pdftilecut.rb
@@ -4,10 +4,11 @@ class Pdftilecut < Formula
   url "https://github.com/oxplot/pdftilecut/archive/v0.6.tar.gz"
   sha256 "fd2383ee0d0acfa56cf6e80ac62881bd6dda4555adcd7f5a397339e7d3eca9ac"
   license "BSD-3-Clause"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pdftilecut"
-    sha256 cellar: :any, mojave: "ea5b27e650595c6808ab08563dd0847dc3f4e93c30bf19b85734415ec4a23440"
+    sha256 cellar: :any, mojave: "266d2d9bd7e9178dcc6a160977441d784211b497a42c97f350758cb7535e0c17"
   end
 
   depends_on "go" => :build
