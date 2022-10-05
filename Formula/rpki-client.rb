9c213a0da625ef7ede7d6b5a3f42cdbf56806638
Adrian Ho <the.gromgit@gmail.com>
2022-10-05 23:24:52 +0800

    rpki-client: update 8.0_1 bottle.

--- Formula/rpki-client.rb
+++ Formula/rpki-client.rb
@@ -1,21 +1,24 @@
 class RpkiClient < Formula
   desc "OpenBSD portable rpki-client"
   homepage "https://www.rpki-client.org/index.html"
-  url "https://ftp.openbsd.org/pub/OpenBSD/rpki-client/rpki-client-7.9.tar.gz"
-  sha256 "accf531c885a9d95a37a6627399a59b360fa29a11810aba15b27d7526ce43e75"
+  url "https://ftp.openbsd.org/pub/OpenBSD/rpki-client/rpki-client-8.0.tar.gz"
+  sha256 "5b710ccee2e7e949587e54daf823811671174a50c671746e5a276afaa0ce55be"
   license "ISC"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rpki-client"
-    sha256 mojave: "4ae8baa6889792514e3f588f44ecd98e563ac475a91858c14893d345da7064a9"
+    sha256 mojave: "bf355e9c60c8e1741f8afa25d82a9d1d1d5a1f2d5cfec8cddef9dbab8d159386"
   end
 
   depends_on "pkg-config" => :build
   depends_on "libressl"
-  depends_on :macos
+  depends_on "rsync"
+  uses_from_macos "expat"
 
   def install
     system "./configure", *std_configure_args,
+                          "--with-rsync=#{Formula["rsync"].opt_bin}/rsync",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}",
@@ -31,6 +34,7 @@ class RpkiClient < Formula
   end
 
   test do
-    assert_match "VRP Entries: 0 (0 unique)", shell_output("#{sbin}/rpki-client -n -d . -R . 2>&1").lines.last
+    assert_match "VRP Entries: 0 (0 unique)", shell_output("#{sbin}/rpki-client -n -d . -R . 2>&1")
+    assert_match "rpki-client-portable #{version}", shell_output("#{sbin}/rpki-client -V 2>&1")
   end
 end
