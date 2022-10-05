655d0f2192487ec0c223e459c2113ffa122c4d54
Adrian Ho <the.gromgit@gmail.com>
2022-10-06 06:23:30 +0800

    tere: update 1.2.0 bottle.

--- Formula/tere.rb
+++ Formula/tere.rb
@@ -1,14 +1,14 @@
 class Tere < Formula
   desc "Terminal file explorer"
   homepage "https://github.com/mgunyho/tere"
-  url "https://github.com/mgunyho/tere/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "a4ebd2f0b5728d6ec61749564106d67d74720fa05cb906686d559a5f15549698"
+  url "https://github.com/mgunyho/tere/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "cb332750a1f1ea9f7de442559d6ccde46ad2ee0b954852866e23557aa38dc63b"
   license "EUPL-1.2"
   head "https://github.com/mgunyho/tere.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tere"
-    sha256 cellar: :any_skip_relocation, mojave: "2976ad4ae8265090a8aa670437b32e3af46f16c6cd416853aadf195781974729"
+    sha256 cellar: :any_skip_relocation, mojave: "c741150e08476bc3b4ff3b2a5cb5516a0b9f2ac9f59a013c933e17e014f92d19"
   end
 
   depends_on "rust" => :build
