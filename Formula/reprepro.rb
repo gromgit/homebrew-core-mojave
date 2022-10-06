b2f665176e0d255de8a895dfea183224e773d772
Adrian Ho <the.gromgit@gmail.com>
2022-10-07 01:42:45 +0800

    reprepro: update 5.3.0_5 bottle.

--- Formula/reprepro.rb
+++ Formula/reprepro.rb
@@ -3,19 +3,15 @@ class Reprepro < Formula
   homepage "https://salsa.debian.org/brlink/reprepro"
   url "https://deb.debian.org/debian/pool/main/r/reprepro/reprepro_5.3.0.orig.tar.gz"
   sha256 "5a5404114b43a2d4ca1f8960228b1db32c41fb55de1996f62bc1b36001f3fab4"
-  revision 3
+  license "GPL-2.0-only"
+  revision 5
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "fd7c803d65c1220c5f683324a6ec01c65b58262bd5c8e0edadbd5fca8247bb31"
-    sha256                               arm64_big_sur:  "693fdd1c5fca04420ddc514398668a446fdc70a0d9ba9b3c1ee4a6fba0d9cb9e"
-    sha256 cellar: :any,                 monterey:       "1e9d326fc8472fce03ffa4b8121ee57a042e33e1dc5a9928c35c2af8d66cfb06"
-    sha256 cellar: :any,                 big_sur:        "0a1ef02efd94289dea92547ed6735422eaf66fd92a02d69472af8ae69bfdc056"
-    sha256 cellar: :any,                 catalina:       "92ecf42593483a44d3a39af6e7e3be0a4336f499ce19dcdbaac7294ef7f7b4b5"
-    sha256 cellar: :any,                 mojave:         "5478d8a1d013eaf8ce47c4c5b2e0afab9b2dbd76b4f4d3dbe09e6f0efa0683b0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "748c1afa5578eb56c95c19ccb7c7f850eb44596f69d21ebb8fe25060f1f1224f"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/reprepro"
+    sha256 cellar: :any, mojave: "4e7a781e795ae0e36b2351cd627d53ca685df7834899b712d3758e81f4154e9d"
   end
 
-  depends_on "berkeley-db@4"
+  depends_on "berkeley-db@5"
   depends_on "gpgme"
   depends_on "libarchive"
   depends_on "xz"
