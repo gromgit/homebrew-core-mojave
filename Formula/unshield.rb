ec5ed2826699222b1afdcf7ed0c9d2bba5c7e4b7
Adrian Ho <the.gromgit@gmail.com>
2022-10-06 08:39:24 +0800

    unshield: update 1.5.1_1 bottle.

--- Formula/unshield.rb
+++ Formula/unshield.rb
@@ -4,11 +4,12 @@ class Unshield < Formula
   url "https://github.com/twogood/unshield/archive/1.5.1.tar.gz"
   sha256 "34cd97ff1e6f764436d71676e3d6842dc7bd8e2dd5014068da5c560fe4661f60"
   license "MIT"
+  revision 1
   head "https://github.com/twogood/unshield.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/unshield"
-    sha256 mojave: "603db0951be78a7c4d69cf3f7dc8318582923492b2d9e4cc991481c1b3c0e5aa"
+    sha256 cellar: :any, mojave: "17bbe799b9c8ea91c6cdf098aa42e627dd1a0a24eb827e7831a09c9d8fe96cb0"
   end
 
   depends_on "cmake" => :build
@@ -17,11 +18,13 @@ class Unshield < Formula
   uses_from_macos "zlib"
 
   def install
+    # cmake check for libiconv will miss the OS library without this hint
+    ENV.append "LDFLAGS", "-liconv" if OS.mac?
     system "cmake", ".", *std_cmake_args
     system "make", "install"
   end
 
   test do
-    system bin/"unshield", "-V"
+    system bin/"unshield", "-e", "sjis", "-V"
   end
 end
