7de41c784ae96d656fdfd6a23085fd104cb9e7e3
Adrian Ho <the.gromgit@gmail.com>
2022-10-06 12:25:38 +0800

    webalizer: update 2.23-08_3 bottle.

--- Formula/webalizer.rb
+++ Formula/webalizer.rb
@@ -5,24 +5,16 @@ class Webalizer < Formula
   mirror "https://deb.debian.org/debian/pool/main/w/webalizer/webalizer_2.23.08.orig.tar.gz"
   sha256 "edaddb5aa41cc4a081a1500e3fa96615d4b41bc12086bcedf9938018ce79ed8d"
   license "GPL-2.0-or-later"
-  revision 2
+  revision 3
 
   bottle do
-    sha256 arm64_monterey: "83f6bd5042bab75a5ffbfc154d31217c4a7ca770d105ae8fa19163d398f196d9"
-    sha256 arm64_big_sur:  "e5dd2dbc62dc88bcae72d46858d1ffc12bd32504e791419487e543bb0cc016c9"
-    sha256 monterey:       "5dc996c4c3ba12ebe94f4e11efe803128014d66a2b366baa4ef9bdaaad36da16"
-    sha256 big_sur:        "c2b261ea2ecd03ee71f43ecc38ca50ad2de689199fafe0711bc639da2a0af94c"
-    sha256 catalina:       "c7b023658cc745cb0e5d383953e23a2d5a07dcf08b8e4addee7b7a108ef3a725"
-    sha256 mojave:         "525c739550139303d96d823e9f50aca6255bb77eac70d45f2c1259aa59755f6b"
-    sha256 high_sierra:    "e27c0dd7038a5a82e6fa127428c0b98750801e343b1b973b05bb08f38b055cdd"
-    sha256 sierra:         "cb42abb300bb5dc9639c811a13e24cca1be2cceee01d02eabb1ec149414569d4"
-    sha256 el_capitan:     "2bae3de97730aa72807cadcfda25ac395f3e30608d865df998fb474e75d4c780"
-    sha256 x86_64_linux:   "162c45cacfbf8619b563fd65ca79010add2bc05609886c27d3c15ed25c7fb22f"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/webalizer"
+    sha256 mojave: "7ab82c8c113c9ac3db7fb277e5d1365e84a71c74031a57dfc036898923169438"
   end
 
   deprecate! date: "2022-05-29", because: :unmaintained
 
-  depends_on "berkeley-db"
+  depends_on "berkeley-db@5"
   depends_on "gd"
   depends_on "libpng"
 
