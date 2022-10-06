35d2cd7cfa997b28899dd915878325f2483e3f38
Adrian Ho <the.gromgit@gmail.com>
2022-10-06 12:19:27 +0800

    watchexec: update 1.20.6 bottle.

--- Formula/watchexec.rb
+++ Formula/watchexec.rb
@@ -1,8 +1,8 @@
 class Watchexec < Formula
   desc "Execute commands when watched files change"
   homepage "https://github.com/watchexec/watchexec"
-  url "https://github.com/watchexec/watchexec/archive/cli-v1.20.5.tar.gz"
-  sha256 "2bc04c7ecf58d34a48c3eeea54a76b7e621717cb93305497bea2f6399dd119c6"
+  url "https://github.com/watchexec/watchexec/archive/cli-v1.20.6.tar.gz"
+  sha256 "fa490944bbc8eafdc585454d27ec6f75988ce7d159db8ee1244b1fc5bbd86935"
   license "Apache-2.0"
   head "https://github.com/watchexec/watchexec.git", branch: "main"
 
@@ -13,7 +13,7 @@ class Watchexec < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/watchexec"
-    sha256 cellar: :any_skip_relocation, mojave: "308de9f12f44501b4a8167a4d17c5979c829121e82ab76c1470a8b4ce1b56098"
+    sha256 cellar: :any_skip_relocation, mojave: "2cac2e1ec7058cea408fb63abd971f2d6d0a8a3614d5145d113fb6cf3b5ba4f4"
   end
 
   depends_on "rust" => :build
