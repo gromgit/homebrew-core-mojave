468d3ab903f5b7fe44a01838728da8dba0758660
Adrian Ho <the.gromgit@gmail.com>
2022-10-06 14:14:58 +0800

    yosys: update 0.21 bottle.

--- Formula/yosys.rb
+++ Formula/yosys.rb
@@ -1,23 +1,23 @@
 class Yosys < Formula
   desc "Framework for Verilog RTL synthesis"
   homepage "https://yosyshq.net/yosys/"
-  url "https://github.com/YosysHQ/yosys/archive/yosys-0.20.tar.gz"
-  sha256 "ee261487badf1b554616d555da8496a7c84ef21ae66a979ddd946b6949a780a4"
+  url "https://github.com/YosysHQ/yosys/archive/yosys-0.21.tar.gz"
+  sha256 "2b0e140f47d682e1069b1ca53b1fd91cbb1c1546932bd5cb95566f59a673cd8d"
   license "ISC"
   head "https://github.com/YosysHQ/yosys.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/yosys"
-    sha256 mojave: "1615b9487f365f3154b44efa1143801c5f55f0f0c6676531e4ca8f5721bdd5f0"
+    sha256 mojave: "9bd0f0af25fadc2952f8a6867a80427512b06808d8af121eebfb7d01a9808bdc"
   end
 
   depends_on "bison" => :build
   depends_on "pkg-config" => :build
-  depends_on "libffi"
   depends_on "python@3.10"
   depends_on "readline"
 
   uses_from_macos "flex"
+  uses_from_macos "libffi", since: :catalina
   uses_from_macos "tcl-tk"
 
   def install
@@ -25,6 +25,6 @@ class Yosys < Formula
   end
 
   test do
-    system "#{bin}/yosys", "-p", "hierarchy; proc; opt; techmap; opt;", "-o", "synth.v", "#{pkgshare}/adff2dff.v"
+    system bin/"yosys", "-p", "hierarchy; proc; opt; techmap; opt;", "-o", "synth.v", pkgshare/"adff2dff.v"
   end
 end
