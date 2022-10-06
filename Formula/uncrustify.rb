42af4fc9d7429c3236b11d518d678fccadb06ea6
Adrian Ho <the.gromgit@gmail.com>
2022-10-06 08:35:40 +0800

    uncrustify: update 0.75.1 bottle.

--- Formula/uncrustify.rb
+++ Formula/uncrustify.rb
@@ -8,15 +8,12 @@ class Uncrustify < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/uncrustify"
-    sha256 cellar: :any_skip_relocation, mojave: "f1178b7db8d8c435d8b8d8d0f362ca8fa5e289a05a3c022ee2228432427fdffd"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "beaf25a2bb6c629759b246cbfe095b165dcf615433ff5f51a4a50a5687e323ae"
   end
 
   depends_on "cmake" => :build
   uses_from_macos "python" => :build
-
-  on_linux do
-    depends_on "gcc"
-  end
 
   fails_with gcc: "5"
 
