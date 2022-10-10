4cee58dc01f7cb7dd030fe4cfb987f933ab1ec17
Adrian Ho <the.gromgit@gmail.com>
2022-10-10 20:15:14 +0800

    rsyslog: update 8.2208.0 bottle.

--- Formula/rsyslog.rb
+++ Formula/rsyslog.rb
@@ -1,10 +1,9 @@
 class Rsyslog < Formula
   desc "Enhanced, multi-threaded syslogd"
   homepage "https://www.rsyslog.com/"
-  url "https://www.rsyslog.com/files/download/rsyslog/rsyslog-8.2206.0.tar.gz"
-  sha256 "a1377218b26c0767a7a3f67d166d5338af7c24b455d35ec99974e18e6845ba27"
+  url "https://www.rsyslog.com/files/download/rsyslog/rsyslog-8.2208.0.tar.gz"
+  sha256 "14de68e7b8e5ab0c5d734f82e2dc9fff22cd7f4710ad690727eb10a7b9b3df5e"
   license all_of: ["Apache-2.0", "GPL-3.0-or-later", "LGPL-3.0-or-later"]
-  revision 1
 
   livecheck do
     url "https://www.rsyslog.com/downloads/download-v8-stable/"
@@ -13,7 +12,7 @@ class Rsyslog < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rsyslog"
-    sha256 mojave: "3e3358ab23f08938c9784ffdebdb4e3e435ce48caf16087a4e90c993bc4d9fc6"
+    sha256 cellar: :any_skip_relocation, mojave: "9480ae4798842d9e2a05a06461babeb70f5961be46d1806710f64b2ed6c66fb1"
   end
 
   depends_on "pkg-config" => :build
