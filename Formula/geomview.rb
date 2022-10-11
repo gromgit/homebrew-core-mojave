7b659180c0133931b2da4fd3113ce223a3eef891
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:46:42 +0800

    geomview: no rebuilding required

--- Formula/geomview.rb
+++ Formula/geomview.rb
@@ -13,12 +13,14 @@ class Geomview < Formula
   end
 
   bottle do
-    sha256                               monterey:     "16501f149c43a7875f49f90b1c419c982d927a74e06a2624e31b12d91cd45dd8"
-    sha256                               big_sur:      "5b32a3b889e22a91b57549a11fc2d841c773d1f843886d5d42c003bb8797b0e0"
-    sha256                               catalina:     "8fcdf484eb6699c2f4c5bc46dec876ba9b4439d39a2dcc6342f63eec019decf4"
-    sha256                               mojave:       "ff34b05281e51f09386f1c1ae150ec0fee0d1c8c7afe74a63fec22c7add9285c"
-    sha256                               high_sierra:  "8c92e54836c38a56cbb22a0488dab7665d11fd44d918956a899bb4ef2175d338"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ff5407aa61b9c6efadf39cc5831c02b45f924c3926ed9cd1475be4ed81c12796"
+    sha256                               arm64_monterey: "171b71c0d54b089d996a04f0a0febeea6d0cfdadc25d96c83cc30824db5c36ce"
+    sha256                               arm64_big_sur:  "d425aa4b27f1d1ba8b02c8b5b907b9d7fd5f2bbef8d8e67d1d616a51913a9b70"
+    sha256                               monterey:       "16501f149c43a7875f49f90b1c419c982d927a74e06a2624e31b12d91cd45dd8"
+    sha256                               big_sur:        "5b32a3b889e22a91b57549a11fc2d841c773d1f843886d5d42c003bb8797b0e0"
+    sha256                               catalina:       "8fcdf484eb6699c2f4c5bc46dec876ba9b4439d39a2dcc6342f63eec019decf4"
+    sha256                               mojave:         "ff34b05281e51f09386f1c1ae150ec0fee0d1c8c7afe74a63fec22c7add9285c"
+    sha256                               high_sierra:    "8c92e54836c38a56cbb22a0488dab7665d11fd44d918956a899bb4ef2175d338"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "ff5407aa61b9c6efadf39cc5831c02b45f924c3926ed9cd1475be4ed81c12796"
   end
 
   depends_on "libice"
@@ -38,9 +40,12 @@ class Geomview < Formula
   end
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    # Work around for build error due to `finite`/`isfinite` detection on macOS.
+    # ../../../../include/porting.h:68:19: error: expected identifier or '('
+    # static inline int finite(double v)
+    ENV["ac_cv_func_finite"] = "yes" if OS.mac? && Hardware::CPU.arm?
+
+    system "./configure", *std_configure_args, "--disable-silent-rules"
     system "make", "install"
     (bin/"hvectext").unlink
   end
