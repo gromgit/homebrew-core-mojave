cdd8b5d76f9e86ed10d39a7f7560f89b702c8438
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:42:39 +0800

    adwaita-icon-theme: no rebuilding required

--- Formula/adwaita-icon-theme.rb
+++ Formula/adwaita-icon-theme.rb
@@ -1,12 +1,12 @@
 class AdwaitaIconTheme < Formula
   desc "Icons for the GNOME project"
   homepage "https://developer.gnome.org"
-  url "https://download.gnome.org/sources/adwaita-icon-theme/42/adwaita-icon-theme-42.0.tar.xz"
-  sha256 "5e85b5adc8dee666900fcaf271ba717f7dcb9d0a03d96dae08f9cbd27e18b1e0"
+  url "https://download.gnome.org/sources/adwaita-icon-theme/43/adwaita-icon-theme-43.tar.xz"
+  sha256 "2e3ac77d32a6aa5554155df37e8f0a0dd54fc5a65fd721e88d505f970da32ec6"
   license any_of: ["LGPL-3.0-or-later", "CC-BY-SA-3.0"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "7147f4f2c8fda528087c519e7cfac98cefbf14033f5ddef307a44ab0cec386a0"
+    sha256 cellar: :any_skip_relocation, all: "79f3f53c3f0d0bf11c7bec0f906fd2f87c3cec7009622f15b6dde5fa1a0557d5"
   end
 
   depends_on "gettext" => :build
