8af5d198ac9e7a22936f09f68e1996fefc94cda4
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:52:49 +0800

    repo: no rebuilding required

--- Formula/repo.rb
+++ Formula/repo.rb
@@ -4,13 +4,13 @@ class Repo < Formula
   desc "Repository tool for Android development"
   homepage "https://source.android.com/source/developing.html"
   url "https://gerrit.googlesource.com/git-repo.git",
-      tag:      "v2.29.2",
-      revision: "891e8f72ce3551a19c377456574bbfbeac5c8b8e"
+      tag:      "v2.29.3",
+      revision: "acc4c857a0b6e2e33e3647028cbd312498fd5900"
   license "Apache-2.0"
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "875deb0141c851253a13512c84231192b0cc1e8aa251f542bb820562c5cb8f51"
+    sha256 cellar: :any_skip_relocation, all: "8d7aa92c76d4487745f162dc8892a0abbdc6edfa3e0c24efcab2d4beec3cad5f"
   end
 
   uses_from_macos "python"
