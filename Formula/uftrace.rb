7885434c99d27bdc30f1b00d1ced5bc6dedc3951
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:54:55 +0800

    uftrace:

--- Formula/uftrace.rb
+++ Formula/uftrace.rb
@@ -4,10 +4,11 @@ class Uftrace < Formula
   url "https://github.com/namhyung/uftrace/archive/v0.12.tar.gz"
   sha256 "2aad01f27d4f18717b681824c7a28ac3e1efd5e7bbed3ec888a3ea5af60e3700"
   license "GPL-2.0-only"
+  revision 1
   head "https://github.com/namhyung/uftrace.git", branch: "master"
 
   bottle do
-    sha256 x86_64_linux: "908d433c341a9d130f8113b09af7979bcccd3b1b0c544544a2ed2e4dcf7eaea5"
+    sha256 x86_64_linux: "3094d4d4da602ec0f30e172f64eb73a2ee3d20987632c5de99f1aba4ad7963fd"
   end
 
   depends_on "pandoc" => :build
@@ -16,7 +17,7 @@ class Uftrace < Formula
   depends_on "elfutils"
   depends_on "libunwind"
   depends_on :linux
-  depends_on "luajit-openresty"
+  depends_on "luajit"
   depends_on "ncurses"
   depends_on "python@3.10"
 
