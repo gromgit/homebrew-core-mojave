278207b69717c9b2abbaa9cbc943c20633ba073e
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:49:20 +0800

    licenseplist:

--- Formula/licenseplist.rb
+++ Formula/licenseplist.rb
@@ -1,15 +1,15 @@
 class Licenseplist < Formula
   desc "License list generator of all your dependencies for iOS applications"
   homepage "https://www.slideshare.net/mono0926/licenseplist-a-license-list-generator-of-all-your-dependencies-for-ios-applications"
-  url "https://github.com/mono0926/LicensePlist/archive/refs/tags/3.22.5.tar.gz"
-  sha256 "568c3bb40784ee59d5ebf12a6249d95f2eda2f2aabd43ad5be57e1c5db5f7b08"
+  url "https://github.com/mono0926/LicensePlist/archive/refs/tags/3.23.4.tar.gz"
+  sha256 "af46239887893862d8aea5c88a596f1e0b10626de9dd31bc6bdc6c906f91290a"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "f2f7c4a320808606bdf961df95bfc76edeb312aaa59b7e1ee69a5c33c2cd5759"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d9aa8a67a2afa0679a5ef003ca87fe00186bfdea451f429d157c0742ca77b119"
-    sha256 cellar: :any_skip_relocation, monterey:       "1ba7caa7a85d1180f6f1324268a095a21c94e2e04b37241c19c1431821b9c1b6"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0d12059cb4c4bfa51df8b8512defd0a2bcf48b2bfc11d2fc78e6d546a51031ca"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ec02edd588d04760511dbe68588d342995896dc269c91920bb871e77ffebc196"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ee9275ac68ddc69538a9fb8e6ce2261220bc56e2654bf444ef33fcf00575006a"
+    sha256 cellar: :any_skip_relocation, monterey:       "c7ec27a0485dc26db6f82e0530e61f2c7247e27e7ced6d90e99d74720483ec02"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f5472f9f6f7b2d8e3fc9ccd8dfe8b488799f3666b29c7895a5e57976f0cb5e64"
   end
 
   depends_on xcode: ["13.0", :build]
