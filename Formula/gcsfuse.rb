118620f1e3b76c2d85e72b9358c8dbe5ace9afe4
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:46:35 +0800

    gcsfuse:

--- Formula/gcsfuse.rb
+++ Formula/gcsfuse.rb
@@ -1,13 +1,13 @@
 class Gcsfuse < Formula
   desc "User-space file system for interacting with Google Cloud"
   homepage "https://github.com/googlecloudplatform/gcsfuse"
-  url "https://github.com/GoogleCloudPlatform/gcsfuse/archive/v0.41.0.tar.gz"
-  sha256 "7c3833ed090da8aa762fe831fd7c27d73273c5f47ce431dff4100c330bf53047"
+  url "https://github.com/GoogleCloudPlatform/gcsfuse/archive/v0.41.7.tar.gz"
+  sha256 "07aee73827f06ca823f29020b273969d93494647bf2990847e809c643914c667"
   license "Apache-2.0"
   head "https://github.com/GoogleCloudPlatform/gcsfuse.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "a27112808fc54795d62d27a921977a88428587c1c1415050d7934634ac0cf638"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c40d4ec48be72166f8091b734b3475bedb2a022826242ac2732776e347ad5247"
   end
 
   depends_on "go" => :build
