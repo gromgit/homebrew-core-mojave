b7366c913ed776946fbad880b98e9f5a0657e52d
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:49:16 +0800

    libva:

--- Formula/libva.rb
+++ Formula/libva.rb
@@ -1,8 +1,8 @@
 class Libva < Formula
   desc "Hardware accelerated video processing library"
   homepage "https://github.com/intel/libva"
-  url "https://github.com/intel/libva/releases/download/2.15.0/libva-2.15.0.tar.bz2"
-  sha256 "258ed409458d3e7a000e1908bc852619615ee6a933359c745fcd93eb3e461eca"
+  url "https://github.com/intel/libva/releases/download/2.16.0/libva-2.16.0.tar.bz2"
+  sha256 "115faca0db2c417a37da49830870e260450fa13066f5e24e252a20c36d920a1c"
   license "MIT"
 
   livecheck do
@@ -11,7 +11,7 @@ class Libva < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c1e24c5c8d5dd843392730f054a8c5af5473d32ff0e07df7a1b7b019f56526c7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "92709fe5f7c72fafee362eb1ea09e541966b0f42765815966e72f2a8f792e7fe"
   end
 
   depends_on "pkg-config" => [:build, :test]
