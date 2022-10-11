02805fe213cc7c839ddd0fc7c7ffb39b59b49174
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:42:48 +0800

    apache-geode: no rebuilding required

--- Formula/apache-geode.rb
+++ Formula/apache-geode.rb
@@ -1,14 +1,14 @@
 class ApacheGeode < Formula
   desc "In-memory Data Grid for fast transactional data processing"
   homepage "https://geode.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=geode/1.15.0/apache-geode-1.15.0.tgz"
-  mirror "https://archive.apache.org/dist/geode/1.15.0/apache-geode-1.15.0.tgz"
-  mirror "https://downloads.apache.org/geode/1.15.0/apache-geode-1.15.0.tgz"
-  sha256 "97cd96e94991cbd433d93e8474e1c2e65deb92f022d810a1931464017701701b"
+  url "https://www.apache.org/dyn/closer.lua?path=geode/1.15.1/apache-geode-1.15.1.tgz"
+  mirror "https://archive.apache.org/dist/geode/1.15.1/apache-geode-1.15.1.tgz"
+  mirror "https://downloads.apache.org/geode/1.15.1/apache-geode-1.15.1.tgz"
+  sha256 "2668970982d373ef42cff5076e7073b03e82c8e2fcd7757d5799b2506e265d57"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "5c9144215ae6053dfa5182e3245cac977dd2007200b695fa0aca0ccbcd31e26b"
+    sha256 cellar: :any_skip_relocation, all: "5cf4c4a321ae1e5a768773e91021ee8d7d0c4555c2209e532b08efd26929d512"
   end
 
   depends_on "openjdk@11"
