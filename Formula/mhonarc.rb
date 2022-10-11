d6d988bc2eabecfd37062384c0841e1d9d28e17a
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:49:56 +0800

    mhonarc: no rebuilding required

--- Formula/mhonarc.rb
+++ Formula/mhonarc.rb
@@ -4,7 +4,7 @@ class Mhonarc < Formula
   url "https://www.mhonarc.org/release/MHonArc/tar/MHonArc-2.6.19.tar.bz2"
   sha256 "08912eae8323997b940b94817c83149d2ee3ed11d44f29b3ef4ed2a39de7f480"
   license "GPL-2.0-or-later"
-  revision 3
+  revision 4
 
   livecheck do
     url :homepage
@@ -12,7 +12,7 @@ class Mhonarc < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "fe9730eed078aea4b9fcfda2c0f7321ca9b899a3b720802355f84731db643df3"
+    sha256 cellar: :any_skip_relocation, all: "bf14803e198aabe4b95f50db39691499f43fa2aff77d7c90760782eeb4c9cbdb"
   end
 
   depends_on "perl"
