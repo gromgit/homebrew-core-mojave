938989221804da222adbea83a3625fe32c4c1dfc
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:53:26 +0800

    seexpr: no rebuilding required

--- Formula/seexpr.rb
+++ Formula/seexpr.rb
@@ -11,13 +11,14 @@ class Seexpr < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "d525a96b58f896e76192699ed10018bc992287604d1902c7c8614ebc508b7fe7"
-    sha256 cellar: :any, arm64_big_sur:  "722581c38cd9860d5dc9b53f85b9560f3c1039b49a17180114c6f2370556fc86"
-    sha256 cellar: :any, monterey:       "45a50904ddbd4940674d0c844878effb7f7175af28c37feabb0f6ee52509e48d"
-    sha256 cellar: :any, big_sur:        "8045ec68c468b5db8a118006756e34c54425bbcc3e29306c16a52e86ccefdbb7"
-    sha256 cellar: :any, catalina:       "2a55400ad86255b300843f7cde1dbed4130d0ba26ffc4c8725fec83b50e7f9e3"
-    sha256 cellar: :any, mojave:         "e5ba2fcca24837fc43d11524fdeff04d9f4429f6c66421dec6c1925b60893f82"
-    sha256 cellar: :any, high_sierra:    "b5a3d64c08f692d25d3eb12dd9409c414939303b0b9f19396c95a13d07b46fa9"
+    sha256 cellar: :any,                 arm64_monterey: "d525a96b58f896e76192699ed10018bc992287604d1902c7c8614ebc508b7fe7"
+    sha256 cellar: :any,                 arm64_big_sur:  "722581c38cd9860d5dc9b53f85b9560f3c1039b49a17180114c6f2370556fc86"
+    sha256 cellar: :any,                 monterey:       "45a50904ddbd4940674d0c844878effb7f7175af28c37feabb0f6ee52509e48d"
+    sha256 cellar: :any,                 big_sur:        "8045ec68c468b5db8a118006756e34c54425bbcc3e29306c16a52e86ccefdbb7"
+    sha256 cellar: :any,                 catalina:       "2a55400ad86255b300843f7cde1dbed4130d0ba26ffc4c8725fec83b50e7f9e3"
+    sha256 cellar: :any,                 mojave:         "e5ba2fcca24837fc43d11524fdeff04d9f4429f6c66421dec6c1925b60893f82"
+    sha256 cellar: :any,                 high_sierra:    "b5a3d64c08f692d25d3eb12dd9409c414939303b0b9f19396c95a13d07b46fa9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "701bb51401ec76db34e2e1a006c37abb5a4fe480980e34ec03adadd137efb150"
   end
 
   depends_on "cmake" => :build
