6bff8e216d0e73873bbe75c8233e9cd10ca006fc
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:44:42 +0800

    create-api:

--- Formula/create-api.rb
+++ Formula/create-api.rb
@@ -1,17 +1,17 @@
 class CreateApi < Formula
   desc "Delightful code generator for OpenAPI specs"
   homepage "https://github.com/CreateAPI/CreateAPI"
-  url "https://github.com/CreateAPI/CreateAPI/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "eb05a0129153841e9af300acae10533a6926689fd6e5ed67ddb69c1c18b85e36"
+  url "https://github.com/CreateAPI/CreateAPI/archive/refs/tags/0.1.1.tar.gz"
+  sha256 "2a0529e700a1e3dd4a0dbbaa3c68340adf7ce6a331b85bfbcd42b494e5cf5ddb"
   license "MIT"
   head "https://github.com/CreateAPI/CreateAPI.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "663256fe1d561dfa58ef70014d2c9cc37bdd716a40dc2c4d693e4fdeddc6e800"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "18af8d07ebfbe51615112e91ca15307c3a0f896222dbe486f6a2a4693ab093fb"
-    sha256 cellar: :any_skip_relocation, monterey:       "4d1c0939f7c82d1658bdd743dc5cff94a5d96e9af39b0c1b06585aa67333327f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "324849f6e0d1d33179feebd75e67ba132e231434dd135b500ee27834680671f9"
-    sha256                               x86_64_linux:   "6f1c9f9e99281849add956b3fe40c08127abb00b15bd47de3b384b3785d15f38"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c7f83cdedc16bd6b86dddc7931ed1b8de1c6fa1894a5d5d5fa9c6b8552bc00a9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4f8b21dc1fa43c8777e273fc8666337a23fc676e9127d8740344c53284707f38"
+    sha256 cellar: :any_skip_relocation, monterey:       "4889a2c84ee571408001aa8f2000552d6243cc725ea4e86836d05a01ef01e1cc"
+    sha256 cellar: :any_skip_relocation, big_sur:        "4b161768bb43068fae43e52d377b38b1f0e5e0602ceffafa478634eea8cd8804"
+    sha256                               x86_64_linux:   "0b3dc2c272ebf9fd748aeacdaa5eba1fcac7bb9a1fdba49d8b5083aa7937c8c3"
   end
 
   depends_on xcode: "13.0"
