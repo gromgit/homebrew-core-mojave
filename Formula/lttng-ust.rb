03a89e9fed6cfeb96ad7782dfb7eca1c505af4e2
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:49:35 +0800

    lttng-ust:

--- Formula/lttng-ust.rb
+++ Formula/lttng-ust.rb
@@ -1,8 +1,8 @@
 class LttngUst < Formula
   desc "Linux Trace Toolkit Next Generation Userspace Tracer"
   homepage "https://lttng.org/"
-  url "https://lttng.org/files/lttng-ust/lttng-ust-2.13.4.tar.bz2"
-  sha256 "698f82ec5dc56e981c0bb08c46ebabaf31c60e877c2e365b9fd6d3a9fff8b398"
+  url "https://lttng.org/files/lttng-ust/lttng-ust-2.13.5.tar.bz2"
+  sha256 "f1d7bb4984a3dc5dacd3b7bcb4c10c04b041b0eecd7cba1fef3d8f86aff02bd6"
   license all_of: ["LGPL-2.1-only", "MIT", "GPL-2.0-only", "BSD-3-Clause", "BSD-2-Clause", "GPL-3.0-or-later"]
 
   livecheck do
@@ -11,7 +11,7 @@ class LttngUst < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "1cf36e0dd2641f1fa4d31107277cfe363d63b55260ae8a8159ba7f78528f8302"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0f019a9945b2c99f4c1df697fa8e120465b20626154d295f854c36bcf99ee321"
   end
 
   depends_on "pkg-config" => :build
