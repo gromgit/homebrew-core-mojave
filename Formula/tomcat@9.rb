91bda3736dc037fd888ec83b1d11122cbe07eb51
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:54:41 +0800

    tomcat@9: no rebuilding required

--- Formula/tomcat@9.rb
+++ Formula/tomcat@9.rb
@@ -1,9 +1,9 @@
 class TomcatAT9 < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-9/v9.0.65/bin/apache-tomcat-9.0.65.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.65/bin/apache-tomcat-9.0.65.tar.gz"
-  sha256 "e92330e6b0103eb0a5af1ec42f8c0aaaf0da712b7449b962e181a191e8f2264b"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-9/v9.0.68/bin/apache-tomcat-9.0.68.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.68/bin/apache-tomcat-9.0.68.tar.gz"
+  sha256 "af1b2ff3310821b4de9780aa22e9dc4b9a5e9651b08076a62916b42a0cec385d"
   license "Apache-2.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class TomcatAT9 < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "491d0df379ee62963762be3d1b2b46df3e2ecf70f7f16d74ca9dc8dfaa66eafa"
+    sha256 cellar: :any_skip_relocation, all: "079fe3658eded45eb4212cd33651f3ec38d23b214dd4a8c8c1f33d2c70f29687"
   end
 
   keg_only :versioned_formula
