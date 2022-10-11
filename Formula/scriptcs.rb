26677522ef29756f33a075de74056ec825ac4ab8
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:53:16 +0800

    scriptcs: no rebuilding required

--- Formula/scriptcs.rb
+++ Formula/scriptcs.rb
@@ -6,9 +6,15 @@ class Scriptcs < Formula
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/scriptcs"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "b3ffaff58451e6e5cb107720f4e6d0981cb292b9fdab38f2361add255b16bb4b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d25a27968072aa23232a11aceed62f1f8edcbdbe31ac2637cdd5becba69bed6e"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f5187aaee21aced8951a79b8a23c8e9a870ce5e17da5d15837add0aeadb3f5dd"
+    sha256 cellar: :any_skip_relocation, monterey:       "630be0cd09a3dc16e8c7404875adfe89d009ce9a73f6d5dcae9700253a3c110c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9153f24a59a73f78f74915a8e1d8de488ec016deb1aac1825774e19d6420545d"
+    sha256 cellar: :any_skip_relocation, catalina:       "c14c80f7b24ac5f2c14119ec2f371416ae07b9fba8bd9b0eb90d57ef5104f765"
+    sha256 cellar: :any_skip_relocation, mojave:         "121137df4078b2819a16f0f3e75924b10eba51a3ca7ac0a4be3d9010d2d1f7aa"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "263fda7addb857a9ed3c0c15856c422d3684ad069c2efc644858bb1779a92e91"
+    sha256 cellar: :any_skip_relocation, sierra:         "9ccece2f779060ab23e699b07d6cc6ce0b2c2e0058cc995b1541e1170f69a6eb"
+    sha256 cellar: :any_skip_relocation, el_capitan:     "e3b6cb117d23ccf9a745e0ac5e61fcb531d7e8a08476699d2ece6c31e564450e"
   end
 
   depends_on "mono"
