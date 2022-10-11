57790a9bb47a708b0e3d21f95c831c1cfbe6734e
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:53:05 +0800

    s3-backer:

--- Formula/s3-backer.rb
+++ Formula/s3-backer.rb
@@ -1,17 +1,15 @@
 class S3Backer < Formula
   desc "FUSE-based single file backing store via Amazon S3"
   homepage "https://github.com/archiecobbs/s3backer"
-  url "https://github.com/archiecobbs/s3backer/archive/refs/tags/2.0.1.tar.gz"
-  sha256 "16fa56e9d126abf56f9ba610a5e8d487f95907bdd937b00610ff3f4b5ce11153"
+  # Release distributions listed at https://github.com/archiecobbs/s3backer/wiki/Downloads
+  url "https://s3.amazonaws.com/archie-public/s3backer/s3backer-2.0.2.tar.gz"
+  sha256 "0b2432f08e9b986364e35674f39dd11afc1670be382b23cdb7375e86ce132a02"
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "055a37d8f9b6dceeeca5798a68fa931526e1c10144fa0201adf0d02188aaa465"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5c8f5e8a937e4838c0bada7dcbd98727e6e9d73d5558652b75d0a7a1b8657b0c"
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "openssl@1.1"
 
@@ -27,9 +25,8 @@ class S3Backer < Formula
   end
 
   def install
-    system "./autogen.sh"
     inreplace "configure", "-lfuse", "-losxfuse" if OS.mac?
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", *std_configure_args, "--disable-silent-rules"
     system "make", "install"
   end
 
