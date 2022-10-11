d43376e5ba45f7f23d7950e99a41a90a5fcc2486
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:47:25 +0800

    grpc-swift:

--- Formula/grpc-swift.rb
+++ Formula/grpc-swift.rb
@@ -1,8 +1,8 @@
 class GrpcSwift < Formula
   desc "Swift language implementation of gRPC"
   homepage "https://github.com/grpc/grpc-swift"
-  url "https://github.com/grpc/grpc-swift/archive/1.10.0.tar.gz"
-  sha256 "cce2d04a26ab8c606d2da1f3cf732e96e6e68a7e373a4a441b68e0c45755755d"
+  url "https://github.com/grpc/grpc-swift/archive/1.11.0.tar.gz"
+  sha256 "5e5534fdfc8f7e7c94c405cde5da93d6aa249a54d62e6885c32d6683d0d54e39"
   license "Apache-2.0"
   head "https://github.com/grpc/grpc-swift.git", branch: "main"
 
@@ -12,11 +12,11 @@ class GrpcSwift < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ecb8e76fe49315d53c5083f8e796983f7e9ba00620dce293784488fb1a8f7846"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "881c6365a2169a55580a0c07ca2b9f210e77ad9fa3c421528d02d633e3156abe"
-    sha256 cellar: :any_skip_relocation, monterey:       "e89beff013b4dcfa1d1b638aacff1c78538970c6d6d2308d8eee390c366137db"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b123453d2d7dac94fefff62a3909e3baefb1566402d8e0839e8bac8bacf35168"
-    sha256                               x86_64_linux:   "69d9ff6d1500f7287b22f5cb89edfe6425760a1b97aac0fcd93cc3219dbe5a07"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6b4e051113568d7b1a6bf8faa3eac565c3fd3ef6046afc2958802f2a2ab32cb1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eb976caf749fe1979d324fa2c4e1d81213b59276f3dc16d95acaac9e9b8c9ebf"
+    sha256 cellar: :any_skip_relocation, monterey:       "af474b56b31e1ada9677d902038ff51c5a6e6b8e7e49e4b5c6e3ee9953f6fc8e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f0a2da96c038d7cdd29ed95a3ef6c683fd42eaa75f99e0622212222e1dbe6e47"
+    sha256                               x86_64_linux:   "5ad7ccefbb9616a3705ed6ce8edc34e0e6fcea9cc5f3ff9d0814434ac0f1c876"
   end
 
   depends_on xcode: ["12.5", :build]
