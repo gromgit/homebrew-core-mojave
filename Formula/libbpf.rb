d767bb882d10a40f987e58868b6c7fe51a2aaf16
Adrian Ho <aho+vm-mojave@03s.net>
2022-10-11 23:48:51 +0800

    libbpf:

--- Formula/libbpf.rb
+++ Formula/libbpf.rb
@@ -1,12 +1,12 @@
 class Libbpf < Formula
   desc "Berkeley Packet Filter library"
   homepage "https://github.com/libbpf/libbpf"
-  url "https://github.com/libbpf/libbpf/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "16701e7c3a731a03fe08dee9893c95a86c879192955d12bc7495645e9ab1bc6e"
+  url "https://github.com/libbpf/libbpf/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "3d6afde67682c909e341bf194678a8969f17628705af25f900d5f68bd299cb03"
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c746fb954a10b136e5bb2f88fd5d58cb9c60cd3948539381f12d4bc0c1c06ecb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9a904fba9aab6739edd75cec80c055aa7c8fcdeba114bf2f3cf9ef2096dd7a14"
   end
 
   depends_on "pkg-config" => :build
