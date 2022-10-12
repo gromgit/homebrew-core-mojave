b76091a6fd9a8ad6be0164cbde39653a47677718
Adrian Ho <the.gromgit@gmail.com>
2022-10-12 10:06:50 +0800

    bash: update 5.2.2 bottle.

--- Formula/bash.rb
+++ Formula/bash.rb
@@ -5,36 +5,22 @@ class Bash < Formula
   head "https://git.savannah.gnu.org/git/bash.git", branch: "master"
 
   stable do
-    url "https://ftp.gnu.org/gnu/bash/bash-5.1.tar.gz"
-    mirror "https://ftpmirror.gnu.org/bash/bash-5.1.tar.gz"
-    mirror "https://mirrors.kernel.org/gnu/bash/bash-5.1.tar.gz"
-    mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-5.1.tar.gz"
-    sha256 "cc012bc860406dcf42f64431bcd3d2fa7560c02915a601aba9cd597a39329baa"
-    version "5.1.16"
+    url "https://ftp.gnu.org/gnu/bash/bash-5.2.tar.gz"
+    mirror "https://ftpmirror.gnu.org/bash/bash-5.2.tar.gz"
+    mirror "https://mirrors.kernel.org/gnu/bash/bash-5.2.tar.gz"
+    mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-5.2.tar.gz"
+    sha256 "a139c166df7ff4471c5e0733051642ee5556c1cc8a4a78f145583c5c81ab32fb"
+    version "5.2.2"
 
     %w[
-      001 ebb07b3dbadd98598f078125d0ae0d699295978a5cdaef6282fe19adef45b5fa
-      002 15ea6121a801e48e658ceee712ea9b88d4ded022046a6147550790caf04f5dbe
-      003 22f2cc262f056b22966281babf4b0a2f84cb7dd2223422e5dcd013c3dcbab6b1
-      004 9aaeb65664ef0d28c0067e47ba5652b518298b3b92d33327d84b98b28d873c86
-      005 cccbb5e9e6763915d232d29c713007a62b06e65126e3dd2d1128a0dc5ef46da5
-      006 75e17d937de862615c6375def40a7574462210dce88cf741f660e2cc29473d14
-      007 acfcb8c7e9f73457c0fb12324afb613785e0c9cef3315c9bbab4be702f40393a
-      008 f22cf3c51a28f084a25aef28950e8777489072628f972b12643b4534a17ed2d1
-      009 e45cda953ab4b4b4bde6dc34d0d8ca40d1cc502046eb28070c9ebcd47e33c3ee
-      010 a2c8d7b2704eeceff7b1503b7ad9500ea1cb6e9393faebdb3acd2afdd7aeae2a
-      011 58191f164934200746f48459a05bca34d1aec1180b08ca2deeee3bb29622027b
-      012 10f189c8367c4a15c7392e7bf70d0ff6953f78c9b312ed7622303a779273ab98
-      013 c7acb66df435d284304c16ca83a5265f9edd9368612095b01a733d45c77ed5ad
-      014 6a4ee0c81b437b96279a792c1efcec4ba56f009195a318083db6b53b096f83d0
-      015 1b37692ef1f6cc3dcec246773443276066e6b1379868f8c14e01f4dfd4df80f0
-      016 8899144f76a5db1fb41a89ed881c9f19add95728dd71db324f772ef225c5384f
+      001 f42f2fee923bc2209f406a1892772121c467f44533bedfe00a176139da5d310a
+      002 45cc5e1b876550eee96f95bffb36c41b6cb7c07d33f671db5634405cd00fd7b8
     ].each_slice(2) do |p, checksum|
       patch :p0 do
-        url "https://ftp.gnu.org/gnu/bash/bash-5.1-patches/bash51-#{p}"
-        mirror "https://ftpmirror.gnu.org/bash/bash-5.1-patches/bash51-#{p}"
-        mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-5.1-patches/bash51-#{p}"
-        mirror "https://mirrors.kernel.org/gnu/bash/bash-5.1-patches/bash51-#{p}"
+        url "https://ftp.gnu.org/gnu/bash/bash-5.2-patches/bash52-#{p}"
+        mirror "https://ftpmirror.gnu.org/bash/bash-5.2-patches/bash52-#{p}"
+        mirror "https://mirrors.kernel.org/gnu/bash/bash-5.2-patches/bash52-#{p}"
+        mirror "https://mirrors.ocf.berkeley.edu/gnu/bash/bash-5.2-patches/bash52-#{p}"
         sha256 checksum
       end
     end
@@ -78,7 +64,7 @@ class Bash < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bash"
-    sha256 mojave: "9c65941b2622166f202d4634a0e504ee58c0d651213fc69e2b10cf7a3027830a"
+    sha256 mojave: "5fdec6e89ff7c8358af18bd5c2adc682e419d2c0203d31918bda7a7af24a3ce1"
   end
 
   def install
