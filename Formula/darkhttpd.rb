62899a0820cba044fa35b238fff7ba3cfcfdbc1c
Adrian Ho <the.gromgit@gmail.com>
2022-10-12 14:56:38 +0800

    darkhttpd: update 1.14 bottle.

--- Formula/darkhttpd.rb
+++ Formula/darkhttpd.rb
@@ -1,14 +1,13 @@
 class Darkhttpd < Formula
   desc "Small static webserver without CGI"
   homepage "https://unix4lyfe.org/darkhttpd/"
-  url "https://github.com/emikulic/darkhttpd/archive/v1.13.tar.gz"
-  sha256 "1d88c395ac79ca9365aa5af71afe4ad136a4ed45099ca398168d4a2014dc0fc2"
+  url "https://github.com/emikulic/darkhttpd/archive/v1.14.tar.gz"
+  sha256 "e063de9efa5635260c8def00a4d41ec6145226a492d53fa1dac436967670d195"
   license "ISC"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/darkhttpd"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "f417a0bf3183bc06b0e98a66b069937b9b70ac6cc9a99ebced2f1c8ebdaad761"
+    sha256 cellar: :any_skip_relocation, mojave: "9dfb91c9ff3141e2c57c924081e3d2d6c34976ef25fb2a61301ec07127d73e4c"
   end
 
   def install
