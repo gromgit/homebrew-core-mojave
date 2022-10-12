f912db540b7d74d9d1a18e1e1d35ecab20de5fd7
Adrian Ho <the.gromgit@gmail.com>
2022-10-12 10:30:16 +0800

    boxes: update 2.2.0 bottle.

--- Formula/boxes.rb
+++ Formula/boxes.rb
@@ -1,19 +1,14 @@
 class Boxes < Formula
   desc "Draw boxes around text"
   homepage "https://boxes.thomasjensen.com/"
-  url "https://github.com/ascii-boxes/boxes/archive/v2.1.1.tar.gz"
-  sha256 "95ae6b46e057a79c6414b8c0b5b561c3e9d886ab8123a4085d256edccce625f9"
-  license "GPL-2.0-only"
+  url "https://github.com/ascii-boxes/boxes/archive/v2.2.0.tar.gz"
+  sha256 "98b8e3cf5008f46f096d5775d129c34db9f718728bffb0f5d67ae89bb494102e"
+  license "GPL-3.0-only"
   head "https://github.com/ascii-boxes/boxes.git", branch: "master"
 
   bottle do
-    sha256 arm64_monterey: "e8b8eca263957a235a37c69e99c30753c5f784d3b2ca782a51da94a4c339cdf6"
-    sha256 arm64_big_sur:  "8a84a206ca3a46d2364dd51f3e025762545645ff161e593a60149fc55e7a1f97"
-    sha256 monterey:       "aeb6f5c6587634067ea8ba32f7a92cd1d3e8b7503ddd8c8afbaefd9f279e5e87"
-    sha256 big_sur:        "eba500de77351541b21e68725366e61c7f6452cf097d72d7098ebb752d9d6f8f"
-    sha256 catalina:       "26564383c477c7e9e77ae94ab8d4fdb26cbaf2d530b768d01f93b72c1567b0c8"
-    sha256 mojave:         "e42c888a34141a1cff3b52404f0e35d1de820f64b2d7d7bf973684a8e234b1bd"
-    sha256 x86_64_linux:   "4a7881b680878471dfb3ea8d9392439b5f857b7a61c6385f1c4bd405f2333891"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/boxes"
+    sha256 mojave: "218d36a4bbad3cf3528c1e3b26cda45fa7e20893098561049c5fa863c8eaaa13"
   end
 
   depends_on "bison" => :build
