714a71589f1457af8b52d2693885de026b3edbd0
Adrian Ho <the.gromgit@gmail.com>
2022-10-12 14:39:20 +0800

    curlcpp: update 1.5 bottle.

---
+++ Formula/curlcpp.rb
@@ -0,0 +1,69 @@
+class Curlcpp < Formula
+  desc "Object oriented C++ wrapper for CURL (libcurl)"
+  homepage "https://josephp91.github.io/curlcpp"
+  url "https://github.com/JosephP91/curlcpp/archive/refs/tags/1.5.tar.gz"
+  sha256 "d1c236be862965d5882a842be3112b16e7f49d6fdff9906f7aec71ee8669d60e"
+  license "MIT"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/curlcpp"
+    sha256 cellar: :any, mojave: "93b5bbff1fa09d58e46a8c34a30e57e390888495dbc17a2573d7e57a023df346"
+  end
+
+  depends_on "cmake" => :build
+
+  uses_from_macos "curl"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <iostream>
+      #include <ostream>
+
+      #include "curlcpp/curl_easy.h"
+      #include "curlcpp/curl_form.h"
+      #include "curlcpp/curl_ios.h"
+      #include "curlcpp/curl_exception.h"
+
+      using std::cout;
+      using std::endl;
+      using std::ostringstream;
+
+      using curl::curl_easy;
+      using curl::curl_ios;
+      using curl::curl_easy_exception;
+      using curl::curlcpp_traceback;
+
+      int main() {
+          // Create a stringstream object
+          ostringstream str;
+          // Create a curl_ios object, passing the stream object.
+          curl_ios<ostringstream> writer(str);
+
+          // Pass the writer to the easy constructor and watch the content returned in that variable!
+          curl_easy easy(writer);
+          easy.add<CURLOPT_URL>("https://google.com");
+          easy.add<CURLOPT_FOLLOWLOCATION>(1L);
+
+          try {
+              easy.perform();
+          } catch (curl_easy_exception &error) {
+              // If you want to print the last error.
+              std::cerr<<error.what()<<std::endl;
+
+              // If you want to print the entire error stack you can do
+              error.print_traceback();
+          }
+          return 0;
+      }
+    EOS
+
+    system ENV.cxx, "-std=c++11", "test.cpp", "-L#{lib}", "-lcurlcpp", "-lcurl", "-o", "test"
+    system "./test"
+  end
+end
