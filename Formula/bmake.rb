97c1eb80239e867a25933d06dfd559209cc7f18c
Adrian Ho <the.gromgit@gmail.com>
2022-10-12 10:24:14 +0800

    bmake: update 20220928 bottle.

--- Formula/bmake.rb
+++ Formula/bmake.rb
@@ -1,8 +1,8 @@
 class Bmake < Formula
   desc "Portable version of NetBSD make(1)"
   homepage "https://www.crufty.net/help/sjg/bmake.html"
-  url "https://www.crufty.net/ftp/pub/sjg/bmake-20220909.tar.gz"
-  sha256 "34e9704a93fc183e3f1d87a81f9eabeba3200052f57fed4a751a5968d6b70225"
+  url "https://www.crufty.net/ftp/pub/sjg/bmake-20220928.tar.gz"
+  sha256 "c804b77de3feb8e31dee2a4c9fb1e9ec24c5a34764e7a2815c88b4ed01650e90"
   license "BSD-3-Clause"
 
   livecheck do
@@ -12,7 +12,7 @@ class Bmake < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bmake"
-    sha256 mojave: "bcf3e4b626fcd1e8c0a4006a271dbfbf130867c3b8800774c31e76e9ac3d381b"
+    sha256 mojave: "cd6328ea681782e86ca39e516e325703fa74cdf271bc540bd6b3207629438e1b"
   end
 
   def install
