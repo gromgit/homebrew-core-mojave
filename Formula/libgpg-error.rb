244d88d6f9f4b7fbb451ee300b518f468173d5c9
Adrian Ho <the.gromgit@gmail.com>
2022-10-12 14:12:28 +0800

    libgpg-error: update 1.46 bottle.

--- Formula/libgpg-error.rb
+++ Formula/libgpg-error.rb
@@ -1,8 +1,8 @@
 class LibgpgError < Formula
   desc "Common error values for all GnuPG components"
   homepage "https://www.gnupg.org/related_software/libgpg-error/"
-  url "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.45.tar.bz2"
-  sha256 "570f8ee4fb4bff7b7495cff920c275002aea2147e9a1d220c068213267f80a26"
+  url "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.46.tar.bz2"
+  sha256 "b7e11a64246bbe5ef37748de43b245abd72cfcd53c9ae5e7fc5ca59f1c81268d"
   license "LGPL-2.1-or-later"
 
   livecheck do
@@ -12,21 +12,23 @@ class LibgpgError < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libgpg-error"
-    sha256 mojave: "e720bfc90a23a27747d25902c4bb8b909afd8811ff9e67b10b976c0e452cb904"
+    sha256 mojave: "99e807704c5f7e1e15b2a4d8715936f234159aabedf22b7363eb180b746d78b8"
   end
 
   def install
-    system "./configure", "--disable-dependency-tracking",
+    # NOTE: gpg-error-config is deprecated upstream, so we should remove this at some point.
+    # https://dev.gnupg.org/T5683
+    system "./configure", *std_configure_args,
                           "--disable-silent-rules",
-                          "--prefix=#{prefix}",
+                          "--enable-install-gpg-error-config",
                           "--enable-static"
     system "make", "install"
 
     # avoid triggering mandatory rebuilds of software that hard-codes this path
-    inreplace bin/"gpg-error-config", prefix, opt_prefix
+    inreplace [bin/"gpg-error-config", lib/"pkgconfig/gpg-error.pc"], prefix, opt_prefix
   end
 
   test do
-    system "#{bin}/gpg-error-config", "--libs"
+    system bin/"gpgrt-config", "--libs"
   end
 end
