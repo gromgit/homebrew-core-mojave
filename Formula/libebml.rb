2cad7742ee2b99746c17cc8794aace1408de0919
Adrian Ho <the.gromgit@gmail.com>
2022-10-13 15:28:43 +0800

    libebml: update 1.4.4 bottle.

--- Formula/libebml.rb
+++ Formula/libebml.rb
@@ -1,19 +1,10 @@
 class Libebml < Formula
   desc "Sort of a sbinary version of XML"
   homepage "https://www.matroska.org/"
+  url "https://dl.matroska.org/downloads/libebml/libebml-1.4.4.tar.xz"
+  sha256 "82dc5f83356cc9340aee76ed7512210b3a4edf5f346bc9c2c7044f55052687a7"
   license "LGPL-2.1-or-later"
   head "https://github.com/Matroska-Org/libebml.git", branch: "master"
-
-  # Remove stable block in next release with merged patch
-  stable do
-    url "https://dl.matroska.org/downloads/libebml/libebml-1.4.2.tar.xz"
-    sha256 "41c7237ce05828fb220f62086018b080af4db4bb142f31bec0022c925889b9f2"
-
-    # Fix compilation with GCC: error: 'numeric_limits' is not a member of 'std'
-    # Ported from https://github.com/Matroska-Org/libebml/commit/f0bfd53647961e799a43d918c46cf3b6bff89806
-    # Remove in the next release
-    patch :DATA
-  end
 
   livecheck do
     url "https://dl.matroska.org/downloads/libebml/"
@@ -22,8 +13,7 @@ class Libebml < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libebml"
-    rebuild 2
-    sha256 cellar: :any, mojave: "cb5601335ea62cbe703e5fcd76c100525cec7b89593e963d71a21f24696d07f7"
+    sha256 cellar: :any, mojave: "2efb0baf09c2081882141ccc4cde55736e553af17e8afdc54027f51efd33fbff"
   end
 
   depends_on "cmake" => :build
@@ -37,29 +27,3 @@ class Libebml < Formula
     end
   end
 end
-
-__END__
-diff --git a/src/EbmlString.cpp b/src/EbmlString.cpp
-index 27e55fdf6c98c52ab73c8f02d8c69e31505f93d7..4c05fcfea34988672f2d7084f34e874a6c99cfdc 100644
---- a/src/EbmlString.cpp
-+++ b/src/EbmlString.cpp
-@@ -34,6 +34,7 @@
-   \author Steve Lhomme     <robux4 @ users.sf.net>
- */
- #include <cassert>
-+#include <limits>
- 
- #include "ebml/EbmlString.h"
- 
-diff --git a/src/EbmlUnicodeString.cpp b/src/EbmlUnicodeString.cpp
-index 496a16acc293487e842bc5696b1fe2f93c204a12..99fc073776d228692475997fba9a5be3280ffc99 100644
---- a/src/EbmlUnicodeString.cpp
-+++ b/src/EbmlUnicodeString.cpp
-@@ -36,6 +36,7 @@
- */
- 
- #include <cassert>
-+#include <limits>
- 
- #include "ebml/EbmlUnicodeString.h"
- 
