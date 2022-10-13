8ab9dbeee08a7ba2ecb2eba046c33b5eff94004a
Adrian Ho <the.gromgit@gmail.com>
2022-10-13 15:58:56 +0800

    lightgbm: update 3.3.3 bottle.

--- Formula/lightgbm.rb
+++ Formula/lightgbm.rb
@@ -2,14 +2,13 @@ class Lightgbm < Formula
   desc "Fast, distributed, high performance gradient boosting framework"
   homepage "https://github.com/microsoft/LightGBM"
   url "https://github.com/microsoft/LightGBM.git",
-      tag:      "v3.3.2",
-      revision: "dce7e58b020bc14b69eefc31546c366971ecb2d9"
+      tag:      "v3.3.3",
+      revision: "a0a617dfff2e2e1637b72b7a2adbe5453e7f97e0"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lightgbm"
-    rebuild 1
-    sha256 cellar: :any, mojave: "9a6a1e970f5e7d08c2c8bf3c3a740e148db2cddf2dab47555aaa5b3c8728705f"
+    sha256 cellar: :any, mojave: "bdaa950f942356ea9b02ed16c0f74a1b42481b4a0018cc369eedb37054cfecde"
   end
 
   depends_on "cmake" => :build
