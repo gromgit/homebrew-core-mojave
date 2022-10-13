b3533d6093b24bde731eac7874f4f2d8ea40cd14
Adrian Ho <the.gromgit@gmail.com>
2022-10-13 16:08:59 +0800

    logtalk: update 3.58.0 bottle.

--- Formula/logtalk.rb
+++ Formula/logtalk.rb
@@ -1,9 +1,9 @@
 class Logtalk < Formula
   desc "Declarative object-oriented logic programming language"
   homepage "https://logtalk.org/"
-  url "https://github.com/LogtalkDotOrg/logtalk3/archive/lgt3570stable.tar.gz"
-  version "3.57.0"
-  sha256 "6ab16f5a5aca15ac638a5eeca24ec3489a7388d1961fc0de483b315c1b45c942"
+  url "https://github.com/LogtalkDotOrg/logtalk3/archive/lgt3580stable.tar.gz"
+  version "3.58.0"
+  sha256 "ac2c9dcbacdcd24ffe411cfa2dbbbab10e6b2a6075dc49f6ed19c534dd79c38e"
   license "Apache-2.0"
 
   livecheck do
@@ -13,7 +13,7 @@ class Logtalk < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/logtalk"
-    sha256 cellar: :any_skip_relocation, mojave: "73975750033dc259d4e1efbdd8138cff66335dcbf13b11dd9fea5a86f5dff790"
+    sha256 cellar: :any_skip_relocation, mojave: "499fe3deac24fc533aae0ab4956e0119cecce96c5440037a59c7c30a7d235205"
   end
 
   depends_on "gnu-prolog"
