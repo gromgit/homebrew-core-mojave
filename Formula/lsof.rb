59fcebf105d9cbf474b717dc4de588e9d0b518f0
Adrian Ho <the.gromgit@gmail.com>
2022-10-13 16:11:53 +0800

    lsof: update 4.96.3 bottle.

--- Formula/lsof.rb
+++ Formula/lsof.rb
@@ -1,17 +1,20 @@
 class Lsof < Formula
   desc "Utility to list open files"
   homepage "https://github.com/lsof-org/lsof"
-  url "https://github.com/lsof-org/lsof/archive/4.95.0.tar.gz"
-  sha256 "8ff4c77736cc7d9556da9e2c7614cc4292a12f1979f20bd520d3c6f64b66a4d7"
+  url "https://github.com/lsof-org/lsof/archive/4.96.3-freebsd.tar.gz"
+  sha256 "bcb673c547d234da327a8ee0d1ef59d3b7eac12e0c066242ec13cb706485560d"
   license "Zlib"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lsof"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "aafa7fdab49eaf6f6a56a2ca919e7505bdbdede831d3c40b32558deb6f673046"
+    sha256 cellar: :any_skip_relocation, mojave: "d9c2ae8fcd9f015c56e7c42d9f72750da0bd23354ddab00b1703d72f1efab4ee"
   end
 
   keg_only :provided_by_macos
+
+  on_linux do
+    depends_on "libtirpc"
+  end
 
   def install
     if OS.mac?
@@ -23,6 +26,8 @@ class Lsof < Formula
         dialects/darwin/kmem/machine.h
         dialects/darwin/libproc/machine.h
       ], "/usr/include", MacOS.sdk_path/"usr/include"
+    else
+      ENV["LSOF_INCLUDE"] = HOMEBREW_PREFIX/"include"
     end
 
     ENV["LSOF_CC"] = ENV.cc
