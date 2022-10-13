b2df3dd3021166d247359ec634b72c2c7fbb48f9
Adrian Ho <the.gromgit@gmail.com>
2022-10-13 14:56:04 +0800

    ii: update 2.0 bottle.

--- Formula/ii.rb
+++ Formula/ii.rb
@@ -1,8 +1,8 @@
 class Ii < Formula
   desc "Minimalist IRC client"
   homepage "https://tools.suckless.org/ii/"
-  url "https://dl.suckless.org/tools/ii-1.9.tar.gz"
-  sha256 "850cb323b583d261b51bda9993ee733334352a8e6ca1e2f02b57c154bf568c17"
+  url "https://dl.suckless.org/tools/ii-2.0.tar.gz"
+  sha256 "4f67afcd208c07939b88aadbf21497a702ad0a07f9b5a6ce861f9f39ffe5425b"
   license "MIT"
   head "https://git.suckless.org/ii", using: :git, branch: "master"
 
@@ -13,14 +13,14 @@ class Ii < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ii"
-    sha256 cellar: :any_skip_relocation, mojave: "eaf2377c34fe32906e36ddd6d4d5982c88219c4ed13bb7f09e569530c55a7604"
+    sha256 cellar: :any_skip_relocation, mojave: "e64f70bc6300a1c009911b6c5e2df2c2403b8428299e4a4ce1fcc55a5617b10e"
   end
 
   def install
     # macOS already provides strlcpy
     if OS.mac?
-      inreplace "config.mk" do |s|
-        s.gsub! "= -DNEED_STRLCPY -Os", "= -Os"
+      inreplace "Makefile" do |s|
+        s.gsub! "-D_DEFAULT_SOURCE -DNEED_STRLCPY", "-D_DEFAULT_SOURCE"
         s.gsub! "= strlcpy.o", "="
       end
     end
