bf9d3435604aa957eb21adc5c4c5e1c6837e368c
Adrian Ho <the.gromgit@gmail.com>
2022-10-14 23:23:26 +0800

    tmpreaper: update 1.6.17 bottle.

--- Formula/tmpreaper.rb
+++ Formula/tmpreaper.rb
@@ -1,14 +1,19 @@
 class Tmpreaper < Formula
   desc "Clean up files in directories based on their age"
   homepage "https://packages.debian.org/sid/tmpreaper"
-  url "https://deb.debian.org/debian/pool/main/t/tmpreaper/tmpreaper_1.6.16.tar.gz"
-  mirror "https://fossies.org/linux/misc/tmpreaper_1.6.16.tar.gz"
-  sha256 "e543acdd55bb50102c42015e6d399e8abb36ad818cbd3ca6cb1c905b5781e202"
+  url "https://deb.debian.org/debian/pool/main/t/tmpreaper/tmpreaper_1.6.17.tar.gz"
+  mirror "https://fossies.org/linux/misc/tmpreaper_1.6.17.tar.gz"
+  sha256 "1ca94d156eb68160ec9b6ed8b97d70fbee996de21437f0cf7d0c3b46709fecbc"
   license "GPL-2.0-only"
+
+  livecheck do
+    url "https://deb.debian.org/debian/pool/main/t/tmpreaper/"
+    regex(/href=.*?tmpreaper[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tmpreaper"
-    sha256 cellar: :any_skip_relocation, mojave: "f386bd2ba06b1d52baf77ce1b6c359c8ea70f39876ec3aa2b20af00f08390b1e"
+    sha256 cellar: :any_skip_relocation, mojave: "f401ba4b2f518bf971c396566252cec7f29ccfd3e14b134501750b0864764856"
   end
 
   depends_on "autoconf" => :build
