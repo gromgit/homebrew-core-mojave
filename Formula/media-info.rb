41085871a42ac6bedfe2a8f4502c1c2d5e740a99
Adrian Ho <the.gromgit@gmail.com>
2022-10-14 17:15:49 +0800

    media-info: update 22.09 bottle.

--- Formula/media-info.rb
+++ Formula/media-info.rb
@@ -1,8 +1,8 @@
 class MediaInfo < Formula
   desc "Unified display of technical and tag data for audio/video"
   homepage "https://mediaarea.net/"
-  url "https://mediaarea.net/download/binary/mediainfo/22.06/MediaInfo_CLI_22.06_GNU_FromSource.tar.bz2"
-  sha256 "e96633cfced36e7810fc5cd0f15a83362be1f4670e0b38971d8172003dd068d3"
+  url "https://mediaarea.net/download/binary/mediainfo/22.09/MediaInfo_CLI_22.09_GNU_FromSource.tar.bz2"
+  sha256 "cbeb09e63d77e10d74ff5ee3c4955f7ff848796f506d64cbed3d6e828632d52b"
   license "BSD-2-Clause"
 
   livecheck do
@@ -12,7 +12,7 @@ class MediaInfo < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/media-info"
-    sha256 cellar: :any, mojave: "1a6a595636a1f757dba301bc1fd29f47111249d49fcfc0964480d917658b6766"
+    sha256 cellar: :any, mojave: "b4eb6c2b150d974ecd19cf7464287ac0a0260d1b898007ad3975a815d419d116"
   end
 
   depends_on "pkg-config" => :build
