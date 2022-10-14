ffab213d0c1891513dc8405812e2839d9d82a0d6
Adrian Ho <the.gromgit@gmail.com>
2022-10-14 17:57:01 +0800

    ngt: update 2.0.4 bottle.

--- Formula/ngt.rb
+++ Formula/ngt.rb
@@ -1,8 +1,8 @@
 class Ngt < Formula
   desc "Neighborhood graph and tree for indexing high-dimensional data"
   homepage "https://github.com/yahoojapan/NGT"
-  url "https://github.com/yahoojapan/NGT/archive/v1.14.6.tar.gz"
-  sha256 "5d7b092855ecea76f6dcf439beeba774a36893aaf7a61732de07068449f6f86f"
+  url "https://github.com/yahoojapan/NGT/archive/v2.0.4.tar.gz"
+  sha256 "b8e4f8d1c875210d1f6e929af8757432e84adc28a920168bc2700e72c2e6f64d"
   license "Apache-2.0"
 
   livecheck do
@@ -12,13 +12,17 @@ class Ngt < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ngt"
-    sha256 cellar: :any, mojave: "9ca92b5bbfc2bdedfc2b9950970db07914fb2830feb842d9c5f2ca1b50ccd65f"
+    sha256 cellar: :any, mojave: "53391591c7687f497c009f845073adcdf9528a26737fbba45040c329a87385ff"
   end
 
   depends_on "cmake" => :build
 
   on_macos do
     depends_on "libomp"
+  end
+
+  on_linux do
+    depends_on "openblas"
   end
 
   def install
