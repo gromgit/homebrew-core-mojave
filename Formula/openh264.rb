46c5829ec47b1ece6ce67384d130d26449c1c396
Adrian Ho <the.gromgit@gmail.com>
2022-10-14 18:24:45 +0800

    openh264: update 2.3.1 bottle.

--- Formula/openh264.rb
+++ Formula/openh264.rb
@@ -1,14 +1,14 @@
 class Openh264 < Formula
   desc "H.264 codec from Cisco"
   homepage "https://www.openh264.org/"
-  url "https://github.com/cisco/openh264/archive/v2.3.0.tar.gz"
-  sha256 "99b0695272bee73a3b3a5fcb1afef462c11a142d1dc35a2c61fef5a4b7d60bc0"
+  url "https://github.com/cisco/openh264/archive/v2.3.1.tar.gz"
+  sha256 "453afa66dacb560bc5fd0468aabee90c483741571bca820a39a1c07f0362dc32"
   license "BSD-2-Clause"
   head "https://github.com/cisco/openh264.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/openh264"
-    sha256 cellar: :any, mojave: "62a995b5472624a6d495a69c6dca897e9bc23102cd5bfb4d452799715c433f74"
+    sha256 cellar: :any, mojave: "46741288d8c76b24ae50f80a018671ede01be198d30e8b0ef07471ad809059fc"
   end
 
   depends_on "nasm" => :build
