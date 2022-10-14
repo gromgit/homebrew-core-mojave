0872517eb84bb247f279590f01324b1abb52e70e
Adrian Ho <the.gromgit@gmail.com>
2022-10-14 22:36:46 +0800

    spaceship: update 4.6.0 bottle.

--- Formula/spaceship.rb
+++ Formula/spaceship.rb
@@ -1,14 +1,14 @@
 class Spaceship < Formula
   desc "Zsh prompt for Astronauts"
   homepage "https://spaceship-prompt.sh"
-  url "https://github.com/spaceship-prompt/spaceship-prompt/archive/v4.3.2.tar.gz"
-  sha256 "0739d3f06787bdd65bfe6456f7af1a6e4ec477b4caddad43fb9b3adf49c9ccd0"
+  url "https://github.com/spaceship-prompt/spaceship-prompt/archive/v4.6.0.tar.gz"
+  sha256 "30e67f867262606db5d7f9a108b62a75a360e202074337f122ca6459368e5668"
   license "MIT"
   head "https://github.com/spaceship-prompt/spaceship-prompt.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/spaceship"
-    sha256 cellar: :any_skip_relocation, mojave: "d62a10c01da88207d66d769026a38dfb71dad401e26b33147193dbb84c23ffd3"
+    sha256 cellar: :any_skip_relocation, mojave: "88e804c48c8277db7d6fae777afb205468864fdbf5a5813d5bcefbd05c083301"
   end
 
   depends_on "zsh-async"
