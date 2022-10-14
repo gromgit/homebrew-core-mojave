eabdfe9d264a2a672aa83548d706da79943dcf64
Adrian Ho <the.gromgit@gmail.com>
2022-10-14 17:44:48 +0800

    mtools: update 4.0.41 bottle.

--- Formula/mtools.rb
+++ Formula/mtools.rb
@@ -1,20 +1,17 @@
 class Mtools < Formula
   desc "Tools for manipulating MSDOS files"
   homepage "https://www.gnu.org/software/mtools/"
-  url "https://ftp.gnu.org/gnu/mtools/mtools-4.0.40.tar.gz"
-  mirror "https://ftpmirror.gnu.org/mtools/mtools-4.0.40.tar.gz"
-  sha256 "b17ab808a7c08634d467756444d4f1cc63bb0ca66bf5807cd68f0d29b612e10f"
+  url "https://ftp.gnu.org/gnu/mtools/mtools-4.0.41.tar.gz"
+  mirror "https://ftpmirror.gnu.org/mtools/mtools-4.0.41.tar.gz"
+  sha256 "0816539dd3784fdf622de90aa5459d7c897584d69930a076b38fc4e975bd63d3"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mtools"
-    sha256 cellar: :any_skip_relocation, mojave: "7c705f51d77c01fbc63d3fcf07dec64c60e074de7578da92e22ecaba4464d51c"
+    sha256 cellar: :any_skip_relocation, mojave: "a9f93e6bd1781501a082530d2e5054698b7b30bb16bde59f547c4219016e1206"
   end
 
   conflicts_with "multimarkdown", because: "both install `mmd` binaries"
-
-  # 4.0.25 doesn't include the proper osx locale headers.
-  patch :DATA
 
   def install
     args = %W[
@@ -39,18 +36,3 @@ class Mtools < Formula
     assert_match version.to_s, shell_output("#{bin}/mtools --version")
   end
 end
-
-__END__
-diff --git a/sysincludes.h b/sysincludes.h
-index 056218e..ba3677b 100644
---- a/sysincludes.h
-+++ b/sysincludes.h
-@@ -279,6 +279,8 @@ extern int errno;
- #include <pwd.h>
- #endif
- 
-+#include <xlocale.h>
-+#include <strings.h>
- 
- #ifdef HAVE_STRING_H
- # include <string.h>
