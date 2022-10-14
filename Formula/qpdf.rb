24a43c7c78d059d7c6113a2fdf87641c9f804e7e
Adrian Ho <the.gromgit@gmail.com>
2022-10-14 21:51:59 +0800

    qpdf: update 11.1.1 bottle.

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -1,13 +1,13 @@
 class Qpdf < Formula
   desc "Tools for and transforming and inspecting PDF files"
   homepage "https://github.com/qpdf/qpdf"
-  url "https://github.com/qpdf/qpdf/releases/download/v11.1.0/qpdf-11.1.0.tar.gz"
-  sha256 "34a7cf3ac6e239510e9a20d7cbe10a4aff0f572c20e0a9bed0badb820a69e22d"
+  url "https://github.com/qpdf/qpdf/releases/download/v11.1.1/qpdf-11.1.1.tar.gz"
+  sha256 "25e8ec60e290cd134405a51920015b6d351d4e0b93b7b736d257f10725f74b5a"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/qpdf"
-    sha256 cellar: :any, mojave: "079d0abc315dc08afcfe848419256d7185c5f7ca5029ee2ffe376357f5bff4f0"
+    sha256 cellar: :any, mojave: "7ed2ec4d5494bb46361c0b7ff126cc2dfd342b1e6f168b64ce52a49e9e98c98a"
   end
 
   depends_on "cmake" => :build
