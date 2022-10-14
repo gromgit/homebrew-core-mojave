f5f15e7471fc7f95e210a91e918eafbaef806abf
Adrian Ho <the.gromgit@gmail.com>
2022-10-14 18:21:47 +0800

    opencl-headers: update 2022.09.30 bottle.

--- Formula/opencl-headers.rb
+++ Formula/opencl-headers.rb
@@ -1,14 +1,14 @@
 class OpenclHeaders < Formula
   desc "C language header files for the OpenCL API"
   homepage "https://www.khronos.org/registry/OpenCL/"
-  url "https://github.com/KhronosGroup/OpenCL-Headers/archive/refs/tags/v2022.05.18.tar.gz"
-  sha256 "88a1177853b279eaf574e2aafad26a84be1a6f615ab1b00c20d5af2ace95c42e"
+  url "https://github.com/KhronosGroup/OpenCL-Headers/archive/refs/tags/v2022.09.30.tar.gz"
+  sha256 "0ae857ecb28af95a420c800b21ed2d0f437503e104f841ab8db249df5f4fbe5c"
   license "Apache-2.0"
   head "https://github.com/KhronosGroup/OpenCL-Headers.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/opencl-headers"
-    sha256 cellar: :any_skip_relocation, mojave: "60b2ccfedf9128ca15a2e63e5526981c3a9d3d1ab339987c9f585d5bf3e12afd"
+    sha256 cellar: :any_skip_relocation, mojave: "a7c4edafd1bf9f65e7773083fa37a437ca3cc2f653e816588dfb8f52e4c94d35"
   end
 
   keg_only :shadowed_by_macos, "macOS provides OpenCL.framework"
