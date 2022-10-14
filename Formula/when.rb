fa0165ab5220dbcee54800878f54fb9caad9da3c
Adrian Ho <the.gromgit@gmail.com>
2022-10-14 23:58:15 +0800

    when: update 1.1.44 bottle.

--- Formula/when.rb
+++ Formula/when.rb
@@ -1,14 +1,14 @@
 class When < Formula
   desc "Tiny personal calendar"
   homepage "https://www.lightandmatter.com/when/when.html"
-  url "https://github.com/bcrowell/when/archive/1.1.43.tar.gz"
-  sha256 "ddc3332aeadb4b786182128d3b04db38176b59969ae6980a7187d40b14528576"
+  url "https://github.com/bcrowell/when/archive/1.1.44.tar.gz"
+  sha256 "de8334d97a106b9e3aad42d0a169e46e276db0935b3e4239403730eadcb41cbb"
   license "GPL-2.0-only"
   head "https://github.com/bcrowell/when.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/when"
-    sha256 cellar: :any_skip_relocation, mojave: "87866e4e15dcd604cb631fe6a89b6517d9b92bba2071b3caa396a8c79a709f54"
+    sha256 cellar: :any_skip_relocation, mojave: "63155f7606d0225a1c48c8d801dc8f48201da7cd7fe1c723c7fd9e108acae0bf"
   end
 
   def install
