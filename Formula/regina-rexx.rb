7f2b819c4d454f901c6d754660e7fe877c51ca42
Adrian Ho <the.gromgit@gmail.com>
2022-10-14 22:12:14 +0800

    regina-rexx: update 3.9.5_1 bottle.

--- Formula/regina-rexx.rb
+++ Formula/regina-rexx.rb
@@ -4,18 +4,24 @@ class ReginaRexx < Formula
   url "https://downloads.sourceforge.net/project/regina-rexx/regina-rexx/3.9.5/regina-rexx-3.9.5.tar.gz"
   sha256 "08e9a9061bee0038cfb45446de20766ffdae50eea37f6642446ec4e73a2abc51"
   license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/regina-rexx"
-    sha256 mojave: "f366422554f29a15eb5af72f81bc42467373d32290851dd2aa3dbbcc7fac42f4"
+    sha256 mojave: "2f2720be070541292abeb3c23f9ae2515596ed3d3d048c619ac3c4d6ec7e00c6"
   end
 
   uses_from_macos "libxcrypt"
 
   def install
     ENV.deparallelize # No core usage for you, otherwise race condition = missing files.
-    system "./configure", *std_configure_args
-    system "make", "install"
+    system "./configure", *std_configure_args,
+                          "--with-addon-dir=#{HOMEBREW_PREFIX}/lib/regina-rexx/addons",
+                          "--with-brew-addon-dir=#{lib}/regina-rexx/addons"
+    system "make"
+
+    install_targets = OS.mac? ? ["installbase", "installbrewlib"] : ["install"]
+    system "make", *install_targets
   end
 
   test do
