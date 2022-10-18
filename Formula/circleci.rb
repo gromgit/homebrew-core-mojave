fc7435f701d504c95c5626eb19b8de448d85beda
Adrian Ho <the.gromgit@gmail.com>
2022-10-19 02:09:08 +0800

    circleci: update 0.1.22150 bottle.

--- Formula/circleci.rb
+++ Formula/circleci.rb
@@ -3,18 +3,17 @@ class Circleci < Formula
   homepage "https://circleci.com/docs/2.0/local-cli/"
   # Updates should be pushed no more frequently than once per week.
   url "https://github.com/CircleCI-Public/circleci-cli.git",
-      tag:      "v0.1.21194",
-      revision: "0f1ff57542b90c399b7e5675c16f1bd1a09d970c"
+      tag:      "v0.1.22150",
+      revision: "37e2fc36d336b0f20683efc21205d9a7ba299516"
   license "MIT"
   head "https://github.com/CircleCI-Public/circleci-cli.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/circleci"
-    sha256 cellar: :any_skip_relocation, mojave: "9ace899455dd1401f701c8e268dd9414b069341923ec71bc19a4f0f81fa601b3"
+    sha256 cellar: :any_skip_relocation, mojave: "1986041780eb2791f632f36c6d0df46ba8550a3d5a692cf72220bd99e7d7a185"
   end
 
-  # Bump to 1.18 when the x/sys dependency is updated upstream.
-  depends_on "go@1.17" => :build
+  depends_on "go" => :build
   depends_on "packr" => :build
 
   def install
@@ -39,7 +38,7 @@ class Circleci < Formula
     output = shell_output("#{bin}/circleci config pack #{testpath}/.circleci.yml")
     assert_match "version: 2.1", output
     # assert update is not included in output of help meaning it was not included in the build
-    assert_match "update      This command is unavailable on your platform", shell_output("#{bin}/circleci help")
+    assert_match(/update.+This command is unavailable on your platform/, shell_output("#{bin}/circleci help 2>&1"))
     assert_match "`update` is not available because this tool was installed using `homebrew`.",
       shell_output("#{bin}/circleci update")
   end
