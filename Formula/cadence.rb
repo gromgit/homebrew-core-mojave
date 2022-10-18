6ff84dd8e2765b1be25cf263e5765a4140ab9bd5
Adrian Ho <the.gromgit@gmail.com>
2022-10-19 00:56:38 +0800

    cadence: update 0.28.0 bottle.

--- Formula/cadence.rb
+++ Formula/cadence.rb
@@ -1,8 +1,8 @@
 class Cadence < Formula
   desc "Resource-oriented smart contract programming language"
   homepage "https://github.com/onflow/cadence"
-  url "https://github.com/onflow/cadence/archive/v0.26.1.tar.gz"
-  sha256 "1ebc6bc59da9d5873d3b7fdc7ce53c184f87b449db939f38924865e4b05a8c52"
+  url "https://github.com/onflow/cadence/archive/v0.28.0.tar.gz"
+  sha256 "c18f74d4a17b13262f8e7ab372d3a4833b2d2ca2747a12db12321c94c769fb7b"
   license "Apache-2.0"
   head "https://github.com/onflow/cadence.git", branch: "master"
 
@@ -13,7 +13,7 @@ class Cadence < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cadence"
-    sha256 cellar: :any_skip_relocation, mojave: "4882ffa5e7b4fa028f9c6ad3062550f0ba07c7bf1b628dfd0cc70ff024d7f46d"
+    sha256 cellar: :any_skip_relocation, mojave: "132b0a9a12b2cbeafb690b443794a8be5e9f2b42cc425276a53b987ee1b99a8a"
   end
 
   depends_on "go" => :build
