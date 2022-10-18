4deaf6b0f0e5f149e6e788587d2d2bd9ad3094f3
Adrian Ho <the.gromgit@gmail.com>
2022-10-19 01:57:45 +0800

    cilium-cli: update 0.12.4 bottle.

--- Formula/cilium-cli.rb
+++ Formula/cilium-cli.rb
@@ -1,13 +1,13 @@
 class CiliumCli < Formula
   desc "CLI to install, manage & troubleshoot Kubernetes clusters running Cilium"
   homepage "https://cilium.io"
-  url "https://github.com/cilium/cilium-cli/archive/refs/tags/v0.12.3.tar.gz"
-  sha256 "0b62ada53c987db4bfcbdc3eecee1ce05c3cf6d19a11e60be215b1b64f4d574d"
+  url "https://github.com/cilium/cilium-cli/archive/refs/tags/v0.12.4.tar.gz"
+  sha256 "271dd1285b19b7e8d6e2eecae0059b7a22fe87c2a8000c599997397d2494d0d3"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cilium-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "88fb2365c5af08315c6b7233b6cc70e6758c7314cf7783f171c7e4be213c53eb"
+    sha256 cellar: :any_skip_relocation, mojave: "32db8e6fe911d6389cb43a38e2291cefbaa461e4d9a97a289d5c31c2df9ccb6f"
   end
 
   depends_on "go" => :build
