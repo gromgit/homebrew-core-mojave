718eb3da7f6ee7d1f998bcd960e0306ff47fb3e8
Adrian Ho <the.gromgit@gmail.com>
2022-10-18 23:17:33 +0800

    augeas: update 1.13.0 bottle.

--- Formula/augeas.rb
+++ Formula/augeas.rb
@@ -1,10 +1,19 @@
 class Augeas < Formula
   desc "Configuration editing tool and API"
   homepage "https://augeas.net/"
-  url "http://download.augeas.net/augeas-1.12.0.tar.gz"
-  sha256 "321942c9cc32185e2e9cb72d0a70eea106635b50269075aca6714e3ec282cb87"
   license "LGPL-2.1"
-  revision 1
+  head "https://github.com/hercules-team/augeas.git", branch: "master"
+
+  stable do
+    url "https://github.com/hercules-team/augeas/releases/download/release-1.13.0/augeas-1.13.0.tar.gz"
+    sha256 "5002f33f42365ab78be974609a0f3b76a4c277fc404ec79f516305cab5ce5de1"
+
+    # Replace deprecated 'security_context_t' with 'char *'. Remove in the next release.
+    patch do
+      url "https://github.com/hercules-team/augeas/commit/f38398a2d07028b892eac59449a35e1a3d645fac.patch?full_index=1"
+      sha256 "1697379e0676edf94346a3377a75c871d1d0d033e3a37a29d69ae66f6e57553a"
+    end
+  end
 
   livecheck do
     url "http://download.augeas.net/"
@@ -12,17 +21,8 @@ class Augeas < Formula
   end
 
   bottle do
-    sha256 arm64_monterey: "136e012d7695d0ddf73a689d84b521aebc74944dc5b763b0f40abb2b49dff768"
-    sha256 arm64_big_sur:  "9625e271fbbf7bb914bb7475eae95f97f99f5cbf9e71874f599ee620dd907432"
-    sha256 monterey:       "62beef17116c12a01d5cf68595636570e658f3c9714b0601dc8884de458af419"
-    sha256 big_sur:        "83b60962039b8d9c8a2ff343c8eaf21454976394f60ee5f340901d8de6161bd8"
-    sha256 catalina:       "dc69497d1ac32d9f8203fb346965ad8e88f4498c120b7915a3f4dd0d7509d9e2"
-    sha256 mojave:         "446030ec8931aa24eabee1e051daf7365dbd6fb9e43b4f71998dc6d7d0f9fdda"
-    sha256 x86_64_linux:   "1d769bb0e887b5a321e5c89d15959e71fc7d616fb79c3e2e926ff2d1cefc106f"
-  end
-
-  head do
-    url "https://github.com/hercules-team/augeas.git"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/augeas"
+    sha256 mojave: "88ec30b7300b2e079d830c6048973793503d0817286bf9cc47c0b85e76f8fb24"
   end
 
   depends_on "autoconf" => :build
@@ -35,15 +35,13 @@ class Augeas < Formula
   uses_from_macos "libxml2"
 
   def install
-    args = %W[--disable-debug --disable-dependency-tracking --prefix=#{prefix}]
-
     if build.head?
-      system "./autogen.sh", *args
+      system "./autogen.sh", *std_configure_args
     else
       # autoreconf is needed to work around
       # https://debbugs.gnu.org/cgi/bugreport.cgi?bug=44605.
       system "autoreconf", "--force", "--install"
-      system "./configure", *args
+      system "./configure", *std_configure_args
     end
 
     system "make", "install"
