a2d4a92eeb82885e111e515f924b5372a1a6a5ce
Adrian Ho <the.gromgit@gmail.com>
2022-10-20 01:00:42 +0800

    dory: update 1.2.0 bottle.

--- Formula/dory.rb
+++ Formula/dory.rb
@@ -1,18 +1,13 @@
 class Dory < Formula
   desc "Development proxy for docker"
   homepage "https://github.com/freedomben/dory"
-  url "https://github.com/FreedomBen/dory/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "1571e54dab39bc7884523aabeedde71921e64a11ef25b9c59a7b63282f97a237"
+  url "https://github.com/FreedomBen/dory/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "8c385d898aed2de82f7d0ab5c776561ffe801dd4b222a07e25e5837953355b81"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a98dd50fdc6cabcd7f200958b32c6c8c448174b861efd576170d7ac696e57e8d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cbe36a5c87731cae07c1de8b9f7e2cfc04e17bba6e1a6918678ef49f62256bad"
-    sha256 cellar: :any_skip_relocation, monterey:       "0d97718bff64b71e82a4c538cf681a9521c7e5cbf1b79e640355c541d9082909"
-    sha256 cellar: :any_skip_relocation, big_sur:        "0b772ed42dcb84a49c2a223a2fd8a899b942deb457291bef1c281a825fa7069b"
-    sha256 cellar: :any_skip_relocation, catalina:       "0b772ed42dcb84a49c2a223a2fd8a899b942deb457291bef1c281a825fa7069b"
-    sha256 cellar: :any_skip_relocation, mojave:         "52197dfd1668a9c0c3c2daa1cd7e808d89bbb4ba3f3500746e60b56cbc05058b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "858fd6ba202acedbc27ffea81e407ed0d25e6d77386e2809b412fb776a07e5d9"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dory"
+    sha256 cellar: :any_skip_relocation, mojave: "2b997c97ef0977274fc7cf99d73c7685d641f7f512c48fc96067dcff3fe6e138"
   end
 
   depends_on "ruby@2.7"
