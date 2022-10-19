4bba11d3abea7c83758d65fd6d317dc3b289f31f
Adrian Ho <the.gromgit@gmail.com>
2022-10-20 00:33:42 +0800

    dockviz: update 0.6.4 bottle.

--- Formula/dockviz.rb
+++ Formula/dockviz.rb
@@ -2,30 +2,20 @@ class Dockviz < Formula
   desc "Visualizing docker data"
   homepage "https://github.com/justone/dockviz"
   url "https://github.com/justone/dockviz.git",
-      tag:      "v0.6.3",
-      revision: "15f77275c4f7e459eb7d9f824b5908c165cd0ba4"
+      tag:      "v0.6.4",
+      revision: "3ebdb75ed393d6f2eb0b38d83ee22d75c68f6524"
   license "Apache-2.0"
   head "https://github.com/justone/dockviz.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dockviz"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "dd8db4c9e5771d3b741b5937411c0a5057739900d323a5c2ef35be9364dbad20"
+    sha256 cellar: :any_skip_relocation, mojave: "a87d2eabcce2ceaf4ada13fa41c9986ad0114f20f32d7900a08d8c8d1a35e6fd"
   end
 
   depends_on "go" => :build
-  depends_on "govendor" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    ENV["GO111MODULE"] = "auto"
-
-    (buildpath/"src/github.com/justone/dockviz").install buildpath.children
-    cd "src/github.com/justone/dockviz" do
-      system "govendor", "sync"
-      system "go", "build", "-o", bin/"dockviz"
-      prefix.install_metafiles
-    end
+    system "go", "build", *std_go_args
   end
 
   test do
