7fc6e5f2e11f3e6fe7df30058a24b60f95039419
Adrian Ho <the.gromgit@gmail.com>
2022-10-20 03:13:21 +0800

    epinio: update 1.3.0 bottle.

--- Formula/epinio.rb
+++ Formula/epinio.rb
@@ -1,19 +1,21 @@
 class Epinio < Formula
   desc "CLI for Epinio, the Application Development Engine for Kubernetes"
   homepage "https://epinio.io/"
-  url "https://github.com/epinio/epinio/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "54c194a3877074b3e9790d086abaa3d7004ace5970f856947098dcc65948fdb3"
+  url "https://github.com/epinio/epinio/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "4ac5924deb7b10119bcd51edd37bd32e20d3885cc760f23dbfe1a928564c0e32"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/epinio"
-    sha256 cellar: :any_skip_relocation, mojave: "fa269ed8676fc36960448c6fe11c60e78f3dd46d9963338a03f2977a2368da59"
+    sha256 cellar: :any_skip_relocation, mojave: "7efadcb7da40ecd0274d0c8be08a62d707655efb9be9209402b5ae9b8387dde0"
   end
 
   depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/epinio/epinio/internal/version.Version=#{version}")
+
+    generate_completions_from_executable(bin/"epinio", "completion")
   end
 
   test do
