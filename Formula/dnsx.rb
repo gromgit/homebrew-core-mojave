c0e32c091acb18f09e314e4ba6714713c62a5457
Adrian Ho <the.gromgit@gmail.com>
2022-10-20 00:07:34 +0800

    dnsx: update 1.1.1 bottle.

--- Formula/dnsx.rb
+++ Formula/dnsx.rb
@@ -1,14 +1,14 @@
 class Dnsx < Formula
   desc "DNS query and resolution tool"
   homepage "https://github.com/projectdiscovery/dnsx"
-  url "https://github.com/projectdiscovery/dnsx/archive/v1.1.0.tar.gz"
-  sha256 "5cb53066b689982be0d08322c40a82320888184cf5ee2a7fce118d566261de20"
+  url "https://github.com/projectdiscovery/dnsx/archive/v1.1.1.tar.gz"
+  sha256 "b136298b2139bf5a2c94d4b2b41419ef63681c900fc9d6cec586eec7e1ed479c"
   license "MIT"
   head "https://github.com/projectdiscovery/dnsx.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dnsx"
-    sha256 cellar: :any_skip_relocation, mojave: "b29653836620fe7f78e5cd77baadbac2d261b7304a88c32e461e4986c028ba80"
+    sha256 cellar: :any_skip_relocation, mojave: "0d5568428a22a730e66b2d18ed0162cebf1cfe385ec13c371820911ca21323d1"
   end
 
   depends_on "go" => :build
