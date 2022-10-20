8d515313dd122ecfe5cde426ff21fd3f1b08ba9e
Adrian Ho <the.gromgit@gmail.com>
2022-10-20 19:57:39 +0800

    gobuster: update 3.2.0 bottle.

--- Formula/gobuster.rb
+++ Formula/gobuster.rb
@@ -1,28 +1,21 @@
 class Gobuster < Formula
   desc "Directory/file & DNS busting tool written in Go"
   homepage "https://github.com/OJ/gobuster"
-  url "https://github.com/OJ/gobuster.git",
-      tag:      "v3.1.0",
-      revision: "f5051ed456dc158649bb8bf407889ab0978bf1ba"
+  url "https://github.com/OJ/gobuster/archive/refs/tags/v3.2.0.tar.gz"
+  sha256 "63094d24b79622d798f1aed2e497c8a6dd2bbeaa4fda7162ec71bc7070bf1a61"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "44353288958adfaede89054b42dbac2f20f4cf1968d2c1b7101d3ab140d4eef8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e6e7f65fbed3896cb3b63eefddd16de3b621a8d72b205d24b8f7abd21379f87e"
-    sha256 cellar: :any_skip_relocation, monterey:       "ecbea96dbc4a7889cb9c2cd295d84858b5f332b7d44c12f482450e6e1e10266f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "8342b115722243f5c108de8ecdb5aefd20ae5deb884e48732c80595c24897f0d"
-    sha256 cellar: :any_skip_relocation, catalina:       "f8f36299b36b59006637dcc7d062614eb209ba82a31f5a67fce789c4d6ef9562"
-    sha256 cellar: :any_skip_relocation, mojave:         "16912d38db06501d02cdab6066d1da01129779d958ce142c40018cce30328fc4"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "341ce02f5e99ba1bf9cee8d6cbdd150a6e36d8b0fd811ded7a2da8933d877f9a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "493ef7685bc5627b6b382479f3645eda52a36010581e3c9400145775d7da0ca2"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gobuster"
+    sha256 cellar: :any_skip_relocation, mojave: "7342ea5f171d3c7ec7f756b7d18defb7733ddd34ac7b4b4d3e107fa02afadd6d"
   end
 
-  # Bump to 1.18 on the next release.
-  depends_on "go@1.17" => :build
+  depends_on "go" => :build
 
   def install
-    system "go", "build", "-ldflags", "-s -w", "-trimpath", "-o", bin/"gobuster"
-    prefix.install_metafiles
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+
+    generate_completions_from_executable(bin/"gobuster", "completion")
   end
 
   test do
@@ -36,5 +29,7 @@ class Gobuster < Formula
 
     output = shell_output("#{bin}/gobuster dir -u https://buffered.io -w words.txt 2>&1")
     assert_match "Finished", output
+
+    assert_match version.to_s, shell_output(bin/"gobuster version")
   end
 end
