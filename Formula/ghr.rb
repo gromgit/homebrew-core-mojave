983dffb66272220af8052d0a4abbe70dd191dabd
Adrian Ho <the.gromgit@gmail.com>
2022-10-20 18:56:08 +0800

    ghr: update 0.16.0 bottle.

--- Formula/ghr.rb
+++ Formula/ghr.rb
@@ -1,13 +1,13 @@
 class Ghr < Formula
   desc "Upload multiple artifacts to GitHub Release in parallel"
   homepage "https://tcnksm.github.io/ghr"
-  url "https://github.com/tcnksm/ghr/archive/v0.15.0.tar.gz"
-  sha256 "89180208e62bc56e1bc401ca5171291c75c2589d47732c34d8647b3e5e0522e5"
+  url "https://github.com/tcnksm/ghr/archive/v0.16.0.tar.gz"
+  sha256 "c2b1f0a25b3e0b9016418c125441f16615387e32bce5c56049064deffbe1b1c9"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ghr"
-    sha256 cellar: :any_skip_relocation, mojave: "7aa083576132872f22921c8b53db662eed5984cadd9159802a00c98240e6f120"
+    sha256 cellar: :any_skip_relocation, mojave: "91cdc655de45e58089e28f610da16fe1eeea3cb928184aefda9a317cc9911fba"
   end
 
   depends_on "go" => :build
