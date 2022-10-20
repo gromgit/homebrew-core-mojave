871b05fa46d82a61db6b3c48616f34b76712f0e4
Adrian Ho <the.gromgit@gmail.com>
2022-10-20 22:48:49 +0800

    ipinfo-cli: update 2.10.0 bottle.

--- Formula/ipinfo-cli.rb
+++ Formula/ipinfo-cli.rb
@@ -1,8 +1,8 @@
 class IpinfoCli < Formula
   desc "Official CLI for the IPinfo IP Address API"
   homepage "https://ipinfo.io/"
-  url "https://github.com/ipinfo/cli/archive/ipinfo-2.9.0.tar.gz"
-  sha256 "299b853300a81a6dd2c3c22bf335917368b799c2108a354cd8be4ad269753adc"
+  url "https://github.com/ipinfo/cli/archive/ipinfo-2.10.0.tar.gz"
+  sha256 "b48e69f309647d1845c36c0176cf6c07e13b59ca443269df7e4d394838b72211"
   license "Apache-2.0"
 
   livecheck do
@@ -12,7 +12,7 @@ class IpinfoCli < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ipinfo-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "83a2ca43316b704e8e2716e3af117aace495103aac983f3fb0a7381f7ee4be05"
+    sha256 cellar: :any_skip_relocation, mojave: "9d30460d985f43bea6e2dc65d971a1c7e45e512a4fff83c4abfaee8e7e814679"
   end
 
   depends_on "go" => :build
