9015ff599ebab0b013af30a7d6068863f44f75da
Adrian Ho <the.gromgit@gmail.com>
2022-10-20 23:59:58 +0800

    k3sup: update 0.12.8 bottle.

--- Formula/k3sup.rb
+++ Formula/k3sup.rb
@@ -2,8 +2,8 @@ class K3sup < Formula
   desc "Utility to create k3s clusters on any local or remote VM"
   homepage "https://k3sup.dev"
   url "https://github.com/alexellis/k3sup.git",
-      tag:      "0.12.3",
-      revision: "c0a48331967e1c1e6b10dbd997b65c1496fbd32d"
+      tag:      "0.12.8",
+      revision: "018b1a0b08c2c08f0243b9bb7b0139868d1059eb"
   license "MIT"
   head "https://github.com/alexellis/k3sup.git", branch: "master"
 
@@ -14,8 +14,7 @@ class K3sup < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/k3sup"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "cd9baf3b0efa24f6d7cb2d3b56043cd415a1d4dda1f6526bb7a5b97328a0de20"
+    sha256 cellar: :any_skip_relocation, mojave: "763240185552ebd8ac464cf834d79f79b417c9565217257e705784621383f008"
   end
 
   depends_on "go" => :build
