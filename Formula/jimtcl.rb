dab9e4fb306f9e4902b54c74e240e97f1de3d98f
Adrian Ho <the.gromgit@gmail.com>
2022-10-20 23:37:43 +0800

    jimtcl: update 0.81_1 bottle.

--- Formula/jimtcl.rb
+++ Formula/jimtcl.rb
@@ -4,17 +4,24 @@ class Jimtcl < Formula
   url "https://github.com/msteveb/jimtcl/archive/0.81.tar.gz"
   sha256 "ab7eb3680ba0d16f4a9eb1e05b7fcbb7d23438e25185462c55cd032a1954a985"
   license "BSD-2-Clause"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jimtcl"
-    sha256 mojave: "a983b71b8dbe1c0a9e8984a63b10e7547e694705b75821118c82155eaba0378e"
+    sha256 mojave: "66639cbf080a3a24af7ffd1e3bf13befe9b704c83b7a9978c7e41280e3f60f2a"
   end
 
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   depends_on "readline"
 
   uses_from_macos "sqlite"
   uses_from_macos "zlib"
+
+  # Fix EOF detection with openssl@3. Remove in the next release.
+  patch do
+    url "https://github.com/msteveb/jimtcl/commit/b0271cca8e335a1ebe4e3d6a8889bd4d7d5e30e6.patch?full_index=1"
+    sha256 "dbeeb8bb9a1174c4c0d44d8dafc1958994417014176c12d959daa8b31aa4b5b0"
+  end
 
   def install
     system "./configure", "--disable-debug",
