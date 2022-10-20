d6283468e9f51e7884f060efc7898cee48edd9e6
Adrian Ho <the.gromgit@gmail.com>
2022-10-21 00:15:05 +0800

    k9s: update 0.26.6 bottle.

--- Formula/k9s.rb
+++ Formula/k9s.rb
@@ -2,15 +2,14 @@ class K9s < Formula
   desc "Kubernetes CLI To Manage Your Clusters In Style!"
   homepage "https://k9scli.io/"
   url "https://github.com/derailed/k9s.git",
-      tag:      "v0.26.3",
-      revision: "0893f13b3ca6b563dd0c38fdebaefdb8be594825"
+      tag:      "v0.26.6",
+      revision: "c66002d9868fc172e0bb8091ad87d61be027aae2"
   license "Apache-2.0"
   head "https://github.com/derailed/k9s.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/k9s"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "7085e38cf3475bd5408a034ee75495ef0beaec7ab8d490597f3c375fd4685271"
+    sha256 cellar: :any_skip_relocation, mojave: "6d7068f469702400236189b61ab758c2487a1576661204825c994887d2258e77"
   end
 
   depends_on "go" => :build
