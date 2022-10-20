c6039d19369266281238f8fd9a00b6c8a76b29f5
Adrian Ho <the.gromgit@gmail.com>
2022-10-20 21:31:57 +0800

    hugo: update 0.104.3 bottle.

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -1,14 +1,14 @@
 class Hugo < Formula
   desc "Configurable static site generator"
   homepage "https://gohugo.io/"
-  url "https://github.com/gohugoio/hugo/archive/v0.103.1.tar.gz"
-  sha256 "9065beec7b46be56052c44bc4a2f8a561ae842af643f59915a98842531f62efb"
+  url "https://github.com/gohugoio/hugo/archive/v0.104.3.tar.gz"
+  sha256 "f63b3d355326fd71470cee503316105cd2e209e9712b94162bafcac7486e30d8"
   license "Apache-2.0"
   head "https://github.com/gohugoio/hugo.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/hugo"
-    sha256 cellar: :any_skip_relocation, mojave: "af3e3e931db7d55ac51d6206880b65db036c2a8f91fd5ca22aba0fdac8d4e5ff"
+    sha256 cellar: :any_skip_relocation, mojave: "6732db129955185af935c3fd3096848096709da5570ccedea83caf47ef78a334"
   end
 
   depends_on "go" => :build
