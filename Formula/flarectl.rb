60f250fa76c587443ff28b2b00741909931db9da
Adrian Ho <the.gromgit@gmail.com>
2022-10-20 13:41:32 +0800

    flarectl: update 0.51.0 bottle.

--- Formula/flarectl.rb
+++ Formula/flarectl.rb
@@ -1,14 +1,14 @@
 class Flarectl < Formula
   desc "CLI application for interacting with a Cloudflare account"
   homepage "https://github.com/cloudflare/cloudflare-go/tree/master/cmd/flarectl"
-  url "https://github.com/cloudflare/cloudflare-go/archive/v0.50.0.tar.gz"
-  sha256 "392e04697bafffdf6efad8b8d47578e382d4d2252b2306808306c7d61e90f12e"
+  url "https://github.com/cloudflare/cloudflare-go/archive/v0.51.0.tar.gz"
+  sha256 "41d2d534c8f8a4d4ea95f6715ad08be7bb92eff97210395793f8b0dc33dd41ec"
   license "BSD-3-Clause"
   head "https://github.com/cloudflare/cloudflare-go.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/flarectl"
-    sha256 cellar: :any_skip_relocation, mojave: "8937f7bd27e69b3acf82dfa7edf4da832da0d4c05b5f72abf95f445ca35812fb"
+    sha256 cellar: :any_skip_relocation, mojave: "426cf8a998117bc787a82ef725b511c71a2acd36b41f596fb7cb4101a68dae47"
   end
 
   depends_on "go" => :build
