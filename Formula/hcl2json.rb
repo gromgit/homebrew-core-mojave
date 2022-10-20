dccb97690c8432a2e67515b01b74a9cf1d318346
Adrian Ho <the.gromgit@gmail.com>
2022-10-20 21:18:43 +0800

    hcl2json: update 0.3.5 bottle.

--- Formula/hcl2json.rb
+++ Formula/hcl2json.rb
@@ -1,14 +1,14 @@
 class Hcl2json < Formula
   desc "Convert HCL2 to JSON"
   homepage "https://github.com/tmccombs/hcl2json"
-  url "https://github.com/tmccombs/hcl2json/archive/v0.3.4.tar.gz"
-  sha256 "41c63b892e9a1488c5380faee83d341482352199175588fb46fa838f3b75e6a3"
+  url "https://github.com/tmccombs/hcl2json/archive/0.3.5.tar.gz"
+  sha256 "9d91d8f8b9bf204f3827795592a7da89e0dea368a17efe0163f5ef14bcf3bf9c"
   license "Apache-2.0"
   head "https://github.com/tmccombs/hcl2json.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/hcl2json"
-    sha256 cellar: :any_skip_relocation, mojave: "7c01deaa618f21391822daaa4a5a9a9347d2939c03a7d89d613a1376c1f09064"
+    sha256 cellar: :any_skip_relocation, mojave: "0e7ae11f9d3a4af09e555d6a1b686a20171dc58440fee969a7d36987a7cc4ed9"
   end
 
   depends_on "go" => :build
