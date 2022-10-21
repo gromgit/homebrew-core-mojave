233670c8c817772a468280dd5aa1b1cc32777442
Adrian Ho <the.gromgit@gmail.com>
2022-10-22 00:08:43 +0800

    rke: update 1.3.15 bottle.

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -1,8 +1,8 @@
 class Rke < Formula
   desc "Rancher Kubernetes Engine, a Kubernetes installer that works everywhere"
   homepage "https://rancher.com/docs/rke/latest/en/"
-  url "https://github.com/rancher/rke/archive/v1.3.14.tar.gz"
-  sha256 "df7ea657613a80aa5e97904a2d3b5c5660c2dfa60658ce37889cf9d71efbdbe6"
+  url "https://github.com/rancher/rke/archive/v1.3.15.tar.gz"
+  sha256 "1a508964661142046d93fa7a8b49031fc5a0c541d29a726ca87ab13d4e043c96"
   license "Apache-2.0"
 
   # It's necessary to check releases instead of tags here (to avoid upstream
@@ -18,7 +18,7 @@ class Rke < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rke"
-    sha256 cellar: :any_skip_relocation, mojave: "38b48e87951fe35472def58c0c37cdb5d755fc7924c28952ddd08a5c53076f72"
+    sha256 cellar: :any_skip_relocation, mojave: "6af7f563e756c152bac1033beb84bc5c7f6905966e2e37cc81b29a43207d1f2d"
   end
 
   depends_on "go" => :build
