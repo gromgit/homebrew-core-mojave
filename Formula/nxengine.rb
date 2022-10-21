00474a7b997d8f75cd9bbc453e7d544cd6b10291
Adrian Ho <the.gromgit@gmail.com>
2022-10-21 16:03:34 +0800

    nxengine: update 1.0.0.6_1 bottle.

--- Formula/nxengine.rb
+++ Formula/nxengine.rb
@@ -5,19 +5,14 @@ class Nxengine < Formula
   version "1.0.0.6"
   sha256 "cf9cbf15dfdfdc9936720a714876bb1524afbd2931e3eaa4c89984a40b21ad68"
   license "GPL-3.0"
+  revision 1
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any, arm64_monterey: "a367d08abb4b20abadcfefd2cf446ecc2825ee32f3adae0a8f8e3ee78c86c115"
-    sha256 cellar: :any, arm64_big_sur:  "b96f120f2d8959f859b74cb26e0c3a1d9bc028ba0434b44a0a942712b77bb92b"
-    sha256 cellar: :any, monterey:       "d57fb1782ded59a66782ba1bed4e53b41fc9897b7993c7633c6ec036eb258a96"
-    sha256 cellar: :any, big_sur:        "00d86c8f78ae03404f10738b4e7a278dad0d9749efa3ca10be4cd6fb675cb8f8"
-    sha256 cellar: :any, catalina:       "9184041001a3035713542230c9e4a536491919ff0459cc79f294366ab20719a5"
-    sha256 cellar: :any, mojave:         "69ef501ebc7a488fc46b4546b91288c7b8dc1cfdadb2bc1ee73611dd062f38e5"
-    sha256 cellar: :any, high_sierra:    "79eece70c7ab5ddb92c8c711a609b996456ff5b7c6c8a008166a351e6889797f"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nxengine"
+    sha256 cellar: :any, mojave: "66e16d690524bb03c7f21e8a53a157dc466d47e7e1aa1ebf1397f79aede31e55"
   end
 
-  depends_on "sdl"
+  depends_on "sdl12-compat"
   depends_on "sdl_ttf"
 
   # Freeware Cave Story 1.0.0.6 pre-patched with Aeon Genesis English translation
