a832aedf29a7249c38f97f3fd79cefe0f18f920e
Adrian Ho <the.gromgit@gmail.com>
2022-10-21 16:47:14 +0800

    openjazz: update 20190106_1 bottle.

--- Formula/openjazz.rb
+++ Formula/openjazz.rb
@@ -4,24 +4,19 @@ class Openjazz < Formula
   url "https://github.com/AlisterT/openjazz/archive/20190106.tar.gz"
   sha256 "27da3ab32cb6b806502a213c435e1b3b6ecebb9f099592f71caf6574135b1662"
   license "GPL-2.0"
+  revision 1
   head "https://github.com/AlisterT/openjazz.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "e9997f65f26d10adbf4ed7028f9a0c624b6000e586b529cc2969ef2ba2c0b859"
-    sha256 cellar: :any,                 arm64_big_sur:  "0c93957029786455fa5dcaa7441408e0f1f5e4e5503019b056959638f54b082a"
-    sha256 cellar: :any,                 monterey:       "2140149d9e672b0934b291de6aff920e6100f7e4dc99fa9def06c39423981c3e"
-    sha256 cellar: :any,                 big_sur:        "f38330100102887cadfe0929d42e4f61b743e3a9e8417a78d3da7c9bf620217b"
-    sha256 cellar: :any,                 catalina:       "9f6f4144256364824f4c16c430aaa738e6675f031f8bd7eaa76fa33d4d367430"
-    sha256 cellar: :any,                 mojave:         "06066b8e0bf792d894ceb24ed1ec5409ad896982db87ecab8c07278eabdc3f98"
-    sha256 cellar: :any,                 high_sierra:    "b5684fc3faa686f06f9600e8c4bb9c787c7cbf3eb100fc8a64a52502e84ce2ca"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5695fc1b02ff050deb2b42bff183e7fbbdf51ad4a679bf65d64552cf49f1ca63"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/openjazz"
+    sha256 cellar: :any, mojave: "c243a671954834d5b31a1e4219fa1e9d889683a94c5b0e0b33b8f64fb3884ebf"
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
   depends_on "libmodplug"
-  depends_on "sdl"
+  depends_on "sdl12-compat"
 
   uses_from_macos "zlib"
 
