32671264643f4098394493cc082d98e39e0deb2f
Adrian Ho <the.gromgit@gmail.com>
2022-10-22 00:52:38 +0800

    s2n: update 1.3.24 bottle.

--- Formula/s2n.rb
+++ Formula/s2n.rb
@@ -1,8 +1,8 @@
 class S2n < Formula
   desc "Implementation of the TLS/SSL protocols"
   homepage "https://github.com/aws/s2n-tls"
-  url "https://github.com/aws/s2n-tls/archive/v1.3.21.tar.gz"
-  sha256 "415120a7baefaf56e122af93d4c0f51ee96afe00b79b7f4990cb0dbc4d8a6b8b"
+  url "https://github.com/aws/s2n-tls/archive/v1.3.24.tar.gz"
+  sha256 "df6d2642e7b491f56110527bd73686d94ed3b186ff78d24e525cc0c3dd0d6b4b"
   license "Apache-2.0"
   head "https://github.com/aws/s2n-tls.git", branch: "main"
 
@@ -13,16 +13,20 @@ class S2n < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/s2n"
-    sha256 cellar: :any, mojave: "720f6c306db4ff683cc01d763f7c15fa6cd117a0cd3523d3bede1351e1bfe6dc"
+    sha256 cellar: :any, mojave: "ac174b0ad1e8efb0273533763718318c066096f96d6ab0a2122fe8ba2872f5c7"
   end
 
   depends_on "cmake" => :build
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DBUILD_SHARED_LIBS=ON"
-    system "cmake", "--build", "build"
-    system "cmake", "--install", "build"
+    system "cmake", "-S", ".", "-B", "build/static", *std_cmake_args, "-DBUILD_SHARED_LIBS=OFF"
+    system "cmake", "--build", "build/static"
+    system "cmake", "--install", "build/static"
+
+    system "cmake", "-S", ".", "-B", "build/shared", *std_cmake_args, "-DBUILD_SHARED_LIBS=ON"
+    system "cmake", "--build", "build/shared"
+    system "cmake", "--install", "build/shared"
   end
 
   test do
