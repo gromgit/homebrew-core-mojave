c41372f66e260a35d8f83d64a2be88bd876ccb09
Adrian Ho <the.gromgit@gmail.com>
2022-10-21 17:05:16 +0800

    openshift-cli: update 4.12.0-202208031327 bottle.

--- Formula/openshift-cli.rb
+++ Formula/openshift-cli.rb
@@ -1,14 +1,11 @@
 class OpenshiftCli < Formula
   desc "OpenShift command-line interface tools"
   homepage "https://www.openshift.com/"
+  url "https://github.com/openshift/oc.git",
+      tag:      "openshift-clients-4.12.0-202208031327",
+      revision: "3c85519af6c4979c02ebb1886f45b366bbccbf55"
   license "Apache-2.0"
   head "https://github.com/openshift/oc.git", branch: "master"
-
-  stable do
-    url "https://github.com/openshift/oc.git",
-        tag:      "openshift-clients-4.12.0-202208031327",
-        revision: "3c85519af6c4979c02ebb1886f45b366bbccbf55"
-  end
 
   livecheck do
     url :stable
@@ -17,11 +14,12 @@ class OpenshiftCli < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/openshift-cli"
-    sha256 cellar: :any_skip_relocation, mojave: "fa0c614ef0bdd2643a20ee90a1e31e2c765e1c6fa59a5413b245d5f8872e10d7"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "d02482f8593cb8970fe99cd05283cd20018626082318ba702b624d524932c413"
   end
 
   depends_on "coreutils" => :build
-  depends_on "go@1.18" => :build
+  depends_on "go" => :build
   depends_on "socat"
 
   uses_from_macos "krb5"
@@ -33,15 +31,7 @@ class OpenshiftCli < Formula
     # See https://github.com/golang/go/issues/26487
     ENV.O0 if OS.linux?
 
-    args = ["cross-build-#{os}-#{arch}"]
-    args << if build.stable?
-      "WHAT=cmd/oc"
-    else
-      "WHAT=staging/src/github.com/openshift/oc/cmd/oc"
-    end
-    args << "SHELL=/bin/bash" if OS.linux?
-
-    system "make", *args
+    system "make", "cross-build-#{os}-#{arch}", "OS_GIT_VERSION=#{version}", "SHELL=/bin/bash"
     bin.install "_output/bin/#{os}_#{arch}/oc"
 
     bash_completion.install "contrib/completions/bash/oc"
