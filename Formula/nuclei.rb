c213e52656633ef45f7fe4347a850f5f42f4aece
Adrian Ho <the.gromgit@gmail.com>
2022-10-21 15:52:17 +0800

    nuclei: update 2.7.8 bottle.

--- Formula/nuclei.rb
+++ Formula/nuclei.rb
@@ -1,21 +1,21 @@
 class Nuclei < Formula
   desc "HTTP/DNS scanner configurable via YAML templates"
   homepage "https://nuclei.projectdiscovery.io/"
-  url "https://github.com/projectdiscovery/nuclei/archive/v2.7.7.tar.gz"
-  sha256 "1b80ed46281e25f637ee16fcfd84ca2c458d1d4b4ff04f0087f01bcd2d589de5"
+  url "https://github.com/projectdiscovery/nuclei/archive/v2.7.8.tar.gz"
+  sha256 "91ce140b8dbe0e8cd1f4e49fc80bfa1ec51b841919afd24328419334382832ea"
   license "MIT"
   head "https://github.com/projectdiscovery/nuclei.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nuclei"
-    sha256 cellar: :any_skip_relocation, mojave: "c9970e9cbefaf28a7c82b888eb66fbae20f49d543562fe9a4a199630d1b3eea6"
+    sha256 cellar: :any_skip_relocation, mojave: "324d8c6b7aa1beed4b3a4af4c7655117c25a630a1e5e79b96acfc8039e0b0b5a"
   end
 
   depends_on "go" => :build
 
   def install
     cd "v2/cmd/nuclei" do
-      system "go", "build", *std_go_args, "main.go"
+      system "go", "build", *std_go_args(ldflags: "-s -w"), "main.go"
     end
   end
 
