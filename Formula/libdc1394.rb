82cbb4e52b75c67281b122d295642857bf549a7d
Adrian Ho <the.gromgit@gmail.com>
2022-10-21 11:28:13 +0800

    libdc1394: update 2.2.6_1 bottle.

--- Formula/libdc1394.rb
+++ Formula/libdc1394.rb
@@ -2,6 +2,7 @@ class Libdc1394 < Formula
   desc "Provides API for IEEE 1394 cameras"
   homepage "https://damien.douxchamps.net/ieee1394/libdc1394/"
   license "LGPL-2.1"
+  revision 1
 
   stable do
     url "https://downloads.sourceforge.net/project/libdc1394/libdc1394-2/2.2.6/libdc1394-2.2.6.tar.gz"
@@ -23,15 +24,8 @@ class Libdc1394 < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "7d749b90fc87178b9f168bd129f204e4668758e09beda52b6a9cd2e7c680557c"
-    sha256 cellar: :any,                 arm64_big_sur:  "8af168268139e714a725ab1d4384a34ef092a3e50e081baba66fdf583fef3711"
-    sha256 cellar: :any,                 monterey:       "f838aec8a1a70d8e69dde3fee3e93b8869e821aafad8a5e7fcd2c3739cb0a53b"
-    sha256 cellar: :any,                 big_sur:        "7eef59a97b33666b144d68181b081eba93c4c23c58f159a67684d2ed2d043080"
-    sha256 cellar: :any,                 catalina:       "57080908a5da9abb2c0d83d4ad25450a507de8140a812112d9e5751f4004e4d0"
-    sha256 cellar: :any,                 mojave:         "6cf02c5500f83fa2ccd1ff9b880f44f9652d68b0e90a2345d6c62fb92a988f0a"
-    sha256 cellar: :any,                 high_sierra:    "536cbd34a43886d63a3dba41e7877ed63ad0fbe1a5e21cde499bd2c9e1e37e52"
-    sha256 cellar: :any,                 sierra:         "ff1d7c6b07f21d8cd485574b10091eb21c2316390a7d4cfa84d29cccce8097e6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c95c1747f8a7b05bb423f16a0a2fa2106e6b1413e7424f2fee4a2ed07a92f54a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libdc1394"
+    sha256 cellar: :any, mojave: "b0ba88482309bbe45a19bbd14f01c2a77b2bb2d4d6b32433a5230fbe2755de0b"
   end
 
   head do
@@ -44,7 +38,7 @@ class Libdc1394 < Formula
     depends_on "libusb"
   end
 
-  depends_on "sdl"
+  depends_on "sdl12-compat"
 
   def install
     Dir.chdir("libdc1394") if build.head?
