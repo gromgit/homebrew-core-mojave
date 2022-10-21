1baf567d0428bed4d8c1edfdf0a3b44a5020e2dc
Adrian Ho <the.gromgit@gmail.com>
2022-10-21 23:37:28 +0800

    rdb: update 1.0.5 bottle.

--- Formula/rdb.rb
+++ Formula/rdb.rb
@@ -1,13 +1,13 @@
 class Rdb < Formula
   desc "Redis RDB parser"
   homepage "https://github.com/HDT3213/rdb/"
-  url "https://github.com/HDT3213/rdb/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "156df3d22f5291cba6da07808d43a3add2e97a9c054316c53f296e2ab0ab9829"
+  url "https://github.com/HDT3213/rdb/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "a0b1dc198f9d38c36f0f6e502644ea060c84d352303cd53055497f0211871f13"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rdb"
-    sha256 cellar: :any_skip_relocation, mojave: "ee0282138fdd04224590f440c36b38bd1d0da20bf83d0c43b9138016a16e5a27"
+    sha256 cellar: :any_skip_relocation, mojave: "dcde61bc57b4e60c478974e4076068748474d5bf6ed923b3d5fa9a73ab1dc809"
   end
 
   depends_on "go" => :build
