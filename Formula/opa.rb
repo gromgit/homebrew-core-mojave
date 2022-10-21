03d9752617318329a277140280de9825804cf044
Adrian Ho <the.gromgit@gmail.com>
2022-10-21 16:35:12 +0800

    opa: update 0.45.0 bottle.

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -1,21 +1,24 @@
 class Opa < Formula
   desc "Open source, general-purpose policy engine"
   homepage "https://www.openpolicyagent.org"
-  url "https://github.com/open-policy-agent/opa/archive/v0.44.0.tar.gz"
-  sha256 "277745ca4328671791ec3fc37bf16b4e270c4057c9a43eefdf855986c364dc9a"
+  url "https://github.com/open-policy-agent/opa/archive/v0.45.0.tar.gz"
+  sha256 "d3bf43e9061b9fb3c66f23faa91c3eefe4074c4121ad403133303d2bab54bdb6"
   license "Apache-2.0"
   head "https://github.com/open-policy-agent/opa.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/opa"
-    sha256 cellar: :any_skip_relocation, mojave: "83908821e79ac4a72497230ff018ed8e2d3508b407c8da41264bc5379fd00f21"
+    sha256 cellar: :any_skip_relocation, mojave: "99486a84810f9f65645285237e1d0c61603ee116e118a1fea78af1ed4d463a69"
   end
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args,
-              "-ldflags", "-X github.com/open-policy-agent/opa/version.Version=#{version}"
+    ldflags = %W[
+      -s -w
+      -X github.com/open-policy-agent/opa/version.Version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags)
     system "./build/gen-man.sh", "man1"
     man.install "man1"
 
