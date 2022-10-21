3da95bfbce836dc5a493a383f60da2126030550a
Adrian Ho <the.gromgit@gmail.com>
2022-10-22 03:42:46 +0800

    sysbench: update 1.0.20_3 bottle.

--- Formula/sysbench.rb
+++ Formula/sysbench.rb
@@ -4,20 +4,20 @@ class Sysbench < Formula
   url "https://github.com/akopytov/sysbench/archive/1.0.20.tar.gz"
   sha256 "e8ee79b1f399b2d167e6a90de52ccc90e52408f7ade1b9b7135727efe181347f"
   license "GPL-2.0-or-later"
-  revision 2
+  revision 3
   head "https://github.com/akopytov/sysbench.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sysbench"
-    rebuild 1
-    sha256 cellar: :any, mojave: "cca9b3c6563460dc1344b245c4c6b488680f360d3107ae3c6121f75bb2a6b41e"
+    sha256 cellar: :any, mojave: "c5ad9046e7e86cd9641de668dc787f60f6f3c39610ede5d2465ea7af3e1738ed"
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "luajit-openresty"
+  depends_on "libpq"
+  depends_on "luajit"
   depends_on "mysql-client"
   depends_on "openssl@1.1"
 
@@ -25,7 +25,7 @@ class Sysbench < Formula
 
   def install
     system "./autogen.sh"
-    system "./configure", *std_configure_args, "--with-mysql", "--with-system-luajit"
+    system "./configure", *std_configure_args, "--with-mysql", "--with-pgsql", "--with-system-luajit"
     system "make", "install"
   end
 
