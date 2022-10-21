ca79f8dd4c4e5cef6dc29eb84a7272515b6bc624
Adrian Ho <the.gromgit@gmail.com>
2022-10-22 02:00:07 +0800

    snowflake: update 2.3.1 bottle.

--- Formula/snowflake.rb
+++ Formula/snowflake.rb
@@ -1,14 +1,14 @@
 class Snowflake < Formula
   desc "Pluggable Transport using WebRTC, inspired by Flashproxy"
   homepage "https://www.torproject.org"
-  url "https://gitweb.torproject.org/pluggable-transports/snowflake.git/snapshot/snowflake-2.3.0.tar.gz"
-  sha256 "12be2ab46b174b7a72c7261577f5a89ca301f232e5ff100ef771375337a188d3"
+  url "https://gitweb.torproject.org/pluggable-transports/snowflake.git/snapshot/snowflake-2.3.1.tar.gz"
+  sha256 "47e035ff08668317e719b982a924d048fad738e6081005971e895ddcc2283fba"
   license "BSD-3-Clause"
   head "https://git.torproject.org/pluggable-transports/snowflake.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/snowflake"
-    sha256 cellar: :any_skip_relocation, mojave: "9ec9d4dbac0c17ef70eb48aace54d65735261381c1aa3785a006a7a8248e7a26"
+    sha256 cellar: :any_skip_relocation, mojave: "5b15ebfc569566f4b0666ab17b44fdc016fedd1b618a54ba1f3956ce88c66120"
   end
 
   depends_on "go" => :build
