a9c5eef487abc725c72eb8c8726cc8c6a6e6358c
Adrian Ho <the.gromgit@gmail.com>
2022-10-21 23:24:15 +0800

    rabbitmq-c: update 0.11.0 bottle.

--- Formula/rabbitmq-c.rb
+++ Formula/rabbitmq-c.rb
@@ -7,26 +7,29 @@ class RabbitmqC < Formula
   head "https://github.com/alanxz/rabbitmq-c.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_monterey: "5ef6efee964e2f6006a1cd1606cfc8ea9611b74d94bc3649cf701cc0cb545a18"
-    sha256 cellar: :any,                 arm64_big_sur:  "d060c016414d8d55afa295308d1582fee4db9f36cc43770600fa8bc480e42511"
-    sha256 cellar: :any,                 monterey:       "d6171b1b0042e0d02f941da67cb6c57ae56ea50e76b8798e5073880af7da13b6"
-    sha256 cellar: :any,                 big_sur:        "efe8285e7bdfc661fa5cfede54785b44e817b38fa800e64f75dec2755ae69a7a"
-    sha256 cellar: :any,                 catalina:       "1ae238a471c056d01372fed68b25dbcfe5a29a88f144b9cf09b859a4f287af98"
-    sha256 cellar: :any,                 mojave:         "80ecbc2444e12039a77f178dbd7557bcda2795ea182bc7fd788f16e7f5e48e4c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a34fc2c4bd5afb8f42933b4533892385aa479637101a6e48b01312830ed850ad"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rabbitmq-c"
+    rebuild 2
+    sha256 cellar: :any, mojave: "cfb81e1d92a8097ec15a72587fb86ce174daa2e535189c716bbb031d71872890"
   end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "openssl@1.1"
+  depends_on "xmlto" => :build
+  depends_on "openssl@3"
   depends_on "popt"
 
   def install
-    system "cmake", ".", *std_cmake_args, "-DBUILD_EXAMPLES=OFF",
-                         "-DBUILD_TESTS=OFF", "-DBUILD_API_DOCS=OFF",
-                         "-DBUILD_TOOLS=ON", "-DCMAKE_INSTALL_RPATH=#{rpath}"
-    system "make", "install"
+    ENV["XML_CATALOG_FILES"] = etc/"xml/catalog"
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DBUILD_API_DOCS=OFF",
+                    "-DBUILD_EXAMPLES=OFF",
+                    "-DBUILD_TESTS=OFF",
+                    "-DBUILD_TOOLS=ON",
+                    "-DBUILD_TOOLS_DOCS=ON",
+                    "-DCMAKE_INSTALL_RPATH=#{rpath}",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
