3cf344cd770107fbdeeb709f74523d822b26531f
Adrian Ho <the.gromgit@gmail.com>
2022-10-21 10:57:22 +0800

    lefthook: update 1.1.2 bottle.

--- Formula/lefthook.rb
+++ Formula/lefthook.rb
@@ -1,18 +1,17 @@
 class Lefthook < Formula
   desc "Fast and powerful Git hooks manager for any type of projects"
   homepage "https://github.com/evilmartians/lefthook"
-  url "https://github.com/evilmartians/lefthook/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "49bca0e84f0f6658b5e8bc077fa7aeac7ee4f759cff3bb93a6d07b856ccedce4"
+  url "https://github.com/evilmartians/lefthook/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "fb7bb8c4863192b13e802605b7787cbd1e733b92bacaf5ff34ea5bbca1a56281"
   license "MIT"
   head "https://github.com/evilmartians/lefthook.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lefthook"
-    sha256 cellar: :any_skip_relocation, mojave: "48fdf2d6b9e0748f45c2075e7a1e2e1f39c928122dfd8bc2b8c79db624200319"
+    sha256 cellar: :any_skip_relocation, mojave: "1e7bdeab3d5381c84684bb12318eff01bb79169a596dc834ab8ea4a5a504563d"
   end
 
-  # Bump to 1.18 on the next release, if possible.
-  depends_on "go@1.17" => :build
+  depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
