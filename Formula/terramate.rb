1eed172e5c8857c83e1741be41571eaa587b9cc8
Adrian Ho <the.gromgit@gmail.com>
2022-10-22 05:25:12 +0800

    terramate: update 0.1.36 bottle.

--- Formula/terramate.rb
+++ Formula/terramate.rb
@@ -1,14 +1,14 @@
 class Terramate < Formula
   desc "Managing Terraform stacks with change detections and code generations"
   homepage "https://github.com/mineiros-io/terramate"
-  url "https://github.com/mineiros-io/terramate/archive/refs/tags/v0.1.29.tar.gz"
-  sha256 "dcc64e0011478279de5ca9650a1b723077805cceb820bf14a3074a8b0374419b"
+  url "https://github.com/mineiros-io/terramate/archive/refs/tags/v0.1.36.tar.gz"
+  sha256 "11a7eb194a270dbee8ff4d3fc4a5926aeae685bf656d971a86ded93d6392568a"
   license "Apache-2.0"
   head "https://github.com/mineiros-io/terramate.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/terramate"
-    sha256 cellar: :any_skip_relocation, mojave: "c0e2370c3562e8994fd8d254d37b83ae83941976273460f23c3aa127c807df0a"
+    sha256 cellar: :any_skip_relocation, mojave: "942167debb665639fa725fed6a68cfa1493d3989add304d2dc692b27af3619b2"
   end
 
   depends_on "go" => :build
