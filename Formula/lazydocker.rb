901fc37f810f81a231f8b9df0f1339327414f8c0
Adrian Ho <the.gromgit@gmail.com>
2022-10-21 10:45:39 +0800

    lazydocker: update 0.19.0 bottle.

--- Formula/lazydocker.rb
+++ Formula/lazydocker.rb
@@ -2,13 +2,13 @@ class Lazydocker < Formula
   desc "Lazier way to manage everything docker"
   homepage "https://github.com/jesseduffield/lazydocker"
   url "https://github.com/jesseduffield/lazydocker.git",
-      tag:      "v0.18.1",
-      revision: "da650f4384219e13e0dad3de266501aa0b06859c"
+      tag:      "v0.19.0",
+      revision: "26858c31545b06f9c1ade37cab6e35e1d6611fde"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lazydocker"
-    sha256 cellar: :any_skip_relocation, mojave: "5d9a879a1a1910cc4e21f72db58ce0073edf907bca1bf3034d927e0d350f7845"
+    sha256 cellar: :any_skip_relocation, mojave: "7b4f3ea3ffbc842b5c02a166c56ef3e8160ef323493bfa859708ab0ea5f7ec68"
   end
 
   depends_on "go" => :build
