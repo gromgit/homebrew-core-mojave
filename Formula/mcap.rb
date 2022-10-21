f2d40d91b75e08e84b590c7cf4b81dbd0f8a6043
Adrian Ho <the.gromgit@gmail.com>
2022-10-21 13:06:04 +0800

    mcap: update 0.0.21 bottle.

--- Formula/mcap.rb
+++ Formula/mcap.rb
@@ -1,9 +1,9 @@
 class Mcap < Formula
   desc "Serialization-agnostic container file format for pub/sub messages"
   homepage "https://mcap.dev"
-  url "https://github.com/foxglove/mcap/archive/releases/mcap-cli/v0.0.19.tar.gz"
-  sha256 "ce0aa5febb036db24f5cab5f4cf7daf3ddba77bce8f7b4a3afcd646ed154c608"
-  license "Apache-2.0"
+  url "https://github.com/foxglove/mcap/archive/releases/mcap-cli/v0.0.21.tar.gz"
+  sha256 "e18c322084069e96afcfd47c2255f64eb5656e164e65f9306213b875b53fe442"
+  license "MIT"
   head "https://github.com/foxglove/mcap.git", branch: "main"
 
   livecheck do
@@ -13,14 +13,24 @@ class Mcap < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mcap"
-    sha256 cellar: :any_skip_relocation, mojave: "e1912b4bbbb9991f3d9632b97eea28c1abb306c523cfc8531c7a5b0ded7b4b30"
+    sha256 cellar: :any_skip_relocation, mojave: "2e21641a7867e749811cacc6af378b70c5292604f689d9766601691190f66ce7"
   end
 
   depends_on "go" => :build
 
-  resource "homebrew-testdata" do
-    url "https://media.githubusercontent.com/media/foxglove/mcap/releases/mcap-cli/v0.0.10/tests/conformance/data/OneMessage/OneMessage-ch-chx-mx-pad-rch-rsh-st-sum.mcap"
-    sha256 "9db644f7fad2a256b891946a011fb23127b95d67dc03551b78224aa6cad8c5db"
+  resource "homebrew-testdata-OneMessage" do
+    url "https://github.com/foxglove/mcap/raw/releases/mcap-cli/v0.0.20/tests/conformance/data/OneMessage/OneMessage-ch-chx-mx-pad-rch-rsh-st-sum.mcap"
+    sha256 "16e841dbae8aae5cc6824a63379c838dca2e81598ae08461bdcc4e7334e11da4"
+  end
+
+  resource "homebrew-testdata-OneAttachment" do
+    url "https://github.com/foxglove/mcap/raw/releases/mcap-cli/v0.0.20/tests/conformance/data/OneAttachment/OneAttachment-ax-pad-st-sum.mcap"
+    sha256 "f9dde0a5c9f7847e145be73ea874f9cdf048119b4f716f5847513ee2f4d70643"
+  end
+
+  resource "homebrew-testdata-OneMetadata" do
+    url "https://github.com/foxglove/mcap/raw/releases/mcap-cli/v0.0.20/tests/conformance/data/OneMetadata/OneMetadata-mdx-pad-st-sum.mcap"
+    sha256 "cb779e0296d288ad2290d3c1911a77266a87c0bdfee957049563169f15d6ba8e"
   end
 
   def install
@@ -28,30 +38,23 @@ class Mcap < Formula
       system "make", "build", "VERSION=v#{version}"
       bin.install "bin/mcap"
     end
+    generate_completions_from_executable(bin/"mcap", "completion", shells: [:bash, :zsh, :fish])
   end
 
   test do
-    assert_equal "v#{version}", pipe_output("#{bin}/mcap version").strip
+    assert_equal "v#{version}", shell_output("#{bin}/mcap version").strip
 
-    resource("homebrew-testdata").stage do
+    resource("homebrew-testdata-OneMessage").stage do
       assert_equal "2 example [Example] [1 2 3]",
-      pipe_output("#{bin}/mcap cat", File.read("OneMessage-ch-chx-mx-pad-rch-rsh-st-sum.mcap")).strip
-
-      expected_info = <<~EOF
-        library:
-        profile:
-        messages: 1
-        duration: 0s
-        start: 0.000
-        end: 0.000
-        compression:
-        	: [1/1 chunks] (0.00%)
-        channels:
-          	(1) example  1 msgs (+Inf Hz)   : Example [c]
-        attachments: 0
-      EOF
-      assert_equal expected_info.lines.map(&:strip),
-        shell_output("#{bin}/mcap info OneMessage-ch-chx-mx-pad-rch-rsh-st-sum.mcap").lines.map(&:strip)
+      shell_output("#{bin}/mcap cat OneMessage-ch-chx-mx-pad-rch-rsh-st-sum.mcap").strip
+    end
+    resource("homebrew-testdata-OneAttachment").stage do
+      assert_equal "\x01\x02\x03",
+      shell_output("#{bin}/mcap get attachment OneAttachment-ax-pad-st-sum.mcap --name myFile")
+    end
+    resource("homebrew-testdata-OneMetadata").stage do
+      assert_equal({ "foo" => "bar" },
+      JSON.parse(shell_output("#{bin}/mcap get metadata OneMetadata-mdx-pad-st-sum.mcap --name myMetadata")))
     end
   end
 end
