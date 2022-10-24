62be0c76c3d52f926ebb0c305ffefbaf0336269a
Adrian Ho <the.gromgit@gmail.com>
2022-10-25 00:06:50 +0800

    sdl_sound: add 1.0.3_2 bottle.

--- Formula/sdl_sound.rb
+++ Formula/sdl_sound.rb
@@ -4,12 +4,11 @@ class SdlSound < Formula
   url "https://icculus.org/SDL_sound/downloads/SDL_sound-1.0.3.tar.gz"
   mirror "https://deb.debian.org/debian/pool/main/s/sdl-sound1.2/sdl-sound1.2_1.0.3.orig.tar.gz"
   sha256 "3999fd0bbb485289a52be14b2f68b571cb84e380cc43387eadf778f64c79e6df"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sdl_sound"
-    rebuild 2
-    sha256 cellar: :any, mojave: "650a430a82281b12a69721029a7b42afc799cf706d985c339d1d3457600d09fc"
+    sha256 cellar: :any_skip_relocation, mojave: "7bdae09fab4ca29f81db03ca5b8a25748a0113a907dc6fc2cb9cdf850134c197"
   end
 
   head do
@@ -20,6 +19,8 @@ class SdlSound < Formula
     depends_on "libtool" => :build
   end
 
+  keg_only "it conflicts with `sdl2_sound`"
+
   # SDL 1.2 is deprecated, unsupported, and not recommended for new projects.
   # Commented out while this formula still has dependents.
   # deprecate! date: "2013-08-17", because: :deprecated_upstream
@@ -27,7 +28,7 @@ class SdlSound < Formula
   depends_on "pkg-config" => :build
   depends_on "libogg"
   depends_on "libvorbis"
-  depends_on "sdl"
+  depends_on "sdl12-compat"
 
   def install
     system "./autogen.sh" if build.head?
