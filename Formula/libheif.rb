24d34a358c56fe0e80491d45e3196d9c196f44a2
Adrian Ho <the.gromgit@gmail.com>
2022-11-03 21:42:15 +0800

    libheif: add 1.13.0_1 bottle.

--- Formula/libheif.rb
+++ Formula/libheif.rb
@@ -1,14 +1,14 @@
 class Libheif < Formula
   desc "ISO/IEC 23008-12:2017 HEIF file format decoder and encoder"
   homepage "https://www.libde265.org/"
-  url "https://github.com/strukturag/libheif/releases/download/v1.12.0/libheif-1.12.0.tar.gz"
-  sha256 "e1ac2abb354fdc8ccdca71363ebad7503ad731c84022cf460837f0839e171718"
+  url "https://github.com/strukturag/libheif/releases/download/v1.13.0/libheif-1.13.0.tar.gz"
+  sha256 "c20ae01bace39e89298f6352f1ff4a54b415b33b9743902da798e8a1e51d7ca1"
   license "LGPL-3.0-only"
-  revision 2
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libheif"
-    sha256 cellar: :any, mojave: "ac2e096f94e8c4aa51d17d3e08ec58254997f0e49774e34f9b04abb469b5040b"
+    sha256 cellar: :any_skip_relocation, mojave: "bd802245860bec9be35df065c1eb0609c6d6805757a2cbd36ce93dd9689296fd"
   end
 
   depends_on "pkg-config" => :build
@@ -18,12 +18,6 @@ class Libheif < Formula
   depends_on "libpng"
   depends_on "shared-mime-info"
   depends_on "x265"
-
-  # Fix -flat_namespace being used on Big Sur and later.
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-    sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-  end
 
   def install
     system "./configure", *std_configure_args, "--disable-silent-rules"
@@ -45,7 +39,7 @@ class Libheif < Formula
     assert_predicate testpath/"exampleheic-1.jpg", :exist?
     assert_predicate testpath/"exampleheic-2.jpg", :exist?
 
-    output = "File contains 1 images"
+    output = "File contains 1 image"
     example = pkgshare/"example.avif"
     exout = testpath/"exampleavif.jpg"
 
