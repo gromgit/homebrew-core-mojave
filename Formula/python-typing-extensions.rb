d2c0e05970325f482664d846597a3d520d0b0b76
Adrian Ho <the.gromgit@gmail.com>
2022-11-06 08:56:46 +0800

    python-typing-extensions: add 4.4.0 bottle.

---
+++ Formula/python-typing-extensions.rb
@@ -0,0 +1,72 @@
+class PythonTypingExtensions < Formula
+  desc "Backported and experimental type hints for Python"
+  homepage "https://github.com/python/typing_extensions"
+  url "https://files.pythonhosted.org/packages/e3/a7/8f4e456ef0adac43f452efc2d0e4b242ab831297f1bac60ac815d37eb9cf/typing_extensions-4.4.0.tar.gz"
+  sha256 "1511434bb92bf8dd198c12b1cc812e800d4181cfcb867674e0f8279cc93087aa"
+  license "Python-2.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/python-typing-extensions"
+    sha256 cellar: :any_skip_relocation, mojave: "33332bc8bbbef9c68222173485f8aa6ec121f76412355e58a7ee5948db70bfa7"
+  end
+
+  depends_on "flit" => :build
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.8" => [:build, :test]
+  depends_on "python@3.9" => [:build, :test]
+  depends_on "mypy" => :test
+
+  def pythons
+    deps.select { |dep| dep.name.start_with?("python") }
+        .map(&:to_formula)
+        .sort_by(&:version)
+  end
+
+  def install
+    system Formula["flit"].opt_bin/"flit", "build", "--format", "wheel"
+    wheel = Pathname.glob("dist/typing_extensions-*.whl").first
+    pip_flags = %W[
+      --verbose
+      --isolated
+      --no-deps
+      --no-binary=:all:
+      --ignore-installed
+      --prefix=#{prefix}
+    ]
+    pythons.each do |python|
+      pip = python.opt_libexec/"bin/pip"
+      system pip, "install", *pip_flags, wheel
+    end
+  end
+
+  def caveats
+    python_versions = pythons.map { |p| p.version.major_minor }
+                             .map(&:to_s)
+                             .join(", ")
+
+    <<~EOS
+      This formula provides the `typing_extensions` module for Python #{python_versions}.
+      If you need `typing_extensions` for a different version of Python, use pip.
+    EOS
+  end
+
+  test do
+    pythons.each do |python|
+      system python.opt_libexec/"bin/python", "-c", <<~EOS
+        import typing_extensions
+      EOS
+    end
+
+    (testpath/"test.py").write <<~EOS
+      import typing_extensions
+
+      class Movie(typing_extensions.TypedDict):
+          title: str
+          year: typing_extensions.NotRequired[int]
+
+      m = Movie(title="Grease")
+    EOS
+    mypy = Formula["mypy"].opt_bin/"mypy"
+    system mypy, testpath/"test.py"
+  end
+end
