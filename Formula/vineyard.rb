09bc7ccf49e95257bf419d32670df1c62f3899fa
Adrian Ho <the.gromgit@gmail.com>
2022-11-07 23:26:27 +0800

    vineyard: add 0.9.0_1 bottle.

---
+++ Formula/vineyard.rb
@@ -0,0 +1,99 @@
+class Vineyard < Formula
+  include Language::Python::Virtualenv
+
+  desc "In-memory immutable data manager. (Project under CNCF)"
+  homepage "https://v6d.io"
+  url "https://github.com/v6d-io/v6d/releases/download/v0.9.0/v6d-0.9.0.tar.gz"
+  sha256 "bd1e5ba09c162e1ff046266644bf18065a50ee1a4ad1def483cab62051cadb03"
+  license "Apache-2.0"
+  revision 1
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/vineyard"
+    sha256 cellar: :any_skip_relocation, mojave: "a43a02f595e108167058b2776f44a69b8d2733c1616c26286feaddf2e5042b46"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "python@3.10" => :build
+  depends_on "apache-arrow"
+  depends_on "boost"
+  depends_on "etcd"
+  depends_on "etcd-cpp-apiv3"
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "libgrape-lite"
+  depends_on "llvm"
+  depends_on "nlohmann-json"
+  depends_on "open-mpi"
+  depends_on "openssl@1.1"
+  depends_on "tbb"
+
+  fails_with gcc: "5"
+
+  def install
+    python = "python3.10"
+    # LLVM is keg-only.
+    ENV.prepend_path "PYTHONPATH", Formula["llvm"].opt_prefix/Language::Python.site_packages(python)
+
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DCMAKE_CXX_STANDARD=14",
+                    "-DCMAKE_CXX_STANDARD_REQUIRED=TRUE",
+                    "-DPYTHON_EXECUTABLE=#{which(python)}",
+                    "-DUSE_EXTERNAL_ETCD_LIBS=ON",
+                    "-DUSE_EXTERNAL_TBB_LIBS=ON",
+                    "-DUSE_EXTERNAL_NLOHMANN_JSON_LIBS=ON",
+                    "-DBUILD_VINEYARD_TESTS=OFF",
+                    "-DOPENSSL_ROOT_DIR=#{Formula["openssl@1.1"].opt_prefix}",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cc").write <<~EOS
+      #include <iostream>
+      #include <memory>
+
+      #include <vineyard/client/client.h>
+
+      int main(int argc, char **argv) {
+        vineyard::Client client;
+        VINEYARD_CHECK_OK(client.Connect(argv[1]));
+
+        std::shared_ptr<vineyard::InstanceStatus> status;
+        VINEYARD_CHECK_OK(client.InstanceStatus(status));
+        std::cout << "vineyard instance is: " << status->instance_id << std::endl;
+
+        return 0;
+      }
+    EOS
+
+    system ENV.cxx, "test.cc", "-std=c++17",
+                    "-I#{Formula["apache-arrow"].include}",
+                    "-I#{Formula["boost"].include}",
+                    "-I#{include}",
+                    "-I#{include}/vineyard",
+                    "-I#{include}/vineyard/contrib",
+                    "-L#{Formula["apache-arrow"].lib}",
+                    "-L#{Formula["boost"].lib}",
+                    "-L#{lib}",
+                    "-larrow",
+                    "-lboost_thread-mt",
+                    "-lboost_system-mt",
+                    "-lvineyard_client",
+                    "-o", "test_vineyard_client"
+
+    # prepare vineyardd
+    vineyardd_pid = spawn bin/"vineyardd", "--norpc",
+                                           "--meta=local",
+                                           "--socket=#{testpath}/vineyard.sock"
+
+    # sleep to let vineyardd get its wits about it
+    sleep 10
+
+    assert_equal("vineyard instance is: 0\n", shell_output("./test_vineyard_client #{testpath}/vineyard.sock"))
+  ensure
+    # clean up the vineyardd process before we leave
+    Process.kill("HUP", vineyardd_pid)
+  end
+end
