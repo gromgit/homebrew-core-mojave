227f84dacbf1ac050b9e5ecdc54245fb4bc2dfa4
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:37:27 +0800

    spotbugs: no rebuilding required

--- Formula/spotbugs.rb
+++ Formula/spotbugs.rb
@@ -1,12 +1,12 @@
 class Spotbugs < Formula
   desc "Tool for Java static analysis (FindBugs's successor)"
   homepage "https://spotbugs.github.io/"
-  url "https://repo.maven.apache.org/maven2/com/github/spotbugs/spotbugs/4.7.2/spotbugs-4.7.2.tgz"
-  sha256 "f02a023d03b0fde70038ccb4bc8d4a964a504262d13024a97b14d9070f7d4d96"
+  url "https://repo.maven.apache.org/maven2/com/github/spotbugs/spotbugs/4.7.3/spotbugs-4.7.3.tgz"
+  sha256 "f02e2f1135b23f3edfddb75f64be0491353cfeb567b5a584115aa4fd373d4431"
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "cbe989816f6a39c71910102f5dba1879d06273df7e11022524653dd0effc14b7"
+    sha256 cellar: :any_skip_relocation, all: "c4a351e6423603de5b05a114912e6091977f287357ea8c23fbba1944026bba72"
   end
 
   head do
