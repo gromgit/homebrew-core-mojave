fb663819b1a04ee03a6a8de12aa5225769a05f66
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:00:24 +0800

    bindfs:

--- Formula/bindfs.rb
+++ Formula/bindfs.rb
@@ -1,12 +1,12 @@
 class Bindfs < Formula
   desc "FUSE file system for mounting to another location"
   homepage "https://bindfs.org/"
-  url "https://bindfs.org/downloads/bindfs-1.17.0.tar.gz"
-  sha256 "70da57d49e7794fe54b8575bfdd6a7943aab54ada2e8e2fdf4be04e0011451dc"
+  url "https://bindfs.org/downloads/bindfs-1.17.1.tar.gz"
+  sha256 "edb4989144d28f75affc4f5b18074fb97a58d6ee35ad6919ac75eb6a4cbfe352"
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7dc31f9e7e786c82ba5ac338bd20761e588b99cca5468065dad23df2c2193a34"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "591b1b620631425439e0d7ecca9b9ad6b7427dc501a7417535accaea60bc404a"
   end
 
   head do
