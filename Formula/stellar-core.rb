db0eae397cedf956e06edb40799aa08b65ce8d3d
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:37:56 +0800

    stellar-core:

--- Formula/stellar-core.rb
+++ Formula/stellar-core.rb
@@ -2,10 +2,19 @@ class StellarCore < Formula
   desc "Backbone of the Stellar (XLM) network"
   homepage "https://www.stellar.org/"
   url "https://github.com/stellar/stellar-core.git",
-      tag:      "v19.3.0",
-      revision: "9ce6dc4e9889ce86083c0e3ba2e773e0ff2ced3a"
+      tag:      "v19.5.0",
+      revision: "ca2fb06059c15442cb4c9a8c89de1a8fc3579a39"
   license "Apache-2.0"
   head "https://github.com/stellar/stellar-core.git", branch: "master"
+
+  bottle do
+    sha256 cellar: :any,                 arm64_monterey: "4c55b1f92a877b592994d808cb2ad4e5562fe53684434469a9975f4d3d1a7f3c"
+    sha256 cellar: :any,                 arm64_big_sur:  "3ea5f35da2f08549ae689176f80bbaa094de9d7f150c6e84dde12f31638918ec"
+    sha256 cellar: :any,                 monterey:       "bc62a5747f41ae8aa3fbbb669a59c42036e95e858ca1766f9846496f11944968"
+    sha256 cellar: :any,                 big_sur:        "b6367ac81891a77cbc2dcbaa4698bfb9aca03dccac7ae17e6e19aed49892a424"
+    sha256 cellar: :any,                 catalina:       "0ca7388d9c523d084635ad9d94aaafdfa6d8c87e90c9c15ff06aca39285d8f7f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4693307b48dadaf39e0becd4d6dba54efdd662014e29658bbc3046a6f7f1b3da"
+  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -42,7 +51,7 @@ class StellarCore < Formula
 
   test do
     system "#{bin}/stellar-core", "test",
-      "'[bucket],[crypto],[herder],[upgrades],[accountsubentriescount]," \
-      "[bucketlistconsistent],[cacheisconsistent],[fs]'"
+      "'[herder],[upgrades],[accountsubentriescount]," \
+      "[bucketlistconsistent],[topology]'"
   end
 end
