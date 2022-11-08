5d73805311a4c0d659f615b9db20b3f437377c10
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:13:06 +0800

    grunt-cli: no rebuilding required

--- Formula/grunt-cli.rb
+++ Formula/grunt-cli.rb
@@ -8,6 +8,7 @@ class GruntCli < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7e878a48df0184f262a7d4a0d4967b7623f9327e29206faaf90758664de3dd19"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "38f67054b492a11847be41d443b32c017fdbb9b94265ce42299675ea8742ef99"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8eafad607c94848c1bd74eca2a52b92533f399247c85d4de923ff12367ce2cda"
     sha256 cellar: :any_skip_relocation, monterey:       "af276cc7570d11abe7da586cc0dfcee75947df3b58bcd29892722d8654649668"
