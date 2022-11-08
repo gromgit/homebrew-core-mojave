d4a0a3c5408771bb6d55dcbf39bae83e80a8967f
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:30:54 +0800

    pcp: no rebuilding required

--- Formula/pcp.rb
+++ Formula/pcp.rb
@@ -8,9 +8,14 @@ class Pcp < Formula
   head "https://github.com/dennis-tra/pcp.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pcp"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "6f6696dc055c98c59a4df1f23dc599163115fd86714524e9f681d5b6eea186c1"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b1bfa73d57867b1d14809dca455e97ebcb4cb36c67080f64f8a44b242719cf79"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "127549faab2d9cb13ebd4ea2d7dfd17f0054776367769631b387aada1f7eacd6"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8d495ee0f766c2d09355453d4f4691c7d5ca9bdf57ac82c0ef1d3552df19fefe"
+    sha256 cellar: :any_skip_relocation, monterey:       "37aa1824b66b4581a3c2054c26731ee41ab757e3d768399ec43dedbfe2bc183e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7fec012a0331f8ded437d4af20dd6ff527ca0b10ca8cfba73a40aa637358ec54"
+    sha256 cellar: :any_skip_relocation, catalina:       "672ebdfce1cb30d596792cdc8652b1b2f00e19a66f9266a7796cf6b7b6d25a9f"
+    sha256 cellar: :any_skip_relocation, mojave:         "d5e48f3b7f8e0e0034dede947f94b0d5a23311e6ced60093b2cdfea9427389e2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "be128448d96fd7005c9cbda1c1ba2c71e7f7a58468c1c5bb3ec7a1f75c5109c5"
   end
 
   depends_on "go" => :build
