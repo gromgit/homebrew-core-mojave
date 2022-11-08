1972641c26aa1ffb4ffc51ff10d516af8c463670
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:26:22 +0800

    minisat: no rebuilding required

--- Formula/minisat.rb
+++ Formula/minisat.rb
@@ -7,9 +7,14 @@ class Minisat < Formula
   head "https://github.com/stp/minisat.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/minisat"
-    rebuild 1
-    sha256 cellar: :any, mojave: "b77dfd91ad0a32d41f137fc146ade3eecd8fce5bee9a7a0c75edf8823c93abcc"
+    sha256 cellar: :any,                 arm64_ventura:  "518ea1441facc4926c3edddf1fd5e6bf4d2db6460f10d4c4d4ef6c9fd69dcd3d"
+    sha256 cellar: :any,                 arm64_monterey: "22895418f1f5e0d2a1efb9cc700f40bdb29e9809423f0c7949eeaacbbbf7b4f3"
+    sha256 cellar: :any,                 arm64_big_sur:  "b802117d6cc0fa96bedac9eaa086908687ad87c7a368558e47c2417d3d2b7146"
+    sha256 cellar: :any,                 monterey:       "5b028ad6aa66e082709083453b17d150eee4cb2a134b8cccd8c62567928b5859"
+    sha256 cellar: :any,                 big_sur:        "ecc424ea3cde4f1a8d7056802d384ce1ab9823393301cac432dc26260685437a"
+    sha256 cellar: :any,                 catalina:       "0940a0cb0c4c4d6130d1eb7aa698561d6b01904044ee57731ad2c12092e30e46"
+    sha256 cellar: :any,                 mojave:         "cc7b59b30490175c56a6068c97956c39da31068df2522376da99998761972319"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "1277996ab9a57f7c617501f9b386c2bb18ac90e00293738c12222dd89d90a979"
   end
 
   depends_on "cmake" => :build
