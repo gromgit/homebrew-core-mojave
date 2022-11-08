824a2f6446655e2126cac3199377c02be088dab9
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:01:04 +0800

    bubblewrap:

--- Formula/bubblewrap.rb
+++ Formula/bubblewrap.rb
@@ -1,12 +1,12 @@
 class Bubblewrap < Formula
   desc "Unprivileged sandboxing tool for Linux"
   homepage "https://github.com/containers/bubblewrap"
-  url "https://github.com/containers/bubblewrap/releases/download/v0.6.2/bubblewrap-0.6.2.tar.xz"
-  sha256 "8a0ec802d1b3e956c5bb0a40a81c9ce0b055a31bf30a8efa547433603b8af20b"
+  url "https://github.com/containers/bubblewrap/releases/download/v0.7.0/bubblewrap-0.7.0.tar.xz"
+  sha256 "764ab7100bd037ea53d440d362e099d7a425966bc62d1f00ab26b8fbb882a9dc"
   license "LGPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "df72ba0d37df60c17a583716c0b2194c465e001491b559726e3beb9e0fa0452b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "73f968a609c8ccb42909bfdebab5addcd52c59ecda5bfb19a081ae9f95e0dbbf"
   end
 
   head do
