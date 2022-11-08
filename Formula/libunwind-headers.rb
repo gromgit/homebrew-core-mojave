69f2be56417feebc6ca3fee59add757b5d66b5e3
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:22:54 +0800

    libunwind-headers: no rebuilding required

--- Formula/libunwind-headers.rb
+++ Formula/libunwind-headers.rb
@@ -1,14 +1,9 @@
 class LibunwindHeaders < Formula
   desc "C API for determining the call-chain of a program"
   homepage "https://opensource.apple.com/"
-  url "https://opensource.apple.com/tarballs/libunwind/libunwind-201.tar.gz"
-  sha256 "415daa5481e0cd5923b2a923c6906586e7231ebfb9fabf06bc5230bd7a7c7140"
+  url "https://github.com/apple-oss-distributions/libunwind/archive/refs/tags/libunwind-201.tar.gz"
+  sha256 "bbe469bd8778ba5a3e420823b9bf96ae98757a250f198893dee4628e0a432899"
   license "APSL-2.0"
-
-  livecheck do
-    url "https://opensource.apple.com/tarballs/libunwind/"
-    regex(/href=.*?libunwind[._-]v?(\d+(?:\.\d+)*)\.t/i)
-  end
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "f4e338982c769a851a7a3083e7cb9f99931436bcc85b767550e260688733b0b0"
