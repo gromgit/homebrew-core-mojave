1645fa89948116bb7365c94bfe30974709d5083a
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:40:57 +0800

    unittest: no rebuilding required

--- Formula/unittest.rb
+++ Formula/unittest.rb
@@ -10,6 +10,7 @@ class Unittest < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4c301be0c1a8cabb9339ce5f1a2284c6c9d7c54a3f9458f92563d388ec418f0b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "dc9de7f5271e4a57ce5f2bd565924006f39ebb8f136ba5f4493778007c019ff5"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2da59f3f0206902816c2dac6c273768858f092eb917b696b0a8b04096ea97007"
     sha256 cellar: :any_skip_relocation, monterey:       "d83feaeec071b449126875572d29d4df64580c06c2f4a8fcc4fca55a5c68c7f4"
