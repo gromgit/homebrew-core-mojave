788435eab806d62cc6a853b00fbc8fb3cc1085c3
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:09:05 +0800

    flume: no rebuilding required

--- Formula/flume.rb
+++ Formula/flume.rb
@@ -1,13 +1,13 @@
 class Flume < Formula
   desc "Hadoop-based distributed log collection and aggregation"
   homepage "https://flume.apache.org"
-  url "https://www.apache.org/dyn/closer.lua?path=flume/1.10.1/apache-flume-1.10.1-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/flume/1.10.1/apache-flume-1.10.1-bin.tar.gz"
-  sha256 "f82c6625901cd5853853dfbc895a27bb5d6c0beebc365c01fd881eb9753188a1"
+  url "https://www.apache.org/dyn/closer.lua?path=flume/1.11.0/apache-flume-1.11.0-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/flume/1.11.0/apache-flume-1.11.0-bin.tar.gz"
+  sha256 "6eb7806076bdc3dcadb728275eeee7ba5cb12b63a2d981de3da9063008dba678"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "7375590ff5eddb9609f2d330bda1fe998fa775dd800fae3df24c5e9cbd5b9f49"
+    sha256 cellar: :any_skip_relocation, all: "44935d1696a0723df20bbf9b4426ea278eb4293d2c22bfad5d9baf448d718a6a"
   end
 
   depends_on "hadoop"
