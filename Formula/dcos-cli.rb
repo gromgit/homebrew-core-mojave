5dc694cd595fe1ba7882b2c62da3fbb5b2694362
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:05:12 +0800

    dcos-cli: no rebuilding required

--- Formula/dcos-cli.rb
+++ Formula/dcos-cli.rb
@@ -13,6 +13,7 @@ class DcosCli < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "4cf3b69223bb06a5b08f8f48eb320a2b45ef2fcf8e3f63c2e5fb49881872b7bb"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4b7c3e8a7a0d91d84fd13cad41079bd7d718928b1acbaede6f7c5fc0f419b1bc"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f86f45ed4b5244b46a83cb5bf5bc5ac869dfbae3af926f175bee78a0ebd9b47a"
     sha256 cellar: :any_skip_relocation, monterey:       "26c6a023e4d2cf388c41f684d52f3d427f57d0a6eacd54a286c5f7c56efb7957"
