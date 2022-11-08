461ebfa8dbd7df9069f6281022a60b9136ba2ed2
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:07:03 +0800

    e2tools: no rebuilding required

--- Formula/e2tools.rb
+++ Formula/e2tools.rb
@@ -6,6 +6,7 @@ class E2tools < Formula
   license "GPL-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "11e1f3be71a478efda672fea5d23fc466c5d23d571809b459a56cb4967258fe3"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4764d51ea5016a50dc90a7665db17f327742011eb24d6bc3635b5e4cb7e3efda"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e4c94d559938d504bc7ddc19139c8af004385b57db58bfd24946c451b4752928"
     sha256 cellar: :any_skip_relocation, monterey:       "46ec372f7ee195a92d3d0f3ffca17192b48a2e9f1dd1f74b721aed99ad90cdc7"
