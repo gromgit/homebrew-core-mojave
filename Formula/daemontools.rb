8847d0ffdbfdc9c80a94e3f992879535e477f5f9
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:04:51 +0800

    daemontools: no rebuilding required

--- Formula/daemontools.rb
+++ Formula/daemontools.rb
@@ -12,6 +12,7 @@ class Daemontools < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e3e38b536653bd1b7e2e8d0e98dca15f9085205d3eeed09d8d1787afe468a08b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f66a2fccba83ee8fe623c06d8b53dfb241cdeffd4af6ccdd8ac499c5b3191a2a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4970abde6563bd8fa9cae9478b81d241ce0ad0c4d1504aa84269c55ccb45a499"
     sha256 cellar: :any_skip_relocation, monterey:       "23c7f34339a55c30a0e32c45b200030e766eeae0fc7cc873de70bae175849123"
