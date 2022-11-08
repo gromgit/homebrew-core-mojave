f1a494da35cc1c41df94528c73a24ed30f60f450
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:16:24 +0800

    jmxterm: no rebuilding required

--- Formula/jmxterm.rb
+++ Formula/jmxterm.rb
@@ -1,12 +1,12 @@
 class Jmxterm < Formula
   desc "Open source, command-line based interactive JMX client"
   homepage "https://docs.cyclopsgroup.org/jmxterm"
-  url "https://github.com/jiaqi/jmxterm/releases/download/v1.0.3/jmxterm-1.0.3-uber.jar"
-  sha256 "04b16c9e49b77acbe33014fbf564dc07fac4f223b63ef8bfa865abfed337155f"
+  url "https://github.com/jiaqi/jmxterm/releases/download/v1.0.4/jmxterm-1.0.4-uber.jar"
+  sha256 "ce3e78c732a8632f084f8114d50ca5022cef4a69d68a74b45f5007d34349872b"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "29b956c7183066e8c41f5e72ecf54d801781b007a391bc0f64bbb33f89a69e2d"
+    sha256 cellar: :any_skip_relocation, all: "afe466c4ad8752e59e66da6b83f02c7d772aca26a5f0fcd4d1c5df2a27e92c70"
   end
 
   depends_on "openjdk"
