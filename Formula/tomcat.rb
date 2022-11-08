89c38e896f4ed2ca80b5150482f574192f987f7a
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:39:59 +0800

    tomcat: no rebuilding required

--- Formula/tomcat.rb
+++ Formula/tomcat.rb
@@ -1,13 +1,13 @@
 class Tomcat < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-10/v10.1.0/bin/apache-tomcat-10.1.0.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-10/v10.1.0/bin/apache-tomcat-10.1.0.tar.gz"
-  sha256 "5b3436c8f4dd09b928bcaf4ca8a60d2c62ee9e422818b134be941f8f4ac736ba"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-10/v10.1.1/bin/apache-tomcat-10.1.1.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-10/v10.1.1/bin/apache-tomcat-10.1.1.tar.gz"
+  sha256 "db82e7308a1f16ecdd23a3a2949b0f052c46beb0e6f869386affb3d4e840fc7d"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "323c5363483ace1ec02422d0959dad2184eb3463c12e06763b1c5945ebbd94f4"
+    sha256 cellar: :any_skip_relocation, all: "1a0dedbc3ebff66c225e8850a524e3c5598b5e7c9281ff4c3ed67800707f6bbe"
   end
 
   depends_on "openjdk"
