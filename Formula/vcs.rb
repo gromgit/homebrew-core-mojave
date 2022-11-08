b65b4374b4ab6d209fab0e543283290c1f48d8cf
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:41:34 +0800

    vcs: no rebuilding required

--- Formula/vcs.rb
+++ Formula/vcs.rb
@@ -11,6 +11,7 @@ class Vcs < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "032fbce3c72e8ea03c3b4fbcde03f391d7c9df149ae5b664618d7e5b2a265bce"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "032fbce3c72e8ea03c3b4fbcde03f391d7c9df149ae5b664618d7e5b2a265bce"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5a12b2c51afec7626a44463cd9145bf47819e8561f05194f2f17ea8eec0459c9"
     sha256 cellar: :any_skip_relocation, monterey:       "67aafd60a6d2b32a6bb487860c1ba6e6add8e082e9e6b5d4724d9932849940ce"
