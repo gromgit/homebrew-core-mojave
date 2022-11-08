34facaedc7418dc86b9223ed4dad790be2584db9
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:33:08 +0800

    pygments: no rebuilding required

--- Formula/pygments.rb
+++ Formula/pygments.rb
@@ -6,18 +6,42 @@ class Pygments < Formula
   url "https://files.pythonhosted.org/packages/e0/ef/5905cd3642f2337d44143529c941cc3a02e5af16f0f65f81cbef7af452bb/Pygments-2.13.0.tar.gz"
   sha256 "56a8508ae95f98e2b9bdf93a6be5ae3f7d8af858b43e02c5a2ff083726be40c1"
   license "BSD-2-Clause"
+  revision 1
   head "https://github.com/pygments/pygments.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pygments"
-    sha256 cellar: :any_skip_relocation, mojave: "160c038d24aa8d3ad62b15846e72fa092738132c4c33d8382db29b1f771d06e8"
+    sha256 cellar: :any_skip_relocation, all: "4c44f5496e7479ed4e0e020185e2601c4a4d8c16b3e2b88c2a6352c89cd79bb2"
   end
 
-  depends_on "python@3.10"
+  depends_on "python@3.10" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.9" => [:build, :test]
+
+  def pythons
+    deps.select { |dep| dep.name.start_with?("python") }
+        .map(&:to_formula)
+        .sort_by(&:version)
+  end
 
   def install
     bash_completion.install "external/pygments.bashcomp" => "pygmentize"
-    virtualenv_install_with_resources
+
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, *Language::Python.setup_install_args(libexec, python_exe)
+
+      site_packages = Language::Python.site_packages(python_exe)
+      pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
+      (prefix/site_packages/"homebrew-pygments.pth").write pth_contents
+
+      pyversion = Language::Python.major_minor_version(python_exe)
+      bin.install libexec/"bin/pygmentize" => "pygmentize-#{pyversion}"
+
+      next unless python == pythons.max_by(&:version)
+
+      # The newest one is used as the default
+      bin.install_symlink "pygmentize-#{pyversion}" => "pygmentize"
+    end
   end
 
   test do
@@ -26,7 +50,19 @@ class Pygments < Formula
       print(os.getcwd())
     EOS
 
-    system bin/"pygmentize", "-f", "html", "-o", "test.html", testpath/"test.py"
-    assert_predicate testpath/"test.html", :exist?
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      pyversion = Language::Python.major_minor_version(python_exe)
+
+      system bin/"pygmentize-#{pyversion}", "-f", "html", "-o", "test.html", testpath/"test.py"
+      assert_predicate testpath/"test.html", :exist?
+
+      (testpath/"test.html").unlink
+
+      next unless python == pythons.max_by(&:version)
+
+      system bin/"pygmentize", "-f", "html", "-o", "test.html", testpath/"test.py"
+      assert_predicate testpath/"test.html", :exist?
+    end
   end
 end
