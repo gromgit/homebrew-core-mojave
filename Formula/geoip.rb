020466230dd63c05edb96b35cf1abdba84c6da4e
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:10:18 +0800

    geoip: no rebuilding required

--- Formula/geoip.rb
+++ Formula/geoip.rb
@@ -7,9 +7,15 @@ class Geoip < Formula
   head "https://github.com/maxmind/geoip-api-c.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/geoip"
-    rebuild 3
-    sha256 mojave: "abd0d0172e8bfe609294328283ee73810b09d3ff5b0cccac3bc68cbe710d52e5"
+    rebuild 2
+    sha256 cellar: :any,                 arm64_ventura:  "40ef31c663a905f3786b8e410f651ee97cc0ae805d7c22d52c20b9c518ba12ba"
+    sha256 cellar: :any,                 arm64_monterey: "c2ae63b815cc930a90d57c78a1d7a51a57085d67aa20431db716982353a950b2"
+    sha256 cellar: :any,                 arm64_big_sur:  "fd8787e3b88f91d97504992874d3fd358c6865e03fc19c5123b8e7f1a691f0dd"
+    sha256 cellar: :any,                 monterey:       "f441710caf103a53272a50e4720f1885f858986c6306fcc4a078ef8d193cca59"
+    sha256 cellar: :any,                 big_sur:        "a3bcb8d9c69277cb943fa30f00715325ee1c4f0c3e9aeb58e0c4a288f6705313"
+    sha256 cellar: :any,                 catalina:       "140477567aa37b0418e1976bdc5f75bc0f933405c157e7170ae3441ee3fc74aa"
+    sha256 cellar: :any,                 mojave:         "e10d69d8595116bdb562b0ede63d7bd04f606c66becd3c03fb1e6d06ba99a964"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fb045793a66f36a2737a3bdc3c8513c5822702778c223123dceceff61e847080"
   end
 
   resource "database" do
