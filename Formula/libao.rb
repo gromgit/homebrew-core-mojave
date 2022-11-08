b59055de18078a8591c72ce26d18be323f0e4048
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:18:27 +0800

    libao: no rebuilding required

--- Formula/libao.rb
+++ Formula/libao.rb
@@ -7,9 +7,15 @@ class Libao < Formula
   head "https://gitlab.xiph.org/xiph/libao.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libao"
-    rebuild 4
-    sha256 mojave: "6536700dc52a27b8494b9e4256a5b781f311ef69f024e7378227160bd91bb3ba"
+    rebuild 3
+    sha256 arm64_ventura:  "cca7befa22b09e8ecfb3746d8c6de3cd1a519cf76ab970c8e78acde4dd92cef3"
+    sha256 arm64_monterey: "87276ccd471ed61409cc6b1fc45b33e3b9ae60414695629cb519a2d7f03bb7e4"
+    sha256 arm64_big_sur:  "4ffbc11b951c7c833881d1a60d20d8969e30bfb85e817b660e38a3fc581ccb9c"
+    sha256 monterey:       "14bc27effce651df160ad5efbb377773479c6ea28b65f585760aa5316c3dc6ad"
+    sha256 big_sur:        "f27a782e33661e2aa75cbfcbe775a2da08f7f781c6e7608e8f1e3a4a354c4cde"
+    sha256 catalina:       "b6ccd4915aa272b58f267995ce3c87ad42388926535fedea0243c9b0b9941089"
+    sha256 mojave:         "cb57d05c66a19dcfac7e45e6a80f195dfd050ca52a9b316133d131c0c8165cf7"
+    sha256 x86_64_linux:   "bdb709d63e9de2e2dc947887fdc3a383b626d24c200cf80ce58eeaffa5ff7eb2"
   end
 
   depends_on "autoconf" => :build
