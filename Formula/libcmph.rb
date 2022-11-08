475404fadd7d0548aff92f76ad11c9a4119aa7d5
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:18:57 +0800

    libcmph: no rebuilding required

--- Formula/libcmph.rb
+++ Formula/libcmph.rb
@@ -5,6 +5,7 @@ class Libcmph < Formula
   sha256 "365f1e8056400d460f1ee7bfafdbf37d5ee6c78e8f4723bf4b3c081c89733f1e"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "dc5c4b140ee2e3ed459271e26f0fc47b9294626fbcad98a86d6326593a2ca764"
     sha256 cellar: :any,                 arm64_monterey: "85743179d6c3127e57f41d11b451f708a653b3b033e1b725b30fa0c1c6712b9e"
     sha256 cellar: :any,                 arm64_big_sur:  "30d22ddad3521ec07248910864e8caae7f8d959597663a9d21d2447c56e6639c"
     sha256 cellar: :any,                 monterey:       "248ea1c47707f4baf30f540f50803e1f3678ebaf5c80215ed4871f96cf77b314"
