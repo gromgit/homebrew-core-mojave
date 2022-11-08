083aabb9c3f5fda300fbc69044a35feec09f18a4
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:24:35 +0800

    lsdvd: no rebuilding required

--- Formula/lsdvd.rb
+++ Formula/lsdvd.rb
@@ -6,6 +6,7 @@ class Lsdvd < Formula
   revision 4
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "ba757936a28fde65072032578477812044621f4f8f73d9b9919c547ea902b402"
     sha256 cellar: :any,                 arm64_monterey: "0d71d460c8eaa5c01899c164df92f07d04648ca2807511be675cd08b161dbba5"
     sha256 cellar: :any,                 arm64_big_sur:  "d581725cf2628d8c123d3f6f54d1baf06cb33362d9f6dc7d033a4f7768729474"
     sha256 cellar: :any,                 monterey:       "5268aec2b5e5e3c89840661870ac95821c77aef6bfbe38447355402fae4b86f3"
