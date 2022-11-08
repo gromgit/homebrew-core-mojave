9ddc5ab6785c644a95d2772dce05eccf44804879
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:11:28 +0800

    glpk: no rebuilding required

--- Formula/glpk.rb
+++ Formula/glpk.rb
@@ -7,6 +7,7 @@ class Glpk < Formula
   license "GPL-3.0-or-later"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "2fb927d88ff2f1e242e8909a153449ddaf2e6264d28efe7cf11e6a1e84065ce4"
     sha256 cellar: :any,                 arm64_monterey: "c6f0f13896167f69d3dc9fed18ea36e19745006516abbff71d4dbf1a1a0c569b"
     sha256 cellar: :any,                 arm64_big_sur:  "e05ebe154868c3ae41e25c6d2bff72596275dc93c74a4f6f1a88c15a553a9bf2"
     sha256 cellar: :any,                 monterey:       "7eddbd4943eb50123bdfcd539d38932297e56ff61ae66dcb2ec633f60982f6c1"
