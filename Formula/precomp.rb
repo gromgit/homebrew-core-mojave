247a2735b02efcd8811f5c5bafbd826b3567f845
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:32:23 +0800

    precomp: no rebuilding required

--- Formula/precomp.rb
+++ Formula/precomp.rb
@@ -7,6 +7,7 @@ class Precomp < Formula
   head "https://github.com/schnaader/precomp-cpp.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "33c0801e82dd120a4300a93190e59d99da46d28acb0cef6f6946ec9a75cad1e0"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "5567c2d4a2a3e18046d75f916742f0d112c81c2c744e63aabfaa0f75bb3ab0a8"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "527a77954282f384fd4bd01ec67750d73ee5fc0d989cee4be22ba688a461a90f"
     sha256 cellar: :any_skip_relocation, monterey:       "d7b2be3194f675f7db87d9952635a1f0bcb36b2b4cffdc63078d1adf8683bc63"
