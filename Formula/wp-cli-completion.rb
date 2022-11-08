287672dfcf59571c2eb109d1c10a71789ea78f4d
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:42:50 +0800

    wp-cli-completion: no rebuilding required

--- Formula/wp-cli-completion.rb
+++ Formula/wp-cli-completion.rb
@@ -1,13 +1,13 @@
 class WpCliCompletion < Formula
   desc "Bash completion for Wpcli"
   homepage "https://github.com/wp-cli/wp-cli"
-  url "https://github.com/wp-cli/wp-cli/archive/v2.6.0.tar.gz"
-  sha256 "72b4b49331a5a0cba78c3460106a4dbc741cae313fd1654c52ef0113c7c15b4a"
+  url "https://github.com/wp-cli/wp-cli/archive/v2.7.1.tar.gz"
+  sha256 "0062dcbc62658dd2e4361e97d1ec12602f4a6661e771b730158e615aff3ea695"
   license "MIT"
   head "https://github.com/wp-cli/wp-cli.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "b6aab00ea52442b58440887860f7cbdb15fdb4f848e0cda66543f9832f1b2588"
+    sha256 cellar: :any_skip_relocation, all: "69e4f608a405f861788445d6c22e9975f2fbdf0b5659001d5ca96ea112036e91"
   end
 
   def install
