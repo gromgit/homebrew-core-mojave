1ee58f5cbba37e4e74dd6cd03a60481529cdde42
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:22:32 +0800

    libtcod:

--- Formula/libtcod.rb
+++ Formula/libtcod.rb
@@ -1,9 +1,19 @@
 class Libtcod < Formula
   desc "API for roguelike developers"
   homepage "https://github.com/libtcod/libtcod"
-  url "https://github.com/libtcod/libtcod/archive/1.22.3.tar.gz"
-  sha256 "a6f05f009db1f468338cf4cf984245a869ebb197b6d2524a8369c22ff9f1b9f5"
+  url "https://github.com/libtcod/libtcod/archive/1.23.0.tar.gz"
+  sha256 "4b13840769c7369d1c75728b555e26933f513da29c493f73017f0deb3da27e0d"
   license "BSD-3-Clause"
+
+  bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "8244bbb31f18bb7737372c9087bbe749232d41a147e5c84fe55cc236c5fc11bd"
+    sha256 cellar: :any,                 arm64_monterey: "4065cddda8f1448f7df9984ea73e8813102bdfc68f5bbdc5386256b7847ff777"
+    sha256 cellar: :any,                 arm64_big_sur:  "ab0a438306f817bd11535c4953acdd857f99cdb6fcb8060c207380eb3aba93ef"
+    sha256 cellar: :any,                 monterey:       "d43ebb17eafbd7c5b39167dd6b3008d96e7ee6f4738e8c93102ee40553a64782"
+    sha256 cellar: :any,                 big_sur:        "90e0aaf9dd7fbbd670889113d78e885a34e5e04ef389c66580e488a3f4d6768b"
+    sha256 cellar: :any,                 catalina:       "6a476ccabdeda32c5405d77e8f98837efa4beecfa34344ce02a259c93295f06a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8b83468c99c5c51043c413d8567843e6246a7e1db780ad0ca4c6185c44dd76cb"
+  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
