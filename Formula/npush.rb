95342006f492ba61eac1bf0567e7de8cc5d5da34
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:28:52 +0800

    npush: no rebuilding required

--- Formula/npush.rb
+++ Formula/npush.rb
@@ -7,6 +7,7 @@ class Npush < Formula
   head "https://svn.code.sf.net/p/npush/code/"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ee910410e365aa4d509068cb68d14bf871b9a679cfff678db620bbb6603838fc"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ae543262c4b86f3d0babffd21d015739bf5db0838fc7ee604df46cf8e26e3178"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b0be6b0d7949e3e6b3322089f84c10c60c15ea41a0a7ebdaa7ff04862c1be103"
     sha256 cellar: :any_skip_relocation, monterey:       "7f24fc8d2212c912748809e540f129ff5a873d00639c1f2d262d869714746a67"
