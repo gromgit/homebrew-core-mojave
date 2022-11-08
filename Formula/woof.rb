63d3801764c34ab0ed17fe7d1605eec8aa0aa229
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:42:49 +0800

    woof: no rebuilding required

--- Formula/woof.rb
+++ Formula/woof.rb
@@ -8,10 +8,11 @@ class Woof < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "0b65de07c5c918fb82b3c7a047c4c22c2771ef3897cd2f3c1abb64a76a7a867d"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "6f5ec883ad9fdb67965bc8a7a08318adcc1fa5a48e1da24f56cb85697bd28e0d"
   end
 
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   def install
     rewrite_shebang detected_python_shebang, "woof"
