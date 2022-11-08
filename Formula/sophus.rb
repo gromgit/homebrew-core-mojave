d2ff58f9303ec8f99b48010ffc04065c22cf62ad
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:37:07 +0800

    sophus: no rebuilding required

--- Formula/sophus.rb
+++ Formula/sophus.rb
@@ -4,22 +4,23 @@ class Sophus < Formula
   url "https://github.com/strasdat/Sophus/archive/refs/tags/v22.10.tar.gz"
   sha256 "270709b83696da179447cf743357e36a8b9bc8eed5ff4b9d66d33fe691010bad"
   license "MIT"
+  revision 1
   head "https://github.com/strasdat/Sophus.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "7e329dd9eaeba92ba5d0941be275218a16c93837b1c7f9c6bec4fbb5517ac044"
+    sha256 cellar: :any_skip_relocation, all: "5c03acef1a12656ea660610b57869d2df2ac38655fa4e783e2a959e5a65fa008"
   end
 
   depends_on "cmake" => [:build, :test]
   depends_on "ceres-solver"
   depends_on "eigen"
+  depends_on "fmt"
 
   fails_with gcc: "5" # C++17 (ceres-solver dependency)
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args,
-                    "-DBUILD_SOPHUS_EXAMPLES=OFF",
-                    "-DSOPHUS_USE_BASIC_LOGGING=ON"
+                    "-DBUILD_SOPHUS_EXAMPLES=OFF"
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
     (pkgshare/"examples").install "examples/HelloSO3.cpp"
@@ -30,7 +31,6 @@ class Sophus < Formula
     (testpath/"CMakeLists.txt").write <<~EOS
       cmake_minimum_required(VERSION 3.5)
       project(HelloSO3)
-      add_compile_definitions(SOPHUS_USE_BASIC_LOGGING)
       find_package(Sophus REQUIRED)
       add_executable(HelloSO3 HelloSO3.cpp)
       target_link_libraries(HelloSO3 Sophus::Sophus)
