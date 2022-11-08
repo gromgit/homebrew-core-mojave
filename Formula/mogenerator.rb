bc79db79a4175b76d3ad5b4f4edca08a986724c6
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:26:49 +0800

    mogenerator: no rebuilding required

--- Formula/mogenerator.rb
+++ Formula/mogenerator.rb
@@ -8,6 +8,7 @@ class Mogenerator < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8e3905d64fb52d4543d39ab15d24d091829d22c885e5a423db3ab64d0e9b625d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "29159fa7d208a108c0a36a222f7a300151241810eccfef04059a86611dfe41d4"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "51aec3a49207ae357af26a5407494bc88d98027ba06293736b2888ece7b1d71c"
     sha256 cellar: :any_skip_relocation, monterey:       "a9907203474f336c731912e28fad5ec2e912a1e7378d5ba527a7bb3d3b160134"
