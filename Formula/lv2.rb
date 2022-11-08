f9819753dd4ca1a9dca8a5a876f3f35220cc952f
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:24:49 +0800

    lv2: no rebuilding required

--- Formula/lv2.rb
+++ Formula/lv2.rb
@@ -4,9 +4,9 @@ class Lv2 < Formula
 
   desc "Portable plugin standard for audio systems"
   homepage "https://lv2plug.in/"
-  url "https://lv2plug.in/spec/lv2-1.18.8.tar.xz"
-  sha256 "b404cf14f776af40ca43808b45f4219dfa850a4f47aa33f89fa96ae719e174c8"
-  license "ISC"
+  url "https://lv2plug.in/spec/lv2-1.18.10.tar.xz"
+  sha256 "78c51bcf21b54e58bb6329accbb4dae03b2ed79b520f9a01e734bd9de530953f"
+  license any_of: ["0BSD", "ISC"]
   head "https://gitlab.com/lv2/lv2.git", branch: "master"
 
   livecheck do
@@ -15,62 +15,22 @@ class Lv2 < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/lv2"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "f7407c5d7cf6e550213315f137544d3107917f360430373d2cb73f79cfff6121"
+    sha256 cellar: :any_skip_relocation, all: "00e2c051efd2fbb588d34d1b96d1582252ccf0c3a254d4f994d81b6a7fe3fea8"
   end
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "python@3.10"
-  depends_on "six"
-
-  resource "isodate" do
-    url "https://files.pythonhosted.org/packages/db/7a/c0a56c7d56c7fa723988f122fa1f1ccf8c5c4ccc48efad0d214b49e5b1af/isodate-0.6.1.tar.gz"
-    sha256 "48c5881de7e8b0a0d648cb024c8062dc84e7b840ed81e864c7614fd3c127bde9"
-  end
-
-  resource "Markdown" do
-    url "https://files.pythonhosted.org/packages/85/7e/133e943e97a943d2f1d8bae0c5060f8ac50e6691754eb9dbe036b047a9bb/Markdown-3.4.1.tar.gz"
-    sha256 "3b809086bb6efad416156e00a0da66fe47618a5d6918dd688f53f40c8e4cfeff"
-  end
-
-  resource "Pygments" do
-    url "https://files.pythonhosted.org/packages/59/0f/eb10576eb73b5857bc22610cdfc59e424ced4004fe7132c8f2af2cc168d3/Pygments-2.12.0.tar.gz"
-    sha256 "5eb116118f9612ff1ee89ac96437bb6b49e8f04d8a13b514ba26f620208e26eb"
-  end
-
-  resource "rdflib" do
-    url "https://files.pythonhosted.org/packages/fc/8d/2d1c8a08471b4333657c98a3048642095f844f10cd1d4e28f9b08725c7bd/rdflib-6.2.0.tar.gz"
-    sha256 "62dc3c86d1712db0f55785baf8047f63731fa59b2682be03219cb89262065942"
-  end
 
   def install
-    # Python resources and virtualenv are for the lv2specgen.py script that is installed
-    venv = virtualenv_create(libexec, "python3.10")
-    venv.pip_install resources
-    rw_info = python_shebang_rewrite_info("#{libexec}/bin/python3")
-    rewrite_shebang rw_info, *Dir.glob("lv2specgen/*.py")
-
     system "meson", "build", *std_meson_args, "-Dplugins=disabled", "-Dlv2dir=#{lib}/lv2"
     system "meson", "compile", "-C", "build"
     system "meson", "install", "-C", "build"
 
-    (libexec/"bin").install bin/"lv2specgen.py"
-    (bin/"lv2specgen.py").write_env_script libexec/"bin/lv2specgen.py",
-                                           XDG_DATA_DIRS: "#{opt_share}${XDG_DATA_DIRS+:${XDG_DATA_DIRS}}"
     (pkgshare/"example").install "plugins/eg-amp.lv2/amp.c"
   end
 
   test do
-    output = shell_output("#{bin}/lv2specgen.py --help")
-
-    # lv2specgen.py will only display help text if it is able to load required Python modules
-    assert_match "Write HTML documentation for an RDF ontology.", output
-
-    # Pygments support in lv2specgen.py is optional, ensure that there were no errors in loading Pygments
-    refute_match "Error importing pygments, syntax highlighting disabled.", output
-
     # Try building a simple lv2 plugin
     dynamic_flag = OS.mac? ? "-dynamiclib" : "-shared"
     system ENV.cc, pkgshare/"example/amp.c", "-I#{include}",
