7b7cc3a4c74a88801840de5b60ab526f5c5aa2fa
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:36:34 +0800

    six: no rebuilding required

--- Formula/six.rb
+++ Formula/six.rb
@@ -4,15 +4,14 @@ class Six < Formula
   url "https://files.pythonhosted.org/packages/71/39/171f1c67cd00715f190ba0b100d606d440a28c93c7714febeca8b79af85e/six-1.16.0.tar.gz"
   sha256 "1e61c37477a1626458e36f7b1d82aa5c9b094fa4802892072e49de9c60c4c926"
   license "MIT"
-  revision 2
+  revision 3
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "560f73cafaea617d44f93beffdac91ac3b93095b1b64ff3877c5c4903f1cb001"
+    sha256 cellar: :any_skip_relocation, all: "0dee50367c6facbfc8f65e8a82bcd3e08d43da262b1adff6ccf943ef5bfaf313"
   end
 
   depends_on "python@3.10" => [:build, :test]
-  depends_on "python@3.8" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
   depends_on "python@3.9" => [:build, :test]
 
   def pythons
