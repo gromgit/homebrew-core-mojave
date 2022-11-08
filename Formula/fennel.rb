b3f0ff7dc2160b53980e9032170a731ed4f88c7f
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:08:27 +0800

    fennel: no rebuilding required

--- Formula/fennel.rb
+++ Formula/fennel.rb
@@ -1,13 +1,12 @@
 class Fennel < Formula
   desc "Lua Lisp Language"
   homepage "https://fennel-lang.org"
-  url "https://github.com/bakpakin/Fennel/archive/1.2.0.tar.gz"
-  sha256 "a0c9b1d23d9d9714afe93542c5314fad8e1771bf8b616d0decfeabe88318313e"
+  url "https://github.com/bakpakin/Fennel/archive/1.2.1.tar.gz"
+  sha256 "fae8a0b00275529acb3785673b9505f378786ccd2cd2bc16254f6a5fd08e0d19"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fennel"
-    sha256 cellar: :any_skip_relocation, mojave: "9f26ea712b05fce128ba65049e878a657f66ac2091dc62a01bcc82a82f557187"
+    sha256 cellar: :any_skip_relocation, all: "ae18b97efb611231723d835ade911486880d0341eb2cd731ba27516787babe88"
   end
 
   depends_on "lua"
