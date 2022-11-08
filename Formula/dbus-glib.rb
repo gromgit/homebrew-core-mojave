e812673126897a93e91b178d2aa6b5ac984accc7
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:05:07 +0800

    dbus-glib: no rebuilding required

--- Formula/dbus-glib.rb
+++ Formula/dbus-glib.rb
@@ -10,9 +10,14 @@ class DbusGlib < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dbus-glib"
-    rebuild 1
-    sha256 cellar: :any, mojave: "659819a4d896680171b59489ab0861a155a22fe21ea4de5bb1326eb306fc7025"
+    sha256 cellar: :any,                 arm64_ventura:  "c88874e0737f46cfc08bcc30c2b2d1feddeb95b19ef1725a1d006bef17999968"
+    sha256 cellar: :any,                 arm64_monterey: "24a79926eb518b6de78cacfc8d6893655bdbc6c62ad5b84c89131697fa97a568"
+    sha256 cellar: :any,                 arm64_big_sur:  "d87340d630e5e4e08fcaf5c557ad1577736069561e3903f763cb50396b417830"
+    sha256 cellar: :any,                 monterey:       "22c039588770bd957a8905eccdc7bb460b19ffaf193d3e9ae30332a48d653b7a"
+    sha256 cellar: :any,                 big_sur:        "b8b40647f906afbe0c3c35be698d77b02e22fe204cad83e1cda62842aba68ea2"
+    sha256 cellar: :any,                 catalina:       "5f964469ee5636271ebb40d0df69cd2e71dc292cf229b59be6b0a15543016373"
+    sha256 cellar: :any,                 mojave:         "ba4476464714dcde25a52ba1a3ae3a9646b9a293ab63ceb2f0ea5fc4192df97a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "40299090266efbefb057847af56cbccedf7c96ba98107c86fe4c7c0b8254cf0c"
   end
 
   depends_on "pkg-config" => :build
