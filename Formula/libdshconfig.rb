9c025f739a658d30706503f920c1884797ac4640
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:19:21 +0800

    libdshconfig: no rebuilding required

--- Formula/libdshconfig.rb
+++ Formula/libdshconfig.rb
@@ -12,6 +12,7 @@ class Libdshconfig < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "3717933f302d4d371720ea523b77f0fccc3886c9f64e58b085fa8129b70eb2c2"
     sha256 cellar: :any,                 arm64_monterey: "d8ed328326850b16d0398cac9f951616fda597d084ded2363350a998d7105bfd"
     sha256 cellar: :any,                 arm64_big_sur:  "7c8ce322c8a67038c0d2eea98640665aad9a4f17dced7d796ac55e271936918a"
     sha256 cellar: :any,                 monterey:       "80190054e7c4562cf68c8e13c09209a3667e06aca3af0e3b51d83adefc519d2d"
