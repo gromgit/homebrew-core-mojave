d0ac569946b352751670a8fb2cc983593bd13f2e
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:36:19 +0800

    shmcat: no rebuilding required

--- Formula/shmcat.rb
+++ Formula/shmcat.rb
@@ -11,6 +11,7 @@ class Shmcat < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2f501e817599e1da0c0d0edb51931113b58c43b98fca00fb207a8213fc1418d6"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "37e280843c7f422bacd40e5785236613d1a82e712405b6fa68910b6dee91946b"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3bdee0944414bc51a08a7707e29d16a6f08e1d583ad4cd8357587da2a6519d05"
     sha256 cellar: :any_skip_relocation, monterey:       "dc928ec60a7a9af5404195a4f82cc87d51a60f1292c1611b59401cc226fb39f1"
