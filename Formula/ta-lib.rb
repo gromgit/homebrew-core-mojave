5f41efefd742c63860260fa891fd21f8c6162602
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:38:37 +0800

    ta-lib: no rebuilding required

--- Formula/ta-lib.rb
+++ Formula/ta-lib.rb
@@ -6,6 +6,7 @@ class TaLib < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "8270f13fe940810e41e494cb30ee4dce662470751804681a806981d24da75ce4"
     sha256 cellar: :any,                 arm64_monterey: "42dab227aceff238ecc3b475ddffaa3d54e3da69a89c5aae08b4b4463ab2f0e9"
     sha256 cellar: :any,                 arm64_big_sur:  "c251da6ef6b483906eaa894dd86c19da3200acca00d6fab5f45522ff4b2681f1"
     sha256 cellar: :any,                 monterey:       "6c7ef4d661dfe335e942a2086ff237e13a7205a74bda91920bb265f9b5877d6a"
