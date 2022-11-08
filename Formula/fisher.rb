e72d3ceb2eba3b4afeb4803f7b7f54b70e3bef28
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:08:45 +0800

    fisher: no rebuilding required

--- Formula/fisher.rb
+++ Formula/fisher.rb
@@ -1,12 +1,12 @@
 class Fisher < Formula
   desc "Plugin manager for the Fish shell"
   homepage "https://github.com/jorgebucaran/fisher"
-  url "https://github.com/jorgebucaran/fisher/archive/4.4.2.tar.gz"
-  sha256 "619498141f0557ea7eeb4438a97c45748ea5d4c3645340b5464ebb4622af3f64"
+  url "https://github.com/jorgebucaran/fisher/archive/4.4.3.tar.gz"
+  sha256 "c49698fcc839554f453a7c557d860cd4ba0e9529f28f20f8134463af43aa3b6e"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "86f35339a83f2985b49de0effa29cb19a3fc19494f030f022b72ba9ba06eb9e6"
+    sha256 cellar: :any_skip_relocation, all: "3d3c0ad468e65f5370bd46a32b00dd12a8d3d73a4e95486a09d22c26aefecd4a"
   end
 
   depends_on "fish"
