75378c49f55c5c74ec45d6cdd1ddffded90e445c
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:23:45 +0800

    libxmp: no rebuilding required

--- Formula/libxmp.rb
+++ Formula/libxmp.rb
@@ -6,6 +6,7 @@ class Libxmp < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "36592fbb667e6bfb230bfde55bb81931aee10fb9bb86ed7dbcdbc8214a318df9"
     sha256 cellar: :any,                 arm64_monterey: "7f9283a77ff4c8715bd198177863b376a745a79be193da5adb1fb5704abd941d"
     sha256 cellar: :any,                 arm64_big_sur:  "434828daa308ed44a901c51be2e6b5a92e91e28267f77894aff8a5030c07d82c"
     sha256 cellar: :any,                 monterey:       "5401c879119fef7ce2c6101f96ab9977f53ee1fcb0d7015e75b9ed7564ec3442"
