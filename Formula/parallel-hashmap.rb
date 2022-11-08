157aeec48ef92fa2dea39c9be1232f1b9b595f61
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:30:40 +0800

    parallel-hashmap: no rebuilding required

--- Formula/parallel-hashmap.rb
+++ Formula/parallel-hashmap.rb
@@ -1,13 +1,13 @@
 class ParallelHashmap < Formula
   desc "Family of header-only, fast, memory-friendly C++ hashmap and btree containers"
   homepage "https://greg7mdp.github.io/parallel-hashmap/"
-  url "https://github.com/greg7mdp/parallel-hashmap/archive/1.36.tar.gz"
-  sha256 "33acf44158a9661a9d630d13f9250a2aa27a770cb3771df77b1ba1a661c0b766"
+  url "https://github.com/greg7mdp/parallel-hashmap/archive/1.37.tar.gz"
+  sha256 "2ac652be0552fcb53a1163c08c1f28f29f0756594fcc587eebb4d8b363153709"
   license "Apache-2.0"
   head "https://github.com/greg7mdp/parallel-hashmap.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "4880cd61e4cc74cbd16d6aa4d302f57e0904c7c6a177a1ba80261e87c5597cc3"
+    sha256 cellar: :any_skip_relocation, all: "d35958ae9cb30dbf15d9cca4b5dc4a6b3d50aced16db2cca21b96d1c2a550216"
   end
 
   depends_on "cmake" => :build
