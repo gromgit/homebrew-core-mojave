c4f969b0911644652de3156d4f0d70be7727ab42
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:14:31 +0800

    htmlcxx: no rebuilding required

--- Formula/htmlcxx.rb
+++ Formula/htmlcxx.rb
@@ -5,9 +5,16 @@ class Htmlcxx < Formula
   sha256 "5d38f938cf4df9a298a5346af27195fffabfef9f460fc2a02233cbcfa8fc75c8"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/htmlcxx"
-    rebuild 1
-    sha256 mojave: "552a5ce4a36d7f0aff8e6912fe485b7b4065c1c24e5e0268b56acb9d92f88b72"
+    sha256 arm64_ventura:  "a89f6f97c7cdaa4ad3734786c0a15c2bbbfd6ba703127c9239bff75e89ace62a"
+    sha256 arm64_monterey: "f0b1e84a587e9c944b7e6d1952b1feea22e223897a12adf51e28f51ec9bf4e9d"
+    sha256 arm64_big_sur:  "076a461f50d225b8f6d7b1d1541b0fcdb2fba0af77e28d2524815e5d912b623e"
+    sha256 monterey:       "2f62b3bfd180f22804cf7df3161f018769be76311e4d740cc619f3fb2766a1aa"
+    sha256 big_sur:        "5afe59e8f11f3ee3d04448c1e885b433cdcb356c6aaa80bc1e8ed0f6b0c0ec95"
+    sha256 catalina:       "8414d919ae850983832803af525e8b98d3e5aa106c47b05f420d77020c7c99ca"
+    sha256 mojave:         "e910595c43c028e25e0e0a44203e3c95b229162ea89678721b4a7f6e22974aca"
+    sha256 high_sierra:    "062a4b1629ab6f28e59ef0ea15c257c8bfd9e3646f3342fbfe14268727be7649"
+    sha256 sierra:         "4407cb1a50e8d629db9b93bdbbbf2a0892967611f7e579c49c0d084769f8a5ca"
+    sha256 x86_64_linux:   "ba29d98077036799d68c6c6dc56e0e7fa28aee700a89f4128a2d10a29d1ab39e"
   end
 
   # Fix -flat_namespace being used on Big Sur and later.
