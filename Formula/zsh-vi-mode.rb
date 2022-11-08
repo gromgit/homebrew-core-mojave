9de3619a00963938c65d9cb651b0343703f90234
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:44:29 +0800

    zsh-vi-mode: no rebuilding required

--- Formula/zsh-vi-mode.rb
+++ Formula/zsh-vi-mode.rb
@@ -1,13 +1,13 @@
 class ZshViMode < Formula
   desc "Better and friendly vi(vim) mode plugin for ZSH"
   homepage "https://github.com/jeffreytse/zsh-vi-mode"
-  url "https://github.com/jeffreytse/zsh-vi-mode/archive/refs/tags/v0.8.5.tar.gz"
-  sha256 "98ae59b83ee1886929d5c2af5e5b8a2512828cc312815bdfb34db74f2dc0476a"
+  url "https://github.com/jeffreytse/zsh-vi-mode/archive/refs/tags/v0.9.0.tar.gz"
+  sha256 "e6a5c56fd4b594035916057d3def799eeffcaadd2e5eda26299d724ad692693d"
   license "MIT"
   head "https://github.com/jeffreytse/zsh-vi-mode.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "555f8d09b4f7820e41c8ddda5b31ce0745342dd93536de1293523ce5c7b93aae"
+    sha256 cellar: :any_skip_relocation, all: "7b4099bf0e0a3915013d1b3494d2b84a89525408940a8fcb5f4f88b2e580654b"
   end
 
   uses_from_macos "zsh"
