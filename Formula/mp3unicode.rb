542235b8d4d1089dffdc3066f370a6bfa8ebd2b4
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:27:09 +0800

    mp3unicode: no rebuilding required

--- Formula/mp3unicode.rb
+++ Formula/mp3unicode.rb
@@ -12,6 +12,7 @@ class Mp3unicode < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "c9cab3295e2f0715c8427c98d17f8dc4384fe50c30cde51046623de82a32739c"
     sha256 cellar: :any,                 arm64_monterey: "e2f6b5eef63cc1163c65a34fe790a49d41293a35c5f693fd853f982b423141fb"
     sha256 cellar: :any,                 arm64_big_sur:  "a8e60d07b3170593185a88afd02c48bbef315ddcbae9cbfa0ef52541eea5348e"
     sha256 cellar: :any,                 monterey:       "01066562450ecbd024f9b326cdb22041a4e52205cef900bb6c8107f67253d422"
