ed69ad1101ef7429e5634df310d82fba4fdf85ab
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:07:49 +0800

    exact-image: no rebuilding required

--- Formula/exact-image.rb
+++ Formula/exact-image.rb
@@ -11,6 +11,7 @@ class ExactImage < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "20317cde1b2c2c9478ee7dd8b35a68f1d3b3360b36362af190a3323cb5429cd8"
     sha256 cellar: :any,                 arm64_monterey: "a11e0789738c798fc3cf6785353e023e4e27b0328aa7ccb5dd63c39b988a0691"
     sha256                               arm64_big_sur:  "47aa8c7861a759d66f553bc8960ba09c14a3b5acf86e2c0f22779379716cac5f"
     sha256 cellar: :any,                 monterey:       "39352ff44276b4d207a58999302b32fa3e6a1918a955c6c3e1d3235ee4654634"
