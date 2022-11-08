b52dc8ad9df0dc8997fcc5435ed956a1702b8293
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:26:39 +0800

    mlogger: no rebuilding required

--- Formula/mlogger.rb
+++ Formula/mlogger.rb
@@ -6,6 +6,7 @@ class Mlogger < Formula
   license "BSD-4-Clause-UC"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3aa5e9292806dcc8150dafdd703720141806be0b610198826e8d226baad776d8"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "a2446f830d72176c5dda94e2023f588fea40cb7f0d1fbcce88e07f7ba56cb414"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ae9dd6052251a2283883741ec0c1643cf70e62c2acbda9bec06971375b98eced"
     sha256 cellar: :any_skip_relocation, monterey:       "234b0797fe08d79cd45ee6e2587fafe3322657b70b386e3327f4dc7a103edb3f"
