7b104150e533387ba8d71b8fce41b83e724ebc8c
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:15:56 +0800

    ivy: no rebuilding required

--- Formula/ivy.rb
+++ Formula/ivy.rb
@@ -1,13 +1,13 @@
 class Ivy < Formula
   desc "Agile dependency manager"
   homepage "https://ant.apache.org/ivy/"
-  url "https://www.apache.org/dyn/closer.lua?path=ant/ivy/2.5.0/apache-ivy-2.5.0-bin.tar.gz"
-  mirror "https://archive.apache.org/dist/ant/ivy/2.5.0/apache-ivy-2.5.0-bin.tar.gz"
-  sha256 "3855a5769b5dbeafa9fb6a00f130467fd0f89da684a0b33a91e3dc5dae2715c7"
+  url "https://www.apache.org/dyn/closer.lua?path=ant/ivy/2.5.1/apache-ivy-2.5.1-bin.tar.gz"
+  mirror "https://archive.apache.org/dist/ant/ivy/2.5.1/apache-ivy-2.5.1-bin.tar.gz"
+  sha256 "ce9d3d5e37f6bc3c95a21efc94ae4ab73ec27b3e8d0d86515b44b562c4bb431e"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "72bde283303b30c63dbd90af1cd59ed471d5b33c1856e5347d560f38ae8c09f2"
+    sha256 cellar: :any_skip_relocation, all: "9d63a4197a21a9de85a3833f55a2114af1e7b283f274253a0d9bfc8ba829ec79"
   end
 
   depends_on "openjdk"
