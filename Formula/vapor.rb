74e5003573cca96a8165350f08d78b25b442073c
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:41:26 +0800

    vapor:

--- Formula/vapor.rb
+++ Formula/vapor.rb
@@ -4,14 +4,16 @@ class Vapor < Formula
   url "https://github.com/vapor/toolbox/archive/18.6.0.tar.gz"
   sha256 "fa41a5ef847fcb86ce9f2a9837adc9d80ba99839519d230115f2b00ff7a64156"
   license "MIT"
+  revision 1
   head "https://github.com/vapor/toolbox.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "04c7eeb2a71e7e4fc2da6e41aa9d04943373aa62cb9a96d4501a9e95a2690f07"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3bd0dd23b86527ee620a13e401925d0b18de813ac8eb76dbe050e3b3ae354ff3"
-    sha256 cellar: :any_skip_relocation, monterey:       "4287b44b44519eeb45ca9e0bfaa6c12e184ffc9b3cd2296a373d8ac6cf38337f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bcd5983c3d88508b0de84083f3d6865cc92e8413f5c9b63ba5a9ae5561a9444d"
-    sha256                               x86_64_linux:   "5bf987050ac211251bf1f4e965b8a29e29ff3084523010006ad0d9f308ea55b7"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fb51000f032c2cccc9173d690edf7f1cbfb547bc8dbba40161e2849ee50b035e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "299bc502a158f6254414065522c3bbcf452f2cf80a943a607651b53c7492cae8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4d0222d5620acd09dc93c48f0d0075e982f000ee73aea00048827f1d9c52caf8"
+    sha256 cellar: :any_skip_relocation, monterey:       "cdcf8e6879d872e58d28333a059e86dbd2d61ddbad066bcc73248473668a44a0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a7fb64d81ca7c5d495bed3d621c3a2d403b19eac0cdb0c826f73eb8c7c443975"
+    sha256                               x86_64_linux:   "c387415dafc0eba81409dfd847786ec3c56284308f7289c0280b4b9cc8cc6b22"
   end
 
   depends_on xcode: "11.4"
