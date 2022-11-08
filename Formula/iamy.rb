6eca25aae75229014473d066cff5e92621e2a5b4
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:14:59 +0800

    iamy: no rebuilding required

--- Formula/iamy.rb
+++ Formula/iamy.rb
@@ -7,9 +7,14 @@ class Iamy < Formula
   head "https://github.com/99designs/iamy.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/iamy"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "04998f70f30c1dca21545c625af58edb2e5f9c16b780ef8efec20ae4ba9a5af1"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0610f24a24be30691a67a4c3b187cf0a959876c50ad250d7bf5802eb0190e51d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b0283107c1a133b0f8e7295de2fc2970a4824a2638011c63eb37cc55c654f8f1"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9b81ec5512ba8332739f653b1c93a4b2118a1e9929329e0e6c4d2dd80c47d5a6"
+    sha256 cellar: :any_skip_relocation, monterey:       "df95bd8de163fb4fcecd92ba25fa559b75332c6fcb6a5aebb205ffbb3a4148dd"
+    sha256 cellar: :any_skip_relocation, big_sur:        "59dde9a556103175d876fd1fba134133ddd1b162daa491cdbf35bb58bfb4fc85"
+    sha256 cellar: :any_skip_relocation, catalina:       "54c8b998bcfe19443e99f609e34864a39e9d3b49cd5f935c78b9654727a81137"
+    sha256 cellar: :any_skip_relocation, mojave:         "1024d9cc234fb7e94ff17781c2f600ed6d286c5e7b6ab96b20e259e61a56a0ae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "dc26edb3bea1993f7650cce2cfd848318ba19cf3a155ae7838823b4f4c3c8041"
   end
 
   depends_on "go" => :build
