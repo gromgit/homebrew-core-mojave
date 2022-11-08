c6b6019a0e47055c12a49e92abad25471fd67c01
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:19:24 +0800

    libdvbpsi: no rebuilding required

--- Formula/libdvbpsi.rb
+++ Formula/libdvbpsi.rb
@@ -11,6 +11,7 @@ class Libdvbpsi < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "b10d05a1a32a329126bf588f77962518e48a459cb4354881757db39538df3e76"
     sha256 cellar: :any,                 arm64_monterey: "0f7e72f47f7677017fd3a003b0ef61d7d161fdaaf673adefd5a64fb3f83d8a40"
     sha256 cellar: :any,                 arm64_big_sur:  "a61aaac7ff201fdd38a929556c6a64a69993150891690f8ea9532e1b9c9c9ae3"
     sha256 cellar: :any,                 monterey:       "16bdd90a4f0734be90ab9e7c0a955913f07ee21dc41cf91666be43301661b1a4"
