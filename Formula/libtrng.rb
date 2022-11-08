549cceea24bf5e50d4e9abc725a0a3b131e4db70
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:22:49 +0800

    libtrng: no rebuilding required

--- Formula/libtrng.rb
+++ Formula/libtrng.rb
@@ -12,9 +12,14 @@ class Libtrng < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libtrng"
-    rebuild 1
-    sha256 cellar: :any, mojave: "26c60f66b30735d9f0985193437367e7babe43819e2fe8e6553daff1188e3c11"
+    sha256 cellar: :any,                 arm64_ventura:  "03696ee318bd740740dee2edd2c1fa36ec3fb09db5d3d0aeae51ccb1e1c92ec6"
+    sha256 cellar: :any,                 arm64_monterey: "13e5f4fedf353053c7a7a1362123bd6788a2d12a0a730a2422c4283c06cd6160"
+    sha256 cellar: :any,                 arm64_big_sur:  "9cfa0851919690b182b5cd227c71e77b7f07f5179ff5d06b52894fb98ca131df"
+    sha256 cellar: :any,                 monterey:       "3238a818540f3b1d4b0bc5329bb3e5447d137114e9de70dd0e28a8883b530fb7"
+    sha256 cellar: :any,                 big_sur:        "c97a7c825b5a6614dd771cef5f0aebdadb70f5b619e19aa446afff5072ec236d"
+    sha256 cellar: :any,                 catalina:       "044b708b751a88a22b95e4b75c47a8125fe017d6e69ea39c1177c9bc06c0de85"
+    sha256 cellar: :any,                 mojave:         "85e7a9b91ec9df836ce6127af7ca09deffd9052136c34ae1b0d3e310467eddc9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d0bdaf850b042ac22709529e655ac2f94816569096e66935ee039b47d8a41ac8"
   end
 
   depends_on "cmake" => :build
