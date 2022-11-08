66192d24ce98281c39fe0643d8a961ac5d81ca6f
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:03:44 +0800

    configen: no rebuilding required

--- Formula/configen.rb
+++ Formula/configen.rb
@@ -7,6 +7,7 @@ class Configen < Formula
   head "https://github.com/theappbusiness/ConfigGenerator.git", branch: "main"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1c460c8a01dbdd8cccb66749dea6543bb63430129ea4717ecc9a03e27b6674bd"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4a677709fb477a2e97d7791393044062418f5143928cf69d2d373e075c1209cc"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d7aa87ea082759093c1192bf7e0ee69c1146ef4626534731ff506a83ec682641"
     sha256 cellar: :any_skip_relocation, monterey:       "3fde28a899b11fd837c3d30b27b26485a1e33f7bf951fe469d7fe8ea6ec41e0b"
