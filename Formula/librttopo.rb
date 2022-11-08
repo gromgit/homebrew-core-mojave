ce6f246193f02e1a07c483b309494d440aa9d0b8
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:22:00 +0800

    librttopo: no rebuilding required

--- Formula/librttopo.rb
+++ Formula/librttopo.rb
@@ -12,9 +12,14 @@ class Librttopo < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/librttopo"
-    rebuild 1
-    sha256 cellar: :any, mojave: "4963baf51569d82a8a20f72ad8da061a19acea4e3142349773c6cf2aac377235"
+    sha256 cellar: :any,                 arm64_ventura:  "8e6b9b8c094d303209903b45c752ee33d4463751af18871da3f23cc960d779ce"
+    sha256 cellar: :any,                 arm64_monterey: "9eee7c1b207b85c576f61e416c507d7b58d657f4d9e314e26ff3a964066965a3"
+    sha256 cellar: :any,                 arm64_big_sur:  "531ac2ca2e4247da1ebdacac77cbd68faaf5ea5935608de9ff842ae21aa18ce0"
+    sha256 cellar: :any,                 monterey:       "2eb9b1d4133a764edab33bd122c2d7326b95f983edbbb4cea9bec3888be3885f"
+    sha256 cellar: :any,                 big_sur:        "59068843a454371abc25ad9421771eb2770febfaa00d41e1527476f4cbfdb05b"
+    sha256 cellar: :any,                 catalina:       "9512f32068f310fc02c082828e4ebac85a698ef69f370243aa00a5b873569319"
+    sha256 cellar: :any,                 mojave:         "d6bc9674875a3eeb44cec544f6cc9ac9ce6435f7fd951f446801a8aadcb1a323"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "7d6d9cbbb4846b4a3147ef8be9041c2d155e2b9c8c3b7b3720b71ec78b472667"
   end
 
   depends_on "autoconf" => :build
