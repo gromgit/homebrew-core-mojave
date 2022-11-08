7560de2793d3e919cacfcb16cc1504ac367d5ba9
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:11:40 +0800

    gnu-go: no rebuilding required

--- Formula/gnu-go.rb
+++ Formula/gnu-go.rb
@@ -8,6 +8,7 @@ class GnuGo < Formula
   head "https://git.savannah.gnu.org/git/gnugo.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "29d51049554c942b464d900b5bd264c2606af4775cec9c4c76cf1c170beefe91"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ed049166ae53dc93586ab3ac5b3a3b40209dc46c9acaec335683a6c6b2c104ea"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "93fd32bdebd46174fe9d1ff0572dea0859c0a9f29dad7d9e20097da446cea7ab"
     sha256 cellar: :any_skip_relocation, monterey:       "b9394ee3acacef9bcb2c8df6a9fdd7547c99af2c0fd81f1dc1faa9e1b35c94df"
