8ea564874a92ed5e8948028c1186e16f26ab5a27
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:31:49 +0800

    plantuml: no rebuilding required

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://downloads.sourceforge.net/project/plantuml/1.2022.8/plantuml.1.2022.8.jar"
-  sha256 "9e8472b15e9deef57f57a49db1548d777d7a9fd87c0c02f27c383af260a89e53"
+  url "https://github.com/plantuml/plantuml/releases/download/v1.2022.12/plantuml-1.2022.12.jar"
+  sha256 "9014a0fb014fae37584edf1f4271b5f947b60376ba9a95546eb97e82f3b97f2f"
   license "GPL-3.0-or-later"
   version_scheme 1
 
@@ -12,7 +12,7 @@ class Plantuml < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "125f4eb600ef42568d9ffcdf21ee66daaedb07011384e1a35cbc74729b714f94"
+    sha256 cellar: :any_skip_relocation, all: "c0acc646b2d2e7337f3e4e4a944f46aaff6495fc1f32c79d3c5ba4740bc4f772"
   end
 
   depends_on "graphviz"
@@ -20,7 +20,7 @@ class Plantuml < Formula
 
   def install
     jar = "plantuml.jar"
-    libexec.install "plantuml.#{version}.jar" => jar
+    libexec.install "plantuml-#{version}.jar" => jar
     (bin/"plantuml").write <<~EOS
       #!/bin/bash
       if [[ "$*" != *"-gui"* ]]; then
