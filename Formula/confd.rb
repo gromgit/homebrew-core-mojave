35bb8a9dc1560f949210e2518c1331d8806dad3f
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:03:43 +0800

    confd: no rebuilding required

--- Formula/confd.rb
+++ Formula/confd.rb
@@ -7,6 +7,7 @@ class Confd < Formula
   head "https://github.com/kelseyhightower/confd.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "99f4686a6b8780c84e9382061d6b1538aab608d6e33e7e208aef6ec39f1f4b79"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "346a9185c6665e85a0b7a810cfdabb1cd397d628a58a40935028a39d3dac6da0"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f2eb5cdb05b9f92b2472331857765f5b4922183d4cde23e61c44d7bb3d080dfe"
     sha256 cellar: :any_skip_relocation, monterey:       "ba0c1ddd10c298a6283a5d5bcad10304211a68b0d139e27f30c7ef34ae8d2aab"
