82c2a67991f7e95bf1bfc2683f7701dc66f6bb5f
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:03:30 +0800

    cogl: no rebuilding required

--- Formula/cogl.rb
+++ Formula/cogl.rb
@@ -7,6 +7,7 @@ class Cogl < Formula
   head "https://gitlab.gnome.org/GNOME/cogl.git", branch: "cogl-1.22"
 
   bottle do
+    sha256 arm64_ventura:  "320b79387543af4d34a6c5453a339c8206d1f4afdc74cd12aaad3d268234abf5"
     sha256 arm64_monterey: "619709d99ff34b8468b4036dad1a23a355e10672ccffc109cf093d57183c3ae8"
     sha256 arm64_big_sur:  "9a487a4bf7fbe5fdec29d902ba668fe20cbbc05e66864cb8d9c5fe564373e586"
     sha256 monterey:       "b1770215a0378766b70f4337b6ca2608c54fbe78568cff08363dea00dfbc1b23"
