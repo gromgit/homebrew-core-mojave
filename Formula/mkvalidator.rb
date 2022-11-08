9f1d543f23e662e98edd64da1fbaa002819864d8
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:26:34 +0800

    mkvalidator: no rebuilding required

--- Formula/mkvalidator.rb
+++ Formula/mkvalidator.rb
@@ -11,6 +11,7 @@ class Mkvalidator < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b92708846e9acdc1e65588e3404f835f91ef55c272d2d28e37893b519a30c156"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "2d72352ffb6de0c6ce0a925ca48e8e3276a99b4c15e503360a46dfc19f4b12dc"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "132951513f0022d9a2f8e0b0f81a5d76668b292873df6d6c67b81c4cd304ac31"
     sha256 cellar: :any_skip_relocation, monterey:       "3ecaed3130b7884171aac431fffd4b6be19ad322cc990a4659a26407ffb799fd"
