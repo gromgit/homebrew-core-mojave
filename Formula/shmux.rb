e6fcd116ef8f1b19e6581a3ec88ba90288af772b
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:36:20 +0800

    shmux: no rebuilding required

--- Formula/shmux.rb
+++ Formula/shmux.rb
@@ -6,6 +6,7 @@ class Shmux < Formula
   license "BSD-3-Clause"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b4629c31e6233a8e7d7c4caf6491c3e585f8b7eaa5964f426ceab66562cb4fb7"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e32cab0e2063d5b3d43f9e796e5b00b89f7ac2bae966efe236b1e69d8ae6e8bc"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "341ede51de6b3dbb5b0f6acc554a2dfc7c9543cb3800e06a992f8b40a58b3657"
     sha256 cellar: :any_skip_relocation, monterey:       "2ddc25900fb3603227d9ede578fffc8c96c414b8487f6a0d710ac0e39f6a52de"
