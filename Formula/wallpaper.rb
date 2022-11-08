b260173afbe22ee8805e17a4d4f13c00ba47ab52
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:42:16 +0800

    wallpaper:

--- Formula/wallpaper.rb
+++ Formula/wallpaper.rb
@@ -7,7 +7,11 @@ class Wallpaper < Formula
   head "https://github.com/sindresorhus/macos-wallpaper.git", branch: "main"
 
   bottle do
-    sha256 mojave: "f27baf8ae2f171b8f7236ee399bb9df7da423c4ef81b68d7e0ece78df850d204" # fake mojave
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "183473f47ee0c0756b878ea79b9b6af7c1a1ee99e15f2142429a04f599f5695d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f020cda2a5cafba2c001ae6d32cae25d0f63283c221fd3ce75dde1414e97a19a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c730b0f63bdf5395221ab3c45018e23e31b782a31e379691947f1b0199381ba4"
+    sha256 cellar: :any_skip_relocation, monterey:       "a4ca40d5df4a1983cc719122135a56d19f6861a216c347c984a2b7f89c4e4d4e"
+    sha256 cellar: :any_skip_relocation, big_sur:        "2f8fcb711d2e7a94dcef1894d6f23c754e950b1773f7a3da008cb5dd59696dfa"
   end
 
   depends_on xcode: ["13.0", :build]
