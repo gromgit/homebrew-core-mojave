873f9cd2722b6694a27ecb93f371003adc0c64f5
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:11:03 +0800

    gitbucket: no rebuilding required

--- Formula/gitbucket.rb
+++ Formula/gitbucket.rb
@@ -1,12 +1,12 @@
 class Gitbucket < Formula
   desc "Git platform powered by Scala offering"
   homepage "https://github.com/gitbucket/gitbucket"
-  url "https://github.com/gitbucket/gitbucket/releases/download/4.38.2/gitbucket.war"
-  sha256 "5fc84406cf9dd7fb4326eb4569e750777b3ce4bc1d62fcab28ed2032534a092d"
+  url "https://github.com/gitbucket/gitbucket/releases/download/4.38.4/gitbucket.war"
+  sha256 "d293f5e12c2f10592f950bac046880556973c6b903cd84aee2a55b68d82e8eea"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "98b97f4282dcec2c311c2ed9c35e3e43fdbf2340215b124ff36e59fa6372e6f9"
+    sha256 cellar: :any_skip_relocation, all: "c1a2c3f79bb3487ead081c03a36143233976532f6302d40b2b9c76c87a12ef75"
   end
 
   head do
