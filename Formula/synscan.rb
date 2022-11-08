9a403112e2b266e0ce184ad455b46fc424b283a2
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:38:30 +0800

    synscan: no rebuilding required

--- Formula/synscan.rb
+++ Formula/synscan.rb
@@ -12,6 +12,7 @@ class Synscan < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "452098c37c8f4161baa099c7bdf5681453505c53a6b0cb08d2d84f4691fda6a4"
     sha256 cellar: :any,                 arm64_monterey: "a396a4340087cff3494d296c0134cb4089b02b181e6757e01c2428685d12a516"
     sha256 cellar: :any,                 arm64_big_sur:  "86677760d68a0a9efc11560003b4291ff8510b55a03f76a06916c989ec1aa428"
     sha256 cellar: :any,                 monterey:       "69aeb3d3c862761b228189eeed429dc25a5786f24cebe108cdc1dead01e0aeaf"
