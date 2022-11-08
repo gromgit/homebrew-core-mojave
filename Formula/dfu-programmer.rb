c0557ba37ed3fa7e04b03c4ab82a11df0c1c3ef3
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:05:32 +0800

    dfu-programmer: no rebuilding required

--- Formula/dfu-programmer.rb
+++ Formula/dfu-programmer.rb
@@ -11,6 +11,7 @@ class DfuProgrammer < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "9f41045987af7f7daa4470dcb37de69a210e4ccb6bc5f8e9b4d5d2bf310ec562"
     sha256 cellar: :any,                 arm64_monterey: "9559a215bd458ba59d0b037792b7d63dbc3dc4c099b14357e8e53acb5d3af922"
     sha256 cellar: :any,                 arm64_big_sur:  "8bfdfd329dcd8f8590c02bd7ba062f21def25d6009dc7a546956406d921a9181"
     sha256 cellar: :any,                 monterey:       "275aa114a5df563a91342de289d313bf2fdf8a3d85ce77417818ec8fe8fea05a"
