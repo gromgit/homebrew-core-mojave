3d9e239b152f286d6363df7f79df40a60d6dbc4a
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:32:50 +0800

    publish:

--- Formula/publish.rb
+++ Formula/publish.rb
@@ -4,12 +4,13 @@ class Publish < Formula
   url "https://github.com/JohnSundell/Publish/archive/0.9.0.tar.gz"
   sha256 "e098a48e8763d3aef9abd1a673b8b28b4b35f8dbad15218125e18461104874ca"
   license "MIT"
+  revision 1
   head "https://github.com/JohnSundell/Publish.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "867ee726868a7549be1a54c8c9219381b20503473d815843234e79172c2d8721"
-    sha256 cellar: :any_skip_relocation, monterey:       "596dded5de263f2e792564d00b7ad6df19100e3679a3a45638f3778e0972a5df"
-    sha256                               x86_64_linux:   "2c34e93726662d9416d7635aa995303cd0bd0cafb56c687b7b0c9ba9c7678a95"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "edc955e9e2cece8065b4683a4fdad37a905fbfcfd84a2de5d4ee3ba95362bb8a"
+    sha256 cellar: :any_skip_relocation, monterey:       "1fea104781c15f8799fe321f8ab6fd0a3a0c120e24250af828282b52923500ca"
+    sha256                               x86_64_linux:   "b59fbd87fcbee58d181d557dc3214408d476673d3d40c1b67b518c2ca414bd80"
   end
 
   # https://github.com/JohnSundell/Publish#system-requirements
