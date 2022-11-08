c5fff46bf367f0392dfa305bf994db4f65e2fa95
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:38:24 +0800

    swiftdraw:

--- Formula/swiftdraw.rb
+++ Formula/swiftdraw.rb
@@ -4,14 +4,15 @@ class Swiftdraw < Formula
   url "https://github.com/swhitty/SwiftDraw/archive/0.13.2.tar.gz"
   sha256 "f9a13610b38b7eb78112769fa94cd79466b10a532261c383adacbe438c20e83f"
   license "Zlib"
+  revision 1
   head "https://github.com/swhitty/SwiftDraw.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2e5ac2bf4d141aa0dd609bc29436069ec9d0a22a98bdc3e56a08149a08b65b1c"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7d8cceb21a6bdaf7c21a4c0c0e78f33b8b33cd99834dc0d76cebe5550bdf6101"
-    sha256 cellar: :any_skip_relocation, monterey:       "12a9af019792cc0f7557a16946b957a34dc665e4aa680b903f5381e95e8ec893"
-    sha256 cellar: :any_skip_relocation, big_sur:        "6a8566ff06f352f8d85255de7c51597f4dee87ec14df2352922d883347a6296e"
-    sha256                               x86_64_linux:   "900838c63cb35b53136fded97689df67b79c99e79bd61f31a937d052b804d151"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "9f95d453508270539da6af86ea47f2bc6493aee2f776cb8c2a2ab2a2d0d9e0af"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8546420c4b714d33ed403874e528d3d3c7bf71f28ee975a7cbeee52a8f7adc74"
+    sha256 cellar: :any_skip_relocation, monterey:       "90afabb16780e38bfcd45af849ce109561da56afb444b8f021fa2bc23604fa7c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "ffd0df423c90c1f0768df5234869e0da05486d6397d51bd46ee49a06557126a3"
+    sha256                               x86_64_linux:   "c2624b9959c228a26ff122663131393048001e4f45f8f46e42047bdb5b22d825"
   end
 
   depends_on xcode: ["12.5", :build]
