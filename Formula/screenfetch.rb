abf8b5de64e79a492b9009ab044646beff256bdb
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:35:44 +0800

    screenfetch: no rebuilding required

--- Formula/screenfetch.rb
+++ Formula/screenfetch.rb
@@ -7,6 +7,7 @@ class Screenfetch < Formula
   head "https://github.com/KittyKatt/screenFetch.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0d88f370f36c942227b1beb20c7fd27934beb6f8edcc3b8c1e713c1bf0910672"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "7a8a196b1d61f4515270b2009a878dd0ca86052333ef1fea478f085c53b9b042"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6f7e61ea4717eef72e68b006bcef5d6ff1aab08f7ba25f0a5c6b8e014ffb530b"
     sha256 cellar: :any_skip_relocation, monterey:       "64f48b67bd19de55e86169870401c92fc53229b0dfb72d7fb82a266569cf6b1e"
