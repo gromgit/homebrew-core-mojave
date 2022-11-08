a5ea4423957e54b593feaa099d327c49220a0317
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:18:43 +0800

    libbs2b: no rebuilding required

--- Formula/libbs2b.rb
+++ Formula/libbs2b.rb
@@ -7,6 +7,7 @@ class Libbs2b < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "550fc0d9af3d05be26435c1e7393a2d523775d0e2ed9af1cbe4791ed324ea9ca"
     sha256 cellar: :any,                 arm64_monterey: "be6c28752ed45916a1e7aec26599d0b08248d5b5409df335f1df1287956fcbd6"
     sha256 cellar: :any,                 arm64_big_sur:  "348394113062af5f31f9dfd6617f8d248b9bdeba11d28711d83e3e5d9326437f"
     sha256 cellar: :any,                 monterey:       "f4089cd8eb97c9f9f446b3402aa11a2cf920cfe95dc818534c9444b6fd2bba4a"
