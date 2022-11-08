5e9ec72ac57c604423c8e8a9bc48eafcd34ea93b
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-08 14:00:02 +0800

    batik: no rebuilding required

--- Formula/batik.rb
+++ Formula/batik.rb
@@ -1,13 +1,13 @@
 class Batik < Formula
   desc "Java-based toolkit for SVG images"
   homepage "https://xmlgraphics.apache.org/batik/"
-  url "https://www.apache.org/dyn/closer.lua?path=xmlgraphics/batik/binaries/batik-bin-1.15.tar.gz"
-  mirror "https://archive.apache.org/dist/xmlgraphics/batik/binaries/batik-bin-1.15.tar.gz"
-  sha256 "358a3bfbc0e2914983b22a0cd50d585b5b372b0b107939f020a0ebf911f1c72a"
+  url "https://www.apache.org/dyn/closer.lua?path=xmlgraphics/batik/binaries/batik-bin-1.16.tar.gz"
+  mirror "https://archive.apache.org/dist/xmlgraphics/batik/binaries/batik-bin-1.16.tar.gz"
+  sha256 "6386c9e97e3ab0ac75f9f98d90e4b6454ee09fb9f47ca0e790e60cab44bc7d83"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "c762e0eca05fff9911371ebd21075b1fcc8edd03840b7329a3ca359db88b2d4b"
+    sha256 cellar: :any_skip_relocation, all: "ca18391889d2f3ff94fce3367415612d85537d095f8c514dff8e29e2c6b6de7d"
   end
 
   depends_on "openjdk"
