02c930159ea9064c8899c4cf010d9947f57ef9d3
Adrian Ho <the.gromgit@gmail.com>
2022-11-10 21:15:54 +0800

    swig: update 4.1.0 bottle.

--- Formula/swig.rb
+++ Formula/swig.rb
@@ -1,19 +1,13 @@
 class Swig < Formula
   desc "Generate scripting interfaces to C/C++ code"
   homepage "http://www.swig.org/"
-  url "https://downloads.sourceforge.net/project/swig/swig/swig-4.0.2/swig-4.0.2.tar.gz"
-  sha256 "d53be9730d8d58a16bf0cbd1f8ac0c0c3e1090573168bfa151b01eb47fa906fc"
+  url "https://downloads.sourceforge.net/project/swig/swig/swig-4.1.0/swig-4.1.0.tar.gz"
+  sha256 "d6a9a8094e78f7cfb6f80a73cc271e1fe388c8638ed22668622c2c646df5bb3d"
   license "GPL-3.0-or-later"
 
   bottle do
-    sha256 arm64_monterey: "9e60e978ae83f0afd63aa8a27d3e418eda0cb75b89c0d22e25a0ccc5f2e00b3e"
-    sha256 arm64_big_sur:  "918c070202e0138b64b2e27f262aae3a72ab9f273f14842802d1fbe9169e66fc"
-    sha256 monterey:       "170414d38ab55ef50cb1275fbb0a9e0f0248ad1e23755af8ac2184a6825d514e"
-    sha256 big_sur:        "f198353656b61cb35b5c28ed6c9cb10689d2a0fc69529cfbcbc0fbd75a027e27"
-    sha256 catalina:       "530e80b7e7dcd28469b52fc3b668683a97b72642ebf2b6d4e6708d14f05e7286"
-    sha256 mojave:         "50afb5930cb37af2e400f0369f6da15b1d4922c1f72f45d13e7e3f8bd9d6d27b"
-    sha256 high_sierra:    "8bab440005b048ce454a3dd50ba608e1f85391edd73e9e40510269e923cad238"
-    sha256 x86_64_linux:   "2a5fe8e40d8ba5e09fd194e9e36f8fe4bd7a427e31d1456a99bb45539074c92a"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/swig"
+    sha256 mojave: "acdf0e463045ea4d95e7330b44d9ab2551b7de655f008cb0bd2ea6253d0380bd"
   end
 
   head do
@@ -23,7 +17,7 @@ class Swig < Formula
     depends_on "automake" => :build
   end
 
-  depends_on "pcre"
+  depends_on "pcre2"
 
   uses_from_macos "ruby" => :test
 
