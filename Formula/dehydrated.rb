efbf513b7949d922ed34d10a83fa56778ccbbf9f
Adrian Ho <the.gromgit@gmail.com>
2022-11-14 00:34:02 +0800

    dehydrated: update 0.7.1 bottle.

--- Formula/dehydrated.rb
+++ Formula/dehydrated.rb
@@ -1,18 +1,13 @@
 class Dehydrated < Formula
   desc "LetsEncrypt/acme client implemented as a shell-script"
   homepage "https://dehydrated.io"
-  url "https://github.com/dehydrated-io/dehydrated/archive/v0.7.0.tar.gz"
-  sha256 "1c5f12c2e57e64b1762803f82f0f7e767a72e65a6ce68e4d1ec197e61b9dc4f9"
+  url "https://github.com/dehydrated-io/dehydrated/archive/v0.7.1.tar.gz"
+  sha256 "3d993237af5abd4ee83100458867454ed119e41fac72b3d2bce9efc60d4dff32"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e4f179282544d70d072f6ebea22527d7dfbb8a0d810d5965fc7266918fef4f6d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e4f179282544d70d072f6ebea22527d7dfbb8a0d810d5965fc7266918fef4f6d"
-    sha256 cellar: :any_skip_relocation, monterey:       "fb2330cd8c498ee40af3981951e324ef819b632b6c569c30f6ec6b5ae5c4ecd4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "fb2330cd8c498ee40af3981951e324ef819b632b6c569c30f6ec6b5ae5c4ecd4"
-    sha256 cellar: :any_skip_relocation, catalina:       "fb2330cd8c498ee40af3981951e324ef819b632b6c569c30f6ec6b5ae5c4ecd4"
-    sha256 cellar: :any_skip_relocation, mojave:         "fb2330cd8c498ee40af3981951e324ef819b632b6c569c30f6ec6b5ae5c4ecd4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e4f179282544d70d072f6ebea22527d7dfbb8a0d810d5965fc7266918fef4f6d"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dehydrated"
+    sha256 cellar: :any_skip_relocation, mojave: "ebef64bfbbb961baab69a546da164867b6d3d0acc2090b6f0a8d9d1b2e6ac26b"
   end
 
   def install
