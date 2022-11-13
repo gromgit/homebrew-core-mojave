0f70e3351be26a48924ae51145d6203940a17e63
Adrian Ho <the.gromgit@gmail.com>
2022-11-14 00:56:41 +0800

    dtc: update 1.6.1 bottle.

--- Formula/dtc.rb
+++ Formula/dtc.rb
@@ -11,22 +11,18 @@ class Dtc < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "02031d0f89783e390f8618489d5f27ed62324afc53a920288ba66eb0da8b2056"
-    sha256 cellar: :any,                 arm64_big_sur:  "3acf8dbcf556c2271a6162ee0d7e6aa4b1e2f2ffb8dd88541d44ed3a96eecdd5"
-    sha256 cellar: :any,                 monterey:       "139c9cea5c83b9927a0a53898dc357a3b1f438a0c92539adc0c02c701d99613b"
-    sha256 cellar: :any,                 big_sur:        "eed133a2432f709556facdfb154a34ce5d2bddd6b04c67de73421bee3eb35160"
-    sha256 cellar: :any,                 catalina:       "d1910b920613b43b1be2a70ad950139fcd5e908ab45bc9c68dbaba078c676599"
-    sha256 cellar: :any,                 mojave:         "98c38049acdae747c9939bbe271f4318e35d976687caaf6beea0527592e4274d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "20225ca2fdcdf29d6fff97dddf7a26d39617bf4c6dc6adb3dc1da31f86de47fb"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dtc"
+    rebuild 1
+    sha256 cellar: :any, mojave: "e8b22d3afd2636f31f9b6fc6948dd46978c5115b362e71ff3d0516db6966d92e"
   end
 
   depends_on "pkg-config" => :build
 
-  uses_from_macos "bison"
-  uses_from_macos "flex"
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
 
   def install
-    inreplace "libfdt/Makefile.libfdt", "libfdt.$(SHAREDLIB_EXT).1", "libfdt.1.$(SHAREDLIB_EXT)"
+    inreplace "libfdt/Makefile.libfdt", "libfdt.$(SHAREDLIB_EXT).1", "libfdt.1.$(SHAREDLIB_EXT)" if OS.mac?
     system "make", "NO_PYTHON=1"
     system "make", "NO_PYTHON=1", "DESTDIR=#{prefix}", "PREFIX=", "install"
   end
