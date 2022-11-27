ee4529e4bf8cbe6b703b84aaa983005bfeb07867
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:01:28 +0800

    swift-format:

--- Formula/swift-format.rb
+++ Formula/swift-format.rb
@@ -16,6 +16,7 @@ class SwiftFormat < Formula
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f59ef3e47e5f5dad726c372b6be145f302971104aee3ba3256587ee2f7d77337"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "355e227631c799b2c73f0f9815bf9a05e9404d0a47161d90a0d984d90c49ac97"
+    sha256 cellar: :any_skip_relocation, ventura:        "eeea265a826a6651363e2970052038a0b47e11910c112a517efd2a37ec351049"
     sha256 cellar: :any_skip_relocation, monterey:       "6c087279fc6cb17d916e16457268ea617360079443fa11aa8a3d521fecd160bb"
     sha256                               x86_64_linux:   "d68e4d01e9be0aeeab34f2159bd492fbc26620d323b93613cf8a7f223343ad47"
   end
