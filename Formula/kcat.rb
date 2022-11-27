2a002223ffa2f268e419daf647a10c0479f9014c
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:36:32 +0800

    kcat: no rebuilding required

--- Formula/kcat.rb
+++ Formula/kcat.rb
@@ -19,6 +19,7 @@ class Kcat < Formula
     sha256 cellar: :any,                 arm64_ventura:  "91a40bc28ce360a9fa3d40f9a16917cc91e553f13b8cc6bcecbff98827c9d15d"
     sha256 cellar: :any,                 arm64_monterey: "fd220a7e002772622e581f636e59c4a198ec883cbb813d2b31857d0bf24d089d"
     sha256 cellar: :any,                 arm64_big_sur:  "f930080248bb0eff245599536bbc12465c6bf6e256acb283e6d2d5a5d047f11e"
+    sha256 cellar: :any,                 ventura:        "7f2f0da947ecd7fd93bea3a0b53d4e2f0e2d51e5bd9f2b93b870c20c22f88e26"
     sha256 cellar: :any,                 monterey:       "5629c17a2d26e8e36c1e7aa8e54430e004e2d7ef080df3ce6ff6edee4a4eb0e4"
     sha256 cellar: :any,                 big_sur:        "c6d947fa5cbdd948ab09082f1c961d21e5a6e565c36cb6ffffdc9712456cafad"
     sha256 cellar: :any,                 catalina:       "7125450d67cedf6116bc7e2afb22be2b860a715dc1de6663e4e75936d7eb466e"
