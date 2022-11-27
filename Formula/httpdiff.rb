816cb180ca278c129553f28316cfd168b9730090
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:33:41 +0800

    httpdiff: no rebuilding required

--- Formula/httpdiff.rb
+++ Formula/httpdiff.rb
@@ -10,6 +10,7 @@ class Httpdiff < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f7fcb1b3bdfb03fa3238b6e837c99668fcdc340070519d1a8e2144171be14b33"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e8e0883f20c870f02e78385ba604f33cca56c29e41037c1b42c98b9e231a1845"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "290b6c6f1c990249cc760ee510f0669dd9e317ac169be71cd5fb945a8625413e"
+    sha256 cellar: :any_skip_relocation, ventura:        "3dcf894a63441e77ae53928406016eb7b4c061588156aea9a6b5b28fb825b2a0"
     sha256 cellar: :any_skip_relocation, monterey:       "868da1b5aed20834315043ee38f653e34166b799fdf4ee90aa5967de099d8c45"
     sha256 cellar: :any_skip_relocation, big_sur:        "bd0aa59b471dc747b3af50d5c1f4611ed2c4993eebe64ffb4d343d1d7bef0fbb"
     sha256 cellar: :any_skip_relocation, catalina:       "5731d30f22cf63bd619c18f0f91c4547c52f2ae1b38a2cfeb0316958e93995c1"
