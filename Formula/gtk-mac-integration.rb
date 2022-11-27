634064d7b309507074a5fae03a1348853cc5c55f
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:31:53 +0800

    gtk-mac-integration: no rebuilding required

--- Formula/gtk-mac-integration.rb
+++ Formula/gtk-mac-integration.rb
@@ -16,6 +16,7 @@ class GtkMacIntegration < Formula
     sha256 arm64_ventura:  "41bcc5495f77b99f80a3cbe97fb744b43e5141249d226c6d3da222f7f1eb3e25"
     sha256 arm64_monterey: "dab3cf5758414f811f6921ba8cd60df24fdd3c93df1c4b3d582e57a0fe27aeb7"
     sha256 arm64_big_sur:  "f3ab908832ae236c157fcb29c6178a7d0ce0c521990be382270fcb0233351774"
+    sha256 ventura:        "c45af5d6a0da2f60200fcab09074b65159d1ab5f73abbddab698bbb1b3c6e04b"
     sha256 monterey:       "fe06a5ac783e080a93d5b99c11ac607f9b35a607aa670aaf7afd0918270c093c"
     sha256 big_sur:        "11cd268c22f0c1e52774fbc4368d953915cf58d0a212719e01dd721f17c89162"
     sha256 catalina:       "0c2b66f0715a364905ae8d61e1edd06dad96efc0ad72efa90dbdd756397468e0"
