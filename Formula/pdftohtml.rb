fc0635e9d40e1811c836b0bec0407e300f9362e0
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:51:46 +0800

    pdftohtml: no rebuilding required

--- Formula/pdftohtml.rb
+++ Formula/pdftohtml.rb
@@ -12,6 +12,7 @@ class Pdftohtml < Formula
 
   bottle do
     rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a18be87eedeec7c2bb94b6571e532ac3e04abf21567991c177d53f6740c71767"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "0ddb14280f6046adac64bd2d731483b8bf97cd9f8bd61a52f1f7b95b608febe2"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "61d5a75c1351339a1e1cee32c585172bc638503e4fea57232532daae41bbb2e6"
     sha256 cellar: :any_skip_relocation, monterey:       "4c5979a8c315cdbe2babf1a576e86259956f129d67b416fe62dd9a5b51bc9c64"
