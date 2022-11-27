c838b697df83b368feab0ba6870e691938bbac60
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:35:36 +0800

    jhead: no rebuilding required

--- Formula/jhead.rb
+++ Formula/jhead.rb
@@ -9,6 +9,7 @@ class Jhead < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "47c50f4a450eba0ecfa6bdcd466d192724697b4a5885181ae98283dd4c79e4c5"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e473e54dcc077f4b803d1fa3acf390d8d74845f9c01e8316dbccf195844b5738"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8ba17f494afc0590a876112f9f7f6b00664fbd1ad62ec3a9a21a5941188b9bbe"
+    sha256 cellar: :any_skip_relocation, ventura:        "5be3868791c615b094607a955575219af8a7286a04e24b4abef54bb99c1bb44d"
     sha256 cellar: :any_skip_relocation, monterey:       "cd587b58853b3f1adbdb7bf1cf7bf019d7f86354ffa4ba3de04ec60220858d6f"
     sha256 cellar: :any_skip_relocation, big_sur:        "ff1612a2a1d386e153b934b790f6e3e4897a0cb9509805de91f0cf432a422f57"
     sha256 cellar: :any_skip_relocation, catalina:       "57866edae4ac5a6b63988d3f7c9c1d261fa33eaff6dc1e6833a086fda2a7671f"
