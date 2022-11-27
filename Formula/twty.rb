59ff7d11010f9256be4c70fe3bc2c2e190afd0d9
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:04:36 +0800

    twty: no rebuilding required

--- Formula/twty.rb
+++ Formula/twty.rb
@@ -6,9 +6,14 @@ class Twty < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/twty"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "1c684da854c8d6f826c19cb695d1ddc4a0fae9a201897294983bb70969ba6451"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f646e71ba538e0406565dde123ecea7cc153510e53abd19373d1bd3ec159173e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c6bab324fcbfdfd720834fae87499bd2725318394393f63f277c0212d5a56ce4"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cfda1fe3c8de3bec08877dfc0d1ed0c33c9f88094fb2514e23b724305ec15ee4"
+    sha256 cellar: :any_skip_relocation, ventura:        "5805a300e13f68dcbe7b998bab1c07bee79ad2d62e666a25c67e4ac73759b78f"
+    sha256 cellar: :any_skip_relocation, monterey:       "de800aefbbc7f4299b2b8db41be41f8270c8bfda3b926e1e6fafa1d67c4bdcf1"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a77b1519634272bbb35b4f5fa588cc2cda46fc8aacf6414205291900e18a1ece"
+    sha256 cellar: :any_skip_relocation, catalina:       "65a106670027565383dee061957cd83963dc4df76bd7725b4d4ae40b21bfdc55"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "fb45326df2a5cb80531378ae156fc20400346d5c5c8d82548429b62463ec603e"
   end
 
   depends_on "go" => :build
