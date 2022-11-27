eb17bb9942273ab441805b34f5f25c8e6b458b08
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:21:20 +0800

    daemontools: no rebuilding required

--- Formula/daemontools.rb
+++ Formula/daemontools.rb
@@ -15,6 +15,7 @@ class Daemontools < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "e3e38b536653bd1b7e2e8d0e98dca15f9085205d3eeed09d8d1787afe468a08b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f66a2fccba83ee8fe623c06d8b53dfb241cdeffd4af6ccdd8ac499c5b3191a2a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4970abde6563bd8fa9cae9478b81d241ce0ad0c4d1504aa84269c55ccb45a499"
+    sha256 cellar: :any_skip_relocation, ventura:        "73a2cd834d4b1ef90c48199eaaaba09f077d153252b1d383a4a619a211b1eab7"
     sha256 cellar: :any_skip_relocation, monterey:       "23c7f34339a55c30a0e32c45b200030e766eeae0fc7cc873de70bae175849123"
     sha256 cellar: :any_skip_relocation, big_sur:        "2de015542410e14eb8e17bb9affc37f19fc81e7005e4bec60ecd64c13629b02a"
     sha256 cellar: :any_skip_relocation, catalina:       "0a39db96c9e2926beea8224ca844264d4ddec3b6561d5dfc019f3ecfd7cc86fe"
