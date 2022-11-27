1210caf8e866f65018cae2c4a546f3c186a49737
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:38:48 +0800

    libdnet: no rebuilding required

--- Formula/libdnet.rb
+++ Formula/libdnet.rb
@@ -14,6 +14,7 @@ class Libdnet < Formula
     sha256 cellar: :any,                 arm64_ventura:  "229deed00d9f2282c65e7ededd4723a9cef9a258b0921110ffc21b45745a01d9"
     sha256 cellar: :any,                 arm64_monterey: "589d70b5dcf0377cca4636db9b3141f5b9032a1d65bb689ad0cab8a5af24dd85"
     sha256 cellar: :any,                 arm64_big_sur:  "54a55ad8719e269c07cbef8a5c61924e2431a5a5c88cce4148dbeffede79a353"
+    sha256 cellar: :any,                 ventura:        "6af250681f01f3f785d6bf811e0b754e45d4592df7fccecfcf74600c79ac769c"
     sha256 cellar: :any,                 monterey:       "e5672d9ee85274aea69f2cb3a180ba097e652ddedb5ccae9e67cce5faa46ed1a"
     sha256 cellar: :any,                 big_sur:        "8e5f269aa55ecbf1a5f6855ef355696159b6155ea338cfb1e3b3b4ae8409bb72"
     sha256 cellar: :any,                 catalina:       "f8c9ace5eb112c484f50da7624df13c551b14114ece91a155ce2394b30e264b7"
