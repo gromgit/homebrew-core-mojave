ca6febf8d53272d361b6767fe5403f016779f549
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:51:43 +0800

    pdfpc: no rebuilding required

--- Formula/pdfpc.rb
+++ Formula/pdfpc.rb
@@ -10,6 +10,7 @@ class Pdfpc < Formula
     sha256 arm64_ventura:  "3bf28b63e9aa3d5823c76aa28a1e62f7b8866c6fd801abebdcae0e80ef91610a"
     sha256 arm64_monterey: "69fcafdc5492f2c38753aac0d2e146c929eeecefe7f0e7091b3a90d2463cdb46"
     sha256 arm64_big_sur:  "91b6ccda2deea3571d72dde84a374ef36be20ec5a2641b8f18ac701988e63051"
+    sha256 ventura:        "5eb8accefd6f35ed6990a09aa71f13c3e64bee7f08b7034d6bd444a8bb129297"
     sha256 monterey:       "7eb9b89630d7285c2b20fa0a131bec86dcf2b6a304fdea6f680949f396cd0397"
     sha256 big_sur:        "b2de1a251cd401445b171247210e1e3a729cd793eeddfe7e725039b4ea9d272c"
     sha256 catalina:       "6797e6bfdcff10e4e4b099d28547f608fbbc4aa94c0063d04b0e4d5195924f63"
