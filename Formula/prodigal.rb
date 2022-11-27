35a49c4933f74832a6c186c3a4b11c4327550905
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:53:54 +0800

    prodigal: no rebuilding required

--- Formula/prodigal.rb
+++ Formula/prodigal.rb
@@ -11,6 +11,7 @@ class Prodigal < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a790b0ef414bf71bd6382b1e7e2acee149988003a3def80085c4ae555e436ed0"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "bcf9c5deb46b542d95f4bcca4f4f947d60fee5138b76eab5018b61eb6f86279d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "abcf0f632ae6187b7b29e2ebd9680717c3678d3e2694b87840591a0f51d4db09"
     sha256 cellar: :any_skip_relocation, monterey:       "19a6b172b25f41612c11427cb12b19cca27580935c756ac5337c9bce27e3b4a5"
