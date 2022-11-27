7635d4bb7773cfaef25efbcd13fc899b4decc158
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:57:43 +0800

    sbjson: no rebuilding required

--- Formula/sbjson.rb
+++ Formula/sbjson.rb
@@ -7,8 +7,10 @@ class Sbjson < Formula
   head "https://github.com/SBJson/SBJson.git", branch: "trunk"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "a9198287e6912393936ae4881bc483e6afcce1f9ae9665b561ae4edcbf7ef72e"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "3fbf857117011107c8e7d3c8e82ab89468abddaa74d29b4696cba6338e89b454"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d3cb04de93bf5460c41623c296b5cd81884379d277afc874a64b0ff1cc34ac6a"
+    sha256 cellar: :any_skip_relocation, ventura:        "7bd3ad976f084a295edade17403886612453e3b0da624c22a4e1d45496fad5b7"
     sha256 cellar: :any_skip_relocation, monterey:       "b75a6403eff1226c4e348d9766c20694d43388c9ee9afa00fe4f637ab3dd7a9c"
     sha256 cellar: :any_skip_relocation, big_sur:        "695ced76533bfe9a4e893ffe22ea58402dbd93bd180e2ca4b4a96004d5c60581"
     sha256 cellar: :any_skip_relocation, catalina:       "e703b87ff205bfec1cfc09e9c200ebca6be643df15ec99b85c590110a4885fb2"
