e6de0afd7e964be192566999d911ae2607b79eaf
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:26:48 +0800

    fragroute: no rebuilding required

--- Formula/fragroute.rb
+++ Formula/fragroute.rb
@@ -17,6 +17,7 @@ class Fragroute < Formula
     sha256 cellar: :any, arm64_ventura:  "81e64dc3533f9469fbf91253816511df14307fe3260fbf3b4c3d2b0c2945adac"
     sha256 cellar: :any, arm64_monterey: "7a01636214817acbaffacc3eb4f5c38b5a44c3b63d0239e548c923cc22e17381"
     sha256               arm64_big_sur:  "35adad42ecbe16056a06708e7d0a3af1b9611aa3cfc1b1dc8cede40ee6f3f69d"
+    sha256               ventura:        "de37d085aa93a0213819b702ea9393705ada7dff78d1d02651ed56550cfe0c16"
     sha256               monterey:       "de505dc5218cbde66b8d8dc1538be12fa87ab717c35ea3002c3e8dd017c50fe1"
     sha256               big_sur:        "6d9bc388969f3798ca6ff4bc6e4cf5ecbc03f995b5f21268ae57fd49a69ec1c2"
     sha256               catalina:       "1427f299e84d0b1662a3492dc9c69cd46776265dc8b76488752b19eee1126ba6"
