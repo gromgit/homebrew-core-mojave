f1aab54d6d8d20a5f70a47dd365ade2d3e1b9f81
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:23:27 +0800

    dsvpn: no rebuilding required

--- Formula/dsvpn.rb
+++ Formula/dsvpn.rb
@@ -7,9 +7,16 @@ class Dsvpn < Formula
   head "https://github.com/jedisct1/dsvpn.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dsvpn"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "ea7226c79cf63122b6da3c957ec060ebe126c9489a74eb33e48733e0c55cba69"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "889d8f51fbe721906b84001adfcced14178739901beead09b88977a8b194c97f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "533ca096976136d7eeb0923c0164a0b3e8305d344036c1ad095a586f8768f5a2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5f0269b49b371b7330a7397c73ca1a8791da2e1215242eacd3e612f6705dd9c4"
+    sha256 cellar: :any_skip_relocation, ventura:        "bf4216d9f7d920dd9c3addf7c946ee730f74e2fdb361ea76e835855106a97ba1"
+    sha256 cellar: :any_skip_relocation, monterey:       "aaedaf19c0e6217233075e082167848fb03b2b2e3d3f27e690ac8f82064647f2"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9113686d8da5885ab67bae686914b0d27cc5eb2588a49b83f7c3fe66c89c039b"
+    sha256 cellar: :any_skip_relocation, catalina:       "a08464eca0167991c580594ecd9f1893a7be6d1cb522ceb385ff1883dca507c3"
+    sha256 cellar: :any_skip_relocation, mojave:         "31a8359d756b673788aad04e1b776c0e1d5b6331f7e64494d3c6680280ea11ec"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "d34ff5d83b0b259c5051de2e2e8cf4599679d1d7e61dd282065afb0516fe62b1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b6cc8aa7ec1d462fb028d5858de3863d2d131175ddfa102cfd44c9c0614d003f"
   end
 
   def install
