a8d68de1287ba94f915bb513b797bf07431feb1d
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:18:27 +0800

    chkrootkit: no rebuilding required

--- Formula/chkrootkit.rb
+++ Formula/chkrootkit.rb
@@ -15,6 +15,7 @@ class Chkrootkit < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "df424f7b9dcd0ff298d054020bb1086be26c1276aa617da306a54561986615b1"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "59a2d7c6798a78d907c269ab5ed9effdc111e4ff6b4dd7e5bd65ed3721846c72"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7a0131227a3d12132068de046c3a7fa851a452bae907c635a92a484868977104"
+    sha256 cellar: :any_skip_relocation, ventura:        "a53b31363f10d0fca17efc6fa2173b937f63776c19a01a2d2633b9c700b35f82"
     sha256 cellar: :any_skip_relocation, monterey:       "818406aced3c057489cea7395219de7aeff1c7da9e2b3218939d291c21595c72"
     sha256 cellar: :any_skip_relocation, big_sur:        "7fac603ca267b72c2882d460a458957f843a5e821fb0406d6230d42f1fe0557b"
     sha256 cellar: :any_skip_relocation, catalina:       "a181e6fe3a682bfb24742eac3823e3be8fa531e18954e99b02098bb7c43323fb"
