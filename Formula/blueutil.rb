01f645d57b3587814cdcbd574651e0d90c393df4
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:16:02 +0800

    blueutil: no rebuilding required

--- Formula/blueutil.rb
+++ Formula/blueutil.rb
@@ -10,6 +10,7 @@ class Blueutil < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "bfc27ffc84b837c882787d2f3f3cd0a37ebae88c9e00c747b1b6f3e6019667e5"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "adcb4a19c564cbd895f7e216df0a3293e17a009b243f5710edb9ba60965231db"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e29ddccdf7253406a3685f4099f3424ffb6d399ff2643f2d79f281ad97b93a67"
+    sha256 cellar: :any_skip_relocation, ventura:        "8ca9aacdc644af8a83fcf29a0877c20c9fdd7a2312c8b4e3f7d48ec8fcf11a0d"
     sha256 cellar: :any_skip_relocation, monterey:       "bdcffcaabd1270881f5b28da53aa1a6a28b93cbeb134d2b08891861653364b92"
     sha256 cellar: :any_skip_relocation, big_sur:        "429703e2bae1445d2a6ae2e1f52ed2c1f0bad3a94e80b44bfe36e698ba5ded30"
     sha256 cellar: :any_skip_relocation, catalina:       "3717d1a6753d4d94c4b4cbc92afa8ce58ed02bc1435806646fba4c7c1d04787a"
