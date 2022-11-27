9a8dc00f3d691844e1ee005ee8f79af370d9f1ef
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:36:42 +0800

    killswitch: no rebuilding required

--- Formula/killswitch.rb
+++ Formula/killswitch.rb
@@ -9,6 +9,7 @@ class Killswitch < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "4e8274a56bcdefb89eec6aa37d881866e77833f8e29291066adcfc55a7436088"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "73d4c8aa15e1aa363089be53bc11fb347994c1144ad35a65d01584727f74082d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cf8c995419a923e0512fd5e2d54a88d46719e157f1f9592ec82289e97f3da876"
+    sha256 cellar: :any_skip_relocation, ventura:        "894d0603001920b3ac4f419cb730b52e3b273ba4d0cd38a5f318e86fb4c3d577"
     sha256 cellar: :any_skip_relocation, monterey:       "aaddf26597e4de2554dfc11bb909615a612e0389774cea37f4356432a666bb41"
     sha256 cellar: :any_skip_relocation, big_sur:        "8d3e4381fb6137e38fd394941e40f1c0fab743c7b4bfef3c2ecb43679b6ab03b"
     sha256 cellar: :any_skip_relocation, catalina:       "3e00a8591a897509a48c65d76e529c6f4ef6fc910ebb762c8e5e7f54e2e03a43"
