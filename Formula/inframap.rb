f6d18b3713fc393d16d195cd5afcf306a22c4d04
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:34:39 +0800

    inframap: no rebuilding required

--- Formula/inframap.rb
+++ Formula/inframap.rb
@@ -10,6 +10,7 @@ class Inframap < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "a8346b10cb9a2054a86aaefba38ffa80f36bb2696b987aca77ab9e2c7e68dca3"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "763af27a8d3dbb2ccb3c7bcb63d337ee96e7f5c011dcecbee7768d76214814e4"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ee096fc9fe0909cc34675dbd7a7269c8092ffee0d6ba71fa333e75afc4ff28c1"
+    sha256 cellar: :any_skip_relocation, ventura:        "41de7c7b91cc5ccf46b5d4de5fc59e9ca9d941a4dd060fab01c3db3b7e62b80d"
     sha256 cellar: :any_skip_relocation, monterey:       "36b5b88b60390bc015fb56166cb7b890c8e5eb2f736a30384dc3fa92cda9f351"
     sha256 cellar: :any_skip_relocation, big_sur:        "0a4fe8ae6b7fcf419ab21ea31beb6b90dbeedd73ba2a771672e41c7a30ff4d0d"
     sha256 cellar: :any_skip_relocation, catalina:       "ba85244090ace0d6a94dbb41c9693f11647cb8e949acc5511c8d9c25a3f74c1b"
