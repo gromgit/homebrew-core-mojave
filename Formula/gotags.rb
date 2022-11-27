6e467602ab29d97aed429abf7b8659a647995181
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:30:25 +0800

    gotags: no rebuilding required

--- Formula/gotags.rb
+++ Formula/gotags.rb
@@ -11,6 +11,7 @@ class Gotags < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "06c5aa68685eed2b5118a8dae5e6755fdb8a5498f134ea67640cf9b767b27433"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "5ea97844e0e99ab34a8e1efa76325fc7550a737d4bde6c0af1a36fe3b93ce7f9"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2bb7b2f4eda136713179e19853088af77dff04472ac4efc25fa0e48648398547"
+    sha256 cellar: :any_skip_relocation, ventura:        "ddb7aa582f4a228079c940fa4fb9a3cb003655e5ef73de506bb75aa23f49e070"
     sha256 cellar: :any_skip_relocation, monterey:       "703f8c955b818375693bbc165872b0253d9aedcff2c25cad5aeafc2d0e37448d"
     sha256 cellar: :any_skip_relocation, big_sur:        "fc346e7abc09f27730ca2face102e704855a4e105310b27d0dc25b465e8fb453"
     sha256 cellar: :any_skip_relocation, catalina:       "c1b5430e2c3544fc021bc9bbc35c33a1f2c4482a30dbbc8d4977c1f0ee5638a1"
