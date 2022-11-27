f191a4e143c94d7770bb7238e7c7a2a84d384965
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:40:10 +0800

    libmypaint: no rebuilding required

--- Formula/libmypaint.rb
+++ Formula/libmypaint.rb
@@ -10,6 +10,7 @@ class Libmypaint < Formula
     sha256 cellar: :any,                 arm64_ventura:  "45f120eb85a644dae61e2bcf2683256dc3cae8531fa59d339e07ff9a3ba1f135"
     sha256 cellar: :any,                 arm64_monterey: "b481fb4e3ed5cb542d1ef073a5852a0a65361f0825051302ccdd6bc224901d90"
     sha256 cellar: :any,                 arm64_big_sur:  "4f5f706833fb183d4ad43a0b065b2b767a7787e7963eabced95016bd04ffdd12"
+    sha256 cellar: :any,                 ventura:        "4a895f28ea58e5415711bf7f3a415f639a958354992acd5a1ffd7719417fd5e9"
     sha256 cellar: :any,                 monterey:       "30623690f18dafe72d96daad871d4f7018ab3e89970ebdeda2fbf2d56c781c68"
     sha256 cellar: :any,                 big_sur:        "65d3c8c494c5e3a454526e4254c4f4c1a1883ca1e99c2dcb09c2abdff141d72a"
     sha256 cellar: :any,                 catalina:       "699014970a67055822e7ee2abc92c4ea2b45e51bcd58cfa01cb24c2ed08f6a2b"
