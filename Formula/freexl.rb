27e486d2c0f5f01ba2dd1647e6ffdc211615f981
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:26:59 +0800

    freexl: no rebuilding required

--- Formula/freexl.rb
+++ Formula/freexl.rb
@@ -14,6 +14,7 @@ class Freexl < Formula
     sha256 cellar: :any,                 arm64_ventura:  "d3b03b75eac2a439db87a56492103fe5eb0e619b1f5597cffc7f03bb11f4640c"
     sha256 cellar: :any,                 arm64_monterey: "f2f26c1449a3f79a89ec85deb5cd22507ee4715827afdf31469fd605c8a31b7f"
     sha256 cellar: :any,                 arm64_big_sur:  "57d5fc25946a587bfebb8724bff578688f605edcabba8cd80b9ebbf5840616d0"
+    sha256 cellar: :any,                 ventura:        "3baeaebe274fc3c9db2a59278fbd334f5a009dfbf03614d63b6803097a5da85b"
     sha256 cellar: :any,                 monterey:       "0dc034f726d2ad3850742e7f9a1676d67c14c48fd9735470fd3691a3c080182c"
     sha256 cellar: :any,                 big_sur:        "a20523355d18f1ed2259ae198b45c5aa93080fbd4926e0eb6969d2919b7a97ac"
     sha256 cellar: :any,                 catalina:       "1bab8437de88ce5564702dcc3e30a9c2f9491aa9358e767aa3d8ec62ba76922d"
