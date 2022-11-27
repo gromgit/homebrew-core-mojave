55725fb8f2453c05180720b3e064d72aa916f153
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:47:51 +0800

    netcat: no rebuilding required

--- Formula/netcat.rb
+++ Formula/netcat.rb
@@ -10,6 +10,7 @@ class Netcat < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "6b8d6a5ba62a3ea60b855cf4cff46ab35aeee9bb49966c3a769522a0df186a31"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1b67683e14760ec8ceda14c44b85d16411f5f3331e6385269fc6c1a4ee063273"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "70fa1400d39bcb39a3452bca1c921d1cc76783d8fa2ad41b1742a0c317c1aceb"
+    sha256 cellar: :any_skip_relocation, ventura:        "de0cce7840c9836ae8003805ca0817d03e8c54e62ff4044fee1085a97883d033"
     sha256 cellar: :any_skip_relocation, monterey:       "7c33ed98a6c81011f5923240e11b87f07add5cea280f5e2754b2f3d7fc3d9eee"
     sha256 cellar: :any_skip_relocation, big_sur:        "ec93ed2ce809a78373e1b747f20075fabe5e9d612e2f84f85f125e4ce81eadb3"
     sha256 cellar: :any_skip_relocation, catalina:       "13bd349dfb08b3a5a474498eec4e20ffff722f82446b255d9c6e0540b02b362b"
