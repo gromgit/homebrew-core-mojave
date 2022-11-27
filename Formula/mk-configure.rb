fd3d0dbd01ba0c8c53ee807c3a00d3b21fe4c00b
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:45:38 +0800

    mk-configure: no rebuilding required

--- Formula/mk-configure.rb
+++ Formula/mk-configure.rb
@@ -14,6 +14,7 @@ class MkConfigure < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "833084703288803fe5bf6e7b21e2dc3fc3bb4e8ff3d1dfe0aa349418f49f384e"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ca9449aa389f16613661553243de67150d36b019d6b77b767f95c03fd0da1bba"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "99cfb90e5b63675ee36307557d4c4981843cb39f96824d3b0424d0feaa215ae8"
+    sha256 cellar: :any_skip_relocation, ventura:        "e0d3babe5b626e23e8acf33e2a34c4dc330394cb84a9bdaed6344fb38c71a059"
     sha256 cellar: :any_skip_relocation, monterey:       "a8c8a8dd1f1710a987a6417b3ad3972cb03461213148284185ea535a54f373b3"
     sha256 cellar: :any_skip_relocation, big_sur:        "74fd29adec76158ebe619c4116609fe56b88aed78f64161fa352cad842a5a760"
     sha256 cellar: :any_skip_relocation, catalina:       "d49908536866ff5ad40d08035125236d0f3c0cdcb0d4db190b4d2fc72deee1c1"
