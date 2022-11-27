e9835d3d47b0183f16c7a8483f638300dceb27d9
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:41:39 +0800

    libusrsctp: no rebuilding required

--- Formula/libusrsctp.rb
+++ Formula/libusrsctp.rb
@@ -10,6 +10,7 @@ class Libusrsctp < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "1327b9355f6c879443e9a3b8d405559d87ee245e05b435a0fe5257b3d72fd5b4"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "2bf65d4cad3f3abcec79432f82c040761b3bf8b5b04417172ea343efc7ff878c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cccdb95cc428680b9dc8c57ae970f23874889797d8438eaa9079e675473ab394"
+    sha256 cellar: :any_skip_relocation, ventura:        "f0b2353ab9feebdbd5b7d37551429d7956a4fdf2d7fd628aff589b41da0f9d04"
     sha256 cellar: :any_skip_relocation, monterey:       "3a5ea16561b37ed98f235bd62e33534c85439244a41f3e917ffdabd6c97d74b0"
     sha256 cellar: :any_skip_relocation, big_sur:        "ca45d1d9431028ad9b7025e6d5486a10f98c6c49e39dd1a4e1d033c75bee6135"
     sha256 cellar: :any_skip_relocation, catalina:       "5c2a6b26e354c0498e0e3ef590dfc9f9651f70ce36112f196baec64ef76aec31"
