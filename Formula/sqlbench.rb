a686dab6e9a8003123367d7c6eeddc2f329210db
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:00:25 +0800

    sqlbench: no rebuilding required

--- Formula/sqlbench.rb
+++ Formula/sqlbench.rb
@@ -7,6 +7,7 @@ class Sqlbench < Formula
   head "https://github.com/felixge/sqlbench.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0598ce5d9b3c1aed37b1690a3e8357300b3ef8906ea2275fe1ee997375c09e98"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "14d3a0b3a26e3291ae1039e67c72970b4a1b0388387b919f2c71e01e24e6a429"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8373986acd8ee9e32df964c5bff6b365f29afa06fa256789017112d9b07ffcf2"
     sha256 cellar: :any_skip_relocation, monterey:       "a59e25067b830b0062a0d3c7fa98da5c31ef16c0763303f5acf16238aead26a6"
