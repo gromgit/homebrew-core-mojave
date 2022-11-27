ce8aeabb96ed4a18b61986b725022c56d864ff2a
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:16:24 +0800

    brew-cask-completion: no rebuilding required

--- Formula/brew-cask-completion.rb
+++ Formula/brew-cask-completion.rb
@@ -11,6 +11,7 @@ class BrewCaskCompletion < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "fb88b0184cc69f44b9c0e9744a4ce7a46685aea6e1e529deae45f5edd4497788"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "fb88b0184cc69f44b9c0e9744a4ce7a46685aea6e1e529deae45f5edd4497788"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a058f8dd7fb25aa2ca8452d32f7d419b3b461b0f3b1dfe4f2f2e6d0e79b014ab"
+    sha256 cellar: :any_skip_relocation, ventura:        "2154b23c163900381ba68cfc78f2d961f5599e3a7116368ad516a7b02e2b7b4e"
     sha256 cellar: :any_skip_relocation, monterey:       "2154b23c163900381ba68cfc78f2d961f5599e3a7116368ad516a7b02e2b7b4e"
     sha256 cellar: :any_skip_relocation, big_sur:        "a5256bbd0456fc15083d843e7cc3778fd45dfd3562b14bf076e0a08bcc04948a"
     sha256 cellar: :any_skip_relocation, catalina:       "a5256bbd0456fc15083d843e7cc3778fd45dfd3562b14bf076e0a08bcc04948a"
