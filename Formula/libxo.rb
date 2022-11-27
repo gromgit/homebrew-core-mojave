bdc799b5608098e80df45d4318fbae3992ab6d52
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:42:20 +0800

    libxo: no rebuilding required

--- Formula/libxo.rb
+++ Formula/libxo.rb
@@ -9,6 +9,7 @@ class Libxo < Formula
     sha256 arm64_ventura:  "b9e3fdf6f9bf02d0a13c076f9460cc3ea55a78a1387d9b89d52e40375ea6d664"
     sha256 arm64_monterey: "82180c9986e803236f8caba30c00b2dcdddaebaedfeb93a9c19a35fb959d6199"
     sha256 arm64_big_sur:  "56e3e01d82e65127da77eabfa5c109689c752aa039771ad9ec48b6de9910ddfd"
+    sha256 ventura:        "73c60cd82012ce8ef4b2db4df25e3ba117c19731cdb844eb53e19df05410b54e"
     sha256 monterey:       "8621ad5d4d88b8313bf012dffba7a9f5c500f0c2f3489aa7fc1726d470e0ff9a"
     sha256 big_sur:        "4fcbf10d05037ee979e9b58ff784cf98528311a686bb4a5325701cd3fc90f784"
     sha256 catalina:       "b77f42736665614730b6f1dfb0645c73a137451435914f5583148e67e265ec1b"
