528e206a806eb1be228ac0b4f1d95d0271616c51
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:01:04 +0800

    stoken: no rebuilding required

--- Formula/stoken.rb
+++ Formula/stoken.rb
@@ -9,6 +9,7 @@ class Stoken < Formula
     sha256 cellar: :any,                 arm64_ventura:  "620455231b34b4e2c51dc636d2a2c34d7c17edca87e38b6c268b3cb11dc59bc7"
     sha256 cellar: :any,                 arm64_monterey: "545f8479e92a7c8dcf0d9a42a5c0027e34e8760596ca53e308aedebe65b80477"
     sha256 cellar: :any,                 arm64_big_sur:  "2f66cb207fe048720b4497e774752de500d005b4bcc7bd45ccb164ecd11fafc8"
+    sha256 cellar: :any,                 ventura:        "915c2a3c455ca7e656336d77d193ce96af60d46cf2e90836c55b96c9c57f251d"
     sha256 cellar: :any,                 monterey:       "c2f16c9907f9d412da320f03902cc1ff86af807acdf9b7581316337e561a711f"
     sha256 cellar: :any,                 big_sur:        "701102c6cb8138920a8ccf7aae6d89ea247d259d17f7f4ce3e4af46cad516802"
     sha256 cellar: :any,                 catalina:       "423dbce4e76710fe932fc4d86fa25b39ced8f138d781fcccbc3982ce83136216"
