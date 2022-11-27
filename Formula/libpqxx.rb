a01494cee69152e127f6a21c7488473ae510f086
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:40:33 +0800

    libpqxx:

--- Formula/libpqxx.rb
+++ Formula/libpqxx.rb
@@ -10,6 +10,7 @@ class Libpqxx < Formula
     sha256 cellar: :any,                 arm64_ventura:  "4f7eac329b43c9339b132bf4dfb9fd8bdfd157b38372d62fff7fb07a46f81fed"
     sha256 cellar: :any,                 arm64_monterey: "efc6a46c8f5be1b0cdba4b225d2c5ee6ab4a1ab781c6d15e3e91377b75086b27"
     sha256 cellar: :any,                 arm64_big_sur:  "4d404ad40773faa6ee7c43f265dfad2469e6df3b225f081791beafca23e08ba6"
+    sha256 cellar: :any,                 ventura:        "ab9d2a71d7e69db88b1ed5a7fb10cabe6e9a9c9cf6c41385196f3bc5957f5cce"
     sha256 cellar: :any,                 monterey:       "817cffc912456f0e233dbee4be5db19bad19374051c27179b44af3da228470d3"
     sha256 cellar: :any,                 big_sur:        "225c2c1f68f97c7cd1dc12e252dba50a7b987a8e6897f76a2091b5d81ea9f993"
     sha256 cellar: :any,                 catalina:       "970f82f123653e725658e03e5cf1d8de48ba0af2d7e9a2eee7c01249fbcaac6a"
