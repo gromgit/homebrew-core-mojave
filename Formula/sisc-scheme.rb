a4ef66bf3630c0ca207804152489f7e17c71c5d5
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:59:09 +0800

    sisc-scheme: no rebuilding required

--- Formula/sisc-scheme.rb
+++ Formula/sisc-scheme.rb
@@ -5,8 +5,10 @@ class SiscScheme < Formula
   sha256 "7a2f1ee46915ef885282f6df65f481b734db12cfd97c22d17b6c00df3117eea8"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f65d81b8af3efb3351510362fdde92e8b9fc5a32eaba361a438abed4fb265991"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f65d81b8af3efb3351510362fdde92e8b9fc5a32eaba361a438abed4fb265991"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f65d81b8af3efb3351510362fdde92e8b9fc5a32eaba361a438abed4fb265991"
+    sha256 cellar: :any_skip_relocation, ventura:        "e76fa3836cfb1020d76de3ccda011d84223260860c78372930dbe99eeef6f46b"
     sha256 cellar: :any_skip_relocation, monterey:       "e76fa3836cfb1020d76de3ccda011d84223260860c78372930dbe99eeef6f46b"
     sha256 cellar: :any_skip_relocation, big_sur:        "e76fa3836cfb1020d76de3ccda011d84223260860c78372930dbe99eeef6f46b"
     sha256 cellar: :any_skip_relocation, catalina:       "e76fa3836cfb1020d76de3ccda011d84223260860c78372930dbe99eeef6f46b"
