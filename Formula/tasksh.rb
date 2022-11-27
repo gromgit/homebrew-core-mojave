89bad5b50921b128dc4b2257833bedcb91adaad5
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:02:00 +0800

    tasksh: no rebuilding required

--- Formula/tasksh.rb
+++ Formula/tasksh.rb
@@ -14,8 +14,10 @@ class Tasksh < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "1a8bbc54e5712ab5b9caa686e6348365da4c8bdaebeaae474be2edda28368d72"
     sha256 cellar: :any,                 arm64_monterey: "6c6390a79e5f4645f2cf2507a1c29ad1b935a5f51d87387412e557343688a11d"
     sha256 cellar: :any,                 arm64_big_sur:  "590c43b791080cc6ca56cef896c9e75a8ca77915b061a1d0a711a0489e69ab63"
+    sha256 cellar: :any,                 ventura:        "d0a97986a19732ce4d3818c5b452f6ce636ce957e401350cade47190e5b7b2e0"
     sha256 cellar: :any,                 monterey:       "778d32859e2a65b224819a39d022611b7959fba4d72d08a45d42f76bf6cf6cf8"
     sha256 cellar: :any,                 big_sur:        "987789014e770fb3b4b1d4500321877c457ba2a1dde2fc9925762dfb0d7da541"
     sha256 cellar: :any,                 catalina:       "68a13aa8ea81fd1fe7c2c5e9eadd3850fe21265b34c4cf2f1cf7e7ede3caeaee"
