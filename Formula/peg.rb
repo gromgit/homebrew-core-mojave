6cdbf03b62ae669ee07ab5474237aad15708bff2
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:51:53 +0800

    peg: no rebuilding required

--- Formula/peg.rb
+++ Formula/peg.rb
@@ -15,6 +15,7 @@ class Peg < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f19c265ab4e83b4ac64c6d37e15227a23f199986185d447860580bce9354cda6"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ab5b7c51553751ef8e97394ebd30d901df33e46c5f2c6bf4b38f966673ce1932"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1b9a4c3a056a8c36357592bec0a6b3f72d8741f6b2dad69e68d3653799628012"
+    sha256 cellar: :any_skip_relocation, ventura:        "070085f152f57fecc73b066cfd8d5118f133b4f622cfed5a76231fff8bdbf86d"
     sha256 cellar: :any_skip_relocation, monterey:       "730dd20c7291e466bd568056e72f4723feab53eccf80ba35a2f0101d7f7ba1ba"
     sha256 cellar: :any_skip_relocation, big_sur:        "7f1a4a1e00c14050d3246b66cf554797b184e5c6be2eb53ebb2edfe6845412e5"
     sha256 cellar: :any_skip_relocation, catalina:       "bb6bfe4999b4675d3e3d16a52552fe571430fa5956fdff245837f922d7e39842"
