411ca27e339a6c322ab1d8fd7bc6801014be9d2e
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:07:17 +0800

    wgetpaste:

--- Formula/wgetpaste.rb
+++ Formula/wgetpaste.rb
@@ -1,12 +1,12 @@
 class Wgetpaste < Formula
   desc "Automate pasting to a number of pastebin services"
   homepage "https://wgetpaste.zlin.dk/"
-  url "https://github.com/zlin/wgetpaste/releases/download/2.32/wgetpaste-2.32.tar.xz"
-  sha256 "621dbafbc7bcf5438f11447a325d8974069e3df03ef0c8bb2a2cc3de2c0cdb13"
+  url "https://github.com/zlin/wgetpaste/releases/download/2.33/wgetpaste-2.33.tar.xz"
+  sha256 "e9359d84a3a63bbbd128621535c5302f2e3a85e23a52200e81e8fab9b77e971b"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "19e1162aaba99186726c81d52e599888ad597e4d7ec889604dc6f86f3049ddd9"
+    sha256 cellar: :any_skip_relocation, all: "e28f46fd5ff5ace39fab378e3e156ada2996420f9db32e78c5679f5b461fee16"
   end
 
   depends_on "wget"
