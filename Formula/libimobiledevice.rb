f11612757c7a53b5a83ef4e83ee0aff164006541
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:39:42 +0800

    libimobiledevice: no rebuilding required

--- Formula/libimobiledevice.rb
+++ Formula/libimobiledevice.rb
@@ -9,6 +9,7 @@ class Libimobiledevice < Formula
     sha256 cellar: :any,                 arm64_ventura:  "011e027433848f23cd9d96aee9f46531f48f8462bd763fe799e09b36eeaa4851"
     sha256 cellar: :any,                 arm64_monterey: "f3c97e567f59c4a8ab79f8a3d66a32d109fc9a7c22891589b998edb6a4e5ba28"
     sha256 cellar: :any,                 arm64_big_sur:  "41a64c9856f7845bb4c21bba4f42eb55c640301b59c032eb4db416db19ecf97d"
+    sha256 cellar: :any,                 ventura:        "3db04118fec82077bd2b1a3e137f3a6a6037aeaa094865fc3d1187d7f795a308"
     sha256 cellar: :any,                 monterey:       "2cde67c8eef4e971ce74428a9162e9680d7a9ab542571f438602efe431d3a121"
     sha256 cellar: :any,                 big_sur:        "0fe21433f470130b972354d411d05f43ab37d82198565bb6b947734a95e98c5d"
     sha256 cellar: :any,                 catalina:       "eb7f28d86797461d5ef859d00629176e1ce3234790ef17b9ee3f9c9990a664e2"
