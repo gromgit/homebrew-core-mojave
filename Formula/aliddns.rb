baaf15a1a4e273a2153dace6d5b415ac39363860
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:12:47 +0800

    aliddns: no rebuilding required

--- Formula/aliddns.rb
+++ Formula/aliddns.rb
@@ -11,6 +11,7 @@ class Aliddns < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "850026dd34a736d0fbf82dbb55b6c021672be35170276a23692e117f6fe1a90f"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "23ab8335a2b758f3557053847a848d61dfb53c614abd8e52eadd62352bc45c5f"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "779eac8f4e88b704f068d542d1c4f209a96edc5ded85e5ecfefa961d798d6620"
+    sha256 cellar: :any_skip_relocation, ventura:        "c8b8d305a0925d9e322b260417298ca4210a829391888c388774b521c6a63d6d"
     sha256 cellar: :any_skip_relocation, monterey:       "ad0bfb1477ef8ec7145eca1cbeb4147157c928ebd440a757f054de2d961f2578"
     sha256 cellar: :any_skip_relocation, big_sur:        "107cb79b754c414f6ffc4bf48ad086e502ad1b39aba147177e3490258e840f8b"
     sha256 cellar: :any_skip_relocation, catalina:       "67937c8e5e9379f4eb36adb24f5b1c5330d488dbe032e0cca376709c03f7b29b"
