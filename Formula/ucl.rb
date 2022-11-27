6ab4596ebfd064872d241eaad5c853344e94eaad
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:04:46 +0800

    ucl: no rebuilding required

--- Formula/ucl.rb
+++ Formula/ucl.rb
@@ -15,6 +15,7 @@ class Ucl < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "462fd1b3bf0382d5dd445f4d12b86f47e4577ab52a17d227b2c80a8e2faf5307"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4c7f82e2d2c969d71a2dbaca2cc6c0f2577c422a16281a981e3193535263803c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ef63a86669c63e486c3682494a93c9db453a33089a2a71398efb8e5e26cf5e44"
+    sha256 cellar: :any_skip_relocation, ventura:        "395e6919528cb44ea1daecd6a41cc6e73da22f100e84310c67072b411ccd5db2"
     sha256 cellar: :any_skip_relocation, monterey:       "4317885999b8297a1919d5d65a9246efdd7bc1807fd2df4a9268a202fbf3a97c"
     sha256 cellar: :any_skip_relocation, big_sur:        "91ce0597dc8e648e4ee0d0caaa30bceb5f569acc90634d88fa5e7859f2ae682a"
     sha256 cellar: :any_skip_relocation, catalina:       "116db1f8157bf88831fece730fb3e6fa82420d53c29b032afd63b979df42b386"
