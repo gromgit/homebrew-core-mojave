d4a01ed30f49e3dc24ff68899e3cf3a61760a84f
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:20:01 +0800

    convmv: no rebuilding required

--- Formula/convmv.rb
+++ Formula/convmv.rb
@@ -13,6 +13,7 @@ class Convmv < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "a814874b65a3d67de4d5c4f0c1ae2dd94584e23cc8092b2df1cc95b35f7c260d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f70af7ba1886110a7e4eef42e929480994cf73f6fa0511df4a84b17fc542720b"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3b5226bc4a647dfcfacba43b505e94cd1c2ad5037f04660b267fe4d2f1a2158a"
+    sha256 cellar: :any_skip_relocation, ventura:        "333fc843d1aa7546df1fc8d19b0da7691125c6b5337e2ec54f458ad97cf99014"
     sha256 cellar: :any_skip_relocation, monterey:       "3a165d560e9a22558381b704eafe08dfaa0267b7d04b56505072ca9fdc6feaa5"
     sha256 cellar: :any_skip_relocation, big_sur:        "20517059aabe3b68553cc8a756331db3692732cddd5f3cf10905189f1494ddef"
     sha256 cellar: :any_skip_relocation, catalina:       "203e34d5e76b55fabbf8548b93f749cd044ad843ca9062a916026e548332f7b0"
