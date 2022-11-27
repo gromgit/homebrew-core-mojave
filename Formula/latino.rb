bc165ef799f8bb3b7c96420c0a10e68642017c72
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:37:31 +0800

    latino: no rebuilding required

--- Formula/latino.rb
+++ Formula/latino.rb
@@ -8,8 +8,10 @@ class Latino < Formula
   head "https://github.com/lenguaje-latino/latino.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "aac984134bbd599ee76ccd80ae017c738f633a4642a7a99ce8c40ead45078d8b"
     sha256 cellar: :any,                 arm64_monterey: "96c4a79b87f5f1c44ee9a39d13e75ced35a8b23ed5ee209279690363e79cb876"
     sha256 cellar: :any,                 arm64_big_sur:  "e7ac6aa7973d222c73097942a233c2998e542358b21aa725dcbbf8e1e6010b06"
+    sha256 cellar: :any,                 ventura:        "0b965dcb6c5701e3519aaeb04e676b53e3962763b5eb9a437548ff8b5d06c839"
     sha256 cellar: :any,                 monterey:       "952cc3322325e09ceb9fcbb00c957a4179031f7522c99ae6afbc20bb13bc3d13"
     sha256 cellar: :any,                 big_sur:        "0848f83a97ae97c615e2a448eb28573fce7c20f20b3b52ddb6d9f487c80524ac"
     sha256 cellar: :any,                 catalina:       "46af81ff7b1693cd40465ce5e0defb7a708c918a996db2af775b913cc682a567"
