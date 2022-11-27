910682b9fca26c97db8fb7520a6aed325b937407
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:36:49 +0800

    known_hosts: no rebuilding required

--- Formula/known_hosts.rb
+++ Formula/known_hosts.rb
@@ -6,8 +6,10 @@ class KnownHosts < Formula
   head "https://github.com/markmcconachie/known_hosts.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f3a25e4320813f741ccbd44b32856f31f3cd687239f9f409a336465e2f2bcb27"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f6f7c9c4638ffa5cff8f90e57ff9a8bbb3b2114e87d709e930e88120884859ad"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bf3e0af3ffe33fc928ada23b36fd1549b8435234480371f678cb125ecdf3cb74"
+    sha256 cellar: :any_skip_relocation, ventura:        "785882a8af04336a4d06487a4b12b1ef1381b141388fa04c22e5def3ca99d0fb"
     sha256 cellar: :any_skip_relocation, monterey:       "aa8e1755201bfe310f46071a14105071fc49b39b415b128e138d7d765b3050ef"
     sha256 cellar: :any_skip_relocation, big_sur:        "9d89c5861e842476e98fe66ec06b418b328fa0c9d9827aba4d4063fc6c69eeb1"
     sha256 cellar: :any_skip_relocation, catalina:       "dcf7fb7a2fc7436eb50d0ded0dd0059f082cb6652ea107c6f37696d1fe08bd70"
