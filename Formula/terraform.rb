9c365c83b27d8558a704ca7793946ae34941035b
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:02:37 +0800

    terraform: no rebuilding required

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -1,8 +1,8 @@
 class Terraform < Formula
   desc "Tool to build, change, and version infrastructure"
   homepage "https://www.terraform.io/"
-  url "https://github.com/hashicorp/terraform/archive/v1.3.2.tar.gz"
-  sha256 "51c41c40bcd87b917046b786cf6d32d3b2b5f6c5fc4cb84121944efc9be54cc0"
+  url "https://github.com/hashicorp/terraform/archive/v1.3.5.tar.gz"
+  sha256 "6c44d7b30b31c68333688a794646077842aea94fe6073368824c47d57c22862e"
   license "MPL-2.0"
   head "https://github.com/hashicorp/terraform.git", branch: "main"
 
@@ -12,8 +12,14 @@ class Terraform < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/terraform"
-    sha256 cellar: :any_skip_relocation, mojave: "a2c2b0ef6b4d8a5d7b08c6f866d2824b868852e3601b3106675009fc2c404944"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "56768bc60e7bc8300587d80e47f457e1897c292fa2f634404d69d2c8e307b40e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c433a55671fe94926cbd3f72f08a2a7815124bcee534aab3beb76e258c2b9bd7"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "290f992fd4ede7b09bab90186da6974dfc526ca025180c4856a2db4cdcbb6c4b"
+    sha256 cellar: :any_skip_relocation, ventura:        "d5c768d0651d49ae233ca743af0ee8ae50ca3b9661fdcaa171d05bdf001b55c3"
+    sha256 cellar: :any_skip_relocation, monterey:       "d908cc879a56a23831a57e5e93f05bf8ade367d18448815b328154cbb07b364c"
+    sha256 cellar: :any_skip_relocation, big_sur:        "a7cab10bee32c1c7153a9d11b74f48dd1186c07bae9a1a490f2a9dce1d3f4268"
+    sha256 cellar: :any_skip_relocation, catalina:       "cec9a6abc72ee2a7bff108a71814d7bc01caa9a39309650a3aa2e08b266af8a5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4443716deb077a71796124be168f2586cba2e6f069940c491537c17080e9bae5"
   end
 
   depends_on "go" => :build
