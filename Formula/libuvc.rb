c2bec2a7c17ed7bbfb48d5b0c5e4e5fd031725c4
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:41:41 +0800

    libuvc: no rebuilding required

--- Formula/libuvc.rb
+++ Formula/libuvc.rb
@@ -11,6 +11,7 @@ class Libuvc < Formula
     sha256 cellar: :any,                 arm64_ventura:  "4b9154f39788c2368a311ba87d4027d4cc9832d3159e7cafefcd64e3d9e99694"
     sha256 cellar: :any,                 arm64_monterey: "619dc091b52aaf2461b8f6935d9be9bf5b30fe99ac40545458761a8879dd4e61"
     sha256 cellar: :any,                 arm64_big_sur:  "51c899af95c83a96799af845463826cd9c4a939c030bfa1fa7597603f3470dd5"
+    sha256 cellar: :any,                 ventura:        "49b9d13d86ac72c08e3238848776fca2784443b4791e95400a1e4b5cd609a645"
     sha256 cellar: :any,                 monterey:       "66d3c4e163006cbe0d76765ee075b7ef2e5eec8c1e6f720051b0ac4f56abeb85"
     sha256 cellar: :any,                 big_sur:        "1bb47911fa2b70dabe09cb9520498700ff75d0349991f36460b28d762c5cf03d"
     sha256 cellar: :any,                 catalina:       "4051cd7aa8dbf7a5e940a85f8c38900829e66cabfb00be22a592dd3a421e3cca"
