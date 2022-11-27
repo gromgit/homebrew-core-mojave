a6a970b954faf4d5371817cad56eb1afbee44d33
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:02:22 +0800

    teku: no rebuilding required

--- Formula/teku.rb
+++ Formula/teku.rb
@@ -2,12 +2,12 @@ class Teku < Formula
   desc "Java Implementation of the Ethereum 2.0 Beacon Chain"
   homepage "https://docs.teku.consensys.net/"
   url "https://github.com/ConsenSys/teku.git",
-        tag:      "22.10.2",
-        revision: "182f5601ff5ef35ce10dba59eb64d441358843d5"
+        tag:      "22.11.0",
+        revision: "a3aacebe93d6d41e39fd2f6a4732d35cc3cc9940"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "21372b639d8eacf8e648ec52f2afef66f425b3add2a461d4c4b0ea896b011748"
+    sha256 cellar: :any_skip_relocation, all: "ca2d21eb0f3ea9e5d8c50203684a52d2a20569004607e110d884153fb9f8a2fa"
   end
 
   depends_on "gradle" => :build
