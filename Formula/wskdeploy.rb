b46ef6371cfd0caebc4a911100095a7d3f5f7307
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:07:44 +0800

    wskdeploy: no rebuilding required

--- Formula/wskdeploy.rb
+++ Formula/wskdeploy.rb
@@ -6,8 +6,10 @@ class Wskdeploy < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "08cf43fae8646a2da12684d40c5271ea647950788125e7e896d5641984ec98f4"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4b42f8375e4d73e6fc92323e40b2c12f98227b4293e0e948cdfc514e698207fe"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "87364286a37d26de6a051ba1d0244de932de11058e041e969ea20400560df8e5"
+    sha256 cellar: :any_skip_relocation, ventura:        "9a5292f9f476d5fcde994572952c3e1c454367e0a95ec6ac13e416eb37eede43"
     sha256 cellar: :any_skip_relocation, monterey:       "e3d3c61f5b230af2a2a5776448d06985cee14f9c7aa5b51dc40179922d655df1"
     sha256 cellar: :any_skip_relocation, big_sur:        "c77d6ad2c5fa8acec45bf9507d840f3de1a125edb5759f6de49427efb454fd38"
     sha256 cellar: :any_skip_relocation, catalina:       "17ff44da88c60d8c8c3a17fd4e2844c90d1bf7fe460928ae21731da5a7f52740"
