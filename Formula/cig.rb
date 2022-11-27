bf50875393924312217e4322f102441b8e45976d
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:18:37 +0800

    cig: no rebuilding required

--- Formula/cig.rb
+++ Formula/cig.rb
@@ -11,6 +11,7 @@ class Cig < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "e3b74f7b2d3b2d121ccb6946718893b4f0c09d6bae5e5a6a7d48b8144da9b452"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "48e80e3f418f825caceccd69e9ec911f5c03b8fa7f38883edfd66d353f70a481"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2d4f345393a0553e40003b46523a07e2bb0162bba0309ca9c0d322f606e73b76"
+    sha256 cellar: :any_skip_relocation, ventura:        "340790bf6ab897640e1a1f350d8c106d3bc2835ad71cec69b7a717894749cc35"
     sha256 cellar: :any_skip_relocation, monterey:       "fbca3e3336ec9a9848091d2b1845a48dc915ae798da5e1d54fe6aa4ee2261da6"
     sha256 cellar: :any_skip_relocation, big_sur:        "c41c70e517158f1a31bb4b29a6fa01b12570001353b8800d55aadd4ddc99080e"
     sha256 cellar: :any_skip_relocation, catalina:       "3ccce3238efd259041dbb0f0427d5ac06cc4dfafdfbfd336ddd0023e02e9dd7d"
