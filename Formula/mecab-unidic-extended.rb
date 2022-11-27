533ad4500cfdf15f2b0086301733cbaa651e3f16
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:44:50 +0800

    mecab-unidic-extended: no rebuilding required

--- Formula/mecab-unidic-extended.rb
+++ Formula/mecab-unidic-extended.rb
@@ -14,15 +14,18 @@ class MecabUnidicExtended < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "cd9013ad7c93c14834061c6591eff7ef5fd67fe8508ee461ab484bd74e68edef"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "3afff6a9a967b7c92eb79ca6efcaf3289596c331214ba6130989d757cd7757b2"
-    sha256 cellar: :any_skip_relocation, big_sur:       "7566096a08a09b4c695c2f59766dc3d8ed5156d87180a3da1d504be9e038a30b"
-    sha256 cellar: :any_skip_relocation, catalina:      "2624e794b1f765e78dbd593b50b8ea2b3055ede30192694cfc3ed7f5c4eb8a9b"
-    sha256 cellar: :any_skip_relocation, mojave:        "7b297ee9b0d51169a5d5f7dd7047c1aaf18ade6bcaeccf2b570263aa6ab694b2"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "8890b4d95d0c3ee114ec48ea95d66c1d8b8a273949a912140fa477ad9265be0b"
-    sha256 cellar: :any_skip_relocation, sierra:        "8890b4d95d0c3ee114ec48ea95d66c1d8b8a273949a912140fa477ad9265be0b"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "8890b4d95d0c3ee114ec48ea95d66c1d8b8a273949a912140fa477ad9265be0b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4445b4f69062be6b5142324088eef2a83e958a584665e9ac36806a2c1102bef5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cd9013ad7c93c14834061c6591eff7ef5fd67fe8508ee461ab484bd74e68edef"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "cd9013ad7c93c14834061c6591eff7ef5fd67fe8508ee461ab484bd74e68edef"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3afff6a9a967b7c92eb79ca6efcaf3289596c331214ba6130989d757cd7757b2"
+    sha256 cellar: :any_skip_relocation, ventura:        "cd9013ad7c93c14834061c6591eff7ef5fd67fe8508ee461ab484bd74e68edef"
+    sha256 cellar: :any_skip_relocation, monterey:       "cd9013ad7c93c14834061c6591eff7ef5fd67fe8508ee461ab484bd74e68edef"
+    sha256 cellar: :any_skip_relocation, big_sur:        "7566096a08a09b4c695c2f59766dc3d8ed5156d87180a3da1d504be9e038a30b"
+    sha256 cellar: :any_skip_relocation, catalina:       "2624e794b1f765e78dbd593b50b8ea2b3055ede30192694cfc3ed7f5c4eb8a9b"
+    sha256 cellar: :any_skip_relocation, mojave:         "7b297ee9b0d51169a5d5f7dd7047c1aaf18ade6bcaeccf2b570263aa6ab694b2"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "8890b4d95d0c3ee114ec48ea95d66c1d8b8a273949a912140fa477ad9265be0b"
+    sha256 cellar: :any_skip_relocation, sierra:         "8890b4d95d0c3ee114ec48ea95d66c1d8b8a273949a912140fa477ad9265be0b"
+    sha256 cellar: :any_skip_relocation, el_capitan:     "8890b4d95d0c3ee114ec48ea95d66c1d8b8a273949a912140fa477ad9265be0b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4445b4f69062be6b5142324088eef2a83e958a584665e9ac36806a2c1102bef5"
   end
 
   depends_on "mecab"
