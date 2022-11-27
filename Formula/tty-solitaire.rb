38bba13ad20eaac339b5e3a97cae1ce2e1b195e6
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:04:23 +0800

    tty-solitaire: no rebuilding required

--- Formula/tty-solitaire.rb
+++ Formula/tty-solitaire.rb
@@ -9,6 +9,7 @@ class TtySolitaire < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "c6e0d4124e3f8a37e045611694d87d3b5a7399eb14487ce2e1b874fce96149eb"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "a8ea8909f9e522b9e6fbbbe1db5901a0f584ef25bb788099267ee6e035f84580"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "91eea2f4dd14f6a81e3b3cc44fdb72916e1ae2dec5075b85136d5a224cc8d655"
+    sha256 cellar: :any_skip_relocation, ventura:        "994d065c78b8dc3eb957b1e533df8c280d2c5adbb7be126580f728bfbb6e3cdb"
     sha256 cellar: :any_skip_relocation, monterey:       "0f84c6f6c017c4db02499d375ce1d6be389d39327fb0d5f8d050a797380c705e"
     sha256 cellar: :any_skip_relocation, big_sur:        "ca5bfa5041e8870b14c3ba92e73ad2766c8edc8cf139f03e9d983fc8d9a8976f"
     sha256 cellar: :any_skip_relocation, catalina:       "b315a94abfcf02186d94a6d4e78c71fec3538a65c5cc885df5dd9245efbca51c"
