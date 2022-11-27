4f0a50b7888116b9da982a8318f7270df6a0b0b6
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:45:02 +0800

    mesalib-glw: no rebuilding required

--- Formula/mesalib-glw.rb
+++ Formula/mesalib-glw.rb
@@ -12,8 +12,10 @@ class MesalibGlw < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "a9fdf656540dc268519f8e3fded305c1e86c4690cf7cfa5571e3edbee9e56cc5"
     sha256 cellar: :any,                 arm64_monterey: "f19366ec40b0666882b3d10a0e6635ecc25e75446a85bb695f44ccaf35ca809a"
     sha256 cellar: :any,                 arm64_big_sur:  "fed357436c36aa832c46cad896d75a9b3f0015658732af9cad3a18b19769ea72"
+    sha256 cellar: :any,                 ventura:        "04daf708d2f8327ba0ae42652d4b6d332fd560cdcf6c7ac9b09140bd0ced8a67"
     sha256 cellar: :any,                 monterey:       "a94da8b984b2f2f08057324d812b03bbd8108f541b409f481e4ea41d4323df30"
     sha256 cellar: :any,                 big_sur:        "9580a442aa0843b284317be696caa8742165a1574d20e8398c9fadbdfc426dc6"
     sha256 cellar: :any,                 catalina:       "1a1690918045f775ea6d71216a5b674b5762556aeaf0285e70533150aa7f14b6"
