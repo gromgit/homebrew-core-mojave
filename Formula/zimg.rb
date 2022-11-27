4953bb876c7ff5f9e5ec0e4726170dc8e4bb82f4
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:09:26 +0800

    zimg:

--- Formula/zimg.rb
+++ Formula/zimg.rb
@@ -7,8 +7,14 @@ class Zimg < Formula
   head "https://github.com/sekrit-twc/zimg.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/zimg"
-    sha256 cellar: :any, mojave: "ff06be4e2acf12f6c2360a94920e0071c18b7e405a79f76a9f009e339c11aa39"
+    sha256 cellar: :any,                 arm64_ventura:  "67bf243cac048e9834cabe798291937c500bb79406932a438ac1dc02fbd648a3"
+    sha256 cellar: :any,                 arm64_monterey: "afc67759e761ce7569c11a844f4af29802880ed9ac955b08a2bd7cfc56133c9b"
+    sha256 cellar: :any,                 arm64_big_sur:  "40efe797c81967168a03455886ae5bca77f8fbd887ad852bf6075e9cbea163a5"
+    sha256 cellar: :any,                 ventura:        "e830c58c7a65075dbbcf439fc7d2c0d621669ff1781678f7d382acdeb94ce22c"
+    sha256 cellar: :any,                 monterey:       "67e43a8f648f630798a701cfce4dabe306c9fed320e272ee7e3108971bcdaf80"
+    sha256 cellar: :any,                 big_sur:        "9c106b35b00ef8c6dc27e928d6ab407dc068a86423c246c0fbaf28b7c32ffbad"
+    sha256 cellar: :any,                 catalina:       "672bca8794463c7d4debe5cbac9ae5c7b5cd8fc57553f7bcf8a8092603d367b6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8e46696be2a61e5ee59177aa5f5d7f13944341a7244cd07be9fe7fb7aabfce65"
   end
 
   depends_on "autoconf" => :build
