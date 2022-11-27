15d1ff7f355270ab50ae19da30dfd56bfd4e6a0d
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:54:15 +0800

    psutils: no rebuilding required

--- Formula/psutils.rb
+++ Formula/psutils.rb
@@ -23,6 +23,7 @@ class Psutils < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "bb268fd6eb5dd600ae0c81387e8a88a074f68a1ffde072a3e2ac387e142449e4"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8d31594cd841f9c3fab5b2d57b7562f27614e91b88c0021f2d6132d4100f3133"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "02cd6e56f1a40d01069ee8d59ceafdab15e0c9ec6c75873f845f2588df87d31c"
+    sha256 cellar: :any_skip_relocation, ventura:        "8046101881e3fb43865364afc706a0b75283f83fc312414d8f9b5a2cdaa8fbb1"
     sha256 cellar: :any_skip_relocation, monterey:       "5531ac88d24275129272f8e0c14f185ff06cd114f4c530624d1d436bd4e4df54"
     sha256 cellar: :any_skip_relocation, big_sur:        "229bde3f399638b21570063c1586fce976f4498475901f28bce30546a4e60220"
     sha256 cellar: :any_skip_relocation, catalina:       "c2aed2811e263c3e3abcf66eb27d6fdd1b622ca033fa2e3bf4e8095c733df08a"
