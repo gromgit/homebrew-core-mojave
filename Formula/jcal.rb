c7f4dc73610490ec6e35c2c367dec5ac3dd1588c
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:35:24 +0800

    jcal: no rebuilding required

--- Formula/jcal.rb
+++ Formula/jcal.rb
@@ -11,8 +11,10 @@ class Jcal < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "199711a3aa65d8d6b5d7ae804e99f0025ad8eb59c77f37d4058566edc6c0d1eb"
     sha256 cellar: :any,                 arm64_monterey: "6f1986d499d27fd07525390066318239e9efdac990a58578ef3fe2147d32563b"
     sha256 cellar: :any,                 arm64_big_sur:  "6995c49236be96cf2adcf11cd03a88f46436ca66061de24d087c1c69aa4b9f6c"
+    sha256 cellar: :any,                 ventura:        "feac90459a79da88e1fc37a090d811e5c76a43cae3f1fc6075c2da8e6a3e3fcb"
     sha256 cellar: :any,                 monterey:       "2ebbf3f00eaedd9e2a539af5eab274bc5a1417cf3e7d44d889272bef86c83a79"
     sha256 cellar: :any,                 big_sur:        "00a9eec192b14b6b4a442e1268bd7727df19923901d36ca225a32e69477df5de"
     sha256 cellar: :any,                 catalina:       "0544ee162b480d5999a312cf721b40007901f964b20edbdd8e062b2e95c64157"
