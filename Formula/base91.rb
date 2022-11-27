952cd3081bf674765c5e4e36895700fa5b92f71e
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:14:59 +0800

    base91: no rebuilding required

--- Formula/base91.rb
+++ Formula/base91.rb
@@ -6,8 +6,10 @@ class Base91 < Formula
   license "BSD-3-Clause"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "67a2d373e37965d8f4af56c50d704340f2a85efd2563a1801e8b61eeaa1d8755"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "cd1312d937857681b5c54a9d135b0e7b37b934fe8c3ef4be414222a876904dce"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f97be9aed077e34c2aaf8aac0157fae476dedf2cf02eae3b23de8a74541e8135"
+    sha256 cellar: :any_skip_relocation, ventura:        "17bcc913dc97e5e52dbdd67aae0e622eea1593adbe692adc584c888c754577e7"
     sha256 cellar: :any_skip_relocation, monterey:       "2580639cbba0238bdf477650b1759dcb51d328fcdc9375ebbc031b355cb0ed23"
     sha256 cellar: :any_skip_relocation, big_sur:        "039bcf75c09fb75a7472e8f92ef349f2908073b4ac76c1c4573d1a393e248229"
     sha256 cellar: :any_skip_relocation, catalina:       "239a9f51f67e7b1de09d10c838dffd2c34ce0f6f58641269e2278d6478b36542"
