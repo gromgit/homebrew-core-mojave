985abb66ebd711843d60bf2c8e2a8f72cc99afaa
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:59:03 +0800

    simple-obfs: no rebuilding required

--- Formula/simple-obfs.rb
+++ Formula/simple-obfs.rb
@@ -11,6 +11,7 @@ class SimpleObfs < Formula
     sha256 cellar: :any,                 arm64_ventura:  "3ede7782d7c846a3f01749e297231356e89af0cd4650d9bf4b62d64fe0e513fc"
     sha256 cellar: :any,                 arm64_monterey: "6671d16d00fb419e65ef7a686d4fa956e64882efcf22ee7ccf84207ebb7a4fe7"
     sha256 cellar: :any,                 arm64_big_sur:  "a79c2a9ba2a6473853a0907355290287b5f9faf4dcd808e1819608f0498b236f"
+    sha256 cellar: :any,                 ventura:        "8dd7246903c8398049305665a51c44299358f80164d8dd3264e98ff7699ee704"
     sha256 cellar: :any,                 monterey:       "83daaaeccbb621e796269d648c943871ce9ddbf85edd0d7ec66685ea71e9c286"
     sha256 cellar: :any,                 big_sur:        "6fc8b9d2aef7c813449e5298e1fcbcb7e5f1ba6da4c34b9e4b3c3a5e0005110e"
     sha256 cellar: :any,                 catalina:       "64ac7bb71b3dd0a0d087d7f981c53516abfb294f709d84cb969b192456310c51"
