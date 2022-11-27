8e3a50f4a74855c1dd27bdb109fdc64ba4440bb6
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:06:43 +0800

    wait_on: no rebuilding required

--- Formula/wait_on.rb
+++ Formula/wait_on.rb
@@ -6,6 +6,7 @@ class WaitOn < Formula
   sha256 "d7f40655f5c11e882890340826d1163050e2748de66b292c15b10d32feb6490f"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2d6736732caacfe89a285f06256c397fefb2e47f11032f282d84e874b384fd21"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "5d7664213ff9c0136cb6e8add7a2bfc87f2bf316594f9305e53a287834b12c72"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ca26e2aeef0c80f7b61b392ea50b03f7465bcdc0c8c3be2248aca538935fdc78"
     sha256 cellar: :any_skip_relocation, monterey:       "abc2d21a1add07205d46997d869de4a896bd47174033c0e392e74c17813251b1"
