fa06f783944ede23285c7435c78f98862154705b
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:17:30 +0800

    cbmbasic: no rebuilding required

--- Formula/cbmbasic.rb
+++ Formula/cbmbasic.rb
@@ -10,6 +10,7 @@ class Cbmbasic < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "8382a3a84c232895e8581de79435c6c7629079ec8dbdec6b8d193f73715e4ca8"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4e090b459f2e44ad8f04e3e70a25b909fe16771f4b2fa325cc06ea233b019803"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b693c2b8fbfe49736bdc0ae4bce13d96295da75a6683e593c021c9335f6c57fd"
+    sha256 cellar: :any_skip_relocation, ventura:        "032eb3fb2490ed7ae7d4017bffd81e1052fbae0929427a6efc55cb2e612dae18"
     sha256 cellar: :any_skip_relocation, monterey:       "1328d1be681fd3f2be33cbab9b19e59a71e1ed0a5191c89bf7595ebcf0ef3236"
     sha256 cellar: :any_skip_relocation, big_sur:        "29f1eb35e6acf1bf907d2c89c0f5938507718b290cdeef92dfcee473b00f8fe5"
     sha256 cellar: :any_skip_relocation, catalina:       "f4e101b38bb21ff46ce301f2c9a0f59f567df9a3265c4906969f1e4426160d9c"
