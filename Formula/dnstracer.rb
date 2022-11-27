d95584c77e1e0b4f92d51767b5cc72b484428f53
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:22:45 +0800

    dnstracer: no rebuilding required

--- Formula/dnstracer.rb
+++ Formula/dnstracer.rb
@@ -16,6 +16,7 @@ class Dnstracer < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c90d0674735e546310ec3e6052242171258f29faf127138282ea4e396946d737"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "07ec9123818d074dcbd378fc46fcf0bf17557f1f4ceccb631aef4669af57373d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dc60fdb473c11615a27f62abb5aba5ae0a270c4b3d33001c07668266254c7486"
     sha256 cellar: :any_skip_relocation, monterey:       "3d7210cdf69f4ebae90e2a1b65729dce288b4dab03a921d64d521ac9e137dfd1"
