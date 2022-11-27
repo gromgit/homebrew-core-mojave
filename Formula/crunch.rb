4952c8e0be61e95f6245b05fabc9d140cd46cac7
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:20:42 +0800

    crunch: no rebuilding required

--- Formula/crunch.rb
+++ Formula/crunch.rb
@@ -10,6 +10,7 @@ class Crunch < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "7012aebe6b421760ccec1550bb6ca1684f5808d8b7f4aa4b31a939f4a50fcf58"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f7f96f0cb4350722c3b04aca1d7d0d9a94b9084649888216d1ec4f089809fe8c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dfc7c5b56963d28a58c64a2a28d0c19b61dfce141b83c9facaad0d8d606949b2"
+    sha256 cellar: :any_skip_relocation, ventura:        "21786261bea2b66d97b4f2b089929175cf79690491b1d45768900cb9384f69b0"
     sha256 cellar: :any_skip_relocation, monterey:       "7797dae15adc8701e4d93c3a0455adb8796aca9f7e366bfa12448ce9aeac7153"
     sha256 cellar: :any_skip_relocation, big_sur:        "9cd9d5ae5afb6b6223e720b99d30990f1884cd8ed4e0b5654a9ab2d72cc4d132"
     sha256 cellar: :any_skip_relocation, catalina:       "67570938790b20aaabcb31c8ac86d4356702b87ce2ae8ea01d19553f531397a6"
