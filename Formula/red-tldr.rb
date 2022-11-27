d1d8c6965f0c4f15e106fe137e017fc83e871165
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:56:03 +0800

    red-tldr: no rebuilding required

--- Formula/red-tldr.rb
+++ Formula/red-tldr.rb
@@ -10,6 +10,7 @@ class RedTldr < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "2fc1ecafec88eae6a750b9813084c58cedffbf15cfa8a5c1c4d8223783f9b410"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "2f63431f0038bac796d94604322236db7f31b15759ccf3b1874597dc215a74ad"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bd91dc84b16cc8c51e243b6808f428e94ff4ba2b63165e7adf5563b5b1f9171c"
+    sha256 cellar: :any_skip_relocation, ventura:        "ba21a9519a354aa85fc24bfcb6bed7233ce46d2194e28d9bf49062ded1e0057d"
     sha256 cellar: :any_skip_relocation, monterey:       "a93d3617f3167e69b45961b876ea5ea4dfbc60b080d143e2ca95870089cb24ab"
     sha256 cellar: :any_skip_relocation, big_sur:        "7052eff293193940046fd1dc13c439fa81fcca5e1d7b71d9ea81e060f31284bd"
     sha256 cellar: :any_skip_relocation, catalina:       "b55c620a9eae179704c06f4cbb6421b6a4dc8fe4ffe6771f1e15d538ec0eaf92"
