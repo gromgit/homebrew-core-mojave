115807c92f1f750bff0b49ce69cce82db681acec
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:48:05 +0800

    nfcutils: no rebuilding required

--- Formula/nfcutils.rb
+++ Formula/nfcutils.rb
@@ -10,6 +10,7 @@ class Nfcutils < Formula
     sha256 cellar: :any,                 arm64_ventura:  "5f52daea8355a598eb9226fb170019d92e831ad54dbc11eea3473a42ad34262c"
     sha256 cellar: :any,                 arm64_monterey: "d502000cd5655c0ec554e4789b846582cc8421bf69a79d1df7ffaeb0f497af9c"
     sha256 cellar: :any,                 arm64_big_sur:  "257b8265cf3e136dd2a11c3b26b37f31cc3de371d97401a5fadaf1681330fbd8"
+    sha256 cellar: :any,                 ventura:        "a2675c309347279b2a0373bea9335e99bdb45973c53c95420f120a8e168114b8"
     sha256 cellar: :any,                 monterey:       "835a1d70f054d1eb3a05947f085331948024d3b2daa75dd7fd8e46e2ec84f9e0"
     sha256 cellar: :any,                 big_sur:        "ae40ef6e8f1d98d6fc6114893715c713c28e0747a5c5a84779c89726970f8a95"
     sha256 cellar: :any,                 catalina:       "963e5bf77bc285e81b9f7480f8b0362c73e5138bced77608043742df6e0992cd"
