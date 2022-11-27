7b73239bbc7aa5b40ebe9b32d89a0a0210286d70
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:45:44 +0800

    mksh: no rebuilding required

--- Formula/mksh.rb
+++ Formula/mksh.rb
@@ -12,8 +12,10 @@ class Mksh < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7f8fa2427f871cd4e4e7143244f5862988cb72e0a264c0b947c4f2f72d3655d8"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c9467abce8ddfd5d5dbdfda62ab8b36d020c92ae4c06805ecafd9973e20f4307"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4e5b153d2643f455c5be25fa8f751eed863393b454abc3df8bb9b145799152e0"
+    sha256 cellar: :any_skip_relocation, ventura:        "410441bef1f6b4cbe36c35ec7fad198fcdbeae06a37e65c4a95471dcaa52bf26"
     sha256 cellar: :any_skip_relocation, monterey:       "df3d31ae543c8f8008eba65b86ac9090f21ca2f365df41e0c9984f93f1372df0"
     sha256 cellar: :any_skip_relocation, big_sur:        "879b0a3185d7cb75235a3204cba1b66e7353b77458b63deef3c7635e75f52ba4"
     sha256 cellar: :any_skip_relocation, catalina:       "ab6ffddb634219464c5993a9109a051fa728f217b7c89daa95d5b85748127bf5"
