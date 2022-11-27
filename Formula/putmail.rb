3a9b997877a509c6be81b55ae06134cc39ec2a04
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:54:26 +0800

    putmail: no rebuilding required

--- Formula/putmail.rb
+++ Formula/putmail.rb
@@ -3,6 +3,7 @@ class Putmail < Formula
   homepage "https://putmail.sourceforge.io"
   url "https://downloads.sourceforge.net/project/putmail/putmail.py/1.4/putmail.py-1.4.tar.bz2"
   sha256 "1f4e6f33496100ad89b8f029621fb78ab2f80258994c7cd8687fd46730df45be"
+  license "ICU"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "1df81ef382665ba394fb3ee99383dd7662a776290f9efdb0a2c7765ce3a663e4"
