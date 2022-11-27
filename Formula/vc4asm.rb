e015e64c7cbae4354d7e0ae99f096810776674d8
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:05:48 +0800

    vc4asm:

--- Formula/vc4asm.rb
+++ Formula/vc4asm.rb
@@ -5,8 +5,10 @@ class Vc4asm < Formula
   sha256 "f712fb27eb1b7d46b75db298fd50bb62905ccbdd7c0c7d27728596c496f031c2"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "49337c5ba68c5868f9804568af0ab0fc29223eee04b6f50154fa30ef49fb23f1"
     sha256 cellar: :any,                 arm64_monterey: "af417dc649bc49003da98b9756e05da700076907cc4963818b839b817f721bc5"
     sha256 cellar: :any,                 arm64_big_sur:  "64082d8d1fd7f2a360f9ffdd29a9fbde0a24b600f38806bc4556e4ea9b5175bc"
+    sha256 cellar: :any,                 ventura:        "3962b9835cebd3238c331effab584026a12adfec4be373c174e7f7575a9e8ff7"
     sha256 cellar: :any,                 monterey:       "1a80a7ddd01b91da357ecae8507af244f0dde770275e36ee523b0da7c0f489f4"
     sha256 cellar: :any,                 big_sur:        "1eefbd03ec375b8021eb783af2fdf5e343c5548201eddbb29e6cf9b6db47e80c"
     sha256 cellar: :any,                 catalina:       "cd4f683e1e968cb0577b7e6d9b054c503719b10f9bd37442feb8481a19d75fd7"
