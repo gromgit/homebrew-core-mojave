1b75fdd4a4062c84f0ad8466cc7e23d4cbdeadd2
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:36:12 +0800

    jthread: no rebuilding required

--- Formula/jthread.rb
+++ Formula/jthread.rb
@@ -13,6 +13,7 @@ class Jthread < Formula
     sha256 cellar: :any,                 arm64_ventura:  "1f6f395e12547fcfcafcfcf52501dce17024aacb8fcb8a2270e42595ac5a80c2"
     sha256 cellar: :any,                 arm64_monterey: "7a786a2608afa79835cab95860405402c716916bb2f79b5e562c838269e178b4"
     sha256 cellar: :any,                 arm64_big_sur:  "12a85b410fa6b4c3e47e518813e0907b09ea01ed917ecb39354488ba1afb8ee8"
+    sha256 cellar: :any,                 ventura:        "0e4540078fce3d303b3a5cc1aa147f1eacb41367d39fdbeccd15e8d9c125d86f"
     sha256 cellar: :any,                 monterey:       "9c27b5547869cf439f7d5fa99b8bc7de3931f3ea73d113e14d1ad013dbb189d8"
     sha256 cellar: :any,                 big_sur:        "8932e35ce2fd13b2ba082af71db656adc9c9413280b279067773ceea8542dc3b"
     sha256 cellar: :any,                 catalina:       "e228f81df252c35872df1c6e0711857ad7a7312aae17304a7bcefa0905106b61"
