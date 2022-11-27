4b747d21c4cc1c2f4a13ddc284bf19337c26e5b1
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:22:23 +0800

    diffutils: no rebuilding required

--- Formula/diffutils.rb
+++ Formula/diffutils.rb
@@ -10,6 +10,7 @@ class Diffutils < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "db92359b7a7cc8eacdc1b5998df69cfa24bdaa54e02ae06f4a10bdb339c190f8"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4261be9ec928e4e841efd205eef060dc8a536b033c7d4377eae6e51f099b9eb2"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "239aaf4a4b3e63ade873472f3442e6a910130f1f999b494ee02fa6acffa11c0b"
+    sha256 cellar: :any_skip_relocation, ventura:        "7483b137cb5e88f2c41cc1dbe62ac952f9998a6b8945b774836eac4fe7129d14"
     sha256 cellar: :any_skip_relocation, monterey:       "64ddb0cccbc7163969ac093ca0251053991af34393188d3457695a358a0f3034"
     sha256 cellar: :any_skip_relocation, big_sur:        "43ed975b1f8cd9c8aedc16848691972950c2c95405395bc646650fbf8e3d60c5"
     sha256 cellar: :any_skip_relocation, catalina:       "c0a2132f021243dc25d19e6638eea2a423e09957d2c6c11582fc134301fffefd"
