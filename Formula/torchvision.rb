b1da7f088dcf3b36d36e9b3c1e746b30a0b55da3
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:03:46 +0800

    torchvision: no rebuilding required

--- Formula/torchvision.rb
+++ Formula/torchvision.rb
@@ -1,8 +1,10 @@
 class Torchvision < Formula
+  include Language::Python::Virtualenv
+
   desc "Datasets, transforms, and models for computer vision"
   homepage "https://github.com/pytorch/vision"
-  url "https://github.com/pytorch/vision/archive/refs/tags/v0.13.1.tar.gz"
-  sha256 "c32fab734e62c7744dadeb82f7510ff58cc3bca1189d17b16aa99b08afc42249"
+  url "https://github.com/pytorch/vision/archive/refs/tags/v0.14.0.tar.gz"
+  sha256 "be1621c85c56eb40537cb74e6ec5d8e58ed8b69f8374a58bcb6ec413cb540c8b"
   license "BSD-3-Clause"
 
   livecheck do
@@ -11,27 +13,77 @@ class Torchvision < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/torchvision"
-    sha256 cellar: :any, mojave: "18744c88f7b034e1eab2048931502b6dcafd033e30ff4dfca5a440698cc9b4e9"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "05e505a4b1f096f7fb43ef54108f02e70245267f2a98622fac536e759ceafdc6"
+    sha256 cellar: :any,                 arm64_monterey: "85dadcd30747d1545d423e5382be022f08b4cbe64d4792e51ac8be2b2a6bfe65"
+    sha256 cellar: :any,                 arm64_big_sur:  "74c3a9b9996b6f33a0dac17c3fc00448427849f05d3b3af96e7c90be3ffb28b3"
+    sha256 cellar: :any,                 ventura:        "2eff3282b96c376e89b839a03583528e215d211eda31cfb3830c938a8c9d1333"
+    sha256 cellar: :any,                 monterey:       "6c0e377f96b59173b241a959d09ffbf1a18b2d65108488e670f6cd950e6d4c7c"
+    sha256 cellar: :any,                 big_sur:        "818ed82d4d092f8b868a5dc330566d4d2bdca3692177d99d1e36ce0f3e8e5ffe"
+    sha256 cellar: :any,                 catalina:       "fb7b87345a559aca821f60d053990a4cc5015ce949b4530daf2a3b662db44577"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "4bb6e9a517f01141547e5d84053dc90cbe8fcdbb8837cff0cb3adf213300a5ef"
   end
 
   depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "python@3.11" => [:build, :test]
   depends_on "jpeg-turbo"
   depends_on "libpng"
-  depends_on "libtorch"
+  depends_on "numpy"
+  depends_on "pillow"
+  depends_on "python-typing-extensions"
+  depends_on "pytorch"
 
   on_macos do
     depends_on "libomp"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/cb/a4/7de7cd59e429bd0ee6521ba58a75adaec136d32f91a761b28a11d8088d44/certifi-2022.9.24.tar.gz"
+    sha256 "0d9c601124e5a6ba9712dbc60d9c53c21e34f5f641fe83002317394311bdce14"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/a1/34/44964211e5410b051e4b8d2869c470ae8a68ae274953b1c7de6d98bbcf94/charset-normalizer-2.1.1.tar.gz"
+    sha256 "5a3d016c7c547f69d6f81fb0db9449ce888b418b5b9952cc5e6e66843e9dd845"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/8b/e1/43beb3d38dba6cb420cefa297822eac205a277ab43e5ba5d5c46faf96438/idna-3.4.tar.gz"
+    sha256 "814f528e8dead7d329833b91c5faa87d60bf71824cd12a7530b5526063d02cb4"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/a5/61/a867851fd5ab77277495a8709ddda0861b28163c4613b011bc00228cc724/requests-2.28.1.tar.gz"
+    sha256 "7c5599b102feddaa661c826c56ab4fee28bfd17f5abca1ebbe3e7f19d7c97983"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/b2/56/d87d6d3c4121c0bcec116919350ca05dc3afd2eeb7dc88d07e8083f8ea94/urllib3-1.26.12.tar.gz"
+    sha256 "3fa96cf423e6987997fc326ae8df396db2a8b7c667747d47ddd8ecba91f4a74e"
   end
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
+
+    jpeg = Formula["jpeg-turbo"]
+    inreplace "setup.py",
+      "(jpeg_found, jpeg_conda, jpeg_include, jpeg_lib) = find_library(\"jpeglib\", vision_include)",
+      "(jpeg_found, jpeg_conda, jpeg_include, jpeg_lib) = (True, False, \"#{jpeg.include}\", \"#{jpeg.lib}\")"
+
+    inreplace "pyproject.toml",
+      'requires = ["setuptools", "torch", "wheel"]',
+      'requires = ["setuptools", "wheel"]'
+
+    virtualenv_install_with_resources using: "python@3.11"
+
     pkgshare.install "examples"
   end
 
   test do
+    # test that C++ libraries are available
     (testpath/"test.cpp").write <<~EOS
       #include <assert.h>
       #include <torch/script.h>
@@ -43,7 +95,7 @@ class Torchvision < Formula
         assert(ops.size() == 1);
       }
     EOS
-    libtorch = Formula["libtorch"]
+    pytorch = Formula["pytorch"]
     openmp_flags = if OS.mac?
       libomp = Formula["libomp"]
       %W[
@@ -55,11 +107,20 @@ class Torchvision < Formula
       %w[-fopenmp]
     end
     system ENV.cxx, "-std=c++14", "test.cpp", "-o", "test", *openmp_flags,
-                    "-I#{libtorch.opt_include}",
-                    "-I#{libtorch.opt_include}/torch/csrc/api/include",
-                    "-L#{libtorch.opt_lib}", "-ltorch", "-ltorch_cpu", "-lc10",
+                    "-I#{pytorch.opt_include}",
+                    "-I#{pytorch.opt_include}/torch/csrc/api/include",
+                    "-L#{pytorch.opt_lib}", "-ltorch", "-ltorch_cpu", "-lc10",
                     "-L#{lib}", "-ltorchvision"
 
     system "./test"
+
+    # test that the `torchvision` Python module is available
+    cp test_fixtures("test.png"), "test.png"
+    system libexec/"bin/python", "-c", <<~EOS
+      import torch
+      import torchvision
+      t = torchvision.io.read_image("test.png")
+      assert isinstance(t, torch.Tensor)
+    EOS
   end
 end
