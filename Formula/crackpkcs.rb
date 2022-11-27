06d475fea3546474b948418337a910b5c7c1572e
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:20:31 +0800

    crackpkcs: no rebuilding required

--- Formula/crackpkcs.rb
+++ Formula/crackpkcs.rb
@@ -9,6 +9,7 @@ class Crackpkcs < Formula
     sha256 cellar: :any,                 arm64_ventura:  "941c264ba6fed6fb691c2bae290d4a47bf68b62d8fbbbf3cd3f5767ad6b437c7"
     sha256 cellar: :any,                 arm64_monterey: "531a0ea5e420e1a5d8a33efc3f3f027bed2125c1bb00aea565bd2bdde220d677"
     sha256 cellar: :any,                 arm64_big_sur:  "c78cddbd0a61219de5e403a5cf43c710a5a50141ccb3c66767823f0fb8941a70"
+    sha256 cellar: :any,                 ventura:        "8042cedd03a5382d67488238ddcce502b6a38cc2a3f2e101f9f8ad819051d4d8"
     sha256 cellar: :any,                 monterey:       "cf977d20a83190062068fb6c57d631761af2f8a1cf8985de088abc61db757384"
     sha256 cellar: :any,                 big_sur:        "6616b50b8bdc80f28a69184fe91d3c191c39f8259f539cab29009c65a79d2a99"
     sha256 cellar: :any,                 catalina:       "32c0e6b6e178c8afd6cf5e98807d1f165a5fccf41d2120f57036653fef1952c7"
