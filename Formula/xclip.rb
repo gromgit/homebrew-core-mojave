c6a80097889b2ed9d3b22280430ba6754c87e1b7
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:08:07 +0800

    xclip:

--- Formula/xclip.rb
+++ Formula/xclip.rb
@@ -10,6 +10,7 @@ class Xclip < Formula
     sha256 cellar: :any,                 arm64_ventura:  "e335771d3bb8f072b25c2cf6404f12540cfbd8692d97cd678a086a08b76f2771"
     sha256 cellar: :any,                 arm64_monterey: "ebf047556b2d594089b26ea72250eb9ea3c4a4c1f779642e08d1cfd0d131f631"
     sha256 cellar: :any,                 arm64_big_sur:  "2a9e42621fbc329856454f299e2da20b8776de9136cf1233a97ec4662ef2b5fe"
+    sha256 cellar: :any,                 ventura:        "b1641761e78f6f9ca7d89ca8999e8ad04b2116ca28cee87d0f1d609b50fce4b5"
     sha256 cellar: :any,                 monterey:       "47b1812c7d1e2aa7f70f2721693b3e2ddb89761886e4432009240d4349369da0"
     sha256 cellar: :any,                 big_sur:        "4b3d034f8770dd75585b98910ce1ad1c0bbe010f91f61c814f9b655cc978e122"
     sha256 cellar: :any,                 catalina:       "2229de2d3139a5a916be1d7e6c3227ef989ff20ce4322f0881eaeb22ee34caf1"
