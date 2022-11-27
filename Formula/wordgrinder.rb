a8b6a9f6ff5fe663f39a673ba6ce3e7953b3a97e
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:07:33 +0800

    wordgrinder: no rebuilding required

--- Formula/wordgrinder.rb
+++ Formula/wordgrinder.rb
@@ -8,6 +8,7 @@ class Wordgrinder < Formula
   head "https://github.com/davidgiven/wordgrinder.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "b89498bd5c54678e5460ccf146084abdde90853f465f17775657298fe1ba5c91"
     sha256 cellar: :any,                 arm64_monterey: "3eb4bf8cff526d9a6e6c9e285ba2a63879eb157a6ba091dff6be7ad49da749b3"
     sha256 cellar: :any,                 arm64_big_sur:  "370093b3705f72a5d6b87bacd2e64e229f3d6ac82e52e92fe147c037d65f210b"
     sha256 cellar: :any,                 monterey:       "2ab17d2541132790b1b134a5d33e5e9178ce96b7afeadf28819a9694f87712da"
