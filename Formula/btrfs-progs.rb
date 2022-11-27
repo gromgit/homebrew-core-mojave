b0ab2d1c301594abad7739e1ab56f63e42e9fa52
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:16:33 +0800

    btrfs-progs:

---
+++ Formula/btrfs-progs.rb
@@ -0,0 +1,56 @@
+class BtrfsProgs < Formula
+  desc "Userspace utilities to manage btrfs filesystems"
+  homepage "https://btrfs.wiki.kernel.org/index.php/Main_Page"
+  url "https://mirrors.edge.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v6.0.2.tar.xz"
+  sha256 "6665863049d945fc32ccdacc55a7306767aa8f640f3b8b1fa5e0568a39ae7018"
+  license all_of: [
+    "GPL-2.0-only",
+    "LGPL-2.1-or-later", # libbtrfsutil
+  ]
+
+  livecheck do
+    url "https://mirrors.edge.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs/"
+    regex(/href=.*?btrfs-progs[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "22b904b30357b9f7453826f4b042fb3032ff0bef156d81415c9a65a91c984c2c"
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "sphinx-doc" => :build
+  depends_on "e2fsprogs"
+  depends_on :linux
+  depends_on "lzo"
+  depends_on "systemd" # for libudev
+  depends_on "util-linux"
+  depends_on "zlib"
+  depends_on "zstd"
+
+  def install
+    system "./configure", "--disable-python", *std_configure_args
+    # Override `udevdir` since Homebrew's `pkg-config udev --variable=udevdir` output
+    # is #{Formula["systemd"].lib}/udev. This path is used to install udev rules.
+    system "make", "install", "V=1", "udevdir=#{lib}/udev"
+    bash_completion.install "btrfs-completion" => "btrfs"
+
+    # We don't use the make target `install_python` due to Homebrew's prefix scheme patch
+    cd "libbtrfsutil/python" do
+      python3 = "python3.11"
+      system python3, *Language::Python.setup_install_args(prefix, python3)
+    end
+  end
+
+  test do
+    device = testpath/"test.img"
+    system "truncate", "-s", "128M", device
+
+    output = shell_output("#{bin}/mkfs.btrfs #{device}")
+    assert_match(/Filesystem size:\s*128\.00MiB/, output)
+    output = shell_output("#{bin}/btrfs filesystem show #{device}")
+    assert_match "Total devices 1 FS bytes used 144.00KiB", output
+
+    system "python3.11", "-c", "import btrfsutil"
+  end
+end
