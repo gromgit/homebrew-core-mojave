33dc84fe9ccc93e107591a993fd021b0bbb17e43
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:44:23 +0800

    masscan: no rebuilding required

--- Formula/masscan.rb
+++ Formula/masscan.rb
@@ -15,6 +15,7 @@ class Masscan < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "d67db10c93bcd8154956c4165289fbc04edce1b1d63a65af983150d9fafbcf43"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ba6a70814b1e311a2b817fd79e7d9a70657ceb74be1691215802a4470ca3be87"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b99bf991011be4ee7d76fe43aa000159f0665b888a0cbc7c4d528d102a3daa67"
+    sha256 cellar: :any_skip_relocation, ventura:        "ff1b10ddec63626516bc7ae647b67524f975d77a52d1dbf93df9c32a09fe77c3"
     sha256 cellar: :any_skip_relocation, monterey:       "80601cda78b927edb63ae9e0a6b15bb9aa7d621b793d7a6cfa094a0465e66070"
     sha256 cellar: :any_skip_relocation, big_sur:        "8d21dd16d333a573d7146d13c31dea07df5c72fcfe137af338e6f7722b393dbe"
     sha256 cellar: :any_skip_relocation, catalina:       "a77ea3fd36501d9a0d0398e585f1d30fd64163ca378e6af9660601a10e1ddce3"
