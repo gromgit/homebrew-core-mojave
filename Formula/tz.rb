4a94d14e42049246180f72c33e2dcd41914b0794
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:04:42 +0800

    tz: no rebuilding required

--- Formula/tz.rb
+++ Formula/tz.rb
@@ -10,6 +10,7 @@ class Tz < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "7c7b5d50f3a4932dd5938d575c745556751e0bdb422365e81b8940fd8095468e"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ffcc6e47c89080907b571031d530046fe710d4c74c45183a8981c6c67ed3515a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ffcc6e47c89080907b571031d530046fe710d4c74c45183a8981c6c67ed3515a"
+    sha256 cellar: :any_skip_relocation, ventura:        "d5cf2912da3e2e77cf17c1c6fecffb8712756b0ac02618035963bab34059aabb"
     sha256 cellar: :any_skip_relocation, monterey:       "2ce8046b150966b4e05f0f13255f17d6589650331578256f6f6f41ec25823f81"
     sha256 cellar: :any_skip_relocation, big_sur:        "2ce8046b150966b4e05f0f13255f17d6589650331578256f6f6f41ec25823f81"
     sha256 cellar: :any_skip_relocation, catalina:       "2ce8046b150966b4e05f0f13255f17d6589650331578256f6f6f41ec25823f81"
