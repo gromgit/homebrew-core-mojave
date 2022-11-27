ee56cc5450eefdcf6b4e9f16517c015d88eabbe5
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:30:29 +0800

    gowsdl: no rebuilding required

--- Formula/gowsdl.rb
+++ Formula/gowsdl.rb
@@ -11,6 +11,7 @@ class Gowsdl < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "4aa45e1319557557ca74011ec372ffa4d404a3d4199d0b2c7ae78480b68cf266"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "0391c1e4a9fdbd2f14d5daff2789d490802da3b4a09a227d021dcb1a58b2f92a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1e00fdcd6cf4d9f6b4d8df22fbd7622ec946b4188944e4f5fb04aed3c29878f4"
+    sha256 cellar: :any_skip_relocation, ventura:        "03fd4af0ea805054cf29bc1156533b8540de308889d827459da3809f65cb6335"
     sha256 cellar: :any_skip_relocation, monterey:       "c810ea3ef7b3a12aeb84b71b71c0c67f6853857cd6fd778c65b9bb0b9fad157e"
     sha256 cellar: :any_skip_relocation, big_sur:        "311386c63c7c7ffda0e89c0cf753b5ce53745909e492b2866f75c157c97e48a8"
     sha256 cellar: :any_skip_relocation, catalina:       "5decccb2c1f5b9c093dc045fceede062ea21bfb0273c7cfa9d7c1cde04229e4c"
