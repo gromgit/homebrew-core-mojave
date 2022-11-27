bd7bbaf9e108acd6286c984bcdae5e7869ce5f35
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:01:36 +0800

    switchaudio-osx: no rebuilding required

--- Formula/switchaudio-osx.rb
+++ Formula/switchaudio-osx.rb
@@ -10,6 +10,7 @@ class SwitchaudioOsx < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "c33e14ec4b34a9514c06ab392b36c2f5fcf579aee16fed0fb02fc3a4b935ee27"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "a6bf6a7e8873d101da1d5f24be8a8c3470af13f56ed21de847685ebf9e6efad8"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "631619e7f83da181c8287e6795de8d54c03cab15bf29ba4ce5b5c0fb1f3aafcb"
+    sha256 cellar: :any_skip_relocation, ventura:        "9ad594e05d7bee40a400641d34a06c59fcd2483f6e588ff98ac7faec379181a1"
     sha256 cellar: :any_skip_relocation, monterey:       "7d94bc90f9d9d0362c8fc36634170bd3e433dfd5a61b8bad3243205f4af81bec"
     sha256 cellar: :any_skip_relocation, big_sur:        "4e6b47d1a51d71706f160ed4f19cc6fe6cacd1370eab298c029976ae6c32484e"
     sha256 cellar: :any_skip_relocation, catalina:       "59e92aaf09b86e49b6bd5f4300db02cf0812118e08146eaa2c540cb8e34b1a6b"
