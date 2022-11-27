6449c0de1d347da3cfb9b558c637b2ed0b20e79a
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:14:32 +0800

    aws-es-proxy: no rebuilding required

--- Formula/aws-es-proxy.rb
+++ Formula/aws-es-proxy.rb
@@ -6,8 +6,10 @@ class AwsEsProxy < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "6027f93eaf468cef549261cb4ab8d9605fbc7eb039f00d387a066c958e549692"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "b053918e93c51c2b3a562dc30cfbcf30f07f2c10b841b5c61ab146595920368d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9ae9e19bb22445be989da3b8407bc42fba17a3f512d692bd8d727751b1703757"
+    sha256 cellar: :any_skip_relocation, ventura:        "ba6e4eb41e1319edc2fb59dce4c0e9ac41a3ee461fac5240fc1bd3e98b66ed21"
     sha256 cellar: :any_skip_relocation, monterey:       "d6b34390ba856f75db3adf881e2659bf48c6d420abe8d4de1226e59c607e0a41"
     sha256 cellar: :any_skip_relocation, big_sur:        "5d172bf29028041152acbd6635aee845193fc19f0b8d4e086ed4a28ee9354a37"
     sha256 cellar: :any_skip_relocation, catalina:       "1e1cb5b16185e9948621055c4960b608973110c5d68ab10cc07c61f52d456010"
