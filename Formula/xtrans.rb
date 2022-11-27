11580b3de9fd2f3a709c70cb702d5c8da8a95661
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:08:45 +0800

    xtrans: no rebuilding required

--- Formula/xtrans.rb
+++ Formula/xtrans.rb
@@ -9,6 +9,7 @@ class Xtrans < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "24285595cf66a05c9399cb7e9b3d1899ff2c494e0dbce260a2c6e4f748f76f0a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "24285595cf66a05c9399cb7e9b3d1899ff2c494e0dbce260a2c6e4f748f76f0a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1738fd7b80c0ebfe717716a192b00400e804df83d42edf172d42acba1cf09fee"
+    sha256 cellar: :any_skip_relocation, ventura:        "24285595cf66a05c9399cb7e9b3d1899ff2c494e0dbce260a2c6e4f748f76f0a"
     sha256 cellar: :any_skip_relocation, monterey:       "24285595cf66a05c9399cb7e9b3d1899ff2c494e0dbce260a2c6e4f748f76f0a"
     sha256 cellar: :any_skip_relocation, big_sur:        "784cdda022187276b0428069a1c57b7f598d4748d2bb824bd483a809fb94ae06"
     sha256 cellar: :any_skip_relocation, catalina:       "74e4e5cf12976f0b9ef865052f6b40b6d3bb17fad1f6298f7cb54792aec3cb8e"
