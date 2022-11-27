3b861bcf22bc5185c5610bd1c1f5fef2da0ae95d
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:04:45 +0800

    uchardet: no rebuilding required

--- Formula/uchardet.rb
+++ Formula/uchardet.rb
@@ -9,6 +9,7 @@ class Uchardet < Formula
     sha256 cellar: :any,                 arm64_ventura:  "eca4ce7dd7360c3739d795ff79d166e35413b5969fa351f6c56b294ffdf2cbc7"
     sha256 cellar: :any,                 arm64_monterey: "6f144a202080194afbba458511b7250a27a5b0ff5f0baa4f81a4ab4148906aa5"
     sha256 cellar: :any,                 arm64_big_sur:  "33989aa08f1ffc6f71f536c2a3cb28aabcc774e90da2aa40db110048f4b38238"
+    sha256 cellar: :any,                 ventura:        "7e575e77685897002867a6a8444a0132c99daa8b564785cf921b6d91404d116e"
     sha256 cellar: :any,                 monterey:       "2b32584a9ef94d89025ed7b2d027608013571f12e30b363fa46bfddd5aab2893"
     sha256 cellar: :any,                 big_sur:        "f5125cd193034470e278536d8b5d2498e99ddbd5c2853e214fe24fe845cc56d7"
     sha256 cellar: :any,                 catalina:       "34bd4791834a762da961de136a3cad253800a98deea2d1dededf8efb465f215d"
