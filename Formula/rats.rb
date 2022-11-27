43d61025764645f74018d664c7367628f3599dfb
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:55:43 +0800

    rats: no rebuilding required

--- Formula/rats.rb
+++ Formula/rats.rb
@@ -6,6 +6,7 @@ class Rats < Formula
   license "GPL-2.0"
 
   bottle do
+    sha256 arm64_ventura:  "9a47934dabb7a37c3d8a2a0a68a2e25961bfd8fa56be202d4bc604c4850d1cbf"
     sha256 arm64_monterey: "a210be283710fa3c506e9fae4dd915bcd737904272df4c985c5f54d666b3a745"
     sha256 arm64_big_sur:  "bfe1ae23fc4335ffdc160f80613e519c810b259b48ddef7de9d0d227625a3407"
     sha256 monterey:       "5798bdf316715050aee914343db4155c7ef89fa274908b85def50a84729c0845"
