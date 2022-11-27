cacd29951c95e1e501365904df4beaf7b7e80d2b
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:04:53 +0800

    uggconv: no rebuilding required

--- Formula/uggconv.rb
+++ Formula/uggconv.rb
@@ -3,6 +3,7 @@ class Uggconv < Formula
   homepage "https://wyrmcorp.com/software/uggconv/index.shtml"
   url "https://wyrmcorp.com/software/uggconv/uggconv-1.0.tar.gz"
   sha256 "9a215429bc692b38d88d11f38ec40f43713576193558cd8ca6c239541b1dd7b8"
+  license "BSD-3-Clause"
 
   # The homepage gives the status as "Final (will not be updated)" and it was
   # last modified on 2001-12-12.
@@ -14,6 +15,7 @@ class Uggconv < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "ad9e49d325916e5ae774fdb74c4ab2c9f8624316a920dfe8a1394d2a395ff049"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "27654b49d8d698a39c1e7592fad41eaa5c81a8151196b7eb054c0e711bee4a36"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "002cd504e9385234ab9b1b4595709ffccb2fda5d49c377add9c27eb422c04ed3"
+    sha256 cellar: :any_skip_relocation, ventura:        "2118cac3d4f75602660b9c8249ad80889313144e1705b9be04dccfd4e8ef5257"
     sha256 cellar: :any_skip_relocation, monterey:       "ad5e3778249cab414b73491a205ffc0c45817d0061dcf59d561315e544eeca4c"
     sha256 cellar: :any_skip_relocation, big_sur:        "820cca76f8c5618ba401a343644cffae2ece5526e0295dc29d0dd0f5f524789a"
     sha256 cellar: :any_skip_relocation, catalina:       "c1fc237ad27120a1804b37d765a47833d67bcd38cfa67be07aed808739021146"
