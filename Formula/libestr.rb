c065ce1d6571c530f0143870bf7e40bc30a60a87
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:38:59 +0800

    libestr: no rebuilding required

--- Formula/libestr.rb
+++ Formula/libestr.rb
@@ -14,6 +14,7 @@ class Libestr < Formula
     sha256 cellar: :any,                 arm64_ventura:  "5acb64697d9ef2237c384b6b2f265e2c74770f218259707485fcdf99dfdca7d5"
     sha256 cellar: :any,                 arm64_monterey: "b186e7aa04c176161a97f6955e2d4cc5dff415be78213f0f842c03acd0614c4e"
     sha256 cellar: :any,                 arm64_big_sur:  "20863614b37a81431737f276b1273bf542e374a323b9a1e486af7775e659d688"
+    sha256 cellar: :any,                 ventura:        "47ab08fd7c8294b4e47eae018b76a6ce91837ec1705b071099cf7d5c65410f15"
     sha256 cellar: :any,                 monterey:       "21c59f29aad1c8053e27ce8946abbc9bb5b82e8ae8672b331524282fd835b60b"
     sha256 cellar: :any,                 big_sur:        "11fa154962682f47b57b2dac7ceee697b5cf57c21e56d3c713f6e5a646d318da"
     sha256 cellar: :any,                 catalina:       "f539c76e3acdd0a93def55a0e82ecf45c53de65dc6dc18fd123efe815d8a65cd"
