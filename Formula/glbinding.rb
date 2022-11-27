da0b98039ba9dca0c6eea0c85d4d8d7afff31401
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:29:06 +0800

    glbinding: no rebuilding required

--- Formula/glbinding.rb
+++ Formula/glbinding.rb
@@ -10,6 +10,7 @@ class Glbinding < Formula
     sha256 cellar: :any,                 arm64_ventura:  "8414c2062a0413be5ce4d3104464d77ded39a971883839f5162d1eda60c1dc9e"
     sha256 cellar: :any,                 arm64_monterey: "6679892b95d63354d1aa3cde01824915003d8decbbe4479a19cea82e31d20be6"
     sha256 cellar: :any,                 arm64_big_sur:  "5c77227ab2d41d56069711ea964f5222feb1d9f1f88228b88ff657131cec9093"
+    sha256 cellar: :any,                 ventura:        "7eef393fef969da975966933406213df6d32a9ec757d3a581751c83aa7e0ef4a"
     sha256 cellar: :any,                 monterey:       "53e55b3996a3e0a93dda11fe2060a9fd7e9a15f2b5985938b7c04beca5a49542"
     sha256 cellar: :any,                 big_sur:        "a77f29c6cc40472d39646027e3d9b068ff5cf912edf600087ef4902e30f501a0"
     sha256 cellar: :any,                 catalina:       "6a371e47b76cd227e12699de3e7a095e620150532789cdac48e1c9b59bee06b6"
