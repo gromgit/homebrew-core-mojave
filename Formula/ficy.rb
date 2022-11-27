4b68d25739f871a6dd90c90c2981e48de06cb06f
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:25:49 +0800

    ficy: no rebuilding required

--- Formula/ficy.rb
+++ Formula/ficy.rb
@@ -13,8 +13,10 @@ class Ficy < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1886cf04ca32f5bdde1f3bc5489ac636cfdaabcf8ea7ef2a4a902e7b2a91843c"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c25a3ab155e4dea715aa37ef9fc544adc571300c67aab77e93fb66423337052a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "586d692ab7b64ad5805d51280e78ef997bf0ad2ebd1db2ed57ddc05b126f950b"
+    sha256 cellar: :any_skip_relocation, ventura:        "ef2563d59bf9e6e918bf73928c2002d3d5e75f57f97e9fa24140c7dac10f0d17"
     sha256 cellar: :any_skip_relocation, monterey:       "b6eeba4f52cc614eca8a65cae2f346d41e2e0376f27459fdede8e7eb82ef7a12"
     sha256 cellar: :any_skip_relocation, big_sur:        "eb6228a79e94bd6a6d37ec647da5b0ca1863ba03992d93bed90071858d0be55e"
     sha256 cellar: :any_skip_relocation, catalina:       "9974dd8c30bcfe482222a8e6f4040c6c5ccb21c7ef6b893dbbf3033f7e5a85ab"
