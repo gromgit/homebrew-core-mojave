8323fa9e41b187769ca132d7ba43f70a73a5ac14
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:07:41 +0800

    write-good: no rebuilding required

--- Formula/write-good.rb
+++ Formula/write-good.rb
@@ -8,8 +8,10 @@ class WriteGood < Formula
   license "MIT"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3c0ae187140e31156c039234b5c38df8a326125edcd1ca45a3ef661c620002e1"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "0d7f61673e4bf3404da929427c3ac1003ba259f9d781248d1cfde01da6780d6c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "61d0833a983986732c1d6abb5491a3cc787f30c6987d2c420ef69af5a82e8340"
+    sha256 cellar: :any_skip_relocation, ventura:        "a6794938945be53cbfe3e94f6bf16e8f260bb3ff5f40eeb2d37f64d97c48382d"
     sha256 cellar: :any_skip_relocation, monterey:       "86bd56a8ef2b340804f9da813b086f49f2f2f9f6eb8f31fe1d4137c2c44a1202"
     sha256 cellar: :any_skip_relocation, big_sur:        "9d0239747d4aaff293b839c0cd3d4ee175eb69260d965d810b33b4081a20845c"
     sha256 cellar: :any_skip_relocation, catalina:       "1bb59d5fc6bc1e3350b3ff45eef3aa3e78500e3cd9342c690f6dcf8b6163a77b"
