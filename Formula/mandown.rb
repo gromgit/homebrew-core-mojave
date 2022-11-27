98f54069f29df3dd77493044a606f572388bfdd3
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:44:08 +0800

    mandown: no rebuilding required

--- Formula/mandown.rb
+++ Formula/mandown.rb
@@ -9,6 +9,7 @@ class Mandown < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "72f91c87b01ae0fb51ee58518fcb963201b8a833c09d7e81556f09bda80c553f"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "6f06d6f6ada8676f6644e8491823bcb2310eaec516120b3455465ce4bd03a3dd"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d0b7966c93183b64046728563d3f9f95fd54e836f74cd13560ea013f1261aee3"
+    sha256 cellar: :any_skip_relocation, ventura:        "58b84452b3493987cfe1bf1c5db63e043f01f5822d23d8a95eb7b25b3b6d65aa"
     sha256 cellar: :any_skip_relocation, monterey:       "e4b32eed833a22db7e51e2cf957a4ed80ff8403d2959dce0b9af6242406a057c"
     sha256 cellar: :any_skip_relocation, big_sur:        "dd0e5fef1f35544524a29e7704d68ccce992ee099a34a088155c9fc579da5cf7"
     sha256 cellar: :any_skip_relocation, catalina:       "09ad2e54a3b54c9687580b4499f4c5247dfd2e18fb64230b3c255fbc7df1c5be"
