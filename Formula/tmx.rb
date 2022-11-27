bddbe828835715866ddf6f20af9ea2bb37fb7ac7
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:03:31 +0800

    tmx: no rebuilding required

--- Formula/tmx.rb
+++ Formula/tmx.rb
@@ -6,8 +6,10 @@ class Tmx < Formula
   license "BSD-2-Clause"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "5714907b97ede6353e5f461db191396197605b67df3bd5d79a53e01c9edc5fa5"
     sha256 cellar: :any,                 arm64_monterey: "1741909448819ba29272e002c9d25fdb8cc315e0ea70991799e2bbfa4bcdb88f"
     sha256 cellar: :any,                 arm64_big_sur:  "770cdb601ea6b496a29832960cb5fd79626a99f55f01c635985aa921f3e5f31d"
+    sha256 cellar: :any,                 ventura:        "25da4b79d7d3f89fb78a74c5cffba62085adda61a932f01d3dd3d70b858ed0fd"
     sha256 cellar: :any,                 monterey:       "fd62803bd77e4f17e11137d591ee0f916eea138b4e9a076355fea04f5a01d67c"
     sha256 cellar: :any,                 big_sur:        "91e9846b6d59e0694918753e357736c229c2a70d8021fdbaa2eb506e5be746c2"
     sha256 cellar: :any,                 catalina:       "1013715fdb263f6d6985c9145a5dbc05d2e41ba6c4aa28af766f0bc82a87f2c5"
