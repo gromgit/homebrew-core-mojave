c6cfe68e859f4ea31fe0072658bfd43ffd51caee
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:22:14 +0800

    dhcpdump: no rebuilding required

--- Formula/dhcpdump.rb
+++ Formula/dhcpdump.rb
@@ -14,6 +14,7 @@ class Dhcpdump < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "00e07738f28c1a7171439c05e7bbbe082a58db87bb52b7227920c5f6b1fb17d9"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1a948ff9d54f73ace571fef91ff0ea06c09216ecac07e4665526ad4caeb680de"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ea331a3bb132f7f9fbc334e85fd7d7cfc6cf7314df93bf123c5e042febbe1951"
+    sha256 cellar: :any_skip_relocation, ventura:        "51665ae0067f51c4ce8afa58c84ce5dd507834610a8bc03a7c212bd7026f4a09"
     sha256 cellar: :any_skip_relocation, monterey:       "305c41098112a0cd36fcbf838f9a9d575ae2764ffa7db65fa2f53874639b4cfa"
     sha256 cellar: :any_skip_relocation, big_sur:        "c6661ad77e8aa1a0355ca8c93454e9a5cba9936790b2fdbf97ada7c7e094af1a"
     sha256 cellar: :any_skip_relocation, catalina:       "508365f542d3fc6574525a2b2ccacc4467b40fd53b9885e8c4e91e9e2b172f21"
