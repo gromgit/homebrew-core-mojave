21c3dbc07c28b9592acc6a3b7ccaaa01ade36f03
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:07:14 +0800

    wellington: no rebuilding required

--- Formula/wellington.rb
+++ Formula/wellington.rb
@@ -7,8 +7,10 @@ class Wellington < Formula
   head "https://github.com/wellington/wellington.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7141518809cbba8d42a2ff794af1fab87c327748020ae47d148f78ef3fdad0ad"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f247dbeda01b8853ec7c6bc57fe172ffc8f3c65be366623fe11c1583f75a725c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "27f36c238e415ad9209494e1760b9bcb64fddc52fd45c8b7a4cd218e603e6503"
+    sha256 cellar: :any_skip_relocation, ventura:        "fb33ee897dbb2534defdf1cd8e37b2cded9404c2c6e3c337cb292cbfec7f3fec"
     sha256 cellar: :any_skip_relocation, monterey:       "712fce49467b7fdaf6a2113bd61bd35b067dedaa3021914ce1b1654d7e8ea416"
     sha256 cellar: :any_skip_relocation, big_sur:        "1042d237d74c534c987e63bb1e4eef6ec4070a1c9b080e52283d65c5edd9e40a"
     sha256 cellar: :any_skip_relocation, catalina:       "9aaeb3a098cbee88efc4e60d1edbfec242d6b2271f821b4d096fe6acb3d16987"
