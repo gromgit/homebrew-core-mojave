3b462ab1a417c872b216450dc33981e1d124ba2d
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:05:21 +0800

    uptoc: no rebuilding required

--- Formula/uptoc.rb
+++ Formula/uptoc.rb
@@ -11,6 +11,7 @@ class Uptoc < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "942cacc0ae14be9ca4c89497326c0c2d2a80dd9387489ba12de73da0fd98cb62"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "09100a8e6f85e66cd71378e628bc57cf200f224750a000be31b6bef6be1a1ae2"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7e6500f982ce6d54888461d97c8bad6dda039d065e883a60047ca549d9e06329"
+    sha256 cellar: :any_skip_relocation, ventura:        "cc771d7e3dd47f766d7a7e9fd76a9c5b25367f2f6f2ddafc2ab1c5a08fb5db7a"
     sha256 cellar: :any_skip_relocation, monterey:       "e9d53160af121806f4eed752935163d9ffc17b84ec42afeedec7eafb8c1bc0a4"
     sha256 cellar: :any_skip_relocation, big_sur:        "40353c235e30210ab737684f85d6c444b580192eccbf50d84fbae0fa8a64c27b"
     sha256 cellar: :any_skip_relocation, catalina:       "7a2f3982a14ad4176b17832e87dfb2a41fd10d87e0e35ade341b8baa17d3b7ab"
