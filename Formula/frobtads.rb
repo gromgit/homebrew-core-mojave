6cdc903a41447d89e0601ad05c15b4632d04d09a
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:27:01 +0800

    frobtads: no rebuilding required

--- Formula/frobtads.rb
+++ Formula/frobtads.rb
@@ -5,8 +5,10 @@ class Frobtads < Formula
   sha256 "893bd3fd77dfdc8bfe8a96e8d7bfac693da0e4278871f10fe7faa59cc239a090"
 
   bottle do
+    sha256 arm64_ventura:  "dce25a4ec2c4448a2f9f201b00beb6c99c2abef5c50c63214df3d177e7028024"
     sha256 arm64_monterey: "0d58231276310d6f781bf35dcc430fa6495fe40222248cf6763f8042e92b87ec"
     sha256 arm64_big_sur:  "c73a8dc0cb339a9f039ccedd7950372a0c2fa136f11d59ac7d0f0cc03a2a1651"
+    sha256 ventura:        "56d000455960c5ad32e0905ec5279ff0bfae157acfe419afc8b616a6e2fc51dc"
     sha256 monterey:       "4238e7e1b6e4bc87e4b23ad9e73f5b801a5aabc29301ab5cbaaa332176a47fdb"
     sha256 big_sur:        "b58665483ecdcaa4ebca91dc048884c6168dfc9d0265bd7bb62bc7a57f1814ed"
     sha256 catalina:       "9f7593c956b859ba6592f85e9741e04c2092e13ab4d689548bf5d4d92042501f"
