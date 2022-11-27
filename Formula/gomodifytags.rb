1250d86fd1123dd2a57a46ca3d4bc10b5d054eb4
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:30:10 +0800

    gomodifytags: no rebuilding required

--- Formula/gomodifytags.rb
+++ Formula/gomodifytags.rb
@@ -9,6 +9,7 @@ class Gomodifytags < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "9493515299a08d2d57bad868109ed74e987149b170e6670b02b3489f4c5b6ce0"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f99010f77d3726d789cd1d870442fb867595fa62b5f1addc44f27840581c18a5"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dbd3a4c7a66090a7c17afd699b0b35d70663b5e9305085a573a3493a5cc4ebe2"
+    sha256 cellar: :any_skip_relocation, ventura:        "73d9f9b00d7e4be16971263be7141170d4d3fdb7620d7275549f50647d1b0e2d"
     sha256 cellar: :any_skip_relocation, monterey:       "762d9e674caf68cff1c1ed195064ddb01b5705e45b079832cb2aa414ee38086a"
     sha256 cellar: :any_skip_relocation, big_sur:        "c3c815e6288c0c3474800937bbb15fa2569e34a7715121b94c713b8575c51203"
     sha256 cellar: :any_skip_relocation, catalina:       "c3c815e6288c0c3474800937bbb15fa2569e34a7715121b94c713b8575c51203"
