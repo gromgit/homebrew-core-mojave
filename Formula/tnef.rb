67ef854fe5f1b121c3506e92d57deb0085c1629b
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:03:32 +0800

    tnef: no rebuilding required

--- Formula/tnef.rb
+++ Formula/tnef.rb
@@ -6,8 +6,10 @@ class Tnef < Formula
   license "GPL-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "920a01f591be96275e201cfd5dc6e34014cfe036cf255ad5c67daa3167f327e7"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1201de0e009993b46a273968cbcd8c9a43c468c30310807712052d398c7034f2"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4e6bdd57047af524d3efd157858a47d857a12fc110142a3d94bd47c8552fdc0a"
+    sha256 cellar: :any_skip_relocation, ventura:        "cd8e00e27d2857404867233b70ccbe5f991ca50c03d9660533a0b60de47db438"
     sha256 cellar: :any_skip_relocation, monterey:       "a79b4c0a54e6b4454281c5f40a500e7f3923588815374f28684e9d0fc53adda4"
     sha256 cellar: :any_skip_relocation, big_sur:        "37ce4eac19eaa6e7111d7ef7897595ac71ebb58f6d7da32dc309bb02bc5a90b4"
     sha256 cellar: :any_skip_relocation, catalina:       "ff92eb820b2efae9e87e42491a590601f400160f27ea2804b176b02b1648be66"
