a08877d1a864f11530cb08d5c5e09c342e45d280
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:21:43 +0800

    dcfldd: no rebuilding required

--- Formula/dcfldd.rb
+++ Formula/dcfldd.rb
@@ -9,6 +9,7 @@ class Dcfldd < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "0a1e4bce6c8af34771e2f96e5c15f22f90388a39a7f44022e9efd51e4b007a94"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "985ab7c04a8b19079d969c4984327bd680cf4e03d9352816201c6d0808270e75"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0467bb8c411c332e45b3dc5c5cfa82a0ee98d7d3c53e73357306986f81cd5b53"
+    sha256 cellar: :any_skip_relocation, ventura:        "96920378fc450ebd2f65cb63a78aebadc19356a4af7f60c11266876b306a0215"
     sha256 cellar: :any_skip_relocation, monterey:       "1ef108ea64574e6434232a84245ec5993c4a88667c76e4e4ea29950b6797814b"
     sha256 cellar: :any_skip_relocation, big_sur:        "e9355a9c6885f7c22087b05988e99e5fcf563c1bcd9856ee5de0d207e1dfa54e"
     sha256 cellar: :any_skip_relocation, catalina:       "17bf5e7a79a3453103e9fd5a70f0b12d49c93c5302a6ada8bd021ca918979992"
