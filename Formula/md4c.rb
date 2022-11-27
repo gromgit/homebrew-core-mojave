7ddbb736696755c8562649224ce25f5188839e62
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:44:37 +0800

    md4c: no rebuilding required

--- Formula/md4c.rb
+++ Formula/md4c.rb
@@ -9,6 +9,7 @@ class Md4c < Formula
     sha256 cellar: :any,                 arm64_ventura:  "8057cee0cc192a2db1c29a4a49a3c9b175891b9c2aff8b0481a64c8da807d4b3"
     sha256 cellar: :any,                 arm64_monterey: "154f5f1eb77492727a5475922c642db680724a1b5abbd221b6ac65cb0d4c7c52"
     sha256 cellar: :any,                 arm64_big_sur:  "1873e516760235dba2670bb63a612b99b51714baf657b99c2c411da40c3162ec"
+    sha256 cellar: :any,                 ventura:        "4ab00b6654d56bdf2805cfcec8b4bec4ff1704a0cf499fafc5fd451b86c9fe78"
     sha256 cellar: :any,                 monterey:       "29d7e311c2821193496c531b4f8bec1d3857d5bd949da3578ee1abc6e9d2d20c"
     sha256 cellar: :any,                 big_sur:        "8b4ae9a5232e84db3d0ab6793a8dd5bd56071fb32832fd94d16fa8162ec08a16"
     sha256 cellar: :any,                 catalina:       "8368b905e33301b5019e8520f7d010e3a57f74855ebd5cbbbf87aa1d8ded50a7"
