6309553eb8792d9d8ad862afb5d8df459de7a723
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:12:55 +0800

    amazon-ecs-cli: no rebuilding required

--- Formula/amazon-ecs-cli.rb
+++ Formula/amazon-ecs-cli.rb
@@ -11,6 +11,7 @@ class AmazonEcsCli < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "ef77e9ec799164485117cc7971285ef4cd0d9f7ce526eff3955c18270f215c0c"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "997efae6c4c5bd40f018cf944093c3842d52c8ba09e6bbc68715b697438a1f77"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "dfe17f71140b30b124679fee0a7b402f8f16a3d284ea357b315f2f9896f67beb"
+    sha256 cellar: :any_skip_relocation, ventura:        "f020f685eb458c206f9dad7f6f2d6767bc088a5132655569bd5d652552cf211c"
     sha256 cellar: :any_skip_relocation, monterey:       "c052c78ca3a7a564bfb1544cf70d6cee45fb933ae0ce6f4c9abe987df2e194d9"
     sha256 cellar: :any_skip_relocation, big_sur:        "66dcb9af8a67215a8a1f4fef00dbf0c16e836cc65985a86d113cb4f208dff50c"
     sha256 cellar: :any_skip_relocation, catalina:       "0bb03d95203b20aebc66ee008946951dfc66a991d6015f38d9158cda3dc36b8c"
