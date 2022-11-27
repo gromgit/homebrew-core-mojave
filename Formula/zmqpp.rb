01719596a906fd119c34527532b2bdf845ba27d3
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:09:32 +0800

    zmqpp: no rebuilding required

--- Formula/zmqpp.rb
+++ Formula/zmqpp.rb
@@ -6,8 +6,10 @@ class Zmqpp < Formula
   license "MPL-2.0"
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "79259180cd88ddb59497fbb3075a02c19ac854e5a57f1ff33f53c328789b4dd5"
     sha256 cellar: :any,                 arm64_monterey: "ce6be56e7c768bc4c35b43d78a7938b5bf415293ce42e47230a47dc17d05e091"
     sha256 cellar: :any,                 arm64_big_sur:  "8784a9ab7929336cc1677315a134b8d379491e9980f1e2fc0c705bb0adf7c904"
+    sha256 cellar: :any,                 ventura:        "13235aadbe1f01545aa68211914aca2ee5c209121db4eb1964129476b515eb80"
     sha256 cellar: :any,                 monterey:       "7b68028743e0b92d94b1eb8a486901944d5bb77d1fb1e49b76bcfabea4f86caa"
     sha256 cellar: :any,                 big_sur:        "f85d36f077eab8c580e4e22411a9c2d89bff47a14f6b53c42eb6544c4e4250e6"
     sha256 cellar: :any,                 catalina:       "6ff257636778c3cb51a42ec7fd41d701ebb311dcbdca7fb0e63772078b59123c"
