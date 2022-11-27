4680d4017b5e9544edbed7e98bbdc1887da3ff81
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:00:45 +0800

    sshguard: no rebuilding required

--- Formula/sshguard.rb
+++ Formula/sshguard.rb
@@ -11,6 +11,7 @@ class Sshguard < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "89678a0c0443f86ccdc5674e288f51afc18ff2c7659af3c2e2a21602dbbf89e9"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "432b5c348a85223ac41b4ff38a1416847227c74e3372ddb1aeb1575612421dbb"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9d878d2defd31509f5248eb407a15b358e1b5e78e317cb4ecb58ea971eb0e21f"
+    sha256 cellar: :any_skip_relocation, ventura:        "b389fedf2144e6ccd91a51b07d5d433aa942a92da15cde4511b2bfcb379ea778"
     sha256 cellar: :any_skip_relocation, monterey:       "223206d74a0c9637ef21524dd7031262fad932ec9edd60996cf88272eff27e19"
     sha256 cellar: :any_skip_relocation, big_sur:        "fbd36be947e48cf8617b3889334ac8c0941b51e03b4c5193027791a727588999"
     sha256 cellar: :any_skip_relocation, catalina:       "02f3958ed46f151af475f82d9056fd4ba2d7cc6992f95d5ee35351ec0091256e"
