e9e00367de1c308c7bc51cee3567d7f9fa6e418a
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:01:06 +0800

    stone: no rebuilding required

--- Formula/stone.rb
+++ Formula/stone.rb
@@ -10,8 +10,10 @@ class Stone < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fd7a8b20854a50d343960a965e340d0178fd82728e7c74025146b2ac053b24a0"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "a4274010ee7d7f736080d17b23ee12250fc7f68a530c9149a0a625922a9db1bc"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "47a2008041ad4e5e76fe87a4218d4c21e5b1a2cea828aa97b9e9e5b6c6ecc882"
+    sha256 cellar: :any_skip_relocation, ventura:        "f6a0ba71f94f62594fe4e859a9be7b4616f8984410f7de532112f8c7f5566c97"
     sha256 cellar: :any_skip_relocation, monterey:       "dc89da0846364ce236eed2a2cfe0eb39abd9dc71f42f3b357d6a5709efb025d5"
     sha256 cellar: :any_skip_relocation, big_sur:        "f943cab7f931ae2b7c124a83b63150b9c3b75090eb63353fbe0732792b97a0bf"
     sha256 cellar: :any_skip_relocation, catalina:       "cadf40dd1d8aa5de47b9d3d3baa5bbc22fc5a8a50abe688e77520b035369f492"
