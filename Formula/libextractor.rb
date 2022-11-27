389397378e79545d453eba9eaf244e42bffcefe6
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:39:04 +0800

    libextractor: no rebuilding required

--- Formula/libextractor.rb
+++ Formula/libextractor.rb
@@ -10,6 +10,7 @@ class Libextractor < Formula
     sha256 arm64_ventura:  "dadca081f82925bb7fddd14a386c4f3a5dc789605224670be7baee6a16b3b411"
     sha256 arm64_monterey: "ddbc2b96c8ce52eb446434949f0322190627ed6359b6e9d6cbc41bf4a1ae9b4a"
     sha256 arm64_big_sur:  "46684b1a7a45edf4a8febdff6138463abd97cde89520824575f476f2d500f576"
+    sha256 ventura:        "eaeb728e70963b3ccc62b6ab38b1263f14b29f290452684e90184ea678f0b2e8"
     sha256 monterey:       "b3ec83de128a91eef34977dec6b29e61bdfa8904323d8cc179f7e4d6d12fd267"
     sha256 big_sur:        "c79547c7f5d513ede389034b436fce8a68898827dcbe814540b700286f0ad76b"
     sha256 catalina:       "ecd3a409a45003e3800c0c4e266a991ea7b2b2291e624d5e596f618a8ffbda84"
