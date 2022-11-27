5358bb1eebc4d38c32e75ceb843f148b8e03134b
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:45:40 +0800

    mkclean: no rebuilding required

--- Formula/mkclean.rb
+++ Formula/mkclean.rb
@@ -6,6 +6,7 @@ class Mkclean < Formula
   license "BSD-3-Clause"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "33ee8ae207d85761440d0bfb65995068d7de526dda7f41c7828f4853eb499bd4"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "6d88364bba58d17c5a2dcee261628dcaa7f488c65e59ba2d94470bdedf7a4315"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9cbb79d68f3b6f25830b76a374782b1cee440c6112280393a718f0950a561ecc"
     sha256 cellar: :any_skip_relocation, monterey:       "6406bf244beccc28185413a3409dbc788c494017f237231320bf42efa54ce4db"
