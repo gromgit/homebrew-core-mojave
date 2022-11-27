9fcc5b9b26e5a345ea88d7316bdbeae605b192fa
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:39:45 +0800

    libirecovery: no rebuilding required

--- Formula/libirecovery.rb
+++ Formula/libirecovery.rb
@@ -9,6 +9,7 @@ class Libirecovery < Formula
     sha256 cellar: :any,                 arm64_ventura:  "4d21cd165479e408542f64d4b03e607650bed9eeaf8564ace51746310a443bea"
     sha256 cellar: :any,                 arm64_monterey: "f84e04eff5b2a9a9679179f427ff5eca930ea9210f86e58d905c23a4185229b2"
     sha256 cellar: :any,                 arm64_big_sur:  "934427f0de5e9990ca8569960ac0d6cd80f5739401e017b49bb4f79244c953ee"
+    sha256 cellar: :any,                 ventura:        "7be7894d857845b641bf1e0caad511eb5698d1f4f3b504db2e91bdf8c31289d4"
     sha256 cellar: :any,                 monterey:       "5bca397cb6420f3a30580995c2f452548f0e9947bf7bc511eb4f09f4510e5370"
     sha256 cellar: :any,                 big_sur:        "4237290aa629bfa59e546e4da6d76d190ca44df8a6205dccf8974541b0d3bc1e"
     sha256 cellar: :any,                 catalina:       "a2733550b10ce601236c7e88f8bf689371c42d83e11875459f57a2da8b5bd4e0"
