88b3e4ddb48d0ebc1cdfe0ccde3a1addc8127051
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:26:23 +0800

    flvstreamer: no rebuilding required

--- Formula/flvstreamer.rb
+++ Formula/flvstreamer.rb
@@ -12,8 +12,10 @@ class Flvstreamer < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3de5f4ef4be5bd145b783b309b8be860215f308abbd3134d6def0734861ae63d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c96f83be0a0a72e6172cd9d1459d268cd58c16c930bf2a5c5e60de50654faef3"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "12ad45a6edb6af98a60963e3a5f302ca40055290233228adf54ae4cd9e491094"
+    sha256 cellar: :any_skip_relocation, ventura:        "b80c82043109dae0ab1868a5f3fadd6897663f55cea6321b6a76364bd91ea5bb"
     sha256 cellar: :any_skip_relocation, monterey:       "ce990e2bc2f6fe933e3203b6a62e0a7f42c899a29a4b77f453ba9ee93b82f8b5"
     sha256 cellar: :any_skip_relocation, big_sur:        "b148a052d107098db010c7f1884784dacba4f2f27e7ca9d50c9e3347096a4aa3"
     sha256 cellar: :any_skip_relocation, catalina:       "cfc6a5308ead52bccf753068f8de3a57abd47cf4bdf12d046ca540f3b38ebf8d"
