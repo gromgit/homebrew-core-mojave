e43ebc06979440f9a9f0e747e1c2b79bca9b8d5a
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:41:19 +0800

    libstxxl: no rebuilding required

--- Formula/libstxxl.rb
+++ Formula/libstxxl.rb
@@ -9,6 +9,7 @@ class Libstxxl < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "c553b5d40fc2b5c081634ce666570017b82f4c74c0f5915173bd9024d959de1c"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e487af039d6286b34dc4aeac3c0eaba10668054b33a368e54f9cb8647834ed55"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8454123ffed231405d684ed18c2ef1a0ab1bd118d74614748a5b5df23d8bb5fe"
+    sha256 cellar: :any_skip_relocation, ventura:        "2da4cce955e2b91e309712edd7e0271ff7671e3b472536972d802102547ba5a3"
     sha256 cellar: :any_skip_relocation, monterey:       "5916828893681e83b360fbd3638499592b65f39a546b64156aac85bf52a90550"
     sha256 cellar: :any_skip_relocation, big_sur:        "c3f8dceb4e0a1716a2c193daf4b5eeb4ae3e8e96224bdc78ae8f74c2a3059152"
     sha256 cellar: :any_skip_relocation, catalina:       "b4d5ef6b70735617973eb1f45214b11e3e6baec242bc6aa5ba9ed4da1834f6ad"
