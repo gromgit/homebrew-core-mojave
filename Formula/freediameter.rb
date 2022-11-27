c0509d55a6e2c3950adb7f9bd6d11caaa4b4b5c6
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:26:52 +0800

    freediameter: no rebuilding required

--- Formula/freediameter.rb
+++ Formula/freediameter.rb
@@ -16,6 +16,7 @@ class Freediameter < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "d692eec62f99ce01bafd11d1e6931fe4dd6e439a88768edda86cc7a6957a4bf0"
     sha256 cellar: :any,                 arm64_monterey: "ef01b2ee94b8f7b8794b6880188ec09d7a8458b0e10a2b6063b4dcc0f9b7c798"
     sha256 cellar: :any,                 arm64_big_sur:  "a2fd2271af79fd86ec7162e0af3adbaf611f280563a84dc2a98af96b7b3a3a4d"
     sha256 cellar: :any,                 monterey:       "506a0a7375314a874e8a04b4904d0fe9d7c83bda4c494171c4ceee242debd81d"
