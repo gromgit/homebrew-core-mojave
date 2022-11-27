9957d9c930a974d6d29986ddd300e24d1bd61fd0
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:27:19 +0800

    fuzzy-find: no rebuilding required

--- Formula/fuzzy-find.rb
+++ Formula/fuzzy-find.rb
@@ -4,6 +4,7 @@ class FuzzyFind < Formula
   url "https://github.com/silentbicycle/ff/archive/v0.6-flag-features.tar.gz"
   version "0.6.0"
   sha256 "104300ba16af18d60ef3c11d70d2ec2a95ddf38632d08e4f99644050db6035cb"
+  license "MIT"
   head "https://github.com/silentbicycle/ff.git", branch: "master"
 
   # This regex intentionally allows anything to come after the numeric version
