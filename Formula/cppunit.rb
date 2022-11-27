aefdcc3eafd1e09c526efe8415038dd6086e214b
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:20:24 +0800

    cppunit: no rebuilding required

--- Formula/cppunit.rb
+++ Formula/cppunit.rb
@@ -14,6 +14,7 @@ class Cppunit < Formula
     sha256 cellar: :any,                 arm64_ventura:  "45a8c4e8ca0defb8adee6028c2d213c7868bac9fdc9a4e814ad1b45aea1c3c14"
     sha256 cellar: :any,                 arm64_monterey: "7acd81de05bc607287b7133050c269e2ea86ac4e1141cb71ae85af4cd62e7f50"
     sha256 cellar: :any,                 arm64_big_sur:  "42291951a34e6fae578a9c25d2a5c399dc1e13ec3f700c017f96d7576acabbf1"
+    sha256 cellar: :any,                 ventura:        "7e5529650f22b5b4e19ff31a2af7134a11a112b8cbdbbd383cb50b0257e6fe7b"
     sha256 cellar: :any,                 monterey:       "a373aa7f91fd426cccdf9c8775439b21e620f529488030af40f5bd8ea79013e1"
     sha256 cellar: :any,                 big_sur:        "8890cb5c9b85681b735c3756d301df29beb98e2c0d0b10f2fad073e6f1870101"
     sha256 cellar: :any,                 catalina:       "3e194b84577c733e5641f305a1cb5cc76355f13037898afc56c3096f98bb78fe"
