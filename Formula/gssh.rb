37af91b54ea13f777db222b5c8201337bc10ef1e
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:31:43 +0800

    gssh: no rebuilding required

--- Formula/gssh.rb
+++ Formula/gssh.rb
@@ -7,9 +7,15 @@ class Gssh < Formula
   revision 1
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gssh"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "b80c5893bf2352a621866f5abdba12781553b1d76738e3b43d3385f3873ce6b5"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fb94322029d9beb3032089198fb9b755b38396151200ac14bd1be0e333f6e6bf"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "4ce6770535e1cb673b8b08918358a14ab02d06df3721d65a170d65e4b36a0680"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1c356a6f35f6377301e86f13be938d7db63325140a97650c34696614c36196c2"
+    sha256 cellar: :any_skip_relocation, monterey:       "3a65a2e6deb96da24f664ca4eaacce4034d30195d482fedd123a61f4498cac35"
+    sha256 cellar: :any_skip_relocation, big_sur:        "31f745f57d10191ea547bdcfbbf544342a09b87a51b4298114baacc9f35589a0"
+    sha256 cellar: :any_skip_relocation, catalina:       "170b7e0b5c81eb30f12471b8342f23179d5febff44835297d1648c124cd94733"
+    sha256 cellar: :any_skip_relocation, mojave:         "d4f7b427c456b891a7604988bba90850c81da7d9714788383ab6e9083a763d1c"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "51e468107da90b71b335a7dcdf0ff6149930634a8625cf2019d45add410d2852"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "aaad8801891848cb3ce21624f38c36a60762a4b913c8c0751a6d34136af21ed3"
   end
 
   depends_on "openjdk@11"
