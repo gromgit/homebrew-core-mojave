66d891a242335df83b750271b93e545f6d9cc1f5
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:42:28 +0800

    libxt: no rebuilding required

--- Formula/libxt.rb
+++ Formula/libxt.rb
@@ -9,6 +9,7 @@ class Libxt < Formula
     sha256 cellar: :any,                 arm64_ventura:  "2f18dc5ac2a79e09dee386201da716fdaeb04cdfc544287c5382bd399e1e99ba"
     sha256 cellar: :any,                 arm64_monterey: "7e817f2cb5cf9b4708246377c23745307d8cb7d0ee03d77b3a6e3b4fb16708ff"
     sha256 cellar: :any,                 arm64_big_sur:  "0fd900ad5097946ee343ba7c15311a3b85540dcd058233e05f198c15405b9da0"
+    sha256 cellar: :any,                 ventura:        "599e334d26186c677e16ce83c2cac7e116b5f0491b9d57e96e841192a50f487c"
     sha256 cellar: :any,                 monterey:       "f2dacb815eb7792b97b5e163bf6f8eef12b5d2283409322664ab937eb9954545"
     sha256 cellar: :any,                 big_sur:        "db76d4efdf96e00af7b4230245b81c26539b4ec20e93c1d379a3c92b39217885"
     sha256 cellar: :any,                 catalina:       "a1bcc92d37e1602ef760fecb79f7729db7e38aee2835879689596d478480217b"
