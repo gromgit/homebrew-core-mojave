719fd29f3397cfb516db328b03857be196fc13e6
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:21:55 +0800

    demumble: no rebuilding required

--- Formula/demumble.rb
+++ Formula/demumble.rb
@@ -7,8 +7,10 @@ class Demumble < Formula
   head "https://github.com/nico/demumble.git", branch: "main"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7bfa72834af03942bb6eacc60ec52006997dd1fbdc68351398a78ffe046e19d8"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ff3563d728d6ad3ba851fe1a9e95e599f0fb835ea6e046b30e329537f19aaedc"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b43c1b4396d845e300f9f5c7746fb6a393ea8a3189c822ba96c35cb1ddd7786b"
+    sha256 cellar: :any_skip_relocation, ventura:        "e98aeeb71355a2125a7e5c7d91c3926e2889864242ca65cf961b2de49900d55d"
     sha256 cellar: :any_skip_relocation, monterey:       "3677ec9a895d02cd6694710152625d98f903720e891774d292a5ea454d11bbc0"
     sha256 cellar: :any_skip_relocation, big_sur:        "f7dc50c58c1188bd07b90c0ecb57031f24ad3b463f41befd78ea5b5cfd1577e0"
     sha256 cellar: :any_skip_relocation, catalina:       "c79b46404de20da59fb13b225006147666199f6a03b56e43820d20ae818dafff"
