d133dd8493fbf8907538ec157378655c013c235d
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:35:02 +0800

    ircd-irc2: no rebuilding required

--- Formula/ircd-irc2.rb
+++ Formula/ircd-irc2.rb
@@ -12,8 +12,10 @@ class IrcdIrc2 < Formula
 
   bottle do
     rebuild 1
+    sha256 arm64_ventura:  "8ca253a45db11738e82beb74dac8f6bc70fa455f738ec1252de0102826247697"
     sha256 arm64_monterey: "9278b13e2b860697e5868ca1624103f2dc4d81b6bc0e6e58801c1bdb52bac550"
     sha256 arm64_big_sur:  "ed3eac7c4635484c94d12579948947bff1eb6a671846fcd9273dd5ed226759fa"
+    sha256 ventura:        "dc5322bc271aae9ea0b3c84981f73ddfde9602a453335fa3ed3f164b048373c1"
     sha256 monterey:       "d4c8fb409f40a0b28fd5058260e3ceef2520c654eda0398271e04a6fe9918c66"
     sha256 big_sur:        "855bb8b0254ee0f410d6bdf3ad8479900f39f0ad120145485d9bdbe146f7a399"
     sha256 catalina:       "35ae4defa513772b1e1b5b0400976d49cb213818a2272a9760a3da3a7e8c0765"
