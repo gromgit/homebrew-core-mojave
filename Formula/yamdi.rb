3a7c34338c91cb5205632e3670e6b3fb308806c8
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:08:51 +0800

    yamdi: no rebuilding required

--- Formula/yamdi.rb
+++ Formula/yamdi.rb
@@ -6,6 +6,7 @@ class Yamdi < Formula
   license "BSD-3-Clause"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "385b4af3db0b7f734f690539553c250c0e08fb8a472c7bef07e31fb273f20fbd"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "b50a1157642d1cf0bca8199e5da07b8a71788775d3d1425642cfd3739ee2db26"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f6796b10d1af7ae38ed559313b5646047cb1456c66428d9c32f5e3a0981f1f4c"
     sha256 cellar: :any_skip_relocation, monterey:       "2bbf1bbbc7bb411fe09900dbe61093834ffb41f314ba241deee6ff764c285232"
