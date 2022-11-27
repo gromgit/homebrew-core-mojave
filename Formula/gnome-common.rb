a677be674c8bde9a06305d9bf5fc8caf7e025f93
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:29:30 +0800

    gnome-common: no rebuilding required

--- Formula/gnome-common.rb
+++ Formula/gnome-common.rb
@@ -8,6 +8,7 @@ class GnomeCommon < Formula
   bottle do
     rebuild 1
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "f0301bdbf6c3d84a7c95e2ddc3c94a82ba79ac415a49e93b36a793dda8c02e34"
+    sha256 cellar: :any_skip_relocation, ventura:       "186e36c90aadeb3fe96a17c685b1610b43ff5522d7cd4bad372d6f823938bb07"
     sha256 cellar: :any_skip_relocation, big_sur:       "ff4e1b1395991ae955f575ed3529e66a881ada9561866b10030d00883b1ceab5"
     sha256 cellar: :any_skip_relocation, catalina:      "079756ae6ef88387933614b1adcd2a76f239f779817f6128493cdac85c8f5baa"
     sha256 cellar: :any_skip_relocation, mojave:        "7c853c9cdcd84eddb2a3567d161182b27b42dd28c2d696005dc43cf27bdb7038"
