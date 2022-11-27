8641d60b67c03516d10970ec01fd1fa2ae246fe5
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:29:49 +0800

    gnupg@1.4: no rebuilding required

--- Formula/gnupg@1.4.rb
+++ Formula/gnupg@1.4.rb
@@ -15,6 +15,7 @@ class GnupgAT14 < Formula
     sha256 arm64_ventura:  "0745d4b4468cfecd559c6ddd3aa81582bde9f1def99761e8bf7989ddb9a76abf"
     sha256 arm64_monterey: "d35c8557e2e77c6074a75bf4f4e575bd0a24ed57fbd061f9bd2a06d58bf8415c"
     sha256 arm64_big_sur:  "30169aa8ef5373a4d5b36ee5714bd7e34d7222d02ad090bf3094b92b1c203bd6"
+    sha256 ventura:        "d1beb68c6abb3a1d249621c8995d8e26bd1c8792260f36407c47627f84c84668"
     sha256 monterey:       "d010750043549f48f60a95d8b6c02bc5754168d09f298a4dace80eb84ccacd52"
     sha256 big_sur:        "408013c7748d2b6de6c09520e4b3948a493fcc338624493050c081e200820390"
     sha256 catalina:       "3796803df0956a54dfc5ed26f17a92791622c4ddc6b0dfa6b8fabc0f65afdd0d"
