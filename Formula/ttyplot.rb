d3bffc9619f11ee77611ff7cd6edabc6b4fc0abd
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:04:26 +0800

    ttyplot: no rebuilding required

--- Formula/ttyplot.rb
+++ Formula/ttyplot.rb
@@ -6,8 +6,10 @@ class Ttyplot < Formula
   license "Apache-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "2aa30c7f0765b8a9eb2bfd3d56e2ed0c50a93f8deddc884eab1a2984b3b27cc6"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "87a224c655b39dc35f8467b724f6dbce85752ce208ff793a05073bc94ca6e110"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4283b097475510418957e8bfc92b952f6033e615b4b805ec76df7e80c59c9209"
+    sha256 cellar: :any_skip_relocation, ventura:        "b640f1300a6b7ce6e97d9c4357fc9049e99f5d398655239d60f4e0122e85553f"
     sha256 cellar: :any_skip_relocation, monterey:       "6491a1d546da726daaf3c87c4bf61885bd66abcd03bc2a42899a03f3efdd01f7"
     sha256 cellar: :any_skip_relocation, big_sur:        "e2076fd6d1c3921d941d143b49e41a75c1a8fe7b46e640752a7abffb94b92aca"
     sha256 cellar: :any_skip_relocation, catalina:       "760b571d2cc940c6a1ad14655f2e2c0cac4aa64dea8e63d37454528cf969d8b2"
