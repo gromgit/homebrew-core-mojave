d4020b30e571aac3dd63fadd964aff68a561c94f
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:13:05 +0800

    ansifilter: no rebuilding required

--- Formula/ansifilter.rb
+++ Formula/ansifilter.rb
@@ -11,8 +11,10 @@ class Ansifilter < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "dc395b12e05e924850d9c3140e2df688645dbd3cb6fbe0ee55a85efecfc3a25d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "b2b6a8ec1dbee03ec73761e62abdb29e25daf9a5f5cd2339281784e73e35b27f"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "604a45cdc202a4cb1ee084a350b8ad989f135e29d1d4153a9c9e20818da8e31e"
+    sha256 cellar: :any_skip_relocation, ventura:        "82beb4fa65191d059c74f556d92b2c2250f676a60fe9ca69fea353b40cc14ec5"
     sha256 cellar: :any_skip_relocation, monterey:       "a4c13e32789f3e2dc6e8c1c41d7a680e5fad4464ed59cddda4b859de3d669573"
     sha256 cellar: :any_skip_relocation, big_sur:        "bce4e87b0f16217d84a01e664c6e96c530d561da301912bfd9e39ec7ff584a74"
     sha256 cellar: :any_skip_relocation, catalina:       "95487d963289a6fbc97a933a492a8f82ae88ad7087ca525d710edea34e100f0a"
