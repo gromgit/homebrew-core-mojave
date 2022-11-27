a61b03c7ef5f233cee449a7dda46cb659d286c50
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:04:29 +0800

    tunnel:

--- Formula/tunnel.rb
+++ Formula/tunnel.rb
@@ -6,9 +6,15 @@ class Tunnel < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tunnel"
-    rebuild 3
-    sha256 cellar: :any_skip_relocation, mojave: "48fb4ee67098a2f8743710b755ca2ad6bb44e53796f097c5f48e40432efc640d"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "8bd84b399a3ff97b60c445f54dcc918c98fd1804bfb4fd4ffbf0c2583fa6327d"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "fc5f7baf6232a8be13a17350b3ac13d264c530e40c93cdc5aacbe9af856fb060"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "57607af5d1084a837f6dac59d268a9c5ed879acddac12840a1a4e8f375a070c6"
+    sha256 cellar: :any_skip_relocation, ventura:        "10d71f8a7917d1f09b4292bd74c0bf9b71191ecf2f771b16c4272c8825a82331"
+    sha256 cellar: :any_skip_relocation, monterey:       "b63c0576e0a46d3177fcb0574dc63bc252885ab52840f3454eb3660f31fcb744"
+    sha256 cellar: :any_skip_relocation, big_sur:        "cb4ebc8c76b4866aeaa4da672e81e7027ffad1f4d78a7867aa1b17511827b3bd"
+    sha256 cellar: :any_skip_relocation, catalina:       "8119646cdda9c9578230f230b8b9156159d9adecbf30b1c8d6fed8ddbdce54bb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9a079e012dffd514c536355269716695a150db0dc30cd3ea2c27aee789615805"
   end
 
   # Bump to 1.18 on the next release, if possible.
