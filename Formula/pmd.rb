582451328fb410176642fe11f2a880db8b35c6cc
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:53:01 +0800

    pmd: no rebuilding required

--- Formula/pmd.rb
+++ Formula/pmd.rb
@@ -1,12 +1,12 @@
 class Pmd < Formula
   desc "Source code analyzer for Java, JavaScript, and more"
   homepage "https://pmd.github.io"
-  url "https://github.com/pmd/pmd/releases/download/pmd_releases/6.51.0/pmd-bin-6.51.0.zip"
-  sha256 "0577ae83e6f3cc82ea9e7fd0304043e1b81d001d24313c41cf2d8a9095d95891"
+  url "https://github.com/pmd/pmd/releases/download/pmd_releases/6.52.0/pmd-bin-6.52.0.zip"
+  sha256 "fa757430ee45bb79ce3d10bb5706b71aa7bd2f0df40c135b1ae2ffd2ae91810e"
   license "BSD-4-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "2901f82814ae825fdbd87bfeea9dfbaaaaa376595951380042cea4ed06011a94"
+    sha256 cellar: :any_skip_relocation, all: "1840276f7d52f3dcf3d5b02dd95b2e43f285e0a59a53d3f660bd92e0b15579e0"
   end
 
   depends_on "openjdk"
