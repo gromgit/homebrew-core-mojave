9f649c4dbb733fbcad53aeb4a710d61e4f94e733
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:19:50 +0800

    configen: no rebuilding required

--- Formula/configen.rb
+++ Formula/configen.rb
@@ -10,6 +10,7 @@ class Configen < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "1c460c8a01dbdd8cccb66749dea6543bb63430129ea4717ecc9a03e27b6674bd"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4a677709fb477a2e97d7791393044062418f5143928cf69d2d373e075c1209cc"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d7aa87ea082759093c1192bf7e0ee69c1146ef4626534731ff506a83ec682641"
+    sha256 cellar: :any_skip_relocation, ventura:        "ef53a9ef9dc4b97d7d735ba7b75663734b4f4d84020610ffb724bb3bd2ceaafb"
     sha256 cellar: :any_skip_relocation, monterey:       "3fde28a899b11fd837c3d30b27b26485a1e33f7bf951fe469d7fe8ea6ec41e0b"
     sha256 cellar: :any_skip_relocation, big_sur:        "78a7c0604f2a98b2f488b2bfefebff47e08342e69d5f47b7123f15f71bcb9653"
     sha256 cellar: :any_skip_relocation, catalina:       "9bdb2988618d5a1e9291a8579207d9dad1092f377d29d13af68cf6ef5afcb202"
