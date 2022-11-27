b7414f76023903b237f5c8650ed6512dcacba800
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:42:17 +0800

    libxmp-lite: no rebuilding required

--- Formula/libxmp-lite.rb
+++ Formula/libxmp-lite.rb
@@ -9,6 +9,7 @@ class LibxmpLite < Formula
     sha256 cellar: :any,                 arm64_ventura:  "ef306919a426ff7cb4bbeb9ee62191ea08539b25776fbb386f501c7cf7de3a07"
     sha256 cellar: :any,                 arm64_monterey: "026c3cdd675caf62652f8f2a1c5c4b6183565d1ac9bc0aa8f439926e496fa865"
     sha256 cellar: :any,                 arm64_big_sur:  "3e5ad6d3a6c5e8f8e78055aa92d7b92bcc28faf2ae4904154b28d8d5cc83426e"
+    sha256 cellar: :any,                 ventura:        "e3b45143abda1714c0a13c2d061582bc16857da21b4281a6f8f646d3a18d87ca"
     sha256 cellar: :any,                 monterey:       "ecd2af08822aa07e9b7a2a3b872a368979de55b8d359a22fbd6f4826175e24d2"
     sha256 cellar: :any,                 big_sur:        "da41c0decc2231fc35a884c1bfa61240261958e6cde77d863c3d62dd34830d5f"
     sha256 cellar: :any,                 catalina:       "2fd028d14695096bcb8fb8839758fb962c36d3edfd83e959a652bd735d4684b4"
