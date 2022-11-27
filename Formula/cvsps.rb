687eb9dd9591e822a08fd2575b718fd441848f50
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:21:11 +0800

    cvsps:

--- Formula/cvsps.rb
+++ Formula/cvsps.rb
@@ -3,6 +3,7 @@ class Cvsps < Formula
   homepage "http://www.catb.org/~esr/cvsps/"
   url "http://www.catb.org/~esr/cvsps/cvsps-3.13.tar.gz"
   sha256 "5f078a6e02c394f663893751f128caf643fe00a30b559e87db6f45190c623799"
+  license "GPL-2.0-only"
 
   bottle do
     rebuild 1
