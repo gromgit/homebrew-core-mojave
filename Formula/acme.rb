3557031db7b1c41153cc8c4aa6ee34fc0016f222
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:12:19 +0800

    acme: no rebuilding required

--- Formula/acme.rb
+++ Formula/acme.rb
@@ -15,6 +15,7 @@ class Acme < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "8f8b137715ebc611f81b2e49445396488a26a0240a7e9fe24f8fb897aecf964f"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "2c56301699b1c2419655c5fac5e5a1c16767729d7a4afa9560231ecdd60972d5"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eed2df7b934a52ca875e02a7b89588ac602cfa0cfbde0e795bbcdaff72bb5201"
+    sha256 cellar: :any_skip_relocation, ventura:        "4de66d662e3631ae88f197b7b28045407b929d05d5d844cc7f3569696df7078a"
     sha256 cellar: :any_skip_relocation, monterey:       "057df491fd1784cad46df8397ddd2cb972c256d094b5849731899bd03163184a"
     sha256 cellar: :any_skip_relocation, big_sur:        "7890b8c1a32b202ab913553d534db373de3d61bb274a564fb9304cd4de043736"
     sha256 cellar: :any_skip_relocation, catalina:       "54080f9a08a3f958c5a024fd536c2308c392521a4a4092afb115f368b3256fd2"
