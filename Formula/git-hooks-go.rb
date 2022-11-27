75836a254e6c42b4a2777e15843e618910d43c9b
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:28:33 +0800

    git-hooks-go: no rebuilding required

--- Formula/git-hooks-go.rb
+++ Formula/git-hooks-go.rb
@@ -10,6 +10,7 @@ class GitHooksGo < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "1d0e39f15e97b5304db3ffdc34c9d5425340adbfde20bbf1c7f42a3e9b60b223"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "0b44ba2ea899d62f65370ebbf36356d2534ccd3af038f72e06774252f8770546"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0162bfccf604080a5c520a02bf84cb006390935f8cc59c0ef4c1f7f08d071cbd"
+    sha256 cellar: :any_skip_relocation, ventura:        "ca8f15aef832912d96b0df4f61890c61e61e3e4fd174aebdee7ba25e7310d167"
     sha256 cellar: :any_skip_relocation, monterey:       "cfd0c4ba88b9fa6debe95d147e069636e78093976eafefb0e245185a313a6a8a"
     sha256 cellar: :any_skip_relocation, big_sur:        "bb65c1d92db2e31b8d3d2447e3c4642a1865658f9d8075a381439ca311b2ddde"
     sha256 cellar: :any_skip_relocation, catalina:       "c297503f6623a3c258c84a887225f3690433a16e97492f7071cc0c3ebee0d073"
