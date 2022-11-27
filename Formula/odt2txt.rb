d319af2c9986a7eb1149b347c0f291e32bd93b9d
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:49:19 +0800

    odt2txt: no rebuilding required

--- Formula/odt2txt.rb
+++ Formula/odt2txt.rb
@@ -10,6 +10,7 @@ class Odt2txt < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "6da6cd6c262ef01c0f0c7cf9de4e4897f255bcf5266313a373c6b89ebc15d162"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1990d5bd2ed1d5d9b5f7165ecd8285ded82ff8ed0d622b5f9820e9bc2123252c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "00a813eeef30b44f3c760435055a40f5471aa64fac6f390fcbc5bea64f34e9cd"
+    sha256 cellar: :any_skip_relocation, ventura:        "d0beae42729ad6c48197a6575aa419f1c9b9c742252ce70b3105a3d19a2a6815"
     sha256 cellar: :any_skip_relocation, monterey:       "0567b8e8d71e49da55816d890481a29b0a173a88ec59ab5141a2b7581cff8e0c"
     sha256 cellar: :any_skip_relocation, big_sur:        "255a40ee5035ec02702587440eee33cbfd83d110daf1c90a965c4de5f92a34f0"
     sha256 cellar: :any_skip_relocation, catalina:       "31e17f05898b06469cbc33244f357c61baf059120e96b34d472325e38adfa4d7"
