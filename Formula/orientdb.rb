689d6a560b46fd940156f8d2fd0fe75d1f15dd3b
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:50:33 +0800

    orientdb: no rebuilding required

--- Formula/orientdb.rb
+++ Formula/orientdb.rb
@@ -1,8 +1,8 @@
 class Orientdb < Formula
   desc "Graph database"
   homepage "https://orientdb.org/"
-  url "https://search.maven.org/remotecontent?filepath=com/orientechnologies/orientdb-community/3.2.11/orientdb-community-3.2.11.zip"
-  sha256 "5caca4a8532c4b13b28b156ee344da8f3af8a1aa6c23c276c296b820c1192b1c"
+  url "https://search.maven.org/remotecontent?filepath=com/orientechnologies/orientdb-community/3.2.12/orientdb-community-3.2.12.zip"
+  sha256 "6a1c28ac67f680966ce448e56b213c66ebc3d4ff8fabc8b78901a492908e5e25"
   license "Apache-2.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class Orientdb < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "ecfdb6e49e879d4ebb29d6c585574b1ce63516f986fbdcc3ff1f6764f7e5b5f1"
+    sha256 cellar: :any_skip_relocation, all: "ed81585f2c7c5d115515aabb319cb80bef5438ff707d6c600df57c4e76bd55a4"
   end
 
   depends_on "maven" => :build
