595a4c69f401accc7f723e4f70a1edae4a09083e
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:58:57 +0800

    signify-osx: no rebuilding required

--- Formula/signify-osx.rb
+++ Formula/signify-osx.rb
@@ -7,8 +7,10 @@ class SignifyOsx < Formula
   head "https://github.com/jpouellet/signify-osx.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "61d6a3ff6667bb16d42a052ed831e635048647d2d6bb0b0828d03a8c0b8da1cf"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "b524debbee38eb3d651123e84189121d4249051dec29bf21c02d8e094916cdd6"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "04f7c99002246fb5765666759b9a5a1f7e461a6d2d0c77e360af77951ee5de97"
+    sha256 cellar: :any_skip_relocation, ventura:        "b3118edf392c51017526fab8898c14855b9e3531fdebe3a0d98b65faf279e341"
     sha256 cellar: :any_skip_relocation, monterey:       "f3b58c34d1a01564e16b46364359f42330e1279f2a90025ce7541c9d5f69f370"
     sha256 cellar: :any_skip_relocation, big_sur:        "0a60c4b4955d38a1effe307e2326373c069621c1135e28820783aedd5aba9788"
     sha256 cellar: :any_skip_relocation, catalina:       "74a8c2fa3d258ad59a5ab7302411a194903ea5295fbf5ecd95a43c2ac28677f4"
