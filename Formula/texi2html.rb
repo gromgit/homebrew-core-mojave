29b2910474676879038a985c86e3dea1fee569f8
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:02:48 +0800

    texi2html: no rebuilding required

--- Formula/texi2html.rb
+++ Formula/texi2html.rb
@@ -14,6 +14,7 @@ class Texi2html < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "7114d60ab173de6a576098ba484cb226ad2a65c3f6ef2f668f8c5c2d0676fc2f"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "7114d60ab173de6a576098ba484cb226ad2a65c3f6ef2f668f8c5c2d0676fc2f"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "03feaacb6b615ca2dda676bf5fe4f9551c488a851ccd1e89b12d257a5c7d932b"
+    sha256 cellar: :any_skip_relocation, ventura:        "f1d5a70912d5b39d5da118a19474ec509a34a903cefb2c00ff403569325fe0ae"
     sha256 cellar: :any_skip_relocation, monterey:       "f1d5a70912d5b39d5da118a19474ec509a34a903cefb2c00ff403569325fe0ae"
     sha256 cellar: :any_skip_relocation, big_sur:        "4ad9c71802c3258a3c0c7ff8800ddd70cc230ddfecc095080d0144ba153bc2dc"
     sha256 cellar: :any_skip_relocation, catalina:       "10f6d76de400799fb21dc900a2344ef444d43658dd502f0c040ad7c0a4bf0fbb"
