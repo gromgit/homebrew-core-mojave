c97e7b7d5b7064bb64d1724429a08dc66ffe0338
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:53:08 +0800

    pngpaste: no rebuilding required

--- Formula/pngpaste.rb
+++ Formula/pngpaste.rb
@@ -9,6 +9,7 @@ class Pngpaste < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "6a78c376be51591db8cee5254f92b275a0d7c5516b96818400b60786a6b1a7b6"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1199369118afc2095238084d9b632fc8d277b17d4a73d2edf40582e4d329614f"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d28443efa53d8c51e0ba85a6985506cc21aad15a346df76ff04c2eea0acd33ff"
+    sha256 cellar: :any_skip_relocation, ventura:        "b41718216a8c084f6c8aa9324e17083daa05dd4a1099e71f0e317cbfca2f92d6"
     sha256 cellar: :any_skip_relocation, monterey:       "e9a350b69811aa02e6b01d906cc184f35a8806e322bf62430b81bff8a6d27fd2"
     sha256 cellar: :any_skip_relocation, big_sur:        "20d394d6036f0ffe382b36151c15d3ea9b20ce9d1e5fe6166ce11546c5e871f4"
     sha256 cellar: :any_skip_relocation, catalina:       "692e8f099ee7426310daa078d6bf2103b763b4549804f1775a5238acb1ead616"
