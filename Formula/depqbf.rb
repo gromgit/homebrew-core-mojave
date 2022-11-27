53fda8c6505a732de33173e1c097a5ce9ee7c750
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:22:00 +0800

    depqbf: no rebuilding required

--- Formula/depqbf.rb
+++ Formula/depqbf.rb
@@ -10,6 +10,7 @@ class Depqbf < Formula
     sha256 cellar: :any,                 arm64_ventura:  "221268b1bc924d55e1f8f3554b85f4ae91475792834f699217b03cce46ea63a3"
     sha256 cellar: :any,                 arm64_monterey: "204b1a36f581b9609dcf0a47c0778169d0f26748e6a5fd869a1e7bba826be6f8"
     sha256 cellar: :any,                 arm64_big_sur:  "afc477a7b941f95abf0a3e7db86d60b2ee9ef9e8b2f4ecb84d84044a9dbb0bdf"
+    sha256 cellar: :any,                 ventura:        "209da2f6f71ffd18105f99dd6333a0547865da8725e109138fe9e3e138b8300c"
     sha256 cellar: :any,                 monterey:       "135536ee418fef5b3e8002301dca15913770d3b2d81c8b08b9bb2fef67bb56cc"
     sha256 cellar: :any,                 big_sur:        "e86513b7cd6ad6ac68c7aa8a1738d8586fe6e20a7a46237dcbc3d54d735ff6d0"
     sha256 cellar: :any,                 catalina:       "432518e2ccee50695a9e79b4fe558142d78945ef96fcdbf7cccf090d72ec6543"
