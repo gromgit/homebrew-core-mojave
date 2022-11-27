63274566bc0c280e615fadf535499b109d182b27
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:37:42 +0800

    lcs: no rebuilding required

--- Formula/lcs.rb
+++ Formula/lcs.rb
@@ -14,8 +14,10 @@ class Lcs < Formula
   end
 
   bottle do
+    sha256 arm64_ventura:  "af4b1c073592ae9b4f5f168b00c13fd02fddd09be18938c0d392812ae36a507b"
     sha256 arm64_monterey: "fe4700d7dbd9901cd46e1fdd19453b308d918391914bac529059b25f229b7bc9"
     sha256 arm64_big_sur:  "1ec069485376de05c00be777102bcef25f3f1349d84ecfc2e53990d6c6e403dd"
+    sha256 ventura:        "3ee4e2b8a059045bf17d7d0b32860c1ec4ae23a996c9babf5a58efa0871659d6"
     sha256 monterey:       "a90a57e3001f38f79b787bbaad00e38099fcac09da06780f2996ae0666d80420"
     sha256 big_sur:        "9e3c6957bab58eaf828f8420fd7e493bb352544ec552c96eb24c8d1ec8d4adc6"
     sha256 catalina:       "65391613e5fd3ea3d8e5f7a2d5586105e3408bb09cddb77aebcfd4bcb7e3396a"
