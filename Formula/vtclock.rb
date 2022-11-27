6aee2972f3ba6037fbb7fadb8d7c7068bd9ff40f
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:06:34 +0800

    vtclock: no rebuilding required

--- Formula/vtclock.rb
+++ Formula/vtclock.rb
@@ -13,8 +13,10 @@ class Vtclock < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "25259a2e776866b62ab94a2b3540f212ff1210fb5bb6aa8bc64a979f771053a2"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "89f35de639c75067c0b64b5df9642a6b08652b8574eb403f2d51038362b7c16e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7ba3ce880fdedf82e166ed0f7f7a4124c57510680d5c591fd363ac14887618f5"
+    sha256 cellar: :any_skip_relocation, ventura:        "a9d0ccf5a02245e652c282d971eaf01115c97a43b176d4f2f4d41656ba013112"
     sha256 cellar: :any_skip_relocation, monterey:       "d8623d74ca8e085cba72cfcfdd04c87327dd5bd9bed6b6f0c5b60181448922fa"
     sha256 cellar: :any_skip_relocation, big_sur:        "747b98b591c6a732012006db26bc8cdd871509e6407f557a1b616c54d2c2079d"
     sha256 cellar: :any_skip_relocation, catalina:       "77d6846683ebe827ad322e81cd281e79d28b5e93122aadf32c49a1566b1c9f2e"
