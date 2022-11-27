9c0fe72f6a8417ce2b6749a4f62544165ed300ea
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:08:00 +0800

    xcb-proto: no rebuilding required

--- Formula/xcb-proto.rb
+++ Formula/xcb-proto.rb
@@ -6,21 +6,24 @@ class XcbProto < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "2b0c1406f4a3f07a16b071a3641defce88bdf8c8ffa93884e2773572818029e5"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "7aff8671654c8a9bde8b257cff31f05a6f09a322f35c16c5d985eb525faae8f2"
   end
 
   depends_on "pkg-config" => [:build, :test]
-  depends_on "python@3.10" => :build
+  depends_on "python@3.11" => [:build, :test]
+
+  def python3
+    "python3.11"
+  end
 
   def install
-    python = "python3.10"
-
     args = %W[
       --prefix=#{prefix}
       --sysconfdir=#{etc}
       --localstatedir=#{var}
       --disable-silent-rules
-      PYTHON=#{python}
+      PYTHON=#{python3}
     ]
 
     system "./configure", *args
@@ -30,5 +33,10 @@ class XcbProto < Formula
 
   test do
     assert_match "#{share}/xcb", shell_output("pkg-config --variable=xcbincludedir xcb-proto").chomp
+    system python3, "-c", <<~EOS
+      import collections
+      output = collections.defaultdict(int)
+      from xcbgen import xtypes
+    EOS
   end
 end
