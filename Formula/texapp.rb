f05b2547fd6003c8eb001edf0dda78f1a4628d31
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:02:47 +0800

    texapp: no rebuilding required

--- Formula/texapp.rb
+++ Formula/texapp.rb
@@ -5,8 +5,10 @@ class Texapp < Formula
   sha256 "03c3d5475dfb7877000ce238d342023aeab3d44f7bac4feadc475e501aa06051"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3de41340c8be2327e079f024ac9c4cc0ebe27c937934f827b53c20869cebbf88"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "3de41340c8be2327e079f024ac9c4cc0ebe27c937934f827b53c20869cebbf88"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f347b14baaf4d85e9cb2e1655b14e56734f2155dd450a47c66a93a2103f4528b"
+    sha256 cellar: :any_skip_relocation, ventura:        "3de41340c8be2327e079f024ac9c4cc0ebe27c937934f827b53c20869cebbf88"
     sha256 cellar: :any_skip_relocation, monterey:       "3de41340c8be2327e079f024ac9c4cc0ebe27c937934f827b53c20869cebbf88"
     sha256 cellar: :any_skip_relocation, big_sur:        "fb4cd0f1075e83c70659b71c6700819d3fa8004fdf9159081fc02258ff656e4d"
     sha256 cellar: :any_skip_relocation, catalina:       "416f71ee320ac9efd58e5da5cb91cae807c61d542e90ad624bc778e4b060dfed"
