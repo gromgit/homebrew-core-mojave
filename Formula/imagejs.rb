8665fdae077447bc803ef531feec61e99507cd83
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:34:25 +0800

    imagejs: no rebuilding required

--- Formula/imagejs.rb
+++ Formula/imagejs.rb
@@ -16,6 +16,7 @@ class Imagejs < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "e76ab06468439a91138e802c3da8aa8d422a659aebb2abc94fea8a8f576d0b71"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1297a2272e34c7bd91997cb8ec161fac1694089d5e4daeaa2a9377714e197d38"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f03279a8e5c316d74b2b93939714aa16dc624735ca8bda89b20468bc346c4216"
+    sha256 cellar: :any_skip_relocation, ventura:        "e2ad3d26c4cf47649428a26612ebf2db567717311fac840f894526c52efc423a"
     sha256 cellar: :any_skip_relocation, monterey:       "8cd267264a5a90805ce6271406f149e6401f04e851243bf89b6ec70a2975cc92"
     sha256 cellar: :any_skip_relocation, big_sur:        "99e906e8eeb8451f8c2f8408aa990cddb575a02be4cdc5d4ea3f95362d040633"
     sha256 cellar: :any_skip_relocation, catalina:       "7bddae8dab41f73bce7acb3c86a6dc01dcd3edeb5e0abf80b155e498372b8e5e"
