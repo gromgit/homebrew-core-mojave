1b0baa937f6a5bffaf1323a8494554a7c86d6c88
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:56:05 +0800

    redir: no rebuilding required

--- Formula/redir.rb
+++ Formula/redir.rb
@@ -9,6 +9,7 @@ class Redir < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "236941203f68e2338ee2e31c8fc621d97188f8961d691621ae7d4df16c7dcb8d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "31d05c824bc5c340c1bbd775c00bc8803a39e528f8435e78bc7ef284316ce07e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "11abbdbbf74827eaf72cef5c1c2b7a84fc79010a00afc11f69b5797f44449479"
+    sha256 cellar: :any_skip_relocation, ventura:        "9967e8540c4764720846ba3b592fd1ab902953194a87d2f034ed2768bd8fbd76"
     sha256 cellar: :any_skip_relocation, monterey:       "9981c02e789e34dbdf80c39509768b7f3142e25ae15a24a8830993cd40c43fd8"
     sha256 cellar: :any_skip_relocation, big_sur:        "d57505000ee6d3eafe86ede2a16d9902a11378bd9071cbd0fab1773d53bc5bfd"
     sha256 cellar: :any_skip_relocation, catalina:       "a811c4d0057b6d190a615a1da5cfb6dbb7321310f41da5141397295e31ffe354"
