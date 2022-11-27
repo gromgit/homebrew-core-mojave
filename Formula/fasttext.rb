9cc9f52ff5add9baf550abafe2730659df799214
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:25:22 +0800

    fasttext: no rebuilding required

--- Formula/fasttext.rb
+++ Formula/fasttext.rb
@@ -7,9 +7,15 @@ class Fasttext < Formula
   head "https://github.com/facebookresearch/fastText.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fasttext"
-    rebuild 2
-    sha256 cellar: :any, mojave: "e62af9a3264c310fccd0bc1b3f8675f120cdb46ad2c99d78823b638f779b47fa"
+    sha256 cellar: :any,                 arm64_ventura:  "891397ee186031bd9cda06521af21d17f1ddad2f493353320c924bb9f60aca76"
+    sha256 cellar: :any,                 arm64_monterey: "5a2a2a202ee6d5b21bc1857be97e41876353e9ef9c4a2af5466b7def501bc1ce"
+    sha256 cellar: :any,                 arm64_big_sur:  "3bfb7e1ab42dde74ac0692d2016f718e173b9e9dee093e408dda8f4c22ef1a8a"
+    sha256 cellar: :any,                 monterey:       "7b1b5a4cbb2ce1de373b18eec3b8cdaeb1e5ac144b0f191fca05b977cf54c10e"
+    sha256 cellar: :any,                 big_sur:        "3869650705430f8b682416be4e7c0a01c243a2f9517c6668027c6e9576f1e9c6"
+    sha256 cellar: :any,                 catalina:       "ec085551ced1f55b863a65aa60ad8f31d796002702b7effaaaafbf1490df867f"
+    sha256 cellar: :any,                 mojave:         "79f08167fb55b478829434be84d919c08c888563e0abbdeb66bc19cd3e82457f"
+    sha256 cellar: :any,                 high_sierra:    "4602a32c2a373ed97de8fd36bf1e998299682d45e465af39026a32a3a06fe574"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8a90d77cb50372ab1172b354ee29e813f26543067beac35dd24b3da882352718"
   end
 
   depends_on "cmake" => :build
