6595d9961b42a77711284655bccc3660b866a678
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:28:56 +0800

    gitql: no rebuilding required

--- Formula/gitql.rb
+++ Formula/gitql.rb
@@ -10,6 +10,7 @@ class Gitql < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "a137abb5c33caae3f028dc0dd9d5de01233f186e411e85c5f15e9498b8db6ba7"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1cf057b9ba370bc1f4f5c7f2a28df41484304a9acd1578179c9bbe235b187218"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9c51c047a1b0a54fc0442b8116cab4ac4fe15e32a81e1eab298d7ae335350abf"
+    sha256 cellar: :any_skip_relocation, ventura:        "a6314e3549f1d86138a9eb7faaac2b18393e2d69232ef4fcc99687c4a386e550"
     sha256 cellar: :any_skip_relocation, monterey:       "c49e5de44b1ca2e817df8fec931424773605fca428a8552d72bf8f2e4201f738"
     sha256 cellar: :any_skip_relocation, big_sur:        "4da2c5aeb11978212534942830e5dbf06df531e9c20ae13f046fb3c9a8cacee2"
     sha256 cellar: :any_skip_relocation, catalina:       "2a12d8521aa575a1d6026747e3ba1b8aa7887d2bbe260965fe0eea13ff97a5d6"
