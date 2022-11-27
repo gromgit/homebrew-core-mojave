6d5ecba745bdb09b6159cf221ede087726d26769
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:14:19 +0800

    autoconf@2.69: no rebuilding required

--- Formula/autoconf@2.69.rb
+++ Formula/autoconf@2.69.rb
@@ -13,6 +13,7 @@ class AutoconfAT269 < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "fa8878f7ae82d8c8b2f1de7d330ffe52d797aec62c955fd0d62bcf5557ffd4b1"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "fa8878f7ae82d8c8b2f1de7d330ffe52d797aec62c955fd0d62bcf5557ffd4b1"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f7b28e5cdf538418baea43d1d5638a1df52161ef0cd198ee1f261cdc61ac6636"
+    sha256 cellar: :any_skip_relocation, ventura:        "210d6b15bb404f7711955bfeb60dce45a8bd34bda84d043ea32552571bb3d1d6"
     sha256 cellar: :any_skip_relocation, monterey:       "b06ba90b16a3d6d38a2954e5c02710a4225a7e82bd1a5c30e1b7574dbfd1b2b4"
     sha256 cellar: :any_skip_relocation, big_sur:        "e4a0ef0b0b653836a212225fbb5345fb58e898ed7a24cb8386a4169496bbfde3"
     sha256 cellar: :any_skip_relocation, catalina:       "e4a0ef0b0b653836a212225fbb5345fb58e898ed7a24cb8386a4169496bbfde3"
