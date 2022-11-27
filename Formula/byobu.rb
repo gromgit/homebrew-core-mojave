c4b0f33d3e12a8a137d196c422a99148e6b17354
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:16:47 +0800

    byobu: no rebuilding required

--- Formula/byobu.rb
+++ Formula/byobu.rb
@@ -4,31 +4,35 @@ class Byobu < Formula
   url "https://launchpad.net/byobu/trunk/5.133/+download/byobu_5.133.orig.tar.gz"
   sha256 "4d8ea48f8c059e56f7174df89b04a08c32286bae5a21562c5c6f61be6dab7563"
   license "GPL-3.0-only"
-  revision 1
+  revision 2
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "45b2277b96cd2c4c4170fbbe4a8ac92665062e8d0c835bfbd24ee3e3d52f2616"
+    sha256 cellar: :any_skip_relocation, all: "567ec86facce7f9acf776836e49fc4742cd22a34f38a3869e1c2c8356290cc71"
   end
 
   head do
-    url "https://github.com/dustinkirkland/byobu.git"
+    url "https://github.com/dustinkirkland/byobu.git", branch: "master"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
   end
 
-  depends_on "coreutils"
+  depends_on "gettext"
   depends_on "newt"
   depends_on "tmux"
+
+  on_macos do
+    depends_on "coreutils"
+  end
 
   conflicts_with "ctail", because: "both install `ctail` binaries"
 
   def install
     if build.head?
       cp "./debian/changelog", "./ChangeLog"
-      system "autoreconf", "-fvi"
+      system "autoreconf", "--force", "--install", "--verbose"
     end
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--disable-silent-rules"
     system "make", "install"
 
     byobu_python = Formula["newt"].deps
