000eb03dc646e58b3fb4c61a40d7f3d417743764
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:37:32 +0800

    launch: no rebuilding required

--- Formula/launch.rb
+++ Formula/launch.rb
@@ -12,9 +12,15 @@ class Launch < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/launch"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "15160204f16532550741151d2954ebbf21515c1ee7a10ac33d883734b6cd1abd"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "df847f012511a511c1e2b9b70dd171a7a8e3d8a829cc58cb20a113ef6dc5a526"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "37a22a51899be9434558af2a787f253c90af7b1dc5dc17017096b221c0b85dc3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a8bc9136d5da0270c108662086bebfd5cf74cd5eb82a773318e9be1b7a61b986"
+    sha256 cellar: :any_skip_relocation, ventura:        "55dd24d6048328483994c1d22a56a4c26317399322ec52fbf81968bc1b64a4d0"
+    sha256 cellar: :any_skip_relocation, monterey:       "4813f636dd057ce7e61e02019d3886e2519ca4189c2ee1a98a2f9fa111412225"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0190475edf924787849170f68ab44589bbb41e8eb8e72dc86fefb4f15954ce00"
+    sha256 cellar: :any_skip_relocation, catalina:       "1d48da3f7c9c226fe622e83b1ff37bca0b960ab6979b01f2bf2e4b8010febacc"
+    sha256 cellar: :any_skip_relocation, mojave:         "d9eddaed19bcf6f70a4d6039028cc95693a616006541bd07e3ccea619f462ad8"
   end
 
   depends_on xcode: :build
