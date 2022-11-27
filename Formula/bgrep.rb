0452645e40abe086b3dbd12b8b8ee53122414503
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:15:33 +0800

    bgrep: no rebuilding required

--- Formula/bgrep.rb
+++ Formula/bgrep.rb
@@ -7,6 +7,7 @@ class Bgrep < Formula
   head "https://github.com/tmbinc/bgrep.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "b808abe4e0fd7be98cd1d9916c7e839f95cb086d28a987101aa51a73c22da87f"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c65ddc4ca486db177cd63f45e4cb97172f590ed9b2f70364120b2326e7ab3f94"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2d5628a1b93a4ad2e770502b011140bc301051e1679ac5d59eadbd9b94944b1b"
     sha256 cellar: :any_skip_relocation, monterey:       "2264b9b3c17b3faa5c66f612ce460a65e02bf0f3c3620002c90866c699b5cf81"
