2991784d71f57c39d44f241cbaa51dc30e4aebe4
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:17:25 +0800

    catimg: no rebuilding required

--- Formula/catimg.rb
+++ Formula/catimg.rb
@@ -10,6 +10,7 @@ class Catimg < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f7c748a7eab313176ecf7f82a9a4bafb26417dfec707fe041dd5f16033968e26"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ae44d54095f21fcd876b32c951923a3da0f2fd8f3ba2fe80520ee61ce5972992"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f5537238f20cc678e14f52ecdc1bdbf2b9d20d58d51a322ae044bad5c0df2418"
+    sha256 cellar: :any_skip_relocation, ventura:        "892769c3e47215a28fe78b4dc5732a1d173cea66586df1277de35c840c06117e"
     sha256 cellar: :any_skip_relocation, monterey:       "593c2a23f3f4c50b969ac816067faea700f62195c21e9566180c5e46312bec30"
     sha256 cellar: :any_skip_relocation, big_sur:        "4ed745935b27937d85de5e28e9f3345be90bfc725349247cb3b9770a720fe134"
     sha256 cellar: :any_skip_relocation, catalina:       "076781a169c35bba3b5bac8b4e5ea89497b9e21993da49739b4d3b690c4fad2b"
