8853c8c6142dbe6d36009e179d68cbf64ec9bebd
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:40:28 +0800

    libpcap: no rebuilding required

--- Formula/libpcap.rb
+++ Formula/libpcap.rb
@@ -15,6 +15,7 @@ class Libpcap < Formula
     sha256 cellar: :any,                 arm64_ventura:  "344abdd6c5cd76b355264c516d394084883ea0c8ffd0136cdff57f2b52e09c54"
     sha256 cellar: :any,                 arm64_monterey: "806a586f73f832dcc33b9c6019c8f8fd5e38c229abe5a2e84335bf6250e785bb"
     sha256 cellar: :any,                 arm64_big_sur:  "56f83e298a7906e44b44ac6d0c99f9a02d0be2ea4726089171a045543368ea9c"
+    sha256 cellar: :any,                 ventura:        "81a3f27918f451e1fdf46d3ebf9dc9d9523cf829003957befd50fd2f3fa62355"
     sha256 cellar: :any,                 monterey:       "3a77261badf57e93273aa3190348d34a27231e728cb0cd4d4c501e874bd7b2c6"
     sha256 cellar: :any,                 big_sur:        "0aee8b23fc3ede63cbd2cde3bbbaade84ffd6f2ac90315f9738fb872236a8c2f"
     sha256 cellar: :any,                 catalina:       "8d57e7519f14cfec3a1104237e52b56e838faa33b07cdbac05572533e5ae7e07"
