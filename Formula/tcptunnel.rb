af8fe63e5b6e5fdc735a0e447014b64c15040fc4
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:02:13 +0800

    tcptunnel: no rebuilding required

--- Formula/tcptunnel.rb
+++ Formula/tcptunnel.rb
@@ -6,8 +6,10 @@ class Tcptunnel < Formula
   license "GPL-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9503c7ec45671862d4d806931e056c9d0c6e7ea9cdda143bd17ea355b6c6a78a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "08ea4f67e68abd0d281d62a9f59f172b3ada2f84de3efba2c66318909686fd04"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f551ae0b42199f7636702669a5f32d4fb6bafef330036e8b14f3cfda556a4d32"
+    sha256 cellar: :any_skip_relocation, ventura:        "8eb0261ee4669e199766ea61e9b9f7ab7cd1b9ca51794f5d26650c542dc55e10"
     sha256 cellar: :any_skip_relocation, monterey:       "d75d983ef25fb64ae2d4fef51dd7c5a29451a3326ae99f2aaf24254d174d9f3b"
     sha256 cellar: :any_skip_relocation, big_sur:        "65ab13dc5646735a64d821e3eab7f04a55bd0739b83b36769b3d0664de74ed80"
     sha256 cellar: :any_skip_relocation, catalina:       "e82c25ab68b43d632739d345b3ac1c3a6d22a9c8a51d44f9cfc3967e64469794"
