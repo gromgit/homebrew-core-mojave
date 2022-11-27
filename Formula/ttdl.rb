80fd2c4de92c1ee2eadb59ca8f295bceb129808d
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:04:17 +0800

    ttdl:

--- Formula/ttdl.rb
+++ Formula/ttdl.rb
@@ -6,8 +6,15 @@ class Ttdl < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ttdl"
-    sha256 cellar: :any_skip_relocation, mojave: "c6cb60aa369b8e0280a1f36d5bbd57ca8c487cd1c2e75f1191ff4723c5dea01c"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "51bdbe7add454bc0aaf6a6ea87ae9fd645ebb3c7fd5c125c2eddfa9d86d539f3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c5f0a4938284784953d815f18a3c1bd7802beb9b5f5fe359532629676dd50293"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2b7ee2163cafa8983c0c011f1c11f0c0ccad766eb92a753001e6dcabb6f492cc"
+    sha256 cellar: :any_skip_relocation, ventura:        "d3e45eeacdeadae2e1adadddaf0e1f473df3e9bec8006b2157d27012316459d1"
+    sha256 cellar: :any_skip_relocation, monterey:       "eb88e39e38ab03a35b858b35f3f65bc8f7d01c2caac51278fbce7c92a3297dc8"
+    sha256 cellar: :any_skip_relocation, big_sur:        "9424472f8baa2352fb20c553257091ee0c5b3b44344678f4c812ff2f745bf0dd"
+    sha256 cellar: :any_skip_relocation, catalina:       "af1730a74106f8eab864433878f9e60217aeef539dcae12a77d8f812ede3d946"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "124a554e2865c53095f3bbfbac2e33efafc346bcb5949eca8eed3e34130237d5"
   end
 
   depends_on "rust" => :build
