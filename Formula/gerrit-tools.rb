a3c51aef055a0e2e95b94a5746c1c3e8af81ac59
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:28:04 +0800

    gerrit-tools: no rebuilding required

--- Formula/gerrit-tools.rb
+++ Formula/gerrit-tools.rb
@@ -7,8 +7,10 @@ class GerritTools < Formula
   head "https://github.com/indirect/gerrit-tools.git", branch: "main"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e944c907ac7f5bb3d468a7adfa14bdd52c22d0f0261b201f006407f2dc425506"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1d1eb15677d4ed0974a06c21e3ad17fe49b543acbea1ebc0052ef0b2900f65f0"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1d1eb15677d4ed0974a06c21e3ad17fe49b543acbea1ebc0052ef0b2900f65f0"
+    sha256 cellar: :any_skip_relocation, ventura:        "59fc3d02fe118038ef5c4b443b7c5126a693928b24dea6b685efedd4400956fb"
     sha256 cellar: :any_skip_relocation, monterey:       "e4094d8655a172f89aea151baea9298f35827c09fad7ea4d696e5c542d724c06"
     sha256 cellar: :any_skip_relocation, big_sur:        "e4094d8655a172f89aea151baea9298f35827c09fad7ea4d696e5c542d724c06"
     sha256 cellar: :any_skip_relocation, catalina:       "e4094d8655a172f89aea151baea9298f35827c09fad7ea4d696e5c542d724c06"
