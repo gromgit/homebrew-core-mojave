1f9b4ec6588b2932f64fbca091926f13e68f6755
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:41:22 +0800

    libtar: no rebuilding required

--- Formula/libtar.rb
+++ Formula/libtar.rb
@@ -10,6 +10,7 @@ class Libtar < Formula
     sha256 cellar: :any,                 arm64_ventura:  "5b5d861d3e7a24bfbb41d37e9ab4efe883e9f9403d01b6dc1509480f23f7f80f"
     sha256 cellar: :any,                 arm64_monterey: "02f257866b2d60bc629d5c35f70f41889ab2254de7f29533ab01d600979d74c2"
     sha256 cellar: :any,                 arm64_big_sur:  "7481da1834936b6237f152fe8b7e22196ad5c76833af39c9e9f74eae6347c9a5"
+    sha256 cellar: :any,                 ventura:        "d0e2280e4245eda925984747db9ef07b712ccf5e6de713ccc35289e7c6c01c42"
     sha256 cellar: :any,                 monterey:       "de9f3cf843c333e94657378a4b551386f81fe9f3afef5b69539de108330c3c4b"
     sha256 cellar: :any,                 big_sur:        "7424cf8229c7aea825592a76227da3355f32a43b9fbc5e140a0cf1eb07d05c8e"
     sha256 cellar: :any,                 catalina:       "35617f312e3c6fb1e473a5d20a559dcbd1815544bdd99c95419ac7e6e8abf9f6"
