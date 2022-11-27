04f0772294da1e8cbce6b4a57fc4574691c4d62b
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:27:25 +0800

    gambit-scheme: no rebuilding required

--- Formula/gambit-scheme.rb
+++ Formula/gambit-scheme.rb
@@ -12,8 +12,10 @@ class GambitScheme < Formula
   end
 
   bottle do
+    sha256 arm64_ventura:  "fb202eb0660fcfbf84dfdf80bb37dd16ed94da6d24898a336441cdd58573ccd0"
     sha256 arm64_monterey: "91040b009cdde2f167bff43b1521c1bae3b402c28bfe57a1d9fdc603b6e7eeee"
     sha256 arm64_big_sur:  "9abbafcba2c1205b675204642a58527262586625b99a2ae8a32d3b50076a87ef"
+    sha256 ventura:        "f8babf62d122b3f057798a10b9df2688db1994b7e99c68e9d28c17aa7527cb54"
     sha256 monterey:       "eaeab4965a7dc4b0bdeb880287af96157c4fb666897db2a1f23772b80837bc7f"
     sha256 big_sur:        "88dfbed920720584cea9ac1500cc59a7d6df69532e38728314594466bdf8a7a8"
     sha256 catalina:       "cc4d0841423822b27fd424f7eba3a0482f01266ef61c25ec4b1d49d211d6c50e"
