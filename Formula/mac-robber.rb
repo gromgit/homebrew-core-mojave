0cd9b404193b918950211a276186e25cd6f9e306
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:43:48 +0800

    mac-robber: no rebuilding required

--- Formula/mac-robber.rb
+++ Formula/mac-robber.rb
@@ -6,8 +6,10 @@ class MacRobber < Formula
   license "GPL-2.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3a95d6147c4806004b72f8dc70e77a4d3bcb4697dbf991193289facbf4d7b296"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "79d32b7d6cea8d48155f51c6f95ac062b0f905ccc2538b07674118a23371807e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3b884509ff648339c4d66a27fff082ac80b7762c831ae9aa5599419c68d86cfd"
+    sha256 cellar: :any_skip_relocation, ventura:        "5e6711eddd41368e2988dc8d834e0ff2470d9de53576bbb3a9ee757757f4a190"
     sha256 cellar: :any_skip_relocation, monterey:       "9a0cd671a8a723dc4a8207cfc7e5f08628d2cadda34debd8554a92beb3ad2ed5"
     sha256 cellar: :any_skip_relocation, big_sur:        "0cba6aa1a9eeca9b46559e0592b2b667d84d99f344781bce1b994aa5ad7a6e05"
     sha256 cellar: :any_skip_relocation, catalina:       "cb1d422835804b5ea784a2b9157ae77a0940902771397b235d4ad784b88f961a"
