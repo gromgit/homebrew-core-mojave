caf519a37b2f4b62617564ef155b8c5bee99b16e
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:54:22 +0800

    pulseaudio: no rebuilding required

--- Formula/pulseaudio.rb
+++ Formula/pulseaudio.rb
@@ -24,6 +24,7 @@ class Pulseaudio < Formula
     sha256 arm64_ventura:  "973ca6165c6909ac0bed7cb977ba80d85a19b9b9cf0033304dbd6f6cdec8ee2a"
     sha256 arm64_monterey: "ce44a5a697ba790ab27e97f4c96cd5f48489cdfc416a836348584064053eb725"
     sha256 arm64_big_sur:  "efcbf144da932e05394e9768bf27dfa1908dbb17f4b7c52f49e56c791dd51860"
+    sha256 ventura:        "0e9de748555e1f5594f92b91f2bd81db3fef1f55f5987c88893834865bb664d2"
     sha256 monterey:       "835e284178eda5eaa8395aab875305d8ba528336f657844d5791f29e0216d46a"
     sha256 big_sur:        "79684acaac85e9b1b7de55fc7659844d9508c6264faa0aac311e0d8eaf4056b0"
     sha256 catalina:       "e1c181ae27f945ceee403e2e2ec80f44aebd52ac44b8e63140c1c9d2083a643b"
