4a4b862dc3241cc711f6a3925f1ebe265c38ea33
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:33:26 +0800

    hostdb: no rebuilding required

--- Formula/hostdb.rb
+++ Formula/hostdb.rb
@@ -5,8 +5,10 @@ class Hostdb < Formula
   sha256 "beea7cfcdc384eb40d0bc8b3ad2eb094ee81ca75e8eef7c07ea4a47e9f0da350"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d1c397eae6bba51e1082904159e462f7cede76d7c3d1def04a0cfbc65cec17d5"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "d1c397eae6bba51e1082904159e462f7cede76d7c3d1def04a0cfbc65cec17d5"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d1c397eae6bba51e1082904159e462f7cede76d7c3d1def04a0cfbc65cec17d5"
+    sha256 cellar: :any_skip_relocation, ventura:        "15df2ae20a92ce1921922de57e085664cc31557f11fcee4dc4a214c7f4c949d9"
     sha256 cellar: :any_skip_relocation, monterey:       "15df2ae20a92ce1921922de57e085664cc31557f11fcee4dc4a214c7f4c949d9"
     sha256 cellar: :any_skip_relocation, big_sur:        "15df2ae20a92ce1921922de57e085664cc31557f11fcee4dc4a214c7f4c949d9"
     sha256 cellar: :any_skip_relocation, catalina:       "15df2ae20a92ce1921922de57e085664cc31557f11fcee4dc4a214c7f4c949d9"
