93e7c7bf36aea25cc26d81608319938b6f8c6be1
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:32:14 +0800

    gtksourceviewmm3: no rebuilding required

--- Formula/gtksourceviewmm3.rb
+++ Formula/gtksourceviewmm3.rb
@@ -15,6 +15,7 @@ class Gtksourceviewmm3 < Formula
     sha256 cellar: :any,                 arm64_ventura:  "35d19a368cbe09ef00c30b816e506665229bd2fcaa683594007ac3883433ae9f"
     sha256 cellar: :any,                 arm64_monterey: "0f6143ae59613281896b8e61962728fa445e0f01238be952dfeb8402e8de6796"
     sha256 cellar: :any,                 arm64_big_sur:  "787713d0f6802ea858aa728be88507a6d1b3adc6e3e648c34af059651393e83a"
+    sha256 cellar: :any,                 ventura:        "08044847e23c51115e5a4b8030d511857a9452addd35fd8bf7a0ce5b98095ae2"
     sha256 cellar: :any,                 monterey:       "e3f82bddf2add81a84854eac842b29528e19175bcf473a5c29fdecb21702b49b"
     sha256 cellar: :any,                 big_sur:        "e1cc731cb3f1d99a040da7719e8d91f325d7c00a46e19a10ffca8d1ec87991e8"
     sha256 cellar: :any,                 catalina:       "2909f29ff9dce4266ba101d992bb8831487cd2084f467faad39198ce6923b729"
