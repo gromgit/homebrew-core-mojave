f509521ddc2f3cbb5d36fc747ba57042a7e172fb
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:37:15 +0800

    kvazaar: no rebuilding required

--- Formula/kvazaar.rb
+++ Formula/kvazaar.rb
@@ -12,9 +12,14 @@ class Kvazaar < Formula
   end
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/kvazaar"
-    rebuild 1
-    sha256 cellar: :any, mojave: "48f37f2e5548729c64086aa0cbf16318f9893b4a19347041b0121a4078bfd0f5"
+    sha256 cellar: :any,                 arm64_ventura:  "82bad24051a2759adb516d79d2b35ceb2b5f920f81c5b7ec60095a1d8e8c38fe"
+    sha256 cellar: :any,                 arm64_monterey: "d84d0ad2462ff6c0fbe5ea7d2530c17d8d22629081fde5756fec48cd8ac5c409"
+    sha256 cellar: :any,                 arm64_big_sur:  "c2f8e51a5222f0b4f114df45c9a6439b84134c282a4147a3a8f5bd57aa297f14"
+    sha256 cellar: :any,                 monterey:       "2e8db48a773c5754ec93786ae52d7bb09c94cb40fa811d0b12457540fcc378c5"
+    sha256 cellar: :any,                 big_sur:        "68ec96b70d3098071e315f09d9b68d69971dd362d586e69f1c3d6938edc760f5"
+    sha256 cellar: :any,                 catalina:       "09205d5d2e16355e031d467315842030923f4459fed71576889f372f895f3b98"
+    sha256 cellar: :any,                 mojave:         "420616c74f44f1f604a2e4533d91bca6a4e014ae742d887200d927e007ae4fe0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d28aa580e27dade3635f38fa914f2975718e4e7ea2d89b7b1d46569efa6612b3"
   end
 
   depends_on "autoconf" => :build
