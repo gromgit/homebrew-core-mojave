e932334e324d0da0cc10e772272e17b13d170980
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:01:23 +0800

    svgo: no rebuilding required

--- Formula/svgo.rb
+++ Formula/svgo.rb
@@ -3,12 +3,12 @@ require "language/node"
 class Svgo < Formula
   desc "Nodejs-based tool for optimizing SVG vector graphics files"
   homepage "https://github.com/svg/svgo"
-  url "https://github.com/svg/svgo/archive/v3.0.0.tar.gz"
-  sha256 "24ff02b7d6413c48f94bfaf81cf251e79f20e453f26fc33dbec620b4dd9de6c3"
+  url "https://github.com/svg/svgo/archive/v3.0.2.tar.gz"
+  sha256 "fe0a790407c9055025607bf3d717d545f737f98ecb8af1cc21636020adc2c7e5"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "005e511a5ff8b56d009f3e7c69f9d7949cbedfc658eb1f4cdef1c61fe7c99d2e"
+    sha256 cellar: :any_skip_relocation, all: "684741c0ba97f3a8b5976b43eb874a8189742b48580d970ba80c3a10ee0dc863"
   end
 
   depends_on "node"
