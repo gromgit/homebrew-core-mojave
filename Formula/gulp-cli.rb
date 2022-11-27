1cedaa0b1401e3a88fe92c4aad841bc3583c49f8
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:32:20 +0800

    gulp-cli: no rebuilding required

--- Formula/gulp-cli.rb
+++ Formula/gulp-cli.rb
@@ -11,6 +11,7 @@ class GulpCli < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "e9f72528f44356b837abfe0ea3a09de449f56a8d7e5281ad46e3a3a0d42eec51"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "8ea471114a8812cf87daaea09ea00dc61297ac35c4227372a9c68aaa04ed1901"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "fc2ba1fab80c6b58a6f916e317141e155369834a16526ce271758f46384813a5"
+    sha256 cellar: :any_skip_relocation, ventura:        "185236b4489a9c64db268f73e0aed7a353eb6a6477192d1382de5e2bcfeefbff"
     sha256 cellar: :any_skip_relocation, monterey:       "32786bd48576d4da7d9ad45254c5a414b651ec4a385db0b422fa2496a3ee6bed"
     sha256 cellar: :any_skip_relocation, big_sur:        "e011832e0a4186df90c8b6131b1ec39e60540de336bd557879351011246cfcdf"
     sha256 cellar: :any_skip_relocation, catalina:       "231b635ddf8a704a3be4a6ba34611248ece69ed1de04fb82adfa6a20ac83fddb"
