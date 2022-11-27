4113f8e23a286f449bdd5818412e2d7d0acc7d6f
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:25:23 +0800

    fatsort: no rebuilding required

--- Formula/fatsort.rb
+++ Formula/fatsort.rb
@@ -12,8 +12,10 @@ class Fatsort < Formula
   end
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5c889cd549f7283bb46f5a6118355979a1a9df632809775a79bf12c4cda559ff"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "b7d4a8f9701c12a1d3dd1acf54c007e7e554c139e13b1ec0038ef9c1203cb77e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8b9d29782c670f94450fadca0c8f79027da00b123811f405b0d3b09075ab8d5c"
+    sha256 cellar: :any_skip_relocation, ventura:        "171b82442e7e70a45b64094434cb5b772cecb00e5347afb9e7bfa09c5ffe539f"
     sha256 cellar: :any_skip_relocation, monterey:       "422321e0d2f3d63e9d547a7e5b647cb6da4ba62f633c97aa065bfcbeb346a06c"
     sha256 cellar: :any_skip_relocation, big_sur:        "01b749fcfecc1b3a506b7f4e61562625861d693086803b1251491027be95e682"
     sha256 cellar: :any_skip_relocation, catalina:       "956dec116bc78d27db0553cf8f1bb9dcb499e22d7d162dfc1cd9ec13acc66412"
