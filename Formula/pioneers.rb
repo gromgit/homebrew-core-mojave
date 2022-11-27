00751b3490df6e4eddbb88d948012899242372fe
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:52:33 +0800

    pioneers: no rebuilding required

--- Formula/pioneers.rb
+++ Formula/pioneers.rb
@@ -8,6 +8,7 @@ class Pioneers < Formula
     sha256 arm64_ventura:  "df9880ff99449ca709eefd610061a0195de270ebac6fac9f6d1941db6a6dd2f9"
     sha256 arm64_monterey: "3742eced28912e519fc675ba5ccbff4ad9e324e73994e785b9aceb78fe61f6f4"
     sha256 arm64_big_sur:  "ef6e9c03dcc0c5d86ae0b08805352937c74d4ccaadbe4ac9aead56b5f7e0c7bb"
+    sha256 ventura:        "6d5c825e0a32001fe6fe04bcf44466a3545f80cffb4fbd1d2855523bf2483228"
     sha256 monterey:       "0e14d946b05d63aba6c15ad997bb4bc2998fe33c7293c14aec0763e8cfa89a99"
     sha256 big_sur:        "b9297939922709fc05a78ee98b24091678bf4e434bea4abfc14b089fde6e9728"
     sha256 catalina:       "c2462078e412d1e2b60108f317550690b28675da5a248de69856a558fa4e07d4"
