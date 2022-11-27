fbf6b1ba4da9a9527efe0ba73b2650c253bf2b66
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:04:39 +0800

    typescript: no rebuilding required

--- Formula/typescript.rb
+++ Formula/typescript.rb
@@ -3,13 +3,13 @@ require "language/node"
 class Typescript < Formula
   desc "Language for application scale JavaScript development"
   homepage "https://www.typescriptlang.org/"
-  url "https://registry.npmjs.org/typescript/-/typescript-4.8.4.tgz"
-  sha256 "8e0118a302a4930ef0799e9fb64ef0038dce546b7889603c744676afd9c93c20"
+  url "https://registry.npmjs.org/typescript/-/typescript-4.9.3.tgz"
+  sha256 "ed523eac657b57710511e36dee0f55a223c95f14edbd8cb9bc739838355efb43"
   license "Apache-2.0"
   head "https://github.com/Microsoft/TypeScript.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "bd7062b7f93b04cbd9efe3f434947d329cf388205db96231dbbf063e5377c79d"
+    sha256 cellar: :any_skip_relocation, all: "3f3af159603c2d64c8b7a04aa7a3105cd71f1b3cccb47d462c994f49aa2df705"
   end
 
   depends_on "node"
