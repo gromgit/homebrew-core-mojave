c61edd9915cdaecc37564f8c4f577560ae4149f6
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:02:10 +0800

    tcpstat: no rebuilding required

--- Formula/tcpstat.rb
+++ Formula/tcpstat.rb
@@ -8,8 +8,10 @@ class Tcpstat < Formula
   head "https://github.com/jtt/tcpstat.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "50c363371b5503288c97c1a00b5dc678aa2e121fc5470bb42676bb1c107fc7da"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4126408cb79eaf56b14fb122539a770f8c593c90576c2f23cc6cfaef2a094f54"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0f5880a02d97d890364b5e98871dabb0682bf1d73d43f6a2cf92f0039f29619c"
+    sha256 cellar: :any_skip_relocation, ventura:        "64eeee455c63e53025d2bae91de42a412c3459afe2cd7c080f6ba272f66f2b7e"
     sha256 cellar: :any_skip_relocation, monterey:       "c4a031f93d9e107740f63c329da289a7b8534d168b66326f67b3f0dc5da82e6e"
     sha256 cellar: :any_skip_relocation, big_sur:        "1a8c9f2f529162b1b5fecee421aaa0c99b80864f752717142fb7f77c5f5acc43"
     sha256 cellar: :any_skip_relocation, catalina:       "86c1f5aad64e2d611dcb9d74b4eb0f51f24f79d1cdcaf636f62199099d625fa1"
