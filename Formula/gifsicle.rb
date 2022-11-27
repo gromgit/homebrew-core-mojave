9ed5a820cb4d8f7ada26cdcc63cd414f8fa2097c
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:28:22 +0800

    gifsicle: no rebuilding required

--- Formula/gifsicle.rb
+++ Formula/gifsicle.rb
@@ -14,6 +14,7 @@ class Gifsicle < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "a217620aa3cf3c906a71e1d4c59e630e50150fa98968a9bf937abc60afb00bd8"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1e566d5e1a954201d5adf69c8eceba9c9bb037e8bc84a9fc36afc63b099c5e2e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ad499b676a6a5e433964de6303f658838239c09b69e02ce2db62b6c5ffc1a017"
+    sha256 cellar: :any_skip_relocation, ventura:        "da4a2920c8c761b708e58d9386be92da6a18b57bf9d5215fd0c17e95c7ac98c9"
     sha256 cellar: :any_skip_relocation, monterey:       "1dd2da2246eef8dcbd3297adc1db453906e22cf08b07e6007b2ba3293eeb3051"
     sha256 cellar: :any_skip_relocation, big_sur:        "75b269e600f8b3446694828d579f4425cfc1c49142e05441a902cdc48c39e143"
     sha256 cellar: :any_skip_relocation, catalina:       "88ac00c8f5e523b04db223a555a0094c62e30c05760ed9abcc5d0f3aef676686"
