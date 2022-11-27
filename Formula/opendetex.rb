6a6cd54db1a6325939c24be93e1d2d43f4cb31e0
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:49:54 +0800

    opendetex: no rebuilding required

--- Formula/opendetex.rb
+++ Formula/opendetex.rb
@@ -9,6 +9,7 @@ class Opendetex < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "ed55ca17def1bdc6fec261be8ddb46618fb1a310170796d1ad90bfadbeeee68d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "7b7c22303e3de7519dc20cc7862c6aa3ac49fa6bae8d3bd043c46c3930dea810"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9416484618318a11e895667857e7d8b39598bc31c2c1d8fbdbb7914176345e5e"
+    sha256 cellar: :any_skip_relocation, ventura:        "7562459cf4cb848fbc8ea3d3d1843355e6c200bb112890bf511ce9d67b6c61bd"
     sha256 cellar: :any_skip_relocation, monterey:       "18e2156648aac0c7429e14b30ebe1444cc6b95fd0ca99366fa39f81148bc5e17"
     sha256 cellar: :any_skip_relocation, big_sur:        "ce26ea02e5c47385374aba395951434319d5e48e6dbda94f7ffa25e4632b54a6"
     sha256 cellar: :any_skip_relocation, catalina:       "46db3f033cb646e360fcabc83eb6fabba87b858eb1cc3e32d4bad78e73816bc6"
