e8e6484f56b9f4f5c1b4195e9b4528fbd68627bb
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:43:17 +0800

    loudmouth: no rebuilding required

--- Formula/loudmouth.rb
+++ Formula/loudmouth.rb
@@ -20,6 +20,7 @@ class Loudmouth < Formula
   end
 
   bottle do
+    sha256 cellar: :any,                 arm64_ventura:  "439e0305cb6c6fa9305ef3ddeb6d35e330d35d245ac836fce6435c34bfa88e89"
     sha256 cellar: :any,                 arm64_monterey: "56ce5d15bd9625075e7160268936d0197a1116c1b3a095ebbbf190d2ac9becc7"
     sha256 cellar: :any,                 arm64_big_sur:  "0b60046b8a592ab656ed824b75774f2e9e8f9749b0a5edb024190019c36da766"
     sha256 cellar: :any,                 monterey:       "890ebdd35bf9275e771984de61b38557ff279e61d15e8c9d11813def0b65689a"
