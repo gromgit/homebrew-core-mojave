6dae8806f818b02a82c57f3c1328851d664ad6a3
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:40:54 +0800

    librsync: no rebuilding required

--- Formula/librsync.rb
+++ Formula/librsync.rb
@@ -9,6 +9,7 @@ class Librsync < Formula
     sha256 cellar: :any,                 arm64_ventura:  "cc61b71352f218c2097897616816c87438636d4087a15060412711a5614b2342"
     sha256 cellar: :any,                 arm64_monterey: "8f48bfa9495ff1e3f5dbb3c84a849205ee4307cfca4ca0d4c9fdc5dd04304617"
     sha256                               arm64_big_sur:  "e2672691faeaba727acde6252c3dba8a39b9a0703f942f8f830d6e5514e15bb3"
+    sha256 cellar: :any,                 ventura:        "8ac966da10799876d5b9e1065d2da6870cfd209c218977801e4d08ad5c7a7b2d"
     sha256 cellar: :any,                 monterey:       "4fa2dba2f40587cb7e1a3c837dae70c121509d858e44a5ec6bd564ba68c2cc3b"
     sha256                               big_sur:        "7561cdbc8327f77db0647112ae1496ca544c659a04dfe83e703c9edeee890869"
     sha256                               catalina:       "4d38d5dbea74b9eac4624877d7c7e29f08c38d68dedaaf9dadbdc5e3a820678b"
