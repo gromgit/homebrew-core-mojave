ab8449d471c0b12c740a355f6c808cee57e65a0c
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 20:07:18 +0800

    whatmask: no rebuilding required

--- Formula/whatmask.rb
+++ Formula/whatmask.rb
@@ -7,8 +7,10 @@ class Whatmask < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "106997f1c14a81903f0c3078308437938af0fded133488f5c7de9fd90cc4cbdc"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "93e5d651f3376c9194a94a3afa5dc7860bbe55ac675339e6c2d9951f57d1b075"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a5bf6f569bef04d197a6eb0c097450e65dcb5082b65ecc82201e15eb873ae755"
+    sha256 cellar: :any_skip_relocation, ventura:        "3a4ce45700a842a5db81df3403ddeac637a368cb6a0ddd514975fa9cb53381bb"
     sha256 cellar: :any_skip_relocation, monterey:       "dedc8e95cb750f18d6b8f03505387a17cc88753810f4e052b30b33126d9a8b8b"
     sha256 cellar: :any_skip_relocation, big_sur:        "55789adc6a9326b814965c6c0fcf41f912638f2e7d55d4167cbe404ec1a6938d"
     sha256 cellar: :any_skip_relocation, catalina:       "89a44972f8d27003b4c91f04a294f0be9a0d00628fb8db21faf46a55a0720cb2"
