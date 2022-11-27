dfa8b276c40d028b041afcc31bcf2befe2b92bfb
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:52:20 +0800

    picoc: no rebuilding required

--- Formula/picoc.rb
+++ Formula/picoc.rb
@@ -20,8 +20,10 @@ class Picoc < Formula
 
   bottle do
     rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "37c038b18d6e2dd02636901429fabaced73eb533eb1254e06bb3a74f53906a66"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4a572c33c2dc1b913132786154a5013d4c380bd1dcc5d64a696071ccb15b2589"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b39c116bb09cdd9f91143aaf9c928feadac8b281807e06ab87c4ca2992c13ec4"
+    sha256 cellar: :any_skip_relocation, ventura:        "9237788f85a665a1e368d6cae408450ea66ae3ad8c0fedcf3fc659ceab88f73c"
     sha256 cellar: :any_skip_relocation, monterey:       "f5483717ef3070c38ddd65011f312f8da456f74aa9c7f6b13480d4e40aa52782"
     sha256 cellar: :any_skip_relocation, big_sur:        "41d9836d62b70b7fe1117f4697417d9a03615639f2ff1390f0f1d21003426bb1"
     sha256 cellar: :any_skip_relocation, catalina:       "168aebca830b719b3645b682c9c3f1208663b6853b62d68ddeb2957ee6c8bc07"
