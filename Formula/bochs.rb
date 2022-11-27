f517b9035ceff9d996416a34abf412cb90418ed3
Adrian Ho <aho+vm-mojave@03s.net>
2022-11-27 19:16:05 +0800

    bochs: no rebuilding required

--- Formula/bochs.rb
+++ Formula/bochs.rb
@@ -11,8 +11,10 @@ class Bochs < Formula
   end
 
   bottle do
+    sha256 arm64_ventura:  "57686b06f051f0c5abd2fbdf7cacdf469bcd9d4b9b1bbbad4aff594dc58e2be9"
     sha256 arm64_monterey: "e148150828ea9d230cf350212dc8d415e3442ff04e285f4cdc358d0477d282b6"
     sha256 arm64_big_sur:  "413baabcb17f8a7da9b41306215280ef7fe9e898477c31eed66f483cfb15475a"
+    sha256 ventura:        "9ba58e0479a3088a00919aef9465eea819b6c6bc6b7dd1c01b007fde19bba3a3"
     sha256 monterey:       "7846c1280fc53365233026350c900bbc481de62b54bce1f454441331e82ce597"
     sha256 big_sur:        "6e644ff1b857016a22941d01d7136a94c39a790dd6ce0f358da5b5b5ab14af78"
     sha256 catalina:       "a903d4549d08e804de103c69866708ac5a993f7a4006687e9465e67991494cb4"
