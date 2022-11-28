fe9edf1907c6cf7451435dd94d00f45ce9e45987
Adrian Ho <the.gromgit@gmail.com>
2022-11-29 00:38:14 +0800

    clib: update 2.8.2 bottle.

--- Formula/clib.rb
+++ Formula/clib.rb
@@ -1,8 +1,8 @@
 class Clib < Formula
   desc "Package manager for C programming"
   homepage "https://github.com/clibs/clib"
-  url "https://github.com/clibs/clib/archive/2.8.1.tar.gz"
-  sha256 "0d2330e3c46ce21d7fa3d29f0f99b9527eb5ab323b1efb9b1a0e6915779c74d6"
+  url "https://github.com/clibs/clib/archive/2.8.2.tar.gz"
+  sha256 "e6bb5bcac18ad64070b70836750a0f3752cbe5fe31e9acd455a700ee57f3a799"
   license "MIT"
   head "https://github.com/clibs/clib.git", branch: "master"
 
@@ -13,8 +13,7 @@ class Clib < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/clib"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "ad60c601ed3fa9eb09ac9b9e3878032c96988b6c2e8a82f6d3adb18e8b85d389"
+    sha256 cellar: :any_skip_relocation, mojave: "c806612b0631f34fbd5ea53ddc74666e14d2302d87310c69cb469764626d1d14"
   end
 
   uses_from_macos "curl"
