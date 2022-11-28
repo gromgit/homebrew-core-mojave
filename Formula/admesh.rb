706f8a9b6f6f87712559755740e50fefa829fffe
Adrian Ho <the.gromgit@gmail.com>
2022-11-28 10:52:23 +0800

    admesh: update 0.98.5 bottle.

--- Formula/admesh.rb
+++ Formula/admesh.rb
@@ -1,20 +1,13 @@
 class Admesh < Formula
   desc "Processes triangulated solid meshes"
   homepage "https://github.com/admesh/admesh"
-  url "https://github.com/admesh/admesh/releases/download/v0.98.4/admesh-0.98.4.tar.gz"
-  sha256 "1c441591f2223034fed2fe536cf73e996062cac840423c3abe5342f898a819bb"
+  url "https://github.com/admesh/admesh/releases/download/v0.98.5/admesh-0.98.5.tar.gz"
+  sha256 "0d7994bfa587c4e958b2ac7c7d2fb90dfb6c5463d32513ada169cf710a438535"
   license "GPL-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "eb02eabe5f4a82ddd37e7099e44b736b1c63afee91b6169cc3c58a51c24f34e5"
-    sha256 cellar: :any,                 arm64_monterey: "d07859389041c5bf1e2c03f5d2ac56a38013263ec3a7f682fbc0357aacbc056b"
-    sha256 cellar: :any,                 arm64_big_sur:  "ee008a3dc86478020116d2289fc6d78dfc44d87a95b7c12c6777e8ed6b053242"
-    sha256 cellar: :any,                 monterey:       "1c89962fabc1d3777f86b52920f251a053a2e9d46603a5c3642613bc281dcb10"
-    sha256 cellar: :any,                 big_sur:        "e6f9a80ab0ef52a4027a6aa4cf1709f2088f0f9f51918d3f0361febe3c61a84d"
-    sha256 cellar: :any,                 catalina:       "d877dfc78d057e2124d06b4826e9044b2686f19de3e84fbab1cd19c07524e6df"
-    sha256 cellar: :any,                 mojave:         "86f1775a6dbca0e6309cdfed9fb83d068873f5e8183204f02cc871d013290f62"
-    sha256 cellar: :any,                 high_sierra:    "2f0fd4e6cda35b4e14f6c8ba627ad7d22ee93507875b6943ea5677c857c4ab36"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8f34afa551d708de7ff07bd9e1e0dd92a72813f325b0720132f15fbc329aae40"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/admesh"
+    sha256 cellar: :any, mojave: "a159847ee98053ed6ea1e81d440882cede1dd482e2d25e499e2e862e606d1555"
   end
 
   def install
