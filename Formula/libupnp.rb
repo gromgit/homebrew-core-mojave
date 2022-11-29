c3c3d4783e7d5688ec5a370c41128a2d3ee2a152
Adrian Ho <the.gromgit@gmail.com>
2022-11-30 05:51:22 +0800

    libupnp: update 1.14.15 bottle.

--- Formula/libupnp.rb
+++ Formula/libupnp.rb
@@ -1,8 +1,8 @@
 class Libupnp < Formula
   desc "Portable UPnP development kit"
   homepage "https://pupnp.sourceforge.io/"
-  url "https://github.com/pupnp/pupnp/releases/download/release-1.14.14/libupnp-1.14.14.tar.bz2"
-  sha256 "3ae23a2f2dbe3c4fe845a14d114d092743dac1184d024a8e900c2d3e78362150"
+  url "https://github.com/pupnp/pupnp/releases/download/release-1.14.15/libupnp-1.14.15.tar.bz2"
+  sha256 "794e92c6ea83456f25a929e2e1faa005f7178db8bc4b0b4b19eaca2cc7e66384"
   license "BSD-3-Clause"
 
   livecheck do
@@ -12,7 +12,7 @@ class Libupnp < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libupnp"
-    sha256 cellar: :any, mojave: "1eafaa8f5749e534f21e564355abf74b02bcd3e3b7b76be31189950bfafbfc00"
+    sha256 cellar: :any, mojave: "4431f3f8897a6808858b2d2e88302aa0b8f0ca837a28954523e9518fefd12b9f"
   end
 
   def install
