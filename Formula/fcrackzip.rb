2318a7e8693b8e3e0b0f6af1e44189b3dc8af45c
Adrian Ho <the.gromgit@gmail.com>
2022-11-29 23:24:41 +0800

    fcrackzip: update 1.0 bottle.

--- Formula/fcrackzip.rb
+++ Formula/fcrackzip.rb
@@ -11,17 +11,9 @@ class Fcrackzip < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "1b454aa25f9a3bab105c3324be688848b1be96127fbb96849bc2ac4753816e29"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7233ae34d595f418531944e3df6a9573175e53790d11a83504e082dbfe0c8075"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "30a4350e75184bde4f486b5291061e59fff1e3063aa2a0c24751d98c764f2b18"
-    sha256 cellar: :any_skip_relocation, monterey:       "62b27ef95d7fcbcdaa6027adf7f2983f9c8c93fe882937a364057f0479518ff3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "416fdb06f1ed0911126a51809e350450db5b09b723fb3aa16d0fddf9e05bd9aa"
-    sha256 cellar: :any_skip_relocation, catalina:       "553e2ed7eb76dcf4a216bf214e0ceed63a72bda2e7fe9f5fb5f2ed86d8e7bfb8"
-    sha256 cellar: :any_skip_relocation, mojave:         "9ac33112f0cb584aca3ac383ca3551cdda570e6f7337607c7f7db9d7f51b2e3a"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "a90e9d404b0ef939f6419559ed58143f556eb3e0b4fb0b8053bae35b82cc7a15"
-    sha256 cellar: :any_skip_relocation, sierra:         "ce2d79b833f5805cbc475711a38db0a7a791b793b24b094e68f3ed54dfe5fd82"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "169a5e7ea0e7ee9d04dc7ecce5288ef3a096fc9875d9af134b342878ce8c55fd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "202f6dd5415938783fa35cbb72f978148667cfc5b98fe1d7dfb6f62d833c80d9"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fcrackzip"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "229417920494e035c248a2faf922c2973cbff9880cd586a5601bb473a881a520"
   end
 
   uses_from_macos "zip" => :test
@@ -31,6 +23,8 @@ class Fcrackzip < Formula
                           "--prefix=#{prefix}"
     system "make"
     system "make", "install"
+    # Avoid conflict with `unzip` on Linux and shadowing `/usr/bin/zipinfo` on macOS
+    bin.install bin/"zipinfo" => "fcrackzipinfo"
   end
 
   test do
