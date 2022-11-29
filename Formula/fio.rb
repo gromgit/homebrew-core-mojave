5ddbca02a1fef1c97d1b9980be3f38fb4ab5dffc
Adrian Ho <the.gromgit@gmail.com>
2022-11-29 23:46:45 +0800

    fio: update 3.33 bottle.

--- Formula/fio.rb
+++ Formula/fio.rb
@@ -1,8 +1,8 @@
 class Fio < Formula
   desc "I/O benchmark and stress test"
   homepage "https://github.com/axboe/fio"
-  url "https://github.com/axboe/fio/archive/fio-3.32.tar.gz"
-  sha256 "409e459840912e68be487dbbda9a7a1b3f6ddf1478e3f456f278f957ce4f7b66"
+  url "https://github.com/axboe/fio/archive/fio-3.33.tar.gz"
+  sha256 "f48b2547313ffd1799c58c6a170175176131bbd42bc847b5650784eaf6d914b3"
   license "GPL-2.0-only"
 
   livecheck do
@@ -12,7 +12,7 @@ class Fio < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fio"
-    sha256 cellar: :any_skip_relocation, mojave: "1493a8266d9707d23fee000adf80e4482baebbaef3f67d57fd785d35a6f8c9ea"
+    sha256 cellar: :any_skip_relocation, mojave: "9b2513aa278abba93e50489aa8596f342fc3a299f566936f71a50a29951add81"
   end
 
   uses_from_macos "zlib"
