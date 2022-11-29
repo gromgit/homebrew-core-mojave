6613ff8d580f02c6efe1d2b7b3ed151329bfcd99
Adrian Ho <the.gromgit@gmail.com>
2022-11-30 03:55:14 +0800

    jsonnet: update 0.19.1 bottle.

--- Formula/jsonnet.rb
+++ Formula/jsonnet.rb
@@ -1,8 +1,8 @@
 class Jsonnet < Formula
   desc "Domain specific configuration language for defining JSON data"
   homepage "https://jsonnet.org/"
-  url "https://github.com/google/jsonnet/archive/v0.18.0.tar.gz"
-  sha256 "85c240c4740f0c788c4d49f9c9c0942f5a2d1c2ae58b2c71068107bc80a3ced4"
+  url "https://github.com/google/jsonnet/archive/v0.19.1.tar.gz"
+  sha256 "f5a20f2dc98fdebd5d42a45365f52fa59a7e6b174e43970fea4f9718a914e887"
   license "Apache-2.0"
   head "https://github.com/google/jsonnet.git", branch: "master"
 
@@ -13,7 +13,7 @@ class Jsonnet < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jsonnet"
-    sha256 cellar: :any_skip_relocation, mojave: "91505108b16a4f4fcdb5cd81ff4bd6ff72f859243a344834b988da72c32f16d2"
+    sha256 cellar: :any_skip_relocation, mojave: "cbfa84411057e33abdc02c82b61ff9729b6ce910b340b7b6002628fb8b2a0560"
   end
 
   conflicts_with "go-jsonnet", because: "both install binaries with the same name"
