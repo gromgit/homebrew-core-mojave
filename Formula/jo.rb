098083e12a1662db9beda90a7b5ce8da2128a820
Adrian Ho <the.gromgit@gmail.com>
2022-11-30 03:42:15 +0800

    jo: update 1.9 bottle.

--- Formula/jo.rb
+++ Formula/jo.rb
@@ -1,13 +1,13 @@
 class Jo < Formula
   desc "JSON output from a shell"
   homepage "https://github.com/jpmens/jo"
-  url "https://github.com/jpmens/jo/releases/download/1.6/jo-1.6.tar.gz"
-  sha256 "eb15592f1ba6d5a77468a1438a20e3d21c3d63bb7d045fb3544f223340fcd1a1"
+  url "https://github.com/jpmens/jo/releases/download/1.9/jo-1.9.tar.gz"
+  sha256 "0195cd6f2a41103c21544e99cd9517b0bce2d2dc8cde31a34867977f8a19c79f"
   license all_of: ["GPL-2.0-or-later", "MIT"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/jo"
-    sha256 cellar: :any_skip_relocation, mojave: "4b43c9f6de92f249ccc1bec209f40403e87b9dc108d6c68753ce7a67cbdd4e9b"
+    sha256 cellar: :any_skip_relocation, mojave: "a6304a6076953553b7a3596c631143ab0b484a9c273688e4089c867c2d8c4af9"
   end
 
   head do
