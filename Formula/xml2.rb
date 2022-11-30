e985384ae102000ca07c6274046caf569790e6c9
Adrian Ho <the.gromgit@gmail.com>
2022-11-30 16:50:03 +0800

    xml2: update 0.5 bottle.

--- Formula/xml2.rb
+++ Formula/xml2.rb
@@ -10,17 +10,9 @@ class Xml2 < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2a8bc2144c34a087bd0ec424f1079c0813e3de5e069e1cf23fa5c904125d42be"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "23f1ef27cd811f9b846f80775e4f0981998a7cad1230b0f98261ba42dc85c325"
-    sha256 cellar: :any_skip_relocation, monterey:       "52c859eeda679620c5b02db82a7a7d1353c53bd43fae6e53c9028b99afe48b1d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c6e91ba5879e8891be5aca28eba77249f18c8860d2d387447da0ca13efbe066c"
-    sha256 cellar: :any_skip_relocation, catalina:       "832aa209cf47c4f18ad512f7eca2acf76aa047522b3a417466722203203bd71e"
-    sha256 cellar: :any_skip_relocation, mojave:         "63b136beee1c47726c6756f3c57bf55fcff4e660cd280d090aa35640138465b6"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "548421fe00487faa136c700e4d18f48b6bc349956044e2aa0f65667c3856883d"
-    sha256 cellar: :any_skip_relocation, sierra:         "d8d4bb9ceb9d97b648d3fd3cffb1e2fad2e4d82aa6aa3397c22f53fe5468ac56"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "85e939873edbb3dd1b072437992a0c404534a5084cccd6f9f76d99b09ddda695"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "51048810fc19ddd5955049fb8d358b0f4862e9333b056efd6d28a0e6c42c6e4e"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/xml2"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, mojave: "c1560ddacb36984c168ea9869b2b3f9f96069916573a0e42eb7aee78707787f2"
   end
 
   depends_on "pkg-config" => :build
