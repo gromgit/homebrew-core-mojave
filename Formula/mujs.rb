7717f44d869784fa2a9a08456c600e4da04d3110
Adrian Ho <the.gromgit@gmail.com>
2022-11-30 08:28:17 +0800

    mujs: update 1.3.0 bottle.

--- Formula/mujs.rb
+++ Formula/mujs.rb
@@ -3,15 +3,14 @@ class Mujs < Formula
   homepage "https://www.mujs.com/"
   # use tag not tarball so the version in the pkg-config file isn't blank
   url "https://github.com/ccxvii/mujs.git",
-      tag:      "1.2.0",
-      revision: "dd0a0972b4428771e6a3887da2210c7c9dd40f9c"
+      tag:      "1.3.0",
+      revision: "ebf235bfea04da1d12c77c84f9398c1c0d5aa0a8"
   license "ISC"
   head "https://github.com/ccxvii/mujs.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mujs"
-    rebuild 1
-    sha256 cellar: :any, mojave: "2413b18feb1628be04194737f23e01478a7de6128b8137c59feeaf0429447957"
+    sha256 cellar: :any, mojave: "d0f67f01a21f279d8878390365d7dac37aaafb3da9c210afe7c48ef0d7c4e808"
   end
 
   on_linux do
