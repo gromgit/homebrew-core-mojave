6b5c652cb8b44b07b52c781ed5d4dc08f78821dc
Adrian Ho <the.gromgit@gmail.com>
2022-11-30 13:28:04 +0800

    sfst: update 1.4.7g bottle.

--- Formula/sfst.rb
+++ Formula/sfst.rb
@@ -1,8 +1,8 @@
 class Sfst < Formula
   desc "Toolbox for morphological analysers and other FST-based tools"
   homepage "https://www.cis.uni-muenchen.de/~schmid/tools/SFST/"
-  url "https://www.cis.uni-muenchen.de/~schmid/tools/SFST/data/SFST-1.4.7f.zip"
-  sha256 "31f331a1cc94eb610bcefc42b18a7cf62c55f894ac01a027ddff29e2a71cc31b"
+  url "https://www.cis.uni-muenchen.de/~schmid/tools/SFST/data/SFST-1.4.7g.zip"
+  sha256 "5f3ab2d8190dc931813b5ba3cf94c72013cce7bf03e16d7fb2eda86bd99ce944"
   license "GPL-2.0-only"
 
   livecheck do
@@ -11,14 +11,8 @@ class Sfst < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "d177f8a557d68a0b25cb8718ef0881b1c4688c1003bf0115e0711f1956985220"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "e9f9ef199fdf0cd03080197135ee96d9ee50cd0229d602511923b0d53ec9a2ab"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1a327a02964854d8ba50b22f12d3197535bc65902f154971e901974ef0b43556"
-    sha256 cellar: :any_skip_relocation, monterey:       "8e15e931308b0cb73a1c09a03cca8cb1b6b0e4ac54a678b8369dc0d40780fb2e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "303e686c5216a73e74ef954e01dbce83b878531ad18df80cb7a29c0c03cd9138"
-    sha256 cellar: :any_skip_relocation, catalina:       "d8c1b35f23af28cfab56a28664109b18e8b0f551f2f680ecfe2fee94cce6224c"
-    sha256 cellar: :any_skip_relocation, mojave:         "d2fc1beee93f11a89ec9dd1762d6eacf393e6b21752d5d0806deeed5aab8f014"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "af25d44ef848916d9a64bebf152b07cae7d697c1b5b8b1c2368ffa115d9e53ac"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sfst"
+    sha256 cellar: :any_skip_relocation, mojave: "bf3b3ef5727c2be4961d384192e21d0f7a9671d060ce7bde32b10dea776d8695"
   end
 
   uses_from_macos "flex" => :build
