f4d342419411c0dc845186983e6e6e052a321ba8
Adrian Ho <the.gromgit@gmail.com>
2022-11-30 14:21:09 +0800

    tass64: update 1.57.2900 bottle.

--- Formula/tass64.rb
+++ Formula/tass64.rb
@@ -1,18 +1,13 @@
 class Tass64 < Formula
   desc "Multi pass optimizing macro assembler for the 65xx series of processors"
   homepage "https://tass64.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/tass64/source/64tass-1.56.2625-src.zip"
-  sha256 "c4e570c717c9500f3af61a3ad5d536f22415e2b29ed1eb09b1a955d310c9f3d3"
+  url "https://downloads.sourceforge.net/project/tass64/source/64tass-1.57.2900-src.zip"
+  sha256 "e4f0dcb1b00d5206ccb391eb5b8b43f4d95f064d63af739a71f5c822bbfc56a4"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.0-or-later", "LGPL-2.1-only", "MIT"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "a728bdf4efe32d5cfd7a15d502cd13d65e1ddec15998dd569d6fa94a7f4fd2f7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7468a371a0c4627bf592d4e6fe36b9c854def7caa2b7270faa20ff97ca2cc7df"
-    sha256 cellar: :any_skip_relocation, monterey:       "878ae43e115b1248160fbc7652f8024705162aa3263660a7e62382e14806b4e3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "70f966e5974c671aac1c1209c2273325be29b5d56de851416344d0559c8b8c34"
-    sha256 cellar: :any_skip_relocation, catalina:       "e59c0c91030879fbc49cd205a1e5e0e5c0026434d1bff86a9742b4d00139824c"
-    sha256 cellar: :any_skip_relocation, mojave:         "a5be46e97063271c7bc9ddadc88e4b318193ee0f0556489593c3db9aed5ef03b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "e5d380c36fde7617c613b0da5854de0f8b73150a3370f0f8b4d281191a67ed60"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tass64"
+    sha256 cellar: :any_skip_relocation, mojave: "4c16544d8988f57a94e62bc1fffc822078457700dcb17f551c5754a52dd1a57e"
   end
 
   def install
