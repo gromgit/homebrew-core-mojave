6f5a5d6f3881ab35d46afa13a2d04042768035c2
Adrian Ho <the.gromgit@gmail.com>
2022-11-30 09:15:46 +0800

    oksh: update 7.2 bottle.

--- Formula/oksh.rb
+++ Formula/oksh.rb
@@ -1,14 +1,14 @@
 class Oksh < Formula
   desc "Portable OpenBSD ksh, based on the public domain Korn shell (pdksh)"
   homepage "https://github.com/ibara/oksh"
-  url "https://github.com/ibara/oksh/releases/download/oksh-7.1/oksh-7.1.tar.gz"
-  sha256 "9dc0b0578d9d64d10c834f9757ca11f526b562bc5454da64b2cb270122f52064"
+  url "https://github.com/ibara/oksh/releases/download/oksh-7.2/oksh-7.2.tar.gz"
+  sha256 "3340ca98b1d5a2800ebe7dba75312d8a4971a3fcff20fcd0d0ec5cf7b719427e"
   license all_of: [:public_domain, "BSD-3-Clause", "ISC"]
   head "https://github.com/ibara/oksh.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/oksh"
-    sha256 cellar: :any_skip_relocation, mojave: "cfd5bcebffad24dba6e9068f321060236edf51b03a8cb5ff58cb9887b05f347d"
+    sha256 cellar: :any_skip_relocation, mojave: "17bde85220e457da4dd444cdce683b6cab1b72d02e96f6f43e92fef88812ec7c"
   end
 
   def install
