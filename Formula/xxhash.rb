c37aac3a7508f45ccf258b1aba86ab7fb2ace182
Adrian Ho <the.gromgit@gmail.com>
2022-11-30 17:00:54 +0800

    xxhash: update 0.8.1 bottle.

--- Formula/xxhash.rb
+++ Formula/xxhash.rb
@@ -3,7 +3,10 @@ class Xxhash < Formula
   homepage "https://github.com/Cyan4973/xxHash"
   url "https://github.com/Cyan4973/xxHash/archive/v0.8.1.tar.gz"
   sha256 "3bb6b7d6f30c591dd65aaaff1c8b7a5b94d81687998ca9400082c739a690436c"
-  license "BSD-2-Clause"
+  license all_of: [
+    "BSD-2-Clause", # library
+    "GPL-2.0-or-later", # `xxhsum` command line utility
+  ]
 
   livecheck do
     url :stable
@@ -12,13 +15,14 @@ class Xxhash < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/xxhash"
-    rebuild 3
-    sha256 cellar: :any, mojave: "95d6a9937ec00ebf69e5f951f1706cdc3706eb52ad307de3b01ffee011e6afc3"
+    rebuild 4
+    sha256 cellar: :any, mojave: "16215f86d4e2b6a39b6b5783a5e43432b43409ea64ae68d3e3167aac01c14160"
   end
 
   def install
     system "make"
     system "make", "install", "PREFIX=#{prefix}"
+    prefix.install "cli/COPYING"
   end
 
   test do
