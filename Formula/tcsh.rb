ac5f1f81d6c88cfe7cb1f84de02ff26ece667079
Adrian Ho <the.gromgit@gmail.com>
2022-11-30 14:42:31 +0800

    tcsh: update 6.24.02 bottle.

--- Formula/tcsh.rb
+++ Formula/tcsh.rb
@@ -1,9 +1,9 @@
 class Tcsh < Formula
   desc "Enhanced, fully compatible version of the Berkeley C shell"
   homepage "https://www.tcsh.org/"
-  url "https://astron.com/pub/tcsh/tcsh-6.24.01.tar.gz"
-  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/tcsh/tcsh-6.24.01.tar.gz"
-  sha256 "5659a1b973b1f947e36238cb17a00210ac4cff3b496f8f851acd77172ab91d7e"
+  url "https://astron.com/pub/tcsh/tcsh-6.24.02.tar.gz"
+  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/tcsh/tcsh-6.24.02.tar.gz"
+  sha256 "6691e15af0719575cad91ce9212c77a754f6c89f0a1f70454625e5e21ba0bdad"
   license "BSD-3-Clause"
 
   livecheck do
@@ -13,9 +13,10 @@ class Tcsh < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tcsh"
-    sha256 mojave: "130d33874b9df7a974e439d04d9be9cfb173a787d38229bb367f8498555b91b1"
+    sha256 mojave: "9ffc8f110eb0be71c43d4fda26a8e1d4eb39b0ed63f6ef52663ff43a1b0cbef0"
   end
 
+  uses_from_macos "libxcrypt"
   uses_from_macos "ncurses"
 
   def install
