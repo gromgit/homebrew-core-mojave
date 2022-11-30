8140430154b0ee187fc8fd5c161667895fb493e8
Adrian Ho <the.gromgit@gmail.com>
2022-11-30 15:38:10 +0800

    uudeview: update 0.5.20_1 bottle.

--- Formula/uudeview.rb
+++ Formula/uudeview.rb
@@ -11,16 +11,9 @@ class Uudeview < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "28d8a4e08275d26926e19c32a6ca833ed3a1c3969ea5126c4def92e72442e66d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8c72240dbc205cb79229af479b8dc1774b4eb11d0ffad47391102e033be4bb07"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "93a098dc40d16b9785888c20c8d1707a62fe471938c99ea8074df042548cfed7"
-    sha256 cellar: :any_skip_relocation, monterey:       "0b5de5467dd832158645bca2006500fadaefc2e187819e883e9ff1a85bb60e64"
-    sha256 cellar: :any_skip_relocation, big_sur:        "94426299f928e2c7985194d2a3f436112b2ca580945eacc82ad5047c619c2417"
-    sha256 cellar: :any_skip_relocation, catalina:       "9b5990b5b763e90614bd2d074e670c20e834541d60082a4e78f90d67a65da5c3"
-    sha256 cellar: :any_skip_relocation, mojave:         "2869df0b09975172227dc83be6d667b3d0f8e4f2cf0f6d9ec0cd3fdca02727f4"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "7bb4c57755efed1b4208d234a0017d785757da04ca8f8e43c92980f3fe16b85c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5cdd9748ec5c1baf9934bade72dd8a3eea06b632d0f1c49e57b682663bbb8371"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/uudeview"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, mojave: "0e0dda71ad229d08f7385f748c129ea8ed9d9149b84ef0afb7df149f31ac822a"
   end
 
   # Fix function signatures (for clang)
