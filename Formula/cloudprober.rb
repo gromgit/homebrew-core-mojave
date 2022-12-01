1143e26a4884169cb082a02efd18e6754e55446f
Adrian Ho <the.gromgit@gmail.com>
2022-12-01 15:18:49 +0800

    cloudprober: add 0.12.1 bottle.

---
+++ Formula/cloudprober.rb
@@ -0,0 +1,26 @@
+class Cloudprober < Formula
+  desc "Active monitoring software to detect failures before your customers do"
+  homepage "https://cloudprober.org"
+  url "https://github.com/cloudprober/cloudprober/archive/refs/tags/v0.12.1.tar.gz"
+  sha256 "1ba5a700d7785e8ac399daca4fa7367797e48071909d72a0e9f12c06b9e62140"
+  license "Apache-2.0"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cloudprober"
+    sha256 cellar: :any_skip_relocation, mojave: "8c71af21961e9fd14ec57c1e55710d59249fde7779f8eb0e129989acd601b2c0"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "cloudprober", "VERSION=v#{version}"
+    bin.install "cloudprober"
+  end
+
+  test do
+    io = IO.popen("#{bin}/cloudprober --logtostderr", err: [:child, :out])
+    io.any? do |line|
+      /Initialized status surfacer/.match?(line)
+    end
+  end
+end
