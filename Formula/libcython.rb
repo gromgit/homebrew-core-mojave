19115eb4ce95a438c2322ac764eac536e641438b
Adrian Ho <the.gromgit@gmail.com>
2022-12-05 03:04:38 +0800

    libcython: add 0.29.32_1 bottle.

--- Formula/libcython.rb
+++ Formula/libcython.rb
@@ -4,6 +4,7 @@ class Libcython < Formula
   url "https://files.pythonhosted.org/packages/4c/76/1e41fbb365ad20b6efab2e61b0f4751518444c953b390f9b2d36cf97eea0/Cython-0.29.32.tar.gz"
   sha256 "8733cf4758b79304f2a4e39ebfac5e92341bce47bcceb26c1254398b2f8c1af7"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     formula "cython"
@@ -11,8 +12,7 @@ class Libcython < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libcython"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "3f069782656ac4a9a2a3ce8343e09fe20463829800b798eca07a4532f48ebdda"
+    sha256 cellar: :any_skip_relocation, mojave: "5bf77cf55e10ed8732eb4c8f1a9a6ed3c6423f7fe62fa30b82494c68d3dbc86f"
   end
 
   keg_only <<~EOS
@@ -21,17 +21,17 @@ class Libcython < Formula
   EOS
 
   depends_on "python@3.10" => [:build, :test]
-  depends_on "python@3.9" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
 
   def pythons
     deps.map(&:to_formula)
         .select { |f| f.name.match?(/^python@\d\.\d+$/) }
+        .sort_by(&:version)
         .map { |f| f.opt_libexec/"bin/python" }
   end
 
   def install
     pythons.each do |python|
-      ENV.prepend_create_path "PYTHONPATH", libexec/Language::Python.site_packages(python)
       system python, *Language::Python.setup_install_args(libexec, python)
     end
   end
@@ -48,9 +48,10 @@ class Libcython < Formula
       )
     EOS
     pythons.each do |python|
-      ENV.prepend_path "PYTHONPATH", libexec/Language::Python.site_packages(python)
-      system python, "setup.py", "build_ext", "--inplace"
-      assert_match phrase, shell_output("#{python} -c 'import package_manager'")
+      with_env(PYTHONPATH: libexec/Language::Python.site_packages(python)) do
+        system python, "setup.py", "build_ext", "--inplace"
+        assert_match phrase, shell_output("#{python} -c 'import package_manager'")
+      end
     end
   end
 end
