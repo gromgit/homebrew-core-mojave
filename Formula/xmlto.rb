a87bf7886c5f0ac053834259cc977c972e4b66f6
Adrian Ho <the.gromgit@gmail.com>
2022-12-11 21:00:27 +0800

    xmlto: add 0.0.28 bottle.

--- Formula/xmlto.rb
+++ Formula/xmlto.rb
@@ -11,16 +11,9 @@ class Xmlto < Formula
   end
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ae8265ff1eca0fe879c1ece963369b58abe515726f99b53832a5aded92b51e58"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "2947a0d38d964b133b1452fa8277d59b333980c549141b990f3f8cbfe9db5c58"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bec2a4a7797f07fc73a636a40e85cb0504da4c5e34328456c28e78c84ce95324"
-    sha256 cellar: :any_skip_relocation, monterey:       "d86630edcfc8976b0a7a22f314b7a7062814fec16d52b90d8c1b4d850907222e"
-    sha256 cellar: :any_skip_relocation, big_sur:        "95502b71000319da58971ea17c0dab0f326d20ce4c09d074fe4c7fe89c66d002"
-    sha256 cellar: :any_skip_relocation, catalina:       "d2c21b9b398191e21dcf6e7ac53e4dd46fb59d29173e4d8443ac296101cce58f"
-    sha256 cellar: :any_skip_relocation, mojave:         "8fca3be2271ae8e7fb646b011969ba4030f7421118a4ea6b11eca1ac0fe6979b"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "1214da1d14a8f01d8b8d0ead6606207ff5a29fb7ab104d6af47e57fbca4ffcc7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "bd0f9ea84a854c7483a7eae5320c54baa3cb956e914044a522e3c3c041ab08eb"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/xmlto"
+    rebuild 3
+    sha256 cellar: :any_skip_relocation, mojave: "a6e94cd6d0337e3dcbdcc2246814ffde8ae10fa25033ff74e42186deb695a841"
   end
 
   depends_on "docbook"
