9ad31651ccf0aad106eee9199a0e770a7a162f22
Adrian Ho <the.gromgit@gmail.com>
2022-12-12 21:25:27 +0800

    eigenpy: add 2.7.14_2 bottle.

--- Formula/eigenpy.rb
+++ Formula/eigenpy.rb
@@ -1,14 +1,15 @@
 class Eigenpy < Formula
   desc "Python bindings of Eigen library with Numpy support"
   homepage "https://github.com/stack-of-tasks/eigenpy"
-  url "https://github.com/stack-of-tasks/eigenpy/releases/download/v2.7.6/eigenpy-2.7.6.tar.gz"
-  sha256 "71cc2763e891b7ef013b1c0306cdb7e497d6aff9f648dca30997e541b1a6e05a"
+  url "https://github.com/stack-of-tasks/eigenpy/releases/download/v2.7.14/eigenpy-2.7.14.tar.gz"
+  sha256 "b98157b78ef8db61e581bc432e44dd851627730626cd01c171e56c70da475ad9"
   license "BSD-2-Clause"
+  revision 2
   head "https://github.com/stack-of-tasks/eigenpy.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/eigenpy"
-    sha256 cellar: :any, mojave: "80299050eb9b82693e2e54d051c97cb69e870dbe90eff94d95493e8cc66ccdb3"
+    sha256 cellar: :any_skip_relocation, mojave: "5398636eefaa835820e7948f0540828ab38447c820579051fcccaf17492952a2"
   end
 
   depends_on "boost" => :build
@@ -17,26 +18,26 @@ class Eigenpy < Formula
   depends_on "boost-python3"
   depends_on "eigen"
   depends_on "numpy"
-  depends_on "python@3.9"
+  depends_on "python@3.11"
+
+  def python3
+    "python3.11"
+  end
 
   def install
-    pyver = Language::Python.major_minor_version "python3"
-    python = Formula["python@#{pyver}"].opt_bin/"python#{pyver}"
-    ENV.prepend_path "PYTHONPATH", Formula["numpy"].opt_prefix/Language::Python.site_packages("python3")
+    ENV.prepend_path "PYTHONPATH", Formula["numpy"].opt_prefix/Language::Python.site_packages(python3)
     ENV.prepend_path "Eigen3_DIR", Formula["eigen"].opt_share/"eigen3/cmake"
 
-    mkdir "build" do
-      args = *std_cmake_args
-      args << "-DPYTHON_EXECUTABLE=#{python}"
-      args << "-DBUILD_UNIT_TESTS=OFF"
-      system "cmake", "..", *args
-      system "make"
-      system "make", "install"
-    end
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DPYTHON_EXECUTABLE=#{which(python3)}",
+                    "-DBUILD_UNIT_TESTS=OFF",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
-    system Formula["python@3.9"].opt_bin/"python3", "-c", <<~EOS
+    system python3, "-c", <<~EOS
       import numpy as np
       import eigenpy
 
