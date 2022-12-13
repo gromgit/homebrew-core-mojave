93b01a7d4c8bf802faca10bfe939a7ff5561d93a
Adrian Ho <the.gromgit@gmail.com>
2022-12-13 10:49:34 +0800

    pyyaml: add 6.0_1 bottle.

--- Formula/pyyaml.rb
+++ Formula/pyyaml.rb
@@ -4,16 +4,16 @@ class Pyyaml < Formula
   url "https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz"
   sha256 "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
   license "MIT"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/pyyaml"
-    sha256 cellar: :any, mojave: "a2f02fb820a039912875d5ee2eb8f60ca398cc618374d9df74aa54d5c7a448b4"
+    sha256 cellar: :any_skip_relocation, mojave: "bbeb89a221166a76cb3111fa41da450bbdb71fc413349c293d37558a437d301c"
   end
 
   depends_on "cython" => :build
   depends_on "python@3.10" => [:build, :test]
-  depends_on "python@3.8" => [:build, :test]
-  depends_on "python@3.9" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
   depends_on "libyaml"
 
   def pythons
