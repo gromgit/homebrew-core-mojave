26db5a3a767b687739d914d64b13748a37e5d312
Adrian Ho <the.gromgit@gmail.com>
2022-12-14 15:37:21 +0800

    tbb: add 2021.7.0 bottle.

--- Formula/tbb.rb
+++ Formula/tbb.rb
@@ -1,17 +1,17 @@
 class Tbb < Formula
   desc "Rich and complete approach to parallelism in C++"
   homepage "https://github.com/oneapi-src/oneTBB"
-  url "https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.5.0.tar.gz"
-  sha256 "e5b57537c741400cf6134b428fc1689a649d7d38d9bb9c1b6d64f092ea28178a"
+  url "https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.7.0.tar.gz"
+  sha256 "2cae2a80cda7d45dc7c072e4295c675fff5ad8316691f26f40539f7e7e54c0cc"
   license "Apache-2.0"
-  revision 2
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/tbb"
-    rebuild 1
-    sha256 cellar: :any, mojave: "295e7d30494fa180f4b3e6b4fab7cfa55f78520f4740b76ba80c57567a2c7193"
+    sha256 cellar: :any_skip_relocation, mojave: "b5a871abedaf1f38baf5ab5d63d926100b3ed606c20c605804b85b4c1f37a6d1"
   end
 
+  # If adding `hwloc` for TBBBind, you *must* add a test for its functionality.
+  # https://github.com/oneapi-src/oneTBB/blob/690aaf497a78a75ff72cddb084579427ab0a8ffc/CMakeLists.txt#L226-L228
   depends_on "cmake" => :build
   depends_on "python@3.10" => [:build, :test]
   depends_on "swig" => :build
@@ -29,6 +29,15 @@ class Tbb < Formula
   end
 
   def install
+    # Prevent `setup.py` from installing tbb4py directly into HOMEBREW_PREFIX.
+    # We need this due to our `python@3.10` patch.
+    python = Formula["python@3.10"].opt_bin/"python3.10"
+    site_packages = Language::Python.site_packages(python)
+    inreplace "python/CMakeLists.txt", "@@SITE_PACKAGES@@", site_packages
+
+    tbb_site_packages = prefix/site_packages/"tbb"
+    ENV.append "LDFLAGS", "-Wl,-rpath,#{rpath},-rpath,#{rpath(source: tbb_site_packages)}"
+
     args = %w[
       -DTBB_TEST=OFF
       -DTBB4PY_BUILD=ON
@@ -49,12 +58,8 @@ class Tbb < Formula
 
     cd "python" do
       ENV.append_path "CMAKE_PREFIX_PATH", prefix.to_s
-      python = Formula["python@3.10"].opt_bin/"python3.10"
-
-      tbb_site_packages = prefix/Language::Python.site_packages(python)/"tbb"
-      ENV.append "LDFLAGS", "-Wl,-rpath,#{rpath(source: tbb_site_packages)}"
-
       ENV["TBBROOT"] = prefix
+
       system python, *Language::Python.setup_install_args(prefix, python)
     end
 
@@ -107,6 +112,15 @@ diff --git a/python/CMakeLists.txt b/python/CMakeLists.txt
 index 1d2b05f..81ba8de 100644
 --- a/python/CMakeLists.txt
 +++ b/python/CMakeLists.txt
+@@ -40,7 +40,7 @@ add_custom_target(
+     ${PYTHON_EXECUTABLE} ${PYTHON_BUILD_WORK_DIR}/setup.py
+         build -b${PYTHON_BUILD_WORK_DIR}
+         build_ext ${TBB4PY_INCLUDE_STRING} -L$<TARGET_FILE_DIR:TBB::tbb>
+-        install --prefix ${PYTHON_BUILD_WORK_DIR}/build -f
++        install --prefix ${PYTHON_BUILD_WORK_DIR}/build --install-lib ${PYTHON_BUILD_WORK_DIR}/build/@@SITE_PACKAGES@@ -f
+     COMMENT "Build and install to work directory the oneTBB Python module"
+ )
+ 
 @@ -49,7 +49,7 @@ add_test(NAME python_test
                   -DPYTHON_MODULE_BUILD_PATH=${PYTHON_BUILD_WORK_DIR}/build
                   -P ${PROJECT_SOURCE_DIR}/cmake/python/test_launcher.cmake)
