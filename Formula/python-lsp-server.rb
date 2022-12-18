6790f3cd5c920f25322ebfaf56ef266be2070a13
Adrian Ho <the.gromgit@gmail.com>
2022-12-19 02:00:46 +0800

    python-lsp-server: add 1.6.0 bottle.

---
+++ Formula/python-lsp-server.rb
@@ -0,0 +1,68 @@
+class PythonLspServer < Formula
+  include Language::Python::Virtualenv
+
+  desc "Python Language Server for the Language Server Protocol"
+  homepage "https://github.com/python-lsp/python-lsp-server"
+  url "https://files.pythonhosted.org/packages/13/c3/02952e2465da01d2b9f3df8972ddce72fe4ac471fa9af9fcc7a89dc8f863/python-lsp-server-1.6.0.tar.gz"
+  sha256 "d75cdff9027c4212e5b9e861e9a0219219c8e2c69508d9f24949951dabd0dc1b"
+  license "MIT"
+  head "https://github.com/python-lsp/python-lsp-server.git", branch: "develop"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/python-lsp-server"
+    sha256 cellar: :any_skip_relocation, mojave: "e1d992414464c89efb923fff6e5724f2cc17222dca68a0147bed4820554b52c3"
+  end
+
+  depends_on "python@3.11"
+
+  resource "docstring-to-markdown" do
+    url "https://files.pythonhosted.org/packages/1e/c5/53e13bb0e3dd8b7fa3595c80deb40a3742dd191a9350141d4daa7ab09a9f/docstring-to-markdown-0.10.tar.gz"
+    sha256 "12f75b0c7b7572defea2d9e24b57ef7ac38c3e26e91c0e5547cfc02b1c168bf6"
+  end
+
+  resource "jedi" do
+    url "https://files.pythonhosted.org/packages/c2/25/273288df952e07e3190446efbbb30b0e4871a0d63b4246475f3019d4f55e/jedi-0.18.1.tar.gz"
+    sha256 "74137626a64a99c8eb6ae5832d99b3bdd7d29a3850fe2aa80a4126b2a7d949ab"
+  end
+
+  resource "parso" do
+    url "https://files.pythonhosted.org/packages/a2/0e/41f0cca4b85a6ea74d66d2226a7cda8e41206a624f5b330b958ef48e2e52/parso-0.8.3.tar.gz"
+    sha256 "8c07be290bb59f03588915921e29e8a50002acaf2cdc5fa0e0114f91709fafa0"
+  end
+
+  resource "pluggy" do
+    url "https://files.pythonhosted.org/packages/a1/16/db2d7de3474b6e37cbb9c008965ee63835bba517e22cdb8c35b5116b5ce1/pluggy-1.0.0.tar.gz"
+    sha256 "4224373bacce55f955a878bf9cfa763c1e360858e330072059e10bad68531159"
+  end
+
+  resource "python-lsp-jsonrpc" do
+    url "https://files.pythonhosted.org/packages/99/45/1c2a272950679af529f7360af6ee567ef266f282e451be926329e8d50d84/python-lsp-jsonrpc-1.0.0.tar.gz"
+    sha256 "7bec170733db628d3506ea3a5288ff76aa33c70215ed223abdb0d95e957660bd"
+  end
+
+  resource "ujson" do
+    url "https://files.pythonhosted.org/packages/6e/4a/03ddad85a10dd52e209993a14afa0cb0dc5c348e4647329f1c53856ad9e6/ujson-5.5.0.tar.gz"
+    sha256 "b25077a971c7da47bd6846a912a747f6963776d90720c88603b1b55d81790780"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+    input = "Content-Length: #{json.size}\r\n\r\n#{json}"
+    output = pipe_output("#{bin}/pylsp", input)
+    assert_match(/^Content-Length: \d+/i, output)
+  end
+end
