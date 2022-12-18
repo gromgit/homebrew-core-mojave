dba661e4885257b330ec9dd505801a32df2146b8
Adrian Ho <the.gromgit@gmail.com>
2022-12-19 01:37:07 +0800

    scipy: add 1.9.3_1 bottle.

--- Formula/scipy.rb
+++ Formula/scipy.rb
@@ -1,14 +1,15 @@
 class Scipy < Formula
   desc "Software for mathematics, science, and engineering"
   homepage "https://www.scipy.org"
-  url "https://files.pythonhosted.org/packages/26/b5/9330f004b9a3b2b6a31f59f46f1617ce9ca15c0e7fe64288c20385a05c9d/scipy-1.8.1.tar.gz"
-  sha256 "9e3fb1b0e896f14a85aa9a28d5f755daaeeb54c897b746df7a55ccb02b340f33"
+  url "https://files.pythonhosted.org/packages/0a/2e/44795c6398e24e45fa0bb61c3e98de1cfea567b1b51efd3751e2f7ff9720/scipy-1.9.3.tar.gz"
+  sha256 "fbc5c05c85c1a02be77b1ff591087c83bc44579c6d2bd9fb798bb64ea5e1a027"
   license "BSD-3-Clause"
-  head "https://github.com/scipy/scipy.git", branch: "master"
+  revision 1
+  head "https://github.com/scipy/scipy.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/scipy"
-    sha256 cellar: :any, mojave: "d4e024bc79d1e041634c52f7b8efca9b04a2757ac60be8e6f51af4aeca8403c0"
+    sha256 cellar: :any_skip_relocation, mojave: "839c23a778fcf6d16f631889762fd33421a9b8783fd8478ede5a1de510d68744"
   end
 
   depends_on "libcython" => :build
@@ -18,16 +19,20 @@ class Scipy < Formula
   depends_on "numpy"
   depends_on "openblas"
   depends_on "pybind11"
-  depends_on "python@3.9"
+  depends_on "python@3.11"
 
   cxxstdlib_check :skip
 
   fails_with gcc: "5"
 
+  def python3
+    "python3.11"
+  end
+
   def install
-    openblas = Formula["openblas"].opt_prefix
+    openblas = Formula["openblas"]
     ENV["ATLAS"] = "None" # avoid linking against Accelerate.framework
-    ENV["BLAS"] = ENV["LAPACK"] = "#{openblas}/lib/#{shared_library("libopenblas")}"
+    ENV["BLAS"] = ENV["LAPACK"] = openblas.opt_lib/shared_library("libopenblas")
 
     config = <<~EOS
       [DEFAULT]
@@ -35,21 +40,20 @@ class Scipy < Formula
       include_dirs = #{HOMEBREW_PREFIX}/include
       [openblas]
       libraries = openblas
-      library_dirs = #{openblas}/lib
-      include_dirs = #{openblas}/include
+      library_dirs = #{openblas.opt_lib}
+      include_dirs = #{openblas.opt_include}
     EOS
 
     Pathname("site.cfg").write config
 
-    site_packages = Language::Python.site_packages("python3")
-    ENV.prepend_create_path "PYTHONPATH", Formula["libcython"].opt_libexec/site_packages
-    ENV.prepend_create_path "PYTHONPATH", Formula["pythran"].opt_libexec/site_packages
-    ENV.prepend_create_path "PYTHONPATH", Formula["numpy"].opt_prefix/site_packages
+    site_packages = Language::Python.site_packages(python3)
+    ENV.prepend_path "PYTHONPATH", Formula["libcython"].opt_libexec/site_packages
+    ENV.prepend_path "PYTHONPATH", Formula["pythran"].opt_libexec/site_packages
+    ENV.prepend_path "PYTHONPATH", Formula["numpy"].opt_prefix/site_packages
     ENV.prepend_create_path "PYTHONPATH", site_packages
 
-    system Formula["python@3.9"].opt_bin/"python3", "setup.py", "build",
-      "--fcompiler=gfortran", "--parallel=#{ENV.make_jobs}"
-    system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
+    system python3, "setup.py", "build", "--fcompiler=gfortran", "--parallel=#{ENV.make_jobs}"
+    system python3, *Language::Python.setup_install_args(prefix, python3)
   end
 
   # cleanup leftover .pyc files from previous installs which can cause problems
@@ -59,6 +63,10 @@ class Scipy < Formula
   end
 
   test do
-    system Formula["python@3.9"].opt_bin/"python3", "-c", "import scipy"
+    (testpath/"test.py").write <<~EOS
+      from scipy import special
+      print(special.exp10(3))
+    EOS
+    assert_equal "1000.0", shell_output("#{python3} test.py").chomp
   end
 end
