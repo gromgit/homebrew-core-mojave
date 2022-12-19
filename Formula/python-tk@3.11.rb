a0e17593e924b553adf4080c9d06cf7c6dbd1d31
Adrian Ho <the.gromgit@gmail.com>
2022-12-19 11:14:44 +0800

    python-tk@3.11: add 3.11.0 bottle.

---
+++ Formula/python-tk@3.11.rb
@@ -0,0 +1,55 @@
+class PythonTkAT311 < Formula
+  desc "Python interface to Tcl/Tk"
+  homepage "https://www.python.org/"
+  url "https://www.python.org/ftp/python/3.11.0/Python-3.11.0.tgz"
+  sha256 "64424e96e2457abbac899b90f9530985b51eef2905951febd935f0e73414caeb"
+  license "Python-2.0"
+
+  livecheck do
+    formula "python@3.11"
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/python-tk@3.11"
+    sha256 cellar: :any_skip_relocation, mojave: "4673f75a8d500bf2964c319c97fbc4d930723fcccd44de2aa365df436681cb02"
+  end
+
+  depends_on "python@3.11"
+  depends_on "tcl-tk"
+
+  def python3
+    "python3.11"
+  end
+
+  def install
+    cd "Modules" do
+      tcltk_version = Formula["tcl-tk"].any_installed_version.major_minor
+      (Pathname.pwd/"setup.py").write <<~EOS
+        from setuptools import setup, Extension
+
+        setup(name="tkinter",
+              description="#{desc}",
+              version="#{version}",
+              ext_modules = [
+                Extension("_tkinter", ["_tkinter.c", "tkappinit.c"],
+                          define_macros=[("WITH_APPINIT", 1)],
+                          include_dirs=["#{Formula["tcl-tk"].opt_include}"],
+                          libraries=["tcl#{tcltk_version}", "tk#{tcltk_version}"],
+                          library_dirs=["#{Formula["tcl-tk"].opt_lib}"])
+              ]
+        )
+      EOS
+      system python3, *Language::Python.setup_install_args(libexec, python3),
+                      "--install-lib=#{libexec}"
+      rm_r libexec.glob("*.egg-info")
+    end
+  end
+
+  test do
+    system python3, "-c", "import tkinter"
+
+    return if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    system python3, "-c", "import tkinter; root = tkinter.Tk()"
+  end
+end
