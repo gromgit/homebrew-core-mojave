48f59ff55661a649e69c31b8315c4e5c5fa70cd2
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:38:55 +0800

    inlets:

--- Formula/inlets.rb
+++ Formula/inlets.rb
@@ -58,7 +58,7 @@ class Inlets < Formula
         socket.print "HTTP/1.1 200 OK\\r\\n" +
                     "Host: localhost:#{upstream_port}\\r\\n" +
                     "Content-Type: text/plain\\r\\n" +
-                    "Content-Length: \#\{response.bytesize\}\\r\\n" +
+                    "Content-Length: #{response.bytesize}\\r\\n" +
                     "Connection: close\\r\\n"
         socket.print "\\r\\n"
         socket.print response
