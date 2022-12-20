ae96871c8f196b904511a8e326ead9597ed4dbde
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:41:00 +0800

    lbzip2: no rebuilding required

--- Formula/lbzip2.rb
+++ Formula/lbzip2.rb
@@ -11,6 +11,7 @@ class Lbzip2 < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "a41953244b7b781c4620ef0b757648ee8f0cd43ef5d616f44a9f4aebf9a5342a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "9815b21683195fdeda12fe2ec7b2f4336d34e7a4b44a4a318c67efed6f9e035e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e9ecc58d178f18ab33d500ed768c156058589fed3132bf804314e76715730333"
+    sha256 cellar: :any_skip_relocation, ventura:        "494a45ea5c053b6bcf63fada0b93c4400ef887d3016eab7e1cee20dd75054763"
     sha256 cellar: :any_skip_relocation, monterey:       "b9882075d6ce9ed47c0e18cc27c9b23706a8ceedc8ff42977b614f8fc92cdb57"
     sha256 cellar: :any_skip_relocation, big_sur:        "4bb02d26e53336134329f3aaacf2ce045375b926b283520788ecdf2ae4d778e6"
     sha256 cellar: :any_skip_relocation, catalina:       "6643ba1c0f17a13e742383c69112df62c1d6bce80e6833d717df4e112922deb5"
