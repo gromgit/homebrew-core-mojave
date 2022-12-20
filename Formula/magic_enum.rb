4122d79842eeb574f85dad8f26ed0f1e2d5299a1
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:44:43 +0800

    magic_enum: no rebuilding required

--- Formula/magic_enum.rb
+++ Formula/magic_enum.rb
@@ -1,13 +1,12 @@
 class MagicEnum < Formula
   desc "Static reflection for enums (to string, from string, iteration) for modern C++"
   homepage "https://github.com/Neargye/magic_enum"
-  url "https://github.com/Neargye/magic_enum/archive/v0.8.1.tar.gz"
-  sha256 "6b948d1680f02542d651fc82154a9e136b341ce55c5bf300736b157e23f9df11"
+  url "https://github.com/Neargye/magic_enum/archive/v0.8.2.tar.gz"
+  sha256 "62bd7034bbbfc3d7806001767d5775ab42f3ff33bb38366e1ceb21102f0dff9a"
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "b6f522d37fbd96ef7ddff60d8ab46a72843dd9757e858c641f6637b34689cbd3"
+    sha256 cellar: :any_skip_relocation, all: "db14fda773775cba338ac65b391fc7af1afb1c3771aefdc471f6f201f708bd2c"
   end
 
   depends_on "cmake" => :build
