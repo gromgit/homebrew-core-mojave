a6a2ffe27abafbafa5be703f66222d79f4a0e9d8
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 19:00:56 +0800

    vcftools: no rebuilding required

--- Formula/vcftools.rb
+++ Formula/vcftools.rb
@@ -9,6 +9,7 @@ class Vcftools < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "b7e5d8712d69c3b8635ee3a626d21c512483e3f87272a36e1629eaa0ede2c12d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e3a70236094425a28dcebf4a60b84cc793c2e55e4198d0f40f25005cb8c33f86"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "02952054dace13adf39f6621591af9c8c33598334dda6c43948fab1e43916239"
+    sha256 cellar: :any_skip_relocation, ventura:        "2841d9ffad2417761e6c116d810512694112942d28f2c650b55ec91dc661e65a"
     sha256 cellar: :any_skip_relocation, monterey:       "8a571823e4f66cb7bd7bf3e8ad91129c5e3ed844f4d9d1f4f4eaca60eb19b23e"
     sha256 cellar: :any_skip_relocation, big_sur:        "fd610cf54b51ec2ec429edfab36b2cf1b3eb14787ab0ef5ff9d805def8c48cb0"
     sha256 cellar: :any_skip_relocation, catalina:       "96424c5e9810127b9f450a88fd314eb94662b35ac88aee4c7efbc8f5420dd989"
