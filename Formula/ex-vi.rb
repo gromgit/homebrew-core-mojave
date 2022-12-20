d73b17d5a805fbd8d97f6f1fe3bf4f47217785e3
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:32:32 +0800

    ex-vi: no rebuilding required

--- Formula/ex-vi.rb
+++ Formula/ex-vi.rb
@@ -3,6 +3,7 @@ class ExVi < Formula
   homepage "https://ex-vi.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/ex-vi/ex-vi/050325/ex-050325.tar.bz2"
   sha256 "da4be7cf67e94572463b19e56850aa36dc4e39eb0d933d3688fe8574bb632409"
+  license all_of: ["BSD-4-Clause", "BSD-4-Clause-UC"]
 
   livecheck do
     url :stable
@@ -13,6 +14,7 @@ class ExVi < Formula
     sha256 arm64_ventura:  "9bfbfbf13fd3909c99c246d69045759540f2affa4bb0061805414d2bfc7400ee"
     sha256 arm64_monterey: "690982e02183d89a46a976131577092a48155ae8e80b9aad89440e7baad51959"
     sha256 arm64_big_sur:  "b5d40f595021f02eb45114157e2e62718b8ee1066ed90d1f4e05ba39f4aa1859"
+    sha256 ventura:        "bbf655f53c0750fb462a03541121de7a4f4007ac148b0f38015979d390c8ba9a"
     sha256 monterey:       "205fe1e5548f4f8b8f357e85f2a1729dc60a12e62669e9c7dd56c4299f2168d9"
     sha256 big_sur:        "8cbdb5b3f60cec0b0b7dcfb9c11c06d159428b5d1c24a1b889fcf470839ff024"
     sha256 catalina:       "843fceed3514fe1506e32619c15c092441d45d553a809b315f38e1b749623492"
