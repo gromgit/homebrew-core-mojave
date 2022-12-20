53bbf01f550d77f80a9f0b047a04e338968187dc
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:26:10 +0800

    box2d: no rebuilding required

--- Formula/box2d.rb
+++ Formula/box2d.rb
@@ -10,6 +10,7 @@ class Box2d < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "1f944702e28ec7033d7c7da6daee29d1b6153b737fe515754adf898a701b3cfc"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "360bc1f63ba8cb1f6c97316c00da7ae547bcdb0be6c18f1dd441a338dbed67f6"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8f7821254f00753fc07028c94c0c08e42b3e05dbee86119887c43d446b786270"
+    sha256 cellar: :any_skip_relocation, ventura:        "4f5a62c8c3cc7c77494381117f15ccd74c6d3a7569bb4e4ade8840bd489181dd"
     sha256 cellar: :any_skip_relocation, monterey:       "ce38e11785a57c7cd416fd4cb6a9cd2567363e1f1bc4938d010b13e0b2eefc34"
     sha256 cellar: :any_skip_relocation, big_sur:        "bec33552a3bf184fd75f6adbb193b15595c7729dd7f457c833b7826c6253c28d"
     sha256 cellar: :any_skip_relocation, catalina:       "5c6508a2d661409273a28ac5f0495d7d7c506b5d1bc7ceeb9ab90298db225178"
