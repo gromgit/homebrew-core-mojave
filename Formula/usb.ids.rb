7361686b8a05f6b843c8398b44850791791aeb45
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 19:00:38 +0800

    usb.ids: no rebuilding required

--- Formula/usb.ids.rb
+++ Formula/usb.ids.rb
@@ -1,8 +1,8 @@
 class UsbIds < Formula
   desc "Repository of vendor, device, subsystem and device class IDs used in USB devices"
   homepage "http://www.linux-usb.org/usb-ids.html"
-  url "https://deb.debian.org/debian/pool/main/u/usb.ids/usb.ids_2022.05.20.orig.tar.xz"
-  sha256 "e78e3d4200bd3165c46450f5416fa7a93b35ecc11670b87c9d687de479225b6f"
+  url "https://deb.debian.org/debian/pool/main/u/usb.ids/usb.ids_2022.12.09.orig.tar.xz"
+  sha256 "cda1471d1bf5a9769017018504e3ebdde6efbac7f6a27af4a40189e66e506b58"
   license any_of: ["GPL-2.0-or-later", "BSD-3-Clause"]
 
   livecheck do
@@ -11,7 +11,7 @@ class UsbIds < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "3a244b475632d3f94cf4197b3dee71d1fd685e9c1bfaa8843e237a1d3f31a066"
+    sha256 cellar: :any_skip_relocation, all: "99f406a9a8e0bd59ce46386dd5c88e6f99e6c3dabff32a461c22e64772591375"
   end
 
   def install
