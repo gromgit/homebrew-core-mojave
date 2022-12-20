4950f951dfcf28881ad739f83b7c3a0d56244e0b
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 19:02:54 +0800

    xctesthtmlreport:

--- Formula/xctesthtmlreport.rb
+++ Formula/xctesthtmlreport.rb
@@ -10,6 +10,7 @@ class Xctesthtmlreport < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "15e3b813ce0361421dd94757db348ec783d6b25643af13ba983cff8d3d075e35"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "09adf8c3aaef36698802f2f4ec9df0c54a2c30331793ad02443486e45bd157db"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f68268a932d48a169c9645c8b9bd9daa7927289736b5ff0bdf8a30363b1d5556"
+    sha256 cellar: :any_skip_relocation, ventura:        "020289c8131fd73d622134430888a9a4b66676628da1644a5359ffa63d198c20"
     sha256 cellar: :any_skip_relocation, monterey:       "8fadf0792948314dff43eeec059bde7cc9ff10494cbb7542c279dca7424dd264"
     sha256 cellar: :any_skip_relocation, big_sur:        "0beaf5cedab2fada15b51b9e6225674e70457aa20ab2f5e682ed89ad3462a1a5"
   end
