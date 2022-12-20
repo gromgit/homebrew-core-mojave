58965d7d372a3b26816cb858d0b6e494e16f08b6
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:37:34 +0800

    hayai: no rebuilding required

--- Formula/hayai.rb
+++ Formula/hayai.rb
@@ -9,6 +9,7 @@ class Hayai < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "dd6a2a14ff0cd666059ddf5aecbbca1679a1fd40248c981566223c308f3dd659"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "9dccf9e4fa4cd6918a8bf6e37008b59044af49aedf965a878d35fe5200d42062"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "41ba5eb3f260d738729a866e1951d9caf2830eacb918944da50ab0761a4b4f56"
+    sha256 cellar: :any_skip_relocation, ventura:        "9db2e26c1c283f4ce4875ba2c5b4639cbdfa800276f059abe407bff4098300df"
     sha256 cellar: :any_skip_relocation, monterey:       "fe3175b383887bb7a3c8d98378c76c09fd1b1bfe9ad64e7f119df3d6054faebc"
     sha256 cellar: :any_skip_relocation, big_sur:        "2ced5bfcd90e829400f4d8f92c5069d1af7b7bb913a0a3dd089f6ef41d89c86d"
     sha256 cellar: :any_skip_relocation, catalina:       "0a63325782e38d9ea125ec2948604856a2d0a95a89607bbe3eb8730ca5034009"
