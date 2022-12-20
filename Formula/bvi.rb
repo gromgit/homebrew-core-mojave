2ccc51d75ce5dfe9665672e668926f2cf62af08e
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:26:26 +0800

    bvi: no rebuilding required

--- Formula/bvi.rb
+++ Formula/bvi.rb
@@ -9,6 +9,7 @@ class Bvi < Formula
     sha256 arm64_ventura:  "b08d6d539610ecfa5cc971f294acd494b09f158f4796fbfb747b6421bbe3b887"
     sha256 arm64_monterey: "a8a313fa812ea288a6a317cb5b3bf255e19180f0fbb568401ab48188594545e5"
     sha256 arm64_big_sur:  "5e71e7f93fd8523543f85723621ca80faa67482ba01019eddcc0408e95a0b21d"
+    sha256 ventura:        "e6b1dbdc4e3a3011ffc92803a1737064d5a64301e5f193b71ffa74230746ac7e"
     sha256 monterey:       "fec4cc7e517b1ee4810e76e4affcf724dea696dbea9fed3bcc0a1063e385ecac"
     sha256 big_sur:        "cc03571d7931314c6fbe6adacace77b4d6b5204b6c34d57da40f45915f84b1db"
     sha256 catalina:       "83cfa7a1fe8848d8eab7f01da94a32e75eb7c57221854a3f3f06a05417975977"
