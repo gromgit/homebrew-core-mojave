e76b929ad2538239431f6c8b0f69e440f7662388
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:48:33 +0800

    objc-codegenutils: no rebuilding required

--- Formula/objc-codegenutils.rb
+++ Formula/objc-codegenutils.rb
@@ -10,6 +10,7 @@ class ObjcCodegenutils < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "d068aa1c50d2e54ceddf2d21a04295829b30e7f3f5504c88364a1bddae5b1a1a"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "d5ddfb8ef11849ea3ef38002ca27c492fb9802886ac6cbaf611baa51606de4b7"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9301f21479af32f32469e8235780f85b74d3a5e7c783fecaef7bb896c734dd55"
+    sha256 cellar: :any_skip_relocation, ventura:        "6e60602b0de8383fe7fceca41a4f13dc5bb65c27b1fdd84586e1e9c25bbffb52"
     sha256 cellar: :any_skip_relocation, monterey:       "508fba7003bf8b7e348c96b131558f19bde0a9f08a6096b79a733e1a4d906386"
     sha256 cellar: :any_skip_relocation, big_sur:        "5bd0a4e91d15de385bce1d3ab0ceefefe56d69d461f763373933e64e2d7db992"
     sha256 cellar: :any_skip_relocation, catalina:       "24745ae53d47e15598835ee0538c3f121c48b31b21902b1fd3fab0a8c9886543"
