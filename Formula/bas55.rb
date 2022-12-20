4a44c6590d0b06ead7e0638652ba724c755f86d2
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:25:23 +0800

    bas55: no rebuilding required

--- Formula/bas55.rb
+++ Formula/bas55.rb
@@ -14,6 +14,7 @@ class Bas55 < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "cc53e9763ae7d94d6ea55e4677812f25224667715a0272beea5269f943ec83fe"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "7789ffd65c9afc8d415385c439205a62e369c598bd6cc7b9133e306cff8a5be2"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ab48670d6620654c1e88b8076d9c9805e6f6b2fde8f1d8773ac96fd5bd234789"
+    sha256 cellar: :any_skip_relocation, ventura:        "13e4a822761b94be8b4f617a318fe41d0f96a54cb6e54589c3d4f36ca38d9c56"
     sha256 cellar: :any_skip_relocation, monterey:       "2696588487bbff56058e0370ec09b63143f5e12c1206490066d7b213d57260db"
     sha256 cellar: :any_skip_relocation, big_sur:        "c9a50cf6904cafd3a75cbb762ea68d3545eadc832be0b2c0313aaadf03dfe453"
     sha256 cellar: :any_skip_relocation, catalina:       "7eaaf506f35bbc8f2149028c7b379f3b5fc7d5bd7899d021b016f94272e9441e"
