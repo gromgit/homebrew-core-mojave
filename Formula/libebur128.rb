faf4f4829e4b3bbbb4ec555d46c1edc0367e447b
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:41:46 +0800

    libebur128: no rebuilding required

--- Formula/libebur128.rb
+++ Formula/libebur128.rb
@@ -9,6 +9,7 @@ class Libebur128 < Formula
     sha256 cellar: :any,                 arm64_ventura:  "c0253f875a0adcd097d523191b855d597d602731e73dc039d4ce707373674d5f"
     sha256 cellar: :any,                 arm64_monterey: "d77d92a90aa8dfcb7642efe678d02804a0a9d0c172e43e005499120ead01b3b8"
     sha256 cellar: :any,                 arm64_big_sur:  "99450597a660d645800d8d0488b657efee8d7ff9b886a80f964fe3394c8a2552"
+    sha256 cellar: :any,                 ventura:        "e6b85bc2e07ac3e7b4d3ab8a9a05f7a2cde5e6377b5f2898e454acb598a82ff0"
     sha256 cellar: :any,                 monterey:       "6a1d7c49352c44807fbddff035842984a6d2e320d4d0625fd2271752246bcdfb"
     sha256 cellar: :any,                 big_sur:        "43567ee920b45921fb0d7787f40d3274ff42360c3048df470aee33be902694e7"
     sha256 cellar: :any,                 catalina:       "a9612342890303e8859ee23c7ce8d154f1d3eb134158322aa4ca0968d471281a"
