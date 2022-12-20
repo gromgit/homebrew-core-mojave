30fa20264b5c2dc4df04a4f37f269fa02d7e1638
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:53:39 +0800

    rack: no rebuilding required

--- Formula/rack.rb
+++ Formula/rack.rb
@@ -12,6 +12,7 @@ class Rack < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "0759cf2ff2e0d842496eb25f1f485532d9aa42ef1f62d1c4210ea4a4fdace3d9"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "d81bffa7fcd9b0e5079359935e35b155a3e0a970b0526b50928c084522fcdc79"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3da24ffbc52e301b97902eee71ecddbbb16c6df508a9241a4a9ed2dc7eed0652"
+    sha256 cellar: :any_skip_relocation, ventura:        "9899299df120a54af4b9137076d412649bcb8bde210fb7a60b49552fbe9e7528"
     sha256 cellar: :any_skip_relocation, monterey:       "5f0280df3a5a8ea3e28533d43434ce2d97e0ba3ff35f74f1b3041008f594b820"
     sha256 cellar: :any_skip_relocation, big_sur:        "8f5f2eac4a06a9295875d213a31505f9b8e66e96b16814176582e3fd5a0a223e"
     sha256 cellar: :any_skip_relocation, catalina:       "8cf224e3f734308bef6c0ef3cd9aa3a63aa4fdedd9ee626e2ee91099affc83c2"
