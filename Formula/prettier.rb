f16f2c8198783403d981cb31f0409044b50df7c5
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:52:07 +0800

    prettier: no rebuilding required

--- Formula/prettier.rb
+++ Formula/prettier.rb
@@ -3,13 +3,13 @@ require "language/node"
 class Prettier < Formula
   desc "Code formatter for JavaScript, CSS, JSON, GraphQL, Markdown, YAML"
   homepage "https://prettier.io/"
-  url "https://registry.npmjs.org/prettier/-/prettier-2.8.0.tgz"
-  sha256 "58ebc3f39f4f1ca4a284811d379a19e19d456547ba784646d68bd97d7d755929"
+  url "https://registry.npmjs.org/prettier/-/prettier-2.8.1.tgz"
+  sha256 "e70f531d8744ae7773dbaaf7a35138638c35fda1a853cafa38f5d2ffa51aba9c"
   license "MIT"
   head "https://github.com/prettier/prettier.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "68529b66fd81f8626197a4b1bb774706b13bffda8c607b2aabe6da3e6150978f"
+    sha256 cellar: :any_skip_relocation, all: "dbc21dfcb0688ae42e6cad545b537806b8e31be8c4958db95f4de5b9719aa4b8"
   end
 
   depends_on "node"
