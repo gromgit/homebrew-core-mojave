ba78c18ae6737da0ffead0fcc7a19561402a8936
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:42:53 +0800

    libpointing: no rebuilding required

--- Formula/libpointing.rb
+++ Formula/libpointing.rb
@@ -9,6 +9,7 @@ class Libpointing < Formula
     sha256 cellar: :any, arm64_ventura:  "79773a252a784d765237674545e3355bec847c95f9ac82cc89826936954f8990"
     sha256 cellar: :any, arm64_monterey: "777a0f897878a4da3693f9d8a5717f42ff70fc281a81b57b4841a31ce17e7100"
     sha256 cellar: :any, arm64_big_sur:  "19de172dd9ad6744f9939955a5c526d3626400727631cdd07a6e22d8791fbf48"
+    sha256 cellar: :any, ventura:        "97732d46ffab874e21adbaeaf3a6953df026772565ccfa5dcb5f5d51378ac75e"
     sha256 cellar: :any, monterey:       "9fad8e2c767cc76679b49546cf443a0ec1d7b7115dbd82faaff20649b3b77ff4"
     sha256 cellar: :any, big_sur:        "e9168eee924fc759e012e3ef41d64750d732f0d09a7af068fd935746835da472"
     sha256 cellar: :any, catalina:       "d56d66f5df0d6e1c80cc4e4951e8add9cbb0c5fb76080c9107f66665b8b46e48"
