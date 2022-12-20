fdfcff86c07f9dcbb12d33c6da3077e4f20454e8
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:51:35 +0800

    plantuml: no rebuilding required

--- Formula/plantuml.rb
+++ Formula/plantuml.rb
@@ -1,8 +1,8 @@
 class Plantuml < Formula
   desc "Draw UML diagrams"
   homepage "https://plantuml.com/"
-  url "https://github.com/plantuml/plantuml/releases/download/v1.2022.13/plantuml-1.2022.13.jar"
-  sha256 "dd54cde638aad75aafdbaa7a28dca0dc6bb4dcba8cacce5c810572c5e5d2a522"
+  url "https://github.com/plantuml/plantuml/releases/download/v1.2022.14/plantuml-1.2022.14.jar"
+  sha256 "159ca2021f2efa5481b56417e2abd0a301348b018fe3c34b106f41023596dd43"
   license "GPL-3.0-or-later"
   version_scheme 1
 
@@ -12,7 +12,7 @@ class Plantuml < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "934413f4923dc4c51997f139abe6fce343babc7e13eea2e9a57b5660adbf0d55"
+    sha256 cellar: :any_skip_relocation, all: "32b268d80ce23aeb0dd058a385461a41b8c0f99c8de9f17cf923d9f3eb498774"
   end
 
   depends_on "graphviz"
