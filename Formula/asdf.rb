ab54394e18293798021b57f885fd827d6529d86d
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:24:32 +0800

    asdf: no rebuilding required

--- Formula/asdf.rb
+++ Formula/asdf.rb
@@ -1,14 +1,13 @@
 class Asdf < Formula
   desc "Extendable version manager with support for Ruby, Node.js, Erlang & more"
   homepage "https://asdf-vm.com/"
-  url "https://github.com/asdf-vm/asdf/archive/refs/tags/v0.10.2.tar.gz"
-  sha256 "a097d40888c276cb20e1489a3da6573dd9d184d8e6518c5f8177d3c2c1066f57"
+  url "https://github.com/asdf-vm/asdf/archive/refs/tags/v0.11.0.tar.gz"
+  sha256 "ee66eb05688ce54dc1459bff90d043610492ebf393ddcf0847a295c92a9b2f7f"
   license "MIT"
   head "https://github.com/asdf-vm/asdf.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "2098bd4ff1da95064d7d112e8287532c077b1e1bda0dcb91fd977f7bac65ba5e"
+    sha256 cellar: :any_skip_relocation, all: "8f0826eeb40dd4dde2399e299ef1231d1207da315143e5e8bccef5e2bb0ce033"
   end
 
   depends_on "autoconf"
@@ -16,7 +15,7 @@ class Asdf < Formula
   depends_on "coreutils"
   depends_on "libtool"
   depends_on "libyaml"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
   depends_on "readline"
   depends_on "unixodbc"
 
