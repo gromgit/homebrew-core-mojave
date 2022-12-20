aa043fc06ff8a65d389e87f008afecddadeffc79
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:52:40 +0800

    puf: no rebuilding required

--- Formula/puf.rb
+++ Formula/puf.rb
@@ -9,6 +9,7 @@ class Puf < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "1ba1fc9eecb1a5920c6eadb88fe5c2142bafd356d420356f15afb0a923ef5b3b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "48d6dada2a26fdd71146a6e83b2cd5792af6a365804f5d714171eccbdcbcdc7b"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f9daf26921e00b28187380efab232b5f4d4e02877e258ec6e4e204d446c25cf0"
+    sha256 cellar: :any_skip_relocation, ventura:        "34f663287a1e6eccd24478afff1260281cdca68dda5efa9a6211d1aebd6d425b"
     sha256 cellar: :any_skip_relocation, monterey:       "c1afcd4b99802518b7cee90c23225bec8d2d7104efa503c4115830ab51f582e0"
     sha256 cellar: :any_skip_relocation, big_sur:        "590e0087a563c0fa38996f69c80316f95a54a3a788f0e07390ba192db1d67c44"
     sha256 cellar: :any_skip_relocation, catalina:       "cad4c55abee941651ac9e1f203041240aae43b990f3e9efdce7cd9e0342b727c"
