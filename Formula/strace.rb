6f701a7599fb44ceeaa82330348ebfe2452c8524
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:57:50 +0800

    strace:

--- Formula/strace.rb
+++ Formula/strace.rb
@@ -1,12 +1,12 @@
 class Strace < Formula
   desc "Diagnostic, instructional, and debugging tool for the Linux kernel"
   homepage "https://strace.io/"
-  url "https://github.com/strace/strace/releases/download/v6.0/strace-6.0.tar.xz"
-  sha256 "92d720a666855e9f1c6a11512fd6e99674a82bbfe1442557815f2ce8e1293338"
+  url "https://github.com/strace/strace/releases/download/v6.1/strace-6.1.tar.xz"
+  sha256 "2579e9cec37dbb786f6ea0bebd15f40dd561ef2bde2a2a2ecdce5963b01859fd"
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "bb4b65bcd9d129f38512dfbf6604fbd7a085ff11c19fcf6a6997e2d123228a02"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ed23dd4081a62bb5c797619220352d2fc256a4d942a13d29fc777094209dcb75"
   end
 
   head do
