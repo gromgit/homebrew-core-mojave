84f19a5f99f1a56bd1dc9971fc2bac2332fcbeb1
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:41:10 +0800

    ledit: no rebuilding required

--- Formula/ledit.rb
+++ Formula/ledit.rb
@@ -18,6 +18,7 @@ class Ledit < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f4e5f04c79d703b1e22f1b49794bbf416135f209fcad88ae3bc043bc17114c1b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "642349a8d05b4f9048fcd7d9fdf389e35d98b921e3d52bd06eee365b50a4f1e2"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "06141836398681d2250bf04d1bba965038f5f707482f0ecab1cc464c8a95bcfb"
+    sha256 cellar: :any_skip_relocation, ventura:        "2f70d37553e6bb5b2e1953781022747f4f1d0659934ef5103e92baa10b481d70"
     sha256 cellar: :any_skip_relocation, monterey:       "da6338af250d9b52557f52707b0730c379bdd8216c53e10477c351f72d6aa406"
     sha256 cellar: :any_skip_relocation, big_sur:        "2d404ace597c8a7062fbe96e15e9e7d1226ec5ca97e0c8981062c77fef10b4eb"
     sha256 cellar: :any_skip_relocation, catalina:       "158141ebf4edc253de428b8789d77eae0b19fdd4d8002e9910cf4c2486a12bb6"
