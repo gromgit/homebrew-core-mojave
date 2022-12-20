62d1290027da48ac42979c449d9f8b4da791a58c
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:31:50 +0800

    editorconfig: no rebuilding required

--- Formula/editorconfig.rb
+++ Formula/editorconfig.rb
@@ -10,6 +10,7 @@ class Editorconfig < Formula
     sha256 cellar: :any,                 arm64_ventura:  "b30ba34e0591e4f6f86c6ad0a9ba8fa7ad766ff7692f010254e242a10a1b0e1f"
     sha256 cellar: :any,                 arm64_monterey: "e1be5f7ae1a5d746932cde2791637c52c9457abf4decffa0c3339600e1f7b2f3"
     sha256 cellar: :any,                 arm64_big_sur:  "8de5a49262229377ff5f97f670c729626c7c2a688c14cb901e1d6342eee99f92"
+    sha256 cellar: :any,                 ventura:        "4e788b50b9f6009cd6dd3efc52ea327f70c33a24c92e8b029294344d3072fc4e"
     sha256 cellar: :any,                 monterey:       "8460e9b0785718dc81aa4c895a9e7a5225648e0958ae076d3f93b1bc897232ec"
     sha256 cellar: :any,                 big_sur:        "35b906670a62d96a889967f904b9b157ca860f72993be36f4085081df24d23d1"
     sha256 cellar: :any,                 catalina:       "b6cac809fe16ed3e788d3bab017f128a6c960652104ed541791cca02b6e4bc16"
