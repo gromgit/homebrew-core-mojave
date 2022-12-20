ee06a27dea0182466919dd74af2784e15e58106b
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:53:22 +0800

    qstat: no rebuilding required

--- Formula/qstat.rb
+++ Formula/qstat.rb
@@ -9,6 +9,7 @@ class Qstat < Formula
     sha256                               arm64_ventura:  "11a9a33fc7615e85441444af37ed41d87f0659f672d958824f7cca77eacd2729"
     sha256                               arm64_monterey: "407570a7c868ec0815588e1b2c5ed8716b1392bc7452fcc31ab1b16f217febaf"
     sha256                               arm64_big_sur:  "381072b9472ed1ab8a8f0d7409571262555d33da9d02f3ccdabc117bc9d6a6c5"
+    sha256                               ventura:        "288eff7f500462b9adcc7e3704fdd2b07ce5853830adfa9d246aa67ff1411df3"
     sha256                               monterey:       "1e7728d7b8689eec71c2285e3c1595180a985f6d44d32d6452df62da33ab4bab"
     sha256                               big_sur:        "e7620697e587b4e46f1f0a1558e2fb9a1dee96c289958ea54269cf7321197d26"
     sha256                               catalina:       "c8308182a8669cd883eca05230d449aaeb0026bb47bd4b1a2d420a34b6051549"
