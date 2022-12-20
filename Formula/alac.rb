090bc0d463d104c4a30d0ee8fea3931daf3d49d5
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:23:48 +0800

    alac: no rebuilding required

--- Formula/alac.rb
+++ Formula/alac.rb
@@ -8,6 +8,7 @@ class Alac < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "71a71cbbe491b65fd89198aab4168b7ec3d2436896c913a43858c806469a56ea"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "71cd043cec976255c6ae78ad79021dd5ba499b5678e5163e6353f571cae2eb6d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3d12d2c7b28c99fba529faea181dc91a04ea469e68607f9e3263c082dcb5cde4"
+    sha256 cellar: :any_skip_relocation, ventura:        "5385e4fecc817b09a2be15e6eed1184cd2c4bb1014c6c89d9611a151adb49555"
     sha256 cellar: :any_skip_relocation, monterey:       "c0e143a554186ce5b3c1ad9850c5f65d0248461eeea9f8f02389f74e78989a14"
     sha256 cellar: :any_skip_relocation, big_sur:        "8d6293bbacf08bada008f799f03c6ea3265dd48bd5c81d77d042e4a3bedcf84f"
     sha256 cellar: :any_skip_relocation, catalina:       "0cb8439e4028ea823fb442559c12365bae08499a142ad46d0c89f010f9eb7e5d"
