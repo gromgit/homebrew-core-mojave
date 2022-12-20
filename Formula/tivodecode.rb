dd970264992b4b99ba9cda23527e316ac938bb9a
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:59:19 +0800

    tivodecode: no rebuilding required

--- Formula/tivodecode.rb
+++ Formula/tivodecode.rb
@@ -13,6 +13,7 @@ class Tivodecode < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "edea82441461a1fb59d0b4ffff0c70063e2dd064bbcbaf7dd2f35a9fbc464602"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "d2289a446ab7ec226d1b6b3ddb042336d0a223009268984fa5d949394842e2e6"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5171d7774e6eed485e7b3d57e3dd2b9bc0e935f97dd7890109ccdb3c4975ab62"
+    sha256 cellar: :any_skip_relocation, ventura:        "2e6c1b18e7f2309d3ff7197901b23c59e548242cf2489daca28f0b8faca44ee5"
     sha256 cellar: :any_skip_relocation, monterey:       "dab7b05eb81397cfcb9e875a351d24d3a05741c77aaf28da7411318ef72dd770"
     sha256 cellar: :any_skip_relocation, big_sur:        "aeec3ab80bd78902f47343a699f3ebd4566b2d3fd9ce8076550bd705caf69486"
     sha256 cellar: :any_skip_relocation, catalina:       "153d8b8e152ccf3c87041ff40739a80952da5ad06c572d7febb9b222b16069e1"
