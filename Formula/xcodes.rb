21265a2ab5a3d505888ea425e4dce6669e3eb8ed
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 19:02:50 +0800

    xcodes:

--- Formula/xcodes.rb
+++ Formula/xcodes.rb
@@ -1,15 +1,15 @@
 class Xcodes < Formula
   desc "Best command-line tool to install and switch between multiple versions of Xcode"
   homepage "https://github.com/RobotsAndPencils/xcodes#readme"
-  url "https://github.com/RobotsAndPencils/xcodes/archive/refs/tags/1.1.0.tar.gz"
-  sha256 "c6003d8ea2450b9d8f6b6de0c59b45471cda820289ee26f017148fcb4d662c4e"
+  url "https://github.com/RobotsAndPencils/xcodes/archive/refs/tags/1.2.0.tar.gz"
+  sha256 "561a6646421cbdb37cd44f89d34647f40e6cbe643f33a51888bcf5046840b9ba"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9c2dbbbf7469ae6b790a42718ad8ad3a0a08d0f139caa7f70dc3c6528ac92a55"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9029b3feb389e7120455cfa942895b94878615516b3556602ff5254761b0ff7d"
-    sha256 cellar: :any_skip_relocation, ventura:        "e3d5bbc993046e1f6da3ce17fb00531371128a46dae98316137ba1b64480bf75"
-    sha256 cellar: :any_skip_relocation, monterey:       "2751e68b83633b2728f9939a21bafb993049ac78165ed09c928005bcff763f50"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "7e1392fa4b7eda1597523fc50a848108f1417682b6c87bcf2331eb3e065b6801"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "a6ce9c2019f07f0a0a1dc73b626e04bd571d2d882323528b9d19e8b8e841443d"
+    sha256 cellar: :any_skip_relocation, ventura:        "71a947353cf60e60c0bf901ec57ae5b13cb651e4b70ac5ee7a89e8db7269aef6"
+    sha256 cellar: :any_skip_relocation, monterey:       "1c65f7057e80b37aebb51e8e8d4e063bb5fe54b23f2d9bc0bdf9a4dd38999808"
   end
 
   depends_on xcode: ["13.3", :build]
