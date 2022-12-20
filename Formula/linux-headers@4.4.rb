0f9ca652090be527048e07e8311c678fd4083e5c
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:43:47 +0800

    linux-headers@4.4:

--- Formula/linux-headers@4.4.rb
+++ Formula/linux-headers@4.4.rb
@@ -1,13 +1,12 @@
 class LinuxHeadersAT44 < Formula
   desc "Header files of the Linux kernel"
   homepage "https://kernel.org/"
-  url "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.4.80.tar.gz"
-  sha256 "291d844619b5e7c43bd5aa0b2c286274fc5ffe31494ba475f167a21157e88186"
+  url "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.4.302.tar.gz"
+  sha256 "66271f9d9fce8596622e8154ca0ea160e46b78a5a6c967a15b55855f744d1b0b"
   license "GPL-2.0-only"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "e924cdf506f91e9010ea3dd8a32396262a28bc54ac0290982fd85a3215e7db2a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "08339aac091f6bef8e643268c0801898e14acd349420f7e08df8c2094efcef8b"
   end
 
   keg_only :versioned_formula
