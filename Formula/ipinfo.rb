d8e5035fa8930541dee4685ea4eb72ac38bbbd0a
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:39:05 +0800

    ipinfo: no rebuilding required

--- Formula/ipinfo.rb
+++ Formula/ipinfo.rb
@@ -16,6 +16,7 @@ class Ipinfo < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "48870fd5a3ee495bbc62c79d5706403ccef08c28f00d503b7bd661315bf2016c"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "e36096abf98dc91542e89ef61e240b9470d7b4203d721f18d9e0021a0bc373e8"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c631709cb3810dbbef8f37e1b2d7c76fb301ae36bb9b2d63885ecde62152b7ef"
+    sha256 cellar: :any_skip_relocation, ventura:        "b6200ef7097f9b8deaf0b7c28b968e6af3577e22a38345fccc859c89ded033b1"
     sha256 cellar: :any_skip_relocation, monterey:       "dd6a283e541551be0bda8e82d322dc5f057e363021482cb8111afd0d045d6924"
     sha256 cellar: :any_skip_relocation, big_sur:        "9b70f868f6a9a1c2e59247a09510e14e3da1a45c2acaa86fde9b93a155a14e68"
     sha256 cellar: :any_skip_relocation, catalina:       "b2202f465e419b0bc7e3667d75247cc37a46b49d9a4eb5f23f1f63cb361fd366"
