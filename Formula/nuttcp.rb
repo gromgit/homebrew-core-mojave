421dd35d74ffefc3020d56148dd7b03839a57f5e
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:48:21 +0800

    nuttcp: no rebuilding required

--- Formula/nuttcp.rb
+++ Formula/nuttcp.rb
@@ -14,6 +14,7 @@ class Nuttcp < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "7ac8108263034cac96c76efb07222a474c603fee130f0751dd9957bd52111280"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "354b4a9b24a8af78f93bc7214b10137897a2bb04d49e42273a7b203265309fce"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c284b20a30f158f7321ca918bc27ffac8f5e644e85acba6231477aa9c4a9f06e"
+    sha256 cellar: :any_skip_relocation, ventura:        "3f32f4184daaad9ca38c76c61eef16551706949199fc6ca890357547e249509c"
     sha256 cellar: :any_skip_relocation, monterey:       "23f6274a513fc5e03b2eba2ea26496beb8e1b7e21ac0824fb7abea9e3487b296"
     sha256 cellar: :any_skip_relocation, big_sur:        "9001ef97c90c4097f1ebabed20e63305f82a5f04d7ffc0f0d788c249c49d236d"
     sha256 cellar: :any_skip_relocation, catalina:       "0f5e7a2b61f91360023ef643c0a77fa711855b34006ff07867f1283051aded5c"
