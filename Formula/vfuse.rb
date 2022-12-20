cc53f11648ac416e4d3ffe088a8be1cb121db227
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 19:01:07 +0800

    vfuse: no rebuilding required

--- Formula/vfuse.rb
+++ Formula/vfuse.rb
@@ -3,6 +3,7 @@ class Vfuse < Formula
   homepage "https://github.com/chilcote/vfuse"
   url "https://github.com/chilcote/vfuse/archive/2.2.6.tar.gz"
   sha256 "fbf5f8a1c664b03c7513a70aa05c3fc501a7ebdb53f128f1f05c24395871a314"
+  license "Apache-2.0"
 
   livecheck do
     url :stable
