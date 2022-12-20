458ec35ba4d059a19fdb9eaabe5a9efb1393e4f3
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:24:53 +0800

    avce00: no rebuilding required

--- Formula/avce00.rb
+++ Formula/avce00.rb
@@ -3,6 +3,7 @@ class Avce00 < Formula
   homepage "http://avce00.maptools.org/avce00/index.html"
   url "http://avce00.maptools.org/dl/avce00-2.0.0.tar.gz"
   sha256 "c0851f86b4cd414d6150a04820491024fb6248b52ca5c7bd1ca3d2a0f9946a40"
+  license "MIT"
 
   livecheck do
     url :homepage
@@ -13,6 +14,7 @@ class Avce00 < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "9078894641a0060c43d87875285e11baab6f2a7f24c36f109f56687b2eaf5674"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "5d68b4c2b28928e6f435666ae990ecc9f6ad4b082158374c24437c7d18d6a84f"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "9c4e10ddc6cad4b6a7001a697b8318513a09166e70fa6831ed5fdbede1e71d47"
+    sha256 cellar: :any_skip_relocation, ventura:        "eb6894812ee0f45a4922522cc2c8a38c6f023dc49be73b70209908be6c373ea3"
     sha256 cellar: :any_skip_relocation, monterey:       "eea33df8503aff86d07c0e10947fa54e57c579755086838aca78da96b00d4dce"
     sha256 cellar: :any_skip_relocation, big_sur:        "1fdd45d6a401ca88019bbd58cb3afdda23dedf706c4556e87a7cc48b1a3e952a"
     sha256 cellar: :any_skip_relocation, catalina:       "db71ee14a03d041413530c0974ce7703100dc3259fc0d2ea5a32fadcf7180133"
