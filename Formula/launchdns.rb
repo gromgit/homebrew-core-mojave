fbcfa039c8046193dc3c57dc6f176f8954a3750b
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:40:58 +0800

    launchdns: no rebuilding required

--- Formula/launchdns.rb
+++ Formula/launchdns.rb
@@ -12,6 +12,7 @@ class Launchdns < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "480788e6d0135672b26a62c84111d1d6cc1e3f8407da7338cd1ea334767679bd"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "76976e31629220e8697a50b0e52d080cef29a6b761a987175b07438d35225ff8"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1b7e3e37f394c83c8957c6c2253260805a3abcbb843890c90208d7d743da3328"
+    sha256 cellar: :any_skip_relocation, ventura:        "13f7711fc211beda24f6150ce03ded562696f8748c484e92608574308b9e2474"
     sha256 cellar: :any_skip_relocation, monterey:       "7883b009f177ae1ede81bc9d27706e26fc8d8bde4cd3e1c45c5cd8f4021cbafd"
     sha256 cellar: :any_skip_relocation, big_sur:        "87785cae4d4966c318e8fb8424749261b16bb543576e1c45d5fa2bae7f4c3f0e"
     sha256 cellar: :any_skip_relocation, catalina:       "ebae3446c46a7a6662c3e9b95d61bbee372f1f277a07a4beea1eafc00d64570a"
