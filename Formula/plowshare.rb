725355ab3027534740100ca9cfe84d80a236f235
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:51:39 +0800

    plowshare: no rebuilding required

--- Formula/plowshare.rb
+++ Formula/plowshare.rb
@@ -11,6 +11,7 @@ class Plowshare < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "861153a64f192c3e5ba25f43e59d5b3d8a96064cfb422b4d7a76986f96a4699d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "0fdf55cf9624e4d8a9abfd52b93db4edb8540082d2ddad5bdee597612862aca0"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d5552280803160034db82652d6eb8fa9ead72d8bd4c9be2c0e03c9b6ee2a897c"
+    sha256 cellar: :any_skip_relocation, ventura:        "459e03bc4ae1b5474cb45ede0e4cf578ac07e09ddf53ef5576a4d8565727f25c"
     sha256 cellar: :any_skip_relocation, monterey:       "9ee2632cc598eab9744d758e037c8a0eecc202c19e93ed01670bdefecb6ba00a"
     sha256 cellar: :any_skip_relocation, big_sur:        "3d995918e629820f93c9a6d9e2661c4182ba181d2959306adbbfea1b24af5498"
     sha256 cellar: :any_skip_relocation, catalina:       "71fc52474893fbb6b7d0a9644ea1a368a59f91fb59c946052a060a10e493157b"
