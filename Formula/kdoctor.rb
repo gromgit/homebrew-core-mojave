7fef181c57d0d0d49435524e11d555bce805fd7f
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:40:14 +0800

    kdoctor:

--- Formula/kdoctor.rb
+++ Formula/kdoctor.rb
@@ -1,23 +1,24 @@
 class Kdoctor < Formula
   desc "Environment diagnostics for Kotlin Multiplatform Mobile app development"
   homepage "https://github.com/kotlin/kdoctor"
-  url "https://github.com/Kotlin/kdoctor/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "cc972dfee89417c760eb845a0928cd99d3d7e4edf7d16ac5e43b5ea8425e1301"
+  url "https://github.com/Kotlin/kdoctor/archive/refs/tags/v0.0.6.tar.gz"
+  sha256 "5d4e7800f312991ab8546f563b469ce2d4cdeb2b41ef6f4e66ab36bd4bb754d7"
   license "Apache-2.0"
   head "https://github.com/Kotlin/kdoctor.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "665c11a1bc4fab8e432fd90fa4dd9c4febb2132a448de9e7ec7dcd8af97fb9c8"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "9f5f58367195b2e6615ecf2f5e058383762e1960672deee009f1f4e99152c08b"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "563f1aa5a78fca16dac048c7e1a14d54209cc25b80d3df8ebee7e49f0bc3423d"
-    sha256 cellar: :any_skip_relocation, ventura:        "ceb31cc77e28b09075e2c833c9f670a3270877376accdc7c71a7afdf0dc7ba12"
-    sha256 cellar: :any_skip_relocation, monterey:       "268c40758894f4acdc19a21586c4d1268ce5a407bfb291937fdf4dac3cf2a5ae"
-    sha256 cellar: :any_skip_relocation, big_sur:        "956ff7b276c22eb1828d049685858f0c3df23b287340ea04a052d54477dd74cb"
+    sha256 cellar: :any, arm64_ventura:  "095b49bdacbc9e40c7da081533f87e54aa7053cc897a2a7a16690d9edbf5726f"
+    sha256 cellar: :any, arm64_monterey: "e02293c5e22037d46c7aa5381f2f7ec7cc09f0de971e4b95100acaa3bcd4f8cc"
+    sha256 cellar: :any, arm64_big_sur:  "c5dd4c19bb59df578963e1b60b8bc427b66e9b6f0f49a249ffc0a2209e50960e"
+    sha256 cellar: :any, ventura:        "eed0101170752ca261c184ade26a6cc83db1d2839b603517602d2685ae406b51"
+    sha256 cellar: :any, monterey:       "eed0101170752ca261c184ade26a6cc83db1d2839b603517602d2685ae406b51"
+    sha256 cellar: :any, big_sur:        "f1a4c78c51bbce87f4e03ff0c63280e5850afda7008ebb9dcb919858b53103a4"
   end
 
   depends_on "gradle" => :build
   depends_on "openjdk" => :build
   depends_on xcode: ["12.5", :build]
+  depends_on "curl"
   depends_on :macos
 
   def install
