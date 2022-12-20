4e0e72dca112a83bcb3b9a9924b02de929b0c981
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:53:16 +0800

    qodem: no rebuilding required

--- Formula/qodem.rb
+++ Formula/qodem.rb
@@ -9,6 +9,7 @@ class Qodem < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "c88b2b10336d0562d9858744dac90f4f895efa643ae7441170a2c33fafd76c23"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "230b60c0f6dbd68eb6842acc073e21b0fb2bb5e4e47a8f37b2fd812980849c7f"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "0edd8a307de97c0844df4940c87ea88cfe68e5b41ca55e6c6d67f4b024b31477"
+    sha256 cellar: :any_skip_relocation, ventura:        "eb099f141c43635088e5069c8f519aa88100348db0172f1f54847de32a6c45b8"
     sha256 cellar: :any_skip_relocation, monterey:       "ded91d17b85bfa34b3cd3eb01a338f19ed91711a2c4fdea71d5543cc3953c2ab"
     sha256 cellar: :any_skip_relocation, big_sur:        "ac2537f733ed6952656aec3016302414b37166b64b2d89836f17354008276f73"
     sha256 cellar: :any_skip_relocation, catalina:       "14491121c60a5368cf41e4cab4df43bd918f31342f8aedf7e43241a3e49b22b7"
