8924200d437ec594ea6af1bbcdff10b61642dcc1
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:42:24 +0800

    libmd:

--- Formula/libmd.rb
+++ Formula/libmd.rb
@@ -10,7 +10,22 @@ class Libmd < Formula
     regex(/href=.*?libmd[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
-  depends_on :linux
+  bottle do
+    rebuild 1
+    sha256 cellar: :any,                 arm64_ventura:  "159d4661e6477b40c07ad7fd1b9832b8bfd1dfa0664a137b4ff7160c360e0c36"
+    sha256 cellar: :any,                 arm64_monterey: "176db8960cd4ac83cf0f5145c3b1f7560ac239c867a2b3b41127e5badd238a38"
+    sha256 cellar: :any,                 arm64_big_sur:  "4251c8ad6b6962dddba3270630943a53779af7eaf7b7963be34f5f3de7b14667"
+    sha256 cellar: :any,                 ventura:        "897b410369a68b9a2db954519ace1d396cac501669d1890534754cb6a7e6f77b"
+    sha256 cellar: :any,                 monterey:       "051da8fdcf3a6907760fd2983e9351d529a1a28d4f48ab32a942a232a9fe7fdf"
+    sha256 cellar: :any,                 big_sur:        "1aa4b3d7c3fe5ad30d0711d045ca94e05a6fed6f928cdf11ac80acfc1fb31928"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "10df9063da043dc8dc8a2b5b0018f2fc7f4f055fe9ee5d7ad2640c26472e877f"
+  end
+
+  # build patch, https://github.com/macports/macports-ports/blob/master/devel/libmd/files/patch-symbol-alias.diff
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/62ea945/libmd/patch-symbol-alias.diff"
+    sha256 "a9bb67cbc2243d12fe81b6c9f998dddbe2f58f11570749f98ee23b07d9a02d53"
+  end
 
   def install
     system "./configure", "--disable-dependency-tracking",
