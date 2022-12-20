f0c3e9b8ac49ea7060e854786036d8ecb27e46f2
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:44:16 +0800

    lpc21isp: no rebuilding required

--- Formula/lpc21isp.rb
+++ Formula/lpc21isp.rb
@@ -4,11 +4,13 @@ class Lpc21isp < Formula
   url "https://downloads.sourceforge.net/project/lpc21isp/lpc21isp/1.97/lpc21isp_197.tar.gz"
   version "1.97"
   sha256 "9f7d80382e4b70bfa4200233466f29f73a36fea7dc604e32f05b9aa69ef591dc"
+  license "LGPL-3.0-or-later"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "4ab5cc792a65bd498458aa93dc79e77e921c2cff17e1f17629915332b4e4134b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "df672a0be96a7703b2f4ff5db9c8c5b322de22822ea0fc6749becea3d3a0cc21"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b6f26711684bfbf3ddde4574aab70336278338b206a5fe3e278d868903bd8910"
+    sha256 cellar: :any_skip_relocation, ventura:        "5f30330abb1e75c57fdbfe0177bda58a25c64598a8af7d008d04cfcbc930d6f0"
     sha256 cellar: :any_skip_relocation, monterey:       "60c1dfa18f24845046fd13390aab73665d086859831c999a75bed2dbb37d902f"
     sha256 cellar: :any_skip_relocation, big_sur:        "47edc941f73249c62b66a4d795bcec7c2916082ab60f1cc1e1c0c46ebe99b694"
     sha256 cellar: :any_skip_relocation, catalina:       "e5231b41e3d08d835d4c3a457b594c60576c42802347c01555acc94c04067d94"
