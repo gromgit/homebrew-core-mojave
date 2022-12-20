b41cc43cbe9be8384f8d2b5199b96b1de4f359d3
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:33:30 +0800

    flint-checker: no rebuilding required

--- Formula/flint-checker.rb
+++ Formula/flint-checker.rb
@@ -9,6 +9,7 @@ class FlintChecker < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "d5056c257e0cc996faf15565e602c5e5a8fc7655a864f1bbf4fb655bb3599908"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c7a3874abb22d4ba7d1641f6fe8f8bff8262150f5031c335430b20156062165d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "beaa467d1e1290795f33302eee355ab5e32216602a82cbe8083d82d163e9aa50"
+    sha256 cellar: :any_skip_relocation, ventura:        "7560d772fafc17dd5037aa271009e8e18645cc6ad2afb57df36316cd042a1195"
     sha256 cellar: :any_skip_relocation, monterey:       "a173b1a8329d81cc39c220f535e84c940a3f4e3e8b7505d590fb7411571a6710"
     sha256 cellar: :any_skip_relocation, big_sur:        "167b3ab36ec6ddb5b07b4524d090596b18fd9267cf9da5161e78d11b1f988397"
     sha256 cellar: :any_skip_relocation, catalina:       "48211955f96e66b5254338d9f6ba56e6e35f6680fb0379190f5b4a3d8f6fe6f4"
