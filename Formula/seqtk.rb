0a528d3d97dde78bb23e91fcf86d68b3d4918046
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:55:39 +0800

    seqtk: no rebuilding required

--- Formula/seqtk.rb
+++ Formula/seqtk.rb
@@ -9,6 +9,7 @@ class Seqtk < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "e6cf3d61b32d87a06c5f10459779e4d8694cdac21a315654c447bc56ed3f4e00"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f31a109971c3906efdec050e77ae151e16f3c54410d3b9ee4b229839b610eb0d"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4e1d4c35dc64c5ea008ecb2e83bfc5d7a4047256e37923a981e5a6cd6038f368"
+    sha256 cellar: :any_skip_relocation, ventura:        "3afddde22ed6a5e2cb5c49389ea6894c758624f34ca09e64430a14385293085e"
     sha256 cellar: :any_skip_relocation, monterey:       "b9b943a7ee965c28916f2db8dd49e734022e74a5fce1e78af2fef017649c9b72"
     sha256 cellar: :any_skip_relocation, big_sur:        "bdcc9b85644f98bbcdffbb84816e40714c254f8e9e8c8e5c042b4d25496e0010"
     sha256 cellar: :any_skip_relocation, catalina:       "5abbf374f3dab69b198b98a3126f521b64baf01ac5ed69b99be91ffd97f891f8"
