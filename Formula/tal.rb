3927fd96a8e0348ed86b1eaa5a4a9f155a271aac
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:58:24 +0800

    tal: no rebuilding required

--- Formula/tal.rb
+++ Formula/tal.rb
@@ -5,11 +5,13 @@ class Tal < Formula
   url "https://www.mirrorservice.org/sites/download.salixos.org/x86_64/extra-14.2/source/misc/tal/tal-1.9.tar.gz"
   mirror "https://web.archive.org/web/20160406172703/https://thomasjensen.com/software/tal/tal-1.9.tar.gz"
   sha256 "5d450cee7162c6939811bca945eb475e771efe5bd6a08b520661d91a6165bb4c"
+  license :public_domain
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "717e28a9ea9f325fd72962d66372a1e4edc3b27e81ea54a861f8c54dceea0a56"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "94237b7f329c30c17aed19d82bb18a790abf2abcbfc404a57ef9e237db4a0735"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "7ca62e367e9b35b079d0419bc02c4baec570154a089337f4ffce87441e9ba7af"
+    sha256 cellar: :any_skip_relocation, ventura:        "d586b3fdc04b4b8b8379249958cae1be5f1d15417b50aa8699a7c63ab99598c2"
     sha256 cellar: :any_skip_relocation, monterey:       "b869e27ee13cb3e2ec83b0bb24a3dfc17b753ae94afa8e11d5e75943e8f1c132"
     sha256 cellar: :any_skip_relocation, big_sur:        "ff2e907039f88888af6724c384e32b3191fd39097266bbdcf415faa3b9cc927f"
     sha256 cellar: :any_skip_relocation, catalina:       "1d2978734ee3f0c63efdd0acdff401014954c34ed709ed397348dc1f3f973b88"
