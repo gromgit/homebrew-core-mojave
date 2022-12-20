fa311bdc44f7f4e974a3daf3991d4c443a0aad3f
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:49:31 +0800

    orc-tools: no rebuilding required

--- Formula/orc-tools.rb
+++ Formula/orc-tools.rb
@@ -1,8 +1,8 @@
 class OrcTools < Formula
   desc "ORC java command-line tools and utilities"
   homepage "https://orc.apache.org/"
-  url "https://search.maven.org/remotecontent?filepath=org/apache/orc/orc-tools/1.8.0/orc-tools-1.8.0-uber.jar"
-  sha256 "4f320ae642071f2514cfc996f6a8e9b8f06272822eef141e7b063e5636194dcb"
+  url "https://search.maven.org/remotecontent?filepath=org/apache/orc/orc-tools/1.8.1/orc-tools-1.8.1-uber.jar"
+  sha256 "5014773914c975145003b01b3231ded6cf7ecf070def6d66156df882d035f7ca"
   license "Apache-2.0"
 
   livecheck do
@@ -11,7 +11,7 @@ class OrcTools < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "018b34e4c2ba5e0df099200574d9f27db8f433ee5fe490397a15254ec9a4a58c"
+    sha256 cellar: :any_skip_relocation, all: "8cc905935e60d9fcb68dae57779a167c938dce8298fb20888df4f39d4c4c44b7"
   end
 
   depends_on "openjdk"
