4ee025fdc3a7c861331df83d807de23bf89fbe96
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 19:00:00 +0800

    two-lame: no rebuilding required

--- Formula/two-lame.rb
+++ Formula/two-lame.rb
@@ -9,6 +9,7 @@ class TwoLame < Formula
     sha256 cellar: :any,                 arm64_ventura:  "7ea2224ac3c69dcbbc8a2bced6b9bd1b478effea5ded33806420e70e51f396dd"
     sha256 cellar: :any,                 arm64_monterey: "3b49ddbaac0612e42ffcfa8cda457d5602063d645f124da8c6353f15abc5c9c5"
     sha256 cellar: :any,                 arm64_big_sur:  "15f7868c873400af4b2f9e566356bfa00217f563d51511120367c4dc75d2867f"
+    sha256 cellar: :any,                 ventura:        "5d52f911e18dfde2ae188d42b7f57033c4b246771da3faebd7b276962f6e88e4"
     sha256 cellar: :any,                 monterey:       "5f40da46c678cf30b76918f6ba6d38a80425fe52b6fd06c290608de175b3cfad"
     sha256 cellar: :any,                 big_sur:        "4847a0b0e48b6f8ac14113793e18a6b31d8dce22b09dc707c35306ec2b3ecdfa"
     sha256 cellar: :any,                 catalina:       "9ba9f3afb14f2ac2fa911046a83ee50ed6a93d747f0c305788a61b4138e5fe5a"
