a25574582e866b7d09d0a67c23ddcbf704332e31
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:24:10 +0800

    apng2gif: no rebuilding required

--- Formula/apng2gif.rb
+++ Formula/apng2gif.rb
@@ -9,6 +9,7 @@ class Apng2gif < Formula
     sha256 cellar: :any,                 arm64_ventura:  "96885dd78971006a1a9c2110cf491436d16cb705fd29c67a8506c06c41cf867c"
     sha256 cellar: :any,                 arm64_monterey: "06feb5f45bd0926a23bcda94caf5af7eb1f29166b71853ea3c9136c74c63d000"
     sha256 cellar: :any,                 arm64_big_sur:  "b11d6a2f6d1eba7587c8541f65440b027596ae8b3b60ea23e080a237d0b215a3"
+    sha256 cellar: :any,                 ventura:        "f67f54a6e9f79ca11e7d1c0f5b9d4fbbf43ed6f4b0045875287ad4dafb5df70d"
     sha256 cellar: :any,                 monterey:       "58b5118280140555684d30a9682450f95ddcbbb7f2f03d6a354da850b3f432db"
     sha256 cellar: :any,                 big_sur:        "8c541ad0b322c10bacc60230d91daf242f3b7ebb8e5deb72860fe2dc1b8cb551"
     sha256 cellar: :any,                 catalina:       "e602a9876003067007cdd579101e1fafa937e7a2ca328a0406e872d6be4f5705"
