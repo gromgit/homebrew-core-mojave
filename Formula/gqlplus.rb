19e76898eac326f95b7909a6b4e95457a2e4bd30
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:36:24 +0800

    gqlplus: no rebuilding required

--- Formula/gqlplus.rb
+++ Formula/gqlplus.rb
@@ -9,6 +9,7 @@ class Gqlplus < Formula
     sha256 cellar: :any,                 arm64_ventura:  "a0306aa2fa07da51f02dec57525850ec4fe73e37d044fc34cb0b722cb83d1310"
     sha256 cellar: :any,                 arm64_monterey: "ced72667294e921a6625eff47bdb23df67595692f8f5b4d1ed253eefc4b708ac"
     sha256 cellar: :any,                 arm64_big_sur:  "e93b52a5967e87f0ccc282ad897c4d64b42e0bcf4e41f7ece17186f1fd36bcd6"
+    sha256 cellar: :any,                 ventura:        "7d53a459847266fa2e56d0c5a2ccaa5101106988b5d95448a987b4e8cff6ecc3"
     sha256 cellar: :any,                 monterey:       "9af9ffcfb971028cd45c552c13624d26aa5abd0a34df1bba31504070879b474b"
     sha256 cellar: :any,                 big_sur:        "6b4b7972c9c29e749bb0546aa83f756e967aca10793ff70de9bb1711536d929a"
     sha256 cellar: :any,                 catalina:       "2ffb1031a83fe666dc574d17d72b08781dd08e48f1dba88c5c67550472f819df"
