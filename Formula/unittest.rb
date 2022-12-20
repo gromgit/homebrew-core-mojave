1ccbbdc028386ade19ac18a2a2b91244b9c4879b
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 19:00:26 +0800

    unittest: no rebuilding required

--- Formula/unittest.rb
+++ Formula/unittest.rb
@@ -13,6 +13,7 @@ class Unittest < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "4c301be0c1a8cabb9339ce5f1a2284c6c9d7c54a3f9458f92563d388ec418f0b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "dc9de7f5271e4a57ce5f2bd565924006f39ebb8f136ba5f4493778007c019ff5"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2da59f3f0206902816c2dac6c273768858f092eb917b696b0a8b04096ea97007"
+    sha256 cellar: :any_skip_relocation, ventura:        "a6600c783f2a191f11bb0f4b8f236819216ee20e2d4610bcc006a4478a706ded"
     sha256 cellar: :any_skip_relocation, monterey:       "d83feaeec071b449126875572d29d4df64580c06c2f4a8fcc4fca55a5c68c7f4"
     sha256 cellar: :any_skip_relocation, big_sur:        "8f449bf2ba73aaf03dd8316d6057639bd2c3a38ef347157f3721cbabfb60212f"
     sha256 cellar: :any_skip_relocation, catalina:       "ef8f5c6e18c32b813cb825ce467a6997592dca4762833f8e03f156629ffa74f9"
