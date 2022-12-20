98788710c765e55c407362fb8f0dd81aa78d78f8
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:41:52 +0800

    libfishsound: no rebuilding required

--- Formula/libfishsound.rb
+++ Formula/libfishsound.rb
@@ -16,6 +16,7 @@ class Libfishsound < Formula
     sha256 cellar: :any,                 arm64_ventura:  "adb772f247d86852efce4345ef9a4d7496648412c5233e6417476256b531ddef"
     sha256 cellar: :any,                 arm64_monterey: "04042bd85b176dc50f99153f267fae3e5f82176ad010aaccff0c71d1434ab550"
     sha256 cellar: :any,                 arm64_big_sur:  "3ec17aed1c22c99831e01e1938bf9b240439f45c130422dd90e06ccd8a57cd74"
+    sha256 cellar: :any,                 ventura:        "05f682b7c9612e5a31bb0cef75d324aa20ade7c5cf205a3999d973a3f7fb80bd"
     sha256 cellar: :any,                 monterey:       "7c299a38462e967259b6e396193585bf37c2a0d5bbce0e4dbbff32b15e9ed102"
     sha256 cellar: :any,                 big_sur:        "a1ae8b29698509de3de412402ce463cf32a08573348526dc42020731fdaff314"
     sha256 cellar: :any,                 catalina:       "5599c6eaed21c2f66ebb8209ca8e436fd306214de6d9db6ccf21bd9c2710e1b7"
