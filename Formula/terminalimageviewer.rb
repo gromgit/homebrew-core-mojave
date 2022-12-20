5011188ead60c19652fc0e62277d55fd4c49b4b6
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:58:51 +0800

    terminalimageviewer:

--- Formula/terminalimageviewer.rb
+++ Formula/terminalimageviewer.rb
@@ -11,6 +11,7 @@ class Terminalimageviewer < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "c81b92d798635c6fdae0106ed4a1633c70985e93f80e8dee1506e53b214ce29d"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "7f6aeba5ada7ff92bc303930dfc3fba0d89cc3d6a06a0ce12db97856e2b4dde8"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1cfe13918f86cb72157a042bc009874829d4cf59ab9426ab3bdda4614421d22c"
+    sha256 cellar: :any_skip_relocation, ventura:        "1743e0ace153f89e0a9e7e4fc05c17ddb0cca6f8c2c2c20aa4309ee1966cb06b"
     sha256 cellar: :any_skip_relocation, monterey:       "9197c1aa0d01e3bafaa8500ea96b56c0b7fba1f0b20d2fede4b8d30df2d10b9a"
     sha256 cellar: :any_skip_relocation, big_sur:        "dbec90a4aa92c309150e28ba05bb18ba992f4904e590c97b43dc5a8273917aa6"
     sha256 cellar: :any_skip_relocation, catalina:       "87321c04895355910aa3fdeb3ddd7ee0bb57360dc4e2ca833757c0143a730632"
