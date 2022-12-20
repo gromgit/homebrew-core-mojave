8c1996f369076d40bc6949bc2cf60054e2af531f
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 19:00:05 +0800

    typedb: no rebuilding required

--- Formula/typedb.rb
+++ Formula/typedb.rb
@@ -1,20 +1,25 @@
 class Typedb < Formula
-  desc "Distributed hyper-relational database for knowledge engineering"
+  desc "Strongly-typed database with a rich and logical type system"
   homepage "https://vaticle.com/"
-  url "https://github.com/vaticle/typedb/releases/download/2.14.1/typedb-all-mac-2.14.1.zip"
-  sha256 "48ed0385b29061774c3f577aecdf4ec5f3980c0bb891cd6d4c4928cf65eb95ce"
+  url "https://github.com/vaticle/typedb/releases/download/2.14.3/typedb-all-mac-2.14.3.zip"
+  sha256 "41a574d4d0fafcdfd678599b488dfb3aa7e2c4664e111dbd479bdf0a4dbd12a7"
   license "AGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "2f7191a6a31731d8f7f9d471352a44232eba34e24885ab63bc2804426c022038"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "c166911dd3db89b53b24cc74b839ee7f9f53e1cac04d7c57071ca218fc5ecb1d"
   end
 
-  depends_on "openjdk@11"
+  depends_on "openjdk"
 
   def install
     libexec.install Dir["*"]
+    mkdir_p var/"typedb/data"
+    inreplace libexec/"server/conf/config.yml", "server/data", var/"typedb/data"
+    mkdir_p var/"log/typedb"
+    inreplace libexec/"server/conf/config.yml", "server/logs", var/"typedb/logs"
     bin.install libexec/"typedb"
-    bin.env_script_all_files(libexec, Language::Java.java_home_env("11"))
+    bin.env_script_all_files(libexec, Language::Java.java_home_env)
   end
 
   test do
