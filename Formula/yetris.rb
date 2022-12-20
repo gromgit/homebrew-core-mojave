a3bbf74d5886a26d5534f87a415c02236cc8b594
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 19:03:28 +0800

    yetris: no rebuilding required

--- Formula/yetris.rb
+++ Formula/yetris.rb
@@ -9,6 +9,7 @@ class Yetris < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "bde6f3b8d260bc2b35e850b59223578400c2fda5e97e1ef4b425eb446f9b68b3"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "0aa127e1a907e08cf4b65d83fe0de8c59785457f744ecc2c1e91fd37310037b5"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bcaafa1c4c02615a805d252ce93cf8c38a60876b575867cc280795a00a1f2848"
+    sha256 cellar: :any_skip_relocation, ventura:        "c1d76e812f2a403dd078b3a0b075b48a21721c629b94b4be83874147b3f4b787"
     sha256 cellar: :any_skip_relocation, monterey:       "78e274470e8eb080f6d8c7d0051f4e7f0ee7f7969c88c725a114b39b7f926778"
     sha256 cellar: :any_skip_relocation, big_sur:        "d0d9c0ddd6f7f825024cb4e96978ad43919eb77a216e8788943f1c8d7bfa80bb"
     sha256 cellar: :any_skip_relocation, catalina:       "a43b346adc20fc7d4f84ec1300e839bb4e615ab40ccf8e1a591f099092ad6078"
