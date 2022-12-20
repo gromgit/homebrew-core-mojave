5ee4f13a9808f0e0c56c568a7ec73d3ca7669b9f
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 19:03:57 +0800

    zsxd: no rebuilding required

--- Formula/zsxd.rb
+++ Formula/zsxd.rb
@@ -10,6 +10,7 @@ class Zsxd < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "d3210e812054b1162800685d3a548822065dbe07053515f4e1185fe8a6c5a7fe"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ee42ed381fd488a0e538e30eeff539346f71c0822fcc899907792fc4acc5988a"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4092f599ecb1631608fe3a5109006480d6797bee0ec8b4cd4878b451f4400720"
+    sha256 cellar: :any_skip_relocation, ventura:        "f79284e528dd5c8c412dfd90f96fc8d3d6c54ca839a8dd71fd7fb689c8ae7951"
     sha256 cellar: :any_skip_relocation, monterey:       "5641638453928b8d7dc434051961417e5c3e768c06fc35f9879737640027ece3"
     sha256 cellar: :any_skip_relocation, big_sur:        "ccae47d22a42e29f9d5d37fdd0be7cfa8f451d3b0f42d2db2b933f6a7ae1d129"
     sha256 cellar: :any_skip_relocation, catalina:       "aabcc393aae8f00a45ffa24d959ff57a6023caace90a815f8107c579e113b87e"
