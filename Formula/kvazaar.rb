64bc3f288558995706f5fb116f882768ae2aa8e8
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:40:51 +0800

    kvazaar: no rebuilding required

--- Formula/kvazaar.rb
+++ Formula/kvazaar.rb
@@ -15,6 +15,7 @@ class Kvazaar < Formula
     sha256 cellar: :any,                 arm64_ventura:  "82bad24051a2759adb516d79d2b35ceb2b5f920f81c5b7ec60095a1d8e8c38fe"
     sha256 cellar: :any,                 arm64_monterey: "d84d0ad2462ff6c0fbe5ea7d2530c17d8d22629081fde5756fec48cd8ac5c409"
     sha256 cellar: :any,                 arm64_big_sur:  "c2f8e51a5222f0b4f114df45c9a6439b84134c282a4147a3a8f5bd57aa297f14"
+    sha256 cellar: :any,                 ventura:        "6cf0bc3065d6aae934809aa08a1b8aa3993014c42ecc12d41aa0c8ab601723b5"
     sha256 cellar: :any,                 monterey:       "2e8db48a773c5754ec93786ae52d7bb09c94cb40fa811d0b12457540fcc378c5"
     sha256 cellar: :any,                 big_sur:        "68ec96b70d3098071e315f09d9b68d69971dd362d586e69f1c3d6938edc760f5"
     sha256 cellar: :any,                 catalina:       "09205d5d2e16355e031d467315842030923f4459fed71576889f372f895f3b98"
