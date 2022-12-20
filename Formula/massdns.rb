7c994db936141dd992ad3b118078509f848d0034
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:45:04 +0800

    massdns: no rebuilding required

--- Formula/massdns.rb
+++ Formula/massdns.rb
@@ -9,6 +9,7 @@ class Massdns < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "36501029e41fca6b8b0d302a3e3be12d798f19d7b20cf245ae0a3d0906bcd7b5"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "35a82870db0e5f349bacf0d2ef2f596901c75e42ef40db916c7bd37471d8caa1"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4a07f6b26f6625d833864cbfcfea075eace6957ac9a01d99fbb85874f95e995f"
+    sha256 cellar: :any_skip_relocation, ventura:        "7be8f4b34e6a0194f50f97146ad80d503eca4ae6fda5d90343a977d24580ccd6"
     sha256 cellar: :any_skip_relocation, monterey:       "7365e79331e01ee782e86a393532f2e4a2c3c61b0ddd52dc663b1085814e1435"
     sha256 cellar: :any_skip_relocation, big_sur:        "6cf600e96f9f6e9e693b17894f59e8a14f5cddb2d9690719bbd8553b39b81a0b"
     sha256 cellar: :any_skip_relocation, catalina:       "d24888b27f7a3d0cc3d235e62094d985c378adad90b57939bccf96a14823803c"
