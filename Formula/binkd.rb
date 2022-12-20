77144d4f0b5ba7fb367225c68a9e5b72384e38df
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:25:46 +0800

    binkd: no rebuilding required

--- Formula/binkd.rb
+++ Formula/binkd.rb
@@ -10,6 +10,7 @@ class Binkd < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "b2df2fde26d69ad428bcc89b4108fee501ebf175a068db94ea97366212d66e7c"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "ea0018141c5ebc35b7dca7fb49322682120a2eb5868eb16ff40c8f670123a3fc"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "4cd8c44619d3c04b646a1ea8ad7db21feb95e950656c78d5dc1a983d6472b469"
+    sha256 cellar: :any_skip_relocation, ventura:        "5ffb321d48d1c22395d4c0453ebd2794a12ab267eb67117b17aa66004e0f5175"
     sha256 cellar: :any_skip_relocation, monterey:       "111b63381a81029c8e2b1618e34d7f6def295e9abd5f6b4c2073b636650ad026"
     sha256 cellar: :any_skip_relocation, big_sur:        "3b309f2c4a6ee43441a32c0396a570454737bb85c5467f3e27a8c666140bda59"
     sha256 cellar: :any_skip_relocation, catalina:       "263219b174c193cd86454770d838013daa797994d8d21cdca45493a59d8a5a79"
