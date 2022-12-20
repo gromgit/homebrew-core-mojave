4c6f4fef3a5ee45f1e16169bf562b98189d12482
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:49:26 +0800

    opentracing-cpp: no rebuilding required

--- Formula/opentracing-cpp.rb
+++ Formula/opentracing-cpp.rb
@@ -9,6 +9,7 @@ class OpentracingCpp < Formula
     sha256 cellar: :any,                 arm64_ventura:  "ccbb5e255e5f6708324c7d8853e5d9e16e6409d061d465628fa876f0371454bc"
     sha256 cellar: :any,                 arm64_monterey: "50677ac8a2082e88324b45764c2f2d46dd70df23261c13e4e4fdfe4577505635"
     sha256 cellar: :any,                 arm64_big_sur:  "47fd29e6c0a73f405ac424f95299aa1d68a2735061cb6b96b262eea6a935210e"
+    sha256 cellar: :any,                 ventura:        "41295146fee1dafe218d97dd70061260ffa49bdee9ddc7c23bd465f97a4abb74"
     sha256 cellar: :any,                 monterey:       "a84c5a3c3fe04de1cc52d24381af68f5d054a353bee01fb10fb1abe6c4cbd0c9"
     sha256 cellar: :any,                 big_sur:        "1a904785b31fe03fc39333e81dc06e815b649c92062e23a99cf24137a013227b"
     sha256 cellar: :any,                 catalina:       "151a5af54448492f668979eb3a0e9fb92e2e1a99cb6766ba3985a9a88f26526a"
