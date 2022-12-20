a09f3b48264589af8d22564b73e33bf50edcf224
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 19:02:59 +0800

    xmlformat: no rebuilding required

--- Formula/xmlformat.rb
+++ Formula/xmlformat.rb
@@ -3,6 +3,7 @@ class Xmlformat < Formula
   homepage "http://www.kitebird.com/software/xmlformat/"
   url "http://www.kitebird.com/software/xmlformat/xmlformat-1.04.tar.gz"
   sha256 "71a70397e44760d67645007ad85fea99736f4b6f8679067a3b5f010589fd8fef"
+  license "BSD-3-Clause"
 
   livecheck do
     url :homepage
