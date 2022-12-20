96755eea44d61d45d5f27a8f9046214cc7677a5d
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:53:44 +0800

    rancid: no rebuilding required

--- Formula/rancid.rb
+++ Formula/rancid.rb
@@ -14,6 +14,7 @@ class Rancid < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "ad23b3238d8cdef43a40e060103a58416ffb5043abd8fb94747d467357b8cdd2"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "d22337b82b06224e0c44739c33fbc91b938168533acccde6d6c4293e6fd1e4a1"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "b815068fba2453ad568c0406b1f8bd1b1dfe6c69891ac1301a57b01934141132"
+    sha256 cellar: :any_skip_relocation, ventura:        "1699bebe63224a888ff084d1099654edb6dd9b8af6f278784bcb82a6cf201588"
     sha256 cellar: :any_skip_relocation, monterey:       "a4aeea195843750c9991c57427c79d0d0d61da7d8c0f4b7e64a7539305b18662"
     sha256 cellar: :any_skip_relocation, big_sur:        "3c22c8b4feebcaf1b03f4feb919c352d2b449aab6341b1fe81164fa771240826"
     sha256 cellar: :any_skip_relocation, catalina:       "6840b7e2cb719007f53317491e8fe88a56820c121d52ff2bda4403bbcd0ea151"
