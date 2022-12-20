7246af909390d71d90be1e4b8795905ad288f0bf
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:24:26 +0800

    arp-sk: no rebuilding required

--- Formula/arp-sk.rb
+++ Formula/arp-sk.rb
@@ -10,6 +10,7 @@ class ArpSk < Formula
     sha256 cellar: :any,                 arm64_ventura:  "c910b1eeb3587b770b5a4c77904a7e5ad35824740762571a93fdef18175c0c39"
     sha256 cellar: :any,                 arm64_monterey: "814f89b6e1bfcf86c29eefef47ccc5077c8d38efd4626cccc029363097048328"
     sha256 cellar: :any,                 arm64_big_sur:  "e9a3123cc035debcdac3582b5aa868cf8ab2f64d10c2ddac6e41df4df0121d52"
+    sha256 cellar: :any,                 ventura:        "cc3c9357bd9440f49aa61f7483fe31561a035e544416727ee785bcef94014022"
     sha256 cellar: :any,                 monterey:       "43b6e66bf25c5be9893862c174e4a1aaaf3928f38bc68c25d0177026d3923a4f"
     sha256 cellar: :any,                 big_sur:        "206b69b4456fabe2614dbf5c5ab2886530d2b238f18adb28545a9758fc9a4561"
     sha256 cellar: :any,                 catalina:       "bc28c6d58a3838fac59ab625ab26a917b3b0282ac54a8f37a95034efd0740007"
