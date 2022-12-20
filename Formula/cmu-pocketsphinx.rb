234903d0d5cfa4ea61d61ebee8703b89fb9b1280
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:28:17 +0800

    cmu-pocketsphinx: no rebuilding required

--- Formula/cmu-pocketsphinx.rb
+++ Formula/cmu-pocketsphinx.rb
@@ -23,6 +23,7 @@ class CmuPocketsphinx < Formula
     sha256 arm64_ventura:  "cb3db8f8860e169f7bcdb427935fccdf2b9cddb1c3c1891d296a098664f4ba42"
     sha256 arm64_monterey: "314f80412a27862cdb5b857b3ae8273e28e29f81dcb4279d60228b149df9b97f"
     sha256 arm64_big_sur:  "4c3fc6b03df2530653a4ff38b270e9a37dd968748b38a011cae6500ab38d8084"
+    sha256 ventura:        "b3ce7baee259f699334d2a00e412c99ee1ee582f104875bc747e372933b19625"
     sha256 monterey:       "263434383a2856a30d3119a4683a7c61a52521f08b8ee7bd08f02d8b1efa2de7"
     sha256 big_sur:        "25ce90366420de438c34e8f037f1da3c7ded680375fcc69127cdec7695f84fed"
     sha256 catalina:       "f2fc23a67634f26befdd128d21e886d7b3789484a14d498a40949e0e100d8afa"
