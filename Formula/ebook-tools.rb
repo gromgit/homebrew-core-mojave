d056b9302e755f9dc41de166bdf8097b5ff579af
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:31:44 +0800

    ebook-tools: no rebuilding required

--- Formula/ebook-tools.rb
+++ Formula/ebook-tools.rb
@@ -10,6 +10,7 @@ class EbookTools < Formula
     sha256 cellar: :any,                 arm64_ventura:  "019f7789541693a154f71bb507db24ab2cd3901f539e08e2ce2e51f53aae48da"
     sha256 cellar: :any,                 arm64_monterey: "ea1b7dcf30a98ec82dbe77c369168185565450ee4c4af8e4d77f9ae2ffaa360d"
     sha256 cellar: :any,                 arm64_big_sur:  "22676305647bc9cad4335aba2d28d27cbee0db6092901cf1682fff9c833c92bd"
+    sha256 cellar: :any,                 ventura:        "2bd78f9654202568c6d95821ca7d5f04a83859748a818b1234528993c5ba72e4"
     sha256 cellar: :any,                 monterey:       "64d14b86d0aa4270c9f918b1e8539100f6c2eb495ccc33a16e7bb7a322790328"
     sha256 cellar: :any,                 big_sur:        "e9c49bae08503eaf6e213454bd4f5ce58ead342ef192798c6d7d9c04fb6c2918"
     sha256 cellar: :any,                 catalina:       "65d014f4c91fec7b0d156a751b1e3b409574f3606264f8ae9ccab0a1db0f564f"
