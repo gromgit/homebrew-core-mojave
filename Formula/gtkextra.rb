114ea007eab7b021f86642fb3e28367dc9148785
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:37:02 +0800

    gtkextra: no rebuilding required

--- Formula/gtkextra.rb
+++ Formula/gtkextra.rb
@@ -10,6 +10,7 @@ class Gtkextra < Formula
     sha256 cellar: :any,                 arm64_ventura:  "c18b82670dfbf23a56678c5246745f082d19647089c14b46c35456111ce154c3"
     sha256 cellar: :any,                 arm64_monterey: "8fdb88a373d7427090b1fecf13971afe93314c4b6fec59dea719c5e97f25786c"
     sha256 cellar: :any,                 arm64_big_sur:  "841d46dfdaee00be8a853e8069db2b8ca1fbbfcaf298360411b6f9c0a0706da6"
+    sha256 cellar: :any,                 ventura:        "46904ec2a184a12c24903e832d611096a611117c422f3931a40309c0f9bc40f4"
     sha256 cellar: :any,                 monterey:       "edc44c31e40d7a41a5b9d9f5074732f39896ba827f61e608c4c86c50c20fe841"
     sha256 cellar: :any,                 big_sur:        "3c35df2372587b0cc5bde265a9ff06774ec70651ac5aa103639dc41e669ae3b7"
     sha256 cellar: :any,                 catalina:       "17ba389425eea1e26e308f07b94a3f8637645e83a7b8314681f2285e09996d9b"
