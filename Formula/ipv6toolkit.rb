e307b62382435cbbdef6a930f657d1856f8e29fa
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:39:11 +0800

    ipv6toolkit: no rebuilding required

--- Formula/ipv6toolkit.rb
+++ Formula/ipv6toolkit.rb
@@ -16,6 +16,7 @@ class Ipv6toolkit < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "a1b2e4c72b667645eecf35a6fd6f5a155833a859d0871f8774d1c507cdac5eb1"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "79ca05285bd1af83adba5abc616a2b991fd65c0255466745b0b3a6815d49b0d2"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ee329b82ef00e47b858fe38adc0e0320635c66847c3986dc2e1727fa529173af"
+    sha256 cellar: :any_skip_relocation, ventura:        "9a075376fd95cbbbce662b52d4cb90a67c1a7970fe53cd48a730b9553e431745"
     sha256 cellar: :any_skip_relocation, monterey:       "63e06e58b85f10ca935dbfd52860ef330d734ec7c29425aaa8f33a05037c2979"
     sha256 cellar: :any_skip_relocation, big_sur:        "7ccda456d8eb276a1a462bc06e63167984e5c1a45f58ba453063c5a22b5b31bd"
     sha256 cellar: :any_skip_relocation, catalina:       "6ab4963d7d80f42fb444fabe02122f0290842cffd620a38e15060ed1c1b120ef"
