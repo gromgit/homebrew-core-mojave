8a1e6a9bf6336224a1bffdd266ce51439e76700a
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 19:02:39 +0800

    wtf: no rebuilding required

--- Formula/wtf.rb
+++ Formula/wtf.rb
@@ -1,8 +1,8 @@
 class Wtf < Formula
   desc "Translate common Internet acronyms"
   homepage "https://sourceforge.net/projects/bsdwtf/"
-  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20220926.tar.gz"
-  sha256 "34cd594b8e8393dd7b3d223a45622652373a3fc4fc00d98d32cc3a57f7f3f294"
+  url "https://downloads.sourceforge.net/project/bsdwtf/wtf-20221208.tar.gz"
+  sha256 "c43c1febb33b8b8f59e479c3f66f10662ee52481cf36c47d95c0496fc95d526a"
   license :public_domain
 
   livecheck do
@@ -11,7 +11,7 @@ class Wtf < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "1851d703156f4b3def4794157dc1afd2baf1a9ec4545cd2c9d061ee751225e19"
+    sha256 cellar: :any_skip_relocation, all: "a5c460724fb4c1cc1199eae328a819a2b359f5e1b69d768daf7fd1393780ba0a"
   end
 
   def install
