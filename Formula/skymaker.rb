8febec169fc6cd8214bd10703620919431db7003
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:56:23 +0800

    skymaker: no rebuilding required

--- Formula/skymaker.rb
+++ Formula/skymaker.rb
@@ -11,6 +11,7 @@ class Skymaker < Formula
     sha256 cellar: :any,                 arm64_ventura:  "27e46c46cf30f77353d447b24629b1d2824c70369fdcd08307be2e0984a0fb3e"
     sha256 cellar: :any,                 arm64_monterey: "41786c90ee63b22f0d1a582dc7b42ed9aa9a749e4be35772169805c6b7804510"
     sha256 cellar: :any,                 arm64_big_sur:  "44e3eadc4f4a2984ffff332f93e9aa7a383b7a395ccd9fd6714a237a48ed72d0"
+    sha256 cellar: :any,                 ventura:        "b535c7f706a5139cde6cbbb0893009287cc2bf7062474f90de68a557ba3a4397"
     sha256 cellar: :any,                 monterey:       "9c6b33e5127676aaec7d1d8c0232fb78be8a0bb8a5ee4fc429e0516b45e0846c"
     sha256 cellar: :any,                 big_sur:        "2262e9a1f11f41c75fc8ccb47717f779bbb62bf86ddbabd42975fe684278efb9"
     sha256 cellar: :any,                 catalina:       "af78e7af9c84517e8f7db071ef3718a34eafc39d6eac3357d77ee183d4fe2cdf"
