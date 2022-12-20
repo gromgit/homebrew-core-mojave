bd746f990c01024885df2480163ed33e70f54805
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:58:14 +0800

    swiftplantuml:

--- Formula/swiftplantuml.rb
+++ Formula/swiftplantuml.rb
@@ -9,6 +9,7 @@ class Swiftplantuml < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "41bb594b21a0a27497bee8de2a0f18e0dc5f416846f8ddfffad369edf0df60b5"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "35e239464c15500b77d934892b6d1848637581a51240dd43326eab217b652183"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "1e4fe2418e139ca0d1cda671c5acd7572d18890cb78d73139e89dbac7c553b33"
+    sha256 cellar: :any_skip_relocation, ventura:        "21a89201d53ca6819dde849d49d4da79de254a2a8f67a330aff6ad5f0ede0d8b"
     sha256 cellar: :any_skip_relocation, monterey:       "06ce1ef56e536dd11c773e730f90796e4da8be41b3844aabb4f6ba30d34dddd9"
     sha256 cellar: :any_skip_relocation, big_sur:        "c456ca66dd30f08214b084af6985724ac2bea855d48988890d67959ba8a8d6d5"
     sha256 cellar: :any_skip_relocation, catalina:       "143f92e5793600648b794dd20f62cfdb2601ac5607d17bd8ddc8b9ddcaa8072c"
