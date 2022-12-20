bc743ab244c13270ef1e2bbfd307b314d47cfbcd
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:38:35 +0800

    id3tool: no rebuilding required

--- Formula/id3tool.rb
+++ Formula/id3tool.rb
@@ -14,6 +14,7 @@ class Id3tool < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "bd6e6e24689e48d6ec5c67fef4ab113b40d92b91a02d85a8d67744ad454e820e"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "eaab957f04597caeef0f4d080d4e936743d154c3c0c2e62e228678f5d5123b58"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "eec5850fee5d290bad13de2cb4456b2e600560632be7b86309ac5c3f3f03697f"
+    sha256 cellar: :any_skip_relocation, ventura:        "cef8ba04668ad9e85b60642b34da3c81c2cb9bcd6a509ed28cf50a99bd822699"
     sha256 cellar: :any_skip_relocation, monterey:       "3703e5cde176d1f1855da2b8091570378d77f64d7870222d5d6cf149d702cb74"
     sha256 cellar: :any_skip_relocation, big_sur:        "7a6a25738f1961b7ebb929fe0e4746a60ef71fd28af06ea305c5f5d95ef10e60"
     sha256 cellar: :any_skip_relocation, catalina:       "ec3431bcd97a7852c292f72d45de19ad742a2e18bc8d6830ce5fc6e2351a8d29"
