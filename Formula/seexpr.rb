00f5b25934dd3aa49dc62f052f7f1af16b24f425
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:55:33 +0800

    seexpr: no rebuilding required

--- Formula/seexpr.rb
+++ Formula/seexpr.rb
@@ -14,6 +14,7 @@ class Seexpr < Formula
     sha256 cellar: :any,                 arm64_ventura:  "6e190a9b5d3db5beb483db33d39c1b7446269d6e088a9aef7b0bad7e77d8c1e0"
     sha256 cellar: :any,                 arm64_monterey: "d525a96b58f896e76192699ed10018bc992287604d1902c7c8614ebc508b7fe7"
     sha256 cellar: :any,                 arm64_big_sur:  "722581c38cd9860d5dc9b53f85b9560f3c1039b49a17180114c6f2370556fc86"
+    sha256 cellar: :any,                 ventura:        "00301575495e4c6ff4493576f95647e070258e87630a20fd1d61baf98c5d4c51"
     sha256 cellar: :any,                 monterey:       "45a50904ddbd4940674d0c844878effb7f7175af28c37feabb0f6ee52509e48d"
     sha256 cellar: :any,                 big_sur:        "8045ec68c468b5db8a118006756e34c54425bbcc3e29306c16a52e86ccefdbb7"
     sha256 cellar: :any,                 catalina:       "2a55400ad86255b300843f7cde1dbed4130d0ba26ffc4c8725fec83b50e7f9e3"
