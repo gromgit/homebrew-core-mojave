8e396c16ef587277ccf7d2833a1902f3290aa407
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:31:30 +0800

    dvd-vr: no rebuilding required

--- Formula/dvd-vr.rb
+++ Formula/dvd-vr.rb
@@ -14,6 +14,7 @@ class DvdVr < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f134891dbbb4f74aa606a2f560ae6992764387c39e8d92977828d1983f04e575"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "09d9566df3e194af10ca708f99f20d0d7ca0f2d14dae84c797508c443cd5fb8e"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "34cfb579dcddb0ded88010dea55a3b5bb4d78628ab6c0bc0e7f70d93882b2156"
+    sha256 cellar: :any_skip_relocation, ventura:        "fd4bf8df87ae39182a966d3f85daa450777ad8e1f2a247a64309a0045246a244"
     sha256 cellar: :any_skip_relocation, monterey:       "8374a6b1aee80538c9b31d48edb0065deb63fb3bfdcea4e62b206818cbb7f181"
     sha256 cellar: :any_skip_relocation, big_sur:        "4c1ab9eca5fcff27e5aa6185a9b908c1c4c0569ceede8ef574d8365da6f1d914"
     sha256 cellar: :any_skip_relocation, catalina:       "bd9d4471e3e4832bbfcc4ddf0fa34f32bfe0fc6efea5ec17414157cc060a141d"
