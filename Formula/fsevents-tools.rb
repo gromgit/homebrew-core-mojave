799e6f9d4650d1fb3c9d71e2f15424bd9c33deeb
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:34:10 +0800

    fsevents-tools: no rebuilding required

--- Formula/fsevents-tools.rb
+++ Formula/fsevents-tools.rb
@@ -14,6 +14,7 @@ class FseventsTools < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "862af188ba8ede21f7810c642424284d257e2ffdb88ae8652d0eff0ce519d270"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "4485f966db472e54a07bc973d25945a4b72e110e68222bbd6ffb206bef843d74"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "613a2ee6e962c3681f00a36382fe87089c92a235d2db0dec7e8fb8e74f993b0e"
+    sha256 cellar: :any_skip_relocation, ventura:        "3136f299634d309fd98b7aaec18e4b03b28b4c61716b86e8a32f3933f0bba669"
     sha256 cellar: :any_skip_relocation, monterey:       "1d2134afbb595faece7c4025d78a7f0de8c52e3c90ff8c6965aa645526fb867a"
     sha256 cellar: :any_skip_relocation, big_sur:        "da9e4eed81589e2ea9e7f6a9186cd178ad965d5cba6b71ed2a3515729cd1cfb7"
     sha256 cellar: :any_skip_relocation, catalina:       "a59b40a61a49089e4a3ba5bbf0bd51790f043975c51c05c8eec39bf54425ae2e"
