6350db00bc121110c98d71e30d490359ac3fc673
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:46:32 +0800

    mongroup: no rebuilding required

--- Formula/mongroup.rb
+++ Formula/mongroup.rb
@@ -9,6 +9,7 @@ class Mongroup < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "16deb20baa844034c6a1c8bfcb61971a71f576fbebd0dd6170321aeed2866ee7"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "16deb20baa844034c6a1c8bfcb61971a71f576fbebd0dd6170321aeed2866ee7"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "bc40264f75aafc4f194f10d2b7472e152a4c9f58ec104db6ebeac3dc346c4370"
+    sha256 cellar: :any_skip_relocation, ventura:        "16deb20baa844034c6a1c8bfcb61971a71f576fbebd0dd6170321aeed2866ee7"
     sha256 cellar: :any_skip_relocation, monterey:       "16deb20baa844034c6a1c8bfcb61971a71f576fbebd0dd6170321aeed2866ee7"
     sha256 cellar: :any_skip_relocation, big_sur:        "d2112e201508ee22715b2e318b2fd97ae0cf4044e1d74d0893abf12598fb72bc"
     sha256 cellar: :any_skip_relocation, catalina:       "57c107a2746fae7e9db832f54df3d5170449defc30334094939794288659f026"
