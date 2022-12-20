a76c6941fcb440abd5a5b57bbaaa434bffa362a9
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 19:03:45 +0800

    zelda-roth-se: no rebuilding required

--- Formula/zelda-roth-se.rb
+++ Formula/zelda-roth-se.rb
@@ -3,6 +3,10 @@ class ZeldaRothSe < Formula
   homepage "https://www.solarus-games.org/en/games/the-legend-of-zelda-return-of-the-hylian-se"
   url "https://gitlab.com/solarus-games/zelda-roth-se/-/archive/v1.2.1/zelda-roth-se-v1.2.1.tar.bz2"
   sha256 "1cff44fe97eab1327a0c0d11107ca10ea983a652c4780487f00f2660a6ab23c0"
+  license all_of: [
+    "GPL-3.0-only", # lua scripts
+    "CC-BY-SA-4.0", # data files
+  ]
   head "https://gitlab.com/solarus-games/zelda-roth-se.git", branch: "dev"
 
   bottle do
@@ -10,6 +14,7 @@ class ZeldaRothSe < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "3f6f18d2ea37dd7ddd63d5a050a9944e184d0db94f89fe54c6ad376a0464b65b"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "f1f9b1fadb798f87d38698f31a0bc8cfb36c21c272087a25fc50256de704d379"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5e526691edaef39b1c1d1ab41b09bf2d821d156c89d7ca6dd585e8599024742f"
+    sha256 cellar: :any_skip_relocation, ventura:        "2bff31ad635bfa2e8a1b135e3f100b1c24d70682ad915af130e5af9c70d5f873"
     sha256 cellar: :any_skip_relocation, monterey:       "bd2099609e574fa6b5703c40455f7d7187eda55f938d7418bf4eb2dd68a1dd66"
     sha256 cellar: :any_skip_relocation, big_sur:        "28b1bd5308092389db177a9b277a29f1da892c1a4a71dd9b12e483a045e52808"
     sha256 cellar: :any_skip_relocation, catalina:       "1531cd6fc89cca4cc08287e569cdd8b86e41a52bb8c66fb10f6a74bb5006bc24"
