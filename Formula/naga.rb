4dd519b79b74331bf78e71d87f3c0b2a374e0661
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:47:11 +0800

    naga: no rebuilding required

--- Formula/naga.rb
+++ Formula/naga.rb
@@ -9,6 +9,7 @@ class Naga < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "d32ce728cc5082fb81b509ee4d2aacdf3f3b3e5c99653493412afca4f4558559"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "060c060175d76dd77c768ec1fd07fe74fc01e404e4f4a6b8be3a75cead596abb"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5598664fc7fd64d0f76d0291bbe79c209a65fd8142d6cbf7f7164531d538b9c5"
+    sha256 cellar: :any_skip_relocation, ventura:        "b1a38c76c5088aab68b817aea8d249a40f2aec9101606494d42182826528561b"
     sha256 cellar: :any_skip_relocation, monterey:       "6b013d2185f67b684ab4f49db162fdb32bba2dc6914d9855c6f7fbb4bd5603f9"
     sha256 cellar: :any_skip_relocation, big_sur:        "58d4a48fe33e676993449a2cdf332f74b6858681bc5519374d6e7a8842df9434"
     sha256 cellar: :any_skip_relocation, catalina:       "4a397ca0cf60725415818826e47fbf20c4b9cad2bc754128ece0d50279b715fd"
@@ -20,6 +21,8 @@ class Naga < Formula
   end
 
   uses_from_macos "ncurses"
+
+  conflicts_with "naga-cli", because: "both install `naga` binary"
 
   def install
     bin.mkpath
