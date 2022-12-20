00db5ddec3f6575df0c11bfd74d4762fb0ecf255
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:29:06 +0800

    crystal-icr: no rebuilding required

--- Formula/crystal-icr.rb
+++ Formula/crystal-icr.rb
@@ -9,6 +9,7 @@ class CrystalIcr < Formula
     sha256 arm64_ventura:  "a7d8abeeb9c8fc24d04a94fc93744c4c8136c8c72dacf16dcb78f8fd5bd14ddf"
     sha256 arm64_monterey: "0c38a89034238cb822d76c5d61b1ecd57aa960356a6aabdf7f9e44fc5d8ba29a"
     sha256 arm64_big_sur:  "d98e57c4a69bfe60a4d30b304f11b04bd946d5b922325fd20aaa1895f6a392c8"
+    sha256 ventura:        "50ff1cc9f734f1fa2bcb8b3443f57702c985231a520268f32285c9b1a2842092"
     sha256 monterey:       "eae95948e77fbb4ae9c57358096b0fd394075af88dec1c5ceb5cd8108fbd7da0"
     sha256 big_sur:        "8daaa1313d4bde47396ed4f6e0801e937b128b64cc4eb7528325cb27404dd765"
     sha256 catalina:       "4d419018a1b470514b9ee5833dd8062fa56b954ed8ae255a2062554368f0185f"
