112dbe6f54156939d57d5e94e68b55833360634b
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:46:10 +0800

    mlogger: no rebuilding required

--- Formula/mlogger.rb
+++ Formula/mlogger.rb
@@ -9,6 +9,7 @@ class Mlogger < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "3aa5e9292806dcc8150dafdd703720141806be0b610198826e8d226baad776d8"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "a2446f830d72176c5dda94e2023f588fea40cb7f0d1fbcce88e07f7ba56cb414"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ae9dd6052251a2283883741ec0c1643cf70e62c2acbda9bec06971375b98eced"
+    sha256 cellar: :any_skip_relocation, ventura:        "23fb2fbde1f3dcf119ad7d994555dd2920170009a3cf768af1e98b877521f981"
     sha256 cellar: :any_skip_relocation, monterey:       "234b0797fe08d79cd45ee6e2587fafe3322657b70b386e3327f4dc7a103edb3f"
     sha256 cellar: :any_skip_relocation, big_sur:        "251a03f6e4954f46183a2eaa1ead28e993974d1ab5e6b4b6ae85d1777b15a379"
     sha256 cellar: :any_skip_relocation, catalina:       "553fe787f0d6a1982544a74ec268d3db6bdf800d538238cd627ba39d8bb1cc37"
