925d20376059187584c8025befa13a86c8ae7fa6
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:52:54 +0800

    pyoxidizer:

--- Formula/pyoxidizer.rb
+++ Formula/pyoxidizer.rb
@@ -15,6 +15,7 @@ class Pyoxidizer < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "a438770a4ce6bb1ea06f3d23d1c0cf505f93383de2ff27d99b22d5d5b34c9f51"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "cb62cb641b9b69e0995c19c288a051a5f59e11de3a174aceb4c67daf48332b4c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2dbd06e01ecb46b93bb3250bd0d5e349295ba6c56b77622d29c64676ed17e3e8"
+    sha256 cellar: :any_skip_relocation, ventura:        "4b03858e038e6d7a4a40abf538618704f9b891815766a45492ae41af608eba72"
     sha256 cellar: :any_skip_relocation, monterey:       "80d2014e88d9937d1a88b99da5a8d035e8ac42ccc4df7480512e020e6ee30fb6"
     sha256 cellar: :any_skip_relocation, big_sur:        "e9aa0d494c359e0f8b16c8c3ae31159d8f540f3f9d7946ca4bdc20bc6a83fe10"
     sha256 cellar: :any_skip_relocation, catalina:       "3d1bc85db1bcbb5f69c8dba9c294d65ef9b674d217f6eab670ccc6eb4dedf812"
