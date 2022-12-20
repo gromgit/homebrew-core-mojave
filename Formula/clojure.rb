c31027bd95a7427d48297e4e9e779dd3e6775973
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:28:03 +0800

    clojure: no rebuilding required

--- Formula/clojure.rb
+++ Formula/clojure.rb
@@ -1,8 +1,8 @@
 class Clojure < Formula
   desc "Dynamic, general-purpose programming language"
   homepage "https://clojure.org"
-  url "https://download.clojure.org/install/clojure-tools-1.11.1.1200.tar.gz"
-  sha256 "dbde9c4bdd42b7da5a67d879548b4528e4903ce73ee26c329626b6165d57b61c"
+  url "https://download.clojure.org/install/clojure-tools-1.11.1.1208.tar.gz"
+  sha256 "6e7f6e34ce3aa18734c31249f073066c4f3fda69d8c66fe9f52e8c8587e17103"
   license "EPL-1.0"
   version_scheme 1
 
@@ -12,7 +12,7 @@ class Clojure < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "4fbd839618f5808db69500f795fbd2bc2267d9fe7b1534a8b13584faaf26e224"
+    sha256 cellar: :any_skip_relocation, all: "ab7309d3cd4c38346e70e789e962608145f33983ee23f6ef3245d253d0e6397b"
   end
 
   depends_on "openjdk"
