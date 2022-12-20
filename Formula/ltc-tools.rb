136ea5133a6ddfc7b757234024be24d3a726bc84
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:44:22 +0800

    ltc-tools: no rebuilding required

--- Formula/ltc-tools.rb
+++ Formula/ltc-tools.rb
@@ -10,6 +10,7 @@ class LtcTools < Formula
     sha256 cellar: :any,                 arm64_ventura:  "bb595fe27ddac376f1ea097a824c17712c34dde20e01cb310388897b52f3f57d"
     sha256 cellar: :any,                 arm64_monterey: "d8d6d714abdb13a7c1a42503b1f367af936975d51454967bb445acb631b58259"
     sha256 cellar: :any,                 arm64_big_sur:  "2131abaab3877ab6a2425fe8b635612c5d7235026d6098a3eea78d266038378a"
+    sha256 cellar: :any,                 ventura:        "c118c95af01d6294a78e22dcbc2ea901ce5c7bf52a62912895c2ed3c344df105"
     sha256 cellar: :any,                 monterey:       "84e20bc4899a76f661ff8bde1ec354a3d32f6f53b4aa434782095868971a2cc7"
     sha256 cellar: :any,                 big_sur:        "b31fe1140d71357035fd130e73c286d8892cda1103fccd96971205bd860cd9a7"
     sha256 cellar: :any,                 catalina:       "bcd064f64a21f101f6599646306ba65c40ce7ec44fd7b6e2d8f29b4fefeebcc9"
