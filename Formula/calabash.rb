f2f98de993b3f744a3358ea85edc180e18aaf961
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:26:39 +0800

    calabash: no rebuilding required

--- Formula/calabash.rb
+++ Formula/calabash.rb
@@ -1,12 +1,12 @@
 class Calabash < Formula
   desc "XProc (XML Pipeline Language) implementation"
   homepage "https://xmlcalabash.com/"
-  url "https://github.com/ndw/xmlcalabash1/releases/download/1.5.2-110/xmlcalabash-1.5.2-110.zip"
-  sha256 "0bf7426d5d639c897e1dee80d6d51115965714ea059bb5be5ea10e76a04755d5"
+  url "https://github.com/ndw/xmlcalabash1/releases/download/1.5.3-110/xmlcalabash-1.5.3-110.zip"
+  sha256 "e599484bc95b1dc2086a04c4be24e15da9eada8139b3e6a35d3636e2a8be18d7"
   license any_of: ["GPL-2.0-only", "CDDL-1.0"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "8facaa4f43d14c528c7efc1c3b66fd61fe8f4af01022f82e0a7aa72082f844dd"
+    sha256 cellar: :any_skip_relocation, all: "a94df53dffca513f8708a4c8ece8fc520eb431a30c15af964d46f7385d2cf4b5"
   end
 
   depends_on "openjdk"
