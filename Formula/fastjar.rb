d8bd2562f63fad9646678132fd2227ca27e11142
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:32:53 +0800

    fastjar: no rebuilding required

--- Formula/fastjar.rb
+++ Formula/fastjar.rb
@@ -14,6 +14,7 @@ class Fastjar < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "b0499cae7fac86fbe57a98cbceffb53e2ff047cdadbcca9a103083a0cc6e9a20"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "c5fa05b957bd369862d66cefb2cfe2ec5fdb86bf6ea3bcde2b8f95c1d872a293"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d9bb50e8b725164441e747625b381631edf82e0040babda6f187466295f80e3f"
+    sha256 cellar: :any_skip_relocation, ventura:        "9581595d1660096f047ee3c601481f6877a209c4cfd9fcfb9860fbfd79adad60"
     sha256 cellar: :any_skip_relocation, monterey:       "06ceeffa38b10f099a521d5ea3ac4dd52d6d0d5740c3fc64f6b4e8509a842cef"
     sha256 cellar: :any_skip_relocation, big_sur:        "1cbf953373b3d48accd872aa603fa66fb3b96e1ec33d4a17dfe6da2f97ad5cbb"
     sha256 cellar: :any_skip_relocation, catalina:       "ee758c76cb694c96ea30cb9e6ac204f2797c78be36610dcdf36c2a75301b5835"
