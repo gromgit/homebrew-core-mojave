4b6bde92a02868d9dd6b5709a42b220ef1f44703
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:46:24 +0800

    mockolo:

--- Formula/mockolo.rb
+++ Formula/mockolo.rb
@@ -9,6 +9,7 @@ class Mockolo < Formula
     sha256 cellar: :any,                 arm64_ventura:  "d15b1e0d9c4101d06bebcd24e12727cb8243cd76fa51cf2ccc539560876483b7"
     sha256 cellar: :any,                 arm64_monterey: "0f4677a443def0c1db5736b49186f1e752ee8a4dcf3f01f471a1bd54f543c2dc"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "ab052d6c093c64bbc22d54ef082487560b42ba812d28e4ecfca174da009a47bf"
+    sha256 cellar: :any,                 ventura:        "60fb1bd8615d03b3b02e38b59341d2a792ac328f85c6b044c624772747ab38d5"
     sha256 cellar: :any,                 monterey:       "2de4ba04ddf7154f718147c4e198194a7e7eeb37312f26bb481deab5c9099995"
     sha256 cellar: :any_skip_relocation, big_sur:        "e042c9a875089f7a2fb8df3374ad94a3edd22ff4d24a51198fef77a20c85bb78"
   end
