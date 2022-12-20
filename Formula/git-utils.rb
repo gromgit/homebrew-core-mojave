e5a5f00ddf2fe023dbde9693f4fb9003c686ed3b
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:35:19 +0800

    git-utils:

--- Formula/git-utils.rb
+++ Formula/git-utils.rb
@@ -3,11 +3,7 @@ class GitUtils < Formula
   homepage "https://github.com/ddollar/git-utils"
   url "https://github.com/ddollar/git-utils/archive/v1.0.tar.gz"
   sha256 "a65252222222981d769fe2b19508e698fac4a0ce72e4ff07e74851e99a8fc813"
-  head "https://github.com/ddollar/git-utils.git"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "7c7e5940ae9e46d0dfacb6b2b8db5dcf98626012d57d563a9ec1ca852bf9f0fb"
-  end
+  head "https://github.com/ddollar/git-utils.git", branch: "master"
 
   disable! date: "2021-12-07", because: :no_license
 
