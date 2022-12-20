53fef06d98780a54a1b0e67907925a5640e8b64d
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 19:02:34 +0800

    wordplay: no rebuilding required

--- Formula/wordplay.rb
+++ Formula/wordplay.rb
@@ -19,6 +19,7 @@ class Wordplay < Formula
     sha256                               arm64_ventura:  "e5046dfd7e922872308efed4f745399220ddc5f7f62f4d200748700d136e956a"
     sha256                               arm64_monterey: "28dcc7b1dd3d809b79e3b331309d2f3ebd4b23383e76813b0397dcc617527e48"
     sha256                               arm64_big_sur:  "5ce75cc234b4d54de31124443d207bae75ed01211cdb23770363efc0b984bc75"
+    sha256                               ventura:        "1074a6a9d010d50bfe73cea4fbafb6a2d7d90155057e1c25e90394533c9eba83"
     sha256                               monterey:       "f456578ef358b10b91008a83f42e6877a4daa32615588fbb3ac629d9db804c5a"
     sha256                               big_sur:        "086d078ef82bce278ad9bc25d901f1ffd3bd539aef410c7e81466b0616ef2c32"
     sha256                               catalina:       "bf3847365e1920baf313fc0286116de59fee392ccde5182173ed7198b9883626"
