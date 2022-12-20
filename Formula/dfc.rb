c88ead6e0a7e02f3b5ec04cfdfb711d0a003f7e7
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:30:10 +0800

    dfc: no rebuilding required

--- Formula/dfc.rb
+++ Formula/dfc.rb
@@ -12,6 +12,7 @@ class Dfc < Formula
     sha256 arm64_ventura:  "3a7c3a4bb6e644fb06bc3b28dcce2f2a61a2be235cb65e4a188bf55a63657c43"
     sha256 arm64_monterey: "6aa0c0d2ad81bf179b61fced051ea22e5cb85376eafbb8e1d7376d8f3fc9cec7"
     sha256 arm64_big_sur:  "6f2d7350e0c7e1c905718b6dcf282367bc846bbd51538a9a525f681dda03be61"
+    sha256 ventura:        "fefbde9ea38d983a69c406f0ed63ceecdea1460fa34d97ca64a158b86da1f2f2"
     sha256 monterey:       "127dd250819075427ca4a6f35f292d29c4af8b070c6ee368645817d54ac5a50b"
     sha256 big_sur:        "a89714cadb5ca91708c9f0c0f37266726517418e0ee592003c1cff38cc7599b1"
     sha256 catalina:       "cefa6f0f5e96a815ebbee4d4618dc927f88052f4137d52c31d21688fac211aa8"
