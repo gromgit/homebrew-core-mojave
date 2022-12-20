713175e9a2550fb1927230758782ecf974521406
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 19:03:20 +0800

    yamdi: no rebuilding required

--- Formula/yamdi.rb
+++ Formula/yamdi.rb
@@ -9,6 +9,7 @@ class Yamdi < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "385b4af3db0b7f734f690539553c250c0e08fb8a472c7bef07e31fb273f20fbd"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "b50a1157642d1cf0bca8199e5da07b8a71788775d3d1425642cfd3739ee2db26"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f6796b10d1af7ae38ed559313b5646047cb1456c66428d9c32f5e3a0981f1f4c"
+    sha256 cellar: :any_skip_relocation, ventura:        "6a318c89f3e6924e58b9ed87ee9611260eb226a1a6ed924b176742eac2900854"
     sha256 cellar: :any_skip_relocation, monterey:       "2bbf1bbbc7bb411fe09900dbe61093834ffb41f314ba241deee6ff764c285232"
     sha256 cellar: :any_skip_relocation, big_sur:        "375c99c3793fe45e70a76ef708f9d1b8d5f4e9a7c7f64eca0f7f522926432d82"
     sha256 cellar: :any_skip_relocation, catalina:       "6a3483a957ef3a480f5391d9483b0d3cf8adfce2ec2f6b48289f16733ce29771"
