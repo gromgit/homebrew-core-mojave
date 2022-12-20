14bcbc26ae60408472e71ec0ba4e78ee6b6a095e
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:24:58 +0800

    aws-console: no rebuilding required

--- Formula/aws-console.rb
+++ Formula/aws-console.rb
@@ -13,6 +13,7 @@ class AwsConsole < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "508d88fc97215096bc0998bcc709cef5aa33499bfdc17ac47d123caccbe37bfb"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "43a5d8ececf9b15da91d81d6218a9c137c1a4372d726888025437cf757d2cb18"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "cad10ed651e199a6ca16f3aed1a4ba7b78e29f7924af2e3b7fef9b88a3429ee8"
+    sha256 cellar: :any_skip_relocation, ventura:        "7b0aa226d8a52ac09199c52763ff8b9ef9ad0439e00a42009a47f15ebec7f33f"
     sha256 cellar: :any_skip_relocation, monterey:       "8f81d0733650c747c3a8c2b45e6f520897bd204a11887d32248195a89eab6bfc"
     sha256 cellar: :any_skip_relocation, big_sur:        "2268358c33a9f5bc64a9e0dade281df98a6a9b0b9669f8d84ec41da7a0731a10"
     sha256 cellar: :any_skip_relocation, catalina:       "33f94ef8581875ef09f33d6b9fdd619cf46be7d9fa90c62e3b9f4be73122715d"
