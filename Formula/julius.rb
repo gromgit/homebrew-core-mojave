0485419dc0e1fe41dd939375ea5a81d6b93d95d8
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:40:01 +0800

    julius: no rebuilding required

--- Formula/julius.rb
+++ Formula/julius.rb
@@ -9,6 +9,7 @@ class Julius < Formula
     sha256 cellar: :any,                 arm64_ventura:  "345963770e4eaa6cd0a4308d657f4b0a45b00d30e23d5e7aba9143dee08f418b"
     sha256 cellar: :any,                 arm64_monterey: "61e0a2f95974a4fdfaee2253963fe3ab20284c4325e0f34aa22bc4a9e40a09c0"
     sha256 cellar: :any,                 arm64_big_sur:  "dcbb2b7bfd4ba078ec6473c2193ca6fefd3f1cbe6375bd662401a5b607d99387"
+    sha256 cellar: :any,                 ventura:        "c4229059cd82483a8bb92be7fdaadb2d1958e4be61001fa539f71fde025eada6"
     sha256 cellar: :any,                 monterey:       "73f943f1011686778fdae712733eb0987f97976c6f0ab26d9771c57eccf304c9"
     sha256 cellar: :any,                 big_sur:        "4b8251857584f844fe5469a0283a773428383053f8d80eaeff885b745578aa1d"
     sha256 cellar: :any,                 catalina:       "b06b9ca71df4cccff10e36a4a75a55f7d5bdb009f4dba9f940044da6ba0c258d"
