c95e6bb02a6c9ad68c559d51f10d4a1f99a97913
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:54:46 +0800

    rswift:

--- Formula/rswift.rb
+++ Formula/rswift.rb
@@ -1,19 +1,20 @@
 class Rswift < Formula
   desc "Get strong typed, autocompleted resources like images, fonts and segues"
   homepage "https://github.com/mac-cain13/R.swift"
-  url "https://github.com/mac-cain13/R.swift/releases/download/v6.1.0/rswift-v6.1.0-source.tar.gz"
-  sha256 "f4b4c3f8748358c569c219d7f506d3b34ea5af82c882ee4a23381f23c4d277c8"
+  url "https://github.com/mac-cain13/R.swift/releases/download/7.2.4/rswift-7.2.4-source.tar.gz"
+  sha256 "76d1c32848987250fb684edd3eb7539af49a50b6b50164c61ad4b1b34c73520e"
   license "MIT"
   head "https://github.com/mac-cain13/R.swift.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rswift"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "04afe6ec87f5f5aad3cc66d425b1eb71107200f703ddf160f37445c954b58f78"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f070f8ef57caba4f93c38a5f60d0c0fdac457289949a50b66007ed477d281043"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "51a4e6c8e8cc24590966f9a02b8e13a089f143770ecc8217224f42a758268efd"
+    sha256 cellar: :any_skip_relocation, ventura:        "81e656fc368c7bddbca9a095518bca63c4d11408c274d558fd26e6313952f2d3"
+    sha256 cellar: :any_skip_relocation, monterey:       "2514bf9e6e8a9fe4e3e84ec13af6606dc6bb5ac23bba6e273a6b63aa65e2c49b"
   end
 
   depends_on :macos # needs CoreGraphics, a macOS-only library
-  depends_on xcode: "10.2"
+  depends_on xcode: "13.3"
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
@@ -22,7 +23,7 @@ class Rswift < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/rswift --version")
-    expected_output="[R.swift] Missing value for `PROJECT_FILE_PATH`"
-    assert_match expected_output, shell_output("#{bin}/rswift generate #{testpath} 2>1&")
+    expected_output="Error: Missing argument PROJECT_FILE_PATH"
+    assert_match expected_output, shell_output("#{bin}/rswift generate #{testpath} 2>&1", 64)
   end
 end
