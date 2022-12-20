14ffe75ae1697821757d7fa5edea91c936a97ead
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:28:14 +0800

    cmix: no rebuilding required

--- Formula/cmix.rb
+++ Formula/cmix.rb
@@ -7,6 +7,7 @@ class Cmix < Formula
   license "GPL-3.0"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, ventura:      "0e7c677e5c3b587dc81178d363b9a1210ad52daaf903a248d1c77d4e589a6c3b"
     sha256 cellar: :any_skip_relocation, monterey:     "ad293329e67ffa22e3e159643c1324b587096c27daf0f0b77feb3633f3adf0c3"
     sha256 cellar: :any_skip_relocation, big_sur:      "3ab169d97ef3e781f7fab12bcf99a74a584462fa154e59d85f96449aa525077c"
     sha256 cellar: :any_skip_relocation, catalina:     "9dbbd3e8367f799405fbb237d68fe46e968bda502a5779f3be1c467b56e394b8"
