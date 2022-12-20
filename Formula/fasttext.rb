9cf9104bdeb1836d45efb12d3c3f1c84c302caae
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:32:57 +0800

    fasttext: no rebuilding required

--- Formula/fasttext.rb
+++ Formula/fasttext.rb
@@ -10,6 +10,7 @@ class Fasttext < Formula
     sha256 cellar: :any,                 arm64_ventura:  "891397ee186031bd9cda06521af21d17f1ddad2f493353320c924bb9f60aca76"
     sha256 cellar: :any,                 arm64_monterey: "5a2a2a202ee6d5b21bc1857be97e41876353e9ef9c4a2af5466b7def501bc1ce"
     sha256 cellar: :any,                 arm64_big_sur:  "3bfb7e1ab42dde74ac0692d2016f718e173b9e9dee093e408dda8f4c22ef1a8a"
+    sha256 cellar: :any,                 ventura:        "05f913451fccb021b6c17c9b4d7e42eee0bcb9a2840c185c3099e3c2b1acbe0e"
     sha256 cellar: :any,                 monterey:       "7b1b5a4cbb2ce1de373b18eec3b8cdaeb1e5ac144b0f191fca05b977cf54c10e"
     sha256 cellar: :any,                 big_sur:        "3869650705430f8b682416be4e7c0a01c243a2f9517c6668027c6e9576f1e9c6"
     sha256 cellar: :any,                 catalina:       "ec085551ced1f55b863a65aa60ad8f31d796002702b7effaaaafbf1490df867f"
