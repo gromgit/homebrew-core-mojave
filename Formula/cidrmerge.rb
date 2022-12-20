525c490101b134a19750b1df478377d38dfd598d
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:27:44 +0800

    cidrmerge: no rebuilding required

--- Formula/cidrmerge.rb
+++ Formula/cidrmerge.rb
@@ -9,6 +9,7 @@ class Cidrmerge < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "66c685d8c347fd583afe154475091c456ec7c9e7f0891542fbe1a46bebfad216"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "1917c09d7f9dd006a7688c9be3e1673c3a93950f9e58d23eb6b2dab14b2a334f"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "49083b84e43debb1921bf1e3788dd5614bcbac3d70b68d099734421fa94f7fd5"
+    sha256 cellar: :any_skip_relocation, ventura:        "ec886d391031fb25d40a8430fb5a73e07b7c8cfb33963ec78ad10cf13dceb0fe"
     sha256 cellar: :any_skip_relocation, monterey:       "a447eb73e2385698470c99081afdc04f33f15ad684573828f2e8531eb7a84786"
     sha256 cellar: :any_skip_relocation, big_sur:        "5f11e096d4f5b0af52ec6822f2fba79bd053c083b114f41fcc9ca40112daf5db"
     sha256 cellar: :any_skip_relocation, catalina:       "5828da34c41143336cced7cc8051efd63d525c1a1a4788c6c1235d4bc75cf3df"
