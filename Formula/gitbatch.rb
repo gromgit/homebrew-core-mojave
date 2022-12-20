2eadb283b21e1750c3aa16ad63973de1c689aa21
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:35:22 +0800

    gitbatch: no rebuilding required

--- Formula/gitbatch.rb
+++ Formula/gitbatch.rb
@@ -9,6 +9,7 @@ class Gitbatch < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "5ae6361b3bc790e3b226978f4a841f3dbae69050e0214ad1ee6d30a383249a42"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "019454b03cd4d76a4fd4921b211dd74a1ac5d21263e5c81119b02fc56b4de151"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a6870e51c395a0c2bb7da37785ddd6932d18dd516905d0ab5c02632015edb39c"
+    sha256 cellar: :any_skip_relocation, ventura:        "00104e93f6610498cccea2112338f3f9fa516ca3bdf66ead50640e9d97739b8b"
     sha256 cellar: :any_skip_relocation, monterey:       "f79aa114971d190366dc81c1bc6deda1c176c74a33e0abc3a89875c4785414f4"
     sha256 cellar: :any_skip_relocation, big_sur:        "3ae2045b1f348f381d15ff6eb708ac5ace0f550c733f3f5002c5edde50514c08"
     sha256 cellar: :any_skip_relocation, catalina:       "6ea0a1220269223eb0ffb99eda340f726a146dafe10b8e558e44eea278d15a37"
