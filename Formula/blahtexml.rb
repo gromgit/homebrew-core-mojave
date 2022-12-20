0af1a3f6b04c9ed92e92577db54bd4f2f1c46612
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:25:54 +0800

    blahtexml: no rebuilding required

--- Formula/blahtexml.rb
+++ Formula/blahtexml.rb
@@ -10,6 +10,7 @@ class Blahtexml < Formula
     sha256 cellar: :any,                 arm64_ventura:  "8649e81f949cbc530d00a446b56fee8ea6013a9bac9f8abc732c0ceda928b9d9"
     sha256 cellar: :any,                 arm64_monterey: "8783f6707647e97b97332dfae2ae2705823786ac65977d239e9d38dfb1f5b0c7"
     sha256 cellar: :any,                 arm64_big_sur:  "83b3c821686faba9f464198f22fc7432001e93d09c66192ff95c8d9ddbb69a80"
+    sha256 cellar: :any,                 ventura:        "33ed53295e49baaae6a268923b6ac906e6529faa52a5230090b73eda06131e68"
     sha256 cellar: :any,                 monterey:       "a959c9373710994104947e5fe99e22d8824c5523fc4974e5634560d02cec0813"
     sha256 cellar: :any,                 big_sur:        "b65b2c94d4c7b015a9ebf54ca27d71d52e09fc946ea1ecd170c952f3d262a599"
     sha256 cellar: :any,                 catalina:       "3f883672f92e2039c22bb278ca50ece210c2c01e58f4c230c3ab1e3101eeb74f"
