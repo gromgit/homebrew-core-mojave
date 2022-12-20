bdadb1b0b76855e8c602720dedfdbfdbd3c5d77c
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:29:26 +0800

    czg: no rebuilding required

--- Formula/czg.rb
+++ Formula/czg.rb
@@ -3,12 +3,12 @@ require "language/node"
 class Czg < Formula
   desc "Interactive Commitizen CLI that generate standardized commit messages"
   homepage "https://github.com/Zhengqbbb/cz-git"
-  url "https://registry.npmjs.org/czg/-/czg-1.3.12.tgz"
-  sha256 "b4f2f11e3a8b95cd417a75142d7aaf8b22c7949c60133dc0a14b65c2c7725f33"
+  url "https://registry.npmjs.org/czg/-/czg-1.4.1.tgz"
+  sha256 "23234a955dbc92d4ff852c5a62774922537edc77924d0071d1f0e2b1dc6987b4"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "31307d18aa0bf23ffddd4fc098bf41f65de46e3cdfb39ff4f4048af31ab19650"
+    sha256 cellar: :any_skip_relocation, all: "2f6283e2bde6b6942f39e810b28fdcaf148773db9d5ea5e92a1fd27f0569ea3e"
   end
 
   depends_on "node"
