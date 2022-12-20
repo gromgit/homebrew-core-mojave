553f53fc7f3b6e4d2f67cf7106db53774998e65d
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:50:03 +0800

    pam_yubico: no rebuilding required

--- Formula/pam_yubico.rb
+++ Formula/pam_yubico.rb
@@ -14,6 +14,7 @@ class PamYubico < Formula
     sha256 cellar: :any,                 arm64_ventura:  "e763d80b575c27eb381f559494636cb7737a0721f884d98523d0452911262273"
     sha256 cellar: :any,                 arm64_monterey: "203b85ed98819720e7f40971f9978956b4fd458133a3935f402a10dfc2ab85b5"
     sha256 cellar: :any,                 arm64_big_sur:  "8d4405a65463be4dc6b5472b2cff454301591e57ff5c3b5fb4e8e40fb6981a66"
+    sha256 cellar: :any,                 ventura:        "3c949be5b7c5dd7f64b427e4bc90599c3bc8101deb6bf1b282eebb42312b10c8"
     sha256 cellar: :any,                 monterey:       "07bd1f48953cef8653bc75f23fcbf9fab5de45a3551d7ba7f23db22e558b9247"
     sha256 cellar: :any,                 big_sur:        "4abde2a6a123b3816945f79b07c760b95d2709fc791b5c5c7509d9ed1544e491"
     sha256 cellar: :any,                 catalina:       "2405af18c4c1b4c2573c221ff6699afcb37a42fe211ebb8b726314d31e13ce1a"
