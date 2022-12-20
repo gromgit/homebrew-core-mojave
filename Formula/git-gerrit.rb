c0ff138f7816ebc2c5b5c91c4423c20eceb06373
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:35:09 +0800

    git-gerrit: no rebuilding required

--- Formula/git-gerrit.rb
+++ Formula/git-gerrit.rb
@@ -10,6 +10,7 @@ class GitGerrit < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "f787bba2e4465a7f5df3bebcdb625c3815331a721abc0024ab09b14b868b3ec5"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "865ffd990de756622dd9c3bb37fd0f8e7af04eba58bd38013cda9b6a6c551f95"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "865ffd990de756622dd9c3bb37fd0f8e7af04eba58bd38013cda9b6a6c551f95"
+    sha256 cellar: :any_skip_relocation, ventura:        "89d72fc6229fb8d4a7a1e8e403bbe382bfea7463965440554e517179eeaeed70"
     sha256 cellar: :any_skip_relocation, monterey:       "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
     sha256 cellar: :any_skip_relocation, big_sur:        "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
     sha256 cellar: :any_skip_relocation, catalina:       "911fa65cea72dfe849d36ab108a6a36acd1295ea9c8f395a8f8ded8bdd930fbf"
