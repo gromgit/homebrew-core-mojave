af2bc481b57714e0b8c0b668ae47aba34a5d2b61
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:33:39 +0800

    fmdiff: no rebuilding required

--- Formula/fmdiff.rb
+++ Formula/fmdiff.rb
@@ -11,6 +11,7 @@ class Fmdiff < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "edb8bfe9b90e82db787a068b452976814e41ee0cd1be7672f1439b45dee8cd43"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "d55ab53b4e1f2ffb241d5b26422e2fb33dcaf8cbfdb287e0c21044ad450e0084"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "a49b1ebc974dfdea7865f1a4177f57fd85b64a138ab6273cd825e3739c9517cf"
+    sha256 cellar: :any_skip_relocation, ventura:        "8502adf0d116a2b6d467a77af41b9e1f957ea51c87b37ea744e72bd3b89e3e67"
     sha256 cellar: :any_skip_relocation, monterey:       "c8b64bcf072b5404481a44740af5613c49174eb58aa3fb9838d2483d5c6c6a57"
     sha256 cellar: :any_skip_relocation, big_sur:        "bf75678c84061a3bdcf2c0865bfa87c4453007abf21b3dad875f737ae48922b6"
     sha256 cellar: :any_skip_relocation, catalina:       "4dc5d4eed6916cab57ab1350c7623014c1f2136b69dcb5593c9e344b86328f6c"
