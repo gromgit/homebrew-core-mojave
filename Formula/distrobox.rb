38f26ed5cb5ef30dc0100f5e14e5bb657f154c9c
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:30:29 +0800

    distrobox:

--- Formula/distrobox.rb
+++ Formula/distrobox.rb
@@ -1,13 +1,13 @@
 class Distrobox < Formula
   desc "Use any Linux distribution inside your terminal"
   homepage "https://distrobox.privatedns.org/"
-  url "https://github.com/89luca89/distrobox/archive/refs/tags/1.4.1.tar.gz"
-  sha256 "be3266f77011b30da5d0035e246d4e4a9360b7c90fbd8b90ac581165a00217b8"
+  url "https://github.com/89luca89/distrobox/archive/refs/tags/1.4.2.1.tar.gz"
+  sha256 "cbe9217315f848396ec1eb8e21929d23065caa6a1a55f8988500475e823b1f31"
   license "GPL-3.0-only"
   head "https://github.com/89luca89/distrobox.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "466922b6aaa2240b66b25b61a5d844beb82cdff19e4d63a02760aec73a465dc3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1f6c52b74824f52456bab567688685d454b3c759f3ed7e26e1f630b825f3a828"
   end
 
   depends_on :linux
