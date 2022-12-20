76e06768a9e8a8a7e43bc2862452eb12a814c6ad
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:47:24 +0800

    nedit: no rebuilding required

--- Formula/nedit.rb
+++ Formula/nedit.rb
@@ -10,6 +10,7 @@ class Nedit < Formula
     sha256 cellar: :any,                 arm64_ventura:  "2ce67e319d75468e209d20d306cb6c3dcacea83feafd8114902273b1cb4b3344"
     sha256 cellar: :any,                 arm64_monterey: "9ee06ab3c8df64e26b87d80ac63acfff12ce97286ce115f2fc8882b3e4c88605"
     sha256 cellar: :any,                 arm64_big_sur:  "e81454f55e5a3b396abe741f65a4a2600d18e93301385edd3481d8d55cee20c0"
+    sha256 cellar: :any,                 ventura:        "4a0ede3b87a97df3bb77ef2ffaac6632cb6f3834c1208cf9c076c0b3eda751c4"
     sha256 cellar: :any,                 monterey:       "97501b6d060c2da91ce81ff637fc9898e5f3696c0ca6005ea862d5cf3b9b59f6"
     sha256 cellar: :any,                 big_sur:        "d39ce752a03c79732c908a3cbe93df61f413a12126f764e7e1c3d71f4106f701"
     sha256 cellar: :any,                 catalina:       "c726811764a5d12465d4c11b273229482af935921df472f6d083a27e34b39b3f"
