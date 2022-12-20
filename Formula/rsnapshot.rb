8265cf335711b2cf801059f21778d451d21ac720
Adrian Ho <aho+vm-mojave@03s.net>
2022-12-20 18:54:44 +0800

    rsnapshot: no rebuilding required

--- Formula/rsnapshot.rb
+++ Formula/rsnapshot.rb
@@ -10,6 +10,7 @@ class Rsnapshot < Formula
     sha256 cellar: :any_skip_relocation, arm64_ventura:  "51bde7434f2ad12b3d0ce70396665ab1d636e41639a87ec7da43f7d5a57ef991"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "51bde7434f2ad12b3d0ce70396665ab1d636e41639a87ec7da43f7d5a57ef991"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "d3b962467213ee16410bc0e619994698c176ca9795ddb6f6087dbba934687144"
+    sha256 cellar: :any_skip_relocation, ventura:        "b80d39853be289fe0056893db5c0f977540a01704e65eb0a12168761a62c137d"
     sha256 cellar: :any_skip_relocation, monterey:       "b80d39853be289fe0056893db5c0f977540a01704e65eb0a12168761a62c137d"
     sha256 cellar: :any_skip_relocation, big_sur:        "18ba069ea2a52a986acf2027299e9ae5d85593c011361e2448f3b54752e42078"
     sha256 cellar: :any_skip_relocation, catalina:       "9d5272a4186bd3d895a5491bac76e66e3cac54e906bf03022d6ea7ad421b8fd8"
