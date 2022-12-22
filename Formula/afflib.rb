af11f2fc9952a7965dd67d107d9a8828bc706491
Adrian Ho <the.gromgit@gmail.com>
2022-12-22 17:48:55 +0800

    afflib: update 3.7.19_2 bottle.

--- Formula/afflib.rb
+++ Formula/afflib.rb
@@ -6,53 +6,47 @@ class Afflib < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "d0874ef5a186705eac544762239a87a41d01107ca802bf47083818f910c7168a"
-    sha256 cellar: :any,                 arm64_monterey: "06fc39593f42e77d5fa123c087ea516e8be899985d0d0dd04b5d5bebede9add1"
-    sha256 cellar: :any,                 arm64_big_sur:  "aa8dd52d5800f5ac0464a37b9f456ea830c62ae8a2775373f330ab7d8253bacc"
-    sha256 cellar: :any,                 monterey:       "614c38500b3602a7dac28ee776713c1ff88b6c25dd30b19b28581fc890a1e86c"
-    sha256 cellar: :any,                 big_sur:        "d5502071af61c4768c056d6ac7d3f7d1048044e9290b7a3823350b7df05a1e86"
-    sha256 cellar: :any,                 catalina:       "6662001d7ea73f9ec2f36bf94937c84581254ca4637a07d6a696116314a438bb"
-    sha256 cellar: :any,                 mojave:         "360c80c6323ff67028b0154508967eaa5b426675892147ca2d70bb11ce273d9e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "44a7c836a4474870e2e68b88916853703af597798eadde2c5f0c5024a46d41f1"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/afflib"
+    rebuild 1
+    sha256 cellar: :any, mojave: "f428404d149607a278eb7d11dc979d6fe612e8f4874c885c6dc5df43e9a3d676"
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "libcython" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on "openssl@1.1"
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   uses_from_macos "curl"
   uses_from_macos "expat"
 
-  # Fix for Python 3.9, remove in next version
-  patch do
-    url "https://github.com/sshock/AFFLIBv3/commit/aeb444da.patch?full_index=1"
-    sha256 "90cbb0b55a6e273df986b306d20e0cfb77a263cb85e272e01f1b0d8ee8bd37a0"
+  def python3
+    which("python3.11")
   end
 
   def install
-    python = Formula["python@3.10"].opt_bin/"python3.10"
-    ENV["PYTHON"] = python
+    # Fix build with Python 3.11 by regenerating cythonized file.
+    (buildpath/"pyaff/pyaff.c").unlink
+    site_packages = Language::Python.site_packages(python3)
+    ENV.prepend_path "PYTHONPATH", Formula["libcython"].opt_libexec/site_packages
 
-    args = %w[
-      --enable-s3
-      --enable-python
-      --disable-fuse
-    ]
-
+    ENV["PYTHON"] = python3
     system "autoreconf", "--force", "--install", "--verbose"
-    system "./configure", *std_configure_args, *args
+    system "./configure", *std_configure_args,
+                          "--enable-s3",
+                          "--enable-python",
+                          "--disable-fuse"
 
     # Prevent installation into HOMEBREW_PREFIX.
-    prefix_site_packages = prefix/Language::Python.site_packages(python)
     inreplace "pyaff/Makefile", "--single-version-externally-managed",
-                                "--install-lib=#{prefix_site_packages} \\0"
+                                "--install-lib=#{prefix/site_packages} \\0"
     system "make", "install"
   end
 
   test do
     system "#{bin}/affcat", "-v"
+    system python3, "-c", "import pyaff"
   end
 end
