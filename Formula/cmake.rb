1c377842db5846befc87184426b40b4582c3d1a2
Adrian Ho <the.gromgit@gmail.com>
2022-12-22 15:02:59 +0800

    cmake: update 3.25.1 bottle.

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -1,10 +1,10 @@
 class Cmake < Formula
   desc "Cross-platform make"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.25.0/cmake-3.25.0.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/cmake-3.25.0.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.25.0.tar.gz"
-  sha256 "306463f541555da0942e6f5a0736560f70c487178b9d94a5ae7f34d0538cdd48"
+  url "https://github.com/Kitware/CMake/releases/download/v3.25.1/cmake-3.25.1.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/cmake-3.25.1.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.25.1.tar.gz"
+  sha256 "1c511d09516af493694ed9baf13c55947a36389674d657a2d5e0ccedc6b291d8"
   license "BSD-3-Clause"
   head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
 
@@ -17,13 +17,13 @@ class Cmake < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cmake"
-    sha256 cellar: :any_skip_relocation, mojave: "69397dcb6985c0d3db961f6b01ffc5842607645867cd994f720dfdaa278bb575"
+    sha256 cellar: :any_skip_relocation, mojave: "af5e40107762e571101173402d1501fbccdd9f6dd7c5ceed07a1f614e344c7f5"
   end
 
   uses_from_macos "ncurses"
 
   on_linux do
-    depends_on "openssl@1.1"
+    depends_on "openssl@3"
   end
 
   # The completions were removed because of problems with system bash
