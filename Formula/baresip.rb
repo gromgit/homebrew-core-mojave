b30aa9a61a3f4ada5087e29d58ea26d937f29791
Adrian Ho <the.gromgit@gmail.com>
2022-12-22 19:37:15 +0800

    baresip: update 2.10.0 bottle.

--- Formula/baresip.rb
+++ Formula/baresip.rb
@@ -1,13 +1,13 @@
 class Baresip < Formula
   desc "Modular SIP useragent"
   homepage "https://github.com/baresip/baresip"
-  url "https://github.com/baresip/baresip/archive/v2.7.0.tar.gz"
-  sha256 "6bc3ac1b2a301b6de91a40079a9ec44545a00c57662ca0bdf2518fbb932ff181"
+  url "https://github.com/baresip/baresip/archive/refs/tags/v2.10.0.tar.gz"
+  sha256 "1bbeda0cb6c0f0480fb67f4fcc389df42384da7da909d9d950538fbbbfb92495"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/baresip"
-    sha256 mojave: "bb10b09d7e782040ab6cd09ffc969f14f263b7a6a825ffd9024d204f7b78dbe5"
+    sha256 mojave: "26b01d6e164a37ebd156c432408ad07ec33c4b3eeb9257d03e83e77170f3b763"
   end
 
   depends_on "libre"
@@ -16,20 +16,20 @@ class Baresip < Formula
   def install
     libre = Formula["libre"]
     librem = Formula["librem"]
-    # NOTE: `LIBRE_SO` is a directory but `LIBREM_SO` is a shared library.
     args = %W[
       PREFIX=#{prefix}
       LIBRE_MK=#{libre.opt_share}/re/re.mk
       LIBRE_INC=#{libre.opt_include}/re
       LIBRE_SO=#{libre.opt_lib}
       LIBREM_PATH=#{librem.opt_prefix}
-      LIBREM_SO=#{librem.opt_lib/shared_library("librem")}
+      LIBREM_SO=#{librem.opt_lib}
       MOD_AUTODETECT=
       USE_G711=1
       USE_OPENGL=1
       USE_STDIO=1
       USE_UUID=1
       HAVE_GETOPT=1
+      RELEASE=1
       V=1
     ]
     if OS.mac?
