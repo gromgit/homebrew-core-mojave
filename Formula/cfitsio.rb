3438b2cf111976e05d36b2a267f6e4d1bc1f09d7
Adrian Ho <the.gromgit@gmail.com>
2022-12-22 21:19:21 +0800

    cfitsio: update 4.2.0 bottle.

--- Formula/cfitsio.rb
+++ Formula/cfitsio.rb
@@ -1,8 +1,8 @@
 class Cfitsio < Formula
   desc "C access to FITS data files with optional Fortran wrappers"
   homepage "https://heasarc.gsfc.nasa.gov/docs/software/fitsio/fitsio.html"
-  url "https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-4.1.0.tar.gz"
-  sha256 "b367c695d2831958e7166921c3b356d5dfa51b1ecee505b97416ba39d1b6c17a"
+  url "https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-4.2.0.tar.gz"
+  sha256 "eba53d1b3f6e345632bb09a7b752ec7ced3d63ec5153a848380f3880c5d61889"
 
   livecheck do
     url :homepage
@@ -11,8 +11,7 @@ class Cfitsio < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cfitsio"
-    rebuild 2
-    sha256 cellar: :any, mojave: "427e277300bdc41d3cddac8629646bf29542cb5e4275d3fde4eb697d64473798"
+    sha256 cellar: :any, mojave: "e2940f67d431879a8e5b38583b654ea0f8c189a52afec93102aa1674285f1bf5"
   end
 
   uses_from_macos "zlib"
