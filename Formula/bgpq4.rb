e24fdc3c36fdcac2e5308c6e11ca4a756cf0be58
Adrian Ho <the.gromgit@gmail.com>
2022-12-22 19:44:14 +0800

    bgpq4: update 1.7 bottle.

--- Formula/bgpq4.rb
+++ Formula/bgpq4.rb
@@ -1,8 +1,8 @@
 class Bgpq4 < Formula
   desc "BGP filtering automation for Cisco, Juniper, BIRD and OpenBGPD routers"
   homepage "https://github.com/bgp/bgpq4"
-  url "https://github.com/bgp/bgpq4/archive/refs/tags/1.6.tar.gz"
-  sha256 "7780d4f2fd2f7aeb53ec5378e6e6602124bc6fb7ef8b372590869b82ac29b2df"
+  url "https://github.com/bgp/bgpq4/archive/refs/tags/1.7.tar.gz"
+  sha256 "c0c4a92f26577e6076248e46641862e251fec820ccf4c3c13a87e5987c5595f2"
   license "BSD-2-Clause"
   head "https://github.com/bgp/bgpq4.git", branch: "main"
 
@@ -13,7 +13,7 @@ class Bgpq4 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bgpq4"
-    sha256 cellar: :any_skip_relocation, mojave: "92ab23caee042087efa7547d84a2a58f90e41740323188179f7305eac1c0b040"
+    sha256 cellar: :any_skip_relocation, mojave: "1a96aedcb8547d1ac0336010b7f58d9fccc26ee3bb2a8004b54fc9526fdf57b4"
   end
 
   depends_on "autoconf" => :build
