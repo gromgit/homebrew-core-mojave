d27aba847379f37acf7735f27099eab72722d337
Adrian Ho <the.gromgit@gmail.com>
2022-12-22 19:32:54 +0800

    librem: update 2.10.0 bottle.

--- Formula/librem.rb
+++ Formula/librem.rb
@@ -1,13 +1,13 @@
 class Librem < Formula
   desc "Toolkit library for real-time audio and video processing"
   homepage "https://github.com/baresip/rem"
-  url "https://github.com/baresip/rem/archive/refs/tags/v2.8.0.tar.gz"
-  sha256 "7f2b4e8db0fbf2d8dc593fb3037d4752aecf3bf50658c3762fe53494cd508cee"
+  url "https://github.com/baresip/rem/archive/refs/tags/v2.10.0.tar.gz"
+  sha256 "82d417f9ece6cafdbfb1e342cf1c7cf4390136578dd7c77b4c7995cbbf4792a0"
   license "BSD-3-Clause"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/librem"
-    sha256 cellar: :any, mojave: "80ce337cd1c50070c24bf72498a501538de825346f6675a3c4dea158f13c9d97"
+    sha256 cellar: :any, mojave: "c0e01b3fb9643c439b19b37b4683b0a07418aab02961d5d32f0109bcd76b7526"
   end
 
   depends_on "libre"
