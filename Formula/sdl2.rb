7c1d0875c21972fcd24a5ba2ab4e8f3f2b62236a
Adrian Ho <the.gromgit@gmail.com>
2022-12-22 15:08:10 +0800

    sdl2: update 2.26.1 bottle.

--- Formula/sdl2.rb
+++ Formula/sdl2.rb
@@ -1,13 +1,19 @@
 class Sdl2 < Formula
   desc "Low-level access to audio, keyboard, mouse, joystick, and graphics"
   homepage "https://www.libsdl.org/"
-  url "https://github.com/libsdl-org/SDL/releases/download/release-2.26.0/SDL2-2.26.0.tar.gz"
-  sha256 "8000d7169febce93c84b6bdf376631f8179132fd69f7015d4dadb8b9c2bdb295"
+  url "https://github.com/libsdl-org/SDL/releases/download/release-2.26.1/SDL2-2.26.1.tar.gz"
+  sha256 "02537cc7ebd74071631038b237ec4bfbb3f4830ba019e569434da33f42373e04"
   license "Zlib"
+
+  livecheck do
+    url :stable
+    regex(%r{href=["']?[^"' >]*?/tag/release[._-](\d+(?:\.\d+)+)["' >]}i)
+    strategy :github_latest
+  end
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sdl2"
-    sha256 cellar: :any, mojave: "f57c449808f602f118502b0223ff7757b3914f31b078d15bb91696144e8ee336"
+    sha256 cellar: :any, mojave: "cd4c4ed4b5a11fa88a86c0f144f93b0ec9457002f9e78f071eeb46b4d94f95d1"
   end
 
   head do
