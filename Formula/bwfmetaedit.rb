edc13569342cf0c2729a1dcd52b585e03bfd5682
Adrian Ho <the.gromgit@gmail.com>
2022-12-22 21:04:11 +0800

    bwfmetaedit: update 22.11 bottle.

--- Formula/bwfmetaedit.rb
+++ Formula/bwfmetaedit.rb
@@ -1,8 +1,8 @@
 class Bwfmetaedit < Formula
   desc "Tool for embedding, validating, and exporting BWF file metadata"
   homepage "https://mediaarea.net/BWFMetaEdit"
-  url "https://mediaarea.net/download/binary/bwfmetaedit/21.07/BWFMetaEdit_CLI_21.07_GNU_FromSource.tar.bz2"
-  sha256 "7b55d9c6df989a3cbff2bb2c9a18b0b0f3a80fa716539a160d612194906d94f2"
+  url "https://mediaarea.net/download/binary/bwfmetaedit/22.11/BWFMetaEdit_CLI_22.11_GNU_FromSource.tar.bz2"
+  sha256 "82ddb459b5c624dd1debf03e2a21758c514fbcebdea69cdd94364f777751f897"
   license "0BSD"
 
   livecheck do
@@ -11,15 +11,8 @@ class Bwfmetaedit < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "c0a9a3927c8b531621556b0467f1f394450d8fc466b0863eed6d10fc50a87e93"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "55db76b2cdd9dea54d82e644f9de1c5f5ca53cc552602115ab45704f30aef612"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "990843dbcb7a326453234dcdedb6af987be548adecc3edca6dba0086c16d689d"
-    sha256 cellar: :any_skip_relocation, ventura:        "629b2045d3acac2947e07e9c57eeaa6b3ea307ae411735316ad3bb6b25acda92"
-    sha256 cellar: :any_skip_relocation, monterey:       "79b70c2fe4f006d8997f05cc548148682050712366f6c26cdb90bba21e40bb42"
-    sha256 cellar: :any_skip_relocation, big_sur:        "bc4935bced4e3226e4e0fb61dbf8821ee25fb36126c494e4e93f084728032be3"
-    sha256 cellar: :any_skip_relocation, catalina:       "c5168b4682ae67f6ad9618761706b4589d6ad45fb55df851602dab205a8a7299"
-    sha256 cellar: :any_skip_relocation, mojave:         "836a3ad812aaeb0128ac0e3fe75eee3d33c6f2c2a98f92138af02cf74173c665"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "08a47ae2efd7c64a5a5eb5cc9ecdd2046d0b0f20169257af529dd9ae0e3891d3"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bwfmetaedit"
+    sha256 cellar: :any_skip_relocation, mojave: "0b828a6584298e5c10ebffd6f2c9d9e5b1c2c1400ccb03530392eff861e9f312"
   end
 
   def install
