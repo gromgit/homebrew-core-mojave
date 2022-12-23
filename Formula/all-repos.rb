6318c9d404eff752743191fe2845fee195b92d69
Adrian Ho <the.gromgit@gmail.com>
2022-12-23 16:29:34 +0800

    all-repos: update 1.23.1 bottle.

--- Formula/all-repos.rb
+++ Formula/all-repos.rb
@@ -3,20 +3,20 @@ class AllRepos < Formula
 
   desc "Clone all your repositories and apply sweeping changes"
   homepage "https://github.com/asottile/all-repos"
-  url "https://files.pythonhosted.org/packages/b7/4c/e99264b9f39e447e5077840037c8bb9e7ef7cd49de4c8646654b83dbbdfb/all_repos-1.23.0.tar.gz"
-  sha256 "5ecba787dfbacd45632cfcba53ab01a1d81a52633ceb7555a0f81d1325fdcc70"
+  url "https://files.pythonhosted.org/packages/8e/b0/3777ac29c61c256aba07fcaa2a3c3a32b0bf5cf6f7eb6d800ea1e7809510/all_repos-1.23.1.tar.gz"
+  sha256 "6891c738651f4e0963a67f7ce689fe5efe3ba53b32c39b781da1e2e51decff6d"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/all-repos"
-    sha256 cellar: :any_skip_relocation, mojave: "d4924a02371c783d2ad14653aeb8c23baf772657a7ec7c4e206f84fc74156b43"
+    sha256 cellar: :any_skip_relocation, mojave: "b92e9d8aec632564a9281e563a853597fdde62485e9ad472de6523dcf455e5e6"
   end
 
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   resource "identify" do
-    url "https://files.pythonhosted.org/packages/e5/8e/408d590e26fbc75a2e974aa1103d95a3ffef014209967f66f491306c4824/identify-2.5.1.tar.gz"
-    sha256 "3d11b16f3fe19f52039fb7e39c9c884b21cb1b586988114fbe42671f03de3e82"
+    url "https://files.pythonhosted.org/packages/67/e1/869d7b8df41a3ac2a3c74a2a4ba401df468044dccc489b8937aad40d148e/identify-2.5.8.tar.gz"
+    sha256 "7a214a10313b9489a0d61467db2856ae8d0b8306fc923e03a9effa53d8aedc58"
   end
 
   def install
@@ -26,7 +26,7 @@ class AllRepos < Formula
   test do
     (testpath/"all-repos.json").write <<~EOS
       {
-        "output_dir": ".",
+        "output_dir": "out",
         "source": "all_repos.source.json_file",
         "source_settings": {"filename": "repos.json"},
         "push": "all_repos.push.readonly",
@@ -39,8 +39,8 @@ class AllRepos < Formula
     EOS
 
     system "all-repos-clone"
-    assert_predicate testpath/"discussions", :exist?
+    assert_predicate testpath/"out/discussions", :exist?
     output = shell_output("#{bin}/all-repos-grep discussions")
-    assert_match "./discussions:README.md", output
+    assert_match "out/discussions:README.md", output
   end
 end
