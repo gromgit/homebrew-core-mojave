bcd54a239e0d9a76a1dcc8370765c1b425ec59e5
Adrian Ho <the.gromgit@gmail.com>
2022-12-24 02:40:00 +0800

    argocd-vault-plugin: update 1.13.1 bottle.

--- Formula/argocd-vault-plugin.rb
+++ Formula/argocd-vault-plugin.rb
@@ -2,14 +2,13 @@ class ArgocdVaultPlugin < Formula
   desc "Argo CD plugin to retrieve secrets from Secret Management tools"
   homepage "https://argocd-vault-plugin.readthedocs.io"
   url "https://github.com/argoproj-labs/argocd-vault-plugin.git",
-      tag:      "v1.13.0",
-      revision: "6866b7206719e9b5b2ea4d7cb870e18f76534637"
+      tag:      "v1.13.1",
+      revision: "9eabb455a194a3f3c4d235c16cdc84e707f948a6"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/argocd-vault-plugin"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "d0be7c5a8c2ee751fa3420ffdafd75c088b72d3abb031c9756c56693e7243469"
+    sha256 cellar: :any_skip_relocation, mojave: "a62fa470aacd75c48132796e14bf31a157b1d5109a6888e7a182d904388b8416"
   end
 
   depends_on "go" => :build
