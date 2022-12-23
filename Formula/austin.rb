154dc3ab669a180ee0202e499f0de5ed57a03824
Adrian Ho <the.gromgit@gmail.com>
2022-12-24 06:44:17 +0800

    austin: update 3.4.1 bottle.

--- Formula/austin.rb
+++ Formula/austin.rb
@@ -1,31 +1,30 @@
 class Austin < Formula
   desc "Python frame stack sampler for CPython"
   homepage "https://github.com/P403n1x87/austin"
-  url "https://github.com/P403n1x87/austin/archive/v3.3.0.tar.gz"
-  sha256 "a0dcfee0dffecb00b85a84f3c7befff7e61fd4b504228c1e6ce7bc5af9790506"
+  url "https://github.com/P403n1x87/austin/archive/v3.4.1.tar.gz"
+  sha256 "e668af1172f0c2f8740bd7d2eed6613e916e97a7cc88aa6b0cf8420055c2bcc1"
   license "GPL-3.0-or-later"
   head "https://github.com/P403n1x87/austin.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/austin"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "4c128cc47040dfc6448fb280f3e5015de0134a6fcb5c8c724d10ec1392cf9608"
+    sha256 cellar: :any_skip_relocation, mojave: "447617e3bcfd434b8790e07433b0ce306801823f5b5e5b9f3a5a99dfa550089f"
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "python@3.10" => :test
+  depends_on "python@3.11" => :test
 
   def install
     system "autoreconf", "--install"
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", *std_configure_args, "--disable-silent-rules"
     system "make"
     system "make", "install"
     man1.install "src/austin.1"
   end
 
   test do
-    python = Formula["python@3.10"].opt_bin/"python3.10"
-    shell_output(bin/"austin #{python} -c \"from time import sleep; sleep(1)\"", 37)
+    python3 = "python3.11"
+    shell_output(bin/"austin #{python3} -c \"from time import sleep; sleep(1)\"", 37)
   end
 end
