d7dacc29db4d71749a9c36eb2c034f91e52ae4f1
Adrian Ho <the.gromgit@gmail.com>
2022-12-24 03:58:15 +0800

    asroute: update 0.1.0 bottle.

--- Formula/asroute.rb
+++ Formula/asroute.rb
@@ -7,15 +7,9 @@ class Asroute < Formula
   head "https://github.com/stevenpack/asroute.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "95fed5d2399c60c38bf16a397ed05407d9bce377a88634cc6e2ec947772c20c7"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "de290cf1ef3bef31bef256e64ddc4d7446fb2d81bcdd553660cb65baeb2db425"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "816be1190f677bb1ba13d1b1fd92a0ca7550341810310b59be200227936afc9c"
-    sha256 cellar: :any_skip_relocation, monterey:       "b166f13f96b3c06f632e3924b1c0a03ab0dfc0d3b3ade5e9b58599dccbf2e4ed"
-    sha256 cellar: :any_skip_relocation, big_sur:        "dded44d666d600e065f0a49e0c26eaa56597f1235ff937703c8dff298f665453"
-    sha256 cellar: :any_skip_relocation, catalina:       "873d80ef73a84598637218e467a0887477e113a9f26d8dd1f1a4b6a4571b11b8"
-    sha256 cellar: :any_skip_relocation, mojave:         "6dbd83956cb0d73b74fd8fa6706206c7d9701eeb6a44f0e6eebcaebd9b96fbc2"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "77fd60fff4aa4abf7e9fd7bb7e14961b3eaab1aae0f074a318d874ecd869d32b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b5551363cc3debab97b447917844cee65766c8c3833f3d853738e09d11418766"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/asroute"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "7b507139543b1698272caedfa7f40804c4321d485dfa79529996b6d352b0b127"
   end
 
   depends_on "rust" => :build
