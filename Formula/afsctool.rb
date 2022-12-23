06b124d2483763e8d0245cc4e840aff12e9c38d9
Adrian Ho <the.gromgit@gmail.com>
2022-12-23 12:36:28 +0800

    afsctool: update 1.7.3 bottle.

--- Formula/afsctool.rb
+++ Formula/afsctool.rb
@@ -1,14 +1,14 @@
 class Afsctool < Formula
   desc "Utility for manipulating APFS and ZFS compressed files"
   homepage "https://brkirch.wordpress.com/afsctool/"
-  url "https://github.com/RJVB/afsctool/archive/refs/tags/1.7.2.tar.gz"
-  sha256 "a0d01953e36a333c29369a126a81b905b70a5603533caeebc2f04bbd3aa1b0df"
+  url "https://github.com/RJVB/afsctool/archive/refs/tags/v1.7.3.tar.gz"
+  sha256 "5776ff5aaf05c513bead107536d9e98e6037019a0de8a1435cc9da89ea8d49b8"
   license all_of: ["GPL-3.0-only", "BSL-1.0"]
   head "https://github.com/RJVB/afsctool.git"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/afsctool"
-    sha256 cellar: :any_skip_relocation, mojave: "0655c23a93444b72e7c734237442cd4f7b5e96fde110a0a5d029864c59ccb59c"
+    sha256 cellar: :any_skip_relocation, mojave: "8bcf62e25d7e296fdc797d932e467f0dc65f220850872db239725eb3a126376c"
   end
 
   depends_on "cmake" => :build
