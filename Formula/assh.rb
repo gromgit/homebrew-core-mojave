2f5efd1de537933fbe2d9f6b2d7e170cbe7f19d9
Adrian Ho <the.gromgit@gmail.com>
2022-12-24 04:08:09 +0800

    assh: update 2.15.0 bottle.

--- Formula/assh.rb
+++ Formula/assh.rb
@@ -1,15 +1,14 @@
 class Assh < Formula
   desc "Advanced SSH config - Regex, aliases, gateways, includes and dynamic hosts"
   homepage "https://manfred.life/assh"
-  url "https://github.com/moul/assh/archive/v2.14.0.tar.gz"
-  sha256 "711721e2aece3b4d39c170924db8bf0f82c75690811d8b50f2310ccb2aa6b07f"
+  url "https://github.com/moul/assh/archive/v2.15.0.tar.gz"
+  sha256 "ff80cb7dc818af1bd2d7a031058cb2d4074b0f4af6f7d3077619901689744387"
   license "MIT"
   head "https://github.com/moul/assh.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/assh"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "d0ce56d489f6fc6f815604008f5d6c5fcd61080e2ad2efc243c0fc4ca87af175"
+    sha256 cellar: :any_skip_relocation, mojave: "b8904eda8314423761821a4e7320bce4458f08ce32886d159e4d00749b4e01ca"
   end
 
   depends_on "go" => :build
