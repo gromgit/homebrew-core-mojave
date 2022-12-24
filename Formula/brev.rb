b7896d94fee65a11dff5e72520e785079593f161
Adrian Ho <the.gromgit@gmail.com>
2022-12-25 02:35:19 +0800

    brev: update 0.6.197 bottle.

--- Formula/brev.rb
+++ Formula/brev.rb
@@ -1,8 +1,8 @@
 class Brev < Formula
   desc "CLI tool for managing workspaces provided by brev.dev"
   homepage "https://docs.brev.dev"
-  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.118.tar.gz"
-  sha256 "1d0ec8807a44e1117c35045dc0b414ee3c0404e610cb06a4e35b980fa0bb80ea"
+  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.197.tar.gz"
+  sha256 "1a25e9e6935c3dbbd85a5fdee823bf0f2c63d00ca857d82c84d4cd9bc93900d6"
   license "MIT"
 
   livecheck do
@@ -12,15 +12,13 @@ class Brev < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/brev"
-    sha256 cellar: :any_skip_relocation, mojave: "c3790f259eadb5adab6e4cdb1114d9cec5113d65e99528254c99755adf540c01"
+    sha256 cellar: :any_skip_relocation, mojave: "2e218ec60799675a9bb7d716074d510ed5ba09cceb626d64dd6fb0d89654751e"
   end
 
-  # Required latest gvisor.dev/gvisor/pkg/gohacks instead of inet.af/netstack/gohacks
-  # Try to switch to the latest go on the next release
-  depends_on "go@1.18" => :build
+  depends_on "go" => :build
 
   def install
-    ldflags = "-X github.com/brevdev/brev-cli/pkg/cmd/version.Version=v#{version}"
+    ldflags = "-s -w -X github.com/brevdev/brev-cli/pkg/cmd/version.Version=v#{version}"
     system "go", "build", *std_go_args(ldflags: ldflags)
 
     generate_completions_from_executable(bin/"brev", "completion")
