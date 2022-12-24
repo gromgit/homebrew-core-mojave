9d6125f182db78bd5e01c6565e50782a3cd93508
Adrian Ho <the.gromgit@gmail.com>
2022-12-25 02:58:29 +0800

    buf: update 1.11.0 bottle.

---
+++ Formula/buf.rb
@@ -0,0 +1,57 @@
+class Buf < Formula
+  desc "New way of working with Protocol Buffers"
+  homepage "https://github.com/bufbuild/buf"
+  url "https://github.com/bufbuild/buf/archive/refs/tags/v1.11.0.tar.gz"
+  sha256 "1bbeeaf741105bcafca8f0c59140cc60c237970c375cbd8e72ac9eb932fc2fc5"
+  license "Apache-2.0"
+  head "https://github.com/bufbuild/buf.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/buf"
+    sha256 cellar: :any_skip_relocation, mojave: "27190030cc76a300c9742c9d11f9dc582346bf919627bb172528fffedb691c6b"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    %w[buf protoc-gen-buf-breaking protoc-gen-buf-lint].each do |name|
+      system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/name), "./cmd/#{name}"
+    end
+
+    generate_completions_from_executable(bin/"buf", "completion")
+    man1.mkpath
+    system bin/"buf", "manpages", man1
+  end
+
+  test do
+    (testpath/"invalidFileName.proto").write <<~EOS
+      syntax = "proto3";
+      package examplepb;
+    EOS
+
+    (testpath/"buf.yaml").write <<~EOS
+      version: v1
+      name: buf.build/bufbuild/buf
+      lint:
+        use:
+          - DEFAULT
+          - UNARY_RPC
+      breaking:
+        use:
+          - FILE
+        ignore_unstable_packages: true
+    EOS
+
+    expected = <<~EOS
+      invalidFileName.proto:1:1:Filename "invalidFileName.proto" should be \
+      lower_snake_case.proto, such as "invalid_file_name.proto".
+      invalidFileName.proto:2:1:Files with package "examplepb" must be within \
+      a directory "examplepb" relative to root but were in directory ".".
+      invalidFileName.proto:2:1:Package name "examplepb" should be suffixed \
+      with a correctly formed version, such as "examplepb.v1".
+    EOS
+    assert_equal expected, shell_output("#{bin}/buf lint invalidFileName.proto 2>&1", 100)
+
+    assert_match version.to_s, shell_output("#{bin}/buf --version")
+  end
+end
