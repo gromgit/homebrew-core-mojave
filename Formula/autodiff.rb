d82b97e539e6daccdfa6234fe6b001641c0f685b
Adrian Ho <the.gromgit@gmail.com>
2022-12-24 12:06:47 +0800

    autodiff: update 0.6.12 bottle.

--- Formula/autodiff.rb
+++ Formula/autodiff.rb
@@ -1,27 +1,34 @@
 class Autodiff < Formula
   desc "Automatic differentiation made easier for C++"
   homepage "https://autodiff.github.io"
-  url "https://github.com/autodiff/autodiff/archive/v0.6.11.tar.gz"
-  sha256 "ac7a52387a10ecb8ba77ce5385ffb23893ff9a623467b4392bd204422a3b5c09"
+  url "https://github.com/autodiff/autodiff/archive/v0.6.12.tar.gz"
+  sha256 "3e9d667b81bba8e43bbe240a0321e25f4be248d1761097718664445306882dcc"
   license "MIT"
-  head "https://github.com/autodiff/autodiff.git", branch: "master"
+  head "https://github.com/autodiff/autodiff.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/autodiff"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "7d3b07bc3c8d4d3c8a7ba47b5923a1da5f8b52f3301eaca755c0574d666f9222"
+    sha256 cellar: :any_skip_relocation, mojave: "f6b6761f88e5ee808dcfdf930638b729058e7baf8adb034f9609757339fdb929"
   end
 
   depends_on "cmake" => :build
-  depends_on "python@3.10" => :build
+  depends_on "python@3.11" => [:build, :test]
   depends_on "eigen"
   depends_on "pybind11"
 
   fails_with gcc: "5"
 
+  def python3
+    "python3.11"
+  end
+
   def install
-    system "cmake", ".", *std_cmake_args, "-DAUTODIFF_BUILD_TESTS=off"
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "_build",
+                    "-DAUTODIFF_BUILD_TESTS=OFF",
+                    "-DPYTHON_EXECUTABLE=#{which(python3)}",
+                    *std_cmake_args
+    system "cmake", "--build", "_build"
+    system "cmake", "--install", "_build"
     (pkgshare/"test").install "examples/forward/example-forward-single-variable-function.cpp" => "forward.cpp"
     (pkgshare/"test").install "examples/reverse/example-reverse-single-variable-function.cpp" => "reverse.cpp"
   end
@@ -33,5 +40,6 @@ class Autodiff < Formula
                     "-I#{include}", "-I#{Formula["eigen"].opt_include}", "-o", "reverse"
     assert_match "u = 8.19315\ndu/dx = 5.25\n", shell_output(testpath/"forward")
     assert_match "u = 8.19315\nux = 5.25\n", shell_output(testpath/"reverse")
+    system python3, "-c", "import autodiff"
   end
 end
