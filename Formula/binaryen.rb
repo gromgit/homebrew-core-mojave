59dd495b06a17222d57bff60faa65a37f35f11ba
Adrian Ho <the.gromgit@gmail.com>
2022-12-25 00:06:47 +0800

    binaryen: update 111 bottle.

--- Formula/binaryen.rb
+++ Formula/binaryen.rb
@@ -1,31 +1,34 @@
 class Binaryen < Formula
   desc "Compiler infrastructure and toolchain library for WebAssembly"
   homepage "https://webassembly.org/"
-  url "https://github.com/WebAssembly/binaryen/archive/version_110.tar.gz"
-  sha256 "0f80250a02b94dd81bdedeae029eb805abf607fcdadcfee5ca8b5e6b77035601"
+  url "https://github.com/WebAssembly/binaryen/archive/version_111.tar.gz"
+  sha256 "c518b1146713bc2406dd09a1a1b18f31a283f8ac1fe200efa38834dbd95a1997"
   license "Apache-2.0"
   head "https://github.com/WebAssembly/binaryen.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/binaryen"
-    rebuild 1
-    sha256 cellar: :any, mojave: "ec1eecfbec6cc2333a5803ec97272cf45839a8e9d33ae7690abb8b1c4dea2403"
+    sha256 cellar: :any, mojave: "3f38923a5bd552ba248f631ace0f4025e2b775cd7a737c47d7e101de7c067b1e"
   end
 
   depends_on "cmake" => :build
-  depends_on "python@3.10" => :build
+  depends_on macos: :mojave # needs std::variant
 
-  fails_with gcc: "5"
+  fails_with :gcc do
+    version "6"
+    cause "needs std::variant"
+  end
 
   def install
-    system "cmake", ".", *std_cmake_args, "-DBUILD_TESTS=false"
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DBUILD_TESTS=false"
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
 
     pkgshare.install "test/"
   end
 
   test do
-    system "#{bin}/wasm-opt", "-O", "#{pkgshare}/test/passes/O1_print-stack-ir.wast", "-o", "1.wast"
-    assert_match "stacky-help", File.read("1.wast")
+    system bin/"wasm-opt", "-O", pkgshare/"test/passes/O1_print-stack-ir.wast", "-o", "1.wast"
+    assert_match "stacky-help", (testpath/"1.wast").read
   end
 end
