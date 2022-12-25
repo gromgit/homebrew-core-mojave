4829fcb92d2e5f7d373842f7301dafd9f226aaa6
Adrian Ho <the.gromgit@gmail.com>
2022-12-25 18:11:01 +0800

    clang-format: update 15.0.6 bottle.

--- Formula/clang-format.rb
+++ Formula/clang-format.rb
@@ -7,12 +7,17 @@ class ClangFormat < Formula
   head "https://github.com/llvm/llvm-project.git", branch: "main"
 
   stable do
-    url "https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.6/llvm-14.0.6.src.tar.xz"
-    sha256 "050922ecaaca5781fdf6631ea92bc715183f202f9d2f15147226f023414f619a"
+    url "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.6/llvm-15.0.6.src.tar.xz"
+    sha256 "0b32199401f27e2e0353846a8c5fbadd77beca2570654fb9ef7ac9b7f26967e3"
 
     resource "clang" do
-      url "https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.6/clang-14.0.6.src.tar.xz"
-      sha256 "2b5847b6a63118b9efe5c85548363c81ffe096b66c3b3675e953e26342ae4031"
+      url "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.6/clang-15.0.6.src.tar.xz"
+      sha256 "10119ae195f1b4f979fe42e67b781e175b0c0d4e982fd6a2f44c4aa7fc925233"
+    end
+
+    resource "cmake" do
+      url "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.6/cmake-15.0.6.src.tar.xz"
+      sha256 "7613aeeaba9b8b12b35224044bc349b5fa45525919625057fa54dc882dcb4c86"
     end
   end
 
@@ -24,7 +29,7 @@ class ClangFormat < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/clang-format"
-    sha256 cellar: :any_skip_relocation, mojave: "f7de8456451e580b04260a143a4887e48f1c4b206e89983a55a9f5593f0912ee"
+    sha256 cellar: :any, mojave: "d0a19fc1fc4b8851f97c0d0982ffe62fb0533462890f1b5b41885886c9964bcd"
   end
 
   depends_on "cmake" => :build
@@ -44,11 +49,11 @@ class ClangFormat < Formula
 
       buildpath/"llvm"
     else
-      resource("clang").stage do |r|
-        (buildpath/"llvm-#{version}.src/tools/clang").install Pathname("clang-#{r.version}.src").children
-      end
+      (buildpath/"src").install buildpath.children
+      (buildpath/"src/tools/clang").install resource("clang")
+      (buildpath/"cmake").install resource("cmake")
 
-      buildpath/"llvm-#{version}.src"
+      buildpath/"src"
     end
 
     system "cmake", "-S", llvmpath, "-B", "build",
@@ -57,10 +62,8 @@ class ClangFormat < Formula
                     *std_cmake_args
     system "cmake", "--build", "build", "--target", "clang-format"
 
-    git_clang_format = llvmpath/"tools/clang/tools/clang-format/git-clang-format"
-    inreplace git_clang_format, %r{^#!/usr/bin/env python$}, "#!/usr/bin/env python3"
-
-    bin.install "build/bin/clang-format", git_clang_format
+    bin.install "build/bin/clang-format"
+    bin.install llvmpath/"tools/clang/tools/clang-format/git-clang-format"
     (share/"clang").install llvmpath.glob("tools/clang/tools/clang-format/clang-format*")
   end
 
@@ -78,6 +81,6 @@ class ClangFormat < Formula
         shell_output("#{bin}/clang-format -style=Google test.c")
 
     ENV.prepend_path "PATH", bin
-    assert_match "test.c", shell_output("git clang-format")
+    assert_match "test.c", shell_output("git clang-format", 1)
   end
 end
