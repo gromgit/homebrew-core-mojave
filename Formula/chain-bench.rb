8390537e6a0e8362444328304ed61b23f5f00f87
Adrian Ho <the.gromgit@gmail.com>
2022-12-25 14:50:49 +0800

    chain-bench: update 0.1.7 bottle.

--- Formula/chain-bench.rb
+++ Formula/chain-bench.rb
@@ -1,15 +1,14 @@
 class ChainBench < Formula
   desc "Software supply chain auditing tool based on CIS benchmark"
   homepage "https://github.com/aquasecurity/chain-bench"
-  url "https://github.com/aquasecurity/chain-bench/archive/v0.1.4.tar.gz"
-  sha256 "b75e3e1f5eba97d4d8a29a476ea4fca4a0f354ceb232028efdb50a19ebdc5afc"
+  url "https://github.com/aquasecurity/chain-bench/archive/v0.1.7.tar.gz"
+  sha256 "bc42551bde3ce61500d0eea3f4443e29bc2c5d86b1f01e87f929c46ce308ca9a"
   license "Apache-2.0"
   head "https://github.com/aquasecurity/chain-bench.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/chain-bench"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "7663d0c808b00e24f592c0256d0c46128fdd9cd8387d8d34d2c016305dc3de45"
+    sha256 cellar: :any_skip_relocation, mojave: "cfc88728b547bb622ee760b1041bf8809f19fbad3f0afdc53bf06ecafb495803"
   end
 
   depends_on "go" => :build
@@ -21,7 +20,8 @@ class ChainBench < Formula
   end
 
   test do
-    assert_match("Fetch Starting", shell_output("#{bin}/chain-bench scan", 1))
+    repo_url = "https://github.com/Homebrew/homebrew-core"
+    assert_match "Fetch Starting", shell_output("#{bin}/chain-bench scan --repository-url #{repo_url}")
 
     assert_match version.to_s, shell_output("#{bin}/chain-bench --version")
   end
