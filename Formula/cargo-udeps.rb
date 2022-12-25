ed0dc40c2c3f5d06b2d01fa1e67d33a3fcad30f4
Adrian Ho <the.gromgit@gmail.com>
2022-12-25 13:09:27 +0800

    cargo-udeps: update 0.1.35 bottle.

--- Formula/cargo-udeps.rb
+++ Formula/cargo-udeps.rb
@@ -1,13 +1,13 @@
 class CargoUdeps < Formula
   desc "Find unused dependencies in Cargo.toml"
   homepage "https://github.com/est31/cargo-udeps"
-  url "https://github.com/est31/cargo-udeps/archive/refs/tags/v0.1.33.tar.gz"
-  sha256 "edb369e61b658f68561be93a88634e0ee7322175b4c19627b601d67a3417fd85"
+  url "https://github.com/est31/cargo-udeps/archive/refs/tags/v0.1.35.tar.gz"
+  sha256 "43a15acf51dfc053746cfc362d0c236d2c1b215067a6ff921bca74b9e4dbf554"
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cargo-udeps"
-    sha256 cellar: :any, mojave: "8b7e22cc3fb0cb1215718bc67224c50d73a9bdeef6af2a10681c0a50bc0b7a5a"
+    sha256 cellar: :any, mojave: "0e95030298f3954ed14751575b64b5ba5ea8b5b6c22276df96dc899efca8af7f"
   end
 
   depends_on "rust" => [:build, :test]
