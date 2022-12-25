d813adbf6c094e44c5a2ed67b3803fc38b6d73b0
Adrian Ho <the.gromgit@gmail.com>
2022-12-25 16:14:34 +0800

    cheat: update 4.4.0 bottle.

--- Formula/cheat.rb
+++ Formula/cheat.rb
@@ -1,13 +1,13 @@
 class Cheat < Formula
   desc "Create and view interactive cheat sheets for *nix commands"
   homepage "https://github.com/cheat/cheat"
-  url "https://github.com/cheat/cheat/archive/refs/tags/4.3.3.tar.gz"
-  sha256 "6a1739b71d436f45dc7c028ec79863a34e30cc13da7159bf23604b77f43faaf2"
+  url "https://github.com/cheat/cheat/archive/refs/tags/4.4.0.tar.gz"
+  sha256 "8694d75896dcb1dfb91ed95ec37f7fe409ad2bde76e66f80b20be24ee92ae3ec"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cheat"
-    sha256 cellar: :any_skip_relocation, mojave: "40562182d1348c670edc426a77e2636ee459d8f5d8f3be27b9e2cb34fee0c27d"
+    sha256 cellar: :any_skip_relocation, mojave: "1e0834f62a19e0763ea5b0b8cd77694353b0fdf7778423995b528c8b811cc391"
   end
 
   depends_on "go" => :build
