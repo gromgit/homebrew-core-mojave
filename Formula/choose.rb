a4080daba3521dc341f0d569477c9b62925e6a00
Adrian Ho <the.gromgit@gmail.com>
2022-12-25 16:45:18 +0800

    choose: update 0.1.0_4 bottle.

--- Formula/choose.rb
+++ Formula/choose.rb
@@ -1,4 +1,6 @@
 class Choose < Formula
+  include Language::Python::Shebang
+
   desc "Make choices on the command-line"
   homepage "https://github.com/geier/choose"
   url "https://github.com/geier/choose/archive/v0.1.0.tar.gz"
@@ -8,17 +10,12 @@ class Choose < Formula
   head "https://github.com/geier/choose.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "5ac0cdbe50a8545762d80f5cb75a3ddb88c255bbfe5d6b556f6b078d6aa1046c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8cc4e4eaabc3341ed4728d11d5cee96f7954d6d24bef173d1848884581ddf1f9"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2b715be8453394e1f1931994780ad42075c05d483d24257ef1226054ff2b450e"
-    sha256 cellar: :any_skip_relocation, monterey:       "ed6b3c62ea97cf196ce3843bb855c75539e756e6a582d48957cce31286a0626d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b22b7b549f0f9621163fc1b6b5923a2de0f79177686e8747ffb1b71201e979d7"
-    sha256 cellar: :any_skip_relocation, catalina:       "37aa7b900d689cedd0c834703663f782082f66595d9b84f27405180832311385"
-    sha256 cellar: :any_skip_relocation, mojave:         "9d2fb7796ec1e3b55838d9fd932f91dae4c3268f343d1d8cba2cac7ac77b04da"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9241612ee60fcabf7fa8471843592f7063fb28f32a92e12dad97c1ab8774041f"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/choose"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "ac709f20ecd778f4d02546e222fdf956dce66615e09aca7e348e62bc7e25a179"
   end
 
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   conflicts_with "choose-gui", because: "both install a `choose` binary"
   conflicts_with "choose-rust", because: "both install a `choose` binary"
@@ -29,7 +26,7 @@ class Choose < Formula
   end
 
   def install
-    python3 = "python3.10"
+    python3 = "python3.11"
     ENV.prepend_create_path "PYTHONPATH", libexec/Language::Python.site_packages(python3)
 
     resource("urwid").stage do
@@ -37,14 +34,16 @@ class Choose < Formula
     end
 
     bin.install "choose"
-
+    rewrite_shebang detected_python_shebang, bin/"choose"
     bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
   end
 
   test do
-    # There isn't really a better test than that the executable exists
-    # and is executable because you can't run it without producing an
-    # interactive selection ui.
     assert_predicate bin/"choose", :executable?
+
+    # [Errno 6] No such device or address: '/dev/tty'
+    return if OS.linux? && ENV["HOMEBREW_GITHUB_ACTIONS"]
+
+    assert_equal "homebrew-test", pipe_output(bin/"choose", "homebrew-test\n").strip
   end
 end
