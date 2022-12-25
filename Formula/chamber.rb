c5a6f14db5f8313c299bb43a0c21db3f0bc46a54
Adrian Ho <the.gromgit@gmail.com>
2022-12-25 15:33:34 +0800

    chamber: update 2.11.0 bottle.

--- Formula/chamber.rb
+++ Formula/chamber.rb
@@ -1,8 +1,8 @@
 class Chamber < Formula
   desc "CLI for managing secrets through AWS SSM Parameter Store"
   homepage "https://github.com/segmentio/chamber"
-  url "https://github.com/segmentio/chamber/archive/v2.10.12.tar.gz"
-  sha256 "7766ef7f84b8fe09aaad13b5bbe2256b05fd1e44cac6279de5051ecfb9cc5c66"
+  url "https://github.com/segmentio/chamber/archive/v2.11.0.tar.gz"
+  sha256 "1d4138752fa168e05a24dec5c60be4fd564c62a414b99c47d523159f6efd2adf"
   license "MIT"
   head "https://github.com/segmentio/chamber.git", branch: "master"
 
@@ -14,8 +14,7 @@ class Chamber < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/chamber"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "0d60b1eac00b47ae004e9dd8f0f8a99a35d6d4fad4f30f1f942b601214a3bef9"
+    sha256 cellar: :any_skip_relocation, mojave: "bc17931f5ff2a05a486eccc2b0831a7eab67b51cbc83c766d51d1190342f5ea5"
   end
 
   depends_on "go" => :build
