ce9204b3e9cc2f49a23d458eee1b21ca45302a84
Adrian Ho <the.gromgit@gmail.com>
2022-12-25 13:28:17 +0800

    cassowary: update 0.16.0 bottle.

--- Formula/cassowary.rb
+++ Formula/cassowary.rb
@@ -1,14 +1,14 @@
 class Cassowary < Formula
   desc "Modern cross-platform HTTP load-testing tool written in Go"
   homepage "https://github.com/rogerwelin/cassowary"
-  url "https://github.com/rogerwelin/cassowary/archive/refs/tags/v0.14.1.tar.gz"
-  sha256 "1f10e23af218d661e8493e111d425da0ef6f4408d845a473fdbaf45dd6e2d94d"
+  url "https://github.com/rogerwelin/cassowary/archive/refs/tags/v0.16.0.tar.gz"
+  sha256 "672981232e2ae859f831de5d3e5a9f0c749739bcc41c0b17d511ca186ff56b93"
   license "MIT"
   head "https://github.com/rogerwelin/cassowary.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cassowary"
-    sha256 cellar: :any_skip_relocation, mojave: "e8dcda4ce51786551dbd2158dae8b8b35b9e9ed725e23b7c39f58cf5b4eb1db3"
+    sha256 cellar: :any_skip_relocation, mojave: "b695cd6f28fcb11d10d78b3adb5d53de1e400218fe6ca0f772966f57cd257f00"
   end
 
   depends_on "go" => :build
