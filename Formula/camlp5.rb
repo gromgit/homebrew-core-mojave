135a550e93ea6ae5e72412b85b7075ed6e5b992f
Adrian Ho <the.gromgit@gmail.com>
2022-12-25 11:09:52 +0800

    camlp5: update 8.00.04 bottle.

--- Formula/camlp5.rb
+++ Formula/camlp5.rb
@@ -1,10 +1,9 @@
 class Camlp5 < Formula
   desc "Preprocessor and pretty-printer for OCaml"
   homepage "https://camlp5.github.io/"
-  url "https://github.com/camlp5/camlp5/archive/refs/tags/rel8.00.03.tar.gz"
-  sha256 "1a710e2a6dbb0f4440867850d605f31fe8407ee8a56c9e067866e34e584385b4"
+  url "https://github.com/camlp5/camlp5/archive/refs/tags/rel8.00.04.tar.gz"
+  sha256 "bddbcb5c3c2d410c9a61c4dfb6e46e3bbe984d25ac68221a7a65c82a29956b1d"
   license "BSD-3-Clause"
-  revision 1
   head "https://github.com/camlp5/camlp5.git", branch: "master"
 
   livecheck do
@@ -14,8 +13,7 @@ class Camlp5 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/camlp5"
-    rebuild 1
-    sha256 mojave: "b75947396f1273e2725615f97f7f4e1dbba9fd1bfd067e61b2cd1486e05151e2"
+    sha256 mojave: "a2874084738d56aa92b7a8b27403dad7bae7e892f97519433b66ffcbd4cd7c46"
   end
 
   depends_on "ocaml-findlib" => :build
