0f690025b2efa3d6047f3ff6950eec76ea389cdb
Adrian Ho <the.gromgit@gmail.com>
2022-12-26 02:45:34 +0800

    cloudflared: update 2022.12.0 bottle.

--- Formula/cloudflared.rb
+++ Formula/cloudflared.rb
@@ -1,14 +1,14 @@
 class Cloudflared < Formula
   desc "Cloudflare Tunnel client (formerly Argo Tunnel)"
   homepage "https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/tunnel-guide"
-  url "https://github.com/cloudflare/cloudflared/archive/refs/tags/2022.10.0.tar.gz"
-  sha256 "f83c1ab042e7b15da69d2e7764898e0bc30f94ecd7db36dbd6270ebade1d5d27"
+  url "https://github.com/cloudflare/cloudflared/archive/refs/tags/2022.12.0.tar.gz"
+  sha256 "0c7f3f1d48c5b676e2555c8dea2168f09c7500993cbfc351ddb743eee0744630"
   license "Apache-2.0"
   head "https://github.com/cloudflare/cloudflared.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cloudflared"
-    sha256 cellar: :any_skip_relocation, mojave: "682c4db6a648a66c5de74af073fd264ce9b3a8dc1c44e1380b96e58b7f05a0fd"
+    sha256 cellar: :any_skip_relocation, mojave: "b08cbb770185ab4c0d04f36cd61af190efbac0bfb244900404f96536f4c45231"
   end
 
   depends_on "go" => :build
