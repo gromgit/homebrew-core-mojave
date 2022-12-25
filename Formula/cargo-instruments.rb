5950e9cfe3cc08e8a8abd869c6d7a68acb1992fa
Adrian Ho <the.gromgit@gmail.com>
2022-12-25 12:28:46 +0800

    cargo-instruments: update 0.4.8 bottle.

--- Formula/cargo-instruments.rb
+++ Formula/cargo-instruments.rb
@@ -1,18 +1,18 @@
 class CargoInstruments < Formula
   desc "Easily generate Instruments traces for your rust crate"
   homepage "https://github.com/cmyr/cargo-instruments"
-  url "https://github.com/cmyr/cargo-instruments/archive/v0.4.7.tar.gz"
-  sha256 "05827aae15603ab8a3538a5c9df5d3571f8d28d9b5d52c50728fbc5b6f6bbfd6"
+  url "https://github.com/cmyr/cargo-instruments/archive/v0.4.8.tar.gz"
+  sha256 "c5fc2a4133a888633ce132f41c3ec542788de41cd03848ac34e36cfb8bafcce0"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cargo-instruments"
-    sha256 cellar: :any, mojave: "9a0c9ab1773e3f5c0d1505fb8c2aa47362412f4e42967d44c5bf7c3a977fb949"
+    sha256 cellar: :any, mojave: "7e46b98d7d06ac437725de7c80f1ed1a58b0f6dea5cb747b80fd55e746d70981"
   end
 
   depends_on "rust" => :build
   depends_on :macos
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   def install
     system "cargo", "install", *std_cargo_args
