0040f04802e21f83cb330e2887bdbd23d8d28690
Adrian Ho <the.gromgit@gmail.com>
2022-12-26 12:40:26 +0800

    crane: update 0.12.1 bottle.

--- Formula/crane.rb
+++ Formula/crane.rb
@@ -1,14 +1,13 @@
 class Crane < Formula
   desc "Tool for interacting with remote images and registries"
   homepage "https://github.com/google/go-containerregistry"
-  url "https://github.com/google/go-containerregistry/archive/v0.11.0.tar.gz"
-  sha256 "e2eac75200a38f9fecdd8a6b80a839a50898e3539e5625c2150f6d5d31317ade"
+  url "https://github.com/google/go-containerregistry/archive/v0.12.1.tar.gz"
+  sha256 "6f8060933ace2acff468ce17359aa858b7ca3db049ed8d0ac5d4ae62359573c3"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/crane"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "db007b35f1ba8352b8897f95963b9db5aaa4e15d6182180a995ced698b5246ca"
+    sha256 cellar: :any_skip_relocation, mojave: "0a914acd2420b061b90839d589aa058d277d4632758e82cef204a93dddc69a62"
   end
 
   depends_on "go" => :build
