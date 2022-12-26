19a3b11d9f3e101a4d196366319143a6ad512531
Adrian Ho <the.gromgit@gmail.com>
2022-12-26 11:43:31 +0800

    cortex: update 1.14.1 bottle.

--- Formula/cortex.rb
+++ Formula/cortex.rb
@@ -1,8 +1,8 @@
 class Cortex < Formula
   desc "Long term storage for Prometheus"
   homepage "https://cortexmetrics.io/"
-  url "https://github.com/cortexproject/cortex/archive/v1.13.1.tar.gz"
-  sha256 "aa742a608c8201fcca41061cd2264d1b8eebe61c22259a5289de3a226aeee7e3"
+  url "https://github.com/cortexproject/cortex/archive/v1.14.1.tar.gz"
+  sha256 "cfa37a1e8a30a49c8aba4da1cf2f9a60f64dc6a46e74375bbc3d0e9961206cc5"
   license "Apache-2.0"
 
   livecheck do
@@ -12,16 +12,16 @@ class Cortex < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cortex"
-    sha256 cellar: :any_skip_relocation, mojave: "d4bd4b5efb27ded3a05506ea13713b3ce88fe33cdc6cf82a68e0ad69eca8fa2b"
+    sha256 cellar: :any_skip_relocation, mojave: "d473d2bf1c49053b25cd4c209c23857b83258984ebb1f05e4677ea55b0f969c6"
   end
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args, "./cmd/cortex"
-    cd "docs/chunks-storage" do
-      inreplace "single-process-config.yaml", "/tmp", var
-      etc.install "single-process-config.yaml" => "cortex.yaml"
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/cortex"
+    cd "docs/configuration" do
+      inreplace "single-process-config-blocks.yaml", "/tmp", var
+      etc.install "single-process-config-blocks.yaml" => "cortex.yaml"
     end
   end
 
@@ -34,18 +34,45 @@ class Cortex < Formula
   end
 
   test do
+    require "open3"
+    require "timeout"
+
     port = free_port
 
-    cp etc/"cortex.yaml", testpath
-    inreplace "cortex.yaml" do |s|
-      s.gsub! "9009", port.to_s
-      s.gsub! var, testpath
+    # A minimal working config modified from
+    # https://github.com/cortexproject/cortex/blob/master/docs/configuration/single-process-config-blocks.yaml
+    (testpath/"cortex.yaml").write <<~EOS
+      server:
+        http_listen_port: #{port}
+      ingester:
+        lifecycler:
+          ring:
+            kvstore:
+              store: inmemory
+            replication_factor: 1
+      blocks_storage:
+        backend: filesystem
+        filesystem:
+          dir: #{testpath}/data/tsdb
+    EOS
+
+    Open3.popen3(
+      bin/"cortex", "-config.file=cortex.yaml",
+                    "-server.grpc-listen-port=#{free_port}"
+    ) do |_, _, stderr, wait_thr|
+      Timeout.timeout(5) do
+        stderr.each do |line|
+          refute line.start_with? "level=error"
+          # It is important to wait for this line. Finishing the test too early
+          # may shadow errors that only occur when modules are fully loaded.
+          break if line.include? "Cortex started"
+        end
+        output = shell_output("curl -s http://localhost:#{port}/services")
+        assert_match "Running", output
+      end
+    ensure
+      Process.kill "TERM", wait_thr.pid
+      Process.wait wait_thr.pid
     end
-
-    fork { exec bin/"cortex", "-config.file=cortex.yaml", "-server.grpc-listen-port=#{free_port}" }
-    sleep 3
-
-    output = shell_output("curl -s localhost:#{port}/services")
-    assert_match "Running", output
   end
 end
