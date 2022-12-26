2ea762a4b4fba4f73857fe6dc81f503eb311a97d
Adrian Ho <the.gromgit@gmail.com>
2022-12-26 13:45:46 +0800

    ctlptl: update 0.8.15 bottle.

--- Formula/ctlptl.rb
+++ Formula/ctlptl.rb
@@ -1,13 +1,13 @@
 class Ctlptl < Formula
   desc "Making local Kubernetes clusters fun and easy to set up"
   homepage "https://github.com/tilt-dev/ctlptl"
-  url "https://github.com/tilt-dev/ctlptl/archive/v0.8.9.tar.gz"
-  sha256 "94332ed48728e59f9d07005909aa65d9c99ccec9914447710007ecc08553cad8"
+  url "https://github.com/tilt-dev/ctlptl/archive/v0.8.15.tar.gz"
+  sha256 "64aa07c2553f7189f345223cbffeb02589bce1c7700364d8cae8727f5d97a55a"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ctlptl"
-    sha256 cellar: :any_skip_relocation, mojave: "c5e48288a71d078200aefd680b489d11b025c6ad201c0b64fdae07682cf764a9"
+    sha256 cellar: :any_skip_relocation, mojave: "1b3f2bdc23a1cb25cbf481f06abb577d54e9084194505b7f97a5cafc65413e57"
   end
 
   depends_on "go" => :build
