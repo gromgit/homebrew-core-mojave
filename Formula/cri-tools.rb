909a7951548544971cb9d7e5681d8b60ed39046f
Adrian Ho <the.gromgit@gmail.com>
2022-12-26 12:53:43 +0800

    cri-tools: update 1.26.0 bottle.

--- Formula/cri-tools.rb
+++ Formula/cri-tools.rb
@@ -1,15 +1,14 @@
 class CriTools < Formula
   desc "CLI and validation tools for Kubelet Container Runtime Interface (CRI)"
   homepage "https://github.com/kubernetes-sigs/cri-tools"
-  url "https://github.com/kubernetes-sigs/cri-tools/archive/v1.25.0.tar.gz"
-  sha256 "bd900fac00b3247fa601f7aca4e0e44825dfd67d399c2830cbc643b7d73dbe52"
+  url "https://github.com/kubernetes-sigs/cri-tools/archive/v1.26.0.tar.gz"
+  sha256 "adf6e710fcf9b40b2ca6dbe0f3bbbdb70ebf2b7df349175a67a11b2f79195fb9"
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/cri-tools.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cri-tools"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "1eeeeba05bec486d114fce61b0e13bf5c349d7737f90cc58b467c4777c3457a9"
+    sha256 cellar: :any_skip_relocation, mojave: "c4c6e6e5f004bbf524fecc0c7e7f22db95e68c77cea34e9369b9c3971671ec9d"
   end
 
   depends_on "go" => :build
@@ -30,7 +29,7 @@ class CriTools < Formula
     crictl_output = shell_output(
       "#{bin}/crictl --runtime-endpoint unix:///var/run/nonexistent.sock --timeout 10ms info 2>&1", 1
     )
-    assert_match "unable to determine runtime API version", crictl_output
+    assert_match "Status from runtime service failed", crictl_output
 
     critest_output = shell_output("#{bin}/critest --ginkgo.dryRun 2>&1")
     assert_match "PASS", critest_output
