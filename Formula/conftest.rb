d020d73426e1eebf42245a184dc4e5b7268a0dcf
Adrian Ho <the.gromgit@gmail.com>
2022-12-26 10:06:38 +0800

    conftest: update 0.36.0 bottle.

--- Formula/conftest.rb
+++ Formula/conftest.rb
@@ -1,15 +1,14 @@
 class Conftest < Formula
   desc "Test your configuration files using Open Policy Agent"
   homepage "https://www.conftest.dev/"
-  url "https://github.com/open-policy-agent/conftest/archive/v0.34.0.tar.gz"
-  sha256 "7d305b88dd2aae866851301ee0a0db7f4ea37688ff09b77ec2a748240d5c92b9"
+  url "https://github.com/open-policy-agent/conftest/archive/v0.36.0.tar.gz"
+  sha256 "90a84d0f1aee5e71cddd524ff943f487eac7ec923699de764e941174a8bdb509"
   license "Apache-2.0"
   head "https://github.com/open-policy-agent/conftest.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/conftest"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "1855fb59ecd34d6c40b72689b4d60292509b98aa3a0e4932bbed9bd804775d2c"
+    sha256 cellar: :any_skip_relocation, mojave: "969b4cea1fb3633e232f93699bf4e30e7111cc370a56c981b6719359e9d9ed24"
   end
 
   depends_on "go" => :build
