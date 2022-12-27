4a1367f05a56dc195bb4a0dc6c9434244bf566a0
Adrian Ho <the.gromgit@gmail.com>
2022-12-27 22:32:39 +0800

    codespell: add 2.2.2 bottle.

--- Formula/codespell.rb
+++ Formula/codespell.rb
@@ -3,16 +3,16 @@ class Codespell < Formula
 
   desc "Fix common misspellings in source code and text files"
   homepage "https://github.com/codespell-project/codespell"
-  url "https://files.pythonhosted.org/packages/71/66/d5e8b69e1572e0165be57fd980ef74394dcea0f49d810407eab2c46449ef/codespell-2.2.1.tar.gz"
-  sha256 "569b67e5e5c3ade02a1e23f6bbc56c64b608a3ab48ddd943ece0a03e6c346ed1"
+  url "https://files.pythonhosted.org/packages/42/57/2b07dc5eb131d34a820bbc08a4437ca2ddfff7a47476bffd1822437de910/codespell-2.2.2.tar.gz"
+  sha256 "c4d00c02b5a2a55661f00d5b4b3b5a710fa803ced9a9d7e45438268b099c319c"
   license "GPL-2.0-only"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/codespell"
-    sha256 cellar: :any_skip_relocation, mojave: "11aee07f3b9bb98467b85a60c47aec8cf0892e5f78c9efbebe61b27d4d0aefd4"
+    sha256 cellar: :any_skip_relocation, mojave: "c7b47dbd9ce844615a59bd0a67368538ab5c3dec23a89388c1ee55e624145793"
   end
 
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   def install
     virtualenv_install_with_resources
