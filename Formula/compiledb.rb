fb16207776bdf283807fd38c7e56316e38ce429f
Adrian Ho <the.gromgit@gmail.com>
2022-12-27 23:51:17 +0800

    compiledb: add 0.10.1 bottle.

--- Formula/compiledb.rb
+++ Formula/compiledb.rb
@@ -3,25 +3,26 @@ class Compiledb < Formula
 
   desc "Generate a Clang compilation database for Make-based build systems"
   homepage "https://github.com/nickdiego/compiledb"
-  url "https://github.com/nickdiego/compiledb/archive/refs/tags/v0.10.1.tar.gz"
-  sha256 "3f288e4897e2b17b4dd8070d3ad9e9fc627961faa4d0be29a78f6c619e055f36"
+  url "https://files.pythonhosted.org/packages/76/62/30fb04404b1d4a454f414f792553d142e8acc5da27fddcce911fff0fe570/compiledb-0.10.1.tar.gz"
+  sha256 "06bb47dd1fa04de3a12720379ff382d40441074476db7c16a27e2ad79b7e966e"
   license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/compiledb"
-    sha256 cellar: :any_skip_relocation, mojave: "8aff5b217cede1205c94fdb48b746062f717d1ee255ad43bbaea1bc1b4be1e0f"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "c1132e8418ef05dbf9388dc84ca9ac070db88b92bb5b549f2463b34b16b66cd9"
   end
 
-  depends_on "python@3.10"
-
-  resource "click" do
-    url "https://files.pythonhosted.org/packages/dd/cf/706c1ad49ab26abed0b77a2f867984c1341ed7387b8030a6aa914e2942a0/click-8.0.4.tar.gz"
-    sha256 "8458d7b1287c5fb128c90e23381cf99dcde74beaf6c7ff6384ce84d6fe090adb"
-  end
+  depends_on "python@3.11"
 
   resource "bashlex" do
     url "https://files.pythonhosted.org/packages/1b/57/8de844f7702f644382def6aee76c64da5a1acfbc22a23ffbc565e0ec69cd/bashlex-0.16.tar.gz"
     sha256 "dc6f017e49ce2d0fe30ad9f5206da9cd13ded073d365688c9fda525354e8c373"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
+    sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
   end
 
   resource "shutilwhich" do
