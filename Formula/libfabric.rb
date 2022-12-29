1ad928b67622d6f69c69aee80887b2f443b4626e
Adrian Ho <the.gromgit@gmail.com>
2022-12-29 22:10:41 +0800

    libfabric: update 1.17.0 bottle.

--- Formula/libfabric.rb
+++ Formula/libfabric.rb
@@ -1,14 +1,14 @@
 class Libfabric < Formula
   desc "OpenFabrics libfabric"
   homepage "https://ofiwg.github.io/libfabric/"
-  url "https://github.com/ofiwg/libfabric/releases/download/v1.16.1/libfabric-1.16.1.tar.bz2"
-  sha256 "53f992d33f9afe94b8a4ea3d105504887f4311cf4b68cea99a24a85fcc39193f"
+  url "https://github.com/ofiwg/libfabric/releases/download/v1.17.0/libfabric-1.17.0.tar.bz2"
+  sha256 "579c0f5ef636c0c72f4d3d6bd4da91a5aed9ac3ac4ea387404c45dbbdee4745d"
   license any_of: ["BSD-2-Clause", "GPL-2.0-only"]
   head "https://github.com/ofiwg/libfabric.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libfabric"
-    sha256 cellar: :any, mojave: "bff3be3105f3d3223fd1a331612e2858a6f6f283e5f935630691a196131743a5"
+    sha256 cellar: :any, mojave: "3b1fedf58859a86ce84821d737ef4cb8d119993b3ba98e71a6113644fe1f060f"
   end
 
   depends_on "autoconf" => :build
