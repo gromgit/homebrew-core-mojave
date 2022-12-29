a4f184c098ca5ba83fbb9d9320e26a96675a12b6
Adrian Ho <the.gromgit@gmail.com>
2022-12-30 04:59:17 +0800

    gtk+3: update 3.24.35 bottle.

--- Formula/gtk+3.rb
+++ Formula/gtk+3.rb
@@ -1,8 +1,8 @@
 class Gtkx3 < Formula
   desc "Toolkit for creating graphical user interfaces"
   homepage "https://gtk.org/"
-  url "https://download.gnome.org/sources/gtk+/3.24/gtk+-3.24.34.tar.xz"
-  sha256 "dbc69f90ddc821b8d1441f00374dc1da4323a2eafa9078e61edbe5eeefa852ec"
+  url "https://download.gnome.org/sources/gtk+/3.24/gtk+-3.24.35.tar.xz"
+  sha256 "ec10fe6d712ef0b3c63b5f932639c9d1ae99fce94f500f6f06965629fef60bd1"
   license "LGPL-2.0-or-later"
 
   livecheck do
@@ -12,7 +12,7 @@ class Gtkx3 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gtk+3"
-    sha256 mojave: "f1cfd518d26ab3b90921b52cef8ad65baabed915ba4d4c73238776e50d7775fc"
+    sha256 mojave: "ed74a41d65c7725a9824e262ae0985398ed5279fe499eb20f7173825f587c562"
   end
 
   depends_on "docbook" => :build
@@ -37,8 +37,13 @@ class Gtkx3 < Formula
     depends_on "cairo"
     depends_on "iso-codes"
     depends_on "libxkbcommon"
-    depends_on "xorgproto"
     depends_on "wayland-protocols"
+    depends_on "xorgproto"
+
+    # fix ERROR: Non-existent build file 'gdk/wayland/cursor/meson.build'
+    # upstream commit reference, https://gitlab.gnome.org/GNOME/gtk/-/commit/66a19980
+    # remove in next release
+    patch :DATA
   end
 
   def install
@@ -140,3 +145,23 @@ class Gtkx3 < Formula
     assert_match version.to_s, shell_output("cat #{lib}/pkgconfig/gtk+-3.0.pc").strip
   end
 end
+
+__END__
+diff --git a/gdk/wayland/cursor/meson.build b/gdk/wayland/cursor/meson.build
+new file mode 100644
+index 0000000000000000000000000000000000000000..02d5f2bed8d926ee26bcf4c4081d18fc9d53fd5b
+--- /dev/null
++++ b/gdk/wayland/cursor/meson.build
+@@ -0,0 +1,12 @@
++wayland_cursor_sources = files([
++  'wayland-cursor.c',
++  'xcursor.c',
++  'os-compatibility.c'
++])
++
++libwayland_cursor = static_library('wayland+cursor',
++  sources: wayland_cursor_sources,
++  include_directories: [ confinc, ],
++  dependencies: [ glib_dep, wlclientdep, ],
++  c_args: common_cflags,
++)
