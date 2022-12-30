ddba6da2b2d3ed14ab9a2767e8fb9edb0b8984b6
Adrian Ho <the.gromgit@gmail.com>
2022-12-30 18:49:23 +0800

    librist: update 0.2.7_2 bottle.

--- Formula/librist.rb
+++ Formula/librist.rb
@@ -4,7 +4,7 @@ class Librist < Formula
   url "https://code.videolan.org/rist/librist/-/archive/v0.2.7/librist-v0.2.7.tar.gz"
   sha256 "7e2507fdef7b57c87b461d0f2515771b70699a02c8675b51785a73400b3c53a1"
   license "BSD-2-Clause"
-  revision 1
+  revision 2
   head "https://code.videolan.org/rist/librist.git", branch: "master"
 
   livecheck do
@@ -14,8 +14,7 @@ class Librist < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/librist"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "6637d1cca743763a8bbdfa768dc565c5dcdfde445cd44f62f998e1a9aa62f787"
+    sha256 cellar: :any, mojave: "82e4129160e6b169c44a116a1bf83768361349cdb5377f4eb0d8c5518515a9fc"
   end
 
   depends_on "meson" => :build
