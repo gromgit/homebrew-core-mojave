edb859db303aaf8ae73fdc0328ed12b4d2b39119
Adrian Ho <the.gromgit@gmail.com>
2022-12-31 08:19:57 +0800

    bandwhich: update 0.20.0 bottle.

--- Formula/bandwhich.rb
+++ Formula/bandwhich.rb
@@ -6,17 +6,20 @@ class Bandwhich < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "ea3006254b7dd73866c339a6ae37663c41eae1eb89d1d4d61945acade47c9dc7"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e8a4b58469686ecaaa23518da3bf9fa2ab3e78a4826b1d2331b6883e3770f6bd"
-    sha256 cellar: :any_skip_relocation, monterey:       "3e1d94079bbe7ec292a13c2d012d410f503ca8b88888212fb94ea15d0c2c71f5"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f140095cd6eb79ad6d06396a3c3b1bd8d9c5072639e77e54de24b45f0e82ab26"
-    sha256 cellar: :any_skip_relocation, catalina:       "424d3eff3b11609ad4645c028b3806babf18d9457749486fceff2522e2dd703d"
-    sha256 cellar: :any_skip_relocation, mojave:         "99d4980e850a91edc9e12749150151a0803aa0f2591a790f7236bc7031d1f8da"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "5ca8f58d406af543ec3833c190472cbefaa8fb614cd5f42cfc42392e3139283c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0dfeb11084a21c529d74299c699089df7689f2b3d62439acea0bea1f0f9a3aa3"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/bandwhich"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "a0961054d0248165ca5b6b47a2787f4974cdad2a428df7a42e2821f895c7783b"
   end
 
   depends_on "rust" => :build
+
+  # patch build
+  # upstream issue, https://github.com/imsnif/bandwhich/issues/258
+  # upstream PR, https://github.com/imsnif/bandwhich/pull/259
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/96bfb409db46dfe53b76da7682ddcf650af45921/bandwhich/0.20.0.patch"
+    sha256 "ea446f63c9e766ab9c987c83f1ca5f6759175df4e2b3e377604fd87a2b0b26de"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
