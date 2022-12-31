17a3aab062634f11f3afcf0c85f1951559bb111e
Adrian Ho <the.gromgit@gmail.com>
2022-12-31 20:41:03 +0800

    minizip: update 1.2.13 bottle.

--- Formula/minizip.rb
+++ Formula/minizip.rb
@@ -1,9 +1,9 @@
 class Minizip < Formula
   desc "C library for zip/unzip via zLib"
   homepage "https://www.winimage.com/zLibDll/minizip.html"
-  url "https://zlib.net/zlib-1.2.12.tar.gz"
-  mirror "https://downloads.sourceforge.net/project/libpng/zlib/1.2.12/zlib-1.2.12.tar.gz"
-  sha256 "91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9"
+  url "https://zlib.net/zlib-1.2.13.tar.gz"
+  mirror "https://downloads.sourceforge.net/project/libpng/zlib/1.2.13/zlib-1.2.13.tar.gz"
+  sha256 "b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30"
   license "Zlib"
 
   livecheck do
@@ -12,7 +12,7 @@ class Minizip < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/minizip"
-    sha256 cellar: :any, mojave: "f0e1ae84835b9f23305afac79c5b36278b0efff5968a12601b2650e8cca097f5"
+    sha256 cellar: :any, mojave: "afd8c2ef4095468c36638d3548633d60b2ca6233f6708ffcc44a10510229a06f"
   end
 
   depends_on "autoconf" => :build
@@ -23,13 +23,6 @@ class Minizip < Formula
 
   conflicts_with "minizip-ng",
     because: "both install a `libminizip.a` library"
-
-  # Patch for configure issue
-  # https://github.com/madler/zlib/pull/607
-  patch do
-    url "https://github.com/madler/zlib/commit/05796d3d8d5546cf1b4dfe2cd72ab746afae505d.patch?full_index=1"
-    sha256 "68573842f1619bb8de1fa92071e38e6e51b8df71371e139e4e96be19dd7e9694"
-  end
 
   def install
     system "./configure", "--prefix=#{prefix}"
