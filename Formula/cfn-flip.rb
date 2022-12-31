efba9d8a114472050c3de6f9628b3c7885b7f01e
Adrian Ho <the.gromgit@gmail.com>
2022-12-31 16:06:39 +0800

    cfn-flip: update 1.3.0_1 bottle.

--- Formula/cfn-flip.rb
+++ Formula/cfn-flip.rb
@@ -10,20 +10,17 @@ class CfnFlip < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cfn-flip"
-    sha256 cellar: :any_skip_relocation, mojave: "f7088eabcd4209d23cf91246009135381ca55934c1087eaef7bba611b580de30"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "703b9ce40aa1b8cab19e85ce08e228d949d13af32512af0984af022ca9e0c93c"
   end
 
-  depends_on "python@3.10"
+  depends_on "python@3.11"
+  depends_on "pyyaml"
   depends_on "six"
 
   resource "click" do
     url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
     sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
-  end
-
-  resource "PyYAML" do
-    url "https://files.pythonhosted.org/packages/36/2b/61d51a2c4f25ef062ae3f74576b01638bebad5e045f747ff12643df63844/PyYAML-6.0.tar.gz"
-    sha256 "68fb519c14306fec9720a2a5b45bc9f0c8d1b9c72adf45c37baedfcd949c35a2"
   end
 
   def install
