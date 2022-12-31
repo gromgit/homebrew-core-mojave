124ac517e376caf39bb21984971d816ad4f5f7db
Adrian Ho <the.gromgit@gmail.com>
2022-12-31 19:43:04 +0800

    clip: update 0.7_2 bottle.

--- Formula/clip.rb
+++ Formula/clip.rb
@@ -1,19 +1,14 @@
 class Clip < Formula
   desc "Create high-quality charts from the command-line"
-  homepage "https://clip-lang.org/"
+  homepage "https://github.com/asmuth/clip"
   url "https://github.com/asmuth/clip/archive/v0.7.tar.gz"
   sha256 "f38f455cf3e9201614ac71d8a871e4ff94a6e4cf461fd5bf81bdf457ba2e6b3e"
   license "Apache-2.0"
-  revision 1
+  revision 2
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "02d8da157f889f384abb2900d10ead88cc6bfaba984165e78a92f6fbce100f6e"
-    sha256 cellar: :any,                 arm64_big_sur:  "20bb528940048dacd9f0eb67b3a1fe849e9e54d1c7e9a7c58da1fc9814beb1c8"
-    sha256 cellar: :any,                 monterey:       "14853fce6b1e8ccd447df646d60b9277a23e72279dd74c40677b823451801995"
-    sha256 cellar: :any,                 big_sur:        "cb33d312ff97b10b5761ceb105e3a5d63ab11c48db072c199108167cecf47854"
-    sha256 cellar: :any,                 catalina:       "c6e0fc40838d76e9632e21e1a781603a1a75d584a3206300fe0a9f7272d9b702"
-    sha256 cellar: :any,                 mojave:         "72f17c87e0c824cea89ba5fa383cf22d013751997a916a408eaa8408544292fb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "adbd6fc3d72a07a847e887fa050e91598e3045fa9d9dc251cec0953a6f6626d5"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/clip"
+    sha256 cellar: :any, mojave: "951e3559112850ff8169d03f5ac50ce3d4c2760b00df5b20a38e519c76c8c3ec"
   end
 
   depends_on "cmake" => :build
@@ -24,11 +19,7 @@ class Clip < Formula
   depends_on "fribidi"
   depends_on "harfbuzz"
 
-  on_linux do
-    depends_on "gcc" # for C++17
-  end
-
-  fails_with gcc: "5"
+  fails_with gcc: "5" # for C++17
 
   def install
     system "cmake", ".", *std_cmake_args
