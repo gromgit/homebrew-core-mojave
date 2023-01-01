afc04a3cb84758a916af5a448b2d7cd96803e23f
Adrian Ho <the.gromgit@gmail.com>
2023-01-02 03:08:38 +0800

    davix: update 0.8.3 bottle.

--- Formula/davix.rb
+++ Formula/davix.rb
@@ -1,14 +1,14 @@
 class Davix < Formula
   desc "Library and tools for advanced file I/O with HTTP-based protocols"
   homepage "https://github.com/cern-fts/davix"
-  url "https://github.com/cern-fts/davix/releases/download/R_0_8_2/davix-0.8.2.tar.gz"
-  sha256 "8817a24c23f1309b9de233b9a882455f457c42edc2a649dc70fe2524cf76d94c"
+  url "https://github.com/cern-fts/davix/releases/download/R_0_8_3/davix-0.8.3.tar.gz"
+  sha256 "7e30b5541e08d32dbf5ae03c6bcabeaec063aec10a6647787822227b4541ae3e"
   license "LGPL-2.1-or-later"
   head "https://github.com/cern-fts/davix.git", branch: "devel"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/davix"
-    sha256 cellar: :any, mojave: "3fe564846d6be67802c10618fa1311417a3c29cae0a89b307acd9b8870121a7f"
+    sha256 cellar: :any, mojave: "972c894ce4ea306e481c1cdff9aa97dc92ffec7adfec35a54c2c4692410a2694"
   end
 
   depends_on "cmake" => :build
@@ -27,6 +27,7 @@ class Davix < Formula
     args = std_cmake_args + %W[
       -DEMBEDDED_LIBCURL=FALSE
       -DCMAKE_INSTALL_RPATH=#{rpath}
+      -DLIB_SUFFIX=
     ]
 
     system "cmake", "-S", ".", "-B", "build", *args
