9226bf9ec957ecd6bae55c8fcee1e894642904e1
Adrian Ho <the.gromgit@gmail.com>
2023-01-01 15:32:07 +0800

    mpi4py: add 3.1.4_1 bottle.

--- Formula/mpi4py.rb
+++ Formula/mpi4py.rb
@@ -1,23 +1,22 @@
 class Mpi4py < Formula
   desc "Python bindings for MPI"
   homepage "https://mpi4py.github.io/"
-  url "https://github.com/mpi4py/mpi4py/releases/download/3.1.3/mpi4py-3.1.3.tar.gz"
-  sha256 "f1e9fae1079f43eafdd9f817cdb3fd30d709edc093b5d5dada57a461b2db3008"
+  url "https://github.com/mpi4py/mpi4py/releases/download/3.1.4/mpi4py-3.1.4.tar.gz"
+  sha256 "17858f2ebc623220d0120d1fa8d428d033dde749c4bc35b33d81a66ad7f93480"
   license "BSD-2-Clause"
   revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mpi4py"
-    rebuild 1
-    sha256 cellar: :any, mojave: "38030463c34800303ffd21144d85616f5ec9fc3f683860e8ea76e1667dd03b94"
+    sha256 cellar: :any, mojave: "46f33446d898c2ab83738b88bf0c2db3ecd6817204bddc4bb2bb6c6a4825b271"
   end
 
   depends_on "libcython" => :build
   depends_on "open-mpi"
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   def python3
-    "python3.10"
+    "python3.11"
   end
 
   def install
@@ -31,15 +30,13 @@ class Mpi4py < Formula
   end
 
   test do
-    python = Formula["python@3.10"].opt_bin/python3
-
-    system python, "-c", "import mpi4py"
-    system python, "-c", "import mpi4py.MPI"
-    system python, "-c", "import mpi4py.futures"
+    system python3, "-c", "import mpi4py"
+    system python3, "-c", "import mpi4py.MPI"
+    system python3, "-c", "import mpi4py.futures"
 
     system "mpiexec", "-n", ENV.make_jobs, "--use-hwthread-cpus",
-           python, "-m", "mpi4py.run", "-m", "mpi4py.bench", "helloworld"
+           python3, "-m", "mpi4py.run", "-m", "mpi4py.bench", "helloworld"
     system "mpiexec", "-n", ENV.make_jobs, "--use-hwthread-cpus",
-           python, "-m", "mpi4py.run", "-m", "mpi4py.bench", "ringtest", "-l", "10", "-n", "1024"
+           python3, "-m", "mpi4py.run", "-m", "mpi4py.bench", "ringtest", "-l", "10", "-n", "1024"
   end
 end
