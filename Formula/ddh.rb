d5883b167f1a8b1aa097285e2d23fc337608c4f9
Adrian Ho <the.gromgit@gmail.com>
2023-01-02 04:37:07 +0800

    ddh: update 0.13.0 bottle.

--- Formula/ddh.rb
+++ Formula/ddh.rb
@@ -1,8 +1,8 @@
 class Ddh < Formula
   desc "Fast duplicate file finder"
   homepage "https://github.com/darakian/ddh"
-  url "https://github.com/darakian/ddh/archive/refs/tags/0.12.0.tar.gz"
-  sha256 "f16dd4da04852670912c1b3fd65ce9b6ebd01ba2d0df97cb8c9bdf91ba453384"
+  url "https://github.com/darakian/ddh/archive/refs/tags/0.13.0.tar.gz"
+  sha256 "87010f845fa68945d2def4a05a3eb796222b67c5d3cea41e576cfaf2ab078ef8"
   license "LGPL-3.0-only"
 
   livecheck do
@@ -12,8 +12,7 @@ class Ddh < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ddh"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "771c6c56a3bb413376ad9362e2a8a700d2d7551e9bd002c9fdc4f074f3910cde"
+    sha256 cellar: :any_skip_relocation, mojave: "ed62dc9b03d277b3dfe6cd5d218291273da9ea326f951d0e8bc9a721c8290bc0"
   end
 
   depends_on "rust" => :build
@@ -27,10 +26,10 @@ class Ddh < Formula
     (testpath/"test/file2").write "brew test"
 
     expected = <<~EOS
-      2 Total files (with duplicates): 0 Megabytes
-      1 Total files (without duplicates): 0 Megabytes
-      0 Single instance files: 0 Megabytes
-      1 Shared instance files: 0 Megabytes (2 instances)
+      2 Total files (with duplicates): 0 Kilobytes
+      1 Total files (without duplicates): 0 Kilobytes
+      0 Single instance files: 0 Kilobytes
+      1 Shared instance files: 0 Kilobytes (2 instances)
       Standard results written to Results.txt
     EOS
 
