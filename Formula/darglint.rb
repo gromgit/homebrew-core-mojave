d8e31045b46435ff91ee8f97ffe4c5fb49cfae93
Adrian Ho <the.gromgit@gmail.com>
2023-01-02 01:36:51 +0800

    darglint: update 1.8.1 bottle.

--- Formula/darglint.rb
+++ Formula/darglint.rb
@@ -9,22 +9,22 @@ class Darglint < Formula
   head "https://github.com/terrencepreilly/darglint.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "7462727f3dc2fa353d3a7c06d2151acd34d361c7e2756c13f61b6f6834c03870"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "42e91f209c5c3a31378b08128e872de32cf6be51cf54ced378a51ef8a2220e42"
-    sha256 cellar: :any_skip_relocation, monterey:       "9a671f2790a10f4f7d5c8ccd55249eba10991aadd39040fa4edaf04586d35f33"
-    sha256 cellar: :any_skip_relocation, big_sur:        "f1a2ee8eeca7a285648d9c4e2c3d00031025c3f50614f992878bab0f00b57d61"
-    sha256 cellar: :any_skip_relocation, catalina:       "f9d1151558abf70184f0ed3d66c94e8a9f1f89a9cf20bae7db030e71e091263a"
-    sha256 cellar: :any_skip_relocation, mojave:         "aa84254d72fcccfece16713b9f7648aeaef79831465d2f14032b942a48a17801"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "25cac2c982564d37e9bc4159ef583f2053b5061f5397d2c1a5661df1293916f3"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/darglint"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "b7686faeb0876646a62efd3d39fac328f01ea5f4803e24c3b40ecb35daa895f3"
   end
 
-  depends_on "poetry" => :build
-  depends_on "python@3.10"
+  depends_on "python@3.11"
+
+  # Switch build-system to poetry-core to avoid rust dependency on Linux.
+  # Remove when merged/released: https://github.com/terrencepreilly/darglint/pull/203
+  patch do
+    url "https://github.com/terrencepreilly/darglint/commit/aa10a220bbbce522bee2c986606a1650f1c2be1e.patch?full_index=1"
+    sha256 "871a4790feabd4e6a5feb2d618ef5802dc3c9ecd47345c3ba2f9377068ba4fa7"
+  end
 
   def install
-    venv = virtualenv_create(libexec, "python3.10")
-    system Formula["poetry"].opt_bin/"poetry", "build", "--format", "wheel", "--verbose", "--no-interaction"
-    venv.pip_install_and_link Dir["dist/darglint-*.whl"].first
+    virtualenv_install_with_resources
   end
 
   test do
