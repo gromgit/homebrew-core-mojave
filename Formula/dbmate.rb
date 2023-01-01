e64efbed4939d1bb95616f2797905f73527c30f9
Adrian Ho <the.gromgit@gmail.com>
2023-01-02 03:21:09 +0800

    dbmate: update 1.16.0 bottle.

--- Formula/dbmate.rb
+++ Formula/dbmate.rb
@@ -1,14 +1,14 @@
 class Dbmate < Formula
   desc "Lightweight, framework-agnostic database migration tool"
   homepage "https://github.com/amacneil/dbmate"
-  url "https://github.com/amacneil/dbmate/archive/v1.15.0.tar.gz"
-  sha256 "1460d1591c0c7340a90e6aee32aa123ec24d88f8e5486482d2bab318497a42bf"
+  url "https://github.com/amacneil/dbmate/archive/v1.16.0.tar.gz"
+  sha256 "1b3feae085837bd21243ef1966e64536a837ee260ccb809323879940fa5df877"
   license "MIT"
   head "https://github.com/amacneil/dbmate.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dbmate"
-    sha256 cellar: :any_skip_relocation, mojave: "f615b872b16033ced373c907d238fabd8028c5889f41f215b82f58f6fe65f95a"
+    sha256 cellar: :any_skip_relocation, mojave: "f50db1c05da6cc1aae927b7558822930d78de98e0c3e2618524f9e6757bee37c"
   end
 
   depends_on "go" => :build
