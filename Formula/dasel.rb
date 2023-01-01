776fb2f345a4010c291731a12eaa15eafa6eb769
Adrian Ho <the.gromgit@gmail.com>
2023-01-02 02:28:51 +0800

    dasel: update 2.0.2 bottle.

--- Formula/dasel.rb
+++ Formula/dasel.rb
@@ -1,14 +1,14 @@
 class Dasel < Formula
   desc "JSON, YAML, TOML, XML, and CSV query and modification tool"
   homepage "https://github.com/TomWright/dasel"
-  url "https://github.com/TomWright/dasel/archive/v1.27.1.tar.gz"
-  sha256 "4dd83d68347b895d62ec2749d782a9dbddf314bafef502eac7c5fdcc4058b85d"
+  url "https://github.com/TomWright/dasel/archive/v2.0.2.tar.gz"
+  sha256 "cdb209c838b0f24520f35997c2856efbbdf695c314bc43ddcc3dc5180e8b812b"
   license "MIT"
   head "https://github.com/TomWright/dasel.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dasel"
-    sha256 cellar: :any_skip_relocation, mojave: "b4cb4d7a49d287a0e4d878b75813089bc40c2d7c1f554bd64c94fcb6e3aa3829"
+    sha256 cellar: :any_skip_relocation, mojave: "b5fc2f4b425665e25f17dd57888eee3d7f298b74efc8240ada109241dcadbb9b"
   end
 
   depends_on "go" => :build
@@ -21,7 +21,7 @@ class Dasel < Formula
   end
 
   test do
-    json = "[{\"name\": \"Tom\"}, {\"name\": \"Jim\"}]"
-    assert_equal "Tom\nJim", pipe_output("#{bin}/dasel --plain -p json -m '.[*].name'", json).chomp
+    assert_equal "\"Tom\"", shell_output("echo '{\"name\": \"Tom\"}' | #{bin}/dasel -r json 'name'").chomp
+    assert_match version.to_s, shell_output("#{bin}/dasel --version")
   end
 end
