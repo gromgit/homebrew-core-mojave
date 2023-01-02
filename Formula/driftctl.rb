c2ce696d747db6c79c1cb1a311df4f1f6166f2ca
Adrian Ho <the.gromgit@gmail.com>
2023-01-03 00:01:16 +0800

    driftctl: update 0.38.1 bottle.

--- Formula/driftctl.rb
+++ Formula/driftctl.rb
@@ -1,21 +1,19 @@
 class Driftctl < Formula
   desc "Detect, track and alert on infrastructure drift"
   homepage "https://driftctl.com"
-  url "https://github.com/snyk/driftctl/archive/v0.37.0.tar.gz"
-  sha256 "6eb05e26edd1d53182e58ae1ed7b5231bf2fb05f5b7b04ab4be39e38461b965f"
+  url "https://github.com/snyk/driftctl/archive/v0.38.1.tar.gz"
+  sha256 "507731116447958ebcee89e3fa5e6c476cd5afdb3c10c7be414f5d55efcff647"
   license "Apache-2.0"
+  head "https://github.com/snyk/driftctl.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/driftctl"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "4b5575f4899d7c241646461bf9c49d0aba5f7ca074213ac1c3d618f23b21f12f"
+    sha256 cellar: :any_skip_relocation, mojave: "c9e398dcbfb7bd6a59c4468873f99acc5a8623124a2a39923aeb6f884f83453f"
   end
 
   depends_on "go" => :build
 
   def install
-    ENV["CGO_ENABLED"] = "0"
-
     ldflags = %W[
       -s -w
       -X github.com/snyk/driftctl/build.env=release
@@ -28,8 +26,9 @@ class Driftctl < Formula
   end
 
   test do
-    assert_match "v#{version}", shell_output("#{bin}/driftctl version")
     assert_match "Could not find a way to authenticate on AWS!",
       shell_output("#{bin}/driftctl --no-version-check scan 2>&1", 2)
+
+    assert_match version.to_s, shell_output("#{bin}/driftctl version")
   end
 end
