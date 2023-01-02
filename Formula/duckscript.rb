369805ae31ae93c755d59f534871f1a30fe6cce6
Adrian Ho <the.gromgit@gmail.com>
2023-01-03 02:27:53 +0800

    duckscript: update 0.8.16 bottle.

--- Formula/duckscript.rb
+++ Formula/duckscript.rb
@@ -1,27 +1,25 @@
 class Duckscript < Formula
   desc "Simple, extendable and embeddable scripting language"
   homepage "https://sagiegurari.github.io/duckscript"
-  url "https://github.com/sagiegurari/duckscript/archive/0.8.14.tar.gz"
-  sha256 "34c9a0fda9c635706a2b82dcaf11f6536cba6f24c6ff403fdf377e323f8f6272"
+  url "https://github.com/sagiegurari/duckscript/archive/0.8.16.tar.gz"
+  sha256 "d5c8aa607986e542746df5b4e68a20b77f32fcbf4d34faea718b09a89b22407e"
   license "Apache-2.0"
   head "https://github.com/sagiegurari/duckscript.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/duckscript"
-    sha256 cellar: :any_skip_relocation, mojave: "38ce2fad7e2a8d1d7e4b3d8368749452dc7fa3da06cf867ba6cb179dc4c4a361"
+    sha256 cellar: :any_skip_relocation, mojave: "8c147931d462325f942129cea3dd4c3438e7a875fe32392006597e28799402d6"
   end
 
   depends_on "rust" => :build
 
   on_linux do
     depends_on "pkg-config" => :build
-    depends_on "openssl@1.1" # Uses Secure Transport on macOS
+    depends_on "openssl@3" # Uses Secure Transport on macOS
   end
 
   def install
-    cd "duckscript_cli" do
-      system "cargo", "install", *std_cargo_args
-    end
+    system "cargo", "install", "--features", "tls-native", *std_cargo_args(path: "duckscript_cli")
   end
 
   test do
