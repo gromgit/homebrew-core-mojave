0c25a4e41fa8f3984da8ec2594837c8358323c62
Adrian Ho <the.gromgit@gmail.com>
2023-01-03 07:24:17 +0800

    gstreamer: update 1.20.4 bottle.

--- Formula/gstreamer.rb
+++ Formula/gstreamer.rb
@@ -1,8 +1,8 @@
 class Gstreamer < Formula
   desc "Development framework for multimedia applications"
   homepage "https://gstreamer.freedesktop.org/"
-  url "https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-1.20.3.tar.xz"
-  sha256 "607daf64bbbd5fb18af9d17e21c0d22c4d702fffe83b23cb22d1b1af2ca23a2a"
+  url "https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-1.20.4.tar.xz"
+  sha256 "67c1edf8c3c339cda5dde85f4f7b953bb9607c2d13ae970e2491c5c4c055ef5f"
   license "LGPL-2.0-or-later"
   head "https://gitlab.freedesktop.org/gstreamer/gstreamer.git", branch: "main"
 
@@ -13,7 +13,7 @@ class Gstreamer < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gstreamer"
-    sha256 mojave: "f47a7c1ffc6a2a5ae93d8051e857d9c0e1ace5b73d643c7b8e7c74e8d3ded457"
+    sha256 mojave: "dc2334df96df5b6f42b3d97b0450ca31b62947a77258108e692b61eda55f3666"
   end
 
   depends_on "bison" => :build
