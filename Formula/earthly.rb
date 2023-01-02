6f47a61d78fa9c9c270838a9878f66a7e07c5c8a
Adrian Ho <the.gromgit@gmail.com>
2023-01-03 05:36:12 +0800

    earthly: update 0.6.30 bottle.

--- Formula/earthly.rb
+++ Formula/earthly.rb
@@ -2,19 +2,20 @@ class Earthly < Formula
   desc "Build automation tool for the container era"
   homepage "https://earthly.dev/"
   url "https://github.com/earthly/earthly.git",
-      tag:      "v0.6.25",
-      revision: "b9a16aa25c15201998ddc7781efca5934c1f7660"
+      tag:      "v0.6.30",
+      revision: "743687991d227f2f7e2e6ba11a90f443b4f99c67"
   license "MPL-2.0"
   head "https://github.com/earthly/earthly.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/earthly"
-    sha256 cellar: :any_skip_relocation, mojave: "9401185665060bf8ff9147118d3af7980709d4e4181a600c444aa7c64fed7e94"
+    sha256 cellar: :any_skip_relocation, mojave: "ab113bcbd185a6a629fb7c29b433e5daf72f461ca44e21a9454f1051fb34f165"
   end
 
   depends_on "go" => :build
 
   def install
+    ENV["CGO_ENABLED"] = "0"
     ldflags = %W[
       -s -w
       -X main.DefaultBuildkitdImage=earthly/buildkitd:v#{version}
@@ -22,7 +23,7 @@ class Earthly < Formula
       -X main.GitSha=#{Utils.git_head}
       -X main.BuiltBy=homebrew
     ]
-    tags = "dfrunmount dfrunsecurity dfsecrets dfssh dfrunnetwork"
+    tags = "dfrunmount dfrunsecurity dfsecrets dfssh dfrunnetwork dfheredoc forceposix"
     system "go", "build", "-tags", tags, *std_go_args(ldflags: ldflags), "./cmd/earthly"
 
     generate_completions_from_executable(bin/"earthly", "bootstrap", "--source", shells: [:bash, :zsh])
