4bd7892333eb8333a638e23b81caeef9dcc4b0d5
Adrian Ho <the.gromgit@gmail.com>
2023-01-02 23:50:35 +0800

    draco: update 1.5.5 bottle.

--- Formula/draco.rb
+++ Formula/draco.rb
@@ -1,13 +1,13 @@
 class Draco < Formula
   desc "3D geometric mesh and point cloud compression library"
   homepage "https://google.github.io/draco/"
-  url "https://github.com/google/draco/archive/1.5.3.tar.gz"
-  sha256 "7882a942a1da14a9ae9d557b1a3af7f44bdee7f5d42b745c4e474fb8b28d4e5e"
+  url "https://github.com/google/draco/archive/1.5.5.tar.gz"
+  sha256 "6b7994150bbc513abcdbe22ad778d6b2df10fc8cdc7035e916985b2a209ab826"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/draco"
-    sha256 cellar: :any_skip_relocation, mojave: "f66c62403ec6897af5e92bce2b173ab3384d9354236ac2f147050bdc6de191b7"
+    sha256 cellar: :any_skip_relocation, mojave: "ad9793146e5e0e1e0edc478646285cc0fea538eca1d62496fc07b0a419892fb4"
   end
 
   depends_on "cmake" => :build
