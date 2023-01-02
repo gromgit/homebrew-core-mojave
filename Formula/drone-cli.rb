2c6ff13750d1165ba9b62a77c07e135cdd3b38a9
Adrian Ho <the.gromgit@gmail.com>
2023-01-03 00:27:03 +0800

    drone-cli: update 1.6.2 bottle.

--- Formula/drone-cli.rb
+++ Formula/drone-cli.rb
@@ -2,24 +2,22 @@ class DroneCli < Formula
   desc "Command-line client for the Drone continuous integration server"
   homepage "https://drone.io"
   url "https://github.com/harness/drone-cli.git",
-      tag:      "v1.5.0",
-      revision: "92e84c4e2452f82ad093722d87ad054e1821805e"
+      tag:      "v1.6.2",
+      revision: "24bea58512640de52ef16c45f48645e255d8b46c"
   license "Apache-2.0"
   head "https://github.com/harness/drone-cli.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/drone-cli"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "7b4f0f90db0a38413a18227d8a57c1dc5d6ad80477095e53b778ab8bebd65f64"
+    sha256 cellar: :any_skip_relocation, mojave: "80b1eef6a1defaa0141f3a322d49ccd510d64ca1c3472f9686f15befa28269ac"
   end
 
   depends_on "go" => :build
 
   def install
     ENV["CGO_ENABLED"] = "0"
-    system "go", "build", "-ldflags", "-s -w -X main.version=#{version}", "-trimpath", "-o",
-           bin/"drone", "drone/main.go"
-    prefix.install_metafiles
+    ldflags = "-s -w -X main.version=#{version}"
+    system "go", "build", *std_go_args(output: bin/"drone", ldflags: ldflags), "drone/main.go"
   end
 
   test do
