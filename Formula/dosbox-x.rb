217a8a7fabb04eef31c2986269b1b5505c5205b0
Adrian Ho <the.gromgit@gmail.com>
2023-01-02 22:23:06 +0800

    dosbox-x: update 0.84.1 bottle.

--- Formula/dosbox-x.rb
+++ Formula/dosbox-x.rb
@@ -1,32 +1,36 @@
 class DosboxX < Formula
   desc "DOSBox with accurate emulation and wide testing"
   homepage "https://dosbox-x.com/"
-  url "https://github.com/joncampbell123/dosbox-x/archive/dosbox-x-v0.84.0.tar.gz"
-  sha256 "564fbf8f0ab090c8b32bc38637c8204358c386b9cbffcb4f99a81bc82fddbad7"
+  url "https://github.com/joncampbell123/dosbox-x/archive/dosbox-x-v0.84.1.tar.gz"
+  sha256 "d5ba5b3d87b0dc69d70f6c9701eec36772bbc3716e0b201b74ec73d4b3ff38cc"
   license "GPL-2.0-or-later"
   version_scheme 1
   head "https://github.com/joncampbell123/dosbox-x.git", branch: "master"
 
   livecheck do
-    url :stable
-    regex(/^dosbox-x[._-]v?(\d+(?:\.\d+)+)$/i)
+    url "https://github.com/joncampbell123/dosbox-x/releases?q=prerelease%3Afalse"
+    regex(%r{href=["']?[^"' >]*?/tag/dosbox-x[._-]v?(\d+(?:\.\d+)+)["' >]}i)
+    strategy :page_match
   end
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/dosbox-x"
-    rebuild 1
-    sha256 cellar: :any, mojave: "cac35851920e907ae2adff9fc49db8ebd0d3793e9aab3b40b1d929e8128da335"
+    sha256 mojave: "ae7ee714f652218daf23f8799ce0255547c5e13f39e9fb7738251f7cb7d52f63"
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
   depends_on "fluid-synth"
+  depends_on "freetype"
+  depends_on "libpng"
+  depends_on "libslirp"
   depends_on macos: :high_sierra # needs futimens
+  depends_on "sdl2"
+  uses_from_macos "zlib"
 
   on_linux do
     depends_on "linux-headers@5.15" => :build
-    depends_on "sdl2"
   end
 
   fails_with gcc: "5"
@@ -34,13 +38,18 @@ class DosboxX < Formula
   def install
     ENV.cxx11
 
-    args = %W[
-      --prefix=#{prefix}
-      --disable-dependency-tracking
+    # See flags in `build-macos-sdl2`.
+    args = %w[
+      --enable-core-inline
+      --enable-sdl2
+      --disable-sdl2test
+      --disable-sdl
       --disable-sdltest
     ]
-    build_script = OS.mac? ? "./build-macosx" : "./build"
-    system build_script, *args
+
+    system "./autogen.sh"
+    system "./configure", *std_configure_args, *args
+    system "make" # Needs to be called separately from `make install`.
     system "make", "install"
   end
 
