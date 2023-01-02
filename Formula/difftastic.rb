2cb91005f3e2be0e3936c7d7731036aca23497c2
Adrian Ho <the.gromgit@gmail.com>
2023-01-02 09:32:53 +0800

    difftastic: update 0.39.0 bottle.

--- Formula/difftastic.rb
+++ Formula/difftastic.rb
@@ -1,13 +1,13 @@
 class Difftastic < Formula
   desc "Diff that understands syntax"
   homepage "https://github.com/Wilfred/difftastic"
-  url "https://github.com/Wilfred/difftastic/archive/refs/tags/0.36.1.tar.gz"
-  sha256 "f7ffd83be50918993669a60821f037af110b809f4cd330ebd59c0b51418a6fa4"
+  url "https://github.com/Wilfred/difftastic/archive/refs/tags/0.39.0.tar.gz"
+  sha256 "ba05464d8a5c77374cb9a07d901eda56c67f4d558def515454bc4b7431c8ca38"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/difftastic"
-    sha256 cellar: :any_skip_relocation, mojave: "ee22b1123b7cd88872322c4579df5cf7c1708959d55e35e459c037034fd85cee"
+    sha256 cellar: :any_skip_relocation, mojave: "82efbdd4aba65bf0b605da8e11c41f5b3cbf59baf208da5117c8400d14816ebc"
   end
 
   depends_on "rust" => :build
@@ -23,7 +23,7 @@ class Difftastic < Formula
     (testpath/"b.py").write("print(43)\n")
     expected = <<~EOS
       b.py --- Python
-      1 print(42) 1 print(43)\n
+      1 print(42)                             1 print(43)\n
     EOS
     assert_equal expected, shell_output("#{bin}/difft --color never --width 80 a.py b.py")
   end
