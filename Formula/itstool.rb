2e660855b2761944241178e57e42ed8677dd9e80
Adrian Ho <the.gromgit@gmail.com>
2023-01-03 05:52:03 +0800

    itstool: update 2.0.7_1 bottle.

--- Formula/itstool.rb
+++ Formula/itstool.rb
@@ -1,32 +1,37 @@
 class Itstool < Formula
   desc "Make XML documents translatable through PO files"
   homepage "http://itstool.org/"
-  url "https://github.com/itstool/itstool/archive/2.0.7.tar.gz"
-  sha256 "fba78a37dc3535e4686c7f57407b97d03c676e3a57beac5fb2315162b0cc3176"
+  url "http://files.itstool.org/itstool/itstool-2.0.7.tar.bz2"
+  sha256 "6b9a7cd29a12bb95598f5750e8763cee78836a1a207f85b74d8b3275b27e87ca"
   license "GPL-3.0-or-later"
   revision 1
-  head "https://github.com/itstool/itstool.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/itstool"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "fbfc77345dc63340e7bfed0340671d34e12ce8d388070bdc3e833816c6bf6df0"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, mojave: "d7df12dd9d64c70c59da80ea8e65bdc10a2cb3f74e5cd701a48f9958c62def29"
   end
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
+  head do
+    url "https://github.com/itstool/itstool.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   depends_on "libxml2"
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   def install
-    python3 = "python3.10"
+    python3 = "python3.11"
     ENV.append_path "PYTHONPATH", Formula["libxml2"].opt_prefix/Language::Python.site_packages(python3)
 
-    system "./autogen.sh", "--prefix=#{libexec}", "PYTHON=#{which(python3)}"
+    configure = build.head? ? "./autogen.sh" : "./configure"
+    system configure, "--prefix=#{libexec}", "PYTHON=#{which(python3)}"
     system "make", "install"
 
-    bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files(libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"])
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files libexec/"bin", PYTHONPATH: ENV["PYTHONPATH"]
     pkgshare.install_symlink libexec/"share/itstool/its"
     man1.install_symlink libexec/"share/man/man1/itstool.1"
   end
