61cc4886f8cb3dc9f8730a09c734bc90d5a5e809
Adrian Ho <the.gromgit@gmail.com>
2023-01-04 02:30:30 +0800

    faust: update 2.50.6 bottle.

--- Formula/faust.rb
+++ Formula/faust.rb
@@ -1,24 +1,20 @@
 class Faust < Formula
   desc "Functional programming language for real time signal processing"
   homepage "https://faust.grame.fr"
-  url "https://github.com/grame-cncm/faust/releases/download/2.41.1/faust-2.41.1.tar.gz"
-  sha256 "72145e1d4ffcdd8e687ed7960d1d0717fa2c1dd2566e0bbc3a78fa95bb8b683e"
+  url "https://github.com/grame-cncm/faust/releases/download/2.50.6/faust-2.50.6.tar.gz"
+  sha256 "d8b7a89d82ee5d3259c8194c363296bc13353160f847661ab84b2dbefdee7173"
   license "GPL-2.0-or-later"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/faust"
-    sha256 cellar: :any, mojave: "a825472d524cacd54dec13b790d1014844b5e1bf720c0aae5522695910d7093d"
+    sha256 cellar: :any, mojave: "dbcc3c3f0822f80be8690bf3012fac347e060767a9ac825675ffd08d45a8affd"
   end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "libmicrohttpd"
   depends_on "libsndfile"
-  depends_on "llvm"
-
-  on_linux do
-    depends_on "gcc"
-  end
+  depends_on "llvm@14" # Needs LLVM 14 for `csound`.
 
   fails_with gcc: "5"
 
