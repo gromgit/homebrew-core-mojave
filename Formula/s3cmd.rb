b9561e8b1ba4a6a22a88c095a4f45a2487d1ee48
Adrian Ho <the.gromgit@gmail.com>
2023-01-04 03:53:22 +0800

    s3cmd: update 2.3.0 bottle.

--- Formula/s3cmd.rb
+++ Formula/s3cmd.rb
@@ -3,18 +3,17 @@ class S3cmd < Formula
 
   desc "Command-line tool for the Amazon S3 service"
   homepage "https://s3tools.org/s3cmd"
-  url "https://files.pythonhosted.org/packages/65/6c/f51ba2fbc74916f4fe3883228450306135e13be6dcca03a08d3e91239992/s3cmd-2.2.0.tar.gz"
-  sha256 "2a7d2afe09ce5aa9f2ce925b68c6e0c1903dd8d4e4a591cd7047da8e983a99c3"
+  url "https://files.pythonhosted.org/packages/97/10/5ae9b5c69d0482dda2927c67a4db26a3e9e064964577a81be9239a419b3f/s3cmd-2.3.0.tar.gz"
+  sha256 "15330776e7ff993d8ae0ac213bf896f210719e9b91445f5f7626a8fa7e74e30b"
   license "GPL-2.0-or-later"
-  revision 3
   head "https://github.com/s3tools/s3cmd.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/s3cmd"
-    sha256 cellar: :any_skip_relocation, mojave: "9cbfc380f0761ed6ea8d93b1adedcda917fe68681664ebe28068cf1d8d807950"
+    sha256 cellar: :any_skip_relocation, mojave: "a95e19ea56860f6961c452df9c2e35de2c38b6acc5586884ee24d82deb9ff541"
   end
 
-  depends_on "python@3.10"
+  depends_on "python@3.11"
   depends_on "six"
 
   resource "python-dateutil" do
@@ -23,8 +22,8 @@ class S3cmd < Formula
   end
 
   resource "python-magic" do
-    url "https://files.pythonhosted.org/packages/f7/46/fecfd32c126d26c8dd5287095cad01356ec0a761205f0b9255998bff96d1/python-magic-0.4.25.tar.gz"
-    sha256 "21f5f542aa0330f5c8a64442528542f6215c8e18d2466b399b0d9d39356d83fc"
+    url "https://files.pythonhosted.org/packages/da/db/0b3e28ac047452d079d375ec6798bf76a036a08182dbb39ed38116a49130/python-magic-0.4.27.tar.gz"
+    sha256 "c1ba14b08e4a5f5c31a302b7721239695b2f0f058d125bd5ce1ee36b9d9d3c3b"
   end
 
   def install
