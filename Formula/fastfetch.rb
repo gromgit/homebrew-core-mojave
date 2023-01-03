423b47b3763484ae2581db84b0e3d434ef10c84d
Adrian Ho <the.gromgit@gmail.com>
2023-01-04 01:46:40 +0800

    fastfetch: update 1.7.5 bottle.

--- Formula/fastfetch.rb
+++ Formula/fastfetch.rb
@@ -1,14 +1,14 @@
 class Fastfetch < Formula
   desc "Like neofetch, but much faster because written in C"
   homepage "https://github.com/LinusDierheimer/fastfetch"
-  url "https://github.com/LinusDierheimer/fastfetch/archive/refs/tags/1.7.4.tar.gz"
-  sha256 "eabf1e5af377901b92717a4a87b87da9bf6799a34880bf6110cf24316527c48e"
+  url "https://github.com/LinusDierheimer/fastfetch/archive/refs/tags/1.7.5.tar.gz"
+  sha256 "e9807568c2c5a10240c635e1e9ad5dbe63326eb730ca3aac005e19d91d2cd1c5"
   license "MIT"
   head "https://github.com/LinusDierheimer/fastfetch.git", branch: "dev"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fastfetch"
-    sha256 cellar: :any_skip_relocation, mojave: "bf8d1ef9cd469f3e0ac6918a9a6b74d5c09f5c2d5f62d4e450d6eaab73d66e7c"
+    sha256 mojave: "5fed52acf846ec327b437ea7b5a398cf3ede2d8bcc49a9bb7378b3ab81977baa"
   end
 
   depends_on "cmake" => :build
