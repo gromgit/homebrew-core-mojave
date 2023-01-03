45c4709a2f711fb538ad6a121c958c1ea20be6f1
Adrian Ho <the.gromgit@gmail.com>
2023-01-03 23:32:40 +0800

    eva: update 0.3.1 bottle.

--- Formula/eva.rb
+++ Formula/eva.rb
@@ -1,14 +1,14 @@
 class Eva < Formula
   desc "Calculator REPL, similar to bc(1)"
   homepage "https://github.com/NerdyPepper/eva/"
-  url "https://github.com/NerdyPepper/eva/archive/v0.3.0.tar.gz"
-  sha256 "05e2cdcfd91e6abef91cb01ad3074583b8289f6e74054e070bfbf6a4e684865e"
+  url "https://github.com/NerdyPepper/eva/archive/v0.3.1.tar.gz"
+  sha256 "d6a6eb8e0d46de1fea9bd00c361bd7955fcd7cc8f3310b786aad48c1dce7b3f7"
   license "MIT"
   head "https://github.com/NerdyPepper/eva.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/eva"
-    sha256 cellar: :any_skip_relocation, mojave: "2f74dfc0445bc9079af0ef73ff21da3034d3ebb76464ca4ee8042fe25f7ac04f"
+    sha256 cellar: :any_skip_relocation, mojave: "f057aa837c4f66c29411de622fb9fa5750cf184197a9044a192fb0960f7e5e80"
   end
 
   depends_on "rust" => :build
@@ -18,6 +18,6 @@ class Eva < Formula
   end
 
   test do
-    assert_equal "6.0", shell_output("#{bin}/eva '2 + abs(-4)'").strip
+    assert_equal "6.0000000000", shell_output("#{bin}/eva '2 + abs(-4)'").strip
   end
 end
