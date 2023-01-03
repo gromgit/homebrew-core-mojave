1cffe784f8f500796a8fa5bd27e1859d6258f470
Adrian Ho <the.gromgit@gmail.com>
2023-01-03 14:32:35 +0800

    epinio: update 1.5.2 bottle.

--- Formula/epinio.rb
+++ Formula/epinio.rb
@@ -1,13 +1,13 @@
 class Epinio < Formula
   desc "CLI for Epinio, the Application Development Engine for Kubernetes"
   homepage "https://epinio.io/"
-  url "https://github.com/epinio/epinio/archive/refs/tags/v1.3.0.tar.gz"
-  sha256 "4ac5924deb7b10119bcd51edd37bd32e20d3885cc760f23dbfe1a928564c0e32"
+  url "https://github.com/epinio/epinio/archive/refs/tags/v1.5.2.tar.gz"
+  sha256 "30f67a107c7a436f09e94b901f8377d15b28cafe3f8497910b824712084ee182"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/epinio"
-    sha256 cellar: :any_skip_relocation, mojave: "7efadcb7da40ecd0274d0c8be08a62d707655efb9be9209402b5ae9b8387dde0"
+    sha256 cellar: :any_skip_relocation, mojave: "e24c3deee4c6307339c52e4c73b8c9c8a2a7aae220a842ef4d56ef7eff0297ec"
   end
 
   depends_on "go" => :build
