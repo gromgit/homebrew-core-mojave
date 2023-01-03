b4f3fb1b37de50de9125432b84e1a3ca48e91321
Adrian Ho <the.gromgit@gmail.com>
2023-01-03 14:55:38 +0800

    erigon: update 2.32.0 bottle.

---
+++ Formula/erigon.rb
@@ -0,0 +1,59 @@
+class Erigon < Formula
+  desc "Implementation of Ethereum (execution client), on the efficiency frontier"
+  homepage "https://github.com/ledgerwatch/erigon"
+  url "https://github.com/ledgerwatch/erigon/archive/refs/tags/v2.32.0.tar.gz"
+  sha256 "965dc615d72e81981809e6e8c72e7fa6615962cf1eae7df45668714cc8486f90"
+  license all_of: ["GPL-3.0-or-later", "LGPL-3.0-or-later"]
+  head "https://github.com/ledgerwatch/erigon.git", branch: "devel"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/erigon"
+    sha256 cellar: :any_skip_relocation, mojave: "9a75980921848deb09dfbeb4652d8eee5a37aa232253142c6213f23b6272dcb0"
+  end
+
+  depends_on "gcc" => :build
+  depends_on "go" => :build
+  depends_on "make" => :build
+
+  def install
+    unless build.head?
+      ENV["GIT_COMMIT"] = "unknown"
+      ENV["GIT_BRANCH"] = "release"
+      ENV["GIT_TAG"] = "v#{version}"
+    end
+
+    system "make", "all"
+    bin.install Dir["build/bin/*"]
+  end
+
+  test do
+    (testpath/"genesis.json").write <<~EOS
+      {
+        "config": {
+          "homesteadBlock": 10
+        },
+        "nonce": "0",
+        "difficulty": "0x20000",
+        "mixhash": "0x00000000000000000000000000000000000000647572616c65787365646c6578",
+        "coinbase": "0x0000000000000000000000000000000000000000",
+        "timestamp": "0x00",
+        "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "extraData": "0x",
+        "gasLimit": "0x2FEFD8",
+        "alloc": {}
+      }
+    EOS
+    args = %W[
+      --datadir testchain
+      --log.dir.verbosity debug
+      --log.dir.path #{testpath}
+    ]
+    system "#{bin}/erigon", *args, "init", "genesis.json"
+    assert_predicate testpath/"debug-user.log", :exist?
+  end
+end
