f7b6a5b4319c63ff6df616cae2f763589df46553
Adrian Ho <the.gromgit@gmail.com>
2023-01-03 11:08:36 +0800

    mockery: update 2.15.0 bottle.

--- Formula/mockery.rb
+++ Formula/mockery.rb
@@ -1,20 +1,22 @@
 class Mockery < Formula
   desc "Mock code autogenerator for Golang"
   homepage "https://github.com/vektra/mockery"
-  url "https://github.com/vektra/mockery/archive/v2.14.0.tar.gz"
-  sha256 "1a87d16b264d21c65eb7c18b0a55a206798017bb48672ef8bc403b420dc0d5e8"
+  url "https://github.com/vektra/mockery/archive/v2.15.0.tar.gz"
+  sha256 "6d708828e4641c5c596376300363a24863975ff0a5567e26affc4197d498fed3"
   license "BSD-3-Clause"
   head "https://github.com/vektra/mockery.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/mockery"
-    sha256 cellar: :any_skip_relocation, mojave: "115e6598ce9005a1059c8f3ba2ae4a0c18f25231bdf5d494a7f492625b6e78ad"
+    sha256 cellar: :any_skip_relocation, mojave: "b2b97a6fffe3f45a54a2225fa0e4f9485bbcf785ea77cdf5f66bf5620e56ef2a"
   end
 
   depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/vektra/mockery/v2/pkg/config.SemVer=v#{version}")
+
+    generate_completions_from_executable(bin/"mockery", "completion")
   end
 
   test do
