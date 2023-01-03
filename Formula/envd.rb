df8fa48239734325a7bc6665abb5bc7905b5aaae
Adrian Ho <the.gromgit@gmail.com>
2023-01-03 13:53:12 +0800

    envd: update 0.3.0 bottle.

---
+++ Formula/envd.rb
@@ -0,0 +1,45 @@
+class Envd < Formula
+  desc "Reproducible development environment for AI/ML"
+  homepage "https://envd.tensorchord.ai"
+  url "https://github.com/tensorchord/envd/archive/v0.3.0.tar.gz"
+  sha256 "8e3da783a1bfe7c184e40f2bf872225b368ff3364cc94e38a9de5867624c3ff6"
+  license "Apache-2.0"
+  head "https://github.com/tensorchord/envd.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/envd"
+    sha256 cellar: :any_skip_relocation, mojave: "4c68df37948b0b2531a061c113b35cc5997580c814eebd8217d8763a2b89a68a"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["CGO_ENABLED"] = "0"
+    ldflags = %W[
+      -s -w
+      -X github.com/tensorchord/envd/pkg/version.buildDate=#{time.iso8601}
+      -X github.com/tensorchord/envd/pkg/version.version=#{version}
+      -X github.com/tensorchord/envd/pkg/version.gitTag=v#{version}
+      -X github.com/tensorchord/envd/pkg/version.gitCommit=#{version}-#{tap.user}
+      -X github.com/tensorchord/envd/pkg/version.gitTreeState=clean
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/envd"
+    generate_completions_from_executable(bin/"envd", "completion", "--no-install",
+                                         shell_parameter_format: "--shell=",
+                                         shells:                 [:bash, :zsh])
+  end
+
+  test do
+    output = shell_output("#{bin}/envd version --short")
+    assert_equal "envd: v#{version}", output.strip
+
+    expected = if OS.mac?
+      "error: Cannot connect to the Docker daemon"
+    else
+      "error: permission denied"
+    end
+
+    stderr = shell_output("#{bin}/envd env list 2>&1", 1)
+    assert_match expected, stderr
+  end
+end
