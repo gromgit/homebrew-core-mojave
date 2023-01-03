9076d5d85a7f665b4b5c534d9c4888121114d3fd
Adrian Ho <the.gromgit@gmail.com>
2023-01-03 10:29:54 +0800

    einstein: update 2.1.1_1 bottle.

--- Formula/einstein.rb
+++ Formula/einstein.rb
@@ -3,13 +3,14 @@ class Einstein < Formula
   homepage "https://github.com/lksj/einstein-puzzle"
   url "https://github.com/lksj/einstein-puzzle/archive/refs/tags/v2.1.1.tar.gz"
   sha256 "46cf0806c3792b995343e46bec02426065f66421c870781475d6d365522c10fc"
+  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/einstein"
-    sha256 cellar: :any, mojave: "9814fd8121a68d78ffa9d8e2e4174b6f37ba4fcb75c65e0ad0a585f1c73ab5be"
+    sha256 cellar: :any, mojave: "e0a7cf1c8a0e00e0a7bf999ea455c8087379217eb826d444ef79c4b6e26a2d82"
   end
 
-  depends_on "sdl"
+  depends_on "sdl12-compat"
   depends_on "sdl_mixer"
   depends_on "sdl_ttf"
 
