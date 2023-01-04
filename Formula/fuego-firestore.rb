ae8fd10fe4376265759c6d5042af7528aefd9254
Adrian Ho <the.gromgit@gmail.com>
2023-01-04 23:04:02 +0800

    fuego-firestore: update 0.33.0 bottle.

--- Formula/fuego-firestore.rb
+++ Formula/fuego-firestore.rb
@@ -1,13 +1,13 @@
 class FuegoFirestore < Formula
   desc "Command-line client for the Firestore database"
   homepage "https://github.com/sgarciac/fuego"
-  url "https://github.com/sgarciac/fuego/archive/refs/tags/0.32.0.tar.gz"
-  sha256 "ed16bcdef7e3463fbd12bd5daba80b36401ed0ea2ea4acc699eecde3d91b17fd"
+  url "https://github.com/sgarciac/fuego/archive/refs/tags/0.33.0.tar.gz"
+  sha256 "25281f2242fe41b0533255a0d4f0450b1f3f8622d1585f8ae8cda1b116ca75d0"
   license "GPL-3.0-only"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fuego-firestore"
-    sha256 cellar: :any_skip_relocation, mojave: "dea5e0b7031c86b676125e5a127c076095605027f41e22faad23c10aaebe25c8"
+    sha256 cellar: :any_skip_relocation, mojave: "2235fe1fb651aa3b822067d0f6752e86d5e16a48aba2995e11f4de653cb5da5e"
   end
 
   depends_on "go" => :build
