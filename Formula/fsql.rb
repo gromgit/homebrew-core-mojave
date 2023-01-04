6aae687c97e2a2902a36414f923ae41c3f14bf21
Adrian Ho <the.gromgit@gmail.com>
2023-01-04 22:54:36 +0800

    fsql: update 0.5.1 bottle.

--- Formula/fsql.rb
+++ Formula/fsql.rb
@@ -1,14 +1,13 @@
 class Fsql < Formula
   desc "Search through your filesystem with SQL-esque queries"
   homepage "https://github.com/kashav/fsql"
-  url "https://github.com/kashav/fsql/archive/v0.4.0.tar.gz"
-  sha256 "5f028446e31f1a8be2f8a72cd2c1ae888e748220e4c4ece38a62fd8fe41bf70a"
+  url "https://github.com/kashav/fsql/archive/v0.5.1.tar.gz"
+  sha256 "743ab740e368f80fa7cb076679b8d72a5aa13c2a10e5c820608558ed1d7634bc"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fsql"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "58c68c15d5d0d8003e083cac83a5b7966498280eaaf2751d9064aee30bc58374"
+    sha256 cellar: :any_skip_relocation, mojave: "30a2434bfd0c07a2c24006625b77c1ef40930d8c105a1f4af9b04075e98bb190"
   end
 
   # Bump to 1.18 on the next release, if possible.
@@ -25,7 +24,7 @@ class Fsql < Formula
     assert_match %r{^foo\s+foo/baz.txt$}, shell_output(cmd)
     cmd = "#{bin}/fsql SELECT name FROM . WHERE name = bar.txt"
     assert_equal "bar.txt", shell_output(cmd).chomp
-    cmd = "#{bin}/fsql SELECT name FROM . WHERE FORMAT\\(size\, GB\\) \\> 500"
+    cmd = "#{bin}/fsql SELECT name FROM . WHERE FORMAT\\(size, GB\\) \\> 500"
     assert_equal "", shell_output(cmd)
   end
 end
