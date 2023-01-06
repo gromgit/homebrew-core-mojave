2a9a56f467560a2acc2acca1d1f722a12519fe73
Adrian Ho <the.gromgit@gmail.com>
2023-01-06 18:31:51 +0800

    fruit: add 3.7.0 bottle.

--- Formula/fruit.rb
+++ Formula/fruit.rb
@@ -1,21 +1,13 @@
 class Fruit < Formula
   desc "Dependency injection framework for C++"
   homepage "https://github.com/google/fruit/wiki"
-  url "https://github.com/google/fruit/archive/v3.6.0.tar.gz"
-  sha256 "b35b9380f3affe0b3326f387505fa80f3584b0d0a270362df1f4ca9c39094eb5"
+  url "https://github.com/google/fruit/archive/v3.7.0.tar.gz"
+  sha256 "134d65c8e6dff204aeb771058c219dcd9a353853e30a3961a5d17a6cff434a09"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "478ae15d5a3a1592f5c72ba379b2425930cf02ae0d6fe20958eb12d3c533f653"
-    sha256 cellar: :any,                 arm64_monterey: "c42c1879ad107e5ee827ad6bcd18097575bf8818c3d7b6147a5aa33d166bec0a"
-    sha256 cellar: :any,                 arm64_big_sur:  "1efe400614d7043d482a1983f780a4d4792112e039a5939e0fb0e5cdc64d2ad6"
-    sha256 cellar: :any,                 ventura:        "3f85771adb643e352cdbd0b5f2ff7dc0a39abf5a33f05a4d471835e34ad8c448"
-    sha256 cellar: :any,                 monterey:       "ac4e32b4cce26112871dcc8d01fd93a5ccbeac0dbae23a6d4bced050cea99eeb"
-    sha256 cellar: :any,                 big_sur:        "a08a4deb118150ef8237de0dfe4ac6215a729b504f25881950e0113016a9011b"
-    sha256 cellar: :any,                 catalina:       "10f1081e14b11a547b36020cdfa75486fac42036389b37d2df831f586fc78429"
-    sha256 cellar: :any,                 mojave:         "fc0a6e56340a21a4548f589d38df7b52bd6edabb483e6c6e0a9fe8605a373a8f"
-    sha256 cellar: :any,                 high_sierra:    "73e0c030fb7984d5b3b72d11410ca2f30c4e5a66ba183070fc3ae8a919ea5094"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0a526dc1dff57ea51ec8256439697d5f061c3b99085e4b41281de2512ba45716"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fruit"
+    sha256 cellar: :any, mojave: "8982e8f5d710786ba850c35696ad0542dc52063e4e55e4bf7429f43ee775ee87"
   end
 
   depends_on "cmake" => :build
