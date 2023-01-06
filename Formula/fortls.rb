1f04387f872a3b4ac316a0f7d9f468fc99831251
Adrian Ho <the.gromgit@gmail.com>
2023-01-06 12:13:28 +0800

    fortls: add 2.13.0 bottle.

--- Formula/fortls.rb
+++ Formula/fortls.rb
@@ -3,30 +3,33 @@ class Fortls < Formula
 
   desc "Fortran language server"
   homepage "https://gnikit.github.io/fortls"
-  url "https://pypi.io/packages/source/f/fortls/fortls-2.13.0.tar.gz"
+  url "https://files.pythonhosted.org/packages/38/04/db988efbcaac142999af91888e9750dfa422108a318ec3038c2cd42ecf04/fortls-2.13.0.tar.gz"
   sha256 "23c5013e8dd8e1d65bf07be610d0827bc48aa7331a7a7ce13612d4c646d0db31"
   license "MIT"
   head "https://github.com/gnikit/fortls.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fortls"
-    sha256 cellar: :any_skip_relocation, mojave: "566f2fa3a271c33149a7c5f81ef985b4ab1c805de6384460801e8cdca6d5b307"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "70cf583ded8a5e2d6eb47b07a836e03e97633aab61c94c4e36e1c5845196dd35"
   end
 
-  depends_on "python@3.10"
+  depends_on "python@3.11"
+
+  conflicts_with "fortran-language-server", because: "both install `fortls` binaries"
 
   resource "json5" do
-    url "https://pypi.io/packages/source/j/json5/json5-0.9.5.tar.gz"
-    sha256 "703cfee540790576b56a92e1c6aaa6c4b0d98971dc358ead83812aa4d06bdb96"
+    url "https://files.pythonhosted.org/packages/47/12/611bf15000c1fc54af909565aed1ad045e5ae1890d8c56cbfe5ceaf52446/json5-0.9.10.tar.gz"
+    sha256 "ad9f048c5b5a4c3802524474ce40a622fae789860a86f10cc4f7e5f9cf9b46ab"
   end
 
   resource "packaging" do
-    url "https://pypi.io/packages/source/p/packaging/packaging-21.3.tar.gz"
+    url "https://files.pythonhosted.org/packages/df/9e/d1a7217f69310c1db8fdf8ab396229f55a699ce34a203691794c5d1cad0c/packaging-21.3.tar.gz"
     sha256 "dd47c42927d89ab911e606518907cc2d3a1f38bbd026385970643f9c5b8ecfeb"
   end
 
   resource "pyparsing" do
-    url "https://pypi.io/packages/source/p/pyparsing/pyparsing-3.0.9.tar.gz"
+    url "https://files.pythonhosted.org/packages/71/22/207523d16464c40a0310d2d4d8926daffa00ac1f5b1576170a32db749636/pyparsing-3.0.9.tar.gz"
     sha256 "2b020ecf7d21b687f219b71ecad3631f644a47f01403fa1d1036b0c6416d70fb"
   end
 
@@ -34,7 +37,7 @@ class Fortls < Formula
     virtualenv_install_with_resources
 
     # Disable automatic update check
-    rm bin/"fortls"
+    (bin/"fortls").unlink
     # Replace with `exec python3 -m fortls --disable_autoupdate "$@"` in the future
     (bin/"fortls").write <<~EOS
       #!#{libexec}/bin/python3
@@ -45,7 +48,7 @@ class Fortls < Formula
       from fortls.__init__ import main
 
       if __name__ == '__main__':
-          sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+          sys.argv[0] = re.sub(r'(-script.pyw?|.exe)?$', '', sys.argv[0])
           sys.argv.append('--disable_autoupdate')
           sys.exit(main())
     EOS
