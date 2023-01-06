9a19817b54444411b1eb8075bcb0d59e5c3593d3
Adrian Ho <the.gromgit@gmail.com>
2023-01-07 01:33:33 +0800

    g3log: update 2.1 bottle.

--- Formula/g3log.rb
+++ Formula/g3log.rb
@@ -1,25 +1,21 @@
 class G3log < Formula
   desc "Asynchronous, 'crash safe', logger that is easy to use"
   homepage "https://github.com/KjellKod/g3log"
-  url "https://github.com/KjellKod/g3log/archive/1.3.4.tar.gz"
-  sha256 "2fe8815e5f5afec6b49bdfedfba1e86b8e58a5dc89fd97f4868fb7f3141aed19"
+  url "https://github.com/KjellKod/g3log/archive/2.1.tar.gz"
+  sha256 "13c9d8cc0387792301f264c4f623618fc4dea9814d9b5844931ffbfd9aafb1fe"
   license "Unlicense"
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "97f67f26de801800280fb37f686a862ae12d5d422d01f960950cdea78de56b9c"
-    sha256 cellar: :any,                 arm64_big_sur:  "d6ab9b85de4f0bc70d278210ac4a89c2780b4a271dc474fdd2a4ac16933a3d38"
-    sha256 cellar: :any,                 monterey:       "d4fa3e742bfa3fb54e89abcb97c9d0fadcfbcc064ee8fa828e9a2783d8138232"
-    sha256 cellar: :any,                 big_sur:        "5e24eda970bf16a1d737e0112ef7e86651c6cdd29b14b6dd4beec2faf9f9d292"
-    sha256 cellar: :any,                 catalina:       "3325a5a22c63c02f6c3a7d9b35f533e579f369ff2871f7152d0ca4994bb049d3"
-    sha256 cellar: :any,                 mojave:         "f44e98ef652573827da51288539acb1122af634b79f61f8ec2687b7b5184e971"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6dbe7ea8df080a2e437eca1bc85bf23ff34a0b5c86078e112617189aea8487cc"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/g3log"
+    sha256 cellar: :any, mojave: "68bbc80d694f64b85932d3bb2decb58afac2bb85812af1f3560e9bccf5af5fcc"
   end
 
   depends_on "cmake" => :build
 
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
@@ -36,7 +32,7 @@ class G3log < Formula
         return 0;
       }
     EOS
-    system ENV.cxx, "-std=c++14", "test.cpp", "-L#{lib}", "-lg3log", "-o", "test"
+    system ENV.cxx, "-std=c++17", "test.cpp", "-L#{lib}", "-lg3log", "-o", "test"
     system "./test"
     Dir.glob(testpath/"test.g3log.*.log").any?
   end
