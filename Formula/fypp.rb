a611ecaad2e7c4b91be94c8426a8c1d4e5eb4f6e
Adrian Ho <the.gromgit@gmail.com>
2023-01-06 22:46:59 +0800

    fypp: add 3.1 bottle.

--- Formula/fypp.rb
+++ Formula/fypp.rb
@@ -6,22 +6,23 @@ class Fypp < Formula
   url "https://github.com/aradi/fypp/archive/refs/tags/3.1.tar.gz"
   sha256 "0f66e849869632978a8a0623ee510bb860a74004fdabfbfb542656c6c1a7eb5a"
   license "BSD-2-Clause"
-  head "https://github.com/aradi/fypp.git", branch: "master"
+  head "https://github.com/aradi/fypp.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fypp"
-    sha256 cellar: :any_skip_relocation, mojave: "20d62a61da2cc4ff53250237b0545572ace9952699dcb23783ca398714e9beff"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "51a4b53b4f3493e7d2c9452561be9200d1dee62503e322f85a5ce76ce1dec3cb"
   end
 
   depends_on "gcc" => :test
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   def install
     virtualenv_install_with_resources
   end
 
   test do
-    system "#{bin}/fypp", "--version"
+    system bin/"fypp", "--version"
     (testpath/"hello.F90").write <<~EOS
       program hello
       #:for val in [_SYSTEM_, _MACHINE_, _FILE_, _LINE_]
@@ -29,7 +30,7 @@ class Fypp < Formula
       #:endfor
       end
     EOS
-    system "#{bin}/fypp", testpath/"hello.F90", testpath/"hello.f90"
+    system bin/"fypp", testpath/"hello.F90", testpath/"hello.f90"
     ENV.fortran
     system ENV.fc, testpath/"hello.f90", "-o", testpath/"hello"
     system testpath/"hello"
