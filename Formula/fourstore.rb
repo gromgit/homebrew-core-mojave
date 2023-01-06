520a09ebbcc4dd1a7843d5d7234d9009e0cb1fc9
Adrian Ho <the.gromgit@gmail.com>
2023-01-06 12:20:54 +0800

    fourstore: add 1.1.6_2 bottle.

--- Formula/fourstore.rb
+++ Formula/fourstore.rb
@@ -1,6 +1,7 @@
 class Fourstore < Formula
   desc "Efficient, stable RDF database"
   homepage "https://github.com/4store/4store"
+  # NOTE: Try building without `avahi` at version bump.
   url "https://github.com/4store/4store/archive/v1.1.6.tar.gz"
   sha256 "a0c8143fcceeb2f1c7f266425bb6b0581279129b86fdd10383bf1c1e1cab8e00"
   license "GPL-3.0"
@@ -8,7 +9,8 @@ class Fourstore < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fourstore"
-    sha256 mojave: "590050a99a855172c43937bdda0b7f9075466c803704520071e43fa3e40c933e"
+    rebuild 1
+    sha256 mojave: "7ec2e4cffdd92f0af3a365efdfc324229120f4b8b94677d005e8aecb633f7ac6"
   end
 
   depends_on "autoconf" => :build
@@ -23,10 +25,28 @@ class Fourstore < Formula
   depends_on "rasqal"
   depends_on "readline"
 
+  # TODO: Check if this dependency can be removed at version bump.
+  on_linux do
+    depends_on "avahi"
+  end
+
+  # Fix build on Linux. Remove on the next release
+  # sigval_t.h:13:3: error: #error "sigval_t defined for standard not including union sigval"
+  patch do
+    on_linux do
+      url "https://github.com/4store/4store/commit/c5a56d7c7504551a1f2fff6c16c4d9a440e4a317.patch?full_index=1"
+      sha256 "f476aa38098aaaebf970603a5003aae40fa43f3243890e404f799be8bdbf0aad"
+    end
+  end
+
   def install
+    # Work around failure from GCC 10+ using default of `-fno-common`
+    # /usr/bin/ld: query.o:(.bss+0x0): multiple definition of `rasqal_mutex'
+    ENV.append_to_cflags "-fcommon" if OS.linux?
     # Upstream issue garlik/4store#138
     # Otherwise .git directory is needed
     (buildpath/".version").write("v1.1.6")
+
     system "./autogen.sh"
     (var/"fourstore").mkpath
     system "./configure", "--prefix=#{prefix}",
