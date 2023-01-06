bac1ca0a7b25e60c67a3912405a6cddfb75a184d
Adrian Ho <the.gromgit@gmail.com>
2023-01-07 04:34:27 +0800

    gaze: update 1.1.5 bottle.

--- Formula/gaze.rb
+++ Formula/gaze.rb
@@ -1,14 +1,13 @@
 class Gaze < Formula
   desc "Execute commands for you"
   homepage "https://github.com/wtetsu/gaze"
-  url "https://github.com/wtetsu/gaze/archive/refs/tags/v1.1.3.tar.gz"
-  sha256 "53c46a09f477433e9105b9df8db9ddf287fae6734b3078d79f5f994500e8625d"
+  url "https://github.com/wtetsu/gaze/archive/refs/tags/v1.1.5.tar.gz"
+  sha256 "e954f074bcd3e7f8c898bf83faecfe5f2acff7001f1001157798c9e723916aab"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gaze"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "50be79c8fb492dccc9a587b2e8fd0008f6916c22bfbe9ac114405acc9dd213e1"
+    sha256 cellar: :any_skip_relocation, mojave: "621647564764fd31765cd66e03ae804d483caf3cf0e028f254969c72a6190ecc"
   end
 
   depends_on "go" => :build
