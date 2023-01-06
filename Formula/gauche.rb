300c58b4545d274ad50f972b1100d36030512bc1
Adrian Ho <the.gromgit@gmail.com>
2023-01-07 04:20:48 +0800

    gauche: update 0.9.12_2 bottle.

--- Formula/gauche.rb
+++ Formula/gauche.rb
@@ -4,28 +4,32 @@ class Gauche < Formula
   url "https://github.com/shirok/Gauche/releases/download/release0_9_12/Gauche-0.9.12.tgz"
   sha256 "b4ae64921b07a96661695ebd5aac0dec813d1a68e546a61609113d7843f5b617"
   license "BSD-3-Clause"
-  revision 1
+  revision 2
 
   livecheck do
     url :stable
-    strategy :github_latest
-    regex(/href=.*?Gauche[._-]v?(\d+(?:\.\d+)+(?:[._-]p\d+)?)\.t/i)
+    regex(%r{href=["']?[^"' >]*?/tag/\D*?(\d+(?:[._]\d+)+(?:[._-]?p\d+)?)["' >]}i)
+    strategy :github_latest do |page, regex|
+      page.scan(regex).map { |match| match[0].tr("_", ".") }
+    end
   end
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gauche"
-    rebuild 1
-    sha256 mojave: "bf9d9ad9ba17dc84706d78a0a6f12cc70db94f1e0fe5b94a94ed033b632ec043"
+    sha256 mojave: "75e843ceddb42b67f2942aa89389a7bb4312b1082623467eebb067e7388f771f"
   end
 
+  depends_on "ca-certificates"
   depends_on "mbedtls"
 
   uses_from_macos "libxcrypt"
   uses_from_macos "zlib"
 
   def install
-    system "./configure", *std_configure_args,
-                          "--enable-multibyte=utf-8"
+    system "./configure",
+           *std_configure_args,
+           "--enable-multibyte=utf-8",
+           "--with-ca-bundle=#{HOMEBREW_PREFIX}/share/ca-certificates/cacert.pem"
     system "make"
     system "make", "install"
   end
