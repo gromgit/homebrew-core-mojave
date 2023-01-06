b613f90dced82914d3c9b25b468058f337d383f7
Adrian Ho <the.gromgit@gmail.com>
2023-01-06 12:02:59 +0800

    fnm: add 1.33.1 bottle.

--- Formula/fnm.rb
+++ Formula/fnm.rb
@@ -1,8 +1,8 @@
 class Fnm < Formula
   desc "Fast and simple Node.js version manager"
   homepage "https://github.com/Schniz/fnm"
-  url "https://github.com/Schniz/fnm/archive/v1.31.1.tar.gz"
-  sha256 "ddb7bde503ef990c95c762863f4c858499f17c00d8e6ded7885b4fbbf1600250"
+  url "https://github.com/Schniz/fnm/archive/v1.33.1.tar.gz"
+  sha256 "84a2173a47f942d1247a08348a20b3cdf4cb906b9f0a662585dc1784256d73c2"
   license "GPL-3.0-only"
   head "https://github.com/Schniz/fnm.git", branch: "master"
 
@@ -13,8 +13,7 @@ class Fnm < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/fnm"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "2d2eec91fce99f9134dff2ef9483ae9544bd83081c2d575c6ba3ac7cd9944254"
+    sha256 cellar: :any_skip_relocation, mojave: "58e477dc6f4a4de3546b2b96bfb5f831e844d18a60f21f76e63eb3a12ad7816d"
   end
 
   depends_on "rust" => :build
@@ -28,7 +27,7 @@ class Fnm < Formula
   end
 
   test do
-    system bin/"fnm", "install", "12.0.0"
-    assert_match "v12.0.0", shell_output("#{bin}/fnm exec --using=12.0.0 -- node --version")
+    system bin/"fnm", "install", "19.0.1"
+    assert_match "v19.0.1", shell_output("#{bin}/fnm exec --using=19.0.1 -- node --version")
   end
 end
