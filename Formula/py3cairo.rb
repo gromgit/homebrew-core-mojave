3bcd2e49babd4ccc97ca788d32d4fd2cd05e2cae
Adrian Ho <the.gromgit@gmail.com>
2023-01-07 22:11:16 +0800

    py3cairo: update 1.23.0 bottle.

--- Formula/py3cairo.rb
+++ Formula/py3cairo.rb
@@ -1,20 +1,18 @@
 class Py3cairo < Formula
   desc "Python 3 bindings for the Cairo graphics library"
   homepage "https://cairographics.org/pycairo/"
-  url "https://github.com/pygobject/pycairo/releases/download/v1.21.0/pycairo-1.21.0.tar.gz"
-  sha256 "251907f18a552df938aa3386657ff4b5a4937dde70e11aa042bc297957f4b74b"
+  url "https://github.com/pygobject/pycairo/releases/download/v1.23.0/pycairo-1.23.0.tar.gz"
+  sha256 "9b61ac818723adc04367301317eb2e814a83522f07bbd1f409af0dada463c44c"
   license any_of: ["LGPL-2.1-only", "MPL-1.1"]
-  revision 1
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/py3cairo"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "625d540e11d87b84881457749feb182f6a6e1cc174adca3c11fedb253b5f2614"
+    sha256 cellar: :any, mojave: "a93d345210fbe732d98a8b52b8d3c2ddc2aa6c891688994fc80898609b448da2"
   end
 
   depends_on "pkg-config" => :build
   depends_on "python@3.10" => [:build, :test]
-  depends_on "python@3.9" => [:build, :test]
+  depends_on "python@3.11" => [:build, :test]
   depends_on "cairo"
 
   def pythons
