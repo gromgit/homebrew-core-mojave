bcbf9a2e3d13ebb04a59f4787f805f26271131fa
Adrian Ho <the.gromgit@gmail.com>
2023-01-07 19:04:39 +0800

    genometools: update 1.6.2_1 bottle.

--- Formula/genometools.rb
+++ Formula/genometools.rb
@@ -5,24 +5,28 @@ class Genometools < Formula
   url "https://github.com/genometools/genometools/archive/v1.6.2.tar.gz"
   sha256 "974825ddc42602bdce3d5fbe2b6e2726e7a35e81b532a0dc236f6e375d18adac"
   license "ISC"
+  revision 1
   head "https://github.com/genometools/genometools.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/genometools"
-    rebuild 1
-    sha256 cellar: :any, mojave: "622530231e77a868323b7e5ea6375d1f46f8cd07b20e1ffe941d935ebd6853e9"
+    sha256 cellar: :any, mojave: "5c74c1b5e87e5101677c6374a1569373a4195c249a7cc0c6555cd46d86caa741"
   end
 
   depends_on "pkg-config" => :build
   depends_on "cairo"
   depends_on "pango"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   on_linux do
     depends_on "libpthread-stubs" => :build
   end
 
   conflicts_with "libslax", because: "both install `bin/gt`"
+
+  def python3
+    "python3.10"
+  end
 
   def install
     system "make", "prefix=#{prefix}"
@@ -34,13 +38,13 @@ class Genometools < Formula
         "gtlib = CDLL(\"libgenometools\" + soext)",
         "gtlib = CDLL(\"#{lib}/libgenometools\" + soext)"
 
-      system "python3", *Language::Python.setup_install_args(prefix)
-      system "python3", "-m", "unittest", "discover", "tests"
+      system python3, *Language::Python.setup_install_args(prefix, python3)
+      system python3, "-m", "unittest", "discover", "tests"
     end
   end
 
   test do
     system "#{bin}/gt", "-test"
-    system Formula["python@3.9"].opt_bin/"python3", "-c", "import gt"
+    system python3, "-c", "import gt"
   end
 end
