0f02378dfc7ffa69ff2caba0dc44ffb5d5ecb0b0
Adrian Ho <the.gromgit@gmail.com>
2023-01-09 01:01:43 +0800

    glslviewer: update 3.10.1 bottle.

--- Formula/glslviewer.rb
+++ Formula/glslviewer.rb
@@ -2,26 +2,20 @@ class Glslviewer < Formula
   desc "Live-coding console tool that renders GLSL Shaders"
   homepage "http://patriciogonzalezvivo.com/2015/glslViewer/"
   url "https://github.com/patriciogonzalezvivo/glslViewer.git",
-      tag:      "2.1.2",
-      revision: "c6eaf01456db4baa61f876762fdb2d8bf49727e4"
+      tag:      "v3.10.1",
+      revision: "2671e0f0b362bfd94ea5160f2ecb7f7363d4991d"
   license "BSD-3-Clause"
   head "https://github.com/patriciogonzalezvivo/glslViewer.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/glslviewer"
-    sha256 cellar: :any, mojave: "96a6cfc8cf6885177e8d6d08ada1e2b2402f25cf6a3acb4af903d390412053a1"
+    sha256 cellar: :any, mojave: "120700a6dfeb9c4a6ae297c23298b8c0b967c440ce4c0d5d9d93b145368e57fe"
   end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
-  depends_on "ffmpeg@4"
+  depends_on "ffmpeg"
   depends_on "glfw"
-
-  on_linux do
-    depends_on "gcc"
-  end
-
-  fails_with gcc: "5" # rubberband is built with GCC
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
@@ -32,8 +26,8 @@ class Glslviewer < Formula
   end
 
   test do
-    cp_r "#{pkgshare}/examples/2D/01_buffers/.", testpath
-    pid = fork { exec "#{bin}/glslViewer", "00_ripples.frag", "-l" }
+    cp_r "#{pkgshare}/examples/io/.", testpath
+    pid = fork { exec "#{bin}/glslViewer", "orca.frag", "-l" }
   ensure
     Process.kill("HUP", pid)
   end
