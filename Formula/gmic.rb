7ffaada145a6dcff7c59917368d478ca330c0a59
Adrian Ho <the.gromgit@gmail.com>
2023-01-09 01:50:02 +0800

    gmic: update 3.1.6 bottle.

--- Formula/gmic.rb
+++ Formula/gmic.rb
@@ -1,8 +1,8 @@
 class Gmic < Formula
   desc "Full-Featured Open-Source Framework for Image Processing"
   homepage "https://gmic.eu/"
-  url "https://gmic.eu/files/source/gmic_3.0.1.tar.gz"
-  sha256 "6cc20a20e3ab53ce485ccf6e044a30141b3d62cf7743b83bb04906ff29453035"
+  url "https://gmic.eu/files/source/gmic_3.1.6.tar.gz"
+  sha256 "e73d63ba6520637c782b6e5e6805037a27b6255c011c9e2a60a56f9c28822ce4"
   license "CECILL-2.1"
   head "https://github.com/dtschump/gmic.git", branch: "master"
 
@@ -13,22 +13,50 @@ class Gmic < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gmic"
-    sha256 cellar: :any, mojave: "56be301a074c6c3d14f756cd65fa4b81b282716349365bf22eac88b3ed278bd8"
+    sha256 cellar: :any, mojave: "bc06b00077921f05ac4d0c2ef3256b691cf4efba5fd9300b8dbb8362133e59af"
   end
 
-  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
   depends_on "fftw"
-  depends_on "jpeg"
+  depends_on "jpeg-turbo"
   depends_on "libpng"
   depends_on "libtiff"
+  depends_on "openexr"
+
+  uses_from_macos "curl"
+  uses_from_macos "zlib"
+
+  # Use .dylibs instead of .so on macOS
+  patch do
+    on_macos do
+      url "https://raw.githubusercontent.com/macports/macports-ports/a859c5929c929548f5156f5cab13a2f341982e72/science/gmic/files/patch-src-Makefile.diff"
+      sha256 "5b4914a05135f6c137bb5980d0c3bf8d94405f03d4e12b6ee38bd0e0e004a358"
+      directory "src"
+    end
+  end
 
   def install
-    system "cmake", *std_cmake_args,
-                    "-DENABLE_FFMPEG=OFF",
-                    "-DENABLE_OPENCV=OFF",
-                    "-DENABLE_OPENEXR=OFF",
-                    "-DENABLE_X=OFF"
-    system "make", "install"
+    # The Makefile is not safe to run in parallel.
+    # Issue ref: https://github.com/dtschump/gmic/issues/406
+    ENV.deparallelize
+
+    # Use PLUGINDIR to avoid trying to create "/plug-ins" on Linux without GIMP.
+    # Disable X11 by using the values from Makefile when "/usr/X11" doesn't exist.
+    args = %W[
+      PLUGINDIR=#{buildpath}/plug-ins
+      USR=#{prefix}
+      X11_CFLAGS=-Dcimg_display=0
+      X11_LIBS=-lpthread
+      SOVERSION=#{version}
+    ]
+    system "make", "lib", "cli_shared", *args
+    system "make", "install", *args
+    lib.install "src/libgmic.a"
+
+    # Need gmic binary to build completions
+    ENV.prepend_path "PATH", bin
+    system "make", "bashcompletion", *args
+    bash_completion.install "resources/gmic_bashcompletion.sh" => "gmic"
   end
 
   test do
