5ed35eb1aac3791667b300d4ce8482c1adeebff6
Adrian Ho <the.gromgit@gmail.com>
2023-01-09 00:29:02 +0800

    glooctl: update 1.12.37 bottle.

--- Formula/glooctl.rb
+++ Formula/glooctl.rb
@@ -4,8 +4,8 @@ class Glooctl < Formula
   # NOTE: Please wait until the newest stable release is finished building and
   # no longer marked as "Pre-release" before creating a PR for a new version.
   url "https://github.com/solo-io/gloo.git",
-      tag:      "v1.12.29",
-      revision: "4755de01751fc76bad75c4336c2f2a9fb74e8a20"
+      tag:      "v1.12.37",
+      revision: "e912fc966e14b0db70dff17469c22c2358761ba4"
   license "Apache-2.0"
   head "https://github.com/solo-io/gloo.git", branch: "master"
 
@@ -16,13 +16,13 @@ class Glooctl < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/glooctl"
-    sha256 cellar: :any_skip_relocation, mojave: "7ab575ef68682bc2fc3eb87241c0c82dcf0c551285092c0ac0d6d503c972a592"
+    sha256 cellar: :any_skip_relocation, mojave: "9537e972ec06c8f56f10c1dd53f7e6d7bb989b88c43eee78a3f35eafdb54924e"
   end
 
   depends_on "go" => :build
 
   def install
-    system "make", "glooctl", "VERSION=v#{version}"
+    system "make", "glooctl", "VERSION=#{version}"
     bin.install "_output/glooctl"
 
     generate_completions_from_executable(bin/"glooctl", "completion", shells: [:bash, :zsh])
@@ -33,7 +33,7 @@ class Glooctl < Formula
     assert_match "glooctl is the unified CLI for Gloo.", run_output
 
     version_output = shell_output("#{bin}/glooctl version 2>&1")
-    assert_match "Client: {\"version\":\"v#{version}\"}", version_output
+    assert_match "Client: {\"version\":\"#{version}\"}", version_output
     assert_match "Server: version undefined", version_output
 
     # Should error out as it needs access to a Kubernetes cluster to operate correctly
