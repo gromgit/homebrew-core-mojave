14d1ccf1fea82d5ef6064627e88f52dc8ff1f3d4
Adrian Ho <the.gromgit@gmail.com>
2023-01-08 20:17:32 +0800

    gitleaks: update 8.15.2 bottle.

--- Formula/gitleaks.rb
+++ Formula/gitleaks.rb
@@ -1,13 +1,13 @@
 class Gitleaks < Formula
   desc "Audit git repos for secrets"
   homepage "https://github.com/zricethezav/gitleaks"
-  url "https://github.com/zricethezav/gitleaks/archive/v8.14.0.tar.gz"
-  sha256 "0dd51bf638457c7c9a734db11e78b01afcf7d0d45db1716a597bf9e9f048fcf5"
+  url "https://github.com/zricethezav/gitleaks/archive/v8.15.2.tar.gz"
+  sha256 "8a4b742ad4a0c7fd125b7293bc34bbb25a1b0f7db2e20a457146b1cbe4fa38e1"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gitleaks"
-    sha256 cellar: :any_skip_relocation, mojave: "ef6d20b82284ee2d8a66056c96fc6829439815b6ffb7f1ec35f93cd18b0f25c5"
+    sha256 cellar: :any_skip_relocation, mojave: "c17a84f8e12b4e93dd634403e6bdcd528e267a7fe9478392dc9da55439685ebc"
   end
 
   depends_on "go" => :build
