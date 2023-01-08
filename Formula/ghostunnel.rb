8eef18f6c583164d73cc59010cea88fa880b68ff
Adrian Ho <the.gromgit@gmail.com>
2023-01-08 15:01:48 +0800

    ghostunnel: update 1.7.1 bottle.

--- Formula/ghostunnel.rb
+++ Formula/ghostunnel.rb
@@ -1,14 +1,14 @@
 class Ghostunnel < Formula
   desc "Simple SSL/TLS proxy with mutual authentication"
   homepage "https://github.com/ghostunnel/ghostunnel"
-  url "https://github.com/ghostunnel/ghostunnel/archive/refs/tags/v1.6.1.tar.gz"
-  sha256 "7fb1f9e8f60a6128b8b49cb2d3749b5fafad7d1d8c422adad48f34e240a8be6a"
+  url "https://github.com/ghostunnel/ghostunnel/archive/refs/tags/v1.7.1.tar.gz"
+  sha256 "b4eced76660e5e4bcdead3a3026608d500576fac574e49371cf9de8c98041b71"
   license "Apache-2.0"
   head "https://github.com/ghostunnel/ghostunnel.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/ghostunnel"
-    sha256 cellar: :any_skip_relocation, mojave: "7c8cd278d213eaf6c099dea33ca607713bdf6cca19eb69f5eabc4934a3f817e1"
+    sha256 cellar: :any_skip_relocation, mojave: "95c965b8208c8365c0303d3846fca264a8dc11500d71ecf4a94c7bfe8066f752"
   end
 
   depends_on "go" => :build
