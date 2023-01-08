e67fc21c1471499c1c690ac83937d2c85dadb8b2
Adrian Ho <the.gromgit@gmail.com>
2023-01-08 16:53:16 +0800

    git-cinnabar: update 0.5.11_1 bottle.

--- Formula/git-cinnabar.rb
+++ Formula/git-cinnabar.rb
@@ -1,19 +1,19 @@
 class GitCinnabar < Formula
   desc "Git remote helper to interact with mercurial repositories"
   homepage "https://github.com/glandium/git-cinnabar"
-  url "https://github.com/glandium/git-cinnabar/archive/0.5.10.tar.gz"
-  sha256 "20792358201417fa64cb3b1b9ccd6753909f081b0bf11cb9908f55a3607627e1"
+  url "https://github.com/glandium/git-cinnabar/archive/0.5.11.tar.gz"
+  sha256 "20f94f6a9b05fff2684e8c5619a1a5703e7d472fd2d0e87b020b20b4190a6338"
   license "GPL-2.0-only"
+  revision 1
   head "https://github.com/glandium/git-cinnabar.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-cinnabar"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "5280355a2969d3b9917f629400b6950d87034ddccd1945eb9ca0ca591fb8bf08"
+    sha256 cellar: :any_skip_relocation, mojave: "5afbf52013b2edeac55407417649c2eadaa1539a0c3c7e58da139d1efb84f7df"
   end
 
   depends_on "mercurial"
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   uses_from_macos "curl"
 
@@ -24,7 +24,7 @@ class GitCinnabar < Formula
     prefix.install "cinnabar"
     bin.install "git-cinnabar", "git-cinnabar-helper", "git-remote-hg"
     bin.env_script_all_files(libexec, PYTHONPATH:          prefix,
-                                      GIT_CINNABAR_PYTHON: which("python3.10"))
+                                      GIT_CINNABAR_PYTHON: which("python3.11"))
   end
 
   test do
