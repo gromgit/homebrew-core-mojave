824bf779485140334425ed6fd1dbcfb024e8d89f
Adrian Ho <the.gromgit@gmail.com>
2023-01-08 18:07:17 +0800

    git-machete: update 3.13.0 bottle.

--- Formula/git-machete.rb
+++ Formula/git-machete.rb
@@ -3,16 +3,16 @@ class GitMachete < Formula
 
   desc "Git repository organizer & rebase workflow automation tool"
   homepage "https://github.com/VirtusLab/git-machete"
-  url "https://pypi.org/packages/source/g/git-machete/git-machete-3.12.4.tar.gz"
-  sha256 "dfebd62944838c6c2404ed8967f9cf8999577936dd7dbd296f3793b85c306a76"
+  url "https://pypi.org/packages/source/g/git-machete/git-machete-3.13.0.tar.gz"
+  sha256 "1dc441ee9ce1332a3cf9869bbe0fdcffd255c9e0cb2afa07e08cb67c9dc81e69"
   license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-machete"
-    sha256 cellar: :any_skip_relocation, mojave: "36b0d04dee82aa5002aa81021c6ae606fc37a7314ebcd7ecc29baa7ad4889e19"
+    sha256 cellar: :any_skip_relocation, mojave: "85aa51fadf81da0e60dd352ba2e6276a7e81a08c9e303e34436247be8690d0d8"
   end
 
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   def install
     virtualenv_install_with_resources
