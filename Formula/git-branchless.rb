8375b644dffd2285696ebfa84d99a67554e90c3f
Adrian Ho <the.gromgit@gmail.com>
2023-01-08 16:38:13 +0800

    git-branchless: update 0.6.0 bottle.

--- Formula/git-branchless.rb
+++ Formula/git-branchless.rb
@@ -1,14 +1,14 @@
 class GitBranchless < Formula
   desc "High-velocity, monorepo-scale workflow for Git"
   homepage "https://github.com/arxanas/git-branchless"
-  url "https://github.com/arxanas/git-branchless/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "773eadf51fadf503b90f4a4dfb6ea38662294d9a98ec797d4b68592a731de4e0"
+  url "https://github.com/arxanas/git-branchless/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "362ac1ff6da00b1e5c1ab68614753fb5ffff3f1860a9a8272bf067cd8a9edccc"
   license "GPL-2.0-only"
   head "https://github.com/arxanas/git-branchless.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-branchless"
-    sha256 cellar: :any_skip_relocation, mojave: "738199593d5603fe112d6054dd8cf9d1d9095940d29b12ba850773bf7c26db26"
+    sha256 cellar: :any_skip_relocation, mojave: "82c907088b63561d9edcbea1041249689e1656225c37dcffeb40917083166002"
   end
 
   depends_on "rust" => :build
