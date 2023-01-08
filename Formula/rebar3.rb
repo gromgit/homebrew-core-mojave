fa5ea1c8d5e961bed1996868a916cf116bceff58
Adrian Ho <the.gromgit@gmail.com>
2023-01-08 23:23:30 +0800

    rebar3: update 3.20.0 bottle.

--- Formula/rebar3.rb
+++ Formula/rebar3.rb
@@ -1,8 +1,8 @@
 class Rebar3 < Formula
   desc "Erlang build tool"
   homepage "https://github.com/erlang/rebar3"
-  url "https://github.com/erlang/rebar3/archive/3.19.0.tar.gz"
-  sha256 "ff9ef42c737480477ebdf0dd9d95ece534a14c96f05edafbf32e9af973280bc3"
+  url "https://github.com/erlang/rebar3/archive/3.20.0.tar.gz"
+  sha256 "53ed7f294a8b8fb4d7d75988c69194943831c104d39832a1fa30307b1a8593de"
   license "Apache-2.0"
 
   livecheck do
@@ -12,7 +12,7 @@ class Rebar3 < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rebar3"
-    sha256 cellar: :any_skip_relocation, mojave: "23a1d13c672b3f4d031de60fe6b8a8b492dc0ef595955ac5c65cc3fd07f52b3a"
+    sha256 cellar: :any_skip_relocation, mojave: "d37c6548d2f31953cc0d38f99384ef3d775f1534a84e8e25874e4880493fdf19"
   end
 
   depends_on "erlang"
@@ -21,9 +21,9 @@ class Rebar3 < Formula
     system "./bootstrap"
     bin.install "rebar3"
 
-    bash_completion.install "priv/shell-completion/bash/rebar3"
-    zsh_completion.install "priv/shell-completion/zsh/_rebar3"
-    fish_completion.install "priv/shell-completion/fish/rebar3.fish"
+    bash_completion.install "apps/rebar/priv/shell-completion/bash/rebar3"
+    zsh_completion.install "apps/rebar/priv/shell-completion/zsh/_rebar3"
+    fish_completion.install "apps/rebar/priv/shell-completion/fish/rebar3.fish"
   end
 
   test do
