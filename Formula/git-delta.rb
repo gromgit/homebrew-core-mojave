ef3af245921bb9814b66193aba55b45e2374887b
Adrian Ho <the.gromgit@gmail.com>
2023-01-08 17:31:04 +0800

    git-delta: update 0.15.1 bottle.

--- Formula/git-delta.rb
+++ Formula/git-delta.rb
@@ -1,14 +1,14 @@
 class GitDelta < Formula
   desc "Syntax-highlighting pager for git and diff output"
   homepage "https://github.com/dandavison/delta"
-  url "https://github.com/dandavison/delta/archive/0.14.0.tar.gz"
-  sha256 "7d1ab2949d00f712ad16c8c7fc4be500d20def9ba70394182720a36d300a967c"
+  url "https://github.com/dandavison/delta/archive/0.15.1.tar.gz"
+  sha256 "b9afd2f80ae1d57991a19832d6979c7080a568d42290a24e59d6a2a82cbc1728"
   license "MIT"
   head "https://github.com/dandavison/delta.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-delta"
-    sha256 cellar: :any_skip_relocation, mojave: "07ebbfce2c4f65d63d35f3baab30926440c09209e16803b55ea0d2bf31beb607"
+    sha256 cellar: :any_skip_relocation, mojave: "ef8a07faaf220291579853d498408b21bbfd5752e176b32f29d58693400276cd"
   end
 
   depends_on "rust" => :build
@@ -19,6 +19,7 @@ class GitDelta < Formula
   def install
     system "cargo", "install", *std_cargo_args
     bash_completion.install "etc/completion/completion.bash" => "delta"
+    fish_completion.install "etc/completion/completion.fish" => "delta.fish"
     zsh_completion.install "etc/completion/completion.zsh" => "_delta"
   end
 
