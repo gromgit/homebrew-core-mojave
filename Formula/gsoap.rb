b21b08ea1e00917ff53c544dea3570a42e98c9f9
Adrian Ho <the.gromgit@gmail.com>
2023-01-10 05:12:17 +0800

    gsoap: update 2.8.124 bottle.

--- Formula/gsoap.rb
+++ Formula/gsoap.rb
@@ -1,9 +1,17 @@
 class Gsoap < Formula
   desc "SOAP stub and skeleton compiler for C and C++"
   homepage "https://www.genivia.com/products.html"
-  url "https://downloads.sourceforge.net/project/gsoap2/gsoap_2.8.123.zip"
-  sha256 "e018500ac942bb7627612cc9a8229610efe293a450359c413da1a006eb7c193d"
-  license any_of: ["GPL-2.0-or-later", "gSOAP-1.3b"]
+  url "https://downloads.sourceforge.net/project/gsoap2/gsoap_2.8.124.zip"
+  sha256 "4b798780989338f665ef8e171bbcc422a271004d62d5852666d5eeca33a6a636"
+  # Parts of the software are alternatively licensed under gSOAP-1.3b, but this
+  # license is considered non-free by Debian and Fedora due to section 3.2:
+  #
+  # 3.2. Availability of Source Code.
+  # Any Modification created by You will be provided to the Initial Developer in
+  # Source Code form and are subject to the terms of the License.
+  #
+  # Ref: https://salsa.debian.org/ellert/gsoap/-/blob/master/debian/copyright#L7-26
+  license "GPL-2.0-or-later" => { with: "openvpn-openssl-exception" }
 
   livecheck do
     url :stable
@@ -12,18 +20,18 @@ class Gsoap < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gsoap"
-    sha256 mojave: "3da241110fdd1503b9aad1a99626f5c524dcbe328686eaa18d0e859f6728d2e7"
+    sha256 mojave: "b58a04f48e80492b38202548b1f49c9e68ba369b0c1d647b6da16ad57bade725"
   end
 
   depends_on "autoconf" => :build
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
-  uses_from_macos "bison"
-  uses_from_macos "flex"
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
   uses_from_macos "zlib"
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", *std_configure_args, "--disable-silent-rules"
     system "make"
     system "make", "install"
   end
