9fd5953e71909b730d5a7084161b8b33d31c0050
Adrian Ho <the.gromgit@gmail.com>
2023-01-09 19:08:49 +0800

    gopass: update 1.15.2 bottle.

--- Formula/gopass.rb
+++ Formula/gopass.rb
@@ -1,14 +1,14 @@
 class Gopass < Formula
   desc "Slightly more awesome Standard Unix Password Manager for Teams"
   homepage "https://github.com/gopasspw/gopass"
-  url "https://github.com/gopasspw/gopass/releases/download/v1.14.7/gopass-1.14.7.tar.gz"
-  sha256 "443de8190da2913033fe0a18ac0aee41155a9f004a6f608675c99de850a9e03b"
+  url "https://github.com/gopasspw/gopass/releases/download/v1.15.2/gopass-1.15.2.tar.gz"
+  sha256 "8d02d41ca980cd3895bbba53b7842da413bddbfc5d11900235adfc9473e831c2"
   license "MIT"
   head "https://github.com/gopasspw/gopass.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gopass"
-    sha256 cellar: :any_skip_relocation, mojave: "ff352940912e3c7b86736017c5e8fcd163fb4249027625600844909db9517ae5"
+    sha256 cellar: :any_skip_relocation, mojave: "a0058e27cf14f3f13a58220fd0421e4d0fd9b44c3ae75c19c8cde6a521c211ad"
   end
 
   depends_on "go" => :build
