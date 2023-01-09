5b53518479e18f84c4f9ebb60c2b5afb31a8719b
Adrian Ho <the.gromgit@gmail.com>
2023-01-09 22:49:25 +0800

    libxp: update 1.0.4 bottle.

--- Formula/libxp.rb
+++ Formula/libxp.rb
@@ -1,8 +1,8 @@
 class Libxp < Formula
   desc "X Print Client Library"
   homepage "https://gitlab.freedesktop.org/xorg/lib/libxp"
-  url "https://gitlab.freedesktop.org/xorg/lib/libxp/-/archive/libXp-1.0.3/libxp-libXp-1.0.3.tar.bz2"
-  sha256 "bd1e449572359921dd5fa20707757f57d7535aff1772570ab2c29c6b49b86266"
+  url "https://gitlab.freedesktop.org/xorg/lib/libxp/-/archive/libXp-1.0.4/libxp-libXp-1.0.4.tar.bz2"
+  sha256 "81468f6d5d8d8f847aac50af60b36c43d84d976d907ab1dfd667683dbfb5fb90"
   license "MIT"
 
   livecheck do
@@ -11,14 +11,8 @@ class Libxp < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "15125f120ead5edb95bf231ee97dd9faacd845589e8c12f085e86d53e107dd72"
-    sha256 cellar: :any,                 arm64_big_sur:  "d9e17f99eb3a084585f049a3243affb67c0bc494d55ecea910ddbeb6b4c7d79c"
-    sha256 cellar: :any,                 monterey:       "e49c62d4d05ac0ed1ff300817124973bd03a0c30160eda4cb18e7d45cdf3c005"
-    sha256 cellar: :any,                 big_sur:        "21e04a90fe93b05faeb09b3a7e65c68a3449bcd5596c5df8d21ff1afb22b0a93"
-    sha256 cellar: :any,                 catalina:       "e70342d93c5cf690582f559318b05b26da9175fc7620493fa15a224a847ec1da"
-    sha256 cellar: :any,                 mojave:         "1cc823e7fe3acb64e58b554e5e956302959f28cb5737eea5c6d15655128aee15"
-    sha256 cellar: :any,                 high_sierra:    "8e904b533b4c4264232ae6391e7d4bc37dade77d5d20539ed3d42900ab3950ce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d81a760a81e1602499f04fd3b9809a73324f4eaec3c521326869ddb824372c88"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libxp"
+    sha256 cellar: :any, mojave: "03365385fe83d481c89462f493acd8c57c2872cf050ef1683e2fe1b8f02ce007"
   end
 
   depends_on "autoconf" => :build
