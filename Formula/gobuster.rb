b7ea83468f0e122fbfe241d8d0a51f1f5f59269e
Adrian Ho <the.gromgit@gmail.com>
2023-01-09 17:05:50 +0800

    gobuster: update 3.4.0 bottle.

--- Formula/gobuster.rb
+++ Formula/gobuster.rb
@@ -1,13 +1,13 @@
 class Gobuster < Formula
   desc "Directory/file & DNS busting tool written in Go"
   homepage "https://github.com/OJ/gobuster"
-  url "https://github.com/OJ/gobuster/archive/refs/tags/v3.2.0.tar.gz"
-  sha256 "63094d24b79622d798f1aed2e497c8a6dd2bbeaa4fda7162ec71bc7070bf1a61"
+  url "https://github.com/OJ/gobuster/archive/refs/tags/v3.4.0.tar.gz"
+  sha256 "6c1d7a3aa9604d90ca818d6fc7a0b09501e419ecd4ab7665566c52fd0981b52d"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gobuster"
-    sha256 cellar: :any_skip_relocation, mojave: "7342ea5f171d3c7ec7f756b7d18defb7733ddd34ac7b4b4d3e107fa02afadd6d"
+    sha256 cellar: :any_skip_relocation, mojave: "b2f4cb2167a52316f009dc1353b9859b88aae67179bcc71bf25c1c37ea3421e7"
   end
 
   depends_on "go" => :build
@@ -30,6 +30,6 @@ class Gobuster < Formula
     output = shell_output("#{bin}/gobuster dir -u https://buffered.io -w words.txt 2>&1")
     assert_match "Finished", output
 
-    assert_match version.to_s, shell_output(bin/"gobuster version")
+    assert_match version.major_minor.to_s, shell_output(bin/"gobuster version")
   end
 end
