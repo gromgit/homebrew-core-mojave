a64e150313c4396c6c85dc41b99d3320977892aa
Adrian Ho <the.gromgit@gmail.com>
2023-01-10 05:44:54 +0800

    gst-editing-services: update 1.20.4 bottle.

--- Formula/gst-editing-services.rb
+++ Formula/gst-editing-services.rb
@@ -1,8 +1,8 @@
 class GstEditingServices < Formula
   desc "GStreamer Editing Services"
   homepage "https://gstreamer.freedesktop.org/modules/gst-editing-services.html"
-  url "https://gstreamer.freedesktop.org/src/gst-editing-services/gst-editing-services-1.20.3.tar.xz"
-  sha256 "5fd896de69fbe24421eb6b0ff8d2f8b4c3cba3f3025ceacd302172f39a8abaa2"
+  url "https://gstreamer.freedesktop.org/src/gst-editing-services/gst-editing-services-1.20.4.tar.xz"
+  sha256 "aa03e983af5d79c1befffe3575b034e60960619a96bf877447cb73c28016fc41"
   license "LGPL-2.0-or-later"
 
   livecheck do
@@ -12,7 +12,7 @@ class GstEditingServices < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gst-editing-services"
-    sha256 cellar: :any, mojave: "93b494b27b76fab27a866b942354d16ef5e64e2d796d455d392309f74cef3b6d"
+    sha256 cellar: :any, mojave: "51afbef38c88360ee56541344c3b28c233006a36147b47dc38307e72f0e80f79"
   end
 
   depends_on "gobject-introspection" => :build
