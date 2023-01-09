296aabbad11cf430aee22d331419e228525ed6a9
Adrian Ho <the.gromgit@gmail.com>
2023-01-10 02:51:49 +0800

    grex: update 1.4.1 bottle.

--- Formula/grex.rb
+++ Formula/grex.rb
@@ -1,13 +1,13 @@
 class Grex < Formula
   desc "Command-line tool for generating regular expressions"
   homepage "https://github.com/pemistahl/grex"
-  url "https://github.com/pemistahl/grex/archive/v1.4.0.tar.gz"
-  sha256 "38610a97665a30a3b66519df41f979a39a0503d2e9ac6e1e3e3394ec305e7987"
+  url "https://github.com/pemistahl/grex/archive/v1.4.1.tar.gz"
+  sha256 "8413aae520d696969525961438d22e31cd966058ce3510e91e77da18603c96b9"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/grex"
-    sha256 cellar: :any_skip_relocation, mojave: "7d27da98b734043d400441b05ccb7bf3b6917940615f8ebb67517fbd58a657a4"
+    sha256 cellar: :any_skip_relocation, mojave: "f8e581c38262f826d9afd5c7ec0a51958cf00c11e6b2c840f83217644554b627"
   end
 
   depends_on "rust" => :build
