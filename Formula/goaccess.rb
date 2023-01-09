5b4a9e6bc9ea3310b22fee14f5b1b1a0235ac1f5
Adrian Ho <the.gromgit@gmail.com>
2023-01-09 16:13:38 +0800

    goaccess: update 1.6.5 bottle.

--- Formula/goaccess.rb
+++ Formula/goaccess.rb
@@ -1,8 +1,8 @@
 class Goaccess < Formula
   desc "Log analyzer and interactive viewer for the Apache Webserver"
   homepage "https://goaccess.io/"
-  url "https://tar.goaccess.io/goaccess-1.6.4.tar.gz"
-  sha256 "01bcd0bb17332132d0c1c2b452c935a789729bd2518c5a476b79c10293ee2341"
+  url "https://tar.goaccess.io/goaccess-1.6.5.tar.gz"
+  sha256 "355edbf8af2c14879fa4b90c68adcde340eb4efbc443d27de4f8a03a01002df9"
   license "MIT"
   head "https://github.com/allinurl/goaccess.git", branch: "master"
 
@@ -13,7 +13,7 @@ class Goaccess < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/goaccess"
-    sha256 mojave: "fb170d7538869bd28c1de842210d0d9b9a8625facba80a875ca2563ac96f2bce"
+    sha256 mojave: "464f9babb025e929abdc660c15ddb90d8f060bdf962dd2af623a407a6c317b84"
   end
 
   depends_on "autoconf" => :build
