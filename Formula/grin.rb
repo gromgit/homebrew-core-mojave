4290874aa82e8b48c068fde00de132d3eb85dee9
Adrian Ho <the.gromgit@gmail.com>
2023-01-10 03:35:48 +0800

    grin: update 5.1.2 bottle.

--- Formula/grin.rb
+++ Formula/grin.rb
@@ -1,13 +1,15 @@
 class Grin < Formula
   desc "Minimal implementation of the Mimblewimble protocol"
   homepage "https://grin.mw/"
+  # TODO: remove the `cargo update` line when this is next updated (5.2.x).
   url "https://github.com/mimblewimble/grin/archive/v5.1.2.tar.gz"
   sha256 "a4856335d88630e742b75e877f1217d7c9180b89f030d2e1d1c780c0f8cc475c"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/grin"
-    sha256 cellar: :any_skip_relocation, mojave: "9a52a62dd8896d38dadb60d488afbb8e588a598a4e751dd39de212c09871bb4c"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, mojave: "3d728fd4ab7563850d6e067510b8930052cee7605d51eca76b3c164544523c00"
   end
 
   depends_on "llvm" => :build # for libclang
@@ -16,6 +18,10 @@ class Grin < Formula
   uses_from_macos "ncurses"
 
   def install
+    # Fixes compile with newer Rust.
+    # REMOVE ME in the next release.
+    system "cargo", "update", "--package", "socket2", "--precise", "0.3.16"
+
     ENV["CLANG_PATH"] = Formula["llvm"].opt_bin/"clang"
 
     system "cargo", "install", *std_cargo_args
