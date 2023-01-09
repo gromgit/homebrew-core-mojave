8cd32ae0b0e505dc0cd34428bbaff6f1330a4ce9
Adrian Ho <the.gromgit@gmail.com>
2023-01-09 19:20:50 +0800

    gopls: update 0.11.0 bottle.

--- Formula/gopls.rb
+++ Formula/gopls.rb
@@ -1,8 +1,8 @@
 class Gopls < Formula
   desc "Language server for the Go language"
   homepage "https://github.com/golang/tools/tree/master/gopls"
-  url "https://github.com/golang/tools/archive/gopls/v0.9.5.tar.gz"
-  sha256 "c7b3a09f3820a0b3183ab5a08845f8d328565076d2834bb3b049a12eff6191e5"
+  url "https://github.com/golang/tools/archive/gopls/v0.11.0.tar.gz"
+  sha256 "b5dd0040bcc75b4760eb543826b6503a163c835c90f50843e626a112ca083931"
   license "BSD-3-Clause"
 
   livecheck do
@@ -13,7 +13,7 @@ class Gopls < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gopls"
-    sha256 cellar: :any_skip_relocation, mojave: "85e3bf57ac4b313d24d26b7536012b4f17768e417fa056a8cdc2077d7f712438"
+    sha256 cellar: :any_skip_relocation, mojave: "3949f8d97f975978ade4a7bdb984da03b6ef0a0087372629e5ec4522d914b928"
   end
 
   depends_on "go" => :build
