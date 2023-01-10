c0ad9706c76e7cb359a2044530c4c77cf35b8b9a
Adrian Ho <the.gromgit@gmail.com>
2023-01-10 15:01:57 +0800

    hamlib: update 4.5.1 bottle.

--- Formula/hamlib.rb
+++ Formula/hamlib.rb
@@ -1,15 +1,14 @@
 class Hamlib < Formula
   desc "Ham radio control libraries"
   homepage "http://www.hamlib.org/"
-  url "https://github.com/Hamlib/Hamlib/releases/download/4.4/hamlib-4.4.tar.gz"
-  sha256 "8bf0107b071f52f08587f38e2dee8a7848de1343435b326f8f66d95e1f8a2487"
+  url "https://github.com/Hamlib/Hamlib/releases/download/4.5.1/hamlib-4.5.1.tar.gz"
+  sha256 "acdaf2f91c052e17276b06b9170a03f929600763caaf94a6377e1a906cd631c8"
   license "LGPL-2.1-or-later"
   head "https://github.com/hamlib/hamlib.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/hamlib"
-    rebuild 2
-    sha256 cellar: :any, mojave: "987f337f4d3d2eade4b4a813c58dfd777c423886487ba38a87cbb5371e2eb7d7"
+    sha256 cellar: :any, mojave: "d52af9d6f0451c12b8261f218940b58d5665d7fcfeb9a81acfa37e8621a71565"
   end
 
   depends_on "autoconf" => :build
