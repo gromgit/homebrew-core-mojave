54ad31efece5783af508480e941297c52c5c279a
Adrian Ho <the.gromgit@gmail.com>
2023-01-11 02:16:52 +0800

    himalaya: update 0.6.1 bottle.

--- Formula/himalaya.rb
+++ Formula/himalaya.rb
@@ -1,24 +1,24 @@
 class Himalaya < Formula
   desc "CLI email client written in Rust"
   homepage "https://github.com/soywod/himalaya"
-  url "https://github.com/soywod/himalaya/archive/v0.5.10.tar.gz"
-  sha256 "2922b60caa37de70464a11df44704d8b5589f6d968532350267ffb136da54cbd"
-  license "BSD-4-Clause"
+  url "https://github.com/soywod/himalaya/archive/v0.6.1.tar.gz"
+  sha256 "e0627eab2758b470051309e4c60bd4e2b0b22cc7a4d6810d94d629648a06d81e"
+  license "MIT"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/himalaya"
-    sha256 cellar: :any_skip_relocation, mojave: "aa829b7d9233f80c469cafed6c704cabb9040f41577ca8dc8d98c9028145bcbf"
+    sha256 cellar: :any_skip_relocation, mojave: "54584cd9c48d095284f6a977d4c28192b26b1891f161a0c2d17f4f0e4c172634"
   end
 
   depends_on "rust" => :build
 
   on_linux do
     depends_on "pkg-config" => :build
-    depends_on "openssl@1.1"
+    depends_on "openssl@3"
   end
 
   def install
-    system "cargo", "install", *std_cargo_args(path: "cli")
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
@@ -35,17 +35,19 @@ class Himalaya < Formula
       default = true
       email = "your.email@gmail.com"
 
+      backend = "imap"
       imap-host = "imap.gmail.com"
       imap-port = 993
       imap-login = "your.email@gmail.com"
       imap-passwd-cmd = "pass show gmail"
 
+      sender = "smtp"
       smtp-host = "smtp.gmail.com"
       smtp-port = 465
       smtp-login = "your.email@gmail.com"
       smtp-passwd-cmd = "security find-internet-password -gs gmail -w"
     EOS
 
-    assert_match "Error: cannot login to IMAP server", shell_output("#{bin}/himalaya 2>&1", 1)
+    assert_match "Error: cannot get imap password: password is empty", shell_output("#{bin}/himalaya 2>&1", 1)
   end
 end
