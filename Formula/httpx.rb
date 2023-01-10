75b2cd88bbfab8ca057959a764373654751e429b
Adrian Ho <the.gromgit@gmail.com>
2023-01-11 05:31:27 +0800

    httpx: update 1.2.5 bottle.

--- Formula/httpx.rb
+++ Formula/httpx.rb
@@ -1,25 +1,25 @@
 class Httpx < Formula
   desc "Fast and multi-purpose HTTP toolkit"
   homepage "https://github.com/projectdiscovery/httpx"
-  url "https://github.com/projectdiscovery/httpx/archive/v1.2.4.tar.gz"
-  sha256 "5d95a35586c851bba43be2fb79b790ff417e58aa34ae515a79b3bd8c6f2df7e0"
+  url "https://github.com/projectdiscovery/httpx/archive/v1.2.5.tar.gz"
+  sha256 "297b34f574bedd9575926f1e8c9f630a39ab54311bd2df821536359735a2b161"
   license "MIT"
   head "https://github.com/projectdiscovery/httpx.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/httpx"
-    sha256 cellar: :any_skip_relocation, mojave: "5a45c189938825d0e927a477f036b98bac5af9facc4f3355dcc55198e4345b61"
+    sha256 cellar: :any_skip_relocation, mojave: "971c019aae9dbb049df2acfec7d4c6ebf7d49d3bdc9e73135d3569ef386dd44b"
   end
 
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args, "./cmd/httpx"
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/httpx"
   end
 
   test do
-    output = JSON.parse(pipe_output("#{bin}/httpx -silent -status-code -title -json", "example.org"))
-    assert_equal 200, output["status-code"]
+    output = JSON.parse(shell_output("#{bin}/httpx -silent -title -json -u example.org"))
+    assert_equal 200, output["status_code"]
     assert_equal "Example Domain", output["title"]
   end
 end
