88ba92c8ff56606a80d119f9dd2be19c4ed26cb5
Adrian Ho <the.gromgit@gmail.com>
2023-01-11 00:27:24 +0800

    helib: update 2.2.2 bottle.

--- Formula/helib.rb
+++ Formula/helib.rb
@@ -1,14 +1,13 @@
 class Helib < Formula
   desc "Implementation of homomorphic encryption"
   homepage "https://github.com/homenc/HElib"
-  url "https://github.com/homenc/HElib/archive/v2.2.1.tar.gz"
-  sha256 "cbe030c752c915f1ece09681cadfbe4f140f6752414ab000b4cf076b6c3019e4"
+  url "https://github.com/homenc/HElib/archive/v2.2.2.tar.gz"
+  sha256 "70c07d2a2da393c695095fe755836524e3d98efb27a336e206291f71db9cec7d"
   license "Apache-2.0"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/helib"
-    rebuild 1
-    sha256 cellar: :any, mojave: "c4658396c7b8b65a666e0b45b57d6def5ada0d40321a449cabab20f6a6fbd2d7"
+    sha256 cellar: :any, mojave: "b9b9c2987c6eaecb2d26deca4df806d9482cbbf04ab5a1ee58bac797f2210afc"
   end
 
   depends_on "cmake" => :build
