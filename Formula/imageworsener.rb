e7ddbe8d9237222cf4c6dd3e588a997af24a1831
Adrian Ho <the.gromgit@gmail.com>
2023-01-11 13:27:44 +0800

    imageworsener: update 1.3.5 bottle.

--- Formula/imageworsener.rb
+++ Formula/imageworsener.rb
@@ -1,19 +1,9 @@
 class Imageworsener < Formula
   desc "Utility and library for image scaling and processing"
   homepage "https://entropymine.com/imageworsener/"
+  url "https://entropymine.com/imageworsener/imageworsener-1.3.5.tar.gz"
+  sha256 "a7fbb65c5ade67d9ebc32e52c58988a4f986bacc008c9021fe36b598466d5c8d"
   license "MIT"
-  revision 1
-
-  stable do
-    url "https://entropymine.com/imageworsener/imageworsener-1.3.4.tar.gz"
-    sha256 "bae0b2bb35e565133dd804a6f4af303992527f53068cd67b03e5d9961d8512b6"
-
-    # Fix -flat_namespace being used on Big Sur and later.
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-      sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-    end
-  end
 
   livecheck do
     url :homepage
@@ -22,7 +12,7 @@ class Imageworsener < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/imageworsener"
-    sha256 cellar: :any, mojave: "7406c5fca92bc9394269a35b087cafc067250b16feed167687e0a36db980183f"
+    sha256 cellar: :any, mojave: "85ad1b5f99978966914f4fc54e5dd589754698cdbd7a119495964e654587be2c"
   end
 
   head do
