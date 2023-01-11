c7dca715041577c3fcd469d89fb606546f344af3
Adrian Ho <the.gromgit@gmail.com>
2023-01-11 16:13:51 +0800

    infracost: update 0.10.15 bottle.

--- Formula/infracost.rb
+++ Formula/infracost.rb
@@ -1,14 +1,14 @@
 class Infracost < Formula
   desc "Cost estimates for Terraform"
   homepage "https://www.infracost.io/docs/"
-  url "https://github.com/infracost/infracost/archive/v0.10.11.tar.gz"
-  sha256 "25956ef500ae171d2d36c3c61017b427a8ea986c2ef17ba6f89e5f64745dfe2a"
+  url "https://github.com/infracost/infracost/archive/v0.10.15.tar.gz"
+  sha256 "566be4b7fc228d07a842c11797a5976d4699226ece752b0ff5f9867c2e0e4301"
   license "Apache-2.0"
   head "https://github.com/infracost/infracost.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/infracost"
-    sha256 cellar: :any_skip_relocation, mojave: "61d6898006b8a4ad72e9b84372eaa61033d10f8e6662ffc3cc04d4bb64c3f2c3"
+    sha256 cellar: :any_skip_relocation, mojave: "e5192abd8bb31618295d3d0b8e478f55de1883f923f8bd6865cf0836dd65f91f"
   end
 
   depends_on "go" => :build
@@ -18,6 +18,8 @@ class Infracost < Formula
     ENV["CGO_ENABLED"] = "0"
     ldflags = "-X github.com/infracost/infracost/internal/version.Version=v#{version}"
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/infracost"
+
+    generate_completions_from_executable(bin/"infracost", "completion", "--shell")
   end
 
   test do
