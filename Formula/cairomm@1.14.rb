b0107802b9e548bfc91b9e5d292a8163e37649b1
Adrian Ho <the.gromgit@gmail.com>
2023-01-13 00:45:12 +0800

    cairomm@1.14: add 1.14.4 bottle.

--- Formula/cairomm@1.14.rb
+++ Formula/cairomm@1.14.rb
@@ -1,8 +1,8 @@
 class CairommAT114 < Formula
   desc "Vector graphics library with cross-device output support"
   homepage "https://cairographics.org/cairomm/"
-  url "https://cairographics.org/releases/cairomm-1.14.3.tar.xz"
-  sha256 "0d37e067c5c4ca7808b7ceddabfe1932c5bd2a750ad64fb321e1213536297e78"
+  url "https://cairographics.org/releases/cairomm-1.14.4.tar.xz"
+  sha256 "4749d25a2b2ef67cc0c014caaf5c87fa46792fc4b3ede186fb0fc932d2055158"
   license "LGPL-2.0-or-later"
 
   livecheck do
@@ -11,13 +11,8 @@ class CairommAT114 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_monterey: "0b85a16abade85d8a54c06c5dd2ad262c3679f011e5a9e3b4b87bb90f441a163"
-    sha256 cellar: :any, arm64_big_sur:  "71bcd76e90894734f2e5d63935bc183cd8486235f9a664d4e205db1b96a0c822"
-    sha256 cellar: :any, monterey:       "cb2b5dbb06c874a8e133bfa71c8c4969a4cf1b8156ab4d52ff2a36b08e9dc90b"
-    sha256 cellar: :any, big_sur:        "67f908984fdf0cefc9d18cc14bc59517f5b41a821acee8b6f98538f3bfea864a"
-    sha256 cellar: :any, catalina:       "61e7e813f3a0a75951b2a25ef451bc97268330768cb2963fd00e8debf4d26a92"
-    sha256 cellar: :any, mojave:         "919c048378cc28132ddbff1138796dcdc1f1d330a6f60b97b0ddfa2c77394780"
-    sha256               x86_64_linux:   "55a72b3ff6eb503eebf7aced50b3bad26f50422804e9802d1c92ebe242146595"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/cairomm@1.14"
+    sha256 cellar: :any, mojave: "f937c7d94efe75281479539c667eeca7e2ccce4377aee54adcc3d83de2551616"
   end
 
   depends_on "meson" => :build
