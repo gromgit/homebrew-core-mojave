a51590682c64bbd451364928f3c25dc41abeb9d8
Adrian Ho <the.gromgit@gmail.com>
2023-01-12 15:04:15 +0800

    goffice: add 0.10.53 bottle.

--- Formula/goffice.rb
+++ Formula/goffice.rb
@@ -1,22 +1,13 @@
 class Goffice < Formula
   desc "Gnumeric spreadsheet program"
   homepage "https://gitlab.gnome.org/GNOME/goffice"
+  url "https://download.gnome.org/sources/goffice/0.10/goffice-0.10.53.tar.xz"
+  sha256 "27fd58796faa1cd4cc0120c34ea85315a0891ec71f55bc6793c14ecf168a3f57"
   license any_of: ["GPL-3.0-only", "GPL-2.0-only"]
-
-  stable do
-    url "https://download.gnome.org/sources/goffice/0.10/goffice-0.10.52.tar.xz"
-    sha256 "60b9efd94370f0969b394f0aac8c6eb91e15ebc0ce1236b44aa735eb1c98840c"
-
-    # Fix -flat_namespace being used on Big Sur and later.
-    patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/03cf8088210822aa2c1ab544ed58ea04c897d9c4/libtool/configure-big_sur.diff"
-      sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
-    end
-  end
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/goffice"
-    sha256 mojave: "2bb7423f3ffa2a27e8f779d99bc2a8103710f118270d71fecf815a52469007cd"
+    sha256 mojave: "921b1cd3e35110530a6cda169848dd96c50df1a98193ef740b10ffcd765dcaa8"
   end
 
   head do
