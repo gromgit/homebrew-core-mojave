9a9652b5a7ddb6946d2f1ae4bf443cda3162caf1
Adrian Ho <the.gromgit@gmail.com>
2023-01-13 01:14:12 +0800

    halide: add 14.0.0_4 bottle.

--- Formula/halide.rb
+++ Formula/halide.rb
@@ -1,10 +1,15 @@
 class Halide < Formula
   desc "Language for fast, portable data-parallel computation"
   homepage "https://halide-lang.org"
-  url "https://github.com/halide/Halide/archive/v14.0.0.tar.gz"
-  sha256 "f9fc9765217cbd10e3a3e3883a60fc8f2dbbeaac634b45c789577a8a87999a01"
   license "MIT"
-  revision 1
+  revision 4
+
+  # Remove `stable` when we switch to `llvm`.
+  stable do
+    url "https://github.com/halide/Halide/archive/v14.0.0.tar.gz"
+    sha256 "f9fc9765217cbd10e3a3e3883a60fc8f2dbbeaac634b45c789577a8a87999a01"
+    depends_on "llvm@14"
+  end
 
   livecheck do
     url :stable
@@ -13,28 +18,46 @@ class Halide < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/halide"
-    sha256 cellar: :any, mojave: "066e1e5af0ee40f79d1af3fdbb41147d5e5062d3d12c93c661ecaee082aaf6b5"
+    sha256 cellar: :any, mojave: "ccfbe21a914a3ca3bcc941ad5c18df1602fec0883cb1f3753670a85a49ee5042"
+  end
+
+  head do
+    url "https://github.com/halide/Halide.git", branch: "main"
+    depends_on "llvm"
   end
 
   depends_on "cmake" => :build
-  depends_on "jpeg"
+  depends_on "pybind11" => :build
+  depends_on "jpeg-turbo"
   depends_on "libpng"
-  depends_on "llvm"
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
-  fails_with gcc: "5" # LLVM is built with Homebrew GCC
+  fails_with :gcc do
+    version "6"
+    cause "Requires C++17"
+  end
+
+  def python3
+    "python3.11"
+  end
 
   def install
-    mkdir "build" do
-      system "cmake", "..", *std_cmake_args, "-DCMAKE_INSTALL_RPATH=#{rpath}", "-DHalide_SHARED_LLVM=ON"
-      system "make"
-      system "make", "install"
-    end
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DCMAKE_INSTALL_RPATH=#{rpath}",
+                    "-DHalide_INSTALL_PYTHONDIR=#{prefix/Language::Python.site_packages(python3)}",
+                    "-DHalide_SHARED_LLVM=ON",
+                    "-DPYBIND11_USE_FETCHCONTENT=OFF",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
     cp share/"doc/Halide/tutorial/lesson_01_basics.cpp", testpath
     system ENV.cxx, "-std=c++17", "lesson_01_basics.cpp", "-L#{lib}", "-lHalide", "-o", "test"
     assert_match "Success!", shell_output("./test")
+
+    cp share/"doc/Halide/tutorial-python/lesson_01_basics.py", testpath
+    assert_match "Success!", shell_output("#{python3} lesson_01_basics.py")
   end
 end
