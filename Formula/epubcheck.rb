23b46eba272c8047b04ecbfb11d2c09c0f277ae8
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:40:05 +0800

    epubcheck: no rebuilding required

--- Formula/epubcheck.rb
+++ Formula/epubcheck.rb
@@ -1,12 +1,12 @@
 class Epubcheck < Formula
   desc "Validate EPUB files, version 2.0 and later"
   homepage "https://github.com/w3c/epubcheck"
-  url "https://github.com/w3c/epubcheck/releases/download/v4.2.6/epubcheck-4.2.6.zip"
-  sha256 "3f73c1265cc92e3b53ffda6cd5bbeb58505b2b0c43411c26e74d8df1b193b2c0"
+  url "https://github.com/w3c/epubcheck/releases/download/v5.0.0/epubcheck-5.0.0.zip"
+  sha256 "98c5ecce0a6a6bf37034f73465613c4088916b3ef3489f50a7f6897a37a9725a"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "94dc30bc00494c090c9b47d285fb2e1b904c5e22c59b721ad11a0e018a1d4291"
+    sha256 cellar: :any_skip_relocation, all: "942d5ff8e4985575d7e089ee2a3690ba02450f26289c754f8011d8bc044566b0"
   end
 
   depends_on "openjdk"
