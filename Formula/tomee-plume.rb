c1dce69667321ac43ceda977acd4d857ff6c8e13
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:52:18 +0800

    tomee-plume: no rebuilding required

--- Formula/tomee-plume.rb
+++ Formula/tomee-plume.rb
@@ -1,13 +1,13 @@
 class TomeePlume < Formula
   desc "Apache TomEE Plume"
   homepage "https://tomee.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-8.0.13/apache-tomee-8.0.13-plume.tar.gz"
-  mirror "https://archive.apache.org/dist/tomee/tomee-8.0.13/apache-tomee-8.0.13-plume.tar.gz"
-  sha256 "94bc0347849fcb9c10407a6e0177ac4304ce54d803528e6f4716c02090ab384e"
+  url "https://www.apache.org/dyn/closer.lua?path=tomee/tomee-9.0.0/apache-tomee-9.0.0-plume.tar.gz"
+  mirror "https://archive.apache.org/dist/tomee/tomee-9.0.0/apache-tomee-9.0.0-plume.tar.gz"
+  sha256 "6fc93289536edcba09cc1ad38d045a88acff9330198e755043960d9a3e2190e6"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "f46866db42983b924da50d464240efd1fd6602fd463b616af08abc01613e3ac3"
+    sha256 cellar: :any_skip_relocation, all: "a1d324faeeeca5c81b7654bd1584fa25cbee3cfcf7b4bd66fa20fa28c48e7797"
   end
 
   depends_on "openjdk"
