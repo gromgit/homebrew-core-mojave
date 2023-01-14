8626f25f7a3471b867c76deda860ff06f60d6e61
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:46:29 +0800

    nanopb-generator: no rebuilding required

--- Formula/nanopb-generator.rb
+++ Formula/nanopb-generator.rb
@@ -3,8 +3,8 @@ class NanopbGenerator < Formula
 
   desc "C library for encoding and decoding Protocol Buffer messages"
   homepage "https://jpa.kapsi.fi/nanopb/docs/index.html"
-  url "https://jpa.kapsi.fi/nanopb/download/nanopb-0.4.6.tar.gz"
-  sha256 "e379d9babd86b9cfd8f8900fd0da8705cbd9bea4491178fb2b8be5e217bf02ab"
+  url "https://jpa.kapsi.fi/nanopb/download/nanopb-0.4.7.tar.gz"
+  sha256 "87055a0d9a99a95429a95497e566e59d8d16103cfd90b7b6e8ee364b6559666a"
   license "Zlib"
 
   livecheck do
@@ -13,8 +13,7 @@ class NanopbGenerator < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "ac1eb681935c128508d1d5f2159ec8fe2cb4c7d248b8f955ce53f35f7ca0a63e"
+    sha256 cellar: :any_skip_relocation, all: "8fa99a6f2d8fbb4fc69952cc533309c554578f983f5413d882bd86d9ddef37b7"
   end
 
   depends_on "protobuf"
@@ -29,6 +28,7 @@ class NanopbGenerator < Formula
       rewrite_shebang detected_python_shebang, "nanopb_generator.py"
       libexec.install "nanopb_generator.py", "protoc-gen-nanopb", "proto"
       bin.install_symlink libexec/"protoc-gen-nanopb", libexec/"nanopb_generator.py"
+      rewrite_shebang detected_python_shebang, libexec/"protoc-gen-nanopb"
     end
   end
 
