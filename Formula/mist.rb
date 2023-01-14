601474129f379d1dc1e050cb7da82f96b49109c5
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:45:59 +0800

    mist:

--- Formula/mist.rb
+++ Formula/mist.rb
@@ -1,22 +1,20 @@
 class Mist < Formula
   desc "Mac command-line tool that automatically downloads macOS Firmwares / Installers"
   homepage "https://github.com/ninxsoft/mist-cli"
-  url "https://github.com/ninxsoft/mist-cli/archive/refs/tags/v1.9.1.tar.gz"
-  sha256 "c4973976384d2c801bd28243b5b4be5929b98f9aad14718d05539a22c337cae9"
+  url "https://github.com/ninxsoft/mist-cli/archive/refs/tags/v1.10.tar.gz"
+  sha256 "c40ae66b84ab20998794a2e6d5e94e71afc65a4b3f9b4417babb2f34cd09608b"
   license "MIT"
   head "https://github.com/ninxsoft/mist-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "9c577efc7fb3bda73e9d5fba24d1f898f270b92e101a9820a9f5bf446eee9b8c"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8eccc9c06cbc334be6e0c10734bfc27ff3407ca6689679b588c51356bba87712"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "034e6dcd59c97041a1033abe1c6cf69331baffeb13623f9209357d55683edaac"
-    sha256 cellar: :any_skip_relocation, ventura:        "ed30c419439e153bf435bec3daa359145d46c5ce76218deca63b7751a0b61238"
-    sha256 cellar: :any_skip_relocation, monterey:       "04c3a513218e8f1ce6bf5864476417a2c9794684a0226ec61848c30949459542"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cc83e0f359cadc97a5c48cbc7e6c57634187d4a7d16a675c4158339ed196df96"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "bc508927c1bdda99abe23778eac259286e54a7f36ccd0c76ed02821440261b9b"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "b81d11c1a272ed78fc7dfd4c476d34d3f4e46184232e33013e83cfa917b937fc"
+    sha256 cellar: :any_skip_relocation, ventura:        "6772980dd498a6b6ff21ad5541257c51f9eba540a3b252908ebc3b63a8a0f4db"
+    sha256 cellar: :any_skip_relocation, monterey:       "1b55c65fcc47bc62c4ca6700610a4722a350deb69a1126cea19dce2b6dfd0765"
   end
 
-  # Mist requires Swift 5.5
-  depends_on xcode: ["13.1", :build]
+  # Mist requires Swift 5.7
+  depends_on xcode: ["14.0", :build]
   depends_on :macos
   uses_from_macos "swift"
 
