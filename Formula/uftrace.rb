af2fa83c2bae5b6c273daad8bd0efccc177e6c29
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:52:43 +0800

    uftrace:

--- Formula/uftrace.rb
+++ Formula/uftrace.rb
@@ -1,14 +1,13 @@
 class Uftrace < Formula
   desc "Function graph tracer for C/C++/Rust"
   homepage "https://uftrace.github.io/slide/"
-  url "https://github.com/namhyung/uftrace/archive/v0.12.tar.gz"
-  sha256 "2aad01f27d4f18717b681824c7a28ac3e1efd5e7bbed3ec888a3ea5af60e3700"
+  url "https://github.com/namhyung/uftrace/archive/v0.13.tar.gz"
+  sha256 "cffae82c68446c20cc3c7e87e71e57498805767a0d4085b4846f3c49f9e472d9"
   license "GPL-2.0-only"
-  revision 1
   head "https://github.com/namhyung/uftrace.git", branch: "master"
 
   bottle do
-    sha256 x86_64_linux: "3094d4d4da602ec0f30e172f64eb73a2ee3d20987632c5de99f1aba4ad7963fd"
+    sha256 x86_64_linux: "f740d7e5ecd3705d66300ebba0db9087a59e34f959df1ac2ea1bcb989358eabb"
   end
 
   depends_on "pandoc" => :build
@@ -19,11 +18,17 @@ class Uftrace < Formula
   depends_on :linux
   depends_on "luajit"
   depends_on "ncurses"
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   def install
-    # Obsolete with git master, to be removed when updating to next release
+    # TODO: Obsolete with git master, to be removed when updating to next release
     inreplace "misc/version.sh", "deps/have_libpython2.7", "deps/have_libpython*"
+
+    python3 = "python3.11"
+    pyver = Language::Python.major_minor_version python3
+    # Help pkg-config find python as we only provide `python3-embed` for aliased python formula
+    inreplace Dir["check-deps/Makefile{,.check}"], "pkg-config python3", "pkg-config python-#{pyver}"
+
     system "./configure", *std_configure_args, "--disable-silent-rules"
     system "make", "install", "V=1"
   end
