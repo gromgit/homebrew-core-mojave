9223c4e816b46db27391eec6bbdea27bf2259b0f
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:46:25 +0800

    mx: no rebuilding required

--- Formula/mx.rb
+++ Formula/mx.rb
@@ -1,8 +1,8 @@
 class Mx < Formula
   desc "Command-line tool used for the development of Graal projects"
   homepage "https://github.com/graalvm/mx"
-  url "https://github.com/graalvm/mx/archive/refs/tags/6.14.9.tar.gz"
-  sha256 "23ff44244c4ff8a3b88702463665de11a0c72f82541b00e4d162ee7ea6240f38"
+  url "https://github.com/graalvm/mx/archive/refs/tags/6.14.14.tar.gz"
+  sha256 "b944697c6f2ce17e12bab4b78116ed5bed004473037c7280a54b1137e53fd8b2"
   license "GPL-2.0-only"
 
   livecheck do
@@ -11,15 +11,15 @@ class Mx < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "cb0b6992feb0b25660e63b79795ac1dfc1de4f944a6d0c36166b921933da85ab"
+    sha256 cellar: :any_skip_relocation, all: "a2a776ec218f0cebe65631c8c9991e7792308cc915df25c06701c3320505bc16"
   end
 
   depends_on "openjdk" => :test
   depends_on "python@3.11"
 
   resource "homebrew-testdata" do
-    url "https://github.com/oracle/graal/archive/refs/tags/vm-22.1.0.1.tar.gz"
-    sha256 "7653558bc4e4a5f89e5ddef7242ddc1ec5582ec75bdc94997feb76ed12ce8e94"
+    url "https://github.com/oracle/graal/archive/refs/tags/vm-22.3.0.tar.gz"
+    sha256 "410a003b8bab17af86fbc072d549e02e795b862a8396d08af9794febee17bad4"
   end
 
   def install
