c23cda77eb9d8dbeb7ec117b584ea9e0ecf58912
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:52:23 +0800

    traefik@1:

--- Formula/traefik@1.rb
+++ Formula/traefik@1.rb
@@ -5,15 +5,14 @@ class TraefikAT1 < Formula
   sha256 "0f068c2720dadd66ce303863a80d2386a4d13b5475d4219ba3e65b8445c653f2"
   license "MIT"
 
-  livecheck do
-    url "https://github.com/traefik/traefik.git"
-    regex(/^v?(1(?:\.\d+)+)$/i)
-  end
-
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/traefik@1"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, mojave: "bdfd04e38d8ae39dcebbd5b903bdf9befcca9b5e9d8a88739e88d26f4209e0de"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "1e31f7d05eef29c95084bfca1f7dae694b56a6926e4810e1d151d7a483c1a93a"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "6e8060b579d2b50e927526da6c8ced95ee5b297a15085e2358651dff21aa41d7"
+    sha256 cellar: :any_skip_relocation, ventura:        "fd0dd9a960fbb6e5abd966c41e6c4423ec5b4051954e7d17b57d72f7d4da5c53"
+    sha256 cellar: :any_skip_relocation, monterey:       "66cc839bf105e161e16aec5b056c5a5c908d7d5fbe2fadb5668614d8c764e783"
+    sha256 cellar: :any_skip_relocation, big_sur:        "95d3b3da8a19aa4cb133c8e5b95b63a957551732c2a81fb4b80015dc84df0237"
+    sha256 cellar: :any_skip_relocation, catalina:       "a1f090529ee3ed1646d872898219662f76d215c5cf7389cb84d4f22e540b7afd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "69adbe00d348f14f732bbd9fe57d57e6f513f6f1c72f5ff2f4e09fae5b4ee2d9"
   end
 
   keg_only :versioned_formula
