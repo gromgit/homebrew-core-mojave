19e4b9c8938adf46f890c505f7a68b4f0566cad2
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:44:05 +0800

    libfuse:

--- Formula/libfuse.rb
+++ Formula/libfuse.rb
@@ -1,28 +1,23 @@
 class Libfuse < Formula
   desc "Reference implementation of the Linux FUSE interface"
   homepage "https://github.com/libfuse/libfuse"
-  url "https://github.com/libfuse/libfuse/releases/download/fuse-3.11.0/fuse-3.11.0.tar.xz"
-  sha256 "8982c4c521daf3974dda8a5d55d575c988da13a571970f00aea149eb54fdf14c"
+  url "https://github.com/libfuse/libfuse/releases/download/fuse-3.13.0/fuse-3.13.0.tar.xz"
+  sha256 "1e54d3ee1d7d04f41e77617c4f7514f611b94332215dd88394bd82803032752a"
   license any_of: ["LGPL-2.1-only", "GPL-2.0-only"]
   head "https://github.com/libfuse/libfuse.git", branch: "master"
 
   bottle do
-    sha256 x86_64_linux: "ea609a3093702c051c16f7bd0bc1140fecdfa51a81b854445335cc3d94aaeb75"
+    sha256 x86_64_linux: "d63ad2a79c24739bab51d90f0e14a7476ce74ceb479c4025b79391f0e75ebc64"
   end
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on :linux
 
-  # prevent libfuse from installing into /etc
-  patch :p0 do
-    url "https://raw.githubusercontent.com/conda-forge/libfuse-feedstock/fb966bfef17fa050eb0d2b819f7c6c06575962b9/recipe/0001-Install-fusermount-init-script-into-sysconfdir.patch"
-    sha256 "ce2a512bbb1e7432c12e7c1c8fa97f07d9b6c1060bea90af8cb6ba5010b38a3c"
-  end
-
   def install
     args = std_meson_args + %W[
       --sysconfdir=#{etc}
+      -Dinitscriptdir=#{etc}/init.d
       -Dudevrulesdir=#{etc}/udev/rules.d
       -Duseroot=false
     ]
