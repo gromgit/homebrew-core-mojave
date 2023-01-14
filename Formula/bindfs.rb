5b7bc19e1adbf7093571e6ce7eb19184a70d9eb3
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:37:11 +0800

    bindfs:

--- Formula/bindfs.rb
+++ Formula/bindfs.rb
@@ -10,7 +10,7 @@ class Bindfs < Formula
   end
 
   head do
-    url "https://github.com/mpartel/bindfs.git"
+    url "https://github.com/mpartel/bindfs.git", branch: "master"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
@@ -18,14 +18,8 @@ class Bindfs < Formula
   end
 
   depends_on "pkg-config" => :build
-
-  on_macos do
-    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
-  end
-
-  on_linux do
-    depends_on "libfuse"
-  end
+  depends_on "libfuse"
+  depends_on :linux # on macOS, requires closed-source macFUSE
 
   def install
     args = %W[
@@ -41,18 +35,6 @@ class Bindfs < Formula
     end
 
     system "make", "install"
-  end
-
-  def caveats
-    on_macos do
-      <<~EOS
-        The reasons for disabling this formula can be found here:
-          https://github.com/Homebrew/homebrew-core/pull/64491
-
-        An external tap may provide a replacement formula. See:
-          https://docs.brew.sh/Interesting-Taps-and-Forks
-      EOS
-    end
   end
 
   test do
