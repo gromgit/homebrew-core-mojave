8ea6bdc3075540e9449139f5822a54dc8adfe2ee
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:52:54 +0800

    urdfdom_headers: no rebuilding required

--- Formula/urdfdom_headers.rb
+++ Formula/urdfdom_headers.rb
@@ -1,21 +1,16 @@
 class UrdfdomHeaders < Formula
   desc "Headers for Unified Robot Description Format (URDF) parsers"
   homepage "https://wiki.ros.org/urdfdom_headers/"
-  url "https://github.com/ros/urdfdom_headers/archive/1.0.5.tar.gz"
-  sha256 "76a68657c38e54bb45bddc4bd7d823a3b04edcd08064a56d8e7d46b9912035ac"
+  url "https://github.com/ros/urdfdom_headers/archive/1.1.0.tar.gz"
+  sha256 "01b91c2f7cb42b0033cbdf559684a60001f9927e5d0a5a3682a344cc354f1d39"
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "9d1960994a18f178a7ff7b5f6a0b479bbd5abcba370fb72b816fd99ff044283c"
-    sha256 cellar: :any_skip_relocation, big_sur:       "45e38439bb14220663e856c3db0017987adfe1765c815344c38f404cf49849a6"
-    sha256 cellar: :any_skip_relocation, catalina:      "c6840c3177042a718ae29bc8238e5ec20dbcde4f9269b3042f8eed1a2aa6292c"
-    sha256 cellar: :any_skip_relocation, mojave:        "c6840c3177042a718ae29bc8238e5ec20dbcde4f9269b3042f8eed1a2aa6292c"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "c6840c3177042a718ae29bc8238e5ec20dbcde4f9269b3042f8eed1a2aa6292c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f9b714415e5939d4ee8c7cbee3494f3f84eb06def8635f8d579dff740af05d6f"
-    sha256 cellar: :any_skip_relocation, all:           "c851880d554db90688c10eb68a1ea22051f0bb7453667590aa61f9821983bedb"
+    sha256 cellar: :any_skip_relocation, all: "7af8770bcbebfab71cb7921651eae92d0d57a1e4176842c0a63dc7fc64715e8c"
   end
 
   depends_on "cmake" => :build
+  depends_on "pkg-config" => :test
 
   def install
     ENV.cxx11
@@ -33,7 +28,7 @@ class UrdfdomHeaders < Formula
         return 0;
       }
     EOS
-    system ENV.cxx, "test.cpp", "-std=c++11", "-o", "test"
+    system ENV.cxx, shell_output("pkg-config --cflags urdfdom_headers").chomp, "test.cpp", "-std=c++11", "-o", "test"
     system "./test"
   end
 end
