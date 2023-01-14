024db7223f29f29a85cab7cbd559d50f503b34b4
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:38:43 +0800

    cpp-httplib: no rebuilding required

--- Formula/cpp-httplib.rb
+++ Formula/cpp-httplib.rb
@@ -1,12 +1,12 @@
 class CppHttplib < Formula
   desc "C++ header-only HTTP/HTTPS server and client library"
   homepage "https://github.com/yhirose/cpp-httplib"
-  url "https://github.com/yhirose/cpp-httplib/archive/refs/tags/v0.11.3.tar.gz"
-  sha256 "799b2daa0441d207f6cd1179ae3a34869722084a434da6614978be1682c1e12d"
+  url "https://github.com/yhirose/cpp-httplib/archive/refs/tags/v0.11.4.tar.gz"
+  sha256 "28f76b875a332fb80972c3212980c963f0a7d2e11a8fe94a8ed0d847b9a2256f"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "a8e77a0a4aeb29b9a9f0eb4419bb188dd157dfafa64c5f7d32aad521d1729d60"
+    sha256 cellar: :any_skip_relocation, all: "c86c70d3dcf73dcd981b11dd31694ec11434c17105a75ab8ffbef43e649428ab"
   end
 
   depends_on "meson" => :build
