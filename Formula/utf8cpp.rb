89d1196aef12af07c58e1115e130a2a6512a71d5
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:52:57 +0800

    utf8cpp: no rebuilding required

--- Formula/utf8cpp.rb
+++ Formula/utf8cpp.rb
@@ -1,12 +1,12 @@
 class Utf8cpp < Formula
   desc "UTF-8 with C++ in a Portable Way"
   homepage "https://github.com/nemtrif/utfcpp"
-  url "https://github.com/nemtrif/utfcpp/archive/v3.2.2.tar.gz"
-  sha256 "6f81e7cb2be2a6a9109a8a0cb7dc39ec947f1bcdb5dfa4a660e11a23face19f5"
+  url "https://github.com/nemtrif/utfcpp/archive/v3.2.3.tar.gz"
+  sha256 "3ba9b0dbbff08767bdffe8f03b10e596ca351228862722e4c9d4d126d2865250"
   license "BSL-1.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "92ba7b2aed9ef53efabbaee1b3abe3fc96c61b981ba266c52ad436d464deca0d"
+    sha256 cellar: :any_skip_relocation, all: "c8b82f1b5ab9d42d52df6ea117c78381bf3ff62b44e417901b0e5942ca32ff10"
   end
 
   depends_on "cmake" => [:build, :test]
