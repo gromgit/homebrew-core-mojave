635ec428c6128f7c27d4735538e5c6e9455cf0e3
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:40:56 +0800

    func-e:

--- Formula/func-e.rb
+++ Formula/func-e.rb
@@ -1,16 +1,22 @@
 class FuncE < Formula
   desc "Easily run Envoy"
   homepage "https://func-e.io"
-  url "https://github.com/tetratelabs/func-e/archive/v1.1.3.tar.gz"
-  sha256 "2dd1598efd743dae38a55f6943eaa62d17f2db9996be249edf5e52495338b5e9"
+  url "https://github.com/tetratelabs/func-e/archive/v1.1.4.tar.gz"
+  sha256 "f8829bde3201960edbea764002c16ade4d68e0a7ddc453d042cd1fb5bba3f6d5"
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/func-e"
-    sha256 cellar: :any_skip_relocation, mojave: "7166b6b9379cf204b62f6704eb9f8298ac33cab6b39f50b7be29af18bca32ff8"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "83588b07dd168fdf5138e2b55d5388423b8f4c138a18f7de3186fd512f23bdbe"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "c2969bb7e8054052b3736688519486280031fa0224a9d4a9de20394bce7ce514"
+    sha256 cellar: :any_skip_relocation, ventura:        "d00c61a8a944fcf6b8aa3338d755a9534f0cf1379ad94d870c93dc7b203f3b46"
+    sha256 cellar: :any_skip_relocation, monterey:       "ecfeb425d1362879536b073f154b3f8957c889288495e5f533a367d1bee3e0e3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "9271ead514ec571869cb59f8f6eef82935489654b33b25497ce72aa12baff3df"
   end
 
   depends_on "go" => :build
+  # archive-envoy does not support macos-11
+  # https://github.com/Homebrew/homebrew-core/pull/119899#issuecomment-1374663837
+  depends_on macos: :monterey
 
   def install
     ldflags = %W[
