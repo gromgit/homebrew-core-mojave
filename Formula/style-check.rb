c2b85d8f59392c1f26cfad7f2245d1638daa001f
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:51:17 +0800

    style-check: no rebuilding required

--- Formula/style-check.rb
+++ Formula/style-check.rb
@@ -4,6 +4,7 @@ class StyleCheck < Formula
   url "https://www.cs.umd.edu/~nspring/software/style-check-0.14.tar.gz"
   sha256 "2ae806fcce9e3b80162c64634422dc32d7f0e6f8a81ba5bc7879358744b4e119"
   license "GPL-2.0"
+  revision 1
 
   # The homepage links to an unversioned tarball (style-check-current.tar.gz)
   # and the GitHub repository (https://github.com/nspring/style-check) has no
@@ -13,21 +14,14 @@ class StyleCheck < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "abef781d8bcc9e2070e039fe4adfb5f56d5fb428af473439628ec98fe87b788d"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "abef781d8bcc9e2070e039fe4adfb5f56d5fb428af473439628ec98fe87b788d"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "225c5a3dd8f66d7b42f60d165aa7190e252f4432bf0c41cfed8216746bbbbfef"
-    sha256 cellar: :any_skip_relocation, ventura:        "abef781d8bcc9e2070e039fe4adfb5f56d5fb428af473439628ec98fe87b788d"
-    sha256 cellar: :any_skip_relocation, monterey:       "abef781d8bcc9e2070e039fe4adfb5f56d5fb428af473439628ec98fe87b788d"
-    sha256 cellar: :any_skip_relocation, big_sur:        "ad0fe7316475b3384c68b37f288a76e9e20933fe382dd28c3e93944ecb3cf52d"
-    sha256 cellar: :any_skip_relocation, catalina:       "5f652a7db0691978cb42a5be5d0e293e671c2953364b64b75a0ba663b581051c"
-    sha256 cellar: :any_skip_relocation, mojave:         "6427d438896bf32d45e0f71aa3d81817e59ac39ffc23cc6fc0ef1a29fb413491"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "3fdd513cff0a9f298357e5e918532bc2c236cbb6bf4c981a77270c69b49b9f08"
-    sha256 cellar: :any_skip_relocation, sierra:         "1a6787ed0c5cbe6ee567e112b16d5d32193bb0dacbf23e515f512df3287a3abd"
-    sha256 cellar: :any_skip_relocation, el_capitan:     "8ad833cef9ca35a1ae95569f93c8936a82c6d6039e3bd4560e2b1dc93fd029d8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "abef781d8bcc9e2070e039fe4adfb5f56d5fb428af473439628ec98fe87b788d"
+    sha256 cellar: :any_skip_relocation, all: "0e16c4ab38be982971253cf502fea88db6ee637c3dcdee98c5256ac663ab7dad"
   end
 
   uses_from_macos "ruby"
+
+  # Fix compatibility with Ruby 3.2.
+  # Should be deletable on next release.
+  patch :DATA
 
   def install
     inreplace "style-check.rb", "/etc/style-check.d/", etc/"style-check.d/"
@@ -43,3 +37,18 @@ class StyleCheck < Formula
     system "#{bin}/style-check.rb", "-v", "paper.tex"
   end
 end
+__END__
+diff --git a/style-check.rb b/style-check.rb
+index 893a59f..43e0e84 100755
+--- a/style-check.rb
++++ b/style-check.rb
+@@ -163,7 +163,7 @@ def do_cns(line, file, linenum, phra_hash)
+     if(detected = phra_hash.keys.detect { |r| m = r.match(line) and (line.index("\n") == nil or m.begin(0) < line.index("\n"))  } ) then
+       matchedlines = ( m.end(0) <= ( line.index("\n") or 0 ) ) ? line.gsub(/\n.*/,'') : line.chomp
+       puts "%s:%d:%s%d: %s (%s)" % [ file, linenum, Gedit_Mode ? ' ': '', m.begin(0)+1, matchedlines, m.to_s.tr("\n", ' ') ]
+-      $exit_status = 1 if(!phra_hash[detected] =~ /\?\s*$/) 
++      $exit_status = 1 if(! /\?\s*$/.match(phra_hash[detected]))
+       if($VERBOSE && phra_hash[detected]) then
+         puts "  " + phra_hash[detected]
+         phra_hash[detected] = nil # don't print the reason more than once
+
