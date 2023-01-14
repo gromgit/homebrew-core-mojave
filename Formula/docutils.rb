b3e24bd74ee0e767165fe03dc4d38f28571c150c
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:39:33 +0800

    docutils: no rebuilding required

--- Formula/docutils.rb
+++ Formula/docutils.rb
@@ -6,55 +6,23 @@ class Docutils < Formula
   license all_of: [:public_domain, "BSD-2-Clause", "GPL-3.0-or-later", "Python-2.0"]
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/docutils"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "d507d68624caaed5c68fc7b375a5b778299db1e03ef6b7a3db123bece080fc4f"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, all: "137618cfdeedcb9cdb104c8ac7ba4e17d1f08dafc0ebabdddfe6f18dde061793"
   end
 
-  depends_on "python@3.10" => [:build, :test]
-  depends_on "python@3.11" => [:build, :test]
-
-  def pythons
-    deps.map(&:to_formula)
-        .select { |f| f.name.match?(/^python@3\.\d+$/) }
-  end
+  depends_on "python@3.11"
 
   def install
-    pythons.each do |python|
-      python_exe = python.opt_libexec/"bin/python"
-      system python_exe, *Language::Python.setup_install_args(libexec, python_exe)
+    python3 = "python3.11"
+    system python3, *Language::Python.setup_install_args(prefix, python3)
 
-      site_packages = Language::Python.site_packages(python_exe)
-      pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
-      (prefix/site_packages/"homebrew-docutils.pth").write pth_contents
-
-      pyversion = Language::Python.major_minor_version(python_exe)
-      Dir.glob("#{libexec}/bin/*.py") do |f|
-        bname = File.basename(f, ".py")
-        bin.install_symlink f => "#{bname}-#{pyversion}"
-        bin.install_symlink f => "#{bname}.py-#{pyversion}"
-      end
-
-      next unless python == pythons.max_by(&:version)
-
-      # The newest one is used as the default
-      Dir.glob("#{libexec}/bin/*.py") do |f|
-        bname = File.basename(f, ".py")
-        bin.install_symlink f => bname
-        bin.install_symlink f => "#{bname}.py"
-      end
+    bin.glob("*.py") do |f|
+      bin.install_symlink f => f.basename(".py")
     end
   end
 
   test do
-    pythons.each do |python|
-      python_exe = python.opt_libexec/"bin/python"
-      pyversion = Language::Python.major_minor_version(python_exe)
-      system "#{bin}/rst2man.py-#{pyversion}", "#{prefix}/HISTORY.txt"
-      system "#{bin}/rst2man-#{pyversion}", "#{prefix}/HISTORY.txt"
-    end
-
-    system "#{bin}/rst2man.py", "#{prefix}/HISTORY.txt"
-    system "#{bin}/rst2man", "#{prefix}/HISTORY.txt"
+    system bin/"rst2man.py", prefix/"HISTORY.txt"
+    system bin/"rst2man", prefix/"HISTORY.txt"
   end
 end
