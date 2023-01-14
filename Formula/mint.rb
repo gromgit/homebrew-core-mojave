4e9d9aa99f59438fc16c275a8edf4c8139afa00e
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:45:58 +0800

    mint:

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,18 +1,18 @@
 class Mint < Formula
   desc "Dependency manager that installs and runs Swift command-line tool packages"
   homepage "https://github.com/yonaskolb/Mint"
-  url "https://github.com/yonaskolb/Mint/archive/0.17.4.tar.gz"
-  sha256 "1c4ccf124ab883a6f8c50d1d7cc5feba92c527cdc2dbcb4d2b1ae8960131aedf"
+  url "https://github.com/yonaskolb/Mint/archive/0.17.5.tar.gz"
+  sha256 "f55350f7778c4ccd38311ed36f39287ff74bb63eb230f6d448e35e7f934c489c"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "0fe1c5f8db79dfa97fb36ec9646079f23a18441dbcd83cca9649f2f59b4fe593"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "1797a7a9073fdd6900ed06e6e7305add5c5a62626947913c032b528bc4543c33"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "61d91ea891ced7d9c9927f57898c6d3ae7d1056410c99244ac60e6efcb642536"
-    sha256 cellar: :any_skip_relocation, ventura:        "c3f28c2b81d7da7d93b0a67b0506efce2bd485e9a01f9d81f560b9fe93b85ba9"
-    sha256 cellar: :any_skip_relocation, monterey:       "771fd3357631887772c69a9340cbe88f9ae488458270b3491b683fac0aae4685"
-    sha256 cellar: :any_skip_relocation, big_sur:        "9a257fa4677c1cc00b53290855874700f5b39eac9e4c90098627549130b01747"
-    sha256                               x86_64_linux:   "0b06ed938d61bf55d577b305cddd1fb43081d1f8d9cb06e8b2d5755daaf7fbda"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "250948fe6fc14179d7c381d084a90d6796861ba9a8456617cadda9ac62cbc2b8"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6546b80b980a45036415162189dd340b1f8d3f4e82a80d40a24e7b5dd672eb04"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "39f9d254b248a44bb44e399081b7e50a6c598834e2bf86bb7de3ebc349f11e0d"
+    sha256 cellar: :any_skip_relocation, ventura:        "068f9984e81b578f2ed6cef4cc9659835a689bdecf121651ea24ebcfefd49339"
+    sha256 cellar: :any_skip_relocation, monterey:       "f8b09a640942548a151c7450c85f33d40162c7540049666131740d49c68e61e6"
+    sha256 cellar: :any_skip_relocation, big_sur:        "528ea907912e8002cd3a769e8ddda4556cf2482122c3f848a7d923146df37101"
+    sha256                               x86_64_linux:   "7c8dd63f0310a46f67550f92ee48a370fadfc1a4d884b8a3904a36b7b610b3f2"
   end
 
   depends_on xcode: ["12.0", :build]
