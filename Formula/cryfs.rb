685f35301864679f1a31c75b65583ab44400582e
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:38:52 +0800

    cryfs:

--- Formula/cryfs.rb
+++ Formula/cryfs.rb
@@ -3,13 +3,13 @@ class Cryfs < Formula
 
   desc "Encrypts your files so you can safely store them in Dropbox, iCloud, etc."
   homepage "https://www.cryfs.org"
-  url "https://github.com/cryfs/cryfs/releases/download/0.11.2/cryfs-0.11.2.tar.gz"
-  sha256 "a89ab8fea2d494b496867107ec0a3772fe606ebd71ef12152fcd233f463a2c00"
+  url "https://github.com/cryfs/cryfs/releases/download/0.11.3/cryfs-0.11.3.tar.gz"
+  sha256 "cffef7669b8cbec3e7420088faac492390b9e1f3d3d0dc2a245b87f8df05f190"
   license "LGPL-3.0"
-  revision 3
+  revision 2
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "79d878d0658d912d5291ee93fa4505c840bbb6e2a39d7fcf988902924d19f158"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "998aed8cbec6408a24741d7b6ca69a7f85aed5d6b4bdc66d80755aa288b5191a"
   end
 
   head do
@@ -19,38 +19,23 @@ class Cryfs < Formula
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "boost"
+  depends_on "curl"
+  depends_on "libfuse@2"
+  depends_on :linux # on macOS, requires closed-source macFUSE
   depends_on "openssl@1.1"
-  depends_on "python@3.10"
+  depends_on "python@3.11"
   depends_on "range-v3"
   depends_on "spdlog"
 
-  uses_from_macos "curl"
-
-  on_macos do
-    depends_on "libomp"
-    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
-  end
-
-  on_linux do
-    depends_on "libfuse@2"
-  end
-
   fails_with gcc: "5"
 
-  resource("versioneer") do
-    url "https://files.pythonhosted.org/packages/25/ba/abbf66b15ad1c195c96d533a70ca7962ddd8e37d682b60b03e59afec4487/versioneer-0.22.tar.gz"
-    sha256 "9f0e9a2cb5ef521cbfd104d43a208dd9124dfb4accfa72d694e0d0430a0142bc"
-  end
-
-  # Fix build with fmt 9+
-  # https://github.com/cryfs/cryfs/pull/433
-  patch do
-    url "https://github.com/cryfs/cryfs/commit/01cf1d5fc98b6c9ac4d7dacb59c6fb787225ea48.patch?full_index=1"
-    sha256 "1ad5022b6054e9ee98721c30cd8c038bf5f2fb5750047a954df62aefbd1ee3fd"
+  resource "versioneer" do
+    url "https://files.pythonhosted.org/packages/15/86/bed1c929495d8ca30512c8fcc6e9c2555ecffcdd32f0c04f11e492eba9e0/versioneer-0.28.tar.gz"
+    sha256 "7175ca8e7bb4dd0e3c9779dd2745e5b4a6036304af3f5e50bd896f10196586d6"
   end
 
   def install
-    python = "python3.10"
+    python = "python3.11"
     venv_root = buildpath/"venv"
 
     venv = virtualenv_create(venv_root, python)
@@ -63,34 +48,11 @@ class Cryfs < Formula
       "-DBUILD_TESTING=off",
     ]
 
-    if build.head? && OS.mac?
-      libomp = Formula["libomp"]
-      configure_args.concat(
-        [
-          "-DOpenMP_CXX_FLAGS='-Xpreprocessor -fopenmp -I#{libomp.include}'",
-          "-DOpenMP_CXX_LIB_NAMES=omp",
-          "-DOpenMP_omp_LIBRARY=#{libomp.lib}/libomp.dylib",
-        ],
-      )
-    end
-
     system "cmake", "-B", "build", "-S", ".", *configure_args, *std_cmake_args,
                     "-DCRYFS_UPDATE_CHECKS=OFF",
                     "-DDEPENDENCY_CONFIG=cmake-utils/DependenciesFromLocalSystem.cmake"
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
-  end
-
-  def caveats
-    on_macos do
-      <<~EOS
-        The reasons for disabling this formula can be found here:
-          https://github.com/Homebrew/homebrew-core/pull/64491
-
-        An external tap may provide a replacement formula. See:
-          https://docs.brew.sh/Interesting-Taps-and-Forks
-      EOS
-    end
   end
 
   test do
@@ -106,7 +68,7 @@ class Cryfs < Formula
     # the cryfs bottle was compiled with and the crypto++ library installed by homebrew to.
     mkdir "basedir"
     mkdir "mountdir"
-    expected_output = OS.mac? ? "Operation not permitted" : "fuse: device not found, try 'modprobe fuse' first"
+    expected_output = "fuse: device not found, try 'modprobe fuse' first"
     assert_match expected_output, pipe_output("#{bin}/cryfs -f basedir mountdir 2>&1", "password")
   end
 end
