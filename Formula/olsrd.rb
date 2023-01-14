d718ea3f6c20dfb9a535f51b14f86a0f32dad135
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:47:05 +0800

    olsrd: no rebuilding required

--- Formula/olsrd.rb
+++ Formula/olsrd.rb
@@ -74,32 +74,6 @@ class Olsrd < Formula
     system "make", "install_all", *args
   end
 
-  plist_options startup: true, manual: "olsrd -f #{HOMEBREW_PREFIX}/etc/olsrd.conf"
-
-  def startup_plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-        <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-            <string>#{HOMEBREW_PREFIX}/sbin/olsrd</string>
-            <string>-f</string>
-            <string>#{etc}/olsrd.conf</string>
-          </array>
-          <key>KeepAlive</key>
-          <dict>
-            <key>NetworkState</key>
-            <true/>
-          </dict>
-        </dict>
-      </plist>
-    EOS
-  end
-
   test do
     assert_match version.to_s, pipe_output("#{sbin}/olsrd")
   end
