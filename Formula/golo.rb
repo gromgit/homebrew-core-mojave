60fd5bf999b9593a20a46da115e50cf29e0a5448
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:41:49 +0800

    golo: no rebuilding required

--- Formula/golo.rb
+++ Formula/golo.rb
@@ -1,19 +1,16 @@
 class Golo < Formula
   desc "Lightweight dynamic language for the JVM"
   homepage "https://golo-lang.org/"
-  url "https://github.com/eclipse/golo-lang/releases/download/release%2F3.4.0/golo-3.4.0.zip"
+  url "https://github.com/eclipse-archived/golo-lang/releases/download/release%2F3.4.0/golo-3.4.0.zip"
   sha256 "867c462a41a20e4b7dc1aef461b809d193a505c2a757477b147f0e30235bd545"
   license "EPL-2.0"
-  head "https://github.com/eclipse/golo-lang.git", branch: "master"
-
-  livecheck do
-    url :stable
-    regex(%r{^release/v?(\d+(?:\.\d+)+)$}i)
-  end
+  head "https://github.com/eclipse-archived/golo-lang.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "069c041fccb8a95fd9ea3c7a9c2105b384433a0e724d5b48812aa37d0d5c9f2d"
   end
+
+  deprecate! date: "2023-01-01", because: :repo_archived
 
   depends_on "openjdk@11"
 
