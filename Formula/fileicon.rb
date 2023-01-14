bcbc2e0bd7423386c819f9a10a7da5daf942e88d
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:40:30 +0800

    fileicon: no rebuilding required

--- Formula/fileicon.rb
+++ Formula/fileicon.rb
@@ -1,12 +1,12 @@
 class Fileicon < Formula
   desc "macOS CLI for managing custom icons for files and folders"
   homepage "https://github.com/mklement0/fileicon"
-  url "https://github.com/mklement0/fileicon/archive/v0.3.1.tar.gz"
-  sha256 "3ccc1c65afa39f41c5e63a923128ec41d4fba911b22142cc5f578bccd9b96d03"
+  url "https://github.com/mklement0/fileicon/archive/v0.3.2.tar.gz"
+  sha256 "b3b0621bddde671b44707399fbf006ad9871e7878c03f1b1a7080a3939369919"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "04506e64f0255d1ba3f8a9f694987b79837ed907a4963a812b1cc5a13cbcfbd2"
+    sha256 cellar: :any_skip_relocation, all: "128446c8e98b25bf0e92168aee7e2c15e29eb8b517f6d83bc04308d6f66f588e"
   end
 
   depends_on :macos
@@ -21,6 +21,6 @@ class Fileicon < Formula
     system bin/"fileicon", "set", testpath, icon
     assert_predicate testpath/"Icon\r", :exist?
     stdout = shell_output "#{bin}/fileicon test #{testpath}"
-    assert_includes stdout, "HAS custom icon: '#{testpath}'"
+    assert_includes stdout, "HAS custom icon: folder '#{testpath}'"
   end
 end
