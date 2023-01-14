51931cfc8a0362a4908386ba7d8622e123301c2e
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:36:43 +0800

    avfs:

--- Formula/avfs.rb
+++ Formula/avfs.rb
@@ -3,46 +3,39 @@ class Avfs < Formula
   homepage "https://avf.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/avf/avfs/1.1.4/avfs-1.1.4.tar.bz2"
   sha256 "3a7981af8557f864ae10d4b204c29969588fdb526e117456e8efd54bf8faa12b"
+  license all_of: [
+    "GPL-2.0-only",
+    "LGPL-2.0-only", # for shared library
+    "GPL-2.0-or-later", # modules/dav_ls.c
+    "Zlib", # zlib/*
+  ]
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/avfs[._-]v?(\d+(?:\.\d+)+)\.t}i)
+  end
 
   bottle do
-    sha256 x86_64_linux: "6e490770e6562a092085e1a0855e7d6c988a6d8ce9d5cafdcbde082baea8679b"
+    rebuild 1
+    sha256 x86_64_linux: "32fa53bd6af3f41059f363e42069fee2455442dec722834ac4b47299cce5ec7d"
   end
 
   depends_on "pkg-config" => :build
-  depends_on macos: :sierra # needs clock_gettime
-  depends_on "openssl@1.1"
+  depends_on "bzip2"
+  depends_on "libfuse@2"
+  depends_on :linux # on macOS, requires closed-source macFUSE
   depends_on "xz"
-
-  on_macos do
-    disable! date: "2021-04-08", because: "requires closed-source macFUSE"
-  end
-
-  on_linux do
-    depends_on "libfuse@2"
-  end
+  depends_on "zlib"
 
   def install
-    args = %W[
-      --disable-silent-rules
-      --enable-fuse
-      --enable-library
-      --with-ssl=#{Formula["openssl@1.1"].opt_prefix}
-    ]
-
-    system "./configure", *std_configure_args, *args
+    system "./configure", *std_configure_args,
+                          "--disable-silent-rules",
+                          "--enable-fuse",
+                          "--enable-library",
+                          "--with-system-zlib",
+                          "--with-system-bzlib",
+                          "--with-xz"
     system "make", "install"
-  end
-
-  def caveats
-    on_macos do
-      <<~EOS
-        The reasons for disabling this formula can be found here:
-          https://github.com/Homebrew/homebrew-core/pull/64491
-
-        An external tap may provide a replacement formula. See:
-          https://docs.brew.sh/Interesting-Taps-and-Forks
-      EOS
-    end
   end
 
   test do
