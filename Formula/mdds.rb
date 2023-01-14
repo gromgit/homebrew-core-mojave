ca4e615d6a0c55eac6a9dd8c9d1b8ad87366428e
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:45:38 +0800

    mdds: no rebuilding required

--- Formula/mdds.rb
+++ Formula/mdds.rb
@@ -1,17 +1,16 @@
 class Mdds < Formula
   desc "Multi-dimensional data structure and indexing algorithm"
   homepage "https://gitlab.com/mdds/mdds"
-  url "https://kohei.us/files/mdds/src/mdds-2.0.3.tar.bz2"
-  sha256 "9771fe42e133443c13ca187253763e17c8bc96a1a02aec9e1e8893367ffa9ce5"
+  url "https://kohei.us/files/mdds/src/mdds-2.1.0.tar.bz2"
+  sha256 "3359f70624f2358a773acb39c0ebf2f36954c01895fc9c0f60e1ab0916bd07e0"
   license "MIT"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "69b451e38108463d874aa74896add69ed039c0aace5cd92ddf1af00aface4383"
+    sha256 cellar: :any_skip_relocation, all: "6a5302edc4b765664e2ae40d09f03c6ca7dde35ff915076d4c74d5772aa43da4"
   end
 
   head do
-    url "https://gitlab.com/mdds/mdds.git"
+    url "https://gitlab.com/mdds/mdds.git", branch: "master"
 
     depends_on "automake" => :build
   end
@@ -49,7 +48,7 @@ class Mdds < Formula
       }
     EOS
     system ENV.cxx, "test.cpp", "-o", "test",
-                    "-std=c++11",
+                    "-std=c++17",
                     "-I#{include.children.first}"
     system "./test"
   end
