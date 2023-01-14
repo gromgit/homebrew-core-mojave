6bac62e29a8266ef0ceb866c4e67a676434e47e5
Adrian Ho <aho+vm-mojave@03s.net>
2023-01-14 23:47:47 +0800

    patchutils: no rebuilding required

--- Formula/patchutils.rb
+++ Formula/patchutils.rb
@@ -20,7 +20,7 @@ class Patchutils < Formula
   end
 
   head do
-    url "https://github.com/twaugh/patchutils.git"
+    url "https://github.com/twaugh/patchutils.git", branch: "master"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "docbook" => :build
