5bcb38618ef41b4c339884a581b405f89dbd6598
Adrian Ho <the.gromgit@gmail.com>
2023-01-26 00:24:54 +0800

    libunistring: add 1.1 bottle.

--- Formula/libunistring.rb
+++ Formula/libunistring.rb
@@ -1,16 +1,15 @@
 class Libunistring < Formula
   desc "C string library for manipulating Unicode strings"
   homepage "https://www.gnu.org/software/libunistring/"
-  url "https://ftp.gnu.org/gnu/libunistring/libunistring-1.0.tar.gz"
-  mirror "https://ftpmirror.gnu.org/libunistring/libunistring-1.0.tar.gz"
-  mirror "http://ftp.gnu.org/gnu/libunistring/libunistring-1.0.tar.gz"
-  sha256 "3c0184c0e492d7c208ce31d25dd1d2c58f0c3ed6cbbe032c5b248cddad318544"
+  url "https://ftp.gnu.org/gnu/libunistring/libunistring-1.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/libunistring/libunistring-1.1.tar.gz"
+  mirror "http://ftp.gnu.org/gnu/libunistring/libunistring-1.1.tar.gz"
+  sha256 "a2252beeec830ac444b9f68d6b38ad883db19919db35b52222cf827c385bdb6a"
   license any_of: ["GPL-2.0-only", "LGPL-3.0-or-later"]
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libunistring"
-    rebuild 1
-    sha256 cellar: :any, mojave: "777a93c4f10c379d63c410f3fa3b7c335f1edb8a63bd7da55169ffe2ebdef679"
+    sha256 cellar: :any, mojave: "8d9474b6be8ee8f310daf6c26978148fe7c3bdcb339a10ba22a0875ddde278a0"
   end
 
   def install
