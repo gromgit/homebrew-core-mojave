92d4413903dcf2d472f89aab85bdd355bb9aa5f8
Adrian Ho <the.gromgit@gmail.com>
2023-01-26 14:41:55 +0800

    libxrender: add 0.9.11 bottle.

--- Formula/libxrender.rb
+++ Formula/libxrender.rb
@@ -1,21 +1,13 @@
 class Libxrender < Formula
   desc "X.Org: Library for the Render Extension to the X11 protocol"
   homepage "https://www.x.org/"
-  url "https://www.x.org/archive/individual/lib/libXrender-0.9.10.tar.bz2"
-  sha256 "c06d5979f86e64cabbde57c223938db0b939dff49fdb5a793a1d3d0396650949"
+  url "https://www.x.org/archive/individual/lib/libXrender-0.9.11.tar.gz"
+  sha256 "6aec3ca02e4273a8cbabf811ff22106f641438eb194a12c0ae93c7e08474b667"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_ventura:  "211f41e664d83f68df02764690f151714704fcfeb3ab75478d52720d8c5015ef"
-    sha256 cellar: :any,                 arm64_monterey: "fc2e9e34f51fbc1b9a0650084d6aa43d60b4c285d2b21cfcbdf9ae77be4472fb"
-    sha256 cellar: :any,                 arm64_big_sur:  "46243f05a17674c00950dddc105b33aa479af7d605533d1aeada27d4d89d4275"
-    sha256 cellar: :any,                 ventura:        "887c5c72fe075f1b7898c42664a3609fd92df4568e91f25c3ffadce625ec407e"
-    sha256 cellar: :any,                 monterey:       "6b5d5e53d3f001a196b0e8ee0df7db0190885d1fb9e45dd0e57f9d7353f1a783"
-    sha256 cellar: :any,                 big_sur:        "65fbad7a9da818086574a40880db58c8f2457db77aee1982d141cd09dad7e3a9"
-    sha256 cellar: :any,                 catalina:       "cb7f48876d362f919ed1c34ece8ec5abb16f6e414a6119655e3948fffab5dfab"
-    sha256 cellar: :any,                 mojave:         "77563596957d673a9f4acb0cb4f1e1d28c3b99e7f4a13f0a1dd1e3e403c454b9"
-    sha256 cellar: :any,                 high_sierra:    "b1bf08a6c6c6827af5a3472ac979c9328b780da52c1d234d385ef1fc3b0771b6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "10b4f9c57a6af40dda0ca45ddecb325f9de9bee28959ed11fc0fa52d2accf562"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/libxrender"
+    sha256 cellar: :any, mojave: "6b1d35896915abb40e62019a1053584d43130872f103d9a466b652323c05a3da"
   end
 
   depends_on "pkg-config" => :build
