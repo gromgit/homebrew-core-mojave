5b8732afa69a598cac5b65d82d3956d09911dd2f
Adrian Ho <the.gromgit@gmail.com>
2023-01-26 14:47:01 +0800

    harfbuzz: add 6.0.0 bottle.

--- Formula/harfbuzz.rb
+++ Formula/harfbuzz.rb
@@ -1,19 +1,20 @@
 class Harfbuzz < Formula
   desc "OpenType text shaping engine"
   homepage "https://github.com/harfbuzz/harfbuzz"
-  url "https://github.com/harfbuzz/harfbuzz/archive/5.3.1.tar.gz"
-  sha256 "77c8c903f4539b050a6d3a5be79705c7ccf7b1cb66d68152a651486e261edbd2"
+  url "https://github.com/harfbuzz/harfbuzz/archive/6.0.0.tar.gz"
+  sha256 "6d753948587db3c7c3ba8cc4f8e6bf83f5c448d2591a9f7ec306467f3a4fe4fa"
   license "MIT"
   head "https://github.com/harfbuzz/harfbuzz.git", branch: "main"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/harfbuzz"
-    sha256 cellar: :any_skip_relocation, mojave: "efece6d2993b420929a86a1c78216b9a5b2bd9400df16ccb1fe346cf917cce6a"
+    sha256 cellar: :any, mojave: "4600e01bfaa73d5b9129fbee2e51e0f4b426c43c2ac7d4e9e9bd04154d2b0c1a"
   end
 
   depends_on "gobject-introspection" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
   depends_on "python@3.11" => [:build, :test]
   depends_on "pygobject3" => :test
   depends_on "cairo"
