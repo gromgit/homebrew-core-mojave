9f26811855f604acecb8dd7235669be4ab571bd2
Adrian Ho <the.gromgit@gmail.com>
2023-01-26 15:31:53 +0800

    svt-av1: add 1.4.1 bottle.

--- Formula/svt-av1.rb
+++ Formula/svt-av1.rb
@@ -1,21 +1,23 @@
 class SvtAv1 < Formula
   desc "AV1 encoder"
   homepage "https://gitlab.com/AOMediaCodec/SVT-AV1"
-  url "https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v1.2.1/SVT-AV1-v1.2.1.tar.gz"
-  sha256 "5c2466eb2990fa045b29c63b92a2b5f13d9cb841709df5e7206f96a59e119fdd"
+  url "https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v1.4.1/SVT-AV1-v1.4.1.tar.bz2"
+  sha256 "0e988582f315fe76c909accf5e7f81b975c5bd2b850ee760d8e9fac297f70b5d"
   license "BSD-3-Clause"
   head "https://gitlab.com/AOMediaCodec/SVT-AV1.git", branch: "master"
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/svt-av1"
-    sha256 cellar: :any, mojave: "e52739a897468408fa382377781d2aa76033dcf9c25449a5725eb22420a87c9b"
+    sha256 cellar: :any, mojave: "9bdabbeb7caf9e068bceb7562975fbe8fad7054dd406cbba6e8b86f8c4a238c9"
   end
 
   depends_on "cmake" => :build
-  depends_on "make" => :build
-  depends_on "yasm" => :build
 
-  resource("homebrew-testvideo") do
+  on_intel do
+    depends_on "yasm" => :build
+  end
+
+  resource "homebrew-testvideo" do
     url "https://github.com/grusell/svt-av1-homebrew-testdata/raw/main/video_64x64_yuv420p_25frames.yuv"
     sha256 "0c5cc90b079d0d9c1ded1376357d23a9782a704a83e01731f50ccd162e246492"
   end
