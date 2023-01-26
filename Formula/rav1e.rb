9941164eeeab8619b4ac6ad34f11848d144d6aca
Adrian Ho <the.gromgit@gmail.com>
2023-01-26 15:20:37 +0800

    rav1e: add 0.6.3 bottle.

--- Formula/rav1e.rb
+++ Formula/rav1e.rb
@@ -5,12 +5,13 @@ class Rav1e < Formula
   head "https://github.com/xiph/rav1e.git", branch: "master"
 
   stable do
-    url "https://github.com/xiph/rav1e/archive/v0.6.1.tar.gz"
-    sha256 "dd12132ad9dac229ce00a9caad132c4ad23d7db2b3ad4b5a59e89658fee04d9a"
+    url "https://github.com/xiph/rav1e/archive/v0.6.3.tar.gz"
+    sha256 "660a243dd9ee3104c0844a7af819b406193a7726614a032324557f81bb2bebaa"
 
+    # keep the version in sync
     resource "Cargo.lock" do
-      url "https://github.com/xiph/rav1e/releases/download/v0.6.1/Cargo.lock"
-      sha256 "da13dcb2ea6bc0212b077a2e2e2e55b2a674a0ae683b7a2c4cf3e4f137c920cb"
+      url "https://github.com/xiph/rav1e/releases/download/v0.6.3/Cargo.lock"
+      sha256 "f22049598eb6f9948b4d852850eeb48c1236a7a068692db85cbfe72b94498581"
     end
   end
 
@@ -21,7 +22,7 @@ class Rav1e < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/rav1e"
-    sha256 cellar: :any, mojave: "f3f6f3de290ab26e0d80c6f01c11778e87acbb981930b2835d4686f7bca9fdb3"
+    sha256 cellar: :any, mojave: "d0750da7066d41788451f7aa7ec4eff3f1589250dff240e57a194e039ce14e5a"
   end
 
   depends_on "cargo-c" => :build
@@ -31,7 +32,7 @@ class Rav1e < Formula
     depends_on "nasm" => :build
   end
 
-  resource "bus_qcif_7.5fps.y4m" do
+  resource "homebrew-bus_qcif_7.5fps.y4m" do
     url "https://media.xiph.org/video/derf/y4m/bus_qcif_7.5fps.y4m"
     sha256 "1f5bfcce0c881567ea31c1eb9ecb1da9f9583fdb7d6bb1c80a8c9acfc6b66f6b"
   end
@@ -43,7 +44,7 @@ class Rav1e < Formula
   end
 
   test do
-    resource("bus_qcif_7.5fps.y4m").stage do
+    resource("homebrew-bus_qcif_7.5fps.y4m").stage do
       system "#{bin}/rav1e", "--tile-rows=2",
                                    "bus_qcif_7.5fps.y4m",
                                    "--output=bus_qcif_15fps.ivf"
