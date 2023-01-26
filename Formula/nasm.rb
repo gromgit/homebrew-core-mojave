d620a862aafeebf30db99b683f5425c8fc566e85
Adrian Ho <the.gromgit@gmail.com>
2023-01-26 15:08:43 +0800

    nasm: add 2.16.01 bottle.

--- Formula/nasm.rb
+++ Formula/nasm.rb
@@ -1,8 +1,8 @@
 class Nasm < Formula
   desc "Netwide Assembler (NASM) is an 80x86 assembler"
   homepage "https://www.nasm.us/"
-  url "https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.xz"
-  sha256 "3caf6729c1073bf96629b57cee31eeb54f4f8129b01902c73428836550b30a3f"
+  url "https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/nasm-2.16.01.tar.xz"
+  sha256 "c77745f4802375efeee2ec5c0ad6b7f037ea9c87c92b149a9637ff099f162558"
   license "BSD-2-Clause"
 
   livecheck do
@@ -11,20 +11,12 @@ class Nasm < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "ea233a725bc6ecde3e61967f64d1ffad54e17a3cf3947b700b8d4a46ef9bc310"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "b890027bb7fef2cf20c69f3fd38ed0c19c373bce79c949ab6d123601617ae949"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8e3fb019ffdfcd4ce6b418adbe3d1d1d0e1e232a7de3b62fc4872c2401543cb3"
-    sha256 cellar: :any_skip_relocation, ventura:        "39cfdb815cf5285b57f62f149abab2da481ca72c4b28fd8aac7cc65d6d1a1280"
-    sha256 cellar: :any_skip_relocation, monterey:       "d22292dce7f323c908b6774fbc76c4716142e2b73a292556e22d3b42589fd5d4"
-    sha256 cellar: :any_skip_relocation, big_sur:        "cc45793ac9f3fedd01dd08bbbf766137a40bf22e18c43742498b0a542aa319e8"
-    sha256 cellar: :any_skip_relocation, catalina:       "1875e67160bac1675dd505d66a6b78469767d2dfe8baab2652409f91ac0549ef"
-    sha256 cellar: :any_skip_relocation, mojave:         "4b3614f857264edfa9aeab961c523b3910cdef0ceccaf9957888b477c1c512f4"
-    sha256 cellar: :any_skip_relocation, high_sierra:    "8f8c181994b1f05bf425a4034f76d6973c9e1a85ecb64af7f67d47556f23a0d6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "21fd7041a27b500983d56bf9b24739790f5de243da14a4a549a2afb3ed23deb4"
+    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/nasm"
+    sha256 cellar: :any_skip_relocation, mojave: "bf1f1d794bc40162bd319609b62480d1e360c6827effc0a7126e7b5d0fd34a87"
   end
 
   head do
-    url "https://github.com/netwide-assembler/nasm.git"
+    url "https://github.com/netwide-assembler/nasm.git", branch: "master"
     depends_on "asciidoc" => :build
     depends_on "autoconf" => :build
     depends_on "automake" => :build
@@ -35,8 +27,7 @@ class Nasm < Formula
     system "./autogen.sh" if build.head?
     system "./configure", "--prefix=#{prefix}"
     system "make", "manpages" if build.head?
-    system "make", "rdf"
-    system "make", "install", "install_rdf"
+    system "make", "install"
   end
 
   test do
