a49efe9f908f49f257caa0961f37c336fa8a62f2
Adrian Ho <the.gromgit@gmail.com>
2023-01-26 14:25:14 +0800

    gnutls: add 3.7.8_1 bottle.

--- Formula/gnutls.rb
+++ Formula/gnutls.rb
@@ -5,6 +5,7 @@ class Gnutls < Formula
   mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnutls/v3.7/gnutls-3.7.8.tar.xz"
   sha256 "c58ad39af0670efe6a8aee5e3a8b2331a1200418b64b7c51977fb396d4617114"
   license all_of: ["LGPL-2.1-or-later", "GPL-3.0-only"]
+  revision 1
 
   livecheck do
     url "https://www.gnutls.org/news.html"
@@ -13,8 +14,7 @@ class Gnutls < Formula
 
   bottle do
     root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/gnutls"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "387fa46a1c411a22885776e89eb6e9b5804eab2248c2962709d068542b819a28"
+    sha256 mojave: "4410506d45f9072a0a630026450df5ca1cdb6a2ca13063c7740240fecc4b23d2"
   end
 
   depends_on "pkg-config" => :build
@@ -27,6 +27,8 @@ class Gnutls < Formula
   depends_on "nettle"
   depends_on "p11-kit"
   depends_on "unbound"
+
+  uses_from_macos "zlib"
 
   def install
     args = %W[
