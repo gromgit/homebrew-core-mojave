8081877576e8275e34569f4131ae240837e03d1b
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:21:53 +0800

    pari-seadata: no rebuilding required

--- Formula/pari-seadata.rb
+++ Formula/pari-seadata.rb
@@ -18,21 +18,14 @@ class PariSeadata < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "311f9eeb6ce606b854b072f1b8f7b7120b19bc1e00d9fe02bcfc5a08b32453da"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "311f9eeb6ce606b854b072f1b8f7b7120b19bc1e00d9fe02bcfc5a08b32453da"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e1310a89e74bc49b8fb00213aa9210b7e2fa89c6529a23771b185a63c3b1a668"
-    sha256 cellar: :any_skip_relocation, ventura:        "311f9eeb6ce606b854b072f1b8f7b7120b19bc1e00d9fe02bcfc5a08b32453da"
-    sha256 cellar: :any_skip_relocation, monterey:       "311f9eeb6ce606b854b072f1b8f7b7120b19bc1e00d9fe02bcfc5a08b32453da"
-    sha256 cellar: :any_skip_relocation, big_sur:        "e1310a89e74bc49b8fb00213aa9210b7e2fa89c6529a23771b185a63c3b1a668"
-    sha256 cellar: :any_skip_relocation, catalina:       "e1310a89e74bc49b8fb00213aa9210b7e2fa89c6529a23771b185a63c3b1a668"
-    sha256 cellar: :any_skip_relocation, mojave:         "e1310a89e74bc49b8fb00213aa9210b7e2fa89c6529a23771b185a63c3b1a668"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "b521d8b719cf49b0b8a4a1d81d4819e7789af3f3a141bfda7a7f7b19fa7a8ee4"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "955ca29bbc133092ed50a7bd87d5393ca170f42df185c1f6ea9fdbe17d711251"
   end
 
   depends_on "pari"
 
   def install
-    (share/"pari/seadata").install gzip(*Dir["#{buildpath}/seadata/sea*"])
+    (share/"pari/seadata").install Utils::Gzip.compress(*Dir["#{buildpath}/seadata/sea*"])
     doc.install "seadata/README"
   end
 
