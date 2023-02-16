1b3aab42aaee4a6181e99fe2dcb78b3572a3b936
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:12:23 +0800

    glassfish: no rebuilding required

--- Formula/glassfish.rb
+++ Formula/glassfish.rb
@@ -1,9 +1,9 @@
 class Glassfish < Formula
   desc "Java EE application server"
   homepage "https://glassfish.org/"
-  url "https://download.eclipse.org/ee4j/glassfish/glassfish-7.0.0.zip"
-  mirror "https://github.com/eclipse-ee4j/glassfish/releases/download/7.0.0/glassfish-7.0.0.zip"
-  sha256 "3bf2140d9cab4dafe773afaf85dd7489c075e8bfb883a95f12d0aa2be306a4e3"
+  url "https://download.eclipse.org/ee4j/glassfish/glassfish-7.0.1.zip"
+  mirror "https://github.com/eclipse-ee4j/glassfish/releases/download/7.0.1/glassfish-7.0.1.zip"
+  sha256 "24d2f12dbff42782af1adb6e2c2898c19c901669ef3e4428b0cb9bf74595e6ac"
   license "EPL-2.0"
 
   livecheck do
@@ -12,12 +12,15 @@ class Glassfish < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "8f56c18877af7bd6d49496282807e73e9efe1dcf795af88d3bfb1d3c3d18fc14"
+    sha256 cellar: :any_skip_relocation, all: "dbd8ad7e40f11a6b08bf9cf4a217c146e8de3945a4ae5aa3ab38acafbebbcf7d"
   end
 
   depends_on "openjdk@17"
 
   conflicts_with "payara", because: "both install the same scripts"
+
+  # upstream PR ref, https://github.com/eclipse-ee4j/glassfish/pull/24283
+  patch :DATA
 
   def install
     # Remove all windows files
@@ -43,9 +46,10 @@ class Glassfish < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/asadmin version")
-
     port = free_port
+    # `asadmin` needs this to talk to a custom port when running `asadmin version`
+    ENV["AS_ADMIN_PORT"] = port.to_s
+
     cp_r libexec/"glassfish/domains", testpath
     inreplace testpath/"domains/domain1/config/domain.xml", "port=\"4848\"", "port=\"#{port}\""
 
@@ -56,5 +60,22 @@ class Glassfish < Formula
 
     output = shell_output("curl -s -X GET localhost:#{port}")
     assert_match "GlassFish Server", output
+
+    assert_match version.to_s, shell_output("#{bin}/asadmin version")
   end
 end
+
+__END__
+diff --git a/glassfish/config/branding/glassfish-version.properties b/glassfish/config/branding/glassfish-version.properties
+index e92142e..2147005 100644
+--- a/glassfish/config/branding/glassfish-version.properties
++++ b/glassfish/config/branding/glassfish-version.properties
+@@ -19,7 +19,7 @@ product_name=Eclipse GlassFish
+ abbrev_product_name=GlassFish
+ major_version=7
+ minor_version=0
+-update_version=0
++update_version=1
+ build_id=master-b160-g0b1e109 2020-12-19T17:24:00+0000
+ version_prefix=
+ version_suffix=
