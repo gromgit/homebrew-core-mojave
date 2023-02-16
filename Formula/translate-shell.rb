3611fb9cc0f1ef4d96b34509b9fd7dc43a722aa1
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:27:38 +0800

    translate-shell: no rebuilding required

--- Formula/translate-shell.rb
+++ Formula/translate-shell.rb
@@ -1,14 +1,13 @@
 class TranslateShell < Formula
   desc "Command-line translator using Google Translate and more"
   homepage "https://www.soimort.org/translate-shell"
-  url "https://github.com/soimort/translate-shell/archive/v0.9.7.tar.gz"
-  sha256 "fe328bff9670a925f6dd6f80629ed92b078edd9a4c3f8414fbb3d921365c59a7"
+  url "https://github.com/soimort/translate-shell/archive/v0.9.7.1.tar.gz"
+  sha256 "f949f379779b9e746bccb20fcd180d041fb90da95816615575b49886032bcefa"
   license "Unlicense"
   head "https://github.com/soimort/translate-shell.git", branch: "develop"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "2aeb23457e19de6d2502a6966e0b312e60435d7c257a19d88ac8bd54063341eb"
+    sha256 cellar: :any_skip_relocation, all: "e4e1003f63ebe71cf50e49f32093a3af1e777c5e24b2c7b4b996c753be83e5f7"
   end
 
   depends_on "fribidi"
