0fcaa36be1f21db06a4209fff5f5d0bbd598e973
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:16:42 +0800

    libdrm:

--- Formula/libdrm.rb
+++ Formula/libdrm.rb
@@ -1,8 +1,8 @@
 class Libdrm < Formula
   desc "Library for accessing the direct rendering manager"
   homepage "https://dri.freedesktop.org"
-  url "https://dri.freedesktop.org/libdrm/libdrm-2.4.114.tar.xz"
-  sha256 "3049cf843a47d12e5eeefbc3be3496d782fa09f42346bf0b7defe3d1e598d026"
+  url "https://dri.freedesktop.org/libdrm/libdrm-2.4.115.tar.xz"
+  sha256 "554cfbfe0542bddb391b4e3e05bfbbfc3e282b955bd56218d21c0616481f65eb"
   license "MIT"
 
   livecheck do
@@ -11,7 +11,7 @@ class Libdrm < Formula
   end
 
   bottle do
-    sha256 x86_64_linux: "f831178f945c86dd02df7a721a11aa1863513969c2156ea365e1dcf9ccb4760d"
+    sha256 x86_64_linux: "07bf2efc3b29fa65e560cc13c43f86fe2b250b47e3cb4f4251ad4976979e927c"
   end
 
   depends_on "docutils" => :build
