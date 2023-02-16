4a4c38800bda00fb5af54ffc23ede1c8afc00d52
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:27:58 +0800

    typewritten: no rebuilding required

--- Formula/typewritten.rb
+++ Formula/typewritten.rb
@@ -1,13 +1,13 @@
 class Typewritten < Formula
   desc "Minimal zsh prompt"
   homepage "https://typewritten.dev"
-  url "https://github.com/reobin/typewritten/archive/v1.5.0.tar.gz"
-  sha256 "0e05ced82a53776da05e49c898ae211e3b2939df1707afa2c1ac1b0358561113"
+  url "https://github.com/reobin/typewritten/archive/v1.5.1.tar.gz"
+  sha256 "db9165ea4490941d65bfa6d7d74ba0312e1667f5bbe712922a6d384bb5166aa6"
   license "MIT"
   head "https://github.com/reobin/typewritten.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "f0561ef935eb07b69c7d71ec06e2b9a043b92d7f974107e548785d2f27f9e429"
+    sha256 cellar: :any_skip_relocation, all: "0ad832359f435b0616e314ad96ac7d067632edb2c70f983b019d1ab3503784ba"
   end
 
   depends_on "zsh" => :test
