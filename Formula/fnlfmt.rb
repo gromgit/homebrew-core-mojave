6a1e3943eb444152719f3eced39e8c4494dd041c
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:10:59 +0800

    fnlfmt: no rebuilding required

--- Formula/fnlfmt.rb
+++ Formula/fnlfmt.rb
@@ -1,12 +1,12 @@
 class Fnlfmt < Formula
   desc "Formatter for Fennel code"
   homepage "https://git.sr.ht/~technomancy/fnlfmt"
-  url "https://git.sr.ht/~technomancy/fnlfmt/archive/0.2.3.tar.gz"
-  sha256 "0a7d7aaf4d88a81b6f13ed02fa88e74ed6c634ba50254de09eeb5a7777ae90d6"
+  url "https://git.sr.ht/~technomancy/fnlfmt/archive/0.3.0.tar.gz"
+  sha256 "8baa2f5ed98f449139f6bd8b4859575c4cfeacc60c0a8884ecaa27bc94e69156"
   license "LGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "28781ba4b856d98bae1c75b51e9d164dbb1f91c569ea421e5a41586410c363d5"
+    sha256 cellar: :any_skip_relocation, all: "a733d7bd0372d92ed0f756daf5ce0d816f2a20d49330602047e3dcdc4823b21d"
   end
 
   depends_on "lua"
@@ -18,7 +18,7 @@ class Fnlfmt < Formula
 
   test do
     (testpath/"testfile.fnl").write("(fn [abc def] nil)")
-    expected = "(fn [abc def]\n  nil)\n\n"
+    expected = "(fn [abc def] nil)\n\n"
     assert_equal expected, shell_output("#{bin}/fnlfmt #{testpath}/testfile.fnl")
   end
 end
