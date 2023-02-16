c7fa2eb157a647458e1edf844d956211be2b19a6
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:20:42 +0800

    ohdear-cli: no rebuilding required

--- Formula/ohdear-cli.rb
+++ Formula/ohdear-cli.rb
@@ -1,12 +1,12 @@
 class OhdearCli < Formula
   desc "Tool to manage your Oh Dear sites"
   homepage "https://github.com/ohdearapp/ohdear-cli"
-  url "https://github.com/ohdearapp/ohdear-cli/releases/download/v3.2.2/ohdear-cli.phar"
-  sha256 "bc477d85a7213e9c023eb81c185db98dbfc3adf4e540d15e192cca18710cb9e3"
+  url "https://github.com/ohdearapp/ohdear-cli/releases/download/v3.3.0/ohdear-cli.phar"
+  sha256 "54e1b773aaf0278a1df9ae9f88aa5c9a6c3b411fea5434177cec12fab8843b9c"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "1a6ebc2ee313a6e8cb8a16e0b6a5cd8878ea25a837456626ac99d2b58eac2705"
+    sha256 cellar: :any_skip_relocation, all: "d25b01d5f723612a16d5e7dde40a54507af4457d27b6dbb534c798fb064b605c"
   end
 
   depends_on "php"
