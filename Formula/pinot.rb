c985e3513bbc144153ac0e6d24d21841742f41d3
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:22:27 +0800

    pinot: no rebuilding required

--- Formula/pinot.rb
+++ Formula/pinot.rb
@@ -1,16 +1,16 @@
 class Pinot < Formula
   desc "Realtime distributed OLAP datastore"
   homepage "https://pinot.apache.org/"
-  url "https://downloads.apache.org/pinot/apache-pinot-0.10.0/apache-pinot-0.10.0-bin.tar.gz"
-  sha256 "2a317523791d2ef3f02c054977bee27f8581ddd4a273acac6a72728e727f647e"
+  url "https://downloads.apache.org/pinot/apache-pinot-0.12.0/apache-pinot-0.12.0-bin.tar.gz"
+  sha256 "f2fa34d9c1f3798f644f07210b4f8450b1d32aeda9614f707b985c3272066823"
   license "Apache-2.0"
   head "https://github.com/apache/pinot.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "8323dd5ffe20cd57eb84997675d05ccb927668c7717b37e31a41ec41dc27293a"
+    sha256 cellar: :any_skip_relocation, all: "f0c4b47e1b9272d02f791a49a0f429b0b09acf6cd510946e9e4137ff674129a2"
   end
 
-  depends_on "openjdk"
+  depends_on "openjdk@11"
 
   def install
     (var/"lib/pinot/data").mkpath
@@ -19,7 +19,7 @@ class Pinot < Formula
     libexec.install "plugins"
 
     prefix.install "bin"
-    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env)
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("11"))
     bin.glob("*.sh").each { |f| mv f, bin/f.basename(".sh") }
   end
 
