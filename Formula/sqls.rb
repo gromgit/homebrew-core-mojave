1c8d317b991f108c389f087ebb8f9709d5d055db
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:26:03 +0800

    sqls:

--- Formula/sqls.rb
+++ Formula/sqls.rb
@@ -7,9 +7,17 @@ class Sqls < Formula
   head "https://github.com/lighttiger2505/sqls.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/sqls"
-    sha256 cellar: :any_skip_relocation, mojave: "2f5fd4e922e00d7a96507a54a89b9c4d763c2dbc657cc2c3833316ae57ff9ad9"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "faf408ae757458bf1e26893201d6e900f05eca022fb1ee01559fa5a0f1d1622f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "6dbca6e8e07e366b9926ddb5da7068e4aab06a8f44ac4c10e39a4cc956c823f3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "3dea0bf951c056448af9f587db401b9aeaf8fb3def88ce54a5db05c3737cadae"
+    sha256 cellar: :any_skip_relocation, ventura:        "fd2c24c485ca9676c83fb24b735961b6a1f7436e81818e558657e058fce4b3cf"
+    sha256 cellar: :any_skip_relocation, monterey:       "7e0b61b52ec2b7f34122844099657f8be14a27af74509e7f7ecb59f9aad0f0a0"
+    sha256 cellar: :any_skip_relocation, big_sur:        "facee7e345867ac52396628dd98093a326032cefe43f1e16ce91941665dd39bd"
+    sha256 cellar: :any_skip_relocation, catalina:       "5ec498c6c475d39d5badcdc0a4e81c5a7f10d9c51c9d6019021754351ed26361"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "5eb770bee66bb17bad1307ce42b6e4d1d90352612c28ffe832ba8eeae74dd271"
   end
+
+  disable! date: "2022-11-17", because: :repo_archived
 
   depends_on "go" => :build
 
