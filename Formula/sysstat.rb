863f70d2d2c6c5f25b89113b99bc38f8a0b23634
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:26:36 +0800

    sysstat:

--- Formula/sysstat.rb
+++ Formula/sysstat.rb
@@ -1,13 +1,13 @@
 class Sysstat < Formula
   desc "Performance monitoring tools for Linux"
   homepage "https://github.com/sysstat/sysstat"
-  url "https://github.com/sysstat/sysstat/archive/v12.7.1.tar.gz"
-  sha256 "d53698c7a15c307a3debb84607891c2be21e3bcfcb31f0fdab78394b689b2bdf"
+  url "https://github.com/sysstat/sysstat/archive/v12.7.2.tar.gz"
+  sha256 "f029c520d0c3ca561b2873bc69fd69521f668ed4ee9c98e4e9bea6c3d1cad990"
   license "GPL-2.0-or-later"
   head "https://github.com/sysstat/sysstat.git", branch: "master"
 
   bottle do
-    sha256 x86_64_linux: "53f7af2480a924cd85ef62988a6da1955abf69c9b724a24674ae008590d4be04"
+    sha256 x86_64_linux: "05f96db9a925026977b6551beb9d1ad4fcbf98a55291065bd8662b57f659987d"
   end
 
   depends_on :linux
