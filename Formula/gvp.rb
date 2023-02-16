d524cec5400d9e26a8e437fdd7e859b665aeb19f
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:13:52 +0800

    gvp: no rebuilding required

--- Formula/gvp.rb
+++ Formula/gvp.rb
@@ -6,15 +6,8 @@ class Gvp < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e7ecd2c48a06d5549d508115da2b17f58d9aca5ed231cadec8d0bf09af903cb5"
-    sha256 cellar: :any_skip_relocation, big_sur:       "883562299458f2ce52046bd9616153e2e28ce70307d7003d2f4ace0fb1987159"
-    sha256 cellar: :any_skip_relocation, catalina:      "5ac60db3ccf491e68595a4ae2185ea2acb1e733bbdd4dabe24617e7f19409790"
-    sha256 cellar: :any_skip_relocation, mojave:        "dce646d2b2bbc9cfecbeb99360b0df2d1149b758a40af3ab81138b544a6e3871"
-    sha256 cellar: :any_skip_relocation, high_sierra:   "c62a176dc8bee30dcd1453a8b3c608dcd059dc133167df74802d515931470f6d"
-    sha256 cellar: :any_skip_relocation, sierra:        "2405a1e481ebfafcd4fbfdc2874feacc402b851fafdc69596d1afa120924c157"
-    sha256 cellar: :any_skip_relocation, el_capitan:    "ddd00ded9d21c3ecfe23e807619d3ab1b3011bc586db0d7d4aa8d5d87e3689c6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "edc2b278418b14f3e34504829b05b43db8920d2ddba011653f59688fb9a43068"
-    sha256 cellar: :any_skip_relocation, all:           "edc2b278418b14f3e34504829b05b43db8920d2ddba011653f59688fb9a43068"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, all: "edc2b278418b14f3e34504829b05b43db8920d2ddba011653f59688fb9a43068"
   end
 
   # Upstream fix for "syntax error near unexpected token `;'"
