1a35f46e572a0449c618ee628f308ff3af414825
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:12:06 +0800

    git-secrets: no rebuilding required

--- Formula/git-secrets.rb
+++ Formula/git-secrets.rb
@@ -6,10 +6,18 @@ class GitSecrets < Formula
   license "Apache-2.0"
   head "https://github.com/awslabs/git-secrets.git", branch: "master"
 
-bottle do
-    root_url "https://github.com/gromgit/homebrew-core-mojave/releases/download/git-secrets"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, mojave: "789926d6382721b816a9066694aeba6895bb30e4ac8c06ad103b944474999cf9"
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "3514dfa1af5dfeacdee08546d64f3d60766318d386af8ff7f9a0057314fe7d33"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3514dfa1af5dfeacdee08546d64f3d60766318d386af8ff7f9a0057314fe7d33"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "e9de924f892439c26efd7c99bf498edb0425f2ca2ed1b2f6a41894f22a45e362"
+    sha256 cellar: :any_skip_relocation, ventura:        "3514dfa1af5dfeacdee08546d64f3d60766318d386af8ff7f9a0057314fe7d33"
+    sha256 cellar: :any_skip_relocation, monterey:       "3514dfa1af5dfeacdee08546d64f3d60766318d386af8ff7f9a0057314fe7d33"
+    sha256 cellar: :any_skip_relocation, big_sur:        "847ce4314909e69c8c5e30d67599c1b7892ceb27016a1b4636a4b92a9996bb97"
+    sha256 cellar: :any_skip_relocation, catalina:       "92ae3c8447754b1f5d23b5e7e3a601fca133f4b691b51b5004ffe17f4d763622"
+    sha256 cellar: :any_skip_relocation, mojave:         "d77761ee552d2963788f2bcab6c695d1b52f9d0c1d68dad65230901c750e63aa"
+    sha256 cellar: :any_skip_relocation, high_sierra:    "d77761ee552d2963788f2bcab6c695d1b52f9d0c1d68dad65230901c750e63aa"
+    sha256 cellar: :any_skip_relocation, sierra:         "fc2745b24be00e6b8e4b82d6768632810823ffff3f80ad99ca9943b31d003003"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "375d6c697493099bc84e7b61eadeadbb75a34c05d724b558a3ff03d7e267d196"
   end
 
   def install
