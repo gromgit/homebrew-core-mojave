c8001a03c4e90324f016ca737c123132c4d9d17f
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:04:50 +0800

    apibuilder-cli: no rebuilding required

--- Formula/apibuilder-cli.rb
+++ Formula/apibuilder-cli.rb
@@ -1,23 +1,15 @@
 class ApibuilderCli < Formula
   desc "Command-line interface to generate clients for api builder"
   homepage "https://www.apibuilder.io"
-  url "https://github.com/apicollective/apibuilder-cli/archive/0.1.43.tar.gz"
-  sha256 "69c8c100ba9d56e83c146b5338c2a68c189fc6d1ca2eb184f357a84091224077"
+  url "https://github.com/apicollective/apibuilder-cli/archive/0.1.45.tar.gz"
+  sha256 "4048f5a0de69ef02e83245baf17b7bf5ee7f969c6d7e400194042f8e519f7fb9"
   license "MIT"
-  revision 1
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "635386febe5f3684c6ad68bc0a3372dfe899042ecd6fcb73f0a7e0ac39f8dfc1"
+    sha256 cellar: :any_skip_relocation, all: "3bce45cf8db50e54af85288fe5de380d0a0e42f3fda646eb30fc44b1e1ace844"
   end
 
   uses_from_macos "ruby"
-
-  # Fix compatibility with Ruby 3.2.
-  # https://github.com/apicollective/apibuilder-cli/pull/85
-  patch do
-    url "https://github.com/apicollective/apibuilder-cli/commit/63f84f3f021ef11eb75304bdd902950fb7cafb49.patch?full_index=1"
-    sha256 "0a99973808f37455f544fad91d677107418f008a8baff3bb59a47add63c6401e"
-  end
 
   def install
     system "./install.sh", prefix
