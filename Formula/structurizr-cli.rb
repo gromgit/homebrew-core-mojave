7ca014dbd5510f787febd093c260ed22c1d9a106
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:26:20 +0800

    structurizr-cli: no rebuilding required

--- Formula/structurizr-cli.rb
+++ Formula/structurizr-cli.rb
@@ -1,12 +1,12 @@
 class StructurizrCli < Formula
   desc "Command-line utility for Structurizr"
   homepage "https://structurizr.com"
-  url "https://github.com/structurizr/cli/releases/download/v1.20.1/structurizr-cli-1.20.1.zip"
-  sha256 "6ba9133243e1780200042c030760b56ca89ca802673a0618726419522c82be8b"
+  url "https://github.com/structurizr/cli/releases/download/v1.25.0/structurizr-cli-1.25.0.zip"
+  sha256 "1a9ca6684a7711d6613458de158788f4350d975f5eb112a220c74eb9f9a7a899"
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "0610b6c37e4931615f7410ddf32f0c8e999728604443d08419f24d755cc4fadf"
+    sha256 cellar: :any_skip_relocation, all: "4f8855b37a9cd3fd3283a6c70890b79b3eecbe3e0ad3bfe977ca6b5199462c1b"
   end
 
   depends_on "openjdk"
@@ -20,7 +20,7 @@ class StructurizrCli < Formula
   test do
     result = pipe_output("#{bin}/structurizr-cli").strip
     # not checking `Structurizr DSL` version as it is different binary
-    assert_match "Structurizr CLI v#{version}", result
-    assert_match "Usage: structurizr push|pull|lock|unlock|export|validate|list [options]", result
+    assert_match "structurizr-cli: #{version}", result
+    assert_match "Usage: structurizr push|pull|lock|unlock|export|validate|list|help [options]", result
   end
 end
