c63203aca7f24fef496743cae4cede74635a4c9d
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:08:11 +0800

    crun:

--- Formula/crun.rb
+++ Formula/crun.rb
@@ -1,12 +1,12 @@
 class Crun < Formula
   desc "Fast and lightweight fully featured OCI runtime and C library"
   homepage "https://github.com/containers/crun"
-  url "https://github.com/containers/crun/releases/download/1.7.2/crun-1.7.2.tar.xz"
-  sha256 "dfce0fdf042c7de84e8672369f54f723c2f788d2bde076a4c6edf530e6306b5a"
+  url "https://github.com/containers/crun/releases/download/1.8/crun-1.8.tar.xz"
+  sha256 "6dc621b6825eaf0d6a8f70d2a7fa9adde24cee3ad9221a5efbadb0b8b25e0843"
   license any_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "aa657e22ae5f3aac1539214d2535f12c93c26dc43099234a5c2e328b124c0e21"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "78b07b4f879e5d8b707a1e66a22088b5742e996963705e24bb98ec8eb5389493"
   end
 
   depends_on "autoconf" => :build
@@ -30,6 +30,6 @@ class Crun < Formula
   end
 
   test do
-    assert_equal "", shell_output("crun --rootless=true list -q").strip
+    assert_empty shell_output("#{bin}/crun --rootless=true list -q").strip
   end
 end
