a79cd8a828dc6b6965391d885fec2a9c68136014
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:19:52 +0800

    ncc: no rebuilding required

--- Formula/ncc.rb
+++ Formula/ncc.rb
@@ -3,12 +3,12 @@ require "language/node"
 class Ncc < Formula
   desc "Compile a Node.js project into a single file"
   homepage "https://github.com/vercel/ncc"
-  url "https://registry.npmjs.org/@vercel/ncc/-/ncc-0.36.0.tgz"
-  sha256 "6b31b548faf760f9b8feb2a091fcfbf98c6590515d14df799ff615cba487236a"
+  url "https://registry.npmjs.org/@vercel/ncc/-/ncc-0.36.1.tgz"
+  sha256 "8cd2353563a2d1d60b8f48b647aa2f0a695f3e4a265f8fdf09eb53f23b62df54"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "bc7bb6a47adac6089878c4799d4a61ee35b34878b1db481915a6c5c6479a79ac"
+    sha256 cellar: :any_skip_relocation, all: "3dbe068a96ae2bef00692db5257194bd9cbe02309ecf4fda2a50cade428beb58"
   end
 
   depends_on "node"
