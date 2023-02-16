58446ecd2998bd6e1a1f99c598a08b695a74681b
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:23:24 +0800

    pyqt-builder: no rebuilding required

--- Formula/pyqt-builder.rb
+++ Formula/pyqt-builder.rb
@@ -1,14 +1,13 @@
 class PyqtBuilder < Formula
   desc "Tool to build PyQt"
   homepage "https://www.riverbankcomputing.com/software/pyqt-builder/intro"
-  url "https://files.pythonhosted.org/packages/3e/ed/fa5ffe4a72903747cdd5860a46483795edc0b70562015dde42724e9ce00f/PyQt-builder-1.14.0.tar.gz"
-  sha256 "6755931c6d2f8940553e0334d10c933ce5cc18b64425e94fda1accf4ff774f59"
+  url "https://files.pythonhosted.org/packages/21/6c/685981114cb350f2f8e4a0827aa7f60f142e15816aa48f3204e5a1e2578c/PyQt-builder-1.14.1.tar.gz"
+  sha256 "83bc3e300aff8b41405804b6a9c2913389ab59c48ad9f0cb8584a6ef73bca502"
   license any_of: ["GPL-2.0-only", "GPL-3.0-only"]
-  revision 1
   head "https://www.riverbankcomputing.com/hg/PyQt-builder", using: :hg
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "be08ab36285909a7513b5e25d70cc9c515ac4a3dbb6487b21c38084aa1250765"
+    sha256 cellar: :any_skip_relocation, all: "8aa06ea0083fdd3f9ce128acf4cec5b124e4f47cca394cc62c5519b418f99285"
   end
 
   depends_on "python@3.11"
