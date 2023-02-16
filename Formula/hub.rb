b7fe8cd0981b50948b62558cb92a5e83c800db06
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:14:29 +0800

    hub: no rebuilding required

--- Formula/hub.rb
+++ Formula/hub.rb
@@ -19,8 +19,8 @@ class Hub < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "213636e856404251ffd7897357ab91cc9519d3852e4b28cbb43575988d9bbc1b"
   end
 
-  # Bump to 1.18 on the next release, if possible.
-  depends_on "go@1.17" => :build
+  # Bump to 1.19 on the next release, if possible.
+  depends_on "go@1.18" => :build
 
   uses_from_macos "ruby" => :build
 
