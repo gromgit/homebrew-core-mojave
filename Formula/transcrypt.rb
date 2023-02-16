d4984be9bfbeb22027c5277603356b6a809d03a6
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:27:37 +0800

    transcrypt: no rebuilding required

--- Formula/transcrypt.rb
+++ Formula/transcrypt.rb
@@ -1,17 +1,18 @@
 class Transcrypt < Formula
   desc "Configure transparent encryption of files in a Git repo"
   homepage "https://github.com/elasticdog/transcrypt"
-  url "https://github.com/elasticdog/transcrypt/archive/v2.2.0.tar.gz"
-  sha256 "6ea1dd8c3c7306e54f056728c7344ed31cf52403566e6c2268acc0f984d53391"
+  url "https://github.com/elasticdog/transcrypt/archive/v2.2.1.tar.gz"
+  sha256 "6453d2b2c3f0d903d65d9658d638f40d6e3aeba29ed792919f21477ea570a525"
   license "MIT"
   head "https://github.com/elasticdog/transcrypt.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "c2808cb2a2183c73a48c6588ad8678367dcfa4447eb88d12daeb591e0f37cf22"
+    sha256 cellar: :any_skip_relocation, all: "e56695d14e372bf34d9efe2b621aad7867b075bc136142fb2cf1c96bdc8318bd"
   end
 
   on_linux do
     depends_on "util-linux"
+    depends_on "vim" # needed for xxd
   end
 
   def install
