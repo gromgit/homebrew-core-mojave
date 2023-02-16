597cb284f6b96cd46151d79b95ff2cb66a5d52ba
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:05:44 +0800

    bats-core: no rebuilding required

--- Formula/bats-core.rb
+++ Formula/bats-core.rb
@@ -1,12 +1,12 @@
 class BatsCore < Formula
   desc "Bash Automated Testing System"
   homepage "https://github.com/bats-core/bats-core"
-  url "https://github.com/bats-core/bats-core/archive/v1.8.2.tar.gz"
-  sha256 "0f2df311a536e625a72bff64c838e67c7b5032e6ea9edcdf32758303062b2f3b"
+  url "https://github.com/bats-core/bats-core/archive/v1.9.0.tar.gz"
+  sha256 "3c64295318d157616ca2478de819371e2c579ccdab30c1f0b7279f7b214fb1f4"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "40d5c46e73b90a20ba13bc3bdfdfd3d9bfcbf3286c1d4d1e524fef64b87ffdb1"
+    sha256 cellar: :any_skip_relocation, all: "71fce4432c97bf46f4ae681afbf2958175b25bb6297a1b11ee10d553d6eddc93"
   end
 
   depends_on "coreutils"
