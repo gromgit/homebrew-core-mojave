42afe80d434000a22948190cdb0a62c2eda4a37e
Adrian Ho <aho+vm-mojave@03s.net>
2023-02-16 18:27:14 +0800

    threemux: no rebuilding required

--- Formula/threemux.rb
+++ Formula/threemux.rb
@@ -7,8 +7,10 @@ class Threemux < Formula
   head "https://github.com/aaronjanse/3mux.git", branch: "master"
 
   bottle do
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "cef0aa7766c9cf4045c403ae53f20fa8fcb4913c80148b01755b4a4da7449d8e"
     sha256 cellar: :any_skip_relocation, arm64_monterey: "471a6870e6b9d45e994d2977c855e0aea4ed22b1f59f0d09afaf4d096edd0e2c"
     sha256 cellar: :any_skip_relocation, arm64_big_sur:  "2d3dd3465938d0ac5b845b07689a08b667613210d9d58649c9a152ade32dc347"
+    sha256 cellar: :any_skip_relocation, ventura:        "2ca8026a1a322986b2ad2697314f37faf58634c0de2ca4722cfbe194c05006d8"
     sha256 cellar: :any_skip_relocation, monterey:       "64497230017e4e033cbafc7ae8f684e5ff77c1a8af3ec153f715269d5f4dab01"
     sha256 cellar: :any_skip_relocation, big_sur:        "c87ed9904dccc4872aa6c8ed0e6de39bc7f3ccdb5fa7fef1b99e45871d85da18"
     sha256 cellar: :any_skip_relocation, catalina:       "8071788129cb66bd2e7c6fe9f877a56fe2807b70204747a858a4e68a650a07b8"
@@ -16,8 +18,8 @@ class Threemux < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "33200f3fd9175837129386fdfa67575eebacc806d9da099d98b0888aaff56124"
   end
 
-  # Bump to 1.18 on the next release, if possible.
-  depends_on "go@1.17" => :build
+  # Bump to 1.19 on the next release, if possible.
+  depends_on "go@1.18" => :build
 
   def install
     system "go", "build", *std_go_args, "-o", bin/"3mux"
